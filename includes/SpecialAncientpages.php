4105137d8a03315a73f996fe9691818e0bb0d733
Erik Moeller <erik@users.mediawiki.org>
2003-05-20 00:53:04 +0000

    New feature (special page): Ancient pages: list oldest articles first

---
+++ includes/SpecialAncientpages.php
@@ -0,0 +1,50 @@
+<?
+
+function wfSpecialAncientpages()
+{
+	global $wgUser, $wgOut, $wgLang, $wgTitle;
+	global $limit, $offset; # From query string
+	$fname = "wfSpecialAncientpages";
+
+	if ( ! $limit ) {
+		$limit = $wgUser->getOption( "rclimit" );
+		if ( ! $limit ) { $limit = 50; }
+	}
+	if ( ! $offset ) { $offset = 0; }
+
+	$sql = "SELECT cur_title,cur_user,cur_user_text,cur_comment," .
+	  "cur_timestamp FROM cur " .
+	  "WHERE cur_namespace=0 AND cur_is_redirect=0 " .
+	  " ORDER BY cur_timestamp LIMIT {$offset}, {$limit}";
+	$res = wfQuery( $sql, $fname );
+
+	$top = wfShowingResults( $offset, $limit );
+	$wgOut->addHTML( "<p>{$top}\n" );
+
+	$sl = wfViewPrevNext( $offset, $limit,
+	  $wgLang->specialPage( "Ancientpages" ) );
+	$wgOut->addHTML( "<br>{$sl}\n" );
+
+	$sk = $wgUser->getSkin();
+	$s = "<ol start=" . ( $offset + 1 ) . ">";
+	while ( $obj = wfFetchObject( $res ) ) {
+		$u = $obj->cur_user;
+		$ut = $obj->cur_user_text;
+		$c = wfEscapeHTML( $obj->cur_comment );
+		if ( 0 == $u ) { $ul = $ut; }
+		else { $ul = $sk->makeLink( $wgLang->getNsText(2).":{$ut}", $ut ); }
+
+		$d = $wgLang->timeanddate( $obj->cur_timestamp, true );
+		$link = $sk->makeKnownLink( $obj->cur_title, "" );
+		$s .= "<li>{$d} {$link} . . {$ul}";
+
+		if ( "" != $c && "*" != $c ) { $s .= " <em>({$c})</em>"; }
+		$s .= "</li>\n";
+	}
+	wfFreeResult( $res );
+	$s .= "</ol>";
+	$wgOut->addHTML( $s );
+	$wgOut->addHTML( "<p>{$sl}\n" );
+}
+
+?>
