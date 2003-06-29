8ef1df9f60cf9de4db5fe201a4e77bc536697ec4
Brion Vibber <brion@users.mediawiki.org>
2003-06-29 20:24:26 +0000

    Add link to help page from preferences screen (now wiki text, can set appropriately for each language...

--- languages/Language.php
+++ languages/Language.php
@@ -596,7 +596,9 @@ $2 List redirects &nbsp; Search for $3 $9",
   wfLocalUrl( "Special:Userlogin" ) . "\">logged in</a>
 to set user preferences.",
 "prefslogintext" => "You are logged in as \"$1\".
-Your internal ID number is $2.",
+Your internal ID number is $2.
+
+See [[Wikipedia:User preferences help]] for help deciphering the options.",
 "prefsreset"	=> "Preferences have been reset from storage.",
 "qbsettings"	=> "Quickbar settings", 
 "changepassword" => "Change password",
@@ -1183,11 +1185,11 @@ class Language {
 		}
 		if ( 0 == $diff ) { return $ts; }
 
-		$t = mktime( ( (int)substr( $ts, 8, 2) ) + $diff,
+		$t = gmmktime( ( (int)substr( $ts, 8, 2) ) + $diff,
 		  (int)substr( $ts, 10, 2 ), (int)substr( $ts, 12, 2 ),
 		  (int)substr( $ts, 4, 2 ), (int)substr( $ts, 6, 2 ),
 		  (int)substr( $ts, 0, 4 ) );
-		return date( "YmdHis", $t );
+		return gmdate( "YmdHis", $t );
 	}
  
 	function date( $ts, $adj = false )
@@ -1222,7 +1224,7 @@ class Language {
 
 	function rfc1123( $ts )
 	{
-		return date( "D, d M Y H:i:s T", $ts );
+		return gmdate( "D, d M Y H:i:s T", $ts );
 	}
 
 	function getValidSpecialPages()
