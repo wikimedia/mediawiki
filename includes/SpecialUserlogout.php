caae822a1b9a6cd1383bc004ae86952bb2205ff5
Tim Starling <tstarling@users.mediawiki.org>
2003-08-25 13:25:39 +0000

    bug fix to main branch

--- includes/SpecialUserlogout.php
+++ includes/SpecialUserlogout.php
@@ -5,6 +5,7 @@ function wfSpecialUserlogout()
 	global $wgUser, $wgOut, $returnto;
 
 	$wgUser->logout();
+	$wgOut->mCookies = array();
 	$wgOut->setRobotpolicy( "noindex,nofollow" );
 	$wgOut->addHTML( wfMsg( "logouttext" ) . "\n<p>" );
 	$wgOut->returnToMain();
