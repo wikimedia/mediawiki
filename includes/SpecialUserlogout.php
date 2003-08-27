16369025c28190a0890354890dec69053b572c03
Tim Starling <tstarling@users.mediawiki.org>
2003-08-27 14:33:12 +0000

    removed useless (possibly dangerous) line

--- includes/SpecialUserlogout.php
+++ includes/SpecialUserlogout.php
@@ -5,7 +5,6 @@ function wfSpecialUserlogout()
 	global $wgUser, $wgOut, $returnto;
 
 	$wgUser->logout();
-	$wgOut->mCookies = array();
 	$wgOut->setRobotpolicy( "noindex,nofollow" );
 	$wgOut->addHTML( wfMsg( "logouttext" ) . "\n<p>" );
 	$wgOut->returnToMain();
