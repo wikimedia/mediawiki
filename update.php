f45ca8f9aaa692d7df36c33963338a006f065abb
Mr. E23 <e23@users.mediawiki.org>
2003-11-08 15:32:28 +0000

    Further link-cache related stuff.

--- update.php
+++ update.php
@@ -1,5 +1,18 @@
 <?
 
+
+/*
+
+  TODO: Links cache will be very unhappy without a table like this //e23
+
+	CREATE TABLE linkscc (lcc_pageid INT UNSIGNED NOT NULL UNIQUE KEY,
+                             lcc_title VARCHAR(255) NOT NULL UNIQUE KEY,
+                             lcc_cacheobj MEDIUMBLOB NOT NULL);
+
+*/
+
+
+
 # Update already-installed software
 #
 
