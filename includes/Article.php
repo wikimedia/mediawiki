502964b2dcc691fbf1027c921b0649121fcbe4a2
Brion Vibber <brion@users.mediawiki.org>
2003-12-17 09:41:17 +0000

    We no longer check the content, so have to make an explicit exception for the main page for short cache expiry

--- includes/Article.php
+++ includes/Article.php
@@ -1219,8 +1219,8 @@ class Article {
 		$called = true;
 		if($this->isFileCacheable()) {
 			$touched = $this->mTouched;
-			if( strpos( $this->mContent, "{{" ) !== false ) {
-				# Expire pages with variable replacements in an hour
+			if( $this->mTitle->getPrefixedDBkey() == wfMsg( "mainpage" ) ) {
+				# Expire the main page quicker
 				$expire = wfUnix2Timestamp( time() - 3600 );
 				$touched = max( $expire, $touched );
 			}
