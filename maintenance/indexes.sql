b203102412d3840536d244bf450cbfb8f0c9c356
Brion Vibber <brion@users.mediawiki.org>
2004-01-25 22:56:54 +0000

    Add index to recentchanges to optimize Special:Newpages

--- maintenance/indexes.sql
+++ maintenance/indexes.sql
@@ -58,7 +58,8 @@ ALTER TABLE searchindex
 ALTER TABLE recentchanges
   ADD INDEX rc_timestamp (rc_timestamp),
   ADD INDEX rc_namespace_title (rc_namespace, rc_title),
-  ADD INDEX rc_cur_id (rc_cur_id);
+  ADD INDEX rc_cur_id (rc_cur_id),
+  ADD INDEX new_name_timestamp(rc_new,rc_namespace,rc_timestamp);
 
 ALTER TABLE archive
   ADD KEY `name_title_timestamp` (`ar_namespace`,`ar_title`,`ar_timestamp`);
