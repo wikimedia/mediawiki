a84402a5301e4ede2210c043bed05e366f6c3252
Tim Starling <tstarling@users.mediawiki.org>
2004-01-25 02:27:49 +0000

    Ability to declare all query pages "expensive"

--- includes/SpecialPopularpages.php
+++ includes/SpecialPopularpages.php
@@ -4,25 +4,25 @@ include_once( "QueryPage.php" );
 
 class PopularPagesPage extends QueryPage {
 
-    function getName() {
-	return "Popularpages";
-    }
-    
-    function isExpensive() {
-	return 1;
-    }
-    
-    function getSQL( $offset, $limit ) {
-	return "SELECT DISTINCT cur_title, cur_counter FROM cur " .
-	  "WHERE cur_namespace=0 AND cur_is_redirect=0 ORDER BY " .
-	  "cur_counter DESC LIMIT {$offset}, {$limit}";
-    }
-    
-    function formatResult( $skin, $result ) {
-	$link = $skin->makeKnownLink( $result->cur_title, "" );
-	$nv = wfMsg( "nviews", $result->cur_counter );
-	return "{$link} ({$nv})";
-    }
+	function getName() {
+		return "Popularpages";
+	}
+
+	function isExpensive() {
+		return 1;
+	}
+
+	function getSQL( $offset, $limit ) {
+		return "SELECT DISTINCT cur_title, cur_counter FROM cur " .
+		  "WHERE cur_namespace=0 AND cur_is_redirect=0 ORDER BY " .
+		  "cur_counter DESC LIMIT {$offset}, {$limit}";
+	}
+
+	function formatResult( $skin, $result ) {
+		$link = $skin->makeKnownLink( $result->cur_title, "" );
+		$nv = wfMsg( "nviews", $result->cur_counter );
+		return "{$link} ({$nv})";
+	}
 }
 
 function wfSpecialPopularpages()
--- includes/SpecialWantedpages.php
+++ includes/SpecialWantedpages.php
@@ -3,42 +3,42 @@
 include_once ( "QueryPage.php" ) ;
 
 class WantedPagesPage extends QueryPage {
-    
-    function getName() {
-	return "Wantedpages";
-    }
-    
-    function isExpensive() {
-	return 1;
-    }
-    
-    function getSQL( $offset, $limit ) {
-	return "SELECT bl_to, COUNT( DISTINCT bl_from ) as nlinks " .
-	  "FROM brokenlinks GROUP BY bl_to HAVING nlinks > 1 " .
-	  "ORDER BY nlinks DESC LIMIT {$offset}, {$limit}";
-    }
 
-    function formatResult( $skin, $result ) {
-	global $wgLang;
-	
-	$nt = Title::newFromDBkey( $result->bl_to );
+	function getName() {
+		return "Wantedpages";
+	}
 
-	$plink = $skin->makeBrokenLink( $nt->getPrefixedText(), "" );
-	$nl = wfMsg( "nlinks", $result->nlinks );
-	$nlink = $skin->makeKnownLink( $wgLang->specialPage( "Whatlinkshere" ), $nl,
-				     "target=" . $nt->getPrefixedURL() );
-	
-	return "{$plink} ({$nlink})";
-    }
+	function isExpensive() {
+		return 1;
+	}
+
+	function getSQL( $offset, $limit ) {
+		return "SELECT bl_to, COUNT( DISTINCT bl_from ) as nlinks " .
+		  "FROM brokenlinks GROUP BY bl_to HAVING nlinks > 1 " .
+		  "ORDER BY nlinks DESC LIMIT {$offset}, {$limit}";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgLang;
+
+		$nt = Title::newFromDBkey( $result->bl_to );
+
+		$plink = $skin->makeBrokenLink( $nt->getPrefixedText(), "" );
+		$nl = wfMsg( "nlinks", $result->nlinks );
+		$nlink = $skin->makeKnownLink( $wgLang->specialPage( "Whatlinkshere" ), $nl,
+		  "target=" . $nt->getPrefixedURL() );
+
+		return "{$plink} ({$nlink})";
+	}
 }
 
 function wfSpecialWantedpages()
 {
-    list( $limit, $offset ) = wfCheckLimits();
-    
-    $wpp = new WantedPagesPage();
-    
-    $wpp->doQuery( $offset, $limit );
+	list( $limit, $offset ) = wfCheckLimits();
+
+	$wpp = new WantedPagesPage();
+
+	$wpp->doQuery( $offset, $limit );
 }
 
 ?>
