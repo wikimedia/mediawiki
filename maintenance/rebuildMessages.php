d6dd3689f893c2b763825650a895de651c780288
Tim Starling <tstarling@users.mediawiki.org>
2004-01-25 02:40:50 +0000

    Better namespace 8 updating, and watchlist caching in memcached.doc

--- maintenance/rebuildMessages.php
+++ maintenance/rebuildMessages.php
@@ -13,15 +13,39 @@ include_once( "../LocalSettings.php" );
 include_once( "../AdminSettings.php" );
 
 $sep = strchr( $include_path = ini_get( "include_path" ), ";" ) ? ";" : ":";
-ini_set( "include_path", "$IP$sep$include_path" );
+ini_set( "include_path", "../includes$sep../languages$sep$include_path" );
 
 include_once( "Setup.php" );
 include_once( "./InitialiseMessages.inc" );
+include_once( "../install-utils.inc" );
 $wgTitle = Title::newFromText( "Rebuild messages script" );
 $wgCommandLineMode = true;
 set_time_limit(0);
 
-initialiseMessages( true );
+print	"1. Update messages to include latest additions to Language.php\n" . 
+		"2. Delete all messages and reinitialise namespace\n" .
+		"3. Quit\n\n".
+		
+		"Please enter a number: ";
+
+do {
+	$response = IntVal(readconsole());
+	if ( $response >= 1 && $response <= 3 ) {
+		$good = true;
+	} else {
+		$good = false;
+		print "Please type a number between 1 and 3: ";
+	}
+} while ( !$good );
+
+switch ( $response ) {
+	case 1:
+		initialiseMessages( false );
+		break;
+	case 2:
+		initialiseMessages( true );
+		break;
+}
 
 exit();
 
