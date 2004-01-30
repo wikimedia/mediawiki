f51100df8605bf3a3741dd5f477d8d6ef92de706
Gabriel Wicke <gwicke@users.mediawiki.org>
2004-01-30 16:06:22 +0000

    *** empty log message ***

---
+++ includes/SquidUpdate.php
@@ -0,0 +1,34 @@
+<?
+# See deferred.doc
+
+class SquidUpdate {
+
+        function SquidUpdate( $title, $urlArr = Array() )
+        {
+                $this->title = $title;
+                $this->urlArr = $urlArr;
+        }
+
+
+        function doUpdate()
+        {
+                if (count( $this->urlArr ) == 0) { // newly created Article
+                        global $wgInternalServer;
+                        /* prepare the list of urls to purge */
+                        $id= $this->title->getArticleID();
+                        $sql = "SELECT l_from FROM links WHERE l_to={$id}" ;
+                        $res = wfQuery ( $sql, DB_READ ) ;
+                        while ( $BL = wfFetchObject ( $res ) )
+                        {
+                                $t = Title::newFromDBkey( $BL->l_from) ; 
+                                $this->urlArr[] = $wgInternalServer.wfLocalUrl( $t->getPrefixedURL() );
+                        }
+                        wfFreeResult ( $res ) ;
+
+        }
+
+        wfPurgeSquidServers($this->urlArr);
+}
+}
+
+?>
