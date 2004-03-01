f46917fd9b253faacc792050b3c2be59b0094232
Brion Vibber <brion@users.mediawiki.org>
2004-03-01 05:51:55 +0000

    Clear message cache on rebuild

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6,7 +6,7 @@
 # like $wgScriptPath, you must also localize everything that
 # depends on it.
 
-$wgVersion			= "1.2.0rc1";
+$wgVersion			= "1.3.0pre-alpha";
 
 $wgSitename         = "MediaWiki"; # Please customize!
 $wgMetaNamespace    = FALSE; # will be same as you set $wgSitename
