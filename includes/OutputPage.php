ed5f76e4818d6e95c6fa0fbe69d357f597ff4c42
Brion Vibber <brion@users.mediawiki.org>
2004-03-06 09:34:05 +0000

    Fix timestamp error in redirects (fails in particularly ugly fashion on Windows server)

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -247,8 +247,7 @@ class OutputPage {
 				if( !$wgDebugRedirects ) {
 					header("HTTP/1.1 {$this->mRedirectCode} Moved Permanently");
 				}
-				$this->mLastModified = gmdate( "D, j M Y H:i:s", wfTimestamp2Unix(
-					max( $timestamp, $wgUser->mTouched ) ) ) . " GMT";
+				$this->mLastModified = gmdate( "D, j M Y H:i:s" ) . " GMT";
 			}
                         
 			$this->sendCacheControl();
