ec45cbb0453062e459097d5cdd2b2bf5cd6da892
Tim Starling <tstarling@users.mediawiki.org>
2004-03-08 09:43:36 +0000

    code formatting

--- includes/SquidUpdate.php
+++ includes/SquidUpdate.php
@@ -3,32 +3,32 @@
 
 class SquidUpdate {
 
-        function SquidUpdate( $title, $urlArr = Array() )
-        {
-                $this->title = $title;
-                $this->urlArr = $urlArr;
-        }
+	function SquidUpdate( $title, $urlArr = Array() )
+	{
+		$this->title = $title;
+		$this->urlArr = $urlArr;
+	}
 
 
-        function doUpdate()
-        {
-                if (count( $this->urlArr ) == 0) { // newly created Article
-                        global $wgInternalServer;
-                        /* prepare the list of urls to purge */
-                        $id= $this->title->getArticleID();
-                        $sql = "SELECT l_from FROM links WHERE l_to={$id}" ;
-                        $res = wfQuery ( $sql, DB_READ ) ;
-                        while ( $BL = wfFetchObject ( $res ) )
-                        {
-                                $t = Title::newFromDBkey( $BL->l_from) ; 
-                                $this->urlArr[] = $wgInternalServer.$t->getLocalURL() ;
-                        }
-                        wfFreeResult ( $res ) ;
+	function doUpdate()
+	{
+		if (count( $this->urlArr ) == 0) { // newly created Article
+			global $wgInternalServer;
+			/* prepare the list of urls to purge */
+			$id= $this->title->getArticleID();
+			$sql = "SELECT l_from FROM links WHERE l_to={$id}" ;
+			$res = wfQuery ( $sql, DB_READ ) ;
+			while ( $BL = wfFetchObject ( $res ) )
+			{
+				$t = Title::newFromDBkey( $BL->l_from) ; 
+				$this->urlArr[] = $wgInternalServer.$t->getLocalURL() ;
+			}
+			wfFreeResult ( $res ) ;
 
-        }
+		}
 
-        wfPurgeSquidServers($this->urlArr);
-}
+		wfPurgeSquidServers($this->urlArr);
+	}
 }
 
 ?>
