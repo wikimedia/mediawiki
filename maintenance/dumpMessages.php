523af302002ef73a8b62c36ff76d6d58219afe4a
Tim Starling <tstarling@users.mediawiki.org>
2004-03-08 09:33:23 +0000

    ability to dump messages to a file, not quite sure what to use it for yet

---
+++ maintenance/dumpMessages.php
@@ -0,0 +1,17 @@
+<?php
+include_once( "commandLine.inc" );
+
+$messages = array();
+foreach ( $wgAllMessagesEn as $key => $englishValue )
+{
+	$messages[$key] = wfMsg( $key );
+}
+
+if ( count( $argv ) >= 2 ) {
+	$res = fopen( $argv[2] );
+	fwrite( $res, serialize( $messages ) );
+} else {
+	print serialize( $messages );
+}
+
+?>
