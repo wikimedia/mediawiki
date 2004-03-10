cda4adc30d4799d55cc4863e468dca3924f3108b
Mr. E23 <e23@users.mediawiki.org>
2004-03-10 23:43:54 +0000

    Made linkscc title column case sensitive

--- maintenance/archives/patch-linkscc.sql
+++ maintenance/archives/patch-linkscc.sql
@@ -5,6 +5,6 @@
 
 CREATE TABLE IF NOT EXISTS linkscc (
   lcc_pageid INT UNSIGNED NOT NULL UNIQUE KEY,
-  lcc_title VARCHAR(255) NOT NULL UNIQUE KEY,
+  lcc_title VARCHAR(255) binary NOT NULL UNIQUE KEY,
   lcc_cacheobj MEDIUMBLOB NOT NULL);
 
--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -93,7 +93,7 @@ CREATE TABLE brokenlinks (
 DROP TABLE IF EXISTS linkscc;
 CREATE TABLE linkscc (
   lcc_pageid INT UNSIGNED NOT NULL UNIQUE KEY,
-  lcc_title VARCHAR(255) NOT NULL UNIQUE KEY,
+  lcc_title VARCHAR(255) binary NOT NULL UNIQUE KEY,
   lcc_cacheobj MEDIUMBLOB NOT NULL
 ) TYPE=MyISAM;
 
