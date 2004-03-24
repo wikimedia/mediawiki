635388356be70130c00c823d630fe88ded4c3042
Brion Vibber <brion@users.mediawiki.org>
2004-03-24 08:40:00 +0000

    Some doc updates for release...

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6,7 +6,7 @@
 # like $wgScriptPath, you must also localize everything that
 # depends on it.
 
-$wgVersion			= "1.2.0rc4";
+$wgVersion			= "1.2.0";
 
 $wgSitename         = "MediaWiki"; # Please customize!
 $wgMetaNamespace    = FALSE; # will be same as you set $wgSitename
