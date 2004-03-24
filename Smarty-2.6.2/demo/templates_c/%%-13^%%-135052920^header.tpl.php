616049cdd5b220097a0783bccdd7358dffb0a56b
Jens Frank <jeluf@users.mediawiki.org>
2004-03-24 19:55:25 +0000

    Smarty-2.6.2 import

---
+++ Smarty-2.6.2/demo/templates_c/%%-13^%%-135052920^header.tpl.php
@@ -0,0 +1,13 @@
+<?php /* Smarty version 2.6.2, created on 2004-03-24 20:26:07
+         compiled from header.tpl */ ?>
+<?php require_once(SMARTY_DIR . 'core' . DIRECTORY_SEPARATOR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('function', 'popup_init', 'header.tpl', 3, false),)), $this); ?>
+<HTML>
+<HEAD>
+<?php echo smarty_function_popup_init(array('src' => "/javascripts/overlib.js"), $this);?>
+
+<TITLE><?php echo $this->_tpl_vars['title']; ?>
+ - <?php echo $this->_tpl_vars['Name']; ?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
