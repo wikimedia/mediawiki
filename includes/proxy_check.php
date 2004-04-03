2f7803c61a9b8996c8c48c20fc615e028426961f
Erik Moeller <erik@users.mediawiki.org>
2004-04-03 08:23:25 +0000

    update from live site (proxy_check2.php)

--- includes/proxy_check.php
+++ includes/proxy_check.php
@@ -4,27 +4,40 @@
 
 # Exit if there are not enough parameters, or if it's not command line mode
 
+$output = "";
 if ( ( isset( $_REQUEST ) && array_key_exists( "argv", $_REQUEST ) ) || count( $argv ) < 4 ) {
-	exit();
+	$output .= "Incorrect parameters\n";
+} else {
+
+	# Get parameters
+	$ip = $argv[1];
+	$port = $argv[2];
+	$url = $argv[3];
+	$host = trim(`hostname`);
+	$output = "Connecting to $ip:$port, target $url, this hostname $host\n";
+
+	# Open socket
+	$sock = @fsockopen($ip, $port, $errno, $errstr, 5);
+	if ($errno == 0 ) {
+		$output .= "Connected\n";
+		# Send payload
+		$request = "GET $url HTTP/1.0\r\n";
+#		$request .= "Proxy-Connection: Keep-Alive\r\n";
+#		$request .= "Pragma: no-cache\r\n";
+#		$request .= "Host: ".$url."\r\n";
+#		$request .= "User-Agent: MediaWiki open proxy check\r\n";
+		$request .= "\r\n";
+		@fputs($sock, $request);
+		$response = fgets($sock, 65536);
+		$output .= $response;
+		@fclose($sock);
+	} else {
+		$output .= "No connection\n";
+	}
 }
 
-# Get parameters
-$ip = $argv[1];
-$port = $argv[2];
-$url = $argv[3];
+$output = escapeshellarg( $output );
 
-# Open socket
-$sock = fsockopen($ip, $port, $errno, $errstr, 5);
-if ($errno == 0 ) {
-	# Send payload
-	$request = "GET $url HTTP/1.0\r\n";
-	$request .= "Proxy-Connection: Keep-Alive\r\n";
-	$request .= "Pragma: no-cache\r\n";
-	$request .= "Host: ".$url."\r\n";
-	$request .= "User-Agent: MediaWiki open proxy check\r\n";
-	$request .= "\r\n";
-	fputs($sock, $request);
-	$response = fgets($sock, 128);
-	fclose($sock);
-}
+#`echo $output >> /home/tstarling/open/proxy.log`;
+
 ?>
