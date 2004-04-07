a33e48cfc0bb38f1f2c560f6265f73664b60fd40
Erik Moeller <erik@users.mediawiki.org>
2004-04-07 10:02:22 +0000

    -unnecessary skin declaration; thanks, marco

--- includes/SpecialNewpages.php
+++ includes/SpecialNewpages.php
@@ -20,13 +20,12 @@ class NewPagesPage extends QueryPage {
 	}
 
 	function formatResult( $skin, $result ) {
-		global $wgLang,$wgUser;
-		$sk=$wgUser->getSkin();
+		global $wgLang;
 		$u = $result->cur_user;
 		$ut = $result->cur_user_text;
 
 		$length = wfmsg( "nbytes", $wgLang->formatNum( $result->cur_length ) );
-		$c = $sk->formatComment($result->cur_comment );
+		$c = $skin->formatComment($result->cur_comment );
 
 		if ( 0 == $u ) { # not by a logged-in user
 			$ul = $ut;
