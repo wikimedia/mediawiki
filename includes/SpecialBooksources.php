8a48c8cea709e3d91e9d5da9cfa920c4772ab21f
Brion Vibber <brion@users.mediawiki.org>
2004-04-09 06:27:18 +0000

    If somebody accidentally wanders into Special:Booksources, ask for an ISBN number. The raw list isn't that useful, and will lead to broken search pages.

--- includes/SpecialBooksources.php
+++ includes/SpecialBooksources.php
@@ -18,63 +18,80 @@ function wfSpecialBooksources( $par )
 }
 
 class BookSourceList {
-
 	var $mIsbn;
 
-	function BookSourceList( $isbn )
-	{
+	function BookSourceList( $isbn ) {
 		$this->mIsbn = $isbn;
 	}
 
-	function show()
-	{
-		global $wgOut, $wgUser, $wgLang;
-		$fname = "BookSourceList::show()";
-		$noautolist = false;
+	function show() {
+		global $wgOut;
 
 		$wgOut->setPagetitle( wfMsg( "booksources" ) );
-		$bstext = wfMsg( "booksourcetext" );
-
-		if( $this->mIsbn ) {
-			$bstitle = Title::newFromText( wfmsg( "booksources" ) );
-			$sql = "SELECT cur_text FROM cur " .
-				"WHERE cur_namespace=4 and cur_title='" .
-				wfStrencode( $bstitle->getDBkey() ) . "'";
-			$res = wfQuery( $sql, DB_READ, $fname );
-			if( ( $s = wfFetchObject( $res ) ) and ( $s->cur_text != "" ) ) {	
-				$bstext = $s->cur_text;
-				$bstext = str_replace( "MAGICNUMBER", $this->mIsbn, $bstext );
-				$noautolist = true;
-			}
+		if( empty( $this->mIsbn ) ) {
+			$this->askForm();
+		} else {
+			$this->showList();
 		}
-
-		$wgOut->addWikiText( $bstext );
+	}
+	
+	function showList() {
+		global $wgOut, $wgUser, $wgLang;
+		$fname = "BookSourceList::showList()";
 		
-		# If ISBN is blank, just show a list of links to the
-		# home page of the various book sites.  Otherwise, show
-		# a list of links directly to the book.
-
-		if( !$noautolist ) { # only do this if we haven't already shown [[Wikipedia:Book sources]]
-			$s = "<ul>\n";
-			$bs = $wgLang->getBookstoreList() ;
-			$bsn = array_keys ( $bs ) ;
-			foreach ( $bsn as $name ) {
-				$adr = $bs[$name] ;
-				if ( ! $this->mIsbn ) {
-					$adr = explode ( ":" , $adr , 2 ) ;
-					$adr = explode ( "/" , $adr[1] ) ;
-					$a = "" ;
-					while ( $a == "" ) $a = array_shift ( $adr ) ;
-					$adr = "http://".$a ;
-				} else {
-					$adr = str_replace ( "$1" , $this->mIsbn , $adr ) ;
-				}
-				$s .= "<li><a href=\"{$adr}\" class=\"external\">{$name}</a></li>\n" ;
-			}
-			$s .= "</ul>\n";
-
-			$wgOut->addHTML( $s );
+		# First, see if we have a custom list setup in
+		# [[Wikipedia:Book sources]] or equivalent.
+		$bstitle = Title::newFromText( wfmsg( "booksources" ) );
+		$sql = "SELECT cur_text FROM cur " .
+			"WHERE cur_namespace=4 and cur_title='" .
+			wfStrencode( $bstitle->getDBkey() ) . "'";
+		$res = wfQuery( $sql, DB_READ, $fname );
+		if( ( $s = wfFetchObject( $res ) ) and ( $s->cur_text != "" ) ) {	
+			$bstext = $s->cur_text;
+			$bstext = str_replace( "MAGICNUMBER", $this->mIsbn, $bstext );
+			
+			$wgOut->addWikiText( $bstext );
+			return;
 		}
+		
+		# Otherwise, use the list of links in the default Language.php file.
+		$s = wfMsg( "booksourcetext" ) . "<ul>\n";
+		$bs = $wgLang->getBookstoreList() ;
+		$bsn = array_keys ( $bs ) ;
+		foreach ( $bsn as $name ) {
+			$adr = $bs[$name] ;
+			if ( ! $this->mIsbn ) {
+				$adr = explode( ":" , $adr , 2 );
+				$adr = explode( "/" , $adr[1] );
+				$a = "";
+				while ( $a == "" ) {
+					$a = array_shift( $adr );
+				}
+				$adr = "http://".$a ;
+			} else {
+				$adr = str_replace ( "$1" , $this->mIsbn , $adr ) ;
+			}
+			$name = htmlspecialchars( $name );
+			$adr = htmlspecialchars( $adr );
+			$s .= "<li><a href=\"{$adr}\" class=\"external\">{$name}</a></li>\n" ;
+		}
+		$s .= "</ul>\n";
+
+		$wgOut->addHTML( $s );
+	}
+	
+	function askForm() {
+		global $wgOut, $wgLang, $wgTitle;
+		$fname = "BookSourceList::askForm()";
+		
+		$action = $wgTitle->escapeLocalUrl();
+		$isbn = htmlspecialchars( wfMsg( "isbn" ) );
+		$go = htmlspecialchars( wfMsg( "go" ) );
+		$out = "<form action=\"$action\" method='post'>
+			$isbn: <input name='isbn' id='isbn' />
+			<input type='submit' value=\"$go\" />
+		</form>";
+		$wgOut->addHTML( $out );
 	}
 }
 
