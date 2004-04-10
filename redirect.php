054e5060995a44bda2f4f82c4d9e248d25231c28
Brion Vibber <brion@users.mediawiki.org>
2004-04-10 23:57:25 +0000

    Fix for register_globals off

--- redirect.php
+++ redirect.php
@@ -1,6 +1,17 @@
 <?php
+unset( $DP );
+unset( $IP );
+$wgCommandLineMode = false;
+
 include_once( "./LocalSettings.php" );
-global $wpDropdown, $wgArticlePath;
-$url = str_replace( "$1", $wpDropdown, $wgArticlePath );
+global $wgArticlePath;
+
+include_once( "WebRequest.php" );
+$wgRequest = new WebRequest();
+
+$page = $wgRequest->getVal( "wpDropdown" );
+
+$url = str_replace( "$1", $page, $wgArticlePath );
+
 header( "Location: {$url}" );
 ?>
