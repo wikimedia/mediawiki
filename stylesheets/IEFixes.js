3ea52811848fe6f0ffdf0d97e7e7d4d12c99d92f
Gabriel Wicke <gwicke@users.mediawiki.org>
2004-04-15 09:15:38 +0000

    some ie fixes

--- stylesheets/IEFixes.js
+++ stylesheets/IEFixes.js
@@ -8,15 +8,30 @@ function fixalpha(){
     var rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
     if (rslt != null && Number(rslt[1]) >= 5.5)
     {
+        var logoa = document.getElementById('portlet-logo').firstChild;
+        var bg = logoa.currentStyle.backgroundImage;
+        if (bg.match(/\.png/i) != null){
+            var mypng = bg.substring(5,bg.length-2);
+            logoa.style.backgroundImage = "none";
+            logoa.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+mypng+"', sizingMethod='image')";
+        }
+
+
+    
+    /*
         for (i=0; i<document.all.length; i++){
-            var bg = document.all[i].currentStyle.backgroundImage;
+            if(document.all[i].currentStyle && document.all[i].currentStyle.backgroundImage) {
+                var bg = document.all[i].currentStyle.backgroundImage;
+            } else {
+                bg = false;
+            }
             if (bg){
                 if (bg.match(/\.png/i) != null){
                     var mypng = bg.substring(5,bg.length-2);
-                    document.all[i].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+mypng+"', sizingMethod='image')";
                     document.all[i].style.backgroundImage = "none";
+                    document.all[i].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+mypng+"', sizingMethod='image')";
                 }
             }
-        }
+        }*/
     }
 }
