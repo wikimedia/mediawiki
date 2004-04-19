f26cfcb59da8e7109b2d919313789a3e934528bf
Gabriel Wicke <gwicke@users.mediawiki.org>
2004-04-19 17:04:22 +0000

    allow del and ins, other xhtml tags might be worth to allow as well (acronym and similar)

--- includes/Parser.php
+++ includes/Parser.php
@@ -1431,7 +1431,7 @@ class Parser
 		$fname = "Parser::removeHTMLtags";
 		wfProfileIn( $fname );
 		$htmlpairs = array( # Tags that must be closed
-			"b", "i", "u", "font", "big", "small", "sub", "sup", "h1",
+			"b", "del", "i", "ins", "u", "font", "big", "small", "sub", "sup", "h1",
 			"h2", "h3", "h4", "h5", "h6", "cite", "code", "em", "s",
 			"strike", "strong", "tt", "var", "div", "center",
 			"blockquote", "ol", "ul", "dl", "table", "caption", "pre",
