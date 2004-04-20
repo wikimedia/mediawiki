c9514b77351a867cead3b070b37ac0046a429b20
Brion Vibber <brion@users.mediawiki.org>
2004-04-20 06:13:11 +0000

    Obsolete, not the actual version no

--- Version.php
+++ Version.php
@@ -1,13 +1,8 @@
 <?php
-# This file is copied to the install directory so that
-# later update scripts will be able to use it to determine
-# what they need to update. The version number here must
-# be updated any time you make a change that requires
-# the update to do anything other than copy the new files
-# over, such as changing the database layout. If you
-# change this version number, you should also update the
-# update.php script.
-#
+# The actual version number is in DefaultSettings.php
+# and can be seen through the wiki at [[Special:Version]]
 
+# This is for an ancient update item and probably is not
+# necessary.
 $wgSoftwareRevision = 1002;
 ?>
