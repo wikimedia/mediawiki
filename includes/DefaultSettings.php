7cc24419732dcc006f8411abe0f2d3a9111182cc
Brion Vibber <brion@users.mediawiki.org>
2004-05-21 19:04:52 +0000

    Update version and docs to 1.3.0beta1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6,7 +6,7 @@
 # like $wgScriptPath, you must also localize everything that
 # depends on it.
 
-$wgVersion			= "1.3.0pre-alpha";
+$wgVersion			= "1.3.0beta1";
 
 $wgSitename         = "MediaWiki"; # Please customize!
 $wgMetaNamespace    = FALSE; # will be same as you set $wgSitename
