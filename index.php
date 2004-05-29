14e3056e0e0a413a012a073796567fb442bdb310
Brion Vibber <brion@users.mediawiki.org>
2004-05-29 01:24:33 +0000

    Fix preg_quote usage in interwiki loop detection

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if ( $search = $wgRequest->getText( 'search' ) ) {
 } else if ( $wgTitle->getInterwiki() != "" ) {
 	$url = $wgTitle->getFullURL();
 	# Check for a redirect loop
-	if ( !preg_match( "/^" . preg_quote( $wgServer ) . "/", $url ) && $wgTitle->isLocal() ) {
+	if ( !preg_match( "/^" . preg_quote( $wgServer, "/" ) . "/", $url ) && $wgTitle->isLocal() ) {
 		$wgOut->redirect( $url );
 	} else {
 		$wgTitle = Title::newFromText( wfMsg( "badtitle" ) );
