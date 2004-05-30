e49bd083e1fdc5124027b386f21b84b91a76bcb5
Tim Starling <tstarling@users.mediawiki.org>
2004-05-30 02:34:44 +0000

    Removed PST <br> conversion, unnecessary, was annoying people

--- includes/Parser.php
+++ includes/Parser.php
@@ -1881,11 +1881,13 @@ class Parser
 			);
 		$text = str_replace(array_keys($pairs), array_values($pairs), $text);
 		// now with regexes
+		/*
 		$pairs = array(
 			"/<br.+(clear|break)=[\"']?(all|both)[\"']?\\/?>/i" => '<br style="clear:both;"/>',
 			"/<br *?>/i" => "<br/>",
 		);
 		$text = preg_replace(array_keys($pairs), array_values($pairs), $text);
+		*/
 		$text = $this->strip( $text, $stripState, false );
 		$text = $this->pstPass2( $text, $user );
 		$text = $this->unstrip( $text, $stripState );
