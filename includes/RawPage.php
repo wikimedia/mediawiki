807acca58a3b391802491cf078a10dd4b5bfc655
Gabriel Wicke <gwicke@users.mediawiki.org>
2004-06-03 00:54:18 +0000

    additional smaxage parameter

--- includes/RawPage.php
+++ includes/RawPage.php
@@ -21,12 +21,14 @@ class RawPage {
 			
 		$charset = $wgRequest->getText( 'charset' );
 		$this->mCharset = !empty($charset) ? $charset : $wgInputEncoding;
+		$smaxage = $wgRequest->getText( 'smaxage' );
+		$this->mSmaxage = !empty($smaxage) ? $smaxage : 0;
 		$this->mOldId = $wgRequest->getInt( 'oldid' );
 	}
 	function view() {
 		header( "Content-type: ".$this->mContentType.'; charset='.$this->mCharset );
 		# allow the client to cache this for 24 hours
-		header( 'Cache-Control: s-maxage=0, max-age=86400' );
+		header( 'Cache-Control: s-maxage='.$this->mSmaxage.', max-age=86400' );
 		echo $this->getrawtext();
 		wfAbruptExit();
 	}
