c92a39257281e3399489d454acee22241084d1b6
Antoine Musso <hashar@users.mediawiki.org>
2004-06-09 01:25:31 +0000

    double quotes to single quotes :o)

--- includes/Setup.php
+++ includes/Setup.php
@@ -11,7 +11,7 @@ if( !isset( $wgProfiling ) )
 	$wgProfiling = false;
 
 if ( $wgProfiling and (0 == rand() % $wgProfileSampleRate ) ) {        
-	require_once( "Profiling.php" );
+	require_once( 'Profiling.php' );
 } else {
 	function wfProfileIn( $fn = '' ) {}
 	function wfProfileOut( $fn = '' ) {}
@@ -20,52 +20,52 @@ if ( $wgProfiling and (0 == rand() % $wgProfileSampleRate ) ) {
 }
 
 /* collect the originating ips */
-if( $wgUseSquid && isset( $_SERVER["HTTP_X_FORWARDED_FOR"] ) ) {
+if( $wgUseSquid && isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
 	# If the web server is behind a reverse proxy, we need to find
 	# out where our requests are really coming from.
-	$hopips = array_map( "trim", explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) );
+	$hopips = array_map( 'trim', explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) );
 
 	while(in_array(trim(end($hopips)), $wgSquidServers)){
 		array_pop($hopips);
 	}
 	$wgIP = trim(end($hopips));
 } else {
-	$wgIP = getenv("REMOTE_ADDR");
+	$wgIP = getenv('REMOTE_ADDR');
 }
 
 
-$fname = "Setup.php";
+$fname = 'Setup.php';
 wfProfileIn( $fname );
 global $wgUseDynamicDates;
-wfProfileIn( "$fname-includes" );
+wfProfileIn( $fname.'-includes' );
 
-require_once( "GlobalFunctions.php" );
-require_once( "Namespace.php" );
-require_once( "RecentChange.php" ); 
-require_once( "Skin.php" );
-require_once( "OutputPage.php" );
-require_once( "User.php" );
-require_once( "LinkCache.php" );
-require_once( "Title.php" );
-require_once( "Article.php" );
-require_once( "MagicWord.php" );
-require_once( "memcached-client.php" );
-require_once( "Block.php" );
-require_once( "SearchEngine.php" );
-require_once( "DifferenceEngine.php" );
-require_once( "MessageCache.php" );
-require_once( "BlockCache.php" );
-require_once( "Parser.php" );
-require_once( "ParserCache.php" );
-require_once( "WebRequest.php" );
-require_once( "SpecialPage.php" );
+require_once( 'GlobalFunctions.php' );
+require_once( 'Namespace.php' );
+require_once( 'RecentChange.php' ); 
+require_once( 'Skin.php' );
+require_once( 'OutputPage.php' );
+require_once( 'User.php' );
+require_once( 'LinkCache.php' );
+require_once( 'Title.php' );
+require_once( 'Article.php' );
+require_once( 'MagicWord.php' );
+require_once( 'memcached-client.php' );
+require_once( 'Block.php' );
+require_once( 'SearchEngine.php' );
+require_once( 'DifferenceEngine.php' );
+require_once( 'MessageCache.php' );
+require_once( 'BlockCache.php' );
+require_once( 'Parser.php' );
+require_once( 'ParserCache.php' );
+require_once( 'WebRequest.php' );
+require_once( 'SpecialPage.php' );
 
 $wgRequest = new WebRequest();
 
 
 
-wfProfileOut( "$fname-includes" );
-wfProfileIn( "$fname-memcached" );
+wfProfileOut( $fname.'-includes' );
+wfProfileIn( $fname.'-memcached' );
 global $wgUser, $wgLang, $wgOut, $wgTitle;
 global $wgArticle, $wgDeferredUpdateList, $wgLinkCache;
 global $wgMemc, $wgMagicWords, $wgMwRedir, $wgDebugLogFile;
@@ -76,7 +76,7 @@ global $wgBlockCache, $wgParserCache, $wgParser, $wgDontTrustMemcachedWithImport
 # Useful debug output
 if ( $wgCommandLineMode ) {
 	# wfDebug( '"' . implode( '"  "', $argv ) . '"' );
-} elseif ( function_exists( "getallheaders" ) ) {
+} elseif ( function_exists( 'getallheaders' ) ) {
 	wfDebug( "\nStart request\n" );
 	wfDebug( $_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['REQUEST_URI'] . "\n" );
 	$headers = getallheaders();
@@ -126,7 +126,7 @@ if( $wgUseMemCached ) {
 
 	# Test it to see if it's working
 	# This is necessary because otherwise wfMsg would be extremely inefficient
-	if ( !$wgMemc->set( "test", "", 0 ) ) {
+	if ( !$wgMemc->set( 'test', '', 0 ) ) {
 		wfDebug( "Memcached failed setup test - connection error?\n" );
 		$wgUseMemCached = false;
 		$wgMemc = new FakeMemCachedClient();
@@ -137,60 +137,60 @@ if( $wgUseMemCached ) {
 	
 	# Give the message cache a separate cache in the DB.
 	# This is a speedup over separately querying every message used
-	require_once( "ObjectCache.php" );
-	$messageMemc = new MediaWikiBagOStuff("objectcache");
+	require_once( 'ObjectCache.php' );
+	$messageMemc = new MediaWikiBagOStuff('objectcache');
 }
 
-wfProfileOut( "$fname-memcached" );
-wfProfileIn( "$fname-language" );
-require_once( "languages/Language.php" );
+wfProfileOut( $fname.'-memcached' );
+wfProfileIn( $fname.'-language' );
+require_once( 'languages/Language.php' );
 
 $wgMessageCache = new MessageCache; 
 
-$wgLangClass = "Language" . ucfirst( $wgLanguageCode );
-if( ! class_exists( $wgLangClass ) || ($wgLanguageCode == "en" && strcasecmp( $wgInputEncoding, "utf-8" ) == 0 ) ) {
-	require_once( "languages/LanguageUtf8.php" );
-	$wgLangClass = "LanguageUtf8";
+$wgLangClass = 'Language' . ucfirst( $wgLanguageCode );
+if( ! class_exists( $wgLangClass ) || ($wgLanguageCode == 'en' && strcasecmp( $wgInputEncoding, 'utf-8' ) == 0 ) ) {
+	require_once( 'languages/LanguageUtf8.php' );
+	$wgLangClass = 'LanguageUtf8';
 }
 
 $wgLang = new $wgLangClass();
 if ( !is_object($wgLang) ) {
 	print "No language class ($wgLang)\N";
 }
-wfProfileOut( "$fname-language" );
-wfProfileIn( "$fname-MessageCache" );
+wfProfileOut( $fname.'-language' );
+wfProfileIn( $fname.'-MessageCache' );
 
 $wgMessageCache->initialise( $messageMemc, $wgUseDatabaseMessages, $wgMsgCacheExpiry, $wgDBname );
 
-wfProfileOut( "$fname-MessageCache" );
-wfProfileIn( "$fname-OutputPage" );
+wfProfileOut( $fname.'-MessageCache' );
+wfProfileIn( $fname.'-OutputPage' );
 
 $wgOut = new OutputPage();
 wfDebug( "\n\n" );
 
-wfProfileOut( "$fname-OutputPage" );
-wfProfileIn( "$fname-DateFormatter" );
+wfProfileOut( $fname.'-OutputPage' );
+wfProfileIn( $fname.'-DateFormatter' );
 
 if ( $wgUseDynamicDates ) {
-	require_once( "DateFormatter.php" );
+	require_once( 'DateFormatter.php' );
 	global $wgDateFormatter;
 	$wgDateFormatter = new DateFormatter;
 }
 
-wfProfileOut( "$fname-DateFormatter" );
-wfProfileIn( "$fname-SetupSession" );
+wfProfileOut( $fname.'-DateFormatter' );
+wfProfileIn( $fname.'-SetupSession' );
 
-if( !$wgCommandLineMode && ( isset( $_COOKIE[ini_get("session.name")] ) || isset( $_COOKIE["{$wgDBname}Password"] ) ) ) {
+if( !$wgCommandLineMode && ( isset( $_COOKIE[ini_get('session.name')] ) || isset( $_COOKIE[$wgDBname.'Password'] ) ) ) {
 	User::SetupSession();
 }
 
-wfProfileOut( "$fname-SetupSession" );
-wfProfileIn( "$fname-BlockCache" );
+wfProfileOut( $fname.'-SetupSession' );
+wfProfileIn( $fname.'-BlockCache' );
 
 $wgBlockCache = new BlockCache( true );
 
-wfProfileOut( "$fname-BlockCache" );
-wfProfileIn( "$fname-User" );
+wfProfileOut( $fname.'-BlockCache' );
+wfProfileIn( $fname.'-User' );
 
 if( $wgCommandLineMode ) {
 	# Used for some maintenance scripts; user session cookies can screw things up
@@ -200,8 +200,8 @@ if( $wgCommandLineMode ) {
 	$wgUser = User::loadFromSession();
 }
 
-wfProfileOut( "$fname-User" );
-wfProfileIn( "$fname-misc" );
+wfProfileOut( $fname.'-User' );
+wfProfileIn( $fname.'-misc' );
 
 $wgDeferredUpdateList = array();
 $wgLinkCache = new LinkCache();
@@ -212,15 +212,15 @@ $wgParser = new Parser();
 $wgOut->setParserOptions( ParserOptions::newFromUser( $wgUser ) );
 
 if ( !$wgAllowSysopQueries ) {
-	SpecialPage::removePage( "Asksql" );
+	SpecialPage::removePage( 'Asksql' );
 }
 
 # Placeholders in case of DB error
-$wgTitle = Title::newFromText( wfMsg( "badtitle" ) );
+$wgTitle = Title::newFromText( wfMsg( 'badtitle' ) );
 $wgArticle = new Article($wgTitle);
 
-wfProfileOut( "$fname-misc" );
-wfProfileIn( "$fname-extensions" );
+wfProfileOut( $fname.'-misc' );
+wfProfileIn( $fname.'-extensions' );
 
 # Extension setup functions
 # Entries should be added to this variable during the inclusion 
@@ -230,7 +230,7 @@ foreach ( $wgExtensionFunctions as $func ) {
 	$func();
 }
 
-wfProfileOut( "$fname-extensions" );
+wfProfileOut( $fname.'-extensions' );
 wfProfileOut( $fname );
 
 
