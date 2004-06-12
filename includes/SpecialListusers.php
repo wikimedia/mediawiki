69e12ea61f81b8bfb8276164efc9b0c7ac2b115c
Antoine Musso <hashar@users.mediawiki.org>
2004-06-12 20:14:55 +0000

    Migrated as a QueryPage extension * isExpensive : false

--- includes/SpecialListusers.php
+++ includes/SpecialListusers.php
@@ -1,39 +1,48 @@
 <?php
 
-function wfSpecialListusers()
-{
+require_once("QueryPage.php");
+
+class ListUsersPage extends QueryPage {
+
+	function getName() {
+		return "Listusers";
+	}
+
+	function isExpensive() {
+		return false;
+	}
+
+	function getSQL() {
+		global $wgIsPg;
+		$usertable = $wgIsPg?'"user"':'user';
+		$userspace = Namespace::getUser();
+		return "SELECT user_rights as type, $userspace as namespace, user_name as title, user_name as value FROM $usertable";
+	}
+	
+	function sortDescending() {
+		return false;
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgLang;
+		$name = $skin->makeKnownLink( $wgLang->getNsText($result->namespace) . ':' . $result->title, $result->title );
+		if( '' != $result->type ) {
+			$name .= ' (' .
+			$skin->makeKnownLink( wfMsg( "administrators" ), $result->type) .
+			')';
+		}
+		return $name;
+	}
+}
+
+function wfSpecialListusers() {
 	global $wgUser, $wgOut, $wgLang, $wgIsPg;
 
 	list( $limit, $offset ) = wfCheckLimits();
 
-	$top = wfShowingResults( $offset, $limit );
-	$wgOut->addHTML( "<p>{$top}\n" );
+	$slu = new ListUsersPage();
 
-	$sl = wfViewPrevNext( $offset, $limit,
-	  $wgLang->specialPage( "Listusers" ) );
-	$wgOut->addHTML( "<br />{$sl}</p>\n<ol start='" . ( $offset + 1 ) . "'>" );
-
-	$usertable=$wgIsPg?'"user"':'user';
-	$sql = "SELECT user_name,user_rights FROM $usertable ORDER BY user_name" .
-		wfLimitResult($limit,$offset);
-	$res = wfQuery( $sql, DB_READ, "wfSpecialListusers" );
-
-	$sk = $wgUser->getSkin();
-	while ( $s = wfFetchObject( $res ) ) {
-		$n = $s->user_name;
-		$r = $s->user_rights;
-
-		$l = $sk->makeLink( $wgLang->getNsText(
-		  Namespace::getUser() ) . ":{$n}", $n );
-
-		if ( "" != $r ) {
-			$link = $sk->makeKnownLink( wfMsg( "administrators" ), $r );
-			$l .= " ({$link})";
-		}
-		$wgOut->addHTML( "<li>{$l}</li>\n" );
-	}
-	wfFreeResult( $res );
-	$wgOut->addHTML( "</ol>\n<p>{$sl}</p>\n" );
+	return $slu->doQuery( $offset, $limit );
 }
 
 ?>
