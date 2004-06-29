3c4d38faee679dfc63fded82d7fb6ce9610ccc3f
Tim Starling <tstarling@users.mediawiki.org>
2004-06-29 12:23:59 +0000

    Removed arbitrary change in timestamp formats between MySQL and PostgreSQL, moved global functions to GlobalFunctions

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -62,7 +62,6 @@ class EditPage {
 	}
 
 	function importFormData( &$request ) {
-		global $wgIsMySQL, $wgIsPg;
 		# These fields need to be checked for encoding.
 		# Also remove trailing whitespace, but don't remove _initial_
 		# whitespace from the text boxes. This may be significant formatting.
@@ -71,12 +70,7 @@ class EditPage {
 		$this->summary = trim( $request->getText( "wpSummary" ) );
 
 		$this->edittime = $request->getVal( 'wpEdittime' );
-		if ($wgIsMySQL) 
-			if( !preg_match( '/^\d{14}$/', $this->edittime )) $this->edittime = "";
-		if ($wgIsPg)
-			if ( !preg_match( '/^\d{4}-\d\d-\d\d \d\d:\d\d:\d\d$/', 
-				$this->edittime ))
-				$this->edittime = "";
+		if( !preg_match( '/^\d{14}$/', $this->edittime )) $this->edittime = "";
 
 		$this->preview = $request->getCheck( 'wpPreview' );
 		$this->save = $request->wasPosted() && !$this->preview;
