19a829fc383d0fee0419fcbf60cccaade1d66c64
Gabriel Wicke <gwicke@users.mediawiki.org>
2004-07-16 17:27:18 +0000

    same as in head

--- includes/SkinPHPTal.php
+++ includes/SkinPHPTal.php
@@ -536,12 +536,12 @@
 			if( $this->loggedin ) {
 				if($wgTitle->isCssSubpage() and $action == 'submit' and  $wgTitle->userCanEditCssJsSubpage()) {
 					# generated css
-					$this->usercss = '@import "'.$this->makeUrl('-','action=raw&gen=css&smaxage=0&maxage=0').');'."\n";
+					$this->usercss = '@import "'.$this->makeUrl('-','action=raw&smaxage=0&maxage=0&gen=css').'";'."\n";
 					// css preview
 					$this->usercss .= $wgRequest->getText('wpTextbox1');
 				} else {
 					# generated css
-					$this->usercss .= '@import "'.$this->makeUrl('-','action=raw&gen=css&smaxage=0').'";'."\n";
+					$this->usercss .= '@import "'.$this->makeUrl('-','action=raw&smaxage=0&gen=css').'";'."\n";
 					# import user stylesheet
 					$this->usercss .= '@import "'.
 					$this->makeUrl($this->userpage.'/'.$this->skinname.'.css', 'action=raw&ctype=text/css').'";'."\n";
@@ -550,7 +550,7 @@
 					# XXX: additional security check/prompt?
 					$this->userjsprev = $wgRequest->getText('wpTextbox1');
 				} else {
-					$this->userjs = $this->makeUrl($this->userpage.'/'.$this->skinname.'.js', 'action=raw&ctype=text/javascript');
+					$this->userjs = $this->makeUrl($this->userpage.'/'.$this->skinname.'.js', 'action=raw&ctype=text/javascript&dontcountme=s');
 				}
 			}
 		}
