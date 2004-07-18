1a788d69db2b2804c71d1d911669d31347e3a747
Magnus Manske <magnusmanske@users.mediawiki.org>
2004-07-18 20:45:28 +0000

    Article validation code (a start)

--- includes/SpecialGeo.php
+++ includes/SpecialGeo.php
@@ -1,5 +1,5 @@
 <?php
-# Copyright (C) 2003 Brion Vibber <brion@pobox.com>
+# Copyright (C) 2004 Magnus Manske <magnus.manske@web.de>
 # http://www.mediawiki.org/
 # 
 # This program is free software; you can redistribute it and/or modify
