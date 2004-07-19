fd1c5c0f455faf415770e42b8191dcaa138568a4
Magnus Manske <magnusmanske@users.mediawiki.org>
2004-07-19 17:21:11 +0000

    Article validation, now with comments

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -260,5 +260,6 @@ CREATE TABLE `validate` (
   `val_timestamp` varchar(14) binary NOT NULL default '',
   `val_type` int(10) unsigned NOT NULL default '0',
   `val_value` int(11) default '0',
+  `val_comment` varchar(255) NOT NULL default '',
   KEY `val_user` (`val_user`,`val_title`,`val_timestamp`)
 ) TYPE=MyISAM;
