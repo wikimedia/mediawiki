9362cef664c77c0c3aa37ad57905d73b5ba4de1e
Brion Vibber <brion@users.mediawiki.org>
2004-08-02 01:38:54 +0000

    Switch literal \xA0 for unicode-friendly &#160;

--- includes/DifferenceEngine.php
+++ includes/DifferenceEngine.php
@@ -1013,7 +1013,7 @@ class DiffFormatter
  * 
  */
 
-define('NBSP', "\xA0");			// iso-8859-x non-breaking space.
+define('NBSP', '&#160;');			// iso-8859-x non-breaking space.
 
 class _HWLDF_WordAccumulator {
 	function _HWLDF_WordAccumulator () {
