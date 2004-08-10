120cd615f2ef0c00d3137977070c31b980ee4d05
Arne Heizmann <timwi@users.mediawiki.org>
2004-08-10 23:57:46 +0000

    - fixing my UTTER STUPIDITY in SpecialUncategorizedpages.php   (should actually return uncategorised articles now) - brion said to replace "cl_title as value" with "0 as value" - doing the same in SpecialLonelypages.php - also adding function sortDescending() { return false; }

--- includes/SpecialLonelypages.php
+++ includes/SpecialLonelypages.php
@@ -8,12 +8,16 @@ class LonelyPagesPage extends PageQueryPage {
 		return "Lonelypages";
 	}
 	
+	function sortDescending() {
+		return false;
+	}
+
 	function isExpensive() {
 		return true;
 	}
 	
 	function getSQL() {
-		return "SELECT 'Lonelypages' as type, cur_namespace AS namespace, cur_title AS title, cur_title AS value " .
+		return "SELECT 'Lonelypages' as type, cur_namespace AS namespace, cur_title AS title, 0 AS value " .
 			"FROM cur LEFT JOIN links ON cur_id=l_to ".
 			"WHERE l_to IS NULL AND cur_namespace=0 AND cur_is_redirect=0";
 	}
--- includes/SpecialUncategorizedpages.php
+++ includes/SpecialUncategorizedpages.php
@@ -17,9 +17,9 @@ class UncategorizedPagesPage extends PageQueryPage {
 	}
 
 	function getSQL() {
-		return "SELECT 'Uncategorizedpages' as type, cur_namespace AS namespace, cur_title AS title, cur_title AS value " .
-			"FROM cur LEFT JOIN categorylinks ON cur_id=cl_to ".
-			"WHERE cl_to IS NULL AND cur_namespace=0 AND cur_is_redirect=0";
+		return "SELECT 'Uncategorizedpages' as type, cur_namespace AS namespace, cur_title AS title, 0 AS value " .
+			"FROM cur LEFT JOIN categorylinks ON cur_id=cl_from ".
+			"WHERE cl_from IS NULL AND cur_namespace=0 AND cur_is_redirect=0";
 	}
 }
 
