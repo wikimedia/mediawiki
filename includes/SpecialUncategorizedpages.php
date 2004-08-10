3a147b2a51ca42cce578fd13c2f7d2e92572d1e1
Arne Heizmann <timwi@users.mediawiki.org>
2004-08-10 23:46:24 +0000

    Fix my broken backport of this file.

--- includes/SpecialUncategorizedpages.php
+++ includes/SpecialUncategorizedpages.php
@@ -15,13 +15,10 @@ class UncategorizedPagesPage extends PageQueryPage {
 	function isExpensive() {
 		return true;
 	}
-	
-	function getSQL() {
-		$dbr =& wfGetDB( DB_SLAVE );
-		extract( $dbr->tableNames( 'cur', 'categorylinks' ) );
 
+	function getSQL() {
 		return "SELECT 'Uncategorizedpages' as type, cur_namespace AS namespace, cur_title AS title, cur_title AS value " .
-			"FROM $cur LEFT JOIN $categorylinks ON cur_id=cl_to ".
+			"FROM cur LEFT JOIN categorylinks ON cur_id=cl_to ".
 			"WHERE cl_to IS NULL AND cur_namespace=0 AND cur_is_redirect=0";
 	}
 }
