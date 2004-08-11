5e7b22393f905f79ebfdcc8fb0481ec6b4c147e1
Brion Vibber <brion@users.mediawiki.org>
2004-08-11 09:02:20 +0000

    Fix some of the rebuild scripts

--- maintenance/rebuildall.php
+++ maintenance/rebuildall.php
@@ -10,21 +10,23 @@ require_once( "refreshlinks.inc" );
 require_once( "rebuildtextindex.inc" );
 require_once( "rebuildrecentchanges.inc" );
 
-$wgDBuser			= $wgDBadminuser;
-$wgDBpassword		= $wgDBadminpassword;
+$database = Database::newFromParams( $wgDBserver, $wgDBadminuser, $wgDBadminpassword, $wgDBname );
+
+print "** Rebuilding fulltext search index (if you abort this will break searching; run this script again to fix):\n";
+dropTextIndex( $database );
+rebuildTextIndex( $database );
+createTextIndex( $database );
+
+print "\n\n** Rebuilding recentchanges table:\n";
+rebuildRecentChangesTablePass1();
+rebuildRecentChangesTablePass2();
 
 # Doesn't work anymore
 # rebuildLinkTables();
 
 # Use the slow incomplete one instead. It's designed to work in the background
-#refreshLinks( 1 );
-
-dropTextIndex();
-rebuildTextIndex();
-createTextIndex();
-
-rebuildRecentChangesTablePass1();
-rebuildRecentChangesTablePass2();
+print "\n\n** Rebuilding links tables -- this can take a long time. It should be safe to about via ctrl+C if you get bored.\n";
+refreshLinks( 1 );
 
 print "Done.\n";
 exit();
