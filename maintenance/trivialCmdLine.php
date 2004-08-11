79b1087a3f5db8ea7f5c1a0a05b4b12739ce6bf8
Brion Vibber <brion@users.mediawiki.org>
2004-08-11 09:45:57 +0000

    tweak from head

--- maintenance/trivialCmdLine.php
+++ maintenance/trivialCmdLine.php
@@ -1,8 +1,11 @@
 <?php
+
 require_once( "commandLine.inc" );
-print "DB name: $wgDBname\n";
-print "DB user: $wgDBuser\n";
-print "DB password: $wgDBpassword\n";
+#print "DB name: $wgDBname\n";
+#print "DB user: $wgDBuser\n";
+#print "DB password: $wgDBpassword\n";
+
+print "This is an example command-line maintenance script.\n";
 
 $res = wfQuery( "SELECT MAX(cur_id) as m FROM cur", DB_READ );
 $row = wfFetchObject( $res );
