dd2562528a028a815caad4c381b7c71ca2e8e84a
Arne Heizmann <timwi@users.mediawiki.org>
2004-08-14 20:33:01 +0000

    # Please don't move this line to includes/Defines.php. This line essentially defines # a valid entry point. If you put it in includes/Defines.php, then any script that includes # it becomes an entry point, thereby defeating its purpose.

--- index.php
+++ index.php
@@ -10,9 +10,10 @@ if(!file_exists("LocalSettings.php")) {
 	die( "You'll have to <a href='config/index.php'>set the wiki up</a> first!" );
 }
 
-# Valid web server entry point, enable includes
-# The define must not be in ./includes/Defines.php because non valid entry
-# points can includes it.
+# Valid web server entry point, enable includes.
+# Please don't move this line to includes/Defines.php. This line essentially defines
+# a valid entry point. If you put it in includes/Defines.php, then any script that includes
+# it becomes an entry point, thereby defeating its purpose.
 define( "MEDIAWIKI", true );
 
 require_once( "./includes/Defines.php" );
