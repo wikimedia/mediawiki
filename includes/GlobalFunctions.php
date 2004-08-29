5080997cb4a893b2e0972d6b537a1d9b36795b01
Brion Vibber <brion@users.mediawiki.org>
2004-08-29 02:04:01 +0000

    Adjust UTF-8 generator for correctness. (Upper private use area, probably affects nothing.)

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -145,11 +145,12 @@ function wfUtf8Sequence( $codepoint ) {
 	if($codepoint <  0x10000) return chr($codepoint >> 12 & 0x0f | 0xe0) .
 									 chr($codepoint >>	6 & 0x3f | 0x80) .
 									 chr($codepoint		  & 0x3f | 0x80);
-	if($codepoint < 0x100000) return chr($codepoint >> 18 & 0x07 | 0xf0) . # Double-check this
+	if($codepoint < 0x110000) return chr($codepoint >> 18 & 0x07 | 0xf0) .
 									 chr($codepoint >> 12 & 0x3f | 0x80) .
 									 chr($codepoint >>	6 & 0x3f | 0x80) .
 									 chr($codepoint		  & 0x3f | 0x80);
-	# Doesn't yet handle outside the BMP
+
+	# There should be no assigned code points outside this range, but...
 	return "&#$codepoint;";
 }
 
