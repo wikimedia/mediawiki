e037d700261539c98fb3a29f26edbea32a4ce612
Arne Heizmann <timwi@users.mediawiki.org>
2004-08-30 23:23:10 +0000

    comment was incomplete

--- includes/DifferenceEngine.php
+++ includes/DifferenceEngine.php
@@ -1325,7 +1325,7 @@ class TableDiffFormatter extends DiffFormatter
 		$add = $diff->closing();
 
 		# Notice that WordLevelDiff returns HTML-escaped output.
-		# Hence, we will be calling addedLine without HTML-escaping.
+		# Hence, we will be calling addedLine/deletedLine without HTML-escaping.
 
 		while ( $line = array_shift( $del ) ) {
 			$aline = array_shift( $add );
