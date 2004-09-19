e4772694655639ee07d0ce123295593d67561c89
Antoine Musso <hashar@users.mediawiki.org>
2004-09-19 19:04:52 +0000

    single quotes

--- includes/SpecialBlockip.php
+++ includes/SpecialBlockip.php
@@ -19,9 +19,9 @@ function wfSpecialBlockip() {
 	$ipb = new IPBlockForm();
 
 	$action = $wgRequest->getVal( 'action' );
-	if ( "success" == $action ) { $ipb->showSuccess(); }
-	else if ( $wgRequest->wasPosted() && "submit" == $action ) { $ipb->doSubmit(); }
-	else { $ipb->showForm( "" ); }
+	if ( 'success' == $action ) { $ipb->showSuccess(); }
+	else if ( $wgRequest->wasPosted() && 'submit' == $action ) { $ipb->doSubmit(); }
+	else { $ipb->showForm( '' ); }
 }
 
 /**
@@ -40,27 +40,26 @@ class IPBlockForm {
 		$this->BlockExpiry = $wgRequest->getVal( 'wpBlockExpiry' );
 	}
 	
-	function showForm( $err )
-	{
+	function showForm( $err ) {
 		global $wgOut, $wgUser, $wgLang, $wgDefaultBlockExpiry;
 		global $wgRequest;
 
-		$wgOut->setPagetitle( htmlspecialchars( wfMsg( "blockip" ) ) );
-		$wgOut->addWikiText( htmlspecialchars( wfMsg( "blockiptext" ) ) );
+		$wgOut->setPagetitle( htmlspecialchars( wfMsg( 'blockip' ) ) );
+		$wgOut->addWikiText( htmlspecialchars( wfMsg( 'blockiptext' ) ) );
 
-		if ( is_null( $this->BlockExpiry ) || $this->BlockExpiry === "" ) {
+		if ( is_null( $this->BlockExpiry ) || $this->BlockExpiry === '' ) {
 			$this->BlockExpiry = $wgDefaultBlockExpiry;
 		}
 
-		$mIpaddress = htmlspecialchars( wfMsg( "ipaddress" ) );
-		$mIpbexpiry = htmlspecialchars( wfMsg( "ipbexpiry" ) );
-		$mIpbreason = htmlspecialchars( wfMsg( "ipbreason" ) );
-		$mIpbsubmit = htmlspecialchars( wfMsg( "ipbsubmit" ) );
-		$titleObj = Title::makeTitle( NS_SPECIAL, "Blockip" );
+		$mIpaddress = htmlspecialchars( wfMsg( 'ipaddress' ) );
+		$mIpbexpiry = htmlspecialchars( wfMsg( 'ipbexpiry' ) );
+		$mIpbreason = htmlspecialchars( wfMsg( 'ipbreason' ) );
+		$mIpbsubmit = htmlspecialchars( wfMsg( 'ipbsubmit' ) );
+		$titleObj = Title::makeTitle( NS_SPECIAL, 'Blockip' );
 		$action = $titleObj->escapeLocalURL( "action=submit" );
 
 		if ( "" != $err ) {
-			$wgOut->setSubtitle( htmlspecialchars( wfMsg( "formerror" ) ) );
+			$wgOut->setSubtitle( htmlspecialchars( wfMsg( 'formerror' ) ) );
 			$wgOut->addHTML( "<p class='error'>{$err}</p>\n" );
 		}
 
@@ -113,13 +112,13 @@ class IPBlockForm {
 		  	if ( preg_match( "/^($rxIP)\\/(\\d{1,2})$/", $this->BlockAddress, $matches ) ) {
 				if ( $wgSysopRangeBans ) {
 					if ( $matches[2] > 31 || $matches[2] < 16 ) {
-						$this->showForm( wfMsg( "ip_range_invalid" ) );
+						$this->showForm( wfMsg( 'ip_range_invalid' ) );
 						return;
 					}
 					$this->BlockAddress = Block::normaliseRange( $this->BlockAddress );
 				} else {
 					# Range block illegal
-					$this->showForm( wfMsg( "range_block_disabled" ) );
+					$this->showForm( wfMsg( 'range_block_disabled' ) );
 					return;
 				}
 			} else {
@@ -127,34 +126,33 @@ class IPBlockForm {
 				if ( $wgSysopUserBans ) {	
 					$userId = User::idFromName( $this->BlockAddress );
 					if ( $userId == 0 ) {
-						$this->showForm( wfMsg( "nosuchuser", htmlspecialchars( $this->BlockAddress ) ) );
+						$this->showForm( wfMsg( 'nosuchuser', htmlspecialchars( $this->BlockAddress ) ) );
 						return;
 					}
 				} else {
-					$this->showForm( wfMsg( "badipaddress" ) );
+					$this->showForm( wfMsg( 'badipaddress' ) );
 					return;
 				}
 			}
 		}
 
-		if ( $this->BlockExpiry == "infinite" || $this->BlockExpiry == "indefinite" ) {
+		if ( $this->BlockExpiry == 'infinite' || $this->BlockExpiry == 'indefinite' ) {
 			$expiry = '';
 		} else {
 			# Convert GNU-style date, returns -1 on error
 			$expiry = strtotime( $this->BlockExpiry );
 
 			if ( $expiry < 0 ) {
-				$this->showForm( wfMsg( "ipb_expiry_invalid" ) );
+				$this->showForm( wfMsg( 'ipb_expiry_invalid' ) );
 				return;
 			}
 			
 			$expiry = wfUnix2Timestamp( $expiry );
 
 		}
-
 		
-		if ( "" == $this->BlockReason ) {
-			$this->showForm( wfMsg( "noblockreason" ) );
+		if ( $this->BlockReason == '') {
+			$this->showForm( wfMsg( 'noblockreason' ) );
 			return;
 		}
 		
@@ -170,16 +168,16 @@ class IPBlockForm {
 		$log->addEntry( 'block', Title::makeTitle( NS_USER, $this->BlockAddress ), $this->BlockReason );
 
 		# Report to the user
-		$titleObj = Title::makeTitle( NS_SPECIAL, "Blockip" );
-		$wgOut->redirect( $titleObj->getFullURL( "action=success&ip={$this->BlockAddress}" ) );
+		$titleObj = Title::makeTitle( NS_SPECIAL, 'Blockip' );
+		$wgOut->redirect( $titleObj->getFullURL( 'action=success&ip='.$this->BlockAddress ) );
 	}
 
 	function showSuccess() {
 		global $wgOut, $wgUser;
 
-		$wgOut->setPagetitle( wfMsg( "blockip" ) );
-		$wgOut->setSubtitle( wfMsg( "blockipsuccesssub" ) );
-		$text = wfMsg( "blockipsuccesstext", $this->BlockAddress );
+		$wgOut->setPagetitle( wfMsg( 'blockip' ) );
+		$wgOut->setSubtitle( wfMsg( 'blockipsuccesssub' ) );
+		$text = wfMsg( 'blockipsuccesstext', $this->BlockAddress );
 		$wgOut->addWikiText( $text );
 	}
 }
