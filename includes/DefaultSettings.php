c7d650cd7d1a96d9619701a98fed46c299cddb01
Brion Vibber <brion@users.mediawiki.org>
2004-09-29 00:03:15 +0000

    Bump version number to 1.3.4

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -9,7 +9,7 @@ if( defined( "MEDIAWIKI" ) ) {
 # like $wgScriptPath, you must also localize everything that
 # depends on it.
 
-$wgVersion			= '1.3.3+';
+$wgVersion			= '1.3.4';
 
 $wgSitename         = 'MediaWiki'; # Please customize!
 $wgMetaNamespace    = FALSE; # will be same as you set $wgSitename
