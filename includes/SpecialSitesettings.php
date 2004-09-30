646822a06c221a97a99b7fd2bc7f24c9efb09ae7
Antoine Musso <hashar@users.mediawiki.org>
2004-09-30 22:48:38 +0000

    moving html forms builders to its own class. Do we want them in wgOut instead ?

--- includes/SpecialSitesettings.php
+++ includes/SpecialSitesettings.php
@@ -1,14 +1,14 @@
 <?php
+require_once('HTMLForm.php');
 
-function wfSpecialSiteSettings()
-{
+function wfSpecialSiteSettings() {
 	global $wgRequest;
 
 	$form = new SiteSettingsForm( $wgRequest );
 	$form->execute();
 }
 
-class SiteSettingsForm {
+class SiteSettingsForm extends HTMLForm {
 	var $mPosted, $mRequest, $mReset, $mSaveprefs;
 	
 	function SiteSettingsForm ( &$request ) {
@@ -34,44 +34,6 @@ class SiteSettingsForm {
 
 	/* private */ function resetPrefs() {
 		return;
-	}
-
-	/* private */ function fieldset( $name, $content ) {
-		return "<fieldset><legend>".wfMsg($name)."</legend>\n" .
-			$content . "\n</fieldset>\n";
-	}
-
-	/* private */ function checkbox( $varname, $checked=false ) {
-		$checked = isset( $GLOBALS[$varname] ) && $GLOBALS[$varname] ;
-		return "<div><input type='checkbox' value=\"1\" id=\"{$varname}\" name=\"wpOp{$varname}\"" .
-			( $checked ? ' checked="checked"' : '' ) .
-			" /><label for=\"{$varname}\">". wfMsg( "sitesettings-".$varname ) .
-			"</label></div>\n";
-	}
-
-	/* private */ function textbox( $varname, $value='', $size=20 ) {
-		$value = isset( $GLOBALS[$varname] ) ? $GLOBALS[$varname] : '';
-		return "<div><label>". wfMsg( "sitesettings-".$varname ) .
-			"<input type='text' name=\"wpOp{$varname}\" value=\"{$value}\" size=\"{$size}\" /></label></div>\n";
-	}
-	/* private */ function radiobox( $varname, $fields ) {
-		foreach ( $fields as $value => $checked ) {
-			$s .= "<div><label><input type='radio' name=\"wpOp{$varname}\" value=\"{$value}\"" .
-				( $checked ? ' checked="checked"' : '' ) . " />" . wfMsg( 'sitesettings-'.$varname.'-'.$value ) .
-				"</label></div>\n";
-		}
-		return $this->fieldset( 'sitesettings-'.$varname, $s );
-	}
-
-	/* private */ function arraybox( $varname , $size=20 ) {
-		$s = '';
-		if ( isset( $GLOBALS[$varname] ) && is_array( $GLOBALS[$varname] ) ) {
-			foreach ( $GLOBALS[$varname] as $index=>$element ) {
-				$s .= $element."\n";
-			}
-		}
-		return "<div><label>".wfMsg( 'sitesettings-'.$varname ).
-			"<textarea name=\"wpOp{$varname}\" rows=\"5\" cols=\"{$size}\">{$s}</textarea>\n";
 	}
 
 	/* private */ function mainPrefsForm( $err ) {
