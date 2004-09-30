ccac8f09edadd94c2da5296e9bb8cac3e4885ba2
Antoine Musso <hashar@users.mediawiki.org>
2004-09-30 23:44:54 +0000

    removing occurences of sitesettings. Replaced by an internal name.

--- includes/HTMLForm.php
+++ includes/HTMLForm.php
@@ -4,8 +4,11 @@
  * Class to build various forms
  */
 class HTMLForm {
+	/** name of our form. Used as prefix for labels */
+	var $mName;
+
 	/* private */ function fieldset( $name, $content ) {
-		return "<fieldset><legend>".wfMsg($name)."</legend>\n" .
+		return "<fieldset><legend>".wfMsg($this->mName.'-'.$name)."</legend>\n" .
 			$content . "\n</fieldset>\n";
 	}
 
@@ -13,22 +16,22 @@ class HTMLForm {
 		$checked = isset( $GLOBALS[$varname] ) && $GLOBALS[$varname] ;
 		return "<div><input type='checkbox' value=\"1\" id=\"{$varname}\" name=\"wpOp{$varname}\"" .
 			( $checked ? ' checked="checked"' : '' ) .
-			" /><label for=\"{$varname}\">". wfMsg( "sitesettings-".$varname ) .
+			" /><label for=\"{$varname}\">". wfMsg( $this->mName.'-'.$varname ) .
 			"</label></div>\n";
 	}
 
 	/* private */ function textbox( $varname, $value='', $size=20 ) {
 		$value = isset( $GLOBALS[$varname] ) ? $GLOBALS[$varname] : '';
-		return "<div><label>". wfMsg( "sitesettings-".$varname ) .
+		return "<div><label>". wfMsg( $this->mName.'-'.$varname ) .
 			"<input type='text' name=\"wpOp{$varname}\" value=\"{$value}\" size=\"{$size}\" /></label></div>\n";
 	}
 	/* private */ function radiobox( $varname, $fields ) {
 		foreach ( $fields as $value => $checked ) {
 			$s .= "<div><label><input type='radio' name=\"wpOp{$varname}\" value=\"{$value}\"" .
-				( $checked ? ' checked="checked"' : '' ) . " />" . wfMsg( 'sitesettings-'.$varname.'-'.$value ) .
+				( $checked ? ' checked="checked"' : '' ) . " />" . wfMsg( $this->mName.'-'.$varname.'-'.$value ) .
 				"</label></div>\n";
 		}
-		return $this->fieldset( 'sitesettings-'.$varname, $s );
+		return $this->fieldset( $this->mName'-'.$varname, $s );
 	}
 
 	/* private */ function arraybox( $varname , $size=20 ) {
@@ -38,7 +41,7 @@ class HTMLForm {
 				$s .= $element."\n";
 			}
 		}
-		return "<div><label>".wfMsg( 'sitesettings-'.$varname ).
+		return "<div><label>".wfMsg( $this->mName.'-'.$varname ).
 			"<textarea name=\"wpOp{$varname}\" rows=\"5\" cols=\"{$size}\">{$s}</textarea>\n";
 	}
 }
