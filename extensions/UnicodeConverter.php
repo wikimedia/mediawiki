8a328f0e4862e082c32026443cf1b0d1856946a7
Brion Vibber <brion@users.mediawiki.org>
2004-10-02 20:39:12 +0000

    * Fixed potential HTML/JavaScript injection attack in the UnicodeConverter   extension. (This extension is not enabled by default.)

--- extensions/UnicodeConverter.php
+++ extensions/UnicodeConverter.php
@@ -7,6 +7,9 @@
 $wgExtensionFunctions[] = "wfUnicodeConverter";
 
 function wfUnicodeConverter() {
+
+require_once( "includes/SpecialPage.php" );
+
 class UnicodeConverter extends SpecialPage
 {
 	function UnicodeConverter() {
@@ -20,20 +23,20 @@ class UnicodeConverter extends SpecialPage
 
 		$q = $wgRequest->getText( 'q' );
 		$encQ = htmlspecialchars( $q );
-		$action = $wgTitle->getLocalUrl();
-		$ok = wfMsg( "ok" );
+		$action = $wgTitle->escapeLocalUrl();
+		$ok = htmlspecialchars( wfMsg( "ok" ) );
 
-		$wgOut->addHTML( "
-<form name=ucf method=post action=\"$action\">
-<textarea rows=15 cols=80 name=q>
-$encQ
-</textarea><br />
-<input type=submit name=submit value=\"$ok\"><br /><br />
-</form>" );
+		$wgOut->addHTML( <<<END
+<form name="ucf" method="post" action="$action">
+<textarea rows="15" cols="80" name="q">$encQ</textarea><br />
+<input type="submit" name="submit" value="$ok" /><br /><br />
+</form>
+END
+);
 
 		if ( !is_null( $q ) ) {
-			$html = wfUtf8ToHTML( $q );
-			$wgOut->addHTML( "\n\n\n" . nl2br( $html ) . "\n<hr>\n" .
+			$html = wfUtf8ToHTML( htmlspecialchars( $q ) );
+			$wgOut->addHTML( "\n\n\n" . nl2br( $html ) . "\n<hr />\n" .
 			  nl2br( htmlspecialchars( $html ) ) . "\n\n" );
 		}
 	}
