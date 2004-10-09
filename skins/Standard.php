5742f5f299560086572a14a59e8cff865e45d79f
Brion Vibber <brion@users.mediawiki.org>
2004-10-09 08:34:52 +0000

    Re-tabification

--- skins/Standard.php
+++ skins/Standard.php
@@ -80,203 +80,202 @@ class SkinStandard extends Skin {
 		return $a;
 	}
 
-        function doAfterContent() {
-                global $wgUser, $wgOut, $wgContLang;
-                $fname =  'SkinStandard::doAfterContent';
-                wfProfileIn( $fname );
-                wfProfileIn( $fname.'-1' );
+	function doAfterContent() {
+		global $wgUser, $wgOut, $wgContLang;
+		$fname =  'SkinStandard::doAfterContent';
+		wfProfileIn( $fname );
+		wfProfileIn( $fname.'-1' );
 
-                $s = "\n</div><br style=\"clear:both\" />\n";
-                $s .= "\n<div id='footer'>";
-                $s .= '<table border="0" cellspacing="0"><tr>';
+		$s = "\n</div><br style=\"clear:both\" />\n";
+		$s .= "\n<div id='footer'>";
+		$s .= '<table border="0" cellspacing="0"><tr>';
 
-                wfProfileOut( $fname.'-1' );
-                wfProfileIn( $fname.'-2' );
+		wfProfileOut( $fname.'-1' );
+		wfProfileIn( $fname.'-2' );
 
-                $qb = $this->qbSetting();
-                $shove = ($qb != 0);
-                $left = ($qb == 1 || $qb == 3);
-                if($wgContLang->isRTL()) $left = !$left;
+		$qb = $this->qbSetting();
+		$shove = ($qb != 0);
+		$left = ($qb == 1 || $qb == 3);
+		if($wgContLang->isRTL()) $left = !$left;
 
-                if ( $shove && $left ) { # Left
-                        $s .= $this->getQuickbarCompensator();
-                }
-                wfProfileOut( $fname.'-2' );
-                wfProfileIn( $fname.'-3' );
-                $l = $wgContLang->isRTL() ? 'right' : 'left';
-                $s .= "<td class='bottom' align='$l' valign='top'>";
+		if ( $shove && $left ) { # Left
+				$s .= $this->getQuickbarCompensator();
+		}
+		wfProfileOut( $fname.'-2' );
+		wfProfileIn( $fname.'-3' );
+		$l = $wgContLang->isRTL() ? 'right' : 'left';
+		$s .= "<td class='bottom' align='$l' valign='top'>";
 
-                $s .= $this->bottomLinks();
-                $s .= "\n<br />" . $this->mainPageLink()
-                  . ' | ' . $this->aboutLink()
-                  . ' | ' . $this->specialLink( 'recentchanges' )
-                  . ' | ' . $this->searchForm()
-                  . '<br /><span id="pagestats">' . $this->pageStats() . '</span>';
+		$s .= $this->bottomLinks();
+		$s .= "\n<br />" . $this->mainPageLink()
+		  . ' | ' . $this->aboutLink()
+		  . ' | ' . $this->specialLink( 'recentchanges' )
+		  . ' | ' . $this->searchForm()
+		  . '<br /><span id="pagestats">' . $this->pageStats() . '</span>';
 
-                $s .= "</td>";
-                if ( $shove && !$left ) { # Right
-                        $s .= $this->getQuickbarCompensator();
-                }
-                $s .= "</tr></table>\n</div>\n</div>\n";
+		$s .= "</td>";
+		if ( $shove && !$left ) { # Right
+			$s .= $this->getQuickbarCompensator();
+		}
+		$s .= "</tr></table>\n</div>\n</div>\n";
 
-                wfProfileOut( $fname.'-3' );
-                wfProfileIn( $fname.'-4' );
-                if ( 0 != $qb ) { $s .= $this->quickBar(); }
-                wfProfileOut( $fname.'-4' );
-                wfProfileOut( $fname );
-                return $s;
-        }
+		wfProfileOut( $fname.'-3' );
+		wfProfileIn( $fname.'-4' );
+		if ( 0 != $qb ) { $s .= $this->quickBar(); }
+		wfProfileOut( $fname.'-4' );
+		wfProfileOut( $fname );
+		return $s;
+	}
 
-        function quickBar() {
-                global $wgOut, $wgTitle, $wgUser, $wgRequest, $wgContLang;
-                global $wgDisableUploads, $wgRemoteUploads, $wgNavigationLinks;
+	function quickBar() {
+		global $wgOut, $wgTitle, $wgUser, $wgRequest, $wgContLang;
+		global $wgDisableUploads, $wgRemoteUploads, $wgNavigationLinks;
 
-                $fname =  'Skin::quickBar';
-                wfProfileIn( $fname );
+		$fname =  'Skin::quickBar';
+		wfProfileIn( $fname );
 
-                $action = $wgRequest->getText( 'action' );
-                $wpPreview = $wgRequest->getBool( 'wpPreview' );
-                $tns=$wgTitle->getNamespace();
+		$action = $wgRequest->getText( 'action' );
+		$wpPreview = $wgRequest->getBool( 'wpPreview' );
+		$tns=$wgTitle->getNamespace();
 
-                $s = "\n<div id='quickbar'>";
-                $s .= "\n" . $this->logoText() . "\n<hr class='sep' />";
+		$s = "\n<div id='quickbar'>";
+		$s .= "\n" . $this->logoText() . "\n<hr class='sep' />";
 
-                $sep = "\n<br />";
+		$sep = "\n<br />";
 
-                foreach ( $wgNavigationLinks as $link ) {
-                        $msg = wfMsgForContent( $link['href'] );
-                        if ( $msg != '-' ) {
-                                $s .= '<a href="' . $this->makeInternalOrExternalUrl( $msg ) . '">' .
-                                        wfMsg( $link['text'] ) . '</a>' . $sep;
-                        }
-                }
-
-
-                if ($wgUser->getID()) {
-                	$s.= $this->specialLink( 'watchlist' ) ;
-                	$s .= $sep . $this->makeKnownLink( $wgContLang->specialPage( 'Contributions' ),
-                  		wfMsg( 'mycontris' ), 'target=' . wfUrlencode($wgUser->getName() ) );
-
-                }
-                // only show watchlist link if logged in
-                $s .= "\n<hr class='sep' />";
-                $articleExists = $wgTitle->getArticleId();
-                if ( $wgOut->isArticle() || $action =='edit' || $action =='history' || $wpPreview) {
-                        if($wgOut->isArticle()) {
-                                $s .= '<strong>' . $this->editThisPage() . '</strong>';
-                        } else { # backlink to the article in edit or history mode
-                                if($articleExists){ # no backlink if no article
-                                        switch($tns) {
-                                                case 0:
-                                                $text = wfMsg('articlepage');
-                                                break;
-                                                case 1:
-                                                $text = wfMsg('viewtalkpage');
-                                                break;
-                                                case 2:
-                                                $text = wfMsg('userpage');
-                                                break;
-                                                case 3:
-                                                $text = wfMsg('viewtalkpage');
-                                                break;
-                                                case 4:
-                                                $text = wfMsg('wikipediapage');
-                                                break;
-                                                case 5:
-                                                $text = wfMsg('viewtalkpage');
-                                                break;
-                                                case 6:
-                                                $text = wfMsg('imagepage');
-                                                break;
-                                                case 7:
-                                                $text = wfMsg('viewtalkpage');
-                                                break;
-                                                default:
-                                                $text= wfMsg('articlepage');
-                                        }
-
-                                        $link = $wgTitle->getText();
-                                        if ($nstext = $wgContLang->getNsText($tns) ) { # add namespace if necessary
-                                                $link = $nstext . ':' . $link ;
-                                        }
-
-                                        $s .= $this->makeLink( $link, $text );
-                                } elseif( $wgTitle->getNamespace() != Namespace::getSpecial() ) {
-                                        # we just throw in a "New page" text to tell the user that he's in edit mode,
-                                        # and to avoid messing with the separator that is prepended to the next item
-                                        $s .= '<strong>' . wfMsg('newpage') . '</strong>';
-                                }
-
-                        }
+		foreach ( $wgNavigationLinks as $link ) {
+			$msg = wfMsgForContent( $link['href'] );
+			if ( $msg != '-' ) {
+				$s .= '<a href="' . $this->makeInternalOrExternalUrl( $msg ) . '">' .
+					wfMsg( $link['text'] ) . '</a>' . $sep;
+			}
+		}
 
 
-                        if( $tns%2 && $action!='edit' && !$wpPreview) {
-                                $s.= '<br />'.$this->makeKnownLink($wgTitle->getPrefixedText(),wfMsg('postcomment'),'action=edit&section=new');
-                        }
+		if ($wgUser->getID()) {
+			$s.= $this->specialLink( 'watchlist' ) ;
+			$s .= $sep . $this->makeKnownLink( $wgContLang->specialPage( 'Contributions' ),
+				wfMsg( 'mycontris' ), 'target=' . wfUrlencode($wgUser->getName() ) );
+		}
+		// only show watchlist link if logged in
+		$s .= "\n<hr class='sep' />";
+		$articleExists = $wgTitle->getArticleId();
+		if ( $wgOut->isArticle() || $action =='edit' || $action =='history' || $wpPreview) {
+			if($wgOut->isArticle()) {
+				$s .= '<strong>' . $this->editThisPage() . '</strong>';
+			} else { # backlink to the article in edit or history mode
+				if($articleExists){ # no backlink if no article
+					switch($tns) {
+						case 0:
+						$text = wfMsg('articlepage');
+						break;
+						case 1:
+						$text = wfMsg('viewtalkpage');
+						break;
+						case 2:
+						$text = wfMsg('userpage');
+						break;
+						case 3:
+						$text = wfMsg('viewtalkpage');
+						break;
+						case 4:
+						$text = wfMsg('wikipediapage');
+						break;
+						case 5:
+						$text = wfMsg('viewtalkpage');
+						break;
+						case 6:
+						$text = wfMsg('imagepage');
+						break;
+						case 7:
+						$text = wfMsg('viewtalkpage');
+						break;
+						default:
+						$text= wfMsg('articlepage');
+					}
 
-                        /*
-                        watching could cause problems in edit mode:
-                        if user edits article, then loads "watch this article" in background and then saves
-                        article with "Watch this article" checkbox disabled, the article is transparently
-                        unwatched. Therefore we do not show the "Watch this page" link in edit mode
-                        */
-                        if ( 0 != $wgUser->getID() && $articleExists) {
-                                if($action!='edit' && $action != 'submit' )
-                                {
-                                        $s .= $sep . $this->watchThisPage();
-                                }
-                                if ( $wgTitle->userCanEdit() )
-                                        $s .= $sep . $this->moveThisPage();
-                        }
-                        if ( $wgUser->isSysop() and $articleExists ) {
-                                $s .= $sep . $this->deleteThisPage() .
-                                $sep . $this->protectThisPage();
-                        }
-                        $s .= $sep . $this->talkLink();
-                        if ($articleExists && $action !='history') {
-                                $s .= $sep . $this->historyLink();
-                        }
-                        $s.=$sep . $this->whatLinksHere();
+					$link = $wgTitle->getText();
+					if ($nstext = $wgContLang->getNsText($tns) ) { # add namespace if necessary
+						$link = $nstext . ':' . $link ;
+					}
 
-                        if($wgOut->isArticleRelated()) {
-                                $s .= $sep . $this->watchPageLinksLink();
-                        }
+					$s .= $this->makeLink( $link, $text );
+				} elseif( $wgTitle->getNamespace() != Namespace::getSpecial() ) {
+					# we just throw in a "New page" text to tell the user that he's in edit mode,
+					# and to avoid messing with the separator that is prepended to the next item
+					$s .= '<strong>' . wfMsg('newpage') . '</strong>';
+				}
 
-                        if ( Namespace::getUser() == $wgTitle->getNamespace()
-                        || $wgTitle->getNamespace() == Namespace::getTalk(Namespace::getUser())
-                        ) {
+			}
 
-                                $id=User::idFromName($wgTitle->getText());
-                                $ip=User::isIP($wgTitle->getText());
 
-                                if($id||$ip) {
-                                        $s .= $sep . $this->userContribsLink();
-                                }
-                                if ( 0 != $wgUser->getID() ) {
-                                        if($id) { # can only email real users
-                                                $s .= $sep . $this->emailUserLink();
-                                        }
-                                }
-                        }
-                        $s .= "\n<br /><hr class='sep' />";
-                }
+			if( $tns%2 && $action!='edit' && !$wpPreview) {
+				$s.= '<br />'.$this->makeKnownLink($wgTitle->getPrefixedText(),wfMsg('postcomment'),'action=edit&section=new');
+			}
 
-                if ( 0 != $wgUser->getID() && ( !$wgDisableUploads || $wgRemoteUploads ) ) {
-                        $s .= $this->specialLink( 'upload' ) . $sep;
-                }
-                $s .= $this->specialLink( 'specialpages' )
-                  . $sep . $this->bugReportsLink();
+			/*
+			watching could cause problems in edit mode:
+			if user edits article, then loads "watch this article" in background and then saves
+			article with "Watch this article" checkbox disabled, the article is transparently
+			unwatched. Therefore we do not show the "Watch this page" link in edit mode
+			*/
+			if ( 0 != $wgUser->getID() && $articleExists) {
+				if($action!='edit' && $action != 'submit' )
+				{
+					$s .= $sep . $this->watchThisPage();
+				}
+				if ( $wgTitle->userCanEdit() )
+					$s .= $sep . $this->moveThisPage();
+			}
+			if ( $wgUser->isSysop() and $articleExists ) {
+				$s .= $sep . $this->deleteThisPage() .
+				$sep . $this->protectThisPage();
+			}
+			$s .= $sep . $this->talkLink();
+			if ($articleExists && $action !='history') {
+				$s .= $sep . $this->historyLink();
+			}
+			$s.=$sep . $this->whatLinksHere();
 
-                global $wgSiteSupportPage;
-                if( $wgSiteSupportPage ) {
-                        $s .= "\n<br /><a href=\"" . htmlspecialchars( $wgSiteSupportPage ) .
-                          '" class="internal">' . wfMsg( 'sitesupport' ) . '</a>';
-                }
+			if($wgOut->isArticleRelated()) {
+				$s .= $sep . $this->watchPageLinksLink();
+			}
 
-                $s .= "\n<br /></div>\n";
-                wfProfileOut( $fname );
-                return $s;
-        }
+			if ( Namespace::getUser() == $wgTitle->getNamespace()
+			|| $wgTitle->getNamespace() == Namespace::getTalk(Namespace::getUser())
+			) {
+
+				$id=User::idFromName($wgTitle->getText());
+				$ip=User::isIP($wgTitle->getText());
+
+				if($id||$ip) {
+					$s .= $sep . $this->userContribsLink();
+				}
+				if ( 0 != $wgUser->getID() ) {
+					if($id) { # can only email real users
+						$s .= $sep . $this->emailUserLink();
+					}
+				}
+			}
+			$s .= "\n<br /><hr class='sep' />";
+		}
+
+		if ( 0 != $wgUser->getID() && ( !$wgDisableUploads || $wgRemoteUploads ) ) {
+			$s .= $this->specialLink( 'upload' ) . $sep;
+		}
+		$s .= $this->specialLink( 'specialpages' )
+		  . $sep . $this->bugReportsLink();
+
+		global $wgSiteSupportPage;
+		if( $wgSiteSupportPage ) {
+			$s .= "\n<br /><a href=\"" . htmlspecialchars( $wgSiteSupportPage ) .
+			  '" class="internal">' . wfMsg( 'sitesupport' ) . '</a>';
+		}
+
+		$s .= "\n<br /></div>\n";
+		wfProfileOut( $fname );
+		return $s;
+	}
 
 
 }
