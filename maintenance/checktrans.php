b9a1a4f4adfc575d0411a0fed40f1ac71989de4c
Evan Prodromou <evanprodromou@users.mediawiki.org>
2004-10-10 23:28:45 +0000

    Updated the checktrans.php script to be a little more modern and to WFM. Removed the two variations on the theme, since they don't work. Might as well have just one broken script instead of three.

--- maintenance/checktrans.php
+++ maintenance/checktrans.php
@@ -1,51 +1,29 @@
 <?php
-/**
- * Check to see if all messages have been translated into
- * the selected language. To run this script, you must have
- * a working installation, and it checks the selected language
- * of that installation.
- *
- * @package MediaWiki
- * @subpackage Maintenance
-*/
 
-/** */
-if ( ! is_readable( "../LocalSettings.php" ) ) {
-	print "A copy of your installation's LocalSettings.php\n" .
-	  "must exist in the source directory.\n";
-	exit();
-}
+# Check to see if all messages have been translated into
+# the selected language. To run this script, you must have
+# a working installation, and it checks the selected language
+# of that installation.
+#
 
-$wgCommandLineMode = true;
-$DP = "../includes";
-require_once( "../LocalSettings.php" );
+require_once("commandLine.inc");
 
 if ( "en" == $wgLanguageCode ) {
 	print "Current selected language is English. Cannot check translations.\n";
 	exit();
 }
-$include = "Language" . ucfirst( $wgLanguageCode ) . ".php";
-if ( ! is_readable( "{$IP}/{$include}" ) ) {
-	print "Translation file \"{$include}\" not found in installation directory.\n" .
-	  "You must have the software installed to run this script.\n";
-	exit();
-}
-
-umask( 000 );
-set_time_limit( 0 );
-
-require_once( "{$IP}/Setup.php" );
-$wgTitle = Title::newFromText( "Translation checking script" );
 
 $count = $total = 0;
 $msgarray = "wgAllMessages" . ucfirst( $wgLanguageCode );
 
 foreach ( $wgAllMessagesEn as $code => $msg ) {
 	++$total;
-
 	if ( ! array_key_exists( $code, $$msgarray ) ) {
-		print "{$code}\n";
+		print "'{$code}' => \"$msg\",\n";
 		++$count;
 	}
 }
+
 print "{$count} messages of {$total} not translated.\n";
+
+?>
