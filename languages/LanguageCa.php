ce57f2a191aa26c2311f58310f9bca3669071d6c
Brion Vibber <brion@users.mediawiki.org>
2004-10-11 18:33:31 +0000

    Bug 686: broken linktrail in Catalan

--- languages/LanguageCa.php
+++ languages/LanguageCa.php
@@ -158,7 +158,7 @@
 
 # Bits of text used by many pages:
 #
-"linktrail"     => "/^((?:[a-z]|à|è|é|í|ò|ó|ú|ç|·|ï|ü|')]+)(.*)\$/sD",
+"linktrail"     => "/^((?:[a-z]|à|è|é|í|ò|ó|ú|ç|·|ï|ü|')+)(.*)\$/sD",
 "mainpage"		=> "Portada",
 "about"			=> "Quant a...",
 "aboutwikipedia" => "Quant a la Viquipèdia",
