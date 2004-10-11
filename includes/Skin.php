f942f854fe8efee9c207c7cd494929d340871ccb
Jens Frank <jeluf@users.mediawiki.org>
2004-10-11 20:03:24 +0000

    BUG#187, BUG#669 Fix centered images. span apparently didn't work properly

--- includes/Skin.php
+++ includes/Skin.php
@@ -1738,8 +1738,8 @@ class Skin {
 			}
 			if ( 'center' == $align )
 			{
-				$prefix  = '<span style="text-align: center">';
-				$postfix = '</span>';
+				$prefix  = '<div class="center">';
+				$postfix = '</div>';
 				$align   = 'none';
 			}
 
