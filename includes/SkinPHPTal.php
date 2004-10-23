a196db1a110f65c248263f1183c84279b2e4a582
Brion Vibber <brion@users.mediawiki.org>
2004-10-23 07:37:48 +0000

    Kludge for the present state of PHPTAL 1.0.0's i18n:attributes, which isn't backwards-compatible with PHPTAL 0.7.0's version

--- includes/SkinPHPTal.php
+++ includes/SkinPHPTal.php
@@ -63,6 +63,9 @@ class MediaWiki_I18N {
 	}
 
 	function translate($value) {
+		// Hack for i18n:attributes in PHPTAL 1.0.0 dev version as of 2004-10-23
+		$value = preg_replace( '/^string:/', '', $value );
+		
 		$value = wfMsg( $value );
 		// interpolate variables
 		while (preg_match('/\$([0-9]*?)/sm', $value, $m)) {
