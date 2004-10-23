5a1ca030a2ba98399b1fa17b08ec45f1eb827584
Tim Starling <tstarling@users.mediawiki.org>
2004-10-23 01:07:38 +0000

    alsatian, inheriting from de

---
+++ languages/LanguageGem_alsatian.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+<Melancholie> for the moment it would be the best if LanguageAls.php would be 
+              the same like LanguageDe.php. That would help us a lot at als.
+<Melancholie> at the moment all is in English
+<TimStarling> ok
+<Melancholie> great
+<TimStarling> I'll make a stub language file that fetches everything from de
+<Melancholie> cool
+*/
+
+include_once( "LanguageDe.php" );
+
+class LanguageGem_alsatian extends LanguageDe {
+	/* Inherit everything. */
+}
+
+?>
