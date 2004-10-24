31ffc09b46fbe4d927fb38b910d10f1d5c2cca77
Brion Vibber <brion@users.mediawiki.org>
2004-10-24 22:20:52 +0000

    Install fixes for group table

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -347,6 +347,7 @@ CREATE TABLE `group` (
   group_id int(5) unsigned NOT NULL auto_increment,
   group_name varchar(50) NOT NULL default '',
   group_description varchar(255) NOT NULL default '',
+  group_rights tinyblob,
   PRIMARY KEY  (group_id)
 );
 
