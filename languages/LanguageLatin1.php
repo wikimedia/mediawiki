81a01ff18d8a888a13da4259f27cf50375d1d2f6
Jens Frank <jeluf@users.mediawiki.org>
2004-10-24 08:31:22 +0000

    fixed typo

--- languages/LanguageLatin1.php
+++ languages/LanguageLatin1.php
@@ -29,7 +29,7 @@
 
 # This file and LanguageUtf8.php may be included from within functions, so
 # we need to have global statements
-global $wgInputEncoding, $wgOutputEncoding, 
+global $wgInputEncoding, $wgOutputEncoding; 
 
 $wgInputEncoding = "ISO-8859-1";
 $wgOutputEncoding = "ISO-8859-1";
