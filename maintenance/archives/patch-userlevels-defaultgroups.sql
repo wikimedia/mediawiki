c21b133171c6f0d767effe8c91a282f9dab3990d
Brion Vibber <brion@users.mediawiki.org>
2004-10-24 22:28:21 +0000

    Upgrade fixes

--- maintenance/archives/patch-userlevels-defaultgroups.sql
+++ maintenance/archives/patch-userlevels-defaultgroups.sql
@@ -3,8 +3,11 @@
 -- Should probably be inserted when someone create a new database
 --
 
-INSERT INTO `group` VALUES (1,'Anonymous','Anonymous users','read,edit,createaccount');
-INSERT INTO `group` VALUES (2,'Loggedin','General logged in users','read,edit,move,upload');
-INSERT INTO `group` VALUES (3,'Sysops','Operators of this site','read,edit,move,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface');
-INSERT INTO `group` VALUES (4,'Bureaucrat','The bureaucrat group is able to make sysops for example. They have no other rights.','read,edit,move,delete,undelete,protect,block,userrights,createaccount,upload,asksql,rollback,patrol,editinterface,siteadmin');
-
+INSERT INTO `group` (group_id,group_name,group_description,group_rights)
+	VALUES (1,'Anonymous','Anonymous users','read,edit,createaccount');
+INSERT INTO `group` (group_id,group_name,group_description,group_rights)
+	VALUES (2,'Loggedin','General logged in users','read,edit,move,upload');
+INSERT INTO `group` (group_id,group_name,group_description,group_rights)
+	VALUES (3,'Sysops','Operators of this site','read,edit,move,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface');
+INSERT INTO `group` (group_id,group_name,group_description,group_rights)
+	VALUES (4,'Bureaucrat','The bureaucrat group is able to make sysops for example. They have no other rights.','read,edit,move,delete,undelete,protect,block,userrights,createaccount,upload,asksql,rollback,patrol,editinterface,siteadmin');
