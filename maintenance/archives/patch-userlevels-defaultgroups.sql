af322aba1878db22bbad94a276569b9c012694d5
Brion Vibber <brion@users.mediawiki.org>
2004-10-27 07:20:47 +0000

    Give sysops & bureaucrats the 'sysop' permission so they can edit protected pages!

--- maintenance/archives/patch-userlevels-defaultgroups.sql
+++ maintenance/archives/patch-userlevels-defaultgroups.sql
@@ -8,6 +8,6 @@ INSERT INTO `group` (group_id,group_name,group_description,group_rights)
 INSERT INTO `group` (group_id,group_name,group_description,group_rights)
 	VALUES (2,'Loggedin','General logged in users','read,edit,move,upload');
 INSERT INTO `group` (group_id,group_name,group_description,group_rights)
-	VALUES (3,'Sysops','Operators of this site','read,edit,move,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface');
+	VALUES (3,'Sysops','Operators of this site','read,edit,move,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,sysop');
 INSERT INTO `group` (group_id,group_name,group_description,group_rights)
-	VALUES (4,'Bureaucrat','The bureaucrat group is able to make sysops for example. They have no other rights.','read,edit,move,delete,undelete,protect,block,userrights,createaccount,upload,asksql,rollback,patrol,editinterface,siteadmin');
+	VALUES (4,'Bureaucrat','The bureaucrat group is able to make sysops for example. They have no other rights.','read,edit,move,delete,undelete,protect,block,userrights,createaccount,upload,asksql,rollback,patrol,editinterface,siteadmin,sysop');
