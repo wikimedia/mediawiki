84a5213d5b63ebc83bcb6b0a57deab801d8557da
Brion Vibber <brion@users.mediawiki.org>
2004-10-27 07:33:54 +0000

    Add sysop group fix to updaters

---
+++ maintenance/archives/patch-group-sysopfix.sql
@@ -0,0 +1,7 @@
+-- Fix to alpha versions that had incorrect rights assignments
+-- breaking protected page edits by sysops.
+-- 2004-10-27
+
+UPDATE `group`
+   SET group_rights=CONCAT(group_rights,',sysop')
+ WHERE group_name IN('Sysops','Bureaucrat');
