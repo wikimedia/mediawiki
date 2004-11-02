59c8c52f5b15f72d3184b125b3963bc6c04b43fe
Brion Vibber <brion@users.mediawiki.org>
2004-11-02 19:22:00 +0000

    Month names

--- languages/LanguageKn.php
+++ languages/LanguageKn.php
@@ -2,6 +2,21 @@
 
 require_once( "LanguageUtf8.php" );
 
+/* private */ $wgAllMessagesKn = array(
+'jan' => "ಜನವರಿ",
+'feb' => "ಫೆಬ್ರುವರಿ",
+'mar' => "ಮಾರ್ಚ್",
+'apr' => "ಎಪ್ರಿಲ್",
+'may' => "ಮೇ",
+'jun' => "ಜೂನ್",
+'jul' => "ಜುಲೈ",
+'aug' => "ಆಗಸ್ಟ್",
+'sep' => "ಸೆಪ್ಟೆಂಬರ್",
+'oct' => "ಅಕ್ಟೋಬರ್",
+'nov' => "ನವೆಂಬರ್",
+'dec' => "ಡಿಸೆಂಬರ್",
+);
+
 class LanguageKn extends LanguageUtf8 {
 	var $digitTransTable = array(
 		"0" => "೦",
@@ -15,6 +30,14 @@ class LanguageKn extends LanguageUtf8 {
 		"8" => "೮",
 		"9" => "೯"
 	);
+
+	function getMessage( $key ) {
+		global $wgAllMessagesKn;
+		if( array_key_exists( $key, $wgAllMessagesKn ) )
+			return $wgAllMessagesKn[$key];
+		else
+			return Language::getMessage($key);
+	}
 
 	function formatNum( $number ) {
 		global $wgTranslateNumerals;
