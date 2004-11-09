2e702317c01698df4274e8cc653426d5575bec17
Antoine Musso <hashar@users.mediawiki.org>
2004-11-09 15:29:03 +0000

    Backport from HEAD: Patch by "Neil Barsema" <neil@barsema.org> : http://bugzilla.wikipedia.org/show_bug.cgi?id=809
    Attempt to load mysql php extension if it is not loaded.

--- includes/Database.php
+++ includes/Database.php
@@ -90,6 +90,11 @@ class Database {
 		global $wgEmergencyContact;
 		
 		# Test for missing mysql.so
+		# First try to load it
+		if (!@extension_loaded('mysql')) {
+			@dl('mysql.so');
+		}
+
 		# Otherwise we get a suppressed fatal error, which is very hard to track down
 		if ( !function_exists( 'mysql_connect' ) ) {
 			die( "MySQL functions missing, have you compiled PHP with the --with-mysql option?\n" );
