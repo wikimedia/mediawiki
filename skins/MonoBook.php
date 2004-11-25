eacaf8693b77fb0a607bac591638e0dd7fe5c719
Brion Vibber <brion@users.mediawiki.org>
2004-11-25 05:31:49 +0000

    Rearrange the new template system a bit to make things less verbose in most cases, and easier to reuse...

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -28,51 +28,50 @@ class SkinMonoBook extends SkinTemplate {
 		$this->template  = 'MonoBookTemplate';
 	}
 }
-
-/**
- * Template filter callback for MonoBook skin.
- * Takes an associative array of data set from a SkinTemplate-based
- * class, and a wrapper for MediaWiki's localization database, and
- * outputs a formatted page.
- *
- * @param array $data
- * @param MediaWiki_I18N $translator
- * @access private
- */
-function MonoBookTemplate( &$data, &$translator ) {
+	
+class MonoBookTemplate extends QuickTemplate {
+	/**
+	 * Template filter callback for MonoBook skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo htmlspecialchars($data['lang']) ?>" lang="<?php echo htmlspecialchars($data['lang']) ?>" dir="<?php echo htmlspecialchars($data['dir']) ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
   <head>
-    <meta http-equiv="Content-Type" content="<?php echo htmlspecialchars($data['mimetype']) ?>; charset=<?php echo htmlspecialchars($data['charset']) ?>" />
-    <?php echo $data['headlinks'] ?>
-    <title><?php echo htmlspecialchars( $data['pagetitle'] ) ?></title>
-    <style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php echo htmlspecialchars($data['stylepath']) ?>/<?php echo htmlspecialchars($data['stylename']) ?>/main.css"; /*]]>*/</style>
-    <link rel="stylesheet" type="text/css" media="print" href="<?php echo htmlspecialchars($data['stylepath']) ?>/common/commonPrint.css" />
-    <!--[if IE]><style type="text/css" media="all">@import "<?php echo htmlspecialchars($data['stylepath']) ?>/<?php echo htmlspecialchars($data['stylename']) ?>/IEFixes.css";</style>
-    <script type="text/javascript" src="<?php echo htmlspecialchars($data['stylepath']) ?>/common/IEFixes.js"></script>
+    <meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
+    <?php $this->html('headlinks') ?>
+    <title><?php $this->text('pagetitle') ?></title>
+    <style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css"; /*]]>*/</style>
+    <link rel="stylesheet" type="text/css" media="print" href="<?php $this->text('stylepath') ?>/common/commonPrint.css" />
+    <!--[if IE]><style type="text/css" media="all">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IEFixes.css";</style>
+    <script type="text/javascript" src="<?php $this->text('stylepath') ?>/common/IEFixes.js"></script>
     <meta http-equiv="imagetoolbar" content="no" /><![endif]-->
-    <?php if($data['jsvarurl'  ]) { ?><script type="text/javascript" src="<?php echo htmlspecialchars($data['jsvarurl'  ]) ?>"></script><?php } ?>
-    <script type="text/javascript" src="<?php                                   echo htmlspecialchars($data['stylepath'])  ?>/common/wikibits.js"></script>
-    <?php if($data['usercss'   ]) { ?><style type="text/css"><?php              echo                  $data['usercss']     ?></style><?php    } ?>
-    <?php if($data['userjs'    ]) { ?><script type="text/javascript" src="<?php echo htmlspecialchars($data['userjs'    ]) ?>"></script><?php } ?>
-    <?php if($data['userjsprev']) { ?><script type="text/javascript"><?php      echo                  $data['userjsprev']  ?></script><?php   } ?>
+    <?php if($this->data['jsvarurl'  ]) { ?><script type="text/javascript" src="<?php $this->text('jsvarurl'  ) ?>"></script><?php } ?>
+    <script type="text/javascript" src="<?php                                   $this->text('stylepath' ) ?>/common/wikibits.js"></script>
+    <?php if($this->data['usercss'   ]) { ?><style type="text/css"><?php              $this->html('usercss'   ) ?></style><?php    } ?>
+    <?php if($this->data['userjs'    ]) { ?><script type="text/javascript" src="<?php $this->text('userjs'    ) ?>"></script><?php } ?>
+    <?php if($this->data['userjsprev']) { ?><script type="text/javascript"><?php      $this->html('userjsprev') ?></script><?php   } ?>
   </head>
-  <body <?php if($data['body_ondblclick']) { ?>ondblclick="<?php echo htmlspecialchars($data['body_ondblclick']) ?>"<?php } ?>
-        <?php if($data['nsclass'        ]) { ?>class="<?php      echo htmlspecialchars($data['nsclass'        ]) ?>"<?php } ?>>
+  <body <?php if($this->data['body_ondblclick']) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+        <?php if($this->data['nsclass'        ]) { ?>class="<?php      $this->text('nsclass')         ?>"<?php } ?>>
     <div id="globalWrapper">
       <div id="column-content">
 	<div id="content">
 	  <a name="top" id="contentTop"></a>
-	  <?php if($data['sitenotice']) { ?><div id="siteNotice"><?php echo $data['sitenotice'] ?></div><?php } ?>
-	  <h1 class="firstHeading"><?php echo htmlspecialchars($data['title']) ?></h1>
+	  <?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+	  <h1 class="firstHeading"><?php $this->text('title') ?></h1>
 	  <div id="bodyContent">
-	    <h3 id="siteSub"><?php echo htmlspecialchars($translator->translate('tagline')) ?></h3>
-	    <div id="contentSub"><?php echo $data['subtitle'] ?></div>
-	    <?php if($data['undelete']) { ?><div id="contentSub"><?php     echo $data['undelete'] ?></div><?php } ?>
-	    <?php if($data['newtalk'] ) { ?><div class="usermessage"><?php echo $data['newtalk']  ?></div><?php } ?>
+	    <h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+	    <div id="contentSub"><?php $this->html('subtitle') ?></div>
+	    <?php if($this->data['undelete']) { ?><div id="contentSub"><?php     $this->html('undelete') ?></div><?php } ?>
+	    <?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
 	    <!-- start content -->
-	    <?php echo $data['bodytext'] ?>
-	    <?php if($data['catlinks']) { ?><div id="catlinks"><?php echo $data['catlinks'] ?></div><?php } ?>
+	    <?php $this->html('bodytext') ?>
+	    <?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
 	    <!-- end content -->
 	    <div class="visualClear"></div>
 	  </div>
@@ -82,7 +81,7 @@ function MonoBookTemplate( &$data, &$translator ) {
 	<div id="p-cactions" class="portlet">
 	  <h5>Views</h5>
 	  <ul>
-	    <?php foreach($data['content_actions'] as $key => $action) {
+	    <?php foreach($this->data['content_actions'] as $key => $action) {
 	       ?><li id="ca-<?php echo htmlspecialchars($key) ?>"
 	       <?php if($action['class']) { ?>class="<?php echo htmlspecialchars($action['class']) ?>"<?php } ?>
 	       ><a href="<?php echo htmlspecialchars($action['href']) ?>"><?php
@@ -91,10 +90,10 @@ function MonoBookTemplate( &$data, &$translator ) {
 	  </ul>
 	</div>
 	<div class="portlet" id="p-personal">
-	  <h5><?php echo htmlspecialchars($translator->translate('personaltools')) ?></h5>
+	  <h5><?php $this->msg('personaltools') ?></h5>
 	  <div class="pBody">
 	    <ul>
-	    <?php foreach($data['personal_urls'] as $key => $item) {
+	    <?php foreach($this->data['personal_urls'] as $key => $item) {
 	       ?><li id="pt-<?php echo htmlspecialchars($key) ?>"><a href="<?php
 	       echo htmlspecialchars($item['href']) ?>"<?php
 	       if(!empty($item['class'])) { ?> class="<?php
@@ -105,15 +104,15 @@ function MonoBookTemplate( &$data, &$translator ) {
 	  </div>
 	</div>
 	<div class="portlet" id="p-logo">
-	  <a style="background-image: url(<?php echo htmlspecialchars($data['logopath']) ?>);"
-	    href="<?php echo htmlspecialchars($data['nav_urls']['mainpage']['href'])?>"
-	    title="<?php echo htmlspecialchars($translator->translate('mainpage')) ?>"></a>
+	  <a style="background-image: url(<?php $this->text('logopath') ?>);"
+	    href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"
+	    title="<?php $this->msg('mainpage') ?>"></a>
 	</div>
 	<div class="portlet" id="p-nav">
-	  <h5><?php echo htmlspecialchars($translator->translate('navigation')) ?></h5>
+	  <h5><?php $this->msg('navigation') ?></h5>
 	  <div class="pBody">
 	    <ul>
-	      <?php foreach($data['navigation_urls'] as $navlink) { ?>
+	      <?php foreach($this->data['navigation_urls'] as $navlink) { ?>
 	      <li id="<?php echo htmlspecialchars($navlink['id'])
 	        ?>"><a href="<?php echo htmlspecialchars($navlink['href']) ?>"><?php 
 	        echo htmlspecialchars($navlink['text']) ?></a></li><?php } ?>
@@ -121,46 +120,46 @@ function MonoBookTemplate( &$data, &$translator ) {
 	  </div>
 	</div>
 	<div id="p-search" class="portlet">
-	  <h5><?php echo htmlspecialchars($translator->translate('search')) ?></h5>
+	  <h5><?php $this->msg('search') ?></h5>
 	  <div class="pBody">
-	    <form name="searchform" action="<?php echo htmlspecialchars($data['searchaction']) ?>" id="searchform">
+	    <form name="searchform" action="<?php $this->text('searchaction') ?>" id="searchform">
 	      <input id="searchInput" name="search" type="text"
-	        <?php if($accesskey = $translator->translate('accesskey-search')) {
-	          ?>accesskey="<?php echo htmlspecialchars($accesskey) ?>"<?php } ?> />
+	        <?php if($this->haveMsg('accesskey-search')) {
+	          ?>accesskey="<?php $this->msg('accesskey-search') ?>"<?php } ?> />
 	      <input type='submit' name="go" class="searchButton"
-	        value="<?php echo htmlspecialchars($translator->translate('go')) ?>"
+	        value="<?php $this->msg('go') ?>"
 	        />&nbsp;<input type='submit' name="fulltext"
 	        class="searchButton"
-	        value="<?php echo htmlspecialchars($translator->translate('search')) ?>" />
+	        value="<?php $this->msg('search') ?>" />
 	    </form>
 	  </div>
 	</div>
 	<div class="portlet" id="p-tb">
-	  <h5><?php echo htmlspecialchars($translator->translate('toolbox')) ?></h5>
+	  <h5><?php $this->msg('toolbox') ?></h5>
 	  <div class="pBody">
 	    <ul>
-		  <?php if($data['notspecialpage']) { foreach( array( 'whatlinkshere', 'recentchangeslinked' ) as $special ) { ?>
+		  <?php if($this->data['notspecialpage']) { foreach( array( 'whatlinkshere', 'recentchangeslinked' ) as $special ) { ?>
 		  <li id="t-<?php echo $special?>"><a href="<?php
-		    echo htmlspecialchars($data['nav_urls'][$special]['href']) 
-		    ?>"><?php echo htmlspecialchars($translator->translate($special)) ?></a></li>
+		    echo htmlspecialchars($this->data['nav_urls'][$special]['href']) 
+		    ?>"><?php echo $this->msg($special) ?></a></li>
 		  <?php } } ?>
-	      <?php if($data['feeds']) { ?><li id="feedlinks"><?php foreach($data['feeds'] as $key => $feed) {
+	      <?php if($this->data['feeds']) { ?><li id="feedlinks"><?php foreach($this->data['feeds'] as $key => $feed) {
 	        ?><span id="feed-<?php echo htmlspecialchars($key) ?>"><a href="<?php
 	        echo htmlspecialchars($feed['href']) ?>"><?php echo htmlspecialchars($feed['text'])?>&nbsp;</span>
 	        <?php } ?></li><?php } ?>
 	      <?php foreach( array('contributions', 'emailuser', 'upload', 'specialpages') as $special ) { ?>
-	      <?php if($data['nav_urls'][$special]) {?><li id="t-<?php echo $special ?>"><a href="<?php
-	        echo htmlspecialchars($data['nav_urls'][$special]['href'])
-	        ?>"><?php echo htmlspecialchars($translator->translate($special)) ?></a></li><?php } ?>
+	      <?php if($this->data['nav_urls'][$special]) {?><li id="t-<?php echo $special ?>"><a href="<?php
+	        echo htmlspecialchars($this->data['nav_urls'][$special]['href'])
+	        ?>"><?php $this->msg($special) ?></a></li><?php } ?>
 	      <?php } ?>
 	    </ul>
 	  </div>
 	</div>
-	<?php if( $data['language_urls'] ) { ?><div id="p-lang" class="portlet">
-	  <h5><?php echo htmlspecialchars($translator->translate('otherlanguages')) ?></h5>
+	<?php if( $this->data['language_urls'] ) { ?><div id="p-lang" class="portlet">
+	  <h5><?php $this->msg('otherlanguages') ?></h5>
 	  <div class="pBody">
 	    <ul>
-	      <?php foreach($data['language_urls'] as $langlink) { ?>
+	      <?php foreach($this->data['language_urls'] as $langlink) { ?>
 	      <li>
 	      <a href="<?php echo htmlspecialchars($langlink['href'])
 	        ?>"><?php echo htmlspecialchars($langlink['text']) ?></a>
@@ -173,22 +172,23 @@ function MonoBookTemplate( &$data, &$translator ) {
       </div><!-- end of the left (by default at least) column -->
       <div class="visualClear"></div>
       <div id="footer">
-    <?php if($data['poweredbyico']) { ?><div id="f-poweredbyico"><?php echo $data['poweredbyico'] ?></div><?php } ?>
-	<?php if($data['copyrightico']) { ?><div id="f-copyrightico"><?php echo $data['copyrightico'] ?></div><?php } ?>
+    <?php if($this->data['poweredbyico']) { ?><div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div><?php } ?>
+	<?php if($this->data['copyrightico']) { ?><div id="f-copyrightico"><?php $this->html('copyrightico') ?></div><?php } ?>
 	<ul id="f-list">
-	  <?php if($data['lastmod'   ]) { ?><li id="f-lastmod"><?php    echo $data['lastmod'   ] ?></li><?php } ?>
-	  <?php if($data['viewcount' ]) { ?><li id="f-viewcount"><?php  echo $data['viewcount' ] ?></li><?php } ?>
-	  <?php if($data['credits'   ]) { ?><li id="f-credits"><?php    echo $data['credits'   ] ?></li><?php } ?>
-	  <?php if($data['copyright' ]) { ?><li id="f-copyright"><?php  echo $data['copyright' ] ?></li><?php } ?>
-	  <?php if($data['about'     ]) { ?><li id="f-about"><?php      echo $data['about'     ] ?></li><?php } ?>
-	  <?php if($data['disclaimer']) { ?><li id="f-disclaimer"><?php echo $data['disclaimer'] ?></li><?php } ?>
+	  <?php if($this->data['lastmod'   ]) { ?><li id="f-lastmod"><?php    $this->html('lastmod')    ?></li><?php } ?>
+	  <?php if($this->data['viewcount' ]) { ?><li id="f-viewcount"><?php  $this->html('viewcount')  ?></li><?php } ?>
+	  <?php if($this->data['credits'   ]) { ?><li id="f-credits"><?php    $this->html('credits')    ?></li><?php } ?>
+	  <?php if($this->data['copyright' ]) { ?><li id="f-copyright"><?php  $this->html('copyright')  ?></li><?php } ?>
+	  <?php if($this->data['about'     ]) { ?><li id="f-about"><?php      $this->html('about')      ?></li><?php } ?>
+	  <?php if($this->data['disclaimer']) { ?><li id="f-disclaimer"><?php $this->html('disclaimer') ?></li><?php } ?>
 	</ul>
       </div>
     </div>
-    <?php echo $data['reporttime'] ?>
+    <?php $this->html('reporttime') ?>
   </body>
 </html>
 <?php
+	}
 }
 
 ?>
