f434795aaf26f948c4e238fce22cbdbb47ac494e
Brion Vibber <brion@users.mediawiki.org>
2004-12-07 07:55:26 +0000

    Fix fatal error on search update

--- includes/SearchMySQL4.php
+++ includes/SearchMySQL4.php
@@ -95,7 +95,7 @@ class SearchMySQL4 extends SearchEngine {
                 $searchindex = $dbw->tableName( 'searchindex' );
 
                 $sql = "UPDATE $lowpri $searchindex SET si_title='" .
-                          $db->strencode( $title ) .
+                          $dbw->strencode( $title ) .
                           "' WHERE si_page={$id}";
 
                 $dbw->query( $sql, "SearchMySQL4::updateTitle" );
