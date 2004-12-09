9a2a36ce5e322f7ad8ac50b65de54b6a4171da36
Brion Vibber <brion@users.mediawiki.org>
2004-12-09 05:51:20 +0000

    * Defer message cache initialization, shaving a few ms off file cache hits

--- languages/Language.php
+++ languages/Language.php
@@ -2230,6 +2230,20 @@ class Language {
 	function getExtraHashOptions() {
 		return array();
 	}
+	
+	/**
+	 * A regular expression to match legal word-trailing characters
+	 * which should be merged onto a link of the form [[foo]]bar.
+	 * FIXME
+	 *
+	 * @return string
+	 * @access public
+	 */
+	function linkTrail() {
+		$trail = $this->getMessage( 'linktrail' );
+		if( empty( $trail ) ) $trail = Language::linkTrail();
+		return $trail;
+	}
 
 }
 
