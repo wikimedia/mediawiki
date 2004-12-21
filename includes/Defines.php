43162f48ce9f6e773edfb623fe63987896024100
Guillaume Blanchard <aoineko_jp@users.mediawiki.org>
2004-12-21 14:22:25 +0000

    Reserved namespace 100 and 101 for forum-like's thread pages

--- includes/Defines.php
+++ includes/Defines.php
@@ -40,6 +40,8 @@ define('NS_HELP', 12);
 define('NS_HELP_TALK', 13);
 define('NS_CATEGORY', 14);
 define('NS_CATEGORY_TALK', 15);
+define('NS_THREAD', 100);           // Reserved for Forum-like
+define('NS_THREAD_TALK', 101);      // Reserved for Forum-like
 /**#@-*/
 
 /**#@+
