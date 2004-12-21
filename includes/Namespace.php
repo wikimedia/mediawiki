4bb7079f8a74364ee626841d4374b1cbe00590bc
Guillaume Blanchard <aoineko_jp@users.mediawiki.org>
2004-12-21 14:22:25 +0000

    Reserved namespace 100 and 101 for forum-like's thread pages

--- includes/Namespace.php
+++ includes/Namespace.php
@@ -32,6 +32,9 @@ $wgCanonicalNamespaceNames = array(
 	NS_HELP_TALK        => 'Help_talk',
 	NS_CATEGORY	        => 'Category',
 	NS_CATEGORY_TALK    => 'Category_talk'
+    
+	NS_THREAD	        => 'Thread',        // Reserved for Forum-like
+	NS_THREAD_TALK      => 'Thread_talk'    // Reserved for Forum-like
 );
 
 if( defined( 'MEDIAWIKI' ) && is_array( $wgExtraNamespaces ) ) {
