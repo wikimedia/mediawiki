c7c31c6fecde75b0fa96a215e6de4a05eabc020f
Zheng Zhu <zhengzhu@users.mediawiki.org>
2004-12-29 02:47:33 +0000

    Added magic word MAG_NOTITLECONVERT to indicate that the title of the page do not need to be converted. Useful in zh:

--- includes/MagicWord.php
+++ includes/MagicWord.php
@@ -45,6 +45,7 @@ define('MAG_PAGENAMEE', 31);
 define('MAG_NAMESPACE', 32);
 define('MAG_TOC', 33);
 define('MAG_GRAMMAR', 34);
+define('MAG_NOTITLECONVERT', 35);
 
 $wgVariableIDs = array(
 	MAG_CURRENTMONTH,
