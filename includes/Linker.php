a4c210ce37757c8335ba2ed53c7d01d79e2088b1
Tom Gilder <tomgilder@users.mediawiki.org>
2005-01-12 01:46:33 +0000

    Minor XHTML fix (and my first commit, scary!)

--- includes/Linker.php
+++ includes/Linker.php
@@ -509,7 +509,7 @@ class Linker {
 		$u = $nt->escapeLocalURL();
 		if ( $url == '' ) {
 			$s = wfMsg( 'missingimage', $img->getName() );
-			$s .= "<br>{$alt}<br>{$url}<br>\n";
+			$s .= "<br />{$alt}<br />{$url}<br />\n";
 		} else {
 			$s = '<a href="'.$u.'" class="image" title="'.$alt.'">' .
 				 '<img src="'.$url.'" alt="'.$alt.'" longdesc="'.$u.'" /></a>';
