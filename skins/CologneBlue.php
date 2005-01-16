48b874056d5b3cb93da3af24406b45d0de2f3cd0
Brion Vibber <brion@users.mediawiki.org>
2005-01-16 02:04:22 +0000

    * (bug 1339) Fix order of @imports in Cologne Blue CSS

--- skins/CologneBlue.php
+++ skins/CologneBlue.php
@@ -99,7 +99,7 @@ class SkinCologneBlue extends Skin {
 	function doGetUserStyles()
 	{
 		global $wgUser, $wgOut, $wgStyleSheetPath;
-		$s = '';
+		$s = parent::doGetUserStyles();
 		$qb = $this->qbSetting();
 
 		if ( 2 == $qb ) { # Right
@@ -114,7 +114,6 @@ class SkinCologneBlue extends Skin {
 			  "#article { margin-left:148px; margin-right: 4px; } \n" .
 			  "body>#quickbar { position:fixed; left:4px; top:4px; overflow:auto ;bottom:4px;} \n"; # Hides from IE
 		}
-		$s .= parent::doGetUserStyles();
 		return $s;
 	}
 	function sysLinks()
