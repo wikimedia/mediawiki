01ebb5a381ae675c0ccf8dacec142877c80aa500
Jens Frank <jeluf@users.mediawiki.org>
2005-01-23 15:49:39 +0000

    BUG#1364 Backport whitespace regex from 1.55.2.15

--- includes/Title.php
+++ includes/Title.php
@@ -1071,7 +1071,7 @@ class Title {
 
 		# Clean up whitespace
 		#
-		$t = preg_replace( '/[\\s_]+/', '_', $this->mDbkeyform );
+		$t = preg_replace( '/[ _]+/', '_', $this->mDbkeyform );
 		$t = trim( $t, '_' );
 
 		if ( '' == $t ) {
