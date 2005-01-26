67c18cd718a93943785394e2dd325e76236a5983
Brion Vibber <brion@users.mediawiki.org>
2005-01-26 09:49:40 +0000

    * Add TitleMoveComplete hook on page renames

--- includes/Title.php
+++ includes/Title.php
@@ -1408,6 +1408,7 @@ class Title {
 		$u = new SearchUpdate( $newid, $this->getPrefixedDBkey(), '' );
 		$u->doUpdate();
 
+		wfRunHooks( 'TitleMoveComplete', $this, $nt, $wgUser, $oldid, $newid );
 		return true;
 	}
 	
