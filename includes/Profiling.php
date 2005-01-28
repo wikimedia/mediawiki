b06411128df1401a22a4ffffb4a3887a45c74efc
Antoine Musso <hashar@users.mediawiki.org>
2005-01-28 00:50:58 +0000

    cleaner code formatting.

--- includes/Profiling.php
+++ includes/Profiling.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * This file is only included if profiling is enabled
  * @package MediaWiki
@@ -7,22 +7,22 @@
 /**
  * @param $functioname name of the function we will profile
  */
-function wfProfileIn( $functionname ) {
+function wfProfileIn($functionname) {
 	global $wgProfiler;
-	$wgProfiler->profileIn( $functionname );
+	$wgProfiler->profileIn($functionname);
 }
 
 /**
  * @param $functioname name of the function we have profiled
  */
-function wfProfileOut( $functionname = 'missing' ) {
+function wfProfileOut($functionname = 'missing') {
 	global $wgProfiler;
-	$wgProfiler->profileOut( $functionname );
+	$wgProfiler->profileOut($functionname);
 }
 
-function wfGetProfilingOutput( $start, $elapsed ) {
+function wfGetProfilingOutput($start, $elapsed) {
 	global $wgProfiler;
-	return $wgProfiler->getOutput( $start, $elapsed );
+	return $wgProfiler->getOutput($start, $elapsed);
 }
 
 function wfProfileClose() {
@@ -30,7 +30,7 @@ function wfProfileClose() {
 	$wgProfiler->close();
 }
 
-if( !function_exists( 'memory_get_usage' ) ) {
+if (!function_exists('memory_get_usage')) {
 	# Old PHP or --enable-memory-limit not compiled in
 	function memory_get_usage() {
 		return 0;
@@ -41,10 +41,9 @@ if( !function_exists( 'memory_get_usage' ) ) {
  * @todo document
  * @package MediaWiki
  */
-class Profiler
-{
-	var $mStack = array(), $mWorkStack = array(), $mCollated = array();
-	var $mCalls = array(), $mTotals = array();
+class Profiler {
+	var $mStack = array (), $mWorkStack = array (), $mCollated = array ();
+	var $mCalls = array (), $mTotals = array ();
 	/*
 	function Profiler()
 	{
@@ -53,34 +52,35 @@ class Profiler
 		$this->mCollated = array();
 	}
 	*/
-	
-	function profileIn( $functionname ) {
+
+	function profileIn($functionname) {
 		global $wgDebugFunctionEntry;
-		if ( $wgDebugFunctionEntry && function_exists( 'wfDebug' ) ) {
-			wfDebug( str_repeat( ' ', count( $this->mWorkStack ) ) . 'Entering '.$functionname."\n" );
+		if ($wgDebugFunctionEntry && function_exists('wfDebug')) {
+			wfDebug(str_repeat(' ', count($this->mWorkStack)).'Entering '.$functionname."\n");
 		}
-		$this->mWorkStack[] = array($functionname, count( $this->mWorkStack ), microtime(), memory_get_usage() );
+		$this->mWorkStack[] = array ($functionname, count($this->mWorkStack), microtime(), memory_get_usage());
 	}
 
-	function profileOut( $functionname ) {
+	function profileOut($functionname) {
 		$memory = memory_get_usage();
 		$time = microtime();
 		global $wgDebugProfiling, $wgDebugFunctionEntry;
 
-		if ( $wgDebugFunctionEntry && function_exists( 'wfDebug' ) ) {
-			wfDebug( str_repeat( ' ', count( $this->mWorkStack ) ) . 'Exiting '.$functionname."\n" );
+		if ($wgDebugFunctionEntry && function_exists('wfDebug')) {
+			wfDebug(str_repeat(' ', count($this->mWorkStack)).'Exiting '.$functionname."\n");
 		}
-		
-		$bit = array_pop( $this->mWorkStack );
-		
-		if ( !$bit ) {
-			wfDebug( "Profiling error, !\$bit: $functionname\n" );
+
+		$bit = array_pop($this->mWorkStack);
+
+		if (!$bit) {
+			wfDebug("Profiling error, !\$bit: $functionname\n");
 		} else {
-			if ( $wgDebugProfiling ) {
-				if ( $functionname == 'close' ) {
-					wfDebug( "Profile section ended by close(): {$bit[0]}\n" );
-				} elseif ( $bit[0] != $functionname ) {
-					wfDebug( "Profiling error: in({$bit[0]}), out($functionname)\n" );
+			if ($wgDebugProfiling) {
+				if ($functionname == 'close') {
+					wfDebug("Profile section ended by close(): {$bit[0]}\n");
+				}
+				elseif ($bit[0] != $functionname) {
+					wfDebug("Profiling error: in({$bit[0]}), out($functionname)\n");
 				}
 			}
 			$bit[] = $time;
@@ -88,10 +88,10 @@ class Profiler
 			$this->mStack[] = $bit;
 		}
 	}
-	
+
 	function close() {
-		while ( count( $this->mWorkStack ) ) {
-			$this->profileOut( 'close' );
+		while (count($this->mWorkStack)) {
+			$this->profileOut('close');
 		}
 	}
 
@@ -99,145 +99,140 @@ class Profiler
 		global $wgDebugFunctionEntry;
 		$wgDebugFunctionEntry = false;
 
-		if( !count( $this->mStack ) ) {
+		if (!count($this->mStack)) {
 			return "No profiling output\n";
 		}
 		$this->close();
-		
+
 		global $wgProfileCallTree;
-		if( $wgProfileCallTree ) {
+		if ($wgProfileCallTree) {
 			return $this->getCallTree();
 		} else {
 			return $this->getFunctionReport();
 		}
 	}
-	
-	function getCallTree( $start = 0 ) {
-		return implode( '',
-			array_map(
-				array( &$this, 'getCallTreeLine' ),
-				$this->remapCallTree( $this->mStack ) ) );
+
+	function getCallTree($start = 0) {
+		return implode('', array_map(array (& $this, 'getCallTreeLine'), $this->remapCallTree($this->mStack)));
 	}
-	
-	function remapCallTree( $stack ) {
-		if( count( $stack ) < 2 ) {
+
+	function remapCallTree($stack) {
+		if (count($stack) < 2) {
 			return $stack;
 		}
-		$outputs = array();
-		for( $max = count( $stack ) - 1; $max > 0; ) {
+		$outputs = array ();
+		for ($max = count($stack) - 1; $max > 0;) {
 			/* Find all items under this entry */
 			$level = $stack[$max][1];
-			$working = array();
-			for( $i = $max - 1; $i >= 0; $i-- ) {
-				if( $stack[$i][1] > $level ) {
+			$working = array ();
+			for ($i = $max -1; $i >= 0; $i --) {
+				if ($stack[$i][1] > $level) {
 					$working[] = $stack[$i];
 				} else {
 					break;
 				}
 			}
-			$working = $this->remapCallTree( array_reverse( $working ) );
-			$output = array();
-			foreach( $working as $item ) {
-				array_push( $output, $item );
+			$working = $this->remapCallTree(array_reverse($working));
+			$output = array ();
+			foreach ($working as $item) {
+				array_push($output, $item);
 			}
-			array_unshift( $output, $stack[$max] );
+			array_unshift($output, $stack[$max]);
 			$max = $i;
-			
-			array_unshift( $outputs, $output );
+
+			array_unshift($outputs, $output);
 		}
-		$final = array();
-		foreach( $outputs as $output ) {
-			foreach( $output as $item ) {
+		$final = array ();
+		foreach ($outputs as $output) {
+			foreach ($output as $item) {
 				$final[] = $item;
 			}
 		}
 		return $final;
 	}
-	
-	function getCallTreeLine( $entry ) {
-		list( $fname, $level, $start, $x, $end ) = $entry;
-		$delta = $this->microDelta( $start, $end );
-		$space = str_repeat( ' ', $level );
-		
+
+	function getCallTreeLine($entry) {
+		list ($fname, $level, $start, $x, $end) = $entry;
+		$delta = $this->microDelta($start, $end);
+		$space = str_repeat(' ', $level);
+
 		# The ugly double sprintf is to work around a PHP bug,
 		# which has been fixed in recent releases.
-		return sprintf( "%10s %s %s\n",
-			trim( sprintf( "%7.3f", $delta * 1000.0 ) ),
-			$space, $fname );
+		return sprintf("%10s %s %s\n", trim(sprintf("%7.3f", $delta * 1000.0)), $space, $fname);
 	}
-	
-	function micro2Float( $micro ) {
-		list( $whole, $fractional ) = explode( ' ', $micro );
-		return (float)$whole + (float)$fractional;
+
+	function micro2Float($micro) {
+		list ($whole, $fractional) = explode(' ', $micro);
+		return (float) $whole + (float) $fractional;
 	}
-	
-	function microDelta( $start, $end ) {
-		return $this->micro2Float( $end ) -
-		       $this->micro2Float( $start );
+
+	function microDelta($start, $end) {
+		return $this->micro2Float($end) - $this->micro2Float($start);
 	}
-	
-	function getFunctionReport() {		
+
+	function getFunctionReport() {
 		$width = 125;
-		$format = "%-" . ($width - 34) . "s %6d %6.3f %6.3f %7.3f%% %6d (%6.3f-%6.3f) [%d]\n";
-		$titleFormat = "%-" . ($width - 34) . "s %9s %9s %9s %9s %6s\n";
+		$format = "%-". ($width -34)."s %6d %6.3f %6.3f %7.3f%% %6d (%6.3f-%6.3f) [%d]\n";
+		$titleFormat = "%-". ($width -34)."s %9s %9s %9s %9s %6s\n";
 		$prof = "\nProfiling data\n";
-		$prof .= sprintf( $titleFormat, 'Name', 'Calls', 'Total', 'Each', '%', 'Mem' );
-		$this->mCollated = array();
-		$this->mCalls = array();
-		$this->mMemory = array();
-		
+		$prof .= sprintf($titleFormat, 'Name', 'Calls', 'Total', 'Each', '%', 'Mem');
+		$this->mCollated = array ();
+		$this->mCalls = array ();
+		$this->mMemory = array ();
+
 		# Estimate profiling overhead
-		$profileCount = count( $this->mStack );
-		wfProfileIn( '-overhead-total' );
-		for ($i=0; $i<$profileCount ; $i++) {
-			wfProfileIn( '-overhead-internal' );
-			wfProfileOut( '-overhead-internal' );
+		$profileCount = count($this->mStack);
+		wfProfileIn('-overhead-total');
+		for ($i = 0; $i < $profileCount; $i ++) {
+			wfProfileIn('-overhead-internal');
+			wfProfileOut('-overhead-internal');
 		}
-		wfProfileOut( '-overhead-total' );
-		
+		wfProfileOut('-overhead-total');
+
 		# First, subtract the overhead!
-		foreach( $this->mStack as $entry ) {
+		foreach ($this->mStack as $entry) {
 			$fname = $entry[0];
 			$thislevel = $entry[1];
-			$start = explode( ' ', $entry[2]);
-			$start = (float)$start[0] + (float)$start[1];
-			$end = explode( ' ', $entry[4]);
-			$end = (float)$end[0] + (float)$end[1];
+			$start = explode(' ', $entry[2]);
+			$start = (float) $start[0] + (float) $start[1];
+			$end = explode(' ', $entry[4]);
+			$end = (float) $end[0] + (float) $end[1];
 			$elapsed = $end - $start;
 			$memory = $entry[5] - $entry[3];
-			
-			if( $fname == '-overhead-total' ) {
+
+			if ($fname == '-overhead-total') {
 				$overheadTotal[] = $elapsed;
 				$overheadMemory[] = $memory;
-			} elseif( $fname == '-overhead-internal' ) {
+			}
+			elseif ($fname == '-overhead-internal') {
 				$overheadInternal[] = $elapsed;
 			}
 		}
-		$overheadTotal = array_sum( $overheadTotal ) / count( $overheadInternal );
-		$overheadMemory = array_sum( $overheadMemory ) / count( $overheadInternal );
-		$overheadInternal = array_sum( $overheadInternal ) / count( $overheadInternal );
-		
+		$overheadTotal = array_sum($overheadTotal) / count($overheadInternal);
+		$overheadMemory = array_sum($overheadMemory) / count($overheadInternal);
+		$overheadInternal = array_sum($overheadInternal) / count($overheadInternal);
+
 		# Collate
-		foreach ( $this->mStack as $index => $entry ) {
+		foreach ($this->mStack as $index => $entry) {
 			$fname = $entry[0];
 			$thislevel = $entry[1];
-			$start = explode( ' ', $entry[2]);
-			$start = (float)$start[0] + (float)$start[1];
-			$end = explode( ' ', $entry[4]);
-			$end = (float)$end[0] + (float)$end[1];
+			$start = explode(' ', $entry[2]);
+			$start = (float) $start[0] + (float) $start[1];
+			$end = explode(' ', $entry[4]);
+			$end = (float) $end[0] + (float) $end[1];
 			$elapsed = $end - $start;
-			
+
 			$memory = $entry[5] - $entry[3];
-			$subcalls = $this->calltreeCount( $this->mStack, $index );
-			
-			if( !preg_match( '/^-overhead/', $fname ) ) {
+			$subcalls = $this->calltreeCount($this->mStack, $index);
+
+			if (!preg_match('/^-overhead/', $fname)) {
 				# Adjust for profiling overhead
 				$elapsed -= $overheadInternal;
 				$elapsed -= ($subcalls * $overheadTotal);
 				$memory -= ($subcalls * $overheadMemory);
 			}
-			
-			if ( !array_key_exists( $fname, $this->mCollated ) ) {
+
+			if (!array_key_exists($fname, $this->mCollated)) {
 				$this->mCollated[$fname] = 0;
 				$this->mCalls[$fname] = 0;
 				$this->mMemory[$fname] = 0;
@@ -247,31 +242,27 @@ class Profiler
 			}
 
 			$this->mCollated[$fname] += $elapsed;
-			$this->mCalls[$fname] ++;
+			$this->mCalls[$fname]++;
 			$this->mMemory[$fname] += $memory;
-			$this->mMin[$fname] = min( $this->mMin[$fname], $elapsed );
-			$this->mMax[$fname] = max( $this->mMax[$fname], $elapsed );
+			$this->mMin[$fname] = min($this->mMin[$fname], $elapsed);
+			$this->mMax[$fname] = max($this->mMax[$fname], $elapsed);
 			$this->mOverhead[$fname] += $subcalls;
 		}
 
-		$total = @$this->mCollated['-total'];
+		$total = @ $this->mCollated['-total'];
 		$this->mCalls['-overhead-total'] = $profileCount;
 
 		# Output
-		asort( $this->mCollated, SORT_NUMERIC );
-		foreach ( $this->mCollated as $fname => $elapsed ) {
+		asort($this->mCollated, SORT_NUMERIC);
+		foreach ($this->mCollated as $fname => $elapsed) {
 			$calls = $this->mCalls[$fname];
 			$percent = $total ? 100. * $elapsed / $total : 0;
 			$memory = $this->mMemory[$fname];
-			$prof .= sprintf( $format, $fname, $calls, (float)($elapsed * 1000), 
-					(float)($elapsed * 1000) / $calls, $percent, $memory,
-					($this->mMin[$fname] * 1000.0),
-					($this->mMax[$fname] * 1000.0),
-					$this->mOverhead[$fname] );
+			$prof .= sprintf($format, $fname, $calls, (float) ($elapsed * 1000), (float) ($elapsed * 1000) / $calls, $percent, $memory, ($this->mMin[$fname] * 1000.0), ($this->mMax[$fname] * 1000.0), $this->mOverhead[$fname]);
 
 			global $wgProfileToDatabase;
-			if( $wgProfileToDatabase ) {
-				Profiler::logToDB( $fname, (float)($elapsed * 1000), $calls );
+			if ($wgProfileToDatabase) {
+				Profiler :: logToDB($fname, (float) ($elapsed * 1000), $calls);
 			}
 		}
 		$prof .= "\nTotal: $total\n\n";
@@ -288,11 +279,11 @@ class Profiler
 	 * @return int
 	 * @access private
 	 */
-	function calltreeCount( &$stack, $start ) {
+	function calltreeCount(& $stack, $start) {
 		$level = $stack[$start][1];
 		$count = 0;
-		for( $i = $start - 1; $i >= 0 && $stack[$i][1] > $level; $i-- ) {
-			$count++;
+		for ($i = $start -1; $i >= 0 && $stack[$i][1] > $level; $i --) {
+			$count ++;
 		}
 		return $count;
 	}
@@ -302,24 +293,17 @@ class Profiler
 	 */
 	function logToDB($name, $timeSum, $eventCount) {
 		$fname = 'Profiler::logToDB';
-		$dbw =& wfGetDB( DB_MASTER );
-		$profiling = $dbw->tableName( 'profiling' );
+		$dbw = & wfGetDB(DB_MASTER);
+		$profiling = $dbw->tableName('profiling');
 
-		$name = substr($name,0,255);
+		$name = substr($name, 0, 255);
 		$encname = $dbw->strencode($name);
-		$sql = "UPDATE $profiling ".
-			"SET pf_count=pf_count+{$eventCount}, ".
-			"pf_time=pf_time + {$timeSum} ".
-			"WHERE pf_name='{$encname}'";
+		$sql = "UPDATE $profiling "."SET pf_count=pf_count+{$eventCount}, "."pf_time=pf_time + {$timeSum} "."WHERE pf_name='{$encname}'";
 		$dbw->query($sql);
 
-		$rc = $dbw->affectedRows();	
-		if( $rc == 0) {
-			$dbw->insert('profiling',array(
-				'pf_name'=>$name,
-				'pf_count'=>$eventCount,
-				'pf_time'=>$timeSum),
-				$fname,array('IGNORE'));
+		$rc = $dbw->affectedRows();
+		if ($rc == 0) {
+			$dbw->insert('profiling', array ('pf_name' => $name, 'pf_count' => $eventCount, 'pf_time' => $timeSum), $fname, array ('IGNORE'));
 		}
 		// When we upgrade to mysql 4.1, the insert+update
 		// can be merged into just a insert with this construct added:
@@ -330,7 +314,6 @@ class Profiler
 
 }
 
-
 $wgProfiler = new Profiler();
-$wgProfiler->profileIn( '-total' );
+$wgProfiler->profileIn('-total');
 ?>
