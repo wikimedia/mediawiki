cf842318c9ec94a5ea3141e9638cf38974bf294f
Jens Frank <jeluf@users.mediawiki.org>
2005-03-01 21:44:37 +0000

    BUG#1610 Add Old Norse stub

---
+++ languages/LanguageNon.php
@@ -0,0 +1,11 @@
+<?php
+# Old Norse stub localization; default to Icelandic instead of English.
+# See language.doc
+
+require_once( 'LanguageIs.php' );
+
+class LanguageNon extends LanguageIs {
+	# Inherit everything
+
+}
+?>
