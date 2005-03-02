c0e3e3329dd3d927d73a192de46712eec08c93b5
Antoine Musso <hashar@users.mediawiki.org>
2005-03-02 16:29:36 +0000

    Fix bug #1618, patch by Mark Bergsma. Stop using hardcoded value for maximum number of namespaces.

--- includes/SpecialAllpages.php
+++ includes/SpecialAllpages.php
@@ -43,11 +43,12 @@ function namespaceForm ( $namespace = NS_MAIN, $from = '' ) {
 
 	$namespaceselect = '<select name="namespace">';
 	$arr = $wgContLang->getNamespaces();
-	for ( $i = 0; $i <= 17; $i++ ) {
-		$namespacename = str_replace ( '_', ' ', $arr[$i] );
-		$n = ($i == 0) ? wfMsg ( 'articlenamespace' ) : $namespacename;
-		$sel = ($i == $namespace) ? ' selected="selected"' : '';
-		$namespaceselect .= "<option value='{$i}'{$sel}>{$n}</option>";
+	foreach ( $arr as $ns => $name ) {
+		if ( $ns < NS_MAIN ) continue;
+		$namespacename = str_replace ( '_', ' ', $name);
+		$n = ($ns == 0) ? wfMsg ( 'articlenamespace' ) : $namespacename;
+		$sel = ($ns == $namespace) ? ' selected="selected"' : '';
+		$namespaceselect .= "<option value='{$ns}'{$sel}>{$n}</option>";		
 	}
 	$namespaceselect .= '</select>';
 
