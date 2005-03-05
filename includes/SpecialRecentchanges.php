7ccabb3ea4abb3e82e201914b5a3b5b96a9a8c09
Brion Vibber <brion@users.mediawiki.org>
2005-03-05 13:59:06 +0000

    * Run UTF-8 validation on old text in Recentchanges RSS diffs

--- includes/SpecialRecentchanges.php
+++ includes/SpecialRecentchanges.php
@@ -348,6 +348,13 @@ function rcFormatDiff( $row ) {
 				array( 'old_id' => $row->rc_last_oldid ) );
 			$oldtext = Article::getRevisionText( $oldrow );
 			
+			global $wgUseLatin1;
+			if( !$wgUseLatin1 ) {
+				# Old entries may contain illegal characters
+				# which will damage output
+				$oldtext = UtfNormal::cleanUp( $oldtext );
+			}
+			
 			global $wgFeedDiffCutoff;
 			if( strlen( $newtext ) > $wgFeedDiffCutoff ||
 			    strlen( $oldtext ) > $wgFeedDiffCutoff ) {
