cc4b6351fb00a5aef9b0ea14551083ae2bb7969e
Magnus Manske <magnusmanske@users.mediawiki.org>
2005-03-18 10:51:17 +0000

    Re-establishing validation feature (the beginnings)

--- maintenance/archives/patch-validate.sql
+++ maintenance/archives/patch-validate.sql
@@ -1,12 +1,12 @@
 -- For article validation
 
-CREATE TABLE /*$wgDBprefix*/validate (
+DROP TABLE IF EXISTS `validate`;
+CREATE TABLE `validate` (
   `val_user` int(11) NOT NULL default '0',
-  `val_title` varchar(255) binary NOT NULL default '',
-  `val_timestamp` varchar(14) binary NOT NULL default '',
-  `val_type` int(10) unsigned NOT NULL default '0',
+  `val_page` int(11) unsigned NOT NULL default '0',
+  `val_revision` int(11) unsigned NOT NULL default '0',
+  `val_type` int(11) unsigned NOT NULL default '0',
   `val_value` int(11) default '0',
   `val_comment` varchar(255) NOT NULL default '',
-  KEY `val_user` (`val_user`,`val_title`,`val_timestamp`)
+  KEY `val_user` (`val_user`,`val_revision`)
 ) TYPE=MyISAM;
-
