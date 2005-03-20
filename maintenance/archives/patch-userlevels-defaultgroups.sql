b96e5ce26645ade3308c302cbf351595c9caf251
Tim Starling <tstarling@users.mediawiki.org>
2005-03-20 06:10:55 +0000

    Changed default groups to be more like Wikipedia

--- maintenance/archives/patch-userlevels-defaultgroups.sql
+++ maintenance/archives/patch-userlevels-defaultgroups.sql
@@ -4,10 +4,27 @@
 --
 
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
-	VALUES (1,'Anonymous','Anonymous users','read,edit,createaccount');
+	VALUES (
+		1,'Anonymous','Anonymous users',
+		'read,edit,createaccount'
+	);
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
-	VALUES (2,'Loggedin','General logged in users','read,edit,move,upload');
+	VALUES (
+		2,'Logged in','General logged in users',
+		'read,edit,move,upload,validate,createaccount'
+	);
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
-	VALUES (3,'Sysops','Operators of this site','read,edit,move,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,sysop');
+	VALUES (
+		3,'Administrator','Trusted users able to block users and delete articles',
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop'
+	);
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
-	VALUES (4,'Bureaucrat','The bureaucrat group is able to make sysops for example. They have no other rights.','read,edit,move,delete,undelete,protect,block,userrights,grouprights,createaccount,upload,asksql,rollback,patrol,editinterface,siteadmin,sysop');
+	VALUES (
+		4,'Bureaucrat','The bureaucrat group is able to make sysops. They have no other rights.',
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop,makesysop'
+	);
+INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
+	VALUES (
+		5,'Steward','Full access',
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop,makesysop,userrights,grouprights,siteadmin'
+	);
