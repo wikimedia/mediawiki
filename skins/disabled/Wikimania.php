d8453a6752664e6b17fddb263cb3f11b6a1d81f9
Jens Frank <jeluf@users.mediawiki.org>
2005-03-23 23:26:03 +0000

    Wikimania minimal skin

---
+++ skins/disabled/Wikimania.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * MonoBook nouveau
+ *
+ * Translated from gwicke's previous TAL template version to remove
+ * dependency on PHPTAL.
+ *
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+
+if( !defined( 'MEDIAWIKI' ) )
+	die();
+
+/** */
+require_once('includes/SkinTemplate.php');
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Skins
+ */
+class SkinWikimania extends SkinTemplate {
+	/** Using monobook. */
+	function initPage( &$out ) {
+		SkinTemplate::initPage( $out );
+		$this->skinname  = 'wikimania';
+		$this->stylename = 'monobook';
+		$this->template  = 'WikimaniaTemplate';
+	}
+}
+	
+class WikimaniaTemplate extends QuickTemplate {
+	/**
+	 * Template filter callback for MonoBook skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
+  <head>
+    <meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
+    <?php $this->html('headlinks') ?>
+    <title><?php $this->text('pagetitle') ?></title>
+    <style type="text/css" media="screen,projection">/*<![CDATA[*/ @import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css"; /*]]>*/</style>
+    <link rel="stylesheet" type="text/css" media="print" href="<?php $this->text('stylepath') ?>/common/commonPrint.css" />
+    <!--[if lt IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE50Fixes.css";</style><![endif]-->
+    <!--[if IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE55Fixes.css";</style><![endif]-->
+    <!--[if IE 6]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE60Fixes.css";</style><![endif]-->
+    <!--[if IE]><script type="text/javascript" src="<?php $this->text('stylepath') ?>/common/IEFixes.js"></script>
+    <meta http-equiv="imagetoolbar" content="no" /><![endif]-->
+    <?php if($this->data['jsvarurl'  ]) { ?><script type="text/javascript" src="<?php $this->text('jsvarurl'  ) ?>"></script><?php } ?>
+    <script type="text/javascript" src="<?php                                   $this->text('stylepath' ) ?>/common/wikibits.js"></script>
+    <?php if($this->data['usercss'   ]) { ?><style type="text/css"><?php              $this->html('usercss'   ) ?></style><?php    } ?>
+    <?php if($this->data['userjs'    ]) { ?><script type="text/javascript" src="<?php $this->text('userjs'    ) ?>"></script><?php } ?>
+    <?php if($this->data['userjsprev']) { ?><script type="text/javascript"><?php      $this->html('userjsprev') ?></script><?php   } ?>
+  </head>
+  <body <?php if($this->data['body_ondblclick']) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
+        <?php if($this->data['nsclass'        ]) { ?>class="<?php      $this->text('nsclass')         ?>"<?php } ?>>
+    <div id="globalWrapper">
+      <div id="column-content">
+	<div id="content">
+	  <a name="top" id="contentTop"></a>
+	  <?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+	  <?php if ( $this->data['title'] != wfMsg( 'mainpage' ) ) {
+	     print '<h1 class="firstHeading">';
+	     $this->text('title');
+	     print '</h1>';
+	    }
+	  ?>
+	  <div id="bodyContent">
+	    <h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+	    <div id="contentSub"><?php $this->html('subtitle') ?></div>
+	    <?php if($this->data['undelete']) { ?><div id="contentSub"><?php     $this->html('undelete') ?></div><?php } ?>
+	    <?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+	    <!-- start content -->
+	    <?php $this->html('bodytext') ?>
+	    <?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
+	    <!-- end content -->
+	    <div class="visualClear"></div>
+	  </div>
+	</div>
+      </div>
+      <div id="column-one">
+	<div class="portlet" id="p-personal">
+	  <h5><?php $this->msg('personaltools') ?></h5>
+	  <div class="pBody">
+	    <ul>
+	    <?php foreach($this->data['personal_urls'] as $key => $item) {
+	       ?><li id="pt-<?php echo htmlspecialchars($key) ?>"><a href="<?php
+	       echo htmlspecialchars($item['href']) ?>"<?php
+	       if(!empty($item['class'])) { ?> class="<?php
+	       echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+	       echo htmlspecialchars($item['text']) ?></a></li><?php
+	    } ?>
+	    </ul>
+	  </div>
+	</div>
+	<div class="portlet" id="p-logo">
+	  <a style="background-image: url(<?php $this->text('logopath') ?>);"
+	    href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"
+	    title="<?php $this->msg('mainpage') ?>"></a>
+	</div>
+	<script type="text/javascript"> if (window.isMSIE55) fixalpha(); </script>
+	<div class="portlet" id="p-nav">
+	  <h5><?php $this->msg('navigation') ?></h5>
+	  <div class="pBody">
+	    <ul>
+		<?php foreach ( array( 'Main Page') as $navlink ) {
+				echo '<li><a href="'.$navlink.'">'.$navlink.'</a></li>';
+			}
+		?>
+	    </ul>
+	  </div>
+	</div>
+	<div class="portlet" id="p-nav">
+	  <h5>Conference</h5>
+	  <div class="pBody">
+	    <ul>
+		<?php foreach ( array( 'Register', 'Call for Papers', 'Programme', 'Speakers',
+			'Social events', 'Competitions', 'Hacking days') as $navlink ) {
+				echo '<li><a href="'.$navlink.'">'.$navlink.'</a></li>';
+			}
+		?>
+	    </ul>
+	  </div>
+	</div>
+	<div class="portlet" id="p-nav">
+	  <h5>Venue</h5>
+	  <div class="pBody">
+	    <ul>
+		<?php foreach ( array( 'Location', 'Accomodation', 'Transport' ) as $navlink ) {
+				echo '<li><a href="'.$navlink.'">'.$navlink.'</a></li>';
+			}
+		?>
+	    </ul>
+	  </div>
+	</div>
+	<div class="portlet" id="p-nav">
+	  <h5>About Wikimania</h5>
+	  <div class="pBody">
+	    <ul>
+		<?php foreach ( array( 'Press', 'Sponsors', 'Contact' ) as $navlink ) {
+				echo '<li><a href="'.$navlink.'">'.$navlink.'</a></li>';
+			}
+		?>
+	    </ul>
+	  </div>
+	</div>
+	<?php if( $this->data['language_urls'] ) { ?><div id="p-lang" class="portlet">
+	  <h5><?php $this->msg('otherlanguages') ?></h5>
+	  <div class="pBody">
+	    <ul>
+	      <?php foreach($this->data['language_urls'] as $langlink) { ?>
+	      <li>
+	      <a href="<?php echo htmlspecialchars($langlink['href'])
+	        ?>"><?php echo $langlink['text'] ?></a>
+	      </li>
+	      <?php } ?>
+	    </ul>
+	  </div>
+	</div>
+	<?php } ?> 
+      </div><!-- end of the left (by default at least) column -->
+      <div class="visualClear"></div>
+      <div id="footer">
+    <?php if($this->data['poweredbyico']) { ?><div id="f-poweredbyico"><?php $this->html('poweredbyico') ?></div><?php } ?>
+	<?php if($this->data['copyrightico']) { ?><div id="f-copyrightico"><?php $this->html('copyrightico') ?></div><?php } ?>
+	<ul id="f-list">
+	  <?php if($this->data['credits'   ]) { ?><li id="f-credits"><?php    $this->html('credits')    ?></li><?php } ?>
+	  <?php if($this->data['copyright' ]) { ?><li id="f-copyright"><?php  $this->html('copyright')  ?></li><?php } ?>
+	</ul>
+      </div>
+    </div>
+    <?php $this->html('reporttime') ?>
+  </body>
+</html>
+<?php
+	}
+}
+
+?>
