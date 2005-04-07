78e2c6792bab3029229f9141e464433567997bc4
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-07 16:57:21 +0000

    * New file!; only has support for the local number format for now.

---
+++ languages/LanguageOr.php
@@ -0,0 +1,34 @@
+<?php
+/** Oriya (ଓଡ଼ିଆ)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  */
+
+require_once( 'LanguageUtf8.php' );
+
+class LanguageOr extends LanguageUtf8 {
+	var $digitTransTable = array(
+		'0' => '୦',
+		'1' => '୧',
+		'2' => '୨',
+		'3' => '୩',
+		'4' => '୪',
+		'5' => '୫',
+		'6' => '୬',
+		'7' => '୭',
+		'8' => '୮',
+		'9' => '୯',
+	);
+
+	function formatNum( $number ) {
+		global $wgTranslateNumerals;
+		if( $wgTranslateNumerals ) {
+			return strtr( $number, $this->digitTransTable );
+		} else {
+			return $number;
+		}
+	}
+}
+
+?>
