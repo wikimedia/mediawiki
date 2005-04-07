3b582b804932eda6b348f4c69aa2286116b4a671
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-07 16:55:24 +0000

    * Added support for Malayalam numbers

--- languages/LanguageMl.php
+++ languages/LanguageMl.php
@@ -1,14 +1,34 @@
 <?php
-/**
+/** Malayalam (മലയാളം)
+  *
   * @package MediaWiki
   * @subpackage Language
   */
-# See language.doc
 
-require_once( "LanguageUtf8.php" );
+require_once( 'LanguageUtf8.php' );
 
 class LanguageMl extends LanguageUtf8 {
-	# Inherit everything
+	var $digitTransTable = array(
+		'0' => '൦',
+		'1' => '൧',
+		'2' => '൨',
+		'3' => '൩',
+		'4' => '൪',
+		'5' => '൫',
+		'6' => '൬',
+		'7' => '൭',
+		'8' => '൮',
+		'9' => '൯'
+	);
 
+	function formatNum( $number ) {
+		global $wgTranslateNumerals;
+		if( $wgTranslateNumerals ) {
+			return strtr( $number, $this->digitTransTable );
+		} else {
+			return $number;
+		}
+	}
 }
+
 ?>
