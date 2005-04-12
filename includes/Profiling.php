5e510b9bc10fe1350526d91d76ccf1b858e611e5
Tim Starling <tstarling@users.mediawiki.org>
2005-04-12 03:27:20 +0000

    Format tweak

--- includes/Profiling.php
+++ includes/Profiling.php
@@ -179,8 +179,8 @@ class Profiler
 	function getFunctionReport() {		
 		$width = 140;
 		$nameWidth = $width - 65;
-		$format =      "%-{$nameWidth}s %6d %9.3f %9.3f %9.3f%% %6d (%9.3f -%9.3f) [%d]\n";
-		$titleFormat = "%-{$nameWidth}s %6s %13s %13s %13s %6s %14s  %14s %9s\n";
+		$format =      "%-{$nameWidth}s %6d %13.3f %13.3f %13.3f%% %9d  (%13.3f -%13.3f) [%d]\n";
+		$titleFormat = "%-{$nameWidth}s %6s %13s %13s %13s %9s %14s   %14s %9s\n";
 		$prof = "\nProfiling data\n";
 		$prof .= sprintf( $titleFormat, 'Name', 'Calls', 'Total', 'Each', '%', 
 			'Mem', 'Min', 'Max', 'Overhead' );
