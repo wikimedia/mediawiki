1ae96e604f27fed8039b3d525cf4e212e01c675f
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-13 08:28:24 +0000

    * Documented

--- includes/SpecialMytalk.php
+++ includes/SpecialMytalk.php
@@ -5,7 +5,10 @@
  * @subpackage SpecialPage
  */
 
-/** @todo document */
+/**
+ * Redirects a user to hir talk page, used by buildPersonalUrls() in
+ * SkinTemplate.php.
+ */
 function wfSpecialMytalk() {
 	global $wgUser, $wgOut;
 	$t = Title::makeTitle( NS_USER_TALK, $wgUser->getName() );
