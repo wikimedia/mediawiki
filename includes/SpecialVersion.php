87a4827ffa269321889e7edb5dc1ae021776c056
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-13 08:30:46 +0000

    * Converted to wikitext (don't we have some syntax for $wgScriptPath?)

--- includes/SpecialVersion.php
+++ includes/SpecialVersion.php
@@ -9,7 +9,7 @@
  * constructor
  */
 function wfSpecialVersion() {
-	global $wgUser, $wgOut, $wgVersion;
+	global $wgUser, $wgOut, $wgVersion, $wgScriptPath;
 	$fname = 'wfSpecialVersion';
 
 	$prefix = wfMsg( 'special_version_prefix' );
@@ -17,26 +17,25 @@ function wfSpecialVersion() {
 	if( $prefix != '&nbsp;' ) {
 		$wgOut->addWikiText( $prefix );
 	}
-	$wgOut->addHTML( '
- <p>This wiki is powered by <b><a href="http://www.mediawiki.org/">MediaWiki</a></b>,  
- copyright (C) 2001-2005 by Magnus Manske, Brion Vibber, Lee Daniel Crocker,
- Tim Starling, Erik M&ouml;ller, and others.</p>
+	$wgOut->addWikiText( "
+This wiki is powered by '''[http://www.mediawiki.org/ MediaWiki]''',  
+copyright (C) 2001-2005 Magnus Manske, Brion Vibber, Lee Daniel Crocker,
+Tim Starling, Erik Möller, and others.
  
- <p>MediaWiki is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.</p>
+MediaWiki is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
  
- <p>MediaWiki is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.</p>
- 
- <p>You should have received <a href="../COPYING">a copy of the GNU General Public License</a>
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- or <a href="http://www.gnu.org/copyleft/gpl.html">read it online</a></p>
-	');
+MediaWiki is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received [{{SERVER}}$wgScriptPath/COPYING a copy of the GNU General Public License]
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+or [http://www.gnu.org/copyleft/gpl.html read it online]");
 	if( $postfix != '&nbsp;' ) {
 		$wgOut->addWikiText( $postfix );
 	}
@@ -51,7 +50,7 @@ function wfSpecialVersion() {
 	
 	$out = '';
 	foreach( $versions as $module => $ver ) {
-		$out .= ":$module: $ver\n";
+		$out .= "*$module: $ver\n";
 	}
 	$wgOut->addWikiText( $out );
 }
