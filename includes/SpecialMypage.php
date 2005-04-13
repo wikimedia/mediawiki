dbaf32d0e88e96b42fd8a70a8b7bf7d9f161dce6
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-13 08:28:24 +0000

    * Documented

--- includes/SpecialMypage.php
+++ includes/SpecialMypage.php
@@ -5,7 +5,10 @@
  * @subpackage SpecialPage
  */
 
-/** @todo document */
+/**
+ * Redirects a user to hir personal page, used by buildPersonalUrls() in
+ * SkinTemplate.php.
+ */
 function wfSpecialMypage() {
 	global $wgUser, $wgOut;
 	$t = Title::makeTitle( NS_USER, $wgUser->getName() );
