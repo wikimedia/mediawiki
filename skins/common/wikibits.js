6eb85b88a8b05de44a83ff967ed72cc6ee4337f6
Tim Starling <tstarling@users.mediawiki.org>
2005-04-16 17:17:18 +0000

    Feature: user can specify upload destination name, separately from source name. JavaScript fills in the default destination, a blank destination also uses the default for the convenience of non-JS users. Also fixed a bug caused by the stat cache.

--- skins/common/wikibits.js
+++ skins/common/wikibits.js
@@ -453,3 +453,30 @@ function addRightClickEditHandler( el ) {
 		}
 	}
 }
+
+function fillDestFilename() {
+	if (!document.getElementById) return;
+	var path = document.getElementById('wpUploadFile').value;
+	// Find trailing part
+	var slash = path.lastIndexOf( '/' );
+	var backslash = path.lastIndexOf( '\\' );
+	var fname;
+	if ( slash == -1 && backslash == -1 ) {
+		fname = path;
+	} else if ( slash > backslash ) {
+		fname = path.substring( slash+1, 10000 );
+	} else {
+		fname = path.substring( backslash+1, 10000 );
+	}
+
+	// Capitalise first letter and replace spaces by underscores
+	fname = fname.charAt(0).toUpperCase().concat(fname.substring(1,10000)).replace( / /g, '_' );
+
+	// Output result
+	var destFile = document.getElementById('wpDestFile');
+	if (destFile.value == '' || destFile.value == document.fname ) {
+		document.getElementById('wpDestFile').value = fname;
+	}
+	document.fname = fname;
+}
+	
