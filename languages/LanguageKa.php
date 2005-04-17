b30365fda53c5c3b7f5f3cb4d252c4f787db19f4
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-17 20:50:32 +0000

    * (bug 1910) Localized all the namespaces for Georgian.

--- languages/LanguageKa.php
+++ languages/LanguageKa.php
@@ -1,5 +1,5 @@
 <?php
-/** Kartuli (Georgian) stub
+/** Kartuli (Georgian)
   *
   * @package MediaWiki
   * @subpackage Language
@@ -7,8 +7,25 @@
 
 require_once( 'LanguageUtf8.php' );
 
-$wgNamespaceNamesKa = array(
-	NS_CATEGORY => "კატეგორია",
+/* private */ $wgNamespaceNamesKa = array(
+	NS_MEDIA            => 'მედია',
+	NS_SPECIAL          => 'სპეციალური',
+	NS_MAIN             => '',
+	NS_TALK             => 'განხილვა',
+	NS_USER             => 'მომხმარებელი',
+	NS_USER_TALK        => 'მომხმარებელი_განხილვა',
+	NS_PROJECT          => 'ვიკიპედია',
+	NS_PROJECT_TALK     => 'ვიკიპედია_განხილვა',
+	NS_IMAGE            => 'სურათი',
+	NS_IMAGE_TALK       => 'სურათი_განხილვა',
+	NS_MEDIAWIKI        => 'მედიავიკი',
+	NS_MEDIAWIKI_TALK   => 'მედიავიკი_განხილვა',
+	NS_TEMPLATE         => 'თარგი',
+	NS_TEMPLATE_TALK    => 'თარგი_განხილვა',
+	NS_HELP             => 'დახმარება',
+	NS_HELP_TALK        => 'დახმარება_განხილვა',
+	NS_CATEGORY         => 'კატეგორია',
+	NS_CATEGORY_TALK    => 'კატეგორია_განხილვა'
 ) + $wgNamespaceNamesEn;
 
 class LanguageKa extends LanguageUtf8 {
