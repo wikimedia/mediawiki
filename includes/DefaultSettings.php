9b2430f72c9c8f4e90db8bc6d0452ed503fa40ca
Magnus Manske <magnusmanske@users.mediawiki.org>
2005-04-21 09:23:34 +0000

    Adding patched EXIF data display on image page, based on include file by Vinay Yadav

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -733,6 +733,8 @@ $wgDisableTextSearch = false;
 $wgDisableSearchUpdate = false;
 /** Uploads have to be specially set up to be secure */
 $wgEnableUploads = false;
+/** Show EXIF data, on by default */
+$wgShowEXIF = true ;
 /**
  * Set to true to enable the upload _link_ while local uploads are disabled.
  * Assumes that the special page link will be bounced to another server where
