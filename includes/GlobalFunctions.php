b956849dab0745ef8b0c58981581eec053a444b1
Brion Vibber <brion@users.mediawiki.org>
2005-04-21 06:28:48 +0000

    * (bug 1636) Refs like &#0355; were misinterpreted as octal in some places Backport test case and one-line fix from HEAD

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -213,7 +213,7 @@ function wfUtf8Sequence( $codepoint ) {
 function wfMungeToUtf8( $string ) {
 	global $wgInputEncoding; # This is debatable
 	#$string = iconv($wgInputEncoding, "UTF-8", $string);
-	$string = preg_replace ( '/&#([0-9]+);/e', 'wfUtf8Sequence($1)', $string );
+	$string = preg_replace ( '/&#0*([0-9]+);/e', 'wfUtf8Sequence($1)', $string );
 	$string = preg_replace ( '/&#x([0-9a-f]+);/ie', 'wfUtf8Sequence(0x$1)', $string );
 	# Should also do named entities here
 	return $string;
