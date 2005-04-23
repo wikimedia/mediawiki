98ae5906dd0e26b2d90d6a085d89b8ea074c6538
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-04-23 15:55:21 +0000

    * Removing  $wgDefaultUserOptionsPt, languages shouldn't set this manually,   this particular array wasn't exported though and did nothing but waste a   few bytes

--- languages/LanguagePt.php
+++ languages/LanguagePt.php
@@ -36,16 +36,6 @@
 
 ) + $wgNamespaceNamesEn;
 
-/* private */ $wgDefaultUserOptionsPt = array(
-    "quickbar" => 1, "underline" => 1,
-    "cols" => 80, "rows" => 25, "searchlimit" => 20,
-    "contextlines" => 5, "contextchars" => 50,
-    "skin" => 0, "math" => 1, "rcdays" => 7, "rclimit" => 50,
-    "highlightbroken" => 1, "stubthreshold" => 0,
-    "previewontop" => 1, "editsection"=>1,"editsectiononrightclick"=>0, "showtoc"=>1,
-    "date" => 0
-);
-
 /* private */ $wgQuickbarSettingsPt = array(
     "Nada", "Fixado �  esquerda", "Fixado �  direita", "Flutuando �  Esquerda"
 );
