4f1403613c3818425c325f7ef1cddb01860a1f60
Tim Starling <tstarling@users.mediawiki.org>
2005-04-25 13:43:11 +0000

    Cleared $wgLinkHolders after they are used. This fixes bug 1770 and probably improves performance too.

--- includes/Parser.php
+++ includes/Parser.php
@@ -2920,6 +2920,17 @@ class Parser
 			wfProfileOut( $fname.'-interwiki' );
 		}
 
+		# Clear link holders, no need to fetch these ones again in a subsequent invocation
+		$wgLinkHolders = array(
+			'namespaces' => array(),
+			'dbkeys' => array(),
+			'queries' => array(),
+			'texts' => array(),
+			'titles' => array()
+		);
+		$wgInterwikiLinkHolders = array();
+
+
 		wfProfileOut( $fname );
 		return $colours;
 	}
