d083281dbfa6780a4a7825062d1f3867b020f7db
Brion Vibber <brion@users.mediawiki.org>
2005-05-04 10:51:15 +0000

    Stub file with namespaces from http://csb.wikipedia.org/wiki/User:Tsca

---
+++ languages/LanguageCsb.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+
+/* private */ $wgNamespaceNamesCsb = array(
+	NS_MEDIA            => "Media",
+	NS_SPECIAL          => "Specjalnô",
+	NS_MAIN             => "",
+	NS_TALK             => "Diskùsëjô",
+	NS_USER             => "Brëkòwnik",
+	NS_USER_TALK        => "Diskùsëjô_brëkòwnika",
+	NS_PROJECT          => "Wiki",
+	NS_PROJECT_TALK     => "Diskùsëjô_Wiki",
+	NS_IMAGE            => "Òbrôzk",
+	NS_IMAGE_TALK       => "Diskùsëjô_òbrôzków",
+	NS_MEDIAWIKI        => "MediaWiki",
+	NS_MEDIAWIKI_TALK   => "Diskùsëjô_MediaWiki",
+	NS_TEMPLATE         => "Szablóna",
+	NS_TEMPLATE_TALK    => "Diskùsëjô_Szablónë",
+	NS_HELP             => "Pòmòc",
+	NS_HELP_TALK        => "Diskùsëjô_Pòmòcë",
+	NS_CATEGORY         => "Kategòrëjô",
+	NS_CATEGORY_TALK    => "Diskùsëjô_Kategòrëji"
+);
+
+require_once( 'LanguageUtf8.php' );
+class LanguageCsb extends LanguageUtf8 {
+	function getNamespaces() {
+		global $wgNamespaceNamesCsb;
+		return $wgNamespaceNamesCsb;
+	}
+	
+	function getNsText( $index ) {
+		global $wgNamespaceNamesCsb;
+		return $wgNamespaceNamesCsb[$index];
+	}
+
+	function getNsIndex( $text ) {
+		global $wgNamespaceNamesCsb;
+
+		foreach ( $wgNamespaceNamesCsb as $i => $n ) {
+			if ( 0 == strcasecmp( $n, $text ) ) { return $i; }
+		}
+		return false;
+	}
+
+}
+
+?>
