f59443320278a66a5a59cddebde191de5cf21187
Tim Starling <tstarling@users.mediawiki.org>
2005-05-14 05:38:49 +0000

    Removed sysop right, it was removed from Defines.php long ago, rendering it useless, this is just cleaning up

--- maintenance/archives/patch-userlevels-defaultgroups.sql
+++ maintenance/archives/patch-userlevels-defaultgroups.sql
@@ -16,15 +16,15 @@ INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
 	VALUES (
 		3,':group-admin-name',':group-loggedin-desc',
-		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop'
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import'
 	);
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
 	VALUES (
 		4,':group-bureaucrat-name',':group-bureaucrat-desc',
-		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop,makesysop'
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,makesysop'
 	);
 INSERT INTO /*$wgDBprefix*/`group` (group_id,group_name,group_description,group_rights)
 	VALUES (
 		5,':group-steward-name',':group-steward-desc',
-		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,sysop,makesysop,userrights,grouprights,siteadmin'
+		'read,edit,move,upload,validate,createaccount,delete,undelete,protect,block,upload,asksql,rollback,patrol,editinterface,import,makesysop,userrights,grouprights,siteadmin'
 	);
