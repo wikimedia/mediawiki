ad5ea9da634f6c5185cb6b01bc76821cc3b80722
Tim Starling <tstarling@users.mediawiki.org>
2005-05-16 06:47:24 +0000

    restoring

---
+++ maintenance/dumpMessages.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @todo document
+ * @package MediaWiki
+ * @subpackage Maintenance
+ */
+
+/** */
+require_once( "commandLine.inc" );
+$wgMessageCache->disableTransform();
+$messages = array();
+foreach ( $wgAllMessagesEn as $key => $englishValue )
+{
+	$messages[$key] = wfMsg( $key );
+}
+print "MediaWiki $wgVersion language file\n";
+print serialize( $messages );
+
+?>
