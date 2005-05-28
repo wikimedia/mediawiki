e2bda0bef6f01d26034e75bc4f6d42dd0241e686
Evan Prodromou <evanprodromou@users.mediawiki.org>
2005-05-28 20:57:20 +0000

    Removed commented-out lines and commas that broke patch.

--- maintenance/archives/patch-img_width.sql
+++ maintenance/archives/patch-img_width.sql
@@ -6,15 +6,13 @@
 ALTER TABLE /*$wgDBprefix*/image ADD (
   img_width int(5) NOT NULL default 0,
   img_height int(5) NOT NULL default 0,
-  img_bits int(5) NOT NULL default 0,
-  -- img_type int(5) NOT NULL default -1
+  img_bits int(5) NOT NULL default 0
 );
 
 ALTER TABLE /*$wgDBprefix*/oldimage ADD (
   oi_width int(5) NOT NULL default 0,
   oi_height int(5) NOT NULL default 0,
-  oi_bits int(3) NOT NULL default 0,
-  -- oi_type int(3) NOT NULL default 0
+  oi_bits int(3) NOT NULL default 0
 );
 
 
