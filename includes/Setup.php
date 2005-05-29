5e555383b3502d329e037f9c44911db401b30bb0
Tim Starling <tstarling@users.mediawiki.org>
2005-05-29 06:18:10 +0000

    forward-ported $wgSkipSkin from REL1_4, with enhancements

--- includes/Setup.php
+++ includes/Setup.php
@@ -102,6 +102,10 @@ if (!$wgUseOldExistenceCheck) {
 	$wgEnablePersistentLC = false;
 }
 
+if ( $wgSkipSkin ) {
+	$wgSkipSkins[] = $wgSkipSkin;
+}
+
 wfProfileOut( $fname.'-misc1' );
 wfProfileIn( $fname.'-memcached' );
 
