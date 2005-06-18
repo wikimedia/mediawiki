9206f4ff07ec396c0a4159044834b99505702f8b
Brion Vibber <brion@users.mediawiki.org>
2005-06-18 09:34:25 +0000

    Use Language::truncate()

--- includes/Article.php
+++ includes/Article.php
@@ -1635,14 +1635,14 @@ class Article {
 
 				# comment field=255, let's grep the first 150 to have some user
 				# space left
-				$text=mb_substr($text,0,150);
+				global $wgContLang;
+				$text = $wgContLang->truncate( $text, 150, '...' );
+				
 				# let's strip out newlines and HTML tags
 				$text=preg_replace('/\"/',"'",$text);
 				$text=preg_replace('/\</','&lt;',$text);
 				$text=preg_replace('/\>/','&gt;',$text);
 				$text=preg_replace("/[\n\r]/",'',$text);
-				
-				if( $length > 150 ) { $text .= '...'; } # we've only pasted part of the text
 				
 				if(!$blanked) {
 					if(!$all_same_user) {
