77792e7e89465893426cec82322d978bfb8b1e96
Tim Starling <tstarling@users.mediawiki.org>
2005-06-19 01:30:56 +0000

    nicer formatting

--- maintenance/dumpRev.php
+++ maintenance/dumpRev.php
@@ -8,7 +8,13 @@
 /** */
 require_once( 'commandLine.inc' );
 $dbr =& wfGetDB( DB_SLAVE );
-$row = $dbr->selectRow( 'old', array( 'old_flags', 'old_text' ), array( 'old_id' => 52 ) );
+$row = $dbr->selectRow( 'old', array( 'old_flags', 'old_text' ), array( 'old_id' => $args[0] ) );
 $obj = unserialize( $row->old_text );
-print_r( array_keys( $obj->mItems ) );
+
+if ( get_class( $obj ) == 'concatenatedgziphistoryblob' ) {
+	print_r( array_keys( $obj->mItems ) );
+} else {
+	var_dump( $obj );
+}
+
 ?>
