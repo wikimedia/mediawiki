deb023f725d183b332adfdb55157a5baad1d69de
Tim Starling <tstarling@users.mediawiki.org>
2005-06-19 01:28:50 +0000

    nicer output

--- maintenance/dumpRev.php
+++ maintenance/dumpRev.php
@@ -3,6 +3,10 @@ require_once( 'commandLine.inc' );
 $dbr =& wfGetDB( DB_SLAVE );
 $row = $dbr->selectRow( 'old', array( 'old_flags', 'old_text' ), array( 'old_id' => $args[0] ) );
 $obj = unserialize( $row->old_text );
-print_r( array_keys( $obj->mItems ) );
+if ( get_class( $obj ) == 'concatenatedgziphistoryblob' ) {
+	print_r( array_keys( $obj->mItems ) );
+} else {
+	var_dump( $obj );
+}
 
 ?>
