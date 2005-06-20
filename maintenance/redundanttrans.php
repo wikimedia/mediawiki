5417fcc86daefb84f0b5e42012e2b7ec1adaa720
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-06-20 11:56:24 +0000

    * A new tool to check for redundant translations

---
+++ maintenance/redundanttrans.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Prints out messages that are no longer used.
+ *
+ * @package MediaWiki
+ * @subpackage Maintenance
+ */
+
+require_once('commandLine.inc');
+
+if ( 'en' == $wgLanguageCode ) {
+	print "Current selected language is English. Cannot check translations.\n";
+	exit();
+}
+
+$count = $total = 0;
+$msgarray = 'wgAllMessages' . ucfirst( $wgLanguageCode );
+
+$redundant = array();
+foreach ( $$msgarray as $code => $msg ) {
+	++$total;
+	if ( ! array_key_exists( $code, $wgAllMessagesEn ) ) {
+		print "* $code\n";
+		++$count;
+	}
+}
+
+print "{$count} messages of {$total} are redundant\n";
+?>
