2f3b0771ec9c93d3fa774a81f3c5029922681c95
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-07-07 21:50:53 +0000

    * (bug 2721) New language file for Vietnamese with the Vietnamese number notation

---
+++ languages/LanguageVi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Vietnamese (Tiếng Việt)
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+require_once( 'LanguageUtf8.php' );
+
+class LanguageVi extends LanguageUtf8 {
+	function formatNum( $number, $year = false ) {
+		return $year ? $number : strtr($this->commafy($number), '.,', ',.' );
+	}
+}
