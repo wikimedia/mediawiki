8f258c530903ed8d4915fe1271d06ebf38e0e9b5
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-07-07 14:56:53 +0000

    * Removing backticks in the validate table definition

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -775,14 +775,14 @@ CREATE TABLE /*$wgDBprefix*/objectcache (
 
 -- For article validation
 CREATE TABLE /*$wgDBprefix*/validate (
-  `val_user` int(11) NOT NULL default '0',
-  `val_page` int(11) unsigned NOT NULL default '0',
-  `val_revision` int(11) unsigned NOT NULL default '0',
-  `val_type` int(11) unsigned NOT NULL default '0',
-  `val_value` int(11) default '0',
-  `val_comment` varchar(255) NOT NULL default '',
-  `val_ip` varchar(20) NOT NULL default '',
-  KEY `val_user` (`val_user`,`val_revision`)
+  val_user int(11) NOT NULL default '0',
+  val_page int(11) unsigned NOT NULL default '0',
+  val_revision int(11) unsigned NOT NULL default '0',
+  val_type int(11) unsigned NOT NULL default '0',
+  val_value int(11) default '0',
+  val_comment varchar(255) NOT NULL default '',
+  val_ip varchar(20) NOT NULL default '',
+  KEY val_user (val_user,val_revision)
 ) TYPE=InnoDB;
 
 
