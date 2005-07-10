11290994d2e479aa6b772b7c303b3a45bd6658e6
River Tarnell <kateturner@users.mediawiki.org>
2005-07-10 00:22:30 +0000

    add missing wfmsg

--- includes/PageHistory.php
+++ includes/PageHistory.php
@@ -78,7 +78,7 @@ class PageHistory {
 		 */
 		$id = $this->mTitle->getArticleID();
 		if( $id == 0 ) {
-			$wgOut->addWikiText( 'nohistory' );
+			$wgOut->addWikiText( wfMsg('nohistory') );
 			wfProfileOut( $fname );
 			return;
 		}
