fc4eb0ad568d4d8611b7453de2487de3b39c3c03
Brion Vibber <brion@users.mediawiki.org>
2005-07-16 06:31:46 +0000

    * PHP 4.1.2 compatibility fix: define floatval() equivalent if missing This triggered if the 'days' option in Special:Watchlist was selected

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -79,6 +79,17 @@ if ( !function_exists( 'mb_substr' ) ) {
 	}
 }
 
+if( !function_exists( 'floatval' ) ) {
+	/**
+	 * First defined in PHP 4.2.0
+	 * @param mixed $var;
+	 * @return float
+	 */
+	function floatval( $var ) {
+		return (float)$var;
+	}
+}
+
 /**
  * Where as we got a random seed
  * @var bool $wgTotalViews
