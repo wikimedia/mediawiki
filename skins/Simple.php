9f6c026c362731d6e6353713f384852cf83e12ea
River Tarnell <kateturner@users.mediawiki.org>
2005-07-24 06:43:15 +0000

    add missing image link decoration fixes

--- skins/Simple.php
+++ skins/Simple.php
@@ -25,6 +25,47 @@ class SkinSimple extends SkinTemplate {
 		$this->stylename = 'simple';
 		$this->template  = 'MonoBookTemplate';
 	}
+
+	function reallyDoGetUserStyles() {
+		global $wgUser;
+		$s = '';
+		$s .= "/* highlightbroken: " . $wgUser->getOption('highlightbroken') . " */\n";
+		if (($undopt = $wgUser->getOption("underline")) != 2) {
+			$underline = $undopt ? 'underline' : 'none';
+			$s .= "a { text-decoration: $underline; }\n";
+		}
+		if (!$wgUser->getOption('highlightbroken')) {
+			$s .= "a.new, #quickbar a.new { text-decoration: line-through; }\n";
+		} else {
+			$s .= <<<END
+a.new, #quickbar a.new,
+a.stub, #quickbar a.stub {
+	color: inherit;
+	text-decoration: inherit;
+}
+a.new:after, #quickbar a.new:after {
+	content: "?";
+	color: #CC2200;
+	text-decoration: $underline;
+}
+a.stub:after, #quickbar a.stub:after {
+	content: "!";
+	color: #772233;
+	text-decoration: $underline;
+}
+END;
+		}
+		if ($wgUser->getOption('justify')) {
+			$s .= "#article, #bodyContent { text-align: justify; }\n";
+		}
+		if (!$wgUser->getOption('showtoc')) {
+			$s .= "#toc { display: none; }\n";
+		}
+		if (!$wgUser->getOption('editsection')) {
+			$s .= ".editsection { display: none; }\n";
+		}
+		return $s;
+	}
 }
 
 ?>
