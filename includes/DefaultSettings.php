924f7a32668c2c6000ef263cb31422b3441f86de
Brion Vibber <brion@users.mediawiki.org>
2005-08-24 03:11:29 +0000

    Bump 1.5rc2; rc1 tarball was mistakenly pulled from CVS HEAD

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -28,7 +28,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.5rc1';
+$wgVersion			= '1.5rc2';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
