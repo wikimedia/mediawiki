1eb686e2dda0c812979510b42f952c7e7b83a7ca
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-08-26 14:05:12 +0000

    * No longer passing the name of the language class by reference

--- includes/Setup.php
+++ includes/Setup.php
@@ -153,7 +153,7 @@ wfProfileIn( $fname.'-language1' );
 
 require_once( "$IP/languages/Language.php" );
 
-function setupLangObj(&$langclass) {
+function setupLangObj($langclass) {
 	global $IP;
 
 	if( ! class_exists( $langclass ) ) {
