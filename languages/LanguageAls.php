2d98091cafdf18f3e01a94b42f90accce7b9e98b
Antoine Musso <hashar@users.mediawiki.org>
2005-08-26 17:14:17 +0000

    Fix 2819: Alemannic user interface not selectable. Dummy file :)

---
+++ languages/LanguageAls.php
@@ -0,0 +1,14 @@
+<?php
+/** Alemannic (Alemannisch)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  */
+
+require_once( 'LanguageUtf8.php' );
+
+class LanguageAls extends LanguageUtf8 {
+	#FIXME: inherit everything for now
+}
+
+?>
