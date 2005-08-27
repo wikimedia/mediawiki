82006ba0be97d2bf6daf5fdd02494bee6f6c8160
Tim Starling <tstarling@users.mediawiki.org>
2005-08-27 08:50:44 +0000

    respect bot flag in RecentChange::notifyLog()

--- includes/RecentChange.php
+++ includes/RecentChange.php
@@ -340,7 +340,7 @@ class RecentChange
 			'rc_comment'	=> $comment,
 			'rc_this_oldid'	=> 0,
 			'rc_last_oldid'	=> 0,
-			'rc_bot'	=> 0,
+			'rc_bot'	=> $user->isBot() ? 1 : 0,
 			'rc_moved_to_ns'	=> 0,
 			'rc_moved_to_title'	=> '',
 			'rc_ip'	=> $ip,
