f35b79c587249834fd078482f7bfc56b6cea6b29
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-09-13 17:04:18 +0000

    * Adding $wgUseMemCached = false to defaultsettings, only exited in localsettings

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -499,6 +499,7 @@ $wgLinkCacheMemcached = false; # Not fully tested
  * Memcached-specific settings
  * See docs/memcached.txt
  */
+$wgUseMemCached     = false;
 $wgMemCachedDebug   = false; # Will be set to false in Setup.php, if the server isn't working
 $wgMemCachedServers = array( '127.0.0.1:11000' );
 $wgMemCachedDebug   = false;
