fee61cae809f44ff589485789c5bf7cfb25de411
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-09-18 20:19:16 +0000

    * improvements

--- maintenance/checkUsernames.php
+++ maintenance/checkUsernames.php
@@ -3,10 +3,11 @@ error_reporting(E_ALL ^ E_NOTICE);
 require_once 'commandLine.inc';
 
 class checkUsernames {
-	var $stderr;
+	var $stderr, $log;
 
 	function checkUsernames() {
 		$this->stderr = fopen( 'php://stderr', 'wt' );
+		$this->log = fopen( '/home/wikipedia/logs/checkUsernames.log', 'at' );
 	}
 	function main() {
 		global $wgDBname;
@@ -22,11 +23,13 @@ class checkUsernames {
 
 		fwrite( $this->stderr, "Checking $wgDBname\n" );
 		while ( $row = $dbr->fetchObject( $res ) ) {
-			if ( ! User::isValidUserName( $row->user_name ) )
-				printf( "%s: %6d: %s\n", $wgDBname, $row->user_id, $row->user_name );
+			if ( ! User::isValidUserName( $row->user_name ) ) {
+				$out = sprintf( "%s: %6d: %s\n", $wgDBname, $row->user_id, $row->user_name );
+				fwrite( $this->log, $out );
+			}
 		}
 	}
 }
 
-$cun = new checkUsernames;
+$cun = new checkUsernames();
 $cun->main();
