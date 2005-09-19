cebd6c62b615c9f573d82a39bc1c97b359e09cee
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-09-19 13:09:28 +0000

    * Adding a basic extension for testing extensions in the parser tests

---
+++ maintenance/parserTestsParserHook.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('MEDIAWIKI')) die();
+/**
+ * A basic extension that's used by the parser tests to test whether input and
+ * arguments are passed to extensions properly.
+ *
+ * @package MediaWiki
+ * @subpackage Maintenance
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+$wgHooks['ParserTestParser'][] = 'wfParserSetup';
+
+function wfParserSetup( &$parser ) {
+	$parser->setHook( 'tag', 'wfParserHook' );
+}
+	
+function wfParserHook( $in, $argv ) {
+	if ( count( $argv ) ) 
+		return "<pre>\n" . print_r( $argv, true ) . '</pre>';
+	else
+		return $in;
+}
