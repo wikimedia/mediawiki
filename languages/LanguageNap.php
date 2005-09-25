a59e04cd1114fd5169d55eec87c86720fe5456a3
Antoine Musso <hashar@users.mediawiki.org>
2005-09-25 13:42:52 +0000

    Makes neapolitan language inherit from italian language

---
+++ languages/LanguageNap.php
@@ -0,0 +1,16 @@
+<?php
+/** Neapolitan (Nnapulitano)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  */
+
+/** */
+require_once( 'LanguageIt.php' );
+
+/** @package MediaWiki */
+class LanguageEn extends LanguageIt {
+	# Inherit everything
+}
+
+?>
