7c23ad61f020ce18aa5928ed2f0fdc310d6a3f19
Brion Vibber <brion@users.mediawiki.org>
2005-09-30 22:06:40 +0000

    Reverting hook in getRevisionText() which was added with no explanation of what it's meant to do. Incorrect modification in this location would cause data corruption, so is almost certainly wrong.

--- includes/Revision.php
+++ includes/Revision.php
@@ -471,7 +471,6 @@ class Revision {
 			global $wgInputEncoding, $wgContLang;
 			$text = $wgContLang->iconv( $wgLegacyEncoding, $wgInputEncoding, $text );
 		}
-		wfRunHooks( 'RevisionAfterGetRevisionText', array( &$text ) );
 		wfProfileOut( $fname );
 		return $text;
 	}
