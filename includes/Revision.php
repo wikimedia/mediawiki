7d0bd2326b123e6ba7be68cd469c4cfce5f82b87
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-09-30 15:25:55 +0000

    * (bug 3577) Added a RevisionAfterGetRevisionText hook to getRevisionText()

--- includes/Revision.php
+++ includes/Revision.php
@@ -475,6 +475,7 @@ class Revision {
 			global $wgInputEncoding, $wgContLang;
 			$text = $wgContLang->iconv( $wgLegacyEncoding, $wgInputEncoding, $text );
 		}
+		wfRunHooks( 'RevisionAfterGetRevisionText', array( &$text ) );
 		wfProfileOut( $fname );
 		return $text;
 	}
