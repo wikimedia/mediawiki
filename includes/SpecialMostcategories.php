05030348977f55ec22c457af52550ad702e1c387
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-10-08 21:20:18 +0000

    * Three new special pages:   - A special page to display the articles with the most revisions   - A special page to display the articles with the most categories   - A special page to display the articles with the most images

---
+++ includes/SpecialMostcategories.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/* */
+require_once 'QueryPage.php';
+
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ */
+class MostcategoriesPage extends QueryPage {
+
+	function getName() { return 'Mostcategories'; }
+	function isExpensive() { return true; }
+	function isSyndicated() { return false; }
+
+	function getSQL() {
+		$dbr =& wfGetDB( DB_SLAVE );
+		extract( $dbr->tableNames( 'categorylinks', 'page' ) );
+		return
+			"
+			SELECT
+			 	'Mostcategories' as type,
+				page_namespace as namespace,
+				page_title as title,
+				COUNT(*) as value
+			FROM $categorylinks
+			LEFT JOIN $page ON cl_from = page_id
+			WHERE page_namespace = " . NS_MAIN . "
+			GROUP BY cl_from
+			HAVING COUNT(*) > 1
+			";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgContLang;
+
+		$nt = Title::makeTitle( $result->namespace, $result->title );
+		$text = $wgContLang->convert( $nt->getPrefixedText() );
+		
+		$plink = $skin->makeKnownLink( $nt->getPrefixedText(), $text );
+		
+		$nl = wfMsg( 'ncategories', $result->value );
+		$nlink = $skin->makeKnownLink( $wgContLang->specialPage( 'Categories' ), $nl, 'article=' . $nt->getPrefixedURL() );
+
+		return "{$plink} ({$nlink})";
+	}
+}
+
+/**
+ * constructor
+ */
+function wfSpecialMostcategories() {
+	list( $limit, $offset ) = wfCheckLimits();
+
+	$wpp = new MostcategoriesPage();
+
+	$wpp->doQuery( $offset, $limit );
+}
+
+?>
---
+++ includes/SpecialMostrevisions.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * A special page to show pages in the 
+ *
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/* */
+require_once 'QueryPage.php';
+
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ */
+class MostrevisionsPage extends QueryPage {
+
+	function getName() { return 'Mostrevisions'; }
+	function isExpensive() { return true; }
+	function isSyndicated() { return false; }
+
+	function getSQL() {
+		$dbr =& wfGetDB( DB_SLAVE );
+		extract( $dbr->tableNames( 'revision', 'page' ) );
+		return
+			"
+			SELECT
+				'Mostrevisions' as type,
+				page_namespace as namespace,
+				page_title as title,
+				COUNT(*) as value
+			FROM $revision
+			LEFT JOIN $page ON page_id = rev_page
+			WHERE page_namespace = " . NS_MAIN . "
+			GROUP BY rev_page
+			HAVING COUNT(*) > 1
+			";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgContLang;
+
+		$nt = Title::makeTitle( $result->namespace, $result->title );
+		$text = $wgContLang->convert( $nt->getPrefixedText() );
+		
+		$plink = $skin->makeKnownLink( $nt->getPrefixedText(), $text );
+		
+		$nl = wfMsg( 'nrevisions', $result->value );
+		$nlink = $skin->makeKnownLinkObj( $nt, $nl, 'action=history' );
+
+		return "{$plink} ({$nlink})";
+	}
+}
+
+/**
+ * constructor
+ */
+function wfSpecialMostrevisions() {
+	list( $limit, $offset ) = wfCheckLimits();
+
+	$wpp = new MostrevisionsPage();
+
+	$wpp->doQuery( $offset, $limit );
+}
+
+?>
