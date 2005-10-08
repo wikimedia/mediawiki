d33abb0ac0007df66a6d9ae35d2c3dcfb0fc564d
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-10-08 21:20:18 +0000

    * Three new special pages:   - A special page to display the articles with the most revisions   - A special page to display the articles with the most categories   - A special page to display the articles with the most images

---
+++ includes/SpecialMostimages.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ *
+ * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/* */
+require_once 'QueryPage.php';
+
+/**
+ * @package MediaWiki
+ * @subpackage SpecialPage
+ */
+class MostimagesPage extends QueryPage {
+
+	function getName() { return 'Mostimages'; }
+	function isExpensive() { return true; }
+	function isSyndicated() { return false; }
+
+	function getSQL() {
+		$dbr =& wfGetDB( DB_SLAVE );
+		extract( $dbr->tableNames( 'imagelinks' ) );
+		return
+			"
+			SELECT
+				'Mostimages' as type,
+				" . NS_IMAGE . " as namespace,
+				il_to as title,
+				COUNT(*) as value
+			FROM $imagelinks
+			GROUP BY il_to
+			HAVING COUNT(*) > 1
+			";
+	}
+
+	function formatResult( $skin, $result ) {
+		global $wgContLang;
+
+		$nt = Title::makeTitle( $result->namespace, $result->title );
+		$text = $wgContLang->convert( $nt->getPrefixedText() );
+		
+		$plink = $skin->makeKnownLink( $nt->getPrefixedText(), $text );
+		
+		$nl = wfMsg( 'nlinks', $result->value );
+		$nlink = $skin->makeKnownLink( $nt->getPrefixedText() . '#filelinks', $nl );
+
+		return "{$plink} ({$nlink})";
+	}
+}
+
+/**
+ * Constructor
+ */
+function wfSpecialMostimages() {
+	list( $limit, $offset ) = wfCheckLimits();
+
+	$wpp = new MostimagesPage();
+
+	$wpp->doQuery( $offset, $limit );
+}
+
+?>
