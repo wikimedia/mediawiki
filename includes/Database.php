4d1bc8009e4055b1ba7ef2ab368ae18a6ea17466
Tom Gilder <tomgilder@users.mediawiki.org>
2005-10-10 18:02:50 +0000

    "$wgSitename is offline" is a bit harsh if it's just a one-off glitch, change to "has a problem"

--- includes/Database.php
+++ includes/Database.php
@@ -1711,7 +1711,7 @@ function wfEmergencyAbort( &$conn, $error ) {
 	# I give up, Brion is right. Getting the message cache to work when there is no DB is tricky.
 	# Hard coding strings instead.
 
-	$noconnect = "<h1><img src='$wgLogo' style='float:left;margin-right:1em' alt=''>$wgSitename is currently offline</h1><p><strong>Sorry! This site is experiencing technical difficulties.</strong></p><p><small>(Can't contact the database server: $1)</small></p>";
+	$noconnect = "<h1><img src='$wgLogo' style='float:left;margin-right:1em' alt=''>$wgSitename has a problem</h1><p><strong>Sorry! This site is experiencing technical difficulties.</strong></p><p>Try waiting a few minutes and reloading.</p><p><small>(Can't contact the database server: $1)</small></p>";
 	$mainpage = 'Main Page';
 	$searchdisabled = <<<EOT
 <p style="margin: 1.5em 2em 1em">$wgSitename search is disabled for performance reasons. You can search via Google in the meantime.
