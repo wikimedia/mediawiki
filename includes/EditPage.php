fe8c04b7bb4c79c80ff3d46c5ec65d8369f0fbd5
Brion Vibber <brion@users.mediawiki.org>
2005-10-22 20:27:26 +0000

    * Avoid notice warning on edit with no User-Agent header

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -1131,9 +1131,19 @@ END
 		}
 	}
 
-
+	/**
+	 * Check if the browser is on a blacklist of user-agents known to
+	 * mangle UTF-8 data on form submission. Returns true if Unicode
+	 * should make it through, false if it's known to be a problem.
+	 * @return bool
+	 * @access private
+	 */
 	function checkUnicodeCompliantBrowser() {
 		global $wgBrowserBlackList;
+		if( empty( $_SERVER["HTTP_USER_AGENT"] ) ) {
+			// No User-Agent header sent? Trust it by default...
+			return true;
+		}
 		$currentbrowser = $_SERVER["HTTP_USER_AGENT"];
 		foreach ( $wgBrowserBlackList as $browser ) {
 			if ( preg_match($browser, $currentbrowser) ) {
