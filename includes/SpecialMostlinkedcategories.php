8e37d66fd20f254ebbdcb7b77c30a7d085410e45
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-10-29 03:01:44 +0000

    * Logic error: $row->namespace not NS_USER

--- includes/SpecialMostlinkedcategories.php
+++ includes/SpecialMostlinkedcategories.php
@@ -49,7 +49,7 @@ class MostlinkedCategoriesPage extends QueryPage {
 
 		$batch = new LinkBatch;
 		while ( $row = $db->fetchObject( $res ) )
-			$batch->addObj( Title::makeTitleSafe( NS_USER, $row->title ) );
+			$batch->addObj( Title::makeTitleSafe( $row->namespace, $row->title ) );
 		$batch->execute( $wgLinkCache );
 
 		// Back to start for display
