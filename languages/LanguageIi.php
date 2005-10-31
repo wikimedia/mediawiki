81a2398fc697bd9f1d92fc56f955ce44320a05b8
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-10-31 11:30:23 +0000

    * (bug 3844) Dummy language file that extends another language

---
+++ languages/LanguageIi.php
@@ -0,0 +1,24 @@
+<?php
+/** Yi (ꆇꉙ)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  *
+  * @bug 3844
+  *
+  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+  */
+
+/**
+ *
+ */
+require_once 'LanguageZh.php';
+
+/**
+ *
+ */
+class LanguageIi extends LanguageZh {}
+
+?>
---
+++ languages/LanguageZa.php
@@ -0,0 +1,24 @@
+<?php
+/** Zhuang (壮语)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  *
+  * @bug 3844
+  *
+  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
+  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+  */
+
+/**
+ *
+ */
+require_once 'LanguageZh.php';
+
+/**
+ *
+ */
+class LanguageZa extends LanguageZh {}
+
+?>
