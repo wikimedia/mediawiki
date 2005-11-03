e5c6aee16a9ccbdcb5a7e4aba0614103e83a8b40
Brion Vibber <brion@users.mediawiki.org>
2005-11-03 01:20:27 +0000

    Fix for PHP 4.4.1; bump to 1.3.18 in preparation for maintenance release

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -9,7 +9,7 @@ if( defined( "MEDIAWIKI" ) ) {
 # like $wgScriptPath, you must also localize everything that
 # depends on it.
 
-$wgVersion			= '1.3.17';
+$wgVersion			= '1.3.18';
 
 $wgSitename         = 'MediaWiki'; # Please customize!
 $wgMetaNamespace    = FALSE; # will be same as you set $wgSitename
