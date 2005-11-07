438bd25603012f9d4ee9d0ad48f4c781f6622827
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-11-07 12:39:24 +0000

    * (bug 3810) Translated the namespace names

--- languages/LanguageYi.php
+++ languages/LanguageYi.php
@@ -1,13 +1,40 @@
 <?php
 /** Yiddish (ייִדיש)
   *
+  * @bug 3810
+  *
   * @package MediaWiki
   * @subpackage Language
   */
 
-require_once( 'LanguageUtf8.php' );
+require_once 'LanguageUtf8.php';
+
+$wgNamespaceNamesYi = array(
+	NS_MEDIA => 'מעדיע',
+	NS_SPECIAL => 'באַזונדער',
+	NS_MAIN => '',
+	NS_TALK => 'רעדן',
+	NS_USER => 'באַניצער',
+	NS_USER_TALK => 'באַניצער_רעדן',
+	NS_PROJECT => $wgMetaNamespace,
+	NS_PROJECT_TALK => $wgMetaNamespace . '_רעדן',
+	NS_IMAGE => 'בילד',
+	NS_IMAGE_TALK => 'בילד_רעדן',
+	NS_MEDIAWIKI => 'מעדיעװיקי',
+	NS_MEDIAWIKI_TALK => 'מעדיעװיקי_רעדן',
+	NS_TEMPLATE => 'מוסטער',
+	NS_TEMPLATE_TALK => 'מוסטער_רעדן',
+	NS_HELP => 'הילף',
+	NS_HELP_TALK => 'הילף_רעדן',
+	NS_CATEGORY => 'קאַטעגאָריע',
+	NS_CATEGORY_TALK=> 'קאַטעגאָריע_רעדן'
+);
 
 class LanguageYi extends LanguageUtf8 {
+	function getNamespaces() {
+		global $wgNamespaceNamesYi;
+		return $wgNamespaceNamesYi;
+	}
 
 	function getDefaultUserOptions() {
 		$opt = parent::getDefaultUserOptions();
