d0b4d2cc9ae43f450b525c6addc363b23a33f853
Domas Mituzas <midom@users.mediawiki.org>
2005-11-12 20:43:21 +0000

    categories deserve recentchanges based recentchanges linked queries as well

--- includes/SpecialRecentchangeslinked.php
+++ includes/SpecialRecentchangeslinked.php
@@ -67,21 +67,28 @@ function wfSpecialRecentchangeslinked( $par = NULL ) {
 	// If target is a Category, use categorylinks and invert from and to
 	if( $nt->getNamespace() == NS_CATEGORY ) {
 		$catkey = $dbr->addQuotes( $nt->getDBKey() );
-		$sql =
- "SELECT page_id,page_namespace,page_title,rev_id,rev_user,rev_comment,
-         rev_user_text,rev_timestamp,rev_minor_edit,
-         page_is_new
-    FROM $categorylinks, $revision, $page
-   WHERE rev_timestamp > '{$cutoff}' AND page_touched > '{$cutoff}'
-         {$cmq}
-     AND rev_page=page_id
-     AND cl_from=page_id
-     AND cl_to=$catkey
-GROUP BY page_id,page_namespace,page_title,
-         rev_user,rev_comment,rev_user_text,rev_timestamp,rev_minor_edit,
-         page_is_new
-ORDER BY rev_timestamp DESC
-   LIMIT {$limit}";
+		$sql = "SELECT /* wfSpecialRecentchangeslinked */ 
+        			rc_cur_id page_id,
+        			rc_namespace page_namespace,
+        			rc_title page_title,
+        			rc_this_oldid rev_id,
+        			rc_user rev_user,
+        			rc_comment rev_comment,
+                 rc_user_text rev_user_text,
+        			rc_timestamp rev_timestamp,
+        			rc_minor rev_minor_edit,
+                 rc_new page_is_new
+            FROM $categorylinks, $recentchanges
+           WHERE rc_timestamp > '{$cutoff}'
+             {$cmq}
+             AND cl_from=rc_cur_id
+             AND cl_to=$catkey
+        GROUP BY page_id,page_namespace,page_title,
+                 rev_user,rev_comment,rev_user_text,rev_timestamp,rev_minor_edit,
+                 page_is_new
+        ORDER BY rev_timestamp DESC
+           LIMIT {$limit};
+ ";
 	} else {
 		$sql =
 "SELECT /* wfSpecialRecentchangeslinked */ 
