a9f26877c149be4e421c862301795ee556f17d89
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2005-11-13 02:48:19 +0000

    * @copyright tag

--- includes/Licenses.php
+++ includes/Licenses.php
@@ -6,6 +6,7 @@
  * @subpackage SpecialPage
  *
  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+ * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
