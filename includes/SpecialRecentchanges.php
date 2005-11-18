7f5d509d2d20cefb264a67bc2e85915bca4a29ff
Brion Vibber <brion@users.mediawiki.org>
2005-11-18 19:56:19 +0000

    * (bug 3996) Fix text for new entries in RC RSS/Atom feed

--- includes/SpecialRecentchanges.php
+++ includes/SpecialRecentchanges.php
@@ -523,6 +523,12 @@ function rcFormatDiff( $row ) {
 			}
 			wfProfileOut( "$fname-dodiff" );
 		} else {
+			$rev = Revision::newFromId( $row->rc_this_oldid );
+			if( is_null( $rev ) ) {
+				$newtext = '';
+			} else {
+				$newtext = $rev->getText();
+			}
 			$diffText = '<p><b>' . wfMsg( 'newpage' ) . '</b></p>' .
 				'<div>' . nl2br( htmlspecialchars( $newtext ) ) . '</div>';
 		}
