11665110cfeb66639fc61a5495e63505b3a201dc
Antoine Musso <hashar@users.mediawiki.org>
2005-11-29 20:25:20 +0000

    Fix bug 4048 by adding a new 'privacy' link in footer. Disable it with '-'

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -223,6 +223,7 @@ class MonoBookTemplate extends QuickTemplate {
 	  <?php if($this->data['numberofwatchingusers' ]) { ?><li id="f-numberofwatchingusers"><?php  $this->html('numberofwatchingusers') ?></li><?php } ?>
 	  <?php if($this->data['credits'   ]) { ?><li id="f-credits"><?php    $this->html('credits')    ?></li><?php } ?>
 	  <?php if($this->data['copyright' ]) { ?><li id="f-copyright"><?php  $this->html('copyright')  ?></li><?php } ?>
+	  <?php if($this->data['privacy'   ]) { ?><li id="f-privacy"><?php    $this->html('privacy')    ?></li><?php } ?>
 	  <?php if($this->data['about'     ]) { ?><li id="f-about"><?php      $this->html('about')      ?></li><?php } ?>
 	  <?php if($this->data['disclaimer']) { ?><li id="f-disclaimer"><?php $this->html('disclaimer') ?></li><?php } ?>
 	  <?php if($this->data['tagline']) { ?><li id="f-tagline"><?php echo $this->data['tagline'] ?></li><?php } ?>
