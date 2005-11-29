215077cbe4e804f31f96422374984ec305f6cf42
Brion Vibber <brion@users.mediawiki.org>
2005-11-29 07:47:26 +0000

    * Reduce fractions in display of exif exposure time

--- includes/Exif.php
+++ includes/Exif.php
@@ -998,7 +998,7 @@ class FormatExif {
 			case 'ExposureTime':
 				// Show the pretty fraction as well as decimal version
 				$tags[$tag] = wfMsg( 'exif-exposuretime-format',
-					$val, $this->formatNum( $val ) );
+					$this->formatFraction( $val ), $this->formatNum( $val ) );
 				break;
 			
 			case 'FNumber':
@@ -1052,5 +1052,53 @@ class FormatExif {
 			return $m[2] != 0 ? $m[1] / $m[2] : $num;
 		else
 			return $num;
+	}
+	
+	/**
+	 * Format a rational number, reducing fractions
+	 *
+	 * @access private
+	 *
+	 * @param mixed $num The value to format
+	 * @return mixed A floating point number or whatever we were fed
+	 */
+	function formatFraction( $num ) {
+		if ( preg_match( '/^(\d+)\/(\d+)$/', $num, $m ) ) {
+			$numerator = intval( $m[1] );
+			$denominator = intval( $m[2] );
+			$gcd = $this->gcd( $numerator, $denominator );
+			if( $gcd != 0 ) {
+				// 0 shouldn't happen! ;)
+				return $numerator / $gcd . '/' . $denominator / $gcd;
+			}
+		}
+		return $this->formatNum( $num );
+	}
+
+	/**
+	 * Calculate the greatest common divisor of two integers.
+	 *
+	 * @param int $a
+	 * @param int $b
+	 * @return int
+	 * @access private
+	 */
+	function gcd( $a, $b ) {
+		/*
+			// http://en.wikipedia.org/wiki/Euclidean_algorithm
+			// Recursive form would be:
+			if( $b == 0 )
+				return $a;
+			else
+				return gcd( $b, $a % $b );
+		*/
+		while( $b != 0 ) {
+			$remainder = $a % $b;
+			
+			// tail recursion...
+			$a = $b;
+			$b = $remainder;
+		}
+		return $a;
 	}
 }
