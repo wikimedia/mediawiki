df1b023cb4de00d7aee8774edce517dbf0cb9c82
Antoine Musso <hashar@users.mediawiki.org>
2005-12-15 20:32:15 +0000

    Backport from HEAD: Bug #4267 : switch dv sd ug ks arc languages to RTL

---
+++ languages/LanguageKs.php
@@ -0,0 +1,18 @@
+<?php
+/** Kashmiri language file ( कश्मीरी - (ﻚﺸﻤﻳﺮﻳ) )
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  */
+
+require_once( 'LanguageUtf8.php' );
+
+class LanguageKs extends LanguageUtf8 {
+	#FIXME: inherit almost everything for now
+
+	function isRTL() {
+		return true;
+	}
+}
+
+?>
