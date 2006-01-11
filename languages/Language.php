63d37a2e089bb54798d50c7b54b795171c6fecb6
Brion Vibber <brion@users.mediawiki.org>
2006-01-11 00:09:42 +0000

    Revert namespacebanner feature: * causes fatal error during database error reporting * indirect keys won't show up in allmessages; check this for correctness and safety * no release notes entry for new feature!

--- languages/Language.php
+++ languages/Language.php
@@ -505,7 +505,6 @@ See $1.',
 'restorelink' => '$1 deleted edits',
 'feedlinks' => 'Feed:',
 'sitenotice'	=> '-', # the equivalent to wgSiteNotice
-'namespacebanner'	=> 'Namespacebanner',
 
 # Short words for each namespace, by default used in the 'article' tab in monobook
 'nstab-main' => 'Article',
