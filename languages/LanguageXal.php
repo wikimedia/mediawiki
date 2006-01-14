9e6e7c0feba306f0fa468d15aed09ea93eaacd70
Alexander Sigachov <ajvol@users.mediawiki.org>
2006-01-14 13:11:12 +0000

    New Kalmyk language file

---
+++ languages/LanguageXal.php
@@ -0,0 +1,78 @@
+<?php
+/**
+  * @package MediaWiki
+  * @subpackage Language
+  */
+# Kalmyk stub localization;
+
+require_once( 'LanguageUtf8.php' );
+
+/* private */ $wgNamespaceNamesXal = array(
+	NS_MEDIA            => 'Аһар',
+	NS_SPECIAL          => 'Көдлхнə',
+	NS_MAIN             => '',
+	NS_TALK             => 'Ухалвр',
+	NS_USER             => 'Орлцач',
+	NS_USER_TALK        => 'Орлцачна_тускар_ухалвр',
+	NS_PROJECT          => $wgMetaNamespace,
+	NS_PROJECT_TALK     => $wgMetaNamespace+'_тускар_ухалвр',
+	NS_IMAGE            => 'Зург',
+	NS_IMAGE_TALK       => 'Зургин_тускар_ухалвр',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_тускар_ухалвр',
+	NS_TEMPLATE         => 'Зура',
+	NS_TEMPLATE_TALK    => 'Зуран_тускар_ухалвр',
+	NS_HELP             => 'Цəəлһлһн',
+	NS_HELP_TALK        => 'Цəəлһлһин_тускар_ухалвр',
+	NS_CATEGORY         => 'Янз',
+	NS_CATEGORY_TALK    => 'Янзин_тускар_ухалвр',
+) + $wgNamespaceNamesEn;
+
+
+/* private */ $wgAllMessagesXal = array(
+
+'edit' => 'Чиклх',
+'article' => 'Халх',
+'discussion' => 'Ухалвр',
+'history' => 'Чикллһнə бүрткл',
+'nstab-main' => 'Халх',
+'nstab-user' => 'Орлцач',
+'nstab-template' => 'Зура',
+'nstab-help' => 'Цəəлһлһн',
+'nstab-category' => 'Янз',
+'talkpage' => 'Ухалвр',
+'history_short' => 'Чикллһнə бүрткл',
+'categories1' => 'Янз',
+'category' => 'Янз',
+
+
+'createaccount' => 'Выль вики-авторлэн регистрациез',
+'login' => 'Оруллһн',
+'mycontris' => 'Мини өгүллһдүд',
+'mytalk' => 'Мини күүндлһн бəəрм',
+'preferences' => 'Дурллһн',
+
+);
+
+class LanguageXal extends LanguageUtf8 {
+	function LanguageXal() {
+		global $wgNamespaceNamesXal, $wgMetaNamespace;
+		LanguageUtf8::LanguageUtf8();
+	}
+
+	function getNamespaces() {
+		global $wgNamespaceNamesXal;
+		return $wgNamespaceNamesXal;
+	}
+
+	function getMessage( $key ) {
+		global $wgAllMessagesXal;
+		return isset($wgAllMessagesXal[$key]) ? $wgAllMessagesXal[$key] : parent::getMessage($key);
+	}
+
+	function fallback8bitEncoding() {
+		return "windows-1251";
+	}
+
+}
+?>
