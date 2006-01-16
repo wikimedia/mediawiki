2e3bda7e2ba05930cf9e77919e13bab3b84c6d1a
Antoine Musso <hashar@users.mediawiki.org>
2006-01-16 18:34:11 +0000

    Fix #1122: "older revision" link displayed when viewing first revision of an article

--- includes/Article.php
+++ includes/Article.php
@@ -2231,7 +2231,11 @@ class Article {
 	}
 
 	/**
-	 * @todo document this function
+	 * Generate the navigation links when browsing through an article revisions
+	 * It shows the information as:
+	 *   Revision as of <date>; view current revision
+	 *   <- Previous version | Next Version ->
+	 *
 	 * @access private
 	 * @param string $oldid		Revision ID of this article revision
 	 */
@@ -2244,7 +2248,10 @@ class Article {
 		$lnk = $current
 			? wfMsg( 'currentrevisionlink' )
 			: $lnk = $sk->makeKnownLinkObj( $this->mTitle, wfMsg( 'currentrevisionlink' ) );
-		$prevlink = $sk->makeKnownLinkObj( $this->mTitle, wfMsg( 'previousrevision' ), 'direction=prev&oldid='.$oldid );
+		$prev = $this->mTitle->getPreviousRevisionID( $oldid ) ;
+		$prevlink = $prev
+			? $sk->makeKnownLinkObj( $this->mTitle, wfMsg( 'previousrevision' ), 'direction=prev&oldid='.$oldid )
+			: wfMsg( 'previousrevision' );
 		$nextlink = $current
 			? wfMsg( 'nextrevision' )
 			: $sk->makeKnownLinkObj( $this->mTitle, wfMsg( 'nextrevision' ), 'direction=next&oldid='.$oldid );
