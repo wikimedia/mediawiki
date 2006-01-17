b380b0a76aba653869d9d14933e1c21f88993a17
Brion Vibber <brion@users.mediawiki.org>
2006-01-17 23:43:15 +0000

    * Add ".deps.php" include-file preloaders for some dynamically-loaded   language and skin classes. Should help with the broken base-class   problem under PHP 5 with APC as opcode cache. See details:   http://mail.wikipedia.org/pipermail/wikitech-l/2006-January/033660.html

---
+++ languages/LanguageCv.deps.php
@@ -0,0 +1,8 @@
+<?php
+// This file exists to ensure that base classes are preloaded before
+// LanguageCv.php is compiled, working around a bug in the APC opcode
+// cache on PHP 5, where cached code can break if the include order
+// changed on a subsequent page view.
+
+require_once( "LanguageRu.php" );
+?>
---
+++ skins/MySkin.deps.php
@@ -0,0 +1,9 @@
+<?php
+// This file exists to ensure that base classes are preloaded before
+// MySkin.php is compiled, working around a bug in the APC opcode
+// cache on PHP 5, where cached code can break if the include order
+// changed on a subsequent page view.
+
+require_once('includes/SkinTemplate.php');
+require_once('MonoBook.php');
+?>
