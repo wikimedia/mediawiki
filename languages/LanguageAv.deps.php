5408d18323f567b4d800a1134307a9cf1ba79ced
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2006-01-18 17:01:13 +0000

    * Adding link to the relevant mailing list post in the comment for deps.php stuff

--- languages/LanguageAv.deps.php
+++ languages/LanguageAv.deps.php
@@ -3,6 +3,7 @@
 // LanguageAv.php is compiled, working around a bug in the APC opcode
 // cache on PHP 5, where cached code can break if the include order
 // changed on a subsequent page view.
+// see http://mail.wikipedia.org/pipermail/wikitech-l/2006-January/033660.html
 
 require_once( "LanguageRu.php" );
 ?>
