b017c003932710710ea464eb5f2aea065761aa0b
Rob Church <robchurch@users.mediawiki.org>
2006-01-27 23:51:46 +0000

    Added missing table defs. for transcache (were in updaters, but not install schema)

--- maintenance/mysql5/tables.sql
+++ maintenance/mysql5/tables.sql
@@ -853,3 +853,10 @@ CREATE TABLE /*$wgDBprefix*/trackbacks (
 
 	INDEX (tb_page)
 ) TYPE=InnoDB, DEFAULT CHARSET=utf8;
+
+CREATE TABLE /*$wgDBprefix*/transcache (
+	tc_url		VARCHAR(255) NOT NULL,
+	tc_contents	TEXT,
+	tc_time		INT NOT NULL,
+	UNIQUE INDEX tc_url_idx(tc_url)
+);
--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -840,3 +840,10 @@ CREATE TABLE /*$wgDBprefix*/trackbacks (
 
 	INDEX (tb_page)
 );
+
+CREATE TABLE /*$wgDBprefix*/transcache (
+	tc_url		VARCHAR(255) NOT NULL,
+	tc_contents	TEXT,
+	tc_time		INT NOT NULL,
+	UNIQUE INDEX tc_url_idx(tc_url)
+);
