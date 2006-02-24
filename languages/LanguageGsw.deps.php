7e7fed34265f017c9d0e6008cb18b5c8a554e40f
Brion Vibber <brion@users.mediawiki.org>
2006-02-24 21:15:33 +0000

    * Rename the stub LanguageAls / LanguageGem_alsation to LanguageGsw to follow   updated language code assignments

--- languages/LanguageGsw.deps.php
+++ languages/LanguageGsw.deps.php
@@ -1,6 +1,6 @@
 <?php
 // This file exists to ensure that base classes are preloaded before
-// LanguageGem_alsatian.php is compiled, working around a bug in the APC opcode
+// LanguageGsw.php is compiled, working around a bug in the APC opcode
 // cache on PHP 5, where cached code can break if the include order
 // changed on a subsequent page view.
 // see http://mail.wikipedia.org/pipermail/wikitech-l/2006-January/033660.html
