fa0f62dd76d3e04f11882475ed16ccee05d0bfb2
Tim Starling <tstarling@users.mediawiki.org>
2006-02-24 01:56:31 +0000

    Added job table, for deferred processing of jobs. The immediate application is to complete the link table refresh operation when templates are changed.

--- maintenance/mysql5/tables.sql
+++ maintenance/mysql5/tables.sql
@@ -914,3 +914,23 @@ CREATE TABLE /*$wgDBprefix*/trackbacks (
 
 	INDEX (tb_page)
 ) TYPE=InnoDB, DEFAULT CHARSET=utf8;
+
+-- Jobs performed by parallel apache threads or a command-line daemon
+CREATE TABLE /*$wgDBprefix*/job (
+  job_id int(9) unsigned NOT NULL auto_increment,
+  
+  -- Command name, currently only refreshLinks is defined
+  job_cmd varchar(255) NOT NULL default '',
+
+  -- Namespace and title to act on
+  -- Should be 0 and '' if the command does not operate on a title
+  job_namespace int NOT NULL,
+  job_title varchar(255) binary NOT NULL,
+
+  -- Any other parameters to the command
+  -- Presently unused, format undefined
+  job_params blob NOT NULL default '',
+
+  PRIMARY KEY job_id (job_id),
+  KEY (job_cmd, job_namespace, job_title)
+) TYPE=InnoDB, DEFAULT CHARSET=utf8;
