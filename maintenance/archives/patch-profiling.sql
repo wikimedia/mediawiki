ad1ef67aec6ef957222805340ff22a68ef782d4f
Tim Starling <tstarling@users.mediawiki.org>
2006-02-28 05:45:51 +0000

    fixed accidentally committed live hack

--- maintenance/archives/patch-profiling.sql
+++ maintenance/archives/patch-profiling.sql
@@ -1,11 +1,9 @@
 -- profiling table
 -- This is optional
 
-CREATE TABLE profiling (
-  pf_count int(11) NOT NULL default '0',
-  pf_time float NOT NULL default '0',
-  pf_name varchar(255) NOT NULL default '',
-  pf_server varchar(30) NOT NULL default '',
-  UNIQUE KEY pf_name_server (pf_name,pf_server)
+CREATE TABLE /*$wgDBprefix*/profiling (
+	pf_count integer not null default 0,
+	pf_time float not null default 0,
+	pf_name varchar(255) not null default '',
+	UNIQUE KEY pf_name (pf_name)
 ) TYPE=HEAP;
-
