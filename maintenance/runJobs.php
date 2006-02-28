f2dde49c4404a24b7ac59c5313c8ab53cd3a4ac1
Tim Starling <tstarling@users.mediawiki.org>
2006-02-28 05:20:31 +0000

    Die on inappropriate references to $wgTitle, especially from parser extensions.

--- maintenance/runJobs.php
+++ maintenance/runJobs.php
@@ -2,6 +2,10 @@
 
 require_once( 'commandLine.inc' );
 require_once( "$IP/includes/JobQueue.php" );
+require_once( "$IP/includes/FakeTitle.php" );
+
+// Trigger errors on inappropriate use of $wgTitle
+$wgTitle = new FakeTitle;
 
 while ( false != ($job = Job::pop()) ) {
 	wfWaitForSlaves( 5 );
