631d940e924499bd7438683a7436fe7896864ae9
Jens Frank <jeluf@users.mediawiki.org>
2006-03-24 23:34:29 +0000

    Dutch Lower Saxon, bug 4497

---
+++ languages/LanguageNds_nl.php
@@ -0,0 +1,24 @@
+<?php
+/** Dutch Lower Saxon (Nedersaksisch)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  *
+  * @bug 4497
+  *
+  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>, Jens Frank
+  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason, Jens Frank
+  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+  */
+
+/**
+ *
+ */
+require_once 'LanguageNds.php';
+
+/**
+ *
+ */
+class LanguageNds_nl extends LanguageNds {}
+
+?>
