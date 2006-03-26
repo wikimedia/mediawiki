21b6bdfe5bfe83a438dbb1dcf135110f4338e375
Jens Frank <jeluf@users.mediawiki.org>
2006-03-26 21:27:40 +0000

    bug 5362 Piedmontese

---
+++ languages/LanguagePms.php
@@ -0,0 +1,25 @@
+<?php
+/** Piedmontese
+  * Users are bilingual in Piedmontese and Italian, using Italian as template.
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  *
+  * @bug 5362
+  *
+  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>, Jens Frank
+  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason, Jens Frank
+  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+  */
+
+/**
+ *
+ */
+require_once 'LanguageIt.php';
+
+/**
+ *
+ */
+class LanguagePms extends LanguageIt {}
+
+?>
