710e487676ce77234480c5f8ed7839011fad00f2
Brion Vibber <brion@users.mediawiki.org>
2006-03-26 19:38:24 +0000

    backport fragment html fix hack from head

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -31,7 +31,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.5.7';
+$wgVersion			= '1.5.8';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
