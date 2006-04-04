3533e2d9559292f45c27543b7bda961796b9fbd6
Rob Church <robchurch@users.mediawiki.org>
2006-04-04 15:02:40 +0000

    Missing table prefix in ss_images updater patch

--- maintenance/archives/patch-ss_images.sql
+++ maintenance/archives/patch-ss_images.sql
@@ -2,4 +2,4 @@
 
 ALTER TABLE /*$wgDBprefix*/site_stats ADD ss_images int(10) default '0';
 SELECT @images := COUNT(*) FROM /*$wgDBprefix*/image;
-UPDATE site_stats SET ss_images=@images;
+UPDATE /*$wgDBprefix*/site_stats SET ss_images=@images;
