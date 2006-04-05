8f7bd04b1e6e8804fd3abca0ea25d5def3214601
Brion Vibber <brion@users.mediawiki.org>
2006-04-05 07:43:17 +0000

    Update the FSF's address in all these GPL stub headers

--- config/index.php
+++ config/index.php
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # http://www.gnu.org/copyleft/gpl.html
 
 error_reporting( E_ALL );
@@ -1480,7 +1480,7 @@ function testMemcachedServer( $server ) {
 
 	 <p>You should have received <a href="../COPYING">a copy of the GNU General Public License</a>
 	 along with this program; if not, write to the Free Software
-	 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+	 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 	 or <a href="http://www.gnu.org/copyleft/gpl.html">read it online</a></p>
 	</div>
 
--- includes/SpecialVersion.php
+++ includes/SpecialVersion.php
@@ -68,7 +68,7 @@ class SpecialVersion {
 
 		You should have received [{{SERVER}}{{SCRIPTPATH}}/COPYING a copy of the GNU General Public License]
 		along with this program; if not, write to the Free Software
-		Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 		or [http://www.gnu.org/copyleft/gpl.html read it online]
 
 		* [http://www.mediawiki.org/ MediaWiki]: $wgVersion
