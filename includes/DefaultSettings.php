06ce1d3f3a349a1cb4aa2699b2d307f7d5a876bb
Rob Church <robchurch@users.mediawiki.org>
2006-04-06 15:46:55 +0000

    Correcting version number format so it'll work with version comparison functions

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -32,7 +32,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.7-alpha';
+$wgVersion			= '1.7alpha';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
