db828926229c9cc0f8d3874abd941d9b2326f307
Rob Church <robchurch@users.mediawiki.org>
2006-04-11 21:55:41 +0000

    (bug 5537) Add stub language file for Samogitian (bat-smg); inherits Lithuanian (lt)

---
+++ languages/LanguageBat_smg.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Samogitian stub language file
+ * Inherits Lithuanian
+ *
+ * @package MediaWiki
+ * @subpackage Internationalisation
+ * @author Rob Church <robchur@gmail.com>
+ */
+ 
+require_once( 'LanguageLt.php' );
+class LanguageBat_smg extends LanguageLt { }
+
+?>
