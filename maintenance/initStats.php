1bf79fecc8a0b0a10c85c5c3766e59c3abdda1c4
Rob Church <robchurch@users.mediawiki.org>
2006-05-01 18:08:11 +0000

    Updated initStats maintenance script

--- maintenance/initStats.php
+++ maintenance/initStats.php
@@ -1,26 +1,68 @@
 <?php
 
+/**
+ * Maintenance script to re-initialise or update the site statistics table
+ *
+ * @package MediaWiki
+ * @subpackage Maintenance
+ * @author Brion Vibber
+ * @author Rob Church <robchur@gmail.com>
+ * @licence GNU General Public Licence 2.0 or later
+ */
+ 
+$options = array( 'help', 'update' );
 require_once( 'commandLine.inc' );
-
+echo( "Refresh Site Statistics\n\n" );
 $dbr =& wfGetDB( DB_SLAVE );
+$fname = 'initStats';
 
-$edits = $dbr->selectField( 'revision', 'COUNT(rev_id)', '' );
-$pages = $dbr->selectField( 'page', 'COUNT(page_id)',
-	array(
-		'page_namespace' => 0,
-		'page_is_redirect' => 0,
-		'page_len > 0',
-	)
-); // HACK APPROXIMATION
+if( isset( $options['help'] ) ) {
+	showHelp();
+	exit();
+}
 
-echo "$wgDBname: setting edits $edits, pages $pages\n";
+echo( "Counting total edits..." );
+$edits = $dbr->selectField( 'revision', 'COUNT(*)', '', $fname );
+echo( "{$edits}\nCounting number of articles..." );
+
+$good  = $dbr->selectField( 'page', 'COUNT(*)', array( 'page_namespace' => 0, 'page_is_redirect' => 0, 'page_len > 0' ), $fname );
+echo( "{$good}\nCounting total pages..." );
+
+$pages = $dbr->selectField( 'page', 'COUNT(*)', '', $fname );
+echo( "{$pages}\nCounting number of users..." );
+
+$users = $dbr->selectField( 'user', 'COUNT(*)', '', $fname );
+echo( "{$users}\nCounting number of admins..." );
+
+$admin = $dbr->selectField( 'user_groups', 'COUNT(*)', array( 'ug_group' => 'sysop' ), $fname );
+echo( "{$admin}\nCounting number of images..." );
+
+$image = $dbr->selectField( 'image', 'COUNT(*)', '', $fname );
+echo( "{$image}\n\nUpdating site statistics..." );
 
 $dbw =& wfGetDB( DB_MASTER );
-$dbw->delete( 'site_stats', array( 'ss_row_id' => 1 ) );
-$dbw->insert( 'site_stats',
-	array( 'ss_row_id'=> 1,
-	       'ss_total_views'   => 0,
-	       'ss_total_edits'   => $edits,
-	       'ss_good_articles' => $pages ) );
+$values = array( 'ss_total_edits' => $edits,
+				'ss_good_articles' => $good,
+				'ss_total_pages' => $pages,
+				'ss_users' => $users,
+				'ss_admins' => $admin,
+				'ss_images' => $image );
+$conds = array( 'ss_row_id' => 1 );
+$views = array( 'ss_total_views' => 0 );
+				
+if( isset( $options['update'] ) ) {
+	$dbw->update( 'site_stats', $values, $conds, $fname );
+} else {
+	$dbw->delete( 'site_stats', $conds, $fname );
+	$dbw->insert( 'site_stats', array_merge( $values, $conds, $views ), $fname );
+}
+
+echo( "done.\n\n" );
+
+function showHelp() {
+	echo( "Re-initialise the site statistics tables.\n\n" );
+	echo( "Usage: php initStats.php [--update]\n\n" );
+	echo( "		--update : Update the existing statistics (preserves the ss_total_views field)\n\n" );
+}
 
 ?>
