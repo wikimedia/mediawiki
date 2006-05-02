ee83ca6ecb957a972726f53d57f854d1266fab2b
Rob Church <robchurch@users.mediawiki.org>
2006-05-02 20:12:10 +0000

    Add 'EmailConfirmed' hook

--- includes/User.php
+++ includes/User.php
@@ -259,7 +259,7 @@ class User {
 			(false !== strpos( $addr, '@' ) );
 	}
 
-	/**
+	/**f
 	 * Count the number of edits of a user
 	 *
 	 * @param int $uid The user ID to check
@@ -1821,13 +1821,18 @@ class User {
 	function isEmailConfirmed() {
 		global $wgEmailAuthentication;
 		$this->loadFromDatabase();
-		if( $this->isAnon() )
-			return false;
-		if( !$this->isValidEmailAddr( $this->mEmail ) )
-			return false;
-		if( $wgEmailAuthentication && !$this->getEmailAuthenticationTimestamp() )
-			return false;
-		return true;
+		$confirmed = true;
+		if( wfRunHooks( 'EmailConfirmed', array( &$this, &$confirmed ) ) ) {
+			if( $this->isAnon() )
+				return false;
+			if( !$this->isValidEmailAddr( $this->mEmail ) )
+				return false;
+			if( $wgEmailAuthentication && !$this->getEmailAuthenticationTimestamp() )
+				return false;
+			return true;
+		else {
+			return $confirmed;
+		}
 	}
 
 	/**
