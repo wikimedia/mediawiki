7ac5dc95ee12d3ee114fbc3eb73e9d7be8b048fc
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-05-05 15:33:19 +0000

    * Prettify

--- languages/LanguageNds_nl.php
+++ languages/LanguageNds_nl.php
@@ -17,24 +17,24 @@
 require_once 'LanguageNds.php';
 
 /* private */ $wgNamespaceNamesNds_nl = array(
-NS_MEDIA => 'Media',
-NS_SPECIAL => 'Speciaol',
-NS_MAIN => '',
-NS_TALK => 'Overleg',
-NS_USER => 'Gebruker',
-NS_USER_TALK => 'Overleg_gebruker',
-NS_PROJECT => $wgMetaNamespace,
-NS_PROJECT_TALK => 'Overleg_' . $wgMetaNamespace,
-NS_IMAGE => 'Ofbeelding',
-NS_IMAGE_TALK => 'Overleg_ofbeelding',
-NS_MEDIAWIKI => 'MediaWiki',
-NS_MEDIAWIKI_TALK => 'Overleg_MediaWiki',
-NS_TEMPLATE => 'Sjabloon',
-NS_TEMPLATE_TALK => 'Overleg_sjabloon',
-NS_HELP => 'Help',
-NS_HELP_TALK => 'Overleg_help',
-NS_CATEGORY => 'Categorie',
-NS_CATEGORY_TALK => 'Overleg_categorie'
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Speciaol',
+	NS_MAIN             => '',
+	NS_TALK             => 'Overleg',
+	NS_USER             => 'Gebruker',
+	NS_USER_TALK        => 'Overleg_gebruker',
+	NS_PROJECT          => $wgMetaNamespace,
+	NS_PROJECT_TALK     => 'Overleg_' . $wgMetaNamespace,
+	NS_IMAGE            => 'Ofbeelding',
+	NS_IMAGE_TALK       => 'Overleg_ofbeelding',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Overleg_MediaWiki',
+	NS_TEMPLATE         => 'Sjabloon',
+	NS_TEMPLATE_TALK    => 'Overleg_sjabloon',
+	NS_HELP             => 'Help',
+	NS_HELP_TALK        => 'Overleg_help',
+	NS_CATEGORY         => 'Categorie',
+	NS_CATEGORY_TALK    => 'Overleg_categorie'
 );
 
 /* private */ $wgSkinNamesNds_nl = array(
