5905888e956bcb5d8b127288bade44dd0e6aa6ac
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-05-08 16:58:07 +0000

    * Added formatNum call * Support for {{plural:}} (ref 5805)

--- includes/SpecialMostcategories.php
+++ includes/SpecialMostcategories.php
@@ -40,15 +40,18 @@ class MostcategoriesPage extends QueryPage {
 	}
 
 	function formatResult( $skin, $result ) {
-		global $wgContLang;
+		global $wgContLang, $wgLang;
 
 		$nt = Title::makeTitle( $result->namespace, $result->title );
 		$text = $wgContLang->convert( $nt->getPrefixedText() );
 
 		$plink = $skin->makeKnownLink( $nt->getPrefixedText(), $text );
 
-		$nl = wfMsg( 'ncategories', $result->value );
-		$nlink = $skin->makeKnownLink( $wgContLang->specialPage( 'Categories' ), $nl, 'article=' . $nt->getPrefixedURL() );
+		$nl = wfMsgExt( 'ncategories', array( 'parsemag', 'escape' ),
+			$wgLang->formatNum( $result->value ) );
+
+		$nlink = $skin->makeKnownLink( $wgContLang->specialPage( 'Categories' ),
+			$nl, 'article=' . $nt->getPrefixedURL() );
 
 		return wfSpecialList($plink, $nlink);
 	}
