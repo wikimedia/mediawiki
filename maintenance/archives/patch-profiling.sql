5009897bee0407018e5fa7bb56b6d0c8ff2610f6
Domas Mituzas <midom@users.mediawiki.org>
2006-05-28 17:05:22 +0000

    Added proper pf_server definition to table, enabled to turn on/off per-app-server profiling

--- maintenance/archives/patch-profiling.sql
+++ maintenance/archives/patch-profiling.sql
@@ -5,5 +5,6 @@ CREATE TABLE /*$wgDBprefix*/profiling (
 	pf_count integer not null default 0,
 	pf_time float not null default 0,
 	pf_name varchar(255) not null default '',
-	UNIQUE KEY pf_name (pf_name)
+	pf_server varchar(30) not null default '',
+	UNIQUE KEY pf_name_server (pf_name, pf_server)
 ) TYPE=HEAP;
