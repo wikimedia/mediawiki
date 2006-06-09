61969556a05c060d25612c2508d773cc013d6675
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-09 22:16:37 +0000

    * Restructured

--- languages/LanguageIa.php
+++ languages/LanguageIa.php
@@ -1,71 +1,81 @@
 <?php
-/**
-  * @package MediaWiki
-  * @subpackage Language
-  */
+/** Interlingua (Interlingua)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
 
-/* private */ $wgNamespaceNamesIa = array(
-	NS_MEDIA          => "Media",
-	NS_SPECIAL        => "Special",
-	NS_MAIN           => "",
-	NS_TALK           => "Discussion",
-	NS_USER           => "Usator",
-	NS_USER_TALK      => "Discussion_Usator",
-	NS_PROJECT        => $wgMetaNamespace,
-	NS_PROJECT_TALK   => "Discussion_". $wgMetaNamespace,
-	NS_IMAGE          => "Imagine",
-	NS_IMAGE_TALK     => "Discussion_Imagine",
-	NS_MEDIAWIKI      => "MediaWiki",
-	NS_MEDIAWIKI_TALK => "Discussion_MediaWiki",
-	NS_TEMPLATE       => "Patrono",
-	NS_TEMPLATE_TALK  => "Discussion_Patrono",
-	NS_HELP           => 'Adjuta',
-	NS_HELP_TALK      => 'Discussion_Adjuta',
-	NS_CATEGORY       => 'Categoria',
-	NS_CATEGORY_TALK  => 'Discussion_Categoria'
-) + $wgNamespaceNamesEn;
-
-/* private */ $wgQuickbarSettingsIa = array(
-	"Nulle", "Fixe a sinistra", "Fixe a dextera", "Flottante a sinistra"
-);
-
-/* private */ $wgSkinNamesIa = array(
-	'cologneblue' => "Blau Colonia",
-) + $wgSkinNamesEn;
-
+require_once( 'LanguageUtf8.php' );
 
 if (!$wgCachedMessageArrays) {
 	require_once('MessagesIa.php');
 }
 
-require_once( "LanguageUtf8.php" );
-
 class LanguageIa extends LanguageUtf8 {
+	private $mMessagesIa, $mNamespaceNamesIa = null;
+
+	private $mQuickbarSettingsIa = array(
+		'Nulle', 'Fixe a sinistra', 'Fixe a dextera', 'Flottante a sinistra'
+	);
+	
+	private $mSkinNamesIa = array(
+		'cologneblue' => 'Blau Colonia',
+	);
+
+
+	function LanguageIa() {
+		LanguageUtf8::LanguageUtf8();
+
+		global $wgAllMessagesIa;
+		$this->mMessagesIa =& $wgAllMessagesIa;
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesIa = array(
+			NS_MEDIA          => 'Media',
+			NS_SPECIAL        => 'Special',
+			NS_MAIN           => '',
+			NS_TALK           => 'Discussion',
+			NS_USER           => 'Usator',
+			NS_USER_TALK      => 'Discussion_Usator',
+			NS_PROJECT        => $wgMetaNamespace,
+			NS_PROJECT_TALK   => 'Discussion_'. $wgMetaNamespace,
+			NS_IMAGE          => 'Imagine',
+			NS_IMAGE_TALK     => 'Discussion_Imagine',
+			NS_MEDIAWIKI      => 'MediaWiki',
+			NS_MEDIAWIKI_TALK => 'Discussion_MediaWiki',
+			NS_TEMPLATE       => 'Patrono',
+			NS_TEMPLATE_TALK  => 'Discussion_Patrono',
+			NS_HELP           => 'Adjuta',
+			NS_HELP_TALK      => 'Discussion_Adjuta',
+			NS_CATEGORY       => 'Categoria',
+			NS_CATEGORY_TALK  => 'Discussion_Categoria'
+		);
+
+	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesIa;
-		return $wgNamespaceNamesIa;
+		return $this->mNamespaceNamesIa + parent::getNamespaces();
 	}
 
 	function getQuickbarSettings() {
-		global $wgQuickbarSettingsIa;
-		return $wgQuickbarSettingsIa;
+		return $this->mQuickbarSettingsIa;
 	}
 
 	function getSkinNames() {
-		global $wgSkinNamesIa;
-		return $wgSkinNamesIa;
+		return $this->mSkinNamesIa + parent::getSkinNames();
 	}
 
 	function getMessage( $key ) {
-		global $wgAllMessagesIa;
-		if( isset( $wgAllMessagesIa[$key] ) ) {
-			return $wgAllMessagesIa[$key];
+		if( isset( $this->mMessagesIa[$key] ) ) {
+			return $this->mMessagesIa[$key];
 		} else {
 			return parent::getMessage( $key );
 		}
 	}
 
+	function getAllMessages() {
+		return $this->mMessagesIa;
+	}
 
 }
 
