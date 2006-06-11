3f33cc81d3f21922546762f8468616bc4b6697f4
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-11 15:40:17 +0000

    * Restructured

--- languages/LanguageLa.php
+++ languages/LanguageLa.php
@@ -5,60 +5,80 @@
   * @subpackage Language
   */
 
-/* private */ $wgNamespaceNamesLa = array(
-	NS_SPECIAL        => 'Specialis',
-	NS_MAIN           => '',
-	NS_TALK           => 'Disputatio',
-	NS_USER           => 'Usor',
-	NS_USER_TALK      => 'Disputatio_Usoris',
-	NS_PROJECT        => $wgMetaNamespace,
-	NS_PROJECT_TALK   => FALSE,  # Set in constructor
-	NS_IMAGE          => 'Imago',
-	NS_IMAGE_TALK     => 'Disputatio_Imaginis',
-	NS_MEDIAWIKI      => 'MediaWiki',
-	NS_MEDIAWIKI_TALK => 'Disputatio_MediaWiki',
-	NS_TEMPLATE       => 'Formula',
-	NS_TEMPLATE_TALK  => 'Disputatio_Formulae',
-	NS_HELP           => 'Auxilium',
-	NS_HELP_TALK      => 'Disputatio_Auxilii',
-	NS_CATEGORY       => 'Categoria',
-	NS_CATEGORY_TALK  => 'Disputatio_Categoriae',
-) + $wgNamespaceNamesEn;
-
-/* private */ $wgQuickbarSettingsLa = array(
-	"Nullus", "Constituere a sinistra", "Constituere a dextra", "Innens a sinistra"
-);
-
-/* private */ $wgSkinNamesLa = array(
-	'standard' => 'Norma',
-	'nostalgia' => 'Nostalgia',
-	'cologneblue' => 'Caerulus Colonia'
-) + $wgSkinNamesEn;
-
 if (!$wgCachedMessageArrays) {
 	require_once('MessagesLa.php');
 }
 
-require_once( "LanguageUtf8.php" );
-
 class LanguageLa extends LanguageUtf8 {
+	private $mMessagesLa, $mNamespaceNamesLa = null;
+
+	private $mQuickbarSettingsLa = array(
+		'Nullus', 'Constituere a sinistra', 'Constituere a dextra', 'Innens a sinistra'
+	);
+	
+	private $mSkinNamesLa = array(
+		'standard' => 'Norma',
+		'nostalgia' => 'Nostalgia',
+		'cologneblue' => 'Caerulus Colonia'
+	);
+
 	function LanguageLa() {
-		global $wgNamespaceNamesLa, $wgMetaNamespace;
 		LanguageUtf8::LanguageUtf8();
-		$wgNamespaceNamesLa[NS_PROJECT_TALK] = 'Disputatio_' .
-			$this->convertGrammar( $wgMetaNamespace, 'genitive' );
+
+		global $wgAllMessagesLa;
+		$this->mMessagesLa =& $wgAllMessagesLa;
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesLa = array(
+			NS_SPECIAL        => 'Specialis',
+			NS_MAIN           => '',
+			NS_TALK           => 'Disputatio',
+			NS_USER           => 'Usor',
+			NS_USER_TALK      => 'Disputatio_Usoris',
+			NS_PROJECT        => $wgMetaNamespace,
+			NS_PROJECT_TALK   => 'Disputatio_' . $this->convertGrammar( $wgMetaNamespace, 'genitive' ),
+			NS_IMAGE          => 'Imago',
+			NS_IMAGE_TALK     => 'Disputatio_Imaginis',
+			NS_MEDIAWIKI      => 'MediaWiki',
+			NS_MEDIAWIKI_TALK => 'Disputatio_MediaWiki',
+			NS_TEMPLATE       => 'Formula',
+			NS_TEMPLATE_TALK  => 'Disputatio_Formulae',
+			NS_HELP           => 'Auxilium',
+			NS_HELP_TALK      => 'Disputatio_Auxilii',
+			NS_CATEGORY       => 'Categoria',
+			NS_CATEGORY_TALK  => 'Disputatio_Categoriae',
+		);
+
 	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesLa;
-		return $wgNamespaceNamesLa;
+		return $this->mNamespaceNamesLa + parent::getNamespaces();
+	}
+
+	function getQuickbarSettings() {
+		return $this->mQuickbarSettingsLa;
+	}
+
+	function getSkinNames() {
+		return $this->mSkinNamesLa + parent::getSkinNames();
+	}
+
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesLa[$key] ) ) {
+			return $this->mMessagesLa[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
+
+	function getAllMessages() {
+		return $this->mMessagesLa;
 	}
 
 	function getNsIndex( $text ) {
-		global $wgNamespaceNamesLa;
 		global $wgMetaNamespace;
 
-		foreach ( $wgNamespaceNamesLa as $i => $n ) {
+		foreach ( $this->mNamespaceNamesLa as $i => $n ) {
 			if ( 0 == strcasecmp( $n, $text ) ) { return $i; }
 		}
 
@@ -69,29 +89,13 @@ class LanguageLa extends LanguageUtf8 {
 		return false;
 	}
 
-	function getQuickbarSettings() {
-		global $wgQuickbarSettingsLa;
-		return $wgQuickbarSettingsLa;
-	}
-
-	function getSkinNames() {
-		global $wgSkinNamesLa;
-		return $wgSkinNamesLa;
-	}
-
-	function getMessage( $key ) {
-		global $wgAllMessagesLa;
-		if( isset( $wgAllMessagesLa[$key] ) ) {
-			return $wgAllMessagesLa[$key];
-		}
-		return parent::getMessage( $key );
-	}
-
 	/**
 	 * Convert from the nominative form of a noun to some other case
 	 *
 	 * Just used in a couple places for sitenames; special-case as necessary.
 	 * Rules are far from complete.
+	 *
+	 * Cases: genitive
 	 */
 	function convertGrammar( $word, $case ) {
 		global $wgGrammarForms;
