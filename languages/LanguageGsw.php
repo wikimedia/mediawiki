fa7553787751907b02c62badfddc1970a40bbd04
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-16 22:20:19 +0000

    * Update to Alemannic localization (gsw)

--- languages/LanguageGsw.php
+++ languages/LanguageGsw.php
@@ -1,8 +1,9 @@
 <?php
-/**
-  * @package MediaWiki
-  * @subpackage Language
-  */
+/** Alemannic (Alemannisch)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
 
 /*
 <Melancholie> for the moment it would be the best if LanguageAls.php would be
@@ -16,8 +17,99 @@
 
 include_once( "LanguageDe.php" );
 
+if (!$wgCachedMessageArrays) {
+	require_once('MessagesGsw.php');
+}
+
 class LanguageGsw extends LanguageDe {
-	/* Inherit everything. */
+	private $mMessagesGsw = null;
+
+	function LanguageGsw() {
+		LanguageDe::LanguageDe();
+
+		global $wgAllMessagesGsw;
+		$this->mMessagesGsw =& $wgAllMessagesGsw;
+
+	}
+
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesGsw[$key] ) ) {
+			return $this->mMessagesGsw[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
+
+	function getAllMessages() {
+		return $this->mMessagesGsw;
+	}
+
+	function getFallbackLanguage() {
+		return 'de';
+	}
+
+	function linkTrail() {
+		return '/^([äöüßa-z]+)(.*)$/sDu';
+	}
+
+   # Convert from the nominative form of a noun to some other case
+   # Invoked with result
+
+   function convertGrammar( $word, $case ) {
+       global $wgGrammarForms;
+       if ( isset($wgGrammarForms['gsw'][$case][$word]) ) {
+           return $wgGrammarForms['gsw'][$case][$word];
+       }
+       switch ( $case ) {
+           case 'dativ':
+               if ( $word == 'Wikipedia' ) {
+                   $word = 'vo de Wikipedia';
+               } elseif ( $word == 'Wikinorchrichte' ) {
+                   $word = 'vo de Wikinochrichte';
+               } elseif ( $word == 'Wiktionaire' ) {
+                   $word = 'vom Wiktionaire';
+               } elseif ( $word == 'Wikibuecher' ) {
+                   $word = 'vo de Wikibuecher';
+               } elseif ( $word == 'Wikisprüch' ) {
+                   $word = 'vo de Wikisprüch';
+               } elseif ( $word == 'Wikiquälle' ) {
+                   $word = 'vo de Wikiquälle';
+               }
+               break;
+           case 'akkusativ':
+               if ( $word == 'Wikipedia' ) {
+                   $word = 'd Wikipedia';
+               } elseif ( $word == 'Wikinorchrichte' ) {
+                   $word = 'd Wikinochrichte';
+               } elseif ( $word == 'Wiktionaire' ) {
+                   $word = 's Wiktionaire';
+               } elseif ( $word == 'Wikibuecher' ) {
+                   $word = 'd Wikibuecher';
+               } elseif ( $word == 'Wikisprüch' ) {
+                   $word = 'd Wikisprüch';
+               } elseif ( $word == 'Wikiquälle' ) {
+                   $word = 'd Wikiquälle';
+               }
+               break;
+           case 'nominativ':
+               if ( $word == 'Wikipedia' ) {
+                   $word = 'd Wikipedia';
+               } elseif ( $word == 'Wikinorchrichte' ) {
+                   $word = 'd Wikinochrichte';
+               } elseif ( $word == 'Wiktionaire' ) {
+                   $word = 's Wiktionaire';
+               } elseif ( $word == 'Wikibuecher' ) {
+                   $word = 'd Wikibuecher';
+               } elseif ( $word == 'Wikisprüch' ) {
+                   $word = 'd Wikisprüch';
+               } elseif ( $word == 'Wikiquälle' ) {
+                   $word = 'd Wikiquälle';
+               }
+               break;
+       } 
+       return $word;
+   }
+
 }
 
 ?>
