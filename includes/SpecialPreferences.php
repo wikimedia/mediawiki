1a9e871a70ba910c387813dc49570c012f767c7b
Rob Church <robchurch@users.mediawiki.org>
2006-06-22 01:40:51 +0000

    (bug 6392) Fix misbehaving <br /> in preferences form

--- includes/SpecialPreferences.php
+++ includes/SpecialPreferences.php
@@ -691,12 +691,14 @@ class PreferencesForm {
 		}
 		# </FIXME>
 
-		if ($wgAllowRealName || $wgEnableEmail) {
-			$wgOut->addHTML("<div class='prefsectiontip'>");
-			$rn = $wgAllowRealName ? wfMsg('prefs-help-realname') : '';
-			$em = $wgEnableEmail ? '<br />' .  wfMsg('prefs-help-email') : '';
-			$wgOut->addHTML( $rn . $em  . '</div>');
-		}
+		# Show little "help" tips for the real name and email address options
+		if( $wgAllowRealName || $wgEnableEmail ) {
+			if( $wgAllowRealName )
+				$tips[] = wfMsg( 'prefs-help-realname' );
+			if( $wgEnableEmail )
+				$tips[] = wfMsg( 'prefs-help-email' );
+			$wgOut->addHtml( '<div class="prefsectiontip">' . implode( '<br />', $tips ) . '</div>' );
+		}		
 
 		$wgOut->addHTML( '</fieldset>' );
 
