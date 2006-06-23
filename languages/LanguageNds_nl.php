41e4a29265f863ae91e2003287370f4a6aa3cb6a
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-23 22:25:55 +0000

    * Restructured

--- languages/LanguageNds_nl.php
+++ languages/LanguageNds_nl.php
@@ -11,54 +11,60 @@
   * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
   */
 
-/**
- *
- */
 require_once 'LanguageNds.php';
 
-/* private */ $wgNamespaceNamesNds_nl = array(
-	NS_MEDIA            => 'Media',
-	NS_SPECIAL          => 'Speciaol',
-	NS_MAIN             => '',
-	NS_TALK             => 'Overleg',
-	NS_USER             => 'Gebruker',
-	NS_USER_TALK        => 'Overleg_gebruker',
-	NS_PROJECT          => $wgMetaNamespace,
-	NS_PROJECT_TALK     => 'Overleg_' . $wgMetaNamespace,
-	NS_IMAGE            => 'Ofbeelding',
-	NS_IMAGE_TALK       => 'Overleg_ofbeelding',
-	NS_MEDIAWIKI        => 'MediaWiki',
-	NS_MEDIAWIKI_TALK   => 'Overleg_MediaWiki',
-	NS_TEMPLATE         => 'Sjabloon',
-	NS_TEMPLATE_TALK    => 'Overleg_sjabloon',
-	NS_HELP             => 'Help',
-	NS_HELP_TALK        => 'Overleg_help',
-	NS_CATEGORY         => 'Categorie',
-	NS_CATEGORY_TALK    => 'Overleg_categorie'
-);
-
-/* private */ $wgSkinNamesNds_nl = array(
-	'standard'      => 'Klassiek',
-	'nostalgia'     => 'Nostalgie',
-	'cologneblue'   => 'Keuls blauw',
-	'smarty'        => 'Paddington',
-	'chick'         => 'Sjiek'
-) + $wgSkinNamesEn;
-
-
-/**
- *
- */
 class LanguageNds_nl extends LanguageNds {
+	private $mNamespaceNamesNds_nl = null;
+
+	private $mSkinNamesNds_nl = array(
+		'standard'      => 'Klassiek',
+		'nostalgia'     => 'Nostalgie',
+		'cologneblue'   => 'Keuls blauw',
+		'smarty'        => 'Paddington',
+		'chick'         => 'Sjiek'
+	);
+
+	function __construct() {
+		parent::__construct();
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesNds_nl = array(
+			NS_MEDIA            => 'Media',
+			NS_SPECIAL          => 'Speciaol',
+			NS_MAIN             => '',
+			NS_TALK             => 'Overleg',
+			NS_USER             => 'Gebruker',
+			NS_USER_TALK        => 'Overleg_gebruker',
+			NS_PROJECT          => $wgMetaNamespace,
+			NS_PROJECT_TALK     => 'Overleg_' . $wgMetaNamespace,
+			NS_IMAGE            => 'Ofbeelding',
+			NS_IMAGE_TALK       => 'Overleg_ofbeelding',
+			NS_MEDIAWIKI        => 'MediaWiki',
+			NS_MEDIAWIKI_TALK   => 'Overleg_MediaWiki',
+			NS_TEMPLATE         => 'Sjabloon',
+			NS_TEMPLATE_TALK    => 'Overleg_sjabloon',
+			NS_HELP             => 'Help',
+			NS_HELP_TALK        => 'Overleg_help',
+			NS_CATEGORY         => 'Categorie',
+			NS_CATEGORY_TALK    => 'Overleg_categorie'
+		);
+
+	}
+
+	function getFallbackLanguage() {
+		return 'nds';
+	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesNds_nl;
-		return $wgNamespaceNamesNds_nl;
+		return $this->mNamespaceNamesNds_nl;
 	}
 
 	function getSkinNames() {
-		global $wgSkinNamesNds_nl;
-		return $wgSkinNamesNds_nl;
+		return $this->mSkinNamesNds_nl + parent::getSkinNames();
+	}
+
+	function getAllMessages() {
+		return null;
 	}
 
 	function formatDay( $day, $format ) {
