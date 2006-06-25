2bbe0a75eca7af3317c37c3217ae710a4a9d13e2
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-25 10:13:09 +0000

    * Restructured

--- languages/LanguageHr.php
+++ languages/LanguageHr.php
@@ -5,75 +5,89 @@
   * @subpackage Language
   */
 
-/** */
 require_once( 'LanguageUtf8.php' );
-
-/* private */ $wgNamespaceNamesHr = array(
- NS_MEDIA           => "Mediji",
- NS_SPECIAL         => "Posebno",
- NS_MAIN            => "",
- NS_TALK            => "Razgovor",
- NS_USER            => "Suradnik",
- NS_USER_TALK       => "Razgovor_sa_suradnikom",
- NS_PROJECT         => $wgMetaNamespace,
- NS_PROJECT_TALK    => "Razgovor_" . $wgMetaNamespace,
- NS_IMAGE           => "Slika",
- NS_IMAGE_TALK      => "Razgovor_o_slici",
- NS_MEDIAWIKI       => "MediaWiki",
- NS_MEDIAWIKI_TALK  => "MediaWiki_razgovor",
- NS_TEMPLATE        => "Predložak",
- NS_TEMPLATE_TALK   => "Razgovor_o_predlošku",
- NS_HELP            => "Pomoć",
- NS_HELP_TALK       => "Razgovor_o_pomoći",
- NS_CATEGORY        => "Kategorija",
- NS_CATEGORY_TALK   => "Razgovor_o_kategoriji"
-) + $wgNamespaceNamesEn;
-
-/* private */ $wgQuickbarSettingsHr = array(
- "Bez", "Lijevo nepomično", "Desno nepomično", "Lijevo leteće"
-);
-
-/* private */ $wgSkinNamesHr = array(
- 'standard'  => "Standardna",
- 'nostalgia'  => "Nostalgija",
- 'cologneblue'  => "Kölnska plava",
- 'smarty'  => "Paddington",
- 'montparnasse'  => "Montparnasse",
- 'davinci'  => "DaVinci",
- 'mono'   => "Mono",
- 'monobook'  => "MonoBook",
- "myskin"  => "MySkin",
- "chick"  => "Chick"
-);
 
 if (!$wgCachedMessageArrays) {
 	require_once('MessagesHr.php');
 }
 
-#--------------------------------------------------------------------------
-# Internationalisation code
-#--------------------------------------------------------------------------
-
 class LanguageHr extends LanguageUtf8 {
+	private $mMessagesHr, $mNamespaceNamesHr = null;
+
+	private $mQuickbarSettingsHr = array(
+	'Bez', 'Lijevo nepomično', 'Desno nepomično', 'Lijevo leteće'
+	);
+	
+	private $mSkinNamesHr = array(
+	'standard'  => 'Standardna',
+	'nostalgia'  => 'Nostalgija',
+	'cologneblue'  => 'Kölnska plava',
+	'smarty'  => 'Paddington',
+	'montparnasse'  => 'Montparnasse',
+	'davinci'  => 'DaVinci',
+	'mono'   => 'Mono',
+	'monobook'  => 'MonoBook',
+	'myskin'  => 'MySkin',
+	'chick'  => 'Chick'
+	);
+
+	function __construct() {
+		parent::__construct();
+
+		global $wgAllMessagesHr;
+		$this->mMessagesHr =& $wgAllMessagesHr;
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesHr = array(
+			NS_MEDIA           => 'Mediji',
+			NS_SPECIAL         => 'Posebno',
+			NS_MAIN            => '',
+			NS_TALK            => 'Razgovor',
+			NS_USER            => 'Suradnik',
+			NS_USER_TALK       => 'Razgovor_sa_suradnikom',
+			NS_PROJECT         => $wgMetaNamespace,
+			NS_PROJECT_TALK    => 'Razgovor_' . $wgMetaNamespace,
+			NS_IMAGE           => 'Slika',
+			NS_IMAGE_TALK      => 'Razgovor_o_slici',
+			NS_MEDIAWIKI       => 'MediaWiki',
+			NS_MEDIAWIKI_TALK  => 'MediaWiki_razgovor',
+			NS_TEMPLATE        => 'Predložak',
+			NS_TEMPLATE_TALK   => 'Razgovor_o_predlošku',
+			NS_HELP            => 'Pomoć',
+			NS_HELP_TALK       => 'Razgovor_o_pomoći',
+			NS_CATEGORY        => 'Kategorija',
+			NS_CATEGORY_TALK   => 'Razgovor_o_kategoriji'
+		);
+
+	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesHr;
-		return $wgNamespaceNamesHr;
+		return $this->mNamespaceNamesHr + parent::getNamespaces();
+	}
+
+	function getQuickbarSettings() {
+		return $this->mQuickbarSettingsHr;
+	}
+
+	function getSkinNames() {
+		return $this->mSkinNamesHr + parent::getSkinNames();
 	}
 
 	function getDateFormats() {
 		return false;
 	}
 
-	function getQuickbarSettings() {
-	 	global $wgQuickbarSettingsHr;
-		return $wgQuickbarSettingsHr;
- 	}
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesHr[$key] ) ) {
+			return $this->mMessagesHr[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
 
-	function getSkinNames() {
-		global $wgSkinNamesHr;
-		return $wgSkinNamesHr;
- 	}
+	function getAllMessages() {
+		return $this->mMessagesHr;
+	}
 
 	function date( $ts, $adj = false, $format = true, $timecorrection = false ) {
 		if ( $adj ) { $ts = $this->userAdjust( $ts, $timecorrection ); }
@@ -85,21 +99,13 @@ class LanguageHr extends LanguageUtf8 {
 		return $d;
 	}
 
-	function getMessage( $key ) {
-		global $wgAllMessagesHr;
-		if( isset( $wgAllMessagesHr[$key] ) ) {
-			return $wgAllMessagesHr[$key];
-		} else {
-			return parent::getMessage( $key );
-		}
-	}
 
 	function separatorTransformTable() {
 		return array(',' => '.', '.' => ',' );
 	}
 
  	function fallback8bitEncoding() {
-		return "iso-8859-2";
+		return 'iso-8859-2';
  	}
 
 	function convertPlural( $count, $wordform1, $wordform2, $wordform3) {
