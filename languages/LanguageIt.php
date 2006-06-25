dd7b211d2ba2f52a1c0918660a00703bba187323
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-25 10:31:32 +0000

    * Restructured

--- languages/LanguageIt.php
+++ languages/LanguageIt.php
@@ -1,63 +1,75 @@
 <?php
-/**
-  * @package MediaWiki
-  * @subpackage Language
-  */
+/** Italian (Italiano)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
 
-/* private */ $wgNamespaceNamesIt = array(
-	NS_MEDIA            => 'Media',
-	NS_SPECIAL          => 'Speciale',
-	NS_MAIN             => '',
-	NS_TALK             => 'Discussione',
-	NS_USER             => 'Utente',
-	NS_USER_TALK        => 'Discussioni_utente',
-	NS_PROJECT          => $wgMetaNamespace,
-	NS_PROJECT_TALK     => 'Discussioni_'.$wgMetaNamespace,
-	NS_IMAGE            => 'Immagine',
-	NS_IMAGE_TALK       => 'Discussioni_immagine',
-	NS_MEDIAWIKI        => 'MediaWiki',
-	NS_MEDIAWIKI_TALK   => 'Discussioni_MediaWiki',
-	NS_TEMPLATE         => 'Template',
-	NS_TEMPLATE_TALK    => 'Discussioni_template',
-	NS_HELP             => 'Aiuto',
-	NS_HELP_TALK        => 'Discussioni_aiuto',
-	NS_CATEGORY         => 'Categoria',
-	NS_CATEGORY_TALK    => 'Discussioni_categoria'
-
-) + $wgNamespaceNamesEn;
-
-/* private */ $wgQuickbarSettingsIt = array(
-	"Nessuno", "Fisso a sinistra", "Fisso a destra", "Fluttuante a sinistra"
-);
+require_once( 'LanguageUtf8.php' );
 
 if (!$wgCachedMessageArrays) {
 	require_once('MessagesIt.php');
 }
 
-require_once( "LanguageUtf8.php" );
-
 class LanguageIt extends LanguageUtf8 {
+	private $mMessagesIt, $mNamespaceNamesIt = null;
+	
+	private $mQuickbarSettingsIt = array(
+		'Nessuno', 'Fisso a sinistra', 'Fisso a destra', 'Fluttuante a sinistra'
+	);
+
+	function __construct() {
+		parent::__construct();
+
+		global $wgAllMessagesIt;
+		$this->mMessagesIt =& $wgAllMessagesIt;
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesIt = array(
+			NS_MEDIA            => 'Media',
+			NS_SPECIAL          => 'Speciale',
+			NS_MAIN             => '',
+			NS_TALK             => 'Discussione',
+			NS_USER             => 'Utente',
+			NS_USER_TALK        => 'Discussioni_utente',
+			NS_PROJECT          => $wgMetaNamespace,
+			NS_PROJECT_TALK     => 'Discussioni_' . $wgMetaNamespace,
+			NS_IMAGE            => 'Immagine',
+			NS_IMAGE_TALK       => 'Discussioni_immagine',
+			NS_MEDIAWIKI        => 'MediaWiki',
+			NS_MEDIAWIKI_TALK   => 'Discussioni_MediaWiki',
+			NS_TEMPLATE         => 'Template',
+			NS_TEMPLATE_TALK    => 'Discussioni_template',
+			NS_HELP             => 'Aiuto',
+			NS_HELP_TALK        => 'Discussioni_aiuto',
+			NS_CATEGORY         => 'Categoria',
+			NS_CATEGORY_TALK    => 'Discussioni_categoria'
+		);
+
+	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesIt;
-		return $wgNamespaceNamesIt;
+		return $this->mNamespaceNamesIt + parent::getNamespaces();
 	}
 
 	function getQuickbarSettings() {
-		global $wgQuickbarSettingsIt;
-		return $wgQuickbarSettingsIt;
+		return $this->mQuickbarSettingsIt;
+	}
+
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesIt[$key] ) ) {
+			return $this->mMessagesIt[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
+
+	function getAllMessages() {
+		return $this->mMessagesIt;
 	}
 
 	function formatMonth( $month, $format ) {
 		return $this->getMonthAbbreviation( $month );
-	}
-
-	function getMessage( $key ) {
-		global $wgAllMessagesIt;
-		if(array_key_exists($key, $wgAllMessagesIt))
-			return $wgAllMessagesIt[$key];
-		else
-			return parent::getMessage($key);
 	}
 
 	/**
