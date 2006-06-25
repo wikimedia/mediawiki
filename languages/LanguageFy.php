a771b0fccf98441c40b46600dd276b6dda0551ca
Niklas Laxström <nikerabbit@users.mediawiki.org>
2006-06-25 15:56:30 +0000

    * Restructured

--- languages/LanguageFy.php
+++ languages/LanguageFy.php
@@ -1,74 +1,98 @@
 <?php
-/**
-  * @package MediaWiki
-  * @subpackage Language
-  */
+/** Frisian (Frysk)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ *
+ * @author Niklas Laxström
+ */
 
-require_once( "LanguageUtf8.php" );
-
-/* private */ $wgNamespaceNamesFy = array(
-	NS_MEDIA          => "Media",
-	NS_SPECIAL        => "Wiki",
-	NS_MAIN           => "",
-	NS_TALK           => "Oerlis",
-	NS_USER           => "Meidogger",
-	NS_USER_TALK      => "Meidogger_oerlis",
-	NS_PROJECT        => $wgMetaNamespace,
-	NS_PROJECT_TALK   => $wgMetaNamespace . "_oerlis",
-	NS_IMAGE          => "Ofbyld",
-	NS_IMAGE_TALK     => "Ofbyld_oerlis",
-	NS_MEDIAWIKI      => "MediaWiki",
-	NS_MEDIAWIKI_TALK => "MediaWiki_oerlis",
-	NS_TEMPLATE       => "Berjocht",
-	NS_TEMPLATE_TALK  => "Berjocht_oerlis",
-	NS_HELP           => "Hulp",
-	NS_HELP_TALK      => "Hulp_oerlis",
-	NS_CATEGORY       => "Kategory",
-	NS_CATEGORY_TALK  => "Kategory_oerlis"
-) + $wgNamespaceNamesEn;
-
-/* private */ $wgQuickbarSettingsFy = array(
-	"Ut", "Lofts fêst", "Rjochts fêst", "Lofts sweevjend"
-);
-
-/* private */ $wgSkinNamesFy = array(
-	'standard' => "Standert",
-	'nostalgia' => "Nostalgy",
-) + $wgSkinNamesEn;
-
-
-/* private */ $wgDateFormatsFy = array(
-	'Gjin foarkar',
-	'16.12, jan 15, 2001',
-	'16.12, 15 jan 2001',
-	'16.12, 2001 jan 15',
-	'ISO 8601' => '2001-01-15 16:12:34'
-);
-
-/* private */ $wgBookstoreListFy = array(
-);
+require_once( 'LanguageUtf8.php' );
 
 if (!$wgCachedMessageArrays) {
 	require_once('MessagesFy.php');
 }
 
 class LanguageFy extends LanguageUtf8 {
+	private $mMessagesFy, $mNamespaceNamesFy = null;
 
-	function getBookstoreList () {
-		global $wgBookstoreListFy ;
-		return $wgBookstoreListFy ;
+	private $mQuickbarSettingsFy = array(
+		'Ut', 'Lofts fêst', 'Rjochts fêst', 'Lofts sweevjend'
+	);
+	
+	private $mSkinNamesFy = array(
+		'standard' => 'Standert',
+		'nostalgia' => 'Nostalgy',
+	);
+		
+	private $mDateFormatsFy = array(
+		'Gjin foarkar',
+		'16.12, jan 15, 2001',
+		'16.12, 15 jan 2001',
+		'16.12, 2001 jan 15',
+		'ISO 8601' => '2001-01-15 16:12:34'
+	);
+
+	function __construct() {
+		parent::__construct();
+
+		global $wgAllMessagesFy;
+		$this->mMessagesFy =& $wgAllMessagesFy;
+
+		global $wgMetaNamespace;
+		$this->mNamespaceNamesFy = array(
+			NS_MEDIA          => 'Media',
+			NS_SPECIAL        => 'Wiki',
+			NS_MAIN           => '',
+			NS_TALK           => 'Oerlis',
+			NS_USER           => 'Meidogger',
+			NS_USER_TALK      => 'Meidogger_oerlis',
+			NS_PROJECT        => $wgMetaNamespace,
+			NS_PROJECT_TALK   => $wgMetaNamespace . '_oerlis',
+			NS_IMAGE          => 'Ofbyld',
+			NS_IMAGE_TALK     => 'Ofbyld_oerlis',
+			NS_MEDIAWIKI      => 'MediaWiki',
+			NS_MEDIAWIKI_TALK => 'MediaWiki_oerlis',
+			NS_TEMPLATE       => 'Berjocht',
+			NS_TEMPLATE_TALK  => 'Berjocht_oerlis',
+			NS_HELP           => 'Hulp',
+			NS_HELP_TALK      => 'Hulp_oerlis',
+			NS_CATEGORY       => 'Kategory',
+			NS_CATEGORY_TALK  => 'Kategory_oerlis'
+		);
+
 	}
 
 	function getNamespaces() {
-		global $wgNamespaceNamesFy;
-		return $wgNamespaceNamesFy;
+		return $this->mNamespaceNamesFy + parent::getNamespaces();
 	}
 
+	function getQuickbarSettings() {
+		return $this->mQuickbarSettingsFy;
+	}
+
+	function getSkinNames() {
+		return $this->mSkinNamesFy + parent::getSkinNames();
+	}
+
+	function getDateFormats() {
+		return $this->mDateFormatsFy;
+	}
+
+	function getMessage( $key ) {
+		if( isset( $this->mMessagesFy[$key] ) ) {
+			return $this->mMessagesFy[$key];
+		} else {
+			return parent::getMessage( $key );
+		}
+	}
+
+	function getAllMessages() {
+		return $this->mMessagesFy;
+	}
 
 	function getNsIndex( $text ) {
-		global $wgNamespaceNamesFy;
-
-		foreach ( $wgNamespaceNamesFy as $i => $n ) {
+		foreach ( $this->mNamespaceNamesFy as $i => $n ) {
 			if ( 0 == strcasecmp( $n, $text ) ) { return $i; }
 		}
 		if ( 0 == strcasecmp( "Brûker", $text ) ) return 2;
@@ -76,94 +100,22 @@ class LanguageFy extends LanguageUtf8 {
 		return false;
 	}
 
-
-	function getQuickbarSettings() {
-		global $wgQuickbarSettingsFy;
-		return $wgQuickbarSettingsFy;
+	function timeSeparator( $format ) {
+		return '.';
 	}
 
-	function getSkinNames() {
-		global $wgSkinNamesFy;
-		return $wgSkinNamesFy;
+	function formatMonth( $month, $format ) {
+		return $this->getMonthAbbreviation( $month );
 	}
 
 	function separatorTransformTable() {
 		return array(',' => '.', '.' => ',' );
 	}
 
-	function getDateFormats() {
-		global $wgDateFormatsFy;
-		return $wgDateFormatsFy;
+	function linkTrail() {
+		return '/^([a-zàáèéìíòóùúâêîôûäëïöü]+)(.*)$/sDu';
 	}
 
-	/**
-	 * @access public
-	 * @param mixed  $ts the time format which needs to be turned into a
-	 *               date('YmdHis') format with wfTimestamp(TS_MW,$ts)
-	 * @param bool   $adj whether to adjust the time output according to the
-	 *               user configured offset ($timecorrection)
-	 * @param bool   $format true to use user's date format preference
-	 * @param string $timecorrection the time offset as returned by
-	 *               validateTimeZone() in Special:Preferences
-	 * @return string
-	 */
-	function date( $ts, $adj = false, $format = true, $timecorrection = false ) {
-
-		if ( $adj ) { $ts = $this->userAdjust( $ts, $timecorrection ); }
-
-		$datePreference = $this->dateFormat( $format );
-
-		$month = $this->getMonthAbbreviation( substr( $ts, 4, 2 ) );
-		$day = 0 + substr( $ts, 6, 2 );
-		$year = substr( $ts, 0, 4 );
-
-		switch( $datePreference ) {
-			case MW_DATE_DMY: return "$day $month $year";
-			case MW_DATE_YMD: return "$year $month $day";
-			case MW_DATE_ISO: return substr($ts, 0, 4). '-' . substr($ts, 4, 2). '-' .substr($ts, 6, 2);
-			default: return "$month $day, $year";
-		}
-	}
-
-	/**
-	* @access public
-	* @param mixed  $ts the time format which needs to be turned into a
-	*               date('YmdHis') format with wfTimestamp(TS_MW,$ts)
-	* @param bool   $adj whether to adjust the time output according to the
-	*               user configured offset ($timecorrection)
-	* @param bool   $format true to use user's date format preference
-	* @param string $timecorrection the time offset as returned by
-	*               validateTimeZone() in Special:Preferences
-	* @return string
-	*/
-	function time( $ts, $adj = false, $format = true, $timecorrection = false ) {
-		global $wgUser;
-
-		if ( $adj ) { $ts = $this->userAdjust( $ts, $timecorrection ); }
-		$datePreference = $this->dateFormat( $format );
-
-		if ( $datePreference == MW_DATE_ISO ) {
-			$sep = ':';
-		} else {
-			$sep = '.';
-		}
-
-		$t = substr( $ts, 8, 2 ) . $sep . substr( $ts, 10, 2 );
-
-		if ( $datePreference == MW_DATE_ISO ) {
-			$t .= $sep . substr( $ts, 12, 2 );
-		}
-		return $t;
-	}
-
-	function getMessage( $key ) {
-		global $wgAllMessagesFy;
-		if( isset( $wgAllMessagesFy[$key] ) ) {
-			return $wgAllMessagesFy[$key];
-		} else {
-			return parent::getMessage( $key );
-		}
-	}
 }
 
 ?>
