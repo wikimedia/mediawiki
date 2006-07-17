bd06221f8121a4d9746af283873a9f96615705a7
Brion Vibber <brion@users.mediawiki.org>
2006-07-17 19:27:03 +0000

    * Bishnupriya (bpy) stub file http://meta.wikimedia.org/wiki/Approved_requests_for_new_languages#Bishnupriya_Manipuri

---
+++ languages/LanguageBpy.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Bishnupriya
+ * most speakers have Bengali as second language, use as default fallback
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+class LanguageBpy extends LanguageBn {
+
+	function getFallbackLanguage() {
+		return 'bn';
+	}
+}
+
+?>
