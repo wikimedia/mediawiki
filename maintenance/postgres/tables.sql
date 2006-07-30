7703f8f90b632fc89357c6f96b7d16e557a0b4e5
Greg Sabino Mullane <greg@users.mediawiki.org>
2006-07-30 23:36:14 +0000

    Add profiling table.

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -441,6 +441,16 @@ $mw$
   SELECT 1;
 $mw$;
 
+-- This table is not used unless profiling is turned on
+CREATE TABLE profiling (
+  pf_count   INTEGER         NOT NULL DEFAULT 0,
+  pf_time    NUMERIC(18,10)  NOT NULL DEFAULT 0,
+  pf_name    TEXT            NOT NULL,
+  pf_server  TEXT            NOT NULL
+);
+CREATE UNIQUE INDEX pf_name_server ON profiling (pf_name, pf_server);
+
+
 CREATE TABLE mediawiki_version (
   type         TEXT         NOT NULL,
   mw_version   TEXT         NOT NULL,
