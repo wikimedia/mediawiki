3ed7ee64a8ac37e037f23285bd3a4db6b33831f4
Rotem Liss <rotem@users.mediawiki.org>
2006-08-01 16:24:55 +0000

    rmy localisation: Namespaces + grammar.

--- languages/MessagesRmy.php
+++ languages/MessagesRmy.php
@@ -1,19 +1,38 @@
 <?php
-/** Vlax Romany (Romani)
-  *
-  * @subpackage Language
-  *
-  * @bug 5422
-  *
-  * @author Niklas Laxström
-  *
-  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-  */
+/**
+ * Vlax Romany (Romani)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ *
+ * @author Niklas Laxström
+ */
 
 /**
  * Use Romanian as default instead of English
  */
 $fallback = 'ro';
+
+$namespaceNames = array(
+	NS_MEDIA          => 'Mediya',
+	NS_SPECIAL        => 'Uzalutno',
+	NS_MAIN           => '',
+	NS_TALK           => 'Vakyarimata',
+	NS_USER           => 'Jeno',
+	NS_USER_TALK      => 'Jeno_vakyarimata',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK   => '{{GRAMMAR:genitive-pl|$1}}_vakyarimata',
+	NS_IMAGE          => 'Chitro',
+	NS_IMAGE_TALK     => 'Chitro_vakyarimata',
+	NS_MEDIAWIKI      => 'MediyaViki',
+	NS_MEDIAWIKI_TALK => 'MediyaViki_vakyarimata',
+	NS_TEMPLATE       => 'Sikavno',
+	NS_TEMPLATE_TALK  => 'Sikavno_vakyarimata',
+	NS_HELP           => 'Zhutipen',
+	NS_HELP_TALK      => 'Zhutipen_vakyarimata',
+	NS_CATEGORY       => 'Shopni',
+	NS_CATEGORY_TALK  => 'Shopni_vakyarimata'
+);
 
 $messages = array(
 'subcategories'         => 'Telekategoriye',
