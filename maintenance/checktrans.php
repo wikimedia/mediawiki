83f7f25ea2db9c639ea68263eae2a94e44c429fd
Rotem Liss <rotem@users.mediawiki.org>
2006-08-04 20:58:19 +0000

    In the language scripts: * Using Language::getMessagesFor instead of creating new language objects. * Allowing a parameter which specifies the language code to check instead of the default one, in checktrans.php, duplicatetrans.php and unusedMessages.php. checktrans.php still does not work; duplicatetrans.php still works improperly.

--- maintenance/checktrans.php
+++ maintenance/checktrans.php
@@ -3,32 +3,37 @@
  * @package MediaWiki
  * @subpackage Maintenance
  * Check to see if all messages have been translated into the selected language.
- * To run this script, you must have a working installation, and it checks the
- * selected language of that installation.
+ * To run this script, you must have a working installation, and you can specify
+ * a language, or the script will check the installation language.
  */
 
 /** */
 require_once('commandLine.inc');
 
-die( "This script currently *does not work*, please wait for fix.\n" );
+#die( "This script currently *does not work*, please wait for fix.\n" );
 
-if ( $wgLang->getCode() == 'en' ) {
+if ( isset( $args[0] ) ) {
+	$code = $args[0];
+} else {
+	$code = $wgLang->getCode();
+}
+
+if ( $code == 'en' ) {
 	print "Current selected language is English. Cannot check translations.\n";
 	exit();
 }
 
 $count = $total = 0;
-$wgEnglishLang = Language::factory( 'en' );
-$wgEnglishMessages = $wgEnglishLang->getAllMessages();
-$wgLocalMessages = $wgLang->getAllMessages();
+$wgEnglishMessages = Language::getMessagesFor( 'en' );
+$wgLocalMessages = Language::getMessagesFor( $code );
 
-foreach ( $wgEnglishMessages as $code => $msg ) {
+foreach ( $wgEnglishMessages as $key => $msg ) {
 	++$total;
-	if ( !isset( $wgLocalMessages[$code] ) ) {
-		print "'{$code}' => \"$msg\",\n";
+	if ( !isset( $wgLocalMessages[$key] ) ) {
+		print "'{$key}' => \"$msg\",\n";
 		++$count;
 	}
 }
 
-print "{$count} messages of {$total} not translated.\n";
+print "{$count} messages of {$total} are not translated in the language {$code}.\n";
 ?>
--- maintenance/unusedMessages.php
+++ maintenance/unusedMessages.php
@@ -8,23 +8,28 @@
 
 require_once('commandLine.inc');
 
-if ( $wgLang->getCode() == 'en' ) {
+if ( isset( $args[0] ) ) {
+	$code = $args[0];
+} else {
+	$code = $wgLang->getCode();
+}
+
+if ( $code == 'en' ) {
 	print "Current selected language is English. Cannot check translations.\n";
 	exit();
 }
 
 $count = $total = 0;
-$wgEnglishLang = Language::factory( 'en' );
-$wgEnglishMessages = $wgEnglishLang->getAllMessages();
-$wgLocalMessages = $wgLang->getAllMessages();
+$wgEnglishMessages = Language::getMessagesFor( 'en' );
+$wgLocalMessages = Language::getMessagesFor( $code );
 
-foreach ( $wgLocalMessages as $code => $msg ) {
+foreach ( $wgLocalMessages as $key => $msg ) {
 	++$total;
-	if ( !isset( $wgEnglishMessages[$code] ) ) {
-		print "* $code\n";
+	if ( !isset( $wgEnglishMessages[$key] ) ) {
+		print "* $key\n";
 		++$count;
 	}
 }
 
-print "{$count} messages of {$total} are unused\n";
+print "{$count} messages of {$total} are unused in the language {$code}\n";
 ?>
