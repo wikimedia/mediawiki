bb8f8a4212cef0f1c44cba8d9d03bffcb015f901
Tim Starling <tstarling@users.mediawiki.org>
2006-08-14 05:43:30 +0000

    Namespace updates for Urdu, bug 6552

--- languages/MessagesUr.php
+++ languages/MessagesUr.php
@@ -8,5 +8,26 @@ $defaultUserOptionOverrides = array(
 	'underline' => 0,
 );
 
+$namespaceNames = array(
+	NS_MEDIA => 'زریعہ',
+	NS_SPECIAL => 'خاص',
+	NS_MAIN => '',
+	NS_TALK => 'تبادلۂ_خیال',
+	NS_USER => 'صارف',
+	NS_USER_TALK => 'تبادلۂ_خیال_صارف',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK => 'تبادلۂ_خیال_$1',
+	NS_IMAGE => 'تصویر',
+	NS_IMAGE_TALK => 'تبادلۂ_خیال_تصویر',
+	NS_MEDIAWIKI => 'میڈیاوکی',
+	NS_MEDIAWIKI_TALK => 'تبادلۂ_خیال_میڈیاوکی',
+	NS_TEMPLATE => 'سانچہ',
+	NS_TEMPLATE_TALK => 'تبادلۂ_خیال_سانچہ',
+	NS_HELP => 'معاونت',
+	NS_HELP_TALK => 'تبادلۂ_خیال_معاونت',
+	NS_CATEGORY => 'زمرہ',
+	NS_CATEGORY_TALK => 'تبادلۂ_خیال_زمرہ',
+);
+
 
 ?>
