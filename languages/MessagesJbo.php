16fd5f445e66fd2869c521d0697f383ada526981
Jimmy Collins <collinj@users.mediawiki.org>
2006-09-21 22:03:13 +0000

    Added Lojban translation (jbo).

---
+++ languages/MessagesJbo.php
@@ -0,0 +1,23 @@
+<?php
+/** Lojban (Lojban)
+  *
+  * @package MediaWiki
+  * @subpackage Language
+  */
+
+$messages = array(
+
+'1movedto2'		=> 'le pu se cmene lu [[$1]] li\'u cu ca se cmene lu [[$2]] li\'u',
+'categories'		=> '{{PLURAL:$1|klesi|klesi}}',
+'currentevents'		=> 'nuzba ckupau',
+'currentevents-url'	=> 'nuzba ckupau',
+'help'			=> 'sidju ckupau',
+'mainpage'		=> 'ralju ckupau',
+'movedto'		=> 'te muvdu',
+'pagemovedtext'		=> '[[$1]] te muvdu [[$2]] le vreji',
+'portal'		=> 'bende ckupau',
+'randompage'		=> 'cunso ckupau'
+
+);
+
+?>
