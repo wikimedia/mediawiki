4068b1315356941bc1bb586c13add605b037ab41
Tim Starling <tstarling@users.mediawiki.org>
2006-10-03 05:44:34 +0000

    Obsolete, use initStats.php

--- maintenance/recount.sql
+++
@@ -1,8 +0,0 @@
--- 
--- Recalculate the article count
--- 
-
-SELECT @foo:=COUNT(*) FROM /*$wgDBprefix*/cur
-  WHERE cur_namespace=0 AND cur_is_redirect=0 AND cur_text like '%[[%';
-UPDATE /*$wgDBprefix*/site_stats SET ss_good_articles=@foo, ss_total_pages=-1, ss_users=-1, ss_admins=-1;
-
