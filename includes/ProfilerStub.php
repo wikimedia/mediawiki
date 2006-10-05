8b2aad209005129619602eed9fc9b553fdf06238
Brion Vibber <brion@users.mediawiki.org>
2006-10-05 21:20:23 +0000

    Revert to previous version using $wgDBname instead of wfWikiID(); functions are called at times that wfWikiID() is not defined, causing fatal errors when profiling is enabled

--- includes/ProfilerStub.php
+++ includes/ProfilerStub.php
@@ -4,20 +4,20 @@
 
 $haveProctitle=function_exists("setproctitle");
 function wfProfileIn( $fn = '' ) {
-	global $hackwhere, $haveProctitle;
+	global $hackwhere, $wgDBname, $haveProctitle;
 	if ($haveProctitle) {
 		$hackwhere[] = $fn;
-		setproctitle($fn . ' ['.wfWikiID().']');
+		setproctitle($fn . " [$wgDBname]");
 	}
 }
 function wfProfileOut( $fn = '' ) {
-	global $hackwhere, $haveProctitle;
+	global $hackwhere, $wgDBname, $haveProctitle;
 	if (!$haveProctitle)
 		return;
 	if (count($hackwhere))
 		array_pop($hackwhere);
 	if (count($hackwhere))
-		setproctitle($hackwhere[count($hackwhere)-1] . ' ['.wfWikiID().']');
+		setproctitle($hackwhere[count($hackwhere)-1] . " [$wgDBname]");
 }
 function wfGetProfilingOutput( $s, $e ) {}
 function wfProfileClose() {}
