576777c21e966ec0682a974defbe4704a016e183
Brion Vibber <brion@users.mediawiki.org>
2006-10-16 06:02:13 +0000

    bump style version forgotten in r17047 which made big reverts

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1030,7 +1030,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '12';
+$wgStyleVersion = '13';
 
 # Server-side caching:
 
