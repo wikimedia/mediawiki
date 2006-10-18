b32e5630715aa8116d3cf79b624e97abeee2f97d
Tim Starling <tstarling@users.mediawiki.org>
2006-10-18 09:22:33 +0000

    Added --show-output option to parserTests.php, to allow capture of complete output for test generation. Fixed various parser tests broken by tidy and incorrect capture method (i.e. web browser). Updated "Fuzz testing: Parser14-table", this appears to be fixed.

--- maintenance/parserTests.php
+++ maintenance/parserTests.php
@@ -26,22 +26,24 @@
 require('parserTests.inc');
 
 if( isset( $options['help'] ) ) {
-    echo <<<END
+    echo <<<ENDS
 MediaWiki $wgVersion parser test suite
-Usage: php parserTests.php [--quick] [--quiet] [--color[=(yes|no|light)]]
+Usage: php parserTests.php [--quick] [--quiet] [--show-output]
+                           [--color[=(yes|no|light)]]
                            [--regex=<expression>] [--file=<testfile>]
                            [--help]
 Options:
-  --quick  Suppress diff output of failed tests
-  --quiet  Suppress notification of passed tests (shows only failed tests)
-  --color  Override terminal detection and force color output on or off
-           'light' option is similar to 'yes' but with color for dark backgrounds
-  --regex  Only run tests whose descriptions which match given regex
-  --file   Run test cases from a custom file instead of parserTests.txt
-  --help   Show this help message
+  --quick          Suppress diff output of failed tests
+  --quiet          Suppress notification of passed tests (shows only failed tests)
+  --show-output    Show expected and actual output
+  --color          Override terminal detection and force color output on or off
+                   'light' option is similar to 'yes' but with color for dark backgrounds
+  --regex          Only run tests whose descriptions which match given regex
+  --file           Run test cases from a custom file instead of parserTests.txt
+  --help           Show this help message
 
 
-END;
+ENDS;
     exit( 0 );
 }
 
