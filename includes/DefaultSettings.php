87a29789ca047d04be0c3b0ea92fb5d44409abcf
Aryeh Gregor <simetrical@users.mediawiki.org>
2006-10-18 05:02:46 +0000

    * Finish fixing bug 3205, and bump up $wgStyleVersion. * Added $wgStyleVersion to a stylesheet call that brion missed. * Add informative comment to the top of common_rtl.css to the effect that it is really common to all skins, unlike common.css.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1030,7 +1030,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '13';
+$wgStyleVersion = '14';
 
 # Server-side caching:
 
