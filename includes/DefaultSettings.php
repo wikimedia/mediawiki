b96fbdea0daaef87dd05112f98532703cd27f153
Brion Vibber <brion@users.mediawiki.org>
2006-10-21 17:22:02 +0000

    bumping style version to match live hack. partially just testing to confirm svn mail is working

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1030,7 +1030,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '17';
+$wgStyleVersion = '18';
 
 # Server-side caching:
 
