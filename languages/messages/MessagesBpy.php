ec1364d0b828b68a417adbc2275f3b7f49515261
Rotem Liss <rotem@users.mediawiki.org>
2006-10-28 13:26:23 +0000

    (bug 7714) Update for Bishnupriya Manipuri language (bpy) interface

--- languages/messages/MessagesBpy.php
+++ languages/messages/MessagesBpy.php
@@ -1,11 +1,120 @@
 <?php
-/**
- * Bishnupriya
- * most speakers have Bengali as second language, use as default fallback
+/** Bishnupriya Manipuri (বিষ্ণুপ্রিয়া মণিপুরী)
  *
  * @package MediaWiki
  * @subpackage Language
  */
 
 $fallback = 'bn';
+
+$namespaceNames = array(
+	NS_SPECIAL        => 'বিশেষ',
+	NS_MAIN           => '',
+	NS_TALK           => 'য়্যারী',
+	NS_USER           => 'ব্যবহারকরেকুরা',
+	NS_USER_TALK      => 'ব্যবহারকরেকুরার_য়্যারী',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK   => '$1_য়্যারী',
+	NS_IMAGE          => 'ছবি',
+	NS_IMAGE_TALK     => 'ছবি_য়্যারী',
+	NS_MEDIAWIKI_TALK => 'MediaWiki_য়্যারী'
+);
+
+$messages = array(
+# Dates
+
+'sunday' => 'লামুইসিং',
+'monday' => 'নিংথৌকাপা',
+'tuesday' => 'লেইপাকপা',
+'wednesday' => 'ইনসাইনসা',
+'thursday' => 'সাকলসেন',
+'friday' => 'ইরেই',
+'saturday' => 'থাংচা',
+'january' => 'জানুয়ারী',
+'february' => 'ফেব্রুয়ারী',
+'march' => 'মার্চ',
+'april' => 'এপ্রিল',
+'may_long' => 'মে',
+'june' => 'জুন',
+'july' => 'জুলাই',
+'august' => 'আগস্ট',
+'september' => 'সেপ্টেম্বর',
+'october' => 'অক্টোবর',
+'november' => 'নভেম্বর',
+'december' => 'ডিসেম্বর',
+'jan' => 'জানু',
+'feb' => 'ফেব্রু',
+'mar' => 'মার্চ',
+'apr' => 'এপ্রিল',
+'may' => 'মে',
+'jun' => 'জুন',
+'jul' => 'জুলাই',
+'aug' => 'আগস্ট',
+'sep' => 'সেপ্টে',
+'oct' => 'অক্টো',
+'nov' => 'নভে',
+'dec' => 'ডিসে',
+
+# Bits of text used by many pages:
+#
+
+'mainpage'    => 'পয়লা পাতা',
+'about'     => 'বৃত্তান্ত',
+'aboutsite'      => '{{SITENAME}}র বৃত্তান্ত', // TODO: grammar
+'aboutpage'   => '{{ns:project}}:বৃত্তান্ত',
+'help'      => 'পাংলাক',
+'helppage'    => '{{ns:project}}:পাংলাকপা',
+'bugreports'  => 'লাল বিবরণী',
+'bugreportspage' => '{{ns:project}}:লাল_বিবরণী',
+'faq'     => 'প্রশ্নরজুয়াপ',
+'faqpage'   => '{{ns:project}}:প্রশ্নরজুয়াপ',
+'edithelp'    => 'সম্পাদনা পাংলাক',
+'edithelppage'  => '{{ns:project}}:কিসাদে_পাতা_আহান_সম্পাদনা_করানি',
+'cancel'    => 'বাতিল করানি',
+'qbfind'    => 'বিসারিয়া চা',
+'qbbrowse'    => 'বুলিয়া চা',
+'qbedit'    => 'সম্পাদনা করানি',
+'qbpageoptions' => 'পাতা এহানর সারুক',
+'qbpageinfo'  => 'পাতা এহানর পৌ',
+'qbmyoptions' => 'মর পছন',
+'mypage'    => 'মর পাতাহান',
+'mytalk'    => 'মর য়্যারি-পরি',
+'currentevents' => 'আজিকালিকার ঘটনা',
+'errorpagetitle' => 'লাল',
+'returnto'    => 'আলথকে যাগা $1.',
+'whatlinkshere' => 'যে পাতাহানিত্ত এহানাত মিলাপ আসে',
+'help'      => 'পাংলাক',
+'search'    => 'বিসারিয়া চা',
+'searchbutton'    => 'বিসারিয়া চা',
+'go'    => 'হাত',
+'searcharticle'    => 'হাত',
+'history'   => 'পাতা এহানর ইতিহাস',
+'printableversion' => 'ছাপানি একরব সংস্করণ',
+'editthispage'  => 'পাতা এহান সম্পাদনা করিক',
+'deletethispage' => 'পাতা এহান মুছে বেলিক',
+'protectthispage' => 'পাতা এহান ইতু করিক',
+'unprotectthispage' => 'পাতা এহানর ইতু এরাদিক',
+'newpage' => 'নুৱা পাতা',
+'talkpage'    => 'পাতা এহান্ন অতারিক',
+'articlepage' => 'নিবন্ধ চেইক',
+'userpage' => 'ব্যাবহারকরেকুরার পাতাহান চেইক',
+'projectpage' => 'মেটা-পাতাহান চেইক',
+'imagepage' =>  'ছবির পাতাহান চেইক',
+'viewtalkpage' => 'য়্যারীর পাতাহান চেইক',
+'otherlanguages' => 'আরআর ঠারে',
+'redirectedfrom' => '($1 -ত্ত পাকদিয়া আহিল)',
+'lastmodifiedat'  => 'পাতা এহানর লমিলগা পতানিহান $2, $1.',
+'viewcount'   => 'পাতা এহান $1 মাউ চানা ইল।',
+'nbytes'    => '$1 বাইট',
+'ok'      => 'চুমিসে',
+'retrievedfrom' => '\'$1\' -ত্ত আনানি অসে',
+'newmessageslink' => 'নুৱা পৌ',
+'editsection'=>'সম্পাদনা করিক',
+'editold'=>'সম্পাদনা করিক',
+'toc' => 'মেথেল',
+'showtoc' => 'দেহানি',
+'hidetoc' => 'গুসানি',
+
+);
+
 ?>
