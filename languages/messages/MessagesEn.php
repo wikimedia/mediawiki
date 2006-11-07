af93d96b8790b363e23016e59299704deb74927c
Aryeh Gregor <simetrical@users.mediawiki.org>
2006-11-07 05:40:38 +0000

    (bug 7826) Fix another typo in an English message, and add to RELEASE-NOTES.

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1492,7 +1492,7 @@ You can narrow down the view by selecting a log type, the user name, or the affe
 'allpagesnext'		=> 'Next',
 'allpagessubmit'	=> 'Go',
 'allpagesprefix'	=> 'Display pages with prefix:',
-'allpagesbadtitle'	=> 'The given page title was invalid or had an inter-language or inter-wiki prefix. It may contain one more characters which cannot be used in titles.',
+'allpagesbadtitle'	=> 'The given page title was invalid or had an inter-language or inter-wiki prefix. It may contain one or more characters which cannot be used in titles.',
 
 # Special:Listusers
 'listusersfrom' => 'Display users starting at:',
