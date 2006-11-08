4864b286924d5af971dcac6ff9bff92316f58c3a
Andrew Garrett <werdna@users.mediawiki.org>
2006-11-08 09:54:06 +0000

    * (bug 5149) When autoblocks are enabled, retroactively apply an autoblock to the most recently used IP of a user when they are blocked. * Add an index on (rc_user_text,rc_timestamp) on the recentchanges table. This will make CheckUser.php and the new retroactive autoblock functionality faster.

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -804,7 +804,8 @@ CREATE TABLE /*$wgDBprefix*/recentchanges (
   INDEX rc_cur_id (rc_cur_id),
   INDEX new_name_timestamp (rc_new,rc_namespace,rc_timestamp),
   INDEX rc_ip (rc_ip),
-  INDEX rc_ns_usertext (rc_namespace, rc_user_text)
+  INDEX rc_ns_usertext (rc_namespace, rc_user_text),
+  INDEX rc_user_text (rc_user_text, rc_timestamp)
 
 ) TYPE=InnoDB;
 
