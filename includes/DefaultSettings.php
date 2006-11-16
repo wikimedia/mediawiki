90bfc33afdb37e3443a63e0a0d4e56d8026d5a64
Aryeh Gregor <simetrical@users.mediawiki.org>
2006-11-16 03:40:11 +0000

    (bug 7932) Make sure that edit toolbar clears floats so it appears correctly.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1037,7 +1037,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '27';
+$wgStyleVersion = '28';
 
 # Server-side caching:
 
