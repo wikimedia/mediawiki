0b5b69de79c25ca659e25d063b81267aa8f695f4
Tim Starling <tstarling@users.mediawiki.org>
2006-11-21 12:12:20 +0000

    bump style version for wikibits.js change

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1048,7 +1048,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '28';
+$wgStyleVersion = '29';
 
 # Server-side caching:
 
