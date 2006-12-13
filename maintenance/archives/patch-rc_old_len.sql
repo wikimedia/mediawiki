c62e068b274b032fef6ff98d84fd611cf525278a
Leon Weber <leon@users.mediawiki.org>
2006-12-13 20:08:02 +0000

    * Made the show change size function work on page moves, page creations, and   log entries. * Also fixed it in the javascript recentchanges. * Moved the value behind the page title to ensure the columns are still lined up * Merged both sql patches into one. * Set the default for both columns to NULL to make it work on edits before the db   update.

--- maintenance/archives/patch-rc_old_len.sql
+++
@@ -1,9 +0,0 @@
---
--- patch-rc_old_len.sql
--- Adds a row to recentchanges to hold the text size before the edit
--- 2006-12-03
---
-
-ALTER TABLE /*$wgDBprefix*/recentchanges
-	ADD COLUMN rc_old_len int(10) default 0;
-
