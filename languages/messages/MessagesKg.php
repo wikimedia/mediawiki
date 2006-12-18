9483aa0f37f1d3b0cd484bad009087183d94b679
Rotem Liss <rotem@users.mediawiki.org>
2006-12-18 10:36:27 +0000

    (bug 8295) Add Kongo localisation (kg) (real commit)

---
+++ languages/messages/MessagesKg.php
@@ -0,0 +1,99 @@
+<?php
+/** Kongo (Kikongo)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+$messages = array(
+
+# dates
+'sunday'                => 'Lumingu',
+'monday'                => 'Kimosi',
+'tuesday'               => 'Kizole',
+'wednesday'             => 'Kitatu',
+'thursday'              => 'Kiya',
+'friday'                => 'Kitanu',
+'saturday'              => 'Sabala',
+
+# months
+'january'               => 'ngonda ya ntete',
+'february'              => 'ngonda ya zole',
+'march'                 => 'ngonda ya tatu',
+'april'                 => 'ngonda ya iya',
+'may_long'              => 'ngonda ya tanu',
+'june'                  => 'ngonda ya sambanu',
+'july'                  => 'ngonda ya nsambwadi',
+'august'                => 'ngonda ya nana',
+'september'             => 'ngonda ya uvwa',
+'october'               => 'ngonda ya kumi',
+'november'              => 'ngonda ya kumi na mosi',
+'december'              => 'ngonda ya kumi na zole',
+
+# Bits of text used by many pages:
+'categories'            => 'Bakalasi',
+'pagecategories'        => '{{PLURAL:$1|Kalasi|Bakalasi}}',
+'category_header'       => 'Mikanda na kalasi "$1"',
+'mainpage'              => 'Lukaya ya mfumu',
+
+'article'               => 'Pagina contenta continens',
+'help'                  => 'Nsadisa',
+'mytalk'                => 'Disolo ya munu',
+'currentevents'         => 'Mambu ya mpa',
+'currentevents-url'     => 'Mambu ya mpa',
+'search'                => 'Sosa',
+'searchbutton'          => 'Sosa',
+'searcharticle'         => 'Kwenda',
+'history'               => 'Bampaluka ya lukaya',
+'history_short'         => 'Bampaluka',
+'edit'                  => 'Sonika',
+'talk'                  => 'Disolo',
+'toolbox'               => 'Bisadilu',
+'otherlanguages'        => 'Bandinga ya nkaka',
+'redirectedfrom'        => '(Balulama tuka $1)',
+'retrievedfrom'         => 'Receptum de "$1"',
+
+# nstab
+'nstab-main'            => 'Mukanda',
+'nstab-category'        => 'Kalasi',
+
+
+'yourlanguage'          => 'Ndinga:',
+
+# Login and logout pages
+'loginlanguagelabel'    => 'Ndinga: $1',
+'logout'                => 'Basika',
+'userlogout'            => 'Basika',
+
+'histfirst'             => 'Ya ntete',
+'histlast'              => 'Ya nsuka',
+
+'prevn'                 => 'biyita $1',
+'nextn'                 => 'bilandi $1',
+'viewprevnext'          => 'Mona ($1) ($2) ($3).',
+
+'powersearch'           => 'Sosa',
+
+'mypreferences'         => 'Konte ya munu',
+
+# Recentchanges
+'recentchanges'         => 'Bampaluka ya mpa',
+
+
+'ncategories'           => '{{PLURAL:$1|kalasi|bakalasi}} $1',
+
+'randompage'            => 'Lukaya na kintulumukini',
+'mycontris'             => 'Makabu ya munu',
+'whatlinkshere'         => 'Balukaya ke songa awa',
+
+'categoryarticlecount'  => '{{PLURAL:$1|Mukanda mosi|Mikanda $1 }} ni na kalasi yayi.', //
+
+# Table pager
+'table_pager_first'     => 'Lukaya ya ntete',
+'table_pager_last'      => 'Lukaya ya nsuka',
+'table_pager_next'      => 'Lukaya ya kulanda',
+'table_pager_prev'      => 'Lukaya ya kuyita',
+
+);
+
+?>
