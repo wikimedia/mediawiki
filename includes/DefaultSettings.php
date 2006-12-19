040adeffb1a148c04fde0c076dc9d08f34efab74
Brion Vibber <brion@users.mediawiki.org>
2006-12-19 22:21:45 +0000

    * (bug 8326) Fix regression in thumb styles of cached content

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1041,7 +1041,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '36';
+$wgStyleVersion = '37';
 
 
 # Server-side caching:
