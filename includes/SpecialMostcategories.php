e783c5d41f2ac6a4d334e81fb0d5cf74e7b1a267
Rob Church <robchurch@users.mediawiki.org>
2006-12-23 14:09:38 +0000

    (bug 7612) Remove superfluous link to Special:Categories from result items on Special:Mostcategories

--- includes/SpecialMostcategories.php
+++ includes/SpecialMostcategories.php
@@ -37,20 +37,11 @@ class MostcategoriesPage extends QueryPage {
 	}
 
 	function formatResult( $skin, $result ) {
-		global $wgContLang, $wgLang;
-
-		$nt = Title::makeTitle( $result->namespace, $result->title );
-		$text = $wgContLang->convert( $nt->getPrefixedText() );
-
-		$plink = $skin->makeKnownLink( $nt->getPrefixedText(), $text );
-
-		$nl = wfMsgExt( 'ncategories', array( 'parsemag', 'escape' ),
-			$wgLang->formatNum( $result->value ) );
-
-		$nlink = $skin->makeKnownLink( $wgContLang->specialPage( 'Categories' ),
-			$nl, 'article=' . $nt->getPrefixedURL() );
-
-		return wfSpecialList($plink, $nlink);
+		global $wgLang;
+		$title = Title::makeTitleSafe( $result->namespace, $result->title );
+		$count = wfMsgExt( 'ncategories', array( 'parsemag', 'escape' ), $wgLang->formatNum( $result->value ) );
+		$link = $skin->makeKnownLinkObj( $title, $title->getText() );
+		return wfSpecialList( $link, $count );
 	}
 }
 
