fafbe2d25496522137a2dc58cf2d2a95e107580e
Aryeh Gregor <simetrical@users.mediawiki.org>
2006-12-27 01:17:55 +0000

    Revert the conditional un-including of ajax.js from last commit after conversation with brion on IRC.  Will someone think of the poor extensions!

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -535,10 +535,7 @@ class OutputPage {
 		$sk = $wgUser->getSkin();
 
 		if ( $wgUseAjax ) {
-			// No point in enabling it if watching is the only enabled thing and the user isn't logged in
-			if( $wgAjaxSearch || ( $wgAjaxWatch && $wgUser->isLoggedIn() )) {
-				$this->addScript( "<script type=\"{$wgJsMimeType}\" src=\"{$wgStylePath}/common/ajax.js?$wgStyleVersion\"></script>\n" );
-			}
+			$this->addScript( "<script type=\"{$wgJsMimeType}\" src=\"{$wgStylePath}/common/ajax.js?$wgStyleVersion\"></script>\n" );
 			if( $wgAjaxSearch ) {
 				$this->addScript( "<script type=\"{$wgJsMimeType}\" src=\"{$wgStylePath}/common/ajaxsearch.js\"></script>\n" );
 				$this->addScript( "<script type=\"{$wgJsMimeType}\">hookEvent(\"load\", sajax_onload);</script>\n" );
