f31e482270a0ac0f2a696cc811db4be5cd989477
Greg Sabino Mullane <greg@users.mediawiki.org>
2007-01-03 16:36:05 +0000

    Only do stripforsearch if we are using MySQL, per bug # 8470

--- languages/Language.php
+++ languages/Language.php
@@ -954,6 +954,11 @@ class Language {
 	 * @return string
 	 */
 	function stripForSearch( $string ) {
+		global $wgDBtype;
+		if ( $wgDBtype != 'mysql' ) {
+			return $string;
+		}
+
 		# MySQL fulltext index doesn't grok utf-8, so we
 		# need to fold cases and convert to hex
 
