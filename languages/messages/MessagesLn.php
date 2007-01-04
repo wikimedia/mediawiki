ec5a5c1571c45402e20bc43db191bd9428d8de86
Rob Church <robchurch@users.mediawiki.org>
2007-01-04 23:16:29 +0000

    (bug 8485) Correct Lingala number formatting

--- languages/messages/MessagesLn.php
+++ languages/messages/MessagesLn.php
@@ -1,10 +1,17 @@
 <?php
+
 /**
+ * Lingala language
+ *
  * @package MediaWiki
  * @subpackage language
  */
 
 $linkPrefixExtension = true;
 
+# Same as the French (bug 8485)
+$separatorTransformTable = array( ',' => "\xc2\xa0", '.' => ',' );
+
 $messages = array( 'linkprefix' => '/^(.*?)([a-zA-Z\x80-\xff]+)$/sD' );
+
 ?>
