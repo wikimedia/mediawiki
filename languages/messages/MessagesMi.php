ac858e7e7ffbf5938db59a752d8778a429b0d1d5
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-01-06 15:59:28 +0000

    * Committed some message files which have been hanging around

---
+++ languages/messages/MessagesMi.php
@@ -0,0 +1,111 @@
+<?php
+/** Maori (Māori)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+$messages = array(
+# Dates
+'sunday'    => 'Rātapu',
+'monday'    => 'Rāhina',
+'tuesday'   => 'Rātū',
+'wednesday' => 'Rāapa',
+'thursday'  => 'Rāpare',
+'friday'    => 'Rāmere',
+'saturday'  => 'Rāhoroi',
+'january'   => 'Kohi-tātea',
+'february'  => 'Hui-tanguru',
+'march'     => 'Poutū-te-rangi',
+'april'     => 'Paenga-whāwhā',
+'may_long'  => 'Haratua',
+'june'      => 'Pipiri',
+'july'      => 'Hōngongoi',
+'august'    => 'Here-turi-kōkā',
+'september' => 'Mahuru',
+'october'   => 'Whiringa-ā-nuku',
+'november'  => 'Whiringa-ā-rangi',
+'december'  => 'Hakihea',
+
+'mainpage' => 'Hau Kāinga',
+
+'portal'         => 'Tomokanga hapori',
+'portal-url'     => 'Project:Tomokanga hapori',
+'help'           => 'Whakamārama',
+'sitesupport'    => 'Koha',
+'cancel'         => 'Whakakore',
+'qbspecialpages' => 'Whārangi motuhake',
+'mytalk'         => 'Karere mōku',
+'navigation'     => 'Huarahi',
+
+'currentevents'     => 'Kōrero',
+'currentevents-url' => 'Project:Kōrero',
+
+'search'           => 'Rapua',
+'go'               => 'Haere',
+'history'          => 'Kōrero Nehe',
+'history_short'    => 'Tuhinga/kaituhi',
+'printableversion' => 'Tāia',
+'permalink'        => 'Hononga toitū',
+'edit'             => 'Whakatika',
+'delete'           => 'tangohia',
+'protect'          => 'Whakangungua',
+'talk'             => 'Kōrerorero',
+'toolbox'          => 'Pouaka utauta',
+
+'toc'     => 'Rārangi kōrero',
+'showtoc' => 'whakakite',
+'hidetoc' => 'hunaia',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'     => 'tuhi pānui',
+'nstab-template' => 'papa tauira',
+'nstab-help'     => 'Āwhina',
+'nstab-category' => 'Wāhanga',
+
+# Login and logout pages
+'userlogout' => 'Whakarere',
+
+# Edit pages
+'savearticle' => 'Tiaki',
+'showdiff'    => 'Tiro rerekētanga',
+
+# Preferences page
+'preferences' => 'Kōwhiringa',
+
+# Recent changes
+'recentchanges' => 'Rerekētanga hōu',
+'hide'          => 'Hunaia',
+'show'          => 'Whakaaturia',
+
+# Upload
+'upload' => 'Tuku atu',
+
+# Miscellaneous special pages
+'allpages'            => 'Ngā whārangi katoa',
+'randompage'          => 'Tipihaere',
+'specialpages'        => 'Whārangi motuhake',
+'recentchangeslinked' => 'Rerekētanga pū tahi',
+'move'                => 'Nekehia',
+
+# Special:Allpages
+'allpagessubmit' => 'Haere',
+
+# Watchlist
+'watchlist' => 'Rārangi mātaki',
+'watch'     => 'Mātaki',
+'unwatch'   => 'kāti te mātaki',
+
+# Contributions
+'contributions' => 'Ngā mahi a tēnei mema',
+'mycontris'     => 'āku mahi',
+
+# What links here
+'whatlinkshere' => 'Ngā hononga mai',
+
+# Namespace 8 related
+'allmessages' => 'Pānui pūnaha',
+
+);
+
+?>
---
+++ languages/messages/MessagesMt.php
@@ -0,0 +1,99 @@
+<?php
+/** Maltese (Malti)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ */
+
+$messages = array(
+'mainpage' => 'Paġna prinċipali',
+
+'portal'          => 'Portal tal-komunità',
+'portal-url'      => 'Project:Portal tal-komunità',
+'aboutsite'       => 'Dwar {{SITENAME}}',
+'help'            => 'Għajnuna',
+'sitesupport'     => 'Donazzjonijiet',
+'sitesupport-url' => 'Project:Donazzjonijiet',
+'mytalk'          => 'Diskussjonijiet tiegħi',
+'navigation'      => 'Navigazzjoni',
+
+'currentevents'     => 'Ġrajjiet kurrenti',
+'currentevents-url' => 'Project:Ġrajjiet kurrenti',
+
+'disclaimers'      => 'Ċaħdiet',
+'search'           => 'Fittex',
+'go'               => 'Mur',
+'history_short'    => 'Kronoloġija',
+'printableversion' => 'Verżjoni għall-ipprintjar',
+'permalink'        => 'Link permanenti',
+'edit'             => 'Editja',
+'delete'           => 'Ħassar',
+'protect'          => 'Ipproteġi',
+'unprotect'        => 'Tibqax tipproteġi',
+'personaltools'    => 'Strumenti personali',
+'talk'             => 'Diskussjoni',
+'toolbox'          => 'Għodda',
+
+'youhavenewmessages' => 'Għandek $1 ($2).',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'      => 'Artiklu',
+'nstab-mediawiki' => 'Messaġġ',
+
+# General errors
+'viewsource' => 'Ara l-fonti',
+
+# Login and logout pages
+'userlogin'  => 'Idħol jew oħloq kont ġdid',
+'userlogout' => 'Oħroġ',
+
+# Edit pages
+'savearticle'    => 'Salva l-paġna',
+'editing'        => 'Qed jiġi editjat l-artiklu $1',
+'editingcomment' => 'Edit $1 (kumment)',
+
+# Preferences page
+'preferences' => 'Preferenzi',
+
+# Recent changes
+'recentchanges' => 'Tibdil riċenti',
+
+# Upload
+'upload' => "Tella' file",
+
+# Miscellaneous special pages
+'allpages'            => 'Il-paġni kollha',
+'randompage'          => 'Paġna kwalunkwe',
+'specialpages'        => 'Paġni speċjali',
+'recentchangeslinked' => 'Tibdil relatat',
+'move'                => 'Mexxi',
+
+'version' => 'Verżjoni',
+
+# E-mail user
+'emailuser' => 'Ibgħat e-mail lil dan l-utent',
+
+# Watchlist
+'watchlist' => "Lista t'osservazzjoni tiegħi",
+'watch'     => 'Segwi',
+'unwatch'   => 'Tibqax issegwi',
+
+# Undelete
+'undelete' => 'Irkupra l-paġna mħassra',
+
+# Contributions
+'contributions' => 'Kontribuzzjonijiet tal-user',
+'mycontris'     => 'Kontribuzzjonijiet tiegħi',
+
+# What links here
+'whatlinkshere' => 'Links li jwasslu hawn',
+
+# Block/unblock
+'blockip' => 'Ibblokkja l-utent',
+
+# Namespace 8 related
+'allmessages' => 'Il-messaġġi kollha tas-sistema',
+
+);
+
+?>
