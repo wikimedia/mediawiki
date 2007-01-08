02b8bebfc3b672f6030ae56d2c0acca999a15a4e
Leon Weber <leon@users.mediawiki.org>
2007-01-08 20:43:27 +0000

    Reverting the accidently commited feature nobody wants to see.

--- includes/SpecialUnusedimages.php
+++ includes/SpecialUnusedimages.php
@@ -40,10 +40,6 @@ class UnusedimagesPage extends QueryPage {
 		}
 	}
 
-	function getOrder() {
-		return ' ORDER BY img_name';
-	}
-
 	function formatResult( $skin, $result ) {
 		global $wgLang, $wgContLang;
 		$title = Title::makeTitle( NS_IMAGE, $result->title );
