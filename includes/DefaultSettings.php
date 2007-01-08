cf53f41c0d420db75a5d94381ed001758409ad95
Brion Vibber <brion@users.mediawiki.org>
2007-01-08 12:05:25 +0000

    bump to 1.10alpha

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -32,7 +32,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.9alpha';
+$wgVersion			= '1.10alpha';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
