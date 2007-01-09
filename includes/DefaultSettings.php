73c7bd9e24caaac35a3c229588d4faaa66262f9a
Brion Vibber <brion@users.mediawiki.org>
2007-01-09 07:14:59 +0000

    Merge r19013 and r19015 from trunk: XSS fix with $wgUseAjax enabled

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -32,7 +32,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.9.0rc1';
+$wgVersion			= '1.9.0rc2';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
