69f5d0a9ea5661817a5280dede36be8c44349058
Brion Vibber <brion@users.mediawiki.org>
2007-01-09 07:32:00 +0000

    xss ajax fix

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -32,7 +32,7 @@ require_once( 'includes/SiteConfiguration.php' );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.6.8';
+$wgVersion			= '1.6.9';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
