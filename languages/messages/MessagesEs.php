wikimedia/mediawiki

commit c6d1d3d0ab6b832c23f3249b95855878227f89af

Spanish Internationalisation; patch by Titoxd
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@19053 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/messages/MessagesEs.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /** Spanish (Español)
   *
   * @bug 4401
@@ -528,6 +528,7 @@ $2 Listar redirecciones   Buscar $3 $9',
 'searchdisabled'        => 'La búsqueda en {{SITENAME}} está temporalmente deshabilitada. Mientras tanto puedes buscar mediante estos buscadores externos, pero ten en cuenta que sus índices del contenido de {{SITENAME}} pueden estar desactualizados.',
 'blanknamespace'        => '(Principal)',
 'preferences'           => 'Preferencias',
+'mypreferences'	=> 'Mis preferencias',
 'prefsnologin'          => 'No has entrado',
 'prefsnologintext'      => 'Debes [[Special:Userlogin|entrar]] para cambiar las preferencias de usuario.',
 'prefsreset'            => 'Las preferencias han sido repuestas a sus valores almacenados.',

commit 1e31a9b998c5b361f3e74a7ae56e88792be4dea5

Fix line-ending breakage from r19053

languages/messages/MessagesEs.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /** Spanish (Español)
   *
   * @bug 4401