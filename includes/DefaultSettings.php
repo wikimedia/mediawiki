0f60d3f9314fb536ce3f7b5d060aca5bd63fdd70
Aryeh Gregor <simetrical@users.mediawiki.org>
2007-01-14 23:48:42 +0000

    * Don't iterate through all elements to update tooltips/accesskeys, iterate through as few as possible while still guaranteeing that you'll add all accesskeys * Only call updateTooltipAccessKeys from addPortletLink if necessary * Give search box a tooltip in Monobook

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1105,7 +1105,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '46';
+$wgStyleVersion = '47';
 
 
 # Server-side caching:
