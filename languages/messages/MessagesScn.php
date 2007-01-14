44d7cbb0aac28e7b472dc05dc8e11f9496c0c7ac
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-01-14 14:44:21 +0000

    * Committing this, has been idling around too long

---
+++ languages/messages/MessagesScn.php
@@ -0,0 +1,85 @@
+<?php
+/** Sicilian (Sicilianu)
+ *
+ * @package MediaWiki
+ * @subpackage Language
+ *
+ * @author Niklas Laxström
+ */
+
+//$fallback = 'it';
+
+$messages = array(
+'mainpage' => 'Pàggina principali',
+
+'portal'          => 'Porta dâ cumunità',
+'portal-url'      => 'Project:Porta dâ cumunità',
+'help'            => 'Aiutu',
+'sitesupport'     => 'Dunazzioni',
+'sitesupport-url' => 'Project:Dunazzioni',
+'mytalk'          => 'discursu miu',
+'navigation'      => 'Navigazzioni',
+
+'currentevents'     => 'Nutizzî',
+'currentevents-url' => 'Project:Nutizzî',
+
+'search'           => 'Trova',
+'history_short'    => 'storia',
+'printableversion' => 'Virsioni stampàbbili',
+'permalink'        => 'Liami pirmanenti',
+'edit'             => 'cancia',
+'delete'           => 'elìmina',
+'protect'          => 'pruteggi',
+'talk'             => 'discussioni',
+'toolbox'          => 'Strummenta',
+'otherlanguages'   => 'Àutri lingui',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'      => 'artìculu',
+'nstab-special'   => 'spiciali',
+'nstab-mediawiki' => 'missagiu',
+
+# General errors
+'viewsource' => 'Talìa la fonti',
+
+# Login and logout pages
+'userlogin'  => 'Riggìstrati o trasi',
+'userlogout' => 'Nesci',
+
+# Edit pages
+'savearticle' => 'sarva la pàggina',
+
+# Preferences page
+'preferences' => 'prifirenzi',
+
+# Recent changes
+'recentchanges' => 'Ùrtimi canciamenti',
+
+# Miscellaneous special pages
+'allpages'            => 'Tutti li paggini',
+'randompage'          => 'Na pàggina ammuzzu',
+'specialpages'        => 'Pàggini spiciali',
+'recentchangeslinked' => 'Canciamenti culligati',
+
+'version' => 'virsioni',
+
+# E-mail user
+'emailuser' => "Manna n'imail a stu utenti",
+
+# Watchlist
+'watchlist' => 'Lista taliata mia',
+'watch'     => 'talìa',
+
+# Contributions
+'contributions' => 'cuntribbuti',
+'mycontris'     => 'Li mei cuntribbuti',
+
+# What links here
+'whatlinkshere' => 'Chi punta ccà',
+
+# Namespace 8 related
+'allmessages' => 'Missaggi di sistema',
+
+);
+
+?>
