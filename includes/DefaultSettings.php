d7c506bc31b1fca413bc067735a4d702a10a8300
Ilmari Karonen <vyznev@users.mediawiki.org>
2007-01-15 04:28:07 +0000

    Make setupCheckboxShiftClick() process all checkboxes, not just those in ULs.  This not only makes it more versatile, but it's faster too, since checkboxes tend to be much less common than list items.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1105,7 +1105,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '47';
+$wgStyleVersion = '48';
 
 
 # Server-side caching:
