eed9a3aa46828bf6b730c8a7f46f1fc21ce4592e
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-01-17 23:44:43 +0000

    * Escape page title

--- includes/User.php
+++ includes/User.php
@@ -2478,7 +2478,7 @@ class User {
 		if( $title ) {
 			global $wgUser;
 			$sk = $wgUser->getSkin();
-			return $sk->makeLinkObj( $title, $text );
+			return $sk->makeLinkObj( $title, htmlspecialchars( $text ) );
 		} else {
 			return $text;
 		}
