b06c810385b96aeedddb71e5bdf3741f56f7d60a
Rob Church <robchurch@users.mediawiki.org>
2007-01-20 13:05:00 +0000

    (bug 8708) Namespace translations for Zealandic language

---
+++ languages/messages/MessagesZea.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Zealandic language (zea)
+ *
+ * @package MediaWiki
+ * @subpackage Internationalisation
+ * @author Rob Church <robchur@gmail.com>
+ */
+ 
+/**
+ * Namespace names
+ * (bug 8708)
+ */
+$namespaceNames = array(
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Speciaol',
+	NS_MAIN	            => '',
+	NS_TALK	            => 'Overleg',
+	NS_USER             => 'Gebruker',
+	NS_USER_TALK        => 'Overleg_Gebruker',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK     => 'Overleg_$1',
+	NS_IMAGE            => 'Plaetje',
+	NS_IMAGE_TALK       => 'Overleg_Plaetje',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Overleg_MediaWiki',
+	NS_TEMPLATE         => 'Sjabloon',
+	NS_TEMPLATE_TALK    => 'Overleg_Sjabloon',
+	NS_HELP             => 'Ulpe',
+	NS_HELP_TALK        => 'Overleg_Ulpe',
+	NS_CATEGORY         => 'Categorie',
+	NS_CATEGORY_TALK    => 'Overleg_Categorie',
+);
+
+?>
