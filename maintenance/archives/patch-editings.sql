f0177106f478d28a558dd55f65ec51703f5485d9
Leon Weber <leon@users.mediawiki.org>
2007-01-20 21:28:25 +0000

    Added missing comma in hashar's updater

--- maintenance/archives/patch-editings.sql
+++ maintenance/archives/patch-editings.sql
@@ -9,6 +9,6 @@ CREATE TABLE /*$wgDBprefix*/editings (
   `editings_started` char(14) NOT NULL,
   `editings_touched` char(14) NOT NULL,
   PRIMARY KEY  (`editings_page`,`editings_user`),
-  KEY `editings_page` (`editings_page`)
+  KEY `editings_page` (`editings_page`),
   KEY `editings_page_started` (`editings_page`,`editings_user`,`editings_started`)
 ) TYPE=InnoDB;
