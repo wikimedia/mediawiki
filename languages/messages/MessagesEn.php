c1d90ff1ef0616daeb9b70609d668b0798745adb
Rotem Liss <rotem@users.mediawiki.org>
2007-01-22 09:11:37 +0000

    Mistake.

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1738,9 +1738,6 @@ Last edit was by [[User:$3|$3]] ([[User talk:$3|Talk]]).",
 this action has been canceled as a precaution against session hijacking.
 Please hit "back" and reload the page you came from, then try again.',
 'protectlogpage' => 'Protection log',
-'blocklogtext'	=> 'This is a log of user blocking and unblocking actions. Automatically
-blocked IP addresses are not listed. See the [[Special:Ipblocklist|IP block list]] for
-the list of currently operational bans and blocks.',
 'protectlogtext' => "Below is a list of page locks and unlocks. See the [[Special:Protectedpages|protected pages list]] for the list of currently operational page protections.",
 'protectedarticle' => 'protected "[[$1]]"',
 'unprotectedarticle' => 'unprotected "[[$1]]"',
