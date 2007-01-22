a433c3d045d139b9b9dc1e76fe52a46a9a254a48
Brion Vibber <brion@users.mediawiki.org>
2007-01-22 17:59:43 +0000

    * (bug 8719) Firefox release notes lie! Fix tooltips for Firefox 2 on x11;   accesskeys default settings appear to be same as Windows.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1109,7 +1109,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '51';
+$wgStyleVersion = '52';
 
 
 # Server-side caching:
