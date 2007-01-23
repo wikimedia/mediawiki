0d5212f03558b330e48fe4a216bcc343465d3bc5
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-01-23 17:38:25 +0000

    * (bug 8739) MessagesMr.php file (Marathi)

---
+++ languages/messages/MessagesMr.php
@@ -0,0 +1,36 @@
+<?php
+
+$namespaceNames = array(
+	NS_MEDIA          => 'Media',
+	NS_SPECIAL        => 'विशेष',
+	NS_MAIN           => '',
+	NS_TALK           => 'चर्चा',
+	NS_USER           => 'सदस्य',
+	NS_USER_TALK      => 'सदस्य_चर्चा',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK   => '$1_चर्चा',
+	NS_IMAGE          => 'चित्र',
+	NS_IMAGE_TALK     => 'चित्र_चर्चा',
+	NS_MEDIAWIKI      => 'MediaWiki',
+	NS_MEDIAWIKI_TALK => 'MediaWiki_talk',
+	NS_TEMPLATE       => 'साचा',
+	NS_TEMPLATE_TALK  => 'साचा_चर्चा',
+	NS_CATEGORY       => 'वर्ग',
+	NS_CATEGORY_TALK  => 'वर्ग_चर्चा',
+);
+
+$digitTransformTable = array(
+	"0" => "०",
+	"1" => "१",
+	"2" => "२",
+	"3" => "३",
+	"4" => "४",
+	"5" => "५",
+	"6" => "६",
+	"7" => "७",
+	"8" => "८",
+	"9" => "९"
+);
+$linkTrail = "/^([a-z]+)(.*)\$/sDu";
+
+?>
