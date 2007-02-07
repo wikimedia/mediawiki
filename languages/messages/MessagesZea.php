a2608187f04f0e9082293c3d061d8bec15e75bbe
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-02-07 21:31:28 +0000

    * Fix for 8708

--- languages/messages/MessagesZea.php
+++ languages/messages/MessagesZea.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Zealandic language (zea)
+ * Zealandic (Zeêuws)
  *
  * @addtogroup Language
  * @author Rob Church <robchur@gmail.com>
@@ -14,22 +13,22 @@
 $namespaceNames = array(
 	NS_MEDIA            => 'Media',
 	NS_SPECIAL          => 'Speciaol',
-	NS_MAIN	            => '',
-	NS_TALK	            => 'Overleg',
+	NS_MAIN             => '',
+	NS_TALK             => 'Overleg',
 	NS_USER             => 'Gebruker',
-	NS_USER_TALK        => 'Overleg_Gebruker',
+	NS_USER_TALK        => 'Overleg_gebruker',
 	# NS_PROJECT set by $wgMetaNamespace
 	NS_PROJECT_TALK     => 'Overleg_$1',
 	NS_IMAGE            => 'Plaetje',
-	NS_IMAGE_TALK       => 'Overleg_Plaetje',
+	NS_IMAGE_TALK       => 'Overleg_plaetje',
 	NS_MEDIAWIKI        => 'MediaWiki',
 	NS_MEDIAWIKI_TALK   => 'Overleg_MediaWiki',
 	NS_TEMPLATE         => 'Sjabloon',
-	NS_TEMPLATE_TALK    => 'Overleg_Sjabloon',
+	NS_TEMPLATE_TALK    => 'Overleg_sjabloon',
 	NS_HELP             => 'Ulpe',
-	NS_HELP_TALK        => 'Overleg_Ulpe',
+	NS_HELP_TALK        => 'Overleg_ulpe',
 	NS_CATEGORY         => 'Categorie',
-	NS_CATEGORY_TALK    => 'Overleg_Categorie',
+	NS_CATEGORY_TALK    => 'Overleg_categorie',
 );
 
 ?>
