af165ead9a2d92329eaaafb8b753f9bdaf2951df
Andrew Garrett <werdna@users.mediawiki.org>
2007-02-16 23:01:44 +0000

    Silly me, forgot $wgDBprefix

--- maintenance/archives/patch-page_restrictions_sortkey.sql
+++ maintenance/archives/patch-page_restrictions_sortkey.sql
@@ -3,6 +3,6 @@
 -- of Special:Protectedpages.
 -- Andrew Garrett, February 2007
 
-ALTER TABLE page_restrictions
+ALTER TABLE /*$wgDBprefix*/page_restrictions
 	ADD COLUMN pr_id int unsigned not null auto_increment,
 	ADD UNIQUE KEY pr_id (pr_id);
