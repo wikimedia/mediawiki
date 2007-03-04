32ee63766b3e1a35c0697865e324a157f0798082
Leon Weber <leon@users.mediawiki.org>
2007-03-04 20:53:48 +0000

    Finish what I began in r20065. Tested it in all skins and works except in MySkin. I guess this is okay, but I didn't understand yet what MySkin is :) If something's wrong, please feel free to correct it, as I'll be away for some weeks now.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1117,7 +1117,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '60';
+$wgStyleVersion = '61';
 
 
 # Server-side caching:
