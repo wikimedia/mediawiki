b21d51236be7413696e43217ebfaf3764191d7f3
Tim Starling <tstarling@users.mediawiki.org>
2007-03-05 11:55:29 +0000

    Use a separate connection for commons DB

--- includes/Image.php
+++ includes/Image.php
@@ -347,7 +347,7 @@ class Image
 			# capitalize the first letter of the filename before
 			# looking it up in the shared repository.
 			$name = $wgContLang->ucfirst($this->name);
-			$dbc = wfGetDB( DB_SLAVE, 'commons' );
+			$dbc = Image::getCommonsDB();
 
 			$row = $dbc->selectRow( "`$wgSharedUploadDBname`.{$wgSharedUploadDBprefix}image",
 				array(
@@ -449,8 +449,7 @@ class Image
 
 			// Write to the other DB using selectDB, not database selectors
 			// This avoids breaking replication in MySQL
-			$dbw = wfGetDB( DB_MASTER, 'commons' );
-			$dbw->selectDB( $wgSharedUploadDBname );
+			$dbw = Image::getCommonsDB();
 		} else {
 			$dbw = wfGetDB( DB_MASTER );
 		}
@@ -2327,6 +2326,18 @@ class Image
 			wfDebug( "Requested pageCount() for bogus multi-page metadata for '$this->name'\n" );
 			return null;
 		}
+	}
+
+	static function getCommonsDB() {
+		static $dbc;
+		global $wgLoadBalancer;
+		if ( !isset( $dbc ) ) {
+			$i = $wgLoadBalancer->getGroupIndex( 'commons' );
+			$dbinfo = $wgLoadBalancer->mServers[$i];
+			$dbc = new Database( $dbinfo['host'], $dbinfo['user'], 
+				$dbinfo['password'], $wgSharedUploadDBname );
+		}
+		return $dbc;
 	}
 
 } //class
