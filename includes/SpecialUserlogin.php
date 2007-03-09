32b36bf7dcee19340c7a99869e74a0c26e88d1d9
Ryan Lane <laner@users.mediawiki.org>
2007-03-09 19:31:52 +0000

    Adding setInternalPassword(), and changing the behavior of setPassword in User.php. Adding a check into SpecialUserLogin's initUser() function to only set a password if authentication plugins allow it.

--- includes/SpecialUserlogin.php
+++ includes/SpecialUserlogin.php
@@ -301,13 +301,18 @@ class LoginForm {
 	 * @private
 	 */
 	function initUser( $u ) {
+		global $wgAuth;
+
 		$u->addToDatabase();
-		$u->setPassword( $this->mPassword );
+
+		if ( $wgAuth->allowPasswordChange() ) {
+			$u->setPassword( $this->mPassword );
+		}
+
 		$u->setEmail( $this->mEmail );
 		$u->setRealName( $this->mRealName );
 		$u->setToken();
 
-		global $wgAuth;
 		$wgAuth->initUser( $u );
 
 		$u->setOption( 'rememberpassword', $this->mRemember ? 1 : 0 );
