1ed58139f6cd7be4d45c007bebe2871a176b5bac
Brion Vibber <brion@users.mediawiki.org>
2007-03-12 15:57:17 +0000

    * (bug 9256) Add a quick note to index.php header comments References documentation and main site, explains that web server is misconfigured if you're reading it, and lists copyright/license info.

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 
+/**
+ * This is the main web entry point for MediaWiki.
+ *
+ * If you are reading this in your web browser, your server is probably
+ * not configured correctly to run PHP applications!
+ *
+ * See the README, INSTALL, and UPGRADE files for basic setup instructions
+ * and pointers to the online documentation.
+ *
+ * http://www.mediawiki.org/
+ *
+ * ----------
+ *
+ * Copyright (C) 2001-2007 Magnus Manske, Brion Vibber, Lee Daniel Crocker,
+ * Tim Starling, Erik Möller, Gabriel Wicke, Ævar Arnfjörð Bjarmason,
+ * Niklas Laxström, Domas Mituzas, Rob Church and others.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+
 # Initialise common code
 require_once( './includes/WebStart.php' );
 
