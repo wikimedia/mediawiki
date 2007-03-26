3d57f7278f526832f2b2aa7422c63064eb72cc48
Aryeh Gregor <simetrical@users.mediawiki.org>
2007-03-26 16:42:45 +0000

    Fix E_NOTICE on line 76 if $_SERVER['HTTP_ACCEPT'] not set

--- includes/Metadata.php
+++ includes/Metadata.php
@@ -73,6 +73,10 @@ function wfCreativeCommonsRdf($article) {
 function rdfSetup() {
 	global $wgOut, $_SERVER;
 
+	if( !isset($_SERVER['HTTP_ACCEPT']) ) {
+		$_SERVER['HTTP_ACCEPT'] = '';
+	}
+
 	$rdftype = wfNegotiateType(wfAcceptToPrefs($_SERVER['HTTP_ACCEPT']), wfAcceptToPrefs(RDF_TYPE_PREFS));
 
 	if (!$rdftype) {
