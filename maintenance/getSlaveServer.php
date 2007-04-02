a22655ca7750e40e1e50f410c9ca262a7e63d792
Brion Vibber <brion@users.mediawiki.org>
2007-04-02 13:57:03 +0000

    Add --group parameter on getSlaveServer to select from a query group

--- maintenance/getSlaveServer.php
+++ maintenance/getSlaveServer.php
@@ -1,7 +1,15 @@
 <?php
 
 require_once( dirname(__FILE__).'/commandLine.inc' );
-$i = $wgLoadBalancer->getReaderIndex();
-print $wgDBservers[$i]['host'] . "\n";
+
+if( isset( $options['group'] ) ) {
+	$db = wfGetDB( DB_SLAVE, $options['group'] );
+	$host = $db->getProperty( 'mServer' );
+} else {
+	$i = $wgLoadBalancer->getReaderIndex();
+	$host = $wgDBservers[$i]['host'];
+}
+
+print "$host\n";
 
 ?>
