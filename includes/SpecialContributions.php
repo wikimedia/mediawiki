f36993d21b3807cd610d26552f7edd8a318628c8
Brion Vibber <brion@users.mediawiki.org>
2007-04-19 14:26:19 +0000

    remove sssllloowwwwwww range checks

--- includes/SpecialContributions.php
+++ includes/SpecialContributions.php
@@ -71,29 +71,6 @@ class ContribsFinder {
 		if ( $this->username == 'newbies' ) {
 			$max = $this->dbr->selectField( 'user', 'max(user_id)', false, 'make_sql' );
 			$condition = 'rev_user >' . (int)($max - $max / 100);
-		} else if ( IP::isIPv4( $this->username ) && preg_match("/\/(24|16)$/", $this->username, $matches) ) {
-			$abcd = explode( ".", $this->username );
-			if( $matches[1] == 24 ) $ipmask = $abcd[0] . '.' . $abcd[1] . '.' . $abcd[2] . '.%';
-			else $ipmask=$abcd[0] . '.' . $abcd[1] . '.%';
-			$condition = 'rev_user_text LIKE ' . $this->dbr->addQuotes($ipmask);
-		}  else if ( IP::isIPv6( $this->username ) && preg_match("/^(?:64|80|96|112)$/", $this->username) ) {
-			$abcdefgh = explode( ":", IP::sanitizeIP($this->username) );
-			$abcd = implode( ":", array_slice($abcdefgh, 0, 4) );
-			switch( $matches[1] ) {
-				case '112':
-					$ipmask = $abcd . ':' . $abcd[4] . ':' . $abcd[5] . ':' . $abcd[6] . ':%';
-					break;
-				case '96':
-					$ipmask = $abcd . ':' . $abcd[4] . ':' . $abcd[5] . ':%';
-					break;
-				case '80':
-					$ipmask = $abcd . ':' . $abcd[4] . ':%';
-					break;
-				case '64':
-					$ipmask = $abcd . ':%';
-					break;
-			}
-			$condition = 'rev_user_text LIKE ' . $this->dbr->addQuotes($ipmask);
 		}
 		
 		else if ( IP::isIPv6( $this->username ) ) {
