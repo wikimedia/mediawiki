29ee35cde82fad6a1ff06fc62bc6d9fa4d4fd190
Aaron Schulz <aaron@users.mediawiki.org>
2007-04-21 21:44:19 +0000

    *Use appropriate comment

--- maintenance/updateRestrictions.php
+++ maintenance/updateRestrictions.php
@@ -1,7 +1,10 @@
 <?php
 
 /*
- * Makes the required database changes for the CheckUser extension
+ * Makes the required database changes for Special:ProtectedPages
+ * to show all protected pages, even ones before the page restrictions
+ * schema change. All remaining page_restriction column values are moved
+ * to the new table.
  */
 
 define( 'BATCH_SIZE', 100 );
