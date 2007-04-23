60b131c607538781d6c99ef858e64c6e0cb7aed0
Aaron Schulz <aaron@users.mediawiki.org>
2007-04-23 19:51:41 +0000

    Revert r21504, wrong file :/

--- maintenance/rebuildImages.php
+++ maintenance/rebuildImages.php
@@ -144,7 +144,7 @@ class ImageBuilder extends FiveUpgrade {
 	}
 
 	function oldimageCallback( $row ) {
-		if( $row->oi_width && $row->oi_media_type ) {
+		if( $row->oi_width ) {
 			return null;
 		}
 
@@ -153,11 +153,7 @@ class ImageBuilder extends FiveUpgrade {
 		return array(
 			'oi_width'  => $info['width' ],
 			'oi_height' => $info['height'],
-			'oi_bits'   => $info['bits'  ],
-			'oi_metadata' => '', // filled in on-demand
-			'oi_media_type' => $info['media'],
-			'oi_major_mime' => $info['major'],
-			'oi_minor_mime' => $info['minor'] );
+			'oi_bits'   => $info['bits'  ] );
 	}
 
 	function crawlMissing() {
