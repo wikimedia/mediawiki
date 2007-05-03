57f30e2d8910c54397b8005e0b65d1c081bf3a08
Daniel Kinzler <daniel@users.mediawiki.org>
2007-05-03 12:44:05 +0000

    don't show excerpts from non-readable articles

--- includes/SpecialSearch.php
+++ includes/SpecialSearch.php
@@ -320,6 +320,14 @@ class SpecialSearch {
 		$contextchars = $wgUser->getOption( 'contextchars', 50 );
 
 		$link = $sk->makeKnownLinkObj( $t );
+
+		//If page content is not readable, just return the title.
+		//This is not quite safe, but better than showing excerpts from non-readable pages
+		//Note that hiding the entry entirely would screw up paging.
+		if (!$t->userCanRead()) {
+			return "<li>{$link}</li>\n";
+		}
+
 		$revision = Revision::newFromTitle( $t );
 		$text = $revision->getText();
 		$size = wfMsgExt( 'nbytes', array( 'parsemag', 'escape'),
