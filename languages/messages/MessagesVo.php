254ece5ec6092d541f171eadc5dfcc8605af9932
River Tarnell <river@users.mediawiki.org>
2007-05-08 13:23:35 +0000

    Volapük namespaces

---
+++ languages/messages/MessagesVo.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Volapük
+ * @addtogroup Language
+ *
+ */
+
+$namespaceNames = array(
+	NS_MEDIA          => 'Nünamakanäd',
+	NS_SPECIAL        => 'Patikos',
+	NS_MAIN           => '',
+	NS_TALK           => 'Bespik',
+	NS_USER           => 'Geban',
+	NS_USER_TALK      => 'Gebanibespik',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK   => 'Bespik_dö_$1',
+	NS_IMAGE          => 'Magod',
+	NS_IMAGE_TALK     => 'Magodibespik',
+	NS_MEDIAWIKI      => 'Sitanuns',
+	NS_MEDIAWIKI_TALK => 'Bespik_dö_sitanuns',
+	NS_TEMPLATE       => 'Samafomot',
+	NS_TEMPLATE_TALK  => 'Samafomotibespik',
+	NS_HELP           => 'Yuf',
+	NS_HELP_TALK      => 'Yufibespik',
+	NS_CATEGORY       => 'Klad',
+	NS_CATEGORY_TALK  => 'Kladibespik',
+);
+
+?>
