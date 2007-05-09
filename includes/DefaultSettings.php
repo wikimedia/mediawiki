497a9794a0509b309dc9c159d9b020a5cac0da1b
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-05-09 15:31:43 +0000

    * Use overflow: hidden instead of auto as suggested in bug 9854

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1125,7 +1125,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '66';
+$wgStyleVersion = '67';
 
 
 # Server-side caching:
