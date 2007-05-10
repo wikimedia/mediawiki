f2f48103b1dbc77ecc0070bfd5c51350c6ebcf85
Aaron Schulz <aaron@users.mediawiki.org>
2007-05-10 22:10:25 +0000

    *Add a script to delete archived images

--- maintenance/deleteArchivedFiles.php
+++ maintenance/deleteArchivedFiles.php
@@ -1,28 +1,28 @@
 <?php
 
 /**
- * Delete arcived (deleted from public) revisions from the database
+ * Delete archived (non-current) files from the database
  *
  * @addtogroup Maintenance
  * @author Aaron Schulz
- * Shamelessly stolen from deleteOldRevisions.php by Rob Church :)
+ * Based on deleteOldRevisions.php by Rob Church
  */
 
 $options = array( 'delete', 'help' );
 require_once( 'commandLine.inc' );
-require_once( 'deleteArchivedRevisions.inc' );
+require_once( 'deleteArchivedFiles.inc' );
 
-echo( "Delete Archived Revisions\n\n" );
+echo( "Delete Archived Images\n\n" );
 
 if( @$options['help'] ) {
 	ShowUsage();
 } else {
-	DeleteArchivedRevisions( @$options['delete'] );
+	DeleteArchivedFiles( @$options['delete'] );
 }
 
 function ShowUsage() {
-	echo( "Deletes all archived revisions.\n\n" );
-	echo( "These revisions will no longer be restorable.\n\n" );
+	echo( "Deletes all archived images.\n\n" );
+	echo( "These images will no longer be restorable.\n\n" );
 	echo( "Usage: php deleteArchivedRevisions.php [--delete|--help]\n\n" );
 	echo( "delete : Performs the deletion\n" );
 	echo( "  help : Show this usage information\n" );
