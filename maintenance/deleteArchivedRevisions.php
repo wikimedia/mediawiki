b038c4dacffeedbb97912bc07942e50647515987
Aaron Schulz <aaron@users.mediawiki.org>
2007-05-10 21:05:10 +0000

    *Add a script to delete archived revisions

---
+++ maintenance/deleteArchivedRevisions.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Delete old (non-current) revisions from the database
+ *
+ * @addtogroup Maintenance
+ * @author Rob Church <robchur@gmail.com>
+ */
+
+$options = array( 'delete', 'help' );
+require_once( 'commandLine.inc' );
+require_once( 'deleteArchivedRevisions.inc' );
+
+echo( "Delete Archived Revisions\n\n" );
+
+if( @$options['help'] ) {
+	ShowUsage();
+} else {
+	DeleteArchivedRevisions( @$options['delete'] );
+}
+
+function ShowUsage() {
+	echo( "Deletes all archived revisions.\n\n" );
+	echo( "These revisions will no longer be restorable.\n\n" );
+	echo( "Usage: php deleteArchivedRevisions.php [--delete|--help]\n\n" );
+	echo( "delete : Performs the deletion\n" );
+	echo( "  help : Show this usage information\n" );
+}
+
+?>
