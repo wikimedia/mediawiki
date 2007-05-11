3f4f7e42d03a683a13bac888771264aa7d208d05
Brion Vibber <brion@users.mediawiki.org>
2007-05-11 19:25:53 +0000

    keep compat link for be-x-old for now

--- languages/Names.php
+++ languages/Names.php
@@ -26,6 +26,7 @@
 	'bat-smg' => 'Žemaitėška', # Samogitian 
 	'be' => 'Беларуская',	#  Belarusian normative
 	'be-tarask' => 'Беларуская (тарашкевіца)',	# Belarusian in Taraskievica orthography
+	'be-x-old' => 'Беларуская (тарашкевіца)',	# Belarusian in Taraskievica orthography; compat link
 	'bg' => 'Български',	# Bulgarian
 	'bh' => 'भोजपुरी',	# Bihara
 	'bi' => 'Bislama',		# Bislama
