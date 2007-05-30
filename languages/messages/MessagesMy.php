305a2f63f45d64e8d96f3cc73621265ba5bff8e2
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-05-30 13:41:32 +0000

    * Start of Burmese (my) translation by Hakka

---
+++ languages/messages/MessagesMy.php
@@ -0,0 +1,205 @@
+<?php
+/** Burmese (Myanmasa)
+ *
+ * @addtogroup Language
+ */
+
+$messages = array(
+# Dates
+'sunday'        => 'တ​နင်္ဂ​နွေ​',
+'monday'        => 'တ​နင်္လာ​',
+'tuesday'       => 'အင်္ဂါ​',
+'wednesday'     => 'ဗုဒ္ဒ​ဟူး​',
+'thursday'      => 'ကြာ​သာ​ပ​တေး​',
+'friday'        => 'သော​ကြာ​',
+'saturday'      => 'စ​နေ​',
+'january'       => 'ဇန်​န​ဝါ​ရီ​',
+'february'      => 'ဖေ​ဖော်​ဝါ​ရီ​',
+'march'         => 'မတ်​',
+'april'         => 'ဧ​ပြီ​',
+'may_long'      => 'မေ​',
+'june'          => 'ဇွန်​',
+'july'          => 'ဇူ​လိုင်​',
+'august'        => 'ဩ​ဂုတ်​',
+'september'     => 'စက်​တင်​ဘာ​',
+'october'       => 'အောက်​တို​ဘာ​',
+'november'      => 'နို​ဝင်​ဘာ​',
+'december'      => 'ဒီ​ဇင်​ဘာ​',
+'january-gen'   => 'ဇန်​န​ဝါ​ရီ​',
+'february-gen'  => 'ဖေ​ဖော်​ဝါ​ရီ​',
+'march-gen'     => 'မတ်​',
+'april-gen'     => 'ဧ​ပြီ​',
+'may-gen'       => 'မေ​',
+'june-gen'      => 'ဇွန်​',
+'july-gen'      => 'ဇူ​လိုင်​',
+'august-gen'    => 'ဩ​ဂုတ်​',
+'september-gen' => 'စက်​တင်​ဘာ​',
+'october-gen'   => 'အောက်​တို​ဘာ​',
+'november-gen'  => 'နို​ဝင်​ဘာ​',
+'december-gen'  => 'ဒီ​ဇင်​ဘာ​',
+'may'           => 'မေ​',
+
+'cancel'         => 'မ​လုပ်​တော့​ပါ​',
+'qbedit'         => 'ပြင်​ဆင်​ရန်​',
+'qbspecialpages' => 'အ​ထူး​စာ​မျက်​နှာ​',
+'mytalk'         => 'ကျွန်​တော့​ပြော​ရေး​ဆို​ရာ​',
+'navigation'     => 'အ​ညွှန်း​',
+
+'help'             => 'အ​ကူ​အ​ညီ​',
+'search'           => 'ရှာ​ဖွေ​ရန်​',
+'searchbutton'     => 'ရှာ​ဖွေ​ရန်​',
+'go'               => 'သွား​ပါ​',
+'history_short'    => 'မှတ်​တမ်း​',
+'printableversion' => 'ပ​ရင်​တာ​ထုတ်​ရန်​',
+'permalink'        => 'ပုံ​သေ​လိပ်​စာ​',
+'edit'             => 'ပြင်​ဆင်​ရန်​',
+'delete'           => 'ဖျက်​ပါ​',
+'protect'          => 'ထိမ်း​သိမ်း​ပါ​',
+'talk'             => 'ပြော​ရေး​ဆို​ရာ​',
+'toolbox'          => 'တန်​ဆာ​ပ​လာ​',
+'otherlanguages'   => 'အ​ခြား​ဘာ​သာ​ဖြင့်​',
+'jumptonavigation' => 'အ​ညွှန်း​',
+'jumptosearch'     => 'ရှာ​ဖွေ​ရန်​',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'currentevents'     => 'လက်​ရှိ​လုပ်​ငန်း​များ​',
+'currentevents-url' => 'လက်​ရှိ​လုပ်​ငန်း​များ​',
+'disclaimers'       => 'သ​တိ​ပေး​ချက်​များ​',
+'edithelp'          => 'ပြင်​ဆင်​ခြင်း​အ​ကူ​အ​ညီ​',
+'mainpage'          => 'ဗ​ဟို​စာ​မျက်​နှာ​',
+'portal'            => 'ပြော​ရေး​ဆို​ရာ​',
+'sitesupport'       => 'လှု​ဒါန်း​မှု​',
+
+'newmessageslink' => 'သ​တင်း​အ​သစ်​',
+'editsection'     => 'ပြင်​ဆင်​ရန်​',
+'editold'         => 'ပြင်​ဆင်​ရန်​',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main' => 'စာ​မျက်​နှာ​',
+'nstab-user' => 'မှတ်​ပုံ​တင်​အ​သုံး​ပြု​သူ​၏​စာ​မျက်​နှာ​',
+
+# General errors
+'viewsource' => 'ဆို့​ကို​ပြ​ပါ​',
+
+# Login and logout pages
+'welcomecreation'    => 'မင်္ဂ​လာ​ပါ​ $1။ သင့်​အား​မှတ်​ပုံ​တင်​ပြီး​ပါ​ပြီ။​ ဝီ​ကီ​အ​တွက်​သင့်​စိတ်​ကြိုက်​များ​ကို​ရွေး​ချယ်​နိုင်​ပါ​သည်။​',
+'yourname'           => 'မှတ်​ပုံ​တင်​အ​မည်:',
+'yourpassword'       => 'လှို့​ဝှက်​စ​ကား​လုံး:',
+'yourpasswordagain'  => 'ပြန်​ရိုက်​ပါ:',
+'remembermypassword' => 'ဤ​ကွန်​ပျူ​တာ​တွင်​ကျွန်​တော့​ကို​မှတ်​ထား​ပါ​',
+'login'              => 'မှတ်​ပုံ​တင်​ဖြင့်​ဝင်​ပါ​',
+'logout'             => 'ထွက်​ပါ​',
+'userlogout'         => 'ထွက်​ပါ​',
+'notloggedin'        => 'မှတ်​ပုံ​တင်​ဖြင့်​မ​ဝင်​ရ​သေး​ပါ​',
+'createaccount'      => 'မှတ်​ပုံ​တင်​ပြု​လုပ်​ပါ​',
+'gotaccountlink'     => 'မှတ်​ပုံ​တင်​ဖြင့်​ဝင်​ပါ​',
+'youremail'          => 'အီ​မေး:',
+'username'           => 'မှတ်​ပုံ​တင်​အ​မည်:',
+'email'              => 'အီ​မေး​',
+
+# Edit pages
+'summary'            => 'အ​ကျဉ်း​ချုပ်​',
+'minoredit'          => 'သာ​မန်​ပြင်​ဆင်​မှု​ဖြစ်​ပါ​သည်​',
+'watchthis'          => 'ဤ​စာ​မျက်​နှာ​အား​စောင့်​ကြည့်​ပါ​',
+'savearticle'        => 'သိမ်း​ပါ​',
+'showpreview'        => 'န​မူ​နာ​ပြ​ပါ​',
+'showdiff'           => 'ပြင်​ဆင်​ထား​သည်​များ​ကို​ပြ​ပါ​',
+'whitelistedittitle' => 'ပြင်​ဆင်​ခြင်း​သည်​မှတ်​ပုံ​တင်​ရန်​လို​သည်​',
+'loginreqlink'       => 'မှတ်​ပုံ​တင်​ဖြင့်​ဝင်​ပါ​',
+
+# Search results
+'powersearch' => 'ရှာ​ဖွေ​ရန်​',
+
+# Preferences page
+'mypreferences'     => 'ကျွန်​တော့​ရွေး​ချယ်​စ​ရာ​များ​',
+'prefsnologin'      => 'မှတ်​ပုံ​တင်​ဖြင့်​မ​ဝင်​ရ​သေး​ပါ​',
+'searchresultshead' => 'ရှာ​ဖွေ​ရန်​',
+
+# Recent changes
+'recentchanges' => 'လတ်​တ​လော​အ​ပြောင်း​အ​လဲ​များ​',
+
+# Recent changes linked
+'recentchangeslinked' => 'ဆက်​ဆပ်​သော​အ​ပြောင်း​အ​လဲ​များ​',
+
+# Upload
+'upload'            => 'ဖိုင်​တင်​ရန်​',
+'uploadbtn'         => 'ဖိုင်​တင်​ရန်​',
+'uploadnologin'     => 'မှတ်​ပုံ​တင်​ဖြင့်​မ​ဝင်​ရ​သေး​ပါ​',
+'filedesc'          => 'အ​ကျဉ်း​ချုပ်​',
+'fileuploadsummary' => 'အ​ကျဉ်း​ချုပ်:',
+'watchthisupload'   => 'ဤ​စာ​မျက်​နှာ​အား​စောင့်​ကြည့်​ပါ​',
+
+# Image list
+'ilsubmit' => 'ရှာ​ဖွေ​ရန်​',
+
+'brokenredirects-edit'   => '(ပြင်​ဆင်​ရန်)',
+'brokenredirects-delete' => '(ဖျက်​ပါ)',
+
+# Miscellaneous special pages
+'randompage'        => 'ကျ​ပန်း​စာ​မျက်​နှာ​',
+'specialpages'      => 'အ​ထူး​စာ​မျက်​နှာ​',
+'newpages-username' => 'မှတ်​ပုံ​တင်​အ​မည်:',
+'move'              => 'ရွေ့​ပြောင်း​ပါ​',
+
+# Book sources
+'booksources-go' => 'သွား​ပါ​',
+
+# Special:Log
+'log-search-submit' => 'သွား​ပါ​',
+
+# Special:Allpages
+'allpagessubmit' => 'သွား​ပါ​',
+
+# Watchlist
+'watchlist'     => 'စောင့်​ကြည့်​စာ​ရင်း​',
+'mywatchlist'   => 'စောင့်​ကြည့်​စာ​ရင်း​',
+'watch'         => 'စောင့်​ကြည့်​ပါ​',
+'watchthispage' => 'ဤ​စာ​မျက်​နှာ​အား​စောင့်​ကြည့်​ပါ​',
+
+# Restrictions (nouns)
+'restriction-edit' => 'ပြင်​ဆင်​ရန်​',
+'restriction-move' => 'ရွေ့​ပြောင်း​ပါ​',
+
+# Undelete
+'undelete-search-submit' => 'ရှာ​ဖွေ​ရန်​',
+
+# Contributions
+'contributions' => 'မှတ်​ပုံ​တင်​အ​သုံး​ပြု​သူ​:ပံ​ပိုး​မှု​များ​',
+'mycontris'     => 'ကျွန်​တော်​ပေး​သော​ပံ​ပိုး​မှု​များ​',
+
+'sp-contributions-submit' => 'ရှာ​ဖွေ​ရန်​',
+
+# What links here
+'whatlinkshere' => 'မည်​သည့်​စာ​မျက်​နှာ​များ​မှ​ညွန်း​ထား​သည်​',
+
+# Block/unblock
+'ipbreason'          => 'အ​ကြောင်း​ပြ​ချက်:',
+'ipblocklist-submit' => 'ရှာ​ဖွေ​ရန်​',
+
+# Move page
+'movepage'    => 'စာ​မျက်​နှာ​အား​ရွေ့​ပြောင်း​ပါ​',
+'movearticle' => 'စာ​မျက်​နှာ​အား​ရွေ့​ပြောင်း​ပါ​',
+'movenologin' => 'မှတ်​ပုံ​တင်​ဖြင့်​မ​ဝင်​ရ​သေး​ပါ​',
+'movepagebtn' => 'စာ​မျက်​နှာ​အား​ရွေ့​ပြောင်း​ပါ​',
+'movedto'     => 'ရွေ့​ပြောင်း​ရန်​နေ​ရာ​',
+'1movedto2'   => '[[$1]]  မှ​ [[$2]] သို့​',
+'movereason'  => 'အ​ကြောင်း​ပြ​ချက်​',
+
+# Namespace 8 related
+'allmessages' => 'စ​နစ်​၏​သ​တင်း​များ​',
+
+# Tooltip help for the actions
+'tooltip-pt-logout' => 'ထွက်​ပါ​',
+'tooltip-ca-move'   => 'ဤ​စာ​မျက်​နှာ​အား​ရွေ့​ပြောင်း​ပါ​',
+
+'youhavenewmessagesmulti' => 'သင့်​အ​တွက်​သီ​တင်း​အ​သစ်​ $1 တွင်​ရှိ​သည်​',
+
+# Multipage image navigation
+'imgmultigo' => 'သွား​ပါ!',
+
+# Table pager
+'table_pager_limit_submit' => 'သွား​ပါ​',
+
+);
+
+?>
