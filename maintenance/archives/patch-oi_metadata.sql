c9490765dd577537c9d450bcad2fee526ab99512
Aaron Schulz <aaron@users.mediawiki.org>
2007-05-31 15:37:58 +0000

    *Add semicolon and \n

--- maintenance/archives/patch-oi_metadata.sql
+++ maintenance/archives/patch-oi_metadata.sql
@@ -13,4 +13,5 @@ ALTER TABLE /*$wgDBprefix*/oldimage
    ADD oi_media_type ENUM("UNKNOWN", "BITMAP", "DRAWING", "AUDIO", "VIDEO", "MULTIMEDIA", "OFFICE", "TEXT", "EXECUTABLE", "ARCHIVE") default NULL,
    ADD oi_major_mime ENUM("unknown", "application", "audio", "image", "text", "video", "message", "model", "multipart") NOT NULL default "unknown",
    ADD oi_minor_mime varchar(32) NOT NULL default "unknown",
-   ADD oi_deleted tinyint(1) unsigned NOT NULL default '0',
+   ADD oi_deleted tinyint(1) unsigned NOT NULL default '0';
+   
