a634974312483050a173198ed3a12df75c0b05e9
Brion Vibber <brion@users.mediawiki.org>
2007-05-31 22:05:45 +0000

    Remove a long-obsolete file, used by the ancient command-line installer. Testing new SVN server...

--- maintenance/database.sql
+++
@@ -1,7 +0,0 @@
--- SQL script to create database for wiki.  This is run from
--- the installation script which replaces the variables with
--- their values from local settings.
---
-
-DROP DATABASE IF EXISTS `{$wgDBname}`;
-CREATE DATABASE `{$wgDBname}`;
