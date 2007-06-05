440debaa1c7a0494c586ac15add7903deeb93e11
Rob Church <robchurch@users.mediawiki.org>
2007-06-05 15:03:18 +0000

    (bug 9383) Don't set a default value for BLOB column in patch-rc-deleted.sql, it's not valid for STRICT mode. Allow a NULL value.

--- maintenance/archives/patch-rc_deleted.sql
+++ maintenance/archives/patch-rc_deleted.sql
@@ -5,4 +5,4 @@ ALTER TABLE /*$wgDBprefix*/recentchanges
   ADD rc_logid int(10) unsigned NOT NULL default '0',
   ADD rc_log_type varchar(255) binary NULL default NULL,
   ADD rc_log_action varchar(255) binary NULL default NULL,
-  ADD rc_params blob NOT NULL default '';
+  ADD rc_params BLOB NULL;
