936fbd329c4fd3d684a35ca4a53730325d7b545e
Rob Church <robchurch@users.mediawiki.org>
2007-06-10 16:04:36 +0000

    (bug 10190) Volapük message updates

--- languages/messages/MessagesVo.php
+++ languages/messages/MessagesVo.php
@@ -26,4 +26,54 @@ $namespaceNames = array(
 	NS_CATEGORY_TALK  => 'Kladibespik',
 );
 
+$messages = array(
+# Dates
+'sunday'    => 'sudel',
+'monday'    => 'mudel',
+'tuesday'   => 'tudel',
+'wednesday' => 'vedel',
+'thursday'  => 'dödel',
+'friday'    => 'fridel',
+'saturday'  => 'zädel',
+'sun'       => 'sud',
+'mon'       => 'mud',
+'tue'       => 'tud',
+'wed'       => 'ved',
+'thu'       => 'död',
+'fri'       => 'fri',
+'sat'       => 'zäd',
+'january'   => 'yanul',
+'february'  => 'febul',
+'march'     => 'mäzul',
+'april'     => 'prilul',
+'may_long'  => 'mayul',
+'june'      => 'yunul',
+'july'      => 'yulul',
+'august'    => 'gustul',
+'september' => 'setul',
+'october'   => 'tobul',
+'november'  => 'novul',
+'december'  => 'dekul',
+'jan'       => 'yan',
+'feb'       => 'feb',
+'mar'       => 'mäz',
+'apr'       => 'pri',
+'may'       => 'may',
+'jun'       => 'yun',
+'jul'       => 'yul',
+'aug'       => 'gus',
+'sep'       => 'set',
+'oct'       => 'tob',
+'nov'       => 'nov',
+'dec'       => 'dek',
+
+# Bits of text used by many pages
+'categories'     => 'Klads',
+'pagecategories' => '{{PLURAL:$1|Klad|Klads}}',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'mainpage' => 'Cifapad',
+
+);
+
 ?>
