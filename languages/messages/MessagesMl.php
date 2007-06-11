8f5cc248ca8a137b917d54e029bd55842e14967d
Raimond Spekking <raymond@users.mediawiki.org>
2007-06-11 17:36:59 +0000

    * (bug 10220) Malayalam namespace translation   Patch by Sadik Khalid

--- languages/messages/MessagesMl.php
+++ languages/messages/MessagesMl.php
@@ -4,6 +4,11 @@
   * @addtogroup Language
   *
   * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
+  * @edited by:
+  *		Sadik Khalid <sadik.khalid@gmail.com>,
+  *		Manjith Joseph <manjithkaini@gmail.com>,
+  *		Praveen Prakash <me.praveen@gmail.com>,
+  *		Shiju Alex <shijualexonline@gmail.com>, 
   */
 
 $digitTransformTable = array(
@@ -17,6 +22,27 @@ $digitTransformTable = array(
 	'7' => '൭', # &#x0d6d;
 	'8' => '൮', # &#x0d6e;
 	'9' => '൯', # &#x0d6f;
+);
+
+$namespaceNames = array(
+	NS_MEDIA => 'മീഡിയ',
+	NS_SPECIAL => 'പ്രത്യേകം',
+	NS_MAIN => '',
+	NS_TALK => 'സംവാദം',
+	NS_USER => 'ഉപയോക്താവ്',
+	NS_USER_TALK => 'ഉപയോക്താവിന്റെ_സംവാദം',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK => '$1_പദ്ധതി',
+	NS_IMAGE => 'ചിത്രം',
+	NS_IMAGE_TALK => 'ചിത്രത്തിന്റെ_സംവാദം',
+	NS_MEDIAWIKI => 'മീഡിയവിക്കി',
+	NS_MEDIAWIKI_TALK => 'മീഡിയവിക്കി_സംവാദം',
+	NS_TEMPLATE => 'ഫലകം',
+	NS_TEMPLATE_TALK => 'ഫലകത്തിന്റെ_സംവാദം',
+	NS_CATEGORY => 'വിഭാഗം',
+	NS_CATEGORY_TALK => 'വിഭാഗത്തിന്റെ_സംവാദം',
+	NS_HELP => 'സഹായം',
+	NS_HELP_TALK => 'സഹായത്തിന്റെ_സംവാദം',
 );
 
 ?>
