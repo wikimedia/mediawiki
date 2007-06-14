11b93d3b94eadd415589630422ac3f74b6686efd
Raimond Spekking <raymond@users.mediawiki.org>
2007-06-14 09:40:41 +0000

    * (bug 10252) Update Volapük translations   Patch by Malafaya

--- languages/messages/MessagesVo.php
+++ languages/messages/MessagesVo.php
@@ -71,8 +71,33 @@ $messages = array(
 'categories'     => 'Klads',
 'pagecategories' => '{{PLURAL:$1|Klad|Klads}}',
 
+'mypage' => 'Pad obik',
+'mytalk' => 'Bespiks obik',
+
 # All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
 'mainpage' => 'Cifapad',
+
+# Login and logout pages
+'login'      => 'Ninädolös obi',
+'userlogin'  => 'Nunädön oki / jafön kali',
+'logout'     => 'Senunädön oki',
+'userlogout' => 'Senunädön oki',
+
+# Preferences page
+'mypreferences' => 'Buükams obik',
+
+# Recent changes
+'recentchanges' => 'Votükams nulik',
+
+# Miscellaneous special pages
+'randompage'   => 'Pad fädik',
+'specialpages' => 'Pads patik',
+
+# Watchlist
+'mywatchlist' => 'Galädalised obik',
+
+# Contributions
+'mycontris' => 'Keblünots obik',
 
 );
 
