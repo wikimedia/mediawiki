a298cfb5f776ba8d07f9d8f2b5fdfc040fd81dde
Brion Vibber <brion@users.mediawiki.org>
2007-06-22 18:27:06 +0000

    Spiff up the JS message a little bit

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1183,7 +1183,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '76';
+$wgStyleVersion = '77';
 
 
 # Server-side caching:
