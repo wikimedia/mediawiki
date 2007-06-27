a9618297b996a4e54a65955974994d834722b3c5
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-06-27 14:41:45 +0000

    * First Tigrinya (ti) messages by Blake

---
+++ languages/messages/MessagesTi.php
@@ -0,0 +1,146 @@
+<?php
+/** Tigrinya (ትግርኛ)
+ *
+ * Translators: Blake
+ *
+ * @addtogroup Language
+ * @author Niklas Laxström
+ */
+
+$messages = array(
+'underline-always' => 'ጥራይ',
+'underline-never'  => 'ኣይትግበሩ',
+
+# Dates
+'sunday'    => 'ሰንበት',
+'monday'    => 'ሰኑይ',
+'tuesday'   => 'ሰሉስ',
+'wednesday' => 'ረብዓ',
+'thursday'  => 'ኃሙስ',
+'friday'    => 'ዓርቢ',
+'saturday'  => 'ቀዳም',
+'january'   => 'ትሪ',
+'february'  => 'ለካቲት',
+'march'     => 'መጋቢት',
+'april'     => 'ሚያዝያ',
+'may_long'  => 'ግንቦት',
+'june'      => 'ሰነ',
+'july'      => 'ሓምለ',
+'august'    => 'ነሓሰ',
+'september' => 'መስከረም',
+'october'   => 'ጥቅምቲ',
+'november'  => 'ኅዳር',
+'december'  => 'ታኅሣሥ',
+
+'about'         => 'ብዛዕባ ብጠቅላላ',
+'qbpageoptions' => 'ፅሑፍዚ',
+'qbmyoptions'   => 'ፅሑፋተይ',
+'mypage'        => 'ፅሑፈይ',
+
+'search'        => 'ድሉይ',
+'searchbutton'  => 'ድሉይ',
+'history'       => 'ናይ ፅሑፍ ታሪኽ',
+'history_short' => 'ታሪኽ',
+'jumptosearch'  => 'ድሉይ',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite' => 'ብዛዕባ {{SITENAME}} ብጠቅላላ',
+'mainpage'  => 'መበገሲ ገጽ',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'    => 'ፅሑፍ',
+'nstab-user'    => 'ናይ ፅሑፍ ኣባል',
+'nstab-project' => 'ናይ ፅሑፍ ዊኪፒዲያ',
+'nstab-image'   => 'ምስሊ',
+
+# General errors
+'viewsourcefor' => 'ን$1',
+
+# Login and logout pages
+'yourlanguage' => 'ቋንቋ:',
+
+# History pages
+'next' => 'ዝቕጽል',
+
+# Search results
+'prevn'       => 'ናይ ቀደም $1',
+'nextn'       => 'ዝቕጽል $1',
+'powersearch' => 'ድሉይ',
+
+# Preferences page
+'preferences'       => 'ኣማረጽቲ',
+'mypreferences'     => 'ናተይ ኣማረጽቲ',
+'searchresultshead' => 'ድሉይ',
+'files'             => 'ምስልታት',
+
+# Groups
+'group'       => 'ብሓበር:',
+'group-sysop' => 'ሲሶፓት',
+
+'group-sysop-member' => 'ሲሶፕ',
+
+# Recent changes
+'hist' => 'ታሪኽ',
+
+# Image list
+'imagelist'      => 'ምስልታት',
+'ilsubmit'       => 'ድሉይ',
+'imgfile'        => 'ምስሊ',
+'imghistory'     => 'ናይ ምስሊ ታሪኽ',
+'imagelist_user' => 'ኣባል',
+
+# Miscellaneous special pages
+'nbytes'    => '$1 {{PLURAL:$1|ባይት|ባይታት}}',
+'listusers' => 'ኣባላት',
+'newpages'  => 'ሓደሽቲ ዛዕባታት',
+
+'alphaindexline' => '$1 ናብ $2',
+
+# Special:Log
+'specialloguserlabel' => 'ኣባል:',
+
+# Special:Allpages
+'nextpage'     => 'ዝቕጽል ፅሑፍ ($1)',
+'prevpage'     => 'ናይ ቀደም ፅሑፍ ($1)',
+'allpagesprev' => 'ናይ ቀደም',
+'allpagesnext' => 'ዝቕጽል',
+
+# Watchlist
+'watchlistfor' => "(ን'''$1''')",
+
+# Delete/protect/revert
+'pagesize' => '(ባይታት)',
+
+# Undelete
+'undelete-search-submit' => 'ድሉይ',
+
+# Contributions
+'contribsub2' => 'ን$1 ($2)',
+
+'sp-contributions-submit' => 'ድሉይ',
+
+# Block/unblock
+'ipblocklist-submit' => 'ድሉይ',
+
+# Tooltip help for the actions
+'tooltip-pt-preferences' => 'ናተይ ኣማረጽቲ',
+
+'loginlanguagelabel' => 'ቋንቋ: $1',
+
+# Multipage image navigation
+'imgmultipageprev' => '← ናይ ቀደም ፅሑፍ',
+'imgmultipagenext' => 'ዝቕጽል ፅሑፍ →',
+
+# Table pager
+'table_pager_next' => 'ዝቕጽል ፅሑፍ',
+'table_pager_prev' => 'ናይ ቀደም ፅሑፍ',
+
+# Size units
+'size-bytes'     => '$1 ባይታት',
+'size-kilobytes' => '$1 ኪሎባይታት',
+'size-megabytes' => '$1 ሜጋባይታት',
+'size-gigabytes' => '$1 ጊጋባይታት',
+
+);
+
+?>
