9d9a0e797b5a021932b92bd22a5feddac6855a2f
Tim Starling <tstarling@users.mediawiki.org>
2007-07-02 16:33:23 +0000

    Bug 10435: oi_name_archive_name index length overflow

--- maintenance/archives/patch-oi_metadata.sql
+++ maintenance/archives/patch-oi_metadata.sql
@@ -8,7 +8,7 @@
 ALTER TABLE /*$wgDBprefix*/oldimage
    DROP INDEX oi_name,
    ADD INDEX oi_name_timestamp (oi_name,oi_timestamp),
-   ADD INDEX oi_name_archive_name (oi_name,oi_archive_name),
+   ADD INDEX oi_name_archive_name (oi_name,oi_archive_name(14)),
    ADD oi_metadata mediumblob NOT NULL,
    ADD oi_media_type ENUM("UNKNOWN", "BITMAP", "DRAWING", "AUDIO", "VIDEO", "MULTIMEDIA", "OFFICE", "TEXT", "EXECUTABLE", "ARCHIVE") default NULL,
    ADD oi_major_mime ENUM("unknown", "application", "audio", "image", "text", "video", "message", "model", "multipart") NOT NULL default "unknown",
