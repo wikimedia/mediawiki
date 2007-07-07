559049abdcebb6fb6f462e1f7fbda91f7a8df136
Rob Church <robchurch@users.mediawiki.org>
2007-07-07 00:46:39 +0000

    Brighten up the high slave lag warning in Monobook; looked a bit...grubby

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1198,7 +1198,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '80';
+$wgStyleVersion = '81';
 
 
 # Server-side caching:
