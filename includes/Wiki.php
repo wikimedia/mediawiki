ff5f8810de6a0a18145e6449f9ddcdd1821158b1
Brion Vibber <brion@users.mediawiki.org>
2007-07-10 20:01:43 +0000

    woops -- parens broke teh diffs

--- includes/Wiki.php
+++ includes/Wiki.php
@@ -102,7 +102,7 @@ class MediaWiki {
 				$lang->findVariantLink( $title, $ret );
 
 		}
-		if ( $oldid = $request->getInt( 'oldid' )
+		if ( ( $oldid = $request->getInt( 'oldid' ) )
 			&& ( is_null( $ret ) || $ret->getNamespace() != NS_SPECIAL ) ) {
 			// Allow oldid to override a changed or missing title.
 			$rev = Revision::newFromId( $oldid );
