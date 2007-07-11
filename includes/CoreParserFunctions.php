d2ae2bd802ce0cf4637ea82e303f668fe719f559
Aryeh Gregor <simetrical@users.mediawiki.org>
2007-07-11 05:40:05 +0000

    PHP typing strikes again!  Padding parser functions now work with strings like '0' that evaluate to false.

--- includes/CoreParserFunctions.php
+++ includes/CoreParserFunctions.php
@@ -161,7 +161,7 @@ class CoreParserFunctions {
 	static function pad( $string = '', $length = 0, $char = 0, $direction = STR_PAD_RIGHT ) {
 		$length = min( max( $length, 0 ), 500 );
 		$char = substr( $char, 0, 1 );
-		return ( $string && (int)$length > 0 && strlen( trim( (string)$char ) ) > 0 )
+		return ( $string !== '' && (int)$length > 0 && strlen( trim( (string)$char ) ) > 0 )
 				? str_pad( $string, $length, (string)$char, $direction )
 				: $string;
 	}
