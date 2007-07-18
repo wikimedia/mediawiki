9d93eb1b13bd145c00180b1252c6c38f00639a4b
Rob Church <robchurch@users.mediawiki.org>
2007-07-18 09:29:41 +0000

    Introduce IndexPager::preprocessResults(); useful for doing batch existence checks, etc.

--- includes/Pager.php
+++ includes/Pager.php
@@ -107,6 +107,9 @@ abstract class IndexPager implements Pager {
 		$this->mResult = $this->reallyDoQuery( $this->mOffset, $queryLimit, $descending );
 		$this->extractResultInfo( $this->mOffset, $queryLimit, $this->mResult );
 		$this->mQueryDone = true;
+		
+		$this->preprocessResults( $this->mResult );
+		$this->mResult->rewind(); // Paranoia
 
 		wfProfileOut( $fname );
 	}
@@ -191,6 +194,13 @@ abstract class IndexPager implements Pager {
 		$res = $this->mDb->select( $tables, $fields, $conds, $fname, $options );
 		return new ResultWrapper( $this->mDb, $res );
 	}
+
+	/**
+	 * Pre-process results; useful for performing batch existence checks, etc.
+	 *
+	 * @param ResultWrapper $result Result wrapper
+	 */
+	protected function preprocessResult( $result ) {}
 
 	/**
 	 * Get the formatted result list. Calls getStartBody(), formatRow() and 
