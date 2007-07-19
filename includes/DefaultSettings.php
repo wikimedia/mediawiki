ce6fa57a19e1452cf266541d4bb342be3ea7f2eb
Brion Vibber <brion@users.mediawiki.org>
2007-07-19 14:46:14 +0000

    and bump style ver

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1201,7 +1201,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '88';
+$wgStyleVersion = '89';
 
 
 # Server-side caching:
