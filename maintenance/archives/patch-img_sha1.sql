5d1bf41015dbd21453f50d6fb1357f68908165d0
Aaron Schulz <aaron@users.mediawiki.org>
2007-07-23 03:03:04 +0000

    *grr

--- maintenance/archives/patch-img_sha1.sql
+++ maintenance/archives/patch-img_sha1.sql
@@ -1,8 +1,8 @@
 -- Add img_sha1, oi_sha1 and related indexes
-ALTER TABLE image
+ALTER TABLE /*$wgDBprefix*/image
   ADD COLUMN img_sha1 varbinary(31) NOT NULL default '',
   ADD INDEX img_sha1 (img_sha1);
 
-ALTER TABLE oldimage
+ALTER TABLE /*$wgDBprefix*/oldimage
   ADD COLUMN oi_sha1 varbinary(31) NOT NULL default '',
   ADD INDEX oi_sha1 (oi_sha1);
