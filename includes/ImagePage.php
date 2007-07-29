a49e134c38dd2d26dcd359d413b3cd33eddfa128
Rob Church <robchurch@users.mediawiki.org>
2007-07-29 18:41:49 +0000

    * Use a table to present file histories * (bug 10741) Don't show deletion links for non-sysops [in file histories]

--- includes/ImagePage.php
+++ includes/ImagePage.php
@@ -693,85 +693,105 @@ EOT
 }
 
 /**
- * @todo document
+ * Builds the image revision log shown on image pages
+ *
  * @addtogroup Media
  */
 class ImageHistoryList {
-	var $img, $skin;
-	function ImageHistoryList( $skin, $img ) {
+
+	protected $img, $skin, $title;
+
+	public function __construct( $skin, $img ) {
 		$this->skin = $skin;
 		$this->img = $img;
+		$this->title = $img->getTitle();
 	}
 
-	function beginImageHistoryList() {
-		$s = "\n" .
-			Xml::element( 'h2', array( 'id' => 'filehistory' ), wfMsg( 'imghistory' ) ) .
-			"\n<p>" . wfMsg( 'imghistlegend' ) . "</p>\n".'<ul class="special">';
-		return $s;
+	public function beginImageHistoryList() {
+		global $wgOut, $wgUser;
+		return Xml::element( 'h2', array( 'id' => 'filehistory' ), wfMsg( 'filehist' ) )
+			. $wgOut->parse( wfMsgNoTrans( 'filehist-help' ) )
+			. Xml::openElement( 'table', array( 'class' => 'filehistory' ) ) . "\n"
+			. '<tr><th></th>'
+			. ( $this->img->isLocal() && $wgUser->isAllowed( 'delete' ) ? '<th></th>' : '' )
+			. '<th>' . wfMsgHtml( 'filehist-datetime' ) . '</th>'
+			. '<th>' . wfMsgHtml( 'filehist-user' ) . '</th>'
+			. '<th>' . wfMsgHtml( 'filehist-dimensions' ) . '</th>'
+			. '<th>' . wfMsgHtml( 'filehist-filesize' ) . '</th>'
+			. '<th>' . wfMsgHtml( 'filehist-comment' ) . '</th>'
+			. "</tr>\n";
 	}
 
-	function endImageHistoryList() {
-		$s = "</ul>\n";
-		return $s;
+	public function endImageHistoryList() {
+		return "</table>\n";
 	}
 
-	function imageHistoryLine( $iscur, $timestamp, $img, $user, $usertext, $size, $description, $width, $height ) {
+	public function imageHistoryLine( $iscur, $timestamp, $img, $user, $usertext, $size, $description, $width, $height ) {
 		global $wgUser, $wgLang, $wgTitle, $wgContLang;
-
-		$datetime = $wgLang->timeanddate( $timestamp, true );
-		$del = wfMsgHtml( 'deleteimg' );
-		$delall = wfMsgHtml( 'deleteimgcompletely' );
-		$cur = wfMsgHtml( 'cur' );
 		$local = $this->img->isLocal();
-
-		if ( $iscur ) {
-			$url = htmlspecialchars( $this->img->getURL() );
-			$rlink = $cur;
-			if ( $local && $wgUser->isAllowed('delete') ) {
-				$link = $wgTitle->escapeLocalURL( 'image=' . $wgTitle->getPartialURL() .
-				  '&action=delete' );
-				$style = $this->skin->getInternalLinkAttributes( $link, $delall );
-
-				$dlink = '<a href="'.$link.'"'.$style.'>'.$delall.'</a>';
-			} else {
-				$dlink = $del;
-			}
-		} else {
-			$url = htmlspecialchars( $this->img->getArchiveUrl( $img ) );
-			if( $local && $wgUser->getID() != 0 && $wgTitle->userCan( 'edit' ) ) {
-				$token = urlencode( $wgUser->editToken( $img ) );
-				$rlink = $this->skin->makeKnownLinkObj( $wgTitle,
-				           wfMsgHtml( 'revertimg' ), 'action=revert&oldimage=' .
-				           urlencode( $img ) . "&wpEditToken=$token" );
-				$dlink = $this->skin->makeKnownLinkObj( $wgTitle,
-				           $del, 'action=delete&oldimage=' . urlencode( $img ) .
-				           "&wpEditToken=$token" );
-			} else {
-				# Having live active links for non-logged in users
-				# means that bots and spiders crawling our site can
-				# inadvertently change content. Baaaad idea.
-				$rlink = wfMsgHtml( 'revertimg' );
-				$dlink = $del;
-			}
+		$row = '';
+		
+		// Deletion link
+		if( $local && $wgUser->isAllowed( 'delete' ) ) {
+			$row .= '<td>';
+			$q[] = 'action=delete';
+			$q[] = ( $iscur ? 'image=' . $this->title->getPartialUrl() : 'oldimage=' . urlencode( $img ) );
+			if( !$iscur )
+				$q[] = 'wpEditToken=' . urlencode( $wgUser->editToken( $img ) );
+			$row .= '(' . $this->skin->makeKnownLinkObj(
+				$this->title,
+				wfMsgHtml( $iscur ? 'filehist-deleteall' : 'filehist-deleteone' ),
+				implode( '&', $q )
+			) . ')';
+			$row .= '</td>';
 		}
-
-		if ( $local ) {
-			$userlink = $this->skin->userLink( $user, $usertext ) . $this->skin->userToolLinks( $user, $usertext );
-		} else {
-			$userlink = htmlspecialchars( $usertext );
+		
+		// Reversion link/current indicator
+		$row .= '<td>';
+		if( $iscur ) {
+			$row .= '(' . wfMsgHtml( 'filehist-current' ) . ')';
+		} elseif( $local && $wgUser->isLoggedIn() && $this->title->userCan( 'edit' ) ) {
+			$q[] = 'action=revert';
+			$q[] = 'oldimage=' . urlencode( $img );
+			$q[] = 'wpEditToken=' . urlencode( $wgUser->editToken( $img ) );
+			$row .= '(' . $this->skin->makeKnownLinkObj(
+				$this->title,
+				wfMsgHtml( 'filehist-revert' ),
+				implode( '&', $q )
+			) . ')';
 		}
-		$nbytes = wfMsgExt( 'nbytes', array( 'parsemag', 'escape' ),
-			$wgLang->formatNum( $size ) );
-		$widthheight = wfMsgHtml( 'widthheight', $width, $height );
-		$style = $this->skin->getInternalLinkAttributes( $url, $datetime );
-
-		$s = "<li> ({$dlink}) ({$rlink}) <a href=\"{$url}\"{$style}>{$datetime}</a> . . {$userlink} . . {$widthheight} ({$nbytes})";
-
-		$s .= $this->skin->commentBlock( $description, $wgTitle );
-		$s .= "</li>\n";
-		return $s;
+		$row .= '</td>';
+		
+		// Date/time and image link
+		$row .= '<td>';
+		$url = $iscur ? $this->img->getUrl() : $this->img->getArchiveUrl( $img );
+		$row .= Xml::element(
+			'a',
+			array( 'href' => $url ),
+			$wgLang->timeAndDate( $timestamp, true )
+		);
+		$row .= '</td>';
+		
+		// Uploading user
+		$row .= '<td>';
+		if( $local ) {
+			$row .= $this->skin->userLink( $user, $usertext ) . $this->skin->userToolLinks( $user, $usertext );
+		} else {
+			$row .= htmlspecialchars( $usertext );
+		}
+		$row .= '</td>';
+		
+		// Image dimensions
+		// FIXME: What about sound files? Should have the duration instead...
+		$row .= '<td>' . wfMsgHtml( 'widthheight', $width, $height ) . '</td>';
+		
+		// File size
+		$row .= '<td>' . $this->skin->formatSize( $size ) . '</td>';
+		
+		// Comment
+		$row .= '<td>' . $this->skin->formatComment( $description, $this->title ) . '</td>';
+		
+		return "<tr>{$row}</tr>\n";
 	}
+
 }
-
-
-
