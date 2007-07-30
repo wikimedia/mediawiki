bc85bbfa03fa9d554310e2188533333310cccc83
Rob Church <robchurch@users.mediawiki.org>
2007-07-30 00:46:09 +0000

    Some style tweaks, per Brion

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1195,7 +1195,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '92';
+$wgStyleVersion = '93';
 
 
 # Server-side caching:
