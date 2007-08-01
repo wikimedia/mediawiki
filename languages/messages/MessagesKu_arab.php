ad6c0ba0a600941ab66f3a3b5407bac07bf39a2f
Raimond Spekking <raymond@users.mediawiki.org>
2007-08-01 07:34:12 +0000

    Update Kurdish translations (arabic script)

--- languages/messages/MessagesKu_arab.php
+++ languages/messages/MessagesKu_arab.php
@@ -8,4 +8,27 @@
 
 $fallback = 'ku-latn';
 
+$skinNames = array(
+'standard'    => 'كلاسیك',
+'nostalgia'   => 'قاوه‌یی',
+'cologneblue' => 'شین',
+'monobook'    => 'مۆنۆ',
+'myskin'      => 'پێستی خۆم',
+'chick'       => 'جوجه‌',
+'simple'      => 'ساده‌'
+);
 
+$messages = array(
+# User preference toggles
+'tog-underline'       => 'هێڵ به‌ژێر به‌سه‌ره‌كاندا:',
+'tog-highlightbroken' => 'لابردنی به‌سته‌ری په‌ڕه‌ به‌تاڵه‌كان',
+'tog-justify'         => 'ده‌ق وه‌كو كۆپله‌',
+'tog-hideminor'       => 'گۆڕانكارییه‌ بچووكه‌كان بشاره‌وه‌',
+'tog-extendwatchlist' => 'لیسته‌ی چاودێری كردن - په‌ره‌پێدراو',
+'tog-usenewrc'        => 'نیشاندان په‌ره‌پێدراو (پێویستی به‌  JavaScript)',
+'tog-numberheadings'  => 'هێڵ به‌ژێر سه‌ردێره‌كان',
+'tog-showtoolbar'     => 'ئامرازه‌كانی كاركردن نیشانبده‌',
+'tog-editondblclick'  => 'په‌ڕه‌كان به‌ دوو كرته‌ بكه‌وه‌ بۆ كارتیاكردن (JavaScript)',
+'tog-editsection'     => 'به‌سته‌ر بۆ كۆپله‌كان بۆ ده‌ستكاریی كردن نیشانبده‌',
+
+);
