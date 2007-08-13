190f17af229c42c1b6be82074a70138c0cf4e7b4
Raimond Spekking <raymond@users.mediawiki.org>
2007-08-13 13:40:48 +0000

    * (bug 10906) Update for Haitian Creole language (ht) and namespace translation Patch by Remi Kaupp

---
+++ languages/messages/MessagesHt.php
@@ -0,0 +1,84 @@
+<?php
+/** Haitian Creole (Kreyòl ayisyen)
+ *
+ * @addtogroup Language
+ *
+ */
+
+$namespaceNames = array(
+	NS_MEDIA          => 'Medya',
+	NS_SPECIAL        => 'Espesyal',
+	NS_MAIN           => '',
+	NS_TALK           => 'Diskite',
+	NS_USER           => 'Itilizatè',
+	NS_USER_TALK      => 'Diskisyon_Itilizatè',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK   => 'Diskisyon_$1',
+	NS_IMAGE          => 'Imaj',
+	NS_IMAGE_TALK     => 'Diskisyon_Imaj',
+	NS_MEDIAWIKI      => 'MedyaWiki',
+	NS_MEDIAWIKI_TALK => 'Diskisyon_MedyaWiki',
+	NS_TEMPLATE       => 'Modèl',
+	NS_TEMPLATE_TALK  => 'Diskisyon_Modèl',
+	NS_HELP           => 'Èd',
+	NS_HELP_TALK      => 'Diskisyon_Èd',
+	NS_CATEGORY       => 'Kategori',
+	NS_CATEGORY_TALK  => 'Diskisyon_Kategori'
+);
+
+$linkTrail = '/^([a-zàèòÀÈÒ]+)(.*)$/sDu';
+
+$messages = array(
+# Dates
+'sunday'        => 'dimanch',
+'monday'        => 'lendi',
+'tuesday'       => 'mardi',
+'wednesday'     => 'mèkredi',
+'thursday'      => 'jedi',
+'friday'        => 'vandredi',
+'saturday'      => 'samdi',
+'sun'           => 'dim',
+'mon'           => 'len',
+'tue'           => 'mar',
+'wed'           => 'mèr',
+'thu'           => 'jed',
+'fri'           => 'van',
+'sat'           => 'sam',
+'january'       => 'janvye',
+'february'      => 'fevriye',
+'march'         => 'mas',
+'april'         => 'avril',
+'may_long'      => 'me',
+'june'          => 'jen',
+'july'          => 'jiyè',
+'august'        => 'out',
+'september'     => 'septanm',
+'october'       => 'oktòb',
+'november'      => 'novanm',
+'december'      => 'desanm',
+'january-gen'   => 'janvye',
+'february-gen'  => 'fevriye',
+'march-gen'     => 'mas',
+'april-gen'     => 'avril',
+'may-gen'       => 'me',
+'june-gen'      => 'jen',
+'july-gen'      => 'jiyè',
+'august-gen'    => 'out',
+'september-gen' => 'septanm',
+'october-gen'   => 'oktòb',
+'november-gen'  => 'novanm',
+'december-gen'  => 'desanm',
+'jan'           => 'jan',
+'feb'           => 'fev',
+'mar'           => 'mas',
+'apr'           => 'avr',
+'may'           => 'me',
+'jun'           => 'jen',
+'jul'           => 'jiy',
+'aug'           => 'out',
+'sep'           => 'sep',
+'oct'           => 'okt',
+'nov'           => 'nov',
+'dec'           => 'des',
+
+);
