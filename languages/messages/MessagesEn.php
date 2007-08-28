78dfa6f8e8e73e990c6244f418373bd27d19f282
Rob Church <robchurch@users.mediawiki.org>
2007-08-28 16:50:31 +0000

    (bug 11022) Use a more accurate page title for Special:Whatlinkshere and Special:Recentchangeslinked

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1353,6 +1353,7 @@ Unselected groups will not be changed. You can deselect a group with CTRL + Left
 
 # Recent changes linked
 'recentchangeslinked'          => 'Related changes',
+'recentchangeslinked-title' => 'Changes related to $1',
 'recentchangeslinked-noresult' => 'No changes on linked pages during the given period.',
 'recentchangeslinked-summary'  => "This special page lists the last changes on pages who are linked. Pages on your watchlist are '''bold'''.",
 
@@ -1971,6 +1972,7 @@ Consult the [[Special:Log/delete|deletion log]] for a record of recent deletions
 
 # What links here
 'whatlinkshere'         => 'What links here',
+'whatlinkshere-title' => 'Pages that link to $1',
 'whatlinkshere-summary' => '', # only translate this message to other languages if you have to change it
 'whatlinkshere-barrow'  => '&lt;', # only translate this message to other languages if you have to change it
 'notargettitle'         => 'No target',
