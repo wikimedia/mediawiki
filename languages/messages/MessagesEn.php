acf5c172e292ecc0f83558e901a02b41eeaaf7ad
Aaron Schulz <aaron@users.mediawiki.org>
2007-08-31 08:19:49 +0000

    *Tweak odd phrase

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -952,7 +952,7 @@ format. Please enter a well-formatted address or empty that field.',
 
 The block was made by $1. The reason given is ''$2''.
 
-* Begin of block: $8
+* Start of block: $8
 * Expiry of block: $6
 * Intended blockee: $7
 
@@ -965,7 +965,7 @@ The reason given is this:
 
 :\'\'$2\'\'
 
-* Begin of block: $8
+* Start of block: $8
 * Expiry of block: $6
 
 You may contact $1 or one of the other
