c05344727d695482a8da608f9f41babd6e47ae27
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-04 18:51:17 +0000

    Localisation updates for MediaWiki core messages from Betawiki * Farsi (fa) by Huji * Français Cadien (frc) (new) by RoyAlcatraz and JeanVoisin * Kinaray-a (krj) (new) by RonaldPanaligan and Harvzsf * Winaray (war) (new) by Harvzsf * updated RELEASE-NOTES

---
+++ languages/messages/MessagesWar.php
@@ -0,0 +1,136 @@
+<?php
+/** Winaray
+ * This is the Winaray localisation file
+ *
+ * @author Harvzsf (August 2007)
+ * 
+ * @addtogroup Language
+ */
+
+$messages = array(
+# User preference toggles
+'tog-underline' => 'Bagisa ha ilarom an mga sumpay:',
+
+# Dates
+'sunday'        => 'Dominggo',
+'monday'        => 'Lunes',
+'tuesday'       => 'Martes',
+'wednesday'     => 'Miyerkoles',
+'thursday'      => 'Huwebes',
+'friday'        => 'Biyernes',
+'saturday'      => 'Sabado',
+'sun'           => 'Dom',
+'mon'           => 'Lun',
+'tue'           => 'Mar',
+'wed'           => 'Mi',
+'thu'           => 'Hu',
+'fri'           => 'Bi',
+'sat'           => 'Sab',
+'january'       => 'Enero',
+'february'      => 'Pebrero',
+'march'         => 'Marso',
+'april'         => 'Abril',
+'may_long'      => 'Mayo',
+'june'          => 'Hunyo',
+'july'          => 'Hulyo',
+'august'        => 'Agosto',
+'september'     => 'Septyembre',
+'october'       => 'Oktubre',
+'november'      => 'Nobyembre',
+'december'      => 'Disyembre',
+'january-gen'   => 'han Enero',
+'february-gen'  => 'han Pebrero',
+'march-gen'     => 'han Marso',
+'april-gen'     => 'han Abril',
+'may-gen'       => 'han Mayo',
+'june-gen'      => 'han Hunyo',
+'july-gen'      => 'han Hulyo',
+'august-gen'    => 'han Agosto',
+'september-gen' => 'han Septyembre',
+'october-gen'   => 'han Oktubre',
+'november-gen'  => 'han Nobyembre',
+'december-gen'  => 'han Disyembre',
+
+# Bits of text used by many pages
+'categories' => '{{PLURAL:$1|Kategorya|Mga Kategorya}}',
+
+'navigation' => 'Paglayag',
+
+'tagline'          => 'Tikang ha {{SITENAME}}',
+'help'             => 'Bulig',
+'search'           => 'Bilnga',
+'searchbutton'     => 'Bilnga',
+'go'               => 'Kadto-a',
+'searcharticle'    => 'Kadto-a',
+'history'          => 'Kaagi han pakli',
+'history_short'    => 'Kaagi',
+'printableversion' => 'Maipapatik nga bersyon',
+'permalink'        => 'Sumpay nga unob',
+'edit'             => 'Igliwat',
+'editthispage'     => 'Igliwat ini nga pakli',
+'delete'           => 'Para-a',
+'deletethispage'   => 'Para-a ini nga pakli',
+'newpage'          => 'Bag-o nga pakli',
+'talkpagelinktext' => 'Hiruhimangraw',
+'specialpage'      => 'Ispisyal nga Pakli',
+'talk'             => 'Hiruhimangraw',
+'views'            => 'Mga paglantaw',
+'toolbox'          => 'Garamiton',
+'otherlanguages'   => 'Ha iba nga mga yinaknan',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'currentevents'     => 'Mga panhitabo',
+'currentevents-url' => 'Project:Mga panhitabo',
+'disclaimers'       => 'Mga Disclaimer',
+'mainpage'          => 'Syahan nga Pakli',
+'portal'            => 'Ganghaan han Komunidad',
+'portal-url'        => 'Project:Ganghaan han Komunidad',
+'sitesupport'       => 'Mga Donasyon',
+'sitesupport-url'   => 'Project:Mga Donasyon',
+
+'youhavenewmessages' => 'Mayda ka $1 ($2).',
+'newmessageslink'    => 'bag-o nga mga mensahe',
+'hidetoc'            => 'tago-a',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'      => 'Artikulo',
+'nstab-mediawiki' => 'Mensahe',
+
+# Login and logout pages
+'login'              => 'Sakob',
+'userlogin'          => 'Sakob/Pagrehistro',
+'userlogout'         => 'Gawas',
+'loginlanguagelabel' => 'Yinaknan: $1',
+
+# Search results
+'powersearch' => 'Bilnga',
+
+# Recent changes
+'recentchanges' => 'Mga kabag-ohan',
+'hide'          => 'Tago-a',
+
+# Recent changes linked
+'recentchangeslinked' => 'Mga may kalabotan nga binag-o',
+
+# Upload
+'upload'    => 'Pagkarga hin file',
+'uploadbtn' => 'Igkarga an file',
+
+# Miscellaneous special pages
+'randompage'   => 'Bisan ano nga pakli',
+'longpages'    => 'Haglaba nga mga pakli',
+'specialpages' => 'Mga Ispisyal nga Pakli',
+'move'         => 'Balhina',
+
+# Watchlist
+'watchlist'     => 'Akon barantayan',
+'watch'         => 'Bantayi',
+'watchthispage' => 'Bantayi ini nga pakli',
+
+# Contributions
+'mycontris' => 'Akon mga ámot',
+
+# What links here
+'whatlinkshere' => 'Mga nasumpay dinhi',
+
+);
