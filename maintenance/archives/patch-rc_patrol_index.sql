74e1bf30a51bf4e8c1d335d2e40fb4bcc513bad7
Domas Mituzas <midom@users.mediawiki.org>
2007-09-05 08:14:18 +0000

    destroy rc_patrolling index, no removers added, I hope not that many people did run update with that revision :)

--- maintenance/archives/patch-rc_patrol_index.sql
+++
@@ -1,4 +0,0 @@
--- Index to speed up locating unpatrolled changes
--- matching specific edit criteria
-ALTER TABLE /*$wgDBprefix*/recentchanges
-	ADD INDEX `rc_patrolling` ( `rc_this_oldid` , `rc_last_oldid` , `rc_patrolled` );
