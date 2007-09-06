204d2b8fd8d8558968d68c57477d7d28e02ceb29
Greg Sabino Mullane <greg@users.mediawiki.org>
2007-09-06 16:32:00 +0000

    Since slashes in page titles have special meaning, force them to delimit words.

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -457,9 +457,9 @@ CREATE FUNCTION ts2_page_title() RETURNS TRIGGER LANGUAGE plpgsql AS
 $mw$
 BEGIN
 IF TG_OP = 'INSERT' THEN
-  NEW.titlevector = to_tsvector('default',NEW.page_title);
+  NEW.titlevector = to_tsvector('default',REPLACE(NEW.page_title,'/',' '));
 ELSIF NEW.page_title != OLD.page_title THEN
-  NEW.titlevector := to_tsvector('default',NEW.page_title);
+  NEW.titlevector := to_tsvector('default',REPLACE(NEW.page_title,'/',' '));
 END IF;
 RETURN NEW;
 END;
