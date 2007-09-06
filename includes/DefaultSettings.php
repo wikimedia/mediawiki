951b445982cf573511166e32a8650325a7848a27
Rob Church <robchurch@users.mediawiki.org>
2007-09-06 00:17:56 +0000

    * Bump version to 1.12alpha * Move old RELEASE NOTES to HISTORY, skeleton for RELEASE NOTES for 1.12

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -31,7 +31,7 @@ require_once( "$IP/includes/SiteConfiguration.php" );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.11alpha';
+$wgVersion			= '1.12alpha';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
