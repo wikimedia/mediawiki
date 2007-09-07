ab40d5ad524fad7f876593d55f7a917dbf4b2dd3
Brion Vibber <brion@users.mediawiki.org>
2007-09-07 13:30:28 +0000

    Revert 25602 and 25604 schema changes for now. Please don't commit schema changes to trunk until you've cleared them with me and Tim -- remember trunk is meant to be ready-to-run live at all times. They force operational work before we can update the software again, and it's a big pain in the ass to deal with them unexpectedly.

--- maintenance/archives/patch-rev_bot.sql
+++
@@ -1,5 +0,0 @@
--- Add rev_bot column.  We don't need an index if all we want to be able to do
--- is, e.g., hide bot edits: most edits are non-bot, so for normal use it's
--- better to just look at 20% more rows or whatever than to add a new index.
-ALTER TABLE /*$wgDBprefix*/revision
-  ADD COLUMN rev_bot tinyint unsigned NOT NULL default 0 AFTER rev_deleted;
