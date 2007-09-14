339e0681e314e3664166b3627c597a316292f476
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-14 23:07:02 +0000

    Localisation updates from Betawiki. * cs, za

--- languages/messages/MessagesZa.php
+++ languages/messages/MessagesZa.php
@@ -6,3 +6,76 @@
 
 $fallback = 'zh-cn';
 
+$messages = array(
+# Dates
+'january'       => 'Nin Wtj',
+'february'      => 'Nin Yeih',
+'march'         => 'Nin Sam',
+'april'         => 'Nin Seiq',
+'may_long'      => 'Nin Haj',
+'june'          => 'Nin Loekj',
+'july'          => 'Nin Caet',
+'august'        => 'Nin Betq',
+'september'     => 'Nin Gouj',
+'october'       => 'Nin Cwbx',
+'november'      => 'Nin Cwbx Wtj',
+'december'      => 'Nin Cwbx Yeih',
+'may-gen'       => 'Nin Haj',
+'september-gen' => 'Nin Gouj',
+'sep'           => 'Nin Gouj',
+
+'cancel'     => 'Fwtqfex',
+'mytalk'     => 'Gaeq gou gangj',
+'navigation' => 'Dazzaeuh',
+
+'help'          => 'Caeuq',
+'search'        => 'Cwmh',
+'searchbutton'  => 'Cwmh',
+'searcharticle' => 'Bei',
+'history_short' => 'Doenghmatq',
+'protect'       => 'Hahdiuz',
+'talk'          => 'Kwng',
+'toolbox'       => 'Loengx hong',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'mainpage' => 'Mae meh',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-user' => 'Mae bouxdeiz',
+
+# General errors
+'viewsource' => 'Liuq lagh mae-nej',
+
+# Login and logout pages
+'yourname'    => 'Ciu bouxdeiz:',
+'notloggedin' => 'Maengx laezhaej',
+'username'    => 'Ciu bouxdeiz:',
+
+# Edit pages
+'summary'   => 'Saegmangj',
+'watchthis' => 'Demq mae-nej',
+
+# Preferences page
+'mypreferences' => 'Doenghgaeq gou hae',
+
+# Recent changes
+'recentchanges' => 'Gaeqgaij suijnej',
+
+# Miscellaneous special pages
+'randompage'   => 'Mae zaihzex',
+'move'         => 'Caen',
+'movethispage' => 'Caen mae nej',
+
+# Watchlist
+'mywatchlist'   => 'Gaeq gou demq',
+'watch'         => 'Demq',
+'watchthispage' => 'Demq mae-nej',
+
+# What links here
+'whatlinkshere' => 'Maelaez lamh haenznej',
+
+# Move page
+'movepage' => 'Caen doengh mae',
+'movedto'  => 'Caen bei',
+
+);
