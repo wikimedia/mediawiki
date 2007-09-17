f3707e7044563baf952bfeb261533469b74fd4da
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-17 19:58:23 +0000

    Localisation updates from Betawiki. * sk, tet (new).

---
+++ languages/messages/MessagesTet.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Tetun
+ *
+ * @addtogroup Language
+ *
+ * @author MF-Warburg
+ */
+
+$messages = array(
+# User preference toggles
+'tog-hideminor' => "Lá'os hatudu osan-rahun sira iha mudansa foufoun sira",
+
+# Dates
+'sunday'    => 'Loron-domingu',
+'monday'    => 'Loron-segunda',
+'tuesday'   => 'Loron-tersa',
+'wednesday' => 'Loron-kuarta',
+'thursday'  => 'Loron-kinta',
+'friday'    => 'Loron-sesta',
+'saturday'  => 'Loron-sábadu',
+'january'   => 'Janeiru',
+'february'  => 'Fevereiru',
+'march'     => 'Marsu',
+'april'     => 'Abríl',
+'may_long'  => 'Maiu',
+'june'      => 'Juñu',
+'july'      => 'Jullu',
+'august'    => 'Agostu',
+'september' => 'Setembru',
+'october'   => 'Outubru',
+'november'  => 'Novembru',
+'december'  => 'Dezembru',
+
+# Bits of text used by many pages
+'categories'      => 'Kategoría',
+'pagecategories'  => '{{PLURAL:$1|Kategoría|Kategoría}}',
+'category_header' => 'Artigu iha kategoría "$1"',
+'category-empty'  => "''Iha kategoría ne'e iha momentu lá'os pájina ruma.''",
+
+'about'         => 'Kona-ba',
+'qbfind'        => 'Hetan',
+'qbedit'        => 'Edita',
+'qbpageoptions' => "Pájina ne'e",
+'qbmyoptions'   => "Ha'u-nia pájina sira",
+'mypage'        => "Ha'u-nia pájina",
+'mytalk'        => "Ha'u-nia diskusaun",
+
+'errorpagetitle' => 'Sala',
+'history'        => 'Istória pájina',
+'history_short'  => 'Istória',
+'edit'           => 'Edita',
+'editthispage'   => "Edita pájina ne'e",
+'delete'         => 'Halakon',
+'deletethispage' => "Halakon pájina ne'e",
+'protect'        => 'Proteje',
+'otherlanguages' => 'Iha lian seluk',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite' => 'Kona-ba {{SITENAME}}',
+'aboutpage' => 'Project:Kona-ba',
+'mainpage'  => 'Pájina Mahuluk',
+
+'versionrequired'     => 'Presiza MediaWiki versaun $1',
+'versionrequiredtext' => "Presiza MediaWiki versaun $1 ba uza pájina ne'e. Haree [[Special:Version|pájina versaun]].",
+
+'youhavenewmessages' => 'Ó iha $1 ($2).',
+'newmessageslink'    => 'mensajen foun',
+'editsection'        => 'edita',
+'editold'            => 'edita',
+'showtoc'            => 'hatudu',
+'hidetoc'            => 'subar',
+
+# Short words for each namespace, by default used in the 'article' tab in monobook
+'nstab-main'    => 'Artigu',
+'nstab-project' => 'Pájina projetu nian',
+
+# General errors
+'error' => 'Sala',
+
+# User rights log
+'rightsnone' => '(mamuk)',
+
+# Recent changes
+'recentchanges'   => 'Mudansa foufoun sira',
+'minoreditletter' => 'k',
+'newpageletter'   => 'F',
+
+# Image list
+'ilsubmit'       => 'Buka',
+'imagelist_name' => 'Naran',
+'imagelist_user' => "Uza-na'in",
+
+# Miscellaneous special pages
+'allpages' => 'Pájina hotu',
+
+);
