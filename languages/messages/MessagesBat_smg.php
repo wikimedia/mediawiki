a01bacb052b0afd430d257bf96d68fa1cca9ee71
Raimond Spekking <raymond@users.mediawiki.org>
2007-10-08 13:43:36 +0000

    * (bug 11557) Samogitian namespace names Aliases from fallback 'lt' added to avoid broken links.

--- languages/messages/MessagesBat_smg.php
+++ languages/messages/MessagesBat_smg.php
@@ -4,6 +4,48 @@
   * @addtogroup Language
   */
 
+$namespaceNames = array(
+//	NS_MEDIA            => '',
+	NS_SPECIAL          => 'Specēlos',
+	NS_MAIN             => '',
+	NS_TALK             => 'Aptarėms',
+	NS_USER             => 'Nauduotuos',
+	NS_USER_TALK        => 'Nauduotuojė_aptarėms',
+	# NS_PROJECT set by $wgMetaNamespace
+	NS_PROJECT_TALK     => '$1_aptarėms',
+	NS_IMAGE            => 'Abruozdielis',
+	NS_IMAGE_TALK       => 'Abruozdielė_aptarėms',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_aptarėms',
+	NS_TEMPLATE         => 'Šabluons',
+	NS_TEMPLATE_TALK    => 'Šabluona_aptarėms',
+	NS_HELP             => 'Pagelba',
+	NS_HELP_TALK        => 'Pagelbas_aptarėms',
+	NS_CATEGORY         => 'Kateguorėjė',
+	NS_CATEGORY_TALK    => 'Kateguorėjės_aptarėms'
+);
+
+/** 
+  * Aliases from the fallback language 'lt' to avoid breakage of links
+  */
+
+$namespaceAliases = array(
+	'Specialus'             => NS_SPECIAL,
+	'Aptarimas'             => NS_TALK,
+	'Naudotojas'            => NS_USER,
+	'Naudotojo_aptarimas'   => NS_USER_TALK,
+	'$1_aptarimas'          => NS_PROJECT_TALK,
+	'Vaizdas'               => NS_IMAGE,
+	'Vaizdo_aptarimas'      => NS_IMAGE_TALK,
+	'MediaWiki_aptarimas'   => NS_MEDIAWIKI_TALK,
+	'Šablonas'              => NS_TEMPLATE,
+	'Šablono_aptarimas'     => NS_TEMPLATE_TALK,
+	'Pagalba'               => NS_HELP,
+	'Pagalbos_aptarimas'    => NS_HELP_TALK,
+	'Kategorija'            => NS_CATEGORY,
+	'Kategorijos_aptarimas' => NS_CATEGORY_TALK,
+);
+
 $fallback = 'lt';
 
 $messages = array(
