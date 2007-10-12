fb0a61bed71060bf5691664a666553caa9ca3ec1
Daniel Cannon <amidaniel@users.mediawiki.org>
2007-10-12 04:04:12 +0000

    API: Compatibility fix for 5.0.x. Define array_intersect_key if not already defined. Implementation by Rod Byrnes, published by php.net.

--- includes/api/ApiResult.php
+++ includes/api/ApiResult.php
@@ -179,3 +179,30 @@ class ApiResult extends ApiBase {
 	}
 }
 
+/* For compatibility with PHP versions < 5.1.0, define our own array_intersect_key function. */
+if (!function_exists('array_intersect_key')) {
+	function array_intersect_key($isec, $keys) {
+		$argc = func_num_args();
+		
+		if ($argc > 2) {
+			for ($i = 1; !empty($isec) && $i < $argc; $i++) {
+				$arr = func_get_arg($i);
+				
+				foreach (array_keys($isec) as $key) {
+					if (!isset($arr[$key])) 
+						unset($isec[$key]);
+				}
+			}
+			
+			return $isec;
+		} else {
+			$res = array();
+			foreach (array_keys($isec) as $key) {
+				if (isset($keys[$key]))
+					$res[$key] = $isec[$key];
+			}
+		
+			return $res;
+		}
+	}
+}
