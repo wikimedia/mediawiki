458016e5cab8ff597a3fd0fbd87c1527314be44e
David McCabe <david@users.mediawiki.org>
2007-10-15 21:12:28 +0000

    Added hook to SkinTemplate::tabAction.

--- includes/SkinTemplate.php
+++ includes/SkinTemplate.php
@@ -602,7 +602,12 @@ class SkinTemplate extends Skin {
 			global $wgContLang;
 			$text = $wgContLang->getFormattedNsText( Namespace::getSubject( $title->getNamespace() ) );
 		}
-
+		
+		$result = array();
+		if( wfRunHooks('SkinTemplateTabAction', array(&$this, $title, $message, $selected, $checkEdit,
+				&$classes, &$query, &$text, &$result)) ) {
+			return $result;
+		}
 		return array(
 			'class' => implode( ' ', $classes ),
 			'text' => $text,
