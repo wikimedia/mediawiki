bfcd95c8e5b8f88b2604cc017c7b090a6d10cfe8
Daniel Cannon <amidaniel@users.mediawiki.org>
2007-10-24 19:34:29 +0000

    (bug 11721) API: Use a different title for results than for the help page.

--- includes/api/ApiFormatBase.php
+++ includes/api/ApiFormatBase.php
@@ -111,7 +111,11 @@ abstract class ApiFormatBase extends ApiBase {
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-	<title>MediaWiki API</title>
+<? if ($this->mUnescapeAmps) { 
+?>	<title>MediaWiki API</title>
+<? } else {
+?>	<title>MediaWiki API Result</title>
+<? } ?>
 </head>
 <body>
 <?php
