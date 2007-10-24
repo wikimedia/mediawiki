0b4039c80f4a9cf8570b9aa7d5fb5697f68de6dd
Daniel Cannon <amidaniel@users.mediawiki.org>
2007-10-24 19:34:29 +0000

    (bug 11721) API: Use a different title for results than for the help page.

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -320,7 +320,6 @@ class ApiMain extends ApiBase {
 	protected function printResult($isError) {
 		$printer = $this->mPrinter;
 		$printer->profileIn();
-		$printer->initPrinter($isError);
 	
 		/* If the help message is requested in the default (xmlfm) format,
 		 * tell the printer not to escape ampersands so that our links do
@@ -328,6 +327,8 @@ class ApiMain extends ApiBase {
 		$params = $this->extractRequestParams();
 		$printer->setUnescapeAmps ( $this->mAction == 'help' 
 				&& $params['format'] == ApiMain::API_DEFAULT_FORMAT );
+
+		$printer->initPrinter($isError);
 
 		$printer->execute();
 		$printer->closePrinter();
