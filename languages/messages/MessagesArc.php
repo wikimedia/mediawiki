852c1772f7c83443d249361684d2120a05a72353
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-05 16:36:23 +0000

    Localisation updates from Betawiki. * arc

--- languages/messages/MessagesArc.php
+++ languages/messages/MessagesArc.php
@@ -1,14 +1,58 @@
 <?php
-
-/** Aramaic ( ܕܥܒܪܸܝܛ )
-  *
-  * @addtogroup Language
-  */
+/** Aramaic (ܐܪܡܝܐ)
+ *
+ * @addtogroup Language
+ *
+ * @author A2raya07
+ * @author The Thadman
+ * @author Siebrand
+ */
 
 $rtl = true;
+
 $defaultUserOptionOverrides = array(
 	# Swap sidebar to right side by default
 	'quickbar' => 2,
 );
 
+$messages = array(
+# Dates
+'april'  => 'ܢܝܣܢ',
+'august' => 'ܐܒ',
+'apr'    => 'ܢܝܣܢ',
+'aug'    => 'ܐܒ',
 
+'cancel' => 'ܒܛܘܠ',
+'mypage' => 'ܦܐܬܝ',
+'mytalk' => 'ܡܡܠܠܝ',
+
+# Login and logout pages
+'userlogout' => 'ܦܠܘܛ',
+
+# Preferences page
+'mypreferences' => 'ܦܪܝܫܘܬܝ',
+
+# Miscellaneous special pages
+'allpages' => 'ܟܠ ܦܐܬܐ',
+
+# Book sources
+'booksources' => 'ܙܠ',
+
+# Special:Allpages
+'allarticles'    => 'ܟܠ ܡ̈ܓܠܐ',
+'allpagessubmit' => 'ܙܠ',
+
+# Watchlist
+'mywatchlist' => 'ܟܬܒܢܘܬܝ',
+'watch'       => 'ܢܛܪ',
+
+# Namespace form on various pages
+'blanknamespace' => '(ܪܫܝܐ)',
+
+# Namespace 8 related
+'allmessagesname' => 'ܫܡܐ',
+
+# Attribution
+'and' => 'ܘ',
+
+);
