e7fd737fa370683af9addb5cc2d2447d4f2e5a51
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-05 17:01:33 +0000

    Localisation updates from Betawiki. * as

--- languages/messages/MessagesAs.php
+++ languages/messages/MessagesAs.php
@@ -2,6 +2,9 @@
 /** Assamese (অসমীয়া)
  *
  * @addtogroup Language
+ *
+ * @author Priyankoo
+ * @author Siebrand
  */
 
 $digitTransformTable = array(
@@ -17,3 +20,40 @@ $digitTransformTable = array(
 	'9' => '৯', # &#x09ef;
 );
 
+$messages = array(
+# Dates
+'april'      => 'এপ্ৰিল',
+'august'     => 'আগষ্ট',
+'april-gen'  => 'এপ্ৰিল',
+'august-gen' => 'আগষ্ট',
+'apr'        => 'এপ্ৰিল',
+'aug'        => 'আগষ্ট',
+
+'cancel'   => 'বৰ্খাস্ত কৰক',
+'mypage'   => 'মোৰ পৃষ্ঠা',
+'mytalk'   => 'মোৰ কথা-বতৰা',
+'anontalk' => 'এই IPটোৰে যোগাযোগ কৰক',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite' => '{{SITENAME}}ৰ বিষয়ে',
+
+# Edit pages
+'accmailtext' => '"$1"ৰ পাছৱৰ্ড $2 লৈ পঠোৱা হ\'ল|',
+
+# Preferences page
+'mypreferences' => 'মোৰ পচন্দ',
+
+# Special:Allpages
+'allpagesprev' => 'আগৰ',
+
+# Watchlist
+'watchlist'   => 'মই চোৱাচিতা কৰা পৃষ্ঠাসমুহ',
+'mywatchlist' => 'গতিবিধি লক্ষ্য কৰা পৃষ্ঠাসমূহ',
+
+# Contributions
+'mycontris' => 'মোৰ অৱদানসমুহ',
+
+# Block/unblock
+'blocklink' => 'বৰ্খাস্ত কৰক',
+
+);
