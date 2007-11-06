ee36bcdfb7f92b5ac369b89286a441c9356b3134
Raimond Spekking <raymond@users.mediawiki.org>
2007-11-06 19:31:08 +0000

    Add comments for optional messages

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -2999,17 +2999,17 @@ $1',
 'watchlisttools-raw'   => 'Edit raw watchlist',
 
 # Iranian month names
-'iranian-calendar-m1'  => 'Farvardin',
-'iranian-calendar-m2'  => 'Ordibehesht',
-'iranian-calendar-m3'  => 'Khordad',
-'iranian-calendar-m4'  => 'Tir',
-'iranian-calendar-m5'  => 'Mordad',
-'iranian-calendar-m6'  => 'Shahrivar',
-'iranian-calendar-m7'  => 'Mehr',
-'iranian-calendar-m8'  => 'Aban',
-'iranian-calendar-m9'  => 'Azar',
-'iranian-calendar-m10' => 'Dey',
-'iranian-calendar-m11' => 'Bahman',
-'iranian-calendar-m12' => 'Esfand',
+'iranian-calendar-m1'  => 'Farvardin', # only translate this message to other languages if you have to change it
+'iranian-calendar-m2'  => 'Ordibehesht', # only translate this message to other languages if you have to change it
+'iranian-calendar-m3'  => 'Khordad', # only translate this message to other languages if you have to change it
+'iranian-calendar-m4'  => 'Tir', # only translate this message to other languages if you have to change it
+'iranian-calendar-m5'  => 'Mordad', # only translate this message to other languages if you have to change it
+'iranian-calendar-m6'  => 'Shahrivar', # only translate this message to other languages if you have to change it
+'iranian-calendar-m7'  => 'Mehr', # only translate this message to other languages if you have to change it
+'iranian-calendar-m8'  => 'Aban', # only translate this message to other languages if you have to change it
+'iranian-calendar-m9'  => 'Azar', # only translate this message to other languages if you have to change it
+'iranian-calendar-m10' => 'Dey', # only translate this message to other languages if you have to change it
+'iranian-calendar-m11' => 'Bahman', # only translate this message to other languages if you have to change it
+'iranian-calendar-m12' => 'Esfand', # only translate this message to other languages if you have to change it
 
 );
