29f87d9598d63e9df88180aac8da93e77e49b7c7
Aaron Schulz <aaron@users.mediawiki.org>
2007-11-08 21:34:11 +0000

    This was changed a while ago

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1927,8 +1927,7 @@ comment field and all checkboxes.",
 'undeleterevisions'        => '$1 {{PLURAL:$1|revision|revisions}} archived',
 'undeletehistory'          => 'If you restore the page, all revisions will be restored to the history.
 If a new page with the same name has been created since the deletion, the restored
-revisions will appear in the prior history, and the current revision of the live page
-will not be automatically replaced. Also note that restrictions on file revisions are lost upon restoration',
+revisions will appear in the prior history. Also note that restrictions on file revisions are lost upon restoration',
 'undeleterevdel'           => "Undeletion will not be performed if it will result in the top page revision being
 partially deleted. In such cases, you must uncheck or unhide the newest deleted revisions. Revisions of files
 that you don't have permission to view will not be restored.",
