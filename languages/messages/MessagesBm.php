5e88034e593e7a22874f4bdabc8a65a9d44b8ebd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-15 19:03:08 +0000

    Importing usable messages from bm.wikipedia.org

--- languages/messages/MessagesBm.php
+++ languages/messages/MessagesBm.php
@@ -1,8 +1,127 @@
 <?php
-/** Bambara (Bamanankan)
-  *
-  * @addtogroup Language
-  */
+/** Bamanankan (Bamanankan)
+ *
+ * @addtogroup Language
+ *
+ * @author Guaka
+ * @author Siebrand
+ */
 
 $fallback = 'fr';
 
+$messages = array(
+# User preference toggles
+'tog-hideminor' => 'Yɛlɛmaliw kura fitini dogo',
+
+# Dates
+'sunday'    => 'karidon',
+'monday'    => 'ntɛnɛdon',
+'tuesday'   => 'taratadon',
+'wednesday' => 'arabadon',
+'thursday'  => 'alamisadon',
+'friday'    => 'jumadon',
+'saturday'  => 'sibiridon',
+'august'    => 'uti',
+'aug'       => 'uti',
+
+# Bits of text used by many pages
+'categories'      => 'Nyɛ ka cogoyaw',
+'category_header' => 'Nyɛw cogoya kɔnɔ: "$1"',
+
+'article'        => 'Nyɛ',
+'qbfind'         => 'Nyini',
+'qbedit'         => 'Yɛlɛma',
+'qbspecialpages' => 'Nyɛw kɛrɛnkɛrɛnnen',
+'mypage'         => "N'ka nyɛ",
+'mytalk'         => "N'ka baronyɛ",
+
+'help'           => 'Dɛmɛ',
+'search'         => 'Nyini',
+'history'        => 'Nyɛka tariku',
+'history_short'  => 'Tariku',
+'info_short'     => 'Kunnafoni',
+'edit'           => 'yɛlɛma',
+'editthispage'   => 'Nyɛ nin yɛlɛma',
+'newpage'        => 'Nyɛ kura',
+'specialpage'    => 'Nyɛ kɛrɛnkɛrɛnnen',
+'talk'           => 'Baro',
+'toolbox'        => 'Minanw',
+'userpage'       => 'Utilisateur ka nyɛ',
+'imagepage'      => 'Janin ka nyɛ',
+'otherlanguages' => 'Kanw wɛrɛ',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'currentevents'     => 'Kunnafoni',
+'currentevents-url' => 'Kunnafoni',
+'disclaimers'       => 'Kangari',
+'edithelp'          => 'Dɛmɛ',
+'mainpage'          => 'Nyɛ fɔlɔ',
+'portal'            => 'Jamaje',
+'sitesupport'       => 'Banumanke',
+
+'editsection' => 'yɛlɛma',
+'hidetoc'     => 'dogo',
+
+# Short words for each namespace, by default used in the namespace tab in monobook
+'nstab-main'     => 'Nyɛ',
+'nstab-image'    => 'Janin',
+'nstab-help'     => 'Dɛmɛ',
+'nstab-category' => 'Cogoya',
+
+# Edit pages
+'minoredit'      => 'Yɛlɛma fitini.',
+'watchthis'      => 'Nyɛ tulomajɔ',
+'newarticle'     => '(Kura)',
+'editing'        => '$1 ka yɛlɛmali',
+'editingsection' => '$1 ka yɛlɛmali (section)',
+'editingcomment' => '$1 ka yɛlɛmali (baro)',
+
+# Search results
+'powersearch' => 'Nyini',
+
+# Preferences page
+'preferences' => 'Fisayali',
+'skin'        => 'Apparence',
+
+# Recent changes
+'hide'     => 'dogo',
+
+# Image list
+'ilsubmit' => 'Nyini',
+
+# Random page
+'randompage' => 'Nyɛ min man ni ja',
+
+# Miscellaneous special pages
+'popularpages' => 'Nyɛw les plus consultées',
+'allpages'     => 'Nyɛw bɛɛ',
+'specialpages' => 'Nyɛw kɛrɛnkɛrɛnnen',
+'spheading'    => 'Nyɛw kɛrɛnkɛrɛnnen',
+'newpages'     => 'Nyɛw kura',
+'ancientpages' => 'Nyɛw les plus anciens',
+'move'         => 'telun',
+'movethispage' => 'Nyɛ telun',
+
+# Watchlist
+'watch'         => 'Tulomajɔ',
+'watchthispage' => 'Nyɛ tulomajɔ',
+
+# Contributions
+'mycontris' => "N'ka contributions",
+
+# Developer tools
+'lockconfirm' => 'Awɔ, je confirme que je souhaite verrouiller la base de données.',
+
+# Move page
+'movepage'    => 'Nyɛ telun',
+'movearticle' => 'Nyɛ telun',
+'movepagebtn' => 'Nyɛ telun',
+
+# Thumbnails
+'missingimage' => '<b>Janin tɛ</b><br /><i>$1</i>',
+
+# Attribution
+'and'    => 'ani',
+'others' => 'wɛrɛ',
+
+);
