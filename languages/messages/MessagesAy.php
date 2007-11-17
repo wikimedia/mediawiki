33709c60062102b90e50bd042637ee9c50c812f0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-17 23:36:07 +0000

    Importing usable interface messages from ay.wikipedia.org

--- languages/messages/MessagesAy.php
+++ languages/messages/MessagesAy.php
@@ -1,8 +1,122 @@
 <?php
-/** Aymara
-  *
-  * @addtogroup Language
-  */
-
+/** Aymara (Aymar)
+ *
+ * @addtogroup Language
+ *
+ * @author Kanon6917
+ * @author Siebrand
+ */
+ 
 $fallback = 'es';
 
+$messages = array(
+'article' => 'Amtachinu',
+'cancel'  => 'Jan saña',
+'qbfind'  => 'Thaqaña',
+'qbedit'  => 'Trukaña',
+
+'errorpagetitle'   => 'Pantja',
+'help'             => 'Yanapaña',
+'search'           => 'Thaqaña',
+'searchbutton'     => 'Thaqaña',
+'go'               => 'Saraña',
+'searcharticle'    => 'Saraña',
+'history'          => 'Nayra qillqata',
+'history_short'    => 'Nayra qillqata',
+'edit'             => 'Trukaña',
+'delete'           => 'Pichaña',
+'talkpagelinktext' => 'Kutkatasiña',
+'talk'             => 'Tuqisiwi',
+'otherlanguages'   => 'Yaqha arunaka',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'currentevents'     => 'Jichha pacha',
+'currentevents-url' => 'Jichha pacha',
+'mainpage'          => 'Nayriri Uñstawi',
+'portal'            => 'Jayma punku',
+'portal-url'        => 'Project:Jayma punku',
+'sitesupport'       => 'Ramañanaka',
+
+'editsection' => 'trukaña',
+'editold'     => 'trukaña',
+'showtoc'     => 'uñachayaña',
+
+# Short words for each namespace, by default used in the namespace tab in monobook
+'nstab-main'     => 'Amtachinu',
+'nstab-user'     => 'Apnaqiri',
+'nstab-help'     => 'Yanapaña',
+'nstab-category' => 'Patawi',
+
+# General errors
+'error' => 'Pantja',
+
+# Login and logout pages
+'loginpagetitle'     => "Qillqt'ayasiña/Entrar",
+'login'              => "Qillqt'ayasiña/Entrar",
+'userlogin'          => "Qillqt'ayasiña/Entrar",
+'logout'             => 'Mistuña',
+'userlogout'         => 'Mistuña',
+'yourlanguage'       => 'Aru:',
+'email'              => "Aru jalayiri (''E-mail'')",
+'loginlanguagelabel' => 'Aru: $1',
+
+# Edit pages
+'summary'  => 'Juk’aptayawi',
+'subject'  => 'Yatsuwi/titulu',
+'yourdiff' => 'Jalaqtatanaka',
+
+# Preferences page
+'prefs-rc'       => 'Jichha kutikiptatanaka',
+'timezoneoffset' => 'Jalaqtata',
+
+# Recent changes
+'recentchanges' => 'Jichha kutikiptatanaka',
+'show'          => 'uñachayaña',
+
+# Upload
+'fileuploadsummary' => 'Uñt’ayawi:',
+
+'license' => 'Iyawsawi',
+
+# Image list
+'filehist-user'         => 'Apnaqiri',
+'imagelist_date'        => 'Uru jakhu',
+'imagelist_user'        => 'Apnaqiri',
+'imagelist_description' => 'Uñt’ayawi',
+
+# Special:Log
+'specialloguserlabel'  => 'Apnaqiri:',
+'speciallogtitlelabel' => 'Titulu:',
+'log-search-submit'    => 'Saraña',
+
+# Special:Listusers
+'listusers-submit' => 'Uñachayaña',
+
+# Watchlist
+'watch' => 'Uñjaña',
+
+# Undelete
+'undelete-search-submit' => 'Thaqaña',
+
+'sp-contributions-submit' => 'Thaqaña',
+
+# Block/unblock
+'ipblocklist-submit' => 'Thaqaña',
+'proxyblocksuccess'  => 'Kamachawi',
+
+# Tooltip help for the actions
+'tooltip-p-logo' => 'Nayriri Uñstawi',
+
+# Attribution
+'others' => 'yaqha',
+
+# action=purge
+'confirm_purge_button' => 'Iyaw saña',
+
+# Multipage image navigation
+'imgmultigo' => '¡Saraña!',
+
+# Table pager
+'table_pager_limit_submit' => 'Saraña',
+
+);
