a2bb4c7f71360e1bee08067826f4e443db9873a4
Domas Mituzas <midom@users.mediawiki.org>
2007-12-02 16:17:30 +0000

    mark unused images as expensive, and fix queries to be cacheable

--- includes/SpecialUnusedimages.php
+++ includes/SpecialUnusedimages.php
@@ -9,7 +9,9 @@
  * @addtogroup SpecialPage
  */
 class UnusedimagesPage extends ImageQueryPage {
-
+	
+	function isExpensive() { return true; }
+	
 	function getName() {
 		return 'Unusedimages';
 	}
@@ -26,16 +28,18 @@ class UnusedimagesPage extends ImageQueryPage {
 		if ( $wgCountCategorizedImagesAsUsed ) {
 			list( $page, $image, $imagelinks, $categorylinks ) = $dbr->tableNamesN( 'page', 'image', 'imagelinks', 'categorylinks' );
 
-			return 'SELECT img_name as title, img_user, img_user_text, img_timestamp as value, img_description
-					FROM ((('.$page.' AS I LEFT JOIN '.$categorylinks.' AS L ON I.page_id = L.cl_from)
-						LEFT JOIN '.$imagelinks.' AS P ON I.page_title = P.il_to)
-						INNER JOIN '.$image.' AS G ON I.page_title = G.img_name)
-					WHERE I.page_namespace = '.NS_IMAGE.' AND L.cl_from IS NULL AND P.il_to IS NULL';
+			return "SELECT 'Unusedimages' as type, 6 as namespace, img_name as title, img_timestamp as value,
+						img_user, img_user_text,  img_description
+					FROM ((($page AS I LEFT JOIN $categorylinks AS L ON I.page_id = L.cl_from)
+						LEFT JOIN $imagelinks AS P ON I.page_title = P.il_to)
+						INNER JOIN $image AS G ON I.page_title = G.img_name)
+					WHERE I.page_namespace = ".NS_IMAGE." AND L.cl_from IS NULL AND P.il_to IS NULL";
 		} else {
 			list( $image, $imagelinks ) = $dbr->tableNamesN( 'image','imagelinks' );
 
-			return 'SELECT img_name as title, img_user, img_user_text, img_timestamp as value, img_description' .
-			' FROM '.$image.' LEFT JOIN '.$imagelinks.' ON img_name=il_to WHERE il_to IS NULL ';
+			return "SELECT 'Unusedimages' as type, 6 as namespace, img_name as title, img_timestamp as value, 
+				img_user, img_user_text,  img_description
+				FROM $image LEFT JOIN $imagelinks ON img_name=il_to WHERE il_to IS NULL ";
 		}
 	}
 
