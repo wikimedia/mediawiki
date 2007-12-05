91422b75896b551d030f630ca90eb34010d6415e
Shinjiman <shinjiman@users.mediawiki.org>
2007-12-05 02:43:35 +0000

    update for the generic Chinese message file, when the $wgContLanguageCode is set to 'zh', reflecting the site-wide settings when it is set to 'zh'

--- languages/messages/MessagesZh.php
+++ languages/messages/MessagesZh.php
@@ -8,3 +8,21 @@
 # Stub message file for converter code "zh"
 
 $fallback = 'zh-hans';
+
+$messages = array(
+
+/*
+Short names for language variants used for language conversion links.
+To disable showing a particular link, set it to 'disable', e.g.
+'variantname-zh-sg' => 'disable',
+Variants for Chinese language
+*/
+'variantname-zh-hans' => '简体',
+'variantname-zh-hant' => '繁體',
+'variantname-zh-cn'   => '大陆简体',
+'variantname-zh-tw'   => '台灣正體',
+'variantname-zh-hk'   => '香港繁體',
+'variantname-zh-sg'   => '新加坡简体',
+'variantname-zh'      => '不转换／不轉換',
+
+);
