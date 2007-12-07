b7286dad585a63e383440e3a511324e3bdf182db
Brion Vibber <brion@users.mediawiki.org>
2007-12-07 15:27:27 +0000

    Keep rollback disabled for regular users for now. (Can be configured per-site if there is consensus to do so.)

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1065,7 +1065,6 @@ $wgGroupPermissions['user' ]['reupload']        = true;
 $wgGroupPermissions['user' ]['reupload-shared'] = true;
 $wgGroupPermissions['user' ]['minoredit']       = true;
 $wgGroupPermissions['user' ]['purge']           = true; // can use ?action=purge without clicking "ok"
-$wgGroupPermissions['user' ]['rollback']        = true;
 
 // Implicit group for accounts that pass $wgAutoConfirmAge
 $wgGroupPermissions['autoconfirmed']['autoconfirmed'] = true;
