609c73f6be2ad2504ab15ba0d59dcb2c7b6c96a7
Greg Sabino Mullane <greg@users.mediawiki.org>
2007-12-16 23:35:14 +0000

    Yet more cleanup of the protected_titles stuff.

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -509,14 +509,13 @@ CREATE UNIQUE INDEX pf_name_server ON profiling (pf_name, pf_server);
 CREATE TABLE protected_titles (
   pt_namespace   SMALLINT    NOT NULL,
   pt_title       TEXT        NOT NULL,
-  pt_by          INTEGER     NOT NULL,
-  pt_reason      TEXT,
+  pt_user        INTEGER         NULL  REFERENCES mwuser(user_id) ON DELETE SET NULL,
+  pt_reason      TEXT            NULL,
   pt_timestamp   TIMESTAMPTZ NOT NULL,
   pt_expiry      TIMESTAMPTZ     NULL,
   pt_create_perm TEXT        NOT NULL DEFAULT ''
 );
 CREATE UNIQUE INDEX protected_titles_unique ON protected_titles(pt_namespace, pt_title);
-CREATE INDEX protected_titles_index ON protected_titles(pt_by, pt_timestamp);
 
 CREATE TABLE mediawiki_version (
   type         TEXT         NOT NULL,
