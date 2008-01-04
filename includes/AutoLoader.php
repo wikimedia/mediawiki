ed0a58e5271ec5f23528c9f2850f925aafd3ae68
Roan Kattouw <catrope@users.mediawiki.org>
2008-01-04 22:33:22 +0000

    Re-adding ApiChangeRights, but commenting out its entries in ApiMain and AutoLoader. This way the module will be inaccessible for clients, but still present in SVN to be viewed and modified.

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -349,6 +349,8 @@ function __autoload($className) {
 
 		# apiedit branch
 		'ApiBlock' => 'includes/api/ApiBlock.php',
+		#'ApiChangeRights' => 'includes/api/ApiChangeRights.php',
+		# Disabled for now
 		'ApiDelete' => 'includes/api/ApiDelete.php',
 		'ApiMove' => 'includes/api/ApiMove.php',
 		'ApiProtect' => 'includes/api/ApiProtect.php',
