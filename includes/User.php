12812855bf90395a678e0a09b18d8c0b9abca44f
Domas Mituzas <midom@users.mediawiki.org>
2008-01-12 23:10:08 +0000

    Well, logging out is an action, where people don't want to have any of their their credentials still sit in browser. Actually, keeping a session is still bad. And trying to ensure that they don't see cached content... Well, thats wrong idea. See, if someone is logged out, he is anonymous and deserves to see cached content as everyone else.
    So, let's destroy all cookies.

--- includes/User.php
+++ includes/User.php
@@ -1977,12 +1977,13 @@ class User {
 		$this->clearInstanceCache( 'defaults' );
 
 		$_SESSION['wsUserID'] = 0;
-
+		
 		setcookie( $wgCookiePrefix.'UserID', '', time() - 3600, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
 		setcookie( $wgCookiePrefix.'Token', '', time() - 3600, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
-
-		# Remember when user logged out, to prevent seeing cached pages
-		setcookie( $wgCookiePrefix.'LoggedOut', wfTimestampNow(), time() + 86400, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
+		setcookie( $wgCookiePrefix.'UserName', '', time() - 3600, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
+		setcookie( $wgCookiePrefix.'LoggedOut', '', time() - 3600, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
+		setcookie( session_name(), '', time() - 3600, $wgCookiePath, $wgCookieDomain, $wgCookieSecure );
+		session_destroy();
 	}
 
 	/**
