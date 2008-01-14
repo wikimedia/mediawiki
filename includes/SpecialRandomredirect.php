411f83e07b86c7c1f39312df4ef3c8d5b4d8a8f8
Domas Mituzas <midom@users.mediawiki.org>
2008-01-14 12:50:20 +0000

    Cleanup for Special:Randompage and Special: Randomredirect after r29725 - http://bugzilla.wikimedia.org/show_bug.cgi?id=12624 Thanks to: alex.emsenhuber

--- includes/SpecialRandomredirect.php
+++ includes/SpecialRandomredirect.php
@@ -7,27 +7,14 @@
  * @author Rob Church <robchur@gmail.com>, Ilmari Karonen
  * @license GNU General Public Licence 2.0 or later
  */
-
-/**
- * Main execution point
- * @param $par Namespace to select the redirect from
- */
-function wfSpecialRandomredirect( $par = null ) {
-	global $wgOut, $wgContLang;
-
-	$rnd = new RandomPage();
-	$rnd->setNamespace( $wgContLang->getNsIndex( $par ) );
-	$rnd->setRedirect( true );
-
-	$title = $rnd->getRandomTitle();
-
-	if( is_null( $title ) ) {
-		$wgOut->addWikiText( wfMsg( 'randomredirect-nopages' ) );
-		return;
+class SpecialRandomredirect extends RandomPage {
+	function __construct(){
+		parent::__construct( 'Randomredirect' );	
 	}
 
-	$wgOut->reportTime();
-	$wgOut->redirect( $title->getFullUrl( 'redirect=no' ) );
+	// Override parent::isRedirect()
+	public function isRedirect(){
+		return true;
+	}
 }
-
 
