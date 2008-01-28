d32cab9f45c293e411fc9325e34207e69ac9c774
River Tarnell <river@users.mediawiki.org>
2008-01-28 08:55:36 +0000

    $wgStyleVersion++

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1314,7 +1314,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '106';
+$wgStyleVersion = '107';
 
 
 # Server-side caching:
