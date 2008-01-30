e48241342938e65c36fe4b6b6de752c00e2a6061
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-30 22:47:53 +0000

    Localisation updates for core messages from Betawiki

--- languages/messages/MessagesHu.php
+++ languages/messages/MessagesHu.php
@@ -1569,6 +1569,8 @@ Visszajelzés és további segítség:
 'excontentauthor'             => 'a lap tartalma: „$1” (és csak „$2” szerkesztette)',
 'exbeforeblank'               => 'az eltávolítás előtti tartalom: $1',
 'exblank'                     => 'a lap üres volt',
+'delete-confirm'              => '$1 törlése',
+'delete-legend'               => 'Törlés',
 'historywarning'              => 'Figyelem: a lapnak, amit törölni készülsz, története van:',
 'confirmdeletetext'           => 'Egy lap vagy kép teljes laptörténetével együtti
 végleges törlésére készülsz.
