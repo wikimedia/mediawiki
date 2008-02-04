67def013d952916607bccaa7967efe2b84a29bbe
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-04 20:21:00 +0000

    add dummy language file for 'be-x-old'

---
+++ languages/messages/MessagesBe_x_old.php
@@ -0,0 +1,8 @@
+<?php
+/** Belarusian in Taraškievica orthography (Беларуская тарашкевіца)
+  *
+  * @addtogroup Language
+  * @comment dummy language file. Falls back to 'be-tarask'. Backward compat.
+  */
+
+$fallback = 'be-tarask';
