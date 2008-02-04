cf9cd9dad33740aa788acc7bf4cfc5de771e27c9
Brion Vibber <brion@users.mediawiki.org>
2008-02-04 19:53:21 +0000

    Reverting r30413 for now. 'parentpagename' seems to duplicate 'basepagename'; why?

--- includes/Parser_OldPP.php
+++ includes/Parser_OldPP.php
@@ -2453,15 +2453,7 @@ class Parser_OldPP
 			case 'basepagename':
 				return wfEscapeWikiText( $this->mTitle->getBaseText() );
 			case 'basepagenamee':
-                return wfUrlEncode( str_replace( ' ', '_', $this->mTitle->getBaseText() ) );
-			case 'parentpagename':
-				return wfEscapeWikiText( $this->mTitle->getBaseText() );
-			case 'parentpagenamee':
 				return wfUrlEncode( str_replace( ' ', '_', $this->mTitle->getBaseText() ) );
-			case 'rootpagename':
-				return wfEscapeWikiText( $this->mTitle->getRootText() );
-			case 'rootpagenamee':
-				return wfUrlEncode( str_replace( ' ', '_', $this->mTitle->getRootText() ) );
 			case 'talkpagename':
 				if( $this->mTitle->canTalk() ) {
 					$talkPage = $this->mTitle->getTalkPage();
