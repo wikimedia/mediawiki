6e6d23ad20a261088884667f59cf9fe3bf4bd310
Huji <huji@users.mediawiki.org>
2008-02-13 09:10:36 +0000

    (bug 12972) Add page break rules to the "print" style-sheet

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1325,7 +1325,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '114';
+$wgStyleVersion = '115';
 
 
 # Server-side caching:
