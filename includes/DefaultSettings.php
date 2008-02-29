fd38d9f46aaa6456645f022c0e2da81e814e7df0
Huji <huji@users.mediawiki.org>
2008-02-29 13:59:11 +0000

    Making the empty space to the sides and the bottom of the category box equal in size, for monobook skin.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1326,7 +1326,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '118';
+$wgStyleVersion = '119';
 
 
 # Server-side caching:
