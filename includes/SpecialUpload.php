039e23c42368a5ca114846a8b4aff759b9235de2
Niklas Laxström <nikerabbit@users.mediawiki.org>
2008-03-07 10:35:19 +0000

    * Let's remove hardcoding together

--- includes/SpecialUpload.php
+++ includes/SpecialUpload.php
@@ -999,8 +999,8 @@ wgAjaxLicensePreview = {$alp};
 			}
 		}
 
-		$sourcefilename = wfMsgHtml( 'sourcefilename' );
-		$destfilename = wfMsgHtml( 'destfilename' );
+		$sourcefilename = wfMsgExt( 'sourcefilename', 'escapenoentities' );
+		$destfilename = wfMsgExt( 'destfilename', 'escapenoentities' );
 		$summary = wfMsgExt( 'fileuploadsummary', 'parseinline' );
 
 		$licenses = new Licenses();
@@ -1060,20 +1060,19 @@ wgAjaxLicensePreview = {$alp};
 		$encComment = htmlspecialchars( $this->mComment );
 		$align1 = $wgContLang->isRTL() ? 'left' : 'right';
 		$align2 = $wgContLang->isRTL() ? 'right' : 'left';
-		$colon = wfMsgExt( 'colon-separator', 'escapenoentities' );
 
 		$wgOut->addHTML( <<<EOT
 	<form id='upload' method='post' enctype='multipart/form-data' action="$action">
 		<table border='0'>
 		<tr>
 			{$this->uploadFormTextTop}
-			<td align='$align1' valign='top'><label for='wpUploadFile'>{$sourcefilename}$colon</label></td>
+			<td align='$align1' valign='top'><label for='wpUploadFile'>{$sourcefilename}</label></td>
 			<td align='$align2'>
 				{$filename_form}
 			</td>
 		</tr>
 		<tr>
-			<td align='$align1'><label for='wpDestFile'>{$destfilename}$colon</label></td>
+			<td align='$align1'><label for='wpDestFile'>{$destfilename}</label></td>
 			<td align='$align2'>
 				<input tabindex='2' type='text' name='wpDestFile' id='wpDestFile' size='60' 
 					value="$encDestName" $destOnkeyup />
@@ -1094,7 +1093,7 @@ EOT
 		if ( $licenseshtml != '' ) {
 			global $wgStylePath;
 			$wgOut->addHTML( "
-			<td align='$align1'><label for='wpLicense'>$license$colon</label></td>
+			<td align='$align1'><label for='wpLicense'>$license</label></td>
 			<td align='$align2'>
 				<select name='wpLicense' id='wpLicense' tabindex='4'
 					onchange='licenseSelectorCheck()'>
@@ -1114,18 +1113,18 @@ EOT
 		}
 
 		if ( $wgUseCopyrightUpload ) {
-			$filestatus = wfMsgHtml ( 'filestatus' );
+			$filestatus = wfMsgExt( 'filestatus', 'escapenoentities' );
 			$copystatus =  htmlspecialchars( $this->mCopyrightStatus );
-			$filesource = wfMsgHtml ( 'filesource' );
+			$filesource = wfMsgExt( 'filesource', 'escapenoentities' );
 			$uploadsource = htmlspecialchars( $this->mCopyrightSource );
 
 			$wgOut->addHTML( "
-				<td align='$align1' nowrap='nowrap'><label for='wpUploadCopyStatus'>$filestatus$colon</label></td>
+				<td align='$align1' nowrap='nowrap'><label for='wpUploadCopyStatus'>$filestatus</label></td>
 					<td><input tabindex='5' type='text' name='wpUploadCopyStatus' id='wpUploadCopyStatus' 
 					  value=\"$copystatus\" size='60' /></td>
 			</tr>
 			<tr>
-				<td align='$align1'><label for='wpUploadCopyStatus'>$filesource$colon</label></td>
+				<td align='$align1'><label for='wpUploadCopyStatus'>$filesource</label></td>
 					<td><input tabindex='6' type='text' name='wpUploadSource' id='wpUploadCopyStatus' 
 					  value=\"$uploadsource\" size='60' /></td>
 			</tr>
