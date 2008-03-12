721deaac2cb7bcde47313baa968bfb7761f1be8d
Rotem Liss <rotem@users.mediawiki.org>
2008-03-12 15:29:00 +0000

    Update.

--- languages/messages/MessagesHe.php
+++ languages/messages/MessagesHe.php
@@ -1639,6 +1639,7 @@ $NEWPAGE
 'modifiedarticleprotection'   => 'שינה את רמת ההגנה של [[$1]]',
 'unprotectedarticle'          => 'ביטל את ההגנה על [[$1]]',
 'protect-title'               => '(שינוי רמת ההגנה של "$1"',
+'protect-backlink'            => '→ $1',
 'protect-legend'              => 'אישור הפעלת ההגנה',
 'protectcomment'              => 'הערה:',
 'protectexpiry'               => 'פקיעת ההגנה:',
