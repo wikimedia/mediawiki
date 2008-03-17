3217cae8fe5fee1f8baf1ad40831a390263bfb8f
Huji <huji@users.mediawiki.org>
2008-03-17 21:34:06 +0000

    (bug 13407) MediaWiki:Powersearch is used in two places

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1226,6 +1226,7 @@ Make sure that this change will maintain historical page continuity.
 'showingresultsnum'     => "Showing below {{PLURAL:$3|'''1''' result|'''$3''' results}} starting with #'''$2'''.",
 'nonefound'             => "'''Note''': Unsuccessful searches are often caused by searching for common words like \"have\" and \"from\", which are not indexed, or by specifying more than one search term (only pages containing all of the search terms will appear in the result).",
 'powersearch'           => 'Advanced search',
+'powersearch-legend'    => 'Advanced search',
 'powersearchtext'       => 'Search in namespaces:<br />$1<br />$2 List redirects<br />Search for $3 $9',
 'searchdisabled'        => '{{SITENAME}} search is disabled. You can search via Google in the meantime. Note that their indexes of {{SITENAME}} content may be out of date.',
 'googlesearch'          => '<form method="get" action="http://www.google.com/search" id="googlesearch">
