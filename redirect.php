7176ccdf27fea2966183f77cdc1b949435360f28
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2008-03-19 12:43:00 +0000

    In continuation of r23243:  is already created by Setup.php, it don't need to set twice.

--- redirect.php
+++ redirect.php
@@ -2,12 +2,8 @@
 require_once( './includes/WebStart.php' );
 global $wgArticlePath;
 
-require_once( 'includes/WebRequest.php' );
-$wgRequest = new WebRequest();
-
 $page = $wgRequest->getVal( 'wpDropdown' );
 
 $url = str_replace( "$1", urlencode( $page ), $wgArticlePath );
 
 header( "Location: {$url}" );
-
