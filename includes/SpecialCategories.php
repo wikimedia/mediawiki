4aae5b078cdf4efe56aaf14b1e14da308c76f710
Aryeh Gregor <simetrical@users.mediawiki.org>
2008-03-20 01:49:33 +0000

    Nighttime commit when I'm tired and want to get this thing checked in and go to bed!  Yay!  Allow sorting by number of members on Special:Categories, and also allow descending sorts.  Mostlinkedcategories should be killed and turned into a redirect to this.

--- includes/SpecialCategories.php
+++ includes/SpecialCategories.php
@@ -13,7 +13,7 @@ function wfSpecialCategories() {
 		$cap->getNavigationBar()
 		. '<ul>' . $cap->getBody() . '</ul>' .
 		$cap->getNavigationBar()
-		);
+	);
 }
 
 /**
@@ -21,20 +21,42 @@ function wfSpecialCategories() {
  * @addtogroup Pager
  */
 class CategoryPager extends AlphabeticPager {
+	private $mOrderType = 'abc';
+
+	public function __construct() {
+		parent::__construct();
+		if( $this->mRequest->getText( 'order' ) == 'count' ) {
+			$this->mOrderType = 'count';
+		}
+		if( $this->mRequest->getText( 'direction' ) == 'asc' ) {
+			$this->mDefaultDirection = false;
+		} elseif( $this->mRequest->getText( 'direction' ) == 'desc'
+		|| $this->mOrderType == 'count' ) {
+			$this->mDefaultDirection = true;
+		}
+	}
+
 	function getQueryInfo() {
 		global $wgRequest;
 		return array(
-			'tables' => array('category'),
-			'fields' => array('cat_title','cat_pages')
+			'tables' => array( 'category' ),
+			'fields' => array( 'cat_title','cat_pages' ),
+			'conds' => array( 'cat_pages > 0' )
 		);
 	}
 	
 	function getIndexField() {
-		return "cat_title";
+		# We can't use mOrderType here, since this is called from the parent
+		# constructor.  Hmm.
+		if( $this->mRequest->getText( 'order' ) == 'count' ) {
+			return 'cat_pages';
+		} else {
+			return "cat_title";
+		}
 	}
 	
 	/* Override getBody to apply LinksBatch on resultset before actually outputting anything. */
-	function getBody() {
+	public function getBody() {
 		if (!$this->mQueryDone) {
 			$this->doQuery();
 		}
@@ -57,6 +79,39 @@ class CategoryPager extends AlphabeticPager {
 		$count = wfMsgExt( 'nmembers', array( 'parsemag', 'escape' ),
 				$wgLang->formatNum( $result->cat_pages ) );
 		return Xml::tags('li', null, "$titleText ($count)" ) . "\n";
+	}
+
+	/** Override this to order by count */
+	public function getNavigationBar() {
+		$nav = parent::getNavigationBar() . ' (';
+		if( $this->mOrderType == 'abc' ) {
+			$nav .= $this->makeLink(
+				wfMsgHTML( 'special-categories-sort-count' ),
+				array( 'order' => 'count' )
+			);
+		} else {
+			$nav .= $this->makeLink(
+				wfMsgHTML( 'special-categories-sort-abc' ),
+				array( 'order' => 'abc' )
+			);
+		}
+		$nav .= ') (';
+		# FIXME, these are stupid query names.  "order" and "dir" are already
+		# used.
+		if( $this->mDefaultDirection ) {
+			# Descending
+			$nav .= $this->makeLink(
+				wfMsgHTML( 'special-categories-sort-asc' ),
+				array( 'direction' => 'asc' )
+			);
+		} else {
+			$nav .= $this->makeLink(
+				wfMsgHTML( 'special-categories-sort-desc' ),
+				array( 'direction' => 'desc' )
+			);
+		}
+		$nav .= ')';
+		return $nav;
 	}
 }
 
