a5a0ef46b5af73688e8e90f76761b6955085976e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-23 10:53:09 +0000

    * Add Tatar in Cyrillic script with fallback to Russian * Fix capitalisation of Татарча

---
+++ languages/messages/MessagesTt_cyrl.php
@@ -0,0 +1,22 @@
+<?php
+/** Tatar (Cyrillic) (Tatarça/татарча (Cyrillic))
+ *
+ * @addtogroup Language
+ *
+ * @author Ерней
+ */
+
+$fallback = 'ru';
+
+$messages = array(
+# User preference toggles
+'tog-underline'        => 'Сылтамаларны астына сызу:',
+'tog-justify'          => 'Абзацларны киңлек буенча тигезләргә',
+'tog-hideminor'        => 'Баягы үзгәртүләрдә кечкенә төзәтүләрне яшерергә',
+'tog-extendwatchlist'  => 'Барлык кулланган үзгәртүләрне күрсәтү өчен күзәтү исемлегене җәяргә',
+'tog-showtoolbar'      => 'Төзәтү кораллар аслыгыны күрсәтергә (JavaScript)',
+'tog-editondblclick'   => 'Ике тапкыр чирттерү белән битләрне үзгәртергә (JavaScript)',
+'tog-showtoc'          => 'Эчтәлек җәдвәлене күрсәтергә (3-тән күбрәк башисемле битләр өчен)',
+'tog-rememberpassword' => 'Теркәү исемемне бу компьютердә онытмаска',
+
+);
