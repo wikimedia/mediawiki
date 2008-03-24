90f9e03cbed6faabd2b593db2889f3dd9101fec0
Brion Vibber <brion@users.mediawiki.org>
2008-03-24 19:27:25 +0000

    * (bug 11325) Wrapped page titles in MonoBook skin spaced more nicely

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1326,7 +1326,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '125';
+$wgStyleVersion = '126';
 
 
 # Server-side caching:
