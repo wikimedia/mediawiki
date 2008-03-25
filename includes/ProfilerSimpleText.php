5fb1c9c0b5575c4010aa96e6d3fcf80c6a153912
Domas Mituzas <midom@users.mediawiki.org>
2008-03-25 17:45:35 +0000

    make it static :)

--- includes/ProfilerSimpleText.php
+++ includes/ProfilerSimpleText.php
@@ -20,7 +20,7 @@ class ProfilerSimpleText extends ProfilerSimple {
 			else print "-->\n";
 	}
 	/* dense is good */
-	function sort($a,$b) { return $a['real']<$b['real']; /* sort descending by time elapsed */ }
-	function format($item,$key) { printf("%3.6f %6d - %s\n",$item['real'],$item['count'], $key); }	
+	static function sort($a,$b) { return $a['real']<$b['real']; /* sort descending by time elapsed */ }
+	static function format($item,$key) { printf("%3.6f %6d - %s\n",$item['real'],$item['count'], $key); }	
 }
 ?>
