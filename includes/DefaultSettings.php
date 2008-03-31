e9c8000b115647ec9481296a7ab08205bdff6b0d
Aaron Schulz <aaron@users.mediawiki.org>
2008-03-31 16:47:04 +0000

    $wgStyleVersion

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1324,7 +1324,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '128';
+$wgStyleVersion = '129';
 
 
 # Server-side caching:
