9155d276eb6cd48f83d2d71e844c111202f024a9
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-31 14:27:11 +0000

    Old Norse/non (dead language since 1300) removed as UI language.

--- languages/Names.php
+++ languages/Names.php
@@ -210,7 +210,6 @@
 	'nl' => 'Nederlands',	# Dutch
 	'nn' => "\xE2\x80\xAANorsk (nynorsk)\xE2\x80\xAC",	# Norwegian (Nynorsk)
 	'no' => "\xE2\x80\xAANorsk (bokmål)\xE2\x80\xAC",		# Norwegian
-	'non' => 'Norrǿna',		# Old Norse
 	'nov' => 'Novial',		# Novial
 	'nrm' => 'Nouormand',	# Norman
 	'nso' => 'Sesotho sa Leboa',	# Northern Sotho
