a2a815ceb5d145356ca4a36dcb426e843c9ef511
Niklas Laxström <nikerabbit@users.mediawiki.org>
2008-04-06 09:48:21 +0000

    * Use correct class, I wonder how long this has been broken

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1338,7 +1338,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '129';
+$wgStyleVersion = '130';
 
 
 # Server-side caching:
