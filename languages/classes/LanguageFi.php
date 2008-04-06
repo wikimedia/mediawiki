7f0d5fcd01afb863212abf5cd66f976fadc391a2
Niklas Laxström <nikerabbit@users.mediawiki.org>
2008-04-06 17:39:13 +0000

    * (bug 13435) Number grouping

--- languages/classes/LanguageFi.php
+++ languages/classes/LanguageFi.php
@@ -6,16 +6,6 @@
  * @author Niklas Laxström
  */
 class LanguageFi extends Language {
-	/**
-	 * Avoid grouping whole numbers between 0 to 9999
-	 */
-	function commafy($_) {
-		if (!preg_match('/^-?\d{1,4}$/',$_)) {
-			return strrev((string)preg_replace('/(\d{3})(?=\d)(?!\d*\.)/','$1,',strrev($_)));
-		} else {
-			return $_;
-		}
-	}
 
 	# Convert from the nominative form of a noun to some other case
 	# Invoked with {{GRAMMAR:case|word}}
