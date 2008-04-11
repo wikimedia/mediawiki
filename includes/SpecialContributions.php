b8a4079b6265c601c36a5ff9223e1c59cbfc0373
Aaron Schulz <aaron@users.mediawiki.org>
2008-04-11 18:27:21 +0000

    Revert self - diff links gives nice perma-link

--- includes/SpecialContributions.php
+++ includes/SpecialContributions.php
@@ -148,7 +148,7 @@ class ContribsPager extends ReverseChronologicalPager {
 
 		}
 		# Is there a visable previous revision?
-		if( $rev->userCan(Revision::DELETED_TEXT) && $rev->getParentId() !== 0 ) {
+		if( $rev->userCan(Revision::DELETED_TEXT) ) {
 			$difftext = '(' . $sk->makeKnownLinkObj( $page, $this->messages['diff'], 'diff=prev&oldid='.$row->rev_id ) . ')';
 		} else {
 			$difftext = '(' . $this->messages['diff'] . ')';
