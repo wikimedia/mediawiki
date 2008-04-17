41b6446cc750c800829d2acca3614f7390546b53
Brion Vibber <brion@users.mediawiki.org>
2008-04-17 18:52:20 +0000

    Revert r33478 for now; I don't much like the field name user_timestamp, it's very unclear. Timestamp may not be ideal to build a diff link with, either.

--- maintenance/archives/patch-user_timestamp.sql
+++
@@ -1,3 +0,0 @@
--- For getting diff since last view
-ALTER TABLE /*$wgDBprefix*/user_newtalk
-  ADD user_timestamp binary(14) NOT NULL default '';
--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -166,8 +166,6 @@ CREATE TABLE /*$wgDBprefix*/user_newtalk (
   -- If the user is an anonymous user their IP address is stored here
   -- since the user_id of 0 is ambiguous
   user_ip varbinary(40) NOT NULL default '',
-  -- The lowest timestamp of revisions that user has yet to view
-  user_timestamp binary(14) NOT NULL default '',
   INDEX user_id (user_id),
   INDEX user_ip (user_ip)
 
