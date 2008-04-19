8e2b8417e892fff98fefba1961c35822da61bbd4
Bryan Tong Minh <btongminh@users.mediawiki.org>
2008-04-19 17:24:38 +0000

    (bug 13792) Broken titles are now silently skipped in API search results.

--- includes/api/ApiQuerySearch.php
+++ includes/api/ApiQuerySearch.php
@@ -78,6 +78,9 @@ class ApiQuerySearch extends ApiQueryGeneratorBase {
 				break;
 			}
 
+			// Silently skip broken titles
+			if ($result->isBrokenTitle()) continue;
+			
 			$title = $result->getTitle();
 			if (is_null($resultPageSet)) {
 				$data[] = array(
