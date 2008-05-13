5bdaf697191489f19ca9aa388e67b1a85dced0fe
Roan Kattouw <catrope@users.mediawiki.org>
2008-05-13 14:41:35 +0000

    Actually this *should* be a LEFT JOIN, or we'll be dropping log entries

--- includes/api/ApiQueryRecentChanges.php
+++ includes/api/ApiQueryRecentChanges.php
@@ -166,7 +166,7 @@ class ApiQueryRecentChanges extends ApiQueryBase {
 			if($this->fld_redirect || isset($show['redirect']) || isset($show['!redirect']))
 			{
 				$this->addTables('page');
-				$this->addWhere(array('page_namespace=rc_namespace', 'page_title=rc_title'));
+				$this->addJoinConds(array('page' => 'LEFT JOIN', array('rc_namespace=page_namespace', 'rc_title=page_title')));
 				$this->addFields('page_is_redirect');
 			}
 		}
