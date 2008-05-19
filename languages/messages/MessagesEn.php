bddd428304e9fa1785526b8be9c60852a2acd170
Nicolas Dumazet <nicdumz@users.mediawiki.org>
2008-05-19 21:33:47 +0000

    #14094 When the expansion size is reached, the editor is not warned about it : 	Adding 2 messages + 2 categories, similarly to "Warning: This page contains too many expensive parser function calls" to warn the user when template post include size (or template argument post include size) is too large

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1142,6 +1142,10 @@ The deletion log for this page is provided here for convenience:",
 
 It should have less than $2, there are now $1.',
 'expensive-parserfunction-category' => 'Pages with too many expensive parser function calls',
+'post-expand-template-inclusion-warning'	=> "Warning: Template include size is too large, some templates will not be included.",
+'post-expand-template-inclusion-category'	=> "Pages where template include size is exceeded",
+'post-expand-template-argument-warning'		=> "Warning: This page contains at least one template argument which has a too large expansion size. These arguments have been omitted.",
+'post-expand-template-argument-category'	=> "Pages containing omitted template arguments",
 
 # "Undo" feature
 'undo-success' => 'The edit can be undone. Please check the comparison below to verify that this is what you want to do, and then save the changes below to finish undoing the edit.',
