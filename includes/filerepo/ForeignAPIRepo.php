58f39cbdc702e6679d88d217c2e16240c53f55d1
Brion Vibber <brion@users.mediawiki.org>
2008-05-21 22:39:08 +0000

    ForeignAPIRepo fixes: * Now supports thumbnails! * SVG seems to work * Comments now working in version info * Can fetch description pages without extra configuration (assuming standard script layout) * Asks server for mime type, but doesn't get it back yet

--- includes/filerepo/ForeignAPIRepo.php
+++ includes/filerepo/ForeignAPIRepo.php
@@ -3,7 +3,16 @@
 /**
  * A foreign repository with a remote MediaWiki with an API thingy
  * Very hacky and inefficient
- * do not use :D
+ * do not use except for testing :D
+ *
+ * Example config:
+ *
+ * $wgForeignFileRepos[] = array(
+ *   'class'            => 'ForeignAPIRepo',
+ *   'name'             => 'shared',
+ *   'apibase'          => 'http://en.wikipedia.org/w/api.php',
+ *   'fetchDescription' => true, // Optional
+ * );
  *
  * @ingroup FileRepo
  */
@@ -11,6 +20,10 @@ class ForeignAPIRepo extends FileRepo {
 	function __construct( $info ) {
 		parent::__construct( $info );
 		$this->mApiBase = $info['apibase']; // http://commons.wikimedia.org/w/api.php
+		if( !$this->scriptDirUrl ) {
+			// hack for description fetches
+			$this->scriptDirUrl = dirname( $this->mApiBase );
+		}
 	}
 
 	function storeBatch( $triplets, $flags = 0 ) {
@@ -32,25 +45,51 @@ class ForeignAPIRepo extends FileRepo {
 	function newFile( $title, $time = false ) {
 		return false;
 	}
-	function findFile( $title, $time = false ) {
+	
+	protected function queryImage( $query ) {
 		$url = $this->mApiBase .
 			'?' .
-			wfArrayToCgi( array(
-				'format' => 'json',
-				'action' => 'query',
-				'titles' => $title, // fixme -- canonical namespacea
-				'prop' => 'imageinfo',
-				'iiprop' => 'timestamp|user|comment|url|size|sha1|metadata' ) );
+			wfArrayToCgi(
+				array_merge( $query,
+					array(
+						'format' => 'json',
+						'action' => 'query',
+						'prop' => 'imageinfo' ) ) );
 		$json = Http::get( $url );
 		$data = json_decode( $json, true );
 		
 		if( isset( $data['query']['pages'] ) ) {
 			foreach( $data['query']['pages'] as $pageid => $info ) {
 				if( isset( $info['imageinfo'][0] ) ) {
-					return new ForeignAPIFile( $title, $this, $info['imageinfo'][0] );
+					return $info['imageinfo'][0];
 				}
 			}
 		}
 		return false;
+	}
+	
+	function findFile( $title, $time = false ) {
+		$info = $this->queryImage( array(
+			'titles' => 'Image:' . $title->getText(),
+			'prop' => 'imageinfo',
+			'iiprop' => 'timestamp|user|comment|url|size|sha1|metadata|mimetype' ) );
+		if( $info ) {
+			return new ForeignAPIFile( $title, $this, $info );
+		} else {
+			return false;
+		}
+	}
+	
+	function getThumbUrl( $name, $width=-1, $height=-1 ) {
+		$info = $this->queryImage( array(
+			'titles' => 'Image:' . $name,
+			'iiprop' => 'url',
+			'iiurlwidth' => $width,
+			'iiurlheight' => $height ) );
+		if( $info ) {
+			return $info['thumburl'];
+		} else {
+			return false;
+		}
 	}
 }
