01896e53d4b2539d70185e1d3624fcd6c013d241
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2008-06-01 16:13:58 +0000

    Per r35670: Bump $wgStyleVersion

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1324,7 +1324,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '149';
+$wgStyleVersion = '150';
 
 
 # Server-side caching:
