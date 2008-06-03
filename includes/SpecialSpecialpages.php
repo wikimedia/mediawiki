11731e3b3bff37b189d0664c26a0a6faac669677
Rotem Liss <rotem@users.mediawiki.org>
2008-06-03 12:56:33 +0000

    Reverting new Special:SpecialPages layout: Problems in RTL wikis in Firefox (didn't check other browsers), list bullets are indented to the left. The change to the restricted special pages was not reverted, and they are still in the combined list.

--- includes/SpecialSpecialpages.php
+++ includes/SpecialSpecialpages.php
@@ -16,12 +16,12 @@ function wfSpecialSpecialpages() {
 	$sk = $wgUser->getSkin();
 
 	$pages = SpecialPage::getUsablePages();
-	
+
 	if( count( $pages ) == 0 ) {
 		# Yeah, that was pointless. Thanks for coming.
 		return;
 	}
-	
+
 	/** Put them into a sortable array */
 	$groups = array();
 	foreach ( $pages as $page ) {
@@ -30,45 +30,55 @@ function wfSpecialSpecialpages() {
 			if( !isset($groups[$group]) ) {
 				$groups[$group] = array();
 			}
+			//$groups[$group][$page->getDescription()] = $page->getTitle();
 			$groups[$group][$page->getDescription()] = array( $page->getTitle(), $page->isRestricted() );
 		}
 	}
-	
+
 	/** Sort */
 	if ( $wgSortSpecialPages ) {
 		foreach( $groups as $group => $sortedPages ) {
 			ksort( $groups[$group] );
 		}
 	}
-	
+
 	/** Always move "other" to end */
 	if( array_key_exists('other',$groups) ) {
 		$other = $groups['other'];
 		unset( $groups['other'] );
 		$groups['other'] = $other;
 	}
-	
+
 	/** Now output the HTML */
 	$restrictedPages = false;
 	foreach ( $groups as $group => $sortedPages ) {
+		$middle = ceil( count($sortedPages)/2 );
+		$total = count($sortedPages);
+		$count = 0;
+
 		$wgOut->addHTML( "<h4 class='mw-specialpagesgroup'>".wfMsgHtml("specialpages-group-$group")."</h4>\n" );
-		$wgOut->addHTML( "<ul class='mw-specialpages-section'>" );
-		foreach ( $sortedPages as $desc => $specialpage ) {
+		$wgOut->addHTML( "<table style='width: 100%;' class='mw-specialpages-table'><tr>" );
+		$wgOut->addHTML( "<td width='30%' valign='top'><ul>\n" );
+		foreach( $sortedPages as $desc => $specialpage ) {
 			list( $title, $restricted ) = $specialpage;
 			$link = $sk->makeKnownLinkObj( $title , htmlspecialchars( $desc ) );
 			if( $restricted ) {
 				$wgOut->addHTML( "<li class='mw-specialpages-page mw-specialpagerestricted'>{$link}</li>\n" );
-				$restrictedPages = true;
 			} else {
-				$wgOut->addHTML( "<li class='mw-specialpages-page'>{$link}</li>\n" );
+				$wgOut->addHTML( "<li>{$link}</li>\n" );
+			}
+
+			# Split up the larger groups
+			$count++;
+			if( $total > 3 && $count == $middle ) {
+				$wgOut->addHTML( "</ul></td><td width='10%'></td><td width='30%' valign='top'><ul>" );
 			}
 		}
-		$wgOut->addHTML( "</ul>\n" );
+		$wgOut->addHTML( "</ul></td><td width='30%' valign='top'></td></tr></table>\n" );
 	}
 	$wgOut->addHTML(
 		Xml::openElement('div', array( 'class' => 'mw-specialpages-notes' )).
 		wfMsgWikiHtml('specialpages-note').
 		Xml::closeElement('div')
 	);
-	
 }
