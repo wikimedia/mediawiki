94dad9b45faf940e6f958a0431f6c95d118b94e9
Chad Horohoe <demon@users.mediawiki.org>
2008-06-12 15:37:38 +0000

    Bump $wgStyleVersion to 155.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1341,7 +1341,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '154';
+$wgStyleVersion = '155';
 
 
 # Server-side caching:
