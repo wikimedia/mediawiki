4fb072a7d30587c7d69ea2cfd2ec4891a49613e5
Raimond Spekking <raymond@users.mediawiki.org>
2008-06-13 09:27:14 +0000

    Remove non standard, Gecko specific CSS. At least Opera whines about it and the round corners are not consistent to all other boxes with square corners.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1341,7 +1341,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '155';
+$wgStyleVersion = '156';
 
 
 # Server-side caching:
