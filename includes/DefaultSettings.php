924b14859309b93277e182113bbc60242a255940
Brion Vibber <brion@users.mediawiki.org>
2008-06-19 22:11:03 +0000

    Revert r36446, 36447 -- spews a whole bunch of table alignment into various skins' skin-specific CSS. This seems a bit odd; if there's a reason for it *at all* it should be in the shared CSS, but I don't see any reason for it to begin with, really.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1341,7 +1341,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '157';
+$wgStyleVersion = '156';
 
 
 # Server-side caching:
