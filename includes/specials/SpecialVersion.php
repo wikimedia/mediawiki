662e0a358a5e0266f831e4b9bb01dc9ba4c2a571
Aryeh Gregor <simetrical@users.mediawiki.org>
2008-06-26 00:10:15 +0000

    May as well add my name here.  It might come in handy sometime.

--- includes/specials/SpecialVersion.php
+++ includes/specials/SpecialVersion.php
@@ -57,7 +57,8 @@ class SpecialVersion {
 		This wiki is powered by '''[http://www.mediawiki.org/ MediaWiki]''',
 		copyright (C) 2001-2008 Magnus Manske, Brion Vibber, Lee Daniel Crocker,
 		Tim Starling, Erik Möller, Gabriel Wicke, Ævar Arnfjörð Bjarmason,
-		Niklas Laxström, Domas Mituzas, Rob Church, Yuri Astrakhan and others.
+		Niklas Laxström, Domas Mituzas, Rob Church, Yuri Astrakhan, Aryeh Gregor
+		and others.
 
 		MediaWiki is free software; you can redistribute it and/or modify
 		it under the terms of the GNU General Public License as published by
