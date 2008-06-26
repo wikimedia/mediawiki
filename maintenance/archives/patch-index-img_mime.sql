d840f82c1087cd7354022ef239168b0ab0b64cd8
Chad Horohoe <demon@users.mediawiki.org>
2008-06-26 20:41:40 +0000

    Undo previous commit. Schema changes are a no-no without permission first :-)

--- maintenance/archives/patch-index-img_mime.sql
+++
@@ -1,6 +0,0 @@
--- Indexing MIME types
---
--- Allows MIME search to work on large databases like Wikimedia one
-
-ALTER TABLE /*$wgDBprefix*/image
-   ADD INDEX img_mime (img_major_mime, img_minor_mime);
