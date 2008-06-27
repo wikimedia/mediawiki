8e655dc0ac608e6d1b7a67e78564282e04ae73d1
Bryan Tong Minh <btongminh@users.mediawiki.org>
2008-06-27 19:45:24 +0000

    * Allow extensions to modify the user creation form by calling addInputItem();

--- includes/templates/Userlogin.php
+++ includes/templates/Userlogin.php
@@ -97,6 +97,15 @@ class UserloginTemplate extends QuickTemplate {
  * @ingroup Templates
  */
 class UsercreateTemplate extends QuickTemplate {
+	function addInputItem( $name, $value, $type, $msg ) {
+		$this->data['extraInput'][] = array(
+			'name' => $name,
+			'value' => $value,
+			'type' => $type,
+			'msg' => $msg,
+		);
+	}
+	
 	function execute() {
 		if( $this->data['message'] ) {
 ?>
@@ -197,15 +206,57 @@ class UsercreateTemplate extends QuickTemplate {
 					/> <label for="wpRemember"><?php $this->msg('remembermypassword') ?></label>
 			</td>
 		</tr>
+<?php
+		$tabIndex = 8;
+		if ( isset( $this->data['extraInput'] ) && is_array( $this->data['extraInput'] ) ) {
+			foreach ( $this->data['extraInput'] as $inputItem ) { ?>
+		<tr>
+			<?php 
+				if ( !empty( $inputItem['msg'] ) && $inputItem['type'] != 'checkbox' ) {
+					?><td class="mw-label"><label for="<?php 
+					echo htmlspecialchars( $inputItem['name'] ); ?>"><?php
+					$this->msgWiki( $inputItem['msg'] ) ?></label><?php
+				} else {
+					?><td><?php
+				}
+			?></td>
+			<td class="mw-input">
+				<input type="<?php echo htmlspecialchars( $inputItem['type'] ) ?>" name="<?php
+				echo htmlspecialchars( $inputItem['name'] ); ?>"
+					tabindex="<?php echo $tabIndex++; ?>"
+					value="<?php 
+				if ( $inputItem['type'] != 'checkbox' ) {
+					echo htmlspecialchars( $inputItem['value'] );
+				} else {
+					echo '1';
+				}					
+					?>" id="<?php echo htmlspecialchars( $inputItem['name'] ); ?>"
+					<?php 
+				if ( $inputItem['type'] == 'checkbox' && !empty( $inputItem['value'] ) )
+					echo 'checked="checked"'; 
+					?> /> <?php 
+					if ( $inputItem['type'] == 'checkbox' && !empty( $inputItem['msg'] ) ) {
+						?>
+				<label for="<?php echo htmlspecialchars( $inputItem['name'] ); ?>"><?php
+					$this->msg( $inputItem['msg'] ) ?></label><?php
+					}
+				?>
+			</td>
+		</tr>
+<?php				
+				
+			}
+		}
+?>
 		<tr>
 			<td></td>
 			<td class="mw-submit">
 				<input type='submit' name="wpCreateaccount" id="wpCreateaccount"
-					tabindex="8"
+					tabindex="<?php echo $tabIndex++; ?>"
 					value="<?php $this->msg('createaccount') ?>" />
 				<?php if( $this->data['createemail'] ) { ?>
 				<input type='submit' name="wpCreateaccountMail" id="wpCreateaccountMail"
-					tabindex="9"
+					tabindex="<?php echo $tabIndex++; ?>"
 					value="<?php $this->msg('createaccountmail') ?>" />
 				<?php } ?>
 			</td>
