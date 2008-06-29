wikimedia/mediawiki

commit ba747a9843a00f63682315cfca4040e667536035

Use the proper Title methods in ApiQueryBase::titleToKey() and keyToTitle(). This fixes bug 14651 for much more than just apprefix.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@36678 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/api/ApiQueryBase.php
@@ -319,14 +319,13 @@ abstract class ApiQueryBase extends ApiBase {
 	}
 
 	/**
-	 * This is a very simplistic utility function
-	 * to convert a non-namespaced title string to a db key.
-	 * It will replace all ' ' with '_'
+	 * Convert a title to a DB key
 	 * @param string $title Page title with spaces
 	 * @return string Page title with underscores
 	 */
 	public static function titleToKey($title) {
-		return str_replace(' ', '_', $title);
+		$t = Title::newFromText($title);
+		return $t->getDbKey();
 	}
 
 	/**
@@ -335,7 +334,8 @@ abstract class ApiQueryBase extends ApiBase {
 	 * @return string Page title with spaces
 	 */
 	public static function keyToTitle($key) {
-		return str_replace('_', ' ', $key);
+		$t = Title::newFromDbKey($key);
+		return $t->getPrefixedText();
 	}
 
 	/**

RELEASE-NOTES
@@ -469,6 +469,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * Added nocreate parameter to action=edit
 * (bug 14402) Added maxage and smaxage parameters to api.php
 * Added bkip parameter to list=blocks
+* (bug 14651) apprefix and similar parameters are now canonicalized
 
 === Languages updated in 1.13 ===
 

commit 13d29b36c166ebb7dcdf746db5c9eceecb300710

Another regression from r36678: dieUsageMsg() expects an array, not varargs.

includes/api/ApiQueryBase.php
@@ -326,7 +326,7 @@ abstract class ApiQueryBase extends ApiBase {
 	public function titleToKey($title) {
 		$t = Title::newFromText($title);
 		if(!$t)
-			$this->dieUsageMsg('invalidtitle', $title);
+			$this->dieUsageMsg(array('invalidtitle', $title));
 		return $t->getDbKey();
 	}
 
@@ -339,7 +339,7 @@ abstract class ApiQueryBase extends ApiBase {
 		$t = Title::newFromDbKey($key);
 		# This really shouldn't happen but we gotta check anyway
 		if(!$t)
-			$this->dieUsageMsg('invalidtitle', $key);
+			$this->dieUsageMsg(array('invalidtitle', $key));
 		return $t->getPrefixedText();
 	}
 