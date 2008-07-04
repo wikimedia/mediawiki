798bb1cdc4f9867d0138bd7d8a6536eba8e4c7e0
Brion Vibber <brion@users.mediawiki.org>
2008-07-04 21:23:00 +0000

    Revert r37063 "KTHMLfixes no longer loaded on skins where it doesn't exist. (bug 14717)" * Hardcoding to monobook doesn't make sense here; other skins may include such fix files * Only addressing KHTMLfixes doesn't make sense here; there's a big pile of Opera fixes right above it which would presumably suffer the same problem
    If these are required, and are required per-skin, then the skin should probably specify to the JS which ones it needs.
    On the other hand, if they're not required, or can be done in a skin-independent fashion, then that should be checked.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1353,7 +1353,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '161';
+$wgStyleVersion = '160';
 
 
 # Server-side caching:
