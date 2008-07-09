438f6d2925fe3c615467e743bbbbde5de6b9a76a
Daniel Kinzler <daniel@users.mediawiki.org>
2008-07-09 15:05:25 +0000

    improved integration into category bar. Note:  options is not functional until SkinJoinCategoryLinks hook is implemented (merge pending)

--- StartProfiler.php
+++ StartProfiler.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+#require_once( dirname(__FILE__).'/includes/ProfilerStub.php' );
+
+ require_once(  dirname(__FILE__).'/includes/Profiler.php' );
+ $wgProfiler = new Profiler;
 
 /**
  * To use a profiler, delete the line above and add something like this:
