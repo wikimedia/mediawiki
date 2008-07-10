d966b3424ec7480df00bd5d83d7be8ac2e60fac9
Chad Horohoe <demon@users.mediawiki.org>
2008-07-10 14:53:00 +0000

    (bug 14778) 'limit' parameter now applies to history feeds as well as history pages

--- includes/PageHistory.php
+++ includes/PageHistory.php
@@ -501,7 +501,7 @@ class PageHistory {
 	 * @param string $type
 	 */
 	function feed( $type ) {
-		global $wgFeedClasses;
+		global $wgFeedClasses, $wgRequest, $wgFeedLimit;
 		if ( !FeedUtils::checkFeedOutput($type) ) {
 			return;
 		}
@@ -512,7 +512,14 @@ class PageHistory {
 			wfMsgForContent( 'history-feed-description' ),
 			$this->mTitle->getFullUrl( 'action=history' ) );
 
-		$items = $this->fetchRevisions(10, 0, PageHistory::DIR_NEXT);
+		// Get a limit on number of feed entries. Provide a sane default
+		// of 10 if none is defined (but limit to $wgFeedLimit max)
+		$limit = $wgRequest->getInt( 'limit', 10 );
+		if( $limit > $wgFeedLimit ) {
+			$limit = $wgFeedLimit;
+		}
+ 		$items = $this->fetchRevisions($limit, 0, PageHistory::DIR_NEXT);
+
 		$feed->outHeader();
 		if( $items ) {
 			foreach( $items as $row ) {
