24e2e0afecc1fd1da3a400a544d373a437f64688
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-12 12:23:41 +0000

    Add option to rebuildLanguage.php * --remove-unknown: Remove unknown messages

--- maintenance/language/rebuildLanguage.php
+++ maintenance/language/rebuildLanguage.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Rewrite the messages array in the files languages/messages/MessagesXX.php.
+ * Rewrite the messages array in the files languages/messages/MessagesXx.php.
  *
  * @file
  * @ingroup MaintenanceLanguage
@@ -17,12 +17,13 @@ require_once( 'writeMessagesArray.inc' );
  * @param $code The language code.
  * @param $write Write to the messages file?
  * @param $listUnknown List the unknown messages?
+ * @param $removeUnKnown Remove the unknown messages?
  */
-function rebuildLanguage( $code, $write, $listUnknown ) {
+function rebuildLanguage( $code, $write, $listUnknown, $removeUnknown ) {
 	global $wgLanguages;
 	$messages = $wgLanguages->getMessages( $code );
 	$messages = $messages['all'];
-	MessageWriter::writeMessagesToFile( $messages, $code, $write, $listUnknown );
+	MessageWriter::writeMessagesToFile( $messages, $code, $write, $listUnknown, $removeUnknown );
 }
 
 # Show help
@@ -33,8 +34,9 @@ Parameters:
 	* lang: Language code (default: the installation default language). You can also specify "all" to check all the languages.
 	* help: Show this help.
 Options:
-	* dry-run: Don't write the array to the file.
-	* no-unknown: Don't list the unknown messages.
+	* dry-run: Do not write the array to the file.
+	* no-unknown: Do not list the unknown messages.
+	* remove-unknown: Remove unknown messages.
 
 END;
 	exit();
@@ -50,6 +52,7 @@ if ( isset( $options['lang'] ) ) {
 # Get the options
 $wgWriteToFile = !isset( $options['dry-run'] );
 $wgListUnknownMessages = !isset( $options['no-unknown'] );
+$wgRemoveUnknownMessages = isset( $options['remove-unknown'] );
 
 # Get language objects
 $wgLanguages = new languages();
@@ -57,10 +60,8 @@ $wgLanguages = new languages();
 # Write all the language
 if ( $wgCode == 'all' ) {
 	foreach ( $wgLanguages->getLanguages() as $language ) {
-		rebuildLanguage( $language, $wgWriteToFile, $wgListUnknownMessages );
+		rebuildLanguage( $language, $wgWriteToFile, $wgListUnknownMessages, $wgRemoveUnknownMessages );
 	}
 } else {
-	rebuildLanguage( $wgCode, $wgWriteToFile, $wgListUnknownMessages );
+	rebuildLanguage( $wgCode, $wgWriteToFile, $wgListUnknownMessages, $wgRemoveUnknownMessages );
 }
-
-
