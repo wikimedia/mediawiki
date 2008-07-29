e0e0b5e019fba5661d7cc3427a011dc89dc8104a
Niklas Laxström <nikerabbit@users.mediawiki.org>
2008-07-29 16:13:49 +0000

    * Number and date formats for my requested by Hintha@Betawiki

---
+++ languages/classes/LanguageMy.php
@@ -0,0 +1,18 @@
+<?php
+/** Burmese (Myanmasa)
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Niklas Laxström, 2008
+ */
+
+class LanguageMy extends Language {
+	function commafy($_) {
+		/* NO-op. Cannot use
+		 * $separatorTransformTable = array( ',' => '' )
+		 * That would break when parsing and doing strstr '' => 'foo';
+		 */
+		return $_;
+	}
+}
