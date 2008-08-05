6022dacb6fbd3daab4d8fc9f2d2050842f28ebbf
Raimond Spekking <raymond@users.mediawiki.org>
2008-08-05 06:01:54 +0000

    Reapply part of r38587: * 'skinpreview' renamed to 'skin-preview': brackets outside of the hyperlink now. Betawiki has done the rename job for all languages already.

--- includes/specials/SpecialPreferences.php
+++ includes/specials/SpecialPreferences.php
@@ -816,7 +816,7 @@ class PreferencesForm {
 		#
 		$wgOut->addHTML( "<fieldset>\n<legend>\n" . wfMsg('skin') . "</legend>\n" );
 		$mptitle = Title::newMainPage();
-		$previewtext = wfMsg('skinpreview');
+		$previewtext = wfMsg('skin-preview');
 		# Only show members of Skin::getSkinNames() rather than
 		# $skinNames (skins is all skin names from Language.php)
 		$validSkinNames = Skin::getSkinNames();
@@ -835,7 +835,7 @@ class PreferencesForm {
 			$checked = $skinkey == $this->mSkin ? ' checked="checked"' : '';
 
 			$mplink = htmlspecialchars($mptitle->getLocalURL("useskin=$skinkey"));
-			$previewlink = "<a target='_blank' href=\"$mplink\">$previewtext</a>";
+			$previewlink = "(<a target='_blank' href=\"$mplink\">$previewtext</a>)";
 			if( $skinkey == $wgDefaultSkin )
 				$sn .= ' (' . wfMsg( 'default' ) . ')';
 			$wgOut->addHTML( "<input type='radio' name='wpSkin' id=\"wpSkin$skinkey\" value=\"$skinkey\"$checked /> <label for=\"wpSkin$skinkey\">{$sn}</label> $previewlink<br />\n" );
