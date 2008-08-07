4c9f07ea2988b838c03b81a29a3ddb2a1f092b32
Raimond Spekking <raymond@users.mediawiki.org>
2008-08-07 18:46:18 +0000

    Add a more descriptive message to the links-to-image section for the case that more than 100 pages link to a file. Ths existing link to Whatlinkshere at the end of the list is easily be overseen if not scrolled to the bottom. Fixes the (cosmetic) bug that the list shows 101 entries instead of 100. Question: Should we make the hardcoded limit of 100 configurable via DefaultSettings?

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1762,6 +1762,9 @@ A click on a column header changes the sorting.',
 'filehist-comment'               => 'Comment',
 'imagelinks'                     => 'Links',
 'linkstoimage'                   => 'The following {{PLURAL:$1|page links|$1 pages link}} to this file:',
+'linkstoimage-more'              => 'More than $1 {{PLURAL:$2|page links|pages link}} to this file.
+The following list shows the {{PLURAL:$1|first page link|first $1 page links}} to this file only.
+For a full list follow [[Special:Whatlinkshere/$2|this link]].',
 'nolinkstoimage'                 => 'There are no pages that link to this file.',
 'morelinkstoimage'               => 'View [[Special:Whatlinkshere/$1|more links]] to this file.',
 'redirectstofile'                => 'The following {{PLURAL:$1|file redirects|$1 files redirect}} to this file:',
