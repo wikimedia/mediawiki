wikimedia/mediawiki

commit e2c053407e13cc4ad84acf1a51f1ccb8c687a913

* (bug 14165, bug 14294) Wikimedia specific configuration in convertGrammar() for 13 localisations (be, be-tarask, bs, cs, dsb, ga, gsw, hsb, hu, lv, rmy, sk, sl) removed. The settings have been put in extension WikimediaMessages. For be, be-tarask, bs, dsb, hsb some duplicates (1=1) were removed. Patch for Czech by Danny B. * Also put the wgGrammarForms from the Wikimedia configuration in here, so everything is in one place. Bonus with this setup is that there will be a little less work for Wikimedia shell operators, as the wgGrammarForms configuration is now accessible to i18n committers. * Header for WikimediaMessages.php, and add myself to extension credits.
WARNING to Wikimedia shell operators: the whole section "wgGrammarForms" in InitialiseSettings.php.html should be remove after updating to this version. It will most probably overrule all settings in WikimediaMessages because of "'default' => array()"

git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@39077 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/classes/LanguageSl.php
@@ -17,55 +17,10 @@ class LanguageSl extends Language {
 		}
 
 		switch ( $case ) {
-			case 'rodilnik': # genitive
-				switch ( $word ) {
-					case 'Wikipedija': $word = 'Wikipedije'; break 2;
-					case 'Wikiknjige': $word = 'Wikiknjig'; break 2;
-					case 'Wikinovice': $word = 'Wikinovic'; break 2;
-					case 'Wikinavedek': $word = 'Wikinavedka'; break 2;
-					case 'Wikivir': $word = 'Wikivira'; break 2;
-					case 'Wikislovar': $word = 'Wikislovarja'; break 2;
-				}
-			case 'dajalnik': # dativ
-				switch ( $word ) {
-					case 'Wikipedija': $word = 'Wikipediji'; break 2;
-					case 'Wikiknjige': $word = 'Wikiknjigam'; break 2;
-					case 'Wikinovice': $word = 'Wikinovicam'; break 2;
-					case 'Wikinavedek': $word = 'Wikinavedku'; break 2;
-					case 'Wikivir': $word = 'Wikiviru'; break 2;
-					case 'Wikislovar': $word = 'Wikislovarju'; break 2;
-				}
-			case 'tožilnik': # akuzatív
-				switch ( $word ) {
-					case 'Wikipedija': $word = 'Wikipedijo'; break 2;
-					case 'Wikiknjige':
-					case 'Wikinovice':
-					case 'Wikinavedek':
-					case 'Wikivir':
-					case 'Wikislovar':
-						// Don't change, just fall through
-						break 2;
-				}
 			case 'mestnik': # locative
-				switch ( $word ) {
-					case 'Wikipedija': $word = 'o Wikipediji'; break 2;
-					case 'Wikiknjige': $word = 'o Wikiknjigah'; break 2;
-					case 'Wikinovice': $word = 'o Wikinovicah'; break 2;
-					case 'Wikinavedek': $word = 'o Wikinavedku'; break 2;
-					case 'Wikivir': $word = 'o Wikiviru'; break 2;
-					case 'Wikislovar': $word = 'o Wikislovarju'; break 2;
-					default: $word = 'o ' . $word; break 2;
-				}
+				$word = 'o ' . $word; break 2;
 			case 'orodnik': # instrumental
-				switch ( $word ) {
-					case 'Wikipedija': $word = 'z Wikipedijo'; break 2;
-					case 'Wikiknjige': $word = 'z Wikiknjigami'; break 2;
-					case 'Wikinovice': $word = 'z Wikinovicami'; break 2;
-					case 'Wikinavedek': $word = 'z Wikinavedkom'; break 2;
-					case 'Wikivir': $word = 'z Wikivirom'; break 2;
-					case 'Wikislovar': $word = 'z Wikislovarjem'; break 2;
-					default: $word = 'z ' . $word;
-				}
+				$word = 'z ' . $word;
 		}
 
 		return $word; # this will return the original value for 'imenovalnik' (nominativ) and all undefined case values

CREDITS
@@ -22,6 +22,7 @@ following names for their contribution to the product.
 
 == Patch Contributors ==
 * Daniel Arnold
+* Danny B.
 * Max Semenik
 * RememberTheDot
 

RELEASE-NOTES
@@ -95,6 +95,9 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 15079) Add class="ns-talk" / "ns-subject" to <body>. Also added ns-special to 
   special pages.
 * (bug 15052) Skins should add their name as a class in <body>
+* (bug 14165, bug 14294) Wikimedia specific configuration in convertGrammar()
+  for several languages was removed. The settings have been put in extension
+  WikimediaMessages. Patch for Czech by Danny B.
 
 === API changes in 1.14 ===
 

languages/classes/LanguageBe.php
@@ -31,61 +31,4 @@ class LanguageBe extends Language {
 			}
 		}
 	}
-
-	# Convert from the nominative form of a noun to some other case
-	# Invoked with {{GRAMMAR:case|word}}
-	/**
-   * Cases: родны, вінавальны, месны
-   */
-	function convertGrammar( $word, $case ) {
-		switch ( $case ) {
-			case 'родны': # genitive
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыі';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўніка';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКніг';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцы';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавін';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВідаў';
-				}
-			break;
-			case 'вінавальны': # akusative
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыю';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўнік';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКнігі';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцу';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавіны';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВіды';
-				}
-			break;
-			case 'месны': # prepositional
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыі';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўніку';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКнігах';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцы';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавінах';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВідах';
-				}
-			break;
-		}
-
-		return $word; # this will return the original value for 'назоўны' (nominative) and all undefined case values
-	}
 }

languages/classes/LanguageBe_tarask.php
@@ -39,61 +39,4 @@ class LanguageBe_tarask extends Language {
 			}
 		}
 	}
-
-	# Convert from the nominative form of a noun to some other case
-	# Invoked with {{GRAMMAR:case|word}}
-	/**
-   * Cases: родны, вінавальны, месны
-   */
-	function convertGrammar( $word, $case ) {
-		switch ( $case ) {
-			case 'родны': # genitive
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыі';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўніка';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКніг';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцы';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавін';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВідаў';
-				}
-			break;
-			case 'вінавальны': # akusative
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыю';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўнік';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКнігі';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцу';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавіны';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВіды';
-				}
-			break;
-			case 'месны': # prepositional
-				if ( $word == 'Вікіпэдыя' ) {
-					$word = 'Вікіпэдыі';
-				} elseif ( $word == 'ВікіСлоўнік' ) {
-					$word = 'ВікіСлоўніку';
-				} elseif ( $word == 'ВікіКнігі' ) {
-					$word = 'ВікіКнігах';
-				} elseif ( $word == 'ВікіКрыніца' ) {
-					$word = 'ВікіКрыніцы';
-				} elseif ( $word == 'ВікіНавіны' ) {
-					$word = 'ВікіНавінах';
-				} elseif ( $word == 'ВікіВіды' ) {
-					$word = 'ВікіВідах';
-				}
-			break;
-		}
-
-		return $word; # this will return the original value for 'назоўны' (nominative) and all undefined case values
-	}
 }

languages/classes/LanguageBs.php
@@ -34,99 +34,11 @@ class LanguageBs extends Language {
 			return $wgGrammarForms['bs'][$case][$word];
 		}
 		switch ( $case ) {
-			case 'genitiv': # genitive
-				if ( $word == 'Wikipedia' ) {
-					$word = 'Wikipedije';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 'Wikiknjiga';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 'Wikivijesti';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 'Wikicitata';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 'Wikiizvora';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 'Vikirječnika';
-				}
-			break;
-			case 'dativ': # dative
-				if ( $word == 'Wikipedia' ) {
-					$word = 'Wikipediji';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 'Wikiknjigama';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 'Wikicitatima';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 'Wikivijestima';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 'Wikiizvoru';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 'Vikirječniku';
-				}
-			break;
-			case 'akuzativ': # akusative
-				if ( $word == 'Wikipedia' ) {
-					$word = 'Wikipediju';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 'Wikiknjige';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 'Wikicitate';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 'Wikivijesti';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 'Wikiizvora';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 'Vikirječnika';
-				}
-			break;
-			case 'vokativ': # vocative
-				if ( $word == 'Wikipedia' ) {
-					$word = 'Wikipedijo';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 'Wikiknjige';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 'Wikicitati';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 'Wikivijesti';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 'Wikizivoru';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 'Vikirječniče';
-				}
-			break;
 			case 'instrumental': # instrumental
-				if ( $word == 'Wikipedia' ) {
-					$word = 's Wikipediom';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 's Wikiknjigama';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 's Wikicitatima';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 's Wikivijestima';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 's Wikiizvorom';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 's Vikirječnikom';
-				} else {
-					$word = 's ' . $word;
-				}
+				$word = 's ' . $word;
 			break;
 			case 'lokativ': # locative
-				if ( $word == 'Wikipedia' ) {
-					$word = 'o Wikipediji';
-				} elseif ( $word == 'Wikiknjige' ) {
-					$word = 'o Wikiknjigama';
-				} elseif ( $word == 'Wikicitati' ) {
-					$word = 'o Wikicitatima';
-				} elseif ( $word == 'Wikivijesti' ) {
-					$word = 'o Wikivijestima';
-				} elseif ( $word == 'Wikiizvor' ) {
-					$word = 'o Wikiizvoru';
-				} elseif ( $word == 'Vikirječnik' ) {
-					$word = 'o Vikirječniku';
-				} else {
-					$word = 'o ' . $word;
-				}
+				$word = 'o ' . $word;
 			break;
 		}
 

languages/classes/LanguageCs.php
@@ -5,115 +5,6 @@
  * @ingroup Language
  */
 class LanguageCs extends Language {
-	# Grammatical transformations, needed for inflected languages
-	# Invoked by putting {{grammar:case|word}} in a message
-	function convertGrammar( $word, $case ) {
-		global $wgGrammarForms;
-		if ( isset($wgGrammarForms['cs'][$case][$word]) ) {
-			return $wgGrammarForms['cs'][$case][$word];
-		}
-		# allowed values for $case:
-		#	1sg, 2sg, ..., 7sg -- nominative, genitive, ... (in singular)
-		switch ( $word ) {
-			case 'Wikibooks':
-			case 'Wikiknihy':
-				switch ( $case ) {
-					case '2sg':
-						return 'Wikiknih';
-					case '3sg':
-						return 'Wikiknihám';
-					case '6sg':
-						return 'Wikiknihách';
-					case '7sg':
-						return 'Wikiknihami';
-					default:
-						return 'Wikiknihy';
-				}
-			case 'Wikipedia':
-			case 'Wikipedie':
-				switch ( $case ) {
-					case '3sg':
-					case '4sg':
-					case '6sg':
-						return 'Wikipedii';
-					case '7sg':
-						return 'Wikipedií';
-					default:
-						return 'Wikipedie';
-				}
-			case 'Wiktionary':
-			case 'Wikcionář':
-			case 'Wikislovník':
-				switch ( $case ) {
-					case '2sg':
-					case '3sg':
-					case '5sg':
-					case '6sg':
-						return 'Wikislovníku';
-					case '7sg':
-						return 'Wikislovníkem';
-					default:
-						return 'Wikislovník';
-				}
-			case 'Wikiquote':
-			case 'Wikicitáty':
-				switch ( $case ) {
-					case '2sg':
-						return 'Wikicitátů';
-					case '3sg':
-						return 'Wikicitátům';
-					case '6sg':
-						return 'Wikicitátech';
-					default:
-						return 'Wikicitáty';
-				}
-			case 'Wikinews':
-			case 'Wikizprávy':
-				switch ( $case ) {
-					case '2sg':
-						return 'Wikizpráv';
-					case '3sg':
-						return 'Wikizprávám';
-					case '6sg':
-						return 'Wikizprávách';
-					case '7sg':
-						return 'Wikizprávami';
-					default:
-						return 'Wikizprávy';
-				}
-			case 'Wikiversity':
-			case 'Wikiverzita':
-				switch ( $case ) {
-					case '2sg':
-						return 'Wikiverzity';
-					case '3sg':
-					case '6sg':
-						return 'Wikiverzitě';
-					case '4sg':
-						return 'Wikiverzitu';
-					case '5sg':
-						return 'Wikiverzito';
-					case '7sg':
-						return 'Wikiverzitou';
-					default:
-						return 'Wikiverzita';
-				}
-			case 'Wikispecies':
-			case 'Wikidruhy':
-				switch ( $case ) {
-					case '2sg':
-						return 'Wikidruhů';
-					case '3sg':
-						return 'Wikidruhům';
-					case '6sg':
-						return 'Wikidruzích';
-					default:
-						return 'Wikidruhy';
-				}
-		}
-		# unknown
-		return $word;
-	}
 
 	function convertPlural( $count, $forms ) {
 		if ( !count($forms) ) { return ''; }

languages/classes/LanguageDsb.php
@@ -16,84 +16,10 @@ class LanguageDsb extends Language {
 		}
 
 		switch ( $case ) {
-			case 'genitiw': # genitive
-				if ( $word == 'Wikipedija' ) {
-					$word = 'Wikipedije';
-				} elseif ( $word == 'Wikiknihi' ) {
-					$word = 'Wikiknih';
-				} elseif ( $word == 'Wikinowiny' ) {
-					$word = 'Wikinowin';
-				} elseif ( $word == 'Wikižórło' ) {
-					$word = 'Wikižórła';
-				} elseif ( $word == 'Wikicitaty' ) {
-					$word = 'Wikicitatow';
-				} elseif ( $word == 'Wikisłownik' ) {
-					$word = 'Wikisłownika';
-				}
-				break;
-			case 'datiw': # dativ
-				if ( $word == 'Wikipedija' ) {
-					$word = 'Wikipediji';
-				} elseif ( $word == 'Wikiknihi' ) {
-					$word = 'Wikikniham';
-				} elseif ( $word == 'Wikinowiny' ) {
-					$word = 'Wikinowinam';
-				} elseif ( $word == 'Wikižórło' ) {
-					$word = 'Wikižórłu';
-				} elseif ( $word == 'Wikicitaty' ) {
-					$word = 'Wikicitatam';
-				} elseif ( $word == 'Wikisłownik' ) {
-					$word = 'Wikisłownikej';
-				}
-				break;
-			case 'akuzativ': # akuzativ
-				if ( $word == 'Wikipedija' ) {
-					$word = 'Wikipediju';
-				} elseif ( $word == 'Wikiknihi' ) {
-					$word = 'Wikiknknihi';
-				} elseif ( $word == 'Wikinowiny' ) {
-					$word = 'Wikinowiny';
-				} elseif ( $word == 'Wikižórło' ) {
-					$word = 'Wikižórło';
-				} elseif ( $word == 'Wikicitaty' ) {
-					$word = 'Wikicitaty';
-				} elseif ( $word == 'Wikisłownik' ) {
-					$word = 'Wikisłownik';
-				}
-				break;
 			case 'instrumental': # instrumental
-				if ( $word == 'Wikipedija' ) {
-					$word = 'Wikipediju';
-				} elseif ( $word == 'Wikiknihi' ) {
-					$word = 'Wikiknihami';
-				} elseif ( $word == 'Wikinowiny' ) {
-					$word = 'Wikinowinami';
-				} elseif ( $word == 'Wikižórło' ) {
-					$word = 'Wikižórłom';
-				} elseif ( $word == 'Wikicitaty' ) {
-					$word = 'Wikicitatami';
-				} elseif ( $word == 'Wikisłownik' ) {
-					$word = 'Wikisłownikom';
-				} else {
-					$word = 'z ' . $word;
-				}
-				break;
+				$word = 'z ' . $word;
 			case 'lokatiw': # lokatiw
-				if ( $word == 'Wikipedija' ) {
-					$word = 'Wikipediji';
-				} elseif ( $word == 'Wikiknihi' ) {
-					$word = 'Wikiknihach';
-				} elseif ( $word == 'Wikinowiny' ) {
-					$word = 'Wikinowinach';
-				} elseif ( $word == 'Wikižórło' ) {
-					$word = 'Wikižórłu';
-				} elseif ( $word == 'Wikicitaty' ) {
-					$word = 'Wikicitatach';
-				} elseif ( $word == 'Wikisłownik' ) {
-					$word = 'Wikisłowniku';
-				} else {
-					$word = 'wo ' . $word;
-				}
+				$word = 'wo ' . $word;
 				break;
 			}
 

commit 08050066342d1aa326f3d77e9e0114930ac68ed5

Fix fatal error regression from r39077 -- 'break 2' must become 'break' when the second-level switch() is removed :)

languages/classes/LanguageSl.php
@@ -18,7 +18,7 @@ class LanguageSl extends Language {
 
 		switch ( $case ) {
 			case 'mestnik': # locative
-				$word = 'o ' . $word; break 2;
+				$word = 'o ' . $word; break;
 			case 'orodnik': # instrumental
 				$word = 'z ' . $word;
 		}