d418344d66a00f55ca21625c0f38502e9524e5d2
Tim Starling <tstarling@users.mediawiki.org>
2008-08-15 16:02:00 +0000

    Don't use $wgParser when {{int:}} is called, use $parser->replaceVariables() instead. Removes an unnecessary potential $wgTitle reference, and fixes Parser_DiffTest.

--- includes/parser/CoreParserFunctions.php
+++ includes/parser/CoreParserFunctions.php
@@ -56,7 +56,10 @@ class CoreParserFunctions {
 	static function intFunction( $parser, $part1 = '' /*, ... */ ) {
 		if ( strval( $part1 ) !== '' ) {
 			$args = array_slice( func_get_args(), 2 );
-			return wfMsgReal( $part1, $args, true );
+			$message = wfMsgGetKey( $part1, true, false, false );
+			$message = $parser->replaceVariables( $message ); // like $wgMessageCache->transform()
+			$message = wfMsgReplaceArgs( $message, $args );
+			return $message;
 		} else {
 			return array( 'found' => false );
 		}
