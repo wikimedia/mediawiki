e37b1ea422931c73efa3472ec0c86fbf4e1acc3f
Brion Vibber <brion@users.mediawiki.org>
2008-08-19 00:15:05 +0000

    Revert r39503, 39507 "(bug 14468) Lines in classic RecentChanges and Watchlist now have classes "odd" and "even" to make colouring using css possible."
    These alternating background colors are imho not very attractive, and make the display busy and confusing, rather than easier to read.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1382,7 +1382,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '166';
+$wgStyleVersion = '165';
 
 
 # Server-side caching:
