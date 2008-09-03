wikimedia/mediawiki

commit cbefdc5107d3ad667c6536bb10e1a7ae13c54ce3

API: (bug 11719) Remove trailing blanks in YAML output.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@32485 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -168,6 +168,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * Added help text message that specifies whether a module is POST-only
 * Added createonly parameter to action=edit
 * Replaced $wgAPIUCUserPrefixMinLength by the more generic $wgAPIMaxDBRows
+* (bug 11719) Remove trailing blanks in YAML output.
 
 === Languages updated in 1.13 ===
 

includes/api/ApiFormatYaml_spyc.php
@@ -424,10 +424,16 @@
 
       if (is_int($key)) {
         // It's a sequence
-        $string = $spaces.'- '.$value."\n";
+		if ($value)  
+			$string = $spaces.'- '.$value."\n";
+		else
+			$string = $spaces . "-\n";
       } else {
-        // It's mapped
-        $string = $spaces.$key.': '.$value."\n";
+		// It's mapped
+		if ($value)
+	        $string = $spaces.$key.': '.$value."\n";
+		else
+			$string = $spaces . $key . ":\n";
       }
       return $string;
     }

commit 1881098b81b48945138821e56a0ae3a3e06b8d48

(bug 15448) YAML output returns empty strings instead of 0. Surprisingly, this was not caused by PHP's "helpful" 0=='' ; research shows this was a regression from r32485 that no one ever noticed

RELEASE-NOTES
@@ -208,8 +208,9 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * Database replication lag doesn't cause all action=edit requests to return the
   nochange flag any more
 * (bug 15392) ApiFormatBase::formatHTML now uses $wgUrlProtocols.
-* (bug 15444) action=edit returned "Unknown error: ``AS_END''" where it should
+* (bug 15444) action=edit returns "Unknown error: ``AS_END''" where it should
   return just "Unknown error"
+* (bug 15448) YAML output returns empty values instead of 0
 
 === Languages updated in 1.14 ===
 

includes/api/ApiFormatYaml_spyc.php
@@ -424,14 +424,14 @@
 
       if (is_int($key)) {
         // It's a sequence
-		if ($value)
+		if ($value !== '' && !is_null($value))
 			$string = $spaces.'- '.$value."\n";
 		else
 			$string = $spaces . "-\n";
       } else {
 		// It's mapped
-		if ($value)
-	        $string = $spaces.$key.': '.$value."\n";
+		if ($value !== '' && !is_null($value))
+			$string = $spaces . $key . ': ' . $value . "\n";
 		else
 			$string = $spaces . $key . ":\n";
       }
@@ -589,7 +589,7 @@
           $array = $array + $this->_toType($v);
         }
         $value = $array;
-      } elseif (strtolower($value) == 'null' or $value == '' or $value == '~') {
+      } elseif (strtolower($value) == 'null' or $value === '' or $value == '~') {
         $value = NULL;
       } elseif (ctype_digit($value)) {
         $value = (int)$value;