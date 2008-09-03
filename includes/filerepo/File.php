6e70b8ee26c6729adeba845106c51aee60a57df3
Chad Horohoe <demon@users.mediawiki.org>
2008-09-03 15:47:42 +0000

    Implement 'thumbDir' configuration parameter for local and foreign repos. Replaces apiThumbCacheDir as it's redundant. Also allows for customization of the default '/thumb/' path on local repos.

--- includes/filerepo/File.php
+++ includes/filerepo/File.php
@@ -749,7 +749,7 @@ abstract class File {
 
 	/** Get relative path for a thumbnail file */
 	function getThumbRel( $suffix = false ) {
-		$path = 'thumb/' . $this->getRel();
+		$path = $this->repo->thumbDir . $this->getRel();
 		if ( $suffix !== false ) {
 			$path .= '/' . $suffix;
 		}
@@ -779,7 +779,7 @@ abstract class File {
 
 	/** Get the URL of the thumbnail directory, or a particular file if $suffix is specified */
 	function getThumbUrl( $suffix = false ) {
-		$path = $this->repo->getZoneUrl('public') . '/thumb/' . $this->getUrlRel();
+		$path = $this->repo->getZoneUrl('public') . '/'. $this->repo->thumbDir . $this->getUrlRel();
 		if ( $suffix !== false ) {
 			$path .= '/' . rawurlencode( $suffix );
 		}
@@ -799,7 +799,7 @@ abstract class File {
 
 	/** Get the virtual URL for a thumbnail file or directory */
 	function getThumbVirtualUrl( $suffix = false ) {
-		$path = $this->repo->getVirtualUrl() . '/public/thumb/' . $this->getUrlRel();
+		$path = $this->repo->getVirtualUrl() . '/public/' . $this->repo->thumbDir . $this->getUrlRel();
 		if ( $suffix !== false ) {
 			$path .= '/' . rawurlencode( $suffix );
 		}
--- includes/filerepo/ForeignAPIRepo.php
+++ includes/filerepo/ForeignAPIRepo.php
@@ -121,7 +121,7 @@ class ForeignAPIRepo extends FileRepo {
 		}
 		else {
 			$foreignUrl = $this->getThumbUrl( $name, $width, $height );
-			$path = $this->apiThumbCacheDir . '/' . $this->name . '/' .
+			$path = $this->thumbDir . '/' . $this->name . '/' .
 						$name . '/';
 			if ( !is_dir($wgUploadDirectory . '/' . $path) ) {
 				wfMkdirParents($wgUploadDirectory . '/' . $path);
