d394f88cdc50e27532ece83e1d74869515f2ea2f
Rotem Liss <rotem@users.mediawiki.org>
2008-09-07 16:57:33 +0000

    Localization update.

--- languages/messages/MessagesHe.php
+++ languages/messages/MessagesHe.php
@@ -1933,8 +1933,8 @@ $NEWPAGE
 'protect-cantedit'            => 'אינכם יכולים לשנות את רמת ההגנה על דף זה, כיוון שאין לכם הרשאה לערוך אותו.',
 'protect-othertime'           => 'זמן אחר:',
 'protect-othertime-op'        => 'זמן אחר',
-'protect-otherreason'         => 'אחר/נוסף:',
-'protect-otherreason-op'      => 'אחר/נוסף',
+'protect-otherreason'         => 'סיבה אחרת/נוספת:',
+'protect-otherreason-op'      => 'סיבה אחרת/נוספת',
 'protect-dropdown'            => '* סיבות הגנה נפוצות
 ** השחתה רבה
 ** ספאם רב
