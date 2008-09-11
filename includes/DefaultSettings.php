31b60f050a2537a27370866a48122aee585678d0
Alex Z <mrzman@users.mediawiki.org>
2008-09-11 03:50:53 +0000

    (bug 15551) Show deletion log excerpts when a user follows a redlink, even if they can't edit the page.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1383,7 +1383,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '174';
+$wgStyleVersion = '175';
 
 
 # Server-side caching:
