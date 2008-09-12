832ee701a56b2575fff1c67a5308da685bdc415a
Chad Horohoe <demon@users.mediawiki.org>
2008-09-12 01:32:23 +0000

    Bump style version for r40733.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1383,7 +1383,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '175';
+$wgStyleVersion = '176';
 
 
 # Server-side caching:
