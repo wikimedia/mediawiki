be21139554b5b6139f3d946548943a0dc26608cc
Danny B <danny_b@users.mediawiki.org>
2008-09-13 18:29:26 +0000

    + documentation

--- languages/classes/LanguageCs.php
+++ languages/classes/LanguageCs.php
@@ -1,11 +1,16 @@
 <?php
 
-/** Czech (česky)
+/** Czech (čeština [subst.], český [adj.], česky [adv.])
  *
  * @ingroup Language
  */
 class LanguageCs extends Language {
 
+	# Plural transformations
+	# Invoked by putting
+	#   {{plural:count|form1|form2-4|form0,5+}} for two forms plurals
+	#   {{plural:count|form1|form0,2+}} for single form plurals
+	# in a message
 	function convertPlural( $count, $forms ) {
 		if ( !count($forms) ) { return ''; }
 		$forms = $this->preConvertPlural( $forms, 3 );
