8027e4cec38472f2af9cb9dd2485c961c27b68ce
Aaron Schulz <aaron@users.mediawiki.org>
2008-09-14 12:25:53 +0000

    Don't throw fatals for bogus feed types (bug 15596)

--- includes/ChangesFeed.php
+++ includes/ChangesFeed.php
@@ -12,7 +12,8 @@ class ChangesFeed {
 	public function getFeedObject( $title, $description ) {
 		global $wgSitename, $wgContLanguageCode, $wgFeedClasses, $wgTitle;
 		$feedTitle = "$wgSitename  - {$title} [$wgContLanguageCode]";
-
+		if( !isset($wgFeedClasses[$this->format] ) )
+			return false;
 		return new $wgFeedClasses[$this->format](
 			$feedTitle, htmlspecialchars( $description ), $wgTitle->getFullUrl() );
 	}
