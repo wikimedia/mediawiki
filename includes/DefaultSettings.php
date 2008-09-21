520e44d4f94e2ddeb24517f35f9ffd4b5f787252
Chad Horohoe <demon@users.mediawiki.org>
2008-09-21 20:17:08 +0000

    +style version for r41128

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1381,7 +1381,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '177';
+$wgStyleVersion = '178';
 
 
 # Server-side caching:
