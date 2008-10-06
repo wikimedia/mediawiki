b612544e81c11217a4f86788480281a795e13b4f
Raimond Spekking <raymond@users.mediawiki.org>
2008-10-06 21:16:28 +0000

    * Format page protection warning identical to similar warnings * Move these lines to shared.css

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1385,7 +1385,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '179';
+$wgStyleVersion = '180';
 
 
 # Server-side caching:
