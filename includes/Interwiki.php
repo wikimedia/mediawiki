3b7bb258f664eff1003b1c3c5fb8647dc9c41e52
Matt Johnston <mattj@users.mediawiki.org>
2008-10-13 09:31:46 +0000

    Fix r42022, always returning true for isValidInterwiki as I forgot to add the check.

--- includes/Interwiki.php
+++ includes/Interwiki.php
@@ -50,7 +50,7 @@ class Interwiki {
 			unset( self::$smCache[ key( self::$smCache ) ] );
 		}
 		self::$smCache[$prefix] = $iw;
-		return true;
+		return ($iw != false);
 	}
 
 	/**
@@ -175,7 +175,9 @@ class Interwiki {
 		$iw = false;
 		if( $mc && is_array( $mc ) ){ // is_array is hack for old keys
 			$iw = Interwiki::loadFromArray( $mc );
-			return $iw;
+			if( $iw ){
+				return $iw;
+			}
 		}
 		
 		$db = wfGetDB( DB_SLAVE );
