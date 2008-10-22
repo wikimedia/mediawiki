c8f9f1dc086dcdb5b819dd020295e81307f70fda
Raimond Spekking <raymond@users.mediawiki.org>
2008-10-22 11:07:35 +0000

    Add 'mk' specific date/time formats per request on Translatewiki

--- languages/messages/MessagesMk.php
+++ languages/messages/MessagesMk.php
@@ -45,6 +45,49 @@ $skinNames = array(
 	'chick'       => 'Шик'
 );
 
+$datePreferences = array(
+	'default',
+	'dmy mk',
+	'ymd mk',
+	'ymdt mk',
+	'mdy',
+	'dmy',
+	'ymd',
+	'ISO 8601',
+);
+
+$defaultDateFormat = 'dmy or mdy';
+
+$dateFormats = array(
+	'dmy mk time' => 'H:i',
+	'dmy mk date' => 'j.m.Y',
+	'dmy mk both' => 'H:i, j.m.Y',
+
+	'ymd mk time' => 'H:i',
+	'ymd mk date' => 'Y.m.j',
+	'ymd mk both' => 'H:i, Y.m.j',
+
+	'ymdt mk time' => 'H:i:s',
+	'ymdt mk date' => 'Y.m.j',
+	'ymdt mk both' => 'Y.m.j, H:i:s',
+
+	'mdy time' => 'H:i',
+	'mdy date' => 'F j, Y',
+	'mdy both' => 'H:i, F j, Y',
+
+	'dmy time' => 'H:i',
+	'dmy date' => 'j F Y',
+	'dmy both' => 'H:i, j F Y',
+
+	'ymd time' => 'H:i',
+	'ymd date' => 'Y F j',
+	'ymd both' => 'H:i, Y F j',
+
+	'ISO 8601 time' => 'xnH:xni:xns',
+	'ISO 8601 date' => 'xnY-xnm-xnd',
+	'ISO 8601 both' => 'xnY-xnm-xnd"T"xnH:xni:xns',
+);
+
 $specialPageAliases = array(
 	'DoubleRedirects'           => array( 'ДвојниПренасочувања' ),
 	'BrokenRedirects'           => array( 'ПрекинатиПренасочувања' ),
