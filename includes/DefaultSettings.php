d974adaff433dcd58fd4bcaaf450f93b3ad485b7
Andrew Garrett <werdna@users.mediawiki.org>
2008-10-22 06:59:48 +0000

    Set minimum width for TablePager tables to 80%. It's really annoying when they're about a sixth of the screen width, and grow and shrink on the slightest change to their contents.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1385,7 +1385,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '181';
+$wgStyleVersion = '182';
 
 
 # Server-side caching:
