f2c3df36d0b0d5cc2388377e6a283d04eed96dd0
Alex Z <mrzman@users.mediawiki.org>
2008-10-28 02:38:59 +0000

    partial revert of r42695, don't trim the comment

--- includes/RecentChange.php
+++ includes/RecentChange.php
@@ -645,19 +645,6 @@ class RecentChange
 		# no colour (\003) switches back to the term default
 		$fullString = "\00314[[\00307$title\00314]]\0034 $flag\00310 " .
 		              "\00302$url\003 \0035*\003 \00303$user\003 \0035*\003 $szdiff \00310$comment\003\n";
-		# RFC 2812 sets the limit for IRC message length at 512 bytes
-		# If the message is longer than that, trim the comment as much as necessary
-		# It might still be too long, but there's not much else we can trim without losing anything important
-		if ( strlen($fullString) > 512 ) {
-			$extra = strlen($fullString) - 512;
-			if ( strlen($comment) > $extra ) {
-				$comment = substr($comment, 0, strlen($comment) - $extra);
-			} else {
-				$comment = '';
-			}
-			$fullString = "\00314[[\00307$title\00314]]\0034 $flag\00310 " .
-			              "\00302$url\003 \0035*\003 \00303$user\003 \0035*\003 $szdiff \00310$comment\003\n";
-		}
 			
 		return $fullString;
 	}
