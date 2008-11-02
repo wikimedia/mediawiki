bcd3c1612e24af3f1bce8ba4a63d9526c23d03a1
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-11-02 22:58:35 +0000

    (bug 16113) Show/hide for redirects in Special:NewPages. Patch by Paul Copperman

--- includes/specials/SpecialNewpages.php
+++ includes/specials/SpecialNewpages.php
@@ -26,6 +26,7 @@ class SpecialNewpages extends SpecialPage {
 		$opts->add( 'hideliu', false );
 		$opts->add( 'hidepatrolled', false );
 		$opts->add( 'hidebots', false );
+		$opts->add( 'hideredirs', true );
 		$opts->add( 'limit', (int)$wgUser->getOption( 'rclimit' ) );
 		$opts->add( 'offset', '' );
 		$opts->add( 'namespace', '0' );
@@ -58,6 +59,8 @@ class SpecialNewpages extends SpecialPage {
 				$this->opts->setValue( 'hidepatrolled', true );
 			if ( 'hidebots' == $bit )
 				$this->opts->setValue( 'hidebots', true );
+			if ( 'showredirs' == $bit )
+				$this->opts->setValue( 'hideredirs', false );
 			if ( is_numeric( $bit ) )
 				$this->opts->setValue( 'limit', intval( $bit ) );
 
@@ -125,7 +128,8 @@ class SpecialNewpages extends SpecialPage {
 		$filters = array(
 			'hideliu' => 'rcshowhideliu',
 			'hidepatrolled' => 'rcshowhidepatr',
-			'hidebots' => 'rcshowhidebots'
+			'hidebots' => 'rcshowhidebots',
+			'hideredirs' => 'newpages-showhideredirs'
 		);
 
 		// Disable some if needed
@@ -142,8 +146,8 @@ class SpecialNewpages extends SpecialPage {
 		$self = $this->getTitle();
 		foreach ( $filters as $key => $msg ) {
 			$onoff = 1 - $this->opts->getValue($key);
-			$link = $this->skin->makeKnownLinkObj( $self, $showhide[$onoff],
-				wfArrayToCGI( array( $key => $onoff ), $changed )
+			$link = $this->skin->link( $self, $showhide[$onoff], array(),
+				 array( $key => $onoff ) + $changed
 			);
 			$links[$key] = wfMsgHtml( $msg, $link );
 		}
@@ -348,8 +352,6 @@ class NewPagesPager extends ReverseChronologicalPager {
 	// Stored opts
 	protected $opts, $mForm;
 
-	private $hideliu, $hidepatrolled, $hidebots, $namespace, $user, $spTitle;
-
 	function __construct( $form, FormOptions $opts ) {
 		parent::__construct();
 		$this->mForm = $form;
@@ -381,7 +383,7 @@ class NewPagesPager extends ReverseChronologicalPager {
 			$rcIndexes = array( 'rc_timestamp' );
 		}
 		$conds[] = 'page_id = rc_cur_id';
-		$conds['page_is_redirect'] = 0;
+
 		# $wgEnableNewpagesUserFilter - temp WMF hack
 		if( $wgEnableNewpagesUserFilter && $user ) {
 			$conds['rc_user_text'] = $user->getText();
@@ -396,6 +398,10 @@ class NewPagesPager extends ReverseChronologicalPager {
 		}
 		if( $this->opts->getValue( 'hidebots' ) ) {
 			$conds['rc_bot'] = 0;
+		}
+
+		if ( $this->opts->getValue( 'hideredirs' ) ) {
+			$conds['page_is_redirect'] = 0;
 		}
 
 		return array(
