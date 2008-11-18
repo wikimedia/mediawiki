94e2fb4ae90107cfabfca4c2165697bcd054bc8d
Chad Horohoe <demon@users.mediawiki.org>
2008-11-18 12:11:03 +0000

    Cleanup to r43657, don't really need strlen() here.

--- includes/filerepo/ForeignAPIRepo.php
+++ includes/filerepo/ForeignAPIRepo.php
@@ -128,8 +128,7 @@ class ForeignAPIRepo extends FileRepo {
 			$foreignUrl = $this->getThumbUrl( $name, $width, $height );
 			
 			// We need the same filename as the remote one :)
-			$fileName = ltrim( substr( $foreignUrl, strrpos( $foreignUrl, '/' ),
-								strlen ( $foreignUrl ) ), '/' );
+			$fileName = ltrim( substr( $foreignUrl, strrpos( $foreignUrl, '/' ) ), '/' );
 			$path = 'thumb/' . $this->getHashPath( $name ) . $name . "/";
 			if ( !is_dir($wgUploadDirectory . '/' . $path) ) {
 				wfMkdirParents($wgUploadDirectory . '/' . $path);
