9a8b02e42f33fda3fe76f9b8382445d0ad8641d2
Greg Sabino Mullane <greg@users.mediawiki.org>
2008-11-22 06:44:45 +0000

    Add CASCADE back in: better to break older Postgres versions than new ones. Per bug 16256

--- maintenance/postgres/mediawiki_mysql2postgres.pl
+++ maintenance/postgres/mediawiki_mysql2postgres.pl
@@ -275,7 +275,7 @@ $verbose and warn qq{Writing truncates to empty existing tables\n};
 for my $t (@torder, 'objectcache', 'querycache') {
 	next if $t eq '---';
 	my $tname = $special{$t}||$t;
-	printf qq{TRUNCATE TABLE %-20s;\n}, qq{"$tname"};
+	printf qq{TRUNCATE TABLE %-20s CASCADE;\n}, qq{"$tname"};
 }
 print "\n\n";
 
