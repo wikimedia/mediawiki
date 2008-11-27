f26a332afce9479860ff94d9e0b2414e62537b2d
Daniel Kinzler <daniel@users.mediawiki.org>
2008-11-27 13:07:30 +0000

    image import new supports per-image description files

--- maintenance/importImages.inc.php
+++ maintenance/importImages.inc.php
@@ -47,3 +47,42 @@ function splitFilename( $filename ) {
 	$fname = implode( '.', $parts );
 	return array( $fname, $ext );
 }
+
+/**
+ * Find an auxilliary file with the given extension, matching 
+ * the give base file path. $maxStrip determines how many extensions 
+ * may be stripped from the original file name before appending the
+ * new extension. For example, with $maxStrip = 1 (the default), 
+ * file files acme.foo.bar.txt and acme.foo.txt would be auxilliary
+ * files for acme.foo.bar and the extension ".txt". With $maxStrip = 2,
+ * acme.txt would also be acceptable.
+ *
+ * @param $file base path
+ * @param $auxExtension the extension to be appended to the base path
+ * @param $maxStrip the maximum number of extensions to strip from the base path (default: 1)
+ * @return string or false
+ */
+function findAuxFile( $file, $auxExtension, $maxStrip = 1 ) {
+	if ( strpos( $auxExtension, '.' ) !== 0 ) {
+		$auxExtension = '.' . $auxExtension;
+	}
+
+	$d = dirname( $file );
+	$n = basename( $file );
+
+	while ( $maxStrip >= 0 ) {
+		$f = $d . '/' . $n . $auxExtension;
+
+		if ( file_exists( $f ) ) {
+			return $f;
+		}
+
+		$idx = strpos( $n, '.' );
+		if ( !$idx ) break;
+
+		$n = substr( $n, 0, $idx );
+		$maxStrip -= 1;
+	}
+
+	return false;
+}
