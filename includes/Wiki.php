eac021bb08fb8107bd96dade4a1cfd383c731331
Aaron Schulz <aaron@users.mediawiki.org>
2008-12-09 11:51:00 +0000

    Tweak profile calls

--- includes/Wiki.php
+++ includes/Wiki.php
@@ -351,13 +351,11 @@ class MediaWiki {
 	 * @param $updates array of objects that hold an update to do
 	 */
 	function doUpdates( &$updates ) {
-		wfProfileIn( __METHOD__ );
 		/* No need to get master connections in case of empty updates array */
-		if (!$updates) {
-			wfProfileOut( __METHOD__ );
+		if( !$updates ) {
 			return;
 		}
-
+		wfProfileIn( __METHOD__ );
 		$dbw = wfGetDB( DB_MASTER );
 		foreach( $updates as $up ) {
 			$up->doUpdate();
