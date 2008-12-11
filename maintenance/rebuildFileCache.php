bdb1e0073daaab46d5549f4be29b8b5d80b88e38
Aaron Schulz <aaron@users.mediawiki.org>
2008-12-11 14:35:33 +0000

    Add a script to filecache content pages on the wiki

---
+++ maintenance/rebuildFileCache.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Build file cache for content pages
+ *
+ * @file
+ * @ingroup Maintenance
+ */
+
+/** */
+require_once( "commandLine.inc" );
+if( !$wgUseFileCache ) exit(0);
+
+$start = isset($args[0]) ? intval($args[0]) : 0;
+echo "Building content page file cache from page {$start}!\n";
+
+$dbr = wfGetDB( DB_SLAVE );
+$start = $start > 0 ? $start : $dbr->selectField( 'page', 'MIN(page_id)', false, __FUNCTION__ );
+$end = $dbr->selectField( 'page', 'MAX(page_id)', false, __FUNCTION__ );
+if( !$start ) {
+	die("Nothing to do.\n");
+}
+
+$_SERVER['HTTP_ACCEPT_ENCODING'] = 'bgzip'; // hack, no real client
+OutputPage::setEncodings(); # Not really used yet
+
+$BATCH_SIZE = 100;
+# Do remaining chunk
+$end += $BATCH_SIZE - 1;
+$blockStart = $start;
+$blockEnd = $start + $BATCH_SIZE - 1;
+
+// Go through each page and save the output
+while( $blockEnd <= $end ) {
+	// Get the pages
+	$res = $dbr->select( 'page', array('page_namespace','page_title','page_id'),
+		array('page_namespace' => $wgContentNamespaces,
+			"page_id BETWEEN $blockStart AND $blockEnd" ),
+		array('ORDER BY' => 'page_id ASC','USE INDEX' => 'PRIMARY')
+	);
+	while( $row = $dbr->fetchObject( $res ) ) {
+		$wgTitle = Title::makeTitleSafe( $row->page_namespace, $row->page_title );
+		if( null == $wgTitle ) continue; // broken title?
+		$wgArticle = new Article( $wgTitle );
+		// If the article is cacheable, then load it
+		if( $wgArticle->isFileCacheable() ) {
+			$cache = new HTMLFileCache( $wgTitle );
+			if( $cache->isFileCacheGood() ) {
+				echo "Page {$row->page_id} already cached\n";
+				continue; // done already!
+			}
+			ob_start( array(&$cache, 'saveToFileCache' ) ); // save on ob_end_clean()
+			$wgUseFileCache = false; // hack, we don't want $wgArticle fiddling with filecache
+			$wgArticle->view();
+			@$wgOut->output(); // header notices
+			$wgUseFileCache = true;
+			ob_end_clean(); // clear buffer
+			$wgOut = new OutputPage(); // empty out any output page garbage
+			echo "Cached page {$row->page_id}\n";
+		}
+	}
+	$blockStart += $BATCH_SIZE - 1;
+	$blockEnd += $BATCH_SIZE - 1;
+	wfWaitForSlaves( 5 );
+}
+echo "Done!\n";
+
+// Remove these to be safe
+if( isset($wgTitle) )
+	unset($wgTitle);
+if( isset($wgArticle) )
+	unset($wgArticle);
