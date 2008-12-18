0d36d84c3187e0c2ad5e3e1561f31f30a2c6a735
Aaron Schulz <aaron@users.mediawiki.org>
2008-12-18 23:09:49 +0000

    Streamline links a bit

--- includes/Skin.php
+++ includes/Skin.php
@@ -1160,15 +1160,11 @@ END;
 		global $wgOut;
 		$sep = " |\n";
 
-		$s = $this->mainPageLink() . $sep
-		  . $this->specialLink( 'recentchanges' );
+		$s = $this->mainPageLink();
 
 		if ( $wgOut->isArticleRelated() ) {
-			$s .=  $sep . $this->editThisPage()
-			  . $sep . $this->historyLink();
+			$s .=  $sep . $this->editThisPage() . $sep . $this->historyLink();
 		}
-		# Many people don't like this dropdown box
-		#$s .= $sep . $this->specialPagesList();
 
 		$s .= $this->variantLinks();
 
