4223b1c209a549538815c81f64b7d8fd1dec2d58
Aaron Schulz <aaron@users.mediawiki.org>
2008-12-30 16:09:11 +0000

    Escape entities in h1 title html

--- includes/Skin.php
+++ includes/Skin.php
@@ -1018,7 +1018,7 @@ END;
 
 	function pageTitle() {
 		global $wgOut;
-		$s = '<h1 class="pagetitle">' . $wgOut->getPageTitle() . '</h1>';
+		$s = '<h1 class="pagetitle">' . htmlspecialchars( $wgOut->getPageTitle() ) . '</h1>';
 		return $s;
 	}
 
