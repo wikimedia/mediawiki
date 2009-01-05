3272f4a08a1965a0a9e2c7bd2261b16300bdadfa
Aryeh Gregor <simetrical@users.mediawiki.org>
2009-01-05 15:59:17 +0000

    Add comment on urlencoding fragments

--- includes/Title.php
+++ includes/Title.php
@@ -452,6 +452,10 @@ class Title {
 	 */
 	static function escapeFragmentForURL( $fragment ) {
 		global $wgEnforceHtmlIds;
+		# Note that we don't urlencode the fragment.  urlencoded Unicode
+		# fragments appear not to work in IE (at least up to 7) or in at least
+		# one version of Opera 9.x.  The W3C validator, for one, doesn't seem
+		# to care if they aren't encoded.
 		return Sanitizer::escapeId( $fragment,
 			$wgEnforceHtmlIds ? array() : 'xml' );
 	}
