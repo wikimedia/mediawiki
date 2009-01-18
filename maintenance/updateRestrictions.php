455038dd9f12467a900c057f733dbab550ff6e6b
Aaron Schulz <aaron@users.mediawiki.org>
2009-01-18 19:55:01 +0000

    Tweak r45600: kill the broken page_restrictions columns too

--- maintenance/updateRestrictions.php
+++ maintenance/updateRestrictions.php
@@ -22,25 +22,23 @@ if ( !$db->tableExists( 'page_restrictions' ) ) {
 migrate_page_restrictions( $db );
 
 function migrate_page_restrictions( $db ) {
-	
 	$start = $db->selectField( 'page', 'MIN(page_id)', false, __FUNCTION__ );
-	$end = $db->selectField( 'page', 'MAX(page_id)', false, __FUNCTION__ );
-	
 	if( !$start ) {
 		die("Nothing to do.\n");
 	}
+	$end = $db->selectField( 'page', 'MAX(page_id)', false, __FUNCTION__ );
 	
 	# Do remaining chunk
 	$end += BATCH_SIZE - 1;
 	$blockStart = $start;
 	$blockEnd = $start + BATCH_SIZE - 1;
 	$encodedExpiry = 'infinity';
-	while ( $blockEnd <= $end ) {
+	while( $blockEnd <= $end ) {
 		echo "...doing page_id from $blockStart to $blockEnd\n";
 		$cond = "page_id BETWEEN $blockStart AND $blockEnd AND page_restrictions !=''";
-		$res = $db->select( 'page', array('page_id', 'page_restrictions'), $cond, __FUNCTION__ );
+		$res = $db->select( 'page', array('page_id','page_namespace','page_restrictions'), $cond, __FUNCTION__ );
 		$batch = array();
-		while ( $row = $db->fetchObject( $res ) ) {
+		while( $row = $db->fetchObject( $res ) ) {
 			$oldRestrictions = array();
 			foreach( explode( ':', trim( $row->page_restrictions ) ) as $restrict ) {
 				$temp = explode( '=', trim( $restrict ) );
@@ -52,6 +50,12 @@ function migrate_page_restrictions( $db ) {
 				} else if( $temp[1] ) {
 					$oldRestrictions[$temp[0]] = trim( $temp[1] );
 				}
+			}
+			# Clear invalid columns
+			if( $row->page_namespace == NS_MEDIAWIKI ) {
+				$db->update( 'page', array( 'page_restrictions' => '' ),
+					array( 'page_id' => $row->page_id ), __FUNCTION__ );
+				echo "...removed dead page_restrictions column for page {$row->page_id}\n";
 			}
 			# Update restrictions table
 			foreach( $oldRestrictions as $action => $restrictions ) {
@@ -67,9 +71,8 @@ function migrate_page_restrictions( $db ) {
 		# We use insert() and not replace() as Article.php replaces
 		# page_restrictions with '' when protected in the restrictions table
 		if ( count( $batch ) ) {
-			$ok = $db->deadlockLoop(
-				array( $db, 'insert' ),
-				'page_restrictions', $batch, __FUNCTION__, array( 'IGNORE' ) );
+			$ok = $db->deadlockLoop( array( $db, 'insert' ), 'page_restrictions', 
+				$batch, __FUNCTION__, array( 'IGNORE' ) );
 			if( !$ok ) {
 				throw new MWException( "Deadlock loop failed wtf :(" );
 			}
@@ -82,7 +85,7 @@ function migrate_page_restrictions( $db ) {
 	// Kill any broken rows from previous imports
 	$db->delete( 'page_restrictions', array( 'pr_level' => '' ) );
 	// Kill other invalid rows
-	#$db->deleteJoin( 'page_restrictions', 'page', 'pr_page', 'page_id', array('page_namespace' => NS_MEDIAWIKI) );
+	$db->deleteJoin( 'page_restrictions', 'page', 'pr_page', 'page_id', array('page_namespace' => NS_MEDIAWIKI) );
 	echo "...Done!\n";
 }
 
