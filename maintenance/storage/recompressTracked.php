33d2aed65440a1e47d6cb082ea85b78b68a7c1fb
Tim Starling <tstarling@users.mediawiki.org>
2009-01-27 07:24:46 +0000

    Fixed a bug where the last batch of orphans would be skipped.

--- maintenance/storage/recompressTracked.php
+++ maintenance/storage/recompressTracked.php
@@ -333,7 +333,6 @@ class RecompressTracked {
 		} else {
 			$this->info( "Moving orphans..." );
 		}
-		$ids = array();
 
 		while ( true ) {
 			$res = $dbr->select( 'blob_tracking',
@@ -353,6 +352,7 @@ class RecompressTracked {
 			if ( !$res->numRows() ) {
 				break;
 			}
+			$ids = array();
 			foreach ( $res as $row ) {
 				$ids[] = $row->bt_text_id;
 				$i++;
@@ -366,6 +366,12 @@ class RecompressTracked {
 				array_unshift( $args, 'doOrphanList' );
 				call_user_func_array( array( $this, 'dispatch' ), $args );
 			}
+			if ( count( $ids ) ) {
+				$args = $ids;
+				array_unshift( $args, 'doOrphanList' );
+				call_user_func_array( array( $this, 'dispatch' ), $args );
+			}
+
 			$startId = $row->bt_text_id;
 			$this->report( 'orphans', $i, $numOrphans );
 		}
