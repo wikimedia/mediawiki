19c5fa8712ea1692c8efd9d32eb8d53507fd61ff
Aaron Schulz <aaron@users.mediawiki.org>
2009-01-31 09:07:42 +0000

    Styled #mw-data-after-content in cologneblue.css to match the rest of the font (bug 17110)

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1455,7 +1455,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '203';
+$wgStyleVersion = '204';
 
 
 # Server-side caching:
