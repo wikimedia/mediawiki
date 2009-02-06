51fb4dc09075e0f414e80dc83c2d97e3063c7ca1
Tim Starling <tstarling@users.mediawiki.org>
2009-02-06 05:43:26 +0000

    Fixed missing quotes in r46840

--- config/index.php
+++ config/index.php
@@ -1398,7 +1398,7 @@ if( count( $errs ) ) {
 	<label class='column'>Database type:</label>
 <?php 
 	if (isset($errs['DBpicktype'])) {
-		print "\t<span class='error'>" . htmlspecialchars( $errs[DBpicktype] ) . "</span>\n";
+		print "\t<span class='error'>" . htmlspecialchars( $errs['DBpicktype'] ) . "</span>\n";
 	}
 ?>
 	<ul class='plain'><?php 
