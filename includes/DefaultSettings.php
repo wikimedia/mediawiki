c50f0e8785c1c8cde8fb1efda1d23f966e93c098
Tim Starling <tstarling@users.mediawiki.org>
2009-02-07 02:54:07 +0000

    Updates for release

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -31,7 +31,7 @@ require_once( "$IP/includes/SiteConfiguration.php" );
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion			= '1.12.3';
+$wgVersion			= '1.12.4';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
