381ca54e98e10ee747922be8d66e3c997c760677
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-02-09 14:31:48 +0000

    * remove forgotten case of hard coded pipe separator in Pager * fix bug in GlobalFunctions.php introduced in r47028. pipeList() needs to be fed an array.

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -986,11 +986,11 @@ function wfViewPrevNext( $offset, $limit, $link, $query = '', $atend = false ) {
 	} else {
 		$nlink = '<a href="' . $title->escapeLocalUrl( $q ) . "\" class=\"mw-nextlink\">{$next}</a>";
 	}
-	$nums = $wgLang->pipeList( wfNumLink( $offset, 20, $title, $query ),
+	$nums = $wgLang->pipeList( array( wfNumLink( $offset, 20, $title, $query ),
 	  wfNumLink( $offset, 50, $title, $query ),
 	  wfNumLink( $offset, 100, $title, $query ),
 	  wfNumLink( $offset, 250, $title, $query ),
-	  wfNumLink( $offset, 500, $title, $query ) );
+	  wfNumLink( $offset, 500, $title, $query ) ) );
 
 	return wfMsg( 'viewprevnext', $plink, $nlink, $nums );
 }
