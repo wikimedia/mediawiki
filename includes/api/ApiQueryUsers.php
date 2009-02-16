wikimedia/mediawiki

commit a678068a5c325c155ad74b0be44b3885488aa9a9

* API: BREAKING CHANGE: (bug 11430) Return fewer results than the limit in some cases to prevent running out of memory * This means queries could possibly return fewer results than the limit and still set a query-continue * Add iicontinue, rvcontinue, cicontinue, incontinue, amfrom to faciliate query-continue for these modules * Implemented by blocking additions to the ApiResult object if they would make it too large ** Important things like query-continue values and warnings are exempt from this check ** RSS feeds and exported XML are also exempted (size-checking them would be too messy) ** Result size is checked against $wgAPIMaxResultSize, which defaults to 8 MB
For those who really care, per-file details follow:

ApiResult.php:
* Introduced ApiResult::$mSize which keeps track of the result size.
* Introduced ApiResult::size() which calculates an array's size
  (which is the sum of the strlen()s of its elements).
* ApiResult::addValue() now checks that the result size stays below
  $wgAPIMaxResultSize. If the item won't fit, it won't be added and addValue()
  will return false. Callers should check the return value and set a
  query-continue if it's false.
* Closed the back door that is ApiResult::getData(): callers can't manipulate
  the data array directly anymore so they can't bypass the result size limit.
* Added ApiResult::setIndexedTagName_internal() which will call
  setIndexedTagName() on an array already in the result. This is needed for the
  'new' order of adding results, which means addValue()ing one result at a time
  until you hit the limit or run out, then calling this function to set the tag
  name.
* Added ApiResult::disableSizeCheck() and enableSizeCheck() which disable and
  enable size checking in addValue(). This is used for stuff like query-continue
  elements and warnings which shouldn't count towards the result size.
* Added ApiResult::unsetValue() which removes an element from the result and
  decreases $mSize.

ApiBase.php:

includes/api/ApiQueryUsers.php
@@ -51,71 +51,72 @@ if (!defined('MEDIAWIKI')) {
 			$this->prop = array();
 		}
 
-		if(is_array($params['users'])) {
-			$r = $this->getOtherUsersInfo($params['users']);
-			$result->setIndexedTagName($r, 'user');
-		}
-		$result->addValue("query", $this->getModuleName(), $r);
-	}
-
-	protected function getOtherUsersInfo($users) {
-		$goodNames = $retval = array();
+		$users = (array)$params['users'];
+		$goodNames = $done = array();
+		$result = $this->getResult();
 		// Canonicalize user names
 		foreach($users as $u) {
 			$n = User::getCanonicalName($u);
 			if($n === false || $n === '')
-				$retval[] = array('name' => $u, 'invalid' => '');
+			{
+				$vals = array('name' => $u, 'invalid' => '');
+				$fit = $result->addValue(array('query', $this->getModuleName()),
+						null, $vals);
+				if(!$fit)
+				{
+					$this->setContinueEnumParameter('users',
+							implode('|', array_diff($users, $done)));
+					$goodNames = array();
+					break;
+				}
+				$done[] = $u;
+			}
 			 else
 				$goodNames[] = $n;
 		}
-		if(!count($goodNames))
-			return $retval;
+		if(count($goodNames))
+		{
+			$db = $this->getDb();
+			$this->addTables('user', 'u1');
+			$this->addFields('u1.user_name');
+			$this->addWhereFld('u1.user_name', $goodNames);
+			$this->addFieldsIf('u1.user_editcount', isset($this->prop['editcount']));
+			$this->addFieldsIf('u1.user_registration', isset($this->prop['registration']));
 
-		$db = $this->getDB();
-		$this->addTables('user', 'u1');
-		$this->addFields('u1.*');
-		$this->addWhereFld('u1.user_name', $goodNames);
+			if(isset($this->prop['groups'])) {
+				$this->addTables('user_groups');
+				$this->addJoinConds(array('user_groups' => array('LEFT JOIN', 'ug_user=u1.user_id')));
+				$this->addFields('ug_group');
+			}
+			if(isset($this->prop['blockinfo'])) {
+				$this->addTables('ipblocks');
+				$this->addTables('user', 'u2');
+				$u2 = $this->getAliasedName('user', 'u2');
+				$this->addJoinConds(array(
+					'ipblocks' => array('LEFT JOIN', 'ipb_user=u1.user_id'),
+					$u2 => array('LEFT JOIN', 'ipb_by=u2.user_id')));
+				$this->addFields(array('ipb_reason', 'u2.user_name blocker_name'));
+			}
 
-		if(isset($this->prop['groups'])) {
-			$this->addTables('user_groups');
-			$this->addJoinConds(array('user_groups' => array('LEFT JOIN', 'ug_user=u1.user_id')));
-			$this->addFields('ug_group');
+			$data = array();
+			$res = $this->select(__METHOD__);
+			while(($r = $db->fetchObject($res))) {
+				$data[$r->user_name]['name'] = $r->user_name;
+				if(isset($this->prop['editcount']))
+					$data[$r->user_name]['editcount'] = $r->user_editcount;
+				if(isset($this->prop['registration']))
+					$data[$r->user_name]['registration'] = wfTimestampOrNull(TS_ISO_8601, $r->user_registration);
+				if(isset($this->prop['groups']))
+					// This row contains only one group, others will be added from other rows
+					if(!is_null($r->ug_group))
+						$data[$r->user_name]['groups'][] = $r->ug_group;
+				if(isset($this->prop['blockinfo']))
+					if(!is_null($r->blocker_name)) {
+						$data[$r->user_name]['blockedby'] = $r->blocker_name;
+						$data[$r->user_name]['blockreason'] = $r->ipb_reason;
+					}
+			}
 		}
-		if(isset($this->prop['blockinfo'])) {
-			$this->addTables('ipblocks');
-			$this->addTables('user', 'u2');
-			$u2 = $this->getAliasedName('user', 'u2');
-			$this->addJoinConds(array(
-				'ipblocks' => array('LEFT JOIN', 'ipb_user=u1.user_id'),
-				$u2 => array('LEFT JOIN', 'ipb_by=u2.user_id')));
-			$this->addFields(array('ipb_reason', 'u2.user_name blocker_name'));
-		}
-
-		$data = array();
-		$res = $this->select(__METHOD__);
-		while(($r = $db->fetchObject($res))) {
-			$user = User::newFromRow($r);
-			$name = $user->getName();
-			$data[$name]['name'] = $name;
-			if(isset($this->prop['editcount']))
-				// No proper member function in User class for this
-				$data[$name]['editcount'] = $r->user_editcount;
-			if(isset($this->prop['registration']))
-				// Nor for this one
-				$data[$name]['registration'] = wfTimestampOrNull(TS_ISO_8601, $r->user_registration);
-			if(isset($this->prop['groups']))
-				// This row contains only one group, others will be added from other rows
-				if(!is_null($r->ug_group))
-					$data[$name]['groups'][] = $r->ug_group;
-			if(isset($this->prop['blockinfo']))
-				if(!is_null($r->blocker_name)) {
-					$data[$name]['blockedby'] = $r->blocker_name;
-					$data[$name]['blockreason'] = $r->ipb_reason;
-				}
-			if(isset($this->prop['emailable']) && $user->canReceiveEmail())
-				$data[$name]['emailable'] = '';
-		}
-
 		// Second pass: add result data to $retval
 		foreach($goodNames as $u) {
 			if(!isset($data[$u]))
@@ -125,8 +126,9 @@ if (!defined('MEDIAWIKI')) {
 					$this->getResult()->setIndexedTagName($data[$u]['groups'], 'g');
 				$retval[] = $data[$u];
 			}
+			$done[] = $u;
 		}
-		return $retval;
+		return $this->getResult()->setIndexedTagName_internal(array('query', $this->getModuleName()), 'user');
 	}
 
 	public function getAllowedParams() {

includes/DefaultSettings.php
@@ -3519,6 +3519,12 @@ $wgAPIListModules = array();
 $wgAPIMaxDBRows = 5000;
 
 /**
+ * The maximum size (in bytes) of an API result.
+ * Don't set this lower than $wgMaxArticleSize*1024
+ */
+$wgAPIMaxResultSize = 8388608;
+
+/**
  * Parser test suite files to be run by parserTests.php when no specific
  * filename is passed to it.
  *

includes/api/ApiBase.php
@@ -147,7 +147,7 @@ abstract class ApiBase {
 	/**
 	 * Get the result data array
 	 */
-	public function & getResultData() {
+	public function getResultData() {
 		return $this->getResult()->getData();
 	}
 
@@ -156,20 +156,23 @@ abstract class ApiBase {
 	 * notice any changes in API.
 	 */
 	public function setWarning($warning) {
-		# If there is a warning already, append it to the existing one
-		$data =& $this->getResult()->getData();
+		$data = $this->getResult()->getData();
 		if(isset($data['warnings'][$this->getModuleName()]))
 		{
 			# Don't add duplicate warnings
 			$warn_regex = preg_quote($warning, '/');
 			if(preg_match("/{$warn_regex}(\\n|$)/", $data['warnings'][$this->getModuleName()]['*']))
 				return;
-			$warning = "{$data['warnings'][$this->getModuleName()]['*']}\n$warning";
-			unset($data['warnings'][$this->getModuleName()]);
+			$oldwarning = $data['warnings'][$this->getModuleName()]['*'];
+			# If there is a warning already, append it to the existing one
+			$warning = "$oldwarning\n$warning";
+			$this->getResult()->unsetValue('warnings', $this->getModuleName());
 		}
 		$msg = array();
 		ApiResult :: setContent($msg, $warning);
+		$this->getResult()->disableSizeCheck();
 		$this->getResult()->addValue('warnings', $this->getModuleName(), $msg);
+		$this->getResult()->enableSizeCheck();
 	}
 
 	/**

includes/api/ApiFormatBase.php
@@ -261,9 +261,13 @@ class ApiFormatFeedWrapper extends ApiFormatBase {
 	public static function setResult($result, $feed, $feedItems) {
 		// Store output in the Result data.
 		// This way we can check during execution if any error has occured
-		$data = & $result->getData();
-		$data['_feed'] = $feed;
-		$data['_feeditems'] = $feedItems;
+		// Disable size checking for this because we can't continue
+		// cleanly; size checking would cause more problems than it'd
+		// solve
+		$result->disableSizeCheck();
+		$result->addValue(null, '_feed', $feed);
+		$result->addValue(null, '_feeditems', $feedItems);
+		$result->enableSizeCheck();
 	}
 
 	/**
@@ -282,8 +286,8 @@ class ApiFormatFeedWrapper extends ApiFormatBase {
 
 	/**
 	 * This class expects the result data to be in a custom format set by self::setResult()
-	 * $result['_feed']		 - an instance of one of the $wgFeedClasses classes
-	 * $result['_feeditems'] - an array of FeedItem instances
+	 * $result['_feed']		- an instance of one of the $wgFeedClasses classes
+	 * $result['_feeditems']	- an array of FeedItem instances
 	 */
 	public function execute() {
 		$data = $this->getResultData();

includes/api/ApiMain.php
@@ -361,9 +361,11 @@ class ApiMain extends ApiBase {
 			}
 
 			$this->getResult()->reset();
+			$this->getResult()->disableSizeCheck();
 			// Re-add the id
-			if($this->mRequest->getCheck('requestid'))
-				$this->getResult()->addValue(null, 'requestid', $this->mRequest->getVal('requestid'));
+			$requestid = $this->getParameter('requestid');
+			if(!is_null($requestid))
+				$this->getResult()->addValue(null, 'requestid', $requestid);
 			$this->getResult()->addValue(null, 'error', $errMessage);
 
 		return $errMessage['code'];
@@ -374,8 +376,9 @@ class ApiMain extends ApiBase {
 	 */
 	protected function executeAction() {
 		// First add the id to the top element
-		if($this->mRequest->getCheck('requestid'))
-			$this->getResult()->addValue(null, 'requestid', $this->mRequest->getVal('requestid'));
+		$requestid = $this->getParameter('requestid');
+		if(!is_null($requestid))
+			$this->getResult()->addValue(null, 'requestid', $requestid);
 
 		$params = $this->extractRequestParams();
 

includes/api/ApiQuery.php
@@ -255,6 +255,10 @@ class ApiQuery extends ApiBase {
 		$pageSet = $this->getPageSet();
 		$result = $this->getResult();
 
+		# We don't check for a full result set here because we can't be adding
+		# more than 380K. The maximum revision size is in the megabyte range,
+		# and the maximum result size must be even higher than that.
+
 		// Title normalizations
 		$normValues = array ();
 		foreach ($pageSet->getNormalizedTitles() as $rawTitleStr => $titleStr) {
@@ -368,6 +372,10 @@ class ApiQuery extends ApiBase {
 				$exporter->closeStream();
 				$exportxml = ob_get_contents();
 				ob_end_clean();
+				// Don't check the size of exported stuff
+				// It's not continuable, so it would cause more
+				// problems than it'd solve
+				$result->disableSizeCheck();
 				if ($this->params['exportnowrap']) {
 					$result->reset();
 					// Raw formatter will handle this
@@ -378,6 +386,7 @@ class ApiQuery extends ApiBase {
 					ApiResult::setContent($r, $exportxml);
 					$result->addValue('query', 'export', $r);
 				}
+				$result->enableSizeCheck();
 			}
 		}
 	}

includes/api/ApiQueryAllCategories.php
@@ -110,14 +110,18 @@ class ApiQueryAllCategories extends ApiQueryGeneratorBase {
 				}
 				if( isset( $prop['hidden'] ) && $row->cat_hidden )
 					$item['hidden'] = '';
-				$categories[] = $item;
+				$fit = $result->addValue(array('query', $this->getModuleName()), null, $item);
+				if(!$fit)
+				{
+					$this->setContinueEnumParameter('from', $this->keyToTitle($row->cat_title));
+					break;
+				}
 			}
 		}
 		$db->freeResult($res);
 
 		if (is_null($resultPageSet)) {
-			$result->setIndexedTagName($categories, 'c');
-			$result->addValue('query', $this->getModuleName(), $categories);
+			$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'c');
 		} else {
 			$resultPageSet->populateFromTitles($pages);
 		}

includes/api/ApiQueryAllLinks.php
@@ -101,8 +101,9 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 
 		$res = $this->select(__METHOD__);
 
-		$data = array ();
+		$pageids = array ();
 		$count = 0;
+		$result = $this->getResult();
 		while ($row = $db->fetchObject($res)) {
 			if (++ $count > $limit) {
 				// We've reached the one extra which shows that there are additional pages to be had. Stop here...
@@ -123,7 +124,13 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 					$vals['ns'] = intval($title->getNamespace());
 					$vals['title'] = $title->getPrefixedText();
 				}
-				$data[] = $vals;
+				$fit = $result->addValue(array('query', $this->getModuleName()), null, $vals);
+				if(!$fit)
+				{
+					$this->setContinueEnumParameter('continue',
+						$this->keyToTitle($row->pl_title) . "|" . $row->pl_from);
+					break;
+				}
 			} else {
 				$pageids[] = $row->pl_from;
 			}
@@ -131,9 +138,7 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 		$db->freeResult($res);
 
 		if (is_null($resultPageSet)) {
-			$result = $this->getResult();
-			$result->setIndexedTagName($data, 'l');
-			$result->addValue('query', $this->getModuleName(), $data);
+			$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'l');
 		} else {
 			$resultPageSet->populateFromPageIDs($pageids);
 		}

commit 036351d36238e39bfdf9c262d11ee9f5d58c1c0f

API: Fix yet another regression from r46845 that completely broke list=users. Patch by Brad Jorsch

includes/api/ApiQueryUsers.php
@@ -120,11 +120,18 @@ if (!defined('MEDIAWIKI')) {
 		// Second pass: add result data to $retval
 		foreach($goodNames as $u) {
 			if(!isset($data[$u]))
-				$retval[] = array('name' => $u, 'missing' => '');
+				$data[$u] = array('name' => $u, 'missing' => '');
 			else {
 				if(isset($this->prop['groups']) && isset($data[$u]['groups']))
 					$this->getResult()->setIndexedTagName($data[$u]['groups'], 'g');
-				$retval[] = $data[$u];
+			}
+			$fit = $result->addValue(array('query', $this->getModuleName()),
+					null, $data[$u]);
+			if(!$fit)
+			{
+					$this->setContinueEnumParameter('users',
+							implode('|', array_diff($users, $done)));
+					break;
 			}
 			$done[] = $u;
 		}