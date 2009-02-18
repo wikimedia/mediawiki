d12152cdec953f55ddc5beece0bc8ffcb7173977
Chad Horohoe <demon@users.mediawiki.org>
2009-02-18 02:00:14 +0000

    Call static isValidIsbn() statically. Not sure why E_ALL | E_STRICT didn't yell at this.

--- includes/specials/SpecialBooksources.php
+++ includes/specials/SpecialBooksources.php
@@ -34,7 +34,7 @@ class SpecialBookSources extends SpecialPage {
 		$wgOut->addWikiMsg( 'booksources-summary' );
 		$wgOut->addHTML( $this->makeForm() );
 		if( strlen( $this->isbn ) > 0 ) {
-			if( !$this->isValidIsbn( $this->isbn ) ) {
+			if( !self::isValidISBN( $this->isbn ) ) {
 				$wgOut->wrapWikiMsg( '<div class="error">$1</div>', 'booksources-invalid-isbn' );
 			}
 			$this->showList();
