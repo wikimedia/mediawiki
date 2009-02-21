dd0a2536d03b641a4a8f7b7541b9978387b3e315
Chad Horohoe <demon@users.mediawiki.org>
2009-02-21 22:31:39 +0000

    Set eol-style and somehow duplicated content wtf?

--- skins/common/changepassword.js
+++ skins/common/changepassword.js
@@ -14,19 +14,3 @@ function onNameChangeHook() {
 }
 
 addOnloadHook( onNameChangeHook );
-
-function onNameChange() {
-	if ( wgUserName != document.getElementById('wpName').value ) {
-		document.getElementById('wpPassword').disabled = true;
-		document.getElementById('wpComment').disabled = false;
-	} else {
-		document.getElementById('wpPassword').disabled = false;
-		document.getElementById('wpComment').disabled = true;
-	}
-}
-
-function onNameChangeHook() {
-	document.getElementById( 'wpName' ).onblur = onNameChange;
-}
-
-addOnloadHook( onNameChangeHook );
