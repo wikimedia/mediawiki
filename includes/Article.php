0c260a4362a05fd0a2940355e2092b4dcc48e743
Niklas Laxström <nikerabbit@users.mediawiki.org>
2009-02-21 10:41:17 +0000

    Tweak for bug 17103 (patch: Marcin Cieślak)

--- includes/Article.php
+++ includes/Article.php
@@ -1806,7 +1806,7 @@ class Article {
 		}
 
 		#It would be nice to see where the user had actually come from, but for now just guess
-		$returnto = $rc->getAttribute( 'rc_type' ) == RC_NEW ? 'newpages' : 'recentchanges';
+		$returnto = $rc->getAttribute( 'rc_type' ) == RC_NEW ? 'Newpages' : 'Recentchanges';
 		$return = SpecialPage::getTitleFor( $returnto );
 
 		$dbw = wfGetDB( DB_MASTER );
