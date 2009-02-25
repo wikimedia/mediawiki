75634f3414abf58dc5347b9291db4b27f8192cb7
Rotem Liss <rotem@users.mediawiki.org>
2009-02-25 07:32:25 +0000

    Localization fix.

--- languages/messages/MessagesHe.php
+++ languages/messages/MessagesHe.php
@@ -434,7 +434,7 @@ $messages = array(
 'category-file-count-limited'    => 'קטגוריה זו כוללת את {{PLURAL:$1|הקובץ הבא|$1 הקבצים הבאים}}.',
 'listingcontinuesabbrev'         => '(המשך)',
 
-'mainpagetext'      => "'''תוכנת מדיה־ויקי הותקנה בהצלחה.'''",
+'mainpagetext'      => "<big>'''תוכנת מדיה־ויקי הותקנה בהצלחה.'''</big>",
 'mainpagedocfooter' => 'היעזרו ב[http://meta.wikimedia.org/wiki/Help:Contents מדריך למשתמש] למידע על שימוש בתוכנת הוויקי.
 
 == קישורים שימושיים ==
