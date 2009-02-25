b85c2f57437e437d62a73c7b2901cf395dcc8881
Rotem Liss <rotem@users.mediawiki.org>
2009-02-25 07:27:52 +0000

    Localization update for he + fixing comment.

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -2557,7 +2557,7 @@ $1',
 'contributions-title' => 'User contributions for $1',
 'mycontris'           => 'My contributions',
 'contribsub2'         => 'For $1 ($2)',
-'nocontribs'          => 'No changes were found matching these criteria.', # optional parameter: $1 is the user name
+'nocontribs'          => 'No changes were found matching these criteria.', # Optional parameter: $1 is the user name
 'uctop'               => '(top)',
 'month'               => 'From month (and earlier):',
 'year'                => 'From year (and earlier):',
