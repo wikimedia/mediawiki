6b7039df1bfe79901fab97732142ee3fd379bef2
Tim Starling <tstarling@users.mediawiki.org>
2009-02-26 08:04:28 +0000

    * Submit jobs in batches and wait for each batch, to avoid overflowing the queue * Report success before running exec() so that the client doesn't wait forever

--- maintenance/gearman/gearmanRefreshLinks.php
+++ maintenance/gearman/gearmanRefreshLinks.php
@@ -9,17 +9,37 @@ if ( !$args ) {
 	$args = array( 'localhost' );
 }
 $client = new Net_Gearman_Client( $args );
+$batchSize = 1000;
 
 $dbr = wfGetDB( DB_SLAVE );
-$res = $dbr->select( 'page', array( 'page_namespace', 'page_title' ), false, __METHOD__ );
-foreach ( $res as $row ) {
-	$title = Title::makeTitle( $row->page_namespace, $row->page_title );
-	$params = array(
-		'wiki' => wfWikiID(),
-		'title' => $title->getPrefixedDBkey(),
-		'command' => 'refreshLinks',
-		'params' => false,
+$startId = 0;
+$endId = $dbr->selectField( 'page', 'MAX(page_id)', false, __METHOD__ );
+while ( true ) {
+	$res = $dbr->select( 
+		'page', 
+		array( 'page_namespace', 'page_title', 'page_id' ),
+		array( 'page_id > ' . intval( $startId ) ), 
+		__METHOD__,
+		array( 'LIMIT' => $batchSize ) 
 	);
-	$client->mw_job( $params );
+
+	if ( $res->numRows() == 0 ) {
+		break;
+	}
+	$set = new Net_Gearman_Set;
+	foreach ( $res as $row ) {
+		$startId = $row->page_id;
+		$title = Title::makeTitle( $row->page_namespace, $row->page_title );
+		$params = array(
+			'wiki' => wfWikiID(),
+			'title' => $title->getPrefixedDBkey(),
+			'command' => 'refreshLinks',
+			'params' => false,
+		);
+		$task = new Net_Gearman_Task( 'mw_job', $params );
+		$set->addTask( $task );
+	}
+	$client->runSet( $set );
+	print "$startId / $endId\n";
 }
 
