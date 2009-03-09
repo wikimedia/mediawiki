fb663a7de96443e9abe983bbc2c12b2891bc7805
Roan Kattouw <catrope@users.mediawiki.org>
2009-03-09 10:44:34 +0000

    API: Change Image: to File: in examples

--- includes/api/ApiQueryDuplicateFiles.php
+++ includes/api/ApiQueryDuplicateFiles.php
@@ -147,7 +147,7 @@ class ApiQueryDuplicateFiles extends ApiQueryGeneratorBase {
 	}
 
 	protected function getExamples() {
-		return array (	'api.php?action=query&titles=Image:Albert_Einstein_Head.jpg&prop=duplicatefiles',
+		return array (	'api.php?action=query&titles=File:Albert_Einstein_Head.jpg&prop=duplicatefiles',
 				'api.php?action=query&generator=allimages&prop=duplicatefiles',
 			);
 	}
