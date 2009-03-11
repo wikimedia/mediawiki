48ffee64499e16db04bd0a6dd8fccfdbb6089134
Tim Starling <tstarling@users.mediawiki.org>
2009-03-11 00:29:44 +0000

    Extend log_type/log_action from 10 to 32 instead of 15.

--- maintenance/archives/patch-log_user_text.sql
+++ maintenance/archives/patch-log_user_text.sql
@@ -1,7 +1,7 @@
 ALTER TABLE /*$wgDBprefix*/logging 
 	ADD log_user_text varchar(255) binary NOT NULL default '',
 	ADD log_target_id int unsigned NULL,
-	CHANGE `log_type` `log_type` VARBINARY( 15 ) NOT NULL,
-	CHANGE `log_action` `log_action` VARBINARY( 15 ) NOT NULL;
+	CHANGE log_type log_type varbinary(32) NOT NULL,
+	CHANGE log_action log_action varbinary(32) NOT NULL;
 
 CREATE INDEX /*i*/user_type_time ON /*_*/logging (log_user, log_type, log_timestamp);
