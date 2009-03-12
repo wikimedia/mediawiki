2a6c9a1dfc96d67549d18c6ca1db7155c778b08e
Greg Sabino Mullane <greg@users.mediawiki.org>
2009-03-12 14:35:40 +0000

    Add user_hidden column to mwuser table for Postgres schema.

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -25,7 +25,8 @@ CREATE TABLE mwuser ( -- replace reserved word 'user'
   user_options              TEXT,
   user_touched              TIMESTAMPTZ,
   user_registration         TIMESTAMPTZ,
-  user_editcount            INTEGER
+  user_editcount            INTEGER,
+  user_hidden               SMALLINT NOT NULL DEFAULT 0
 );
 CREATE INDEX user_email_token_idx ON mwuser (user_email_token);
 
