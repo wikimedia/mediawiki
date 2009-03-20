b76473b62688a67b4e638de4adf354782aedd6d1
Brion Vibber <brion@users.mediawiki.org>
2009-03-20 01:15:41 +0000

    Revert r48212 "Add user_hidden field to complement ipb_deleted and gu_hidden" Unauthorized schema change, also appears to be unused.

--- maintenance/archives/patch-user_hidden.sql
+++
@@ -1,3 +0,0 @@
--- Adding user_hidden field for revisiondelete
-ALTER TABLE /*$wgDBprefix*/user
-  ADD user_hidden bool NOT NULL default 0;
--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -126,10 +126,7 @@ CREATE TABLE /*_*/user (
   -- Meant primarily for heuristic checks to give an impression of whether
   -- the account has been used much.
   --
-  user_editcount int,
-  
-   -- Flag for entries hidden from users and Sysops
-  user_hidden bool NOT NULL default 0
+  user_editcount int
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/user_name ON /*_*/user (user_name);
