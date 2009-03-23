cf0148c290809ffa97d2d3cfb8023d477c16b903
Roan Kattouw <catrope@users.mediawiki.org>
2009-03-23 19:58:07 +0000

    API: Add a check for PHP bug 45314 (PHP's WDDX formatter messes up UTF-8) and fix up a similar check for JSON, which couldn't possibly succeed due to missing quotes. See also http://lists.wikimedia.org/pipermail/mediawiki-api/2009-March/001039.html

--- includes/api/ApiFormatWddx.php
+++ includes/api/ApiFormatWddx.php
@@ -42,7 +42,13 @@ class ApiFormatWddx extends ApiFormatBase {
 	}
 
 	public function execute() {
-		if (function_exists('wddx_serialize_value') && !$this->getIsHtml()) {
+		// Some versions of PHP have a broken wddx_serialize_value, see
+		// PHP bug 45314. Test encoding an affected character (U+00A0)
+		// to avoid this.
+		$expected = "<wddxPacket version='1.0'><header/><data><string>\xc2\xa0</string></data></wddxPacket>";
+		if (function_exists('wddx_serialize_value')
+				&& !$this->getIsHtml()
+				&& wddx_serialize_value("\xc2\xa0") != $expected) {
 			$this->printText(wddx_serialize_value($this->getResultData()));
 		} else {
 			// Don't do newlines and indentation if we weren't asked
@@ -60,8 +66,8 @@ class ApiFormatWddx extends ApiFormatBase {
 	}
 
 	/**
-	* Recursivelly go through the object and output its data in WDDX format.
-	*/
+	 * Recursively go through the object and output its data in WDDX format.
+	 */
 	function slowWddxPrinter($elemValue, $indent = 0) {
 		$indstr = ($this->getIsHtml() ? "" : str_repeat(' ', $indent));
 		$indstr2 = ($this->getIsHtml() ? "" : str_repeat(' ', $indent + 2));
