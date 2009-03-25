9ecc912a9e3f9d9dda3a1988fb5334ca0ffbd009
Raimond Spekking <raymond@users.mediawiki.org>
2009-03-25 08:09:08 +0000

    Fix per comment in r48720: Remove style from code and use same table stylings as other tables.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1469,7 +1469,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '206';
+$wgStyleVersion = '207';
 
 
 # Server-side caching:
