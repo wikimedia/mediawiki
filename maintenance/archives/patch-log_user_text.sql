180e2ce0177f7955a14cbfca827c04b9fa44325f
Aaron Schulz <aaron@users.mediawiki.org>
2009-04-11 23:46:12 +0000

    log_target_id -> log_page. Indexed to allow better log tracking wrt page moves.

--- maintenance/archives/patch-log_user_text.sql
+++ maintenance/archives/patch-log_user_text.sql
@@ -1,7 +1,8 @@
 ALTER TABLE /*$wgDBprefix*/logging 
 	ADD log_user_text varchar(255) binary NOT NULL default '',
-	ADD log_target_id int unsigned NULL,
+	ADD log_page int unsigned NULL,
 	CHANGE log_type log_type varbinary(32) NOT NULL,
 	CHANGE log_action log_action varbinary(32) NOT NULL;
 
 CREATE INDEX /*i*/user_type_time ON /*_*/logging (log_user, log_type, log_timestamp);
+CREATE INDEX /*i*/page_type ON /*_*/logging (log_page,log_timestamp);
