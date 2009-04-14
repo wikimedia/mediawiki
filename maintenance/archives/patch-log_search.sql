5d0cec5cbdaa04d1cdeb0c62544752e5aac29aa7
Aaron Schulz <aaron@users.mediawiki.org>
2009-04-14 15:38:51 +0000

    Follow up to r49454: redid indexing

--- maintenance/archives/patch-log_search.sql
+++ maintenance/archives/patch-log_search.sql
@@ -4,6 +4,7 @@ CREATE TABLE /*_*/log_search (
 	-- The value of the ID
 	ls_value varchar(255) NOT NULL,
 	-- Key to log_id
-	ls_log_id int unsigned NOT NULL default 0
+	ls_log_id int unsigned NOT NULL default 0,
+	PRIMARY KEY (ls_field,ls_value,ls_log_id)
 );
-CREATE INDEX /*i*/log_field_value ON /*_*/log_search (ls_field,ls_value,ls_log_id);
+CREATE INDEX /*i*/ls_log_id ON /*_*/log_search (ls_log_id);
