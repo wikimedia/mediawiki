a2eb09f724ddb979a50bcafd058a822fd9fa6e15
Aaron Schulz <aaron@users.mediawiki.org>
2009-04-14 04:54:54 +0000

    Tweak r49454: remove duplication

--- maintenance/archives/patch-log_search.sql
+++ maintenance/archives/patch-log_search.sql
@@ -1,9 +1,5 @@
 CREATE TABLE /*_*/log_search (
-	-- Key to log_type
-	ls_log_type varbinary(32) NOT NULL,
-	-- Key to log_action
-	ls_log_action varbinary(32) NOT NULL,
-	-- The type of ID (rev ID, timestamp, other)
+	-- The type of ID (rev ID, log ID, timestamp, other)
 	ls_field varbinary(32) NOT NULL,
 	-- The value of the ID
 	ls_value varchar(255) NOT NULL,
