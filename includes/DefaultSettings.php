799911d7e58567ee4856cc0cdf53fcffc394d16b
Aaron Schulz <aaron@users.mediawiki.org>
2009-04-16 04:51:43 +0000

    Tweaked r49408: Improved hiding and changed 1 revision case handling

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1477,7 +1477,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches don't keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '211';
+$wgStyleVersion = '212';
 
 
 # Server-side caching:
