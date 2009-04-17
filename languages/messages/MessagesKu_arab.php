c580250f6edbdd3f0efa3f5a0913f779e9ccb887
Raimond Spekking <raymond@users.mediawiki.org>
2009-04-17 10:54:51 +0000

    Follow up r49596: Add fallback for ku-arab

---
+++ languages/messages/MessagesKu_arab.php
@@ -0,0 +1,28 @@
+<?php
+/** Kurdish (Arabic) (كوردي (عەرەبی))
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'ckb-arab';
+
+$digitTransformTable = array(
+	'0' => '٠', # &#x0660;
+	'1' => '١', # &#x0661;
+	'2' => '٢', # &#x0662;
+	'3' => '٣', # &#x0663;
+	'4' => '٤', # &#x0664;
+	'5' => '٥', # &#x0665;
+	'6' => '٦', # &#x0666;
+	'7' => '٧', # &#x0667;
+	'8' => '٨', # &#x0668;
+	'9' => '٩', # &#x0669;
+	'.' => '٫', # &#x066b; wrong table ?
+	',' => '٬', # &#x066c;
+);
+
