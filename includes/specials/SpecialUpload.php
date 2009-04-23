c8dc7c875f225294f9e9fdf24242b65dc196a146
Tim Starling <tstarling@users.mediawiki.org>
2009-04-23 14:38:03 +0000

    Fixed XSS.

--- includes/specials/SpecialUpload.php
+++ includes/specials/SpecialUpload.php
@@ -1166,7 +1166,7 @@ wgUploadAutoFill = {$autofill};
 		else {
 			$wgOut->addHTML(
 				"<input tabindex='2' type='text' name='wpDestFile' id='wpDestFile' size='60'
-						value='{$encDestName}' onchange='toggleFilenameFiller()' $destOnkeyup />"
+						value=\"{$encDestName}\" onchange='toggleFilenameFiller()' $destOnkeyup />"
 			);
 		}
 		
