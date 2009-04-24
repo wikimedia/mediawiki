ba1fef9b55c64528f4f7f5a74f9802e614ca3af3
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2009-04-24 07:50:45 +0000

    and... *Removed trailing whitespaces
    (forgot to save the file before commiting :D)

--- includes/specials/SpecialPreferences.php
+++ includes/specials/SpecialPreferences.php
@@ -4,26 +4,26 @@ class SpecialPreferences extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'Preferences' );
 	}
-	
+
 	function execute( $par ) {
 		global $wgOut, $wgUser, $wgRequest;
-		
+
 		$this->setHeaders();
 		$this->outputHeader();
 
 		$wgOut->addScriptFile( 'prefs.js' );
 
 		$wgOut->disallowUserJs();  # Prevent hijacked user scripts from sniffing passwords etc.
-		
+
 		if ( $wgUser->isAnon() ) {
-			$wgOut->showErrorPage( 'prefsnologin', 'prefsnologintext', array($this->getTitle()->getPrefixedDBkey()) );
+			$wgOut->showErrorPage( 'prefsnologin', 'prefsnologintext', array( $this->getTitle()->getPrefixedDBkey() ) );
 			return;
 		}
 		if ( wfReadOnly() ) {
 			$wgOut->readOnlyPage();
 			return;
 		}
-		
+
 		if ( $par == 'reset' ) {
 			$this->showResetForm();
 			return;
@@ -35,35 +35,35 @@ class SpecialPreferences extends SpecialPage {
 				'savedprefs'
 			);
 		}
-		
+
 		$htmlForm = Preferences::getFormObject( $wgUser );
 
 		$htmlForm->show();
 	}
-	
+
 	function showResetForm() {
 		global $wgOut;
-		
+
 		$wgOut->addWikiMsg( 'prefs-reset-intro' );
-		
+
 		$htmlForm = new HTMLForm( array(), 'prefs-restore' );
-		
+
 		$htmlForm->setSubmitText( wfMsg( 'restoreprefs' ) );
-		$htmlForm->setTitle( $this->getTitle('reset') );
+		$htmlForm->setTitle( $this->getTitle( 'reset' ) );
 		$htmlForm->setSubmitCallback( array( __CLASS__, 'submitReset' ) );
 		$htmlForm->suppressReset();
-		
+
 		$htmlForm->show();
 	}
-	
+
 	static function submitReset( $formData ) {
 		global $wgUser, $wgOut;
 		$wgUser->resetOptions();
-		
+
 		$url = SpecialPage::getTitleFor( 'Preferences' )->getFullURL( 'success' );
-		
+
 		$wgOut->redirect( $url );
-		
+
 		return true;
 	}
 }
