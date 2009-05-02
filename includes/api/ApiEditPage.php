cae958bf12b230d907b22c93a8b3cfa05c58bd53
Roan Kattouw <catrope@users.mediawiki.org>
2009-05-02 14:47:26 +0000

    API: Set $wgTitle to a dummy title in api.php, and introduce defined('API') as a check for API mode. This should fix errors about $wgTitle being null

--- includes/api/ApiEditPage.php
+++ includes/api/ApiEditPage.php
@@ -58,6 +58,9 @@ class ApiEditPage extends ApiBase {
 		$titleObj = Title::newFromText($params['title']);
 		if(!$titleObj)
 			$this->dieUsageMsg(array('invalidtitle', $params['title']));
+		// Some functions depend on $wgTitle == $ep->mTitle
+		global $wgTitle;
+		$titleObj = $wgTitle;
 
 		if($params['createonly'] && $titleObj->exists())
 			$this->dieUsageMsg(array('createonly-exists'));
@@ -197,10 +200,6 @@ class ApiEditPage extends ApiBase {
 		# Do the actual save
 		$oldRevId = $articleObj->getRevIdFetched();
 		$result = null;
-		# *Something* is setting $wgTitle to a title corresponding to "Msg",
-		# but that breaks API mode detection through is_null($wgTitle)
-		global $wgTitle;
-		$wgTitle = null;
 		# Fake $wgRequest for some hooks inside EditPage
 		# FIXME: This interface SUCKS
 		$oldRequest = $wgRequest;
