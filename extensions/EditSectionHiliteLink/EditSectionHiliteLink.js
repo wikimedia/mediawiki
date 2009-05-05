1bf5285304970d0d9ae6a28a3c189d1388999b1d
Arash Boostani <aboostani@users.mediawiki.org>
2009-05-05 23:16:58 +0000

    Inadvertently committed an extension to the extension directory, so I am now removing those files

--- extensions/EditSectionHiliteLink/EditSectionHiliteLink.js
+++
@@ -1,9 +0,0 @@
-/* JavaScript for Drafts extension */
-
-function editSectionHiliteOn (sectionID) {
-	document.getElementById(sectionID).className = 'edit_section_hilite';
-}
-
-function editSectionHiliteOff (sectionID) {
-	document.getElementById(sectionID).className = '';
-}
