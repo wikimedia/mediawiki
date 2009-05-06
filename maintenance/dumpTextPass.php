c251c4804828ac80849d202ce0bd8bdbcb274133
Brion Vibber <brion@users.mediawiki.org>
2009-05-06 23:47:31 +0000

    Fix a silly bug that isn't the mystery dump bug we were looking for, but could result in infinite loop during text pass if it fails just at the right time.

--- maintenance/dumpTextPass.php
+++ maintenance/dumpTextPass.php
@@ -408,7 +408,7 @@ class TextPassDumper extends BackupDumper {
 		// Subprocess may not send everything at once, we have to loop.
 		while( $nbytes > strlen( $text ) ) {
 			$buffer = fread( $this->spawnRead, $nbytes - strlen( $text ) );
-			if( $text === false ) break;
+			if( $buffer === false ) break;
 			$text .= $buffer;
 		}
 		
