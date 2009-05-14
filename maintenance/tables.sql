d0a3dc38a1c346e1ff25400caf17da5912b000c3
Brion Vibber <brion@users.mediawiki.org>
2009-05-14 22:43:57 +0000

    Cleanup for r50567: * Include DB table options on log_search table creation * Adjust tabs->spaces in table def... sounds silly, but the tabs sometimes confuse things when cut-and-pasting into mysql command line. :)

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -1125,14 +1125,14 @@ CREATE INDEX /*i*/times ON /*_*/logging (log_timestamp);
 
 
 CREATE TABLE /*_*/log_search (
-	-- The type of ID (rev ID, log ID, rev timestamp, username)
-	ls_field varbinary(32) NOT NULL,
-	-- The value of the ID
-	ls_value varchar(255) NOT NULL,
-	-- Key to log_id
-	ls_log_id int unsigned NOT NULL default 0,
-	PRIMARY KEY (ls_field,ls_value,ls_log_id)
-);
+  -- The type of ID (rev ID, log ID, rev timestamp, username)
+  ls_field varbinary(32) NOT NULL,
+  -- The value of the ID
+  ls_value varchar(255) NOT NULL,
+  -- Key to log_id
+  ls_log_id int unsigned NOT NULL default 0,
+  PRIMARY KEY (ls_field,ls_value,ls_log_id)
+) /*$wgDBTableOptions*/;
 CREATE INDEX /*i*/ls_log_id ON /*_*/log_search (ls_log_id);
 
 
