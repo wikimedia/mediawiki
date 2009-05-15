908c43105feb5b2f0ad8ad1f09bb5610baefc306
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2009-05-15 11:34:18 +0000

    Follow-up r50567: also update PostgreSQL and SQLite schema

---
+++ maintenance/postgres/archives/patch-log_search.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE log_search (
+  ls_field TEXT NOT NULL,
+  ls_value TEXT NOT NULL,
+  ls_log_id INTEGER NOT NULL DEFAULT 0
+);
+
+ALTER TABLE log_search ADD CONSTRAINT log_search_pk PRIMARY KEY(ls_field, ls_value, ls_log_id);
+CREATE INDEX ls_log_id ON log_search (ls_log_id);
