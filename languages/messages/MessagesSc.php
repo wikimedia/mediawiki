786928e3fb77096fa8a5a5e8b96f5546fcc3e783
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-05-17 10:23:04 +0000

    Localisation updates from translatewiki.net * special pages aliases, magic words, namespace names and namespace aliases

--- languages/messages/MessagesSc.php
+++ languages/messages/MessagesSc.php
@@ -13,15 +13,29 @@
  */
 
 $namespaceNames = array(
-	NS_SPECIAL         => 'Speciale',
-	NS_MAIN            => '',
-	NS_TALK            => 'Contièndha',
-	NS_USER            => 'Utente',
-	NS_USER_TALK       => 'Utente_discussioni',
-	# NS_PROJECT set by $wgMetaNamespace
-	NS_PROJECT_TALK    => '$1_discussioni',
-	NS_FILE            => 'Immàgini',
-	NS_FILE_TALK       => 'Immàgini_contièndha'
+	NS_SPECIAL          => 'Ispetziale',
+	NS_TALK             => 'Cuntierra',
+	NS_USER             => 'Usuàriu',
+	NS_USER_TALK        => 'Cuntierra_usuàriu',
+	NS_PROJECT_TALK     => 'Cuntierra_$1',
+	NS_FILE             => 'File',
+	NS_FILE_TALK        => 'Cuntierra_file',
+	NS_MEDIAWIKI_TALK   => 'Cuntierra_MediaWiki',
+	NS_TEMPLATE_TALK    => 'Cuntierra_template',
+	NS_HELP             => 'Agiudu',
+	NS_HELP_TALK        => 'Cuntierra_agiudu',
+	NS_CATEGORY         => 'Categoria',
+	NS_CATEGORY_TALK    => 'Cuntierra_categoria',
+);
+
+$namespaceAliases = array(
+	'Speciale'            => NS_SPECIAL,
+	'Contièndha'          => NS_TALK,
+	'Utente'              => NS_USER,
+	'Utente_discussioni'  => NS_USER_TALK,
+	'$1_discussioni'      => NS_PROJECT_TALK,
+	'Immàgini'            => NS_FILE,
+	'Immàgini_contièndha' => NS_FILE_TALK
 );
 
 $dateFormats = array(
