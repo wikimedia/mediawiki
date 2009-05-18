b97f829ce6c3070dbace11ccd27a35e51df8b7c3
Aaron Schulz <aaron@users.mediawiki.org>
2009-05-18 22:15:12 +0000

    Fixed bug in r49227; use raw function now

--- includes/Linker.php
+++ includes/Linker.php
@@ -1384,7 +1384,7 @@ class Linker {
 	 * @return string HTML
 	 */
 	function revComment( Revision $rev, $local = false, $isPublic = false ) {
-		if( $rev->getComment() == "" ) return "";
+		if( $rev->getRawComment() == "" ) return "";
 		if( $rev->isDeleted( Revision::DELETED_COMMENT ) && $isPublic ) {
 			$block = " <span class=\"comment\">" . wfMsgHtml( 'rev-deleted-comment' ) . "</span>";
 		} else if( $rev->userCan( Revision::DELETED_COMMENT ) ) {
