2c9926dd12b0a162b6dac3ce635755eac2cac63f
Shinjiman <shinjiman@users.mediawiki.org>
2009-05-26 13:40:13 +0000

    Cleanup: removing obsolete/outdated messages for zh-tw/zh-hk which are same as zh-hant and zh-cn/zh-sg which are same as zh-hans respectively.

--- languages/messages/MessagesZh_hk.php
+++ languages/messages/MessagesZh_hk.php
@@ -18,48 +18,6 @@ $fallback8bitEncoding = 'Big5-HKSCS';
 $messages = array(
 # User preference toggles
 'tog-watchlisthidebots' => '監視列表中隱藏機械人的編輯',
-'tog-norollbackdiff'    => '進行回退後略過差異比較',
-
-# Dates
-'sunday'        => '星期日',
-'monday'        => '星期一',
-'tuesday'       => '星期二',
-'wednesday'     => '星期三',
-'thursday'      => '星期四',
-'friday'        => '星期五',
-'saturday'      => '星期六',
-'sun'           => '日',
-'mon'           => '一',
-'tue'           => '二',
-'wed'           => '三',
-'thu'           => '四',
-'fri'           => '五',
-'sat'           => '六',
-'january'       => '1月',
-'february'      => '2月',
-'march'         => '3月',
-'april'         => '4月',
-'may_long'      => '5月',
-'june'          => '6月',
-'july'          => '7月',
-'august'        => '8月',
-'september'     => '9月',
-'october'       => '10月',
-'november'      => '11月',
-'december'      => '12月',
-'january-gen'   => '一月',
-'february-gen'  => '二月',
-'march-gen'     => '三月',
-'april-gen'     => '四月',
-'may-gen'       => '五月',
-'june-gen'      => '六月',
-'july-gen'      => '七月',
-'august-gen'    => '八月',
-'september-gen' => '九月',
-'october-gen'   => '十月',
-'november-gen'  => '十一月',
-'december-gen'  => '十二月',
-'jan'           => '1月',
 
 'printableversion' => '可打印版',
 'print'            => '打印',
@@ -77,9 +35,6 @@ $messages = array(
 
 # Recent changes
 'rcshowhidebots' => '$1機械人的編輯',
-
-# Move page
-'move-redirect-suppressed' => '已禁止重新定向',
 
 # Tooltip help for the actions
 'tooltip-t-print' => '這個頁面的可打印版本',
