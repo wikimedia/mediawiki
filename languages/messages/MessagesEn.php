e3deb77c3f929739962e0f749784c3c507685b3b
Robert Stojnić <rainman@users.mediawiki.org>
2009-05-31 13:31:46 +0000

    Remove trailing space in powersearch-togglelabel

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1490,7 +1490,7 @@ Try prefixing your query with ''all:'' to search all content (including talk pag
 'powersearch-ns'                   => 'Search in namespaces:',
 'powersearch-redir'                => 'List redirects',
 'powersearch-field'                => 'Search for',
-'powersearch-togglelabel'          => 'Check: ',
+'powersearch-togglelabel'          => 'Check:',
 'powersearch-toggleall'            => 'All', 
 'powersearch-togglenone'           => 'None',
 'search-external'                  => 'External search',
