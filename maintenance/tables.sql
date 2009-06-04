7aadbb783bb197bb0fe3e59aa346a6c53d8db304
Aaron Schulz <aaron@users.mediawiki.org>
2009-06-04 12:57:22 +0000

    Tweaked r49468 - sqllite fix

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -1130,9 +1130,9 @@ CREATE TABLE /*_*/log_search (
   -- The value of the ID
   ls_value varchar(255) NOT NULL,
   -- Key to log_id
-  ls_log_id int unsigned NOT NULL default 0,
-  PRIMARY KEY (ls_field,ls_value,ls_log_id)
+  ls_log_id int unsigned NOT NULL default 0
 ) /*$wgDBTableOptions*/;
+CREATE UNIQUE INDEX /*i*/ls_field_val ON /*_*/log_search (ls_field,ls_value,ls_log_id);
 CREATE INDEX /*i*/ls_log_id ON /*_*/log_search (ls_log_id);
 
 
