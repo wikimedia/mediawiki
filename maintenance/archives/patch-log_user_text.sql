721b14ab5677498ff89bf00c5d728a1bd9801494
Tim Starling <tstarling@users.mediawiki.org>
2009-06-08 06:11:29 +0000

    Renamed indexes, new indexes must be prefixed to avoid conflicts in SQLite's global index namespace. These indexes are not in tables.sql or any USE INDEX yet.

--- maintenance/archives/patch-log_user_text.sql
+++ maintenance/archives/patch-log_user_text.sql
@@ -4,5 +4,5 @@ ALTER TABLE /*$wgDBprefix*/logging
 	CHANGE log_type log_type varbinary(32) NOT NULL,
 	CHANGE log_action log_action varbinary(32) NOT NULL;
 
-CREATE INDEX /*i*/user_type_time ON /*_*/logging (log_user, log_type, log_timestamp);
-CREATE INDEX /*i*/page_id_time ON /*_*/logging (log_page,log_timestamp);
+CREATE INDEX /*i*/log_user_type_time ON /*_*/logging (log_user, log_type, log_timestamp);
+CREATE INDEX /*i*/log_page_id_time ON /*_*/logging (log_page,log_timestamp);
