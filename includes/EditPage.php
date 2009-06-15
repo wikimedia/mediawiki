7e031c418cb64e035275c71dd90fb1499794b9b7
Roan Kattouw <catrope@users.mediawiki.org>
2009-06-15 13:50:05 +0000

    Fix up r51559: Linker::link() expects Title objects, not strings

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -1288,7 +1288,7 @@ class EditPage {
 		$subject = wfMsgExt( 'subject', 'parseinline' );
 
 		$cancel = $sk->link(
-			$wgTitle->getPrefixedText(),
+			$wgTitle,
 			wfMsgExt( 'cancel', array( 'parseinline' ) ),
 			array(),
 			array(),
