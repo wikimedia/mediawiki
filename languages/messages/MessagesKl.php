370a701d58393fbebd85dd8323f8571598281522
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-06-21 23:08:40 +0000

    Localisation updates for namespaces from translatewiki.net

--- languages/messages/MessagesKl.php
+++ languages/messages/MessagesKl.php
@@ -14,6 +14,44 @@
 
 $fallback = 'da';
 
+$namespaceNames = array(
+	NS_SPECIAL          => 'Immikkut',
+	NS_TALK             => 'Oqallinneq',
+	NS_USER             => 'Atuisoq',
+	NS_USER_TALK        => 'Atuisup oqalliffia',
+	NS_PROJECT_TALK     => '$1ip oqalliffia',
+	NS_FILE             => 'Fiileq',
+	NS_FILE_TALK        => 'Fiilip oqalliffia',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Mediawikip oqalliffia',
+	NS_TEMPLATE         => 'Ilisserut',
+	NS_TEMPLATE_TALK    => 'Ilisserummi oqallinneq',
+	NS_HELP             => 'Ikiuutit',
+	NS_HELP_TALK        => 'Ikiuutini oqallinneq',
+	NS_CATEGORY         => 'Sumut atassuseq',
+	NS_CATEGORY_TALK    => 'Sumut atassusermi oqallinneq',
+);
+
+$namespaceAliases = array(
+	'Speciel' => NS_SPECIAL,
+	'Diskussion' => NS_TALK,
+	'Bruger' => NS_USER,
+	'Brugerdiskussion' => NS_USER_TALK,
+	'$1-diskussion' => NS_PROJECT_TALK,
+	'Fil' => NS_FILE,
+	'Fildiskussion' => NS_FILE_TALK,
+	'Billede' => NS_FILE,
+	'Billeddiskussion' => NS_FILE_TALK,
+	'MediaWiki' => NS_MEDIAWIKI,
+	'MediaWiki-diskussion' => NS_MEDIAWIKI_TALK,
+	'Skabelon' => NS_TEMPLATE,
+	'Skabelondiskussion' => NS_TEMPLATE_TALK,
+	'Hjælp' => NS_HELP,
+	'Hjælp-diskussion' => NS_HELP_TALK,
+	'Kategori' => NS_CATEGORY,
+	'Kategoridiskussion' => NS_CATEGORY_TALK
+);
+
 $messages = array(
 # Dates
 'sunday'        => 'sapaat',
