b41659de73566a28f560c5617e3e25b9352542a5
Aryeh Gregor <simetrical@users.mediawiki.org>
2009-06-24 22:02:52 +0000

    Remove useless HTML comment

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -88,7 +88,6 @@ class MonoBookTemplate extends QuickTemplate {
 		<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
 
 		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
-		<!-- Head Scripts -->
 <?php $this->html('headscripts') ?>
 <?php	if($this->data['jsvarurl']) { ?>
 		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl') ?>"><!-- site js --></script>
