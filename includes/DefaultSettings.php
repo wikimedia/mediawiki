4a71b9a8106b1ba04c84435f8993f19d175e51b6
Roan Kattouw <catrope@users.mediawiki.org>
2009-06-25 21:10:10 +0000

    Revert r52410 (Put the [edit] link after the section header instead of before, and don't right-float it anymore) for now because of unresolved issues

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1496,7 +1496,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '229';
+$wgStyleVersion = '228';
 
 
 # Server-side caching:
