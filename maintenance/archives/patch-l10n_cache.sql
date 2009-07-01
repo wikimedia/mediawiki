6964e122dd6a5baf08712549c0b76618534a0b7b
Chad Horohoe <demon@users.mediawiki.org>
2009-07-01 00:55:45 +0000

    (bug 19428) PG and MySQL followups to r52503. Patch by OverlordQ

--- maintenance/archives/patch-l10n_cache.sql
+++ maintenance/archives/patch-l10n_cache.sql
@@ -3,6 +3,6 @@ CREATE TABLE /*_*/l10n_cache (
   lc_lang varbinary(32) NOT NULL,
   lc_key varchar(255) NOT NULL,
   lc_value mediumblob NOT NULL
-);
+) /*$wgDBTableOptions*/;
 CREATE INDEX /*i*/lc_lang_key ON /*_*/l10n_cache (lc_lang, lc_key);
 
