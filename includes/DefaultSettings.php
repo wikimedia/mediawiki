29789f0f76ff6b306db1b248eb4f6049c72d2e60
Aryeh Gregor <simetrical@users.mediawiki.org>
2009-07-12 17:55:18 +0000

    Turn off $wgHtml5 by default
    I discovered that the HTML 5 doctype does in fact trigger standards
    mode in all major browsers . . . but the old XHTML 1.0 Transitional
    doctype we were using did *not*.  It triggers "almost standards mode" in
    some browsers.  The difference is causing some rendering issues that
    will have to block deployment for the moment.
    
    See r53034, r53035, r53136.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -875,12 +875,11 @@ $wgDTD				= 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd';
 $wgXhtmlDefaultNamespace	= 'http://www.w3.org/1999/xhtml';
 
 /**
- * Temporary setting to disable HTML 5 for the unlikely event that it causes 
- * everything to blow up.  If all goes well, should be removed (and forced 
- * true) well before it ever makes it into a release.  If set to false, go back 
- * to serving an XHTML 1.0 Transitional doctype (per $wgDocType et al. above).
+ * Should we output an HTML 5 doctype?  This needs to be left off until some
+ * lingering rendering issues are resolved -- apparently it switches some
+ * browsers from "almost standards" mode to standards mode.
  */
-$wgHtml5 = true;
+$wgHtml5 = false;
 
 /**
  * Permit other namespaces in addition to the w3.org default.
