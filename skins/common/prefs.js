2a544840abea9b237a92cc2eee6b36b90b44c908
Andrew Garrett <werdna@users.mediawiki.org>
2009-07-13 20:16:07 +0000

    Remove is_khtml condition from prefs.js -- KHTMLFixes.css has been deleted

--- skins/common/prefs.js
+++ skins/common/prefs.js
@@ -18,7 +18,7 @@ function tabbedprefs() {
 		if (children[i].nodeName.toLowerCase() == 'fieldset') {
 			children[i].id = 'prefsection-' + seci;
 			children[i].className = 'prefsection';
-			if (is_opera || is_khtml) {
+			if (is_opera) {
 				children[i].className = 'prefsection operaprefsection';
 			}
 			var legends = children[i].getElementsByTagName('legend');
