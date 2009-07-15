af01db6f62aaedb6374344a6359a0ac6c411dd84
Chad Horohoe <demon@users.mediawiki.org>
2009-07-15 23:36:16 +0000

    (bug 18708) CSS plainlinks class now available to all skins

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1566,7 +1566,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '232';
+$wgStyleVersion = '233';
 
 
 # Server-side caching:
