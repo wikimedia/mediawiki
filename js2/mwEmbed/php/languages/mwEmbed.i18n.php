bbbb1f6010885b013cf6533cfd512be15a8eaf1c
Michael Dale <dale@users.mediawiki.org>
2009-07-31 02:44:37 +0000

    * fixed IE bug with firefogg disabled loading js2 for upload page * added support for two-pass encoding (firefogg bzr trunk) * added support for setting in and out points

--- js2/mwEmbed/php/languages/mwEmbed.i18n.php
+++ js2/mwEmbed/php/languages/mwEmbed.i18n.php
@@ -10,106 +10,6 @@
 #<JAVASCRIPT EN REPLACE>
 $messages['en'] = array(
 	/*
-	* js file: /libTimedText/mvTextInterface.js
-	*/
-	'select_transcript_set' => 'Select Layers',
-	'auto_scroll' => 'auto scroll',
-	'close' => 'close',
-	'improve_transcript' => 'Improve',
-
-	/*
-	* js file: /libAddMedia/mvBaseUploadInterface.js
-	*/
-	'upload-transcode-in-progress' => 'Doing Transcode & Upload (do not close this window)',
-	'upload-in-progress' => 'Upload in Progress (do not close this window)',
-	'upload-transcoded-status' => 'Transcoded',
-	'uploaded-status' => 'Uploaded',
-	'wgfogg_wrong_version' => 'You have firefogg installed but its outdated, <a href="http://firefogg.org">please upgrade</a> ',
-	'upload-stats-fileprogres' => '$1 of $2',
-	'mv_upload_completed' => 'Your upload is complete',
-	'mv_upload_done' => 'Your upload <i>should be</i> accessible <a href="$1">here</a>',
-	'upload-unknown-size' => 'Unknown size',
-	'mv-cancel-confim' => 'Are you sure you want to cancel?',
-	'successfulupload' => 'Successful Upload',
-	'uploaderror' => 'Upload error',
-	'uploadwarning' => 'Upload warning',
-	'unknown-error' => 'Unknown Error',
-	'return-to-form' => 'Return to form',
-	'file-exists-duplicate' => 'This file is a duplicate of the following file',
-	'fileexists' => 'A file with this name exists already, please check <b><tt>$1</tt></b> if you are not sure if you want to change it.',
-	'fileexists-thumb' => '<center><b>Existing file</b></center>',
-	'ignorewarning' => 'Ignore warning and save file anyway',
-	'file-thumbnail-no' => 'The filename begins with <b><tt>$1</tt></b>',
-	'go-to-resource' => 'Go to Resource Page',
-	'wgfogg_waring_bad_extension' => 'You have selected a file with an unsuported extension (<a href="http://commons.wikimedia.org/wiki/Commons:Firefogg#Supported_File_Types">more information</a>).',
-	'cancel-button' => 'Cancel',
-	'ok-button' => 'OK',
-
-	/*
-	* js file: /libAddMedia/mvAdvFirefogg.js
-	*/
-	'help-sticky' => 'Help (Click to Keep Help on Screen)',
-	'fogg-cg-preset' => 'Preset: <strong>$1</strong>',
-	'fogg-cg-quality' => 'Basic Quality and Resolution Control',
-	'fogg-cg-meta' => 'Meta Data for the Clip',
-	'fogg-cg-advVideo' => 'Advanced Video Encoding Controls',
-	'fogg-cg-advAudio' => 'Advanced Audio Encoding Controls',
-	'fogg-preset-custom' => 'Custom Settings',
-
-	/*
-	* js file: /libAddMedia/searchLibs/metavidSearch.js
-	*/
-	'mv_stream_title' => '$1 $2 to $3',
-
-	/*
-	* js file: /libAddMedia/searchLibs/baseRemoteSearch.js
-	*/
-	'imported_from' => '$1 imported from [$2 $3]. See the original [$4 resource page] for more info',
-
-	/*
-	* js file: /libAddMedia/mvFirefogg.js
-	*/
-	'fogg-select_file' => 'Select File',
-	'fogg-select_new_file' => 'Select New File',
-	'fogg-save_local_file' => 'Save Ogg',
-	'fogg-check_for_fogg' => 'Checking for Firefogg <blink>...</blink>',
-	'fogg-installed' => 'Firefogg is Installed',
-	'fogg-for_improved_uplods' => 'For Improved uploads: ',
-	'fogg-please_install' => '<a href="$1">Install Firefogg</a>. More <a href="http://commons.wikimedia.org/wiki/Commons:Firefogg">about firefogg</a>',
-	'fogg-use_latest_fox' => 'Please first install <a href="http://www.mozilla.com/en-US/firefox/upgrade.html?from=firefogg">Firefox 3.5</a>. <i>then revisit this page to install the <b>firefogg</b> extention</i>',
-	'fogg-passthrough_mode' => 'Your selected file is already ogg or not a video file',
-	'fogg-transcoding' => 'Encoding Video to Ogg',
-	'fogg-encoding-done' => 'Encoding Done',
-
-	/*
-	* js file: /libAddMedia/remoteSearchDriver.js
-	*/
-	'add_media_wizard' => 'Add Media Wizard',
-	'mv_media_search' => 'Media Search',
-	'rsd_box_layout' => 'Box layout',
-	'rsd_list_layout' => 'List Layout',
-	'rsd_results_desc' => 'Results ',
-	'rsd_results_next' => 'next ',
-	'rsd_results_prev' => 'previous ',
-	'rsd_no_results' => 'No search results for <b>$1</b>',
-	'mv_upload' => 'Upload',
-	'rsd_layout' => 'Layout:',
-	'rsd_resource_edit' => 'Edit Resource: $1',
-	'resource_description_page' => 'Resource Description Page',
-	'rsd_local_resource_title' => 'Local Resource Title',
-	'rsd_do_insert' => 'Do Insert',
-	'cc_title' => 'Creative Commons',
-	'cc_by_title' => 'Attribution',
-	'cc_nc_title' => 'Noncommercial',
-	'cc_nd_title' => 'No Derivative Works',
-	'cc_sa_title' => 'Share Alike',
-	'cc_pd_title' => 'Public Domain',
-	'unknown_license' => 'Unknown License',
-	'no_import_by_url' => 'This User or Wiki <b>can not</b> import assets from remote URLs. </p><p> Do you need to Login? </p><p> If permissions are set you may have to enable $wgAllowCopyUploads, <a href="http://www.mediawiki.org/wiki/Manual:$wgAllowCopyUploads">more info</a></p>',
-	'results_from' => 'Results from <a href="$1" target="_new" >$2</a>',
-	'missing_desc_see_soruce' => 'This Asset is missing a description. Please see the [$1 orginal source] and help describe it',
-
-	/*
 	* js file: /libSequencer/mvSequencer.js
 	*/
 	'menu_clipedit' => 'Edit Media',
@@ -158,6 +58,134 @@ $messages['en'] = array(
 	'edit_transout' => 'Edit Transition Out of Clip',
 
 	/*
+	* js file: /libClipEdit/mvClipEdit.js
+	*/
+	'mv_crop' => 'Crop Image',
+	'mv_apply_crop' => 'Apply Crop to Image',
+	'mv_reset_crop' => 'Rest Crop',
+	'mv_insert_image_page' => 'Insert Into page',
+	'mv_insert_into_sequence' => 'Insert Into Sequence',
+	'mv_preview_insert' => 'Preview Insert',
+	'mv_cancel_image_insert' => 'Cancel Insert',
+	'sc_fileopts' => 'Clip Detail Edit',
+	'sc_inoutpoints' => 'Set In-Out points',
+	'sc_overlays' => 'Overlays',
+	'sc_audio' => 'Audio Control',
+	'sc_duration' => 'Duration',
+	'mv_template_properties' => 'Template Properties',
+	'mv_custom_title' => 'Custom Title',
+	'mv_edit_properties' => 'Edit Properties',
+	'mv_other_properties' => 'Other Properties',
+	'mv_resource_page' => 'Resource Page:',
+	'mv_set_in_out_points' => 'Set in-out points',
+	'mv_start_time' => 'Start Time',
+	'mv_end_time' => 'End Time',
+	'mv_preview_inout' => 'Preview/Play In-out points',
+
+	/*
+	* js file: /libAddMedia/searchLibs/metavidSearch.js
+	*/
+	'mv_stream_title' => '$1 $2 to $3',
+
+	/*
+	* js file: /libAddMedia/searchLibs/baseRemoteSearch.js
+	*/
+	'imported_from' => '$1 imported from [$2 $3]. See the original [$4 resource page] for more info',
+
+	/*
+	* js file: /libAddMedia/remoteSearchDriver.js
+	*/
+	'add_media_wizard' => 'Add Media Wizard',
+	'mv_media_search' => 'Media Search',
+	'rsd_box_layout' => 'Box layout',
+	'rsd_list_layout' => 'List Layout',
+	'rsd_results_desc' => 'Results ',
+	'rsd_results_next' => 'next ',
+	'rsd_results_prev' => 'previous ',
+	'rsd_no_results' => 'No search results for <b>$1</b>',
+	'mv_upload' => 'Upload',
+	'rsd_layout' => 'Layout:',
+	'rsd_resource_edit' => 'Edit Resource: $1',
+	'resource_description_page' => 'Resource Description Page',
+	'rsd_local_resource_title' => 'Local Resource Title',
+	'rsd_do_insert' => 'Do Insert',
+	'cc_title' => 'Creative Commons',
+	'cc_by_title' => 'Attribution',
+	'cc_nc_title' => 'Noncommercial',
+	'cc_nd_title' => 'No Derivative Works',
+	'cc_sa_title' => 'Share Alike',
+	'cc_pd_title' => 'Public Domain',
+	'unknown_license' => 'Unknown License',
+	'no_import_by_url' => 'This User or Wiki <b>can not</b> import assets from remote URLs. </p><p> Do you need to Login? </p><p> If permissions are set you may have to enable $wgAllowCopyUploads, <a href="http://www.mediawiki.org/wiki/Manual:$wgAllowCopyUploads">more info</a></p>',
+	'results_from' => 'Results from <a href="$1" target="_new" >$2</a>',
+	'missing_desc_see_soruce' => 'This Asset is missing a description. Please see the [$1 orginal source] and help describe it',
+
+	/*
+	* js file: /libAddMedia/mvFirefogg.js
+	*/
+	'fogg-select_file' => 'Select File',
+	'fogg-select_new_file' => 'Select New File',
+	'fogg-save_local_file' => 'Save Ogg',
+	'fogg-check_for_fogg' => 'Checking for Firefogg <blink>...</blink>',
+	'fogg-installed' => 'Firefogg is Installed',
+	'fogg-for_improved_uplods' => 'For Improved uploads: ',
+	'fogg-please_install' => '<a href="$1">Install Firefogg</a>. More <a href="http://commons.wikimedia.org/wiki/Commons:Firefogg">about firefogg</a>',
+	'fogg-use_latest_fox' => 'Please first install <a href="http://www.mozilla.com/en-US/firefox/upgrade.html?from=firefogg">Firefox 3.5</a> (or later). <i>then revisit this page to install the <b>firefogg</b> extention</i>',
+	'fogg-passthrough_mode' => 'Your selected file is already ogg or not a video file',
+	'fogg-transcoding' => 'Encoding Video to Ogg',
+	'fogg-encoding-done' => 'Encoding Done',
+
+	/*
+	* js file: /libAddMedia/mvBaseUploadInterface.js
+	*/
+	'upload-transcode-in-progress' => 'Doing Transcode & Upload (do not close this window)',
+	'upload-in-progress' => 'Upload in Progress (do not close this window)',
+	'upload-transcoded-status' => 'Transcoded',
+	'uploaded-status' => 'Uploaded',
+	'wgfogg_wrong_version' => 'You have firefogg installed but its outdated, <a href="http://firefogg.org">please upgrade</a> ',
+	'upload-stats-fileprogres' => '$1 of $2',
+	'mv_upload_completed' => 'Your upload is complete',
+	'mv_upload_done' => 'Your upload <i>should be</i> accessible <a href="$1">here</a>',
+	'upload-unknown-size' => 'Unknown size',
+	'mv-cancel-confim' => 'Are you sure you want to cancel?',
+	'successfulupload' => 'Successful Upload',
+	'uploaderror' => 'Upload error',
+	'uploadwarning' => 'Upload warning',
+	'unknown-error' => 'Unknown Error',
+	'return-to-form' => 'Return to form',
+	'file-exists-duplicate' => 'This file is a duplicate of the following file',
+	'fileexists' => 'A file with this name exists already, please check <b><tt>$1</tt></b> if you are not sure if you want to change it.',
+	'fileexists-thumb' => '<center><b>Existing file</b></center>',
+	'ignorewarning' => 'Ignore warning and save file anyway',
+	'file-thumbnail-no' => 'The filename begins with <b><tt>$1</tt></b>',
+	'go-to-resource' => 'Go to Resource Page',
+	'wgfogg_waring_bad_extension' => 'You have selected a file with an unsuported extension (<a href="http://commons.wikimedia.org/wiki/Commons:Firefogg#Supported_File_Types">more information</a>).',
+	'cancel-button' => 'Cancel',
+	'ok-button' => 'OK',
+
+	/*
+	* js file: /libAddMedia/mvAdvFirefogg.js
+	*/
+	'help-sticky' => 'Help (Click to Stick)',
+	'fogg-cg-preset' => 'Preset: <strong>$1</strong>',
+	'fogg-cg-quality' => 'Basic Quality and Resolution Control',
+	'fogg-cg-meta' => 'Meta Data for the Clip',
+	'fogg-cg-range' => 'Encoding Range',
+	'fogg-cg-advVideo' => 'Advanced Video Encoding Controls',
+	'fogg-cg-advAudio' => 'Advanced Audio Encoding Controls',
+	'fogg-preset-custom' => 'Custom Settings',
+
+	/*
+	* js file: /mv_embed.js
+	*/
+	'loading_txt' => 'loading <blink>...</blink>',
+	'loading_title' => 'Loading...',
+	'size-gigabytes' => '$1 GB',
+	'size-megabytes' => '$1 MB',
+	'size-kilobytes' => '$1 K',
+	'size-bytes' => '$1 B',
+
+	/*
 	* js file: /libEmbedVideo/embedVideo.js
 	*/
 	'loading_plugin' => 'loading plugin<blink>...</blink>',
@@ -197,40 +225,5 @@ $messages['en'] = array(
 	'mv_generic_missing_plugin' => 'You browser does not appear to support playback type: <b>$1</b><br> visit the <a href="http://commons.wikimedia.org/wiki/Commons:Media_help">Playback Methods</a> page to download a player<br>',
 	'mv_for_best_experience' => 'For a better video playback experience we recommend <b><a href="http://www.mozilla.com/en-US/firefox/upgrade.html?from=mv_embed">Firefox 3.5</a></b>',
 	'mv_do_not_warn_again' => 'Do not warn me again.',
-
-	/*
-	* js file: /libClipEdit/mvClipEdit.js
-	*/
-	'mv_crop' => 'Crop Image',
-	'mv_apply_crop' => 'Apply Crop to Image',
-	'mv_reset_crop' => 'Rest Crop',
-	'mv_insert_image_page' => 'Insert Into page',
-	'mv_insert_into_sequence' => 'Insert Into Sequence',
-	'mv_preview_insert' => 'Preview Insert',
-	'mv_cancel_image_insert' => 'Cancel Insert',
-	'sc_fileopts' => 'Clip Detail Edit',
-	'sc_inoutpoints' => 'Set In-Out points',
-	'sc_overlays' => 'Overlays',
-	'sc_audio' => 'Audio Control',
-	'sc_duration' => 'Duration',
-	'mv_template_properties' => 'Template Properties',
-	'mv_custom_title' => 'Custom Title',
-	'mv_edit_properties' => 'Edit Properties',
-	'mv_other_properties' => 'Other Properties',
-	'mv_resource_page' => 'Resource Page:',
-	'mv_set_in_out_points' => 'Set in-out points',
-	'mv_start_time' => 'Start Time',
-	'mv_end_time' => 'End Time',
-	'mv_preview_inout' => 'Preview/Play In-out points',
-
-	/*
-	* js file: /mv_embed.js
-	*/
-	'loading_txt' => 'loading <blink>...</blink>',
-	'loading_title' => 'Loading...',
-	'size-gigabytes' => '$1 GB',
-	'size-megabytes' => '$1 MB',
-	'size-kilobytes' => '$1 K',
-	'size-bytes' => '$1 B',
 );
 #</JAVASCRIPT EN REPLACE>
