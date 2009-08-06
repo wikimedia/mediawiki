wikimedia/mediawiki

commit b5a8e27b680a1a9dff29f1a256a56efd89e7e8c9

Convert check to use wfShorthandToInteger()
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@54428 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialUpload.php
@@ -735,21 +735,7 @@ wgUploadAutoFill = {$autofill};
 
 		# Get the maximum file size from php.ini as $wgMaxUploadSize works for uploads from URL via CURL only
 		# See http://www.php.net/manual/en/ini.core.php#ini.upload-max-filesize for possible values of upload_max_filesize
-		$val = trim( ini_get( 'upload_max_filesize' ) );
-		$last = strtoupper( ( substr( $val, -1 ) ) );
-		switch( $last ) {
-			case 'G':
-				$val2 = substr( $val, 0, -1 ) * 1024 * 1024 * 1024;
-				break;
-			case 'M':
-				$val2 = substr( $val, 0, -1 ) * 1024 * 1024;
-				break;
-			case 'K':
-				$val2 = substr( $val, 0, -1 ) * 1024;
-				break;
-			default:
-				$val2 = $val;
-		}
+		$val = wfShorthandToInteger( ini_get( 'upload_max_filesize' ) );
 		$maxUploadSize = '<div id="mw-upload-maxfilesize">' .
 			wfMsgExt( 'upload-maxfilesize', array( 'parseinline', 'escapenoentities' ),
 				$wgLang->formatSize( $val2 ) ) .

commit 07a286fd0ce68a4bd36b1d468b03b2ce4326ca8d

* <b>Notice</b>:  Undefined variable: val2 in <b>/var/www/sandwiki/includes/specials/SpecialUpload.php</b> on line <b>741</b> * Fixed regression from r54428

includes/specials/SpecialUpload.php
@@ -738,7 +738,7 @@ wgUploadAutoFill = {$autofill};
 		$val = wfShorthandToInteger( ini_get( 'upload_max_filesize' ) );
 		$maxUploadSize = '<div id="mw-upload-maxfilesize">' .
 			wfMsgExt( 'upload-maxfilesize', array( 'parseinline', 'escapenoentities' ),
-				$wgLang->formatSize( $val2 ) ) .
+				$wgLang->formatSize( $val ) ) .
 				"</div>\n";
 		//add a hidden filed for upload by url (uses the $wgMaxUploadSize var)
 		if( UploadFromUrl::isEnabled() ){