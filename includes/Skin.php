d4d082787f85593736e454f8156ea58a7cc25361
Chad Horohoe <demon@users.mediawiki.org>
2009-08-13 00:47:56 +0000

    (bug 20203) "Powered by Mediawiki" now has height/width on image tag

--- includes/Skin.php
+++ includes/Skin.php
@@ -1503,7 +1503,7 @@ END;
 	function getPoweredBy() {
 		global $wgStylePath;
 		$url = htmlspecialchars( "$wgStylePath/common/images/poweredby_mediawiki_88x31.png" );
-		$img = '<a href="http://www.mediawiki.org/"><img src="'.$url.'" alt="Powered by MediaWiki" /></a>';
+		$img = '<a href="http://www.mediawiki.org/"><img src="'.$url.'" height="31" width="88" alt="Powered by MediaWiki" /></a>';
 		return $img;
 	}
 
