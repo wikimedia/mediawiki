11a36ca94b6a841de43b5362fee41152e56f5be0
Brion Vibber <brion@users.mediawiki.org>
2009-08-13 00:32:41 +0000

    Set up CKB as RTL...

--- languages/messages/MessagesCkb_arab.php
+++ languages/messages/MessagesCkb_arab.php
@@ -15,6 +15,17 @@
  * @author رزگار
  */
 
+$linkPrefixExtension = true;
+$fallback8bitEncoding = 'windows-1256';
+
+$rtl = true;
+$defaultUserOptionOverrides = array(
+	# Swap sidebar to right side by default
+	'quickbar' => 2,
+	# Underlines seriously harm legibility. Force off:
+	'underline' => 0,
+);
+
 $digitTransformTable = array(
 	'0' => '٠', # &#x0660;
 	'1' => '١', # &#x0661;
