6f6d35083cbaf6deb6ca0c735af89e5b89e877e7
Raimond Spekking <raymond@users.mediawiki.org>
2009-08-14 08:16:51 +0000

    Recommit r53268 (simetrical) after general revert r54735: Fix bug, werdna not on Special:Version Recommit r53485 (siebrand) after general revert r54735: Add major contributors raymond, ialex, siebrand Recommit r54450 (demon) after general revert r54735: Might as well.

--- includes/specials/SpecialVersion.php
+++ includes/specials/SpecialVersion.php
@@ -62,7 +62,8 @@ class SpecialVersion extends SpecialPage {
 		copyright © 2001-2009 Magnus Manske, Brion Vibber, Lee Daniel Crocker,
 		Tim Starling, Erik Möller, Gabriel Wicke, Ævar Arnfjörð Bjarmason,
 		Niklas Laxström, Domas Mituzas, Rob Church, Yuri Astrakhan, Aryeh Gregor,
-		Aaron Schulz and others.
+		Aaron Schulz, Andrew Garrett, Raimond Spekking, Alexandre Emsenhuber,
+		Siebrand Mazeland, Chad Horohoe and others.
 
 		MediaWiki is free software; you can redistribute it and/or modify
 		it under the terms of the GNU General Public License as published by
