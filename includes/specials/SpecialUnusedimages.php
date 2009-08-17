4163fd609e0aa5ef54ebcb8395acf1d14ccc3fbf
Chad Horohoe <demon@users.mediawiki.org>
2009-08-17 00:59:25 +0000

    (bug 20265) Special:AncientPages (and UnusedFiles) broken for SQLite.

--- includes/specials/SpecialUnusedimages.php
+++ includes/specials/SpecialUnusedimages.php
@@ -32,6 +32,9 @@ class UnusedimagesPage extends ImageQueryPage {
 			case 'oracle': 
 				$epoch = '((trunc(img_timestamp) - to_date(\'19700101\',\'YYYYMMDD\')) * 86400)'; 
 				break;
+			case 'sqlite':
+				$epoch = 'img_timestamp';
+				break;
 			default:
 				$epoch = 'EXTRACT(epoch FROM img_timestamp)';
 		}
