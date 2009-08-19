a2f5e3a15119b1240d214efd56c0806d8cc2f8c0
Brion Vibber <brion@users.mediawiki.org>
2009-08-19 02:07:00 +0000

    Revert image crop for now: r54746 "Update formatting for r54745" r54745 "Added crop support for inline images." r54748 "Making demon happy (adding public/protected to function definitions) and add some comments along the way."
    Several issues:
    * Implementation is ImageMagick-specific, no support for GD backend
    * Specification syntax is pretty ugly and non-obvious imo... [[File:foo.jpg|<width>px|<left>x<top>x<width>x<height]]
    * Crop syntax seems to be tied to pixels... I _presume_ source pixels? This would break if a file is re-uploaded with a new size.
    * In general I'm very leery of tacking on more infinite-options-in-infinte-combinations for image rendering; our treatment of resizing already has implications for CPU and disk usage and this just adds another level of arbitrary-rendered horror. ;)
    
    I'd much rather we move towards limiting the number of rendered variants we have, not increase them... IMO cropping would be better served with an interface allowing for explicit, visible cropping which creates either a new saved version, or a 'cloned' virtual file which can be referred to by name... and more importantly, uses of it would be trackable so that if crops needs to be updated they can be cleanly.

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -288,7 +288,6 @@ $magicWords = array(
 	'img_text_bottom'        => array( 1,    'text-bottom'            ),
 	'img_link'               => array( 1,    'link=$1'                ),
 	'img_alt'                => array( 1,    'alt=$1'                 ),
-	'img_crop'               => array( 1,    'crop=$1'                ),
 	'int'                    => array( 0,    'INT:'                   ),
 	'sitename'               => array( 1,    'SITENAME'               ),
 	'ns'                     => array( 0,    'NS:'                    ),
