8da1f64159143641b95d159c77a3dc5315402cad
Rotem Liss <rotem@users.mediawiki.org>
2009-08-20 21:49:59 +0000

    Localization update.

--- languages/messages/MessagesHe.php
+++ languages/messages/MessagesHe.php
@@ -140,7 +140,6 @@ $magicWords = array(
 	'img_bottom'            => array( '1', 'למטה', 'bottom' ),
 	'img_text_bottom'       => array( '1', 'בתחתית הטקסט', 'text-bottom' ),
 	'img_link'              => array( '1', 'קישור=$1', 'link=$1' ),
-	'img_crop'              => array( '1', 'חיתוך=$1', 'crop=$1'                                ),
 	'img_alt'               => array( '1', 'טקסט=$1', 'alt=$1' ),
 	'int'                   => array( '0', 'הודעה:', 'INT:' ),
 	'sitename'              => array( '1', 'שם האתר', 'SITENAME' ),
