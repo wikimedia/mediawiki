e7df37b1d2e5f8c38d46fc9c67d517f3c67d4944
Brion Vibber <brion@users.mediawiki.org>
2009-08-21 22:42:09 +0000

    Revert r53600 "Add $wgSecondaryGoNamespace, used if an exact match is not found when pressing go - will try to find a match in a different namespace before performing a search." Revert r53618 "* Replace $wgSecondaryGoNamespace with $wgSecondaryGoNamespaces (taking an array of integers instead of just an integer), per code review of r53600." Revert r53672 "* Add release note for r53618." Per bug 11380 this should probably be using the default search namespaces or the user's search namespaces; an additional config array seems unnecessary.

--- includes/search/SearchEngine.php
+++ includes/search/SearchEngine.php
@@ -63,7 +63,7 @@ class SearchEngine {
 	 * @return Title
 	 */
 	public static function getNearMatch( $searchterm ) {
-		global $wgContLang, $wgSecondaryGoNamespaces;
+		global $wgContLang;
 
 		$allSearchTerms = array($searchterm);
 
@@ -86,14 +86,6 @@ class SearchEngine {
 			$article = MediaWiki::articleFromTitle( $title );
 			if( $article->hasViewableContent() ) {
 				return $title;
-			}
-
-			# If a match is not found in the main namespace look in secondary go namespaces.
-			if( $wgSecondaryGoNamespaces && $title->getNamespace() == NS_MAIN ) {
-				foreach( $wgSecondaryGoNamespaces as $ns ) {
-					$title = Title::newFromText( $term, $ns );
-					if( $title && $title->exists() ) return $title;
-				}
 			}
 
 			# Now try all lower case (i.e. first letter capitalized)
