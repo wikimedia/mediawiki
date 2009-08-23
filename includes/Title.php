8dfb5c2e2390014bb652949c2e136e6b2d1d455a
Brion Vibber <brion@users.mediawiki.org>
2009-08-23 00:45:36 +0000

    * (bug 11143) Links containing invalid UTF-8 percent-code sequences are now   cleanly disabled instead of breaking parsing entirely on PHP 5.2.
    This was fixed as a side-effect of r55382 for bug 15248; adding comments
    in Title::secureAndSplit nothing this.

--- includes/Title.php
+++ includes/Title.php
@@ -2241,6 +2241,9 @@ class Title {
 		$dbkey = preg_replace( '/\xE2\x80[\x8E\x8F\xAA-\xAE]/S', '', $dbkey );
 
 		# Clean up whitespace
+		# Note: use of the /u option on preg_replace here will cause
+		# input with invalid UTF-8 sequences to be nullified out in PHP 5.2.x,
+		# conveniently disabling them.
 		#
 		$dbkey = preg_replace( '/[ _\xA0\x{1680}\x{180E}\x{2000}-\x{200B}\x{2028}\x{2029}\x{202F}\x{205F}\x{3000}]+/u', '_', $dbkey );
 		$dbkey = trim( $dbkey, '_' );
