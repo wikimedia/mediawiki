f17ebcd5419653c70c15daaf8787d15ce59c94f1
Happy-melon <happy-melon@users.mediawiki.org>
2009-08-30 21:50:10 +0000

    * Allow IndexPager->mLimitsShown to be an associative array of limit => text-to-display-in-limit-form pairs. * Use this to add an "all" value to the limit form in Special:AllMessages.  In reality, "all" is only 5000, but this is plenty more than most wikis should have.

--- includes/specials/SpecialAllmessages.php
+++ includes/specials/SpecialAllmessages.php
@@ -128,6 +128,7 @@ class AllmessagesTablePager extends TablePager {
 
 	var $messages  = null;
 	var $talkPages = null;
+	public $mLimitsShown;
 
 	function __construct( $page, $conds, $langObj = null ) {
 		parent::__construct();
@@ -135,6 +136,10 @@ class AllmessagesTablePager extends TablePager {
 		$this->mPage = $page;
 		$this->mConds = $conds;
 		$this->mDefaultDirection = true; // always sort ascending
+		// We want to have an option for people to view *all* the messages, 
+		// so they can use Ctrl+F to search them.  5000 is the maximum that 
+		// will get through WebRequest::getLimitOffset().
+		$this->mLimitsShown = array( 20, 50, 100, 250, 500, 5000 => wfMsg('messagesall') );
 
 		global $wgLang, $wgContLang, $wgRequest;
 
