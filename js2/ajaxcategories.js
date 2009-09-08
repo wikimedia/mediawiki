ae177744090bbc69bf18e5874a2b1cd131c00018
Andrew Garrett <werdna@users.mediawiki.org>
2009-09-08 15:19:52 +0000

    Remove ghost of patch applications past

--- js2/ajaxcategories.js
+++ js2/ajaxcategories.js
@@ -300,16 +300,3 @@ var ajaxCategories = {
 };
 
 js2AddOnloadHook( ajaxCategories.setupAJAXCategories );
-loadGM( {
-		"ajax-add-category":"[Add Category]",
-		"ajax-add-category-submit":"[Add]",
-		"ajax-confirm-prompt":"[Confirmation Text]",
-		"ajax-confirm-title":"[Confirmation Title]",
-		"ajax-confirm-save":"[Save]",
-		"ajax-add-category-summary":"[Add category $1]",
-		"ajax-remove-category-summary":"[Remove category $2]",
-		"ajax-confirm-actionsummary":"[Summary]",
-		"ajax-error-title":"Error",
-		"ajax-error-dismiss":"OK",
-		"ajax-remove-category-error":"[RemoveErr]"
-		} );
