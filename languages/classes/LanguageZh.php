70464b0cdf83b78df8c40643e278d36d325a9c5c
Andrew Garrett <werdna@users.mediawiki.org>
2009-09-16 23:53:31 +0000

    Fix E_NOTICE in r55415 breaking Zh variants

--- languages/classes/LanguageZh.php
+++ languages/classes/LanguageZh.php
@@ -132,8 +132,7 @@ class LanguageZh extends LanguageZh_hans {
 		global $wgHooks;
 		parent::__construct();
 
-		$variants_all = array('zh','zh-hans','zh-hant','zh-cn','zh-hk','zh-mo','zh-my','zh-sg','zh-tw');
-		$variants = array();
+		$variants = array('zh','zh-hans','zh-hant','zh-cn','zh-hk','zh-mo','zh-my','zh-sg','zh-tw');
 		
 		$variantfallbacks = array(
 			'zh'      => array('zh-hans','zh-hant','zh-cn','zh-tw','zh-hk','zh-sg','zh-mo','zh-my'),
