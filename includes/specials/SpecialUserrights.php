6f1b6ba7276986b8d0e1c2a3d6d5653dd3cd80c3
Alex Z. <mrzman@users.mediawiki.org>
2009-09-16 21:38:45 +0000

    use fetchUser statically per r56133

--- includes/specials/SpecialUserrights.php
+++ includes/specials/SpecialUserrights.php
@@ -144,7 +144,7 @@ class UserrightsPage extends SpecialPage {
 	function saveUserGroups( $username, $reason = '' ) {
 		global $wgRequest, $wgUser, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf;
 
-		$user = $this->fetchUser( $username );
+		$user = self::fetchUser( $username );
 		if( $user instanceof WikiErrorMsg ) {
 			$wgOut->addWikiMsgArray( $user->getMessageKey(), $user->getMessageArgs() );
 			return;
@@ -250,7 +250,7 @@ class UserrightsPage extends SpecialPage {
 	function editUserGroupsForm( $username ) {
 		global $wgOut;
 
-		$user = $this->fetchUser( $username );
+		$user = self::fetchUser( $username );
 		if( $user instanceof WikiErrorMsg ) {
 			$wgOut->addWikiMsgArray( $user->getMessageKey(), $user->getMessageArgs() );
 			return;
