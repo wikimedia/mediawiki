wikimedia/mediawiki

commit 7031c65a0f8beddc2b0d4060cbe65867cc809248

Fix dodgy uses of wfMsgHtml() and related HTML escaping
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@53173 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/LogPage.php
@@ -214,11 +214,12 @@ class LogPage {
 							self::formatBlockFlags( $params[2], is_null( $skin ) ) : '';
 					// Page protections
 					} else if ( $type == 'protect' && count($params) == 3 ) {
-						$details .= " {$params[1]}"; // restrictions and expiries
 						if( $params[2] ) {
 							if ( $skin ) {
+								$details .= htmlspecialchars( " {$params[1]}" ); // restrictions and expiries
 								$details .= ' ['.wfMsg('protect-summary-cascade').']';
 							} else {
+								$details .= " {$params[1]}";
 								$details .= ' ['.wfMsgForContent('protect-summary-cascade').']';
 							}
 						}
@@ -245,7 +246,7 @@ class LogPage {
 						$details .= ': '.RevisionDeleter::getLogMessage( $count, $nfield, $ofield, true );
 					}
 					if ( $skin ) {
-						$rv = wfMsgHtml( $wgLogActions[$key], $params ) . $details;
+						$rv = htmlspecialchars( wfMsg( $wgLogActions[$key], $params ) ) . $details;
 					} else {
 						$rv = wfMsgExt( $wgLogActions[$key], array( 'parsemag', 'escape', 'replaceafter', 'content' ), $params ) . $details;
 					}

includes/specials/SpecialContributions.php
@@ -144,7 +144,7 @@ class SpecialContributions extends SpecialPage {
 		$sk = $wgUser->getSkin();
 
 		if( 0 == $id ) {
-			$user = $nt->getText();
+			$user = htmlspecialchars( $nt->getText() );
 		} else {
 			$user = $sk->link( $nt, htmlspecialchars( $nt->getText() ) );
 		}

includes/specials/SpecialDeletedContributions.php
@@ -324,7 +324,7 @@ class DeletedContributionsPage extends SpecialPage {
 		$sk = $wgUser->getSkin();
 
 		if ( 0 == $id ) {
-			$user = $nt->getText();
+			$user = htmlspecialchars( $nt->getText() );
 		} else {
 			$user = $sk->link( $nt, htmlspecialchars( $nt->getText() ) );
 		}

includes/specials/SpecialUserrights.php
@@ -498,9 +498,13 @@ class UserrightsPage extends SpecialPage {
 			$ret .= "\t<td style='vertical-align:top;'>\n";
 			foreach( $column as $group => $checkbox ) {
 				$attr = $checkbox['disabled'] ? array( 'disabled' => 'disabled' ) : array();
-				$text = $checkbox['irreversible']
-					? wfMsgHtml( 'userrights-irreversible-marker', User::getGroupMember( $group ) )
-					: User::getGroupMember( $group );
+
+				if ( $checkbox['irreversible'] ) {
+					$text = htmlspecialchars( wfMsg( 'userrights-irreversible-marker', 
+						User::getGroupMember( $group ) ) );
+				} else {
+					$text = htmlspecialchars( User::getGroupMember( $group ) );
+				}
 				$checkboxHtml = Xml::checkLabel( $text, "wpGroup-" . $group,
 					"wpGroup-" . $group, $checkbox['set'], $attr );
 				$ret .= "\t\t" . ( $checkbox['disabled']

commit 151ef9d2e0627e857bf93d6ec24e8e3406358d56

bug 20701: fixed strange breakage from r53173

includes/LogPage.php
@@ -219,12 +219,17 @@ class LogPage {
 							self::formatBlockFlags( $params[2], is_null( $skin ) ) : '';
 					// Page protections
 					} else if ( $type == 'protect' && count($params) == 3 ) {
+						// Restrictions and expiries
+						if( $skin ) {
+							$details .= htmlspecialchars( " {$params[1]}" );
+						} else {
+							$details .= " {$params[1]}";
+						}
+						// Cascading flag...
 						if( $params[2] ) {
 							if ( $skin ) {
-								$details .= htmlspecialchars( " {$params[1]}" ); // restrictions and expiries
 								$details .= ' ['.wfMsg('protect-summary-cascade').']';
 							} else {
-								$details .= " {$params[1]}";
 								$details .= ' ['.wfMsgForContent('protect-summary-cascade').']';
 							}
 						}