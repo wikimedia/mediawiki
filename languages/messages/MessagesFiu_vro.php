2ff816905d69d3fa9d839956aa94bf7db01fbfb7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-09-28 07:53:56 +0000

    Revert r56566 for MessagesFiu_vro.php. Should remain empty.

--- languages/messages/MessagesFiu_vro.php
+++ languages/messages/MessagesFiu_vro.php
@@ -1,22 +1,12 @@
 <?php
-/** Võro (Võro)
+/** Võro
  *
  * See MessagesQqq.php for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
  *
  * @ingroup Language
  * @file
- *
- * @author Võrok
+ * @comment Deprecated language code. Falls back to 'vro'.
  */
 
 $fallback = 'vro';
-
-$messages = array(
-'talk' => 'Arotus',
-
-# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
-'mainpage'   => 'Pääleht',
-'policy-url' => 'Project:Säädüseq',
-
-);
