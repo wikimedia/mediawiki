086f5720f78b9f1a75d1cdacc0105d2d52f8ac2a
Brion Vibber <brion@users.mediawiki.org>
2009-09-29 23:16:30 +0000

    Revert r56635 "Implement a 'root' permission, which grants all other permissions. Useful for debugging and administration." I'd much rather keep permission keys clean and separate.

--- includes/User.php
+++ includes/User.php
@@ -2154,8 +2154,7 @@ class User {
 		}
 		# Use strict parameter to avoid matching numeric 0 accidentally inserted
 		# by misconfiguration: 0 == 'foo'
-		return in_array( $action, $this->getRights(), true )
-			|| in_array( 'root', $this->getRights(), true );
+		return in_array( $action, $this->getRights(), true );
 	}
 
 	/**
