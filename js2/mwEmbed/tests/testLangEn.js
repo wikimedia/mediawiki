5f700eb8542209836f0c7800932e946c1a699fd4
Michael Dale <dale@users.mediawiki.org>
2009-10-07 16:37:27 +0000

    * quick hack at recursive template parsing probably should be replaced by some more feature complete / planed out javasciprt wikit text parsing approach  )

--- js2/mwEmbed/tests/testLangEn.js
+++ js2/mwEmbed/tests/testLangEn.js
@@ -7,7 +7,7 @@ loadGM({
 	'test_plural_msg' : '{{PLURAL:$1|one|other}}',
 	//sample real world msgs: 
 	'undelete_short'  : 'Undelete {{PLURAL:$1|one edit|$1 edits}}',
-	'category-subcat-count' : "{{PLURAL:$2|This category has only the following subcategory.|This category has the following {{PLURAL:$1|subcategory|$1 subcategories}}, out of $2 total.}}"
+	'category-subcat-count' : "fish {{PLURAL:$2|This {{PLURAL:$1|fistsub|$1 secondsub}} category has only the following subcategory.|This category has the following {{PLURAL:$1|subcategory|$1 subcategories}}, out of $2 total.}}"
 });
 
 $mw.lang.loadRS({
