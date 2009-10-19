e6bf1234a58e92a51782c4ce634307dd6dab6d4d
Roan Kattouw <catrope@users.mediawiki.org>
2009-10-19 20:53:20 +0000

    Bump style version for r57916

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1605,7 +1605,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '243';
+$wgStyleVersion = '244';
 
 
 # Server-side caching:
