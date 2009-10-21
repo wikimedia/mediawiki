f04065ab40350039b2b1044692910168fe8461af
OverlordQ <overlordq@users.mediawiki.org>
2009-10-21 02:50:45 +0000

    Followup to r57964, forgot to revert column change

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -633,6 +633,6 @@ INSERT INTO mediawiki_version (type,mw_version,sql_version,sql_date)
 CREATE TABLE l10n_cache (
   lc_lang     TEXT    NOT NULL,
   lc_key      TEXT    NOT NULL,
-  lc_value    BYTEA   NOT NULL
+  lc_value    TEXT    NOT NULL
 );
 CREATE INDEX l10n_cache_lc_lang_key ON l10n_cache (lc_lang, lc_key);
