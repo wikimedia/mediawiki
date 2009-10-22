0969c9c2c4951784914a87027e042f0f83e376bd
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2009-10-22 14:50:45 +0000

    * (bug 21234) Moving subpages of titles containing \\ now works properly
    Per http://www.php.net/manual/en/function.preg-replace.php \ must be doubled in $replacement

--- includes/specials/SpecialMovepage.php
+++ includes/specials/SpecialMovepage.php
@@ -458,7 +458,7 @@ class MovePageForm {
 
 			$newPageName = preg_replace(
 				'#^'.preg_quote( $ot->getDBkey(), '#' ).'#',
-				$nt->getDBkey(),
+				str_replace( '\\', '\\\\', $nt->getDBkey() ), # bug 21234
 				$oldSubpage->getDBkey()
 			);
 			if( $oldSubpage->isTalkPage() ) {
