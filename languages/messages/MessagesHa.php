ec3199fbf5fbfa6faef8d7c056fb9aa84c905aa0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-10-24 09:10:11 +0000

    Localisation updates for core messages from translatewiki.net * Follow up to r1284: finally add some translations for Hausa (yay!)

---
+++ languages/messages/MessagesHa.php
@@ -0,0 +1,85 @@
+<?php
+/** Hausa (هَوُسَ)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Mladanali
+ */
+
+$messages = array(
+# Dates
+'january'   => 'Janairu',
+'february'  => 'Faburairu',
+'march'     => 'Maris',
+'april'     => 'Afirilu',
+'may_long'  => 'Mayu',
+'june'      => 'Yuni',
+'july'      => 'Yuli',
+'august'    => 'Agusta',
+'september' => 'Satumba',
+'october'   => 'Oktoba',
+'november'  => 'Nuwamba',
+'december'  => 'Disamba',
+
+'navigation' => 'Shawagi',
+
+'tagline'          => 'Daga {{SITENAME}}',
+'search'           => 'Nema',
+'searchbutton'     => 'Mu je',
+'searcharticle'    => 'Binciko',
+'printableversion' => 'Sigar bugawa',
+'permalink'        => 'Dawwamammen zare',
+'edit'             => 'Gyarawa',
+'talkpagelinktext' => 'Hira',
+'personaltools'    => 'Zaɓaɓɓin kayan aiki',
+'views'            => 'Hange',
+'toolbox'          => 'Akwatin kayan aiki',
+'jumpto'           => 'A tsallaka zuwa:',
+'jumptonavigation' => 'Shawagi',
+'jumptosearch'     => 'Nema',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
+'aboutsite'      => 'Game da {{SITENAME}}',
+'aboutpage'      => 'Project:Game da',
+'disclaimers'    => 'Gargaɗi',
+'disclaimerpage' => 'Project:Babban gargaɗi',
+'mainpage'       => 'Marhabin',
+'privacy'        => 'Manufar kare sirri',
+'privacypage'    => 'Project:Manufar kare sirri',
+
+'retrievedfrom'   => 'Daga "$1"',
+'editsectionhint' => 'Gyara sashe: $1',
+'site-rss-feed'   => 'Kwararen RSS na $1',
+'site-atom-feed'  => 'Kwararen Atom na $1',
+'red-link-title'  => '$1 (babu wannan shafi)',
+
+# Diffs
+'editundo' => 'Janyewa',
+
+# Search results
+'search-result-size'        => '$1 ({{PLURAL:$2|1 kalma|$2 kalmomi}})',
+'search-mwsuggest-enabled'  => 'Tare da shawarwari',
+'search-mwsuggest-disabled' => 'Banda shawarwari',
+
+# Block/unblock
+'contribslink' => 'Gudummuwa',
+
+# Tooltip help for the actions
+'tooltip-search'          => 'Binciko {{SITENAME}}',
+'tooltip-search-go'       => 'A je ga shafi mai wannan suna idan akwai shi',
+'tooltip-search-fulltext' => 'Binciko shafuka masu wannan matani',
+'tooltip-n-mainpage'      => 'Duba shafin Marhabin',
+'tooltip-n-portal'        => 'A game da wannan shiri, abinda za a iya yi, ina za a samu abubuwa',
+'tooltip-n-recentchanges' => 'Jerin sabin sauye-sauye a wannan wiki',
+'tooltip-n-randompage'    => 'A nuno wani shafi da ka',
+'tooltip-n-help'          => 'A nuno taimako',
+'tooltip-t-specialpages'  => 'Jerin duk shafuka na musamman',
+
+# Special:SpecialPages
+'specialpages' => 'Shafuka na musamman',
+
+);
