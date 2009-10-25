52023a835643e3d5d2b21aadc8abe56b4b81c360
Raimond Spekking <raymond@users.mediawiki.org>
2009-10-25 21:28:14 +0000

    Revert r58064: half done work committed by accident

--- includes/specials/SpecialIpblocklist.php
+++ includes/specials/SpecialIpblocklist.php
@@ -311,13 +311,6 @@ class IPUnblockForm {
 			$wgOut->addHTML( $this->searchForm() );
 			$wgOut->addWikiMsg( 'ipblocklist-empty' );
 		}
-
-		$otherBlockLink = '';
-		wfRunHooks( 'getBlockList', array( &$otherBlockLink, $this->ip ) );
-		if( $otherBlockLink != '' ) {
-			$wgOut->addHTML( $otherBlockLink );
-		}
-
 	}
 
 	function searchForm() {
