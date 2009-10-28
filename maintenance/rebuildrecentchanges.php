b2a66abedaa5149675ac0ed36b600f7c3e4b4fbf
OverlordQ <overlordq@users.mediawiki.org>
2009-10-28 18:24:04 +0000

    (bug 13453) Fix rebuildrecentchanges for DB's with FK constraints

--- maintenance/rebuildrecentchanges.php
+++ maintenance/rebuildrecentchanges.php
@@ -186,7 +186,7 @@ class RebuildRecentchanges extends Maintenance {
 				'rc_this_oldid' => 0,
 				'rc_last_oldid' => 0,
 				'rc_type'       => RC_LOG,
-				'rc_cur_id'     => 'COALESCE(page_id, 0)',
+				'rc_cur_id'     => $dbw->cascadingDeletes() ? 'page_id' : 'COALESCE(page_id, 0)',
 				'rc_log_type'   => 'log_type',
 				'rc_log_action' => 'log_action',
 				'rc_logid'      => 'log_id',
