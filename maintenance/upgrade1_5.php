56336f74e467272972e484c3bc4421990cf4208e
Max Semenik <maxsem@users.mediawiki.org>
2009-11-12 13:12:18 +0000

    Some measures to prevent people from update.php/upgrade1_5.php confusion: the latter now displays a warning and requires --update switch to do something. Probably, it's time to put it to death completely?

--- maintenance/upgrade1_5.php
+++ maintenance/upgrade1_5.php
@@ -18,6 +18,15 @@ $options = array( 'step', 'noimages' );
 require_once( dirname(__FILE__) . '/commandLine.inc' );
 require_once( 'FiveUpgrade.inc' );
 
+echo "ATTENTION: This script is for upgrades from 1.4 to 1.5 (NOT 1.15) in very special cases.\n";
+echo "Use update.php for usual updates.\n";
+
+// Seems to confuse some people
+if ( !array_search( '--upgrade', $_SERVER['argv'] ) ) {
+	echo "Please run this script with --upgrade key to actually run the updater.\n";
+	die;
+}
+
 $upgrade = new FiveUpgrade();
 $step = isset( $options['step'] ) ? $options['step'] : null;
 $upgrade->upgrade( $step );
