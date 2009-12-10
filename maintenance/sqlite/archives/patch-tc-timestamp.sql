48ba1f8b5e0274cb0b0851e47285e43514186424
OverlordQ <overlordq@users.mediawiki.org>
2009-12-10 10:10:56 +0000

    Addendum to r59925, fix remaining straggling files that set rc_time to int

--- maintenance/sqlite/archives/patch-tc-timestamp.sql
+++ maintenance/sqlite/archives/patch-tc-timestamp.sql
@@ -1,4 +1,4 @@
-ALTER TABLE /*_*/transcache MODIFY tc_time varchar(14);
+ALTER TABLE /*_*/transcache MODIFY tc_time binary(14);
 UPDATE /*_*/transcache SET tc_time = DATE_FORMAT(FROM_UNIXTIME(tc_time), "%Y%c%d%H%i%s");
 
 INSERT INTO /*_*/updatelog VALUES ('convert transcache field');
