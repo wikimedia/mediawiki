06a1faead331398074fcec5d26b41a7636282511
Tim Starling <tstarling@users.mediawiki.org>
2009-12-11 06:49:48 +0000

    Restored the function of ApiMain::requestWriteMode(), as per the suggestion on CR r50833.

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -203,7 +203,12 @@ class ApiMain extends ApiBase {
 	 * Only kept for backwards compatibility
 	 * @deprecated Use isWriteMode() instead
 	 */
-	public function requestWriteMode() {}
+	public function requestWriteMode() {
+		if (!$this->mEnableWrite)
+			$this->dieUsageMsg(array('writedisabled'));
+		if (wfReadOnly())
+			$this->dieUsageMsg(array('readonlytext'));		
+	}
 
 	/**
 	 * Set how long the response should be cached.
