100d62dd876fcdce888a1f6ce7a87c4852545b7c
Max Semenik <maxsem@users.mediawiki.org>
2009-12-11 23:30:08 +0000

    Whee, a debugging piece of SQL had made it through in r59932

--- maintenance/sqlite/archives/patch-tc-timestamp.sql
+++ maintenance/sqlite/archives/patch-tc-timestamp.sql
@@ -1,3 +1,3 @@
-UPDATE /*_*/transcache SET tc_time = strftime('%Y%m%d%H%M%S', datetime(1260465428, 'unixepoch'));
+UPDATE /*_*/transcache SET tc_time = strftime('%Y%m%d%H%M%S', datetime(tc_time, 'unixepoch'));
 
 INSERT INTO /*_*/updatelog VALUES ('convert transcache field');
