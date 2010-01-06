deccf2fa1eac294f44bcf9c76cb894c2c5bf31c4
Roan Kattouw <catrope@users.mediawiki.org>
2010-01-06 11:49:03 +0000

    Followup to r60697: add a comment to js2stopgap.js pointing out that we patched jQuery there, and raise $wgStyleVersion

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1655,7 +1655,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '258';
+$wgStyleVersion = '259';
 
 
 # Server-side caching:
