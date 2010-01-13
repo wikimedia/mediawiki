17f00855ee310e903ab69d5803da723a3ae9f91b
Jack Phoenix <ashley@users.mediawiki.org>
2010-01-13 13:17:19 +0000

    JavaScript cleanup: add spacing, missing braces, use single quotes instead of double quotes where appropriate and fix some comparisons

--- skins/common/block.js
+++ skins/common/block.js
@@ -1,19 +1,19 @@
-addOnloadHook(considerChangingExpiryFocus);
+addOnloadHook( considerChangingExpiryFocus );
 
 function considerChangingExpiryFocus() {
-	if (!document.getElementById) {
+	if ( !document.getElementById ) {
 		return;
 	}
-	var drop = document.getElementById('wpBlockExpiry');
-	if (!drop) {
+	var drop = document.getElementById( 'wpBlockExpiry' );
+	if ( !drop ) {
 		return;
 	}
-	var field = document.getElementById('wpBlockOther');
-	if (!field) {
+	var field = document.getElementById( 'wpBlockOther' );
+	if ( !field ) {
 		return;
 	}
 	var opt = drop.value;
-	if (opt == 'other') {
+	if ( opt == 'other' ) {
 		field.style.display = '';
 	} else {
 		field.style.display = 'none';
@@ -21,34 +21,36 @@ function considerChangingExpiryFocus() {
 }
 
 function updateBlockOptions() {
-	if (!document.getElementById)
+	if ( !document.getElementById ) {
 		return;
+	}
 
-	var target = document.getElementById('mw-bi-target');
-	if (!target)
+	var target = document.getElementById( 'mw-bi-target' );
+	if ( !target ) {
 		return;
+	}
 
 	var addy = target.value;
 	var isEmpty = addy.match(/^\s*$/);
 	var isIp = addy.match(/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|:(:[0-9A-Fa-f]{1,4}){1,7}|[0-9A-Fa-f]{1,4}(:{1,2}[0-9A-Fa-f]{1,4}|::$){1,7})(\/\d+)?$/);
 	var isIpRange = isIp && addy.match(/\/\d+$/);
 
-	var anonymousRow = document.getElementById('wpAnonOnlyRow');
+	var anonymousRow = document.getElementById( 'wpAnonOnlyRow' );
 	if( anonymousRow ) {
-		anonymousRow.style.display = (!isIp && !isEmpty) ? 'none' : '';
+		anonymousRow.style.display = ( !isIp && !isEmpty ) ? 'none' : '';
 	}
 
-	var autoblockRow = document.getElementById('wpEnableAutoblockRow');
+	var autoblockRow = document.getElementById( 'wpEnableAutoblockRow' );
 	if( autoblockRow ) {
 		autoblockRow.style.display = isIp && !isEmpty ? 'none' : '';
 	}
 	
-	var hideuserRow = document.getElementById('wpEnableHideUser');
+	var hideuserRow = document.getElementById( 'wpEnableHideUser' );
 	if( hideuserRow ) {
 		hideuserRow.style.display = isIp && !isEmpty ? 'none' : '';
 	}
 
-	var watchuserRow = document.getElementById('wpEnableWatchUser');
+	var watchuserRow = document.getElementById( 'wpEnableWatchUser' );
 	if( watchuserRow ) {
 		watchuserRow.style.display = isIpRange && !isEmpty ? 'none' : '';
 	}
--- skins/common/search.js
+++ skins/common/search.js
@@ -1,40 +1,45 @@
 // JS specific to Special:Search
 
 // change the search link to what user entered
-function mwSearchHeaderClick(obj){
-	var searchbox = document.getElementById("searchText");
-	if( searchbox == null )
-		searchbox = document.getElementById("powerSearchText");
-	if( searchbox == null)
+function mwSearchHeaderClick( obj ) {
+	var searchbox = document.getElementById( 'searchText' );
+	if( searchbox === null ) {
+		searchbox = document.getElementById( 'powerSearchText' );
+	}
+	if( searchbox === null ) {
 		return; // should always have either normal or advanced search
-	
+	}
+
 	var searchterm = searchbox.value;
-	var parts = obj.href.split("search=");
+	var parts = obj.href.split( 'search=' );
 	var lastpart = '';
-	var prefix = "search=";
-	if( parts.length > 1 && parts[1].indexOf('&') >= 0 )
-		lastpart = parts[1].substring( parts[1].indexOf('&') )
-	else 
-		prefix = "&search=";
-	obj.href = parts[0] + prefix + encodeURIComponent(searchterm) + lastpart;
+	var prefix = 'search=';
+	if( parts.length > 1 && parts[1].indexOf('&') >= 0 ) {
+		lastpart = parts[1].substring( parts[1].indexOf('&') );
+	} else {
+		prefix = '&search=';
+	}
+	obj.href = parts[0] + prefix + encodeURIComponent( searchterm ) + lastpart;
 }
 
 function mwToggleSearchCheckboxes( btn ) {
-	if( !document.getElementById ) return;
-	
+	if( !document.getElementById ) {
+		return;
+	}
+
 	var nsInputs = document.getElementById( 'powersearch' ).getElementsByTagName( 'input' );
 	var isChecked = false;
-	
+
 	for ( var i = 0; i < nsInputs.length; i++ ) {
 		var pattern = /^ns/;
 		if ( ( nsInputs[i].type == 'checkbox' ) && ( pattern.test( nsInputs[i].name ) ) ) {
 			switch ( btn ) {
-				case "none":
+				case 'none':
 					if ( nsInputs[i].checked ) {
 						nsInputs[i].checked = false;
 					}
 					break;
-				case "all": 
+				case 'all':
 					if ( !nsInputs[i].checked ) {
 						nsInputs[i].checked = true;
 					}
