3d1137ebdfcaaa9949ef4fa4bc161c036736e40f
Chad Horohoe <demon@users.mediawiki.org>
2010-01-15 14:26:52 +0000

    Kill off IE50Fixes. Adds extra clutter to the head, and market share is down below 0.03% (http://is.gd/6jyWo).

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -35,7 +35,6 @@ class SkinMonoBook extends SkinTemplate {
 			$out->addStyle( $wgHandheldStyle, 'handheld' );
 		}
 
-		$out->addStyle( 'monobook/IE50Fixes.css', 'screen', 'lt IE 5.5000' );
 		$out->addStyle( 'monobook/IE55Fixes.css', 'screen', 'IE 5.5000' );
 		$out->addStyle( 'monobook/IE60Fixes.css', 'screen', 'IE 6' );
 		$out->addStyle( 'monobook/IE70Fixes.css', 'screen', 'IE 7' );
