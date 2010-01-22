5a08fb0debdbe1e7d4457ce429ae716bfe7d19ca
Rotem Liss <rotem@users.mediawiki.org>
2010-01-22 17:44:37 +0000

    Localization update for he, and rebuilding en.

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -2151,9 +2151,10 @@ This wiki is configured as a public wiki.
 For optimal security, img_auth.php is disabled.',
 'img-auth-noread'       => 'User does not have access to read "$1".',
 
-'http-invalid-url'      => 'Invalid URL: $1',
-'http-invalid-scheme'   => 'URLs with the "$1" scheme are not supported',
-'http-request-error'    => 'Error sending request:',
+# HTTP errors
+'http-invalid-url'    => 'Invalid URL: $1',
+'http-invalid-scheme' => 'URLs with the "$1" scheme are not supported',
+'http-request-error'  => 'Error sending request:',
 
 # Some likely curl errors. More could be added from <http://curl.haxx.se/libcurl/c/libcurl-errors.html>
 'upload-curl-error6'       => 'Could not reach URL',
