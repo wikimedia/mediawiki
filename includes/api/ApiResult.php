a4d411953b601a5678c668d523cd0649de254edd
Sam Reed <reedy@users.mediawiki.org>
2010-01-23 22:31:37 +0000

    Refactor array_intersect_key from ApiResult.php to GlobalFunctions.php

--- includes/api/ApiResult.php
+++ includes/api/ApiResult.php
@@ -315,31 +315,3 @@ class ApiResult extends ApiBase {
 		return __CLASS__ . ': $Id$';
 	}
 }
-
-/* For compatibility with PHP versions < 5.1.0, define our own array_intersect_key function. */
-if ( !function_exists( 'array_intersect_key' ) ) {
-	function array_intersect_key( $isec, $keys ) {
-		$argc = func_num_args();
-
-		if ( $argc > 2 ) {
-			for ( $i = 1; $isec && $i < $argc; $i++ ) {
-				$arr = func_get_arg( $i );
-
-				foreach ( array_keys( $isec ) as $key ) {
-					if ( !isset( $arr[$key] ) )
-						unset( $isec[$key] );
-				}
-			}
-
-			return $isec;
-		} else {
-			$res = array();
-			foreach ( array_keys( $isec ) as $key ) {
-				if ( isset( $keys[$key] ) )
-					$res[$key] = $isec[$key];
-			}
-
-			return $res;
-		}
-	}
-}
