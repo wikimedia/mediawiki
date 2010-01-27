fd93e5890425ca07876f6048a56bf01e7a601485
Tim Starling <tstarling@users.mediawiki.org>
2010-01-27 05:17:26 +0000

    * Removed JS2 stuff from jquery.js. If there's anything we really need from it, it can be re-added to wikibits.js, but that can wait for the extension analysis which I'm going to do next. * Added Ryan Grove's jsmin.php and wrote a maintenance script wrapper called minify.php * Added a makefile to rebuild skins/common * Rebuilt jquery.min.js

--- skins/common/jquery.js
+++ skins/common/jquery.js
@@ -4377,71 +4377,8 @@ jQuery.each([ "Height", "Width" ], function(i, name){
 });
 })();
 
-/* JavaScript for MediaWIki JS2 */
-
 /**
- * This is designed to be directly compatible with (and is essentially taken
- * directly from) the mv_embed code for bringing internationalized messages into
- * the JavaScript space. As such, if we get to the point of merging that stuff
- * into the main branch this code will be uneeded and probably cause issues.
- */
-
-/**
- * Mimics the no-conflict method used by the js2 stuff
+ * Add a suitable MW-specific alias
  */
 $j = jQuery.noConflict();
-/**
- * Provides js2 compatible mw functions
- */
-if( typeof mw == 'undefined' || !mw ){
-   mw = { };
-   /**
-    * Provides js2 compatible onload hook
-    * @param func Function to call when ready
-    */
-   mw.ready = function( func ) {
-       $j(document).ready( func );
-   }
-   // Define a dummy mw.load function:
-   mw.load = function( deps, callback ) { callback(); };
-   
-   // Deinfe a dummy mw.loadDone function: 
-   mw.loadDone = function( className ) { };
-   
-   // Creates global message object if not already in existence
-	if ( !gMsg ) var gMsg = {};
-   
-	/**
-	 * Caches a list of messages for later retrieval
-	 * @param {Object} msgSet Hash of key:value pairs of messages to cache
-	 */	 
-	mw.addMessages = function ( msgSet ){
-		for ( var i in msgSet ){
-			gMsg[ i ] = msgSet[i];
-		}
-	}
-	/**
-	 * Retieves a message from the global message cache, performing on-the-fly
-	 * replacements using MediaWiki message syntax ($1, $2, etc.)
-	 * @param {String} key Name of message as it is in MediaWiki
-	 * @param {Array} args Array of replacement arguments
-	 */
-	function gM( key, args ) {
-		var ms = '';	
-		if ( key in gMsg ) {
-			ms = gMsg[ key ];
-			if ( typeof args == 'object' || typeof args == 'array' ) {
-				for ( var v in args ){
-					var rep = '\$'+ ( parseInt(v) + 1 );
-					ms = ms.replace( rep, args[v]);
-				}
-			} else if ( typeof args =='string' || typeof args =='number' ) {
-				ms = ms.replace( /\$1/, args );
-			}
-			return ms;
-		} else {
-			return '[' + key + ']';
-		}
-	}
-   
-}
+
