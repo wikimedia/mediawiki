f443f2ea626b7c7f28b6b74a51b12a2546b29ef1
Sam Reed <reedy@users.mediawiki.org>
2010-01-28 07:15:49 +0000

    Minor followup to r61609
    If we're checking the key is *, no point reusing it, as we know what it is. Simplfy bug link/reference

--- includes/api/ApiFormatYaml_spyc.php
+++ includes/api/ApiFormatYaml_spyc.php
@@ -185,8 +185,8 @@ class Spyc {
 			else
 				$string = $spaces . "-\n";
 		} else {
-			 if ($key == '*') //https://bugzilla.wikimedia.org/show_bug.cgi?id=21922 - Quote asterix used as keys
-				$key = "'{$key}'";
+			 if ($key == '*') //bug 21922 - Quote asterix used as keys
+				$key = "'*'";
 		
 			// It's mapped
 			if ( $value !== '' && !is_null( $value ) )
