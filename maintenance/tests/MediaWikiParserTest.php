1222b9c86d4c0c33039fbc38319ed9a546fed2f4
Mark A. Hershberger <mah@users.mediawiki.org>
2010-02-05 04:01:15 +0000

    PHPUnit now sees individual parser tests as separate tests.  This involved changing parserTests.inc slightly to create an iterator that reads parserTests.txt — separating the reading of the file from the execution of the tests.
    PHPUnit still doesn't see individual assertions, but that is less
    important right now.
    
    Tested maintenance/parserTests.php against maintenance/parserTests.txt
    and extensions/Cite/citeParserTests.txt to verify that the parser tests
    still worked as expected.

--- maintenance/tests/MediaWikiParserTest.php
+++ maintenance/tests/MediaWikiParserTest.php
@@ -2,45 +2,109 @@
 
 global $IP;
 define( "NO_COMMAND_LINE", 1 );
+define( "PARSER_TESTS", "$IP/maintenance/parserTests.txt" );
+
 require_once( "$IP/maintenance/parserTests.inc" );
-require_once( "ImageFunctions.php" );
-require_once( "ProxyTools.php" );
-require_once( "ObjectCache.php" );
 
-class PTShell extends ParserTest {
+class PHPUnitTestRecorder extends TestRecorder {
 
-	private $cb;
+	function record( $test, $result ) {
+		$this->total++;
+		$this->success += $result;
 
-	function setCallback( $cb ) {
-		$this->cb = $cb;
 	}
 
-	function showTesting( $desc ) {
-	}
-
-	function showRunFile( $path ) {
-	}
-
-	function showSuccess( $desc ) {
-		$this->cb->assertTrue( true, $desc );
-		echo "PASSED: $desc\n";
-		return true;
-	}
-
-	function showFailure( $desc, $expected, $got ) {
-		/* $this->cb->assertEquals( $expected, $got, $desc ); */
-		echo "FAILED: $desc\n";
-		echo "got: $got\n";
-		echo "expected: $expected\n";
-	}
+	function reportPercentage( $success, $total ) {}
 }
 
-class MediaWikiParserTest extends PHPUnit_Framework_TestCase {
-	private $parserTester;
+class MediaWikiParserTestSuite extends PHPUnit_Framework_TestSuite {
+#implements PHPUnit_Framework_SelfDescribing {
+	static private $count;
+	static public $parser;
+	static public $iter;
+
+	public static function suite() {
+        $suite = new PHPUnit_Framework_TestSuite();
+
+		self::$iter = new TestFileIterator( PARSER_TESTS );
+
+		foreach(self::$iter as $i => $test) {
+			$suite->addTest(new ParserUnitTest($i, $test['test']));
+			self::$count++;
+		}
+		unset($tests);
+
+		self::$parser = new PTShell;
+		self::$iter->setParser(self::$parser);
+		self::$parser->recorder->start();
+		self::$parser->setupDatabase();
+		self::$iter->rewind();
+	/* } */
+	/* function setUp() { */
+		global $wgParser,  $wgParserConf, $IP, $messageMemc, $wgMemc, $wgDeferredUpdateList,
+                  $wgUser, $wgLang, $wgOut, $wgRequest, $wgStyleDirectory, $wgEnableParserCache,
+                  $wgMessageCache, $wgUseDatabaseMessages, $wgMsgCacheExpiry, $parserMemc,
+                  $wgNamespaceAliases, $wgNamespaceProtection, $wgLocalFileRepo,
+                  $wgNamespacesWithSubpages, $wgThumbnailScriptPath, $wgScriptPath,
+                  $wgArticlePath, $wgStyleSheetPath, $wgScript, $wgStylePath;
+
+		$wgScript = '/index.php';
+		$wgScriptPath = '/';
+		$wgArticlePath = '/wiki/$1';
+		$wgStyleSheetPath = '/skins';
+		$wgStylePath = '/skins';
+		$wgThumbnailScriptPath = false;
+		$wgLocalFileRepo = array(
+			'class' => 'LocalRepo',
+			'name' => 'local',
+			'directory' => '',
+			'url' => 'http://example.com/images',
+			'hashLevels' => 2,
+			'transformVia404' => false,
+		);
+                $wgNamespaceProtection[NS_MEDIAWIKI] = 'editinterface';
+		$wgNamespaceAliases['Image'] = NS_FILE;
+		$wgNamespaceAliases['Image_talk'] = NS_FILE_TALK;
+
+
+		$wgEnableParserCache = false;
+		$wgDeferredUpdateList = array();
+		$wgMemc =& wfGetMainCache();
+		$messageMemc =& wfGetMessageCacheStorage();
+		$parserMemc =& wfGetParserCacheStorage();
+
+		$wgContLang = new StubContLang;
+		$wgUser = new StubUser;
+		$wgLang = new StubUserLang;
+		$wgOut = new StubObject( 'wgOut', 'OutputPage' );
+		$wgParser = new StubObject( 'wgParser', $wgParserConf['class'], array( $wgParserConf ) );
+		$wgRequest = new WebRequest;
+
+		$wgMessageCache = new StubObject( 'wgMessageCache', 'MessageCache',
+										  array( $messageMemc, $wgUseDatabaseMessages,
+												 $wgMsgCacheExpiry, wfWikiID() ) );
+		if( $wgStyleDirectory === false) $wgStyleDirectory   = "$IP/skins";
+
+		return $suite;
+	}
+
+	public function tearDown() {
+		$this->teardownDatabase();
+		$this->recorder->report();
+		$this->recorder->end();
+		$this->teardownUploadDir($this->uploadDir);
+	}
+
+	public function count() {return self::$count;}
+
+	public function toString() {
+		return "MediaWiki Parser Tests";
+	}
+
+
 	private $db;
 	private $uploadDir;
 	private $keepUploads;
-
 	/**
 	 * Remove the dummy uploads directory
 	 */
@@ -132,88 +196,84 @@ class MediaWikiParserTest extends PHPUnit_Framework_TestCase {
 		copy( "$IP/skins/monobook/headbg.jpg", "$dir/0/09/Bad.jpg" );
 		return $dir;
 	}
-
-	function setUp() {
-		global $wgParser,  $wgParserConf, $IP, $messageMemc, $wgMemc, $wgDeferredUpdateList,
-			$wgUser, $wgLang, $wgOut, $wgRequest, $wgStyleDirectory,  $wgEnableParserCache, $wgMessageCache,
-			$wgUseDatabaseMessages, $wgMsgCacheExpiry, $parserMemc, $wgNamespaceAliases, $wgNamespaceProtection,
-			$wgLocalFileRepo, $wgNamespacesWithSubpages, $wgThumbnailScriptPath, $wgScriptPath,
-			$wgArticlePath, $wgStyleSheetPath, $wgScript, $wgStylePath;
-
-		$wgScript = '/index.php';
-		$wgScriptPath = '/';
-		$wgArticlePath = '/wiki/$1';
-		$wgStyleSheetPath = '/skins';
-		$wgStylePath = '/skins';
-		$wgThumbnailScriptPath = false;
-		$this->uploadDir = $this->setupUploadDir();
-		$wgLocalFileRepo = array(
-			'class' => 'LocalRepo',
-			'name' => 'local',
-			'directory' => $this->uploadDir,
-			'url' => 'http://example.com/images',
-			'hashLevels' => 2,
-			'transformVia404' => false,
-		);
-		//$wgNamespacesWithSubpages = array( 0 => isset( $opts['subpage'] ) );
-        $wgNamespaceProtection[NS_MEDIAWIKI] = 'editinterface';
-		$wgNamespaceAliases['Image'] = NS_FILE;
-		$wgNamespaceAliases['Image_talk'] = NS_FILE_TALK;
-
-
-		$wgEnableParserCache = false;
-		$wgDeferredUpdateList = array();
-		$wgMemc =& wfGetMainCache();
-		$messageMemc =& wfGetMessageCacheStorage();
-		$parserMemc =& wfGetParserCacheStorage();
-
-		$wgContLang = new StubContLang;
-		$wgUser = new StubUser;
-		$wgLang = new StubUserLang;
-		$wgOut = new StubObject( 'wgOut', 'OutputPage' );
-		$wgParser = new StubObject( 'wgParser', $wgParserConf['class'], array( $wgParserConf ) );
-		$wgRequest = new WebRequest;
-
-		$wgMessageCache = new StubObject( 'wgMessageCache', 'MessageCache',
-										  array( $messageMemc, $wgUseDatabaseMessages, $wgMsgCacheExpiry, wfWikiID() ) );
-		if( $wgStyleDirectory === false) $wgStyleDirectory   = "$IP/skins";
-
-		$this->parserTester = new PTShell();
-		$this->parserTester->setCallback( $this );
-
-		/* global $wgDBtype, $wgDBserver, $wgDBname, $wgDBuser, $wgDBpassword, $wgDBport, $wgDBmwschema, $wgDBts2chema; */
-		/* $this->db['type'] = $wgDBtype; */
-		/* $this->db['server'] = $wgDBserver; */
-		/* $this->db['name'] = $wgDBname; */
-		/* $this->db['user'] = $wgDBuser; */
-		/* $this->db['password'] = $wgDBpassword; */
-		/* $this->db['port'] = $wgDBport; */
-		/* $this->db['mwschema'] = $wgDBmwschema; */
-		/* $this->db['ts2schema'] = $wgDBts2chema; */
-	}
-
-	function tearDown() {
-		$this->teardownUploadDir($this->uploadDir);
-		/* $db = wfGetDB( DB_MASTER ); */
-		/* $db->close(); */
-		/* global $wgDBtype, $wgDBserver, $wgDBname, $wgDBuser, $wgDBpassword, $wgDBport, $wgDBmwschema, $wgDBts2chema; */
-
-		/* $wgDBtype = $this->db['type']; */
-		/* $wgDBserver = $this->db['server']; */
-		/* $wgDBname = $this->db['name']; */
-		/* $wgDBuser = $this->db['user']; */
-		/* $wgDBpassword = $this->db['password']; */
-		/* $wgDBport = $this->db['port']; */
-		/* $wgDBmwschema = $this->db['mwschema']; */
-		/* $wgDBts2chema = $this->db['ts2schema']; */
-
-	}
-
-
-	function testParser() {
-		global $IP;
-
-		$this->parserTester->runTestsFromFiles( array( "$IP/maintenance/parserTests.txt" ) );
-	}
 }
+
+class ParserUnitTest extends PHPUnit_Framework_TestCase {
+	private $number = 0;
+	private $test = "";
+
+	public function __construct($number, $test) {
+		$this->number = $number;
+		$this->test = $test;
+	}
+
+	function count() {return 1;}
+
+	public function run(PHPUnit_Framework_TestResult $result = NULL) {
+        PHPUnit_Framework_Assert::resetCount();
+        if ($result === NULL) {
+            $result = new PHPUnit_Framework_TestResult;
+        }
+
+		$t = MediaWikiParserTestSuite::$iter->current();
+		$k = MediaWikiParserTestSuite::$iter->key();
+
+		if(!MediaWikiParserTestSuite::$iter->valid()) {
+			return;
+		}
+
+		// The only way this should happen is if the parserTest.txt
+		// file were modified while the script is running.
+		if($k != $this->number) {
+			$i = $this->number;
+			wfDie("I got confused!\n");
+		}
+
+		$result->startTest($this);
+		PHPUnit_Util_Timer::start();
+
+		$r = false;
+		try {
+			$r = MediaWikiParserTestSuite::$parser->runTest(
+				$t['test'], $t['input'], $t['result'], $t['options'], $t['config']
+  			);
+			PHPUnit_Framework_Assert::assertTrue(true, $t['test']);
+		}
+		catch (PHPUnit_Framework_AssertionFailedError $e) {
+			$result->addFailure($this, $e, PHPUnit_Util_Timer::stop());
+		}
+		catch (Exception $e) {
+			$result->addError($this, $e, PHPUnit_Util_Timer::stop());
+		}
+		PHPUnit_Framework_Assert::assertTrue(true, $t['test']);
+
+		$result->endTest($this, PHPUnit_Util_Timer::stop());
+
+		MediaWikiParserTestSuite::$parser->recorder->record($t['test'], $r);
+		MediaWikiParserTestSuite::$iter->next();
+		$this->addToAssertionCount(PHPUnit_Framework_Assert::getCount());
+
+		return $result;
+	}
+
+}
+
+class PTShell extends ParserTest {
+	function showTesting( $desc ) {
+	}
+
+	function showRunFile( $path ) {
+	}
+
+	function showSuccess( $desc ) {
+		PHPUnit_Framework_Assert::assertTrue(true, $desc);
+		return true;
+	}
+
+	function showFailure( $desc, $expected, $got ) {
+		PHPUnit_Framework_Assert::assertEquals($expected, $got, $desc);
+	}
+
+}
+
 
