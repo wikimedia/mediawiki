596afe56faee5d221c45a530488384407fe47511
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-02-07 16:42:50 +0000

    * Modified MediaWiki_Setup to use the normal mechanism to get a database connection * Include commandLine.inc from bootstrap.php to get admin credentials * Marked SearchEngineTest as stub and exclude it

--- maintenance/tests/MediaWiki_Setup.php
+++ maintenance/tests/MediaWiki_Setup.php
@@ -1,45 +1,40 @@
 <?php
 
 abstract class MediaWiki_Setup extends PHPUnit_Framework_TestCase {
-	/**
-	 * @param string $serverType
-	 * @param array $tables
-	 */
-	protected function buildTestDatabase( $tables ) {
-		global $testOptions, $wgDBprefix, $wgDBserver, $wgDBadminuser, $wgDBadminpassword, $wgDBname;
-		$this->markTestIncomplete("This test requires DB admin user credentials.");
-		$wgDBprefix = 'parsertest_';
 
-		$db = new DatabaseMysql(
-			$wgDBserver,
-			$wgDBadminuser,
-			$wgDBadminpassword,
-			$wgDBname );
+	protected function buildTestDatabase( $tables ) {
+		global $wgDBprefix;
+
+		$db = wfGetDB( DB_MASTER );
+		$oldTableNames = array();
+		foreach( $tables as $table )
+			$oldTableNames[$table] = $db->tableName( $table );
+		$db->tablePrefix( 'parsertest_' );
+
 		if( $db->isOpen() ) {
-			if (!(strcmp($db->getServerVersion(), '4.1') < 0 and stristr($db->getSoftwareLink(), 'MySQL'))) {
+			if ( !( strcmp( $db->getServerVersion(), '4.1' ) < 0 and $db->getType() == 'mysql' ) ) {
 				# Database that supports CREATE TABLE ... LIKE
-				foreach ($tables as $tbl) {
+				foreach ( $tables as $tbl ) {
 					$newTableName = $db->tableName( $tbl );
-					#$tableName = $this->oldTableNames[$tbl];
-					$tableName = $tbl;
-					$db->query("CREATE TEMPORARY TABLE $newTableName (LIKE $tableName)");
+					$tableName = $oldTableNames[$tbl];
+					$db->query( "CREATE TEMPORARY TABLE $newTableName (LIKE $tableName)" );
 				}
 			} else {
 				# Hack for MySQL versions < 4.1, which don't support
 				# "CREATE TABLE ... LIKE". Note that
 				# "CREATE TEMPORARY TABLE ... SELECT * FROM ... LIMIT 0"
 				# would not create the indexes we need....
-				foreach ($tables as $tbl) {
-					$res = $db->query("SHOW CREATE TABLE $tbl");
+				foreach ( $tables as $tbl ) {
+					$res = $db->query( "SHOW CREATE TABLE $tbl" );
 					$row = $db->fetchRow($res);
 					$create = $row[1];
-					$create_tmp = preg_replace('/CREATE TABLE `(.*?)`/', 'CREATE TEMPORARY TABLE `'
-						. $wgDBprefix . '\\1`', $create);
+					$create_tmp = preg_replace( '/CREATE TABLE `(.*?)`/', 'CREATE TEMPORARY TABLE `'
+						. $wgDBprefix . '\\1`', $create );
 					if ($create === $create_tmp) {
 						# Couldn't do replacement
 						wfDie( "could not create temporary table $tbl" );
 					}
-					$db->query($create_tmp);
+					$db->query( $create_tmp );
 				}
 
 			}
