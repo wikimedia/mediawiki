314dc49af8e067bba50c81e595e8c56812d262fa
Roan Kattouw <catrope@users.mediawiki.org>
2010-02-09 16:58:53 +0000

    Fix up r62154: don't throw a JS error when $j is undefined, and bump $wgStyleVersion

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1686,7 +1686,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '265';
+$wgStyleVersion = '266';
 
 
 # Server-side caching:
