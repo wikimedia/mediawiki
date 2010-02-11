9234b425885a1e3797de6a2a36d6a0b24e98ba07
Aryeh Gregor <simetrical@users.mediawiki.org>
2010-02-11 17:25:10 +0000

    War on xml:lang
    There is no reason to use this in HTML in addition to the lang
    attribute.  No spec requires it (AFAIK) and it's completely redundant.

--- config/Installer.php
+++ config/Installer.php
@@ -112,7 +112,7 @@ $ourdb['oracle'] = array(
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<meta name="robots" content="noindex,nofollow"/>
