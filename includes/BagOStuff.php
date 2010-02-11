8df2fd834749eae0928f68cb144268185508cef4
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-02-11 12:33:17 +0000

    Implement keys() for APCBagOStuff

--- includes/BagOStuff.php
+++ includes/BagOStuff.php
@@ -349,7 +349,7 @@ class SqlBagOStuff extends BagOStuff {
 
 	public function keys() {
 		$db = $this->getDB();
-		$res = $db->select( 'objectcache', array( 'keyname' ), false, __METHOD__ );	
+		$res = $db->select( 'objectcache', array( 'keyname' ), false, __METHOD__ );
 		$result = array();
 		foreach ( $res as $row ) {
 			$result[] = $row->keyname;
@@ -527,6 +527,16 @@ class APCBagOStuff extends BagOStuff {
 	public function delete( $key, $time = 0 ) {
 		apc_delete( $key );
 		return true;
+	}
+
+	public function keys() {
+		$info = apc_cache_info( 'user' );
+		$list = $info['cache_list'];
+		$keys = array();
+		foreach( $list as $entry ) {
+			$keys[] = $entry['info'];
+		}
+		return $keys;
 	}
 }
 
