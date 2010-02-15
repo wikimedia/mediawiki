92c45ae6665adb60fab2625aece63366e634babb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-02-15 20:08:39 +0000

    Revert r62421. Removing "set_time_limit( 0 );" causes all kinds of time out issues running command line scripts in both maintenance/ and with cli scripts in extensions.

--- maintenance/Maintenance.php
+++ maintenance/Maintenance.php
@@ -638,6 +638,7 @@ abstract class Maintenance {
 		}
 
 		$wgShowSQLErrors = true;
+		@set_time_limit( 0 );
 		ini_set( 'memory_limit', $this->memoryLimit() );
 
 		$wgProfiling = false; // only for Profiler.php mode; avoids OOM errors
