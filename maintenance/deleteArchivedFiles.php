ffc662b09647501b4d349b3379eb76f35f608455
Mark A. Hershberger <mah@users.mediawiki.org>
2010-02-22 01:12:50 +0000

    follow up r62800 fix typo

--- maintenance/deleteArchivedFiles.php
+++ maintenance/deleteArchivedFiles.php
@@ -38,7 +38,7 @@ class DeleteArchivedFiles extends Maintenance {
 			$this->output( "Use --delete to actually confirm this script\n" );
 			return;
 		}
-		$force = $this->hasOption( 'force ');
+		$force = $this->hasOption( 'force' );
 		# Data should come off the master, wrapped in a transaction
 		$dbw = wfGetDB( DB_MASTER );
 		$dbw->begin();
