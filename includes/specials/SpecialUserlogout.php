7726ba8816c0afadd0decf362173868a25eea74d
Max Semenik <maxsem@users.mediawiki.org>
2010-02-25 20:29:38 +0000

    Backported r62967 and r62968 to 1.16: (bug 17790) Users instantly logged off on HughesNet

--- includes/specials/SpecialUserlogout.php
+++ includes/specials/SpecialUserlogout.php
@@ -10,6 +10,16 @@
 function wfSpecialUserlogout() {
 	global $wgUser, $wgOut;
 
+	/**
+	 * Some satellite ISPs use broken precaching schemes that log people out straight after
+	 * they're logged in (bug 17790). Luckily, there's a way to detect such requests.
+	 */
+	if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], '&amp;' ) !== false ) {
+		wfDebug( "Special:Userlogout request {$_SERVER['REQUEST_URI']} looks suspicious, denying.\n" );
+		wfHttpError( 400, wfMsg( 'loginerror' ), wfMsg( 'suspicious-userlogout' ) );
+		return;
+	}
+	
 	$oldName = $wgUser->getName();
 	$wgUser->logout();
 	$wgOut->setRobotPolicy( 'noindex,nofollow' );
