634b81f8d0fc3058e54256f1d49061ee1998c9a8
Platonides <platonides@users.mediawiki.org>
2010-03-03 18:22:26 +0000

    Follow-up r63213. Move it to wfEscapeShellArg, which is the proper place.

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -1460,13 +1460,17 @@ function wfEscapeShellArg( ) {
 				}
 				$delim = !$delim;
 			}
+			
 			// Double the backslashes before the end of the string, because
 			// we will soon add a quote
 			$m = array();
 			if ( preg_match( '/^(.*?)(\\\\+)$/', $arg, $m ) ) {
 				$arg = $m[1] . str_replace( '\\', '\\\\', $m[2] );
 			}
-
+			
+			// The caret is also an special character
+			$arg = str_replace( "^", "^^", $arg );
+			
 			// Add surrounding quotes
 			$retVal .= '"' . $arg . '"';
 		} else {
