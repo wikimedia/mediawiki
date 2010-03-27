9990ba163eb596adb867355ffc6a1dba16bfdd9b
Happy-melon <happy-melon@users.mediawiki.org>
2010-03-27 15:05:56 +0000

    Per r64228 CR: make the check a static method in IPBlockForm to reduce duplication.

--- includes/api/ApiUnblock.php
+++ includes/api/ApiUnblock.php
@@ -64,18 +64,10 @@ class ApiUnblock extends ApiBase {
 		}
 		# bug 15810: blocked admins should have limited access here
 		if( $wgUser->isBlocked() ){
-			$user = User::newFromName( $params['user'] );
-			if( $user instanceof User
-				&& $user->getId() == $wgUser->getId() )
-			{
-				# User is trying to unblock themselves
-				if( !$wgUser->isAllowed( 'unblockself' ) ){
-					$this->dieUsageMsg( array( 'ipbnounblockself' ) );
-				}
-			} else {
-				# User is trying to block/unblock someone else
-				$this->dieUsageMsg( array( 'ipbblocked' ) );
-			}
+			$status = IPBlockForm::checkUnblockSelf( $params['user'] );
+			if( $status !== true ){
+				$this->dieUsageMsg( array( $status ) );
+			} 
 		}
 
 		$id = $params['id'];
