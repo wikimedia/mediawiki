48d9d6cb66c107e244372d5dc16320bd2b33ca92
Tim Starling <tstarling@users.mediawiki.org>
2010-04-09 12:57:34 +0000

    revert r64792

--- includes/Title.php
+++ includes/Title.php
@@ -826,9 +826,6 @@ class Title {
 			$pref = $wgContLang->getPreferredVariant(false);
 			if($pref != $wgContLang->getCode())
 				$variant = $pref;
-		} elseif ( $variant == $wgContLang->getCode() ) {
-			// Don't use $wgVariantArticlePath for the main code
-			$variant = false;
 		}
 
 		if ( $this->isExternal() ) {
