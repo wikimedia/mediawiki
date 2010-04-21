aaada6f1c61f52ae1132f5357e75114b38460aa5
Trevor Parscal <tparscal@users.mediawiki.org>
2010-04-21 13:39:20 +0000

    Style bump for r65380.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1682,7 +1682,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '270';
+$wgStyleVersion = '271';
 
 
 # Server-side caching:
