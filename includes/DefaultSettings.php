5bf6ee4bb668174f3e1fba9c232f3035fef331f6
Trevor Parscal <tparscal@users.mediawiki.org>
2010-04-21 13:46:06 +0000

    Fixed the font size of pre elements, which was previously too small.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1682,7 +1682,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '271';
+$wgStyleVersion = '272';
 
 
 # Server-side caching:
