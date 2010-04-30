373ae16b857af3f8b7110598aaa26ee527398d00
Jack Phoenix <ashley@users.mediawiki.org>
2010-04-30 22:21:36 +0000

    more follow-up to r65715: coding style tweaks, etc.

--- maintenance/tests/RunSeleniumTests.php
+++ maintenance/tests/RunSeleniumTests.php
@@ -1,49 +1,50 @@
 <?php
- /**
-  * Copyright (C) Wikimedia Deuschland, 2009
-  * Authors Hallo Welt! Medienwerkstatt GmbH
-  * Authors Markus Glaser
-  *
-  * This program is free software; you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation; either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License along
-  * with this program; if not, write to the Free Software Foundation, Inc.,
-  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-  * http://www.gnu.org/copyleft/gpl.html
-  *
-  */
+/**
+ * @file
+ * @ingroup Maintenance
+ * @copyright Copyright © Wikimedia Deuschland, 2009
+ * @author Hallo Welt! Medienwerkstatt GmbH
+ * @author Markus Glaser
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
-define( "MEDIAWIKI", true );
-define( "SELENIUMTEST", true);
+define( 'MEDIAWIKI', true );
+define( 'SELENIUMTEST', true );
 
-//command line only
+// Command line only
 $wgSeleniumTestsRunMode = 'cli';
 if( $wgSeleniumTestsRunMode == 'cli' && php_sapi_name() != 'cli' ) {
 	echo 'Must be run from the command line.';
 	die( -1 );
 }
-//include path and installation instructions
+// include path and installation instructions
 
-//URL: http://localhost/tests/RunSeleniumTests.php
-//set_include_path(get_include_path() . PATH_SEPARATOR . './PEAR/');
+// URL: http://localhost/tests/RunSeleniumTests.php
+//set_include_path( get_include_path() . PATH_SEPARATOR . './PEAR/' );
 
 // Hostname of selenium server
-$wgSeleniumTestsSeleniumHost          = "http://localhost";
+$wgSeleniumTestsSeleniumHost = 'http://localhost';
 
 // URL of the wiki to be tested.
-$wgSeleniumTestsWikiUrl               = 'http://localhost';
+$wgSeleniumTestsWikiUrl = 'http://localhost';
 
 // Wiki login. Used by Selenium to log onto the wiki
-$wgSeleniumTestsWikiUser      = "WikiSysop";
-$wgSeleniumTestsWikiPassword  = "password";
+$wgSeleniumTestsWikiUser      = 'WikiSysop';
+$wgSeleniumTestsWikiPassword  = 'password';
 
 // Common browsers on Windows platform
 // Use the *chrome handler in order to be able to test file uploads
@@ -69,12 +70,16 @@ require_once 'selenium/SeleniumTestSuite.php';
 require_once 'selenium/SeleniumTestCase.php';
 
 $result = new PHPUnit_Framework_TestResult;
-switch ($wgSeleniumTestsRunMode) {
-    case 'html' : $logger = new SeleniumTestHTMLLogger; break;
-    case 'cli'  : $logger = new SeleniumTestConsoleLogger; break;
+switch ( $wgSeleniumTestsRunMode ) {
+	case 'html':
+		$logger = new SeleniumTestHTMLLogger;
+		break;
+	case 'cli':
+		$logger = new SeleniumTestConsoleLogger;
+		break;
 }
-$result->addListener(new SeleniumTestListener($logger));
 
+$result->addListener( new SeleniumTestListener( $logger ) );
 
 $wgSeleniumTestSuites = array();
 
@@ -82,12 +87,11 @@ $wgSeleniumTestSuites = array();
 #include_once '<your tests>';
 
 // Here, you can override standard setting
-if (file_exists('LocalSeleniumSettings.php'))
-{
+if ( file_exists( 'LocalSeleniumSettings.php' ) ) {
 	include_once 'LocalSeleniumSettings.php';
 }
 
 // run tests
-foreach ($wgSeleniumTestSuites as $suite)
-    $suite->run($result);
-?>
+foreach ( $wgSeleniumTestSuites as $suite ) {
+	$suite->run( $result );
+}
--- maintenance/tests/selenium/Selenium.php
+++ maintenance/tests/selenium/Selenium.php
@@ -4,53 +4,47 @@
  * This is implemented as a singleton.
  */
 
-if (!defined('MEDIAWIKI') || !defined('SELENIUMTEST')) {
+if ( !defined( 'MEDIAWIKI' ) || !defined( 'SELENIUMTEST' ) ) {
 	echo "This script cannot be run standalone";
-	exit(1);
+	exit( 1 );
 }
 
-class Selenium extends Testing_Selenium
-{
+class Selenium extends Testing_Selenium {
 	protected static $_instance = null;
 	public $isStarted = false;
-	public static function getInstance()
-	{
+
+	public static function getInstance() {
 		global $wgSeleniumTestsBrowsers, $wgSeleniumTestsSeleniumHost, $wgSeleniumTestsUseBrowser;
-		if (null === self::$_instance)
-		{
-			self::$_instance = new self($wgSeleniumTestsBrowsers[$wgSeleniumTestsUseBrowser], $wgSeleniumTestsSeleniumHost);
+		if ( null === self::$_instance ) {
+			self::$_instance = new self( $wgSeleniumTestsBrowsers[$wgSeleniumTestsUseBrowser], $wgSeleniumTestsSeleniumHost );
 		}
 		return self::$_instance;
 	}
 
-	public function start()
-	{
+	public function start() {
 		global $wgSeleniumTestsBrowsers, $wgSeleniumTestsSeleniumHost;
 		parent::start();
 		$this->isStarted = true;
 	}
 
-	public function stop()
-	{
+	public function stop() {
 		parent::stop();
 		$this->isStarted = false;
 	}
 
-	public function login()
-	{
+	public function login() {
 		global $wgSeleniumTestsWikiUser, $wgSeleniumTestsWikiPassword, $wgSeleniumTestsWikiUrl;
 
-		$this->open($wgSeleniumTestsWikiUrl.'/index.php?title=Special:Userlogin');
-		$this->type("wpName1", $wgSeleniumTestsWikiUser);
-		$this->type("wpPassword1", $wgSeleniumTestsWikiPassword);
-		$this->click("//input[@id='wpLoginAttempt']");
-		$value = $this->doCommand('assertTitle', array('Anmeldung erfolgreich*'));
+		$this->open( $wgSeleniumTestsWikiUrl . '/index.php?title=Special:Userlogin' );
+		$this->type( 'wpName1', $wgSeleniumTestsWikiUser );
+		$this->type( 'wpPassword1', $wgSeleniumTestsWikiPassword );
+		$this->click( "//input[@id='wpLoginAttempt']" );
+		$value = $this->doCommand( 'assertTitle', array( 'Anmeldung erfolgreich*' ) );
 	}
 
-	public function loadPage($title, $action)
-	{
+	public function loadPage( $title, $action ) {
 		global $wgSeleniumTestsWikiUrl;
-		$this->open($wgSeleniumTestsWikiUrl.'/index.php?title='.$title.'&action='.$action);
+		$this->open( $wgSeleniumTestsWikiUrl . '/index.php?title=' . $title . '&action=' . $action );
 	}
 
 	// Prevent external cloning
