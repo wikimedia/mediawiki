7ba1eae0eb6942c6c38fee24042db96e39332f3f
Jack Phoenix <ashley@users.mediawiki.org>
2010-04-30 22:06:13 +0000

    follow-up to r65715: convert spaces to tabs for indentation, as per our coding standards. no functional changes.

--- maintenance/tests/selenium/SeleniumTestConsoleLogger.php
+++ maintenance/tests/selenium/SeleniumTestConsoleLogger.php
@@ -6,24 +6,24 @@ if (!defined('MEDIAWIKI') || !defined('SELENIUMTEST')) {
 
 class SeleniumTestConsoleLogger
 {
-    public function __construct()
-    {
-        // Prepare testsuite for immediate output
-        @ini_set('zlib.output_compression', 0);
-        @ini_set('implicit_flush', 1);
-        for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
-        ob_implicit_flush(1);
-        
-    }
+	public function __construct()
+	{
+		// Prepare testsuite for immediate output
+		@ini_set('zlib.output_compression', 0);
+		@ini_set('implicit_flush', 1);
+		for ($i = 0; $i < ob_get_level(); $i++) { ob_end_flush(); }
+		ob_implicit_flush(1);
+		
+	}
 
-    public function write($message, $mode = false)
-    {
-        $out .= '';
-        //if ($mode == MW_TESTLOGGER_RESULT_OK) $out .= '<font color="green">';
-        $out .=  htmlentities($message);
-        //if ($mode == MW_TESTLOGGER_RESULT_OK) $out .= '</font>';
-        if ($mode != MW_TESTLOGGER_CONTINUE_LINE) $out .= "\n";
+	public function write($message, $mode = false)
+	{
+		$out .= '';
+		//if ($mode == MW_TESTLOGGER_RESULT_OK) $out .= '<font color="green">';
+		$out .=  htmlentities($message);
+		//if ($mode == MW_TESTLOGGER_RESULT_OK) $out .= '</font>';
+		if ($mode != MW_TESTLOGGER_CONTINUE_LINE) $out .= "\n";
 
-        echo $out;
-    }
+		echo $out;
+	}
 }
--- maintenance/tests/selenium/SeleniumTestListener.php
+++ maintenance/tests/selenium/SeleniumTestListener.php
@@ -6,69 +6,69 @@ if (!defined('MEDIAWIKI') || !defined('SELENIUMTEST')) {
 
 class SeleniumTestListener implements PHPUnit_Framework_TestListener
 {
-    private $logger;
-    private $tests_ok = 0;
-    private $tests_failed = 0;
+	private $logger;
+	private $tests_ok = 0;
+	private $tests_failed = 0;
 
-    public function __construct($loggerInstance)
-    {
-        $this->logger = $loggerInstance;
-    }
+	public function __construct($loggerInstance)
+	{
+		$this->logger = $loggerInstance;
+	}
 
-    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        $this->logger->write("Error: ".$e->getMessage());
-        $this->tests_failed++;
-    }
+	public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+	{
+		$this->logger->write("Error: ".$e->getMessage());
+		$this->tests_failed++;
+	}
 
-    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
-    {
-        $this->logger->write("Failed: ".$e->getMessage());
-        $this->tests_failed++;
+	public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+	{
+		$this->logger->write("Failed: ".$e->getMessage());
+		$this->tests_failed++;
 
-    }
+	}
 
-    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        $this->logger->write("Incomplete.");
-        $this->tests_failed++;
-    }
+	public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+	{
+		$this->logger->write("Incomplete.");
+		$this->tests_failed++;
+	}
 
-    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
-    {
-        $this->logger->write("Skipped.");
-        $this->tests_failed++;
-    }
+	public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+	{
+		$this->logger->write("Skipped.");
+		$this->tests_failed++;
+	}
 
-    public function startTest(PHPUnit_Framework_Test $test)
-    {
-        $this->logger->write("Testing ".$test->getName()." ... ", MW_TESTLOGGER_CONTINUE_LINE);
-    }
+	public function startTest(PHPUnit_Framework_Test $test)
+	{
+		$this->logger->write("Testing ".$test->getName()." ... ", MW_TESTLOGGER_CONTINUE_LINE);
+	}
 
-    public function endTest(PHPUnit_Framework_Test $test, $time)
-    {
-        if (!$test->hasFailed())
-        {
-            $this->logger->write("OK", MW_TESTLOGGER_RESULT_OK);
-            $this->tests_ok++;
-        }
-    }
+	public function endTest(PHPUnit_Framework_Test $test, $time)
+	{
+		if (!$test->hasFailed())
+		{
+			$this->logger->write("OK", MW_TESTLOGGER_RESULT_OK);
+			$this->tests_ok++;
+		}
+	}
 
-    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-        $this->logger->write("Testsuite ".$suite->getName()." started.");
-        $this->tests_ok = 0;
-    }
+	public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+	{
+		$this->logger->write("Testsuite ".$suite->getName()." started.");
+		$this->tests_ok = 0;
+	}
 
-    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-        $this->logger->write("Testsuite ".$suite->getName()." ended. OK: ".$this->tests_ok." Failed: ".$this->tests_failed);
+	public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+	{
+		$this->logger->write("Testsuite ".$suite->getName()." ended. OK: ".$this->tests_ok." Failed: ".$this->tests_failed);
 
-    }
+	}
 
-    public function statusMessage($message)
-    {
-        $this->logger->write($message);
-    }
+	public function statusMessage($message)
+	{
+		$this->logger->write($message);
+	}
 }
 
