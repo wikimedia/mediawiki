wikimedia/mediawiki

commit b2f3644102ea86bb4e4d89649eeb91d1e5c24c74

EditPage refactor and improvements. - EditPage::showEditForm broken up into task specific methods - Subclasses can indicate they can't support section mode - Standard inputs should all be now in methods they can be grabbed from by subclasses that want to re-arange things - Many more places to override and hook into to change behavior - showTextbox1 parameters changed from $classes to $customAttribs and $textoverride - showContentForm and importContentFormData added; New workflow to override the wpTextbox1 behavior to use an alternate edit form ui or handle wpTextbox1 content in an alternate way. - getActionURL added for EditPage subclasses used in places where $this->action isn't enough (ie: EditPage on special pages) Html::textarea added
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@59655 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -292,6 +292,7 @@ Hopefully we will remove this configuration var soon)
 * Allow \pagecolor and \definecolor in texvc
 * $wgTexvcBackgroundColor contains background color for texvc call
 * (bug 21574) Redirects can now have "303 See Other" HTTP status
+* EditPage refactored to allow extensions to derive new edit modes much easier.
 
 === Bug fixes in 1.16 ===
 

includes/Html.php
@@ -486,4 +486,29 @@ class Html {
 	public static function hidden( $name, $value, $attribs = array() ) {
 		return self::input( $name, $value, 'hidden', $attribs );
 	}
+
+	/**
+	 * Convenience function to produce an <input> element.  This supports leaving
+	 * out the cols= and rows= which Xml requires and are required by HTML4/XHTML
+	 * but not required by HTML5 and will silently set cols="" and rows="" if
+	 * $wgHtml5 is false and cols and rows are omitted (HTML4 validates present
+	 * but empty cols="" and rows="" as valid).
+	 *
+	 * @param $name    string name attribute
+	 * @param $value   string value attribute
+	 * @param $attribs array  Associative array of miscellaneous extra
+	 *   attributes, passed to Html::element()
+	 * @return string Raw HTML
+	 */
+	public static function textarea( $name, $value = '', $attribs = array() ) {
+		global $wgHtml5;
+		$attribs['name'] = $name;
+		if ( !$wgHtml5 ) {
+			if ( !array_key_exists('cols', $attribs) )
+				$attribs['cols'] = "";
+			if ( !array_key_exists('rows', $attribs) )
+				$attribs['rows'] = "";
+		}
+		return self::element( 'textarea', $attribs, $value );
+	}
 }

languages/messages/MessagesEn.php
@@ -1338,6 +1338,8 @@ The administrator who locked it offered this explanation: $1",
 'nocreatetext'                     => '{{SITENAME}} has restricted the ability to create new pages.
 You can go back and edit an existing page, or [[Special:UserLogin|log in or create an account]].',
 'nocreate-loggedin'                => 'You do not have permission to create new pages.',
+'sectioneditnotsupported-title'    => 'Section editing not supported',
+'sectioneditnotsupported-text'     => 'Section editing is not supported in this edit page.',
 'permissionserrors'                => 'Permissions Errors',
 'permissionserrorstext'            => 'You do not have permission to do that, for the following {{PLURAL:$1|reason|reasons}}:',
 'permissionserrorstext-withaction' => 'You do not have permission to $2, for the following {{PLURAL:$1|reason|reasons}}:',

skins/common/edit.js
@@ -5,13 +5,13 @@ var currentFocused;
 function addButton(imageFile, speedTip, tagOpen, tagClose, sampleText, imageId) {
 	// Don't generate buttons for browsers which don't fully
 	// support it.
-	mwEditButtons[mwEditButtons.length] =
+	mwEditButtons.push(
 		{"imageId": imageId,
 		 "imageFile": imageFile,
 		 "speedTip": speedTip,
 		 "tagOpen": tagOpen,
 		 "tagClose": tagClose,
-		 "sampleText": sampleText};
+		 "sampleText": sampleText});
 }
 
 // this function generates the actual toolbar buttons with localized text
@@ -44,11 +44,9 @@ function mwSetupToolbar() {
 	var toolbar = document.getElementById('toolbar');
 	if (!toolbar) { return false; }
 
-	var textbox = document.getElementById('wpTextbox1');
-	if (!textbox) { return false; }
-
 	// Don't generate buttons for browsers which don't fully
 	// support it.
+	var textbox = document.createElement('textarea'); // abstract, don't assume wpTextbox1 is always there
 	if (!(document.selection && document.selection.createRange)
 		&& textbox.selectionStart === null) {
 		return false;
@@ -154,17 +152,13 @@ function scrollEditBox() {
 		if( scrollTop.value )
 			editBox.scrollTop = scrollTop.value;
 		addHandler( editForm, 'submit', function() {
-			document.getElementById( 'wpScrolltop' ).value = document.getElementById( 'wpTextbox1' ).scrollTop;
+			scrollTop.value = editBox.scrollTop;
 		} );
 	}
 }
 hookEvent( 'load', scrollEditBox );
 hookEvent( 'load', mwSetupToolbar );
 hookEvent( 'load', function() {
-	if ( document.editform ) {
-		currentFocused = document.editform.wpTextbox1;
-		document.editform.wpTextbox1.onfocus = function() { currentFocused = document.editform.wpTextbox1; };
-		document.editform.wpSummary.onfocus = function() { currentFocused = document.editform.wpSummary; };
-	}
+	currentFocused = document.getElementById( 'wpTextbox1' );
 } );
 

commit 0d60325aee650d921ca3208fa7a9dad36eda3cd5

* (bug 23448) MediaWiki:Summary-preview is now displayed instead of MediaWiki:Subject-preview when previewing summary Regression from r59655.

RELEASE-NOTES
@@ -156,6 +156,8 @@ in a negative namespace (which is invalid).
 * (bug 23240) Add ID to namespace selector form on Special:Watchlist
 * The pipe | character in urls is now escaped.
 * (bug 23422) mp3 files can now be moved
+* (bug 23448) MediaWiki:Summary-preview is now displayed instead of
+  MediaWiki:Subject-preview when previewing summary
 
 === API changes in 1.17 ===
 * (bug 22738) Allow filtering by action type on query=logevent

includes/EditPage.php
@@ -1551,7 +1551,9 @@ HTML
 		if ( $isSubjectPreview )
 			$summary = wfMsgForContent( 'newsectionsummary', $wgParser->stripSectionName( $summary ) );
 
-		$summary = wfMsgExt( 'subject-preview', 'parseinline' ) . $sk->commentBlock( $summary, $this->mTitle, !!$isSubjectPreview );
+		$message = $isSubjectPreview ? 'subject-preview' : 'summary-preview';
+
+		$summary = wfMsgExt( $message, 'parseinline' ) . $sk->commentBlock( $summary, $this->mTitle, $isSubjectPreview );
 		return Xml::tags( 'div', array( 'class' => 'mw-summary-preview' ), $summary );
 	}
 