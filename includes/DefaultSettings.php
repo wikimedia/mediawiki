77713365c5119084b4fda3c18f94886ca9f77710
Trevor Parscal <tparscal@users.mediawiki.org>
2010-05-10 20:39:47 +0000

    Adjusted padding to compensate for reducing the icon sizes in r66151.

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1677,7 +1677,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '277';
+$wgStyleVersion = '278';
 
 
 # Server-side caching:
