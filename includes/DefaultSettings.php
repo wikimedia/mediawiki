f0f9e098336a1dc5b00bda8f95652ef2bf3db616
Roan Kattouw <catrope@users.mediawiki.org>
2010-05-18 16:58:47 +0000

    Widen SimpleSearch search box in Vector from 9em to 14em

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1677,7 +1677,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '280';
+$wgStyleVersion = '281';
 
 
 # Server-side caching:
