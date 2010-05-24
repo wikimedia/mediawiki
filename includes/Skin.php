5543cbb1063ccd221c7123333d1d7ded1419befc
Platonides <platonides@users.mediawiki.org>
2010-05-24 20:15:39 +0000

    MFT r66846

--- includes/Skin.php
+++ includes/Skin.php
@@ -2114,6 +2114,9 @@ CSS;
 				}
 			} else {
 				if( strpos( $line, '|' ) !== false ) { // sanity check
+					global $wgMessageCache;
+					$line = $wgMessageCache->transform( $line );
+					
 					$line = array_map( 'trim', explode( '|', trim( $line, '* ' ), 2 ) );
 					$link = wfMsgForContent( $line[0] );
 					if( $link == '-' ) {
