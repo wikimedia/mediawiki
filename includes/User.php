55e9ddb509a5d7c81d9185be33d09515216bc79f
Sean Colombo <sean_colombo@users.mediawiki.org>
2010-05-28 19:01:00 +0000

    Removed an unused local variable in a User function.

--- includes/User.php
+++ includes/User.php
@@ -3556,7 +3556,6 @@ class User {
 	 * @return \bool
 	 */
 	static function comparePasswords( $hash, $password, $userId = false ) {
-		$m = false;
 		$type = substr( $hash, 0, 3 );
 
 		$result = false;
