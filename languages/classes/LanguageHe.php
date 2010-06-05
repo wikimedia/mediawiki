3065e28d2d3def016ea2125c73f86e2a610bdfba
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-06-05 19:15:50 +0000

    Fixed some doxygen warnings

--- languages/classes/LanguageHe.php
+++ languages/classes/LanguageHe.php
@@ -8,21 +8,22 @@
  * @author Rotem Liss
  */
 class LanguageHe extends Language {
+
 	/**
 	 * Convert grammar forms of words.
 	 *
 	 * Available cases:
 	 * "prefixed" (or "תחילית") - when the word has a prefix
 	 *
-	 * @param string the word to convert
-	 * @param string the case
+	 * @param $word String: the word to convert
+	 * @param $case String: the case
 	 */
 	public function convertGrammar( $word, $case ) {
 		global $wgGrammarForms;
 		if ( isset($wgGrammarForms['he'][$case][$word]) ) {
 			return $wgGrammarForms['he'][$case][$word];
 		}
-		
+
 		switch ( $case ) {
 			case 'prefixed':
 			case 'תחילית':
@@ -30,32 +31,27 @@ class LanguageHe extends Language {
 				if ( substr( $word, 0, 2 ) == "ו" && substr( $word, 0, 4 ) != "וו" ) {
 					$word = "ו".$word;
 				}
-				
+
 				# Remove the "He" if prefixed
 				if ( substr( $word, 0, 2 ) == "ה" ) {
 					$word = substr( $word, 2 );
 				}
-				
+
 				# Add a hyphen if non-Hebrew letters
 				if ( substr( $word, 0, 2 ) < "א" || substr( $word, 0, 2 ) > "ת" ) {
 					$word = "־".$word;
 				}
 		}
-		
+
 		return $word;
 	}
-	
+
 	/**
 	 * Gets a number and uses the suited form of the word.
 	 *
-	 * @param integer the number of items
-	 * @param string the first form (singular)
-	 * @param string the second form (plural)
-	 * @param string the third form (2 items, plural is used if not applicable and not specified
-	 * @param not used (for compatibility with ancestor)
-	 * @param not used (for compatibility with ancestor)
-	 *
-	 * @return string of the suited form of word
+	 * @param $count Integer: the number of items
+	 * @param $forms Array with 3 items: the three plural forms
+	 * @return String: the suited form of word
 	 */
 	function convertPlural( $count, $forms ) {
 		if ( !count($forms) ) { return ''; }
