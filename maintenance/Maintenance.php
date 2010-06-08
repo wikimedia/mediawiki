0da044cc5a5acbb07268e01966c4b4608683fac2
Tim Starling <tstarling@users.mediawiki.org>
2010-06-08 06:53:36 +0000

    MFT r67320: fix Maintenance::output() line endings

--- maintenance/Maintenance.php
+++ maintenance/Maintenance.php
@@ -221,8 +221,15 @@ abstract class Maintenance {
 		if( $this->mQuiet ) {
 			return;
 		}
-		$out = preg_replace( '/\n\z/', '', $out );
-		$this->outputChanneled( $out, $channel );
+		if ( $channel === null ) {
+			$f = fopen( 'php://stdout', 'w' );
+			fwrite( $f, $out );
+			fclose( $f );
+		}
+		else {
+			$out = preg_replace( '/\n\z/', '', $out );
+			$this->outputChanneled( $out, $channel );
+		}
 	}
 
 	/**
