ad8b86bd04d029475e457f520cd4703c8ce7f341
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-06-13 20:23:53 +0000

    Array indexes start from zero

--- languages/classes/LanguageSe.php
+++ languages/classes/LanguageSe.php
@@ -11,11 +11,11 @@ class LanguageSe extends Language {
 		$forms = $this->preConvertPlural( $forms, 3 );
 
 		if ( $count == 1 ) {
-			$index = 1;
+			$index = 0;
 		} elseif( $count == 2 ) {
-			$index = 2;
+			$index = 1;
 		} else {
-			$index = 3;
+			$index = 2;
 		}
 		return $forms[$index];
 	}
