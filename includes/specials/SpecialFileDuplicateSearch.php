0b1e7b28c267070fb466aaed6e9b580d728cd339
Sam Reed <reedy@users.mediawiki.org>
2010-06-21 12:59:04 +0000

    GPL Headers for all!

--- includes/specials/SpecialFileDuplicateSearch.php
+++ includes/specials/SpecialFileDuplicateSearch.php
@@ -1,5 +1,23 @@
 <?php
 /**
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
  * A special page to search for files by hash value as defined in the
  * img_sha1 field in the image table
  *
@@ -7,7 +25,6 @@
  * @ingroup SpecialPage
  *
  * @author Raimond Spekking, based on Special:MIMESearch by Ævar Arnfjörð Bjarmason
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
 /**
