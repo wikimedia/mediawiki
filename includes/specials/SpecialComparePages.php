5c85a18e3f29fc7b2984cdcb02546604c75ed28e
Derk-Jan Hartman <hartman@users.mediawiki.org>
2010-06-21 13:55:55 +0000

    Add a copyright line for myself to SpecialComparePages

--- includes/specials/SpecialComparePages.php
+++ includes/specials/SpecialComparePages.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (C) 2010 Derk-Jan Hartman <hartman@videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
