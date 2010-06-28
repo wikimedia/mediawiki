f2cad4bd26a3e7e51cff15f9758c614ad21aa0c4
Trevor Parscal <tparscal@users.mediawiki.org>
2010-06-28 19:46:31 +0000

    Added @noflip to div.tleft and div.rleft

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1548,7 +1548,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '292';
+$wgStyleVersion = '293';
 
 /**
  * This will cache static pages for non-logged-in users to reduce
