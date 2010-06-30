24b9527b1cf818a55b4f26102f0b257594e10a48
Aryeh Gregor <simetrical@users.mediawiki.org>
2010-06-30 22:29:19 +0000

    Backport r68797 "Disable form validation more thoroughly"
    Regression fix, same as r67283.  Does not need to be enabled on WMF
    because it's only relevant if $wgHtml5 = true.  Doesn't need
    RELEASE-NOTES, since it's already covered in existing RELEASE-NOTES.
    Original commit message:
    
    Expands on r67283 by not using any HTML5 input types either, except
    search.  Otherwise you'd still have problems when changing integer
    fields in Special:Preferences, say.  Sad, since in Opera it had a cute
    little widget for incrementing/decrementing, and types like email have
    some neat effects on platforms like the iPhone (see
    <http://diveintohtml5.org/forms.html#type-email>).  But there's no other
    way to disable the constraints these impose without using JS, and given
    how broken WebKit is right now . . .

--- includes/Html.php
+++ includes/Html.php
@@ -143,30 +143,32 @@ class Html {
 		# consistency and better compression.
 		$element = strtolower( $element );
 
-		# Remove HTML5-only attributes if we aren't doing HTML5
-		if ( !$wgHtml5 ) {
-			if ( $element == 'input' ) {
-				# Whitelist of valid XHTML1 types
-				$validTypes = array(
-					'hidden',
-					'text',
-					'password',
-					'checkbox',
-					'radio',
-					'file',
-					'submit',
-					'image',
-					'reset',
-					'button',
-				);
-				if ( isset( $attribs['type'] )
-				&& !in_array( $attribs['type'], $validTypes ) ) {
-					# Fall back to type=text, the default
-					unset( $attribs['type'] );
-				}
+		# Remove HTML5-only attributes if we aren't doing HTML5, and disable
+		# form validation regardless (see bug 23769 and the more detailed
+		# comment in expandAttributes())
+		if ( $element == 'input' ) {
+			# Whitelist of types that don't cause validation.  All except
+			# 'search' are valid in XHTML1.
+			$validTypes = array(
+				'hidden',
+				'text',
+				'password',
+				'checkbox',
+				'radio',
+				'file',
+				'submit',
+				'image',
+				'reset',
+				'button',
+				'search',
+			);
+			if ( isset( $attribs['type'] )
+			&& !in_array( $attribs['type'], $validTypes ) ) {
+				unset( $attribs['type'] );
 			}
-			if ( $element == 'textarea' && isset( $attribs['maxlength'] ) ) {
-				unset( $attribs['maxlength'] );
+			if ( isset( $attribs['type'] ) && $attribs['type'] == 'search'
+			&& !$wgHtml5 ) {
+				unset( $attribs['type'] );
 			}
 			# Here we're blacklisting some HTML5-only attributes...
 			$html5attribs = array(
@@ -184,6 +186,9 @@ class Html {
 			foreach ( $html5attribs as $badAttr ) {
 				unset( $attribs[$badAttr] );
 			}
+		}
+		if ( !$wgHtml5 && $element == 'textarea' && isset( $attribs['maxlength'] ) ) {
+			unset( $attribs['maxlength'] );
 		}
 
 		return "<$element" . self::expandAttributes(
