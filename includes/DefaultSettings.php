559fece8900f5a8bb195bcb40cba15c77b79e2b5
Jack Phoenix <ashley@users.mediawiki.org>
2010-07-05 20:45:20 +0000

    removed TablePager CSS from Modern's main.css as shared.css already contains that and Modern includes shared.css. Also merged two .TablePager rules into one in shared.css and updated comments

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1547,7 +1547,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '293';
+$wgStyleVersion = '294';
 
 /**
  * This will cache static pages for non-logged-in users to reduce
