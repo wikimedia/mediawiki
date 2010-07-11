ef91ed0c3dad114b6ec7b24114cd8fe16ef89a8e
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-07-11 14:17:17 +0000

    Modified Special:Userlogout to subclass UnlistedSpecialPage

--- includes/specials/SpecialUserlogout.php
+++ includes/specials/SpecialUserlogout.php
@@ -18,34 +18,41 @@
  */
 
 /**
- * @file
+ * Implements Special:Userlogout
  * @ingroup SpecialPage
  */
+class SpecialUserlogout extends UnlistedSpecialPage {
 
-/**
- * constructor
- */
-function wfSpecialUserlogout() {
-	global $wgUser, $wgOut;
-
-	/**
-	 * Some satellite ISPs use broken precaching schemes that log people out straight after
-	 * they're logged in (bug 17790). Luckily, there's a way to detect such requests.
-	 */
-	if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], '&amp;' ) !== false ) {
-		wfDebug( "Special:Userlogout request {$_SERVER['REQUEST_URI']} looks suspicious, denying.\n" );
-		wfHttpError( 400, wfMsg( 'loginerror' ), wfMsg( 'suspicious-userlogout' ) );
-		return;
+	function __construct() {
+		parent::__construct( 'Userlogout' );
 	}
-	
-	$oldName = $wgUser->getName();
-	$wgUser->logout();
-	$wgOut->setRobotPolicy( 'noindex,nofollow' );
 
-	// Hook.
-	$injected_html = '';
-	wfRunHooks( 'UserLogoutComplete', array(&$wgUser, &$injected_html, $oldName) );
+	function execute( $par ) {
+		global $wgUser, $wgOut;
 
-	$wgOut->addHTML( wfMsgExt( 'logouttext', array( 'parse' ) ) . $injected_html );
-	$wgOut->returnToMain();
+		/**
+		 * Some satellite ISPs use broken precaching schemes that log people out straight after
+		 * they're logged in (bug 17790). Luckily, there's a way to detect such requests.
+		 */
+		if ( isset( $_SERVER['REQUEST_URI'] ) && strpos( $_SERVER['REQUEST_URI'], '&amp;' ) !== false ) {
+			wfDebug( "Special:Userlogout request {$_SERVER['REQUEST_URI']} looks suspicious, denying.\n" );
+			wfHttpError( 400, wfMsg( 'loginerror' ), wfMsg( 'suspicious-userlogout' ) );
+			return;
+		}
+
+		$this->setHeaders();
+		$this->outputHeader();
+
+		$oldName = $wgUser->getName();
+		$wgUser->logout();
+
+		$wgOut->addWikiMsg( 'logouttext' );
+
+		// Hook.
+		$injected_html = '';
+		wfRunHooks( 'UserLogoutComplete', array( &$wgUser, &$injected_html, $oldName ) );
+		$wgOut->addHTML( $injected_html );
+
+		$wgOut->returnToMain();
+	}
 }
