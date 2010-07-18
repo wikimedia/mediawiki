2c9d0dc4a4251c54a70ed527ef820badfc9ea9f6
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-07-18 12:10:39 +0000

    * Modified Special:Log to extend SpecialPage * Use FormOptions

--- includes/SpecialPage.php
+++ includes/SpecialPage.php
@@ -134,7 +134,7 @@ class SpecialPage {
 
 		# Recent changes and logs
 		'Newimages'                 => array( 'IncludableSpecialPage', 'Newimages' ),
-		'Log'                       => array( 'SpecialPage', 'Log' ),
+		'Log'                       => 'SpecialLog',
 		'Watchlist'                 => array( 'SpecialPage', 'Watchlist' ),
 		'Newpages'                  => 'SpecialNewpages',
 		'Recentchanges'             => 'SpecialRecentchanges',
