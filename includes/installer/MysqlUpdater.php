0e037e0a93e87ded2578c4649dcc259908eaa5c9
Chad Horohoe <demon@users.mediawiki.org>
2010-07-20 22:28:50 +0000

    * Split Mysql/Sqlite updates into their own files * Rename Update -> DatabaseUpdaters to be more consistent with DatabaseInstaller * Made DatabaseUpdaters abstract and merged the Updaters interface in, had Mysql/Oracle/Sqlite subclass it. Entry point is now factory DatabaseUpdaters::newForDB()

--- includes/installer/MysqlUpdater.php
+++ includes/installer/MysqlUpdater.php
@@ -1,29 +1,10 @@
 <?php
 
 /**
- * All DBs supported by MediaWiki need to implement this. Base interface for
- * Updaters, which is replacing updaters.inc
+ * Mysql update list and mysql-specific update functions
  */
-interface Updaters {
-	
-	/**
-	 * Get an array of updates to perform on the database. Should return a
-	 * mutli-dimensional array. The main key is the MediaWiki version (1.12,
-	 * 1.13...) with the values being arrays of updates, identical to how
-	 * updaters.inc did it (for now)
-	 *
-	 * @return Array
-	 */
-	public function getUpdates();
-	
-}
-
-/**
- * Mysql implementation.
- */
-class MysqlUpdater implements Updaters {
-
-	public function getUpdates() {
+class MysqlUpdater extends DatabaseUpdater {
+	protected function getCoreUpdateList() {
 		return array(
 			'1.2' => array(
 				array( 'add_field', 'ipblocks',      'ipb_id',           'patch-ipblocks.sql' ),
@@ -174,58 +155,4 @@ class MysqlUpdater implements Updaters {
 			),
 		);
 	}
-	
-}
-
-/**
- * Sqlite implementation.
- */
-class SqliteUpdater implements Updaters {
-	
-	public function getUpdates() {
-		return array(
-			'1.14' => array(
-				array( 'add_field', 'site_stats',    'ss_active_users',  'patch-ss_active_users.sql' ),
-				array( 'do_active_users_init' ),
-				array( 'add_field', 'ipblocks',      'ipb_allow_usertalk', 'patch-ipb_allow_usertalk.sql' ),
-				array( 'sqlite_initial_indexes' ),
-			),
-			'1.15' => array(
-				array( 'add_table', 'change_tag',                        'patch-change_tag.sql' ),
-				array( 'add_table', 'tag_summary',                       'patch-change_tag.sql' ),
-				array( 'add_table', 'valid_tag',                         'patch-change_tag.sql' ),
-			),
-			'1.16' => array(
-				array( 'add_table', 'user_properties',                   'patch-user_properties.sql' ),
-				array( 'add_table', 'log_search',                        'patch-log_search.sql' ),
-				array( 'do_log_search_population' ),
-				array( 'add_field', 'logging',       'log_user_text',    'patch-log_user_text.sql' ),
-				array( 'add_table', 'l10n_cache',                        'patch-l10n_cache.sql' ),
-				array( 'add_table', 'external_user',                     'patch-external_user.sql' ),
-				array( 'add_index', 'log_search',    'ls_field_val',     'patch-log_search-rename-index.sql' ),
-				array( 'add_index', 'change_tag',    'change_tag_rc_tag', 'patch-change_tag-indexes.sql' ),
-				array( 'add_field', 'redirect',      'rd_interwiki',     'patch-rd_interwiki.sql' ),
-				array( 'do_update_transcache_field' ),
-				array( 'sqlite_setup_searchindex' ),
-			),
-			'1.17' => array(
-				array( 'add_table', 'iwlinks',                            'patch-iwlinks.sql' ),
-				array( 'add_index', 'iwlinks',   'iwl_prefix_from_title', 'patch-rename-iwl_prefix.sql' ),
-				array( 'add_field', 'updatelog', 'ul_value',              'patch-ul_value.sql' ),
-				array( 'add_field', 'interwiki',     'iw_api',           'patch-iw_api_and_wikiid.sql' ),
-			),
-		);
-	}
-	
-}
-
-/**
- * Oracle implementation.
- */
-class OracleUpdater implements Updaters {
-	
-	public function getUpdates() {
-		return array();
-	}
-	
 }
