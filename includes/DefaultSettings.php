5ff9e7b357373398ccb61c0af1359471fe3a3e4b
Tim Starling <tstarling@users.mediawiki.org>
2010-07-28 07:11:03 +0000

    Updates for release of 1.16.0

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ if ( !defined( 'MW_PHP4' ) ) {
 }
 
 /** MediaWiki version number */
-$wgVersion = '1.16.0beta3';
+$wgVersion = '1.16.0';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
