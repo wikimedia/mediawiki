fbc6621ee173f809b70474caa28fd2f9861ace39
OverlordQ <overlordq@users.mediawiki.org>
2010-07-28 05:08:21 +0000

    Followup to r69961, update PG schema

--- maintenance/postgres/archives/patch-categorylinks-better-collation.sql
+++ maintenance/postgres/archives/patch-categorylinks-better-collation.sql
@@ -1,7 +1,7 @@
 CREATE TYPE link_type AS ENUM ('page', 'subcat', 'file');
 DROP INDEX cl_sortkey;
 ALTER TABLE categorylinks
-	ADD COLUMN cl_raw_sortkey TEXT NULL DEFAULT NULL,
+	ADD COLUMN cl_sortkey_prefix TEXT NOT NULL DEFAULT '',
 	ADD COLUMN cl_collation SMALLINT NOT NULL DEFAULT 0,
 	ADD COLUMN cl_type link_type NOT NULL DEFAULT 'page';
 CREATE INDEX cl_collation ON categorylinks ( cl_collation );
