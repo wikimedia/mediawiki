efd70cb30e88c35fa49ca9e48d563362c883ff85
Mark A. Hershberger <mah@users.mediawiki.org>
2010-07-30 20:17:28 +0000

    * Make parsertests work with interwiki table on sqlite * re r69542 temporary fix for new-installer + interwiki table * temporary fix for outputing LocalSettings.php in the CliInstaller

--- includes/installer/DatabaseInstaller.php
+++ includes/installer/DatabaseInstaller.php
@@ -398,8 +398,9 @@ abstract class DatabaseInstaller {
 		foreach( $rows as $row ) {
 			$row = preg_replace( '/^\s*([^#]*?)\s*(#.*)?$/', '\\1', $row ); // strip comments - whee
 			if ( $row == "" ) continue;
+			$row .= "||";
 			$interwikis[] = array_combine(
-				array( 'iw_prefix', 'iw_url', 'iw_local' ),
+				array( 'iw_prefix', 'iw_url', 'iw_local', 'iw_api', 'iw_wikiid' ),
 				explode( '|', $row )
 			);
 		}
