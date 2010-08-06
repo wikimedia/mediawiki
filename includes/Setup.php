46668eb156afe5f89dfe3507c9241deee90285af
Chad Horohoe <demon@users.mediawiki.org>
2010-08-06 21:10:18 +0000

    Fix for r70571, still have to use $_COOKIE here too, $wgContLang is undefined so far

--- includes/Setup.php
+++ includes/Setup.php
@@ -305,7 +305,7 @@ if( !wfIniGetBool( 'session.auto_start' ) )
 	session_name( $wgSessionName ? $wgSessionName : $wgCookiePrefix . '_session' );
 
 if( !defined( 'MW_NO_SESSION' ) ) {
-	if( !$wgCommandLineMode && ( $wgRequest->checkSessionCookie() || $wgRequest->getCookie( 'Token' ) ) ) {
+	if( !$wgCommandLineMode && ( $wgRequest->checkSessionCookie() || isset( $_COOKIE[$wgCookiePrefix.'Token'] ) ) ) {
 		wfIncrStats( 'request_with_session' );
 		wfSetupSession();
 		$wgSessionStarted = true;
