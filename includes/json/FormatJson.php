ad9b37f457a78b7caa3eafe98728ec02a5693e92
Chad Horohoe <demon@users.mediawiki.org>
2010-08-11 12:42:40 +0000

    Boolean parameters suck, but we can make them suck less by adding constants you can use. Also doc, spacing, etc

--- includes/json/FormatJson.php
+++ includes/json/FormatJson.php
@@ -1,12 +1,24 @@
 <?php
-/**
- * Simple wrapper for json_econde and json_decode that falls back on Services_JSON class
- */
+
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 1 );
 }
 
+/**
+ * Simple wrapper for json_econde and json_decode that falls back on Services_JSON class
+ */
 class FormatJson {
+
+	// Constants for decode() return types
+	const AS_OBJECT = true;
+	const AS_ARRAY = false;
+
+	/**
+	 * Turn an array or object into a JSON string
+	 * @param $value Mixed. Array or object to turn into JSON
+	 * @param $isHtml bool ???
+	 * @return <type>
+	 */
 	public static function encode( $value, $isHtml = false ) {
 		// Some versions of PHP have a broken json_encode, see PHP bug
 		// 46944. Test encoding an affected character (U+20000) to
@@ -19,6 +31,12 @@ class FormatJson {
 		}
 	}
 
+	/**
+	 * Decode some JSON into an array or object
+	 * @param $value String of Json
+	 * @param $assoc bool One of AS_OBJECT or AS_ARRAY to specify return type
+	 * @return Array or Object
+	 */
 	public static function decode( $value, $assoc = false ) {
 		if ( !function_exists( 'json_decode' ) ) {
 			$json = new Services_JSON();
