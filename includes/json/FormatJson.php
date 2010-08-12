539ab6f5e5f730a4dae78fa34780689897ec4675
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-08-12 07:27:48 +0000

    Documentation o_O

--- includes/json/FormatJson.php
+++ includes/json/FormatJson.php
@@ -7,6 +7,15 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 class FormatJson {
+	
+	/**
+	 * Returns the JSON representation of a value.
+	 * 
+	 * @param $value Mixed: the value being encoded. Can be any type except a resource.
+	 * @param $isHtml Boolean
+	 * 
+	 * @return string
+	 */
 	public static function encode( $value, $isHtml = false ) {
 		// Some versions of PHP have a broken json_encode, see PHP bug
 		// 46944. Test encoding an affected character (U+20000) to
@@ -19,6 +28,17 @@ class FormatJson {
 		}
 	}
 
+	/**
+	 * Decodes a JSON string.
+	 * 
+	 * @param $value String: the json string being decoded.
+	 * @param $assoc Boolean: when true, returned objects will be converted into associative arrays.
+	 * 
+	 * @return Mixed: the value encoded in json in appropriate PHP type.
+	 * Values true, false and null (case-insensitive) are returned as true, false
+	 * and &null; respectively. &null; is returned if the json cannot be
+	 * decoded or if the encoded data is deeper than the recursion limit.
+	 */
 	public static function decode( $value, $assoc = false ) {
 		if ( !function_exists( 'json_decode' ) ) {
 			$json = new Services_JSON();
@@ -31,4 +51,5 @@ class FormatJson {
 			return json_decode( $value, $assoc );
 		}
 	}
+	
 }
