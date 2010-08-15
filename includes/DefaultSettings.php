94c6600f99b1e4a0784b1f88c208e3a18ea07bce
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-08-15 10:02:17 +0000

    Follow up to r71107 - moved forgotten global variables

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5088,6 +5088,37 @@ $wgUploadMaintenance = false;
  * Enabes or disables JavaScript-based suggestions of password strength
  */
 $wgLivePasswordStrengthChecks = false;
+
+/**
+ * The location of the MediaWiki package repository to use.
+ * 
+ * @since 1.17
+ * @var string
+ */
+$wgRepositoryApiLocation = 'http://www.mediawiki.org/w/api.php';
+
+/**
+ * The location of the remote web interface for the selected repository. 
+ * 
+ * @since 1.17
+ * @var string
+ */
+$wgRepositoryLocation = 'http://www.mediawiki.org/wiki/Special:Repository';
+
+/**
+ * List of package states to filter update detection and extension listing on. 
+ * 
+ * @since 1.17
+ * @var array
+ */
+$wgRepositoryPackageStates = array(
+	//'dev',
+	//'alpha',
+	'beta',
+	//'rc',
+	'stable',
+	//'deprecated',
+);
  
 /**
  * For really cool vim folding this needs to be at the end:
