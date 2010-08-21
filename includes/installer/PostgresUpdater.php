3923c58fc7031cb3f74fcfb609a17fc66ae41b33
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-08-21 15:53:46 +0000

    @TODO @FIXME -> @todo so that doxygen recognise it correctly

--- includes/installer/PostgresUpdater.php
+++ includes/installer/PostgresUpdater.php
@@ -3,7 +3,7 @@
 /**
  * Class for handling updates to Postgres databases.
  *
- * @TODO @FIXME - Postgres should use sequential updates like Mysql, Sqlite
+ * @todo FIXME: Postgres should use sequential updates like Mysql, Sqlite
  * and everybody else. It never got refactored like it should've. For now,
  * just wrap the old do_postgres_updates() in this class so we can clean up
  * the higher-level stuff.
