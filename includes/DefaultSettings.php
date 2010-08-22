982a434cee45a6f67edd3ca3996dda544eafdb94
Jack Phoenix <ashley@users.mediawiki.org>
2010-08-22 21:14:36 +0000

    coding style tweaks for ajax.js

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -1560,7 +1560,7 @@ $wgCacheEpoch = '20030516000000';
  * to ensure that client-side caches do not keep obsolete copies of global
  * styles.
  */
-$wgStyleVersion = '300';
+$wgStyleVersion = '301';
 
 /**
  * This will cache static pages for non-logged-in users to reduce
