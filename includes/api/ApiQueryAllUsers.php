3c71718952e8175616c1ca3371faebbebbe63fc8
Sam Reed <reedy@users.mediawiki.org>
2010-08-26 22:12:01 +0000

    I really do hate whitespace

--- includes/api/ApiQueryAllUsers.php
+++ includes/api/ApiQueryAllUsers.php
@@ -185,12 +185,12 @@ class ApiQueryAllUsers extends ApiQueryBase {
 		}
 
 		if ( is_array( $lastUserData ) ) {
-		        $fit = $result->addValue( array( 'query', $this->getModuleName() ),
-		                        null, $lastUserData );
-		        if ( !$fit ) {
-		                $this->setContinueEnumParameter( 'from',
-		                                $this->keyToTitle( $lastUserData['name'] ) );
-		        }
+			$fit = $result->addValue( array( 'query', $this->getModuleName() ),
+				null, $lastUserData );
+			if ( !$fit ) {
+				$this->setContinueEnumParameter( 'from',
+					$this->keyToTitle( $lastUserData['name'] ) );
+			}
 		}
 
 		$result->setIndexedTagName_internal( array( 'query', $this->getModuleName() ), 'u' );
