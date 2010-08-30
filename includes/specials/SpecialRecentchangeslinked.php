49a4b3f3eca6a1d702fa36d6f96634453001bc71
Chad Horohoe <demon@users.mediawiki.org>
2010-08-30 19:07:09 +0000

    Tweak SpecialRecentChangesQuery hook. Need to be able to modify selected fields on RCL (part 1 for bug 23916)

--- includes/specials/SpecialRecentchangeslinked.php
+++ includes/specials/SpecialRecentchangeslinked.php
@@ -114,7 +114,7 @@ class SpecialRecentchangeslinked extends SpecialRecentChanges {
 				$query_options, $opts['tagfilter'] );
 		}
 
-		if ( !wfRunHooks( 'SpecialRecentChangesQuery', array( &$conds, &$tables, &$join_conds, $opts, &$query_options ) ) )
+		if ( !wfRunHooks( 'SpecialRecentChangesQuery', array( &$conds, &$tables, &$join_conds, $opts, &$query_options, &$select ) ) )
 			return false;
 
 		if( $ns == NS_CATEGORY && !$showlinkedto ) {
