1a92eb0082073da87ba3e10029b369913b43ef94
OverlordQ <overlordq@users.mediawiki.org>
2010-09-03 21:17:33 +0000

    Fix sequence renaming to be backwards-compat with older versions of PG. Needs merged with 1.16

--- maintenance/postgres/archives/patch-update_sequences.sql
+++ maintenance/postgres/archives/patch-update_sequences.sql
@@ -1,20 +1,20 @@
-ALTER SEQUENCE rev_rev_id_val RENAME TO revision_rev_id_seq;
+ALTER TABLE revision RENAME rev_rev_id_val TO revision_rev_id_seq;
 ALTER TABLE revision ALTER COLUMN rev_id SET DEFAULT NEXTVAL('revision_rev_id_seq');
 
-ALTER SEQUENCE text_old_id_val RENAME TO text_old_id_seq;
+ALTER TABLE pagecontent RENAME text_old_id_val TO text_old_id_seq;
 ALTER TABLE pagecontent ALTER COLUMN old_id SET DEFAULT nextval('text_old_id_seq');
 
-ALTER SEQUENCE category_id_seq RENAME TO category_cat_id_seq;
+ALTER TABLE category RENAME category_id_seq TO category_cat_id_seq;
 ALTER TABLE category ALTER COLUMN cat_id SET DEFAULT nextval('category_cat_id_seq');
 
-ALTER SEQUENCE ipblocks_ipb_id_val RENAME TO ipblocks_ipb_id_seq;
+ALTER TABLE ipblocks RENAME ipblocks_ipb_id_val TO ipblocks_ipb_id_seq;
 ALTER TABLE ipblocks ALTER COLUMN ipb_id SET DEFAULT nextval('ipblocks_ipb_id_seq');
 
-ALTER SEQUENCE rc_rc_id_seq RENAME TO recentchanges_rc_id_seq;
+ALTER TABLE recentchanges RENAME rc_rc_id_seq TO recentchanges_rc_id_seq;
 ALTER TABLE recentchanges ALTER COLUMN rc_id SET DEFAULT nextval('recentchanges_rc_id_seq');
 
-ALTER SEQUENCE log_log_id_seq RENAME TO logging_log_id_seq;
+ALTER TABLE logging RENAME log_log_id_seq TO logging_log_id_seq;
 ALTER TABLE logging ALTER COLUMN log_id SET DEFAULT nextval('logging_log_id_seq');
 
-ALTER SEQUENCE pr_id_val RENAME TO page_restrictions_pr_id_seq;
+ALTER TABLE page_restrictions RENAME pr_id_val TO page_restrictions_pr_id_seq;
 ALTER TABLE page_restrictions ALTER COLUMN pr_id SET DEFAULT nextval('page_restrictions_pr_id_seq');
