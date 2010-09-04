495f6344becb97c5b5c754acc347048903ce2b15
Mark A. Hershberger <mah@users.mediawiki.org>
2010-09-04 00:58:57 +0000

    * Remove $wg*Selenium* globals as they are only used for testing. * mglaser said the SpecialSelenium page wasn't needed so I'm removing it – I suggest its functionality be put in an extension if needed. * Add verbose option (requires patch to Testing_Selenium) to RunSeleniumTests. * TODO: add .ini file to hold local configuration * TODO: add ability to sniff available browsers from Grid server instead of hard-coding.

--- maintenance/tests/selenium/Selenium.php
+++ maintenance/tests/selenium/Selenium.php
@@ -4,49 +4,76 @@
  * This is implemented as a singleton.
  */
 
-class Selenium extends Testing_Selenium {
+require( 'Testing/Selenium.php' );
+class Selenium {
 	protected static $_instance = null;
 	public $isStarted = false;
+	public $tester;
 
-	public static function getInstance() {
-		global $wgSeleniumTestsBrowsers, $wgSeleniumTestsSeleniumHost, $wgSeleniumTestsUseBrowser;
-		global $wgSeleniumServerPort, $wgSeleniumLogger;
+	protected $port;
+	protected $host;
+	protected $browser;
+	protected $browsers;
+	protected $logger;
+	protected $user;
+	protected $pass;
+	protected $timeout = 30000;
+	protected $verbose;
 
+	/**
+	 * @todo this shouldn't have to be static
+	 */
+	static protected $url;
+
+	/**
+	 * Override parent
+	 */
+	public function __construct() {
+		/**
+		 * @todo this is an ugly hack to make information available to
+		 * other tests.  It should be fixed.
+		 */
 		if ( null === self::$_instance ) {
-			$wgSeleniumLogger->write( "Browser: " . $wgSeleniumTestsBrowsers[$wgSeleniumTestsUseBrowser] );
-			self::$_instance = new self( $wgSeleniumTestsBrowsers[$wgSeleniumTestsUseBrowser],
-							self::getBaseUrl(),
-							$wgSeleniumTestsSeleniumHost,
-							$wgSeleniumServerPort );
+			self::$_instance = $this;
+		} else {
+			throw new MWException("Already have one Selenium instance.");
  		}
-		return self::$_instance;
 	}
 
 	public function start() {
-		parent::start();
+		$this->tester = new Testing_Selenium( $this->browser, self::$url, $this->host,
+			$this->port, $this->timeout );
+		if ( method_exists( $this->tester, "setVerbose" ) ) $this->tester->setVerbose( $this->verbose );
+
+		$this->tester->start();
 		$this->isStarted = true;
 	}
 
 	public function stop() {
-		parent::stop();
+		$this->tester->stop();
+		$this->tester = null;
 		$this->isStarted = false;
 	}
 
-	static function getBaseUrl() {
-		global $wgSeleniumTestsWikiUrl, $wgServer, $wgScriptPath;
-		if ( $wgSeleniumTestsWikiUrl ) {
-			return $wgSeleniumTestsWikiUrl;
-		} else {
-			return $wgServer . $wgScriptPath;
+	protected function setupBrowsers() {
+		/**
+		 * @todo This needs to be replaced with something not hard
+		 * coded. This would be entries in a .ini file or
+		 * screen-scraping
+		 * http://grid.tesla.usability.wikimedia.org:4444/console for
+		 * example.
+		 */
+		return array(
+			'firefox' => 'Firefox 3.5 on Linux',
+			'iexplorer' => '*iexploreproxy',
+			'chrome' => '*googlechrome',
+		);
 		}
-	}
 
 	public function login() {
-		global $wgSeleniumTestsWikiUser, $wgSeleniumTestsWikiPassword;
-
-		$this->open( self::getBaseUrl() . '/index.php?title=Special:Userlogin' );
-		$this->type( 'wpName1', $wgSeleniumTestsWikiUser );
-		$this->type( 'wpPassword1', $wgSeleniumTestsWikiPassword );
+		$this->open( self::$url . '/index.php?title=Special:Userlogin' );
+		$this->type( 'wpName1', $this->user );
+		$this->type( 'wpPassword1', $this->pass );
 		$this->click( "//input[@id='wpLoginAttempt']" );
 		$this->waitForPageToLoad(5000);
 		//after login we redirect to the main page. So check whether the "Prefernces" top menu item exists
@@ -57,18 +84,71 @@ class Selenium extends Testing_Selenium {
 		
 	}
 
+	public static function getInstance() {
+		if ( null === self::$_instance ) {
+			throw new MWException( "No instance set yet" );
+		}
+
+		return self::$_instance;
+	}
+
 	public function loadPage( $title, $action ) {
-		$this->open( self::getBaseUrl() . '/index.php?title=' . $title . '&action=' . $action );
+		$this->open( self::$url . '/index.php?title=' . $title . '&action=' . $action );
 	}
 	
-	/*
-	 * Log to console or html depending on the value of $wgSeleniumTestsRunMode
-	 */
+	public function setLogger( $logger ) {
+		$this->logger = $logger;
+	}
+
+	public function getLogger( ) {
+		return $this->logger;
+	}
+
 	public function log( $message ) {
-		global $wgSeleniumLogger;
-		$wgSeleniumLogger->write( $message );
+		$this->logger->write( $message );
 	}
 	
+	public function setUrl( $url ) {
+		self::$url = $url;
+	}
+
+	static public function getUrl() {
+		return self::$url;
+	}
+
+	public function setPort( $port ) {
+		$this->port = $port;
+	}
+
+	public function setUser( $user ) {
+		$this->user = $user;
+	}
+
+	public function setPass( $pass ) {
+		$this->pass = $pass;
+	}
+
+	public function setHost( $host ) {
+		$this->host = $host;
+	}
+
+	public function setVerbose( $verbose ) {
+		$this->verbose = $verbose;
+	}
+
+	public function setBrowser( $b ) {
+		$browsers = $this->setupBrowsers();
+		if ( !isset( $browsers[$b] ) ) {
+			throw new MWException( "Invalid Browser: $b.\n" );
+		}
+		$this->browser = $browsers[$b];
+	}
+
+	public function __call( $name, $args ) {
+		$t = call_user_func_array( array( $this->tester, $name), $args );
+		return $t;
+	}
+
 	// Prevent external cloning
 	protected function __clone() { }
 	// Prevent external construction
