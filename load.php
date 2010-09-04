515bb232f72c0f9ccb1b0f75f1a67fc6fd3993cb
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-09-04 06:46:50 +0000

    * simplified file description header or load.php * and while I'm a it: svn:eol-style native (!)

--- load.php
+++ load.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * This file is the entry point for the resource loader.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -15,14 +17,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
  *
+ * @file
  * @author Roan Kattouw
  * @author Trevor Parscal
  *
- */
-
-/**
- * @file
- * This file is the entry point for the resource loader.
  */
  
 require ( dirname( __FILE__ ) . '/includes/WebStart.php' );
