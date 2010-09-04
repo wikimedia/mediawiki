2d50fc0efe3058f81ee38b09f52295ecb863eaf6
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-09-04 06:17:07 +0000

    * added @file * whitespaces fixes * double quotes to simple quotes where possible

--- maintenance/helloWorld.php
+++ maintenance/helloWorld.php
@@ -1,21 +1,22 @@
 <?php
-
 /**
  * To the extent possible under law,  I, Mark Hershberger, have waived all copyright and
  * related or neighboring rights to Hello World. This work is published from United States.
+ *
+ * @file
+ * @ingroup Maintenance
  * @copyright CC0 http://creativecommons.org/publicdomain/zero/1.0/
  * @author Mark A. Hershberger <mah@everybody.org>
- * @ingroup Maintenance
  */
 
-require_once( dirname( __FILE__ ) . "/Maintenance.php" );
+require_once( dirname( __FILE__ ) . '/Maintenance.php' );
 
 class CommandLineInstaller extends Maintenance {
 
 	public function __construct() {
 		parent::__construct();
 
-		$this->addOption( 'name', 'Who to say Hello to', false, true);
+		$this->addOption( 'name', 'Who to say Hello to', false, true );
 	}
 
 	public function execute() {
@@ -24,5 +25,4 @@ class CommandLineInstaller extends Maintenance {
 	}
 }
 
-wfRunMaintenance( "CommandLineInstaller" );
-
+wfRunMaintenance( 'CommandLineInstaller' );
