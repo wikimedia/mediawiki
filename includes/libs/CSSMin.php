a71f8e3f5fd3a3070fc0348182f2fc1e32157410
Trevor Parscal <tparscal@users.mediawiki.org>
2010-09-09 22:12:54 +0000

    Changed the license to Apache 2.0 - I also hereby grant the copyright to the Wikimedia Foundation.

--- includes/libs/CSSMin.php
+++ includes/libs/CSSMin.php
@@ -1,19 +1,15 @@
 <?php
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+/*
+ * Copyright 2010 Wikimedia Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. 
  */
 
 /**
@@ -21,7 +17,11 @@
  * 
  * This class provides minification, URL remapping, URL extracting, and data-URL embedding.
  * 
- * @author Trevor Parscal
+ * @file
+ * @version 0.1.0 -- 2010-09-09
+ * @author Trevor Parscal <tparscal@wikimedia.org>
+ * @copyright Copyright 2010 Wikimedia Foundation
+ * @license http://www.apache.org/licenses/LICENSE-2.0
  */
 class CSSMin {
 	
