7333c14a1b5172840160120a0201186f23ef48d0
Platonides <platonides@users.mediawiki.org>
2010-09-12 13:42:41 +0000

    Follow up r72566. Per CR r9132#c9132 these files were intended to explicitely require the files holding the parent classes.

--- maintenance/tests/phpunit/includes/UploadFromUrlTest.php
+++ maintenance/tests/phpunit/includes/UploadFromUrlTest.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'api/ApiSetup.php';
 
 class UploadFromUrlTest extends ApiTestSetup {
 
--- maintenance/tests/phpunit/includes/search/SearchEngineTest.php
+++ maintenance/tests/phpunit/includes/search/SearchEngineTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__FILE__) . '/../../bootstrap.php';
+
 /**
  * @group Stub
  */
