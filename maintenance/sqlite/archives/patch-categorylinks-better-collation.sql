8fbb4bafc4d21c9a98fdc9be3a027965cb562f76
Max Semenik <maxsem@users.mediawiki.org>
2010-09-12 17:48:56 +0000

    Follow-up r72870: fixed patch file so that it actually works, switched it to native SQLite types

--- maintenance/sqlite/archives/patch-categorylinks-better-collation.sql
+++ maintenance/sqlite/archives/patch-categorylinks-better-collation.sql
@@ -1,7 +1,7 @@
-ALTER TABLE /*_*/categorylinks ADD COLUMN cl_sortkey_prefix TEXT binary NOT NULL default '';
+ALTER TABLE /*_*/categorylinks ADD COLUMN cl_sortkey_prefix TEXT NOT NULL default '';
 ALTER TABLE /*_*/categorylinks ADD COLUMN cl_collation BLOB NOT NULL default '';
-ALTER TABLE /*_*/categorylinks ADD COLUMN cl_type ENUM('page', 'subcat', 'file') NOT NULL default 'page';
+ALTER TABLE /*_*/categorylinks ADD COLUMN cl_type TEXT NOT NULL default 'page';
 CREATE INDEX cl_collation ON /*_*/categorylinks (cl_collation);
 DROP INDEX cl_sortkey;
 CREATE INDEX cl_sortkey ON /*_*/categorylinks (cl_to, cl_type, cl_sortkey, cl_from);
-INSERT IGNORE INTO /*_*/updatelog (ul_key) VALUES ('cl_fields_update');
+INSERT OR IGNORE INTO /*_*/updatelog (ul_key) VALUES ('cl_fields_update');
