92d8037bb9f802ea45b1affa5e9d36ebd5686d5b
Trevor Parscal <tparscal@users.mediawiki.org>
2010-09-15 02:03:24 +0000

    Added collapsible tabs plugin, reworked tabIndex functions, and fixed some syntax errors.

--- resources/jquery/jquery.tabIndex.js
+++ resources/jquery/jquery.tabIndex.js
@@ -1,11 +1,24 @@
-( function( $ ) {
-
 /**
- * Finds the highest tabindex in use.
+ * Finds the lowerst tabindex in use within a selection
+ * 
+ * @return Integer of lowest tabindex on the page
+ */
+jQuery.fn.firstTabIndex = function() {
+	var minTabIndex = 0;
+	$(this).find( '[tabindex]' ).each( function() {
+		var tabIndex = parseInt( $(this).attr( 'tabindex' ) );
+		if ( tabIndex > minTabIndex ) {
+			minTabIndex = tabIndex;
+		}
+	} );
+	return minTabIndex;
+};
+/**
+ * Finds the highest tabindex in use within a selection
  * 
  * @return Integer of highest tabindex on the page
  */
-$.fn.maxTabIndex( function() {
+jQuery.fn.lastTabIndex = function() {
 	var maxTabIndex = 0;
 	$(this).find( '[tabindex]' ).each( function() {
 		var tabIndex = parseInt( $(this).attr( 'tabindex' ) );
@@ -14,6 +27,4 @@ $.fn.maxTabIndex( function() {
 		}
 	} );
 	return maxTabIndex;
-} );
-
-} )();
+};
