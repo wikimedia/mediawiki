9a1511ec6b1ef47015d36cb2c612134f230077df
Chad Horohoe <demon@users.mediawiki.org>
2010-09-19 23:25:57 +0000

    Followup r70608, don't use master connection just for type check, and use getType() instead of instanceof

--- includes/Revision.php
+++ includes/Revision.php
@@ -314,8 +314,8 @@ class Revision {
 
 			if( isset( $row->page_latest ) ) {
 				$this->mCurrent = ( $row->rev_id == $row->page_latest );
-				$this->mTitle = Title::makeTitle( $row->page_namespace, $row->page_title );
-				$this->mTitle->resetArticleID( $this->mPage );
+				$row->page_id = $this->mPage;
+				$this->mTitle = Title::newFromRow( $row );
 			} else {
 				$this->mCurrent = false;
 				$this->mTitle = null;
