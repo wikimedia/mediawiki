4dc810b089702ec6123664e2e9a81843cdbdbe3f
Platonides <platonides@users.mediawiki.org>
2010-09-22 16:50:09 +0000

    Fix bug from r68337. The variable is called $res, not $result

--- includes/api/ApiQueryImages.php
+++ includes/api/ApiQueryImages.php
@@ -108,7 +108,7 @@ class ApiQueryImages extends ApiQueryGeneratorBase {
 		} else {
 			$titles = array();
 			$count = 0;
-			foreach ( $result as $row ) {
+			foreach ( $res as $row ) {
 				if ( ++$count > $params['limit'] ) {
 					// We've reached the one extra which shows that
 					// there are additional pages to be had. Stop here...
