2a0a2b7a5bd0b52cc4951528ed72c1720e2fc1cd
Platonides <platonides@users.mediawiki.org>
2010-09-22 16:06:27 +0000

    No need of $wgMemc after r73386. Change the comment added in r73260 so that it doesn't look that relying on how previous tests set the globals is ever acceptable.

--- maintenance/tests/phpunit/includes/MessageTest.php
+++ maintenance/tests/phpunit/includes/MessageTest.php
@@ -3,10 +3,10 @@
 class MessageTest extends PHPUnit_Framework_TestCase {
 
 	function setUp() {
-		global $wgLanguageCode, $wgLang, $wgContLang, $wgMemc, $wgMessageCache;
+		global $wgLanguageCode, $wgLang, $wgContLang, $wgMessageCache;
 
 		$wgLanguageCode = 'en'; # For mainpage to be 'Main Page'
-		//Some test set this to a Stub Object. For this test we need the real deal
+		//Note that a Stub Object is not enough for this test
 		$wgContLang = $wgLang = Language::factory( $wgLanguageCode );
 		$wgMessageCache = new MessageCache( false, true, 3600 );
 	}
