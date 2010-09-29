c9ed45c9488381c76c85943f7274b2466d8a3b9d
Antoine Musso <hashar@users.mediawiki.org>
2010-09-29 19:51:48 +0000

    Implements bug 24343 "localurl discards section id". * parser hook 'linkurl' which use Title::getLinkUrl() * very basic tests for the Title class

--- maintenance/tests/phpunit/includes/TitleTest.php
+++ maintenance/tests/phpunit/includes/TitleTest.php
@@ -1,6 +1,10 @@
 <?php
 
 class TitleTest extends PHPUnit_Framework_TestCase {
+	function setUp() {
+		global $wgContLang;
+		$wgContLang = Language::factory( 'en' );
+	}
 
 	function testLegalChars() {
 		$titlechars = Title::legalChars();
@@ -13,5 +17,25 @@ class TitleTest extends PHPUnit_Framework_TestCase {
 				$this->assertTrue( (bool)preg_match( "/[$titlechars]/", $chr ), "chr($num) = $chr is a valid titlechar" );
 			}
 		}
+	}
+
+	/**
+	 * Test originally wrote to investigate bug 24343
+	 * FIXME : some tests might fail depending on local settings.
+	 */
+	function testGetURLS() {
+		global $wgArticlePath, $wgScript;
+	
+		$title = Title::newFromText( 'User:Bob#section' );
+	
+		$this->assertEquals( "$wgScript/User:Bob", $title->getLocalURL(),
+			'Title::getLocalURL() does NOT have fragment' );
+		$this->assertEquals( "$wgScript/User:Bob", $title->escapeLocalURL(),
+			'Title::escapeLocalURL() does NOT have fragment' );
+		$this->assertEquals( "$wgScript/User:Bob#section", $title->getLinkURL(),
+			'Title::getLinkURL() does have fragment' );
+		
+		#$this->assertEquals( 'toto', $title->getFullURL()     );
+		#$this->assertEquals( 'toto', $title->escapeFullURL()  );
 	}
 }
