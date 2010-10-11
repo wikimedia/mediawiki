c3185e6167e926d20115150d082a7a6794d7098f
Derk-Jan Hartman <hartman@users.mediawiki.org>
2010-10-11 15:20:57 +0000

    Remove $wgRemoteUploads. It was not well supported and superseded by $wgUploadNavigationUrl.
    Follow up to r66566

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -394,13 +394,6 @@ $wgUseInstantCommons = false;
 $wgShowEXIF = function_exists( 'exif_read_data' );
 
 /**
- * Set to true to enable the upload _link_ while local uploads are disabled.
- * Assumes that the special page link will be bounced to another server where
- * uploads do work.
- */
-$wgRemoteUploads = false;
-
-/**
  * If you operate multiple wikis, you can define a shared upload path here.
  * Uploads to this wiki will NOT be put there - they will be put into
  * $wgUploadDirectory.
