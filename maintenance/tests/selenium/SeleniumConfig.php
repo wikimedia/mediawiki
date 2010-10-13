f9894d559b1e1c00db35e3c1f2c7aad0d3548823
Markus Glaser <mglaser@users.mediawiki.org>
2010-10-13 23:52:42 +0000

    * handle PHP 5.2 ini files * sample ini file for PHP 5.2 * credits to dan nessett

--- maintenance/tests/selenium/SeleniumConfig.php
+++ maintenance/tests/selenium/SeleniumConfig.php
@@ -10,6 +10,7 @@ class SeleniumConfig {
 	 * See sample config file in selenium_settings.ini.sample
 	 * 
 	 */
+
 	public static function getSeleniumSettings ( &$seleniumSettings, 
 			&$seleniumBrowsers, 
 			&$seleniumTestSuites, 
@@ -23,7 +24,16 @@ class SeleniumConfig {
 			throw new MWException( "Unable to read local Selenium Settings from " . $seleniumConfigFile . "\n" );
 		}
 		
-		$configArray = parse_ini_file($seleniumConfigFile, true);
+		if ( !defined( 'PHP_VERSION_ID' ) ||
+			( PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION < 3 ) ) {
+			$configArray = self::parse_5_2_ini_file( $seleniumConfigFile );
+		} else {
+			$configArray = parse_ini_file( $seleniumConfigFile, true );
+		}
+		if ( $configArray === false ) {
+			throw new MWException( "Error parsing " . $seleniumConfigFile . "\n" );
+		}
+
 		if ( array_key_exists( 'SeleniumSettings', $configArray)  ) {
 			wfSuppressWarnings();
 			//we may need to change how this is set. But for now leave it in the ini file
@@ -43,6 +53,30 @@ class SeleniumConfig {
 			wfRestoreWarnings();
 		}
 		return true;	
+	}
+
+	private static function parse_5_2_ini_file ( $ConfigFile ) {
+
+		$configArray = parse_ini_file( $ConfigFile, true );
+		if ( $configArray === false ) return false;
+
+		// PHP 5.2 ini files have [browsers] and [testSuite] sections
+		// to get around lack of support for array keys. It then
+		// inserts the section arrays into the appropriate places in
+		// the SeleniumSettings and SeleniumTests arrays.
+
+		if ( isset( $configArray[browsers] ) ) {
+			$configArray[SeleniumSettings][browsers] = $configArray[browsers];
+			unset ( $configArray[browsers] );
+		}
+
+		if ( isset( $configArray[testSuite] ) ) {
+			$configArray[SeleniumTests][testSuite] = $configArray[testSuite];
+			unset ( $configArray[testSuite] );
+		}
+
+		return $configArray;
+
 	}
 
 }
