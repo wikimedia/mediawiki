5e3fca9f401fa3f189fc03c4a7c58bb1ff959219
Krinkle <krinkle@users.mediawiki.org>
2010-10-24 19:32:11 +0000

    added support for standard, cologneblue, nostalgia in mw.util.addPortletLink + updated the test suite

--- resources/mediawiki/mediawiki.utiltest.js
+++ resources/mediawiki/mediawiki.utiltest.js
@@ -8,7 +8,6 @@
 	mediaWiki.test = {
 
 		/* Variables */
-		'$bodyContent' : null,
 		'$table' : null,
 		'addedTests' : [],
 
@@ -18,10 +17,14 @@
 		* Adds a row to the test-table
 		*
 		* @param String code	Code of the test to be executed
-		* @param String result	Expected result in 'var (vartype)' form.
+		* @param String result	Expected result in 'var (vartype)' form
+		* @param String contain	Important part of the result, if result is different but does contain this it will not return ERROR but PARTIALLY
 		*/
-		'addTest' : function (code, result) {
-			this.addedTests.push([code, result]);
+		'addTest' : function (code, result, contain) {
+			if (!contain) {
+				contain = result;
+			}
+			this.addedTests.push([code, result, contain]);
 			this.$table.append('<tr><td>' + mw.util.htmlEscape(code) + '</td><td>' + mw.util.htmlEscape(result) + '<td></td></td><td>?</td></tr>');
 		},
 
@@ -36,8 +39,7 @@
 						// Build page
 						document.title = 'mediaWiki.util JavaScript Test - ' + wgSiteName;
 						$('#firstHeading').text('mediaWiki.util JavaScript Test');
-						mw.test.bodyContent = $('#bodyContent');
-						mw.test.bodyContent.html(
+						mw.util.$content.html(
 							'<p>Below is a list of tests to confirm proper functionality of the mediaWiki.util functions</p>' +
 							'<hr />' +
 							'<table id="mw-mwutiltest-table" class="wikitable sortable"><tr><th>Exec</th><th>Should return</th><th>Does return</th><th>Equal ?</th></tr></table>'
@@ -95,14 +97,17 @@
 							'function (string)');
 						mw.test.addTest('typeof mw.util.addPortletLink("p-tb", "http://mediawiki.org/", "MediaWiki.org", "t-mworg", "Go to MediaWiki.org ", "m", "#t-print")',
 							'object (string)');
-						mw.test.addTest('mw.util.addPortletLink("p-tb", "http://mediawiki.org/", "MediaWiki.org", "t-mworg", "Go to MediaWiki.org ", "m", "#t-print").outerHTML',
-							'<li id="t-mworg"><span><a href="http://mediawiki.org/" accesskey="m" title="Go to MediaWiki.org  [ctrl-alt-m]">MediaWiki.org</a></span></li> (string)');
+						mw.test.addTest('a = mw.util.addPortletLink("p-tb", "http://mediawiki.org/", "MediaWiki.org", "t-mworg", "Go to MediaWiki.org ", "m", "#t-print"); if(a){ a.outerHTML; }',
+							'<li id="t-mworg"><span><a href="http://mediawiki.org/" accesskey="m" title="Go to MediaWiki.org  [ctrl-alt-m]">MediaWiki.org</a></span></li> (string)',
+							'href="http://mediawiki.org/"');
 
 						// Run tests and compare results
 						var	exec,
 							result,
 							resulttype,
 							numberoftests = 0,
+							numberofpasseds = 0,
+							numberofpartials = 0,
 							numberoferrors = 0,
 							$testrows;
 						$testrows = mw.test.$table.find('tr');
@@ -111,13 +116,20 @@
 								
 								exec = mw.test.addedTests[i][0];
 								shouldreturn = mw.test.addedTests[i][1];
+								shouldcontain = mw.test.addedTests[i][2];
 								doesreturn = eval(exec);
 								doesreturn = doesreturn + ' (' + typeof doesreturn + ')';
 								$thisrow = $testrows.eq(i + 1);
 								$thisrow.find('> td').eq(2).text(doesreturn);
 								
-								if (shouldreturn === doesreturn) {
-									$thisrow.find('> td').eq(3).css('background', '#EFE').text('OK');
+								if (doesreturn.indexOf(shouldcontain) !== -1) {
+									if (doesreturn == shouldreturn){
+										$thisrow.find('> td').eq(3).css('background', '#EFE').text('OK');
+										numberofpasseds++;
+									} else {
+										$thisrow.find('> td').eq(3).css('background', '#FFE').html('<small>PARTIALLY</small>');
+										numberofpartials++;
+									}
 								} else {
 									$thisrow.find('> td').eq(3).css('background', '#FEE').text('ERROR');
 									numberoferrors++;
@@ -125,7 +137,7 @@
 
 							})
 						);
-						mw.test.$table.before('<p><strong>Ran ' + numberoftests + ' tests. ' + numberoferrors + ' error(s). </p>');
+						mw.test.$table.before('<p><strong>Ran ' + numberoftests + ' tests. ' + numberofpasseds + ' passed test(s). ' + numberoferrors + ' error(s). ' + numberofpartials + ' partially passed test(s). </p>');
 
 					}
 				});
