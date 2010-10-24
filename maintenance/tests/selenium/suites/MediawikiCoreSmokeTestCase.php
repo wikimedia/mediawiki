d8ab89b4cf91bfbfaede68a4e83776485cbb849f
Priyanka Dhanda <pdhanda@users.mediawiki.org>
2010-10-24 15:09:13 +0000

    Moved tests to a dub directory. Added a stub smoke test that we're going to start populating

---
+++ maintenance/tests/selenium/suites/MediawikiCoreSmokeTestCase.php
@@ -0,0 +1,47 @@
+<?php
+/* 
+ * Stub of tests be need as part of the hack-a-ton
+ */
+class MediawikiCoreSmokeTestCase extends SeleniumTestCase {
+	public function testUserLogin() {
+		
+	}
+	
+	public function testChangeUserPreference() {
+		
+	}
+	
+	/*
+	 * TODO: generalize this test to be reusable for different skins
+	 */
+	public function testCreateNewPageVector() {
+		
+	}
+	
+	/*
+	 * TODO: generalize this test to be reusable for different skins
+	 */
+	public function testEditExistingPageVector() {
+		
+	}
+	
+	/*
+	 * TODO: generalize this test to be reusable for different skins
+	 */
+	public function testCreateNewPageMonobook() {
+		
+	}
+	
+	/*
+	 * TODO: generalize this test to be reusable for different skins
+	 */
+	public function testEditExistingPageMonobook() {
+		
+	}
+	
+	public function testImageUpload() {
+		
+	}
+	
+
+}
