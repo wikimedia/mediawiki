c93abc66e6e2352f5e0a0dd8cc9b4af85d23bad5
Antoine Musso <hashar@users.mediawiki.org>
2010-10-27 19:19:11 +0000

    Compare latency between http and https by querying localhost.

---
+++ maintenance/benchmarks/bench_HTTP_HTTPS.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This come from r75429 message
+ * @author Platonides 
+ */
+
+require_once( dirname( __FILE__ ) . '/Benchmarker.php' );
+class bench_HTTP_HTTPS extends Benchmarker {
+
+	public function __construct() {
+		parent::__construct();	
+	}
+
+	public function execute() {
+		$this->bench( array(
+			array( 'function' => array( $this, 'getHTTP' ) ),
+			array( 'function' => array( $this, 'getHTTPS' ) ),
+		));
+		print $this->getFormattedResults();
+	}
+
+	static function doRequest( $proto ) {
+		Http::get( "$proto://localhost/" );
+	}
+
+	// bench function 1
+	function getHTTP() {
+		$this->doRequest( 'http' );
+	}
+
+	// bench function 2
+	function getHTTPS() {
+		$this->doRequest( 'https' );
+	}
+}
+
+$maintClass = 'bench_HTTP_HTTPS';
+require_once( DO_MAINTENANCE );
