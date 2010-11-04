08b577352314755187803b49d4d3a934ed3a3b86
Sam Reed <reedy@users.mediawiki.org>
2010-11-04 01:59:26 +0000

    Minor expansion of document. Per patch by Jonathan Wiltshire on bug 24132

--- maintenance/Maintenance.php
+++ maintenance/Maintenance.php
@@ -838,7 +838,8 @@ abstract class Maintenance {
 
 		if ( !is_readable( $settingsFile ) ) {
 			$this->error( "A copy of your installation's LocalSettings.php\n" .
-						"must exist and be readable in the source directory.", true );
+						"must exist and be readable in the source directory.\n" .
+						"Use --conf to specify it." , true );
 		}
 		$wgCommandLineMode = true;
 		return $settingsFile;
