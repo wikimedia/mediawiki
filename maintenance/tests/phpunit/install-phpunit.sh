abbd8a508ac9f745576978127f070084a89b1f74
Chad Horohoe <demon@users.mediawiki.org>
2010-11-16 15:29:48 +0000

    Move preference for PEAR higher up the list, apt and yum repos tend to be out of date

--- maintenance/tests/phpunit/install-phpunit.sh
+++ maintenance/tests/phpunit/install-phpunit.sh
@@ -7,16 +7,16 @@ fi
 
 if ( has_binary phpunit ); then
     echo PHPUnit already installed
+else if ( has_binary pear ); then
+    echo Installing phpunit with pear
+	pear channel-discover pear.phpunit.de
+    pear install phpunit/PHPUnit
 else if ( has_binary apt-get ); then
     echo Installing phpunit with apt-get
     apt-get install phpunit
 else if ( has_binary yum ); then
     echo Installing phpunit with yum
     yum install phpunit
-else if ( has_binary pear ); then
-    echo Installing phpunit with pear
-    pear channel-discover pear.phpunit.de
-    pear install phpunit/PHPUnit
 fi
 fi
 fi
