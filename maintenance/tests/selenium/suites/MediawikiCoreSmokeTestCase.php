901721bdb5020f6144d3db6d45f083acbacb2be9
Markus Glaser <mglaser@users.mediawiki.org>
2010-11-18 16:12:41 +0000

    * simple smoke test for file upload

--- maintenance/tests/selenium/suites/MediawikiCoreSmokeTestCase.php
+++ maintenance/tests/selenium/suites/MediawikiCoreSmokeTestCase.php
@@ -40,7 +40,29 @@ class MediawikiCoreSmokeTestCase extends SeleniumTestCase {
 	}
 	
 	public function testImageUpload() {
+		$this->login();
+		$this->open( $this->getUrl() .
+			'/index.php?title=Special:Upload' );
+		$this->type( 'wpUploadFile', dirname( __FILE__ ) .
+			"\\..\\data\\Wikipedia-logo-v2-de.png" );
+		$this->check( 'wpIgnoreWarning' );
+		$this->click( 'wpUpload' );
+		$this->waitForPageToLoad( 30000 );
 		
+		$this->assertSeleniumHTMLContains(
+				'//h1[@class="firstHeading"]', "Wikipedia-logo-v2-de.png" );
+		
+		/*
+		$this->open( $this->getUrl() . '/index.php?title=Image:'
+			. ucfirst( $this->filename ) . '&action=delete' );
+		$this->type( 'wpReason', 'Remove test file' );
+		$this->click( 'mw-filedelete-submit' );
+		$this->waitForPageToLoad( 10000 );
+
+		// Todo: This message is localized
+		$this->assertSeleniumHTMLContains( '//div[@id="bodyContent"]/p',
+			ucfirst( $this->filename ) . '.*has been deleted.' );
+		 */
 	}
 	
 
