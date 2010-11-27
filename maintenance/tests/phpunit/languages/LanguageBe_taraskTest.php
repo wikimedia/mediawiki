851b89a76671f0728517b41d7b4100dbf01d26ab
Antoine Musso <hashar@users.mediawiki.org>
2010-11-27 11:29:23 +0000

    Test normalizeForSearch and commafy
    See bug 23156 and r64981

---
+++ maintenance/tests/phpunit/languages/LanguageBe_taraskTest.php
@@ -0,0 +1,31 @@
+<?php
+require_once dirname(dirname(__FILE__)). '/bootstrap.php';
+
+class LanguageBeTaraskTest extends MediaWikiTestSetup {
+	private $lang;
+
+	function setUp() {
+		$this->lang = Language::factory( 'Be_tarask' );
+	}
+	function tearDown() {
+		unset( $this->lang );
+	}
+
+	/** see bug 23156 & r64981 */
+	function testSearchRightSingleQuotationMarkAsApostroph() {
+		$this->assertEquals(
+			"'",
+			$this->lang->normalizeForSearch( '’' ),
+			'bug 23156: U+2019 conversion to U+0027'
+		);
+	}
+	/** see bug 23156 & r64981 */
+	function testCommafy() {
+		$this->assertEquals( '1,234,567', $this->lang->commafy( '1234567' ) );
+		$this->assertEquals(    '12,345', $this->lang->commafy(   '12345' ) );
+	}
+	/** see bug 23156 & r64981 */
+	function testDoesNotCommafyFourDigitsNumber() {
+		$this->assertEquals(      '1234', $this->lang->commafy(    '1234' ) );
+	}
+}
