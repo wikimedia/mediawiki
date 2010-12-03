7876c1d324b3a2bdcbefad4a8846f9bd8d379d04
nadeesha <nadeesha@users.mediawiki.org>
2010-12-03 14:12:52 +0000

    Add following common functions for selenium scripts. getExistingPage(), getNewPage($pageName), loadWikiEditor(), clearWikiEditor(), clickShowPreviewBtn(), clickSavePageBtn(), clickEditLink(), deletePage($pageName)

--- maintenance/tests/selenium/SeleniumTestCase.php
+++ maintenance/tests/selenium/SeleniumTestCase.php
@@ -26,4 +26,78 @@ class SeleniumTestCase extends PHPUnit_Framework_TestCase { // PHPUnit_Extension
 		$this->assertRegExp( "/$text/", $innerHTML );
 	}
 
+//Common Funtions Added for Selenium Tests
+
+        public function getExistingPage(){
+                $this->open( $this->getUrl() .
+			'/index.php?title=Main_Page&action=edit' );
+                $this->type("searchInput", "new" );
+                $this->click("searchGoButton");
+                $this->waitForPageToLoad("30000");
+        }
+
+        public function getNewPage($pageName){
+
+                $this->open( $this->getUrl() .
+			'/index.php?title=Main_Page&action=edit' );
+                $this->type("searchInput", $pageName );
+                $this->click("searchGoButton");
+                $this->waitForPageToLoad("30000");
+                $this->click("link=".$pageName);
+                $this->waitForPageToLoad("600000");
+
+
+        }
+       // Loading the mediawiki editor
+        public function loadWikiEditor(){
+                $this->open( $this->getUrl() .
+			'/index.php?title=Main_Page&action=edit' );
+        }
+
+        // Clear the content of the mediawiki editor
+        public function clearWikiEditor(){
+                $this->type("wpTextbox1", "");
+        }
+
+        // Click on the 'Show preview' button of the mediawiki editor
+        public function clickShowPreviewBtn(){
+                $this->click("wpPreview");
+        }
+
+        // Click on the 'Save Page' button of the mediawiki editor
+        public function clickSavePageBtn(){
+                $this->click("wpSave");
+        }
+
+        // Click on the 'Edit' link
+        public function clickEditLink(){
+                $this->click("link=Edit");
+                $this->waitForPageToLoad("30000");
+        }
+
+        public function deletePage($pageName){
+                $isLinkPresent  = False;
+                $this->open( $this->getUrl() .
+			'/index.php?title=Main_Page&action=edit' );
+                $this->click("link=Log out");
+                $this->waitForPageToLoad( "30000" );
+                $this->click( "link=Log in / create account" );
+                $this->waitForPageToLoad( "30000" );
+                $this->type( "wpName1", "nadeesha" );
+                $this->type( "wpPassword1", "12345" );
+                $this->click( "wpLoginAttempt" );
+                $this->waitForPageToLoad( "30000" );
+                $this->type( "searchInput", $pageName );
+                $this->click( "searchGoButton");
+                $this->waitForPageToLoad( "30000" );
+
+                $this->click( "link=Delete" );
+                $this->waitForPageToLoad( "30000" );
+                $this->click( "wpConfirmB" );
+                $this->waitForPageToLoad( "30000" );
+
+        }
+
+
+
 }
