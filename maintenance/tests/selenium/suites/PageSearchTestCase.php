a477c099cad10791148b1475107e0fde815dacad
nadeesha <nadeesha@users.mediawiki.org>
2010-12-03 14:10:09 +0000

    Added test scripts for wiki editor additional features and updated scripts on wiki basic editor.  MediaWikiCommonFunction.php is deleted, since it is no longer used by the scripts.

--- maintenance/tests/selenium/suites/PageSearchTestCase.php
+++ maintenance/tests/selenium/suites/PageSearchTestCase.php
@@ -1,123 +1,105 @@
 <?php
-/* 
- * This test case is part of the SimpleSeleniumTestSuite.
- * Configuration for these tests are dosumented as part of SimpleSeleniumTestSuite.php
+
+/**
+ * Selenium server manager
+ *
+ * @file
+ * @ingroup Maintenance
+ * Copyright (C) 2010 Dan Nessett <dnessett@yahoo.com>
+ * http://citizendium.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @addtogroup Maintenance
+ *
  */
-class PageSearchTestCase extends SeleniumTestCase{
 
-             // Verify the functionality of the 'Go' button
-             public function testPageSearchBtnGo(){
+class PageSearchTestCase extends SeleniumTestCase {
 
-                $this->open( $this->getUrl() .
-			'/index.php?title=Main_Page&action=edit' );
-                $this->type( "searchInput", "calcey qa" );
-                $this->click( "searchGoButton" );
-                $this->waitForPageToLoad( "600000" );
+    // Verify the functionality of the 'Go' button
+    public function testPageSearchBtnGo() {
 
-                // Verify  no page matched with the entered search text
-                $source = $this->gettext( "//div[@id='bodyContent']/div[4]/p/b" );
-                $correct = strstr ( $source, "Create the page \"Calcey qa\" on this wiki!" );
+        $this->open( $this->getUrl() .
+                '/index.php?title=Main_Page&action=edit' );
+        $this->type( "searchInput", "calcey qa" );
+        $this->click( "searchGoButton" );
+        $this->waitForPageToLoad( "600000" );
 
-                try{
-                    $this->assertEquals( $correct, true );
-                    echo "\n Pass : No page matched with the entered search text using the 'Go' button \n";
-                } catch (PHPUnit_Framework_AssertionFailedError $e){
-                    echo "\n Pass : No page matched with the entered search text using the 'Go' button \n";
-                }
+        // Verify  no page matched with the entered search text
+        $source = $this->gettext( "//div[@id='bodyContent']/div[4]/p/b" );
+        $correct = strstr ( $source, "Create the page \"Calcey qa\" on this wiki!" );
+        $this->assertEquals( $correct, true );
 
-               $this->click( "link=Calcey qa" );
-               $this->waitForPageToLoad( "600000" );
+        $this->click( "link=Calcey qa" );
+        $this->waitForPageToLoad( "600000" );
 
-               $this->type( "wpTextbox1", "Calcey QA team" );
-               $this->click( "wpSave" );
-               $this->waitForPageToLoad( "600000" );
+        $this->type( "wpTextbox1", "Calcey QA team" );
+        $this->click( "wpSave" );
+        $this->waitForPageToLoad( "600000" );
 
-            }
+    }
 
-            // Verify the functionality of the 'Search' button
-            public function testPageSearchBtnSearch(){
+    // Verify the functionality of the 'Search' button
+    public function testPageSearchBtnSearch() {
 
-                $this->open( $this->getUrl() .
-			'/index.php?title=Main_Page&action=edit' );
-                $this->type( "searchInput", "Calcey web" );
-                $this->click( "mw-searchButton" );
-                $this->waitForPageToLoad( "30000" );
+        $this->open( $this->getUrl() .
+                '/index.php?title=Main_Page&action=edit' );
+        $this->type( "searchInput", "Calcey web" );
+        $this->click( "mw-searchButton" );
+        $this->waitForPageToLoad( "30000" );
 
-                 // Verify  no page is available as the search text
-                $source = $this->gettext( "//div[@id='bodyContent']/div[4]/p[2]/b" );
-                $correct = strstr ( $source, "Create the page \"Calcey web\" on this wiki!" );
+        // Verify  no page is available as the search text
+        $source = $this->gettext( "//div[@id='bodyContent']/div[4]/p[2]/b" );
+        $correct = strstr ( $source, "Create the page \"Calcey web\" on this wiki!" );
+        $this->assertEquals( $correct, true );
 
-                try{
-                    $this->assertEquals( $correct, true );
-                    echo "\n Pass : No page matched with the entered search text using the 'Search' button \n";
-                } catch (PHPUnit_Framework_AssertionFailedError $e){
-                    echo "\n Pass : No page matched with the entered search text using the 'Search' button \n";
-                }
+        $this->click( "link=Calcey web" );
+        $this->waitForPageToLoad( "600000" );
 
-               $this->click( "link=Calcey web" );
-               $this->waitForPageToLoad( "600000" );
+        $this->type( "wpTextbox1", "Calcey web team" );
+        $this->click( "wpSave" );
+        $this->waitForPageToLoad( "600000" );
 
-               $this->type( "wpTextbox1", "Calcey web team" );
-               $this->click( "wpSave" );
-               $this->waitForPageToLoad( "600000" );
+        // Verify saved page is opened  when the exact page name is given
+        $this->type( "searchInput", "Calcey web" );
+        $this->click( "mw-searchButton" );
+        $this->waitForPageToLoad( "30000" );
 
+        // Verify exact page matched with the entered search text using 'Search' button
+        $source = $this->getText( "//*[@id='bodyContent']/div[4]/p/b" );
+        $correct = strstr( $source, "There is a page named \"Calcey web\" on this wiki." );
+        $this->assertEquals( $correct, true );
 
-               // Verify saved page is opened  when the exact page name is given
-               $this->type( "searchInput", "Calcey web" );
-               $this->click( "mw-searchButton" );
-               $this->waitForPageToLoad( "30000" );
+        // Verify resutls available when partial page name is entered as the search text
+        $this->type( "searchInput", "Calcey" );
+        $this->click( "mw-searchButton" );
+        $this->waitForPageToLoad( "30000" );
 
-               $source = $this->getText( "//*[@id='bodyContent']/div[4]/p/b" );
-               $correct = strstr( $source, "There is a page named \"Calcey web\" on this wiki." );
+        //  Verify text avaialble in the search result under the page titles
+        if($this->isElementPresent( "Page_title_matches" )) {
+            $textPageTitle = $this->getText( "//*[@id='bodyContent']/div[4]/ul[1]/li[1]/div[1]/a" );
+            $this->assertContains( 'Calcey', $textPageTitle );
+        }
 
-               try{
-                   $this->assertEquals( $correct, true );
-                   echo "\n Pass : Exact page matched with the entered search text using 'Search' button \n";
-               } catch ( PHPUnit_Framework_AssertionFailedError $e ){
-                   echo "\n Fail : Exact page matched with the entered search text using 'Search' button \n";
-               }
-
-
-               // Verify resutls available when partial page name is entered as the search text
-
-               $this->type( "searchInput", "Calcey" );
-               $this->click( "mw-searchButton" );
-               $this->waitForPageToLoad( "30000" );
-
-               //  Verify text avaialble in the search result under the page titles
-               if($this->isElementPresent( "Page_title_matches" )){
-                   try{
-                       $textPageTitle = $this->getText( "//*[@id='bodyContent']/div[4]/ul[1]/li[1]/div[1]/a" );
-                       $this->assertContains( 'Calcey', $textPageTitle );
-                       echo "\n Pass : Results displayed under 'Page title matches' heading for the partial title match match using the 'Search' button\n";
-                } catch ( PHPUnit_Framework_AssertionFailedError $e ){
-                       echo "\n Fail : Results displayed under 'Page title matches' heading for the partial title match match using the 'Search' button \n";
-                }
-               }
-               else{
-                    echo "\n Pass : No results avaialble for the 'Page title matches' heading for the partial title match using the 'Search' button \n";
-               }
-
-                //  Verify text avaialble in the search result under the page text
-                if($this->isElementPresent( "Page_text_matches" )){
-                    try{
-                        $textPageText = $this->getText( "//*[@id='bodyContent']/div[4]/ul[2]/li[2]/div[2]/span" );
-                        $this->assertContains( 'Calcey', $textPageText );
-                        
-                        echo "\n Pass : Results displayed under 'Page text matches' heading for the partial text match using the 'Search' button \n";
-
-                    } catch ( PHPUnit_Framework_AssertionFailedError $e ){
-                        echo "\n Fail : Results displayed under 'Page text matches' heading for the partial text match using the 'Search' button \n";
-                    }
-                }
-                else{
-                    echo "\n Pass : No results avaialble for the 'Page text matches' heading for the partial title match using the 'Search' button\n";
-                }
-               $this->deletePage("Calcey QA");
-               $this->deletePage("Calcey web");
-               
-         }
-
-
-        
+        //  Verify text avaialble in the search result under the page text
+        if($this->isElementPresent( "Page_text_matches" )) {
+            $textPageText = $this->getText( "//*[@id='bodyContent']/div[4]/ul[2]/li[2]/div[2]/span" );
+            $this->assertContains( 'Calcey', $textPageText );
+        }
+        $this->deletePage("Calcey QA");
+        $this->deletePage("Calcey web");
+    }
 }
