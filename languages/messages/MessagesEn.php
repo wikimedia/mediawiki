e6daa8a2722d6eb4db5cd38786de64f1651fa94e
Sam Reed <reedy@users.mediawiki.org>
2010-12-03 09:36:02 +0000

    Followup r77618 per Nikerabbit, remove now unused message 'cantcreateaccount-nonblock-text'

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1413,7 +1413,6 @@ Please check the comparison below to verify that this is what you want to do, an
 'cantcreateaccount-text'          => "Account creation from this IP address ('''$1''') has been blocked by [[User:$3|$3]].
 
 The reason given by $3 is ''$2''",
-'cantcreateaccount-nonblock-text' => '', # do not translate or duplicate this message to other languages
 
 # History pages
 'viewpagelogs'           => 'View logs for this page',
