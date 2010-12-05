2ef6a1a46ea9a03d62c66ec80d69fca25961ab66
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-12-05 10:41:58 +0000

    Follow-up r77763, get rid of boolean param and whitespace fixes

--- includes/Skin.php
+++ includes/Skin.php
@@ -1633,18 +1633,18 @@ class Skin extends Linker {
 	/**
 	 * Renders a $wgFooterIcons icon acording to the method's arguments
 	 * @param $icon Array: The icon to build the html for
-	 * @param $useImg Boolean: Whether to use the icon's image or output a text-only footericon
+	 * @param $withImage Boolean: Whether to use the icon's image or output a text-only footericon
 	 */
-	function makeFooterIcon( $icon, $useImg = true ) {
-		if ( is_string($icon) ) {
+	function makeFooterIcon( $icon, $withImage = 'withImage' ) {
+		if ( is_string( $icon ) ) {
 			$html = $icon;
-		} else {
+		} else { // Assuming array
 			$url = $icon["url"];
-			unset($icon["url"]);
-			if ( isset($icon["src"]) && $useImg ) {
+			unset( $icon["url"] );
+			if ( isset( $icon["src"] ) && $withImage === 'withImage' ) {
 				$html = Html::element( 'img', $icon ); // do this the lazy way, just pass icon data as an attribute array
 			} else {
-				$html = htmlspecialchars($icon["alt"]);
+				$html = htmlspecialchars( $icon["alt"] );
 			}
 			if ( $url ) {
 				$html = Html::rawElement( 'a', array( "href" => $url ), $html );
