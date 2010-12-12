89bc577c5ae63cd7161a24c0571620f5acf4a0cd
Benjamin Lees <emufarmers@users.mediawiki.org>
2010-12-12 23:19:24 +0000

    Follow-up to r77956: instead of making $wgFileExtensions override $wgFileBlacklist, make sure the upload page does not mark blacklisted extensions as "Permitted file types".  I've updated the MediaWiki.org pages to explain how to remove items from the blacklist.

--- includes/specials/SpecialUpload.php
+++ includes/specials/SpecialUpload.php
@@ -899,6 +899,9 @@ class UploadForm extends HTMLForm {
 		$wgFileExtensions, $wgFileBlacklist;
 
 		if( $wgCheckFileExtensions ) {
+			//don't show blacklisted types as permitted
+			$wgFileExtensions = array_diff ( $wgFileExtensions, $wgFileBlacklist );
+			
 			if( $wgStrictFileExtensions ) {
 				# Everything not permitted is banned
 				$extensionsList =
