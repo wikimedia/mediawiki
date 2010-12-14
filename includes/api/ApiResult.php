d8a241f6a7a0b70b256e0fb1b51a8ab636ad07f2
Sam Reed <reedy@users.mediawiki.org>
2010-12-14 21:40:14 +0000

    * (bug 26339) Throw warning when truncating an overlarge API result

--- includes/api/ApiResult.php
+++ includes/api/ApiResult.php
@@ -257,6 +257,9 @@ class ApiResult extends ApiBase {
 		if ( $this->mCheckingSize ) {
 			$newsize = $this->mSize + self::size( $value );
 			if ( $newsize > $wgAPIMaxResultSize ) {
+				$this->setWarning(
+					"This result was truncated because it would otherwise be larger than the " .
+							"limit of {$wgAPIMaxResultSize} bytes" );
 				return false;
 			}
 			$this->mSize = $newsize;
