2f2a74de9d92e9724edabe574ee82f46298c2ce9
Chad Horohoe <demon@users.mediawiki.org>
2010-12-14 16:28:52 +0000

    svn ci'd from the wrong directory, missed the deletes in r78383

--- maintenance/tests/selenium/SeleniumLoader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class SeleniumLoader {
-	static function load() {
-		require_once( 'Testing/Selenium.php' );
-		require_once( 'PHPUnit/Framework.php' );
-		require_once( 'PHPUnit/Extensions/SeleniumTestCase.php' );
-	}
-}
