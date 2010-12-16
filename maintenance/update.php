9f5d06527c649911ba4dcbe1c42df728d76faabe
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2010-12-16 19:15:12 +0000

    Part of bug 26280: added license headers to PHP files in maintenance

--- maintenance/update.php
+++ maintenance/update.php
@@ -5,6 +5,21 @@
  * This is used when the database schema is modified and we need to apply patches.
  * It is kept compatible with php 4 parsing so that it can give out a meaningful error.
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
  * @file
  * @todo document
  * @ingroup Maintenance
