7b3f28e55f6eeed0e58077de7fdf74f88aadabad
Happy-melon <happy-melon@users.mediawiki.org>
2010-12-20 19:03:00 +0000

    Revert rollback implementation of r78585, r78599.  The way the API is set up, it's not possible to have some tokens on the new system and some on the old.

--- includes/Linker.php
+++ includes/Linker.php
@@ -1500,7 +1500,7 @@ class Linker {
 		$query = array(
 			'action' => 'rollback',
 			'from' => $rev->getUserText(),
-			'token' => Token::prepare( array( $title->getPrefixedText(), $rev->getUserText() ) ),
+			'token' => $wgUser->editToken( array( $title->getPrefixedText(), $rev->getUserText() ) ),
 		);
 		if ( $wgRequest->getBool( 'bot' ) ) {
 			$query['bot'] = '1';
