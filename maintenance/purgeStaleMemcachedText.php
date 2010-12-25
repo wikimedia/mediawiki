4876dc3aef32349922d2f21f1c048e12cc90f14a
Platonides <platonides@users.mediawiki.org>
2010-12-25 21:58:50 +0000

    Whitespace. Add $wgDBname to the debug message.

--- maintenance/purgeStaleMemcachedText.php
+++ maintenance/purgeStaleMemcachedText.php
@@ -3,15 +3,15 @@
 require_once( dirname( __FILE__ ) . '/commandLine.inc' );
 
 function purgeStaleMemcachedText() {
-	global $wgMemc;
+	global $wgMemc, $wgDBname;
 	$db = wfGetDB( DB_MASTER );
 	$maxTextId = $db->selectField( 'text', 'max(old_id)' );
-	$latestReplicatedTextId = $db->selectField( array( 'revision','recentchanges'), 'rev_text_id', 
+	$latestReplicatedTextId = $db->selectField( array( 'recentchanges', 'revision' ), 'rev_text_id', 
 		array( 'rev_id = rc_this_oldid', "rc_timestamp < '20101225183000'"),  'purgeStaleMemcachedText', 
 		array( 'ORDER BY' => 'rc_timestamp DESC' ) );
 	$latestReplicatedTextId -= 100; # A bit of paranoia
 
-	echo "Going to purge text entries from $latestReplicatedTextId to $maxTextId\n";
+	echo "Going to purge text entries from $latestReplicatedTextId to $maxTextId in $wgDBname\n";
 
 	for ( $i = $latestReplicatedTextId; $i < $maxTextId; $i++ ) {
 		$key = wfMemcKey( 'revisiontext', 'textid', $i++ );
