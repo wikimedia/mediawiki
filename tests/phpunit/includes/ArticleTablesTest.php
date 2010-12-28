b548f1db22262e96eaab9f75e096c209113e1bbb
Platonides <platonides@users.mediawiki.org>
2010-12-28 18:43:49 +0000

    Add failing test for bug 14404.

---
+++ tests/phpunit/includes/ArticleTablesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @group Database
+ * @group Destructive
+ */
+class ArticleTablesTest extends PHPUnit_Framework_TestCase {
+	
+	function testbug14404() {
+		global $wgUser, $wgContLang, $wgLang;
+		
+		$title = Title::newFromText("Bug 14404");
+		$article = new Article( $title );
+		$wgUser = new User();
+		$wgUser->mRights = array( 'createpage', 'edit', 'purge' );
+		$wgContLang = Language::factory( 'es' );
+		
+		$wgLang = Language::factory( 'fr' );
+		$status = $article->doEdit( '{{:{{int:history}}}}', 'Test code for bug 14404', EDIT_NEW | EDIT_UPDATE );
+		$templates1 = $article->getUsedTemplates();
+
+		$wgLang = Language::factory( 'de' );		
+		$article->mParserOptions = null; // Let it pick the new user language
+		$article->mPreparedEdit = false; // In order to force the rerendering of the same wikitext
+		
+		// We need an edit, a purge is not enough to regenerate the tables
+		$status = $article->doEdit( '{{:{{int:history}}}}', 'Test code for bug 14404', EDIT_UPDATE );
+		$templates2 = $article->getUsedTemplates();
+		
+		$this->assertEquals( $templates1, $templates2 );
+		$this->assertEquals( $templates1[0]->getFullText(), 'Historial' );
+	}
+	
+}
