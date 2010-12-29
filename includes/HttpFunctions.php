c31f0bc39f709fe7bbdf3aaeb2300418d5020736
Platonides <platonides@users.mediawiki.org>
2010-12-29 16:15:23 +0000

    Follow up r67663. Now with 100% less sugar, er.. @s!

--- includes/HttpFunctions.php
+++ includes/HttpFunctions.php
@@ -867,12 +867,14 @@ class CurlHttpRequest extends MWHttpRequest {
 		}
 
 		if ( $this->followRedirects && $this->canFollowRedirects() ) {
-			if ( ! @curl_setopt( $curlHandle, CURLOPT_FOLLOWLOCATION, true ) ) {
+			wfSuppressWarnings();
+			if ( ! curl_setopt( $curlHandle, CURLOPT_FOLLOWLOCATION, true ) ) {
 				wfDebug( __METHOD__ . ": Couldn't set CURLOPT_FOLLOWLOCATION. " .
 					"Probably safe_mode or open_basedir is set.\n" );
 				// Continue the processing. If it were in curl_setopt_array,
 				// processing would have halted on its entry
 			}
+			wfRestoreWarnings();
 		}
 
 		if ( false === curl_exec( $curlHandle ) ) {
