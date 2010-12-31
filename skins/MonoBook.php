c510db2665c0f580d636f4c3aab72ac20722262b
Daniel Friesen <dantman@users.mediawiki.org>
2010-12-31 23:30:00 +0000

    Merging Vector's navigation_urls and SkinTemplate's content_actions code into content_navigation. content_actions is now built by folding content_navigation and cleaning it up a bit. content_actions hooks no longer work and have been dropped from the code, the hooks that affected vector before now affect all skins. A few logic changes were made to make for a clean merge: - vector was using vector-???-??? messages while SkinTemplate was using '???' messages. So as a side effect of merging that together all skins now support messages like '$skinname-view-history' which will fallback to the standard message if not defined. - For MediaWiki: pages where the page does not exist but the message does in the i18n system SkinTemplate displayed "Edit" while Vector displayed "Create"; All skins now display "Edit". - For users without undelete permissions SkinTemplate displayed an "Undelete" tab if the user had deletedhistory and deletedtext permissions. Vector would only display the tab for users with both deletedhistory and undelete permissions; The new behavior in all skins is to always display a tab if you have deletedhistory (since Special:Undelete will always have something to display) but display a "View ... deleted" instead of "Undelete" message if you do not have undelete permissions. - Skins no longer need to hardcode tests for the &action= to decide if they should ignore the accesskey on some tabs, tabs which should not have an accesskey in the current page now have a "tooltiponly" key set to true.

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -107,14 +107,7 @@ class MonoBookTemplate extends BaseTemplate {
 						echo ' class="'.htmlspecialchars($tab['class']).'"';
 					}
 					echo '><a href="'.htmlspecialchars($tab['href']).'"';
-					# We don't want to give the watch tab an accesskey if the
-					# page is being edited, because that conflicts with the
-					# accesskey on the watch checkbox.  We also don't want to
-					# give the edit tab an accesskey, because that's fairly su-
-					# perfluous and conflicts with an accesskey (Ctrl-E) often
-					# used for editing in Safari.
-				 	if( in_array( $action, array( 'edit', 'submit' ) )
-				 	&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
+				 	if( $tab["tooltiponly"] ) {
 				 		echo $skin->tooltip( "ca-$key" );
 				 	} else {
 				 		echo $skin->tooltipAndAccesskey( "ca-$key" );
