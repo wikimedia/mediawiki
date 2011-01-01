7c28be431ca291c85ca0b35df4175910ae847772
X! <soxred93@users.mediawiki.org>
2011-01-01 22:16:54 +0000

    Followup r79443: Add some form of organization to the test autoloader

--- tests/TestsAutoLoader.php
+++ tests/TestsAutoLoader.php
@@ -4,10 +4,20 @@ global $wgAutoloadClasses;
 $testFolder = dirname( __FILE__ );
 
 $wgAutoloadClasses += array(
+
+	//PHPUnit
 	'MediaWikiTestCase' => "$testFolder/phpunit/MediaWikiTestCase.php",
 	'MediaWikiPHPUnitCommand' => "$testFolder/phpunit/MediaWikiPHPUnitCommand.php",
-	'ApiTestSetup' => "$testFolder/phpunit/includes/api/ApiSetup.php",
-	'RandomImageGenerator' => "$testFolder/phpunit/includes/api/RandomImageGenerator.php",
+		
+		//API
+		'ApiTestSetup' => "$testFolder/phpunit/includes/api/ApiSetup.php",
+		'RandomImageGenerator' => "$testFolder/phpunit/includes/api/RandomImageGenerator.php",
+	
+		//Parser
+		'ParserTestFileIterator' => "$testFolder/phpunit/includes/parser/NewParserHelpers.php",
+		
+		
+	//Selenium
 	'SeleniumTestConstants' => "$testFolder/selenium/SeleniumTestConstants.php",
 );
 
