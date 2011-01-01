1b7f9fc8b46d4563cc4f18527af2a2f97b7cf2d4
Markus Glaser <mglaser@users.mediawiki.org>
2011-01-01 15:52:52 +0000

    SimpleSeleniumTest now has its own resources (database and images dir)

--- tests/selenium/suites/SimpleSeleniumConfig.php
+++ tests/selenium/suites/SimpleSeleniumConfig.php
@@ -1,15 +1,22 @@
 <?php
 class SimpleSeleniumConfig {
 	
-	public static function getSettings(&$includeFiles, &$globalConfigs) {
+	public static function getSettings(&$includeFiles, &$globalConfigs, &$resourceFiles) {
+		global $IP;
 		$includes = array(
 			//files that needed to be included would go here
 		);
 		$configs = array(
 			'wgDefaultSkin' => 'chick'
 		);
+		$resources = array(
+			'db' => "$IP/tests/selenium/data/SimpleSeleniumTestDB.sql",
+			'images' => "$IP/tests/selenium/data/SimpleSeleniumTestImages.zip"
+		);
+
 		$includeFiles = array_merge( $includeFiles, $includes );
 		$globalConfigs = array_merge( $globalConfigs, $configs);
+		$resourceFiles = array_merge( $resourceFiles, $resources );
 		return true; 
 	}
 }
