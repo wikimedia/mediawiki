99297e62966e9208b5458ff2750273636d05ac0c
X! <soxred93@users.mediawiki.org>
2011-01-01 05:53:04 +0000

    More work on new parser tests: -No need to specify a new function for each argument, use PHP5 magic to automatically set it -Create (g|s)etCliArg() functions -Implement some features from the old parser tests

--- tests/phpunit/MediaWikiPHPUnitCommand.php
+++ tests/phpunit/MediaWikiPHPUnitCommand.php
@@ -1,19 +1,34 @@
 <?php
 
 class MediaWikiPHPUnitCommand extends PHPUnit_TextUI_Command {
-	static $additionalArgs = array( 'verbose' => false );
+	
+	static $additionalOptions = array( 
+		'regex=' => false, 
+		'record' => false,
+		'file=' => false,
+		'keep-uploads' => false,
+	);
+	
+	//Fixme: These aren't shown on the --help menu
 
 	public function __construct() {
-		$this->longOptions['verbose'] = 'verboseHandler';
+		foreach( self::$additionalOptions as $option => $default ) {
+			$this->longOptions[$option] = $option . 'Handler';
+		}
+		
 	}
 	
 	public static function main( $exit = true ) {
         $command = new self;
         $command->run($_SERVER['argv'], $exit);
     }
-
-	protected function verboseHandler($value) {
-		self::$additionalArgs['verbose'] = true;
+    
+    public function __call( $func, $args ) {
+    	
+		if( substr( $func, -7 ) == 'Handler' ) {
+			if( is_null( $args[0] ) ) $args[0] = true; //Booleans
+			self::$additionalOptions[substr( $func, 0, -7 ) ] = $args[0];
+		}
 	}
-
+	
 }
