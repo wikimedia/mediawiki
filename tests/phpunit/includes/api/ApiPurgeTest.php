4cce1efbe7fc7d5c6fdc1647f6f324a5537bf58b
X! <soxred93@users.mediawiki.org>
2011-01-02 06:15:32 +0000

    Followup to r79477: Add invalid purge title

--- tests/phpunit/includes/api/ApiPurgeTest.php
+++ tests/phpunit/includes/api/ApiPurgeTest.php
@@ -22,16 +22,21 @@ class ApiPurgeTest extends ApiTestSetup {
 
 		$data = $this->doApiRequest( array(
 			'action' => 'purge',
-			'titles' => 'UTPage|' . $somePage ) );
+			'titles' => 'UTPage|' . $somePage . '|%5D' ) );
 	
 		$this->assertArrayHasKey( 'purge', $data[0] );
+		
 		$this->assertArrayHasKey( 0, $data[0]['purge'] );
 		$this->assertArrayHasKey( 'purged', $data[0]['purge'][0] );
 		$this->assertEquals( 'UTPage', $data[0]['purge'][0]['title'] );
 		
 		$this->assertArrayHasKey( 1, $data[0]['purge'] );
 		$this->assertArrayHasKey( 'missing', $data[0]['purge'][1] );
-		$this->assertEquals(  $somePage, $data[0]['purge'][1]['title'] );
+		$this->assertEquals( $somePage, $data[0]['purge'][1]['title'] );
+		
+		$this->assertArrayHasKey( 2, $data[0]['purge'] );
+		$this->assertArrayHasKey( 'invalid', $data[0]['purge'][2] );
+		$this->assertEquals( '%5D', $data[0]['purge'][2]['title'] );
 		
 	}
 
