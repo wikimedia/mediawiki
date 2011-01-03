e867a659245276448a93cf00f0907d92dab48f44
nadeesha <nadeesha@users.mediawiki.org>
2011-01-03 09:18:21 +0000

    Change the xpath of the 'You can now Folder/index.php start using your wiki' text.

--- tests/selenium/installer/MediaWikiUpgradeExistingDatabaseTestCase.php
+++ tests/selenium/installer/MediaWikiUpgradeExistingDatabaseTestCase.php
@@ -38,77 +38,77 @@ require_once (dirname(__FILE__).'/'.'MediaWikiInstallationCommonFunction.php');
 
 
 class MediaWikiUpgradeExistingDatabaseTestCase extends MediaWikiInstallationCommonFunction {
-
+    
     function setUp() {
         parent::setUp();
     }
-
+    
     // Install Mediawiki using 'MySQL' database type.
     public function testUpgradeExistingDatabase() {
-
+        
         $databaseName = DB_NAME_PREFIX."_upgrade_existing";
         parent::navigateInstallPage( $databaseName );
-
+        
         $this->open( "http://localhost:".PORT."/".DIRECTORY_NAME."/config/index.php" );
         $this->assertEquals( "Install", $this->getText( LINK_DIV."h2" ));
         $this->assertEquals( "Warning: You seem to have already installed MediaWiki and are trying to install it again. Please proceed to the next page.",
                 $this->getText( LINK_DIV."div[2]/form/div[1]/div[2]" ));
-
+        
         // 'Optionis' page
         parent::clickBackButton();
-
+        
         // 'Name' page
         parent::clickBackButton();
-
+        
         // 'Database settings' page
         parent::clickBackButton();
-
+        
         // 'Connect to database' page
         parent::clickBackButton();
         $this->type( "mysql_wgDBname", $databaseName );
         parent::clickContinueButton();
-
+        
         // 'Upgrade existing installation' page  displayed next to the 'Connect to database' page.
         $this->assertEquals( "Upgrade existing installation", $this->getText( LINK_DIV."h2" ));
-
+        
         // Warning message displayed.
         $this->assertEquals( "There are MediaWiki tables in this database. To upgrade them to MediaWiki 1.18alpha, click Continue.",
                 $this->getText( LINK_DIV."div[2]/form/div[1]/div[2]" ));
-
+        
         parent::clickContinueButton();
         $this->assertEquals( "Upgrade existing installation",
                 $this->getText( LINK_DIV."h2" ));
-
+        
         // 'Upgrade complete.' text display
         $this->assertEquals("Upgrade complete.",
-                 $this->getText( "[@id='bodyContent']/div/div[1]/div[4]/form/div[1]/div[2]/p[1]" ));
-   
+                $this->getText("//div[@id='bodyContent']/div/div[1]/div[4]/form/div[1]/div[2]/p[1]"));
+        
         $this->assertEquals("You can now Folder/index.php start using your wiki.",
-                $this->getText( "[@id='bodyContent']/div/div[1]/div[4]/form/div[1]/div[2]/p[2]" ));
-
-          $this->assertEquals( "Folder/index.php start using your wiki",
-                  $this->getText( "link=Folder/index.php start using your wiki" ));
-
+                $this->getText("//div[@id='bodyContent']/div/div[1]/div[4]/form/div[1]/div[2]/p[2]" ));
+        
+        $this->assertEquals( "Folder/index.php start using your wiki",
+                $this->getText( "link=Folder/index.php start using your wiki" ));
+        
         $this->assertTrue($this->isElementPresent( "submit-regenerate" ));
         $this->click( "submit-regenerate" );
         $this->waitForPageToLoad( PAGE_LOAD_TIME );
         $this->assertEquals( "Database settings",
                 $this->getText( LINK_DIV."h2" ));
-
+        
         // 'Database settings' page
         parent::clickContinueButton();
-
+        
         // Name page
         parent::completeNamePage();
-
+        
         // Options page
         parent::clickContinueButton();
-
+        
         // Install page
         $this->assertEquals( "Warning: You seem to have already installed MediaWiki and are trying to install it again. Please proceed to the next page.",
                 $this->getText( LINK_FORM."div[1]/div[2]" ));
         parent::clickContinueButton();
-
+        
         // complete
         parent::completePageSuccessfull();
         $this->chooseCancelOnNextConfirmation();
