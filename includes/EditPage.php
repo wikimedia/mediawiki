767da0cb6125d36fef5d2c30e5e73812f3f1c622
Brian Wolff <bawolff@users.mediawiki.org>
2011-01-07 00:17:52 +0000

    Comment in code says - truncate to 250 bytes, then add ellipse. but we don't add the ellipse, so change it to add the ellipse. The comment and code should be the same, and since we have the room might as well add the ellipse.
    This normally wouldn't affect much, since users with JS on
    will be prevented from adding that long a summary on the
    client side

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -557,7 +557,7 @@ class EditPage {
 			}
 
 			# Truncate for whole multibyte characters. +5 bytes for ellipsis
-			$this->summary = $wgLang->truncate( $request->getText( 'wpSummary' ), 250, '' );
+			$this->summary = $wgLang->truncate( $request->getText( 'wpSummary' ), 250 );
 
 			# Remove extra headings from summaries and new sections.
 			$this->summary = preg_replace('/^\s*=+\s*(.*?)\s*=+\s*$/', '$1', $this->summary);
