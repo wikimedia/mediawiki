fa0220c57c699287673a511b2753a69879e58af0
Sam Reed <reedy@users.mediawiki.org>
2011-01-10 04:44:33 +0000

    Documentation addition/tweaks

--- includes/Xml.php
+++ includes/Xml.php
@@ -620,7 +620,7 @@ class Xml {
 	 * Create a call to a JavaScript function. The supplied arguments will be 
 	 * encoded using Xml::encodeJsVar(). 
 	 *
-	 * @param $name The name of the function to call, or a JavaScript expression
+	 * @param $name String The name of the function to call, or a JavaScript expression
 	 *    which evaluates to a function object which is called.
 	 * @param $args Array of arguments to pass to the function.
 	 * @since 1.17
