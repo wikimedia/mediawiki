ec6595c03e3a60b6fb8e579e81dd9633894b9ed8
Krinkle <krinkle@users.mediawiki.org>
2011-01-10 04:47:38 +0000

    traversal indention. Follow-up r79924

--- resources/mediawiki.special/mediawiki.special.preferences.js
+++ resources/mediawiki.special/mediawiki.special.preferences.js
@@ -7,37 +7,38 @@ $( '#preferences' )
 	.addClass( 'jsprefs' )
 	.before( $( '<ul id="preftoc"></ul>' ) )
 	.children( 'fieldset' )
-	.hide()
-	.addClass( 'prefsection' )
-	.children( 'legend' )
-	.addClass( 'mainLegend' )
-	.each( function( i ) {
-		$(this).parent().attr( 'id', 'prefsection-' + i );
-		if ( i === 0 ) {
-			$(this).parent().show();
-		}
-		$( '#preftoc' ).append(
-			$( '<li></li>' )
-				.addClass( i === 0 ? 'selected' : null )
-				.append(
-					$( '<a></a>')
-						.text( $(this).text() )
-						.attr( 'href', '#prefsection-' + i )
-						.mousedown( function( e ) {
-							$(this).parent().parent().find( 'li' ).removeClass( 'selected' );
-							$(this).parent().addClass( 'selected' );
-							e.preventDefault();
-							return false;
-						} )
-						.click( function( e ) {
-							$( '#preferences > fieldset' ).hide();
-							$( '#prefsection-' + i ).show();
-							e.preventDefault();
-							return false;
-						} )
-				)
-		);
-	} );
+		.hide()
+		.addClass( 'prefsection' )
+		.children( 'legend' )
+			.addClass( 'mainLegend' )
+			.each( function( i ) {
+					$(this).parent().attr( 'id', 'prefsection-' + i );
+					if ( i === 0 ) {
+						$(this).parent().show();
+					}
+					$( '#preftoc' ).append(
+						$( '<li></li>' )
+							.addClass( i === 0 ? 'selected' : null )
+							.append(
+								$( '<a></a>')
+									.text( $(this).text() )
+									.attr( 'href', '#prefsection-' + i )
+									.mousedown( function( e ) {
+										$(this).parent().parent().find( 'li' ).removeClass( 'selected' );
+										$(this).parent().addClass( 'selected' );
+										e.preventDefault();
+										return false;
+									} )
+									.click( function( e ) {
+										$( '#preferences > fieldset' ).hide();
+										$( '#prefsection-' + i ).show();
+										e.preventDefault();
+										return false;
+									} )
+							)
+					);
+				}
+			);
 
 /**
  * Given an email validity status (true, false, null) update the label CSS class
