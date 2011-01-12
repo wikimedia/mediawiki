b37dccdb1de0f5c7f46a80a4625eb73e471fac55
Trevor Parscal <tparscal@users.mediawiki.org>
2011-01-12 20:43:23 +0000

    Fixed IE bug, you must use html:msg for the msg elements that jquery.localize uses to translate things otherwise IE chokes when you insert them cause it doesn't know about the msg element.

--- resources/jquery/jquery.localize.js
+++ resources/jquery/jquery.localize.js
@@ -6,9 +6,12 @@
  * with localized text, elements with title-msg and alt-msg attributes will receive localized title
  * and alt attributes.
  *
+ * Note that "msg" elements must have html namespacing such as "<html:msg />" to be compatible with
+ * Internet Explorer.
+ *
  * Example:
  *		<p class="somethingCool">
- *			<msg key="my-message" />
+ *			<html:msg key="my-message" />
  *			<img src="something.jpg" title-msg="my-title-message" alt-msg="my-alt-message" />
  *		</p>
  *
@@ -26,10 +29,11 @@
  * @param Object: options Map of options
  *  * prefix: Message prefix to use when localizing elements and attributes
  */
+
 $.fn.localize = function( options ) {
 	options = $.extend( { 'prefix': '' }, options );
 	return $(this)
-		.find( 'msg' )
+		.find( 'msg,html\\:msg' )
 			.each( function() {
 				$(this)
 					.text( mediaWiki.msg( options.prefix + $(this).attr( 'key' ) ) )
@@ -51,3 +55,6 @@ $.fn.localize = function( options ) {
 			} )
 			.end();
 };
+
+// Let IE know about the msg tag before it's used...
+document.createElement( 'msg' );
