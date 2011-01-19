9ff7d5b26237f9fbd999d0d41b4b9cb351a785d6
Roan Kattouw <catrope@users.mediawiki.org>
2011-01-19 17:49:05 +0000

    Followup r80554: coding style, add var

--- resources/mediawiki.action/mediawiki.action.edit.js
+++ resources/mediawiki.action/mediawiki.action.edit.js
@@ -3,15 +3,14 @@
  */
 
 //make sure edit summary does not exceed byte limit
-$( '#wpSummary' ).attr( 'maxLength', 250 ).keypress( function (e) {
-		
+$( '#wpSummary' ).attr( 'maxLength', 250 ).keypress( function( e ) {
 	// first check to see if this is actually a character key
 	// being pressed.
 	// Based on key-event info from http://unixpapa.com/js/key.html
 	// JQuery should also normalize e.which to be consistent cross-browser,
 	// however the same check is still needed regardless of jQuery.
 
-	if (e.which === 0 || e.charCode === 0 || e.ctrlKey || e.altKey || e.metaKey) {
+	if ( e.which === 0 || e.charCode === 0 || e.ctrlKey || e.altKey || e.metaKey ) {
 		return true; //a special key (backspace, etc) so don't interfere.
 	}
 
@@ -21,9 +20,9 @@ $( '#wpSummary' ).attr( 'maxLength', 250 ).keypress( function (e) {
 	// and the actual character takes 4 bytes in UTF-8 (2*2=4). Might not work perfectly in edge cases
 	// such as illegal sequences, but that should never happen.
 
-	len = this.value.replace(/[\u0080-\u07FF\uD800-\uDFFF]/g, '**').replace(/[\u0800-\uD7FF\uE000-\uFFFF]/g, '***').length;
+	var len = this.value.replace( /[\u0080-\u07FF\uD800-\uDFFF]/g, '**' ).replace( /[\u0800-\uD7FF\uE000-\uFFFF]/g, '***' ).length;
 	//247 as this doesn't count character about to be inserted.
-	if (len > 247) {
+	if ( len > 247 ) {
 		e.preventDefault();
 	}
 });
