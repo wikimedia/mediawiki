6ca0ea07a2261c8e1e423e5f05df55a0d76126b7
Brian Wolff <bawolff@users.mediawiki.org>
2011-01-20 01:49:49 +0000

    (bug 26716, well partially anyways. kind of second half of r80608) Add a comment to external editor ini control file explaining what it is. This will give people who accidently select the preference a fighting chance to debug what is going on.
    Of course, since it has a mime type of application/x-external-edit (or something like that), and the download would have
    a .php extension, the chance they will open it in a text editor is pretty slim...
    
    See bug 2760 for proposal to just kill the preference altogether.

--- includes/ExternalEdit.php
+++ includes/ExternalEdit.php
@@ -53,6 +53,11 @@ class ExternalEdit {
 		}
 		$special=$wgLang->getNsText(NS_SPECIAL);
 		$control = <<<CONTROL
+; You're seeing this file because you're using Mediawiki's External Editor
+; feature. This is probably because you selected use external editor
+; in your preferences. To edit normally, either disable that preference
+; or go to the URL $url .
+; See http://www.mediawiki.org/wiki/Manual:External_editors for details.
 [Process]
 Type=$type
 Engine=MediaWiki
