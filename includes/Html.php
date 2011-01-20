36f4b7a6e68222feaf4ee3f8f2e6acfbd52100d0
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-01-20 19:26:30 +0000

    Added since notice to indicate method was not present in MW 1.16 yet

--- includes/Html.php
+++ includes/Html.php
@@ -200,7 +200,8 @@ class Html {
 	/**
 	 * Returns "</$element>", except if $wgWellFormedXml is off, in which case
 	 * it returns the empty string when that's guaranteed to be safe.
-	 *
+	 * 
+	 * @since 1.17
 	 * @param $element string Name of the element, e.g., 'a'
 	 * @return string A closing tag, if required
 	 */
