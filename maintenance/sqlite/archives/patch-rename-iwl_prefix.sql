c4a741d7f65386c75fe0ea63faaa1d227e343bc5
Matěj Grabovský <mgrabovsky@users.mediawiki.org>
2011-01-26 10:33:21 +0000

    Create index only if it doesn't already exist

--- maintenance/sqlite/archives/patch-rename-iwl_prefix.sql
+++ maintenance/sqlite/archives/patch-rename-iwl_prefix.sql
@@ -2,4 +2,4 @@
 -- Recreates the iwl_prefix for the iwlinks table
 --
 DROP INDEX IF EXISTS /*i*/iwl_prefix;
-CREATE INDEX /*i*/iwl_prefix_from_title ON /*_*/iwlinks (iwl_prefix, iwl_from, iwl_title);
+CREATE INDEX IF NOT EXISTS /*i*/iwl_prefix_from_title ON /*_*/iwlinks (iwl_prefix, iwl_from, iwl_title);
