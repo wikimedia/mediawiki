66e484e6f6c39c96f33e4a22446f115f2b10fcb2
Markus Glaser <mglaser@users.mediawiki.org>
2011-01-27 14:51:58 +0000

    Added triggerClientTestResources, which makes the use of separate test resourecs optional. Needed for testing installer functions.

--- tests/selenium/SeleniumTestSuite.php
+++ tests/selenium/SeleniumTestSuite.php
@@ -4,6 +4,7 @@ abstract class SeleniumTestSuite extends PHPUnit_Framework_TestSuite {
 	private $selenium;
 	private $isSetUp = false;
 	private $loginBeforeTests = true;
+	private $triggerClientTestResources = true;
 
 	// Do not add line break after test output
 	const CONTINUE_LINE = 1;
@@ -21,16 +22,20 @@ abstract class SeleniumTestSuite extends PHPUnit_Framework_TestSuite {
 		$this->isSetUp = true;
 		$this->selenium = Selenium::getInstance();
 		$this->selenium->start();
-		$this->selenium->open( $this->selenium->getUrl() . '/index.php?setupTestSuite=' . $this->getName() );
-		//wait a little longer for the db operation
-		$this->selenium->waitForPageToLoad( 6000  );
+		if ( $this->triggerClientTestResources ) {
+			$this->selenium->open( $this->selenium->getUrl() . '/index.php?setupTestSuite=' . $this->getName() );
+			//wait a little longer for the db operation
+			$this->selenium->waitForPageToLoad( 6000  );
+		}
 		if ( $this->loginBeforeTests ) {
 			$this->login();
 		}
 	}
 
 	public function tearDown() {
-		$this->selenium->open( $this->selenium->getUrl() . '/index.php?clearTestSuite=' . $this->getName() );
+		if ( $this->triggerClientTestResources ) {
+			$this->selenium->open( $this->selenium->getUrl() . '/index.php?clearTestSuite=' . $this->getName() );
+		}
 		$this->selenium->stop();
 	}
 
@@ -44,5 +49,9 @@ abstract class SeleniumTestSuite extends PHPUnit_Framework_TestSuite {
 
 	protected function setLoginBeforeTests( $loginBeforeTests = true ) {
 		$this->loginBeforeTests = $loginBeforeTests;
+	}
+	
+	protected function setTriggerClientTestResources( $triggerClientTestResources = true ) {
+		$this->triggerClientTestResources = $triggerClientTestResources;
 	}
 }
