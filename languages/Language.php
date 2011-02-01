7164d7d646b1dd777528ad7f69754c50d26149ee
Tim Starling <tstarling@users.mediawiki.org>
2011-02-01 22:54:50 +0000

    Relax Language::isValidCode() to avoid breaking uselang hacks

--- languages/Language.php
+++ languages/Language.php
@@ -196,7 +196,7 @@ class Language {
 	 * not it exists.
 	 */
 	public static function isValidCode( $code ) {
-		return (bool)preg_match( '/^[a-z-]+$/', $code );
+		return strcspn( $code, "/\\\000" ) === strlen( $code );
 	}
 
 	/**
