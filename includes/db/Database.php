ed748e8fe62804473eaf4c971690a6a784151e45
Sam Reed <reedy@users.mediawiki.org>
2011-02-09 19:02:56 +0000

    MFT r81657, and fixing revision r81833

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -1608,7 +1608,7 @@ abstract class DatabaseBase implements DatabaseType {
 		if ( !$alias || $alias == $name ) {
 			return $this->tableName( $name );
 		} else {
-			return $this->tableName( $name ) . ' `' . $alias . '`';
+			return $this->tableName( $name ) . ' ' . $this->addIdentifierQuotes( $alias );
 		}
 	}
 
