35c03f77c2251ea6b555ae8b6d7708e57242d6c7
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2011-02-12 17:15:24 +0000

    * Fix hook name * Update paths in findhooks script

--- maintenance/findhooks.php
+++ maintenance/findhooks.php
@@ -63,14 +63,15 @@ class FindHooks extends Maintenance {
 			$IP . '/includes/installer/',
 			$IP . '/includes/parser/',
 			$IP . '/includes/resourceloader/',
+			$IP . '/includes/revisiondelete/',
 			$IP . '/includes/search/',
 			$IP . '/includes/specials/',
 			$IP . '/includes/upload/',
 			$IP . '/languages/',
 			$IP . '/maintenance/',
-			$IP . '/maintenance/tests/',
-			$IP . '/maintenance/tests/parser/',
-			$IP . '/maintenance/tests/phpunit/suites/',
+			$IP . '/tests/',
+			$IP . '/tests/parser/',
+			$IP . '/tests/phpunit/suites/',
 			$IP . '/skins/',
 		);
 
