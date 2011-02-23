5875959e25267f034c3c29816da4c751a3927ee8
Max Semenik <maxsem@users.mediawiki.org>
2011-02-23 08:06:11 +0000

    Very basic JavaScriiptDistiller tests, needs serious expansion

---
+++ tests/phpunit/includes/libs/distiller-in.js
@@ -0,0 +1,15 @@
+/* this is a comment */
+/* a multi-
+		  line comment */
+/* a /* weird comment */
+// one-line comment
+var string = /* comment */'/* comment in string */';
+
+move_to_the_same_line()
+
+				dont_move();
+				
+if ( / a + bfoo.* /i.test( bar )  && ( a / b ) * c + d - e) { /* blah */
+	statement();
+	statement()
+}
