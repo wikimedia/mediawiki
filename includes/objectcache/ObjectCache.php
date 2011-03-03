d3f36ffd4d263861f475da4b45c98a95e8840431
Tim Starling <tstarling@users.mediawiki.org>
2011-03-03 04:48:14 +0000

    More renames and splits for objectcache reorganisation.

--- includes/objectcache/ObjectCache.php
+++ includes/objectcache/ObjectCache.php
@@ -6,31 +6,6 @@
  * @ingroup Cache
  */
 
-/**
- * FakeMemCachedClient imitates the API of memcached-client v. 0.1.2.
- * It acts as a memcached server with no RAM, that is, all objects are
- * cleared the moment they are set. All set operations succeed and all
- * get operations return null.
- * @ingroup Cache
- */
-class FakeMemCachedClient {
-	function add ($key, $val, $exp = 0) { return true; }
-	function decr ($key, $amt=1) { return null; }
-	function delete ($key, $time = 0) { return false; }
-	function disconnect_all () { }
-	function enable_compress ($enable) { }
-	function forget_dead_hosts () { }
-	function get ($key) { return null; }
-	function get_multi ($keys) { return array_pad(array(), count($keys), null); }
-	function incr ($key, $amt=1) { return null; }
-	function replace ($key, $value, $exp=0) { return false; }
-	function run_command ($sock, $cmd) { return null; }
-	function set ($key, $value, $exp=0){ return true; }
-	function set_compress_threshold ($thresh){ }
-	function set_debug ($dbg) { }
-	function set_servers ($list) { }
-}
-
 global $wgCaches;
 $wgCaches = array();
 
