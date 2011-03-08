db5675164de0f39d644143b12525fa242178e444
Max Semenik <maxsem@users.mediawiki.org>
2011-03-08 09:28:13 +0000

    LocalSettings generation tweaks: * Mention extensions only if user chose some during installation * Added a comment at the bottom suggesting where to put new settings

--- includes/installer/LocalSettingsGenerator.php
+++ includes/installer/LocalSettingsGenerator.php
@@ -111,13 +111,19 @@ class LocalSettingsGenerator {
 		$localSettings = $this->getDefaultText();
 
 		if( count( $this->extensions ) ) {
-			$localSettings .= "\n# The following extensions were automatically enabled:\n";
+			$localSettings .= "
+# Enabled Extensions. Most extensions are enabled by including the base extension file here
+# but check specific extension documentation for more details
+# The following extensions were automatically enabled:\n";
 
 			foreach( $this->extensions as $extName ) {
 				$encExtName = self::escapePhpString( $extName );
 				$localSettings .= "require( \"extensions/$encExtName/$encExtName.php\" );\n";
 			}
 		}
+
+		$localSettings .= "\n\n# End of automatically generated settings.
+# Add more configuration options below.\n\n";
 
 		return $localSettings;
 	}
@@ -305,9 +311,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 
 # Path to the GNU diff3 utility. Used for conflict resolution.
 \$wgDiff3 = \"{$this->values['wgDiff3']}\";
-
-# Enabled Extensions. Most extensions are enabled by including the base extension file here
-# but check specific extension documentation for more details
 ";
 	}
 
