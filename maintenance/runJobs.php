358c44381960d6bbff9a41063a472416d9de81b9
Tim Starling <tstarling@users.mediawiki.org>
2011-03-10 05:26:34 +0000

    Add --maxtime parameter to allow job runners to be limited by time instead of job count. This makes more sense when --procs is used. Using it on Wikimedia will reduce the effect of laggy slaves.

--- maintenance/runJobs.php
+++ maintenance/runJobs.php
@@ -31,6 +31,7 @@ class RunJobs extends Maintenance {
 		parent::__construct();
 		$this->mDescription = "Run pending jobs";
 		$this->addOption( 'maxjobs', 'Maximum number of jobs to run', false, true );
+		$this->addOption( 'maxtime', 'Maximum amount of wall-clock time', false, true );
 		$this->addOption( 'type', 'Type of job to run', false, true );
 		$this->addOption( 'procs', 'Number of processes to use', false, true );
 	}
@@ -52,7 +53,9 @@ class RunJobs extends Maintenance {
 				exit( 0 );
 			}
 		}
-		$maxJobs = $this->getOption( 'maxjobs', 10000 );
+		$maxJobs = $this->getOption( 'maxjobs', false );
+		$maxTime = $this->getOption( 'maxtime', false );
+		$startTime = time();
 		$type = $this->getOption( 'type', false );
 		$wgTitle = Title::newFromText( 'RunJobs.php' );
 		$dbw = wfGetDB( DB_MASTER );
@@ -80,7 +83,11 @@ class RunJobs extends Maintenance {
 				} else {
 					$this->runJobsLog( $job->toString() . " t=$timeMs good" );
 				}
+
 				if ( $maxJobs && ++$n > $maxJobs ) {
+					break 2;
+				}
+				if ( $maxTime && time() - $startTime > $maxTime ) {
 					break 2;
 				}
 			}
