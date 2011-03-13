86ff9e4f3b63913479526568fffc184f375febd4
Marcin Cieślak <saper@users.mediawiki.org>
2011-03-13 23:12:59 +0000

    Bug 2557: Sidebar items missing from classic, cologne blue, simple skins
    * Simple skin support multiple sidebar items
    * Standard (a.k.a. Classic) likes to add "my watchlist" and "my contributions"
      links at the bottom of the first sidebar box. It also has no titles for boxes.
    * Cologne blue supports multiple sidebar boxes with titles.

--- skins/Standard.php
+++ skins/Standard.php
@@ -118,25 +118,33 @@ class StandardTemplate extends LegacyTemplate {
 		unset( $bar['SEARCH'] );
 		unset( $bar['LANGUAGES'] );
 		unset( $bar['TOOLBOX'] );
-		$browseLinks = reset( $bar );
 
-		foreach ( $browseLinks as $link ) {
-			if ( $link['text'] != '-' ) {
-				$s .= "<a href=\"{$link['href']}\">" .
-					htmlspecialchars( $link['text'] ) . '</a>' . $sep;
+		$barnumber = 1;
+		foreach ( $bar as $heading => $browseLinks ) {
+			if ( $barnumber > 1 ) {
+				$s .= "\n<hr class='sep' />";
+			} 
+			foreach ( $browseLinks as $link ) {
+				if ( $link['text'] != '-' ) {
+					$s .= "<a href=\"{$link['href']}\">" .
+						htmlspecialchars( $link['text'] ) . '</a>' . $sep;
+				}
 			}
+			if ( $barnumber == 1 ) {
+				// only show watchlist link if logged in
+				if( $wgUser->isLoggedIn() ) {
+					$s.= $this->getSkin()->specialLink( 'Watchlist' ) ;
+					$s .= $sep . $this->getSkin()->linkKnown(
+						SpecialPage::getTitleFor( 'Contributions' ),
+						wfMsg( 'mycontris' ),
+						array(),
+						array( 'target' => $wgUser->getName() )
+					);
+				}
+			}
+			$barnumber = $barnumber + 1;
 		}
 
-		if( $wgUser->isLoggedIn() ) {
-			$s.= $this->getSkin()->specialLink( 'Watchlist' ) ;
-			$s .= $sep . $this->getSkin()->linkKnown(
-				SpecialPage::getTitleFor( 'Contributions' ),
-				wfMsg( 'mycontris' ),
-				array(),
-				array( 'target' => $wgUser->getName() )
-			);
-		}
-		// only show watchlist link if logged in
 		$s .= "\n<hr class='sep' />";
 		$articleExists = $this->getSkin()->getTitle()->getArticleId();
 		if ( $wgOut->isArticle() || $action == 'edit' || $action == 'history' || $wpPreview ) {
