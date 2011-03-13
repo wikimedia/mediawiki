79f1871353264539b7ba6b61fbda2cd6c4f60e9c
Sam Reed <reedy@users.mediawiki.org>
2011-03-13 13:10:40 +0000

    Followup r83812, add /*_*/ to allow for prefixed tables

--- maintenance/archives/patch-cl_type.sql
+++ maintenance/archives/patch-cl_type.sql
@@ -3,4 +3,4 @@
 -- the < and > operators when working with enums
 --
 
-ALTER TABLE categorylinks MODIFY cl_type varchar(6) NOT NULL default 'page';
+ALTER TABLE /*_*/categorylinks MODIFY cl_type varchar(6) NOT NULL default 'page';
