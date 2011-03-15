274d317a0ca2777a5c5fae2007c53811397540f5
Marcin Cieślak <saper@users.mediawiki.org>
2011-03-15 02:53:00 +0000

    r83812, r83814: Don't use cl_type at all when paging categorylinks
    * Remove cl_type from paging in categorylinks - it's not
      really needed there. Although cl_type is in WHERE but not
      in ORDER BY clause the worst thing that can happen
      is to have a filesort going again through <500 entries
      selected by index. Or will FORCE INDEX work anyway?
    
    * Revert schema change, as we don't need cl_type there
      anyway (or even if we wanted to compare, it should
      work as expected by using INT values against ENUM).

--- maintenance/archives/patch-cl_type.sql
+++
@@ -1,6 +0,0 @@
---
--- Change cl_type to a varchar from an enum because of the weird semantics of
--- the < and > operators when working with enums
---
-
-ALTER TABLE /*_*/categorylinks MODIFY cl_type varchar(6) NOT NULL default 'page';
