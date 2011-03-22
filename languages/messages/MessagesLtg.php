421698919eb6ab9a212198b25f8988b07d223754
Raimond Spekking <raymond@users.mediawiki.org>
2011-03-22 19:02:48 +0000

    Bug 28184 - Namespaces for the Latgalian Wikipedia Pls sync with 1.17wmf1. Thanks.

--- languages/messages/MessagesLtg.php
+++ languages/messages/MessagesLtg.php
@@ -11,6 +11,25 @@
  * @author Jureits
  * @author Reedy
  */
+ 
+$namespaceNames = array(
+	NS_MEDIA            => 'Medeja',
+	NS_SPECIAL          => 'Seviškuo',
+	NS_TALK             => 'Sprīža',
+	NS_USER             => 'Lītuotuojs',
+	NS_USER_TALK        => 'Sprīža_ap_lītuotuoju',
+	NS_PROJECT_TALK     => 'Sprīža_ap_{{GRAMMAR:accusative|$1}}',
+	NS_FILE             => 'Fails',
+	NS_FILE_TALK        => 'Sprīža_ap_failu',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Sprīža_ap_MediaWiki',
+	NS_TEMPLATE         => 'Taiss',
+	NS_TEMPLATE_TALK    => 'Sprīža_ap_taisu',
+	NS_HELP             => 'Paleigs',
+	NS_HELP_TALK        => 'Sprīža_ap_paleigu',
+	NS_CATEGORY         => 'Kategoreja',
+	NS_CATEGORY_TALK    => 'Sprīža_ap_kategoreju',
+);
 
 $fallback = 'lv';
 
