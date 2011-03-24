e82ca13996c703effe003e8edb0bc5e447b6d43c
Sam Reed <reedy@users.mediawiki.org>
2011-03-24 11:11:15 +0000

    Don't pass $this by reference

--- includes/api/ApiQuerySiteinfo.php
+++ includes/api/ApiQuerySiteinfo.php
@@ -160,7 +160,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 		$data['wikiid'] = wfWikiID();
 		$data['time'] = wfTimestamp( TS_ISO_8601, time() );
 
-		wfRunHooks( 'APIQuerySiteInfoGeneralInfo', array( &$this, &$data ) );
+		wfRunHooks( 'APIQuerySiteInfoGeneralInfo', array( $this, &$data ) );
 
 		return $this->getResult()->addValue( 'query', $property, $data );
 	}
