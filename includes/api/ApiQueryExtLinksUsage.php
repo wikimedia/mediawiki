634d826ea96d1af7ff567e5b503e13c2f75828a0
Sam Reed <reedy@users.mediawiki.org>
2011-03-24 20:02:41 +0000

    Remove 2 erroneous $db parameters from External links modules

--- includes/api/ApiQueryExtLinksUsage.php
+++ includes/api/ApiQueryExtLinksUsage.php
@@ -73,7 +73,7 @@ class ApiQueryExtLinksUsage extends ApiQueryGeneratorBase {
 			$this->addWhereFld( 'page_namespace', $params['namespace'] );
 		}
 
-		$whereQuery = $this->prepareUrlQuerySearchString( $db, $query, $protocol );
+		$whereQuery = $this->prepareUrlQuerySearchString( $query, $protocol );
 
 		if ( $whereQuery !== null ) {
 			$this->addWhere( $whereQuery );
