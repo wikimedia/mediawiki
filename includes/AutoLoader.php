4d9578a4c62fa2d1e09ef286e17f39049be1cb61
Happy-melon <happy-melon@users.mediawiki.org>
2011-03-27 16:49:01 +0000

    Follow-up r84805: convert SpecialActiveusers to not use the now-absent UsersPager.

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -585,7 +585,6 @@ $wgAutoloadLocalClasses = array(
 
 	# includes/specials
 	'SpecialAllmessages' => 'includes/specials/SpecialAllmessages.php',
-	'ActiveUsersPager' => 'includes/specials/SpecialActiveusers.php',
 	'AllmessagesTablePager' => 'includes/specials/SpecialAllmessages.php',
 	'AncientPagesPage' => 'includes/specials/SpecialAncientpages.php',
 	'BrokenRedirectsPage' => 'includes/specials/SpecialBrokenRedirects.php',
