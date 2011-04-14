c4a6c2b05003d25cf865c86d7253aa56758613bc
Tim Starling <tstarling@users.mediawiki.org>
2011-04-14 07:14:34 +0000

    * MFT r86027: fix IE6 XSS again * Updates for release of 1.16.4

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ if ( !defined( 'MW_PHP4' ) ) {
 }
 
 /** MediaWiki version number */
-$wgVersion = '1.16.3';
+$wgVersion = '1.16.4';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
