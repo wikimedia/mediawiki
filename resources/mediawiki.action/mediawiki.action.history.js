ddaf0fe2b27c8b7fb044397fe963c5242a701f7c
Leo Koppelkamm <diebuche@users.mediawiki.org>
2011-04-14 13:18:39 +0000

    History: Adding JS that converts  buttons to links for better UX. Patch based on one by Matthew Flaschen. Fixes Bug 16165

--- resources/mediawiki.action/mediawiki.action.history.js
+++ resources/mediawiki.action/mediawiki.action.history.js
@@ -5,4 +5,7 @@ jQuery( function( $ ) {
 	// Replaces histrowinit
 	$( '#pagehistory li input[name="diff"], #pagehistory li input[name="oldid"]' ).click( diffcheck );
 	diffcheck();
+	mediaWiki.loader.using('jquery.ui.button', function() {
+		window.fixCompare();
+	});
 });
