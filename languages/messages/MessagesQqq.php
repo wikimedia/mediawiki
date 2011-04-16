34fa69be9698b470d6e56abc0c5fd81d7aa38e9b
Max Semenik <maxsem@users.mediawiki.org>
2011-04-16 08:45:57 +0000

    Fixed syntax error in message docs

--- languages/messages/MessagesQqq.php
+++ languages/messages/MessagesQqq.php
@@ -3450,7 +3450,7 @@ See also Wikipedia on [http://en.wikipedia.org/wiki/Focal_length#In_photography 
 'exif-gpsdatestamp'                => 'Date (does not generally include time unless recorded in XMP) that GPS measurement was taken, in UTC. Since often this is at the same date as photo was taken, this is sometimes more reliable than {{msg-mw|exif-datetimeoriginal}}.',
 'exif-gpsmeasuremode'              => 'Is the measurement 2D (latitude and longitude) or 3D (latitude, longitude, and altitude).',
 'exif-gpsdop'                      => 'How accurate the GPS information is. See http://en.wikipedia.org/wiki/Dilution_of_precision_(GPS)',
-'exif-gpslatitude'                 => 'The latitude of the location from where the picture was taken from.
+'exif-gpslatitude'                 => 'The latitude of the location from where the picture was taken from.',
 'exif-objectname'                  => 'This message labels a field in the image metadata table that is a short name or title for the image. (As compared to {{msg-mw|exif-imagedescription}} which is a long description of the image).',
 
 {{Identical|Latitude}}',
