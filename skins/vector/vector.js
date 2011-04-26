34a0a2f1201e88f6e53ff8e49159040ff8002d46
Krinkle <krinkle@users.mediawiki.org>
2011-04-26 18:01:52 +0000

    Selectors go from right to left. Starting with a generic 'a' is kind of slow. Also loses the advantage of an id-selector. * Optimizing by selecting the ID seperately (very fast) and doing a find() within that context (also caching pCactions ). * while at it:  - caching selector  - aliasing $ to jQuery locally  - Shorthand dom-ready
    (Follow-up r86861)

--- skins/vector/vector.js
+++ skins/vector/vector.js
@@ -1,18 +1,19 @@
 /*
  * Vector-specific scripts
  */
-$(document).ready( function() {	
+jQuery( function( $ ) {
+	var $pCactions = $( '#p-cactions' );
 	// For accessibility, show the menu when the hidden link in the menu is clicked
-	$( '#p-cactions h5 a' ).click( function() {
-		$( '#p-cactions .menu' ).toggleClass( 'menuForceShow' );
+	$pCactions.find( 'h5 a' ).click( function() {
+		$pCactions.find( '.menu' ).toggleClass( 'menuForceShow' );
 	});
-	
+
 	// When the hidden link has focus, also set a class that will change the arrow icon
-	$( '#p-cactions h5 a' ).focus( function () {
-		$( '#p-cactions' ).addClass( 'vectorMenuFocus' );
+	$pCactions.find( 'h5 a' ).focus( function () {
+		$pCactions.addClass( 'vectorMenuFocus' );
 	});
-	
-	$( '#p-cactions h5 a' ).blur( function () {
-		$( '#p-cactions' ).removeClass( 'vectorMenuFocus' );
+
+	$pCactions.find( 'h5 a' ).blur( function () {
+		$pCactions.removeClass( 'vectorMenuFocus' );
 	});
 });
