358df9dc7806485535e508a19fe82ac5e4c7f540
Krinkle <krinkle@users.mediawiki.org>
2011-04-26 18:18:29 +0000

    * Fix ancient jumpy bug when clicking the hover-triangle (normally nobody does that, but just tried it for screenreader thing, it's been around for quite some time (href="#": needs preventDefault) * jQuery chaining :) * Add bugzilla ref. in comment ("hidden link being clicked") (Follow-up r86958, r86861)

--- skins/vector/vector.js
+++ skins/vector/vector.js
@@ -3,17 +3,17 @@
  */
 jQuery( function( $ ) {
 	var $pCactions = $( '#p-cactions' );
-	// For accessibility, show the menu when the hidden link in the menu is clicked
-	$pCactions.find( 'h5 a' ).click( function() {
-		$pCactions.find( '.menu' ).toggleClass( 'menuForceShow' );
-	});
-
-	// When the hidden link has focus, also set a class that will change the arrow icon
-	$pCactions.find( 'h5 a' ).focus( function () {
-		$pCactions.addClass( 'vectorMenuFocus' );
-	});
-
-	$pCactions.find( 'h5 a' ).blur( function () {
-		$pCactions.removeClass( 'vectorMenuFocus' );
-	});
+	$pCactions.find( 'h5 a' )
+		// For accessibility, show the menu when the hidden link in the menu is clicked (bug 24298)
+		.click( function( e ) {
+			$pCactions.find( '.menu' ).toggleClass( 'menuForceShow' );
+			e.preventDefault();
+		})
+		// When the hidden link has focus, also set a class that will change the arrow icon
+		.focus( function() {
+			$pCactions.addClass( 'vectorMenuFocus' );
+		})
+		.blur( function() {
+			$pCactions.removeClass( 'vectorMenuFocus' );
+		});
 });
