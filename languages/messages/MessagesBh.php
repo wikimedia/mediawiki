1e7e23adfc41f21840c17e9fb50601abedf49dff
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-05-08 07:39:13 +0000

    Add placeholder for "bh" code. Falls back to "bho" per r87676.

---
+++ languages/messages/MessagesBh.php
@@ -0,0 +1,13 @@
+<?php
+/** Bihari (macro language)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ * @comment falls back to Bhojpuri.
+ *
+ */
+
+$fallback = 'bho';
