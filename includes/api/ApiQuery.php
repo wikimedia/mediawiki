e303397d43d5ad5f45786c8995e3a1bdd77f42ff
Sam Reed <reedy@users.mediawiki.org>
2011-05-14 12:00:46 +0000

    Fix daft loading error I added in r88047

--- includes/api/ApiQuery.php
+++ includes/api/ApiQuery.php
@@ -82,7 +82,7 @@ class ApiQuery extends ApiBase {
 		'filearchive' => 'ApiQueryFilearchive',
 		'imageusage' => 'ApiQueryBacklinks',
 		'iwbacklinks' => 'ApiQueryIWBacklinks',
-		'iwlanglinks' => 'ApiQueryLangLinks',
+		'langbacklinks' => 'ApiQueryLangBacklinks',
 		'logevents' => 'ApiQueryLogEvents',
 		'recentchanges' => 'ApiQueryRecentChanges',
 		'search' => 'ApiQuerySearch',
