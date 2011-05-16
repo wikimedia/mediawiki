7131283a7bcf26786ffe05fb4fbc1d3c3dd3ea11
Chad Horohoe <demon@users.mediawiki.org>
2011-05-16 21:04:55 +0000

    Initial commit of configuration management backend proposal. Feedback desired before I go much further. * Common use case is Conf::get( 'myVar' ); * Support for default install (new `config` table) added, should be trivial to add backends for CDB, Memcache, etc... *

---
+++ includes/conf/DatabaseConf.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Database configuration class
+ *
+ * Copyright © 2011 Chad Horohoe <chadh@wikimedia.org>
+ * http://www.mediawiki.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Config
+ */
+class DatabaseConf extends Conf {
+	protected function initChangedSettings() {
+		$res = wfGetDB( DB_MASTER )->select( 'config', '*', array(), __METHOD__ );
+		foreach( $res as $row ) {
+			$this->values[$row->cf_name] = $row->cf_value;
+		}
+	}
+}
