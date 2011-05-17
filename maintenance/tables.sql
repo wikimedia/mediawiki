bb30a17b710b9797ed82051f7c6075c61645456d
Mark A. Hershberger <mah@users.mediawiki.org>
2011-05-17 11:56:53 +0000

    Follow up r88270 — remove commas from table creation that break a sqlite install.

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -1414,7 +1414,7 @@ CREATE TABLE /*_*/config (
   -- Config var name
   cf_name varbinary(255) NOT NULL PRIMARY KEY,
   -- Config var value
-  cf_value blob NOT NULL,
+  cf_value blob NOT NULL
 ) /*$wgDBTableOptions*/;
 -- Should cover *most* configuration - strings, ints, bools, etc.
 CREATE INDEX /*i*/cf_name_value ON /*_*/config (cf_name,cf_value(255));
