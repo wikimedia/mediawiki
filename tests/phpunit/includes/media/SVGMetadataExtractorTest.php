11e017e68a2738f509be1b98dbc3839d149af32e
Brion Vibber <brion@users.mediawiki.org>
2011-05-25 23:59:43 +0000

    * (bug 27465) Fix metadata extraction for SVG files using unusual namespace names
    The previous fix (in r82307) only checked explicitly for a namespace given the 'svg' prefix; this fix use XML namespacing support on XMLReader to check for the actual namespace URI correctly.
    Fixed up a test case (for RDF extraction) and added trimming on the whitespace.
    Also added another test case file that doesn't use a namespace name on the root.

--- tests/phpunit/includes/media/SVGMetadataExtractorTest.php
+++ tests/phpunit/includes/media/SVGMetadataExtractorTest.php
@@ -26,6 +26,13 @@ class SVGMetadataExtractorTest extends MediaWikiTestCase {
 		$base = dirname( __FILE__ );
 		return array(
 			array(
+				"$base/Wikimedia-logo.svg",
+				array(
+					'width' => 1024,
+					'height' => 1024
+				)
+			),
+			array(
 				"$base/QA_icon.svg",
 				array(
 					'width' => 60,
@@ -42,8 +49,15 @@ class SVGMetadataExtractorTest extends MediaWikiTestCase {
 			array(
 				"$base/US_states_by_total_state_tax_revenue.svg",
 				array(
-					'width' => 593,
-					'height' => 959
+					'height' => 593,
+					'metadata' =>
+    '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+      <ns4:Work xmlns:ns4="http://creativecommons.org/ns#" rdf:about="">
+        <ns5:format xmlns:ns5="http://purl.org/dc/elements/1.1/">image/svg+xml</ns5:format>
+        <ns5:type xmlns:ns5="http://purl.org/dc/elements/1.1/" rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
+      </ns4:Work>
+    </rdf:RDF>',
+					'width' => 959
 				)
 			),
 		);
