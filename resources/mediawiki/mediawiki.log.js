wikimedia/mediawiki

commit 1ecdbbf4bde4d1c8360c4893111019a69ba276d0

Adding or adjusting front-end function and variable documentation per our conventions.
Also did some trivial clean up, JSHint fixes and performance optimizations while at it. (per http://www.mediawiki.org/wiki/JavaScript_performance )
* Missing semicolons.
* [mediawiki.js Line 540] Unreachable 'registry' after 'throw'.
* [mediawiki.log.js] Checked for window.console but used console.
* Added callback option to mw.util.toggleToc (forwarded to jQuery.fn.slideUp/Down)
* Made mw.log argument more descriptive (logmsg instead of string)
* Using deepEqual(, true) instead of ok() when asserting true. (ok() is like casting a boolean (or a plain if() statement) to verify that something is not falsy)

Added missing test suites for:
* mw.util.toggleToc (follow-up r88732, Dummy check was introduced in r87898)

/me has been infected by Reedy and will be doing more of this tonight.


git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@89082 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

resources/mediawiki/mediawiki.log.js
@@ -13,16 +13,16 @@
 	 *
 	 * @author Michael Dale <mdale@wikimedia.org>
 	 * @author Trevor Parscal <tparscal@wikimedia.org>
-	 * @param {string} string Message to output to console
+	 * @param logmsg string Message to output to console.
 	 */
-	mw.log = function( string ) {
+	mw.log = function( msg ) {
 		// Allow log messages to use a configured prefix to identify the source window (ie. frame)
 		if ( mw.config.exists( 'mw.log.prefix' ) ) {
-			string = mw.config.get( 'mw.log.prefix' ) + '> ' + string;
+			logmsg = mw.config.get( 'mw.log.prefix' ) + '> ' + logmsg;
 		}
 		// Try to use an existing console
-		if ( typeof window.console !== 'undefined' && typeof window.console.log == 'function' ) {
-			console.log( string );
+		if ( window.console !== undefined && $.isFunction( window.console.log ) ) {
+			window.console.log( logmsg );
 		} else {
 			// Set timestamp
 			var d = new Date();
@@ -58,7 +58,7 @@
 						'white-space': 'pre-wrap',
 						'padding': '0.125em 0.25em'
 					} )
-					.text( string )
+					.text( logmsg )
 					.prepend( '<span style="float:right">[' + time + ']</span>' )
 			);
 		}

resources/mediawiki.util/mediawiki.util.jpegmeta.js
@@ -723,11 +723,11 @@
 	};
 	
 	/* JsJpegMeta ends here */
-	
+
 	mw.util = $.extend( mw.util || {}, {
 		jpegmeta: function( fileReaderResult, fileName ) {
 			return new JpegMeta.JpegFile( fileReaderResult, fileName );
 		}
 	} );
-	
+
 } )( jQuery, mediaWiki );

resources/mediawiki.util/mediawiki.util.js
@@ -6,6 +6,9 @@
 	mw.util = $.extend( mw.util || {}, {
 
 		/* Initialisation */
+		/**
+		 * @var boolean Wether or not already initialised
+		 */
 		'initialised' : false,
 		'init' : function() {
 			if ( this.initialised === false ) {
@@ -93,9 +96,11 @@
 					// Only add it if there is a TOC and there is no toggle added already
 					if ( $tocContainer.size() && $tocTitle.size() && !$tocToggleLink.size() ) {
 						var	hideTocCookie = $.cookie( 'mw_hidetoc' );
-							$tocToggleLink = $( '<a href="#" class="internal" id="togglelink">' ).text( mw.msg( 'hidetoc' ) ).click( function(e){
-								e.preventDefault();
-								mw.util.toggleToc( $(this) );
+							$tocToggleLink = $( '<a href="#" class="internal" id="togglelink">' )
+								.text( mw.msg( 'hidetoc' ) )
+								.click( function(e){
+									e.preventDefault();
+									mw.util.toggleToc( $(this) );
 							} );
 						$tocTitle.append( $tocToggleLink.wrap( '<span class="toctoggle">' ).parent().prepend( '&nbsp;[' ).append( ']&nbsp;' ) );
 
@@ -116,7 +121,7 @@
 		/**
 		 * Encode the string like PHP's rawurlencode
 		 *
-		 * @param str String to be encoded
+		 * @param str string String to be encoded
 		 */
 		'rawurlencode' : function( str ) {
 			str = ( str + '' ).toString();
@@ -130,7 +135,7 @@
 		 * We want / and : to be included as literal characters in our title URLs
 		 * as they otherwise fatally break the title
 		 *
-		 * @param str String to be encoded
+		 * @param str string String to be encoded
 		 */
 		'wikiUrlencode' : function( str ) {
 			return this.rawurlencode( str )
@@ -140,7 +145,7 @@
 		/**
 		 * Get the link to a page name (relative to wgServer)
 		 *
-		 * @param str Page name to get the link for.
+		 * @param str string Page name to get the link for.
 		 * @return string Location for a page with name of 'str' or boolean false on error.
 		 */
 		'wikiGetlink' : function( str ) {
@@ -152,8 +157,8 @@
 		 * Get address to a script in the wiki root.
 		 * For index.php use mw.config.get( 'wgScript' )
 		 *
-		 * @param str Name of script (eg. 'api'), defaults to 'index'
-		 * @return str Address to script (eg. '/w/api.php' )
+		 * @param str string Name of script (eg. 'api'), defaults to 'index'
+		 * @return string Address to script (eg. '/w/api.php' )
 		 */
 		'wikiScript' : function( str ) {
 			return mw.config.get( 'wgScriptPath' ) + '/' + ( str || 'index' ) + mw.config.get( 'wgScriptExtension' );
@@ -162,8 +167,8 @@
 		/**
 		 * Append a new style block to the head
 		 *
-		 * @param text String CSS to be appended
-		 * @return CSSStyleSheet object
+		 * @param text string CSS to be appended
+		 * @return CSSStyleSheet
 		 */
 		'addCSS' : function( text ) {
 			var s = document.createElement( 'style' );
@@ -174,24 +179,27 @@
 			} else {
 				s.appendChild( document.createTextNode( text + '' ) ); // Safari sometimes borks on null
 			}
-			document.getElementsByTagName("head")[0].appendChild( s );
+			document.getElementsByTagName('head')[0].appendChild( s );
 			return s.sheet || s;
 		},
 
 		/**
 		 * Hide/show the table of contents element
 		 *
-		 * @param $toggleLink jQuery object of the toggle link
-		 * @return String boolean visibility of the toc (true means it's visible)
+		 * @param $toggleLink jQuery A jQuery object of the toggle link.
+		 * @param callback function Function to be called after the toggle is
+		 * completed (including the animation) (optional)
+		 * @return mixed Boolean visibility of the toc (true if it's visible)
+		 * or Null if there was no table of contents.
 		 */
-		'toggleToc' : function( $toggleLink ) {
+		'toggleToc' : function( $toggleLink, callback ) {
 			var $tocList = $( '#toc ul:first' );
 
 			// This function shouldn't be called if there's no TOC,
 			// but just in case...
 			if ( $tocList.size() ) {
 				if ( $tocList.is( ':hidden' ) ) {
-					$tocList.slideDown( 'fast' );
+					$tocList.slideDown( 'fast', callback );
 					$toggleLink.text( mw.msg( 'hidetoc' ) );
 					$.cookie( 'mw_hidetoc', null, {
 						expires: 30,
@@ -199,7 +207,7 @@
 					} );
 					return true;
 				} else {
-					$tocList.slideUp( 'fast' );
+					$tocList.slideUp( 'fast', callback );
 					$toggleLink.text( mw.msg( 'showtoc' ) );
 					$.cookie( 'mw_hidetoc', '1', {
 						expires: 30,
@@ -208,7 +216,7 @@
 					return false;
 				}
 			} else {
-				return false;
+				return null;
 			}
 		},
 
@@ -216,8 +224,9 @@
 		 * Grab the URL parameter value for the given parameter.
 		 * Returns null if not found.
 		 *
-		 * @param param The parameter name
-		 * @param url URL to search through (optional)
+		 * @param param string The parameter name.
+		 * @param url string URL to search through (optional)
+		 * @return mixed Parameter value or null.
 		 */
 		'getParamValue' : function( param, url ) {
 			url = url ? url : document.location.href;
@@ -230,12 +239,17 @@
 			return null;
 		},
 
-		// Access key prefix.
-		// Will be re-defined based on browser/operating system detection in
-		// mw.util.init().
+		/**
+		 * @var string
+		 * Access key prefix. Will be re-defined based on browser/operating system
+		 * detection in mw.util.init().
+		 */
 		'tooltipAccessKeyPrefix' : 'alt-',
 
-		// Regex to match accesskey tooltips
+		/**
+		 * @var RegExp
+		 * Regex to match accesskey tooltips.
+		 */
 		'tooltipAccessKeyRegexp': /\[(ctrl-)?(alt-)?(shift-)?(esc-)?(.)\]$/,
 
 		/**
@@ -244,7 +258,7 @@
 		 * otherwise, all the nodes that will probably have accesskeys by
 		 * default are updated.
 		 *
-		 * @param nodeList jQuery object, or array of elements
+		 * @param nodeList mixed A jQuery object, or array of elements to update.
 		 */
 		'updateTooltipAccessKeys' : function( nodeList ) {
 			var $nodes;
@@ -274,8 +288,11 @@
 			} );
 		},
 
-		// jQuery object that refers to the page-content element
-		// Populated by init()
+		/*
+		 * @var jQuery
+		 * A jQuery object that refers to the page-content element
+		 * Populated by init().
+		 */
 		'$content' : null,
 
 		/**
@@ -284,8 +301,8 @@
 		 * p-cactions (Content actions), p-personal (Personal tools),
 		 * p-navigation (Navigation), p-tb (Toolbox)
 		 *
-		 * The first three paramters are required, others are optionals. Though
-		 * providing an id and tooltip is recommended.
+		 * The first three paramters are required, the others are optional and
+		 * may be null. Though providing an id and tooltip is recommended.
 		 *
 		 * By default the new link will be added to the end of the list. To
 		 * add the link before a given existing item, pass the DOM node
@@ -297,21 +314,21 @@
 		 *	 'MediaWiki.org', 't-mworg', 'Go to MediaWiki.org ', 'm', '#t-print'
 		 * )
 		 *
-		 * @param portlet ID of the target portlet ( 'p-cactions' or 'p-personal' etc.)
-		 * @param href Link URL
-		 * @param text Link text
-		 * @param id ID of the new item, should be unique and preferably have
-		 *	 the appropriate prefix ( 'ca-', 'pt-', 'n-' or 't-' )
-		 * @param tooltip Text to show when hovering over the link, without accesskey suffix
-		 * @param accesskey Access key to activate this link (one character, try
-		 *	 to avoid conflicts. Use $( '[accesskey=x]' ).get() in the console to
-		 *	 see if 'x' is already used.
-		 * @param nextnode DOM node or jQuery-selector string of the item that the new
-		 *	 item should be added before, should be another item in the same
-		 *	 list, it will be ignored otherwise
+		 * @param portlet string ID of the target portlet ( 'p-cactions' or 'p-personal' etc.)
+		 * @param href string Link URL
+		 * @param text string Link text
+		 * @param id string ID of the new item, should be unique and preferably have
+		 * the appropriate prefix ( 'ca-', 'pt-', 'n-' or 't-' )
+		 * @param tooltip string Text to show when hovering over the link, without accesskey suffix
+		 * @param accesskey string Access key to activate this link (one character, try
+		 * to avoid conflicts. Use $( '[accesskey=x]' ).get() in the console to
+		 * see if 'x' is already used.
+		 * @param nextnode mixed DOM Node or jQuery-selector string of the item that the new
+		 * item should be added before, should be another item in the same
+		 * list, it will be ignored otherwise
 		 *
-		 * @return The DOM node of the new item (a LI element, or A element for
-		 *	 older skins) or null.
+		 * @return mixed The DOM Node of the added item (a ListItem or Anchor element,
+		 * depending on the skin) or null if no element was added to the document.
 		 */
 		'addPortletLink' : function( portlet, href, text, id, tooltip, accesskey, nextnode ) {
 
@@ -399,7 +416,7 @@
 				} else {
 					$ul.append( $item );
 				}
-				
+
 
 				return $item[0];
 			}
@@ -412,8 +429,8 @@
 		 *
 		 * @param message mixed The DOM-element or HTML-string to be put inside the message box.
 		 * @param className	string Used in adding a class; should be different for each call
-		 *                         to allow CSS/JS to hide different boxes. null = no class used.
-		 * @return boolean True on success, false on failure
+		 * to allow CSS/JS to hide different boxes. null = no class used.
+		 * @return boolean True on success, false on failure.
 		 */
 		'jsMessage' : function( message, className ) {
 
@@ -457,7 +474,11 @@
 		 * according to HTML5 specification. Please note the specification
 		 * does not validate a domain with one character.
 		 *
-		 * FIXME: should be moved to or replaced by a JavaScript validation module.
+		 * @todo FIXME: should be moved to or replaced by a JavaScript validation module.
+		 *
+		 * @param mailtxt string E-mail address to be validated.
+		 * @return mixed Null if mailtxt was an empty string, otherwise true/false
+		 * is determined by validation.
 		 */
 		'validateEmail' : function( mailtxt ) {
 			if( mailtxt === '' ) {
@@ -525,14 +546,27 @@
 				);
 			return (null !== mailtxt.match( HTML5_email_regexp ) );
 		},
-		// Note: borrows from IP::isIPv4
+
+		/**
+		 * Note: borrows from IP::isIPv4
+		 *
+		 * @param address string
+		 * @param allowBlock boolean
+		 * @return boolean
+		 */
 		'isIPv4Address' : function( address, allowBlock ) {
 			var block = allowBlock ? '(?:\\/(?:3[0-2]|[12]?\\d))?' : '';
 			var RE_IP_BYTE = '(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|0?[0-9]?[0-9])';
 			var RE_IP_ADD = '(?:' + RE_IP_BYTE + '\\.){3}' + RE_IP_BYTE;
 			return address.search( new RegExp( '^' + RE_IP_ADD + block + '$' ) ) != -1;
 		},
-		// Note: borrows from IP::isIPv6
+		/**
+		 * Note: borrows from IP::isIPv6
+		 *
+		 * @param address string
+		 * @param allowBlock boolean
+		 * @return boolean
+		 */
 		'isIPv6Address' : function( address, allowBlock ) {
 			var block = allowBlock ? '(?:\\/(?:12[0-8]|1[01][0-9]|[1-9]?\\d))?' : '';
 			var RE_IPV6_ADD =

resources/mediawiki/mediawiki.htmlform.js
@@ -3,15 +3,27 @@
  */
 ( function( $ ) { 
 
-// Fade or snap depending on argument
+/**
+ * jQuery plugin to fade or snap to visible state.
+ *
+ * @param boolean instantToggle (optional)
+ * @return jQuery
+ */
 $.fn.goIn = function( instantToggle ) {
-	if ( typeof instantToggle != 'undefined' && instantToggle === true ) {
+	if ( instantToggle !== undefined && instantToggle === true ) {
 		return $(this).show();
 	}
 	return $(this).stop( true, true ).fadeIn();
 };
+
+/**
+ * jQuery plugin to fade or snap to hiding state.
+ *
+ * @param boolean instantToggle (optional)
+ * @return jQuery
+ */
 $.fn.goOut = function( instantToggle ) {
-	if ( typeof instantToggle != 'undefined' && instantToggle === true ) {
+	if ( instantToggle !== undefined && instantToggle === true ) {
 		return $(this).hide();
 	}
 	return $(this).stop( true, true ).fadeOut();
@@ -34,12 +46,12 @@ $.fn.liveAndTestAtStart = function( callback ){
 // Document ready:
 $( function() {
 
-	// animate the SelectOrOther fields, to only show the text field when
-	// 'other' is selected
+	// Animate the SelectOrOther fields, to only show the text field when
+	// 'other' is selected.
 	$( '.mw-htmlform-select-or-other' ).liveAndTestAtStart( function( instant ) {
 		var $other = $( '#' + $(this).attr( 'id' ) + '-other' );
 		$other = $other.add( $other.siblings( 'br' ) );
-		if ( $(this).val() == 'other' ) {
+		if ( $(this).val() === 'other' ) {
 			$other.goIn( instant );
 		} else {
 			$other.goOut( instant );

resources/mediawiki/mediawiki.js
@@ -210,7 +210,7 @@ window.mediaWiki = new ( function( $ ) {
 
 	/*
 	 * Dummy function which in debug mode can be replaced with a function that
-	 * emulates console.log in console-less environments. 
+	 * emulates console.log in console-less environments.
 	 */
 	this.log = function() { };
 
@@ -318,7 +318,8 @@ window.mediaWiki = new ( function( $ ) {
 					return $marker;
 				}
 				mw.log( 'getMarker> No <meta name="ResourceLoaderDynamicStyles"> found, inserting dynamically.' );
-				return $marker = $( '<meta>' ).attr( 'name', 'ResourceLoaderDynamicStyles' ).appendTo( 'head' );
+				$marker = $( '<meta>' ).attr( 'name', 'ResourceLoaderDynamicStyles' ).appendTo( 'head' );
+				return $marker;
 			}
 		}
 
@@ -512,7 +513,7 @@ window.mediaWiki = new ( function( $ ) {
 				};
 				if ( $.isArray( script ) ) {
 					var done = 0;
-					if (script.length == 0 ) {
+					if ( script.length === 0 ) {
 						// No scripts in this module? Let's dive out early.
 						markModuleReady();
 					}
@@ -534,8 +535,8 @@ window.mediaWiki = new ( function( $ ) {
 				if ( window.console && typeof window.console.log === 'function' ) {
 					console.log( _fn + 'Exception thrown by ' + module + ': ' + e.message );
 				}
-				throw e;
 				registry[module].state = 'error';
+				throw e;
 			}
 		}
 
@@ -674,8 +675,8 @@ window.mediaWiki = new ( function( $ ) {
 							!done
 							&& (
 								!this.readyState
-								|| this.readyState === "loaded"
-								|| this.readyState === "complete"
+								|| this.readyState === 'loaded'
+								|| this.readyState === 'complete'
 							)
 						) {
 							done = true;
@@ -1031,7 +1032,7 @@ window.mediaWiki = new ( function( $ ) {
 		*/
 		this.version = function() {
 			return mediaWiki.loader.getVersion.apply( mediaWiki.loader, arguments );
-		}
+		};
 
 		/**
 		 * Gets the state of a module
@@ -1120,7 +1121,7 @@ window.mediaWiki = new ( function( $ ) {
 			}
 			// Regular open tag
 			s += '>';
-			if ( typeof contents === 'string') {
+			if ( typeof contents === 'string' ) {
 				// Escaped
 				s += this.escape( contents );
 			} else if ( contents instanceof this.Raw ) {

tests/qunit/suites/resources/jquery/jquery.autoEllipsis.js
@@ -1,7 +1,7 @@
 module( 'jquery.autoEllipsis.js' );
 
 test( '-- Initial check', function(){
-
+	expect(1);
 	ok( jQuery.fn.autoEllipsis, 'jQuery.fn.autoEllipsis defined' );
 });
 
@@ -21,6 +21,8 @@ function findDivergenceIndex( a, b ) {
 }
 
 test( 'Position right', function() {
+	expect(3);
+
 	// We need this thing to be visible, so append it to the DOM
 	var origText = 'This is a really long random string and there is no way it fits in 100 pixels.';
 	var $wrapper = createWrappedDiv( origText );
@@ -33,7 +35,7 @@ test( 'Position right', function() {
 
 	// Check that the text fits by turning on word wrapping
 	$span.css( 'whiteSpace', 'nowrap' );
-	ok( $span.width() <= $span.parent().width(), "Text fits (span's width is no larger than its parent's width)" );
+	deepEqual( $span.width() <= $span.parent().width(), true, "Text fits (span's width is no larger than its parent's width)" );
 
 	// Add one character using scary black magic
 	var spanText = $span.text();
@@ -42,7 +44,7 @@ test( 'Position right', function() {
 
 	// Put this text in the span and verify it doesn't fit
 	$span.text( spanText );
-	ok( $span.width() > $span.parent().width(), 'Fit is maximal (adding one character makes it not fit any more)' );
+	deepEqual( $span.width() > $span.parent().width(), true, 'Fit is maximal (adding one character makes it not fit any more)' );
 
 	// Clean up
 	$wrapper.remove();

tests/qunit/suites/resources/jquery/jquery.colorUtil.js
@@ -1,11 +1,12 @@
 module( 'jquery.colorUtil.js' );
 
 test( '-- Initial check', function(){
-
+	expect(1);
 	ok( jQuery.colorUtil, 'jQuery.colorUtil defined' );
 });
 
 test( 'getRGB', function(){
+	expect(18);
 
 	equal( typeof jQuery.colorUtil.getRGB(), 'undefined', 'No arguments' );
 	equal( typeof jQuery.colorUtil.getRGB( '' ), 'undefined', 'Empty string' );
@@ -32,6 +33,8 @@ test( 'getRGB', function(){
 });
 
 test( 'rgbToHsl', function(){
+	expect(4);
+
 	var hsl = jQuery.colorUtil.rgbToHsl( 144, 238, 144 );
 	var dualDecimals = function(a,b){
 		return Math.round(a*100)/100;
@@ -45,6 +48,8 @@ test( 'rgbToHsl', function(){
 });
 
 test( 'hslToRgb', function(){
+	expect(4);
+
 	var rgb = jQuery.colorUtil.hslToRgb( 0.3, 0.7, 0.8 );
 
 	ok( rgb, 'Basic return evaluation' );
@@ -55,6 +60,7 @@ test( 'hslToRgb', function(){
 });
 
 test( 'getColorBrightness', function(){
+	expect(2);
 
 	var a = jQuery.colorUtil.getColorBrightness( 'red', +0.1 );
 

tests/qunit/suites/resources/jquery/jquery.mwPrototypes.js
@@ -54,5 +54,5 @@ test( 'Comparison functions', function(){
 	ok( $j.compareObject( { foo: 1 }, { foo: 1 } ), 'compareObject: Two the same objects' );
 	ok( !$j.compareObject( { bar: true }, { baz: false } ),
 	 'compareObject: Two different objects (false)' );
-	
+
 });

commit 15cdd55fb6762ef0f5dc75593c666b343e3b0818

Fix mw.log. Broken by variable renaming in r89082

resources/mediawiki/mediawiki.log.js
@@ -15,7 +15,7 @@
 	 * @author Trevor Parscal <tparscal@wikimedia.org>
 	 * @param logmsg string Message to output to console.
 	 */
-	mw.log = function( msg ) {
+	mw.log = function( logmsg ) {
 		// Allow log messages to use a configured prefix to identify the source window (ie. frame)
 		if ( mw.config.exists( 'mw.log.prefix' ) ) {
 			logmsg = mw.config.get( 'mw.log.prefix' ) + '> ' + logmsg;