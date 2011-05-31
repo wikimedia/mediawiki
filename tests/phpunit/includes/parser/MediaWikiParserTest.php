6c2654b7f8a2705c77ff9468e1bc18b87e47102d
Platonides <platonides@users.mediawiki.org>
2011-05-31 20:30:12 +0000

    Revert r89223. MediaWikiParserTest is needed to run all the $wgParserTestFiles NewParserTest.php standalone running of the first $wgParserTestFiles item is just a convenience.

---
+++ tests/phpunit/includes/parser/MediaWikiParserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once( dirname( __FILE__ ) . '/ParserHelpers.php' );
+require_once( dirname( __FILE__ ) . '/NewParserTest.php' );
+require_once( dirname(dirname(dirname( __FILE__ ))) . '/bootstrap.php' );
+
+/**
+ * The UnitTest must be either a class that inherits from PHPUnit_Framework_TestCase 
+ * or a class that provides a public static suite() method which returns 
+ * an PHPUnit_Framework_Test object
+ * 
+ * @group Parser
+ * @group Database
+ */
+class MediaWikiParserTest {
+
+	public static function suite() {
+		global $wgParserTestFiles;
+
+		$suite = new PHPUnit_Framework_TestSuite;
+
+		foreach ( $wgParserTestFiles as $filename ) {
+			$testsName = basename( $filename, '.txt' );
+			$className = /*ucfirst( basename( dirname( $filename ) ) ) .*/ ucfirst( basename( $filename, '.txt' ) );
+			
+			eval( "/** @group Database\n@group Parser\n*/ class $className extends NewParserTest { protected \$file = \"" . addslashes( $filename ) . "\"; } " );
+
+			$parserTester = new $className( $testsName );
+			$suite->addTestSuite( new ReflectionClass ( $parserTester ) );
+		}
+		
+
+		return $suite;
+	}
+}
