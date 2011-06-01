22508f43d58a8e2fa7946db1c8a5799fcc2417a9
Platonides <platonides@users.mediawiki.org>
2011-06-01 22:04:44 +0000

    Not silly, but parser error (r89263)... fixing PHP Parse error:  syntax error, unexpected T_STRING, expecting T_VARIABLE in ExtensionUpdater.php on line 7

--- includes/installer/ExtensionUpdater.php
+++ includes/installer/ExtensionUpdater.php
@@ -4,7 +4,7 @@ class ExtensionUpdater extends DatabaseUpdater {
 
 	// overload the constructor, so the init and hooks don't get called
 	// while still have all the patching code without duplicating
-	public __construct( DatabaseBase &$db, $shared, Maintenance $maintenance = null  ) {
+	public function __construct( DatabaseBase &$db, $shared, Maintenance $maintenance = null  ) {
 		$this->db = $db;
 		$this->shared = $shared;
 		$this->maintenance = $maintenance;
