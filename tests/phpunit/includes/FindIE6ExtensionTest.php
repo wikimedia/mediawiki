529e92b2830f02906a7df7854f2aecf46326742d
Tim Starling <tstarling@users.mediawiki.org>
2011-06-01 00:51:09 +0000

    Fixes for r88883, r89197: * Modified WebRequest::findIE6Extension() to fix the performance issue and the hash parsing issue I noted on CR * In FindIE6ExtensionTest, fixed all the assertEquals() calls, I had the expected and actual around the wrong way * Added a couple of extra tests for cases that seemed important during the rewrite.

--- tests/phpunit/includes/FindIE6ExtensionTest.php
+++ tests/phpunit/includes/FindIE6ExtensionTest.php
@@ -6,97 +6,113 @@
 class FindIE6ExtensionTest extends MediaWikiTestCase {
 	function testSimple() {
 		$this->assertEquals( 
-			WebRequest::findIE6Extension( 'x.y' ),
 			'y',
+			WebRequest::findIE6Extension( 'x.y' ),
 			'Simple extension'
 		);
 	}
 
 	function testSimpleNoExt() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'x' ),
 			'',
+			WebRequest::findIE6Extension( 'x' ),
 			'No extension'
 		);
 	}
 
 	function testEmpty() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( '' ),
 			'',
+			WebRequest::findIE6Extension( '' ),
 			'Empty string'
 		);
 	}
 
 	function testQuestionMark() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( '?' ),
 			'',
+			WebRequest::findIE6Extension( '?' ),
 			'Question mark only'
 		);
 	}
 
 	function testExtQuestionMark() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( '.x?' ),
 			'x',
+			WebRequest::findIE6Extension( '.x?' ),
 			'Extension then question mark'
 		);
 	}
 
 	function testQuestionMarkExt() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( '?.x' ),
 			'x',
+			WebRequest::findIE6Extension( '?.x' ),
 			'Question mark then extension'
 		);
 	}
 
 	function testInvalidChar() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( '.x*' ),
 			'',
+			WebRequest::findIE6Extension( '.x*' ),
 			'Extension with invalid character'
+		);
+	}
+
+	function testInvalidCharThenExtension() {
+		$this->assertEquals(
+			'x',
+			WebRequest::findIE6Extension( '*.x' ),
+			'Invalid character followed by an extension'
 		);
 	}
 
 	function testMultipleQuestionMarks() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'a?b?.c?.d?e?f' ),
 			'c',
+			WebRequest::findIE6Extension( 'a?b?.c?.d?e?f' ),
 			'Multiple question marks'
 		);
 	}
 
 	function testExeException() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'a?b?.exe?.d?.e' ),
 			'd',
+			WebRequest::findIE6Extension( 'a?b?.exe?.d?.e' ),
 			'.exe exception'
 		);
 	}
 
 	function testExeException2() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'a?b?.exe' ),
 			'exe',
+			WebRequest::findIE6Extension( 'a?b?.exe' ),
 			'.exe exception 2'
 		);
 	}
 
 	function testHash() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'a#b.c' ),
 			'',
+			WebRequest::findIE6Extension( 'a#b.c' ),
 			'Hash character preceding extension'
 		);
 	}
 
 	function testHash2() {
 		$this->assertEquals(
-			WebRequest::findIE6Extension( 'a?#b.c' ),
 			'',
+			WebRequest::findIE6Extension( 'a?#b.c' ),
 			'Hash character preceding extension 2'
+		);
+	}
+
+	function testDotAtEnd() {
+		$this->assertEquals(
+			'',
+			WebRequest::findIE6Extension( '.' ),
+			'Dot at end of string'
 		);
 	}
 }
