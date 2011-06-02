53ec48fe15ec94e1518e840ae5673c84428c1376
Robin Pepermans <robin@users.mediawiki.org>
2011-06-02 13:14:16 +0000

    Follow-up to r89329: Add MessagesRoa_rup.php as fallback to 'rup'

---
+++ languages/messages/MessagesRoa_rup.php
@@ -0,0 +1,15 @@
+<?php
+/** Aromanian (Armãneashce)
+ *
+ * ISO 639-3 code 'rup' should be used. This is code is maintained for backward
+ * compatilibity.
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'rup';
