0283bae46567a8cc862f580f0b5aba011303c5eb
Chad Horohoe <demon@users.mediawiki.org>
2011-06-04 21:34:07 +0000

    pear -> port

--- tests/phpunit/install-phpunit.sh
+++ tests/phpunit/install-phpunit.sh
@@ -26,7 +26,7 @@ else if ( has_binary apt-get ); then
 else if ( has_binary yum ); then
     echo Installing phpunit with yum
     yum install phpunit
-else if ( has_binary pear ); then
+else if ( has_binary port ); then
     echo Installing phpunit with macports
     port install php5-unit
 fi
