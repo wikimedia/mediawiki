9d5f31068e9e31bd1d38b71e1ceaccf836190ef0
Platonides <platonides@users.mediawiki.org>
2011-06-04 17:53:46 +0000

    Add BlockTest to the tests autoloader. Needed by ApiBlockTest.php Added tabs to other items (compare ignoring whitespace).

--- tests/TestsAutoLoader.php
+++ tests/TestsAutoLoader.php
@@ -9,17 +9,17 @@ $wgAutoloadClasses += array(
 	'MediaWikiTestCase' => "$testFolder/phpunit/MediaWikiTestCase.php",
 	'MediaWikiPHPUnitCommand' => "$testFolder/phpunit/MediaWikiPHPUnitCommand.php",
 	'MediaWikiLangTestCase' => "$testFolder/phpunit/MediaWikiLangTestCase.php",
-		
-		//API
-		'ApiTestSetup' => "$testFolder/phpunit/includes/api/ApiSetup.php",
-		'ApiTestCase' => "$testFolder/phpunit/includes/api/ApiTestCase.php",
-		'ApiTestUser' => "$testFolder/phpunit/includes/api/ApiTestUser.php",
-		'RandomImageGenerator' => "$testFolder/phpunit/includes/api/RandomImageGenerator.php",
+		//includes
+		'BlockTest' => "$testFolder/phpunit/includes/BlockTest.php",	
+			//API
+			'ApiTestSetup' => "$testFolder/phpunit/includes/api/ApiSetup.php",
+			'ApiTestCase' => "$testFolder/phpunit/includes/api/ApiTestCase.php",
+			'ApiTestUser' => "$testFolder/phpunit/includes/api/ApiTestUser.php",
+			'RandomImageGenerator' => "$testFolder/phpunit/includes/api/RandomImageGenerator.php",
 	
-		//Parser
-		'ParserTestFileIterator' => "$testFolder/phpunit/includes/parser/NewParserHelpers.php",
-		
-		
+			//Parser
+			'ParserTestFileIterator' => "$testFolder/phpunit/includes/parser/NewParserHelpers.php",
+	
 	//Selenium
 	'SeleniumTestConstants' => "$testFolder/selenium/SeleniumTestConstants.php",
 
