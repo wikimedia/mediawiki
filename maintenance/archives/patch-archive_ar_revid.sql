0af0200bc77f4a5be4514f64693915b87e622a20
Max Semenik <maxsem@users.mediawiki.org>
2011-06-05 19:39:28 +0000

    SQLite: sync a couple of indexes with MySQL

--- maintenance/archives/patch-archive_ar_revid.sql
+++ maintenance/archives/patch-archive_ar_revid.sql
@@ -1,4 +1,3 @@
 -- Hopefully temporary index.
 -- For https://bugzilla.wikimedia.org/show_bug.cgi?id=21279
-ALTER TABLE /*$wgDBprefix*/archive
-	ADD INDEX ar_revid ( ar_rev_id );
+CREATE INDEX /*i*/ar_revid ON /*$wgDBprefix*/archive ( ar_rev_id );
