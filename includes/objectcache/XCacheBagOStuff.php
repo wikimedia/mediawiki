c2319387fa980b6481efc993d241eb262ddb7256
Chad Horohoe <demon@users.mediawiki.org>
2011-06-08 00:53:56 +0000

    Revert r87145, bug 28752: Xcache doesn't work in cli mode. As pointed out on CR, this didn't fix it, it just hid the issue.

--- includes/objectcache/XCacheBagOStuff.php
+++ includes/objectcache/XCacheBagOStuff.php
@@ -8,27 +8,12 @@
  */
 class XCacheBagOStuff extends BagOStuff {
 	/**
-	 * Are we operating in CLI mode? Since xcache doesn't work then and they 
-	 * don't want to change that
-	 * @see bug 28752
-	 * @var bool
-	 */
-	private $isCli = false;
-
-	public function __construct() {
-		$this->isCli = php_sapi_name() == 'cli';
-	}
-
-	/**
 	 * Get a value from the XCache object cache
 	 *
 	 * @param $key String: cache key
 	 * @return mixed
 	 */
 	public function get( $key ) {
-		if( $this->isCli ) {
-			return false;
-		}
 		$val = xcache_get( $key );
 
 		if ( is_string( $val ) ) {
@@ -47,9 +32,7 @@ class XCacheBagOStuff extends BagOStuff {
 	 * @return bool
 	 */
 	public function set( $key, $value, $expire = 0 ) {
-		if( !$this->isCli ) {
-			xcache_set( $key, serialize( $value ), $expire );
-		}
+		xcache_set( $key, serialize( $value ), $expire );
 		return true;
 	}
 
@@ -61,9 +44,7 @@ class XCacheBagOStuff extends BagOStuff {
 	 * @return bool
 	 */
 	public function delete( $key, $time = 0 ) {
-		if( !$this->isCli ) {
-			xcache_unset( $key );
-		}
+		xcache_unset( $key );
 		return true;
 	}
 }
