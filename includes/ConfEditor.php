678482beaaf357a20f8545276927614fca5e92b5
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2011-06-09 19:52:07 +0000

    Removed some usage of error suppression operator

--- includes/ConfEditor.php
+++ includes/ConfEditor.php
@@ -315,7 +315,7 @@ class ConfEditor {
 			// extra quote on the end!
 			return strtr( substr( trim( $str ), 1, -1 ),
 				array( '\\\'' => '\'', '\\\\' => '\\' ) );
-		if ( $str !== '' && @$str[0] == '"' )
+		if ( $str !== '' && $str[0] == '"' )
 			// Double-quoted string
 			// @todo FIXME: trim() call is due to mystery bug where whitespace gets
 			// appended to the token; without it we ended up reading in the
