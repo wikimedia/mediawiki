1d7cf0ff336e1d97f57e6c339a14271f86e882bc
Sam Reed <reedy@users.mediawiki.org>
2011-06-11 07:33:35 +0000

    Followup r88873, remove setting of $wgEnableCreativeCommonsRdf in LocalSettingsGenerator

--- includes/installer/LocalSettingsGenerator.php
+++ includes/installer/LocalSettingsGenerator.php
@@ -322,7 +322,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 ## For attaching licensing metadata to pages, and displaying an
 ## appropriate copyright notice / icon. GNU Free Documentation
 ## License and Creative Commons licenses are supported so far.
-{$rightsUrl}\$wgEnableCreativeCommonsRdf = true;
 \$wgRightsPage = \"\"; # Set to the title of a wiki page that describes your license/copyright
 \$wgRightsUrl  = \"{$this->values['wgRightsUrl']}\";
 \$wgRightsText = \"{$this->values['wgRightsText']}\";
