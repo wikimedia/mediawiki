9bbe0bc56625bad3740f485fb8275fea56fcf5af
Bryan Tong Minh <btongminh@users.mediawiki.org>
2011-06-18 14:56:14 +0000

    (bug 29408) Key 'something.' is not in a proper format Fixed this by relaxing the stash key regex. For some files MediaWiki is simply not able to guess an extension. If the wiki has been configured to allow them, we should just let them pass in the stash as well.

---
+++ tests/phpunit/includes/upload/UploadStashTest.php
@@ -0,0 +1,35 @@
+<?php
+
+class UploadStashTest extends MediaWikiTestCase {
+	public function setUp() {
+		parent::setUp();
+		
+		// Setup a fake session if necessary
+		if ( !isset( $_SESSION ) ) {
+			$GLOBALS['_SESSION'] = array();
+		}
+		
+		// Setup a file for bug 29408
+		$this->bug29408File = dirname( __FILE__ ) . '/bug29408';
+		file_put_contents( $this->bug29408File, "\x00" );		
+	}
+	
+	public function testBug29408() {
+		$repo = RepoGroup::singleton()->getLocalRepo();
+		$stash = new UploadStash( $repo );
+		
+		// Throws exception caught by PHPUnit on failure
+		$file = $stash->stashFile( $this->bug29408File );
+		// We'll never reach this point if we hit bug 29408
+		$this->assertTrue( true, 'Unrecognized file without extension' );
+		
+		$file->remove();
+	}
+	
+	public function tearDown() {
+		parent::tearDown();
+		
+		unlink( $this->bug29408File );
+		
+	}
+}
