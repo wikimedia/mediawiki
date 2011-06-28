819f9adffe23914d71975e59dd489f0ea4e8d0df
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2011-06-28 17:20:16 +0000

    * Fixed documentation * Added GPL header

--- includes/Wiki.php
+++ includes/Wiki.php
@@ -1,6 +1,27 @@
 <?php
 /**
- * MediaWiki is the to-be base class for this whole project
+ * Helper class for the index.php entry point.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * The MediaWiki class is the helper class for the index.php entry point.
  *
  * @internal documentation reviewed 15 Mar 2010
  */
@@ -34,7 +55,7 @@ class MediaWiki {
 	}
 
 	/**
-	 * Parse $request to get the Title object
+	 * Parse the request to get the Title object
 	 *
 	 * @return Title object to be $wgTitle
 	 */
