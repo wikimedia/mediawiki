707a2fa8fe3aa36e43fc020d57675daa6c073cae
Bryan Tong Minh <btongminh@users.mediawiki.org>
2011-06-30 17:35:37 +0000

    Follow-up r80988: fix function documentation

--- includes/HTMLForm.php
+++ includes/HTMLForm.php
@@ -317,6 +317,7 @@ class HTMLForm {
 	/**
 	 * Add header text, inside the form.
 	 * @param $msg String complete text of message to display
+	 * @param $section The section to add the header to
 	 */
 	function addHeaderText( $msg, $section = null ) {
 		if ( is_null( $section ) ) {
@@ -332,6 +333,7 @@ class HTMLForm {
 	/**
 	 * Add footer text, inside the form.
 	 * @param $msg String complete text of message to display
+	 * @param $section string The section to add the footer text to
 	 */
 	function addFooterText( $msg, $section = null ) {
 		if ( is_null( $section ) ) {
