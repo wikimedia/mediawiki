13be8d3afb180c168e88306bfd9ccab811ffba1a
Leo Koppelkamm <diebuche@users.mediawiki.org>
2011-07-08 17:07:11 +0000

    Migrate skins/common/search.js functionality to mediawiki.special.search.js. Make JS unobtrusive.

--- resources/mediawiki.special/mediawiki.special.search.js
+++ resources/mediawiki.special/mediawiki.special.search.js
@@ -8,4 +8,30 @@ if ( !( 'autofocus' in document.createElement( 'input' ) ) ) {
 	$( 'input[autofocus]:first' ).focus();
 }
 
+// Bind check all/none button
+var $checkboxes = $('#powersearch input[id^=mw-search-ns]');
+$('#mw-search-toggleall').click( function() {
+	$checkboxes.prop("checked", true);
+} );
+$('#mw-search-togglenone').click( function() {
+	$checkboxes.prop("checked", false);
+} );
+
+// Change the header search links to what user entered
+var headerLinks = $('.search-types a');
+$('#searchText, #powerSearchText').change(function() {
+	var searchterm = $(this).val();
+	headerLinks.each( function() {
+		var parts = this.href.split( 'search=' );
+		var lastpart = '';
+		var prefix = 'search=';
+		if( parts.length > 1 && parts[1].indexOf('&') >= 0 ) {
+			lastpart = parts[1].substring( parts[1].indexOf('&') );
+		} else {
+			prefix = '&search=';
+		}
+		this.href = parts[0] + prefix + encodeURIComponent( searchterm ) + lastpart;
+	});
+}).trigger('change');
+
 } )( jQuery );
