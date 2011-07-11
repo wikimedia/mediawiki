258635c7968d0688a19c1d2d4000e0776ad9b6e3
Brian Wolff <bawolff@users.mediawiki.org>
2011-07-11 16:42:15 +0000

    (follow-up r90256) Unit tests.

---
+++ tests/phpunit/includes/media/JpegTest.php
@@ -0,0 +1,24 @@
+<?php
+class JpegTest extends MediaWikiTestCase {
+
+	public function testInvalidFile() {
+		global $wgShowEXIF;
+		if ( !$wgShowEXIF ) {
+			$this->markTestIncomplete( "This test needs the exif extension." );
+		}
+		$jpeg = new JpegHandler;
+		$res = $jpeg->getMetadata( null, dirname( __FILE__ ) . '/README' );
+		$this->assertEquals( ExifBitmapHandler::BROKEN_FILE, $res );
+	}
+	public function testTiffFile() {
+		global $wgShowEXIF;
+		if ( !$wgShowEXIF ) {
+			$this->markTestIncomplete( "This test needs the exif extension." );
+		}
+		$h = new JpegHandler;
+		$res = $h->getMetadata( null, dirname( __FILE__ ) . '/test.jpg' );
+		$expected = 'a:7:{s:16:"ImageDescription";s:9:"Test file";s:11:"XResolution";s:4:"72/1";s:11:"YResolution";s:4:"72/1";s:14:"ResolutionUnit";i:2;s:16:"YCbCrPositioning";i:1;s:15:"JPEGFileComment";a:1:{i:0;s:17:"Created with GIMP";}s:22:"MEDIAWIKI_EXIF_VERSION";i:2;}';
+		// Hopefully php always serializes things in the same order.
+		$this->assertEquals( $expected, $res );
+	}
+}
---
+++ tests/phpunit/includes/media/TiffTest.php
@@ -0,0 +1,24 @@
+<?php
+class TiffTest extends MediaWikiTestCase {
+
+	public function testInvalidFile() {
+		global $wgShowEXIF;
+		if ( !$wgShowEXIF ) {
+			$this->markTestIncomplete( "This test needs the exif extension." );
+		}
+		$tiff = new TiffHandler;
+		$res = $tiff->getMetadata( null, dirname( __FILE__ ) . '/README' );
+		$this->assertEquals( ExifBitmapHandler::BROKEN_FILE, $res );
+	}
+	public function testTiffFile() {
+		global $wgShowEXIF;
+		if ( !$wgShowEXIF ) {
+			$this->markTestIncomplete( "This test needs the exif extension." );
+		}
+		$tiff = new TiffHandler;
+		$res = $tiff->getMetadata( null, dirname( __FILE__ ) . '/test.tiff' );
+		$expected = 'a:16:{s:10:"ImageWidth";i:20;s:11:"ImageLength";i:20;s:13:"BitsPerSample";a:3:{i:0;i:8;i:1;i:8;i:2;i:8;}s:11:"Compression";i:5;s:25:"PhotometricInterpretation";i:2;s:16:"ImageDescription";s:17:"Created with GIMP";s:12:"StripOffsets";i:8;s:11:"Orientation";i:1;s:15:"SamplesPerPixel";i:3;s:12:"RowsPerStrip";i:64;s:15:"StripByteCounts";i:238;s:11:"XResolution";s:19:"1207959552/16777216";s:11:"YResolution";s:19:"1207959552/16777216";s:19:"PlanarConfiguration";i:1;s:14:"ResolutionUnit";i:2;s:22:"MEDIAWIKI_EXIF_VERSION";i:2;}';
+		// Hopefully php always serializes things in the same order.
+		$this->assertEquals( $expected, $res );
+	}
+}
