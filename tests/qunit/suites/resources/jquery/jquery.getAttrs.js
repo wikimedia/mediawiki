ffbff574bdfd30e00c437bbc49f06f7e226ea940
Krinkle <krinkle@users.mediawiki.org>
2011-07-13 22:46:17 +0000

    New jquery.getAttrs module - A cross-browser compatible way of getting a direct key/value object of attributes - Will help making comparisons such as in r92113 easier and work cross-browser

---
+++ tests/qunit/suites/resources/jquery/jquery.getAttrs.js
@@ -0,0 +1,17 @@
+module( 'jquery.getAttrs.js' );
+
+test( '-- Initial check', function() {
+	expect(1);
+	ok( $.fn.getAttrs, 'jQuery.fn.getAttrs defined' );
+} );
+
+test( 'Check', function() {
+	expect(1);
+	var	attrs = {
+			foo: 'bar',
+			'class': 'lorem'
+		},
+		$el = $( '<div>', attrs );
+
+	deepEqual( $el.getAttrs(), attrs, 'getAttrs() return object should match the attributes set, no more, no less' );
+} );
