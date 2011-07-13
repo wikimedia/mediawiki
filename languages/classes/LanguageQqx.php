0b1ebe5690648da7525148d7df5e61eb1a7fcca6
Robin Pepermans <robin@users.mediawiki.org>
2011-07-13 17:46:13 +0000

    (bug 15802) An easy way to look up messages: language qqx which returns the message keys (It doesn't show up in preferences, but you can use ?uselang=qqx)
    Patch by Conrad Irwin (slightly modified)

---
+++ languages/classes/LanguageQqx.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * For all translated messages, this returns the name of the message bracketed.
+ * This does not affect untranslated messages.
+ *
+ * NOTE: It returns a valid title, because there are some poorly written
+ * extentions that assume the contents of some messages are valid.
+ *
+ * @ingroup Language
+ */
+class LanguageQqx extends Language {
+	function getMessage( $key ) {
+		return "[$key]";
+	}
+}
