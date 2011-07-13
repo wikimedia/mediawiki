8e4efbe786a372f54ca710cb5bc6115603e1f57a
Chad Horohoe <demon@users.mediawiki.org>
2011-07-13 01:55:49 +0000

    Forgot to change my default header, followup r92045

--- tests/phpunit/includes/GlobalFunctions/wfExpandUrl.php
+++ tests/phpunit/includes/GlobalFunctions/wfExpandUrl.php
@@ -1,7 +1,6 @@
 <?php
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Unit tests for wfExpandUrl()
  */
 
 class wfExpandUrl extends MediaWikiTestCase {
