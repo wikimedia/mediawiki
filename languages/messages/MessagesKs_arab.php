d839b3ada1a58a48b12b3ac3ac17fb38b62a5584
Robin Pepermans <robin@users.mediawiki.org>
2011-07-15 23:28:30 +0000

    Split MessagesKs into ks-arab and ks-deva, as Kashmiri is written in two scripts. The MessagesKs file contained rtl = true, which is only for the Arab script, and the digit transform table which was for the Devanagari script. Added one lonely message from kswiki.
    Also specified comment of Quechua to Southern Quechua, as it is the variety used in MessagesQu and on quwiki.

---
+++ languages/messages/MessagesKs_arab.php
@@ -0,0 +1,33 @@
+<?php
+/** Kashmiri (Perso-Arabic) (كشميري)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+# @todo FIXME: Inherit almost everything for now
+$rtl = true;
+
+$digitTransformTable = array(
+	'0' => '٠', # &#x0660;
+	'1' => '١', # &#x0661;
+	'2' => '٢', # &#x0662;
+	'3' => '٣', # &#x0663;
+	'4' => '٤', # &#x0664;
+	'5' => '٥', # &#x0665;
+	'6' => '٦', # &#x0666;
+	'7' => '٧', # &#x0667;
+	'8' => '٨', # &#x0668;
+	'9' => '٩', # &#x0669;
+	'.' => '٫', # &#x066b; wrong table ?
+	',' => '٬', # &#x066c;
+);
+
+$messages = array(
+# Recent changes
+'recentchanges' => 'نَوِ تبدیلی',
+);
