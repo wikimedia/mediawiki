d18c0dc3155d3d9f0c70549c61af915ad4844a7b
Sam Reed <reedy@users.mediawiki.org>
2011-07-17 16:18:09 +0000

    * (bug 28817) Add reference help page link to API Modules
    Start of this bug, fleshed out base code implemented, plus a few key modules (will finish rest up soon)
    
    Tweaked/improved/updated some documentation as I went through also

--- includes/api/ApiLogout.php
+++ includes/api/ApiLogout.php
@@ -73,6 +73,10 @@ class ApiLogout extends ApiBase {
 		);
 	}
 
+	public function getHelpUrl() {
+		return 'http://www.mediawiki.org/wiki/API:Logout';
+	}
+
 	public function getVersion() {
 		return __CLASS__ . ': $Id$';
 	}
