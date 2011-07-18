34e3b75f483a3b1092f5d16ad8c327d5f2d6d993
Sam Reed <reedy@users.mediawiki.org>
2011-07-18 23:04:09 +0000

    Revert 2 from r92498

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.18';
+$wgVersion = '1.19alpha';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
