1cf71a1b1a9c34f59992a530995f65dc66fe89cf
Antoine Musso <hashar@users.mediawiki.org>
2011-07-27 19:20:06 +0000

    revert r93264 : breaks ton of parser tests.
    Please update the parser tests and reapply.

--- includes/Linker.php
+++ includes/Linker.php
@@ -1456,10 +1456,10 @@ class Linker {
 	static function tocList( $toc, $lang = false ) {
 		$title = wfMsgExt( 'toc', array( 'language' => $lang, 'escape' ) );
 		return
-		   '<div id="toc" class="toc">'
+		   '<table id="toc" class="toc"><tr><td>'
 		 . '<div id="toctitle"><h2>' . $title . "</h2></div>\n"
 		 . $toc
-		 . "</ul>\n</div>\n";
+		 . "</ul>\n</td></tr></table>\n";
 	}
 
 	/**
