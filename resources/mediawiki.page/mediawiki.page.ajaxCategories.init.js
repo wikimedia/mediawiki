0554848efa30783b671836cc5f55c53a8c856109
Roan Kattouw <catrope@users.mediawiki.org>
2011-08-11 14:47:18 +0000

    Fix a few comment typos noticed when doing JS review

--- resources/mediawiki.page/mediawiki.page.ajaxCategories.init.js
+++ resources/mediawiki.page/mediawiki.page.ajaxCategories.init.js
@@ -1,6 +1,6 @@
 mw.page.ajaxCategories = new mw.ajaxCategories();
 jQuery( document ).ready( function(){
-	// Seperate function for call to prevent jQuery
+	// Separate function for call to prevent jQuery
 	// from executing it in the document context.
 	mw.page.ajaxCategories.setup();
 } );
