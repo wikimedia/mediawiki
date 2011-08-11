28b8e13ff204e55818b84536d61c98ccbfc03094
Sam Reed <reedy@users.mediawiki.org>
2011-08-11 19:10:22 +0000

    I think 3 and a half years is long enough for the redirection to be left around

--- maintenance/dumpHTML.php
+++
@@ -1,7 +0,0 @@
-dumpHTML has moved to the DumpHTML extension.
-
-WebDAV/SVN:
-http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/DumpHTML/
-
-Web:
-http://svn.wikimedia.org/viewvc/mediawiki/trunk/extensions/DumpHTML/
