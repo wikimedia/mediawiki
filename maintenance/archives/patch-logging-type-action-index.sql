6026ca43ea4ce8b2d998bea635716a8defa9321d
Sam Reed <reedy@users.mediawiki.org>
2011-08-11 12:49:30 +0000

    Followup r93211, add log_timestamp to end of the index

--- maintenance/archives/patch-logging-type-action-index.sql
+++ maintenance/archives/patch-logging-type-action-index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX /*i*/type_action ON /*_*/logging(log_type, log_action);
+CREATE INDEX /*i*/type_action ON /*_*/logging(log_type, log_action, log_timestamp);
--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -1226,7 +1226,7 @@ CREATE INDEX /*i*/page_time ON /*_*/logging (log_namespace, log_title, log_times
 CREATE INDEX /*i*/times ON /*_*/logging (log_timestamp);
 CREATE INDEX /*i*/log_user_type_time ON /*_*/logging (log_user, log_type, log_timestamp);
 CREATE INDEX /*i*/log_page_id_time ON /*_*/logging (log_page,log_timestamp);
-CREATE INDEX /*i*/type_action ON /*_*/logging(log_type, log_action);
+CREATE INDEX /*i*/type_action ON /*_*/logging(log_type, log_action, log_timestamp);
 
 
 CREATE TABLE /*_*/log_search (
