57c33820a97397ce90932febab26ced25ef2420e
Daniel Friesen <dantman@users.mediawiki.org>
2011-08-12 10:20:17 +0000

    Use a regex when checking for external urls. It's concise and DRY, less prone to bugs like "Whoops I got that hardcoded length int wrong and created a condition that'll never be true",... and it's 4 time faster ;)

--- resources/mediawiki/mediawiki.js
+++ resources/mediawiki/mediawiki.js
@@ -1071,7 +1071,7 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 			// Allow calling with an external script or single dependency as a string
 			if ( typeof modules === 'string' ) {
 				// Support adding arbitrary external scripts
-				if ( modules.substr( 0, 7 ) === 'http://' || modules.substr( 0, 8 ) === 'https://' || modules.substr( 0, 2 ) === '//' ) {
+				if ( /^(https?:)?\/\//.test( modules ) ) {
 					if ( type === 'text/css' ) {
 						$( 'head' ).append( $( '<link/>', {
 							rel: 'stylesheet',
