0c0d8871853685173516533ddfbdbf4c61408e85
Sam Reed <reedy@users.mediawiki.org>
2011-08-17 22:38:36 +0000

    Stylize
    +upstream Wikia changes

--- includes/profiler/ProfilerSimpleUDP.php
+++ includes/profiler/ProfilerSimpleUDP.php
@@ -26,21 +26,28 @@ class ProfilerSimpleUDP extends ProfilerSimple {
 		}
 
 		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-		$plength=0;
-		$packet="";
-		foreach ($this->mCollated as $entry=>$pfdata) {
-			$pfline=sprintf ("%s %s %d %f %f %f %f %s\n", $this->getProfileID(),"-",$pfdata['count'],
-				$pfdata['cpu'],$pfdata['cpu_sq'],$pfdata['real'],$pfdata['real_sq'],$entry);
-			$length=strlen($pfline);
-			/* printf("<!-- $pfline -->"); */
-			if ($length+$plength>1400) {
-				socket_sendto($sock,$packet,$plength,0,$wgUDPProfilerHost,$wgUDPProfilerPort);
-				$packet="";
-				$plength=0;
+		$plength = 0;
+		$packet = "";
+		foreach ( $this->mCollated as $entry => $pfdata ) {
+			if( !isset($pfdata['count'])
+				|| !isset( $pfdata['cpu'] )
+				|| !isset( $pfdata['cpu_sq'] )
+				|| !isset( $pfdata['real'] )
+				|| !isset( $pfdata['real_sq'] ) ) {
+				continue;
 			}
-			$packet.=$pfline;
-			$plength+=$length;
+			$pfline = sprintf( "%s %s %d %f %f %f %f %s\n", $this->getProfileID(), "-", $pfdata['count'],
+				$pfdata['cpu'], $pfdata['cpu_sq'], $pfdata['real'], $pfdata['real_sq'], $entry);
+			$length = strlen( $pfline );
+			/* printf("<!-- $pfline -->"); */
+			if ( $length + $plength > 1400 ) {
+				socket_sendto( $sock, $packet, $plength, 0, $wgUDPProfilerHost, $wgUDPProfilerPort );
+				$packet = "";
+				$plength = 0;
+			}
+			$packet .= $pfline;
+			$plength += $length;
 		}
-		socket_sendto($sock,$packet,$plength,0x100,$wgUDPProfilerHost,$wgUDPProfilerPort);
+		socket_sendto( $sock, $packet, $plength, 0x100, $wgUDPProfilerHost, $wgUDPProfilerPort );
 	}
 }
