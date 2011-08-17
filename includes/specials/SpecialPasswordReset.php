782edae4cbe8eba8952e4588ddbb6c1a6a914395
Daniel Friesen <dantman@users.mediawiki.org>
2011-08-17 00:41:17 +0000

    Fix some bad "Implements Special:*" comments in includes/specials/ files.

--- includes/specials/SpecialPasswordReset.php
+++ includes/specials/SpecialPasswordReset.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Implements Special:Blankpage
+ * Implements Special:PasswordReset
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- includes/specials/SpecialUserlogout.php
+++ includes/specials/SpecialUserlogout.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Implements Special:Upload
+ * Implements Special:Userlogout
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
