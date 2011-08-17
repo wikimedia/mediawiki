10bed635cc218ab8b11b92ecfc5a0e567f3e9e67
Brian Wolff <bawolff@users.mediawiki.org>
2011-08-17 21:53:45 +0000

    follow-up r91885 - address CR comments
    * rename testTiffFile to something more descriptive
    * unserialize the serialized results before comparing
    Also follow-up r92635 - rm an extra wfDl that was outside any functions, and I'm assuming accidentally there (?)

--- tests/phpunit/includes/media/JpegTest.php
+++ tests/phpunit/includes/media/JpegTest.php
@@ -1,6 +1,4 @@
 <?php
-wfDl('exif');
-
 class JpegTest extends MediaWikiTestCase {
 
 	public function testInvalidFile() {
@@ -18,7 +16,8 @@ class JpegTest extends MediaWikiTestCase {
 		$h = new JpegHandler;
 		$res = $h->getMetadata( null, dirname( __FILE__ ) . '/test.jpg' );
 		$expected = 'a:7:{s:16:"ImageDescription";s:9:"Test file";s:11:"XResolution";s:4:"72/1";s:11:"YResolution";s:4:"72/1";s:14:"ResolutionUnit";i:2;s:16:"YCbCrPositioning";i:1;s:15:"JPEGFileComment";a:1:{i:0;s:17:"Created with GIMP";}s:22:"MEDIAWIKI_EXIF_VERSION";i:2;}';
-		// Hopefully php always serializes things in the same order.
-		$this->assertEquals( $expected, $res );
+
+		// Unserialize in case serialization format ever changes.
+		$this->assertEquals( unserialize( $expected ), unserialize( $res ) );
 	}
 }
