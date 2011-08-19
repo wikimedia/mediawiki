fa6715894f230bee238cf98a43469ce62b1785a4
Roan Kattouw <catrope@users.mediawiki.org>
2011-08-19 15:25:50 +0000

    Add a PROTO_CANONICAL mode to wfExpandUrl(), which uses $wgCanonicalServer

--- tests/phpunit/includes/GlobalFunctions/wfExpandUrlTest.php
+++ tests/phpunit/includes/GlobalFunctions/wfExpandUrlTest.php
@@ -5,11 +5,13 @@
 
 class wfExpandUrl extends MediaWikiTestCase {
 	/** @dataProvider provideExpandableUrls */
-	public function testWfExpandUrl( $fullUrl, $shortUrl, $defaultProto, $server, $httpsMode, $message ) {
-		// Fake $wgServer
-		global $wgServer;
+	public function testWfExpandUrl( $fullUrl, $shortUrl, $defaultProto, $server, $canServer, $httpsMode, $message ) {
+		// Fake $wgServer and $wgCanonicalServer
+		global $wgServer, $wgCanonicalServer;
 		$oldServer = $wgServer;
+		$oldCanServer = $wgCanonicalServer;
 		$wgServer = $server;
+		$wgCanonicalServer = $canServer;
 		
 		// Fake $_SERVER['HTTPS'] if needed
 		if ( $httpsMode ) {
@@ -20,8 +22,9 @@ class wfExpandUrl extends MediaWikiTestCase {
 		
 		$this->assertEquals( $fullUrl, wfExpandUrl( $shortUrl, $defaultProto ), $message );
 		
-		// Restore $wgServer
+		// Restore $wgServer and $wgCanonicalServer
 		$wgServer = $oldServer;
+		$wgCanonicalServer = $oldCanServer;
 	}
 
 	/**
@@ -32,32 +35,43 @@ class wfExpandUrl extends MediaWikiTestCase {
 	public function provideExpandableUrls() {
 		$modes = array( 'http', 'https' );
 		$servers = array( 'http' => 'http://example.com', 'https' => 'https://example.com', 'protocol-relative' => '//example.com' );
-		$defaultProtos = array( 'http' => PROTO_HTTP, 'https' => PROTO_HTTPS, 'protocol-relative' => PROTO_RELATIVE, 'current' => PROTO_CURRENT );
+		$defaultProtos = array( 'http' => PROTO_HTTP, 'https' => PROTO_HTTPS, 'protocol-relative' => PROTO_RELATIVE, 'current' => PROTO_CURRENT, 'canonical' => PROTO_CANONICAL );
 		
 		$retval = array();
 		foreach ( $modes as $mode ) {
 			$httpsMode = $mode == 'https';
 			foreach ( $servers as $serverDesc => $server ) {
-				foreach ( $defaultProtos as $protoDesc => $defaultProto ) {
-					$retval[] = array( 'http://example.com', 'http://example.com', $defaultProto, $server, $httpsMode, "Testing fully qualified http URLs (no need to expand) (defaultProto: $protoDesc , wgServer: $server, current request protocol: $mode )" );
-					$retval[] = array( 'https://example.com', 'https://example.com', $defaultProto, $server, $httpsMode, "Testing fully qualified https URLs (no need to expand) (defaultProto: $protoDesc , wgServer: $server, current request protocol: $mode )" );
-					# Would be nice to support this, see fixme on wfExpandUrl()
-					$retval[] = array( "wiki/FooBar", 'wiki/FooBar', $defaultProto, $server, $httpsMode, "Test non-expandable relative URLs (defaultProto: $protoDesc , wgServer: $server, current request protocol: $mode )" );
-					
-					// Determine expected protocol
-					$p = $protoDesc . ':'; // default case
-					if ( $protoDesc == 'protocol-relative' ) {
-						$p = '';
-					} else if ( $protoDesc == 'current' ) {
-						$p = "$mode:";
-					} else {
-						$p = $protoDesc . ':';
+				foreach ( $modes as $canServerMode  ) {
+					$canServer = "$canServerMode://example2.com";
+					foreach ( $defaultProtos as $protoDesc => $defaultProto ) {
+						$retval[] = array( 'http://example.com', 'http://example.com', $defaultProto, $server, $canServer, $httpsMode, "Testing fully qualified http URLs (no need to expand) (defaultProto: $protoDesc , wgServer: $server, wgCanonicalServer: $canServer, current request protocol: $mode )" );
+						$retval[] = array( 'https://example.com', 'https://example.com', $defaultProto, $server, $canServer, $httpsMode, "Testing fully qualified https URLs (no need to expand) (defaultProto: $protoDesc , wgServer: $server, wgCanonicalServer: $canServer, current request protocol: $mode )" );
+						# Would be nice to support this, see fixme on wfExpandUrl()
+						$retval[] = array( "wiki/FooBar", 'wiki/FooBar', $defaultProto, $server, $canServer, $httpsMode, "Test non-expandable relative URLs (defaultProto: $protoDesc , wgServer: $server, wgCanonicalServer: $canServer, current request protocol: $mode )" );
+						
+						// Determine expected protocol
+						$p = $protoDesc . ':'; // default case
+						if ( $protoDesc == 'protocol-relative' ) {
+							$p = '';
+						} else if ( $protoDesc == 'current' ) {
+							$p = "$mode:";
+						} else if ( $protoDesc == 'canonical' ) {
+							$p = "$canServerMode:";
+						} else {
+							$p = $protoDesc . ':';
+						}
+						// Determine expected server name
+						if ( $protoDesc == 'canonical' ) {
+							$srv = $canServer;
+						} else if ( $serverDesc == 'protocol-relative' ) {
+							$srv = $p . $server;
+						} else {
+							$srv = $server;
+						}
+						
+						$retval[] = array( "$p//wikipedia.org", '//wikipedia.org', $defaultProto, $server, $canServer, $httpsMode, "Test protocol-relative URL (defaultProto: $protoDesc, wgServer: $server, wgCanonicalServer: $canServer, current request protocol: $mode )" );
+						$retval[] = array( "$srv/wiki/FooBar", '/wiki/FooBar', $defaultProto, $server, $canServer, $httpsMode, "Testing expanding URL beginning with / (defaultProto: $protoDesc , wgServer: $server, wgCanonicalServer: $canServer, current request protocol: $mode )" );
 					}
-					// Determine expected server name
-					$srv = $serverDesc == 'protocol-relative' ? $p . $server : $server;
-					
-					$retval[] = array( "$p//wikipedia.org", '//wikipedia.org', $defaultProto, $server, $httpsMode, "Test protocol-relative URL (defaultProto: $protoDesc, wgServer: $server, current request protocol: $mode )" );
-					$retval[] = array( "$srv/wiki/FooBar", '/wiki/FooBar', $defaultProto, $server, $httpsMode, "Testing expanding URL beginning with / (defaultProto: $protoDesc , wgServer: $server, current request protocol: $mode )" );
 				}
 			}
 		}
