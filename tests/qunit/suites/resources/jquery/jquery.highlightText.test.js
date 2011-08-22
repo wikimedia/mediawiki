81b60e315f0ef233d8e773cf89c77a8eb18c4ae9
Thomas Gries <wikinaut@users.mediawiki.org>
2011-08-22 22:15:43 +0000

    follow up to r94807 : more test cases incl. hebrew RTL and Japanese, the test cases 100 and 101 have been intentionally added with wrong expected result. It shows that the current qunit software may have problems with wrong  double closing span tags. test cases 100 and 101 fail *correctly* on internet explorer 8.0, but with Firefox 6.0 and Chrome no errors are detected --- false negative with Firefox and Chrome and maybe other browsers

--- tests/qunit/suites/resources/jquery/jquery.highlightText.test.js
+++ tests/qunit/suites/resources/jquery/jquery.highlightText.test.js
@@ -8,110 +8,154 @@ test( '-- Initial check', function() {
 test( 'Check', function() {
 	var cases = [
 		{
+		        desc: 'Test 001',
 			text: 'Blue Öyster Cult',
 			highlight: 'Blue',
 			expected: '<span class="highlight">Blue</span> Öyster Cult'
 		},
 		{
+		        desc: 'Test 002',
 			text: 'Blue Öyster Cult',
 			highlight: 'Blue ',
 			expected: '<span class="highlight">Blue</span> Öyster Cult'
 		},
 		{
+			desc: 'Test 003',
 			text: 'Blue Öyster Cult',
 			highlight: 'Blue Ö',
 			expected: '<span class="highlight">Blue</span> <span class="highlight">Ö</span>yster Cult'
 		},
 		{
+			desc: 'Test 004',
 			text: 'Blue Öyster Cult',
 			highlight: 'Blue Öy',
 			expected: '<span class="highlight">Blue</span> <span class="highlight">Öy</span>ster Cult'
 		},
 		{
+			desc: 'Test 005',
 			text: 'Blue Öyster Cult',
 			highlight: ' Blue',
 			expected: '<span class="highlight">Blue</span> Öyster Cult'
 		},
 		{
+			desc: 'Test 006',
 			text: 'Blue Öyster Cult',
 			highlight: ' Blue ',
 			expected: '<span class="highlight">Blue</span> Öyster Cult'
 		},
 		{
+			desc: 'Test 007',
 			text: 'Blue Öyster Cult',
 			highlight: ' Blue Ö',
 			expected: '<span class="highlight">Blue</span> <span class="highlight">Ö</span>yster Cult'
 		},
 		{
+			desc: 'Test 008',
 			text: 'Blue Öyster Cult',
 			highlight: ' Blue Öy',
 			expected: '<span class="highlight">Blue</span> <span class="highlight">Öy</span>ster Cult'
 		},
 		{
-			desc: 'Highlighter broken on starting Umlaut?',
+			desc: 'Test 009: Highlighter broken on starting Umlaut?',
 			text: 'Österreich',
 			highlight: 'Österreich',
 			expected: '<span class="highlight">Österreich</span>'
 		},
 		{
-			desc: 'Highlighter broken on starting Umlaut?',
+			desc: 'Test 010: Highlighter broken on starting Umlaut?',
 			text: 'Österreich',
 			highlight: 'Ö',
-			expected: '<span class="highlight">Ö</span>sterreich</span>'
+			expected: '<span class="highlight">Ö</span>sterreich'
 		},
 		{
-			desc: 'Highlighter broken on starting Umlaut?',
+			desc: 'Test 011: Highlighter broken on starting Umlaut?',
 			text: 'Österreich',
 			highlight: 'Öst',
 			expected: '<span class="highlight">Öst</span>erreich'
 		},
 		{
-			desc: 'Highlighter broken on starting Umlaut?',
+			desc: 'Test 012: Highlighter broken on starting Umlaut?',
 			text: 'Österreich',
 			highlight: 'Oe',
 			expected: 'Österreich'
 		},
 		{
-			desc: 'Highlighter broken on punctuation mark?',
+			desc: 'Test 013: Highlighter broken on punctuation mark?',
 			text: 'So good. To be there',
 			highlight: 'good',
 			expected: 'So <span class="highlight">good</span>. To be there'
 		},
 		{
-			desc: 'Highlighter broken on space?',
+			desc: 'Test 014: Highlighter broken on space?',
 			text: 'So good. To be there',
 			highlight: 'be',
 			expected: 'So good. To <span class="highlight">be</span> there'
 		},
 		{
-			desc: 'Highlighter broken on space?',
+			desc: 'Test 015: Highlighter broken on space?',
 			text: 'So good. To be there',
 			highlight: ' be',
 			expected: 'So good. To <span class="highlight">be</span> there'
 		},
 		{
-			desc: 'Highlighter broken on space?',
+			desc: 'Test 016: Highlighter broken on space?',
 			text: 'So good. To be there',
 			highlight: 'be ',
 			expected: 'So good. To <span class="highlight">be</span> there'
 		},
 		{
-			desc: 'Highlighter broken on space?',
+			desc: 'Test 017: Highlighter broken on space?',
 			text: 'So good. To be there',
 			highlight: ' be ',
 			expected: 'So good. To <span class="highlight">be</span> there'
 		},
 		{
-			desc: 'Highlighter broken on special character at the end?',
+			desc: 'Test 018: Highlighter broken on special character at the end?',
 			text: 'So good. xbß',
 			highlight: 'xbß',
 			expected: 'So good. <span class="highlight">xbß</span>'
 		},
 		{
-			desc: 'Highlighter broken on special character at the end?',
+			desc: 'Test 019: Highlighter broken on special character at the end?',
 			text: 'So good. xbß.',
 			highlight: 'xbß.',
 			expected: 'So good. <span class="highlight">xbß.</span>'
+		},
+		{
+			desc: 'Test 020: Hebrew',
+			text: 'חסיד אומות העולם',
+			highlight: 'חסיד אומות העולם',
+			expected: '<span class="highlight">חסיד</span> <span class="highlight">אומות</span> <span class="highlight">העולם</span>'
+		},
+		{
+			desc: 'Test 021: Hebrew',
+			text: 'חסיד אומות העולם',
+			highlight: 'חסי',
+			expected: '<span class="highlight">חסי</span>ד אומות העולם'
+		},
+		{
+			desc: 'Test 022: Japanese',
+			text: '諸国民の中の正義の人',
+			highlight: '諸国民の中の正義の人',
+			expected: '<span class="highlight">諸国民の中の正義の人</span>'
+		},
+		{
+			desc: 'Test 023: Japanese',
+			text: '諸国民の中の正義の人',
+			highlight: '諸国',
+			expected: '<span class="highlight">諸国</span>民の中の正義の人'
+		},
+		{
+			desc: 'Test 100: TESTCASE FOR QUNIT FAILS: a wrong expected string was not detected in Firefox 6.0 and Chrome',
+			text: 'Österreich',
+			highlight: 'Ö',
+			expected: '<span class="highlight">Ö</span>sterreich</span this wrong tag is not detected as test failure in Firefox 6.0 or Chrome>'
+		},
+		{
+			desc: 'Test 101: TESTCASE FOR QUNIT FAILS: a wrong expected string was not detected in Firefox 6.0 or Chrome',
+			text: 'Oesterreich',
+			highlight: 'Oe',
+			expected: '<span class="highlight">Oe</span>sterreich</span this wrong tag is not detected as test failure in Firefox 6.0 or Chrome>'
 		}
 	];
 	expect(cases.length);
