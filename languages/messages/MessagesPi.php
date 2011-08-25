7db4f4db2730114b567b2b64dc68adda4351213a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-08-25 10:33:27 +0000

    MFT r95471: (bug 30504) Core namespaces for Pali (pi).

--- languages/messages/MessagesPi.php
+++ languages/messages/MessagesPi.php
@@ -10,6 +10,25 @@
  * @author Vibhijain
  */
 
+$namespaceNames = array(
+	NS_MEDIA            => 'मीडिया',
+	NS_SPECIAL          => 'विसेस',
+	NS_TALK             => 'सम्भासित',
+	NS_USER             => 'अवयव',
+	NS_USER_TALK        => 'अवयव_सम्भासित',
+	NS_PROJECT_TALK     => '$1_सम्भासित',
+	NS_FILE             => 'पटिमा',
+	NS_FILE_TALK        => 'पटिमा_सम्भासित',
+	NS_MEDIAWIKI        => 'मीडियाविकि',
+	NS_MEDIAWIKI_TALK   => 'मीडियाविकि_सम्भासित',
+	NS_TEMPLATE         => 'पटिरूप',
+	NS_TEMPLATE_TALK    => 'पटिरूप_सम्भासित',
+	NS_HELP             => 'अवस्सय',
+	NS_HELP_TALK        => 'अवस्सय_सम्भासित',
+	NS_CATEGORY         => 'विभाग',
+	NS_CATEGORY_TALK    => 'विभाग_सम्भासित',
+);
+
 $digitTransformTable = array(
 	'0' => '०', # &#x0966;
 	'1' => '१', # &#x0967;
