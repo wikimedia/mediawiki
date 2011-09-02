93f08dd483a78be0fc10e36b2b560e55f714f403
Jure Kajzer <freakolowsky@users.mediawiki.org>
2011-09-02 11:26:54 +0000

    * MFT of 1.18 relevant part of r95986

--- includes/installer/OracleUpdater.php
+++ includes/installer/OracleUpdater.php
@@ -30,10 +30,12 @@ class OracleUpdater extends DatabaseUpdater {
 			array( 'doSchemaUpgrade17' ),
 			array( 'doInsertPage0' ),
 			array( 'doRemoveNotNullEmptyDefaults' ),
+			array( 'addTable', 'user_former_groups', 'patch-user_former_groups.sql' ),
 			
 			//1.18
 			array( 'addIndex',	'user',          'i02',       'patch-user_email_index.sql' ),
 			array( 'modifyField', 'user_properties', 'up_property', 'patch-up_property.sql' ),
+			array( 'addTable', 'uploadstash', 'patch-uploadstash.sql' ),
 
 			// till 2.0 i guess
 			array( 'doRebuildDuplicateFunction' ),
--- maintenance/oracle/archives/patch-user_email_index.sql
+++ maintenance/oracle/archives/patch-user_email_index.sql
@@ -1,4 +1,4 @@
 define mw_prefix='{$wgDBprefix}';
 
-CREATE INDEX &mw_prefix.mwuser_i02 ON &mw_prefix.mwuser (user_email, user_name);
+CREATE INDEX &mw_prefix.mwuser_i02 ON &mw_prefix.mwuser (user_email);
 
