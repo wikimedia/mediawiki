b2a4eb77abe8ad962c59ead5b6d4ceb7ca858c9c
Robin Pepermans <robin@users.mediawiki.org>
2011-09-04 14:07:51 +0000

    Followup r87677 / r87676: add 'bh' to dummy language codes (and also, Bihari is not a macrolanguage but a group of languages, per ISO 639-3 terminology)

--- languages/messages/MessagesBh.php
+++ languages/messages/MessagesBh.php
@@ -1,5 +1,5 @@
 <?php
-/** Bihari (macro language)
+/** Bihari (language group)
  *
  * See MessagesQqq.php for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
