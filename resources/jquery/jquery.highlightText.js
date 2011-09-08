wikimedia/mediawiki

commit dae07d5c3a76d507d4331cf79288862ed0956635

fix for bug29371 . regex wordwrap with UTF8: do not use \b metacharacter. The problem is that JavaScript recognizes word boundaries only before/after ASCII letters (and numbers/underscore)
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@90092 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

resources/jquery/jquery.highlightText.js
@@ -21,9 +21,10 @@ $.highlightText = {
 		// if this is a text node
 		if ( node.nodeType == 3 ) {
 			// TODO - need to be smarter about the character matching here. 
-			// non latin characters can make regex think a new word has begun. 
-			// look for an occurence of our pattern and store the starting position 
-			var pos = node.data.search( new RegExp( "\\b" + $.escapeRE( pat ), "i" ) );
+			// non latin characters can make regex think a new word has begun: do not use \b
+			// http://stackoverflow.com/questions/3787072/regex-wordwrap-with-utf8-characters-in-js
+			// look for an occurence of our pattern and store the starting position
+			var pos = node.data.search( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
 			if ( pos >= 0 ) {
 				// create the span wrapper for the matched text
 				var spannode = document.createElement( 'span' );

resources/jquery/jquery.autoEllipsis.js
@@ -5,7 +5,7 @@
 
 // Cache ellipsed substrings for every string-width-position combination
 var cache = { };
-// Use a seperate cache when match highlighting is enabled
+// Use a separate cache when match highlighting is enabled
 var matchTextCache = { };
 
 $.fn.autoEllipsis = function( options ) {

resources/jquery/jquery.suggestions.js
@@ -229,7 +229,7 @@ $.suggestions = {
 				} else {
 					result = selected.prev();
 					if ( selected.length == 0 ) {
-						// we are at the begginning, so lets jump to the last item
+						// we are at the beginning, so lets jump to the last item
 						if ( context.data.$container.find( '.suggestions-special' ).html() != "" ) {
 							result = context.data.$container.find( '.suggestions-special' );
 						} else {

commit 25c0050d6c03b39f0a22b57202705fa02f766352

MFT r94702: fix for a jquery.highlightText regression in r90092

resources/jquery/jquery.highlightText.js
@@ -24,8 +24,9 @@ $.highlightText = {
 			// non latin characters can make regex think a new word has begun: do not use \b
 			// http://stackoverflow.com/questions/3787072/regex-wordwrap-with-utf8-characters-in-js
 			// look for an occurence of our pattern and store the starting position
-			var pos = node.data.search( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
-			if ( pos >= 0 ) {
+			var match = node.data.match( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
+			if ( match ) {
+				var pos = match.index + match[1].length; // include length of any matched spaces
 				// create the span wrapper for the matched text
 				var spannode = document.createElement( 'span' );
 				spannode.className = 'highlight';