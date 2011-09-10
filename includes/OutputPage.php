307657888f0bd5e86bc6a39ff41985ed74faa5e6
Robin Pepermans <robin@users.mediawiki.org>
2011-09-10 23:51:08 +0000

    * Per Aaron on r96261, use string 'flip' instead of boolean * Apparently @noflip should only be set once, otherwise it behaves differently

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -2955,10 +2955,10 @@ $distantTemplates
 	/**
 	 * Adds inline CSS styles
 	 * @param $style_css Mixed: inline CSS
-	 * @param $flip Boolean: Whether to flip the CSS if needed
+	 * @param $flip False or String: Set to 'flip' to flip the CSS if needed
 	 */
 	public function addInlineStyle( $style_css, $flip = false ) {
-		if( $flip && $this->getLang()->isRTL() ) {
+		if( $flip === 'flip' && $this->getLang()->isRTL() ) {
 			# If wanted, and the interface is right-to-left, flip the CSS
 			$style_css = CSSJanus::transform( $style_css, true, false );
 		}
--- skins/Simple.php
+++ skins/Simple.php
@@ -44,7 +44,7 @@ class SkinSimple extends SkinTemplate {
 			$rules[] = "a.stub:after { $underline; }";
 		}
 		$style = implode( "\n", $rules );
-		$out->addInlineStyle( $style, /* flip css if RTL */true );
+		$out->addInlineStyle( $style, 'flip' );
 
 	}
 }
