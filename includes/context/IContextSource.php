43cbeb90623217b726b2195179699b4107ce9fb0
Daniel Friesen <dantman@users.mediawiki.org>
2011-09-15 17:42:17 +0000

    Separate RequestContext.php into separate files inside of context/ Update the AutoLoader too. This is also a follow up to r97161 since I forgot to add the AutoLoader line.

---
+++ includes/context/IContextSource.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Request-dependant objects containers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.18
+ *
+ * @author Happy-melon
+ * @file
+ */
+
+/**
+ * Interface for objects which can provide a context on request.
+ */
+interface IContextSource {
+
+	/**
+	 * Get the WebRequest object
+	 *
+	 * @return WebRequest
+	 */
+	public function getRequest();
+
+	/**
+	 * Get the Title object
+	 *
+	 * @return Title
+	 */
+	public function getTitle();
+
+	/**
+	 * Get the OutputPage object
+	 *
+	 * @return OutputPage object
+	 */
+	public function getOutput();
+
+	/**
+	 * Get the User object
+	 *
+	 * @return User
+	 */
+	public function getUser();
+
+	/**
+	 * Get the Language object
+	 *
+	 * @return Language
+	 */
+	public function getLang();
+
+	/**
+	 * Get the Skin object
+	 *
+	 * @return Skin
+	 */
+	public function getSkin();
+}
+
