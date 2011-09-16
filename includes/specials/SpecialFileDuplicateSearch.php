cff13bc144e9a52e6287704e55d96fabbd17c91b
Sam Reed <reedy@users.mediawiki.org>
2011-09-16 14:12:52 +0000

    REL1_18 MFT r96406

--- includes/specials/SpecialFileDuplicateSearch.php
+++ includes/specials/SpecialFileDuplicateSearch.php
@@ -131,7 +131,7 @@ class FileDuplicateSearchPage extends QueryPage {
 			if ( $img ) {
 				$thumb = $img->transform( array( 'width' => 120, 'height' => 120 ) );
 				if( $thumb ) {
-					$wgOut->addHTML( '<div class="mw-float-end" id="mw-fileduplicatesearch-icon">' .
+					$wgOut->addHTML( '<div id="mw-fileduplicatesearch-icon">' .
 						$thumb->toHtml( array( 'desc-link' => false ) ) . '<br />' .
 						wfMsgExt( 'fileduplicatesearch-info', array( 'parse' ),
 							$wgLang->formatNum( $img->getWidth() ),
