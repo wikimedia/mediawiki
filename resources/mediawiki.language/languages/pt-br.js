d10c74ab08a6e1f651f662bddcaa42da867cd37b
Santhosh Thottingal <santhosh@users.mediawiki.org>
2011-09-26 05:23:50 +0000

    Add digitTransformTable for pt, pt-br and nl

--- resources/mediawiki.language/languages/pt-br.js
+++ resources/mediawiki.language/languages/pt-br.js
@@ -6,3 +6,8 @@ mediaWiki.language.convertPlural = function( count, forms ) {
 	forms = mediaWiki.language.preConvertPlural( forms, 2 );
 	return ( count <= 1 ) ? forms[0] : forms[1];
 };
+
+mediaWiki.language.digitTransformTable = {
+    '.' : ',',
+    ',' : ' '
+};
