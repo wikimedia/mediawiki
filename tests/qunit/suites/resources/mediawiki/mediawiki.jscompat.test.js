2823ab5a7845e737861adb34070d14c7420ef832
Brion Vibber <brion@users.mediawiki.org>
2011-09-30 22:16:37 +0000

    QUnit test case to confirm consistent browser behavior with initial newlines inside <textarea> HTML source. (for bug 12130)

--- tests/qunit/suites/resources/mediawiki/mediawiki.jscompat.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.jscompat.test.js
@@ -33,3 +33,30 @@ test( 'Keyword workaround: "if" as member variable name using Unicode escapes', 
 	deepEqual( foo.\u0069\u0066, orig, 'foo.\\u0069\\u0066' );
 });
 */
+
+test( 'Stripping of single initial newline from textarea\'s literal contents (bug 12130)', function() {
+	var maxn = 4;
+	expect(maxn * 2);
+
+	var repeat = function(str, n) {
+		if (n <= 0) {
+			return '';
+		} else {
+			var out = Array(n);
+			for (var i = 0; i < n; i++) {
+				out[i] = str;
+			}
+			return out.join('');
+		}
+	};
+
+	for (var n = 0; n < maxn; n++) {
+		var expected = repeat('\n', n) + 'some text';
+
+		var $textarea = $('<textarea>\n' + expected + '</textarea>');
+		equal($textarea.val(), expected, 'Expecting ' + n + ' newlines (HTML contained ' + (n + 1) + ')');
+
+		var $textarea2 = $('<textarea>').val(expected);
+		equal($textarea2.val(), expected, 'Expecting ' + n + ' newlines (from DOM set with ' + n + ')');
+	}
+});
