c4f708556da1288f4d6676f4fab2c4ef1d52fcf1
Santhosh Thottingal <santhosh@users.mediawiki.org>
2011-10-01 18:42:36 +0000

    Fix Bug 29299 - Zero should be considered plural for pt-br

--- resources/mediawiki.language/languages/pt-br.js
+++ resources/mediawiki.language/languages/pt-br.js
@@ -4,7 +4,7 @@
 
 mediaWiki.language.convertPlural = function( count, forms ) {
 	forms = mediaWiki.language.preConvertPlural( forms, 2 );
-	return ( count <= 1 ) ? forms[0] : forms[1];
+	return ( count == 1 ) ? forms[0] : forms[1];
 };
 
 mediaWiki.language.digitTransformTable = {
