21c5b5d82965af03f976c970c1e3055d7f266c12
Santhosh Thottingal <santhosh@users.mediawiki.org>
2011-10-01 22:34:54 +0000

    mediaWiki.language.convertPlural not required since it is same as the default implimentation in mediawiki.language.js

--- resources/mediawiki.language/languages/pt-br.js
+++ resources/mediawiki.language/languages/pt-br.js
@@ -2,11 +2,6 @@
  * Brazilian Portugese (Portuguêsi do Brasil) language functions
  */
 
-mediaWiki.language.convertPlural = function( count, forms ) {
-	forms = mediaWiki.language.preConvertPlural( forms, 2 );
-	return ( count == 1 ) ? forms[0] : forms[1];
-};
-
 mediaWiki.language.digitTransformTable = {
     '.' : ',',
     ',' : ' '
