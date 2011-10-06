157a66c5ce8d48e552ab614618c3a17ea58e2186
Philip Tzou <philip@users.mediawiki.org>
2011-10-06 12:07:28 +0000

    Partly revert r99083 because the new behaviour is still buggy...

--- skins/vector/vector.js
+++ skins/vector/vector.js
@@ -2,35 +2,20 @@
  * Vector-specific scripts
  */
 jQuery( function( $ ) {
-
-	// For accessibility, show the menu whe 
-	// the hidden link in the menu is focused (bug 24298)
 	$( 'div.vectorMenu' ).each( function() {
 		var self = this;
-		var focused = false;
 		$( 'h5:first a:first', this )
+			// For accessibility, show the menu when the hidden link in the menu is clicked (bug 24298)
 			.click( function( e ) {
+				$( '.menu:first', self ).toggleClass( 'menuForceShow' );
 				e.preventDefault();
-			} )
-			// Blur the link if it was focused before the click
-			.mousedown( function( e ) {
-				focused = $( this ).is( ':focus' );
-			} )
-			.mouseup( function( e ) {
-				if ( focused ) {
-					$( this ).blur();
-				}
-				e.preventDefault();
-			} )
-			// When the hidden link has focus, show the menu
-			// and set a class that will change the arrow icon
+			})
+			// When the hidden link has focus, also set a class that will change the arrow icon
 			.focus( function() {
-				$( '.menu:first', self ).addClass( 'menuForceShow' );
 				$( self ).addClass( 'vectorMenuFocus' );
-			} )
+			})
 			.blur( function() {
-				$( '.menu:first', self ).removeClass( 'menuForceShow' );
 				$( self ).removeClass( 'vectorMenuFocus' );
-			} );
-	} );
+			});
+	});
 });
