b5e2ac1d255c6b6258ec94bfedd427410092a16d
Brion Vibber <brion@users.mediawiki.org>
2011-10-06 01:13:33 +0000

    MFT r99067: fix bug 31400, followup to r87997.
    Fixes use of Special:MyPage in importScript() on IE 9

--- includes/SpecialPage.php
+++ includes/SpecialPage.php
@@ -1018,7 +1018,9 @@ class SpecialMypage extends RedirectSpecialPage {
 	function __construct() {
 		parent::__construct( 'Mypage' );
 		$this->mAllowedRedirectParams = array( 'action' , 'preload' , 'editintro',
-			'section', 'oldid', 'diff', 'dir' );
+			'section', 'oldid', 'diff', 'dir',
+			// Options for action=raw; missing ctype can break JS or CSS in some browsers
+			'ctype', 'maxage', 'smaxage' );
 	}
 
 	function getRedirect( $subpage ) {
