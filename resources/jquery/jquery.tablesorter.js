08d90ea00286f317d25b98d12a352032a506a64a
Roan Kattouw <catrope@users.mediawiki.org>
2011-10-06 12:52:26 +0000

    (bug 31420) Fix weird tablesorter bug where headers spanning multiple rows would get screwed up. Thanks to TheDJ for essentially telling me exactly how to fix this, he was spot on.

--- resources/jquery/jquery.tablesorter.js
+++ resources/jquery/jquery.tablesorter.js
@@ -442,7 +442,7 @@
 
 	function explodeRowspans( $table ) {
 		// Split multi row cells into multiple cells with the same content
-		$table.find( '[rowspan]' ).each(function() {
+		$table.find( 'tbody [rowspan]' ).each(function() {
 			var rowSpan = this.rowSpan;
 			this.rowSpan = 1;
 			var cell = $( this );
