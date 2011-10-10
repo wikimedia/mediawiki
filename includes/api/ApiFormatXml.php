wikimedia/mediawiki

commit 7fa25b7e8fe6099a25337254209dc747d7f2d780

(bug 24781) Define XML namespace for API output. Also created the referenced URL on the cluster.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88007 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/api/ApiFormatXml.php
@@ -36,6 +36,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 class ApiFormatXml extends ApiFormatBase {
 
 	private $mRootElemName = 'api';
+	public static $namespace = 'http://www.mediawiki.org/xml/api/';
 	private $mDoubleQuote = false;
 	private $mXslt = null;
 
@@ -66,7 +67,7 @@ class ApiFormatXml extends ApiFormatBase {
 		}
 		$this->printText(
 			self::recXmlPrint( $this->mRootElemName,
-				$this->getResultData(),
+				array( 'xmlns' => self::$namespace ) + $this->getResultData(),
 				$this->getIsHtml() ? - 2 : null,
 				$this->mDoubleQuote
 			)

commit 125d66a877a08ca1e34adaffe158c66c8afeedbe

MFT r99135 -- partially undoes breakage from r88007
Per bug 31542 this got merged to 1.18wmf1 but not REL1_18.

RELEASE-NOTES-1.18
@@ -600,6 +600,8 @@ production.
 * (bug 28817) Add reference help page link to API Modules
 * (bug 29935) Improve formatting of examples in ApiParamInfo
 * (bug 29938) list=users&usprop=rights shows rights the user doesn't have
+* (bug 24781) The API will include an XML namespace if the includexmlnamespace
+  parameter is set.
 
 === Languages updated in 1.18 ===
 

includes/api/ApiFormatXml.php
@@ -38,6 +38,7 @@ class ApiFormatXml extends ApiFormatBase {
 	private $mRootElemName = 'api';
 	public static $namespace = 'http://www.mediawiki.org/xml/api/';
 	private $mDoubleQuote = false;
+	private $mIncludeNamespace = false;
 	private $mXslt = null;
 
 	public function __construct( $main, $format ) {
@@ -59,15 +60,22 @@ class ApiFormatXml extends ApiFormatBase {
 	public function execute() {
 		$params = $this->extractRequestParams();
 		$this->mDoubleQuote = $params['xmldoublequote'];
+		$this->mIncludeNamespace = $params['includexmlnamespace'];
 		$this->mXslt = $params['xslt'];
 
 		$this->printText( '<?xml version="1.0"?>' );
 		if ( !is_null( $this->mXslt ) ) {
 			$this->addXslt();
 		}
+		if ( $this->mIncludeNamespace ) {
+			$data = array( 'xmlns' => self::$namespace ) + $this->getResultData();
+		} else {
+			$data = $this->getResultData();
+		}
+		
 		$this->printText(
 			self::recXmlPrint( $this->mRootElemName,
-				array( 'xmlns' => self::$namespace ) + $this->getResultData(),
+				$data,
 				$this->getIsHtml() ? - 2 : null,
 				$this->mDoubleQuote
 			)
@@ -201,6 +209,7 @@ class ApiFormatXml extends ApiFormatBase {
 		return array(
 			'xmldoublequote' => false,
 			'xslt' => null,
+			'includexmlnamespace' => false,
 		);
 	}
 
@@ -208,6 +217,7 @@ class ApiFormatXml extends ApiFormatBase {
 		return array(
 			'xmldoublequote' => 'If specified, double quotes all attributes and content',
 			'xslt' => 'If specified, adds <xslt> as stylesheet',
+			'includexmlnamespace' => 'If specified, adds an XML namespace'
 		);
 	}
 