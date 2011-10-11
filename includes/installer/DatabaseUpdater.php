8dbcf5210457c9d8bdf36111d94ed7efe637967d
Thomas Gries <wikinaut@users.mediawiki.org>
2011-10-11 18:33:35 +0000

    added 'key' in 'Dropping .. key' in dropIndex which was missing there

--- includes/installer/DatabaseUpdater.php
+++ includes/installer/DatabaseUpdater.php
@@ -470,7 +470,7 @@ abstract class DatabaseUpdater {
 	 */
 	protected function dropIndex( $table, $index, $patch, $fullpath = false ) {
 		if ( $this->db->indexExists( $table, $index ) ) {
-			$this->output( "Dropping $index from table $table... " );
+			$this->output( "Dropping $index key from table $table... " );
 			$this->applyPatch( $patch, $fullpath );
 			$this->output( "ok\n" );
 		} else {
