db1010a3f08197ada1ca0bb05f96566612df9b3b
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2011-10-23 07:55:45 +0000

    Check first that $wgDisableLangConversion is false and we are not in a special page before doing the language variant check

--- includes/SkinTemplate.php
+++ includes/SkinTemplate.php
@@ -1011,32 +1011,32 @@ class SkinTemplate extends Skin {
 				array( &$this, &$content_navigation ) );
 		}
 
-		$pageLang = $title->getPageLanguage();
-
-		// Gets list of language variants
-		$variants = $pageLang->getVariants();
-		// Checks that language conversion is enabled and variants exist
-		// And if it is not in the special namespace
-		if( !$wgDisableLangConversion && count( $variants ) > 1
-			&& $title->getNamespace() != NS_SPECIAL ) {
-			// Gets preferred variant (note that user preference is 
-			// only possible for wiki content language variant)
-			$preferred = $pageLang->getPreferredVariant();
-			// Loops over each variant
-			foreach( $variants as $code ) {
-				// Gets variant name from language code
-				$varname = $pageLang->getVariantname( $code );
-				// Checks if the variant is marked as disabled
-				if( $varname == 'disable' ) {
-					// Skips this variant
-					continue;
+		if ( !$wgDisableLangConversion && $title->getNamespace() != NS_SPECIAL ) {
+			$pageLang = $title->getPageLanguage();
+			// Gets list of language variants
+			$variants = $pageLang->getVariants();
+			// Checks that language conversion is enabled and variants exist
+			// And if it is not in the special namespace
+			if( count( $variants ) > 1 ) {
+				// Gets preferred variant (note that user preference is 
+				// only possible for wiki content language variant)
+				$preferred = $pageLang->getPreferredVariant();
+				// Loops over each variant
+				foreach( $variants as $code ) {
+					// Gets variant name from language code
+					$varname = $pageLang->getVariantname( $code );
+					// Checks if the variant is marked as disabled
+					if( $varname == 'disable' ) {
+						// Skips this variant
+						continue;
+					}
+					// Appends variant link
+					$content_navigation['variants'][] = array(
+						'class' => ( $code == $preferred ) ? 'selected' : false,
+						'text' => $varname,
+						'href' => $title->getLocalURL( '', $code )
+					);
 				}
-				// Appends variant link
-				$content_navigation['variants'][] = array(
-					'class' => ( $code == $preferred ) ? 'selected' : false,
-					'text' => $varname,
-					'href' => $title->getLocalURL( '', $code )
-				);
 			}
 		}
 
