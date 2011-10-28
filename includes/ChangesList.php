c93dfd61fb97be07d6bde2568ff250e880cbd082
Amir E. Aharoni <amire80@users.mediawiki.org>
2011-10-28 08:04:58 +0000

    Followup to r101120, replaced hardcoded parentheses with the 'parentheses' message.

--- includes/ChangesList.php
+++ includes/ChangesList.php
@@ -218,7 +218,7 @@ class ChangesList extends ContextSource {
 
 		return Html::element( $tag,
 			array( 'dir' => 'ltr', 'class' => $formattedSizeClass ),
-			"($formattedSize)" ) . $wgLang->getDirMark();
+			wfMessage( 'parentheses', $formattedSize )->plain() ) . $wgLang->getDirMark();
 	}
 
 	/**
