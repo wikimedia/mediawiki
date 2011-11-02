06de2ecc9683fa9dc4485b1941453bee6e255067
Mark A. Hershberger <mah@users.mediawiki.org>
2011-11-02 20:15:28 +0000

    avoid E_STRICT warnings when no --installdbpass is given

--- includes/installer/CliInstaller.php
+++ includes/installer/CliInstaller.php
@@ -82,7 +82,7 @@ class CliInstaller extends Installer {
 			$this->setVar( '_InstallUser',
 				$option['installdbuser'] );
 			$this->setVar( '_InstallPassword',
-				$option['installdbpass'] );
+				isset( $option['installdbpass'] ) ? $option['installdbpass'] : "" );
 
 			// Assume that if we're given the installer user, we'll create the account.
 			$this->setVar( '_CreateDBAccount', true );
