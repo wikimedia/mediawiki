31a08811bf9a09e84b0379e338977608543467e2
Sam Reed <reedy@users.mediawiki.org>
2011-11-03 22:32:49 +0000

    Swap from 1.18alpha version to 1.18.0beta1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.18alpha';
+$wgVersion = '1.180beta1';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
