df91f627a8415fed9abf7003195c87d2aacc4fa8
Daniel Friesen <dantman@users.mediawiki.org>
2011-11-06 23:49:16 +0000

    Allow for installation of php in ~/.mwphp and don't die if maintenance/dev/data already existsy

--- maintenance/dev/install.sh
+++ maintenance/dev/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "x$BASH_SOURCE" == "x" ]]; then echo '$BASH_SOURCE not set'; exit 1; fi
+if [ "x$BASH_SOURCE" == "x" ]; then echo '$BASH_SOURCE not set'; exit 1; fi
 DEV=$(cd -P "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 "$DEV/installphp.sh"
