1a8557b19b4f4b5fa331b1c3fa677298c833292e
Sam Reed <reedy@users.mediawiki.org>
2011-11-11 00:05:04 +0000

    * (bug 30062) Add $wgDBprefix option to cli installer
    Patch by Grunny

--- maintenance/install.php
+++ maintenance/install.php
@@ -51,6 +51,7 @@ class CommandLineInstaller extends Maintenance {
 		$this->addOption( 'dbport', 'The database port; only for PostgreSQL (5432)', false, true );
 		$this->addOption( 'dbname', 'The database name (my_wiki)', false, true );
 		$this->addOption( 'dbpath', 'The path for the SQLite DB (/var/data)', false, true );
+		$this->addOption( 'dbprefix', 'Optional database table name prefix', false, true );
 		$this->addOption( 'installdbuser', 'The user to use for installing (root)', false, true );
 		$this->addOption( 'installdbpass', 'The pasword for the DB user to install as.', false, true );
 		$this->addOption( 'dbuser', 'The user to use for normal operations (wikiuser)', false, true );
