56508fdca683cea881f5524ef5e3efbd559cb0e8
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2011-11-13 07:25:56 +0000

    Removed usage of $wgUser in block and unblock processing: * Made HTMLFormField pass the HTMLForm object to the validation and filter callbacks (so that they can get a context) * Added new parameter to SpecialBlock::checkUnblockSelf() to pass the user doing the request * SpecialBlock::processForm() and SpecialUnblock::processUnblock() now require a context as second parameter; added SpecialBlock::processUIForm() and SpecialUnblock::processUIUnblock() as adaptators from HTMLForm as second parameter to context

--- includes/specials/SpecialUnblock.php
+++ includes/specials/SpecialUnblock.php
@@ -58,7 +58,7 @@ class SpecialUnblock extends SpecialPage {
 
 		$form = new HTMLForm( $this->getFields(), $this->getContext() );
 		$form->setWrapperLegend( wfMsg( 'unblockip' ) );
-		$form->setSubmitCallback( array( __CLASS__, 'processUnblock' ) );
+		$form->setSubmitCallback( array( __CLASS__, 'processUIUnblock' ) );
 		$form->setSubmitText( wfMsg( 'ipusubmit' ) );
 		$form->addPreText( wfMsgExt( 'unblockiptext', 'parse' ) );
 
@@ -143,12 +143,21 @@ class SpecialUnblock extends SpecialPage {
 	}
 
 	/**
+	 * Submit callback for an HTMLForm object
+	 */
+	public static function processUIUnblock( array $data, HTMLForm $form ) {
+		return self::processUnblock( $data, $form->getContext() );
+	}
+
+	/**
 	 * Process the form
+	 *
+	 * @param $data Array
+	 * @param $context IContextSource
 	 * @return Array( Array(message key, parameters) ) on failure, True on success
 	 */
-	public static function processUnblock( array $data ){
-		global $wgUser;
-
+	public static function processUnblock( array $data, IContextSource $context ){
+		$performer = $context->getUser();
 		$target = $data['Target'];
 		$block = Block::newFromTarget( $data['Target'] );
 
@@ -159,7 +168,7 @@ class SpecialUnblock extends SpecialPage {
 		# bug 15810: blocked admins should have limited access here.  This
 		# won't allow sysops to remove autoblocks on themselves, but they
 		# should have ipblock-exempt anyway
-		$status = SpecialBlock::checkUnblockSelf( $target );
+		$status = SpecialBlock::checkUnblockSelf( $target, $performer );
 		if ( $status !== true ) {
 			throw new ErrorPageError( 'badaccess', $status );
 		}
@@ -174,7 +183,7 @@ class SpecialUnblock extends SpecialPage {
 
 		# If the name was hidden and the blocking user cannot hide
 		# names, then don't allow any block removals...
-		if( !$wgUser->isAllowed( 'hideuser' ) && $block->mHideName ) {
+		if( !$performer->isAllowed( 'hideuser' ) && $block->mHideName ) {
 			return array( 'unblock-hideuser' );
 		}
 
