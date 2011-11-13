7d194e92783d6aae2c10cd545b52b575ffc0b33f
Santhosh Thottingal <santhosh@users.mediawiki.org>
2011-11-13 12:22:10 +0000

    Correct the name of Sanskrit Language. Bug 32393

--- languages/Names.php
+++ languages/Names.php
@@ -1,4 +1,3 @@
-<?php
 /**
   * These determine things like interwikis, language selectors, and so on.
   * Safe to change without running scripts on the respective sites.
@@ -290,7 +289,7 @@
 	# 'ruq-grek' => 'Βλαεστε',	# Megleno-Romanian (Greek script)
 	'ruq-latn' => 'Vlăheşte',	# Megleno-Romanian (Latin script)
 	'rw' => 'Kinyarwanda',	# Kinyarwanda, should possibly be Kinyarwandi
-	'sa' => 'संस्कृत',	# Sanskrit
+	'sa' => 'संस्कृतम्',	# Sanskrit
 	'sah' => 'Саха тыла', # Sakha
 	'sc' => 'Sardu',		# Sardinian
 	'scn' => 'Sicilianu',	# Sicilian
