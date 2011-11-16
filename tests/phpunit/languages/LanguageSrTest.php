2483246e564a6373e5e641fc3ce61479080e1f5e
Antoine Musso <hashar@users.mediawiki.org>
2011-11-16 15:12:00 +0000

    bug 28643 improvement to serbian variants conversion
    This patch is a PARTIAL merge of /branches/nikola/phase3 ::
    
    r85224 avoid double conversion when text already use the correct variant
    r85239 minor fixes to previous
    r85308 documentation (@since 1.18 update to 1.19)
    r101359 guessVariant doc + boolean typecast
    r101369 tests
    r103131 additional test
    
    Test plan:
    ==========
    $ ./phpunit.php --filter LanguageSr
    PHPUnit 3.6.3 by Sebastian Bergmann.
    
    Configuration read from /srv/trunk/tests/phpunit/suite.xml
    
    .....
    
    Time: 1 second, Memory: 78.50Mb
    
    OK (5 tests, 19 assertions)
    $

---
+++ tests/phpunit/languages/LanguageSrTest.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * PHPUnit tests for the Serbian language.
+ * The language can be represented using two scripts:
+ *  - Latin (SR_el)
+ *  - Cyrillic (SR_ec)
+ * Both representations seems to be bijective, hence MediaWiki can convert
+ * from one script to the other.
+ *
+ * @author Antoine Musso <hashar at free dot fr>
+ * @copyright Copyright © 2011, Antoine Musso <hashar at free dot fr>
+ * @file
+ */
+
+require_once dirname(dirname(__FILE__)). '/bootstrap.php';
+
+/** Tests for MediaWiki languages/LanguageTr.php */
+class LanguageSrTest extends MediaWikiTestCase {
+	/* Language object. Initialized before each test */
+	private $lang;
+
+	function setUp() {
+		$this->lang = Language::factory( 'Sr' );
+	}
+	function tearDown() {
+		unset( $this->lang );
+	}
+
+	##### TESTS #######################################################
+
+	function testEasyConversions( ) {
+		$this->assertCyrillic(
+			'шђчћжШЂЧЋЖ',
+			'Cyrillic guessing characters'
+		);
+		$this->assertLatin(
+			'šđčćžŠĐČĆŽ',
+			'Latin guessing characters'
+		);
+	}
+
+	function testMixedConversions() {
+		$this->assertCyrillic(
+			'шђчћжШЂЧЋЖ - šđčćž',
+			'Mostly cyrillic characters'
+		);
+		$this->assertLatin(
+			'šđčćžŠĐČĆŽ - шђчћж',
+			'Mostly latin characters'
+		);
+	}
+
+	function testSameAmountOfLatinAndCyrillicGetConverted() {
+		$this->assertConverted(
+			'4 latin: šđčć | 4 cyrillic: шђчћ',
+			'sr-ec'
+		);
+		$this->assertConverted(
+			'4 latin: šđčć | 4 cyrillic: шђчћ',
+			'sr-el'
+		);
+	}
+
+	/**
+	 * @author Nikola Smolenski 
+	 */
+	function testConversionToCyrillic() {
+		$this->assertEquals( 'абвг',
+			$this->convertToCyrillic( 'abvg' )
+		);
+		$this->assertEquals( 'абвг',
+			$this->convertToCyrillic( 'абвг' )
+		);
+		$this->assertEquals( 'abvgшђжчћ',
+			$this->convertToCyrillic( 'abvgшђжчћ' )
+		);
+		$this->assertEquals( 'абвгшђжчћ',
+			$this->convertToCyrillic( 'абвгšđžčć' )
+		);
+		//Roman numerals are not converted
+		$this->assertEquals( 'а I б II в III г IV шђжчћ',
+			$this->convertToCyrillic( 'a I b II v III g IV šđžčć' )
+		);
+	}
+
+	function testConversionToLatin() {
+		$this->assertEquals( 'abcd',
+			$this->convertToLatin( 'abcd' )
+		);
+		$this->assertEquals( 'abcd',
+			$this->convertToLatin( 'абцд' )
+		);
+		$this->assertEquals( 'abcdšđžčć',
+			$this->convertToLatin( 'abcdшђжчћ' )
+		);
+		$this->assertEquals( 'абцдšđžčć',
+			$this->convertToLatin( 'абцдšđžčć' )
+		);
+
+	}
+
+	##### HELPERS #####################################################
+	/**
+	 *Wrapper to verify text stay the same after applying conversion
+	 * @param $text string Text to convert
+	 * @param $variant string Language variant 'sr-ec' or 'sr-el'
+	 * @param $msg string Optional message
+	 */
+	function assertUnConverted( $text, $variant, $msg = '' ) {
+		$this->assertEquals(
+			$text,
+			$this->convertTo( $text, $variant ),
+			$msg
+		);
+	}
+	/**
+	 * Wrapper to verify a text is different once converted to a variant.
+	 * @param $text string Text to convert
+	 * @param $variant string Language variant 'sr-ec' or 'sr-el'
+	 * @param $msg string Optional message
+	 */
+	function assertConverted( $text, $variant, $msg = '' ) {
+		$this->assertNotEquals(
+			$text,
+			$this->convertTo( $text, $variant ),
+			$msg
+		);
+	}
+
+	/**
+	 * Verifiy the given Cyrillic text is not converted when using
+	 * using the cyrillic variant and converted to Latin when using
+	 * the Latin variant.
+	 */
+	function assertCyrillic( $text, $msg = '' ) {
+		$this->assertUnConverted( $text, 'sr-ec', $msg );
+		$this->assertConverted( $text, 'sr-el', $msg );
+	}
+	/**
+	 * Verifiy the given Latin text is not converted when using
+	 * using the Latin variant and converted to Cyrillic when using
+	 * the Cyrillic variant.
+	 */
+	function assertLatin( $text, $msg = '' ) {
+		$this->assertUnConverted( $text, 'sr-el', $msg );
+		$this->assertConverted( $text, 'sr-ec', $msg );
+	}
+
+
+	/** Wrapper for converter::convertTo() method*/
+	function convertTo( $text, $variant ) {
+		return $this
+			->lang
+			->mConverter
+			->convertTo(
+				$text, $variant
+			);
+	}
+	function convertToCyrillic( $text ) {
+		return $this->convertTo( $text, 'sr-ec' );
+	}
+	function convertToLatin( $text ) {
+		return $this->convertTo( $text, 'sr-el' );
+	}
+}
