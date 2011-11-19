577c65ffa1c5b6bc36988f284d3853d91eb88e7a
Sam Reed <reedy@users.mediawiki.org>
2011-11-19 16:19:35 +0000

    Global tables died

--- maintenance/oracle/archives/patch-globalinterwiki.sql
+++
@@ -1,8 +0,0 @@
-define mw_prefix='{$wgDBprefix}';
-
-CREATE TABLE &mw_prefix.globalinterwiki (
-	giw_wikiid VARCHAR2(64) NOT NULL,
-	giw_prefix VARCHAR2(32) NOT NULL
-);
-CREATE UNIQUE INDEX &mw_prefix.globalinterwiki_u01 ON &mw_prefix.globalinterwiki (giw_wikiid, giw_prefix);
-
