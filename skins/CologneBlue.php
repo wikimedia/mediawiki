cd8bf14f924d2f0ea283e353efe6979f4d5e7acd
Derk-Jan Hartman <hartman@users.mediawiki.org>
2011-11-19 13:48:02 +0000

    Fix an issue with the header of CologneBlue. Introduced in r97657
    Fixes bug 32474

--- skins/CologneBlue.php
+++ skins/CologneBlue.php
@@ -32,23 +32,23 @@ class SkinCologneBlue extends SkinLegacy {
 		if ( 2 == $qb ) { # Right
 			$rules[] = "/* @noflip */#quickbar { position: absolute; right: 4px; }";
 			$rules[] = "/* @noflip */#article { margin-left: 4px; margin-right: 148px; }";
-			$rules[] = "/* @noflip */#topbar, #footer { margin-right: 152px; }";
+			$rules[] = "/* @noflip */#footer { margin-right: 152px; }";
 		} elseif ( 1 == $qb ) {
 			$rules[] = "/* @noflip */#quickbar { position: absolute; left: 4px; }";
 			$rules[] = "/* @noflip */#article { margin-left: 148px; margin-right: 4px; }";
-			$rules[] = "/* @noflip */#topbar, #footer { margin-left: 152px; }";
+			$rules[] = "/* @noflip */#footer { margin-left: 152px; }";
 		} elseif ( 3 == $qb ) { # Floating left
 			$rules[] = "/* @noflip */#quickbar { position:absolute; left:4px }";
 			$rules[] = "/* @noflip */#topbar { margin-left: 148px }";
 			$rules[] = "/* @noflip */#article { margin-left:148px; margin-right: 4px; }";
 			$rules[] = "/* @noflip */body>#quickbar { position:fixed; left:4px; top:4px; overflow:auto; bottom:4px;}"; # Hides from IE
-			$rules[] = "/* @noflip */#topbar, #footer { margin-left: 152px; }";
+			$rules[] = "/* @noflip */#footer { margin-left: 152px; }";
 		} elseif ( 4 == $qb ) { # Floating right
 			$rules[] = "/* @noflip */#quickbar { position: fixed; right: 4px; }";
 			$rules[] = "/* @noflip */#topbar { margin-right: 148px }";
 			$rules[] = "/* @noflip */#article { margin-right: 148px; margin-left: 4px; }";
 			$rules[] = "/* @noflip */body>#quickbar { position: fixed; right: 4px; top: 4px; overflow: auto; bottom:4px;}"; # Hides from IE
-			$rules[] = "/* @noflip */#topbar, #footer { margin-left: 152px; }";
+			$rules[] = "/* @noflip */#footer { margin-right: 152px; }";
 		}
 		$style = implode( "\n", $rules );
 		$out->addInlineStyle( $style, 'flip' );
