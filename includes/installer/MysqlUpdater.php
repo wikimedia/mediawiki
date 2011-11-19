c6b3c2f05b45c2ebdc7ff72743eb53afdaadbaa1
Sam Reed <reedy@users.mediawiki.org>
2011-11-19 16:26:28 +0000

    * (bug 32470) Increase the length of ug_group
    Postgres doesn't need extending...

--- includes/installer/MysqlUpdater.php
+++ includes/installer/MysqlUpdater.php
@@ -189,7 +189,8 @@ class MysqlUpdater extends DatabaseUpdater {
 			array( 'dropField', 'user',         'user_options', 'patch-drop-user_options.sql' ),
 			array( 'addField', 'revision',      'rev_sha1',         'patch-rev_sha1.sql' ),
 			array( 'addField', 'archive',       'ar_sha1',          'patch-ar_sha1.sql' ),
-			array( 'addIndex', 'page', 'page_redirect_namespace_len', 'patch-page_redirect_namespace_len.sql' )
+			array( 'addIndex', 'page', 'page_redirect_namespace_len', 'patch-page_redirect_namespace_len.sql' ),
+			array( 'doIncreaseUserGroupLength' ),
 		);
 	}
 
---
+++ maintenance/archives/patch-ug_group-length-increase.sql
@@ -0,0 +1,2 @@
+ALTER TABLE /*_*/user_groups
+	MODIFY COLUMN ug_group varbinary(32) NOT NULL default '';
