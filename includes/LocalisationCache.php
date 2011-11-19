4d03992a67b1cc2edef561c44c3fb129f2ccd841
Antoine Musso <hashar@users.mediawiki.org>
2011-11-19 17:00:57 +0000

    revert r103694 r103687
    Need to find a better way to fix that cache issue :(

--- includes/LocalisationCache.php
+++ includes/LocalisationCache.php
@@ -358,10 +358,6 @@ class LocalisationCache {
 		$deps = $this->store->get( $code, 'deps' );
 		$keys = $this->store->get( $code, 'list', 'messages' );
 		$preload = $this->store->get( $code, 'preload' );
-		if( $this->store instanceof LCStore_CDB ) {
-			// Dont leave open filehandler behind after being called
-			$this->store->close( $code );
-		}
 		// Different keys may expire separately, at least in LCStore_Accel
 		if ( $deps === null || $keys === null || $preload === null ) {
 			wfDebug( __METHOD__."($code): cache missing, need to make one\n" );
@@ -1034,13 +1030,6 @@ class LCStore_CDB implements LCStore {
 			}
 			return unserialize( $value );
 		}
-	}
-
-	public function close( $code ) {
-		if ( !isset( $this->readers[$code] ) ) {
-			return;
-		}
-		$this->readers[$code]->close();
 	}
 
 	public function startWrite( $code ) {
