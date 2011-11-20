e0da5c113c468fed5d1c98fa6bf701f7d98dedea
Aaron Schulz <aaron@users.mediawiki.org>
2011-11-20 08:50:13 +0000

    image_auth.php cleanups: * Factored main code into wfImageAuthMain() * Made preg_match() for $name account for "page3-" type specifiers in the thumb name * Code style cleanups

--- img_auth.php
+++ img_auth.php
@@ -10,14 +10,15 @@
  *
  * Optional Parameters
  *
- * - Set $wgImgAuthDetails = true if you want the reason the access was denied messages to be displayed
- *       instead of just the 403 error (doesn't work on IE anyway),  otherwise will only appear in error logs
+ * - Set $wgImgAuthDetails = true if you want the reason the access was denied messages to
+ *       be displayed instead of just the 403 error (doesn't work on IE anyway),
+ *       otherwise it will only appear in error logs
  * - Set $wgImgAuthPublicTest false if you don't want to just check and see if all are public
  *       must be set to false if using specific restrictions such as LockDown or NSFileRepo
  *
- *  For security reasons, you usually don't want your user to know *why* access was denied, just that it was.
- *  If you want to change this, you can set $wgImgAuthDetails to 'true' in localsettings.php and it will give the user the reason
- *  why access was denied.
+ *  For security reasons, you usually don't want your user to know *why* access was denied,
+ *  just that it was. If you want to change this, you can set $wgImgAuthDetails to 'true'
+ *  in localsettings.php and it will give the user the reason why access was denied.
  *
  * Your server needs to support PATH_INFO; CGI-based configurations usually don't.
  *
@@ -33,74 +34,92 @@ if ( isset( $_SERVER['MW_COMPILED'] ) ) {
 }
 wfProfileIn( 'img_auth.php' );
 
+# Set action base paths so that WebRequest::getPathInfo()
+# recognizes the "X" as the 'title' in ../image_auth/X urls.
 $wgActionPaths[] = $_SERVER['SCRIPT_NAME'];
-// See if this is a public Wiki (no protections)
-if ( $wgImgAuthPublicTest
-	&& in_array( 'read', User::getGroupPermissions( array( '*' ) ), true ) )
-{
-	wfForbidden('img-auth-accessdenied','img-auth-public');
-}
 
-$matches = WebRequest::getPathInfo();
-$path = $matches['title'];
-
-// Check for bug 28235: QUERY_STRING overriding the correct extension
-$dotPos = strrpos( $path, '.' );
-$whitelist = array();
-if ( $dotPos !== false ) {
-	$whitelist[] = substr( $path, $dotPos + 1 );
-}
-if ( !$wgRequest->checkUrlExtension( $whitelist ) ) {
-	return;
-}
-
-$filename = realpath( $wgUploadDirectory . $path );
-$realUpload = realpath( $wgUploadDirectory );
-
-// Basic directory traversal check
-if( substr( $filename, 0, strlen( $realUpload ) ) != $realUpload ) {
-	wfForbidden('img-auth-accessdenied','img-auth-notindir');
-}
-
-// Extract the file name and chop off the size specifier
-// (e.g. 120px-Foo.png => Foo.png)
-$name = wfBaseName( $path );
-if( preg_match( '!\d+px-(.*)!i', $name, $m ) ) {
-	$name = $m[1];
-}
-
-// Check to see if the file exists
-if( !file_exists( $filename ) ) {
-	wfForbidden('img-auth-accessdenied','img-auth-nofile',$filename);
-}
-
-// Check to see if tried to access a directory
-if( is_dir( $filename ) ) {
-	wfForbidden('img-auth-accessdenied','img-auth-isdir',$filename);
-}
-
-$title = Title::makeTitleSafe( NS_FILE, $name );
-
-// See if could create the title object
-if( !$title instanceof Title ) {
-	wfForbidden('img-auth-accessdenied','img-auth-badtitle',$name);
-}
-
-// Run hook
-if (!wfRunHooks( 'ImgAuthBeforeStream', array( &$title, &$path, &$name, &$result ) ) ) {
-	wfForbidden($result[0],$result[1],array_slice($result,2));
-}
-
-//  Check user authorization for this title
-//  UserCanRead Checks Whitelist too
-if( !$title->userCanRead() ) {
-	wfForbidden('img-auth-accessdenied','img-auth-noread',$name);
-}
-
-// Stream the requested file
-wfDebugLog( 'img_auth', "Streaming `".$filename."`." );
-StreamFile::stream( $filename, array( 'Cache-Control: private', 'Vary: Cookie' ) );
+wfImageAuthMain();
 wfLogProfilingData();
+
+function wfImageAuthMain() {
+	global $wgImgAuthPublicTest, $wgRequest, $wgUploadDirectory;
+
+	// See if this is a public Wiki (no protections).
+	if ( $wgImgAuthPublicTest
+		&& in_array( 'read', User::getGroupPermissions( array( '*' ) ), true ) )
+	{
+		// This is a public wiki, so disable this script (for private wikis only)
+		wfForbidden( 'img-auth-accessdenied', 'img-auth-public' );
+		return;
+	}
+
+	// Get the requested file path (source file or thumbnail)
+	$matches = WebRequest::getPathInfo();
+	$path = $matches['title']; // path with leading '/'
+
+	// Check for bug 28235: QUERY_STRING overriding the correct extension
+	$whitelist = array();
+	$dotPos = strrpos( $path, '.' );
+	if ( $dotPos !== false ) {
+		$whitelist[] = substr( $path, $dotPos + 1 );
+	}
+	if ( !$wgRequest->checkUrlExtension( $whitelist ) ) {
+		return;
+	}
+
+	// Get the full file path
+	$filename = realpath( $wgUploadDirectory . $path );
+	$realUpload = realpath( $wgUploadDirectory );
+
+	// Basic directory traversal check
+	if ( substr( $filename, 0, strlen( $realUpload ) ) != $realUpload ) {
+		wfForbidden( 'img-auth-accessdenied', 'img-auth-notindir' );
+		return;
+	}
+
+	// Check to see if the file exists
+	if ( !file_exists( $filename ) ) {
+		wfForbidden( 'img-auth-accessdenied','img-auth-nofile', $filename );
+		return;
+	}
+	
+	// Check to see if tried to access a directory
+	if ( is_dir( $filename ) ) {
+		wfForbidden( 'img-auth-accessdenied','img-auth-isdir', $filename );
+		return;
+	}
+
+	// Extract the file name and chop off the size specifier
+	// (e.g. 120px-Foo.png => Foo.png or page2-120px-Foo.png => Foo.png).
+	// This only applies to thumbnails, and all thumbnails have a -px specifier.
+	$name = wfBaseName( $path );
+	if ( preg_match( '!(?:[^-]*-)*?\d+px-(.*)!i', $name, $m ) ) {
+		$name = $m[1]; // this file is a thumbnail
+	}
+
+	$title = Title::makeTitleSafe( NS_FILE, $name );
+	if ( !$title instanceof Title ) { // files have valid titles
+		wfForbidden( 'img-auth-accessdenied', 'img-auth-badtitle', $name );
+		return;
+	}
+
+	// Run hook for extension authorization plugins
+	if ( !wfRunHooks( 'ImgAuthBeforeStream', array( &$title, &$path, &$name, &$result ) ) ) {
+		wfForbidden( $result[0], $result[1], array_slice( $result, 2 ) );
+		return;
+	}
+	
+	// Check user authorization for this title
+	// UserCanRead Checks Whitelist too
+	if( !$title->userCanRead() ) {
+		wfForbidden( 'img-auth-accessdenied', 'img-auth-noread', $name );
+		return;
+	}
+
+	// Stream the requested file
+	wfDebugLog( 'img_auth', "Streaming `".$filename."`." );
+	StreamFile::stream( $filename, array( 'Cache-Control: private', 'Vary: Cookie' ) );
+}
 
 /**
  * Issue a standard HTTP 403 Forbidden header ($msg1-a message index, not a message) and an
@@ -111,24 +130,29 @@ wfLogProfilingData();
  */
 function wfForbidden( $msg1, $msg2 ) {
 	global $wgImgAuthDetails;
+
 	$args = func_get_args();
 	array_shift( $args );
 	array_shift( $args );
-	$MsgHdr = htmlspecialchars( wfMsg( $msg1 ) );
-	$detailMsg = ( htmlspecialchars( wfMsg( ( $wgImgAuthDetails ? $msg2 : 'badaccess-group0' ), $args ) ) );
-	wfDebugLog('img_auth', "wfForbidden Hdr:".wfMsgExt( $msg1, array( 'language' => 'en' ) )." Msg: ".
-		wfMsgExt( $msg2, array('language' => 'en' ), $args ) );
+
+	$msgHdr = htmlspecialchars( wfMsg( $msg1 ) );
+	$detailMsgKey = $wgImgAuthDetails ? $msg2 : 'badaccess-group0';
+	$detailMsg = htmlspecialchars( wfMsg( $detailMsgKey, $args ) );
+
+	wfDebugLog( 'img_auth',
+		"wfForbidden Hdr:" . wfMsgExt( $msg1, array( 'language' => 'en' ) ). " Msg: ".
+		wfMsgExt( $msg2, array( 'language' => 'en' ), $args )
+	);
+
 	header( 'HTTP/1.0 403 Forbidden' );
 	header( 'Cache-Control: no-cache' );
 	header( 'Content-Type: text/html; charset=utf-8' );
 	echo <<<ENDS
 <html>
 <body>
-<h1>$MsgHdr</h1>
+<h1>$msgHdr</h1>
 <p>$detailMsg</p>
 </body>
 </html>
 ENDS;
-	wfLogProfilingData();
-	exit();
 }
