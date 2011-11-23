900e5acb91dfcecd06c54f2159284e30cc41912c
Roan Kattouw <catrope@users.mediawiki.org>
2011-11-23 10:25:07 +0000

    Fix typo in r103751

--- includes/specials/SpecialDeletedContributions.php
+++ includes/specials/SpecialDeletedContributions.php
@@ -345,7 +345,7 @@ class DeletedContributionsPage extends SpecialPage {
 		if ( $userObj->isAnon() ) {
 			$user = htmlspecialchars( $userObj->getName() );
 		} else {
-			$user = Linker::link( $userObj->getUserPage(), htmlspecialchars( $userObj->getText() ) );
+			$user = Linker::link( $userObj->getUserPage(), htmlspecialchars( $userObj->getName() ) );
 		}
 		$nt = $userObj->getUserPage();
 		$id = $userObj->getID();
