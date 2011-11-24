wikimedia/mediawiki

commit 5a3474d0592708c02d9a5ca35aff17a4c04b8560

* (bug 32376) XML export tweak to use canonical user namespaces instead of gendered ones for now
The gendered namespaces aren't listed in the <siteinfo> namespaces list, and just adding them as-is may break reader implementations.
Using the canonical form should have little effect on most activity while keeping things working.

Should resolve Lucene search problems (bug 31697) related to the Lucene search indexer not understanding the gendered namespace names.
After fix and reindex, the canonical forms should get returned and automatically transformed back into appropriate gendered forms in the search view.

[per IRC discussion check-up with Ariel about this bug and the patches on bug 30513 which cover related namespacey issues]




git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@103945 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Export.php
@@ -476,14 +476,14 @@ class XmlDumpWriter {
 	function openPage( $row ) {
 		$out = "  <page>\n";
 		$title = Title::makeTitle( $row->page_namespace, $row->page_title );
-		$out .= '    ' . Xml::elementClean( 'title', array(), $title->getPrefixedText() ) . "\n";
+		$out .= '    ' . Xml::elementClean( 'title', array(), self::canonicalTitle( $title ) ) . "\n";
 		$out .= '    ' . Xml::element( 'ns', array(), strval( $row->page_namespace) ) . "\n";
 		$out .= '    ' . Xml::element( 'id', array(), strval( $row->page_id ) ) . "\n";
 		if ( $row->page_is_redirect ) {
 			$page = WikiPage::factory( $title );
 			$redirect = $page->getRedirectTarget();
 			if ( $redirect instanceOf Title && $redirect->isValidRedirectTarget() ) {
-				$out .= '    ' . Xml::element( 'redirect', array( 'title' => $redirect->getPrefixedText() ) ) . "\n";
+				$out .= '    ' . Xml::element( 'redirect', array( 'title' => self::canonicalTitle( $redirect ) ) ) . "\n";
 			}
 		}
 		if ( $row->page_restrictions != '' ) {
@@ -595,7 +595,7 @@ class XmlDumpWriter {
 			$out .= "      " . Xml::element( 'text', array( 'deleted' => 'deleted' ) ) . "\n";
 		} else {
 			$title = Title::makeTitle( $row->log_namespace, $row->log_title );
-			$out .= "      " . Xml::elementClean( 'logtitle', null, $title->getPrefixedText() ) . "\n";
+			$out .= "      " . Xml::elementClean( 'logtitle', null, self::canonicalTitle( $title ) ) . "\n";
 			$out .= "      " . Xml::elementClean( 'params',
 				array( 'xml:space' => 'preserve' ),
 				strval( $row->log_params ) ) . "\n";
@@ -677,6 +677,29 @@ class XmlDumpWriter {
 			"    </upload>\n";
 	}
 
+	/**
+	 * Return prefixed text form of title, but using the content language's
+	 * canonical namespace. This skips any special-casing such as gendered
+	 * user namespaces -- which while useful, are not yet listed in the
+	 * XML <siteinfo> data so are unsafe in export.
+	 * 
+	 * @param Title $title
+	 * @return string
+	 */
+	public static function canonicalTitle( Title $title ) {
+		if ( $title->getInterwiki() ) {
+			return $title->getPrefixedText();
+		}
+
+		global $wgContLang;
+		$prefix = $wgContLang->getNsText( $title->getNamespace() );
+
+		if ($prefix !== '') {
+			$prefix .= ':';
+		}
+
+		return $prefix . $title->getText();
+	}
 }
 
 

commit 1f91192fcde84098fd9f43ffc25adca9099b973e

MFT r104124 (fixes XML export regression in r103945 / r103953 on branch)

includes/Export.php
@@ -687,7 +687,7 @@ class XmlDumpWriter {
 		}
 
 		global $wgContLang;
-		$prefix = $wgContLang->getNsText( $title->getNamespace() );
+		$prefix = str_replace( '_', ' ', $wgContLang->getNsText( $title->getNamespace() ) );
 
 		if ($prefix !== '') {
 			$prefix .= ':';