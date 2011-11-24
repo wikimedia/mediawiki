148018b7da95f99983cb04f502c27e9dbcf57030
Sam Reed <reedy@users.mediawiki.org>
2011-11-24 20:19:08 +0000

    Bump $wgVersion to 1.18.0
    Update RELEASE-NOTES-1.18 and add a summary of selected changes

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.18.0rc1';
+$wgVersion = '1.18.0';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
