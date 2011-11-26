ac19a8601c381ff56b429c415a84c08e59caa1a2
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-11-26 23:48:57 +0000

    added action message

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -2025,6 +2025,7 @@ Your e-mail address is not revealed when other users contact you.',
 'action-userrights'           => 'edit all user rights',
 'action-userrights-interwiki' => 'edit user rights of users on other wikis',
 'action-siteadmin'            => 'lock or unlock the database',
+'action-sendemail'            => 'send emails',
 
 # Recent changes
 'nchanges'                          => '$1 {{PLURAL:$1|change|changes}}',
