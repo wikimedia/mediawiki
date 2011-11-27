eb43fae8d3f904265ee914689b5e0e370d68c4d2
Platonides <platonides@users.mediawiki.org>
2011-11-27 16:12:20 +0000

    Test to ensure that after adding a category to a template, the articles get moved into that category.
    Articles containing [[es:Plantilla:Ficha_de_película]] showed the categories at the bottom, but were 
    not in categorylinks (solved by a doing a null edit to the template and waiting for the job queue, but
    how did that situation happen to begin with?).

---
+++ tests/phpunit/includes/TemplateCategoriesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @group Database
+ */
+require dirname( __FILE__ ) . "/../../../maintenance/runJobs.php";
+
+class TemplateCategoriesTest extends MediaWikiLangTestCase {
+
+	function testTemplateCategories() {
+		global $wgUser;
+
+		$title = Title::newFromText( "Categorized from template" );
+		$article = new Article( $title );
+		$wgUser = new User();
+		$wgUser->mRights['*'] = array( 'createpage', 'edit', 'purge' );
+
+		$status = $article->doEdit( '{{Categorising template}}', 'Create a page with a template', 0 );
+		$this->assertEquals( $title->getParentCategories(), array() );
+
+		$template = new Article( Title::newFromText( 'Template:Categorising template' ) );
+		$status = $template->doEdit( '[[Category:Solved bugs]]', 'Add a category through a template', 0 );
+
+		// Run the job queue
+		$jobs = new RunJobs;
+		$jobs->loadParamsAndArgs( null, array( 'quiet' => true ), null );
+		$jobs->execute();
+
+		$this->assertEquals( $title->getParentCategories(), array( 'Category:Solved_bugs' => $title->getPrefixedText() ) );
+	}
+
+}
