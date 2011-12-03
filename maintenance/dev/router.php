5ab34e35178f181697e7da2f787ad963e55f9b5b
Daniel Friesen <dantman@users.mediawiki.org>
2011-12-03 23:19:34 +0000

    Followup r103678; Add a comment.

--- maintenance/dev/router.php
+++ maintenance/dev/router.php
@@ -1,5 +1,8 @@
 <?php
 
+# Router for the php cli-server built-in webserver
+# http://ca2.php.net/manual/en/features.commandline.webserver.php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
