5bfce03820d1e511b1d9e3caca029d2bb6026d24
Sam Reed <reedy@users.mediawiki.org>
2011-12-05 16:50:58 +0000

    More documentation of parameters following up r104591

--- includes/Skin.php
+++ includes/Skin.php
@@ -288,7 +288,7 @@ abstract class Skin extends ContextSource {
 	abstract function outputPage( OutputPage $out = null );
 
 	/**
-	 * @param $data
+	 * @param $data array
 	 * @return string
 	 */
 	static function makeVariablesScript( $data ) {
@@ -543,7 +543,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $debugText
+	 * @param $debugText string
 	 * @return string
 	 */
 	private function formatDebugHTML( $debugText ) {
@@ -734,7 +734,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param string $type
+	 * @param $type string
 	 * @return string
 	 */
 	function getCopyright( $type = 'detect' ) {
@@ -976,7 +976,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $id
+	 * @param $id User|int
 	 * @return bool
 	 */
 	function showEmailUser( $id ) {
@@ -1027,7 +1027,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $name
+	 * @param $name string
 	 * @param $urlaction string
 	 * @return String
 	 */
@@ -1037,8 +1037,8 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $name
-	 * @param $subpage
+	 * @param $name string
+	 * @param $subpage string
 	 * @param $urlaction string
 	 * @return String
 	 */
@@ -1048,7 +1048,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $name
+	 * @param $name string
 	 * @param $urlaction string
 	 * @return String
 	 */
@@ -1059,7 +1059,7 @@ abstract class Skin extends ContextSource {
 	}
 
 	/**
-	 * @param $name
+	 * @param $name string
 	 * @param $urlaction string
 	 * @return String
 	 */
@@ -1134,7 +1134,7 @@ abstract class Skin extends ContextSource {
 	 * make sure we have some title to operate on
 	 *
 	 * @param $title Title
-	 * @param $name
+	 * @param $name string
 	 */
 	static function checkTitle( &$title, $name ) {
 		if ( !is_object( $title ) ) {
