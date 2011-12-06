f19bd2153b40d674ba63e80e988163ed7e94e0a3
Chad Horohoe <demon@users.mediawiki.org>
2011-12-06 23:06:50 +0000

    Indentation fix

--- includes/PHPVersionError.php
+++ includes/PHPVersionError.php
@@ -32,7 +32,7 @@ function wfPHPVersionError( $type ){
 		header( 'Cache-control: none' );
 		header( 'Pragma: nocache' );
 
-$finalOutput = <<<HTML
+		$finalOutput = <<<HTML
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns='http://www.w3.org/1999/xhtml' lang='en'>
 	<head>
