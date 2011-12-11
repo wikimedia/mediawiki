fad5513f2bdf615c0653ff756f58879ac5e86134
Daniel Friesen <dantman@users.mediawiki.org>
2011-12-11 19:45:15 +0000

    Follow up r102210: - Update maintenance/dev/ to install php inside ~/.mediawiki/php instead of ~/.mwphp - Tweak README a bit - Move the router.php into an includes along with two helper .sh files to cut out some of the repetition

--- maintenance/dev/start.sh
+++ maintenance/dev/start.sh
@@ -3,14 +3,7 @@
 if [ "x$BASH_SOURCE" == "x" ]; then echo '$BASH_SOURCE not set'; exit 1; fi
 DEV=$(cd -P "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-if [ -d "$DEV/php" -a -x "$DEV/php/bin/php" ]; then
-	PHP="$DEV/php/bin/php"
-elif [ -d "$HOME/.mwphp" -a -x "$HOME/.mwphp/bin/php" ]; then
-	PHP="$HOME/.mwphp/bin/php"
-else
-	echo "Local copy of PHP is not installed"
-	echo 1
-fi
+. "$DEV/includes/require-php.sh"
 
 PORT=4881
 
@@ -18,4 +11,4 @@ echo "Starting up MediaWiki at http://localhost:$PORT/"
 echo ""
 
 cd "$DEV/../../"; # $IP
-"$PHP" -S "localhost:$PORT" "$DEV/router.php"
+"$PHP" -S "localhost:$PORT" "$DEV/includes/router.php"
