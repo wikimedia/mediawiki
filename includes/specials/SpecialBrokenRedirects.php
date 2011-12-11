e41db1309d72ae0e246911d9e44911ee49f90cb8
Sam Reed <reedy@users.mediawiki.org>
2011-12-11 16:35:50 +0000

    * (bug 31759) Undefined property notice in querypages API

--- includes/specials/SpecialBrokenRedirects.php
+++ includes/specials/SpecialBrokenRedirects.php
@@ -47,6 +47,7 @@ class BrokenRedirectsPage extends PageQueryPage {
 					'p2' => 'page' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
+					'p1.page_title AS value',
 					'rd_namespace',
 					'rd_title'
 			),
