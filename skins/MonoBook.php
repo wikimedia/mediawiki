2fde32bed64e6dec115018a5f056f2c2af2fa883
Amir E. Aharoni <amire80@users.mediawiki.org>
2011-12-12 12:47:07 +0000

    Followup to r105854. Made directionality inline rather than block-level, because text alignment should be consistent with the rest of the site. Added support for Monobook and Modern skins.

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -69,7 +69,7 @@ class MonoBookTemplate extends BaseTemplate {
 	<a id="top"></a>
 	<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
 
-	<h1 id="firstHeading" class="firstHeading"><?php $this->html('title') ?></h1>
+	<h1 id="firstHeading" class="firstHeading"><span dir="auto"><?php $this->html('title') ?></span></h1>
 	<div id="bodyContent" class="mw-body">
 		<div id="siteSub"><?php $this->msg('tagline') ?></div>
 		<div id="contentSub"<?php $this->html('userlangattributes') ?>><?php $this->html('subtitle') ?></div>
