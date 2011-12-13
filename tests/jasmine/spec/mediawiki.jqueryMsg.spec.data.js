462dc23d0399b1feff93793121abc1a8481ddb94
Neil Kandalgaonkar <neilk@users.mediawiki.org>
2011-12-13 03:03:05 +0000

    moved language library to core mediawiki.jqueryMsg

---
+++ tests/jasmine/spec/mediawiki.jqueryMsg.spec.data.js
@@ -0,0 +1,488 @@
+// This file stores the results from the PHP parser for certain messages and arguments,
+// so we can test the equivalent Javascript libraries.
+// Last generated with makeLanguageSpec.php at 2011-01-28T02:04:09+00:00
+
+mediaWiki.messages.set( {
+	"en_undelete_short": "Undelete {{PLURAL:$1|one edit|$1 edits}}",
+	"en_category-subcat-count": "{{PLURAL:$2|This category has only the following subcategory.|This category has the following {{PLURAL:$1|subcategory|$1 subcategories}}, out of $2 total.}}",
+	"fr_undelete_short": "Restaurer $1 modification{{PLURAL:$1||s}}",
+	"fr_category-subcat-count": "Cette cat\u00e9gorie comprend {{PLURAL:$2|la sous-cat\u00e9gorie|$2 sous-cat\u00e9gories, dont {{PLURAL:$1|celle|les $1}}}} ci-dessous.",
+	"ar_undelete_short": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 {{PLURAL:$1|\u062a\u0639\u062f\u064a\u0644 \u0648\u0627\u062d\u062f|\u062a\u0639\u062f\u064a\u0644\u064a\u0646|$1 \u062a\u0639\u062f\u064a\u0644\u0627\u062a|$1 \u062a\u0639\u062f\u064a\u0644|$1 \u062a\u0639\u062f\u064a\u0644\u0627}}",
+	"ar_category-subcat-count": "{{PLURAL:$2|\u0644\u0627 \u062a\u0635\u0627\u0646\u064a\u0641 \u0641\u0631\u0639\u064a\u0629 \u0641\u064a \u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641|\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a \u0627\u0644\u062a\u0627\u0644\u064a \u0641\u0642\u0637.|\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 {{PLURAL:$1||\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a|\u0647\u0630\u064a\u0646 \u0627\u0644\u062a\u0635\u0646\u064a\u0641\u064a\u0646 \u0627\u0644\u0641\u0631\u0639\u064a\u064a\u0646|\u0647\u0630\u0647 \u0627\u0644$1 \u062a\u0635\u0627\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a\u0629|\u0647\u0630\u0647 \u0627\u0644$1 \u062a\u0635\u0646\u064a\u0641\u0627 \u0641\u0631\u0639\u064a\u0627|\u0647\u0630\u0647 \u0627\u0644$1 \u062a\u0635\u0646\u064a\u0641 \u0641\u0631\u0639\u064a}}\u060c \u0645\u0646 \u0625\u062c\u0645\u0627\u0644\u064a $2.}}",
+	"jp_undelete_short": "Undelete {{PLURAL:$1|one edit|$1 edits}}",
+	"jp_category-subcat-count": "{{PLURAL:$2|This category has only the following subcategory.|This category has the following {{PLURAL:$1|subcategory|$1 subcategories}}, out of $2 total.}}",
+	"zh_undelete_short": "\u6062\u590d\u88ab\u5220\u9664\u7684$1\u9879\u4fee\u8ba2",
+	"zh_category-subcat-count": "{{PLURAL:$2|\u672c\u5206\u7c7b\u53ea\u6709\u4e0b\u5217\u4e00\u4e2a\u5b50\u5206\u7c7b\u3002|\u672c\u5206\u7c7b\u5305\u542b\u4e0b\u5217$1\u4e2a\u5b50\u5206\u7c7b\uff0c\u5171\u6709$2\u4e2a\u5b50\u5206\u7c7b\u3002}}"
+} );
+var jasmineMsgSpec = [
+	{
+		"name": "en undelete_short 0",
+		"key": "en_undelete_short",
+		"args": [
+			0
+		],
+		"result": "Undelete 0 edits",
+		"lang": "en"
+	},
+	{
+		"name": "en undelete_short 1",
+		"key": "en_undelete_short",
+		"args": [
+			1
+		],
+		"result": "Undelete one edit",
+		"lang": "en"
+	},
+	{
+		"name": "en undelete_short 2",
+		"key": "en_undelete_short",
+		"args": [
+			2
+		],
+		"result": "Undelete 2 edits",
+		"lang": "en"
+	},
+	{
+		"name": "en undelete_short 5",
+		"key": "en_undelete_short",
+		"args": [
+			5
+		],
+		"result": "Undelete 5 edits",
+		"lang": "en"
+	},
+	{
+		"name": "en undelete_short 21",
+		"key": "en_undelete_short",
+		"args": [
+			21
+		],
+		"result": "Undelete 21 edits",
+		"lang": "en"
+	},
+	{
+		"name": "en undelete_short 101",
+		"key": "en_undelete_short",
+		"args": [
+			101
+		],
+		"result": "Undelete 101 edits",
+		"lang": "en"
+	},
+	{
+		"name": "en category-subcat-count 0,10",
+		"key": "en_category-subcat-count",
+		"args": [
+			0,
+			10
+		],
+		"result": "This category has the following 0 subcategories, out of 10 total.",
+		"lang": "en"
+	},
+	{
+		"name": "en category-subcat-count 1,1",
+		"key": "en_category-subcat-count",
+		"args": [
+			1,
+			1
+		],
+		"result": "This category has only the following subcategory.",
+		"lang": "en"
+	},
+	{
+		"name": "en category-subcat-count 1,2",
+		"key": "en_category-subcat-count",
+		"args": [
+			1,
+			2
+		],
+		"result": "This category has the following subcategory, out of 2 total.",
+		"lang": "en"
+	},
+	{
+		"name": "en category-subcat-count 3,30",
+		"key": "en_category-subcat-count",
+		"args": [
+			3,
+			30
+		],
+		"result": "This category has the following 3 subcategories, out of 30 total.",
+		"lang": "en"
+	},
+	{
+		"name": "fr undelete_short 0",
+		"key": "fr_undelete_short",
+		"args": [
+			0
+		],
+		"result": "Restaurer 0 modification",
+		"lang": "fr"
+	},
+	{
+		"name": "fr undelete_short 1",
+		"key": "fr_undelete_short",
+		"args": [
+			1
+		],
+		"result": "Restaurer 1 modification",
+		"lang": "fr"
+	},
+	{
+		"name": "fr undelete_short 2",
+		"key": "fr_undelete_short",
+		"args": [
+			2
+		],
+		"result": "Restaurer 2 modifications",
+		"lang": "fr"
+	},
+	{
+		"name": "fr undelete_short 5",
+		"key": "fr_undelete_short",
+		"args": [
+			5
+		],
+		"result": "Restaurer 5 modifications",
+		"lang": "fr"
+	},
+	{
+		"name": "fr undelete_short 21",
+		"key": "fr_undelete_short",
+		"args": [
+			21
+		],
+		"result": "Restaurer 21 modifications",
+		"lang": "fr"
+	},
+	{
+		"name": "fr undelete_short 101",
+		"key": "fr_undelete_short",
+		"args": [
+			101
+		],
+		"result": "Restaurer 101 modifications",
+		"lang": "fr"
+	},
+	{
+		"name": "fr category-subcat-count 0,10",
+		"key": "fr_category-subcat-count",
+		"args": [
+			0,
+			10
+		],
+		"result": "Cette cat\u00e9gorie comprend 10 sous-cat\u00e9gories, dont celle ci-dessous.",
+		"lang": "fr"
+	},
+	{
+		"name": "fr category-subcat-count 1,1",
+		"key": "fr_category-subcat-count",
+		"args": [
+			1,
+			1
+		],
+		"result": "Cette cat\u00e9gorie comprend la sous-cat\u00e9gorie ci-dessous.",
+		"lang": "fr"
+	},
+	{
+		"name": "fr category-subcat-count 1,2",
+		"key": "fr_category-subcat-count",
+		"args": [
+			1,
+			2
+		],
+		"result": "Cette cat\u00e9gorie comprend 2 sous-cat\u00e9gories, dont celle ci-dessous.",
+		"lang": "fr"
+	},
+	{
+		"name": "fr category-subcat-count 3,30",
+		"key": "fr_category-subcat-count",
+		"args": [
+			3,
+			30
+		],
+		"result": "Cette cat\u00e9gorie comprend 30 sous-cat\u00e9gories, dont les 3 ci-dessous.",
+		"lang": "fr"
+	},
+	{
+		"name": "ar undelete_short 0",
+		"key": "ar_undelete_short",
+		"args": [
+			0
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 \u062a\u0639\u062f\u064a\u0644 \u0648\u0627\u062d\u062f",
+		"lang": "ar"
+	},
+	{
+		"name": "ar undelete_short 1",
+		"key": "ar_undelete_short",
+		"args": [
+			1
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 \u062a\u0639\u062f\u064a\u0644\u064a\u0646",
+		"lang": "ar"
+	},
+	{
+		"name": "ar undelete_short 2",
+		"key": "ar_undelete_short",
+		"args": [
+			2
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 2 \u062a\u0639\u062f\u064a\u0644\u0627\u062a",
+		"lang": "ar"
+	},
+	{
+		"name": "ar undelete_short 5",
+		"key": "ar_undelete_short",
+		"args": [
+			5
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 5 \u062a\u0639\u062f\u064a\u0644",
+		"lang": "ar"
+	},
+	{
+		"name": "ar undelete_short 21",
+		"key": "ar_undelete_short",
+		"args": [
+			21
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 21 \u062a\u0639\u062f\u064a\u0644\u0627",
+		"lang": "ar"
+	},
+	{
+		"name": "ar undelete_short 101",
+		"key": "ar_undelete_short",
+		"args": [
+			101
+		],
+		"result": "\u0627\u0633\u062a\u0631\u062c\u0627\u0639 101 \u062a\u0639\u062f\u064a\u0644\u0627",
+		"lang": "ar"
+	},
+	{
+		"name": "ar category-subcat-count 0,10",
+		"key": "ar_category-subcat-count",
+		"args": [
+			0,
+			10
+		],
+		"result": "\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 \u060c \u0645\u0646 \u0625\u062c\u0645\u0627\u0644\u064a 10.",
+		"lang": "ar"
+	},
+	{
+		"name": "ar category-subcat-count 1,1",
+		"key": "ar_category-subcat-count",
+		"args": [
+			1,
+			1
+		],
+		"result": "\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a \u0627\u0644\u062a\u0627\u0644\u064a \u0641\u0642\u0637.",
+		"lang": "ar"
+	},
+	{
+		"name": "ar category-subcat-count 1,2",
+		"key": "ar_category-subcat-count",
+		"args": [
+			1,
+			2
+		],
+		"result": "\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 \u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a\u060c \u0645\u0646 \u0625\u062c\u0645\u0627\u0644\u064a 2.",
+		"lang": "ar"
+	},
+	{
+		"name": "ar category-subcat-count 3,30",
+		"key": "ar_category-subcat-count",
+		"args": [
+			3,
+			30
+		],
+		"result": "\u0647\u0630\u0627 \u0627\u0644\u062a\u0635\u0646\u064a\u0641 \u0641\u064a\u0647 \u0647\u0630\u0647 \u0627\u06443 \u062a\u0635\u0627\u0646\u064a\u0641 \u0627\u0644\u0641\u0631\u0639\u064a\u0629\u060c \u0645\u0646 \u0625\u062c\u0645\u0627\u0644\u064a 30.",
+		"lang": "ar"
+	},
+	{
+		"name": "jp undelete_short 0",
+		"key": "jp_undelete_short",
+		"args": [
+			0
+		],
+		"result": "Undelete 0 edits",
+		"lang": "jp"
+	},
+	{
+		"name": "jp undelete_short 1",
+		"key": "jp_undelete_short",
+		"args": [
+			1
+		],
+		"result": "Undelete one edit",
+		"lang": "jp"
+	},
+	{
+		"name": "jp undelete_short 2",
+		"key": "jp_undelete_short",
+		"args": [
+			2
+		],
+		"result": "Undelete 2 edits",
+		"lang": "jp"
+	},
+	{
+		"name": "jp undelete_short 5",
+		"key": "jp_undelete_short",
+		"args": [
+			5
+		],
+		"result": "Undelete 5 edits",
+		"lang": "jp"
+	},
+	{
+		"name": "jp undelete_short 21",
+		"key": "jp_undelete_short",
+		"args": [
+			21
+		],
+		"result": "Undelete 21 edits",
+		"lang": "jp"
+	},
+	{
+		"name": "jp undelete_short 101",
+		"key": "jp_undelete_short",
+		"args": [
+			101
+		],
+		"result": "Undelete 101 edits",
+		"lang": "jp"
+	},
+	{
+		"name": "jp category-subcat-count 0,10",
+		"key": "jp_category-subcat-count",
+		"args": [
+			0,
+			10
+		],
+		"result": "This category has the following 0 subcategories, out of 10 total.",
+		"lang": "jp"
+	},
+	{
+		"name": "jp category-subcat-count 1,1",
+		"key": "jp_category-subcat-count",
+		"args": [
+			1,
+			1
+		],
+		"result": "This category has only the following subcategory.",
+		"lang": "jp"
+	},
+	{
+		"name": "jp category-subcat-count 1,2",
+		"key": "jp_category-subcat-count",
+		"args": [
+			1,
+			2
+		],
+		"result": "This category has the following subcategory, out of 2 total.",
+		"lang": "jp"
+	},
+	{
+		"name": "jp category-subcat-count 3,30",
+		"key": "jp_category-subcat-count",
+		"args": [
+			3,
+			30
+		],
+		"result": "This category has the following 3 subcategories, out of 30 total.",
+		"lang": "jp"
+	},
+	{
+		"name": "zh undelete_short 0",
+		"key": "zh_undelete_short",
+		"args": [
+			0
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u76840\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh undelete_short 1",
+		"key": "zh_undelete_short",
+		"args": [
+			1
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u76841\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh undelete_short 2",
+		"key": "zh_undelete_short",
+		"args": [
+			2
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u76842\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh undelete_short 5",
+		"key": "zh_undelete_short",
+		"args": [
+			5
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u76845\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh undelete_short 21",
+		"key": "zh_undelete_short",
+		"args": [
+			21
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u768421\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh undelete_short 101",
+		"key": "zh_undelete_short",
+		"args": [
+			101
+		],
+		"result": "\u6062\u590d\u88ab\u5220\u9664\u7684101\u9879\u4fee\u8ba2",
+		"lang": "zh"
+	},
+	{
+		"name": "zh category-subcat-count 0,10",
+		"key": "zh_category-subcat-count",
+		"args": [
+			0,
+			10
+		],
+		"result": "\u672c\u5206\u7c7b\u5305\u542b\u4e0b\u52170\u4e2a\u5b50\u5206\u7c7b\uff0c\u5171\u670910\u4e2a\u5b50\u5206\u7c7b\u3002",
+		"lang": "zh"
+	},
+	{
+		"name": "zh category-subcat-count 1,1",
+		"key": "zh_category-subcat-count",
+		"args": [
+			1,
+			1
+		],
+		"result": "\u672c\u5206\u7c7b\u53ea\u6709\u4e0b\u5217\u4e00\u4e2a\u5b50\u5206\u7c7b\u3002",
+		"lang": "zh"
+	},
+	{
+		"name": "zh category-subcat-count 1,2",
+		"key": "zh_category-subcat-count",
+		"args": [
+			1,
+			2
+		],
+		"result": "\u672c\u5206\u7c7b\u5305\u542b\u4e0b\u52171\u4e2a\u5b50\u5206\u7c7b\uff0c\u5171\u67092\u4e2a\u5b50\u5206\u7c7b\u3002",
+		"lang": "zh"
+	},
+	{
+		"name": "zh category-subcat-count 3,30",
+		"key": "zh_category-subcat-count",
+		"args": [
+			3,
+			30
+		],
+		"result": "\u672c\u5206\u7c7b\u5305\u542b\u4e0b\u52173\u4e2a\u5b50\u5206\u7c7b\uff0c\u5171\u670930\u4e2a\u5b50\u5206\u7c7b\u3002",
+		"lang": "zh"
+	}
+];
---
+++ tests/jasmine/spec_makers/makeJqueryMsgSpec.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This PHP script defines the spec that the Javascript message parser should conform to.
+ *
+ * It does this by looking up the results of various string kinds of string parsing, with various languages,
+ * in the current installation of MediaWiki. It then outputs a static specification, mapping expected inputs to outputs,
+ * which can be used with the JasmineBDD framework. This specification can then be used by simply including it into
+ * the SpecRunner.html file.
+ *
+ * This is similar to Michael Dale (mdale@mediawiki.org)'s parser tests, except that it doesn't look up the 
+ * API results while doing the test, so the Jasmine run is much faster(at the cost of being out of date in rare
+ * circumstances. But mostly the parsing that we are doing in Javascript doesn't change much.)
+ *
+ */ 
+
+$maintenanceDir = dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/maintenance';
+
+require( "$maintenanceDir/Maintenance.php" );
+
+class MakeLanguageSpec extends Maintenance {
+
+	static $keyToTestArgs = array(
+		'undelete_short' => array( 
+			array( 0 ), 
+			array( 1 ), 
+			array( 2 ), 
+			array( 5 ), 
+			array( 21 ), 
+			array( 101 ) 
+		),
+		'category-subcat-count' => array(  
+			array( 0, 10 ), 
+			array( 1, 1 ), 
+			array( 1, 2 ), 
+			array( 3, 30 ) 
+		)
+	);
+
+	public function __construct() {
+                parent::__construct();
+                $this->mDescription = "Create a JasmineBDD-compatible specification for message parsing";
+                // add any other options here
+        }
+
+	public function execute() {
+		list( $messages, $tests ) = $this->getMessagesAndTests();
+		$this->writeJavascriptFile( $messages, $tests, "spec/mediawiki.language.parser.spec.data.js" );
+	}
+
+	private function getMessagesAndTests() {
+		$messages = array();
+		$tests = array();
+		$wfMsgExtOptions = array( 'parsemag' );
+		foreach ( array( 'en', 'fr', 'ar', 'jp', 'zh' ) as $languageCode ) {
+			$wfMsgExtOptions['language'] = $languageCode;
+			foreach ( self::$keyToTestArgs as $key => $testArgs ) {
+				foreach ($testArgs as $args) {
+					// get the raw template, without any transformations
+					$template = wfMsgGetKey( $key, /* useDb */ true, $languageCode, /* transform */ false );
+
+					// get the magic-parsed version with args
+					$wfMsgExtArgs = array_merge( array( $key, $wfMsgExtOptions ), $args );
+					$result = call_user_func_array( 'wfMsgExt', $wfMsgExtArgs ); 
+
+					// record the template, args, language, and expected result
+					// fake multiple languages by flattening them together	
+					$langKey = $languageCode . '_' . $key;
+					$messages[ $langKey ] = $template;
+					$tests[] = array( 
+						'name' => $languageCode . " " . $key . " " . join( ",", $args ),
+						'key' => $langKey,
+						'args' => $args, 
+						'result' => $result,
+						'lang' => $languageCode
+					);
+				}
+			}
+		}
+		return array( $messages, $tests );
+	}
+
+	private function writeJavascriptFile( $messages, $tests, $dataSpecFile ) {
+		global $argv;
+		$arguments = count($argv) ? $argv : $_SERVER[ 'argv' ];
+
+		$json = new Services_JSON;
+		$json->pretty = true;
+		$javascriptPrologue = "// This file stores the results from the PHP parser for certain messages and arguments,\n"
+				      . "// so we can test the equivalent Javascript libraries.\n"
+				      . '// Last generated with ' . join(' ', $arguments) . ' at ' . gmdate('c') . "\n\n";
+		$javascriptMessages = "mediaWiki.messages.set( " . $json->encode( $messages, true ) . " );\n";
+		$javascriptTests = 'var jasmineMsgSpec = ' . $json->encode( $tests, true ) . ";\n";
+
+		$fp = fopen( $dataSpecFile, 'w' );
+		if ( !$fp ) {
+			die( "couldn't open $dataSpecFile for writing" );
+		}
+		$success = fwrite( $fp, $javascriptPrologue . $javascriptMessages . $javascriptTests );
+		if ( !$success ) { 
+			die( "couldn't write to $dataSpecFile" );
+		}
+		$success = fclose( $fp );
+		if ( !$success ) {
+			die( "couldn't close $dataSpecFile" );
+		}
+	}
+}
+
+$maintClass = "MakeLanguageSpec";
+require_once( "$maintenanceDir/doMaintenance.php" );
+
+
+
