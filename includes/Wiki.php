f518f7529bee9c2c80a9c83a931fe48719381b32
Chad Horohoe <demon@users.mediawiki.org>
2011-12-15 16:52:51 +0000

    Get rid of a few more $wgTitles

--- includes/Wiki.php
+++ includes/Wiki.php
@@ -593,11 +593,11 @@ class MediaWiki {
 			return;
 		}
 
-		if ( $wgUseFileCache && $wgTitle->getNamespace() >= 0 ) {
+		if ( $wgUseFileCache && $this->getTitle()->getNamespace() >= 0 ) {
 			wfProfileIn( 'main-try-filecache' );
 			if ( HTMLFileCache::useFileCache( $this->context ) ) {
 				/* Try low-level file cache hit */
-				$cache = HTMLFileCache::newFromTitle( $wgTitle, $action );
+				$cache = HTMLFileCache::newFromTitle( $this->getTitle(), $action );
 				if ( $cache->isCacheGood( /* Assume up to date */ ) ) {
 					/* Check incoming headers to see if client has this cached */
 					$timestamp = $cache->cacheTimestamp();
@@ -605,7 +605,7 @@ class MediaWiki {
 						$cache->loadFromFileCache( $this->context );
 					}
 					# Do any stats increment/watchlist stuff
-					$article = WikiPage::factory( $wgTitle );
+					$article = WikiPage::factory( $this->getTitle() );
 					$article->doViewUpdates( $user );
 					# Tell OutputPage that output is taken care of
 					$this->context->getOutput()->disable();
