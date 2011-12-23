29c8873d9367b0e44631d0ee6bebc4b88d41c889
Jan Gerber <j@users.mediawiki.org>
2011-12-23 09:30:02 +0000

    Use getPath, all operations are on filerepo level no need to get a local path. Follow up r107064

--- includes/upload/UploadFromChunks.php
+++ includes/upload/UploadFromChunks.php
@@ -56,7 +56,7 @@ class UploadFromChunks extends UploadFromFile {
 		$this->mFileKey = $this->mLocalFile->getFileKey();
 
 		// Output a copy of this first to chunk 0 location:
-		$status = $this->outputChunk( $this->mLocalFile->getLocalRefPath() );
+		$status = $this->outputChunk( $this->mLocalFile->getPath() );
 		
 		// Update db table to reflect initial "chunk" state 
 		$this->updateChunkStatus();
