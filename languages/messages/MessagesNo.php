8c49d4cebc574b9259070bfa634b6ef3806c71f2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-12-25 09:27:07 +0000

    Rename no to nb (step 2/4): Re-add MessagesNo.php. Falls back to 'nb'.

---
+++ languages/messages/MessagesNo.php
@@ -0,0 +1,12 @@
+<?php
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ * @comment Deprecated language code. Falls back to 'nb'.
+ */
+
+$fallback = 'nb';
