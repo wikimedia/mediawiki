e46fed83d09138b73cd652872fdf991d436a6c9f
Markus Glaser <mglaser@users.mediawiki.org>
2012-01-03 00:53:01 +0000

    Use FileOp when file exists ($this->backend has a null reference)

--- includes/filerepo/backend/FileBackend.php
+++ includes/filerepo/backend/FileBackend.php
@@ -585,12 +585,12 @@ abstract class FileBackend extends FileBackendBase {
 	 */
 	protected function doMoveInternal( array $params ) {
 		// Copy source to dest
-		$status = $this->backend->doCopyInternal( $params );
+		$status = $this->copy( $params );
 		if ( !$status->isOK() ) {
 			return $status;
 		}
 		// Delete source (only fails due to races or medium going down)
-		$status->merge( $this->backend->doDeleteInternal( array( 'src' => $params['src'] ) ) );
+		$status->merge( $this->delete( array( 'src' => $params['src'] ) ) );
 		$status->setResult( true, $status->value ); // ignore delete() errors
 		return $status;
 	}
