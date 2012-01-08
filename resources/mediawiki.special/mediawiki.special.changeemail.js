96ec13479a913bc63fb5679f9cc6c00785ab095c
Krinkle <krinkle@users.mediawiki.org>
2012-01-08 00:22:44 +0000

    Re-do svn copy from r106516 preserving history

--- resources/mediawiki.special/mediawiki.special.changeemail.js
+++ resources/mediawiki.special/mediawiki.special.changeemail.js
@@ -2,6 +2,7 @@
  * JavaScript for Special:ChangeEmail
  */
 ( function( $, mw ) {
+
 /**
  * Given an email validity status (true, false, null) update the label CSS class
  */
@@ -26,13 +27,14 @@ var updateMailValidityLabel = function( mail ) {
 // Lame tip to let user know if its email is valid. See bug 22449
 // Only bind once for 'blur' so that the user can fill it in without errors
 // After that look at every keypress for direct feedback if it was invalid onblur
-$( '#wpNewEmail' ).one( 'blur', function() {
+$( '#mw-input-wpemailaddress' ).one( 'blur', function() {
 	if ( $( '#mw-emailaddress-validity' ).length === 0 ) {
-		$(this).after( '<label for="wpNewEmail" id="mw-emailaddress-validity"></label>' );
+		$(this).after( '<label for="mw-input-wpemailaddress" id="mw-emailaddress-validity"></label>' );
 	}
 	updateMailValidityLabel( $(this).val() );
 	$(this).keyup( function() {
 		updateMailValidityLabel( $(this).val() );
 	} );
 } );
+
 } )( jQuery, mediaWiki );
