2880a7b723fee02b911365cdb6fb4c7095a20760
Santhosh Thottingal <santhosh@users.mediawiki.org>
2012-01-09 12:46:53 +0000

    Cleanup the convertPLural method for Lithuanian(lt) Add phpunit test cases.

--- languages/classes/LanguageLt.php
+++ languages/classes/LanguageLt.php
@@ -12,6 +12,7 @@ class LanguageLt extends Language {
 	*/
 
 	/**
+	 * Lithuanian plural forms as per http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html#lt
 	 * @param $count int
 	 * @param $forms array
 	 *
@@ -19,13 +20,10 @@ class LanguageLt extends Language {
 	 */
 	function convertPlural( $count, $forms ) {
 		if ( !count( $forms ) ) { return ''; }
-
-		// if no number with word, then use $form[0] for singular and $form[1] for plural or zero
-		if ( count( $forms ) === 2 ) return $count == 1 ? $forms[0] : $forms[1];
-
 		$forms = $this->preConvertPlural( $forms, 3 );
-
+		// Form[0] if n mod 10 is 1 and n mod 100 not in 11..19;
 		if ( $count % 10 == 1 && $count % 100 != 11 ) return $forms[0];
+		// Forms[1] if n mod 10 in 2..9 and n mod 100 not in 11..19;
 		if ( $count % 10 >= 2 && ( $count % 100 < 10 || $count % 100 >= 20 ) ) return $forms[1];
 		return $forms[2];
 	}
