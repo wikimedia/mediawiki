3156c71bd2b7111bdf4dc62814e6f6e4d6cebecd
Aaron Schulz <aaron@users.mediawiki.org>
2012-01-10 18:05:36 +0000

    Reverted r108502, r108504 per CR

--- maintenance/archives/patch-drop_old_text_indexes.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX old_namespace ON /*_*/text;
-DROP INDEX old_timestamp ON /*_*/text;
-DROP INDEX name_title_timestamp ON /*_*/text;
-DROP INDEX user_timestamp ON /*_*/text;
-DROP INDEX usertext_timestamp ON /*_*/text;
