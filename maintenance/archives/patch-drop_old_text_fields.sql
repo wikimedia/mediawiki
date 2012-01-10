df1d4e7e2256efd72d2c98edffefc38a5bbc0dc2
Sam Reed <reedy@users.mediawiki.org>
2012-01-10 15:04:05 +0000

    Cleanup "text" (nee old) table, dropping old indexes and fields
    Add oldwatchlist to dropAncientTables

---
+++ maintenance/archives/patch-drop_old_text_fields.sql
@@ -0,0 +1,8 @@
+ALTER TABLE /*_*/text DROP COLUMN old_namespace;
+ALTER TABLE /*_*/text DROP COLUMN old_title;
+ALTER TABLE /*_*/text DROP COLUMN old_comment;
+ALTER TABLE /*_*/text DROP COLUMN old_user;
+ALTER TABLE /*_*/text DROP COLUMN old_user_text;
+ALTER TABLE /*_*/text DROP COLUMN old_timestamp;
+ALTER TABLE /*_*/text DROP COLUMN old_minor_edit;
+ALTER TABLE /*_*/text DROP COLUMN inverse_timestamp;
---
+++ maintenance/archives/patch-drop_old_text_indexes.sql
@@ -0,0 +1,5 @@
+DROP INDEX old_namespace ON /*_*/text;
+DROP INDEX old_timestamp ON /*_*/text;
+DROP INDEX name_title_timestamp ON /*_*/text;
+DROP INDEX user_timestamp ON /*_*/text;
+DROP INDEX usertext_timestamp ON /*_*/text;
