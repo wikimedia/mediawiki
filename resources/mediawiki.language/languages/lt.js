0047c07b39b6a92f6aaa161eba1dca126f81843d
Santhosh Thottingal <santhosh@users.mediawiki.org>
2012-01-16 10:05:48 +0000

    reintroduce the shortcut evaluation removed in r108403 and r108405

--- resources/mediawiki.language/languages/lt.js
+++ resources/mediawiki.language/languages/lt.js
@@ -3,6 +3,10 @@
  */
 
 mediaWiki.language.convertPlural = function( count, forms ) {
+	// if the number is not mentioned in message, then use $form[0] for singular and $form[1] for plural or zero
+	if ( forms.length == 2 ) {
+		return count == 1 ? forms[0] : forms[1];
+	}
 	forms = mediaWiki.language.preConvertPlural( forms, 3 );
 	if ( count % 10 == 1 && count % 100 != 11 ) {
 		return forms[0];
