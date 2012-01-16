4c140d3306c209e28e7473afb161fd694d828a9a
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2012-01-16 21:29:05 +0000

    Only run some MWDebug code when really needed: * Add the mediawiki.debug module from OutputPage::addDefaultModules() along with other modules * Get the request information when building the JS output instead of in Setup.php

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -2440,7 +2440,7 @@ $templates
 	 * Add the default ResourceLoader modules to this object
 	 */
 	private function addDefaultModules() {
-		global $wgIncludeLegacyJavaScript, $wgUseAjax, $wgAjaxWatch, $wgEnableMWSuggest;
+		global $wgIncludeLegacyJavaScript, $wgDebugToolbar, $wgUseAjax, $wgAjaxWatch, $wgEnableMWSuggest;
 
 		// Add base resources
 		$this->addModules( array(
@@ -2451,6 +2451,10 @@ $templates
 		) );
 		if ( $wgIncludeLegacyJavaScript ){
 			$this->addModules( 'mediawiki.legacy.wikibits' );
+		}
+
+		if ( $wgDebugToolbar ) {
+			$this->addModules( 'mediawiki.debug' );
 		}
 
 		// Add various resources if required
