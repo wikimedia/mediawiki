c1a127d8a9d135e0dce6f20bce189eb0ccd7749b
Santhosh Thottingal <santhosh@users.mediawiki.org>
2012-01-17 08:54:39 +0000

    Make the QunitTestResources in sync with qunit/index.html. JqueryMsg tests were not preset in QunitTestResources.

--- tests/qunit/QUnitTestResources.php
+++ tests/qunit/QUnitTestResources.php
@@ -24,6 +24,7 @@ return array(
 			'tests/qunit/suites/resources/mediawiki/mediawiki.user.test.js',
 			'tests/qunit/suites/resources/mediawiki/mediawiki.util.test.js',
 			'tests/qunit/suites/resources/mediawiki.special/mediawiki.special.recentchanges.test.js',
+			"tests/qunit/suites/resources/mediawiki/mediawiki.jqueryMsg.test.js",
 		),
 		'dependencies' => array(
 			'jquery.autoEllipsis',
@@ -43,6 +44,7 @@ return array(
 			'mediawiki.user',
 			'mediawiki.util',
 			'mediawiki.special.recentchanges',
+			'mediawiki.jqueryMsg',
 		),
 	)
 );
