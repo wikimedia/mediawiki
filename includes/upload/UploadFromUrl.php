af198296ef37eba89df1723075e84f58a83d6c9f
Sam Reed <reedy@users.mediawiki.org>
2012-01-22 17:33:44 +0000

    Revert feature out of r109562, r109564, r109570
    Leaving typo/brace fixes and other minor code improvements

--- includes/upload/UploadFromUrl.php
+++ includes/upload/UploadFromUrl.php
@@ -38,28 +38,6 @@ class UploadFromUrl extends UploadBase {
 	}
 
 	/**
-	 * Checks whether the URL is for an allowed host
-	 *
-	 * @param $url string
-	 * @return bool
-	 */
-	public static function isAllowedHost( $url ) {
-		global $wgCopyUploadsDomains;
-		if ( !count( $wgCopyUploadsDomains ) ) {
-			return true;
-		}
-		$valid = false;
-		$parsedUrl = wfParseUrl( $url );
-		foreach( $wgCopyUploadsDomains as $domain ) {
-			if ( $parsedUrl['host'] === $domain ) {
-				$valid = true;
-				break;
-			}
-		}
-		return $valid;
-	}
-
-	/**
 	 * Entry point for API upload
 	 *
 	 * @param $name string
@@ -124,9 +102,6 @@ class UploadFromUrl extends UploadBase {
 			return Status::newFatal( 'http-invalid-url' );
 		}
 
-		if( !self::isAllowedHost( $this->mUrl ) ) {
-			return Status::newFatal( 'upload-copy-upload-invalid-domain' );
-		}
 		if ( !$this->mAsync ) {
 			return $this->reallyFetchFile();
 		}
