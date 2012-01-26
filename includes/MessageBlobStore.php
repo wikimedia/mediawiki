59b37461ce6397320422149c181b23a37ba8e134
Neil Kandalgaonkar <neilk@users.mediawiki.org>
2012-01-26 02:15:04 +0000

    sanitize outgoing messages

--- includes/MessageBlobStore.php
+++ includes/MessageBlobStore.php
@@ -350,7 +350,12 @@ class MessageBlobStore {
 		$messages = array();
 
 		foreach ( $module->getMessages() as $key ) {
-			$messages[$key] = wfMsgExt( $key, array( 'language' => $lang ) );
+			$messages[$key] =
+				Sanitizer::normalizeCharReferences(
+					Sanitizer::removeHTMLtags(
+						wfMsgExt( $key, array( 'language' => $lang ) )
+					)
+				);
 		}
 
 		return FormatJson::encode( (object)$messages );
