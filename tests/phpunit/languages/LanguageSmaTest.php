38d8a2b8e0e9c3cac775be46c1074d64b619c320
Amir E. Aharoni <amire80@users.mediawiki.org>
2012-02-01 04:53:55 +0000

    Adding plural tests for more languages.

---
+++ tests/phpunit/languages/LanguageSmaTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Amir E. Aharoni
+ * @copyright Copyright © 2012, Amir E. Aharoni
+ * @file
+ */
+
+/** Tests for MediaWiki languages/classes/LanguageSma.php */
+class LanguageSmaTest extends MediaWikiTestCase {
+	private $lang;
+
+	function setUp() {
+		$this->lang = Language::factory( 'sma' );
+	}
+	function tearDown() {
+		unset( $this->lang );
+	}
+
+	/** @dataProvider providerPluralThreeForms */
+	function testPluralThreeForms( $result, $value ) {
+		$forms = array( 'one', 'two', 'other' );
+		$this->assertEquals( $result, $this->lang->convertPlural( $value, $forms ) );
+	}
+
+	function providerPluralThreeForms() {
+		return array (
+			array( 'other', 0 ),
+			array( 'one',   1 ),
+			array( 'two',   2 ),
+			array( 'other', 3 ),
+		);
+	}
+
+	/** @dataProvider providerPlural */
+	function testPlural( $result, $value ) {
+		$forms = array( 'one', 'other' );
+		$this->assertEquals( $result, $this->lang->convertPlural( $value, $forms ) );
+	}
+
+	function providerPlural() {
+		return array (
+			array( 'other', 0 ),
+			array( 'one',   1 ),
+			array( 'other', 2 ),
+			array( 'other', 3 ),
+		);
+	}
+}
