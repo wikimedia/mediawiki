9d6997fa3df3c42bc480c8ef6cc6ef9e7eee5c2a
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2012-02-05 15:26:41 +0000

    Fix call to deprecated to fix test since it was throwing errors

--- tests/phpunit/skins/SideBarTest.php
+++ tests/phpunit/skins/SideBarTest.php
@@ -37,7 +37,7 @@ class SideBarTest extends MediaWikiLangTestCase {
 		parent::setUp();
 		$this->initMessagesHref();
 		$this->skin = new SkinTemplate();
-		$this->skin->getContext()->setLang( Language::factory( 'en' ) );
+		$this->skin->getContext()->setLanguage( Language::factory( 'en' ) );
 	}
 	function tearDown() {
 		parent::tearDown();
