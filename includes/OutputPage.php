wikimedia/mediawiki

commit 2e9a7db0a413b3992027a9e3dbf6f5d5a661b75e

Move embedded ResourceLoader modules to the <head> * Embed "private" ResourceLoader modules in the <head> before the first loader link, instead of in the <body> after the last loader link * Wow, was in the opposite place where it should've been :D * Fixes bug 30914
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@107534 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/OutputPage.php
@@ -2575,7 +2575,9 @@ $templates
 				continue;
 			}
 
-			// Support inlining of private modules if configured as such
+			// Support inlining of private modules if configured as such. Note that these
+			// modules should be loaded from getHeadScripts() before the first loader call.
+			// Otherwise other modules can't properly use them as dependencies (bug 30914)
 			if ( $group === 'private' && $wgResourceLoaderInlinePrivateModules ) {
 				if ( $only == ResourceLoaderModule::TYPE_STYLES ) {
 					$links .= Html::inlineStyle(
@@ -2661,6 +2663,10 @@ $templates
 			)
 		);
 
+		// Load embeddable private modules before any loader links
+		$embedScripts = array( 'user.options', 'user.tokens' );
+		$scripts .= $this->makeResourceLoaderLink( $embedScripts, ResourceLoaderModule::TYPE_COMBINED );
+
 		// Script and Messages "only" requests marked for top inclusion
 		// Messages should go first
 		$scripts .= $this->makeResourceLoaderLink( $this->getModuleMessages( true, 'top' ), ResourceLoaderModule::TYPE_MESSAGES );
@@ -2708,7 +2714,7 @@ $templates
 		// Legacy Scripts
 		$scripts .= "\n" . $this->mScripts;
 
-		$userScripts = array( 'user.options', 'user.tokens' );
+		$userScripts = array();
 
 		// Add site JS if enabled
 		if ( $wgUseSiteJs ) {

RELEASE-NOTES-1.19
@@ -197,6 +197,8 @@ production.
 * (bug 30711) When adding a new section to a page with section=new, the text is
   now always added to the current version of the page
 * (bug 31719) Recognize &ns_svg; as the svg namespace when extracting width
+* (bug 30914) Embeddable ResourceLoader modules (user.options, user.tokens)
+  should be loaded in <head> for proper dependency resolution
 
 === API changes in 1.19 ===
 * (bug 19838) siprop=interwikimap can now use the interwiki cache.

includes/resourceloader/ResourceLoader.php
@@ -913,8 +913,7 @@ class ResourceLoader {
 	 * @return string
 	 */
 	public static function makeLoaderConditionalScript( $script ) {
-		$script = str_replace( "\n", "\n\t", trim( $script ) );
-		return "if(window.mw){\n\t$script\n}\n";
+		return "if(window.mw){\n".trim( $script )."\n}";
 	}
 
 	/**

commit 253613a3358b62055cbb3fb9f7d8baac1ae0ce78

(bug 34289) user.options CSS loaded twice. Caused by r107534. Made embedded private modules scripts-only, because the styles for user.options are included separately in buildCssLinks(), because they need to be applied without JavaScript and because they need to respect $wgAllowUserCssPrefs. No RELEASE-NOTES because I want to merge this into 1.19 as a regression fix

includes/OutputPage.php
@@ -2682,7 +2682,7 @@ $templates
 
 		// Load embeddable private modules before any loader links
 		$embedScripts = array( 'user.options', 'user.tokens' );
-		$scripts .= $this->makeResourceLoaderLink( $embedScripts, ResourceLoaderModule::TYPE_COMBINED );
+		$scripts .= $this->makeResourceLoaderLink( $embedScripts, ResourceLoaderModule::TYPE_SCRIPTS );
 
 		// Script and Messages "only" requests marked for top inclusion
 		// Messages should go first