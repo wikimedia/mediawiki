4fb835398d293981e96feba2126fae8681034d8d
Mark A. Hershberger <mah@users.mediawiki.org>
2012-02-10 19:29:28 +0000

    Fixes Bug 18704 - Add an unique CSS class or ID to the tagfilter table row at RecentChanges Patch from Jarry1250

--- includes/ChangeTags.php
+++ includes/ChangeTags.php
@@ -209,8 +209,8 @@ class ChangeTags {
 		if ( !$wgUseTagFilter || !count( self::listDefinedTags() ) )
 			return $fullForm ? '' : array();
 
-		$data = array( Html::rawElement( 'label', array( 'for' => 'tagfilter' ), wfMsgExt( 'tag-filter', 'parseinline' ) ),
-			Xml::input( 'tagfilter', 20, $selected ) );
+		$data = array( Html::rawElement( 'label', array( 'for' => 'tagfilter' ), wfMsgExt( 'tag-filter', 'parseinline' ),
+				Xml::input( 'tagfilter', 20, $selected, array( 'class' => 'tagfilter-input' ) ) ) );
 
 		if ( !$fullForm ) {
 			return $data;
@@ -220,6 +220,7 @@ class ChangeTags {
 		$html .= "\n" . Xml::element( 'input', array( 'type' => 'submit', 'value' => wfMsg( 'tag-filter-submit' ) ) );
 		$html .= "\n" . Html::hidden( 'title', $title->getPrefixedText() );
 		$html = Xml::tags( 'form', array( 'action' => $title->getLocalURL(), 'method' => 'get' ), $html );
+		$html = Xml::tags( 'form', array( 'action' => $title->getLocalURL(), 'class' => 'tagfilter-form', 'method' => 'get' ), $html );
 
 		return $html;
 	}
