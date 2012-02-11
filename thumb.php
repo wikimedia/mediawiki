f9e524e941c43e82c9678a1e355944679ee806dc
Sam Reed <reedy@users.mediawiki.org>
2012-02-11 15:14:15 +0000

    Drag in a change noticed in wmf branches

--- thumb.php
+++ thumb.php
@@ -124,7 +124,7 @@ function wfStreamThumb( array $params ) {
 
 	// Check permissions if there are read restrictions
 	if ( !in_array( 'read', User::getGroupPermissions( array( '*' ) ), true ) ) {
-		if ( !$img->getTitle()->userCan( 'read' ) ) {
+		if ( !$img->getTitle() || !$img->getTitle()->userCan( 'read' ) ) {
 			wfThumbError( 403, 'Access denied. You do not have permission to access ' .
 				'the source file.' );
 			wfProfileOut( __METHOD__ );
