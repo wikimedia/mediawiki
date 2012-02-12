6067d45aa6388e9772bb3a29f27d7abc17c45405
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2012-02-12 15:37:29 +0000

    Fix exception's text: use __construct() instead of class name for constructor and WikiPage::doEditUpdates() instead of Article::editUpdates()

--- includes/LinksUpdate.php
+++ includes/LinksUpdate.php
@@ -57,8 +57,8 @@ class LinksUpdate {
 		$this->mDb = wfGetDB( DB_MASTER );
 
 		if ( !is_object( $title ) ) {
-			throw new MWException( "The calling convention to LinksUpdate::LinksUpdate() has changed. " .
-				"Please see Article::editUpdates() for an invocation example.\n" );
+			throw new MWException( "The calling convention to LinksUpdate::__construct() has changed. " .
+				"Please see WikiPage::doEditUpdates() for an invocation example.\n" );
 		}
 		$this->mTitle = $title;
 		$this->mId = $title->getArticleID();
