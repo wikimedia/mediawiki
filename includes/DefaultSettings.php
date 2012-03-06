ac76fdd5a83ad2555a056844f6853bf131ddd170
Sam Reed <reedy@users.mediawiki.org>
2012-03-06 21:06:12 +0000

    Bump version, tweak RELEASE-NOTES-1.19

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19alpha';
+$wgVersion = '1.19.0beta1';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
