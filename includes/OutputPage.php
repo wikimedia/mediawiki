1e68eb8c0db78c94ecab66736d6b2849f790be19
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2012-03-08 20:06:49 +0000

    * (bug 35014) Fix for r100529: including a special page no longer sets the page's title to the included page

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -3432,7 +3432,7 @@ $templates
 	 * @param $args array
 	 */
 	public function addWikiMsgArray( $name, $args ) {
-		$this->addWikiText( $this->msg( $name, $args )->plain() );
+		$this->addHTML( $this->msg( $name, $args )->parseAsBlock() );
 	}
 
 	/**
