48e70666220054c15f2095ede6c16197c2bc78f6
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2012-03-09 20:26:08 +0000

    No need to create a new Revision object if we already have one available

--- includes/Article.php
+++ includes/Article.php
@@ -1093,7 +1093,12 @@ class Article extends Page {
 			$extraParams['unhide'] = 1;
 		}
 
-		$revision = Revision::newFromId( $oldid );
+		if ( $this->mRevision && $this->mRevision->getId() === $oldid ) {
+			$revision = $this->mRevision;
+		} else {
+			$revision = Revision::newFromId( $oldid );
+		}
+
 		$timestamp = $revision->getTimestamp();
 
 		$current = ( $oldid == $this->mPage->getLatest() );
