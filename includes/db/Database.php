d6658019a8bf2f39da40d66ec79c29e8d8249087
Sam Reed <reedy@users.mediawiki.org>
2012-03-11 23:40:33 +0000

    Bug 35162 - Change in Database api break extension MSSQLBackCompat
    $canonicalDBTypes is probably superfluous information, but doesn't harm as a shortcut
    
    Ping r108051

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -677,7 +677,7 @@ abstract class DatabaseBase implements DatabaseType {
 		$dbType = strtolower( $dbType );
 		$class = 'Database' . ucfirst( $dbType );
 
-		if( in_array( $dbType, $canonicalDBTypes ) ) {
+		if( in_array( $dbType, $canonicalDBTypes ) || ( class_exists( $class ) && is_subclass_of( $class, 'DatabaseBase' ) ) ) {
 			return new $class(
 				isset( $p['host'] ) ? $p['host'] : false,
 				isset( $p['user'] ) ? $p['user'] : false,
@@ -686,8 +686,6 @@ abstract class DatabaseBase implements DatabaseType {
 				isset( $p['flags'] ) ? $p['flags'] : 0,
 				isset( $p['tablePrefix'] ) ? $p['tablePrefix'] : 'get from global'
 			);
-		} elseif ( class_exists( $class ) && is_subclass_of( $class, 'DatabaseBase' ) ) {
-			return new $class( $p );
 		} else {
 			return null;
 		}
