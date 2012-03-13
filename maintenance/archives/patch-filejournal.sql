85fe21f27380bdf44af127451fe6f27ea7db3e3f
Aaron Schulz <aaron@users.mediawiki.org>
2012-03-13 01:46:33 +0000

    [FileBackend] Filejournal class
    * Added FileJournal class to log file changes for file backends. This can be used for migrations (like moving to Swift), syncing mirror repos, consistency checks, finishing/reverting operation batches, and such. The default journal is the "null" journal, which simply does nothing.
    * Added the optional schema change required for using the DBFileJournal (MySQL, SQLite).
    
    Patchset2:
    updated FileBackend constructor documentation
    squash change 3321 (r113742)
    
    Change-Id: I33c9f9a598ba1f164c862b9dc3c718f9172db02b

---
+++ maintenance/archives/patch-filejournal.sql
@@ -0,0 +1,24 @@
+-- File backend operation journal
+CREATE TABLE /*_*/filejournal (
+  -- Unique ID for each file operation
+  fj_id bigint unsigned NOT NULL PRIMARY KEY auto_increment,
+  -- UUID of the batch this operation belongs to
+  fj_batch_uuid varbinary(32) NOT NULL,
+  -- The registered file backend name
+  fj_backend varchar(255) NOT NULL,
+  -- The storage path that was affected (may be internal paths)
+  fj_path blob NOT NULL,
+  -- SHA-1 file path hash in base-36
+  fj_path_sha1 varbinary(32) NOT NULL default '',
+  -- Primitive operation description (create/update/delete)
+  fj_op varchar(16) NOT NULL default '',
+  -- SHA-1 file content hash in base-36
+  fj_new_sha1 varbinary(32) NOT NULL default '',
+  -- Timestamp of the batch operation
+  fj_timestamp varbinary(14) NOT NULL default ''
+);
+
+CREATE INDEX /*i*/fj_batch_id ON /*_*/filejournal (fj_batch_uuid,fj_id);
+CREATE INDEX /*i*/fj_path_id ON /*_*/filejournal (fj_path_sha1,fj_id);
+CREATE INDEX /*i*/fj_new_sha1 ON /*_*/filejournal (fj_new_sha1,fj_id);
+CREATE INDEX /*i*/fj_timestamp ON /*_*/filejournal (fj_timestamp);
