2330bd514af1b316bf48056a0bf96bfe133773e4
Aaron Schulz <aaron@users.mediawiki.org>
2012-03-14 21:30:26 +0000

    [FileRepo] * Added getReadOnlyReason() function to FileRepo to check read-only status. Added such checks to the File object functions that mutate files in storage. This should make read-only mode more tolerable (which is needed at least briefly when switching backends). * Added lock()/unlock() calls to File restore() function. * Use proper isOK() accessor for Status objects.
    Change-Id: I7368912b01f7fd1ec34125d12c0844388cbb3b2d

--- includes/filerepo/FileRepo.php
+++ includes/filerepo/FileRepo.php
@@ -120,9 +120,18 @@ class FileRepo {
 	}
 
 	/**
+	 * Get an explanatory message if this repo is read-only
+	 *
+	 * @return string|bool Returns false if the repo is not read-only
+	 */
+	public function getReadOnlyReason() {
+		return $this->backend->getReadOnlyReason();
+	}
+
+	/**
 	 * Prepare a single zone or list of zones for usage.
 	 * See initDeletedDir() for additional setup needed for the 'deleted' zone.
-	 * 
+	 *
 	 * @param $doZones Array Only do a particular zones
 	 * @return Status
 	 */
