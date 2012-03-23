a111efd81ae3507b0b8a5e2d8bd75ed482d5c0d3
jarry1250 <jarry1250@gmail.com>
2012-03-23 19:26:31 +0000

    Resolve bug #18704 ("Add an unique CSS class or ID to the tagfilter table row at RecentChanges") / followup previous attempt (r111186)
    Change-Id: If11e2351ae7553da9ce010fce30354dbaf82b2dd

--- includes/specials/SpecialRecentchanges.php
+++ includes/specials/SpecialRecentchanges.php
@@ -568,15 +568,15 @@ class SpecialRecentChanges extends IncludableSpecialPage {
 		$submit = ' ' . Xml::submitbutton( wfMsg( 'allpagessubmit' ) );
 
 		$out = Xml::openElement( 'table', array( 'class' => 'mw-recentchanges-table' ) );
-		foreach( $extraOpts as $optionRow ) {
+		foreach( $extraOpts as $name=>$optionRow ) {
 			# Add submit button to the last row only
 			++$count;
-			$addSubmit = $count === $extraOptsCount ? $submit : '';
+			$addSubmit = ( $count === $extraOptsCount ) ? $submit : '';
 
 			$out .= Xml::openElement( 'tr' );
 			if( is_array( $optionRow ) ) {
 				$out .= Xml::tags( 'td', array( 'class' => 'mw-label' ), $optionRow[0] );
-				$out .= Xml::tags( 'td', array( 'class' => 'mw-input' ), $optionRow[1] . $addSubmit );
+				$out .= Xml::tags( 'td', array( 'class' => 'mw-label ' . $name . '-label' ), $optionRow[0] );
 			} else {
 				$out .= Xml::tags( 'td', array( 'class' => 'mw-input', 'colspan' => 2 ), $optionRow . $addSubmit );
 			}
