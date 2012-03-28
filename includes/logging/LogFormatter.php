c945156ddedabf8c4f6c947cadb7654f7dba6398
Max Semenik <maxsem.wiki@gmail.com>
2012-03-29 02:39:32 +0400

    (bug 35565) Special:Log/patrol doesn't indicate whether patrolling was automatic
    Change-Id: Ia4d9ffcfda719fd94ac5e01f0e35c4c5823d7d72

--- includes/logging/LogFormatter.php
+++ includes/logging/LogFormatter.php
@@ -662,7 +662,6 @@ class PatrolLogFormatter extends LogFormatter {
 
 	protected function getMessageParameters() {
 		$params = parent::getMessageParameters();
-		$newParams = array_slice( $params, 0, 3 );
 
 		$target = $this->entry->getTarget();
 		$oldid = $params[3];
@@ -680,8 +679,8 @@ class PatrolLogFormatter extends LogFormatter {
 			$revlink = htmlspecialchars( $revision );
 		}
 
-		$newParams[3] = Message::rawParam( $revlink );
-		return $newParams;
+		$params[3] = Message::rawParam( $revlink );
+		return $params;
 	}
 }
 
