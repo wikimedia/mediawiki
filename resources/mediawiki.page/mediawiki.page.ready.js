b7d9c0a253d2bdbb048934423a70583e21a0f80b
Akshay Chugh <chughakshay16@gmail.com>
2012-03-30 17:49:00 +0530

    bug 21119) patch for the requested feature in bug 21119 Patch set 2 : Fixed the issues which were made in the comments and have also made some new changes to overcome some new issues in the code. Change-Id: I44a447a1d89e06c82ec0045072e4954d20be8b75

--- resources/mediawiki.page/mediawiki.page.ready.js
+++ resources/mediawiki.page/mediawiki.page.ready.js
@@ -17,7 +17,29 @@ jQuery( document ).ready( function( $ ) {
 
 	/* Enable CheckboxShiftClick */
 	$( 'input[type=checkbox]:not(.noshiftselect)' ).checkboxShiftClick();
-
+	
+	/* Enable toggle category link if at all the page has hidden category links */
+	if($("#catlinks").has("#mw-hidden-catlinks"))
+	{
+			jQuery('<a/>', {
+			    id: 'mw-cat-toggle',
+			    href: 'javascript:void(0)',
+			    text: mw.msg('category-toggle','Show'),
+			    click: handler
+			}).prependTo('#catlinks');
+	}
+	var toggleCatBool = true;
+	function handler(event){
+		$("#mw-hidden-catlinks").toggle();
+		if(toggleCatBool)
+			$(this).text(mw.msg('category-toggle','Hide'));
+		else
+			$(this).text(mw.msg('category-toggle','Show'));
+		toggleCatBool=!toggleCatBool;
+		event.preventDefault();
+		
+	};
+	
 	/* Add accesskey hints to the tooltips */
 	mw.util.updateTooltipAccessKeys();
 
