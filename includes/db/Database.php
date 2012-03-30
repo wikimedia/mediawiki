wikimedia/mediawiki

commit 94633ff448e9253c5503124df6748be1fc26b8e2

PostgreSQL: Improve SQL error handling
After a query error, PostgreSQL transaction is aborted
until it's terminated or the query is closed. 

All further queries result in:

  ERROR: current transaction is aborted, commands ignored 
  until end of transaction block

Those subsequent errors are ignored by double fault handling in
DatabaseBase::reportQueryError but they cause all localization
of error messages to fail (unable to issue queries to message
tables) and errors lke


This resulted in a broken MediaWiki screen with

  <databaseerror>
  <dberrortext>

instead of localized error message.

We need to fully reset database connection because after
pg_connection_reset() various session parameters need to
be set again (like "search_path"), otherwise tables will not be
found.

   ERROR: relation "msg_resource" does not exist
   ERROR:  relation "l10n_cache" does not exist

includes/db/DatabasePostgres.php
@@ -159,7 +159,6 @@ class DatabasePostgres extends DatabaseBase {
 			return;
 		}
 
-		$this->close();
 		$this->mServer = $server;
 		$port = $wgDBport;
 		$this->mUser = $user;
@@ -177,10 +176,14 @@ class DatabasePostgres extends DatabaseBase {
 		if ( $port != false && $port != '' ) {
 			$connectVars['port'] = $port;
 		}
-		$connectString = $this->makeConnectionString( $connectVars, PGSQL_CONNECT_FORCE_NEW );
+		$this->connectString = $this->makeConnectionString( $connectVars, PGSQL_CONNECT_FORCE_NEW );
+		$this->reOpen();
+	}
 
+	function reOpen() {
+		$this->close();
 		$this->installErrorHandler();
-		$this->mConn = pg_connect( $connectString );
+		$this->mConn = pg_connect( $this->connectString );
 		$phpError = $this->restoreErrorHandler();
 
 		if ( !$this->mConn ) {
@@ -252,6 +255,13 @@ class DatabasePostgres extends DatabaseBase {
 		$this->mAffectedRows = null; // use pg_affected_rows(mLastResult)
 		return $this->mLastResult;
 	}
+
+	function reportQueryError( $error, $errno, $sql, $fname, $tempIgnore = false ) {
+		$this->rollback( __METHOD__ );
+		$this->reOpen();
+		parent::reportQueryError( $error, $errno, $sql, $fname, $tempIgnore );
+	}
+
 
 	function queryIgnore( $sql, $fname = 'DatabasePostgres::queryIgnore' ) {
 		return $this->query( $sql, $fname, true );

commit a358f21ffe39ce7c76a27300aa8002108399d331

(bug 27283) SqlBagOStuff breaks PostgreSQL txns
* SqlBagOStuff should not turn off automatic transaction
               control just like that. Keep previous
			   connection for PostgreSQL and SQLite

* 94633ff448e9253c5503124df6748be1fc26b8e2 introduced
               rollback on error to restore sanity
			   for the connection. Don't do this
			   if you are in the middle of INSERT IGNORE
			   and we have a savepoint open.

			   Making sure query syntax errors don't
			   get silenced by our "wonderful" implementation
			   of INSERT IGNORE is bug 35572.

Change-Id: I841b03895e1189c47307fefb1516c4c7c4102e25

RELEASE-NOTES-1.20
@@ -54,6 +54,7 @@ production.
 * (bug 18704) Add an unique CSS class or ID to the tagfilter table row at RecentChanges 
 * (bug 33689) Upgrade to 1.19 on Postgres fails due to incomplete query when
               trying to defer foreign key for externallinks
+* (bug 27283) SqlBagOStuff breaks PostgreSQL transactions
 
 === API changes in 1.20 ===
 * (bug 34316) Add ability to retrieve maximum upload size from MediaWiki API.

includes/db/Database.php
@@ -514,7 +514,11 @@ abstract class DatabaseBase implements DatabaseType {
 	 *   - DBO_PERSISTENT: use persistant database connection
 	 */
 	function setFlag( $flag ) {
+		global $wgDebugDBTransactions;
 		$this->mFlags |= $flag;
+		if ( ( $flag & DBO_TRX) & $wgDebugDBTransactions ) {
+			wfDebug("Implicit transactions are now  disabled.\n");
+		}
 	}
 
 	/**
@@ -523,7 +527,11 @@ abstract class DatabaseBase implements DatabaseType {
 	 * @param $flag: same as setFlag()'s $flag param
 	 */
 	function clearFlag( $flag ) {
+		global $wgDebugDBTransactions;
 		$this->mFlags &= ~$flag;
+		if ( ( $flag & DBO_TRX ) && $wgDebugDBTransactions ) {
+			wfDebug("Implicit transactions are now disabled.\n");
+		}
 	}
 
 	/**
@@ -588,15 +596,21 @@ abstract class DatabaseBase implements DatabaseType {
 	function __construct( $server = false, $user = false, $password = false, $dbName = false,
 		$flags = 0, $tablePrefix = 'get from global'
 	) {
-		global $wgDBprefix, $wgCommandLineMode;
+		global $wgDBprefix, $wgCommandLineMode, $wgDebugDBTransactions;
 
 		$this->mFlags = $flags;
 
 		if ( $this->mFlags & DBO_DEFAULT ) {
 			if ( $wgCommandLineMode ) {
 				$this->mFlags &= ~DBO_TRX;
+				if ( $wgDebugDBTransactions ) {
+					wfDebug("Implicit transaction open disabled.\n");
+				}
 			} else {
 				$this->mFlags |= DBO_TRX;
+				if ( $wgDebugDBTransactions ) {
+					wfDebug("Implicit transaction open enabled.\n");
+				}
 			}
 		}
 
@@ -852,8 +866,13 @@ abstract class DatabaseBase implements DatabaseType {
 			# that would delay transaction initializations to once connection
 			# is really used by application
 			$sqlstart = substr( $sql, 0, 10 ); // very much worth it, benchmark certified(tm)
-			if ( strpos( $sqlstart, "SHOW " ) !== 0 && strpos( $sqlstart, "SET " ) !== 0 )
+			if ( strpos( $sqlstart, "SHOW " ) !== 0 && strpos( $sqlstart, "SET " ) !== 0 ) {
+				global $wgDebugDBTransactions;
+				if ( $wgDebugDBTransactions ) {
+					wfDebug("Implicit transaction start.\n");
+				}
 				$this->begin( __METHOD__ . " ($fname)" );
+			}
 		}
 
 		if ( $this->debug() ) {
@@ -2868,7 +2887,7 @@ abstract class DatabaseBase implements DatabaseType {
 	}
 
 	/**
-	 * Begin a transaction, committing any previously open transaction
+	 * Begin a transaction
 	 *
 	 * @param $fname string
 	 */
@@ -3476,5 +3495,12 @@ abstract class DatabaseBase implements DatabaseType {
 	 */
 	public function setBigSelects( $value = true ) {
 		// no-op
+	}
+
+	/**
+	 * @since 1.19
+	 */
+	public function __toString() {
+		return (string)$this->mConn;
 	}
 }

includes/db/DatabasePostgres.php
@@ -106,14 +106,14 @@ class PostgresTransactionState {
 
 	static $WATCHED = array(
 		array(
-			"desc" => "Connection state changed from %s -> %s\n",  
+			"desc" => "%s: Connection state changed from %s -> %s\n",  
 			"states" => array(
 				PGSQL_CONNECTION_OK       => "OK",
 				PGSQL_CONNECTION_BAD      => "BAD"
 			)
 		),
 		array(
-			"desc" => "Transaction state changed from %s -> %s\n",
+			"desc" => "%s: Transaction state changed from %s -> %s\n",
 			"states" => array(
 				PGSQL_TRANSACTION_IDLE    => "IDLE",
 				PGSQL_TRANSACTION_ACTIVE  => "ACTIVE",
@@ -167,9 +167,83 @@ class PostgresTransactionState {
 
 	protected function log_changed( $old, $new, $watched ) {
 		wfDebug(sprintf($watched["desc"], 
+			$this->mConn,
 			$this->describe_changed( $old, $watched["states"] ),
 			$this->describe_changed( $new, $watched["states"] ))
 		);
+	}
+}
+
+/**
+ * Manage savepoints within a transaction
+ * @ingroup Database
+ * @since 1.19
+ */
+class SavepointPostgres {
+	/**
+	 * Establish a savepoint within a transaction
+	 */
+	protected $dbw;
+	protected $id;
+	protected $didbegin;
+
+	public function __construct ($dbw, $id) {
+		$this->dbw = $dbw;
+		$this->id = $id;
+		$this->didbegin = false;
+		/* If we are not in a transaction, we need to be for savepoint trickery */
+		if ( !$dbw->trxLevel() ) {
+				$dbw->begin( "FOR SAVEPOINT" );
+				$this->didbegin = true;
+		}
+	}
+
+	public function __destruct() {
+		if ( $this->didbegin ) {
+			$this->dbw->rollback();
+		}
+	}
+
+	public function commit() {
+		if ( $this->didbegin ) {
+			$this->dbw->commit();
+		}
+	}
+
+	protected function query( $keyword, $msg_ok, $msg_failed ) {
+		global $wgDebugDBTransactions;
+		if ( $this->dbw->doQuery( $keyword . " " . $this->id ) !== false ) {
+			if ( $wgDebugDBTransactions ) {
+				wfDebug( sprintf ($msg_ok, $this->id ) );
+			}
+		} else {
+			wfDebug( sprintf ($msg_failed, $this->id ) );
+		}
+	}
+
+	public function savepoint() {
+		$this->query("SAVEPOINT", 
+			"Transaction state: savepoint \"%s\" established.\n",
+			"Transaction state: establishment of savepoint \"%s\" FAILED.\n"
+		);
+	}
+
+	public function release() {
+		$this->query("RELEASE", 
+			"Transaction state: savepoint \"%s\" released.\n",
+			"Transaction state: release of savepoint \"%s\" FAILED.\n"
+		);
+	}
+
+	public function rollback() {
+		$this->query("ROLLBACK TO",
+			"Transaction state: savepoint \"%s\" rolled back.\n",
+			"Transaction state: rollback of savepoint \"%s\" FAILED.\n"
+		);
+	}
+
+	public function __toString() {
+		return (string)$this->id;
 	}
 }
 
@@ -314,8 +388,7 @@ class DatabasePostgres extends DatabaseBase {
 		return pg_close( $this->mConn );
 	}
 
-	protected function doQuery( $sql ) {
-		global $wgDebugDBTransactions;
+	public function doQuery( $sql ) {
 		if ( function_exists( 'mb_convert_encoding' ) ) {
 			$sql = mb_convert_encoding( $sql, 'UTF-8' );
 		}
@@ -327,8 +400,13 @@ class DatabasePostgres extends DatabaseBase {
 	}
 
 	function reportQueryError( $error, $errno, $sql, $fname, $tempIgnore = false ) {
-		/* Transaction stays in the ERROR state until rolledback */
-		$this->rollback( __METHOD__ );
+		if ( !$tempIgnore ) {
+		    /*
+			 * Transaction stays in the ERROR state until rolledback 
+			 * However, don't rollback when a SAVEPOINT is set by "IGNORE" option
+			 */
+			$this->rollback( __METHOD__ );
+		}
 		parent::reportQueryError( $error, $errno, $sql, $fname, $tempIgnore );
 	}
 
@@ -535,15 +613,9 @@ class DatabasePostgres extends DatabaseBase {
 		}
 
 		// If IGNORE is set, we use savepoints to emulate mysql's behavior
-		$ignore = in_array( 'IGNORE', $options ) ? 'mw' : '';
-
-		// If we are not in a transaction, we need to be for savepoint trickery
-		$didbegin = 0;
-		if ( $ignore ) {
-			if ( !$this->mTrxLevel ) {
-				$this->begin( __METHOD__ );
-				$didbegin = 1;
-			}
+		$savepoint = null;
+		if ( in_array( 'IGNORE', $options ) ) {
+			$savepoint = new SavepointPostgres( $this, 'mw' );
 			$olde = error_reporting( 0 );
 			// For future use, we may want to track the number of actual inserts
 			// Right now, insert (all writes) simply return true/false
@@ -553,7 +625,7 @@ class DatabasePostgres extends DatabaseBase {
 		$sql = "INSERT INTO $table (" . implode( ',', $keys ) . ') VALUES ';
 
 		if ( $multi ) {
-			if ( $this->numeric_version >= 8.2 && !$ignore ) {
+			if ( $this->numeric_version >= 8.2 && !$savepoint ) {
 				$first = true;
 				foreach ( $args as $row ) {
 					if ( $first ) {
@@ -563,7 +635,7 @@ class DatabasePostgres extends DatabaseBase {
 					}
 					$sql .= '(' . $this->makeList( $row ) . ')';
 				}
-				$res = (bool)$this->query( $sql, $fname, $ignore );
+				$res = (bool)$this->query( $sql, $fname, $savepoint );
 			} else {
 				$res = true;
 				$origsql = $sql;
@@ -571,18 +643,18 @@ class DatabasePostgres extends DatabaseBase {
 					$tempsql = $origsql;
 					$tempsql .= '(' . $this->makeList( $row ) . ')';
 
-					if ( $ignore ) {
-						$this->doQuery( "SAVEPOINT $ignore" );
+					if ( $savepoint ) {
+						$savepoint->savepoint();
 					}
 
-					$tempres = (bool)$this->query( $tempsql, $fname, $ignore );
+					$tempres = (bool)$this->query( $tempsql, $fname, $savepoint );
 
-					if ( $ignore ) {
+					if ( $savepoint ) {
 						$bar = pg_last_error();
 						if ( $bar != false ) {
-							$this->doQuery( $this->mConn, "ROLLBACK TO $ignore" );
+							$savepoint->rollback();
 						} else {
-							$this->doQuery( $this->mConn, "RELEASE $ignore" );
+							$savepoint->release();
 							$numrowsinserted++;
 						}
 					}
@@ -596,27 +668,25 @@ class DatabasePostgres extends DatabaseBase {
 			}
 		} else {
 			// Not multi, just a lone insert
-			if ( $ignore ) {
-				$this->doQuery( "SAVEPOINT $ignore" );
+			if ( $savepoint ) {
+				$savepoint->savepoint();
 			}
 
 			$sql .= '(' . $this->makeList( $args ) . ')';
-			$res = (bool)$this->query( $sql, $fname, $ignore );
-			if ( $ignore ) {
+			$res = (bool)$this->query( $sql, $fname, $savepoint );
+			if ( $savepoint ) {
 				$bar = pg_last_error();
 				if ( $bar != false ) {
-					$this->doQuery( "ROLLBACK TO $ignore" );
+					$savepoint->rollback();
 				} else {
-					$this->doQuery( "RELEASE $ignore" );
+					$savepoint->release();
 					$numrowsinserted++;
 				}
 			}
 		}
-		if ( $ignore ) {
+		if ( $savepoint ) {
 			$olde = error_reporting( $olde );
-			if ( $didbegin ) {
-				$this->commit( __METHOD__ );
-			}
+			$savepoint->commit();
 
 			// Set the affected row count for the whole operation
 			$this->mAffectedRows = $numrowsinserted;
@@ -642,9 +712,6 @@ class DatabasePostgres extends DatabaseBase {
 	{
 		$destTable = $this->tableName( $destTable );
 
-		// If IGNORE is set, we use savepoints to emulate mysql's behavior
-		$ignore = in_array( 'IGNORE', $insertOptions ) ? 'mw' : '';
-
 		if( is_array( $insertOptions ) ) {
 			$insertOptions = implode( ' ', $insertOptions ); // FIXME: This is unused
 		}
@@ -658,16 +725,13 @@ class DatabasePostgres extends DatabaseBase {
 			$srcTable = $this->tableName( $srcTable );
 		}
 
-		// If we are not in a transaction, we need to be for savepoint trickery
-		$didbegin = 0;
-		if ( $ignore ) {
-			if( !$this->mTrxLevel ) {
-				$this->begin( __METHOD__ );
-				$didbegin = 1;
-			}
+		// If IGNORE is set, we use savepoints to emulate mysql's behavior
+		$savepoint = null;
+		if ( in_array( 'IGNORE', $options ) ) {
+			$savepoint = new SavepointPostgres( $this, 'mw' );
 			$olde = error_reporting( 0 );
 			$numrowsinserted = 0;
-			$this->doQuery( "SAVEPOINT $ignore" );
+			$savepoint->savepoint();
 		}
 
 		$sql = "INSERT INTO $destTable (" . implode( ',', array_keys( $varMap ) ) . ')' .
@@ -680,19 +744,17 @@ class DatabasePostgres extends DatabaseBase {
 
 		$sql .= " $tailOpts";
 
-		$res = (bool)$this->query( $sql, $fname, $ignore );
-		if( $ignore ) {
+		$res = (bool)$this->query( $sql, $fname, $savepoint );
+		if( $savepoint ) {
 			$bar = pg_last_error();
 			if( $bar != false ) {
-				$this->doQuery( "ROLLBACK TO $ignore" );
+				$savepoint->rollback();
 			} else {
-				$this->doQuery( "RELEASE $ignore" );
+				$savepoint->release();
 				$numrowsinserted++;
 			}
 			$olde = error_reporting( $olde );
-			if( $didbegin ) {
-				$this->commit( __METHOD__ );
-			}
+			$savepoint->commit();
 
 			// Set the affected row count for the whole operation
 			$this->mAffectedRows = $numrowsinserted;

includes/objectcache/SqlBagOStuff.php
@@ -66,24 +66,33 @@ class SqlBagOStuff extends BagOStuff {
 	 * @return DatabaseBase
 	 */
 	protected function getDB() {
+		global $wgDebugDBTransactions;
 		if ( !isset( $this->db ) ) {
 			# If server connection info was given, use that
 			if ( $this->serverInfo ) {
+				if ( $wgDebugDBTransactions ) {
+					wfDebug( sprintf( "Using provided serverInfo for SqlBagOStuff\n" ) );
+				}
 				$this->lb = new LoadBalancer( array(
 					'servers' => array( $this->serverInfo ) ) );
 				$this->db = $this->lb->getConnection( DB_MASTER );
 				$this->db->clearFlag( DBO_TRX );
 			} else {
-				# We must keep a separate connection to MySQL in order to avoid deadlocks
-				# However, SQLite has an opposite behaviour.
-				# @todo Investigate behaviour for other databases
-				if ( wfGetDB( DB_MASTER )->getType() == 'sqlite' ) {
-					$this->db = wfGetDB( DB_MASTER );
-				} else {
+				/*
+				 * We must keep a separate connection to MySQL in order to avoid deadlocks
+				 * However, SQLite has an opposite behaviour. And PostgreSQL needs to know
+				 * if we are in transaction or no
+				 */
+				if ( wfGetDB( DB_MASTER )->getType() == 'mysql' ) {
 					$this->lb = wfGetLBFactory()->newMainLB();
 					$this->db = $this->lb->getConnection( DB_MASTER );
 					$this->db->clearFlag( DBO_TRX );
+				} else {
+					$this->db = wfGetDB( DB_MASTER );
 				}
+			}
+			if ( $wgDebugDBTransactions ) {
+				wfDebug( sprintf( "Connection %s will be used for SqlBagOStuff\n", $this->db ) );
 			}
 		}
 