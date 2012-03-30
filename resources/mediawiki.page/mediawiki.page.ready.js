661f01b607a1d45ef714ce13f57b7c05526f8845
Akshay Chugh <chughakshay16@gmail.com>
2012-03-30 17:49:00 +0530

    (bug 21119) link to toggle hidden categories
    Patchset 3: Fixed the issues which were made in the comments and have
    also made some new changes to overcome some new issues in the code.
    
    Patchset 4: use jQuery.toggle(), rewrite commit message, fix minor
    whitespaces issues.
    
    Change-Id: I44a447a1d89e06c82ec0045072e4954d20be8b75

--- resources/mediawiki.page/mediawiki.page.ready.js
+++ resources/mediawiki.page/mediawiki.page.ready.js
@@ -18,6 +18,30 @@ jQuery( document ).ready( function( $ ) {
 	/* Enable CheckboxShiftClick */
 	$( 'input[type=checkbox]:not(.noshiftselect)' ).checkboxShiftClick();
 
+	/* Enable toggle category link if at all the page has hidden category links */
+	if($("#mw-hidden-catlinks").length>0)
+	{
+			// Craft the link:
+			jQuery('<a/>', {
+			    id: 'mw-cat-toggle',
+			    href: '#',
+			    text: mw.msg('category-toggle-show'),
+//			    click: handler
+			})
+			// Let it show/hide the #mw-hidden-catlinks div
+			.toggle(
+			function() {
+				$("#mw-hidden-catlinks").show();
+				$("#mw-cat-toggle").text(mw.msg('category-toggle-hide'));
+			},
+			function() {
+				$("#mw-hidden-catlinks").hide();
+				$("#mw-cat-toggle").text(mw.msg('category-toggle-show'));
+			}
+		)
+		// Adding the link on top of the category list
+		.prependTo('#catlinks');
+	}
 	/* Add accesskey hints to the tooltips */
 	mw.util.updateTooltipAccessKeys();
 
