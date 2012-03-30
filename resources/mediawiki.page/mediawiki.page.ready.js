0c6444a9b1a4f67aab3ef4e6f6f230ae46b6e068
Akshay Chugh <chughakshay16@gmail.com>
2012-03-30 17:49:00 +0530

    (bug 21119) link to toggle hidden categories
    Patchset 9:
    - made changes in MessagesQqq.php and messages.inc
    - added plural support for the new messages
    - modified the text of the new messages
    - modified the logic for displaying Expand|Collapse link in
      mediawiki.page.ready.js (now it takes care of the scenario pointed
      out by Brion and Hashar in my previous patchsets)
    
    Patchset 6: Fixed the issues which were made in the comments and have
    also made some new changes to overcome some new issues in the code.
    
    Patchset 4: use jQuery.toggle(), rewrite commit message, fix minor
    whitespaces issues.
    
    Patchset 5: the link now appears below the normal categories and above the
    hidden categories
    Change-Id: I44a447a1d89e06c82ec0045072e4954d20be8b75

--- resources/mediawiki.page/mediawiki.page.ready.js
+++ resources/mediawiki.page/mediawiki.page.ready.js
@@ -18,6 +18,23 @@ jQuery( document ).ready( function( $ ) {
 	/* Enable CheckboxShiftClick */
 	$( 'input[type=checkbox]:not(.noshiftselect)' ).checkboxShiftClick();
 
+	/* Enable toggle category link, if at all the page has hidden category links and user preference is set to true */
+	if ( mw.user.options.get( 'showhiddencats' ) && $( '#mw-hidden-catlinks' ).length > 0 ) {
+		$count = $( '#mw-hidden-catlinks' ).find( 'li' ).length;
+		$( '#mw-hidden-catlinks' ).before( $( '<a />' ), {
+			id: 'mw-cat-toggle',
+			href: '#',
+			text: mw.msg( 'category-toggle-expand', $count )
+		}).toggle(
+		function() {
+			$( '#mw-hidden-catlinks' ).show();
+			$( '#mw-cat-toggle' ).text( mw.msg( 'category-toggle-collapse', $count ) );
+		},
+		function() {
+			$( '#mw-hidden-catlinks' ).hide();
+			$( '#mw-cat-toggle' ).text( mw.msg( 'category-toggle-expand', $count ) );
+		})
+	}
 	/* Add accesskey hints to the tooltips */
 	mw.util.updateTooltipAccessKeys();
 
