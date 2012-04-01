02d67a2b994b8dd9527e463498fc5b71fe2f9eca
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-03-31 21:04:01 -0700

    (bug 23772) Implement a "Not what you were looking for?" search feature.
    This implements a "Not the page you were looking for? Try searching for pages containing "<x>"."
    message that shows up after a "Go" search. This is a usability enhancement that allows
    readers landing on a page different than the one they wanted to immediately switch to a
    full text search without needing to know how.
    
    For now the message shows up in a notification until someone comes up with a better spot.
    
    Change-Id: I0f3861cc1c57f474cfa24964bc23fdf40034a8ef

---
+++ resources/mediawiki.page/mediawiki.page.search.js
@@ -0,0 +1,54 @@
+( function ( mw, $ ) {
+
+	try {
+		// Skip this feature if the browser does not support session storage
+		if ( !window.sessionStorage ) {
+			return;
+		}
+	} catch ( e ) {
+		// Avoid being mauled by https://bugzilla.mozilla.org/show_bug.cgi?id=748620
+		return;
+	}
+
+	$( function () {
+
+		// Handle the value of previous request
+		if ( window.sessionStorage.mwLastRequestSearchText ) {
+			var value = window.sessionStorage.mwLastRequestSearchText;
+
+			// We don't want to insert anything on Special:Search
+			var specialPage = mw.config.get( 'wgCanonicalSpecialPageName' );
+			if ( specialPage !== 'Search' ) {
+				var uri = new mw.Uri( mw.config.get( 'wgServer' ) + mw.config.get( 'wgScript' ) );
+				uri.extend( {
+					title: (new mw.Title( 'Search', -1 )).getPrefixedText(),
+					search: value,
+					fulltext: 1
+				})
+				mw.notify( mw.message( 'search-notwhatlookingfor', value, uri ) );
+			}
+
+			// Once we've shown the message drop the data
+			// We don't need to keep this around for refresh or anything
+			delete window.sessionStorage.mwLastRequestSearchText;
+		}
+
+		/* Implement "Not what you were looking for?" handling for "Go" searches */
+		function submitHandler( e ) {
+			// Get the search value
+			var value = this.$input.val();
+
+			// Before continuing with the search store it in session storage
+			window.sessionStorage.mwLastRequestSearchText = value;
+		}
+
+		mw.ui.searchBoxes().each( function () {
+			var searchBox = this;
+			searchBox.$form.submit( function ( e ) {
+				submitHandler.call( searchBox, e );
+			} );
+		} );
+
+	} );
+
+}( mediaWiki, jQuery ) );
