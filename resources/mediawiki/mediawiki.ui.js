6b595dddbd52ece777c6ab90970a72ffd5347209
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-03-31 21:04:01 -0700

    (bug 23772) Implement a "Not what you were looking for?" search feature.
    This implements a "Not the page you were looking for? Try searching for pages containing "<x>"."
    message that shows up after a "Go" search. This is a usability enhancement that allows
    readers landing on a page different than the one they wanted to immediately switch to a
    full text search without needing to know how.
    
    For now the message shows up in a notification until someone comes up with a better spot.
    
    Change-Id: I0f3861cc1c57f474cfa24964bc23fdf40034a8ef

---
+++ resources/mediawiki/mediawiki.ui.js
@@ -0,0 +1,33 @@
+/**
+ * Implements mediaWiki.ui library
+ *
+ * This library gives abstract access to various parts of the MediaWiki UI.
+ * Note that not every UI piece belongs in this library. Things which are legacy
+ * or not abstract like addPortletLink should be left out of here until they are
+ * replaced with something cleanly abstract.
+ */
+( function ( mw, $ ) {
+
+	function SearchBox( form ) {
+		this.$form = $( form );
+		this.$input = this.$form.find( 'input[name="search"]:first' )
+	}
+
+	var ui = {
+		searchBoxes: function () {
+			var query = [
+				// class="mw-search", should be used in all new skins
+				'form.mw-search',
+				// Legacy form IDs for old skins not implementing .mw-search
+				'form#searchform', 'form#searchform2', 'form#powersearch', 'form#search'
+			];
+			var searchBoxes = [];
+			return $( query.join( ', ' ) ).map( function () {
+				return new SearchBox( this );
+			} );
+		}
+	};
+
+	mw.ui = ui;
+
+}( mediaWiki, jQuery ) );
