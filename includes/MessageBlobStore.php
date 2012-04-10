6affb6182c139179b9bf0a2cf19daa826df26f7a
Reedy <reedy@formey.wikimedia.org>
2012-04-10 18:27:59 +0000

    Commit of various live hacks
    Disable of MessageBlobStore clear
    
    Reset $wgAutopromote (should be moved to a config change!!)
    
    Disable setting of wgStyleSheetPath
    
    Disable squid updates in HTMLCacaheUpdate invalidateTitles
    
    Throttle page_touched
    
    Add apc htcp packet numbers to SquidUpdate
    
    Disable set names binary/utf8
    
    Commment out searchindex table indexes
    
    Was c532e81d583d3d0439fe76eea4d105d675461b56
    
    Original revision Change-Id I42c4f859e55eb198f6c6841e582b3552aad7b31f
    https://gerrit.wikimedia.org/r/#/c/7606
    
    Change-Id: I5ec8dd53188e9e4128f99ceaff38ebf9dcf570bb

--- includes/MessageBlobStore.php
+++ includes/MessageBlobStore.php
@@ -231,6 +231,9 @@ class MessageBlobStore {
 	}
 
 	public static function clear() {
+		// HACK: disable clear() on WMF servers
+		return;
+
 		// TODO: Give this some more thought
 		// TODO: Is TRUNCATE better?
 		$dbw = wfGetDB( DB_MASTER );
