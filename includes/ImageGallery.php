90602a08d90fc1c6593e4387c59ad6585f29672f
Kim Eik <kim@heldig.org>
2012-04-10 12:30:17 +0200

    (bug 34852) new optional 'link' parameter to <gallery>
    The patch adds an optional parameter |link= to the <gallery>
    tag. This will allow for images to link to other pages and
    externals urls instead of being hardlinked to the image file
    that is displayed in the gallery.
    
    Here are a couple of examples.
    
    <gallery>
    File:20120106_001.jpg|link=[[DisplayLine]]
    </gallery>
    
    <gallery>
    File:20120106_001.jpg|my caption|alt=my alt
    text|link=http://bugzilla.wikimedia.org
    </gallery>
    
    this would cause the link on the thumbnails rendered by the gallery tag to link
    to a custom page/url instead of the actual media/image.
    
    a link should be an internal [[WikiFormatted]] link or an absolute uri.
    
    Change-Id: I21b276ad5c7a8df13b3a716957d23fd53c37d29e

--- includes/ImageGallery.php
+++ includes/ImageGallery.php
@@ -141,23 +141,24 @@ class ImageGallery {
 	 * @param $title Title object of the image that is added to the gallery
 	 * @param $html  String: Additional HTML text to be shown. The name and size of the image are always shown.
 	 * @param $alt   String: Alt text for the image
+	 * @param $link  String: Override image link (optional)
 	 */
-	function add( $title, $html = '', $alt = '' ) {
+	function add( $title, $html = '', $alt = '', $link = '') {
 		if ( $title instanceof File ) {
 			// Old calling convention
 			$title = $title->getTitle();
 		}
-		$this->mImages[] = array( $title, $html, $alt );
+		$this->mImages[] = array( $title, $html, $alt, $link );
 		wfDebug( 'ImageGallery::add ' . $title->getText() . "\n" );
 	}
 
 	/**
-	* Add an image at the beginning of the gallery.
-	*
-	* @param $title Title object of the image that is added to the gallery
-	* @param $html  String: Additional HTML text to be shown. The name and size of the image are always shown.
-	* @param $alt   String: Alt text for the image
-	*/
+	 * Add an image at the beginning of the gallery.
+	 *
+	 * @param $title Title object of the image that is added to the gallery
+	 * @param $html  String: Additional HTML text to be shown. The name and size of the image are always shown.
+	 * @param $alt   String: Alt text for the image
+	 */
 	function insert( $title, $html = '', $alt = '' ) {
 		if ( $title instanceof File ) {
 			// Old calling convention
@@ -245,6 +246,7 @@ class ImageGallery {
 			$nt = $pair[0];
 			$text = $pair[1]; # "text" means "caption" here
 			$alt = $pair[2];
+			$link = $pair[3];
 
 			$descQuery = false;
 			if ( $nt->getNamespace() == NS_FILE ) {
@@ -289,6 +291,7 @@ class ImageGallery {
 					'desc-link' => true,
 					'desc-query' => $descQuery,
 					'alt' => $alt,
+					'custom-url-link' => $link
 				);
 				# In the absence of both alt text and caption, fall back on providing screen readers with the filename as alt text
 				if ( $alt == '' && $text == '' ) {
@@ -346,9 +349,9 @@ class ImageGallery {
 					. '<div style="width: ' . ( $this->mWidths + self::THUMB_PADDING + self::GB_PADDING ) . 'px">'
 					. $thumbhtml
 					. "\n\t\t\t" . '<div class="gallerytext">' . "\n"
-						. $textlink . $text . $fileSize
+					. $textlink . $text . $fileSize
 					. "\n\t\t\t</div>"
-				. "\n\t\t</div></li>";
+					. "\n\t\t</div></li>";
 		}
 		$output .= "\n</ul>";
 
@@ -378,8 +381,8 @@ class ImageGallery {
 	 */
 	public function getContextTitle() {
 		return is_object( $this->contextTitle ) && $this->contextTitle instanceof Title
-				? $this->contextTitle
-				: false;
+			? $this->contextTitle
+			: false;
 	}
 
 } //class
