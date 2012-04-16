18c4f81721a23820a9f8534253280b585da8bd0a
Joan Creus <joan.creus.c@gmail.com>
2012-04-16 22:37:20 +0200

    Add auto-save feature for edit textareas, via localStorage
    This commit adds an auto-save feature for edit textareas. That way,
    the typical problem when the browser crashes and minutes-hours of
    work are lost disappears (for modern browsers, since it uses
    localStorage). When a key is pressed, the localStorage is updated.
    When the user enters the edit page for that saved page (there can
    be multiple saved pages), they are asked whether to replace the textarea
    contents with the saved ones, or continue without doing anything.
    
    It should be noted that, whenever the page is saved, the localStorage
    for that page is erased.
    
    Change-Id: I38372e4ae384c4d477f4cee1e03ec06051861cf7

--- resources/mediawiki.action/mediawiki.action.edit.js
+++ resources/mediawiki.action/mediawiki.action.edit.js
@@ -136,6 +136,62 @@
 	// Expose API publicly
 	mw.toolbar = toolbar;
 
+	function saveToLocalStorage( page, contents ) {
+		localStorage.setItem( mw.config.get( 'wgDBname' ) + '-savedta-' + page, contents );
+	}
+
+	function getLocalStorage( page ) {
+		return localStorage.getItem( mw.config.get( 'wgDBname' ) + '-savedta-' + page );
+	}
+
+	function removeFromLocalStorage( page ) {
+		return localStorage.removeItem( mw.config.get( 'wgDBname' ) + '-savedta-' + page );
+	}
+
+	function textAreaAutoSaveInit() {
+		// If there's no localStorage, there's no point in doing the checks or adding listeners.
+		// Also, it should only be run when editing, and not previewing.
+		if ( 'localStorage' in window && mw.config.get( 'wgAction' ) === 'edit' ) {
+			var pageName = mw.config.get( 'wgPageName' ),
+				textArea = $( '#wpTextbox1' ),
+				node = $( '<span>' ),
+				notif,
+				replaceLink,
+				discardLink;
+
+			textArea.change( function () {
+				saveToLocalStorage( pageName, textArea.val() );
+			} );
+
+			$( '#wpSave' ).click( function () {
+				removeFromLocalStorage( pageName );
+			} );
+
+			if ( $.trim( getLocalStorage( pageName ) ) !== $.trim( textArea.val() ) ) {
+				replaceLink = $( '<a>' ).click( function ( e ) {
+					textArea.val( getLocalStorage( pageName ) );
+					e.preventDefault();
+					notif.close();
+				} ).text( mw.msg( 'textarea-use-draft' ) );
+
+				discardLink = $( '<a>' ).click( function ( e ) {
+					removeFromLocalStorage( pageName );
+					e.preventDefault();
+					notif.close();
+				} ).text( mw.msg( 'textarea-use-current-version' ) );
+
+				node.append(
+					$( '<span>' ).text( mw.msg( 'textarea-draft-found' ) ),
+					'<br>',
+					replaceLink,
+					' · ',
+					discardLink
+				);
+				notif = mw.notification.notify( node, { autoHide: false } );
+			}
+		}
+	}
+
 	$( function () {
 		var i, b, editBox, scrollTop, $editForm;
 
@@ -186,6 +242,8 @@
 		$( document ).on( 'focus', 'textarea, input:text', function () {
 			$currentFocused = $( this );
 		} );
+
+		textAreaAutoSaveInit();
 	});
 
 }( mediaWiki, jQuery ) );
