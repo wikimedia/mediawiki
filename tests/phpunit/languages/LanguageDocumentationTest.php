3d43f31836c69580ed8f0b3b0afc606154459de9
Antoine Musso <hashar@free.fr>
2012-04-20 18:08:33 +0200

    tests to compare En, Qqq and messages.inc
    DO NOT SUBMIT: messages.inc should only be compared against core
    messages and exclude any extensions messages.
    
    Change-Id: I3b7d6d893227ae3f7cfca378452beb87613474d2

---
+++ tests/phpunit/languages/LanguageDocumentationTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @medium
+ */
+class LanguageDocumentationTest extends MediaWikiTestCase {
+
+	protected static $keys;
+
+	static function setUpBeforeClass() {
+		self::$keys = array(
+			'qqq' => Language::getMessageKeysFor( 'qqq' ),
+			'en'  => Language::getMessageKeysFor( 'en' ),
+		);
+
+		global $IP, $wgMessageStructure;
+		if( !isset( $wgMessageStructure ) ) {
+			require_once( $IP . '/maintenance/language/messages.inc' );
+		}
+
+		foreach( $wgMessageStructure as $block ) {
+			foreach( $block as $key ) {
+				self::$keys['messages.inc'][] = $key;
+			}
+		}
+	}
+
+	/**
+	 * Compares en against qqq, make sure all messages are documented
+	 */
+	function testAllEnglishMessagesAreDocumentedInQqq() {
+		$delta = array_diff( self::$keys['en'], self::$keys['qqq'] );
+		$this->assertEmpty( $delta,
+			'All English messages should be documented in qqq'
+		);
+	}
+	/**
+	 * Compares qqq against en, make sure qqq does not document removed messages
+	 */
+	function testQqqDocumentsExistingMessages() {
+		$delta = array_diff( self::$keys['qqq'], self::$keys['en'] );
+		$this->assertEmpty( $delta,
+			'Qqq should only document existing English messages.'
+		);
+	}
+
+	/**
+	 * Compares en against messages.inc
+	 */
+	function testMessageInMessagesincFile() {
+		$this->markTestIncomplete( "should check core messages only" );
+
+		$this->assertEquals(
+			array()
+			, array_diff( self::$keys['en'], self::$keys['messages.inc'] )
+			, 'All messages should be described in messages.inc'
+		);
+	}
+
+	/**
+	 * Provide all languages BUT english (en)
+	 */
+	function provideLanguageKeys() {
+		$cases = array();
+
+		$langs = Language::getLanguageNames();
+		unset( $langs['en'] );
+
+		foreach( $langs as $code => $name ) {
+			$cases[] = array( $code );
+		}
+		return $cases;
+	}
+}
