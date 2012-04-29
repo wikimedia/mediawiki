fad001b84fcd8845cf26060fe2c95e3fe18e94f0
daniel <daniel.kinzler@wikimedia.de>
2012-04-29 22:22:07 +0200

    Introducing ContentHandler facility to support different content types in MediaWiki.
    This patchset merely lays the foundation for content type support, it does not by itself
    implement new functionality. I have prepared the integration into the Article and EditPage classes,
    into different actions, API modules, etc, and will submit these changes once the fundamentals are
    merged into master.
    
    Details about the ContentHandler facility at <http://meta.wikimedia.org/wiki/Wikidata/Notes/ContentHandler>
    
    Discussion in wikitech: <http://www.gossamer-threads.com/lists/wiki/wikitech/276708?search_string=cutting;#276708>
    
    Changeset 2: fixed WikiPageTest.php to work under sqlite.
    
    Changeset 3: required explicit ParserOptions and reduced dependency on globals, as suggested by IAlex
    
    Changeset 5: fixing inconsistent method signatures to pass E_STRICT
    
    Changeset 9: trying to find out why testDoRollback fails in jenkins. Can't reproduce locally, even with sqlite;
                 also fixing insert into archive table upon deletion.
    
    Changeset 10: introducing feature switch $wgContentHandlerUseDB as requested by RobLa, to allow
                  code deployment without a db schema change. Added test cases to test operation with and
                  without that flag set.
    
    Changeset 12: fixed hardcoded include path (apparently got injected by the IDE).
    
    Changeset 14: mark testDoRollback as broken. avoid test confusion due to tidy. added sanity check in redirect test.
    
    Change-Id: Id0db0847c01b93cea09928da46a89ad1248fe4c8

--- tests/phpunit/includes/RevisionTest.php
+++ tests/phpunit/includes/RevisionTest.php
@@ -6,20 +6,45 @@ class RevisionTest extends MediaWikiTestCase {
 	function setUp() {
 		global $wgContLang;
 		$wgContLang = Language::factory( 'en' );
+
 		$globalSet = array(
 			'wgLegacyEncoding' => false,
 			'wgCompressRevisions' => false,
+
+			'wgContentHandlerTextFallback' => $GLOBALS['wgContentHandlerTextFallback'],
+			'wgExtraNamespaces' => $GLOBALS['wgExtraNamespaces'],
+			'wgNamespaceContentModels' => $GLOBALS['wgNamespaceContentModels'],
+			'wgContentHandlers' => $GLOBALS['wgContentHandlers'],
 		);
+
 		foreach ( $globalSet as $var => $data ) {
 			$this->saveGlobals[$var] = $GLOBALS[$var];
 			$GLOBALS[$var] = $data;
 		}
+
+		global $wgExtraNamespaces, $wgNamespaceContentModels, $wgContentHandlers, $wgContLang;
+		$wgExtraNamespaces[ 12312 ] = 'Dummy';
+		$wgExtraNamespaces[ 12313 ] = 'Dummy_talk';
+
+		$wgNamespaceContentModels[ 12312 ] = 'DUMMY';
+		$wgContentHandlers[ 'DUMMY' ] = 'DummyContentHandlerForTesting';
+
+		MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
+		$wgContLang->resetNamespaces(); # reset namespace cache
+
+		global $wgContentHandlerTextFallback;
+		$wgContentHandlerTextFallback = 'ignore';
 	}
 
 	function tearDown() {
+		global $wgContLang;
+
 		foreach ( $this->saveGlobals as $var => $data ) {
 			$GLOBALS[$var] = $data;
 		}
+
+		MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
+		$wgContLang->resetNamespaces(); # reset namespace cache
 	}
 
 	function testGetRevisionText() {
@@ -120,6 +145,196 @@ class RevisionTest extends MediaWikiTestCase {
 		$this->assertEquals( "Wiki est l'\xc3\xa9cole superieur !",
 			Revision::getRevisionText( $row ), "getRevisionText" );
 	}
+
+	# =================================================================================================================
+
+	/**
+	 * @param string $text
+	 * @param string $title
+	 * @param string $model
+	 * @return Revision
+	 */
+	function newTestRevision( $text, $title = "Test", $model = CONTENT_MODEL_WIKITEXT, $format = null ) {
+		if ( is_string( $title ) ) {
+			$title = Title::newFromText( $title );
+		}
+
+		$content = ContentHandler::makeContent( $text, $title, $model, $format );
+
+		$rev = new Revision(
+			array(
+				'id'         => 42,
+				'page'       => 23,
+				'title'      => $title,
+
+				'content'    => $content,
+				'length'     => $content->getSize(),
+				'comment'    => "testing",
+				'minor_edit' => false,
+
+				'content_format' => $format,
+			)
+		);
+
+		return $rev;
+	}
+
+	function dataGetContentModel() {
+		return array(
+			array( 'hello world', 'Hello', null, null, CONTENT_MODEL_WIKITEXT ),
+			array( 'hello world', 'User:hello/there.css', null, null, CONTENT_MODEL_CSS ),
+			array( serialize('hello world'), 'Dummy:Hello', null, null, 'DUMMY' ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetContentModel
+	 */
+	function testGetContentModel( $text, $title, $model, $format, $expectedModel ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+
+		$this->assertEquals( $expectedModel, $rev->getContentModelName() );
+	}
+
+	function dataGetContentFormat() {
+		return array(
+			array( 'hello world', 'Hello', null, null, 'text/x-wiki' ),
+			array( 'hello world', 'Hello', CONTENT_MODEL_CSS, null, 'text/css' ),
+			array( 'hello world', 'User:hello/there.css', null, null, 'text/css' ),
+			array( serialize('hello world'), 'Dummy:Hello', null, null, 'dummy' ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetContentFormat
+	 */
+	function testGetContentFormat( $text, $title, $model, $format, $expectedFormat ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+
+		$this->assertEquals( $expectedFormat, $rev->getContentFormat() );
+	}
+
+	function dataGetContentHandler() {
+		return array(
+			array( 'hello world', 'Hello', null, null, 'WikitextContentHandler' ),
+			array( 'hello world', 'User:hello/there.css', null, null, 'CssContentHandler' ),
+			array( serialize('hello world'), 'Dummy:Hello', null, null, 'DummyContentHandlerForTesting' ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetContentHandler
+	 */
+	function testGetContentHandler( $text, $title, $model, $format, $expectedClass ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+
+		$this->assertEquals( $expectedClass, get_class( $rev->getContentHandler() ) );
+	}
+
+	function dataGetContent() {
+		return array(
+			array( 'hello world', 'Hello', null, null, Revision::FOR_PUBLIC, 'hello world' ),
+			array( serialize('hello world'), 'Hello', 'DUMMY', null, Revision::FOR_PUBLIC, serialize('hello world') ),
+			array( serialize('hello world'), 'Dummy:Hello', null, null, Revision::FOR_PUBLIC, serialize('hello world') ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetContent
+	 */
+	function testGetContent( $text, $title, $model, $format, $audience, $expectedSerialization ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+		$content = $rev->getContent( $audience );
+
+		$this->assertEquals( $expectedSerialization, is_null( $content ) ? null : $content->serialize( $format ) );
+	}
+
+	function dataGetText() {
+		return array(
+			array( 'hello world', 'Hello', null, null, Revision::FOR_PUBLIC, 'hello world' ),
+			array( serialize('hello world'), 'Hello', 'DUMMY', null, Revision::FOR_PUBLIC, null ),
+			array( serialize('hello world'), 'Dummy:Hello', null, null, Revision::FOR_PUBLIC, null ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetText
+	 */
+	function testGetText( $text, $title, $model, $format, $audience, $expectedText ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+
+		$this->assertEquals( $expectedText, $rev->getText( $audience ) );
+	}
+
+	/**
+	 * @dataProvider dataGetText
+	 */
+	function testGetRawText( $text, $title, $model, $format, $audience, $expectedText ) {
+		$rev = $this->newTestRevision( $text, $title, $model, $format );
+
+		$this->assertEquals( $expectedText, $rev->getRawText( $audience ) );
+	}
+
+
+	public function dataGetSize( ) {
+		return array(
+			array( "hello world.", null, 12 ),
+			array( serialize( "hello world." ), "DUMMY", 12 ),
+		);
+	}
+
+	/**
+	 * @covers Revision::getSize
+	 * @dataProvider dataGetSize
+	 */
+	public function testGetSize( $text, $model, $expected_size )
+	{
+		$rev = $this->newTestRevision( $text, 'RevisionTest_testGetSize', $model );
+		$this->assertEquals( $expected_size, $rev->getSize() );
+	}
+
+	public function dataGetSha1( ) {
+		return array(
+			array( "hello world.", null, Revision::base36Sha1( "hello world." ) ),
+			array( serialize( "hello world." ), "DUMMY", Revision::base36Sha1( serialize( "hello world." ) ) ),
+		);
+	}
+
+	/**
+	 * @covers Revision::getSha1
+	 * @dataProvider dataGetSha1
+	 */
+	public function testGetSha1( $text, $model, $expected_hash )
+	{
+		$rev = $this->newTestRevision( $text, 'RevisionTest_testGetSha1', $model );
+		$this->assertEquals( $expected_hash, $rev->getSha1() );
+	}
+
+	public function testConstructWithText() {
+		$rev = new Revision( array(
+		                          'text' => 'hello world.',
+		                          'content_model' => CONTENT_MODEL_JAVASCRIPT
+		                     ));
+
+		$this->assertNotNull( $rev->getText(), 'no content text' );
+		$this->assertNotNull( $rev->getContent(), 'no content object available' );
+		$this->assertEquals( CONTENT_MODEL_JAVASCRIPT, $rev->getContent()->getModelName() );
+		$this->assertEquals( CONTENT_MODEL_JAVASCRIPT, $rev->getContentModelName() );
+	}
+
+	public function testConstructWithContent() {
+		$title = Title::newFromText( 'RevisionTest_testConstructWithContent' );
+
+		$rev = new Revision( array(
+		                          'content' => ContentHandler::makeContent( 'hello world.', $title, CONTENT_MODEL_JAVASCRIPT ),
+		                     ));
+
+		$this->assertNotNull( $rev->getText(), 'no content text' );
+		$this->assertNotNull( $rev->getContent(), 'no content object available' );
+		$this->assertEquals( CONTENT_MODEL_JAVASCRIPT, $rev->getContent()->getModelName() );
+		$this->assertEquals( CONTENT_MODEL_JAVASCRIPT, $rev->getContentModelName() );
+	}
+
 }
 
 
--- tests/phpunit/includes/TitleMethodsTest.php
+++ tests/phpunit/includes/TitleMethodsTest.php
@@ -2,6 +2,30 @@
 
 class TitleMethodsTest extends MediaWikiTestCase {
 
+	public function setup() {
+		global $wgExtraNamespaces, $wgNamespaceContentModels, $wgContLang;
+
+		$wgExtraNamespaces[ 12302 ] = 'TEST-JS';
+		$wgExtraNamespaces[ 12303 ] = 'TEST-JS_TALK';
+
+		$wgNamespaceContentModels[ 12302 ] = CONTENT_MODEL_JAVASCRIPT;
+
+		MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
+		$wgContLang->resetNamespaces(); # reset namespace cache
+	}
+
+	public function teardown() {
+		global $wgExtraNamespaces, $wgNamespaceContentModels, $wgContLang;
+
+		unset( $wgExtraNamespaces[ 12302 ] );
+		unset( $wgExtraNamespaces[ 12303 ] );
+
+		unset( $wgNamespaceContentModels[ 12302 ] );
+
+		MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
+		$wgContLang->resetNamespaces(); # reset namespace cache
+	}
+
 	public function dataEquals() {
 		return array(
 			array( 'Main Page', 'Main Page', true ),
@@ -43,7 +67,7 @@ class TitleMethodsTest extends MediaWikiTestCase {
 	 */
 	public function testInNamespace( $title, $ns, $expectedBool ) {
 		$title = Title::newFromText( $title );
-		$this->assertEquals( $title->inNamespace( $ns ), $expectedBool );
+		$this->assertEquals( $expectedBool, $title->inNamespace( $ns ) );
 	}
 
 	public function testInNamespaces() {
@@ -73,6 +97,178 @@ class TitleMethodsTest extends MediaWikiTestCase {
 	public function testHasSubjectNamespace( $title, $ns, $expectedBool ) {
 		$title = Title::newFromText( $title );
 		$this->assertEquals( $expectedBool, $title->hasSubjectNamespace( $ns ) );
+	}
+
+
+	public function dataGetContentModelName() {
+		return array(
+			array( 'Foo', CONTENT_MODEL_WIKITEXT ),
+			array( 'Foo.js', CONTENT_MODEL_WIKITEXT ),
+			array( 'Foo/bar.js', CONTENT_MODEL_WIKITEXT ),
+			array( 'User:Foo', CONTENT_MODEL_WIKITEXT ),
+			array( 'User:Foo.js', CONTENT_MODEL_WIKITEXT ),
+			array( 'User:Foo/bar.js', CONTENT_MODEL_JAVASCRIPT ),
+			array( 'User:Foo/bar.css', CONTENT_MODEL_CSS ),
+			array( 'User talk:Foo/bar.css', CONTENT_MODEL_WIKITEXT ),
+			array( 'User:Foo/bar.js.xxx', CONTENT_MODEL_WIKITEXT ),
+			array( 'User:Foo/bar.xxx', CONTENT_MODEL_WIKITEXT ),
+			array( 'MediaWiki:Foo.js', CONTENT_MODEL_JAVASCRIPT ),
+			array( 'MediaWiki:Foo.css', CONTENT_MODEL_CSS ),
+			array( 'MediaWiki:Foo/bar.css', CONTENT_MODEL_CSS ),
+			array( 'MediaWiki:Foo.JS', CONTENT_MODEL_WIKITEXT ),
+			array( 'MediaWiki:Foo.CSS', CONTENT_MODEL_WIKITEXT ),
+			array( 'MediaWiki:Foo.css.xxx', CONTENT_MODEL_WIKITEXT ),
+			array( 'TEST-JS:Foo', CONTENT_MODEL_JAVASCRIPT ),
+			array( 'TEST-JS:Foo.js', CONTENT_MODEL_JAVASCRIPT ),
+			array( 'TEST-JS:Foo/bar.js', CONTENT_MODEL_JAVASCRIPT ),
+			array( 'TEST-JS_TALK:Foo.js', CONTENT_MODEL_WIKITEXT ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataGetContentModelName
+	 */
+	public function testGetContentModelName( $title, $expectedModelName ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedModelName, $title->getContentModelName() );
+	}
+
+	/**
+	 * @dataProvider dataGetContentModelName
+	 */
+	public function testHasContentModel( $title, $expectedModelName ) {
+		$title = Title::newFromText( $title );
+		$this->assertTrue( $title->hasContentModel( $expectedModelName ) );
+	}
+
+	public function dataIsCssOrJsPage() {
+		return array(
+			array( 'Foo', false ),
+			array( 'Foo.js', false ),
+			array( 'Foo/bar.js', false ),
+			array( 'User:Foo', false ),
+			array( 'User:Foo.js', false ),
+			array( 'User:Foo/bar.js', false ),
+			array( 'User:Foo/bar.css', false ),
+			array( 'User talk:Foo/bar.css', false ),
+			array( 'User:Foo/bar.js.xxx', false ),
+			array( 'User:Foo/bar.xxx', false ),
+			array( 'MediaWiki:Foo.js', true ),
+			array( 'MediaWiki:Foo.css', true ),
+			array( 'MediaWiki:Foo.JS', false ),
+			array( 'MediaWiki:Foo.CSS', false ),
+			array( 'MediaWiki:Foo.css.xxx', false ),
+			array( 'TEST-JS:Foo', false ),
+			array( 'TEST-JS:Foo.js', false ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataIsCssOrJsPage
+	 */
+	public function testIsCssOrJsPage( $title, $expectedBool ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedBool, $title->isCssOrJsPage() );
+	}
+
+
+	public function dataIsCssJsSubpage() {
+		return array(
+			array( 'Foo', false ),
+			array( 'Foo.js', false ),
+			array( 'Foo/bar.js', false ),
+			array( 'User:Foo', false ),
+			array( 'User:Foo.js', false ),
+			array( 'User:Foo/bar.js', true ),
+			array( 'User:Foo/bar.css', true ),
+			array( 'User talk:Foo/bar.css', false ),
+			array( 'User:Foo/bar.js.xxx', false ),
+			array( 'User:Foo/bar.xxx', false ),
+			array( 'MediaWiki:Foo.js', false ),
+			array( 'User:Foo/bar.JS', false ),
+			array( 'User:Foo/bar.CSS', false ),
+			array( 'TEST-JS:Foo', false ),
+			array( 'TEST-JS:Foo.js', false ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataIsCssJsSubpage
+	 */
+	public function testIsCssJsSubpage( $title, $expectedBool ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedBool, $title->isCssJsSubpage() );
+	}
+
+	public function dataIsCssSubpage() {
+		return array(
+			array( 'Foo', false ),
+			array( 'Foo.css', false ),
+			array( 'User:Foo', false ),
+			array( 'User:Foo.js', false ),
+			array( 'User:Foo.css', false ),
+			array( 'User:Foo/bar.js', false ),
+			array( 'User:Foo/bar.css', true ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataIsCssSubpage
+	 */
+	public function testIsCssSubpage( $title, $expectedBool ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedBool, $title->isCssSubpage() );
+	}
+
+	public function dataIsJsSubpage() {
+		return array(
+			array( 'Foo', false ),
+			array( 'Foo.css', false ),
+			array( 'User:Foo', false ),
+			array( 'User:Foo.js', false ),
+			array( 'User:Foo.css', false ),
+			array( 'User:Foo/bar.js', true ),
+			array( 'User:Foo/bar.css', false ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataIsJsSubpage
+	 */
+	public function testIsJsSubpage( $title, $expectedBool ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedBool, $title->isJsSubpage() );
+	}
+
+	public function dataIsWikitextPage() {
+		return array(
+			array( 'Foo', true ),
+			array( 'Foo.js', true ),
+			array( 'Foo/bar.js', true ),
+			array( 'User:Foo', true ),
+			array( 'User:Foo.js', true ),
+			array( 'User:Foo/bar.js', false ),
+			array( 'User:Foo/bar.css', false ),
+			array( 'User talk:Foo/bar.css', true ),
+			array( 'User:Foo/bar.js.xxx', true ),
+			array( 'User:Foo/bar.xxx', true ),
+			array( 'MediaWiki:Foo.js', false ),
+			array( 'MediaWiki:Foo.css', false ),
+			array( 'MediaWiki:Foo/bar.css', false ),
+			array( 'User:Foo/bar.JS', true ),
+			array( 'User:Foo/bar.CSS', true ),
+			array( 'TEST-JS:Foo', false ),
+			array( 'TEST-JS:Foo.js', false ),
+			array( 'TEST-JS_TALK:Foo.js', true ),
+		);
+	}
+
+	/**
+	 * @dataProvider dataIsWikitextPage
+	 */
+	public function testIsWikitextPage( $title, $expectedBool ) {
+		$title = Title::newFromText( $title );
+		$this->assertEquals( $expectedBool, $title->isWikitextPage() );
 	}
 
 }
