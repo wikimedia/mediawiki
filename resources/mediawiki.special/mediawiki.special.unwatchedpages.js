31973690ec7a5090d5c5dfc4a8389179092b0a17
Eranroz <eranroz89@gmail.com>
2012-04-30 21:58:27 +0300

    ajaxify watch links on SpecialUnwatchedpages.
    Adding a user friendly option to watch pages from Special:UnwatchedPages
    with ajax.
    Adding textual confirm message besides the line through.
    See also: bug 17367
    Change-Id: Ie32cc54abb6f888a433f7f18ef5ad747a80d7187

---
+++ resources/mediawiki.special/mediawiki.special.unwatchedpages.js
@@ -0,0 +1,31 @@
+/**
+ * JavaScript for Special:UnwatchedPages
+ *
+ * set the watch links to use Ajax
+ * see bug #17367
+*/
+( function ( mw, $ ) {
+	 $( 'a.mw-watch-link' ).click( function ( e ) {
+		var api, apiCall, $watchedLi, $link, $subjectLink, articleTitle;
+		api = new mw.Api();
+		$link = $( this );
+		$watchedLi = $link.parents( 'li' );
+		$subjectLink = $watchedLi.children( 'a:first' );
+		articleTitle = this.title;
+		//use the text as indication wether to add to watch or unwatch
+		if ( !$subjectLink.hasClass( 'mw-watched-item' ) ) {
+			apiCall = api.watch( articleTitle, function () {
+				$subjectLink.toggleClass( 'mw-watched-item' );
+				$link.text( mw.msg( 'unwatch' ) );
+				mw.notify( mw.msg( 'addedwatchtext-short', articleTitle ) );
+			} );
+		} else {
+			apiCall = api.unwatch( articleTitle, function () {
+				$subjectLink.toggleClass( 'mw-watched-item' );
+				$link.text( mw.msg( 'watch' ) );
+				mw.notify( mw.msg( 'removedwatchtext-short', articleTitle ) );
+			} );
+		}
+		e.preventDefault();
+	} );
+}( mediaWiki, jQuery ) );
