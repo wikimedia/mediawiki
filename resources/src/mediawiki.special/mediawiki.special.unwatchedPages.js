38f3929fc687cea2c7252cac7bdf65f5d12b1eb9
Eranroz <eranroz89@gmail.com>
2012-04-30 21:58:27 +0300

    SpecialUnwatchedpages: Ajaxify watch links
    Add user friendly handling to watch pages from Special:UnwatchedPages
    using ajax. Emits a notification message upon completion of the action
    and strikes through the list item.
    
    Bug: 17367
    Change-Id: Ie32cc54abb6f888a433f7f18ef5ad747a80d7187

---
+++ resources/src/mediawiki.special/mediawiki.special.unwatchedPages.js
@@ -0,0 +1,51 @@
+/**
+ * JavaScript for Special:UnwatchedPages
+ */
+( function ( mw, $ ) {
+	$( function () {
+		$( 'a.mw-watch-link' ).click( function ( e ) {
+			var promise,
+				api = new mw.Api(),
+				$link = $( this ),
+				$subjectLink = $link.parents( 'li' ).children( 'a' ).eq( 0 ),
+				title = mw.util.getParamValue( 'title', $link.attr( 'href' ) );
+
+			// Disable link whilst we're busy to avoid double handling
+			if ( $link.data( 'mwDisabled' ) ) {
+				// mw-watch-link-disabled disables pointer-events which prevents the click event
+				// from happening in the first place. In older browsers we kill the event here.
+				return false;
+			}
+			$link.data( 'mwDisabled', true ).addClass( 'mw-watch-link-disabled' );
+
+			// Use the class to determine whether to watch or unwatch
+			if ( !$subjectLink.hasClass( 'mw-watched-item' ) ) {
+				$link.text( mw.msg( 'watching' ) );
+				promise = api.watch( title ).done( function () {
+					$subjectLink.addClass( 'mw-watched-item' );
+					$link.text( mw.msg( 'unwatch' ) );
+					mw.notify( mw.msg( 'addedwatchtext-short', title ) );
+				} ).fail( function () {
+					$link.text( mw.msg( 'watch' ) );
+					mw.notify( mw.msg( 'watcherrortext', title ) );
+				} );
+			} else {
+				$link.text( mw.msg( 'unwatching' ) );
+				promise = api.unwatch( title ).done( function () {
+					$subjectLink.removeClass( 'mw-watched-item' );
+					$link.text( mw.msg( 'watch' ) );
+					mw.notify( mw.msg( 'removedwatchtext-short', title ) );
+				} ).fail( function () {
+					$link.text( mw.msg( 'unwatch' ) );
+					mw.notify( mw.msg( 'watcherrortext', title ) );
+				} );
+			}
+
+			promise.always( function () {
+				$link.data( 'mwDisabled', false ).removeClass( 'mw-watch-link-disabled' );
+			} );
+
+			e.preventDefault();
+		} );
+	} );
+}( mediaWiki, jQuery ) );
