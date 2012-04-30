4cb22e52f572e5f9c829b93aa46112d14fe9141d
Eranroz <eranroz89@gmail.com>
2012-04-30 21:58:27 +0300

    ajaxify watch links on SpecialUnwatchedpages.
    Fixing issuses according to review.
    
    Change-Id: Ie32cc54abb6f888a433f7f18ef5ad747a80d7187

---
+++ resources/mediawiki.special/mediawiki.special.unwatchedpages.js
@@ -0,0 +1,30 @@
+/*
+ * JavaScript for Special:UnwatchedPages
+ *
+ */
+ //set the watch links to use Ajax
+ //see bug #17367
+( function ( $, mw ) {
+	 $(document).ready(function(){
+		 $('a.mw-watchLink').click(function(){
+			var api, watchedLi, link, toWatch;
+			api=new mw.Api();
+			watchedLi=$(this).parents('li');
+			link=$(this);
+			toWatch=(link.text() === mw.msg('watch'));
+			if(toWatch) {
+				api.watch(this.title, function(){
+					watchedLi.toggleClass('mw-watched-item');
+					link.text(mw.msg('unwatch'));
+				});
+			}
+			else {
+				api.unwatch(this.title, function(){
+					watchedLi.toggleClass('mw-watched-item');
+					link.text(mw.msg('watch'));
+				});
+			}
+			return false;
+		});
+	});
+}( jQuery, mediaWiki ) );
