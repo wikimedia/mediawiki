cb82979b64317e684686c2908fb7f76745094cb7
Reedy <reedy@wikimedia.org>
2012-05-02 14:31:03 +0100

    Bump version and update release notes for 1.19.0 release
    Change-Id: I5f6283950e0e4cbc32fe858ecbf18275c3f8df1e

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.0rc1';
+$wgVersion = '1.19.0';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
