2afabd9dcf8e51cce2cafa990855685fbd8faf5a
Aaron Schulz <aschulz@wikimedia.org>
2012-05-06 11:49:59 -0700

    [FileBackend] Changed copy script to use batches for concurrency.
    Change-Id: Id8c3a89a4cb30978f66db3bf95ebfebc0a9c01b4

--- maintenance/copyFileBackend.php
+++ maintenance/copyFileBackend.php
@@ -31,11 +31,12 @@ require_once( dirname( __FILE__ ) . '/Maintenance.php' );
 class CopyFileBackend extends Maintenance {
 	public function __construct() {
 		parent::__construct();
-		$this->mDescription = "Copy all the files in one backend to another.";
+		$this->mDescription = "Copy files in one backend to another.";
 		$this->addOption( 'src', 'Backend containing the source files', true, true );
 		$this->addOption( 'dst', 'Backend where files should be copied to', true, true );
 		$this->addOption( 'containers', 'Pipe separated list of containers', true, true );
 		$this->addOption( 'fast', 'Skip SHA-1 checks on pre-existing files' );
+		$this->mBatchSize = 50;
 	}
 
 	public function execute() {
@@ -44,47 +45,65 @@ class CopyFileBackend extends Maintenance {
 
 		$containers = explode( '|', $this->getOption( 'containers' ) );
 		foreach ( $containers as $container ) {
-			$this->output( "Doing container $container...\n" );
+			$this->output( "Doing container '$container'...\n" );
 
 			$srcPathsRel = $src->getFileList(
 				array( 'dir' => $src->getRootStoragePath() . "/$container" ) );
 			if ( $srcPathsRel === null ) {
 				$this->error( "Could not list files in $container.", 1 ); // die
 			}
+
+			$batchPaths = array();
 			foreach ( $srcPathsRel as $srcPathRel ) {
-				$srcPath = $src->getRootStoragePath() . "/$container/$srcPathRel";
-				$dstPath = $dst->getRootStoragePath() . "/$container/$srcPathRel";
-
-				if ( $dst->fileExists( array( 'src' => $dstPath, 'latest' => 1 ) ) ) {
-					if ( $this->hasOption( 'fast' ) ) {
-						$this->output( "Already have $dstPath.\n" );
-						continue; // assume already copied...
-					}
-					$srcSha1 = $src->getFileSha1Base36( array( 'src' => $srcPath ) );
-					$dstSha1 = $dst->getFileSha1Base36( array( 'src' => $dstPath ) );
-					if ( $srcSha1 && $srcSha1 === $dstSha1 ) {
-						$this->output( "Already have $dstPath.\n" );
-						continue; // already copied...
-					}
+				$batchPaths[$srcPathRel] = 1; // remove duplicates
+				if ( count( $batchPaths ) >= $this->mBatchSize ) {
+					$this->syncFileBatch( array_keys( $batchPaths ), $container, $src, $dst );
+					$batchPaths = array(); // done
 				}
-
-				$fsFile = $src->getLocalReference( array( 'src' => $srcPath, 'latest' => 1 ) );
-				if ( !$fsFile ) {
-					$this->error( "Could not get local copy of $srcPath.", 1 ); // die
-				}
-
-				$status = $dst->prepare( array( 'dir' => dirname( $dstPath ) ) );
-				$status->merge( $dst->store(
-					array( 'src' => $fsFile->getPath(), 'dst' => $dstPath ),
-					array( 'nonLocking' => 1, 'nonJournaled' => 1 )
-				) );
-				if ( !$status->isOK() ) {
-					print_r( $status->getErrorsArray() );
-					$this->error( "Could not copy $srcPath to $dstPath.", 1 ); // die
-				}
-
-				$this->output( "Copied $srcPath to $dstPath.\n" );
 			}
+			if ( count( $batchPaths ) ) { // left-overs
+				$this->syncFileBatch( array_keys( $batchPaths ), $container, $src, $dst );
+				$batchPaths = array(); // done
+			}
+
+			$this->output( "Finished container '$container'.\n" );
+		}
+	}
+
+	protected function syncFileBatch(
+		array $srcPathsRel, $container, FileBackend $src, FileBackend $dst
+	) {
+		$ops = array();
+		foreach ( $srcPathsRel as $srcPathRel ) {
+			$srcPath = $src->getRootStoragePath() . "/$container/$srcPathRel";
+			$dstPath = $dst->getRootStoragePath() . "/$container/$srcPathRel";
+			if ( $dst->fileExists( array( 'src' => $dstPath, 'latest' => 1 ) ) ) {
+				if ( $this->hasOption( 'fast' ) ) {
+					$this->output( "Already have $srcPathRel.\n" );
+					continue; // assume already copied...
+				}
+			}
+			// Note: getLocalReference() is fast for FS backends
+			$fsFile = $src->getLocalReference( array( 'src' => $srcPath, 'latest' => 1 ) );
+			if ( !$fsFile ) {
+				$this->error( "Could not get local copy of $srcPath.", 1 ); // die
+			}
+			// Note: prepare() is usually fast for key/value backends
+			$status = $dst->prepare( array( 'dir' => dirname( $dstPath ) ) );
+			if ( !$status->isOK() ) {
+				print_r( $status->getErrorsArray() );
+				$this->error( "Could not copy $srcPath to $dstPath.", 1 ); // die
+			}
+			$ops[] = array( 'op' => 'store',
+				'src' => $fsFile->getPath(), 'dst' => $dstPath, 'overwriteSame' => 1 );
+		}
+
+		$status = $dst->doOperations( $ops, array( 'nonLocking' => 1, 'nonJournaled' => 1 ) );
+		if ( !$status->isOK() ) {
+			print_r( $status->getErrorsArray() );
+			$this->error( "Could not copy file batch.", 1 ); // die
+		} else {
+			$this->output( "Copied these file(s):\n" . implode( "\n", $srcPathsRel ) . "\n" );
 		}
 	}
 }
