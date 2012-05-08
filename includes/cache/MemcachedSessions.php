4471b932535bce4314e2e54a3bbf22d1d3d2a1b4
Alexandre Emsenhuber <ialex.wiki@gmail.com>
2012-05-08 14:51:21 +0200

    Added missing GPLv2 headers in some places.
    Also made file/class documentation more consistent.
    
    Change-Id: I604ed8ddc572e89001e49df6740ab42307bc0330

--- includes/cache/MemcachedSessions.php
+++ includes/cache/MemcachedSessions.php
@@ -1,10 +1,27 @@
 <?php
 /**
+ * Session storage in object cache.
+ *
  * This file gets included if $wgSessionsInMemcache is set in the config.
  * It redirects session handling functions to store their data in memcached
  * instead of the local filesystem. Depending on circumstances, it may also
  * be necessary to change the cookie settings to work across hostnames.
  * See: http://www.php.net/manual/en/function.session-set-save-handler.php
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
  * @ingroup Cache
