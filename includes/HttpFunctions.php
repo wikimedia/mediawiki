wikimedia/mediawiki

commit ded6170bd2072971f72db794f3ab1625a8e13475

Treat truncated HTTP requests as errors.
We detect such prematurely ended request when there's a Content-Length
greater than the body we got. A strict comparison would easily fail
when there was a content encoding layer, but hopefully we won't hit
a compressed request _larger_ than the original content.

See http://thread.gmane.org/gmane.org.wikimedia.mediawiki/39622
for breakage caused by truncated HTTP replies.

Change-Id: I71418424730d46a781bde5cbfda8038457ec79c5

includes/HttpFunctions.php
@@ -756,6 +756,12 @@ class CurlHttpRequest extends MWHttpRequest {
 		$this->parseHeader();
 		$this->setStatus();
 
+		if ( isset( $this->respHeaders['content-length'] ) ) {
+			if ( strlen( $this->content ) < $this->respHeaders['content-length'] ) {
+				$this->status->fatal( 'http-truncated-body' );
+			}
+		}
+
 		return $this->status;
 	}
 

languages/messages/MessagesEn.php
@@ -2347,6 +2347,7 @@ For optimal security, img_auth.php is disabled.',
 'http-curl-error'       => 'Error fetching URL: $1',
 'http-host-unreachable' => 'Could not reach URL.',
 'http-bad-status'       => 'There was a problem during the HTTP request: $1 $2',
+'http-truncated-body'   => 'The request body was only partially received.',
 
 # Some likely curl errors. More could be added from <http://curl.haxx.se/libcurl/c/libcurl-errors.html>
 'upload-curl-error6'       => 'Could not reach URL',

languages/messages/MessagesQqq.php
@@ -2016,6 +2016,7 @@ Siebrand think this has to do with allowing MediaWiki to fetch remote URLs, and 
 
 If \'scheme\' is difficult to translate, then you could use \'prefix\' instead.',
 'http-bad-status' => '$1 is an HTTP error code (e.g. 404), $2 is the HTTP error message (e.g. File Not Found)',
+'http-truncated-body'   => 'Seems the connection closed prematurely. The HTTP response contained a Content-length greated than the received body.',
 
 'license' => 'This appears in the upload form for the license drop-down. The header in the file description page is now at {{msg-mw|License-header}}.',
 'nolicense' => '{{Identical|None selected}}',

maintenance/language/messages.inc
@@ -1445,6 +1445,7 @@ $wgMessageStructure = array(
 		'http-curl-error',
 		'http-host-unreachable',
 		'http-bad-status',
+		'http-truncated-body',
 	),
 
 	'upload-curl-errors' => array(

commit 8c1204327785343c6e7f6171d75a0350b9063e83

[HTTP] Fixed ded6170bd2072971f72db794f3ab1625a8e13475, which broke all HTTP calls.
* Instant Commons was reported as being totally broken by this.
* Also cleaned up a bit of w/s in foreign repo code while in the area.

Change-Id: I429b0a36618cc2b873077516e9c1023dc5b7d693

includes/HttpFunctions.php
@@ -757,7 +757,7 @@ class CurlHttpRequest extends MWHttpRequest {
 		$this->setStatus();
 
 		if ( isset( $this->respHeaders['content-length'] ) ) {
-			if ( strlen( $this->content ) < $this->respHeaders['content-length'] ) {
+			if ( strlen( $this->content ) < $this->getResponseHeader( 'content-length' ) ) {
 				$this->status->fatal( 'http-truncated-body' );
 			}
 		}

includes/filerepo/ForeignAPIRepo.php
@@ -130,8 +130,8 @@ class ForeignAPIRepo extends FileRepo {
 
 		$query = array_merge( $query,
 			array(
-				'format' => 'json',
-				'action' => 'query',
+				'format'    => 'json',
+				'action'    => 'query',
 				'redirects' => 'true'
 			) );
 		if ( $this->mApiBase ) {

includes/filerepo/file/ForeignAPIFile.php
@@ -54,9 +54,9 @@ class ForeignAPIFile extends File {
 	 */
 	static function newFromTitle( Title $title, $repo ) {
 		$data = $repo->fetchImageQuery( array(
-			'titles' => 'File:' . $title->getDBKey(),
-			'iiprop' => self::getProps(),
-			'prop'   => 'imageinfo',
+			'titles'            => 'File:' . $title->getDBKey(),
+			'iiprop'            => self::getProps(),
+			'prop'              => 'imageinfo',
 			'iimetadataversion' => MediaHandler::getMetadataVersion()
 		) );
 