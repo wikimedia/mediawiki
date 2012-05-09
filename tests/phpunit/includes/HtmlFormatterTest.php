6acc4e8acd0f995ea4a9e8662b63966e1ee6d6fb
Max Semenik <maxsem.wiki@gmail.com>
2012-05-09 15:00:20 +0400

    First shot at integrating MobileFrontend into core: move prop=extracts here
    It didn't quite belong in MF in the first place, but it used some parts of
    MF's infrastructure that are moving into core, too.
    
    Patchset 2: @since, rm trailing ws
    Patchset 3: more ws, license headers and deadbeef removed from tests
    Patchset 4: merge bug 37049 fix from MobileFrontend
    
    Change-Id: I4d524fd7e8de6a96519b4a2cd28542589c7acda3

---
+++ tests/phpunit/includes/HtmlFormatterTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @group Mobile
+ */
+class HtmlFormatterTest extends MediaWikiTestCase {
+	/**
+	 * @dataProvider getHtmlData
+	 */
+	public function testTransform( $input, $expected, $callback = false ) {
+		$input = self::normalize( $input );
+		$formatter = new HtmlFormatter( HtmlFormatter::wrapHTML( $input ) );
+		$formatter->setHtmlMode( true );
+		if ( $callback ) {
+			$callback( $formatter );
+		}
+		$formatter->filterContent();
+		$html = $formatter->getText();
+		$this->assertEquals( self::normalize( $expected ), self::normalize( $html ) );
+	}
+
+	private static function normalize( $s ) {
+		return str_replace( "\n", '',
+			str_replace( "\r", '', $s ) // "yay" to Windows!
+		);
+	}
+
+	public function getHtmlData() {
+		$disableImages = function( HtmlFormatter $f ) {
+			$f->removeImages();
+		};
+		$removeTags = function( HtmlFormatter $f ) {
+			$f->remove( array( 'table', '.foo', '#bar', 'div.baz' ) );
+			$f->whitelistIds( 'jedi' );
+		};
+		$flattenSomeStuff = function( HtmlFormatter $f ) {
+			$f->flatten( array( 's', 'div' ) );
+		};
+		$flattenEverything = function( HtmlFormatter $f ) {
+			$f->flattenAllTags();
+		};
+		return array(
+			// remove images if asked
+			array(
+				'<img src="/foo/bar.jpg">Blah</img>',
+				'Blah',
+				$disableImages,
+			),
+			// basic tag removal
+			array(
+				'<table><tr><td>foo</td></tr></table><div class="foo">foo</div><span id="bar">bar</span>
+<strong class="foo" id="bar">foobar</strong><div class="notfoo">test</div><div class="baz"/>
+<span class="baz">baz</span> <span class="foo" id="jedi">jedi</span>',
+
+				'<div class="notfoo">test</div>
+<span class="baz">baz</span> <span class="foo" id="jedi">jedi</span>',
+				$removeTags,
+			),
+			// don't flatten tags that start like chosen ones
+			array(
+				'<div><s>foo</s> <span>bar</span></div>',
+				'foo <span>bar</span>',
+				$flattenSomeStuff,
+			),
+			// total flattening
+			array(
+				'<div style="foo">bar<sup>2</sup></div>',
+				'bar2',
+				$flattenEverything,
+			),
+			// UTF-8 preservation and security
+			array(
+				'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&#38;&#0038;&#x26;&#x026;',
+				'<span title="&quot; \' &amp;">&lt;Тест!&gt;</span> &amp;&lt;&amp;&amp;&amp;&amp;',
+			),
+		);
+	}
+}
