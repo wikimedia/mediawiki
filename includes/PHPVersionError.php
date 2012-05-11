d1f8640a3b95243ce0d28564b07ed5da336de542
Alexandre Emsenhuber <ialex.wiki@gmail.com>
2012-05-11 10:34:29 +0200

    Added missing GPLv2 headers in some places.
    Also made file/class documentation more consistent.
    
    Change-Id: Ia45d0756cd84138da267bf1cbd1a58be7f28c6d1

--- includes/PHPVersionError.php
+++ includes/PHPVersionError.php
@@ -1,6 +1,27 @@
 <?php
 /**
  * Display something vaguely comprehensible in the event of a totally unrecoverable error.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Display something vaguely comprehensible in the event of a totally unrecoverable error.
  * Does not assume access to *anything*; no globals, no autloader, no database, no localisation.
  * Safe for PHP4 (and putting this here means that WebStart.php and GlobalSettings.php
  * no longer need to be).
