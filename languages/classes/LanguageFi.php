ed68864ecd47ce8ef80be5499cdf301b4d2a0c1a
jeblad <john.blad@wikimedia.de>
2012-05-12 21:04:51 +0200

    Minor spelling correction

--- languages/classes/LanguageFi.php
+++ languages/classes/LanguageFi.php
@@ -81,7 +81,7 @@ class LanguageFi extends Language {
 			'monday' => 'maanantai',
 			'tuesday' => 'tiistai',
 			'wednesday' => 'keskiviikko',
-			'thursay' => 'torstai',
+			'thurday' => 'torstai',
 			'friday' => 'perjantai',
 			'saturday' => 'lauantai',
 			'sunday' => 'sunnuntai',
