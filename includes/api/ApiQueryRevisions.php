b87454bf5c90ca78f15dfa2965434a15d34277c2
umherirrender <umherirrender_de.wp@web.de>
2012-05-12 21:42:04 +0200

    prop=revisions: join user table to get user information
    Change-Id: I5d8983476e03cc2f6dad074577402f5255080838

--- includes/api/ApiQueryRevisions.php
+++ includes/api/ApiQueryRevisions.php
@@ -224,6 +224,13 @@ class ApiQueryRevisions extends ApiQueryBase {
 			}
 		}
 
+		// need user information
+		if ( $this->fld_user || $this->fld_userid ) {
+			$this->addTables( 'user' );
+			$this->addJoinConds( array( 'user' => Revision::userJoinCond() ) );
+			$this->addFields( Revision::selectUserFields() );
+		}
+
 		// Bug 24166 - API error when using rvprop=tags
 		$this->addTables( 'revision' );
 
