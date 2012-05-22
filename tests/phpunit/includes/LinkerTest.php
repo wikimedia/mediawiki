698f6069bb7c2c69fde2dbbf6e4803aa716cb5f6
csteipp <csteipp@wikimedia.org>
2012-05-22 23:30:07 +0000

    (bug 36780)  Improve display of IPv6 addresses
    Also add partial tests for Linker::userLink()
    
    Change-Id: I680a59bb0ec3ca1d85ed12b0debb3446c73a3aab

---
+++ tests/phpunit/includes/LinkerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+class LinkerTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider provideCasesForUserLink
+	 * @cover Linker::userLink
+	 */
+	function testUserLink( $expected, $userId, $userName, $altUserName = false, $msg='' ) {
+		$this->assertEquals( $expected,
+			Linker::userLink( $userId, $userName, $altUserName, $msg )
+		);
+	}
+
+	function provideCasesForUserLink() {
+		# Format:
+		# - expected
+		# - userid
+		# - username
+		# - optional altUserName
+		# - optional message
+		return array(
+
+			### ANONYMOUS USER ########################################
+			array(
+				'<a href="/index.php/Special:Contributions/JohnDoe" title="Special:Contributions/JohnDoe" class="mw-userlink">JohnDoe</a>',
+				0, 'JohnDoe', false,
+		   	),
+			array(
+				'<a href="/index.php/Special:Contributions/::1" title="Special:Contributions/::1" class="mw-userlink">::1</a>',
+				0, '::1', false,
+				'Anonymous with pretty IPv6'
+		   	),
+			array(
+				'<a href="/index.php/Special:Contributions/0:0:0:0:0:0:0:1" title="Special:Contributions/0:0:0:0:0:0:0:1" class="mw-userlink">::1</a>',
+				0, '0:0:0:0:0:0:0:1', false,
+				'Anonymous with almost pretty IPv6'
+		   	),
+			array(
+				'<a href="/index.php/Special:Contributions/0000:0000:0000:0000:0000:0000:0000:0001" title="Special:Contributions/0000:0000:0000:0000:0000:0000:0000:0001" class="mw-userlink">::1</a>',
+				0, '0000:0000:0000:0000:0000:0000:0000:0001', false,
+				'Anonymous with full IPv6'
+		   	),
+			array(
+				'<a href="/index.php/Special:Contributions/::1" title="Special:Contributions/::1" class="mw-userlink">AlternativeUsername</a>',
+				0, '::1', 'AlternativeUsername',
+				'Anonymous with pretty IPv6 and an alternative username'
+			),
+
+			# IPV4
+			array(
+				'<a href="/index.php/Special:Contributions/127.0.0.1" title="Special:Contributions/127.0.0.1" class="mw-userlink">127.0.0.1</a>',
+				0, '127.0.0.1', false,
+				'Anonymous with IPv4'
+			),
+			array(
+				'<a href="/index.php/Special:Contributions/127.0.0.1" title="Special:Contributions/127.0.0.1" class="mw-userlink">AlternativeUsername</a>',
+				0, '127.0.0.1', 'AlternativeUsername',
+				'Anonymous with IPv4 and an alternative username'
+			),
+
+			### Regular user ##########################################
+			# TODO!
+		);
+	}
+}
