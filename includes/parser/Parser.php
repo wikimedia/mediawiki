ea22180c4c5f01043da5b964d4080c91238fd7cb
Dan Collins <dcollinsn@gmail.com>
2012-05-24 12:24:56 -0400

    (Bug 2700) Pre-save transform inside extension hooks
    Patch provided by bluehairedlawyer on Bugzilla, I also weeded through
    the provided parser tests to see which ones belonged in trunk. Tests
    now pass and this works correctly for any extension which uses it.
    <gallery> tag now supported. Patches for extensions to follow.
    
    Change-Id: I1cb6750dd5ba9db9fbcccf74f8987e80d011210d

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -124,6 +124,7 @@ class Parser {
 	var $mFunctionTagHooks = array();
 	var $mStripList  = array();
 	var $mDefaultStripList  = array();
+	var $mPstAcceptableXmlTags = array();
 	var $mVarCache = array();
 	var $mImageParams = array();
 	var $mImageParamsMagicArray = array();
@@ -205,6 +206,11 @@ class Parser {
 	 * duplicate language links to the ParserOutput.
 	 */
 	var $mLangLinkLanguages;
+
+	/**
+	 * @var boolean
+	 */
+	var $mDuringPreSaveTransform = false;
 
 	/**
 	 * Constructor
@@ -895,7 +901,11 @@ class Parser {
 	 * @return array
 	 */
 	function getStripList() {
-		return $this->mStripList;
+		if ( $this->mDuringPreSaveTransform ) {
+			return $this->mStripList;
+		} else {
+			return array_merge( $this->mStripList, $this->mPstAcceptableXmlTags );
+		}
 	}
 
 	/**
@@ -4412,6 +4422,8 @@ class Parser {
 		$this->startParse( $title, $options, self::OT_WIKI, $clearState );
 		$this->setUser( $user );
 
+		$this->mDuringPreSaveTransform = true;
+
 		$pairs = array(
 			"\r\n" => "\n",
 		);
@@ -4422,6 +4434,8 @@ class Parser {
 		$text = $this->mStripState->unstripBoth( $text );
 
 		$this->setUser( null ); #Reset
+
+		$this->mDuringPreSaveTransform = false;
 
 		return $text;
 	}
@@ -4713,15 +4727,20 @@ class Parser {
 	 * @throws MWException
 	 * @return Mixed|null The old value of the mTagHooks array associated with the hook
 	 */
-	public function setHook( $tag, $callback ) {
+	public function setHook( $tag, $callback, $options = array() ) {
 		$tag = strtolower( $tag );
 		if ( preg_match( '/[<>\r\n]/', $tag, $m ) ) {
 			throw new MWException( "Invalid character {$m[0]} in setHook('$tag', ...) call" );
 		}
 		$oldVal = isset( $this->mTagHooks[$tag] ) ? $this->mTagHooks[$tag] : null;
 		$this->mTagHooks[$tag] = $callback;
-		if ( !in_array( $tag, $this->mStripList ) ) {
-			$this->mStripList[] = $tag;
+
+		if ( isset( $options['preSaveTrans'] ) && $options['preSaveTrans'] === true ) {
+			if ( !in_array( $tag, $this->mPstAcceptableXmlTags ) ) {
+				$this->mPstAcceptableXmlTags[] = $tag;
+			}
+		} elseif ( !in_array( $tag, $this->mStripList ) ) {
+				$this->mStripList[] = $tag;
 		}
 
 		return $oldVal;
@@ -4762,6 +4781,8 @@ class Parser {
 		$this->mTagHooks = array();
 		$this->mFunctionTagHooks = array();
 		$this->mStripList = $this->mDefaultStripList;
+		$this->mPstAcceptableXmlTags = array();
+		$this->mDuringPreSaveTransform = false;
 	}
 
 	/**
