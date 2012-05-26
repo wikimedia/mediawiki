5c557e630ab6363cf59ede57976cc025ed0a010d
Matthias Mullie <git@mullie.eu>
2012-05-25 17:42:47 -0700

    mediawiki.jqueryMsg: Fix double-escaped attributes in the parser
    E.g.: message: "Your feedback has been [$1 posted here]."
    In mediawiki.jqueryMsg.js, L31, the parser would (rightfully) escape
    XML entities in the parameter (a link) being assigned through $1
    
    When actually adding the (already escaped) parameter as href-
    attribute of the link-to-be-built, it'll automatically have its HTML
    entities escaped once again (thus double-escaping &'s)
    
    This patch will identify regular links and links with replacement-
    url's in a different way; now the escaping can be moved to when the
    variables are actually being replaced (for normal parameter
    replacement) and can be processed differently (= not escaping) when
    dealing with a link.
    
    Tests:
     - Removed useless ok() tests. I accidentally introduced that pattern
       last year for no good reason. Slowly getting it out again. It
       either can't return false, or it tests unrelated things that have
       their own tests.
     - Simplified examples to reduce repetition of unrelated strings.
     - Improved assertion titles to better describe what is being tested
       to help find actual problems.
       (e.g. "Neutral from mw.user object" instead of
       "Gender neutral or unknown").
    
    Change-Id: If060b75f5575f4c7c3a25e5280ae697171120e84

--- tests/qunit/suites/resources/mediawiki/mediawiki.jqueryMsg.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.jqueryMsg.test.js
@@ -1,55 +1,105 @@
 module( 'mediawiki.jqueryMsg' );
 
-test( '-- Initial check', function() {
+test( '-- Initial check', function () {
 	expect( 1 );
 	ok( mw.jqueryMsg, 'mw.jqueryMsg defined' );
 } );
 
-test( 'mw.jqueryMsg Plural', function() {
-	expect( 5 );
+test( 'mw.jqueryMsg Plural', function () {
+	expect( 3 );
 	var parser = mw.jqueryMsg.getMessageFunction();
-	ok( parser, 'Parser Function initialized' );
-	ok( mw.messages.set( 'plural-msg', 'Found $1 {{PLURAL:$1|item|items}}' ), 'mw.messages.set: Register' );
-	equal( parser( 'plural-msg', 0 ) , 'Found 0 items', 'Plural test for english with zero as count' );
-	equal( parser( 'plural-msg', 1 ) , 'Found 1 item', 'Singular test for english' );
-	equal( parser( 'plural-msg', 2 ) , 'Found 2 items', 'Plural test for english' );
+
+	mw.messages.set( 'plural-msg', 'Found $1 {{PLURAL:$1|item|items}}' );
+	equal( parser( 'plural-msg', 0 ), 'Found 0 items', 'Plural test for english with zero as count' );
+	equal( parser( 'plural-msg', 1 ), 'Found 1 item', 'Singular test for english' );
+	equal( parser( 'plural-msg', 2 ), 'Found 2 items', 'Plural test for english' );
 } );
 
 
-test( 'mw.jqueryMsg Gender', function() {
-	expect( 16 );
-	//TODO: These tests should be for mw.msg once mw.msg integrated with mw.jqueryMsg
-	var user = mw.user;
+test( 'mw.jqueryMsg Gender', function () {
+	expect( 11 );
+	// TODO: These tests should be for mw.msg once mw.msg integrated with mw.jqueryMsg
+	// TODO: English may not be the best language for these tests. Use a language like Arabic or Russian
+	var user = mw.user,
+		parser = mw.jqueryMsg.getMessageFunction();
+
+	// The values here are not significant,
+	// what matters is which of the values is choosen by the parser
+	mw.messages.set( 'gender-msg', '$1: {{GENDER:$2|blue|pink|green}}' );
+
 	user.options.set( 'gender', 'male' );
-	var parser = mw.jqueryMsg.getMessageFunction();
-	ok( parser, 'Parser Function initialized' );
-	//TODO: English may not be the best language for these tests. Use a language like Arabic or Russian
-	ok( mw.messages.set( 'gender-msg', '$1 reverted {{GENDER:$2|his|her|their}} last edit' ), 'mw.messages.set: Register' );
-	equal( parser( 'gender-msg', 'Bob', 'male' ) , 'Bob reverted his last edit', 'Gender masculine' );
-	equal( parser( 'gender-msg', 'Bob', user ) , 'Bob reverted his last edit', 'Gender masculine' );
+	equal(
+		parser( 'gender-msg', 'Bob', 'male' ),
+		'Bob: blue',
+		'Masculine from string "male"'
+	);
+	equal(
+		parser( 'gender-msg', 'Bob', user ),
+		'Bob: blue',
+		'Masculine from mw.user object'
+	);
+
 	user.options.set( 'gender', 'unknown' );
-	equal( parser( 'gender-msg', 'They', user ) , 'They reverted their last edit', 'Gender neutral or unknown' );
-	equal( parser( 'gender-msg', 'Alice', 'female' ) , 'Alice reverted her last edit', 'Gender feminine' );
-	equal( parser( 'gender-msg', 'User' ) , 'User reverted their last edit', 'Gender neutral' );
-	equal( parser( 'gender-msg', 'User', 'unknown' ) , 'User reverted their last edit', 'Gender neutral' );
-	ok( mw.messages.set( 'gender-msg-one-form', '{{GENDER:$1|User}} reverted last $2 {{PLURAL:$2|edit|edits}}' ), 'mw.messages.set: Register' );
-	equal( parser( 'gender-msg-one-form', 'male', 10 ) , 'User reverted last 10 edits', 'Gender neutral and plural form' );
-	equal( parser( 'gender-msg-one-form', 'female', 1 ) , 'User reverted last 1 edit', 'Gender neutral and singular form' );
-	ok( mw.messages.set( 'gender-msg-lowercase', '{{gender:$1|he|she}} is awesome' ), 'mw.messages.set: Register' );
-	equal( parser( 'gender-msg-lowercase', 'male' ) , 'he is awesome', 'Gender masculine' );
-	equal( parser( 'gender-msg-lowercase', 'female' ) , 'she is awesome', 'Gender feminine' );
-	ok( mw.messages.set( 'gender-msg-wrong', '{{gender}} is awesome' ), 'mw.messages.set: Register' );
-	equal( parser( 'gender-msg-wrong', 'female' ) , ' is awesome', 'Wrong syntax used, but ignore the {{gender}}' );
+	equal(
+		parser( 'gender-msg', 'Foo', user ),
+		'Foo: green',
+		'Neutral from mw.user object' );
+	equal(
+		parser( 'gender-msg', 'Alice', 'female' ),
+		'Alice: pink',
+		'Feminine from string "female"' );
+	equal(
+		parser( 'gender-msg', 'User' ),
+		'User: green',
+		'Neutral when no parameter given' );
+	equal(
+		parser( 'gender-msg', 'User', 'unknown' ),
+		'User: green',
+		'Neutral from string "unknown"'
+	);
+
+	mw.messages.set( 'gender-msg-one-form', '{{GENDER:$1|User}}: $2 {{PLURAL:$2|edit|edits}}' );
+
+	equal(
+		parser( 'gender-msg-one-form', 'male', 10 ),
+		'User: 10 edits',
+		'Gender neutral and plural form'
+	);
+	equal(
+		parser( 'gender-msg-one-form', 'female', 1 ),
+		'User: 1 edit',
+		'Gender neutral and singular form'
+	);
+
+	mw.messages.set( 'gender-msg-lowercase', '{{gender:$1|he|she}} is awesome' );
+	equal(
+		parser( 'gender-msg-lowercase', 'male' ),
+		'he is awesome',
+		'Gender masculine'
+	);
+	equal(
+		parser( 'gender-msg-lowercase', 'female' ),
+		'she is awesome',
+		'Gender feminine'
+	);
+
+	mw.messages.set( 'gender-msg-wrong', '{{gender}} test' );
+	equal(
+		parser( 'gender-msg-wrong', 'female' ),
+		' test',
+		'Invalid syntax should result in {{gender}} simply being stripped away'
+	);
 } );
 
 
-test( 'mw.jqueryMsg Grammar', function() {
-	expect( 5 );
+test( 'mw.jqueryMsg Grammar', function () {
+	expect( 2 );
 	var parser = mw.jqueryMsg.getMessageFunction();
-	ok( parser, 'Parser Function initialized' );
-	// Hope the grammar form grammar_case_foo is not valid in any language
-	ok( mw.messages.set( 'grammar-msg', 'Przeszukaj {{GRAMMAR:grammar_case_foo|{{SITENAME}}}}' ), 'mw.messages.set: Register' );
-	equal( parser( 'grammar-msg' ) , 'Przeszukaj ' + mw.config.get( 'wgSiteName' ) , 'Grammar Test with sitename' );
-	ok( mw.messages.set( 'grammar-msg-wrong-syntax', 'Przeszukaj {{GRAMMAR:grammar_case_xyz}}' ), 'mw.messages.set: Register' );
-	equal( parser( 'grammar-msg-wrong-syntax' ) , 'Przeszukaj ' , 'Grammar Test with wrong grammar template syntax' );
+
+	// Assume the grammar form grammar_case_foo is not valid in any language
+	mw.messages.set( 'grammar-msg', 'Przeszukaj {{GRAMMAR:grammar_case_foo|{{SITENAME}}}}' );
+	equal( parser( 'grammar-msg' ), 'Przeszukaj ' + mw.config.get( 'wgSiteName' ), 'Grammar Test with sitename' );
+
+	mw.messages.set( 'grammar-msg-wrong-syntax', 'Przeszukaj {{GRAMMAR:grammar_case_xyz}}' );
+	equal( parser( 'grammar-msg-wrong-syntax' ), 'Przeszukaj ' , 'Grammar Test with wrong grammar template syntax' );
 } );
