72a00e4e08c0d56e0b323796163dda0502a9419f
Liangent <liangent@gmail.com>
2012-05-29 11:40:51 +0800

    Add parameters to LanguageQqx-generated messages.
    This avoids outputing bunch of unhelpful (parentheses) messages.
    
    PS2: rebased to master
    
    Change-Id: I55fab4f132f28be3fe7e4bc8f51d403b11d24284

--- languages/classes/LanguageQqx.php
+++ languages/classes/LanguageQqx.php
@@ -9,11 +9,27 @@
  * @ingroup Language
  */
 class LanguageQqx extends Language {
+	var $languageEn;
+
 	/**
 	 * @param $key string
 	 * @return string
 	 */
 	function getMessage( $key ) {
-		return "($key)";
+		if ( !$this->languageEn ) {
+			$this->languageEn = Language::factory( 'en' );
+		}
+		$message = "($key";
+		$messageEn = $this->languageEn->getMessage( $key );
+		$numParams = 1;
+		while ( strpos( $messageEn, '$' . $numParams ) !== false ) {
+			if ( $numParams === 1 ) {
+				$message .= ': $1';
+			} else {
+				$message .= ', $' . $numParams;
+			}
+			$numParams++;
+		}
+		return $message . ')';
 	}
 }
