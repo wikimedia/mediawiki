f99f4945a5413f1c6840bcae00b47a734b339da2
robin <robinp.1273@gmail.com>
2012-05-30 23:06:26 +0200

    Change of Title->exists() to Title->isKnown() for subpage links
    isKnown() calls both exists() and isAlwaysKnown(), so subpage links below the page title are also added for pages that "always exist", as can be changed through a hook
    
    Change-Id: I345bb8e6e611afd53afbc7b2d92910e360f598ee

--- includes/Skin.php
+++ includes/Skin.php
@@ -727,7 +727,7 @@ abstract class Skin extends ContextSource {
 					$display .= $link;
 					$linkObj = Title::newFromText( $growinglink );
 
-					if ( is_object( $linkObj ) && $linkObj->exists() ) {
+					if ( is_object( $linkObj ) && $linkObj->isKnown() ) {
 						$getlink = Linker::linkKnown(
 							$linkObj,
 							htmlspecialchars( $display )
