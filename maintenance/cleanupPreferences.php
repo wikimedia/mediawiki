bf185f1c86e4d24f55dc94ab667fca32581a66c1
TyA <tya.wiki@gmail.com>
2012-05-30 04:49:17 +0100

    *(bug 30976) Add maintenance script to remove preferences from $wgHiddenPrefs
    Change-Id: Ie74fab1edeac2c6b275c51e6af3a7924b3aac46b

---
+++ maintenance/cleanupPreferences.php
@@ -0,0 +1,30 @@
+﻿<?php
+/**
+ * Description: This script takes $wgHiddenPrefs and removes their preference from the DB. [[bugzilla:30976]]
+ * @author TyA <tya.wiki@gmail.com>
+ * @ingroup Maintenance
+ */
+
+require_once( dirname( __FILE__ ) . '/Maintenance.php' );
+
+class CleanupPreferences extends Maintenance {
+	public function execute() {
+		global $wgHiddenPrefs;
+
+		$dbw = wfGetDB( DB_MASTER );
+
+		foreach( $wgHiddenPrefs as $item ) {
+			$dbw->delete(
+				'user_properties',
+				array( 'up_property' => $item ),
+				__METHOD__
+			);
+		};
+
+		$this->output('Finished!\n'); # Displays a finished message
+
+	}
+}
+
+$maintClass = 'CleanupPreferences'; // Tells it to run the class
+require_once( RUN_MAINTENANCE_IF_MAIN );
