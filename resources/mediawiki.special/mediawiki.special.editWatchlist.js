97b864f5cd2e505140c35578e604c5141b3ab76e
Eranroz <eranroz89@gmail.com>
2012-06-03 17:39:04 +0300

    AJAXify watchlist editor
    This patch makes the watchlist editor to use pagination. That would
    avoid old browsers crashing (bug 20483).
    Patch also add some AJAX operations to the editor, for example to remove
    items from watchlist (bug 32151).
    
    The AJAX support is an ALTERNATIVE to the form based method, to keep
    support for non javascript users.
    
    This change contains a required change in the API for watch operation,
    to allow batch operation, by support titles parameter. The old title
    (single page) parameter is still used to keep backward compatibility.
    
    Change-Id: I1d8c66db9ba6456858ef655397935a2b3a421632

---
+++ resources/mediawiki.special/mediawiki.special.editWatchlist.js
@@ -0,0 +1,47 @@
+/*
+ * JavaScript for Special:EditWatchlist
+ */
+
+/**
+ * Replace the submit button action to operate with ajax.
+ */
+( function ( mw, $ ) {
+	$( '#watchlistedit-submit' ).parents( 'form:first' ).on( 'submit.ajax', function ( e ) {
+		var titlesToRemove, params, api;
+		titlesToRemove = $.map( $( '.mw-htmlform-flatlist-item input:checked' ), function ( el ) {
+								return $( el ).val();
+							} ).join( '|' );
+		params = {
+			action: 'watch',
+			titles: titlesToRemove,
+			token: mw.user.tokens.get( 'watchToken' ),
+			unwatch: true
+		};
+		api = new mw.Api();
+		api.post( params,
+			{ ok: function ( data ) {
+				$( data.watch ).each( function ( e ) {
+					var removedItem = this.title;
+					var item = $( '.watchlist-item a' ).filter( function ( ) {
+								return this.title == removedItem;
+							} ).parents( '.mw-htmlform-flatlist-item' ).
+							fadeOut();
+				} );
+				$( '#watchlistedit-submit' ).prop( {
+					disabled: false,
+					value: mw.msg( 'watchlistedit-normal-submit' )
+				} );
+			},
+			err: function ( ) {
+				//some error occurred.
+				//re-enable the submit and try to send normal submit
+				$( '#watchlistedit-submit' ).prop( {
+					disabled: false,
+					value: mw.msg( 'watchlistedit-normal-submit' )
+				} );
+				$( '#watchlistedit-submit' ).parents( 'form:first' ).off( 'submit.ajax' ).submit();
+			} } );
+		$( '#watchlistedit-submit' ).prop( { disabled: true, value:  mw.msg( 'watchlistedit-normal-submitting')  } );
+		e.preventDefault();
+	} );
+} )( mediaWiki, jQuery );
