79dcce058e16f5bf0b6a9b1a98f9c0484f1baeb2
Dereckson <dereckson@espace-win.org>
2012-06-03 16:09:11 +0200

    (bug 37179) Allow undelete revision rows l10n
    Patch set 5: Update spacing in changes in SpecialUndelete.php
                 per convention.
    
    Change-Id: I95756fa53c1f3b2a62fa9b5f6a63475dd5462442

--- includes/specials/SpecialUndelete.php
+++ includes/specials/SpecialUndelete.php
@@ -1166,7 +1166,7 @@ class SpecialUndelete extends SpecialPage {
 	private function formatRevisionRow( $row, $earliestLiveTime, $remaining ) {
 		$rev = Revision::newFromArchiveRow( $row,
 			array( 'page' => $this->mTargetObj->getArticleID() ) );
-		$stxt = '';
+		$revTextSize = '';
 		$ts = wfTimestamp( TS_MW, $row->ar_timestamp );
 		// Build checkboxen...
 		if( $this->mAllowed ) {
@@ -1215,13 +1215,15 @@ class SpecialUndelete extends SpecialPage {
 		// Revision text size
 		$size = $row->ar_len;
 		if( !is_null( $size ) ) {
-			$stxt = Linker::formatRevisionSize( $size );
+			$revTextSize = Linker::formatRevisionSize( $size );
 		}
 		// Edit summary
 		$comment = Linker::revComment( $rev );
 		// Revision delete links
 		$revdlink = Linker::getRevDeleteLink( $user, $rev, $this->mTargetObj );
-		return "<li>$checkBox $revdlink ($last) $pageLink . . $userLink $stxt $comment</li>";
+
+		$revisionRow = $this->msg( 'undelete-revisionrow' )->rawParams( $checkBox, $revdlink, $last, $pageLink , $userLink, $revTextSize, $comment )->escaped();
+		return "<li>$revisionRow</li>";
 	}
 
 	private function formatFileRow( $row ) {
