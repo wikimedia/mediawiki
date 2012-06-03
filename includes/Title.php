d7f7a6234b36a3ab64df46ff46319f22d9f9ff1a
Brion VIBBER <brion@wikimedia.org>
2012-06-03 11:00:54 +0000

    Revert "Adding sanity check to Title::isRedirect()."
    This reverts commit 7d12b7b72c2060c58b6bd0759b80d985bb9431be

--- includes/Title.php
+++ includes/Title.php
@@ -2817,13 +2817,7 @@ class Title {
 			return $this->mRedirect = false;
 		}
 		$linkCache = LinkCache::singleton();
-		$cached = $linkCache->getGoodLinkFieldObj( $this, 'redirect' );
-
-		if ( $cached === null ) { # check the assumption that the cache actually knows about this title
-			throw new MWException( "LinkCache doesn't currently know about this title: " . $this->getPrefixedDBkey() );
-		}
-
-		$this->mRedirect = (bool)$cached;
+		$this->mRedirect = (bool)$linkCache->getGoodLinkFieldObj( $this, 'redirect' );
 
 		return $this->mRedirect;
 	}
