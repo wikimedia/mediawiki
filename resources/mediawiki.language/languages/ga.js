7c680ba0dca6867dc5d972f953ab8060f0c47eb5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-06-04 14:49:35 +0530

    Custom  grammar rules in javascript for some languages
    Splitted from https://gerrit.wikimedia.org/r/4554
    Grammar rules ported to js based on the php code.
    Depends on the javascript Grammar parsing support by jqueryMsg
    
    Change-Id: I701954a623467bf6c9abd3160e239c59cab567e9

--- resources/mediawiki.language/languages/ga.js
+++ resources/mediawiki.language/languages/ga.js
@@ -12,3 +12,38 @@ mediaWiki.language.convertPlural = function( count, forms ) {
 	}
 	return forms[2];
 };
+
+
+mediaWiki.language.convertGrammar = function( word, form ) {
+	var grammarForms = mw.language.getData( 'ga', 'grammarForms' );
+	if ( grammarForms && grammarForms[form] ) {
+		return grammarForms[form][word] ;
+	}
+	switch ( form ) {
+		case 'ainmlae':
+			switch ( word ) {
+				case 'an Domhnach':
+					word = 'Dé Domhnaigh';
+					break;
+				case 'an Luan':
+					word = 'Dé Luain';
+					break;
+				case 'an Mháirt':
+					word = 'Dé Mháirt';
+					break;
+				case 'an Chéadaoin':
+					word = 'Dé Chéadaoin';
+					break;
+				case 'an Déardaoin':
+					word = 'Déardaoin';
+					break;
+				case 'an Aoine':
+					word = 'Dé hAoine';
+					break;
+				case 'an Satharn':
+					word = 'Dé Sathairn';
+					break;
+			}
+	}
+	return word;
+};
