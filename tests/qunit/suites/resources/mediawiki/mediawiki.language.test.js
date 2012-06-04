d2bc01b702f4d37ae6917ce5b499a2d4d03161d3
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-06-04 15:10:16 +0530

    Qunit tests for custom language rules for various languages.
    The language specific tests will be executed for the user language
    (see also bug 37321).
    
    Splitted from I4f3c7da84c4794fbe7bcbef52cfd49b1b7a3a521
    
    Change-Id: I9b1c991d00f4af1d8ee409f65f123a006f0eb443

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.language.test.js
@@ -0,0 +1,395 @@
+var liveLangData = mw.language.data.values;
+QUnit.module( 'mediawiki.language', QUnit.newMwEnvironment({
+	setup: function () {
+		// New independent copy for this test
+		mw.language.data = $.extend( {}, liveLangData, true );
+	},
+	teardown: function () {
+		// Restore original
+		mw.language.data = liveLangData;
+	}
+}) );
+
+QUnit.test( 'mw.language getData and setData', function ( assert ) {
+	QUnit.expect( 2 );
+
+	mw.language.setData( 'en', 'testkey', 'testvalue' );
+	assert.equal(  mw.language.getData( 'en', 'testkey' ), 'testvalue', 'Getter setter test for mw.language' );
+	assert.equal(  mw.language.getData( 'en', 'invalidkey' ), undefined, 'Getter setter test for mw.language with invalid key' );
+} );
+
+function grammarTest( langCode, test ) {
+	// The test works only if the content language is opt.language
+	// because it requires [lang].js to be loaded.
+	QUnit.test( '-- Grammar Test for ' + langCode, function ( assert ) {
+		QUnit.expect( test.length );
+
+		for ( var i = 0 ; i < test.length; i++ ) {
+			assert.equal(
+				mw.language.convertGrammar( test[i].word, test[i].grammarForm ),
+				test[i].expected,
+				test[i].description
+			);
+		}
+	});
+}
+
+var grammarTests = {
+	bs: [
+		{
+			word: 'word',
+			grammarForm: 'instrumental',
+			expected: 's word',
+			description: 'Grammar test for Bosnian, instrumental case'
+		},
+		{
+			word: 'word',
+			grammarForm: 'lokativ',
+			expected: 'o word',
+			description: 'Grammar test for Bosnian, lokativ case'
+		}
+	],
+
+	he: [
+		{
+			word: "ויקיפדיה",
+			grammarForm: 'prefixed',
+			expected: "וויקיפדיה",
+			description: 'Grammar test for Hebrew, Duplicate the "Waw" if prefixed'
+		},
+		{
+			word: "וולפגנג",
+			grammarForm: 'prefixed',
+			expected: "וולפגנג",
+			description: 'Grammar test for Hebrew, Duplicate the "Waw" if prefixed, but not if it is already duplicated.'
+		},
+		{
+			word: "הקובץ",
+			grammarForm: 'prefixed',
+			expected: "קובץ",
+			description: 'Grammar test for Hebrew, Remove the "He" if prefixed'
+		},
+		{
+			word: 'Wikipedia',
+			grammarForm: 'תחילית',
+			expected: '־Wikipedia',
+			description: 'Grammar test for Hebrew, Add a hyphen (maqaf) before non-Hebrew letters'
+		},
+		{
+			word: '1995',
+			grammarForm: 'תחילית',
+			expected: '־1995',
+			description: 'Grammar test for Hebrew, Add a hyphen (maqaf) before numbers'
+		}
+	],
+
+	hsb: [
+		{
+			word: 'word',
+			grammarForm: 'instrumental',
+			expected: 'z word',
+			description: 'Grammar test for Upper Sorbian, instrumental case'
+		},
+		{
+			word: 'word',
+			grammarForm: 'lokatiw',
+			expected: 'wo word',
+			description: 'Grammar test for Upper Sorbian, lokatiw case'
+		}
+	],
+
+	dsb: [
+		{
+			word: 'word',
+			grammarForm: 'instrumental',
+			expected: 'z word',
+			description: 'Grammar test for Lower Sorbian, instrumental case'
+		},
+		{
+			word: 'word',
+			grammarForm: 'lokatiw',
+			expected: 'wo word',
+			description: 'Grammar test for Lower Sorbian, lokatiw case'
+		}
+	],
+
+	hy: [
+		{
+			word: 'Մաունա',
+			grammarForm: 'genitive',
+			expected: 'Մաունայի',
+			description: 'Grammar test for Armenian, genitive case'
+		},
+		{
+			word: 'հետո',
+			grammarForm: 'genitive',
+			expected: 'հետոյի',
+			description: 'Grammar test for Armenian, genitive case'
+		},
+		{
+			word: 'գիրք',
+			grammarForm: 'genitive',
+			expected: 'գրքի',
+			description: 'Grammar test for Armenian, genitive case'
+		},
+		{
+			word: 'ժամանակի',
+			grammarForm: 'genitive',
+			expected: 'ժամանակիի',
+			description: 'Grammar test for Armenian, genitive case'
+		}
+	],
+
+	fi: [
+		{
+			word: 'talo',
+			grammarForm: 'genitive',
+			expected: 'talon',
+			description: 'Grammar test for Finnish, genitive case'
+		},
+		{
+			word: 'linux',
+			grammarForm: 'genitive',
+			expected: 'linuxin',
+			description: 'Grammar test for Finnish, genitive case'
+		},
+		{
+			word: 'talo',
+			grammarForm: 'elative',
+			expected: 'talosta',
+			description: 'Grammar test for Finnish, elative case'
+		},
+		{
+			word: 'pastöroitu',
+			grammarForm: 'partitive',
+			expected: 'pastöroitua',
+			description: 'Grammar test for Finnish, partitive case'
+		},
+		{
+			word: 'talo',
+			grammarForm: 'partitive',
+			expected: 'taloa',
+			description: 'Grammar test for Finnish, partitive case'
+		},
+		{
+			word: 'talo',
+			grammarForm: 'illative',
+			expected: 'taloon',
+			description: 'Grammar test for Finnish, illative case'
+		},
+		{
+			word: 'linux',
+			grammarForm: 'inessive',
+			expected: 'linuxissa',
+			description: 'Grammar test for Finnish, inessive case'
+		}
+	],
+
+	ru: [
+		{
+			word: 'тесть',
+			grammarForm: 'genitive',
+			expected: 'тестя',
+			description: 'Grammar test for Russian, genitive case'
+		},
+		{
+			word: 'привилегия',
+			grammarForm: 'genitive',
+			expected: 'привилегии',
+			description: 'Grammar test for Russian, genitive case'
+		},
+		{
+			word: 'установка',
+			grammarForm: 'genitive',
+			expected: 'установки',
+			description: 'Grammar test for Russian, genitive case'
+		},
+		{
+			word: 'похоти',
+			grammarForm: 'genitive',
+			expected: 'похотей',
+			description: 'Grammar test for Russian, genitive case'
+		},
+		{
+			word: 'доводы',
+			grammarForm: 'genitive',
+			expected: 'доводов',
+			description: 'Grammar test for Russian, genitive case'
+		},
+		{
+			word: 'песчаник',
+			grammarForm: 'genitive',
+			expected: 'песчаника',
+			description: 'Grammar test for Russian, genitive case'
+		}
+	],
+
+
+	hu: [
+		{
+			word: 'Wikipédiá',
+			grammarForm: 'rol',
+			expected: 'Wikipédiáról',
+			description: 'Grammar test for Hungarian, rol case'
+		},
+		{
+			word: 'Wikipédiá',
+			grammarForm: 'ba',
+			expected: 'Wikipédiába',
+			description: 'Grammar test for Hungarian, ba case'
+		},
+		{
+			word: 'Wikipédiá',
+			grammarForm: 'k',
+			expected: 'Wikipédiák',
+			description: 'Grammar test for Hungarian, k case'
+		}
+	],
+
+	ga: [
+		{
+			word: 'an Domhnach',
+			grammarForm: 'ainmlae',
+			expected: 'Dé Domhnaigh',
+			description: 'Grammar test for Irish, ainmlae case'
+		},
+		{
+			word: 'an Luan',
+			grammarForm: 'ainmlae',
+			expected: 'Dé Luain',
+			description: 'Grammar test for Irish, ainmlae case'
+		},
+		{
+			word: 'an Satharn',
+			grammarForm: 'ainmlae',
+			expected: 'Dé Sathairn',
+			description: 'Grammar test for Irish, ainmlae case'
+		}
+	],
+
+	uk: [
+		{
+			word: 'тесть',
+			grammarForm: 'genitive',
+			expected: 'тестя',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'Вікіпедія',
+			grammarForm: 'genitive',
+			expected: 'Вікіпедії',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'установка',
+			grammarForm: 'genitive',
+			expected: 'установки',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'похоти',
+			grammarForm: 'genitive',
+			expected: 'похотей',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'доводы',
+			grammarForm: 'genitive',
+			expected: 'доводов',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'песчаник',
+			grammarForm: 'genitive',
+			expected: 'песчаника',
+			description: 'Grammar test for Ukrainian, genitive case'
+		},
+		{
+			word: 'Вікіпедія',
+			grammarForm: 'accusative',
+			expected: 'Вікіпедію',
+			description: 'Grammar test for Ukrainian, accusative case'
+		}
+	],
+
+	sl: [
+		{
+			word: 'word',
+			grammarForm: 'orodnik',
+			expected: 'z word',
+			description: 'Grammar test for Slovenian, orodnik case'
+		},
+		{
+			word: 'word',
+			grammarForm: 'mestnik',
+			expected: 'o word',
+			description: 'Grammar test for Slovenian, mestnik case'
+		}
+	],
+
+	os: [
+		{
+			word: 'бæстæ',
+			grammarForm: 'genitive',
+			expected: 'бæсты',
+			description: 'Grammar test for Ossetian, genitive case'
+		},
+		{
+			word: 'бæстæ',
+			grammarForm: 'allative',
+			expected: 'бæстæм',
+			description: 'Grammar test for Ossetian, allative case'
+		},
+		{
+			word: 'Тигр',
+			grammarForm: 'dative',
+			expected: 'Тигрæн',
+			description: 'Grammar test for Ossetian, dative case'
+		},
+		{
+			word: 'цъити',
+			grammarForm: 'dative',
+			expected: 'цъитийæн',
+			description: 'Grammar test for Ossetian, dative case'
+		},
+		{
+			word: 'лæппу',
+			grammarForm: 'genitive',
+			expected: 'лæппуйы',
+			description: 'Grammar test for Ossetian, genitive case'
+		},
+		{
+			word: '2011',
+			grammarForm: 'equative',
+			expected: '2011-ау',
+			description: 'Grammar test for Ossetian, equative case'
+		}
+	],
+
+	la: [
+		{
+			word: 'Translatio',
+			grammarForm: 'genitive',
+			expected: 'Translationis',
+			description: 'Grammar test for Latin, genitive case'
+		},
+		{
+			word: 'Translatio',
+			grammarForm: 'accusative',
+			expected: 'Translationem',
+			description: 'Grammar test for Latin, accusative case'
+		},
+		{
+			word: 'Translatio',
+			grammarForm: 'ablative',
+			expected: 'Translatione',
+			description: 'Grammar test for Latin, ablative case'
+		}
+	]
+};
+
+$.each( grammarTests, function ( langCode, test ) {
+	if ( langCode === mw.config.get( 'wgContentLanguage' ) ) {
+		grammarTest( langCode, test );
+	}
+});
