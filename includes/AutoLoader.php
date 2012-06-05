4aa4b4d71794f9657b63b1a2d56fe4d9453db2db
jeroendedauw <jeroendedauw@gmail.com>
2012-06-06 00:11:00 +0200

    Adding a base query API module that allows very easily creating query modules for tables that have an associated ORMTable class
    Change-Id: I4a68e4b014ed093779312c2493b492687947c170

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -382,6 +382,7 @@ $wgAutoloadLocalClasses = array(
 	'ApiQueryLangLinks' => 'includes/api/ApiQueryLangLinks.php',
 	'ApiQueryLinks' => 'includes/api/ApiQueryLinks.php',
 	'ApiQueryLogEvents' => 'includes/api/ApiQueryLogEvents.php',
+	'ApiQueryORM' => 'includes/api/ApiQueryORM.php',
 	'ApiQueryPageProps' => 'includes/api/ApiQueryPageProps.php',
 	'ApiQueryProtectedTitles' => 'includes/api/ApiQueryProtectedTitles.php',
 	'ApiQueryQueryPage' => 'includes/api/ApiQueryQueryPage.php',
