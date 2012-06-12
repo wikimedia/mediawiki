f00d60000b97fcb93a865fb95ae212d7a75a817b
Reedy <reedy@wikimedia.org>
2012-06-12 17:43:59 +0100

    Bump $wgVersion and add release-notes for 1.18.4
    Change-Id: I587f70957e93d0947eb97d97cf1a25da40a6b8ef

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.18.3';
+$wgVersion = '1.18.4';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename         = 'MediaWiki';
