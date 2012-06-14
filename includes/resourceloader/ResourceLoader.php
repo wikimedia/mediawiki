wikimedia/mediawiki

commit 15e5193a19fb55339e60d192142dfd7c2e546596

Fix media type handling for CSS that's not loaded with only=styles
We were only doing server-side @media wrapping in only=styles mode but
not in regular mode. Meanwhile, the client-side loader had dropped all
support for media types under the assumption that this was all handled
server side, which caused media type handling to break for modules
loaded the normal way. We didn't notice this before because the only
core module that uses this is skins.vector, which is loaded through
only=styles.

Change-Id: I8228cc0317328440256b45b166eef1b57dbf4602

includes/resourceloader/ResourceLoader.php
@@ -752,8 +752,12 @@ class ResourceLoader {
 								}
 							}
 						}
-						$out .= self::makeLoaderImplementScript( $name, $scripts, $styles,
-							new XmlJsCode( $messagesBlob ) );
+						$out .= self::makeLoaderImplementScript(
+							$name,
+							$scripts,
+							self::makeCombinedStyles( $styles ),
+							new XmlJsCode( $messagesBlob )
+						);
 						break;
 				}
 			} catch ( Exception $e ) {

commit 156cabc42bced4dc2949045564c966a36548d7b6

Fix for 15e5193a19, broke in debug mode
We shouldn't be combining styles in debug mode anyways.

Change-Id: I8f0503e665df870e0940314e10185b94624869b9

includes/resourceloader/ResourceLoader.php
@@ -718,7 +718,7 @@ class ResourceLoader {
 					if ( $context->getDebug() && !$context->getOnly() && $module->supportsURLLoading() ) {
 						$styles = $module->getStyleURLsForDebug( $context );
 					} else {
-						$styles = $module->getStyles( $context );
+						$styles = self::makeCombinedStyles( $module->getStyles( $context ) );
 					}
 				}
 
@@ -737,7 +737,7 @@ class ResourceLoader {
 						}
 						break;
 					case 'styles':
-						$out .= self::makeCombinedStyles( $styles );
+						$out .= $styles;
 						break;
 					case 'messages':
 						$out .= self::makeMessageSetScript( new XmlJsCode( $messagesBlob ) );
@@ -755,7 +755,7 @@ class ResourceLoader {
 						$out .= self::makeLoaderImplementScript(
 							$name,
 							$scripts,
-							self::makeCombinedStyles( $styles ),
+							$styles,
 							new XmlJsCode( $messagesBlob )
 						);
 						break;