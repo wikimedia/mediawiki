6223d53262c7e496efe8560f10ea6a08fd6d775f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2012-06-16 15:42:17 +0300

    Adding JavaScript CLDR plural parser.
    The JavaScript code of the parser was written by Santhosh.
    The original project is hosted at GitHub:
    https://github.com/santhoshtr/CLDRPluralRuleParser
    
    Added partial qUnit tests for the parser.
    
    Fixed some bugs in the parser:
    * Fixed the 'within' rule.
    * Fixed the 'is not' rule.
    * Fixed the 'not in' rule.
    
    This commit adds it as a ResourceLoader module. Better ideas
    are welcome, especially about:
    * Module naming.
    * Dependencies.
    * Better code reuse: It is a good idea to keep the original code
      as a module that would be reusable by other programs and not to
      force it into ResourceLoader style.
    
    The actual rules will be taken straight from CLDR and they are not
    integrated in this commit yet.
    
    When all CLDR modules will be integrated, it will be possible to use
    the current automatic tests for PLURAL.
    
    Change-Id: I1aa3b081f4dad68515fd6cd46e4ab2dbdb3291eb

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.language.cldr.pluralparser.test.js
@@ -0,0 +1,243 @@
+module( 'mediawiki.cldr' );
+
+test( '-- Initial check', function() {
+	expect( 1 );
+	ok( mw.cldr, 'mw.cldr defined' );
+} );
+
+var cldrPluralTestcases = [
+	{
+		rule     : 'n is 0',
+		number   : 0,
+		expected : true
+	},
+	{
+		rule     : 'n is 0',
+		number   : 1,
+		expected : false
+	},
+	{
+		rule     : 'n is 0',
+		number   : 19,
+		expected : false
+	},
+	{
+		rule     : 'n is 1',
+		number   : 1,
+		expected : true
+	},
+	{
+		rule     : 'n is 1',
+		number   : 10,
+		expected : false
+	},
+	{
+		rule     : 'n is 2',
+		number   : 2,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 is 1',
+		number   : 1,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 is 1',
+		number   : 201,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 is 1',
+		number   : 11,
+		expected : false
+	},
+	{
+		rule     : 'n mod 100 is 2',
+		number   : 2,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 is 2',
+		number   : 202,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 is 2',
+		number   : 42,
+		expected : false
+	},
+	{
+		rule     : 'n mod 100 in 3..4',
+		number   : 2,
+		expected : false
+	},
+	{
+		rule     : 'n mod 100 in 3..4',
+		number   : 3,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 in 3..4',
+		number   : 4,
+		expected : true
+	},
+	{
+		rule     : 'n mod 100 in 3..4',
+		number   : 5,
+		expected : false
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 2,
+		expected : false
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 3,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 4,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 9,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 10,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 11,
+		expected : false
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 12,
+		expected : false
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 13,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 14,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 18,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 19,
+		expected : true
+	},
+	{
+		rule     : 'n in 3..10 or n in 13..19',
+		number   : 20,
+		expected : false
+	},
+	{
+		rule     : 'n within 0..1',
+		number   : 0,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..1',
+		number   : 0.5,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..1',
+		number   : 1,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..1',
+		number   : 2,
+		expected : false
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 0,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 0.5,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 1,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 1.5,
+		expected : true
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 2,
+		expected : false
+	},
+	{
+		rule     : 'n within 0..2 and n is not 2',
+		number   : 3,
+		expected : false
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 0,
+		expected : true
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 1,
+		expected : false
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 2,
+		expected : false
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 3,
+		expected : false
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 4,
+		expected : false
+	},
+	{
+		rule     : 'n not in 1..4',
+		number   : 5,
+		expected : true
+	}
+];
+
+test( '-- Plural parser tests', function() {
+	expect( cldrPluralTestcases.length );
+	for ( testcase = 0; testcase < cldrPluralTestcases.length; testcase++ ) {
+		result = mw.cldr.pluralRuleParser(
+			cldrPluralTestcases[testcase].rule,
+			cldrPluralTestcases[testcase].number
+		);
+		testName = 'n=' + cldrPluralTestcases[testcase].number + ' '
+			+ ( cldrPluralTestcases[testcase].expected ? "matches" : "doesn't match" )
+			+ " the rule '" + cldrPluralTestcases[testcase].rule + "'";
+		equal( result, cldrPluralTestcases[testcase].expected, testName );
+	}
+} );
