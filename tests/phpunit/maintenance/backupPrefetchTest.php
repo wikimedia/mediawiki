066c4095e78dfdd31d847e834edf1d7b307f2bca
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2012-06-18 15:11:04 +0200

    (bug 37458) permission errors running BaseDumpTest on Windows
    Change-Id: I58264c5f516f8dcb124d753b43a80b0875b0e974

--- tests/phpunit/maintenance/backupPrefetchTest.php
+++ tests/phpunit/maintenance/backupPrefetchTest.php
@@ -17,11 +17,13 @@ class BaseDumpTest extends MediaWikiTestCase {
 	private $dump = null;
 
 	protected function tearDown() {
-		parent::tearDown();
-
 		if ( $this->dump !== null ) {
 			$this->dump->close();
 		}
+
+		// Bug 37458, parent teardown need to be done after closing the
+		// dump or it might cause some permissions errors.
+		parent::tearDown();
 	}
 
 	/**
