35e2c120568b523b2dd990e2221f56fa4dbf38b6
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-06-18 13:58:44 +0530

    CLDR Plural rules based plural form calculation
    * Use the plurals.xml of CLDR for the plural rules of languages
    * Use plurals-mediawiki.xml to override or extend the rules inside MW
    * Remove the convertPlural method in each LanguageXX.php
    * Parse and load the xml files in LocalisationCache
    * Use the CLDRPluralRuleEvaluator.php for parsing the cldr plural rules
      (This is taken from Translate extension and might require a replacement
       parser without using eval)
    * Add getPluralRules() to make the CLDR plural rules available to JS.
    
    PS3: More method documentation, cleanup
    
    Change-Id: I58a9cdfe60c7b9027bf031c91370472054f04ae2

--- languages/classes/LanguageAr.php
+++ languages/classes/LanguageAr.php
@@ -30,31 +30,6 @@
 class LanguageAr extends Language {
 
 	/**
-	 * @param $count int
-	 * @param $forms array
-	 * @return string
-	 */
-	function convertPlural( $count, $forms ) {
-		if ( !count( $forms ) ) { return ''; }
-		$forms = $this->preConvertPlural( $forms, 6 );
-
-		if ( $count == 0 ) {
-			$index = 0;
-		} elseif ( $count == 1 ) {
-			$index = 1;
-		} elseif ( $count == 2 ) {
-			$index = 2;
-		} elseif ( $count % 100 >= 3 && $count % 100 <= 10 ) {
-			$index = 3;
-		} elseif ( $count % 100 >= 11 && $count % 100 <= 99 ) {
-			$index = 4;
-		} else {
-			$index = 5;
-		}
-		return $forms[$index];
-	}
-
-	/**
 	 * Temporary hack for bug 9413: replace Arabic presentation forms with their
 	 * standard equivalents.
 	 *
