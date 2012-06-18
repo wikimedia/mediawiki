a9ba50edd0f535ec7fe6966a7bc1c6a7d2860903
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-06-18 13:58:44 +0530

    CLDR Plural rules based plural form calculation
    * Use the plurals.xml of CLDR for the plural rules of languages
    * Use plurals-mediawiki.xml to override or extend the rules inside MW
    * Remove the convertPlural method in each LanguageXX.php
    * Parse and load the xml files in LocalisationCache
    * Use the CLDRPluralRuleEvaluator.php for parsing the cldr plural rules
      (This is taken from Translate extension and might require a replacement
       parser without using eval)
    * Add getPluralRules() to make the CLDR plural rules available to JS.
    
    PS3: More method documentation, cleanup
    
    Change-Id: I58a9cdfe60c7b9027bf031c91370472054f04ae2

---
+++ languages/utils/CLDRPluralRuleEvaluator.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Parse and evaluate a plural rule
+ *
+ * @author Niklas Laxstrom
+ *
+ * @copyright Copyright © 2010-2012, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @file
+ */
+
+class CLDRPluralRuleEvaluator {
+	/**
+	 * Evaluate a number against a set of plural rules. If a rule passes,
+	 * return the index of plural rule.
+	 *
+	 * @param int The number to be evaluated against the rules
+	 * @param array The associative array of plural rules in pluralform => rule format.
+	 * @return int The index of the plural form which passed the evaluation
+	 */
+	public static function evaluate( $number, $rules ) {
+		$formIndex = 0;
+		if ( !$rules ) {
+			return 0;
+		}
+		foreach ( $rules as $form => $rule ) {
+			$parsedRule = self::parseCLDRRule( $rule, $number );
+			// FIXME eval is bad.
+			if ( eval( "return $parsedRule;" ) ) {
+				return $formIndex;
+			}
+			$formIndex++;
+		}
+		return $formIndex;
+	}
+	private static function parseCLDRRule( $rule ) {
+		$rule = preg_replace( '/\bn\b/', '$number', $rule );
+		$rule = preg_replace( '/([^ ]+) mod (\d+)/', 'self::mod(\1,\2)', $rule );
+		$rule = preg_replace( '/([^ ]+) is not (\d+)/' , '\1!=\2', $rule );
+		$rule = preg_replace( '/([^ ]+) is (\d+)/', '\1==\2', $rule );
+		$rule = preg_replace( '/([^ ]+) not in (\d+)\.\.(\d+)/', '!self::in(\1,\2,\3)', $rule );
+		$rule = preg_replace( '/([^ ]+) not within (\d+)\.\.(\d+)/', '!self::within(\1,\2,\3)', $rule );
+		$rule = preg_replace( '/([^ ]+) in (\d+)\.\.(\d+)/', 'self::in(\1,\2,\3)', $rule );
+		$rule = preg_replace( '/([^ ]+) within (\d+)\.\.(\d+)/', 'self::within(\1,\2,\3)', $rule );
+		// AND takes precedence over OR
+		$andrule = '/([^ ]+) and ([^ ]+)/i';
+		while ( preg_match( $andrule, $rule ) ) {
+			$rule = preg_replace( $andrule, '(\1&&\2)', $rule );
+		}
+		$orrule = '/([^ ]+) or ([^ ]+)/i';
+		while ( preg_match( $orrule, $rule ) ) {
+			$rule = preg_replace( $orrule, '(\1||\2)', $rule );
+		}
+
+		return $rule;
+	}
+
+	private static function in( $num, $low, $high ) {
+		return is_int( $num ) && $num >= $low && $num <= $high;
+	}
+
+	private static function within( $num, $low, $high ) {
+		return $num >= $low && $num <= $high;
+	}
+
+	private static function mod( $num, $mod ) {
+		if ( is_int( $num ) ) {
+			return (int) fmod( $num, $mod );
+		}
+		return fmod( $num, $mod );
+	}
+}
