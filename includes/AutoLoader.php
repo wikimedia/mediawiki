732b2ada50ce5de8e7c4599530a6030b7777f010
jeroendedauw <jeroendedauw@gmail.com>
2012-06-27 14:26:05 +0200

    Add missing entry in autoloader for abstract test base class
    Change-Id: I44edbb02ae570f9c4b486c9f2c435bddb87e76d5

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -1043,6 +1043,9 @@ $wgAutoloadLocalClasses = array(
 	'TestFileIterator' => 'tests/testHelpers.inc',
 	'TestRecorder' => 'tests/testHelpers.inc',
 
+	# tests/phpunit/includes/db
+	'ORMRowTest' => 'tests/phpunit/includes/db/ORMRowTest.php',
+
 	# tests/parser
 	'ParserTest' => 'tests/parser/parserTest.inc',
 	'ParserTestParserHook' => 'tests/parser/parserTestsParserHook.php',
