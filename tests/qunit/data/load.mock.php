5583d10bdc29ee07aa5b5c62af2fb53b49846ae7
Timo Tijhof <ttijhof@wikimedia.org>
2012-06-29 10:34:12 +0200

    Tests: Fix race-condition broken mw.loader unit test
    * testloader.php was resolving dependencies when it shouldn't
      the server *never* does that. That's handled by the client, which
      knows the state of what is and isn't module.
    
      Due to a race condition between:
      - handlePending() call from loader.state() call for
        load.phpmodules?=testMissing
      - asynchronous callback from mw.loader.using as dependencies
        are being resolved.
    
      It didn't expose any actual problem, the unit test was simply
      assuming that they would arrive in a certain order, when that isn't
      the case by design.
    
      Random failures are seen here:
      - https://integration.mediawiki.org/testswarm/user/mediawiki/
      - http://integration.wmflabs.org/testswarm/job/4
    
      > Module "test.missing" must have state "missing"
      > - "missing"
      > + "loading"
      -> Because callback was triggered from mw.loader.state() already
         it didn't get to the implement() call yet.
    
      > Module "test.use_missing" must have state "error"
      > - "error"
      > + "loading"
      -> (same reason)
    
      This is now fixed.
    
    * Greatly simplified the mock load.php
    
    Change-Id: I000ee726a062f6c6d630ad6c07cfc0b48d145d35

---
+++ tests/qunit/data/load.mock.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Mock load.php with pre-defined test modules.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @package MediaWiki
+ * @author Lupo
+ * @since 1.20
+ */
+header( 'Content-Type: text/javascript; charset=utf-8' );
+
+require_once '../../../includes/Xml.php';
+
+$moduleImplementations = array(
+	'testUsesMissing' => "
+mw.loader.implement( 'testUsesMissing', function () {
+	QUnit.ok( false, 'Module test.usesMissing script should not run.');
+	QUnit.start();
+}, {}, {});
+",
+
+	'testUsesNestedMissing' => "
+mw.loader.implement( 'testUsesNestedMissing', function () {
+	QUnit.ok( false, 'Module testUsesNestedMissing script should not run.');
+}, {}, {});
+",
+);
+
+$response = '';
+
+// Only support for non-encoded module names, full module names expected
+if ( isset( $_GET['modules'] ) ) {
+	$modules = explode( ',', $_GET['modules'] );
+	foreach ( $modules as $module ) {
+		if ( isset( $moduleImplementations[$module] ) ) {
+			$response .= $moduleImplementations[$module];
+		} else {
+			$response .= Xml::encodeJsCall( 'mw.loader.state', array( $module, 'missing' ) );
+		}
+	}
+}
+
+echo $response;
