a9ec0bc1442d374ffe72ee2074dc1ce0297a522c
Aaron Schulz <aschulz@wikimedia.org>
2012-06-29 20:08:06 -0700

    [FileBackend] Factored out some code duplication into ProcessCacheLRU.
    Change-Id: Ib4475f21879ef6286ce2a28f248acd296fdbd45d

---
+++ includes/cache/ProcessCacheLRU.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Per process item memory cache.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Cache
+ */
+
+/**
+ * Handles per process caching of items
+ * @ingroup Cache
+ */
+class ProcessCacheLRU {
+	/** @var Array */
+	protected $cache = array(); // (key => prop => value)
+
+	protected $maxCacheSize; // integer; max entries
+
+	/**
+	 * @param $maxSize integer Maximum number of entries allowed
+	 */
+	public function __construct( $maxSize ) {
+		$this->maxCacheSize = $maxSize;
+	}
+
+	/**
+	 * Set a property field for a cache entry.
+	 * This will prune the cache if it gets too large.
+	 *
+	 * @param $key string
+	 * @param $prop string
+	 * @param $value mixed
+	 * @return void
+	 */
+	public function set( $key, $prop, $value ) {
+		if ( count( $this->cache ) >= $this->maxCacheSize ) { // LRU
+			reset( $this->cache );
+			unset( $this->cache[key( $this->cache )] );
+		}
+		$this->cache[$key][$prop] = $value;
+	}
+
+	/**
+	 * Check if a property field exists for a cache entry.
+	 *
+	 * @param $key string
+	 * @param $prop string
+	 * @return bool
+	 */
+	public function has( $key, $prop ) {
+		return isset( $this->cache[$key][$prop] );
+	}
+
+	/**
+	 * Get a property field for a cache entry.
+	 * This returns null if the property is not set.
+	 *
+	 * @param $key string
+	 * @param $prop string
+	 * @return mixed
+	 */
+	public function get( $key, $prop ) {
+		if ( isset( $this->cache[$key][$prop] ) ) {
+			$item = $this->cache[$key];
+			unset( $this->cache[$key] );
+			$this->cache[$key] = $item;
+			return $item[$prop];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Clear one or several cache entries, or all cache entries
+	 *
+	 * @param $keys string|Array
+	 * @return void
+	 */
+	public function clear( $keys = null ) {
+		if ( $keys === null ) {
+			$this->cache = array();
+		} else {
+			foreach ( (array)$keys as $key ) {
+				unset( $this->cache[$key] );
+			}
+		}
+	}
+}
