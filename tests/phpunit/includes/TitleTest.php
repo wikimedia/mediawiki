3319dc4d4b39f227dcb619ef0bc9e372ff9271da
nullspoon <nullspoon@iohq.net>
2012-07-10 11:10:38 -0600

    Added $wgWhitelistReadRegexp for regex whitelisting
    Allows for regular expression whitelists. This would be useful in
    a semi-public team wiki situation where the admin would want to
    hide an entire namespace from everyone except those in a
    particular group due to sensative team specific information.
    
    Also set up regular expression match to allow for regex modifiers
    specified in each index value in $wgWhitelistReadRegexp such as
    'i' for case insensitive matching.
    
    Added new unit test testUserCan in includes/TitleTest.php to
    test this new functionality.
    
    Updated RELEASE-NOTES-1.20 new features
    Updated CREDITS
    
    Change-Id: I83f6a614874c3d289ff4bd8d015f1d9c92e500b6

--- tests/phpunit/includes/TitleTest.php
+++ tests/phpunit/includes/TitleTest.php
@@ -1,6 +1,19 @@
 <?php
 
 class TitleTest extends MediaWikiTestCase {
+        var $db, $functionTest = false;
+
+        function setUp() {
+                $this->db = wfGetDB( DB_MASTER );
+        }
+
+        function tearDown() {
+                if ( $this->functionTest ) {
+                        $this->dropFunctions();
+                        $this->functionTest = false;
+                }
+        }
+
 
 	function testLegalChars() {
 		$titlechars = Title::legalChars();
@@ -61,6 +74,83 @@ class TitleTest extends MediaWikiTestCase {
 		}
 	}
 	
+	function dataTestIsValidMoveOperation() {
+		return array( 
+			array( 'Test', 'Test', 'selfmove' ),
+			array( 'File:Test.jpg', 'Page', 'imagenocrossnamespace' )
+		);
+	}
+
+        /**
+         * Auth-less test of Title::getUserPermissionsErrorsInternal
+         * 
+         * @param array $whitelistRegexp
+         * @param string $source
+         * @param string $action
+         * @param string $user
+         * @param string $doExpensiveQueries
+         * @param array|string|true $expected Required error
+         * @dataProvider dataUserCan
+         */
+        function testUserCan($whitelistRegexp, $source, $action, $user, $doExpensiveQueries, $expected) {
+                //$title = Title::newFromText( $source );
+                $title = Title::newFromDBkey( $source );
+
+                global $wgGroupPermissions;
+                $oldPermissions = $wgGroupPermissions;
+                // Disallow all so we can ensure our regex works
+                $wgGroupPermissions=array();
+                $wgGroupPermissions['*']['read'] = false;
+
+                global $wgWhitelistRead;
+                $oldWhitelist=$wgWhitelistRead;
+                // Undo any LocalSettings explicite whitelists so 
+                // they won't cause a failing test to succeed
+                $wgWhitelistRead=array();
+
+                global $wgWhitelistReadRegexp;
+                $oldWhitelistRegexp = $wgWhitelistReadRegexp;
+                $wgWhitelistReadRegexp=$whitelistRegexp ;
+
+                $errors = $title->userCan( $action, $user, $doExpensiveQueries);
+
+                $wgGroupPermissions = $oldPermissions;
+                $wgWhitelistRead = $oldWhitelist;
+                $wgWhitelistReadRegexp = $oldWhitelistRegexp;
+                if ( $expected === true ) {
+                        $this->assertTrue( $errors );
+                } else if ( $expected === false ) {
+                        $this->assertFalse ($errors );
+                } else {
+                        $errors = $this->flattenErrorsArray( $errors );
+                        foreach ( (array)$expected as $error ) {
+                                $this->assertContains( $error, $errors );
+                        }
+                }
+        }
+
+        function dataUserCan() {
+                return array(
+                        // Everything
+                        // If this doesn't work, we're really in trouble
+                        array( array('/.*/'), 'Main_Page', 'read', '127.0.0.1', 1, true ),
+                        // Main page
+                        array( array('/^Main/'), 'Main_Page', 'read', '127.0.0.1', 1, true ),
+                        array( array('/^Main.*/'), 'Main_Page', 'read', '127.0.0.1', 1, true ),
+                        // With spaces
+                        array( array('/Mic\sCheck/'), 'Mic Check', 'read', '127.0.0.1', 1, true ),
+                        // Unicode multibyte
+			// All unicode multibyte tests should succeed because the unicode switch is in the code
+                        // ...without unicode modifier
+                        array( array('/Unicode_Test_._Yes/'), 'Unicode_Test_Ñ_Yes', 'read', '127.0.0.1', 1, true ),
+                        // ...with unicode modifier
+                        array( array('/Unicode_Test_Ñ_Yes/u'), 'Unicode_Test_Ñ_Yes', 'read', '127.0.0.1', 1, true ),
+                        // Case insensitive
+                        array( array('/MiC ChEcK/'), 'mic check', 'read', '127.0.0.1', 1, false ),
+                        array( array('/MiC ChEcK/i'), 'mic check', 'read', '127.0.0.1', 1, true )
+                );
+        }
+	
 	function flattenErrorsArray( $errors ) {
 		$result = array();
 		foreach ( $errors as $error ) {
@@ -68,13 +158,4 @@ class TitleTest extends MediaWikiTestCase {
 		}
 		return $result;
 	}
-	
-	function dataTestIsValidMoveOperation() {
-		return array( 
-			array( 'Test', 'Test', 'selfmove' ),
-			array( 'File:Test.jpg', 'Page', 'imagenocrossnamespace' )
-		);
-	}
-	
-	
 }
