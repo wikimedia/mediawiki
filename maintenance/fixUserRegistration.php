02e15ebd0ba7690bd499ab0d29a4f0d631b8a055
Alexandre Emsenhuber <ialex.wiki@gmail.com>
2012-07-10 18:50:19 +0200

    Improve documentation of maintenance scripts.
    Change-Id: I0e2869970c46f4fd14b792f2978cc1ba9d8550be

--- maintenance/fixUserRegistration.php
+++ maintenance/fixUserRegistration.php
@@ -18,11 +18,17 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
  *
+ * @file
  * @ingroup Maintenance
  */
 
 require_once( dirname( __FILE__ ) . '/Maintenance.php' );
 
+/**
+ * Maintenance script that fixes the user_registration field.
+ *
+ * @ingroup Maintenance
+ */
 class FixUserRegistration extends Maintenance {
 	public function __construct() {
 		parent::__construct();
--- maintenance/formatInstallDoc.php
+++ maintenance/formatInstallDoc.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Format RELEASE-NOTE file to wiki text or HTML markup.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -21,6 +23,11 @@
 
 require_once( dirname( __FILE__ ) .'/Maintenance.php' );
 
+/**
+ * Maintenance script that formats RELEASE-NOTE file to wiki text or HTML markup.
+ *
+ * @ingroup Maintenance
+ */
 class MaintenanceFormatInstallDoc extends Maintenance {
 	function __construct() {
 		parent::__construct();
