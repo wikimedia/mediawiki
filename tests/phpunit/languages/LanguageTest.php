55e3e645d3d9675dd7ba40b618121fd0dbdf0d76
Reedy <reedy@wikimedia.org>
2012-07-17 23:56:51 +0100

    Add testRomanNumerals
    Up Language::romanNumeral() to work upto 10,000
    
    Does anyone know how do do letters with an underscore ontop of them? ;)
    
    Change-Id: Ib1b1415126af855ce5fb55f81b71534c26d84cc9

--- tests/phpunit/languages/LanguageTest.php
+++ tests/phpunit/languages/LanguageTest.php
@@ -855,4 +855,58 @@ class LanguageTest extends MediaWikiTestCase {
 			)
 		);
 	}
+
+	/**
+	 * @dataProvider provideRomanNumeralsData
+	 */
+	function testRomanNumerals( $num, $numerals ) {
+		$this->assertEquals(
+			$numerals,
+			Language::romanNumeral( $num ),
+			"romanNumeral('$num')"
+		);
+	}
+
+	function provideRomanNumeralsData() {
+		return array(
+			array( 1, 'I' ),
+			array( 2, 'II' ),
+			array( 3, 'III' ),
+			array( 4, 'IV' ),
+			array( 5, 'V' ),
+			array( 6, 'VI' ),
+			array( 7, 'VII' ),
+			array( 8, 'VIII' ),
+			array( 9, 'IX' ),
+			array( 10, 'X' ),
+			array( 20, 'XX' ),
+			array( 30, 'XXX' ),
+			array( 40, 'XL' ),
+			array( 50, 'L' ),
+			array( 60, 'LX' ),
+			array( 70, 'LXX' ),
+			array( 80, 'LXXX' ),
+			array( 90, 'XC' ),
+			array( 100, 'C' ),
+			array( 200, 'CC' ),
+			array( 300, 'CCC' ),
+			array( 400, 'CD' ),
+			array( 500, 'D' ),
+			array( 600, 'DC' ),
+			array( 700, 'DCC' ),
+			array( 800, 'DCCC' ),
+			array( 900, 'CM' ),
+			array( 1000, 'M' ),
+			array( 2000, 'MM' ),
+			array( 3000, 'MMM' ),
+			array( 4000, 'MMMM' ),
+			array( 5000, 'MMMMM' ),
+			array( 6000, 'MMMMMM' ),
+			array( 7000, 'MMMMMMM' ),
+			array( 8000, 'MMMMMMMM' ),
+			array( 9000, 'MMMMMMMMM' ),
+			array( 10000, 'MMMMMMMMMM' ),
+		);
+	}
 }
+
