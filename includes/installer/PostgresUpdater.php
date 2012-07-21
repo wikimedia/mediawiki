673f63b8cb37062e7c178401921779cce8b4bcf0
Aaron Pramana <aaron@sociotopia.com>
2012-07-21 09:57:44 -0700

    Watchlist grouping
    One changeset containing all the changes
    made for the watchlist grouping project.
    
    Release notes: http://mw-watchlist.tumblr.com/
    
    Change-Id: I702b9a8f3cda39a49c625654ca10fdd292e1e74c

--- includes/installer/PostgresUpdater.php
+++ includes/installer/PostgresUpdater.php
@@ -90,6 +90,7 @@ class PostgresUpdater extends DatabaseUpdater {
 			array( 'addTable', 'uploadstash',       'patch-uploadstash.sql' ),
 			array( 'addTable', 'user_former_groups','patch-user_former_groups.sql' ),
 			array( 'addTable', 'external_user',     'patch-external_user.sql' ),
+			array( 'addTable', 'watchlist_groups',  'patch-watchlist_groups.sql' ),
 
 			# Needed before new field
 			array( 'convertArchive2' ),
@@ -158,6 +159,7 @@ class PostgresUpdater extends DatabaseUpdater {
 			array( 'addPgField', 'job',           'job_token',            "TEXT NOT NULL DEFAULT ''" ),
 			array( 'addPgField', 'job',           'job_token_timestamp',  "TIMESTAMPTZ" ),
 			array( 'addPgField', 'job',           'job_sha1',             "TEXT NOT NULL DEFAULT ''" ),
+			array( 'addPgField', 'watchlist',     'wl_group',             "INTEGER NOT NULL DEFAULT 0" ),
 
 			# type changes
 			array( 'changeField', 'archive',       'ar_deleted',      'smallint', '' ),
