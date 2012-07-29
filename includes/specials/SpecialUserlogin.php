0d70409b5e0e84e237f881dbc88f0ad207664b27
Derk-Jan Hartman <hartman@videolan.org>
2012-07-29 16:06:37 +0200

    Add lang and hreflang attributes to the language links of the login page.
    - Adding these attribues should make them easier to interpret for VoiceOver and other assitive technologies.
    - This assumes that people will enter the name of the language in the target language to MediaWiki:loginlanguagelinks, which is a convention but not a requirement of course. However I think making the bet that people actually do that is safe enough.
    - Switch the name of the old attr variable to query, which better reflects what it is supposed to be used for.
    
    Change-Id: I0cdae9f1e539b9b9c2b99ea1db1ba62bbbb0bdc5

--- includes/specials/SpecialUserlogin.php
+++ includes/specials/SpecialUserlogin.php
@@ -1269,19 +1269,24 @@ class LoginForm extends SpecialPage {
 			// no link for currently used language
 			return htmlspecialchars( $text );
 		}
-		$attr = array( 'uselang' => $lang );
+		$query = array( 'uselang' => $lang );
 		if( $this->mType == 'signup' ) {
-			$attr['type'] = 'signup';
+			$query['type'] = 'signup';
 		}
 		if( $this->mReturnTo !== '' ) {
-			$attr['returnto'] = $this->mReturnTo;
-			$attr['returntoquery'] = $this->mReturnToQuery;
+			$query['returnto'] = $this->mReturnTo;
+			$query['returntoquery'] = $this->mReturnToQuery;
 		}
+
+		$attr = array();
+		$targetLanguage = Language::factory( $lang );
+		$attr['lang'] = $attr['hreflang'] = $targetLanguage->getHtmlCode();
+
 		return Linker::linkKnown(
 			$this->getTitle(),
 			htmlspecialchars( $text ),
-			array(),
-			$attr
+			$attr,
+			$query
 		);
 	}
 }
