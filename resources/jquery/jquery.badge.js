00da38f6b4842a0c84bf183f3e6b83ba731a7e02
Andrew Garrett <agarrett@wikimedia.org>
2012-07-30 14:16:56 -0700

    Clean up badger.{css,js} from PageTriage, generalise it and move it into core.
    Change-Id: I0f5e754146359448eb125456e240cf5768d4f541

---
+++ resources/jquery/jquery.badge.js
@@ -0,0 +1,78 @@
+// Badger v1.0 by Daniel Raftery
+// http://thrivingkings.com/badger
+// http://twitter.com/ThrivingKings
+// Modified by Ryan Kaldari <rkaldari@wikimedia.org>
+
+/**
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY.
+ */
+
+(function( $ ) {
+	$.fn.badge = function( badge, options ) {
+		var existingBadge = this.find( '.mw-badge' );
+		options = $.extend( {}, options );
+
+		badge = String(badge);
+		if ( badge.charAt(0) === '+' ) {
+			if ( existingBadge.length > 0 ) {
+				oldBadge = existingBadge.text();
+				badge = Math.round( Number( oldBadge ) + Number( badge.substr(1) ) );
+			} else {
+				badge = badge.substr(1);
+			}
+		} else if ( badge.charAt(0) === '-' ) {
+			if ( existingBadge.length > 0 ) {
+				oldBadge = existingBadge.text();
+				badge = Math.round( Number( oldBadge ) - Number( badge.substr(1) ) );
+			} else {
+				badge = 0;
+			}
+		}
+
+		if ( Number(badge) <= 0 ) {
+			// Clear any existing badge
+			existingBadge.remove();
+		} else {
+			// Don't add duplicates
+			var $badge = existingBadge;
+			if ( existingBadge.length > 0 ) {
+				this.find( '.mw-badge-content' ).text( badge );
+			} else {
+				$badge = $('<div/>')
+					.addClass('mw-badge')
+					.addClass('mw-badge-overlay')
+					.append(
+						$('<span/>')
+							.addClass('mw-badge-content')
+							.text(badge)
+					);
+				this.append($badge);
+			}
+
+			if ( options.type ) {
+				if ( options.type == 'inline' ) {
+					$badge.removeClass('mw-badge-overlay')
+						.addClass('mw-badge-inline');
+				} else if ( options.type == 'overlay' ) {
+					$badge.removeClass('mw-badge-inline')
+						.addClass('mw-badge-overlay');
+				}
+			}
+
+			// If a callback was specified, call it with the badge number
+			if ( options.callback ) {
+				options.callback( badge );
+			}
+		}
+	};
+} ) ( jQuery );
