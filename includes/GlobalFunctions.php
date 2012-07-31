83a8f9d03a298d2d51ba2534e280c7d7a03e6cc5
Tyler Anthony Romeo <tylerromeo@gmail.com>
2012-07-31 15:37:01 -0400

    Added PHP complement for mw.Time.
    Figured since a JS Timestamp class was being implemented
    it might be appropriate to create a complementary PHP
    Timestamp class. Much of code is taken from wfTimestamp().
    
    Note: Another change (I53dcf547) is attempting a related
    issue. Not sure how much of scope overlap there is.
    
    Change-Id: I68eb9f27eebe80df757187f634392e1bcba5551f
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -2386,6 +2386,7 @@ define( 'TS_ISO_8601_BASIC', 9 );
 /**
  * Get a timestamp string in one of various formats
  *
+ * @deprecated
  * @param $outputtype Mixed: A timestamp in one of the supported formats, the
  *                    function will autodetect which format is supplied and act
  *                    accordingly.
@@ -2393,118 +2394,8 @@ define( 'TS_ISO_8601_BASIC', 9 );
  * @return Mixed: String / false The same date in the format specified in $outputtype or false
  */
 function wfTimestamp( $outputtype = TS_UNIX, $ts = 0 ) {
-	$uts = 0;
-	$da = array();
-	$strtime = '';
-
-	if ( !$ts ) { // We want to catch 0, '', null... but not date strings starting with a letter.
-		$uts = time();
-		$strtime = "@$uts";
-	} elseif ( preg_match( '/^(\d{4})\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)$/D', $ts, $da ) ) {
-		# TS_DB
-	} elseif ( preg_match( '/^(\d{4}):(\d\d):(\d\d) (\d\d):(\d\d):(\d\d)$/D', $ts, $da ) ) {
-		# TS_EXIF
-	} elseif ( preg_match( '/^(\d{4})(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)$/D', $ts, $da ) ) {
-		# TS_MW
-	} elseif ( preg_match( '/^-?\d{1,13}$/D', $ts ) ) {
-		# TS_UNIX
-		$uts = $ts;
-		$strtime = "@$ts"; // http://php.net/manual/en/datetime.formats.compound.php
-	} elseif ( preg_match( '/^\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}.\d{6}$/', $ts ) ) {
-		# TS_ORACLE // session altered to DD-MM-YYYY HH24:MI:SS.FF6
-		$strtime = preg_replace( '/(\d\d)\.(\d\d)\.(\d\d)(\.(\d+))?/', "$1:$2:$3",
-				str_replace( '+00:00', 'UTC', $ts ) );
-	} elseif ( preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.*\d*)?Z$/', $ts, $da ) ) {
-		# TS_ISO_8601
-	} elseif ( preg_match( '/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})(?:\.*\d*)?Z$/', $ts, $da ) ) {
-		#TS_ISO_8601_BASIC
-	} elseif ( preg_match( '/^(\d{4})\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)\.*\d*[\+\- ](\d\d)$/', $ts, $da ) ) {
-		# TS_POSTGRES
-	} elseif ( preg_match( '/^(\d{4})\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)\.*\d* GMT$/', $ts, $da ) ) {
-		# TS_POSTGRES
-	} elseif (preg_match( '/^(\d{4})\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)\.\d\d\d$/', $ts, $da ) ) {
-		# TS_DB2
-	} elseif ( preg_match( '/^[ \t\r\n]*([A-Z][a-z]{2},[ \t\r\n]*)?' . # Day of week
-							'\d\d?[ \t\r\n]*[A-Z][a-z]{2}[ \t\r\n]*\d{2}(?:\d{2})?' .  # dd Mon yyyy
-							'[ \t\r\n]*\d\d[ \t\r\n]*:[ \t\r\n]*\d\d[ \t\r\n]*:[ \t\r\n]*\d\d/S', $ts ) ) { # hh:mm:ss
-		# TS_RFC2822, accepting a trailing comment. See http://www.squid-cache.org/mail-archive/squid-users/200307/0122.html / r77171
-		# The regex is a superset of rfc2822 for readability
-		$strtime = strtok( $ts, ';' );
-	} elseif ( preg_match( '/^[A-Z][a-z]{5,8}, \d\d-[A-Z][a-z]{2}-\d{2} \d\d:\d\d:\d\d/', $ts ) ) {
-		# TS_RFC850
-		$strtime = $ts;
-	} elseif ( preg_match( '/^[A-Z][a-z]{2} [A-Z][a-z]{2} +\d{1,2} \d\d:\d\d:\d\d \d{4}/', $ts ) ) {
-		# asctime
-		$strtime = $ts;
-	} else {
-		# Bogus value...
-		wfDebug("wfTimestamp() fed bogus time value: TYPE=$outputtype; VALUE=$ts\n");
-
-		return false;
-	}
-
-	static $formats = array(
-		TS_UNIX => 'U',
-		TS_MW => 'YmdHis',
-		TS_DB => 'Y-m-d H:i:s',
-		TS_ISO_8601 => 'Y-m-d\TH:i:s\Z',
-		TS_ISO_8601_BASIC => 'Ymd\THis\Z',
-		TS_EXIF => 'Y:m:d H:i:s', // This shouldn't ever be used, but is included for completeness
-		TS_RFC2822 => 'D, d M Y H:i:s',
-		TS_ORACLE => 'd-m-Y H:i:s.000000', // Was 'd-M-y h.i.s A' . ' +00:00' before r51500
-		TS_POSTGRES => 'Y-m-d H:i:s',
-		TS_DB2 => 'Y-m-d H:i:s',
-	);
-
-	if ( !isset( $formats[$outputtype] ) ) {
-		throw new MWException( 'wfTimestamp() called with illegal output type.' );
-	}
-
-	if ( function_exists( "date_create" ) ) {
-		if ( count( $da ) ) {
-			$ds = sprintf("%04d-%02d-%02dT%02d:%02d:%02d.00+00:00",
-				(int)$da[1], (int)$da[2], (int)$da[3],
-				(int)$da[4], (int)$da[5], (int)$da[6]);
-
-			$d = date_create( $ds, new DateTimeZone( 'GMT' ) );
-		} elseif ( $strtime ) {
-			$d = date_create( $strtime, new DateTimeZone( 'GMT' ) );
-		} else {
-			return false;
-		}
-
-		if ( !$d ) {
-			wfDebug("wfTimestamp() fed bogus time value: $outputtype; $ts\n");
-			return false;
-		}
-
-		$output = $d->format( $formats[$outputtype] );
-	} else {
-		if ( count( $da ) ) {
-			// Warning! gmmktime() acts oddly if the month or day is set to 0
-			// We may want to handle that explicitly at some point
-			$uts = gmmktime( (int)$da[4], (int)$da[5], (int)$da[6],
-				(int)$da[2], (int)$da[3], (int)$da[1] );
-		} elseif ( $strtime ) {
-			$uts = strtotime( $strtime );
-		}
-
-		if ( $uts === false ) {
-			wfDebug("wfTimestamp() can't parse the timestamp (non 32-bit time? Update php): $outputtype; $ts\n");
-			return false;
-		}
-
-		if ( TS_UNIX == $outputtype ) {
-			return $uts;
-		}
-		$output = gmdate( $formats[$outputtype], $uts );
-	}
-
-	if ( ( $outputtype == TS_RFC2822 ) || ( $outputtype == TS_POSTGRES ) ) {
-		$output .= ' GMT';
-	}
-
-	return $output;
+	$timestamp = new MWTimestamp( $ts );
+	return $timestamp->getTimestamp( $outputtype );
 }
 
 /**
