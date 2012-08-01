a41aecb347b6f4ee40ece91e810da40ec879cdea
Eranroz <eranroz89@gmail.com>
2012-08-01 21:41:51 +0300

    AJAXify watchlist editor
    This patch makes the watchlist editor to use pagination. That would
    avoid old browsers crashing (bug 20483).
    Patch also add some AJAX operations to the editor, for example to remove
    items from watchlist (bug 32151).
    
    The AJAX support is an ALTERNATIVE to the form based method, to keep
    support for non javascript users.
    
    This change contains a required change in the API for watch operation,
    to allow batch operation: the module now support pageset opetions (e.g. titles, pageids).
    The old title (single page) parameter is still used to keep backward compatibility.
    
    Change-Id: If993f6e8efaf69253fceba9127a984223adfd20e

--- includes/api/ApiWatch.php
+++ includes/api/ApiWatch.php
@@ -40,14 +40,29 @@ class ApiWatch extends ApiBase {
 		if ( !$user->isLoggedIn() ) {
 			$this->dieUsage( 'You must be logged-in to have a watchlist', 'notloggedin' );
 		}
-
 		$params = $this->extractRequestParams();
-		$title = Title::newFromText( $params['title'] );
-
-		if ( !$title || $title->getNamespace() < 0 ) {
-			$this->dieUsageMsg( array( 'invalidtitle', $params['title'] ) );
+		// by default we use pageset to extract the page to work on.
+		// title is still supported for backward compatability
+		if ( !isset( $params['title'] ) ) {
+			$pageSet = new ApiPageSet( $this );
+			$pageSet->execute();
+			$res = array();
+			foreach ( $pageSet->getTitles() as $title ) {
+				$r = $this->watchTitle( $title, $user, $params);
+				$res[] = $r;
+			}
+			$this->getResult()->setIndexedTagName( $res, 'w' );
+		} else {
+			$title = Title::newFromText( $params['title'] );
+			if ( !$title || $title->getNamespace() < 0 ) {
+				$this->dieUsageMsg( array( 'invalidtitle', $params['title'] ) );
+			}
+			$res = $this->watchTitle( $title, $user, $params );
 		}
+		$this->getResult()->addValue( null, $this->getModuleName(), $res );
+	}
 
+	private function watchTitle( $title, $user, $params ) {
 		$res = array( 'title' => $title->getPrefixedText() );
 
 		// Currently unnecessary, code to act as a safeguard against any change in current behaviour of uselang
@@ -75,7 +90,7 @@ class ApiWatch extends ApiBase {
 		if ( !$success ) {
 			$this->dieUsageMsg( 'hookaborted' );
 		}
-		$this->getResult()->addValue( null, $this->getModuleName(), $res );
+		return $res;
 	}
 
 	public function mustBePosted() {
@@ -95,10 +110,11 @@ class ApiWatch extends ApiBase {
 	}
 
 	public function getAllowedParams() {
-		return array(
+		$psModule = new ApiPageSet( $this );
+		return $psModule->getAllowedParams() + array(
 			'title' => array(
 				ApiBase::PARAM_TYPE => 'string',
-				ApiBase::PARAM_REQUIRED => true
+				ApiBase::PARAM_DEPRECATED => true
 			),
 			'unwatch' => false,
 			'uselang' => null,
@@ -110,8 +126,9 @@ class ApiWatch extends ApiBase {
 	}
 
 	public function getParamDescription() {
-		return array(
-			'title' => 'The page to (un)watch',
+		$psModule = new ApiPageSet( $this );
+		return $psModule->getParamDescription() + array(
+			'title' => 'The page to (un)watch. use titles instead',
 			'unwatch' => 'If set the page will be unwatched rather than watched',
 			'uselang' => 'Language to show the message in',
 			'token' => 'A token previously acquired via prop=info',
@@ -143,8 +160,8 @@ class ApiWatch extends ApiBase {
 
 	public function getExamples() {
 		return array(
-			'api.php?action=watch&title=Main_Page' => 'Watch the page "Main Page"',
-			'api.php?action=watch&title=Main_Page&unwatch=' => 'Unwatch the page "Main Page"',
+			'api.php?action=watch&titles=Main_Page' => 'Watch the page "Main Page"',
+			'api.php?action=watch&titles=Main_Page&unwatch=' => 'Unwatch the page "Main Page"',
 		);
 	}
 
