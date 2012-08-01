e9e24cbfe7849c7e2387a0953cce3e059b452a1a
DaSch <dasch@daschmedia.de>
2012-08-01 22:49:17 +0200

    Add git HEAD date to Special:Version for core and extensions
    The patch adds the localised commit date of i) core and ii) extensions
    in the Special:Version page tables. It requires the Git version control
    system being installed, which is checked during the installation.
    
    Introduces a new parameter for the git binary in DefaultSettings.php:
    $wgGitBin = '/usr/bin/git';
    
    Patch authored by DaSch <dasch@daschmedia.de> and updated and fixed
    by Wikinaut<mail@tgries.de>.
    
    Bug: 38783
    Change-Id: I0931400ecacf91ed2ab4fc7aa46dceac17661768

--- includes/specials/SpecialVersion.php
+++ includes/specials/SpecialVersion.php
@@ -274,10 +274,11 @@ class SpecialVersion extends SpecialPage {
 	}
 
 	/**
-	 * @return bool|string wgVersion + HEAD sha1 stripped to the first 7 chars. False on failure
+	 * @since 1.22 Returns the HEAD date in addition to the sha1 and link
+	 * @return bool|string wgVersion + HEAD sha1 stripped to the first 7 chars with link and date, or false on failure
 	 */
 	private static function getVersionLinkedGit() {
-		global $IP;
+		global $IP, $wgLang;
 
 		$gitInfo = new GitInfo( $IP );
 		$headSHA1 = $gitInfo->getHeadSHA1();
@@ -286,10 +287,17 @@ class SpecialVersion extends SpecialPage {
 		}
 
 		$shortSHA1 = '(' . substr( $headSHA1, 0, 7 ) . ')';
-		$viewerUrl = $gitInfo->getHeadViewUrl();
-		if ( $viewerUrl !== false ) {
-			$shortSHA1 = "[$viewerUrl $shortSHA1]";
+
+		$gitHeadUrl = $gitInfo->getHeadViewUrl();
+		if ( $gitHeadUrl !== false ) {
+			$shortSHA1 = "[$gitHeadUrl $shortSHA1]";
 		}
+
+		$gitHeadCommitDate = $gitInfo->getHeadCommitDate();
+		if ( $gitHeadCommitDate ) {
+			$shortSHA1 .= "<br/>" . $wgLang->timeanddate( $gitHeadCommitDate, true );
+		}
+
 		return self::getwgVersionLinked() . " $shortSHA1";
 	}
 
@@ -461,6 +469,8 @@ class SpecialVersion extends SpecialPage {
 	 * @return string
 	 */
 	function getCreditsForExtension( array $extension ) {
+		global $wgLang;
+
 		$name = isset( $extension['name'] ) ? $extension['name'] : '[no name]';
 
 		$vcsText = false;
@@ -473,6 +483,10 @@ class SpecialVersion extends SpecialPage {
 				$gitViewerUrl = $gitInfo->getHeadViewUrl();
 				if ( $gitViewerUrl !== false ) {
 					$vcsText = "[$gitViewerUrl $vcsText]";
+				}
+				$gitHeadCommitDate = $gitInfo->getHeadCommitDate();
+				if ( $gitHeadCommitDate ) {
+					$vcsText .= "<br/>" .  $wgLang->timeanddate( $gitHeadCommitDate, true );
 				}
 			} else {
 				$svnInfo = self::getSvnInfo( dirname( $extension['path'] ) );
