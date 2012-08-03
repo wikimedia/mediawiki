c437b3ae009b58dde03b8a6a1f2c1b11eb0c51f7
Trevor Parscal <tparscal@wikimedia.org>
2012-08-03 11:50:36 -0700

    Made jsMessage look like a "growl" notification in vector and monobook
    The message will also auto-hide in all skins, which is especially useful with VisualEditor because you don't re-load the page as often. Auto-hiding is disabled while the mouse if hovering the message. By clicking on the notification, it will also manually hide it. This doesn't affect the links inside the notification however.
    
    Also cleaned up some whitespace issues in vector.js
    
    Change-Id: I41c70d78c8ed8aeb91a598dc4a7b26dfad8d8f6c

--- skins/vector/vector.js
+++ skins/vector/vector.js
@@ -1,21 +1,21 @@
 /*
  * Vector-specific scripts
  */
-jQuery( function( $ ) {
-	$( 'div.vectorMenu' ).each( function() {
+jQuery( function ( $ ) {
+	$( 'div.vectorMenu' ).each( function () {
 		var self = this;
 		$( 'h5:first a:first', this )
 			// For accessibility, show the menu when the hidden link in the menu is clicked (bug 24298)
-			.click( function( e ) {
+			.click( function ( e ) {
 				$( '.menu:first', self ).toggleClass( 'menuForceShow' );
 				e.preventDefault();
-			})
+			} )
 			// When the hidden link has focus, also set a class that will change the arrow icon
-			.focus( function() {
+			.focus( function () {
 				$( self ).addClass( 'vectorMenuFocus' );
-			})
-			.blur( function() {
+			} )
+			.blur( function () {
 				$( self ).removeClass( 'vectorMenuFocus' );
-			});
-	});
-});
+			} );
+	} );
+} );
