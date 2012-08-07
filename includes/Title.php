3b0d00d19a958e7745a4ae0de9b77a431e95ad39
nullspoon <nullspoon@iohq.net>
2012-08-07 16:06:12 -0600

    Added $wgWhitelistReadRegexp for regex whitelisting
    Allows for regular expression whitelists. This would be useful in
    a semi-public team wiki situation where the admin would want to
    hide an entire namespace from everyone except those in a
    particular group due to sensitive team specific information.
    
    Added new unit test testUserCan in includes/TitleTest.php to
    test this new functionality.
    
    Added $wgWhitelistReadRegexp to DefaultSettings.php
    Updated RELEASE-NOTES-1.20 new features
    Updated CREDITS
    
    Change-Id: I83f6a614874c3d289ff4bd8d015f1d9c92e500b6

--- includes/Title.php
+++ includes/Title.php
@@ -1990,7 +1990,7 @@ class Title {
 	 * @return Array list of errors
 	 */
 	private function checkReadPermissions( $action, $user, $errors, $doExpensiveQueries, $short ) {
-		global $wgWhitelistRead, $wgGroupPermissions, $wgRevokePermissions;
+		global $wgWhitelistRead, $wgWhitelistReadRegexp, $wgGroupPermissions, $wgRevokePermissions;
 		static $useShortcut = null;
 
 		# Initialize the $useShortcut boolean, to determine if we can skip quite a bit of code below
@@ -2035,7 +2035,6 @@ class Title {
 			# Only do these checks is there's something to check against
 			$name = $this->getPrefixedText();
 			$dbName = $this->getPrefixedDBKey();
-
 			// Check for explicit whitelisting with and without underscores
 			if ( in_array( $name, $wgWhitelistRead, true ) || in_array( $dbName, $wgWhitelistRead, true ) ) {
 				$whitelisted = true;
@@ -2054,6 +2053,17 @@ class Title {
 					if ( in_array( $pure, $wgWhitelistRead, true ) ) {
 						$whitelisted = true;
 					}
+				}
+			}
+		}
+
+		if ( is_array( $wgWhitelistReadRegexp ) && !empty( $wgWhitelistReadRegexp ) ) {
+			$name = $this->getPrefixedText();
+			// Check for regex whitelisting
+			foreach ( $wgWhitelistReadRegexp as $listItem ) {
+				if ( preg_match( $listItem, $name ) ) {
+					$whitelisted = true;
+					break;
 				}
 			}
 		}
