a3cd79827b2a27795e4405b42ee4e09ecc2299bb
Aaron Schulz <aschulz@wikimedia.org>
2012-08-07 23:22:49 -0700

    Revision and WikiPage cleanup with IDBAccessObject interface.
    * Replaced WikiPage::DATA_FROM_* constants with IDBAccessObject ones.
    * Renamed IDBAccessObject constants a bit for visual consistency.
    * Removed AVOID_MASTER parameter and replaced calling instances with READ_NORMAL.
      Instead of getting page_latest from the master and the revision from a
      slave, just get it all from the master in one RTT. Most callers used
      AVOID_MASTER (and now READ_NORMAL), so this case is barely hit anymore.
    
    Change-Id: Ifbefdcd4490094b38e49bbb46c95fdb71b5c9e1a

--- includes/dao/IDBAccessObject.php
+++ includes/dao/IDBAccessObject.php
@@ -22,11 +22,34 @@
  */
 
 /**
- * Interface for database access objects
+ * Interface for database access objects.
+ *
+ * Classes using this support a set of constants in a bitfield argument to their data loading
+ * functions. In general, objects should assume READ_NORMAL if no flags are explicitly given,
+ * though certain objects may assume READ_LATEST for common use case or legacy reasons.
+ *
+ * There are three types of reads:
+ *   - READ_NORMAL  : Potentially cached read of data (e.g. from a slave or stale replica)
+ *   - READ_LATEST  : Up-to-date read as of transaction start (e.g. from master or a quorum read)
+ *   - READ_LOCKING : Up-to-date read as of now, that locks the records for the transaction
+ *
+ * Callers should use READ_NORMAL (or pass in no flags) unless the read determines a write.
+ * In theory, such cases may require READ_LOCKING, though to avoid contention, READ_LATEST is
+ * often good enough. If UPDATE race condition checks are required on a row and expensive code
+ * must run after the row is fetched to determine the UPDATE, it may help to do something like:
+ *   - a) Read the current row
+ *   - b) Determine the new row (expensive, so we don't want to hold locks now)
+ *   - c) Re-read the current row with READ_LOCKING; if it changed then bail out
+ *   - d) otherwise, do the updates
  */
 interface IDBAccessObject {
-	const LATEST_READ  = 1; // read from the master
-	const FOR_UPDATE   = 2; // lock the rows read
-	const LOCKING_READ = 3; // LATEST_READ | FOR_UPDATE
-	const AVOID_MASTER = 4; // avoiding checking the master
+	// Constants for object loading bitfield flags (higher => higher QoS)
+	const READ_LATEST  = 1; // read from the master
+	const READ_LOCKING = 3; // READ_LATEST and "FOR UPDATE"
+
+	// Convenience constant for callers to explicitly request slave data
+	const READ_NORMAL = 0; // read from the slave
+
+	// Convenience constant for tracking how data was loaded (higher => higher QoS)
+	const READ_NONE = -1; // not loaded yet (or the object was cleared)
 }
