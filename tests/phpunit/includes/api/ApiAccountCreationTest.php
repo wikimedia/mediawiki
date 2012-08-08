e158b954bce1a3b3450770376c3c6a0992567af4
Tyler Romeo <tylerromeo@gmail.com>
2012-08-08 09:58:47 -0400

    Added account creation API.
    Created new API action "createaccount" that
    allows access to account creation. Takes
    username, password, email, realname, token,
    and optionally mailpassword and reason.
    
    Errors are given in an errors array. Note
    there is no way to natively handle CAPTCHAs
    as there is no uniform implementation of
    presenting CAPTCHA links to user. Right now
    the best an extension can do is return an
    error in AbortNewAccount, which will then
    be displayed as an error in the Api result.
    
    Change-Id: Ibdb1e50d434fb857683e1e4ff5a4a5a91c6b7c3a
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>

---
+++ tests/phpunit/includes/api/ApiAccountCreationTest.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * @group Database
+ * @group API
+ */
+class ApiCreateAccounTest extends ApiTestCase {
+	function setUp() {
+		parent::setUp();
+		LoginForm::setCreateaccountToken();
+	}
+
+	/**
+	 * Test the account creation API with a valid request. Also
+	 * make sure the new account can log in and is valid.
+	 */
+	function testValid() {
+		global $wgServer;
+
+		if ( !isset( $wgServer ) ) {
+			$this->markTestIncomplete( 'This test needs $wgServer to be set in LocalSettings.php' );
+		}
+
+		$password = User::randomPassword();
+
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'name' => 'Apitestnew',
+			'password' => $password,
+			'email' => 'test@example.com',
+			'realname' => 'Test Name'
+		) );
+
+		$result = $ret[0];
+		$this->assertNotInternalType( 'bool', $result );
+		$this->assertNotInternalType( 'null', $result['createaccount'] );
+
+		// Should first ask for token.
+		$a = $result['createaccount'];
+		$this->assertEquals( 'needtoken', $a['result'] );
+		$token = $a['token'];
+
+		// Finally create the account
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'name' => 'Apitestnew',
+			'password' => $password,
+			'token' => $token,
+			'email' => 'test@invalid.domain',
+			'realname' => 'Test Name' ), $ret[2]
+		);
+
+		$result = $ret[0];
+		$this->assertNotInternalType( 'bool', $result );
+		$this->assertEquals( 'success', $result['createaccount']['result'] );
+
+		// Try logging in with the new user.
+		$ret = $this->doApiRequest( array(
+			'action' => 'login',
+			'lgname' => 'Apitestnew',
+			'lgpassword' => $password,
+			)
+		);
+
+		$result = $ret[0];
+		$this->assertNotInternalType( 'bool', $result );
+		$this->assertNotInternalType( 'null', $result['login'] );
+
+		$a = $result['login']['result'];
+		$this->assertEquals( 'NeedToken', $a );
+		$token = $result['login']['token'];
+
+		$ret = $this->doApiRequest( array(
+			'action' => 'login',
+			'lgtoken' => $token,
+			'lgname' => 'Apitestnew',
+			'lgpassword' => $password,
+			), $ret[2]
+		);
+
+		$result = $ret[0];
+
+		$this->assertNotInternalType( 'bool', $result );
+		$a = $result['login']['result'];
+
+		$this->assertEquals( 'Success', $a );
+	}
+
+	/**
+	 * Make sure requests with no names are invalid.
+	 * @expectedException UsageException
+	 */
+	function testNoName() {
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'token' => LoginForm::getCreateaccountToken(),
+			'password' => 'password',
+		) );
+	}
+
+	/**
+	 * Make sure requests with no password are invalid.
+	 * @expectedException UsageException
+	 */
+	function testNoPassword() {
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'name' => 'testName',
+			'token' => LoginForm::getCreateaccountToken(),
+		) );
+	}
+
+	/**
+	 * Make sure requests with existing users are invalid.
+	 * @expectedException UsageException
+	 */
+	function testExistingUser() {
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'name' => 'Apitestsysop',
+			'token' => LoginForm::getCreateaccountToken(),
+			'password' => 'password',
+			'email' => 'test@invalid.domain',
+		) );
+	}
+
+	/**
+	 * Make sure requests with invalid emails are invalid.
+	 * @expectedException UsageException
+	 */
+	function testInvalidEmail() {
+		$ret = $this->doApiRequest( array(
+			'action' => 'createaccount',
+			'name' => 'Test User',
+			'token' => LoginForm::getCreateaccountToken(),
+			'password' => 'password',
+			'email' => 'sjlfsjklj',
+		) );
+	}
+}
