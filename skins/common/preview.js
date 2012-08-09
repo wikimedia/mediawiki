446fa95c27cffd0862d773c2125ec30081dae1cf
MatmaRex <matma.rex@gmail.com>
2012-08-09 15:51:28 +0200

    (bug 25830) fix JS preview causing the page to "jump"
    Originally a patch by DieBuche <diebuche@gmail.com>, fixed to apply on
    master (only whitespace changes were needed), with multiple new tweaks and
    fixes.
    
    The patch introduces fadeTo(..., 0.4) instead of fadeIn/fadeOut, causing
    the preview not to be removed, but only faded out slightly. The reason
    for this change is twofold:
    * We do not want to completely remove the elements, which is what the old
      version did. This causes the edit field to jump up and then back down
      when the preview is reinserted.
    * We do not want to display a huge blank white space where the preview
      used to be before it disappeared.
    
    Instead of scrolling to the top, we scroll straight to the preview. This
    works when the preview is set to appear below edit field and possibly with
    non-default skins. (And also is much neater.) Thanks to Jdlrobson for
    noticing this issue.
    
    Change-Id: I743ed45e039445242a99a23cad719a75731c5562

--- skins/common/preview.js
+++ skins/common/preview.js
@@ -7,6 +7,15 @@
 
 		$( mw ).trigger( 'LivePreviewPrepare' );
 
+		var $wikiPreview = $( '#wikiPreview' );
+		
+		// show #wikiPreview if it's hidden (if it is hidden, it's also empty, so nothing changes in the rendering)
+		// to be able to scroll to it
+		$wikiPreview.show();
+		
+		// Jump to where the preview will appear
+		$wikiPreview[0].scrollIntoView();
+
 		var postData = $('#editform').formToArray();
 		postData.push( { 'name' : 'wpPreview', 'value' : '1' } );
 
@@ -15,11 +24,15 @@
 							'#catlinks'];
 		var copySelector = copyElements.join(',');
 
-		$.each( copyElements, function(k,v) { $(v).fadeOut('fast'); } );
+		$.each( copyElements, function( k, v ) {
+			$( v ).fadeTo( 'fast', 0.4 );
+		} );
 
 		// Display a loading graphic
 		var loadSpinner = $('<div class="mw-ajax-loader"/>');
-		$('#wikiPreview').before( loadSpinner );
+		// Move away from header (default is -16px)
+		loadSpinner.css('top', '0');
+		$wikiPreview.before( loadSpinner );
 
 		var page = $('<div/>');
 		var target = $('#editform').attr('action');
@@ -41,9 +54,11 @@
 					$(copyElements[i]).prop( 'class', newClasses );
 				}
 
-				$.each( copyElements, function(k,v) {
+				$.each( copyElements, function( k, v ) {
 					// Don't belligerently show elements that are supposed to be hidden
-					$(v).fadeIn( 'fast', function() { $(this).css('display', ''); } );
+					$( v ).fadeTo( 'fast', 1, function() {
+						$( this ).css( 'display', '' );
+					} );
 				} );
 
 				loadSpinner.remove();
