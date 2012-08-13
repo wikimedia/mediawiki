c172c22df07a80c68da251272eecf9c298b97230
Daniel Friesen <pub-github@nadir-seen-fire.com>
2012-08-12 23:18:07 -0700

    [mw.notification] Introduce a full fledged notification system into core replacing jsMessage.
    - Support for multiple messages instead of just one is included.
    - Persistent messages are also supported.
    - Tagged message replacement is supported so that only one message of a type will show up at once.
    - A light flood protection is included to stop all the notifications from disappearing at once.
    - Watchlist uses it's own tag now.
    - Legacy jsMessage messages still replace each other by using a tag: 'legacy' option
    - #mw-js-message was an old id used for something with a completely different style. So rather than re-using it that id is being dropped. Skins can now style the new notifications.
    - Core is no longer using messageBox and no extension uses it so instead of making a compatibility layer for it that module is just being dropped.
    
    This is a follow up to I41c70d78.
    
    Change-Id: I2b3d9cbce6d1514d754b4403ec878d89ea6af29d

--- resources/Resources.php
+++ resources/Resources.php
@@ -175,10 +175,6 @@ return array(
 		'styles' => 'resources/jquery/jquery.makeCollapsible.css',
 		'messages' => array( 'collapsible-expand', 'collapsible-collapse' ),
 	),
-	'jquery.messageBox' => array(
-		'scripts' => 'resources/jquery/jquery.messageBox.js',
-		'styles' => 'resources/jquery/jquery.messageBox.css',
-	),
 	'jquery.mockjax' => array(
 		'scripts' => 'resources/jquery/jquery.mockjax.js',
 	),
@@ -606,6 +602,15 @@ return array(
 	'mediawiki.htmlform' => array(
 		'scripts' => 'resources/mediawiki/mediawiki.htmlform.js',
 	),
+	'mediawiki.notification' => array(
+		'scripts' => 'resources/mediawiki/mediawiki.notification.js',
+		'dependencies' => array(
+			'mediawiki.page.startup',
+		),
+	),
+	'mediawiki.notify' => array(
+		'scripts' => 'resources/mediawiki/mediawiki.notify.js',
+	),
 	'mediawiki.Title' => array(
 		'scripts' => 'resources/mediawiki/mediawiki.Title.js',
 		'dependencies' => 'mediawiki.util',
@@ -625,8 +630,8 @@ return array(
 		'dependencies' => array(
 			'jquery.client',
 			'jquery.cookie',
-			'jquery.messageBox',
 			'jquery.mwExtension',
+			'mediawiki.notify',
 		),
 		'messages' => array( 'showtoc', 'hidetoc' ),
 		'position' => 'top', // For $wgPreloadJavaScriptMwUtil
@@ -781,7 +786,8 @@ return array(
 		'dependencies' => array(
 			'mediawiki.page.startup',
 			'mediawiki.api.watch',
-			'mediawiki.util'
+			'mediawiki.util',
+			'mediawiki.notify',
 		),
 		'messages' => array(
 			'watch',
