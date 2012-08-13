ebd4d281ea1d10eb53c7afb857000694f376b36b
Alex Monk <krenair@gmail.com>
2012-08-13 21:15:53 +0100

    (bug 24491) Don't show reupload link if the user can't upload because of protection
    Also remove a linebreak which someone complained about recently.
    
    Change-Id: I92ab8ad5eebcdd38c0d5d9f7bab7d11e5c844586

--- includes/ImagePage.php
+++ includes/ImagePage.php
@@ -632,10 +632,11 @@ EOT
 		}
 
 		$out = $this->getContext()->getOutput();
-		$out->addHTML( "<br /><ul>\n" );
+		$out->addHTML( "<ul>\n" );
 
 		# "Upload a new version of this file" link
-		if ( UploadBase::userCanReUpload( $this->getContext()->getUser(), $this->mPage->getFile()->name ) ) {
+		$canUpload = !$this->getTitle()->getUserPermissionsErrors( 'upload', $this->getContext()->getUser() );
+		if ( $canUpload && UploadBase::userCanReUpload( $this->getContext()->getUser(), $this->mPage->getFile()->name ) ) {
 			$ulink = Linker::makeExternalLink( $this->getUploadUrl(), wfMsg( 'uploadnewversion-linktext' ) );
 			$out->addHTML( "<li id=\"mw-imagepage-reupload-link\"><div class=\"plainlinks\">{$ulink}</div></li>\n" );
 		}
