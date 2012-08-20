22c33764a79e515d72599dde9a9cd69373793ebe
tpt <thomaspt@hotmail.fr>
2012-08-20 12:12:47 +0200

    (bug 23226) Add of class parameter to image links in order to add class(es) to HTML img tag.
    Change-Id: If58802ad2c513c1db7bc3488daf4e078b8694b02

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -282,6 +282,7 @@ $magicWords = array(
 	'img_text_bottom'         => array( 1,    'text-bottom' ),
 	'img_link'                => array( 1,    'link=$1' ),
 	'img_alt'                 => array( 1,    'alt=$1' ),
+	'img_class'               => array( 1,    'class=$1' ),
 	'int'                     => array( 0,    'INT:' ),
 	'sitename'                => array( 1,    'SITENAME' ),
 	'ns'                      => array( 0,    'NS:' ),
