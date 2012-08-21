a442a2e934fc8d4b3aaa1d6ebd2061786aae9fa8
Tyler Romeo <tylerromeo@gmail.com>
2012-08-21 11:52:47 -0400

    New hook ApiCheckCanExecute.
    Added new hook in ApiMain::checkCanExecute
    so that extensions can authenticate and
    authorize API clients before the module is
    execute. (Necessary for extensions like
    OAuth that externally authnz clients.)
    
    Change-Id: I1b059fd9a4aa717928af8b09f5edebe899ab3ce1
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -758,6 +758,12 @@ class ApiMain extends ApiBase {
 				$this->dieReadOnly();
 			}
 		}
+
+		// Allow extensions to stop execution for arbitrary reasons.
+		$message = false;
+		if( !wfRunHooks( 'ApiCheckCanExecute', array( $module, $user, &$message ) ) ) {
+			$this->dieUsage( $message );
+		}
 	}
 
 	/**
