wikimedia/mediawiki

commit 6fe1f0509baec67647dffd2e3bc123c6511d1511

Replace deprecated wfMsg* calls with Message class calls.
Doing this in steps of roughly 100 changes per commit, so that it remains reviewable.

Change-Id: Ib15e670badd3f6aecae8b60e2f9129a31341ce16

includes/Linker.php
@@ -345,7 +345,7 @@ class Linker {
 		} elseif ( in_array( 'known', $options ) ) {
 			$defaults['title'] = $target->getPrefixedText();
 		} else {
-			$defaults['title'] = wfMsg( 'red-link-title', $target->getPrefixedText() );
+			$defaults['title'] = wfMessage( 'red-link-title', $target->getPrefixedText() )->text();
 		}
 
 		# Finally, merge the custom attribs with the default ones, and iterate
@@ -825,7 +825,7 @@ class Linker {
 			$s .= self::makeBrokenImageLinkObj( $title, $fp['title'], '', '', '', $time == true );
 			$zoomIcon = '';
 		} elseif ( !$thumb ) {
-			$s .= htmlspecialchars( wfMsg( 'thumbnail_error', '' ) );
+			$s .= wfMessage( 'thumbnail_error', '' )->escaped();
 			$zoomIcon = '';
 		} else {
 			$params = array(
@@ -841,7 +841,7 @@ class Linker {
 					Html::rawElement( 'a', array(
 						'href' => $url,
 						'class' => 'internal',
-						'title' => wfMsg( 'thumbnail-more' ) ),
+						'title' => wfMessage( 'thumbnail-more' )->text() ),
 						Html::element( 'img', array(
 							'src' => $wgStylePath . '/common/images/magnify-clip' . ( $wgContLang->isRTL() ? '-rtl' : '' ) . '.png',
 							'width' => 15,
@@ -971,7 +971,7 @@ class Linker {
 			$key = strtolower( $name );
 		}
 
-		return self::linkKnown( SpecialPage::getTitleFor( $name ) , wfMsg( $key ) );
+		return self::linkKnown( SpecialPage::getTitleFor( $name ) , wfMessage( $key )->text() );
 	}
 
 	/**
@@ -1063,7 +1063,7 @@ class Linker {
 			}
 			$contribsPage = SpecialPage::getTitleFor( 'Contributions', $userText );
 
-			$items[] = self::link( $contribsPage, wfMsgHtml( 'contribslink' ), $attribs );
+			$items[] = self::link( $contribsPage, wfMessage( 'contribslink' )->escaped(), $attribs );
 		}
 		if ( $blockable && $wgUser->isAllowed( 'block' ) ) {
 			$items[] = self::blockLink( $userId, $userText );
@@ -1104,7 +1104,7 @@ class Linker {
 	 */
 	public static function userTalkLink( $userId, $userText ) {
 		$userTalkPage = Title::makeTitle( NS_USER_TALK, $userText );
-		$userTalkLink = self::link( $userTalkPage, wfMsgHtml( 'talkpagelinktext' ) );
+		$userTalkLink = self::link( $userTalkPage, wfMessage( 'talkpagelinktext' )->escaped() );
 		return $userTalkLink;
 	}
 
@@ -1115,7 +1115,7 @@ class Linker {
 	 */
 	public static function blockLink( $userId, $userText ) {
 		$blockPage = SpecialPage::getTitleFor( 'Block', $userText );
-		$blockLink = self::link( $blockPage, wfMsgHtml( 'blocklink' ) );
+		$blockLink = self::link( $blockPage, wfMessage( 'blocklink' )->escaped() );
 		return $blockLink;
 	}
 
@@ -1126,7 +1126,7 @@ class Linker {
 	 */
 	public static function emailLink( $userId, $userText ) {
 		$emailPage = SpecialPage::getTitleFor( 'Emailuser', $userText );
-		$emailLink = self::link( $emailPage, wfMsgHtml( 'emaillink' ) );
+		$emailLink = self::link( $emailPage, wfMessage( 'emaillink' )->escaped() );
 		return $emailLink;
 	}
 
@@ -1138,12 +1138,12 @@ class Linker {
 	 */
 	public static function revUserLink( $rev, $isPublic = false ) {
 		if ( $rev->isDeleted( Revision::DELETED_USER ) && $isPublic ) {
-			$link = wfMsgHtml( 'rev-deleted-user' );
+			$link = wfMessage( 'rev-deleted-user' )->escaped();
 		} elseif ( $rev->userCan( Revision::DELETED_USER ) ) {
 			$link = self::userLink( $rev->getUser( Revision::FOR_THIS_USER ),
 				$rev->getUserText( Revision::FOR_THIS_USER ) );
 		} else {
-			$link = wfMsgHtml( 'rev-deleted-user' );
+			$link = wfMessage( 'rev-deleted-user' )->escaped();
 		}
 		if ( $rev->isDeleted( Revision::DELETED_USER ) ) {
 			return '<span class="history-deleted">' . $link . '</span>';
@@ -1159,7 +1159,7 @@ class Linker {
 	 */
 	public static function revUserTools( $rev, $isPublic = false ) {
 		if ( $rev->isDeleted( Revision::DELETED_USER ) && $isPublic ) {
-			$link = wfMsgHtml( 'rev-deleted-user' );
+			$link = wfMessage( 'rev-deleted-user' )->escaped();
 		} elseif ( $rev->userCan( Revision::DELETED_USER ) ) {
 			$userId = $rev->getUser( Revision::FOR_THIS_USER );
 			$userText = $rev->getUserText( Revision::FOR_THIS_USER );
@@ -1167,7 +1167,7 @@ class Linker {
 				. wfMessage( 'word-separator' )->plain()
 				. self::userToolLinks( $userId, $userText );
 		} else {
-			$link = wfMsgHtml( 'rev-deleted-user' );
+			$link = wfMessage( 'rev-deleted-user' )->escaped();
 		}
 		if ( $rev->isDeleted( Revision::DELETED_USER ) ) {
 			return ' <span class="history-deleted">' . $link . '</span>';
@@ -1281,11 +1281,11 @@ class Linker {
 			}
 			if ( $pre ) {
 				# written summary $presep autocomment (summary /* section */)
-				$pre .= wfMsgExt( 'autocomment-prefix', array( 'escapenoentities', 'content' ) );
+				$pre .= wfMessage( 'autocomment-prefix' )->inContentLanguage()->escaped();
 			}
 			if ( $post ) {
 				# autocomment $postsep written summary (/* section */ summary)
-				$auto .= wfMsgExt( 'colon-separator', array( 'escapenoentities', 'content' ) );
+				$auto .= wfMessage( 'colon-separator' )->inContentLanguage()->escaped();
 			}
 			$auto = '<span class="autocomment">' . $auto . '</span>';
 			$comment = $pre . $link . $wgLang->getDirMark() . '<span dir="auto">' . $auto . $post . '</span>';
@@ -1507,12 +1507,12 @@ class Linker {
 			return "";
 		}
 		if ( $rev->isDeleted( Revision::DELETED_COMMENT ) && $isPublic ) {
-			$block = " <span class=\"comment\">" . wfMsgHtml( 'rev-deleted-comment' ) . "</span>";
+			$block = " <span class=\"comment\">" . wfMessage( 'rev-deleted-comment' )->escaped() . "</span>";
 		} elseif ( $rev->userCan( Revision::DELETED_COMMENT ) ) {
 			$block = self::commentBlock( $rev->getComment( Revision::FOR_THIS_USER ),
 				$rev->getTitle(), $local );
 		} else {
-			$block = " <span class=\"comment\">" . wfMsgHtml( 'rev-deleted-comment' ) . "</span>";
+			$block = " <span class=\"comment\">" . wfMessage( 'rev-deleted-comment' )->escaped() . "</span>";
 		}
 		if ( $rev->isDeleted( Revision::DELETED_COMMENT ) ) {
 			return " <span class=\"history-deleted\">$block</span>";
@@ -1526,13 +1526,11 @@ class Linker {
 	 */
 	public static function formatRevisionSize( $size ) {
 		if ( $size == 0 ) {
-			$stxt = wfMsgExt( 'historyempty', 'parsemag' );
+			$stxt = wfMessage( 'historyempty' )->escaped();
 		} else {
-			global $wgLang;
-			$stxt = wfMsgExt( 'nbytes', 'parsemag', $wgLang->formatNum( $size ) );
+			$stxt = wfMessage( 'nbytes' )->numParams( $size )->escaped();
 			$stxt = wfMessage( 'parentheses' )->rawParams( $stxt )->escaped();
 		}
-		$stxt = htmlspecialchars( $stxt );
 		return "<span class=\"history-size\">$stxt</span>";
 	}
 
@@ -1588,7 +1586,7 @@ class Linker {
 	 * @return String: full html of the TOC
 	 */
 	public static function tocList( $toc, $lang = false ) {
-		$title = wfMsgExt( 'toc', array( 'language' => $lang, 'escape' ) );
+		$title = wfMessage( 'toc' )->inLanguage( $lang )->escaped();
 		return
 		   '<table id="toc" class="toc"><tr><td>'
 		 . '<div id="toctitle"><h2>' . $title . "</h2></div>\n"
@@ -1803,11 +1801,14 @@ class Linker {
 			# Construct the HTML
 			$outText = '<div class="mw-templatesUsedExplanation">';
 			if ( $preview ) {
-				$outText .= wfMsgExt( 'templatesusedpreview', array( 'parse' ), count( $templates ) );
+				$outText .= wfMessage( 'templatesusedpreview' )->numParams( count( $templates ) )
+					->parseAsBlock();
 			} elseif ( $section ) {
-				$outText .= wfMsgExt( 'templatesusedsection', array( 'parse' ), count( $templates ) );
+				$outText .= wfMessage( 'templatesusedsection' )->numParams( count( $templates ) )
+					->parseAsBlock();
 			} else {
-				$outText .= wfMsgExt( 'templatesused', array( 'parse' ), count( $templates ) );
+				$outText .= wfMessage( 'templatesused' )->numParams( count( $templates ) )
+					->parseAsBlock();
 			}
 			$outText .= "</div><ul>\n";
 
@@ -1815,23 +1816,23 @@ class Linker {
 			foreach ( $templates as $titleObj ) {
 				$r = $titleObj->getRestrictions( 'edit' );
 				if ( in_array( 'sysop', $r ) ) {
-					$protected = wfMsgExt( 'template-protected', array( 'parseinline' ) );
+					$protected = wfMessage( 'template-protected' )->parse();
 				} elseif ( in_array( 'autoconfirmed', $r ) ) {
-					$protected = wfMsgExt( 'template-semiprotected', array( 'parseinline' ) );
+					$protected = wfMessage( 'template-semiprotected' )->parse();
 				} else {
 					$protected = '';
 				}
 				if ( $titleObj->quickUserCan( 'edit' ) ) {
 					$editLink = self::link(
 						$titleObj,
-						wfMsg( 'editlink' ),
+						wfMessage( 'editlink' )->text(),
 						array(),
 						array( 'action' => 'edit' )
 					);
 				} else {
 					$editLink = self::link(
 						$titleObj,
-						wfMsg( 'viewsourcelink' ),
+						wfMessage( 'viewsourcelink' )->text(),
 						array(),
 						array( 'action' => 'edit' )
 					);
@@ -1859,7 +1860,7 @@ class Linker {
 		if ( count( $hiddencats ) > 0 ) {
 			# Construct the HTML
 			$outText = '<div class="mw-hiddenCategoriesExplanation">';
-			$outText .= wfMsgExt( 'hiddencategories', array( 'parse' ), $wgLang->formatnum( count( $hiddencats ) ) );
+			$outText .= wfMessage( 'hiddencategories' )->numParams( count( $hiddencats ) )->parseAsBlock();
 			$outText .= "</div><ul>\n";
 
 			foreach ( $hiddencats as $titleObj ) {
@@ -2019,7 +2020,8 @@ class Linker {
 	 */
 	public static function revDeleteLink( $query = array(), $restricted = false, $delete = true ) {
 		$sp = SpecialPage::getTitleFor( 'Revisiondelete' );
-		$html = $delete ? wfMsgHtml( 'rev-delundel' ) : wfMsgHtml( 'rev-showdeleted' );
+		$msgKey = $delete ? 'rev-delundel' : 'rev-showdeleted';
+		$html = wfMessage( $msgKey )->escaped();
 		$tag = $restricted ? 'strong' : 'span';
 		$link = self::link( $sp, $html, array(), $query, array( 'known', 'noclasses' ) );
 		return Xml::tags( $tag, array( 'class' => 'mw-revdelundel-link' ), wfMessage( 'parentheses' )->rawParams( $link )->escaped() );
@@ -2034,8 +2036,10 @@ class Linker {
 	 * of appearance with CSS
 	 */
 	public static function revDeleteLinkDisabled( $delete = true ) {
-		$html = $delete ? wfMsgHtml( 'rev-delundel' ) : wfMsgHtml( 'rev-showdeleted' );
-		return Xml::tags( 'span', array( 'class' => 'mw-revdelundel-link' ), wfMessage( 'parentheses' )->rawParams( $html )->escaped() );
+		$msgKey = $delete ? 'rev-delundel' : 'rev-showdeleted';
+		$html = wfMessage( $msgKey )->escaped();
+		$htmlParentheses = wfMessage( 'parentheses' )->rawParams( $html )->escaped();
+		return Xml::tags( 'span', array( 'class' => 'mw-revdelundel-link' ), $htmlParentheses );
 	}
 
 	/* Deprecated methods */

includes/Exception.php
@@ -45,7 +45,7 @@ class MWException extends Exception {
 	}
 
 	/**
-	 * Can the extension use wfMsg() to get i18n messages?
+	 * Can the extension use the Message class/wfMessage to get i18n-ed messages?
 	 *
 	 * @return bool
 	 */
@@ -109,7 +109,7 @@ class MWException extends Exception {
 		$args = array_slice( func_get_args(), 2 );
 
 		if ( $this->useMessageCache() ) {
-			return wfMsgNoTrans( $key, $args );
+			return wfMessage( $key, $args )->plain();
 		} else {
 			return wfMsgReplaceArgs( $fallback, $args );
 		}
@@ -325,7 +325,7 @@ class ErrorPageError extends MWException {
 	 *
 	 * @param $title A title
 	 * @param $msg String|Message . In string form, should be a message key
-	 * @param $params Array Array to wfMsg()
+	 * @param $params Array Array to wfMessage()
 	 */
 	function __construct( $title, $msg, $params = null ) {
 		$this->title = $title;
@@ -335,7 +335,7 @@ class ErrorPageError extends MWException {
 		if( $msg instanceof Message ){
 			parent::__construct( $msg );
 		} else {
-			parent::__construct( wfMsg( $msg ) );
+			parent::__construct( wfMessage( $msg )->text() );
 		}
 	}
 
@@ -359,7 +359,7 @@ class BadTitleError extends ErrorPageError {
 
 	/**
 	 * @param $msg string A message key (default: 'badtitletext')
-	 * @param $params Array parameter to wfMsg()
+	 * @param $params Array parameter to wfMessage()
 	 */
 	function __construct( $msg = 'badtitletext', $params = null ) {
 		parent::__construct( 'badtitle', $msg, $params );
@@ -477,7 +477,7 @@ class UserBlockedError extends ErrorPageError {
 
 		$reason = $block->mReason;
 		if( $reason == '' ) {
-			$reason = wfMsg( 'blockednoreason' );
+			$reason = wfMessage( 'blockednoreason' )->text();
 		}
 
 		/* $ip returns who *is* being blocked, $intended contains who was meant to be blocked.
@@ -536,7 +536,7 @@ class UserNotLoggedIn extends ErrorPageError {
 	 *        Optional, default: 'exception-nologin-text'
 	 * @param $titleMsg A message key to set the page title.
 	 *        Optional, default: 'exception-nologin'
-	 * @param $params Parameters to wfMsg().
+	 * @param $params Parameters to wfMessage().
 	 *        Optiona, default: null
 	 */
 	public function __construct(

includes/FeedUtils.php
@@ -129,22 +129,22 @@ class FeedUtils {
 		if( $oldid ) {
 			wfProfileIn( __METHOD__."-dodiff" );
 
-			#$diffText = $de->getDiff( wfMsg( 'revisionasof',
+			#$diffText = $de->getDiff( wfMessage( 'revisionasof',
 			#	$wgLang->timeanddate( $timestamp ),
 			#	$wgLang->date( $timestamp ),
-			#	$wgLang->time( $timestamp ) ),
-			#	wfMsg( 'currentrev' ) );
+			#	$wgLang->time( $timestamp ) )->text(),
+			#	wfMessage( 'currentrev' )->text() );
 
 			$diffText = '';
 			// Don't bother generating the diff if we won't be able to show it
 			if ( $wgFeedDiffCutoff > 0 ) {
 				$de = new DifferenceEngine( $title, $oldid, $newid );
 				$diffText = $de->getDiff(
-					wfMsg( 'previousrevision' ), // hack
-					wfMsg( 'revisionasof',
+					wfMessage( 'previousrevision' )->text(), // hack
+					wfMessage( 'revisionasof',
 					$wgLang->timeanddate( $timestamp ),
 					$wgLang->date( $timestamp ),
-					$wgLang->time( $timestamp ) ) );
+					$wgLang->time( $timestamp ) )->text() );
 			}
 
 			if ( $wgFeedDiffCutoff <= 0 || ( strlen( $diffText ) > $wgFeedDiffCutoff ) ) {
@@ -170,7 +170,7 @@ class FeedUtils {
 				// Omit large new page diffs, bug 29110
 				$diffText = self::getDiffLink( $title, $newid );
 			} else {
-				$diffText = '<p><b>' . wfMsg( 'newpage' ) . '</b></p>' .
+				$diffText = '<p><b>' . wfMessage( 'newpage' )->text() . '</b></p>' .
 					'<div>' . nl2br( htmlspecialchars( $newtext ) ) . '</div>';
 			}
 		}
@@ -196,7 +196,7 @@ class FeedUtils {
 		$diffUrl = $title->getFullUrl( $queryParameters );
 
 		$diffLink = Html::element( 'a', array( 'href' => $diffUrl ),
-			wfMsgForContent( 'showdiff' ) );
+			wfMessage( 'showdiff' )->inContentLanguage()->text() );
 
 		return $diffLink;
 	}

includes/FileDeleteForm.php
@@ -101,7 +101,8 @@ class FileDeleteForm {
 				$reason = $deleteReason;
 			} elseif ( $deleteReason != '' ) {
 				// Entry from drop down menu + additional comment
-				$reason = $deleteReasonList . wfMsgForContent( 'colon-separator' ) . $deleteReason;
+				$reason = $deleteReasonList . wfMessage( 'colon-separator' )
+					->inContentLanguage()->text() . $deleteReason;
 			} else {
 				$reason = $deleteReasonList;
 			}
@@ -156,9 +157,10 @@ class FileDeleteForm {
 			$status = $file->deleteOld( $oldimage, $reason, $suppress );
 			if( $status->ok ) {
 				// Need to do a log item
-				$logComment = wfMsgForContent( 'deletedrevision', $oldimage );
+				$logComment = wfMessage( 'deletedrevision', $oldimage )->inContentLanguage()->text();
 				if( trim( $reason ) != '' ) {
-					$logComment .= wfMsgForContent( 'colon-separator' ) . $reason;
+					$logComment .= wfMessage( 'colon-separator' )
+						->inContentLanguage()->text() . $reason;
 				}
 
 				$logtype = $suppress ? 'suppress' : 'delete';
@@ -214,7 +216,7 @@ class FileDeleteForm {
 			$suppress = "<tr id=\"wpDeleteSuppressRow\">
 					<td></td>
 					<td class='mw-input'><strong>" .
-						Xml::checkLabel( wfMsg( 'revdelete-suppress' ),
+						Xml::checkLabel( wfMessage( 'revdelete-suppress' )->text(),
 							'wpSuppress', 'wpSuppress', false, array( 'tabindex' => '3' ) ) .
 					"</strong></td>
 				</tr>";
@@ -226,23 +228,28 @@ class FileDeleteForm {
 		$form = Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->getAction(),
 			'id' => 'mw-img-deleteconfirm' ) ) .
 			Xml::openElement( 'fieldset' ) .
-			Xml::element( 'legend', null, wfMsg( 'filedelete-legend' ) ) .
+			Xml::element( 'legend', null, wfMessage( 'filedelete-legend' )->text() ) .
 			Html::hidden( 'wpEditToken', $wgUser->getEditToken( $this->oldimage ) ) .
 			$this->prepareMessage( 'filedelete-intro' ) .
 			Xml::openElement( 'table', array( 'id' => 'mw-img-deleteconfirm-table' ) ) .
 			"<tr>
 				<td class='mw-label'>" .
-					Xml::label( wfMsg( 'filedelete-comment' ), 'wpDeleteReasonList' ) .
+					Xml::label( wfMessage( 'filedelete-comment' )->text(), 'wpDeleteReasonList' ) .
 				"</td>
 				<td class='mw-input'>" .
-					Xml::listDropDown( 'wpDeleteReasonList',
-						wfMsgForContent( 'filedelete-reason-dropdown' ),
-						wfMsgForContent( 'filedelete-reason-otherlist' ), '', 'wpReasonDropDown', 1 ) .
+					Xml::listDropDown(
+						'wpDeleteReasonList',
+						wfMessage( 'filedelete-reason-dropdown' )->inContentLanguage()->text(),
+						wfMessage( 'filedelete-reason-otherlist' )->inContentLanguage()->text(),
+						'',
+						'wpReasonDropDown',
+						1
+					) .
 				"</td>
 			</tr>
 			<tr>
 				<td class='mw-label'>" .
-					Xml::label( wfMsg( 'filedelete-otherreason' ), 'wpReason' ) .
+					Xml::label( wfMessage( 'filedelete-otherreason' )->text(), 'wpReason' ) .
 				"</td>
 				<td class='mw-input'>" .
 					Xml::input( 'wpReason', 60, $wgRequest->getText( 'wpReason' ),
@@ -255,7 +262,7 @@ class FileDeleteForm {
 			<tr>
 				<td></td>
 				<td class='mw-input'>" .
-					Xml::checkLabel( wfMsg( 'watchthis' ),
+					Xml::checkLabel( wfMessage( 'watchthis' )->text(),
 						'wpWatch', 'wpWatch', $checkWatch, array( 'tabindex' => '3' ) ) .
 				"</td>
 			</tr>";
@@ -264,7 +271,7 @@ class FileDeleteForm {
 			<tr>
 				<td></td>
 				<td class='mw-submit'>" .
-					Xml::submitButton( wfMsg( 'filedelete-submit' ),
+					Xml::submitButton( wfMessage( 'filedelete-submit' )->text(),
 						array( 'name' => 'mw-filedelete-submit', 'id' => 'mw-filedelete-submit', 'tabindex' => '4' ) ) .
 				"</td>
 			</tr>" .
@@ -276,7 +283,7 @@ class FileDeleteForm {
 				$title = Title::makeTitle( NS_MEDIAWIKI, 'Filedelete-reason-dropdown' );
 				$link = Linker::link(
 					$title,
-					wfMsgHtml( 'filedelete-edit-reasonlist' ),
+					wfMessage( 'filedelete-edit-reasonlist' )->escaped(),
 					array(),
 					array( 'action' => 'edit' )
 				);
@@ -307,19 +314,17 @@ class FileDeleteForm {
 	private function prepareMessage( $message ) {
 		global $wgLang;
 		if( $this->oldimage ) {
-			return wfMsgExt(
+			return wfMessage(
 				"{$message}-old", # To ensure grep will find them: 'filedelete-intro-old', 'filedelete-nofile-old', 'filedelete-success-old'
-				'parse',
 				wfEscapeWikiText( $this->title->getText() ),
 				$wgLang->date( $this->getTimestamp(), true ),
 				$wgLang->time( $this->getTimestamp(), true ),
-				wfExpandUrl( $this->file->getArchiveUrl( $this->oldimage ), PROTO_CURRENT ) );
+				wfExpandUrl( $this->file->getArchiveUrl( $this->oldimage ), PROTO_CURRENT ) )->parseAsBlock();
 		} else {
-			return wfMsgExt(
+			return wfMessage(
 				$message,
-				'parse',
 				wfEscapeWikiText( $this->title->getText() )
-			);
+			)->parseAsBlock();
 		}
 	}
 

includes/GlobalFunctions.php
@@ -1684,7 +1684,7 @@ function wfMsgExt( $key, $options ) {
  * looked up didn't exist but a XHTML string, this function checks for the
  * nonexistance of messages by checking the MessageCache::get() result directly.
  *
- * @deprecated since 1.18
+ * @deprecated since 1.18. Use Message::isDisabled().
  *
  * @param $key      String: the message key looked up
  * @return Boolean True if the message *doesn't* exist.

includes/HTMLForm.php
@@ -424,7 +424,7 @@ class HTMLForm extends ContextSource {
 
 	/**
 	 * Set a message to display on a validation error.
-	 * @param $msg Mixed String or Array of valid inputs to wfMsgExt()
+	 * @param $msg Mixed String or Array of valid inputs to wfMessage()
 	 *	 (so each entry can be either a String or Array)
 	 * @return HTMLForm $this for chaining calls (since 1.20)
 	 */
@@ -700,7 +700,7 @@ class HTMLForm extends ContextSource {
 				'input',
 				array(
 					'type' => 'reset',
-					'value' => wfMsg( 'htmlform-reset' )
+					'value' => wfMessage( 'htmlform-reset' )->text()
 				)
 			) . "\n";
 		}
@@ -776,7 +776,7 @@ class HTMLForm extends ContextSource {
 			$errorstr .= Html::rawElement(
 				'li',
 				array(),
-				wfMsgExt( $msg, array( 'parseinline' ), $error )
+				wfMessage( $msg, $error )->parse()
 			);
 		}
 
@@ -813,7 +813,7 @@ class HTMLForm extends ContextSource {
 	function getSubmitText() {
 		return $this->mSubmitText
 			? $this->mSubmitText
-			: wfMsg( 'htmlform-submit' );
+			: wfMessage( 'htmlform-submit' )->text();
 	}
 
 	/**
@@ -1051,7 +1051,7 @@ class HTMLForm extends ContextSource {
 	 * @return String
 	 */
 	public function getLegend( $key ) {
-		return wfMsg( "{$this->mMessagePrefix}-$key" );
+		return wfMessage( "{$this->mMessagePrefix}-$key" )->text();
 	}
 
 	/**
@@ -1110,7 +1110,7 @@ abstract class HTMLFormField {
 	 */
 	function validate( $value, $alldata ) {
 		if ( isset( $this->mParams['required'] ) && $this->mParams['required'] !== false && $value === '' ) {
-			return wfMsgExt( 'htmlform-required', 'parseinline' );
+			return wfMessage( 'htmlform-required' )->parse();
 		}
 
 		if ( isset( $this->mValidationCallback ) ) {
@@ -1170,7 +1170,7 @@ abstract class HTMLFormField {
 				$msgInfo = array();
 			}
 
-			$this->mLabel = wfMsgExt( $msg, 'parseinline', $msgInfo );
+			$this->mLabel = wfMessage( $msg, $msgInfo )->parse();
 		} elseif ( isset( $params['label'] ) ) {
 			$this->mLabel = $params['label'];
 		}
@@ -1642,7 +1642,7 @@ class HTMLFloatField extends HTMLTextField {
 		# http://dev.w3.org/html5/spec/common-microsyntaxes.html#real-numbers
 		# with the addition that a leading '+' sign is ok.
 		if ( !preg_match( '/^((\+|\-)?\d+(\.\d+)?(E(\+|\-)?\d+)?)?$/i', $value ) ) {
-			return wfMsgExt( 'htmlform-float-invalid', 'parse' );
+			return wfMessage( 'htmlform-float-invalid' )->parseAsBlock();
 		}
 
 		# The "int" part of these message names is rather confusing.
@@ -1651,7 +1651,7 @@ class HTMLFloatField extends HTMLTextField {
 			$min = $this->mParams['min'];
 
 			if ( $min > $value ) {
-				return wfMsgExt( 'htmlform-int-toolow', 'parse', array( $min ) );
+				return wfMessage( 'htmlform-int-toolow', array( $min ) )->parseAsBlock();
 			}
 		}
 
@@ -1659,7 +1659,7 @@ class HTMLFloatField extends HTMLTextField {
 			$max = $this->mParams['max'];
 
 			if ( $max < $value ) {
-				return wfMsgExt( 'htmlform-int-toohigh', 'parse', array( $max ) );
+				return wfMessage( 'htmlform-int-toohigh', array( $max ) )->parseAsBlock();
 			}
 		}
 
@@ -1686,7 +1686,7 @@ class HTMLIntField extends HTMLFloatField {
 		# value to, eg, save in the DB, clean it up with intval().
 		if ( !preg_match( '/^((\+|\-)?\d+)?$/', trim( $value ) )
 		) {
-			return wfMsgExt( 'htmlform-int-invalid', 'parse' );
+			return wfMessage( 'htmlform-int-invalid' )->parseAsBlock();
 		}
 
 		return true;
@@ -1770,7 +1770,7 @@ class HTMLSelectField extends HTMLFormField {
 		if ( in_array( $value, $validOptions ) )
 			return true;
 		else
-			return wfMsgExt( 'htmlform-select-badoption', 'parseinline' );
+			return wfMessage( 'htmlform-select-badoption' )->parse();
 	}
 
 	function getInputHTML( $value ) {
@@ -1808,7 +1808,9 @@ class HTMLSelectOrOtherField extends HTMLTextField {
 
 	function __construct( $params ) {
 		if ( !in_array( 'other', $params['options'], true ) ) {
-			$msg = isset( $params['other'] ) ? $params['other'] : wfMsg( 'htmlform-selectorother-other' );
+			$msg = isset( $params['other'] ) ?
+				$params['other'] :
+				wfMessage( 'htmlform-selectorother-other' )->text();
 			$params['options'][$msg] = 'other';
 		}
 
@@ -1912,7 +1914,7 @@ class HTMLMultiSelectField extends HTMLFormField {
 		if ( count( $validValues ) == count( $value ) ) {
 			return true;
 		} else {
-			return wfMsgExt( 'htmlform-select-badoption', 'parseinline' );
+			return wfMessage( 'htmlform-select-badoption' )->parse();
 		}
 	}
 
@@ -2114,7 +2116,7 @@ class HTMLSelectAndOtherField extends HTMLSelectField {
 			} elseif ( $text == '' ) {
 				$final = $list;
 			} else {
-				$final = $list . wfMsgForContent( 'colon-separator' ) . $text;
+				$final = $list . wfMessage( 'colon-separator' )->inContentLanguage()->text() . $text;
 			}
 
 		} else {
@@ -2123,7 +2125,7 @@ class HTMLSelectAndOtherField extends HTMLSelectField {
 			$list = 'other';
 			$text = $final;
 			foreach ( $this->mFlatOptions as $option ) {
-				$match = $option . wfMsgForContent( 'colon-separator' );
+				$match = $option . wfMessage( 'colon-separator' )->inContentLanguage()->text();
 				if ( strpos( $text, $match ) === 0 ) {
 					$list = $option;
 					$text = substr( $text, strlen( $match ) );
@@ -2151,7 +2153,7 @@ class HTMLSelectAndOtherField extends HTMLSelectField {
 		}
 
 		if ( isset( $this->mParams['required'] ) && $this->mParams['required'] !== false && $value[1] === '' ) {
-			return wfMsgExt( 'htmlform-required', 'parseinline' );
+			return wfMessage( 'htmlform-required' )->parse();
 		}
 
 		return true;
@@ -2180,7 +2182,7 @@ class HTMLRadioField extends HTMLFormField {
 		if ( in_array( $value, $validOptions ) ) {
 			return true;
 		} else {
-			return wfMsgExt( 'htmlform-select-badoption', 'parseinline' );
+			return wfMessage( 'htmlform-select-badoption' )->parse();
 		}
 	}
 

commit f80b212aa2fc1ae9067269d9058f35abf21f6d45

Fix tocList() breakage in 6fe1f050
The $lang parameter defaults to false, but Message::inLanguage() doesn't
like false, so do an explicit check for false.

Change-Id: I66fb2d11269a988ae92b1594314807a4eed54f1b

includes/Linker.php
@@ -1594,7 +1594,11 @@ class Linker {
 	 * @return String: full html of the TOC
 	 */
 	public static function tocList( $toc, $lang = false ) {
-		$title = wfMessage( 'toc' )->inLanguage( $lang )->escaped();
+		$msg = wfMessage( 'toc' );
+		if ( $lang !== false ) {
+			$msg = $msg->inLanguage( $lang );
+		}
+		$title = $msg->escaped();
 		return
 		   '<table id="toc" class="toc"><tr><td>'
 		 . '<div id="toctitle"><h2>' . $title . "</h2></div>\n"