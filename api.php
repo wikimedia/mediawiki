37df22248f27ce73d4d51a99bc0f29c8beec730d
Hoo man <hoo@online.de>
2012-08-28 23:20:43 +0200

    Follow up to dirname( __FILE__ ) clean up
    Remove a trailing whitespace added by me in dirname( __FILE__ )
    clean up and add some comments to the PHP version checks (they
    can't use __DIR__ as they have to run in older PHP versions.
    
    Change-Id: I6a7e8cb7dbf384d91dda4d0ecbef7ce588bc3073

--- api.php
+++ api.php
@@ -35,6 +35,7 @@ define( 'MW_API', true );
 
 // Bail if PHP is too low
 if ( !function_exists( 'version_compare' ) || version_compare( phpversion(), '5.3.2' ) < 0 ) {
+	//We need to use dirname( __FILE__ ) here cause __DIR__ is PHP5.3+
 	require( dirname( __FILE__ ) . '/includes/PHPVersionError.php' );
 	wfPHPVersionError( 'api.php' );
 }
