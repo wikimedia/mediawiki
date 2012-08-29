4aac300deadd83c8498121fcc64d6927fa4b8279
Krinkle <ttijhof@wikimedia.org>
2012-08-29 20:56:43 +0000

    Revert "Revert "Update jQuery UI to 1.8.23""
    Problems solved.
    
    This reverts commit 731219d5d1c82eb199eb9808948740555d2ebe14
    
    This commit merges c24daca2ff833adcd0a496b63d91302606b7ffe5 from master.
    
    Change-Id: I35ed086e9a8c6de582a8cbaa6f8bacb40769ac69

--- resources/jquery.ui/jquery.ui.tabs.js
+++ resources/jquery.ui/jquery.ui.tabs.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI Tabs 1.8.22
+ * jQuery UI Tabs 1.8.23
  *
  * Copyright 2012, AUTHORS.txt (http://jqueryui.com/about)
  * Dual licensed under the MIT or GPL Version 2 licenses.
@@ -698,7 +698,7 @@ $.widget( "ui.tabs", {
 });
 
 $.extend( $.ui.tabs, {
-	version: "1.8.22"
+	version: "1.8.23"
 });
 
 /*
