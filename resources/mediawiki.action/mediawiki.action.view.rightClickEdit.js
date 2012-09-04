a5c83c2c1a47889dc79fb8c377d266fc8e0f6807
Timo Tijhof <ttijhof@wikimedia.org>
2012-09-04 21:47:20 +0200

    Various javascript optimizations (fixes bug 39959)
    * (bug 39959) Right click to edit section causing browser hangs
    
      The :has() selector has gotten significantly slower in
      jQuery 1.8.1 however the reason it affected us so much is
      partly because of the very inefficient way we used it.
    
      Namely duplicated 6 times and evaluated from a live() binding
      In order words, on every instance of the event.
    
      Optimizing by
      - only select headings, do the :has implicitly by checking
        for the result of .find().
      - document.on(.., selector) instead of .live()
      - remove redundant and overkill 'return false'
    
      Results in Firefox 15/Chrome 21:
      - up to 130x (one hundred and thirty) faster!
    
    * /resources/* (except for third party libs)
      - Use .on() instead of bind, delegate or live
      - Use .off() instead of unbind, undelegate or die
      - Use document.getElementById instead of $('#' + variable)
        not for performance but for predictability/security.
        (e.g. rel="foo.bar" should select id="foo.bar" not
        id="foo" class="bar")
    
      - Fix some minor whitespace and code conventions.
    
    Change-Id: I2fefb5376d0de40f4997a3a1763eee23fcd3e7fa

--- resources/mediawiki.action/mediawiki.action.view.rightClickEdit.js
+++ resources/mediawiki.action/mediawiki.action.view.rightClickEdit.js
@@ -1,24 +1,30 @@
 /*
- * JavaScript to enable right click edit functionality
+ * JavaScript to enable right click edit functionality.
+ * When the user right-clicks in a heading, it will open the
+ * edit screen.
  */
-jQuery( function( $ ) {
+jQuery( function ( $ ) {
 	// Select all h1-h6 elements that contain editsection links
-	$( 'h1:has(.editsection a), ' +
-		'h2:has(.editsection a), ' +
-		'h3:has(.editsection a), ' +
-		'h4:has(.editsection a), ' +
-		'h5:has(.editsection a), ' +
-		'h6:has(.editsection a)'
-	).live( 'contextmenu', function( e ) {
-		// Get href of the [edit] link
-		var href = $(this).find( '.editsection a' ).attr( 'href' );
-		// Check if target is the anchor link itself. If so, don't suppress the context menu; this
-		// way the reader can still do things like copy URL, open in new tab etc.
-		var $target = $( e.target );
-		if ( !$target.is( 'a' ) && !$target.parent().is( '.editsection' ) ){
+	// Don't use the ":has:(.editsection a)" selector because it performs very bad.
+	// http://jsperf.com/jq-1-7-2-vs-jq-1-8-1-performance-of-mw-has/2
+	$( document ).on( 'contextmenu', 'h1, h2, h3, h4, h5, h6', function ( e ) {
+		var $edit, href;
+
+		$edit = $( this ).find( '.editsection a' );
+		if ( !$edit.length ) {
+			return;
+		}
+
+		// Get href of the editsection link
+		href = $edit.prop( 'href' );
+
+		// Headings can contain rich text.
+		// Make sure to not block contextmenu events on (other) anchor tags
+		// inside the heading (e.g. to do things like copy URL, open in new tab, ..).
+		// e.target can be the heading, but it can also be anything inside the heading.
+		if ( href && e.target.nodeName.toLowerCase() !== 'a' ){
 			window.location = href;
 			e.preventDefault();
-			return false;
 		}
 	} );
 } );
