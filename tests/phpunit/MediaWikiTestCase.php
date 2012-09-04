3bae3f4e32eab05a08720cf4b8947428cc7f4cd5
jeroendedauw <jeroendedauw@gmail.com>
2012-09-04 15:53:05 +0200

    Added MediaWikiTestCase::assertTypeOrValue to facilitate common type checks
    Also added MediaWikiTestCase::assertType which accepts both internal types, classes and interfaces
    
    Change-Id: I168ef17ad2da3b744a106742760ef34cc683bf69

--- tests/phpunit/MediaWikiTestCase.php
+++ tests/phpunit/MediaWikiTestCase.php
@@ -478,4 +478,58 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 		}
 	}
 
+	/**
+	 * Asserts that the provided variable is of the specified
+	 * internal type or equals the $value argument. This is useful
+	 * for testing return types of functions that return a certain
+	 * type or *value* when not set or on error.
+	 *
+	 * @since 1.20
+	 *
+	 * @param string $type
+	 * @param mixed $actual
+	 * @param mixed $value
+	 * @param string $message
+	 */
+	protected function assertTypeOrValue( $type, $actual, $value = false, $message = '' ) {
+		if ( $actual === $value ) {
+			$this->assertTrue( true, $message );
+		}
+		else {
+			$this->assertType( $type, $actual, $message );
+		}
+	}
+
+	/**
+	 * Asserts the type of the provided value. This can be either
+	 * in internal type such as boolean or integer, or a class or
+	 * interface the value extends or implements.
+	 *
+	 * @since 1.20
+	 *
+	 * @param string $type
+	 * @param mixed $actual
+	 * @param string $message
+	 */
+	protected function assertType( $type, $actual, $message = '' ) {
+		// http://php.net/manual/en/function.gettype.php
+		$internalTypes = array(
+			'boolean',
+			'integer',
+			'double',
+			'float',
+			'string',
+			'array',
+			'resource',
+			'NULL',
+		);
+
+		if ( in_array( gettype( $actual ), $internalTypes ) ) {
+			$this->assertInternalType( $type, $actual, $message );
+		}
+		else {
+			$this->assertInstanceOf( $type, $actual, $message );
+		}
+	}
+
 }
