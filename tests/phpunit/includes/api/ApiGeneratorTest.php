af8bfab7f442d3f46ea47ce71e240af4b676979c
Reedy <reedy@wikimedia.org>
2012-09-06 19:36:53 +0100

    * (bug 39665) List of query generators is now not built using reflection,   instead it is defined in code.
    Per Domas, make this a hard coded list, added $wgAPIGeneratorModules
    for people to register their API generator modules
    
    Change-Id: I12da92da33527e414c9b125a50b82c9bdbb3ed99

---
+++ tests/phpunit/includes/api/ApiGeneratorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class ApiGeneratorTest extends MediaWikiTestCase {
+	/**
+	 * Tests whether all registered query modules which are subclasses of ApiQueryGeneratorBase
+	 * are listed as being a generator.
+	 * Also tests whether all modules listed as a Generator in ApiQuery
+	 * are actually a generator (subclass of ApiQueryGeneratorBase).
+	 *
+	 * Tests user additions to $wgAPIGeneratorModules aswell.
+	 */
+	public function testIfGenerator() {
+		$main = new ApiMain( new FauxRequest() );
+		$query = new ApiQuery( $main, 'foo', 'bar' );
+		$generators = $query->getGenerators();
+		$modules = $query->getModules();
+		foreach ( $modules as $moduleName => $moduleClass ) {
+			if ( is_subclass_of( $moduleClass, 'ApiQueryGeneratorBase' ) ) {
+				if ( !isset( $generators[$moduleName] ) ) {
+					$this->fail( "'{$moduleClass}' is a subclass of ApiQueryGeneratorBase, but isn't listed as being a generator" );
+				}
+			} elseif ( isset( $generators[$moduleName] ) ) {
+				$this->fail( "'{$moduleClass}' is not a subclass of ApiQueryGeneratorBase, but is listed as being a generator" );
+			}
+		}
+		$this->assertTrue( true );
+	}
+}
