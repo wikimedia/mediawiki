68b6bcbe687bac0897e4727a034590a2bc233e5c
Bergi <a.d.bergi@web.de>
2012-09-12 21:37:30 +0200

    Fixing some bugs in jquery.byteLimit.js
    As reported at http://de.wikipedia.org?oldid=107992278, FF13 and IE8
    refuse any input when a custom modifier function is given and
    $el.removeProp("maxLength") is called.
    A test of this behaviour is available at http://jsfiddle.net/Rtnj6/
    Removing the attribute works, so I've changed this and some minor tweat
    
    Change-Id: I70d09aaecd14aa01b7a305e39a5d512cda3d3fd2

--- resources/jquery/jquery.byteLimit.js
+++ resources/jquery/jquery.byteLimit.js
@@ -39,7 +39,7 @@
 			// Default limit to current attribute value
 			// Can't re-use 'limit' variable because it's in the higher scope
 			// that affects the next each() iteration as well.
-			elLimit = limit === undefined ? $el.prop( 'maxLength' ) : limit;
+			elLimit = limit === undefined ? el.maxLength : limit;
 	
 			// If there is no (valid) limit passed or found in the property,
 			// skip this. The < 0 check is required for Firefox, which returns
@@ -55,9 +55,10 @@
 			// the value property through JavaScript, but Safari 4 violates that rule, so
 			// we have to remove or not set the property if we have a callback.
 			if ( fn === undefined ) {
-				$el.prop( 'maxLength', elLimit );
+				el.maxLength = elLimit;
 			} else {
-				$el.removeProp( 'maxLength' );
+				// removeProp does not work in FireFox - instead it would set it to 0
+				$el.removeAttr( 'maxlength' );
 			}
 	
 			// Save function for reference
@@ -77,13 +78,12 @@
 				// Backspace will be, so not big issue.
 	
 				if ( e.which === 0 || e.charCode === 0 || e.which === 8 ||
-					e.ctrlKey || e.altKey || e.metaKey )
-				{
+					e.ctrlKey || e.altKey || e.metaKey ) {
 					// A special key (backspace, etc) so don't interfere
 					return true;
 				}
 	
-				val = fn !== undefined ? fn( $( this ).val() ): $( this ).val();
+				val = fn !== undefined ? fn( this.value ) : this.value;
 				len = $.byteLength( val );
 				// Note that keypress returns a character code point, not a keycode.
 				// However, this may not be super reliable depending on how keys come in...
