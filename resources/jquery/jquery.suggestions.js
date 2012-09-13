7f31489158e1257b97c5d734e10250ecdd50ece3
Ebrahim Byagowi <ebrahim@gnu.org>
2012-09-13 19:16:09 +0200

    (bug 17808) (bug 21167) use real links for search suggestions
    Original patch by Ebrahim Byagowi <ebrahim@gnu.org>.
    
    The original patch was for MW 1.19 and needed some changes to work
    properly on current master.
    
    My (Matma Rex's) changes and comments:
    * we need to add a couple !importants to force correct colors on the links
      (otherwise global color - blue - overrides ours)
    * do not interfere with clicks with a mouse button other than left or with
      modifier keys pressed
    
    Change-Id: I87940ca86a2b3776969cbcee8cdf93e3c66b0cd9

--- resources/jquery/jquery.suggestions.js
+++ resources/jquery/jquery.suggestions.js
@@ -156,14 +156,25 @@ $.suggestions = {
 						for ( var i = 0; i < context.config.suggestions.length; i++ ) {
 							/*jshint loopfunc:true */
 							var text = context.config.suggestions[i];
-							var $result = $( '<div>' )
+							var form = context.config.$region.closest( 'form' );
+							var formAction = form.attr( 'action' );
+							var textbox = context.data.$textbox;
+							var originalValue = textbox.val();
+							// Simulating form state
+							textbox.val( text );
+							var href = formAction + '?' + form.serialize();
+							// Return input value to its original value
+							textbox.val( originalValue );
+
+							var $result = $( '<a>' )
 								.addClass( 'suggestions-result' )
 								.attr( 'rel', i )
+								.attr( 'href', href )
 								.data( 'text', context.config.suggestions[i] )
 								.mousemove( function ( e ) {
 									context.data.selectedWithMouse = true;
 									$.suggestions.highlight(
-										context, $(this).closest( '.suggestions-results div' ), false
+										context, $(this).closest( '.suggestions-results .suggestions-result' ), false
 									);
 								} )
 								.appendTo( $results );
@@ -234,14 +245,14 @@ $.suggestions = {
 						if ( context.data.$container.find( '.suggestions-special' ).html() !== '' ) {
 							result = context.data.$container.find( '.suggestions-special' );
 						} else {
-							result = context.data.$container.find( '.suggestions-results div:last' );
+							result = context.data.$container.find( '.suggestions-results .suggestions-result:last' );
 						}
 					}
 				}
 			} else if ( result === 'next' ) {
 				if ( selected.length === 0 ) {
 					// No item selected, go to the first one
-					result = context.data.$container.find( '.suggestions-results div:first' );
+					result = context.data.$container.find( '.suggestions-results .suggestions-result:first' );
 					if ( result.length === 0 && context.data.$container.find( '.suggestions-special' ).html() !== '' ) {
 						// No suggestion exists, go to the special one directly
 						result = context.data.$container.find( '.suggestions-special' );
@@ -431,13 +442,17 @@ $.fn.suggestions = function () {
 						// Can't use click() because the container div is hidden when the textbox loses focus. Instead,
 						// listen for a mousedown followed by a mouseup on the same div
 						.mousedown( function ( e ) {
-							context.data.mouseDownOn = $( e.target ).closest( '.suggestions-results div' );
+							context.data.mouseDownOn = $( e.target ).closest( '.suggestions-results .suggestions-result' );
 						} )
 						.mouseup( function ( e ) {
-							var $result = $( e.target ).closest( '.suggestions-results div' );
+							var $result = $( e.target ).closest( '.suggestions-results .suggestions-result' );
 							var $other = context.data.mouseDownOn;
 							context.data.mouseDownOn = $( [] );
 							if ( $result.get( 0 ) !== $other.get( 0 ) ) {
+								return;
+							}
+							// do not interfere with non-left clicks or if modifier keys are pressed (e.g. ctrl-click)
+							if ( e.which !== 1 || e.altKey || e.ctrlKey || e.shiftKey || e.metaKey ) {
 								return;
 							}
 							$.suggestions.highlight( context, $result, true );
@@ -449,7 +464,7 @@ $.fn.suggestions = function () {
 						} )
 				)
 				.append(
-					$( '<div>' ).addClass( 'suggestions-special' )
+					$( '<a>' ).addClass( 'suggestions-special' )
 						// Can't use click() because the container div is hidden when the textbox loses focus. Instead,
 						// listen for a mousedown followed by a mouseup on the same div
 						.mousedown( function ( e ) {
