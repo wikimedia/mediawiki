53e9d07e026acb418d3dd453fb249b9cd7ea5482
Timo Tijhof <ttijhof@wikimedia.org>
2012-09-14 04:09:47 +0200

    jquery.byteLimit: Rewrite (fix bug 38158, bug 38163)
    It used to be fairly simply and that seemed good enough. Listen
    on keypress, get current value, add character from key code in
    event object, and calculate the byteLength. If it is too long,
    preventDefault().
    
    However there were so many edge cases (too many of them to be
    considered edge cases) where this failed whereas the native
    maxLength handling was fine.
    
    For example:
     - Cut and paste
     - Selecting text and replacing or removing it (by any of the
       methods on this list)
     - Custom javascript input methods (e.g. Narayam)
     - Spelling suggestions and corrections
     - Drag and drop
     - etc.
    
    Now it listens only for keyup and change events. Both of which
    happen after the fact.
    
    I considered building in a timeout loop in addition to this so
    that when javascript gadgets change the input value it would
    detect it, however I'd like to hold that off for now. We can
    add that later if needed. For now scripts are expected to fire
    the 'change' event if they change it, which seems reasonable.
    
    The only challenge that introduces however is the need for
    basic history reconstruction. Figure out what was already there
    and what is new, and cut down the new part until the total sum
    satisfies the limit.
    
    This is the same behavior that WebKit browsers implement.
    e.g. of "barbaz" is pasted between "f" and "o" in "foo" with a
    maxLength of 5, then the resulting string will be "fbaoo".
    
    Also fixed bug 38158 while at it. Filed an upstream bug against
    webkit/Chrome but was wont fixed. Either way, we'd have to be
    compatible for a little while with old WebKit builds out there
    (iOS 3, Safari 5, etc.).
    http://code.google.com/p/chromium/issues/detail?id=136004
    
    Use removeAttribute('maxlength') to let the browser internally
    reset the maxLength property. Resetting it directly (to undefined
    or with `delete el.maxLength') does not work and casts it to 0,
    which blocks the input field entirely.
    
    Updated unit tests, and also removed the "type=text" code
    afterwards which that doesn't work in IE (Internet Explorer does
    not support dynamic changing of input types), needs to be in the
    HTML directly. So far it was passing in IE because 'text' is the
    default. Tests now use attr('maxlength') everywhere for
    consistency, though properties work the same. Browsers
    synchronize these things (just like the "class" attribute and
    "className" property).
    
    References:
    * Fixes bug 38158: Unexpected 0 maxLength.
    * Fixes bug 38163: Incorrect limiting of input when using input
      methods other than basic per-char typing.
    * Follows-up: 39cb0c19
    * Supersedes: I70d09aae
    
    Change-Id: I9c204243d0bdf7158aa86a62b591ce717a36fe27

--- resources/jquery/jquery.byteLimit.js
+++ resources/jquery/jquery.byteLimit.js
@@ -1,5 +1,5 @@
 /**
- * jQuery byteLimit plugin
+ * jQuery byteLimit plugin.
  *
  * @author Jan Paul Posma, 2011
  * @author Timo Tijhof, 2011-2012
@@ -7,19 +7,19 @@
 ( function ( $ ) {
 
 	/**
-	 * Enforces a byte limit to a textbox, so that UTF-8 entries are counted as well, when, for example,
-	 * a database field has a byte limit rather than a character limit.
-	 * Plugin rationale: Browser has native maxlength for number of characters, this plugin exists to
-	 * limit number of bytes instead.
+	 * Enforces a byte limit on an input field, so that UTF-8 entries are counted as well,
+	 * when, for example, a database field has a byte limit rather than a character limit.
+	 * Plugin rationale: Browser has native maxlength for number of characters, this plugin
+	 * exists to limit number of bytes instead.
 	 *
-	 * Can be called with a custom limit (to use that limit instead of the maxlength attribute value),
-	 * a filter function (in case the limit should apply to something other than the exact input value),
-	 * or both. Order of arguments is important!
+	 * Can be called with a custom limit (to use that limit instead of the maxlength attribute
+	 * value), a filter function (in case the limit should apply to something other than the
+	 * exact input value), or both. Order of parameters is important!
 	 *
 	 * @context {jQuery} Instance of jQuery for one or more input elements
-	 * @param limit {Number} [optional] Limit to enforce, fallsback to maxLength-attribute,
-	 * called with fetched value as argument.
-	 * @param fn {Function} [optional] Function to call on the input string before assessing the length
+	 * @param {Number} limit [optional] Limit to enforce, fallsback to maxLength-attribute,
+	 *  called with fetched value as argument.
+	 * @param {Function} fn [optional] Function to call on the string before assessing the length.
 	 * @return {jQuery} The context
 	 */
 	$.fn.byteLimit = function ( limit, fn ) {
@@ -28,19 +28,31 @@
 		if ( $.isFunction( limit ) ) {
 			fn = limit;
 			limit = undefined;
+		// Either way, verify it is a function so we don't have to call
+		// isFunction again after this.
+		} else if ( !fn || !$.isFunction( fn ) ) {
+			fn = undefined;
 		}
 
-		// The following is specific to each element in the collection
+		// The following is specific to each element in the collection.
 		return this.each( function ( i, el ) {
 			var $el, elLimit;
 
 			$el = $( el );
 
-			// Default limit to current attribute value
+			// If no limit was passed to byteLimit(), use the maxlength value.
 			// Can't re-use 'limit' variable because it's in the higher scope
-			// that affects the next each() iteration as well.
-			elLimit = limit === undefined ? $el.prop( 'maxLength' ) : limit;
-	
+			// that would affect the next each() iteration as well.
+			// Note that we use attribute to read the value instead of property,
+			// because in Chrome the maxLength property by default returns the
+			// highest supported value (no indiciation that it is being enforced
+			// by choice). We don't want to bind all of this for some redicilous
+			// high default number, unless it was explicitly set in the HTML.
+			// Also cast to a (primitive) number (most commonly because the maxlength
+			// attribute contains a string, but theoratically the limit parameter
+			// could be something else as well).
+			elLimit = Number( limit === undefined ? $el.attr( 'maxlength' ) : limit );
+
 			// If there is no (valid) limit passed or found in the property,
 			// skip this. The < 0 check is required for Firefox, which returns
 			// -1  (instead of undefined) for maxLength if it is not set.
@@ -48,51 +60,109 @@
 				return;
 			}
 
-			// Update/set attribute value, but only if there is no callback set.
-			// If there's a callback set, it's possible that the limit being enforced
-			// is too low (ie. if the callback would return "Foo" for "User:Foo").
-			// Usually this isn't a problem since browsers ignore maxLength when setting
-			// the value property through JavaScript, but Safari 4 violates that rule, so
-			// we have to remove or not set the property if we have a callback.
-			if ( fn === undefined ) {
-				$el.prop( 'maxLength', elLimit );
-			} else {
-				$el.removeProp( 'maxLength' );
+			if ( fn ) {
+				// Save function for reference
+				$el.data( 'byteLimit.callback', fn );
 			}
-	
-			// Save function for reference
-			$el.data( 'byteLimitCallback', fn );
-	
-			// We've got something, go for it:
-			$el.keypress( function ( e ) {
-				var val, len, charLen;
-				// First check to see if this is actually a character key
-				// being pressed.
-				// Based on key-event info from http://unixpapa.com/js/key.html
-				// jQuery should also normalize e.which to be consistent cross-browser,
-				// however the same check is still needed regardless of jQuery.
-	
-				// Note: At the moment, for some older opera versions (~< 10.5)
-				// some special keys won't be recognized (aka left arrow key).
-				// Backspace will be, so not big issue.
-	
-				if ( e.which === 0 || e.charCode === 0 || e.which === 8 ||
-					e.ctrlKey || e.altKey || e.metaKey )
-				{
-					// A special key (backspace, etc) so don't interfere
-					return true;
+
+			$el
+				// Disable the native maxLength (if there is any), because it interferes
+				// with the (differently calculated) byte limit.
+				// Aside from being differently calculated (average chars with byteLimit
+				// is lower), we also support a callback which can make it to allow longer
+				// values (e.g. count "Foo" from "User:Foo").
+				// maxLength is a strange property. Removing or setting the property to
+				// undefined directly doesn't work. Instead, it can only be unset internally
+				// by the browser when removing the associated attribute (Firefox/Chrome).
+				// http://code.google.com/p/chromium/issues/detail?id=136004
+				.removeAttr( 'maxlength' )
+
+				// Safe base value, used to determine the path between the previous state
+				// and the state that triggered the event handler below - and enforce the
+				// limit approppiately (e.g. don't chop from the end if text was inserted
+				// at the beginning of the string).
+				.data( 'byteLimit.value', '' )
+
+				// Remove old event handlers (if there are any)
+				.off( '.byteLimit' );
+
+			// Use keyup and change instead of keypress,
+			// See http://www.w3.org/TR/DOM-Level-3-Events/#events-keyboard-event-order for
+			// the order and characteristics of the key events.
+			// We need to listen to after the change has already happened because we've
+			// learned that trying to guess the new value and canceling the event
+			// accordingly doesn't work because the new value is not always as simple as:
+			// oldValue + String.fromCharCode( e.which ); because of cut, paste, select-drag
+			// replacements, and custom input methods and what not.
+			$el.on( 'keyup.byteLimit change.byteLimit', function ( e ) {
+				var inpVal, oldVal, newVal,
+					matchStart, matchEnd, matchLen, inpParts, insertedMax,
+					el = this;
+
+				inpVal = el.value;
+				oldVal = $.data( el, 'byteLimit.value' );
+
+				// Run the hook if one was provided, but only on the length
+				// assessment. The value itself is not to be affected by the hook.
+				if ( $.byteLength( fn ? fn( inpVal ) : inpVal ) <= elLimit ) {
+					// Limit was not reached, just remember the value
+					// and let the user continue.
+					$.data( el, 'byteLimit.value', inpVal );
+					return;
 				}
-	
-				val = fn !== undefined ? fn( $( this ).val() ): $( this ).val();
-				len = $.byteLength( val );
-				// Note that keypress returns a character code point, not a keycode.
-				// However, this may not be super reliable depending on how keys come in...
-				charLen = $.byteLength( String.fromCharCode( e.which ) );
-	
-				if ( ( len + charLen ) > elLimit ) {
-					e.preventDefault();
+
+				// Current input is longer than the active limit.
+				// Figure out what was added and limit the addition.
+				matchStart = 0;
+				matchEnd = 0;
+				// It is important that neither don't exceed either string.
+				// Imagine the new value adding content that matches the end of
+				// the old value (e.g. "foo" -> "foofoo"). matchStart would be 3.
+				// but without limiting both to the Math.min length, then matchEnd
+				// would also be 3.
+				matchLen = Math.min( inpVal.length, oldVal.length );
+
+				// Count same characters from the left, first.
+				// (if "foo" -> "foofoo", assume addition was at the end).
+				while (
+					matchStart < matchLen &&
+					oldVal[matchStart] === inpVal[matchStart]
+				) {
+					matchStart += 1;
 				}
-			});
-		});
+
+				while (
+					matchEnd <  ( matchLen - matchStart ) &&
+					oldVal[oldVal.length - 1 - matchEnd] === inpVal[inpVal.length - 1 - matchEnd]
+				) {
+					matchEnd += 1;
+				}
+
+				inpParts = [
+					// Same start
+					inpVal.substring( 0, matchStart ),
+					// Inserted content
+					inpVal.substring( matchStart, inpVal.length - matchEnd ),
+					// Same end
+					inpVal.substring( inpVal.length - matchEnd )
+				];
+
+				// Chop off last character until limit is statisfied
+				if ( fn ) {
+					while ( $.byteLength( fn( inpParts.join( '' ) ) ) > elLimit ) {
+						inpParts[1] = inpParts[1].slice( 0, -1 );
+					}
+				} else {
+					while ( $.byteLength( inpParts.join( '' ) ) > elLimit ) {
+						inpParts[1] = inpParts[1].slice( 0, -1 );
+					}
+				}
+
+				newVal = inpParts.join( '' );
+
+				el.value = newVal;
+				$.data( el, 'byteLimit.value', newVal );
+			} );
+		} );
 	};
 }( jQuery ) );
