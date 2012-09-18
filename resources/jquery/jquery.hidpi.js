e03c5c787c9e7cab46091aa2ef5eb77f62d00b72
Brion Vibber <brion@pobox.com>
2012-09-18 00:18:50 -0700

    Initial stab at responsive images for screen densities.
    * adds $wgResponsiveImages setting, defaulting to true, to enable the feature
    * adds jquery.hidpi plugin to check window.devicePixelRatio and replace images with data-src-1-5 or data-src-2-0 depending on the ratio
    * adds mediawiki.hidpi RL script to trigger hidpi loads after main images load
    * renders images from wiki image & thumb links at 1.5x and 2.0x and includes data-src-1-5 and data-src-2-0 attributes with the targets
    
    Note that this is a work in progress. There will be places where this doesn't yet work which output their imgs differently. If moving from a low to high-DPI screen on a MacBook Pro Retina display, you won't see images load until you reload.
    
    Confirmed basic images and thumbs in wikitext appear to work in Safari 6 on MacBook Pro Retina display.
    
    Patchset 2:
    * fix for images that specify width but not height
    * apply image replacements immediately on dom-ready to reduce double loading
    
    Change-Id: Id80ebd07a1a9f401a2c2bfeb21aae987e5aa863b

---
+++ resources/jquery/jquery.hidpi.js
@@ -0,0 +1,41 @@
+/**
+ * Responsive images based on data-src-* attributes.
+ *
+ * Recognizes data-src-1-5 and data-src-2-0 for 1.5 and 2.0 device pixel ratios.
+ * Call on a document or part of a document to replace image srcs in that section.
+ *
+ */
+( function ( $ ) {
+
+/**
+ * Implement responsive images based on data-src-* attributes.
+ *
+ * @method
+ * @returns {jQuery} This selection
+ */
+$.fn.hidpi = function ( options ) {
+	var $target = this;
+	
+	// @todo add support for dpi media query checks on Firefox, IE
+	var devicePixelRatio = window.devicePixelRatio || 1;
+	
+	if ( devicePixelRatio > 1 ) {
+		var srcKey;
+		if ( devicePixelRatio >= 2 ) {
+			srcKey = 'src-2-0';
+		} else {
+			srcKey = 'src-1-5';
+		}
+		$target.find( 'img' ).each( function() {
+			var $img = $( this ),
+				src = $img.data( srcKey );
+			if ( src ) {
+				$img.attr( 'src', src );
+			}
+		});
+	}
+
+	return $target;
+};
+
+}( jQuery ) );
