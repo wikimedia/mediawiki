4b5524d5d5d356f09f70be447cedc57d0d5bb8a9
Brion Vibber <brion@pobox.com>
2012-09-18 00:18:50 -0700

    Initial stab at responsive images for screen densities.
    * adds $wgResponsiveImages setting, defaulting to true, to enable the feature
    * adds 'srcset' attribute with 1.5x and 2x URLs to image links and image thumbs
    * adds jquery.hidpi plugin to check pixel density and implement partial 'srcset' polyfill
    ** $.devicePixelRatio() returns window.devicePixelRatio, with compat fallback for IE 9/10
    ** $().hidpi() performs a 'srcset' polyfill for browsers with no native 'srcset' support
    * adds mediawiki.hidpi RL script to trigger hidpi loads after main images load
    
    Note that this is a work in progress. There will be places where this doesn't yet work which output their imgs differently. If moving from a low to high-DPI screen on a MacBook Pro Retina display, you won't see images load until you reload.
    
    Confirmed basic images and thumbs in wikitext appear to work in Safari 6 and Chrome 21 on MacBook Pro Retina display, and IE 10 in Windows 8 at 150% zoom, 200% zoom, and 140% and 180%-ratio Metro tablet sizes.
    
    Change-Id: Id80ebd07a1a9f401a2c2bfeb21aae987e5aa863b

---
+++ resources/jquery/jquery.hidpi.js
@@ -0,0 +1,90 @@
+/**
+ * Responsive images based on data-src-* attributes.
+ *
+ * Recognizes data-src-1-5 and data-src-2-0 for 1.5 and 2.0 device pixel ratios.
+ * Call on a document or part of a document to replace image srcs in that section.
+ *
+ */
+( function ( $ ) {
+
+/**
+ * Detect reported or approximate device pixel ratio.
+ * 1.0 means 1 CSS pixel is 1 hardware pixel
+ * 2.0 means 1 CSS pixel is 2 hardware pixels
+ * etc
+ *
+ * Uses window.devicePixelRatio if available, or CSS media queries on IE.
+ *
+ * @method
+ * @returns {number} Device pixel ratio
+ */
+$.devicePixelRatio = function() {
+	if (typeof window.devicePixelRatio !== 'undefined') {
+		return window.devicePixelRatio;
+	} else if (typeof window.msMatchMedia !== 'undefined') {
+		// IE 9, 10 don't report pixel ratio directly, but we can get the
+		// screen DPI and divide by 96. We'll bracket to [1, 1.5, 2.0] for
+		// simplicity, but you may get different values depending on zoom
+		// factor, size of screen and orientation in Metro IE.
+		if (window.msMatchMedia('(min-resolution: 192dpi)').matches ) {
+			return 2;
+		} else if (window.msMatchMedia('(min-resolution: 144dpi)').matches ) {
+			return 1.5;
+		} else {
+			return 1;
+		}
+	} else {
+		// Assume 1 if unknown.
+		return 1;
+	}
+};
+
+/**
+ * Implement responsive images based on srcset attributes, if browser has no
+ * native srcset support.
+ *
+ * @method
+ * @returns {jQuery} This selection
+ */
+$.fn.hidpi = function() {
+	var $target = this,
+		// @todo add support for dpi media query checks on Firefox, IE
+		devicePixelRatio = $.devicePixelRatio(),
+		testImage = new Image();
+
+	if ( devicePixelRatio > 1 && typeof testImage.srcset === 'undefined' ) {
+		// No native srcset support.
+		$target.find( 'img' ).each( function() {
+			var $img = $( this ),
+				srcset = $img.attr( 'srcset' ),
+				candidates,
+				candidate,
+				bits,
+				src,
+				i,
+				ratioStr,
+				ratio;
+			if ( typeof srcset === 'string' && srcset !== '' ) {
+				candidates = srcset.split( / *, */ );
+				for ( i = 0; i < candidates.length; i++ ) {
+					candidate = candidates[i];
+					bits = candidate.split( / +/ );
+					src = bits[0];
+					if ( bits.length > 1 && bits[1].substr( -1, 1 ) === 'x' ) {
+						ratioStr = bits[1].substr( 0, bits[1].length - 1 );
+						ratio = parseFloat( ratioStr );
+						if ( ratio === devicePixelRatio ) {
+							// @fixme: check closest match rather than exact match
+							// current code assumes we'll see 1.5 or 2 exactly.
+							$img.attr( 'src', src );
+						}
+					}
+				}
+			}
+		});
+	}
+
+	return $target;
+};
+
+}( jQuery ) );
