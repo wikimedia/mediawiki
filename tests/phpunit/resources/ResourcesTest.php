f85cfd0dec00b6f10e9e6a934755948d9fad9b2d
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-09-21 17:05:52 +0000

    Add tests to check that files referenced by RL modules exist
    Change-Id: I9e148f6fd12f07a27b5e387aee585d1f74895ab8

---
+++ tests/phpunit/resources/ResourcesTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Sanity checks for making sure registered resources are sane.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @copyright Copyright © 2012, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+class ResourcesTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider fileProvider
+	 */
+	public function testFileExistence( $file, $comment ) {
+		$this->assertTrue( file_exists( $file ), $comment );
+	}
+
+	public function fileProvider() {
+		global $IP, $wgResourceModules;
+		$files = array();
+		$modules = require( "$IP/resources/Resources.php" );
+		$modules = array_merge( $modules, $wgResourceModules );
+
+		foreach ( $modules as $id => $module ) {
+			if ( isset( $module['localBasePath'] ) ) {
+				$prefix = $module['localBasePath'];
+			} else {
+				$prefix = $IP;
+			}
+
+			foreach ( array( 'styles', 'scripts', 'languageScripts' ) as $type ) {
+				if ( isset( $module[$type] ) ) {
+					foreach ( (array) $module[$type] as $key => $value ) {
+						if ( is_int( $key ) || $type === 'languageScripts' ) {
+							$file = $value;
+						} else {
+							$file = $key;
+						}
+						$files[] = array( "$prefix/$file", "File $prefix/$file of $type referenced by ResourceLoader module $id exists" );
+					}
+				}
+			}
+		}
+
+		$tempList = array();
+		if ( isset( $module['skinStyles'] ) ) {
+			$tempList = $module['skinStyles'];
+		}
+		foreach ( $tempList as $skin => $file ) {
+			$files[] = array( "$prefix/$file", "File $prefix/$file of $type referenced by ResourceLoader module $id exists" );
+		}
+
+		return $files;
+	}
+
+}
