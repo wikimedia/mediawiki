7899363603f5f42fb0615c6b60896598ffe4fcaf
Tim Landscheidt <tim@tim-landscheidt.de>
2012-09-22 23:50:52 +0000

    Do not flip partial keys in CSSJanus.
    Change-Id: I1ee9d16069c4627aa81024eeb642714ef796cec5

--- tests/phpunit/includes/libs/CSSJanusTest.php
+++ tests/phpunit/includes/libs/CSSJanusTest.php
@@ -458,6 +458,16 @@ class CSSJanusTest extends MediaWikiTestCase {
 				".foo\t{\tleft\t:\t0;}",
 				".foo\t{\tright\t:\t0;}"
 			),
+
+			// Guard against partial keys
+			array(
+				'.foo { leftxx: 0; }',
+				'.foo { leftxx: 0; }'
+			),
+			array(
+				'.foo { rightxx: 0; }',
+				'.foo { rightxx: 0; }'
+			),
 		);
 	}
 
@@ -534,16 +544,6 @@ class CSSJanusTest extends MediaWikiTestCase {
 	 */
 	function provideTransformBrokenCases() {
 		return array(
-			// Guard against partial keys
-			array(
-				'.foo { leftxx: 0; }',
-				'.foo { leftxx: 0; }'
-			),
-			array(
-				'.foo { rightxx: 0; }',
-				'.foo { rightxx: 0; }'
-			),
-
 			// Guard against selectors that look flippable
 			array(
 				# <foo-left-x attr="x">
