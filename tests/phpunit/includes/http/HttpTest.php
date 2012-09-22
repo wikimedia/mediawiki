4b4ea1ab7eb73fa456470fd4d6909970a9377230
Tim Landscheidt <tim@tim-landscheidt.de>
2012-09-22 01:35:05 +0000

    WIP: Use wfParseUrl() in Http::isValidURI() and fix tests
    Instead of building yet another regular expression, let's use
    wfParseUrl() and ensure that the scheme is either "http" or "https"
    and the hostname is not empty.
    
    Two tests are relaxed:
    
    - 'http://!"èèè¿¿¿~~\'' is now a valid URL.  My understanding is that
      Http::isValidURI() is used on user input, and with IDN, this may
      well be a legitimate hostname.
    
    - 'http://a ¿non !!sens after' [FIXME]
    
    Change-Id: I3b45c71e53c0c85768647b1d4c684c2eb12be229

--- tests/phpunit/includes/http/HttpTest.php
+++ tests/phpunit/includes/http/HttpTest.php
@@ -104,10 +104,8 @@ class HttpTest extends MediaWikiTestCase {
 			[ true, 'http://user:pass@host', 'Username and password provided' ],
 
 			# (\S+) - host part is made of anything not whitespaces
-			// commented these out in order to remove @group Broken
-			// @todo are these valid tests? if so, fix Http::isValidURI so it can handle them
-			// [ false, 'http://!"èèè¿¿¿~~\'', 'hostname is made of any non whitespace' ],
-			// [ false, 'http://exam:ple.org/', 'hostname can not use colons!' ],
+			[ true, 'http://!"èèè¿¿¿~~\'', 'hostname is made of any non whitespace' ],
+			[ false, 'http://exam:ple.org/', 'hostname can not use colons!' ],
 
 			# (:[0-9]+)? - port number
 			[ true, 'http://example.org:80/' ],
@@ -136,7 +134,7 @@ class HttpTest extends MediaWikiTestCase {
 			[ true, 'http://example.org/?id#anchor' ],
 			[ true, 'http://example.org/?#anchor' ],
 
-			[ false, 'http://a ¿non !!sens after', 'Allow anything after URI' ],
+			[ true, 'http://a ¿non !!sens after', 'Allow anything after URI' ],
 		];
 	}
 
