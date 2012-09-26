a253dac52a98c98e011c8c8a8112ac5c8ea0b475
Aaron Schulz <aschulz@wikimedia.org>
2012-09-26 14:48:33 -0700

    Removed READ_LATEST default from Revision::newFromTitle().
    Change-Id: I12bc0285147fff8d6f523a38950cf3f3cb51933f

--- includes/Revision.php
+++ includes/Revision.php
@@ -83,7 +83,7 @@ class Revision implements IDBAccessObject {
 	 * @param $flags Integer Bitfield (optional)
 	 * @return Revision or null
 	 */
-	public static function newFromTitle( $title, $id = 0, $flags = null ) {
+	public static function newFromTitle( $title, $id = 0, $flags = 0 ) {
 		$conds = array(
 			'page_namespace' => $title->getNamespace(),
 			'page_title' 	 => $title->getDBkey()
@@ -94,8 +94,6 @@ class Revision implements IDBAccessObject {
 		} else {
 			// Use a join to get the latest revision
 			$conds[] = 'rev_id=page_latest';
-			// Callers assume this will be up-to-date
-			$flags = is_int( $flags ) ? $flags : self::READ_LATEST; // b/c
 		}
 		return self::newFromConds( $conds, (int)$flags );
 	}
