d72f5f4eb0e33d79a3cdb8ff11da7594e1447238
Reedy <Reedy reedy@wikimedia.org>
2012-10-01 13:13:33 +0000

    Applied patches to new WMF 1.21wmf1 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -59,7 +59,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion = '1.21alpha';
+$wgVersion = '1.21wmf1';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
