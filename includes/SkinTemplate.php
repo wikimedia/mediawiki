a32f1573ef392fe3df3edd5e978b07f16ced7c4b
parent5446 <tylerromeo@gmail.com>
2012-10-03 15:00:25 -0400

    Re-implemented Special:Userlogin using FormSpecialPage.
    Added new SpecialUserlogin implementation using
    FormSpecialPage. The UI is slightly different due
    to HTMLForm's internal rendering, but all previous
    hooks are supported.
    
    Added function to QuickTemplate and made a FakeTemplate
    class to facilitate backwards-compatibility.
    Changed LoginForm (the old login class) to make sure
    that the account creation form still works.
    
    NOTE: This moves the account creation page specifically
    to Special:CreateAccount, which is no longer a redirect
    but the actual registration page.
    
    Change-Id: Ic736be0640312e178a50ee336b155c87d2f18671

--- includes/SkinTemplate.php
+++ includes/SkinTemplate.php
@@ -1352,6 +1352,13 @@ abstract class QuickTemplate {
 	}
 
 	/**
+	 * Get data from the private array.
+	 */
+	function getData( $str ) {
+		return $this->data[$str];
+	}
+
+	/**
 	 * @private
 	 * @return bool
 	 */
