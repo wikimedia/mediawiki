9e6dafa31a4068526a9243059c5d024c47023c18
Marius Hoch <hoo@online.de>
2012-10-03 03:33:55 +0200

    (bug 7851) Added AJAX patrolling (mediawiki.page.patrol.ajax)
    Introducing AJAX patrolling into the MediaWiki core with the new
    mediawiki.page.patrol.ajax module, which makes use of the API via
    mediawiki.api.
    
    During the patrol process a spinner (created by jquery.spinner) shows
    up and after it a suitable message gets shown via mediawiki.notify.
    Depending on whether we had success or not the link then turns up again
    or the brackets completly disappear just like on a normal page view.
    
    On top of adding the module, I've changed the following things:
    - Introduced $wgAjaxPatrol to disable this feature
    - Added the patrol token to the ResourceLoaderUserTokensModule
    - Added the two messages 'markedaspatrollednotify' and
      'markedaspatrollederrornotify'
    
    Change-Id: I472357566dda0ab572c20e2e4b87508b0f2f4c73

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6023,6 +6023,11 @@ $wgAjaxExportList = array();
 $wgAjaxWatch = true;
 
 /**
+ * Enable patrolling pages using AJAX.
+ */
+$wgAjaxPatrol = true;
+
+/**
  * Enable AJAX check for file overwrite, pre-upload
  */
 $wgAjaxUploadDestCheck = true;
