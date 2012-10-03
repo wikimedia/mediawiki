b9101258a50a55b3b032873205bf0150468bd40b
parent5446 <tylerromeo@gmail.com>
2012-10-03 15:00:25 -0400

    (DRAFT) Re-implemented Special:Userlogin using FormSpecialPage.
    Added new SpecialUserlogin implementation using
    FormSpecialPage. The UI is slightly different due
    to HTMLForm's internal rendering, but all previous
    hooks are supported.
    
    Change-Id: Ic736be0640312e178a50ee336b155c87d2f18671

--- includes/SkinTemplate.php
+++ includes/SkinTemplate.php
@@ -1337,6 +1337,13 @@ abstract class QuickTemplate {
 	}
 
 	/**
+	 * Get data from the private array.
+	 */
+	function getData( $str ) {
+		return $this->data[$str];
+	}
+
+	/**
 	 * @private
 	 * @return bool
 	 */
