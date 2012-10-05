503ce75e27dc003de19aec6f3455bfd22b8afcfb
daniel <daniel.kinzler@wikimedia.de>
2012-10-05 18:28:21 +0200

    Fix behavior of Hooks class.
    Until now, Hooks::run() would execute hooks registered via $wgHooks, but
    Hooks:isRegistered() would not consider them and Hooks::getHandlers() would
    not return them. That is inconsistent and misleading. This change aims to
    make the methods of the Hooks class behave consistently, and allows them
    to be used as a generic way of interacting with hooks.
    
    Change-Id: I39bd5de2bc8ccbe8df729446363960af9d29b0be

--- tests/phpunit/includes/HooksTest.php
+++ tests/phpunit/includes/HooksTest.php
@@ -75,27 +75,62 @@ class HooksTest extends MediaWikiTestCase {
 
 		$this->assertEquals( 'bah', $foo, 'Standard static method' );
 		$foo = 'Foo';
+
+		Hooks::clear( 'MediaWikiHooksTest001' );
+	}
+
+	public function testNewStyleHookInteraction() {
+		global $wgHooks;
+
+		$a = new NothingClass();
+		$b = new NothingClass();
+
+		// make sure to start with a clean slate
+		Hooks::clear( 'MediaWikiHooksTest001' );
+		unset( $wgHooks['MediaWikiHooksTest001'] );
+
+		$wgHooks['MediaWikiHooksTest001'][] = $a;
+		$this->assertTrue( Hooks::isRegistered( 'MediaWikiHooksTest001' ), 'Hook registered via $wgHooks should be noticed by Hooks::isRegistered' );
+
+		Hooks::register( 'MediaWikiHooksTest001', $b );
+		$this->assertEquals( 2, count( Hooks::getHandlers( 'MediaWikiHooksTest001' ) ), 'Hooks::getHandlers() should return hooks registered via wgHooks as well as Hooks::register' );
+
+		$foo = 'quux';
+		$bar = 'qaax';
+
+		Hooks::run( 'MediaWikiHooksTest001', array( &$foo, &$bar ) );
+		$this->assertEquals( 1, $a->calls, 'Hooks::run() should run hooks registered via wgHooks as well as Hooks::register' );
+		$this->assertEquals( 1, $b->calls, 'Hooks::run() should run hooks registered via wgHooks as well as Hooks::register' );
+
+		// clean up
+		Hooks::clear( 'MediaWikiHooksTest001' );
+		unset( $wgHooks['MediaWikiHooksTest001'] );
 	}
 }
 
 class NothingClass {
+	public $calls = 0;
+
 	static public function someStatic( &$foo, &$bar ) {
 		$foo = 'bah';
 		return true;
 	}
 
 	public function someNonStatic( &$foo, &$bar ) {
+		$this->calls++;
 		$foo = 'fOO';
 		$bar = 'bAR';
 		return true;
 	}
 
 	public function onMediaWikiHooksTest001( &$foo, &$bar ) {
+		$this->calls++;
 		$foo = 'foo';
 		return true;
 	}
 
 	public function someNonStaticWithData( $foo, &$bar ) {
+		$this->calls++;
 		$bar = $foo;
 		return true;
 	}
