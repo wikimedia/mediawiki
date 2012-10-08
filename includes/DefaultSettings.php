c9e225f860d5c80e9d45e6daf34430e509129ef1
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-10-08 21:00:29 +0200

    Reduce user preference clutter by removing 'justify'
    Change-Id: I9eb6ac9eda015fae6ff139f828192af63224d279

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3553,7 +3553,6 @@ $wgDefaultUserOptions = array(
 	'hideminor'               => 0,
 	'hidepatrolled'           => 0,
 	'imagesize'               => 2,
-	'justify'                 => 0,
 	'math'                    => 1,
 	'minordefault'            => 0,
 	'newpageshidepatrolled'   => 0,
