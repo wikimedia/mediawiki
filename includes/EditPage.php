a90af94f5e62084d1abd9db58c2d64ee7316052e
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-10-08 20:14:40 +0200

    Reduce user preference clutter by removing 'minordefault'
    Change-Id: I5139d8ec4c48eae29a28da9bdb083a20c770ea77

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -790,9 +790,6 @@ class EditPage {
 			# Already watched
 			$this->watchthis = true;
 		}
-		if ( $wgUser->getOption( 'minordefault' ) && !$this->isNew ) {
-			$this->minoredit = true;
-		}
 		if ( $this->textbox1 === false ) {
 			return false;
 		}
