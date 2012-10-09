8451240ac04c6d7001dc4f4ea3e575d49f7abe47
raymond <raimond.spekking@gmail.com>
2012-10-09 21:23:45 +0200

    Remove artefact from the ContentHandler merge
    Change-Id: I0a2d615c73efef8a7319d59e24991c403ce8e7cf

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1428,7 +1428,7 @@ If you save it, any changes made since this revision will be lost.",
 'yourdiff'                         => 'Differences',
 'copyrightwarning'                 => "Please note that all contributions to {{SITENAME}} are considered to be released under the $2 (see $1 for details).
 If you do not want your writing to be edited mercilessly and redistributed at will, then do not submit it here.<br />
-You are also promising us that you wrote this yourself, or copied editpageit from a public domain or similar free resource.
+You are also promising us that you wrote this yourself, or copied it from a public domain or similar free resource.
 '''Do not submit copyrighted work without permission!'''",
 'copyrightwarning2'                => "Please note that all contributions to {{SITENAME}} may be edited, altered, or removed by other contributors.
 If you do not want your writing to be edited mercilessly, then do not submit it here.<br />
