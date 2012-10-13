9888756942758af8ee260947dc9406e00307df36
Timo Tijhof <ttijhof@wikimedia.org>
2012-10-13 09:41:10 +0200

    ResourcesTest: Assert that all registered resources in RL exist.
    For now only tests ResourceLoaderFileModule.
    
    This will help print broken commits things like:
    * Ib40d09071ba315da6b17fc94cca5746ed4c26342
    * f47dfe9939f7929f1e3fd1c501539cbd0d12373f
    
    .. from landing in the repository.
    
    Change-Id: I9e148f6fd12f07a27b5e387aee585d1f74895ab8

---
+++ tests/phpunit/resources/ResourcesTest.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Sanity checks for making sure registered resources are sane.
+ *
+ * @file
+ * @author Niklas Laxström, 2012
+ * @author Antoine Musso, 2012
+ * @author Santhosh Thottingal, 2012
+ * @author Timo Tijhof, 2012
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+class ResourcesTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider provideResourceFiles
+	 */
+	public function testFileExistence( $filename, $module, $resource ) {
+		$this->assertFileExists( $filename,
+			"File '$resource' referenced by '$module' must exist."
+		);
+	}
+
+	/**
+	 * This ask the ResouceLoader for all registered files from modules
+	 * created by ResourceLoaderFileModule (or one of its descendants).
+	 *
+	 *
+	 * Since the raw data is stored in protected properties, we have to
+	 * overrride this through ReflectionObject methods.
+	 */
+	public static function provideResourceFiles() {
+		global $wgEnableJavaScriptTest;
+
+		// Test existance of test suite files as well
+		// (can't use setUp or setMwGlobals because providers are static)
+		$live_wgEnableJavaScriptTest = $wgEnableJavaScriptTest;
+		$wgEnableJavaScriptTest = true;
+
+		// Array with arguments for the test function
+		$cases = array();
+
+		// Initialize ResourceLoader
+		$rl = new ResourceLoader();
+
+		// See also ResourceLoaderFileModule::__construct
+		$filePathProps = array(
+			// Lists of file paths
+			'lists' => array(
+				'scripts',
+				'debugScripts',
+				'loaderScripts',
+				'styles',
+			),
+
+			// Collated lists of file paths
+			'nested-lists' => array(
+				'languageScripts',
+				'skinScripts',
+				'skinStyles',
+			),
+		);
+
+		foreach ( $rl->getModuleNames() as $moduleName ) {
+			$module = $rl->getModule( $moduleName );
+			if ( ! $module instanceof ResourceLoaderFileModule ) {
+				continue;
+			}
+
+			$reflectedModule = new ReflectionObject( $module );
+
+			$files = array();
+
+			foreach ( $filePathProps['lists'] as $propName ) {
+				$property = $reflectedModule->getProperty( $propName );
+				$property->setAccessible( true );
+				$list = $property->getValue( $module );
+				foreach ( $list as $key => $value ) {
+					// 'scripts' are numeral arrays.
+					// 'styles' can be numeral or associative.
+					// In case of associative the key is the file path
+					// and the value is the 'media' attribute.
+					if ( is_int( $key ) ) {
+						$files[] = $value;
+					} else {
+						$files[] = $key;
+					}
+				}
+			}
+
+			foreach ( $filePathProps['nested-lists'] as $propName ) {
+				$property = $reflectedModule->getProperty( $propName );
+				$property->setAccessible( true );
+				$lists = $property->getValue( $module );
+				foreach ( $lists as $group => $list ) {
+					foreach ( $list as $key => $value ) {
+						// We need the same filter as for 'lists',
+						// due to 'skinStyles'.
+						if ( is_int( $key ) ) {
+							$files[] = $value;
+						} else {
+							$files[] = $key;
+						}
+					}
+				}
+			}
+
+			// Get method for resolving the paths to full paths
+			$method = $reflectedModule->getMethod( 'getLocalPath' );
+			$method->setAccessible( true );
+
+			// Populate cases
+			foreach ( $files as $file ) {
+				$cases[] = array(
+					$method->invoke( $module, $file ),
+					$module->getName(),
+					$file,
+				);
+			}
+
+		}
+
+		// Restore settings
+		$wgEnableJavaScriptTest = $live_wgEnableJavaScriptTest;
+
+		return $cases;
+	}
+
+}
