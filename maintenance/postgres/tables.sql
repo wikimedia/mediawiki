82e14d2ca475fbcf02aee8416079d138756ed7da
aude <aude.wiki@gmail.com>
2012-10-14 19:20:55 +0000

    PostgreSQL schema update for new sites stuff
    See also the sites functionality patch:
    https://gerrit.wikimedia.org/r/#/c/23528/
    
    Change-Id: I870929e340679644bca7b6e37c08a773aa10ddf3

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -18,6 +18,7 @@ DROP SEQUENCE IF EXISTS recentchanges_rc_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS logging_log_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS job_job_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS category_cat_id_seq CASCADE;
+DROP SEQUENCE IF EXISTS sites_site_id_seq CASCADE;
 DROP FUNCTION IF EXISTS page_deleted() CASCADE;
 DROP FUNCTION IF EXISTS ts2_page_title() CASCADE;
 DROP FUNCTION IF EXISTS ts2_page_text() CASCADE;
@@ -284,7 +285,6 @@ CREATE TABLE ipblocks (
   ipb_block_email       SMALLINT     NOT NULL  DEFAULT 0,
   ipb_allow_usertalk    SMALLINT     NOT NULL  DEFAULT 0,
   ipb_parent_block_id             INTEGER          NULL  REFERENCES ipblocks(ipb_id) ON DELETE SET NULL DEFERRABLE INITIALLY DEFERRED
-
 );
 CREATE UNIQUE INDEX ipb_address_unique ON ipblocks (ipb_address,ipb_user,ipb_auto,ipb_anon_only);
 CREATE INDEX ipb_user    ON ipblocks (ipb_user);
@@ -695,3 +695,37 @@ CREATE TABLE config (
   cf_value  TEXT  NOT NULL
 );
 CREATE INDEX cf_name_value ON config (cf_name, cf_value);
+
+CREATE SEQUENCE sites_site_id_seq MINVALUE 0 START WITH 0;
+CREATE TABLE IF NOT EXISTS sites (
+  site_id           INTEGER     NOT NULL PRIMARY KEY DEFAULT nextval('sites_site_id_seq'),
+  site_global_key   TEXT        NOT NULL,
+  site_type         TEXT        NOT NULL,
+  site_group        TEXT        NOT NULL,
+  site_source       TEXT        NOT NULL,
+  site_language     TEXT        NOT NULL,
+  site_protocol     TEXT        NOT NULL,
+  site_domain       TEXT        NOT NULL,
+  site_data         TEXT        NOT NULL,
+  site_forward      SMALLINT    NOT NULL DEFAULT 0,
+  site_config       TEXT        NOT NULL
+);
+
+CREATE UNIQUE INDEX sites_global_key ON sites (site_global_key);
+CREATE INDEX sites_type ON sites (site_type);
+CREATE INDEX sites_group ON sites (site_group);
+CREATE INDEX sites_source ON sites (site_source);
+CREATE INDEX sites_language ON sites (site_language);
+CREATE INDEX sites_protocol ON sites (site_protocol);
+CREATE INDEX sites_domain ON sites (site_domain);
+CREATE INDEX sites_forward ON sites (site_forward);
+
+CREATE TABLE IF NOT EXISTS site_identifiers (
+  si_site   INTEGER  NOT NULL,
+  si_type   TEXT     NOT NULL,
+  si_key    TEXT     NOT NULL
+);
+
+CREATE UNIQUE INDEX site_ids_type ON site_identifiers (si_type, si_key);
+CREATE INDEX site_ids_site ON site_identifiers (si_site);
+CREATE INDEX site_ids_key ON site_identifiers (si_key);
