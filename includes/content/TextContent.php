eabfbfdf5a59396dd6ab6486be8155b9723fe4d1
daniel <daniel.kinzler@wikimedia.de>
2012-10-18 20:14:46 +0200

    (Bug 41169) Apply rtrim on on preSaveTransfrom.
    Wikitext should not have trailing whitespace.
    
    Change-Id: I75eb1c4bef7217ec2b7440594e3fc4b68dc3c022

--- includes/content/TextContent.php
+++ includes/content/TextContent.php
@@ -117,6 +117,24 @@ class TextContent extends AbstractContent {
 	}
 
 	/**
+	 * Returns a Content object with pre-save transformations applied.
+	 * This implementation just trims trailing whitespace.
+	 *
+	 * @param $title Title
+	 * @param $user User
+	 * @param $popts ParserOptions
+	 * @return Content
+	 */
+	public function preSaveTransform( Title $title, User $user, ParserOptions $popts ) {
+		global $wgParser;
+
+		$text = $this->getNativeData();
+		$pst = rtrim( $text );
+
+		return ( $text === $pst ) ? $this : new WikitextContent( $pst );
+	}
+
+	/**
 	 * Diff this content object with another content object..
 	 *
 	 * @since 1.21diff
