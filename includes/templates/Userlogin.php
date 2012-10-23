13d862d80e7c1a52f4575cba0bd076a115fa408e
Munaf Assaf <munaf.assaf@gmail.com>
2012-10-23 10:54:51 -0700

    Change userlogin to new appearance
    - Added new special module, mediawiki.special.userlogin
    - Added custom stylesheet to special module
    - Custom stylesheet contains Agora styling for buttons, might dupe
    - Added several messages to the messages list for translation
    - Still TBD on what to do with several items in original template
    - Moved some loginend text into contextual tips and links
    
    ---- Patch 2 retain existing logic --------
    
    Revert to earlier programmatic version of
    includes/templates/Userlogin.php so as not to lose special cases
    (loginlanguagelinks, domain select, extra fields, etc.)
    Move password reset logic and HTML up.
    Move the "Don't have an account? [Join Wikipedia]" call to action within
    the userloginForm div.
    
    (I had to squash in earlier reviews 30636 and 30635 otherwise gerrit complains.)
    
    Change-Id: I859edab4fc4fa9fe35fdef15fc429ae19a95305d

--- includes/templates/Userlogin.php
+++ includes/templates/Userlogin.php
@@ -46,66 +46,63 @@ class UserloginTemplate extends QuickTemplate {
 <div id="userloginForm">
 <form name="userlogin" method="post" action="<?php $this->text('action') ?>">
 	<h2><?php $this->msg('login') ?></h2>
-	<p id="userloginlink"><?php $this->html('link') ?></p>
 	<?php $this->html('header'); /* pre-table point for form plugins... */ ?>
 	<div id="userloginprompt"><?php  $this->msgWiki('loginprompt') ?></div>
 	<?php if( $this->haveData( 'languages' ) ) { ?><div id="languagelinks"><p><?php $this->html( 'languages' ); ?></p></div><?php } ?>
-	<table>
-		<tr>
-			<td class="mw-label"><label for='wpName1'><?php $this->msg('yourname') ?></label></td>
-			<td class="mw-input">
-				<?php
+	<ul class="mw-form-list">
+		<li>
+			<label for='wpName1' class="pos-above"><?php $this->msg('yourname') ?>
+				<a href="#" class="pull-right">
+					<img src="resources/mediawiki.special/images/icon-lock.png"/>&nbsp;
+					<?php $this->msg( 'signwithsecure' ) ?>
+				</a>
+			</label>
+			<?php
 			echo Html::input( 'wpName', $this->data['name'], 'text', array(
 				'class' => 'loginText',
 				'id' => 'wpName1',
 				'tabindex' => '1',
 				'size' => '20',
-				'required'
+				'required',
+				'placeholder' => wfMessage( 'yournamehelp' )	// XXX here and elsewhere should I use wfMessage()->text() ?
 				# Can't do + array( 'autofocus' ) because + for arrays in PHP
 				# only works right for associative arrays!  Thanks, PHP.
 			) + ( $this->data['name'] ? array() : array( 'autofocus' => '' ) ) ); ?>
 
-			</td>
-		</tr>
-		<tr>
-			<td class="mw-label"><label for='wpPassword1'><?php $this->msg('yourpassword') ?></label></td>
-			<td class="mw-input">
-				<?php
+		</li>
+		<li>
+			<label for='wpPassword1' class="pos-above"><?php $this->msg('yourpassword') ?></label>
+			<?php
 			echo Html::input( 'wpPassword', null, 'password', array(
 				'class' => 'loginPassword',
 				'id' => 'wpPassword1',
 				'tabindex' => '2',
-				'size' => '20'
+				'size' => '20',
+				'placeholder' => wfMessage( 'yourpasswordhelp' )
 			) + ( $this->data['name'] ? array( 'autofocus' ) : array() ) ); ?>
 
-			</td>
-		</tr>
+		</li>
 	<?php if( isset( $this->data['usedomain'] ) && $this->data['usedomain'] ) {
 		$doms = "";
 		foreach( $this->data['domainnames'] as $dom ) {
 			$doms .= "<option>" . htmlspecialchars( $dom ) . "</option>";
 		}
 	?>
-		<tr id="mw-user-domain-section">
-			<td class="mw-label"><?php $this->msg( 'yourdomainname' ) ?></td>
-			<td class="mw-input">
+		<li id="mw-user-domain-section">
+			<label for='wpDomain' class="pos-above"><?php $this->msg( 'yourdomain' ) ?></label>
 				<select name="wpDomain" value="<?php $this->text( 'domain' ) ?>"
 					tabindex="3">
 					<?php echo $doms ?>
 				</select>
-			</td>
-		</tr>
+		</li>
 	<?php }
 
 	if( $this->haveData( 'extrafields' ) ) {
 		echo $this->data['extrafields'];
-	}
+	} ?>
 
-	if( $this->data['canremember'] ) { ?>
-		<tr>
-			<td></td>
-			<td class="mw-input">
-				<?php
+		<li class="pull-up">
+			<?php if( $this->data['canremember'] ) {
 				global $wgCookieExpiration;
 				$expirationDays = ceil( $wgCookieExpiration / ( 3600 * 24 ) );
 				echo Xml::checkLabel(
@@ -114,35 +111,10 @@ class UserloginTemplate extends QuickTemplate {
 					'wpRemember',
 					$this->data['remember'],
 					array( 'tabindex' => '8' )
-				)
-				?>
-			</td>
-		</tr>
-<?php } ?>
-<?php if( $this->data['cansecurelogin'] ) { ?>
-		<tr>
-			<td></td>
-			<td class="mw-input">
-			<?php
-			echo Xml::checkLabel(
-				wfMessage( 'securelogin-stick-https' )->text(),
-				'wpStickHTTPS',
-				'wpStickHTTPS',
-				$this->data['stickHTTPS'],
-				array( 'tabindex' => '9' )
-			);
-		?>
-			</td>
-		</tr>
-<?php } ?>
-		<tr>
-			<td></td>
-			<td class="mw-submit">
-				<?php
-		echo Html::input( 'wpLoginAttempt', wfMessage( 'login' )->text(), 'submit', array(
-			'id' => 'wpLoginAttempt',
-			'tabindex' => '9'
-		) );
+				);
+			} ?>
+			<label class='pull-right'>
+		<?php
 		if ( $this->data['useemail'] && $this->data['canreset'] ) {
 			if( $this->data['resetlink'] === true ){
 				echo '&#160;';
@@ -162,13 +134,43 @@ class UserloginTemplate extends QuickTemplate {
 				);
 			}
 		} ?>
+			</label>
 
+		</li>
+<?php if( $this->data['cansecurelogin'] ) { ?>
+		<li>
+			<?php
+			echo Xml::checkLabel(
+				wfMessage( 'securelogin-stick-https' )->text(),
+				'wpStickHTTPS',
+				'wpStickHTTPS',
+				$this->data['stickHTTPS'],
+				array( 'tabindex' => '9' )
+			);
+			?>
+		</li>
+<?php } ?>
+		<li>
+			<?php
+		echo Html::input( 'wpLoginAttempt', wfMessage( 'login' )->text(), 'submit', array(
+			'id' => 'wpLoginAttempt',
+			'tabindex' => '9',
+			'class' => 'mw-btn blue xl'
+		) );
+		?>
 			</td>
 		</tr>
 	</table>
 <?php if( $this->haveData( 'uselang' ) ) { ?><input type="hidden" name="uselang" value="<?php $this->text( 'uselang' ); ?>" /><?php } ?>
 <?php if( $this->haveData( 'token' ) ) { ?><input type="hidden" name="wpLoginToken" value="<?php $this->text( 'token' ); ?>" /><?php } ?>
 </form>
+<div id="createaccount-cta">
+    <h3><?php $this->msg( 'donthaveaccount' ) ?><button class="mw-btn green"><?php $this->msg( 'joinwikipedia' ) ?></button></h3> 
+</div>
+<p>
+	^BAD Can't hardcode this button, wiki may not allow creating account!<br>
+	old dynamic userloginlink link text: <p id="userloginlink"><?php $this->html('link') ?>
+</p>
 </div>
 <div id="loginend"><?php $this->html( 'loginend' ); ?></div>
 <?php
