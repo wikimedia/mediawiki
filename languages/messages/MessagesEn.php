d5e92939ff8726b0e53f2259a02b33b156a6b595
MatmaRex <matma.rex@gmail.com>
2012-10-27 20:34:15 +0200

    add sidebar-heading message
    Change-Id: I9a2ebd503ec9dd901802a9b3b1f5137a85990373

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -569,6 +569,7 @@ $preloadedMessages = array(
 	'searcharticle',
 	'searchbutton',
 	'sidebar',
+	'sidebar-heading',
 	'site-atom-feed',
 	'sitenotice',
 	'specialpages',
@@ -807,6 +808,7 @@ XHTML id names.
 'namespaces'                     => 'Namespaces',
 'variants'                       => 'Variants',
 
+'sidebar-heading'   => 'Navigation menu',
 'errorpagetitle'    => 'Error',
 'returnto'          => 'Return to $1.',
 'tagline'           => 'From {{SITENAME}}',
