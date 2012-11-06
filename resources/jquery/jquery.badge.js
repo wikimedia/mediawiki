4451dd15285edbf7e24f65f7f42ad85e84f5e629
Kaldari <rkaldari@wikimedia.org>
2012-11-06 11:46:22 -0800

    (bug 39383, bug 40059) Allowing badges to use strings and returning this
    Change-Id: Ic79c6116dfb3c7248eae4ad66092bd1edb8ef0a2

--- resources/jquery/jquery.badge.js
+++ resources/jquery/jquery.badge.js
@@ -1,8 +1,6 @@
 /**
  * jQuery Badge plugin
  *
- * Based on Badger plugin by Daniel Raftery (http://thrivingkings.com/badger).
- *
  * @license MIT
  */
 
@@ -29,89 +27,85 @@
 	 * Allows you to put a numeric "badge" on an item on the page.
 	 * See mediawiki.org/wiki/ResourceLoader/Default_modules#jQuery.badge
 	 *
-	 * @param {string|number} badgeCount An explicit number, or "+n"/ "-n"
-	 *  to modify the existing value. If the new value is equal or lower than 0,
-	 *  any existing badge will be removed. The badge container will be appended
-	 *  to the selected element(s).
-	 * @param {Object} options Optional parameters specified below
+	 * @param {string|number} badgeValue The value to display in the badge. If
+	 *  the new value is numeric and equal to or lower than 0, any existing
+	 *  badge will be removed. The badge container will be appended to the
+	 *  selected element(s).
+	 * @param {Object} options Optional parameters specified below:
 	 *   type: 'inline' or 'overlay' (default)
-	 *   callback: will be called with the number now shown on the badge as a parameter
+	 *   callback: will be called with the value now shown on the badge as a parameter
 	 */
-	$.fn.badge = function ( badgeCount, options ) {
+	$.fn.badge = function ( badgeValue, options ) {
 		var $badge,
-			oldBadgeCount,
-			newBadgeCount,
+			newBadgeValue,
+			newBadgeValueIsNumeric = true,
 			$existingBadge = this.find( '.mw-badge' );
 
+		// Set overlay as default badge type.
 		options = $.extend( { type : 'overlay' }, options );
 
-		// If there is no existing badge, this will give an empty string
-		oldBadgeCount = Number( $existingBadge.text() );
-		if ( isNaN( oldBadgeCount ) ) {
-			oldBadgeCount = 0;
-		}
-
-		// If badgeCount is a number, use that as the new badge
-		if ( typeof badgeCount === 'number' ) {
-			newBadgeCount = badgeCount;
-		} else if ( typeof badgeCount === 'string' ) {
-			// If badgeCount is "+x", add x to the old badge
-			if ( badgeCount.charAt(0) === '+' ) {
-				newBadgeCount = oldBadgeCount + Number( badgeCount.substr(1) );
-			// If badgeCount is "-x", subtract x from the old badge
-			} else if ( badgeCount.charAt(0) === '-' ) {
-				newBadgeCount = oldBadgeCount - Number( badgeCount.substr(1) );
-			// If badgeCount can be converted into a number, convert it
-			} else if ( !isNaN( Number( badgeCount ) ) ) {
-				newBadgeCount = Number( badgeCount );
+		// If badgeValue is a number, use that as the new badge value.
+		if ( typeof badgeValue === 'number' && badgeValue !== NaN ) {
+			newBadgeValue = badgeValue;
+		} else if ( typeof badgeValue === 'string' ) {
+			// If badgeValue can be converted into a number, convert it.
+			// Empty string will be converted to 0.
+			} else if ( !isNaN( badgeValue ) ) {
+				newBadgeValue = Number( badgeValue );
+			// Otherwise, just use the string as the new badge value.
 			} else {
-				newBadgeCount = 0;
+				newBadgeValue = badgeValue;
+				newBadgeValueIsNumeric = false;
 			}
-		// Other types are not supported, fall back to 0.
 		} else {
-			newBadgeCount = 0;
+			// badgeValue is neither a number nor a string
+			return this;
 		}
 
-		// Badge count must be a whole number
-		newBadgeCount = Math.round( newBadgeCount );
+		if ( newBadgeValueIsNumeric ) {
+			// Numeric badge values must be whole numbers.
+			newBadgeValue = Math.round( newBadgeValue );
+			if ( newBadgeValue <= 0 ) {
+				// Badges should only exist for values > 0.
+				$existingBadge.remove();
+				return this;
+			}
+		}
 
-		if ( newBadgeCount <= 0 ) {
-			// Badges should only exist for values > 0.
-			$existingBadge.remove();
+		// Don't add duplicate badges.
+		if ( $existingBadge.length ) {
+			$badge = $existingBadge;
+			// Insert the new value into the badge.
+			this.find( '.mw-badge-content' ).text( newBadgeValue );
 		} else {
-			// Don't add duplicates
-			if ( $existingBadge.length ) {
-				$badge = $existingBadge;
-				// Insert the new count into the badge
-				this.find( '.mw-badge-content' ).text( newBadgeCount );
-			} else {
-				// Contruct a new badge with the count
-				$badge = $( '<div>' )
-					.addClass( 'mw-badge' )
-					.append(
-						$( '<span>' )
-							.addClass( 'mw-badge-content' )
-							.text( newBadgeCount )
-					);
-				this.append( $badge );
-			}
-
-			if ( options.type === 'inline' ) {
-				$badge
-					.removeClass( 'mw-badge-overlay' )
-					.addClass( 'mw-badge-inline' );
-			// Default: overlay
-			} else {
-				$badge
-					.removeClass( 'mw-badge-inline' )
-					.addClass( 'mw-badge-overlay' );
-
-			}
-
-			// If a callback was specified, call it with the badge count
-			if ( $.isFunction( options.callback ) ) {
-				options.callback( newBadgeCount );
-			}
+			// Construct a new badge with the value.
+			$badge = $( '<div>' )
+				.addClass( 'mw-badge' )
+				.append(
+					$( '<span>' )
+						.addClass( 'mw-badge-content' )
+						.text( newBadgeValue )
+				);
+			this.append( $badge );
 		}
+
+		// Style the badge according to the type
+		if ( options.type === 'inline' ) {
+			$badge
+				.removeClass( 'mw-badge-overlay' )
+				.addClass( 'mw-badge-inline' );
+		// Default: overlay
+		} else {
+			$badge
+				.removeClass( 'mw-badge-inline' )
+				.addClass( 'mw-badge-overlay' );
+
+		}
+
+		// If a callback was specified, call it with the badge value.
+		if ( $.isFunction( options.callback ) ) {
+			options.callback( newBadgeValue );
+		}
+		return this;
 	};
 }( jQuery ) );
