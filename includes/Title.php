ba4d0e529cd15eaa4cf61e30d444a34ec65dfe27
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-11-09 17:52:14 +0400

    Give permission hooks a chance to override deny permissions.
    An example application of this could be to allow modifications
    of some messages in MediaWiki namespace for some users.
    Previous implementation does not allow this.
    
    The patch extends standard MW hook logic to permission hooks:
    they are checked first, and if some hook returns 'false' which
    means 'stop hook processing', next permission checks are not
    performed.
    
    Also &$errors and $short parameters are added to getUserPermissionsErrors,
    so hooks can append errors directly to $errors and decide whether
    to run checks or not based or previous $errors and $short values.
    
    Change-Id: I62bb977fe9988212bd5e3daffbe52b23d83af7b5

--- includes/Title.php
+++ includes/Title.php
@@ -1804,25 +1804,37 @@ class Title {
 	 * @param $doExpensiveQueries Boolean whether or not to perform expensive queries
 	 * @param $short Boolean short circuit on first error
 	 *
-	 * @return Array list of errors
+	 * @return boolean Whether to continue permission hook processing
 	 */
-	private function checkPermissionHooks( $action, $user, $errors, $doExpensiveQueries, $short ) {
+	private function checkPermissionHooks( $action, $user, &$errors, $doExpensiveQueries, $short ) {
 		// Use getUserPermissionsErrors instead
-		$result = '';
+		$result = true;
 		if ( !wfRunHooks( 'userCan', array( &$this, &$user, $action, &$result ) ) ) {
-			return $result ? array() : array( array( 'badaccess-group0' ) );
+			if ( !$result ) {
+				$errors[] = array( 'badaccess-group0' );
+			}
+			return false;
 		}
 		// Check getUserPermissionsErrors hook
-		if ( !wfRunHooks( 'getUserPermissionsErrors', array( &$this, &$user, $action, &$result ) ) ) {
-			$errors = $this->resultToError( $errors, $result );
+		$result = true;
+		if ( !wfRunHooks( 'getUserPermissionsErrors', array( &$this, &$user, $action, &$result, &$errors, $short ) ) ) {
+			if ( $result !== true ) {
+				// Backwards compatibility - old hooks return error in $result
+				$errors = $this->resultToError( $errors, $result );
+			}
+			return false;
 		}
 		// Check getUserPermissionsErrorsExpensive hook
-		if ( $doExpensiveQueries && !( $short && count( $errors ) > 0 ) &&
-			 !wfRunHooks( 'getUserPermissionsErrorsExpensive', array( &$this, &$user, $action, &$result ) ) ) {
-			$errors = $this->resultToError( $errors, $result );
+		$result = true;
+		if ( $doExpensiveQueries &&
+			 !wfRunHooks( 'getUserPermissionsErrorsExpensive', array( &$this, &$user, $action, &$result, &$errors, $short ) ) ) {
+			if ( $result !== true ) {
+				// Backwards compatibility - old hooks return error in $result
+				$errors = $this->resultToError( $errors, $result );
+			}
+			return false;
 		}
-
-		return $errors;
+		return true;
 	}
 
 	/**
@@ -2212,13 +2224,11 @@ class Title {
 		# Read has special handling
 		if ( $action == 'read' ) {
 			$checks = array(
-				'checkPermissionHooks',
 				'checkReadPermissions',
 			);
 		} else {
 			$checks = array(
 				'checkQuickPermissions',
-				'checkPermissionHooks',
 				'checkSpecialsAndNSPermissions',
 				'checkCSSandJSPermissions',
 				'checkPageRestrictions',
@@ -2226,6 +2236,11 @@ class Title {
 				'checkActionPermissions',
 				'checkUserBlock'
 			);
+		}
+
+		if ( !$this->checkPermissionHooks( $action, $user, $errors, $doExpensiveQueries, $short ) ) {
+			// Give extensions a chance to override anything
+			return $errors;
 		}
 
 		$errors = array();
