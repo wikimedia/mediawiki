7cb5b3aa4f238348f90ee0b0d13353ae48e53f3a
MatmaRex <matma.rex@gmail.com>
2012-11-09 17:47:29 +0100

    (bug 41886) make $.tablesorter support headers with colspan
    Previously, if a header had colspan set, all headers after it would sort
    wrong columns (shifted by the cumulating colspans). This fixes the issue,
    from now on every header sorts on all columns it spans over (using the
    multi-column sorting funcitonality already present in $.tablesorter).
    
    Change-Id: I89766c9640b83ca38f41d698d1ef052a9e3f68d3

--- resources/jquery/jquery.tablesorter.js
+++ resources/jquery/jquery.tablesorter.js
@@ -324,20 +324,14 @@
 		return false;
 	}
 
-	function setHeadersCss( table, $headers, list, css, msg ) {
+	function setHeadersCss( table, $headers, list, css, msg, columnToHeader ) {
 		// Remove all header information and reset titles to default message
 		$headers.removeClass( css[0] ).removeClass( css[1] ).attr( 'title', msg[1] );
 
-		var h = [];
-		$headers.each( function ( offset ) {
-			if ( !this.sortDisabled ) {
-				h[this.column] = $( this );
-			}
-		} );
-
-		var l = list.length;
-		for ( var i = 0; i < l; i++ ) {
-			h[ list[i][0] ].addClass( css[ list[i][1] ] ).attr( 'title', msg[ list[i][1] ] );
+		for ( var i = 0; i < list.length; i++ ) {
+			$headers.eq( columnToHeader[ list[i][0] ] )
+				.addClass( css[ list[i][1] ] )
+				.attr( 'title', msg[ list[i][1] ] );
 		}
 	}
 
@@ -544,6 +538,7 @@
 				return $tables.each( function ( i, table ) {
 					// Declare and cache.
 					var $document, $headers, cache, config, sortOrder,
+						headerToColumns, columnToHeader, colspanOffset,
 						$table = $( table ),
 						shiftDown = 0;
 
@@ -610,6 +605,22 @@
 					// initially build the cache for the tbody cells (to be able to sort initially)
 					cache = buildCache( table );
 
+					// as each header can span over multiple columns (using colspan=N),
+					// we have to bidirectionally map headers to their columns and columns to their headers
+					headerToColumns = [];
+					columnToHeader = [];
+					colspanOffset = 0;
+					$headers.each( function ( headerIndex ) {
+						var columns = [];
+						for ( var i = 0; i < this.colSpan; i++ ) {
+							columnToHeader[ colspanOffset + i ] = headerIndex;
+							columns.push( colspanOffset + i )
+						}
+
+						headerToColumns[ headerIndex ] = columns;
+						colspanOffset += this.colSpan;
+					} );
+
 					// Apply event handling to headers
 					// this is too big, perhaps break it out?
 					$headers.filter( ':not(.unsortable)' ).click( function ( e ) {
@@ -631,44 +642,49 @@
 						if ( !table.sortDisabled && totalRows > 0 ) {
 
 							// Cache jQuery object
-							var $cell = $( this );
-
-							// Get current column index
-							var i = this.column;
+							var $cell = $( this ), cell = this;
 
 							// Get current column sort order
 							this.order = this.count % 2;
 							this.count++;
 
-							// User only wants to sort on one column
-							if ( !e[config.sortMultiSortKey] ) {
-								// Flush the sort list
-								config.sortList = [];
-								// Add column to sort list
-								config.sortList.push( [i, this.order] );
+							// Get current column index
+							var columns = headerToColumns[this.column];
+							var newSortList = $.map( columns, function (c) {
+								// jQuery "helpfully" flattens the arrays...
+								return [[c, cell.order]];
+							});
+							// Index of first column belonging to this header
+							var i = columns[0];
 
-							// Multi column sorting
+							if ( !e[config.sortMultiSortKey] ) {
+								// User only wants to sort on one column set
+								// Flush the sort list and add new columns
+								config.sortList = newSortList;
 							} else {
-								// The user has clicked on an already sorted column.
+								// Multi column sorting
+								// It is not possible for one column to belong to multiple headers,
+								// so this is okay - we don't need to check for every value in the columns array
 								if ( isValueInArray( i, config.sortList ) ) {
+									// The user has clicked on an already sorted column.
 									// Reverse the sorting direction for all tables.
 									for ( var j = 0; j < config.sortList.length; j++ ) {
 										var s = config.sortList[j],
 											o = config.headerList[s[0]];
-										if ( s[0] === i ) {
+										if ( isValueInArray( s[0], newSortList ) ) {
 											o.count = s[1];
 											o.count++;
 											s[1] = o.count % 2;
 										}
 									}
 								} else {
-									// Add column to sort list array
-									config.sortList.push( [i, this.order] );
+									// Add columns to sort list array
+									config.sortList = config.sortList.concat( newSortList );
 								}
 							}
 
 							// Set CSS for headers
-							setHeadersCss( $table[0], $headers, config.sortList, sortCSS, sortMsg );
+							setHeadersCss( $table[0], $headers, config.sortList, sortCSS, sortMsg, columnToHeader );
 							appendToTable(
 								$table[0], multisort( $table[0], config.sortList, cache )
 							);
@@ -707,7 +723,7 @@
 						cache = buildCache( table );
 
 						// set css for headers
-						setHeadersCss( table, $headers, sortList, sortCSS, sortMsg );
+						setHeadersCss( table, $headers, sortList, sortCSS, sortMsg, columnToHeader );
 
 						// sort the table and append it to the dom
 						appendToTable( table, multisort( table, sortList, cache ) );
