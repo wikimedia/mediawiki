8d198333d30f8878eed73793e616b042b71f4682
Brad Jorsch <bjorsch@wikimedia.org>
2012-11-09 16:54:57 -0800

    Add EditPage hooks AlternateEditPreview, EditPage::showStandardInputs:options
    Adds two new hooks:
    * AlternateEditPreview allows an extension to override the standard
      page preview display.
    * EditPage::showStandardInputs:options allows an extension to add
      additional HTML to the end of the editOptions area of the edit form.
    
    Change-Id: Ic5d35c8e9ff71282b5ebccc87c64894a385e5836

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2780,7 +2780,9 @@ HTML
 			wfMessage( 'newwindow' )->parse();
 		$wgOut->addHTML( "	<span class='cancelLink'>{$cancel}</span>\n" );
 		$wgOut->addHTML( "	<span class='editHelp'>{$edithelp}</span>\n" );
-		$wgOut->addHTML( "</div><!-- editButtons -->\n</div><!-- editOptions -->\n" );
+		$wgOut->addHTML( "</div><!-- editButtons -->\n" );
+		wfRunHooks( 'EditPage::showStandardInputs:options', array( $this, $wgOut, &$tabindex ) );
+		$wgOut->addHTML( "</div><!-- editOptions -->\n" );
 	}
 
 	/**
@@ -2929,6 +2931,12 @@ HTML
 
 		try {
 			$content = $this->toEditContent( $this->textbox1 );
+
+			$previewHTML = '';
+			if ( !wfRunHooks( 'AlternateEditPreview', array( $this, &$content, &$previewHTML, &$this->mParserOutput ) ) ) {
+				wfProfileOut( __METHOD__ );
+				return $previewHTML;
+			}
 
 			if ( $this->mTriedSave && !$this->mTokenOk ) {
 				if ( $this->mTokenOkExceptSuffix ) {
