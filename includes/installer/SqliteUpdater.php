0d9d76c3e6535c8ab3f046720d236948d38e0509
Jan Gerber <jgerber@wikimedia.org>
2012-11-22 19:12:42 +0000

    add index to image table to query by type
    this allows queries for all ogg video
    or all ogg audio files
    
    Change-Id: I85715b0553a771ab88d9763ba921923551988bd0

--- includes/installer/SqliteUpdater.php
+++ includes/installer/SqliteUpdater.php
@@ -107,6 +107,7 @@ class SqliteUpdater extends DatabaseUpdater {
 			array( 'addField', 'job',           'job_token',         'patch-job_token.sql' ),
 			array( 'addField', 'job',           'job_attempts',      'patch-job_attempts.sql' ),
 			array( 'doEnableProfiling' ),
+			array( 'addIndex', 'image', 'img_media_type', 'patch-image-img-media-type-index.sql' ),
 		);
 	}
 
