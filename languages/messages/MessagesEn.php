f2ef4325c931fcd47c7816333f70daf5e8018ff0
Tyler Anthony Romeo <tylerromeo@gmail.com>
2012-11-21 19:11:31 -0500

    Added throttling framework.
    Created new Throttle class that manages a generic
    throttling framework. Also added functions to ApiBase
    and HTMLForm to allow the use of Throttle objects
    in form actions and API actions.
    
    Change-Id: I00730e26bf4cffedc2bd43ce0ac73a9fcfcd3d9d

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1056,6 +1056,7 @@ Query: $2',
 'actionthrottled'               => 'Action throttled',
 'actionthrottledtext'           => 'As an anti-spam measure, you are limited from performing this action too many times in a short space of time, and you have exceeded this limit.
 Please try again in a few minutes.',
+'actionthrottledtext-expiry'    => 'You have performed this action too many times in a short space of time. You can try again at $1.',
 'protectedpagetext'             => 'This page has been protected to prevent editing or other actions.',
 'viewsourcetext'                => 'You can view and copy the source of this page:',
 'viewyourtext'                  => "You can view and copy the source of '''your edits''' to this page:",
