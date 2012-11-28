978e5cf3b6aeef6fd11171a1f2f86fab3b6b14b3
csteipp <csteipp@wikimedia.org>
2012-11-28 09:44:08 -0800

    Update release notes and incremented version
    Change-Id: Icd5e61bfeb34519d65829944af201886fe7e6abc

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -59,7 +59,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion = '1.20.0';
+$wgVersion = '1.20.1';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
