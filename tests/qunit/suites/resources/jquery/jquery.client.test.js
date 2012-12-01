45654d3e8cdde300a2109c5b454d8853c66f3694
MatmaRex <matma.rex@gmail.com>
2012-12-01 14:34:29 +0100

    (bug 42604) fix faulty browser detection code for Opera
    Opera's `navigator.userAgent` might not include the "Version/major.minor"
    part, causing the code relying on it being present to blow up. This patch
    fixes the issue.
    
    Also added tests for Opera.
    
    Change-Id: I9ceb7517e00042366e45cf13aa89262ed19709c0

--- tests/qunit/suites/resources/jquery/jquery.client.test.js
+++ tests/qunit/suites/resources/jquery/jquery.client.test.js
@@ -161,7 +161,42 @@
 				}
 			},
 			// Safari 5
-			// Opera 10
+			// Opera 10+
+			'Opera/9.80 (Windows NT 5.1)': {
+				title: 'Opera 10+ (exact version unspecified)',
+				platform: 'Win32',
+				profile: {
+					name: 'opera',
+					layout: 'presto',
+					layoutVersion: 'unknown',
+					platform: 'win',
+					version: '10',
+					versionBase: '10',
+					versionNumber: 10
+				}
+				wikiEditor: {
+					ltr: true,
+					rtl: true
+				}
+			},
+			// Opera 12
+			'Opera/9.80 (Windows NT 5.1) Presto/2.12.388 Version/12.11': {
+				title: 'Opera 12',
+				platform: 'Win32',
+				profile: {
+					name: 'opera',
+					layout: 'presto',
+					layoutVersion: 'unknown',
+					platform: 'win',
+					version: '12.11',
+					versionBase: '12',
+					versionNumber: 12.11
+				},
+				wikiEditor: {
+					ltr: true,
+					rtl: true
+				}
+			},
 			// Chrome 5
 			// Chrome 6
 			// Chrome 7
