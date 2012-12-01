09dde1685773efab18bc899ddef70f6cb182cb2d
Alex Monk <krenair@gmail.com>
2012-12-01 00:19:05 +0000

    Cross-wiki user rights UI should show target wiki groups instead of local
    Bug: 12518
    Change-Id: Idfec62d55d9a7a794e463f762d6959fe56ec0fc0

--- includes/api/ApiQuerySiteinfo.php
+++ includes/api/ApiQuerySiteinfo.php
@@ -460,7 +460,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 
 	protected function appendUserGroups( $property, $numberInGroup ) {
 		global $wgGroupPermissions, $wgAddGroups, $wgRemoveGroups;
-		global $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf;
+		global $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf, $wgImplicitGroups;
 
 		$data = array();
 		$result = $this->getResult();
@@ -468,6 +468,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 			$arr = array(
 				'name' => $group,
 				'rights' => array_keys( $permissions, true ),
+				'implicit' => in_array( $group, $wgImplicitGroups ),
 			);
 
 			if ( $numberInGroup ) {
