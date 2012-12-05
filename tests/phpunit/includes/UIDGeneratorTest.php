d7ca3e6eca4f263cae5243defbc8e553a0668cc3
Aaron Schulz <aschulz@wikimedia.org>
2012-12-05 11:48:46 -0800

    Added UIDGenerator class for getting statistically unique IDs.
    * This is useful for making IDs for object in partitioned stores.
    * The "UID" functions are not called "UUIDs" since they don't follow RFC4122.
      This is for performance reasons since the ID fields tend to be indexed.
    * This includes newUUIDv4(), which is compliant with the aforementioned RFC.
    
    This reverts commit 86f895bfe395d9fe00792422b052e7ebf43c7ae9.
    
    Change-Id: I989626dfb7976d17f5931a34fe040f41cf14dfe3

---
+++ tests/phpunit/includes/UIDGeneratorTest.php
@@ -0,0 +1,61 @@
+<?php
+
+class UIDGeneratorTest extends MediaWikiTestCase {
+	/**
+	 * @dataProvider provider_testTimestampedUID
+	 */
+	public function testTimestampedUID( $method, $digitlen, $bits, $hostbits ) {
+		$id = call_user_func( array( 'UIDGenerator', $method ) );
+		$this->assertEquals( true, ctype_digit( $id ), "UID made of digit characters" );
+		$this->assertLessThanOrEqual( $digitlen, strlen( $id ),
+			"UID has the right number of digits" );
+		$this->assertLessThanOrEqual( $bits, strlen( wfBaseConvert( $id, 10, 2 ) ),
+			"UID has the right number of bits" );
+
+		$lastId = $id;
+		if ( $hostbits ) {
+			$lastHost = substr( wfBaseConvert( $lastId, 10, 2, $bits ), -$hostbits );
+		}
+		for ( $i = 0; $i < 300; $i++ ) {
+			$id = call_user_func( array( 'UIDGenerator', $method ) );
+			$this->assertGreaterThan( $lastId, $id,
+				"New ID ($id) greater than prior one ($lastId)." );
+			$lastId = $id;
+			if ( $hostbits ) {
+				$host = substr( wfBaseConvert( $id, 10, 2, $bits ), -$hostbits );
+				$this->assertEquals( $lastHost, $host, "UID has same host ID" );
+				$lastHost = $host;
+			}
+		}
+	}
+
+	/**
+	 * array( method, length, bits, hostbits )
+	 */
+	public static function provider_testTimestampedUID() {
+		return array(
+			array( 'newTimestampedUID128', 39, 128, 48 ),
+			array( 'newTimestampedUID88', 27, 88, 32 ),
+		);
+	}
+
+	public function testUUIDv4() {
+		for ( $i = 0; $i < 100; $i++ ) {
+			$id = UIDGenerator::newUUIDv4();
+
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$!', $id ),
+				"UID $id has the right format" );
+
+			$id = UIDGenerator::newUUIDv4( 'normal' );
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$!', $id ),
+				"UID $id has the right format" );
+
+			$id = UIDGenerator::newUUIDv4( 'raw' );
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}$!', $id ),
+				"UID $id has the right format" );
+		}
+	}
+}
