58d05d89a6d6fdae0ec869ed8ddaef48d183b66e
S Page <spage@wikimedia.org>
2012-12-07 18:31:25 -0800

    (bug 42253) link user in accountcreatedtext msg
    Why not provide links to new user's page and talk page?  This is only
    when a logged-in user creates a new account, so presumably he or she
    knows what he's doing and appreciates the convenience links.  I tested
    on my local wiki and it worked.
    
    Also document accountcreated and accountcreatedtext in MessagesQqq.php,
    they were missing before.
    
    Change-Id: I4e88829d8665c5c61a59877962a8a14b55c91aa0

--- languages/messages/MessagesQqq.php
+++ languages/messages/MessagesQqq.php
@@ -1292,6 +1292,8 @@ Parameters:
 'emailconfirmlink' => 'Link to [[Special:ConfirmEmail]]. Appears in [[Special:Preferences]] > {{int:prefs-personal}} > {{int:email}} after saving your e-mail address but before it has been authenticated.',
 'invalidemailaddress' => 'Shown as a warning when written an invalid e-mail adress in [[Special:Preferences]] and {{fullurl:Special:UserLogin|type=signup}} page',
 'cannotchangeemail' => 'Error message shown when user goes to [[Special:ChangeEmail]] but email addresses cannot be changed on the site.',
+'accountcreated' => 'Title of page when a logged-in user creates an account.',
+'accountcreatedtext' => 'Text shown when a logged-in user creates an account. $1 is the new username.',
 'emaildisabled' => 'Error message shown when user tries to set an e-mail address but e-mail features are disabled.',
 'accountcreated' => 'Used as page title in [[Special:UserLogin]].
 
