7515b0b3daaf07f7d4292c1430d1967e84c44621
Chad Horohoe <chadh@wikimedia.org>
2012-12-10 20:57:34 -0500

    Revert "Applied patches to new WMF 1.21wmf6 branch"
    This reverts commit b92b9b62c0c93d16c93541ea1cd5afa5d60eef52.
    
    Change-Id: I4324813533ad984877b58d503aa0dc96010dbd50

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -59,7 +59,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgConf = new SiteConfiguration;
 
 /** MediaWiki version number */
-$wgVersion = '1.21wmf6';
+$wgVersion = '1.21alpha';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
