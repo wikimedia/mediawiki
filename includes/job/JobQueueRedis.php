8c9b306e23c85ce6268707149d7784160dc9a47a
Aaron Schulz <aschulz@wikimedia.org>
2012-12-11 16:04:03 -0800

    [JobQueue] Added a JobQueueRedis class.
    * The Job class now includes an extra "metadata" field now for usage
      by the queue classes. This is useful for stores that lack IDs.
    * A RedisConnectionPool class was added for reusability.
    
    Change-Id: Ie72536240e27fe1caf11096cdc4c85dfa821d181

---
+++ includes/job/JobQueueRedis.php
@@ -0,0 +1,548 @@
+<?php
+/**
+ * Redis-backed job queue code.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Aaron Schulz
+ */
+
+/**
+ * Class to handle job queues stored in Redis
+ *
+ * @ingroup JobQueue
+ * @since 1.21
+ */
+class JobQueueRedis extends JobQueue {
+	/** @var RedisConnectionPool */
+	protected $redisPool;
+
+	protected $server; // string; server address
+
+	const ROOTJOB_TTL   = 1209600; // integer; seconds to remember root jobs (14 days)
+	const MAX_AGE_PRUNE = 604800; // integer; seconds a job can live once claimed (7 days)
+	const MAX_ATTEMPTS  = 3; // integer; number of times to try a job
+
+	/**
+	 * @params include:
+	 *   - redisConf : An array of parameters to RedisConnectionPool::__construct().
+	 *   - server    : A hostname/port combination or the absolute path of a UNIX socket.
+	 *                 If a hostname is specified but no port, the standard port number
+	 *                 6379 will be used. Required.
+	 * @param array $params
+	 */
+	public function __construct( array $params ) {
+		parent::__construct( $params );
+		$this->server = $params['redisConf']['server'];
+		$this->redisPool = RedisConnectionPool::singleton( $params['redisConf'] );
+	}
+
+	/**
+	 * @see JobQueue::doIsEmpty()
+	 * @return bool
+	 * @throws MWException
+	 */
+	protected function doIsEmpty() {
+		$conn = $this->getConnection();
+		try {
+			return ( $conn->lSize( $this->getQueueKey( 'l-unclaimed' ) ) == 0 );
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+	}
+
+	/**
+	 * @see JobQueue::doGetSize()
+	 * @return integer
+	 * @throws MWException
+	 */
+	protected function doGetSize() {
+		$conn = $this->getConnection();
+		try {
+			return $conn->lSize( $this->getQueueKey( 'l-unclaimed' ) );
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+	}
+
+	/**
+	 * @see JobQueue::doGetAcquiredCount()
+	 * @return integer
+	 * @throws MWException
+	 */
+	protected function doGetAcquiredCount() {
+		$conn = $this->getConnection();
+		try {
+			if ( $this->claimTTL > 0 ) {
+				return $conn->lSize( $this->getQueueKey( 'l-claimed' ) );
+			} else {
+				return 0;
+			}
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+	}
+
+	/**
+	 * @see JobQueue::doBatchPush()
+	 * @param array $jobs
+	 * @param $flags
+	 * @return bool
+	 * @throws MWException
+	 */
+	protected function doBatchPush( array $jobs, $flags ) {
+		if ( !count( $jobs ) ) {
+			return true;
+		}
+
+		// Convert the jobs into a list of field maps
+		$items = array(); // (job sha1 or uid => job fields map)
+		foreach ( $jobs as $job ) {
+			$item = $this->getNewJobFields( $job );
+			$items[$item['uid']] = $item;
+		}
+
+		$conn = $this->getConnection();
+		try {
+			$conn->multi( Redis::MULTI ); // begin (atomic trx)
+			$conn->mset( $this->prefixKeysWithQueueKey( 'data', $items ) );
+			call_user_func_array(
+				array( $conn, 'lPush' ),
+				array_merge( array( $this->getQueueKey( 'l-unclaimed' ) ), array_keys( $items ) )
+			);
+			$res = $conn->exec(); // commit (atomic trx)
+
+			if ( in_array( false, $res, true ) ) {
+				wfDebugLog( 'JobQueueRedis', "Could not insert {$this->type} job(s)." );
+				return false;
+			} else {
+				wfIncrStats( 'job-insert', count( $items ) );
+			}
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+
+		return true;
+	}
+
+	/**
+	 * @see JobQueue::doPop()
+	 * @return Job|bool
+	 * @throws MWException
+	 */
+	protected function doPop() {
+		$job = false;
+
+		$conn = $this->getConnection();
+		try {
+			do {
+				// Periodically delete and/or recycle jobs that were claimed for too long
+				if ( mt_rand( 0, 99 ) == 0 ) {
+					$this->doInternalMaintenance();
+				}
+				// Atomically pop an item off the queue and onto the "claimed" list
+				$uid = $conn->rpoplpush(
+					$this->getQueueKey( 'l-unclaimed' ),
+					$this->getQueueKey( 'l-claimed' )
+				);
+				if ( $uid === false ) {
+					break; // no jobs; nothing to do
+				}
+
+				wfIncrStats( 'job-pop' );
+				$conn->multi( Redis::PIPELINE );
+				$conn->get( $this->prefixWithQueueKey( 'data', $uid ) );
+				if ( $this->claimTTL > 0 ) {
+					// Set the claim timestamp metadata. If this step fails, then
+					// the timestamp will be assumed to be the current timestamp by
+					// recycleStaleJobs() as of the next time that function runs.
+					// If two runners claim duplicate jobs, one will abort here.
+					$conn->hSetNx( $this->getQueueKey( 'h-claimtimes' ), $uid, time() );
+				} else {
+					// If this fails, the message key will be deleted in cleanupClaimedJobs().
+					// If two runners claim duplicate jobs, one will abort here.
+					$conn->delete( $this->prefixWithQueueKey( 'data', $uid ) );
+				}
+				list( $item, $ok ) = $conn->exec();
+				if ( $item === false || ( $this->claimTTL && !$ok ) ) {
+					wfDebug( "Could not find or delete job $uid; probably got a duplicate." );
+					continue; // job was probably a duplicate
+				}
+
+				$job = $this->getJobFromFields( $item );
+				if ( !$job && $this->claimTTL > 0 ) { // remove if invalid
+					$conn->multi( Redis::MULTI ); // begin (atomic trx)
+					$conn->lRem( $this->getQueueKey( 'l-claimed' ), $item, -1 );
+					$conn->hDel( $this->getQueueKey( 'h-claimtimes' ), $uid );
+					$conn->delete( $this->prefixWithQueueKey( 'data', $uid ) );
+					$conn->exec(); // commit (atomic trx)
+				}
+			} while ( !$job ); // job may be false if invalid
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+
+		// Delete and/or recycle jobs that were claimed for too long
+		if ( !$job ) { // useful for small queues
+			$this->doInternalMaintenance();
+		}
+
+		// Flag this job as an old duplicate based on its "root" job...
+		try {
+			if ( $job && $this->isRootJobOldDuplicate( $job ) ) {
+				wfIncrStats( 'job-pop-duplicate' );
+				return DuplicateJob::newFromJob( $job ); // convert to a no-op
+			}
+		} catch ( MWException $e ) {} // don't lose jobs over this
+
+		return $job;
+	}
+
+	/**
+	 * @see JobQueue::doAck()
+	 * @param Job $job
+	 * @return Job|bool
+	 * @throws MWException
+	 */
+	protected function doAck( Job $job ) {
+		if ( $this->claimTTL > 0 ) {
+			$conn = $this->getConnection();
+			try {
+				// Get the exact field map this Job came from, regardless of whether
+				// the job was transformed into a DuplicateJob or anything of the sort.
+				$item = $job->metadata['sourceFields'];
+
+				$conn->multi( Redis::MULTI ); // begin (atomic trx)
+				// Remove the first instance of this job scanning right-to-left.
+				// This is O(N) in the worst case, but is likely to be much faster since
+				// jobs are pushed to the left and we are starting from the right, where
+				// the longest running jobs are likely to be. These should be the first
+				// jobs to be acknowledged assuming that job run times are roughly equal.
+				$conn->lRem( $this->getQueueKey( 'l-claimed' ), $item['uid'], -1 );
+				// Delete the claim timestamp metadata
+				$conn->hDel( $this->getQueueKey( 'h-claimtimes' ), $item['uid'] );
+				// Delete the job message itself
+				$conn->delete( $this->prefixWithQueueKey( 'data', $item['uid'] ) );
+				$res = $conn->exec(); // commit (atomic trx)
+
+				if ( in_array( false, $res, true ) ) {
+					wfDebugLog( 'JobQueueRedis', "Could not acknowledge {$this->type} job." );
+					return false;
+				}
+			} catch ( RedisException $e ) {
+				$this->throwRedisException( $this->server, $conn, $e );
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @see JobQueue::doDeduplicateRootJob()
+	 * @param Job $job
+	 * @return bool
+	 * @throws MWException
+	 */
+	protected function doDeduplicateRootJob( Job $job ) {
+		$params = $job->getParams();
+		if ( !isset( $params['rootJobSignature'] ) ) {
+			throw new MWException( "Cannot register root job; missing 'rootJobSignature'." );
+		} elseif ( !isset( $params['rootJobTimestamp'] ) ) {
+			throw new MWException( "Cannot register root job; missing 'rootJobTimestamp'." );
+		}
+		$key = $this->getRootJobKey( $params['rootJobSignature'] );
+
+		$conn = $this->getConnection();
+		try {
+			$timestamp = $conn->get( $key ); // current last timestamp of this job
+			if ( $timestamp && $timestamp >= $params['rootJobTimestamp'] ) {
+				return true; // a newer version of this root job was enqueued
+			}
+			// Update the timestamp of the last root job started at the location...
+			return $conn->set( $key, $params['rootJobTimestamp'], self::ROOTJOB_TTL ); // 2 weeks
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+	}
+
+	/**
+	 * Check if the "root" job of a given job has been superseded by a newer one
+	 *
+	 * @param $job Job
+	 * @return bool
+	 * @throws MWException
+	 */
+	protected function isRootJobOldDuplicate( Job $job ) {
+		$params = $job->getParams();
+		if ( !isset( $params['rootJobSignature'] ) ) {
+			return false; // job has no de-deplication info
+		} elseif ( !isset( $params['rootJobTimestamp'] ) ) {
+			wfDebugLog( 'JobQueueRedis', "Cannot check root job; missing 'rootJobTimestamp'." );
+			return false;
+		}
+
+		$conn = $this->getConnection();
+		try {
+			// Get the last time this root job was enqueued
+			$timestamp = $conn->get( $this->getRootJobKey( $params['rootJobSignature'] ) );
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+
+		// Check if a new root job was started at the location after this one's...
+		return ( $timestamp && $timestamp > $params['rootJobTimestamp'] );
+	}
+
+	/**
+	 * Do any job recycling or queue cleanup as needed
+	 *
+	 * @return void
+	 * @return integer Number of jobs recycled/deleted
+	 * @throws MWException
+	 */
+	protected function doInternalMaintenance() {
+		return ( $this->claimTTL > 0 ) ? $this->recycleStaleJobs() : $this->cleanupClaimedJobs();
+	}
+
+	/**
+	 * Recycle or destroy any jobs that have been claimed for too long
+	 *
+	 * @return integer Number of jobs recycled/deleted
+	 * @throws MWException
+	 */
+	protected function recycleStaleJobs() {
+		$count = 0;
+		// For each job item that can be retried, we need to add it back to the
+		// main queue and remove it from the list of currenty claimed job items.
+		$conn = $this->getConnection();
+		try {
+			// Avoid duplicate insertions of items to be re-enqueued
+			$conn->multi( Redis::MULTI );
+			$conn->setnx( $this->getQueueKey( 'lock' ), 1 );
+			$conn->expire( $this->getQueueKey( 'lock' ), 3600 );
+			if ( $conn->exec() !== array( true, true ) ) { // lock
+				return $count; // already in progress
+			}
+
+			$claimCutoff = time() - $this->claimTTL;
+			$pruneCutoff = time() - self::MAX_AGE_PRUNE;
+
+			$conn->multi( Redis::PIPELINE );
+			// Get the list of all claimed jobs
+			$conn->lRange( $this->getQueueKey( 'l-claimed' ), 0, -1 );
+			// Get a map of (uid => claim timestamp) for all claimed jobs
+			$conn->hGetAll( $this->getQueueKey( 'h-claimtimes' ) );
+			list( $claimedUids, $claimTimes ) = $conn->exec();
+
+			$uidsRemove = array(); // item IDs to remove from "claimed" queue
+			$itemsAdd = array(); // items to add to the "unclaimed" queue
+			foreach ( $claimedUids as $uid ) { // all claimed items
+				$item = $conn->get( $this->prefixWithQueueKey( 'data', $uid ) );
+				// Job is in list but is not defined?
+				if ( !is_array( $item ) ) {
+					$uidsRemove[] = $uid; // just remove it
+				// Claimed job has no "claim timestamp" set?
+				} elseif ( !isset( $claimTimes[$uid] ) ) {
+					// If pop() failed to set the claim timestamp, set it to the current time.
+					// Since that function sets this non-atomically *after* moving the job to
+					// the "claimed" queue, it may be the case that it just didn't set it yet.
+					$conn->hSetNx( $this->getQueueKey( 'h-claimtimes' ), $uid, time() );
+				// Claimed job claimed for too long?
+				} elseif ( $claimTimes[$uid] < $claimCutoff ) {
+					if ( $item['attempts'] < self::MAX_ATTEMPTS ) { // can be retried
+						$uidsRemove[] = $uid;
+						++$item['attempts'];
+						$itemsAdd[$uid] = $item;
+					} elseif ( $claimTimes[$uid] < $pruneCutoff ) {
+						$uidsRemove[] = $uid; // just remove it
+					}
+				}
+			}
+
+			$conn->multi( Redis::MULTI ); // begin (atomic trx)
+			if ( count( $itemsAdd ) ) { // copy jobs to unclaimed queue
+				$uidsInsert = array_keys( $itemsAdd );
+				$conn->mset( $this->prefixKeysWithQueueKey( 'data', $itemsAdd ) );
+				call_user_func_array(
+					array( $conn, 'lPush' ),
+					array_merge( array( $this->getQueueKey( 'l-unclaimed' ) ), $uidsInsert )
+				);
+			}
+			foreach ( $uidsRemove as $uid ) { // remove jobs from claimed queue
+				$conn->lRem( $this->getQueueKey( 'l-claimed' ), $uid, -1 ); // item likely near end
+				$conn->hDel( $this->getQueueKey( 'h-claimtimes' ), $uid );
+			}
+			$res = $conn->exec(); // commit (atomic trx)
+
+			if ( in_array( false, $res, true ) ) {
+				wfDebugLog( 'JobQueueRedis', "Could not recycle {$this->type} job(s)." );
+			} else {
+				$count += ( count( $itemsAdd ) + count( $uidsRemove ) );
+				wfIncrStats( 'job-recycle', count( $itemsAdd ) );
+			}
+
+			$conn->delete( $this->getQueueKey( 'lock' ) ); // unlock
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+
+		return $count;
+	}
+
+	/**
+	 * Destroy any jobs that have been claimed
+	 *
+	 * @return integer Number of jobs deleted
+	 * @throws MWException
+	 */
+	protected function cleanupClaimedJobs() {
+		$count = 0;
+		// Make sure the message for claimed jobs was deleted
+		// and remove the claimed job IDs from the "claimed" list.
+		$conn = $this->getConnection();
+		try {
+			// Avoid races and duplicate effort
+			$conn->multi( Redis::MULTI );
+			$conn->setnx( $this->getQueueKey( 'lock' ), 1 );
+			$conn->expire( $this->getQueueKey( 'lock' ), 3600 );
+			if ( $conn->exec() !== array( true, true ) ) { // lock
+				return $count; // already in progress
+			}
+			// Get the list of all claimed jobs
+			$uids = $conn->lRange( $this->getQueueKey( 'l-claimed' ), 0, -1 );
+			if ( count( $uids ) ) {
+				// Delete the message keys and delist the corresponding ids.
+				// Since the only other changes to "l-claimed" are left pushes, we can just strip
+				// off the elements read here using a right trim based on the number of ids read.
+				$conn->multi( Redis::MULTI ); // begin (atomic trx)
+				$conn->delete( array_keys(
+					$this->prefixKeysWithQueueKey( 'data', array_flip( $uids ) ) ) );
+				$conn->lTrim( $this->getQueueKey( 'l-claimed' ), 0, -count( $uids ) - 1 );
+				$res = $conn->exec(); // commit (atomic trx)
+
+				if ( in_array( false, $res, true ) ) {
+					wfDebugLog( 'JobQueueRedis', "Could not purge {$this->type} job(s)." );
+				} else {
+					$count += count( $uids );
+				}
+			}
+			$conn->delete( $this->getQueueKey( 'lock' ) ); // unlock
+		} catch ( RedisException $e ) {
+			$this->throwRedisException( $this->server, $conn, $e );
+		}
+
+		return $count;
+	}
+
+	/**
+	 * @param $job Job
+	 * @return array
+	 */
+	protected function getNewJobFields( Job $job ) {
+		return array(
+			// Fields that describe the nature of the job
+			'type'      => $job->getType(),
+			'namespace' => $job->getTitle()->getNamespace(),
+			'title'     => $job->getTitle()->getDBkey(),
+			'params'    => $job->getParams(),
+			// Additional metadata
+			'uid'       => $job->ignoreDuplicates()
+				? wfBaseConvert( sha1( serialize( $job->getDeduplicationInfo() ) ), 16, 36, 31 )
+				: wfRandomString( 32 ),
+			'timestamp' => time(), // UNIX timestamp
+			'attempts'  => 0
+		);
+	}
+
+	/**
+	 * @param $fields array
+	 * @return Job|bool
+	 */
+	protected function getJobFromFields( array $fields ) {
+		$title = Title::makeTitleSafe( $fields['namespace'], $fields['title'] );
+		if ( $title ) {
+			$job = Job::factory( $fields['type'], $title, $fields['params'] );
+			$job->metadata['sourceFields'] = $fields;
+			return $job;
+		}
+		return false;
+	}
+
+	/**
+	 * Get a connection to the server that handles all sub-queues for this queue
+	 *
+	 * @return Array (server name, Redis instance)
+	 * @throws MWException
+	 */
+	protected function getConnection() {
+		$conn = $this->redisPool->getConnection( $this->server );
+		if ( !$conn ) {
+			throw new MWException( "Unable to connect to redis server." );
+		}
+		return $conn;
+	}
+
+	/**
+	 * @param $server string
+	 * @param $conn RedisConnRef
+	 * @param $e RedisException
+	 * @throws MWException
+	 */
+	protected function throwRedisException( $server, RedisConnRef $conn, $e ) {
+		$this->redisPool->handleException( $server, $conn, $e );
+		throw new MWException( "Redis server error: {$e->getMessage()}\n" );
+	}
+
+	/**
+	 * @param $prop string
+	 * @return string
+	 */
+	private function getQueueKey( $prop ) {
+		list( $db, $prefix ) = wfSplitWikiID( $this->wiki );
+		return wfForeignMemcKey( $db, $prefix, 'jobqueue', $this->type, $prop );
+	}
+
+	/**
+	 * @param string $signature Hash identifier of the root job
+	 * @return string
+	 */
+	private function getRootJobKey( $signature ) {
+		list( $db, $prefix ) = wfSplitWikiID( $this->wiki );
+		return wfForeignMemcKey( $db, $prefix, 'jobqueue', $this->type, 'rootjob', $signature );
+	}
+
+	/**
+	 * @param $prop string
+	 * @param $string string
+	 * @return string
+	 */
+	private function prefixWithQueueKey( $prop, $string ) {
+		return $this->getQueueKey( $prop ) . ':' . $string;
+	}
+
+	/**
+	 * @param $prop string
+	 * @param $items array
+	 * @return Array
+	 */
+	private function prefixKeysWithQueueKey( $prop, array $items ) {
+		$res = array();
+		foreach ( $items as $key => $item ) {
+			$res[$this->prefixWithQueueKey( $prop, $key )] = $item;
+		}
+		return $res;
+	}
+}
