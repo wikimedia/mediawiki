833b97a9b72117b29f42881c276f5bab360c14e1
daniel <daniel.kinzler@wikimedia.de>
2012-12-13 17:26:43 +0100

    (bug 42915) make MovePage aware of whether redirects are supported.
    Special:Movepage and ApiMovePage should be aware whether the present content
    model supports redirects, and only offer to keep a redirect on moving
    of it's supported.
    
    Change-Id: I2d02b2668ea8ebe39387b79d165f6cf1037303b9

--- includes/content/WikitextContentHandler.php
+++ includes/content/WikitextContentHandler.php
@@ -60,6 +60,17 @@ class WikitextContentHandler extends TextContentHandler {
 	}
 
 	/**
+	 * Returns true because wikitext supports redirects.
+	 *
+	 * @see ContentHandler::supportsRedirects
+	 *
+	 * @return boolean whether sections are supported.
+	 */
+	public function supportsRedirects() {
+		return false;
+	}
+
+	/**
 	 * Returns true because wikitext supports sections.
 	 *
 	 * @return boolean whether sections are supported.
