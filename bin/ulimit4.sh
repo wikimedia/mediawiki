d4f4d0e04669e09b1ee580ad18dc9a3d91618439
Jan Gerber <jgerber@wikimedia.org>
2012-12-21 10:19:35 +0000

    limit memory via cgroups if available
    	If cgroups are setup use them to limit memory if run via ulimit4.sh
    	Change I029a3999 enables cgroups on imagescalers
    
    Change-Id: Iaa83812230214e448d436078a06bdc2ae9ae1e38

--- bin/ulimit4.sh
+++ bin/ulimit4.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if [ -e /sys/fs/cgroup/memory/mediawiki/job/ ]; then
+	mkdir -m 0700 /sys/fs/cgroup/memory/mediawiki/job/$$
+	echo $$ > /sys/fs/cgroup/memory/mediawiki/job/$$/tasks
+	echo "1" > /sys/fs/cgroup/memory/mediawiki/job/$$/notify_on_release
+	#memory
+	echo $(($2*1024)) > /sys/fs/cgroup/memory/mediawiki/job/$$/memory.limit_in_bytes
+	#memory+swap
+	echo $(($2*1024)) > /sys/fs/cgroup/memory/mediawiki/job/$$/memory.memsw.limit_in_bytes
+fi
+
 ulimit -t $1 -v $2 -f $3
 eval "$4"
