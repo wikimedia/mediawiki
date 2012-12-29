8550dd92314ba3cc0e9cc7f2c01090b463940e99
Matthew Flaschen <mflaschen@wikimedia.org>
2012-12-28 21:39:12 -0500

    (bug 43498) (bug 43574) Two wikilink types and {{int:}}.
    * Bug 43498 - Implement two wikilink types, with tests. Also add test
    for external link, which was already implemented in the
    main code.
    
    * Bug 43574 - Support {{int:}} message includes in jqueryMsg, with tests.
    * Add tests for when the old mw.msg should be called, vs. getMessageFunction.
    Change-Id: I7a748eb99259103f491ff5a5aa0bf223fc0b76c4

--- tests/qunit/suites/resources/mediawiki/mediawiki.jqueryMsg.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.jqueryMsg.test.js
@@ -4,9 +4,15 @@ QUnit.module( 'mediawiki.jqueryMsg', QUnit.newMwEnvironment( {
 	setup: function () {
 		this.orgMwLangauge = mw.language;
 		mw.language = $.extend( true, {}, this.orgMwLangauge );
+		jQuery.fn.getOuterHtml = function() {
+			var $div = $( '<div>' );
+			$div.append($(this).eq(0).clone());
+			return $div.html();
+		};
 	},
 	teardown: function () {
 		mw.language = this.orgMwLangauge;
+		delete jQuery.fn.getOuterHtml;
 	}
 }) );
 
@@ -153,5 +159,167 @@ QUnit.test( 'Output matches PHP parser', mw.libs.phpParserData.tests.length, fun
 		} );
 	} );
 });
+
+QUnit.test( 'Links', 6, function ( assert ) {
+	var parser = mw.jqueryMsg.getMessageFunction(),
+		expectedListUsers,
+		expectedDisambiguationsText,
+		expectedMultipleBars,
+		expectedSpecialCharacters,
+		specialCharactersPageName;
+
+	/*
+	 The below three are all identical to or based on real messages.  For disambiguations-text,
+	 the bold was removed because it is not yet implemented.
+	*/
+
+	mw.messages.set( 'statistics-users', '注册[[Special:ListUsers|用户]]' );
+
+	expectedListUsers = '注册' + $( '<a>' ).attr( {
+		title: 'Special:ListUsers',
+		href: mw.util.wikiGetlink( 'Special:ListUsers' )
+	} ).text( '用户' ).getOuterHtml();
+
+	assert.equal(
+		parser( 'statistics-users' ),
+		expectedListUsers,
+		'Piped wikilink'
+	);
+
+	expectedDisambiguationsText = 'The following pages contain at least one link to a disambiguation page.\nThey may have to link to a more appropriate page instead.<br>\nA page is treated as a disambiguation page if it uses a template that is linked from ' +
+		$( '<a>' ).attr( {
+			title: 'MediaWiki:Disambiguationspage',
+			href: mw.util.wikiGetlink( 'MediaWiki:Disambiguationspage' )
+		} ).text( 'MediaWiki:Disambiguationspage' ).getOuterHtml() + '.';
+	mw.messages.set( 'disambiguations-text', 'The following pages contain at least one link to a disambiguation page.\nThey may have to link to a more appropriate page instead.<br />\nA page is treated as a disambiguation page if it uses a template that is linked from [[MediaWiki:Disambiguationspage]].' );
+	assert.equal(
+		parser( 'disambiguations-text' ),
+		expectedDisambiguationsText,
+		'Wikilink without pipe'
+	);
+
+	mw.messages.set( 'version-entrypoints-index-php', '[https://www.mediawiki.org/wiki/Manual:index.php index.php]' );
+	assert.equal(
+		parser( 'version-entrypoints-index-php' ),
+		'<a href="https://www.mediawiki.org/wiki/Manual:index.php">index.php</a>',
+		'External link'
+	);
+
+	// Not supported currently, but should not parse as text either.
+	mw.messages.set( 'pipe-trick', '[[Tampa, Florida|]]' );
+	assert.throws(
+		parser( 'pipe-trick' ),
+		Error,
+		'Pipe trick is not supported'
+	);
+
+	expectedMultipleBars = $( '<a>' ).attr( {
+		title: 'Main Page',
+		href: mw.util.wikiGetlink( 'Main Page' )
+	} ).text( 'Main|Page' ).getOuterHtml();
+	mw.messages.set( 'multiple-bars', '[[Main Page|Main|Page]]' );
+	assert.equal(
+		parser( 'multiple-bars' ),
+		expectedMultipleBars,
+		'Bar in anchor'
+	);
+
+	specialCharactersPageName = '"Who" wants to be a millionaire & live on \'Exotic Island\'?';
+	expectedSpecialCharacters = $( '<a>' ).attr( {
+		title: specialCharactersPageName,
+		href: mw.util.wikiGetlink( specialCharactersPageName )
+	} ).text( specialCharactersPageName ).getOuterHtml();
+
+	mw.messages.set( 'special-characters', '[[' + specialCharactersPageName + ']]' );
+	assert.equal(
+		parser( 'special-characters' ),
+		expectedSpecialCharacters,
+		'Special characters'
+	);
+});
+
+QUnit.test( 'Int magic word', 4, function ( assert ) {
+	var parser = mw.jqueryMsg.getMessageFunction(),
+	    newarticletextSource = 'You have followed a link to a page that does not exist yet. To create the page, start typing in the box below (see the [[{{Int:Helppage}}|help page]] for more info). If you are here by mistake, click your browser\'s back button.',
+		expectedNewarticletext;
+
+	mw.messages.set( 'helppage', 'Help:Contents' );
+
+	expectedNewarticletext = 'You have followed a link to a page that does not exist yet. To create the page, start typing in the box below (see the <a href="' +
+		mw.util.wikiGetlink( mw.msg( 'helppage' ) ) +
+		'">help page</a> for more info). If you are here by mistake, click your browser\'s back button.';
+
+	mw.messages.set( 'newarticletext', newarticletextSource );
+
+	assert.equal(
+		parser( 'newarticletext' ),
+		expectedNewarticletext,
+		'Link with nested message'
+	);
+
+	mw.messages.set( 'portal-url', 'Project:Community portal' );
+	mw.messages.set( 'see-portal-url', '{{Int:portal-url}} is an important community page.' );
+	assert.equal(
+		parser( 'see-portal-url' ),
+		'Project:Community portal is an important community page.',
+		'Nested message'
+	);
+
+	mw.messages.set( 'newarticletext-lowercase',
+		newarticletextSource.replace( 'Int:Helppage', 'int:helppage' ) );
+
+	assert.equal(
+		parser( 'newarticletext-lowercase' ),
+		expectedNewarticletext,
+		'Link with nested message, lowercase include'
+	);
+
+	mw.messages.set( 'uses-missing-int', '{{int:doesnt-exist}}' );
+
+	assert.equal(
+		parser( 'uses-missing-int' ),
+		'[doesnt-exist]',
+		'int: where nested message does not exist'
+	);
+});
+
+// Tests that getMessageFunction is used for messages with curly braces or square brackets,
+// but not otherwise.
+QUnit.test( 'Calls to mw.msg', 8, function ( assert ) {
+	// Should be
+	var map, oldGMF, outerCalled, innerCalled;
+
+	map = new mw.Map();
+	map.set( {
+		'curly-brace': '{{int:message}}',
+		'single-square-bracket': '[https://www.mediawiki.org/ MediaWiki]',
+		'double-square-bracket': '[[Some page]]',
+		'regular': 'Other message'
+	} );
+
+	oldGMF = mw.jqueryMsg.getMessageFunction;
+
+	mw.jqueryMsg.getMessageFunction = function() {
+		outerCalled = true;
+		return function() {
+			innerCalled = true;
+		};
+	};
+
+	function verifyGetMessageFunction( key, shouldCall ) {
+		outerCalled = false;
+		innerCalled = false;
+		( new mw.Message( map, key ) ).parser();
+		assert.strictEqual( outerCalled, shouldCall, 'Outer function called for ' + key );
+		assert.strictEqual( innerCalled, shouldCall, 'Inner function called for ' + key );
+	}
+
+	verifyGetMessageFunction( 'curly-brace', true );
+	verifyGetMessageFunction( 'single-square-bracket', true );
+	verifyGetMessageFunction( 'double-square-bracket', true );
+	verifyGetMessageFunction( 'regular', false );
+
+	mw.jqueryMsg.getMessageFunction = oldGMF;
+} );
 
 }( mediaWiki, jQuery ) );
