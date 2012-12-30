32db6e96267235533f139fc3073fca81151c27b4
jarry1250 <jarry1250@gmail.com>
2012-12-30 00:51:08 +0000

    Use <ul>,<li> in search suggestions instead of endless <div>s.
    It's more semantic, and, well, sensible. Instead of find/replacing,
    I've tried to make the JavaScript more markup agnostic.
    
    Change-Id: Id772bb0ceb25a06c8564434c7d4318597bd32262

--- resources/jquery/jquery.suggestions.js
+++ resources/jquery/jquery.suggestions.js
@@ -229,7 +229,7 @@ $.suggestions = {
 						for ( i = 0; i < context.config.suggestions.length; i++ ) {
 							/*jshint loopfunc:true */
 							text = context.config.suggestions[i];
-							$result = $( '<div>' )
+							$result = $( '<li>' )
 								.addClass( 'suggestions-result' )
 								.attr( 'rel', i )
 								.data( 'text', context.config.suggestions[i] )
@@ -237,7 +237,7 @@ $.suggestions = {
 									context.data.selectedWithMouse = true;
 									$.suggestions.highlight(
 										context,
-										$(this).closest( '.suggestions-results .suggestions-result' ),
+										$(this).closest( '.suggestions-result' ),
 										false
 									);
 								} )
@@ -320,14 +320,14 @@ $.suggestions = {
 						if ( context.data.$container.find( '.suggestions-special' ).html() !== '' ) {
 							result = context.data.$container.find( '.suggestions-special' );
 						} else {
-							result = context.data.$container.find( '.suggestions-results .suggestions-result:last' );
+							result = context.data.$container.find( '.suggestions-result' ).last();
 						}
 					}
 				}
 			} else if ( result === 'next' ) {
 				if ( selected.length === 0 ) {
 					// No item selected, go to the first one
-					result = context.data.$container.find( '.suggestions-results .suggestions-result:first' );
+					result = context.data.$container.find( '.suggestions-result' ).first();
 					if ( result.length === 0 && context.data.$container.find( '.suggestions-special' ).html() !== '' ) {
 						// No suggestion exists, go to the special one directly
 						result = context.data.$container.find( '.suggestions-special' );
@@ -509,15 +509,15 @@ $.fn.suggestions = function () {
 				.css( 'display', 'none' )
 				.addClass( 'suggestions' )
 				.append(
-					$( '<div>' ).addClass( 'suggestions-results' )
-						// Can't use click() because the container div is hidden when the
+					$( '<ul>' ).addClass( 'suggestions-results' )
+						// Can't use click() because the container ul is hidden when the
 						// textbox loses focus. Instead, listen for a mousedown followed
-						// by a mouseup on the same div.
+						// by a mouseup on the same ul.
 						.mousedown( function ( e ) {
-							context.data.mouseDownOn = $( e.target ).closest( '.suggestions-results .suggestions-result' );
+							context.data.mouseDownOn = $( e.target ).closest( '.suggestions-result' );
 						} )
 						.mouseup( function ( e ) {
-							var $result = $( e.target ).closest( '.suggestions-results .suggestions-result' ),
+							var $result = $( e.target ).closest( '.suggestions-result' ),
 								$other = context.data.mouseDownOn;
 
 							context.data.mouseDownOn = $( [] );
