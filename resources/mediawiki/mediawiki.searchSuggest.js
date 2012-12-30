1ac936b73d79eaa6d899cf11eff11fdf569782e0
jarry1250 <jarry1250@gmail.com>
2012-12-30 00:51:08 +0000

    Use <ul>,<li> in search suggestions instead of endless <div>s.
    It's more semantic, and, well, sensible. Instead of find/replacing,
    I've tried to make the JavaScript more markup agnostic.
    
    Change-Id: Id772bb0ceb25a06c8564434c7d4318597bd32262

--- resources/mediawiki/mediawiki.searchSuggest.js
+++ resources/mediawiki/mediawiki.searchSuggest.js
@@ -92,10 +92,12 @@
 						$( '<div>' )
 							.addClass( 'special-label' )
 							.text( mw.msg( 'searchsuggest-containing' ) ),
-						$( '<div>' )
+						$( '<ul>' )
 							.addClass( 'special-query' )
-							.text( query )
-							.autoEllipsis()
+							.append( $( '<li>' )
+								.text( query )
+								.autoEllipsis()
+							)
 					)
 					.show();
 			} else {
