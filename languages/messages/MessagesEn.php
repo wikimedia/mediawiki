860b2432b0ac529d631c8bbc58f215db5c276f1b
Brad Jorsch <bjorsch@wikimedia.org>
2012-12-31 16:30:24 -0500

    Add PagesWithProp special page and API module
    It would be useful to be able to list pages using a particular page
    property, particularly in light of the new Disambiguator extension.
    
    This adds a special page Special:PagesWithProp and an API query module
    list=pageswithprop to do just this. It also adds an API query module
    list=pagepropnames to list the prop names currently in use on the wiki.
    
    Change-Id: Ib0d4e17f22b8d0cb9894eac6095962315480e809

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -429,6 +429,7 @@ $specialPageAliases = array(
 	'Myuploads'                 => array( 'MyUploads' ),
 	'Newimages'                 => array( 'NewFiles', 'NewImages' ),
 	'Newpages'                  => array( 'NewPages' ),
+	'PagesWithProp'             => array( 'PagesWithProp', 'Pageswithprop', 'PagesByProp', 'Pagesbyprop' ),
 	'PasswordReset'             => array( 'PasswordReset' ),
 	'PermanentLink'             => array( 'PermanentLink', 'PermaLink' ),
 	'Popularpages'              => array( 'PopularPages' ),
@@ -2564,6 +2565,13 @@ Remember to check for other links to the templates before deleting them.',
 'disambiguations-text'    => "The following pages contain at least one link to a '''disambiguation page'''.
 They may have to link to a more appropriate page instead.<br />
 A page is treated as a disambiguation page if it uses a template that is linked from [[MediaWiki:Disambiguationspage]].",
+
+'pageswithprop'         => 'Pages with a page property',
+'pageswithprop-summary' => '', # do not translate or duplicate this message to other languages
+'pageswithprop-legend'  => 'Pages with a page property',
+'pageswithprop-text'    => 'This page lists pages that use a particular page property.',
+'pageswithprop-prop'    => 'Property name:',
+'pageswithprop-submit'  => 'Go',
 
 'doubleredirects'                   => 'Double redirects',
 'doubleredirects-summary'           => '', # do not translate or duplicate this message to other languages
