f2cd2b5f14556621033d4d954d562337373a836d
Reedy <reedy@wikimedia.org>
2013-01-02 20:20:22 +0000

    Bug 11057 - Increase size of ug_group in user_groups table
    Change-Id: I79fd5c6e1566de3145ac39420da4fce77099745a

--- maintenance/oracle/archives/patch-user_former_groups.sql
+++ maintenance/oracle/archives/patch-user_former_groups.sql
@@ -2,7 +2,7 @@ define mw_prefix='{$wgDBprefix}';
 
 CREATE TABLE &mw_prefix.user_former_groups (
   ufg_user   NUMBER      DEFAULT 0 NOT NULL,
-  ufg_group  VARCHAR2(16)     NOT NULL
+  ufg_group  VARCHAR2(255)     NOT NULL
 );
 ALTER TABLE &mw_prefix.user_former_groups ADD CONSTRAINT &mw_prefix.user_former_groups_fk1 FOREIGN KEY (ufg_user) REFERENCES &mw_prefix.mwuser(user_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
 CREATE UNIQUE INDEX &mw_prefix.user_former_groups_u01 ON &mw_prefix.user_former_groups (ufg_user,ufg_group);
