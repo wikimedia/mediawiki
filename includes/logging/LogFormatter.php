8d70936ced6970946a901126df487a5776f6a226
Alex Monk <krenair@gmail.com>
2013-01-03 04:00:06 +0000

    Show changed bits on file version (oldimage) revdel logs.
    Don't know if there is a bug for this or not, but I noticed that we don't get "content hidden/unhidden" type notes for revdel logs when they're for file revisions (instead of page revisions)
    
    Change-Id: I0de389e601aaad7fd8abb989b4a0004082206894

--- includes/logging/LogFormatter.php
+++ includes/logging/LogFormatter.php
@@ -891,9 +891,11 @@ class DeleteLogFormatter extends LogFormatter {
 		$params = parent::getMessageParameters();
 		$subtype = $this->entry->getSubtype();
 		if ( in_array( $subtype, array( 'event', 'revision' ) ) ) {
+			// $params[3] here is 'revision' for page revisions, 'oldimage' for file versions, or a comma-separated list of log_ids for log entries.
+			// $subtype here is 'revision' for page revisions and file versions, or 'event' for log entries.
 			if (
-				($subtype === 'event' && count( $params ) === 6 ) ||
-				($subtype === 'revision' && isset( $params[3] ) && $params[3] === 'revision' )
+				( $subtype === 'event' && count( $params ) === 6 ) ||
+				( $subtype === 'revision' && isset( $params[3] ) && ( $params[3] === 'revision' || $params[3] === 'oldimage' ) )
 			) {
 				$paramStart = $subtype === 'revision' ? 4 : 3;
 
