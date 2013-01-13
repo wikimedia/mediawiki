e42cb8c18804427fae0cd1a8525e3802992f14dc
Tim Starling <tstarling@wikimedia.org>
2013-01-14 09:48:03 +1100

    Implement wall clock time limits for shell commands
    * Use the "timeout" command to implement wall clock time limits.
    * Write a message to stderr when the time limit is exceeded, for
    consistency with ulimit
    * Configure with $wgMaxShellWallClockTime
    * If "time" is specified in $limits, use that as the wall clock limit in
    preference to the global variable, to support legacy callers which
    usually want to disable the limits.
    * Remove the requirement for ulimit5.sh to be executable since this is
    not necessary when you specify the interpreter. Should avoid subtle
    problems when the executable bit is accidentally stripped.
    * Move the interpretation of 0/-1/false limits to the shell script, so
    that e.g. wall clock time can be limited even if memory is unlimited
    
    Change-Id: Id9f2fea7c3b027565bdc33b88a1a7d4cdfe43a6c

---
+++ bin/ulimit5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" -gt 0 ]; then
+	ulimit -t "$1"
+fi
+if [ "$2" -gt 0 ]; then
+	ulimit -v "$2"
+fi
+if [ "$3" -gt 0 ]; then
+	ulimit -f "$3"
+fi
+if [ "$4" -gt 0 ]; then
+	timeout $4 /bin/bash -c "$5"
+	STATUS="$?"
+	if [ "$STATUS" == 124 ]; then
+		echo "ulimit5.sh: timed out." 1>&2
+	fi
+	exit "$STATUS"
+else
+	eval "$5"
+fi
