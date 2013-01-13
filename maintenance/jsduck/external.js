a5ca2960bcee722727ad42ec11ef66a43b6d27db
Timo Tijhof <ttijhof@wikimedia.org>
2013-01-13 01:47:00 +0100

    Initial JSDuck implementation
    * Adding configuration files for jsduck.
    * Fixing our previously undocumented code documentation to comply
      with jsduck specifications
      .. for the following modules:
      - mediawiki
      - mediawiki.Title
      - mediawiki.util
      - mediawiki.notification
      - mediawiki.api (and plugins thereof)
      - jquery.localize
    
    I've choose these to show as many different examples for
    different types of modules and methods that we encounter
    in our code base.
    
    Hopefully with this as a start it will be easy for other people
    to extend the documentation for most (if not, all) other modules
    we have in MediaWiki core.
    
    Change-Id: Ieb8c5d2d2cb4672f1d6abc3f865c6fb1470d8feb

---
+++ maintenance/jsduck/external.js
@@ -0,0 +1,26 @@
+/**
+ * @class jQuery
+ */
+
+/**
+ * @method ajax
+ * @return {jqXHR}
+ */
+
+/**
+ * @class jQuery.Event
+ */
+
+/**
+ * @class jQuery.Promise
+ */
+
+/**
+ * @class jQuery.Deferred
+ * @mixins jQuery.Promise
+ */
+
+/**
+ * @class jQuery.jqXHR
+ * @alternateClassName jqXHR
+ */
