7ed9983ca8e4dc89641550c608935e3e5603f78a
Alex Monk <krenair@gmail.com>
2013-01-13 16:53:29 +0000

    (bug 43913) Create a maint script to fix remote user rights logs for the current wiki
    Untested because I don't have any of these logs.
    
    Change-Id: Ie13ccb412c91bf88431fa514032537bdfc317d81

---
+++ maintenance/consolidateRemoteLocalUserRightLogs.php
@@ -0,0 +1,35 @@
+<?php
+require_once( __DIR__ . '/Maintenance.php' );
+
+class ConsolidateRemoteLocalUserRightLogs extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Fix remote user rights logs for the local wiki. E.g. User:Krenair@currentwiki -> User:Krenair";
+	}
+
+	public function execute() {
+		global $wgUserrightsInterwikiDelimiter;
+		$dbr = wfGetDB( DB_SLAVE );
+		$result = $dbr->select(
+			'logging',
+			array( 'log_id', 'log_title' ),
+			array( 'log_title' . $dbr->buildLike( $dbr->anyString(), $wgUserrightsInterwikiDelimiter . wfWikiID() ) ),
+			__METHOD__
+		);
+
+		foreach ( $result as $row ) {
+			list( $name, $database ) = explode( $wgUserrightsInterwikiDelimiter, $row->log_title );
+
+			$this->output( "Changed '{$row->log_title}' to '{$name}'.\n" );
+			wfGetDB( DB_MASTER )->update(
+				'logging',
+				array( 'log_title' => $name ),
+				array( 'log_id' => $row->log_id ),
+				__METHOD__
+			);
+		}
+	}
+}
+
+$maintClass = "ConsolidateRemoteLocalUserRightLogs";
+require_once( RUN_MAINTENANCE_IF_MAIN );
