6d3a46a8a4fb4504d119482ecbb0d2972d13e409
Brian Wolff <bawolff+wn@gmail.com>
2013-01-15 17:09:46 -0400

    (bug 29788) Swedish Collation (uppercase-sv). Swaps Ä and Æ
    See I4542f57a. Meant as a temporary meassure until such a time
    generic tailoring code is implemented for uca. This patch
    is mostly Lejonel's code, with the class renamed.
    
    Change-Id: Id39406c37a5277d9e7a9216544de2140411c2b01

--- includes/Collation.php
+++ includes/Collation.php
@@ -43,6 +43,8 @@ abstract class Collation {
 		switch( $collationName ) {
 			case 'uppercase':
 				return new UppercaseCollation;
+			case 'uppercase-sv':
+				return new UppercaseSvCollation;
 			case 'identity':
 				return new IdentityCollation;
 			case 'uca-default':
@@ -118,6 +120,27 @@ class UppercaseCollation extends Collation {
 			$string = substr( $string, 1 );
 		}
 		return $this->lang->ucfirst( $this->lang->firstChar( $string ) );
+	}
+}
+
+/**
+ * Like UppercaseCollation but swaps Ä and Æ.
+ *
+ * This provides an ordering suitable for Swedish.
+ * @author Lejonel
+ */
+class UppercaseSvCollation extends UppercaseCollation {
+
+	/* Unicode code point order is ÄÅÆÖ, Swedish order is ÅÄÖ and Æ is often sorted as Ä.
+	 * Replacing Ä for Æ should give a better collation. */
+	function getSortKey( $string ) {
+		$uppercase = $this->lang->uc( $string );
+		// We use \x1A as temporary placeholder character, so
+		// that we can replace the Æ -> Ä without interfering
+		// with the Ä -> Æ replacement we just did.
+		// \x1A was chosen arbitrarily as a control character
+		// that really should never appear in text.
+		return str_replace( array( "Ä", "Æ", "\x1A" ), array( "\x1A", "Ä", "Æ" ), $uppercase );
 	}
 }
 
