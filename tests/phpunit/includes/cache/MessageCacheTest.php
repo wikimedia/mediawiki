c916461254772363baab9de4cffb32268cc8f62e
Tyler Romeo <tylerromeo@gmail.com>
2013-01-15 23:28:54 -0800

    Enable on-wiki message language fallbacks
    The core function behind wfMessage() (MessageCache->get()) did not
    apply the language fallback chain to on-wiki messages.
    
    This patch has changed the behavior to iterate over all possible
    on-wiki fallbacks (starting with the user's language) before
    using the built-in language cache (CDB files). Previously we only looked
    for the existence of an on-wiki message in the users's language.
    
    This patch was taken from the following changes and then
    adjusted to fix issues that caused bug 46579.
    
    * Change-Id: Iaaf6ccebd8c40c9602748c58c3a5c73c29e7aa4d
    - Author: Matthew Walker <mwalker@wikimedia.org>
    - (cherry picked from commit d434bfcf3bbab05660ed8f798a4622487dd8ba56)
    * Change-Id: Ib607a446d3499a3c042dce408db5cbaf12fa9e3d
    - Author: Mormegil <mormegil@centrum.cz>
    - (cherry picked from commit 1b8cb8dc3119bfb12d86d2f044018dc12553939b)
    
    Bug: 1495
    Bug: 46579
    Change-Id: I420457863eeb79824698d06abc7784032b267af2

---
+++ tests/phpunit/includes/cache/MessageCacheTest.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * @group Database
+ * @group Cache
+ */
+class MessageCacheTest extends MediaWikiLangTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		$this->configureLanguages();
+		MessageCache::singleton()->enable();
+	}
+
+	/**
+	 * Helper function -- setup site language for testing
+	 */
+	protected function configureLanguages() {
+		// for the test, we need the content language to be anything but English,
+		// let's choose e.g. German (de)
+		$langCode = 'de';
+		$langObj = Language::factory( $langCode );
+
+		$this->setMwGlobals( array(
+			'wgLanguageCode' => $langCode,
+			'wgLang' => $langObj,
+			'wgContLang' => $langObj,
+		) );
+	}
+
+	function addDBData() {
+		$this->configureLanguages();
+
+		// Set up messages and fallbacks ab -> ru -> de -> en
+		$this->makePage( 'FallbackLanguageTest-Full', 'ab' );
+		$this->makePage( 'FallbackLanguageTest-Full', 'ru' );
+		$this->makePage( 'FallbackLanguageTest-Full', 'de' );
+		$this->makePage( 'FallbackLanguageTest-Full', 'en' );
+
+		// Add customizations for an existing message.
+		$this->makePage( 'sunday', 'en' );
+
+		// Fallbacks where ab does not exist
+		$this->makePage( 'FallbackLanguageTest-Partial', 'ru' );
+		$this->makePage( 'FallbackLanguageTest-Partial', 'de' );
+		$this->makePage( 'FallbackLanguageTest-Partial', 'en' );
+
+		// Fallback to the content language
+		$this->makePage( 'FallbackLanguageTest-ContLang', 'de' );
+		$this->makePage( 'FallbackLanguageTest-ContLang', 'en' );
+
+		// Fallback to english
+		$this->makePage( 'FallbackLanguageTest-English', 'en' );
+
+		// Full key tests -- always want russian
+		$this->makePage( 'MessageCacheTest-FullKeyTest', 'ab' );
+		$this->makePage( 'MessageCacheTest-FullKeyTest', 'ru' );
+	}
+
+	/**
+	 * Helper function for addDBData -- adds a simple page to the database
+	 *
+	 * @param string $title Title of page to be created
+	 * @param string $lang  Language and content of the created page
+	 */
+	protected function makePage( $title, $lang ) {
+		global $wgContLang;
+
+		$title = Title::newFromText(
+			($lang == $wgContLang->getCode()) ? $title : "$title/$lang",
+			NS_MEDIAWIKI
+		);
+		$wikiPage = new WikiPage( $title );
+		$content = ContentHandler::makeContent( $lang, $title );
+		$wikiPage->doEditContent( $content, "$lang translation test case" );
+	}
+
+	/**
+	 * Test message fallbacks, bug #1495
+	 *
+	 * @dataProvider provideMessagesForFallback
+	 */
+	function testMessageFallbacks( $message, $lang, $expectedContent ) {
+		$result = MessageCache::singleton()->get( $message, true, $lang );
+		$this->assertEquals( $expectedContent, $result, "Message fallback failed." );
+	}
+
+	function provideMessagesForFallback() {
+		return array(
+			array( 'FallbackLanguageTest-Full', 'ab', 'ab' ),
+			array( 'FallbackLanguageTest-Partial', 'ab', 'ru' ),
+			array( 'FallbackLanguageTest-ContLang', 'ab', 'de' ),
+			array( 'FallbackLanguageTest-English', 'ab', 'en' ),
+			array( 'FallbackLanguageTest-None', 'ab', false ),
+			// Existing message with customizations on the fallbacks
+			array( 'sunday', 'ab', 'амҽыш' )
+		);
+	}
+
+	/**
+	 * There's a fallback case where the message key is given as fully qualified -- this
+	 * should ignore the passed $lang and use the language from the key
+	 *
+	 * @dataProvider provideMessagesForFullKeys
+	 */
+	function testFullKeyBehaviour( $message, $lang, $expectedContent ) {
+		$result = MessageCache::singleton()->get( $message, true, $lang, true );
+		$this->assertEquals( $expectedContent, $result, "Full key message fallback failed." );
+	}
+
+	function provideMessagesForFullKeys() {
+		return array(
+			array( 'MessageCacheTest-FullKeyTest/ru', 'ru', 'ru' ),
+			array( 'MessageCacheTest-FullKeyTest/ru', 'ab', 'ru' ),
+			array( 'MessageCacheTest-FullKeyTest/ru/foo', 'ru', false ),
+		);
+	}
+
+}
