9220c668b02e2584a88dec3c14d67afd3ddca7e4
Aarti Dwivedi <aarti@rt-subzero-condensate.(none)>
2013-01-16 18:52:05 +0530

    (bug 42004) Enable subpages per default on Project and Help namespaces.
    Change-Id: Ia722438057999c4146a7fe6618ff4dc31781fd74

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3217,11 +3217,13 @@ $wgNamespacesWithSubpages = array(
 	NS_TALK           => true,
 	NS_USER           => true,
 	NS_USER_TALK      => true,
+	NS_PROJECT        => true,
 	NS_PROJECT_TALK   => true,
 	NS_FILE_TALK      => true,
 	NS_MEDIAWIKI      => true,
 	NS_MEDIAWIKI_TALK => true,
 	NS_TEMPLATE_TALK  => true,
+	NS_HELP           => true,
 	NS_HELP_TALK      => true,
 	NS_CATEGORY_TALK  => true
 );
