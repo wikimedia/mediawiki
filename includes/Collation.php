2c3f1709abbd04363ca5325c6113ba2336ae2c77
MatmaRex <matma.rex@gmail.com>
2013-01-18 22:36:32 +0100

    (bug 43801) add getters for intl and ICU version to ICUCollation
    They will be necessary to be able to use correct version of Unicode
    data files.
    
    The getters are useful, since the constant INTL_ICU_VERSION appears
    to be completely undocumented anywhere.
    
    Change-Id: Iee4b8380406ae71c980dfdd7b9fdd0b58ecb9cd0

--- includes/Collation.php
+++ includes/Collation.php
@@ -374,5 +374,24 @@ class IcuCollation extends Collation {
 		}
 		return false;
 	}
+
+	/**
+	 * Return the version of PHP's intl extension or false when it is missing.
+	 *
+	 * @return string|false
+	 */
+	static function getIntlVersion() {
+		return phpversion( "intl" );
+	}
+
+	/**
+	 * Return the version of ICU library used by PHP's intl extension,
+	 * or false when it is missing or the version in use can't be determined.
+	 *
+	 * @return string|false
+	 */
+	static function getICUVersion() {
+		return defined( "INTL_ICU_VERSION" ) ? INTL_ICU_VERSION : false;
+	}
 }
 
