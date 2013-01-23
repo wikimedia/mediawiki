dfc4acb1b317897a34eb13f65f6dd750e34ac1e5
Kaldari <rkaldari@wikimedia.org>
2013-01-22 18:16:13 -0800

    Allow extensions to define the human readable timestamp via a hook
    Change-Id: Ib316764a2213a1aa24004e42ca5cd11ef9891b81

--- tests/phpunit/includes/TimestampTest.php
+++ tests/phpunit/includes/TimestampTest.php
@@ -54,11 +54,11 @@ class TimestampTest extends MediaWikiTestCase {
 	 */
 	function testHumanOutput() {
 		$timestamp = new MWTimestamp( time() - 3600 );
-		$this->assertEquals( "1 hour ago", $timestamp->getHumanTimestamp()->inLanguage( 'en' )->text() );
-		$timestamp = new MWTimestamp( time() - 5184000 );
-		$this->assertEquals( "2 months ago", $timestamp->getHumanTimestamp()->inLanguage( 'en' )->text() );
-		$timestamp = new MWTimestamp( time() - 31536000 );
-		$this->assertEquals( "1 year ago", $timestamp->getHumanTimestamp()->inLanguage( 'en' )->text() );
+		$this->assertEquals( "1 hour ago", $timestamp->getHumanTimestamp() );
+		$timestamp = new MWTimestamp( time() - 5356000 );
+		$this->assertEquals( "2 months ago", $timestamp->getHumanTimestamp() );
+		$timestamp = new MWTimestamp( time() - 31622000 );
+		$this->assertEquals( "1 year ago", $timestamp->getHumanTimestamp() );
 	}
 
 	/**
