6f457fa352d443452d491f66af0fee1733c69c90
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-01-24 16:14:21 -0500

    Refactor MWTimestamp::getHumanTimestamp and add hook.
    Changed logic in MWTimestamp::getHumanTimestamp so that
    all the message and formatting was offloaded into the
    Language class, keeping only actual timestamp logic in
    the MWTimestamp class.
    
    Also added a hook so extensions can override the
    human timestamp format.
    
    Change-Id: Ie667088010e24eb6cb569f9e8e8e2553005223eb

--- includes/Timestamp.php
+++ includes/Timestamp.php
@@ -46,26 +46,12 @@ class MWTimestamp {
 	);
 
 	/**
-	 * Different units for human readable timestamps.
-	 * @see MWTimestamp::getHumanTimestamp
-	 */
-	private static $units = array(
-		"milliseconds" => 1,
-		"seconds" => 1000, // 1000 milliseconds per second
-		"minutes" => 60, // 60 seconds per minute
-		"hours" => 60, // 60 minutes per hour
-		"days" => 24, // 24 hours per day
-		"months" => 30, // approximately 30 days per month
-		"years" => 12, // 12 months per year
-	);
-
-	/**
 	 * The actual timestamp being wrapped. Either a DateTime
 	 * object or a string with a Unix timestamp depending on
 	 * PHP.
-	 * @var string|DateTime
+	 * @var DateTime
 	 */
-	private $timestamp;
+	public $timestamp;
 
 	/**
 	 * Make a new timestamp and set it to the specified time,
@@ -171,16 +157,7 @@ class MWTimestamp {
 			throw new TimestampException( __METHOD__ . ' : Illegal timestamp output type.' );
 		}
 
-		if( is_object( $this->timestamp  ) ) {
-			// DateTime object was used, call DateTime::format.
-			$output = $this->timestamp->format( self::$formats[$style] );
-		} elseif( TS_UNIX == $style ) {
-			// Unix timestamp was used and is wanted, just return it.
-			$output = $this->timestamp;
-		} else {
-			// Unix timestamp was used, use gmdate().
-			$output = gmdate( self::$formats[$style], $this->timestamp );
-		}
+		$output = $this->timestamp->format( self::$formats[$style] );
 
 		if ( ( $style == TS_RFC2822 ) || ( $style == TS_POSTGRES ) ) {
 			$output .= ' GMT';
@@ -198,30 +175,37 @@ class MWTimestamp {
 	 *
 	 * @since 1.20
 	 *
-	 * @return Message Formatted timestamp
+	 * @param MWTimestamp $relativeTo The base timestamp to compare to (defaults to now)
+	 * @param User $user User the timestamp is being generated for
+	 * @param Language $lang Language to use to make the human timestamp
+	 * @return string Formatted timestamp
 	 */
-	public function getHumanTimestamp() {
-		$then = $this->getTimestamp( TS_UNIX );
-		$now = time();
-		$timeago = ($now - $then) * 1000;
-		$message = false;
-
-		foreach( self::$units as $unit => $factor ) {
-			$next = $timeago / $factor;
-			if( $next < 1 ) {
-				break;
-			} else {
-				$timeago = $next;
-				$message = array( $unit, floor( $timeago ) );
-			}
+	public function getHumanTimestamp( MWTimestamp $relativeTo = null, User $user = null, Language $lang = null ) {
+		if ( $relativeTo === null ) {
+			$relativeTo = new self;
+		}
+		if ( $user === null ) {
+			$user = RequestContext::getMain()->getUser();
+		}
+		if ( $lang === null ) {
+			$lang = RequestContext::getMain()->getLanguage();
 		}
 
-		if( $message ) {
-			$initial = call_user_func_array( 'wfMessage', $message );
-			return wfMessage( 'ago', $initial->parse() );
-		} else {
-			return wfMessage( 'just-now' );
+		// Adjust for the user's timezone.
+		$adj = $lang->timestampUserAdjust( $this, $user );
+		$lang->timestampUserAdjust( $relativeTo, $user );
+
+		$diff = $this->diff( $relativeTo );
+		$ts = '';
+		if ( wfRunHooks( 'GetHumanTimestamp', array( &$ts, $this, $relativeTo, $diff, $user ) ) ) {
+			$ts = $lang->getHumanTimestamp( $this, $relativeTo, $diff, $user );
 		}
+
+		// Reset the timezone on the objects.
+		$this->timestamp->sub( $adj );
+		$relativeTo->timestamp->sub( $adj );
+
+		return $ts;
 	}
 
 	/**
@@ -231,6 +215,17 @@ class MWTimestamp {
 	 */
 	public function __toString() {
 		return $this->getTimestamp();
+	}
+
+	/**
+	 * Calculate the difference between two MWTimestamp objects.
+	 *
+	 * @since 1.21
+	 * @param MWTimestamp Relative current time
+	 * @return DateInterval Difference
+	 */
+	public function diff( MWTimestamp $relativeTo ) {
+		return $this->timestamp->diff( $relativeTo->timestamp );
 	}
 }
 
