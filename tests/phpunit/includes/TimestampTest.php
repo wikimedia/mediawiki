fa84729e54d2256c5b352e096f8a23c049a307d5
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-01-24 17:38:36 -0500

    Added new MWTimestamp::getRelativeTimestamp for pure relative.
    Since MWTimestamp::getHumanTimestamp just makes a pretty
    timestamp, and not a purely relative one, this commit adds
    MWTimestamp::getRelativeTimestamp, which does just that.
    
    Change-Id: I8b3e3fc6eeb4afd58f85fa7d05b5ea1a51b0afb6

--- tests/phpunit/includes/TimestampTest.php
+++ tests/phpunit/includes/TimestampTest.php
@@ -200,4 +200,101 @@ class TimestampTest extends MediaWikiLangTestCase {
 			),
 		);
 	}
+
+	/**
+	 * @test
+	 * @dataProvider provideRelativeTimestampTests
+	 */
+	public function testRelativeTimestamp(
+		$tsTime, // The timestamp to format
+		$currentTime, // The time to consider "now"
+		$timeCorrection, // The time offset to use
+		$dateFormat, // The date preference to use
+		$expectedOutput, // The expected output
+		$desc // Description
+	) {
+		$user = $this->getMock( 'User' );
+		$user->expects( $this->any() )
+			->method( 'getOption' )
+			->with( 'timecorrection' )
+			->will( $this->returnValue( $timeCorrection ) );
+
+		$tsTime = new MWTimestamp( $tsTime );
+		$currentTime = new MWTimestamp( $currentTime );
+
+		$this->assertEquals(
+			$expectedOutput,
+			$tsTime->getRelativeTimestamp( $currentTime, $user ),
+			$desc
+		);
+	}
+
+	public static function provideRelativeTimestampTests() {
+		return array(
+			array(
+				'20111231170000',
+				'20120101000000',
+				'Offset|0',
+				'mdy',
+				'7 hours ago',
+				'"Yesterday" across years',
+			),
+			array(
+				'20120717190900',
+				'20120717190929',
+				'Offset|0',
+				'mdy',
+				'29 seconds ago',
+				'"Just now"',
+			),
+			array(
+				'20120717190900',
+				'20120717191530',
+				'Offset|0',
+				'mdy',
+				'6 minutes and 30 seconds ago',
+				'Combination of multiple units',
+			),
+			array(
+				'20121006173100',
+				'20121006173200',
+				'Offset|0',
+				'mdy',
+				'1 minute ago',
+				'"1 minute ago"',
+			),
+			array(
+				'19910130151500',
+				'20120716193700',
+				'Offset|0',
+				'mdy',
+				'2 decades, 1 year, 168 days, 2 hours, 8 minutes and 48 seconds ago',
+				'A long time ago',
+			),
+			array(
+				'20120101050000',
+				'20120101080000',
+				'Offset|-360',
+				'mdy',
+				'3 hours ago',
+				'"Yesterday" across years with time correction',
+			),
+			array(
+				'20120714184300',
+				'20120716184300',
+				'Offset|-420',
+				'mdy',
+				'2 days ago',
+				'Recent weekday with time correction',
+			),
+			array(
+				'20120714184300',
+				'20120715040000',
+				'Offset|-420',
+				'mdy',
+				'9 hours and 17 minutes ago',
+				'Today at another time with time correction',
+			),
+		);
+	}
 }
