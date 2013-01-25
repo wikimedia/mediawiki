5c843330f8cb13ed0c6197b6b9361a6753f9e9b4
Aaron Schulz <aschulz@wikimedia.org>
2013-01-25 13:24:17 -0800

    (bug 44044) Check for existence of the timeout script in ulimit5.sh
    Change-Id: I89d5dd6535a6b37cadcafd12affaad1a82af22ba

--- bin/ulimit5.sh
+++ bin/ulimit5.sh
@@ -9,8 +9,8 @@ fi
 if [ "$3" -gt 0 ]; then
 	ulimit -f "$3"
 fi
-if [ "$4" -gt 0 ]; then
-	timeout $4 /bin/bash -c "$5"
+if [ "$4" -gt 0 -a -x "/usr/bin/timeout" ]; then
+	/usr/bin/timeout $4 /bin/bash -c "$5"
 	STATUS="$?"
 	if [ "$STATUS" == 124 ]; then
 		echo "ulimit5.sh: timed out." 1>&2
