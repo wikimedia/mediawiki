77ae02cebf73945f4171b1fd6a24432890d5a8a1
Aaron Schulz <aschulz@wikimedia.org>
2013-01-30 20:14:56 -0800

    [JobQueue] Re-try jobs that fail normally.
    * Jobs will be re-attempted if run() returns false.
      This means that claimTTL is useful beyond just the case
      where runners get killed in the middle of a job or an
      uncaught exception is thrown.
    * Moved MAX_ATTEMPTS constant up to base class.
    * Clarified docs a bit.
    
    Change-Id: Id7f970e82a63aa563e9a7a023ce32e5d6680433a

--- includes/job/JobQueueDB.php
+++ includes/job/JobQueueDB.php
@@ -31,7 +31,6 @@ class JobQueueDB extends JobQueue {
 	const CACHE_TTL_SHORT = 30; // integer; seconds to cache info without re-validating
 	const CACHE_TTL_LONG  = 300; // integer; seconds to cache info that is kept up to date
 	const MAX_AGE_PRUNE   = 604800; // integer; seconds a job can live once claimed
-	const MAX_ATTEMPTS    = 3; // integer; number of times to try a job
 	const MAX_JOB_RANDOM  = 2147483647; // integer; 2^31 - 1, used for job_random
 	const MAX_OFFSET      = 255; // integer; maximum number of rows to skip
 
