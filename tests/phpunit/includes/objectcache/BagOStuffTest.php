d84c3743d46ae757f2909bda4bacc9174badb14f
Matthias Mullie <git@mullie.eu>
2013-02-04 17:54:53 +0100

    Read full memcached response before manipulating data
    Memcached response when fetching data typically looks like this:
    VALUE <the stored value for whatever key you requested>
    END
    
    What the code used to do is read the first line (the VALUE) and re-
    assemble the data being fetched there (like unserializing serialized
    data). After that, it will read the next line (END).
    
    The value could be a serialized object, which could have a __wakeup.
    This __wakeup could have code which in turn executes Memcached-
    related stuff. The problem is that, while that object is being
    unserialized already, it's wakeup code is attempting to read new
    stuff from Memcached, but we have yet to read the END of the data
    we're attempting to unserialize (when we'll read a new value from
    Memcached, the first thing we'd get is the END we have not yet read..)
    
    The correct way to go about this would be to first read the full
    Memcached response, and only unserialize the read data after that.
    This is exactly what this patchset does.
    
    Change-Id: I902809c6dde657091c8161a09df823170bd41f7a

--- tests/phpunit/includes/objectcache/BagOStuffTest.php
+++ tests/phpunit/includes/objectcache/BagOStuffTest.php
@@ -15,6 +15,7 @@ class BagOStuffTest extends MediaWikiTestCase {
 			$name = $this->getCliArg( 'use-bagostuff=' );
 
 			$this->cache = ObjectCache::newFromId( $name );
+
 		} else {
 			// no type defined - use simple hash
 			$this->cache = new HashBagOStuff;
@@ -103,5 +104,35 @@ class BagOStuffTest extends MediaWikiTestCase {
 				exit;
 			}
 		}
+	}
+
+	public function testAdd() {
+		$key = wfMemcKey( 'test' );
+		$this->assertTrue( $this->cache->add( $key, 'test' ) );
+	}
+
+	public function testGet() {
+		$value = array( 'this' => 'is', 'a' => 'test' );
+
+		$key = wfMemcKey( 'test' );
+		$this->cache->add( $key, $value );
+		$this->assertEquals( $this->cache->get( $key ), $value );
+	}
+
+	public function testGetMulti() {
+		$value1 = array( 'this' => 'is', 'a' => 'test' );
+		$value2 = array( 'this' => 'is', 'another' => 'test' );
+
+		$key1 = wfMemcKey( 'test1' );
+		$key2 = wfMemcKey( 'test2' );
+
+		$this->cache->add( $key1, $value1 );
+		$this->cache->add( $key2, $value2 );
+
+		$this->assertEquals( $this->cache->getMulti( array( $key1, $key2 ) ), array( $key1 => $value1, $key2 => $value2 ) );
+
+		// cleanup
+		$this->cache->delete( $key1 );
+		$this->cache->delete( $key2 );
 	}
 }
