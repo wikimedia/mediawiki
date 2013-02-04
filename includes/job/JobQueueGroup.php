04e0d75f86b189a389e055b4764a951ba836ca82
Aaron Schulz <aschulz@wikimedia.org>
2013-02-03 16:28:52 -0800

    [JobQueue] Use target wiki configuration for some key functions.
    * getQueueTypes() now gets the config of the target wiki.
      Previously, for WMF, if an extension using jobs was not
      on aawiki, those job queues would not be seen by nextJobDB.
    * Also fixed nextJobDB.php so that it no longer only considers
      jobs types known to aawiki for picking a DB for default jobs.
    * Note that $wgJobTypesExcludedFromDefaultQueue should be global.
    * This adds a SiteConfiguration::getConfig() function, which calls
      a new getConfiguration.php script.
    
    Change-Id: I7e6904ead17efa407291f423a2b18e3c866d55fd

--- includes/job/JobQueueGroup.php
+++ includes/job/JobQueueGroup.php
@@ -200,9 +200,7 @@ class JobQueueGroup {
 	 * @return array List of strings
 	 */
 	public function getQueueTypes() {
-		global $wgJobClasses;
-
-		return array_keys( $wgJobClasses );
+		return array_keys( $this->getCachedConfigVar( 'wgJobClasses' ) );
 	}
 
 	/**
@@ -283,5 +281,24 @@ class JobQueueGroup {
 		} );
 
 		return $count;
+	}
+
+	private function getCachedConfigVar( $name ) {
+		global $wgConf, $wgMemc;
+
+		if ( $this->wiki === wfWikiID() ) {
+			return $GLOBALS[$name]; // common case
+		} else {
+			list( $db, $prefix ) = wfSplitWikiID( $this->wiki );
+			$key = wfForeignMemcKey( $db, $prefix, 'configvalue', $name );
+			$value = $wgMemc->get( $key ); // ('v' => ...) or false
+			if ( is_array( $value ) ) {
+				return $value['v'];
+			} else {
+				$value = $wgConf->getConfig( $this->wiki, $name );
+				$wgMemc->set( $key, array( 'v' => $value ), 86400 + mt_rand( 0, 86400 ) );
+				return $value;
+			}
+		}
 	}
 }
