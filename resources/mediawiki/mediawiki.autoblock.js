2d87019dd193cd650ad0856ba5561216b3d53b06
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-02-07 16:56:54 -0500

    (bug 3233) Send a cookie with autoblocks to prevent vandalism.
    Send a cookie with blocks that have autoblock turned on so that
    the user will be identified to MediaWiki and any IP they try
    to edit anonymously from will be blocked, even without logging
    in to the originally blocked account. Additionally, the block
    info is stored in local storage as well as an even stronger
    deterrence.
    
    Note: this is meant to deter normal vandals, i.e., not attackers
    who know what cookies and local storage are and will be actively
    removing the cookie.
    
    Change-Id: Ic3383af56c555c1592d272490ff4da683b9d7b1b

---
+++ resources/mediawiki/mediawiki.autoblock.js
@@ -0,0 +1,21 @@
+/**
+ * Checks local storage and cookies to see if user has recently
+ * been using a blocked account, and if such block should be sent
+ * to MediaWiki in order to trigger autoblocks.
+ */
+( function( $, mw ) {
+	var prefix = mw.config.get( 'wgCookiePrefix' )
+	if(
+		!$.cookie( prefix + 'BlockID' ) &&
+		typeof( Storage ) != "undefined" &&
+		localStorage.blockID
+	) {
+		// The block ID exists in storage, but not in the cookie.
+		$.cookie( prefix + 'BlockID', localStorage.blockID );
+		$.cookie( prefix + 'BlockHash', localStorage.blockHash );
+	} else if( $.cookie( prefix + 'BlockID' ) != '-1' ) {
+		// The block ID exists in the cookie, but not in storage.
+		localStorage.blockID = $.cookie( prefix + 'BlockID' );
+		localStorage.blockHash = $.cookie( prefix + 'BlockHash' );
+	}
+} )( jQuery, mediaWiki );
