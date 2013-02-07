8ee135b9b69d5a234f6d6611b26a3cb49e776a79
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-02-07 16:56:54 -0500

    (bug 3233) Send a cookie with autoblocks to prevent vandalism.
    Send a cookie with blocks that have autoblock turned on so that
    the user will be identified to MediaWiki and any IP they try
    to edit anonymously from will be blocked, even without logging
    in to the originally blocked account. Additionally, the block
    info is stored in local storage as well as an even stronger
    deterrence.
    
    Note: this is meant to deter normal vandals, i.e., not attackers
    who know what cookies and local storage are and will be actively
    removing the cookie.
    
    Change-Id: Ic3383af56c555c1592d272490ff4da683b9d7b1b

--- resources/mediawiki.page/mediawiki.page.startup.js
+++ resources/mediawiki.page/mediawiki.page.startup.js
@@ -8,6 +8,20 @@
 		.addClass('client-js' )
 		.removeClass( 'client-nojs' );
 
+	// Set necessary block indicators for autoblocks.
+	var prefix = mw.config.get( 'wgCookiePrefix' );
+	if ( typeof localStorage !== 'undefined' ) {
+		if ( !$.cookie( prefix + 'BlockID' ) && localStorage.blockID ) {
+			// The block ID exists in storage, but not in the cookie.
+			$.cookie( prefix + 'BlockID', localStorage.blockID );
+			$.cookie( prefix + 'BlockHash', localStorage.blockHash );
+		} else if ( $.cookie( prefix + 'BlockID' ) !== '-1' ) {
+			// The block ID exists in the cookie, but not in storage.
+			localStorage.blockID = $.cookie( prefix + 'BlockID' );
+			localStorage.blockHash = $.cookie( prefix + 'BlockHash' );
+		}
+	}
+
 	// Initialize utilities as soon as the document is ready (mw.util.$content,
 	// messageBoxNew, profile, tooltip access keys, Table of contents toggle, ..).
 	// Enqueued into domready from here instead of mediawiki.page.ready to ensure that it gets enqueued
