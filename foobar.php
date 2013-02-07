efd0741e57f1c38f1ebdb1fcbcc2d55fae1f3cab
Antoine Musso <hashar@free.fr>
2013-02-07 22:52:45 +0100

    Jenkins job validation (DO NOT SUBMIT)..
    Change-Id: I3c747d495f3a0e6dc01ff77047de6babc0e5fdeb

---
+++ foobar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Some file.
+ *
+ * I am sooo lame.
+ *
+ * @file
+ * @category Foobar
+ * @package DumbPackage
+ * @author John Doe
+ */
+
+/**
+ * This function does nothing.
+ *
+ * @param string $bar
+ */
+function foobar( $bar )
+{
+
+}
