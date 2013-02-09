d5d40f4c067d4946a3040e27156928fe97836c67
jeroendedauw <jeroendedauw@gmail.com>
2013-02-09 21:06:30 +0100

    Added DBConnectionProvider interface and LazyDBConnectionProvider implementation
    This interface allows for nice usage of simple wrappers around db connection info
    that can construct a connection from it or actually hold one. This enables one
    to have a store object that takes such a connection provider without it being
    forced to keep this connection open if it does not want to lose the required info
    to reconstruct one, which is esp important in multi host setup.
    
    This offers a more OOP alternative to the earlier introduced DBAccessBase class
    which forces inheritance and thus often not nicely usable.
    
    Change-Id: Ic8578946c4a50294643581d81f4ca38960c0b81e

---
+++ includes/db/DBConnectionProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Interface for database connection providers.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.21
+ *
+ * @file
+ * @ingroup Database
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+interface DBConnectionProvider {
+
+	/**
+	 * Returns the database connection.
+	 * Initialization of this connection is done if it was not already initialized.
+	 *
+	 * @since 1.21
+	 *
+	 * @return DatabaseBase
+	 */
+	public function getConnection();
+
+	/**
+	 * Releases the connection if doing so makes any sense resource wise.
+	 *
+	 * @since 1.21
+	 */
+	public function releaseConnection();
+
+}
