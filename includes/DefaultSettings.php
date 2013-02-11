98fddc3d7d3653bb9a1df09cf8315d05f76d5b3e
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-02-11 18:12:28 -0500

    (bug 26227) Notify user by email when password changed.
    Added notification feature that e-mails user when their
    password changes and then disables changing their e-mail
    for a certain amount of time. This prevents account hijacking
    where an attacker guesses a user's password, changes it,
    and then attempts to change the e-mail to prevent password
    recovery.
    
    Change-Id: I0e0708194702f7ee10d7380a54e5afc91b2a87bb

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3593,6 +3593,12 @@ $wgPasswordResetRoutes = array(
 );
 
 /**
+ * Timeout between when a user's password changes and when they are allowed to change
+ * their e-mail again.
+ */
+$wgPasswordEmailChangeTimeout = 3600 * 24 * 2;
+
+/**
  * Maximum number of Unicode characters in signature
  */
 $wgMaxSigChars = 255;
