679f4a043f32baf454e9a0f5bb1f28f397be88ad
Kevin Israel <pleasestand@live.com>
2013-02-20 21:30:45 -0500

    Combine JavaScript and JSON encoding logic
    This will help with improving human readability of JS and JSON objects
    encoded by both ResourceLoader and the API. Although I'm leaving the
    ResourceLoader changes for a separate patch, this patch does add some
    new functionality to the API, including a new "utf8" parameter.
    
    Wikimedia note: Do not deploy before change I717c464e to the
    Collection extension has been merged and deployed.
    
    Changes to FormatJson class:
    
    * Added escaping of '<', '>', and '&' by default to protect against XSS.
    * Removed unnecessary escaping of '/' and added an additional option to
      unescape non-ASCII characters (those above U+007F) as well.
    * Removed Services_JSON fallback encoder; added replacement PHP 5.3
      pretty printing code that uses a four-space indent as PHP 5.4 does.
    
    Changes to Xml class:
    
    * Defined Xml::encodeJsVar() in terms of FormatJson::encode()
      and added a pretty printing option. Also added a pretty printing
      option to Xml::encodeJsCall() as well.
    * Deprecated Xml::escapeJsString() and QuickTemplate::jstext();
      callers have to add quotes themselves, hence the escaping of
      both double quotes and apostrophes.
    
    Bug: 26818
    Change-Id: I1987190f1ba5bf41738e7bd611209706c1f6bb5c

--- tests/phpunit/includes/JsonTest.php
+++ tests/phpunit/includes/JsonTest.php
@@ -24,4 +24,128 @@ class JsonTest extends MediaWikiTestCase {
 			'Optional array'
 		);
 	}
+
+	/**
+	 * @dataProvider provideEncoderDefaultTestCases
+	 */
+	public function testEncoderDefaultMode( $from, $to ) {
+		$this->assertSame( $to, FormatJson::encode( $from ) );
+	}
+
+	/**
+	 * @dataProvider provideEncoderUtf8OkTestCases
+	 */
+	public function testEncoderUtf8OkMode( $from, $to ) {
+		$this->assertSame( $to, FormatJson::encode( $from, false, FormatJson::UTF8_OK ) );
+	}
+
+	/**
+	 * @dataProvider provideEncoderXmlMetaOkTestCases
+	 */
+	public function testEncoderXmlMetaOkMode( $from, $to ) {
+		$this->assertSame( $to, FormatJson::encode( $from, false, FormatJson::XMLMETA_OK ) );
+	}
+
+	/**
+	 * @dataProvider provideEncoderAllOkTestCases
+	 */
+	public function testEncoderAllOkMode( $from, $to ) {
+		$this->assertSame( $to, FormatJson::encode( $from, false, FormatJson::ALL_OK ) );
+	}
+
+	public static function provideEncoderDefaultTestCases() {
+		return self::provideEncoderTestCases( false, false );
+	}
+
+	public static function provideEncoderUtf8OkTestCases() {
+		return self::provideEncoderTestCases( false, true );
+	}
+
+	public static function provideEncoderXmlMetaOkTestCases() {
+		return self::provideEncoderTestCases( true, false );
+	}
+
+	public static function provideEncoderAllOkTestCases() {
+		return self::provideEncoderTestCases( true, true );
+	}
+
+	public static function provideEncoderTestCases( $X, $U ) {
+		$rules = array(
+			// Forward slash (always unescaped)
+			'/' => '/',
+
+			// Unicode characters
+			"\xc3\xa9" => $U ?: '\u00e9',
+			"\xf0\x9d\x92\x9e" => $U ?: '\ud835\udc9e', // U+1D49E, outside the BMP
+
+			// XML metacharacters
+			'<' => $X ?: '\u003C', // JSON_HEX_TAG uses uppercase hex digits
+			'>' => $X ?: '\u003E',
+			'&' => $X ?: '\u0026',
+
+			// Control characters
+			"\0" => '\u0000',
+			"\x08" => '\b',
+			"\t" => '\t',
+			"\n" => '\n',
+			"\r" => '\r',
+			"\f" => '\f',
+			"\x1f" => '\u001f', // representative example
+
+			// Double quotes
+			'"' => '\"',
+
+			// Backslashes
+			'\\' => '\\\\',
+			'\\\\' => '\\\\\\\\',
+			'\\u00e9' => '\\\u00e9', // security check for Unicode unescaping
+
+			// Line terminators
+			"\xe2\x80\xa8" => '\u2028',
+			"\xe2\x80\xa9" => '\u2029',
+		);
+
+		$cases = array();
+		foreach ( $rules as $from => $to ) {
+			$cases[] = array( $from, '"' . ( $to === true ? $from : $to ) . '"' );
+		}
+		return $cases;
+	}
+
+	public function testEncoderPrettyPrinting() {
+		$obj = array(
+			'emptyObject' => new stdClass,
+			'emptyArray' => array(),
+			'string' => 'foobar',
+			'filledArray' => array(
+				array(
+					123,
+					456,
+				),
+				'"7":["8",{"9":"10"}]',
+			),
+		);
+
+		$json = implode( "\n", array(
+			'{',
+			'    "emptyObject": {',
+			'', // no trailing whitespace
+			'    },',
+			'    "emptyArray": [',
+			'', // no trailing whitespace
+			'    ],',
+			'    "string": "foobar",',
+			'    "filledArray": [',
+			'        [',
+			'            123,',
+			'            456',
+			'        ],',
+			'        "\\"7\\":[\\"8\\",{\\"9\\":\\"10\\"}]"',
+			'    ]',
+			"}",
+			// no trailing linefeed
+		) );
+
+		$this->assertSame( $json, FormatJson::encode( $obj, true ) );
+	}
 }
