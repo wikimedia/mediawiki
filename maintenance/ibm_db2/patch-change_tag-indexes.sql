ae802745c9e4094ae5134888333746b9ce3a18f7
Chad Horohoe <chadh@wikimedia.org>
2013-02-25 10:55:50 -0800

    Remove DB2 support
    Change-Id: I9b294a213a4000f503c0010d88757db6dda927c0

--- maintenance/ibm_db2/patch-change_tag-indexes.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX change_tag_rc_tag ON change_tag (ct_rc_id,ct_tag);
-CREATE UNIQUE INDEX change_tag_log_tag ON change_tag (ct_log_id,ct_tag);
-CREATE UNIQUE INDEX change_tag_rev_tag ON change_tag (ct_rev_id,ct_tag);
--- Covering index, so we can pull all the info only out of the index.
-CREATE INDEX change_tag_tag_id ON change_tag (ct_tag,ct_rc_id,ct_rev_id,ct_log_id);
