01f39115089cfca45a01e853b1fd8689fe1eefa0
Alex Monk <krenair@gmail.com>
2013-03-03 22:51:27 +0000

    Remove the mediawiki.api.titleblacklist module
    Moved to the TitleBlacklist extension in I5bf7064a.
    
    Bug: 38244
    Change-Id: Ia595085371615da057aaa358d76929916a3e2186

--- resources/Resources.php
+++ resources/Resources.php
@@ -580,13 +580,6 @@ return array(
 		'scripts' => 'resources/mediawiki.api/mediawiki.api.parse.js',
 		'dependencies' => 'mediawiki.api',
 	),
-	'mediawiki.api.titleblacklist' => array(
-		'scripts' => 'resources/mediawiki.api/mediawiki.api.titleblacklist.js',
-		'dependencies' => array(
-			'mediawiki.api',
-			'mediawiki.Title',
-		),
-	),
 	'mediawiki.api.watch' => array(
 		'scripts' => 'resources/mediawiki.api/mediawiki.api.watch.js',
 		'dependencies' => array(
