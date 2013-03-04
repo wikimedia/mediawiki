b1d8b16e4b471f84b5b36073c39c8e2c49b8dad2
Trevor Parscal <tparscal@wikimedia.org>
2013-03-04 15:11:22 -0800

    Added mw-ui-button classes (Agora) to various buttons.
    DO NOT MERGE yet.  In progress.
    
    Change-Id: I63e073b420675b2882ab2717073d6a6d84179a7b

--- includes/specials/SpecialRevisiondelete.php
+++ includes/specials/SpecialRevisiondelete.php
@@ -420,7 +420,11 @@ class SpecialRevisionDelete extends UnlistedSpecialPage {
 					'<td></td>' .
 					'<td class="mw-submit">' .
 						Xml::submitButton( $this->msg( 'revdelete-submit', $numRevisions )->text(),
-							array( 'name' => 'wpSubmit' ) ) .
+							array(
+								'name' => 'wpSubmit',
+								'class' => 'mw-ui-button destructive'
+							)
+						) .
 					'</td>' .
 				"</tr>\n" .
 				Xml::closeElement( 'table' ) .
