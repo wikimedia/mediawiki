44a140c892b344d10b49906a75ad8dc98829583d
S Page <spage@wikimedia.org>
2013-03-04 19:13:39 -0800

    Soft-launch login form with new Agora appearance
    If a global variable is set or if you add ?useNew=1 to the query
    string, Special:Userlogin loads a different login template
    (UserloginVForm.php) with the new look and different messaging.
    Otherwise the current unchanged template renders so that wikis can cut
    over to the new look when ready (with messages and links). Once they do
    so we will retire the variable and flag.
    
    The new template applies mw-ui-vform and mw-ui-button styles defined
    in a new 'mediawiki.ui' CSS module in core (copied from Extension:Agora)
    to create a compact vertical form.
    
    In useNew mode, Special:Userlogin also:
    * Displays a "secure login" link if HTTPS is available.
    * Loads additional CSS for its form features (e.g. more attractive
      errorbox, "Join wiki" messaging).
    * Defines new "userlogin-xx" messages; many are the same as
      existing messages but without ':' on the end.
    * Uses a distinct title for Login instead of generic "Log in /
      Create account".
    * Outputs checkboxes using UserloginTemplateVForm::labelledCheck()
    * Removes the [mailmypassword] code branch from its login template as it
      is never executed.
    
    A known remaining work item is:
    * Only use the Helvetica font stack in the form area in the
      Vector skin.
    
    (An Agora version of the create account form will follow in a separate patch.)
    
    Bug: 44628
    Change-Id: I489042c50aa060c90ca18b05097dbe25c4ae6395

---
+++ includes/templates/UserloginVForm.php
@@ -0,0 +1,256 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Templates
+ */
+
+/**
+ * Parent class for the vertically-stacked template design that provides
+ * helper methods for some VForm-specific layouts.
+ * Inherit from BaseTemplate instead of QuickTemplate for its proper
+ * context-based msg() implementation.
+ */
+abstract class VFormTemplate extends BaseTemplate {
+
+	/**
+	 * Get the Skin object related to this object, so that BaseTemplate msg
+	 * methods work.
+	 *
+	 * @return Skin object
+	 */
+	public function getSkin() {
+		global $wgOut;
+		return $wgOut->getSkin();
+	}
+
+	/**
+	 * Convenience function to build a VForm HTML checkbox nested inside a
+	 * label.  This arguably belongs in class Html, but then VForm clients
+	 * would have to apply an VForm class to the label as well as attrs for the
+	 * checkbox.
+	 *
+	 * @param $label string text for label
+	 * @param $name string form element name
+	 * @param $id
+	 * @param $checked bool (default: false)
+	 * @param $attribs array additional attributes for the input checkbox
+	 *
+	 * @return string HTML
+	 * @see Xml:checkLabel
+	 */
+	public function labelledCheck( $label, $name, $id, $checked = false, $attribs = array() ) {
+		return Html::rawElement(
+				'label',
+				array(
+					'for' => $id,
+					'class' => 'mw-ui-checkbox-label'
+				),
+				Xml::check(
+					$id,
+					$checked,
+					array( 'id' => $id ) + $attribs
+				) .
+				// Html:rawElement doesn't escape contents.
+				htmlspecialchars( $label )
+			);
+	}
+
+}
+
+/**
+ * Html form for user login with new VForm appearance.
+ */
+
+class UserloginTemplateVForm extends VFormTemplate {
+
+	function execute() {
+?>
+<div class="mw-ui-container">
+	<?php
+	// Some extensions including CAPTCHAs add content to header on UserLoginForm hook.
+	$this->html('header');
+	if( $this->haveData( 'languages' ) ) {
+	?>
+		<div id="languagelinks">
+			<p><?php $this->html('languages' ); ?></p>
+		</div>
+	<?php
+	}
+	?>
+<div id="userloginForm">
+<form name="userlogin" class="mw-ui-vform" method="post" action="<?php $this->text('action') ?>">
+	<?php
+	// Some extensions including CAPTCHAs add content to header on UserLoginForm hook.
+	$this->html('header');
+	if( $this->haveData( 'languages' ) ) {
+	?>
+		<div id="languagelinks">
+			<p><?php $this->html('languages' ); ?></p>
+		</div>
+	<?php
+	}
+
+	if( $this->data['message'] ) {
+	?>
+		<div class="<?php $this->text('messagetype') ?>box">
+		<?php
+		if ( $this->data['messagetype'] == 'error' ) {
+		?>
+			<strong><?php $this->msg( 'loginerror' )?></strong><br />
+		<?php
+		}
+		$this->html('message')
+		?>
+		</div>
+	<?php
+	}
+	?>
+		<div>
+			<label for='wpName1'>
+				<?php
+				$this->msg( 'userlogin-yourname' );
+				if ( $this->data['secureLoginUrl'] ) {
+					echo Html::element( 'a', array(
+							'href' => $this->data['secureLoginUrl'],
+							'class' => 'mw-ui-flush-right mw-secure',
+						), $this->getMsg( 'userlogin-signwithsecure' )->text() );
+				} ?>
+			</label>
+			<?php
+			$extraAttrs = array();
+			// Set focus to this field if its blank.
+			if ( !$this->data['name'] ) {
+				$extraAttrs['autofocus'] = '';
+			}
+			echo Html::input( 'wpName', $this->data['name'], 'text', array(
+				'class' => 'loginText',
+				'id' => 'wpName1',
+				'tabindex' => '1',
+				'size' => '20',
+				// 'required' is blacklisted for now in Html.php due to browser issues.
+				// Keeping here in case that changes
+				'required',
+				'placeholder' => $this->getMsg( 'userlogin-yourname-ph' )->text()
+			) + $extraAttrs );
+			?>
+		</div>
+		<div>
+			<label for='wpPassword1'>
+			<?php
+			$this->msg('userlogin-yourpassword');
+
+			if ( $this->data['useemail'] && $this->data['canreset'] && $this->data['resetlink'] === true ) {
+				echo Linker::link(
+					SpecialPage::getTitleFor( 'PasswordReset' ),
+					$this->getMsg( 'userlogin-resetlink' )->parse(),
+					array( 'class' => 'mw-ui-flush-right' )
+					);
+				// TODO: remove the wpMailmypassword code branch from
+				// templates/Userlogin.php as well; it is never executed and
+				// doesn't work.
+			}
+			?>
+			</label>
+			<?php
+			$extraAttrs = array();
+			// Set focus to this field if username is filled in.
+			if ( $this->data['name'] ) {
+				$extraAttrs['autofocus'] = '';
+			}
+			echo Html::input( 'wpPassword', null, 'password', array(
+				'class' => 'loginPassword',
+				'id' => 'wpPassword1',
+				'tabindex' => '2',
+				'size' => '20',
+				'placeholder' => $this->getMsg( 'userlogin-yourpassword-ph' )->text()
+			) + $extraAttrs );
+			?>
+		</div>
+	<?php
+	if( isset( $this->data['usedomain'] ) && $this->data['usedomain'] ) {
+		$doms = "";
+		foreach( $this->data['domainnames'] as $dom ) {
+			$doms .= "<option>" . htmlspecialchars( $dom ) . "</option>";
+		}
+	?>
+		<div id="mw-user-domain-section">
+			<label for='wpDomain' class="pos-above"><?php $this->msg( 'yourdomain' ) ?></label>
+				<select name="wpDomain" value="<?php $this->text( 'domain' ) ?>"
+					tabindex="3">
+					<?php echo $doms ?>
+				</select>
+		</div>
+	<?php }
+
+	if( $this->haveData( 'extrafields' ) ) {
+		echo $this->data['extrafields'];
+	} ?>
+
+		<div>
+			<?php if( $this->data['canremember'] ) {
+				global $wgCookieExpiration;
+				$expirationDays = ceil( $wgCookieExpiration / ( 3600 * 24 ) );
+				echo $this->labelledCheck(
+					$this->getMsg( 'userlogin-remembermypassword' )->numParams( $expirationDays )->text(),
+					'wpRemember',
+					'wpRemember',
+					$this->data['remember'],
+					array ( 'tabindex' => 4 )
+				);
+			} ?>
+		</div>
+<?php if( $this->data['cansecurelogin'] ) { ?>
+		<div>
+			<?php
+			echo $this->labelledCheck(
+				$this->getMsg( 'securelogin-stick-https' )->text(),
+				'wpStickHTTPS',
+				'wpStickHTTPS',
+				$this->data['stickHTTPS'],
+				array( 'tabindex' => '5' )
+			);
+			?>
+		</div>
+<?php } ?>
+		<div>
+			<?php
+			echo Html::input( 'wpLoginAttempt', $this->getMsg( 'login' )->text(), 'submit', array(
+				'id' => 'wpLoginAttempt',
+				'tabindex' => '6',
+				'class' => 'mw-ui-button big block primary'
+			) );
+			?>
+		</div>
+		<div id="mw-userlogin-help">
+			<?php echo $this->getMsg( 'userlogin-helplink' )->parse() ?>
+		</div>
+		<?php if( $this->haveData( 'createOrLoginHref' ) ) { ?>
+			<div id="mw-createaccount-cta">
+				<h3 id="mw-userloginlink"><?php $this->msg( 'userlogin-noaccount' ) ?><a href="<?php $this->text( 'createOrLoginHref' ) ?>" id="mw-createaccount-join" class="mw-ui-button constructive" style="width: auto; display: inline-block;"><?php $this->msg( 'userlogin-joinproject' ) ?></a></h3>
+			</div>
+		<?php } ?>
+	<input type="hidden" id="mw-useNew" name="useNew" value="1" />
+<?php if( $this->haveData( 'uselang' ) ) { ?><input type="hidden" name="uselang" value="<?php $this->text( 'uselang' ); ?>" /><?php } ?>
+<?php if( $this->haveData( 'token' ) ) { ?><input type="hidden" name="wpLoginToken" value="<?php $this->text( 'token' ); ?>" /><?php } ?>
+</form>
+</div>
+</div>
+<?php
+
+} // end execute()
+}
