0a96a84116828c08d51aad723d99a39983dc5df8
lwelling <lwelling@wikimedia.org>
2013-03-07 03:40:35 -0500

    Allow job queues to optionally request jobs not be processed immediately Jobs can be given either a notBefore timestamp, or a delay from now in seconds
    Most jobs don't need this, so unless the queue specifically has delays enabled,
    the not_before column is not joined so performance of normal queues should be unaffected
    
    Commit includes DB patches and unit tests for the new job behavior
    
    Change-Id: I05df4efe7c82b8785b47b5e384879bdf6e4edb7b

---
+++ maintenance/archives/patch-delayed-job-queue.sql
@@ -0,0 +1,10 @@
+--
+-- patch-delayed-queue.sql
+-- Adds a column to job to allow jobs to be deliberately delayed on a job queue
+-- 2013-2-27
+--
+
+ALTER TABLE /*$wgDBprefix*/job
+	ADD COLUMN `job_not_before` varbinary(14) NULL DEFAULT NULL;
+
+CREATE INDEX job_not_before ON /*$wgDBprefix*/job(`job_not_before`);
