020d3f070551d8a6ea979d1087fa34b0e7f0a6e8
Kaldari <rkaldari@wikimedia.org>
2013-03-06 18:38:00 -0800

    Adding hooks for thank you notification links
    These will allow Echo to add a 'thank' link next to the 'undo' links
    
    Change-Id: Ifb93e49b30bb325ecd17ece9f36bcd83c63a2563

--- includes/actions/HistoryAction.php
+++ includes/actions/HistoryAction.php
@@ -654,6 +654,8 @@ class HistoryPager extends ReverseChronologicalPager {
 				$tools[] = "<span class=\"mw-history-undo\">{$undolink}</span>";
 			}
 		}
+		// Allow extension to add their own links here
+		wfRunHooks( 'HistoryRevisionTools', array( $rev, &$tools ) );
 
 		if ( $tools ) {
 			$s2 .= ' '. $this->msg( 'parentheses' )->rawParams( $lang->pipeList( $tools ) )->escaped();
