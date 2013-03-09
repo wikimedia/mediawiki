4d58c20cedd7718a00d287b765605797dfea5262
Matt Walker <mwalker@wikimedia.org>
2013-03-08 17:30:49 -0800

    HTMLForm button behaviour modifications
    - Buttons can now be marked disabled and they will not be clickable.
    - The default submit button may now be opted out of.
    
    Change-Id: I8293a3c09da51152bfc7f6f6951416c71ce0bb38

--- includes/HTMLForm.php
+++ includes/HTMLForm.php
@@ -128,6 +128,7 @@ class HTMLForm extends ContextSource {
 
 	protected $mFieldTree;
 	protected $mShowReset = false;
+	protected $mShowSubmit = true;
 	public $mFieldData;
 
 	protected $mSubmitCallback;
@@ -699,7 +700,9 @@ class HTMLForm extends ContextSource {
 
 		$attribs['class'] = 'mw-htmlform-submit';
 
-		$html .= Xml::submitButton( $this->getSubmitText(), $attribs ) . "\n";
+		if ( $this->mShowSubmit ) {
+			$html .= Xml::submitButton( $this->getSubmitText(), $attribs ) . "\n";
+		}
 
 		if ( $this->mShowReset ) {
 			$html .= Html::element(
@@ -848,6 +851,21 @@ class HTMLForm extends ContextSource {
 	 */
 	function setSubmitID( $t ) {
 		$this->mSubmitID = $t;
+		return $this;
+	}
+
+	/**
+	 * Stop a default submit button being shown for this form. This implies that an
+	 * alternate submit method must be provided manually.
+	 *
+	 * @since 1.21
+	 *
+	 * @param bool $suppressSubmit Set to false to re-enable the button again
+	 *
+	 * @return HTMLForm $this for chaining calls
+	 */
+	function suppressDefaultSubmit( $suppressSubmit = true ) {
+		$this->mShowSubmit = !$suppressSubmit;
 		return $this;
 	}
 
@@ -2528,14 +2546,17 @@ class HTMLSubmitField extends HTMLFormField {
 	}
 
 	public function getInputHTML( $value ) {
-		return Xml::submitButton(
-			$value,
-			array(
-				'class' => 'mw-htmlform-submit ' . $this->mClass,
-				'name' => $this->mName,
-				'id' => $this->mID,
-			)
+		$attr = array(
+			'class' => 'mw-htmlform-submit ' . $this->mClass,
+			'name' => $this->mName,
+			'id' => $this->mID,
 		);
+
+		if ( !empty( $this->mParams['disabled'] ) ) {
+			$attr['disabled'] = 'disabled';
+		}
+
+		return Xml::submitButton( $value, $attr );
 	}
 
 	protected function needsLabel() {
