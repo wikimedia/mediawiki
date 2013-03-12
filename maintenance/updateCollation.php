1db83c1b763d664308dd73939fc1885bc543c2ea
Tim Starling <tstarling@wikimedia.org>
2013-03-13 10:18:12 +1100

    Restore SET cl_timestamp=cl_timestamp
    Apparently cl_timestamp=cl_timestamp is a workaround for obscure
    behaviour of the timestamp type in MySQL
    
    Change-Id: I803f20bcf4e28e8e2833a07bcf00e7edc00ad84b

--- maintenance/updateCollation.php
+++ maintenance/updateCollation.php
@@ -178,6 +178,7 @@ TEXT;
 							'cl_sortkey_prefix' => $prefix,
 							'cl_collation' => $collationName,
 							'cl_type' => $type,
+							'cl_timestamp = cl_timestamp',
 						),
 						array( 'cl_from' => $row->cl_from, 'cl_to' => $row->cl_to ),
 						__METHOD__
