ed4d4c58eb54f061257822b8691d979b9dc8d053
jarry1250 <jarry1250@gmail.com>
2013-03-14 18:00:22 +0000

    The language used to render SVGs should be definable
    See bug for context.
    
    The implementation is slightly untidy because I've written it so
    as to avoid invalidating the existing SVG thumbs -- there will be
    no immediate difference (visual/performance/other) as a result of
    this.
    
    Tested by me in both...
    * [[File:Example.svg|thumb|lang=fr]] AND
    * http://example.org/w/index.php?title=File:Example.svg&lang=fr
    ...modes. Example file on
    https://commons.wikimedia.org/wiki/File:Gerrit_patchset_25838_test.svg
    
    Added parser tests.
    
    Bug: 32987
    Change-Id: I4cadf96ecd5e169a88ad468a0478d355db980103

--- includes/media/MediaTransformOutput.php
+++ includes/media/MediaTransformOutput.php
@@ -32,7 +32,7 @@ abstract class MediaTransformOutput {
 	 */
 	var $file;
 
-	var $width, $height, $url, $page, $path;
+	var $width, $height, $url, $page, $path, $lang;
 
 	/**
 	 * @var array Associative array mapping optional supplementary image files
@@ -192,17 +192,21 @@ abstract class MediaTransformOutput {
 
 	/**
 	 * @param $title string
-	 * @param $params array
+	 * @param $params array|string
 	 * @return array
 	 */
 	public function getDescLinkAttribs( $title = null, $params = '' ) {
 		$query = '';
 		if ( $this->page && $this->page !== 1 ) {
-			$query = 'page=' . urlencode( $this->page );
+			wfAppendQuery( $query, 'page=' . urlencode( $this->page ) );
 		}
 		if( $params ) {
-			$query .= $query ? '&' . $params : $params;
+			wfAppendQuery( $query, $params );
 		}
+		if( $this->lang ) {
+			wfAppendQuery( $query, 'lang=' . urlencode( $this->lang ) );
+		}
+
 		$attribs = array(
 			'href' => $this->file->getTitle()->getLocalURL( $query ),
 			'class' => 'image',
@@ -239,7 +243,8 @@ class ThumbnailImage extends MediaTransformOutput {
 
 		if( is_array( $parameters ) ) {
 			$defaults = array(
-				'page' => false
+				'page' => false,
+				'lang' => false
 			);
 			$actualParams = $parameters + $defaults;
 		} else {
@@ -249,7 +254,7 @@ class ThumbnailImage extends MediaTransformOutput {
 				'width' => $path,
 				'height' => $parameters,
 				'page' => ( $numArgs > 5 ) ? func_get_arg( 5 ) : false
-			);
+			) + $defaults;
 			$path = ( $numArgs > 4 ) ? func_get_arg( 4 ) : false;
 		}
 
@@ -264,6 +269,7 @@ class ThumbnailImage extends MediaTransformOutput {
 		$this->height = round( $actualParams['height'] );
 
 		$this->page = $actualParams['page'];
+		$this->lang = $actualParams['lang'];
 	}
 
 	/**
