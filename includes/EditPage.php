ac98f3b231e0dff99eca8ab81c3d1ba63bfd4b69
Brad Jorsch <bjorsch@wikimedia.org>
2013-03-14 08:43:06 -0400

    Add limit report data on preview pages
    While we've long had the "NewPP limit report" hidden in an HTML comment,
    it is hard for users to find this as they're not likely to look for
    profiling information hidden in an HTML comment. Even for those aware of
    it, it's not particularly convenient to find.
    
    This changeset adds a table showing this information at the bottom of
    the page preview. It also adds the ability for this information to be
    added to the ParserOutput object in a structured manner, and various
    messages so the report can be localized for the end user.
    
    Note that, for backwards compatability, the default English messages are
    used for the "NewPP limit report" comment rather than the localized
    messages.
    
    Change-Id: Ie065c7b5a17bbf1aa484d0ae1f3ee0f5d41f8495

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2239,6 +2239,8 @@ class EditPage {
 		$wgOut->addHTML( Html::rawElement( 'div', array( 'class' => 'hiddencats' ),
 			Linker::formatHiddenCategories( $this->mArticle->getHiddenCategories() ) ) );
 
+		$wgOut->addHTML( self::getPreviewLimitReport( $this->mParserOutput ) );
+
 		if ( $this->isConflict ) {
 			try {
 				$this->showConflict();
@@ -2814,6 +2816,48 @@ HTML
 
 		return "<div id=\"editpage-copywarn\">\n" .
 			call_user_func_array( 'wfMessage', $copywarnMsg )->plain() . "\n</div>";
+	}
+
+	/**
+	 * Get the Limit report for page previews
+	 *
+	 * @since 1.21
+	 * @param ParserOutput $output ParserOutput object from the parse
+	 * @return string HTML
+	 */
+	public static function getPreviewLimitReport( ParserOutput $output ) {
+		$limitReport = Html::openElement( 'table', array(
+			'class' => 'preview-limit-report wikitable mw-collapsible mw-collapsed'
+		) ) .
+			Html::openElement( 'thead' ) .
+			Html::openElement( 'tr' ) .
+			Html::rawElement( 'th', array( 'colspan' => 2 ), wfMessage( 'limitreport-title' )->parse() ) .
+			Html::closeElement( 'tr' ) .
+			Html::closeElement( 'thead' ) .
+			Html::openElement( 'tbody' );
+
+		foreach ( $output->getLimitReportData() as $key => $value ) {
+			if ( wfRunHooks( 'ParserLimitReportFormat',
+				array( $key, $value, &$limitReport, true, true )
+			) ) {
+				$keyMsg = wfMessage( $key );
+				$valueMsg = wfMessage( array( "$key-value-html", "$key-value" ) );
+				if ( !$valueMsg->exists() ) {
+					$valueMsg = new RawMessage( '$1' );
+				}
+				if ( !$keyMsg->isDisabled() && !$valueMsg->isDisabled() ) {
+					$limitReport .= Html::openElement( 'tr' ) .
+						Html::rawElement( 'th', null, $keyMsg->parse() ) .
+						Html::rawElement( 'td', null, $valueMsg->params( $value )->parse() ) .
+						Html::closeElement( 'tr' );
+				}
+			}
+		}
+
+		$limitReport .= Html::closeElement( 'tbody' ) .
+			Html::closeElement( 'table' );
+
+		return $limitReport;
 	}
 
 	protected function showStandardInputs( &$tabindex = 2 ) {
