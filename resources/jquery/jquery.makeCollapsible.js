4257fca001bc308f93b68cb3b9fa0c4e09e770b4
MatmaRex <matma.rex@gmail.com>
2013-03-15 22:51:42 +0100

    (bug 43689) Make the templates/category lists on edit page collapsible
    * Implemented a trigger in jquery.makeCollapsible to make it possible
      to implement cookie persistence.
    
    * Copied icons from resources/mediawiki.special. I see no way to share
      them - other than just using '..' in relative paths, which seems to
      be frowned upon.
    
    * CSS is mostly copied from the Vector extension's version.
    
    * JavaScript is a rewrite of this functionality from scratch (although
      the look&feel is the same as in the Vector extension's version).
      Used the same cookie names to avoid any issues when migrating.
    
    Change-Id: I91a3704cb09b9e17dd8baa516ab6f8ee441b7467

--- resources/jquery/jquery.makeCollapsible.js
+++ resources/jquery/jquery.makeCollapsible.js
@@ -175,6 +175,11 @@
 			$textContainer.text( wasCollapsed ? collapseText : expandText );
 		}
 
+		// Trigger a custom event to allow callers to hook to the collapsing/expanding,
+		// allowing one to e.g. implement persistence via cookies
+		$collapsible.trigger( 'beforeCollapseExpand.mw-collapse', [wasCollapsed ? 'expand' : 'collapse'] );
+		$collapsible.trigger( wasCollapsed ? 'beforeExpand.mw-collapse' : 'beforeCollapse.mw-collapse' );
+
 		// And finally toggle the element state itself
 		toggleElement( $collapsible, wasCollapsed ? 'expand' : 'collapse', $toggle, options );
 	}
