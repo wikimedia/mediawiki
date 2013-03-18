7eabd10b399c0e06ab0c096c7686e7fb5c82d1dd
Reedy <Reedy reedy@wikimedia.org>
2013-03-18 16:54:00 +0000

    Applied patches to new WMF 1.21wmf12 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.21alpha';
+$wgVersion = '1.21wmf12';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
