64828cb03122c80d6df1284333b92a51d29cb0ce
Aaron Schulz <aschulz@wikimedia.org>
2013-03-18 16:16:51 -0700

    Added a PoolCounterWorkViaCallback convenience class.
    Change-Id: I1cbb2cc8a43eea2d28f38cfd2d3a4908dc7d0da9

--- includes/PoolCounter.php
+++ includes/PoolCounter.php
@@ -142,7 +142,7 @@ class PoolCounter_Stub extends PoolCounter {
 }
 
 /**
- * Handy class for dealing with PoolCounters using class members instead of callbacks.
+ * Class for dealing with PoolCounters using class members
  */
 abstract class PoolCounterWork {
 	protected $cacheable = false; //Does this override getCachedWork() ?
@@ -242,5 +242,75 @@ abstract class PoolCounterWork {
 
 	function __construct( $type, $key ) {
 		$this->poolCounter = PoolCounter::factory( $type, $key );
+	}
+}
+
+/**
+ * Convenience class for dealing with PoolCounters using callbacks
+ * @since 1.21
+ */
+class PoolCounterWorkViaCallback extends PoolCounterWork {
+	/** @var callable */
+	protected $doWork;
+	/** @var callable|null */
+	protected $doCachedWork;
+	/** @var callable|null */
+	protected $fallback;
+	/** @var callable|null */
+	protected $error;
+
+	/**
+	 * Build a PoolCounterWork class from a type, key, and callback map.
+	 *
+	 * The callback map must at least have a callback for the 'doWork' method.
+	 * Additionally, callbacks can be provided for the 'doCachedWork', 'fallback',
+	 * and 'error' methods. Methods without callbacks will be no-ops that return false.
+	 * If a 'doCachedWork' callback is provided, then execute() may wait for any prior
+	 * process in the pool to finish and reuse its cached result.
+	 *
+	 * @param string $type
+	 * @param string $key
+	 * @param array $callbacks Map of callbacks
+	 * @throws MWException
+	 */
+	public function __construct( $type, $key, array $callbacks ) {
+		parent::__construct( $type, $key );
+		foreach ( array( 'doWork', 'doCachedWork', 'fallback', 'error' ) as $name ) {
+			if ( isset( $callbacks[$name] ) ) {
+				if ( !is_callable( $callbacks[$name] ) ) {
+					throw new MWException( "Invalid callback provided for '$name' function." );
+				}
+				$this->$name = $callbacks[$name];
+			}
+		}
+		if ( !isset( $this->doWork ) ) {
+			throw new MWException( "No callback provided for 'doWork' function." );
+		}
+		$this->cacheable = isset( $this->doCachedWork );
+	}
+
+	public function doWork() {
+		return call_user_func_array( $this->doWork, array() );
+	}
+
+	public function getCachedWork() {
+		if ( $this->doCachedWork ) {
+			return call_user_func_array( $this->doCachedWork, array() );
+		}
+		return false;
+	}
+
+	function fallback() {
+		if ( $this->fallback ) {
+			return call_user_func_array( $this->fallback, array() );
+		}
+		return false;
+	}
+
+	function error( $status ) {
+		if ( $this->error ) {
+			return call_user_func_array( $this->error, array( $status ) );
+		}
+		return false;
 	}
 }
