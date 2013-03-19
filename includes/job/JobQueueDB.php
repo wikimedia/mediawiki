3cb6d690b30f73dc4a736d0b517dca8ba6dd4fe8
lwelling <lwelling@wikimedia.org>
2013-03-19 10:38:46 -0400

    Use consistent timestamps in JobQueueDB to cut risk of timezone confusion
    Change-Id: Ic486b585d579ea259b8e2c3f16820164702721fd

--- includes/job/JobQueueDB.php
+++ includes/job/JobQueueDB.php
@@ -412,7 +412,7 @@ class JobQueueDB extends JobQueue {
 	public function recycleAndDeleteStaleJobs() {
 		global $wgMemc;
 
-		$now = time();
+		$now = wfTimestampNow();
 		list( $dbw, $scope ) = $this->getMasterDB();
 		$count = 0; // affected rows
 
