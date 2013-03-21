43095296dc189373326523a19ffcc5c3f55b5976
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-03-21 11:37:25 +0200

    Update plural rules for Hebrew
    A recent CLDR update changed the plural rules for Hebrew
    and added a "many" form. That rule has a mistake, however -
    it is not supposed to include the number 10. This is reported as
    http://unicode.org/cldr/trac/ticket/5828
    
    This commit updates the plural overrides for Hebrew and makes
    them closer to the latest CLDR, but with a fix for that rule.
    
    It also updates the tests to include support for the new rule
    and to make sure that the right fallbacks are used when
    less than four rules are supplied, because usually that is
    the case. It is thus a partial revert of the changes introduced
    in I3d72e4105f6244b0695116940e62a2ddef66eb66 .
    
    Mingle-Task: 2715
    
    Change-Id: I1315e6900ef7a89bf246e748b786f7fc31a629c6

--- tests/phpunit/languages/LanguageHeTest.php
+++ tests/phpunit/languages/LanguageHeTest.php
@@ -7,24 +7,71 @@
 
 /** Tests for MediaWiki languages/classes/LanguageHe.php */
 class LanguageHeTest extends LanguageClassesTestCase {
-	/** @dataProvider providePlural */
-	function testPlural( $result, $value ) {
+	/*
+	The most common usage for the plural forms is two forms,
+	for singular and plural. In this case, the second form
+	is technically dual, but in practice it's used as plural.
+	In some cases, usually with expressions of time, three forms
+	are needed - singular, dual and plural.
+	CLDR also specifies a fourth form for multiples of 10,
+	which is very rare. It also has a mistake, because
+	the number 10 itself is supposed to be just plural,
+	so currently it's overridden in MediaWiki.
+	*/
+
+	/** @dataProvider provideTwoPluralForms */
+	function testTwoPluralForms( $result, $value ) {
+		$forms = array( 'one', 'other' );
+		$this->assertEquals( $result, $this->getLang()->convertPlural( $value, $forms ) );
+	}
+
+	/** @dataProvider provideThreePluralForms */
+	function testThreePluralForms( $result, $value ) {
 		$forms = array( 'one', 'two', 'other' );
 		$this->assertEquals( $result, $this->getLang()->convertPlural( $value, $forms ) );
 	}
 
-	/** @dataProvider providePlural */
+	/** @dataProvider provideFourPluralForms */
+	function testFourPluralForms( $result, $value ) {
+		$forms = array( 'one', 'two', 'many', 'other' );
+		$this->assertEquals( $result, $this->getLang()->convertPlural( $value, $forms ) );
+	}
+
+	/** @dataProvider provideFourPluralForms */
 	function testGetPluralRuleType( $result, $value ) {
 		$this->assertEquals( $result, $this->getLang()->getPluralRuleType( $value ) );
 	}
 
-	function providePlural() {
+	function provideTwoPluralForms() {
 		return array (
-			array( 'other', 0 ),
-			array( 'one', 1 ),
-			array( 'two', 2 ),
-			array( 'other', 3 ),
-			array( 'other', 10 ),
+			array( 'other', 0 ), // Zero - plural
+			array( 'one', 1 ), // Singular
+			array( 'other', 2 ), // No third form provided, use it as plural
+			array( 'other', 3 ), // Plural - other
+			array( 'other', 10 ), // No fourth form provided, use it as plural
+			array( 'other', 20 ), // No fourth form provided, use it as plural
+		);
+	}
+
+	function provideThreePluralForms() {
+		return array (
+			array( 'other', 0 ), // Zero - plural
+			array( 'one', 1 ), // Singular
+			array( 'two', 2 ), // Dual
+			array( 'other', 3 ), // Plural - other
+			array( 'other', 10 ), // No fourth form provided, use it as plural
+			array( 'other', 20 ), // No fourth form provided, use it as plural
+		);
+	}
+
+	function provideFourPluralForms() {
+		return array (
+			array( 'other', 0 ), // Zero - plural
+			array( 'one', 1 ), // Singular
+			array( 'two', 2 ), // Dual
+			array( 'other', 3 ), // Plural - other
+			array( 'other', 10 ), // 10 is supposed to be plural (other), not "many"
+			array( 'many', 20 ), // Fourth form provided - rare, but supported by CLDR
 		);
 	}
 
