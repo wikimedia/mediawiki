8b77c18a795902c3e737a606d98dd2a82dc17357
Timo Tijhof <ttijhof@wikimedia.org>
2013-03-21 19:45:17 +0100

    JSDuck: Cover more of external.js and implement @source
    Taken from VisualEditor repo (authored by myself under MIT).
    Hereby released under GPL as well.
    
    Change-Id: I4dc1a0e5c618e0a20ae6f308b4ecf266d041e22c

--- maintenance/jsduck/external.js
+++ maintenance/jsduck/external.js
@@ -1,26 +1,38 @@
 /**
  * @class jQuery
+ * @source <http://api.jquery.com/>
  */
 
 /**
  * @method ajax
+ * @source <http://api.jquery.com/jQuery.ajax/>
  * @return {jqXHR}
  */
 
 /**
  * @class jQuery.Event
+ * @source <http://api.jquery.com/Types/#Event>
  */
 
 /**
  * @class jQuery.Promise
+ * @source <http://api.jquery.com/Types/#Promise>
  */
 
 /**
  * @class jQuery.Deferred
  * @mixins jQuery.Promise
+ * @source <http://api.jquery.com/jQuery.Deferred/>
  */
 
 /**
  * @class jQuery.jqXHR
+ * @source <http://api.jquery.com/Types/#jqXHR>
  * @alternateClassName jqXHR
+ */
+
+
+/**
+ * @class QUnit
+ * @source <http://api.qunitjs.com/>
  */
