6be3f599d8370b3ea8516d42cca080570063aa6b
Max Semenik <maxsem.wiki@gmail.com>
2013-03-23 03:58:12 +0400

    startup: Blacklist incompatible mobile browsers
    These user agents have been blacklisted on the mobile site for some time.
    
    They have been re-reviewed alongside the A grade phones supported by
    jQuery mobile [1] and jQuery [2]
    
    [1] http://jquerymobile.com/gbs/
    [2] http://jquery.com/browser-support/
    
    Change-Id: If7743c476e95002174763514428c17dfcbe7da4c

--- resources/startup.js
+++ resources/startup.js
@@ -12,21 +12,30 @@
  *
  * See also:
  * - https://www.mediawiki.org/wiki/Compatibility#Browser
+ * - http://jquerymobile.com/gbs/
  * - http://jquery.com/browser-support/
  */
 
 /*jshint unused: false */
 function isCompatible( ua ) {
+	var webkitBased;
+
 	if ( ua === undefined ) {
 		ua = navigator.userAgent;
 	}
+
+	webkitBased = ua.match( /WebKit/ );
 
 	// MediaWiki JS or jQuery is known to have issues with:
 	return !(
 		// Internet Explorer < 6
 		( ua.indexOf( 'MSIE' ) !== -1 && parseFloat( ua.split( 'MSIE' )[1] ) < 6 ) ||
 			// Firefox < 4
-			( ua.indexOf( 'Firefox/' ) !== -1 && parseFloat( ua.split( 'Firefox/' )[1] ) < 4 )
+			( ua.indexOf( 'Firefox/' ) !== -1 && parseFloat( ua.split( 'Firefox/' )[1] ) < 4 ) ||
+			// BlackBerry < 6
+			ua.match( /BlackBerry[^\/]*\/[1-5]\./ ) ||
+			// Open WebOS < 1.5
+			ua.match( /webOS\/1\.[0-4]/ )
 	);
 }
 
--- tests/qunit/suites/resources/startup.test.js
+++ tests/qunit/suites/resources/startup.test.js
@@ -20,7 +20,20 @@
 			'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; en-US)',
 			'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)',
 			'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)',
-			'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)'
+			'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+			// BlackBerry 6+
+			'Mozilla/5.0 (BlackBerry; U; BlackBerry 9300; en) AppleWebKit/534.8+ (KHTML, like Gecko) Version/6.0.0.570 Mobile Safari/534.8+',
+			'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.1.0.346 Mobile Safari/534.11+',
+			'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.3+ (KHTML, like Gecko) Version/10.0.9.386 Mobile Safari/537.3+',
+			// Open WebOS 1.4+ (HP Veer 4G)
+			'Mozilla/5.0 (webOS/2.1.2; U; en-US) AppleWebKit/532.2 (KHTML, like Gecko) Version/1.0 Safari/532.2 P160UNA/1.0',
+			// Firefox Mobile
+			'Mozilla/5.0 (Mobile; rv:14.0) Gecko/14.0 Firefox/14.0',
+			// iOS
+			'Mozilla/5.0 (ipod: U;CPU iPhone OS 2_2 like Mac OS X: es_es) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.0 Mobile/3B48b Safari/419.3',
+			'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3B48b Safari/419.3',
+			// Android
+			'Mozilla/5.0 (Linux; U; Android 2.1; en-us; Nexus One Build/ERD62) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17'
 		],
 		// Supported: Uncompatible, serve basic content
 		gradeB: [
@@ -32,7 +45,13 @@
 			// Firefox < 4
 			'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2',
 			'Mozilla/5.0 (X11; U; Linux i686; nl; rv:1.8.1.1) Gecko/20070311 Firefox/2.0.0.1',
-			'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3'
+			'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3',
+			// BlackBerry < 6
+			'BlackBerry9300/5.0.0.716 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/133',
+			'BlackBerry7250/4.0.0 Profile/MIDP-2.0 Configuration/CLDC-1.1',
+			// Open WebOS < 1.5 (Palm Pre, Palm Pixi)
+			'Mozilla/5.0 (webOS/1.0; U; en-US) AppleWebKit/525.27.1 (KHTML, like Gecko) Version/1.0 Safari/525.27.1 Pre/1.0',
+			'Mozilla/5.0 (webOS/1.4.0; U; en-US) AppleWebKit/532.2 (KHTML, like Gecko) Version/1.0 Safari/532.2 Pixi/1.1 '
 
 		],
 		// No special treatment blacklisting, benefit of doubt at own risk
@@ -42,6 +61,14 @@
 			'Mozilla/5.0 (X11; U; Linux i686 (x86_64); en-US; rv:1.8.1.6) Gecko/20070817 IceWeasel/2.0.0.6-g2',
 			// KHTML
 			'Mozilla/5.0 (compatible; Konqueror/4.3; Linux) KHTML/4.3.5 (like Gecko)',
+			// SymbianOS
+			'Mozilla/5.0 (SymbianOS/9.1; U; [en]; SymbianOS/91 Series60/3.0) AppleWebKit/413 (KHTML, like Gecko) Safari/413',
+			// Opera Mini
+			'Opera/9.50 (J2ME/MIDP; Opera Mini/4.0.10031/298; U; en)',
+			// NetFront
+			'Mozilla/4.0 (compatible; Linux 2.6.10) NetFront/3.3 Kindle/1.0 (screen 600x800)',
+			'Mozilla/4.0 (compatible; Linux 2.6.22) NetFront/3.4 Kindle/2.0 (screen 824x1200; rotate)',
+			'Mozilla/4.08 (Windows; Mobile Content Viewer/1.0) NetFront/3.2',
 			// Text browsers
 			'Links (2.1pre33; Darwin 8.11.0 Power Macintosh; x)',
 			'Links (6.9; Unix 6.9-astral sparc; 80x25)',
