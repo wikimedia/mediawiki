e8e2474c8f4518e5ad710b40ac719e4b4343a4f0
Aaron Schulz <aschulz@wikimedia.org>
2013-03-22 20:48:00 +0000

    Revert "Misc follow-ups to I2fc3966e (a161c5e)"
    That apparently caused bug 46397.
    
    This reverts commit 388b14a15de6c531d876796dde02605f046fcf53
    
    
    Bug : 46397
    Change-Id: Ideaa86f0d535873a08e27d6f98f6bd4255b8c594

--- tests/phpunit/includes/parser/MagicVariableTest.php
+++ tests/phpunit/includes/parser/MagicVariableTest.php
@@ -65,68 +65,44 @@ class MagicVariableTest extends MediaWikiTestCase {
 
 	# day
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testCurrentdayIsUnPadded( $day ) {
 		$this->assertUnPadded( 'currentday', $day );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testCurrentdaytwoIsZeroPadded( $day ) {
 		$this->assertZeroPadded( 'currentday2', $day );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testLocaldayIsUnPadded( $day ) {
 		$this->assertUnPadded( 'localday', $day );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testLocaldaytwoIsZeroPadded( $day ) {
 		$this->assertZeroPadded( 'localday2', $day );
 	}
 
 	# month
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testCurrentmonthIsZeroPadded( $month ) {
 		$this->assertZeroPadded( 'currentmonth', $month );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testCurrentmonthoneIsUnPadded( $month ) {
 		$this->assertUnPadded( 'currentmonth1', $month );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testLocalmonthIsZeroPadded( $month ) {
 		$this->assertZeroPadded( 'localmonth', $month );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testLocalmonthoneIsUnPadded( $month ) {
 		$this->assertUnPadded( 'localmonth1', $month );
 	}
@@ -134,36 +110,24 @@ class MagicVariableTest extends MediaWikiTestCase {
 
 	# revision day
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testRevisiondayIsUnPadded( $day ) {
 		$this->assertUnPadded( 'revisionday', $day );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Days
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Days */
 	function testRevisiondaytwoIsZeroPadded( $day ) {
 		$this->assertZeroPadded( 'revisionday2', $day );
 	}
 
 	# revision month
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testRevisionmonthIsZeroPadded( $month ) {
 		$this->assertZeroPadded( 'revisionmonth', $month );
 	}
 
-	/**
-	 * @dataProvider MediaWikiProvide::Months
-	 * @group Database
-	 */
+	/** @dataProvider MediaWikiProvide::Months */
 	function testRevisionmonthoneIsUnPadded( $month ) {
 		$this->assertUnPadded( 'revisionmonth1', $month );
 	}
