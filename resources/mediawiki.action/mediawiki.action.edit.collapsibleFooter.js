f6595c27e973e11b7d70009b72088a83be511317
MatmaRex <matma.rex@gmail.com>
2013-03-23 16:36:14 +0100

    Make the templates/category lists on edit page collapsible
    This is a total rewrite of Vector extension's functionality, to fix
    various accessibility and internationalization issues with the
    original one. Both the JavaScript and the CSS have been redone from
    scratch.
    
    Still, the look&feel is almost the same as in the original version,
    save for the mentioned messaging changes and minor UI differences (the
    toggles are no longer <a> elements). The same cookie names have been
    used to avoid any issues when migrating.
    
    Used standard arrow icons from the mediawiki.icon module.
    
    Bug: 43689
    Change-Id: I91a3704cb09b9e17dd8baa516ab6f8ee441b7467

---
+++ resources/mediawiki.action/mediawiki.action.edit.collapsibleFooter.js
@@ -0,0 +1,48 @@
+( function ( mw, $ ) {
+	var collapsibleLists, i;
+
+	// Collapsible lists of categories and templates
+	collapsibleLists = [
+		{
+			$list: $( '.templatesUsed ul' ),
+			$toggler: $( '.mw-templatesUsedExplanation' ),
+			cookieName: 'templates-used-list'
+		},
+		{
+			$list: $( '.hiddencats ul' ),
+			$toggler: $( '.mw-hiddenCategoriesExplanation' ),
+			cookieName: 'hidden-categories-list'
+		}
+	];
+
+	for ( i = 0; i < collapsibleLists.length; i++ ) {
+		/*jshint loopfunc:true */
+		// Use a closure for iteration-local variables
+		( function ( $list, $toggler, cookieName ) {
+			var isCollapsed = $.cookie( cookieName ) !== 'expanded';
+
+			// Style the toggler with an arrow icon and add a tabIndex and a role for accessibility
+			$toggler.addClass( 'mw-editfooter-toggler' ).prop( 'tabIndex', 0 ).attr( 'role', 'button' );
+			$list.addClass( 'mw-editfooter-list' );
+
+			$list.makeCollapsible( {
+				$customTogglers: $toggler,
+				linksPassthru: true,
+				plainMode: true,
+				collapsed: isCollapsed
+			} );
+
+			$toggler.addClass( isCollapsed ? 'mw-icon-arrow-collapsed' : 'mw-icon-arrow-expanded' );
+
+			$list.on( 'beforeExpand.mw-collapsible', function () {
+				$toggler.removeClass( 'mw-icon-arrow-collapsed' ).addClass( 'mw-icon-arrow-expanded' );
+				$.cookie( cookieName, 'expanded' );
+			} );
+
+			$list.on( 'beforeCollapse.mw-collapsible', function () {
+				$toggler.removeClass( 'mw-icon-arrow-expanded' ).addClass( 'mw-icon-arrow-collapsed' );
+				$.cookie( cookieName, 'collapsed' );
+			} );
+		} )( collapsibleLists[i].$list, collapsibleLists[i].$toggler, collapsibleLists[i].cookieName );
+	}
+}( mediaWiki, jQuery ) );
