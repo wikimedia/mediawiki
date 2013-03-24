75293088c53f213f76f6ee079981ad4ecbc766e8
lfs <schenonef@gmail.com>
2013-03-24 20:10:35 -0300

    Vector: Add the collapsibleTabs script from the Vector extension
    * Move jquery.collapsibleTabs from the Vector extension
      to the Vector skin (see 097f387362)
    * Register the module and its dependencies
    * Merge the init script into the existing vector.js
    
    Bug: 46513
    Change-Id: I7bd77e6e4c384c9e2033f9d05ffcae86b0bb7a69

--- skins/vector/vector.js
+++ skins/vector/vector.js
@@ -18,4 +18,32 @@ jQuery( function ( $ ) {
 				$el.removeClass( 'vectorMenuFocus' );
 			} );
 	} );
+
+	/**
+	 * Collapsible tabs for Vector
+	 */
+	var $cactions = $( '#p-cactions' );
+
+	// Bind callback functions to animate our drop down menu in and out
+	// and then call the collapsibleTabs function on the menu
+	$( '#p-views ul' )
+		.bind( 'beforeTabCollapse', function () {
+			// If the dropdown was hidden, show it
+			if ( $cactions.hasClass( 'emptyPortlet' ) ) {
+				$cactions
+					.removeClass( 'emptyPortlet' )
+					.find( 'h3, h5' )
+						.css( 'width', '1px' ).animate( { 'width': '24px' }, 390 );
+			}
+		} )
+		.bind( 'beforeTabExpand', function () {
+			// If we're removing the last child node right now, hide the dropdown
+			if ( $cactions.find( 'li' ).length === 1 ) {
+				$cactions.find( 'h3, h5' ).animate( { 'width': '1px' }, 390, function () {
+					$( this ).attr( 'style', '' )
+						.parent().addClass( 'emptyPortlet' );
+				});
+			}
+		} )
+		.collapsibleTabs();
 } );
