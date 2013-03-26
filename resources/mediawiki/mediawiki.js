931cbe82f9fc1948c1462f5af1fa32d2307a7f1b
Timo Tijhof <ttijhof@wikimedia.org>
2013-03-26 19:04:19 +0100

    Revert "mw.loader: Add debugging"
    This reverts 8c566e5 and 91f4661.
    
    Change-Id: If24993e57cc432c1ee2723e8354c034e7f5a2a76

--- resources/mediawiki/mediawiki.js
+++ resources/mediawiki/mediawiki.js
@@ -461,12 +461,6 @@ var mw = ( function ( $, undefined ) {
 			 * @return {boolean} False if a new one must be created.
 			 */
 			function canExpandStylesheetWith( cssText ) {
-				// Debug hack to trace the source of garbage input to mw.loader
-				if ( typeof cssText !== 'string' ) {
-					log( cssText );
-					throw new Error( 'Illegal invokation of path mw.loader#canExpandStylesheetWith' );
-				}
-
 				// Makes sure that cssText containing `@import`
 				// rules will end up in a new stylesheet (as those only work when
 				// placed at the start of a stylesheet; bug 35562).
