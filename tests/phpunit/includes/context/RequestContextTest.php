0882cfe93247c69f7e174880af35843fc56904ca
Adam Roses Wight <awight@wikimedia.org>
2013-04-01 17:09:40 -0700

    WIP Add tests to exercise a language bug in DerivativeContext
    TODO:
    * Failing at an asssertion before the one I expected to fail, needs
    some TLC after rebasing by 2 years.
    
    * Not sure what the convention is for known-failing tests, should I
    mark as skipped?  Or just wait to merge until I've fixed the bug
    which I'm demonstrating with the failing test?
    
    Bug: 46766
    Change-Id: I56aa14125bd94537247c4d98b9e2c1e87d741c7a

--- tests/phpunit/includes/context/RequestContextTest.php
+++ tests/phpunit/includes/context/RequestContextTest.php
@@ -93,4 +93,40 @@ class RequestContextTest extends MediaWikiTestCase {
 		$this->assertEquals( $oInfo['sessionId'], $info['sessionId'], "Correct restored session ID." );
 		$this->assertEquals( $oInfo['userId'], $info['userId'], "Correct restored user ID." );
 	}
+
+	public function testContextLanguage() {
+		$this->setMwGlobals( array(
+			'wgUseDatabaseMessages' => true,
+			'wgLanguageCode' => 'en',
+		) );
+
+		$context = new RequestContext();
+
+		// Pick a language other than the site default
+		$lang = 'aa';
+		$dbKey = __METHOD__ . '-Test Message';
+		$transDbKey = "{$dbKey}/{$lang}";
+		$nativeText = 'Untranslated!';
+		$transText = 'Translated message.';
+
+		$msgTitle = Title::newFromText( $dbKey, NS_MEDIAWIKI );
+
+		$page = new WikiPage( $msgTitle );
+		$content = ContentHandler::makeContent( $nativeText, $msgTitle, null );
+		$page->doEditContent( $content, '', EDIT_NEW );
+
+		$transTitle = Title::newFromText( $transDbKey, NS_MEDIAWIKI );
+		$page = new WikiPage( $transTitle );
+		$content = ContentHandler::makeContent( $transText, $transTitle, null );
+		$page->doEditContent( $content, '', EDIT_NEW );
+
+		// We are testing that this works.
+		$context->setLanguage( $lang );
+
+		// Make sure the message has been stored.
+		$this->assertEquals( $transText, $context->msg( $dbKey )->inLanguage( $lang )->text() );
+
+		$this->assertEquals( $transText, $context->msg( $dbKey )->text(),
+			'RequestContext->setLanguage causes the translated message to be fetched.' );
+	}
 }
