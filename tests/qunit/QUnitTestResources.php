045f6030883146e57b723f1e4700106a8c13af4b
Max Semenik <maxsem.wiki@gmail.com>
2013-04-09 23:10:56 +0400

    Backport user-agent blacklisting
    * https://gerrit.wikimedia.org/r/#/c/57693/
    * https://gerrit.wikimedia.org/r/#/c/55446/
    
    Change-Id: I7c36b1a939cec5e19bef8b06ce01173f3ae2f983

--- tests/qunit/QUnitTestResources.php
+++ tests/qunit/QUnitTestResources.php
@@ -6,6 +6,7 @@ return array(
 
 	'mediawiki.tests.qunit.suites' => array(
 		'scripts' => array(
+			'tests/qunit/suites/resources/startup.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.autoEllipsis.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.byteLength.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.byteLimit.test.js',
@@ -16,6 +17,7 @@ return array(
 			'tests/qunit/suites/resources/jquery/jquery.hidpi.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.highlightText.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.localize.test.js',
+			'tests/qunit/suites/resources/jquery/jquery.makeCollapsible.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.mwExtension.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.tabIndex.test.js',
 			'tests/qunit/suites/resources/jquery/jquery.tablesorter.test.js',
@@ -45,6 +47,7 @@ return array(
 			'jquery.hidpi',
 			'jquery.highlightText',
 			'jquery.localize',
+			'jquery.makeCollapsible',
 			'jquery.mwExtension',
 			'jquery.tabIndex',
 			'jquery.tablesorter',
@@ -61,6 +64,5 @@ return array(
 			'mediawiki.language',
 			'mediawiki.cldr',
 		),
-		'position' => 'top',
 	)
 );
