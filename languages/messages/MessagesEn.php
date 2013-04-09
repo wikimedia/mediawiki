0ccd462508dc0f90d620ea94dfa20059ee622c95
Ryan Lane <rlane@wikimedia.org>
2013-04-09 00:48:21 -0700

    Add chosen support to HTMLForm's multiselect field
    For very large multiselects, the list of checkboxes is
    overwhelming. This change adds support for using jquery.chosen
    conditionally on multiselect fields, if 'chosen' is used for the
    cssclass parameter.
    
    Change-Id: If1eba7fb705089deb4cff7a326db0ac803a522ec

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -4834,6 +4834,7 @@ This site is experiencing technical difficulties.',
 'htmlform-selectorother-other' => 'Other',
 'htmlform-no'                  => 'No',
 'htmlform-yes'                 => 'Yes',
+'htmlform-chosen-placeholder'  => 'Select an option',
 
 # SQLite database support
 'sqlite-has-fts' => '$1 with full-text search support',
