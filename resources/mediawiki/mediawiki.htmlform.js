263f6b3c26841abef938c331d4182cf702e73266
Ryan Lane <rlane@wikimedia.org>
2013-04-09 00:48:21 -0700

    Add chosen support to HTMLForm's multiselect field
    For very large multiselects, the list of checkboxes is
    overwhelming. This change adds support for using jquery.chosen
    conditionally on multiselect fields, if 'chosen' is used for the
    cssclass parameter.
    
    Change-Id: If1eba7fb705089deb4cff7a326db0ac803a522ec

--- resources/mediawiki/mediawiki.htmlform.js
+++ resources/mediawiki/mediawiki.htmlform.js
@@ -59,4 +59,55 @@
 
 	} );
 
+	$.fn.addMulti = function( $container ) {
+		var $name = $(this).find( 'input:first-child' ).attr( 'name' );
+		var $select = $( '<select>' );
+		var $dataPlaceholder = mw.message( 'htmlform-chosen-placeholder' );
+		$select.attr( {
+			name: $name,
+			multiple: 'multiple',
+			class: 'chzn-select mw-input',
+			'data-placeholder': $dataPlaceholder.escaped()
+		} );
+		$(this).find( 'input' ).each( function() {
+			var $checked = $(this).attr( 'checked' );
+			var $option = $( '<option>' );
+			$option.attr( 'value', $(this).attr( 'value' ) );
+			if ( $checked == 'checked' ) {
+				$option.attr( 'selected', 'selected' );
+			}
+			$option.text( $(this).attr( 'value' ) );
+			$select.append( $option );
+		} );
+		$container.append( $select );
+	}
+
+	$.fn.convertCheckboxesToMulti = function( $type ) {
+		if ( $type == 'table' ) {
+			var $fieldLabel = $( '<td>' );
+			var $container = $( '<tr>' );
+			var $td = $( '<td>' );
+			$(this).addMulti( $td );
+			$container.append( $td );
+		} else if ( $type == 'div' ) {
+			var $fieldLabel = $( '<div>' );
+			var $container = $( '<div>' );
+			$(this).addMulti( $container );
+		}
+		$fieldLabel.attr( 'class', 'mw-label' );
+		var $fieldLabelText = $( '<label>' );
+		$fieldLabelText.text( $(this).find( '.mw-label label' ).text() );
+		$fieldLabel.append( $fieldLabelText );
+		$container.prepend( $fieldLabel );
+		$(this).parent().append( $container );
+		$(this).remove();
+	}
+
+	mw.loader.using( 'jquery.chosen', function () {	
+		$( 'table .chosen' ).convertCheckboxesToMulti( 'table' );
+		$( 'div .chosen' ).convertCheckboxesToMulti( 'div' );
+		$( '.chzn-select' ).chosen();
+	} );
+
+
 }( jQuery ) );
