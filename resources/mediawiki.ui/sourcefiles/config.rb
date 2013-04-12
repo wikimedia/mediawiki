e345e5bbb61ceb7349607470b71a05d4fb6bdc93
S Page <spage@wikimedia.org>
2013-04-12 16:37:05 -0700

    Set Agora font stack to mw-ui-container in Vector
    We moved the language links outside the new compact vertical form
    layout, hence also need to apply the font stack to mw-ui-container when
    using Vector skin.
    
    Also
    * a comment-only CSS change
    * set relative_assets = true in Compass sourcefiles configuration
    
    Bug: 47187
    Change-Id: Id064dc8ada5c43c33e17d1e66d327ba05e918657

--- resources/mediawiki.ui/sourcefiles/config.rb
+++ resources/mediawiki.ui/sourcefiles/config.rb
@@ -14,7 +14,7 @@ sass_dir = "scss"
 output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
