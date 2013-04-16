559639c68c96589f12f3bcc04be7f261851b4f5a
AVRS Aditya Sastry <ganeshaditya1@gmail.com>
2013-04-16 19:43:38 +0530

    Show a tip at the end of the installer to prompt about extensions
    Almost every wiki install is going to want one or more extensions to
    enhance their users' experience. Give the wiki admin as much info
    as possible so they can find extensions that may be useful to them.
    
    This is shown as an info box on the final page of installation.
    
    Bug: 43504
    Change-Id: Iba5d53a485d7b0b04cb9104d7d964fc01a1c1cae

--- includes/installer/WebInstallerPage.php
+++ includes/installer/WebInstallerPage.php
@@ -1265,6 +1265,9 @@ class WebInstaller_Complete extends WebInstallerPage {
 				)->plain(), 'tick-32.png'
 			)
 		);
+		$this->addHTML( $this->parent->getInfoBox(
+			wfMessage( 'config-extension-link' )->text() ) );
+
 		$this->parent->restoreLinkPopups();
 		$this->endForm( false, false );
 	}
