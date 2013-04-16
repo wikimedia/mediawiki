c7163c7aab4eabcb72fdb5bd13a77c8b7d36f874
AVRS Aditya Sastry <ganeshaditya1@gmail.com>
2013-04-16 19:43:38 +0530

    Modified the installer to show a tip about extensions in last screen.
    In the last screen of the installer I've added a Info box which carries
    two links one to the extensions manual and another to the list of
    extensions available sorted according to there category. This is done to make the admin of the wiki aware of MediaWiki extensions. This message has
    been added to the i18n file in both the english array as well as the qqq
    array. Links added using wikitext.
    
    Bug: 43504
    Change-Id: Iba5d53a485d7b0b04cb9104d7d964fc01a1c1cae

--- includes/installer/WebInstallerPage.php
+++ includes/installer/WebInstallerPage.php
@@ -1265,6 +1265,11 @@ class WebInstaller_Complete extends WebInstallerPage {
 				)->plain(), 'tick-32.png'
 			)
 		);
+		$this->addHTML(
+			$this->parent->getInfoBox(
+				wfMessage('extension-link')->text()
+			)
+		);
 		$this->parent->restoreLinkPopups();
 		$this->endForm( false, false );
 	}
