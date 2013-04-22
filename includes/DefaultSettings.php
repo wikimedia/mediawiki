23e7319e1ab1ebbe1304f273b6844d852f23e9a7
MarkAHershberger <mah@everybody.org>
2013-04-21 21:39:48 -0400

    update version number
    Change-Id: Ie6558f21bef512f7de456fd9edaf6860bcd12315

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.21.0rc3';
+$wgVersion = '1.21.0rc4';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
