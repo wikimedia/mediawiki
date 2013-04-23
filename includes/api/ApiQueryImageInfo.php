cda875b272cbc54ac574250d2694e47c20597eaf
Catrope <roan.kattouw@gmail.com>
2013-04-22 20:46:15 -0700

    Allow iiurlheight to be used without iiurlwidth
    Wanting to get a thumbnail with only the height constrained is a
    reasonable use case, the only reason we don't support it in the
    API is because the backend doesn't support it directly. This is
    easy enough to emulate by setting the width to the full width of
    the image if only the height is specified. This makes the transform
    function use a bounding box of size w x h, and if w is the width of
    the original image that's equivalent to just bounding the height.
    
    Change-Id: I28c7c22ee91669469cbd9e7d25f09100933582de

--- includes/api/ApiQueryImageInfo.php
+++ includes/api/ApiQueryImageInfo.php
@@ -202,21 +202,19 @@ class ApiQueryImageInfo extends ApiQueryBase {
 	public function getScale( $params ) {
 		$p = $this->getModulePrefix();
 
-		// Height and width.
-		if ( $params['urlheight'] != -1 && $params['urlwidth'] == -1 ) {
-			$this->dieUsage( "{$p}urlheight cannot be used without {$p}urlwidth", "{$p}urlwidth" );
-		}
-
 		if ( $params['urlwidth'] != -1 ) {
 			$scale = array();
 			$scale['width'] = $params['urlwidth'];
+			$scale['height'] = $params['urlheight'];
+		} else if ( $params['urlheight'] != -1 ) {
+			// Height is specified but width isn't
+			// Don't set $scale['width']; this signals mergeThumbParams() to fill it with the image's width
 			$scale['height'] = $params['urlheight'];
 		} else {
 			$scale = null;
 			if ( $params['urlparam'] ) {
 				$this->dieUsage( "{$p}urlparam requires {$p}urlwidth", "urlparam_no_width" );
 			}
-			return $scale;
 		}
 
 		return $scale;
@@ -232,6 +230,12 @@ class ApiQueryImageInfo extends ApiQueryBase {
 	 * @return Array of parameters for transform.
 	 */
 	protected function mergeThumbParams( $image, $thumbParams, $otherParams ) {
+
+		if ( !isset( $thumbParams['width'] ) && isset( $thumbParams['height'] ) ) {
+			// Populate the width with the image's width, so only the height restriction applies
+			$thumbParams['width'] = $image->getWidth();
+		}
+
 		if ( !$otherParams ) {
 			return $thumbParams;
 		}
@@ -571,7 +575,8 @@ class ApiQueryImageInfo extends ApiQueryBase {
 			'urlwidth' => array( "If {$p}prop=url is set, a URL to an image scaled to this width will be returned.",
 				'For performance reasons if this option is used, ' .
 					'no more than ' . self::TRANSFORM_LIMIT . ' scaled images will be returned.' ),
-			'urlheight' => "Similar to {$p}urlwidth. Cannot be used without {$p}urlwidth",
+			'urlheight' => array( "Similar to {$p}urlwidth. If this is set but {$p}urlwidth is not set,",
+						"{$p}urlwidth will be deemed to the image's width." ),
 			'urlparam' => array( "A handler specific parameter string. For example, pdf's ",
 				"might use 'page15-100px'. {$p}urlwidth must be used and be consistent with {$p}urlparam" ),
 			'limit' => 'How many image revisions to return per image',
