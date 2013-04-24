0e42bdadd9349fe5b6119ff65322e6c52d813a6e
Aaron Schulz <aschulz@wikimedia.org>
2013-04-23 17:17:50 -0700

    Fix bug in OOM avoidance for RepoGroup cache.
    Change-Id: Ic2e7fb6a9a7207315fd65454d883b84278efafc0
    (cherry picked from commit 042baf43bdc60ef432bb5dc6c889226e62b06404)

--- includes/filerepo/file/LocalFile.php
+++ includes/filerepo/file/LocalFile.php
@@ -1659,11 +1659,11 @@ class LocalFile extends File {
 	}
 
 	/**
-	 * @return bool
+	 * @return bool Whether to cache in RepoGroup (this avoids OOMs)
 	 */
 	function isCacheable() {
-		$this->load();
-		return strlen( $this->metadata ) <= self::CACHE_FIELD_MAX_LEN; // avoid OOMs
+		$this->load(); // if loaded from cache, metadata will be null if it didn't fit
+		return $this->metadata !== null && strlen( $this->metadata ) <= self::CACHE_FIELD_MAX_LEN;
 	}
 
 	/**
