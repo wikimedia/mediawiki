090bcf515994b6c2ccb9c3744f638ad75e3c699d
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2013-04-29 00:14:41 +0200

    Prefix wiki-text redirects with colon
    Prefixing redirect target links with a colon will not affect the redirect
    capability itself at all. However when the redirect is not processed for
    certain reasons, the leading colon will prevent linked image from being
    displayed.
    
    For example when moving an image in the File namespace, the created
    redirect will be this:
    
        #redirect [[File:New name.ext]]
    
    Now consider that someone tags this redirect page for deletion by adding a
    deletion template to it (in front of the redirect code). The resulting
    content would be for example this:
    
        {{delete}}
        #redirect [[File:New name.ext]]
    
    In that case, the redirect is not handled (as it's not the first thing on
    the page) but rendered as an ordered list. And in that list, the image
    link will make the image being displayed, eventually causing display
    issues (e.g. for large images).
    
    By prefixing the redirect link with a colon by default, this behavior is
    prevented because when interpreted as a list, the image link will be just
    a *link* to the image page. When the page is not modified in such a way,
    then the redirect will still work as expected with a leading colon.
    
    Change-Id: Ie9c55f6f5cc77a7e5653a6bf9e9d2a795e804410

--- includes/content/WikitextContentHandler.php
+++ includes/content/WikitextContentHandler.php
@@ -60,7 +60,7 @@ class WikitextContentHandler extends TextContentHandler {
 	 */
 	public function makeRedirectContent( Title $destination ) {
 		$mwRedir = MagicWord::get( 'redirect' );
-		$redirectText = $mwRedir->getSynonym( 0 ) . ' [[' . $destination->getPrefixedText() . ']]';
+		$redirectText = $mwRedir->getSynonym( 0 ) . ' [[:' . $destination->getPrefixedText() . ']]';
 
 		return new WikitextContent( $redirectText );
 	}
