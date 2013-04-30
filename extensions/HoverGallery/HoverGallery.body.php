89ce5c196dd26aef70def1aff6c43d9957f52fdc
Felipe Schenone <schenonef@gmail.com>
2013-05-01 00:14:52 +0200

    Whitespaces
    Change-Id: I97f897959b38cd02d0463141652bd93b3b84974f

--- extensions/HoverGallery/HoverGallery.body.php
+++ extensions/HoverGallery/HoverGallery.body.php
@@ -25,28 +25,28 @@ class HoverGallery {
 		}
 		if ( array_key_exists( 'maxhoverheight', $ARGS ) ) {
 			$maxhoverheight = $ARGS['maxhoverheight'] . 'px';
-		}			
+		}
 
 		$normalGallery = $parser->recursiveTagParse( '<gallery>' . $input . '</gallery>' );
 
 		$hiddenGallery = '<div class="hover-gallery">';
 		$FILENAMES = explode( "\n", $input );
-		$FILENAMES = array_filter( $FILENAMES );		
+		$FILENAMES = array_filter( $FILENAMES );
 		foreach ( $FILENAMES as $filename ) {
 			if ( $start = strpos( $filename, ":" ) ) {
 				$filename = substr( $filename, $start + 1 ); // Remove the namespace
 			}
 			if ( strpos( $filename, "|" ) ) {
-				$filename = strstr( $filename, "|", true ); // Remove the parameters				
+				$filename = strstr( $filename, "|", true ); // Remove the parameters
 			}
 			$file = wfFindFile( $filename );
 			if ( is_object( $file ) ) {
 				$link = $file->getFullUrl();
-				$hiddenGallery .= '<img src="' . $link . '" style="max-width: ' . $maxhoverwidth . '; max-height: ' . $maxhoverheight . '" />';				
+				$hiddenGallery .= '<img src="' . $link . '" style="max-width: ' . $maxhoverwidth . '; max-height: ' . $maxhoverheight . '" />';
 			}
 		}
 		$hiddenGallery .= '</div>';
-		
+
 		return $normalGallery . $hiddenGallery;
 	}
 }
