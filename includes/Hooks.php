a73a683f3b6927d8a3cd190d3e00589de637cc06
Chad Horohoe <chadh@wikimedia.org>
2013-05-01 16:33:00 -0700

    Revert type hinting in Hooks::run()
    Iafdd4cee introduced type hinting to Hooks::run(). Unfortunately,
    not all callers pass an array (some pass an object).
    
    This was noticable by breaking importDump.php with a catchable
    fatal error.
    
    Change-Id: I6b10c780e3cf76a4f1758cf7e59b3ddcd16ad62e

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -129,7 +129,7 @@ class Hooks {
 	 * @throws MWException
 	 * @throws FatalError
 	 */
-	public static function run( $event, array $args = array() ) {
+	public static function run( $event, $args = array() ) {
 		wfProfileIn( 'hook: ' . $event );
 		foreach ( self::getHandlers( $event ) as $hook ) {
 			// Turn non-array values into an array. (Can't use casting because of objects.)
