b29691f2ba27365ade29bf33d41a600a713eff3f
Erik Bernhardson <ebernhardson@wikimedia.org>
2013-05-01 17:27:14 -0700

    Tooltips for HTMLCheckMatrix
    Generates tooltips on the columns labels of HTMLCheckMatrix.
    Bug: 47094
    
    Change-Id: I9670ecdc2cb553df07ee06e3b83b003382471d27

--- resources/mediawiki/mediawiki.htmlform.js
+++ resources/mediawiki/mediawiki.htmlform.js
@@ -125,4 +125,12 @@
 		} );
 	}
 
+	$( document ).ready( function() {
+		var $matrixTooltips = $( '.mw-htmlform-matrix .mw-htmlform-tooltip' );
+		if ( $matrixTooltips.length ) {
+			mw.loader.using( 'jquery.tipsy', function () {
+				$matrixTooltips.tipsy();
+			} );
+		}
+	} );
 }( mediaWiki, jQuery ) );
