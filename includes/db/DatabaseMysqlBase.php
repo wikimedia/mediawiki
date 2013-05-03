3c742d77da61c754bf908ed82602070634d632a1
Jakub Vrana <jakub@vrana.cz>
2013-05-02 23:01:32 -0700

    Add support for mysqli extension
    DatabaseBase::factory() now has an optional $driver parameter to
    allow specifying a specific driver for the database class if we
    support more than one (in practice, just mysql).
    
    Bug: 45288
    Change-Id: I6733fe21c4aa7443e409c5dfa7c789552b2b62b7

--- includes/db/DatabaseMysqlBase.php
+++ includes/db/DatabaseMysqlBase.php
@@ -1004,7 +1004,7 @@ abstract class DatabaseMysqlBase extends DatabaseBase {
  */
 class MySQLField implements Field {
 	private $name, $tablename, $default, $max_length, $nullable,
-		$is_pk, $is_unique, $is_multiple, $is_key, $type;
+		$is_pk, $is_unique, $is_multiple, $is_key, $type, $binary;
 
 	function __construct( $info ) {
 		$this->name = $info->name;
@@ -1017,6 +1017,7 @@ class MySQLField implements Field {
 		$this->is_multiple = $info->multiple_key;
 		$this->is_key = ( $this->is_pk || $this->is_unique || $this->is_multiple );
 		$this->type = $info->type;
+		$this->binary = isset( $info->binary ) ? $info->binary : false;
 	}
 
 	/**
@@ -1063,6 +1064,10 @@ class MySQLField implements Field {
 	 */
 	function isMultipleKey() {
 		return $this->is_multiple;
+	}
+
+	function isBinary() {
+		return $this->binary;
 	}
 }
 
