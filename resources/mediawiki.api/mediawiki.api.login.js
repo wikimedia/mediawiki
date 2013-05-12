c3edbd99e8ff10b8f5802dfc9dc80463d0d83d47
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-05-12 15:26:49 +0000

    mediawiki.api: Implement login method
    Change-Id: I1113a076ff66e20ece1db9380969e7a7b5a68f1a

---
+++ resources/mediawiki.api/mediawiki.api.login.js
@@ -0,0 +1,50 @@
+/**
+ * Make the two-step login easier.
+ * @author Niklas Laxström
+ * @class mw.Api.plugin.login
+ * @since 1.22
+ */
+( function ( mw, $ ) {
+	'use strict';
+
+	$.extend( mw.Api.prototype, {
+		/**
+		 * @param {string} username
+		 * @param {string} password
+		 * @return {jQuery.Promise} See mw.Api#post
+		 */
+		login: function ( username, password ) {
+			var params, request,
+				deferred = $.Deferred(),
+				api = this;
+
+			params = {
+				action: 'login',
+				lgname: username,
+				lgpassword: password
+			};
+
+			request = api.post( params );
+			request.fail( deferred.reject );
+			request.done( function ( data ) {
+				params.lgtoken = data.login.token;
+				api.post( params )
+					.fail( deferred.reject )
+					.done( function ( data ) {
+						var code = 'unknown';
+						if ( data.login && data.login.result === 'Success' ) {
+							deferred.resolve( data );
+						} else {
+							// Set proper error code whenever possible
+							if ( data.error && data.error.code ) {
+								code = data.error.code;
+							}
+							deferred.reject( code, data );
+						}
+					} );
+			} );
+
+			return deferred.promise( { abort: request.abort } );
+		}
+	} );
+}( mediaWiki, jQuery ) );
