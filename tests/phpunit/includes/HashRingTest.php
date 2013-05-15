wikimedia/mediawiki

commit 9d1cd9ad55b47dfe3514d4b31e47708fb98a304b

Strengthened validation for HashRing a bit.
Change-Id: Ib0331662c785d806063ca730905dd88b6545d8f0

includes/HashRing.php
@@ -36,8 +36,8 @@ class HashRing {
 	 * @param array $map (location => weight)
 	 */
 	public function __construct( array $map ) {
-		$sum = array_sum( $map );
-		if ( !count( $map ) || $sum <= 0 ) {
+		$map = array_filter( $map, function( $w ) { return $w > 0; } );
+		if ( !count( $map ) ) {
 			throw new MWException( "Ring is empty or all weights are zero." );
 		}
 		// Sort the locations based on the hash of their names

commit 1b4f0555c3b1dca8f57d58b191679c1c9084a902

Fixed total breakage of HashRing in 9d1cd9a.
* This have PHP warnings but no errors, but was broken.
* Added some unit tests for this class.

Change-Id: I7c7ea2c30ca747aae7a3c3951fc33c69052de085

includes/HashRing.php
@@ -49,6 +49,7 @@ class HashRing {
 			return strcmp( $hashes[$a], $hashes[$b] );
 		} );
 		// Fit the map to weight-proportionate one with a space of size RING_SIZE
+		$sum = array_sum( $map );
 		$standardMap = array();
 		foreach ( $map as $location => $weight ) {
 			$standardMap[$location] = (int)floor( $weight / $sum * self::RING_SIZE );

tests/phpunit/includes/HashRingTest.php
@@ -1,0 +1,53 @@
+<?php
+
+/**
+ * @group HashRing
+ */
+class HashRingTest extends MediaWikiTestCase {
+	function testHashRing() {
+		$ring = new HashRing( array( 's1' => 1, 's2' => 1, 's3' => 2, 's4' => 2, 's5' => 2, 's6' => 3 ) );
+
+		$locations = array();
+		for ( $i=0; $i<20; $i++ ) {
+			$locations[ "hello$i"] = $ring->getLocation( "hello$i" );
+		}
+		$expectedLocations = array(
+			"hello0" => "s5",
+			"hello1" => "s6",
+			"hello2" => "s2",
+			"hello3" => "s5",
+			"hello4" => "s6",
+			"hello5" => "s4",
+			"hello6" => "s5",
+			"hello7" => "s4",
+			"hello8" => "s5",
+			"hello9" => "s5",
+			"hello10" => "s3",
+			"hello11" => "s6",
+			"hello12" => "s1",
+			"hello13" => "s3",
+			"hello14" => "s3",
+			"hello15" => "s5",
+			"hello16" => "s4",
+			"hello17" => "s6",
+			"hello18" => "s6",
+			"hello19" => "s3"
+		);
+
+		$this->assertEquals( $expectedLocations, $locations, 'Items placed at proper locations' );
+
+		$locations = array();
+		for ( $i=0; $i<5; $i++ ) {
+			$locations[ "hello$i"] = $ring->getLocations( "hello$i", 2 );
+		}
+
+		$expectedLocations = array(
+			"hello0" => array( "s5", "s6" ),
+			"hello1" => array( "s6", "s4" ),
+			"hello2" => array( "s2", "s1" ),
+			"hello3" => array( "s5", "s6" ),
+			"hello4" => array( "s6", "s4" ),
+		);
+		$this->assertEquals( $expectedLocations, $locations, 'Items placed at proper locations' );
+	}
+}