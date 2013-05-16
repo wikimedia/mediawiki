00df3d48abbb765b0e9c44e15b2e53f49c682806
mwjames <jamesin.hongkong.1@gmail.com>
2013-05-17 01:51:32 +0900

    SpecialStatistics::getOtherStats use standard language instead of inContentLanguage
    Use standard language instead of inContentLanguage.
    
    Bug: 48542
    Change-Id: Ib7d9cb5aa3051b5d602f7a0838425d92fc7a515f

--- includes/specials/SpecialStatistics.php
+++ includes/specials/SpecialStatistics.php
@@ -298,7 +298,7 @@ class SpecialStatistics extends SpecialPage {
 
 				// Collect all items that belong to the same header
 				foreach ( $items as $key => $value ) {
-					$name = $this->msg( $key )->inContentLanguage()->parse();
+					$name = $this->msg( $key )->parse();
 					$number = htmlspecialchars( $value );
 
 					$return .= $this->formatRow( $name, $this->getLanguage()->formatNum( $number ), array( 'class' => 'mw-statistics-hook' ) );
