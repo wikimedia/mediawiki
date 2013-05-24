c283e9d3648b22a8b392316147c6cd5e2fa66735
Felipe Schenone <schenonef@gmail.com>
2013-05-24 20:43:05 +0200

    Making the MonoBook skin more self-contained
    Most or all the following changes are guided by approach described
    in [[Manual:Skinning/Tutorial]]:
    * Moved skins/MonoBook.php to skins/monobook/monobook.php
    * Moved the MonoBook module declarations from Resources.php to monobook.php
    * Moved the MonoBook classes autoloading from AutoLoader.php to monobook.php
    * Moved the MonoBook classes from monobook.php to monobook.skin.php, to avoid them being loaded when monobook.php is loaded.
    * Added a require_once() for monobook.php to DefaultSettings.php.
    
    If this patch ends up merged, I'll do the same with the other default
    skins.
    
    Change-Id: I2a05114c573d7cc21d50bd81d1feed885fa4b6f7

--- skins/monobook/monobook.skin.php
+++ skins/monobook/monobook.skin.php
@@ -1,43 +1,15 @@
 <?php
 /**
- * MonoBook nouveau.
- *
- * Translated from gwicke's previous TAL template version to remove
- * dependency on PHPTAL.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @todo document
- * @file
- * @ingroup Skins
- */
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( -1 );
-}
-
-/**
  * Inherit main code from SkinTemplate, set the CSS and template filter.
  * @todo document
  * @ingroup Skins
  */
 class SkinMonoBook extends SkinTemplate {
 	/** Using monobook. */
-	var $skinname = 'monobook', $stylename = 'monobook',
-		$template = 'MonoBookTemplate', $useHeadElement = true;
+	var $skinname = 'monobook',
+		$stylename = 'monobook',
+		$template = 'MonoBookTemplate',
+		$useHeadElement = true;
 
 	/**
 	 * @param $out OutputPage
@@ -321,5 +293,3 @@ echo $footerEnd;
 <?php
 	}
 } // end of class
-
-
