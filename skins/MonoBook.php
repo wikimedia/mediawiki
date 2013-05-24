c1492752666feabccd44892bb1e542814b226bc1
Felipe Schenone <schenonef@gmail.com>
2013-05-24 20:43:05 +0200

    MonoBook module declarations moved from Resources.php to MonoBook.php
    The module declarations for a skin should be done from within that
    skin. So far, however, all the default skins have their module
    declarations done from within the resource loader itself. This patch
    fixes this for the MonoBook skin. In order for it to work, the skin
    files had to be removed from AutoLoader.php and loaded instead from
    the DefaultSettings.php
    
    If this patch ends up merged, I'll do the same with the other default
    skins.
    
    Change-Id: I2a05114c573d7cc21d50bd81d1feed885fa4b6f7

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -29,6 +29,17 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( -1 );
 }
 
+$wgResourceModules['skins.monobook'] = array(
+	'styles' => array(
+		'common/commonElements.css' => array( 'media' => 'screen' ),
+		'common/commonContent.css' => array( 'media' => 'screen' ),
+		'common/commonInterface.css' => array( 'media' => 'screen' ),
+		'monobook/main.css' => array( 'media' => 'screen' ),
+	),
+	'remoteBasePath' => & $GLOBALS['wgStylePath'],
+	'localBasePath' => & $GLOBALS['wgStyleDirectory'],
+);
+
 /**
  * Inherit main code from SkinTemplate, set the CSS and template filter.
  * @todo document
@@ -36,8 +47,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  */
 class SkinMonoBook extends SkinTemplate {
 	/** Using monobook. */
-	var $skinname = 'monobook', $stylename = 'monobook',
-		$template = 'MonoBookTemplate', $useHeadElement = true;
+	var $skinname = 'monobook',
+		$stylename = 'monobook',
+		$template = 'MonoBookTemplate',
+		$useHeadElement = true;
 
 	/**
 	 * @param $out OutputPage
@@ -321,5 +334,3 @@ echo $footerEnd;
 <?php
 	}
 } // end of class
-
-
