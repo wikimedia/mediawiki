e44a2ad185a9fd032cec1cd9ceed4ab8ea9b484d
Ori Livneh <ori@wikimedia.org>
2013-05-30 11:10:46 -0700

    Migrate Extension:PostEdit interface to core
    As requested by the VisualEditor team, this change migrates the post-edit
    confirmation notice from Extension:PostEdit to core.
    
    This entails porting the 'postedit-confirmation' en/qqq message from the
    extension's i18n file (the message key remains the same) to the appropriate
    message files and augmenting mediawiki.action.view.postEdit.js to not only
    signal post-edit state but also act on it by displaying the confirmation.
    
    Bug: 48276
    Change-Id: I01cfc0630c3a505af82922844b5e70c1d61f3c1d
    (cherry picked from commit 011ee683acc80a166bfc235a1cd709d7b5617c80)

--- resources/mediawiki.action/mediawiki.action.view.postEdit.js
+++ resources/mediawiki.action/mediawiki.action.view.postEdit.js
@@ -1,15 +1,45 @@
 ( function ( mw, $ ) {
-	// Only a view can be a post-edit.
-	if ( mw.config.get( 'wgAction' ) !== 'view' ) {
+	'use strict';
+
+	var config = mw.config.get( [ 'wgAction', 'wgCookiePrefix', 'wgCurRevisionId' ] ),
+		// This should match EditPage::POST_EDIT_COOKIE_KEY_PREFIX:
+		cookieKey = config.wgCookiePrefix + 'PostEditRevision' + config.wgCurRevisionId,
+		div, id;
+
+	if ( config.wgAction !== 'view' || $.cookie( cookieKey ) !== '1' ) {
 		return;
 	}
 
-	// Matches EditPage::POST_EDIT_COOKIE_KEY_PREFIX
-	var cookieKey = mw.config.get( 'wgCookiePrefix' ) + 'PostEditRevision' + mw.config.get( 'wgCurRevisionId' );
+	$.cookie( cookieKey, null, { path: '/' } );
+	mw.config.set( 'wgPostEdit', true );
 
-	if ( $.cookie( cookieKey ) === '1' ) {
-		// We just saved this page
-		$.cookie( cookieKey, null, { path: '/' } );
-		mw.config.set( 'wgPostEdit', true );
+	function removeConfirmation() {
+		div.parentNode.removeChild( div );
+		mw.hook( 'postEdit.afterRemoval' ).fire();
 	}
+
+	function fadeOutConfirmation() {
+		clearTimeout( id );
+		div.firstChild.className = 'postedit postedit-faded';
+		setTimeout( removeConfirmation, 500 );
+		return false;
+	}
+
+	function showConfirmation() {
+		div = document.createElement( 'div' );
+		div.className = 'postedit-container';
+		div.innerHTML =
+			'<div class="postedit">' +
+				'<div class="postedit-icon postedit-icon-checkmark">' +
+					mw.message( 'postedit-confirmation', mw.user ).escaped() +
+				'</div>' +
+				'<a href="#" class="postedit-close">&times;</a>' +
+			'</div>';
+		id = setTimeout( fadeOutConfirmation, 3000 );
+		div.firstChild.lastChild.onclick = fadeOutConfirmation;
+		document.body.insertBefore( div, document.body.firstChild );
+	}
+
+	mw.hook( 'postEdit' ).add( showConfirmation ).fire();
+
 } ( mediaWiki, jQuery ) );
