6040f3af90740293603c83423ae4b094880c91ae
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-06-02 13:36:30 -0400

    Make autoblocks update with the parent block
    Change SpecialBlock so that when a block already exists, it just updates
    the existing block rather than deleting and inserting a new row. Also,
    Block::update() was changed to update existing autoblocks with the new
    block parameters.
    
    Bug: 48813
    Change-Id: I5403f6e1d7f8f07273cd5da8068b928fdddcdfc9

--- includes/Block.php
+++ includes/Block.php
@@ -488,12 +488,13 @@ class Block {
 	 * Update a block in the DB with new parameters.
 	 * The ID field needs to be loaded first.
 	 *
-	 * @return Int number of affected rows, which should probably be 1 or something has
-	 *     gone slightly awry
+	 * @return bool|array False on failure, array on success: ('id' => block ID, 'autoIds' => array of autoblock IDs)
 	 */
 	public function update() {
 		wfDebug( "Block::update; timestamp {$this->mTimestamp}\n" );
 		$dbw = wfGetDB( DB_MASTER );
+
+		$dbw->startAtomic( __METHOD__ );
 
 		$dbw->update(
 			'ipblocks',
@@ -502,7 +503,23 @@ class Block {
 			__METHOD__
 		);
 
-		return $dbw->affectedRows();
+		$affected = $dbw->affectedRows();
+
+		$dbw->update(
+			'ipblocks',
+			$this->getAutoblockUpdateArray(),
+			array( 'ipb_parent_block_id' => $this->getId() ),
+			__METHOD__
+		);
+
+		$dbw->endAtomic( __METHOD__ );
+
+		if ( $affected ) {
+			$auto_ipd_ids = $this->doRetroactiveAutoblock();
+			return array( 'id' => $this->mId, 'autoIds' => $auto_ipd_ids );
+		}
+
+		return false;
 	}
 
 	/**
@@ -543,6 +560,20 @@ class Block {
 		);
 
 		return $a;
+	}
+
+	/**
+	 * @return Array
+	 */
+	protected function getAutoblockUpdateArray() {
+		return array(
+			'ipb_by'               => $this->getBy(),
+			'ipb_by_text'          => $this->getByName(),
+			'ipb_reason'           => $this->mReason,
+			'ipb_create_account'   => $this->prevents( 'createaccount' ),
+			'ipb_deleted'          => (int)$this->mHideName, // typecast required for SQLite
+			'ipb_allow_usertalk'   => !$this->prevents( 'editownusertalk' ),
+		);
 	}
 
 	/**
