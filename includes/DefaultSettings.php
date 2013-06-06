75c31178f621eb6dfbca16925c99b2906809bd77
Reedy <Reedy reedy@wikimedia.org>
2013-06-06 15:59:39 +0000

    Creating new WMF 1.22wmf6 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.22alpha';
+$wgVersion = '1.22wmf6';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
