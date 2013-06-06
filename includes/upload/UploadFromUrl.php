be9ca2f188058b2d6d0e942a542cf0c6afb7e7dc
Brian Wolff <bawolff+wn@gmail.com>
2013-06-06 00:55:18 -0300

    Have separate timeouts for upload by url ($wgCopyUpload[Async]Timeout)
    Add $wgCopyUploadTimeout and $wgCopyUploadAsyncTimeout to control
    the timeout for the http request used to fetch the file during upload
    by url. People reasonably may want to make this than $wgHTTPTimeout
    for the case where very large files are being downloaded.
    
    Also add the ability for callers of UploadFromUrl::fetchFile
    to override the timeout as they please (and set any other HTTP opts).
    
    This patch was inspired by conversation with dan-nl about gwtoolset.
    
    Change-Id: Ia85a97434c14adcdaafc0802cbe0530bfa57a435

--- includes/upload/UploadFromUrl.php
+++ includes/upload/UploadFromUrl.php
@@ -182,9 +182,13 @@ class UploadFromUrl extends UploadBase {
 	}
 
 	/**
+	 * Download the file (if not async)
+	 *
+	 * @param Array $httpOptions Array of options for MWHttpRequest. Ignored if async.
+	 *   This could be used to override the timeout on the http request.
 	 * @return Status
 	 */
-	public function fetchFile() {
+	public function fetchFile( $httpOptions = array() ) {
 		if ( !Http::isValidURI( $this->mUrl ) ) {
 			return Status::newFatal( 'http-invalid-url' );
 		}
@@ -196,7 +200,7 @@ class UploadFromUrl extends UploadBase {
 			return Status::newFatal( 'upload-copy-upload-invalid-url' );
 		}
 		if ( !$this->mAsync ) {
-			return $this->reallyFetchFile();
+			return $this->reallyFetchFile( $httpOptions );
 		}
 		return Status::newGood();
 	}
@@ -233,9 +237,12 @@ class UploadFromUrl extends UploadBase {
 	/**
 	 * Download the file, save it to the temporary file and update the file
 	 * size and set $mRemoveTempFile to true.
+	 *
+	 * @param Array $httpOptions Array of options for MWHttpRequest
 	 * @return Status
 	 */
-	protected function reallyFetchFile() {
+	protected function reallyFetchFile( $httpOptions = array() ) {
+		global $wgCopyUploadProxy, $wgCopyUploadTimeout;
 		if ( $this->mTempPath === false ) {
 			return Status::newFatal( 'tmp-create-error' );
 		}
@@ -249,12 +256,14 @@ class UploadFromUrl extends UploadBase {
 		$this->mRemoveTempFile = true;
 		$this->mFileSize = 0;
 
-		$options = array(
-			'followRedirects' => true
+		$options = $httpOptions + array(
+			'followRedirects' => true,
 		);
-		global $wgCopyUploadProxy;
 		if ( $wgCopyUploadProxy !== false ) {
 			$options['proxy'] = $wgCopyUploadProxy;
+		}
+		if ( $wgCopyUploadTimeout && !isset( $options['timeout'] ) ) {
+			$options['timeout'] = $wgCopyUploadTimeout;
 		}
 		$req = MWHttpRequest::factory( $this->mUrl, $options );
 		$req->setCallback( array( $this, 'saveTempFileChunk' ) );
