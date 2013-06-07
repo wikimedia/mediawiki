fc8bf56649c386b1b6894096b45f485d2f160d9a
Karun Dambiec <karun.84@gmx.de>
2013-06-07 15:53:12 +0200

    Bug 14931 - changed to use default character set of utf8
    Change-Id: Ied512a75d17b401206b6439f289fe36c01536cec

--- includes/installer/MysqlInstaller.php
+++ includes/installer/MysqlInstaller.php
@@ -475,7 +475,7 @@ class MysqlInstaller extends DatabaseInstaller {
 		$conn = $status->value;
 		$dbName = $this->getVar( 'wgDBname' );
 		if ( !$conn->selectDB( $dbName ) ) {
-			$conn->query( "CREATE DATABASE " . $conn->addIdentifierQuotes( $dbName ), __METHOD__ );
+            $conn->query( "CREATE DATABASE " . $conn->addIdentifierQuotes( $dbName ) . "CHARACTER SET utf8", __METHOD__ );
 			$conn->selectDB( $dbName );
 		}
 		$this->setupSchemaVars();
