3342fafb4839a27b0be3a6e02149e1accf63cc87
Brian Wolff <bawolff+wn@gmail.com>
2013-06-07 14:29:56 -0300

    Make Special:MIMESearch a non-expensive special page.
    This tries to use the index on (img_media_type, img_major_mime, img_minor_mime).
    This is not the ideal index for this purpose (due to starting with
    img_media_type, and not having an img_name at the end). However it
    seems usable due to the low number of values img_media_type can have.
    Thus we can just specify all values, and the index will get used
    as a range index.
    
    The only potentially problematic thing I see is that it uses
    LIMIT X,Y for the offsets (due to being a query page) which can
    be expensive on big offsets. If that's an issue, we could limit
    the number of results to be at most $wgQueryCacheLimit or something.
    
    Note: There's some discussion on the bug rather negative to this
    special page in general. While I agree with much of the criticism,
    I still think this special page can be useful, and if it can be
    made efficient without needing to modify the indicies, we might
    as well.
    
    Bug: 13438
    Change-Id: I88826ae7bff0812374157e596f86a19b248220e1

--- includes/specials/SpecialMIMEsearch.php
+++ includes/specials/SpecialMIMEsearch.php
@@ -35,7 +35,7 @@ class MIMEsearchPage extends QueryPage {
 	}
 
 	function isExpensive() {
-		return true;
+		return false;
 	}
 
 	function isSyndicated() {
@@ -51,12 +51,14 @@ class MIMEsearchPage extends QueryPage {
 	}
 
 	public function getQueryInfo() {
-		return array(
+		$qi = array(
 			'tables' => array( 'image' ),
 			'fields' => array(
 				'namespace' => NS_FILE,
 				'title' => 'img_name',
-				'value' => 'img_major_mime',
+				// Still have a value field just in case,
+				// but it isn't actually used for sorting.
+				'value' => 'img_name',
 				'img_size',
 				'img_width',
 				'img_height',
@@ -65,9 +67,36 @@ class MIMEsearchPage extends QueryPage {
 			),
 			'conds' => array(
 				'img_major_mime' => $this->major,
-				'img_minor_mime' => $this->minor
-			)
+				'img_minor_mime' => $this->minor,
+				// This is in order to trigger using
+				// the img_media_mime index in "range" mode.
+				'img_media_type' => array(
+					MEDIATYPE_BITMAP,
+					MEDIATYPE_DRAWING,
+					MEDIATYPE_AUDIO,
+					MEDIATYPE_VIDEO,
+					MEDIATYPE_MULTIMEDIA,
+					MEDIATYPE_UNKNOWN,
+					MEDIATYPE_OFFICE,
+					MEDIATYPE_TEXT,
+					MEDIATYPE_EXECUTABLE,
+					MEDIATYPE_ARCHIVE,
+				),
+			),
 		);
+		return $qi;
+	}
+
+	/**
+	 * The index is on (img_media_type, img_major_mime, img_minor_mime)
+	 * which unfortunately doesn't have img_name at the end for sorting.
+	 * So tell db to sort it however it wishes (Its not super important
+	 * that this report gives results in a logical order). As an aditional
+	 * note, mysql seems to by default order things by img_name ASC, which
+	 * is what we ideally want, so everything works out fine anyhow.
+	 */
+	function getOrderFields() {
+		return array();
 	}
 
 	function execute( $par ) {
