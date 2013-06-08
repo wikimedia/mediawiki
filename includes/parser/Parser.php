e2566385717945f9686bb8905ce8dcfa8efceb32
Brian Wolff <bawolff+wn@gmail.com>
2013-06-08 01:47:07 -0300

    New more slick gallery display
    This extension adds a "mode" parameter to the gallery
    tag, allowing different formats for the gallery tag
    (galleries in the ui can be controlled by a global)
    The added modes are:
    *traditional - The original gallery
    *nolines - Like the original, no borders, less padding
    *packed - All images aligned by having same height.
      JS also justifies the images.
      (I think this one is the one that will go over best
      with users.)
    *packed-overlay - like packed, but caption goes over
      top the image in a transloucent box.
    *packed-hover - like packed-overlay, but caption only
      visible on hover. Degrades gracefully on screen
      readers, and falls back to packed-overlay if
      you are using a touch screen. I kind of like
      this mode when the caption is not that important
      (ex a category where its just the file name).
    
    This also adds a hook to allow people to make their
    own gallery version. I believe there would be interest
    in this, as different people have done different
    experiments. For example:
    * Wikia: http://community.wikia.com/wiki/Help:Galleries,_Slideshows,_and_Sliders/wikitext
    * Wikinews: https://en.wikinews.org/wiki/Template:Picture_select
    
    What I would like to see for this patch, is first it gets
    enabled, with the default still "traditional". After
    about a month or two we consult with users. If feedback
    is positive, we change the default mode to one of the
    others (probably "packed").
    
    Adds a "mode" parameter to gallery for different
    mode, including one 'height-constrained-overlay'
    which looks much more like other modern websites.
    
    Note: This makes one change to the old gallery format.
    It makes Nonexistent files be rendered like thumbnails
    (i.e. they are rendered with a little grey border).
    
    One thing I'm slightly worried about with this patch,
    is that I added an option to MediaTransformOutput::toHtml
    to override the width attribute. I'm not sure if that
    is the best approach, and would appreciate thoughts
    on that.
    
    This should be merged at the same time as Ie82c1548
    
    Change-Id: I33462a8b52502ed76aeb163b66e3704c8618ba23

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -5034,7 +5034,19 @@ class Parser {
 	 */
 	function renderImageGallery( $text, $params ) {
 		wfProfileIn( __METHOD__ );
-		$ig = new ImageGallery();
+
+		$mode = false;
+		if ( isset( $params['mode'] ) ) {
+			$mode = $params['mode'];
+		}
+
+		try {
+			$ig = ImageGalleryBase::factory( $mode );
+		} catch ( MWException $e ) {
+			// If invalid type set, fallback to default.
+			$ig = ImageGalleryBase::factory( false );
+		}
+
 		$ig->setContextTitle( $this->mTitle );
 		$ig->setShowBytes( false );
 		$ig->setShowFilename( false );
@@ -5062,6 +5074,7 @@ class Parser {
 		if ( isset( $params['heights'] ) ) {
 			$ig->setHeights( $params['heights'] );
 		}
+		$ig->setAdditionalOptions( $params );
 
 		wfRunHooks( 'BeforeParserrenderImageGallery', array( &$this, &$ig ) );
 
