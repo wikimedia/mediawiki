93176a2c96c502a74703a565b13a7830505305a8
MatmaRex <matma.rex@gmail.com>
2013-06-14 18:59:59 +0200

    Refactor watchlist token handling
    Do not allow the user to change it directly; instead create a form
    where they can reset it. (The token can still be changed via the API.)
    The token is autogenerated whenever it is be shown or otherwise used.
    
    This really should have never used the preferences; however, trying to
    change that now would be lots of work for very little gain, so this
    keeps using that mechanism, adding a little abstraction over it.
    
    It's not unconceivable that similar tokens could be used for other
    pieces of data, like Echo's notifications; this enables that with one
    new hook.
    
    ----
    
    Things done here:
    
    * Add getTokenFromOption() and resetTokenFromOption() methods to User,
      abstracting out the get-and-generate-if-empty process of handling
      tokens. Respect $wgHiddenPrefs (Watchlist didn't do that
      previously).
    
    * Create Special:ResetTokens, inspired by Special:Preferences and
      Special:ChangeEmail, presenting the token resetting interface
      (HTMLForm-based with CSRF protection).
    
    * Create a new hook, SpecialResetTokensTokens, allowing extensions to
      register tokens to be shown in the resetting form. Each token needs
      information about the preference it corresponds to and a short
      description (used for checkbox label).
    
    * Hide the preference on Special:Preferences (use type=api to achieve
      this), display a link to aforementioned special page instead. Move
      info blurb to its own section at the bottom.
    
    Bug: 21912
    Change-Id: I0bdd2469972c4af81bfb480e9dde58cdd14c67a8

---
+++ includes/specials/SpecialResetTokens.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Implements Special:ResetTokens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup SpecialPage
+ */
+
+/**
+ * Let users reset tokens like the watchlist token.
+ *
+ * @ingroup SpecialPage
+ */
+class SpecialResetTokens extends FormSpecialPage {
+	public function __construct() {
+		parent::__construct( 'ResetTokens' );
+	}
+
+	/**
+	 * Returns the token information list for this page after running
+	 * the hook and filtering out disabled preferences.
+	 *
+	 * @return array
+	 */
+	protected function getTokensList() {
+		global $wgHiddenPrefs;
+
+		if ( !isset( $this->tokensList ) ) {
+			$tokens = array(
+				array( 'preference' => 'watchlisttoken', 'label-message' => 'resettokens-watchlist-token' ),
+			);
+			wfRunHooks( 'SpecialResetTokensTokens', array( &$tokens ) );
+
+			$tokens = array_filter( $tokens, function ( $tok ) use ( $wgHiddenPrefs ) {
+				return !in_array( $tok['preference'], $wgHiddenPrefs );
+			} );
+
+			$this->tokensList = $tokens;
+		}
+
+		return $this->tokensList;
+	}
+
+	public function execute( $par ) {
+		// This is a preferences page, so no user JS for y'all.
+		$this->getOutput()->disallowUserJs();
+
+		parent::execute( $par );
+
+		$this->getOutput()->addReturnTo( SpecialPage::getTitleFor( 'Preferences' ) );
+	}
+
+	public function onSuccess() {
+		$this->getOutput()->wrapWikiMsg(
+			"<div class='successbox'>\n$1\n</div>",
+			'resettokens-done'
+		);
+	}
+
+	/**
+	 * Display appropriate message if there's nothing to do.
+	 * The submit button is also suppressed in this case (see alterForm()).
+	 */
+	protected function getFormFields() {
+		$user = $this->getUser();
+		$tokens = $this->getTokensList();
+
+		if ( $tokens ) {
+			$tokensForForm = array();
+			foreach ( $tokens as $tok ) {
+				$label = $this->msg( 'resettokens-token-label' )
+					->rawParams( $this->msg( $tok['label-message'] )->text() )
+					->params( $user->getTokenFromOption( $tok['preference'] ) )
+					->text();
+				$tokensForForm[ $label ] = $tok['preference'];
+			}
+
+			$desc = array(
+				'label-message' => 'resettokens-tokens',
+				'type' => 'multiselect',
+				'options' => $tokensForForm,
+			);
+		} else {
+			$desc = array(
+				'label-message' => 'resettokens-no-tokens',
+				'type' => 'info',
+			);
+		}
+
+		return array(
+			'tokens' => $desc,
+		);
+	}
+
+	/**
+	 * Suppress the submit button if there's nothing to do;
+	 * provide additional message on it otherwise.
+	 */
+	protected function alterForm( HTMLForm $form ) {
+		if ( $this->getTokensList() ) {
+			$form->setSubmitTextMsg( 'resettokens-resetbutton' );
+		} else {
+			$form->suppressDefaultSubmit();
+		}
+	}
+
+	public function onSubmit( array $formData ) {
+		$user = $this->getUser();
+		foreach ( $formData['tokens'] as $tokenPref ) {
+			$user->resetTokenFromOption( $tokenPref );
+		}
+
+		// Only claim success if anything was done.
+		return (bool)$formData['tokens'];
+	}
+
+	protected function getGroupName() {
+		return 'users';
+	}
+
+	public function isListed() {
+		return (bool)$this->getTokensList();
+	}
+}
