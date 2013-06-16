25d1af122d9de5a5c8890b170d330d1cf1e7f7a9
Aaron Schulz <aschulz@wikimedia.org>
2013-06-16 13:48:17 -0700

    lockmanager: QuorumLockManager subclasses can get EX/SH locks at once
    * Also reduced rount trips in doUnlockingRequestBucket().
    * Also removed some redundant doc comments.
    
    Change-Id: I81878e92332509bd7fda9ddeef950b774f5b015d

--- includes/filebackend/lockmanager/MemcLockManager.php
+++ includes/filebackend/lockmanager/MemcLockManager.php
@@ -88,11 +88,44 @@ class MemcLockManager extends QuorumLockManager {
 		$this->session = wfRandomString( 32 );
 	}
 
+	// @TODO: change this code to work in one batch
+	protected function getLocksOnServer( $lockSrv, array $pathsByType ) {
+		$status = Status::newGood();
+
+		$lockedPaths = array();
+		foreach ( $pathsByType as $type => $paths ) {
+			$status->merge( $this->doGetLocksOnServer( $lockSrv, $paths, $type ) );
+			if ( $status->isOK() ) {
+				$lockedPaths[$type] = isset( $lockedPaths[$type] )
+					? array_merge( $lockedPaths[$type], $paths )
+					: $paths;
+			} else {
+				foreach ( $lockedPaths as $type => $paths ) {
+					$status->merge( $this->doFreeLocksOnServer( $lockSrv, $paths, $type ) );
+				}
+				break;
+			}
+		}
+
+		return $status;
+	}
+
+	// @TODO: change this code to work in one batch
+	protected function freeLocksOnServer( $lockSrv, array $pathsByType ) {
+		$status = Status::newGood();
+
+		foreach ( $pathsByType as $type => $paths ) {
+			$status->merge( $this->doFreeLocksOnServer( $lockSrv, $paths, $type ) );
+		}
+
+		return $status;
+	}
+
 	/**
 	 * @see QuorumLockManager::getLocksOnServer()
 	 * @return Status
 	 */
-	protected function getLocksOnServer( $lockSrv, array $paths, $type ) {
+	protected function doGetLocksOnServer( $lockSrv, array $paths, $type ) {
 		$status = Status::newGood();
 
 		$memc = $this->getCache( $lockSrv );
@@ -164,7 +197,7 @@ class MemcLockManager extends QuorumLockManager {
 	 * @see QuorumLockManager::freeLocksOnServer()
 	 * @return Status
 	 */
-	protected function freeLocksOnServer( $lockSrv, array $paths, $type ) {
+	protected function doFreeLocksOnServer( $lockSrv, array $paths, $type ) {
 		$status = Status::newGood();
 
 		$memc = $this->getCache( $lockSrv );
