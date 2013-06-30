758990acd49f35785cef926f3d00ca7f1eb32887
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-06-30 22:45:45 +0200

    Fix double-parsing of account creation messages.
    Account creation messages don't need to be parsed. This is
    a temporary fix to follow up when double-parsing was accidentally
    added in 69ea4400037 (I402c6bebcfe).
    
    Bug: 44718
    Change-Id: I333d5468820994625348316ebf6c57d4df025284

--- includes/Status.php
+++ includes/Status.php
@@ -183,19 +183,66 @@ class Status {
 			}
 		}
 		if ( count( $this->errors ) == 1 ) {
-			$s = $this->getErrorMessage( $this->errors[0] );
+			$s = $this->getErrorMessage( $this->errors[0] )->plain();
 			if ( $shortContext ) {
 				$s = wfMessage( $shortContext, $s )->plain();
 			} elseif ( $longContext ) {
 				$s = wfMessage( $longContext, "* $s\n" )->plain();
 			}
 		} else {
-			$s = '* ' . implode( "\n* ",
-				$this->getErrorMessageArray( $this->errors ) ) . "\n";
+			$errors = $this->getErrorMessageArray( $this->errors );
+			foreach ( $errors as &$error ) {
+				$error = $error->plain();
+			}
+			$s = '* ' . implode( "\n* ", $errors ) . "\n";
 			if ( $longContext ) {
 				$s = wfMessage( $longContext, $s )->plain();
 			} elseif ( $shortContext ) {
 				$s = wfMessage( $shortContext, "\n$s\n" )->plain();
+			}
+		}
+		return $s;
+	}
+
+	/**
+	 * Get the error list as a Message object
+	 *
+	 * @param string $shortContext a short enclosing context message name, to
+	 *        be used when there is a single error
+	 * @param string $longContext a long enclosing context message name, for a list
+	 * @param bool $raw Whether to make the message entirely raw
+	 * @return Message
+	 */
+	function getMessage( $shortContext = false, $longContext = false, $raw = false ) {
+		if ( count( $this->errors ) == 0 ) {
+			if ( $this->ok ) {
+				$this->fatal( 'internalerror_info',
+					__METHOD__ . " called for a good result, this is incorrect\n" );
+			} else {
+				$this->fatal( 'internalerror_info',
+					__METHOD__ . ": Invalid result object: no error text but not OK\n" );
+			}
+		}
+		if ( count( $this->errors ) == 1 ) {
+			$s = $this->getErrorMessage( $this->errors[0] );
+			if ( $shortContext ) {
+				$s = wfMessage( $shortContext, $s );
+			} elseif ( $longContext ) {
+				$wrapper = new RawMessage( "* \$1\n" );
+				$wrapper->params( $s )->parse();
+				$s = wfMessage( $longContext, $wrapper );
+			}
+		} else {
+			$msgs =  $this->getErrorMessageArray( $this->errors );
+			$wrapper = new RawMessage( '* $' . implode( "\n* \$", range( 1, count( $msgs ) + 1 ) ) );
+			$wrapper->params( $msgs )->parse();
+
+			if ( $longContext ) {
+				$s = wfMessage( $longContext, $wrapper );
+			} elseif ( $shortContext ) {
+				$wrapper = new RawMessage( "\n\$1\n", $wrapper );
+				$wrapper->parse();
+				$s = wfMessage( $shortContext, $wrapper );
 			}
 		}
 		return $s;
@@ -217,15 +264,18 @@ class Status {
 			} elseif ( isset( $error['message'] ) && isset( $error['params'] ) ) {
 				$msg = wfMessage( $error['message'],
 					array_map( 'wfEscapeWikiText', $this->cleanParams( $error['params'] ) ) );
+				$msg->parse();
 			} else {
 				$msgName = array_shift( $error );
 				$msg = wfMessage( $msgName,
 					array_map( 'wfEscapeWikiText', $this->cleanParams( $error ) ) );
+				$msg->parse();
 			}
 		} else {
 			$msg = wfMessage( $error );
+			$msg->parse();
 		}
-		return $msg->plain();
+		return $msg;
 	}
 
 	/**
@@ -362,15 +412,6 @@ class Status {
 			}
 		}
 		return $replaced;
-	}
-
-	/**
-	 * Backward compatibility function for WikiError -> Status migration
-	 *
-	 * @return String
-	 */
-	public function getMessage() {
-		return $this->getWikiText();
 	}
 
 	/**
