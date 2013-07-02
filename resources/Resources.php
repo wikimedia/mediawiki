299c326c8ad8d34dd12ad77dbdb05426655265bf
MatmaRex <matma.rex@gmail.com>
2013-07-02 16:46:25 +0200

    Provide month names via mediawiki.language.months RL module
    * Deliver appropriate messages via ResourceLoader, for nominative-case,
      genitive-case and abbreviated month names
    * Define mw.language.months which provides access to the above data in
      a structured manner
    
    This supersedes the old way to of accessing these using wgMonthNames
    and wgMonthNamesShort mw.config variables.
    
    Genitive month names were previously not available at all.
    
    Bug: 46496
    Change-Id: I9f48b860d1078a31d620f6f95dde1f19c1c31f79

--- resources/Resources.php
+++ resources/Resources.php
@@ -835,6 +835,16 @@ return array(
 		'targets' => array( 'desktop', 'mobile' ),
 	),
 
+	'mediawiki.language.months' => array(
+		'scripts' => 'resources/mediawiki.language/mediawiki.language.months.js',
+		'dependencies' => 'mediawiki.language',
+		'messages' => array_merge(
+			Language::$mMonthMsgs,
+			Language::$mMonthGenMsgs,
+			Language::$mMonthAbbrevMsgs
+		)
+	),
+
 	/* MediaWiki Libs */
 
 	'mediawiki.libs.jpegmeta' => array(
