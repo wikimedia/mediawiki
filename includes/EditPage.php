0753c3782367f931531146c21cacf7c5d8035de6
umherirrender <umherirrender_de.wp@web.de>
2013-07-12 22:39:59 +0200

    Do not parse param of newsectionsummary on new section preview
    Summary preview of sectio=new shows expand templates. Parsing the
    summary text as raw param avoids this. Other usages of
    'newsectionsummary' does it the same way.
    
    Bug: 40453
    Change-Id: I1fd3adfc64c664dfd490c11bfe60924fa215875c

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2540,7 +2540,7 @@ class EditPage {
 		global $wgParser;
 
 		if ( $isSubjectPreview ) {
-			$summary = wfMessage( 'newsectionsummary', $wgParser->stripSectionName( $summary ) )
+			$summary = wfMessage( 'newsectionsummary' )->rawParams( $wgParser->stripSectionName( $summary ) )
 				->inContentLanguage()->text();
 		}
 
