416ea0d3b51ca4212fa026d575bc4fc91d3d798b
Timo Tijhof <krinklemail@gmail.com>
2013-07-18 02:45:16 +0200

    Move firing of "wikipage.content" mw.hook out of mediawiki.util
    * Update "wikipage.content" arguments to really pass the
      content and not the entire body wrapper (no longer
      mw.util.$content).
    
    * Move the firing of "wikipage.content" from mediawiki.util to
      mediawiiki.page.startup. It didnt really belong in
      mediawiki.util, and now that we're no longer passing it
      mw.util.$content they're no longer related in either direction.
    
    Bug: 30713
    Change-Id: I56d6491527547f898e239b531c98e903863f8999

--- resources/mediawiki.page/mediawiki.page.startup.js
+++ resources/mediawiki.page/mediawiki.page.startup.js
@@ -8,11 +8,15 @@
 		.addClass( 'client-js' )
 		.removeClass( 'client-nojs' );
 
-	// Initialize utilities as soon as the document is ready (mw.util.$content,
-	// messageBoxNew, profile, tooltip access keys, Table of contents toggle, ..).
-	// Enqueued into domready from here instead of mediawiki.page.ready to ensure that it gets enqueued
-	// before other modules hook into document ready, so that mw.util.$content (defined by mw.util.init),
-	// is defined for them.
-	$( document ).ready( mw.util.init );
+	$( function () {
+		// Initialize utilities as soon as the document is ready (mw.util.$content,
+		// messageBoxNew, profile, tooltip access keys, Table of contents toggle, ..).
+		// In the domready here instead of in mediawiki.page.ready to ensure that it gets enqueued
+		// before other modules hook into domready, so that mw.util.$content (defined by
+		// mw.util.init), is defined for them.
+		mw.util.init();
+
+		mw.hook( 'wikipage.content' ).fire( $( '#mw-content-text' ) );
+	} );
 
 }( mediaWiki, jQuery ) );
