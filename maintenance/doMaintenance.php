fcfafa4bf42bbd77a9c08b85ca87c273818db57f
aude <aude.wiki@gmail.com>
2013-07-18 10:28:01 +0000

    Remove unused WMF-specific code
    note that wmf-config/wikimedia-mode does not even exist anymore!
    
    the mwscript and wmf-specific maintenance scripts work fine w/o this code, as far as I know and can test.
    
    Change-Id: I16d4ea6b8eb111f2058023a908a9fcb5299753b1

--- maintenance/doMaintenance.php
+++ maintenance/doMaintenance.php
@@ -79,15 +79,6 @@ if ( defined( 'MW_CONFIG_CALLBACK' ) ) {
 	# Use a callback function to configure MediaWiki
 	MWFunction::call( MW_CONFIG_CALLBACK );
 } else {
-	if ( file_exists( "$IP/../wmf-config/wikimedia-mode" ) ) {
-		// Load settings, using wikimedia-mode if needed
-		// @todo FIXME: Replace this hack with general farm-friendly code
-		# @todo FIXME: Wikimedia-specific stuff needs to go away to an ext
-		# Maybe a hook?
-		global $cluster;
-		$cluster = 'pmtpa';
-		require MWInit::interpretedPath( '../wmf-config/wgConf.php' );
-	}
 	// Require the configuration (probably LocalSettings.php)
 	require $maintenance->loadSettings();
 }
