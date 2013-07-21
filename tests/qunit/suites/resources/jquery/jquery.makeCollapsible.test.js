6413da83256b578680cf15d49c8ded565f21aaea
MatmaRex <matma.rex@gmail.com>
2013-07-21 14:42:02 +0200

    jquery.makeCollapsible: Tests for collapsible <table>, <ul>, <ol>
    Also expanded basic tests for <div>.
    
    Change-Id: I33b067a70f8ea9a788c687c793fbf561db083a34

--- tests/qunit/suites/resources/jquery/jquery.makeCollapsible.test.js
+++ tests/qunit/suites/resources/jquery/jquery.makeCollapsible.test.js
@@ -46,22 +46,106 @@
 		$toggle.trigger( 'click' );
 	} );
 
-	QUnit.asyncTest( 'basic operation (<div>)', 3, function ( assert ) {
-		var $collapsible, $content;
+	QUnit.asyncTest( 'basic operation (<div>)', 5, function ( assert ) {
+		var $collapsible, $content, $toggle;
 		$collapsible = prepareCollapsible(
 			'<div class="mw-collapsible">' + loremIpsum + '</div>'
 		);
 		$content = $collapsible.find( '.mw-collapsible-content' );
+		$toggle = $collapsible.find( '.mw-collapsible-toggle' );
 
 		assert.equal( $content.length, 1, 'content is present' );
+		assert.equal( $content.find( $toggle ).length, 0, 'toggle is not a descendant of content' );
+
 		assert.assertTrue( $content.is( ':visible' ), 'content is visible' );
 
 		$collapsible.on( 'afterCollapse.mw-collapsible', function () {
 			assert.assertTrue( $content.is( ':hidden' ), 'after collapsing: content is hidden' );
-			QUnit.start();
+
+			$collapsible.on( 'afterExpand.mw-collapsible', function () {
+				assert.assertTrue( $content.is( ':visible' ), 'after expanding: content is visible' );
+				QUnit.start();
+			} );
+
+			$toggle.trigger( 'click' );
 		} );
 
-		$collapsible.find( '.mw-collapsible-toggle' ).trigger( 'click' );
+		$toggle.trigger( 'click' );
+	} );
+
+	QUnit.asyncTest( 'basic operation (<table>)', 7, function ( assert ) {
+		var $collapsible, $headerRow, $contentRow, $toggle;
+		$collapsible = prepareCollapsible(
+			'<table class="mw-collapsible">' +
+				'<tr><td>' + loremIpsum + '</td><td>' + loremIpsum + '</td></tr>' +
+				'<tr><td>' + loremIpsum + '</td><td>' + loremIpsum + '</td></tr>' +
+			'</table>'
+		);
+		$headerRow = $collapsible.find( 'tr:first' );
+		$contentRow = $collapsible.find( 'tr:last' );
+
+		$toggle = $headerRow.find( 'td:last .mw-collapsible-toggle' );
+		assert.equal( $toggle.length, 1, 'toggle is added to last cell of first row' );
+
+		assert.assertTrue( $headerRow.is( ':visible' ), 'headerRow is visible' );
+		assert.assertTrue( $contentRow.is( ':visible' ), 'contentRow is visible' );
+
+		$collapsible.on( 'afterCollapse.mw-collapsible', function () {
+			assert.assertTrue( $headerRow.is( ':visible' ), 'after collapsing: headerRow is still visible' );
+			assert.assertTrue( $contentRow.is( ':hidden' ), 'after collapsing: contentRow is hidden' );
+
+			$collapsible.on( 'afterExpand.mw-collapsible', function () {
+				assert.assertTrue( $headerRow.is( ':visible' ), 'after expanding: headerRow is still visible' );
+				assert.assertTrue( $contentRow.is( ':visible' ), 'after expanding: contentRow is visible' );
+				QUnit.start();
+			} );
+
+			$toggle.trigger( 'click' );
+		} );
+
+		$toggle.trigger( 'click' );
+	} );
+
+	QUnit.asyncTest( 'basic operation (<ul>, <ol>)', 16, function ( assert ) {
+		var done = 0;
+		$.each( ['ul', 'ol'], function ( index, listType ) {
+			var $collapsible, $toggleItem, $firstItem, $toggle;
+			$collapsible = prepareCollapsible(
+				'<' + listType + ' class="mw-collapsible">' +
+					'<li>' + loremIpsum + '</li>' +
+					'<li>' + loremIpsum + '</li>' +
+				'</' + listType + '>'
+			);
+			$toggleItem = $collapsible.find( 'li.mw-collapsible-toggle-li:first-child' );
+			$firstItem = $collapsible.find( 'li:eq(1)' ); // actually second
+
+			$toggle = $toggleItem.find( '.mw-collapsible-toggle' );
+			assert.equal( $toggle.length, 1, 'toggle is present, added inside new zeroth list item' );
+
+			assert.equal( $firstItem.attr( 'value' ), '1', 'numeral order is forcefully preserved' );
+
+			assert.assertTrue( $toggleItem.is( ':visible' ), 'toggleItem is visible' );
+			assert.assertTrue( $firstItem.is( ':visible' ), 'firstItem is visible' );
+
+			$collapsible.on( 'afterCollapse.mw-collapsible', function () {
+				assert.assertTrue( $toggleItem.is( ':visible' ), 'after collapsing: toggleItem is still visible' );
+				assert.assertTrue( $firstItem.is( ':hidden' ), 'after collapsing: firstItem is hidden' );
+
+				$collapsible.on( 'afterExpand.mw-collapsible', function () {
+					assert.assertTrue( $toggleItem.is( ':visible' ), 'after expanding: toggleItem is still visible' );
+					assert.assertTrue( $firstItem.is( ':visible' ), 'after expanding: firstItem is visible' );
+
+					done++;
+					if ( done === 2 ) {
+						QUnit.start();
+					}
+				} );
+
+				$toggle.trigger( 'click' );
+			} );
+
+			$toggle.trigger( 'click' );
+		} );
 	} );
 
 	QUnit.test( 'basic operation when synchronous (options.instantHide)', 2, function ( assert ) {
