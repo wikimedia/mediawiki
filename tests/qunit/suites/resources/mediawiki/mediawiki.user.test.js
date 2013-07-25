164bcc780e3d52543f7817926bf133656bc1df56
Matthew Flaschen <mflaschen@wikimedia.org>
2013-07-25 01:49:40 -0700

    Add getId to mw.user, returning wgUserId (or 0 for anons).
    This is equivalent to how User->getId works on the server.
    
    Small formatting update to another documentation block.
    
    Change-Id: I31906398f572dd4f62d89b53dfd05a362bf6d5a5

--- tests/qunit/suites/resources/mediawiki/mediawiki.user.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.user.test.js
@@ -5,7 +5,7 @@
 		assert.ok( mw.user.options instanceof mw.Map, 'options instance of mw.Map' );
 	} );
 
-	QUnit.test( 'user status', 9, function ( assert ) {
+	QUnit.test( 'user status', 11, function ( assert ) {
 		/**
 		 * Tests can be run under three different conditions:
 		 *   1) From tests/qunit/index.html, user will be anonymous.
@@ -15,19 +15,23 @@
 
 		// Forge an anonymous user:
 		mw.config.set( 'wgUserName', null );
+		delete mw.config.values.wgUserId;
 
 		assert.strictEqual( mw.user.getName(), null, 'user.getName() returns null when anonymous' );
 		assert.strictEqual( mw.user.name(), null, 'user.name() compatibility' );
 		assert.assertTrue( mw.user.isAnon(), 'user.isAnon() returns true when anonymous' );
 		assert.assertTrue( mw.user.anonymous(), 'user.anonymous() compatibility' );
+		assert.strictEqual( mw.user.getId(), 0, 'user.getId() returns 0 when anonymous' );
 
 		// Not part of startUp module
 		mw.config.set( 'wgUserName', 'John' );
+		mw.config.set( 'wgUserId', 123 );
 
 		assert.equal( mw.user.getName(), 'John', 'user.getName() returns username when logged-in' );
 		assert.equal( mw.user.name(), 'John', 'user.name() compatibility' );
 		assert.assertFalse( mw.user.isAnon(), 'user.isAnon() returns false when logged-in' );
 		assert.assertFalse( mw.user.anonymous(), 'user.anonymous() compatibility' );
+		assert.strictEqual( mw.user.getId(), 123, 'user.getId() returns correct ID when logged-in' );
 
 		assert.equal( mw.user.id(), 'John', 'user.id Returns username when logged-in' );
 	} );
