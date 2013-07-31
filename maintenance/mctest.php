dff4f11ca1b0682441359b884c53b41422c2a284
Antoine Musso <hashar@free.fr>
2013-07-31 14:25:30 +0200

    mctest.php: nicely align server names
    The servers entries being made of an IP address and a port, they would
    often have different lenghts which clutter the output indentation.
    
    Previously the output columns would be slightly misaligned:
    
    $ mwscript mctest.php --wiki=enwiki
    10.4.1.255:11211 set: 100   time: 0.17192602157593
    10.4.1.1:11211 set: 100   time: 0.19056081771851
    $
    
    With this patch:
    
    $ mwscript mctest.php --wiki=enwiki
    10.4.1.255:11211 set: 100   time: 0.17192602157593
    10.4.1.1:11211   set: 100   time: 0.19056081771851
    $
    
    Change-Id: I80199a229ed6310686411f08e0344c7d906414ea

--- maintenance/mctest.php
+++ maintenance/mctest.php
@@ -61,8 +61,19 @@ class mcTest extends Maintenance {
 			$this->error( "MediaWiki isn't configured for Memcached usage", 1 );
 		}
 
+		# find out the longest server string to nicely align output later on
+		$maxSrvLen = array_reduce( $servers, function ( $size, $server ) {
+				return max( $size, strlen( $server ) );
+			},
+			0  # initial size value
+		);
+
 		foreach ( $servers as $server ) {
-			$this->output( $server . " ", $server );
+			$this->output(
+				str_pad( $server, $maxSrvLen ),
+				$server  # output channel
+			);
+
 			$mcc = new MemCachedClientforWiki( array(
 				'persistant' => true,
 				'timeout' => $wgMemCachedTimeout
