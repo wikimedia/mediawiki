95a8974c6bda2c6353612c40b01b9c78527b8956
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-07-31 05:11:18 +0000

    Added password hashing API
    Deprecated the old User::crypt, et. al password hashing
    system and implemented an extensible password hashing
    API.
    
    The new Password class allows registering of child classes
    and provides factory functions for creating new Password
    objects. The built-in hash types are the old MediaWiki MD5
    types, which are for backwards-compatibility only, and bcrypt.
    
    Also included is support for wrapping existing hashes as well
    as encrypting passwords with a configured encryption key.
    
    Bug: 54948
    Bug: 28419
    Change-Id: I0a9c972931a0eff0cfb2619cef3ddffd03710285

---
+++ includes/password/MWSaltedPassword.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Implements the BcryptPassword class for the MediaWiki software.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * The old style of MediaWiki password hashing, with a salt. It involves
+ * running MD5 on the password, and then running MD5 on the salt concatenated
+ * with the first hash.
+ *
+ * @since 1.24
+ */
+class MWSaltedPassword extends ParameterizedPassword {
+	protected function getDefaultParams() {
+		return array();
+	}
+
+	protected function getDelimiter() {
+		return ':';
+	}
+
+	public function crypt( $plaintext ) {
+		if ( count( $this->args ) == 0 ) {
+			$this->args[] = MWCryptRand::generateHex( 8 );
+		}
+
+		$this->hash = md5( $this->args[0] . '-' . md5( $plaintext ) );
+	}
+}
