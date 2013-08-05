2ca9913e070695ebd0972049ed94d4678245a33a
addshore <addshorewiki@gmail.com>
2013-08-05 22:33:07 +0200

    Can now get specific tokens in API tests
    Also can now get tokens with a default user of $wgUser
    Which should always be the last user to login
    Change-Id: Ibc2c678b82464fe3c3de386e0c5fdfc84b9cc918

--- tests/phpunit/includes/api/ApiWatchTest.php
+++ tests/phpunit/includes/api/ApiWatchTest.php
@@ -12,20 +12,14 @@ class ApiWatchTest extends ApiTestCase {
 		$this->doLogin();
 	}
 
-	function getTokens() {
-		return $this->getTokenList( self::$users['sysop'] );
-	}
-
 	/**
 	 */
 	function testWatchEdit() {
-		$tokens = $this->getTokens();
-
 		$data = $this->doApiRequest( array(
 			'action' => 'edit',
 			'title' => 'Help:UTPage', // Help namespace is hopefully wikitext
 			'text' => 'new text',
-			'token' => $tokens['edittoken'],
+			'token' => $this->getTokens( 'edittoken' ),
 			'watchlist' => 'watch' ) );
 		$this->assertArrayHasKey( 'edit', $data[0] );
 		$this->assertArrayHasKey( 'result', $data[0]['edit'] );
@@ -38,8 +32,6 @@ class ApiWatchTest extends ApiTestCase {
 	 * @depends testWatchEdit
 	 */
 	function testWatchClear() {
-		$tokens = $this->getTokens();
-
 		$data = $this->doApiRequest( array(
 			'action' => 'query',
 			'list' => 'watchlist' ) );
@@ -52,7 +44,7 @@ class ApiWatchTest extends ApiTestCase {
 					'action' => 'watch',
 					'title' => $page['title'],
 					'unwatch' => true,
-					'token' => $tokens['watchtoken'] ) );
+					'token' => $this->getTokens( 'watchtoken' ), ) );
 			}
 		}
 		$data = $this->doApiRequest( array(
@@ -68,11 +60,9 @@ class ApiWatchTest extends ApiTestCase {
 	/**
 	 */
 	function testWatchProtect() {
-		$tokens = $this->getTokens();
-
 		$data = $this->doApiRequest( array(
 			'action' => 'protect',
-			'token' => $tokens['protecttoken'],
+			'token' => $this->getTokens( 'protecttoken' ),
 			'title' => 'Help:UTPage',
 			'protections' => 'edit=sysop',
 			'watchlist' => 'unwatch' ) );
@@ -149,11 +139,9 @@ class ApiWatchTest extends ApiTestCase {
 	/**
 	 */
 	function testWatchDelete() {
-		$tokens = $this->getTokens();
-
 		$data = $this->doApiRequest( array(
 			'action' => 'delete',
-			'token' => $tokens['deletetoken'],
+			'token' => $this->getTokens( 'deletetoken' ),
 			'title' => 'Help:UTPage' ) );
 		$this->assertArrayHasKey( 'delete', $data[0] );
 		$this->assertArrayHasKey( 'title', $data[0]['delete'] );
