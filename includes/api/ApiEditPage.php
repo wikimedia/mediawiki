wikimedia/mediawiki

commit be97167ab61e7d59d86f6a9dca53b7016d615735

Make APIEditBeforeSave give the whole revision
rather then just the current section. This
makes the hook behave as documented in
docs/hooks.txt.

Bug: 52077
Change-Id: Ia59fb6bbcbf2ae4119aa9dc316af8130e0235e78

includes/api/ApiEditPage.php
@@ -320,11 +320,37 @@ class ApiEditPage extends ApiBase {
 
 		$ep->setContextTitle( $titleObj );
 		$ep->importFormData( $req );
+		$content = $ep->textbox1;
+
+		// The following is needed to give the hook the full content of the
+		// new revision rather than just the current section. (Bug 52077)
+		if ( !is_null( $params['section'] ) && $contentHandler->supportsSections() ) {
+
+			$sectionTitle = '';
+			// If sectiontitle is set, use it, otherwise use the summary as the section title (for
+			// backwards compatibility with old forms/bots).
+			if ( $ep->sectiontitle !== '' ) {
+				$sectionTitle = $ep->sectiontitle;
+			} else {
+				$sectionTitle = $ep->summary;
+			}
+
+			$contentObj = $contentHandler->unserializeContent( $content );
+
+			$fullContentObj = $articleObject->replaceSectionContent( $params['section'], $contentObj, $sectionTitle );
+			if ( $fullContentObj ) {
+				$content = $fullContentObj->serialize( $contentFormat );
+			} else {
+				// This most likely means we have an edit conflict which means that the edit
+				// wont succeed anyway.
+				$this->dieUsageMsg( 'editconflict' );
+			}
+		}
 
 		// Run hooks
 		// Handle APIEditBeforeSave parameters
 		$r = array();
-		if ( !wfRunHooks( 'APIEditBeforeSave', array( $ep, $ep->textbox1, &$r ) ) ) {
+		if ( !wfRunHooks( 'APIEditBeforeSave', array( $ep, $content, &$r ) ) ) {
 			if ( count( $r ) ) {
 				$r['result'] = 'Failure';
 				$apiResult->addValue( null, $this->getModuleName(), $r );

RELEASE-NOTES-1.22
@@ -232,6 +232,8 @@ production.
 * (bug 51891) Fixed PHP notice on Special:PagesWithProp when no properties
   are defined.
 * (bug 52006) Corrected documentation of $wgTranscludeCacheExpiry.
+* (bug 52077) The APIEditBeforeSave hook is giving the content of the whole
+  revision as second argument now, rather than just the current section.
 * (bug 49694) $wgSpamRegex is now also applied on the new section headline text
   adding a new topic on a page
 

commit 993de5fbd5dfde376f127f8e86c77c4f5972a722

action=edit API with section=new would fail if page didn't exist
If the page doesn't exist, $articleObject->replaceSectionContent will return null
Fixes a regression introduced in be97167ab61e7d59d86f6a9dca53b7016d615735.

Bug: 52830
Change-Id: Icdc0f130eeb40fff5c656c396b2a2af294015b98

includes/api/ApiEditPage.php
@@ -329,7 +329,7 @@ class ApiEditPage extends ApiBase {
 
 		// The following is needed to give the hook the full content of the
 		// new revision rather than just the current section. (Bug 52077)
-		if ( !is_null( $params['section'] ) && $contentHandler->supportsSections() ) {
+		if ( !is_null( $params['section'] ) && $contentHandler->supportsSections() && $titleObj->exists() ) {
 
 			$sectionTitle = '';
 			// If sectiontitle is set, use it, otherwise use the summary as the section title (for