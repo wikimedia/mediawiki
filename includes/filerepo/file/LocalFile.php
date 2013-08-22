c92ddcbbe49ba0a4ab2c6e6ec68ed316449af07d
Brian Wolff <bawolff+wn@gmail.com>
2013-08-22 16:12:58 -0700

    Backport purge upstream caches when deleting file assets.
    Bug: 51064
    Change-Id: Ibf57e8f32d7dd04b8508a69706be5a40b49e5abd

--- includes/filerepo/file/LocalFile.php
+++ includes/filerepo/file/LocalFile.php
@@ -1356,6 +1356,7 @@ class LocalFile extends File {
 	 * @return FileRepoStatus object.
 	 */
 	function delete( $reason, $suppress = false ) {
+		global $wgUseSquid;
 		if ( $this->getRepo()->getReadOnlyReason() !== false ) {
 			return $this->readOnlyFatalStatus();
 		}
@@ -1378,6 +1379,15 @@ class LocalFile extends File {
 			$this->purgeOldThumbnails( $archiveName );
 		}
 
+		if ( $wgUseSquid ) {
+			// Purge the squid
+			$purgeUrls = array();
+			foreach ($archiveNames as $archiveName ) {
+				$purgeUrls[] = $this->getArchiveUrl( $archiveName );
+			}
+			SquidUpdate::purge( $purgeUrls );
+		}
+
 		return $status;
 	}
 
@@ -1396,6 +1406,7 @@ class LocalFile extends File {
 	 * @return FileRepoStatus object.
 	 */
 	function deleteOld( $archiveName, $reason, $suppress = false ) {
+		global $wgUseSquid;
 		if ( $this->getRepo()->getReadOnlyReason() !== false ) {
 			return $this->readOnlyFatalStatus();
 		}
@@ -1411,6 +1422,11 @@ class LocalFile extends File {
 		if ( $status->isOK() ) {
 			$this->purgeDescription();
 			$this->purgeHistory();
+		}
+
+		if ( $wgUseSquid ) {
+			// Purge the squid
+			SquidUpdate::purge( array( $this->getArchiveUrl( $archiveName ) ) );
 		}
 
 		return $status;
