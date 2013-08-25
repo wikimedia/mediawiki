0c2d7d4130e51d8b43a8cc2d6523adc27022a2e4
Derk-Jan Hartman <hartman.wiki@gmail.com>
2013-08-25 13:55:15 +0200

    Accessibility: Make jump links work in Safari and Chrome
    Use Javascript to focus the first tabbable element starting from the
    target that we jump to.
    
    When navigating to an ID, focus only follows if the target is a
    focusable element (has implicit or explicit tabindex). When navigating
    to an unfocusable element, the position of focus becomes undefined.
    Webkit (safari/chrome) keep it on the element that had the focus, IE
    and Firefox unfocus and on the next tab will select the first tabbable
    element starting from the target.
    
    Change-Id: If1f7f578cc9b1fad7d40d168dc225287a36638fe

--- resources/jquery/jquery.mw-jump.js
+++ resources/jquery/jquery.mw-jump.js
@@ -3,9 +3,15 @@
  */
 jQuery( function ( $ ) {
 
-	$( '.mw-jump' ).on( 'focus blur', 'a', function ( e ) {
+	$( '.mw-jump' ).on( 'focus blur keypress', 'a', function ( e ) {
 		// Confusingly jQuery leaves e.type as focusout for delegated blur events
-		if ( e.type === 'blur' || e.type === 'focusout' ) {
+		if ( e.type === 'keypress' ) {
+			if ( e.keyCode === 14 ) {
+				var $dest = $( $( this ).attr( 'href' ) );
+				$dest.find( ':tabbable:first' ).focus();
+				e.preventDefault();
+			}
+		} else if ( e.type === 'blur' || e.type === 'focusout' ) {
 			$( this ).closest( '.mw-jump' ).css({ height: 0 });
 		} else {
 			$( this ).closest( '.mw-jump' ).css({ height: 'auto' });
