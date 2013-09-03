945cd81705ae005d8bf6a033b96588e13f1c25cd
csteipp <csteipp@wikimedia.org>
2013-09-03 08:41:03 -0700

    Update Releasenotes and version number for 1.19.8
    Change-Id: I6b1df7c8a3ee91f2aaf6c14740028c76988ed602

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.7';
+$wgVersion = '1.19.8';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
