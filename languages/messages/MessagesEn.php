dcedbd537f2b3bd917c3006d97b3237525bc4414
Tim Starling <tstarling@wikimedia.org>
2013-09-12 12:57:16 +1000

    Fix English gender-unknown message
    Fix for I81f02f03: the verb "detail" means to give details, i.e. to
    say more than a single word in response to a question. It does not make
    sense in this context.
    
    Change-Id: Ifddf5c9a07dc62bc22dd0a3d2986e41a55b8ef33

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -1979,7 +1979,7 @@ Check HTML tags.',
 'badsiglength'                  => 'Your signature is too long.
 It must not be more than $1 {{PLURAL:$1|character|characters}} long.',
 'yourgender'                    => 'How do you prefer to be described?',
-'gender-unknown'                => 'I prefer not to detail',
+'gender-unknown'                => 'I prefer not to say',
 'gender-male'                   => 'He edits wiki pages',
 'gender-female'                 => 'She edits wiki pages',
 'prefs-help-gender'             => 'Setting this preference is optional.
