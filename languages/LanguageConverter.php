370cd72360cf299b805c6d4eccd9f8b46086487c
Liangent <liangent@gmail.com>
2013-09-15 14:11:44 -0400

    Don't match HTML entities in language conversion syntax
    RegEx provided by Gabriel Wicke.
    
    Change-Id: Idca127acc6f4cdc159ee85d5f816a5d120cbe44e

--- languages/LanguageConverter.php
+++ languages/LanguageConverter.php
@@ -1263,7 +1263,10 @@ class ConverterRule {
 		$variants = $this->mConverter->mVariants;
 		$varsep_pattern = $this->mConverter->getVarSeparatorPattern();
 
+		// Split according to $varsep_pattern, but ignore semicolons from HTML entities
+		$rules = preg_replace( '/(&[#a-zA-Z0-9]+);/', "$1\x01", $rules );
 		$choice = preg_split( $varsep_pattern, $rules );
+		$choice = str_replace( "\x01", ';', $choice );
 
 		foreach ( $choice as $c ) {
 			$v = explode( ':', $c, 2 );
