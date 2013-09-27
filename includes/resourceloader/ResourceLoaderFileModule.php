d10d6b1783ed4a4c8718c01cbffde43e2da3d816
Timo Tijhof <krinklemail@gmail.com>
2013-09-27 14:29:38 +0200

    less: Use new addParsedFile method, replacing embeddedFiles hack
    Follows-up b67b9e1, lessphp now has a public method to add to the
    list of files for compilation cache.
    
    Change-Id: I62a6c7cdeb34ea742fa7547e3ca10e24ee484b97

--- includes/resourceloader/ResourceLoaderFileModule.php
+++ includes/resourceloader/ResourceLoaderFileModule.php
@@ -776,9 +776,6 @@ class ResourceLoaderFileModule extends ResourceLoaderModule {
 			$result['files'] = array( $fileName => self::safeFilemtime( $fileName ) );
 			$result['updated'] = time();
 		}
-		// Tie cache expiry to the names and mtimes of files that were embedded
-		// as data URIs in the generated CSS source.
-		$result['files'] += $compiler->embeddedFiles;
 		$this->localFileRefs += array_keys( $result['files'] );
 		$cache->set( $key, $result, $expire );
 		return $result['compiled'];
