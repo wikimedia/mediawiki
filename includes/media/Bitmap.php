aa8da5a320aab3fff72060850606c4478ff77c48
Brian Wolff <bawolff+wn@gmail.com>
2013-09-28 23:12:58 -0300

    Move getRotation and getImageArea to the base class
    This way we know we can always safely call getRotation on any image.
    
    In particular, PagedTiffHandler doesn't extend BitmapHandler, and I
    want to make VipsScaler work with PagedTiffHandler, and it calls
    getRotation.
    
    Change-Id: I63f2be2a6f31398918b8562e15343f8a839d91c1

--- includes/media/Bitmap.php
+++ includes/media/Bitmap.php
@@ -100,17 +100,6 @@ class BitmapHandler extends ImageHandler {
 	}
 
 	/**
-	 * Function that returns the number of pixels to be thumbnailed.
-	 * Intended for animated GIFs to multiply by the number of frames.
-	 *
-	 * @param File $image
-	 * @return int
-	 */
-	function getImageArea( $image ) {
-		return $image->getWidth() * $image->getHeight();
-	}
-
-	/**
 	 * @param $image File
 	 * @param  $dstPath
 	 * @param  $dstUrl
@@ -712,24 +701,6 @@ class BitmapHandler extends ImageHandler {
 		imagejpeg( $dst_image, $thumbPath, 95 );
 	}
 
-	/**
-	 * On supporting image formats, try to read out the low-level orientation
-	 * of the file and return the angle that the file needs to be rotated to
-	 * be viewed.
-	 *
-	 * This information is only useful when manipulating the original file;
-	 * the width and height we normally work with is logical, and will match
-	 * any produced output views.
-	 *
-	 * The base BitmapHandler doesn't understand any metadata formats, so this
-	 * is left up to child classes to implement.
-	 *
-	 * @param $file File
-	 * @return int 0, 90, 180 or 270
-	 */
-	public function getRotation( $file ) {
-		return 0;
-	}
 
 	/**
 	 * Returns whether the current scaler supports rotation (im and gd do)
