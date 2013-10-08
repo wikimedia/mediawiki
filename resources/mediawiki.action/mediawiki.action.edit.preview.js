wikimedia/mediawiki

commit becd284d513f52d6ca473c35d11f598f018cfe83

mediawiki.action.edit.preview: Clean up and optimisation
* Remove redundant inline 'position: absolute' on the spinner.
* Remove trailing white space in jquery.spinner.css.
* Cache #editform query result.
* Remove redundant removeSelectors.join(',') logic.
* White space.
* Move delegate click event listener down from body to #editform
  (the common ancestor of the #wpDiff and #wpPreview buttons).
* Remove minHeight (was needed because the spinner was positioned
  absolute), which gets rid of the weird white gap for #wikiPreview if
  you do live diff instead of live preview.

Change-Id: Icdf975f201947ecadb886057131f2531c68791fb

resources/mediawiki.action/mediawiki.action.edit.preview.js
@@ -7,7 +7,7 @@
 	 * @param {jQuery.Event} e
 	 */
 	function doLivePreview( e ) {
-		var $wikiPreview, copySelectors, removeSelectors, $copyElements, $spinner,
+		var $wikiPreview, $editform, copySelectors, $copyElements, $spinner,
 			targetUrl, postData, $previewDataHolder;
 
 		e.preventDefault();
@@ -16,6 +16,7 @@
 		$( mw ).trigger( 'LivePreviewPrepare' );
 
 		$wikiPreview = $( '#wikiPreview' );
+		$editform = $( '#editform' );
 
 		// Show #wikiPreview if it's hidden to be able to scroll to it
 		// (if it is hidden, it's also empty, so nothing changes in the rendering)
@@ -41,11 +42,7 @@
 		$copyElements = $( copySelectors.join( ',' ) );
 
 		// Not shown during normal preview, to be removed if present
-		removeSelectors = [
-			'.mw-newarticletext'
-		];
-
-		$( removeSelectors.join( ',' ) ).remove();
+		$( '.mw-newarticletext' ).remove();
 
 		$spinner = $.createSpinner( {
 			size: 'large',
@@ -53,26 +50,18 @@
 		});
 		$wikiPreview.before( $spinner );
 		$spinner.css( {
-			position: 'absolute',
 			marginTop: $spinner.height()
 		} );
-		// Make sure preview area is at least as tall as 2x the height of the spinner.
-		// 1x because if its smaller, it will spin behind the edit toolbar.
-		// (this happens on the first preview when editPreview is still empty)
-		// 2x because the spinner has 1x margin top breathing room.
-		$wikiPreview.css( 'minHeight', $spinner.height() * 2 );
 
 		// Can't use fadeTo because it calls show(), and we might want to keep some elements hidden
 		// (e.g. empty #catlinks)
-		$copyElements.animate( {
-			opacity: 0.4
-		}, 'fast' );
+		$copyElements.animate( { opacity: 0.4 }, 'fast' );
 
 		$previewDataHolder = $( '<div>' );
-		targetUrl = $( '#editform' ).attr( 'action' );
+		targetUrl = $editform.attr( 'action' );
 
 		// Gather all the data from the form
-		postData = $( '#editform' ).formToArray();
+		postData = $editform.formToArray();
 		postData.push( {
 			name: e.target.name,
 			value: ''
@@ -83,6 +72,7 @@
 		// although that requires figuring out how to convert that raw data into proper HTML.
 		$previewDataHolder.load( targetUrl + ' ' + copySelectors.join( ',' ), postData, function () {
 			var i, $from;
+
 			// Copy the contents of the specified elements from the loaded page to the real page.
 			// Also copy their class attributes.
 			for ( i = 0; i < copySelectors.length; i++ ) {
@@ -133,11 +123,11 @@
 
 		if ( !document.getElementById( 'wikiDiff' ) && document.getElementById( 'wikiPreview' ) ) {
 			$( '#wikiPreview' ).after(
-				$( '<div>' ).attr( 'id', 'wikiDiff')
+				$( '<div>' ).attr( 'id', 'wikiDiff' )
 			);
 		}
 
-		$( document.body ).on( 'click', '#wpPreview, #wpDiff', doLivePreview );
+		$( '#editform' ).on( 'click', '#wpPreview, #wpDiff', doLivePreview );
 	} );
 
 }( mediaWiki, jQuery ) );

resources/jquery/jquery.spinner.css
@@ -33,7 +33,7 @@
 .mw-spinner-inline {
 	display: inline-block;
 	vertical-align: middle;
-	
+
 	/* IE < 8 */
 	zoom: 1;
 	*display: inline;

commit 99e48568e53589ee576af3d74c7be0fed33fb2d5

mediawiki.action.edit.preview: Fix for LiquidThreads hack
LiquidThreads is loading this core module when rendering an
inline edit form. Because the form doesn't have the same ID
as the one in core, this broke after becd284.

It previously worked because the click event on the button (of
which LQT did duplicate the ID from core) bubbles up to the
document body and thus trigger this private handler of the
core edit preview module.

Change-Id: I9e35d0c48b86e1c9073b68313a8a330c6ea85b86

resources/mediawiki.action/mediawiki.action.edit.preview.js
@@ -127,7 +127,10 @@
 			);
 		}
 
-		$( '#editform' ).on( 'click', '#wpPreview, #wpDiff', doLivePreview );
+		// This should be moved down to '#editform', but it kept on the body for
+		// now because LiquidThreads is re-using this module with only half the
+		// EditPage (doens't include #editform presumably)
+		$( document.body ).on( 'click', '#wpPreview, #wpDiff', doLivePreview );
 	} );
 
 }( mediaWiki, jQuery ) );