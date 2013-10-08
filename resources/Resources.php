d41b44927907e665aa83831509e51d71d0c0e996
Ori Livneh <ori@wikimedia.org>
2013-10-07 23:08:54 -0700

    Add mediawiki.inspect module
    This patch adds a mediawiki.inspect ResourceLoader module. The module contains
    tools for inspecting page composition and performance. It is currently
    restricted to a set of static methods for querying the ResourceLoader module
    registry, the most useful of which is #inspectLoadedModules, which prints a
    list of loaded ResourceLoader modules and their size in kilobytes, ordered from
    largest to smallest. This method has a lazy-loading alias: mw.loader.inspect.
    
    Change-Id: I5a007fdf11196664f425cd7800eec7fc3fdb629f

--- resources/Resources.php
+++ resources/Resources.php
@@ -646,6 +646,10 @@ return array(
 		// must be loaded on the bottom
 		'position' => 'bottom',
 	),
+	'mediawiki.inspect' => array(
+		'scripts' => 'resources/mediawiki/mediawiki.inspect.js',
+		'dependencies' => 'jquery.byteLength',
+	),
 	'mediawiki.feedback' => array(
 		'scripts' => 'resources/mediawiki/mediawiki.feedback.js',
 		'styles' => 'resources/mediawiki/mediawiki.feedback.css',
