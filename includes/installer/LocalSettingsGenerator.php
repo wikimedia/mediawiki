5817ef6fa9cb7bbee7c5d970ee48c6ff28049fdb
Mark A. Hershberger <mah@everybody.org>
2013-10-11 21:13:40 +0000

    Include short descriptions for extensions bundled in the release
    Also fix styling for the list of extensions.
    
    Bug: 43817
    Change-Id: I5335225683ec8f1c163bb67f478787ebc52ee3a9

--- includes/installer/LocalSettingsGenerator.php
+++ includes/installer/LocalSettingsGenerator.php
@@ -137,14 +137,18 @@ class LocalSettingsGenerator {
 		$localSettings = $this->getDefaultText();
 
 		if ( count( $this->extensions ) ) {
+			$extensions = $this->installer->findExtensions();
 			$localSettings .= "
 # Enabled Extensions. Most extensions are enabled by including the base extension file here
 # but check specific extension documentation for more details
 # The following extensions were automatically enabled:\n";
 
-			foreach ( $this->extensions as $extName ) {
-				$encExtName = self::escapePhpString( $extName );
-				$localSettings .= "require_once \"\$IP/extensions/$encExtName/$encExtName.php\";\n";
+			$ip = $this->installer->getVar( 'IP' );
+			foreach ( $this->extensions as $ext) {
+				$path = '$IP' . DIR_SEPARATOR .
+					self::escapePhpString( str_replace(
+							$ip, '', $extensions[$ext]['path'] ) );
+				$localSettings .= "require_once \"$path\";\n";
 			}
 		}
 
