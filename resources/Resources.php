893c6be096179696fab7c50c05e90b342c0d6717
jrobson <jrobson@wikimedia.org>
2013-10-16 10:56:34 -0700

    Move Vector's preferences css out of screen.less
    * Move the preferences styling out of screen.less:
      don't penalise all our users with it
    * Limited to Vector skin for time being as other skins do not play nicely
    with these styles
    
    Change-Id: I121b3d88945d994c9e30d96bab7548c839510276

--- resources/Resources.php
+++ resources/Resources.php
@@ -1011,6 +1011,10 @@ return array(
 		'scripts' => 'resources/mediawiki.special/mediawiki.special.preferences.js',
 		'styles' => 'resources/mediawiki.special/mediawiki.special.preferences.css',
 		'position' => 'top',
+		'skinStyles' => array(
+			'default' => '',
+			'vector' => 'skins/vector/special.preferences.less',
+		),
 	),
 	'mediawiki.special.recentchanges' => array(
 		'scripts' => 'resources/mediawiki.special/mediawiki.special.recentchanges.js',
