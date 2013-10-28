0ddaf4c958141e3cebc314dd96d632e2c85302f4
Antoine Musso <hashar@free.fr>
2013-10-28 17:24:50 +0100

    tests for includes/exception.php
    Tests for MWExceptionHandler::jsonSerializeException introduced by Ori
    Livneh in https://gerrit.wikimedia.org/r/#/c/76304
    
    Tests result:
    
      OK (17 tests, 29 assertions)
    
    Change-Id: I37f2a837e2d26bf9780e56edc7ec039e8e447525

---
+++ tests/phpunit/includes/ExceptionTest.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Tests for includes/Exception.php.
+ *
+ * @author Antoine Musso
+ * @copyright Copyright © 2013, Antoine Musso
+ * @copyright Copyright © 2013, Wikimedia Foundation Inc.
+ * @file
+ */
+
+class ExceptionTest extends MediaWikiTestCase {
+
+	/**
+	 * @expectedException MWException
+	 */
+	function testMwexceptionThrowing() {
+		throw new MWException();
+	}
+
+	/**
+	 * @covers MWExceptionHandler::jsonSerializeException
+	 */
+	function testJsonSerializeException() {
+		$json = MWExceptionHandler::jsonSerializeException(
+			new MWException()
+		);
+		$this->assertNotEquals( false, $json,
+			"The exception should be JSON serializable, got false." );
+	}
+
+	/**
+	 * Lame JSON schema validation.
+	 *
+	 * @covers MWExceptionHandler::jsonSerializeException
+	 *
+	 * @param $expectedKeyType String Type expected as returned by gettype()
+	 * @param $exClass String An exception class (ie: Exception, MWException)
+	 * @param $key String Name of the key to validate in the serialized JSON
+	 * @dataProvider provideJsonSerializedKeys
+	 */
+	function testJsonserializeexceptionKeyss($expectedKeyType, $exClass, $key) {
+
+		# Make sure we log a backtrace:
+		$this->setMwGlobals( array( 'wgLogExceptionBacktrace' => true ) );
+
+		$json = json_decode(
+			MWExceptionHandler::jsonSerializeException( new $exClass())
+		);
+		$this->assertObjectHasAttribute( $key, $json,
+			"JSON serialized exception is missing key $key"
+		);
+		$this->assertInternalType( $expectedKeyType, $json->$key,
+			"JSON serialized key '$key' of type " . gettype($json->$key)
+			. " should be of type $expectedKeyType"
+		);
+	}
+
+	/**
+	 * Returns test cases: exception class, key name, gettype()
+	 */
+	function provideJsonSerializedKeys() {
+		$testCases = array();
+		foreach( array( 'Exception', 'MWException' ) as $exClass ) {
+			$exTests = array(
+				array( 'string',  $exClass,  'id' ),
+				array( 'string',  $exClass,  'file' ),
+				array( 'integer', $exClass,  'line' ),
+				array( 'string',  $exClass,  'message' ),
+				array( 'boolean', $exClass,  'url' ),
+				# Backtrace only enabled with wgLogExceptionBacktrace = true
+				array( 'array',   $exClass,  'backtrace' ),
+			);
+			$testCases = array_merge($testCases, $exTests);
+		}
+		return $testCases;
+	}
+
+	/**
+	 * Given wgLogExceptionBacktrace is true
+	 * then serialized exception SHOULD have a backtrace
+	 *
+	 * @covers MWExceptionHandler::jsonSerializeException
+	 */
+	function testJsonserializeexceptionBacktracingEnabled() {
+		$this->setMwGlobals( array( 'wgLogExceptionBacktrace' => true ) );
+		$json = json_decode(
+			MWExceptionHandler::jsonSerializeException( new Exception() )
+		);
+		$this->assertObjectHasAttribute( 'backtrace', $json );
+	}
+
+	/**
+	 * Given wgLogExceptionBacktrace is false
+	 * then serialized exception SHOULD NOT have a backtrace
+	 *
+	 * @covers MWExceptionHandler::jsonSerializeException
+	 */
+	function testJsonserializeexceptionBacktracingDisabled() {
+		$this->setMwGlobals( array( 'wgLogExceptionBacktrace' => false ) );
+		$json = json_decode(
+			MWExceptionHandler::jsonSerializeException( new Exception() )
+		);
+		$this->assertObjectNotHasAttribute( 'backtrace', $json );
+
+	}
+
+}
