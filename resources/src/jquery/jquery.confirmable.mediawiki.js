13a14c70c664372f542d33902c9266414930577d
Bartosz Dziewoński <matma.rex@gmail.com>
2013-10-28 15:56:30 +0100

    jquery.confirmable: New inline confirmation module
    $().confirmable() can be applied to any inline-block element. It will
    cause it to expand into "Are you sure? [Yes] [No]" question and
    buttons when clicked, where the buttons are clones of the original
    element: [Yes] will carry out the default action (or a different one,
    if specified), [No] will collapse the interface back.
    
    Possible uses include:
    * Confirmable "rollback" links
    * Confirmable "unwatch" links on watchlists
    * Confirmable "thank" links (Echo extension's ones)
    
    Added a demo with possible uses on history and watchlist pages.
    Included Hebrew messages courtesy of Moriel.
    
    Change-Id: I2f6e0bd4f6f0a84e1a0d7193cde076738f3cdd25

---
+++ resources/src/jquery/jquery.confirmable.mediawiki.js
@@ -0,0 +1,13 @@
+/*!
+ * jQuery confirmable plugin customization for MediaWiki
+ *
+ * This file serves to inject our localised messages into it.
+ */
+
+( function ( mw, $ ) {
+	$.fn.confirmable.defaultOptions.i18n = {
+		confirm: mw.message( 'confirmable-confirm', mw.user ).text(),
+		yes: mw.message( 'confirmable-yes' ).text(),
+		no: mw.message( 'confirmable-no' ).text()
+	};
+}( mediaWiki, jQuery ) );
