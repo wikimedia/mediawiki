21b64e397cd0d175964dcbc119b270f8cdcce37b
Bartosz Dziewoński <matma.rex@gmail.com>
2013-10-28 15:59:20 +0100

    Inline confirmation for rollback links
    Upon clicking the "rollback" link expands into "Are you sure? [Yes] [No]"
    question and buttons. See parent changeset I2f6e0bd4 for implementation.
    
    This should not be merged until it is clear that the feature is actually
    wanted and that there won't be crazy backlash when this is done.
    
    Bug: T49782
    Change-Id: I13530d5cb427beef751bc5e01dadfb483afc4b8f

---
+++ resources/src/mediawiki/mediawiki.confirmable.rollback.js
@@ -0,0 +1,19 @@
+/*!
+ * Inline confirmation for rollback links.
+ */
+
+( function ( mw, $ ) {
+	$( function () {
+		$( '.mw-rollback-link a' ).confirmable( {
+			i18n: { confirm: mw.message( 'confirmable-confirm-rollback', mw.user ).text() },
+			buttonCallback: function ( $button, which ) {
+				if ( which === 'no' ) {
+					// Remove misleading attributes
+					return $button.removeAttr( 'title href' );
+				} else {
+					return $button;
+				}
+			}
+		} );
+	} );
+}( mediaWiki, jQuery ) );
