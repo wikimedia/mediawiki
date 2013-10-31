c8012a20fd7e7f6a9c62e996061788f1a52ee684
Ori Livneh <ori@wikimedia.org>
2013-10-31 09:47:15 -0700

    mediawiki.inspect#dumpTable: fix broken FF workaround
    In Ib45487d10, we changed to invocation of console#table from
    'console.table( data )' to 'console.table.call( console.table, data)'
    to force an exception on Firefox, which doesn't define console.table but
    doesn't complain if you try to invoke it. But on some versions of Chromium,
    invoking using Function.prototype#call to invoke console#table results in a
    'TypeError: Illegal invocation' exception. Ugh.
    
    Change-Id: Ib2252003f29586a47efb9ec534becc6396936b34

--- resources/mediawiki/mediawiki.inspect.js
+++ resources/mediawiki/mediawiki.inspect.js
@@ -131,7 +131,8 @@
 				// Use Function.prototype#call to force an exception on Firefox,
 				// which doesn't define console#table but doesn't complain if you
 				// try to invoke it.
-				console.table.call( console.table, data );
+				console.table.call;
+				console.table( data );
 				return;
 			} catch (e) {}
 			try {
