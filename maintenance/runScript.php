5c72badf3440485569bfb76ff0bc5ed1295c9dd9
aude <aude.wiki@gmail.com>
2013-11-05 15:54:16 +0000

    Add convenience maintenance script wrapper
    Useful for scripts or extensions installed in non-standard locations.
    
    Example usage:
    
    php maintenance/runScript.php
    extensions/Wikibase/lib/maintenance/dispatchChanges.php
    
    Change-Id: I6ffc72f41f3f96dd535a427d90c7166372459bd2

---
+++ maintenance/runScript.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Convenience maintenance script wrapper, useful for scripts
+ * or extensions located outside of standard locations.
+ *
+ * To use, give the maintenance script as a relative path to
+ * MediaWiki base install path.
+ *
+ * Example usage:
+ *  php maintenance/runScript.php extensions/Wikibase/lib/maintenance/dispatchChanges.php
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @author Katie Filbert < aude.wiki@gmail.com >
+ * @file
+ * @ingroup Maintenance
+ */
+if ( ! getenv( 'MW_INSTALL_PATH' ) ) {
+	require_once __DIR__ . '/Maintenance.php';
+
+	$script = $argv[1];
+	array_shift( $argv );
+
+	require_once __DIR__ . "/../$script";
+}
