decf92ca7cc4dd1ab1cc7d3327087a7d2b25fcf9
Aaron Schulz <aschulz@wikimedia.org>
2013-11-06 16:55:55 -0800

    Handle Swift JSON listings with unqualified timestamps
    * Throw errors next time invalid timestamps are encountered
    
    Change-Id: Ie5d6c63863cacbf7dc3bc3f408b395bdce859db9
    (cherry picked from commit eaa917adad163c50eaa90378db99ed1d0cb13048)

--- includes/Timestamp.php
+++ includes/Timestamp.php
@@ -93,9 +93,9 @@ class MWTimestamp {
 			# TS_ORACLE // session altered to DD-MM-YYYY HH24:MI:SS.FF6
 			$strtime = preg_replace( '/(\d\d)\.(\d\d)\.(\d\d)(\.(\d+))?/', "$1:$2:$3",
 					str_replace( '+00:00', 'UTC', $ts ) );
-		} elseif ( preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.*\d*)?Z$/', $ts, $da ) ) {
+		} elseif ( preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.*\d*)?Z?$/', $ts, $da ) ) {
 			# TS_ISO_8601
-		} elseif ( preg_match( '/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})(?:\.*\d*)?Z$/', $ts, $da ) ) {
+		} elseif ( preg_match( '/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})(?:\.*\d*)?Z?$/', $ts, $da ) ) {
 			#TS_ISO_8601_BASIC
 		} elseif ( preg_match( '/^(\d{4})\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)\.*\d*[\+\- ](\d\d)$/', $ts, $da ) ) {
 			# TS_POSTGRES
