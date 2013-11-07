af4ef8153354b02ec910fd98ec7af024c64ab104
Timo Tijhof <krinklemail@gmail.com>
2013-11-07 18:35:24 +0100

    Migrate usage of wikibits in legacy protect.js and upload.js
    Follows-up fcf4934a52.
    
    protect:
    * Use jQuery instead.
    * Removing now-obsolete dependency.
    * Make cell.appendChild more obvious and potentialy faster
      by grouping the dom actions in 1 sequence.
    
    config:
    * Removing obsolete dependency.
    
    upload:
    * Use jquery.spinner instead.
    * Removing now-obsolete dependency.
    
    jquery.spinner:
    * While at it, updated documentation to be included in our
      jsduck index, similar to jquery.localize.
    
    jsduck/external.js:
    * Added @static to jQuery.ajax which was missing, it showed up
      between instance methods like jQuery#injectSpinner.
    
    The only module left using mediawiki.legacy.wikibits is
    mediawiki.legacy.ajax which remains in tact for now.
    
    Bug: 56726
    Change-Id: I712112626a99dc2d0090f554c56052770cd0ae88

--- resources/Resources.php
+++ resources/Resources.php
@@ -1043,7 +1043,10 @@ return array(
 			'size-gigabytes',
 			'largefileserver',
 		),
-		'dependencies' => array( 'mediawiki.libs.jpegmeta', 'mediawiki.util' ),
+		'dependencies' => array(
+			'mediawiki.libs.jpegmeta',
+			'mediawiki.util',
+		),
 	),
 	'mediawiki.special.userlogin' => array(
 		'styles' => array(
@@ -1124,7 +1127,6 @@ return array(
 		'remoteBasePath' => $GLOBALS['wgStylePath'],
 		'localBasePath' => $GLOBALS['wgStyleDirectory'],
 		'dependencies' => array(
-			'mediawiki.legacy.wikibits',
 			'jquery.byteLimit',
 		),
 		'position' => 'top',
@@ -1145,9 +1147,9 @@ return array(
 		'remoteBasePath' => $GLOBALS['wgStylePath'],
 		'localBasePath' => $GLOBALS['wgStyleDirectory'],
 		'dependencies' => array(
+			'jquery.spinner',
 			'mediawiki.api',
 			'mediawiki.Title',
-			'mediawiki.legacy.wikibits',
 			'mediawiki.util',
 		),
 	),
