5733f4c71537251edfcbceb7deafbe56d9f9652b
Adam Roses Wight <awight@wikimedia.org>
2013-11-09 14:27:54 -0800

    (bug 56849) Pass true oldid for atomic page save
    This is necessary to detect edit conflicts without a race condition.
    
    Change-Id: I25c39aca5280a0810f75d0481afc86d31ce9a812

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -1015,6 +1015,7 @@ class EditPage {
 			return $handler->makeEmptyContent();
 		}
 		$content = $revision->getContent();
+		$this->oldid = $revision->getId();
 		return $content;
 	}
 
@@ -1769,7 +1770,7 @@ class EditPage {
 			( $bot ? EDIT_FORCE_BOT : 0 );
 
 			$doEditStatus = $this->mArticle->doEditContent( $content, $this->summary, $flags,
-															false, null, $this->contentFormat );
+															$this->oldid, null, $this->contentFormat );
 
 		if ( !$doEditStatus->isOK() ) {
 			// Failure from doEdit()
