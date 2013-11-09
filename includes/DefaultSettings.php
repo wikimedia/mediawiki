6bb0e58624962241f04059355d16c45387b203b2
Mark A. Hershberger <mah@everybody.org>
2013-11-08 21:33:38 -0500

    Ready RC1
    * Rearrange release notes
    * Bump version
    
    Change-Id: Iaf209f596185a8168118395d4e9ca58ea3bdc180

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.22.0rc0';
+$wgVersion = '1.22.0rc1';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
