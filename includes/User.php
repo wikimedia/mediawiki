df80f264d7810e49c44022a1c91962d005d407cc
tonythomas01 <01tonythomas@gmail.com>
2013-11-10 20:08:34 +0530

    Removed 'Remember my login' option from the Preferences page
    Removed 'Remember my login' from MW  Preferences, as it was unwanted.
    It adds to the complexity of MW user Preferences.
    
    Bug: 52342
    Change-Id: I7c957e1e1aaecf47f7c47bc063b5d3b364644afc

--- includes/User.php
+++ includes/User.php
@@ -3211,11 +3211,6 @@ class User {
 			'UserID' => $this->mId,
 			'UserName' => $this->getName(),
 		);
-		if ( 1 == $this->getOption( 'rememberpassword' ) ) {
-			$cookies['Token'] = $this->mToken;
-		} else {
-			$cookies['Token'] = false;
-		}
 
 		wfRunHooks( 'UserSetCookies', array( $this, &$session, &$cookies ) );
 
@@ -3239,9 +3234,6 @@ class User {
 		 */
 		if ( $request->getCheck( 'wpStickHTTPS' ) || $this->requiresHTTPS() ) {
 			$time = null;
-			if ( ( 1 == $this->getOption( 'rememberpassword' ) ) ) {
-				$time = 0; // set to $wgCookieExpiration
-			}
 			$this->setCookie(
 				'forceHTTPS',
 				'true',
@@ -4650,8 +4642,7 @@ class User {
 			array( 'up_user' => $userId ), __METHOD__ );
 
 		if ( $hasRows ) {
-			// Only do this delete if there is something there. A very large portion of
-			// calls to this function are for setting 'rememberpassword' for new accounts.
+			// Only do this delete if there is something there.
 			// Doing this delete for new accounts with no rows in the table rougly causes
 			// gap locks on [max user ID,+infinity) which causes high contention since many
 			// updates will pile up on each other since they are for higher (newer) user IDs.
