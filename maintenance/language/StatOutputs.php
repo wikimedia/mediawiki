e250409b999d87223611d4cdccb768f7ab720e20
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-11-11 16:49:37 +0100

    Use suppress/restoreWarnings()
    Instead of @
    
    Change-Id: I0c422e452636ae1af664b24c0859d038592e0e8a

--- maintenance/language/StatOutputs.php
+++ maintenance/language/StatOutputs.php
@@ -26,7 +26,10 @@
 /** A general output object. Need to be overriden */
 class StatsOutput {
 	function formatPercent( $subset, $total, $revert = false, $accuracy = 2 ) {
-		return @sprintf( '%.' . $accuracy . 'f%%', 100 * $subset / $total );
+		wfSuppressWarnings();
+		$return = sprintf( '%.' . $accuracy . 'f%%', 100 * $subset / $total );
+		wfRestoreWarnings();
+		return $return;
 	}
 
 	# Override the following methods
@@ -88,7 +91,10 @@ class WikiStatsOutput extends statsOutput {
 	}
 
 	function formatPercent( $subset, $total, $revert = false, $accuracy = 2 ) {
-		$v = @round( 255 * $subset / $total );
+		wfSuppressWarnings();
+		$v = round( 255 * $subset / $total );
+		wfRestoreWarnings();
+
 		if ( $revert ) {
 			# Weigh reverse with factor 20 so coloring takes effect more quickly as
 			# this option is used solely for reporting 'bad' percentages.
