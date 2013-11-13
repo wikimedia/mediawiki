0eb52399689df5fd1401eab951f3a8ef460f7665
Aaron Schulz <aschulz@wikimedia.org>
2013-11-13 15:25:37 -0800

    Added support to img_auth.php for non-repo containers
    * This adds a new $wgImgAuthUrlPathMap config variable
    * Also fixed ImgAuthBeforeStream hook msg formatting
    
    bug: 51136
    Change-Id: I77528f92b20670e3b09adc79c49e62060f1614f3

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -362,6 +362,22 @@ $wgImgAuthDetails = false;
 $wgImgAuthPublicTest = true;
 
 /**
+ * Map of relative URL directories to match to internal mwstore:// base storage paths.
+ * For img_auth.php requests, everything after "img_auth.php/" is checked to see
+ * if starts with any of the prefixes defined here. The prefixes should not overlap.
+ * The prefix that matches has a corresponding storage path, which the rest of the URL
+ * is assumed to be relative to. The file at that path (or a 404) is send to the client.
+ *
+ * Example:
+ * $wgImgAuthUrlPathMap['/timeline/'] = 'mwstore://local-fs/timeline-render/';
+ * The above maps ".../img_auth.php/timeline/X" to "mwstore://local-fs/timeline-render/".
+ * The name "local-fs" should correspond by name to an entry in $wgFileBackends.
+ *
+ * @see $wgFileBackends
+ */
+$wgImgAuthUrlPathMap = array();
+
+/**
  * File repository structures
  *
  * $wgLocalFileRepo is a single repository structure, and $wgForeignFileRepos is
