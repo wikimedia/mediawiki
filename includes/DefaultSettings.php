54f711293bc06dd0f13efe0b2d017bca50082377
csteipp <csteipp@wikimedia.org>
2013-11-14 10:43:59 -0800

    Update Releasenotes and version number for 1.21.3
    Change-Id: Id1c0f85d461f13e32d84fb0a18b18f552323dda6

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.21.2';
+$wgVersion = '1.21.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
