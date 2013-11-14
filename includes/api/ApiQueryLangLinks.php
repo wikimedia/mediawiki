6cf623389ad7817fb7fa608dd082ba6ff2ba602a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-11-14 15:18:38 +0100

    Fix CodeSniffer errors and warnings (you guessed it, on API classes)
    Change-Id: I56f9632975d53bdfe33ff9412e1a4ba010bdb2aa

--- includes/api/ApiQueryLangLinks.php
+++ includes/api/ApiQueryLangLinks.php
@@ -182,7 +182,8 @@ class ApiQueryLangLinks extends ApiQueryBase {
 
 	public function getExamples() {
 		return array(
-			'api.php?action=query&prop=langlinks&titles=Main%20Page&redirects=' => 'Get interlanguage links from the [[Main Page]]',
+			'api.php?action=query&prop=langlinks&titles=Main%20Page&redirects='
+				=> 'Get interlanguage links from the [[Main Page]]',
 		);
 	}
 
