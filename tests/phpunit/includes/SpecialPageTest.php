21cf1fb88f96d7593daaf37375333f19ad832bd6
aude <aude.wiki@gmail.com>
2013-11-19 22:37:11 +0100

    Add tests for SpecialPage::requireLogin
    Change-Id: Id9ee08c921a3e109e5175bb7ae807615cb15b8e5

--- tests/phpunit/includes/SpecialPageTest.php
+++ tests/phpunit/includes/SpecialPageTest.php
@@ -14,6 +14,7 @@ class SpecialPageTest extends MediaWikiTestCase {
 		parent::setUp();
 
 		$this->setMwGlobals( array(
+			'wgScript' => '/index.php',
 			'wgContLang' => Language::factory( 'en' )
 		) );
 	}
@@ -55,6 +56,60 @@ class SpecialPageTest extends MediaWikiTestCase {
 		return array(
 			array( Title::makeTitle( NS_SPECIAL, 'UserLogin' ), 'UserLogin' )
 		);
+	}
+
+	/**
+	 * @dataProvider requireLoginAnonProvider
+	 */
+	public function testRequireLoginAnon( $expected, $reason, $title ) {
+		$specialPage = new SpecialPage( 'Watchlist', 'viewmywatchlist' );
+
+		$user = User::newFromId( 0 );
+		$specialPage->getContext()->setUser( $user );
+		$specialPage->getContext()->setLanguage( Language::factory( 'en' ) );
+
+		$this->setExpectedException( 'UserNotLoggedIn', $expected );
+
+		if ( $reason === 'blank' && $title === 'blank' ) {
+			$specialPage->requireLogin();
+		} else {
+			$specialPage->requireLogin( $reason, $title );
+		}
+	}
+
+	public function requireLoginAnonProvider() {
+		$lang = 'en';
+
+		$msg = wfMessage( 'loginreqlink' )->inLanguage( $lang )->escaped();
+		$loginLink = '<a href="/index.php?title=Special:UserLogin&amp;returnto=Special%3AWatchlist"'
+			. ' title="Special:UserLogin">' . $msg . '</a>';
+
+		$expected1 = wfMessage( 'exception-nologin-text-manual' )
+			->params( $loginLink )->inLanguage( $lang )->text();
+
+		$expected2 = wfMessage( 'about' )->inLanguage( $lang )->text();
+
+		return array(
+			array( $expected1, null, null ),
+			array( $expected2, 'about', null ),
+			array( $expected2, wfMessage( 'about' ), null ),
+			array( $expected2, 'about', 'about' ),
+			array( $expected2, 'about', wfMessage( 'about' ) ),
+			array( $expected1, 'blank', 'blank' )
+		);
+	}
+
+	public function testRequireLoginNotAnon() {
+		$specialPage = new SpecialPage( 'Watchlist', 'viewmywatchlist' );
+
+		$user = User::newFromId( 0 );
+		$user->setId( 1 );
+		$specialPage->getContext()->setUser( $user );
+
+		$specialPage->requireLogin();
+
+		// no exception thrown, logged in use can access special page
+		$this->assertTrue( true );
 	}
 
 }
