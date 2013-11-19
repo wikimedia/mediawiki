ecdabb4c29103394d9ac215d06d5ad12ba0eb295
addshore <addshorewiki@gmail.com>
2013-11-19 14:08:16 +0100

    Formatting fixes in includes/htmlform/*
    Change-Id: Iee9e4f3fcf8491022ffe97c74312871f97f8ce9c

--- includes/htmlform/HTMLTextAreaField.php
+++ includes/htmlform/HTMLTextAreaField.php
@@ -4,11 +4,11 @@ class HTMLTextAreaField extends HTMLFormField {
 	const DEFAULT_ROWS = 25;
 
 	function getCols() {
-		return isset( $this->mParams[ 'cols' ] ) ? $this->mParams[ 'cols' ] : static::DEFAULT_COLS;
+		return isset( $this->mParams['cols'] ) ? $this->mParams['cols'] : static::DEFAULT_COLS;
 	}
 
 	function getRows() {
-		return isset( $this->mParams[ 'rows' ] ) ? $this->mParams[ 'rows' ] : static::DEFAULT_ROWS;
+		return isset( $this->mParams['rows'] ) ? $this->mParams['rows'] : static::DEFAULT_ROWS;
 	}
 
 	function getInputHTML( $value ) {
@@ -20,24 +20,24 @@ class HTMLTextAreaField extends HTMLFormField {
 			) + $this->getTooltipAndAccessKey();
 
 		if ( $this->mClass !== '' ) {
-			$attribs[ 'class' ] = $this->mClass;
+			$attribs['class'] = $this->mClass;
 		}
 
-		if ( ! empty( $this->mParams[ 'disabled' ] ) ) {
-			$attribs[ 'disabled' ] = 'disabled';
+		if ( ! empty( $this->mParams['disabled'] ) ) {
+			$attribs['disabled'] = 'disabled';
 		}
 
-		if ( ! empty( $this->mParams[ 'readonly' ] ) ) {
-			$attribs[ 'readonly' ] = 'readonly';
+		if ( ! empty( $this->mParams['readonly'] ) ) {
+			$attribs['readonly'] = 'readonly';
 		}
 
-		if ( isset( $this->mParams[ 'placeholder' ] ) ) {
-			$attribs[ 'placeholder' ] = $this->mParams[ 'placeholder' ];
+		if ( isset( $this->mParams['placeholder'] ) ) {
+			$attribs['placeholder'] = $this->mParams['placeholder'];
 		}
 
 		foreach ( array( 'required', 'autofocus' ) as $param ) {
-			if ( isset( $this->mParams[ $param ] ) ) {
-				$attribs[ $param ] = '';
+			if ( isset( $this->mParams[$param] ) ) {
+				$attribs[$param] = '';
 			}
 		}
 
