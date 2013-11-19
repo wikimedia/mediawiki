d62f4de375f3d852648a2644232e832f7e3a8f7b
Bartosz Dziewoński <matma.rex@gmail.com>
2013-11-19 12:15:06 +0100

    mediawiki.hlist: Use mw.hook( 'wikipage.content' ) in JS fallback
    …to make sure that any dynamically-inserted content will be processed
    as well.
    
    Change-Id: Ibc34668fea309c319118b29f51300da3ed06311a

--- resources/mediawiki/mediawiki.hlist.js
+++ resources/mediawiki/mediawiki.hlist.js
@@ -2,23 +2,29 @@
 	IE 8: Add pseudo-selector class to last-child list items
 	@author [[User:Edokter]]
 */
-jQuery( function( $ ) {
-	if ( $.client.profile().name === 'msie' ) {
-		if ( $.client.profile().versionNumber === 8 ) {
-			$( '.hlist' ).find( 'dd:last-child, dt:last-child, li:last-child' )
-				.addClass( 'hlist-last-child' );
-		}
+( function ( mw, $ ) {
+	var profile = $.client.profile();
+
+	if ( profile.name === 'msie' ) {
+		mw.hook( 'wikipage.content' ).add( function ( $content ) {
+			/* IE 8: Fallback for :last-child selector */
+			if ( profile.versionNumber === 8 ) {
+				$content.find( '.hlist' ).find( 'dd:last-child, dt:last-child, li:last-child' )
+					.addClass( 'hlist-last-child' );
+			}
+
 			/* IE 7 and below: Generate interpuncts and parentheses */
-		if ( $.client.profile().versionNumber <= 7 ) {
-			var $hlists = $( '.hlist' );
-			$hlists.find( 'dt:not(:last-child)' )
-				.append( ': ' );
-			$hlists.find( 'dd:not(:last-child)' )
-				.append( '<b>·</b> ' );
-			$hlists.find( 'li:not(:last-child)' )
-				.append( '<b>·</b> ' );
-			$hlists.find( 'dl dl, dl ol, dl ul, ol dl, ol ol, ol ul, ul dl, ul ol, ul ul' )
-				.prepend( '( ' ).append( ') ' );
-		}
+			if ( profile.versionNumber <= 7 ) {
+				var $hlists = $content.find( '.hlist' );
+				$hlists.find( 'dt:not(:last-child)' )
+					.append( ': ' );
+				$hlists.find( 'dd:not(:last-child)' )
+					.append( '<b>·</b> ' );
+				$hlists.find( 'li:not(:last-child)' )
+					.append( '<b>·</b> ' );
+				$hlists.find( 'dl dl, dl ol, dl ul, ol dl, ol ol, ol ul, ul dl, ul ol, ul ul' )
+					.prepend( '( ' ).append( ') ' );
+			}
+		} );
 	}
-} );
+}( mediaWiki, jQuery ) );
