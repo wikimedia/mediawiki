cdeaac6a2aea60101443f4d543149480c44457dc
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-11-20 12:21:34 +0100

    Remove underscore from classes LBFactory_*, LoadBalancer_*, LoadMonitor_*
    Part of program to remove underscores from class names. Checked core and
    600+ extensions for occurrences. All uses are in core in core are updated
    in this patch.
    
    Patches for extensions:
    * https://gerrit.wikimedia.org/r/#/c/96463/ CentralAuth
    * https://gerrit.wikimedia.org/r/#/c/96460/ SimpleSecurity
    * https://gerrit.wikimedia.org/r/#/c/96459/ WikiBase
    
    Change-Id: I507ba00a83bca8375a0215f00891b47e3c7afb9f

--- includes/db/LBFactory.php
+++ includes/db/LBFactory.php
@@ -37,7 +37,7 @@ abstract class LBFactory {
 	 */
 	public static function disableBackend() {
 		global $wgLBFactoryConf;
-		self::$instance = new LBFactory_Fake( $wgLBFactoryConf );
+		self::$instance = new LBFactoryFake( $wgLBFactoryConf );
 	}
 
 	/**
@@ -167,7 +167,7 @@ abstract class LBFactory {
 /**
  * A simple single-master LBFactory that gets its configuration from the b/c globals
  */
-class LBFactory_Simple extends LBFactory {
+class LBFactorySimple extends LBFactory {
 
 	/**
 	 * @var LoadBalancer
@@ -299,7 +299,7 @@ class LBFactory_Simple extends LBFactory {
  * Call LBFactory::disableBackend() to start using this, and
  * LBFactory::enableBackend() to return to normal behavior
  */
-class LBFactory_Fake extends LBFactory {
+class LBFactoryFake extends LBFactory {
 	function __construct( $conf ) {
 	}
 
