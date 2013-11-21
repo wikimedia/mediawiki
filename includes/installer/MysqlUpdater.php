b84bf002abd4b078d5f5f4acf5d820eafcde7012
umherirrender <umherirrender_de.wp@web.de>
2013-11-21 21:54:30 +0100

    Correct version section for rc_source in the updaters
    The patch for rc_source was reverted and at the end was merged for
    version 1.23 (I5412466444526986e1209d0982dd62cd5b387ad1).
    
    Release notes was already moved with
    I5113828701fde7d21c81ef3219d04202a9073f00
    
    Change-Id: If3b41a800f4982745eb883d5c4ac270ac75ffaa9

--- includes/installer/MysqlUpdater.php
+++ includes/installer/MysqlUpdater.php
@@ -242,6 +242,8 @@ class MysqlUpdater extends DatabaseUpdater {
 				'patch-iwlinks-from-title-index.sql' ),
 			array( 'addField', 'archive', 'ar_id', 'patch-archive-ar_id.sql' ),
 			array( 'addField', 'externallinks', 'el_id', 'patch-externallinks-el_id.sql' ),
+
+			// 1.23
 			array( 'addField', 'recentchanges', 'rc_source', 'patch-rc_source.sql' ),
 		);
 	}
