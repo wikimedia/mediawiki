fcb50cc5e9856648bc3c7a19b9b06ba2986a53e7
Chad Horohoe <chadh@wikimedia.org>
2013-11-21 15:37:18 -0800

    Improved CdbException handling in LocalisationCache
    Change-Id: If1ad9a982c2a4a7a1e9ca7739b3c215bb821211a

--- includes/cache/LocalisationCache.php
+++ includes/cache/LocalisationCache.php
@@ -1196,14 +1196,21 @@ class LCStoreCDB implements LCStore {
 			if ( !file_exists( $fileName ) ) {
 				$this->readers[$code] = false;
 			} else {
-				$this->readers[$code] = CdbReader::open( $fileName );
+				try {
+					$this->readers[$code] = CdbReader::open( $fileName );
+				} catch( CdbException $e ) {
+					wfDebug( __METHOD__ . ": unable to open cdb file for reading" );
+				}
 			}
 		}
 
 		if ( !$this->readers[$code] ) {
 			return null;
 		} else {
-			$value = $this->readers[$code]->get( $key );
+			try {
+				$value = $this->readers[$code]->get( $key );
+			} catch ( CdbException $e ) {
+			}
 
 			if ( $value === false ) {
 				return null;
@@ -1226,13 +1233,21 @@ class LCStoreCDB implements LCStore {
 			$this->readers[$code]->close();
 		}
 
-		$this->writer = CdbWriter::open( $this->getFileName( $code ) );
+		try {
+			$this->writer = CdbWriter::open( $this->getFileName( $code ) );
+		} catch ( CdbException $e ) {
+			throw new MWException( $e->getMessage() );
+		}
 		$this->currentLang = $code;
 	}
 
 	public function finishWrite() {
 		// Close the writer
-		$this->writer->close();
+		try {
+			$this->writer->close();
+		} catch ( CdbException $e ) {
+			throw new MWException( $e->getMessage() );
+		}
 		$this->writer = null;
 		unset( $this->readers[$this->currentLang] );
 		$this->currentLang = null;
@@ -1242,7 +1257,11 @@ class LCStoreCDB implements LCStore {
 		if ( is_null( $this->writer ) ) {
 			throw new MWException( __CLASS__ . ': must call startWrite() before calling set()' );
 		}
-		$this->writer->set( $key, serialize( $value ) );
+		try {
+			$this->writer->set( $key, serialize( $value ) );
+		} catch ( CdbException $e ) {
+			throw new MWException( $e->getMessage() );
+		}
 	}
 
 	protected function getFileName( $code ) {
