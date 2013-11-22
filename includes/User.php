254888774b5708e5980ba0a87ba9f8af3114580c
Kris <cblair91@gmail.com>
2013-11-22 01:09:54 +0000

    Fix for IP detection.
    Better IP detector
    
    Change-Id: I8447c8f2c55495c64848f94db5d89dd405ccd518

--- includes/User.php
+++ includes/User.php
@@ -541,11 +541,9 @@ class User {
 	 * Does the string match an anonymous IPv4 address?
 	 *
 	 * This function exists for username validation, in order to reject
-	 * usernames which are similar in form to IP addresses. Strings such
-	 * as 300.300.300.300 will return true because it looks like an IP
-	 * address, despite not being strictly valid.
+	 * usernames which are similar in form to IP addresses.
 	 *
-	 * We match "\d{1,3}\.\d{1,3}\.\d{1,3}\.xxx" as an anonymous IP
+	 * We match 000-255\.000-255\.000-255\.xxx as an anonymous IP
 	 * address because the usemod software would "cloak" anonymous IP
 	 * addresses like this, if we allowed accounts like this to be created
 	 * new users could get the old edits of these anonymous users.
@@ -554,7 +552,8 @@ class User {
 	 * @return bool
 	 */
 	public static function isIP( $name ) {
-		return preg_match( '/^\d{1,3}\.\d{1,3}\.\d{1,3}\.(?:xxx|\d{1,3})$/', $name ) || IP::isIPv6( $name );
+		# Matches if the IP range is actually between 0.0.0.0 and 255.255.255.255
+		return preg_match('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:xxx|25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/',$name || IP::isIPv6($name));
 	}
 
 	/**
