509bbd1a8ceb31fd84e3fb79c22f34ccea7873c8
Razvorotnev Ivan <RAZVOR2@gmail.com>
2013-11-22 20:55:13 +0400

    https://bugzilla.wikimedia.org/show_bug.cgi?id=55893 bug commited
    Change-Id: I82319b616cc92c46f56ec007a2bf4bdc244d6010

--- resources/mediawiki.page/mediawiki.page.image.pagination.js
+++ resources/mediawiki.page/mediawiki.page.image.pagination.js
@@ -8,6 +8,7 @@
 	// tr with the new page's table.multipageimage's tr element.
 	// table.multipageimage always has only one row.
 	function loadPage( page ) {
+		history.pushState({foo:"bar"},"foo",page);
 		var $multipageimage = $( 'table.multipageimage' ),
 			$spinner = $.createSpinner( {
 				size: 'large',
