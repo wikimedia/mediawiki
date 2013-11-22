a0306fc1deff13bcc32c157934891e9bf4cd77c4
Yaroslav Melnychuk <yaroslavmelnuchuk@gmail.com>
2013-11-22 02:03:12 +0200

    Fix collapsibleTabs code cleanup: null != undefined & undefined variables passed to .data().
    The reason why undefinded variables passed to .data(), is that the variable data in innner scope is undefinded,
    the solution is simple as executing this line:
    
    data = $.collapsibleTabs.getSettings( $( ele ) );
    
    before
    
    $( this ).detach().prependTo( target ).data( 'collapsibleTabsSettings', data );
    
    Change-Id: Ia7347f7e95ee6c4421f207166b15572011b188e2

--- skins/vector/collapsibleTabs.js
+++ skins/vector/collapsibleTabs.js
@@ -82,7 +82,7 @@
 		},
 		addData: function ( $collapsible ) {
 			var $settings = $collapsible.parent().data( 'collapsibleTabsSettings' );
-			if ( $settings !== null ) {
+			if ( $settings != null ) {
 				$collapsible.data( 'collapsibleTabsSettings', {
 					expandedContainer: $settings.expandedContainer,
 					collapsedContainer: $settings.collapsedContainer,
@@ -154,11 +154,9 @@
 					$( this ).hide();
 					// add the placeholder
 					$( '<span class="placeholder" style="display: none;"></span>' ).insertAfter( this );
-					// XXX: 'data' is undefined here, should the 'data' from the outer scope have
-					// a different name?
+					data = $.collapsibleTabs.getSettings( $( ele ) );
 					$( this ).detach().prependTo( target ).data( 'collapsibleTabsSettings', data );
 					$( this ).attr( 'style', 'display: list-item;' );
-					data = $.collapsibleTabs.getSettings( $( ele ) );
 					if ( data ) {
 						expContainerSettings = $.collapsibleTabs.getSettings( $( data.expandedContainer ) );
 						if ( expContainerSettings ) {
