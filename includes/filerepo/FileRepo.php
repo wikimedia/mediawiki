687c5b557778885a58902abc0643db7017e2f082
Aaron Schulz <aschulz@wikimedia.org>
2013-11-27 10:39:50 -0800

    Optimized LocalRepo::findFiles()
    Change-Id: Icea9c416c973a6b83c53e5bc201aed4528e9dd34

--- includes/filerepo/FileRepo.php
+++ includes/filerepo/FileRepo.php
@@ -473,7 +473,9 @@ class FileRepo {
 	 *     $findItem = array( 'title' => $title, 'private' => true );
 	 *     $findBatch = array( $findItem );
 	 *     $repo->findFiles( $findBatch );
-	 * @return array
+	 *
+	 *    No title should appear in $items twice, as the result use titles as keys
+	 * @return array (Map of file names => File objects) for matches
 	 */
 	public function findFiles( array $items ) {
 		$result = array();
