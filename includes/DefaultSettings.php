75ba90ea015247145da6d38414d69da47aa05ad3
This, that and the other <at.light@live.com.au>
2013-11-29 11:13:56 +1100

    Hide edit toolbar Signature button in content namespaces by default
    Most wikis do not require user signatures on content pages, so having the
    button available is confusing. For those few wikis that need this feature
    (I am imagining internal/corporate/planning wikis), they can set
    $wgShowSigButtonInContentNamespaces = true in LocalSettings.
    
    Since this is a change to default behavior, a release note is added.
    
    Bug: 5645
    Change-Id: I7ccf1093b888c7b33721234349ca0ac054c3cd3f

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5415,6 +5415,14 @@ $wgUniversalEditButton = true;
  */
 $wgUseAutomaticEditSummaries = true;
 
+/**
+ * Whether to display the Signature button on the edit toolbar in the content
+ * namespaces defined by $wgContentNamespaces. Most wikis will want to set this
+ * to false - only more traditional, "MeatballWiki"-style wikis should need
+ * this set to true.
+ */
+$wgShowSigButtonInContentNamespaces = false;
+
 /** @} */ # end edit UI }
 
 /************************************************************************//**
