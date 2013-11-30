83bf64853efbb1b4644506c15c46cabeafb0fcb4
01tonythomas <01tonythomas@gmail.com>
2013-11-30 19:28:09 +0530

    Removed 'Disable browser page caching" user preference
    It is  a very advanced user preference with little usage and is often misleading.
    Updated Release Note.
    
    Bug: 52809
    Change-Id: I43f6205df53c7a38717c60a2d7d144307feb58a4

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -4013,7 +4013,6 @@ $wgDefaultUserOptions = array(
 	'math' => 1,
 	'minordefault' => 0,
 	'newpageshidepatrolled' => 0,
-	'nocache' => 0,
 	'noconvertlink' => 0,
 	'norollbackdiff' => 0,
 	'numberheadings' => 0,
