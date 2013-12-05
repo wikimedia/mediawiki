aec0571da81d8c64a1e7d564969aa58a0270ee04
tonythomas01 <01tonythomas@gmail.com>
2013-12-05 22:23:16 +0530

    Removed 'Enable simplified search bar (Vector skin only)' from MW user Preference
    The vector-simplesearch preference is enabled by default since version 1.22. The option is
    removed to decrease the clutter in the preference page.
    
    Bug: 52815
    Change-Id: I6d8dd997a4c4a6b8fb06b4674dda5f306432c6af

--- includes/Preferences.php
+++ includes/Preferences.php
@@ -1023,16 +1023,8 @@ class Preferences {
 	 * @param $defaultPreferences Array
 	 */
 	static function searchPreferences( $user, IContextSource $context, &$defaultPreferences ) {
-		global $wgContLang, $wgVectorUseSimpleSearch;
-
-		if ( $wgVectorUseSimpleSearch ) {
-			$defaultPreferences['vector-simplesearch'] = array(
-				'type' => 'toggle',
-				'label-message' => 'vector-simplesearch-preference',
-				'section' => 'searchoptions/displaysearchoptions',
-			);
-		}
-
+		global $wgContLang;	
+		
 		$defaultPreferences['searcheverything'] = array(
 			'type' => 'toggle',
 			'label-message' => 'searcheverything-enable',
