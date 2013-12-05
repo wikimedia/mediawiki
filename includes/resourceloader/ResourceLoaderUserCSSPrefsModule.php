2f9cfa3d26fd11e4d816502df9f3e4a68b5bc4c4
tonythomas01 <01tonythomas@gmail.com>
2013-12-05 10:18:07 +0530

    Removed "Justify paragraphs" from MW user Preference.
    Removed the option 'Justify paragraphs' from MW Preferences
    as it is not a necessary option there.
    Added RELEASE NOTES.
    
    Bug: 52810
    Change-Id: I1fe6a5857070828726077e6ba229b786c017c858

--- includes/resourceloader/ResourceLoaderUserCSSPrefsModule.php
+++ includes/resourceloader/ResourceLoaderUserCSSPrefsModule.php
@@ -74,9 +74,6 @@ class ResourceLoaderUserCSSPrefsModule extends ResourceLoaderModule {
 			$rules[] = 'a:lang(ar), a:lang(kk-arab), a:lang(mzn), ' .
 			'a:lang(ps), a:lang(ur) { text-decoration: none; }';
 		}
-		if ( $options['justify'] ) {
-			$rules[] = "#article, #bodyContent, #mw_content { text-align: justify; }\n";
-		}
 		if ( $options['editfont'] !== 'default' ) {
 			// Double-check that $options['editfont'] consists of safe characters only
 			if ( preg_match( '/^[a-zA-Z0-9_, -]+$/', $options['editfont'] ) ) {
