321bb42a4ad7a1fbfa9ba8c38cf2be8aa9cf9f75
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-12-05 20:40:38 +0100

    Make visibleMetadataField protected
    Related patch sets:
    * https://gerrit.wikimedia.org/r/#/c/99429/ Extension PagedTiffHandler
    
    Change-Id: I0441fdf1ce1e06688f16c62e1993bb05911b45bc

--- includes/media/MediaHandler.php
+++ includes/media/MediaHandler.php
@@ -496,7 +496,7 @@ abstract class MediaHandler {
 	 *
 	 * @return array of strings
 	 */
-	function visibleMetadataFields() {
+	protected function visibleMetadataFields() {
 		return FormatMetadata::getVisibleFields();
 	}
 
