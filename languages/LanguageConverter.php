c17b0fce9af7baf9cc08f414d816ede9b3570740
Liangent <liangent@gmail.com>
2013-12-05 08:57:41 +0000

    Do title conversion on &action=edit if &redlink=1 exists
    Bug: 33231
    Change-Id: I33c3c9df4ff2215710bacb696b64bb4291dda24e

--- languages/LanguageConverter.php
+++ languages/LanguageConverter.php
@@ -754,6 +754,9 @@ class LanguageConverter {
 		global $wgDisableLangConversion, $wgDisableTitleConversion, $wgRequest;
 		$isredir = $wgRequest->getText( 'redirect', 'yes' );
 		$action = $wgRequest->getText( 'action' );
+		if ( $action == 'edit' && $wgRequest->getBool( 'redlink' ) ) {
+			$action = 'view';
+		}
 		$linkconvert = $wgRequest->getText( 'linkconvert', 'yes' );
 		$disableLinkConversion = $wgDisableLangConversion
 			|| $wgDisableTitleConversion;
