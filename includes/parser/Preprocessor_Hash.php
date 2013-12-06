d0c6d8b0a2213c23a72b8327578a6814c4846af4
Brad Jorsch <bjorsch@wikimedia.org>
2013-12-06 18:28:09 -0500

    Add PPFrame::isVolatile and PPFrame::setVolatile
    Most wikitext is safe to parse once and then cache for when that same
    wikitext is used again, such as for multiple transclusions of the same
    template within a page. There are occasions, though, where some piece of
    wikitext has side effects and so should not be cached; a prominent
    example of such wikitext is the <ref> and <references> tags in Cite.php.
    
    This change adds PPFrame::setVolatile so parser hooks such as <ref> and
    <references> can indicate that they have done something that should not
    be cached, and PPFrame::isVolatile so that callers of PPFrame::expand
    can know when to avoid caching.
    
    Bug: 46815
    Bug: 31834
    Change-Id: I95b3cf8781cf047cdb63da221cef45f3e7d1632e

--- includes/parser/Preprocessor_Hash.php
+++ includes/parser/Preprocessor_Hash.php
@@ -919,6 +919,8 @@ class PPFrame_Hash implements PPFrame {
 	 */
 	var $depth;
 
+	private $volatile = false;
+
 	/**
 	 * Construct a new preprocessor frame.
 	 * @param Preprocessor $preprocessor The parent preprocessor
@@ -1372,6 +1374,24 @@ class PPFrame_Hash implements PPFrame {
 	function getTitle() {
 		return $this->title;
 	}
+
+	/**
+	 * Set the volatile flag
+	 *
+	 * @param bool $flag
+	 */
+	function setVolatile( $flag = true ) {
+		$this->volatile = $flag;
+	}
+
+	/**
+	 * Get the volatile flag
+	 *
+	 * @return bool
+	 */
+	function isVolatile() {
+		return $this->volatile;
+	}
 }
 
 /**
@@ -1523,6 +1543,11 @@ class PPTemplateFrame_Hash extends PPFrame_Hash {
 	 */
 	function isTemplate() {
 		return true;
+	}
+
+	function setVolatile( $flag = true ) {
+		parent::setVolatile( $flag );
+		$this->parent->setVolatile( $flag );
 	}
 }
 
