73be6a5482142a705b20f88f9d55bf512ec7667b
Brian Wolff <bawolff+wn@gmail.com>
2013-12-06 16:29:30 -0400

    Normalize newlines in DjVu text-layer metadata.
    Currently, newlines in DjVu text layer are stored as the literal
    string '\n'. Its up to the consumer to unescape that into
    a real newline. Other formats like pdfs return newlines
    as an actual \n character when getPageText() is called.
    I think getPageText() should not require callers to do this.
    
    Change-Id: Ie1a438bbce5444c53ff6b7b3aaf2b5267ba3c8b4

--- includes/media/DjVuImage.php
+++ includes/media/DjVuImage.php
@@ -306,7 +306,8 @@ EOR;
 
 	function pageTextCallback( $matches ) {
 		# Get rid of invalid UTF-8, strip control characters
-		return '<PAGE value="' . htmlspecialchars( UtfNormal::cleanUp( $matches[1] ) ) . '" />';
+		$val = htmlspecialchars( UtfNormal::cleanUp( $matches[1] ) );
+		return '<PAGE value="' . str_replace( '\n', '&#10;', $val ) . '" />';
 	}
 
 	/**
