fe66e75cff565e44ab44cd8b7a479c5f2484d65d
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-12-07 23:13:18 +0100

    Use 'brackets' message instead of hard coded brackets
    WIP: Looking for some advice on how to resolve bug 57518 and found two
    other cases.
    
    Bug: 57518
    Change-Id: I92fa798dce9373419cbc6e888b19674ba28ab6d0

--- resources/Resources.php
+++ resources/Resources.php
@@ -765,7 +765,7 @@ return array(
 			'jquery.mwExtension',
 			'mediawiki.notify',
 		),
-		'messages' => array( 'showtoc', 'hidetoc' ),
+		'messages' => array( 'showtoc', 'hidetoc', 'brackets' ),
 		'position' => 'top', // For $wgPreloadJavaScriptMwUtil
 		'targets' => array( 'desktop', 'mobile' ),
 	),
