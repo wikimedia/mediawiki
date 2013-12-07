bb484679a1d0d0c48c8287e2dc62e993787376b9
Kunal Mehta <legoktm@gmail.com>
2013-12-06 18:37:32 -0800

    Add PreferencesFormPreSave hook
    Allows extensions to modify what preferences will be saved
    
    Change-Id: Ibd8331c39afdde39b688d78f3faade2883f0c997

--- includes/Preferences.php
+++ includes/Preferences.php
@@ -1456,6 +1456,7 @@ class Preferences {
 				$user->setOption( $key, $value );
 			}
 
+			wfRunHooks( 'PreferencesFormPreSave', array( $formData, $form, $user, &$result ) );
 			$user->saveSettings();
 		}
 
