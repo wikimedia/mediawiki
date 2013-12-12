3f2dcd06de7f056501365852221fe3b0f49db82c
Brian Wolff <bawolff+wn@gmail.com>
2013-12-12 13:01:50 -0700

    Fix inconsistency in ArchivedFile::getUser
    It was returning user id, where the interface is to take an
    argument, and by default return the user text.
    
    This caused weird output (user id instead of name) in certain places.
    
    Change-Id: Icae281de67cfaafe52f1fb1fa4c7556aff7cd306
    Follow-up: 8bd386e3680808d1ae1166140ef6917f0498ef3a

--- includes/filerepo/file/ArchivedFile.php
+++ includes/filerepo/file/ArchivedFile.php
@@ -451,15 +451,21 @@ class ArchivedFile {
 	}
 
 	/**
-	 * Return the user ID of the uploader.
+	 * Returns ID or name of user who uploaded the file
 	 *
-	 * @return int
+	 * @note Prior to MediaWiki 1.23, this method always
+	 *   returned the user id, and was inconsistent with
+	 *   the rest of the file classes.
+	 * @param string $type 'text' or 'id'
+	 * @return int|string
 	 */
-	public function getUser() {
+	public function getUser( $type = 'text' ) {
 		$this->load();
 		if ( $this->isDeleted( File::DELETED_USER ) ) {
 			return 0;
-		} else {
+		} elseif ( $type == 'text' ) {
+			return $this->user_text;
+		} elseif ( $type == 'id' ) {
 			return $this->user;
 		}
 	}
@@ -467,9 +473,11 @@ class ArchivedFile {
 	/**
 	 * Return the user name of the uploader.
 	 *
+	 * @deprecated 1.23 Use getUser( 'text' ) instead.
 	 * @return string
 	 */
 	public function getUserText() {
+		wfDeprecated( __METHOD__, '1.23' );
 		$this->load();
 		if ( $this->isDeleted( File::DELETED_USER ) ) {
 			return 0;
