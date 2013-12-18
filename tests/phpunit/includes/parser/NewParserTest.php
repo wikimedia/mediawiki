2d60cb04a4b851bbff7bc4ab26a5a9ce4168707b
Tpt <thomaspt@hotmail.fr>
2013-12-18 18:56:15 +0100

    Addition of a parser test for page= parameter of image inclusion
    Support for DjVu is detected and parser tests that rely on it are disable if needed.
    
    Change-Id: I53fd7b54e765d5f349abe74481bbc6f62f2b349e

--- tests/phpunit/includes/parser/NewParserTest.php
+++ tests/phpunit/includes/parser/NewParserTest.php
@@ -19,6 +19,7 @@ class NewParserTest extends MediaWikiTestCase {
 	public $keepUploads = false;
 	public $runDisabled = false;
 	public $runParsoid = false;
+	public $runDjVu = false;
 	public $regex = '';
 	public $showProgress = true;
 	public $savedWeirdGlobals = array();
@@ -41,6 +42,7 @@ class NewParserTest extends MediaWikiTestCase {
 	protected function setUp() {
 		global $wgNamespaceAliases, $wgContLang;
 		global $wgHooks, $IP;
+		global $wgDjvuRenderer, $wgDjvuDump, $wgDjvuToXML;
 
 		parent::setUp();
 
@@ -130,6 +132,9 @@ class NewParserTest extends MediaWikiTestCase {
 		// Vector images have to be handled slightly differently
 		$tmpGlobals['wgMediaHandlers']['image/svg+xml'] = 'MockSvgHandler';
 
+		// DjVu images have to be handled slightly differently
+		$tmpGlobals['wgMediaHandlers']['image/vnd.djvu'] = 'MockDjVuHandler';
+
 		$tmpHooks = $wgHooks;
 		$tmpHooks['ParserTestParser'][] = 'ParserTestParserHook::setup';
 		$tmpHooks['ParserGetVariableValueTs'][] = 'ParserTest::getFakeTimestamp';
@@ -137,6 +142,19 @@ class NewParserTest extends MediaWikiTestCase {
 		# add a namespace shadowing a interwiki link, to test
 		# proper precedence when resolving links. (bug 51680)
 		$tmpGlobals['wgExtraNamespaces'] = array( 100 => 'MemoryAlpha' );
+
+		//DjVu support
+		$tmpGlobals['wgDjvuRenderer'] = $wgDjvuRenderer ? $wgDjvuRenderer : '/usr/bin/ddjvu';
+		$tmpGlobals['wgDjvuDump'] = $wgDjvuDump ? $wgDjvuDump : '/usr/bin/djvudump';
+		$tmpGlobals['wgDjvuToXML'] = $wgDjvuToXML ? $wgDjvuToXML : '/usr/bin/djvutoxml';
+		$tmpGlobals['wgFileExtensions'][] = 'djvu';
+		if (
+			$this->checkIfToolExists( $tmpGlobals['wgDjvuRenderer'] ) &&
+			$this->checkIfToolExists( $tmpGlobals['wgDjvuDump'] ) &&
+			$this->checkIfToolExists( $tmpGlobals['wgDjvuToXML'] )
+		) {
+			$this->runDjVu = true;
+		}
 
 		$this->setMwGlobals( $tmpGlobals );
 
@@ -148,6 +166,19 @@ class NewParserTest extends MediaWikiTestCase {
 
 		MWNamespace::getCanonicalNamespaces( true ); # reset namespace cache
 		$wgContLang->resetNamespaces(); # reset namespace cache
+	}
+
+	/**
+	 * Check if a cli tool exists
+	 *
+	 * @param string $path path to the tool
+	 * @return bool
+	 */
+	protected function checkIfToolExists( $path ) {
+		wfSuppressWarnings();
+		$result = file_exists( $path );
+		wfRestoreWarnings();
+		return $result;
 	}
 
 	protected function tearDown() {
@@ -272,6 +303,46 @@ class NewParserTest extends MediaWikiTestCase {
 					'sha1'        => wfBaseConvert( '', 16, 36, 31 ),
 					'fileExists'  => true
 			), $this->db->timestamp( '20010115123500' ), $user );
+		}
+
+		# A DjVu file
+		$image = wfLocalFile( Title::makeTitle( NS_FILE, 'LoremIpsum.djvu' ) );
+		if ( !$this->db->selectField( 'image', '1', array( 'img_name' => $image->getName() ) ) ) {
+			$image->recordUpload2( '', 'Upload a DjVu', 'A DjVu', array(
+				'size' => 3249,
+				'width' => 2480,
+				'height' => 3508,
+				'media_type' => MEDIATYPE_BITMAP,
+				'mime' => 'image/vnd.djvu',
+				'metadata' => '<?xml version="1.0" ?>
+<!DOCTYPE DjVuXML PUBLIC "-//W3C//DTD DjVuXML 1.1//EN" "pubtext/DjVuXML-s.dtd">
+<DjVuXML>
+<HEAD></HEAD>
+<BODY><OBJECT height="3508" width="2480">
+<PARAM name="DPI" value="300" />
+<PARAM name="GAMMA" value="2.2" />
+</OBJECT>
+<OBJECT height="3508" width="2480">
+<PARAM name="DPI" value="300" />
+<PARAM name="GAMMA" value="2.2" />
+</OBJECT>
+<OBJECT height="3508" width="2480">
+<PARAM name="DPI" value="300" />
+<PARAM name="GAMMA" value="2.2" />
+</OBJECT>
+<OBJECT height="3508" width="2480">
+<PARAM name="DPI" value="300" />
+<PARAM name="GAMMA" value="2.2" />
+</OBJECT>
+<OBJECT height="3508" width="2480">
+<PARAM name="DPI" value="300" />
+<PARAM name="GAMMA" value="2.2" />
+</OBJECT>
+</BODY>
+</DjVuXML>',
+				'sha1' => wfBaseConvert( '4', 16, 36, 31 ),
+				'fileExists' => true
+			), $this->db->timestamp( '20010115123600' ), $user );
 		}
 	}
 
@@ -446,6 +517,10 @@ class NewParserTest extends MediaWikiTestCase {
 		$backend->store( array(
 			'src' => "$IP/skins/monobook/headbg.jpg", 'dst' => "$base/local-public/0/09/Bad.jpg"
 		) );
+		$backend->prepare( array( 'dir' => "$base/local-public/5/5f" ) );
+		$backend->store( array(
+			'src' => "$IP/tests/phpunit/data/media/LoremIpsum.djvu", 'dst' => "$base/local-public/5/5f/LoremIpsum.djvu"
+		) );
 
 		// No helpful SVG file to copy, so make one ourselves
 		$data = '<?xml version="1.0" encoding="utf-8"?>' .
@@ -507,6 +582,8 @@ class NewParserTest extends MediaWikiTestCase {
 				"$base/local-public/e/ea/Thumb.png",
 
 				"$base/local-public/0/09/Bad.jpg",
+
+				"$base/local-public/5/5f/LoremIpsum.djvu",
 
 				"$base/local-public/f/ff/Foobar.svg",
 				"$base/local-thumb/f/ff/Foobar.svg/180px-Foobar.svg.jpg",
