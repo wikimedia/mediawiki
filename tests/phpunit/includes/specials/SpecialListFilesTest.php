aaf43d50e06f78af87e8d85a5378a171f59cfad1
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-12-27 18:02:40 +0100

    Throw exception for unknown field in SpecialListfiles::formatValue()
    Change-Id: I99fedfb28aaf9b09b5d9fee27020601f3c06e8d4

---
+++ tests/phpunit/includes/specials/SpecialListFilesTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Test class for SpecialListFiles class.
+ *
+ * Copyright © 2013, Antoine Musso
+ * Copyright © 2013, Siebrand Mazeland
+ * Copyright © 2013, Wikimedia Foundation Inc.
+ *
+ */
+
+class SpecialListFilesTest extends MediaWikiTestCase {
+	/**
+	 * @expectedException MWException
+	 * @covers ImageListPager::formatValue
+	 */
+	public function testFormatValuesThrowException() {
+		$page = new ImageListPager( RequestContext::getMain() );
+		$page->formatValue( 'invalid_field', 'invalid_value' );
+	}
+}
