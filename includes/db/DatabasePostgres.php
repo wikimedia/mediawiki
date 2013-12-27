040cd7c6af1a0344056cc542919fc9358f3807d5
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-12-27 01:52:09 +0100

    Remove unused local variables
    Change-Id: I9e5667d3b27c7685004e2f3215ea51176988d3b2

--- includes/db/DatabasePostgres.php
+++ includes/db/DatabasePostgres.php
@@ -765,11 +765,8 @@ __INDEXATTR__;
 		if ( !$res ) {
 			return null;
 		}
-		foreach ( $res as $row ) {
-			return true;
-		}
 
-		return false;
+		return (bool) count( $res );
 	}
 
 	/**
@@ -880,7 +877,7 @@ __INDEXATTR__;
 			}
 		}
 		if ( $savepoint ) {
-			$olde = error_reporting( $olde );
+			error_reporting( $olde );
 			$savepoint->commit();
 
 			// Set the affected row count for the whole operation
@@ -952,7 +949,7 @@ __INDEXATTR__;
 				$savepoint->release();
 				$numrowsinserted++;
 			}
-			$olde = error_reporting( $olde );
+			error_reporting( $olde );
 			$savepoint->commit();
 
 			// Set the affected row count for the whole operation
