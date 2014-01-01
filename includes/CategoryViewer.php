71b44c0904662aa6a5b54b9715f3cd17cf22c59a
gomoko <gomoko@yahoo.com>
2014-01-01 19:54:40 +0100

    Added custom label for links in category pages
    CategoryViewer.php: Allows to give custom label to display in the category
    page, using existing syntax for category definition in pages, with label
    appended.
    Example: [[Category:Cat|Key|Label]]
      will display "Label" in the page Category:Cat for the link to the
      current page.
    Without this label, the page title will be displayed (as today).
    
    Bug: 61414
    Change-Id: I4ece7ec42afc825f27d89b7647404c5f03c172f2

--- includes/CategoryViewer.php
+++ includes/CategoryViewer.php
@@ -252,10 +252,10 @@ class CategoryViewer extends ContextSource {
 	 * @param $pageLength
 	 * @param $isRedirect bool
 	 */
-	function addPage( $title, $sortkey, $pageLength, $isRedirect = false ) {
+	function addPage( $title, $sortkey, $pageLength, $isRedirect = false, $alias = null ) {
 		global $wgContLang;
 
-		$link = Linker::link( $title );
+		$link = Linker::link( $title, strpos($alias, '|')===false?null:substr($alias, strpos($alias, '|')+1) );
 		if ( $isRedirect ) {
 			// This seems kind of pointless given 'mw-redirect' class,
 			// but keeping for back-compatibility with user css.
@@ -353,7 +353,7 @@ class CategoryViewer extends ContextSource {
 				} elseif ( $title->getNamespace() == NS_FILE ) {
 					$this->addImage( $title, $humanSortkey, $row->page_len, $row->page_is_redirect );
 				} else {
-					$this->addPage( $title, $humanSortkey, $row->page_len, $row->page_is_redirect );
+					$this->addPage( $title, substr($humanSortkey, 0, strpos($humanSortkey, '|')===false?strlen($humanSortkey):strpos($humanSortkey, '|')), $row->page_len, $row->page_is_redirect, $row->cl_sortkey_prefix );
 				}
 			}
 		}
