e6dc132be6c1d931484f1fd62eaa7203f311e0ae
mayankmadan <maddiemadan@gmail.com>
2014-01-03 05:01:09 +0530

    getApiWarnings() throws an exception if upload is invalid
    Bug: 38222
    Change-Id: Ieba283fb3b65fa8e67042e4374c95fa02a98ba56

--- includes/api/ApiUpload.php
+++ includes/api/ApiUpload.php
@@ -551,9 +551,13 @@ class ApiUpload extends ApiBase {
 	 * @return array
 	 */
 	protected function getApiWarnings() {
-		$warnings = $this->mUpload->checkWarnings();
+		if ( is_null($this->verifyUpload() ) )  {
+			$warnings = $this->mUpload->checkWarnings();
 
-		return $this->transformWarnings( $warnings );
+			return $this->transformWarnings( $warnings );
+		} else {
+			$this->dieUsage( 'There were errors during upload' );
+		}
 	}
 
 	protected function transformWarnings( $warnings ) {
