4dd1411bf104b16f6c54444974b8339f4c21c0db
umherirrender <umherirrender_de.wp@web.de>
2014-01-03 16:28:24 +0100

    Call Database::makeInsertOptions from Database::insertSelect
    This allows other database types to modify the options.
    
    Bug: 59233
    Change-Id: I45a2c7944057f43453423c8edbe5d173b1fc067d

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -2875,9 +2875,7 @@ abstract class DatabaseBase implements IDatabase, DatabaseType {
 	) {
 		$destTable = $this->tableName( $destTable );
 
-		if ( is_array( $insertOptions ) ) {
-			$insertOptions = implode( ' ', $insertOptions );
-		}
+		$insertOptions = $this->makeInsertOptions( $insertOptions );
 
 		if ( !is_array( $selectOptions ) ) {
 			$selectOptions = array( $selectOptions );
