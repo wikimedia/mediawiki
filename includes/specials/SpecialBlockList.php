76a432c6b820e48f3c7b04592da45acb52ae5ee2
Kunal Mehta <legoktm@gmail.com>
2014-01-03 01:39:13 -0800

    Turn HTMLBlockedUsersItemSelect into HTMLSelectLimitField
    Makes it a "real" HTMLForm option that other code can re-use
    
    Change-Id: If0fb7332daf991b790bbf87e825229dccb10b360

--- includes/specials/SpecialBlockList.php
+++ includes/specials/SpecialBlockList.php
@@ -84,7 +84,7 @@ class SpecialBlockList extends SpecialPage {
 				'flatlist' => true,
 			),
 			'Limit' => array(
-				'class' => 'HTMLBlockedUsersItemSelect',
+				'type' => 'limitselect',
 				'label-message' => 'table_pager_limit_label',
 				'options' => array(
 					$lang->formatNum( 20 ) => 20,
@@ -455,39 +455,5 @@ class BlockListPager extends TablePager {
 
 		$lb->execute();
 		wfProfileOut( __METHOD__ );
-	}
-}
-
-/**
- * Items per page dropdown. Essentially a crap workaround for bug 32603.
- */
-class HTMLBlockedUsersItemSelect extends HTMLSelectField {
-	/**
-	 * Basically don't do any validation. If it's a number that's fine. Also,
-	 * add it to the list if it's not there already
-	 *
-	 * @param string $value
-	 * @param array $alldata
-	 * @return bool
-	 */
-	function validate( $value, $alldata ) {
-		if ( $value == '' ) {
-			return true;
-		}
-
-		// Let folks pick an explicit limit not from our list, as long as it's a real numbr.
-		if ( !in_array( $value, $this->mParams['options'] )
-			&& $value == intval( $value )
-			&& $value > 0
-		) {
-			// This adds the explicitly requested limit value to the drop-down,
-			// then makes sure it's sorted correctly so when we output the list
-			// later, the custom option doesn't just show up last.
-			$this->mParams['options'][$this->mParent->getLanguage()->formatNum( $value )] =
-				intval( $value );
-			asort( $this->mParams['options'] );
-		}
-
-		return true;
 	}
 }
