48fd99354f1e75b6f761ba47e83fb6a8b250b32f
This, that and the other <at.light@live.com.au>
2014-01-07 13:39:18 +1100

    Add namespace-wide page notice functionality
    Akin to editnotices, page notices are arbitrary wikitext-containing
    system messages displayed for every page in a particular namespace.
    
    Naming is similar to editnotices, e.g. MediaWiki:Pagenotice-2 is seen at
    the top of all User pages, below the page title and tag line but above the
    page content. Notices are not shown except on action=view.
    
    This change includes some of the functionality of the PageNotice extension,
    but it is not backwards compatible with that extension (messages names are
    different, and footers are not supported).
    
    This change does not introduce per-page notices. Such notices should be
    added simply by editing the page.
    
    Change-Id: Iedfe730068e4511bd275aa00e3c72e9fb1dd04ab

--- includes/Title.php
+++ includes/Title.php
@@ -4944,6 +4944,30 @@ class Title {
 	}
 
 	/**
+	 * Get a list of rendered page notices (displayed above page content) for this page.
+	 *
+	 * Array is keyed by the original message key, and values are rendered using parseAsBlock, so
+	 * they will already be wrapped in paragraphs.
+	 *
+	 * @since 1.23
+	 * @param int oldid Revision ID that's being viewed
+	 * @return Array
+	 */
+	public function getPageNotices( $oldid = 0 ) {
+		$notices = array();
+
+		# Optional notices on a per-namespace basis
+		$pagenoticeNs = 'pagenotice-' . $this->getNamespace();
+		$pagenoticeNsMessage = wfMessage( $pagenoticeNs );
+		if ( $pagenoticeNsMessage->exists() ) {
+			$notices[$pagenoticeNs] = $pagenoticeNsMessage->parseAsBlock();
+		}
+
+		wfRunHooks( 'TitleGetPageNotices', array( $this, $oldid, &$notices ) );
+		return $notices;
+	}
+
+	/**
 	 * Get a list of rendered edit notices for this page.
 	 *
 	 * Array is keyed by the original message key, and values are rendered using parseAsBlock, so
