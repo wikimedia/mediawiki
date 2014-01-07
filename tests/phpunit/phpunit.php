46db65ed0708822ca35ebc44d71c0d18e53acc72
Adrian Lang <adrian.lang@wikimedia.de>
2014-01-07 10:30:49 +0100

    Update PHPUnit version check to 3.7.0
    assertContainsOnlyInstancesOf has been added in PHPUnit 3.7.0, so we need to
    have that version for running the tests.
    
    Bug: 59759
    Change-Id: I25dbd377b97ec0b9062d3c8cf495ba3f06956ac4

--- tests/phpunit/phpunit.php
+++ tests/phpunit/phpunit.php
@@ -108,9 +108,9 @@ if ( !class_exists( 'PHPUnit_Runner_Version' ) ) {
 }
 
 if ( PHPUnit_Runner_Version::id() !== '@package_version@'
-	&& version_compare( PHPUnit_Runner_Version::id(), '3.6.7', '<' )
+	&& version_compare( PHPUnit_Runner_Version::id(), '3.7.0', '<' )
 ) {
-	die( 'PHPUnit 3.6.7 or later required, you have ' . PHPUnit_Runner_Version::id() . ".\n" );
+	die( 'PHPUnit 3.7.0 or later required, you have ' . PHPUnit_Runner_Version::id() . ".\n" );
 }
 
 if ( !class_exists( 'PHPUnit_TextUI_Command' ) ) {
