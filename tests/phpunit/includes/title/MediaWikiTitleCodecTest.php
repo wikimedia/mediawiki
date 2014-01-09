7c3cbe1e2232b1925f2dcd141370a0093ca2cfc0
daniel <daniel.kinzler@wikimedia.de>
2014-01-09 15:35:16 +0100

    Introducing TitleValue
    TitleValue is intended to be a light weight alternative to the Title
    class. Any operations on TitleValue are implemented in separate
    service classes, like TitleFormatter and PageLinkRenderer. See
    <https://www.mediawiki.org/wiki/Requests_for_comment/TitleValue>
    for more information.
    
    This change updates SpecialCategories and SpecialLinkSearch to use
    TitleValue instead of Title, to demonstrate how TitleValue would be
    used, and how the necessary services can be injected and applied.
    
    The intention is to improve testability and reusability; these
    advantages will however only become apparent with further refactoring
    of the respective special pages. This will be done in follow-up
    changes.
    
    More work will be needed to migrate essential functionality from
    Title and Linker classes into the respective service classes,
    MediaWikiTitleCodec and MediaWikiPageLinkRenderer.
    
    Change-Id: I8eef5a165de4ffcacfbc4911fdacdb15d502fff4

---
+++ tests/phpunit/includes/title/MediaWikiTitleCodecTest.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+
+/**
+ * @covers MediaWikiTitleCodec
+ *
+ * @group Title
+ * @group Database
+ *        ^--- needed because of global state in
+ */
+class MediaWikiTitleCodecTest extends MediaWikiTestCase {
+
+	/**
+	 * Returns a mock GenderCache that will consider a user "female" if the
+	 * first part of the user name ends with "a".
+	 *
+	 * @return GenderCache
+	 */
+	private function getGenderCache() {
+		$genderCache = $this->getMockBuilder( 'GenderCache' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$genderCache->expects( $this->any() )
+			->method( 'getGenderOf' )
+			->will( $this->returnCallback( function( $userName ) {
+				return preg_match( '/^[^- _]+a( |_|$)/u', $userName ) ? 'female' : 'male';
+			} ) );
+
+		return $genderCache;
+	}
+
+	protected function makeCodec( $lang ) {
+		$gender = $this->getGenderCache();
+		$lang = Language::factory( $lang );
+		return new MediaWikiTitleCodec( $lang, $gender );
+	}
+
+	public function provideFormatForDisplay() {
+		return array(
+			array( new TitleValue( TitleValue::DBKEY_FORM, NS_MAIN, 'Foo_Bar' ), TitleFormatter::INCLUDE_ALL, 'en', 'Foo Bar' ),
+			array( new TitleValue( TitleValue::DBKEY_FORM, NS_USER, 'Hansi_Maier', 'stuff' ), TitleFormatter::INCLUDE_ALL, 'en', 'User:Hansi Maier#stuff' ),
+			array( new TitleValue( TitleValue::DBKEY_FORM, NS_USER, 'Hansi_Maier', 'stuff' ), TitleFormatter::INCLUDE_BASE, 'en', 'Hansi Maier' ),
+			array( new TitleValue( TitleValue::UNKNOWN_FORM, NS_USER_TALK, 'Hansi_Maier' ), TitleFormatter::INCLUDE_ALL, 'en', 'User talk:Hansi Maier' ),
+			array( new TitleValue( TitleValue::DBKEY_FORM, NS_USER, 'Lisa_Müller', 'stuff' ), TitleFormatter::INCLUDE_NAMESPACE, 'de', 'Benutzerin:Lisa Müller' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideFormatForDisplay
+	 */
+	public function testFormatForDisplay( TitleValue $title, $parts, $lang, $expected ) {
+		$codec = $this->makeCodec( $lang );
+		$actual = $codec->formatForDisplay( $title, $parts );
+
+		$this->assertEquals( $expected, $actual );
+
+		// test round trip
+		$full = $codec->formatForDisplay( $title, TitleFormatter::INCLUDE_ALL );
+		$parsed = $codec->parseTitle( $full, NS_MAIN, $title->getForm() );
+
+		$this->assertEquals( $title, $parsed );
+	}
+
+	public function provideFormatForDatabase() {
+		return array(
+			array( new TitleValue( TitleValue::TITLE_FORM, NS_MAIN, 'Foo Bar' ), 'en', 'Foo_Bar' ),
+			array( new TitleValue( TitleValue::DBKEY_FORM, NS_USER, 'Hansi_Maier', 'stuff' ), 'en', 'Hansi_Maier' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideFormatForDatabase
+	 */
+	public function testFormatForDatabase( TitleValue $title, $lang, $expected ) {
+		$codec = $this->makeCodec( $lang );
+		$actual = $codec->formatForDatabase( $title );
+
+		$this->assertEquals( $expected, $actual );
+
+		// test round trip
+		$full = $codec->formatForDatabase( $title, TitleFormatter::INCLUDE_ALL );
+		$parsed = $codec->parseTitle( $full, NS_MAIN, $title->getForm() );
+
+		$this->assertEquals( $title, $parsed );
+	}
+
+	public function provideParseTitle() {
+		//TODO: test capitalization and trimming
+		//TODO: test unicode normalization
+
+		return array(
+			array( ':Hansi_Maier', NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_MAIN, 'Hansi Maier', '' ) ),
+			array( ':Hansi_Maier', NS_CATEGORY, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_MAIN, 'Hansi Maier', '' ) ),
+			array( ' lisa Müller', NS_USER, TitleValue::DBKEY_FORM, 'de', new TitleValue( TitleValue::DBKEY_FORM, NS_USER, 'Lisa_Müller', '' ) ),
+			array( 'benutzerin:lisa Müller#stuff', NS_MAIN, TitleValue::TITLE_FORM, 'de', new TitleValue( TitleValue::TITLE_FORM, NS_USER, 'Lisa Müller', 'stuff' ) ),
+			array( 'benutzerin:lisa Müller#stuff', NS_MAIN, TitleValue::DBKEY_FORM, 'en', new TitleValue( TitleValue::DBKEY_FORM, NS_MAIN, 'Benutzerin:lisa_Müller', 'stuff' ) ),
+
+			//NOTE: cases copied from TitleTest::testSecureAndSplit. Keep in sync.
+			array( 'Sandbox', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( 'A "B"', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( 'A \'B\'', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( '.com', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( '~', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( '"', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( '\'', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( 'Talk:Sandbox', NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_TALK, 'Sandbox' ) ),
+			array( 'Talk:Foo:Sandbox', NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_TALK, 'Foo:Sandbox' ) ),
+			array( 'File:Example.svg', NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_FILE, 'Example.svg' ) ),
+			array( 'File_talk:Example.svg', NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_FILE_TALK, 'Example.svg' ) ),
+			array( 'Foo/.../Sandbox', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( 'Sandbox/...', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			array( 'A~~', NS_MAIN, TitleValue::TITLE_FORM, 'en', ),
+			// Length is 256 total, but only title part matters
+			array( 'Category:' . str_repeat( 'x', 248 ), NS_MAIN, TitleValue::TITLE_FORM, 'en', new TitleValue( TitleValue::TITLE_FORM, NS_CATEGORY, 'X' . str_repeat( 'x', 247 ) ) ),
+			array( str_repeat( 'x', 252 ), NS_MAIN, TitleValue::TITLE_FORM, 'en', 'X' . str_repeat( 'x', 251 ) )
+		);
+	}
+
+	/**
+	 * @dataProvider provideParseTitle
+	 */
+	public function testParseTitle( $text, $ns, $form, $lang, $title = null ) {
+		if ( $title === null ) {
+			$title = $text;
+		}
+
+		if ( is_string( $title ) ) {
+			$title = new TitleValue( TitleValue::TITLE_FORM, NS_MAIN, $title, '' );
+		}
+
+		$codec = $this->makeCodec( $lang );
+		$actual = $codec->parseTitle( $text, $ns, $form );
+
+		$this->assertEquals( $title, $actual );
+	}
+
+	public function provideParseTitle_invalid() {
+		//TODO: test unicode errors
+
+		return array(
+			array( '#' ),
+			array( '::' ),
+			array( '::xx' ),
+			array( '::##' ),
+
+			//NOTE: cases copied from TitleTest::testSecureAndSplit. Keep in sync.
+			array( '' ),
+			array( ':' ),
+			array( '__  __' ),
+			array( '  __  ' ),
+			// Bad characters forbidden regardless of wgLegalTitleChars
+			array( 'A [ B' ),
+			array( 'A ] B' ),
+			array( 'A { B' ),
+			array( 'A } B' ),
+			array( 'A < B' ),
+			array( 'A > B' ),
+			array( 'A | B' ),
+			// URL encoding
+			array( 'A%20B' ),
+			array( 'A%23B' ),
+			array( 'A%2523B' ),
+			// XML/HTML character entity references
+			// Note: Commented out because they are not marked invalid by the PHP test as
+			// Title::newFromText runs Sanitizer::decodeCharReferencesAndNormalize first.
+			//array( 'A &eacute; B' ),
+			//array( 'A &#233; B' ),
+			//array( 'A &#x00E9; B' ),
+			// Subject of NS_TALK does not roundtrip to NS_MAIN
+			array( 'Talk:File:Example.svg' ),
+			// Directory navigation
+			array( '.' ),
+			array( '..' ),
+			array( './Sandbox' ),
+			array( '../Sandbox' ),
+			array( 'Foo/./Sandbox' ),
+			array( 'Foo/../Sandbox' ),
+			array( 'Sandbox/.' ),
+			array( 'Sandbox/..' ),
+			// Tilde
+			array( 'A ~~~ Name' ),
+			array( 'A ~~~~ Signature' ),
+			array( 'A ~~~~~ Timestamp' ),
+			array( str_repeat( 'x', 256 ) ),
+			// Namespace prefix without actual title
+			array( 'Talk:' ),
+			array( 'Category: ' ),
+			array( 'Category: #bar' )
+		);
+	}
+
+	/**
+	 * @dataProvider provideParseTitle_invalid
+	 */
+	public function testParseTitle_invalid( $text ) {
+		$this->setExpectedException( 'InvalidArgumentException' );
+		
+		$codec = $this->makeCodec( 'en' );
+		$codec->parseTitle( $text, NS_MAIN, TitleValue::DBKEY_FORM );
+	}
+}
