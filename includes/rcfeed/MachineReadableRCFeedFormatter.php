f1d28a569d0f1a844c4b3c3731817415dd2a2e34
Kunal Mehta <legoktm@gmail.com>
2014-01-10 17:52:25 -0800

    rcfeed: Use wfWikiID() instead of $wgDBname
    Change-Id: Id1f408a4c498f23f656298e3453d2a9264e3ac7c

--- includes/rcfeed/MachineReadableRCFeedFormatter.php
+++ includes/rcfeed/MachineReadableRCFeedFormatter.php
@@ -38,7 +38,7 @@ abstract class MachineReadableRCFeedFormatter implements RCFeedFormatter {
 	 * @see RCFeedFormatter::getLine
 	 */
 	public function getLine( array $feed, RecentChange $rc, $actionComment ) {
-		global $wgCanonicalServer, $wgScriptPath, $wgDBname;
+		global $wgCanonicalServer, $wgScriptPath;
 		$attrib = $rc->getAttributes();
 
 		$packet = array(
@@ -121,7 +121,7 @@ abstract class MachineReadableRCFeedFormatter implements RCFeedFormatter {
 
 		$packet['server_url'] = $wgCanonicalServer;
 		$packet['server_script_path'] = $wgScriptPath ?: '/';
-		$packet['wiki'] = $wgDBname;
+		$packet['wiki'] = wfWikiID();
 
 		return $this->formatArray( $packet );
 	}
