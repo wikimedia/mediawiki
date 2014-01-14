810aede65c9a3e90b7fafdcaf5e05a1cc25bfc6a
MarkAHershberger <mhershberger@wikimedia.org>
2014-01-14 02:57:45 +0000

    Revert "bump to 1.19.10"
    This reverts commit 33aa2afd8c5637e3e7d0464b03881342924926e1.
    
    Change-Id: I1477324f479ad7b0f2900822a496417ead711413

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.10';
+$wgVersion = '1.19.9';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
@@ -4228,7 +4228,7 @@ $wgParserTestFiles = array(
  * );
  */
 $wgParserTestRemote = false;
-
+ 
 /**
  * Allow running of javascript test suites via [[Special:JavaScriptTest]] (such as QUnit).
  */
