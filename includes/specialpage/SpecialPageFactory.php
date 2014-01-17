89d731cff34d4f0f4ef1ab3eb706851dfb25bd49
Aaron Schulz <aschulz@wikimedia.org>
2014-01-16 16:07:09 -0800

    Put ActiveUsers behind the miser mode flag
    Change-Id: If786dea8a6195d11c812d46a583d272c7aa70fe7

--- includes/specialpage/SpecialPageFactory.php
+++ includes/specialpage/SpecialPageFactory.php
@@ -98,7 +98,6 @@ class SpecialPageFactory {
 		'Listusers'                 => 'SpecialListUsers',
 		'Listadmins'                => 'SpecialListAdmins',
 		'Listbots'                  => 'SpecialListBots',
-		'Activeusers'               => 'SpecialActiveUsers',
 		'Userrights'                => 'UserrightsPage',
 		'EditWatchlist'             => 'SpecialEditWatchlist',
 
@@ -180,6 +179,7 @@ class SpecialPageFactory {
 		global $wgSpecialPages;
 		global $wgDisableCounters, $wgDisableInternalSearch, $wgEmailAuthentication;
 		global $wgEnableEmail, $wgEnableJavaScriptTest;
+		global $wgMiserMode;
 
 		if ( !is_object( self::$list ) ) {
 			wfProfileIn( __METHOD__ );
@@ -203,6 +203,10 @@ class SpecialPageFactory {
 
 			if ( $wgEnableJavaScriptTest ) {
 				self::$list['JavaScriptTest'] = 'SpecialJavaScriptTest';
+			}
+
+			if ( !$wgMiserMode ) {
+				self::$list['Activeusers'] = 'SpecialActiveUsers';
 			}
 
 			// Add extension special pages
