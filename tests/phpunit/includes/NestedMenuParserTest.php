ebfaf437c32e1606720463680451d2b96d610d8e
Jack Phoenix <jack@countervandalism.net>
2014-01-17 16:27:54 +0200

    New class for parsing MediaWiki:Sidebar-like messages into modern, nested navigation menus.
    This allows to reduce the amount of code duplication needed by custom
    skins, as well as ease the porting of WordPress themes into MediaWiki
    (many WordPress themes feature a nested navigation menu).
    
    Currently at least the Nimbus and Monaco skins implement a similar feature, just
    without this class.
    
    Based on NavigationService by various Wikia developers.
    
    See https://www.mediawiki.org/wiki/Manual:NestedMenuParser for some more
    documentation and an example on how to use this class with a custom skin.
    
    Change-Id: Ib8dc2787f70dc80be6ba9d3c962edc1463247263

---
+++ tests/phpunit/includes/NestedMenuParserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Changing the visibility of methods to be able to test them.
+ */
+class TestableNestedMenuParser extends NestedMenuParser {
+	public function parseLines( array $lines, array $maxChildrenAtLevel = array() ) {
+		parent::parseLines( $lines, $maxChildrenAtLevel );
+	}
+
+	public function parseOneLine( $line ) {
+		parent::parseOneLine( $line );
+	}
+}
+
+
+/**
+ * @covers NestedMenuParser
+ */
+class NestedMenuParserTest extends MediaWikiTestCase {
+	public function testParseOneLine() {
+		$service = new TestableNestedMenuParser();
+
+		$this->assertEquals(
+			$service->parseOneLine( "** Yet another child|This text differs from the page name" ),
+			array(
+				'original' => '???',
+				'text' => '???',
+				'href' => '???'
+			)
+		);
+	}
+
+	public function testParseLines() {
+		$service = new TestableNestedMenuParser();
+
+
+	}
+
+	public function testParseMessage() {
+		$service = new TestableNestedMenuParser();
+
+
+	}
+}
