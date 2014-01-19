c454bb904679f3e1f0ba12af9af906ffccf74f60
Kunal Mehta <legoktm@gmail.com>
2014-01-18 17:13:16 -0800

    Add tests for API's assert={user|bot}
    Change-Id: I5bff0adf11bccef137d59c05edae45d774596994

--- tests/phpunit/includes/api/ApiMainTest.php
+++ tests/phpunit/includes/api/ApiMainTest.php
@@ -30,4 +30,43 @@ class ApiMainTest extends ApiTestCase {
 		$this->assertThat( $sxe, $this->isInstanceOf( "SimpleXMLElement" ) );
 	}
 
+	public static function provideAssert() {
+		$anon = new User();
+		$bot = new User();
+		$bot->setName( 'Bot' );
+		$bot->addToDatabase();
+		$bot->addGroup( 'bot' );
+		$user = new User();
+		$user->setName( 'User' );
+		$user->addToDatabase();
+		return array(
+			array( $anon, 'user', 'assertuserfailed' ),
+			array( $user, 'user', false ),
+			array( $user, 'bot', 'assertbotfailed' ),
+			array( $bot, 'user', false ),
+			array( $bot, 'bot', false ),
+		);
+	}
+
+	/**
+	 * Tests the assert={user|bot} functionality
+	 *
+	 * @covers ApiMain::checkAsserts
+	 * @dataProvider provideAssert
+	 * @param User $user
+	 * @param string $assert
+	 * @param string|bool $error false if no error expected
+	 */
+	public function testAssert( $user, $assert, $error ) {
+		try {
+			$this->doApiRequest( array(
+				'action' => 'query',
+				'assert' => $assert,
+			), null, null, $user );
+			$this->assertFalse( $error ); // That no error was expected
+		} catch ( UsageException $e ) {
+			$this->assertEquals( $e->getCodeString(), $error );
+		}
+	}
+
 }
