6c874b9181d6dc85a735c102c7ff6fef0d1ac58e
umherirrender <umherirrender_de.wp@web.de>
2014-01-24 21:23:25 +0100

    Little change of readability in Title::secureAndSplit
    Move init of mArticleId to bottom, where other fields are filled and
    change ternary operator to a simple if for better readability.
    
    Change-Id: Ib61435e62e883142dd0c5dcb40e0e50e7ca18439

--- includes/Title.php
+++ includes/Title.php
@@ -3305,10 +3305,6 @@ class Title {
 			break;
 		} while ( true );
 
-		# We already know that some pages won't be in the database!
-		if ( $this->isExternal() || NS_SPECIAL == $this->mNamespace ) {
-			$this->mArticleID = 0;
-		}
 		$fragment = strstr( $dbkey, '#' );
 		if ( false !== $fragment ) {
 			$this->setFragment( $fragment );
@@ -3378,9 +3374,9 @@ class Title {
 		// there are numerous ways to present the same IP. Having sp:contribs scan
 		// them all is silly and having some show the edits and others not is
 		// inconsistent. Same for talk/userpages. Keep them normalized instead.
-		$dbkey = ( $this->mNamespace == NS_USER || $this->mNamespace == NS_USER_TALK )
-			? IP::sanitizeIP( $dbkey )
-			: $dbkey;
+		if ( $this->mNamespace == NS_USER || $this->mNamespace == NS_USER_TALK ) {
+			$dbkey = IP::sanitizeIP( $dbkey );
+		}
 
 		// Any remaining initial :s are illegal.
 		if ( $dbkey !== '' && ':' == $dbkey[0] ) {
@@ -3392,6 +3388,11 @@ class Title {
 		$this->mUrlform = wfUrlencode( $dbkey );
 
 		$this->mTextform = str_replace( '_', ' ', $dbkey );
+
+		# We already know that some pages won't be in the database!
+		if ( $this->isExternal() || $this->mNamespace == NS_SPECIAL ) {
+			$this->mArticleID = 0;
+		}
 
 		return true;
 	}
