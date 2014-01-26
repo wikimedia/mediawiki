13d044085c737fcf14527afa00ec5a21a689541c
This, that and the other <at.light@live.com.au>
2014-01-26 12:55:17 +1100

    Give a meaningful message when Special:ActiveUsers is disabled
    Follow-up to If786dea8a6195d11c812d46a583d272c7aa70fe7.
    
    Bug: 60436
    Change-Id: I29a063bb8659ae0d55397715da5b98ab16d336fc

--- includes/specials/SpecialActiveusers.php
+++ includes/specials/SpecialActiveusers.php
@@ -240,10 +240,14 @@ class SpecialActiveUsers extends SpecialPage {
 	 * @param $par Mixed: parameter passed to the page or null
 	 */
 	public function execute( $par ) {
-		global $wgActiveUserDays;
+		global $wgActiveUserDays, $wgMiserMode;
 
 		$this->setHeaders();
 		$this->outputHeader();
+
+		if ( $wgMiserMode ) {
+			throw new ErrorPageError( 'activeusers', 'querypage-disabled' );
+		}
 
 		$out = $this->getOutput();
 		$out->wrapWikiMsg( "<div class='mw-activeusers-intro'>\n$1\n</div>",
