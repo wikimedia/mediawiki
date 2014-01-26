59c4b6b8a9e8e3f371b5e3efc8cb65f6569e34d7
addshore <addshorewiki@gmail.com>
2014-01-26 18:49:18 +0100

    Split Title and User ArrayFromResult classes to own files
    Change-Id: I8744627330eb9c8f183a8d418d6eb168e6c5407b

--- includes/TitleArray.php
+++ includes/TitleArray.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Classes to walk into a list of Title objects.
+ * Class to walk into a list of Title objects.
  *
  * Note: this entire file is a byte-for-byte copy of UserArray.php with
  * s/User/Title/.  If anyone can figure out how to do this nicely with
@@ -53,66 +53,5 @@ abstract class TitleArray implements Iterator {
 	protected static function newFromResult_internal( $res ) {
 		$array = new TitleArrayFromResult( $res );
 		return $array;
-	}
-}
-
-class TitleArrayFromResult extends TitleArray implements Countable {
-
-	/**
-	 * @var ResultWrapper
-	 */
-	var $res;
-	var $key, $current;
-
-	function __construct( $res ) {
-		$this->res = $res;
-		$this->key = 0;
-		$this->setCurrent( $this->res->current() );
-	}
-
-	/**
-	 * @param $row ResultWrapper
-	 * @return void
-	 */
-	protected function setCurrent( $row ) {
-		if ( $row === false ) {
-			$this->current = false;
-		} else {
-			$this->current = Title::newFromRow( $row );
-		}
-	}
-
-	/**
-	 * @return int
-	 */
-	public function count() {
-		return $this->res->numRows();
-	}
-
-	function current() {
-		return $this->current;
-	}
-
-	function key() {
-		return $this->key;
-	}
-
-	function next() {
-		$row = $this->res->next();
-		$this->setCurrent( $row );
-		$this->key++;
-	}
-
-	function rewind() {
-		$this->res->rewind();
-		$this->key = 0;
-		$this->setCurrent( $this->res->current() );
-	}
-
-	/**
-	 * @return bool
-	 */
-	function valid() {
-		return $this->current !== false;
 	}
 }
