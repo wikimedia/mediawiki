64ab79c64889938b59ed0c2e223c5b26a4e212f6
lekshmi <andnlnbn18@gmail.com>
2014-01-28 19:49:14 +0530

    Changed deprecated $wgCopyrightIcon
    $wgCopyrightIcon was introduced in version 1.4.0 and   deprecated
    in version 1.17.0 and  $wgFooterIcons['copyright']['copyright'] is used instead
    
    Bug: 60459
    Change-Id: I578501e0e756d46e8339336bcc86d8030d069e7d

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5829,12 +5829,6 @@ $wgRightsIcon = null;
 $wgLicenseTerms = false;
 
 /**
- * Set this to some HTML to override the rights icon with an arbitrary logo
- * @deprecated since 1.18 Use $wgFooterIcons['copyright']['copyright']
- */
-$wgCopyrightIcon = null;
-
-/**
  * Set this to true if you want detailed copyright information forms on Upload.
  */
 $wgUseCopyrightUpload = false;
