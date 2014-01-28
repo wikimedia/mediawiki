9f9851eb2780c3192a5c4f7cad4a0180e9f21289
Ori Livneh <ori@wikimedia.org>
2014-01-27 23:37:13 -0800

    Make abstract Config class truly implementation-agnostic
    Some corrections to I13baec0b6 ("Config: Add Config and GlobalConfig classes"):
    
    * Remove $prefix args from Config::set and ::get. The idea of having an
      abstract Config class is to abstract some notion of configuration data from
      the particular way in which it is currently implemented (global variables).
      So the abstract base class has no business dealing with variable name
      prefixes.
    ** Instead GlobalVarConfig now has getWithPrefix() and setWithPrefix()
       convenience functions to get things that don't use 'wg'
    * Rename GlobalConfig to GlobalVarConfig, which makes it clearer that it isn't
      referring to the scope of the configuration value, but to the scope of the
      variable name which provides it.
    * The fact that the unit test didn't care to check the Status object returned
      by the setter is a good indication that the design needs to be rethought.
      What's wrong with true / false? Do we really suppose that Config::set callers
      need to have rich information about why attempting to set a configuration
      variable failed?
    
    Change-Id: I5a5857fcfa07598ba4ce9ae5bbb4ce54a567d31e

--- tests/phpunit/includes/config/GlobalVarConfigTest.php
+++ tests/phpunit/includes/config/GlobalVarConfigTest.php
@@ -1,13 +1,13 @@
 <?php
 
-class GlobalConfigTest extends MediaWikiTestCase {
+class GlobalVarConfigTest extends MediaWikiTestCase {
 
-	/** @var GlobalConfig $config */
+	/** @var GlobalVarConfig $config */
 	protected $config;
 
 	protected function setUp() {
 		parent::setUp();
-		$this->config = new GlobalConfig;
+		$this->config = new GlobalVarConfig;
 	}
 
 	public static function provideGet() {
@@ -23,13 +23,18 @@ class GlobalConfigTest extends MediaWikiTestCase {
 	 * @param string $name
 	 * @param array $params
 	 * @dataProvider provideGet
-	 * @covers GlobalConfig::get
+	 * @covers GlobalVarConfig::get
 	 */
 	public function testGet( $name, $params ) {
 		$rand = wfRandom();
 		$old = isset( $GLOBALS[$name] ) ? $GLOBALS[$name] : null;
 		$GLOBALS[$name] = $rand;
-		$out = call_user_func_array( array( $this->config, 'get' ), $params );
+		if ( isset( $params[1] ) ) {
+			$out = $this->config->getWithPrefix( $params[0], $params[1] );
+		} else {
+			$out = $this->config->get( $params[0] );
+		}
+
 		$this->assertEquals( $rand, $out );
 		if ( $old ) {
 			$GLOBALS[$name] = $old;
