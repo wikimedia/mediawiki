10ba3cfd5608f48522e01d3ebe2819db2ff2509b
umherirrender <umherirrender_de.wp@web.de>
2014-01-31 20:49:34 +0100

    Give alreadyrolled error when rollback will be a null edit
    When a rollback will not create a new version, the rollback should not
    be success normally. Raise the alreadyrolled error to indicate this
    situation.
    
    This can happen, when the same user already rolled back his edit in the
    meantime.
    
    Change-Id: I953ae1ca79abb37bc44f3f8244a42a34111e6ab8

--- includes/WikiPage.php
+++ includes/WikiPage.php
@@ -2969,6 +2969,16 @@ class WikiPage implements Page, IDBAccessObject {
 			return $status->getErrorsArray();
 		}
 
+		// raise error, when the edit is a edit without a new version
+		if ( empty( $status->value['revision'] ) ) {
+			$resultDetails = array( 'current' => $current );
+			return array( array( 'alreadyrolled',
+					htmlspecialchars( $this->mTitle->getPrefixedText() ),
+					htmlspecialchars( $fromP ),
+					htmlspecialchars( $current->getUserText() )
+			) );
+		}
+
 		$set = array();
 		if ( $bot && $guser->isAllowed( 'markbotedits' ) ) {
 			// Mark all reverted edits as bot
@@ -2990,11 +3000,7 @@ class WikiPage implements Page, IDBAccessObject {
 			);
 		}
 
-		if ( !empty( $status->value['revision'] ) ) {
-			$revId = $status->value['revision']->getId();
-		} else {
-			$revId = false;
-		}
+		$revId = $status->value['revision']->getId();
 
 		wfRunHooks( 'ArticleRollbackComplete', array( $this, $guser, $target, $current ) );
 
