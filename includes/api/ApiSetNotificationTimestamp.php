1e7ea938da0cdaf12de73a39441073ec0e9db1af
Brad Jorsch <bjorsch@wikimedia.org>
2014-02-06 20:52:58 -0500

    API: Refactor continuation code and add to purge, watch, etc.
    While generator support was added to various actions, the continuation
    parameters from the generators weren't being returned to the client.
    
    This patch refactors the code for handling continuation out of ApiQuery
    and into ApiResult, and then uses it in the other actions that need it.
    
    Bug: 60734
    Change-Id: I1a6e51cdc964ddcdd69aaa2f735223371233e8af

--- includes/api/ApiSetNotificationTimestamp.php
+++ includes/api/ApiSetNotificationTimestamp.php
@@ -46,6 +46,8 @@ class ApiSetNotificationTimestamp extends ApiBase {
 		$params = $this->extractRequestParams();
 		$this->requireMaxOneParameter( $params, 'timestamp', 'torevid', 'newerthanrevid' );
 
+		$this->getResult()->beginContinuation( $params['continue'], array(), array() );
+
 		$pageSet = $this->getPageSet();
 		if ( $params['entirewatchlist'] && $pageSet->getDataSource() !== null ) {
 			$this->dieUsage(
@@ -175,6 +177,8 @@ class ApiSetNotificationTimestamp extends ApiBase {
 			$apiResult->setIndexedTagName( $result, 'page' );
 		}
 		$apiResult->addValue( null, $this->getModuleName(), $result );
+
+		$apiResult->endContinuation();
 	}
 
 	/**
@@ -220,6 +224,7 @@ class ApiSetNotificationTimestamp extends ApiBase {
 			'newerthanrevid' => array(
 				ApiBase::PARAM_TYPE => 'integer'
 			),
+			'continue' => '',
 		);
 		if ( $flags ) {
 			$result += $this->getPageSet()->getFinalParams( $flags );
@@ -235,6 +240,7 @@ class ApiSetNotificationTimestamp extends ApiBase {
 			'torevid' => 'Revision to set the notification timestamp to (one page only)',
 			'newerthanrevid' => 'Revision to set the notification timestamp newer than (one page only)',
 			'token' => 'A token previously acquired via prop=info',
+			'continue' => 'When more results are available, use this to continue',
 		);
 	}
 
