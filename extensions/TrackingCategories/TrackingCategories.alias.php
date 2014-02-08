e5dd523343682bc0366e4d8f705c2d880f1bd545
unknown <kunalgrover05@gmail.com>
2014-02-09 04:59:53 +0530

    Implement listing for tracking categories
    Fixed Code Formatting-Extension for implementing tracking categories list on a Special page. Needs to be enabled in LocalSettings.php. Global added  containing list of tracking categories and pages
    Bug:60333
    
    Change-Id: I7d4bb90622a6bae60845942ef93cfe64f229d2d2

---
+++ extensions/TrackingCategories/TrackingCategories.alias.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Aliases for TrackingCategories
+ */
+
+$specialPageAliases = array();
+
+/** English
+ * @author <Kunal Grover>
+ */
+$specialPageAliases[ 'en' ] = array(
+	'TrackingCategories' => array( 'TrackingCategories', 'Tracking Categories' ),
+);
