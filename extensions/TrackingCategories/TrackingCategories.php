9fde3e6194faa3c88f5242f5e23431996709598b
unknown <kunalgrover05@gmail.com>
2014-02-09 04:59:53 +0530

    Implement listing for tracking categories
    Extension for implementing tracking categories list on a Special page. Needs to be enabled in LocalSettings.php. Global added  containing list of tracking categories and pages
    Bug:60333
    
    Change-Id: I7d4bb90622a6bae60845942ef93cfe64f229d2d2

---
+++ extensions/TrackingCategories/TrackingCategories.php
@@ -0,0 +1,22 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo <<<EOT
+To install this extension, put the following line in LocalSettings.php:
+require_once( "\$IP/extensions/TrackingCategories/TrackingCategories.php" );
+EOT;
+	exit( 1 );
+}
+ 
+$wgExtensionCredits[ 'specialpage' ][] = array(
+	'path' => __FILE__,
+	'name' => 'Tracking Categories',
+	'author' => 'Kunal Grover',
+	'descriptionmsg' => 'trackingcategories-desc',
+	'version' => '1.0.0',
+);
+
+$wgSpecialPageGroups[ 'TrackingCategories' ] = 'pages';
+$wgAutoloadClasses[ 'SpecialTrackingCategories' ] = __DIR__ . '/SpecialTrackingCategories.php';
+$wgExtensionMessagesFiles[ 'TrackingCategories' ] = __DIR__ . '/TrackingCategories.i18n.php';
+$wgExtensionMessagesFiles[ 'TrackingCategoriesAlias' ] = __DIR__ . '/TrackingCategories.alias.php';
+$wgSpecialPages[ 'TrackingCategories' ] = 'SpecialTrackingCategories';
