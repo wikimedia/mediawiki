c37a11920b1f98ab38b2ecd84b6bd3002e6c70d3
umherirrender <umherirrender_de.wp@web.de>
2014-02-08 14:55:22 +0100

    Use TablePager on Special:ProtectedPages (using log_search)
    This is a alternative patch for
    Ie40057398a7829c698df3d68813006798a72dccb
    and Iade82653999ca770586762b3c1383f73dfe3a5ee to avoid a schema change
    on table page_restriction.
    Idea by Aaron Schulz.
    
    Change-Id: Ia0dc5895ec672896fe4b75c2e0f76da2474bd6e4

--- languages/messages/MessagesQqq.php
+++ languages/messages/MessagesQqq.php
@@ -4952,6 +4952,17 @@ See the following search results:
 'protectedpages-noredirect' => 'Option in [[Special:ProtectedPages]].
 {{Identical|Hide redirect}}',
 'protectedpagesempty' => 'Used in [[Special:ProtectedPages]], when there are no protected pages with the specified parameters.',
+'protectedpages-timestamp' => 'This is a column header for dates and times in the table on the page [[Special:ProtectedPages]].
+{{Identical|Timestamp}}',
+'protectedpages-page' => 'This is a column header in the table on the page [[Special:ProtectedPages]].',
+'protectedpages-expiry' => 'This is a column header in the table on the page [[Special:ProtectedPages]].
+{{Identical|Expire}}',
+'protectedpages-performer' => 'This is a column header in the table on the page [[Special:ProtectedPages]].',
+'protectedpages-params' => 'This is a column header in the table on the page [[Special:ProtectedPages]].',
+'protectedpages-reason' => 'This is a column header in the table on the page [[Special:ProtectedPages]].
+{{Identical|Reason}}',
+'protectedpages-unknown-timestamp' => 'This is shown, when the date and time is unknown for a protection on the page [[Special:ProtectedPages]].',
+'protectedpages-unknown-performer' => 'This is shown, when the protecting user is unknown for a protection on the page [[Special:ProtectedPages]].',
 'protectedtitles' => '{{doc-special|ProtectedTitles}}',
 'protectedtitlesempty' => 'Used on [[Special:ProtectedTitles]]. This text appears if the list of protected titles is empty. See the [[mw:Project:Protected_titles|help page on MediaWiki]] for more information.',
 'listusers' => '{{doc-special|ListUsers}}',
