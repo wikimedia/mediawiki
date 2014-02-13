f86cd858a68830e3e328a445a56506c5cb699818
umherirrender <umherirrender_de.wp@web.de>
2014-02-13 21:22:32 +0100

    Use TablePager on Special:ProtectedTitles
    Also add the timestamp, the reason and the protecting user to the
    output, the values are already in the database.
    
    Change-Id: I06f66f44b623dbebf599bd1ea5416820c448d825

--- languages/messages/MessagesQqq.php
+++ languages/messages/MessagesQqq.php
@@ -5032,6 +5032,15 @@ This refers to expiry timestamp.
 'protectedtitles' => '{{doc-special|ProtectedTitles}}',
 'protectedtitles-summary' => 'Summary of [[Special:ProtectedTitles]].',
 'protectedtitlesempty' => 'Used on [[Special:ProtectedTitles]]. This text appears if the list of protected titles is empty. See the [[mw:Project:Protected_titles|help page on MediaWiki]] for more information.',
+'protectedtitles-timestamp' => 'This is a column header for dates and times in the table on the page [[Special:ProtectedTitles]].
+{{Identical|Timestamp}}',
+'protectedtitles-page' => 'This is a column header in the table on the page [[Special:ProtectedTitles]].',
+'protectedtitles-expiry' => 'This is a column header in the table on the page [[Special:ProtectedTitles]].
+{{Identical|Expire}}',
+'protectedtitles-user' => 'This is a column header in the table on the page [[Special:ProtectedTitles]].',
+'protectedtitles-params' => 'This is a column header in the table on the page [[Special:ProtectedTitles]].',
+'protectedtitles-reason' => 'This is a column header in the table on the page [[Special:ProtectedTitles]].
+{{Identical|Reason}}',
 'listusers' => '{{doc-special|ListUsers}}',
 'listusers-editsonly' => 'Option in [[Special:ListUsers]].',
 'listusers-creationsort' => 'Option in [[Special:ListUsers]].',
