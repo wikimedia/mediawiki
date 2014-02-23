cc49682f843614456c7a9dac710e144fd28c874c
Mayank Jha <mayank25080562@gmail.com>
2014-02-23 15:43:20 +0530

    Automatically copy the LocalSettings.php file to the desired location
    Bug: 60534
    Change-Id: Ibba052ae7c89fb2266b90dce84894e7d3d4c0463

--- includes/installer/WebInstallerPage.php
+++ includes/installer/WebInstallerPage.php
@@ -1287,30 +1287,43 @@ class WebInstaller_Complete extends WebInstallerPage {
 	public function execute() {
 		// Pop up a dialog box, to make it difficult for the user to forget
 		// to download the file
-		$lsUrl = $this->getVar( 'wgServer' ) . $this->parent->getURL( array( 'localsettings' => 1 ) );
-		if ( isset( $_SERVER['HTTP_USER_AGENT'] ) &&
-			strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false
-		) {
-			// JS appears to be the only method that works consistently with IE7+
-			$this->addHtml( "\n<script>jQuery( function () { document.location = " .
-				Xml::encodeJsVar( $lsUrl ) . "; } );</script>\n" );
-		} else {
-			$this->parent->request->response()->header( "Refresh: 0;url=$lsUrl" );
+		if(!file_exists("LocalSettings.php")) {
+		    $lsUrl = $this->getVar( 'wgServer' ) . $this->parent->getURL( array( 'localsettings' => 1 ) );
+		    if ( isset( $_SERVER['HTTP_USER_AGENT'] ) &&
+			    strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false
+		    ) {
+			    // JS appears to be the only method that works consistently with IE7+
+			    $this->addHtml( "\n<script>jQuery( function () { document.location = " .
+				    Xml::encodeJsVar( $lsUrl ) . "; } );</script>\n" );
+		    } else {
+			    $this->parent->request->response()->header( "Refresh: 0;url=$lsUrl" );
+		    }
 		}
-
 		$this->startForm();
 		$this->parent->disableLinkPopups();
-		$this->addHTML(
-			$this->parent->getInfoBox(
-				wfMessage( 'config-install-done',
-					$lsUrl,
-					$this->getVar( 'wgServer' ) .
-					$this->getVar( 'wgScriptPath' ) . '/index' .
-					$this->getVar( 'wgScriptExtension' ),
-					'<downloadlink/>'
-				)->plain(), 'tick-32.png'
-			)
-		);
+		$wikilink = $this->getVar( 'wgServer' ) . $this->getVar( 'wgScriptPath' ) 
+			    . '/index' . $this->getVar( 'wgScriptExtension' );
+		if(!file_exists("LocalSettings.php")) {   
+		    $this->addHTML(
+			    $this->parent->getInfoBox(
+				    wfMessage( 'config-install-done',
+					    $lsUrl,
+					    $wikilink,
+					    '<downloadlink/>'
+				    )->plain(), 'tick-32.png'
+			    )
+		    );
+		}
+		else {
+		    $this->addHTML(
+			    $this->parent->getInfoBox(
+				    wfMessage( 'config-installcopy-done',
+					    $wikilink
+				    )->plain(), 'tick-32.png'
+			    )
+		    );
+		}
+		
 		$this->addHTML( $this->parent->getInfoBox(
 			wfMessage( 'config-extension-link' )->text() ) );
 
