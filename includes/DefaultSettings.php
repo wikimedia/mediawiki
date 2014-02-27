b37342c1d808d703a18183edb7ad11f8eb5cc6fc
mglaser <glaser@hallowelt.biz>
2014-02-27 22:37:42 +0100

    Updated Release Notes and version number for 1.22.3
    This is the MW1.22.3 maintenance release
    
    Change-Id: I9f400338f7f450b097a13fd9932e2b2ec17b5ffb

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.22.2';
+$wgVersion = '1.22.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
