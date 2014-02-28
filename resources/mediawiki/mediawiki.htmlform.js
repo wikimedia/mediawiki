578ac3fccdfd55cbc914b7d31c1b9a68e02f8b34
Brad Jorsch <bjorsch@wikimedia.org>
2014-02-28 16:00:43 -0500

    HTMLForm: Add date type
    Since most browsers don't actually support a date picker for
    <input type=date>, attempt to feature-detect it and use
    jquery.ui.datepicker if necessary.
    
    Change-Id: Ifb856bb1f37947f3b6c5a2d37769347b60ff2053

--- resources/mediawiki/mediawiki.htmlform.js
+++ resources/mediawiki/mediawiki.htmlform.js
@@ -180,6 +180,40 @@
 		}
 	} );
 
+	( function () {
+		var inputs, i;
+
+		inputs = $( 'input[type=date]' );
+		if ( inputs.length === 0 ) {
+			return;
+		}
+
+		// Assume that if the browser implements validation for <input type=date>
+		// (so it rejects "bogus" as a value) then it supports a date picker too.
+		i = document.createElement( 'input' );
+		i.setAttribute( 'type', 'date' );
+		i.value = 'bogus';
+		if ( i.value === 'bogus' ) {
+			mw.loader.using( 'jquery.ui.datepicker', function () {
+				inputs.each( function () {
+					var i = $( this );
+					// Reset the type, Just In Case
+					i.prop( 'type', 'text' );
+					i.datepicker( {
+						dateFormat: 'yy-mm-dd',
+						constrainInput: true,
+						showOn: 'focus',
+						changeMonth: true,
+						changeYear: true,
+						showButtonPanel: true,
+						minDate: i.data( 'min' ),
+						maxDate: i.data( 'max' ),
+					} );
+				} );
+			} );
+		}
+	}() );
+
 	/**
 	 * @class jQuery
 	 * @mixins jQuery.plugin.htmlform
