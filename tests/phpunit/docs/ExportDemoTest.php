7c78f7624f035e4e8307e8e282367f046abf3524
addshore <addshorewiki@gmail.com>
2014-03-05 14:08:56 +0100

    Clean up ExportDemoTest
    Change-Id: I28574b6c83cbd1128ad97b4868cc365336a7d4b8

--- tests/phpunit/docs/ExportDemoTest.php
+++ tests/phpunit/docs/ExportDemoTest.php
@@ -1,39 +1,31 @@
 <?php
+
 /**
- * Test for the demo xml
+ * Test making sure the demo export xml is valid.
+ * This is NOT a unit test
  *
  * @group Dump
+ * @group large
  */
 class ExportDemoTest extends DumpTestCase {
 
-	/**
-	 * @group large
-	 */
-	function testExportDemo() {
-		$this->validateXmlFileAgainstXsd( "../../docs/export-demo.xml" );
-	}
-
-	/**
-	 * Validates a xml file against the xsd.
-	 *
-	 * The validation is slow, because php has to read the xsd on each call.
-	 *
-	 * @param $fname string: name of file to validate
-	 */
-	protected function validateXmlFileAgainstXsd( $fname ) {
+	public function testExportDemo() {
+		$fname = "../../docs/export-demo.xml";
 		$version = WikiExporter::schemaVersion();
-
 		$dom = new DomDocument();
 		$dom->load( $fname );
 
 		// Ensure, the demo is for the current version
-		$this->assertEquals( $dom->documentElement->getAttribute( 'version' ), $version, 'export-demo.xml should have the current version' );
+		$this->assertEquals(
+			$dom->documentElement->getAttribute( 'version' ),
+			$version,
+			'export-demo.xml should have the current version'
+		);
 
-		try {
-			$this->assertTrue( $dom->schemaValidate( "../../docs/export-" . $version . ".xsd" ),
-				"schemaValidate has found an error" );
-		} catch ( Exception $e ) {
-			$this->fail( "xml not valid against xsd: " . $e->getMessage() );
-		}
+		$this->assertTrue(
+			$dom->schemaValidate( "../../docs/export-" . $version . ".xsd" ),
+			"schemaValidate has found an error"
+		);
 	}
+
 }
