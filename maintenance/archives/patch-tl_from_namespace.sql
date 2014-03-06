8324bc9a03d7813ea7a7ba0d0fec7fb9c50359bb
Aaron Schulz <aschulz@wikimedia.org>
2014-03-06 15:01:35 -0800

    Redid WhatLinksHere query and added a _from_namespace field
    * Also tweaked the query so MySQL avoids doing a page_name
      index scan when it should start with the link table index
    * Added population script (triggered by update.php)
    * Also removed uniqueness from some indexes where it is redundant
    
    bug: 60618
    Change-Id: Icca99b6ae0ef76cb77695faf82c615516191da36

---
+++ maintenance/archives/patch-tl_from_namespace.sql
@@ -0,0 +1,4 @@
+ALTER TABLE /*_*/templatelinks
+	ADD COLUMN tl_from_namespace int NOT NULL default 0;
+
+CREATE INDEX /*i*/tl_backlinks_namespace ON /*_*/templatelinks (tl_namespace,tl_title,tl_from_namespace,tl_from);
