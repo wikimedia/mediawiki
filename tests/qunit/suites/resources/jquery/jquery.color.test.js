53448477538fe9c1ed8d83aa8094101a5db8e9f9
Timo Tijhof <krinklemail@gmail.com>
2014-03-06 03:35:18 +0100

    jquery.color.test: Use fake timers
    Also simplified test by asserting the rgb array directly instead
    of each of index separately.
    
    Change-Id: I1ffdd6d5dad3feeb2c6e3dedbb9fc6dbabe8e7c5

--- tests/qunit/suites/resources/jquery/jquery.color.test.js
+++ tests/qunit/suites/resources/jquery/jquery.color.test.js
@@ -1,15 +1,18 @@
 ( function ( $ ) {
-	QUnit.module( 'jquery.color', QUnit.newMwEnvironment() );
+	QUnit.module( 'jquery.color', QUnit.newMwEnvironment( {
+		setup: function () {
+			this.clock = this.sandbox.useFakeTimers();
+		}
+	} ) );
 
-	QUnit.asyncTest( 'animate', 3, function ( assert ) {
+	QUnit.test( 'animate', 1, function ( assert ) {
 		var $canvas = $( '<div>' ).css( 'background-color', '#fff' );
 
-		$canvas.animate( { backgroundColor: '#000' }, 4 ).promise().then( function () {
+		$canvas.animate( { backgroundColor: '#000' }, 10 ).promise().then( function () {
 			var endColors = $.colorUtil.getRGB( $canvas.css( 'background-color' ) );
-			assert.strictEqual( endColors[0], 0 );
-			assert.strictEqual( endColors[1], 0 );
-			assert.strictEqual( endColors[2], 0 );
-			QUnit.start();
+			assert.deepEqual( endColors, [0, 0, 0], 'end state' );
 		} );
+
+		this.clock.tick( 20 );
 	} );
 }( jQuery ) );
