wikimedia/mediawiki

commit 92e284d3fa62f45e20fed34c4359c575481d583c

Fix undefined variable in Status::getMessage()
If $shortContext and $longContext are false, and there
are errors, then $s is undefined.

Change-Id: Ie07f80b43a48a6fc4ed28b2c519f51fd32690bc8

includes/Status.php
@@ -238,8 +238,14 @@ class Status {
 			}
 		} else {
 			$msgs =  $this->getErrorMessageArray( $this->errors );
-			$wrapper = new RawMessage( '* $' . implode( "\n* \$", range( 1, count( $msgs ) + 1 ) ) );
-			$wrapper->params( $msgs )->parse();
+			$msgCount = count( $msgs );
+
+			if ( $shortContext ) {
+				$msgCount++;
+			}
+
+			$wrapper = new RawMessage( '* $' . implode( "\n* \$", range( 1, $msgCount ) ) );
+			$s = $wrapper->params( $msgs )->parse();
 
 			if ( $longContext ) {
 				$s = wfMessage( $longContext, $wrapper );
@@ -249,6 +255,7 @@ class Status {
 				$s = wfMessage( $shortContext, $wrapper );
 			}
 		}
+
 		return $s;
 	}
 

commit f9e728c9e70b9744e027a59a11b7ad10258b4442

Fix Status::getMessage accidentially returning string instead of Message
This mistake was introduced in commit
92e284d3fa62f45e20fed34c4359c575481d583c and the reason for the
disabled test.

The method should return Message objects only but did return a
string in that special case (multiple warnings set but no context
message key).

Unfortunatelly Status::getMessage does have many, many more
problems but I understand it's not a good idea to address them all
in a single confusing patch.

Change-Id: I0dc37e248f407019d5921aaaca3eabba338b0fd3

includes/Status.php
@@ -224,9 +224,11 @@ class Status {
 	/**
 	 * Get the error list as a Message object
 	 *
-	 * @param string $shortContext a short enclosing context message name, to
-	 *        be used when there is a single error
-	 * @param string $longContext a long enclosing context message name, for a list
+	 * @param string|string[] $shortContext A short enclosing context message name (or an array of
+	 * message names), to be used when there is a single error.
+	 * @param string|string[] $longContext A long enclosing context message name (or an array of
+	 * message names), for a list.
+	 *
 	 * @return Message
 	 */
 	public function getMessage( $shortContext = false, $longContext = false ) {
@@ -256,13 +258,13 @@ class Status {
 				$msgCount++;
 			}
 
-			$wrapper = new RawMessage( '* $' . implode( "\n* \$", range( 1, $msgCount ) ) );
-			$s = $wrapper->params( $msgs )->parse();
+			$s = new RawMessage( '* $' . implode( "\n* \$", range( 1, $msgCount ) ) );
+			$s->params( $msgs )->parse();
 
 			if ( $longContext ) {
-				$s = wfMessage( $longContext, $wrapper );
+				$s = wfMessage( $longContext, $s );
 			} elseif ( $shortContext ) {
-				$wrapper = new RawMessage( "\n\$1\n", $wrapper );
+				$wrapper = new RawMessage( "\n\$1\n", $s );
 				$wrapper->parse();
 				$s = wfMessage( $shortContext, $wrapper );
 			}

tests/phpunit/includes/StatusTest.php
@@ -376,8 +376,8 @@ class StatusTest extends MediaWikiLangTestCase {
 	public function testGetMessage( Status $status, $expectedParams = array(), $expectedKey ) {
 		$message = $status->getMessage();
 		$this->assertInstanceOf( 'Message', $message );
-		$this->assertEquals( $expectedParams, $message->getParams() );
-		$this->assertEquals( $expectedKey, $message->getKey() );
+		$this->assertEquals( $expectedParams, $message->getParams(), 'Params' );
+		$this->assertEquals( $expectedKey, $message->getKey(), 'Key' );
 	}
 
 	/**
@@ -407,17 +407,17 @@ class StatusTest extends MediaWikiLangTestCase {
 		$testCases[ '1StringWarning' ] = array(
 			$status,
 			array(),
-			"fooBar!"
+			'fooBar!'
 		);
 
-		//NOTE: this seems to return a string instead of a Message object...
+		// FIXME: Assertion tries to compare a StubUserLang with a Language object.
 //		$status = new Status();
 //		$status->warning( 'fooBar!' );
 //		$status->warning( 'fooBar2!' );
 //		$testCases[ '2StringWarnings' ] = array(
 //			$status,
-//			array(),
-//			''
+//			array( new Message( 'fooBar!' ), new Message( 'fooBar2!' ) ),
+//			"* \$1\n* \$2"
 //		);
 
 		$status = new Status();
@@ -425,18 +425,17 @@ class StatusTest extends MediaWikiLangTestCase {
 		$testCases[ '1MessageWarning' ] = array(
 			$status,
 			array( 'foo', 'bar' ),
-			"fooBar!",
+			'fooBar!'
 		);
 
-		//NOTE: this seems to return a string instead of a Message object...
-//		$status = new Status();
-//		$status->warning( new Message( 'fooBar!', array( 'foo', 'bar' ) ) );
-//		$status->warning( new Message( 'fooBar2!' ) );
-//		$testCases[ '2MessageWarnings' ] = array(
-//			$status,
-//			array(),
-//			"",
-//		);
+		$status = new Status();
+		$status->warning( new Message( 'fooBar!', array( 'foo', 'bar' ) ) );
+		$status->warning( new Message( 'fooBar2!' ) );
+		$testCases[ '2MessageWarnings' ] = array(
+			$status,
+			array( new Message( 'fooBar!', array( 'foo', 'bar' ) ), new Message( 'fooBar2!' ) ),
+			"* \$1\n* \$2"
+		);
 
 		return $testCases;
 	}