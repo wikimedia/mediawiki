188d682c4755562820a8c0ac4fa3f6fc5a4a3547
Bartosz Dziewoński <matma.rex@gmail.com>
2014-03-07 18:39:51 +0100

    Check validity and availability of usernames during signup via AJAX
    This is done in addition to the server-side checks. Users who have
    disabled JavaScript will not notice any difference.
    
    The way this is done (via action=query&list=users API) means that we
    can't check for all possible error conditions, both for the username
    (e.g. these enforced by extensions like AntiSpoof) and other parts of
    the form (e-mail, password…) – these are still handled server-side
    only. Thus we intentionally never say that whatever the user typed in
    is valid – we only warn when we know it's not.
    
    Doing this "properly" would require some reworking of the internals of
    the signup process and this way is already a huge improvement.
    
    (Split off from the reverted a0c72523.)
    
    Bug: 34447
    Change-Id: I42c00b54651fd4e7217a862b3116c6113935f5ae

--- resources/mediawiki.special/mediawiki.special.userlogin.signup.js
+++ resources/mediawiki.special/mediawiki.special.userlogin.signup.js
@@ -3,7 +3,7 @@
  */
 ( function ( mw, $ ) {
 	// When sending password by email, hide the password input fields.
-	function hidePasswordOnEmail() {
+	$( function () {
 		// Always required if checked, otherwise it depends, so we use the original
 		var $emailLabel = $( 'label[for="wpEmail"]' ),
 			originalText = $emailLabel.text(),
@@ -28,7 +28,97 @@
 
 		$createByMailCheckbox.on( 'change', updateForCheckbox );
 		updateForCheckbox();
-	}
+	} );
 
-	$( hidePasswordOnEmail );
+	// Check if the username is invalid or already taken
+	$( function () {
+		var
+			// We need to hook to all of these events to be sure we are notified of all changes to the
+			// value of an <input type=text> field.
+			events = 'keyup keydown change mouseup cut paste focus blur',
+			$input = $( '#wpName2' ),
+			$statusContainer = $( '#mw-createacct-status-area' ),
+			api = new mw.Api(),
+			currentRequest;
+
+		// Hide any present status messages.
+		function clearStatus() {
+			$statusContainer.slideUp( function () {
+				$statusContainer
+					.removeAttr( 'class' )
+					.empty();
+			} );
+		}
+
+		function updateUsernameStatus() {
+			var
+				username = $.trim( $input.val() ),
+				currentRequestInternal;
+
+			// Abort any pending requests.
+			if ( currentRequest ) {
+				currentRequest.abort();
+			}
+
+			if ( username === '' ) {
+				clearStatus();
+				return;
+			}
+
+			currentRequest = currentRequestInternal = api.get( {
+				action: 'query',
+				list: 'users',
+				ususers: username // '|' in usernames is handled below
+			} ).done( function ( resp ) {
+				var userinfo, state, message;
+
+				// Another request was fired in the meantime, the result we got here is no longer current.
+				// This shouldn't happen as we abort pending requests, but you never know.
+				if ( currentRequest !== currentRequestInternal ) {
+					return;
+				}
+				// If we're here, then the current request has finished, avoid calling .abort() needlessly.
+				currentRequest = undefined;
+
+				userinfo = resp.query.users[0];
+
+				if ( resp.query.users.length !== 1 ) {
+					// Happens if the user types '|' into the field
+					state = 'invalid';
+				} else if ( userinfo.invalid !== undefined ) {
+					state = 'invalid';
+				} else if ( userinfo.userid !== undefined ) {
+					state = 'taken';
+				} else {
+					state = 'ok';
+				}
+
+				if ( state === 'ok' ) {
+					clearStatus();
+				} else {
+					if ( state === 'invalid' ) {
+						message = mw.message( 'noname' ).text();
+					} else if ( state === 'taken' ) {
+						message = mw.message( 'userexists' ).text();
+					}
+
+					$statusContainer
+						.attr( 'class', 'errorbox' )
+						.empty()
+						.append(
+							// Ugh…
+							// @todo Change the HTML structure in includes/templates/Usercreate.php
+							$( '<strong>' ).text( mw.message( 'createacct-error' ).text() ),
+							$( '<br>' ),
+							document.createTextNode( message )
+						)
+						.slideDown();
+				}
+			} ).fail( function () {
+				clearStatus();
+			} );
+		}
+
+		$input.on( events, $.debounce( 250, updateUsernameStatus ) );
+	} );
 }( mediaWiki, jQuery ) );
