78b5d902f3d391de81c7b47f7746d21e56bc1bc0
Aditya Sastry <ganeshaditya1@gmail.com>
2014-03-10 18:44:07 +0530

    Display a warning when the user name looked up is not registered.
    Changed the SpecialContributions.php and added a warning message to tell the
    user that the name he looked up isn't registered. Also added the corresponding
    warning to the MessagesEn.php, MessagesQqq.php and to the Messages.inc.
    
    Bug: 60524
    Change-Id: Ie07001e75a9732f727930f8f367bba8feb395b3e

--- languages/messages/MessagesQqq.php
+++ languages/messages/MessagesQqq.php
@@ -6281,6 +6281,10 @@ See examples: [[Special:Contributions/x]] and [[Special:DeletedContributions/x]]
 
 Parameters:
 * $1 - (Unused) the user name',
+'userdoesnotexist' => 'This message is used in [[Special:Contributions]]. It is used to tell the user that the name he searched for doesn\'t exists.
+
+Parameters:
+* $1 is the name that was searched for',
 'uctop' => 'This message is used in [[Special:Contributions]]. It is used to show that a particular edit was the last made to a page. Example: 09:57, 11 February 2008 (hist) (diff) Pagename‎ (edit summary) (current)
 {{Identical|Top}}',
 'month' => 'Used in [[Special:Contributions]] and history pages ([{{fullurl:Sandbox|action=history}} example]), as label for a dropdown box to select a specific month to view the edits made in that month, and the earlier months. See also {{msg-mw|year}}.',
