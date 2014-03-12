787870564454905681c26c7b56be63fd31a7318b
Fomafix <fomafix@googlemail.com>
2014-03-12 09:59:12 +0000

    wpSummaryLabel and wpSummary in one line with full width
    Bug: 62316
    Change-Id: Ifdd8a603ba256f2e612276148f5e79509241695b

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2583,6 +2583,7 @@ class EditPage {
 		}
 
 		$input = Html::input( 'wpSummary', $summary, 'text', $inputAttrs );
+		$input = Html::rawElement( 'div', array( 'id' => 'wpSummaryDiv' ), $input );
 
 		return array( $label, $input );
 	}
