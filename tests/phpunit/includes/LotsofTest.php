9eb3b70caed44fbb5aa48ff63b401980f4c4ed91
csteipp <csteipp@wikimedia.org>
2014-03-13 10:37:24 -0700

    ignore this (more segfault testing)
    DO NOT MERGE
    
    Testing phpunit segfaults
    
    Change-Id: Ie4be5c4270527b83faef6e3505d809c85db5743b

---
+++ tests/phpunit/includes/LotsofTest.php
@@ -0,0 +1,427 @@
+<?php
+/**
+ * Tests to DoS phpunit
+ *
+ * @file
+ */
+/**
+ * @group large
+ */
+class LotsofTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testDoNothing000() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing001() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing002() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing003() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing004() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing005() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing006() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing007() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing008() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing009() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing010() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing011() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing012() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing013() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing014() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing015() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing016() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing017() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing018() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing019() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing020() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing021() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing022() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing023() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing024() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing025() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing026() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing027() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing028() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing029() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing030() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing031() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing032() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing033() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing034() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing035() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing036() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing037() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing038() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing039() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing040() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing041() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing042() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing043() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing044() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing045() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing046() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing047() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing048() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing049() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing050() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing051() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing052() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing053() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing054() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing055() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing056() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing057() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing058() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing059() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing060() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing061() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing062() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing063() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing064() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing065() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing066() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing067() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing068() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing069() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing070() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing071() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing072() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing073() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing074() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing075() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing076() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing077() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing078() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing079() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing080() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing081() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing082() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing083() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing084() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing085() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing086() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing087() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing088() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing089() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing090() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing091() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing092() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing093() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing094() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing095() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing096() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing097() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing098() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing099() {
+		$this->assertFalse( false );
+	}
+
+	public function testDoNothing100() {
+		$this->assertFalse( false );
+	}
+
+
+}
+
+
