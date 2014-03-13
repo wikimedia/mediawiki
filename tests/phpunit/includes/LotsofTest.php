d6c01f0f7a7520e7131d18fd32ffaa7bcce21b94
csteipp <csteipp@wikimedia.org>
2014-03-13 10:37:24 -0700

    ignore this (more segfault testing)
    DO NOT MERGE
    
    Testing phpunit segfaults
    
    Change-Id: Ie4be5c4270527b83faef6e3505d809c85db5743b

---
+++ tests/phpunit/includes/LotsofTest.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * Tests to DoS phpunit
+ *
+ * @file
+ */
+/**
+ * @group large
+ */
+class LotsofTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testDoNothing000() {
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+		$this->assertFalse( false );
+	}
+
+}
+
+
