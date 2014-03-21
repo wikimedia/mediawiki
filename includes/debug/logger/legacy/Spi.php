5ffdd395f28435052b68e1d01548538eeb02576b
Bryan Davis <bd808@wikimedia.org>
2014-03-20 23:01:24 -0600

    Use MWLogger logging for legacy logging methods
    Send wfDebug, wfDebugLog, wfLogDBError and wfLogDBError log messages to
    the new MWLogger PSR-3 logger subsystem. Compatibility with the historic
    logging operations of wfLogDBError are provided by MWLoggerLegacyLogger
    and the MWLoggerLegacySpi logger factory.
    
    Requires the MWLogger system introduced in I5c82299 and the Composer
    managed libraries from Ie667944.
    
    Change-Id: I1e5596d590144fbfdfd5f18bc42cf1ef0dbcac12

---
+++ includes/debug/logger/legacy/Spi.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * MWLogger service provider that creates MWLegacyLogger instances.
+ *
+ * @see MWLogger
+ * @since 1.24
+ * @author Bryan Davis <bd808@wikimedia.org>
+ * @copyright © 2014 Bryan Davis and Wikimedia Foundation.
+ */
+class MWLoggerLegacySpi implements MWLoggerSpi {
+
+	/**
+	 * @var array $singletons
+	 */
+	protected $singletons = array();
+
+
+	/**
+	 * Get a logger instance.
+	 *
+	 * @param string $channel Logging channel
+	 * @return MWLogger Logger instance
+	 */
+	public function getLogger( $channel ) {
+		if ( !isset( $this->singletons[$channel] ) ) {
+			$this->singletons[$channel] = new MWLoggerLegacyLogger($channel);
+		}
+		return $this->singletons[$channel];
+	}
+
+}
