b3735a190f44c8934a2860e1e36772a249a72ee5
Bryan Davis <bd808@wikimedia.org>
2014-03-20 22:51:45 -0600

    Add a PSR-3 based logging interface
    The MWLogger class is actually a thin wrapper around any PSR-3
    LoggerInterface implementation. Named MWLogger instances can be obtained
    from the MWLogger::getInstance() static method. MWLogger expects a class
    implementing the MWLoggerSpi interface to act as a factory for new
    MWLogger instances. A concrete MWLoggerSpi implementation using the
    Monolog library is also provided.
    
    New classes introduced:
    ; MWLogger
    : PSR-3 compatible logger that wraps any \Psr\Log\LoggerInterface
      implementation
    ; MWLoggerSpi
    : Service provider interface for MWLogger factories
    ; MWLoggerMonologSpi
    : MWLoggerSpi for creating instances backed by the monolog logging library
    ; MwLogHandler
    : Monolog handler that replicates the udp2log and file logging
      functionality of wfErrorLog()
    
    New globals introduced:
    ; $wgMWLoggerDefaultSpi
    : Default SPI to use with MWLogger
    ; $wgMWLoggerMonologSpiConfig
    : Configuration for MWLoggerMonologSpi describing how to configure the
      Monolog logger instances.
    
    This change relies on the Composer managed Psr\Log and Monolog libraries
    introduced in Ie667944.
    
    Change-Id: I5c822995a181a38c844f4a13cb172297827e0031

---
+++ includes/debug/Logger.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * @section LICENSE
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * PSR-3 logging service.
+ *
+ * @since 1.23
+ * @author Bryan Davis <bd808@wikimedia.org>
+ * @copyright © 2014 Bryan Davis and Wikimedia Foundation.
+ */
+class MWLogger implements \Psr\Log\LoggerInterface {
+
+	/**
+	 * Service provider.
+	 * @var MWLoggerSpi $spi
+	 */
+	protected static $spi;
+
+
+	/**
+	 * Wrapped PSR-3 logger instance.
+	 *
+	 * @var \Psr\Log\LoggerInterface $deligate
+	 */
+	protected $deligate;
+
+
+	/**
+	 * @param \Psr\Log\LoggerInterface $logger
+	 */
+	public function __construct( \Psr\Log\LoggerInterface $logger ) {
+		$this->deligate = $logger;
+	}
+
+
+	/**
+	 * Logs with an arbitrary level.
+	 *
+	 * @param string|int $level
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function log( $level, $message, array $context = array() ) {
+		$this->deligate->log( $level, $message, $context );
+	}
+
+
+	/**
+	 * System is unusable.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function emergency( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::EMERGENCY, $message, $context );
+	}
+
+
+	/**
+	 * Action must be taken immediately.
+	 *
+	 * Example: Entire website down, database unavailable, etc. This should
+	 * trigger the SMS alerts and wake you up.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function alert( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::ALERT, $message, $context );
+	}
+
+
+	/**
+	 * Critical conditions.
+	 *
+	 * Example: Application component unavailable, unexpected exception.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function critical( $message, array $context = array( ) ) {
+		$this->log( \Psr\Log\LogLevel::CRITICAL, $message, $context );
+	}
+
+
+	/**
+	 * Runtime errors that do not require immediate action but should typically
+	 * be logged and monitored.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function error( $message, array $context = array( ) ) {
+		$this->log( \Psr\Log\LogLevel::ERROR, $message, $context );
+	}
+
+
+	/**
+	 * Exceptional occurrences that are not errors.
+	 *
+	 * Example: Use of deprecated APIs, poor use of an API, undesirable things
+	 * that are not necessarily wrong.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function warning( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::WARNING, $message, $context );
+	}
+
+
+	/**
+	 * Normal but significant events.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function notice( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::NOTICE, $message, $context );
+	}
+
+
+	/**
+	 * Interesting events.
+	 *
+	 * Example: User logs in, SQL logs.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function info( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::INFO, $message, $context );
+	}
+
+
+	/**
+	 * Detailed debug information.
+	 *
+	 * @param string $message
+	 * @param array $context
+	 */
+	public function debug( $message, array $context = array() ) {
+		$this->log( \Psr\Log\LogLevel::DEBUG, $message, $context );
+	}
+
+
+	/**
+	 * Register a service provider to create new MWLogger instances.
+	 *
+	 * @param MWLoggerSpi $provider Provider to register
+	 */
+	public static function registerProvider( MWLoggerSpi $provider ) {
+		self::$spi = $provider;
+	}
+
+
+	/**
+	 * Get a named logger instance from the currently configured logger factory.
+	 *
+	 * @param string $channel Logger channel (name)
+	 * @return MWLogger
+	 */
+	public static function getInstance( $channel ) {
+		if ( self::$spi === null ) {
+			global $wgMWLoggerDefaultSpi;
+			if ( is_callable( $wgMWLoggerDefaultSpi ) ) {
+				$provider = $wgMWLoggerDefaultSpi();
+			} else {
+				$provider = new $wgMWLoggerDefaultSpi();
+			}
+			self::registerProvider( $provider );
+		}
+
+		return self::$spi->getLogger( $channel );
+	}
+
+} //end MWLogger
