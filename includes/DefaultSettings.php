d29b02a674b78d5909f16a4f370b5b8f8f54008e
Bryan Davis <bd808@wikimedia.org>
2014-03-20 23:01:24 -0600

    Use MWLogger logging for legacy logging methods
    Send wfDebug, wfDebugLog, wfLogDBError and wfLogDBError log messages to
    the new MWLogger PSR-3 logger subsystem. Compatibility with the historic
    logging operations of wfLogDBError are provided by MWLoggerLegacyLogger
    and the MWLoggerLegacySpi logger factory.
    
    Requires the MWLogger system introduced in I5c82299 and the Composer
    managed libraries from Ie667944.
    
    Change-Id: I1e5596d590144fbfdfd5f18bc42cf1ef0dbcac12

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5217,7 +5217,7 @@ $wgDebugLogGroups = array();
  * @var $wgMWLoggerDefaultSpi string|callable
  * @see MwLogger
  */
-$wgMWLoggerDefaultSpi = 'MWLoggerNullSpi';
+$wgMWLoggerDefaultSpi = 'MWLoggerLegacySpi';
 
 /**
  * Configuration for MWLoggerMonologSpi logger factory.
