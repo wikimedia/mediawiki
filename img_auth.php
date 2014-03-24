4d9e07056ed0185183d9f80ab3b0353b503edfbe
Gilles Dubuc <gdubuc@wikimedia.org>
2014-03-25 00:31:21 +0100

    Have ?download parameter trigger Content-Disposition: attachment
    This parameter triggers Content-Disposition: Attachment which
    makes the browser download the image instead of displaying it.
    
    This is needed by Media Viewer to allow users to click a button in
    order to download an image at a given resolution or the original.
    
    Change-Id: I470a24a09139ac65588312104995e34d97a89b0f

--- img_auth.php
+++ img_auth.php
@@ -150,6 +150,10 @@ function wfImageAuthMain() {
 		return;
 	}
 
+	if ( $wgRequest->getCheck( 'download' ) ) {
+		header( 'Content-Disposition: attachment' );
+	}
+
 	// Stream the requested file
 	wfDebugLog( 'img_auth', "Streaming `" . $filename . "`." );
 	$repo->streamFile( $filename, array( 'Cache-Control: private', 'Vary: Cookie' ) );
