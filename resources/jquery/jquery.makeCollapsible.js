0db516b9acc7bfb88208086923b69add977acdd2
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-24 13:51:29 +0100

    Use correct Creative Commons license abbreviation
    Change-Id: I7df989de9dca2ee056030ff6f5830d5f0124185e

--- resources/jquery/jquery.makeCollapsible.js
+++ resources/jquery/jquery.makeCollapsible.js
@@ -11,7 +11,7 @@
  * @author Krinkle, 2011-2012
  *
  * Dual license:
- * @license CC-BY 3.0 <http://creativecommons.org/licenses/by/3.0>
+ * @license CC BY 3.0 <http://creativecommons.org/licenses/by/3.0>
  * @license GPL2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  */
 ( function ( $, mw ) {
