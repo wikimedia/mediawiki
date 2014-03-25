d85b17a8881639c4d5962ac3fc17a8edd5e9f50a
Sam Smith <git@samsmith.io>
2014-03-25 15:18:09 +0000

    [WIP] Add the mw.cookie module
    Wrap $.cookie so that setting a cookie is syntactically and functionally
    equivalent to WebResponse#setcookie.
    
    Bug: 61979
    Change-Id: I217ef258aecf1acd335e2cea56ae08b22541c7d4

---
+++ resources/mediawiki.cookie/mediawiki.cookie.js
@@ -0,0 +1,38 @@
+( function( mw, $ ) {
+
+	'use strict';
+
+	mw.cookie = {
+		set: function( key, value, expire, options ) {
+			var config = mw.config.get( [ 'wgCookiePrefix', 'wgCookieDomain',
+				'wgCookiePath', 'wgCookieSecure', 'wgCookieExpiration' ] ),
+				defaultOptions = {
+					prefix: config.wgCookiePrefix,
+					domain: config.wgCookieDomain,
+					path: config.wgCookiePath,
+					secure: config.wgCookieSecure
+				},
+				seconds;
+
+			options = options || {};
+
+			if ( expire === undefined || expire === null ) {
+				expire = 0;
+			} else if ( expire === 0 && config.wgCookieExpiration ) {
+				expire = config.wgCookieExpiration;
+			}
+
+			if ( expire > 0 ) {
+				seconds = expire;
+				expire = new Date();
+				expire.setTime( +expire + seconds );
+			}
+
+			options.expires = expire;
+			options = $.extend( {}, defaultOptions, options );
+
+			$.cookie( key, value, options );
+		}
+	};
+
+} )( mediaWiki, jQuery );
