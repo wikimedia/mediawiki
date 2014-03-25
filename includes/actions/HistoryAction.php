ab9972215a0a4a48ab9d12a17fc3b56ac2f3dba6
tinajohnson.1234 <tinajohnson.1234@gmail.com>
2014-03-25 21:31:51 +0530

    HistoryAction: Implement HistoryPageAddLink hook for adding more links
    This allows AbuseFilter to add an extra link in the tool links section of
    the subtitle on a history page
    
    Co-authored-by: Matěj Suchánek
    Bug: T28934
    Change-Id: I2e0e9e92d3fc303135b0eb9acf06b5fd120178a5

--- includes/actions/HistoryAction.php
+++ includes/actions/HistoryAction.php
@@ -61,13 +61,19 @@ class HistoryAction extends FormlessAction {
 	}
 
 	protected function getDescription() {
+		$links = [];
 		// Creation of a subtitle link pointing to [[Special:Log]]
-		return MediaWikiServices::getInstance()->getLinkRenderer()->makeKnownLink(
+		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
+		$links[] = $linkRenderer->makeKnownLink(
 			SpecialPage::getTitleFor( 'Log' ),
 			$this->msg( 'viewpagelogs' )->text(),
 			[],
 			[ 'page' => $this->getTitle()->getPrefixedText() ]
 		);
+
+		// Allow extensions to add more links
+		Hooks::run( 'HistoryPageAddLink', [ $this->getContext(), $linkRenderer, &$links ] );
+		return $this->getLanguage()->pipeList( $links );
 	}
 
 	/**
