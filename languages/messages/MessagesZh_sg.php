e477595854ad16433a8a1ee2b8260ebe2d773fb6
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-26 11:39:54 +0100

    [WIP] Convert core messages to JSON i18n format
    Change-Id: I35bbb3a7a145fc48d14f05620407dff5ecfdd4fc

--- languages/messages/MessagesZh_sg.php
+++ languages/messages/MessagesZh_sg.php
@@ -7,22 +7,8 @@
  * @ingroup Language
  * @file
  *
- * @author PhiLiP
- * @author Shizhao
- * @author Wong128hk
  */
 
 # Inherit everything for now
 $fallback = 'zh-hans';
 
-$messages = array(
-# User preference toggles
-'tog-norollbackdiff' => '进行回退后略过差异比较',
-
-# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage) and the disambiguation template definition (see disambiguations).
-'portal' => '社区入口',
-
-# Edit pages
-'editing' => '正在编辑 $1',
-
-);
