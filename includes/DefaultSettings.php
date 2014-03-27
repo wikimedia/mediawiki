9fd800884cf69ecd3953903e572434008987109a
mglaser <glaser@hallowelt.biz>
2014-03-27 16:23:17 +0100

    Updated release notes and version number for MediaWiki 1.21.8
    This is MediaWiki 1.21.8 security and maintenance release
    
    Change-Id: I73eda2cffbbf31728dc771181d948f6c45b6b5c1

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.21.7';
+$wgVersion = '1.21.8';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
