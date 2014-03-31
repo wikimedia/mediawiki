f4aa2d3c70bec25941e8bf9b6bde0ff32c2b64b4
Sam Smith <git@samsmith.io>
2014-03-31 15:21:19 +0100

    Delete the post-edit cookie using the correct domain
    Use the mediawiki.cookie#set to delete the post-edit cookie so that the
    correct domain is used.
    
    Bug: 61979
    Change-Id: I0cba8d1421f359ff78f3f994af358e9997f27041

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -1204,8 +1204,6 @@ class EditPage {
 	 * marked HttpOnly. The JavaScript code converts the cookie to a wgPostEdit config
 	 * variable.
 	 *
-	 * We use a path of '/' since wgCookiePath is not exposed to JS
-	 *
 	 * If the variable were set on the server, it would be cached, which is unwanted
 	 * since the post-edit state should only apply to the load right after the save.
 	 */
@@ -1215,7 +1213,6 @@ class EditPage {
 
 		$response = RequestContext::getMain()->getRequest()->response();
 		$response->setcookie( $postEditKey, '1', time() + self::POST_EDIT_COOKIE_DURATION, array(
-			'path' => '/',
 			'httpOnly' => false,
 		) );
 	}
