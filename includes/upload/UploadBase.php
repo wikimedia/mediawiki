af86351e45527ce1710bce15aec8278ac1082837
kaldari <rkaldari@wikimedia.org>
2014-03-31 13:50:03 -0700

    Adding debugging for bug 62241
    In the event of a bad filename warning record the relavent variables
    so that we can figure out what is causing the failure.
    
    Bug: 62241
    Change-Id: If5d8b9e7f34c0f9bd09684d7dada8fafd7f5f7c2

--- includes/upload/UploadBase.php
+++ includes/upload/UploadBase.php
@@ -613,6 +613,8 @@ abstract class UploadBase {
 
 		if ( $this->mDesiredDestName != $filename && $comparableName != $filename ) {
 			$warnings['badfilename'] = $filename;
+			// Debugging for bug 62241
+			wfDebugLog( 'upload', "Filename: '$filename', mDesiredDestName: '$this->mDesiredDestName', comparableName: '$comparableName'";
 		}
 
 		// Check whether the file extension is on the unwanted list
