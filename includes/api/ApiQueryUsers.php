c5ceb9966523ec18be5d555a0ae9e01cc9bd3cf1
Derk-Jan Hartman <hartman.wiki@gmail.com>
2014-03-31 23:17:20 +0200

    [WIP] Add timestamp of block to blockinfo
    Bug: 63326
    Change-Id: Ifdeac5c5f5474deea78bdf4387179199bc788239

--- includes/api/ApiQueryUsers.php
+++ includes/api/ApiQueryUsers.php
@@ -176,6 +176,7 @@ class ApiQueryUsers extends ApiQueryBase {
 					$data[$name]['blockid'] = $row->ipb_id;
 					$data[$name]['blockedby'] = $row->ipb_by_text;
 					$data[$name]['blockedbyid'] = $row->ipb_by;
+					$data[$name]['blockedtimestamp'] = $row->ipb_timestamp;
 					$data[$name]['blockreason'] = $row->ipb_reason;
 					$data[$name]['blockexpiry'] = $row->ipb_expiry;
 				}
@@ -357,6 +358,10 @@ class ApiQueryUsers extends ApiQueryBase {
 					ApiBase::PROP_TYPE => 'integer',
 					ApiBase::PROP_NULLABLE => true
 				),
+				'blockedtimestamp' => array(
+					ApiBase::PROP_TYPE => 'timestamp',
+					ApiBase::PROP_NULLABLE => true
+				)
 				'blockedreason' => array(
 					ApiBase::PROP_TYPE => 'string',
 					ApiBase::PROP_NULLABLE => true
