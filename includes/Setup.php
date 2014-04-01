64c47bd0d6b6b2a1f4c6d3ca56fde4ea7badd409
Alexandre Emsenhuber <mediawiki@emsenhuber.ch>
2014-04-01 21:03:00 +0200

    Make the global objects documentation consistent in Setup.php
    Change-Id: I7ba179ea2202586854194c4f20f403ed9fd60aa3

--- includes/Setup.php
+++ includes/Setup.php
@@ -545,12 +545,19 @@ if ( !defined( 'MW_NO_SESSION' ) && !$wgCommandLineMode ) {
 wfProfileOut( $fname . '-session' );
 wfProfileIn( $fname . '-globals' );
 
+/**
+ * @var $wgContLang Language
+ */
 $wgContLang = Language::factory( $wgLanguageCode );
 $wgContLang->initEncoding();
 $wgContLang->initContLang();
 
 // Now that variant lists may be available...
 $wgRequest->interpolateTitle();
+
+/**
+ * @var $wgUser User
+ */
 $wgUser = RequestContext::getMain()->getUser(); # BackCompat
 
 /**
@@ -559,7 +566,7 @@ $wgUser = RequestContext::getMain()->getUser(); # BackCompat
 $wgLang = new StubUserLang;
 
 /**
- * @var OutputPage
+ * @var $wgOut OutputPage
  */
 $wgOut = RequestContext::getMain()->getOutput(); # BackCompat
 
@@ -573,7 +580,9 @@ if ( !is_object( $wgAuth ) ) {
 	wfRunHooks( 'AuthPluginSetup', array( &$wgAuth ) );
 }
 
-# Placeholders in case of DB error
+/**
+ * @var $wgTitle Title
+ */
 $wgTitle = null;
 
 $wgDeferredUpdateList = array();
