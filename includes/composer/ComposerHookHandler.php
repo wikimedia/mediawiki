2d20084aa282d51bcb7c11d40cb4f226bc7141cb
jeroendedauw <jeroendedauw@gmail.com>
2014-04-05 19:49:21 +0200

    Fix path after file got moved
    Change-Id: Ic67f590fb53f2b96cfd8a0192968cb4953bb0d2b

--- includes/composer/ComposerHookHandler.php
+++ includes/composer/ComposerHookHandler.php
@@ -3,8 +3,8 @@
 use Composer\Package\Package;
 use Composer\Script\Event;
 
-$GLOBALS['IP'] = __DIR__ . '/../';
-require_once '../AutoLoader.php';
+$GLOBALS['IP'] = __DIR__ . '/../../';
+require_once __DIR__ . '/../AutoLoader.php';
 
 /**
  * @licence GNU GPL v2+
