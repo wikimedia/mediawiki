aafbf62f8b2be8fb533313777baa28ca9a96535e
daniel <daniel.kinzler@wikimedia.de>
2014-04-16 21:52:32 +0200

    Allow Status::hasMessage to work with Message objects.
    Change-Id: I52a468bc33f6c25630665ee8f987a25dc87659ee

--- tests/phpunit/includes/StatusTest.php
+++ tests/phpunit/includes/StatusTest.php
@@ -259,7 +259,10 @@ class StatusTest extends MediaWikiLangTestCase {
 	public function testHasMessage() {
 		$status = new Status();
 		$status->fatal( 'bad' );
+		$status->fatal( wfMessage( 'bad-msg' ) );
 		$this->assertTrue( $status->hasMessage( 'bad' ) );
+		$this->assertTrue( $status->hasMessage( 'bad-msg' ) );
+		$this->assertTrue( $status->hasMessage( wfMessage( 'bad-msg' ) ) );
 		$this->assertFalse( $status->hasMessage( 'good' ) );
 	}
 
