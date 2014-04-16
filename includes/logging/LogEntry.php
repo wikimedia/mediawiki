1a34c5366c1ea011af7c8c58bbfa49ffd579e99a
umherirrender <umherirrender_de.wp@web.de>
2014-04-16 19:20:36 +0200

    Allow LogEntry::setRelations to work with array(tag => value)
    Change-Id: I5a3d9799cd2125a02871b1c6a9ef7745eadb518d

--- includes/logging/LogEntry.php
+++ includes/logging/LogEntry.php
@@ -407,7 +407,7 @@ class ManualLogEntry extends LogEntryBase {
 	 * Declare arbitrary tag/value relations to this log entry.
 	 * These can be used to filter log entries later on.
 	 *
-	 * @param array $relations Map of (tag => (list of values))
+	 * @param array $relations Map of (tag => (list of values|value))
 	 * @since 1.22
 	 */
 	public function setRelations( array $relations ) {
@@ -512,6 +512,11 @@ class ManualLogEntry extends LogEntryBase {
 			if ( !strlen( $tag ) ) {
 				throw new MWException( "Got empty log search tag." );
 			}
+
+			if ( !is_array( $values ) ) {
+				$values = array( $values );
+			}
+
 			foreach ( $values as $value ) {
 				$rows[] = array(
 					'ls_field' => $tag,
