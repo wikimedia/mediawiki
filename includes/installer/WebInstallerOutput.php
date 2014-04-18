365cb151366bd62bccaacddac4865b7208045d18
Bartosz Dziewoński <matma.rex@gmail.com>
2014-04-18 16:39:37 +0200

    WebInstallerOutput: Fix path to jquery.js
    Follow-up to Id5ead145 and I335e4c08.
    
    Bug: 64091
    Change-Id: If40f479df7d68071d6ce5d2f3b8251e6bf3eb80e

--- includes/installer/WebInstallerOutput.php
+++ includes/installer/WebInstallerOutput.php
@@ -364,7 +364,7 @@ class WebInstallerOutput {
 	 * @return string
 	 */
 	public function getJQuery() {
-		return Html::linkedScript( "../resources/src/jquery/jquery.js" );
+		return Html::linkedScript( "../resources/lib/jquery/jquery.js" );
 	}
 
 }
