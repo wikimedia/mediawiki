fbeb2abac0c98a03fc31ad3430db67d1a5cb9382
Timo Tijhof <krinklemail@gmail.com>
2014-04-19 04:05:04 +0200

    update-oojs-ui.sh: Replace target dir instead of adding file copies
    * A command like "cp dist/* $TARGET_DIR" does not remove files
      that were deleted in the release dist. Using rsync instead.
    * Use bash -n or -z instead of "" and "x$VAR" == "x" tricks.
    * The "git commit <dir>" doesn't properly take care of staging
      deletion of files removed by the update script, nor does it
      reliably (?) stage new files.
      Instead using a combination of "git add -u <dir>" (The -u
      stages deletion and modifcation of tracked files), and
      "git add <dir>" (stages modification and creation of files in
      that directory).
    * Use "grunt test" instead of "grunt <default>" to make more
      explicit what is going on (no change in behaviour, default=test).
      And document why we don't use plain grunt-build without tests.
    Change-Id: Id97a66df64d9e43e3c05388486b9f959108363c1

---
+++ maintenance/resources/update-oojs-ui.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+# This script generates a commit that updates our distribution copy of OOjs UI
+
+if [ -z "$1" ]
+then
+	# Missing required parameter
+	echo >&2 "Usage: $0 path/to/repo/for/oojs-ui"
+	exit 1
+fi
+
+TARGET_REPO=$(cd $(dirname $0)/../..; pwd)
+TARGET_DIR=resources/lib/oojs-ui
+UI_REPO=$1
+
+function oojsuihash() {
+	grep "OOjs UI v" $TARGET_REPO/$TARGET_DIR/oojs-ui.js \
+		| head -n 1 \
+		| grep -Eo '\([a-z0-9]+\)' \
+		| sed 's/^(//' \
+		| sed 's/)$//'
+}
+
+function oojsuitag() {
+	grep "OOjs UI v" $TARGET_REPO/$TARGET_DIR/oojs-ui.js \
+		| head -n 1 \
+		| grep -Eo '\bv[0-9a-z.-]+\b'
+}
+
+function oojsuiversion() {
+	grep "OOjs UI v" $TARGET_REPO/$TARGET_DIR/oojs-ui.js \
+		| head -n 1 \
+		| grep -Eo '\bv[0-9a-z.-]+\b.*$'
+}
+
+# Prepare working tree
+cd $TARGET_REPO &&
+git reset $TARGET_DIR && git checkout $TARGET_DIR && git fetch origin &&
+git checkout -B upstream-oojsui origin/master || exit 1
+
+cd $UI_REPO || exit 1
+
+# Read the old version and check for changes
+OLDHASH=$(oojsuihash)
+if [ -z "$OLDHASH" ]
+then
+	OLDTAG=$(oojsuitag)
+fi
+if [ "$OLDHASH" == "" ]
+then
+	OLDHASH=$(git rev-parse $OLDTAG)
+	if [ $? != 0 ]
+	then
+		echo Could not find OOjs UI version
+		cd -
+		exit 1
+	fi
+fi
+if [ "$(git rev-parse $OLDHASH)" == "$(git rev-parse HEAD)" ]
+then
+	echo "No changes (already at $OLDHASH)"
+	cd -
+	exit 0
+fi
+
+# Build the distribution (using grunt-test instead of grunt-build, because we
+# want to run unit tests first, and because grunt-build is for a release build
+# and wouldn't put a git hash in the file headers)
+npm install && grunt test || exit 1
+
+# Get the list of changes
+NEWCHANGES=$(git log $OLDHASH.. --oneline --no-merges --reverse --color=never)
+NEWCHANGESDISPLAY=$(git log $OLDHASH.. --oneline --no-merges --reverse --color=always)
+
+# Copy files
+# - Exclude the default non-svg stylesheet
+rsync --recursive --delete --force --exclude 'oojs-ui.css' ./dist/ $TARGET_REPO/$TARGET_DIR || exit 1
+
+# Read the new version
+NEWVERSION=$(oojsuiversion)
+
+# Generate commit
+cd $TARGET_REPO
+COMMITMSG=$(cat <<END
+Update OOjs UI to $NEWVERSION
+
+New changes:
+$NEWCHANGES
+END
+)
+git add -u $TARGET_DIR && git add $TARGET_DIR && git commit -m "$COMMITMSG"
+cat >&2 <<END
+
+
+Created commit with changes:
+$NEWCHANGESDISPLAY
+END
