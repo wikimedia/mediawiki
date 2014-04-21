f3b12fca6602ec469876b0bf6f2eef0b212727b2
swalling <swalling@wikimedia.org>
2014-04-21 16:00:08 -0700

    Remove unnecessary h2 element on create account
    We added these headings during the last account creation and login form
    redesign but on recollection I'm not sure they're needed.
    
    The messages are pretty much redundant with the h1 heading,
    and now h2 elements have a margin-top of 1em it adds too much whitespace.
    
    Change-Id: Ice5e8bf23cc00bf00835e7dc26acc33182654ec5

--- includes/templates/Usercreate.php
+++ includes/templates/Usercreate.php
@@ -51,9 +51,6 @@ class UsercreateTemplate extends BaseTemplate {
 		<div id="signupstart"><?php $this->msgWiki( 'signupstart' ); ?></div>
 	<?php } ?>
 	<div id="userloginForm">
-		<h2 class="createaccount-join">
-			<?php $this->msg( $this->data['loggedin'] ? 'createacct-another-join' : 'createacct-join' ); ?>
-		</h2>
 		<form name="userlogin2" id="userlogin2" class="mw-ui-vform" method="post" action="<?php $this->text( 'action' ); ?>">
 			<section class="mw-form-header">
 				<?php $this->html( 'header' ); /* extensions such as ConfirmEdit add form HTML here */ ?>
