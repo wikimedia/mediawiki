b0bfd2323371ca87ffd915ae75e8222343cf0944
umherirrender <umherirrender_de.wp@web.de>
2014-04-22 14:16:45 +0200

    Add jquery.accessKeyLabel javascript module
    Extracted the accessKey logic from mediawiki.util and moved it to it's
    own module. mediawiki.util now depends on the new module,
    moved the jquery.client dependency from mediawiki.util to the new
    module.
    
    Now the accesskey is also set, when no placeholder like '[x]' is set on
    the title (bug 48505).
    Also the label for a input or textarea gets updated (bug 63849).
    
    Also implements support for the browsers accessKeyLabel function (bug
    37239), which will give a localised accessKey string, if supported (at
    the moment FireFox 8+ only)
    
    Bug: 37239
    Bug: 48505
    Bug: 63849
    Change-Id: I211593629ca23540dcd5a93b11834155f4f04883

--- tests/qunit/suites/resources/mediawiki/mediawiki.util.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.util.test.js
@@ -1,11 +1,10 @@
 ( function ( mw, $ ) {
 	QUnit.module( 'mediawiki.util', QUnit.newMwEnvironment( {
 		setup: function () {
-			this.taPrefix = mw.util.tooltipAccessKeyPrefix;
-			mw.util.tooltipAccessKeyPrefix = 'ctrl-alt-';
+			$.fn.updateTooltipAccessKeys.setTestMode( true );
 		},
 		teardown: function () {
-			mw.util.tooltipAccessKeyPrefix = this.taPrefix;
+			$.fn.updateTooltipAccessKeys.setTestMode( false );
 		}
 	} ) );
 
@@ -89,6 +88,8 @@
 	} );
 
 	QUnit.test( 'tooltipAccessKey', 4, function ( assert ) {
+		this.suppressWarnings();
+
 		assert.equal( typeof mw.util.tooltipAccessKeyPrefix, 'string', 'tooltipAccessKeyPrefix must be a string' );
 		assert.equal( $.type( mw.util.tooltipAccessKeyRegexp ), 'regexp', 'tooltipAccessKeyRegexp is a regexp' );
 		assert.ok( mw.util.updateTooltipAccessKeys, 'updateTooltipAccessKeys is non-empty' );
@@ -96,6 +97,8 @@
 		'Example [a]'.replace( mw.util.tooltipAccessKeyRegexp, function ( sub, m1, m2, m3, m4, m5, m6 ) {
 			assert.equal( m6, 'a', 'tooltipAccessKeyRegexp finds the accesskey hint' );
 		} );
+
+		this.restoreWarnings();
 	} );
 
 	QUnit.test( '$content', 2, function ( assert ) {
@@ -145,7 +148,7 @@
 		assert.ok( $.isDomElement( tbRL ), 'addPortletLink returns a valid DOM Element according to $.isDomElement' );
 
 		tbMW = mw.util.addPortletLink( 'p-test-tb', '//mediawiki.org/',
-			'MediaWiki.org', 't-mworg', 'Go to MediaWiki.org ', 'm', tbRL );
+			'MediaWiki.org', 't-mworg', 'Go to MediaWiki.org', 'm', tbRL );
 		$tbMW = $( tbMW );
 
 		assert.propEqual(
@@ -160,7 +163,7 @@
 			$tbMW.find( 'a' ).getAttrs(),
 			{
 				href: '//mediawiki.org/',
-				title: 'Go to MediaWiki.org [ctrl-alt-m]',
+				title: 'Go to MediaWiki.org [test-m]',
 				accesskey: 'm'
 			},
 			'Validate attributes of anchor tag in created element'
@@ -172,7 +175,7 @@
 		cuQuux = mw.util.addPortletLink( 'p-test-custom', '#', 'Quux', null, 'Example [shift-x]', 'q' );
 		$cuQuux = $( cuQuux );
 
-		assert.equal( $cuQuux.find( 'a' ).attr( 'title' ), 'Example [ctrl-alt-q]', 'Existing accesskey is stripped and updated' );
+		assert.equal( $cuQuux.find( 'a' ).attr( 'title' ), 'Example [test-q]', 'Existing accesskey is stripped and updated' );
 
 		assert.equal(
 			$( '#p-test-custom #c-barmenu ul li' ).length,
