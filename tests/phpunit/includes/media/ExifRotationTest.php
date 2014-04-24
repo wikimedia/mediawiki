e1d58d90711b486cd48fa0cb0c183066798c3867
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-24 19:51:33 +0200

    Pass phpcs-strict on some test files (8/11)
    Change-Id: I138c1f9bf2c3c7e9218d5fa29365e78b309fb459

--- tests/phpunit/includes/media/ExifRotationTest.php
+++ tests/phpunit/includes/media/ExifRotationTest.php
@@ -70,8 +70,16 @@ class ExifRotationTest extends MediaWikiTestCase {
 			$file = $this->dataFile( $name, $type );
 			$thumb = $file->transform( $params, File::RENDER_NOW | File::RENDER_FORCE );
 
-			$this->assertEquals( $out[0], $thumb->getWidth(), "$name: thumb reported width check for $size" );
-			$this->assertEquals( $out[1], $thumb->getHeight(), "$name: thumb reported height check for $size" );
+			$this->assertEquals(
+				$out[0],
+				$thumb->getWidth(),
+				"$name: thumb reported width check for $size"
+			);
+			$this->assertEquals(
+				$out[1],
+				$thumb->getHeight(),
+				"$name: thumb reported height check for $size"
+			);
 
 			$gis = getimagesize( $thumb->getLocalCopyPath() );
 			if ( $out[0] > $info['width'] ) {
@@ -160,8 +168,16 @@ class ExifRotationTest extends MediaWikiTestCase {
 			$file = $this->dataFile( $name, $type );
 			$thumb = $file->transform( $params, File::RENDER_NOW | File::RENDER_FORCE );
 
-			$this->assertEquals( $out[0], $thumb->getWidth(), "$name: thumb reported width check for $size" );
-			$this->assertEquals( $out[1], $thumb->getHeight(), "$name: thumb reported height check for $size" );
+			$this->assertEquals(
+				$out[0],
+				$thumb->getWidth(),
+				"$name: thumb reported width check for $size"
+			);
+			$this->assertEquals(
+				$out[1],
+				$thumb->getHeight(),
+				"$name: thumb reported height check for $size"
+			);
 
 			$gis = getimagesize( $thumb->getLocalCopyPath() );
 			if ( $out[0] > $info['width'] ) {
