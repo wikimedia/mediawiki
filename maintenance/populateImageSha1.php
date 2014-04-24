cc7935259409b67ee46783a5fbe4eea5dda545fe
Aaron Schulz <aschulz@wikimedia.org>
2014-04-24 15:46:08 -0700

    Avoid query error with odd parameter combination
    Change-Id: I1fc4495d665ff2c1e0cbf274ee2081a06c4e603b

--- maintenance/populateImageSha1.php
+++ maintenance/populateImageSha1.php
@@ -82,7 +82,7 @@ class PopulateImageSha1 extends LoggedUpdateMaintenance {
 			}
 			$this->output( "Populating img_sha1 field for specified files\n" );
 		} else {
-			if ( $force ) {
+			if ( $force || $this->hasOption( 'multiversiononly' ) ) {
 				$conds = array();
 				$this->output( "Populating and recalculating img_sha1 field\n" );
 			} else {
