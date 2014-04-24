0c9a979aefa0dabebb5b4aa68d1c11ca1ea354bd
mglaser <glaser@hallowelt.biz>
2014-04-24 20:22:25 +0200

    Updated release notes and version number of MediaWiki 1.22.6
    This is MediaWiki 1.22.6 security release
    
    Change-Id: I1be03b64a52996e7adcf25e9f9354000c24f38f9

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.22.5';
+$wgVersion = '1.22.6';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
