0656fbee01b85521daf3860ae3f5c20f0ed4f2f5
Chad Horohoe <chadh@wikimedia.org>
2014-04-25 16:31:57 -0700

    Pass a Title for context to Linker::makeExternalLink()
    Only two usages without a title left in core:
    - Skin::getCopyright()
    - WikiMap::makeForeignLink()
    
    Change-Id: I461310ab2d5b1661fc7916156c43b66df96bdeea

--- includes/specials/SpecialVersion.php
+++ includes/specials/SpecialVersion.php
@@ -506,7 +506,10 @@ class SpecialVersion extends SpecialPage {
 				Linker::makeExternalLink(
 					'//www.mediawiki.org/wiki/Special:MyLanguage/Manual:Tag_extensions',
 					$this->msg( 'version-parser-extensiontags' )->parse(),
-					false /* msg()->parse() already escapes */
+					false /* msg()->parse() already escapes */,
+					'',
+					array(),
+					$this->getTitle()
 				)
 			);
 
@@ -534,7 +537,10 @@ class SpecialVersion extends SpecialPage {
 			$out = Html::rawElement( 'h2', array( 'class' => 'mw-headline' ), Linker::makeExternalLink(
 				'//www.mediawiki.org/wiki/Special:MyLanguage/Manual:Parser_functions',
 				$this->msg( 'version-parser-function-hooks' )->parse(),
-				false /* msg()->parse() already escapes */
+				false /* msg()->parse() already escapes */,
+				'',
+				array(),
+				$this->getTitle()
 			) );
 
 			$out .= $this->listToText( $fhooks );
@@ -627,7 +633,8 @@ class SpecialVersion extends SpecialPage {
 				$extensionName,
 				true,
 				'',
-				array( 'class' => 'mw-version-ext-name' )
+				array( 'class' => 'mw-version-ext-name' ),
+				$this->getTitle()
 			);
 		} else {
 			$extensionNameLink = $extensionName;
@@ -699,7 +706,8 @@ class SpecialVersion extends SpecialPage {
 					$this->msg( 'version-version', $vcsVersion ),
 					true,
 					'',
-					array( 'class' => 'mw-version-ext-vcs-version' )
+					array( 'class' => 'mw-version-ext-vcs-version' ),
+					$this->getTitle()
 				);
 			} else {
 				$vcsVerString = Html::element( 'span',
