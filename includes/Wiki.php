303924f94302f45d8f19fe4dc9c9196ac26ee679
umherirrender <umherirrender_de.wp@web.de>
2014-04-26 22:34:21 +0200

    http -> https rewrite: Only replace one http by https
    This avoids changing from
    http://de.wikipedia.org/wiki/Spezial:Weblinksuche/http://wikimedia.org
    to
    https://de.wikipedia.org/wiki/Spezial:Weblinksuche/https://wikimedia.org
    instead of
    https://de.wikipedia.org/wiki/Spezial:Weblinksuche/http://wikimedia.org
    
    Bug: 63573
    Change-Id: Ife5099c3c5a0e943bb08b81be5d5669d0e02be17

--- includes/Wiki.php
+++ includes/Wiki.php
@@ -542,7 +542,7 @@ class MediaWiki {
 			$request->getProtocol() == 'http'
 		) {
 			$oldUrl = $request->getFullRequestURL();
-			$redirUrl = str_replace( 'http://', 'https://', $oldUrl );
+			$redirUrl = str_replace( 'http://', 'https://', $oldUrl, 1 );
 
 			if ( $request->wasPosted() ) {
 				// This is weird and we'd hope it almost never happens. This
