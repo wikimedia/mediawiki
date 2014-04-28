0f601afe0963867d04bea81d82a3f1bb2fb01428
Bryan Davis <bd808@wikimedia.org>
2014-04-28 13:50:03 -0600

    Support precomputed data in GitInfo
    Support reading git repository information from a `.mw-git-info.json`
    file. When present in a directory, this file serves to provide
    information needed by getHead, getHeadSHA1, getHeadCommitDate,
    getCurrentBranch and a new getRemoteUrl method.
    A GitInfo::precomputeValues method is also provided which can generate
    the .mw-git-info.json cache file for a given GitInfo instance.
    
    This support can be combined with a deployment step to reduce the need
    to repeatedly gather information from the .git files/git binary. It also
    allows computing information that can be lost when directory structures
    differ between deployment staging hosts and hosts running MediaWiki.
    
    Bug: 53972
    Change-Id: I66e058acc5a71e5d82644f85d819f49d6ee9d1e6

---
+++ tests/phpunit/includes/GitInfoTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @covers GitInfo
+ */
+class GitInfoTest extends MediaWikiTestCase {
+
+	public function testValidJsonData() {
+		$dir = __DIR__ . '/../data/gitinfo/testValidJsonData';
+		$this->assertFileExists( "${dir}/.mw-git-info.json" );
+
+		$fixture = new GitInfo( $dir );
+
+		$this->assertTrue( $fixture->cacheIsComplete() );
+		$this->assertEquals( 'refs/heads/master', $fixture->getHead() );
+		$this->assertEquals( '0123456789abcdef0123456789abcdef01234567',
+			$fixture->getHeadSHA1() );
+		$this->assertEquals( '1070884800', $fixture->getHeadCommitDate() );
+		$this->assertEquals( 'master', $fixture->getCurrentBranch() );
+		$this->assertContains( '0123456789abcdef0123456789abcdef01234567',
+			$fixture->getHeadViewUrl() );
+	}
+
+	public function testMissingJsonData() {
+		$dir = __DIR__ . '/../data/gitinfo/testMissingJsonData';
+		$this->assertFileNotExists( "${dir}/.mw-git-info.json" );
+		$this->assertFileNotExists( "${dir}/.git" );
+
+		$fixture = new GitInfo( $dir );
+
+		$this->assertFalse( $fixture->cacheIsComplete() );
+
+		$this->assertEquals( false, $fixture->getHead() );
+		$this->assertEquals( false, $fixture->getHeadSHA1() );
+		$this->assertEquals( false, $fixture->getHeadCommitDate() );
+		$this->assertEquals( false, $fixture->getCurrentBranch() );
+		$this->assertEquals( false, $fixture->getHeadViewUrl() );
+
+		// After calling all the outputs, the cache should be complete
+		$this->assertTrue( $fixture->cacheIsComplete() );
+	}
+
+}
