5d068d990e9ded6504968a0140e2cfd74c7889e2
Ricordisamoa <ricordisamoa@live.it>
2014-05-04 07:36:05 +0200

    new mediawiki.api.user module
    adds getUserInfo(), setOptions() and updateOptions()
    to mw.Api.prototype
    
    Change-Id: Ib81add66245f1b3ab03151db074bf9f4ab2acbde

---
+++ resources/src/mediawiki.api/mediawiki.api.user.js
@@ -0,0 +1,74 @@
+/**
+ * Get information about the currently logged-in user,
+ * and set user options as well, with ease.
+ * @author Ricordisamoa
+ * @class mw.Api.plugin.user
+ */
+( function ( mw, $ ) {
+	'use strict';
+
+	$.extend( mw.Api.prototype, {
+		/**
+		 * Return information about the currently logged-in user.
+		 * @param {string|string[]} prop which properties to get
+		 * @return {jQuery.Promise} See mw.Api#post
+		 */
+		getUserInfo: function ( prop ) {
+			return this.post( {
+				meta: 'userinfo',
+				uiprop: $.isArray( prop ) ? prop.join( '|' ) : prop
+			} )
+			.then( function ( data ) {
+				return data.query.userinfo;
+			} );
+		},
+
+		/**
+		 * Change options for the currently logged-in user.
+		 * @param {Object} change which options to set, in form of optionname=optionvalue pairs;
+		 *  use null as optionvalue to reset it to the site default.
+		 * @param {boolean} [reset] resets all preferences to the site defaults
+		 * @return {jQuery.Promise} See mw.Api#post
+		 */
+		setOptions: function ( change, reset ) {
+			var params = {
+				action: 'options'
+			};
+
+			if ( reset ) {
+				params.reset = 1;
+			}
+
+			if ( change ) {
+				params.change = $.map( change, function ( value, name ) {
+					if ( value !== null && String(value).indexOf( '|' ) !== -1 ) {
+						params.optionname = name;
+						params.optionvalue = value;
+					} else {
+						return name + ( value === null ? '' : '=' + value );
+					}
+				} )
+				.join( '|' );
+			}
+
+			return this.postWithToken( 'options', params );
+		},
+
+		/**
+		 * Update mw.user.options with live data.
+		 * @return {jQuery.Promise} See mw.Api#post
+		 */
+		updateOptions: function () {
+			return this.getUserInfo( 'options' )
+				.done( function ( data ) {
+					mw.user.options.set( data.options || {} );
+				} );
+		}
+	} );
+
+	/**
+	 * @class mw.Api
+	 * @mixins mw.Api.plugin.user
+	 */
+
+}( mediaWiki, jQuery ) );
