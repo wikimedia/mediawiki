24de549b873564d8ccea73226f4f1c0f53f1a666
addshore <addshorewiki@gmail.com>
2014-05-05 17:10:07 +0100

    Remove MediaWikiPHPUnitCommand
    All functionality has been moved to other places
    
    Change-Id: I6b6b0ef846bc63108c4dff9e17098432fd9d1697

--- tests/phpunit/includes/parser/NewParserTest.php
+++ tests/phpunit/includes/parser/NewParserTest.php
@@ -50,8 +50,8 @@ class NewParserTest extends MediaWikiTestCase {
 		parent::setUp();
 
 		//Setup CLI arguments
-		if ( $this->getCliArg( 'regex=' ) ) {
-			$this->regex = $this->getCliArg( 'regex=' );
+		if ( $this->getCliArg( 'regex' ) ) {
+			$this->regex = $this->getCliArg( 'regex' );
 		} else {
 			# Matches anything
 			$this->regex = '';
@@ -346,11 +346,11 @@ class NewParserTest extends MediaWikiTestCase {
 			self::getOptionValue( 'wgLinkHolderBatchSize', $opts, 1000 );
 
 		$uploadDir = $this->getUploadDir();
-		if ( $this->getCliArg( 'use-filebackend=' ) ) {
+		if ( $this->getCliArg( 'use-filebackend' ) ) {
 			if ( self::$backendToUse ) {
 				$backend = self::$backendToUse;
 			} else {
-				$name = $this->getCliArg( 'use-filebackend=' );
+				$name = $this->getCliArg( 'use-filebackend' );
 				$useConfig = array();
 				foreach ( $wgFileBackends as $conf ) {
 					if ( $conf['name'] == $name ) {
@@ -769,8 +769,8 @@ class NewParserTest extends MediaWikiTestCase {
 
 		$files = $wgParserTestFiles;
 
-		if ( $this->getCliArg( 'file=' ) ) {
-			$files = array( $this->getCliArg( 'file=' ) );
+		if ( $this->getCliArg( 'file' ) ) {
+			$files = array( $this->getCliArg( 'file' ) );
 		}
 
 		$dict = $this->getFuzzInput( $files );
