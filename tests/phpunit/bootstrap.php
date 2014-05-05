159dc79049e4f0b45883d739eecbd93baf69001b
addshore <addshorewiki@gmail.com>
2014-05-05 13:43:00 +0100

    Move teardownTestDB and wfLogProfilingData out of MWPHPUnitCommand
    Change-Id: Ic175c1457e7f76a2b5232a2b5d3563c5982cfee2

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -13,3 +13,18 @@ Running phpunit.php instead is recommended.
 EOF;
 	require_once __DIR__ . "/phpunit.php";
 }
+
+class MediaWikiPHPUnitBootstrap {
+
+	public function __destruct() {
+		// Return to real wiki db, so profiling data is preserved
+		MediaWikiTestCase::teardownTestDB();
+
+		// Log profiling data, e.g. in the database or UDP
+		wfLogProfilingData();
+	}
+
+}
+
+// This will be destructed after all tests have been run
+$mediawikiPHPUnitBootstrap = new MediaWikiPHPUnitBootstrap();
