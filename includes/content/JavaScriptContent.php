06333a471c73bf4ffab326fb3c4720011e59cf8b
Bartosz Dziewoński <matma.rex@gmail.com>
2014-05-05 17:59:54 +0200

    Content: Deprecate and stop using getHighlightHtml()
    That function was clearly a bad idea, everyone should just use
    getHtml() instead. It was supposed to allow extensions to extend
    the content formatting process, but actually doesn't.
    
    A better hook-based solution follows in separate commit I979e2438.
    
    Split off from original I979e2438 and rebased with minor changes
    (added wfDeprecated(), updated documentation comments, etc.).
    
    Co-Authored-By: daniel <daniel.kinzler@wikimedia.de>
    Co-Authored-By: Bartosz Dziewoński <matma.rex@gmail.com>
    Change-Id: I76412f9d28bb145fb5975f59e538f6560e50472f

--- includes/content/JavaScriptContent.php
+++ includes/content/JavaScriptContent.php
@@ -66,7 +66,7 @@ class JavaScriptContent extends TextContent {
 	protected function getHtml() {
 		$html = "";
 		$html .= "<pre class=\"mw-code mw-js\" dir=\"ltr\">\n";
-		$html .= $this->getHighlightHtml();
+		$html .= htmlspecialchars( $this->getNativeData() );
 		$html .= "\n</pre>\n";
 
 		return $html;
