27766d9c3b0a8e1973d01268d34613104cba60ce
Ori Livneh <ori@wikimedia.org>
2014-05-06 02:31:24 -0700

    Add $wgServerName
    This partially reverts r73950 which removed $wgServerName on the ground that it
    was only used for {{SERVERNAME}}. When it was pointed out that $wgServerName was
    also used by several extensions, the response was not to restore the variable, but
    to proceed to remove it from extensions as well.
    
    It is a useful variable to have, as the discussion on Id819246a9 makes clear
    (see Tim's comment on PS12 and Timo's reply). So let's reintroduce it, and expose
    it in mw.config and ApiQuerySiteInfo as well.
    
    Change-Id: I40a6fd427d38c64c628f70a2f407b145443ea204

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -2731,7 +2731,7 @@ class Parser {
 	 * @return string
 	 */
 	function getVariableValue( $index, $frame = false ) {
-		global $wgContLang, $wgSitename, $wgServer;
+		global $wgContLang, $wgSitename, $wgServer, $wgServerName;
 		global $wgArticlePath, $wgScriptPath, $wgStylePath;
 
 		if ( is_null( $this->mTitle ) ) {
@@ -3032,8 +3032,7 @@ class Parser {
 			case 'server':
 				return $wgServer;
 			case 'servername':
-				$serverParts = wfParseUrl( $wgServer );
-				return $serverParts && isset( $serverParts['host'] ) ? $serverParts['host'] : $wgServer;
+				return $wgServerName;
 			case 'scriptpath':
 				return $wgScriptPath;
 			case 'stylepath':
