bd1a344ed3b126ad366a84926c216f305a64b4cd
James D. Forrester <jforrester@wikimedia.org>
2014-05-09 13:27:09 +0200

    Update OOjs UI to v0.1.0-pre (7b3672591f)
    New changes:
    5556abf demo: Make demo console more generic
    142bd44 Dialog demos
    6f65873 demo/widgets: Bring dev console back
    32055e9 Minor refactor of widgets demo
    d6b80e7 Watch dot files
    7b36725 Localisation updates from https://translatewiki.net.
    
    Change-Id: I1ea5c113b24c1c1a1d853cfda3fccfcfd13140cc

--- resources/lib/oojs-ui/oojs-ui.js
+++ resources/lib/oojs-ui/oojs-ui.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (7d2507b267)
+ * OOjs UI v0.1.0-pre (7b3672591f)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: Mon May 05 2014 14:13:13 GMT-0700 (PDT)
+ * Date: Fri May 09 2014 13:27:04 GMT+0200 (CEST)
  */
 ( function ( OO ) {
 
