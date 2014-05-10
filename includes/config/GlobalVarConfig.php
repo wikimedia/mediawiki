3cd08047d6fe780d521c84a61209a9b6a66ff7f6
Kunal Mehta <legoktm@gmail.com>
2014-05-10 01:19:00 -0700

    Make abstract Config class truly implementation-agnostic
    Follow up to I13baec0b6 ("Config: Add Config and GlobalConfig classes"):
    
    Config:
    * Rather than returning Status objects, Config::set will now throw an exception
      if an error is encountered
    * Config::factory was moved into it's own ConfigFactory class.
    * Since there are no more functions in it, Config was turned into an interface.
    
    GlobalConfig:
    * Remove $prefix args from Config::set and ::get. The idea of having an
      abstract Config class is to abstract some notion of configuration data from
      the particular way in which it is currently implemented (global variables).
      So the abstract base class has no business dealing with variable name
      prefixes.
    ** Instead GlobalVarConfig's implementations of get and set call getWithPrefix
       and setWithPrefix internally, which are now protected
    * Rename GlobalConfig to GlobalVarConfig, which makes it clearer that it isn't
      referring to the scope of the configuration value, but to the scope of the
      variable name which provides it.
    
    ConfigFactory:
    * ConfigFactory is where Config objects are registered, and later constructed.
    * Config objects are registered with a given name, and a callback factory function.
      This allows for implementations to construct the object with the parameters they want,
      and avoids the overhead of needing an entire class.
    ** The name 'main' is the default object returned by RequestContext::getConfig(),
       and is intended to be used by core.
    * This is a singleton class, the main instance can be obtained with:
      ConfigFactory::getDefaultInstance()
    
    In addition to the above:
    * $wgConfigClass was removed, and $wgConfigRegistry was introduced, which
      stores a name => callback. The name is to be what the Config instance is
      registered with, and the callback should return an implementation of Config.
    * Tests were written for the new ConfigFactory, and GlobalVarConfig's tests
      were improved.
    
    Co-Authored-By: Ori Livneh <ori@wikimedia.org>
    Co-Authored-By: Chad Horohoe <chadh@wikimedia.org>
    Co-Authored-By: Mattflaschen <mflaschen@wikimedia.org>
    Co-Authored-By: Parent5446 <tylerromeo@gmail.com>
    Co-Authored-By: Reedy <reedy@wikimedia.org>
    Co-Authored-By: Daniel Kinzler: <daniel.kinzler@wikimedia.de>
    Change-Id: I5a5857fcfa07598ba4ce9ae5bbb4ce54a567d31e

---
+++ includes/config/GlobalVarConfig.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright 2014
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Accesses configuration settings from $GLOBALS
+ *
+ * @since 1.23
+ */
+class GlobalVarConfig implements Config {
+
+	/**
+	 * Prefix to use for configuration variables
+	 * @var string
+	 */
+	private $prefix;
+
+	/**
+	 * Default builder function
+	 * @return GlobalVarConfig
+	 */
+	public static function newInstance() {
+		return new GlobalVarConfig();
+	}
+
+	public function __construct( $prefix = 'wg' ) {
+		$this->prefix = $prefix;
+	}
+
+	/**
+	 * @see Config::get
+	 */
+	public function get( $name ) {
+		return $this->getWithPrefix( $this->prefix, $name );
+	}
+
+	/**
+	 * @see Config::set
+	 */
+	public function set( $name, $value ) {
+		$this->setWithPrefix( $this->prefix, $name, $value );
+	}
+
+	/**
+	 * Get a variable with a given prefix, if not the defaults.
+	 *
+	 * @param string $prefix Prefix to use on the variable, if one.
+	 * @param string $name Variable name without prefix
+	 * @throws ConfigException
+	 * @return mixed
+	 */
+	protected function getWithPrefix( $prefix, $name ) {
+		$var = $prefix . $name;
+		if ( !isset( $GLOBALS[ $var ] ) ) {
+			throw new ConfigException( __METHOD__ . ": undefined variable: '$var'" );
+		}
+		return $GLOBALS[ $var ];
+	}
+
+	/**
+	 * Get a variable with a given prefix, if not the defaults.
+	 *
+	 * @param string $prefix Prefix to use on the variable
+	 * @param string $name Variable name without prefix
+	 * @param mixed $value value to set
+	 */
+	protected function setWithPrefix( $prefix, $name, $value ) {
+		$GLOBALS[ $prefix . $name ] = $value;
+	}
+}
