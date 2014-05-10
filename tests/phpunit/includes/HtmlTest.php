6ff9310908f441ac7314569a82a99ea13530ada2
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-05-10 10:58:19 +0200

    Well-defined behavior for array values in all Html attributes
    Currently the behavior is more or less undefined for most attributes
    except for the ones in $spaceSeparatedListAttributes (currently 'class',
    'accesskey' and 'rel'). If an other attribute is set to an array (no
    matter what it contains) the method silently returns broken HTML like
    'key=' and triggers a warning if error_reporting is enabled. If
    error_reporting is not enabled the developer may overlook this.
    
    To make this clear: The method *ALLOWS* array values. This is *NOT*
    about unexpected types but unexpected combinations of values. These
    combinations are already checked in the method but the check is
    incomplete.
    
    I considered several solutions:
    * Simply use the first array element. But we can't know if the first
      element is what the caller expected.
    * Silently drop all arrays if the attribute doesn't allow lists. This
      is close to the current behavior of always returning 'key=' but is a
      breaking change for boolean attributes like 'checked' and 'selected'.
      Browsers accept the current 'checked=' as true while omiting the
      attribute means false.
    * Always throw an exception. As above, this is a breaking change in
      some cases.
    
    Change-Id: Id5fcbdef2696d0a81a91d54338939ee678475ca3

--- tests/phpunit/includes/HtmlTest.php
+++ tests/phpunit/includes/HtmlTest.php
@@ -112,7 +112,8 @@ class HtmlTest extends MediaWikiTestCase {
 			Html::expandAttributes( array( 'foo' => false ) ),
 			'skip keys with false value'
 		);
-		$this->assertNotEmpty(
+		$this->assertEquals(
+			' foo=""',
 			Html::expandAttributes( array( 'foo' => '' ) ),
 			'keep keys with an empty string'
 		);
@@ -150,6 +151,33 @@ class HtmlTest extends MediaWikiTestCase {
 			' selected=""',
 			Html::expandAttributes( array( 'selected' => true ) ),
 			'Boolean attributes have empty string value when value is true (wgWellFormedXml)'
+		);
+	}
+
+	/**
+	 * @covers HTML::expandAttributes
+	 */
+	public function testExpandAttributesForNumbers() {
+		$this->assertEquals(
+			' value=1',
+			Html::expandAttributes( array( 'value' => 1 ) ),
+			'Integer attribute generates string output'
+		);
+		$this->assertEquals(
+			' value=1.1',
+			Html::expandAttributes( array( 'value' => 1.1 ) ),
+			'Float attribute generates string output'
+		);
+	}
+
+	/**
+	 * @covers HTML::expandAttributes
+	 */
+	public function testExpandAttributesForObjects() {
+		$this->assertEquals(
+			' value=stringValue',
+			Html::expandAttributes( array( 'value' => new HtmlAttributeValue() ) ),
+			'Object attribute generates string output'
 		);
 	}
 
@@ -297,6 +325,16 @@ class HtmlTest extends MediaWikiTestCase {
 				'GREEN',
 			) ) )
 		);
+	}
+
+	/**
+	 * @covers Html::expandAttributes
+	 * @expectedException MWException
+	 */
+	public function testExpandAttributes_ArrayOnNonListValueAttribute_ThrowsException() {
+		// Real-life test case found in the Popups extension (see Gerrit cf0fd64),
+		// when used with an outdated BetaFeatures extension (see Gerrit deda1e7)
+		Html::expandAttributes( array( 'src' => array( 'ltr' => 'ltr.svg', 'rtl' => 'rtl.svg' ) ) );
 	}
 
 	/**
@@ -663,5 +701,11 @@ class HtmlTest extends MediaWikiTestCase {
 				'Allow special case "step=any".'
 			)
 		);
+	}
+}
+
+class HtmlAttributeValue {
+	function __toString() {
+		return 'stringValue';
 	}
 }
