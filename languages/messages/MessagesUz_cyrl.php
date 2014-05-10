b70bb522082739dba029aab4578f45ad2f17e532
robin <robinp.1273@gmail.com>
2014-05-10 22:55:10 +0200

    Add missing uz-latn and uz-cyrl
    I once added a language converter for Uzbek, without adding the names because
    there were no separate localisations. However, they are needed (with
    fallbacks). Localisation can remain at 'uz' for now, or forever.
    
    The language codes 'uz-cyrl' and 'uz-latn' are already available in
    Names.php since 85d6495c9. This change allows to use these language
    codes user interface language and shows the user interface in 'uz'
    instead of in 'en'.
    
    Bug: T44397
    Change-Id: Ie7b00c9cb78b6db4542bee2a7ac489270ac3554c

---
+++ languages/messages/MessagesUz_cyrl.php
@@ -0,0 +1,11 @@
+<?php
+/** Uzbek (Cyrillic script) (ўзбекча)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'uz';
---
+++ languages/messages/MessagesUz_latn.php
@@ -0,0 +1,11 @@
+<?php
+/** Uzbek (Latin script) (oʻzbekcha)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'uz';
