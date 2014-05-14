d5d9e8b1166126f6c8c606714558176048b0590e
Timo Tijhof <krinklemail@gmail.com>
2014-05-14 15:45:27 +0200

    [WIP] Add mw.track calls for jquery.migrate
    Can't use mw.log.deprecate because many of these are fired
    for specific behaviour within a method, not the method access
    entirely.
    
    And mustn't fire the events with topic "mw.deprecate" we can't
    give it a search string to find in offending code. I've added
    some generic keys that will make it easier to group these in
    the event logging infrastructure, but unless we start passing
    complex regexes, there is no way we can pass it something that
    mw.inspect.grep in the WikimediaEvents extension can find. These
    tracking hits would be for hit counts only, not for finding where
    it comes from.
    
    Change-Id: I6c868f2ce831d74889ed58b6ab4a69b233b3923f

--- resources/Resources.php
+++ resources/Resources.php
@@ -191,7 +191,7 @@ return array(
 
 	'jquery' => array(
 		'scripts' => 'resources/lib/jquery/jquery.js',
-		'debugScripts' => 'resources/lib/jquery/jquery.migrate.js',
+		'debugScripts' => 'resources/src/jquery/jquery.migrate.js',
 		'debugRaw' => false,
 		'targets' => array( 'desktop', 'mobile' ),
 	),
