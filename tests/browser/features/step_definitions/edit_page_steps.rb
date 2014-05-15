ed830657fdfa5b5d9d2550a056364703fd5603c7
cmcmahon <cmcmahon@wikimedia.org>
2014-05-15 10:40:01 -0700

    WIP: browser acceptance tests
    Apropos of https://www.mediawiki.org/wiki/Wikimedia_Release_and_QA_Team/Quarterly_review,_April_2014#Action_items
    
    Tests pass on a bare wiki install, beta enwiki, beta dewiki, beta hewiki
    
    Change-Id: I2b4bfa512a33ff31cc0369dfd9ec19cb875d5e36

---
+++ tests/browser/features/step_definitions/edit_page_steps.rb
@@ -0,0 +1,16 @@
+When(/^I click Edit$/) do
+  on(MainPage).edit_link
+end
+
+When(/^I edit the page with "(.*?)"$/) do |edit_content|
+  on(EditPage).edit_page_content_element.send_keys(edit_content + @random_string)
+end
+
+When(/^I save the edit$/) do
+  on(EditPage).save_button
+end
+
+Then(/^the edited page content should contain "(.*?)"$/) do |content|
+  on(MainPage).page_content.should match(content+@random_string)
+end
+
