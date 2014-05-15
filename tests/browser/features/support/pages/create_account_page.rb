eb290e201ed8fac469a47a50e840387a0f9e858a
cmcmahon <cmcmahon@wikimedia.org>
2014-05-15 10:40:01 -0700

    browser acceptance tests
    Apropos of https://www.mediawiki.org/wiki/Wikimedia_Release_and_QA_Team/Quarterly_review,_April_2014#Action_items
    
    Tests pass on a bare wiki install, beta enwiki, beta dewiki, beta hewiki
    
    Bug: 66272
    
    Ported create_account.feature and supporting files from /qa/browsertest without needing updates
    Change-Id: I2b4bfa512a33ff31cc0369dfd9ec19cb875d5e36

---
+++ tests/browser/features/support/pages/create_account_page.rb
@@ -0,0 +1,19 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+class CreateAccountPage
+  include PageObject
+
+  include URL
+  page_url URL.url("<%=params[:page_title]%>")
+
+  button(:create_account, id: "wpCreateaccount")
+end
