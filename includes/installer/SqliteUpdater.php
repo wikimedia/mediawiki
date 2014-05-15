a36f633e9ada717816d5bac80fdf8af7ade048e9
withoutaname <drevitchi@gmail.com>
2014-05-14 22:31:37 -0700

    Updater: Create user_password_expires before doMigrateUserOptions
    To avoid undefined property errors, create the column before any
    User objects might be created. This affected the Mysql and Sqlite
    updaters.
    
    Bug: 63677
    Change-Id: I26822d97f40e9b853bac5171e7c16fc1b32c648c

--- includes/installer/SqliteUpdater.php
+++ includes/installer/SqliteUpdater.php
@@ -78,6 +78,8 @@ class SqliteUpdater extends DatabaseUpdater {
 
 			// 1.19
 			array( 'addIndex', 'logging', 'type_action', 'patch-logging-type-action-index.sql' ),
+			// user_password_expires is from 1.23, but must happen before doMigrateUserOptions (bug 63677)
+			array( 'addField', 'user', 'user_password_expires', 'patch-user_password_expire.sql' ),
 			array( 'doMigrateUserOptions' ),
 			array( 'dropField', 'user', 'user_options', 'patch-drop-user_options.sql' ),
 			array( 'addField', 'revision', 'rev_sha1', 'patch-rev_sha1.sql' ),
@@ -127,7 +129,7 @@ class SqliteUpdater extends DatabaseUpdater {
 				'patch-logging_user_text_type_time_index.sql' ),
 			array( 'addIndex', 'logging', 'log_user_text_time', 'patch-logging_user_text_time_index.sql' ),
 			array( 'addField', 'page', 'page_links_updated', 'patch-page_links_updated.sql' ),
-			array( 'addField', 'user', 'user_password_expires', 'patch-user_password_expire.sql' ),
+			// user_password_expires update moved to 1.19 section
 
 			// 1.24
 			array( 'addField', 'page_props', 'pp_sortkey', 'patch-pp_sortkey.sql' ),
