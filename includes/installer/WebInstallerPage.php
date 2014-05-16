35d7c5d1b0543084ae9b795af9ec827862ef6948
Jackmcbarn <jackmcbarn@gmail.com>
2014-05-16 18:58:03 -0400

    Automatically copy the LocalSettings.php file to the desired location
    Bug: 60534
    Change-Id: Ibba052ae7c89fb2266b90dce84894e7d3d4c0463

--- includes/installer/WebInstallerPage.php
+++ includes/installer/WebInstallerPage.php
@@ -1383,32 +1383,55 @@ class WebInstallerInstall extends WebInstallerPage {
 class WebInstallerComplete extends WebInstallerPage {
 
 	public function execute() {
+		//tries to automatically copy the LocalSettings.php file
+		//to the desired location.
+		$ls = InstallerOverrides::getLocalSettingsGenerator( $this->parent );
+		$rightsProfile = $this->parent->rightsProfiles[$this->parent->getVar( '_RightsProfile' )];
+		foreach ( $rightsProfile as $group => $rightsArr ) {
+			$ls->setGroupRights( $group, $rightsArr );
+		}
+		$fp = fopen("LocalSettings.php","w") ;
+		$written=fwrite($fp,$ls->getText()) ;
+		fclose($fp) ;
+
+		if($written === false) {
 		// Pop up a dialog box, to make it difficult for the user to forget
 		// to download the file
-		$lsUrl = $this->getVar( 'wgServer' ) . $this->parent->getURL( array( 'localsettings' => 1 ) );
-		if ( isset( $_SERVER['HTTP_USER_AGENT'] ) &&
-			strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false
-		) {
-			// JS appears to be the only method that works consistently with IE7+
-			$this->addHtml( "\n<script>jQuery( function () { document.location = " .
-				Xml::encodeJsVar( $lsUrl ) . "; } );</script>\n" );
-		} else {
-			$this->parent->request->response()->header( "Refresh: 0;url=$lsUrl" );
+		    $lsUrl = $this->getVar( 'wgServer' ) . $this->parent->getURL( array( 'localsettings' => 1 ) );
+		    if ( isset( $_SERVER['HTTP_USER_AGENT'] ) &&
+			    strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false
+		    ) {
+			    // JS appears to be the only method that works consistently with IE7+
+			    $this->addHtml( "\n<script>jQuery( function () { document.location = " .
+				    Xml::encodeJsVar( $lsUrl ) . "; } );</script>\n" );
+		    } else {
+			    $this->parent->request->response()->header( "Refresh: 0;url=$lsUrl" );
+		    }
 		}
-
 		$this->startForm();
 		$this->parent->disableLinkPopups();
-		$this->addHTML(
-			$this->parent->getInfoBox(
-				wfMessage( 'config-install-done',
-					$lsUrl,
-					$this->getVar( 'wgServer' ) .
-					$this->getVar( 'wgScriptPath' ) . '/index' .
-					$this->getVar( 'wgScriptExtension' ),
-					'<downloadlink/>'
-				)->plain(), 'tick-32.png'
-			)
-		);
+		$wikilink = $this->getVar( 'wgServer' ) . $this->getVar( 'wgScriptPath' )
+			    . '/index' . $this->getVar( 'wgScriptExtension' );
+		if($written === false) {
+		    $this->addHTML(
+			    $this->parent->getInfoBox(
+				    wfMessage( 'config-install-done',
+					    $lsUrl,
+					    $wikilink,
+					    '<downloadlink/>'
+				    )->plain(), 'tick-32.png'
+			    )
+		    );
+		}
+		else {
+		    $this->addHTML(
+			    $this->parent->getInfoBox(
+				    wfMessage( 'config-install-done-copied',
+					    $wikilink
+				    )->plain(), 'tick-32.png'
+			    )
+		    );
+		}
 		$this->addHTML( $this->parent->getInfoBox(
 			wfMessage( 'config-extension-link' )->text() ) );
 
