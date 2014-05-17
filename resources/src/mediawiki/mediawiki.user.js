de5e13598a79b3876de3902f5985a3bf0bd0d6a0
Timo Tijhof <krinklemail@gmail.com>
2014-05-17 02:54:13 +0200

    mediawiki.user: Use mw.log.deprecate to track user() and anonymous()
    Follows-up I5970be9e859358 which deprecated these orignally.
    
    Also remove obsolete tests now that they're linked by reference.
    
    Change-Id: I559efa8a61de9f7b600c7b74edd5a56fb0a33b00

--- resources/src/mediawiki/mediawiki.user.js
+++ resources/src/mediawiki/mediawiki.user.js
@@ -89,14 +89,6 @@
 		},
 
 		/**
-		 * @inheritdoc #getName
-		 * @deprecated since 1.20 Use #getName instead
-		 */
-		name: function () {
-			return user.getName();
-		},
-
-		/**
 		 * Get date user registered, if available
 		 *
 		 * @return {Date|boolean|null} Date user registered, or false for anonymous users, or
@@ -122,14 +114,6 @@
 		 */
 		isAnon: function () {
 			return user.getName() === null;
-		},
-
-		/**
-		 * @inheritdoc #isAnon
-		 * @deprecated since 1.20 Use #isAnon instead
-		 */
-		anonymous: function () {
-			return user.isAnon();
 		},
 
 		/**
@@ -257,5 +241,19 @@
 			return getUserInfo( 'rights', callback );
 		}
 	};
+
+	/**
+	 * @method name
+	 * @inheritdoc #getName
+	 * @deprecated since 1.20 Use #getName instead
+	 */
+	mw.log.deprecate( user, 'name', user.getName );
+
+	/**
+	 * @method anonymous
+	 * @inheritdoc #isAnon
+	 * @deprecated since 1.20 Use #isAnon instead
+	 */
+	mw.log.deprecate( user, 'anonymous', user.isAnon );
 
 }( mediaWiki, jQuery ) );
