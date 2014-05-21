5048f30f69a0f11e52178cb966f8f8dd4198e642
physikerwelt <wiki@physikerwelt.de>
2014-05-21 11:38:56 +0000

    Fix: numFields wrong for sqlite
    Currently the first row of the result
    is used to return the number of fields.
    This fix corrects that calculation, which has returned
    twice the number of fields before. The reason for that
    was that the resulting data array has the following form:
    array() {
     [0] => value_0
     ["field_name0"] =>value_0
     [1] => value_1
     ["field_name1"] =>value_1
    ...
    Furthermore the constant 0 (int) is returned if the result
    set is empty. This issue should be corrected in the future,
    since the number or fields should be answered in a metadata context
    independent of the data stored in the relation.
    
    Bug: 65578
    Change-Id: I399c7c857dcbd774cc4eb6102bbfe8a8764b07c9

--- tests/phpunit/includes/db/DatabaseSqliteTest.php
+++ tests/phpunit/includes/db/DatabaseSqliteTest.php
@@ -431,4 +431,22 @@ class DatabaseSqliteTest extends MediaWikiTestCase {
 		$row = $res->fetchRow();
 		$this->assertFalse( (bool)$row['a'] );
 	}
+
+	/**
+	 * @covers DatabaseSqlite::numFields
+	 */
+	public function testNumFields() {
+		$db = new DatabaseSqliteStandalone( ':memory:' );
+
+		$databaseCreation = $db->query( 'CREATE TABLE a ( a_1 )', __METHOD__ );
+		$this->assertInstanceOf( 'ResultWrapper', $databaseCreation, "Failed to create table a" );
+		$res = $db->select( 'a' , '*');
+		$this->assertEquals( 0,  $db->numFields($res), "expects to get 0 fields for an empty table" );
+		$insertion = $db->insert( 'a', array( 'a_1' => 10 ), __METHOD__ );
+		$this->assertTrue( $insertion, "Insertion failed" );
+		$res = $db->select( 'a' , '*');
+		$this->assertEquals( 1,  $db->numFields($res), "wrong number of fields" );
+
+		$this->assertTrue( $db->close(), "closing database" );
+	}
 }
