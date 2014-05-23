9640ba7fd3475e12f8a7f75c24d118ff7a666312
Gilles Dubuc <gdubuc@wikimedia.org>
2014-05-23 11:52:57 +0200

    [WIP] Generate thumbnails based on buckets
    Change-Id: I285d56b2024c81365247338f85c1e0aa708cb21e
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/600

---
+++ tests/phpunit/includes/filerepo/file/FileTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FileTest {
+
+class FooFile extends \File { };
+
+class FileTest extends \MediaWikiTestCase {
+	/**
+	 * @covers File::getThumbnailBucket
+	 */
+	public function testGetThumbnailBucket() {
+		global $wgThumbnailBuckets;
+
+		$wgThumbnailBuckets = array( 256, 512, 1024, 2048, 4096 );
+
+		$stub = $this->getMock( '\FileTest\FooFile', array( 'getWidth' ), array( 'Foo', false ) );
+
+		$stub->expects( $this->any() )->method( 'getWidth' )->will( $this->returnValue( 3000 ) );
+
+		$this->assertEquals( 256, $stub->getThumbnailBucket( 120 ) );
+		$this->assertEquals( 512, $stub->getThumbnailBucket( 300 ) );
+		$this->assertEquals( 2048, $stub->getThumbnailBucket( 1024 ) );
+		$this->assertEquals( false, $stub->getThumbnailBucket( 2048 ) );
+		$this->assertEquals( false, $stub->getThumbnailBucket( 3500 ) );
+
+		$wgThumbnailBuckets = null;
+
+		$this->assertEquals( false, $stub->getThumbnailBucket( 1024 ) );
+	}
+}
+
+}
