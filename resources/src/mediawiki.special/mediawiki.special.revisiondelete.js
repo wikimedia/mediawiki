f2c79fa1fba773f0244d35474c675ef3476a83a9
umherirrender <umherirrender_de.wp@web.de>
2014-05-24 13:28:09 +0200

    Add dynamic maxlength to Special:RevisionDelete
    A reason on Special:RevisionDelete can have two parts, one of the scroll
    down box and a free text field. The free text field is limited to 100
    bytes. This patch makes that dynamic to respect also the text from the
    scroll down box and allows so a higher bytelimit up to 255 bytes.
    
    Bug: 34467
    Change-Id: I3b21f66f84d68777a18e66e4e5f59886e55d7936

---
+++ resources/src/mediawiki.special/mediawiki.special.revisiondelete.js
@@ -0,0 +1,28 @@
+/*!
+ * JavaScript for Special:RevisionDelete
+ */
+( function ( mw, $ ) {
+	// Add a dynamic byte limit to the reason field
+	// When the reason list is changed an the bytelimit
+	// is longer than the allowed, nothing is done
+	var reasonList = $( '#wpRevDeleteReasonList' ),
+		// cache the current selection to avoid expensive lookup
+		currentValReasonList = reasonList.val(),
+		// cache the separator to avoid object creation on each keypress
+		colonSeparator = mw.message( 'colon-separator' ).plain();
+
+	reasonList.change( function() {
+		currentValReasonList = reasonList.val();
+	});
+
+	$( '#wpReason' ).byteLimit( 255, function( input ) {
+		if( currentValReasonList === 'other' || !currentValReasonList ) {
+			return input;
+		} else if( input ) {
+			return currentValReasonList + colonSeparator + input;
+		} else {
+			return currentValReasonList;
+		}
+	});
+
+}( mediaWiki, jQuery ) );
