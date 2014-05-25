c53c1c9d8e7b7321b50c61afb04e33959ddde989
Kunal Grover <kunalgrover05@gmail.com>
2014-05-25 20:08:50 +0530

    First version of Page Language selector
    Special page PageLanguage to set the page language of a page.
    Permissions not assigned, need to set for 'pagelang'.
    To enable feature, set $wgPageLanguageUseDB as true.
    
    Bug: 35489
    Change-Id: I0f82b146fbe948f917c1c5d29f7469644d797e80

--- resources/Resources.php
+++ resources/Resources.php
@@ -1242,6 +1242,9 @@ return array(
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.movePage.js',
 		'dependencies' => 'jquery.byteLimit',
 	),
+	'mediawiki.special.pageLanguage' => array(
+		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.pageLanguage.js',
+	),
 	'mediawiki.special.pagesWithProp' => array(
 		'styles' => 'resources/src/mediawiki.special/mediawiki.special.pagesWithProp.css',
 	),
