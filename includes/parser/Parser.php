885dd5e98275473c2b4f1a2c92f8d687f0205e2e
Jackmcbarn <jackmcbarn@gmail.com>
2014-05-28 16:17:41 -0400

    Add PPFrame::getTTL() and setTTL()
    Add functions to frames to control the TTL of their output, and expose
    this via expandtemplates in the API.
    
    Bug: 49803
    Change-Id: I412febf3469503bf4839fb1ef4dca098a8c79457

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -622,9 +622,10 @@ class Parser {
 	 * @param Title $title
 	 * @param ParserOptions $options
 	 * @param int|null $revid
+	 * @param bool|PPFrame $frame
 	 * @return mixed|string
 	 */
-	function preprocess( $text, Title $title = null, ParserOptions $options, $revid = null ) {
+	function preprocess( $text, Title $title = null, ParserOptions $options, $revid = null, $frame = false ) {
 		wfProfileIn( __METHOD__ );
 		$magicScopeVariable = $this->lock();
 		$this->startParse( $title, $options, self::OT_PREPROCESS, true );
@@ -633,7 +634,7 @@ class Parser {
 		}
 		wfRunHooks( 'ParserBeforeStrip', array( &$this, &$text, &$this->mStripState ) );
 		wfRunHooks( 'ParserAfterStrip', array( &$this, &$text, &$this->mStripState ) );
-		$text = $this->replaceVariables( $text );
+		$text = $this->replaceVariables( $text, $frame );
 		$text = $this->mStripState->unstripBoth( $text );
 		wfProfileOut( __METHOD__ );
 		return $text;
