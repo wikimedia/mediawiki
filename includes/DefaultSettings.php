e9c4a374f48c7180e02fab10d3bc5988ec33e5f7
mglaser <glaser@hallowelt.biz>
2014-05-28 10:19:00 +0200

    Updated release notes and version number for MediaWiki 1.19.16
    This is MediaWiki 1.19.16 security release
    
    Change-Id: I0bf5466b91eb771f0db4259fbaf5225b5d67ce53

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.15';
+$wgVersion = '1.19.16';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
