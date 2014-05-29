37de1652b7f3f3b20d04e23a082398ffc92a0dc9
Jackmcbarn <jackmcbarn@gmail.com>
2014-05-29 12:55:40 -0400

    Add a tracking category for duplicate arguments
    If a page accidentally duplicates an argument, such as
    {{foo|bar=1|bar=2}} or {{foo|bar|1=baz}}, add it to a tracking category.
    
    Change-Id: I3b6eeff8b51859bc7af0ea985f6f7528c2e9d220

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3754,6 +3754,7 @@ $wgNamespacesWithSubpages = array(
 $wgTrackingCategories = array(
 	'index-category',
 	'noindex-category',
+	'duplicate-args-category',
 	'expensive-parserfunction-category',
 	'post-expand-template-argument-category',
 	'post-expand-template-inclusion-category',
