32e95ead09b45971b690150fca71436f5d05c1c4
James D. Forrester <jforrester@wikimedia.org>
2014-05-31 16:01:41 -0700

    Vector: Label the more actions menu "More", not "Actions"
    Vector has a consistent way of showing most actions – as tabs, or, if
    there are too many or the action is unimportant, shunting them into an
    additional actions menu. This menu was problematically labelled "Actions"
    which though correct is misleading as it is merely /some/ actions,
    alongside the generally-promoted "Read", "Edit" and "View history". The
    recent change I09c8031c makes the label of this menu much more prominent
    (as it is no longer hidden), which makes the labelling issue more acute.
    
    This menu could have many labels, and this is merely a suggestion. Some
    alternatives:
    
     – "Tools" — However, this conflicts with the page tools shown in the
       left-hand nav, which although arguably should be moved into this menu
       is an argument for another day, as it would notably alter the skin for
       readers who otherwise don't get this menu at all.
    
     – "Tasks" — This feels more like a term that would be used for an
       individual, or perhaps for the article itself, to denote a worklist –
       a list of outstanding issues to be ticked off ("Orphaned article";
       "Needs wikifying"; "Copyedit"; etc.) rather than a set of task actions
       you can do as atomic steps to the page.
    
     – "More actions" — This is rather wordy and ideally should be
       unnecessary in the minds of editors who should (through the consistent
       use of simple verbs as action labels) already recognise each of the
       tab links along the top-right of the navigation to be page-
       specific actions.
    
    Bug: 44591
    Change-Id: I8697beab5695198c0acddcf9ad8eeca189660c94

--- skins/Vector.php
+++ skins/Vector.php
@@ -564,7 +564,7 @@ class VectorTemplate extends BaseTemplate {
 						echo ' emptyPortlet';
 					}
 					?>" aria-labelledby="p-cactions-label">
-						<h3 id="p-cactions-label"><span><?php $this->msg( 'actions' ) ?></span><a href="#"></a></h3>
+						<h3 id="p-cactions-label"><span><?php $this->msg( 'vector-more-actions' ) ?></span><a href="#"></a></h3>
 
 						<div class="menu">
 							<ul<?php $this->html( 'userlangattributes' ) ?>>
