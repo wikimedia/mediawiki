c7391af0e98ffee75e09974dd27c00f2a4fa3247
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-06-02 18:34:52 +0000

    PrefixSearch (ApiOpenSearch) now supports searching in multiple namespaces
    I thought there was just an issue with capitalization, but in fact the code
    explicitly only searched one namespace anyway. Fixed that while taking
    capitalization differences in namespaces into account.
    
    Follows-up I3487bb69.
    
    Bug: 65752
    Bug: 30323
    Change-Id: I4bec7b5548fc27ac51a1b4d4961c3bbc31eb7337

--- tests/phpunit/MediaWikiTestCase.php
+++ tests/phpunit/MediaWikiTestCase.php
@@ -454,7 +454,7 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 
 		User::resetIdByNameCache();
 
-		//Make sysop user
+		// Make sysop user
 		$user = User::newFromName( 'UTSysop' );
 
 		if ( $user->idForName() == 0 ) {
@@ -466,7 +466,7 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 			$user->saveSettings();
 		}
 
-		//Make 1 page with 1 revision
+		// Make 1 page with 1 revision
 		$page = WikiPage::factory( Title::newFromText( 'UTPage' ) );
 		if ( $page->getId() == 0 ) {
 			$page->doEditContent(
@@ -474,7 +474,8 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 				'UTPageSummary',
 				EDIT_NEW,
 				false,
-				User::newFromName( 'UTSysop' ) );
+				User::newFromName( 'UTSysop' )
+			);
 		}
 	}
 
