d52fa43891f422b2ba515f3bfebabf41afc63e4e
mglaser <glaser@hallowelt.biz>
2014-06-02 23:20:43 +0200

    Updated release notes and version number for MediaWiki 1.23.0-rc.3
    This is MediaWiki 1.23.0-rc.3 release candidate.
    
    Change-Id: Iad21d8fbd0bfc3f84e000eda649ca12bcd3b0568

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.23.0-rc.2';
+$wgVersion = '1.23.0-rc.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
