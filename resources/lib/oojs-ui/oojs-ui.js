025da4ca1f537009b391136ac50f60bd393a5af3
James D. Forrester <jforrester@wikimedia.org>
2014-06-09 16:46:18 -0700

    Update OOjs UI to v0.1.0-pre (15f4156bac)
    New changes:
    15f4156 Follow-up 2334482: actually clear items in clearItems()
    
    Change-Id: I7d4570364690b82a6fb7de3beb7e7e68ff451366

--- resources/lib/oojs-ui/oojs-ui.js
+++ resources/lib/oojs-ui/oojs-ui.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (098f84f8a0)
+ * OOjs UI v0.1.0-pre (15f4156bac)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: Mon Jun 09 2014 14:32:26 GMT-0700 (PDT)
+ * Date: Mon Jun 09 2014 16:46:13 GMT-0700 (PDT)
  */
 ( function ( OO ) {
 
@@ -2702,6 +2702,7 @@ OO.ui.GroupElement.prototype.clearItems = function () {
 		item.$element.detach();
 	}
 
+	this.items = [];
 	return this;
 };
 /**
