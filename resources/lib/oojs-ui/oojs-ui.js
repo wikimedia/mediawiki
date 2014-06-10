4bfd6135781b6d65a5ace5c06436b4500064ddc7
Trevor Parscal <trevorparscal@gmail.com>
2014-06-10 11:03:43 -0700

    Update OOjs UI to v0.1.0-pre (99068355d6)
    New changes:
    9906835 Make menu toolgroup take up less space and look more like a menu
    
    Change-Id: Ie5e5f3d8f3f6bccdbacd85f1ea8423bfeaa8e2f2

--- resources/lib/oojs-ui/oojs-ui.js
+++ resources/lib/oojs-ui/oojs-ui.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (15f4156bac)
+ * OOjs UI v0.1.0-pre (99068355d6)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: Mon Jun 09 2014 16:46:13 GMT-0700 (PDT)
+ * Date: Tue Jun 10 2014 11:03:38 GMT-0700 (PDT)
  */
 ( function ( OO ) {
 
