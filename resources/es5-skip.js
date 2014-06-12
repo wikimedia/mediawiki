426a7b60afdb63e8a8475bf6e364701b2faa80a3
James D. Forrester <jforrester@wikimedia.org>
2014-06-12 16:19:46 -0700

    resources: Add es5-shim library v3.4.0 with skip function and use it for OOjs, OOjs UI
    The ES5 skip function simply tests for 'use strict' working, which means that all MSIE9
    clients will recieve the library, which should mostly be a no-op but will increase the
    load.
    
    Change-Id: I2556b39d6040d99693f6b9d469b92951ffb2e7b2

---
+++ resources/es5-skip.js
@@ -0,0 +1,16 @@
+/*!
+ * MediaWiki ES5 feature detection shim skip function.
+ *
+ * Test for strict mode as a proxy for full ES5 function support (but not syntax)
+ * Per http://kangax.github.io/compat-table/es5/ this is a reasonable short-cut
+ * that still allows this to be as short as possible (there are no function "No"s
+ * for non-"obsolete" real browsers where strict support is available).
+ *
+ * Note that this will cause IE9 users to get the shim (which should be close to
+ * a no-op but will increase page payload).
+ *
+ * @copyright 2014 James Forrester, Timo Tijhof and others
+ * @license The MIT License (MIT)
+ */
+
+return ( function () { "use strict"; return !this; }() );
