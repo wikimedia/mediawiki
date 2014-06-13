51e4edd4abe598710b1951b5d1d1c1a3e08f60d9
umherirrender <umherirrender_de.wp@web.de>
2014-06-13 21:16:37 +0200

    Add SpecialPagesWithProp::prefixSearchSubpages
    This shows the existing props as subpages on search suggestion
    
    Change-Id: I3d35474bcf840a6feede8079642d83074874c6f3

--- includes/specials/SpecialPagesWithProp.php
+++ includes/specials/SpecialPagesWithProp.php
@@ -30,6 +30,7 @@
  */
 class SpecialPagesWithProp extends QueryPage {
 	private $propName = null;
+	private $existingPropNames = null;
 
 	function __construct( $name = 'PagesWithProp' ) {
 		parent::__construct( $name );
@@ -47,18 +48,7 @@ class SpecialPagesWithProp extends QueryPage {
 		$request = $this->getRequest();
 		$propname = $request->getVal( 'propname', $par );
 
-		$dbr = wfGetDB( DB_SLAVE );
-		$res = $dbr->select(
-			'page_props',
-			'pp_propname',
-			'',
-			__METHOD__,
-			array( 'DISTINCT', 'ORDER BY' => 'pp_propname' )
-		);
-		$propnames = array();
-		foreach ( $res as $row ) {
-			$propnames[$row->pp_propname] = $row->pp_propname;
-		}
+		$propnames = $this->getExistingPropNames();
 
 		$form = new HTMLForm( array(
 			'propname' => array(
@@ -86,6 +76,18 @@ class SpecialPagesWithProp extends QueryPage {
 	public function onSubmit( $data, $form ) {
 		$this->propName = $data['propname'];
 		parent::execute( $data['propname'] );
+	}
+
+	/**
+	 * Return an array of subpages beginning with $search that this special page will accept.
+	 *
+	 * @param string $search Prefix to search for
+	 * @param integer $limit Maximum number of results to return
+	 * @return string[] Matching subpages
+	 */
+	public function prefixSearchSubpages( $search, $limit = 10 ) {
+		$subpages = array_keys( $this->getExistingPropNames() );
+		return self::prefixSearchArray( $search, $limit, $subpages );
 	}
 
 	/**
@@ -148,6 +150,25 @@ class SpecialPagesWithProp extends QueryPage {
 		}
 
 		return $ret;
+	}
+
+	public function getExistingPropNames() {
+		if ( $this->existingPropNames === null ) {
+			$dbr = wfGetDB( DB_SLAVE );
+			$res = $dbr->select(
+				'page_props',
+				'pp_propname',
+				'',
+				__METHOD__,
+				array( 'DISTINCT', 'ORDER BY' => 'pp_propname' )
+			);
+			$propnames = array();
+			foreach ( $res as $row ) {
+				$propnames[$row->pp_propname] = $row->pp_propname;
+			}
+			$this->existingPropNames = $propnames;
+		}
+		return $this->existingPropNames;
 	}
 
 	protected function getGroupName() {
