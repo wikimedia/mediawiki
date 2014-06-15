f7c32468519561bdd1a774f50e6cd72bc314f9a2
umherirrender <umherirrender_de.wp@web.de>
2014-06-15 11:53:31 +0200

    Migrate 'wgBreakFrames' from wikibits.js to mediawiki.page.ready
    This module now contains the same code as in wikibits.js.
    
    For cached entries the new code works automatically since both
    legacy wikibits and mediawiki.page.ready are already present in
    the load() queue of existing pages.
    
    Change-Id: I18b068fbcf4ba0646fb07313c3d9676cc76a0ad5

--- skins/common/wikibits.js
+++ skins/common/wikibits.js
@@ -8,15 +8,6 @@
 		isIE6 = ( /msie ([0-9]{1,}[\.0-9]{0,})/.exec( ua ) && parseFloat( RegExp.$1 ) <= 6.0 ),
 		onloadFuncts = [];
 
-if ( mw.config.get( 'wgBreakFrames' ) ) {
-	// Note: In IE < 9 strict comparison to window is non-standard (the standard didn't exist yet)
-	// it works only comparing to window.self or window.window (http://stackoverflow.com/q/4850978/319266)
-	if ( win.top !== win.self ) {
-		// Un-trap us from framesets
-		win.top.location = win.location;
-	}
-}
-
 /**
  * User-agent sniffing.
  *
