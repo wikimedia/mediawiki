5b9bfe46b435c402caa929d893b7123b0dd0ff39
umherirrender <umherirrender_de.wp@web.de>
2014-06-15 11:53:31 +0200

    Migrate 'wgBreakFrames' from wikibits.js to own module
    This patch creates the new top module 'mediawiki.breakframes'.
    This module contains the same code as in wikibits.js, but the config
    variable will go, because the server side can determine if module is
    needed and only load it than.
    
    Change-Id: I18b068fbcf4ba0646fb07313c3d9676cc76a0ad5

---
+++ resources/src/mediawiki/mediawiki.breakframes.js
@@ -0,0 +1,12 @@
+/*!
+ * JavaScript to break out of framesets.
+ */
+( function () {
+	var win = windows;
+	// Note: In IE < 9 strict comparison to window is non-standard (the standard didn't exist yet)
+	// it works only comparing to window.self or window.window (http://stackoverflow.com/q/4850978/319266)
+	if ( win.top !== win.self ) {
+		// Un-trap us from framesets
+		win.top.location = win.location;
+	}
+}() );
