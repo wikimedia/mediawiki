f34625dad17ec98b8f10df735125b26c820a4f23
Timo Tijhof <krinklemail@gmail.com>
2014-06-18 22:20:37 +0200

    Add json2.js polyfill (v2014-02-04; with module skip function)
    Source from https://github.com/douglascrockford/JSON-js/blob/3d7767b/json2.js
    
    * Deprecate jquery.json which is a little dated and less efficient
      because it always downloads the entire polyfill to the client
      even if it isn't needed. We also can't add a skip function to
      jquery.json because it exposes a different interface ($.toJSON
      instead of JSON.stringify).
    
    * Update 'mediawiki.inspect' to use 'json' instead.
    
    * Update 'jquery.jStorage' to use 'json' instead (doesn't need
      a change to its code as it looks for either JSON.stringify or
      $.toJSON by design).
    
    Change-Id: I8978be3491b08fbbfd83f1306e816b1b20f16161

--- resources/Resources.php
+++ resources/Resources.php
@@ -272,7 +272,11 @@ return array(
 		'scripts' => 'resources/lib/jquery/jquery.hoverIntent.js',
 	),
 	'jquery.json' => array(
-		'scripts' => 'resources/lib/jquery/jquery.json.js',
+		// @deprecated since 1.24: Use the 'json' module and global JSON object instead.
+		'scripts' => array(
+			'resources/lib/jquery/jquery.json.js',
+			'resources/src/jquery.json-deprecate.js',
+		),
 		'targets' => array( 'mobile', 'desktop' ),
 	),
 	'jquery.localize' => array(
@@ -316,7 +320,7 @@ return array(
 	),
 	'jquery.jStorage' => array(
 		'scripts' => 'resources/lib/jquery/jquery.jStorage.js',
-		'dependencies' => 'jquery.json',
+		'dependencies' => 'json',
 	),
 	'jquery.suggestions' => array(
 		'scripts' => 'resources/src/jquery/jquery.suggestions.js',
@@ -716,6 +720,13 @@ return array(
 		'group' => 'jquery.ui',
 	),
 
+	/* json2 */
+
+	'json' => array(
+		'scripts' => 'resources/lib/json2/json2.js',
+		'skipFunction' => 'resources/src/json-skip.js',
+	),
+
 	/* Moment.js */
 
 	'moment' => array(
@@ -909,7 +920,7 @@ return array(
 		'scripts' => 'resources/src/mediawiki/mediawiki.inspect.js',
 		'dependencies' => array(
 			'jquery.byteLength',
-			'jquery.json',
+			'json',
 		),
 		'targets' => array( 'desktop', 'mobile' ),
 	),
---
+++ resources/src/json-skip.js
@@ -0,0 +1,4 @@
+/*!
+ * Skip function for json2.js.
+ */
+return !!( window.JSON && JSON.stringify && JSON.parse );
