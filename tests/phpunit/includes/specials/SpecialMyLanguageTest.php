81656ae456755fc05b948814ec6b111541a3519e
Max Semenik <maxsem.wiki@gmail.com>
2014-06-19 11:36:56 -0700

    Integrate Special:MyLanguage from E:Translate
    Bug: 66762
    Change-Id: I520077c931431b5919e0208f75c20b5b25f3159d

---
+++ tests/phpunit/includes/specials/SpecialMyLanguageTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @group Database
+ * @covers SpecialMyLanguage
+ */
+class SpecialMyLanguageTest extends MediaWikiTestCase {
+	public function addDBData() {
+		$titles = array(
+			'Page/Another',
+			'Page/Another/ru',
+		);
+		foreach ( $titles as $title ) {
+			$page = WikiPage::factory( Title::newFromText( $title ) );
+			if ( $page->getId() == 0 ) {
+				$page->doEditContent(
+					new WikitextContent( 'UTContent' ),
+					'UTPageSummary',
+					EDIT_NEW,
+					false,
+					User::newFromName( 'UTSysop' ) );
+			}
+		}
+	}
+
+	/**
+	 * @covers SpecialMyLanguage::findTitle
+	 * @dataProvider provideFindTitle
+	 * @param $expected
+	 * @param $subpage
+	 * @param $langCode
+	 * @param $userLang
+	 */
+	public function testFindTitle( $expected, $subpage, $langCode, $userLang ) {
+		$this->setMwGlobals( 'wgLanguageCode', $langCode );
+		$special = new SpecialMyLanguage();
+		$special->getContext()->setLanguage( $userLang );
+		// Test with subpages both enabled and disabled
+		$this->mergeMwGlobalArrayValue( 'wgNamespacesWithSubpages', array( NS_MAIN => true ) );
+		$this->assertTitle( $expected, $special->findTitle( $subpage ) );
+		$this->mergeMwGlobalArrayValue( 'wgNamespacesWithSubpages', array( NS_MAIN => false ) );
+		$this->assertTitle( $expected, $special->findTitle( $subpage ) );
+	}
+
+	/**
+	 * @param string $expected
+	 * @param Title|null $title
+	 */
+	private function assertTitle( $expected, $title ) {
+		if ( $title ) {
+			$title = $title->getPrefixedText();
+		}
+		$this->assertEquals( $expected, $title );
+	}
+
+	public function provideFindTitle() {
+		return array(
+			array( null, '::Fail', 'en', 'en' ),
+			array( 'Page/Another', 'Page/Another/en', 'en', 'en' ),
+			array( 'Page/Another', 'Page/Another', 'en', 'en' ),
+			array( 'Page/Another/ru', 'Page/Another', 'en', 'ru' ),
+			array( 'Page/Another', 'Page/Another', 'en', 'es' ),
+		);
+	}
+}
