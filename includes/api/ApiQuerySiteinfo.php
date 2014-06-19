4d80031c400545195ffa2026e8bd7836a1f2be62
This, that and the other <at.light@live.com.au>
2014-06-19 11:20:18 +1000

    Allow interlanguage link prefixes that are not language codes
    $wgExtraInterlanguageLinkPrefixes holds a list of interwiki prefixes to be
    treated as language codes if $wgInterwikiMagic is true.
    
    To set the display text for the interlanguage links generated by this
    code, you need to create MediaWiki:Interlanguage-link-foo, where "foo" is
    the interwiki prefix.  To provide a friendly site name for the link title
    text, use MediaWiki:Interlanguage-link-sitename-foo.  On the WMF cluster,
    these messages could be set using the WikimediaMessages extension.
    
    Information about extra language links (in the site language only) is
    provided via the API in meta=siteinfo&prop=interwikimap.
    
    Bug: 32189
    Change-Id: I3d04760e2d9fb3320bb71e3d5ad115eed54a899c

--- includes/api/ApiQuerySiteinfo.php
+++ includes/api/ApiQuerySiteinfo.php
@@ -370,6 +370,8 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 	}
 
 	protected function appendInterwikiMap( $property, $filter ) {
+		global $wgExtraInterlanguageLinkPrefixes;
+
 		$local = null;
 		if ( $filter === 'local' ) {
 			$local = 1;
@@ -396,9 +398,24 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 			if ( $row['iw_trans'] == '1' ) {
 				$val['trans'] = '';
 			}
+
 			if ( isset( $langNames[$prefix] ) ) {
 				$val['language'] = $langNames[$prefix];
 			}
+			if ( in_array( $prefix, $wgExtraInterlanguageLinkPrefixes ) ) {
+				$val['extralanglink'] = '';
+
+				$linktext = wfMessage( "interlanguage-link-$prefix" );
+				if ( !$linktext->isDisabled() ) {
+					$val['linktext'] = $linktext->text();
+				}
+
+				$sitename = wfMessage( "interlanguage-link-sitename-$prefix" );
+				if ( !$sitename->isDisabled() ) {
+					$val['sitename'] = $sitename->text();
+				}
+			}
+
 			$val['url'] = wfExpandUrl( $row['iw_url'], PROTO_CURRENT );
 			if (substr( $row['iw_url'], 0, 2) == '//') {
 				$val['protorel'] = '';
