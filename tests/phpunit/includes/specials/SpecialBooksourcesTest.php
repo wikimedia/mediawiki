0ed21066aafeb516f1d751f75673a58f14bece68
Kevin Israel <pleasestand@live.com>
2014-06-24 03:14:24 -0400

    Special:BookSources: Correct validation of ISBNs containing X
    PHP's "equal" (==) operator considers the integer 0 to be equal to
    the string 'X', and when 'X' is converted to a number, it becomes 0.
    Neither is desired here.
    
    * Fail when an X is encountered while calculating the check digit.
      (X can only occur as the check digit of an ISBN-10.)
    * Fixed the check digit comparisons by adding an explicit string cast.
    * Used the "identical" operator to make it more obvious that no type
      juggling should take place during the comparisons.
    * Added some test cases.
    * Removed an outdated TODO.
    
    Bug: 67021
    Change-Id: I85f53c41f403a60340e9441757fe66b9764e623c

---
+++ tests/phpunit/includes/specials/SpecialBooksourcesTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @covers SpecialBooksources
+ */
+class SpecialBooksourcesTest extends MediaWikiTestCase {
+	public static function provideISBNs() {
+		return array(
+			// [[en:Wikipedia:ISBN]]
+			array( '978-0-300-14424-6', true ),
+			array( '0-14-020652-3', true ),
+			array( '020652-3', false ),
+			array( '9781234567897', true ),
+			array( '1-4133-0454-0', true ),
+			array( '978-1413304541', true ),
+
+			// http://www.cis.upenn.edu/~cis110/11fa/hw/hw04/index.html
+			array( '0136091814', true ),
+			array( '0136091812', false ),
+			array( '9780136091813', true ),
+			array( '9780136091817', false ),
+			array( '123456789X', true ),
+
+			// Bug 67021
+			array( '1413304541', false ),
+			array( '141330454X', false ),
+			array( '1413304540', true ),
+			array( '14133X4540', false ),
+			array( '97814133X4541', false ),
+			array( '978035642615X', false ),
+			array( '9781413304541', true ),
+			array( '9780356426150', true ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideISBNs
+	 */
+	public function testIsValidISBN( $isbn, $isValid ) {
+		$this->assertSame( $isValid, SpecialBooksources::isValidISBN( $isbn ) );
+	}
+}
