wikimedia/mediawiki

commit 3846d1048766a7a7ba0e5727fb84e464dc1cbb98

Be stricter for file types where we don't know canonical extension
Previously if a file had a format, where we didn't have an
extension associated with it in mime.types, people could upload
it with any extension that is in $wgFileExtensions. This meant
people could upload a non-allowed file type if it had an allowed
extension, and the non-allowed file type didn't have a canonical
extension in mime.types

Bug: 39012
Change-Id: Ib373fafdfceceed65fbd23cf468f3c19196545c9

includes/upload/UploadBase.php
@@ -940,8 +940,13 @@ abstract class UploadBase {
 		$match = $magic->isMatchingExtension( $extension, $mime );
 
 		if ( $match === null ) {
-			wfDebug( __METHOD__ . ": no file extension known for mime type $mime, passing file\n" );
-			return true;
+			if ( $magic->getTypesForExtension( $extension ) !== null ) {
+				wfDebug( __METHOD__ . ": No extension known for $mime, but we know a mime for $extension\n" );
+				return false;
+			} else {
+				wfDebug( __METHOD__ . ": no file extension known for mime type $mime, passing file\n" );
+				return true;
+			}
 		} elseif ( $match === true ) {
 			wfDebug( __METHOD__ . ": mime type $mime matches extension $extension, passing file\n" );
 

RELEASE-NOTES-1.22
@@ -263,6 +263,9 @@ production.
   adding a new topic on a page
 * (bug 41756) Improve treatment of multiple comments on a blank line.
 * (bug 51064) Purge upstream caches when deleting file assets.
+* (bug 39012) File types with a mime that we do not know the extension for
+  can no longer be uploaded as an extension that we do know the mime type
+  for.
 
 === API changes in 1.22 ===
 * (bug 25553) The JSON output formatter now leaves forward slashes unescaped

commit 418417675078667d4c91e31e3160cfbd66fb31c5

Fix mime detection of easily-confused-with text/plain formats
json, csv, and tsv are often detected as text/plain. However that's
not right. This patch causes MediaWiki to look at the file extension
of files detected as text/plain, and if the file extension is
for a "textual" type, use the mime type associated with that extension.

This change also changes the "does mime type match uploaded file
extension" check to use the mime based on the file contents
plus extension, as opposed to just the file contents. Various
documentation suggests this is more appropriate (e.g. line 807
of MimeMagic.php). In my opinion we should use just the file
contents when verifying file is not on blacklist, but use ext
when verifying file type matches extension, and for decided
what handler specific checks to run. Not the detect mime type
with extension doesn't override the detected mime type with
the extension, but only uses the extension if content based
detection is ambigious or not specific enough.

This patch should be reviewed by csteipp before merge for
any potential security implications.

Note: This is partially fixing a regression from 3846d1048766a7,
where previously csv and json files were allowed to be uploaded,
and that change prevented them

Bug: 66036
Bug: 45424
Change-Id: Ib637fe6850a81b26f84dc8c00ab4772f3d3a1f34

includes/MimeMagic.php
@@ -518,6 +518,12 @@ class MimeMagic {
 					".$ext is not a known OPC extension.\n" );
 				$mime = 'application/zip';
 			}
+		} elseif ( $mime === 'text/plain' && $this->findMediaType( ".$ext" ) === MEDIATYPE_TEXT ) {
+			// Textual types are sometimes not recognized properly.
+			// If detected as text/plain, and has an extension which is textual
+			// improve to the extension's type. For example, csv and json are often
+			// misdetected as text/plain.
+			$mime = $this->guessTypesForExtension( $ext );
 		}
 
 		if ( isset( $this->mMimeTypeAliases[$mime] ) ) {

includes/mime.info
@@ -65,6 +65,9 @@ text/plain	[TEXT]
 text/html application/xhtml+xml	[TEXT]
 application/xml text/xml	[TEXT]
 text	[TEXT]
+application/json	[TEXT]
+text/csv	[TEXT]
+text/tab-separated-values	[TEXT]
 
 application/zip application/x-zip	[ARCHIVE]
 application/x-gzip	[ARCHIVE]

includes/mime.types
@@ -35,6 +35,7 @@ application/x-gzip gz
 application/x-hdf hdf
 application/x-jar jar
 application/x-javascript js
+application/json json
 application/x-koan skp skd skt skm
 application/x-latex latex
 application/x-netcdf nc cdf
@@ -109,6 +110,7 @@ model/mesh msh mesh silo
 model/vrml wrl vrml
 text/calendar ics ifb
 text/css css
+text/csv csv
 text/html html htm
 text/plain txt
 text/richtext rtx

includes/upload/UploadBase.php
@@ -435,7 +435,7 @@ abstract class UploadBase {
 		}
 
 		$this->mFileProps = FSFile::getPropsFromPath( $this->mTempPath, $this->mFinalExtension );
-		$mime = $this->mFileProps['file-mime'];
+		$mime = $this->mFileProps['mime'];
 
 		if ( $wgVerifyMimeType ) {
 			# XXX: Missing extension will be caught by validateName() via getTitle()

tests/phpunit/includes/MimeMagicTest.php
@@ -1,0 +1,39 @@
+<?php
+class MimeMagicTest extends MediaWikiTestCase {
+
+	/** @var MimeMagic */
+	private $mimeMagic;
+
+	function setUp() {
+		$this->mimeMagic = MimeMagic::singleton();
+		parent::setUp();
+	}
+
+	/**
+	 * @dataProvider providerImproveTypeFromExtension
+	 * @param $ext String File extension (no leading dot)
+	 * @param $oldMime Initially detected mime
+	 * @param $finalMime Mime type after taking extension into account
+	 */
+	function testImproveTypeFromExtension( $ext, $oldMime, $finalMime ) {
+		$actualMime = $this->mimeMagic->improveTypeFromExtension( $oldMime, $ext );
+		$this->assertEquals( $finalMime, $actualMime );
+	}
+
+	function providerImproveTypeFromExtension() {
+		return array(
+			array( 'gif', 'image/gif', 'image/gif' ),
+			array( 'gif', 'unknown/unknown', 'unknown/unknown' ),
+			array( 'wrl', 'unknown/unknown', 'model/vrml' ),
+			array( 'txt', 'text/plain', 'text/plain' ),
+			array( 'csv', 'text/plain', 'text/csv' ),
+			array( 'tsv', 'text/plain', 'text/tab-separated-values' ),
+			array( 'json', 'text/plain', 'application/json' ),
+			array( 'foo', 'application/x-opc+zip', 'application/zip' ),
+			array( 'docx', 'application/x-opc+zip', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ),
+			array( 'djvu', 'image/x-djvu', 'image/vnd.djvu' ),
+			array( 'wav', 'audio/wav', 'audio/wav' ),
+		);
+	}
+
+}