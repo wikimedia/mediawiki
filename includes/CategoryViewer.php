e79535a582ea9ba22e6fb5e1d5ab13dc7dae38e4
Aaron Schulz <aschulz@wikimedia.org>
2014-06-24 18:07:55 -0700

    Avoid begin/commit in Category::getCountMessage()
    Change-Id: I64e4a859a9adf2930265e282f59816c82e1c3070

--- includes/CategoryViewer.php
+++ includes/CategoryViewer.php
@@ -707,7 +707,10 @@ class CategoryViewer extends ContextSource {
 			// to refresh the incorrect category table entry -- which should be
 			// quick due to the small number of entries.
 			$totalcnt = $rescnt;
-			$this->cat->refreshCounts();
+			$category = $this->cat;
+			wfGetDB( DB_MASTER )->onTransactionIdle( function() use ( $category ) {
+				$category->refreshCounts();
+			} );
 		} else {
 			// Case 3: hopeless.  Don't give a total count at all.
 			// Messages: category-subcat-count-limited, category-article-count-limited,
