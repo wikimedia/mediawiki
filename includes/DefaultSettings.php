d6cd076c9f9ea904cffeb8433a1309216d237f02
mglaser <glaser@hallowelt.biz>
2014-06-25 22:22:29 +0200

    Updated release notes and version number to 1.23.1
    This is MediaWiki 1.23.1 security and maintenance release.
    
    Change-Id: I66d92d53262f54699c8d4c43434481eb328d8e18

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.23.0';
+$wgVersion = '1.23.1';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
