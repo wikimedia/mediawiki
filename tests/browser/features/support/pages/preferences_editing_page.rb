4c6069d5ed48c9536e0e204ff5843046ea1a62f8
Cmcmahon <cmcmahon@wikimedia.org>
2014-06-26 10:57:10 -0700

    Port preferences test from /qa repo
    This works in any English wiki, including a fresh install.
    In order to work in any language wiki, it needs
    https://gerrit.wikimedia.org/r/#/c/142292/ to be
    merged and released.
    
    Also may need to change preferences_appearances_page.rb
    once the change to mediawiki_selenium is merged to be
    language-independent.
    
    Change-Id: I1ba4e2c53a9e1bdb7b832efec4c2c8f6f64d2b86

---
+++ tests/browser/features/support/pages/preferences_editing_page.rb
@@ -0,0 +1,28 @@
+#
+# This file is subject to the license terms in the LICENSE file found in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/LICENSE. No part of
+# qa-browsertests, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+#
+# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
+# qa-browsertests top-level directory and at
+# https://git.wikimedia.org/blob/qa%2Fbrowsertests/HEAD/CREDITS
+#
+class PreferencesEditingPage
+  include PageObject
+
+  include URL
+  page_url URL.url("Special:Preferences#mw-prefsection-rendering")
+
+  select_list(:edit_area_font_style_select, id: "mw-input-wpeditfont")
+  checkbox(:edit_section_double_click_check, id: "mw-input-wpeditondblclick")
+  checkbox(:edit_section_edit_link, id: "mw-input-wpeditsectiononrightclick")
+  checkbox(:edit_section_right_click_check, id: "mw-input-wpeditsectiononrightclick")
+  checkbox(:forced_edit_summary_check, id: "mw-input-wpforceeditsummary")
+  checkbox(:live_preview_check, id: "mw-input-wpuselivepreview")
+  checkbox(:preview_on_first_check, id: "mw-input-wppreviewonfirst")
+  checkbox(:preview_on_top_check, id: "mw-input-wppreviewontop")
+  checkbox(:show_edit_toolbar_check, id: "mw-input-wpshowtoolbar")
+  checkbox(:unsaved_changes_check, id: "mw-input-wpuseeditwarning")
+end
