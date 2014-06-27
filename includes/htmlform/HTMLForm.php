c9bd1a2f4e006fa6961c3e81eaa562d3859ca3b2
Brad Jorsch <bjorsch@wikimedia.org>
2014-06-27 16:16:59 -0400

    Add HTMLAutoCompleteSelectField
    This is much like the one OAuth has in
    Special:OAuthConsumerRegistration/propose, except it stores the
    autocompletion options in a data property rather than a global and uses
    jquery.suggestions rather than jquery.ui.autocomplete.
    
    Change-Id: I42473cea75f3706cc0125167f9191275ca6cb3b0

--- includes/htmlform/HTMLForm.php
+++ includes/htmlform/HTMLForm.php
@@ -118,6 +118,7 @@ class HTMLForm extends ContextSource {
 		'edittools' => 'HTMLEditTools',
 		'checkmatrix' => 'HTMLCheckMatrix',
 		'cloner' => 'HTMLFormFieldCloner',
+		'autocompleteselect' => 'HTMLAutoCompleteSelectField',
 		// HTMLTextField will output the correct type="" attribute automagically.
 		// There are about four zillion other HTML5 input types, like range, but
 		// we don't use those at the moment, so no point in adding all of them.
