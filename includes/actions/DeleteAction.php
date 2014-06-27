d39fec1e70c9c56161fb8d1d6a1c02c62c298c42
withoutaname <drevitchi@gmail.com>
2014-06-27 11:56:56 -0700

    Move deletion-specific code to DeleteAction.php
    Essentially make DeleteAction independent rather than relying
    on Article.php for all the code. Unfortunately, it won't be a
    completely seamless transition; some code had to be modified
    to fit the HTMLForm scheme, and some hooks were removed (but
    also replaced by other hooks in FormAction::getForm() ).
    
    Change-Id: I5e067ea49ce1d3d638a3d2e0fb04cd9823d0a2a4

--- includes/actions/DeleteAction.php
+++ includes/actions/DeleteAction.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * Handle page deletion
- *
- * Copyright © 2012 Timo Tijhof
+ * User interface/form for deleting pages.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,28 +18,250 @@
  *
  * @file
  * @ingroup Actions
- * @author Timo Tijhof
  */
 
 /**
- * Handle page deletion
- *
- * This is a wrapper that will call Article::delete().
+ * User interface/form for deleting pages.
  *
  * @ingroup Actions
  */
-class DeleteAction extends FormlessAction {
+class DeleteAction extends FormAction {
+
+	/**
+	 * @var string Oldimage parameter in URL, if specified.
+	 */
+	protected $oldimage;
+
+	public function __construct( Page $page, IContextSource $context ) {
+		parent::__construct( $page, $context );
+		$this->oldimage = $this->getRequest()->getVal( 'oldimage' );
+	}
+
+	public function getFormFields() {
+		$fields = array();
+		if ( $this->oldimage ) {
+			$fields['OldFile'] = array(
+				'default' => $this->oldimage,
+				'id' => 'wpOldFile',
+				'type' => 'hidden',
+			);
+		}
+		$fields['Reason'] = array(
+			'autofocus' => true,
+			'cssclass' => 'wpReasonDropDown',
+			'id' => 'wpReason',
+			'label-message' => 'deletecomment',
+			'maxlength' => 255,
+			'options-message' => 'deletereason-dropdown',
+			'section' => 'legend',
+			'size' => 60,
+			'type' => 'selectandother',
+		);
+		if ( !$this->getUser()->isAnon() && $this->getUser()->isAllowed( 'editmywatchlist' ) ) {
+			$watchitem = WatchedItem::fromUserTitle(
+				$this->getUser(),
+				$this->getTitle(),
+				WatchedItem::CHECK_USER_RIGHTS
+			);
+			$checkWatch = $this->getUser()->getBoolOption( 'watchdeletion' ) || $watchitem->isWatched();
+			$fields['Watch'] = array(
+				'default' => $checkWatch,
+				'id' => 'wpWatch',
+				'label-message' => 'watchthis',
+				'section' => 'legend',
+				'type' => 'check',
+			);
+		}
+		if ( $this->getUser()->isAllowed( 'suppressrevision' ) ) {
+			$fields['Suppress'] = array(
+				'default' => false,
+				'id' => 'wpSuppress',
+				'label-message' => 'revdelete-suppress',
+				'section' => 'legend',
+				'type' => 'check',
+			);
+		}
+		return $fields;
+	}
 
 	public function getName() {
 		return 'delete';
 	}
 
-	public function onView() {
-		return null;
+	public function getRestriction() {
+		return 'delete';
 	}
 
-	public function show() {
+	public function onSubmit( $data ) {
+		$reason = $data['Reason'][0];
+		$watchitem = WatchedItem::fromUserTitle(
+			$this->getUser(),
+			$this->getTitle(),
+			WatchedItem::CHECK_USER_RIGHTS
+		);
+		if ( isset( $data['Watch'] ) && $data['Watch'] && !$watchitem->isWatched() ) {
+			$watchitem->addWatch();
+			$this->getUser()->invalidateCache();
+		} elseif ( !$data['Watch'] && $watchitem->isWatched() ) {
+			$watchitem->removeWatch();
+			$this->getUser()->invalidateCache();
+		}
+		$suppress = false;
+		if ( isset( $data['Suppress'] ) && $data['Suppress'] ) {
+			$suppress = true;
+		}
+		$dbw = wfGetDB( DB_MASTER );
+		$articleWasDeleted = false;
+		$fileWasDeleted = false;
+		$file = wfLocalFile( $this->getTitle() );
+		if ( $file && $file->exists() ) {
+			$logtype = $suppress ? 'suppress' : 'delete';
+			$logEntry = new ManualLogEntry( $logtype, 'delete' );
+			$logEntry->setPerformer( $this->getUser() );
+			$logEntry->setTarget( $this->getTitle() );
+			if ( isset( $data['OldFile'] ) && $data['OldFile'] ) {
+				try {
+					$reason = $this->msg( 'deletedrevision', $data['OldFile'] )->inContentLanguage()->text() .
+						$this->msg( 'colon-separator' )->inContentLanguage()->text() . trim( $reason );
+					$fileStatus = $file->deleteOld( $data['OldFile'], $reason, $suppress, $this->getUser() );
+				} catch ( MWException $e ) {
+					$dbw->rollback( __METHOD__ );
+					throw $e;
+				}
+			} else {
+				try {
+					$fileStatus = $file->delete( $reason, $suppress, $this->getUser() );
+				} catch ( MWException $e ) {
+					$dbw->rollback( __METHOD__ );
+					throw $e;
+				}
+			}
+			$status = $fileStatus;
+			$fileWasDeleted = true;
+		}
+		if ( $this->page->exists() ) {
+			/**
+			 * @todo Use getWikiPage() when Action is converted into a ContextSource subclass
+			 * @todo FIXME: Workaround pass-by-reference bug for WikiPage::doDeleteArticleReal()
+			 */
+			$articleStatus = $this->page->doDeleteArticleReal( $reason, $suppress, 0, true );
+			$status = $fileWasDeleted ? $articleStatus->merge( $fileStatus ) : $articleStatus;
+			$articleWasDeleted = true;
+		}
+		if ( !$status ) {
+			$status = Status::newFatal( 'cannotdelete', $this->getTitle() );
+		}
+		if ( $status->isGood() ) {
+			if ( $fileWasDeleted && !$articleWasDeleted ) {
+				$logEntry->setComment( $reason );
+				$logid = $logEntry->insert();
+				$logEntry->publish( $logid );
+			}
+			return true;
+		} else {
+			$dbw->rollback( __METHOD__ );
+			return $status->getErrorsArray();
+		}
+	}
 
-		$this->page->delete();
+	public function onSuccess() {
+		$output = $this->getOutput();
+		$deleted = $this->getTitle()->getPrefixedText();
+		$output->setPageTitle( $this->msg( 'actioncomplete' ) );
+		$output->setRobotPolicy( 'noindex,nofollow' );
+		$output->addWikiMsg( 'deletedtext', wfEscapeWikiText( $deleted ),
+			Linker::linkKnown(
+				SpecialPage::getSafeTitleFor( 'Log', 'delete' ),
+				$this->msg( 'deletionlog' )->parse()
+			)
+		);
+		$output->returnToMain();
+	}
+
+	public function preText() {
+		$form = '';
+		$title = $this->getTitle();
+		if ( $this->oldimage ) {
+			$oldfile = OldLocalFile::newFromArchiveName(
+				$title,
+				RepoGroup::singleton()->getLocalRepo(),
+				$this->oldimage
+			);
+			$form .= $this->msg( 'filedelete-intro-old',
+				$title,
+				$this->getLanguage()->date( $oldfile->getTimestamp(), true ),
+				$this->getLanguage()->time( $oldfile->getTimestamp(), true ),
+				wfExpandUrl( wfLocalFile( $title )->getArchiveUrl( $this->oldimage ), null )
+			)->parseAsBlock();
+			return $form;
+		}
+		$revisions = $title->estimateRevisionCount();
+		if ( $revisions > 1 ) {
+			$form .= Html::rawElement( 'strong', array( 'class' => 'mw-delete-warning-revisions' ),
+				$this->msg( 'historywarning' )->numParams( $revisions )->parse() .
+				$this->msg( 'word-separator' )->plain() .
+				Linker::linkKnown(
+					$title,
+					$this->msg( 'history' )->escaped(),
+					array( 'rel' => 'archives' ),
+					array( 'action' => 'history' )
+				)
+			);
+			if ( $title->isBigDeletion() ) {
+				global $wgDeleteRevisionsLimit;
+				$form .= Html::element( 'div', array( 'class' => 'error' ),
+					"\n" .
+					$this->msg( 'delete-warning-toobig' )->numParams( $wgDeleteRevisionsLimit )->parse() .
+					"\n"
+				) .
+				"\n";
+			}
+		}
+		$backlinkCache = $title->getBacklinkCache();
+		if ( $backlinkCache->hasLinks( 'pagelinks' ) || $backlinkCache->hasLinks( 'templatelinks' ) ) {
+			$form .= Html::element( 'div', array( 'class' => 'mw-warning plainlinks' ),
+				"\n" .
+				$this->msg( 'deleting-backlinks-warning' )->parse() .
+				"\n"
+			) .
+			"\n";
+		}
+		$form .= $this->msg( 'confirmdeletetext' )->parseAsBlock();
+		return $form;
+	}
+
+	public function postText() {
+		$form = '';
+		if ( $this->getUser()->isAllowed( 'editinterface' ) ) {
+			$mwtitle = Title::makeTitle( NS_MEDIAWIKI, 'Deletereason-dropdown' );
+			$form .= Html::rawElement( 'p', array( 'class' => 'mw-delete-editreasons' ),
+				Linker::link(
+					$mwtitle,
+					$this->msg( 'delete-edit-reasonlist' )->escaped(),
+					array(),
+					array( 'action' => 'edit' )
+				)
+			);
+		}
+		$deleteLogPage = new LogPage( 'delete' );
+		$form .= Xml::element( 'h2', null, $deleteLogPage->getName()->text() );
+		$html = '';
+		LogEventsList::showLogExtract( $html, 'delete', $this->getTitle() );
+		$form .= $html;
+		return $form;
+	}
+
+	public function alterForm( HTMLForm $form ) {
+		$form->setSubmitID( 'wpConfirmB' );
+		$form->setSubmitName( 'wpConfirmB' );
+		$form->setSubmitText( $this->msg( 'deletepage' )->text() );
+	}
+
+	public function setHeaders() {
+		$output = $this->getOutput();
+		$output->setRobotPolicy( 'noindex,nofollow' );
+		$output->setPageTitle( $this->msg( 'delete-confirm', $this->getTitle()->getPrefixedText() ) );
+		$output->addBacklinkSubtitle( $this->getTitle() );
+		$output->setArticleRelated( true );
 	}
 }
