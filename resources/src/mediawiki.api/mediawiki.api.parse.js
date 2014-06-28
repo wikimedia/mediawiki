d5229821e2019d0dc29844144648277c42caf3a9
Bartosz Dziewoński <matma.rex@gmail.com>
2014-06-28 13:44:19 +0200

    mediawiki.api: Remove deprecated function parameters
    * 'ok' and 'err' parameters to various functions.
      Tracked with mw.track() as 'api.cbParam'.
      Deprecated since MW 1.20, printing console warnings since MW 1.23.
    * 'async' parameter to mediawiki.api.category #getCategories.
      Tracked with mw.track() as 'api.async'.
      Deprecated and printing console warnings since MW 1.23.
    
    Change-Id: I0a650fdb4affd394ae419e21d54baf790116f6f7

--- resources/src/mediawiki.api/mediawiki.api.parse.js
+++ resources/src/mediawiki.api/mediawiki.api.parse.js
@@ -8,31 +8,21 @@
 		 * Convenience method for 'action=parse'.
 		 *
 		 * @param {string} wikitext
-		 * @param {Function} [ok] Success callback (deprecated)
-		 * @param {Function} [err] Error callback (deprecated)
 		 * @return {jQuery.Promise}
 		 * @return {Function} return.done
 		 * @return {string} return.done.data Parsed HTML of `wikitext`.
 		 */
-		parse: function ( wikitext, ok, err ) {
+		parse: function ( wikitext ) {
 			var apiPromise = this.get( {
 				action: 'parse',
 				contentmodel: 'wikitext',
 				text: wikitext
 			} );
 
-			// Backwards compatibility (< MW 1.20)
-			if ( ok || err ) {
-				mw.track( 'mw.deprecate', 'api.cbParam' );
-				mw.log.warn( 'Use of mediawiki.api callback params is deprecated. Use the Promise instead.' );
-			}
-
 			return apiPromise
 				.then( function ( data ) {
 					return data.parse.text['*'];
 				} )
-				.done( ok )
-				.fail( err )
 				.promise( { abort: apiPromise.abort } );
 		}
 	} );
