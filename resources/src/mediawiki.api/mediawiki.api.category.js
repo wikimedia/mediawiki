cd08cceefe8dfafce46ada3b9db933330d62762a
Bartosz Dziewoński <matma.rex@gmail.com>
2014-06-28 13:44:19 +0200

    mediawiki.api: Remove deprecated function parameters
    * 'ok' and 'err' parameters to various functions.
      Tracked with mw.track( 'mw.deprecate' ) as 'api.cbParam'.
      Deprecated since MW 1.20, printing console warnings since MW 1.23.
    * 'async' parameter to mediawiki.api.category #getCategories.
      Tracked with mw.track( 'mw.deprecate' ) as 'api.async'.
      Deprecated and printing console warnings since MW 1.23.
    
    Also removed is mw.Api#normalizeAjaxOptions, which is no longer needed
    and which has never been used by anything outside mediawiki.api module.
    
    Announced in https://www.mail-archive.com/wikitech-l%40lists.wikimedia.org/msg78108.html
    
    Change-Id: I0a650fdb4affd394ae419e21d54baf790116f6f7

--- resources/src/mediawiki.api/mediawiki.api.category.js
+++ resources/src/mediawiki.api/mediawiki.api.category.js
@@ -3,28 +3,20 @@
  */
 ( function ( mw, $ ) {
 
-	var msg = 'Use of mediawiki.api callback params is deprecated. Use the Promise instead.';
 	$.extend( mw.Api.prototype, {
 		/**
 		 * Determine if a category exists.
 		 *
 		 * @param {mw.Title|string} title
-		 * @param {Function} [ok] Success callback (deprecated)
-		 * @param {Function} [err] Error callback (deprecated)
 		 * @return {jQuery.Promise}
 		 * @return {Function} return.done
 		 * @return {boolean} return.done.isCategory Whether the category exists.
 		 */
-		isCategory: function ( title, ok, err ) {
+		isCategory: function ( title ) {
 			var apiPromise = this.get( {
 				prop: 'categoryinfo',
 				titles: String( title )
 			} );
-
-			if ( ok || err ) {
-				mw.track( 'mw.deprecate', 'api.cbParam' );
-				mw.log.warn( msg );
-			}
 
 			return apiPromise
 				.then( function ( data ) {
@@ -38,8 +30,6 @@
 					}
 					return exists;
 				} )
-				.done( ok )
-				.fail( err )
 				.promise( { abort: apiPromise.abort } );
 		},
 
@@ -49,24 +39,17 @@
 		 * E.g. given "Foo", return "Food", "Foolish people", "Foosball tables"...
 		 *
 		 * @param {string} prefix Prefix to match.
-		 * @param {Function} [ok] Success callback (deprecated)
-		 * @param {Function} [err] Error callback (deprecated)
 		 * @return {jQuery.Promise}
 		 * @return {Function} return.done
 		 * @return {string[]} return.done.categories Matched categories
 		 */
-		getCategoriesByPrefix: function ( prefix, ok, err ) {
+		getCategoriesByPrefix: function ( prefix ) {
 			// Fetch with allpages to only get categories that have a corresponding description page.
 			var apiPromise = this.get( {
 				list: 'allpages',
 				apprefix: prefix,
 				apnamespace: mw.config.get( 'wgNamespaceIds' ).category
 			} );
-
-			if ( ok || err ) {
-				mw.track( 'mw.deprecate', 'api.cbParam' );
-				mw.log.warn( msg );
-			}
 
 			return apiPromise
 				.then( function ( data ) {
@@ -78,8 +61,6 @@
 					}
 					return texts;
 				} )
-				.done( ok )
-				.fail( err )
 				.promise( { abort: apiPromise.abort } );
 		},
 
@@ -87,33 +68,16 @@
 		 * Get the categories that a particular page on the wiki belongs to.
 		 *
 		 * @param {mw.Title|string} title
-		 * @param {Function} [ok] Success callback (deprecated)
-		 * @param {Function} [err] Error callback (deprecated)
-		 * @param {boolean} [async=true] Asynchronousness (deprecated)
 		 * @return {jQuery.Promise}
 		 * @return {Function} return.done
 		 * @return {boolean|mw.Title[]} return.done.categories List of category titles or false
 		 *  if title was not found.
 		 */
-		getCategories: function ( title, ok, err, async ) {
+		getCategories: function ( title ) {
 			var apiPromise = this.get( {
 				prop: 'categories',
 				titles: String( title )
-			}, {
-				async: async === undefined ? true : async
 			} );
-
-			if ( ok || err ) {
-				mw.track( 'mw.deprecate', 'api.cbParam' );
-				mw.log.warn( msg );
-			}
-			if ( async !== undefined ) {
-				mw.track( 'mw.deprecate', 'api.async' );
-				mw.log.warn(
-					'Use of mediawiki.api async=false param is deprecated. ' +
-					'The sychronous mode will be removed in the future.'
-				);
-			}
 
 			return apiPromise
 				.then( function ( data ) {
@@ -132,8 +96,6 @@
 					}
 					return titles;
 				} )
-				.done( ok )
-				.fail( err )
 				.promise( { abort: apiPromise.abort } );
 		}
 	} );
