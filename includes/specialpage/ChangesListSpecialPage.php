08fee4ce2f1bbf362c8422a3e306a905c8775094
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-06-30 16:48:08 +0200

    Fix deprecated hooks not having a non-deprecated alternative
    The implementation in ChangesListSpecialPage::doMainQuery() is
    dead code. It's never executed because the relevant subclasses
    override the method completely without ever calling
    parent::doMainQuery().
    
    This means that the deprecation of the hooks in the subclasses
    leaves us with no alternative. Which is a real problem in
    Wikibase, see I27716275e966147ee26d81d8ce3f14951937e718.
    
    The same deprecation in ChangesListSpecialPage::getCustomFilters()
    works because all suclasses I'm aware of call the parent method.
    But please note that extensions may still break if they do this
    different. I can not checked them all.
    
    If you want to be sure you should just revert patch
    I9cceda5d2dcfc53c852c5682c466b48ad8f31202 that introduced this
    non-functional hook. I'm sorry to say that but I wonder how this
    passed review.
    
    Change-Id: I7ba3ea64cb145c06011a856e5b56399da4f42339

--- includes/specialpage/ChangesListSpecialPage.php
+++ includes/specialpage/ChangesListSpecialPage.php
@@ -290,8 +290,8 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 			''
 		);
 
-		if ( !wfRunHooks( 'ChangesListSpecialPageQuery',
-			array( $this->getName(), &$tables, &$fields, &$conds, &$query_options, &$join_conds, $opts ) )
+		if ( !$this->runMainQueryHook( $tables, $fields, $conds, $query_options, $join_conds,
+			$opts )
 		) {
 			return false;
 		}
@@ -305,6 +305,13 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 			__METHOD__,
 			$query_options,
 			$join_conds
+		);
+	}
+
+	protected function runMainQueryHook( &$tables, &$fields, &$conds, &$query_options, &$join_conds, $opts ) {
+		return wfRunHooks(
+			'ChangesListSpecialPageQuery',
+			array( $this->getName(), &$tables, &$fields, &$conds, &$query_options, &$join_conds, $opts )
 		);
 	}
 
