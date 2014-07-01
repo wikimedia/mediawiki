5c7c36baec8b7621389c2718087aad9d4edeef8d
Cmcmahon <cmcmahon@wikimedia.org>
2014-07-01 12:26:57 -0700

    QA: updates to edit test ported from qa/browsertests
    Change-Id: I2ceae6ee6f98df78c12d257fd6503f5bf6395284

--- tests/browser/features/step_definitions/edit_page_steps.rb
+++ tests/browser/features/step_definitions/edit_page_steps.rb
@@ -2,6 +2,14 @@ When(/^I click Edit$/) do
   on(MainPage).edit_link
 end
 
+When(/^I click Preview$/) do
+  on(EditPage).preview_button
+end
+
+When(/^I click Show Changes$/) do
+  on(EditPage).show_changes_button
+end
+
 When(/^I edit the page with "(.*?)"$/) do |edit_content|
   on(EditPage).edit_page_content_element.send_keys(edit_content + @random_string)
 end
--- tests/browser/features/support/pages/edit_page.rb
+++ tests/browser/features/support/pages/edit_page.rb
@@ -2,6 +2,7 @@ class EditPage
   include PageObject
 
   text_area(:edit_page_content, id: "wpTextbox1")
+  button(:preview_button, id: "wpPreview")
+  button(:show_changes_button, id: "wpDiff")
   button(:save_button, id: "wpSave")
-
 end
