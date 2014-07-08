c63665de4541306e6cebc0e921a09429d4ec2bc6
Bartosz Dziewoński <matma.rex@gmail.com>
2014-07-08 02:50:37 +0200

    On redirects update the URL to that of the target page with JavaScript
    Redirects to sections will now update the URL in browser's address bar
    using the HTML5 History API. When [[Dog]] redirects to [[Animals#Dog]],
    the user will now see "Animals#Dog" in their browser instead of "Dog#Dog".
    
    The target URL is generated server-side to avoid pulling in dependencies
    in the client-side RL module, which is loaded in the top queue.
    
    Browsers that do not support the History API are still supported, the
    way they always were.
    
    Given the following three pages:
    * A: #REDIRECT [[C]]
    * B: #REDIRECT [[C#Section]]
    * C: ==Section== \n ==Other section== \n
    
    The following links should behave as follows:
    * A                →  C
    * B                →  C#Section
    * A#Other_section  →  C#Other_section
    * B#Other_section  →  C#Other_section
    
    The code also supports forwarding query parameters like 'debug=1'.
    
    Bug: 35045
    Bug: 39328
    Change-Id: I9d8d834762e19b836b7e35122b6c4cef0f91b7f0

---
+++ resources/src/mediawiki.action/mediawiki.action.view.redirect.js
@@ -0,0 +1,65 @@
+/*!
+ * JavaScript to update page URL when a redirect is viewed, ensuring that the
+ * page is scrolled to the id when it's a redirect with fragment.
+ *
+ * This is loaded in the top queue, so avoid unnecessary dependencies
+ * like mediawiki.Title or mediawiki.Uri.
+ */
+( function ( mw, $ ) {
+	var profile = $.client.profile(),
+		canonical = mw.config.get( 'wgInternalRedirectTargetUrl' ),
+		fragment = null,
+		shouldChangeFragment, index;
+
+	// Clear internal mw.config entries, so that no one tries to depend on them
+	mw.config.set( 'wgInternalRedirectTargetUrl', null );
+
+	// Deployment hack for compatibility with cached HTML, remove before 1.24 release
+	if ( !canonical ) {
+		canonical = mw.config.get( 'wgRedirectToFragment' );
+	}
+
+	index = canonical.indexOf( '#' );
+	if ( index !== -1 ) {
+		fragment = canonical.slice( index );
+	}
+
+	// Never override the fragment if the user intended to look at a different section
+	shouldChangeFragment = fragment && !location.hash;
+
+	// Replace the whole URL if possible, otherwise just change the fragment
+	if ( canonical && history.replaceState ) {
+		if ( !shouldChangeFragment ) {
+			// If the current page view has a fragment already, don't override it
+			canonical = canonical.replace( /#.*$/, '' );
+			canonical += location.hash;
+		}
+
+		// This will also cause the browser to scroll to given fragment
+		history.replaceState( /*data=*/ history.state, /*title=*/ document.title, /*url=*/ canonical );
+
+	} else if ( shouldChangeFragment ) {
+		if ( profile.layout === 'webkit' && profile.layoutVersion < 420 ) {
+			// Released Safari w/ WebKit 418.9.1 messes up horribly
+			// Nightlies of 420+ are ok
+			return;
+		}
+
+		location.hash = fragment;
+	}
+
+	if ( shouldChangeFragment && profile.layout === 'gecko' ) {
+		// Mozilla needs to wait until after load, otherwise the window doesn't
+		// scroll.  See <https://bugzilla.mozilla.org/show_bug.cgi?id=516293>.
+		// There's no obvious way to detect this programmatically, so we use
+		// version-testing.  If Firefox fixes the bug, they'll jump twice, but
+		// better twice than not at all, so make the fix hit future versions as
+		// well.
+		$( function () {
+			if ( location.hash === fragment ) {
+				location.hash = fragment;
+			}
+		} );
+	}
+
+}( mediaWiki, jQuery ) );
