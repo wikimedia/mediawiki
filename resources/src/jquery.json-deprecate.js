ee8dc79b0709204a4ef90af36e75adeede2012c8
Matthew Flaschen <mflaschen@wikimedia.org>
2014-07-11 16:43:09 -0400

    Note that jquery.json will be removed in 1.25
    This is a pretty straightforward migration, and improves performance
    for users with modern browsers (since the skip function means
    browsers with native JSON don't need to download a JSON module)
    
    Stating a removal version will encourage this migration.
    
    Change-Id: I8fe83f30fd27f7bfe50e756846486abdadb91189

--- resources/src/jquery.json-deprecate.js
+++ resources/src/jquery.json-deprecate.js
@@ -1,4 +1,6 @@
 ( function ( mw, $ ) {
+	// @deprecated Since 1.24.  The 'jquery.json' module will be removed in MW 1.25.  Use the 'json' module.
+
 	mw.log.deprecate( $, 'toJSON', $.toJSON, 'Use JSON.stringify instead (module "json" for polyfill).' );
 	mw.log.deprecate( $, 'evalJSON', $.evalJSON, 'Use JSON.parse instead (module "json" for polyfill).' );
 	mw.log.deprecate( $, 'secureEvalJSON', $.secureEvalJSON, 'Use JSON.parse instead (module "json" for polyfill).' );
