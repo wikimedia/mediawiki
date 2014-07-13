87aa58aa7fda87bf6f5291f4892bdce69bd6e167
Étienne Beaulé <beauleetienne0@gmail.com>
2014-07-12 21:36:37 -0400

    Enable the creation of empty pages
    This change enables the direct creation of empty pages without needing
    to use a work-around (such as "{{subst:ns:0}}"). A warning is added as
    the message "blankarticle" to request confirmation that the empty page
    was meant to be blank.
    
    May possibly fix bug 57238
    
    Bug: 57238
    Bug: 65206
    Change-Id: I3457c36a909d1dbfaeed04a1f0568c69e0ef3386

--- includes/api/ApiEditPage.php
+++ includes/api/ApiEditPage.php
@@ -252,7 +252,8 @@ class ApiEditPage extends ApiBase {
 			'format' => $contentFormat,
 			'model' => $contentHandler->getModelID(),
 			'wpEditToken' => $params['token'],
-			'wpIgnoreBlankSummary' => ''
+			'wpIgnoreBlankSummary' => '',
+			'wpIgnoreBlankArticle' => ''
 		);
 
 		if ( !is_null( $params['summary'] ) ) {
