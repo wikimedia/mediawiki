5dc2717f64bd3f7f37e863ae31e4fd4d08a74eda
umherirrender <umherirrender_de.wp@web.de>
2014-07-17 12:57:07 +0200

    Merge two FileTest.php files
    The correct folder is /file not /files
    
    Follow-Up: Id03e1a0e1c151d3c575a695a42c54b709187d10a
    Change-Id: If3d896aa792e77ba8198b8eb8ad2d874e52584fb

--- tests/phpunit/includes/filerepo/file/FileTest.php
+++ tests/phpunit/includes/filerepo/file/FileTest.php
@@ -1,6 +1,34 @@
 <?php
 
-class FileRepoFileTest extends MediaWikiMediaTestCase {
+class FileTest extends MediaWikiMediaTestCase {
+
+	/**
+	 * @param $filename String
+	 * @param $expected boolean
+	 * @dataProvider providerCanAnimate
+	 */
+	function testCanAnimateThumbIfAppropriate( $filename, $expected ) {
+		$this->setMWGlobals( 'wgMaxAnimatedGifArea', 9000 );
+		$file = $this->dataFile( $filename );
+		$this->assertEquals( $file->canAnimateThumbIfAppropriate(), $expected );
+	}
+
+	function providerCanAnimate() {
+		return array(
+			array( 'nonanimated.gif', true ),
+			array( 'jpeg-comment-utf.jpg', true ),
+			array( 'test.tiff', true ),
+			array( 'Animated_PNG_example_bouncing_beach_ball.png', false ),
+			array( 'greyscale-png.png', true ),
+			array( 'Toll_Texas_1.svg', true ),
+			array( 'LoremIpsum.djvu', true ),
+			array( '80x60-2layers.xcf', true ),
+			array( 'Soccer_ball_animated.svg', false ),
+			array( 'Bishzilla_blink.gif', false ),
+			array( 'animated.gif', true ),
+		);
+	}
+
 	/**
 	 * @dataProvider getThumbnailBucketProvider
 	 * @covers File::getThumbnailBucket
