16d649bbb732bfbc41fec1b7c9d5776c3c35ec5e
Max Semenik <maxsem.wiki@gmail.com>
2014-07-18 13:03:06 -0700

    Fix race condition in wfShellExec()
    Especially when executing commands that return a relatively lot of data in stdout
    quickly, proc_get_status() may return that command has terminated before everything
    has been read from pipes. While originally reported against HHVM, it affects Zend
    too, maybe just to a lesser extent.
    
    I'm not sure if this change can result in deadlocks, posting for discussion.
    
    Bug: 67870
    Change-Id: I050292dbb76821f66a15f937bf3aaf4defe67687

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -2951,12 +2951,6 @@ function wfShellExec( $cmd, &$retval = null, $environ = array(),
 	}
 
 	while ( true ) {
-		$status = proc_get_status( $proc );
-		if ( !$status['running'] ) {
-			break;
-		}
-		$status = false;
-
 		$readyPipes = $pipes;
 
 		// Clear last error
@@ -3008,11 +3002,7 @@ function wfShellExec( $cmd, &$retval = null, $environ = array(),
 		fclose( $pipe );
 	}
 
-	// Use the status previously collected if possible, since proc_get_status()
-	// just calls waitpid() which will not return anything useful the second time.
-	if ( $status === false ) {
-		$status = proc_get_status( $proc );
-	}
+	$status = proc_get_status( $proc );
 
 	if ( $logMsg !== false ) {
 		// Read/select error
