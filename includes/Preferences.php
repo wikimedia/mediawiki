1e86d771abacd26a9df9341dcc6e505fbf1e16a8
Bartosz Dziewoński <matma.rex@gmail.com>
2014-07-22 17:35:58 +0200

    Preferences: Remove 'size' => 20 from 'stubthreshold'
    After it was changed from 'selectorother' to 'select' in I88212703,
    the size defines the height of the select box instead of the width of
    the text input field (http://i.imgur.com/t8eR6iS.png).
    
    Change-Id: Iec1ec4b65c52adacb78406a53e4e4ecb1db8a8d4

--- includes/Preferences.php
+++ includes/Preferences.php
@@ -750,7 +750,6 @@ class Preferences {
 			'type' => 'select',
 			'section' => 'rendering/advancedrendering',
 			'options' => $stubThresholdOptions,
-			'size' => 20,
 			'label-raw' => $context->msg( 'stub-threshold' )->text(), // Raw HTML message. Yay?
 		);
 
