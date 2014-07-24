b858b3c06ede5b56d963eaf5d84bae9686a56d1b
jdlrobson <jdlrobson@gmail.com>
2014-07-24 13:01:35 -0700

    Add mw-ui-checkbox
    Move checkbox styling from BetaFeatures extension to core.
    Drop vendor prefixed border radius support (http://caniuse.com/border-radius)
    
    Use adjacent selector which is supported IE 7 and greater
    Use not selector to filter out IE < 8 which do not support :checked
    
    Change-Id: I6a0db7c8ce33d242120f1cba9222db4e2154696c

--- resources/Resources.php
+++ resources/Resources.php
@@ -1448,6 +1448,7 @@ return array(
 		'position' => 'top',
 		'targets' => array( 'desktop', 'mobile' ),
 	),
+
 	// Lightweight module for button styles
 	'mediawiki.ui.button' => array(
 		'styles' => array(
