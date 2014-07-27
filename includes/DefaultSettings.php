fe3b2adbe7e0b4d116c23ceaf91509d6283727a6
Tyler Romeo <tylerromeo@gmail.com>
2014-07-27 15:48:22 -0400

    Changed password default to PBKDF2
    Bug: 28419
    Change-Id: I096a862c884f6a6adbb381c0fcc70844bf007c63

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -4034,7 +4034,7 @@ $wgInvalidPasswordReset = true;
  *
  * @since 1.24
  */
-$wgPasswordDefault = 'B';
+$wgPasswordDefault = 'pbkdf2';
 
 /**
  * Configuration for built-in password types. Maps the password type
