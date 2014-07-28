955c8a093259169af0e54273952d036a96a2418f
umherirrender <umherirrender_de.wp@web.de>
2014-07-28 12:39:12 +0200

    Remove File::getPropsFromPath() (deprecated since 1.19)
    Change-Id: I11b6c582b9ce60cba03e55d55afb7cd02d218049

--- includes/filerepo/file/File.php
+++ includes/filerepo/file/File.php
@@ -2073,25 +2073,6 @@ abstract class File {
 	}
 
 	/**
-	 * Get an associative array containing information about a file in the local filesystem.
-	 *
-	 * @param string $path Absolute local filesystem path
-	 * @param string|bool $ext The file extension, or true to extract it from
-	 *   the filename. Set it to false to ignore the extension.
-	 *
-	 * @return array
-	 * @deprecated since 1.19
-	 */
-	static function getPropsFromPath( $path, $ext = true ) {
-		wfDebug( __METHOD__ . ": Getting file info for $path\n" );
-		wfDeprecated( __METHOD__, '1.19' );
-
-		$fsFile = new FSFile( $path );
-
-		return $fsFile->getProps();
-	}
-
-	/**
 	 * Get a SHA-1 hash of a file in the local filesystem, in base-36 lower case
 	 * encoding, zero padded to 31 digits.
 	 *
