4a91efddc2ccf7b6cde1cabc1fc2182d181a6916
withoutaname <drevitchi@gmail.com>
2014-07-27 17:45:59 -0700

    Static initialize WebResponse and split response() implementation into subclass
    Change-Id: Ibc3b00e0dbe0d5e728e4f54823c045891e2613b3

--- includes/WebRequest.php
+++ includes/WebRequest.php
@@ -39,12 +39,6 @@ class WebRequest {
 	protected $data, $headers = array();
 
 	/**
-	 * Lazy-init response object
-	 * @var WebResponse
-	 */
-	private $response;
-
-	/**
 	 * Cached client IP address
 	 * @var string
 	 */
@@ -824,11 +818,11 @@ class WebRequest {
 	 */
 	public function response() {
 		/* Lazy initialization of response object for this request */
-		if ( !is_object( $this->response ) ) {
-			$class = ( $this instanceof FauxRequest ) ? 'FauxResponse' : 'WebResponse';
-			$this->response = new $class();
+		static $response = null;
+		if ( $response === null ) {
+			$response = new WebResponse();
 		}
-		return $this->response;
+		return $response;
 	}
 
 	/**
@@ -1250,6 +1244,10 @@ class WebRequestUpload {
 class FauxRequest extends WebRequest {
 	private $wasPosted = false;
 	private $session = array();
+	/**
+	 * @var FauxResponse
+	 */
+	public $response;
 
 	/**
 	 * @param array $data Array of *non*-urlencoded key => value pairs, the
@@ -1319,6 +1317,17 @@ class FauxRequest extends WebRequest {
 	 */
 	public function wasPosted() {
 		return $this->wasPosted;
+	}
+
+	/**
+	 * @return FauxResponse
+	 */
+	public function response() {
+		/* Lazy initialization of FauxResponse object for this request */
+		if ( $this->response === null ) {
+			$this->response = new FauxResponse();
+		}
+		return $this->response;
 	}
 
 	public function getCookie( $key, $prefix = null, $default = null ) {
