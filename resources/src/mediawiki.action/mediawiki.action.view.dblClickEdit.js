5482bb3e2f11e00f7d868162133ccc1ca45ef1b3
Alex Monk <krenair@wikimedia.org>
2014-07-29 19:51:09 +0100

    Add JS hook to allow extensions to prevent double click from changing to action=edit
    There's a preference which makes the browser go to action=edit when double clicking
    on the content. In VE we want to disable this while the editor is open. With this
    hook in place, we could do something like:
    
    mw.hook( 'dblclickedit' ).add( function ( hookStatus ) {
    	if ( target.active ) {
    		hookStatus.edit = false;
    	}
    } );
    
    Bug: 53058
    Change-Id: I0f6225b25ad279755548361f82b855727758da2b

--- resources/src/mediawiki.action/mediawiki.action.view.dblClickEdit.js
+++ resources/src/mediawiki.action/mediawiki.action.view.dblClickEdit.js
@@ -4,9 +4,13 @@
 ( function ( mw, $ ) {
 	$( function () {
 		mw.util.$content.dblclick( function ( e ) {
-			e.preventDefault();
-			// Trigger native HTMLElement click instead of opening URL (bug 43052)
-			$( '#ca-edit a' ).get( 0 ).click();
+			var hookStatus = { edit: true };
+			mw.hook( 'dblclickedit' ).fire( hookStatus );
+			if ( hookStatus.edit ) {
+				e.preventDefault();
+				// Trigger native HTMLElement click instead of opening URL (bug 43052)
+				$( '#ca-edit a' ).get( 0 ).click();
+			}
 		} );
 	} );
 }( mediaWiki, jQuery ) );
