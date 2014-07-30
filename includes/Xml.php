66094fb6b6a8a7387547358dcf25e73de7314d59
jdlrobson <jdlrobson@gmail.com>
2014-07-30 10:56:25 -0700

    Add blanket support for mediawiki ui via globals
    This provides better mobile experiences on various pages
    and a more consistent UI across both mobile and desktop.
    
    It does this in two ways.
    
    1) Forces HTMLForms to not use table based layouts so as
    not to interfere with responsive nature of mediawiki ui elements
    
    2) Applies MediaWiki.UI classes to most pages
    If a page is created via Xml or Html classes it will use mediawiki ui
    Where possible I've added classes unconditionally, but for cases of buttons
    this is behind the $wgUseMediaWikiUIEverywhere global since button styling is
    enabled on pages by default and for checkboxes since it is changes HTML markup.
    
    3) Adds all MediaWiki.UI styles to pages which can use it
    When enabled:
    * Apply these styles to all pages which use HTMLForms
    * Apply to EditPage
    * Apply to anything that uses certain elements outputted by the
    Xml or HTML helper classes
    * Apply to History page
    * Apply to protection page
    * Apply to move page
    * Apply to deletion page
    
    Currently kept behind a global to allow us time to finetune
    existing elements. After further testing we will look to kill the
    globals and make mediawiki.ui the default
    
    See: I430c0fbb79d2a33bb828b2427bda0ee01115d73f
    Change-Id: I47db5eab4569514d039261d11b6dedb0eeae17b5

--- includes/Xml.php
+++ includes/Xml.php
@@ -317,7 +317,8 @@ class Xml {
 			$attributes['value'] = $value;
 		}
 
-		return self::element( 'input', $attributes + $attribs );
+		return self::element( 'input',
+			self::getTextInputAttributes( $attributes + $attribs ) );
 	}
 
 	/**
@@ -404,6 +405,35 @@ class Xml {
 	}
 
 	/**
+	 * Modifies a set of attributes meant for text input elements
+	 * and apply a set of default attributes.
+	 * Removes size attribute when $wgUseMediaWikiUIEverywhere enabled.
+	 * @param array $attrs An attribute array.
+	 * @return array $attrs A modified attribute array
+	 */
+	public static function getTextInputAttributes( $attrs ) {
+		global $wgUseMediaWikiUIEverywhere;
+		if ( !$attrs ) {
+			$attrs = array();
+		}
+		if ( isset( $attrs['class'] ) ) {
+			if ( is_array( $attrs['class'] ) ) {
+				$attrs['class'][] = 'mw-ui-input';
+			} else {
+				$attrs['class'] .= ' mw-ui-input';
+			}
+		} else {
+			$attrs['class'] = 'mw-ui-input';
+		}
+		if ( $wgUseMediaWikiUIEverywhere ) {
+			// Note that size can effect the desired width rendering of mw-ui-input elements
+			// so it is removed. Left intact when mediawiki ui not enabled.
+			unset( $attrs['size'] );
+		}
+		return $attrs;
+	}
+
+	/**
 	 * Convenience function to build an HTML text input field with a label
 	 * @param string $label Text of the label
 	 * @param string $name Value of the name attribute
@@ -453,9 +483,16 @@ class Xml {
 	 * @return string HTML
 	 */
 	public static function checkLabel( $label, $name, $id, $checked = false, $attribs = array() ) {
-		return self::check( $name, $checked, array( 'id' => $id ) + $attribs ) .
+		global $wgUseMediaWikiUIEverywhere;
+		$chkLabel = self::check( $name, $checked, array( 'id' => $id ) + $attribs ) .
 			'&#160;' .
 			self::label( $label, $id, $attribs );
+
+		if ( $wgUseMediaWikiUIEverywhere ) {
+			$chkLabel = self::openElement( 'div', array( 'class' => 'mw-ui-checkbox' ) ) .
+				$chkLabel . self::closeElement( 'div' );
+		}
+		return $chkLabel;
 	}
 
 	/**
@@ -485,7 +522,19 @@ class Xml {
 	 * @return string HTML
 	 */
 	public static function submitButton( $value, $attribs = array() ) {
-		return Html::element( 'input', array( 'type' => 'submit', 'value' => $value ) + $attribs );
+		global $wgUseMediaWikiUIEverywhere;
+		$attrs = array(
+			'type' => 'submit',
+			'value' => $value,
+		);
+		// Done conditionally for time being as it is possible
+		// some submit forms
+		// might need to be mw-ui-destructive (e.g. delete a page)
+		if ( $wgUseMediaWikiUIEverywhere ) {
+			$attrs['class'] = 'mw-ui-button mw-ui-constructive';
+		}
+		$attrs += $attribs;
+		return Html::element( 'input', $attrs );
 	}
 
 	/**
@@ -617,12 +666,14 @@ class Xml {
 	 */
 	public static function textarea( $name, $content, $cols = 40, $rows = 5, $attribs = array() ) {
 		return self::element( 'textarea',
-					array(
-						'name' => $name,
-						'id' => $name,
-						'cols' => $cols,
-						'rows' => $rows
-					) + $attribs,
+					self::getTextInputAttributes(
+						array(
+							'name' => $name,
+							'id' => $name,
+							'cols' => $cols,
+							'rows' => $rows
+						) + $attribs
+					),
 					$content, false );
 	}
 
