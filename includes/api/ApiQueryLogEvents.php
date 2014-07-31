6821ce8607d015ced76afd2d42a7220ed0a96b77
tisane <nathanlarson3141@gmail.com>
2014-07-31 17:47:58 -0400

    Make log_params contain null revision rev_id and moved page ID
    * Make log_params contain the revision IDs of null revisions created by page
      move events, as parameter "nullrevid". Make this data available via API
      result property "nullrevid".
    * Make log_params contain the page ID of the moved page, as parameter
      "movedpageid". Make this data available via API result property
      "movedpageid".
    * Store moved page ID in log_search as ls_field "moved_page_id". This is a
      non-breaking way to achieve the main goal of bug 57084, which is to have an
      indexed field containing the moved page ID.
    
    Bug: 68930
    Bug: 68950
    Change-Id: I3694958a69cb80f9001348acd9baffdb457adac5

--- includes/api/ApiQueryLogEvents.php
+++ includes/api/ApiQueryLogEvents.php
@@ -264,6 +264,8 @@ class ApiQueryLogEvents extends ApiQueryBase {
 				} else {
 					$targetKey = '4::target';
 					$noredirKey = '5::noredir';
+					$nullrevidKey = 'nullrevid';
+					$movedpageidKey = 'movedpageid';
 				}
 
 				if ( isset( $params[$targetKey] ) ) {
@@ -276,6 +278,12 @@ class ApiQueryLogEvents extends ApiQueryBase {
 				}
 				if ( isset( $params[$noredirKey] ) && $params[$noredirKey] ) {
 					$vals[$type]['suppressedredirect'] = '';
+				}
+				if ( isset( $params[$nullrevidKey] ) ) {
+					$vals[$type]['nullrevid'] = $params[$nullrevidKey];
+				}
+				if ( isset( $params[$movedpageidKey] ) ) {
+					$vals[$type]['movedpageid'] = $params[$movedpageidKey];
 				}
 				$params = null;
 				break;
