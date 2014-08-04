660beea43d718197c7d0a1f029c0362a5c1a5170
umherirrender <umherirrender_de.wp@web.de>
2014-08-04 21:35:18 +0200

    Migrate import log to new log system
    There is no need for an extra formatter class, because at the moment
    there is nothing extra to format.
    
    This allows use of gender on Special:Log. Old messages are kept for use
    in IRC. A test was added to ensure an unchanged IRC message.
    
    Change-Id: I590075b203548c071a98c1eaa4b1bc9250f60e22

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6659,8 +6659,6 @@ $wgLogActions = array(
 	'protect/modify' => 'modifiedarticleprotection',
 	'protect/unprotect' => 'unprotectedarticle',
 	'protect/move_prot' => 'movedarticleprotection',
-	'import/upload' => 'import-logentry-upload',
-	'import/interwiki' => 'import-logentry-interwiki',
 	'suppress/block' => 'blocklogentry',
 	'suppress/reblock' => 'reblock-logentry',
 );
@@ -6688,6 +6686,8 @@ $wgLogActionsHandlers = array(
 	'upload/overwrite' => 'LogFormatter',
 	'upload/revert' => 'LogFormatter',
 	'merge/merge' => 'MergeLogFormatter',
+	'import/upload' => 'LogFormatter',
+	'import/interwiki' => 'LogFormatter',
 );
 
 /**
