00a5c07b5c56e1176a22bb8d8ac625425219f449
jsahleen <jsahleen@wikimedia.org>
2014-08-06 14:19:07 +0100

    Fixes Algerian messages file so it does not convert to Arabic digits.
    Bug: 69172
    Change-Id: I8ba9e135daa2fc80907703b1023172c680fa571b

---
+++ tests/phpunit/languages/LanguageArqTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Based on LanguageMlTest
+ * @author Joel Sahleen
+ * @copyright Copyright © 2014, Joel Sahleen
+ * @file
+ */
+
+/** Tests for MediaWiki languages/LanguageArq.php */
+class LanguageArqTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider provideNumber
+	 * @covers Language::formatNum
+	 */
+	public function testFormatNum( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->formatNum( $value ) );
+	}
+
+	public static function provideNumber() {
+		return array(
+			array( '1.234.567', '1234567'),
+			array( '-12,89', -12.89 ),
+			);
+	}
+
+}
