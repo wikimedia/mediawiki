670b2da65b4edc19148e38c19ffe233678ff8e21
Kunal Mehta <legoktm@gmail.com>
2014-08-07 21:46:38 +0100

    JobRunner: Use Config instead of globals
    Change-Id: I35ac93a0941ee8b12d6dc6a557edc0817fb96a10

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -666,7 +666,7 @@ class MediaWiki {
 
 		if ( !$this->config->get( 'RunJobsAsync' ) ) {
 			// Fall back to running the job here while the user waits
-			$runner = new JobRunner();
+			$runner = new JobRunner( $this->config );
 			$runner->run( array( 'maxJobs'  => $n ) );
 			return;
 		}
@@ -700,7 +700,7 @@ class MediaWiki {
 		if ( !$sock ) {
 			wfDebugLog( 'runJobs', "Failed to start cron API (socket error $errno): $errstr\n" );
 			// Fall back to running the job here while the user waits
-			$runner = new JobRunner();
+			$runner = new JobRunner( $this->config );
 			$runner->run( array( 'maxJobs'  => $n ) );
 			return;
 		}
