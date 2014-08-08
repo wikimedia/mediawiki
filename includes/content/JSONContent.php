560f6b83d068b3d6675779d1e190bdbb2fb128bf
Kunal Mehta <legoktm@gmail.com>
2014-08-08 17:41:26 +0100

    [WIP] Add JSONContent and handler from EventLogging
    As was discussed at the architecture summit, a basic JSON content
    class which handles validation and basic display. Not intended to
    be used directly, but for extensions to subclass.
    
    Needs:
    * CSS for pretty display
    * Tests!
    
    Change-Id: Ifcde9bcd0efcf15a3ab692dd2a0a3038559e0254

---
+++ includes/content/JSONContent.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * JSON Schema Content Model
+ *
+ * @file
+ * @ingroup Extensions
+ * @ingroup EventLogging
+ *
+ * @author Ori Livneh <ori@wikimedia.org>
+ * @author Kunal Mehta <legoktm@gmail.com>
+ */
+
+
+/**
+ * Represents the content of a JSON Schema article.
+ */
+class JSONContent extends TextContent {
+
+	function __construct( $text, $modelId = CONTENT_MODEL_JSON ) {
+		parent::__construct( $text, $modelId );
+	}
+
+	/**
+	 * Decodes the JSON schema into a PHP associative array.
+	 * @return array: Schema array.
+	 */
+	function getJsonData() {
+		return FormatJson::decode( $this->getNativeData(), true );
+	}
+
+	/**
+	 * @return bool Whether content is valid JSON.
+	 */
+	function isValid() {
+		return $this->getJsonData() !== null;
+	}
+
+	/**
+	 * Pretty-print JSON
+	 *
+	 * @param string $json
+	 * @return bool|null|string
+	 */
+	public function beautifyJSON( $json ) {
+		$decoded = FormatJson::decode( $json, true );
+		if ( !is_array( $decoded ) ) {
+			return null;
+		}
+		return FormatJson::encode( $decoded, true );
+
+	}
+
+	/**
+	 * Beautifies JSON prior to save.
+	 * @param Title $title Title
+	 * @param User $user User
+	 * @param ParserOptions $popts
+	 * @return JsonSchemaContent
+	 */
+	function preSaveTransform( Title $title, User $user, ParserOptions $popts ) {
+		return new JSONContent( $this->beautifyJSON( $this->getNativeData() ) );
+	}
+
+	function getHtml() {
+		return $this->objectTable( $this->getJsonData() );
+	}
+
+	/**
+	 * Constructs an HTML representation of a JSON object.
+	 * @param Array $mapping
+	 * @return string: HTML.
+	 */
+	function objectTable( $mapping ) {
+		$rows = array();
+
+		foreach ( $mapping as $key => $val ) {
+			$rows[] = $this->objectRow( $key, $val );
+		}
+		return Xml::tags( 'table', array( 'class' => 'mw-json' ),
+			Xml::tags( 'tbody', array(), join( "\n", $rows ) )
+		);
+	}
+
+	/**
+	 * Constructs HTML representation of a single key-value pair.
+	 * @param string $key
+	 * @param mixed $val
+	 * @return string: HTML.
+	 */
+	function objectRow( $key, $val ) {
+		$th = Xml::elementClean( 'th', array(), $key );
+		if ( is_array( $val ) ) {
+			$td = Xml::tags( 'td', array(), self::objectTable( $val ) );
+		} else {
+			if ( is_string( $val ) ) {
+				$val = '"' . $val . '"';
+			} else {
+				$val = FormatJson::encode( $val );
+			}
+
+			$td = Xml::elementClean( 'td', array( 'class' => 'value' ), $val );
+		}
+
+		return Xml::tags( 'tr', array(), $th . $td );
+	}
+
+}
---
+++ includes/content/JSONContentHandler.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * JSON Schema Content Handler
+ *
+ * @file
+ *
+ * @author Ori Livneh <ori@wikimedia.org>
+ * @author Kunal Mehta <legoktm@gmail.com>
+ */
+
+class JSONContentHandler extends TextContentHandler {
+
+	public function __construct( $modelId = CONTENT_MODEL_JSON ) {
+		parent::__construct( $modelId, array( CONTENT_FORMAT_JSON ) );
+	}
+
+	/**
+	 * Unserializes a JSONContent object.
+	 *
+	 * @param string $text Serialized form of the content
+	 * @param null|string $format The format used for serialization
+	 *
+	 * @return JSONContent
+	 */
+	public function unserializeContent( $text, $format = null ) {
+		$this->checkFormat( $format );
+		return new JSONContent( $text );
+	}
+
+	/**
+	 * Creates an empty JsonSchemaContent object.
+	 *
+	 * @return Content
+	 */
+	public function makeEmptyContent() {
+		return new JSONContent( '' );
+	}
+
+	/** JSON is English **/
+	public function getPageLanguage( Title $title, Content $content = null ) {
+		return wfGetLangObj( 'en' );
+	}
+
+	/** JSON is English **/
+	public function getPageViewLanguage( Title $title, Content $content = null ) {
+		return wfGetLangObj( 'en' );
+	}
+}
