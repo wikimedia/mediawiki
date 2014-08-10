7a4088d52ff913d4f1c8ac183a98a86ee7b03dcc
Brad Jorsch <bjorsch@wikimedia.org>
2014-08-10 11:25:29 +0100

    API: Move parameter formatting into LogFormatter
    This allows for extensions to format their log entry parameters, and
    keeps the code for formatting API log entry parameters in the same place
    as for other formatting.
    
    This also takes the opportunity to rearrange the output format slightly
    to avoid conflicts like what's happening in T73020.
    
    Bug: T35235
    Bug: T73020
    Bug: T91466
    Change-Id: I6846ce09322eb404c506b5a51780a44ce9279fe2

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6688,7 +6688,7 @@ $wgLogActions = array(
 );
 
 /**
- * The same as above, but here values are names of functions,
+ * The same as above, but here values are names of classes,
  * not messages.
  * @see LogPage::actionText
  * @see LogFormatter
@@ -6706,9 +6706,9 @@ $wgLogActionsHandlers = array(
 	'patrol/patrol' => 'PatrolLogFormatter',
 	'rights/rights' => 'RightsLogFormatter',
 	'rights/autopromote' => 'RightsLogFormatter',
-	'upload/upload' => 'LogFormatter',
-	'upload/overwrite' => 'LogFormatter',
-	'upload/revert' => 'LogFormatter',
+	'upload/upload' => 'UploadLogFormatter',
+	'upload/overwrite' => 'UploadLogFormatter',
+	'upload/revert' => 'UploadLogFormatter',
 	'merge/merge' => 'MergeLogFormatter',
 	'managetags/create' => 'LogFormatter',
 	'managetags/delete' => 'LogFormatter',
