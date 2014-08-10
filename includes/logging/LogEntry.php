aacc55edbb4677d62ece3348466a077b9f2014db
Brad Jorsch <bjorsch@wikimedia.org>
2014-08-10 11:25:29 +0100

    API: Move parameter formatting into LogFormatter
    This allows for extensions to format their log entry parameters, and
    keeps the code for formatting API log entry parameters in the same place
    as for other formatting.
    
    This also takes the opportunity to rearrange the output format slightly
    to avoid conflicts like what's happening in T73020.
    
    Bug: T35235
    Bug: T73020
    Change-Id: I6846ce09322eb404c506b5a51780a44ce9279fe2

--- includes/logging/LogEntry.php
+++ includes/logging/LogEntry.php
@@ -370,6 +370,9 @@ class ManualLogEntry extends LogEntryBase {
 	/** @var int ID of the log entry */
 	protected $id;
 
+	/** @var bool Whether this is a legacy log entry */
+	protected $legacy = false;
+
 	/**
 	 * Constructor.
 	 *
@@ -456,6 +459,16 @@ class ManualLogEntry extends LogEntryBase {
 	 */
 	public function setComment( $comment ) {
 		$this->comment = $comment;
+	}
+
+	/**
+	 * Set the 'legacy' flag
+	 *
+	 * @since 1.25
+	 * @param bool $legacy
+	 */
+	public function setLegacy( $legacy = true ) {
+		$this->legacy = $legacy;
 	}
 
 	/**
@@ -614,6 +627,10 @@ class ManualLogEntry extends LogEntryBase {
 
 	public function getParameters() {
 		return $this->parameters;
+	}
+
+	public function isLegacy() {
+		return $this->legacy;
 	}
 
 	/**
