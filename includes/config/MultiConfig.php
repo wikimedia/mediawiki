46f37eb1a84773d405062aafebafb610bace9e9d
Kunal Mehta <legoktm@gmail.com>
2014-08-11 13:17:37 +0100

    [WIP] Add MultiConfig for fallback logic
    Bug: 69418
    Change-Id: I0ef2fbb86d5c27602d70240219ee08be31e2d09b

---
+++ includes/config/MultiConfig.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright 2014
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Provides a fallback sequence for Config objects
+ *
+ * @since 1.24
+ */
+class MultiConfig implements Config {
+
+	/**
+	 * Array of Config objects to use
+	 * in order of which to be used earlier
+	 *
+	 * @var Config[]
+	 */
+	private $configs;
+
+	/**
+	 * Default builder function
+	 * @param Config[] $configs
+	 * @return MultiConfig
+	 */
+	public static function newInstance( array $configs ) {
+		return new self( $configs );
+	}
+
+	/**
+	 * @param Config[] $configs
+	 */
+	public function __construct( array $configs ) {
+		$this->configs = $configs;
+	}
+
+	/**
+	 * @see Config::get
+	 */
+	public function get( $name ) {
+		foreach( $this->configs as $config ) {
+			if ( $config->has( $name ) ) {
+				return $config->get( $name );
+			}
+		}
+
+		throw new ConfigException( __METHOD__ . ": undefined option: '$name'" );
+	}
+
+	/**
+	 * @see Config::has
+	 */
+	public function has( $name ) {
+		foreach( $this->configs as $config ) {
+			if ( $config->has( $name ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @see Config::set
+	 */
+	public function set( $name, $value ) {
+		$this->configs[0]->set( $name, $value );
+	}
+}
