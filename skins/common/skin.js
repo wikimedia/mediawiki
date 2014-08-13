a6bc88998c440d8ca48aa22b6c1f37bad3467781
jdlrobson <jdlrobson@gmail.com>
2014-08-13 10:28:50 -0700

    Stop using DOM as an api. Start a Skin api for the watchstar
    We can't rely on the DOM. Let's move towards a nicer way to identify
    concepts on a page.
    See this in action on VectorBeta: I410181722e6b4f3fad4fb1de031040c508648dd6
    
    Change-Id: I3c9e607498d231bf37733ff47a33fe614ace3dea

---
+++ skins/common/skin.js
@@ -0,0 +1,30 @@
+(function( $ ) {
+var config = mw.config.get( 'wgSkinConfig' );
+
+function Skin( config ) {
+	this.config = config;
+}
+
+Skin.prototype = {
+	getWatchstar: function() {
+		if ( this.config.watchstar ) {
+			return $( this.config.watchstar );
+		} else {
+			mw.log( 'Skin needs to define watchstar config property' );
+			// Fallback for all the skins
+			return $( '.mw-watchlink a, a.mw-watchlink, ' +
+					'#ca-watch a, #ca-unwatch a, #mw-unwatch-link1, ' +
+					'#mw-unwatch-link2, #mw-watch-link2, #mw-watch-link1' )
+					// Allowing people to add inline animated links is a little scary
+					.filter( ':not( #bodyContent *, #content * )' );
+		}
+	}
+};
+
+if ( !config ) {
+	mw.log( 'Skin needs to use wgSkinConfig' );
+	config = {};
+}
+mw.skin = new Skin( config );
+} ( jQuery ) );
+
