05fff0f9b6f5a9414f9ef44b7b1cbdba70f503ff
withoutaname <drevitchi@gmail.com>
2014-08-12 20:18:20 -0700

    Collapse all functions except wfOutputHandler() in OutputHandler.php
    Change-Id: Iae0018663be6905904172f1d458ca917ac2df719

--- includes/OutputHandler.php
+++ includes/OutputHandler.php
@@ -28,209 +28,112 @@
  * @return string
  */
 function wfOutputHandler( $s ) {
-	global $wgDisableOutputCompression, $wgValidateAllHtml;
-	$s = wfMangleFlashPolicy( $s );
+	// Mangle flash policy tags which open up the site to XSS attacks
+	$s = preg_replace( '/\<\s*cross-domain-policy\s*\>/i', '<NOT-cross-domain-policy>', $s );
+
+	global $wgValidateAllHtml;
 	if ( $wgValidateAllHtml ) {
-		$headers = headers_list();
-		$isHTML = false;
-		foreach ( $headers as $header ) {
+		foreach ( headers_list() as $header ) {
 			$parts = explode( ':', $header, 2 );
 			if ( count( $parts ) !== 2 ) {
 				continue;
 			}
 			$name = strtolower( trim( $parts[0] ) );
 			$value = trim( $parts[1] );
-			if ( $name == 'content-type' && ( strpos( $value, 'text/html' ) === 0
+			if ( $name === 'content-type' && ( strpos( $value, 'text/html' ) === 0
 				|| strpos( $value, 'application/xhtml+xml' ) === 0 )
 			) {
-				$isHTML = true;
+				$errors = '';
+				if ( !MWTidy::checkErrors( $s, $errors ) ) {
+					// Replace the output with an error if the HTML is not valid
+					header( 'Cache-Control: no-cache' );
+					$body = Html::element( 'h1', null, 'HTML validation error' ) .
+						Html::openElement( 'ul' );
+					$error = strtok( $errors, "\n" );
+					$badLines = array();
+					while ( $error !== false ) {
+						if ( preg_match( '/^line (\d+)/', $error, $m ) ) {
+							$lineNum = intval( $m[1] );
+							$badLines[$lineNum] = true;
+							$body .= Html::rawElement( 'li', null,
+								Html::element( 'a', array( 'href' => "#line-{$lineNum}" ), $error )
+							) . "\n";
+						}
+						$error = strtok( "\n" );
+					}
+					$body .= Html::closeElement( 'ul' ) .
+						Html::element( 'pre', null, $errors ) .
+					Html::openElement( 'ol' ) . "\n";
+					$line = strtok( $s, "\n" );
+					for ( $i = 1; $line !== false; $i++ ) {
+						$attrs = array();
+						if ( isset( $badLines[$i] ) ) {
+							$attrs['class'] = 'highlight';
+							$attrs['id'] = "line-{$i}";
+						}
+						$body .= Html::element( 'li', $attrs, $line ) . "\n";
+						$line = strtok( "\n" );
+					}
+					$body .= Html::closeElement( 'ol' );
+					$s = Html::htmlHeader( array( 'lang' => 'en', 'dir' => 'ltr' ) ) .
+						Html::rawElement( 'head', null,
+							Html::element( 'title', null, 'HTML validation error' ) .
+							Html::inlineStyle( ".highlight { background-color: #ffc }\nli { white-space: pre }" )
+						) .
+						Html::rawElement( 'body', null, $body ) .
+					Html::closeElement( 'html' );
+				}
 				break;
 			}
 		}
-		if ( $isHTML ) {
-			$s = wfHtmlValidationHandler( $s );
-		}
 	}
+
+	global $wgDisableOutputCompression;
 	if ( !$wgDisableOutputCompression && !ini_get( 'zlib.output_compression' ) ) {
 		if ( !defined( 'MW_NO_OUTPUT_COMPRESSION' ) ) {
-			$s = wfGzipHandler( $s );
+			// If allowed by the Accept header, handle data compression with gzip
+			// Unlike ob_gzhandler, also works with HEAD requests
+			$path = explode( '?', WebRequest::getRequestURL(), 2 );
+			$period = strrpos( $path[0], '.' );
+			$ext = $period !== false ? strtolower( substr( $path[0], $period ) ) : null;
+			// Don't do gzip compression if the URL path ends in .gz or .tgz
+			// This confuses Safari and triggers a download of the page,
+			// even though it's pretty clearly labeled as viewable HTML
+			if ( !headers_sent() && function_exists( 'gzencode' ) &&
+				( $ext !== '.gz' || $ext !== '.tgz' )
+			) {
+				if ( wfClientAcceptsGzip() ) {
+					header( 'Content-Encoding: gzip' );
+					$s = gzencode( $s, 6 );
+				}
+
+				// Set vary header if it hasn't been set already
+				$foundVary = false;
+				foreach ( headers_list() as $header ) {
+					if ( strpos( $header, 'Vary:' ) === 0 ) {
+						$foundVary = true;
+						break;
+					}
+				}
+				if ( !$foundVary ) {
+					header( 'Vary: Accept-Encoding' );
+					global $wgUseXVO;
+					if ( $wgUseXVO ) {
+						header( 'X-Vary-Options: Accept-Encoding;list-contains=gzip' );
+					}
+				}
+			}
 		}
 		if ( !ini_get( 'output_handler' ) ) {
-			wfDoContentLength( strlen( $s ) );
+			// Add a Content-Length header if possible. This makes it cooperate with squid better
+			if ( !headers_sent() &&
+				isset( $_SERVER['SERVER_PROTOCOL'] ) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.0'
+			) {
+				$length = strlen( $s );
+				header( "Content-Length: {$length}" );
+			}
 		}
 	}
+
 	return $s;
-}
-
-/**
- * Get the "file extension" that some client apps will estimate from
- * the currently-requested URL.
- * This isn't on WebRequest because we need it when things aren't initialized
- * @private
- *
- * @return string
- */
-function wfRequestExtension() {
-	/// @todo FIXME: this sort of dupes some code in WebRequest::getRequestUrl()
-	if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-		// Strip the query string...
-		list( $path ) = explode( '?', $_SERVER['REQUEST_URI'], 2 );
-	} elseif ( isset( $_SERVER['SCRIPT_NAME'] ) ) {
-		// Probably IIS. QUERY_STRING appears separately.
-		$path = $_SERVER['SCRIPT_NAME'];
-	} else {
-		// Can't get the path from the server? :(
-		return '';
-	}
-
-	$period = strrpos( $path, '.' );
-	if ( $period !== false ) {
-		return strtolower( substr( $path, $period ) );
-	}
-	return '';
-}
-
-/**
- * Handler that compresses data with gzip if allowed by the Accept header.
- * Unlike ob_gzhandler, it works for HEAD requests too.
- *
- * @param string $s
- *
- * @return string
- */
-function wfGzipHandler( $s ) {
-	if ( !function_exists( 'gzencode' ) ) {
-		wfDebug( __FUNCTION__ . "() skipping compression (gzencode unavailable)\n" );
-		return $s;
-	}
-	if ( headers_sent() ) {
-		wfDebug( __FUNCTION__ . "() skipping compression (headers already sent)\n" );
-		return $s;
-	}
-
-	$ext = wfRequestExtension();
-	if ( $ext == '.gz' || $ext == '.tgz' ) {
-		// Don't do gzip compression if the URL path ends in .gz or .tgz
-		// This confuses Safari and triggers a download of the page,
-		// even though it's pretty clearly labeled as viewable HTML.
-		// Bad Safari! Bad!
-		return $s;
-	}
-
-	if ( wfClientAcceptsGzip() ) {
-		wfDebug( __FUNCTION__ . "() is compressing output\n" );
-		header( 'Content-Encoding: gzip' );
-		$s = gzencode( $s, 6 );
-	}
-
-	// Set vary header if it hasn't been set already
-	$headers = headers_list();
-	$foundVary = false;
-	foreach ( $headers as $header ) {
-		if ( substr( $header, 0, 5 ) == 'Vary:' ) {
-			$foundVary = true;
-			break;
-		}
-	}
-	if ( !$foundVary ) {
-		header( 'Vary: Accept-Encoding' );
-		global $wgUseXVO;
-		if ( $wgUseXVO ) {
-			header( 'X-Vary-Options: Accept-Encoding;list-contains=gzip' );
-		}
-	}
-	return $s;
-}
-
-/**
- * Mangle flash policy tags which open up the site to XSS attacks.
- *
- * @param string $s
- *
- * @return string
- */
-function wfMangleFlashPolicy( $s ) {
-	# Avoid weird excessive memory usage in PCRE on big articles
-	if ( preg_match( '/\<\s*cross-domain-policy\s*\>/i', $s ) ) {
-		return preg_replace( '/\<\s*cross-domain-policy\s*\>/i', '<NOT-cross-domain-policy>', $s );
-	} else {
-		return $s;
-	}
-}
-
-/**
- * Add a Content-Length header if possible. This makes it cooperate with squid better.
- *
- * @param int $length
- */
-function wfDoContentLength( $length ) {
-	if ( !headers_sent()
-		&& isset( $_SERVER['SERVER_PROTOCOL'] )
-		&& $_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.0'
-	) {
-		header( "Content-Length: $length" );
-	}
-}
-
-/**
- * Replace the output with an error if the HTML is not valid
- *
- * @param string $s
- *
- * @return string
- */
-function wfHtmlValidationHandler( $s ) {
-
-	$errors = '';
-	if ( MWTidy::checkErrors( $s, $errors ) ) {
-		return $s;
-	}
-
-	header( 'Cache-Control: no-cache' );
-
-	$out = Html::element( 'h1', null, 'HTML validation error' );
-	$out .= Html::openElement( 'ul' );
-
-	$error = strtok( $errors, "\n" );
-	$badLines = array();
-	while ( $error !== false ) {
-		if ( preg_match( '/^line (\d+)/', $error, $m ) ) {
-			$lineNum = intval( $m[1] );
-			$badLines[$lineNum] = true;
-			$out .= Html::rawElement( 'li', null,
-				Html::element( 'a', array( 'href' => "#line-{$lineNum}" ), $error ) ) . "\n";
-		}
-		$error = strtok( "\n" );
-	}
-
-	$out .= Html::closeElement( 'ul' );
-	$out .= Html::element( 'pre', null, $errors );
-	$out .= Html::openElement( 'ol' ) . "\n";
-	$line = strtok( $s, "\n" );
-	$i = 1;
-	while ( $line !== false ) {
-		$attrs = array();
-		if ( isset( $badLines[$i] ) ) {
-			$attrs['class'] = 'highlight';
-			$attrs['id'] = "line-$i";
-		}
-		$out .= Html::element( 'li', $attrs, $line ) . "\n";
-		$line = strtok( "\n" );
-		$i++;
-	}
-	$out .= Html::closeElement( 'ol' );
-
-	$style = <<<CSS
-.highlight { background-color: #ffc }
-li { white-space: pre }
-CSS;
-
-	$out = Html::htmlHeader( array( 'lang' => 'en', 'dir' => 'ltr' ) ) .
-		Html::rawElement( 'head', null,
-			Html::element( 'title', null, 'HTML validation error' ) .
-			Html::inlineStyle( $style ) ) .
-		Html::rawElement( 'body', null, $out ) .
-		Html::closeElement( 'html' );
-
-	return $out;
 }
