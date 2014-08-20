45198a08a478c1d350774b62369b33c95fb2bac4
Kunal Mehta <legoktm@gmail.com>
2014-08-20 00:35:35 -0700

    Add more tests for GlobalVarConfig
    Change-Id: I31d50a0636792104e32e3f25694f45942c5c0217

--- tests/phpunit/includes/config/GlobalVarConfigTest.php
+++ tests/phpunit/includes/config/GlobalVarConfigTest.php
@@ -2,6 +2,11 @@
 
 class GlobalVarConfigTest extends MediaWikiTestCase {
 
+	public function testNewInstance() {
+		$config = GlobalVarConfig::newInstance();
+		$this->assertInstanceOf( 'GlobalVarConfig', $config );
+	}
+
 	public function provideGet() {
 		$set = array(
 			'wgSomething' => 'default1',
@@ -28,9 +33,32 @@ class GlobalVarConfigTest extends MediaWikiTestCase {
 	 * @param string $expected
 	 * @dataProvider provideGet
 	 * @covers GlobalVarConfig::get
+	 * @covers GlobalVarConfig::getWithPrefix
 	 */
 	public function testGet( $name, $prefix, $expected ) {
 		$config = new GlobalVarConfig( $prefix );
 		$this->assertEquals( $config->get( $name ), $expected );
+	}
+
+	public static function provideSet() {
+		return array(
+			array( 'Foo', 'wg', 'wgFoo' ),
+			array( 'SomethingRandom', 'wg', 'wgSomethingRandom' ),
+			array( 'FromAnExtension', 'eg', 'egFromAnExtension' ),
+			array( 'NoPrefixHere', '', 'NoPrefixHere' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideSet
+	 * @covers GlobalVarConfig::set
+	 * @covers GlobalVarConfig::setWithPrefix
+	 */
+	public function testSet( $name, $prefix, $var ) {
+		$config = new GlobalVarConfig( $prefix );
+		$random = wfRandomString();
+		$config->set( $name, $random );
+		$this->assertArrayHasKey( $var, $GLOBALS );
+		$this->assertEquals( $random, $GLOBALS[$var] );
 	}
 }
