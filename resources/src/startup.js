280bde709329dffea764f8c23393d25bdea33d1d
Timo Tijhof <krinklemail@gmail.com>
2014-08-24 15:57:31 +0200

    startup: Blacklist Opera < 12
    jQuery hasn't supported Opera < 12 for several releases now, and
    our own support matrix and development practices also consider
    Opera 11 and below being Grade B (there are known problems in
    Opera 8 and 10, such as missing features and implementation bugs).
    
    Update the blacklist to reflect Opera < 12 being Grade B.
    
    https://www.mediawiki.org/wiki/Compatibility#Browser_support_matrix
    
    Change-Id: Ia2764a49c5d1fad73bd4eb52748269407231700e

--- resources/src/startup.js
+++ resources/src/startup.js
@@ -30,6 +30,13 @@ function isCompatible( ua ) {
 		( ua.indexOf( 'MSIE' ) !== -1 && parseFloat( ua.split( 'MSIE' )[1] ) < 7 ) ||
 		// Firefox < 3
 		( ua.indexOf( 'Firefox/' ) !== -1 && parseFloat( ua.split( 'Firefox/' )[1] ) < 3 ) ||
+		// Opera < 12
+		( ua.indexOf( 'Opera/' ) !== -1 && ( ua.indexOf( 'Version/' ) === -1 ?
+			// "Opera/x.y"
+			parseFloat( ua.split( 'Opera/' )[1] ) < 10 :
+			// "Opera/9.80 ... Version/x.y"
+			parseFloat( ua.split( 'Version/' )[1] ) < 12
+		) ) ||
 		// BlackBerry < 6
 		ua.match( /BlackBerry[^\/]*\/[1-5]\./ ) ||
 		// Open WebOS < 1.5
