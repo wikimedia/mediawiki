06f15242904c0488cf82e06b2d97eba08f1d82d1
Timo Tijhof <krinklemail@gmail.com>
2014-08-24 15:57:31 +0200

    startup: Blacklist Opera < 12
    jQuery hasn't supported Opera < 12 for several releases now, and
    our own support matrix and development practices also consider
    Opera 11 and below being Grade B (there are known problems in
    Opera 8 and 10, such as missing features and implementation bugs).
    
    Update the blacklist to reflect Opera < 12 being Grade B.
    
    https://www.mediawiki.org/wiki/Compatibility#Browser_support_matrix
    
    Change-Id: Ia2764a49c5d1fad73bd4eb52748269407231700e

--- tests/qunit/suites/resources/startup.test.js
+++ tests/qunit/suites/resources/startup.test.js
@@ -15,8 +15,13 @@
 			'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Kindle Fire Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Safari/533.1',
 			// Safari 5.0+
 			'Mozilla/5.0 (Macintosh; I; Intel Mac OS X 10_6_7; ru-ru) AppleWebKit/534.31+ (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
-			// Opera 11+
-			'Opera/9.80 (Windows NT 6.1; U; ru) Presto/2.8.131 Version/11.10',
+			// Opera 12+ (Presto-based)
+			'Opera/9.80 (Windows NT 6.1; U; es-ES) Presto/2.9.181 Version/12.00',
+			'Opera/9.80 (Windows NT 5.1) Presto/2.12.388 Version/12.17',
+			// Opera 15+ (Chromium-based)
+			'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36 OPR/15.0.1147.153',
+			'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36 OPR/16.0.1196.62',
+			'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36 OPR/23.0.1522.75',
 			// Internet Explorer 7+
 			'Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0; en-US)',
 			'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; Media Center PC 4.0; SLCC1; .NET CLR 3.0.04320)',
@@ -46,9 +51,19 @@
 			'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98;)',
 			'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
 			'Mozilla/5.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-			// Firefox < 3.6
+			// Firefox < 3
 			'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.2) Gecko/20060308 Firefox/1.5.0.2',
 			'Mozilla/5.0 (X11; U; Linux i686; nl; rv:1.8.1.1) Gecko/20070311 Firefox/2.0.0.1',
+			// Opera < 12
+			'Mozilla/5.0 (Windows NT 5.0; U) Opera 7.54 [en]',
+			'Opera/7.54 (Windows NT 5.0; U) [en]',
+			'Mozilla/5.0 (Windows NT 5.1; U; en) Opera 8.0',
+			'Opera/8.0 (X11; Linux i686; U; cs)',
+			'Opera/9.00 (X11; Linux i686; U; de)',
+			'Opera/9.62 (X11; Linux i686; U; en) Presto/2.1.1',
+			'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.2.15 Version/10.00',
+			'Opera/9.80 (Windows NT 6.1; U; ru) Presto/2.8.131 Version/11.10',
+			'Opera/9.80 (Windows NT 6.1; WOW64; U; pt) Presto/2.10.229 Version/11.62',
 			// BlackBerry < 6
 			'BlackBerry9300/5.0.0.716 Profile/MIDP-2.1 Configuration/CLDC-1.1 VendorID/133',
 			'BlackBerry7250/4.0.0 Profile/MIDP-2.0 Configuration/CLDC-1.1',
@@ -75,8 +90,7 @@
 			// Google Glass
 			'Mozilla/5.0 (Linux; U; Android 4.0.4; en-us; Glass 1 Build/IMM76L; XE11) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
 		],
-		// No explicit support for or against these browsers, they're
-		// given a shot at Grade A at their own risk.
+		// No explicit support for or against these browsers, they're given a shot at Grade A.
 		gradeX: [
 			// Firefox 3.6
 			'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3',
