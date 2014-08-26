fcf8c1587c3e807865deba74244ffe36ee2cce7e
Petr Bena <benapetr@gmail.com>
2014-08-26 15:06:12 +0200

    API: created a new api to flag messages as read
    New api is called "ClearHasMsg" and does nothing but erase a new message
    flag from currently logged in user.
    
    This is useful for tools that can read the new messages using some api, or some
    other way (loading the text using different session, preload the text using a
    buffer where it's unrevealed later if user actually did read it, or closed the
    application before), so it can be useful in situations when you need to flag new
    messages as read in a different time than that when you actually read them.
    
    this resolves bug 64238
    
    Change-Id: Ife575711c32bb8e3bcac789de4a6b37e1888d032

---
+++ includes/api/ApiClearHasMsg.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Created on August 26, 2014
+ *
+ * Copyright © 2014 Petr Bena (benapetr@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * API module that clears the hasmsg flag for current user
+ * @ingroup API
+ */
+class ApiClearHasMsg extends ApiBase {
+	public function execute() {
+		$user = $this->getUser();
+		$user->setNewtalk( false );
+		$this->getResult()->addValue( null, $this->getModuleName(), 'success' );
+	}
+
+	public function isWriteMode() {
+		return true;
+	}
+
+	public function mustBePosted() {
+		return false;
+	}
+
+	public function getDescription() {
+		return array( 'Clears the hasmsg flag for current user.' );
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=clearhasmsg' => 'Clears the hasmsg flag for current user',
+		);
+	}
+
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/API:ClearHasMsg';
+	}
+}
