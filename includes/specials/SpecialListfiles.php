7cb1c09e7e7c8c21a5b431fcd3e5fedf327b0dee
Alexandre Emsenhuber <mediawiki@emsenhuber.ch>
2014-08-27 22:39:19 +0200

    Use as strict comparison to check whether an user name was provided in Special:ListFiles
    Otherwise Special:ListFiles/0 will not behave as expected.
    
    Change-Id: I1fa503b4b514a471ded798b54e867435d30b80c5

--- includes/specials/SpecialListfiles.php
+++ includes/specials/SpecialListfiles.php
@@ -87,7 +87,7 @@ class ImageListPager extends TablePager {
 		$this->mIncluding = $including;
 		$this->mShowAll = $showAll;
 
-		if ( $userName ) {
+		if ( $userName !== null && $userName !== '' ) {
 			$nt = Title::newFromText( $userName, NS_USER );
 			if ( !is_null( $nt ) ) {
 				$this->mUserName = $nt->getText();
