c6289d013eac6194fb6631e8b0d9723f580982b0
Kunal Mehta <legoktm@gmail.com>
2014-08-29 10:13:21 -0700

    ResourceLoaderFileModule: Support fallback in 'languageScripts'
    Bug: 58139
    Change-Id: Idd7657aa48a9eb2b075a85f77e066b425f330890

--- includes/resourceloader/ResourceLoaderFileModule.php
+++ includes/resourceloader/ResourceLoaderFileModule.php
@@ -536,7 +536,7 @@ class ResourceLoaderFileModule extends ResourceLoaderModule {
 			$files,
 			$this->scripts,
 			$context->getDebug() ? $this->debugScripts : array(),
-			self::tryForKey( $this->languageScripts, $context->getLanguage() ),
+			$this->getLanguageScripts( $context->getLanguage() ),
 			self::tryForKey( $this->skinScripts, $context->getSkin(), 'default' ),
 			$this->loaderScripts
 		);
@@ -700,7 +700,7 @@ class ResourceLoaderFileModule extends ResourceLoaderModule {
 	protected function getScriptFiles( ResourceLoaderContext $context ) {
 		$files = array_merge(
 			$this->scripts,
-			self::tryForKey( $this->languageScripts, $context->getLanguage() ),
+			$this->getLanguageScripts( $context->getLanguage() ),
 			self::tryForKey( $this->skinScripts, $context->getSkin(), 'default' )
 		);
 		if ( $context->getDebug() ) {
@@ -708,6 +708,29 @@ class ResourceLoaderFileModule extends ResourceLoaderModule {
 		}
 
 		return array_unique( $files, SORT_REGULAR );
+	}
+
+	/**
+	 * Get the set of language scripts for the given language,
+	 * possibly using a fallback language.
+	 *
+	 * @param string $lang
+	 * @return array
+	 */
+	private function getLanguageScripts( $lang ) {
+		$scripts = self::tryForKey( $this->languageScripts, $lang );
+		if ( $scripts ) {
+			return $scripts;
+		}
+		$fallbacks = Language::getFallbacksFor( $lang );
+		foreach ( $fallbacks as $lang ) {
+			$scripts = self::tryForKey( $this->languageScripts, $lang );
+			if ( $scripts ) {
+				return $scripts;
+			}
+		}
+
+		return array();
 	}
 
 	/**
