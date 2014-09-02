5957856c46c3c351b88b6f150ccd39afb5be2c5c
Kevin Israel <pleasestand@live.com>
2014-09-02 15:33:43 -0400

    wfBaseConvert(): Work around PHP Bug #50175
    Before php/php-src@834daa455bc9, PHP's gmp_init() function would try
    to autodetect hex and binary numbers even when a base was explicitly
    specified[1], so the results for some base-36 numbers having leading
    zeros could be incorrect. Work around this bug by trimming off any
    leading zeros before calling gmp_init().
    
    [1]: https://bugs.php.net/bug.php?id=50175
    
    Bug: 69249
    Change-Id: I5f5458c1a1195f55fa12904c103da6ea7558010a

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -3366,7 +3366,10 @@ function wfBaseConvert( $input, $sourceBase, $destBase, $pad = 1,
 	);
 
 	if ( extension_loaded( 'gmp' ) && ( $engine == 'auto' || $engine == 'gmp' ) ) {
-		$result = gmp_strval( gmp_init( $input, $sourceBase ), $destBase );
+		// Removing leading zeros works around broken base detection code in
+		// some PHP versions (see <https://bugs.php.net/bug.php?id=50175> and
+		// <https://bugs.php.net/bug.php?id=55398>).
+		$result = gmp_strval( gmp_init( ltrim( $input, '0' ), $sourceBase ), $destBase );
 	} elseif ( extension_loaded( 'bcmath' ) && ( $engine == 'auto' || $engine == 'bcmath' ) ) {
 		$decimal = '0';
 		foreach ( str_split( strtolower( $input ) ) as $char ) {
