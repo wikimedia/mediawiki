b99c2019b1300ef5436c2746205f7ec74b1df625
Prateek Saxena <prtksxna@gmail.com>
2014-09-06 11:54:33 +0530

    kss: Add js to hide non current sub-sections
    This can't be done through handlebars as they don't
    allow for conditions
    
    Change-Id: Id0e8263d6c6bf2d89d197d987e617e377af6193e

---
+++ docs/kss/styleguide-template/public/kss.js
@@ -0,0 +1,8 @@
+;( function () {
+	$( function () {
+		// Show sub-section links only of the current section.
+		// Handlebar does not allow for conditions except with external helpers.
+		var currentSection = $( '#content' )[0].classList[0].split( '-' )[1];
+		$( '.subsection').not( '#subsection-' + currentSection ).hide();
+	} );
+} ) ();
