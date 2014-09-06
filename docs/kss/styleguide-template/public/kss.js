b72926f60abd6a9c90bb938fc678d88a76170555
Prateek Saxena <prtksxna@gmail.com>
2014-09-06 16:21:19 +0530

    kss: Highlight section link based on scroll
    Change-Id: Ic73071c841edc78c800916cca6080f93ab5f4347

--- docs/kss/styleguide-template/public/kss.js
+++ docs/kss/styleguide-template/public/kss.js
@@ -1,8 +1,40 @@
 ;( function () {
 	$( function () {
+		var
+			throttleTimer, highlightSection,
+			currentSection = $( '#content' )[0].classList[0].split( '-' )[1],
+			sectionOffsets = {};
+
 		// Show sub-section links only of the current section.
 		// Handlebar does not allow for conditions except with external helpers.
-		var currentSection = $( '#content' )[0].classList[0].split( '-' )[1];
 		$( '.subsection').not( '#subsection-' + currentSection ).hide();
+
+		// Highlight sections on scroll
+		$( '.section-heading' ).each( function () {
+			var $this = $( this );
+			sectionOffsets[ $this.attr( 'name' ) ] = $this.position().top;
+		} );
+
+		highlightSection = function () {
+			var currentSubSection,
+				scrollTop = $( document ).scrollTop() + 200; // FIXME: Magic number
+
+			$.each( sectionOffsets, function ( section, offset ) {
+				$( '.subsection a[href="section-' + currentSection + '.html#' + section + '"]' ).removeClass( 'active' );
+				currentSubSection = ( scrollTop > offset ) ? section : currentSubSection;
+			} );
+
+			if( currentSubSection ) {
+				$( '.subsection a[href="section-' + currentSection + '.html#' + currentSubSection + '"]' ).addClass( 'active' );
+			}
+		}
+
+		$( window ).scroll( function () {
+			if( throttleTimer ) {
+				clearTimeout( throttleTimer );
+			}
+
+			throttleTimer = setTimeout( highlightSection, 100 );
+		} );
 	} );
 } ) ();
