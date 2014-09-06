184f133e2742a1543dae8419968675a3e074511e
Prateek Saxena <prtksxna@gmail.com>
2014-09-06 16:21:19 +0530

    kss: Highlight section link based on scroll
    Change-Id: Ic73071c841edc78c800916cca6080f93ab5f4347

--- docs/kss/styleguide-template/public/kss.js
+++ docs/kss/styleguide-template/public/kss.js
@@ -4,5 +4,37 @@
 		// Handlebar does not allow for conditions except with external helpers.
 		var currentSection = $( '#content' )[0].classList[0].split( '-' )[1];
 		$( '.subsection').not( '#subsection-' + currentSection ).css( 'display', 'none' );
+
+
+		// Highlight sections on scroll
+
+		var sectionOffsets = {};
+		var sectionHeadings = document.querySelectorAll( '.section-heading' )
+
+		for( var i = 0; i < sectionHeadings.length; i++ ) {
+			var el = sectionHeadings[ i ];
+			sectionOffsets[ el.getAttribute( 'name' ) ] = el.offsetTop;
+		}
+
+		console.log( sectionOffsets );
+
+		document.addEventListener( 'scroll', function ( event ) {
+			var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+			var sections = Object.keys( sectionOffsets );
+			var current;
+
+			for( var i = 0; i < sections.length; i++ ) {
+				var section = sections[ i ];
+				var offset = sectionOffsets[ section ];
+				document.querySelector( '.subsection a[href="#' + section + '"]' ).classList.remove( 'active' );
+				if( scrollTop > offset ) {
+					current = section;
+				}
+			}
+
+			if( current ) {
+				document.querySelector( '.subsection a[href="#' + current + '"]' ).classList.add( 'active' );
+			}
+		} );
 	} );
 } ) ();
