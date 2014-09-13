dab72ac97f7062bcaa62e8e8f22ed6ef979d935f
Brad Jorsch <bjorsch@wikimedia.org>
2014-09-13 14:02:37 -0400

    API: Warn when neither rawcontinue nor continue are given to action=query
    The default behavior for continuation for action=query will be changing.
    Warn clients that they will need to update.
    
    Change-Id: I52d048040e898a0c9630e2aed071b339ec12de42

--- includes/api/ApiQuery.php
+++ includes/api/ApiQuery.php
@@ -247,6 +247,16 @@ class ApiQuery extends ApiBase {
 	public function execute() {
 		$this->mParams = $this->extractRequestParams();
 
+		if ( $this->mParams['continue'] === null && !$this->mParams['rawcontinue'] ) {
+			$this->logFeatureUsage( 'action=query&!rawcontinue&!continue' );
+			$this->setWarning(
+				'Formatting of continuation data will be changing soon. ' .
+				'To continue using the current formatting, use the \'rawcontinue\' parameter. ' .
+				'To begin using the new format, pass an empty string for \'continue\' ' .
+				'in the initial query.'
+			);
+		}
+
 		// Instantiate requested modules
 		$allModules = array();
 		$this->instantiateModules( $allModules, 'prop' );
