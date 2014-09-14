3dfff66524381c1b74631945ce88f9223c17c9f6
Bartosz Dziewoński <matma.rex@gmail.com>
2014-09-15 00:16:11 +0200

    mediawiki.api.options: New module for user preferences API (action=options)
    TODO Needs tests.
    
    Change-Id: Ia015898ca910923e00bc53f099b4e5631d6ad45c

---
+++ resources/src/mediawiki.api/mediawiki.api.options.js
@@ -0,0 +1,72 @@
+/**
+ * @class mw.Api.plugin.options
+ */
+( function ( mw, $ ) {
+
+	$.extend( mw.Api.prototype, {
+
+		/**
+		 * Asynchronously save the value of a single user option using the API. See #saveOptions.
+		 *
+		 * @param {string} name
+		 * @param {string} value
+		 * @return {jQuery.Promise}
+		 */
+		saveOption: function ( name, value ) {
+			var param = {};
+			param[name] = value;
+			return this.saveOptions( param );
+		},
+
+		/**
+		 * Asynchronously save the values of user options using the API.
+		 *
+		 * Any warnings returned by the API, including warnings about invalid option names or values,
+		 * are ignored. However, do not rely on this behavior.
+		 *
+		 * If necessary, the options will be saved using several parallel API requests. Only one promise
+		 * is always returned that will be resolved when all requests complete.
+		 *
+		 * @param {Object} options Options as a `{ name: value, … }` object
+		 * @return {jQuery.Promise}
+		 */
+		saveOptions: function ( options ) {
+			var grouped = [], deferreds = [], name, value;
+
+			for ( name in options ) {
+				if ( Object.prototype.hasOwnProperty.call( options, name ) ) {
+					value = String( options[name] );
+					// Can we bundle this option, or does it need a separate request?
+					if (
+						value.indexOf( '|' ) === -1 &&
+						name.indexOf( '|' ) === -1 && name.indexOf( '=' ) === -1
+					) {
+						grouped.push( name + '=' + value );
+					} else {
+						deferreds.push( this.postWithToken( 'options', {
+							action: 'options',
+							optionname: name,
+							optionvalue: value
+						} ) );
+					}
+				}
+			}
+
+			if ( grouped.length ) {
+				deferreds.push( this.postWithToken( 'options', {
+					action: 'options',
+					change: grouped.join( '|' )
+				} ) );
+			}
+
+			return $.when.apply( $, deferreds );
+		}
+
+	} );
+
+	/**
+	 * @class mw.Api
+	 * @mixins mw.Api.plugin.options
+	 */
+
+}( mediaWiki, jQuery ) );
