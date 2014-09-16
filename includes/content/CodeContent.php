9fd9f459e74cb780378e031192923a21a285afff
Yuri Astrakhan <yurik@wikimedia.org>
2014-09-15 20:59:13 -0400

    abstract CodeContent, reworked JsonContent
    * Merged common code from Css, JavaScript, Json Content objects into
      an abstract CodeContent class
    * Reworked JsonContent object:
    ** Support JSON decoding options
    ** Decoding produces a status object that may contain decoded object
    ** Saves optimally formatted, no spaces blob
    ** Removed beautifyJSON() - don't think we need it - encoding is different
       for different use-cases, plus the errors should be handled properly.
    ** Table rendering is done the same as other code content - with a <pre>
    
    Change-Id: Idbca6c091d61bf33174110dd3637a9a913e3503b

---
+++ includes/content/CodeContent.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Content object for the pages with code, such as CSS, JavaScript, JSON.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.24
+ *
+ * @file
+ * @ingroup Content
+ *
+ * @author Yuri Astrakhan
+ */
+
+/**
+ * Content object for the pages with code, such as CSS, JavaScript, JSON.
+ *
+ * @ingroup Content
+ */
+abstract class CodeContent extends TextContent {
+
+	/**
+	 * Returns a Content object with pre-save transformations applied using
+	 * Parser::preSaveTransform().
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param ParserOptions $popts
+	 *
+	 * @return CodeContent
+	 *
+	 * @see TextContent::preSaveTransform
+	 */
+	public function preSaveTransform( Title $title, User $user, ParserOptions $popts ) {
+		global $wgParser;
+		// @todo Make pre-save transformation optional for script pages
+
+		$text = $this->getNativeData();
+		$pst = $wgParser->preSaveTransform( $text, $title, $user, $popts );
+
+		return new static( $pst );
+	}
+
+	/**
+	 * Converts data to a <pre>...</pre> rendering of the code
+	 * @param string $cssClass CSS class, such as 'mw-css' or 'mw-js'
+	 * @param string|null $data if given, will be used instead of self::getNativeData()
+	 * @return string code wrapped in a <pre> tag with the specified css class
+	 */
+	protected function codeToPreElement( $cssClass, $data = null ) {
+		$html = "<pre class='mw-code $cssClass' dir='ltr'>\n";
+		$html .= htmlspecialchars( $data !== null ? $data : $this->getNativeData() );
+		$html .= "\n</pre>\n";
+
+		return $html;
+	}
+}
