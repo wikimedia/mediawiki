7efbb8ac1b99460e8779166699a7f569ae25561e
Timo Tijhof <krinklemail@gmail.com>
2014-09-20 20:06:58 -0700

    Split mediawiki.toolbar from mediawiki.action.edit
    Follows-up 285c52039bf4d2f9b.
    
    Get rid of mediawiki.mediawiki.action.toolbar. Not keeping a
    backwards-compat alias since this module is not supposed to be used
    publicly in its current form.
    
    Change-Id: I403216c6916e1f4d29216a55c3fe6c92eb68400b

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -3636,7 +3636,7 @@ HTML
 			)
 		);
 
-		$script = 'mw.loader.using("mediawiki.action.edit", function() {';
+		$script = 'mw.loader.using("mediawiki.toolbar", function () {';
 		foreach ( $toolarray as $tool ) {
 			if ( !$tool ) {
 				continue;
