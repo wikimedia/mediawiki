81ed1482ec74b77d90fe5283bb0ae311f5f5a418
Timo Tijhof <krinklemail@gmail.com>
2014-09-20 17:00:38 -0700

    Remove deprecated "jquery.json" module
    Announced in https://www.mail-archive.com/wikitech-l%40lists.wikimedia.org/msg78108.html
    
    Change-Id: Icc9dec0146cd374787124b7e1ed295c77737d11b

--- resources/src/jquery.json-deprecate.js
+++
@@ -1,8 +0,0 @@
-( function ( mw, $ ) {
-	// @deprecated since 1.24.  The 'jquery.json' module will be removed in MW 1.25.  Use the 'json' module.
-
-	mw.log.deprecate( $, 'toJSON', $.toJSON, 'Use JSON.stringify instead (module "json" for polyfill).' );
-	mw.log.deprecate( $, 'evalJSON', $.evalJSON, 'Use JSON.parse instead (module "json" for polyfill).' );
-	mw.log.deprecate( $, 'secureEvalJSON', $.secureEvalJSON, 'Use JSON.parse instead (module "json" for polyfill).' );
-	mw.log.deprecate( $, 'quoteString', $.quoteString, 'Use JSON.stringify instead (module "json" for polyfill).' );
-}( mediaWiki, jQuery ) );
