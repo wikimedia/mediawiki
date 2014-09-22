8191cb967e71d298eb8a1a483c0150621902d734
Kunal Mehta <legoktm@gmail.com>
2014-09-21 18:39:29 -0700

    Restructure and move InvalidPasswordTest
    Change-Id: If59879a53a3123859a2652075fcd89aa459c2d18

--- tests/phpunit/includes/password/InvalidPasswordTest.php
+++ tests/phpunit/includes/password/InvalidPasswordTest.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Testing framework for the Password infrastructure
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -20,7 +18,7 @@
  * @file
  */
 
-class PasswordTest extends MediaWikiTestCase {
+class InvalidPasswordTest extends MediaWikiTestCase {
 	/**
 	 * @covers InvalidPassword::equals
 	 */
