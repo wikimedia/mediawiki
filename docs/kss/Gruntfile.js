6ba5928cb67738e0d1d5e28c172894794761d106
Prateek Saxena <prtksxna@gmail.com>
2014-09-22 11:01:16 -0700

    kss: Add Gruntfile
    Bug: 58620
    Change-Id: If3f258e867819d97564e609c69a5daff09a08312

---
+++ docs/kss/Gruntfile.js
@@ -0,0 +1,22 @@
+/*!
+ * Grunt file
+ */
+
+/*jshint node:true*/
+
+module.exports = function( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+		jshint: {
+			options: {
+				jshint: true
+			},
+			dev: [ 'styleguide-template/public/*.js' ]
+		}
+	} );
+
+	grunt.registerTask( 'default', [ 'jshint' ] );
+};
