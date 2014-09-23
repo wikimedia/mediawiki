e11dbf19f82f48f77e450afd006b267a69118fc2
Timo Tijhof <krinklemail@gmail.com>
2014-09-22 17:35:33 -0700

    PrefixSearch: Add unit tests for StringPrefixSearch
    Change-Id: If0fa66b212f70ea39d2e7feec117930fcd1b9642

---
+++ tests/phpunit/includes/PrefixSearchTest.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * @group Search
+ * @group Database
+ */
+class PrefixSearchTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+
+		// Avoid special pages from extensions interferring with the tests
+		$this->setMwGlobals( 'wgSpecialPages', array() );
+	}
+
+	protected function searchProvision( Array $results = null ) {
+		if ( $results === null ) {
+			$this->setMwGlobals( 'wgHooks', array() );
+		} else {
+			$this->setMwGlobals( 'wgHooks', array(
+				'PrefixSearchBackend' => array(
+					function ( $namespaces, $search, $limit, &$srchres ) use ( $results ) {
+						$srchres = $results;
+						return false;
+					}
+				),
+			) );
+		}
+	}
+
+	public function addDBData() {
+		$this->insertPage( 'Sandbox' );
+
+		$this->insertPage( 'Example' );
+		$this->insertPage( 'Example Bar' );
+		$this->insertPage( 'Example Foo' );
+		$this->insertPage( 'Example Foo/Bar' );
+		$this->insertPage( 'Example/Baz' );
+
+		$this->insertPage( 'Talk:Sandbox' );
+		$this->insertPage( 'Talk:Example' );
+
+		$this->insertPage( 'User:Example' );
+	}
+
+	/**
+	 * @covers PrefixSearch::search
+	 * @covers PrefixSearch::searchBackend
+	 */
+	public function testSearch() {
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( '', 3 );
+		$this->assertEquals(
+			array(),
+			$results,
+			'Empty string'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Ex', 3 );
+		$this->assertEquals(
+			array(
+				'Example',
+				'Example/Baz',
+				'Example Bar',
+			),
+			$results,
+			'Main namespace with title prefix'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Talk:', 3 );
+		$this->assertEquals(
+			array(
+				'Talk:Example',
+				'Talk:Sandbox',
+			),
+			$results,
+			'Talk namespace prefix'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'User:', 3 );
+		$this->assertEquals(
+			array(
+				'User:Example',
+			),
+			$results,
+			'User namespace prefix'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Special:', 3 );
+		$this->assertEquals(
+			array(
+				'Special:ActiveUsers',
+				'Special:AllMessages',
+				'Special:AllMyFiles',
+			),
+			$results,
+			'Special namespace prefix'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Special:Un', 3 );
+		$this->assertEquals(
+			array(
+				'Special:Unblock',
+				'Special:UncategorizedCategories',
+				'Special:UncategorizedFiles',
+			),
+			$results,
+			'Special namespace with prefix'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Special:EditWatchlist', 3 );
+		$this->assertEquals(
+			array(
+				'Special:EditWatchlist',
+			),
+			$results,
+			'Special page name'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Special:EditWatchlist/', 3 );
+		$this->assertEquals(
+			array(
+				'Special:EditWatchlist/clear',
+				'Special:EditWatchlist/raw',
+			),
+			$results,
+			'Special page subpages'
+		);
+
+		$this->searchProvision( null );
+		$searcher = new StringPrefixSearch;
+		$results = $searcher->search( 'Special:EditWatchlist/cl', 3 );
+		$this->assertEquals(
+			array(
+				'Special:EditWatchlist/clear',
+			),
+			$results,
+			'Special page subpages with prefix'
+		);
+	}
+}
