b10d5012b5c8a26d98169e4501b5ad6301a1870b
Kunal Mehta <legoktm@gmail.com>
2014-09-23 15:41:03 -0700

    Support redirects in JavaScriptContent
    When a JavaScript page is moved, a "redirect" in the form of
    mw.loader.load(...) will be left behind, so any other
    JavaScript loading the page that way will still work, albeit
    with an extra HTTP request.
    
    This also implements Content::getRedirectTarget(), so redirects
    are marked properly in the database, and users viewing them
    are redirected properly. A magic "/* #REDIRECT */" comment
    must be in front of the mw.loader.load call. This is done so
    that pages which currently are just one mw.loader.load call
    aren't turned into redirects.
    
    Bug: 71200
    Bug: 33973
    Change-Id: I10fdff087a901da56fad64531f0e382f90ebcf37

--- includes/content/JavaScriptContent.php
+++ includes/content/JavaScriptContent.php
@@ -33,6 +33,11 @@
 class JavaScriptContent extends TextContent {
 
 	/**
+	 * @var bool|Title|null
+	 */
+	private $redirectTarget = false;
+
+	/**
 	 * @param string $text JavaScript code.
 	 * @param string $modelId the content model name
 	 */
@@ -71,6 +76,48 @@ class JavaScriptContent extends TextContent {
 		$html .= "\n</pre>\n";
 
 		return $html;
+	}
+
+	/**
+	 * If this page is a redirect, return the content
+	 * if it should redirect to $target instead
+	 *
+	 * @param Title $target
+	 * @return JavaScriptContent
+	 */
+	public function updateRedirect( Title $target ) {
+		if ( !$this->isRedirect() ) {
+			return $this;
+		}
+
+		return $this->getContentHandler()->makeRedirectContent( $target );
+	}
+
+	/**
+	 * @return Title|null
+	 */
+	public function getRedirectTarget() {
+		if ( $this->redirectTarget !== false ) {
+			return $this->redirectTarget;
+		}
+		$this->redirectTarget = null;
+		$text = $this->getNativeData();
+		if ( strpos( $text, '/* #REDIRECT */' ) === 0 ) {
+			// Extract the title from the url
+			preg_match( '/title=(.*?)\\\\u0026action=raw/', $text, $matches );
+			if ( isset( $matches[1] ) ) {
+				$title = Title::newFromText( $matches[1] );
+				if ( $title ) {
+					// Have a title, check that the current content equals what
+					// the redirect content should be
+					if ( $this->equals( $this->getContentHandler()->makeRedirectContent( $title ) ) ) {
+						$this->redirectTarget = $title;
+					}
+				}
+			}
+		}
+
+		return $this->redirectTarget;
 	}
 
 }
