e665ae6a70f89ceca69309ff7a9c1663c2132ba7
Brad Jorsch <bjorsch@wikimedia.org>
2014-09-29 14:40:51 -0400

    PasswordFactory::newFromPlaintext( null ) needs to work
    Various code passes null around to mean "an invalid password". It
    shouldn't all have to test for null and specially handle that.
    
    This also fixes a codepath where User::$mNewpassword could get set to an
    empty string rather than a password object, which would cause problems
    later when anything else tries to use it.
    
    Bug: 71421
    Change-Id: Ib5f94b52c07e7dba89328b98fb43c86db95ee09f

--- includes/password/PasswordFactory.php
+++ includes/password/PasswordFactory.php
@@ -141,11 +141,15 @@ final class PasswordFactory {
 	 * If no existing object is given, make a new default object. If one is given, clone that
 	 * object. Then pass the plaintext to Password::crypt().
 	 *
-	 * @param string $password Plaintext password
+	 * @param string|null $password Plaintext password, or null for an invalid password
 	 * @param Password|null $existing Optional existing hash to get options from
 	 * @return Password
 	 */
 	public function newFromPlaintext( $password, Password $existing = null ) {
+		if ( $password === null ) {
+			return new InvalidPassword( $this, array( 'type' => '' ), null );
+		}
+
 		if ( $existing === null ) {
 			$config = $this->types[$this->default];
 			$obj = new $config['class']( $this, $config );
