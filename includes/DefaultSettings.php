33c76b6a8d718a13f791848420d15297b28f2e55
mglaser <glaser@hallowelt.biz>
2014-10-01 16:31:43 +0200

    Updated release notes and version number for MediaWiki 1.19.20
    This is MediaWiki 1.19.20 security release
    
    Change-Id: I9f7a2ef0be1a7333ec348886f8b2efe06db6f73c

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -33,7 +33,7 @@ $wgConf = new SiteConfiguration;
 /** @endcond */
 
 /** MediaWiki version number */
-$wgVersion = '1.19.19';
+$wgVersion = '1.19.20';
 
 /** Name of the site. It must be changed in LocalSettings.php */
 $wgSitename = 'MediaWiki';
