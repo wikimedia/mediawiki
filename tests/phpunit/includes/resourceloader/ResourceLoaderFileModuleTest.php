aef9c8bb66541c30b26078f3210ef48b728b1616
jdlrobson <jdlrobson@gmail.com>
2014-10-09 17:07:14 -0700

    WIP: Add RL template module with HTML markup language
    Preparation work for templating in core.
    RL should allow us to ship HTML / template markup from server to client.
    Use in Special:Upload and mediawiki.feedback as a proof of concept.
    Separation of concerns etc...
    
    TODO:
    Allow aliasing of template names
    'mediawiki.module' => array( 'templates' => array( 'foo' => 'file/foo.html' ) )
    See Also:
    Ia63d6b6868f23a773e4a41daa0036d4bf2cd6724
    
    Change-Id: I6ff38c12897e3164969a1090449e626001926c3b

---
+++ tests/phpunit/includes/resourceloader/ResourceLoaderFileModuleTest.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * @group ResourceLoader
+ */
+class ResourceLoaderFileModuleTest extends MediaWikiTestCase {
+	static function getModules() {
+		$base = array(
+			'localBasePath' => realpath( dirname( __FILE__ ) ),
+		);
+
+		return array(
+			'noTemplateModule' => array(),
+
+			'badFilePathModule' => array(
+				'localBasePath' => '\/tmp\/',
+				'templates' => array(
+					'templates/template.html',
+				),
+			),
+
+			'htmlTemplateModule' => $base + array(
+				'templates' => array(
+					'templates/template.html',
+					'templates/template2.html',
+				)
+			),
+
+			'templateModuleHandlebars' => $base + array(
+				'templates' => array(
+					'templates/template_awesome.handlebars',
+				),
+			),
+		);
+	}
+
+	public function providerGetTemplateNames() {
+		$modules = self::getModules();
+		return array(
+			array(
+				$modules['noTemplateModule'], array(),
+			),
+			array(
+				$modules['htmlTemplateModule'],
+				array(
+					'templates/template.html',
+					'templates/template2.html',
+				),
+			)
+		);
+	}
+
+	public function providerGetTemplates() {
+		$modules = self::getModules();
+
+		return array(
+			array(
+				$modules['noTemplateModule'],
+				array(),
+			),
+			array(
+				$modules['templateModuleHandlebars'],
+				array(
+					'templates/template_awesome.handlebars' => 'wow\n',
+				),
+			),
+			array(
+				$modules['htmlTemplateModule'],
+				array(
+					'templates/template.html' => 'hello\n',
+					'templates/template2.html' => 'goodbye\n',
+				),
+			)
+		);
+	}
+
+	public function providerGetModifiedTimeTemplates() {
+		$modules = self::getModules();
+
+		return array(
+			// Check the default value when no templates present in module is 1
+			array( $modules['noTemplateModule'], 1 ),
+			// and also when the file paths are bad
+			array( $modules['badFilePathModule'], 1 ),
+		);
+	}
+
+	// tests
+
+	/**
+	 * @dataProvider providerGetTemplateNames
+	 */
+	public function testGetTemplateNames( $module, $expected ) {
+		$rl = new ResourceLoaderFileModule( $module );
+		$names = $rl->getTemplateNames();
+
+		$this->assertEquals( $names, $expected );
+	}
+
+	/**
+	 * @dataProvider providerGetTemplates
+	 */
+	public function testGetTemplates( $module, $expected ) {
+		$rl = new ResourceLoaderFileModule( $module );
+
+		$this->assertEquals( $rl->getTemplates(), $expected );
+	}
+
+	/**
+	 * @dataProvider providerGetModifiedTimeTemplates
+	 */
+	public function testGetModifiedTimeTemplates( $module, $expected ) {
+		$rl = new ResourceLoaderFileModule( $module );
+		$ts = $rl->getModifiedTimeTemplates( new ResourceLoaderContext(
+			new ResourceLoader, new WebRequest() ) );
+		$this->assertEquals( $ts, $expected );
+	}
+}
