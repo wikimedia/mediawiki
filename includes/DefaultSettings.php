1b1f9a18973745df071e37e5fb70787f559887ac
Kunal Mehta <legoktm@gmail.com>
2014-10-09 23:46:12 -0700

    Make allowing site-wide styles on restricted special pages a config option
    This mostly reverts commit 614d7e5c274d927f99bfc52ac3a1e6c7e5902408.
    
    Many wikis use MediaWiki:Common.css and associated pages to create a
    custom "theme" for their wiki, which would no longer load on login
    or preference pages, creating an inconsistent UI.
    
    This re-adds the difference in module origin for different types
    (styles, scripts, etc.), and now OutputPage::disallowUserJs()
    checks the value of the "AllowSiteCSSOnRestrictedPages" config setting
    to determine whether to allow site-wide CSS styles or not.
    
    By default this feature is disabled to be secure by default.
    
    Bug: 71621
    Change-Id: I1bf4dd1845b6952c3985e179fbea48181ffb8907

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3322,6 +3322,38 @@ $wgResourceLoaderLESSImportPaths = array(
 	"$IP/resources/mediawiki.less/",
 );
 
+<<<<<<< HEAD
+=======
+/**
+ * Whether ResourceLoader should attempt to persist modules in localStorage on
+ * browsers that support the Web Storage API.
+ *
+ * @since 1.23 - Client-side module persistence is experimental. Exercise care.
+ */
+$wgResourceLoaderStorageEnabled = false;
+
+/**
+ * Cache version for client-side ResourceLoader module storage. You can trigger
+ * invalidation of the contents of the module store by incrementing this value.
+ *
+ * @since 1.23
+ */
+$wgResourceLoaderStorageVersion = 1;
+
+/**
+ * Whether to allow site-wide CSS (MediaWiki:Common.css and friends) on
+ * restricted pages like Special:UserLogin or Special:Preferences where
+ * JavaScript is disabled for security reasons. As it is possible to
+ * execute JavaScript through CSS, setting this to true opens up a
+ * potential security hole. Some sites may "skin" their wiki by using
+ * site-wide CSS, causing restricted pages to look unstyled and different
+ * from the rest of the site.
+ *
+ * @since 1.25
+ */
+$wgAllowSiteCSSOnRestrictedPages = false;
+
+>>>>>>> f9c0a42... Make allowing site-wide styles on restricted special pages a config option
 /** @} */ # End of resource loader settings }
 
 /*************************************************************************//**
