900d27366015c2e7545c352f4700a938ffcc6d4a
jdlrobson <jdlrobson@gmail.com>
2014-10-09 17:07:14 -0700

    Add support for delivering templates via ResourceLoader
    A base ResourceLoaderModule::getTemplates() exists for subclasses
    to override. An implementation is provided for ResourceLoaderFileModule.
    
    For file modules, templates can be specified in the following manner:
    
    'some.module' => array(
    	'templates' => array(
    		'alias' => 'path/to/template.html',
    	),
    	'dependencies' => array(
    		'mediawiki.templates',
    	),
    ),
    
    The delivery system is template language agnostic, and currently
    only supports "compiling" raw HTML templates.
    
    This also adds template support to the following modules as a POC:
    * mediawiki.feedback
    * mediawiki.action.view.postEdit
    * mediawiki.special.upload
    
    Change-Id: Ia0c5c8ec960aa6dff12c9626cee41ae9a3286b76

--- includes/resourceloader/ResourceLoaderModule.php
+++ includes/resourceloader/ResourceLoaderModule.php
@@ -135,6 +135,16 @@ abstract class ResourceLoaderModule {
 	}
 
 	/**
+	 * Takes named templates by the module and returns an array mapping.
+	 *
+	 * @return array of templates mapping template alias to content
+	 */
+	public function getTemplates() {
+		// Stub, override expected.
+		return array();
+	}
+
+	/**
 	 * @return Config
 	 * @since 1.24
 	 */
