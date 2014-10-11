5cca0292b04ccf0471943c8e5a8685db1eac4043
James D. Forrester <jforrester@wikimedia.org>
2014-10-10 18:05:44 -0700

    Update OOjs UI to v0.1.0-pre (837b2f733e)
    New changes:
    d40dd30 build: Update grunt-contrib-uglify to v0.6.0
    0d89c99 test: Load a theme for the test HTML file so the tests don't fail
    f245aa4 Element: Remove test for deprecated onDOMEvent()
    cd15f18 Follow-up I730e3d7d: Tab not spaces
    a923881 build: Use Karma instead of grunt-contrib-qunit
    381864c build: Use Chrome and Firefox for tests instead of PhantomJS
    044faa5 Follow-up If2da01a2: Fix grunt watch
    d6e7af8 Adding OO.ui.TitledElement as mixin to label widget
    6385af7 MediaWiki theme: Add filled white background to toolbars
    837b2f7 Revert "Try to stop user from tabbing outside of open dialog box"
    
    Change-Id: I63d51311f73d1295a2a35ebadf7d9ac835dec5c7

--- resources/lib/oojs-ui/oojs-ui-mediawiki.js
+++ resources/lib/oojs-ui/oojs-ui-mediawiki.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (26dadbc60f)
+ * OOjs UI v0.1.0-pre (837b2f733e)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-10-06T16:03:39Z
+ * Date: 2014-10-11T01:05:29Z
  */
 /**
  * @class
