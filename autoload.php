7ff51d09503d0854d46a01b72fcab0c5dc7366a7
Adam Roses Wight <awight@wikimedia.org>
2014-10-12 14:26:14 -0700

    Componentize Title permissions logic
    Move Title code dealing with permissions and restrictions into
    TitleSecurity, under a new security/ component.
    
    Public functions on Title are delegated to the helper, but should
    eventually be deprecated.
    
    See https://www.mediawiki.org/wiki/Requests_for_comment/Page_protection_as_a_component
    
    TODO:
    * Verify that tests cover all changed code.
    
    Bug: T386
    Change-Id: Ifa5671f5e4cf16cf5447bf132b5081af95e543ee

--- autoload.php
+++ autoload.php
@@ -1210,6 +1210,7 @@ $wgAutoloadLocalClasses = array(
 	'TitleFormatter' => __DIR__ . '/includes/title/TitleFormatter.php',
 	'TitleParser' => __DIR__ . '/includes/title/TitleParser.php',
 	'TitlePrefixSearch' => __DIR__ . '/includes/PrefixSearch.php',
+	'TitleSecurity' => __DIR__ . '/includes/security/TitleSecurity.php',
 	'TitleValue' => __DIR__ . '/includes/title/TitleValue.php',
 	'TrackBlobs' => __DIR__ . '/maintenance/storage/trackBlobs.php',
 	'TraditionalImageGallery' => __DIR__ . '/includes/gallery/TraditionalImageGallery.php',
