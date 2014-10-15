2f949f327cb7d7acbf5f44cfbc35de2f7e2efaf4
Kunal Mehta <legoktm@gmail.com>
2014-10-14 17:31:15 -0700

    Implement extension registration from an extension.json file
    Introduces wfLoadExtension()/wfLoadSkin() which should be used in
    LocalSettings.php rather than require-ing a PHP entry point.
    
    Extensions and skins would add "extension.json" or "skin.json" files
    in their root, which contains all the information typically
    present in PHP entry point files (classes to autoload, special pages,
    API modules, etc.) A full schema can be found at
    docs/extension.schema.json, and a script to validate these to the
    schema is provided. An additional script is provided to convert
    typical PHP entry point files into their JSON equivalents.
    
    The basic flow of loading an extension goes like:
     * Get the ExtensionRegistry singleton instance
     * ExtensionRegistry takes a filename, reads the file or tries
       to get the parsed JSON from APC if possible.
     * The JSON is run through a Processor instance,
       which registers things with the appropriate
       global settings.
     * The output of the processor is cached in APC if possible.
     * The extension/skin is marked as loaded in the
       ExtensionRegistry and a callback function is executed
       if one was specified.
    
    For ideal performance, a batch loading method is also provided:
     * The absolute path name to the JSON file is queued
       in the ExtensionRegistry instance.
     * When loadFromQueue() is called, it constructs a hash
       unique to the members of the current queue, and sees
       if the queue has been cached in APC. If not, it processes
       each file individually, and combines the result of each
       Processor into one giant array, which is cached in APC.
     * The giant array then sets various global settings,
       defines constants, and calls callbacks.
    
    To invalidate the cached processed info, by default the mtime
    of each JSON file is checked. However that can be slow if you
    have a large number of extensions, so you can set $wgExtensionInfoMTime
    to the mtime of one file, and `touch` it whenever you update
    your extensions.
    
    Change-Id: I7074b65d07c5c7d4e3f1fb0755d74a0b07ed4596

---
+++ includes/registration/ExtensionProcessor.php
@@ -0,0 +1,259 @@
+<?php
+
+class ExtensionProcessor implements Processor {
+
+	/**
+	 * Keys that should be set to $GLOBALS
+	 *
+	 * @var array
+	 */
+	protected static $globalSettings = array(
+		'ResourceLoaderSources',
+		'ResourceLoaderLESSVars',
+		'ResourceLoaderLESSImportPaths',
+		'TrackingCategories',
+		'DefaultUserOptions',
+		'HiddenPrefs',
+		'GroupPermissions',
+		'RevokePermissions',
+		'ImplicitGroups',
+		'GroupsAddToSelf',
+		'GroupsRemoveFromSelf',
+		'AddGroups',
+		'RemoveGroups',
+		'AvailableRights',
+		'ContentHandlers',
+		'RateLimits',
+		'ParserTestFiles',
+		'RecentChangesFlags',
+		'ExtensionFunctions',
+		'ExtensionEntryPointListFiles',
+		'SpecialPages',
+		'SpecialPageGroups',
+		'JobClasses',
+		'LogTypes',
+		'LogRestrictions',
+		'FilterLogTypes',
+		'LogNames',
+		'LogHeaders',
+		'LogActions',
+		'LogActionsHandlers',
+		'Actions',
+		'APIModules',
+		'APIFormatModules',
+		'APIMetaModules',
+		'APIPropModules',
+		'APIListModules',
+	);
+
+	/**
+	 * Keys that are part of the extension credits
+	 *
+	 * @var array
+	 */
+	protected static $creditsAttributes = array(
+		'name',
+		'author',
+		'version',
+		'url',
+		'description',
+		'descriptionmsg',
+		'license-name',
+	);
+
+	/**
+	 * Stuff that is going to be set to $GLOBALS
+	 *
+	 * Some keys are pre-set to arrays so we can += to them
+	 *
+	 * @var array
+	 */
+	protected $globals = array(
+		'wgExtensionMessagesFiles' => array(),
+		'wgMessagesDirs' => array(),
+	);
+
+	/**
+	 * Things that should be define()'d
+	 *
+	 * @var array
+	 */
+	protected $defines = array();
+
+	/**
+	 * Things to be called once registration of these extensions are done
+	 *
+	 * @var callable[]
+	 */
+	protected $callbacks = array();
+
+	/**
+	 * @var array
+	 */
+	protected $credits = array();
+
+	/**
+	 * Any thing else in the $info that hasn't
+	 * already been processed
+	 *
+	 * @var array
+	 */
+	protected $attributes = array();
+
+	/**
+	 * List of keys that have already been processed
+	 *
+	 * @var array
+	 */
+	protected $processed = array();
+
+	/**
+	 * @param string $path
+	 * @param array $info
+	 * @return array
+	 */
+	public function extractInfo( $path, array $info ) {
+		$this->extractConfig( $info );
+		$this->extractHooks( $info );
+		$dir = dirname( $path );
+		$this->extractMessageSettings( $dir, $info );
+		$this->extractNamespaces( $info );
+		$this->extractResourceLoaderModules( $dir, $info );
+		if ( isset( $info['callback'] ) ) {
+			$this->callbacks[] = $info['callback'];
+			$this->processed[] = 'callback';
+		}
+
+		$this->extractCredits( $path, $info );
+		foreach ( $info as $key => $val ) {
+			if ( in_array( $key, self::$globalSettings ) ) {
+				$this->storeToArray( "wg$key", $val, $this->globals );
+			} elseif ( !in_array( $key, $this->processed ) ) {
+				$this->storeToArray( $key, $val, $this->attributes );
+			}
+		}
+
+
+
+	}
+
+	public function getExtractedInfo() {
+		return array(
+			'globals' => $this->globals,
+			'defines' => $this->defines,
+			'callbacks' => $this->callbacks,
+			'credits' => $this->credits,
+			'attributes' => $this->attributes,
+		);
+	}
+
+	protected function extractHooks( array $info ) {
+		if ( isset( $info['Hooks'] ) ) {
+			foreach ( $info['Hooks'] as $name => $callable ) {
+				$this->globals['wgHooks'][$name][] = $callable;
+			}
+			$this->processed[] = 'Hooks';
+		}
+	}
+
+	/**
+	 * Register namespaces with the appropriate global settings
+	 *
+	 * @param array $info
+	 */
+	protected function extractNamespaces( array $info ) {
+		if ( isset( $info['namespaces'] ) ) {
+			foreach ( $info['namespaces'] as $ns ) {
+				$id = $ns['id'];
+				$this->defines[$ns['constant']] = $id;
+				$this->globals['wgExtraNamespaces'][$id] = $ns['name'];
+				if ( isset( $ns['gender'] ) ) {
+					$this->globals['wgExtraGenderNamespaces'][$id] = $ns['gender'];
+				}
+				if ( isset( $ns['subpages'] ) && $ns['subpages'] ) {
+					$this->globals['wgNamespacesWithSubpages'][$id] = true;
+				}
+				if ( isset( $ns['content'] ) && $ns['content'] ) {
+					$this->globals['wgContentNamespaces'][] = $id;
+				}
+				if ( isset( $ns['defaultcontentmodel'] ) ) {
+					$this->globals['wgNamespaceContentModels'][$id] = $ns['defaultcontentmodel'];
+				}
+			}
+			$this->processed[] = 'namespaces';
+		}
+	}
+
+	protected function extractResourceLoaderModules( $dir, array $info ) {
+		if ( isset( $info['ResourceModules'] ) ) {
+			foreach ( $info['ResourceModules'] as $name => $data ) {
+				if ( isset( $data['localBasePath'] ) ) {
+					$data['localBasePath'] = "$dir/{$data['localBasePath']}";
+				}
+				$this->globals['wgResourceModules'][$name] = $data;
+			}
+		}
+	}
+
+	/**
+	 * Set message-related settings, which need to be expanded to use
+	 * absolute paths
+	 *
+	 * @param string $dir
+	 * @param array $info
+	 */
+	protected function extractMessageSettings( $dir, array $info ) {
+		foreach ( array( 'ExtensionMessagesFiles', 'MessagesDirs' ) as $key ) {
+			if ( isset( $info[$key] ) ) {
+				$this->globals["wg$key"] += array_map( function( $file ) use ( $dir ) {
+					return "$dir/$file";
+				}, $info[$key] );
+				$this->processed[] = $key;
+			}
+		}
+	}
+
+	protected function extractCredits( $path, array $info ) {
+		$credits = array(
+			'path' => $path,
+			'type' => isset( $info['type'] ) ? $info['type'] : 'other',
+		);
+		$this->processed[] = 'type';
+		foreach ( self::$creditsAttributes as $attr ) {
+			if ( isset( $info[$attr] ) ) {
+				$credits[$attr] = $info[$attr];
+				$this->processed[] = $attr;
+			}
+		}
+
+		$this->credits[$credits['name']] = $credits;
+	}
+
+	/**
+	 * Set configuration settings
+	 * @todo In the future, this should be done via Config interfaces
+	 *
+	 * @param array $info
+	 */
+	protected function extractConfig( array $info ) {
+		if ( isset( $info['config'] ) ) {
+			foreach ( $info['config'] as $key => $val ) {
+				$this->globals["wg$key"] = $val;
+			}
+			$this->processed[] = 'config';
+		}
+	}
+
+	/**
+	 * @param string $name
+	 * @param mixed $value
+	 * @param array &$array
+	 */
+	protected function storeToArray( $name, $value, &$array ) {
+		if ( isset( $array[$name] ) ) {
+			$array[$name] = array_merge_recursive( $array[$name], $value );
+		} else {
+			$array[$name] = $value;
+		}
+	}
+}
---
+++ tests/phpunit/includes/registration/ExtensionProcessorTest.php
@@ -0,0 +1,135 @@
+<?php
+
+class ExtensionProcessorTest extends MediaWikiTestCase {
+
+	private $dir;
+
+	public function setUp() {
+		parent::setUp();
+		$this->dir = __DIR__ . '/FooBar/extension.json';
+	}
+
+	/**
+	 * 'name' is absolutely required
+	 *
+	 * @var array
+	 */
+	static $default = array(
+		'name' => 'FooBar',
+	);
+
+	public static function provideRegisterHooks() {
+		return array(
+			// No hooks
+			array(
+				array(),
+				self::$default,
+				array(),
+			),
+			// No current hooks, adding one for "FooBaz"
+			array(
+				array(),
+				array( 'Hooks' => array( 'FooBaz' => 'FooBazCallback' ) ) + self::$default,
+				array( 'FooBaz' => array( 'FooBazCallback' ) ),
+			),
+			// Hook for "FooBaz", adding another one
+			array(
+				array( 'FooBaz' => array( 'PriorCallback' ) ),
+				array( 'Hooks' => array( 'FooBaz' => 'FooBazCallback' ) ) + self::$default,
+				array( 'FooBaz' => array( 'PriorCallback', 'FooBazCallback' ) ),
+			),
+			// Hook for "BarBaz", adding one for "FooBaz"
+			array(
+				array( 'BarBaz' => array( 'BarBazCallback' ) ),
+				array( 'Hooks' => array( 'FooBaz' => 'FooBazCallback' ) ) + self::$default,
+				array(
+					'BarBaz' => array( 'BarBazCallback' ),
+					'FooBaz' => array( 'FooBazCallback' ),
+				),
+			),
+		);
+	}
+
+	/**
+	 * @covers ExtensionProcessor::extractHooks
+	 * @dataProvider provideRegisterHooks
+	 */
+	public function testRegisterHooks( $pre, $info, $expected ) {
+		$processor = new MockExtensionProcessor( array( 'wgHooks' => $pre ) );
+		$processor->extractInfo( $this->dir, $info );
+		$extracted = $processor->getExtractedInfo();
+		$this->assertEquals( $expected, $extracted['globals']['wgHooks'] );
+	}
+
+	/**
+	 * @covers ExtensionProcessor::extractConfig
+	 */
+	public function testExtractConfig() {
+		$processor = new ExtensionProcessor;
+		$info = array(
+			'config' => array(
+				'Bar' => 'somevalue',
+				'Foo' => 10,
+			),
+		) + self::$default;
+		$processor->extractInfo( $this->dir, $info );
+		$extracted = $processor->getExtractedInfo();
+		$this->assertEquals( 'somevalue', $extracted['globals']['wgBar'] );
+		$this->assertEquals( 10, $extracted['globals']['wgFoo'] );
+	}
+
+	public static function provideSetToGlobal() {
+		return array(
+			array(
+				array( 'wgAPIModules', 'wgAvailableRights' ),
+				array(),
+				array(
+					'APIModules' => array( 'foobar' => 'ApiFooBar' ),
+					'AvailableRights' => array( 'foobar', 'unfoobar' ),
+				),
+				array(
+					'wgAPIModules' => array( 'foobar' => 'ApiFooBar' ),
+					'wgAvailableRights' => array( 'foobar', 'unfoobar' ),
+				),
+			),
+			array(
+				array( 'wgAPIModules', 'wgAvailableRights' ),
+				array(
+					'wgAPIModules' => array( 'barbaz' => 'ApiBarBaz' ),
+					'wgAvailableRights' => array( 'barbaz' )
+				),
+				array(
+					'APIModules' => array( 'foobar' => 'ApiFooBar' ),
+					'AvailableRights' => array( 'foobar', 'unfoobar' ),
+				),
+				array(
+					'wgAPIModules' => array( 'barbaz' => 'ApiBarBaz', 'foobar' => 'ApiFooBar' ),
+					'wgAvailableRights' => array( 'barbaz', 'foobar', 'unfoobar' ),
+				),
+			),
+			array(
+				array( 'wgGroupPermissions' ),
+				array(
+					'wgGroupPermissions' => array( 'sysop' => array( 'delete' ) ),
+				),
+				array(
+					'GroupPermissions' => array( 'sysop' => array( 'undelete' ), 'user' => array( 'edit' ) ),
+				),
+				array(
+					'wgGroupPermissions' => array( 'sysop' => array( 'delete', 'undelete' ), 'user' => array( 'edit' ) ),
+				)
+			)
+		);
+	}
+}
+
+
+/**
+ * Allow overriding the default value of $this->globals
+ * so we can test merging
+ */
+class MockExtensionProcessor extends ExtensionProcessor {
+	public function __construct( $globals = array() ) {
+		$this->globals = $globals + $this->globals;
+	}
+}
