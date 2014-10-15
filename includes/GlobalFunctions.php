53373d90324fe93f255be33045041293d929d866
Kunal Mehta <legoktm@gmail.com>
2014-10-14 17:31:15 -0700

    [WIP] Implement extension registration from an extension.json file
    Introduces wfLoadExtension()/wfLoadSkin() which should be used in
    LocalSettings.php rather than require-ing a PHP entry point.
    
    Extensions and skins would add "extension.json" or "skin.json" files
    in their root, which contains all the information typically
    present in PHP entry point files (classes to autoload, special pages,
    API modules, etc.) A full schema can be found at docs/extension.schema.json,
    and a script to validate these to the schema is provided. An additional
    script is provided to convert PHP entry point files into their
    JSON equivalents.
    
    The basic flow of loading an extension goes like:
    * Get the ExtensionRegistry singleton instance
    * ExtensionRegistry takes a filename, reads the file or tries
      to get the parsed JSON from APC if possible.
    * The JSON is run through a Processor instance,
      which registers things with the appropriate
      global settings.
    * The extension/skin is marked as loaded in the
      ExtensionRegistry and a callback function is executed
      if specified.
    
    For ideal performance, a batch loading method is also
    provided:
    * The absolute path name to the JSON file is queued
      in the ExtensionRegistry instance.
    * When loadFromQueue() is called, it constructs a hash
      unique to the members of the current queue, and sees
      if the queue has been cached in APC. If not, it processes
      each file individually, and combines the result of each
      Processor into one giant array, which is cached in APC.
    * The giant array then sets various global settings,
      defines constants, and calls callbacks.
    
    TODO:
    * Test performance impact (mtime invalidation?)
    * Fix RL module paths
    * ...
    
    Change-Id: I7074b65d07c5c7d4e3f1fb0755d74a0b07ed4596

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -160,6 +160,32 @@ if ( !function_exists( 'hash_equals' ) ) {
 }
 /// @endcond
 
+function wfLoadExtension( $name, $path = null ) {
+	if ( !$path ) {
+		global $IP;
+		$path = "$IP/extensions/$name/extension.json";
+	}
+	ExtensionRegistry::getInstance()->load( $path );
+}
+
+function wfLoadExtensions( array $exts ) {
+	global $IP;
+	$registry = ExtensionRegistry::getInstance();
+	foreach ( $exts as $ext ) {
+		$registry->queue( "$IP/extensions/$ext/extension.json" );
+	}
+
+	$registry->loadFromQueue();
+}
+
+function wfLoadSkin( $name, $path = null ) {
+	if ( !$path ) {
+		global $IP;
+		$path = "$IP/skins/$name/skin.json";
+	}
+	ExtensionRegistry::getInstance()->load( $path );
+}
+
 /**
  * Like array_diff( $a, $b ) except that it works with two-dimensional arrays.
  * @param array $a
