2c57d8dddfbd73816ea5168411b9625df031a272
Roan Kattouw <roan.kattouw@gmail.com>
2014-10-17 16:34:04 -0700

    Update OOjs UI for cherry-picks
    New changes:
    8fa7b43 Position popups and their anchors correctly in RTL
    ba5901b TextInputMenuWidget: Handle undefined value properly when toggling visibility
    
    Change-Id: Ic82ebc0576f7e9c36a9bf178cb3efe11fa856a5d

--- resources/lib/oojs-ui/oojs-ui.js
+++ resources/lib/oojs-ui/oojs-ui.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (d74a46ca6a)
+ * OOjs UI v0.1.0-pre (ba5901bd56)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-10-15T00:40:17Z
+ * Date: 2014-10-17T23:26:52Z
  */
 ( function ( OO ) {
 
@@ -11272,7 +11272,7 @@ OO.ui.TextInputMenuWidget.prototype.onWindowResize = function () {
  * @inheritdoc
  */
 OO.ui.TextInputMenuWidget.prototype.toggle = function ( visible ) {
-	visible = !!visible;
+	visible = visible === undefined ? !this.isVisible() : !!visible;
 
 	var change = visible !== this.isVisible();
 
