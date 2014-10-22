28d45b2cdf8c5c329b789bf52878d73571b9e9d0
James D. Forrester <jforrester@wikimedia.org>
2014-10-22 16:42:29 -0700

    Update OOjs UI to v0.1.0-pre (40de4dabe6)
    New changes:
    849a687 Mute text color of accelerators in tool groups
    40de4da Remind people to run composer install
    
    Change-Id: I91c369da9170b6561906d27d193ce42b02ade021

--- resources/lib/oojs-ui/oojs-ui.js
+++ resources/lib/oojs-ui/oojs-ui.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (531d497d3b)
+ * OOjs UI v0.1.0-pre (40de4dabe6)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-10-21T23:53:47Z
+ * Date: 2014-10-22T23:42:16Z
  */
 ( function ( OO ) {
 
