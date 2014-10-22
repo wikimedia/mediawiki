fe5170d32b3833f848b2da760183d52ac65040bf
Kunal Mehta <legoktm@gmail.com>
2014-10-21 22:32:13 -0700

    Move tests into separate ResourceLoaderFileModuleTest
    Change-Id: I2504867850b7e6bda2afb8c4fedbe6b8819bc15b

--- tests/phpunit/includes/resourceloader/ResourceLoaderModuleTest.php
+++ tests/phpunit/includes/resourceloader/ResourceLoaderModuleTest.php
@@ -2,65 +2,6 @@
 
 class ResourceLoaderModuleTest extends ResourceLoaderTestCase {
 
-	protected function setUp() {
-		parent::setUp();
-
-		// The return value of the closure shouldn't matter since this test should
-		// never call it
-		SkinFactory::getDefaultInstance()->register(
-			'fakeskin',
-			'FakeSkin',
-			function () {
-			}
-		);
-	}
-
-	/**
-	 * @covers ResourceLoaderFileModule::getAllSkinStyleFiles
-	 */
-	public function testGetAllSkinStyleFiles() {
-		$context = self::getResourceLoaderContext();
-
-		$baseParams = array(
-			'scripts' => array(
-				'foo.js',
-				'bar.js',
-			),
-			'styles' => array(
-				'foo.css',
-				'bar.css' => array( 'media' => 'print' ),
-				'screen.less' => array( 'media' => 'screen' ),
-				'screen-query.css' => array( 'media' => 'screen and (min-width: 400px)' ),
-			),
-			'skinStyles' => array(
-				'default' => 'quux-fallback.less',
-				'fakeskin' => array(
-					'baz-vector.css',
-					'quux-vector.less',
-				),
-			),
-			'messages' => array(
-				'hello',
-				'world',
-			),
-		);
-
-		$module = new ResourceLoaderFileModule( $baseParams );
-
-		$this->assertEquals(
-			array(
-				'foo.css',
-				'baz-vector.css',
-				'quux-vector.less',
-				'quux-fallback.less',
-				'bar.css',
-				'screen.less',
-				'screen-query.css',
-			),
-			array_map( 'basename', $module->getAllStyleFiles() )
-		);
-	}
-
 	/**
 	 * @covers ResourceLoaderModule::getDefinitionSummary
 	 * @covers ResourceLoaderFileModule::getDefinitionSummary
