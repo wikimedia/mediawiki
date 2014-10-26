87965dd2c7ceedf55806153a967dda92c31b4656
Siebrand Mazeland <siebrand@kitano.nl>
2014-10-26 18:56:17 +0100

    Localisation updates from https://translatewiki.net.
    Change-Id: I1c1be5f954c274bb2479cd7068d32562db05b670

--- languages/messages/MessagesMai.php
+++ languages/messages/MessagesMai.php
@@ -26,3 +26,21 @@
 
 $fallback = 'hi';
 
+$namespaceNames = array(
+	NS_MEDIA            => 'मेडिया',
+	NS_SPECIAL          => 'विशेष',
+	NS_TALK             => 'वार्ता',
+	NS_USER             => 'प्रयोगकर्ता',
+	NS_USER_TALK        => 'प्रयोगकर्ता_वार्ता',
+	NS_PROJECT_TALK     => '$1_वार्ता',
+	NS_FILE             => 'फाइल',
+	NS_FILE_TALK        => 'फाइल_वार्ता',
+	NS_MEDIAWIKI        => 'मेडियाविकि',
+	NS_MEDIAWIKI_TALK   => 'मेडियाविकि_वार्ता',
+	NS_TEMPLATE         => 'आकृति',
+	NS_TEMPLATE_TALK    => 'आकृति_वार्ता',
+	NS_HELP             => 'मद्दत',
+	NS_HELP_TALK        => 'मद्दत_वार्ता',
+	NS_CATEGORY         => 'श्रेणी',
+	NS_CATEGORY_TALK    => 'श्रेणी_वार्ता',
+);
