2040142aff6974f57338457c1ace143530108ee8
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2014-10-27 15:35:41 +0100

    Remove meaningless default action name
    Before: Get the value of the action=... parameter. If it's undefined
    return 'view'. Check if 'view' equals 'ajax'.
    
    After: Get the value of the action=... parameter. If it's undefined
    return null. Check if null equals 'ajax'.
    
    Change-Id: I56656218cfb895bffe886919c67ebf3b79d91515

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -489,8 +489,7 @@ class MediaWiki {
 		$request = $this->context->getRequest();
 
 		// Send Ajax requests to the Ajax dispatcher.
-		if ( $this->config->get( 'UseAjax' ) && $request->getVal( 'action', 'view' ) == 'ajax' ) {
-
+		if ( $this->config->get( 'UseAjax' ) && $request->getVal( 'action' ) === 'ajax' ) {
 			// Set a dummy title, because $wgTitle == null might break things
 			$title = Title::makeTitle( NS_MAIN, 'AJAX' );
 			$this->context->setTitle( $title );
