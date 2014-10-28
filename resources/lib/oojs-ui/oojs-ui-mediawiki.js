8765d288799a9e11618855021022cd2ee01e412f
James D. Forrester <jforrester@wikimedia.org>
2014-10-28 09:52:31 -0700

    Update OOjs UI to v0.1.0-pre (deccd11549)
    New changes:
    be1f468 Localisation updates from https://translatewiki.net.
    de6d500 Localisation updates from https://translatewiki.net.
    049b28f Localisation updates from https://translatewiki.net.
    a926d7d Fix a number of documentation syntax issues throughout the codebase
    df625c4 Use primary colours for progress bar
    d721cc1 Localisation updates from https://translatewiki.net.
    c3aa4e1 [BREAKING CHANGE] Apply font-size: 0.8em; to non-isolated windows too
    140509d [BREAKING CHANGE] If no $overlay is specified, default to this.$element
    deccd11 Localisation updates from https://translatewiki.net.
    
    Change-Id: Idf1f1c047dd414edc8cb80a6187055fc811dc95e

--- resources/lib/oojs-ui/oojs-ui-mediawiki.js
+++ resources/lib/oojs-ui/oojs-ui-mediawiki.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (40de4dabe6)
+ * OOjs UI v0.1.0-pre (deccd11549)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-10-22T23:42:16Z
+ * Date: 2014-10-28T16:52:18Z
  */
 /**
  * @class
