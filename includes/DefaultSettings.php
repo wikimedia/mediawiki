43aab90c9a2bff4d236077cd68a912f7833fb52d
Mark A. Hershberger <mah@everybody.org>
2014-10-29 13:22:57 -0400

    Bump version number
    Change-Id: I3c4dcdd603f127f9b2f73a5e5975caecc43278bd

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.23.5';
+$wgVersion = '1.23.6';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
