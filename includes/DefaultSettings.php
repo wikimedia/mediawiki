08139779e0129aba9a1d15ca073a895f6df0d63b
Mark A. Hershberger <mah@everybody.org>
2014-10-30 11:50:04 -0400

    1.24.0-rc.0
    Update RELEASE-NOTES and DefaultSettings.php with pertinent info.
    
    Change-Id: Icebbb3a5b96ca05ad05c2abedbb791cc9935bf6a

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.24alpha';
+$wgVersion = '1.24.0-rc.0';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
