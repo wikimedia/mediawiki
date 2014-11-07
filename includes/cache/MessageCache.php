95e6efc4d490911b4206503d9d26f832788cf97b
Chad Horohoe <chadh@wikimedia.org>
2014-11-07 10:36:29 -0800

    Improved/additional logging for $wgTitle abuses
    Change-Id: I6599aa8abe30c1888ab5df023a0d4bf346315400

--- includes/cache/MessageCache.php
+++ includes/cache/MessageCache.php
@@ -1059,6 +1059,7 @@ class MessageCache {
 		wfProfileIn( __METHOD__ );
 		if ( !$title || !$title instanceof Title ) {
 			global $wgTitle;
+			wfDebugLog( 'GlobalTitleFail', __METHOD__ . ' called by ' . wfGetAllCallers() . ' with no title set.' );
 			$title = $wgTitle;
 		}
 		// Sometimes $wgTitle isn't set either...
