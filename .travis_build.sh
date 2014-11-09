e69f13532488d9c303b9a78dad1c4fcf1356d552
Bryan Davis <bd808@wikimedia.org>
2014-11-08 20:59:33 -0700

    travis-ci: Force object destructors to run under HHVM
    The RequestContextTest::testImportScopedSession test will fail if object
    destructors are not run when an object is destroyed. HHVM does not
    enable this behavior by default to improve performance. Travis-ci does
    not currently have an easy method to set custom ini configuration for
    the hhvm-nightly test runner, so instead we will use a custom test
    runner script and add command line parameters to enable the desired HHVM
    behavior.
    
    Bug: 73177
    Change-Id: I07671ade6f1f281371083b4d6d35a2fe231b415d

---
+++ .travis_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Run phpunit tests
+set -ex
+
+PHP=php
+
+if [[ $(phpenv version-name) =~ hhvm ]]; then
+    PHP="hhvm -c $(pwd)/.travis.hhvm.ini --php"
+fi
+
+exec $PHP tests/phpunit/phpunit.php
