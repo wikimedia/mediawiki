57ef89c1291e0ebe53dabeaa3a34603406d7f712
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-11-11 09:04:48 +0200

    Add new grammar forms for language names in Russian
    CLDR provides translated language names. They are useful for showing
    names by themselves in menus and lists, but it's often problematic to add them
    to Russian sentences, because they need to be declined, so a message like
    "This page is not available in the $1 language" is hard to localize.
    
    This patch adds new cases for Russian -
    "languagegen", "languageprep" and "languageadverb"
    (as its name says, it's not actually a grammatical case,
    but a transformation to an adverbial expression),
    This covers most of the needs for language names that MediaWiki supports.
    
    Change-Id: Ib6a0afa5c3736f8b9b2e121cd752c53ee50fad75

--- resources/src/mediawiki.language/languages/ru.js
+++ resources/src/mediawiki.language/languages/ru.js
@@ -52,6 +52,31 @@ mediaWiki.language.convertGrammar = function ( word, form ) {
 				word = word.slice( 0, -3 ) + 'нике';
 			}
 			break;
+		case 'languagegen': // язык в родительном падеже ("(с) русского")
+			if ( word.slice( -3 ) === 'кий' ) {
+				word = word.slice( 0, -2 ) + 'ого';
+			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
+				word = word + 'а';
+			}
+			break;
+		case 'languageprep': // язык в предложном падеже ("(на) русском")
+			if ( word.slice( -3 ) === 'кий' ) {
+				word = word.slice( 0, -2 ) + 'ом';
+			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
+				word = word + 'е';
+			}
+			break;
+		case 'languageadverb': // наречие с названием языка ("по-русски")
+			if ( word.slice( -3 ) === 'кий' ) {
+				word = 'по-' + word.slice( 0, -1 );
+			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
+				word = 'на ' + word + 'е';
+			} else if ( $.inArray( word, [ 'идо', 'урду', 'хинди', 'эсперанто' ] ) > -1 ) {
+				word = 'на ' + word;
+			} else {
+				word = 'на языке ' + word;
+			}
+			break;
 	}
 	return word;
 };
