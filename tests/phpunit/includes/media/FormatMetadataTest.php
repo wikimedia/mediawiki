b8d2ca802004d2ba2ec592d62f2c76c953abc360
Gergő Tisza <tgr.huwiki@gmail.com>
2014-11-12 19:10:05 +0000

    FormatMetadata::fetchExtendedMetadata: Ignore multiple EXIF/XMP values
    Ignore all but the first value of multivalued metadata fields. Before
    this, the function returned an array of values for such fields,
    causing various errors.
    
    Bug: 72652
    Change-Id: I0fd22b78e4938eecc506d870e352b3b196ee3ace
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/973

--- tests/phpunit/includes/media/FormatMetadataTest.php
+++ tests/phpunit/includes/media/FormatMetadataTest.php
@@ -68,4 +68,36 @@ class FormatMetadataTest extends MediaWikiMediaTestCase {
 			// TODO: more test cases
 		);
 	}
+
+	/**
+	 * @param mixed $input
+	 * @param mixed $output
+	 * @dataProvider provideResolveMultivalueValue
+	 * @covers FormatMetadata::resolveMultivalueValue
+	 */
+	public function testResolveMultivalueValue( $input, $output ) {
+		$formatMetadata = new FormatMetadata();
+		$class = new ReflectionClass( 'FormatMetadata' );
+		$method = $class->getMethod( 'resolveMultivalueValue' );
+		$method->setAccessible( true );
+		$actualInput = $method->invoke( $formatMetadata, $input );
+		$this->assertEquals( $output, $actualInput );
+	}
+
+	public function provideResolveMultivalueValue() {
+		return array(
+			'nonArray' => array( 'foo', 'foo' ),
+			'multiValue' => array( array( 'first', 'second', 'third', '_type' => 'ol' ), 'first' ),
+			'noType' => array( array( 'first', 'second', 'third' ), 'first' ),
+			'typeFirst' => array( array( '_type' => 'ol', 'first', 'second', 'third' ), 'first' ),
+		    'multilang' => array(
+			    array( 'en' => 'first', 'de' => 'Erste', '_type' => 'lang' ),
+			    array( 'en' => 'first', 'de' => 'Erste', '_type' => 'lang' ),
+		    ),
+		    'multilang-multivalue' => array(
+			    array( 'en' => array( 'first', 'second' ), 'de' => array( 'Erste', 'Zweite' ), '_type' => 'lang' ),
+			    array( 'en' => 'first', 'de' => 'Erste', '_type' => 'lang' ),
+		    ),
+		);
+	}
 }
