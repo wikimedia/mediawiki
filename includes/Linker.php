ade4d1513c1062a6daf1d957cd83017fb43cd665
C. Scott Ananian <cscott@cscott.net>
2014-11-14 16:38:04 -0500

    Consistently handle trailing slashes in subpage links.
    The link text for [[/Foo/]] is `Foo` and the link text for
    [[../Foo/]] is `Foo`.  So far so good.
    
    But the link text for [[/Foo//]] is `Foo` while the link text
    for [[../Foo//]] is `Foo/`.  We are stripping all trailing slashes
    in the first case, but not the second.
    
    Fix the code so that we strip all trailing slashes in both cases.
    Update some of the comments in the code while we are at it.
    
    Change-Id: Id61eacafea9820c404699a7902c8eb8102779516

--- includes/Linker.php
+++ includes/Linker.php
@@ -1489,9 +1489,10 @@ class Linker {
 		# Foobar -- normal
 		# :Foobar -- override special treatment of prefix (images, language links)
 		# /Foobar -- convert to CurrentPage/Foobar
-		# /Foobar/ -- convert to CurrentPage/Foobar, strip the initial / from text
+		# /Foobar/ -- convert to CurrentPage/Foobar, strip the initial and final / from text
 		# ../ -- convert to CurrentPage, from CurrentPage/CurrentSubPage
 		# ../Foobar -- convert to CurrentPage/Foobar, from CurrentPage/CurrentSubPage
+		# ../Foobar/ -- convert to CurrentPage/Foobar, from CurrentPage/CurrentSubPage, use 'Foobar' as text
 
 		wfProfileIn( __METHOD__ );
 		$ret = $target; # default return value is no change
@@ -1537,7 +1538,7 @@ class Linker {
 						$ret = implode( '/', array_slice( $exploded, 0, -$dotdotcount ) );
 						# / at the end means don't show full path
 						if ( substr( $nodotdot, -1, 1 ) === '/' ) {
-							$nodotdot = substr( $nodotdot, 0, -1 );
+							$nodotdot = rtrim( $nodotdot, '/' );
 							if ( $text === '' ) {
 								$text = $nodotdot . $suffix;
 							}
