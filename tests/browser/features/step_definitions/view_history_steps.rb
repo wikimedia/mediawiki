882b294072f2e8ea11fbaef6ebe8cb83446c23e5
Baochuan Lu <lubaochuan@gmail.com>
2014-11-18 06:57:40 -0600

    WIP added the simplest test for view history link
    Change-Id: I5f58a6e84e85b7d69aaa37fbb80c49d81c64dd4a

--- tests/browser/features/step_definitions/view_history_steps.rb
+++ tests/browser/features/step_definitions/view_history_steps.rb
@@ -6,3 +6,11 @@ Then(/^I should see a link to a previous version of the page$/) do
   on(ViewHistoryPage).old_version_link_element.should be_visible
 end
 
+When(/^I go to a random page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^View history link should be present$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
