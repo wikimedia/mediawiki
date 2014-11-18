ebc8db46e087c68bdb51c19cd094abdb86a59d12
Roan Kattouw <roan.kattouw@gmail.com>
2014-11-17 19:31:10 -0500

    Update OOjs UI to v0.1.0-pre (d0af1e5b65)
    New changes:
    d0af1e5 Followup ca6a16f: use parent selector both for hiding and showing windows
    
    Change-Id: I67df482a9e6b4ef58e0d220192c04912edeb5f89

--- resources/lib/oojs-ui/oojs-ui-mediawiki.js
+++ resources/lib/oojs-ui/oojs-ui-mediawiki.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (05f0fefc3f)
+ * OOjs UI v0.1.0-pre (d0af1e5b65)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-11-11T16:29:49Z
+ * Date: 2014-11-18T00:28:07Z
  */
 /**
  * @class
