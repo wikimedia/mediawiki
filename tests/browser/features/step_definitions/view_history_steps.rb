20735c2025008c41532525868f2ea4cbfefde533
Baochuan Lu <lubaochuan@gmail.com>
2014-11-18 06:57:40 -0600

    WIP added the simplest test for view history link The recommended changes now cause the view_history.feature test to fail. Tried a few changes but still can not make the test to pass.
    Change-Id: I5f58a6e84e85b7d69aaa37fbb80c49d81c64dd4a

--- tests/browser/features/step_definitions/view_history_steps.rb
+++ tests/browser/features/step_definitions/view_history_steps.rb
@@ -5,3 +5,7 @@ end
 Then(/^I should see a link to a previous version of the page$/) do
   expect(on(ViewHistoryPage).old_version_link_element).to be_visible
 end
+
+Then(/^View history link should be present$/) do
+  expect(on(RandomPage).view_history_link_element).to exist
+end
