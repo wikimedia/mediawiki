ff2aa4203d9cea3ab0f3739d048913ac84f4cbef
Aaron Schulz <aschulz@wikimedia.org>
2014-11-19 16:33:51 -0800

    [WIP] Added ApiPrepareEdit module
    * This lets edits be prepared while users enter edit summaries
    
    Change-Id: I4f4057bc0d1d4a66a8f7cfb7cdc26d443a8eb0c4

--- resources/src/mediawiki.action/mediawiki.action.edit.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.js
@@ -4,7 +4,8 @@
 ( function ( mw, $ ) {
 
 	$( function () {
-		var editBox, scrollTop, $editForm;
+		var editBox, scrollTop, $editForm, $labelBox,
+			prepareBusy = false, textChanged = false;
 
 		// Make sure edit summary does not exceed byte limit
 		$( '#wpSummary' ).byteLimit( 255 );
@@ -22,6 +23,50 @@
 				scrollTop.value = editBox.scrollTop;
 			} );
 		}
+
+		// Pre-render pages while users type edit summaries
+		// @TODO: move elsewhere or put behind a config flag
+		$( '#wpTextbox1, #wpSummary' ).change( function () {
+			textChanged = true;
+		} );
+		$labelBox = $( '#wpSummary' );
+		$labelBox.focus( function () {
+			var api, apiPromise;
+
+			api = new mw.Api();
+			// @todo: use api.postWithToken() when it's unbroken
+			apiPromise = api.get( {
+				action: 'query',
+				meta: 'tokens',
+				type: 'csrf'
+			} );
+
+			apiPromise.then( function ( data ) {
+				var sectionId;
+				// Avoid duplicated work
+				if ( prepareBusy || !textChanged ) {
+					return;
+				}
+				prepareBusy = true;
+				textChanged = false;
+
+				sectionId = $( '#editform input[name="wpSection"]' ).val();
+
+				api.post( {
+					action: 'prepareedit',
+					title: mw.config.get( 'wgPageName' ),
+					section: sectionId,
+					sectionTitle: sectionId === 'new' ? $labelBox.val() : '',
+					text: editBox.value,
+					contentModel: $( '#editform input[name="model"]' ).val(),
+					contentFormat: $( '#editform input[name="format"]' ).val(),
+                    baseRevId: $( '#editform input[name="sectionRevId"]' ).val(),
+					token: data.query.tokens['csrftoken']
+				} ).always ( function () {
+					prepareBusy = false;
+				} );
+			} );
+		} );
 	} );
 
 }( mediaWiki, jQuery ) );
