bb698dd1a0015fafb2636160b8653dd4fa21c31c
James D. Forrester <jforrester@wikimedia.org>
2014-11-20 16:12:50 -0800

    Update OOjs UI to v0.1.0-pre (d4cfcce969)
    New changes:
    f6adfbc Give help content box a reasonable line height for legibility
    af5d71c Remove CSS hack for positioning popupButtonWidget
    c0768f9 PHP: Add missing OOUI\Exception class
    76800b4 PHP: Wrap __toString() in try-catch to avoid unhelpful fatals
    d399920 Revert "Followup ca6a16f: use parent selector both for hiding and showing windows"
    234cd9f Revert "Show the modal overlay as soon as openWindow is called"
    
    Change-Id: I317b4a747389de35411aca4ffdbbc2306b5b74cc

--- resources/lib/oojs-ui/oojs-ui-mediawiki.js
+++ resources/lib/oojs-ui/oojs-ui-mediawiki.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (20c61ec865)
+ * OOjs UI v0.1.0-pre (d4cfcce969)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-11-20T18:41:26Z
+ * Date: 2014-11-21T00:12:34Z
  */
 /**
  * @class
