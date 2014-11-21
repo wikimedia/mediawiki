90f6efc360fd51c5d767a280211ea134c217df66
aude <aude.wiki@gmail.com>
2014-11-20 19:24:39 -0500

    Implement SiteListFileCache and rebuild script
    Provides file-based cache of the SitesStore data,
    using a static json file dump of the data from the
    SiteSQLStore.
    
    Includes a maintenance script to rebuild the sites cache.
    
    Bug: 56602
    Bug: 45532
    Change-Id: Iaee4c1f9fb5d54efe01975f733ebd5c339ac106f

---
+++ includes/site/SiteListFileCacheBuilder.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.25
+ *
+ * @file
+ *
+ * @license GNU GPL v2+
+ */
+class SiteListFileCacheBuilder {
+
+	/**
+	 * @var SiteStore
+	 */
+	private $siteStore;
+
+	/**
+	 * @var string
+	 */
+	private $cacheFile;
+
+	/**
+	 * @param SiteStore $siteStore
+	 * @param string $cacheFile
+	 */
+	public function __construct( SiteStore $siteStore, $cacheFile ) {
+		$this->siteStore = $siteStore;
+		$this->cacheFile = $cacheFile;
+	}
+
+	public function build() {
+		$this->sites = $this->siteStore->getSites( 'recache' );
+		$this->cacheSites( $this->sites->getArrayCopy() );
+	}
+
+	/**
+	 * @param Site[] $sites
+	 *
+	 * @throws MWException if in manualRecache mode
+	 * @return bool
+	 */
+	private function cacheSites( array $sites ) {
+		$sitesArray = array();
+
+		foreach ( $sites as $site ) {
+			$globalId = $site->getGlobalId();
+			$sitesArray[$globalId] = $this->getSiteAsArray( $site );
+		}
+
+		$json = json_encode( array(
+			'sites' => $sitesArray
+		) );
+
+		$result = file_put_contents( $this->cacheFile, $json );
+
+		return $result !== false;
+	}
+
+	/**
+	 * @param Site $site
+	 *
+	 * @return array
+	 */
+	private function getSiteAsArray( Site $site ) {
+		$siteEntry = unserialize( $site->serialize() );
+		$siteIdentifiers = $this->buildLocalIdentifiers( $site );
+		$identifiersArray = array();
+
+		foreach( $siteIdentifiers as $identifier ) {
+			$identifiersArray[] = $identifier;
+		}
+
+		$siteEntry['identifiers'] = $identifiersArray;
+
+		return $siteEntry;
+	}
+
+	/**
+	 * @param Site $site
+	 *
+	 * @return array Site local identifiers
+	 */
+	private function buildLocalIdentifiers( Site $site ) {
+		$localIds = array();
+
+		foreach ( $site->getLocalIds() as $idType => $ids ) {
+			foreach ( $ids as $id ) {
+				$localIds[] = array(
+					'type' => $idType,
+					'key' => $id
+				);
+			}
+		}
+
+		return $localIds;
+	}
+
+}
