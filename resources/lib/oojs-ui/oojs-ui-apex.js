1ca201ed87b23217d3d7ca70106a1a3676685a86
Roan Kattouw <roan.kattouw@gmail.com>
2014-11-20 16:26:00 -0800

    Update OOjs UI to v0.1.0-pre (2a36afc90f)
    New changes:
    77d1ce2 Revert "Followup ca6a16f: use parent selector both for hiding and showing windows"
    2a36afc Revert "Show the modal overlay as soon as openWindow is called"
    
    Change-Id: Ia2f711e680ca92c05323797addc463cc12281a16

--- resources/lib/oojs-ui/oojs-ui-apex.js
+++ resources/lib/oojs-ui/oojs-ui-apex.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.1.0-pre (d0af1e5b65)
+ * OOjs UI v0.1.0-pre (2a36afc90f)
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2014 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2014-11-18T00:28:07Z
+ * Date: 2014-11-21T00:25:43Z
  */
 /* Instantiation */
 
