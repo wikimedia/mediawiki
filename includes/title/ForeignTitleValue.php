bff084ce0a8006baa95f9c91a0acd97642c90da1
This, that and the other <at.light@live.com.au>
2014-11-21 11:04:57 +1100

    Proper namespace handling for WikiImporter
    Up until now, the import backend has tried to resolve titles in the XML
    data using the regular Title class. This is a disastrous idea, as local
    namespace names often do not match foreign namespace titles.
    
    There is enough metadata present in XML dumps generated by modern MW
    versions for the target namespace ID and name to be reliably determined.
    This metadata is contained in the <siteinfo> and <ns> tags, which
    (unbelievably enough) was totally ignored by WikiImporter until now.
    Fallbacks are provided for older XML dump versions which may be missing
    some or all of this metadata.
    
    The ForeignTitleValue class is introduced, based on the existing TitleValue
    model. This is intended specifically for the resolution of titles on
    foreign wikis. In the future, an InterwikiTitleValue class could be added,
    which would inherit ForeignTitleValue and add a member for the interwiki
    prefix (and re-add fragment support).
    
    The 'AfterImportPage' hook has been modified so the second argument is a
    ForeignTitleValue object instead of a Title (the documentation was wrong,
    it was never a string). LiquidThreads, SMW and FacetedSearch all use this
    hook but none of them use the $origTitle parameter.
    
    Bug: 30723
    Bug: 40192
    Change-Id: Iaa58e1b9fd7287cdf999cef6a6f3bb63cd2a4778

---
+++ includes/title/ForeignTitleValue.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * A structure to hold the title of a page on a foreign MediaWiki installation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author This, that and the other
+ */
+
+/**
+ * A simple, immutable structure to hold the title of a page on a foreign
+ * MediaWiki installation.
+ */
+class ForeignTitleValue extends TitleValue {
+	/**
+	 * The internal value used for the namespace ID field when the namespace ID
+	 * is not known. The value is the lowest possible negative integer.
+	 */
+	const UNKNOWN_NAMESPACE = ~PHP_INT_MAX;
+
+	/**
+	 * @var string
+	 */
+	protected $namespaceName;
+
+	/**
+	 * Creates a new ForeignTitleValue object.
+	 *
+	 * @param int $namespaceId Pass ForeignTitleValue::UNKNOWN_NAMESPACE here if
+	 * the namespace ID is unknown (for example, interwiki links)
+	 * @param string $namespaceName
+	 * @param string $pageName
+	 */
+	public function __construct( $namespaceId, $namespaceName, $pageName ) {
+		parent::__construct( intval( $namespaceId ),
+			str_replace( ' ', '_', $pageName ) );
+		$this->namespaceName = $namespaceName;
+	}
+
+	/**
+	 * Returns the current ForeignTitleValue object. ForeignTitleValue does not
+	 * support fragments.
+	 *
+	 * @param string $fragment
+	 * @return ForeignTitleValue
+	 */
+	public function createFragmentTitle( $fragment ) {
+		return $this;
+	}
+
+	/** @return string */
+	public function getNamespaceName() {
+		return $this->namespaceName;
+	}
+
+	/** @return string */
+	public function getFullText() {
+		$result = '';
+		if ( $this->namespaceName ) {
+			$result .= $this->namespaceName . ':';
+		}
+		$result .= $this->getText();
+		return $result;
+	}
+}
