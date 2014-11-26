d4ae22a4450cb523d1b1d02e3a7a6515f6c58d04
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-11-26 12:36:57 +0200

    Update the autonym for aeb in Names.php
    The new name was suggested by Wikimedia's Language committee,
    and as far as I know Arabic, it looks correct to me as well.
    
    A Latin script autonym is being discussed separately and will be
    added if and when there will be a decision about it.
    
    The Arabic script autonym should be changed already now
    because the current one is definitely wrong.
    
    See also https://github.com/wikimedia/jquery.uls/pull/167 .
    
    Change-Id: Ib07d2b49a47fb7f349a2cd70b7acfd88f9bf66ca

--- languages/Names.php
+++ languages/Names.php
@@ -41,7 +41,7 @@
 	'aa' => 'Qafár af',	# Afar
 	'ab' => 'Аҧсшәа',	# Abkhaz
 	'ace' => 'Acèh',	# Aceh
-	'aeb' => 'زَوُن',	# Tunisian Arabic
+	'aeb' => 'تونسي',	# Tunisian Arabic
 	'af' => 'Afrikaans',	# Afrikaans
 	'ak' => 'Akan',		# Akan
 	'aln' => 'Gegë',	# Gheg Albanian
