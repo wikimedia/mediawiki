392697f9bd8f74a1208a7caf395946b435866e69
Ori Livneh <ori@wikimedia.org>
2014-12-01 13:07:02 -0800

    mediawiki.action.edit: Add editing.content.changed hook; minor refactoring
    * Use jQuery to query the DOM; there's no reason to drop down to the DOM API
      here. This makes the code more concise, consistent, and readable.
    * Fire `editing.content.changed` on $( 'wpTextbox1' ).change.
    
    Change-Id: Id75c747fe44a087e84c68eb0f21c85b165983a95

--- resources/src/mediawiki.action/mediawiki.action.edit.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.js
@@ -4,24 +4,32 @@
 ( function ( mw, $ ) {
 
 	$( function () {
-		var editBox, scrollTop, $editForm;
+		var $editForm = $( '#editform' ),
+			$editBox = $editForm.find( '#wpTextbox1' ),
+			$scrollTop = $editForm.find( '#wpScrolltop' );
 
 		// Make sure edit summary does not exceed byte limit
 		$( '#wpSummary' ).byteLimit( 255 );
 
 		// Restore the edit box scroll state following a preview operation,
 		// and set up a form submission handler to remember this state.
-		editBox = document.getElementById( 'wpTextbox1' );
-		scrollTop = document.getElementById( 'wpScrolltop' );
-		$editForm = $( '#editform' );
-		if ( $editForm.length && editBox && scrollTop ) {
-			if ( scrollTop.value ) {
-				editBox.scrollTop = scrollTop.value;
-			}
-			$editForm.submit( function () {
-				scrollTop.value = editBox.scrollTop;
-			} );
+		if ( $scrollTop.val() ) {
+			$editBox.scrollTop( $scrollTop.val() );
 		}
+
+		$editForm.submit( function () {
+			$scrollTop.val( $editBox.scrollTop() );
+		} );
+
+		/**
+		 * Fired when wiki content has been modified by the user
+		 *
+		 * @event editing_content_changed
+		 * @member mw.hook
+		 */
+		$editBox.change( function () {
+			mw.hook( 'editing.content.changed' ).fire();
+		} );
 	} );
 
 }( mediaWiki, jQuery ) );
