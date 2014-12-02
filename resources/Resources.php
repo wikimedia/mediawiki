ba50b32556fa1f2983c80cc6c06b5908f46f7cf4
Timo Tijhof <krinklemail@gmail.com>
2014-12-02 21:48:21 +0000

    SpecialJavaScriptTest: Add export feature
    Add an 'export' subpage to SpecialJavaScriptTest which allows
    one to request a self-sufficient JavaScript payload that will
    bootstrap a ResourceLoader client and load the test suites.
    
    This is needed for using Karma (which only loads JavaScript,
    no full html pages). As such elements from the Skin and OutputPage
    will not exist. While all QUnit tests in MediaWiki core and
    most extensions I've seen already use #qunit-fixture, this is
    now required. This to prevent leakage of elements from one
    test to another, but it also prevents tests from depending
    on elements provided by the server.
    
    While the Karma setup is still in the pipeline (might land before
    this commit loses WIP status), for now this can be tested via
    the 'Special:JavaScriptTest/qunit/plain' subpage.
    
    Refactor:
    * Use HTTP status code 404 in the response for "noframework".
    * Simplify HTML footprint by using <div id="qunit"> instead of
      hardcoding the full structure. This feature was added to QUnit
      since v1.3.0 (Feb 2012), we're using v1.14.0 (Jan 2014).
      QUnit's header is automatically derived from document.title.
    * Remove redundant addModules() for 'test.mediawiki.qunit.testrunner'.
      This is already added by default.
    * Move allowClickjacking() call so that it applies to other modes
      as well. The exported javascript needs to have wgBreakFrame set
      to false so that test runners can frame it.
    * Change mediawiki.special.javaScriptTest to not depend on QUnit.
      It caused QUnit to load on error pages. And in theory the page
      is suited for other frameworks and shouldn't load QUnit this way.
    
    Bug: T74063
    Change-Id: I3d4d0df43bb426d9579eb0349b8b5477281a7cfc

--- resources/Resources.php
+++ resources/Resources.php
@@ -1466,7 +1466,9 @@ return array(
 			'colon-separator',
 			'javascripttest-pagetext-skins',
 		) ),
-		'dependencies' => array( 'jquery.qunit' ),
+		'dependencies' => array(
+			'mediawiki.Uri',
+		),
 		'position' => 'top',
 		'targets' => array( 'desktop', 'mobile' ),
 	),
