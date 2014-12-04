48d4bf708893544dbbafe3c1ea0e96d3c3acc413
Rob Moen <rmoen@wikimedia.org>
2014-12-03 16:21:18 -0800

    hygiene: rename form header to footer on special:search
    It is more of a footer
    
    Change-Id: Ic6ec29817ab7deadc6bc7125f8b4cd16686baa04

--- includes/specials/SpecialSearch.php
+++ includes/specials/SpecialSearch.php
@@ -332,7 +332,7 @@ class SpecialSearch extends SpecialPage {
 			Xml::closeElement( 'div' ) .
 			Xml::closeElement( 'form' ) .
 			$this->didYouMeanHtml .
-			$this->formHeader( $term )
+			$this->formFooter( $term )
 		);
 
 		$filePrefix = $wgContLang->getFormattedNsText( NS_FILE ) . ':';
@@ -994,8 +994,8 @@ class SpecialSearch extends SpecialPage {
 	 * @param string $term
 	 * @return string
 	 */
-	protected function formHeader( $term ) {
-		$out = Xml::openElement( 'div', array( 'class' => 'mw-search-formheader' ) );
+	protected function formFooter( $term ) {
+		$out = Xml::openElement( 'div', array( 'class' => 'mw-search-formfooter' ) );
 
 		$bareterm = $term;
 		if ( $this->startsWithImage( $term ) ) {
