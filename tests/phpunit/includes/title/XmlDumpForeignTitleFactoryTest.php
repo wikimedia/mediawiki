0029b267e282e7fc677f1b6f248299233562215a
This, that and the other <at.light@live.com.au>
2014-12-05 12:26:55 +1100

    Proper namespace handling for WikiImporter
    Up until now, the import backend has tried to resolve titles in the XML
    data using the regular Title class. This is a disastrous idea, as local
    namespace names often do not match foreign namespace titles.
    
    There is enough metadata present in XML dumps generated by modern MW
    versions for the target namespace ID and name to be reliably determined.
    This metadata is contained in the <siteinfo> and <ns> tags, which
    (unbelievably enough) was totally ignored by WikiImporter until now.
    Fallbacks are provided for older XML dump versions which may be missing
    some or all of this metadata.
    
    The ForeignTitle class is introduced. This is intended specifically for
    the resolution of titles on foreign wikis. In the future, an
    InterwikiTitle class could be added, which would inherit ForeignTitle
    and add members for the interwiki prefix and fragment.
    
    The 'AfterImportPage' hook has been modified so the second argument is a
    ForeignTitle object instead of a Title (the documentation was wrong,
    it was never a string). LiquidThreads, SMW and FacetedSearch all use this
    hook but none of them use the $origTitle parameter.
    
    Bug: T32723
    Bug: T42192
    Change-Id: Iaa58e1b9fd7287cdf999cef6a6f3bb63cd2a4778

---
+++ tests/phpunit/includes/title/XmlDumpForeignTitleFactoryTest.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license GPL 2+
+ * @author This, that and the other
+ */
+
+/**
+ * @covers XmlDumpForeignTitleFactory
+ *
+ * @group Title
+ */
+class XmlDumpForeignTitleFactoryTest extends MediaWikiTestCase {
+
+	public function basicProvider() {
+		return array(
+			array(
+				'MainNamespaceArticle', 0,
+				new ForeignTitle( 0, '', 'MainNamespaceArticle' ),
+			),
+			array(
+				'MainNamespaceArticle', null,
+				new ForeignTitle( null, '', 'MainNamespaceArticle' ),
+			),
+			array(
+				'Talk:Nice_talk', 1,
+				new ForeignTitle( 1, 'Talk', 'Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 0,
+				new ForeignTitle( 0, '', 'Bogus:Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 9000, // non-existent local namespace ID
+				new ForeignTitle( 9000, 'Bogus', 'Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 4, // existing local namespace ID
+				new ForeignTitle( 4, 'Bogus', 'Nice_talk' ),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider basicProvider
+	 */
+	public function testBasic( ForeignTitle $foreignTitle, $title, $ns ) {
+		$factory = new XmlDumpForeignTitleFactory( null );
+		$testTitle = $factory->createForeignTitle( $title, $ns );
+
+		$this->assertEquals( $testTitle->isNamespaceIdKnown(),
+			$foreignTitle->isNamespaceIdKnown() );
+
+		if (
+			$testTitle->isNamespaceIdKnown() &&
+			$foreignTitle->isNamespaceIdKnown()
+		) {
+			$this->assertEquals( $testTitle->getNamespaceId(),
+				$foreignTitle->getNamespaceId() );
+		}
+
+		$this->assertEquals( $testTitle->getNamespaceName(),
+			$foreignTitle->getNamespaceName() );
+		$this->assertEquals( $testTitle->getText(), $foreignTitle->getText() );
+	}
+
+	public function foreignNamespacesProvider() {
+		return array(
+			array(
+				'MainNamespaceArticle', 0,
+				new ForeignTitle( 0, '', 'MainNamespaceArticle' ),
+			),
+			array(
+				'MainNamespaceArticle', null,
+				new ForeignTitle( null, '', 'MainNamespaceArticle' ),
+			),
+			array(
+				'Talk:Nice_talk', 1,
+				new ForeignTitle( 1, 'Talk', 'Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 0,
+				new ForeignTitle( 0, '', 'Bogus:Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', null,
+				new ForeignTitle( 9000, 'Bogus', 'Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 4,
+				new ForeignTitle( 4, 'Bogus', 'Nice_talk' ),
+			),
+			array(
+				'Bogus:Nice_talk', 1,
+				new ForeignTitle( 1, 'Talk', 'Nice_talk' ),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider foreignNamespacesProvider
+	 */
+	public function testForeignNamespaces( ForeignTitle $foreignTitle,
+		$title, $ns ) {
+
+		$foreignNamespaces = array(
+			0 => '', 1 => 'Talk', 100 => 'Portal', 9000 => 'Bogus'
+		);
+
+		$factory = new XmlDumpForeignTitleFactory( $foreignNamespaces );
+		$testTitle = $factory->createForeignTitle( $title, $ns );
+
+		$this->assertEquals( $testTitle->isNamespaceIdKnown(),
+			$foreignTitle->isNamespaceIdKnown() );
+
+		if (
+			$testTitle->isNamespaceIdKnown() &&
+			$foreignTitle->isNamespaceIdKnown()
+		) {
+			$this->assertEquals( $testTitle->getNamespaceId(),
+				$foreignTitle->getNamespaceId() );
+		}
+
+		$this->assertEquals( $testTitle->getNamespaceName(),
+			$foreignTitle->getNamespaceName() );
+		$this->assertEquals( $testTitle->getText(), $foreignTitle->getText() );
+	}
+}
