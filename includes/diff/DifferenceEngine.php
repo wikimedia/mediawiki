9fe2e1175ba8aa307cf1269fd19bfe3b1815d118
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-12-06 11:16:15 +0100

    Escape unescaped messages shown on a diff page
    Change-Id: I05c07625a2dbb3c5d3ab46d1cfafeaed6a248bba

--- includes/diff/DifferenceEngine.php
+++ includes/diff/DifferenceEngine.php
@@ -1062,7 +1062,7 @@ class DifferenceEngine extends ContextSource {
 			$key = $title->quickUserCan( 'edit', $user ) ? 'editold' : 'viewsourceold';
 			$msg = $this->msg( $key )->escaped();
 			$editLink = $this->msg( 'parentheses' )->rawParams(
-				Linker::linkKnown( $title, $msg, array( ), $editQuery ) )->plain();
+				Linker::linkKnown( $title, $msg, array( ), $editQuery ) )->escaped();
 			$header .= ' ' . Html::rawElement(
 				'span',
 				array( 'class' => 'mw-diff-edit' ),
