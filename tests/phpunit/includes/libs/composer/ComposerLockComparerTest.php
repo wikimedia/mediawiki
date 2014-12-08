727145116d1ba9b0be5a97377c3140bada711e52
Kunal Mehta <legoktm@gmail.com>
2014-12-08 12:08:52 -0800

    Add checkComposerLockUpToDate.php script
    Checks whether your composer.lock file is up to date
    with the current composer.json file.
    
    Bug: T77388
    Change-Id: I528d63172c238cf1ea9bc02e8eb39b93225865de

---
+++ tests/phpunit/includes/libs/composer/ComposerLockComparerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+class ComposerLockComparerTest extends MediaWikiTestCase {
+
+	private $json, $lock, $new;
+
+	public function setUp() {
+		parent::setUp();
+		global $IP;
+		$this->lock = "$IP/tests/phpunit/data/composer/composer.lock";
+		$this->json = "$IP/tests/phpunit/data/composer/composer.json";
+		$this->new = "$IP/tests/phpunit/data/composer/new-composer.json";
+	}
+
+	public static function provideIsHashUpToDate() {
+		return array(
+			array( 'json', true ),
+			array( 'new', false ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideIsHashUpToDate
+	 * @covers ComposerLockComparer::isHashUpToDate
+	 */
+	public function testIsHashUpToDate( $file, $expected ) {
+		$comparer = new ComposerLockComparer( $this->$file, $this->lock );
+		$this->assertEquals( $expected, $comparer->isHashUpToDate() );
+	}
+
+	/**
+	 * @covers ComposerLockComparer::getInstalledDependencies
+	 */
+	public function testGetInstalledDependencies() {
+		$comparer = new ComposerLockComparer( $this->json, $this->lock );
+		$this->assertArrayEquals( array(
+			'cdb/cdb' => '1.0.0',
+			'cssjanus/cssjanus' => '1.1.1',
+			'leafo/lessphp' => '0.5.0',
+			'psr/log' => '1.0.0',
+		), $comparer->getInstalledDependencies(), false, true );
+	}
+
+	/**
+	 * @covers ComposerLockComparer::getRequiredDependencies
+	 */
+	public function testGetRequiredDependencies() {
+		$comparer = new ComposerLockComparer( $this->json, $this->lock );
+		$this->assertArrayEquals( array(
+			'cdb/cdb' => '1.0.0',
+			'cssjanus/cssjanus' => '1.1.1',
+			'leafo/lessphp' => '0.5.0',
+			'psr/log' => '1.0.0',
+		), $comparer->getRequiredDependencies(), false, true );
+	}
+}
