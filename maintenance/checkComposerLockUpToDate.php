c6458ec3c3ac7e1d4c5b9af47e8873a0b19490ab
Kunal Mehta <legoktm@gmail.com>
2014-12-08 12:08:52 -0800

    Add checkComposerLockUpToDate.php script
    Checks whether your composer.lock file is up to date
    with the current composer.json file.
    
    Bug: T77388
    Change-Id: I528d63172c238cf1ea9bc02e8eb39b93225865de

---
+++ maintenance/checkComposerLockUpToDate.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once __DIR__ . '/Maintenance.php';
+
+/**
+ * Checks whether your composer-installed dependencies are up to date
+ *
+ * Composer creates a "composer.lock" file which specifies which versions are installed
+ * (via `composer install`). It has a hash, which can be compared to the value of
+ * the composer.json file to see if dependencies are up to date.
+ */
+class CheckComposerLockUpToDate extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Checks whether your composer.lock file is up to date with the current composer.json';
+	}
+
+	public function execute() {
+		global $IP;
+		$lock = "$IP/composer.lock";
+		$json = "$IP/composer.json";
+		if ( !file_exists( $lock ) ) {
+			// Maybe they're using mediawiki/vendor?
+			$lock = "$IP/vendor/composer.lock";
+			if ( !file_exists( $lock ) ) {
+				$this->error( 'Could not find composer.lock file. Have you run "composer install"?', 1 );
+			}
+		}
+
+		$comparer = new ComposerLockComparer( $json, $lock );
+
+		if ( $comparer->isHashUpToDate() ) {
+			$this->output( "Your composer.lock file is up to date with current dependencies!\n" );
+			return;
+		}
+		// Out of date, lets figure out which dependencies are old
+		$found = false;
+		$installed = $comparer->getInstalledDependencies();
+		foreach ( $comparer->getRequiredDependencies() as $name => $version ) {
+			if ( isset( $installed[$name] ) ) {
+				if ( $installed[$name] !== $version ) {
+					$this->output( "$name: {$installed[$name]} installed, $version required.\n" );
+					$found = true;
+				}
+			} else {
+				$this->output( "$name: not installed, $version required.\n" );
+				$found = true;
+			}
+		}
+		if ( $found ) {
+			$this->error( 'Error: your composer.lock file is not up to date, run "composer update" to install newer dependencies', 1 );
+		} else {
+			// The hash is the entire composer.json file, so it can be updated without any of the dependencies changing
+			// We couldn't find any out-of-date dependencies, so assume everything is ok!
+			$this->output( "Your composer.lock file is up to date with current dependencies!\n" );
+		}
+
+	}
+}
+
+$maintClass = 'CheckComposerLockUpToDate';
+require_once RUN_MAINTENANCE_IF_MAIN;
