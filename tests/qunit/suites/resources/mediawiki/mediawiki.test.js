4241f12e1bf366f409c345d1c73619d342244e78
Timo Tijhof <krinklemail@gmail.com>
2014-12-09 02:15:28 +0000

    mediawiki.loader: Remove optional params in mw.loader.implement tests
    Follows-up Ic8d7efe49b5d. Kept in the previous change to ensure
    those tests work either way.
    
    Change-Id: I7e185db56e36fdfbb88c2d0938cdf55881e8c313

--- tests/qunit/suites/resources/mediawiki/mediawiki.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.test.js
@@ -404,7 +404,7 @@
 			isAwesomeDone = true;
 		};
 
-		mw.loader.implement( 'test.callback', [QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/callMwLoaderTestCallback.js' )], {}, {} );
+		mw.loader.implement( 'test.callback', [QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/callMwLoaderTestCallback.js' )] );
 
 		mw.loader.using( 'test.callback', function () {
 
@@ -452,7 +452,7 @@
 			isAwesomeDone = true;
 		};
 
-		mw.loader.implement( 'test.promise', [QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/callMwLoaderTestCallback.js' )], {}, {} );
+		mw.loader.implement( 'test.promise', [QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/callMwLoaderTestCallback.js' )] );
 
 		mw.loader.using( 'test.promise' )
 		.done( function () {
@@ -490,8 +490,7 @@
 			},
 			{
 				'all': '.mw-test-implement-a { float: right; }'
-			},
-			{}
+			}
 		);
 
 		mw.loader.load( [
@@ -553,8 +552,7 @@
 						urlStyleTest( '.mw-test-implement-b3', 'float', 'right' )
 					]
 				}
-			},
-			{}
+			}
 		);
 
 		mw.loader.load( [
@@ -584,8 +582,7 @@
 			},
 			{
 				'all': '.mw-test-implement-c { float: right; }'
-			},
-			{}
+			}
 		);
 
 		mw.loader.load( [
@@ -622,8 +619,7 @@
 			{
 				'all': [urlStyleTest( '.mw-test-implement-d', 'float', 'right' )],
 				'print': [urlStyleTest( '.mw-test-implement-d2', 'text-align', 'center' )]
-			},
-			{}
+			}
 		);
 
 		mw.loader.load( [
@@ -710,18 +706,15 @@
 			['test.module5', '0', ['test.module4']],
 			['test.module6', '0', ['test.module5']]
 		] );
-		mw.loader.implement( 'test.module4', function () {
-		}, {}, {} );
+		mw.loader.implement( 'test.module4', function () {} );
 		assert.strictEqual( mw.loader.getState( 'test.module4' ), 'ready', 'Expected "ready" state for test.module4' );
 		assert.strictEqual( mw.loader.getState( 'test.module5' ), 'registered', 'Expected "registered" state for test.module5' );
 		assert.strictEqual( mw.loader.getState( 'test.module6' ), 'registered', 'Expected "registered" state for test.module6' );
-		mw.loader.implement( 'test.module6', function () {
-		}, {}, {} );
+		mw.loader.implement( 'test.module6', function () {} );
 		assert.strictEqual( mw.loader.getState( 'test.module4' ), 'ready', 'Expected "ready" state for test.module4' );
 		assert.strictEqual( mw.loader.getState( 'test.module5' ), 'registered', 'Expected "registered" state for test.module5' );
 		assert.strictEqual( mw.loader.getState( 'test.module6' ), 'loaded', 'Expected "loaded" state for test.module6' );
-		mw.loader.implement( 'test.module5', function () {
-		}, {}, {} );
+		mw.loader.implement( 'test.module5', function () {} );
 		assert.strictEqual( mw.loader.getState( 'test.module4' ), 'ready', 'Expected "ready" state for test.module4' );
 		assert.strictEqual( mw.loader.getState( 'test.module5' ), 'ready', 'Expected "ready" state for test.module5' );
 		assert.strictEqual( mw.loader.getState( 'test.module6' ), 'ready', 'Expected "ready" state for test.module6' );
@@ -733,8 +726,7 @@
 			['test.module8', '0', ['test.module7']],
 			['test.module9', '0', ['test.module8']]
 		] );
-		mw.loader.implement( 'test.module8', function () {
-		}, {}, {} );
+		mw.loader.implement( 'test.module8', function () {} );
 		assert.strictEqual( mw.loader.getState( 'test.module7' ), 'registered', 'Expected "registered" state for test.module7' );
 		assert.strictEqual( mw.loader.getState( 'test.module8' ), 'loaded', 'Expected "loaded" state for test.module8' );
 		assert.strictEqual( mw.loader.getState( 'test.module9' ), 'registered', 'Expected "registered" state for test.module9' );
@@ -742,8 +734,7 @@
 		assert.strictEqual( mw.loader.getState( 'test.module7' ), 'missing', 'Expected "missing" state for test.module7' );
 		assert.strictEqual( mw.loader.getState( 'test.module8' ), 'error', 'Expected "error" state for test.module8' );
 		assert.strictEqual( mw.loader.getState( 'test.module9' ), 'error', 'Expected "error" state for test.module9' );
-		mw.loader.implement( 'test.module9', function () {
-		}, {}, {} );
+		mw.loader.implement( 'test.module9', function () {} );
 		assert.strictEqual( mw.loader.getState( 'test.module7' ), 'missing', 'Expected "missing" state for test.module7' );
 		assert.strictEqual( mw.loader.getState( 'test.module8' ), 'error', 'Expected "error" state for test.module8' );
 		assert.strictEqual( mw.loader.getState( 'test.module9' ), 'error', 'Expected "error" state for test.module9' );
