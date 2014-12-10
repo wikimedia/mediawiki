37b4cd5da2c3adfd0207ad74a2212fe7292341ca
This, that and the other <at.light@live.com.au>
2014-12-10 22:24:47 +1100

    Proper namespace handling for WikiImporter
    Up until now, the import backend has tried to resolve titles in the XML
    data using the regular Title class. This is a disastrous idea, as local
    namespace names often do not match foreign namespace titles.
    
    There is enough metadata present in XML dumps generated by modern MW
    versions for the target namespace ID and name to be reliably determined.
    This metadata is contained in the <siteinfo> and <ns> tags, which
    (unbelievably enough) was totally ignored by WikiImporter until now.
    Fallbacks are provided for older XML dump versions which may be missing
    some or all of this metadata.
    
    The ForeignTitle class is introduced. This is intended specifically for
    the resolution of titles on foreign wikis. In the future, an
    InterwikiTitle class could be added, which would inherit ForeignTitle
    and add members for the interwiki prefix and fragment.
    
    Factory classes to generate ForeignTitle objects from string data, and
    Title objects from ForeignTitle objects, are also added.
    
    The 'AfterImportPage' hook has been modified so the second argument is a
    ForeignTitle object instead of a Title (the documentation was wrong,
    it was never a string). LiquidThreads, SMW and FacetedSearch all use this
    hook but none of them use the $origTitle parameter.
    
    Bug: T32723
    Bug: T42192
    Change-Id: Iaa58e1b9fd7287cdf999cef6a6f3bb63cd2a4778

---
+++ includes/title/NaiveImportTitleFactory.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license GPL 2+
+ */
+
+/**
+ * A class to convert page titles on a foreign wiki (ForeignTitle objects) into
+ * page titles on the local wiki (Title objects), using a default namespace
+ * mapping.
+ *
+ * For built-in namespaces (0 <= ID < 100), we try to find a local namespace
+ * with the same namespace ID as the foreign page. If no such namespace exists,
+ * or the namespace ID is unknown or > 100, we look for a local namespace with
+ * a matching namespace name. If that can't be found, we dump the page in the
+ * main namespace as a last resort.
+ */
+class NaiveImportTitleFactory implements ImportTitleFactory {
+	/**
+	 * Determines which local title best corresponds to the given foreign title.
+	 * If such a title can't be found or would be locally invalid, null is
+	 * returned.
+	 *
+	 * @param ForeignTitle $foreignTitle The ForeignTitle to convert
+	 * @return Title|null
+	 */
+	public function createTitleFromForeignTitle( ForeignTitle $foreignTitle ) {
+		global $wgContLang;
+
+		if ( $foreignTitle->isNamespaceIdKnown() ) {
+			$foreignNs = $foreignTitle->getNamespaceId();
+
+			// For built-in namespaces (0 <= ID < 100), we try to find a local NS with
+			// the same namespace ID
+			if ( $foreignNs < 100 && MWNamespace::exists( $foreignNs ) ) {
+				return Title::makeTitleSafe( $foreignNs, $foreignTitle->getText() );
+			}
+		}
+
+		// Do we have a local namespace by the same name as the foreign
+		// namespace?
+		$targetNs = $wgContLang->getNsIndex( $foreignTitle->getNamespaceName() );
+		if ( $targetNs !== false ) {
+			return Title::makeTitleSafe( $targetNs, $foreignTitle->getText() );
+		}
+
+		// Otherwise, just fall back to main namespace
+		return Title::makeTitleSafe( 0, $foreignTitle->getFullText() );
+	}
+}
---
+++ includes/title/NamespaceImportTitleFactory.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license GPL 2+
+ */
+
+/**
+ * A class to convert page titles on a foreign wiki (ForeignTitle objects) into
+ * page titles on the local wiki (Title objects), placing all pages in a fixed
+ * local namespace.
+ */
+class NamespaceImportTitleFactory implements ImportTitleFactory {
+	/** @var int */
+	protected $ns;
+
+	/**
+	 * @param int $ns The namespace to use for all pages
+	 */
+	public function __construct( $ns ) {
+		if ( !MWNamespace::exists( $ns ) ) {
+			throw new MWException( "Namespace $ns doesn't exist on this wiki" );
+		}
+		$this->ns = $ns;
+	}
+
+	/**
+	 * Determines which local title best corresponds to the given foreign title.
+	 * If such a title can't be found or would be locally invalid, null is
+	 * returned.
+	 *
+	 * @param ForeignTitle $foreignTitle The ForeignTitle to convert
+	 * @return Title|null
+	 */
+	public function createTitleFromForeignTitle( ForeignTitle $foreignTitle ) {
+		return Title::makeTitleSafe( $this->ns, $foreignTitle->getText() );
+	}
+}
