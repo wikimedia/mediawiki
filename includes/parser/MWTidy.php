49bc4331e103ed4e1ef64f0000bbf902b55a7880
Aaron Schulz <aschulz@wikimedia.org>
2014-12-10 11:04:56 -0800

    Fixed internalClean sanity check for HHVM
    Change-Id: I5fa406ed1c4f2eefd1c22e9ab90e72655f31d162

--- includes/parser/MWTidy.php
+++ includes/parser/MWTidy.php
@@ -246,8 +246,8 @@ class MWTidy {
 		global $wgTidyConf, $wgDebugTidy;
 		wfProfileIn( __METHOD__ );
 
-		if ( !class_exists( 'tidy' ) ||
-			( wfIsHHVM() && !function_exists('tidy_repair_string') )
+		if ( ( !wfIsHHVM() && !class_exists( 'tidy' ) ) ||
+			( wfIsHHVM() && !function_exists( 'tidy_repair_string' ) )
 		) {
 			wfWarn( "Unable to load internal tidy class." );
 			$retval = -1;
