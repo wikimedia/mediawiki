02ef5c96c8756d1c9db149e33220fc3a4fee25f4
Kevin Israel <pleasestand@live.com>
2014-12-15 02:42:34 -0500

    tables.sql: Improve description of old_flags
    * Changed the name of the 'utf8' flag to 'utf-8', as that is what
      Revision stores. (This was already corrected in the mediawiki.org
      manual page by RichF.) Noted that 'utf8' was, however, mistakenly
      used in an old version of recompressTracked.php.
    * Added 'external' to the list of flags. This was already added
      to the manual page (by GreenReaper) yet not here. Copied the
      description from there and added a couple of clarifications.
    
    Change-Id: If15b49a28d7d4b0397481d21e30d877298fc7955

--- maintenance/tables.sql
+++ maintenance/tables.sql
@@ -374,13 +374,20 @@ CREATE TABLE /*_*/text (
 
   -- Comma-separated list of flags:
   -- gzip: text is compressed with PHP's gzdeflate() function.
-  -- utf8: text was stored as UTF-8.
-  --       If $wgLegacyEncoding option is on, rows *without* this flag
-  --       will be converted to UTF-8 transparently at load time.
+  -- utf-8: text was stored as UTF-8.
+  --        If $wgLegacyEncoding option is on, rows *without* this flag
+  --        will be converted to UTF-8 transparently at load time. Note
+  --        that due to a bug in a maintenance script, this flag may
+  --        have been stored as 'utf8' in some cases (T18841).
   -- object: text field contained a serialized PHP object.
   --         The object either contains multiple versions compressed
   --         together to achieve a better compression ratio, or it refers
   --         to another row where the text can be found.
+  -- external: text was stored in an external location specified by old_text.
+  --           Any additional flags apply to the data stored at that URL, not
+  --           the URL itself. The 'object' flag is *not* set for URLs of the
+  --           form 'DB://cluster/id/itemid', because the external storage
+  --           system itself decompresses these.
   old_flags tinyblob NOT NULL
 ) /*$wgDBTableOptions*/ MAX_ROWS=10000000 AVG_ROW_LENGTH=10240;
 -- In case tables are created as MyISAM, use row hints for MySQL <5.0 to avoid 4GB limit
