881b2b1823607a05ab2acdc67148a9f98f06845c
Brad Jorsch <bjorsch@wikimedia.org>
2014-12-16 11:52:52 -0500

    Skip ApiFormatWddxTest under HHVM
    It doesn't pass on HHVM (due to an issue in HHVM). Will let us enable
    HHVM based Jenkins jobs.
    
    Whenever the upstream patches is made available in Wikimedia HHVM
    package we can drop the skip.
    
    Bug: T75531
    Change-Id: Ia58ec20b4daf78cd90da1bdf8af6cac86015c5d7

--- tests/phpunit/includes/api/format/ApiFormatWddxTest.php
+++ tests/phpunit/includes/api/format/ApiFormatWddxTest.php
@@ -12,6 +12,11 @@ class ApiFormatWddxTest extends ApiFormatTestBase {
 		if ( !function_exists( 'wddx_deserialize' ) ) {
 			$this->markTestSkipped( "Function 'wddx_deserialize' not exist, skipping." );
 		}
+		if ( wfIsHHVM() ) {
+			# https://phabricator.wikimedia.org/T75531
+			# Remove the skip whenever a new HHVM is released
+			$this->markTestSkipped( "wddx_deserialize is bugged under HHVM" );
+		}
 
 		$data = $this->apiRequest( 'wddx', array( 'action' => 'query', 'meta' => 'siteinfo' ) );
 
