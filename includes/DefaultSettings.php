bdef20f09020fe20f2faf120687736b59b36bdeb
mglaser <glaser@hallowelt.biz>
2014-12-17 18:59:36 +0100

    Updated release notes and version number to MediaWiki 1.22.15
    This is MediaWiki 1.22.15 security and maintenance release.
    
    Change-Id: I78610330a795b28548192c90c1bf5d46de775963

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -63,7 +63,7 @@ $wgConf = new SiteConfiguration;
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.22.14';
+$wgVersion = '1.22.15';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
