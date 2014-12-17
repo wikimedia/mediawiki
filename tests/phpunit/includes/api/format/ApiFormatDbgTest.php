18e7cf330adec8564b3ec5558f64043435a2d4fa
Brad Jorsch <bjorsch@wikimedia.org>
2014-12-17 16:48:03 -0500

    Improve testing for ApiFormatBase subclasses
    I7b37295e is going to be changing around how ApiResult works,
    which is going to need corresponding changes in the formatters. So it
    would probably be a good idea to have a decent starting point to catch
    any breakage. The non-backwards-compatible changes to ApiFormatTestBase
    shouldn't be a concern, as no extensions in Gerrit reference this class
    or any /ApiFormat.*Test/ class.
    
    This also fixes two small bugs in ApiFormatWddx (null handling and
    spacing for non-fm slow path) discovered during testing, and works
    around some HHVM wddx extension bugs.
    
    Change-Id: I9cdf896e7070ed51e42625d61609ad9ef91cd567

---
+++ tests/phpunit/includes/api/format/ApiFormatDbgTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @group API
+ * @covers ApiFormatDbg
+ */
+class ApiFormatDbgTest extends ApiFormatTestBase {
+
+	protected $printerName = 'dbg';
+
+	public function provideGeneralEncoding() {
+		$warning = "\n  'warnings' => \n  array (\n    'dbg' => \n    array (\n" .
+			"      '*' => 'format=dbg has been deprecated. Please use format=json instead.',\n" .
+			"    ),\n  ),";
+
+		return array(
+			// Basic types
+			array( array( null ), "array ({$warning}\n  0 => NULL,\n)" ),
+			array( array( true ), "array ({$warning}\n  0 => true,\n)" ),
+			array( array( false ), "array ({$warning}\n  0 => false,\n)" ),
+			array( array( 42 ), "array ({$warning}\n  0 => 42,\n)" ),
+			array( array( 42.5 ), "array ({$warning}\n  0 => 42.5,\n)" ),
+			array( array( 1e42 ), "array ({$warning}\n  0 => 1.0E+42,\n)" ),
+			array( array( 'foo' ), "array ({$warning}\n  0 => 'foo',\n)" ),
+			array( array( 'fóo' ), "array ({$warning}\n  0 => 'fóo',\n)" ),
+
+			// Arrays and objects
+			array( array( array() ), "array ({$warning}\n  0 => \n  array (\n  ),\n)" ),
+			array( array( array( 1 ) ), "array ({$warning}\n  0 => \n  array (\n    0 => 1,\n  ),\n)" ),
+			array( array( array( 'x' => 1 ) ), "array ({$warning}\n  0 => \n  array (\n    'x' => 1,\n  ),\n)" ),
+			array( array( array( 2 => 1 ) ), "array ({$warning}\n  0 => \n  array (\n    2 => 1,\n  ),\n)" ),
+
+			// Content
+			array( array( '*' => 'foo' ), "array ({$warning}\n  '*' => 'foo',\n)" ),
+		);
+	}
+
+}
