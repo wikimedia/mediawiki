5641f3ef21acf9ed230f2aeb55a618fedb0dab6d
Ricordisamoa <ricordisamoa@openmailbox.org>
2014-12-17 11:09:04 +0000

    New convenience method ApiBase->hasCallback()
    For consistent handling of JSONP requests.
    
    Change-Id: Ic6e3483f5e8819498c693650a11728efc1bafcc0

--- includes/api/ApiTokens.php
+++ includes/api/ApiTokens.php
@@ -55,7 +55,7 @@ class ApiTokens extends ApiBase {
 
 	private function getTokenTypes() {
 		// If we're in JSON callback mode, no tokens can be obtained
-		if ( !is_null( $this->getMain()->getRequest()->getVal( 'callback' ) ) ) {
+		if ( $this->hasCallback() ) {
 			return array();
 		}
 
