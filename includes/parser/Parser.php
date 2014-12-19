b146a5fc4f156ddb7e458510de147911e57e2621
Jackmcbarn <jackmcbarn@users.noreply.github.com>
2014-12-19 02:05:44 -0500

    Allow running code during unstrip
    When adding strip markers, allow closures to be passed in place of text.
    The closure is then called during unstrip. Also, add a hook that runs
    after unstripGeneral.
    
    Change-Id: If83b0623671fd67e5ccc9deaaaab456a6679af8f

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -1293,6 +1293,10 @@ class Parser {
 
 		$text = $this->mStripState->unstripGeneral( $text );
 
+		if ( $isMain ) {
+			Hooks::run( 'ParserAfterUnstrip', array( &$this, &$text ) );
+		}
+
 		# Clean up special characters, only run once, next-to-last before doBlockLevels
 		$fixtags = array(
 			# french spaces, last one Guillemet-left
