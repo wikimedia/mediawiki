9333e2b8ebcbeac3b744070f9b68ed061c4281fd
daniel <daniel.kinzler@wikimedia.de>
2014-12-22 10:30:03 +0000

    Maintenance script for importing site info.
    Change-Id: I3936417bc79e08cf3d04270158a6e483b5515246

---
+++ includes/site/SiteImporter.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * Utility for importing site entries from XML.
+ * For the expected format of the input, see docs/sitelist.txt and docs/sitelist-1.0.xsd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.25
+ *
+ * @file
+ * @ingroup Site
+ *
+ * @license GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class SiteImporter {
+
+	/**
+	 * @var SiteStore
+	 */
+	private $store;
+
+	/**
+	 * @var callable
+	 */
+	private $exceptionCallback;
+
+	public function __construct( SiteStore $store ) {
+		$this->store = $store;
+	}
+
+	/**
+	 * @return callable
+	 */
+	public function getExceptionCallback() {
+		return $this->exceptionCallback;
+	}
+
+	/**
+	 * @param callable $exceptionCallback
+	 */
+	public function setExceptionCallback( $exceptionCallback ) {
+		$this->exceptionCallback = $exceptionCallback;
+	}
+
+	/**
+	 * @param string $file
+	 */
+	public function importFromFile( $file ) {
+		$document = new DOMDocument();
+		$document->load( $file );
+		$this->importFromDOM( $document->documentElement );
+	}
+
+	/**
+	 * @param string $xml
+	 */
+	public function importFromXML( $xml ) {
+		$document = new DOMDocument();
+
+		$oldLibXmlErrors = libxml_use_internal_errors( true );
+		$ok = $document->loadXML( $xml, LIBXML_NONET );
+
+		if ( !$ok ) {
+			$errors = libxml_get_errors();
+			libxml_use_internal_errors( $oldLibXmlErrors );
+
+			foreach ( $errors as $error ) {
+				/** @var LibXMLError $error */
+				throw new MWException( 'Malformed XML: ' . $error->message . ' in line ' . $error->line );
+			}
+
+			throw new MWException( 'Malformed XML!' );
+		}
+
+		libxml_use_internal_errors( $oldLibXmlErrors );
+		$this->importFromDOM( $document->documentElement );
+	}
+
+	/**
+	 * @param DOMElement $root
+	 */
+	private function importFromDOM( DOMElement $root ) {
+		$sites = $this->makeSiteList( $root );
+		$this->store->saveSites( iterator_to_array( $sites ) );
+	}
+
+	/**
+	 * @param DOMElement $root
+	 *
+	 * @return SiteList
+	 */
+	private function makeSiteList( DOMElement $root ) {
+		$sites = new SiteList();
+
+		$current = $root->firstChild;
+		while ( $current ) {
+			if ( $current instanceof DOMElement &&  $current->tagName === 'site' ) {
+				try {
+					$site = $this->makeSite( $current );
+					$sites->append( $site );
+				} catch ( Exception $ex ) {
+					$this->handleException( $ex );
+				}
+			}
+
+			$current = $current->nextSibling;
+		}
+
+		return $sites;
+	}
+
+	/**
+	 * @param DOMElement $siteElement
+	 *
+	 * @return Site
+	 * @throws MWException
+	 */
+	public function makeSite( DOMElement $siteElement ) {
+		if ( $siteElement->tagName !== 'site' ) {
+			throw new InvalidArgumentException( 'Expected <site> tag, found ' . $siteElement->tagName );
+		}
+
+		$type = $this->getAttributeValue( $siteElement, 'type', Site::TYPE_UNKNOWN );
+		$site = Site::newForType( $type );
+
+		$site->setForward( $this->hasChild( $siteElement, 'forward' ) );
+		$site->setGlobalId( $this->getChildValue( $siteElement, 'globalid' ) );
+		$site->setGroup( $this->getChildValue( $siteElement, 'group', Site::GROUP_NONE ) );
+		$site->setSource( $this->getChildValue( $siteElement, 'source', Site::SOURCE_LOCAL ) );
+
+		$pathTags = $siteElement->getElementsByTagName( 'path' );
+		for ( $i = 0; $i < $pathTags->length; $i++ ) {
+			$pathElement = $pathTags->item( $i );
+			$pathType = $this->getAttributeValue( $pathElement, 'type' );
+			$path = $pathElement->textContent;
+
+			$site->setPath( $pathType, $path );
+		}
+
+		$idTags = $siteElement->getElementsByTagName( 'localid' );
+		for ( $i = 0; $i < $idTags->length; $i++ ) {
+			$idElement = $idTags->item( $i );
+			$idType = $this->getAttributeValue( $idElement, 'type' );
+			$id = $idElement->textContent;
+
+			$site->addLocalId( $idType, $id );
+		}
+
+		//@todo: import <data>
+		//@todo: import <config>
+
+		return $site;
+	}
+
+	/**
+	 * @param DOMElement $element
+	 * @param $name
+	 * @param string|null|bool $default
+	 *
+	 * @return null|string
+	 * @throws MWException If the attribute is not found and no default is provided
+	 */
+	private function getAttributeValue( DOMElement $element, $name, $default = false ) {
+		$node = $element->getAttributeNode( $name );
+
+		if ( !$node ) {
+			if ( $default !== false ) {
+				return $default;
+			} else {
+				throw new MWException( 'Required ' . $name . ' attribute not found in <' . $element->tagName . '> tag' );
+			}
+		}
+
+		return $node->textContent;
+	}
+
+	/**
+	 * @param DOMElement $element
+	 * @param string $name
+	 * @param string|null|bool $default
+	 *
+	 * @return null|string
+	 * @throws MWException If the child element is not found and no default is provided
+	 */
+	private function getChildValue( DOMElement $element, $name, $default = false ) {
+		$elements = $element->getElementsByTagName( $name );
+
+		if ( $elements->length < 1 ) {
+			if ( $default !== false ) {
+				return $default;
+			} else {
+				throw new MWException( 'Required <' . $name . '> tag not found inside <' . $element->tagName . '> tag' );
+			}
+		}
+
+		$node = $elements->item( 0 );
+		return $node->textContent;
+	}
+
+	/**
+	 * @param DOMElement $element
+	 * @param string $name
+	 *
+	 * @return bool
+	 * @throws MWException
+	 */
+	private function hasChild( DOMElement $element, $name ) {
+		return $this->getChildValue( $element, $name, null ) !== null;
+	}
+
+	private function handleException( $ex ) {
+		call_user_func( $this->exceptionCallback, $ex );
+	}
+
+}
