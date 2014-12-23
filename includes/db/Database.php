7b2a640954fbd453a60f1ddec46e59ec8abe82e5
Sumit Asthana <asthana.sumit23@gmail.com>
2014-12-24 03:04:36 +0530

    Database::makeList() : Handle NULL when building 'IN' clause
    Amends to Database::makeList() to check if 'NULL' is present in array while
    building IN clause. Presently a NULL in array intended to build IN clause, is
    treated as a usual value. This creates
    problem:https://dev.mysql.com/doc/refman/5.0/en/comparison-operators.html#function_in
    Therefore null is appended at the end of the IN clause as " $field IS NULL"
    rather than within the IN clause
    
    Bug: T50853
    Change-Id: Ic255c37e9702fd44d8173027ab8036ddc48f9d2e

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -2160,16 +2160,36 @@ abstract class DatabaseBase implements IDatabase {
 			} elseif ( ( $mode == LIST_SET ) && is_numeric( $field ) ) {
 				$list .= "$value";
 			} elseif ( ( $mode == LIST_AND || $mode == LIST_OR ) && is_array( $value ) ) {
-				if ( count( $value ) == 0 ) {
+				// Remove null from array to be handled separately if found
+				// $includeNull denotes if null needs to be included
+				$includeNull = false;
+				foreach ( array_keys( $value, null, true ) as $nullKey ) {
+					$includeNull = true;
+					unset( $value[$nullKey] );
+				}
+				if ( count( $value ) == 0 && !$includeNull ) {
 					throw new MWException( __METHOD__ . ": empty input for field $field" );
-				} elseif ( count( $value ) == 1 ) {
-					// Special-case single values, as IN isn't terribly efficient
-					// Don't necessarily assume the single key is 0; we don't
-					// enforce linear numeric ordering on other arrays here.
-					$value = array_values( $value );
-					$list .= $field . " = " . $this->addQuotes( $value[0] );
+				} elseif ( count( $value ) == 0 ) {
+					// only check if $field is null
+					$list .= "$field IS NULL";
 				} else {
-					$list .= $field . " IN (" . $this->makeList( $value ) . ") ";
+					//IN clause contains atleast one valid element
+					$cond = '';
+					if ( count( $value ) == 1 ) {
+						// Special-case single values, as IN isn't terribly efficient
+						// Don't necessarily assume the single key is 0; we don't
+						// enforce linear numeric ordering on other arrays here.
+						$value = array_values( $value );
+						$cond .= $field . " = " . $this->addQuotes( $value[0] );
+					} else {
+						$cond .= $field . " IN (" . $this->makeList( $value ) . ") ";
+					}
+					//if null present in array, append IS NULL
+					if ( $includeNull ) {
+						$list .= "($cond OR $field IS NULL)";
+					} else {
+						$list .= $cond;
+					}
 				}
 			} elseif ( $value === null ) {
 				if ( $mode == LIST_AND || $mode == LIST_OR ) {
