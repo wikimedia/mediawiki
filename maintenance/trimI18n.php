85a3fe8c16f466b887cd87d5e80efb50447499a6
daniel <daniel.kinzler@wikimedia.de>
2014-12-23 11:48:48 +0000

    Utility script for trimming i18n files.
    This allows messages keys that have been removed from
    a master file (typically en.json) manually to be
    automatically removed from the other i18n files.
    
    The most common use case for removing message keys is
    splitting components: moving functionality from core
    into an extension, or from an extension into a library,
    etc.
    
    Change-Id: I60a7683e50968d4786fa72bfef51692817df5aaf

---
+++ maintenance/trimI18n.php
@@ -0,0 +1,109 @@
+<?php
+
+if ( php_sapi_name() != "cli" ) {
+	die( "This is a command line script, it cannot be run in " . php_sapi_name() . " mode\n" );
+}
+
+if ( count( $argv ) < 3 ) {
+	print( "USAGE: trimI18n.php <base> <target>\n" );
+	print( "SYNOPSIS: Given <base> and <target> are JSON files, this will remove any keys from <target>\n" );
+	print( "          which are not in <base>.\n" );
+	die();
+}
+
+class TrimI18n {
+
+	private $jsonFlags = 0;
+	private $tab = "\t";
+
+	/**
+	 * @param int $jsonFlags
+	 */
+	public function setJsonFlags( $jsonFlags ) {
+		$this->jsonFlags = $jsonFlags;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getJsonFlags() {
+		return $this->jsonFlags;
+	}
+
+	private function load( $path ) {
+		$json = file_get_contents( $path );
+
+		if ( $json === false ) {
+			throw new MWException( "Can't load JSON data from $path!" );
+		}
+
+		$data = json_decode( $json, true );
+
+		if ( !is_array( $data ) ) {
+			throw new MWException( "Can't decode JSON data found in $path!" );
+		}
+
+		return $data;
+	}
+
+	private function prettify( $json ) {
+		if ( ( $this->jsonFlags & JSON_PRETTY_PRINT ) === 0 ) {
+			return $json;
+		}
+
+		//NOTE: json_encode uses four spaces for indenting in pretty-print mode.
+
+		if ( $this->tab === '    ' ) {
+			return $json;
+		}
+
+		$tab = $this->tab;
+		$callback = function( $matches ) use ( $tab ) {
+			return preg_replace( '/    /', $tab, $matches[0] );
+		};
+
+		return preg_replace_callback( '/^(    )+/m', $callback, $json );
+	}
+
+	private function save( $path, $data ) {
+		$json = json_encode( $data, $this->jsonFlags );
+
+		if ( $json === false ) {
+			throw new RuntimeException( "Can't encode data as JSON!" );
+		}
+
+		$json = $this->prettify( $json );
+
+		$ok = file_put_contents( $path, $json );
+
+		if ( $ok === false ) {
+			throw new RuntimeException( "Failed to save JSON data to $path!" );
+		}
+
+		return $data;
+	}
+
+	public function run( $basePath, $targetPath ) {
+		$baseData = $this->load( $basePath );
+		$targetData = $this->load( $targetPath );
+
+		$targetData = array_intersect_key( $targetData, $baseData );
+		$this->save( $targetPath, $targetData );
+	}
+
+}
+
+$basePath = $argv[1];
+$targetPath = $argv[2];
+
+try {
+	$trim = new TrimI18n();
+	$trim->setJsonFlags( JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
+
+	$trim->run( $basePath, $targetPath );
+
+	print "Updated $targetPath, retaining all keys found in $basePath.\n";
+} catch ( RuntimeException $ex ) {
+	die( $ex->getMessage() . "\n" );
+}
+
