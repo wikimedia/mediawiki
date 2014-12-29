d9e851af0a770de882d459b8ab9f2c243332da92
This, that and the other <at.light@live.com.au>
2014-12-29 17:22:09 +1100

    Allow sysops to delete change tags
    Currently, if a tag is misspelt (vandlaism) or no longer wanted (HHVM,
    eventually VisualEditor), the wiki is stuck with it forever. This change
    allows users with the "managechangetags" right to delete change tags from
    the database, including removing them from all revisions to which they
    are applied.
    
    Obviously this is a powerful thing to be able to do, but I view change
    tags as a "light" kind of interface, useful for revision patrolling and
    spam/vandalism fighting but not something that necessarily needs to hang
    around forever. It's not a big deal for this kind of data to be thrown
    away without being archived anywhere.
    
    Changes to tags (starting with deletion, in this patch set) are logged in
    the new "tag management" log.
    
    Why is this change important?
    * bug T58237, where this functionality would have been nice to have
    * Special:Tags on enwiki, where you will see random old tags like
      "michael jackson" and "Removal of de: interwiki link"
    * if we are going to resolve T20672, T3189, etc. we will want to be
      able to get rid of any tags created in error, user tests, etc. See
      [[WP:VPR#Bot_tagging_of_edits]].
    
    Bug: T20670
    Change-Id: I77f476c8d0f32c80f720aa2c5e66869c81faa282

--- includes/specials/SpecialTags.php
+++ includes/specials/SpecialTags.php
@@ -40,16 +40,39 @@ class SpecialTags extends SpecialPage {
 		$this->setHeaders();
 		$this->outputHeader();
 
+		// Are we being asked to delete a tag?
+		$request = $this->getRequest();
+		switch ( $request->getVal( 'action' ) ) {
+			case 'delete':
+				$this->showDeleteTagForm( $request->getVal( 'tag' ) );
+				break;
+			default:
+				$this->showTagList();
+				break;
+		}
+	}
+
+	function showTagList() {
 		$out = $this->getOutput();
 		$out->setPageTitle( $this->msg( 'tags-title' ) );
 		$out->wrapWikiMsg( "<div class='mw-tags-intro'>\n$1\n</div>", 'tags-intro' );
+
+		$user = $this->getUser();
+		// Whether to show the "Actions" column
+		// If any actions added in the future require other user rights, add those
+		// rights here
+		$showActions = $user->isAllowed( 'managechangetags' );
 
 		// Write the headers
 		$html = Xml::tags( 'tr', null, Xml::tags( 'th', null, $this->msg( 'tags-tag' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-display-header' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-description-header' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-active-header' )->parse() ) .
-			Xml::tags( 'th', null, $this->msg( 'tags-hitcount-header' )->parse() )
+			Xml::tags( 'th', null, $this->msg( 'tags-hitcount-header' )->parse() ) .
+			( $showActions ?
+				Xml::tags( 'th', array( 'class' => 'unsortable' ),
+					$this->msg( 'tags-actions-header' )->parse() ) :
+				'' )
 		);
 
 		// Used in #doTagRow()
@@ -109,7 +132,114 @@ class SpecialTags extends SpecialPage {
 		// add raw $hitcount for sorting, because tags-hitcount contains numbers and letters
 		$newRow .= Xml::tags( 'td', array( 'data-sort-value' => $hitcount ), $hitcountLink );
 
+		// actions
+		$actionLinks = array();
+		if ( $user->isAllowed( 'managechangetags' ) ) {
+			$actionLinks[] = Linker::linkKnown( $this->getPageTitle(),
+				$this->msg( 'tags-delete' )->escaped(),
+				array(),
+				array( 'action' => 'delete', 'tag' => $tag ) );
+
+			$newRow .= Xml::tags( 'td', null, $this->getLanguage()->pipeList( $actionLinks ) );
+		}
+
 		return Xml::tags( 'tr', null, $newRow ) . "\n";
+	}
+
+	protected function showDeleteTagForm( $tag ) {
+		$user = $this->getUser();
+		if ( !$user->isAllowed( 'managechangetags' ) ) {
+			throw new PermissionsError( 'managechangetags' );
+		}
+
+		$out = $this->getOutput();
+		$out->preventClickjacking();
+		$out->setPageTitle( $this->msg( 'tags-delete-title' ) );
+
+		// is the tag actually able to be deleted?
+		$tagUsage = ChangeTags::tagUsageStatistics();
+		if ( !isset( $tagUsage[$tag] ) ) {
+			$out->wrapWikiMsg( "<div class=\"error\">\n$1\n</div>",
+				array( 'tags-delete-not-found', $tag ) );
+			return;
+		}
+
+		$preText = $this->msg( 'tags-delete-explanation-initial', $tag )->parseAsBlock();
+		if ( $tagUsage[$tag] > 0 ) {
+			$preText .= $this->msg( 'tags-delete-explanation-in-use', $tag,
+				$tagUsage[$tag] )->parseAsBlock();
+		}
+		$preText .= $this->msg( 'tags-delete-explanation-warning', $tag )->parseAsBlock();
+
+		// see if the tag is in use
+		$definedTags = ChangeTags::listDefinedTags();
+		if ( in_array( $tag, $definedTags ) ) {
+			$preText .= $this->msg( 'tags-delete-explanation-active', $tag )->parseAsBlock();
+		}
+
+		$fields = array();
+		$fields['Reason'] = array(
+			'type' => 'text',
+			'label' => $this->msg( 'tags-delete-reason' )->plain(),
+			'size' => 50,
+		);
+		$fields['HiddenAction'] = array(
+			'type' => 'hidden',
+			'name' => 'action',
+			'default' => 'delete',
+		);
+		$fields['HiddenTag'] = array(
+			'type' => 'hidden',
+			'name' => 'tag',
+			'default' => $tag,
+			'required' => true,
+		);
+
+		$form = new HTMLForm( $fields, $this->getContext() );
+		$form->setSubmitCallback( array( $this, 'processDeleteTag' ) );
+		$form->setSubmitTextMsg( 'tags-delete-submit' );
+		$form->addPreText( $preText );
+		$form->show();
+
+		// if $form->show() didn't send us off somewhere else, let's set our
+		// breadcrumb link
+		$out->addBacklinkSubtitle( $this->getPageTitle() );
+	}
+
+	public function processDeleteTag( array $data, HTMLForm $form ) {
+		$tag = $data['HiddenTag'];
+
+		$context = $form->getContext();
+		$out = $context->getOutput();
+
+		// does the tag exist?
+		$tagUsage = ChangeTags::tagUsageStatistics();
+		if ( !isset( $tagUsage[$tag] ) ) {
+			$out->wrapWikiMsg( "<div class=\"error\">\n$1\n</div>",
+				array( 'tags-delete-not-found', $tag ) );
+			return false;
+		}
+
+		// do it!
+		ChangeTags::deleteTagEverywhere( $tag );
+
+		// log it
+		$dbw = wfGetDB( DB_MASTER );
+		$logEntry = new ManualLogEntry( 'tagmanagement', 'delete' );
+		$logEntry->setPerformer( $context->getUser() );
+		// target page is not relevant, but it has to be set, so we just put in
+		// the title of Special:Tags
+		$logEntry->setTarget( $this->getPageTitle() );
+		$logEntry->setComment( $data['Reason'] );
+		$logEntry->setParameters( array(
+			'4:tag' => $tag,
+			'5:count' => $tagUsage[$tag],
+		) );
+		$logEntry->setRelations( array( 'Tag' => $tag ) );
+		$logEntry->insert( $dbw );
+
+		$out->redirect( $this->getPageTitle()->getLocalURL() );
+		return true;
 	}
 
 	protected function getGroupName() {
