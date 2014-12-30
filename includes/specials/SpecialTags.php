42f495ced3baec1c01b786c1373919e903b82d83
This, that and the other <at.light@live.com.au>
2014-12-30 12:01:57 +1100

    Allow sysops to delete change tags with <= 5000 uses
    Currently, if a tag is misspelt ("vandlaism") or no longer wanted (testing
    of OAuth, etc), the wiki is stuck with it forever. This change
    allows users with the "managechangetags" right to delete change tags from
    the database, including removing them from all revisions to which they
    are applied.
    
    Obviously this is a powerful thing to be able to do, but I view change
    tags as a "light" kind of interface, useful for revision patrolling and
    spam/vandalism fighting but not something that necessarily needs to hang
    around forever. It's not a big deal for this kind of data to be thrown
    away without being archived anywhere.
    
    Changes to tags (starting with deletion, in this patch set) are logged in
    the new "tag management" log. There's even an API module, just for fun.
    
    Why is this change important?
    * bug T58237, where this functionality would have been nice to have
    * Special:Tags on enwiki, where you will see random old tags like
      "michael jackson" and "Removal of de: interwiki link"
    * if we are going to resolve T20672, T3189, etc. we will want to be
      able to get rid of any tags created in error, user tests, etc. See
      [[WP:VPR#Bot_tagging_of_edits]].
    
    Bug: T20670
    Change-Id: I77f476c8d0f32c80f720aa2c5e66869c81faa282

--- includes/specials/SpecialTags.php
+++ includes/specials/SpecialTags.php
@@ -32,30 +32,67 @@ class SpecialTags extends SpecialPage {
 	 */
 	public $definedTags;
 
+	/**
+	 * @var array Tag usage data
+	 */
+	protected $tagUsage;
+
+	/**
+	 * Can't delete tags with more than this many uses. Similar in intent to
+	 * the bigdelete user right
+	 */
+	const MAX_DELETE_USES = 5000;
+
 	function __construct() {
 		parent::__construct( 'Tags' );
+
+		// We're going to need this eventually
+		$this->tagUsage = ChangeTags::tagUsageStatistics();
 	}
 
 	function execute( $par ) {
 		$this->setHeaders();
 		$this->outputHeader();
 
+		// Are we being asked to delete a tag?
+		$request = $this->getRequest();
+		switch ( $request->getVal( 'action' ) ) {
+			case 'delete':
+				$this->showDeleteTagForm( $request->getVal( 'tag' ) );
+				break;
+			default:
+				$this->showTagList();
+				break;
+		}
+	}
+
+	function showTagList() {
 		$out = $this->getOutput();
 		$out->setPageTitle( $this->msg( 'tags-title' ) );
 		$out->wrapWikiMsg( "<div class='mw-tags-intro'>\n$1\n</div>", 'tags-intro' );
+
+		$user = $this->getUser();
+		// Whether to show the "Actions" column
+		// If any actions added in the future require other user rights, add those
+		// rights here
+		$showActions = $user->isAllowed( 'managechangetags' );
 
 		// Write the headers
 		$html = Xml::tags( 'tr', null, Xml::tags( 'th', null, $this->msg( 'tags-tag' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-display-header' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-description-header' )->parse() ) .
 			Xml::tags( 'th', null, $this->msg( 'tags-active-header' )->parse() ) .
-			Xml::tags( 'th', null, $this->msg( 'tags-hitcount-header' )->parse() )
+			Xml::tags( 'th', null, $this->msg( 'tags-hitcount-header' )->parse() ) .
+			( $showActions ?
+				Xml::tags( 'th', array( 'class' => 'unsortable' ),
+					$this->msg( 'tags-actions-header' )->parse() ) :
+				'' )
 		);
 
 		// Used in #doTagRow()
 		$this->definedTags = array_fill_keys( ChangeTags::listDefinedTags(), true );
 
-		foreach ( ChangeTags::tagUsageStatistics() as $tag => $hitcount ) {
+		foreach ( $this->tagUsage as $tag => $hitcount ) {
 			$html .= $this->doTagRow( $tag, $hitcount );
 		}
 
@@ -109,7 +146,148 @@ class SpecialTags extends SpecialPage {
 		// add raw $hitcount for sorting, because tags-hitcount contains numbers and letters
 		$newRow .= Xml::tags( 'td', array( 'data-sort-value' => $hitcount ), $hitcountLink );
 
+		// actions
+		$actionLinks = array();
+		if ( $user->isAllowed( 'managechangetags' ) ) {
+			// Can only delete when the number of uses is less than MAX_DELETE_USES
+			if ( $hitcount <= self::MAX_DELETE_USES ) {
+				$actionLinks[] = Linker::linkKnown( $this->getPageTitle(),
+					$this->msg( 'tags-delete' )->escaped(),
+					array(),
+					array( 'action' => 'delete', 'tag' => $tag ) );
+			}
+
+			$newRow .= Xml::tags( 'td', null, $this->getLanguage()->pipeList( $actionLinks ) );
+		}
+
 		return Xml::tags( 'tr', null, $newRow ) . "\n";
+	}
+
+	/**
+	 * @param string $tag Tag that you are interested in deleting
+	 * @return bool|Message True on success, an error message object on failure
+	 */
+	public function canDeleteTag( $tag ) {
+		if ( !isset( $this->tagUsage[$tag] ) ) {
+			return $this->msg( 'tags-delete-not-found', $tag );
+		}
+		if ( $this->tagUsage[$tag] > self::MAX_DELETE_USES ) {
+			return $this->msg( 'tags-delete-too-many-uses', $tag, self::MAX_DELETE_USES );
+		}
+		return true;
+	}
+
+	protected function showDeleteTagForm( $tag ) {
+		$user = $this->getUser();
+		if ( !$user->isAllowed( 'managechangetags' ) ) {
+			throw new PermissionsError( 'managechangetags' );
+		}
+
+		$out = $this->getOutput();
+		$out->preventClickjacking();
+		$out->setPageTitle( $this->msg( 'tags-delete-title' ) );
+
+		// is the tag actually able to be deleted?
+		$canDeleteResult = $this->canDeleteTag( $tag );
+		if ( $canDeleteResult !== true ) {
+			$out->addWikiText( "<div class=\"error\">\n" . $canDeleteResult->text() .
+				"\n</div>" );
+			return;
+		}
+
+		$preText = $this->msg( 'tags-delete-explanation-initial', $tag )->parseAsBlock();
+		if ( $this->tagUsage[$tag] > 0 ) {
+			$preText .= $this->msg( 'tags-delete-explanation-in-use', $tag,
+				$this->tagUsage[$tag] )->parseAsBlock();
+		}
+		$preText .= $this->msg( 'tags-delete-explanation-warning', $tag )->parseAsBlock();
+
+		// see if the tag is in use
+		$definedTags = ChangeTags::listDefinedTags();
+		if ( in_array( $tag, $definedTags ) ) {
+			$preText .= $this->msg( 'tags-delete-explanation-active', $tag )->parseAsBlock();
+		}
+
+		$fields = array();
+		$fields['Reason'] = array(
+			'type' => 'text',
+			'label' => $this->msg( 'tags-delete-reason' )->plain(),
+			'size' => 50,
+		);
+		$fields['HiddenAction'] = array(
+			'type' => 'hidden',
+			'name' => 'action',
+			'default' => 'delete',
+		);
+		$fields['HiddenTag'] = array(
+			'type' => 'hidden',
+			'name' => 'tag',
+			'default' => $tag,
+			'required' => true,
+		);
+
+		$form = new HTMLForm( $fields, $this->getContext() );
+		$form->setSubmitCallback( array( $this, 'processDeleteTagForm' ) );
+		$form->setSubmitTextMsg( 'tags-delete-submit' );
+		$form->addPreText( $preText );
+		$form->show();
+
+		// if $form->show() didn't send us off somewhere else, let's set our
+		// breadcrumb link
+		$out->addBacklinkSubtitle( $this->getPageTitle() );
+	}
+
+	public function processDeleteTagForm( array $data, HTMLForm $form ) {
+		$context = $form->getContext();
+		$out = $context->getOutput();
+
+		$status = $this->processDeleteTag( $data['HiddenTag'], $data['Reason'],
+			$context );
+
+		if ( $status->isGood() ) {
+			$out->redirect( $this->getPageTitle()->getLocalURL() );
+			return true;
+		} else {
+			$out->addWikiText( "<div class=\"error\">\n" . $status->getWikitext() .
+				"\n</div>" );
+			return false;
+		}
+	}
+
+	/**
+	 * @param string $tag
+	 * @param IContextSource $context
+	 * @return Status
+	 */
+	public function processDeleteTag( $tag, $reason, IContextSource $context ) {
+		// does the tag exist?
+		$canDeleteResult = $this->canDeleteTag( $tag );
+		if ( $canDeleteResult !== true ) {
+			return Status::newFatal( $canDeleteResult );
+		}
+
+		// do it!
+		ChangeTags::deleteTagEverywhere( $tag );
+
+		// log it
+		$dbw = wfGetDB( DB_MASTER );
+		$logEntry = new ManualLogEntry( 'tagmanagement', 'delete' );
+		$logEntry->setPerformer( $context->getUser() );
+		// target page is not relevant, but it has to be set, so we just put in
+		// the title of Special:Tags
+		$logEntry->setTarget( $this->getPageTitle() );
+		$logEntry->setComment( $reason );
+		$logEntry->setParameters( array(
+			'4:tag' => $tag,
+			'5:count' => $this->tagUsage[$tag],
+		) );
+		$logEntry->setRelations( array( 'Tag' => $tag ) );
+		$logEntry->insert( $dbw );
+
+		// refresh the tag statistics
+		$this->tagUsage = ChangeTags::tagUsageStatistics();
+
+		return Status::newGood();
 	}
 
 	protected function getGroupName() {
