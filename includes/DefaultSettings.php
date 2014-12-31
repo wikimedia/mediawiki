a7a152f5827789adb35a692fcae43dfc10de8844
Ricordisamoa <ricordisamoa@openmailbox.org>
2014-12-31 01:42:16 +0100

    [WIP] enable native client-side rendering of SVG images
    Off by default, on with $wgSVGConverter = 'native'.
    Based on the original patch by fbstj.
    
    Bug: T5593
    Change-Id: I943303d28d2f415524b8bf4ba4736592a1727cf9

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -972,7 +972,7 @@ $wgExiv2Command = '/usr/bin/exiv2';
 
 /**
  * Scalable Vector Graphics (SVG) may be uploaded as images.
- * Since SVG support is not yet standard in browsers, it is
+ * Since SVG is only supported by newer browsers, it may be
  * necessary to rasterize SVGs to PNG as a fallback format.
  *
  * An external program is required to perform this conversion.
@@ -992,7 +992,8 @@ $wgSVGConverters = array(
 );
 
 /**
- * Pick a converter defined in $wgSVGConverters
+ * Pick a converter defined in $wgSVGConverters,
+ * or use 'native' to rely on web browsers.
  */
 $wgSVGConverter = 'ImageMagick';
 
