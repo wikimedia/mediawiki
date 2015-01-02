9805c4294b037261f2d43b557bdc56bcdfad756c
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-01-02 23:59:31 +0100

    Add namespace translations for nan
    Aliases added from fallback language cdo. Per request in
    https://translatewiki.net/wiki/Thread:User_talk:Nike/outo_p%C3%A4tk%C3%A4_osoitteissa
    
    Change-Id: Ic73d81cd9a477a9818aa0f0c8b596ab49850460a

--- languages/messages/MessagesNan.php
+++ languages/messages/MessagesNan.php
@@ -13,6 +13,44 @@
 
 $fallback = 'cdo, zh-hant';
 
+$namespaceNames = array(
+	NS_MEDIA            => 'Mûi-thé',
+	NS_SPECIAL          => 'Tek-pia̍t',
+	NS_TALK             => 'Thó-lūn',
+	NS_USER             => 'Iōng-chiá',
+	NS_USER_TALK        => 'Iōng-chiá_thó-lūn',
+	NS_PROJECT_TALK     => '$1_thó-lūn',
+	NS_FILE             => 'tóng-àn',
+	NS_FILE_TALK        => 'tóng-àn_thó-lūn',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_thó-lūn',
+	NS_TEMPLATE         => 'Pang-bô͘',
+	NS_TEMPLATE_TALK    => 'Pang-bô͘_thó-lūn',
+	NS_HELP             => 'Pang-chān',
+	NS_HELP_TALK        => 'Pang-chān_thó-lūn',
+	NS_CATEGORY         => 'Lūi-pia̍t',
+	NS_CATEGORY_TALK    => 'Lūi-pia̍t_thó-lūn',
+);
+
+$namespaceAliases = array(
+	'媒體' => NS_MEDIA,
+	'特殊' => NS_SPECIAL,
+	'討論' => NS_TALK,
+	'用戶' => NS_USER,
+	'用戶討論' => NS_USER_TALK,
+	'$1討論' => NS_PROJECT_TALK,
+	'文件' => NS_FILE,
+	'文件討論' => NS_FILE_TALK,
+	'媒體維基' => NS_MEDIAWIKI,
+	'媒體維基討論' => NS_MEDIAWIKI_TALK,
+	'模板' => NS_TEMPLATE,
+	'模板討論' => NS_TEMPLATE_TALK,
+	'幫助' => NS_HELP,
+	'幫助討論' => NS_HELP_TALK,
+	'分類' => NS_CATEGORY,
+	'分類討論' => NS_CATEGORY_TALK,
+);
+
 $datePreferences = array(
 	'default',
 	'ISO 8601',
