e824badc9b9b05e3c92ddbf3508f04fe8894933b
PranavK <pranavmk98@gmail.com>
2015-01-04 12:50:45 +0530

    Add support for passing objects
    Change-Id: I121acab012676e486e19b3f8b0fae50af3d3a567

--- includes/debug/MWDebug.php
+++ includes/debug/MWDebug.php
@@ -91,21 +91,29 @@ class MWDebug {
 	/**
 	 * Adds a line to the log
 	 *
-	 * @todo Add support for passing objects
 	 *
 	 * @since 1.19
-	 * @param string $str
+	 * @param $str
 	 */
-	public static function log( $str ) {
+
+	public static function log( $str=null ){
 		if ( !self::$enabled ) {
 			return;
 		}
+		if ($str===null){
 
 		self::$log[] = array(
+			'msg' => htmlspecialchars( print_r( $obj, true ) ),
+			'type' => 'log',
+			'caller' => wfGetCaller(),
+			);
+		}
+		else { self::$log[] = array(
 			'msg' => htmlspecialchars( $str ),
 			'type' => 'log',
 			'caller' => wfGetCaller(),
-		);
+			);
+		}
 	}
 
 	/**
