fc3b46234af71a1a29bdadb2a5de1344b51c5adb
gladoscc <admin@glados.cc>
2015-01-04 11:14:46 +1100

    Make jquery.suggestions obey maxRows setting
    The maxRows setting was previously unused and had no effect. This patch
    passes the property in the fetch() call, and updates the handlers to
    respect the maxRows setting.
    
    The previously unused setting of 7 is changed to 10 to match current
    behaviour.
    
    Bug: T39316
    Change-Id: I2873963aba9fd751607d11904b94bfcec8eb2d32

--- resources/src/jquery/jquery.suggestions.js
+++ resources/src/jquery/jquery.suggestions.js
@@ -28,7 +28,7 @@
  * suggestions: Suggestions to display
  *		Type: Array of strings
  * maxRows: Maximum number of suggestions to display at one time
- *		Type: Number, Range: 1 - 100, Default: 7
+ *		Type: Number, Range: 1 - 100, Default: 10
  * delay: Number of ms to wait for the user to stop typing
  *		Type: Number, Range: 0 - 1200, Default: 120
  * cache: Whether to cache results from a fetch
@@ -124,6 +124,7 @@ $.suggestions = {
 					context.config.fetch.call(
 						context.data.$textbox,
 						val,
+						context.config.maxRows,
 						function ( suggestions ) {
 							context.data.$textbox.suggestions( 'suggestions', suggestions );
 							if ( context.config.cache ) {
@@ -513,7 +514,7 @@ $.fn.suggestions = function () {
 					result: {},
 					$region: $( this ),
 					suggestions: [],
-					maxRows: 7,
+					maxRows: 10,
 					delay: 120,
 					cache: false,
 					cacheMaxAge: 60000,
