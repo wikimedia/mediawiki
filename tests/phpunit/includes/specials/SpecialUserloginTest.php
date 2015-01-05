4013298f53b1d3b3eb95d573147b8c3d6a740750
Deskana <dgarry@wikimedia.org>
2015-01-05 12:59:46 -0800

    Separate out username validation from addNewAccountInternal()
    This patch lays the groundwork for exposing a username validation function via
    the API, so that API clients can check whether a username is valid and could
    be created without having to actually attempt to create it.
    
    This patch also adds unit tests for the new function to make sure that its
    most crucial functionality continues to work.
    
    Change-Id: Ia0ea8467e8c1c30a17a380dd7f9e431a61366a9f

---
+++ tests/phpunit/includes/specials/SpecialUserloginTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @group Database
+ */
+class SpecialUserloginTest extends MediaWikiTestCase {
+	protected function setUp() {
+		parent::setUp();
+
+		//Create a dummy user to use to test creation attempts where user already exists
+		self::$users = array(
+			'sysop' => new TestUser(
+				'ThisUserExists',
+				'Example User',
+				'exampleuser@domain.test',
+				array( 'sysop' )
+			)
+		);
+	}
+
+	/**
+	 * Verifies that it is not possible to create a username with an empty username.
+	 * @covers LoginForm::createAndValidateUsername
+	 */
+	public function testNoUsername() {
+		$ul = new LoginForm();
+
+		$result = $ul->createAndValidateUsername( '', 'password', 'test@domain.test' );
+		$this->assertEquals( 'noname', $result->getMessage()->getKey() );
+	}
+
+	/**
+	 * Verifies that it is not possible to create a username that already exists.
+	 * @covers LoginForm::createAndValidateUsername
+	 */
+	public function testUserExists() {
+		$ul = new LoginForm();
+
+		$result = $ul->createAndValidateUsername( 'ThisUserExists', 'password', 'test@domain.test' );
+		$this->assertEquals( 'userexists', $result->getMessage()->getKey() );
+	}
+
+	/**
+	 * Verifies that it is not possible to create a username with a password
+	 * shorter than the minimum length.
+	 * @covers LoginForm::createAndValidateUsername
+	 */
+	public function testPasswordTooShort() {
+		$ul = new LoginForm();
+
+		$this->setMwGlobals( 'wgMinimalPasswordLength', 19 );
+		$result = $ul->createAndValidateUsername( 'Username', 'justthewronglength', 'test@domain.test' );
+		$this->assertEquals( 'passwordtooshort', $result->getMessage()->getKey() );
+	}
+
+	/**
+	 * Verifies that it is not possible to create a username with an invalid email address.
+	 * @covers LoginForm::createAndValidateUsername
+	 */
+	public function testInvalidEmail() {
+		$ul = new LoginForm();
+
+		$result = $ul->createAndValidateUsername( 'Username', 'password', 'invalid' );
+		$this->assertEquals( 'invalidemailaddress', $result->getMessage()->getKey() );
+	}
+
+	/**
+	 * Verifies that it is possible to create an account if everything is in order!
+	 * @covers LoginForm::createAndValidateUsername
+	 */
+	public function testSuccess() {
+		$ul = new LoginForm();
+
+		$username = 'Username';
+		$password = 'password';
+		$email = 'test@domain.test';
+
+		$result = $ul->createAndValidateUsername( $username, $password, $email );
+
+		//Assert that the result was success
+		$this->assertTrue( $result->isOK() );
+
+		// Assert that the username and email address are correct
+		// Password is not set by createAndValidateUsername, so cannot be checked here
+		$this->assertEquals( $username, $result->getValue()->getName() );
+		$this->assertEquals( $email, $result->getValue()->getEmail() );
+	}
+}
