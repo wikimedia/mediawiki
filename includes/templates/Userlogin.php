510664c1300f8d581756949b61f9dc2b7d0912da
Bartosz Dziewoński <matma.rex@gmail.com>
2015-01-08 00:11:46 +0100

    [WIP] Convert login form to OOUI
    This fixes a number of silly bugs I spotted. Need to split that out,
    because reviewing this already is going to suck. Also, CSS needs
    customizing. And unimplemented stuff needs implementing, see code
    comments.
    
    Bug: T85853
    Change-Id: I40c33dda71e227b1d85efbec4764a9dc1c4f1fb8

--- includes/templates/Userlogin.php
+++ includes/templates/Userlogin.php
@@ -1,7 +1,6 @@
 <?php
-// @codingStandardsIgnoreFile
 /**
- * Html form for user login (since 1.22 with VForm appearance).
+ * Html form for user login.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,179 +23,267 @@
 
 class UserloginTemplate extends BaseTemplate {
 
+	/**
+	 * Please give me back a normal msg() function before I go mad.
+	 * @return Message
+	 */
+	public function msg( $foo /* $args */ ) {
+		$args = func_get_args();
+		return call_user_func_array( 'wfMessage', $args );
+	}
+
 	function execute() {
 		global $wgCookieExpiration;
 		$expirationDays = ceil( $wgCookieExpiration / ( 3600 * 24 ) );
-?>
-<div class="mw-ui-container">
-	<div id="userloginprompt"><?php $this->msgWiki('loginprompt') ?></div>
-	<?php if ( $this->haveData( 'languages' ) ) { ?>
-		<div id="languagelinks">
-			<p><?php $this->html( 'languages' ); ?></p>
-		</div>
-	<?php } ?>
-	<div id="userloginForm">
-		<form name="userlogin" class="mw-ui-vform" method="post" action="<?php $this->text( 'action' ); ?>">
-			<?php if ( $this->data['loggedin'] ) { ?>
-				<div class="warningbox">
-					<?php echo $this->getMsg( 'userlogin-loggedin' )->params( $this->data['loggedinuser'] )->parse(); ?>
-				</div>
-			<?php } ?>
-			<section class="mw-form-header">
-				<?php $this->html( 'header' ); /* extensions such as ConfirmEdit add form HTML here */ ?>
-			</section>
+		
+		// Eh?
+		echo '<div id="userloginprompt">' . $this->msg( 'loginprompt' )->parse() . '</div>';
+		
+		if ( $this->haveData( 'languages' ) ) {
+			echo '<div id="languagelinks">'; // Meh
+			echo new OOUI\LabelWidget( array(
+				'label' => new OOUI\HtmlSnippet( $this->data['languages'] ),
+			) );
+			echo '</div>';
+		}
+		
+		
+		$form = new OOUI\FormLayout( array(
+			'method' => 'post',
+			'action' => $this->data['action'],
+		) );
+		$form->setAttributes( array( 'id' => 'userloginForm' ) );
+		$items = array();
+		
+		if ( $this->data['loggedin'] ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\LabelWidget( array(
+					'label' => new OOUI\HtmlSnippet(
+						$this->msg( 'userlogin-loggedin' )->params( $this->data['loggedinuser'] )->parse()
+					),
+					'classes' => 'warningbox',
+				)
+			) );
+		}
+		
+		// Extensions such as ConfirmEdit add form HTML here…
+		// This is terrible, we shouldn't be stuff random HTML into FieldsetLayouts, which is why
+		// this is so weird.
+		$items[] = new OOUI\FieldLayout(
+			new OOUI\Widget( array(
+				'content' => new OOUI\HtmlSnippet(
+					'<section class="mw-form-header">' . $this->data['header'] . '</section>'
+				),
+			) ) 
+		);
+		
+		if ( $this->data['message'] ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\LabelWidget( array(
+					'label' => new OOUI\HtmlSnippet(
+						// Damned if I know why we're going through these contortions
+						( $this->data['messagetype'] == 'error'
+							? '<strong>' . $this->msg( 'loginerror' )->escaped() . '</strong><br />'
+							: '' ) .
+						$this->data['message']
+					),
+					'classes' => $this->data['messagetype'] . 'box',
+				) ) 
+			);
+		}
 
-			<?php if ( $this->data['message'] ) { ?>
-				<div class="<?php $this->text( 'messagetype' ); ?>box">
-					<?php if ( $this->data['messagetype'] == 'error' ) { ?>
-						<strong><?php $this->msg( 'loginerror' ); ?></strong>
-						<br />
-					<?php } ?>
-					<?php $this->html( 'message' ); ?>
-				</div>
-			<?php } ?>
-
-			<div class="mw-ui-vform-field">
-				<label for='wpName1'>
-					<?php
-					$this->msg( 'userlogin-yourname' );
-
-					if ( $this->data['secureLoginUrl'] ) {
-						echo Html::element( 'a', array(
-							'href' => $this->data['secureLoginUrl'],
-							'class' => 'mw-ui-flush-right mw-secure',
-						), $this->getMsg( 'userlogin-signwithsecure' )->text() );
-					}
-					?>
-				</label>
-				<?php
-				$extraAttrs = array();
-				echo Html::input( 'wpName', $this->data['name'], 'text', array(
-					'class' => 'loginText mw-ui-input',
+		if ( $this->data['secureLoginUrl'] ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\ButtonWidget( array(
+					'label' => $this->msg( 'userlogin-signwithsecure' )->text(),
+					'href' => $this->data['secureLoginUrl'],
+					'framed' => false,
+					'classes' => 'mw-ui-flush-right mw-secure',
+				) ) 
+			);
+		}
+		
+		// The actual form fields at last
+		
+		$items[] = new OOUI\FieldLayout(
+			new OOUI\TextInputWidget( array(
+				'name' => 'wpName',
+				'value' => $this->data['name'],
+				'classes' => 'loginText',
+				/*
+					// Adding these attributes would suck a lot less if we supported them as config option,
+					// or had a catch-all 'attributes' option (like 'classes' and 'content' right now).
+					
 					'id' => 'wpName1',
 					'tabindex' => '1',
-					'size' => '20',
 					// 'required' is blacklisted for now in Html.php due to browser issues.
 					// Keeping here in case that changes.
 					'required' => true,
 					// Set focus to this field if it's blank.
 					'autofocus' => !$this->data['name'],
-					'placeholder' => $this->getMsg( 'userlogin-yourname-ph' )->text()
-				) );
-				?>
-			</div>
-
-			<div class="mw-ui-vform-field">
-				<label for='wpPassword1'>
-					<?php
-					$this->msg( 'userlogin-yourpassword' );
-					?>
-				</label>
-				<?php
-				echo Html::input( 'wpPassword', null, 'password', array(
-					'class' => 'loginPassword mw-ui-input',
+					'placeholder' => $this->msg( 'userlogin-yourname-ph' )->text(),
+				*/
+			) ),
+			array(
+				'label' => $this->msg( 'userlogin-yourname' )->text(),
+				'align' => 'top',
+			)
+		);
+		
+		$items[] = new OOUI\FieldLayout(
+			new OOUI\TextInputWidget( array(
+				'name' => 'wpPassword',
+				'type' => 'password',
+				'classes' => 'loginPassword',
+				/*
 					'id' => 'wpPassword1',
 					'tabindex' => '2',
-					'size' => '20',
 					// Set focus to this field if username is filled in.
 					'autofocus' => (bool)$this->data['name'],
-					'placeholder' => $this->getMsg( 'userlogin-yourpassword-ph' )->text()
-				) );
-				?>
-			</div>
-
-			<?php
-			if ( isset( $this->data['usedomain'] ) && $this->data['usedomain'] ) {
-				$select = new XmlSelect( 'wpDomain', false, $this->data['domain'] );
-				$select->setAttribute( 'tabindex', 3 );
-				foreach ( $this->data['domainnames'] as $dom ) {
-					$select->addOption( $dom );
-				}
-			?>
-				<div class="mw-ui-vform-field" id="mw-user-domain-section">
-					<label for='wpDomain'><?php $this->msg( 'yourdomainname' ); ?></label>
-					<?php echo $select->getHTML(); ?>
-				</div>
-			<?php } ?>
-
-			<?php
-			if ( $this->haveData( 'extrafields' ) ) {
-				echo $this->data['extrafields'];
+					'placeholder' => $this->msg( 'userlogin-yourpassword-ph' )->text(),
+				*/
+			) ),
+			array(
+				'label' => $this->msg( 'userlogin-yourpassword' )->text(),
+				'align' => 'top',
+			)
+		);
+		
+		if ( isset( $this->data['usedomain'] ) && $this->data['usedomain'] ) {
+			// Aww man, we have no PHP support for any sort of SelectWidget? At all?
+			
+			$select = new XmlSelect( 'wpDomain', false, $this->data['domain'] );
+			$select->setAttribute( 'tabindex', 3 );
+			foreach ( $this->data['domainnames'] as $dom ) {
+				$select->addOption( $dom );
 			}
-			?>
 
-			<div class="mw-ui-vform-field">
-				<?php if ( $this->data['canremember'] ) { ?>
-					<div class="mw-ui-checkbox">
-						<input name="wpRemember" type="checkbox" value="1" id="wpRemember" tabindex="4"
-							<?php if ( $this->data['remember'] ) {
-								echo 'checked="checked"';
-							} ?>
-						><label for="wpRemember">
-							<?php echo $this->getMsg( 'userlogin-remembermypassword' )->numParams( $expirationDays )->escaped(); ?></label>
-					</div>
-				<?php } ?>
-			</div>
-
-			<div class="mw-ui-vform-field">
-				<?php
-				$attrs = array(
-					'id' => 'wpLoginAttempt',
-					'name' => 'wpLoginAttempt',
-					'tabindex' => '6',
-				);
-				$modifiers = array(
-					'mw-ui-big', 'mw-ui-block', 'mw-ui-constructive',
-				);
-				echo Html::submitButton( $this->getMsg( 'pt-login-button' )->text(), $attrs, $modifiers );
-				?>
-			</div>
-
-			<div class="mw-ui-vform-field mw-form-related-link-container" id="mw-userlogin-help">
-				<?php
-				echo Html::element(
-					'a',
-					array(
-						'href' => Skin::makeInternalOrExternalUrl(
-							wfMessage( 'helplogin-url' )->inContentLanguage()->text()
-						),
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\Widget( array(
+					'content' => new OOUI\HtmlSnippet(
+						'<div id="mw-user-domain-section">' .
+							'<label for="wpDomain">' . $this->msg( 'yourdomainname' )->escaped() . '</label>' .
+							$select->getHTML() .
+						'</div>'
 					),
-					$this->getMsg( 'userlogin-helplink2' )->text()
-				);
-				?>
-			</div>
-			<?php
-			if ( $this->haveData( 'createOrLoginHref' ) ) {
-				if ( $this->data['useemail'] && $this->data['canreset'] && $this->data['resetlink'] === true ) {
-					echo Html::openElement( 'div',
-							array(
-								'class' => 'mw-ui-vform-field mw-form-related-link-container',
-							)
-						) .
-						Linker::link(
-							SpecialPage::getTitleFor( 'PasswordReset' ),
-							$this->getMsg( 'userlogin-resetpassword-link' )->parse()
-						) .
-						Html::closeElement( 'div' );
-				}
-				if ( $this->data['loggedin'] ) { ?>
-					<div id="mw-createaccount-another" class="mw-form-related-link-container mw-ui-vform-field">
-						<a href="<?php $this->text( 'createOrLoginHref' ); ?>" id="mw-createaccount-join" tabindex="7"><?php $this->msg( 'userlogin-createanother' ); ?></a>
-					</div>
-				<?php } else { ?>
-					<div id="mw-createaccount-cta" class="mw-form-related-link-container mw-ui-vform-field">
-						<?php $this->msg( 'userlogin-noaccount' ); ?><a href="<?php $this->text( 'createOrLoginHref' ); ?>" id="mw-createaccount-join" tabindex="7"  class="mw-ui-button mw-ui-progressive"><?php $this->msg( 'userlogin-joinproject' ); ?></a>
-					</div>
-				<?php } ?>
-			<?php } ?>
-			<?php if ( $this->haveData( 'uselang' ) ) { ?><input type="hidden" name="uselang" value="<?php $this->text( 'uselang' ); ?>" /><?php } ?>
-			<?php if ( $this->haveData( 'token' ) ) { ?><input type="hidden" name="wpLoginToken" value="<?php $this->text( 'token' ); ?>" /><?php } ?>
-			<?php if ( $this->data['cansecurelogin'] ) {?><input type="hidden" name="wpForceHttps" value="<?php $this->text( 'stickhttps' ); ?>" /><?php } ?>
-			<?php if ( $this->data['cansecurelogin'] && $this->haveData( 'fromhttp' )) {?><input type="hidden" name="wpFromhttp" value="<?php $this->text( 'fromhttp' ); ?>" /><?php } ?>
-		</form>
-	</div>
-</div>
-<?php
+				) ) 
+			);
+		}
+		
+		// I am weeping
+		if ( $this->haveData( 'extrafields' ) ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\Widget( array(
+					'content' => new OOUI\HtmlSnippet( $this->data['extrafields'] ),
+				) ) 
+			);
+		}
 
+		if ( $this->data['canremember'] ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\CheckboxInputWidget( array(
+					'name' => 'wpRemember',
+					// id="wpRemember"
+					// tabindex="4"
+					'value' => '1',
+					'selected' => (bool)$this->data['remember'],
+				) ),
+				array(
+					'label' => $this->msg( 'userlogin-remembermypassword' )->numParams( $expirationDays )->text(),
+					'align' => 'inline',
+				)
+			);
+		}
+	
+		$items[] = new OOUI\FieldLayout(
+			new OOUI\ButtonInputWidget( array(
+				'label' => $this->msg( 'pt-login-button' )->text(),
+				'name' => 'wpLoginAttempt',
+				'value' => '1',
+				// id="wpLoginAttempt"
+				// tabindex="6"
+				'value' => '1',
+				'selected' => (bool)$this->data['remember'],
+				'flags' => array( 'primary', 'constructive' ),
+			) )
+		);
+
+		// Bottom stuff
+		
+		$items[] = new OOUI\FieldLayout(
+			new OOUI\ButtonWidget( array(
+				'label' => $this->msg( 'userlogin-helplink2' )->text(),
+				'href' => Skin::makeInternalOrExternalUrl(
+					$this->msg( 'helplogin-url' )->inContentLanguage()->text()
+				),
+				'framed' => false,
+				'classes' => 'mw-form-related-link-container',
+			) ) 
+		);
+
+		if ( $this->data['useemail'] && $this->data['canreset'] && $this->data['resetlink'] === true ) {
+			$items[] = new OOUI\FieldLayout(
+				new OOUI\ButtonWidget( array(
+					'label' => $this->msg( 'userlogin-resetpassword-link' )->text(),
+					'href' => SpecialPage::getTitleFor( 'PasswordReset' )->getLinkURL(),
+					'framed' => false,
+					'classes' => 'mw-form-related-link-container',
+				) ) 
+			);
+		}
+
+		if ( $this->haveData( 'createOrLoginHref' ) ) {
+			if ( $this->data['loggedin'] ) {
+				$items[] = new OOUI\FieldLayout(
+					new OOUI\ButtonWidget( array(
+						// tabindex="7"
+						'label' => $this->msg( 'userlogin-createanother' )->text(),
+						'href' => $this->data['createOrLoginHref'],
+						'framed' => false,
+						'classes' => 'mw-form-related-link-container',
+					) ) 
+				);
+			} else {
+				$items[] = new OOUI\FieldLayout(
+					new OOUI\ButtonWidget( array(
+						// tabindex="7"
+						'label' => $this->msg( 'userlogin-joinproject' )->text(),
+						'href' => $this->data['createOrLoginHref'],
+						'flags' => array( 'primary', 'progressive' ),
+						'classes' => 'mw-form-related-link-container',
+					) ),
+					array(
+						// id="mw-createaccount-cta"
+						'label' => $this->msg( 'userlogin-noaccount' )->text(),
+						'align' => 'top',
+					)
+				);
+			}
+		}
+
+		$form->appendContent(
+			new OOUI\FieldsetLayout( array(
+				'label' => 'FormLayout that is a real HTML form',
+				'items' => $items,
+			) )
+		);
+		
+		// Hidden fields
+		$fields = '';
+		if ( $this->haveData( 'uselang' ) ) {
+			$fields .= Html::hidden( 'uselang', $this->data['uselang'] );
+		}
+		if ( $this->haveData( 'token' ) ) {
+			$fields .= Html::hidden( 'wpLoginToken', $this->data['token'] );
+		}
+		if ( $this->data['cansecurelogin'] ) {
+			$fields .= Html::hidden( 'wpForceHttps', $this->data['stickhttps'] );
+		}
+		if ( $this->data['cansecurelogin'] && $this->haveData( 'fromhttp' )) {
+			$fields .= Html::hidden( 'wpFromhttp', $this->data['fromhttp'] );
+		}
+		$form->appendContent( new OOUI\HtmlSnippet( $fields ) );
+
+		echo $form;
 	}
 }
