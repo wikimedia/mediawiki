29673989094adcf9cc27c621746d7dec2ca40013
Kunal Mehta <legoktm@gmail.com>
2015-01-07 10:27:01 -0800

    Only try to load UtfNormalDefines.php once
    It's already loaded in Defines.php, so no need to do it again
    
    Change-Id: Ie6f3bf3fbbcbaadd85cfc8e54527b93e98f7b898

--- includes/Setup.php
+++ includes/Setup.php
@@ -480,7 +480,6 @@ wfProfileOut( $fname . '-exception' );
 wfProfileIn( $fname . '-includes' );
 require_once "$IP/includes/normal/UtfNormalUtil.php";
 require_once "$IP/includes/GlobalFunctions.php";
-require_once "$IP/includes/normal/UtfNormalDefines.php";
 wfProfileOut( $fname . '-includes' );
 
 wfProfileIn( $fname . '-defaults2' );
