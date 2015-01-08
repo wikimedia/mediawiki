348ce3594dedfff0807439e61a953998a56555de
Timo Tijhof <krinklemail@gmail.com>
2015-01-08 20:33:47 +0000

    tests: Move npm-test to repo root
    (cherry picked from commit 6be355efdd67dd3e129fb55121f3406d87cd769f)
    
    Change-Id: Iaf5cf65616f82640145fbb6395c36129428602ca

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,11 @@
-/*!
- * Grunt file
- */
-
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-jscs-checker' );
 
-	grunt.file.setBase(  __dirname + '/../..' );
-
 	grunt.initConfig( {
-		pkg: grunt.file.readJSON( __dirname + '/package.json' ),
+		pkg: grunt.file.readJSON( 'package.json' ),
 		jshint: {
 			options: {
 				jshintrc: '.jshintrc'
