55fe2e3f5504b7cfca20b8916f70b613247cf5b2
paladox <thomasmulhall410@yahoo.com>
2015-01-09 20:04:40 +0000

    Update Moment.js from 2.8.4 to 2.10.3
    Project site
    * http://momentjs.com
    * https://github.com/moment/moment/
    
    File source
    * https://github.com/moment/moment/tree/2.10.3
    
    Changelog
    * https://github.com/moment/moment/blob/2.10.3/CHANGELOG.md
    
    Full changelog for 2.10.3
    
    https://gist.github.com/ichernev/f264b9bed5b00f8b1b7f
    
    Difference between 2.8.4 and 2.10.3
    
    2.10.3
    
    * add  moment.fn.to  and  moment.fn.toNow  (similar to  from  and fromNow )
    
    * new locales (Sinhalese (si), Montenegrin (me), Javanese (ja))
    
    * performance improvements
    
    2.10.2
    
    * fixed moment-with-locales in browser env caused by esperanto change
    
    2.10.1
    
    * regression: Add moment.duration.fn back
    
    2.10.0
    
    * Ported code to es6 modules
    
    2.9.0
    
    new languages
    
    * 2104 Frisian (fy) language file with unit test
    * 2097 add ar-tn locale
    
    deprecations
    
    * 2074 Implement moment.fn.utcOffset, deprecate momen.fn.zone
    
    new features
    
    * 2088 add moment.fn.isBetween
    * 2054 Call updateOffset when creating moment (needed for default timezone in moment-timezone)
    * 1893 Add moment.isDate method
    * 1825 Implement toJSON function on Duration
    * 1809 Allowing moment.set() to accept a hash of units
    * 2128 Add firstDayOfWeek, firstDayOfYear locale getters
    * 2131 Add quarter diff support
    
    core bugfixes
    
    * 2117 Add globalScope hack to more reliably distinguish Node vs browser
    * 2111 Create valid duration object when null is passed to moment.duration()
    * 2133 Reimplement month diff logic
    
    lang bugfixes
    
    * 2090 Add a special case for years in Hebrew locale
    * 1950 Fix parsing of meridiem in locales that have meridiem
    
    packaging
    
    * 2059, 2062, 2073 Add meteor packaging support
    * 2098 Add nuget publishing support
    * 2066 Uniform package description across Bower, Npmjs etc.
    * 1963 Use anonymous module when exporting via AMD
    
    Requires https://gerrit.wikimedia.org/r/#/c/207174/ for es6 support.
    
    Change-Id: I63fb2477b8069be52de1324a1a37dabe60f46ba7

--- resources/lib/moment/locale/hi.js
+++ resources/lib/moment/locale/hi.js
@@ -1,16 +1,14 @@
-// moment.js locale configuration
-// locale : hindi (hi)
-// author : Mayank Singhal : https://github.com/mayanksinghal
+//! moment.js locale configuration
+//! locale : hindi (hi)
+//! author : Mayank Singhal : https://github.com/mayanksinghal
 
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['moment'], factory); // AMD
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
-    } else {
-        factory((typeof global !== 'undefined' ? global : this).moment); // node or other global
-    }
-}(function (moment) {
+(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['moment'], factory) :
+   factory(global.moment)
+}(this, function (moment) { 'use strict';
+
+
     var symbolMap = {
         '1': '१',
         '2': '२',
@@ -36,7 +34,7 @@
         '०': '0'
     };
 
-    return moment.defineLocale('hi', {
+    var hi = moment.defineLocale('hi', {
         months : 'जनवरी_फ़रवरी_मार्च_अप्रैल_मई_जून_जुलाई_अगस्त_सितम्बर_अक्टूबर_नवम्बर_दिसम्बर'.split('_'),
         monthsShort : 'जन._फ़र._मार्च_अप्रै._मई_जून_जुल._अग._सित._अक्टू._नव._दिस.'.split('_'),
         weekdays : 'रविवार_सोमवार_मंगलवार_बुधवार_गुरूवार_शुक्रवार_शनिवार'.split('_'),
@@ -85,6 +83,21 @@
         },
         // Hindi notation for meridiems are quite fuzzy in practice. While there exists
         // a rigid notion of a 'Pahar' it is not used as rigidly in modern Hindi.
+        meridiemParse: /रात|सुबह|दोपहर|शाम/,
+        meridiemHour : function (hour, meridiem) {
+            if (hour === 12) {
+                hour = 0;
+            }
+            if (meridiem === 'रात') {
+                return hour < 4 ? hour : hour + 12;
+            } else if (meridiem === 'सुबह') {
+                return hour;
+            } else if (meridiem === 'दोपहर') {
+                return hour >= 10 ? hour : hour + 12;
+            } else if (meridiem === 'शाम') {
+                return hour + 12;
+            }
+        },
         meridiem : function (hour, minute, isLower) {
             if (hour < 4) {
                 return 'रात';
@@ -103,4 +116,7 @@
             doy : 6  // The week that contains Jan 1st is the first week of the year.
         }
     });
+
+    return hi;
+
 }));
