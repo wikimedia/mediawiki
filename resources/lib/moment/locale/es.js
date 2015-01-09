2174f367a10a8ed24e9bbbaaccf0a53b7849b82f
paladox <thomasmulhall410@yahoo.com>
2015-01-09 20:04:40 +0000

    Update Moment.js from 2.8.4 to 2.10.6
    Project site
    
    * http://momentjs.com
    * https://github.com/moment/moment/
    
    File source
    
    * https://github.com/moment/moment/tree/2.10.6
    
    Changelog
    * https://github.com/moment/moment/blob/2.10.6/CHANGELOG.md
    
    Full changelog for 2.10.5
    
    https://gist.github.com/ichernev/6ec13ac7efc396da44b2
    
    Difference between 2.8.4 and 2.10.6
    
    2.10.6
    
    * Fix regression introduced in  2.10.5  related to  moment.ISO_8601 parsing.
    
    2.10.5
    
    Core:
    
    * Improve unit bubbling for ISO dates
    * Fix Strict Date Parsing of ISO-8601 Dates Without Times
    * Fix inconsistent moment.min and moment.max results
    * Mark moment as invalid if bad string weekday repr is parsed, fixes #2423
    * Fix function check to workaround old browsers, fixes #2325
    * Add moment.fn.toObject (redo #2123)
    * Fix two digit year parsing with YYYY format
    * Fix isDSTShifted, fixes #2141
    * Improved duration bubbling, fixes #2166
    * Support up to milliseconds tokens up to 9 digits, fixes #2436
    * Allow custom formats to be passed to calendar function
    * Ensure all longDateFormats are fully expanded
    
    
    
    Refactoring / Perf:
    
    * Code cleanup and speedup
    * perf: use faster form of copying dates, across the board improvement
    * Fix imports in locale files
    * Fix test for fractional-hour offsets, fixes #2389
    * Remove dead code
    
    Locales:
    
    * Add ms locale (copy ms-my)
    * Add Talossan (tzl) locale (redo #2124)
    * Fixes year/week parsing for non-standard dow/doy combinations
    * Change Malay language code to comply IOS639-2
    * Locales fr & fr-ca: set an abbreviation for ordinal numbers >1
    * fix bengali translation of morning
    * fix(hu):Improve long date time formats for hungarian.
    * nominative month translation added to lt.js language
    * Fix seconds in pt-br (redo #2191)
    * Fixed seconds in azerbaijani
    
    2.10.3
    
    * add  moment.fn.to  and  moment.fn.toNow  (similar to  from  and fromNow )
    * new locales (Sinhalese (si), Montenegrin (me), Javanese (ja))
    * performance improvements
    
    2.10.2
    
    * fixed moment-with-locales in browser env caused by esperanto change
    
    2.10.1
    
    * regression: Add moment.duration.fn back
    
    2.10.0
    
    * Ported code to es6 modules
    
    2.9.0
    
    new languages
    
    * 2104 Frisian (fy) language file with unit test
    * 2097 add ar-tn locale
    
    deprecations
    
    * 2074 Implement moment.fn.utcOffset, deprecate momen.fn.zone
    
    new features
    
    * 2088 add moment.fn.isBetween
    * 2054 Call updateOffset when creating moment (needed for default timezone in moment-timezone)
    * 1893 Add moment.isDate method
    * 1825 Implement toJSON function on Duration
    * 1809 Allowing moment.set() to accept a hash of units
    * 2128 Add firstDayOfWeek, firstDayOfYear locale getters
    * 2131 Add quarter diff support
    
    core bugfixes
    
    * 2117 Add globalScope hack to more reliably distinguish Node vs browser
    * 2111 Create valid duration object when null is passed to moment.duration()
    * 2133 Reimplement month diff logic
    
    lang bugfixes
    
    * 2090 Add a special case for years in Hebrew locale
    * 1950 Fix parsing of meridiem in locales that have meridiem
    
    Change-Id: I63fb2477b8069be52de1324a1a37dabe60f46ba7

--- resources/lib/moment/locale/es.js
+++ resources/lib/moment/locale/es.js
@@ -1,21 +1,19 @@
-// moment.js locale configuration
-// locale : spanish (es)
-// author : Julio Napurí : https://github.com/julionc
+//! moment.js locale configuration
+//! locale : spanish (es)
+//! author : Julio Napurí : https://github.com/julionc
 
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['moment'], factory); // AMD
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
-    } else {
-        factory((typeof global !== 'undefined' ? global : this).moment); // node or other global
-    }
-}(function (moment) {
-    var monthsShortDot = 'ene._feb._mar._abr._may._jun._jul._ago._sep._oct._nov._dic.'.split('_'),
-        monthsShort = 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_');
+(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['moment'], factory) :
+   factory(global.moment)
+}(this, function (moment) { 'use strict';
 
-    return moment.defineLocale('es', {
-        months : 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_'),
+
+    var monthsShortDot = 'Ene._Feb._Mar._Abr._May._Jun._Jul._Ago._Sep._Oct._Nov._Dic.'.split('_'),
+        monthsShort = 'Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic'.split('_');
+
+    var es = moment.defineLocale('es', {
+        months : 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
         monthsShort : function (m, format) {
             if (/-MMM-/.test(format)) {
                 return monthsShort[m.month()];
@@ -23,16 +21,16 @@
                 return monthsShortDot[m.month()];
             }
         },
-        weekdays : 'domingo_lunes_martes_miércoles_jueves_viernes_sábado'.split('_'),
-        weekdaysShort : 'dom._lun._mar._mié._jue._vie._sáb.'.split('_'),
+        weekdays : 'Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado'.split('_'),
+        weekdaysShort : 'Dom._Lun._Mar._Mié._Jue._Vie._Sáb.'.split('_'),
         weekdaysMin : 'Do_Lu_Ma_Mi_Ju_Vi_Sá'.split('_'),
         longDateFormat : {
             LT : 'H:mm',
-            LTS : 'LT:ss',
+            LTS : 'H:mm:ss',
             L : 'DD/MM/YYYY',
             LL : 'D [de] MMMM [de] YYYY',
-            LLL : 'D [de] MMMM [de] YYYY LT',
-            LLLL : 'dddd, D [de] MMMM [de] YYYY LT'
+            LLL : 'D [de] MMMM [de] YYYY H:mm',
+            LLLL : 'dddd, D [de] MMMM [de] YYYY H:mm'
         },
         calendar : {
             sameDay : function () {
@@ -74,4 +72,7 @@
             doy : 4  // The week that contains Jan 4th is the first week of the year.
         }
     });
+
+    return es;
+
 }));
