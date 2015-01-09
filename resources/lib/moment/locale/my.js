f69f5bd1b4535d62b53bf2e86c4fe3ae388b2437
paladox <thomasmulhall410@yahoo.com>
2015-01-09 20:04:40 +0000

    Update Moment.js from 2.8.4 to 2.10.6
    Project site
    
    * http://momentjs.com
    * https://github.com/moment/moment/
    
    File source
    
    * https://github.com/moment/moment/tree/2.10.6
    
    Changelog
    * https://github.com/moment/moment/blob/2.10.6/CHANGELOG.md
    
    Full changelog for 2.10.5
    
    https://gist.github.com/ichernev/6ec13ac7efc396da44b2
    
    Difference between 2.8.4 and 2.10.6
    
    2.10.6
    
    * Fix regression introduced in  2.10.5  related to  moment.ISO_8601 parsing.
    
    2.10.5
    
    Core:
    
    * Improve unit bubbling for ISO dates
    * Fix Strict Date Parsing of ISO-8601 Dates Without Times
    * Fix inconsistent moment.min and moment.max results
    * Mark moment as invalid if bad string weekday repr is parsed, fixes #2423
    * Fix function check to workaround old browsers, fixes #2325
    * Add moment.fn.toObject (redo #2123)
    * Fix two digit year parsing with YYYY format
    * Fix isDSTShifted, fixes #2141
    * Improved duration bubbling, fixes #2166
    * Support up to milliseconds tokens up to 9 digits, fixes #2436
    * Allow custom formats to be passed to calendar function
    * Ensure all longDateFormats are fully expanded
    
    
    
    Refactoring / Perf:
    
    * Code cleanup and speedup
    * perf: use faster form of copying dates, across the board improvement
    * Fix imports in locale files
    * Fix test for fractional-hour offsets, fixes #2389
    * Remove dead code
    
    Locales:
    
    * Add ms locale (copy ms-my)
    * Add Talossan (tzl) locale (redo #2124)
    * Fixes year/week parsing for non-standard dow/doy combinations
    * Change Malay language code to comply IOS639-2
    * Locales fr & fr-ca: set an abbreviation for ordinal numbers >1
    * fix bengali translation of morning
    * fix(hu):Improve long date time formats for hungarian.
    * nominative month translation added to lt.js language
    * Fix seconds in pt-br (redo #2191)
    * Fixed seconds in azerbaijani
    
    2.10.3
    
    New locales
    
    * Sinhalese (si)
    * Montenegrin (me)
    * Javanese (ja)
    
    Features
    
    * Add moment.fn.to and moment.fn.toNow (similar to moment.fn.from and moment.fn.fromNow)
    * from/fromNow return Invalid date for invalid dates
    
    Bugfixes
    
    * strict mode now checks if am/pm hour is whitin 1-12
    
    Locales fixes:
    
    * use cyrillic letters for Chuvash diacritics
    * Swedish improvements to last-week
    * fix Croatian month names
    * better spacing in Chinese
    * Slovenian fixes
    * Fix Monday in Burmese
    * Fix text direction format in Arabic
    * Fix polish locale month parsing
    * Various fixes for latvian
    * Fixes for Portugese and Spanish
    * Add minute unit in Chinese
    
    2.10.2
    
    * fixed moment-with-locales in browser env caused by esperanto change
    
    2.10.1
    
    * regression: Add moment.duration.fn back
    
    2.10.0
    
    * Ported code to es6 modules
    
    2.9.0
    
    new languages
    
    * 2104 Frisian (fy) language file with unit test
    * 2097 add ar-tn locale
    
    deprecations
    
    * 2074 Implement moment.fn.utcOffset, deprecate momen.fn.zone
    
    new features
    
    * 2088 add moment.fn.isBetween
    * 2054 Call updateOffset when creating moment (needed for default timezone in moment-timezone)
    * 1893 Add moment.isDate method
    * 1825 Implement toJSON function on Duration
    * 1809 Allowing moment.set() to accept a hash of units
    * 2128 Add firstDayOfWeek, firstDayOfYear locale getters
    * 2131 Add quarter diff support
    
    core bugfixes
    
    * 2117 Add globalScope hack to more reliably distinguish Node vs browser
    * 2111 Create valid duration object when null is passed to moment.duration()
    * 2133 Reimplement month diff logic
    
    lang bugfixes
    
    * 2090 Add a special case for years in Hebrew locale
    * 1950 Fix parsing of meridiem in locales that have meridiem
    
    Bug: T117136
    Change-Id: I63fb2477b8069be52de1324a1a37dabe60f46ba7

--- resources/lib/moment/locale/my.js
+++ resources/lib/moment/locale/my.js
@@ -1,16 +1,14 @@
-// moment.js locale configuration
-// locale : Burmese (my)
-// author : Squar team, mysquar.com
+//! moment.js locale configuration
+//! locale : Burmese (my)
+//! author : Squar team, mysquar.com
 
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['moment'], factory); // AMD
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
-    } else {
-        factory((typeof global !== 'undefined' ? global : this).moment); // node or other global
-    }
-}(function (moment) {
+(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['moment'], factory) :
+   factory(global.moment)
+}(this, function (moment) { 'use strict';
+
+
     var symbolMap = {
         '1': '၁',
         '2': '၂',
@@ -34,19 +32,21 @@
         '၉': '9',
         '၀': '0'
     };
-    return moment.defineLocale('my', {
+
+    var my = moment.defineLocale('my', {
         months: 'ဇန်နဝါရီ_ဖေဖော်ဝါရီ_မတ်_ဧပြီ_မေ_ဇွန်_ဇူလိုင်_သြဂုတ်_စက်တင်ဘာ_အောက်တိုဘာ_နိုဝင်ဘာ_ဒီဇင်ဘာ'.split('_'),
         monthsShort: 'ဇန်_ဖေ_မတ်_ပြီ_မေ_ဇွန်_လိုင်_သြ_စက်_အောက်_နို_ဒီ'.split('_'),
         weekdays: 'တနင်္ဂနွေ_တနင်္လာ_အင်္ဂါ_ဗုဒ္ဓဟူး_ကြာသပတေး_သောကြာ_စနေ'.split('_'),
-        weekdaysShort: 'နွေ_လာ_င်္ဂါ_ဟူး_ကြာ_သော_နေ'.split('_'),
-        weekdaysMin: 'နွေ_လာ_င်္ဂါ_ဟူး_ကြာ_သော_နေ'.split('_'),
+        weekdaysShort: 'နွေ_လာ_ဂါ_ဟူး_ကြာ_သော_နေ'.split('_'),
+        weekdaysMin: 'နွေ_လာ_ဂါ_ဟူး_ကြာ_သော_နေ'.split('_'),
+
         longDateFormat: {
             LT: 'HH:mm',
             LTS: 'HH:mm:ss',
             L: 'DD/MM/YYYY',
             LL: 'D MMMM YYYY',
-            LLL: 'D MMMM YYYY LT',
-            LLLL: 'dddd D MMMM YYYY LT'
+            LLL: 'D MMMM YYYY HH:mm',
+            LLLL: 'dddd D MMMM YYYY HH:mm'
         },
         calendar: {
             sameDay: '[ယနေ.] LT [မှာ]',
@@ -86,4 +86,7 @@
             doy: 4 // The week that contains Jan 1st is the first week of the year.
         }
     });
+
+    return my;
+
 }));
