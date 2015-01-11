c5a802f7ad86af7a5862e5f61b138e0c8d8c83d4
Fomafix <fomafix@googlemail.com>
2015-01-11 21:32:51 +0000

    EditPage: Render wpSummaryLabel and wpSummary on one line with full width
    Bug: T64316
    Change-Id: Ifdd8a603ba256f2e612276148f5e79509241695b

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2777,6 +2777,9 @@ class EditPage {
 		}
 
 		$input = Html::input( 'wpSummary', $summary, 'text', $inputAttrs );
+		$input = Html::rawElement( 'div', array( 'id' => 'wpSummaryWrap' ) ) .
+			Html::rawElement( 'div', array( 'id' => 'wpSummaryDiv' ), $input ) .
+			Html::rawElement( 'div', array( 'id' => 'wpSummaryClear' ) );
 
 		return array( $label, $input );
 	}
