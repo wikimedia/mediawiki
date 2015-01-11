00f6574a1a6bb3c0891f336702ee5539d7bb057a
Timo Tijhof <krinklemail@gmail.com>
2015-01-11 05:21:13 -0800

    mediawiki.js: Move mw.format definition to where it's exposed
    No need for it to be a local and public method at the same time.
    The Message#parser can just use the public one.
    
    Change-Id: I57e040cda6da57cda447ea83f5b9b6b28b538158

--- resources/src/mediawiki/mediawiki.js
+++ resources/src/mediawiki/mediawiki.js
@@ -43,16 +43,6 @@
 		}
 	}
 
-	// String format helper. Replaces $1, $2 .. $N placeholders with positional
-	// args. Used by Message.prototype.parser() and exported as mw.format().
-	function format( formatString ) {
-		var parameters = slice.call( arguments, 1 );
-		return formatString.replace( /\$(\d+)/g, function ( str, match ) {
-			var index = parseInt( match, 10 ) - 1;
-			return parameters[index] !== undefined ? parameters[index] : '$' + match;
-		} );
-	}
-
 	/* Object constructors */
 
 	/**
@@ -307,7 +297,7 @@
 		 * This function will not be called for nonexistent messages.
 		 */
 		parser: function () {
-			return format.apply( null, [ this.map.get( this.key ) ].concat( this.parameters ) );
+			return mw.format.apply( null, [ this.map.get( this.key ) ].concat( this.parameters ) );
 		},
 
 		/**
@@ -444,13 +434,20 @@
 		/**
 		 * Format a string. Replace $1, $2 ... $N with positional arguments.
 		 *
-		 * @method
+		 * Used by Message#parser().
+		 *
 		 * @since 1.25
 		 * @param {string} fmt Format string
 		 * @param {Mixed...} parameters Substitutions for $N placeholders.
 		 * @return {string} Formatted string
 		 */
-		format: format,
+		format: function ( formatString ) {
+			var parameters = slice.call( arguments, 1 );
+			return formatString.replace( /\$(\d+)/g, function ( str, match ) {
+				var index = parseInt( match, 10 ) - 1;
+				return parameters[index] !== undefined ? parameters[index] : '$' + match;
+			} );
+		},
 
 		/**
 		 * Track an analytic event.
