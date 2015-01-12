4f2e31f985562b51073be87b423be2b143e23b29
umherirrender <umherirrender_de.wp@web.de>
2015-01-12 19:41:23 +0100

    Add plural support for message upload-permitted on Special:Upload
    The messages upload-permitted, upload-preferred and upload-prohibited
    containing a comma separated list of items. Adding the count of this
    list allows use of plural for better i18n.
    
    Change-Id: I3fb09db4788bff6ea89731f77848d78fb33dd493

--- includes/specials/SpecialUpload.php
+++ includes/specials/SpecialUpload.php
@@ -937,35 +937,37 @@ class UploadForm extends HTMLForm {
 		$config = $this->getConfig();
 
 		if ( $config->get( 'CheckFileExtensions' ) ) {
+			$fileExtensions = array_unique( $config->get( 'FileExtensions' ) );
 			if ( $config->get( 'StrictFileExtensions' ) ) {
 				# Everything not permitted is banned
 				$extensionsList =
 					'<div id="mw-upload-permitted">' .
 					$this->msg(
 						'upload-permitted',
-						$this->getContext()->getLanguage()->commaList(
-							array_unique( $config->get( 'FileExtensions' ) )
+						$this->getLanguage()->commaList(
+							$fileExtensions
 						)
-					)->parseAsBlock() .
+					)->numParams( count( $fileExtensions ) )->parseAsBlock() .
 					"</div>\n";
 			} else {
 				# We have to list both preferred and prohibited
+				$fileBlacklist = array_unique( $config->get( 'FileBlacklist' ) );
 				$extensionsList =
 					'<div id="mw-upload-preferred">' .
 						$this->msg(
 							'upload-preferred',
-							$this->getContext()->getLanguage()->commaList(
-								array_unique( $config->get( 'FileExtensions' ) )
+							$this->getLanguage()->commaList(
+								$fileExtensions
 							)
-						)->parseAsBlock() .
+						)->numParams( count( $fileExtensions ) )->parseAsBlock() .
 					"</div>\n" .
 					'<div id="mw-upload-prohibited">' .
 						$this->msg(
 							'upload-prohibited',
-							$this->getContext()->getLanguage()->commaList(
-								array_unique( $config->get( 'FileBlacklist' ) )
+							$this->getLanguage()->commaList(
+								$fileBlacklist
 							)
-						)->parseAsBlock() .
+						)->numParams( count( $fileBlacklist ) )->parseAsBlock() .
 					"</div>\n";
 			}
 		} else {
