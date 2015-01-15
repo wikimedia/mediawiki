95498d71df792aa3b475477936cfffaea879e6f7
Geoffrey Mon <geofbot@gmail.com>
2015-01-15 17:59:46 -0500

    Hide saved prefs message on change
    Use the postedit box for saved prefs message.
    Added fadeout for box on tab change or pref change.
    
    Bug: T19496
    Change-Id: Ibecbe21aa52ddc061d4bb27815f6fa5161a96735

--- resources/src/mediawiki.special/mediawiki.special.preferences.js
+++ resources/src/mediawiki.special/mediawiki.special.preferences.js
@@ -1,3 +1,4 @@
+
 /*!
  * JavaScript for Special:Preferences
  */
@@ -43,6 +44,12 @@ jQuery( function ( $ ) {
 			}
 	} ).insertBefore( $preftoc );
 
+	$( '.postedit-container' ).prependTo( 'body' );
+	$( '.postedit a' ).click( function() {
+		$( '.postedit' ).addClass( 'postedit postedit-faded' );
+		setTimeout( $( '.postedit' ).remove(), 500 );
+	} );
+
 	/**
 	 * It uses document.getElementById for security reasons (HTML injections in $()).
 	 *
@@ -78,6 +85,10 @@ jQuery( function ( $ ) {
 
 			$preferences.children( 'fieldset' ).hide().attr( 'aria-hidden', 'true' );
 			$( document.getElementById( 'mw-prefsection-' + name ) ).show().attr( 'aria-hidden', 'false' );
+			$( document.getElementById( 'mw-prefsection-' + name ) ).change( function() {
+				$( '.postedit' ).addClass( 'postedit postedit-faded' );
+				setTimeout( $( '.postedit' ).remove(), 500 );
+			} );
 		}
 	}
 
@@ -102,7 +113,11 @@ jQuery( function ( $ ) {
 				'aria-selected': i === 0 ? 'true' : 'false',
 				'aria-controls': ident
 			} )
-			.text( $legend.text() );
+			.text( $legend.text() )
+			.click( function() {
+				$( '.postedit' ).addClass( 'postedit postedit-faded' );
+				setTimeout( $( '.postedit' ).remove(), 500 );
+			} );
 		$li.append( $a );
 		$preftoc.append( $li );
 	} );
@@ -122,6 +137,8 @@ jQuery( function ( $ ) {
 		}
 		if ( $el.length > 0 ) {
 			switchPrefTab( $el.attr( 'href' ).replace( '#mw-prefsection-', '' ) );
+			$( '.postedit' ).addClass( 'postedit postedit-faded' );
+			setTimeout( $( '.postedit' ).remove(), 500 );
 		}
 	} );
 
@@ -142,6 +159,8 @@ jQuery( function ( $ ) {
 		( document.documentMode === undefined || document.documentMode >= 8 )
 	) {
 		$( window ).on( 'hashchange', function () {
+			$( '.postedit' ).addClass( 'postedit postedit-faded' );
+			setTimeout( $( '.postedit' ).remove(), 500 );
 			var hash = location.hash;
 			if ( hash.match( /^#mw-prefsection-[\w\-]+/ ) ) {
 				switchPrefTab( hash.replace( '#mw-prefsection-', '' ) );
@@ -152,10 +171,12 @@ jQuery( function ( $ ) {
 	// In older browsers we'll bind a click handler as fallback.
 	// We must not have onhashchange *and* the click handlers, other wise
 	// the click handler calls switchPrefTab() which sets the hash value,
-	// which triggers onhashcange and calls switchPrefTab() again.
+	// which triggers onhashchange and calls switchPrefTab() again.
 	} else {
 		$preftoc.on( 'click', 'li a', function ( e ) {
 			switchPrefTab( $( this ).attr( 'href' ).replace( '#mw-prefsection-', '' ) );
+			$( '.postedit' ).addClass( 'postedit postedit-faded' );
+			setTimeout( $( '.postedit' ).remove(), 500 );
 			e.preventDefault();
 		} );
 	}
