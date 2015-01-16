3bb71d9fc2ff297fc74ec4e8f4f71a5bfc5f16a2
Timo Tijhof <krinklemail@gmail.com>
2015-01-15 17:55:00 -0800

    jquery.tablesorter: Fix "capabilities" typo in comment
    Change-Id: Iea1a2956c8927bc29c904569ae594f6978aa8868

--- resources/src/jquery/jquery.tablesorter.js
+++ resources/src/jquery/jquery.tablesorter.js
@@ -15,7 +15,7 @@
  */
 /**
  *
- * @description Create a sortable table with multi-column sorting capabilitys
+ * @description Create a sortable table with multi-column sorting capabilities
  *
  * @example $( 'table' ).tablesorter();
  * @desc Create a simple tablesorter interface.
