991b62727c7db995a1606bab762ff2c934c586d2
m4tx <m4tx@m4tx.pl>
2015-01-20 22:11:33 +0100

    Make data provider in MediaHandlerTest static
    Also, improve function names.
    
    Follows-up Ie1cf501a6a0c8e688aca1a5577a293f526398dd3
    Change-Id: I5eef5f193192041d7b0514eaa8b779c03e6647c7

--- tests/phpunit/includes/media/MediaHandlerTest.php
+++ tests/phpunit/includes/media/MediaHandlerTest.php
@@ -19,24 +19,24 @@ class MediaHandlerTest extends MediaWikiTestCase {
 			"($width, $height, $max) wanted: {$expected}x$y, got: {z$result}x$y2" );
 	}
 
-	public function provideTestFitBoxWidth() {
+	public static function provideTestFitBoxWidth() {
 		return array_merge(
-			$this->provideTestFitBoxWidthSingle( 50, 50, array(
+			static::generateTestFitBoxWidthData( 50, 50, array(
 					50 => 50,
 					17 => 17,
 					18 => 18 )
 			),
-			$this->provideTestFitBoxWidthSingle( 366, 300, array(
+			static::generateTestFitBoxWidthData( 366, 300, array(
 					50 => 61,
 					17 => 21,
 					18 => 22 )
 			),
-			$this->provideTestFitBoxWidthSingle( 300, 366, array(
+			static::generateTestFitBoxWidthData( 300, 366, array(
 					50 => 41,
 					17 => 14,
 					18 => 15 )
 			),
-			$this->provideTestFitBoxWidthSingle( 100, 400, array(
+			static::generateTestFitBoxWidthData( 100, 400, array(
 					50 => 12,
 					17 => 4,
 					18 => 4 )
@@ -44,7 +44,7 @@ class MediaHandlerTest extends MediaWikiTestCase {
 		);
 	}
 
-	private function provideTestFitBoxWidthSingle( $width, $height, $tests ) {
+	private static function generateTestFitBoxWidthData( $width, $height, $tests ) {
 		$result = array();
 		foreach ( $tests as $max => $expected ) {
 			$result[] = array( $width, $height, $max, $expected );
