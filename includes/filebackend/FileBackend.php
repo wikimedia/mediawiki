8d2f7b9178205bc9892602384af94ebbb8bbfce1
Aaron Schulz <aschulz@wikimedia.org>
2015-01-25 01:19:24 -0800

    Never treat persistent stat cache as "latest" in FileBackend
    bug: T89184
    Change-Id: I035c0f85a641efa0e4e3209b2bcf9847c08ab0d2

--- includes/filebackend/FileBackend.php
+++ includes/filebackend/FileBackend.php
@@ -1204,7 +1204,9 @@ abstract class FileBackend {
 
 	/**
 	 * Preload file stat information (concurrently if possible) into in-process cache.
+	 *
 	 * This should be used when stat calls will be made on a known list of a many files.
+	 * This does not make use of the persistent file stat cache.
 	 *
 	 * @see FileBackend::getFileStat()
 	 *
