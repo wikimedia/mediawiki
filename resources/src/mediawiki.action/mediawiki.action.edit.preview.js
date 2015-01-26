40262d3526a5a48e67cc232d6138baa34d6ee159
Fomafix <fomafix@googlemail.com>
2015-01-26 21:08:41 +0000

    LivePreview: Update page status indicators
    Extend ApiParse by a attribute "id" containing the sanitized name.
    
    Bug: T87557
    Change-Id: I0caa7f3d9498943b3b1bc9054e63092042dd9405

--- resources/src/mediawiki.action/mediawiki.action.edit.preview.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.preview.js
@@ -28,6 +28,7 @@
 
 		copySelectors = [
 			// Main
+			'.mw-indicators',
 			'#firstHeading',
 			'#wikiPreview',
 			'#wikiDiff',
@@ -101,19 +102,31 @@
 			$.extend( postData, {
 				pst: '',
 				preview: '',
-				prop: 'text|displaytitle|modules|categorieshtml|templates|langlinks|limitreporthtml'
+				prop: 'text|indicators|displaytitle|modules|categorieshtml|templates|langlinks|limitreporthtml'
 			} );
 			if ( section !== '' ) {
 				postData.sectionpreview = '';
 			}
 			request = api.post( postData );
 			request.done( function ( response ) {
-				var li, newList, $next, $parent, $list;
+				var li, newList, $next, $parent, $list, $indicators;
 				if ( response.parse.modules ) {
 					mw.loader.load( response.parse.modules.concat(
 						response.parse.modulescripts,
 						response.parse.modulestyles,
 						response.parse.modulemessages ) );
+				}
+				$indicators = $( '.mw-indicators' ).empty();
+				if ( response.parse.indicators ) {
+					$.each( response.parse.indicators, function ( i, indicator ) {
+						$indicators
+						.append( $( '<div>' )
+							.addClass( 'mw-indicator' )
+							.attr( 'id', indicator.id )
+							.html( indicator['*'] ),
+							' '
+						);
+					} );
 				}
 				if ( response.parse.displaytitle ) {
 					$( '#firstHeading' ).html( '<span dir="auto">' + response.parse.displaytitle + '</span>' );
