18edc827f52f20ac278de00c5445a6c4c6c1a5a6
Brad Jorsch <bjorsch@wikimedia.org>
2015-01-27 20:34:36 -0800

    API: Add actions for resetting/changing password and email
    This also involves factoring the existing code out of the corresponding
    special pages and into a common backend class.
    
    Bug: T32788
    Change-Id: I5e8c57623cf8db7b285f157a7c8ff68f10e622ab

---
+++ tests/phpunit/includes/UserManglerTest.php
@@ -0,0 +1,238 @@
+<?php
+
+/**
+ * @group Database
+ * @covers UserMangler
+ */
+class UserManglerTest extends MediaWikiTestCase {
+	public function setUp() {
+		parent::setUp();
+		$this->setMwGlobals( array(
+			'wgGroupPermissions' => array(
+				'testcapture' => array( 'passwordreset' => true ),
+			),
+			'wgPasswordReminderResendTime' => false,
+			'wgRateLimits' => array(),
+			'wgHooks' => array(),
+		) );
+	}
+
+	public function testCanResetPassword() {
+		$config = new HashConfig( array() );
+		$context = new RequestContext;
+		$context->setConfig( new MultiConfig( array(
+			$config,
+			$context->getConfig(),
+		) ) );
+
+		$config->set( 'PasswordResetRoutes', array( 'username' => false, 'email' => false ) );
+		$status = UserMangler::canResetPassword( $context );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'passwordreset-disabled', $status->errors[0]['message'] );
+
+		$config->set( 'PasswordResetRoutes', array( 'username' => true, 'email' => true ) );
+		$config->set( 'EnableEmail', false );
+		$status = UserMangler::canResetPassword( $context );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'passwordreset-emaildisabled', $status->errors[0]['message'] );
+	}
+
+	public function testResetPassword() {
+		global $wgHooks;
+
+		$wgHooks['SpecialPasswordResetOnSubmit'] = array();
+
+		$config = new HashConfig( array(
+			'PasswordResetRoutes' => array( 'username' => true, 'email' => true ),
+		) );
+		$context = new RequestContext;
+		$context->setConfig( new MultiConfig( array(
+			$config,
+			$context->getConfig(),
+		) ) );
+
+		$user = User::newFromName( 'ResetPasswordUnitTestUser' );
+		$user->loadDefaults( 'ResetPasswordUnitTestUser' );
+		if ( !$user->getId() ) {
+			$user->addToDatabase();
+		}
+		$user->removeGroup( 'testcapture' );
+		$user->setEmail( '' );
+		$user->saveSettings();
+		$context->setUser( $user );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => '' ), false );
+		$this->assertFalse( $status );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ), true );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'badaccess-groups', $status->errors[0]['message'] );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => 'Invalid>Name' ) );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'nosuchuser', $status->errors[0]['message'] );
+
+		$i = 0;
+		$db = wfGetDB( DB_SLAVE );
+		do {
+			$i++;
+			$email = "example$i@example.invalid";
+		} while ( $db->selectRowCount( 'user', null, array( 'user_email' => $email ) ) > 0 );
+		$status = UserMangler::resetPassword( $context, array( 'email' => $email ) );
+		$this->assertTrue( $status->isGood() );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ) );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'noemail', $status->errors[0]['message'] );
+
+		$user->setEmail( 'example@example.invalid' );
+		$user->saveSettings();
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ) );
+		$this->assertTrue( $status->isGood() );
+		$this->assertNull( $status->value->email );
+
+		$status = UserMangler::resetPassword( $context, array( 'email' => $user->getEmail() ) );
+		$this->assertTrue( $status->isGood() );
+		$this->assertNull( $status->value->email );
+
+		$config->set( 'PasswordResetRoutes', array( 'username' => false, 'email' => true ) );
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ) );
+		$this->assertFalse( $status );
+
+		$config->set( 'PasswordResetRoutes', array( 'username' => true, 'email' => false ) );
+		$status = UserMangler::resetPassword( $context, array( 'email' => $user->getEmail() ) );
+		$this->assertFalse( $status );
+
+		$config->set( 'PasswordResetRoutes', array( 'username' => true, 'email' => true ) );
+
+		$user->addGroup( 'testcapture' );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ) );
+		$this->assertTrue( $status->isGood() );
+		$this->assertNull( $status->value->email );
+
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ), true );
+		$this->assertTrue( $status->isGood() );
+		$this->assertNotNull( $status->value->email );
+
+		$wgHooks['SpecialPasswordResetOnSubmit'] = array( function ( &$users, $hookData, &$error ) {
+			$error = array( 'hook aborted' );
+			return false;
+		} );
+		$status = UserMangler::resetPassword( $context, array( 'username' => $user->getName() ) );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'hook aborted', $status->errors[0]['message'] );
+	}
+
+	public function testChangePassword() {
+		global $wgHooks;
+
+		$context = new RequestContext;
+
+		$userAnon = $this->getMockBuilder( 'User' )
+			->getMock();
+		$userAnon->method( 'getName' )->willReturn( '192.0.2.1' );
+		$userAnon->method( 'isAnon' )->willReturn( true );
+
+		$user = $this->getMockBuilder( 'User' )
+			->getMock();
+		$user->method( 'getName' )->willReturn( 'ResetPasswordUnitTestUser' );
+		$user->method( 'isAnon' )->willReturn( false );
+		$user->method( 'checkPassword' )->will( $this->returnCallback( function ( $p ) {
+			return $p === 'old';
+		} ) );
+		$user->expects( $this->atLeastOnce() )->method( 'setCookies' );
+
+		$user2 = $this->getMockBuilder( 'User' )
+			->getMock();
+		$user2->method( 'getName' )->willReturn( 'ResetPasswordUnitTestUser2' );
+		$user2->method( 'isAnon' )->willReturn( false );
+		$user2->method( 'checkPassword' )->will( $this->returnCallback( function ( $p ) {
+			return $p === 'old';
+		} ) );
+		$user2->expects( $this->never() )->method( 'setCookies' );
+
+		$context->setUser( $user );
+		$mangler = new UserMangler( $userAnon, $context );
+		$status = $mangler->changePassword( 'old', 'new', 'new' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'nosuchusershort', $status->errors[0]['message'] );
+
+		$mangler = new UserMangler( $user, $context );
+		$status = $mangler->changePassword( 'old', 'new', 'new2' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'badretype', $status->errors[0]['message'] );
+
+		$status = $mangler->changePassword( 'wrong', 'new', 'new' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'resetpass-wrong-oldpass', $status->errors[0]['message'] );
+
+		$status = $mangler->changePassword( 'old', 'old', 'old' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'resetpass-recycled', $status->errors[0]['message'] );
+
+		$status = $mangler->changePassword( 'old', 'new', 'new' );
+		$this->assertTrue( $status->isGood() );
+
+		$mangler = new UserMangler( $user2, $context );
+		$status = $mangler->changePassword( 'old', 'new', 'new' );
+		$this->assertTrue( $status->isGood() );
+
+		$user2->method( 'setPassword' )->will( $this->throwException( new PasswordError( 'error!' ) ) );
+		$status = $mangler->changePassword( 'old', 'new', 'new' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( array( 'error!' ), $status->errors[0]['message']->getParams() );
+	}
+
+	public function testChangeEmail() {
+		global $wgHooks;
+
+		$config = new HashConfig( array(
+			'RequirePasswordforEmailChange' => true,
+		) );
+		$context = new RequestContext;
+		$context->setConfig( new MultiConfig( array(
+			$config,
+			$context->getConfig(),
+		) ) );
+
+		$userAnon = $this->getMockBuilder( 'User' )
+			->getMock();
+		$userAnon->method( 'getName' )->willReturn( '192.0.2.1' );
+		$userAnon->method( 'isAnon' )->willReturn( true );
+
+		$user = $this->getMockBuilder( 'User' )
+			->getMock();
+		$user->method( 'getName' )->willReturn( 'ResetPasswordUnitTestUser' );
+		$user->method( 'isAnon' )->willReturn( false );
+		$user->method( 'checkPassword' )->will( $this->returnCallback( function ( $p ) {
+			return $p === 'old';
+		} ) );
+		$user->method( 'setEmailWithConfirmation' )->willReturn( Status::newGood() );
+
+		$context->setUser( $user );
+		$mangler = new UserMangler( $userAnon, $context );
+		$status = $mangler->changeEmail( 'old', 'example@example.invalid' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'nosuchusershort', $status->errors[0]['message'] );
+
+		$context->setUser( $user );
+		$mangler = new UserMangler( $user, $context );
+		$status = $mangler->changeEmail( 'old', 'example.invalid' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'invalidemailaddress', $status->errors[0]['message'] );
+
+		$status = $mangler->changeEmail( 'wrong', 'example@example.invalid' );
+		$this->assertFalse( $status->isOk() );
+		$this->assertSame( 'wrongpassword', $status->errors[0]['message'] );
+
+		$status = $mangler->changeEmail( 'old', 'example@example.invalid' );
+		$this->assertTrue( $status->isGood() );
+
+		$config->set( 'RequirePasswordforEmailChange', false );
+		$status = $mangler->changeEmail( 'wrong', 'example@example.invalid' );
+		$this->assertTrue( $status->isGood() );
+	}
+
+}
