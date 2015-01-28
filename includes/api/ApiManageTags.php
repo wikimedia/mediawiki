0143f31557580df4e0848e2e172ddf745df075b9
This, that and the other <at.light@live.com.au>
2015-01-29 00:35:21 +1100

    Creation, deletion and improved management of change tags
    This allows users with the `managechangetags` right to create tags for
    use by wiki users. (Currently there is no way for editors to apply tags
    to their edits; that's to come in a later patch.)
    
    Extensions can reserve tag names for their own use, even if they do not
    define them or mark them as active.
    
    Tag managers can also delete tags with <= 5000 uses. Currently, if a tag is
    misspelt ("vandlaism") or no longer wanted (testing of OAuth, etc), the
    wiki is stuck with it forever. This change allows users with the
    "managechangetags" right to delete change tags from the database,
    including removing them from all revisions to which they are applied.
    
    Obviously this is a powerful thing to be able to do, but I view change
    tags as a "light" kind of interface, useful for revision patrolling and
    spam/vandalism fighting but not something that necessarily needs to hang
    around forever. It's not a big deal for this kind of data to be thrown
    away without being archived anywhere.
    
    Tags defined by an extension can only be deleted if the extension allows
    it.
    
    Changes to tags are logged in the new "tag management" log. There's even
    a nice API module, just for fun.
    
    Bug: T20670
    Change-Id: I77f476c8d0f32c80f720aa2c5e66869c81faa282

---
+++ includes/api/ApiManageTags.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * @ingroup API
+ * @since 1.25
+ */
+class ApiManageTags extends ApiBase {
+
+	public function execute() {
+		$params = $this->extractRequestParams();
+
+		// make sure the user is allowed
+		if ( !$this->getUser()->isAllowed( 'managechangetags' ) ) {
+			$this->dieUsage( "You don't have permission to manage change tags", 'permissiondenied' );
+		}
+
+		$result = $this->getResult();
+		$funcName = "{$params['operation']}TagWithChecks";
+		$status = ChangeTags::$funcName( $params['tag'], $params['reason'],
+			$this->getUser(), $params['ignorewarnings'] );
+
+		if ( !$status->isOK() ) {
+			$this->dieStatus( $status );
+		} else if ( !$status->isGood() ) {
+			$result->addValue( null, $this->getModuleName(), array(
+				'operation' => $params['operation'],
+				'tag' => $params['tag'],
+				'warnings' => $result->convertStatusToArray( $status, 'warning' ),
+			) );
+		} else {
+			$result->addValue( null, $this->getModuleName(), array(
+				'operation' => $params['operation'],
+				'tag' => $params['tag'],
+				'success' => '',
+				'logid' => $status->getValue(),
+			) );
+		}
+	}
+
+	public function mustBePosted() {
+		return true;
+	}
+
+	public function isWriteMode() {
+		return true;
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'operation' => array(
+				ApiBase::PARAM_TYPE => array( 'create', 'delete', 'activate', 'deactivate' ),
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'tag' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'reason' => array(
+				ApiBase::PARAM_TYPE => 'string',
+			),
+			'ignorewarnings' => array(
+				ApiBase::PARAM_TYPE => 'boolean',
+				ApiBase::PARAM_DFLT => false,
+			),
+		);
+	}
+
+	public function needsToken() {
+		return 'csrf';
+	}
+
+	protected function getExamplesMessages() {
+		return array(
+			'action=managetags&operation=create&tag=spam&reason=For+use+in+edit+patrolling&token=123ABC'
+				=> 'apihelp-managetags-example-create',
+			'action=managetags&operation=delete&tag=vandlaism&reason=Misspelt&token=123ABC'
+				=> 'apihelp-managetags-example-delete',
+			'action=managetags&operation=activate&tag=spam&reason=For+use+in+edit+patrolling&token=123ABC'
+				=> 'apihelp-managetags-example-activate',
+			'action=managetags&operation=deactivate&tag=spam&reason=No+longer+required&token=123ABC'
+				=> 'apihelp-managetags-example-deactivate',
+		);
+	}
+
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/API:Tag_management';
+	}
+}
