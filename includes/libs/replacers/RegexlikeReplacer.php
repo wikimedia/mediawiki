2659a67054aba27230a96f0a3ca643918f6f3c54
Kunal Mehta <legoktm@gmail.com>
2015-01-28 20:42:25 -0800

    Move "Replacers" into includes/libs/replacers/
    Split into separate files while we're at it
    
    Change-Id: I0bba4dcea686de088bd96964833fe6fb649a41e9

---
+++ includes/libs/replacers/RegexlikeReplacer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Class to replace regex matches with a string similar to that used in preg_replace()
+ */
+class RegexlikeReplacer extends Replacer {
+	private $r;
+
+	/**
+	 * @param string $r
+	 */
+	function __construct( $r ) {
+		$this->r = $r;
+	}
+
+	/**
+	 * @param array $matches
+	 * @return string
+	 */
+	function replace( $matches ) {
+		$pairs = array();
+		foreach ( $matches as $i => $match ) {
+			$pairs["\$$i"] = $match;
+		}
+
+		return strtr( $this->r, $pairs );
+	}
+}
