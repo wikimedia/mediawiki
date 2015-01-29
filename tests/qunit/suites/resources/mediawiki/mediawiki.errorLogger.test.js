a6b5b1a5d60e2e1dde394b6dc258bec39c4e31f2
Gergő Tisza <gtisza@wikimedia.org>
2015-01-29 01:21:01 +0000

    Report uncaught errors via mw.track
    Adds a global error handler that sends errors to mw.track as
    an errorLogging.windowOnerror event.
    
    Bug: T88874
    Change-Id: Ic091c9f93c59bda47bda2cfd609c64cd1d014b39

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.errorLogger.test.js
@@ -0,0 +1,42 @@
+( function ( $, mw ) {
+	QUnit.module( 'mediawiki.errorLogger', QUnit.newMwEnvironment() );
+
+	QUnit.test( 'installGlobalHandler', 7, function ( assert ) {
+		var w = {},
+			errorMessage = 'Foo',
+			errorUrl = 'http://example.com',
+			errorLine = '123',
+			errorColumn = '45',
+			errorObject = new Error( 'Foo'),
+			oldHandler = this.sandbox.stub();
+
+		this.sandbox.stub( mw, 'track' );
+
+		mw.errorLogger.installGlobalHandler( w );
+
+		assert.ok( w.onerror, 'Global handler has been installed' );
+		assert.strictEqual( w.onerror( errorMessage, errorUrl, errorLine ), false,
+			'Global handler returns false when there is no previous handler' );
+		sinon.assert.calledWithExactly( mw.track, 'global.error',
+			sinon.match( { errorMessage: errorMessage, url: errorUrl, lineNumber: errorLine } ) );
+
+		mw.track.reset();
+		w.onerror( errorMessage, errorUrl, errorLine, errorColumn, errorObject );
+		sinon.assert.calledWithExactly( mw.track, 'global.error',
+			sinon.match( { errorMessage: errorMessage, url: errorUrl, lineNumber: errorLine,
+			columnNumber: errorColumn, errorObject: errorObject } ) );
+
+		w = { onerror: oldHandler };
+
+		mw.errorLogger.installGlobalHandler( w );
+		w.onerror( errorMessage, errorUrl, errorLine );
+		sinon.assert.calledWithExactly( oldHandler, errorMessage, errorUrl, errorLine );
+
+		oldHandler.returns( false );
+		assert.strictEqual( w.onerror( errorMessage, errorUrl, errorLine ), false,
+			'Global handler preserves false return from previous handler' );
+		oldHandler.returns( true );
+		assert.strictEqual( w.onerror( errorMessage, errorUrl, errorLine ), true,
+			'Global handler preserves true return from previous handler' );
+	} );
+}( jQuery, mediaWiki ) );
