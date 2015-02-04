wikimedia/mediawiki

commit 80ca508ed117bbf54778068f24590c80dfe39128

Add User::equals
Seems stupid omission. Title has one. Why do I need to think how
to determine how to users objects point to the same user. Allows
more expressive code.

Also fixes a bug in multiple places where users "0" and "00" were
considered equal.

Change-Id: I682392e564b332b77ab489f2ad394fa2d28098a5

includes/specials/SpecialUserrights.php
@@ -106,7 +106,7 @@ class UserrightsPage extends SpecialPage {
 			}
 		}
 
-		if ( User::getCanonicalName( $this->mTarget ) == $user->getName() ) {
+		if ( User::getCanonicalName( $this->mTarget ) === $user->getName() ) {
 			$this->isself = true;
 		}
 
@@ -228,7 +228,7 @@ class UserrightsPage extends SpecialPage {
 		global $wgAuth;
 
 		// Validate input set...
-		$isself = ( $user->getName() == $this->getUser()->getName() );
+		$isself = $user->equals( $this->getUser() );
 		$groups = $user->getGroups();
 		$changeable = $this->changeableGroups();
 		$addable = array_merge( $changeable['add'], $isself ? $changeable['add-self'] : array() );

includes/User.php
@@ -4739,7 +4739,7 @@ class User implements IDBAccessObject {
 		if ( $action === true ) {
 			$action = 'byemail';
 		} elseif ( $action === false ) {
-			if ( $this->getName() == $wgUser->getName() ) {
+			if ( $this->equals( $wgUser ) ) {
 				$action = 'create';
 			} else {
 				$action = 'create2';
@@ -5023,5 +5023,16 @@ class User implements IDBAccessObject {
 		} else {
 			return Status::newFatal( 'badaccess-group0' );
 		}
+	}
+
+	/**
+	 * Checks if two user objects point to the same user.
+	 *
+	 * @since 1.25
+	 * @param User $user
+	 * @return bool
+	 */
+	public function equals( User $user ) {
+		return $this->getName() === $user->getName();
 	}
 }

includes/changes/RecentChange.php
@@ -450,7 +450,7 @@ class RecentChange {
 		}
 		// Users without the 'autopatrol' right can't patrol their
 		// own revisions
-		if ( $user->getName() == $this->getAttribute( 'rc_user_text' )
+		if ( $user->getName() === $this->getAttribute( 'rc_user_text' )
 			&& !$user->isAllowed( 'autopatrol' )
 		) {
 			$errors[] = array( 'markedaspatrollederror-noautopatrol' );

includes/diff/DifferenceEngine.php
@@ -487,7 +487,7 @@ class DifferenceEngine extends ContextSource {
 					array( 'USE INDEX' => 'rc_timestamp' )
 				);
 
-				if ( $change && $change->getPerformer()->getName() !== $user->getName() ) {
+				if ( $change && !$change->getPerformer()->equals( $user ) ) {
 					$rcid = $change->getAttribute( 'rc_id' );
 				} else {
 					// None found or the page has been created by the current user.

includes/page/Article.php
@@ -1129,7 +1129,7 @@ class Article implements Page {
 			return false;
 		}
 
-		if ( $rc->getPerformer()->getName() == $user->getName() ) {
+		if ( $rc->getPerformer()->equals( $user ) ) {
 			// Don't show a patrol link for own creations. If the user could
 			// patrol them, they already would be patrolled
 			return false;

tests/phpunit/includes/UserTest.php
@@ -355,4 +355,31 @@ class UserTest extends MediaWikiTestCase {
 				'false' => 'With / slash' ), 'With slash' ),
 		);
 	}
+
+	public function testEquals() {
+		$first = User::newFromName( 'EqualUser' );
+		$second = User::newFromName( 'EqualUser' );
+
+		$this->assertTrue( $first->equals( $first ) );
+		$this->assertTrue( $first->equals( $second ) );
+		$this->assertTrue( $second->equals( $first ) );
+
+		$third = User::newFromName( '0' );
+		$fourth = User::newFromName( '000' );
+
+		$this->assertFalse( $third->equals( $fourth ) );
+		$this->assertFalse( $fourth->equals( $third ) );
+
+		// Test users loaded from db with id
+		$user = User::newFromName( 'EqualUnitTestUser' );
+		if ( !$user->getId() ) {
+			$user->addToDatabase();
+		}
+
+		$id = $user->getId();
+
+		$fifth = User::newFromId( $id );
+		$sixth = User::newFromName( 'EqualUnitTestUser' );
+		$this->assertTrue( $fifth->equals( $sixth ) );
+	}
 }

commit 785f72043bc2ef6c8dd5985961573cb785056250

Unbreak interwiki user rights granting
UserRightsProxy does not have an equals method, nor does
CentralAuthGroupMembershipProxy.

This reverts the problematic part of 80ca508ed117bbf547 which
caused fatal errors.

Bug: T88505
Change-Id: I6c1dfe14bc08193c33f86482c8f835500c0dc21c

includes/specials/SpecialUserrights.php
@@ -218,7 +218,7 @@ class UserrightsPage extends SpecialPage {
 	/**
 	 * Save user groups changes in the database.
 	 *
-	 * @param User $user
+	 * @param User|UserRightsProxy $user
 	 * @param array $add Array of groups to add
 	 * @param array $remove Array of groups to remove
 	 * @param string $reason Reason for group change
@@ -228,7 +228,7 @@ class UserrightsPage extends SpecialPage {
 		global $wgAuth;
 
 		// Validate input set...
-		$isself = $user->equals( $this->getUser() );
+		$isself = $user->getName() == $this->getUser()->getName();
 		$groups = $user->getGroups();
 		$changeable = $this->changeableGroups();
 		$addable = array_merge( $changeable['add'], $isself ? $changeable['add-self'] : array() );