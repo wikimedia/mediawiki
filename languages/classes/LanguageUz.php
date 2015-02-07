077e904be69476deb6664160611c82433d0fa1e9
Bahodir Mansurov <bmansurov@wikimedia.org>
2015-02-06 19:10:57 -0500

    WIP: Update the Uzbek language converter
    * Remove duplicate keys
    * Order letters alphabetically
    * Add ц exceptions
    * Add tests
    
    Change-Id: I3dd18aa562c7255695ba8c041b844fe902db8c8f

--- languages/classes/LanguageUz.php
+++ languages/classes/LanguageUz.php
@@ -30,17 +30,15 @@ class UzConverter extends LanguageConverter {
 	public $toLatin = array(
 		'а' => 'a', 'А' => 'A',
 		'б' => 'b', 'Б' => 'B',
+		'в' => 'v', 'В' => 'V',
+		'г' => 'g', 'Г' => 'G',
 		'д' => 'd', 'Д' => 'D',
 		'е' => 'e', 'Е' => 'E',
-		'э' => 'e', 'Э' => 'E',
-		'в' => 'v', 'В' => 'V',
-		'х' => 'x', 'Х' => 'X',
-		'ғ' => 'gʻ', 'Ғ' => 'Gʻ',
-		'г' => 'g', 'Г' => 'G',
-		'ҳ' => 'h', 'Ҳ' => 'H',
+		'ё' => 'yo', 'Ё' => 'Yo',
 		'ж' => 'j', 'Ж' => 'J',
 		'з' => 'z', 'З' => 'Z',
 		'и' => 'i', 'И' => 'I',
+		'й' => 'y', 'Й' => 'Y',
 		'к' => 'k', 'К' => 'K',
 		'л' => 'l', 'Л' => 'L',
 		'м' => 'm', 'М' => 'M',
@@ -52,57 +50,152 @@ class UzConverter extends LanguageConverter {
 		'т' => 't', 'Т' => 'T',
 		'у' => 'u', 'У' => 'U',
 		'ф' => 'f', 'Ф' => 'F',
-		'ц' => 'c', 'Ц' => 'C',
-		'ў' => 'oʻ', 'Ў' => 'Oʻ',
+		'х' => 'x', 'Х' => 'X',
 		// note: at the beginning of a word and right after a consonant, only "s" is used
 		'ц' => 'ts', 'Ц' => 'Ts',
-		'қ' => 'q', 'Қ' => 'Q',
-		'ё' => 'yo', 'Ё' => 'Yo',
-		'ю' => 'yu', 'Ю' => 'Yu',
 		'ч' => 'ch', 'Ч' => 'Ch',
 		'ш' => 'sh', 'Ш' => 'Sh',
-		'й' => 'y', 'Й' => 'Y',
-		'я' => 'ya', 'Я' => 'Ya',
 		'ъ' => 'ʼ',
+		'ь' => '',
+		'э' => 'e', 'Э' => 'E',
+		'ю' => 'yu', 'Ю' => 'Yu',
+		'я' => 'ya', 'Я' => 'Ya',
+		'ў' => 'oʻ', 'Ў' => 'Oʻ',
+		'қ' => 'q', 'Қ' => 'Q',
+		'ғ' => 'gʻ', 'Ғ' => 'Gʻ',
+		'ҳ' => 'h', 'Ҳ' => 'H',
 	);
 
+	/**
+	 * @var array Latin to Cyrillic
+	 *
+	 * Rules:
+	 * 1. 'e' = 'э' at the beginning of a word or after a vowel (except for after an 'e'),
+	 * 	otherwise 'e' = 'е': iye = ийе (not 'ийэ'), konveyer = конвейер (not 'конвейэр'),
+	 * 	pleyer = плейер (not 'плейэр'), etc.
+	 * 2. There is no reliable rule to convert 's' to 'ц', 's' can be transliterated to 'с' or 'ц':
+	 * 	sirk = цирк, sen = сен. Use $tSWords array below to convert words that have 'ц' in them.
+	 * 3. Replace compounds before ъ rule
+	 * FIXME: add words that have the cyrillic 'ь' in them
+	 */
 	public $toCyrillic = array(
 		'a' => 'а', 'A' => 'А',
 		'b' => 'б', 'B' => 'Б',
 		'd' => 'д', 'D' => 'Д',
-		// at the beginning of a word and after a vowel, "э" is used instead of "e"
-		// (see regex below)
-		'e' => 'э', 'E' => 'Э',
+		// rule 1
+		'e' => 'е', 'E' => 'Е',
 		'f' => 'ф', 'F' => 'Ф',
 		'g' => 'г', 'G' => 'Г',
-		'g‘' => 'ғ', 'G‘' => 'Ғ', 'gʻ' => 'ғ', 'Gʻ' => 'Ғ',
 		'h' => 'ҳ', 'H' => 'Ҳ',
 		'i' => 'и', 'I' => 'И',
+		'j' => 'ж', 'J' => 'Ж',
 		'k' => 'к', 'K' => 'К',
 		'l' => 'л', 'L' => 'Л',
 		'm' => 'м', 'M' => 'М',
 		'n' => 'н', 'N' => 'Н',
 		'o' => 'о', 'O' => 'О',
 		'p' => 'п', 'P' => 'П',
+		'q' => 'қ', 'Q' => 'Қ',
 		'r' => 'р', 'R' => 'Р',
+		// rule 2
 		's' => 'с', 'S' => 'С',
 		't' => 'т', 'T' => 'Т',
 		'u' => 'у', 'U' => 'У',
 		'v' => 'в', 'V' => 'В',
 		'x' => 'х', 'X' => 'Х',
-		'z' => 'з', 'Z' => 'З',
-		'j' => 'ж', 'J' => 'Ж',
-		'o‘' => 'ў', 'O‘' => 'Ў', 'oʻ' => 'ў', 'Oʻ' => 'Ў',
-		'yo‘' => 'йў', 'Yo‘' => 'Йў', 'yoʻ' => 'йў', 'Yoʻ' => 'Йў',
-		'ts' => 'ц', 'Ts' => 'Ц',
-		'q' => 'қ', 'Q' => 'Қ',
-		'yo' => 'ё', 'Yo' => 'Ё',
-		'yu' => 'ю', 'Yu' => 'Ю',
-		'ch' => 'ч', 'Ch' => 'Ч',
-		'sh' => 'ш', 'Sh' => 'Ш',
 		'y' => 'й', 'Y' => 'Й',
-		'ya' => 'я', 'Ya' => 'Я',
+		'z' => 'з', 'Z' => 'З',
+		// START COMPOUNDS
+		// NOTE: If you change something here change the compounds converter below too.
+		'o‘' => 'ў', 'O‘' => 'Ў',
+			'oʻ' => 'ў', 'Oʻ' => 'Ў',
+		'g‘' => 'ғ', 'G‘' => 'Ғ',
+			'gʻ' => 'ғ', 'Gʻ' => 'Ғ',
+		'sh' => 'ш', 'Sh' => 'Ш', 'SH' => 'Ш',
+		'ch' => 'ч', 'Ch' => 'Ч', 'CH' => 'Ч',
+		'yo‘' => 'йў', 'Yo‘' => 'Йў', 'YO‘' => 'ЙЎ',
+			'yoʻ' => 'йў', 'Yoʻ' => 'Йў', 'YOʻ' => 'ЙЎ',
+		'ts' => 'ц', 'Ts' => 'Ц', 'TS' => 'Ц',
+		'yo' => 'ё', 'Yo' => 'Ё', 'YO' => 'Ё',
+		'yu' => 'ю', 'Yu' => 'Ю', 'YU' => 'Ю',
+		'ya' => 'я', 'Ya' => 'Я', 'YA' => 'Я',
+		// rule 1
+		'ye' => 'е', 'Ye' => 'Е', 'YE' => 'Е',
+		// END COMPOUNDS
 		'ʼ' => 'ъ',
+		// quotes
+		'„' => '«', '“' => '»',
+	);
+
+	// Most words that start with an 's' and are transliterated to a 'с' (сен, суд, салом, ...)
+	// The following words start with an 's' but need to be transliterated to 'ц'
+	public $tSWords = array(
+		// The following is tricky because there is a word form санга so maybe disable it for now
+		//'sanga' => 'цанга',
+		'sapfa' => 'цапфа',
+		'sedra' => 'цедра',
+		'seziy' => 'цезий',
+		'seytnot' => 'цейтнот',
+		'sellofan' => 'целлофан',
+		'selluloid' => 'целлулоид',
+		'sellyuloza' => 'целлюлоза',
+		'selsiy' => 'цельсий',
+		'sement' => 'цемент',
+		'sementla' => 'цементла', // this is the stem of the verb
+		'senz' => 'ценз',
+		'senzor' => 'цензор',
+		'senzura' => 'цензура',
+		'sent' => 'цент',
+		'sentner' => 'центнер',
+		'sentnerli' => 'центнерли',
+		'sentnerchi' => 'центнерчи',
+		'sentralizm' => 'централизм',
+		'sentrizm' => 'центризм',
+		'sentrist' => 'центрист',
+		'sentrifuga' => 'центрифуга',
+		'seriy' => 'церий',
+		'sesarka' => 'цесарка',
+		'sex' => 'цех',
+		'sian' => 'циан',
+		'sianli' => 'цианли',
+		'sivilizatsiya' => 'цивилизация',
+		// The following word exists in the dictionary, but the correct form is сигара, so disabling it.
+		//'sigara' => 'цигара',
+		'sikl' => 'цикл',
+		'siklik' => 'циклик',
+		'sikllashtir' => 'цикллаштир', // this is the stem of the verb
+		'sikkli' => 'циклли',
+		'siklon' => 'циклон',
+		'siklotron' => 'циклотрон',
+		'silindr' => 'цилиндр',
+		'silindrik' => 'цилиндрик',
+		'silindrli' => 'цилиндрли',
+		'singa' => 'цинга',
+		'sink' => 'цинк',
+		'sinkograf' => 'цинкограф',
+		'sinkografiya' => 'цинкография',
+		'sirk' => 'цирк',
+		'sirkoniy' => 'цирконий',
+		'sirkul' => 'циркуль',
+		'sirkulyar' => 'циркуляр',
+		'sirkchi' => 'циркчи',
+		'sirroz' => 'цирроз',
+		'sisterna' => 'цистерна',
+		'sisternali' => 'цистернали',
+		'sistit' => 'цистит',
+		'sitata' => 'цитата',
+		'sitatabozlik' => 'цитатабозлик',
+		// The following is not a word, so disabling it.
+		//'sito-' => 'цито-',
+		'sitodiagnostika' => 'цитодиагностика',
+		'sitokimyo' => 'цитокимё',
+		'sitoliz' => 'цитолиз',
+		'sitologiya' => 'цитология',
+		'sitrus' => 'цитрус',
+		'siferblat' => 'циферблат',
+		'siferblatli' => 'циферблатли',
+		'sikol' => 'цоколь',
+		'sunami' => 'цунами',
 	);
 
 	function loadDefaultTables() {
@@ -113,18 +206,77 @@ class UzConverter extends LanguageConverter {
 		);
 	}
 
+	function translateToCyrl ( $text ) {
+		// Replace ц ($tSWords) words first. Replace only the first letter because we want to
+		// preserve the case of other letters.
+		$text = preg_replace_callback(
+			'/\b('. implode( '|', array_keys( $this->tSWords ) ) .')/ui',
+			function( $matches ) {
+				if ( ctype_upper( $matches[1][0]) ) {
+					$result = 'Ц';
+				} else {
+					$result = 'ц';
+				}
+				return $result . substr( $matches[1], 1 );
+			},
+			$text
+		);
+
+		// Replace COMPOUNDS next
+		$text = preg_replace_callback(
+			'/(o‘|oʻ|g‘|gʻ|sh|ch|yo‘|yoʻ|ts|yo|yu|ya|ye)/ui',
+			function( $matches ) {
+				if ( ctype_upper( $matches[1] ) ) {
+					$result = ucfirst( $matches[1] );
+				} else {
+					$result = $matches[1];
+				}
+				return $this->toCyrillic[ $result ];
+			},
+			$text
+		);
+
+		$exceptions = array(
+			// rule 1
+			'e'=> 'э', 'E' => 'Э',
+		);
+		// After a vowel
+		$text = preg_replace_callback(
+			'/([aeiou]|o‘)([e])/ui',
+			function( $matches ) use ( &$exceptions ) {
+				if ( ctype_upper( $matches[2] ) ) {
+					$result = strtoupper( $matches[2] );
+				} else {
+					$result = $matches[2];
+				}
+				return $matches[1] . $exceptions[ $result ];
+			},
+			$text
+		);
+		// Beginning of a word
+		$text = preg_replace_callback(
+			'/\b([e])/ui',
+			function( $matches ) use ( &$exceptions ) {
+				if ( ctype_upper( $matches[1] ) ) {
+					$result = strtoupper( $matches[1] );
+				} else {
+					$result = $matches[1];
+				}
+				return $exceptions[ $result ];
+			},
+			$text
+		);
+		return $text;
+	}
+
 	function translate( $text, $toVariant ) {
 		if ( $toVariant == 'uz-cyrl' ) {
-			$text = str_replace( 'ye', 'е', $text );
-			$text = str_replace( 'Ye', 'Е', $text );
-			$text = str_replace( 'YE', 'Е', $text );
-			// "е" after consonants, otherwise "э" (see above)
-			$text = preg_replace( '/([BVGDJZYKLMNPRSTFXCWQʻ‘H])E/u', '$1Е', $text );
-			$text = preg_replace( '/([bvgdjzyklmnprstfxcwqʻ‘h])e/ui', '$1е', $text );
+			$text = $this->translateToCyrl( $text );
+		} else if ( $toVariant == 'uz-latn' ) {
+			// FIXME: add uz-latn
 		}
 		return parent::translate( $text, $toVariant );
 	}
-
 }
 
 /**
@@ -138,13 +290,13 @@ class LanguageUz extends Language {
 		parent::__construct();
 
 		$variants = array( 'uz', 'uz-latn', 'uz-cyrl' );
-		$variantfallbacks = array(
+		$variantFallbacks = array(
 			'uz' => 'uz-latn',
 			'uz-cyrl' => 'uz',
 			'uz-latn' => 'uz',
 		);
 
-		$this->mConverter = new UzConverter( $this, 'uz', $variants, $variantfallbacks );
+		$this->mConverter = new UzConverter( $this, 'uz', $variants, $variantFallbacks );
 		$wgHooks['PageContentSaveComplete'][] = $this->mConverter;
 	}
 }
