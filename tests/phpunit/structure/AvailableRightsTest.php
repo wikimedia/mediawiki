2f04e7429d725162a8c669228a92df3b40387375
Marius Hoch <hoo@online.de>
2015-02-09 23:50:35 +0100

    Add AvailableRightsTest for $wgAvailableRights completeness
    Because extensions often don't add their rights to it, thus
    User::getAllRights is incomplete.
    
    Change-Id: Id9ae9eff71e822ec5c038c1c1f990ac36f05cea8

---
+++ tests/phpunit/structure/AvailableRightsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Try to make sure that extensions register all rights in $wgAvailableRights.
+ *
+ * @author Marius Hoch < hoo@online.de >
+ */
+class AvailableRightsTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * All rights that should be in $wgAvailableRights + all "core" rights
+	 *
+	 * @return string[]
+	 */
+	private function getAllVisibleRights() {
+		global $wgGroupPermissions, $wgRevokePermissions;
+
+		$rights = User::getAllRights();
+
+		foreach( $wgGroupPermissions as $permissions ) {
+			$rights = array_merge( $rights, array_keys( $permissions ) );
+		}
+
+		foreach( $wgRevokePermissions as $permissions ) {
+			$rights = array_merge( $rights, array_keys( $permissions ) );
+		}
+
+		$rights = array_unique( $rights );
+		sort( $rights );
+
+		return $rights;
+	}
+
+	public function testAvailableRights() {
+		$missingRights = array_diff( $this->getAllVisibleRights(), User::getAllRights() );
+
+		$this->assertEquals(
+			array(),
+			array_values( $missingRights ), // Re-Index to produce nicer output, keys are meaningless
+			'Additional user rights need to be added to $wgAvailableRights'
+		);
+	}
+}
