160fc96f11c081b7c6e1ae9d428f1e0aeb53a298
Kunal Mehta <legoktm@gmail.com>
2015-02-12 23:40:13 -0800

    registration: Improve merging of arrays
    Currently we use array_merge_recursive when merging any array, which is really
    only needed for merging $wgHooks entries, and causes issues when trying to
    merge default settings if the config is already set.
    
    $wgHooks and $wgGroupPermissions are now special cased when merging, and all
    other arrays are just +='d.
    
    Bug: T88665
    Bug: T89364
    Change-Id: I773a9463d4428aa618c17f848c01b24e04610e95

---
+++ tests/phpunit/includes/registration/ExtensionRegistryTest.php
@@ -0,0 +1,164 @@
+<?php
+
+class ExtensionRegistryTest extends MediaWikiTestCase {
+
+	/**
+	 * @covers ExtensionRegistry::exportExtractedData
+	 * @dataProvider provideExportExtractedDataGlobals
+	 * @@backupGlobals enabled
+	 */
+	public function testExportExtractedDataGlobals( $desc, $before, $globals, $expected ) {
+		if ( $before ) {
+			// HHVM doesn't support $GLOBALS += ...
+			foreach ( $before as $key => $value ) {
+				$GLOBALS[$key] = $value;
+			}
+		}
+		$info = array(
+			'globals' => $globals,
+			'callbacks' => array(),
+			'defines' => array(),
+			'credits' => array(),
+			'attributes' => array(),
+		);
+		$registry = new ExtensionRegistry();
+		$class = new ReflectionClass( 'ExtensionRegistry' );
+		$method = $class->getMethod( 'exportExtractedData' );
+		$method->setAccessible( true );
+		$method->invokeArgs( $registry, array( $info ) );
+		foreach ( $expected as $name => $value ) {
+			$this->assertArrayHasKey( $name, $GLOBALS, $desc );
+			$this->assertEquals( $value, $GLOBALS[$name], $desc );
+		}
+	}
+
+	public static function provideExportExtractedDataGlobals() {
+		return array(
+			array(
+				'No global already set, simple array',
+				null,
+				array(
+					'mwtestDefaultOptions' => array(
+						'foobar' => true,
+					)
+				),
+				array(
+					'mwtestDefaultOptions' => array(
+						'foobar' => true,
+					)
+				),
+			),
+			array(
+				'Global already set, simple array',
+				array(
+					'mwtestDefaultOptions' => array(
+						'foobar' => true,
+						'foo' => 'string'
+					),
+				),
+				array(
+					'mwtestDefaultOptions' => array(
+						'barbaz' => 12345,
+						'foobar' => false,
+					),
+				),
+				array(
+					'mwtestDefaultOptions' => array(
+						'barbaz' => 12345,
+						'foo' => 'string',
+						'foobar' => true,
+					),
+				)
+			),
+			array(
+				'No global already set, $wgHooks',
+				array(
+					'wgHooks' => array(),
+				),
+				array(
+					'wgHooks' => array(
+						'FooBarEvent' => array(
+							'FooBarClass::onFooBarEvent'
+						),
+					),
+				),
+				array(
+					'wgHooks' => array(
+						'FooBarEvent' => array(
+							'FooBarClass::onFooBarEvent'
+						),
+					),
+				),
+			),
+			array(
+				'Global already set, $wgHooks',
+				array(
+					'wgHooks' => array(
+						'FooBarEvent' => array(
+							'FooBarClass::onFooBarEvent'
+						),
+						'BazBarEvent' => array(
+							'FooBarClass::onBazBarEvent',
+						),
+					),
+				),
+				array(
+					'wgHooks' => array(
+						'FooBarEvent' => array(
+							'BazBarClass::onFooBarEvent',
+						),
+					),
+				),
+				array(
+					'wgHooks' => array(
+						'FooBarEvent' => array(
+							'FooBarClass::onFooBarEvent',
+							'BazBarClass::onFooBarEvent',
+						),
+						'BazBarEvent' => array(
+							'FooBarClass::onBazBarEvent',
+						),
+					),
+				),
+			),
+			array(
+				'Global already set, $wgGroupPermissions',
+				array(
+					'wgGroupPermissions' => array(
+						'sysop' => array(
+							'something' => true,
+						),
+						'user' => array(
+							'somethingtwo' => true,
+						)
+					),
+				),
+				array(
+					'wgGroupPermissions' => array(
+						'customgroup' => array(
+							'right' => true,
+						),
+						'user' => array(
+							'right' => true,
+							'somethingtwo' => false,
+						)
+					),
+				),
+				array(
+					'wgGroupPermissions' => array(
+						'customgroup' => array(
+							'right' => true,
+						),
+						'sysop' => array(
+							'something' => true,
+						),
+						'user' => array(
+							'somethingtwo' => true,
+							'right' => true,
+						)
+					),
+				),
+			)
+		);
+	}
+}
