eb38aaaffe88fe4c845d45d8e8c36d197456209c
Alex Monk <krenair@wikimedia.org>
2015-02-14 00:18:12 +0000

    Fix hook situation for Skin::doEditSectionLink
    The old hook supplied a way to override the HTML used for the section link,
    but two extensions both trying to use it was obviously not going to work.
    Deprecate it in favour of a hook that goes around gathering info to build
    the initial HTML, then shoves it through the old hook for back-compat.
    
    So that WikiEditor can add in extra URL parameters as well as VE adding in it's
    own link.
    
    Bug: T88027
    Change-Id: I5a7a23709805625bdefb69cd9379be0c95acd982

--- includes/skins/Skin.php
+++ includes/skins/Skin.php
@@ -1584,18 +1584,40 @@ abstract class Skin extends ContextSource {
 			$attribs['title'] = wfMessage( 'editsectionhint' )->rawParams( $tooltip )
 				->inLanguage( $lang )->text();
 		}
-		$link = Linker::link( $nt, wfMessage( 'editsection' )->inLanguage( $lang )->text(),
-			$attribs,
-			array( 'action' => 'edit', 'section' => $section ),
-			array( 'noclasses', 'known' )
+
+		$links = array(
+			'editsection' => array(
+				'text' => wfMessage( 'editsection' )->inLanguage( $lang )->text(),
+				'targetTitle' => $nt,
+				'attribs' => $attribs,
+				'query' => array( 'action' => 'edit', 'section' => $section ),
+				'classes' => array( 'noclasses', 'known' )
+			)
 		);
 
-		# Add the brackets and the span and run the hook.
-		$result = '<span class="mw-editsection">'
-			. '<span class="mw-editsection-bracket">[</span>'
-			. $link
-			. '<span class="mw-editsection-bracket">]</span>'
-			. '</span>';
+		Hooks::run( 'EditSectionLinks', array( $this, $nt, $section, $tooltip, &$links, $lang ) );
+
+		$result = '<span class="mw-editsection"><span class="mw-editsection-bracket">[</span>';
+
+		$linksHtml = array();
+		foreach ( $links as $k => $linkDetails ) {
+			$linksHtml[] = Linker::link(
+				$linkDetails['targetTitle'],
+				$linkDetails['text'],
+				$linkDetails['attribs'],
+				$linkDetails['query'],
+				$linkDetails['options']
+			);
+		}
+
+		$result .= implode(
+			'<span class="mw-editsection-divider">'
+				. wfMessage( 'pipe-separator' )->inLanguage( $lang )->text()
+				. '</span>',
+			$linksHtml
+		);
+
+		$result .= '<span class="mw-editsection-bracket">]</span></span>';
 
 		Hooks::run( 'DoEditSectionLink', array( $this, $nt, $section, $tooltip, &$result, $lang ) );
 		return $result;
