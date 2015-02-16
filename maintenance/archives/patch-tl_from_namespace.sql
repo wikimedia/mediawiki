7843a9e533260ef42fdfab4f889f0c79da4dc41a
Sean Pringle <springle@wikimedia.org>
2015-02-16 16:16:58 +1000

    Reorder backlinks indexes
    Bug: T89630
    Change-Id: I2b04268545b9a88c584f6461f67a38fe3944c17a

--- maintenance/archives/patch-tl_from_namespace.sql
+++ maintenance/archives/patch-tl_from_namespace.sql
@@ -1,4 +1,4 @@
 ALTER TABLE /*_*/templatelinks
 	ADD COLUMN tl_from_namespace int NOT NULL default 0;
 
-CREATE INDEX /*i*/tl_backlinks_namespace ON /*_*/templatelinks (tl_namespace,tl_title,tl_from_namespace,tl_from);
+CREATE INDEX /*i*/tl_backlinks_namespace ON /*_*/templatelinks (tl_from_namespace,tl_namespace,tl_title,tl_from);
