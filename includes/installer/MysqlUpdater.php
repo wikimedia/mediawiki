f96d71ffda5f9012d7fc1a2b11923edf03602d18
Brian Wolff <bawolff+wn@gmail.com>
2015-02-19 20:31:31 -0400

    Allow edit summaries to be up to 767 bytes long
    This is just the db part. The changes to UI validation code will
    come in a dependent patch later.
    
    This changes the max size of various *_comment, *_description
    and *_reason fields to be 767 bytes. This size is chosen to
    be consistent with the largest we can get away with while
    still having covering indexes.
    
    It also unifies the various data types of these fields to
    varbinary. (Note, this was previously done and reverted in
    r80547, but I don't think the reason for revert applies to
    the edit summary fields)
    
    MyISAM has a much smaller max index size, and this wouldn't
    work with that storage engine. However, none of these fields
    are used in default indexes (only in WMF indexes), so this
    should not be an issue for anyone using MyISAM. Postgress
    and sqlite already supported the larger edit summary size.
    
    Note: This patch increases min mysql version to 5.0.3.
    
    Bug: T6715
    Change-Id: I8558e80a18e4591f07f2c3e80f792ea4435c4e71

--- includes/installer/MysqlUpdater.php
+++ includes/installer/MysqlUpdater.php
@@ -271,6 +271,8 @@ class MysqlUpdater extends DatabaseUpdater {
 			array( 'dropField', 'site_stats', 'ss_total_views', 'patch-drop-ss_total_views.sql' ),
 			array( 'dropField', 'page', 'page_counter', 'patch-drop-page_counter.sql' ),
 			array( 'doUserNewTalkUseridUnsigned' ),
+			// note this patch covers other _comment and _description fields too
+			array( 'modifyField', 'recentchanges', 'rc_comment', 'patch-editsummary-length.sql' ),
 		);
 	}
 
