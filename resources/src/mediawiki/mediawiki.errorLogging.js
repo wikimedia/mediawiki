877fc33b33de8a29843e16478a7cffb9d9ae3f7d
Gergő Tisza <gtisza@wikimedia.org>
2015-02-21 02:53:06 +0000

    Add error id and manual reporting to mw.errorLogging
    Adds an id property to events fired by mw.errorLogging. The id can
    be communicated to the user and it can be used to identify which error
    logs are related to a report.
    
    Also adds mw.errorLogging.logError which can be used to manually
    report errors which have been handled by the application.
    
    Id generation is a naive UUID algorithm. This might need to be
    rethought later when scaling up the error reporting service to
    high traffic levels: if the server does not have the throughput
    or storage to keep every id, the id needs to be the same for
    the same error, such as a hash of the stack trace. We'll cross
    that bridge when we get there.
    
    Bug: T89384
    Change-Id: Ic6defcd3206a2155c3d1d3ecf6bab287a1e73cd5

--- resources/src/mediawiki/mediawiki.errorLogging.js
+++ resources/src/mediawiki/mediawiki.errorLogging.js
@@ -16,18 +16,77 @@
 		 * @param {Array} args The arguments with which window.onerror was called. These are brower-
 		 *   dependent; https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onerror
 		 *   has some documentation.
+		 * @param {string} id Error identifier.
 		 */
 
 		/**
+		 * Fired via mw.track when an error is caught and reported to mw.errorLogging.
+		 *
+		 * @event errorLogging_exception
+		 * @param {Error|Mixed} e The error that was thrown. Usually an Error object, but
+		 *   in Javascript any value can be used with 'throw' so no guarantees.
+		 * @param {string} source Name of the function which threw the error.
+		 * @param {string} id Error identifier.
+		 */
+
+		/**
+		 * Report an error and return an id with which it can be referenced.
+		 *
+		 * Note that error logging is an asynchronous operation on multiple levels, and because
+		 * of that returning an error id does not guarantee that the error was successfully logged.
+		 *
+		 * @param {Error|Mixed} error The error that needs to be logged. Typically an Error object
+		 *   that was thrown and caught, but could be a simple string or anything else.
+		 * @param {Object} [context] Additional information about the error.
+		 * @return {string} An error id.
+		 */
+		logError: function ( error, context ) {
+			var id = mw.errorLogging.getId( error ),
+				data = { exception: error, source: 'logError', id: id };
+
+			if ( context ) {
+				data.context = context;
+			}
+
+			mw.track( 'errorLogging.exception', data );
+
+			return id;
+		},
+
+		/**
+		 * Generate an error id that can be shown to the user / logged to the console and
+		 * used to correlate bug reports with error logs.
+		 * @private
+		 * @param {Error|Mixed} error An error value, typically an Error object.
+		 * @return {string} The error id.
+		 */
+		getId: function ( error ) {
+			// We take the easy path and just generate a UUID, this way we don't have to deal
+			// with browser and language dependency of the error details. Algorithm is from
+			// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/2117523#2117523
+			// jshint unused:false, bitwise: false
+			return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace( /[xy]/g, function ( c ) {
+				var r = Math.random() * 16 | 0,
+					v = c === 'x' ? r : ( r & 0x3 | 0x8 );
+				return v.toString( 16 );
+			} );
+		},
+
+		/**
 		 * Dumb window.onerror handler which forwards the errors via mw.track.
+		 * @private
 		 * @param {Mixed...} args Arguments passed to window.onerror. The number of the arguments
 		 *   varies from browser to browser; dealing with that is left to subscribers of this event.
 		 * @fires errorLogging_windowOnerror
 		 */
 		handleWindowOnerror: function ( args ) {
-			args = [].slice.call( arguments );
+			var error, id;
 
-			mw.track( 'errorLogging.windowOnerror', { args: args } );
+			args = [].slice.call( arguments );
+			error = args[4];
+			id = mw.errorLogging.getId( error );
+
+			mw.track( 'errorLogging.windowOnerror', { args: args, id: id } );
 
 			// window.onerror works the opposite way than normal event handlers: returning true
 			// will prevent the default action, returning false will let the browser handle the
