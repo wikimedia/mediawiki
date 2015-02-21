08fe33383af3d7bbd4d7088693939c0a8b299d1b
Gergő Tisza <gtisza@wikimedia.org>
2015-02-21 02:53:06 +0000

    mediawiki.errorLogger: Add error id and manual reporting
    Adds an id property to events fired by mw.errorLogger. The id can
    be communicated to the user and it can be used to identify which error
    logs are related to a report.
    
    Also adds mw.errorLogger.logError which can be used to manually
    report errors which have been handled by the application.
    
    Id generation is a naive UUID algorithm. This might need to be
    rethought later when scaling up the error reporting service to
    high traffic levels: if the server does not have the throughput
    or storage to keep every id, the id needs to be the same for
    the same error, such as a hash of the stack trace. We'll cross
    that bridge when we get there.
    
    Also fixed "global.error" event documentation to reflect that
    the different data points are passed as properties of one object,
    not as separate parameters.
    
    Bug: T89384
    Change-Id: Ic6defcd3206a2155c3d1d3ecf6bab287a1e73cd5

--- tests/qunit/suites/resources/mediawiki/mediawiki.errorLogger.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.errorLogger.test.js
@@ -1,6 +1,30 @@
 ( function ( $, mw ) {
 	QUnit.module( 'mediawiki.errorLogger', QUnit.newMwEnvironment() );
 
+	QUnit.test( 'getId', 2, function ( assert ) {
+		var id = mw.errorLogger.getId();
+		assert.strictEqual( typeof id, 'string', 'Return type' );
+		assert.ok( id.length > 0, 'Non-empty' );
+	} );
+
+	QUnit.test( 'logError', 2, function () {
+		var error = new Error(),
+			context = {};
+
+		this.sandbox.stub( mw, 'track' );
+		mw.errorLogger.logError( error );
+		sinon.assert.calledWith( mw.track, 'errorLogger.exception',
+			sinon.match( { exception: error, id: sinon.match.defined } )
+		);
+
+		mw.track.reset();
+		mw.errorLogger.logError( error, context );
+		sinon.assert.calledWith( mw.track, 'errorLogger.exception',
+			sinon.match( { exception: error, id: sinon.match.defined, context: context } )
+		);
+
+	} );
+
 	QUnit.test( 'installGlobalHandler', 7, function ( assert ) {
 		var w = {},
 			errorMessage = 'Foo',
@@ -18,13 +42,16 @@
 		assert.strictEqual( w.onerror( errorMessage, errorUrl, errorLine ), false,
 			'Global handler returns false when there is no previous handler' );
 		sinon.assert.calledWithExactly( mw.track, 'global.error',
-			sinon.match( { errorMessage: errorMessage, url: errorUrl, lineNumber: errorLine } ) );
+			sinon.match( { errorMessage: errorMessage, url: errorUrl, lineNumber: errorLine, id: sinon.match.defined } )
+		);
 
 		mw.track.reset();
 		w.onerror( errorMessage, errorUrl, errorLine, errorColumn, errorObject );
 		sinon.assert.calledWithExactly( mw.track, 'global.error',
 			sinon.match( { errorMessage: errorMessage, url: errorUrl, lineNumber: errorLine,
-			columnNumber: errorColumn, errorObject: errorObject } ) );
+				columnNumber: errorColumn, errorObject: errorObject, id: sinon.match.defined
+			} )
+		);
 
 		w = { onerror: oldHandler };
 
