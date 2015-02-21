e995280a8f23d7ccb4dcd32d094c9bdd90a8d14d
Gergő Tisza <gtisza@wikimedia.org>
2015-02-21 02:53:06 +0000

    mediawiki.errorLogger: Add error id and manual reporting
    Adds an id property to events fired by mw.errorLogger. The id can
    be communicated to the user and it can be used to identify which error
    logs are related to a report.
    
    Also adds mw.errorLogger.logError which can be used to manually
    report errors which have been handled by the application.
    
    Id generation is a naive UUID algorithm. This might need to be
    rethought later when scaling up the error reporting service to
    high traffic levels: if the server does not have the throughput
    or storage to keep every id, the id needs to be the same for
    the same error, such as a hash of the stack trace. We'll cross
    that bridge when we get there.
    
    Also fixed "global.error" event documentation to reflect that
    the different data points are passed as properties of one object,
    not as separate parameters.
    
    Bug: T89384
    Change-Id: Ic6defcd3206a2155c3d1d3ecf6bab287a1e73cd5

--- resources/src/mediawiki/mediawiki.errorLogger.js
+++ resources/src/mediawiki/mediawiki.errorLogger.js
@@ -13,14 +13,71 @@
 		 * window.onerror handler.
 		 *
 		 * @event global_error
-		 * @param {string} errorMessage Error errorMessage.
-		 * @param {string} url URL where error was raised.
-		 * @param {number} lineNumber Line number where error was raised.
-		 * @param {number} [columnNumber] Line number where error was raised. Not all browsers
+		 * @param {Object} data
+		 * @param {string} data.errorMessage Error message.
+		 * @param {string} data.id UUID for this error.
+		 * @param {string} data.url URL where error was raised.
+		 * @param {number} data.lineNumber Line number where error was raised.
+		 * @param {number} [data.columnNumber] Line number where error was raised. Not all browsers
 		 *   support this.
-		 * @param {Error|Mixed} [errorObject] The error object. Typically an instance of Error, but anything
+		 * @param {Error|Mixed} [data.errorObject] The error object. Typically an instance of Error, but anything
 		 *   (even a primitive value) passed to a throw clause will end up here.
 		 */
+
+		/**
+		 * Fired via mw.track when an error is manually logged (e.g. via mw.errorLogger#logError).
+		 *
+		 * @event errorLogger_exception
+		 * @param {Object} data
+		 * @param {string} data.id UUID for this error.
+		 * @param {string} data.source How the error was logged, e.g. "logError".
+		 * @param {Error|Mixed} data.exception The error object. Typically an instance of Error, but
+		 *  anything (even a primitive value) may be used.
+		 * @param {Object} [data.context] Additional information.
+		 */
+
+		/**
+		 * Report an error and return an id with which it can be referenced.
+		 *
+		 * Note that error logging is an asynchronous operation on multiple levels, and because
+		 * of that returning an error id does not guarantee that the error was successfully logged.
+		 *
+		 * @param {Error|Mixed} error The error that needs to be logged. Typically an Error object
+		 *   that was thrown and caught, but could be a simple string or anything else.
+		 * @param {Object} [context] Additional information about the error.
+		 * @return {string} The generated id associated with this error.
+		 * @fires errorLogger_exception
+		 */
+		logError: function ( error, context ) {
+			var id = mw.errorLogger.getId(),
+				data = { exception: error, source: 'logError', id: id };
+
+			if ( context ) {
+				data.context = context;
+			}
+
+			mw.track( 'errorLogger.exception', data );
+
+			return id;
+		},
+
+		/**
+		 * Generate an error id that can be shown to the user / logged to the console and
+		 * used to correlate bug reports with error logs.
+		 * @private
+		 * @return {string} The error id.
+		 */
+		getId: function () {
+			// We take the easy path and just generate a UUID, this way we don't have to deal
+			// with browser and language dependency of the error details.
+			// Algorithm is from http://stackoverflow.com/a/2117523/319266
+			return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace( /[xy]/g, function ( c ) {
+				/*jshint bitwise:false */
+				var r = Math.random() * 16 | 0,
+					v = c === 'x' ? r : ( r & 0x3 | 0x8 );
+				return v.toString( 16 );
+			} );
+		},
 
 		/**
 		 * Install a window.onerror handler that will report via mw.track, while preserving
@@ -41,8 +98,14 @@
 			 * @fires global_error
 			 */
 			window.onerror = function ( errorMessage, url, lineNumber, columnNumber, errorObject ) {
-				mw.track( 'global.error', { errorMessage: errorMessage, url: url,
-					lineNumber: lineNumber, columnNumber: columnNumber, errorObject: errorObject } );
+				mw.track( 'global.error', {
+					errorMessage: errorMessage,
+					url: url,
+					lineNumber: lineNumber,
+					columnNumber: columnNumber,
+					errorObject: errorObject,
+					id: mw.errorLogger.getId()
+				} );
 				return oldHandler.apply( this, arguments );
 			};
 		}
