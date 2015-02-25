ca602abe1c3447ba15c31a442def5b799dbebdb1
Gergő Tisza <gtisza@wikimedia.org>
2015-02-25 04:07:37 +0000

    Report uncaught errors via mw.track
    Adds a simple onerror handler that sends errors to mw.track as
    an errorLogging.windowOnerror event.
    
    Bug: T88874
    Change-Id: Ia898dabb7c5312d65ff702c15b1d36d2809b1041

--- resources/src/mediawiki/mediawiki.errorLogging.js
+++ resources/src/mediawiki/mediawiki.errorLogging.js
@@ -7,15 +7,27 @@
 ( function ( mw, $ ) {
 	'use strict';
 
+	var oldWindowOnerror;
+
 	mw.errorLogging = {
 		/**
-		 * Fired via mw.track when an error is not handled by local code and is caught by global
-		 * error logging.
+		 * Fired via mw.track when an error is not handled by local code and is caught by an
+		 * automatically added try..catch block.
 		 *
 		 * @event errorLogging_exception
 		 * @param {Error|Mixed} e The error that was thrown. Usually an Error object, but
 		 *   in Javascript any value can be used with 'throw' so no guarantees.
 		 * @param {string} source Name of the function which threw the error.
+		 */
+
+		/**
+		 * Fired via mw.track when an error is not handled by local code and is caught by the
+		 * window.onerror handler.
+		 *
+		 * @event errorLogging_windowOnerror
+		 * @param {Array} args The arguments with which window.onerror was called. These are brower-
+		 *   dependent; https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onerror
+		 *   has some documentation.
 		 */
 
 		/**
@@ -36,7 +48,20 @@
 				try {
 					return mw.errorLogging.safeApply( fn, this, arguments );
 				} catch ( e ) {
+					// Set a flag on the error to avoid re-reporting it when it hits window.onerror.
+					// We can't prevent double-reporting error which are not objects this way, but
+					// that's a rare enough case that we can just ignore it. We also can't prevent
+					// double-reporting in browsers which do not pass the error object to the
+					// global error handler, but there is not much we can do about that.
+					if ( e !== null && typeof e === 'object' || typeof e === 'function' ) {
+						e.mwErrorLoggingProcessed = true;
+					}
+
 					mw.track( 'errorLogging.exception', { exception: e, source: name } );
+
+					// Rethrow the exception. This will result in some duplicate reports via
+					// window.onerror but it will make sure debug tools of the browser (such as
+					// break on error) work nromally.
 					throw e;
 				}
 			};
@@ -93,17 +118,7 @@
 		 * @param {Object} window The window object
 		 * @param {Object} $ The jQuery object
 		 */
-		register: function ( window, $ ) {
-			var registerFlag = mw.config.get( 'wgRegisterJavascriptErrorLogging'),
-				samplingRate = parseInt( registerFlag, 10 );
-
-			// Only register if the feature flag is set to true or some integer N; in the latter
-			// case, only register for one in every N page load. The extra negation is there to
-			// deal with NaN.
-			if ( registerFlag !== true && ( !samplingRate || Math.random() > 1 / samplingRate ) ) {
-				return;
-			}
-
+		registerAsync: function ( window, $ ) {
 			$.each( ['setTimeout', 'setInterval'], function ( _, name ) {
 				window[name] = mw.errorLogging.decorateWithArgsCallback( window[name], function ( args ) {
 					if ( args[0] ) {
@@ -135,6 +150,65 @@
 				// emulate jQuery.proxy() behavior
 				wrappedHandler.guid = handler.guid = handler.guid || $.guid++;
 			} );
+		},
+
+		/**
+		 * Dumb window.onerror handler which forwards the errors via mw.track.
+		 * @param {Mixed...} args Arguments passed to window.onerror. The number of the arguments
+		 *   varies from browser to browser; dealing with that is left to subscribers of this event.
+		 * @fires errorLogging_windowOnerror
+		 */
+		handleWindowOnerror: function ( args ) {
+			args = [].slice.call( arguments );
+
+			// Report the error, unless it was already reported via errorLogging.exception
+			if ( !args[4] || !args[4].mwErrorLoggingProcessed ) {
+				mw.track( 'errorLogging.windowOnerror', { args: args } );
+			}
+
+			if ( oldWindowOnerror ) {
+				oldWindowOnerror.apply( window, args );
+			}
+
+			// Do not return true so the default error handler is invoked and logs to console.
+		},
+
+		/**
+		 * Register a window.onerror handler, preserving the previous handler
+		 * @param {Object} window
+		 */
+		registerOnerror: function ( window ) {
+			oldWindowOnerror = window.onerror;
+			window.onerror = mw.errorLogging.handleWindowOnerror;
+		},
+
+		/**
+		 * Restores the previous window.onerror handler
+		 * @param {Object} window
+		 */
+		unregisterOnerror: function ( window ) {
+			window.onerror = oldWindowOnerror;
+			oldWindowOnerror = undefined;
+		},
+
+		/**
+		 * Registers onerror and async error catching if they are enabled via a feature flag.
+		 * @param window
+		 * @param $
+		 */
+		register: function ( window, $ ) {
+			var registerFlag = mw.config.get( 'wgRegisterJavascriptErrorLogging'),
+				samplingRate = parseInt( registerFlag, 10 );
+
+			// Only register if the feature flag is set to true or some integer N; in the latter
+			// case, only register for one in every N page load. The extra negation is there to
+			// deal with NaN.
+			if ( registerFlag !== true && ( !samplingRate || Math.random() > 1 / samplingRate ) ) {
+				return;
+			}
+
+			mw.errorLogging.registerOnerror( window );
+			mw.errorLogging.registerAsync( window, $ );
 		}
 	};
 }( mediaWiki, jQuery ) );
