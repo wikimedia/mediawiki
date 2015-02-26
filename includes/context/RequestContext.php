0933c3ded1e3b634c6d14ac59cb3c4a6f56b33fc
Aaron Schulz <aschulz@wikimedia.org>
2015-02-26 13:27:05 -0800

    Clarified RequestContext doc comment
    Change-Id: Ibb2a30634b192f4bc77e0b6b1afebc65b2ed3ec9

--- includes/context/RequestContext.php
+++ includes/context/RequestContext.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Request-dependant objects containers.
+ * Container for request-dependant objects and site configuration
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
