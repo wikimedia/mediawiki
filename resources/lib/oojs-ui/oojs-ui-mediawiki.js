df426eeb1cf240ab33e3d1575770dd9ddc306cf0
James D. Forrester <jforrester@wikimedia.org>
2015-02-25 18:11:47 -0800

    Update OOjs UI to v0.8.1
    Release notes:
     https://git.wikimedia.org/blob/oojs%2Fui.git/v0.8.1/History.md
    
    Change-Id: I5ad8d6aac0fb4ef146ef4f36459e4b0e398a66e8

--- resources/lib/oojs-ui/oojs-ui-mediawiki.js
+++ resources/lib/oojs-ui/oojs-ui-mediawiki.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.8.0
+ * OOjs UI v0.8.1
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2015 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2015-02-19T01:33:11Z
+ * Date: 2015-02-26T02:10:32Z
  */
 /**
  * @class
