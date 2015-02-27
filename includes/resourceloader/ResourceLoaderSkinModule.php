d1667eda2eaf99ffdb6b985a2133e3b535d38077
paladox <thomasmulhall410@yahoo.com>
2015-02-27 20:03:25 +0000

    Add support for svg with png fallback for logo
    * Supports svg with fallback to png in logo
    
    Adds new $wgLogoSVG setting to allow $wgLogo for png fallback.
    
    If you want to continue using $wgLogo as normal you can.
    
    and example on how to do it would be like
    
    $wgLogo = "path/to/png";
    $wgLogoSVG = "path/to/svg";
    
    Note: I have tested this to see if it works it does.
    
    Bug: T86229
    Change-Id: I6197d96ce9110f4711ef2c4b198445bc5c6ae110

--- includes/resourceloader/ResourceLoaderSkinModule.php
+++ includes/resourceloader/ResourceLoaderSkinModule.php
@@ -31,13 +31,24 @@ class ResourceLoaderSkinModule extends ResourceLoaderFileModule {
 	 */
 	public function getStyles( ResourceLoaderContext $context ) {
 		$logo = $this->getConfig()->get( 'Logo' );
+		$logoSVG = $this->getConfig()->get( 'LogoSVG' );
 		$styles = parent::getStyles( $context );
-		$styles['all'][] = '.mw-wiki-logo { background-image: ' .
-			CSSMin::buildUrlValue( $logo ) .
-			'; }';
-
+		if ( $logoSVG ) {
+			$styles['all'][] = '.mw-wiki-logo { ' .
+				'background-image: ' .
+					CSSMin::buildUrlValue( $logo ) . '; ' .
+				'background-image: -webkit-linear-gradient(transparent, transparent), ' .
+					CSSMin::buildUrlValue( $logoSVG ) . '; ' .
+				'background-image: linear-gradient( transparent, transparent), ' . 
+					CSSMin::buildUrlValue( $logoSVG ) . '; }';
+		} else {
+			$styles['all'][] = '.mw-wiki-logo { ' .
+				'background-image: ' .
+					CSSMin::buildUrlValue( $logo ) . '; }';
+		}
 		return $styles;
 	}
+
 
 	/**
 	 * @param $context ResourceLoaderContext
@@ -64,6 +75,7 @@ class ResourceLoaderSkinModule extends ResourceLoaderFileModule {
 	 */
 	public function getModifiedHash( ResourceLoaderContext $context ) {
 		$logo = $this->getConfig()->get( 'Logo' );
-		return md5( parent::getModifiedHash( $context ) . $logo );
+		$logoSVG = $this->getConfig()->get( 'LogoSVG' );
+		return md5( parent::getModifiedHash( $context ) . $logo . json_encode( $logoSVG ) );
 	}
 }
