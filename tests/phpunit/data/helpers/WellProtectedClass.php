7a9efba1d3d8ceedb457daa90362d45fd42019b8
Adam Roses Wight <awight@wikimedia.org>
2015-02-27 09:22:16 -0800

    New testing wrapper to circumvent object access
    The new TestingAccessWrapper class provides a convenient way to make
    all of an object's methods and properties public.
    
    TODO: We should organize test helpers into a source directory.  Note that the
    helper and its test are in the same directory.
    
    Change-Id: I958d55df18c74e9d2b25d98cd0316989a0fbbe6f

---
+++ tests/phpunit/data/helpers/WellProtectedClass.php
@@ -0,0 +1,17 @@
+<?php
+
+class WellProtectedClass {
+	protected $property;
+
+	public function __construct() {
+		$this->property = 1;
+	}
+
+	protected function incrementPropertyValue() {
+		$this->property++;
+	}
+
+	public function getProperty() {
+		return $this->property;
+	}
+}
