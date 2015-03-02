8e343c978054b5407ab45480bcca2a732d3ed206
Federico Leva <federicoleva@tiscali.it>
2015-03-02 17:16:58 +0100

    Attempt to count actual watchers in the info action
    Proposed threshold to be considered an "active" watcher:
    two times $wgRCMaxAge, configurable with the new
    configuration setting $wgWatchersMaxAge.
    
    The information is not displayed when the number given would
    be 1 or 0, so that the number (or absence thereof) doesn't
    "disclose" that the page is (potentially) unpatrolled or
    completely unwatched and hence easier to vandalise.
    Also, we don't display this row at all when the user doesn't
    have the right to see the count of total watchers.
    
    Bug: T51506
    Change-Id: I10d294a339b131eee94839ed7088ab20d746d881

--- includes/actions/InfoAction.php
+++ includes/actions/InfoAction.php
@@ -325,8 +325,22 @@ class InfoAction extends FormlessAction {
 		) {
 			// Number of page watchers
 			$pageInfo['header-basic'][] = array(
-				$this->msg( 'pageinfo-watchers' ), $lang->formatNum( $pageCounts['watchers'] )
+				$this->msg( 'pageinfo-watchers' ),
+				$lang->formatNum( $pageCounts['watchers'] )
 			);
+			if ( $config->get( 'ShowUpdatedMarker' ) {
+				if ( $pageCounts['visitingWatchers'] > 1 ) {
+					$pageInfo['header-basic'][] = array(
+						$this->msg( 'pageinfo-visiting-watchers' ),
+						$lang->formatNum( $pageCounts['visitingWatchers'] )
+					);
+				} else {
+					$pageInfo['header-basic'][] = array(
+						$this->msg( 'pageinfo-visiting-watchers' ),
+						$this->msg( 'pageinfo-few-visiting-watchers' )
+					);
+				}
+			}
 		} elseif ( $unwatchedPageThreshold !== false ) {
 			$pageInfo['header-basic'][] = array(
 				$this->msg( 'pageinfo-watchers' ),
@@ -669,6 +683,26 @@ class InfoAction extends FormlessAction {
 			__METHOD__
 		);
 		$result['watchers'] = $watchers;
+
+		if ( $config->get( 'ShowUpdatedMarker' ) ) {
+			// Threshold: last visited about 26 weeks before latest edit
+			$updated = wfTimestamp( TS_UNIX, $this->page->getTimestamp() );
+			$age = $config->get( 'WatchersMaxAge' );
+			$threshold = $dbr->timestamp( $updated - $age );
+			// Number of page watchers who also visited a "recent" edit
+			$visitingWatchers = (int)$dbr->selectField(
+				'watchlist',
+				'COUNT(*)',
+				array(
+					'wl_namespace' => $title->getNamespace(),
+					'wl_title' => $title->getDBkey(),
+					'wl_notificationtimestamp >= ' . $dbr->addQuotes( $threshold ) .
+					' OR wl_notificationtimestamp IS NULL'
+				),
+				__METHOD__
+			);
+			$result['visitingWatchers'] = $visitingWatchers;
+		}
 
 		// Total number of edits
 		$edits = (int)$dbr->selectField(
