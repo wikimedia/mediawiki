a0d422e67f6facfc36714d25102956112b345b3d
Kunal Mehta <legoktm@gmail.com>
2015-03-03 23:51:44 -0800

    maintenance: Defer initilization of 'main' Config until after Setup.php runs
    Bug: T90680
    Change-Id: I72d6306404bc1f6c7a605853130a026e9858b493

--- maintenance/doMaintenance.php
+++ maintenance/doMaintenance.php
@@ -92,10 +92,12 @@ if ( $maintenance->getDbType() === Maintenance::DB_NONE ) {
 	}
 }
 
-$maintenance->setConfig( ConfigFactory::getDefaultInstance()->makeConfig( 'main' ) );
 $maintenance->finalSetup();
 // Some last includes
 require_once "$IP/includes/Setup.php";
+
+// Initialize main config instance
+$maintenance->setConfig( ConfigFactory::getDefaultInstance()->makeConfig( 'main' ) );
 
 // Do the work
 $maintenance->execute();
