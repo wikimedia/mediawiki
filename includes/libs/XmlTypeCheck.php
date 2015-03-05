7938a50ffdcf861cf96d49ffbda948746dac9635
Ubuntu <ubuntu@ip-172-31-39-38.us-west-2.compute.internal>
2015-03-05 08:02:34 +0000

    SVG upload with specific error (warning) message when blocking
    This patch is to generate specific error (warning) message when
    blocking an svg file.
    
    The checkSvgScriptCallback function has been updated, and it's
    return type is changed from boolean to array.
    
    A new variable is added to XmlTypeCheck class that contains the
    type of error when svg file is uploaded, which is used to generate
    concrete error messages later on.
    
    I have added concrete error messages to i18n/en.json and their description
    to qqq.json file. Please review the error messages and their description.
    
    Bug: T85924
    Change-Id: I3f687bf5b86ce66b703591b85fd03f073aacff4f

--- includes/libs/XmlTypeCheck.php
+++ includes/libs/XmlTypeCheck.php
@@ -39,6 +39,13 @@ class XmlTypeCheck {
 	public $filterMatch = false;
 
 	/**
+	 * Will contain the type of filter hit if the optional element filter returned
+	 * a match at some point.
+	 * @var mixed
+	 */
+	public $filterMatchType = false;
+
+	/**
 	 * Name of the document's root element, including any namespace
 	 * as an expanded URL.
 	 */
@@ -173,7 +180,7 @@ class XmlTypeCheck {
 		// First, move through anything that isn't an element, and
 		// handle any processing instructions with the callback
 		do {
-			if( !$this->readNext( $reader ) ) {
+			if ( !$this->readNext( $reader ) ) {
 				// Hit the end of the document before any elements
 				$this->wellFormed = false;
 				return;
@@ -294,17 +301,20 @@ class XmlTypeCheck {
 		list( $name, $attribs ) = array_pop( $this->elementDataContext );
 		$data = array_pop( $this->elementData );
 		$this->stackDepth--;
+		$callbackReturn = false;
 
-		if ( is_callable( $this->filterCallback )
-			&& call_user_func(
+		if ( is_callable( $this->filterCallback ) ) {
+			$callbackReturn = call_user_func(
 				$this->filterCallback,
 				$name,
 				$attribs,
 				$data
-			)
-		) {
-			// Filter hit
+			);
+		}
+		if ( $callbackReturn ) {
+			// Filter hit!
 			$this->filterMatch = true;
+			$this->filterMatchType = $callbackReturn;
 		}
 	}
 
@@ -321,15 +331,18 @@ class XmlTypeCheck {
 	 * @param $data
 	 */
 	private function processingInstructionHandler( $target, $data ) {
+		$callbackReturn = false;
 		if ( $this->parserOptions['processing_instruction_handler'] ) {
-			if ( call_user_func(
+			$callbackReturn = call_user_func(
 				$this->parserOptions['processing_instruction_handler'],
 				$target,
 				$data
-			) ) {
-				// Filter hit!
-				$this->filterMatch = true;
-			}
+			);
+		}
+		if ( $callbackReturn ) {
+			// Filter hit!
+			$this->filterMatch = true;
+			$this->filterMatchType = $callbackReturn;
 		}
 	}
 }
