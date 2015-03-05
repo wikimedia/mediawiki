9f63e1717dc169c85315dabdb14ab501bb94f76e
Željko Filipin <zeljko.filipin@gmail.com>
2015-03-05 14:52:20 +0100

    Changed default MEDIAWIKI_URL to one MediaWiki-Vagrant uses
    It makes it easy to just run the Selenium tests without changing
    MEDIAWIKI_URL environment variable.
    
    Change-Id: Ia41f3be33bc8e08b52d76fe701f2311d4fc12037

--- tests/browser/features/support/modules/url_module.rb
+++ tests/browser/features/support/modules/url_module.rb
@@ -3,7 +3,7 @@ module URL
     if ENV["MEDIAWIKI_URL"]
       mediawiki_url = ENV["MEDIAWIKI_URL"]
     else
-      mediawiki_url = "http://127.0.0.1:80/w/index.php"
+      mediawiki_url = "http://127.0.0.1:8080/wiki/"
     end
     "#{mediawiki_url}#{name}"
   end
