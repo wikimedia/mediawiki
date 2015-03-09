b1f5b7c16487b259f9fe0aa4c5ebc7e632710917
Ritsyy <richarupela95@gmail.com>
2015-03-10 02:41:32 +0530

    Umlauts are displayed fully colored within a internal link.
    Bug: T64998
    Change-Id: If939265589cda9cc6fcedfdcb1de09d8bc747139

--- languages/messages/MessagesEn.php
+++ languages/messages/MessagesEn.php
@@ -500,7 +500,7 @@ $specialPageAliases = array(
  * Regular expression matching the "link trail", e.g. "ed" in [[Toast]]ed, as
  * the first group, and the remainder of the string as the second group.
  */
-$linkTrail = '/^([a-z]+)(.*)$/sD';
+   $linkTrail =  '/^([a-z\x{E0}-\x{FF}]+)(.*)$/sDu';
 
 /**
  * Regular expression charset matching the "link prefix", e.g. "foo" in
