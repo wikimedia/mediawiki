8ca2b9d412733789ede5811604505f0bf62a1e7f
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-03-12 22:28:28 +0100

    LivePreview: Support section=new for preview
    Reproduce the new section behavior for the full submit preview.
    Diffing will require a bit more additional work in Api.
    
    Bug: T84877
    Change-Id: Iec71fdc7ba09f91683214f3d65472ea2f72a06b1

--- resources/src/mediawiki.action/mediawiki.action.edit.preview.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.preview.js
@@ -11,13 +11,17 @@
 		var isDiff, api, request, postData, copySelectors, section,
 			$wikiPreview, $wikiDiff, $editform, $copyElements, $spinner;
 
-		e.preventDefault();
-
 		isDiff = ( e.target.name === 'wpDiff' );
 		$wikiPreview = $( '#wikiPreview' );
 		$wikiDiff = $( '#wikiDiff' );
 		$editform = $( '#editform' );
 		section = $editform.find( '[name="wpSection"]' ).val();
+
+		// Show changes for a new section is not yet supported
+		if ( isDiff && section === 'new' ) {
+			return;
+		}
+		e.preventDefault();
 
 		// Show #wikiPreview if it's hidden to be able to scroll to it
 		// (if it is hidden, it's also empty, so nothing changes in the rendering)
@@ -65,6 +69,14 @@
 			summary: $editform.find( '#wpSummary' ).textSelection( 'getContents' )
 		};
 
+		if ( section !== '' ) {
+			postData.sectionpreview = '';
+			postData.section = section;
+			if ( section === 'new' ) {
+				postData.sectiontitle = postData.summary;
+			}
+		}
+
 		if ( isDiff ) {
 			$wikiPreview.hide();
 
@@ -104,9 +116,6 @@
 				prop: 'text|displaytitle|modules|categorieshtml|templates|langlinks|limitreporthtml',
 				disableeditsection: true
 			} );
-			if ( section !== '' ) {
-				postData.sectionpreview = '';
-			}
 			request = api.post( postData );
 			request.done( function ( response ) {
 				var li, newList, $content, $parent, $list;
@@ -179,8 +188,7 @@
 			} );
 		}
 		request.done( function ( response ) {
-			if ( response.parse.parsedsummary ) {
-				// TODO implement special behavior for section === 'new'
+			if ( response.parse.parsedsummary && section !== 'new' ) {
 				$editform.find( '.mw-summary-preview' )
 					.empty()
 					.append(
