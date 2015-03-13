98ef0a2f5d407f570e84ae1b59dd595d5105edd1
Bartosz Dziewoński <matma.rex@gmail.com>
2015-03-13 20:22:02 +0100

    Remove a hack, and a hack for the hack, for MediaWiki UI input fields
    Hack #1: We were ignoring the 'size' attribute of input fields when
      $wgUseMediaWikiUIEverywhere was true. Let's not do that.
    
    Hack #2: We were setting a min-width for MediaWiki UI input fields,
      because fields which were supposed to be full-line were becoming
      tiny because of hack #1. Let's not do that either.
    
    Bug: T92498
    Change-Id: I1d2c6c9eb60b52a7267c122a719cfdaa1f74f815

--- includes/Html.php
+++ includes/Html.php
@@ -151,10 +151,6 @@ class Html {
 			} else {
 				$attrs['class'] = 'mw-ui-input';
 			}
-
-			// Note that size can effect the desired width rendering of mw-ui-input elements
-			// so it is removed. Left intact when mediawiki ui not enabled.
-			unset( $attrs['size'] );
 		}
 		return $attrs;
 	}
