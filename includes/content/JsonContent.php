b9829c2fea18edb257ad1c1ed7fce046427c310c
Bartosz Dziewoński <matma.rex@gmail.com>
2015-03-16 21:53:33 +0100

    JsonContent: Pass FormatJson::UTF8_OK when beautifying JSON
    Currently it is not beautiful at all.
    
    Change-Id: I82a07238b4213212b8e4a7150e35486f6dc90bc6
    (cherry picked from commit d7389bd865405a77efc2d75f7388b9488a5d0b3f)

--- includes/content/JsonContent.php
+++ includes/content/JsonContent.php
@@ -68,7 +68,7 @@ class JsonContent extends TextContent {
 	 * @return string
 	 */
 	public function beautifyJSON() {
-		return FormatJson::encode( $this->getData()->getValue(), true );
+		return FormatJson::encode( $this->getData()->getValue(), true, FormatJson::UTF8_OK );
 	}
 
 	/**
