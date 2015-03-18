97b06ddd7c815d38cda69b9770b7ae1021f7dc22
Željko Filipin <zeljko.filipin@gmail.com>
2015-03-18 18:52:02 +0100

    Fixed Style/PerlBackrefs RuboCop offense
    Refactored to use `Regexp#match`, avoiding use of match globals
    entirely.
    
    Bug: T91485
    Change-Id: I4b1a40e153c18d92ef9e78291bc4e14781cc6978

--- maintenance/jsduck/custom_tags.rb
+++ maintenance/jsduck/custom_tags.rb
@@ -72,9 +72,11 @@ class SeeTag < CommonTag
   end
 
   def render_long_see(tag, formatter, position)
-    if tag =~ /\A([^\s]+)( .*)?\Z/m
-      name = $1
-      doc = $2 ? ': ' + $2 : ''
+    match = /\A([^\s]+)( .*)?\Z/m.match(tag)
+
+    if match
+      name = match[1]
+      doc = match[2] ? ': ' + match[2] : ''
       return formatter.format("{@link #{name}} #{doc}")
     else
       JsDuck::Logger.warn(nil, 'Unexpected @see argument: "' + tag + '"', position)
@@ -105,8 +107,10 @@ class ContextTag < CommonTag
   end
 
   def render_long_context(tag, formatter, position)
-    if tag =~ /\A([^\s]+)/m
-      name = $1
+    match = /\A([^\s]+)/m.match(tag)
+
+    if match
+      name = match[1]
       return formatter.format("`context` : {@link #{name}}")
     else
       JsDuck::Logger.warn(nil, 'Unexpected @context argument: "' + tag + '"', position)
