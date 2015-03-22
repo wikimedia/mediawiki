17de7d40a46ec5e0f02cc02874252d10bc822176
Timo Tijhof <krinklemail@gmail.com>
2015-03-22 22:01:52 +0000

    TemplateParser: Don't fatal on cache misses
    Also add regression test, and coverage for more methods.
    
    Was trying to eval the code which had the hmac integrity check in front of it,
    which causes a syntax error in valid PHP code.
    
    Follows-up db1866da4, 50c50bea2e.
    
    Bug: T93436
    Bug: T93511
    Change-Id: Ie90074e4885de7340e53f59fdd479f5384b5eac6
    (cherry-picked from commit cfcaa33fcc424cd85444d71f10f6ce7cc2fb1e58)

--- tests/phpunit/includes/TemplateParserTest.php
+++ tests/phpunit/includes/TemplateParserTest.php
@@ -4,6 +4,58 @@
  * @group Templates
  */
 class TemplateParserTest extends MediaWikiTestCase {
+
+	protected $templateDir;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->setMwGlobals( array(
+			'wgSecretKey' => 'foo',
+			'wgMemc' => new EmptyBagOStuff(),
+		) );
+
+		$this->templateDir = dirname( __DIR__ ) . '/data/templates/';
+	}
+
+	/**
+	 * @covers TemplateParser::getTemplateFilename
+	 * @dataProvider provideGetTemplateFilename
+	 */
+	public function testGetTemplateFilename( $dir, $name, $result, $exception = false ) {
+		if ( $exception ) {
+			$this->setExpectedException( $exception );
+		}
+
+		$tp = new TemplateParser( $dir );
+		$path = $tp->getTemplateFilename( $name );
+		$this->assertEquals( $result, $path );
+	}
+
+	public static function provideGetTemplateFilename() {
+		return array(
+			array(
+				'dir/templates',
+				'foobar',
+				'dir/templates/foobar.mustache',
+			),
+			array(
+				'dir/templates',
+				'../foobar',
+				'',
+				'Exception'
+			),
+		);
+	}
+
+	/**
+	 * @covers TemplateParser::getTemplate
+	 */
+	public function testGetTemplate() {
+		$tp = new TemplateParser( $this->templateDir );
+		$this->assertTrue( is_callable( $tp->getTemplate( 'foobar' ) ) );
+	}
+
 	/**
 	 * @covers TemplateParser::compile
 	 */
