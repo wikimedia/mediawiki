743b178c20e7540604ac96eda8b5099472c8c815
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2015-03-23 15:34:00 +0530

    Generate hreflang tags in <head> of article pages
    Improved version of Stu's proof of concept
    
    Bug: T93213
    Change-Id: I30a6af657a11bf822736d667a82b21e813f0db4e

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -3515,6 +3515,17 @@ class OutputPage extends ContextSource {
 			) );
 		}
 
+		# Generate hreflang tags
+		$languageLinks = $this->getLanguageLinks();
+		foreach ( $languageLinks as $languageLinkText ) {
+			$languageLinkTitle = Title::newFromText( $languageLinkText );
+			$tags[] = Html::element( 'link', array(
+				'rel' => 'alternate',
+				'hreflang' => $languageLinkTitle->getInterwiki(),
+				'href' => $languageLinkTitle->getFullURL()
+			) );
+		}
+
 		return $tags;
 	}
 
