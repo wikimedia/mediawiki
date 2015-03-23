81dcc7206da0620d6da0020738e523e80b33e1ae
Baha Man <bmansurov@wikimedia.org>
2015-03-23 19:11:21 -0400

    Track search events
    Tracking works only for the skin-provided search box (top right search
    box on desktop). Also, add the 'update' option the jQuery suggestions
    extension.
    
    Change-Id: Icc0afcd6c9fa8c82a95d9decf988a19ae3edc165

--- resources/src/mediawiki/mediawiki.searchSuggest.js
+++ resources/src/mediawiki/mediawiki.searchSuggest.js
@@ -3,7 +3,7 @@
  */
 ( function ( mw, $ ) {
 	$( function () {
-		var api, map, resultRenderCache, searchboxesSelectors,
+		var api, map, resultRenderCache, searchboxesSelectors, isSampled, previousSearchText,
 			// Region where the suggestions box will appear directly below
 			// (using the same width). Can be a container element or the input
 			// itself, depending on what suits best in the environment.
@@ -12,7 +12,9 @@
 			// element (not the search form, as that would leave the buttons
 			// vertically between the input and the suggestions).
 			$searchRegion = $( '#simpleSearch, #searchInput' ).first(),
-			$searchInput = $( '#searchInput' );
+			$searchInput = $( '#searchInput' ),
+			updateCallbacks = {},
+			isLoggingEnabled = mw.config.get( 'wgCirrusSearchEnableSearchLogging' );
 
 		// Compatibility map
 		map = {
@@ -50,6 +52,50 @@
 			};
 		}
 
+		/**
+		 * Track an event
+		 * @param {Object} data
+		 * @ignore
+		 */
+		function trackEvent( data ) {
+			if ( isLoggingEnabled ) {
+				mw.track( 'mediawiki.searchSuggest',  data );
+			}
+		}
+
+		/**
+		 * Callback that's run when the user changes the search input text
+		 * 'this' is the search input box
+		 * @ignore
+		 */
+		function onBeforeUpdate() {
+			var $this = $( this );
+
+			if ( $this.val() && $this.val() !== previousSearchText ) {
+				trackEvent( {
+					action: 'session-start'
+				} );
+			}
+			previousSearchText = $searchInput.val();
+		}
+
+		/**
+		 * Callback that's run when suggestions have been updated either from the cache or the API
+		 * 'this' is the search input box
+		 * @ignore
+		 */
+		function onAfterUpdate() {
+			var context = this.data( 'suggestionsContext' );
+
+			trackEvent( {
+				action: 'impression-results',
+				numberOfResults: context.config.suggestions.length,
+				// FIXME: when other types of search become available change this value accordingly
+				// See the API call below (opensearch = prefix)
+				resultSetType: 'prefix'
+			} );
+		}
+
 		// The function used to render the suggestions.
 		function renderFunction( text, context ) {
 			if ( !resultRenderCache ) {
@@ -67,6 +113,27 @@
 						.attr( 'title', text )
 						.addClass( 'mw-searchSuggest-link' )
 				);
+		}
+
+		// The function used when the user makes a selection
+		function selectFunction( $input ) {
+			var context,
+				text;
+
+			if ( isLoggingEnabled ) {
+				context = $input.data( 'suggestionsContext' );
+				text = $input.val();
+
+				trackEvent( {
+					action: 'click-result',
+					numberOfResults: context.config.suggestions.length,
+					clickIndex: context.config.suggestions.indexOf( text ) + 1
+				} );
+			}
+
+			// allow the form to be submitted
+			return true;
+
 		}
 
 		function specialRenderFunction( query, context ) {
@@ -104,6 +171,35 @@
 						.addClass( 'mw-searchSuggest-link' )
 				);
 			}
+		}
+
+		// Sample users for event logging.
+		// 1 in a 1000 users is given a golden opportunity to have their searches logged.
+		if ( isLoggingEnabled ) {
+			isSampled = $.cookie( 'isSearchLoggingEnabledForUser' );
+			if ( isSampled === null ) {
+				// 1 in a 1000 using a session cookie
+				isSampled = Math.round( Math.random() * 1000 ) === 42;
+				if ( !isSampled ) {
+					isLoggingEnabled = false;
+				}
+				$.cookie( 'isSearchLoggingEnabledForUser', isSampled );
+			} else if ( isSampled === 'false' ) {
+				isLoggingEnabled = false;
+			}
+		}
+
+		// Load the logging code if the user is in the sample
+		// Also listen to some events
+		if ( isLoggingEnabled ) {
+			mw.loader.using( 'ext.cirrusSearch.loggingSchema', function () {}, function () {
+				isLoggingEnabled = false;
+			} );
+			updateCallbacks = {
+				before: onBeforeUpdate,
+				after: onAfterUpdate
+			};
+			previousSearchText = $searchInput.val();
 		}
 
 		// Generic suggestions functionality for all search boxes
@@ -177,8 +273,13 @@
 			return;
 		}
 
-		// Special suggestions functionality for skin-provided search box
+		// Special suggestions functionality and tracking for skin-provided search box
 		$searchInput.suggestions( {
+			update: updateCallbacks,
+			result: {
+				render: renderFunction,
+				select: selectFunction
+			},
 			special: {
 				render: specialRenderFunction,
 				select: function ( $input ) {
