67dd9ca0c23f5a0a495e2a454423cf178083d8dc
Kunal Mehta <legoktm@gmail.com>
2015-03-27 20:47:19 -0700

    Use TransactionProfiler to find unit tests that cause database queries
    Change-Id: Iefef8c604d804696fdbeb60e9dfe5c95e1ef55c3

--- includes/profiler/TransactionProfiler.php
+++ includes/profiler/TransactionProfiler.php
@@ -78,6 +78,21 @@ class TransactionProfiler {
 	}
 
 	/**
+	 * Get the current hit count for a event
+	 *
+	 * @since 1.25
+	 * @param string $event
+	 * @throws InvalidArgumentException
+	 * @return array
+	 */
+	public function getHits( $event ) {
+		if ( !isset( $this->hits[$event] ) ) {
+			throw new InvalidArgumentException( "'$event' is not a valid event" );
+		}
+		return $this->hits[$event];
+	}
+
+	/**
 	 * Reset performance expectations and hit counters
 	 *
 	 * @since 1.25
