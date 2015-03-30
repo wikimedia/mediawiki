d03d2f66f8496f30018951f574449a3f8380b2b7
Antoine Musso <hashar@free.fr>
2015-03-30 23:56:44 +0200

    Browser test: avoid loading elements over network
    Page object let us search elements for which attributes match a given
    regex.  Unfortunatly it loads the list of elements first, then foreach
    of them trigger a call to Selenium to retrieve the attribute to match.
    
    Attempting to regex match a link href, would cause hundred of roundtrip
    to the Selenium host.
    
    Instead, use a CSS selector to get the element we are interested in
    which skips the bad page object code.
    
    Bug: T92613
    Change-Id: I12949a4d0ef61d38adbaaf622722c89033949cad

--- tests/browser/features/support/pages/view_history_page.rb
+++ tests/browser/features/support/pages/view_history_page.rb
@@ -1,6 +1,6 @@
 class ViewHistoryPage
   include PageObject
 
-  a(:view_history_link, href: /action=history/)
-  a(:old_version_link, href: /oldid=/)
+  a(:view_history_link, css: '#ca-history a')
+  a(:old_version_link, css: '#pagehistory a.mw-changeslist-date:first-child')
 end
