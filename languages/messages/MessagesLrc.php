21efb25d7cb80b49af2ae326b9319350f30143f9
mjbmr <mjbmri@gmail.com>
2015-03-30 00:27:07 +0000

    Update namespaces for Luri (lrc) from translatewiki
    Change-Id: Icec87b9d08e7dce35e361f6250b28106aa4811f7

--- languages/messages/MessagesLrc.php
+++ languages/messages/MessagesLrc.php
@@ -1,5 +1,5 @@
 <?php
-/** لوری (لوری)
+/** Luri (لوری)
  *
  * To improve a translation please visit https://translatewiki.net
  *
@@ -11,4 +11,24 @@
  */
 
 $rtl = true;
+$fallback8bitEncoding = 'windows-1256';
 
+$namespaceNames = array(
+	NS_MEDIA            => 'رسانه',
+	NS_SPECIAL          => 'ویجه',
+	NS_MAIN             => '',
+	NS_TALK             => 'چک_چنه',
+	NS_USER             => 'کارور',
+	NS_USER_TALK        => 'چک_چنه_کارور',
+	NS_PROJECT_TALK     => 'چک_چنه_$1',
+	NS_FILE             => 'جانیا',
+	NS_FILE_TALK        => 'جانیا_چک_چنه',
+	NS_MEDIAWIKI        => 'ویکی_رسانه',
+	NS_MEDIAWIKI_TALK   => 'چک_چنه_ویکی_رسانه',
+	NS_TEMPLATE         => 'قالو',
+	NS_TEMPLATE_TALK    => 'قالو_چک_چنه',
+	NS_HELP             => 'هومیاری',
+	NS_HELP_TALK        => 'چک_چنه_هومیاری',
+	NS_CATEGORY         => 'دسه',
+	NS_CATEGORY_TALK    => 'دسه_چک_چنه',
+);
