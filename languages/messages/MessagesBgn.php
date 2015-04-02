53e0a0046cd48f4a9682b75a171ae61b00cbd3ba
mjbmr <mjbmri@gmail.com>
2015-04-02 23:01:20 +0000

    Add namespaces for Western Balochi (bgn) from translatewiki
    Change-Id: I87e2842b3d204098768fb265b5e2f7bf715d918b

--- languages/messages/MessagesBgn.php
+++ languages/messages/MessagesBgn.php
@@ -10,3 +10,22 @@
 
 $fallback = 'fa';
 $rtl = true;
+
+$namespaceNames = array(
+	NS_MEDIA            => 'رسانگ',
+	NS_SPECIAL          => 'ۆێک',
+	NS_TALK             => 'گپ',
+	NS_USER             => 'کارمرزۆک',
+	NS_USER_TALK        => 'کارمرزۆک_ئی_گپ',
+	NS_PROJECT_TALK     => '$1_ئی_گپ',
+	NS_FILE             => 'ورق',
+	NS_FILE_TALK        => 'ورق_ئی_گپ',
+	NS_MEDIAWIKI        => 'ویکی_رسانگ',
+	NS_MEDIAWIKI_TALK   => 'ویکی_رسانگ_ئی_گپ',
+	NS_TEMPLATE         => 'تراشوان',
+	NS_TEMPLATE_TALK    => 'تراشوان_ئی_گپ',
+	NS_HELP             => 'کومک',
+	NS_HELP_TALK        => 'کومک_ئی_گپ',
+	NS_CATEGORY         => 'تهر',
+	NS_CATEGORY_TALK    => 'تهر_ئی_گپ',
+);
