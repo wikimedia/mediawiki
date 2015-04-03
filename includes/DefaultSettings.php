75f4db4d24610225ef661fc93b2324c9015328c7
This, that and the other <at.light@live.com.au>
2015-04-03 12:46:51 +1100

    Allow users to add, remove and apply change tags using the API
    You can add tags at the same time as performing action=edit, as long as you
    have the "applychangetags" right. Also, you can add or remove tags after
    the fact from revisions and log entries using the API action=tags.
    
    No UI is provided for either of these changes. The target audience is user
    scripts, gadgets and similar tools.
    
    Includes a new log parameter format type: "list", for a comma-separated
    list of values.
    
    Logging of change tag events is limited to those that do not accompany an
    edit (i.e. those done after the fact), and is hidden from Special:Log by
    default, similar to the patrol log.
    
    Bug: T20670
    Change-Id: I37275e0f73fa3127f55da0c320b892551b61ee80

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -4556,6 +4556,8 @@ $wgGroupPermissions['user']['reupload-shared'] = true;
 $wgGroupPermissions['user']['minoredit'] = true;
 $wgGroupPermissions['user']['purge'] = true; // can use ?action=purge without clicking "ok"
 $wgGroupPermissions['user']['sendemail'] = true;
+$wgGroupPermissions['user']['applychangetags'] = true;
+$wgGroupPermissions['user']['changetags'] = true;
 
 // Implicit group for accounts that pass $wgAutoConfirmAge
 $wgGroupPermissions['autoconfirmed']['autoconfirmed'] = true;
@@ -5036,7 +5038,11 @@ $wgRateLimits = array(
 		'newbie' => null,
 		'ip' => null,
 		'subnet' => null,
-	)
+	),
+	'changetag' => array( // adding or removing change tags
+		'user' => null,
+		'newbie' => null,
+	),
 );
 
 /**
@@ -6566,6 +6572,7 @@ $wgLogTypes = array(
 	'patrol',
 	'merge',
 	'suppress',
+	'tag',
 	'managetags',
 );
 
@@ -6603,7 +6610,8 @@ $wgLogRestrictions = array(
  * for the link text.
  */
 $wgFilterLogTypes = array(
-	'patrol' => true
+	'patrol' => true,
+	'tag' => true,
 );
 
 /**
@@ -6688,6 +6696,7 @@ $wgLogActionsHandlers = array(
 	'upload/overwrite' => 'LogFormatter',
 	'upload/revert' => 'LogFormatter',
 	'merge/merge' => 'MergeLogFormatter',
+	'tag/update' => 'TagLogFormatter',
 	'managetags/create' => 'LogFormatter',
 	'managetags/delete' => 'LogFormatter',
 	'managetags/activate' => 'LogFormatter',
