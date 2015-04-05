ef4cee1f58249d53003a120aa646518de48780f1
cenarium <cenarium.sysop@gmail.com>
2015-04-05 04:41:30 +0200

    Minimalist UI for RC patrol
    This adds a config variable $wgUseFullRCPatrolUI (true by default)
    that, when set to false, provides a minimalist UI for RC patrol
    (when enabled).
    In this minimalist UI, a revision is marked as unpatrolled or can
    be patrolled from the UI only when it is tagged with a problem tag.
    And the show/hide patrolled edits filter is shown in RecentChanges
    only when filtering for a problem tag.
    This depends on the fix for T91535.
    
    Bug: T89553
    Change-Id: Id3ceb62d1aa7b3ef0f9af072a313f1664cafe348

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5884,6 +5884,13 @@ $wgRCEngines = array(
 $wgUseRCPatrol = true;
 
 /**
+ * When using RC patrolling, set this to false
+ * if you want a minimalist RC patrol UI
+ * (no effect if RC patrolling is disabled)
+ */
+$wgUseFullRCPatrolUI = true;
+
+/**
  * Use new page patrolling to check new pages on Special:Newpages
  */
 $wgUseNPPatrol = true;
