e81b196a9dc2a2a0c2850e704e1cbacc66378e59
mjbmr <mjbmri@gmail.com>
2015-04-05 23:02:15 +0000

    Add special pages aliases for Western Balochi (bgn) from translatewiki
    Change-Id: Iadc088653b1b0e5188ec0f106d278448edbcf17e

--- languages/messages/MessagesBgn.php
+++ languages/messages/MessagesBgn.php
@@ -29,3 +29,28 @@ $namespaceNames = array(
 	NS_CATEGORY         => 'تهر',
 	NS_CATEGORY_TALK    => 'تهر_ئی_گپ',
 );
+
+$specialPageAliases = array(
+	'Activeusers'               => array( 'پئالین_کارمرزوکان' ),
+	'Allmessages'               => array( 'موچین_پیامان' ),
+	'AllMyUploads'              => array( 'ني_موچین_ایرگیج_ئآن', 'ني_موچین_وّرک_ئآن' ),
+	'Allpages'                  => array( 'موچین_تاکدیمان' ),
+	'ApiHelp'                   => array( 'ای_پی_آی_کومک' ),
+	'Ancientpages'              => array( 'کوهنگین_تاکدیمان' ),
+	'Badtitle'                  => array( 'خرابین_ئینوان' ),
+	'Blankpage'                 => array( 'خالین_دیم' ),
+	'Block'                     => array( 'بستین', 'آی_پی_بستین', 'کارمرزوکی_بستین' ),
+	'Booksources'               => array( 'کتاب_ئی_بُن' ),
+	'BrokenRedirects'           => array( 'خرابین_تغیرمسیر' ),
+	'Categories'                => array( 'تهرئان' ),
+	'ChangeEmail'               => array( 'ایمیل_ئی_گردینتین' ),
+	'ChangePassword'            => array( 'چیهرگال_ئی_پاک_کورتین', 'چیهرگال_ئی_ٹگل' ),
+	'ComparePages'              => array( 'تاکدیمانی_موقایسه_کورتین' ),
+	'Confirmemail'              => array( 'ایمیل_ئی_تائید_کورتین' ),
+	'Contributions'             => array( 'شراکت_ئان' ),
+	'CreateAccount'             => array( 'کارمرزوکین_هیساب_ئی_جۆڑ_کورتین' ),
+	'Deadendpages'              => array( 'بن_بست_ئین_تاکدیمان' ),
+	'DeletedContributions'      => array( 'پاک_بوته_ئین_شراکت_ئان' ),
+	'Diff'                      => array( 'پرک' ),
+	'DoubleRedirects'           => array( 'دوئین_تغیرمسیرئان' ),
+);
