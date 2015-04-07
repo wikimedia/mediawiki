ad6d3ff533d61c485b258f56c2d86fe2d5c25eb4
Chad Horohoe <chadh@wikimedia.org>
2015-04-07 13:48:23 -0700

    Begin 1.26 development cycle
    Change-Id: I9e63789e7bbec95e72fca6dfc1a79f0ae1fba9b0

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.25alpha';
+$wgVersion = '1.26alpha';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
