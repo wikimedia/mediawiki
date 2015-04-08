556ddafcea52e827b989f17081fc138bb8163d8b
Roan Kattouw <roan.kattouw@gmail.com>
2015-04-08 14:55:44 -0700

    ApiParse: Replace isCurrent() check with getLatest() comparison
    Because the $pageObj state is what actually ends up getting used.
    If $pageObj thinks an old rev is the latest one, then we are in
    trouble, even if $rev knows what's really going on.
    
    Bug: T95466
    Change-Id: I4d6ba4f18adaaad052d3bee1a575ba034aaf112b

--- includes/api/ApiParse.php
+++ includes/api/ApiParse.php
@@ -107,7 +107,7 @@ class ApiParse extends ApiBase {
 				$popts = $this->makeParserOptions( $pageObj, $params );
 
 				// If for some reason the "oldid" is actually the current revision, it may be cached
-				if ( $rev->isCurrent() ) {
+				if ( $rev->getId() == $pageObj->getLatest() ) {
 					// May get from/save to parser cache
 					$p_result = $this->getParsedContent( $pageObj, $popts,
 						$pageid, isset( $prop['wikitext'] ) );
