dc78838524a9a6e4f3a194a4c457b590b23ebcbc
aude <aude.wiki@gmail.com>
2015-04-09 00:53:37 -0400

    Replace use of assertType with assertInternalType and assertInstanceOf
    These are available in phpunit since 3.5.0, which I think
    is reasonable to expect people to have at this point,
    especially when we actually require 3.7.0 or higher in phpunit.php:
    
    if ( $puVersion !== '@package_version@' && version_compare( $puVersion, '3.7.0', '<' ) ) {
            die( "PHPUnit 3.7.0 or later required; you have {$puVersion}.\n" );
    }
    
    Change-Id: Ic32ab45110e4c4304ef046ae8d0e98c741255559

--- tests/phpunit/includes/api/query/ApiQueryTestBase.php
+++ tests/phpunit/includes/api/query/ApiQueryTestBase.php
@@ -56,12 +56,12 @@ STR;
 	 * @return array
 	 */
 	private function validateRequestExpectedPair( $v ) {
-		$this->assertType( 'array', $v, self::PARAM_ASSERT );
+		$this->assertInternalType( 'array', $v, self::PARAM_ASSERT );
 		$this->assertEquals( 2, count( $v ), self::PARAM_ASSERT );
 		$this->assertArrayHasKey( 0, $v, self::PARAM_ASSERT );
 		$this->assertArrayHasKey( 1, $v, self::PARAM_ASSERT );
-		$this->assertType( 'array', $v[0], self::PARAM_ASSERT );
-		$this->assertType( 'array', $v[1], self::PARAM_ASSERT );
+		$this->assertInternalType( 'array', $v[0], self::PARAM_ASSERT );
+		$this->assertInternalType( 'array', $v[1], self::PARAM_ASSERT );
 
 		return $v;
 	}
