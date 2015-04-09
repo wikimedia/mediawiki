f92f2fbc641ad5035df1afd7c198f92bddd84af4
This, that and the other <at.light@live.com.au>
2015-04-09 23:38:59 +1000

    UI for adding and removing change tags on revisions and log entries
    There is a new special page, Special:EditTags, which is very similar to
    Special:RevisionDelete in a lot of ways. In fact, the SpecialEditTags class
    started off as a copy-paste of SpecialRevisiondelete.
    
    You invoke this special page by going to an article history page, checking
    some revisions, and clicking "Edit tags of selected revisions". Then you
    pick the modifications you want to make and click "Apply". Very much like
    the revision deletion workflow.
    
    I had to restructure some of the Action routing code, which was only
    designed to handle revision deletion. Also removing some code from
    SpecialRevisiondelete which didn't work as advertised in the first place,
    and definitely doesn't work now.
    
    Change-Id: I7d3ef927b5686f6211bc5817776286ead19d916b

---
+++ includes/changetags/ChangeTagsLogList.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Change tagging
+ */
+
+/**
+ * Stores a list of taggable log entries.
+ */
+class ChangeTagsLogList extends ChangeTagsList {
+	public function getType() {
+		return 'logentry';
+	}
+
+	/**
+	 * @param DatabaseBase $db
+	 * @return mixed
+	 */
+	public function doQuery( $db ) {
+		$ids = array_map( 'intval', $this->ids );
+		$queryInfo = array(
+			'tables' => array( 'logging' ),
+			'fields' => array(
+				'log_id',
+				'log_type',
+				'log_action',
+				'log_timestamp',
+				'log_user',
+				'log_user_text',
+				'log_namespace',
+				'log_title',
+				'log_page',
+				'log_comment',
+				'log_params',
+				'log_deleted'
+			),
+			'conds' => array(
+				'log_id' => $ids,
+			),
+			'options' => array( 'ORDER BY' => 'log_id DESC' ),
+			'join_conds' => array(),
+		);
+		ChangeTags::modifyDisplayQuery(
+			$queryInfo['tables'],
+			$queryInfo['fields'],
+			$queryInfo['conds'],
+			$queryInfo['join_conds'],
+			$queryInfo['options']
+		);
+		return $db->select(
+			$queryInfo['tables'],
+			$queryInfo['fields'],
+			$queryInfo['conds'],
+			__METHOD__,
+			$queryInfo['options'],
+			$queryInfo['join_conds']
+		);
+	}
+
+	public function newItem( $row ) {
+		return new ChangeTagsLogItem( $this, $row );
+	}
+
+	/**
+	 * Add/remove change tags from all the log entries in the list.
+	 *
+	 * @param array $tagsToAdd
+	 * @param array $tagsToRemove
+	 * @param array $params
+	 * @param string $reason
+	 * @param User $user
+	 * @return Status
+	 */
+	public function updateChangeTagsOnAll( $tagsToAdd, $tagsToRemove, $params,
+		$reason, $user ) {
+
+		// @codingStandardsIgnoreStart Generic.CodeAnalysis.ForLoopWithTestFunctionCall.NotAllowed
+		for ( $this->reset(); $this->current(); $this->next() ) {
+			// @codingStandardsIgnoreEnd
+			$item = $this->current();
+			$status = ChangeTags::updateTagsWithChecks( $tagsToAdd, $tagsToRemove,
+				null, null, $item->getId(), $params, $reason, $user );
+			// Should only fail on second and subsequent times if the user trips
+			// the rate limiter
+			if ( !$status->isOK() ) {
+				break;
+			}
+		}
+
+		return $status;
+	}
+}
