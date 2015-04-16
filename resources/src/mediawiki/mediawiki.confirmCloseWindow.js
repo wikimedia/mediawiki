25682ef64d420a2b2201275e8d0520c1bf5330a1
Florian <florian.schmidt.welzow@t-online.de>
2015-04-16 18:29:02 +0200

    Extend mediaWiki.confirmCloseWindow with a custom event
    Some extensions and/or functions doesn't reload the page to destroy forms,
    that can contain a user input. To use confirmCloseWindow to cover these forms,
    too, we need to listen on a custom event (which can be set dynamically by the
    developer using this module) and show the warning to the user, if this event
    is triggered.
    
    Bug: T94194
    Change-Id: I897330bc62b9ef3bfd04f61f5c5d0744c8ec61d3

--- resources/src/mediawiki/mediawiki.confirmCloseWindow.js
+++ resources/src/mediawiki/mediawiki.confirmCloseWindow.js
@@ -13,18 +13,36 @@
 	 *     // ... do stuff that can't be interrupted ...
 	 *     allowCloseWindow();
 	 *
+	 * You can define one additional event name, which, when triggered on the window object, also shows a confirm
+	 * window to the user, if your test function returns true. The confirmation window looks slightly different
+	 * from the one created by onbeforeunload event, but has the same message.
+	 * Usage example:
+	 *
+	 *		var allowCloseWindow = mw.confirmCloseWindow( {
+	 *			test: function () {
+	 *				return true; // example
+	 *			},
+	 *			closeEventName: 'customCloseEvent'
+	 *		} );
+	 *		// .. other things
+	 *		// will be true, if the test function returns false or if the user confirms the warning window
+	 *		var canClosed = $( window ).triggerHandler( 'customCloseEvent' );
+	 *
 	 * @param {Object} [options]
 	 * @param {string} [options.namespace] Namespace for the event registration
 	 * @param {string} [options.message]
 	 * @param {string} options.message.return The string message to show in the confirm dialog.
 	 * @param {Function} [options.test]
+	 * @param {String} [options.closeEventName] Name of one additional event name
 	 * @param {boolean} [options.test.return=true] Whether to show the dialog to the user.
 	 * @return {Function} Execute this when you want to allow the user to close the window
 	 */
 	mw.confirmCloseWindow = function ( options ) {
 		var savedUnloadHandler,
 			mainEventName = 'beforeunload',
-			showEventName = 'pageshow';
+			showEventName = 'pageshow',
+			closeEventName = options.closeEventName || '',
+			message;
 
 		options = $.extend( {
 			message: mw.message( 'mwe-prevent-close' ).text(),
@@ -34,6 +52,15 @@
 		if ( options.namespace ) {
 			mainEventName += '.' + options.namespace;
 			showEventName += '.' + options.namespace;
+			if ( closeEventName ) {
+				closeEventName += '.' + options.namespace;
+			}
+		}
+
+		if ( $.isFunction( options.message ) ) {
+			message = options.message();
+		} else {
+			message = options.message;
 		}
 
 		$( window ).on( mainEventName, function () {
@@ -47,22 +74,26 @@
 				}, 1 );
 
 				// show an alert with this message
-				if ( $.isFunction( options.message ) ) {
-					return options.message();
-				} else {
-					return options.message;
-				}
+				return message;
 			}
 		} ).on( showEventName, function () {
 			// Re-add onbeforeunload handler
 			if ( !window.onbeforeunload && savedUnloadHandler ) {
 				window.onbeforeunload = savedUnloadHandler;
 			}
+		} ).on( closeEventName, function () {
+			// use window.confirm to show the message to the user (if options.text() is true)
+			if ( options.test() && !this.confirm( message ) ) {
+				// the user want to keep the actual page
+				return false;
+			}
+			// otherwise return true
+			return true;
 		} );
 
 		// return the function they can use to stop this
 		return function () {
-			$( window ).off( mainEventName + ' ' + showEventName );
+			$( window ).off( mainEventName + ' ' + showEventName + ' ' + closeEventName );
 		};
 	};
 } )( mediaWiki, jQuery );
