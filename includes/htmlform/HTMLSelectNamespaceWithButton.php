9e12127e02bde281c13cac1e3610103237d1e9a3
Florian <florian.schmidt.welzow@t-online.de>
2015-04-17 18:56:32 +0200

    Use HTMLForm for Special:Export
    * Transform all input fields to use HtmlForm as preparation for enabling
      MediaWiki UI eveywhere.
    * Remove protected whitespace for HTMLCheckField (adds empty line to div-layout)
    * Add a new HTMLForm input field "Text with Button" and "Namespaceselector with Button"
    
    Bug: T73434
    Change-Id: I53cc019c3ca94cec8f3c05500d0c604c1af7f688

---
+++ includes/htmlform/HTMLSelectNamespaceWithButton.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Creates a Html::namespaceSelector input field with a button assigned to the input field.
+ */
+class HTMLSelectNamespaceWithButton extends HTMLSelectNamespace {
+	/** @var HTMLFormClassWithButton $mClassWithButton */
+	protected $mClassWithButton = null;
+
+	public function __construct( $info ) {
+		$this->mClassWithButton = new HTMLFormFieldWithButton( $info );
+		parent::__construct( $info );
+	}
+
+	public function getInputHTML( $value ) {
+		return $this->mClassWithButton->getElement( parent::getInputHTML( $value ) );
+	}
+}
