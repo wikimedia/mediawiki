17bd57096ef507b931d6f30f424505347d24113b
mjbmr <mjbmri@gmail.com>
2015-04-23 08:14:17 +0000

    Add Goan Konkani Language
    Bug: T96468
    Bug: T93359
    Change-Id: I12857c36baa2b931f2db86f6be9a50e5e3057967

---
+++ languages/messages/MessagesGom.php
@@ -0,0 +1,11 @@
+<?php
+/** Goan Konkani (गोवा कोंकणी)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'gom-deva, hi';
