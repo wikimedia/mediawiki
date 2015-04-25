f7917d86800baad340e9585eeffe5809b58fe79a
jdlrobson <jdlrobson@gmail.com>
2015-04-24 17:12:46 -0700

    Add partial support to Mustache Templating offering
    * Add template partial support which matches the server side offering
    and means that we have full mustache support.
    
    Bug: T97188
    Change-Id: Ic752f52669dbffa21c4a514509c3ea1da8ac5d9c

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.template.mustache.test.js
@@ -0,0 +1,31 @@
+( function ( mw ) {
+
+	QUnit.module( 'mediawiki.template', {
+		setup: function () {
+			// Stub register some templates
+			this.sandbox.stub( mw.templates, 'get' ).returns( {
+				'test_templates_foo.mustache': '<div>{{msg}}{{>partial}}</div>',
+				'test_templates_foo_partial.mustache': ' goodbye'
+			} );
+		}
+	} );
+
+
+	QUnit.test( 'render', 2, function ( assert ) {
+		var html, htmlPartial, data,
+			template = mw.template.get( 'module.name.notimportant', 'test_templates_foo.mustache' ),
+			partial = mw.template.get( 'module.name.notimportant', 'test_templates_foo_partial.mustache' );
+
+		data = {
+			msg: 'Hello'
+		};
+
+		html = template.render( data ).html();
+		htmlPartial= template.render( data,{
+			partial: partial
+		} ).html();
+		assert.strictEqual( html, 'Hello', 'Render without partial' );
+		assert.strictEqual( htmlPartial, 'Hello goodbye', 'Render with partial' );
+	} );
+
+}( mediaWiki ) );
