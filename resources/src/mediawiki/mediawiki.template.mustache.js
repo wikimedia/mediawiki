3fb2ade3b6f57b28b8694c19b22e0975f8cb87e9
jdlrobson <jdlrobson@gmail.com>
2015-04-24 17:12:46 -0700

    Add support for partials to Mustache Templating offering
    * Add template partial support which matches the server side offering
    and means that we have full mustache support.
    * Without this change Mustache is pretty useless given that partials
    are a big part of any templating language.
    
    Bug: T97188
    Change-Id: Ic752f52669dbffa21c4a514509c3ea1da8ac5d9c

--- resources/src/mediawiki/mediawiki.template.mustache.js
+++ resources/src/mediawiki/mediawiki.template.mustache.js
@@ -4,8 +4,26 @@
 	mw.template.registerCompiler( 'mustache', {
 		compile: function ( src ) {
 			return {
-				render: function ( data ) {
-					return $( $.parseHTML( Mustache.render( src, data ) ) );
+				/**
+				 * @ignore
+				 * @returns {String} representing the source code of the template
+				 */
+				getSource: function () {
+					return src;
+				},
+				/**
+				 * @ignore
+				 * @param {Object} data to render
+				 * @param {Object} partialTemplates mapping of partial names to templates
+				 */
+				render: function ( data, partialTemplates ) {
+					var partials = {};
+					if ( partialTemplates ) {
+						$.each( partialTemplates, function ( name, template ) {
+							partials[name] = template.getSource();
+						} );
+					}
+					return $( $.parseHTML( Mustache.render( src, data, partials ) ) );
 				}
 			};
 		}
