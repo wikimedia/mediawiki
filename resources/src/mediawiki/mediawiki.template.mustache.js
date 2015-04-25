f27d50268999787c3b0ddc24edfde8f3e130afeb
jdlrobson <jdlrobson@gmail.com>
2015-04-24 17:12:46 -0700

    Add partial support to Mustache Templating offering
    * Add template partial support which matches the server side offering
    and means that we have full mustache support.
    
    TODO: Don't use _getSource, need to find a neater way of doing this...
    
    Bug: T97188
    Change-Id: Ic752f52669dbffa21c4a514509c3ea1da8ac5d9c

--- resources/src/mediawiki/mediawiki.template.mustache.js
+++ resources/src/mediawiki/mediawiki.template.mustache.js
@@ -4,8 +4,22 @@
 	mw.template.registerCompiler( 'mustache', {
 		compile: function ( src ) {
 			return {
-				render: function ( data ) {
-					return $( $.parseHTML( Mustache.render( src, data ) ) );
+				_getSource: function () {
+					return src;
+				},
+				/**
+				 * @ignore
+				 * @param {Object} data to render
+				 * @param {Object} partialTemplates mapping of partial names to templates
+				 */
+				render: function ( data, partialTemplates ) {
+					var partials = {};
+					if ( partialTemplates ) {
+						$.each( partialTemplates, function ( name, template ) {
+							partials[name] = template._getSource();
+						} );
+					}
+					return $( $.parseHTML( Mustache.render( src, data, partials ) ) );
 				}
 			};
 		}
