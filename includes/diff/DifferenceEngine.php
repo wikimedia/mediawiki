f749e1f617ee34ce69392656252de393aabac2c5
Jack Phoenix <jack@countervandalism.net>
2015-04-26 17:59:38 +0300

    Adding a bunch of hooks from wikiHow into DifferenceEngine
    These hooks allow things such as:
    * adding CSS(/JS) into the OutputPage when viewing diffs
    * adding extra HTML content (such as avatars) into diff views
    * hiding the bottom "mark as patrolled" link
    * altering the parser output that is used by DifferenceEngine
    * and more
    
    Example extension using these hooks is wikiHow's
    /extensions/wikihow/hooks/, specifically the file DiffHooks.php (but the
    hooks are setup in WikihowHooks.php).
    
    Live example of the DiffHooks stuff in action can be found at wikiHow.com,
    for example:
    http://www.wikihow.com/index.php?title=Set-Your-Homepage&diff=17112892&oldid=15888129
    (user avatars, additional CSS, changes to the old/new revision header
    texts/links)
    
    Change-Id: Icbc987fa4806e7bfc66743375301912b428dc348

--- includes/diff/DifferenceEngine.php
+++ includes/diff/DifferenceEngine.php
@@ -236,11 +236,13 @@ class DifferenceEngine extends ContextSource {
 	}
 
 	public function showDiffPage( $diffOnly = false ) {
-
 		# Allow frames except in certain special cases
 		$out = $this->getOutput();
 		$out->allowClickjacking();
 		$out->setRobotPolicy( 'noindex,nofollow' );
+
+		// Allow extensions to add any extra output here
+		Hooks::run( 'DifferenceEngineShowDiffPage', array( $out ) );
 
 		if ( !$this->loadRevisionData() ) {
 			$this->showMissingRevision();
@@ -283,6 +285,8 @@ class DifferenceEngine extends ContextSource {
 			$out->setPageTitle( $this->msg( 'difference-title', $this->mNewPage->getPrefixedText() ) );
 			$samePage = true;
 			$oldHeader = '';
+			// Allow extensions to change the $oldHeader variable
+			Hooks::run( 'DifferenceEngineOldHeaderNoOldRev', array( &$oldHeader ) );
 		} else {
 			Hooks::run( 'DiffViewHeader', array( $this, $this->mOldRev, $this->mNewRev ) );
 
@@ -352,6 +356,9 @@ class DifferenceEngine extends ContextSource {
 				'<div id="mw-diff-otitle5">' . $oldChangeTags[0] . '</div>' .
 				'<div id="mw-diff-otitle4">' . $prevlink . '</div>';
 
+			// Allow extensions to change the $oldHeader variable
+			Hooks::run( 'DifferenceEngineOldHeader', array( $this, &$oldHeader, $prevlink, $oldminor, $diffOnly, $ldel, $this->unhide ) );
+
 			if ( $this->mOldRev->isDeleted( Revision::DELETED_TEXT ) ) {
 				$deleted = true; // old revisions text is hidden
 				if ( $this->mOldRev->isDeleted( Revision::DELETED_RESTRICTED ) ) {
@@ -411,6 +418,9 @@ class DifferenceEngine extends ContextSource {
 			Linker::revComment( $this->mNewRev, !$diffOnly, !$this->unhide ) . $rdel . '</div>' .
 			'<div id="mw-diff-ntitle5">' . $newChangeTags[0] . '</div>' .
 			'<div id="mw-diff-ntitle4">' . $nextlink . $this->markPatrolledLink() . '</div>';
+
+		// Allow extensions to change the $newHeader variable
+		Hooks::run( 'DifferenceEngineNewHeader', array( $this, &$newHeader, $formattedRevisionTools, $nextlink, $rollback, $newminor, $diffOnly, $rdel, $this->unhide ) );
 
 		if ( $this->mNewRev->isDeleted( Revision::DELETED_TEXT ) ) {
 			$deleted = true; // new revisions text is hidden
@@ -498,6 +508,13 @@ class DifferenceEngine extends ContextSource {
 					// If the user could patrol this it already would be patrolled
 					$rcid = 0;
 				}
+
+				// Allow extensions to possibly change the rcid here
+				// For example the rcid might be set to zero due to the user
+				// being the same as the performer of the change but an extension
+				// might still want to show it under certain conditions
+				Hooks::run( 'DifferenceEngineMarkPatrolledRCID', array( &$rcid, $this, $change, $user ) );
+
 				// Build the link
 				if ( $rcid ) {
 					$this->getOutput()->preventClickjacking();
@@ -518,6 +535,9 @@ class DifferenceEngine extends ContextSource {
 							'token' => $token,
 						)
 					) . ']</span>';
+
+					// Allow extensions to change the markpatrolled link
+					Hooks::run( 'DifferenceEngineMarkPatrolledLink', array( $this, &$this->mMarkPatrolledLink, $rcid, $token ) );
 				} else {
 					$this->mMarkPatrolledLink = '';
 				}
@@ -595,15 +615,20 @@ class DifferenceEngine extends ContextSource {
 
 				# WikiPage::getParserOutput() should not return false, but just in case
 				if ( $parserOutput ) {
-					$out->addParserOutput( $parserOutput );
+					// Allow extensions to change parser output here
+					if ( !Hooks::run( 'DifferenceEngineRenderRevisionAddParserOutput', array( $this, $out, $parserOutput, $wikiPage ) ) ) {
+						$out->addParserOutput( $parserOutput );
+					}
 				}
 			}
 		}
 		# @codingStandardsIgnoreEnd
 
-		# Add redundant patrol link on bottom...
-		$out->addHTML( $this->markPatrolledLink() );
-
+		// Allow extensions to optionally not show the final patrolled link
+		if ( Hooks::run( 'DifferenceEngineRenderRevisionShowFinalPatrolLink' ) ) {
+			# Add redundant patrol link on bottom...
+			$out->addHTML( $this->markPatrolledLink() );
+		}
 	}
 
 	protected function getParserOutput( WikiPage $page, Revision $rev ) {
@@ -629,6 +654,8 @@ class DifferenceEngine extends ContextSource {
 	 * @return bool
 	 */
 	public function showDiff( $otitle, $ntitle, $notice = '' ) {
+		// Allow extensions to affect the output here
+		Hooks::run( 'DifferenceEngineShowDiff', array( $this ) );
 		$diff = $this->getDiff( $otitle, $ntitle, $notice );
 		if ( $diff === false ) {
 			$this->showMissingRevision();
@@ -702,8 +729,9 @@ class DifferenceEngine extends ContextSource {
 		if ( $this->mOldRev === false || ( $this->mOldRev && $this->mNewRev
 			&& $this->mOldRev->getID() == $this->mNewRev->getID() )
 		) {
-
-			return '';
+			if ( !Hooks::run( 'DifferenceEngineShowEmptyOldContent', array( $this ) ) ) {
+				return '';
+			}
 		}
 		// Cacheable?
 		$key = false;
