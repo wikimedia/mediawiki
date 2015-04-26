aa008ec5a71cedc4cbd1e989a19a8958a6c9fb90
cenarium <cenarium.sysop@gmail.com>
2015-04-26 17:28:44 +0200

    [WIP] Allow patrolling of tagged changes with minimalist RC patrol
    This commit builds on I2e48bd458fc8d7c289f04dc276f9287516e0b987, that
    provided a way to efficiently identify among recent changes e.g. pages
    moved into mainspace (T14363) or expanded from redirects (T92621), simply
    by tagging them as such. In this new patch, a way to patrol these changes
    is provided. Merely identifying them would not fulfill the requests since
    there would be no way for users to know if a change has been reviewed
    or not.
    
    This is accomplished by enabling the existing recent changes patrol
    feature of mediawiki, but with a minimalist user interface.
    The UI is provided only when a change has been tagged with a tag that
    indicates a problem, so should be reviewed. For example, the tags
    'move into mainspace' and 'redirect removed' are problem tags but
    'visual editor' is not.
    
    A tag can be marked as 'problem' by editing MediaWiki:Tags-settings.json.
    This page can also be used by extensions to store additional metadata on
    tags.
    
    Note that patrolling of moves, not possible at this time, is implemented
    separately in commit Ie0fa417feaf930c096b69521fc54d57aecd6cd51.
    
    Bug: T89553
    Bug: T95319
    Bug: T14363
    Bug: T92621
    Change-Id: Id3ceb62d1aa7b3ef0f9af072a313f1664cafe348

--- includes/api/ApiQueryTags.php
+++ includes/api/ApiQueryTags.php
@@ -47,6 +47,7 @@ class ApiQueryTags extends ApiQueryBase {
 		$fld_defined = isset( $prop['defined'] );
 		$fld_source = isset( $prop['source'] );
 		$fld_active = isset( $prop['active'] );
+		$fld_settings = isset( $prop['settings'] );
 
 		$limit = $params['limit'];
 		$result = $this->getResult();
@@ -136,6 +137,10 @@ class ApiQueryTags extends ApiQueryBase {
 				$tag['active'] = $changeTag->isActive();
 			}
 
+			if ( $fld_settings ) {
+				$tag['settings'] = $changeTag->getSettings();
+			}
+
 			$fit = $result->addValue( [ 'query', $this->getModuleName() ], null, $tag );
 			if ( !$fit ) {
 				$this->setContinueEnumParameter( 'continue', $tagName );
@@ -173,6 +178,7 @@ class ApiQueryTags extends ApiQueryBase {
 					'defined',
 					'source',
 					'active',
+					'settings',
 				],
 				ApiBase::PARAM_ISMULTI => true,
 				ApiBase::PARAM_HELP_MSG_PER_VALUE => [],
