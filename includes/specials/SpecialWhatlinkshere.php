567f127179e7f77c17d96a6154788651b8970b60
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2015-04-27 07:34:15 +0200

    Add edit link to each row on Special:WhatLinksHere
    Bug: T97269
    Change-Id: Ic13b775bc0c0ede947086c7eb305ac5d4f2fcd12

--- includes/specials/SpecialWhatlinkshere.php
+++ includes/specials/SpecialWhatlinkshere.php
@@ -314,7 +314,7 @@ class SpecialWhatLinksHere extends IncludableSpecialPage {
 		static $msgcache = null;
 		if ( $msgcache === null ) {
 			static $msgs = array( 'isredirect', 'istemplate', 'semicolon-separator',
-				'whatlinkshere-links', 'isimage' );
+				'whatlinkshere-links', 'isimage', 'edit' );
 			$msgcache = array();
 			foreach ( $msgs as $msg ) {
 				$msgcache[$msg] = $this->msg( $msg )->escaped();
@@ -355,7 +355,7 @@ class SpecialWhatLinksHere extends IncludableSpecialPage {
 		}
 
 		# Space for utilities links, with a what-links-here link provided
-		$wlhLink = $this->wlhLink( $nt, $msgcache['whatlinkshere-links'] );
+		$wlhLink = $this->wlhLink( $nt, $msgcache['whatlinkshere-links'], $msgcache['edit'] );
 		$wlh = Xml::wrapClass(
 			$this->msg( 'parentheses' )->rawParams( $wlhLink )->escaped(),
 			'mw-whatlinkshere-tools'
@@ -370,10 +370,21 @@ class SpecialWhatLinksHere extends IncludableSpecialPage {
 		return Xml::closeElement( 'ul' );
 	}
 
-	protected function wlhLink( Title $target, $text ) {
+	protected function wlhLink( Title $target, $text, $editText ) {
 		static $title = null;
 		if ( $title === null ) {
 			$title = $this->getPageTitle();
+		}
+
+		$editLink = '';
+		if ( $this->getUser()->isAllowed( 'edit' ) ) {
+			$editLink = $this->msg( 'pipe-separator' )->escaped() .
+				Linker::linkKnown(
+					$target,
+					$editText,
+					array(),
+					array( 'action' => 'edit' )
+				);
 		}
 
 		return Linker::linkKnown(
@@ -381,7 +392,7 @@ class SpecialWhatLinksHere extends IncludableSpecialPage {
 			$text,
 			array(),
 			array( 'target' => $target->getPrefixedText() )
-		);
+		) . $editLink;
 	}
 
 	function makeSelfLink( $text, $query ) {
