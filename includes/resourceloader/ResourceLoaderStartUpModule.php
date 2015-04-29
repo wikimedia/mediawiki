d2c6517c007d79ed1db2eee5417a79b5855ba6cf
Timo Tijhof <krinklemail@gmail.com>
2015-04-29 23:53:24 +0100

    [WIP] resourceloader: Replace timestamp system with version hashing
    Modules now track their version via getVersionHash() instead of
    getModifiedTime().
    
    == Background ==
    
    While some resources have simple to observe timestamp (e.g. files stored on
    disk), many other sources of information do not. Such as configuration
    variables, and module definitions.
    
    Aside from static module definitions, there are also dynamic ones. Such as
    wiki modules that allow the inclusion of certain pages but those pages may
    not always exist. E.g. a user module can contain common.js and skin.js. However
    by default neither exists (timestamp: 1). If a user has both and the one
    least-recently modified one is deleted, the timestamp does not increase.
    
    For static file modules, one can e.g. revert one of more files in a module to a
    previous version and not affect the max timestamp.
    
    For client-side caching, batch request used to max() timestamps of multiple
    modules, allowing the same tolerance at a wider level. Again, if one of the
    modules change but another module is more recent, the change would not result
    in a cache miss.
    
    More scenarios can be found in the associated Phabricator tasks.
    
    == Version hash ==
    
    Previously we often manually tracked when values change by tracking a hash in
    ObjectCache (eg. Memcached). Considering the number of different request
    contexts possible (wikis * modules * users * skin * language) this doesn't work
    very well. It also results in needless cache invalidation when the first time
    observation is purged due to LRU algorithms.
    
    As example, the following modules now make use of getVersionHash.
    
    * ResourceLoaderFileModule
    * ResourceLoaderEditToolbarModule
    * ResourceLoaderStartUpModule
    * ResourceLoaderWikiModule
    
    This slightly increases the startup module size because we now store
    hexidecimal strings instead of integers timestamps. After gzip and minify,
    the default startup module is now 6.1KB, compared to 5.8KB before.
    
    == Notes ==
    
    * Remove redundant modifiedTime cache in ResourceLoaderFileModule.
    * Change bugzilla references to Phabricator.
    * Centralised inclusion of wgCacheEpoch using getDefinitionSummary.
      Previously this logic was duplicated in each place the modified timestamp
      was used.
    * tryRespondNotModified() is using HTTP/1.0. While Last-Modified was part
      of HTTP/1.0, ETag was added in HTTP/1.1. Consider changing this to HTTP/1.1.
      Evaluate impact of responding with HTTP/1.0 Not Modified to a HTTP/1.1
      request with If-None-Match. While that may be fine, the non-304 response that
      provides the client with an ETag should be HTTP/1.1.
    
    == Known issues ==
    
    * ResourceFileCache (RL support for $wgUseFileCache) still uses
      timestamps and is broken now.
    
    * It's easy to forget calling the parent class in getDefinitionSummary().
      Previously this method only had 'class' by default, and various extensions
      hardcoded that instead of calling the parent.
      To better detect this, getVersionHash() now asserts that the '_cacheEpoch'
      property made it through.
    
    May also resolve:
    * T44094
    * T90411
    * T94810
    
    Bug: T94074
    Change-Id: Ibb292d2416839327d1807a66c78fd96dac0637d0

--- includes/resourceloader/ResourceLoaderStartUpModule.php
+++ includes/resourceloader/ResourceLoaderStartUpModule.php
@@ -24,13 +24,9 @@
 
 class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 
-	/* Protected Members */
-
-	protected $modifiedTime = array();
+	// Cache for getConfigSettings() as it's called by multiple methods
 	protected $configVars = array();
 	protected $targets = array( 'desktop', 'mobile' );
-
-	/* Protected Methods */
 
 	/**
 	 * @param ResourceLoaderContext $context
@@ -158,7 +154,7 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	 * data send to the client.
 	 *
 	 * @param array &$registryData Modules keyed by name with properties:
-	 *  - number 'version'
+	 *  - string 'version'
 	 *  - array 'dependencies'
 	 *  - string|null 'group'
 	 *  - string 'source'
@@ -209,10 +205,9 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 				continue;
 			}
 
-			// Coerce module timestamp to UNIX timestamp.
-			// getModifiedTime() is supposed to return a UNIX timestamp, but custom implementations
-			// might forget. TODO: Maybe emit warning?
-			$moduleMtime = wfTimestamp( TS_UNIX, $module->getModifiedTime( $context ) );
+			$versionHash = $module->getVersionHash( $context );
+			// Summarise long sequence of serialised data into a short hash
+			$version = substr( sha1( $versionHash ), 0, 16 );
 
 			$skipFunction = $module->getSkipFunction();
 			if ( $skipFunction !== null && !ResourceLoader::inDebugMode() ) {
@@ -225,14 +220,8 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 				);
 			}
 
-			$mtime = max(
-				$moduleMtime,
-				wfTimestamp( TS_UNIX, $this->getConfig()->get( 'CacheEpoch' ) )
-			);
-
 			$registryData[$name] = array(
-				// Convert to numbers as wfTimestamp always returns a string, even for TS_UNIX
-				'version' => (int) $mtime,
+				'version' => $version,
 				'dependencies' => $module->getDependencies(),
 				'group' => $module->getGroup(),
 				'source' => $module->getSource(),
@@ -262,7 +251,7 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 				continue;
 			}
 
-			// Call mw.loader.register(name, timestamp, dependencies, group, source, skip)
+			// Call mw.loader.register(name, version, dependencies, group, source, skip)
 			$registrations[] = array(
 				$name,
 				$data['version'],
@@ -279,8 +268,6 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 
 		return $out;
 	}
-
-	/* Methods */
 
 	/**
 	 * @return bool
@@ -310,14 +297,13 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	public static function getStartupModulesUrl( ResourceLoaderContext $context ) {
 		$moduleNames = self::getStartupModules();
 
-		// Get the latest version
+		// Combine relevant version hashes
 		$loader = $context->getResourceLoader();
-		$version = 1;
+		$hashes = '';
 		foreach ( $moduleNames as $moduleName ) {
-			$version = max( $version,
-				$loader->getModule( $moduleName )->getModifiedTime( $context )
-			);
+			$hashes .= $loader->getModule( $moduleName )->getVersionHash( $context );
 		}
+		$versionHash = substr( sha1( $hashes ), 0, 16 );
 
 		$query = array(
 			'modules' => ResourceLoader::makePackedModulesString( $moduleNames ),
@@ -325,7 +311,7 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 			'lang' => $context->getLanguage(),
 			'skin' => $context->getSkin(),
 			'debug' => $context->getDebug() ? 'true' : 'false',
-			'version' => wfTimestamp( TS_ISO_8601_BASIC, $version )
+			'version' => $versionHash,
 		);
 		// Ensure uniform query order
 		ksort( $query );
@@ -382,59 +368,54 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	}
 
 	/**
+	 * Get the definition summary for this module.
+	 *
 	 * @param ResourceLoaderContext $context
-	 * @return array|mixed
+	 * @return array
 	 */
-	public function getModifiedTime( ResourceLoaderContext $context ) {
+	public function getDefinitionSummary( ResourceLoaderContext $context ) {
 		global $IP;
+		$summary = parent::getDefinitionSummary( $context );
+		$summary[] = array(
+			// Detect changes to variables exposed in mw.config (T30899).
+			'vars' => $this->getConfigSettings( $context ),
+			// Changes how getScript() creates mw.Map for mw.config
+			'wgLegacyJavaScriptGlobals' => $this->getConfig()->get( 'LegacyJavaScriptGlobals' ),
+			// Detect changes to the module registrations
+			'moduleHashes' => $this->getAllModuleHashes( $context ),
 
-		$hash = $context->getHash();
-		if ( isset( $this->modifiedTime[$hash] ) ) {
-			return $this->modifiedTime[$hash];
-		}
-
-		// Call preloadModuleInfo() on ALL modules as we're about
-		// to call getModifiedTime() on all of them
-		$loader = $context->getResourceLoader();
-		$loader->preloadModuleInfo( $loader->getModuleNames(), $context );
-
-		$time = max(
-			wfTimestamp( TS_UNIX, $this->getConfig()->get( 'CacheEpoch' ) ),
-			filemtime( "$IP/resources/src/startup.js" ),
-			$this->getHashMtime( $context )
+			'fileMtimes' => array(
+				filemtime( "$IP/resources/src/startup.js" ),
+			),
 		);
-
-		// ATTENTION!: Because of the line below, this is not going to cause
-		// infinite recursion - think carefully before making changes to this
-		// code!
-		// Pre-populate modifiedTime with something because the loop over
-		// all modules below includes the startup module (this module).
-		$this->modifiedTime[$hash] = 1;
-
-		foreach ( $loader->getModuleNames() as $name ) {
-			$module = $loader->getModule( $name );
-			$time = max( $time, $module->getModifiedTime( $context ) );
-		}
-
-		$this->modifiedTime[$hash] = $time;
-		return $this->modifiedTime[$hash];
+		return $summary;
 	}
 
 	/**
-	 * Hash of all dynamic data embedded in getScript().
-	 *
-	 * Detect changes to mw.config settings embedded in #getScript (bug 28899).
+	 * Helper method for getDefinitionSummary().
 	 *
 	 * @param ResourceLoaderContext $context
-	 * @return string Hash
+	 * @return string SHA-1
 	 */
-	public function getModifiedHash( ResourceLoaderContext $context ) {
-		$data = array(
-			'vars' => $this->getConfigSettings( $context ),
-			'wgLegacyJavaScriptGlobals' => $this->getConfig()->get( 'LegacyJavaScriptGlobals' ),
-		);
+	protected function getAllModuleHashes( ResourceLoaderContext $context ) {
+		// Preload information in a batch to optimise calls to ResourceLoader::getVersionHash below
+		$loader = $context->getResourceLoader();
+		$loader->preloadModuleInfo( $loader->getModuleNames(), $context );
 
-		return md5( serialize( $data ) );
+		// ATTENTION: Because of the line below, this is not going to cause infinite recursion.
+		// Think carefully before making changes to this code!
+		// Pre-populate versionHash with something because the loop over all modules below includes
+		// the startup module (this module).
+		// See ResourceLoaderModule::getVersionHash() for the user of this cache.
+		$this->versionHash[ $context->getHash() ] = 1;
+
+		$hashes = '';
+		foreach ( $loader->getModuleNames() as $name ) {
+			$module = $loader->getModule( $name );
+			$hashes .= $module->getVersionHash( $context );
+		}
+
+		return substr( sha1( $hashes ), 0, 16 );
 	}
 
 	/**
