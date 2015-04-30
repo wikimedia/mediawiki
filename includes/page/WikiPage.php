03a86df230ea7dea9f533e5c654536f02fdb77dd
Mark A. Hershberger <mah@nichework.com>
2015-04-29 22:23:30 -0400

    Create PageViewUpdates hook
    Bug: T97622
    Change-Id: I0f2dedf84a540b6f87623792c7b4f8b8ad8f8059

--- includes/page/WikiPage.php
+++ includes/page/WikiPage.php
@@ -1141,6 +1141,7 @@ class WikiPage implements Page, IDBAccessObject {
 			return;
 		}
 
+		Hooks::run( 'PageViewUpdates', array( $this, $user ) );
 		// Update newtalk / watchlist notification status
 		$user->clearNotification( $this->mTitle, $oldid );
 	}
