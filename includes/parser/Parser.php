c430850154b4346bc02ec7dd6c931c4649088b5e
umherirrender <umherirrender_de.wp@web.de>
2015-05-12 20:43:59 +0200

    Check result of preg_match_all in Parser.php
    preg_match_all can return false on failure, which than results in
    undefined index access.
    Check the result and just keep it as nothing found by processing an
    empty array
    
    Change-Id: I1f11894240dc6869506d68d3513715abdc3abb5d

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -4446,7 +4446,9 @@ class Parser {
 		$tocraw = array();
 		$refers = array();
 
-		foreach ( $matches[3] as $headline ) {
+		$headlines = $numMatches !== false ? $matches[3] : array();
+
+		foreach ( $headlines as $headline ) {
 			$isTemplate = false;
 			$titleText = false;
 			$sectionIndex = false;
