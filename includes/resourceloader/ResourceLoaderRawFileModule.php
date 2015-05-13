232101dd9a200125460be52b611ca1eb249fb0ef
Timo Tijhof <krinklemail@gmail.com>
2015-05-13 19:23:26 +0100

    resourceloader: Implement ResourceLoaderRawFileModule for 'mediawiki.js'
    When adding files to this module, they all imply a global scope.
    While most files have their own closure, it is still inconsistent
    with normal modules which always have a closure via mw.loader.implement.
    
    Base modules like 'mediawiki' are lacking such a closure, thus making
    it hard to share variables within the different files that make up the
    module without making it a globally public.
    
    The use case is 13203c0a2180e5, where we could expose the sha1()
    library within the scope of this module only.
    
    Change-Id: I93fcd045f47ec6a595b26f739939e1d371832ac9

---
+++ includes/resourceloader/ResourceLoaderRawFileModule.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Module containing files that are loaded without ResourceLoader.
+ *
+ * Primary usecase being "base" modules loaded by the startup module,
+ * such as jquery and the mw.loader client itself. These make use of
+ * ResourceLoaderModule and load.php for convenience but aren't actually
+ * registered in the startup module (as it would have to load itself).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Timo Tijhof
+ */
+
+class ResourceLoaderRawFileModule extends ResourceLoaderFileModule {
+
+	/**
+	 * Enable raw mode to omit mw.loader.state() call as mw.loader
+	 * does not yet exist when these modules execute.
+	 * @var boolean
+	 */
+	protected $raw = true;
+
+	/**
+	 * Get all JavaScript code.
+	 *
+	 * @param ResourceLoaderContext $context
+	 * @return string JavaScript code
+	 */
+	public function getScript( ResourceLoaderContext $context ) {
+		$script = parent::getScript( $context );
+		// Add closure explicitly because raw modules can't be wrapped mw.loader.implement.
+		// Unlike with mw.loader.implement, this closure is immediately invoked.
+		// @see ResourceLoader::makeModuleResponse
+		// @see ResourceLoader::makeLoaderImplementScript
+		return "(function () {\n{$script}\n}());";
+	}
+}
