f13c03ac588cb47b5794e900e35807ea9e934f63
cenarium <cenarium.sysop@gmail.com>
2015-05-16 00:23:22 +0200

    Unify ChangeTags hooks, create ChangeTag and ChangeTagsContext classes
    This introduces a single hook for extensions to register tags.
    All of the tag params, including but not limited to 'active'
    status, are provided in this single hook. New params are added :
    'name' to indicate the extension's name for tracking and an array
    of params for use in an extension-specific description placed
    below the custom description (for AbuseFilter, this would be the
    abuse filters using the tag). Separately, an extension-specific
    source message can replace the default source message.
    
    The array passed as argument in the hook takes the tag names as keys
    and params as values. Integers can no longer be tag names since it
    would cause issues (when merging arrays).
    The hooks ListDefinedTags, ChangeTagListActive and ChangeTagCanDelete
    are merged in this hook and can be deprecated. It should improve
    performance since the extensions will need to hit their database
    only once.
    Extensions should purge the cache of registered tags if the newly
    defined tags need to appear quickly (such is the case for AbuseFilter
    - cf commit Ibfadd04acfebbedaad1b67c5818dfa5c1b7037b3).
    
    A new ChangeTag class is created, as well as a ChangeTagsContext
    class that organizes the data retrieved from the hook to provide
    relevant information to a ChangeTag instance. This is useful
    when multiple ChangeTag instances are created, for example at
    Special:Tags, since it allows to fetch all this data only once.
    
    Bug: T91535
    Change-Id: I4f4b097d660ada77f5cf7b4231925009b27127ea

---
+++ tests/phpunit/includes/changetags/ChangeTagsContextTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class ChangeTagsContextTest extends ChangeTagsTest {
+
+	/**
+	 * @covers ChangeTagsContext::definedTags
+	 */
+	public function testDefinedTags() {
+		$returned = array_keys( ChangeTagContext::definedTags() );
+		$expected = array( 'StoredTagNoHits', 'StoredTagWithHits', 'ActiveRegisteredTag', 'InactiveRegisteredTag' );
+
+		sort( $returned );
+		sort( $expected );
+		$this->assertEquals( $returned, $expected );
+	}
+}
