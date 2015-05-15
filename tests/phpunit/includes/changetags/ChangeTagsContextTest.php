1199c2bea85607a774e0bedc76d3d7a82418b2cd
cenarium <cenarium.sysop@gmail.com>
2015-05-16 00:23:22 +0200

    Unify hooks for change tags and create classes to better handle them
    This introduces a single hook for extensions to register tags.
    All of the tag params, including but not limited to 'active'
    status, are provided in this single hook. New params are added:
    'name' to indicate the extension's name for tracking and an array
    of params for use in an extension-specific description placed
    below the custom description (for AbuseFilter, this would be the
    abuse filters using the tag). Separately, an extension-specific
    source message can replace the default source message.
    
    The array passed as argument in the hook takes the tag names as keys
    and params as values. Integers can no longer be tag names since it
    would cause issues (when merging arrays), and it already causes some.
    The hooks ListDefinedTags, ChangeTagListActive and ChangeTagCanDelete
    are merged in this hook and can be deprecated. It should improve
    performance since the extensions will need to hit their database
    only once.
    Extensions should purge the cache of registered tags if the newly
    defined tags need to appear quickly (such is the case for AbuseFilter
    - cf commit Ibfadd04acfebbedaad1b67c5818dfa5c1b7037b3).
    
    A new ChangeTagsContext class that organizes the data retrieved
    from the site config and hooks is created. This is useful
    when data for multiple tags is needed at once, for example at
    Special:Tags, since it allows to fetch all this data only once.
    A ChangeTag class is created to access properties of a single tag.
    And ChangeTagsUpdater and ChangeTagsManager classes are created
    to handle respectively updating and managing change tags.
    
    Bug: T91535
    Change-Id: I4f4b097d660ada77f5cf7b4231925009b27127ea

---
+++ tests/phpunit/includes/changetags/ChangeTagsContextTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class ChangeTagsContextTest extends MediaWikiTestCase {
+	private $context;
+	private $stored;
+	private $registered;
+	private $core;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->registered = [
+			'ActiveRegisteredTag' => [
+				'active' => true, 'extName' => 'myext',
+			],
+			'InactiveRegisteredTag' => [],
+		];
+		$this->stored = [
+			'StoredTag' => [ 'active' => true ],
+		];
+		$this->core = [
+			'mw-contentmodelchange' => [ 'active' => true ],
+		];
+		$config = RequestContext::getMain()->getConfig();
+		$this->context = $this->getMockBuilder( 'ChangeTagsContext' )
+			->setConstructorArgs( [ $config ] )
+			->setMethods( [ 'getUserTags', 'getSoftwareTags' ] )->getMock();
+		$this->context->expects( $this->any() )->method( 'getUserTags' )
+			->will( $this->returnValue( $this->stored ) );
+		$this->context->expects( $this->any() )->method( 'getSoftwareTags' )
+			->will( $this->returnValue( array_merge( $this->registered, $this->core ) ) );
+	}
+
+	/**
+	 * @covers ChangeTagsContext::getDefinedTags
+	 */
+	public function testGetDefinedTags() {
+		$result = $this->context->getDefinedTags();
+		$expected = array_merge( $this->stored, $this->registered, $this->core );
+		ksort( $result );
+		ksort( $expected );
+		$this->assertEquals( $expected, $result );
+	}
+}
