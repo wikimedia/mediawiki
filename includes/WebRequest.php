7ce049155f116cef066cfd8248376a3d88c3de53
Ori Livneh <ori@wikimedia.org>
2015-05-16 14:09:24 -0700

    Introduce 'RequestShutdown' hook for deferred work
    With some PHP setups, it is possible for a script to continue executing after
    sending a response to the client. By deferring work that is not needed for
    generating the response, the response can be sent to the user sooner.
    
    The way to do this differs by runtime. HHVM provides a
    `register_postsend_function` function for this purpose. PHP has
    `register_shutdown_function`, but it does not work properly under PHP-FPM
    unless we instruct PHP to ignore user aborts, call `fastcgi_finish_request`,
    and then explicitly exit.
    
    The idea behind this patch is to hide these idiosyncracies behind a simple
    interface that is usable by both core and extensions. When the global
    WebRequest object is constructed, it will pick the appropriate mechanism to
    register a function to be executed after request processing. This function
    then fires a 'RequestShutdown' hook. Core and extensions can register handlers
    for this hook as a means of deferring work until after request processing.
    
    Change-Id: I2a7b5be1e3ee57277e996eae16d282b3705be11c

--- includes/WebRequest.php
+++ includes/WebRequest.php
@@ -75,6 +75,20 @@ class WebRequest {
 		// POST overrides GET data
 		// We don't use $_REQUEST here to avoid interference from cookies...
 		$this->data = $_POST + $_GET;
+
+		$onShutdown = function () {
+			$context = RequestContext::getMain();
+			$request = $context->getRequest();
+			Hooks::run( 'RequestShutdown', array( $request, $context ) );
+		};
+
+		// On HHVM, use `register_postsend_function`, as recommended in
+		// https://github.com/facebook/hhvm/issues/1230#issuecomment-42626926
+		if ( function_exists( 'register_postsend_function' ) ) {
+			register_postsend_function( $onShutdown );
+		} else {
+			register_shutdown_function( $onShutdown );
+		}
 	}
 
 	/**
@@ -1168,6 +1182,35 @@ HTML;
 	public function setIP( $ip ) {
 		$this->ip = $ip;
 	}
+
+	/**
+	 * Shut down the request, allowing any shutdown handlers to run.
+	 *
+	 * For convenience, allow callers to shut down the request and
+	 * enqueue a post-shutdown callback in one go.
+	 *
+	 * @param callable|null $callback Function to call after shutdown.
+	 *  Optional.
+	 * @since 1.26
+	 */
+	public function shutdown( $callback = null ) {
+		if ( $callback ) {
+			Hooks::register( 'RequestShutdown', $callback );
+		}
+		// Under FastCGI, client disconnects prevent shutdown functions from
+		// running, unless PHP is explicitly instructed to ignore user aborts.
+		// See http://php.net/manual/en/function.register-shutdown-function.php#112746
+		ignore_user_abort( true );
+
+		// PHP-FPM. See http://php.net/manual/en/function.fastcgi-finish-request.php
+		if ( function_exists( 'fastcgi_finish_request' ) ) {
+			fastcgi_finish_request();
+		}
+
+		// Call exit() to ensure shutdown functions get called.
+		// See https://www.drupal.org/node/2157053#comment-8448197
+		exit;
+	}
 }
 
 /**
@@ -1465,6 +1508,10 @@ class FauxRequest extends WebRequest {
 	 */
 	protected function getRawIP() {
 		return '127.0.0.1';
+	}
+
+	public function shutdown() {
+		return;
 	}
 }
 
