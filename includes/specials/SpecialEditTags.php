d493b3a8293922eb9576965f9762040a1a1708fd
cenarium <cenarium.sysop@gmail.com>
2015-05-16 22:08:09 +0200

    Drop down menu for selecting tags
    This provides a drop down menu for selecting tags, instead of the
    cumbersome-to-use input form. Each tag can have its appearance in
    the drop down menu modified in a specific message. The internal
    name might not be user friendly so can't be used for this, while
    the appearance in change lists might contain html or wikitext, so
    can't be used either. If the message is empty, it is skipped from
    the drop down menu, so it can be a way of no longer showing old
    irrelevant tags.
    
    Localization should be made for the tag-$tag-appearance message
    (appearance in drop down menus). The tag-$tag message uses the
    tag-$tag-appearance message as default so does not need l10n,
    except when it should be different, contain links or special
    syntax.
    
    Bug: T27909
    Change-Id: If4b247ee471a1601e6729e30cacdf8aa8d732cb4

--- includes/specials/SpecialEditTags.php
+++ includes/specials/SpecialEditTags.php
@@ -298,7 +298,12 @@ class SpecialEditTags extends UnlistedSpecialPage {
 			$html .= '<tr><td>' . $this->msg( 'tags-edit-existing-tags' )->escaped() .
 				'</td><td>';
 			if ( $tags ) {
-				$html .= $this->getLanguage()->commaList( array_map( 'htmlspecialchars', $tags ) );
+				$appTags = [];
+				foreach ( $tags as $tag ) {
+					// use tag appearance, making sure it is not empty
+					$appTags[] = ChangeTags::tagAppearance( $tag, true );
+				}
+				$html .= $this->getLanguage()->commaList( $appTags );
 			} else {
 				$html .= $this->msg( 'tags-edit-existing-tags-none' )->parse();
 			}
@@ -327,7 +332,8 @@ class SpecialEditTags extends UnlistedSpecialPage {
 				'wpRemoveAllTags', 'mw-edittags-remove-all' );
 			$i = 0; // used for generating checkbox IDs only
 			foreach ( $tags as $tag ) {
-				$html .= Xml::element( 'br' ) . "\n" . Xml::checkLabel( $tag,
+				$tagLabel = ChangeTags::tagAppearance( $tag, true );
+				$html .= Xml::element( 'br' ) . "\n" . Xml::checkLabel( $tagLabel,
 					'wpTagsToRemove[]', 'mw-edittags-remove-' . $i++, false, [
 						'value' => $tag,
 						'class' => 'mw-edittags-remove-checkbox',
@@ -367,13 +373,15 @@ class SpecialEditTags extends UnlistedSpecialPage {
 		$storedTags = $changeTagsContext->getStored();
 
 		$tags = [];
-		// Values of $tags are also used as <option> labels
+		// The appearance of each $tag is used as <option> label,
+		// required to be nonempty
 		foreach ( $storedTags  as $tag => &$val ) {
-			$select->addOption( $tag, $tag );
+			$select->addOption( ChangeTags::tagAppearance( $tag, true ), $tag );
 		}
+		// Also add selected tags if not already present
 		foreach ( $selectedTags as $tag ) {
 			if ( !isset( $storedTags[$tag] ) ) {
-				$select->addOption( $tag, $tag );
+				$select->addOption( ChangeTags::tagAppearance( $tag, true ), $tag );
 			}
 		}
 
