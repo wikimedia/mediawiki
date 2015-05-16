57ef1b8e3c1e37b2daa95429a6feab388f5ff8c5
Bartosz Dziewoński <matma.rex@gmail.com>
2015-05-16 17:38:37 +0200

    Convert mediawiki.action.edit.collapsibleFooter and mediawiki.special.changeslist.legend to using mw.cookie
    Remove redundant parameters, use sane defaults.
    
    This changes the cookie name due to mw.cookie adding the standard
    cookie prefix. This will cause existing values to be lost.
    
    Bug: T67384
    Change-Id: I00ba66b271cef4635ab4c31226c854583b8ba79e

--- resources/src/mediawiki.action/mediawiki.action.edit.collapsibleFooter.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.collapsibleFooter.js
@@ -1,27 +1,15 @@
-jQuery( document ).ready( function ( $ ) {
-	var collapsibleLists, i, handleOne;
-
-	// Collapsible lists of categories and templates
-	collapsibleLists = [
-		{
-			$list: $( '.templatesUsed ul' ),
-			$toggler: $( '.mw-templatesUsedExplanation' ),
-			cookieName: 'templates-used-list'
-		},
-		{
-			$list: $( '.hiddencats ul' ),
-			$toggler: $( '.mw-hiddenCategoriesExplanation' ),
-			cookieName: 'hidden-categories-list'
-		},
-		{
-			$list: $( '.preview-limit-report-wrapper' ),
-			$toggler: $( '.mw-limitReportExplanation' ),
-			cookieName: 'preview-limit-report'
-		}
-	];
-
-	handleOne = function ( $list, $toggler, cookieName ) {
-		var isCollapsed = $.cookie( cookieName ) !== 'expanded';
+( function ( mw, $ ) {
+	/**
+	 * Make a footer area collapsible.
+	 *
+	 * @param {jQuery} $list Footer area
+	 * @param {jQuery} $toggler Expand/collapse button
+	 * @param {string} cookieName Name of the cookie used to remember state
+	 * @private
+	 */
+	function makeFooterCollapsible( $list, $toggler, cookieName ) {
+		// Collapsed by default
+		var isCollapsed = mw.cookie.get( cookieName ) !== 'expanded';
 
 		// Style the toggler with an arrow icon and add a tabIndex and a role for accessibility
 		$toggler.addClass( 'mw-editfooter-toggler' ).prop( 'tabIndex', 0 ).attr( 'role', 'button' );
@@ -38,17 +26,39 @@ jQuery( document ).ready( function ( $ ) {
 
 		$list.on( 'beforeExpand.mw-collapsible', function () {
 			$toggler.removeClass( 'mw-icon-arrow-collapsed' ).addClass( 'mw-icon-arrow-expanded' );
-			$.cookie( cookieName, 'expanded' );
+			mw.cookie.set( cookieName, 'expanded' );
 		} );
 
 		$list.on( 'beforeCollapse.mw-collapsible', function () {
 			$toggler.removeClass( 'mw-icon-arrow-expanded' ).addClass( 'mw-icon-arrow-collapsed' );
-			$.cookie( cookieName, 'collapsed' );
+			mw.cookie.set( cookieName, 'collapsed' );
 		} );
-	};
-
-	for ( i = 0; i < collapsibleLists.length; i++ ) {
-		// Pass to a function for iteration-local variables
-		handleOne( collapsibleLists[i].$list, collapsibleLists[i].$toggler, collapsibleLists[i].cookieName );
 	}
-} );
+
+	$( function () {
+		var collapsibleLists, i;
+
+		// Collapsible lists of categories and templates
+		collapsibleLists = [
+			{
+				$list: $( '.templatesUsed ul' ),
+				$toggler: $( '.mw-templatesUsedExplanation' ),
+				cookieName: 'templates-used-list'
+			},
+			{
+				$list: $( '.hiddencats ul' ),
+				$toggler: $( '.mw-hiddenCategoriesExplanation' ),
+				cookieName: 'hidden-categories-list'
+			},
+			{
+				$list: $( '.preview-limit-report-wrapper' ),
+				$toggler: $( '.mw-limitReportExplanation' ),
+				cookieName: 'preview-limit-report'
+			}
+		];
+
+		for ( i = 0; i < collapsibleLists.length; i++ ) {
+			makeFooterCollapsible( collapsibleLists[i].$list, collapsibleLists[i].$toggler, collapsibleLists[i].cookieName );
+		}
+	} );
+}( mediaWiki, jQuery ) );
