5d62d69421091c43f087cb7fe387692865c5355a
umherirrender <umherirrender_de.wp@web.de>
2015-05-23 21:41:01 +0200

    Special:PageLanguage: Show log extract only when title known
    When open Special:PageLanguage without a page, the whole pagelang log is
    shown and the notice:
    Title::newFromText: $text must be a string. This will throw an
    InvalidArgumentException in future. [Called from LogPager::limitTitle]
    The whole log is useless at this position, so showing it only when
    looking at a specific page.
    
    Change-Id: I2a0b7ccc71789506554617e1d12797e72835a12a

--- includes/specials/SpecialPageLanguage.php
+++ includes/specials/SpecialPageLanguage.php
@@ -87,7 +87,10 @@ class SpecialPageLanguage extends FormSpecialPage {
 	}
 
 	protected function postText() {
-		return $this->showLogFragment( $this->par );
+		if ( $this->par ) {
+			return $this->showLogFragment( $this->par );
+		}
+		return '';
 	}
 
 	protected function getDisplayFormat() {
