4a3e50a541d10691c5610c415b9a0c70c0830888
Brad Jorsch <bjorsch@wikimedia.org>
2015-05-23 08:48:55 +0200

    Export allowed tags from Sanitizer to mediawiki.jqueryMsg
    Specifically, we export only those that are allowed to be paired,
    because its parser doesn't handle self-closing tags.
    
    Bug: T66740
    Change-Id: I9944f9af915715c57a7d9ce3c62c3e61e54a75ba

---
+++ includes/resourceloader/ResourceLoaderJqueryMsgDataModule.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Resource loader module for populating mediawiki.jqueryMsg data.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Brad Jorsch
+ */
+
+/**
+ * ResourceLoader module for populating mediawiki.jqueryMsg data.
+ */
+class ResourceLoaderJqueryMsgDataModule extends ResourceLoaderModule {
+
+	protected $targets = array( 'desktop', 'mobile' );
+
+	/**
+	 * @param ResourceLoaderContext $context
+	 * @return string JavaScript code
+	 */
+	public function getScript( ResourceLoaderContext $context ) {
+		$jsData = array();
+
+		$tagData = Sanitizer::getRecognizedTagData();
+		$jsData['allowedHtmlElements'] = array_merge(
+			array_keys( $tagData['htmlpairs'] ),
+			array_diff(
+				array_keys( $tagData['htmlsingle'] ),
+				array_keys( $tagData['htmlsingleonly'] )
+			)
+		);
+
+		return "if ( !mw.jqueryMsg ) {\n" .
+			"\tmw.jqueryMsg = {};\n" .
+			"}\n" .
+			"mw.jqueryMsg.data = " . Xml::encodeJsVar( $jsData ) . ";\n";
+	}
+
+	/**
+	 * @param ResourceLoaderContext $context
+	 * @return array|null
+	 */
+	public function getDefinitionSummary( ResourceLoaderContext $context ) {
+		$ret = parent::getDefinitionSummary( $context );
+		$ret['hash'] = md5( $this->getScript( $context ) );
+		return $ret;
+	}
+}
