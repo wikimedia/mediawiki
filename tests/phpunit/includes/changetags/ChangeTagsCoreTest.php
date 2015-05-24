3f454d0d7c5353da649aaf52b175dd589e1a5ad3
cenarium <cenarium.sysop@gmail.com>
2015-05-24 15:23:19 +0200

    Implement redirect-related and other tags in mediawiki core
    This implements a class for tags defined and applied in core. This uses two
    config variables, one boolean indicating if the feature is to be used at all,
    and one consisting in an array of core tags with their 'active' status (which
    determines whether they should be applied).
    
    The list of tags implemented in this patch set is as follows:
    	"tag-core-move-crossnamespace": "Cross namespace move",
    	"tag-core-move-tomainspace": "Move to mainspace",
    	"tag-core-move-rename": "Move attempting rename",
    	"tag-core-redirect-new": "New redirect",
    	"tag-core-redirect-changed": "Changed redirect target",
    	"tag-core-redirect-self": "Self redirect",
    	"tag-core-redirect-nonexistent": "Redirect to nonexistent page",
    	"tag-core-redirect-removed": "Removed redirect",
    	"tag-core-edit-blank": "Edit blanking the page",
    	"tag-core-edit-replace": "Edit replaced the page content",
    	"tag-core-newpage-blank": "New page is blank".
    
    They correspond to phabricator requests such as T14363 or T92621, to
    tags commonly implemented as tag-only abuse filters on WMF wikis, or
    to mediawiki autosummaries.
    
    Bug: T73236
    Change-Id: I2e48bd458fc8d7c289f04dc276f9287516e0b987

---
+++ tests/phpunit/includes/changetags/ChangeTagsCoreTest.php
@@ -0,0 +1,63 @@
+<?php
+
+class ChangeTagsCoreTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		$this->setMwGlobals( [ 'wgUseAutoTagging' => true ] );
+	}
+
+	/**
+	 * @dataProvider provideGetAutotagsForEditUpdate
+	 * @covers ChangeTagsCore::getAutotagsForEditUpdate
+	 */
+	public function testGetAutotagsForEditUpdate( $oldText, $newText, $pageTitle, $expected ) {
+		$title = Title::makeTitle( NS_MAIN, $pageTitle );
+		$oldContent =  ContentHandler::makeContent( $oldText, null, CONTENT_MODEL_WIKITEXT );
+		$newContent =  ContentHandler::makeContent( $newText, null, CONTENT_MODEL_WIKITEXT );
+		$actual = ChangeTagsCore::getAutotagsForEditUpdate( $oldContent, $newContent, $title );
+
+		sort( $expected );
+		sort( $actual );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function provideGetAutotagsForEditUpdate() {
+		return [
+			[ 'Hi', '#REDIRECT [[Main Page]]', 'Main Page',
+				[ 'mw-redirect-self', 'mw-redirect-new' ] ],
+			[ '#REDIRECT [[Main Page]]', 'nothing', 'Some Page',
+				[ 'mw-redirect-removed' ] ],
+			[ 'Lorem ipsum', '', 'Some Page',
+				[ 'mw-edit-blank' ] ],
+			[ 'Lorem ipsum', 'Lorem ipsum dolor sit amet', 'Some Page',
+				[] ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideGetAutotagsForMove
+	 * @covers ChangeTagsCore::getAutotagsForMove
+	 */
+	public function testGetAutotagsForMove(
+		$oldNs, $oldPage, $newNs, $newPage, $userName, $expected
+	) {
+		$oldTitle = Title::makeTitle( $oldNs, $oldPage );
+		$newTitle = Title::makeTitle( $newNs, $newPage );
+		$user = User::newFromName( $userName );
+		$actual = ChangeTagsCore::getAutotagsForMove( $oldTitle, $newTitle, $user );
+
+		sort( $expected );
+		sort( $actual );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function provideGetAutotagsForMove() {
+		return [
+			[ NS_USER, 'John', NS_USER, 'James', 'John',
+				[ 'mw-move-rename' ] ],
+			[ NS_MAIN, 'Foo', NS_PROJECT, 'Foo', 'Caesar',
+				[ 'mw-move-crossnamespace' ] ],
+		];
+	}
+}
