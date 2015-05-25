f12268f26beea4cf769c24a25e405898d3311a17
Tyler Cipriani <tcipriani@wikimedia.org>
2015-05-25 03:00:33 -0700

    Add login error browser test
    Change-Id: I19a7f708825d5f11859d8ea70db4cdde752df26c

--- tests/browser/features/step_definitions/create_account_steps.rb
+++ tests/browser/features/step_definitions/create_account_steps.rb
@@ -16,3 +16,11 @@ end
 Then(/^form has Create account button$/) do
   expect(on(CreateAccountPage).create_account_element).to exist
 end
+
+When(/^I submit the form$/) do
+  on(CreateAccountPage).create_account
+end
+
+Then(/^an error message is displayed$/) do
+  expect(on(CreateAccountPage).error_message_element.class_name).to eq "errorbox"
+end
