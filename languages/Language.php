1bbf93252ab1c5658c7c400d1a4a426e7b491b47
Timo Tijhof <krinklemail@gmail.com>
2015-06-02 00:04:26 +0100

    language: Use item 'fallbackSequence' instead of duplicating logic
    The 'fallbackSequence' is exactly generated for this purpose. It
    is equal to the value of 'fallback' after splitting, trimming
    and ensuring 'en' is present. See LocalisationCache::recache().
    
    Also simplify returning of the first array index by returning it
    directly instead of modifying the array first.
    
    Change-Id: I9c1d51b59aabebf5a31f38205304bb8cc22dcd8c

--- languages/Language.php
+++ languages/Language.php
@@ -4373,8 +4373,7 @@ class Language {
 			return false;
 		} else {
 			$fallbacks = self::getFallbacksFor( $code );
-			$first = array_shift( $fallbacks );
-			return $first;
+			return $fallbacks[0];
 		}
 	}
 
@@ -4388,14 +4387,9 @@ class Language {
 	public static function getFallbacksFor( $code ) {
 		if ( $code === 'en' || !Language::isValidBuiltInCode( $code ) ) {
 			return array();
-		} else {
-			$v = self::getLocalisationCache()->getItem( $code, 'fallback' );
-			$v = array_map( 'trim', explode( ',', $v ) );
-			if ( $v[count( $v ) - 1] !== 'en' ) {
-				$v[] = 'en';
-			}
-			return $v;
 		}
+		$value = self::getLocalisationCache()->getItem( $code, 'fallbackSequence' );
+		return $value;
 	}
 
 	/**
