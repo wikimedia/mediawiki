c15a1a3b942826e8835de8d9761d645a3c58c34c
Timo Tijhof <krinklemail@gmail.com>
2015-06-02 00:04:26 +0100

    language: Use item 'fallbackSequence' instead of duplicating logic
    The 'fallbackSequence' is exactly generated for this purpose. It
    is equal to the value of 'fallback' after splitting, trimming
    and ensuring 'en' is present. See LocalisationCache::recache().
    
    Also simplify returning of the first array index by returning it
    directly instead of modifying the array first.
    
    Due to an inconsistency between how LocalisationCache and Language classes
    treat the fallback sequence differently, we have to manually fallback
    to 'en' in case of unknown language codes. This because otherwise
    Language::factory() will throw an exception cause tests to fail.
    We should investigate whether this is desirable or not, but keeping
    existing behaviour for now and documenting it.
    
    Change-Id: I9c1d51b59aabebf5a31f38205304bb8cc22dcd8c

--- languages/Language.php
+++ languages/Language.php
@@ -4373,8 +4373,7 @@ class Language {
 			return false;
 		} else {
 			$fallbacks = self::getFallbacksFor( $code );
-			$first = array_shift( $fallbacks );
-			return $first;
+			return $fallbacks[0];
 		}
 	}
 
@@ -4383,19 +4382,15 @@ class Language {
 	 *
 	 * @since 1.19
 	 * @param string $code Language code
-	 * @return array
+	 * @return array Non-empty array, ending in "en"
 	 */
 	public static function getFallbacksFor( $code ) {
 		if ( $code === 'en' || !Language::isValidBuiltInCode( $code ) ) {
 			return array();
-		} else {
-			$v = self::getLocalisationCache()->getItem( $code, 'fallback' );
-			$v = array_map( 'trim', explode( ',', $v ) );
-			if ( $v[count( $v ) - 1] !== 'en' ) {
-				$v[] = 'en';
-			}
-			return $v;
 		}
+		// For unknown languages, fallbackSequence returns an empty array,
+		// hardcode fallback to 'en' in that case.
+		return self::getLocalisationCache()->getItem( $code, 'fallbackSequence' ) ?: array( 'en' );
 	}
 
 	/**
