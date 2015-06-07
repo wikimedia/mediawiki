026e49279f82eda76bdae8005883684eb25d9ece
Geoffrey Mon <geofbot@gmail.com>
2015-06-06 20:51:41 -0400

    Hide saved prefs message on change
    "Now 98.6% leaner!"
    
    * Use the mw.notify box for saved prefs message.
    * Remove box on keydown or mousedown in #preftoc or .prefsection
    * Remove success=1 querystring on load to prevent
      unnecessary reappearance of notification on reload.
    
    Bug: T19496
    Change-Id: Ibecbe21aa52ddc061d4bb27815f6fa5161a96735

--- resources/Resources.php
+++ resources/Resources.php
@@ -1475,10 +1475,12 @@ return array(
 			'prefs-tabs-navigation-hint',
 			'prefswarning-warning',
 			'saveprefs',
+			'savedprefs',
 		),
 		'dependencies' => array(
 			'mediawiki.language',
 			'mediawiki.confirmCloseWindow',
+			'mediawiki.notification',
 		),
 	),
 	'mediawiki.special.recentchanges' => array(
