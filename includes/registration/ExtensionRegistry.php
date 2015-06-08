84175bba311d426c7ad329c8259358e589a2dfbf
Kunal Mehta <legoktm@gmail.com>
2015-06-07 17:04:07 -0700

    Read extension.json files in the web updater
    The web updater reads LocalSettings.php in function scope to figure out
    what extensions have been loaded. This doesn't work for extensions being
    loaded through the ExtensionRegistry since they're only added to a
    queue.
    
    This adds the ability for the installer to read and get information from
    the current extension load queue. LocalSettings.php is still read for
    extensions that have not been converted yet.
    
    Other uses of Installer::getExistingLocalSettings() were audited and
    determined to be safe with regards to extension usage.
    
    Extensions that register hooks using explicit globals
    ($GLOBALS['wgHooks']['FooBar']) are still broken.
    
    Bug: T100414
    Change-Id: Icc574a38a7947a1e3aff8622a4889e9dcfd7a4b2

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -109,6 +109,24 @@ class ExtensionRegistry {
 	}
 
 	/**
+	 * Get the current load queue. Not intended to be used
+	 * outside of the installer.
+	 *
+	 * @return array
+	 */
+	public function getQueue() {
+		return $this->queued;
+	}
+
+	/**
+	 * Clear the current load queue. Not intended to be used
+	 * outside of the installer.
+	 */
+	public function clearQueue() {
+		$this->queued = array();
+	}
+
+	/**
 	 * Process a queue of extensions and return their extracted data
 	 *
 	 * @param array $queue keys are filenames, values are ignored
