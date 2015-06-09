04242797cbe6a089c7bf536d00be2bf6d23d7363
Fomafix <fomafix@googlemail.com>
2015-06-09 05:57:09 +0000

    Reduce flickering of TOC while loading
    * Move style for TOC hiding to separate module.
    * Load module 'mediawiki.toc' on 'position' => 'top'.
    * Hide TOC before DOM ready.
    
    Change-Id: Ib7c81e9433e7e3976e40e407f63fc8e924957faa

--- resources/src/mediawiki/mediawiki.toc.js
+++ resources/src/mediawiki/mediawiki.toc.js
@@ -1,9 +1,14 @@
 ( function ( mw, $ ) {
 	'use strict';
+	var hideToc = $.cookie( 'mw_hidetoc' ) === '1';
+	if ( hideToc ) {
+		// Hide toc before DOM ready
+		$( document.documentElement ).addClass( 'mw-hidetoc' );
+	}
 
 	// Table of contents toggle
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
-		var $toc, $tocTitle, $tocToggleLink, $tocList, hideToc;
+		var $toc, $tocTitle, $tocToggleLink, $tocList;
 		$toc = $content.find( '#toc' );
 		$tocTitle = $content.find( '#toctitle' );
 		$tocToggleLink = $content.find( '#togglelink' );
@@ -33,8 +38,6 @@
 		// Only add it if there is a complete TOC and it doesn't
 		// have a toggle added already
 		if ( $toc.length && $tocTitle.length && $tocList.length && !$tocToggleLink.length ) {
-			hideToc = $.cookie( 'mw_hidetoc' ) === '1';
-
 			$tocToggleLink = $( '<a href="#" id="togglelink"></a>' )
 				.text( hideToc ? mw.msg( 'showtoc' ) : mw.msg( 'hidetoc' ) )
 				.click( function ( e ) {
