335987beba538622012127096452db182a593b5a
James D. Forrester <jforrester@wikimedia.org>
2015-06-09 15:05:33 -0700

    Update OOjs UI to v0.11.4
    Release notes:
     https://git.wikimedia.org/blob/oojs%2Fui.git/v0.11.4/History.md
    
    Change-Id: Id329e753412b33d5b4cf55bfc04ee072843bacc0

--- resources/lib/oojs-ui/oojs-ui-apex.js
+++ resources/lib/oojs-ui/oojs-ui-apex.js
@@ -1,12 +1,12 @@
 /*!
- * OOjs UI v0.11.3
+ * OOjs UI v0.11.4
  * https://www.mediawiki.org/wiki/OOjs_UI
  *
  * Copyright 2011–2015 OOjs Team and other contributors.
  * Released under the MIT license
  * http://oojs.mit-license.org
  *
- * Date: 2015-05-12T12:15:37Z
+ * Date: 2015-06-09T22:03:14Z
  */
 /**
  * @class
