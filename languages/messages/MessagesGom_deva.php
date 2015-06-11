b39b3944dc807f7598ce224ee78731bc81191edd
mjbmr <mjbmri@gmail.com>
2015-06-11 20:15:07 +0000

    Add Goan Konkani Language
    Bug: T96468
    Change-Id: I12857c36baa2b931f2db86f6be9a50e5e3057967

---
+++ languages/messages/MessagesGom_deva.php
@@ -0,0 +1,31 @@
+<?php
+/** Goan Konkani - Devanagari script (गोवा कोंकणी)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Darshan kandolkar
+ */
+
+$fallback = 'hi';
+
+$namespaceNames = array(
+	NS_MEDIA            => 'मिडिया',
+	NS_SPECIAL          => 'विशेश',
+	NS_TALK             => 'चर्चा',
+	NS_USER             => 'उपेगकर्तो',
+	NS_USER_TALK        => 'उपेगकर्तो_चर्चा',
+	NS_PROJECT_TALK     => '$1_चर्चा',
+	NS_FILE             => 'फायल',
+	NS_FILE_TALK        => 'फायल_चर्चा',
+	NS_MEDIAWIKI        => 'मिडियाविकी',
+	NS_MEDIAWIKI_TALK   => 'मिडियाविकी_चर्चा',
+	NS_TEMPLATE         => 'प्रारूप',
+	NS_TEMPLATE_TALK    => 'प्रारूप_चर्चा',
+	NS_HELP             => 'मजत',
+	NS_HELP_TALK        => 'मजत_चर्चा',
+	NS_CATEGORY         => 'श्रेणी',
+	NS_CATEGORY_TALK    => 'श्रेणी_चर्चा',
+);
