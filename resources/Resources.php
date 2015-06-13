26edd30cc6108a9a3b875d94bb72be30bd2bac03
Ed Sanders <esanders@wikimedia.org>
2015-06-13 16:48:26 +0100

    [BREAKING CHANGE] Merge in VE's link input functionality to title widget
    Major changes:
    * Supports page images and descriptions
    * Uses prefix search
    
    Change-Id: Ib463e60cad9651eb338701279f370711ade6030b

--- resources/Resources.php
+++ resources/Resources.php
@@ -1735,6 +1735,7 @@ return array(
 		'scripts' => array(
 			'resources/src/mediawiki.widgets/mw.widgets.js',
 			'resources/src/mediawiki.widgets/mw.widgets.TitleInputWidget.js',
+			'resources/src/mediawiki.widgets/mw.widgets.TitleOptionWidget.js',
 		),
 		'skinStyles' => array(
 			'default' => 'resources/src/mediawiki.widgets/mw.widgets.TitleInputWidget.css',
@@ -1745,7 +1746,8 @@ return array(
 			'oojs-ui',
 		),
 		'messages' => array(
-			// …
+			'mw-widgets-titleinput-description-new-page',
+			'mw-widgets-titleinput-description-redirect',
 		),
 		'targets' => array( 'desktop', 'mobile' ),
 	),
