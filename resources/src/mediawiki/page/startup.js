9cfb9cb9fba4dcfd25a0924237681b7cc86a20df
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-06-15 00:13:20 +0200

    Add a wikipage.diff hook
    Bug: T53583
    Change-Id: Iba54f26537e0a7ffaaf9465e2f44de2e4367abdb

--- resources/src/mediawiki/page/startup.js
+++ resources/src/mediawiki/page/startup.js
@@ -27,6 +27,21 @@
 		 *   root)
 		 */
 		mw.hook( 'wikipage.content' ).fire( $( '#mw-content-text' ) );
+
+		var $diff = $( 'table.diff' );
+		if ( $diff.length ) {
+			/**
+			 * Fired when the diff is added to a page containing a diff
+			 *
+			 * Similar to the {@link mw.hook#event-wikipage_content wikipage.content hook}
+			 * $diff may still be detached when the hook is fired.
+			 *
+			 * @event wikipage_diff
+			 * @member mw.hook
+			 * @param {jQuery} $diff The root element of the MediaWiki diff (`table.diff`).
+			 */
+			mw.hook( 'wikipage.diff' ).fire( $diff.eq( 0 ) );
+		}
 	} );
 
 }( mediaWiki, jQuery ) );
