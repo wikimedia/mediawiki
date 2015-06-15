2fa783e2455873661f4edd4f9505783f94617c84
paladox <thomasmulhall410@yahoo.com>
2015-06-15 06:35:58 +0000

    Fix phpcs warnings
    Change-Id: Iecea365f83bbb0c336b58f1cfdebfc9b1112087e

--- maintenance/exportSites.php
+++ maintenance/exportSites.php
@@ -17,7 +17,9 @@ class ExportSites extends Maintenance {
 	public function __construct() {
 		$this->mDescription = 'Exports site definitions the sites table to XML file';
 
-		$this->addArg( 'file', 'A file to write the XML to (see docs/sitelist.txt). Use "php://stdout" to write to stdout.', true );
+		$this->addArg('file', 'A file to write the XML to (see docs/sitelist.txt). ' . 
+			'Use "php://stdout" to write to stdout.', true
+		);
 
 		parent::__construct();
 	}
