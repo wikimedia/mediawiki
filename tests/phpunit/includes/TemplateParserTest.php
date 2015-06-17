d2eb1a9169e1e25496a819817e7ca59d65aa338b
joakin <joaquin@chimeces.com>
2015-06-17 11:41:59 +0200

    WIP: Add tests for mustache conditionals
    Trying to reproduce a but encountered with HHVM mustache templates and closures.
    
    DO NOT MERGE.
    
    Change-Id: I713fd184173b2e6a1e0be924e9e8f1fd77a69b16

--- tests/phpunit/includes/TemplateParserTest.php
+++ tests/phpunit/includes/TemplateParserTest.php
@@ -28,11 +28,59 @@ class TemplateParserTest extends MediaWikiTestCase {
 		if ( $exception ) {
 			$this->setExpectedException( $exception );
 		}
-		$tp = new TemplateParser( $this->templateDir );
+		$tp = new TemplateParser( $this->templateDir, true );
 		$this->assertEquals( $result, $tp->processTemplate( $name, $args ) );
 	}
 
 	public static function provideProcessTemplate() {
+		return array(array(
+				'foobar_block_conditional',
+				array (
+					'langdir' => 'ltr',
+					'articleCountMsg' => '8 pages',
+					'privacyMsg' => 'Public',
+					'collectionUrl' => '/wiki/Special:Gather/id/1/Responsive_design_collection',
+					'hasImage' => true,
+					'image' => '<div
+					class=\'list-thumb
+
+						list-thumb-x\'
+	style=\'background-image: url("http://localhost:8080/images/0/02/Fucka_2015-02-16_11-53.jpg")\'></div>
+',
+					'title' => 'Responsive design collection',
+					'owner' => array (
+						'link' => '/wiki/Special:Gather/by/User',
+						'class' => 'mw-ui-icon mw-ui-icon-before mw-ui-icon-profile ',
+						'label' => 'User',
+					),
+				),
+				'<div class=\'collection-card \'>
+	<a href=\'/wiki/Special:Gather/id/1/Responsive_design_collection\' class=\'collection-card-image\'>
+		<div
+					class=\'list-thumb
+
+						list-thumb-x\'
+	style=\'background-image: url("http://localhost:8080/images/0/02/Fucka_2015-02-16_11-53.jpg")\'></div>
+
+	</a>
+	<div class=\'collection-card-overlay\' dir=\'ltr\'>
+		<div class=\'collection-card-title\'>
+			<a href=\'/wiki/Special:Gather/id/1/Responsive_design_collection\'>Responsive design collection</a>
+		</div>
+		
+			<a
+				class="mw-ui-icon mw-ui-icon-before mw-ui-icon-profile  collection-owner"
+				href="/wiki/Special:Gather/by/User">User</a>
+		<span>•</span>
+		
+		
+		<span class=\'collection-card-article-count\'>8 pages</span>
+	</div>
+</div>
+',
+			),
+		);
+
 		return array(
 			array(
 				'foobar',
@@ -45,6 +93,46 @@ class TemplateParserTest extends MediaWikiTestCase {
 					'planet' => 'world',
 				),
 				"hello world!\n",
+			),
+			array(
+				'foobar_block_conditional',
+				array (
+					'langdir' => 'ltr',
+					'articleCountMsg' => '8 pages',
+					'privacyMsg' => 'Public',
+					'collectionUrl' => '/wiki/Special:Gather/id/1/Responsive_design_collection',
+					'hasImage' => true,
+					'image' => '<div
+					class=\'list-thumb
+
+						list-thumb-x\'
+	style=\'background-image: url("http://localhost:8080/images/0/02/Fucka_2015-02-16_11-53.jpg")\'></div>
+',
+					'title' => 'Responsive design collection',
+					'owner' => array (
+						'link' => '/wiki/Special:Gather/by/User',
+						'class' => 'mw-ui-icon mw-ui-icon-before mw-ui-icon-profile ',
+						'label' => 'User',
+					),
+				),
+				'<div class=\'collection-card \'>
+	<a href=\'/wiki/Special:Gather/id/1/Responsive_design_collection\' class=\'collection-card-image\'>
+		<div
+  class=\'list-thumb
+        
+        list-thumb-x\'
+  style=\'background-image: url("http://localhost:8080/images/0/02/Fucka_2015-02-16_11-53.jpg")\'></div>
+
+	</a>
+	<div class=\'collection-card-overlay\' dir=\'ltr\'>
+		<div class=\'collection-card-title\'>
+			<a href=\'/wiki/Special:Gather/id/1/Responsive_design_collection\'>Responsive design collection</a>
+		</div>
+		list-thumb-y
+		
+		<span class=\'collection-card-article-count\'>8 pages</span>
+	</div>
+</div>',
 			),
 			array(
 				'../foobar',
