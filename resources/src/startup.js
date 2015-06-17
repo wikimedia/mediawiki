2071b2004feb7096f22cd26f6c9d45d8fe85daa7
Timo Tijhof <krinklemail@gmail.com>
2015-06-17 18:34:41 +0100

    [WIP] resourceloader: Use feature test instead of UA sniffing
    Bug: T102318
    Change-Id: I847b3f5f80f1eef3f57bbe7518768468e271082b

--- resources/src/startup.js
+++ resources/src/startup.js
@@ -1,61 +1,57 @@
 /**
  * This script provides a function which is run to evaluate whether or not to
- * continue loading jQuery and the MediaWiki modules. This code should work on
- * even the most ancient of browsers, so be very careful when editing.
- */
-
-var mediaWikiLoadStart = ( new Date() ).getTime();
-
-/**
- * Returns false for Grade C supported browsers.
+ * continue initialising the MediaWiki client-side environment.
  *
- * This function should only be used by the Startup module, do not expand it to
- * be generally useful beyond startup.
+ * This code MUST work on even the most ancient of browsers, ES5 or HTML5.
+ *
+ * The isCompatible() function returns false for Grade C supported browsers.
+ *
+ * This function should be kept minimal and should only to be used by the Startup module.
  *
  * See also:
  * - https://www.mediawiki.org/wiki/Compatibility#Browsers
  * - https://jquery.com/browser-support/
  */
-
 /*jshint unused: false */
-function isCompatible( ua ) {
-	if ( ua === undefined ) {
-		ua = navigator.userAgent;
-	}
 
-	// Browsers with outdated or limited JavaScript engines get the no-JS experience
-	return !(
-		// Internet Explorer < 8
-		( ua.indexOf( 'MSIE' ) !== -1 && parseFloat( ua.split( 'MSIE' )[1] ) < 8 ) ||
-		// Firefox < 3
-		( ua.indexOf( 'Firefox/' ) !== -1 && parseFloat( ua.split( 'Firefox/' )[1] ) < 3 ) ||
-		// Opera < 12
-		( ua.indexOf( 'Opera/' ) !== -1 && ( ua.indexOf( 'Version/' ) === -1 ?
-			// "Opera/x.y"
-			parseFloat( ua.split( 'Opera/' )[1] ) < 10 :
-			// "Opera/9.80 ... Version/x.y"
-			parseFloat( ua.split( 'Version/' )[1] ) < 12
-		) ) ||
-		// "Mozilla/0.0 ... Opera x.y"
-		( ua.indexOf( 'Opera ' ) !== -1 && parseFloat( ua.split( ' Opera ' )[1] ) < 10 ) ||
-		// BlackBerry < 6
-		ua.match( /BlackBerry[^\/]*\/[1-5]\./ ) ||
-		// Open WebOS < 1.5
-		ua.match( /webOS\/1\.[0-4]/ ) ||
-		// Anything PlayStation based.
-		ua.match( /PlayStation/i ) ||
-		// Any Symbian based browsers
-		ua.match( /SymbianOS|Series60/ ) ||
-		// Any NetFront based browser
-		ua.match( /NetFront/ ) ||
-		// Opera Mini, all versions
-		ua.match( /Opera Mini/ ) ||
-		// Nokia's Ovi Browser
-		ua.match( /S40OviBrowser/ ) ||
-		// MeeGo's browser
-		ua.match( /MeeGo/ ) ||
-		// Google Glass browser groks JS but UI is too limited
-		( ua.match( /Glass/ ) && ua.match( /Android/ ) )
+var mediaWikiLoadStart = ( new Date() ).getTime();
+
+function isCompatible( win ) {
+	if ( !win ) {
+		win = window;
+	}
+	var ua = window.navigator.userAgent;
+
+	return !!(
+		// HTML5 Storage
+		//
+		// Source:
+		// * http://caniuse.com/#feat=namevalue-storage
+		// * https://developer.blackberry.com/html5/apis/v1_0/localstorage.html
+		//
+		// Rejects:
+		// * Internet Explorer < 8, Firefox < 2, Safari < 4, Opera < 11
+		// * Opera Mini, Blackberry < 6
+		'localStorage' in window && (
+			// Rejects: Firefox < 3
+			( ua.indexOf( 'Firefox/' ) !== -1 && parseFloat( ua.split( 'Firefox/' )[1] ) < 3 ) ||
+			// Rejects: Opera 10-12 (Opera <= 9 had a different UA, rejected by feature test)
+			( ua.indexOf( 'Opera/' ) !== -1 && ua.indexOf( 'Version/' ) !== -1 && parseFloat( ua.split( 'Version/' )[1] ) < 12 ) ||
+			// Rejects: Open WebOS < 1.5
+			ua.match( /webOS\/1\.[0-4]/ ) ||
+			// Rejects: Anything PlayStation based.
+			ua.match( /PlayStation/i ) ||
+			// Rejects: Any Symbian based browsers
+			ua.match( /SymbianOS|Series60/ ) ||
+			// Rejects: Any NetFront based browser
+			ua.match( /NetFront/ ) ||
+			// Rejects: Nokia's Ovi Browser
+			ua.match( /S40OviBrowser/ ) ||
+			// Rejects: MeeGo's browser
+			ua.match( /MeeGo/ ) ||
+			// Rejects: Google Glass browser (groks JS but UI is too limited)
+			( ua.match( /Glass/ ) && ua.match( /Android/ ) )
+		)
 	);
 }
 
