66d568958317bcc99321989398cb8d698d6479a0
Ori.livneh <ori@wikimedia.org>
2015-06-17 19:51:49 +0000

    Revert "StashEdit: Use postWithToken"
    This reverts commit 155ad712c3.
    
    Bug: T102863
    Change-Id: Ia30b535a44a3aa4d6e79f14bbccb6f4a1b065aa2

--- resources/src/mediawiki.action/mediawiki.action.edit.stash.js
+++ resources/src/mediawiki.action/mediawiki.action.edit.stash.js
@@ -11,6 +11,22 @@
 			data = {},
 			timer = null;
 
+		function stashEdit( token ) {
+			data = $form.serializeObject();
+
+			pending = api.post( {
+				action: 'stashedit',
+				token: token,
+				title: mw.config.get( 'wgPageName' ),
+				section: data.wpSection,
+				sectiontitle: '',
+				text: data.wpTextbox1,
+				contentmodel: data.model,
+				contentformat: data.format,
+				baserevid: data.parentRevId
+			} );
+		}
+
 		/* Has the edit body text changed since the last stashEdit() call? */
 		function isChanged() {
 			// Normalize line endings to CRLF, like $.fn.serializeObject does.
@@ -28,17 +44,7 @@
 				pending.abort();
 			}
 
-			data = $form.serializeObject();
-			pending = api.postWithToken( 'edit', {
-				action: 'stashedit',
-				title: mw.config.get( 'wgPageName' ),
-				section: data.wpSection,
-				sectiontitle: '',
-				text: data.wpTextbox1,
-				contentmodel: data.model,
-				contentformat: data.format,
-				baserevid: data.parentRevId
-			} );
+			api.getToken( 'edit' ).then( stashEdit );
 		}
 
 		function onKeyPress( e ) {
