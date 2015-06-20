96c93a8ccebbc912fe039d5a49d691817d69f59d
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2015-06-20 23:49:49 +0300

    maintenance/exportSites: Fix minor whitespace issue
    Opening parentheses should be followed by a single space.
    
    Change-Id: I9d73334c3c79bf5277248c56e0f146a91938b35c

--- maintenance/exportSites.php
+++ maintenance/exportSites.php
@@ -17,7 +17,7 @@ class ExportSites extends Maintenance {
 	public function __construct() {
 		$this->mDescription = 'Exports site definitions the sites table to XML file';
 
-		$this->addArg('file', 'A file to write the XML to (see docs/sitelist.txt). ' .
+		$this->addArg( 'file', 'A file to write the XML to (see docs/sitelist.txt). ' .
 			'Use "php://stdout" to write to stdout.', true
 		);
 
