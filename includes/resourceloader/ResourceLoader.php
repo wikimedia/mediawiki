a8d97deca75ec0a1944b691760aa33854956f5e1
Timo Tijhof <krinklemail@gmail.com>
2015-06-26 06:57:17 +0100

    resourceloader: Add timing metrics for key operations
    * Minification of css and js.
    
    * Total generation of module content.
      Includes:
      - Fetching/generating of content (files, wikipage, dynamic).
      - Pre/post processing (concat, less>css, cssjanus, ..)
    
    Change-Id: Id60e665dfb6280aa254116f0867b909afc6f878a

--- includes/resourceloader/ResourceLoader.php
+++ includes/resourceloader/ResourceLoader.php
@@ -215,11 +215,16 @@ class ResourceLoader implements LoggerAwareInterface {
 			}
 			$result = '';
 			try {
-				wfIncrStats( "resourceloader_cache.$filter.miss" );
+				$startTime = microtime( true );
+
 				$result = $this->applyFilter( $filter, $data );
 				if ( $options['cacheReport'] ) {
 					$result .= "\n/* cache key: $key */";
 				}
+
+				$stats = RequestContext::getMain()->getStats();
+				$stats->timing( "resourceloader_cache.$filter.miss", microtime( true ) - $startTime );
+
 				$cache->set( $key, $result );
 			} catch ( Exception $e ) {
 				MWExceptionHandler::logException( $e );
@@ -984,11 +989,20 @@ MESSAGE;
 			$states[$name] = 'missing';
 		}
 
+		$stats = RequestContext::getMain()->getStats();
+
 		// Generate output
 		$isRaw = false;
 		foreach ( $modules as $name => $module ) {
 			try {
+				$startTime = microtime( true );
+
 				$content = $module->getModuleContent( $context );
+
+				$timing = microtime( true ) - $startTime;
+				$statName = str_replace( '.', '_', $name );
+				$stats->timing( "resourceloader_build.all", $timing );
+				$stats->timing( "resourceloader_build.$statName", $timing );
 
 				// Append output
 				switch ( $context->getOnly() ) {
