514b05a7f78b17fc68957b0562caddcf67ea9117
mjbmr <mjbmri@gmail.com>
2015-06-27 17:13:33 +0000

    Add Southern Luri (luz)
    Change-Id: I1099cb846f029df850126e59306849b0eec88e7b

---
+++ languages/messages/MessagesLuz.php
@@ -0,0 +1,11 @@
+<?php
+/** Southern Luri (لئری جئنوٙڤی)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ */
+
+$fallback = 'fa';
+$rtl = true;
