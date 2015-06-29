b159c990e169935d92296f595405e194d3ec22e2
Fomafix <fomafix@googlemail.com>
2015-06-29 17:00:14 +0200

    Do not double decode HTML entities for IDs
    * in links (T103714)
    * in indicators (T104196)
    
    This change introduces a new option 'noentities' for Sanitizer::escapeId.
    When this option is set HTML entities in the string not get decoded.
    Use this option where entity decoding is not necessary or creates
    double decoded entities.
    
    Explicit decode HTML entities in non local autocomments. (T104311)
    
    Bug: T103714
    Bug: T104196
    Bug: T104311
    Change-Id: I3ea3af2395b6d8faa274000d54473438bb6bf809

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -1930,7 +1930,8 @@ class ApiMain extends ApiBase {
 
 			// Add an additional span with sanitized ID
 			if ( !$this->getConfig()->get( 'ExperimentalHtmlIds' ) ) {
-				$header = Html::element( 'span', [ 'id' => Sanitizer::escapeId( 'main/datatypes' ) ] ) .
+				$header =
+					Html::element( 'span', [ 'id' => Sanitizer::escapeId( 'main/datatypes', 'noentities' ) ] ) .
 					$header;
 			}
 			$help['datatypes'] .= Html::rawElement( 'h' . min( 6, $level ),
@@ -1952,7 +1953,8 @@ class ApiMain extends ApiBase {
 
 			// Add an additional span with sanitized ID
 			if ( !$this->getConfig()->get( 'ExperimentalHtmlIds' ) ) {
-				$header = Html::element( 'span', [ 'id' => Sanitizer::escapeId( 'main/credits' ) ] ) .
+				$header =
+					Html::element( 'span', [ 'id' => Sanitizer::escapeId( 'main/credits', 'noentities' ) ] ) .
 					$header;
 			}
 			$header = $this->msg( 'api-credits-header' )->parse();
--- includes/skins/BaseTemplate.php
+++ includes/skins/BaseTemplate.php
@@ -734,7 +734,7 @@ abstract class BaseTemplate extends QuickTemplate {
 			$out .= Html::rawElement(
 				'div',
 				[
-					'id' => Sanitizer::escapeId( "mw-indicator-$id" ),
+					'id' => Sanitizer::escapeId( "mw-indicator-$id", 'noentities' ),
 					'class' => 'mw-indicator',
 				],
 				$content
