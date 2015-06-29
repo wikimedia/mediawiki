f636cbca7d9c3ac51646db0026ff02633e3196d7
Mark Holmquist <mtraceur@member.fsf.org>
2015-06-29 09:41:05 -0500

    Add stash support for mw.Api.upload
    Bug: T103399
    Change-Id: I2be7d5d874861e4cc24897316cd4933eb85b2c8f

--- resources/src/mediawiki.api/mediawiki.api.upload.js
+++ resources/src/mediawiki.api/mediawiki.api.upload.js
@@ -6,6 +6,8 @@
 ( function ( mw, $ ) {
 	var nonce = 0,
 		fieldsAllowed = {
+			stash: true,
+			filekey: true,
 			filename: true,
 			comment: true,
 			text: true,
@@ -209,7 +211,7 @@
 				}
 			} );
 
-			if ( !filenameFound ) {
+			if ( !filenameFound && !data.stash ) {
 				return $.Deferred().reject( 'Filename not included in file data.' );
 			}
 
@@ -249,7 +251,7 @@
 				}
 			} );
 
-			if ( !filenameFound ) {
+			if ( !filenameFound && !data.stash ) {
 				return $.Deferred().reject( 'Filename not included in file data.' );
 			}
 
@@ -286,6 +288,46 @@
 			} );
 
 			return deferred.promise();
+		},
+
+		/**
+		 * Upload a file to the stash.
+		 * @param {File|HTMLInputElement} file
+		 * @param {Object} [data]
+		 * @return {jQuery.Promise}
+		 * @return {Function} return.finishStashUpload Call this function to finish the upload.
+		 * @return {Object} return.finishStashUpload data Additional data for the upload.
+		 */
+		uploadToStash: function ( file, data ) {
+			var filekey,
+				api = this;
+
+			if ( !data.filename ) {
+				return $.Deferred().reject( 'Filename not included in file data.' );
+			}
+
+			function finishUpload( moreData ) {
+				data = $.extend( data, moreData );
+				data.filekey = filekey;
+				data.action = 'upload';
+				data.format = 'json';
+
+				if ( !data.filename ) {
+					return $.Deferred().reject( 'Filename not included in file data.' );
+				}
+
+				return api.postWithEditToken( data );
+			}
+
+			return this.upload( file, { stash: true, filename: data.filename } ).then( function ( result ) {
+				if ( result && result.upload && result.upload.filekey ) {
+					filekey = result.upload.filekey;
+				} else if ( result && ( result.error || result.warning ) ) {
+					return $.Deferred().reject( result );
+				}
+
+				return finishUpload;
+			} );
 		}
 	} );
 
