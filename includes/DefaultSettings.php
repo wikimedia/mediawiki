ac21576f441c1984e66fb3434d47bdd262ca6ef8
Tyler Cipriani <tcipriani@wikimedia.org>
2015-06-30 11:15:29 -0700

    Creating new WMF 1.26wmf12 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.26alpha';
+$wgVersion = '1.26wmf12';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
