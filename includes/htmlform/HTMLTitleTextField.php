daee0019869861301984d6b2abab64ba53f9ee40
Kunal Mehta <legoktm@gmail.com>
2015-06-30 22:27:23 -0700

    [WIP] HTMLForm: Add 'title' type
    HTMLTitleTextField will automatically validate title input, can
    optionally ensure the title is in a specific namespace, is creatable, or
    already exists.
    
    Bug: T104420
    Change-Id: I45718462570d0a523a148c3830b1116b634df050

---
+++ includes/htmlform/HTMLTitleTextField.php
@@ -0,0 +1,67 @@
+<?php
+
+use MediaWiki\Widget\TitleInputWidget;
+
+/**
+ * Implements a text input field for page titles.
+ * Automatically does validation that the title is valid,
+ * as well as autocompletion if using the OOUI display format.
+ *
+ * Optional parameters:
+ * 'namespace' - Namespace the page must be in
+ * 'creatable' - Whether to validate the title is creatable (not a special page)
+ * 'exists' - Whether to validate that the title already exists
+ *
+ * @since 1.26
+ */
+class HTMLTitleTextField extends HTMLTextField {
+	public function __construct( $params ) {
+		$params += array(
+			'namespace' => false,
+			'creatable' => false,
+			'exists' => false,
+		);
+
+		parent::__construct( $params );
+	}
+
+	public function validate( $value, $alldata ) {
+		try {
+			$title = Title::newFromTextThrow( $value );
+		} catch ( MalformedTitleException $e ) {
+			$msg = $this->msg( $e->getErrorMessage() );
+			$params = $e->getErrorMessageParameters();
+			if ( $params ) {
+				$msg->params( $params );
+			}
+			return $msg->parse();
+		}
+
+		if ( $this->mParams['namespace'] !== false && !$title->inNamespace( $this->mParams['namespace'] ) ) {
+			return $this->msg( 'htmlform-title-badnamespace' )->escaped();
+		}
+
+		if ( $this->mParams['creatable'] && !$title->canExist() ) {
+			return $this->msg( 'htmlform-title-not-creatable' )->escaped();
+		}
+
+		if ( $this->mParams['exists'] && !$title->exists() ) {
+			return $this->msg( 'htmlform-title-not-exists', $title->getPrefixedText() )->parse();
+		}
+
+		return parent::validate( $value, $alldata );
+	}
+
+	protected function getInputWidget( $params ) {
+		if ( isset( $params['readOnly'] ) ) {
+			// Read-only, no point in adding extra JS
+			return parent::getInputWidget( $params );
+		}
+		$this->mParent->getOutput()->addModules( 'mediawiki.widgets' );
+		if ( $this->mParams['namespace'] !== false ) {
+			$params['namespace'] = $this->mParams['namespace'];
+		}
+		$params['relative'] = false;
+		return new TitleInputWidget( $params );
+	}
+}
