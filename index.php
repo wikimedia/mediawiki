05a607851e407d2f8ab8aa3c671bbdd5287f8131
paladox <thomasmulhall410@yahoo.com>
2015-07-05 12:42:28 +0000

    Fix php check
    * This fixes the php check when starting mediawiki.
    
    * It would cause it to fail thinking you had incompatible php version.
    
    * This problem only happended if you had mutiple php version installed or
    * if you were on a shared hosting server with mutiple php installed to
    * choose from the check would think you had php 5.2 when you doint.
    
    Bug: T102094
    Change-Id: I7ae0369e4bab279d5e1bc754fe1a0bc8543bb2ed

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
 
 // Bail on old versions of PHP, or if composer has not been run yet to install
 // dependencies. Using dirname( __FILE__ ) here because __DIR__ is PHP5.3+.
+
 require_once dirname( __FILE__ ) . '/includes/PHPVersionCheck.php';
 wfEntryPointCheck( 'index.php' );
 
