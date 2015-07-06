02a038436165481d77a9f03e6932adbf59fa3842
Florian <florian.schmidt.welzow@t-online.de>
2015-07-06 19:48:31 +0200

    Use OOUI HTMLForm for Special:Watchlist
    Bug: T99256
    Change-Id: I47a8649208279a4090623a3088112fcff9abc4d3

---
+++ includes/htmlform/HTMLAdvancedSelectNamespaceWithButton.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Creates a Html::namespaceSelector input field with a button assigned to the input field.
+ */
+class HTMLAdvancedSelectNamespaceWithButton extends HTMLAdvancedSelectNamespace {
+	/** @var HTMLFormClassWithButton $mClassWithButton */
+	protected $mClassWithButton = null;
+
+	public function __construct( $info ) {
+		$this->mClassWithButton = new HTMLFormFieldWithButton( $info );
+		parent::__construct( $info );
+	}
+
+	public function getInputHTML( $value ) {
+		return $this->mClassWithButton->getElement( parent::getInputHTML( $value ) );
+	}
+
+	public function getInputOOUI( $value, $dataOnly = false ) {
+		return $this->mClassWithButton->getElementOOUI( parent::getInputOOUI( $value, true ) );
+	}
+}
--- includes/htmlform/HTMLFormFieldWithButton.php
+++ includes/htmlform/HTMLFormFieldWithButton.php
@@ -18,6 +18,9 @@ class HTMLFormFieldWithButton extends HTMLFormField {
 	/** @var string $mButtonType Value for the button in this field */
 	protected $mButtonValue;
 
+	/** @var string $mButtonType Value for the button in this field */
+	protected $mButtonFlags = array( 'primary', 'progressive' );
+
 	public function __construct( $info ) {
 		if ( isset( $info['buttonclass'] ) ) {
 			$this->mButtonClass = $info['buttonclass'];
@@ -34,6 +37,9 @@ class HTMLFormFieldWithButton extends HTMLFormField {
 		if ( isset( $info['buttontype'] ) ) {
 			$this->mButtonType = $info['buttontype'];
 		}
+		if ( isset( $info['buttonflags'] ) ) {
+			$this->mButtonFlags = $info['buttonflags'];
+		}
 		parent::__construct( $info );
 	}
 
@@ -46,6 +52,15 @@ class HTMLFormFieldWithButton extends HTMLFormField {
 		return Html::input( $this->mButtonName, $this->mButtonValue, $this->mButtonType, $attr );
 	}
 
+	public function getInputOOUI( $value ) {
+		return new OOUI\ButtonInputWidget( array(
+			'label' => $this->mButtonValue,
+			'type' => $this->mButtonType,
+			'flags' => $this->mButtonFlags,
+			'icon' => 'check',
+		) );
+	}
+
 	/**
 	 * Combines the passed element with a button.
 	 * @param String $element Element to combine the button with.
@@ -53,5 +68,33 @@ class HTMLFormFieldWithButton extends HTMLFormField {
 	 */
 	public function getElement( $element ) {
 		return $element . '&#160;' . $this->getInputHTML( '' );
+	}
+
+	/**
+	 * Same as self::getElement() but for OOUI forms
+	 * @param String $element Element to combine the button with.
+	 * @return String
+	 */
+	public function getElementOOUI( $fields ) {
+		# otherwise, wrap the fields into a horizontal aligned widget
+		$horizontalAlignmentWidget = new OOUI\Widget( array(
+			'classes' => array( 'oo-ui-horizontal-widget' ),
+		) );
+		$fields[] = $this->getInputOOUI( '', true );
+
+		foreach ( $fields as $field ) {
+			# Adding content after the fact does not play well with
+			# infusability.  We should be using a proper Layout here.
+			$horizontalAlignmentWidget->appendContent( $field );
+		}
+
+		# $horizontalAlignmentWidget is not infusable because
+		# it manually added content after creation. If we embed it
+		# in an infusable FieldLayout (through HTMLFormField), it will (recursively)
+		# be made infusable. So protect the widget by wrapping it in a
+		# FieldLayout which isn't infusable (by returning a string, instead of the field itself,
+		# which will instruct HTMLFormField to wrap it into a HtmlSnippet Widget and a not
+		# not infusable FieldLayout.
+		return $horizontalAlignmentWidget->toString();
 	}
 }
