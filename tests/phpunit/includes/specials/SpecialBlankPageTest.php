0a2660ed5f1d17a5946d26d114c21f26c3e91029
addshore <addshorewiki@gmail.com>
2015-07-06 12:49:58 +0200

    Add SpecialBlankPageTest
    This uses SpecialPageTestBase and is proof
    that said class works as expected
    
    Change-Id: Ibbf3fdcb172d7c1c71491d27b534faa65fd9be63

---
+++ tests/phpunit/includes/specials/SpecialBlankPageTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @licence GNU GPL v2+
+ * @author Adam Shorland
+ */
+class SpecialBlankPageTest extends SpecialPageTestBase {
+
+	/**
+	 * Returns a new instance of the special page under test.
+	 *
+	 * @return SpecialPage
+	 */
+	protected function newSpecialPage() {
+		return new SpecialBlankpage();
+	}
+
+	public function testHasWikiMsg() {
+		list( $html, ) = $this->executeSpecialPage();
+		$this->assertContains( wfMessage( 'intentionallyblankpage' )->text(), $html );
+	}
+
+}
