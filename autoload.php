403995c6b441c2163ad67f9b3b2e12d6f55a4b32
umherirrender <umherirrender_de.wp@web.de>
2015-07-07 22:36:41 +0200

    Migrate move protect log to new log system
    Migrate the move protect log as first sub type of the protection log,
    because it does not have complex log parameter, which needs some way of
    handling/migration.
    It also keeps the gerrit change smaller and hopefully makes review
    easier.
    The other sub types of the protection log will be migrated in a later
    patch set.
    
    This allows use of gender on Special:Log. Old message is kept for use
    in IRC. A test was added to ensure an unchanged IRC message.
    
    Change-Id: I57b3bd8a7dc823acdbb56520d2364f5542283373

--- autoload.php
+++ autoload.php
@@ -933,6 +933,7 @@ $wgAutoloadLocalClasses = array(
 	'ProfilerXhprof' => __DIR__ . '/includes/profiler/ProfilerXhprof.php',
 	'Protect' => __DIR__ . '/maintenance/protect.php',
 	'ProtectAction' => __DIR__ . '/includes/actions/ProtectAction.php',
+	'ProtectLogFormatter' => __DIR__ . '/includes/logging/ProtectLogFormatter.php',
 	'ProtectedPagesPager' => __DIR__ . '/includes/specials/SpecialProtectedpages.php',
 	'ProtectedTitlesPager' => __DIR__ . '/includes/specials/SpecialProtectedtitles.php',
 	'ProtectionForm' => __DIR__ . '/includes/ProtectionForm.php',
