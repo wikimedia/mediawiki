93a8c6beb63eb6d44d692807571e320b21681f24
Timo Tijhof <krinklemail@gmail.com>
2015-07-08 19:56:31 +0100

    UserCSSPrefsModule: Remove redundant text-decoration:none rules
    By default user option 'underline' is '2' which means no skin override.
    
    Once a upon a time, none of the popular MediaWiki skins specified whether links should have
    underlines, and most browsers default to having an underline on links (when not hovering).
    
    As such, there was an exception added for languages where underlines cause text to be unreadable.
    
    Nowadays however all modern skins explicitly specify the intended design for links.
    Neither the default skin (Vector) nor other popular skins I checked have underlines by default.
    As such, this rule is redundant.
    
    If a skin comes along that does specify underlines by default, then these rules should be placed
    in that skins' stylesheet. Not in the HTML of all pages.
    
    Incidentally, this was the last code branch in ResourceLoaderUserCSSPrefsModule that is true by
    default. Which means from now on this <style> tag is automatically omitted from the HTML for
    logged-out users and users that haven't changed the 'underline' or 'editfont' preference.
    
    Implement the missing isKnownEmpty() method so that OutputPage does the automatic omission.
    
    To test the new text-decoration rule in mediawiki.skinning:
    * Change user preference language to "ar" and observe anchor links in the interface
      not having underlines on-hover.
    * Change $wgLanguageCode to "ar" and observe anchor links in the content not having
      underlines on-hover.
    
    Bug: T105313
    Change-Id: I94dda30fb80b30e9c12a74a8b09065da55681929

--- includes/resourceloader/ResourceLoaderUserCSSPrefsModule.php
+++ includes/resourceloader/ResourceLoaderUserCSSPrefsModule.php
@@ -50,14 +50,10 @@ class ResourceLoaderUserCSSPrefsModule extends ResourceLoaderModule {
 		// Build CSS rules
 		$rules = array();
 
-		// Underline: 2 = browser default, 1 = always, 0 = never
+		// Underline: 2 = skin default, 1 = always, 0 = never
 		if ( $options['underline'] < 2 ) {
 			$rules[] = "a { text-decoration: " .
 				( $options['underline'] ? 'underline' : 'none' ) . "; }";
-		} else {
-			# The scripts of these languages are very hard to read with underlines
-			$rules[] = 'a:lang(ar), a:lang(kk-arab), a:lang(mzn), ' .
-			'a:lang(ps), a:lang(ur) { text-decoration: none; }';
 		}
 		if ( $options['editfont'] !== 'default' ) {
 			// Double-check that $options['editfont'] consists of safe characters only
@@ -70,6 +66,15 @@ class ResourceLoaderUserCSSPrefsModule extends ResourceLoaderModule {
 			$style = CSSJanus::transform( $style, true, false );
 		}
 		return array( 'all' => $style );
+	}
+
+	/**
+	 * @param ResourceLoaderContext $context
+	 * @return bool
+	 */
+	public function isKnownEmpty( ResourceLoaderContext $context ) {
+		$styles = $this->getStyles( $context );
+		return isset( $styles['all'] ) && $styles['all'] === '';
 	}
 
 	/**
