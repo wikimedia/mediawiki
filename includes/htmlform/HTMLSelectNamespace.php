53b012ce2ae7cd09acec8ba2cdb7f52284532ce7
Bartosz Dziewoński <matma.rex@gmail.com>
2015-07-11 19:12:10 +0200

    OOUIHTMLForm: Implement HTMLSelectNamespace
    * Extracted some common code between this and Html::namespaceSelector
      into a new method Html::namespaceSelectorOptions().
    
    Change-Id: I5e97e5c661582f726153533ad00695b450caed46

--- includes/htmlform/HTMLSelectNamespace.php
+++ includes/htmlform/HTMLSelectNamespace.php
@@ -3,18 +3,40 @@
  * Wrapper for Html::namespaceSelector to use in HTMLForm
  */
 class HTMLSelectNamespace extends HTMLFormField {
-	function getInputHTML( $value ) {
-		$allValue = ( isset( $this->mParams['all'] ) ? $this->mParams['all'] : 'all' );
+	public function __construct( $params ) {
+		parent::__construct( $params );
+		$this->mAllValue = isset( $this->mParams['all'] ) ? $this->mParams['all'] : 'all';
+	}
 
+	function getInputHTML( $value ) {
 		return Html::namespaceSelector(
 			array(
 				'selected' => $value,
-				'all' => $allValue
+				'all' => $this->mAllValue
 			), array(
 				'name' => $this->mName,
 				'id' => $this->mID,
 				'class' => 'namespaceselector',
 			)
 		);
+	}
+
+	public function getInputOOUI( $value ) {
+		$namespaceOptions = Html::namespaceSelectorOptions( array( 'all' => $this->mAllValue ) );
+
+		$options = array();
+		foreach( $namespaceOptions as $id => $name ) {
+			$options[] = array(
+				'data' => (string)$id,
+				'label' => $name,
+			);
+		};
+
+		return new OOUI\DropdownInputWidget( array(
+			'options' => $options,
+			'value' => $value,
+			'name' => $this->mName,
+			'id' => $this->mID,
+		) );
 	}
 }
