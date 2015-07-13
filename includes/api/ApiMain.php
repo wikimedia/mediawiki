f9c89d281443b7f42dc476387362d5188c840c59
Ori.livneh <ori@wikimedia.org>
2015-07-13 20:27:40 +0000

    Revert "Revert Count API module instantiations and Hook runs"
    This reverts commit 12f2ab7f06.
    The underlying problem was fixed by I496ed74800.
    
    Change-Id: Iaf70e1ab16f7d1984153423a897174ea950021a0

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -1090,6 +1090,8 @@ class ApiMain extends ApiBase {
 		$this->checkAsserts( $params );
 
 		// Execute
+		$this->getContext()->getStats()->increment(
+			'api.modules.' . strtr( $module->getModulePath(), '+', '.' ) );
 		$module->execute();
 		Hooks::run( 'APIAfterExecute', array( &$module ) );
 
