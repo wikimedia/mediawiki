086686b59ab82923f153924305dc28ed4d278a7f
Bartosz Dziewoński <matma.rex@gmail.com>
2015-07-14 22:30:06 +0200

    Implement NamespaceInputWidget
    Co-Authored-By: Florian <florian.schmidt.welzow@t-online.de>
    Co-Authored-By: Bartosz Dziewoński <matma.rex@gmail.com>
    Change-Id: I5cbfa9d0f6a8641148ce476b7dbe65e9096b4485

---
+++ resources/src/mediawiki.widgets/mw.widgets.NamespaceInputWidget.js
@@ -0,0 +1,68 @@
+/*!
+ * MediaWiki Widgets - NamespaceInputWidget class.
+ *
+ * @copyright 2011-2015 MediaWiki Widgets Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+( function ( $, mw ) {
+
+	/**
+	 * Creates a mw.widgets.NamespaceInputWidget object.
+	 *
+	 * This is not a complete implementation and is not intended for public usage. It only exists
+	 * because of HTMLForm shenanigans.
+	 *
+	 * @class
+	 * @private
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 * @cfg {OO.ui.DropdownInputWidget} namespace Widget to include
+	 * @cfg {OO.ui.CheckboxInputWidget|null} invert Widget to include
+	 * @cfg {OO.ui.CheckboxInputWidget|null} associated Widget to include
+	 * @cfg {string|null} allValue Value for "all namespaces" option, if any
+	 */
+	mw.widgets.NamespaceInputWidget = function MwWidgetsNamespaceInputWidget( config ) {
+		// Parent constructor
+		OO.ui.Widget.call( this, config );
+
+		// Properties
+		this.namespace = config.namespace;
+		this.invert = config.invert;
+		this.associated = config.associated;
+		this.allValue = config.allValue;
+
+		// Events
+		config.namespace.connect( this, { change: 'updateCheckboxesState' } );
+
+		// Initialization
+		this.$element
+			.addClass( 'mw-widget-namespaceInputWidget' )
+			.append(
+				config.namespace.$element,
+				config.invert ? config.invert.$element : '',
+				config.associated ? config.associated.$element : ''
+			);
+		this.updateCheckboxesState();
+	};
+
+	/* Inheritance */
+
+	OO.inheritClass( mw.widgets.NamespaceInputWidget, OO.ui.Widget );
+
+	/* Methods */
+
+	/**
+	 * Update the disabled state of checkboxes when the value of namespace dropdown changes.
+	 */
+	mw.widgets.TitleInputWidget.prototype.updateCheckboxesState = function () {
+		if ( this.invert ) {
+			this.invert.getField().setDisabled( this.namespace.getValue() === this.allValue );
+		}
+		if ( this.associated ) {
+			this.associated.getField().setDisabled( this.namespace.getValue() === this.allValue );
+		}
+	};
+
+}( jQuery, mediaWiki ) );
