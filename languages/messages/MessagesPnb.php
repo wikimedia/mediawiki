8ed6a2dc05b9606fe36f333fc5e1636b094aab72
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-07-16 16:39:31 -0500

    Namespace names for Western Punjabi (pnb)
    Written at the Wikimania Hackathon 2015 with Khalid Mahmood.
    
    Change-Id: I8d349cc2aaf9e469afda5203b4c4bcbc7387af05

--- languages/messages/MessagesPnb.php
+++ languages/messages/MessagesPnb.php
@@ -19,3 +19,21 @@ $fallback8bitEncoding = 'windows-1256';
 
 $rtl = true;
 
+$namespaceNames = array(
+	NS_MEDIA            => 'میڈیا',
+	NS_SPECIAL          => 'خاص',
+	NS_TALK             => 'گل بات',
+	NS_USER             => 'ورتنوالا',
+	NS_USER_TALK        => 'ورتن گل بات',
+	NS_PROJECT_TALK     => 'ویونت گل بات',
+	NS_FILE             => 'فائل',
+	NS_FILE_TALK        => 'فائل گل بات',
+	NS_MEDIAWIKI        => 'میڈیا وکی',
+	NS_MEDIAWIKI_TALK   => 'میڈیاوکی گل بات',
+	NS_TEMPLATE         => 'سانچہ',
+	NS_TEMPLATE_TALK    => 'سانچہ گل بات',
+	NS_HELP             => 'ہتھونڈائی',
+	NS_HELP_TALK        => 'ہتھونڈائی گل بات',
+	NS_CATEGORY         => 'گٹھ',
+	NS_CATEGORY_TALK    => 'گٹھ گل بات',
+);
