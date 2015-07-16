26820c0179742809ab86bbee4b063229cfd6b37e
Brion Vibber <brion@pobox.com>
2015-07-16 11:31:51 -0500

    Work in progress: rework API imagerotate to use job queue
    In theory this should let us route rotation requests to the
    image scalers or transcode boxes to isolate possible performance
    issues with large files.
    
    Lightly tested via manual API hits. No UI yet.
    
    Note this changes the image rotation to happen asynchronously;
    you get a 'Queued' entry in the API result set and there is so
    far no way to get the status of a queued job.
    
    Switched from 'imagerotate' action to 'filetransform' action to
    be more flexible for future additions.
    
    Started adding a file_transform table to carry the transform info.
    Went ahead and included suitable fields for crop and trim operations
    which will also be useful.
    
    Also had to fix the base Bitmap::rotate implementation which was
    rotating in reverse (used for PNGs and when jpegtran is missing).
    
    Todo:
    * return row id with status output
    * add API method for querying status of pending items
    
    for later:
    * add some UI to trigger the transforms
    * add crop support
    * add trim support for timed media
    * add some sort of user-level notification for failures and successes?
    * HULK SMASH
    
    Bug: T35186
    Change-Id: I5d3ea105b2cf5d61845d57b686d6bf3121785554

---
+++ includes/media/transform/RotateFileTransform.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+class RotateFileTransform extends FileTransform {
+
+	/** @var int */
+	protected $rotation;
+
+	public function __construct( $title = null, $user = null, $rotation = null ) {
+		parent::__construct( $title, $user );
+		$this->op = 'rotate';
+		$this->rotation = intval( $rotation );
+	}
+
+	public function loadFromRow( stdClass $row ) {
+		parent::loadFromRow( $row );
+		$this->rotation = intval( $row->ft_rotate_deg );
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function insertFields() {
+		return array_merge( parent::insertFields(), array(
+			'ft_rotate_deg' => $this->rotation
+		) );
+	}
+
+	/**
+	 * @return Status
+	 */
+	public function isValid() {
+		$status = parent::isValid();
+		
+		if ( !( $this->rotation == 90 || $this->rotation == 180 || $this->rotation == 270 ) ) {
+			$status->fatal( 'file_transform_rotate_bad_value' );
+		}
+
+		if ( !( $this->file->getHandler() && $this->file->getHandler()->canRotate() ) ) {
+			$status->fatal( 'file_transform_rotate_cannot_rotate' );
+		}
+		return $status;
+	}
+
+	/*
+	 * @return Status
+	 */
+	protected function transform() {
+		$status = Status::newGood();
+
+		$srcPath = $this->file->getLocalRefPath();
+		if ( $srcPath === false ) {
+			$status->fatal( 'file_transform_file_missing' );
+			return $status;
+		}
+
+		$ext = strtolower( pathinfo( "$srcPath", PATHINFO_EXTENSION ) );
+		$tmpFile = TempFSFile::factory( 'rotate_', $ext );
+		$dstPath = $tmpFile->getPath();
+		$err = $this->file->getHandler()->rotate( $this->file, array(
+			"srcPath" => $srcPath,
+			"dstPath" => $dstPath,
+			"rotation" => $this->rotation
+		) );
+		if ( $err ) {
+			$status->fatal( 'file_transform_rotate_error' );
+			return $status;
+		}
+
+		$comment = wfMessage(
+			'rotate-comment'
+		)->numParams( $this->rotation )->inContentLanguage()->text();
+
+		$status->merge( $this->file->upload( $dstPath,
+			$comment, $comment, 0, false, false, $this->user ) );
+
+		return $status;
+	}
+}
