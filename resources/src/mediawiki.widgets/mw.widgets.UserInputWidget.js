712142cbc6be9a89ded9af9a4e6c0bee450b7df3
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-07-18 00:04:02 +0200

    Implement UserInputWidget in OOUI/MW Widgets
    To use OOUI for forms with user name autocomplete, the new widget
    UserInputWidget interacts like the jQuery pendant (working with css
    class "mw-autocomplete-user").
    
    It is also available in HTMLForm as "user".
    
    Example usage: Iaeff912e6437d6ebef0d5b1919ce8cf53a7fd5f1
    
    Change-Id: I9501c85f4288c255bbe3a5284e99b57b6169916f

---
+++ resources/src/mediawiki.widgets/mw.widgets.UserInputWidget.js
@@ -0,0 +1,131 @@
+/*!
+ * MediaWiki Widgets - UserInputWidget class.
+ *
+ * @copyright 2011-2015 MediaWiki Widgets Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+( function ( $, mw ) {
+	/**
+	 * Creates an mw.widgets.UserInputWidget object.
+	 *
+	 * @class
+	 * @extends OO.ui.TextInputWidget
+	 * @mixins OO.ui.mixin.LookupElement
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 * @cfg {number} [limit=10] Number of results to show
+	 */
+	mw.widgets.UserInputWidget = function MwWidgetsUserInputWidget( config ) {
+		var widget = this;
+
+		// Config initialization
+		config = config || {};
+
+		// Parent constructor
+		OO.ui.TextInputWidget.call( this, $.extend( {}, config, { autocomplete: false } ) );
+
+		// Mixin constructors
+		OO.ui.mixin.LookupElement.call( this, config );
+
+		// Properties
+		this.limit = config.limit || 10;
+
+		// Initialization
+		this.$element.addClass( 'mw-widget-userInputWidget' );
+		this.lookupMenu.$element.addClass( 'mw-widget-userInputWidget-menu' );
+	};
+
+	/* Inheritance */
+
+	OO.inheritClass( mw.widgets.UserInputWidget, OO.ui.TextInputWidget );
+
+	OO.mixinClass( mw.widgets.UserInputWidget, OO.ui.mixin.LookupElement );
+
+	/* Methods */
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.UserInputWidget.prototype.onLookupMenuItemChoose = function ( item ) {
+		this.closeLookupMenu();
+		this.setLookupsDisabled( true );
+		this.setValue( item.getData() );
+		this.setLookupsDisabled( false );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.UserInputWidget.prototype.focus = function () {
+		var retval;
+
+		// Prevent programmatic focus from opening the menu
+		this.setLookupsDisabled( true );
+
+		// Parent method
+		retval = OO.ui.TextInputWidget.prototype.focus.apply( this, arguments );
+
+		this.setLookupsDisabled( false );
+
+		return retval;
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.UserInputWidget.prototype.getLookupRequest = function () {
+		var req,
+			params,
+			inputValue = this.value,
+			promiseAbortObject = { abort: function () {
+				// Do nothing. This is just so OOUI doesn't break due to abort being undefined.
+			} };
+
+		params = {
+			action: 'query',
+			list: 'allusers',
+			// Prefix of list=allusers is case sensitive. Normalise first
+			// character to uppercase so that "fo" may yield "Foo".
+			auprefix: inputValue[0].toUpperCase() + inputValue.slice( 1 ),
+			aulimit: this.limit
+		};
+
+		req = new mw.Api().get( params );
+		promiseAbortObject.abort = req.abort.bind( req ); // todo: ew
+		return req;
+		//} ).promise( promiseAbortObject );
+	};
+
+	/**
+	 * Get lookup cache item from server response data.
+	 *
+	 * @method
+	 * @param {Mixed} data Response from server
+	 */
+	mw.widgets.UserInputWidget.prototype.getLookupCacheDataFromResponse = function ( data ) {
+		return data.query || {};
+	};
+
+	/**
+	 * Get list of menu items from a server response.
+	 *
+	 * @param {Object} data Query result
+	 * @returns {OO.ui.MenuOptionWidget[]} Menu items
+	 */
+	mw.widgets.UserInputWidget.prototype.getLookupMenuOptionsFromData = function ( data ) {
+		var users = data.allusers,
+			items = [];
+
+		for ( i = 0, len = users.length; i < len; i++ ) {
+			user = users[i] || {};
+			items.push( new OO.ui.MenuOptionWidget( {
+				label: user.name,
+				data: user.name
+			} ) );
+		}
+
+		return items;
+	};
+
+}( jQuery, mediaWiki ) );
