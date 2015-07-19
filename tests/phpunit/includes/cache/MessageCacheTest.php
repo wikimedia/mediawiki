a29e9e4b32e5200269036b66a319a605b4622315
Brian Wolff <bawolff+wn@gmail.com>
2015-07-19 03:02:57 -0600

    Add tests for 47e0f0c3 (MessageCache::normalizeKey())
    Change-Id: If8c88383ccfcfcf08d3d85e5648eb338f0d14268

--- tests/phpunit/includes/cache/MessageCacheTest.php
+++ tests/phpunit/includes/cache/MessageCacheTest.php
@@ -124,4 +124,24 @@ class MessageCacheTest extends MediaWikiLangTestCase {
 		);
 	}
 
+	/**
+	 * @dataProvider provideNormalizeKey
+	 */
+	public function testNormalizeKey( $key, $expected ) {
+		$actual = MessageCache::normalizeKey( $key );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function provideNormalizeKey() {
+		return array(
+			array( 'Foo', 'foo' ),
+			array( 'foo', 'foo' ),
+			array( 'fOo', 'fOo' ),
+			array( 'FOO', 'fOO' ),
+			array( 'Ćab', 'ćab' ),
+			array( 'ĆAB', 'ćAB' ),
+			array( 'ćab', 'ćab' ),
+			array( 'ćaB', 'ćaB' ),
+		);
+	}
 }
