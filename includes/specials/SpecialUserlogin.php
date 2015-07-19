a37f59c930b17951f9964fe2df88e26df71f3428
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-07-20 00:13:40 +0200

    Fix notice on Special:UserLogin
    Follow up: Ia81f7c52f08e8dcc73ac751432560c4077d4bd39
    
    Change-Id: Iee6cdb0f29348badad6fe629c82b1ed36c36e3f2

--- includes/specials/SpecialUserlogin.php
+++ includes/specials/SpecialUserlogin.php
@@ -1390,6 +1390,7 @@ class LoginForm extends SpecialPage {
 			: is_array( $wgPasswordResetRoutes ) && in_array( true, array_values( $wgPasswordResetRoutes ) );
 
 		$template->set( 'header', '' );
+		$template->set( 'formheader', '' );
 		$template->set( 'skin', $this->getSkin() );
 		$template->set( 'name', $this->mUsername );
 		$template->set( 'password', $this->mPassword );
