1fca170318fd1e6070410ad33e6d13c2158dac6e
Jack Phoenix <jack@countervandalism.net>
2015-07-30 00:27:36 +0300

    Add support for Finnish allative case via {{GRAMMAR:}}
    Probably nowhere near perfect, but it should allow us to create somewhat
    better translations and avoid repeating the word "user" in Finnish
    translations.
    
    Change-Id: I0d296df4532a5affffddb3d9c8e47b5cdc0bdff3

--- resources/src/mediawiki.language/languages/fi.js
+++ resources/src/mediawiki.language/languages/fi.js
@@ -1,10 +1,12 @@
 /*!
  * Finnish (Suomi) language functions
  * @author Santhosh Thottingal
+ * @author Jack Phoenix <jack@countervandalism.net>
  */
 
 mediaWiki.language.convertGrammar = function ( word, form ) {
-	var grammarForms, aou, origWord;
+	var grammarForms, aou, origWord, lastFiveCharacters, lastFourCharacters,
+		matches, lastCharacterIsNumeric, newLastFiveCharacters;
 
 	grammarForms = mediaWiki.language.getData( 'fi', 'grammarForms' );
 	if ( grammarForms && grammarForms[form] ) {
@@ -38,6 +40,42 @@ mediaWiki.language.convertGrammar = function ( word, form ) {
 			break;
 		case 'inessive':
 			word += ( aou ? 'ssa' : 'ssä' );
+			break;
+		case 'allative':
+			// Check for double consonants
+			// Regex borrowed from http://tartarus.org/martin/PorterStemmer/php.txt
+			// Needed to handle "special" cases like "Matti", "Pekka", etc.
+			// which need to become Matille and Pekalle, respectively, in
+			// allative
+			lastFiveCharacters = word.substr( -5 );
+			lastFourCharacters = word.substr( -4 );
+			matches = lastFiveCharacters.match( /[bcdfghjklmnpqrstvwxz]/ );
+			// If $word is something like "Test1234" or whatever (last
+			// characters being numbers), the allative form needs to contain
+			// the colon before the "lle" suffix.
+			// @see http://stackoverflow.com/questions/4114609/check-if-a-string-ends-with-a-number-in-php/4114619#4114619
+			lastCharacterIsNumeric = jQuery.isNumeric( word.substr( -1, 1 ) );
+			if ( matches ) {
+				// @see http://stackoverflow.com/questions/801545/how-to-replace-double-more-letters-to-a-single-letter/801571#801571
+				// Get the last five characters instead of the whole word
+				// to avoid fucking up compound words, since usually you
+				// should apply transformation only on the last word of
+				// a compound word.
+				// This is sorta stupid because this replace call runs
+				// even if lastFiveCharacters does *not* contain two
+				// consecutive instances of matches[0], but eh.
+				newLastFiveCharacters = lastFiveCharacters.replace( ( matches[0] + matches[0] ), matches[0] );
+				word = word.replace( lastFiveCharacters, newLastFiveCharacters );
+			}
+			if ( lastFourCharacters === 'neni' ) {
+				// Many Finnish surnames end in -nen, and they are handled
+				// a bit differently...
+				// The superfluous "i" comes from the vowel harmony flag stuff
+				// above
+				// This transforms "Matti Meikäläinen" into "Matti Meikäläiselle"
+				word = word.replace( lastFourCharacters, 'se' );
+			}
+			word += ( lastCharacterIsNumeric ? ':' : '' ) + 'lle';
 			break;
 		default:
 			word = origWord;
---
+++ tests/phpunit/languages/classes/LanguageFiTest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @file
+ */
+
+/** Tests for MediaWiki languages/classes/LanguageFi.php */
+class LanguageFiTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider providerGrammar
+	 * @covers Language::convertGrammar
+	 */
+	public function testGrammar( $word, $result, $case ) {
+		$this->assertEquals( $result, $this->getLang()->convertGrammar( $word, $case ) );
+	}
+
+	public static function providerGrammar() {
+		return array(
+			array(
+				'Wikipedia',
+				'Wikipedian',
+				'genitive'
+			),
+			array(
+				'Wikipedia',
+				'Wikipediasta',
+				'elative',
+			),
+			array(
+				'Wikipedia',
+				'Wikipediaa',
+				'partitive',
+			),
+			array(
+				'Wikipedia',
+				'Wikipediaan',
+				'illative',
+			),
+			array(
+				'Wikipedia',
+				'Wikipediassa',
+				'inessive',
+			),
+			array(
+				'Wikipedia',
+				'Wikipedialle',
+				'allative',
+			),
+			array(
+				'Matti Meikäläinen',
+				'Matti Meikäläiselle',
+				'allative',
+			),
+			array(
+				'Pekka',
+				'Pekalle',
+				'allative',
+			),
+			array(
+				'Saara',
+				'Saaralle',
+				'allative',
+			),
+			array(
+				'Jack Phoenix',
+				'Jack Phoenixille',
+				'allative',
+			),
+			array(
+				'Test123',
+				'Test123:lle',
+				'allative',
+			),
+			array(
+				'123',
+				'123:lle',
+				'allative',
+			),
+		);
+	}
+}
