b02f7b2e5f2b48b11ea10ffd2fa8d660a77761f7
Bryan Davis <bd808@wikimedia.org>
2015-07-30 11:24:07 -0600

    ObjectFactory: add support for setter injection
    Extend ObjectFactory::getObjectFromSpec() to support setter injection on
    created objects when the specification includes a 'calls' member.
    
    Bug: T107440
    Change-Id: Ie2ece2e9658dd2d895d3935da4dc2da8a0a316e2

--- tests/phpunit/includes/libs/ObjectFactoryTest.php
+++ tests/phpunit/includes/libs/ObjectFactoryTest.php
@@ -29,10 +29,17 @@ class ObjectFactoryTest extends PHPUnit_Framework_TestCase {
 			'args' => array( function() {
 				return 'unwrapped';
 			}, ),
+			'calls' => array(
+				'setter' => array( function() {
+					return 'unwrapped';
+				}, ),
+			),
 			'closure_expansion' => false,
 		) );
 		$this->assertInstanceOf( 'Closure', $obj->args[0] );
 		$this->assertSame( 'unwrapped', $obj->args[0]() );
+		$this->assertInstanceOf( 'Closure', $obj->setterArgs[0] );
+		$this->assertSame( 'unwrapped', $obj->setterArgs[0]() );
 	}
 
 	/**
@@ -44,25 +51,43 @@ class ObjectFactoryTest extends PHPUnit_Framework_TestCase {
 			'args' => array( function() {
 				return 'unwrapped';
 			}, ),
+			'calls' => array(
+				'setter' => array( function() {
+					return 'unwrapped';
+				}, ),
+			),
 			'closure_expansion' => true,
 		) );
 		$this->assertInternalType( 'string', $obj->args[0] );
 		$this->assertSame( 'unwrapped', $obj->args[0] );
+		$this->assertInternalType( 'string', $obj->setterArgs[0] );
+		$this->assertSame( 'unwrapped', $obj->setterArgs[0] );
 
 		$obj = ObjectFactory::getObjectFromSpec( array(
 			'class' => 'ObjectFactoryTest_Fixture',
 			'args' => array( function() {
 				return 'unwrapped';
 			}, ),
+			'calls' => array(
+				'setter' => array( function() {
+					return 'unwrapped';
+				}, ),
+			),
 		) );
 		$this->assertInternalType( 'string', $obj->args[0] );
 		$this->assertSame( 'unwrapped', $obj->args[0] );
+		$this->assertInternalType( 'string', $obj->setterArgs[0] );
+		$this->assertSame( 'unwrapped', $obj->setterArgs[0] );
 	}
 }
 
 class ObjectFactoryTest_Fixture {
 	public $args;
+	public $setterArgs;
 	public function __construct( /*...*/ ) {
 		$this->args = func_get_args();
+	}
+	public function setter( /*...*/ ) {
+		$this->setterArgs = func_get_args();
 	}
 }
