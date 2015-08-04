4d11bd19c8ed33193e97c7f616b0e62c9b77d928
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-08-04 11:02:47 -0700

    Produce monolog messages through kafka+avro
    This allows a logging channel to be configured to write
    directly to kafka. The Kafka handler for monolog needs a
    list of one of more kafka servers to query cluster metadata
    from. This should be able to use any monolog formatter,
    although some like JsonFormatter require you to disable
    formatBatch as Kafka protocol would prefer to encode each
    record independetly in the protocol.  This requires the
    https://github.com/nmred/kafka-php master branch as the kafka
    client dependency.
    
    Adds a new formatter which serializes to the apache avro
    format. This is a compact binary format which uses pre-
    defined schemas. This initial implementation is very simple
    and takes the plain schemas as a constructor argument.
    
    Adds a new option to MonologSpi to wrap handlers in a
    BufferHandler. This doesn't flush until the request shuts
    down and prevents any network requests in the logger from
    adding latency to web requests.
    
    The nmred/kafka-php library requires master due to an
    upstreamed patch that removed a dependency on a C library
    which allowed us to more simply use it.
    
    The necessary config is something like:
    
    array(
        'loggers' => array(
            'CirrusSearchRequests' => array(
                'handlers' => array( 'kafka' ),
            ),
        ),
        'handlers' => array(
            'kafka' => array(
                'factory' => '\\MediaWiki\\Logger\\Monolog\\KafkaHandler::factory',
                'args' => array( 'localhost:9092' ),
                'formatter' => 'avro',
                'buffer' => true,
            ),
        ),
        'formatters' => array(
            'avro' => array(
                'class' => '\\MediaWiki\\Logger\\Monolog\\AvroFormatter',
                'args' => array(
                    array(
                        'CirrusSearchRequests' => array(
                            'type' => 'record',
                            'name' => 'CirrusSearchRequests'
                            'fields' => array( ... )
                        ),
                    ),
                ),
            ),
        ),
    )
    
    Bug: T106256
    Change-Id: I6ee744b3e5306af0bed70811b558a543eed22840

---
+++ includes/debug/logger/monolog/KafkaHandler.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Logger\Monolog;
+
+use Kafka\MetaDataFromKafka;
+use Kafka\Produce;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+/**
+ * Log handler sends log events to a kafka server.
+ *
+ * Constructor options array arguments:
+ * * alias: map from monolog channel to kafka topic name. if no
+ *	  alias exists the topic "monolog_$channel" will be used
+ *
+ * @since 1.26
+ * @author Erik Bernhardson <ebernhardson@wikimedia.org>
+ * @copyright © 2015 Erik Bernhardson and Wikimedia Foundation.
+ */
+class KafkaHandler extends AbstractProcessingHandler {
+	// @var Produce Sends requests to kafka
+	protected $produce;
+
+	// @var array Optional handler configuration
+	protected $options;
+
+	// @var array Map from topic name to partition this request
+	//  produces to
+	protected $partitions = array();
+
+	/**
+	 * @param Produce $produce Kafka instance to produce through
+	 * @param array $options optional handler configuration
+	 * @param string $level The minimum logging level at which this handler
+	 *   will be triggered
+	 * @param bool $bubble Whether the messages that are handled can bubble up
+	 *   the stack or not
+	 */
+	public function __construct( Produce $produce, array $options, $level = Logger::DEBUG, $bubble = true ) {
+		parent::__construct( $level, $bubble );
+		$this->produce = $produce;
+		$this->options = array_merge( $options, array(
+			'alias' => array(), // map from monolog channel to kafka topic
+			'formatBatch' => true, // use when formatBatch does not return an array of records
+		) );
+	}
+
+	/**
+	 * Constructs the necessary support objects and returns a KafkaHandler
+	 * instance.
+	 *
+	 * @param string[] $kafkaServers
+	 * @param array $options
+	 * @return KafkaHandler
+	 */
+	public static function factory( $kafkaServers, array $options = array(), $level = Logger::DEBUG, $bubble = true ) {
+		$metadata = new MetaDataFromKafka( (array)$kafkaServers );
+		$produce = new Produce( $metadata );
+		return new self( $produce, $options, $level, $bubble );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function write( array $record ) {
+		$this->addMessages( $record['channel'], array( $record['formatted'] ) );
+		$this->produce->send();
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function handleBatch( array $records ) {
+		$topics = array();
+		foreach ( $records as $record ) {
+			if ( $record['level'] < $this->level ) {
+				continue;
+			}
+			$topics[$record['channel']][] = $this->processRecord( $record );
+		}
+
+		if (!empty( $topics ) ) {
+			$formatter = $this->getFormatter();
+			foreach ( $topics as $channel => $messages ) {
+				if ( $this->options['formatBatch'] ) {
+					$formatted = $formatter->formatBatch( $messages );
+				} else {
+					$formatted = array_map( array( $formatter, 'format' ), $messages );
+				}
+				$this->addMessages( $channel, $formatted );
+			}
+			$this->produce->send();
+		}
+	}
+
+	/**
+	 * @param string $topic Name of topic to get partition for
+	 * @return int The random partition to produce to for this request.
+	 */
+	protected function getRandomPartition( $topic ) {
+		if ( !isset( $this->partitions[$topic] ) ) {
+			$partitions = $this->produce->getAvailablePartitions( $topic );
+			$key = array_rand( $partitions );
+			$this->partitions[$topic] = $partitions[$key];
+		}
+		return $this->partitions[$topic];
+	}
+
+	/**
+	 * Adds records for a channel to $this->produce internal queue.
+	 *
+	 * @var string $topic Name of channel records are from
+	 * @var array $records List of records to append
+	 */
+	protected function addMessages( $channel, array $records ) {
+		if ( isset( $this->alias[$channel] ) ) {
+			$topic = $this->alias[$channel];
+		} else {
+			$topic = "monolog_$channel";
+		}
+		$this->produce->setMessages(
+			$topic,
+			$this->getRandomPartition( $topic ),
+			$records
+		);
+	}
+}
