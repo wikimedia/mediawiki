0a147b05e0913331383f8e90e0426064faf90d63
Timo Tijhof <krinklemail@gmail.com>
2015-08-05 19:55:53 -0700

    Replace native "document.write" with deprecated fallback
    In most browsers, calling "document.write" after the document is
    ready results in the document being re-created with the specified
    content (effectively replacing with blank page).
    
    While this is a browser issue, mitigate this by replacing the
    method with a fallback (wrapped in "mw.log.deprecate") that just
    appends the given HTML to the document body.
    
    Note that scripts do not execute when appended as HTML, they will
    become dormant elements. It'll work for raw content as a basic
    fallback, but it should become apparent to the end user that the
    code needs to be upgraded.
    
    Bug: T108139
    Change-Id: Ic9056d7a779f234a28ddad005fd4d76f2e750fcc
    (cherry picked from commit d861c6593a55c67fef5019a2f3289f17c0c8bd21)

--- resources/src/mediawiki.legacy/wikibits.js
+++ resources/src/mediawiki.legacy/wikibits.js
@@ -215,4 +215,10 @@
 	win.importScript = importScript;
 	win.importStylesheet = importStylesheet;
 
+	// Replace document.write with basic html parsing that appends
+	// to the <body> to avoid blanking pages. Added JavaScript will not run.
+	mw.log.deprecate( document, 'write', function ( html ) {
+		$( 'body' ).append( $.parseHTML( html ) );
+	}, 'Use jQuery or mw.loader.load instead.' );
+
 }( mediaWiki, jQuery ) );
