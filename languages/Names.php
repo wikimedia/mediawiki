afc25311b3fd1292d73fefb33dcd9261e6210ff2
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-08-09 16:27:09 +0300

    Add MessagesSdh.php for Southern Kurdish
    This language has 18% of core localization and it's being exported,
    but it also needs an entry in Names.php, and a definition of
    direction and fallback. Adding them in MessagesSdh.php.
    
    Change-Id: I47febc9252292d2bffa1aad5eb389c41c4c20450

--- languages/Names.php
+++ languages/Names.php
@@ -350,6 +350,7 @@
 	'sco' => 'Scots',       # Scots
 	'sd' => 'سنڌي',	# Sindhi
 	'sdc' => 'Sassaresu',	# Sassarese
+	'sdh' => 'کوردی خوارگ',	# Southern Kurdish
 	'se' => 'sámegiella',	# Northern Sami
 	'sei' => 'Cmique Itom',	# Seri
 	'ses' => 'Koyraboro Senni',	# Koyraboro Senni
