f834e8cab31e40c6289bbc5e5706e181617caebc
csteipp <csteipp@wikimedia.org>
2015-08-10 13:23:01 -0700

    Update Releasenotes and version number for 1.23.10
    Change-Id: Ibcb5605e99cbabbe4159b1131610cc8e9ab5e38f

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.23.9';
+$wgVersion = '1.23.10';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
