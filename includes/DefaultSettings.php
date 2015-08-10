e1b441650669cecd9509dde3c8b355a05127e66c
csteipp <csteipp@wikimedia.org>
2015-08-10 13:35:11 -0700

    Update Releasenotes and version number for 1.25.2
    Change-Id: Idb92283cf1fbd73d73fec98fbbc3e4e86e0de47e

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.25.1';
+$wgVersion = '1.25.2';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
