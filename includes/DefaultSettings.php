42144f0fd616ac344f224b16dbefdf07a3948e12
csteipp <csteipp@wikimedia.org>
2015-08-10 13:32:23 -0700

    Update Releasenotes and version number for 1.24.3
    Change-Id: I4dd24b10d7a484845d4b5a3d9e6eb68ca0e7a8f7

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * Using single quotes is, therefore, important here.
  * @since 1.2
  */
-$wgVersion = '1.24.2';
+$wgVersion = '1.24.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
