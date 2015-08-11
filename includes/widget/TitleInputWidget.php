21d07c4e9c044016d263eca1a18798e008c37bf3
Bartosz Dziewoński <matma.rex@gmail.com>
2015-08-11 21:13:56 +0200

    TitleInputWidget, UserInputWidget: Allow overriding 'infusable'
    Needed for Ice69df851137e3454ae2c9f4c75494b18cf8a75a.
    
    Change-Id: I34048dbd90dffa0409132946fb2864452ab93827

--- includes/widget/TitleInputWidget.php
+++ includes/widget/TitleInputWidget.php
@@ -24,7 +24,7 @@ class TitleInputWidget extends \OOUI\TextInputWidget {
 	 */
 	public function __construct( array $config = array() ) {
 		// Parent constructor
-		parent::__construct( array_merge( $config, array( 'infusable' => true ) ) );
+		parent::__construct( array_merge( array( 'infusable' => true ), $config ) );
 
 		// Properties, which are ignored in PHP and just shipped back to JS
 		if ( isset( $config['namespace'] ) ) {
