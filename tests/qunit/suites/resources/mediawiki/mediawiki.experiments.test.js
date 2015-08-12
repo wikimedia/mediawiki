1c2248c5cc4e51b8b717f605459acc23b6c0edbd
Sam Smith <git@samsmith.io>
2015-08-12 20:53:19 +0100

    Add the mediawiki.experiments module
    The module was extracted from the module of the same name in the
    MobileFrontend extension. The module provides a generic bucketing
    function - it accepts an experiment specification and a token - and
    doesn't have any side effects, i.e. the bucket isn't persisted to
    storage.
    
    This module will be used, at the very least, by the MobileFrontend,
    Gather, and QuickSurveys extensions.
    
    Bug: T109010
    Change-Id: Icf7f6fedf0c2deb5d5548c9e24456cc7a7c6a743

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.experiments.test.js
@@ -0,0 +1,63 @@
+( function ( mw ) {
+
+	var getBucket = mw.experiments.getBucket;
+
+	function createExperiment() {
+		return {
+			name: 'experiment',
+			enabled: true,
+			buckets: {
+				control: 0.25,
+				A: 0.25,
+				B: 0.25,
+				C: 0.25
+			}
+		};
+	}
+
+	QUnit.module( 'mediawiki.experiments' );
+
+	QUnit.test( 'getBucket( experiment, token )', 4, function ( assert ) {
+		var experiment = createExperiment(),
+			token = '123457890';
+
+		assert.equal(
+			getBucket( experiment, token ),
+			getBucket( experiment, token ),
+			'It returns the same bucket for the same experiment-token pair.'
+		);
+
+		// --------
+		experiment = createExperiment();
+		experiment.buckets = {
+			A: 0.314159265359
+		};
+
+		assert.equal(
+			'A',
+			getBucket( experiment, token ),
+			'It returns the bucket if only one is defined.'
+		);
+
+		// --------
+		experiment = createExperiment();
+		experiment.enabled = false;
+
+		assert.equal(
+			'control',
+			getBucket( experiment, token ),
+			'It returns "control" if the experiment is disabled.'
+		);
+
+		// --------
+		experiment = createExperiment();
+		experiment.buckets = {};
+
+		assert.equal(
+			'control',
+			getBucket( experiment, token ),
+			'It returns "control" if the experiment doesn\'t have any buckets.'
+		);
+	} );
+
+}( mediaWiki ) );
