6030aacee978141d1d124ab15885b82ec3321bf4
Aaron Schulz <aschulz@wikimedia.org>
2015-08-19 16:33:03 -0700

    Added $wgDataCenterId/$wgDataCenterRoles
    * This is used to set sticky DC cookies to avoid
      session replication lag (which also makes sure
      ChronologyProtector works)
    
    Bug: T91816
    Change-Id: I7bc2f8185a3c05cb3ca5ccc42d300eccffae48e1

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -504,6 +504,13 @@ class MediaWiki {
 		$factory->shutdown();
 
 		wfDebug( __METHOD__ . ' completed; all transactions committed' );
+
+		// Set a cookie to tell all CDN edge nodes to "stick" the user to the
+		// DC that handles this POST request (e.g. the "master" data center)
+		$request = $this->context->getRequest();
+		if ( $request->wasPosted() && $factory->hasOrMadeRecentMasterChanges() ) {
+			$request->response()->setCookie( 'UseDC', wfDataCenter(), time() + 10 );
+		}
 	}
 
 	/**
