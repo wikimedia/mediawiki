wikimedia/mediawiki

commit 343ef6fb5df2b63e76d83ed8cf6a06e914213fd4

Fix numerous undefined variables
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@91243 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

maintenance/fixSlaveDesync.php
@@ -124,7 +124,7 @@ class FixSlaveDesync extends Maintenance {
 			$masterIDs[] = $row->rev_id;
 		}
 
-		$res = $db->select( 'revision', array( 'rev_id' ), array( 'rev_page' => $pageID ), __METHOD__ );
+		$res = $dbw->select( 'revision', array( 'rev_id' ), array( 'rev_page' => $pageID ), __METHOD__ );
 		$slaveIDs = array();
 		foreach ( $res as $row ) {
 			$slaveIDs[] = $row->rev_id;
@@ -133,7 +133,7 @@ class FixSlaveDesync extends Maintenance {
 			$missingIDs = array_diff( $slaveIDs, $masterIDs );
 			if ( count( $missingIDs ) ) {
 				$this->output( "Found " . count( $missingIDs ) . " lost in master, copying from slave... " );
-				$dbFrom = $db;
+				$dbFrom = $dbw;
 				$found = true;
 				$toMaster = true;
 			} else {

maintenance/deleteOldRevisions.php
@@ -60,6 +60,7 @@ class DeleteOldRevisions extends Maintenance {
 		# Get "active" revisions from the page table
 		$this->output( "Searching for active revisions..." );
 		$res = $dbw->query( "SELECT page_latest FROM $tbl_pag{$pageIdClause}" );
+		$cur = array();
 		foreach ( $res as $row ) {
 			$cur[] = $row->page_latest;
 		}

maintenance/language/StatOutputs.php
@@ -53,6 +53,7 @@ class wikiStatsOutput extends statsOutput {
 		echo "For additional information on specific languages (the message names, the actual problems, etc.), run <code>php maintenance/language/checkLanguage.php --lang=foo</code>.\n\n";
 		echo 'English (en) is excluded because it is the default localization';
 		if( is_array( $wgDummyLanguageCodes ) ) {
+			$dummyCodes = array();
 			foreach( $wgDummyLanguageCodes as $dummyCode ) {
 				$dummyCodes[] = $wgContLang->getLanguageName( $dummyCode ) . ' (' . $dummyCode . ')';
 			}

maintenance/nukePage.php
@@ -56,6 +56,7 @@ class NukePage extends Maintenance {
 			# Get corresponding revisions
 			$this->output( "Searching for revisions..." );
 			$res = $dbw->query( "SELECT rev_id FROM $tbl_rev WHERE rev_page = $id" );
+			$revs = array();
 			foreach ( $res as $row ) {
 				$revs[] = $row->rev_id;
 			}

maintenance/purgeOldText.inc
@@ -44,6 +44,7 @@ function PurgeRedundantText( $delete = false ) {
 	# Get "active" text records from the archive table
 	echo( "Searching for active text records in archive table..." );
 	$res = $dbw->query( "SELECT DISTINCT ar_text_id FROM $tbl_arc" );
+	$cur = array();
 	foreach ( $res as $row ) {
 		$cur[] = $row->ar_text_id;
 	}

maintenance/storage/checkStorage.php
@@ -130,6 +130,7 @@ class CheckStorage {
 					// It's safe to just erase the old_flags field
 					if ( $fix ) {
 						$this->error( 'fixed', "Warning: old_flags set to 0", $id );
+						$dbw = wfGetDB( DB_MASTER );
 						$dbw->ping();
 						$dbw->update( 'text', array( 'old_flags' => '' ),
 							array( 'old_id' => $id ), $fname );

commit a7cb31718818113127d92f126f7355bf1c442632

Remove maintenance script fixSlaveDesync.php
Though this script was run on Wikimedia sites years ago (see T8399), there
are enough reasons to doubt it will be run again:

* There is a hardcoded maximum page_id value, which would have to be
  changed or removed before reuse.
* It scans the entire page table in a single SELECT query and stores all
  values of page_id and page_latest in a PHP array, which might not be
  feasible on a large wiki.
* It writes directly to slaves. In contrast, the manual page for
  pt-table-sync (from Percona Toolkit) says in general, "[...] it always
  makes the changes on the replication master, never the replication slave
  directly. This is in general the only safe way to bring a replica back
  in sync [...]".
* It only works on the page/revision/text tables. In contrast, pt-table-sync
  can work on any table having a primary key.
* It does try to detect whether revisions are missing on the master (instead
  of on the slave). However, this won't work because of a bug introduced in
  r91243 / bb1df74f8715 (it actually queries the master twice and puts the
  second result set in $slaveIDs).

Change-Id: I85c98821af308abf7dde8068d7cbca17d06b1362

RELEASE-NOTES-1.26
@@ -154,6 +154,7 @@ changes to languages because of Phabricator reports.
 * BREAKING CHANGE: mediawiki.legacy.ajax has been removed, following
   a lengthy deprecation period.
 * The ScopedPHPTimeout class was removed.
+* Removed maintenance script fixSlaveDesync.php.
 
 == Compatibility ==
 

autoload.php
@@ -448,7 +448,6 @@ $wgAutoloadLocalClasses = array(
 	'FixBug20757' => __DIR__ . '/maintenance/storage/fixBug20757.php',
 	'FixDoubleRedirects' => __DIR__ . '/maintenance/fixDoubleRedirects.php',
 	'FixExtLinksProtocolRelative' => __DIR__ . '/maintenance/fixExtLinksProtocolRelative.php',
-	'FixSlaveDesync' => __DIR__ . '/maintenance/fixSlaveDesync.php',
 	'FixTimestamps' => __DIR__ . '/maintenance/fixTimestamps.php',
 	'FixUserRegistration' => __DIR__ . '/maintenance/fixUserRegistration.php',
 	'ForeignAPIFile' => __DIR__ . '/includes/filerepo/file/ForeignAPIFile.php',

maintenance/fixSlaveDesync.php
@@ -1,246 +1,0 @@
-<?php
-/**
- * Fix erroneous page_latest values due to slave desynchronisation.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- *
- * @file
- * @ingroup Maintenance
- */
-
-require_once __DIR__ . '/Maintenance.php';
-
-/**
- * Maintenance script that fixes erroneous page_latest values
- * due to slave desynchronisation.
- *
- * @ingroup Maintenance
- */
-class FixSlaveDesync extends Maintenance {
-	/** @var array */
-	private $slaveIndexes;
-
-	public function __construct() {
-		parent::__construct();
-		$this->mDescription = "";
-	}
-
-	public function getDbType() {
-		return Maintenance::DB_ADMIN;
-	}
-
-	public function execute() {
-		$this->slaveIndexes = array();
-		$serverCount = wfGetLB()->getServerCount();
-		for ( $i = 1; $i < $serverCount; $i++ ) {
-			if ( wfGetLB()->isNonZeroLoad( $i ) ) {
-				$this->slaveIndexes[] = $i;
-			}
-		}
-
-		if ( $this->hasArg() ) {
-			$this->desyncFixPage( $this->getArg() );
-		} else {
-			$corrupt = $this->findPageLatestCorruption();
-			foreach ( $corrupt as $id => $dummy ) {
-				$this->desyncFixPage( $id );
-			}
-		}
-	}
-
-	/**
-	 * Find all pages that have a corrupted page_latest
-	 * @return array
-	 */
-	private function findPageLatestCorruption() {
-		$desync = array();
-		$n = 0;
-		$dbw = wfGetDB( DB_MASTER );
-		$masterIDs = array();
-		$res = $dbw->select(
-			'page',
-			array( 'page_id', 'page_latest' ),
-			array( 'page_id<6054123' ),
-			__METHOD__
-		);
-		$this->output( "Number of pages: " . $res->numRows() . "\n" );
-		foreach ( $res as $row ) {
-			$masterIDs[$row->page_id] = $row->page_latest;
-			if ( !( ++$n % 10000 ) ) {
-				$this->output( "$n\r" );
-			}
-		}
-		$this->output( "\n" );
-
-		foreach ( $this->slaveIndexes as $i ) {
-			$db = wfGetDB( $i );
-			$res = $db->select(
-				'page',
-				array( 'page_id', 'page_latest' ),
-				array( 'page_id<6054123' ),
-				__METHOD__
-			);
-			foreach ( $res as $row ) {
-				if ( isset( $masterIDs[$row->page_id] ) && $masterIDs[$row->page_id] != $row->page_latest ) {
-					$desync[$row->page_id] = true;
-					$this->output( $row->page_id . "\t" );
-				}
-			}
-		}
-		$this->output( "\n" );
-
-		return $desync;
-	}
-
-	/**
-	 * Fix a broken page entry
-	 * @param int $pageID The page_id to fix
-	 */
-	private function desyncFixPage( $pageID ) {
-		# Check for a corrupted page_latest
-		$dbw = wfGetDB( DB_MASTER );
-		$dbw->begin( __METHOD__ );
-		$realLatest = $dbw->selectField( 'page', 'page_latest', array( 'page_id' => $pageID ),
-			__METHOD__, 'FOR UPDATE' );
-		# list( $masterFile, $masterPos ) = $dbw->getMasterPos();
-		$found = false;
-		foreach ( $this->slaveIndexes as $i ) {
-			$db = wfGetDB( $i );
-			/*
-			if ( !$db->masterPosWait( $masterFile, $masterPos, 10 ) ) {
-				$this->output( "Slave is too lagged, aborting\n" );
-				$dbw->commit( __METHOD__ );
-				sleep(10);
-				return;
-			}*/
-			$latest = $db->selectField( 'page', 'page_latest', array( 'page_id' => $pageID ), __METHOD__ );
-			$max = $db->selectField( 'revision', 'MAX(rev_id)', false, __METHOD__ );
-			if ( $latest != $realLatest && $realLatest < $max ) {
-				$this->output( "page_latest corrupted in page $pageID, server $i\n" );
-				$found = true;
-				break;
-			}
-		}
-		if ( !$found ) {
-			$this->output( "page_id $pageID seems fine\n" );
-			$dbw->commit( __METHOD__ );
-
-			return;
-		}
-
-		# Find the missing revisions
-		$res = $dbw->select( 'revision', array( 'rev_id' ), array( 'rev_page' => $pageID ),
-			__METHOD__, 'FOR UPDATE' );
-		$masterIDs = array();
-		foreach ( $res as $row ) {
-			$masterIDs[] = $row->rev_id;
-		}
-
-		$res = $dbw->select( 'revision', array( 'rev_id' ), array( 'rev_page' => $pageID ), __METHOD__ );
-		$slaveIDs = array();
-		foreach ( $res as $row ) {
-			$slaveIDs[] = $row->rev_id;
-		}
-		if ( count( $masterIDs ) < count( $slaveIDs ) ) {
-			$missingIDs = array_diff( $slaveIDs, $masterIDs );
-			if ( count( $missingIDs ) ) {
-				$this->output( "Found " . count( $missingIDs )
-					. " lost in master, copying from slave... " );
-				$dbFrom = $dbw;
-				$found = true;
-				$toMaster = true;
-			} else {
-				$found = false;
-			}
-		} else {
-			$missingIDs = array_diff( $masterIDs, $slaveIDs );
-			if ( count( $missingIDs ) ) {
-				$this->output( "Found " . count( $missingIDs )
-					. " missing revision(s), copying from master... " );
-				$dbFrom = $dbw;
-				$found = true;
-				$toMaster = false;
-			} else {
-				$found = false;
-			}
-		}
-
-		if ( $found ) {
-			foreach ( $missingIDs as $rid ) {
-				$this->output( "$rid " );
-				# Revision
-				$row = $dbFrom->selectRow( 'revision', '*', array( 'rev_id' => $rid ), __METHOD__ );
-				if ( $toMaster ) {
-					$id = $dbw->selectField( 'revision', 'rev_id', array( 'rev_id' => $rid ),
-						__METHOD__, 'FOR UPDATE' );
-					if ( $id ) {
-						$this->output( "Revision already exists\n" );
-						$found = false;
-						break;
-					} else {
-						$dbw->insert( 'revision', get_object_vars( $row ), __METHOD__, 'IGNORE' );
-					}
-				} else {
-					foreach ( $this->slaveIndexes as $i ) {
-						$db = wfGetDB( $i );
-						$db->insert( 'revision', get_object_vars( $row ), __METHOD__, 'IGNORE' );
-					}
-				}
-
-				# Text
-				$row = $dbFrom->selectRow( 'text', '*', array( 'old_id' => $row->rev_text_id ), __METHOD__ );
-				if ( $toMaster ) {
-					$dbw->insert( 'text', get_object_vars( $row ), __METHOD__, 'IGNORE' );
-				} else {
-					foreach ( $this->slaveIndexes as $i ) {
-						$db = wfGetDB( $i );
-						$db->insert( 'text', get_object_vars( $row ), __METHOD__, 'IGNORE' );
-					}
-				}
-			}
-			$this->output( "done\n" );
-		}
-
-		if ( $found ) {
-			$this->output( "Fixing page_latest... " );
-			if ( $toMaster ) {
-				/*
-				$dbw->update(
-					'page',
-					array( 'page_latest' => $realLatest ),
-					array( 'page_id' => $pageID ),
-					__METHOD__
-				);
-				*/
-			} else {
-				foreach ( $this->slaveIndexes as $i ) {
-					$db = wfGetDB( $i );
-					$db->update(
-						'page',
-						array( 'page_latest' => $realLatest ),
-						array( 'page_id' => $pageID ),
-						__METHOD__
-					);
-				}
-			}
-			$this->output( "done\n" );
-		}
-		$dbw->commit( __METHOD__ );
-	}
-}
-
-$maintClass = "FixSlaveDesync";
-require_once RUN_MAINTENANCE_IF_MAIN;