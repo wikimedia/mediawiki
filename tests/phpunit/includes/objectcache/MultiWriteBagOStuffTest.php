8c1c67cf28a1d83d6c97a5fc88198955e0c28858
Aaron Schulz <aschulz@wikimedia.org>
2015-08-20 23:53:52 -0700

    Support async writes to secondary MultiWriteBagOStuff stores
    * This is useful for ParserCache, as it tries to focus on memcached
      and use other caches (e.g. mariadb) for the long-tail of less used
      content, as setup on WMF. The class uses BagOStuff in a way that is
      compatible with this approach.
    
    Bug: T109751
    Change-Id: Ia64eb44a9b52a988fde27b468d604d9163bed4b4

---
+++ tests/phpunit/includes/objectcache/MultiWriteBagOStuffTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @group Database
+ */
+class MultiWriteBagOStuffTest extends MediaWikiTestCase {
+	/** @var HashBagOStuff */
+	private $cache1;
+	/** @var HashBagOStuff */
+	private $cache2;
+	/** @var MultiWriteBagOStuff */
+	private $cache;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->cache1 = new HashBagOStuff();
+		$this->cache2 = new HashBagOStuff();
+		$this->cache = new MultiWriteBagOStuff( array(
+			'caches' => array( $this->cache1, $this->cache2 ),
+			'replication' => 'async'
+		) );
+	}
+
+	public function testSetImmediate() {
+		$key = wfRandomString();
+		$value = wfRandomString();
+		$this->cache->set( $key, $value );
+
+		// Set in tier 1
+		$this->assertEquals( $this->cache1->get( $key ), $value );
+		// Set in tier 2
+		$this->assertEquals( $this->cache2->get( $key ), $value );
+	}
+
+	public function testSetDelayed() {
+		$key = wfRandomString();
+		$value = wfRandomString();
+
+		// XXX: DeferredUpdates bound to transactions in CLI mode
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->begin();
+		$this->cache->set( $key, $value );
+
+		// Set in tier 1
+		$this->assertEquals( $this->cache1->get( $key ), $value );
+		// Not yet set in tier 2
+		$this->assertEquals( $this->cache2->get( $key ), false );
+
+		$dbw->commit();
+
+		// Set in tier 2
+		$this->assertEquals( $this->cache2->get( $key ), $value );
+	}
+}
