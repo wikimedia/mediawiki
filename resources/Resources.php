c845586dc7a6b8d274a376433a42f7b5d4fd28ac
jdlrobson <jdlrobson@gmail.com>
2015-08-21 16:59:02 -0700

    Only load gallery styling rules when galleries are on the page
    * Double load styling rules in legacy modules so we have time for
    cached pages to catch up
    ** Double loading styles is acceptable for 30 days. There is no better way.
    * Load gallery css when gallery tag invoked.
    
    To test:
    * Visit a page with a gallery tag and purge it, note styles are present.
    * Visit a page without a gallery tag and purge it, note styles are not present
    
    Bug: T98878
    Change-Id: Ib1aef04dc4fece78e6615386ecaef6a9f368f49e

--- resources/Resources.php
+++ resources/Resources.php
@@ -1416,8 +1416,17 @@ return array(
 	'mediawiki.page.gallery' => array(
 		'scripts' => 'resources/src/mediawiki.page/mediawiki.page.gallery.js',
 		'dependencies' => array(
+			'mediawiki.page.gallery.styles',
 			'jquery.throttle-debounce',
 		)
+	),
+	'mediawiki.page.gallery.styles' => array(
+		'styles' => array(
+			'resources/src/mediawiki.page/mediawiki.page.gallery.print.css' => array( 'media' => 'print' ),
+			'resources/src/mediawiki.page/mediawiki.page.gallery.css',
+		),
+		'position' => 'top',
+		'targets' => array( 'desktop', 'mobile' ),
 	),
 	'mediawiki.page.ready' => array(
 		'scripts' => 'resources/src/mediawiki.page/mediawiki.page.ready.js',
@@ -1700,6 +1709,8 @@ return array(
 	'mediawiki.legacy.commonPrint' => array(
 		'position' => 'top',
 		'styles' => array(
+			// @todo: Remove mediawiki.page.gallery when cache has cleared
+			'resources/src/mediawiki.page/mediawiki.page.gallery.print.css' => array( 'media' => 'print' ),
 			'resources/src/mediawiki.legacy/commonPrint.css' => array( 'media' => 'print' )
 		),
 		'group' => 'print',
@@ -1713,6 +1724,8 @@ return array(
 	'mediawiki.legacy.shared' => array(
 		'position' => 'top',
 		'styles' => array(
+			// @todo: Remove when mediawiki.page.gallery in cached html.
+			'resources/src/mediawiki.page/mediawiki.page.gallery.css',
 			'resources/src/mediawiki.legacy/shared.css' => array( 'media' => 'screen' )
 		),
 	),
