aa7dd856ccbd89120a15d215980f73be07673f36
addshore <addshorewiki@gmail.com>
2015-08-24 17:40:06 +0000

    [WIP] Enable users to watch category membership changes (try #2)
    This reverts commit e412ff5ecc900991cce4f99b7a069f625a5694b3.
    
    TODO since version 1:
     - T109707 Touching a file on wikisource adds and
          removes it from a category... Even when page
          has no changes.... WTF?
     - T109759 Potential white / black list for category
          names to ignore?
    
    Changes since version 1:
     - T109604 - Page names in comment are no longer
          url encoded / have _'s
     - T109638 & T110338 - Reserved username now used
          when we can't determine a username for the change
          (we could perhaps set the user and id to be blank
          in the RC table, but who knows what this might do)
    - T109688 - History links are now disabled in RC....
          (could be fine for the introduction and worked
          on more in the future)
     - Categorization changes are now always patrolled
     - Touching on T109672 in this change emails will never
          be sent regarding categorization changes. (this
          can of course be changed in a followup)
     - Added $wgRCWatchCategoryMembership defaulting to true
          for enabling / disabling the feature
     - T109700 - for cases when no revision was retrieved
          for a category change set the bot flag to true.
          This means all changes caused by parser functions
          & Lua will be marked as bot, as will changes that
          cant find their revision due to slave lag..
    
    Bug: T9148
    Bug: T109604
    Bug: T109638
    Bug: T109688
    Bug: T109700
    Bug: T110338
    Bug: T110340
    Change-Id: I03516bb34144d95e5f25c46ae98ab70ce699b31b

---
+++ includes/changes/CategoryMembershipChange.php
@@ -0,0 +1,234 @@
+<?php
+/**
+ * Helper class for category membership changes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Kai Nissen
+ * @author Adam Shorland
+ * @since 1.26
+ */
+
+class CategoryMembershipChange {
+
+	const CATEGORY_ADDITION = 1;
+	const CATEGORY_REMOVAL = -1;
+
+	/**
+	 * @var string Current timestamp, set during CategoryMembershipChange::__construct()
+	 */
+	private $timestamp;
+
+	/**
+	 * @var Title Title instance of the categorized page
+	 */
+	private $pageTitle;
+
+	/**
+	 * @var WikiPage instance of the categorized page
+	 */
+	private $page;
+
+	/**
+	 * @var Revision|null Latest Revision instance of the categorized page
+	 */
+	private $revision;
+
+	/**
+	 * @var int
+	 * Number of pages this WikiPage is embedded by; set by CategoryMembershipChange::setRecursive()
+	 */
+	private $numTemplateLinks = 0;
+
+	/**
+	 * @var User|bool
+	 * instance of the user that created CategoryMembershipChange::$revision
+	 * false if something went wrong getting a user object
+	 */
+	private $user;
+
+	/**
+	 * @var null|RecentChange
+	 * RecentChange that is referred to in CategoryMembershipChange::$revision
+	 */
+	private $correspondingRC;
+
+	/**
+	 * @param Title $pageTitle Title instance of the categorized page
+	 * @param Revision $revision Latest Revision instance of the categorized page
+	 * @throws MWException
+	 */
+	public function __construct( Title $pageTitle, Revision $revision = null ) {
+		$this->pageTitle = $pageTitle;
+		$this->page = WikiPage::factory( $pageTitle );
+		$this->timestamp = wfTimestampNow();
+		$this->revision = $revision;
+		$this->user = $this->getUser();
+
+		# If no revision is given, the change was probably triggered by parser functions
+		if ( $this->revision !== null ) {
+			$this->correspondingRC = $this->revision->getRecentChange();
+		}
+	}
+
+	/**
+	 * Determines the number of template links for recursive link updates
+	 */
+	public function setRecursive() {
+		$this->numTemplateLinks = $this->pageTitle->getBacklinkCache()->getNumLinks( 'templatelinks' );
+	}
+
+	/**
+	 * Create a recentchanges entry for category additions
+	 * @param string $categoryName
+	 */
+	public function pageAddedToCategory( $categoryName ) {
+		$this->createRecentChangesEntry( $categoryName, self::CATEGORY_ADDITION );
+	}
+
+	/**
+	 * Create a recentchanges entry for category removals
+	 * @param string $categoryName
+	 */
+	public function pageRemovedFromCategory( $categoryName ) {
+		$this->createRecentChangesEntry( $categoryName, self::CATEGORY_REMOVAL );
+	}
+
+	/**
+	 * Create a recentchanges entry using RecentChange::notifyCategorization()
+	 * @param string $categoryName
+	 * @param int $type
+	 */
+	private function createRecentChangesEntry( $categoryName, $type ) {
+		$categoryTitle = Title::newFromText( $categoryName, NS_CATEGORY );
+		if ( !$categoryTitle ) {
+			return;
+		}
+
+		$previousRevTimestamp = $this->getPreviousRevisionTimestamp();
+
+		$lastRevId = 0;
+		$bot = 0;
+		$ip = '';
+		if ( $this->correspondingRC ) {
+			/**
+			 * T109700 - Default bot flag to true when there is no revision associated with the change
+			 * This means all changes caused by parser functions & Lua are marked as bot
+			 * Also in the case no revision could be found due to slave lag
+			 */
+			$bot = $this->correspondingRC->getAttribute( 'rc_bot' ) ?: 1;
+			$ip = $this->correspondingRC->getAttribute( 'rc_ip' ) ?: '';
+			$lastRevId = $this->correspondingRC->getAttribute( 'rc_last_oldid' ) ?: 0;
+		}
+
+		RecentChange::notifyCategorization(
+			$this->timestamp,
+			$categoryTitle,
+			$this->user,
+			$this->getChangeMessage( $type, array(
+				'prefixedUrl' => $this->page->getTitle()->getPrefixedText(),
+				'numTemplateLinks' => $this->numTemplateLinks
+			) ),
+			$this->pageTitle,
+			$lastRevId,
+			$this->revision ? $this->revision->getId() : 0,
+			$previousRevTimestamp,
+			$bot,
+			$ip,
+			$this->revision ? $this->revision->getVisibility() & Revision::SUPPRESSED_USER : 0
+		);
+	}
+
+	/**
+	 * Get the user associated with this change.
+	 *
+	 * If there is no revision associated with the change and thus no editing user
+	 * fallback to a default.
+	 *
+	 * False will be returned if the user name specified in the
+	 * 'autochange_username' message is invalid.
+	 *
+	 * @return User|bool
+	 */
+	private function getUser() {
+		if ( $this->revision ) {
+			$userId = $this->revision->getUser( Revision::RAW );
+			if ( $userId === 0 ) {
+				return User::newFromName( $this->revision->getUserText( Revision::RAW ), false );
+			} else {
+				return User::newFromId( $userId );
+			}
+		}
+
+		$username = wfMessage( 'autochange_username' )->inContentLanguage()->text();
+		$user = User::newFromName( $username );
+		# User::newFromName() can return false on a badly configured wiki.
+		if ( $user && !$user->isLoggedIn() ) {
+			$user->addToDatabase();
+		}
+
+		return $user;
+	}
+
+	/**
+	 * Returns the change message according to the type of category membership change
+	 *
+	 * The message keys created in this method may be one of:
+	 * - recentchanges-page-added-to-category
+	 * - recentchanges-page-added-to-category-bundled
+	 * - recentchanges-page-removed-from-category
+	 * - recentchanges-page-removed-from-category-bundled
+	 *
+	 * @param int $type may be CategoryMembershipChange::CATEGORY_ADDITION
+	 * or CategoryMembershipChange::CATEGORY_REMOVAL
+	 * @param array $params
+	 * - prefixedUrl: result of Title::->getPrefixedURL()
+	 * - numTemplateLinks
+	 * @return string
+	 */
+	private function getChangeMessage( $type, array $params ) {
+		$msgKey = 'recentchanges-';
+
+		switch ( $type ) {
+			case self::CATEGORY_ADDITION:
+				$msgKey .= 'page-added-to-category';
+				break;
+			case self::CATEGORY_REMOVAL:
+				$msgKey .= 'page-removed-from-category';
+				break;
+		}
+
+		if ( intval( $params['numTemplateLinks'] ) > 0 ) {
+			$msgKey .= '-bundled';
+		}
+
+		return wfMessage( $msgKey, $params )->inContentLanguage()->text();
+	}
+
+	/**
+	 * Returns the timestamp of the page's previous revision or null if the latest revision
+	 * does not refer to a parent revision
+	 * @return null|string
+	 */
+	private function getPreviousRevisionTimestamp() {
+		$latestRev = Revision::newFromId( $this->pageTitle->getLatestRevID() );
+		$previousRev = Revision::newFromId( $latestRev->getParentId() );
+
+		return $previousRev ? $previousRev->getTimestamp() : null;
+	}
+
+}
