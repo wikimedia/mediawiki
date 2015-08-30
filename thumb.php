c2306755852e9206c7b85221209138d586e8c0ee
Gergő Tisza <tgr.huwiki@gmail.com>
2015-08-29 17:13:04 -0700

    Log errors in thumb.php
    Add new streamFileWithStatus() methods to FileRepo and
    MediaTransformOutput that can be used to get more detailed error
    information on failure. The historic streamFile() methods become sinple
    wrappers to the new methods. Thumb.php is changed to use the
    streamFileWithStatus() methods so that failure reasons can be logged.
    
    Change-Id: I3088cde2044a7ff00841e53ca252d0b222c8b518

--- thumb.php
+++ thumb.php
@@ -21,6 +21,8 @@
  * @ingroup Media
  */
 
+use MediaWiki\Logger\LoggerFactory;
+
 define( 'MW_NO_OUTPUT_COMPRESSION', 1 );
 require __DIR__ . '/includes/WebStart.php';
 
@@ -262,7 +264,8 @@ function wfStreamThumb( array $params ) {
 		);
 		return;
 	} catch ( MWException $e ) {
-		wfThumbError( 500, $e->getHTML() );
+		wfThumbError( 500, $e->getHTML(), 'Exception caught while extracting thumb name',
+			array( 'exception' => $e ) );
 		return;
 	}
 
@@ -310,13 +313,14 @@ function wfStreamThumb( array $params ) {
 	$thumbPath = $img->getThumbPath( $thumbName );
 	if ( $img->getRepo()->fileExists( $thumbPath ) ) {
 		$starttime = microtime( true );
-		$success = $img->getRepo()->streamFile( $thumbPath, $headers );
+		$status = $img->getRepo()->streamFileWithStatus( $thumbPath, $headers );
 		$streamtime = microtime( true ) - $starttime;
 
-		if ( !$success ) {
-			wfThumbError( 500, 'Could not stream the file' );
-		} else {
+		if ( $status->isOK() ) {
 			RequestContext::getMain()->getStats()->timing( 'media.thumbnail.stream', $streamtime );
+		} else {
+			wfThumbError( 500, 'Could not stream the file', null, array( 'file' => $thumbName,
+				'path' => $thumbPath, 'error' => $status->getWikiText() ) );
 		}
 		return;
 	}
@@ -356,12 +360,13 @@ function wfStreamThumb( array $params ) {
 	}
 
 	if ( $errorMsg !== false ) {
-		wfThumbError( $errorCode, $errorMsg );
+		wfThumbError( $errorCode, $errorMsg, null, array( 'file' => $thumbName, 'path' => $thumbPath ) );
 	} else {
 		// Stream the file if there were no errors
-		$success = $thumb->streamFile( $headers );
-		if ( !$success ) {
-			wfThumbError( 500, 'Could not stream the file' );
+		$status = $thumb->streamFileWithStatus( $headers );
+		if ( !$status->isOK() ) {
+			wfThumbError( 500, 'Could not stream the file', null, array(
+				'file' => $thumbName, 'path' => $thumbPath, 'error' => $status->getWikiText() ) );
 		}
 	}
 }
@@ -579,9 +584,12 @@ function wfThumbErrorText( $status, $msgText ) {
  *
  * @param int $status
  * @param string $msgHtml HTML
+ * @param string $msgText Short error description, for internal logging. Defaults to $msgHtml.
+ *   Only used for HTTP 500 errors.
+ * @param array $context Error context, for internal logging. Only used for HTTP 500 errors.
  * @return void
  */
-function wfThumbError( $status, $msgHtml ) {
+function wfThumbError( $status, $msgHtml, $msgText = null, $context = array() ) {
 	global $wgShowHostnames;
 
 	header( 'Cache-Control: no-cache' );
@@ -592,6 +600,7 @@ function wfThumbError( $status, $msgHtml ) {
 		HttpStatus::header( 403 );
 		header( 'Vary: Cookie' );
 	} else {
+		LoggerFactory::getInstance( 'thumb' )->error( $msgText ?: $msgHtml, $context );
 		HttpStatus::header( 500 );
 	}
 	if ( $wgShowHostnames ) {
