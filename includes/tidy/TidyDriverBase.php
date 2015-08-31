c62731bd257cab0ec53659eb2b38af32d7eb0015
Tim Starling <tstarling@wikimedia.org>
2015-08-31 14:42:55 +1000

    Abstract and refactor Tidy support
    * Split tidy implementations into a class hierarchy
    * Bring all tidy configuration into a single associative array and
      deprecate the old configuration.
    * Remove $wgAlwaysUseTidy
    
    This is preparatory to replacement of Tidy (T89331). I used the name
    "Raggett" for things relating to Dave Raggett's Tidy, since if we use
    "tidy" to mean the new abstract system as well as Raggett's tidy, it
    gets confusing.
    
    Change-Id: I77af1a16cbbb47fc226d05fb9aad56c58e8910b5

---
+++ includes/tidy/TidyDriverBase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MediaWiki\Tidy;
+
+/**
+ * Base class for HTML cleanup utilities
+ */
+abstract class TidyDriverBase {
+	protected $config;
+
+	function __construct( $config ) {
+		$this->config = $config;
+	}
+
+	/**
+	 * Return true if validate() can be used
+	 */
+	public function supportsValidate() {
+		return false;
+	}
+
+	/**
+	 * Check HTML for errors, used if $wgValidateAllHtml = true.
+	 *
+	 * @param string $text
+	 * @param string &$errorStr Return the error string
+	 * @return bool Whether the HTML is valid
+	 */
+	public function validate( $text, &$errorStr ) {
+		throw new MWException( get_class( $this ) . " does not support validate()" );
+	}
+
+	/**
+	 * Clean up HTML
+	 *
+	 * @param string HTML document fragment to clean up
+	 * @param string The corrected HTML output
+	 */
+	public abstract function tidy( $text );
+}
