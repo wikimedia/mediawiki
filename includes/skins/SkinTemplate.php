47b2ef848f6d80950cd95884f4e0f792d3b6b6f0
Ori Livneh <ori@wikimedia.org>
2015-09-01 11:40:27 -0700

    Deprecate unconditional loading of mediawiki.ui.button on all pages
    Formally deprecate the unconditional loading of mediawiki.ui.button on all
    pages. During the deprecation period, the styles will be loaded on pages which
    contain 'mw-ui-button' in the page HTML. Starting with 1.28, the styles will
    only be loaded if explicitly depended upon.
    
    The rationale for the patch was expressed by Spage in a comment on PS4 of
    I835f3eb937: "Wikis have too many templates like
    [[commons:Template:Clickable_button]] dependent on jquery.ui.button that don't
    work if JavaScript isn't enabled.  mediawiki.ui.button is a lighter-weight
    piece of CSS that we own and can improve, and transitioning wiki content to it
    requires that we provide it." Mattflaschen commented that the added payload was
    "justifiable if we announce this widely and make an effort to get wikis to use this."
    
    A year and a half later, [[commons:Template:Clickable_button]] is still not
    using mediawiki.ui.button, and usage elsewhere is not sufficiently widespread
    to merit loading this stylesheet in the critical path.
    
    Change-Id: I290e1db422b7c7b9c1a026aae22e174580cefae9

--- includes/skins/SkinTemplate.php
+++ includes/skins/SkinTemplate.php
@@ -54,12 +54,18 @@ class SkinTemplate extends Skin {
 		$moduleStyles = array(
 			'mediawiki.legacy.shared',
 			'mediawiki.legacy.commonPrint',
-			'mediawiki.ui.button',
 			'mediawiki.sectionAnchor'
 		);
 		if ( $out->isSyndicated() ) {
 			$moduleStyles[] = 'mediawiki.feedlink';
 		}
+
+		// Deprecated since 1.26: Unconditional loading of mediawiki.ui.button
+		// on every page is deprecated. Express a dependency instead.
+		if ( strpos( $out->getHTML(), 'mw-ui-button' ) !== false ) {
+			$moduleStyles[] = 'mediawiki.ui.button';
+		}
+
 		$out->addModuleStyles( $moduleStyles );
 	}
 
