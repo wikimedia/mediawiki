462429ff4a9a82794babad500e05635560372f8b
Prateek Saxena <prtksxna@gmail.com>
2015-09-02 16:18:37 +0530

    Add mw.ForeignStructuredUpload.Dialog
    Change-Id: Ibf283fd185e191ed6ac8668efe6409f11e73f439

--- resources/Resources.php
+++ resources/Resources.php
@@ -1172,6 +1172,18 @@ return array(
 			'upload-dialog-label-usage-filename',
 		),
 	),
+	'mediawiki.ForeignStructuredUpload.Dialog' => array(
+		'scripts' => 'resources/src/mediawiki/mediawiki.ForeignStructuredUpload.Dialog.js',
+		'dependencies' => array(
+			'mediawiki.ForeignStructuredUpload',
+			'mediawiki.Upload.Dialog',
+			'mediawiki.widgets',
+		),
+		'messages' => array(
+			'foreign-structured-upload-dialog-label-infoform-categories',
+			'foreign-structured-upload-dialog-label-infoform-date'
+		),
+	),
 	'mediawiki.toc' => array(
 		'scripts' => 'resources/src/mediawiki/mediawiki.toc.js',
 		'dependencies' => 'mediawiki.cookie',
