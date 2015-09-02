6e39ad710050c842a6eb2813a9500cbb19fd1d14
Prateek Saxena <prtksxna@gmail.com>
2015-09-02 16:18:37 +0530

    Add mw.ForeignStructuredUpload.Dialog
    Bug: T91717
    Change-Id: Ibf283fd185e191ed6ac8668efe6409f11e73f439

--- resources/Resources.php
+++ resources/Resources.php
@@ -1172,6 +1172,21 @@ return array(
 			'upload-dialog-label-usage-filename',
 		),
 	),
+	'mediawiki.ForeignStructuredUpload.Dialog' => array(
+		'scripts' => 'resources/src/mediawiki/mediawiki.ForeignStructuredUpload.Dialog.js',
+		'dependencies' => array(
+			'mediawiki.ForeignStructuredUpload',
+			'mediawiki.Upload.Dialog',
+			'mediawiki.widgets',
+		),
+		'messages' => array(
+			'foreign-structured-upload-dialog-label-infoform-categories',
+			'foreign-structured-upload-dialog-label-infoform-date',
+			'foreign-structured-upload-dialog-label-infoform-legal',
+			'foreign-structured-upload-dialog-label-own-work-message',
+			'foreign-structured-upload-dialog-label-not-own-work-message',
+		),
+	),
 	'mediawiki.toc' => array(
 		'scripts' => 'resources/src/mediawiki/mediawiki.toc.js',
 		'dependencies' => 'mediawiki.cookie',
