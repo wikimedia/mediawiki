96fbd6e16b50d94228de2192b556c295f44b93b1
Aaron Schulz <aschulz@wikimedia.org>
2015-09-03 18:06:01 -0700

    PoolCounterRedis doc cleanups
    Change-Id: I0266941c40d30f631ae77695cbe81bbdc2a85a79

--- includes/poolcounter/PoolCounterRedis.php
+++ includes/poolcounter/PoolCounterRedis.php
@@ -76,7 +76,7 @@ class PoolCounterRedis extends PoolCounter {
 	const AWAKE_ONE = 1; // wake-up if when a slot can be taken from an existing process
 	const AWAKE_ALL = 2; // wake-up if an existing process finishes and wake up such others
 
-	/** @var array List of active PoolCounterRedis objects in this script */
+	/** @var PoolCounterRedis[] List of active PoolCounterRedis objects in this script */
 	protected static $active = null;
 
 	function __construct( $conf, $type, $key ) {
@@ -121,7 +121,6 @@ class PoolCounterRedis extends PoolCounter {
 	}
 
 	function acquireForMe() {
-
 		$status = $this->precheckAcquire();
 		if ( !$status->isGood() ) {
 			return $status;
@@ -131,7 +130,6 @@ class PoolCounterRedis extends PoolCounter {
 	}
 
 	function acquireForAnyone() {
-
 		$status = $this->precheckAcquire();
 		if ( !$status->isGood() ) {
 			return $status;
@@ -141,7 +139,6 @@ class PoolCounterRedis extends PoolCounter {
 	}
 
 	function release() {
-
 		if ( $this->slot === null ) {
 			return Status::newGood( PoolCounter::NOT_LOCKED ); // not locked
 		}
@@ -190,7 +187,7 @@ class PoolCounterRedis extends PoolCounter {
 		return 1
 LUA;
 		try {
-			$res = $conn->luaEval( $script,
+			$conn->luaEval( $script,
 				array(
 					$this->getSlotListKey(),
 					$this->getSlotRTimeSetKey(),
