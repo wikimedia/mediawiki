88da59d295a0a148f73246821f4aac6afeddd6cc
haritha28 <haritha.h20@gmail.com>
2015-09-05 11:15:30 +0530

    Edited the message : Line Length and Spacing
      The message which exceeded more than 80 characters
      are reduced. Spacing between sentences are provided.
    
      Bug: T100940
    
    Change-Id: I12d8d0fb4c93d9bef3573bcbe23384cbf3eb0031

--- includes/installer/LocalSettingsGenerator.php
+++ includes/installer/LocalSettingsGenerator.php
@@ -153,7 +153,7 @@ class LocalSettingsGenerator {
 
 		if ( count( $this->extensions ) ) {
 			$localSettings .= "
-# Enabled Extensions.The extensions can be enabled by adding the wfLoadExtensions('extensionname') command to the LocalSettings.php file
+# Enabled Extensions. Add the wfLoadExtensions('extensionname') command to the LocalSettings.php file
 # but check specific extension documentation for more details
 # The following extensions were automatically enabled:\n";
 
