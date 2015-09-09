1e21a4bcab1826cd95ca020cd14d52bac30f3c27
Brad Jorsch <bjorsch@wikimedia.org>
2015-09-09 11:32:37 -0400

    Fix TestingAccessWrapper private property access and parent classes
    PHP's reflection mechanism is weird: reflecting on a private method will
    find it even if it's defined in a parent class, while reflecting on a
    private property just fails.
    
    It would likely be more useful if TestingAccessWrapper could find
    private properties defined in parent classes, so let's make that happen.
    
    Change-Id: I9cfdde2694136d0e4559cc419a528762ea14ae4b

--- tests/phpunit/data/helpers/WellProtectedClass.php
+++ tests/phpunit/data/helpers/WellProtectedClass.php
@@ -1,18 +1,45 @@
 <?php
 
-class WellProtectedClass {
-	protected $property;
+class WellProtectedParentClass {
+	private $privateParentProperty;
 
 	public function __construct() {
+		$this->privateParentProperty = 9000;
+	}
+
+	private function incrementPrivateParentPropertyValue() {
+		$this->privateParentProperty++;
+	}
+
+	public function getPrivateParentProperty() {
+		return $this->privateParentProperty;
+	}
+}
+
+class WellProtectedClass extends WellProtectedParentClass {
+	protected $property;
+	private $privateProperty;
+
+	public function __construct() {
+		parent::__construct();
 		$this->property = 1;
+		$this->privateProperty = 42;
 	}
 
 	protected function incrementPropertyValue() {
 		$this->property++;
 	}
 
+	private function incrementPrivatePropertyValue() {
+		$this->privateProperty++;
+	}
+
 	public function getProperty() {
 		return $this->property;
+	}
+
+	public function getPrivateProperty() {
+		return $this->privateProperty;
 	}
 
 	protected function whatSecondArg( $a, $b = false ) {
--- tests/phpunit/includes/TestingAccessWrapperTest.php
+++ tests/phpunit/includes/TestingAccessWrapperTest.php
@@ -14,18 +14,36 @@ class TestingAccessWrapperTest extends MediaWikiTestCase {
 
 	function testGetProperty() {
 		$this->assertSame( 1, $this->wrapped->property );
+		$this->assertSame( 42, $this->wrapped->privateProperty );
+		$this->assertSame( 9000, $this->wrapped->privateParentProperty );
 	}
 
 	function testSetProperty() {
 		$this->wrapped->property = 10;
 		$this->assertSame( 10, $this->wrapped->property );
 		$this->assertSame( 10, $this->raw->getProperty() );
+
+		$this->wrapped->privateProperty = 11;
+		$this->assertSame( 11, $this->wrapped->privateProperty );
+		$this->assertSame( 11, $this->raw->getPrivateProperty() );
+
+		$this->wrapped->privateParentProperty = 12;
+		$this->assertSame( 12, $this->wrapped->privateParentProperty );
+		$this->assertSame( 12, $this->raw->getPrivateParentProperty() );
 	}
 
 	function testCallMethod() {
 		$this->wrapped->incrementPropertyValue();
 		$this->assertSame( 2, $this->wrapped->property );
 		$this->assertSame( 2, $this->raw->getProperty() );
+
+		$this->wrapped->incrementPrivatePropertyValue();
+		$this->assertSame( 43, $this->wrapped->privateProperty );
+		$this->assertSame( 43, $this->raw->getPrivateProperty() );
+
+		$this->wrapped->incrementPrivateParentPropertyValue();
+		$this->assertSame( 9001, $this->wrapped->privateParentProperty );
+		$this->assertSame( 9001, $this->raw->getPrivateParentProperty() );
 	}
 
 	function testCallMethodTwoArgs() {
