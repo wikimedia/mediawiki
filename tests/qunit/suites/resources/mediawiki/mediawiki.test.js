wikimedia/mediawiki

commit 6e903b94f2cc76bf27567584e44a34e221dbe001

resourceloader: Introduce new module state "executing"
No longer re-use "loading" to avoid confusion.

Code paths to 'execute' (such as in 'handlePending') already prevent
double execution by only allowing execution to be started when a module
is in state "loaded". So when execute takes ownership by progressing
the state to "executing" (or "loading"), no other code paths will enter
execute until execute itself sets the next state (ready or failed).

Change-Id: I35c8d2bc2da7135456c39d4b513661d2d11fb0f4

resources/src/mediawiki/mediawiki.js
@@ -743,7 +743,7 @@
 			 *    The module is known to the system but not yet requested.
 			 *    Meta data is registered via mw.loader#register. Calls to that method are
 			 *    generated server-side by the startup module.
-			 * - `loading` (1):
+			 * - `loading`:
 			 *    The module is requested through mw.loader (either directly or as dependency of
 			 *    another module). The client will be fetching module contents from the server.
 			 *    The contents are then stashed in the registry via mw.loader#implement.
@@ -751,9 +751,8 @@
 			 *    The module has been requested from the server and stashed via mw.loader#implement.
 			 *    If the module has no more dependencies in-fight, the module will be executed
 			 *    right away. Otherwise execution is deferred, controlled via #handlePending.
-			 * - `loading` (2):
+			 * - `executing`:
 			 *    The module is being executed.
-			 *    TODO: Create a new state instead of re-using the "loading" state.
 			 * - `ready`:
 			 *    The module has been successfully executed.
 			 * - `error`:
@@ -1192,21 +1191,11 @@
 				if ( !hasOwn.call( registry, module ) ) {
 					throw new Error( 'Module has not been registered yet: ' + module );
 				}
-				if ( registry[ module ].state === 'registered' ) {
-					throw new Error( 'Module has not been requested from the server yet: ' + module );
-				}
-				if ( registry[ module ].state === 'loading' ) {
-					throw new Error( 'Module has not completed loading yet: ' + module );
-				}
-				if ( registry[ module ].state === 'ready' ) {
-					throw new Error( 'Module has already been executed: ' + module );
+				if ( registry[ module ].state !== 'loaded' ) {
+					throw new Error( 'Module in state "' + registry[ module ].state + '" may not be executed: ' + module );
 				}
 
-				// This used to be inside runScript, but since that is now fired asychronously
-				// (after CSS is loaded) we need to set it here right away. It is crucial that
-				// when execute() is called this is set synchronously, otherwise modules will get
-				// executed multiple times as the registry will state that it isn't loading yet.
-				registry[ module ].state = 'loading';
+				registry[ module ].state = 'executing';
 
 				runScript = function () {
 					var script, markModuleReady, nestedAddScript, legacyWait,

tests/qunit/suites/resources/mediawiki/mediawiki.test.js
@@ -640,7 +640,7 @@
 				assert.strictEqual( isJsExecuted, undefined, 'script not executed multiple times' );
 				isJsExecuted = true;
 
-				assert.equal( mw.loader.getState( 'test.implement.import' ), 'loading', 'module state during implement() script execution' );
+				assert.equal( mw.loader.getState( 'test.implement.import' ), 'executing', 'module state during implement() script execution' );
 
 				$element = $( '<div class="mw-test-implement-import">Foo bar</div>' ).appendTo( '#qunit-fixture' );
 

commit d4356b7fac2635ab518568fe03ec7cf2ce89b15b

resourceloader: Jobs created in request() should wait for executing modules
Fix a regression from 6e903b94f2. It failed to update this code to account for
the new "executing" state.

This code was sometimes causing the job dependencies to be an empty array
when the module is question is currently being executed. As such the job
wasn't blocked by anything and happily run the next time a random module
invoked handlePending to run any jobs that have been satisfied.

Scenario:

* in-page RLQ: Page loads a module X that contains scripts and styles.
  - state=loading
* Module X is found in storage and implemented.
  - state=executing
* execute() adds styles to css buffer (which is asynchronous).
  It provides a callback from which it would run the module's script, set
  to "ready", and trigger handlePending().
* in-page RLQ: The edit page uses 'mw.loader.using' to require module X
  and has a callback that uses the module.
* using() sees X is not already "ready", so it calls request() for X.
* request() creates a job for "X". It then wrongly filters out X from the list
  of modules to wait for. The filter is intended to remove modules that are
  complete (e.g. "ready", "error", or "missing"). After 6e903b94f2, it also
  removed anything with state "executing". This resulted in a job with
  an empty dependency list (nothing to wait for).
* The callstack is finished. At some point the css buffer will call back.
  Some other module requested before X enters state "ready" and triggers
  handlePending(). It finds an left-over empty job with no dependencies
  and runs it. "It" being the callback of X – eventhough X isn't ready.

The added test fails without the change in mediawiki.js.

Bug: T112232
Change-Id: I3cc0c282e68a37b9b3256b213508362734161655

resources/src/mediawiki/mediawiki.js
@@ -1372,9 +1372,10 @@
 				// Add ready and error callbacks if they were given
 				if ( ready !== undefined || error !== undefined ) {
 					jobs[ jobs.length ] = {
+						// Narrow down the list to modules that are worth waiting for
 						dependencies: $.grep( dependencies, function ( module ) {
 							var state = mw.loader.getState( module );
-							return state === 'registered' || state === 'loaded' || state === 'loading';
+							return state === 'registered' || state === 'loaded' || state === 'loading' || state === 'executing';
 						} ),
 						ready: ready,
 						error: error

tests/qunit/suites/resources/mediawiki/mediawiki.test.js
@@ -925,6 +925,31 @@
 		mw.loader.load( target );
 	} );
 
+	QUnit.asyncTest( 'mw.loader() executing race (T112232)', 2, function ( assert ) {
+		var done = false;
+
+		// The red herring schedules its CSS buffer first. In T112232, a bug in the
+		// state machine would cause the job for testRaceLoadMe to run with an earlier job.
+		mw.loader.implement(
+			'testRaceRedHerring',
+			function () {},
+			{ css: [ '.mw-testRaceRedHerring {}' ] }
+		);
+		mw.loader.implement(
+			'testRaceLoadMe',
+			function () {
+				done = true;
+			},
+			{ css: [ '.mw-testRaceLoadMe { float: left; }' ] }
+		);
+
+		mw.loader.load( [ 'testRaceRedHerring', 'testRaceLoadMe' ] );
+		mw.loader.using( 'testRaceLoadMe', function () {
+			assert.strictEqual( done, true, 'script ran' );
+			assert.strictEqual( mw.loader.getState( 'testRaceLoadMe' ), 'ready', 'state' );
+		} ).always( QUnit.start );
+	} );
+
 	QUnit.test( 'mw.html', 13, function ( assert ) {
 		assert.throws( function () {
 			mw.html.escape();