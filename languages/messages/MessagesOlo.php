02ffb903fd5140d67d24dfdb6351c89db10a83de
Raimond Spekking <raimond.spekking@gmail.com>
2015-09-14 20:25:55 +0000

    Add language 'olo' Livvi-Karelian
    First export with I4893e65a430118dbe35f90144afa492899f90150
    
    Change-Id: I8b77182c0c09058d7361785bc1af0194bcc80ae6

---
+++ languages/messages/MessagesOlo.php
@@ -0,0 +1,12 @@
+<?php
+/** Livvi-Karelian (Livvinкarjala)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$linkTrail = '/^([a-zčČšŠžŽäÄöÖ]+)(.*)$/sDu';
+
