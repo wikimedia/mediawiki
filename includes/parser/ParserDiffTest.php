2ce100257d08676439ee897d9acc608a6317bf9f
Alangi Derick <alangiderick@gmail.com>
2015-09-14 10:47:28 +0100

    Deprecated function wfDiff() in GlobalFunctions.php
    Trying to fix the bugs about deprecated functions in GlobalFunctions.php. I have just fixed this one and wfDiff()
    is still called in different files. I will have to modify them too.
    
    Signed-off-by: Alangi Derick <alangiderick@gmail.com>
    
    Change-Id: I305fee1cb6ea7c68046a4d17bab22160f041cea4

--- includes/parser/ParserDiffTest.php
+++ includes/parser/ParserDiffTest.php
@@ -87,7 +87,9 @@ class ParserDiffTest
 				foreach ( $this->parsers as $i => $parser ) {
 					$resultsList[] = var_export( $results[$i], true );
 				}
-				$diff = wfDiff( $resultsList[0], $resultsList[1] );
+				$diffs = new Diff( explode( "\n", $resultsList[0] ), explode( "\n", $resultsList[1] ) );
+				$diff = new UnifiedDiffFormatter();
+				$format->format( $diff );
 			} else {
 				$diff = '[too many parsers]';
 			}
