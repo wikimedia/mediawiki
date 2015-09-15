a4d46ab17a38e985a403fe71bdc295e3da9803bd
Timo Tijhof <krinklemail@gmail.com>
2015-09-15 01:50:32 +0100

    resourceloader: Proxy 'window.performance' to support Safari bug
    Follows-up 5b0749d, which first introduced a call to performance.mark
    outside of startup.js – thus triggering this excotic bug in Safari.
    
    In Safari the native thread MAY (emphasis on MAY) decide at any
    point to recreate window.performance and blow away any previously
    succesfully attached properties.
    
    Bug: T112287
    Change-Id: I653e580566da5335d34f1cf1ef52f51ceee2cf5a

--- resources/src/mediawiki/mediawiki.js
+++ resources/src/mediawiki/mediawiki.js
@@ -2600,7 +2600,7 @@
 			} );
 		} );
 		$.when.apply( $, loading ).then( function () {
-			performance.mark( 'mwLoadEnd' );
+			mwPerformance.mark( 'mwLoadEnd' );
 			mw.hook( 'resourceloader.loadEnd' ).fire();
 		} );
 	} );
