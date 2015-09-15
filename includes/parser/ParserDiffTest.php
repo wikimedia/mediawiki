d882b4044495ccf3ca24f833ff77033514249a9a
Alangi Derick <alangiderick@gmail.com>
2015-09-15 14:25:39 +0100

    modification to the wfDiff() function call in the parser test
    Change-Id: I8aa71513544fba750ac13702432546eeb4e7ff61

--- includes/parser/ParserDiffTest.php
+++ includes/parser/ParserDiffTest.php
@@ -87,10 +87,9 @@ class ParserDiffTest
 				foreach ( $this->parsers as $i => $parser ) {
 					$resultsList[] = var_export( $results[$i], true );
 				}
-				//$diff = wfDiff( $resultsList[0], $resultsList[1] );
-				$diffs = new Diff( explode( "\n", $resultsList[0] ), explode( "\n", $resultsList[1] ) );
-				$diff = new UnifiedDiffFormatter();
-				$format->format( $diff );
+				$diff = new Diff( explode( "\n", $resultsList[0] ), explode( "\n", $resultsList[1] ) );
+				$format = new UnifiedDiffFormatter();
+				$format->format( $format );
 			} else {
 				$diff = '[too many parsers]';
 			}
