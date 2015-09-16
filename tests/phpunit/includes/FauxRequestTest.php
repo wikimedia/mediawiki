3ab5510ef751d5c9e720fb299dc0163cdf2b4d39
umherirrender <umherirrender_de.wp@web.de>
2015-09-16 18:35:37 +0200

    FauxRequest: Avoid header leak
    Actually override initHeaders to avoid init of the header, if no header
    was set.
    
    The tests was taken from I4c06369968e.
    Follows-up 578a0c082f.
    
    Change-Id: I86b2a9bae2f5938374fd9e5e46997c3fdfe87e39

--- tests/phpunit/includes/FauxRequestTest.php
+++ tests/phpunit/includes/FauxRequestTest.php
@@ -20,4 +20,32 @@ class FauxRequestTest extends MediaWikiTestCase {
 			array( 'text/plain', 'text/html' )
 		);
 	}
+
+	/**
+	 * @covers FauxRequest::getAllHeaders
+	 */
+	public function testGetAllHeaders() {
+		$_SERVER['HTTP_TEST'] = 'Example';
+
+		$request = new FauxRequest();
+
+		$this->assertEquals(
+			array(),
+			$request->getAllHeaders()
+		);
+	}
+
+	/**
+	 * @covers FauxRequest::getHeader
+	 */
+	public function testGetHeader() {
+		$_SERVER['HTTP_TEST'] = 'Example';
+
+		$request = new FauxRequest();
+
+		$this->assertEquals(
+			false,
+			$request->getHeader( 'test' )
+		);
+	}
 }
