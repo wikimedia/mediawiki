5f4d302d711abd92af95b086bd74fb7c8475a3d8
umherirrender <umherirrender_de.wp@web.de>
2015-09-19 20:50:37 +0200

    Add target and sender user objects to hooks in Special:Emailuser
    This would allow extensions to give hints to the correct user, because
    the email address is not unique when trying to re-lookup the correct
    user.
    
    Document the missing $error parameter of the EmailUser hook.
    
    Change-Id: I1c5636dc378667ef2798c69659b43f70734f4144

--- includes/specials/SpecialEmailuser.php
+++ includes/specials/SpecialEmailuser.php
@@ -312,9 +312,10 @@ class SpecialEmailUser extends UnlistedSpecialPage {
 			// Messages used here: notargettext, noemailtext, nowikiemailtext
 			return $context->msg( $target . 'text' )->parseAsBlock();
 		}
+		$sender = $context->getUser();
 
 		$to = MailAddress::newFromUser( $target );
-		$from = MailAddress::newFromUser( $context->getUser() );
+		$from = MailAddress::newFromUser( $sender );
 		$subject = $data['Subject'];
 		$text = $data['Text'];
 
@@ -324,7 +325,7 @@ class SpecialEmailUser extends UnlistedSpecialPage {
 			$from->name, $to->name )->inContentLanguage()->text();
 
 		$error = '';
-		if ( !Hooks::run( 'EmailUser', array( &$to, &$from, &$subject, &$text, &$error ) ) ) {
+		if ( !Hooks::run( 'EmailUser', array( &$to, &$from, &$subject, &$text, &$error, $target, $sender ) ) ) {
 			return $error;
 		}
 
@@ -369,12 +370,15 @@ class SpecialEmailUser extends UnlistedSpecialPage {
 			if ( $data['CCMe'] && $to != $from ) {
 				$cc_subject = $context->msg( 'emailccsubject' )->rawParams(
 					$target->getName(), $subject )->text();
-				Hooks::run( 'EmailUserCC', array( &$from, &$from, &$cc_subject, &$text ) );
+
+				// target and sender are equal, because this is the CC for the sender
+				Hooks::run( 'EmailUserCC', array( &$from, &$from, &$cc_subject, &$text, $sender, $sender ) );
+
 				$ccStatus = UserMailer::send( $from, $from, $cc_subject, $text );
 				$status->merge( $ccStatus );
 			}
 
-			Hooks::run( 'EmailUserComplete', array( $to, $from, $subject, $text ) );
+			Hooks::run( 'EmailUserComplete', array( $to, $from, $subject, $text, $target, $sender ) );
 
 			return $status;
 		}
