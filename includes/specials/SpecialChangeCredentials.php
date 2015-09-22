83fa501367cf52f1305dd6a5a445a5305f4e68a8
Gergő Tisza <gtisza@wikimedia.org>
2015-09-22 22:50:04 +0000

    Use AuthManager on special pages
    Rewrite authentication-related special pages to use AuthManager.
    All the changes mentioned below only take effect when
    $wgDisableAuthManager is false.
    
    LoginForm is rewritten to use HTMLForm and split into UserLogin
    and CreateAccount; ChangePassword and PasswordReset are rewritten;
    ChangeEmail and Preferences are updated. Four new special pages
    are added to handle the new capabilities of AuthManager (linked
    accounts, secondary authentication providers): LinkAccounts,
    UnlinkAccounts, ChangeCredentials, RemoveCredentials.
    
    The old form-based hooks (ChangePasswordForm, UserCreateForm,
    UserLoginForm) are deprecated. A new, more generic hook is
    available to alter the forms (AuthChangeFormFields);
    form changes that involve new fields should be done via
    $wgAuthManagerConfig.
    
    UserLoginComplete is limited to web-based login; for more
    generic functionality UserLoggedIn can be used instead.
    
    Hooks that assume password-based login (PrefsPasswordAudit,
    AbortChangePassword) are removed; the first functionality
    is replaced by ChangeAuthenticationDataAudit, the second is
    handled by AuthManager. LoginPasswordResetMessage is removed,
    the functionality can be recreated via authentication providers.
    
    There are several smaller backwards incompatible changes:
    * Adding fields to the login/signup forms by manipulating the
      template via the extraInput/extrafields parameters is not
      supported anymore. Depending on the authn configuration the
      login/signup process might be multistep and it would be
      complicated to ensure that extensions can access the data
      at the right moment. Instead, you can create an
      AuthenticationProvider which can define its own fields and
      process them when the authentication is over.
      (There is B/C support for a transitional period that  works with
      the default login form, but might break with configurations that
      require multiple steps or redirects.)
    * Removed cookie redirect check. This was added in 2003 in 9ead07fe9
      for the benefit of bots, but with MediaWiki having an API these days
      there is little reason to keep it. Same for the wpSkipCookieCheck
      flag (added in 2008 in 29c73e8265).
    * Instead of embedding a password field on sensitive special pages
      such as ChangeEmail, such pages rely on AuthManager for elevated
      security (which typically involves requiring the user to log in again
      unless their last login was more than a few minutes ago).
      Accordingly, wgRequirePasswordforEmailChange is removed.
    * Special:ChangePassword requires login now.
    * Special:ResetPassword now sends a separate email to each user when called
      with a shared email address.
    * the Reason field had a message with 'prefsectiontip' class
      which was sorta broken but used in extensions for formatting.
      HTMLForm does not support that, so this commit turns it into a help message
      which will break formatting. See https://gerrit.wikimedia.org/r/#/c/231884
    
    Bug: T110277
    Change-Id: I8b52ec8ddf494f23941807638f149f15b5e46b0c
    Depends-On: If4e0dfb6ee6674f0dace80a01850e2d0cbbdb47a

---
+++ includes/specials/SpecialChangeCredentials.php
@@ -0,0 +1,252 @@
+<?php
+
+use MediaWiki\Auth\AuthenticationRequest;
+use MediaWiki\Auth\AuthenticationResponse;
+use MediaWiki\Auth\AuthManager;
+use MediaWiki\Session\SessionManager;
+
+/**
+ * Special change to change credentials (such as the password).
+ *
+ * Also does most of the work for SpecialRemoveCredentials.
+ */
+class SpecialChangeCredentials extends AuthManagerSpecialPage {
+	protected static $allowedActions = [ AuthManager::ACTION_CHANGE ];
+
+	protected static $messagePrefix = 'changecredentials';
+
+	/** Change action needs user data; remove action does not */
+	protected static $loadUserData = true;
+
+	public function __construct( $name = 'ChangeCredentials' ) {
+		parent::__construct( $name, 'editmyprivateinfo' );
+	}
+
+	protected function getGroupName() {
+		return 'users';
+	}
+
+	public function isListed() {
+		$this->loadAuth( '' );
+		return (bool)$this->authRequests;
+	}
+
+	public function doesWrites() {
+		return true;
+	}
+
+	protected function getDefaultAction( $subPage ) {
+		return AuthManager::ACTION_CHANGE;
+	}
+
+	protected function getPreservedParams( $withToken = false ) {
+		$request = $this->getRequest();
+		$params = parent::getPreservedParams( $withToken );
+		$params += [
+			'returnto' => $request->getVal( 'returnto' ),
+			'returntoquery' => $request->getVal( 'returntoquery' ),
+		];
+		return $params;
+	}
+
+	public function onAuthChangeFormFields(
+		array $requests, array $fieldInfo, array &$formDescriptor, $action
+	) {
+		// This method is never called for remove actions.
+
+		$extraFields = [];
+		Hooks::run( 'ChangePasswordForm', [ &$extraFields ], '1.27' );
+		foreach ( $extraFields as $extra ) {
+			list( $name, $label, $type, $default ) = $extra;
+			$formDescriptor[$name] = [
+				'type' => $type,
+				'name' => $name,
+				'label-message' => $label,
+				'default' => $default,
+			];
+
+		}
+
+		return parent::onAuthChangeFormFields( $requests, $fieldInfo, $formDescriptor, $action );
+	}
+
+	public function execute( $subPage ) {
+		$this->setHeaders();
+		$this->outputHeader();
+
+		$this->loadAuth( $subPage );
+
+		if ( !$subPage ) {
+			$this->showSubpageList();
+			return;
+		}
+
+		if ( $this->getRequest()->getCheck( 'wpCancel' ) ) {
+			$returnUrl = $this->getReturnUrl() ?: Title::newMainPage()->getFullURL();
+			$this->getOutput()->redirect( $returnUrl );
+			return;
+		}
+
+		if ( !$this->authRequests ) {
+			// messages used: changecredentials-invalidsubpage, removecredentials-invalidsubpage
+			$this->showSubpageList( $this->msg( static::$messagePrefix . '-invalidsubpage', $subPage ) );
+			return;
+		}
+
+		$status = $this->trySubmit();
+
+		if ( $status === false || !$status->isOK() ) {
+			$this->displayForm( $status );
+			return;
+		}
+
+		$response = $status->getValue();
+
+		switch ( $response->status ) {
+			case AuthenticationResponse::PASS:
+				$this->success();
+				break;
+			case AuthenticationResponse::FAIL:
+				$this->displayForm( Status::newFatal( $response->message ) );
+				break;
+			default:
+				throw new LogicException( 'invalid AuthenticationResponse' );
+		}
+	}
+
+	protected function loadAuth( $subPage, $authAction = null, $reset = false ) {
+		parent::loadAuth( $subPage, $authAction );
+		if ( $subPage ) {
+			$this->authRequests = array_filter( $this->authRequests, function ( $req ) use ( $subPage ) {
+				return $req->getUniqueId() === $subPage;
+			} );
+			if ( count( $this->authRequests ) > 1 ) {
+				throw new LogicException( 'Multiple AuthenticationRequest objects with same ID!' );
+			}
+		}
+	}
+
+	protected function getAuthFormDescriptor( $requests, $action ) {
+		if ( !static::$loadUserData ) {
+			return [];
+		} else {
+			return parent::getAuthFormDescriptor( $requests, $action );
+		}
+	}
+
+	protected function getAuthForm( array $requests, $action ) {
+		$form = parent::getAuthForm( $requests, $action );
+		$req = reset( $requests );
+		$info = $req->describeCredentials();
+
+		$form->addPreText(
+			Html::openElement( 'dl' )
+			. Html::element( 'dt', [], wfMessage( 'credentialsform-provider' ) )
+			. Html::element( 'dd', [], $info['provider'] )
+			. Html::element( 'dt', [], wfMessage( 'credentialsform-account' ) )
+			. Html::element( 'dd', [], $info['account'] )
+			. Html::closeElement( 'dl' )
+		);
+
+		// messages used: changecredentials-submit removecredentials-submit
+		// changecredentials-submit-cancel removecredentials-submit-cancel
+		$form->setSubmitTextMsg( static::$messagePrefix . '-submit' );
+		$form->addButton( [
+			'name' => 'wpCancel',
+			'value' => $this->msg( static::$messagePrefix . '-submit-cancel' )->text()
+		] );
+
+		return $form;
+	}
+
+	protected function needsSubmitButton( $formDescriptor ) {
+		// Change/remove forms show are built from a single AuthenticationRequest and do not allow
+		// for redirect flow; they always need a submit button.
+		return true;
+	}
+
+	public function handleFormSubmit( $data ) {
+		// remove requests do not accept user input
+		$requests = $this->authRequests;
+		if ( static::$loadUserData ) {
+			$requests = AuthenticationRequest::loadRequestsFromSubmission( $this->authRequests, $data );
+		}
+
+		$response = $this->performAuthenticationStep( $this->authAction, $requests );
+
+		// we can't handle FAIL or similar as failure here since it might require changing the form
+		return Status::newGood( $response );
+	}
+
+	/**
+	 * @param Message|null $error
+	 */
+	protected function showSubpageList( $error = null ) {
+		$out = $this->getOutput();
+
+		if ( $error ) {
+			$out->addHTML( $error->parse() );
+		}
+
+		$groupedRequests = [];
+		foreach ( $this->authRequests as $req ) {
+			$info = $req->describeCredentials();
+			$groupedRequests[(string)$info['provider']][] = $req;
+		}
+
+		$out->addHTML( Html::openElement( 'dl' ) );
+		foreach ( $groupedRequests as $group => $members ) {
+			$out->addHTML( Html::element( 'dt', [], $group ) );
+			foreach ( $members as $req ) {
+				/** @var AuthenticationRequest $req */
+				$info = $req->describeCredentials();
+				$out->addHTML( Html::rawElement( 'dd', [],
+					Linker::link( $this->getPageTitle( $req->getUniqueId() ),
+						htmlspecialchars( $info['account'], ENT_QUOTES ) )
+				) );
+			}
+		}
+		$out->addHTML( Html::closeElement( 'dl' ) );
+	}
+
+	protected function success() {
+		$session = $this->getRequest()->getSession();
+		$user = $this->getUser();
+		$out = $this->getOutput();
+		$returnUrl = $this->getReturnUrl();
+
+		// change user token and update the session
+		SessionManager::singleton()->invalidateSessionsForUser( $user );
+		$session->setUser( $user );
+		$session->resetId();
+
+		if ( $returnUrl ) {
+			$out->redirect( $returnUrl );
+		} else {
+			// messages used: changecredentials-success removecredentials-success
+			$out->wrapWikiMsg( "<div class=\"successbox\">\n$1\n</div>", static::$messagePrefix
+				. '-success' );
+			$out->returnToMain();
+		}
+	}
+
+	/**
+	 * @return string|null
+	 */
+	protected function getReturnUrl() {
+		$request = $this->getRequest();
+		$returnTo = $request->getText( 'returnto' );
+		$returnToQuery = $request->getText( 'returntoquery', '' );
+
+		if ( !$returnTo ) {
+			return null;
+		}
+
+		$title = Title::newFromText( $returnTo );
+		return $title->getFullURL( $returnToQuery );
+	}
+
+	protected function getRequestBlacklist() {
+		return $this->getConfig()->get( 'ChangeCredentialsBlacklist' );
+	}
+}
