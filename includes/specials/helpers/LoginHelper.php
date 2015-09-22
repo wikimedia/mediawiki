f27bb630fc5cf67f48ec8cbd1086bfee9e8a1e87
Gergő Tisza <gtisza@wikimedia.org>
2015-09-22 22:50:04 +0000

    Use AuthManager on special pages
    Rewrite authentication-related special pages to use AuthManager.
    All the changes mentioned below only take effect when
    $wgDisableAuthManager is false.
    
    LoginForm is rewritten to use HTMLForm and split into UserLogin
    and CreateAccount; ChangePassword and PasswordReset are rewritten;
    ChangeEmail and Preferences are updated. Four new special pages
    are added to handle the new capabilities of AuthManager (linked
    accounts, secondary authentication providers): LinkAccounts,
    UnlinkAccounts, ChangeCredentials, RemoveCredentials.
    
    The old form-based hooks (ChangePasswordForm, UserCreateForm,
    UserLoginForm) are deprecated. A new, more generic hook is
    available to alter the forms (AuthChangeFormFields);
    form changes that involve new fields should be done via
    $wgAuthManagerConfig.
    
    UserLoginComplete is limited to web-based login; for more
    generic functionality UserLoggedIn can be used instead.
    
    Hooks that assume password-based login (PrefsPasswordAudit,
    AbortChangePassword) are removed; the first functionality
    is replaced by ChangeAuthenticationDataAudit, the second is
    handled by AuthManager. LoginPasswordResetMessage is removed,
    the functionality can be recreated via authentication providers.
    
    There are several smaller backwards incompatible changes:
    * Adding fields to the login/signup forms by manipulating the
      template via the extraInput/extrafields parameters is not
      supported anymore. Depending on the authn configuration the
      login/signup process might be multistep and it would be
      complicated to ensure that extensions can access the data
      at the right moment. Instead, you can create an
      AuthenticationProvider which can define its own fields and
      process them when the authentication is over.
      (There is B/C support for a transitional period that  works with
      the default login form, but might break with configurations that
      require multiple steps or redirects.)
    * Removed cookie redirect check. This was added in 2003 in 9ead07fe9
      for the benefit of bots, but with MediaWiki having an API these days
      there is little reason to keep it. Same for the wpSkipCookieCheck
      flag (added in 2008 in 29c73e8265).
    * Instead of embedding a password field on sensitive special pages
      such as ChangeEmail, such pages rely on AuthManager for elevated
      security (which typically involves requiring the user to log in again
      unless their last login was more than a few minutes ago).
      Accordingly, wgRequirePasswordforEmailChange is removed.
    * Special:ChangePassword requires login now.
    * Special:ResetPassword now sends a separate email to each user when called
      with a shared email address.
    * the Reason field had a message with 'prefsectiontip' class
      which was sorta broken but used in extensions for formatting.
      HTMLForm does not support that, so this commit turns it into a help message
      which will break formatting. See https://gerrit.wikimedia.org/r/#/c/231884
    
    Bug: T110277
    Change-Id: I8b52ec8ddf494f23941807638f149f15b5e46b0c
    Depends-On: If4e0dfb6ee6674f0dace80a01850e2d0cbbdb47a

---
+++ includes/specials/helpers/LoginHelper.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Helper functions for the login form that need to be shared with other special pages
+ * (such as CentralAuth's SpecialCentralLogin).
+ * @since 1.27
+ */
+class LoginHelper extends ContextSource {
+	public function __construct( IContextSource $context ) {
+		$this->setContext( $context );
+	}
+
+	/**
+	 * Show a return link or redirect to it.
+	 * Extensions can change where the link should point or inject content into the page
+	 * (which will change it from redirect to link mode).
+	 *
+	 * @param string $type One of the following:
+	 *    - error: display a return to link ignoring $wgRedirectOnLogin
+	 *    - success: display a return to link using $wgRedirectOnLogin if needed
+	 *    - successredirect: send an HTTP redirect using $wgRedirectOnLogin if needed
+	 * @param string $returnTo
+	 * @param array|string $returnToQuery
+	 * @param bool $stickHTTPS Keep redirect link on HTTPS
+	 */
+	public function showReturnToPage(
+		$type, $returnTo = '', $returnToQuery = '', $stickHTTPS = false
+	) {
+		global $wgRedirectOnLogin, $wgSecureLogin;
+
+		if ( $type !== 'error' && $wgRedirectOnLogin !== null ) {
+			$returnTo = $wgRedirectOnLogin;
+			$returnToQuery = [];
+		} elseif ( is_string( $returnToQuery ) ) {
+			$returnToQuery = wfCgiToArray( $returnToQuery );
+		}
+
+		// Allow modification of redirect behavior
+		Hooks::run( 'PostLoginRedirect', [ &$returnTo, &$returnToQuery, &$type ] );
+
+		$returnToTitle = Title::newFromText( $returnTo ) ?:  Title::newMainPage();
+
+		if ( $wgSecureLogin && !$stickHTTPS ) {
+			$options = [ 'http' ];
+			$proto = PROTO_HTTP;
+		} elseif ( $wgSecureLogin ) {
+			$options = [ 'https' ];
+			$proto = PROTO_HTTPS;
+		} else {
+			$options = [];
+			$proto = PROTO_RELATIVE;
+		}
+
+		if ( $type === 'successredirect' ) {
+			$redirectUrl = $returnToTitle->getFullURL( $returnToQuery, false, $proto );
+			$this->getOutput()->redirect( $redirectUrl );
+		} else {
+			$this->getOutput()->addReturnTo( $returnToTitle, $returnToQuery, null, $options );
+		}
+	}
+}
