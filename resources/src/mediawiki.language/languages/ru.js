ba7c4d4a5d39c950b0b5d7fa7c087b390dd93392
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-09-26 23:28:03 +0300

    Make grammar data loadable as an RL module and usable in JS
    * Create a new global, $wgGrammarTransformations, to store
      the grammar data. It does not replace $wgGrammarForms, which
      is based on simple word-to-word transformations (although it may
      replace it later).
    * Load the data of this variable from a JSON file to the same
      data structure that ResourceLoader uses for digitTransformTable,
      pluralRules, etc.
    * Change the JSON structure to ensure the order of the rules.
      Otherwise JavaScript processes the keys in a random order.
    * Delete the grammar code from JS and replace it with
      the same logic that is used in PHP for processing the data.
    
    For now this is done only for Russian.
    
    The next step will be to make the PHP and JS data processing
    logic reusable.
    
    Change-Id: I6b9b29b7017f958d62611671be017f97cee73415

--- resources/src/mediawiki.language/languages/ru.js
+++ resources/src/mediawiki.language/languages/ru.js
@@ -2,82 +2,37 @@
  * Russian (Русский) language functions
  */
 
-// These tests were originally made for names of Wikimedia
-// websites, so they don't currently cover all the possible
-// cases.
-
-mediaWiki.language.convertGrammar = function ( word, form ) {
-	/*global $ */
+mediaWiki.language.convertGrammar = function ( word, grammarCase ) {
 	'use strict';
 
-	var grammarForms = mediaWiki.language.getData( 'ru', 'grammarForms' );
-	if ( grammarForms && grammarForms[ form ] ) {
-		return grammarForms[ form ][ word ];
+	var grammarForms, grammarTransformations, i, rule, form, regexp, replacement;
+
+	grammarForms = mediaWiki.language.getData( 'ru', 'grammarForms' );
+	if ( grammarForms && grammarForms[ grammarCase ] ) {
+		return grammarForms[ grammarCase ][ word ];
 	}
-	switch ( form ) {
-		case 'genitive': // родительный падеж
-			if ( word.slice( -1 ) === 'ь' ) {
-				word = word.slice( 0, -1 ) + 'я';
-			} else if ( word.slice( -2 ) === 'ия' ) {
-				word = word.slice( 0, -2 ) + 'ии';
-			} else if ( word.slice( -2 ) === 'ка' ) {
-				word = word.slice( 0, -2 ) + 'ки';
-			} else if ( word.slice( -2 ) === 'ти' ) {
-				word = word.slice( 0, -2 ) + 'тей';
-			} else if ( word.slice( -2 ) === 'ды' ) {
-				word = word.slice( 0, -2 ) + 'дов';
-			} else if ( word.slice( -1 ) === 'д' ) {
-				word = word.slice( 0, -1 ) + 'да';
-			} else if ( word.slice( -3 ) === 'ные' ) {
-				word = word.slice( 0, -3 ) + 'ных';
-			} else if ( word.slice( -3 ) === 'ник' ) {
-				word = word.slice( 0, -3 ) + 'ника';
+
+	grammarTransformations = mediaWiki.language.getData( 'ru', 'grammarTransformations' );
+
+	if ( grammarTransformations[ grammarCase ] ) {
+		for ( i = 0; i < grammarTransformations[ grammarCase ].length; i++ ) {
+			rule = grammarTransformations[ grammarCase ][ i ];
+			form = rule[ 0 ];
+
+			if ( form === '@metadata' ) {
+				continue;
 			}
-			break;
-		case 'prepositional': // предложный падеж
-			if ( word.slice( -1 ) === 'ь' ) {
-				word = word.slice( 0, -1 ) + 'е';
-			} else if ( word.slice( -2 ) === 'ия' ) {
-				word = word.slice( 0, -2 ) + 'ии';
-			} else if ( word.slice( -2 ) === 'ка' ) {
-				word = word.slice( 0, -2 ) + 'ке';
-			} else if ( word.slice( -2 ) === 'ти' ) {
-				word = word.slice( 0, -2 ) + 'тях';
-			} else if ( word.slice( -2 ) === 'ды' ) {
-				word = word.slice( 0, -2 ) + 'дах';
-			} else if ( word.slice( -1 ) === 'д' ) {
-				word = word.slice( 0, -1 ) + 'де';
-			} else if ( word.slice( -3 ) === 'ные' ) {
-				word = word.slice( 0, -3 ) + 'ных';
-			} else if ( word.slice( -3 ) === 'ник' ) {
-				word = word.slice( 0, -3 ) + 'нике';
+
+			regexp = new RegExp( form );
+			replacement = rule[ 1 ];
+
+			if ( word.match( form ) ) {
+				word = word.replace( regexp, replacement );
+
+				break;
 			}
-			break;
-		case 'languagegen': // язык в родительном падеже ("(с) русского")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = word.slice( 0, -2 ) + 'ого';
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = word + 'а';
-			}
-			break;
-		case 'languageprep': // язык в предложном падеже ("(на) русском")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = word.slice( 0, -2 ) + 'ом';
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = word + 'е';
-			}
-			break;
-		case 'languageadverb': // наречие с названием языка ("по-русски")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = 'по-' + word.slice( 0, -1 );
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = 'на ' + word + 'е';
-			} else if ( $.inArray( word, [ 'идо', 'урду', 'хинди', 'эсперанто' ] ) > -1 ) {
-				word = 'на ' + word;
-			} else {
-				word = 'на языке ' + word;
-			}
-			break;
+		}
 	}
+
 	return word;
 };
