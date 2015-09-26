19bcbd8dc526a828947892fd483936c078fe51d2
Reedy <reedy@wikimedia.org>
2015-09-26 16:53:01 +0100

    Re-eanble Squiz.WhiteSpace.ScopeClosingBrace.Indent
    Change-Id: I743c4ce019321d631c0a11ee2112fe9aa8ba4c5d

--- tests/phpunit/includes/filerepo/MigrateFileRepoLayoutTest.php
+++ tests/phpunit/includes/filerepo/MigrateFileRepoLayoutTest.php
@@ -64,12 +64,12 @@ class MigrateFileRepoLayoutTest extends MediaWikiTestCase {
 
 	protected function deleteFilesRecursively( $directory ) {
 		foreach ( glob( $directory . '/*' ) as $file ) {
-	    	if ( is_dir( $file ) ) {
-	    		$this->deleteFilesRecursively( $file );
-	    	} else {
-	    		unlink( $file );
-	    	}
-	  	}
+			if ( is_dir( $file ) ) {
+				$this->deleteFilesRecursively( $file );
+			} else {
+				unlink( $file );
+			}
+		}
 
 	  	rmdir( $directory );
 	}
