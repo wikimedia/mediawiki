4efbb9aeb196d2990b419d9ac7e4a33a9b9bdb34
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-09-26 23:28:03 +0300

    Make grammar data loadable as an RL module and usable in JS
    * Load the data of this variable from a JSON file to the same
      data structure that ResourceLoader uses for digitTransformTable,
      pluralRules, etc.
    * Change the JSON structure to ensure the order of the rules.
      Otherwise JavaScript processes the keys in a random order.
    * Delete the grammar code from JS and replace it with
      the same logic that is used in PHP for processing the data.
    
    For now this is done only for Russian.
    
    The next step will be to make the PHP and JS
    data processing logic reusable.
    
    Bug: T115217
    Change-Id: I6b9b29b7017f958d62611671be017f97cee73415

--- resources/src/mediawiki.language/languages/ru.js
+++ resources/src/mediawiki.language/languages/ru.js
@@ -2,82 +2,37 @@
  * Russian (Русский) language functions
  */
 
-// These tests were originally made for names of Wikimedia
-// websites, so they don't currently cover all the possible
-// cases.
-
 mediaWiki.language.convertGrammar = function ( word, form ) {
-	/*global $ */
 	'use strict';
 
-	var grammarForms = mediaWiki.language.getData( 'ru', 'grammarForms' );
-	if ( grammarForms && grammarForms[ form ] ) {
-		return grammarForms[ form ][ word ];
+	var forms, transformations, i, rule, sourcePattern, regexp, replacement;
+
+	forms = mediaWiki.language.getData( 'ru', 'grammarForms' );
+	if ( forms && forms[ form ] ) {
+		return forms[ form ][ word ];
 	}
-	switch ( form ) {
-		case 'genitive': // родительный падеж
-			if ( word.slice( -1 ) === 'ь' ) {
-				word = word.slice( 0, -1 ) + 'я';
-			} else if ( word.slice( -2 ) === 'ия' ) {
-				word = word.slice( 0, -2 ) + 'ии';
-			} else if ( word.slice( -2 ) === 'ка' ) {
-				word = word.slice( 0, -2 ) + 'ки';
-			} else if ( word.slice( -2 ) === 'ти' ) {
-				word = word.slice( 0, -2 ) + 'тей';
-			} else if ( word.slice( -2 ) === 'ды' ) {
-				word = word.slice( 0, -2 ) + 'дов';
-			} else if ( word.slice( -1 ) === 'д' ) {
-				word = word.slice( 0, -1 ) + 'да';
-			} else if ( word.slice( -3 ) === 'ные' ) {
-				word = word.slice( 0, -3 ) + 'ных';
-			} else if ( word.slice( -3 ) === 'ник' ) {
-				word = word.slice( 0, -3 ) + 'ника';
-			}
-			break;
-		case 'prepositional': // предложный падеж
-			if ( word.slice( -1 ) === 'ь' ) {
-				word = word.slice( 0, -1 ) + 'е';
-			} else if ( word.slice( -2 ) === 'ия' ) {
-				word = word.slice( 0, -2 ) + 'ии';
-			} else if ( word.slice( -2 ) === 'ка' ) {
-				word = word.slice( 0, -2 ) + 'ке';
-			} else if ( word.slice( -2 ) === 'ти' ) {
-				word = word.slice( 0, -2 ) + 'тях';
-			} else if ( word.slice( -2 ) === 'ды' ) {
-				word = word.slice( 0, -2 ) + 'дах';
-			} else if ( word.slice( -1 ) === 'д' ) {
-				word = word.slice( 0, -1 ) + 'де';
-			} else if ( word.slice( -3 ) === 'ные' ) {
-				word = word.slice( 0, -3 ) + 'ных';
-			} else if ( word.slice( -3 ) === 'ник' ) {
-				word = word.slice( 0, -3 ) + 'нике';
-			}
-			break;
-		case 'languagegen': // язык в родительном падеже ("(с) русского")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = word.slice( 0, -2 ) + 'ого';
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = word + 'а';
-			}
-			break;
-		case 'languageprep': // язык в предложном падеже ("(на) русском")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = word.slice( 0, -2 ) + 'ом';
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = word + 'е';
-			}
-			break;
-		case 'languageadverb': // наречие с названием языка ("по-русски")
-			if ( word.slice( -3 ) === 'кий' ) {
-				word = 'по-' + word.slice( 0, -1 );
-			} else if ( $.inArray( word, [ 'иврит', 'идиш' ] ) > -1 ) {
-				word = 'на ' + word + 'е';
-			} else if ( $.inArray( word, [ 'идо', 'урду', 'хинди', 'эсперанто' ] ) > -1 ) {
-				word = 'на ' + word;
-			} else {
-				word = 'на языке ' + word;
-			}
-			break;
+
+	transformations = mediaWiki.language.getData( 'ru', 'grammarTransformations' );
+
+	if ( !transformations[ form ] ) {
+		return word;
 	}
+
+	for ( i = 0; i < transformations[ form ].length; i++ ) {
+		rule = transformations[ form ][ i ];
+		sourcePattern = rule[ 0 ];
+
+		if ( sourcePattern === '@metadata' ) {
+			continue;
+		}
+
+		regexp = new RegExp( sourcePattern );
+		replacement = rule[ 1 ];
+
+		if ( word.match( regexp ) ) {
+			return word.replace( regexp, replacement );
+		}
+	}
+
 	return word;
 };
