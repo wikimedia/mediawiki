8124cb5078b9ef6f46bd8fce81a435b178afca34
Kevin Israel <pleasestand@live.com>
2015-09-26 19:34:47 -0400

    Installer: Fix collapsing of field help text
    Follows-up 1e06a9f02f9a.
    
    Change-Id: I23030f48ce89abc460ac9e7ed3c148893ffb24ef

--- mw-config/config.js
+++ mw-config/config.js
@@ -14,15 +14,15 @@
 		}
 
 		// Set up the help system
-		$( '.mw-help-field-data' )
+		$( '.config-help-field-data' )
 			.hide()
-			.closest( '.mw-help-field-container' )
-				.find( '.mw-help-field-hint' )
+			.closest( '.config-help-field-container' )
+				.find( '.config-help-field-hint' )
 					.show()
 					.click( function () {
 						$( this )
-							.closest( '.mw-help-field-container' )
-								.find( '.mw-help-field-data' )
+							.closest( '.config-help-field-container' )
+								.find( '.config-help-field-data' )
 									.slideToggle( 'fast' );
 					} );
 
