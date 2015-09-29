844c240130e19f6b049141fa955d800c3483e756
Mukunda Modell <mmodell@wikimedia.org>
2015-09-29 11:28:27 -0500

    Creating new WMF 1.27.0-wmf.1 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.27alpha';
+$wgVersion = '1.27.0-wmf.1';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
