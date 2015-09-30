6bdb9d7cfd9e17bd9e3b899a7b0aff6dc7236a95
Purodha <purodha@blissenbach.org>
2015-09-30 00:29:38 +0000

    Installation script: Allow $wgCapitalLinks to be set to false
    See bug T113879
    
    Change-Id: Ifeefdc715f487b1bde2621813fce038fa6b0aa5a

--- includes/installer/LocalSettingsGenerator.php
+++ includes/installer/LocalSettingsGenerator.php
@@ -65,7 +65,7 @@ class LocalSettingsGenerator {
 				'wgRightsText', 'wgMainCacheType', 'wgEnableUploads',
 				'wgMainCacheType', '_MemCachedServers', 'wgDBserver', 'wgDBuser',
 				'wgDBpassword', 'wgUseInstantCommons', 'wgUpgradeKey', 'wgDefaultSkin',
-				'wgMetaNamespace', 'wgLogo',
+				'wgMetaNamespace', 'wgLogo', 'wgCapitalLinks',
 			),
 			$db->getGlobalNames()
 		);
@@ -289,6 +289,9 @@ class LocalSettingsGenerator {
 			$serverSetting .= "\$wgServer = \"{$this->values['wgServer']}\";\n";
 		}
 
+		$this->values['wgCapitalLinks'] = ( $this->values['wgCapitalLinks'] == 'standard' )
+			? 'True' : 'False';
+
 		switch ( $this->values['wgMainCacheType'] ) {
 			case 'anything':
 			case 'db':
@@ -410,6 +413,10 @@ ${serverSetting}
 {$groupRights}{$noFollow}## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
 \$wgDefaultSkin = \"{$this->values['wgDefaultSkin']}\";
+
+## Initial letters in page titles are be mapped to upper case, if True.
+\$wgCapitalLinks = {$this->values['wgCapitalLinks']};
+
 ";
 	}
 }
