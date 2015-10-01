f3536d1b8b5bea0a7911cea14eabea6b74eab090
Bartosz Dziewoński <matma.rex@gmail.com>
2015-10-02 01:30:52 +0200

    mw.widgets.ComplexTitleInputWidget: Add infusion helpers
    * reusePreInfuseDOM
    * gatherPreInfuseState
    * restorePreInfuseState
    
    Bug: T114134
    Change-Id: I939551579fe1d03603d65b3562c1c977681994a6

--- resources/src/mediawiki.widgets/mw.widgets.ComplexTitleInputWidget.js
+++ resources/src/mediawiki.widgets/mw.widgets.ComplexTitleInputWidget.js
@@ -50,6 +50,43 @@
 
 	OO.inheritClass( mw.widgets.ComplexTitleInputWidget, OO.ui.Widget );
 
+	/* Static Methods */
+	/*jshint -W024*/
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.ComplexTitleInputWidget.static.reusePreInfuseDOM = function ( node, config ) {
+		config = mw.widgets.ComplexTitleInputWidget.parent.static.reusePreInfuseDOM( node, config );
+		config.namespace = mw.widgets.NamespaceInputWidget.static.reusePreInfuseDOM(
+			$( node ).find( '.mw-widget-namespaceInputWidget' ),
+			config.namespace
+		);
+		config.title = mw.widgets.TitleInputWidget.static.reusePreInfuseDOM(
+			$( node ).find( '.mw-widget-titleInputWidget' ),
+			config.title
+		);
+		return config;
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.ComplexTitleInputWidget.static.gatherPreInfuseState = function ( node, config ) {
+		var state = mw.widgets.ComplexTitleInputWidget.parent.static.gatherPreInfuseState( node, config );
+		state.namespace = mw.widgets.NamespaceInputWidget.static.gatherPreInfuseState(
+			$( node ).find( '.mw-widget-namespaceInputWidget' ),
+			config.namespace
+		);
+		state.title = mw.widgets.TitleInputWidget.static.gatherPreInfuseState(
+			$( node ).find( '.mw-widget-titleInputWidget' ),
+			config.title
+		);
+		return state;
+	};
+
+	/*jshint +W024*/
+
 	/* Methods */
 
 	/**
@@ -58,6 +95,15 @@
 	 */
 	mw.widgets.ComplexTitleInputWidget.prototype.updateTitleNamespace = function () {
 		this.title.setNamespace( Number( this.namespace.getValue() ) );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.widgets.ComplexTitleInputWidget.prototype.restorePreInfuseState = function ( state ) {
+		mw.widgets.ComplexTitleInputWidget.parent.prototype.restorePreInfuseState.call( this, state );
+		this.namespace.restorePreInfuseState( state.namespace );
+		this.title.restorePreInfuseState( state.title );
 	};
 
 }( jQuery, mediaWiki ) );
