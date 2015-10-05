e637c38728f09e7050d44bd58f0a182876c48424
Ori Livneh <ori@wikimedia.org>
2015-10-05 16:10:56 -0700

    Add MemoizedCallable for APC-backed function memoization
    Add a simple class to `libs/` for memoizing functions by caching return values
    in APC. I decided not to make this an external library just yet because I see
    this as potentially a part of a larger functional programming library.
    
    Doesn't use APCBagOStuff for two reasons: (1) avoid dependency on MediaWiki
    code; (2) ability to pass third &$success parameter to apc_store, to
    distinguish between cache misses and cached false values.
    
    Use this in ResourceLoaderFileModule to cache CSSMin::remap.
    
    Change-Id: I00a47983a2583655d4631ecc9c6ba17597e36b5f

---
+++ tests/phpunit/includes/libs/MemoizedCallableTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * PHP Unit tests for MemoizedCallable class.
+ * @covers MemoizedCallable
+ */
+class MemoizedCallableTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Consecutive calls to the memoized callable with the same arguments
+	 * should result in just one invocation of the underlying callable.
+	 *
+	 * @requires function apc_store
+	 */
+	public function testCallableMemoized() {
+		apc_clear_cache();
+
+		$observer = $this->getMock( 'stdClass', array( 'compute' ) );
+		$observer->expects( $this->once() )->method( 'compute' );
+		$memoized = new MemoizedCallable( array( $observer, 'compute' ) );
+		$memoized->invoke();
+		$memoized->invoke();
+	}
+
+	/**
+	 * The memoized callable should relate inputs to outputs in the same
+	 * way as the original underlying callable.
+	 */
+	public function testReturnValuePassedThrough() {
+		$mock = $this->getMock( 'stdClass', array( 'compute' ) );
+		$mock->expects( $this->any() )
+			->method( 'compute' )
+			->will( $this->returnCallback( 'strrev' ) );
+
+		$memoized = new MemoizedCallable( array( $mock, 'compute' ) );
+		$this->assertEquals( $memoized->invoke( 'input' ), 'tupni' );
+	}
+
+	/**
+	 * TTL values should be capped at 86400.
+	 */
+	public function testMaxTTL() {
+		$memoized = new MemoizedCallable( function () {
+		}, 1000000 );
+		$this->assertEquals( 86400, PHPUnit_Framework_Assert::readAttribute( $memoized, 'ttl' ) );
+	}
+
+	/**
+	 * @expectedExceptionMessage non-scalar argument
+	 * @expectedException        InvalidArgumentException
+	 */
+	public function testNonScalarArguments() {
+		$memoized = new MemoizedCallable( function ( $a, $b ) {
+			return $a + $b;
+		} );
+
+		$memoized->invoke( new stdClass() );
+	}
+
+	/**
+	 * @expectedExceptionMessage must be an instance of callable
+	 * @expectedException        InvalidArgumentException
+	 */
+	public function testNotCallable() {
+		$memoized = new MemoizedCallable( 14 );
+	}
+}
