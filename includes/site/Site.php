45b0cc23b9f20031b8b857999550274dcda1880c
Bryan Davis <bd808@wikimedia.org>
2015-10-07 15:21:28 -0600

    Site: fix copy-n-paste phpdoc
    Change-Id: I5e735089b3dacea94044fa96f821fc47940f7029

--- includes/site/Site.php
+++ includes/site/Site.php
@@ -172,7 +172,7 @@ class Site implements Serializable {
 	}
 
 	/**
-	 * Gets the type of the site (ie wikipedia).
+	 * Gets the group of the site (ie wikipedia).
 	 *
 	 * @since 1.21
 	 *
@@ -183,7 +183,7 @@ class Site implements Serializable {
 	}
 
 	/**
-	 * Sets the type of the site (ie wikipedia).
+	 * Sets the group of the site (ie wikipedia).
 	 *
 	 * @since 1.21
 	 *
