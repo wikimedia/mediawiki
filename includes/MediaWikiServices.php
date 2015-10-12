e8f228113d6039581888bb18ae7d35e571629197
daniel <daniel.kinzler@wikimedia.de>
2015-10-12 10:05:45 +0200

    Introduce top level service locator.
    The service locator, MediaWikiServices, is intended to facilitate
    "manual" dependency injection in static entry points.
    
    See also the Dependency Injection RFC T384 and Service Locator
    RFC T124792 for details.
    The following key points were implemented according the
    discussion surrounding these RFCs:
    
    * a configurable DI container that allows extensions to add and replace services.
    * no auto-wiring, since it's prone to add confusion in large and complex applications.
    * no 3rd party framework, since they typically do too much.
    
    The following services in MediaWiki core are made accessible via the service locator
    mechanism to showcase the bootstrapping mechanism:
    
    * DBLoadBalancerFactory and DBLoadBalancer
    * ConfigFactory and MainConfig
    * SiteLookup and SiteStore
    
    However, the implementation of these services was not yet converted to using proper DI
    throughout the code.
    
    Bug: T124792
    Change-Id: I3c25c0ac17300d3dd13e1cf5100558a605eee15f

---
+++ includes/MediaWikiServices.php
@@ -0,0 +1,167 @@
+<?php
+namespace MediaWiki;
+
+use ConfigFactory;
+use GlobalVarConfig;
+use Config;
+use Hooks;
+use LBFactory;
+use LoadBalancer;
+use MediaWiki\Services\ServiceContainer;
+use SiteLookup;
+use SiteStore;
+
+/**
+ * Service locator for MediaWiki core services.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ *
+ * @since 1.27
+ */
+
+/**
+ * MediaWikiServices is the service locator for the application scope of MediaWiki.
+ * Its implemented as a simple configurable DI container.
+ * MediaWikiServices acts as a top level factory/registry for top level services, and builds
+ * the network of service objects that defines MediaWiki's application logic.
+ * It acts as an entry point to MediaWiki's dependency injection mechanism.
+ *
+ * Services are defined in the "wiring" array passed to the constructor,
+ * or by calling defineService().
+ *
+ * @see docs/injection.txt for an overview of using dependency injection in the
+ *      MediaWiki code base.
+ */
+class MediaWikiServices extends ServiceContainer {
+
+	/**
+	 * Returns the global default instance of the top level service locator.
+	 *
+	 * The default instance is initialized using the service constructor callbacks
+	 * defined in ServiceWiring.php.
+	 *
+	 * @note This should only be called by static functions! The instance returned here
+	 * should not be passed around! Objects that need access to a service should have
+	 * that service injected into the constructor, never a service locator!
+	 *
+	 * @return MediaWikiServices
+	 */
+	public static function getInstance() {
+		static $instance = null;
+
+		if ( $instance === null ) {
+			// NOTE: constructing GlobalVarConfig here is not particularly pretty,
+			// but some information from the global scope has to be injected here,
+			// even if it's just a file name or database credentials to load
+			// configuration from.
+			$config = new GlobalVarConfig();
+			$instance = new self( $config );
+
+			// Load the default wiring from the specified files.
+			$wiringFiles = $config->get( 'ServiceWiringFiles' );
+			$instance->loadWiringFiles( $wiringFiles );
+
+			// Provide a traditional hook point to allow extensions to configure services.
+			Hooks::run( 'MediaWikiServices', array( $instance ) );
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * @param Config $config The Config object to be registered as the 'BootstrapConfig' service.
+	 *        This has to contain at least the information needed to set up the 'ConfigFactory'
+	 *        service.
+	 */
+	public function __construct( Config $config ) {
+		parent::__construct();
+
+		// register the given Config object as the bootstrap config service.
+		$this->defineService( 'BootstrapConfig', function() use ( $config ) {
+			return $config;
+		} );
+	}
+
+	/**
+	 * Returns the Config object containing the bootstrap configuration.
+	 * Bootstrap configuration would typically include database credentials
+	 * and other information that may be needed before the ConfigFactory
+	 * service can be instantiated.
+	 *
+	 * @note This should only be used during bootstrapping, in particular
+	 * when creating the MainConfig service. Application logic should
+	 * use getMainConfig() to get a Config instances.
+	 *
+	 * @return Config
+	 */
+	public function getBootstrapConfig() {
+		return $this->getService( 'BootstrapConfig' );
+	}
+
+	/**
+	 * @return ConfigFactory
+	 */
+	public function getConfigFactory() {
+		return $this->getService( 'ConfigFactory' );
+	}
+
+	/**
+	 * Returns the Config object that provides configuration for MediaWiki core.
+	 * This may or may not be the same object that is returned by getBootstrapConfig().
+	 *
+	 * @return Config
+	 */
+	public function getMainConfig() {
+		return $this->getService( 'MainConfig' );
+	}
+
+	/**
+	 * @return SiteLookup
+	 */
+	public function getSiteLookup() {
+		return $this->getService( 'SiteLookup' );
+	}
+
+	/**
+	 * @return SiteStore
+	 */
+	public function getSiteStore() {
+		return $this->getService( 'SiteStore' );
+	}
+
+	/**
+	 * @return LBFactory
+	 */
+	public function getDBLoadBalancerFactory() {
+		return $this->getService( 'DBLoadBalancerFactory' );
+	}
+
+	/**
+	 * @return LoadBalancer The main DB load balancer for the local wiki.
+	 */
+	public function getDBLoadBalancer() {
+		return $this->getService( 'DBLoadBalancer' );
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	// NOTE: When adding a service getter here, don't forget to add a test
+	// case for it in MediaWikiServicesTest::provideGetters() and in
+	// MediaWikiServicesTest::provideGetService()!
+	///////////////////////////////////////////////////////////////////////////
+
+}
---
+++ tests/phpunit/includes/Services/ServiceContainerTest.php
@@ -0,0 +1,302 @@
+<?php
+use MediaWiki\Services\ServiceContainer;
+
+/**
+ * @covers MediaWiki\Services\ServiceContainer
+ *
+ * @group MediaWiki
+ */
+class ServiceContainerTest extends PHPUnit_Framework_TestCase {
+
+	private function newServiceContainer( $extraArgs = array() ) {
+		return new ServiceContainer( $extraArgs );
+	}
+
+	public function testGetServiceNames() {
+		$services = $this->newServiceContainer();
+		$names = $services->getServiceNames();
+
+		$this->assertInternalType( 'array', $names );
+		$this->assertEmpty( $names );
+
+		$name = 'TestService92834576';
+		$services->defineService( $name, function() {
+			return null;
+		} );
+
+		$names = $services->getServiceNames();
+		$this->assertContains( $name, $names );
+	}
+
+	public function testHasService() {
+		$services = $this->newServiceContainer();
+
+		$name = 'TestService92834576';
+		$this->assertFalse( $services->hasService( $name ) );
+
+		$services->defineService( $name, function() {
+			return null;
+		} );
+
+		$this->assertTrue( $services->hasService( $name ) );
+	}
+
+	public function testGetService() {
+		$services = $this->newServiceContainer( array( 'Foo' ) );
+
+		$theService = new stdClass();
+		$name = 'TestService92834576';
+
+		$services->defineService(
+			$name,
+			function( $actualLocator, $extra ) use ( $services, $theService ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				PHPUnit_Framework_Assert::assertSame( $extra, 'Foo' );
+				return $theService;
+			}
+		);
+
+		$this->assertSame( $theService, $services->getService( $name ) );
+	}
+
+	public function testGetService_fail_unknown() {
+		$services = $this->newServiceContainer();
+
+		$name = 'TestService92834576';
+
+		$this->setExpectedException( 'InvalidArgumentException' );
+
+		$services->getService( $name );
+	}
+
+	public function testDefineService() {
+		$services = $this->newServiceContainer();
+
+		$theService = new stdClass();
+		$name = 'TestService92834576';
+
+		$services->defineService( $name, function( $actualLocator ) use ( $services, $theService ) {
+			PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+			return $theService;
+		} );
+
+		$this->assertTrue( $services->hasService( $name ) );
+		$this->assertSame( $theService, $services->getService( $name ) );
+	}
+
+	public function testDefineService_fail_duplicate() {
+		$services = $this->newServiceContainer();
+
+		$theService = new stdClass();
+		$name = 'TestService92834576';
+
+		$services->defineService( $name, function() use ( $theService ) {
+			return $theService;
+		} );
+
+		$this->setExpectedException( 'RuntimeException' );
+
+		$services->defineService( $name, function() use ( $theService ) {
+			return $theService;
+		} );
+	}
+
+	public function testApplyWiring() {
+		$services = $this->newServiceContainer();
+
+		$wiring = array(
+			'Foo' => function() {
+				return 'Foo!';
+			},
+			'Bar' => function() {
+				return 'Bar!';
+			},
+		);
+
+		$services->applyWiring( $wiring );
+
+		$this->assertSame( 'Foo!', $services->getService( 'Foo' ) );
+		$this->assertSame( 'Bar!', $services->getService( 'Bar' ) );
+	}
+
+	public function testLoadWiringFiles() {
+		$services = $this->newServiceContainer();
+
+		$wiringFiles = array(
+			__DIR__ . '/TestWiring1.php',
+			__DIR__ . '/TestWiring2.php',
+		);
+
+		$services->loadWiringFiles( $wiringFiles );
+
+		$this->assertSame( 'Foo!', $services->getService( 'Foo' ) );
+		$this->assertSame( 'Bar!', $services->getService( 'Bar' ) );
+	}
+
+	public function testLoadWiringFiles_fail_duplicate() {
+		$services = $this->newServiceContainer();
+
+		$wiringFiles = array(
+			__DIR__ . '/TestWiring1.php',
+			__DIR__ . '/./TestWiring1.php',
+		);
+
+		// loading the same file twice should fail, because
+		$this->setExpectedException( 'RuntimeException' );
+
+		$services->loadWiringFiles( $wiringFiles );
+	}
+
+	public function testReplaceService() {
+		$services = $this->newServiceContainer( array( 'Foo' ) );
+
+		$theService1 = new stdClass();
+		$theService2 = new stdClass();
+		$name = 'TestService92834576';
+
+		$services->defineService( $name, function() {
+			PHPUnit_Framework_Assert::fail(
+				'The original constructor callback should not get called'
+			);
+		} );
+
+		// replace before instantiation
+		$services->replaceService(
+			$name,
+			function( $actualLocator, $extra ) use ( $services, $theService1 ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				PHPUnit_Framework_Assert::assertSame( 'Foo', $extra );
+				return $theService1;
+			},
+			function ( $instance, $actualLocator, $extra ) {
+				PHPUnit_Framework_Assert::fail(
+					'Cleanup callback should not be called if the services wasn\'t yet instantiated'
+				);
+			}
+		);
+
+		// force instantiation, check result
+		$this->assertSame( $theService1, $services->getService( $name ) );
+
+		// replace after instantiation
+		$cleanedUpInstance = null;
+		$services->replaceService(
+			$name,
+			function( $actualLocator ) use ( $services, $theService2 ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				return $theService2;
+			},
+			function ( $instance, $actualLocator, $extra ) use ( $services, &$cleanedUpInstance ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				PHPUnit_Framework_Assert::assertSame( 'Foo', $extra );
+				$cleanedUpInstance = $instance;
+			}
+		);
+
+		// check cleanup
+		$this->assertSame(
+			$theService1,
+			$cleanedUpInstance,
+			'Cleanup callback should have been called for previous service instance'
+		);
+
+		// check instantiation
+		$this->assertSame( $theService2, $services->getService( $name ) );
+
+		// replace without cleanup
+		$services->replaceService(
+			$name,
+			function( $actualLocator ) use ( $services, $theService1 ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				return $theService1;
+			}
+		);
+
+		// check instantiation
+		$this->assertSame( $theService1, $services->getService( $name ) );
+	}
+
+	public function testReplaceService_fail_undefined() {
+		$services = $this->newServiceContainer();
+
+		$theService = new stdClass();
+		$name = 'TestService92834576';
+
+		$this->setExpectedException( 'RuntimeException' );
+
+		$services->replaceService( $name, function() use ( $theService ) {
+			return $theService;
+		} );
+	}
+
+	public function testResetService() {
+		$services = $this->newServiceContainer( array( 'Foo' ) );
+
+		$name = 'TestService92834576';
+		$counter = 0;
+
+		$services->defineService( $name, function() use ( &$counter ) {
+			$obj = new stdClass();
+			$obj->number = ++$counter;
+			return $obj;
+		} );
+
+		// reset before instantiation
+		$services->resetService(
+			$name,
+			function ( $instance ) {
+				PHPUnit_Framework_Assert::fail(
+					'Cleanup callback should not be called if the services wasn\'t yet instantiated'
+				);
+			}
+		);
+
+		// force instantiation, check result
+		$service1 = $services->getService( $name );
+		$this->assertSame( 1, $service1->number, 'First service instance' );
+
+		// reset after instantiation
+		$cleanedUpInstance = null;
+		$services->resetService(
+			$name,
+			function ( $instance, $actualLocator, $extra ) use ( $services, &$cleanedUpInstance ) {
+				PHPUnit_Framework_Assert::assertSame( $services, $actualLocator );
+				PHPUnit_Framework_Assert::assertSame( 'Foo', $extra );
+				$cleanedUpInstance = $instance;
+			}
+		);
+
+		// check cleanup
+		$this->assertSame(
+			$service1,
+			$cleanedUpInstance,
+			'Cleanup callback should have been called for previous service instance'
+		);
+
+		// check instantiation
+		$service2 = $services->getService( $name );
+		$this->assertNotSame( $service1, $service2, 'New service instance expected after reset' );
+		$this->assertSame( 2, $service2->number, 'New service instance expected after reset' );
+
+		// reset without cleanup
+		$services->resetService(
+			$name
+		);
+
+		// check instantiation
+		$service3 = $services->getService( $name );
+		$this->assertNotSame( $service2, $service3, 'New service instance expected after reset' );
+		$this->assertSame( 3, $service3->number, 'New service instance expected after reset' );
+	}
+
+	public function testResetService_fail_undefined() {
+		$services = $this->newServiceContainer();
+
+		$name = 'TestService92834576';
+
+		$this->setExpectedException( 'RuntimeException' );
+
+		$services->resetService( $name );
+	}
+
+}
