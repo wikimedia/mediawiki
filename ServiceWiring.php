afc1cb4da4d2266806182dfdc3bb6f251f419f74
daniel <daniel.kinzler@wikimedia.de>
2015-10-12 10:05:45 +0200

    Introduce top level service locator.
    The service locator, MediaWikiServices, is intended to facilitate
    "manual" dependency injection in static entry points.
    
    See also the Dependency Injection RFC T384 and Service Locator
    RFC T124792 for details.
    The following key points were implemented according the
    discussion surrounding these RFCs:
    
    * a configurable DI container that allows extensions to add and replace services.
    * no auto-wiring, since it's prone to add confusion in large and complex applications.
    * no 3rd party framework, since they typically do too much.
    
    The following services in MediaWiki core are made accessible via the service locator
    mechanism to showcase the bootstrapping mechanism:
    
    * ConfigFactory and MainConfig
    * SiteLookup and SiteStore
    
    However, the implementation of these services was not yet converted to using proper DI
    throughout the code.
    
    Bug: T124792
    Change-Id: I3c25c0ac17300d3dd13e1cf5100558a605eee15f

---
+++ ServiceWiring.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Default wiring for MediaWiki services.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ *
+ * This file is loaded by MediaWiki\MediaWikiServices::getInstance() during the
+ * bootstrapping of the dependency injection framework.
+ *
+ * This file returns an array that associates service name with constructor callbacks
+ * that instantiate the default instances for the services used by MediaWiki
+ * core. For every service that MediaWiki core requires, a constructor callback
+ * creating a default instance must be defined in this file.
+ *
+ * @note As of version 1.27, MediaWiki is only beginning to use dependency injection.
+ * The services defined here do not yet fully represent all services used by core,
+ * much of the code still relies on global state for this accessing services.
+ *
+ * @since 1.27
+ *
+ * @see docs/injection.txt for an overview of using dependency injection in the
+ *      MediaWiki code base.
+ */
+
+use MediaWiki\MediaWikiServices;
+
+return array(
+	'SiteStore' => function( MediaWikiServices $services ) {
+		$loadBalancer = wfGetLB(); // TODO: use LB from MediaWikiServices
+		$rawSiteStore = new DBSiteStore( $loadBalancer );
+
+		// TODO: replace wfGetCache with a CacheFactory service.
+		// TODO: replace wfIsHHVM with a capabilities service.
+		$cache = wfGetCache( wfIsHHVM() ? CACHE_ACCEL : CACHE_ANYTHING );
+
+		return new CachingSiteStore( $rawSiteStore, $cache );
+	},
+
+	'SiteLookup' => function( MediaWikiServices $services ) {
+		// Use the default SiteStore as the SiteLookup implementation for now
+		return $services->getSiteStore();
+	},
+
+	'ConfigFactory' => function( MediaWikiServices $services ) {
+		// Use the bootstrap config to initialize the ConfigFactory.
+		$registry = $services->getBootstrapConfig()->get( 'ConfigRegistry' );
+		$factory = new ConfigFactory();
+
+		foreach ( $registry as $name => $callback ) {
+			$factory->register( $name, $callback );
+		}
+		return $factory;
+	},
+
+	'MainConfig' => function( MediaWikiServices $services ) {
+		// Use the 'main' config from the ConfigFactory service.
+		return $services->getConfigFactory()->makeConfig( 'main' );
+	},
+
+	///////////////////////////////////////////////////////////////////////////
+	// NOTE: When adding a service here, don't forget to add a getter function
+	// in the MediaWikiServices class. The convenience getter should just call
+	// $this->getService( 'FooBarService' ).
+	///////////////////////////////////////////////////////////////////////////
+
+);
---
+++ tests/phpunit/includes/Services/TestWiring1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Test file for testing ServiceContainer::loadWiringFiles
+ */
+
+return array(
+	'Foo' => function() {
+		return 'Foo!';
+	},
+);
