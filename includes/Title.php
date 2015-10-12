0800b62a8bb5ea630664e4c98f4855305af9bbcf
daniel <daniel.kinzler@wikimedia.de>
2015-10-12 16:27:41 +0200

    Move singletons of MediaWikiTitleCodec and MediaWikiPageLinkRenderer to MediaWikiServices.
    Change-Id: Icda128d4b03901efdab9c0c6ced142b3a7fe6671

--- includes/Title.php
+++ includes/Title.php
@@ -171,33 +171,9 @@ class Title {
 	 * @return TitleParser
 	 */
 	private static function getTitleParser() {
-		global $wgContLang, $wgLocalInterwikis;
-
-		static $titleCodec = null;
-		static $titleCodecFingerprint = null;
-
-		// $wgContLang and $wgLocalInterwikis may change (especially while testing),
-		// make sure we are using the right one. To detect changes over the course
-		// of a request, we remember a fingerprint of the config used to create the
-		// codec singleton, and re-create it if the fingerprint doesn't match.
-		$fingerprint = spl_object_hash( $wgContLang ) . '|' . join( '+', $wgLocalInterwikis );
-
-		if ( $fingerprint !== $titleCodecFingerprint ) {
-			$titleCodec = null;
-		}
-
-		if ( !$titleCodec ) {
+		return MediaWikiServices::getInstance()->getTitleParser();
 			// @todo: move this into MediaWikiServices
-			$titleCodec = new MediaWikiTitleCodec(
-				$wgContLang,
-				GenderCache::singleton(),
 				MediaWikiServices::getInstance()->getInterwikiLookup(),
-				$wgLocalInterwikis
-			);
-			$titleCodecFingerprint = $fingerprint;
-		}
-
-		return $titleCodec;
 	}
 
 	/**
@@ -209,9 +185,7 @@ class Title {
 	 * @return TitleFormatter
 	 */
 	private static function getTitleFormatter() {
-		// NOTE: we know that getTitleParser() returns a MediaWikiTitleCodec,
-		//      which implements TitleFormatter.
-		return self::getTitleParser();
+		return MediaWikiServices::getInstance()->getTitleFormatter();
 	}
 
 	function __construct() {
