404d64a563e76abc507f8d6613b5cc88350cf735
Tim Starling <tstarling@wikimedia.org>
2015-10-13 23:16:44 -0400

    Client-side migration for empty li preservaton
    It is desirable in terms of user-friendly syntax to display an empty
    list item if the user adds one to the source. However, we suspect that
    this change will break the rendering of existing templates. So, preserve
    the empty <li> element, but style it with display:none so that there is
    no user-visible change. Changes can then be observed with a user script,
    then eventually the CSS can be removed so that the desired behaviour will
    be user visible.
    
    This is imagined as a staged deployment of T89331, i.e. it is better to
    resolve differences with Html5Depurate one at a time instead of
    deploying it all at once.
    
    The CSS module is specified in parser/MWTidy.php since the tidy driver
    hierarchy is not meant to be so closely tied to the MW environment.
    
    Bug: T49673
    Change-Id: Ifb44b782c617240e3de73dcdf76c8737c7307d94

--- includes/tidy/RaggettWrapper.php
+++ includes/tidy/RaggettWrapper.php
@@ -51,6 +51,11 @@ class RaggettWrapper {
 		// we can trick Tidy into not stripping them out by including them in tidy's new-empty-tags config
 		$wrappedtext = preg_replace( '!<(link|meta)([^>]*?)(/{0,1}>)!', '<html-$1$2$3', $wrappedtext );
 
+		// Preserve empty li elements (T49673) by abusing Tidy's datafld hack
+		// The whitespace class is as in TY_(InitMap)
+		$wrappedtext = preg_replace( "!<li>([ \r\n\t\f]*)</li>!",
+			'<li datafld="" class="mw-empty-li">\1</li>', $wrappedtext );
+
 		// Wrap the whole thing in a doctype and body for Tidy.
 		$wrappedtext = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"' .
 			' "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html>' .
@@ -78,6 +83,9 @@ class RaggettWrapper {
 	public function postprocess( $text ) {
 		// Revert <html-{link,meta}> back to <{link,meta}>
 		$text = preg_replace( '!<html-(link|meta)([^>]*?)(/{0,1}>)!', '<$1$2$3', $text );
+
+		// Remove datafld
+		$text = str_replace( '<li datafld=""', '<li', $text );
 
 		// Restore the contents of placeholder tokens
 		$text = $this->mTokens->replace( $text );
