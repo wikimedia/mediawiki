1f15863aa7ed0d849065c673afba514643fae01f
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-10-14 19:20:28 +0200

    Use mw.notification for success message on Special:UserRights
    Split out Special:Preferences handling of successbox into it's own
    mediawiki.notification.convertmessagebox module and make it reuseable.
    
    Use this module on Special:UserRights.
    
    Bug: T115463
    Change-Id: I87054b55053d209835d6fdea1f6e3e67f10e3ac8

---
+++ resources/src/mediawiki.special/mediawiki.special.userrights.js
@@ -0,0 +1,8 @@
+/*!
+ * JavaScript for Special:UserRights
+ */
+( function () {
+	var convertmessagebox = require( 'mediawiki.notification.convertmessagebox' );
+	// Replace successbox with notifications
+	convertmessagebox();
+}() );
