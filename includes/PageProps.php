03ab88e1f96f0a916309fe634e3b8f901b23de4f
cicalese <cicalese@mitre.org>
2015-10-14 11:19:14 -0400

    added page_props access class; set link text from displaytitle page property
    Bug: T115331
    Change-Id: I96186d4156e7106ec6fe8bd09ba8dcbf8658d5e4

---
+++ includes/PageProps.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Access to properties of a page.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Gives access to properties of a page.
+ *
+ * @since 1.27
+ *
+ */
+class PageProps {
+
+	private $id;
+
+	/**
+	 * Create a PageProps object
+	 *
+	 * @param int $id
+	 *
+	 */
+	public function __construct( $id ) {
+		$this->id = $id;
+	}
+
+	/**
+	 * Get all page property values.
+	 *
+	 * @return property value array or false if not found
+	 *
+	 */
+	public function getProperties() {
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$result = $dbr->select(
+			'page_props',
+			array(
+				'pp_propname',
+				'pp_value'
+			),
+			array(
+				'pp_page' => $this->id
+			),
+			__METHOD__
+		);
+
+		$pageProperties = array();
+
+		foreach ( $result as $row ) {
+			$pageProperties[$row->pp_propname] = $row->pp_value;
+		}
+
+		return $pageProperties;
+	}
+
+	/**
+	 * Get page property value for a given property name.
+	 *
+	 * @param string $propertyName
+	 *
+	 * @return property value or false if not found
+	 *
+	 */
+	public function getProperty( $propertyName ) {
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$row = $dbr->selectRow(
+			'page_props',
+			array( 'pp_value' ),
+			array(
+				'pp_page' => $this->id,
+				'pp_propname' => $propertyName
+			),
+			__METHOD__
+		);
+
+		if ( $row !== false ) {
+			return $row->pp_value;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Determines the sort key for the given property value.
+	 * This will return $value if it is a float or int,
+	 * 1 or resp. 0 if it is a bool, and null otherwise.
+	 *
+	 * @param mixed $value
+	 *
+	 * @return float|null
+	 *
+	 */
+	private function getPropertySortKeyValue( $value ) {
+		if ( is_int( $value ) || is_float( $value ) || is_bool( $value ) ) {
+			return floatval( $value );
+		}
+
+		return null;
+	}
+
+	/**
+	 * Set page property value.
+	 *
+	 * @param string $propertyName
+	 * @param mixed $propertyValue
+	 *
+	 */
+	 public function setProperty( $propertyName, $propertyValue ) {
+
+		$old_value = $this->getProperty( $propertyName );
+
+		if ( $old_value === false ) {
+
+			$row = array(
+				'pp_page' => $this->id,
+				'pp_propname' => $propertyName,
+				'pp_value' => $propertyValue
+			);
+
+			if ( $wgPagePropsHaveSortkey ) {
+				$row['pp_sortkey'] = $this->getPropertySortKeyValue( $value );
+			}
+
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->insert(
+				'page_props',
+				$row,
+				__METHOD__
+			);
+
+		} elseif ( $old_value !== $propertyValue ) {
+
+			$row = array(
+				'pp_value' => $propertyValue
+			);
+
+			if ( $wgPagePropsHaveSortkey ) {
+				$row['pp_sortkey'] = $this->getPropertySortKeyValue( $value );
+			}
+
+			$dbw = wfGetDB( DB_MASTER );
+			$dbw->update(
+				'page_props',
+				$row,
+				array(
+					'pp_page' => $this->id,
+					'pp_propname' => $propertyName
+				),
+				__METHOD__
+			);
+
+		}
+
+	}
+
+}
