10e69dceb37b2a059f3bb1bf688864452cb20a8b
This, that and the other <at.light@live.com.au>
2015-10-16 12:33:58 +1100

    Handle missing namespace prefix in XML dumps more gracefully
    If an XML dump of a wiki is exported using dumpBackup.php, and there are
    pages in a namespace that is not registered (perhaps because of a missing
    extension), they will appear in the dump in the form
    
    <page> ... <title>PageTitle</title> <ns>1234</ns> ... </page>
    
    This caused the ForeignTitle code to raise an undefined offset error,
    because it assumed that the <title> element was of the form
    "Namespace:PageTitle" when <ns> was nonzero. This assumption is not valid.
    
    Now, the importation of such dumps will no longer throw errors and the
    pages will be correctly imported, although possibly to unexpected
    locations.
    
    Bug: T114115
    Change-Id: I0271435dc208e7ea118339584f8a0e359c96113a

--- includes/title/NamespaceAwareForeignTitleFactory.php
+++ includes/title/NamespaceAwareForeignTitleFactory.php
@@ -115,15 +115,22 @@ class NamespaceAwareForeignTitleFactory implements ForeignTitleFactory {
 	protected function parseTitleWithNs( $title, $ns ) {
 		$pieces = explode( ':', $title, 2 );
 
+		// Is $title of the form Namespace:Title (true), or just Title (false)?
+		$titleIncludesNamespace = ( $ns != '0' && count( $pieces ) === 2 );
+
 		if ( isset( $this->foreignNamespaces[$ns] ) ) {
 			$namespaceName = $this->foreignNamespaces[$ns];
 		} else {
-			$namespaceName = $ns == '0' ? '' : $pieces[0];
+			// If the foreign wiki is misconfigured, XML dumps can contain a page with
+			// a non-zero namespace ID, but whose title doesn't contain a colon
+			// (T114115). In those cases, output a blank namespace name and let the
+			// ImportTitleFactory fill it in.
+			$namespaceName = $titleIncludesNamespace ? $pieces[0] : '';
 		}
 
 		// We assume that the portion of the page title before the colon is the
-		// namespace name, except in the case of namespace 0
-		if ( $ns != '0' ) {
+		// namespace name, except in the case of namespace 0.
+		if ( $titleIncludesNamespace ) {
 			$pageName = $pieces[1];
 		} else {
 			$pageName = $title;
