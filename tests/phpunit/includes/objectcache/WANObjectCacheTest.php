0e4b0d78d643ccade6bffb91015a74557b9ef85a
Aaron Schulz <aschulz@wikimedia.org>
2015-10-17 15:45:08 -0700

    Remove getWithSetCallback() signature backwards-compatability
    All callers have been migrated
    
    Change-Id: I7b6b87594dd724434ba24d8206fe07d66c1d5d25

--- tests/phpunit/includes/objectcache/WANObjectCacheTest.php
+++ tests/phpunit/includes/objectcache/WANObjectCacheTest.php
@@ -3,6 +3,8 @@
 class WANObjectCacheTest extends MediaWikiTestCase {
 	/** @var WANObjectCache */
 	private $cache;
+	/**@var BagOStuff */
+	private $internalCache;
 
 	protected function setUp() {
 		parent::setUp();
@@ -104,7 +106,7 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 		};
 
 		$wasSet = 0;
-		$v = $cache->getWithSetCallback( $key, $func, 30, array(), array( 'lockTSE' => 5 ) );
+		$v = $cache->getWithSetCallback( $key, 30, $func, array( 'lockTSE' => 5 ) );
 		$this->assertEquals( $value, $v, "Value returned" );
 		$this->assertEquals( 1, $wasSet, "Value regenerated" );
 
@@ -114,7 +116,7 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 		$this->assertGreaterThanOrEqual( 19, $curTTL, 'Current TTL between 19-20 (overriden)' );
 
 		$wasSet = 0;
-		$v = $cache->getWithSetCallback( $key, $func, 30, array(), array(
+		$v = $cache->getWithSetCallback( $key, 30, $func, array(
 			'lowTTL' => 0,
 			'lockTSE' => 5,
 		) );
@@ -124,7 +126,8 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 		$priorTime = microtime( true );
 		usleep( 1 );
 		$wasSet = 0;
-		$v = $cache->getWithSetCallback( $key, $func, 30, array( $cKey1, $cKey2 ) );
+		$v = $cache->getWithSetCallback( $key, 30, $func,
+			array( 'checkKeys' => array( $cKey1, $cKey2 ) ) );
 		$this->assertEquals( $value, $v, "Value returned" );
 		$this->assertEquals( 1, $wasSet, "Value regenerated due to check keys" );
 		$t1 = $cache->getCheckKeyTime( $cKey1 );
@@ -134,7 +137,8 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 
 		$priorTime = microtime( true );
 		$wasSet = 0;
-		$v = $cache->getWithSetCallback( $key, $func, 30, array( $cKey1, $cKey2 ) );
+		$v = $cache->getWithSetCallback( $key, 30, $func,
+			array( 'checkKeys' => array( $cKey1, $cKey2 ) ) );
 		$this->assertEquals( $value, $v, "Value returned" );
 		$this->assertEquals( 1, $wasSet, "Value regenerated due to still-recent check keys" );
 		$t1 = $cache->getCheckKeyTime( $cKey1 );
@@ -149,10 +153,10 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 
 		$wasSet = 0;
 		$key = wfRandomString();
-		$v = $cache->getWithSetCallback( $key, $func, 30, array(), array( 'pcTTL' => 5 ) );
+		$v = $cache->getWithSetCallback( $key, 30, $func, array( 'pcTTL' => 5 ) );
 		$this->assertEquals( $value, $v, "Value returned" );
 		$cache->delete( $key );
-		$v = $cache->getWithSetCallback( $key, $func, 30, array(), array( 'pcTTL' => 5 ) );
+		$v = $cache->getWithSetCallback( $key, 30, $func, array( 'pcTTL' => 5 ) );
 		$this->assertEquals( $value, $v, "Value still returned after deleted" );
 		$this->assertEquals( 1, $wasSet, "Value process cached while deleted" );
 	}
@@ -172,13 +176,13 @@ class WANObjectCacheTest extends MediaWikiTestCase {
 		};
 
 		$cache->delete( $key );
-		$ret = $cache->getWithSetCallback( $key, 30, $func, array(), array( 'lockTSE' => 5 ) );
+		$ret = $cache->getWithSetCallback( $key, 30, $func, array( 'lockTSE' => 5 ) );
 		$this->assertEquals( $value, $ret );
 		$this->assertEquals( 1, $calls, 'Value was populated' );
 
 		// Acquire a lock to verify that getWithSetCallback uses lockTSE properly
 		$this->internalCache->lock( $key, 0 );
-		$ret = $cache->getWithSetCallback( $key, 30, $func, array(), array( 'lockTSE' => 5 ) );
+		$ret = $cache->getWithSetCallback( $key, 30, $func, array( 'lockTSE' => 5 ) );
 		$this->assertEquals( $value, $ret );
 		$this->assertEquals( 1, $calls, 'Callback was not used' );
 	}
