48549aaa83393463450e650ea475831ee7c58c7c
Aaron Schulz <aschulz@wikimedia.org>
2015-10-18 12:53:40 -0700

    Decouple ChronologyProtector from user sessions
    * This now works for users without sessions and for
      requests across subdomains.
    * Use merge() to avoid rolling back master positions
      if the user has multiple tabs open at once.
    * Class global state is now removed.
    * Cleaned up post-send LBFactory::shutdown() logic for
      avoiding master position writes with an explicit flag.
    * Use 'replication' debug log group.
    * toString() method is no longer missing from DBMasterPos.
    
    Change-Id: Ib25d05994d62b25c2f89e67b7f51009c54f4bca8

--- includes/db/loadbalancer/LBFactorySimple.php
+++ includes/db/loadbalancer/LBFactorySimple.php
@@ -38,10 +38,11 @@ class LBFactorySimple extends LBFactory {
 	public function __construct( array $conf ) {
 		parent::__construct( $conf );
 
-		$this->chronProt = new ChronologyProtector;
 		$this->loadMonitorClass = isset( $conf['loadMonitorClass'] )
 			? $conf['loadMonitorClass']
 			: null;
+
+		$this->chronProt = $this->newChronologyProtector();
 	}
 
 	/**
@@ -159,14 +160,10 @@ class LBFactorySimple extends LBFactory {
 		}
 	}
 
-	public function shutdown() {
-		if ( $this->mainLB ) {
-			$this->chronProt->shutdownLB( $this->mainLB );
+	public function shutdown( $flags = 0 ) {
+		if ( !( $flags & self::SHUTDOWN_NO_CHRONPROT ) ) {
+			$this->shutdownChronologyProtector( $this->chronProt );
 		}
-		foreach ( $this->extLBs as $extLB ) {
-			$this->chronProt->shutdownLB( $extLB );
-		}
-		$this->chronProt->shutdown();
-		$this->commitMasterChanges();
+		$this->commitMasterChanges(); // sanity
 	}
 }
