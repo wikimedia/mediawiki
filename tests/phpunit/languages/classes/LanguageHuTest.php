e02d4d96b1fdb0f50d29e55a0016320f171940bc
Gergő Tisza <tgr.huwiki@gmail.com>
2015-10-17 23:31:58 -0700

    [WIP] Add proper {{GRAMMAR}} support for Hungarian suffixes + articles
    Change-Id: I1d21efec1b4160b9005e6788e0b03dafe2f0749a

--- tests/phpunit/languages/classes/LanguageHuTest.php
+++ tests/phpunit/languages/classes/LanguageHuTest.php
@@ -32,4 +32,72 @@ class LanguageHuTest extends LanguageClassesTestCase {
 			array( 'other', 200 ),
 		);
 	}
+
+	/**
+	 * @dataProvider provideArticle
+	 * @covers LanguageHu::getArticle
+	 */
+	public function testGetArticle( $expectedArticle, $word ) {
+		$lang = TestingAccessWrapper::newFromObject( $this->getLang() );
+		$actualArticle = $lang->getArticle( $word );
+		$this->assertEquals( $expectedArticle, $actualArticle );
+	}
+
+	public function provideArticle() {
+		return array(
+			array( 'a', 'ház' ),
+			array( 'az', 'ajtó' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideSuffix
+	 * @covers LanguageHu::addSuffix
+	 */
+	public function testAddSuffix( $expectedWord, $word, $backSuffix, $frontSuffix, $labialSuffix ) {
+		$lang = TestingAccessWrapper::newFromObject( $this->getLang() );
+		$actualWord = $lang->addSuffix( $word, $backSuffix, $frontSuffix, $labialSuffix );
+		$this->assertEquals( $expectedWord, $actualWord );
+	}
+
+	public function provideSuffix() {
+		return array(
+			array( 'fát', 'fa', 't', null, null ),
+			array( 'oldalnak', 'oldal', 'nak', 'nek', null ),
+			array( 'embernek', 'ember', 'nak', 'nek', null ),
+			array( 'sofőrnek', 'sofőr', 'nak', 'nek', null ),
+			array( 'oldalhoz', 'oldal', 'hoz', 'hez', 'höz' ),
+			array( 'emberhez', 'ember', 'hoz', 'hez', 'höz' ),
+			array( 'sofőrhöz', 'sofőr', 'hoz', 'hez', 'höz' ),
+			array( 'főnökhöz', 'főnök', 'hoz', 'hez', 'höz' ),
+			array( 'haverhoz', 'haver', 'hoz', 'hez', 'höz' ),
+			array( 'oldallal', 'oldal', 'val', 'vel', null ),
+			array( 'sakkal', 'sakk', 'val', 'vel', null ),
+			array( 'kéménnyel', 'kémény', 'val', 'vel', null ),
+			array( 'passzal', 'passz', 'val', 'vel', null ),
+			array( 'csévével', 'cséve', 'val', 'vel', null ),
+			array( 'tevén', 'teve', 'on', 'en', 'ön' ),
+			array( 'ValamilyenWikivel', 'ValamilyenWiki', 'val', 'vel', null ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideConvertGrammar
+	 * @covers LanguageHu::convertGrammar
+	 */
+	public function testConvertGrammar( $expected, $type, $param1, $param2, $param3, $param4 ) {
+		$actual = $this->getLang()->convertGrammar( $type, $param1, $param2, $param3, $param4 );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function provideConvertGrammar() {
+		return array(
+			array( 'sofőrről', 'suffix', 'sofőr', 'ról', 'ről', null ),
+			array( 'a', 'article', 'sofőr', null, null, null ),
+			array( 'sofőrről', 'rol', 'sofőr', null, null, null ),
+			array( 'sofőrbe', 'ba', 'sofőr', null, null, null ),
+			array( 'kaszák', 'k', 'kasza', null, null, null ),
+		);
+	}
 }
+
