wikimedia/mediawiki

commit 1c57794e371d74e1d88748de567a1c3358c3ad2e

API: Overhaul ApiResult, make format=xml not throw, and add json formatversion
ApiResult was a mess: some methods could only be used with an array
reference instead of manipulating the stored data, methods that had both
array-ref and internal-data versions had names that didn't at all
correspond, some methods that worked on an array reference were
annoyingly non-static, and then the whole mess with setIndexedTagName.

ApiFormatXml is also entirely annoying to deal with, as it liked to
throw exceptions if certain metadata wasn't provided that no other
formatter required. Its legacy also means we have this silly convention
of using empty-string rather than boolean true, annoying restrictions on
keys (leading to things that should be hashes being arrays of key-value
object instead), '*' used as a key all over the place, and so on.

So, changes here:
* ApiResult is no longer an ApiBase or a ContextSource.
* Wherever sensible, ApiResult provides a static method working on an
  arrayref and a non-static method working on internal data.
* Metadata is now always added to ApiResult's internal data structure.
  Formatters are responsible for stripping it if necessary. "raw mode"
  is deprecated.
* New metadata to replace the '*' key, solve the array() => '[]' vs '{}'
  question, and so on.
* New class for formatting warnings and errors using i18n messages, and
  support for multiple errors and a more machine-readable format for
  warnings. For the moment, though, the actual output will not be changing
  yet (see T47843 for future plans).
* New formatversion parameter for format=json and format=php, to select
  between BC mode and the modern output.
* In BC mode, booleans will be converted to empty-string presence style;
  modules currently returning booleans will need to use
  ApiResult::META_BC_BOOLS to preserve their current output.

Actual changes to the API modules' output (e.g. actually returning
booleans for the new formatversion) beyond the use of

includes/api/ApiBase.php
@@ -467,11 +467,17 @@ abstract class ApiBase extends ContextSource {
 	}
 
 	/**
-	 * Get the result data array (read-only)
-	 * @return array
+	 * Get the error formatter
+	 * @return ApiErrorFormatter
 	 */
-	public function getResultData() {
-		return $this->getResult()->getData();
+	public function getErrorFormatter() {
+		// Main module has getErrorFormatter() method overridden
+		// Safety - avoid infinite loop:
+		if ( $this->isMain() ) {
+			ApiBase::dieDebug( __METHOD__, 'base method was called on main module. ' );
+		}
+
+		return $this->getMain()->getErrorFormatter();
 	}
 
 	/**
@@ -484,6 +490,34 @@ abstract class ApiBase extends ContextSource {
 		}
 
 		return $this->mSlaveDB;
+	}
+
+	/**
+	 * Get the continuation manager
+	 * @return ApiContinuationManager|null
+	 */
+	public function getContinuationManager() {
+		// Main module has getContinuationManager() method overridden
+		// Safety - avoid infinite loop:
+		if ( $this->isMain() ) {
+			ApiBase::dieDebug( __METHOD__, 'base method was called on main module. ' );
+		}
+
+		return $this->getMain()->getContinuationManager();
+	}
+
+	/**
+	 * Set the continuation manager
+	 * @param ApiContinuationManager|null
+	 */
+	public function setContinuationManager( $manager ) {
+		// Main module has setContinuationManager() method overridden
+		// Safety - avoid infinite loop:
+		if ( $this->isMain() ) {
+			ApiBase::dieDebug( __METHOD__, 'base method was called on main module. ' );
+		}
+
+		$this->getMain()->setContinuationManager( $manager );
 	}
 
 	/**@}*/
@@ -865,7 +899,7 @@ abstract class ApiBase extends ContextSource {
 							$value = $this->getMain()->canApiHighLimits()
 								? $paramSettings[self::PARAM_MAX2]
 								: $paramSettings[self::PARAM_MAX];
-							$this->getResult()->setParsedLimit( $this->getModuleName(), $value );
+							$this->getResult()->addParsedLimit( $this->getModuleName(), $value );
 						} else {
 							$value = intval( $value );
 							$this->validateLimit(
@@ -1241,28 +1275,8 @@ abstract class ApiBase extends ContextSource {
 	 * @param string $warning Warning message
 	 */
 	public function setWarning( $warning ) {
-		$result = $this->getResult();
-		$data = $result->getData();
-		$moduleName = $this->getModuleName();
-		if ( isset( $data['warnings'][$moduleName] ) ) {
-			// Don't add duplicate warnings
-			$oldWarning = $data['warnings'][$moduleName]['*'];
-			$warnPos = strpos( $oldWarning, $warning );
-			// If $warning was found in $oldWarning, check if it starts at 0 or after "\n"
-			if ( $warnPos !== false && ( $warnPos === 0 || $oldWarning[$warnPos - 1] === "\n" ) ) {
-				// Check if $warning is followed by "\n" or the end of the $oldWarning
-				$warnPos += strlen( $warning );
-				if ( strlen( $oldWarning ) <= $warnPos || $oldWarning[$warnPos] === "\n" ) {
-					return;
-				}
-			}
-			// If there is a warning already, append it to the existing one
-			$warning = "$oldWarning\n$warning";
-		}
-		$msg = array();
-		ApiResult::setContent( $msg, $warning );
-		$result->addValue( 'warnings', $moduleName,
-			$msg, ApiResult::OVERRIDE | ApiResult::ADD_ON_TOP | ApiResult::NO_SIZE_CHECK );
+		$msg = new ApiRawMessage( $warning, 'warning' );
+		$this->getErrorFormatter()->addWarning( $this->getModuleName(), $msg );
 	}
 
 	/**
@@ -2711,6 +2725,15 @@ abstract class ApiBase extends ContextSource {
 	public function getProfileDBTime() {
 		wfDeprecated( __METHOD__, '1.25' );
 		return 0;
+	}
+
+	/**
+	 * Get the result data array (read-only)
+	 * @deprecated since 1.25, use $this->getResult() methods instead
+	 * @return array
+	 */
+	public function getResultData() {
+		return $this->getResult()->getData();
 	}
 
 	/**@}*/

includes/api/ApiUpload.php
@@ -514,20 +514,20 @@ class ApiUpload extends ApiBase {
 					'filetype' => $verification['finalExt'],
 					'allowed' => array_values( array_unique( $this->getConfig()->get( 'FileExtensions' ) ) )
 				);
-				$this->getResult()->setIndexedTagName( $extradata['allowed'], 'ext' );
+				ApiResult::setIndexedTagName( $extradata['allowed'], 'ext' );
 
 				$msg = "Filetype not permitted: ";
 				if ( isset( $verification['blacklistedExt'] ) ) {
 					$msg .= join( ', ', $verification['blacklistedExt'] );
 					$extradata['blacklisted'] = array_values( $verification['blacklistedExt'] );
-					$this->getResult()->setIndexedTagName( $extradata['blacklisted'], 'ext' );
+					ApiResult::setIndexedTagName( $extradata['blacklisted'], 'ext' );
 				} else {
 					$msg .= $verification['finalExt'];
 				}
 				$this->dieUsage( $msg, 'filetype-banned', 0, $extradata );
 				break;
 			case UploadBase::VERIFICATION_ERROR:
-				$this->getResult()->setIndexedTagName( $verification['details'], 'detail' );
+				ApiResult::setIndexedTagName( $verification['details'], 'detail' );
 				$this->dieUsage( 'This file did not pass file verification', 'verification-error',
 					0, array( 'details' => $verification['details'] ) );
 				break;
@@ -559,7 +559,7 @@ class ApiUpload extends ApiBase {
 		if ( $warnings ) {
 			// Add indices
 			$result = $this->getResult();
-			$result->setIndexedTagName( $warnings, 'warning' );
+			ApiResult::setIndexedTagName( $warnings, 'warning' );
 
 			if ( isset( $warnings['duplicate'] ) ) {
 				$dupes = array();
@@ -567,7 +567,7 @@ class ApiUpload extends ApiBase {
 				foreach ( $warnings['duplicate'] as $dupe ) {
 					$dupes[] = $dupe->getName();
 				}
-				$result->setIndexedTagName( $dupes, 'duplicate' );
+				ApiResult::setIndexedTagName( $dupes, 'duplicate' );
 				$warnings['duplicate'] = $dupes;
 			}
 
@@ -696,7 +696,7 @@ class ApiUpload extends ApiBase {
 					);
 				}
 
-				$this->getResult()->setIndexedTagName( $error, 'error' );
+				ApiResult::setIndexedTagName( $error, 'error' );
 				$this->dieUsage( 'An internal error occurred', 'internal-error', 0, $error );
 			}
 			$result['result'] = 'Success';

commit d6f4df21316a1535a8cc8cfa1528ae4d4e309e95

[WIP] Allow passing detailed permission errors data to API
Using the new system introduced in
1c57794e371d74e1d88748de567a1c3358c3ad2e (see T47843).

This is the simplest thing that worked for my use case.
Not sure how well it will work in general.

Goes with I42a0c5b0ea7e61088dd609b764dd7d1396c60cd5 in TitleBlacklist.

Bug: T115258
Change-Id: I1334ba21a2862973a9d8ff5be2c9bec06a82698b

includes/OutputPage.php
@@ -2526,19 +2526,22 @@ class OutputPage extends ContextSource {
 			)->plain() . "\n\n";
 		}
 
-		if ( count( $errors ) > 1 ) {
-			$text .= '<ul class="permissions-errors">' . "\n";
-
-			foreach ( $errors as $error ) {
-				$text .= '<li>';
-				$text .= call_user_func_array( array( $this, 'msg' ), $error )->plain();
-				$text .= "</li>\n";
+		foreach ( $errors as &$error ) {
+			if ( $error instanceof MessageSpecifier ) {
+				$error = $this->msg( $error )->plain();
+			} else {
+				$error = call_user_func_array( array( $this, 'msg' ), $error )->plain();
 			}
-			$text .= '</ul>';
+		}
+
+		if ( count( $errors ) > 1 ) {
+			$text .= '<ul class="permissions-errors">' . "\n" .
+				'<li>' . implode( "</li>\n<li>", $errors ) . "</li>\n" .
+				'</ul>';
 		} else {
 			$text .= "<div class=\"permissions-errors\">\n" .
-					call_user_func_array( array( $this, 'msg' ), reset( $errors ) )->plain() .
-					"\n</div>";
+				reset( $errors ) .
+				"\n</div>";
 		}
 
 		return $text;

includes/Title.php
@@ -2048,8 +2048,11 @@ class Title {
 		if ( is_array( $result ) && count( $result ) && !is_array( $result[0] ) ) {
 			// A single array representing an error
 			$errors[] = $result;
-		} elseif ( is_array( $result ) && is_array( $result[0] ) ) {
-			// A nested array representing multiple errors
+		} elseif ( $result instanceof MessageSpecifier ) {
+			// A single message specifier representing an error
+			$errors[] = $result;
+		} elseif ( is_array( $result ) && ( is_array( $result[0] ) || $result[0] instanceof MessageSpecifier ) ) {
+			// Array or arrays or message specifiers representing multiple errors
 			$errors = array_merge( $errors, $result );
 		} elseif ( $result !== '' && is_string( $result ) ) {
 			// A string representing a message-id

includes/api/ApiBase.php
@@ -1991,10 +1991,18 @@ abstract class ApiBase extends ContextSource {
 
 	/**
 	 * Return the error message related to a certain array
-	 * @param array $error Element of a getUserPermissionsErrors()-style array
+	 * @param array|IApiMessage $error Element of a getUserPermissionsErrors()-style array
 	 * @return array('code' => code, 'info' => info)
 	 */
 	public function parseMsg( $error ) {
+		if ( $error instanceof IApiMessage ) {
+			return array(
+				'code' => $error->getApiCode(),
+				'info' => $error->text(),
+				'data' => $error->getApiData()
+			);
+		}
+
 		$error = (array)$error; // It seems strings sometimes make their way in here
 		$key = array_shift( $error );
 

includes/api/ApiUpload.php
@@ -363,6 +363,10 @@ class ApiUpload extends ApiBase {
 		$data['invalidparameter'] = $parameter;
 
 		$parsed = $this->parseMsg( $error );
+		if ( isset( $parsed['data'] ) ) {
+			$data = array_merge( $data, $parsed['data'] );
+		}
+
 		$this->dieUsage( $parsed['info'], $parsed['code'], 0, $data );
 	}
 