ba70383d1c77fd84e276ff6de032f9c7112cbd08
Timo Tijhof <krinklemail@gmail.com>
2015-10-20 22:14:33 +0100

    resourceloader: Remove CacheEpoch from MessageBlobStore::getFromDB
    CacheEpoch isn't meant for this (it's meant for page cache), I can't
    imagine a scenario in which we'd want to bump that to invalidate
    MessageBlobStore. It should be standalone and can be easily cleared
    if needed by truncating the relevant table (it's automatically
    repopulated).
    
    This also removes wfTimestamp/DateTime overhead.
    
    Change-Id: Iab06edbf71f20f3430207a80df90131c79dc03a7

--- includes/cache/MessageBlobStore.php
+++ includes/cache/MessageBlobStore.php
@@ -374,7 +374,6 @@ class MessageBlobStore {
 			return array();
 		}
 
-		$config = $resourceLoader->getConfig();
 		$retval = array();
 		$dbr = wfGetDB( DB_SLAVE );
 		$res = $dbr->select( 'msg_resource',
@@ -392,11 +391,9 @@ class MessageBlobStore {
 
 			// Update the module's blobs if the set of messages changed or if the blob is
 			// older than the CacheEpoch setting
-			$keys = array_keys( FormatJson::decode( $row->mr_blob, true ) );
-			$values = array_values( array_unique( $module->getMessages() ) );
-			if ( $keys !== $values
-				|| wfTimestamp( TS_MW, $row->mr_timestamp ) <= $config->get( 'CacheEpoch' )
-			) {
+			$blobKeys = array_keys( FormatJson::decode( $row->mr_blob, true ) );
+			$moduleMsgs = array_values( array_unique( $module->getMessages() ) );
+			if ( $blobKeys !== $moduleMsgs ) {
 				$retval[$row->mr_resource] = $this->updateModule( $row->mr_resource, $module, $lang );
 			} else {
 				$retval[$row->mr_resource] = $row->mr_blob;
