0abf9327d2773a11e3ef86c21409d622ea40b42f
Timo Tijhof <krinklemail@gmail.com>
2015-10-21 02:52:52 +0100

    Implement mw.requestIdleCallback for deferred background tasks
    We often use the idiom "window.onload" or "$(window).on('load')".
    Since code loads asynchronous, this is problematic because the event won't
    always be observed as it may fire before the event handler is attached.
    
    Most tasks also don't really want to wait until the page is loaded (in which
    case it would run immediately if the page is already loaded). Rather their intent
    is just to defer it to a later point in time – to avoid disrupting user events.
    
    Bug: T111456
    Change-Id: Ieba0440c6d83086762c777dfbbc167f1c314a751

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.requestIdleCallback.test.js
@@ -0,0 +1,117 @@
+( function ( mw ) {
+	QUnit.module( 'mediawiki.requestIdleCallback', QUnit.newMwEnvironment( {
+		setup: function () {
+			var time = mw.now(),
+				clock = this.clock = this.sandbox.useFakeTimers();
+
+			this.tick = function ( forward ) {
+				time += forward;
+				clock.tick( forward );
+			};
+			this.sandbox.stub( mw, 'now', function () {
+				return time;
+			} );
+
+			// Don't test the native version (if available)
+			this.mwRIC = mw.requestIdleCallback;
+			mw.requestIdleCallback = mw.requestIdleCallbackInternal;
+		},
+		teardown: function () {
+			mw.requestIdleCallback = this.mwRIC;
+		}
+	} ) );
+
+	// Basic scheduling of callbacks
+	QUnit.test( 'callback', 3, function ( assert ) {
+		var sequence,
+			tick = this.tick;
+
+		mw.requestIdleCallback( function () {
+			sequence.push( 'x' );
+			tick( 30 );
+		} );
+		mw.requestIdleCallback( function () {
+			tick( 5 );
+			sequence.push( 'y' );
+			tick( 30 );
+		} );
+		// Task Z is not run in the first sequence because the
+		// first two tasks consumed the available 50ms budget.
+		mw.requestIdleCallback( function () {
+			sequence.push( 'z' );
+			tick( 30 );
+		} );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [ 'x', 'y' ] );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [ 'z' ] );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [] );
+	} );
+
+	// Reschedule the same callback within a callback
+	QUnit.test( 'nest-reschedule', 3, function ( assert ) {
+		var sequence,
+			tick = this.tick;
+
+		mw.requestIdleCallback( function () {
+			sequence.push( 'x' );
+			tick( 30 );
+		} );
+		// Task Y is a task that schedules another task.
+		mw.requestIdleCallback( function () {
+			function other() {
+				sequence.push( 'y' );
+				tick( 35 );
+			}
+			mw.requestIdleCallback( other );
+		} );
+		mw.requestIdleCallback( function () {
+			sequence.push( 'z' );
+			tick( 30 );
+		} );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [ 'x', 'z' ] );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [ 'y' ] );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [] );
+	} );
+
+	// Schedule new callbacks within a callback
+	QUnit.test( 'nest-expand', 2, function ( assert ) {
+		var sequence,
+			tick = this.tick;
+
+		mw.requestIdleCallback( function () {
+			sequence.push( 'x' );
+			mw.requestIdleCallback( function () {
+				sequence.push( 'x-expand' );
+			} );
+		} );
+		mw.requestIdleCallback( function () {
+			sequence.push( 'y' );
+		} );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [ 'x', 'y', 'x-expand' ] );
+
+		sequence = [];
+		tick( 1000 );
+		assert.deepEqual( sequence, [] );
+	} );
+
+}( mediaWiki ) );
