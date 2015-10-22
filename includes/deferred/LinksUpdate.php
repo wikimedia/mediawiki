fe323f9b68bbbf8bef53f50959f428989774bdeb
Aaron Schulz <aschulz@wikimedia.org>
2015-10-21 21:59:47 -0700

    Make LinksUpdate job spec use "refreshLinksPrioritized"
    This was missing in 4fcc427c10 due to being lost in PS4
    
    Change-Id: I24641e7eccc1e61226777504dcfd75e1aa7f58c6

--- includes/deferred/LinksUpdate.php
+++ includes/deferred/LinksUpdate.php
@@ -939,7 +939,7 @@ class LinksUpdate extends SqlDataUpdate implements EnqueueableDataUpdate {
 		return array(
 			'wiki' => $this->mDb->getWikiID(),
 			'job'  => new JobSpecification(
-				'refreshLinks',
+				'refreshLinksPrioritized',
 				array(
 					'prioritize' => true,
 					// Reuse the parser cache if it was saved
