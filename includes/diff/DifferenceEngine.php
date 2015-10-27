6921b5cfdec92baf377549727e5d5fac12820293
Brian Wolff <bawolff+wn@gmail.com>
2015-10-27 02:31:00 -0600

    SECURITY: Add data attribute to patrol links so it can't be spoofed by user
    Javascript used to look just for the patrollinks class, which
    could be set by the user in order to patrol an arbitrary page.
    
    Bug: T103239
    Change-Id: I13fcc3ce479c0a4a90a6217c2e5244f051eaf862
    
    Signed-off-by: Chad Horohoe <chadh@wikimedia.org>

--- includes/diff/DifferenceEngine.php
+++ includes/diff/DifferenceEngine.php
@@ -474,7 +474,7 @@ class DifferenceEngine extends ContextSource {
 			if ( !$linkInfo ) {
 				$this->mMarkPatrolledLink = '';
 			} else {
-				$this->mMarkPatrolledLink = ' <span class="patrollink">[' . Linker::linkKnown(
+				$this->mMarkPatrolledLink = ' <span class="patrollink" data-mw="interface">[' . Linker::linkKnown(
 					$this->mNewPage,
 					$this->msg( 'markaspatrolleddiff' )->escaped(),
 					[],
