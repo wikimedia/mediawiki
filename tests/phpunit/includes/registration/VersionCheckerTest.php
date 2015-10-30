0f6864a9a58078e83daa024e0650c61dbfc1ab72
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-10-30 20:24:20 +0100

    registration: Allow specifying extension dependencies
    There are some extensoins that depend upon another extension or skin,
    usually in different ways:
    * A constant that is added in the dependency extension, and the
    existence of is checked for. This is problematic because it requires a
    specific load order.
    * Checking whether a specific class exists. This is problematic because
    it is extremely fragile, and breaks whenever the class is renamed.
    * Checking ExtensionRegistry::isLoaded(). This is mostly there, but it
    only checks at runtime, and doesn't provide any machine readable data.
    
    Furthermore, developers implement each one differently, with very little
    standardization.
    
    With this, extensions may now specify what other extensions they depend
    on. This is for explicit *hard* dependencies that must be installed.
    
    For example:
    	"requires": {
    		"MediaWiki": ">= 1.25.0",
    		"extensions": {
    			"FooBarBaz": "*"
    		}
    	}
    
    This would add a minimum requirement on MediaWiki 1.25.0+ (already
    implemented), as well as the requirement that the FooBarBaz extension be
    installed. A wildcard (*) is used instead of an explicit version
    requirement as many extensions do not actually version themselves, and
    there is no consistent versioning scheme yet.
    
    Bug: T117277
    Change-Id: If1cccee1a16a867a71bb0285691c400443d8a30a

---
+++ tests/phpunit/includes/registration/VersionCheckerTest.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * @covers CoreVersionChecker
+ */
+class VersionCheckerTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider provideCheck
+	 */
+	public function testCheck( $coreVersion, $constraint, $expected ) {
+		$checker = new VersionChecker();
+		$checker->setCoreVersion( $coreVersion );
+		$this->assertEquals( $expected, !(bool)$checker->checkArray( [
+				'FakeExtension' => [
+					'MediaWiki' => $constraint,
+				],
+			] )
+		);
+	}
+
+	public static function provideCheck() {
+		return [
+			// array( $wgVersion, constraint, expected )
+			[ '1.25alpha', '>= 1.26', false ],
+			[ '1.25.0', '>= 1.26', false ],
+			[ '1.26alpha', '>= 1.26', true ],
+			[ '1.26alpha', '>= 1.26.0', true ],
+			[ '1.26alpha', '>= 1.26.0-stable', false ],
+			[ '1.26.0', '>= 1.26.0-stable', true ],
+			[ '1.26.1', '>= 1.26.0-stable', true ],
+			[ '1.27.1', '>= 1.26.0-stable', true ],
+			[ '1.26alpha', '>= 1.26.1', false ],
+			[ '1.26alpha', '>= 1.26alpha', true ],
+			[ '1.26alpha', '>= 1.25', true ],
+			[ '1.26.0-alpha.14', '>= 1.26.0-alpha.15', false ],
+			[ '1.26.0-alpha.14', '>= 1.26.0-alpha.10', true ],
+			[ '1.26.1', '>= 1.26.2, <=1.26.0', false ],
+			[ '1.26.1', '^1.26.2', false ],
+			// Accept anything for un-parsable version strings
+			[ '1.26mwf14', '== 1.25alpha', true ],
+			[ 'totallyinvalid', '== 1.0', true ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideType
+	 */
+	public function testType( $given, $expected ) {
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( [
+				'FakeDependency' => [
+					'version' => '1.0.0',
+				],
+			] );
+		$this->assertEquals( $expected, $checker->checkArray( [
+				'FakeExtension' => $given,
+			] )
+		);
+	}
+
+	public static function provideType() {
+		return [
+			// valid type
+			[
+				[
+					'extensions' => [
+						'FakeDependency' => '1.0.0'
+					]
+				],
+				[]
+			],
+			[
+				[
+					'MediaWiki' => '1.0.0'
+				],
+				[]
+			],
+		];
+	}
+
+	/**
+	 * Check, if a non-parsable version constraint does not throw an exception or
+	 * returns any error message.
+	 */
+	public function testInvalidConstraint() {
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( [
+				'FakeDependency' => [
+					'version' => 'not really valid',
+				],
+			] );
+		$this->assertEquals( [ "Dependency FakeDependency provides an invalid version string." ],
+			$checker->checkArray( [
+				'FakeExtension' => [
+					'extensions' => [
+						'FakeDependency' => '1.24.3',
+					],
+				],
+			] )
+		);
+
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( [
+				'FakeDependency' => [
+					'version' => '1.24.3',
+				],
+			] );
+
+		$this->setExpectedException( 'UnexpectedValueException' );
+		$checker->checkArray( [
+			'FakeExtension' => [
+				'FakeDependency' => 'not really valid',
+			]
+		] );
+	}
+}
