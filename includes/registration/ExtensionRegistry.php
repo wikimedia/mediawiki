9e89e62cd04d1a92ec356ab995c8f569854fd956
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-10-30 20:24:20 +0100

    ExtensionRegistration: Implement Extension dependencies
    There are some extensions, that depend on another extension or skin,
    which actually is checked in php with:
     - A constant, that is added in the dependency extension and checked in
       the other one.
       -> the problem: The dependency extension needs to be loaded before the
          one, that depends on it
     - A check is a specific class is loaded with "class_exists" or
       "ExtensionRegistry::getInstance()->isLoaded( 'ExtensionName' )", if the
       extension is loaded with ExtensionRegistration
       -> Both doesn't support version checks (if a specific version of a
          dependency is installed or not) and both needs code written in php.
    
    Both solutions are, by the way, not machine readable and can differ in compared
    extensions by "Where they are implemented, extension.php entry point, wgExtensionFunctions,
    or maybe an extension.json callback)" and "What check is implemented". That makes it hard
    to define and document the "right" way. It would be great, if ExtensionRegistration could
    handle such and other dependencies for a developer.
    
    This change implements the possibility to name extensions, and a specific version constraint
    for each one, that this extension depends on. It uses the requires section for that, which
    already handles a dependency to MediaWiki core and could, in the future, handle other dependencies,
    too. An example for an extension.json, that depends on FakeExtension in version 1.2.0 or greater,
    would be:
    
    	"requires": {
    		"MediaWiki": ">= 1.25.0", // a dependency to MediaWiki core
    		"Extension:FakeExtension": ">=1.2.0" // a dependency to FakeExtension at least with version 1.2.0
    	},
    
    A developer needs to add the "Extension:" prefix (type) to all requirements, that are extensions. That allows
    (hopefully) the implementation of other requirements (that maybe follows, I don't know), if needed. So
    all dependencies can be distinguished (except the MediaWiki core special thing).
    
    Bug: T117277
    Change-Id: If1cccee1a16a867a71bb0285691c400443d8a30a

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -175,8 +175,7 @@ class ExtensionRegistry {
 		$autoloadClasses = array();
 		$autoloaderPaths = array();
 		$processor = new ExtensionProcessor();
-		$incompatible = array();
-		$coreVersionParser = new CoreVersionChecker( $wgVersion );
+		$extDependencies = array();
 		foreach ( $queue as $path => $mtime ) {
 			$json = file_get_contents( $path );
 			if ( $json === false ) {
@@ -198,16 +197,13 @@ class ExtensionRegistry {
 			// Set up the autoloader now so custom processors will work
 			$GLOBALS['wgAutoloadClasses'] += $autoload;
 			$autoloadClasses += $autoload;
-			// Check any constraints against MediaWiki core
+
+			// get all requirements/dependencies for this extension
 			$requires = $processor->getRequirements( $info );
-			if ( isset( $requires[self::MEDIAWIKI_CORE] )
-				&& !$coreVersionParser->check( $requires[self::MEDIAWIKI_CORE] )
-			) {
-				// Doesn't match, mark it as incompatible.
-				$incompatible[] = "{$info['name']} is not compatible with the current "
-					. "MediaWiki core (version {$wgVersion}), it requires: " . $requires[self::MEDIAWIKI_CORE]
-					. '.';
-				continue;
+
+			// validate the information needed and add the requirements
+			if ( is_array( $requires ) && $requires && isset( $info['name'] ) ) {
+				$extDependencies[$info['name']] = $requires;
 			}
 			// Get extra paths for later inclusion
 			$autoloaderPaths = array_merge( $autoloaderPaths,
@@ -215,6 +211,15 @@ class ExtensionRegistry {
 			// Compatible, read and extract info
 			$processor->extractInfo( $path, $info, $version );
 		}
+		$data = $processor->getExtractedInfo();
+
+		// check for incompatible extensions
+		$versionParser = new VersionChecker();
+		$incompatible = $versionParser
+			->setCoreVersion( $wgVersion )
+			->setLoaded( $data['credits'] )
+			->checkArray( $extDependencies );
+
 		if ( $incompatible ) {
 			if ( count( $incompatible ) === 1 ) {
 				throw new Exception( $incompatible[0] );
@@ -222,7 +227,6 @@ class ExtensionRegistry {
 				throw new Exception( implode( "\n", $incompatible ) );
 			}
 		}
-		$data = $processor->getExtractedInfo();
 		// Need to set this so we can += to it later
 		$data['globals']['wgAutoloadClasses'] = array();
 		$data['autoload'] = $autoloadClasses;
---
+++ tests/phpunit/includes/registration/VersionCheckerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @covers CoreVersionChecker
+ */
+class VersionCheckerTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider provideCheck
+	 */
+	public function testCheck( $coreVersion, $constraint, $expected ) {
+		$checker = new VersionChecker();
+		$checker->setCoreVersion( $coreVersion );
+		$this->assertEquals( $expected, !(bool)$checker->checkArray( array(
+				'FakeExtension' => array(
+					'MediaWiki' => $constraint,
+				),
+			) )
+		);
+	}
+
+	public static function provideCheck() {
+		return array(
+			// array( $wgVersion, constraint, expected )
+			array( '1.25alpha', '>= 1.26', false ),
+			array( '1.25.0', '>= 1.26', false ),
+			array( '1.26alpha', '>= 1.26', true ),
+			array( '1.26alpha', '>= 1.26.0', true ),
+			array( '1.26alpha', '>= 1.26.0-stable', false ),
+			array( '1.26.0', '>= 1.26.0-stable', true ),
+			array( '1.26.1', '>= 1.26.0-stable', true ),
+			array( '1.27.1', '>= 1.26.0-stable', true ),
+			array( '1.26alpha', '>= 1.26.1', false ),
+			array( '1.26alpha', '>= 1.26alpha', true ),
+			array( '1.26alpha', '>= 1.25', true ),
+			array( '1.26.0-alpha.14', '>= 1.26.0-alpha.15', false ),
+			array( '1.26.0-alpha.14', '>= 1.26.0-alpha.10', true ),
+			array( '1.26.1', '>= 1.26.2, <=1.26.0', false ),
+			array( '1.26.1', '^1.26.2', false ),
+			// Accept anything for un-parsable version strings
+			array( '1.26mwf14', '== 1.25alpha', true ),
+			array( 'totallyinvalid', '== 1.0', true ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideType
+	 */
+	public function testInvalidType( $type, $expected ) {
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( array(
+				'FakeDependency' => array(
+					'version' => '1.0.0',
+				),
+			) );
+		$this->assertEquals( $expected, !(bool)$checker->checkArray( array(
+				'FakeExtension' => array(
+					$type => '1.0.0',
+				)
+			) )
+		);
+	}
+
+	public static function provideType() {
+		return array(
+			// Invalid type
+			array( 'FakeType:FakeDependency', false ),
+			// valid type
+			array( 'Extension:FakeDependency', true ),
+			// check the protection of the MediaWiki core type
+			array( 'MediaWiki:FakeDependency', false ),
+			array( 'MediaWiki', true ),
+		);
+	}
+
+	/**
+	 * Check, if a non-parsable version constraint does not throw an exception or
+	 * returns any error message.
+	 */
+	public function testInvaliConstraint() {
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( array(
+				'FakeDependency' => array(
+					'version' => 'not really valid',
+				),
+			) );
+		$this->assertEquals( array(), $checker->checkArray( array(
+				'FakeExtension' => array(
+					'Extension:FakeDependency' => '1.24.3',
+				)
+			) )
+		);
+	}
+}
