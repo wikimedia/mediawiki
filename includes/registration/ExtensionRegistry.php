09f71389c681e2f4c93ea4cdc86b2a8a731b7618
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-10-30 20:24:20 +0100

    registration: Allow specifying extension dependencies
    There are some extensoins that depend upon another extension or skin,
    usually in different ways:
    * A constant that is added in the dependency extension, and the
    existence of is checked for. This is problematic because it requires a
    specific load order.
    * Checking whether a specific class exists. This is problematic because
    it is extremely fragile, and breaks whenever the class is renamed.
    * Checking ExtensionRegistry::isLoaded(). This is mostly there, but it
    only checks at runtime, and doesn't provide any machine readable data.
    
    Furthermore, developers implement each one differently, with very little
    standardization.
    
    With this, extensions may now specify what other extensions they depend
    on. This is for explicit *hard* dependencies that must be installed.
    
    For example:
    	"requires": {
    		"MediaWiki": ">= 1.25.0",
    		"extensions": {
    			"FooBarBaz": "*"
    		}
    	}
    
    This would add a minimum requirement on MediaWiki 1.25.0+ (already
    implemented), as well as the requirement that the FooBarBaz extension be
    installed. A wildcard (*) is used instead of an explicit version
    requirement as many extensions do not actually version themselves, and
    there is no consistent versioning scheme yet.
    
    Bug: T117277
    Change-Id: If1cccee1a16a867a71bb0285691c400443d8a30a

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -60,6 +60,13 @@ class ExtensionRegistry {
 	protected $queued = [];
 
 	/**
+	 * Whether we are done loading things
+	 *
+	 * @var bool
+	 */
+	private $finished = false;
+
+	/**
 	 * Items in the JSON file that aren't being
 	 * set as globals
 	 *
@@ -120,6 +127,12 @@ class ExtensionRegistry {
 			return;
 		}
 
+		if ( $this->finished ) {
+			throw new MWException(
+				"The following paths tried to load late: " . implode( ', ', $this->queued )
+			);
+		}
+
 		// A few more things to vary the cache on
 		$versions = [
 			'registration' => self::CACHE_VERSION,
@@ -165,6 +178,15 @@ class ExtensionRegistry {
 	}
 
 	/**
+	 * After this is called, no more extensions can be loaded
+	 *
+	 * @since 1.28
+	 */
+	public function finish() {
+		$this->finished = true;
+	}
+
+	/**
 	 * Process a queue of extensions and return their extracted data
 	 *
 	 * @param array $queue keys are filenames, values are ignored
@@ -176,8 +198,7 @@ class ExtensionRegistry {
 		$autoloadClasses = [];
 		$autoloaderPaths = [];
 		$processor = new ExtensionProcessor();
-		$incompatible = [];
-		$coreVersionParser = new CoreVersionChecker( $wgVersion );
+		$extDependencies = [];
 		foreach ( $queue as $path => $mtime ) {
 			$json = file_get_contents( $path );
 			if ( $json === false ) {
@@ -188,25 +209,9 @@ class ExtensionRegistry {
 				throw new Exception( "$path is not a valid JSON file." );
 			}
 
-			// Check any constraints against MediaWiki core
-			$requires = $processor->getRequirements( $info );
-			if ( isset( $requires[self::MEDIAWIKI_CORE] )
-				&& !$coreVersionParser->check( $requires[self::MEDIAWIKI_CORE] )
-			) {
-				// Doesn't match, mark it as incompatible.
-				$incompatible[] = "{$info['name']} is not compatible with the current "
-					. "MediaWiki core (version {$wgVersion}), it requires: " . $requires[self::MEDIAWIKI_CORE]
-					. '.';
-				continue;
-			}
-
 			if ( !isset( $info['manifest_version'] ) ) {
 				// For backwards-compatability, assume a version of 1
 				$info['manifest_version'] = 1;
-			}
-			$version = $info['manifest_version'];
-			if ( $version < self::OLDEST_MANIFEST_VERSION || $version > self::MANIFEST_VERSION ) {
-				throw new Exception( "$path: unsupported manifest_version: {$version}" );
 			}
 
 			$autoload = $this->processAutoLoader( dirname( $path ), $info );
@@ -214,12 +219,30 @@ class ExtensionRegistry {
 			$GLOBALS['wgAutoloadClasses'] += $autoload;
 			$autoloadClasses += $autoload;
 
+			// get all requirements/dependencies for this extension
+			$requires = $processor->getRequirements( $info );
+
+			// validate the information needed and add the requirements
+			if ( is_array( $requires ) && $requires && isset( $info['name'] ) ) {
+				$extDependencies[$info['name']] = $requires;
+			}
+			$manifestVersion = $info['manifest_version'];
+			$extDependencies[$info['name']]['manifest_version'] = $manifestVersion;
 			// Get extra paths for later inclusion
 			$autoloaderPaths = array_merge( $autoloaderPaths,
 				$processor->getExtraAutoloaderPaths( dirname( $path ), $info ) );
 			// Compatible, read and extract info
-			$processor->extractInfo( $path, $info, $version );
+			$processor->extractInfo( $path, $info, $manifestVersion );
 		}
+		$data = $processor->getExtractedInfo();
+
+		// check for incompatible extensions
+		$versionParser = new VersionChecker();
+		$incompatible = $versionParser
+			->setCoreVersion( $wgVersion )
+			->setLoaded( $data['credits'] )
+			->checkArray( $extDependencies );
+
 		if ( $incompatible ) {
 			if ( count( $incompatible ) === 1 ) {
 				throw new Exception( $incompatible[0] );
@@ -227,7 +250,6 @@ class ExtensionRegistry {
 				throw new Exception( implode( "\n", $incompatible ) );
 			}
 		}
-		$data = $processor->getExtractedInfo();
 		// Need to set this so we can += to it later
 		$data['globals']['wgAutoloadClasses'] = [];
 		$data['autoload'] = $autoloadClasses;
