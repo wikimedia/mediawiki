a72b6c2bc150b4e2aabb684168f9f1e55fbf6a53
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-10-30 20:24:20 +0100

    ExtensionRegistration: Implement Extension dependencies
    There are some extensions, that depend on another extension or skin,
    which actually is checked in php with:
     - A constant, that is added in the dependency extension and checked in
       the other one.
       -> the problem: The dependency extension needs to be loaded before the
          one, that depends on it
     - A check is a specific class is loaded with "class_exists" or
       "ExtensionRegistry::getInstance()->isLoaded( 'ExtensionName' )", if the
       extension is loaded with ExtensionRegistration
       -> Both doesn't support version checks (if a specific version of a
          dependency is installed or not) and both needs code written in php.
    
    Both solutions are, by the way, not machine readable and can differ in compared
    extensions by "Where they are implemented, extension.php entry point, wgExtensionFunctions,
    or maybe an extension.json callback)" and "What check is implemented". That makes it hard
    to define and document the "right" way. It would be great, if ExtensionRegistration could
    handle such and other dependencies for a developer.
    
    This change implements the possibility to name extensions, and a specific version constraint
    for each one, that this extension depends on. It uses the requires section for that, which
    already handles a dependency to MediaWiki core and could, in the future, handle other dependencies,
    too. An example for an extension.json, that depends on FakeExtension in version 1.2.0 or greater,
    would be:
    
    	"requires": {
    		"MediaWiki": ">= 1.25.0", // a dependency to MediaWiki core
    		"Extension:FakeExtension": ">=1.2.0" // a dependency to FakeExtension at least with version 1.2.0
    	},
    
    A developer needs to add the "Extension:" prefix (type) to all requirements, that are extensions. That allows
    (hopefully) the implementation of other requirements (that maybe follows, I don't know), if needed. So
    all dependencies can be distinguished (except the MediaWiki core special thing).
    
    Bug: T117277
    Change-Id: If1cccee1a16a867a71bb0285691c400443d8a30a

--- tests/phpunit/includes/registration/VersionCheckerTest.php
+++ tests/phpunit/includes/registration/VersionCheckerTest.php
@@ -3,13 +3,19 @@
 /**
  * @covers CoreVersionChecker
  */
-class CoreVersionCheckerTest extends PHPUnit_Framework_TestCase {
+class VersionCheckerTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider provideCheck
 	 */
 	public function testCheck( $coreVersion, $constraint, $expected ) {
-		$checker = new CoreVersionChecker( $coreVersion );
-		$this->assertEquals( $expected, $checker->check( $constraint ) );
+		$checker = new VersionChecker();
+		$checker->setCoreVersion( $coreVersion );
+		$this->assertEquals( $expected, !(bool)$checker->checkArray( array(
+				'FakeExtension' => array(
+					'MediaWiki' => $constraint,
+				),
+			) )
+		);
 	}
 
 	public static function provideCheck() {
@@ -33,6 +39,38 @@ class CoreVersionCheckerTest extends PHPUnit_Framework_TestCase {
 			// Accept anything for un-parsable version strings
 			array( '1.26mwf14', '== 1.25alpha', true ),
 			array( 'totallyinvalid', '== 1.0', true ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideType
+	 */
+	public function testInvalidType( $type, $expected ) {
+		$checker = new VersionChecker();
+		$checker
+			->setCoreVersion( '1.0.0' )
+			->setLoaded( array(
+				'FakeDependency' => array(
+					'version' => '1.0.0',
+				),
+			) );
+		$this->assertEquals( $expected, !(bool)$checker->checkArray( array(
+				'FakeExtension' => array(
+					$type => '1.0.0',
+				)
+			) )
+		);
+	}
+
+	public static function provideType() {
+		return array(
+			// Invalid type
+			array( 'FakeType:FakeDependency', false ),
+			// valid type
+			array( 'Extension:FakeDependency', true ),
+			// check the protection of the MediaWiki core type
+			array( 'MediaWiki:FakeDependency', false ),
+			array( 'MediaWiki', true ),
 		);
 	}
 }
