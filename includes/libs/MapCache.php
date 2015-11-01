0ee5f9522574d42963b7c27149dfffd203ce20a1
Ori Livneh <ori@wikimedia.org>
2015-10-31 22:47:26 -0700

    Add `MapCache` class, sans LRU
    Make MapCacheLRU extend an even simpler base class which provides the same
    get/set/clear interface for an associative array but which does not implement
    LRU eviction or a size limit.
    
    Change-Id: If9d95956e21194e3925604b1d4bf14a5f2f81b5d

---
+++ includes/libs/MapCache.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Per-process memory cache for storing items.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Cache
+ */
+
+/**
+ * Trivial get/set interface for an associative array
+ *
+ * @ingroup Cache
+ * @since 1.27
+ */
+class MapCache {
+	/** @var array */
+	protected $cache = array(); // (key => value)
+
+	/**
+	 * @param array $initialData Map of key/values to pre-load.
+	 */
+	public function __construct( $initialData = array() ) {
+		$this->cache = $initialData;
+	}
+
+	/**
+	 * Set a key/value pair.
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function set( $key, $value ) {
+		$this->cache[$key] = $value;
+	}
+
+	/**
+	 * Check if a key exists
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function has( $key ) {
+		return array_key_exists( $key, $this->cache );
+	}
+
+	/**
+	 * Get the value for a key.
+	 * This returns null if the key is not set.
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get( $key ) {
+		if ( !array_key_exists( $key, $this->cache ) ) {
+			return null;
+		}
+		return $this->cache[$key];
+	}
+
+	/**
+	 * @return array
+	 * @since 1.25
+	 */
+	public function getAllKeys() {
+		return array_keys( $this->cache );
+	}
+
+	/**
+	 * Clear one or several cache entries, or all cache entries
+	 *
+	 * @param string|array $keys
+	 * @return void
+	 */
+	public function clear( $keys = null ) {
+		if ( $keys === null ) {
+			$this->cache = array();
+		} else {
+			foreach ( (array)$keys as $key ) {
+				unset( $this->cache[$key] );
+			}
+		}
+	}
+}
