39c1b0a93b91a567cb70199f45ac9b7fe4479e21
Timo Tijhof <krinklemail@gmail.com>
2015-11-03 05:28:18 +0000

    objectcache: Move unit tests for HashBagOStuff to its own suite
    Follows-up 7cddc22.
    
    Also use PHPUnit_Framework_TestCase as parent instead of MediaWikiTestCase
    in preparation for library extraction.
    
    Change-Id: I0e68e56ecf8376b52a59c33ba6dd18b671bdcfc9

---
+++ tests/phpunit/includes/libs/objectcache/HashBagOStuffTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @group BagOStuff
+ */
+class HashBagOStuffTest extends PHPUnit_Framework_TestCase {
+
+	public function testHashBagEviction() {
+		$cache = new HashBagOStuff( array( 'maxKeys' => 10 ) );
+		for ( $i=0; $i<10; ++$i ) {
+			$cache->set( "key$i", 1 );
+			$this->assertEquals( 1, $cache->get( "key$i" ) );
+		}
+		for ( $i=10; $i<20; ++$i ) {
+			$cache->set( "key$i", 1 );
+			$this->assertEquals( 1, $cache->get( "key$i" ) );
+			$this->assertEquals( false, $cache->get( "key" . $i - 10 ) );
+		}
+	}
+}
