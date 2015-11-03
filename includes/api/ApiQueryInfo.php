c2478d60ea03125d7dcf5f95a2482770c92c1271
Fomafix <fomafix@googlemail.com>
2015-11-03 20:31:20 +0000

    ApiQueryInfo.php: Add code and direction of the page content language
    This change introduces two new keys in the response:
    * "pagelanguagehtmlcode" for the BCP 47 conform language code
    * "pagelanguagedir" for the direction of the language
    
    Example:
     "pagelanguagehtmlcode": "en-GB"
     "pagelanguagedir": "ltr"
    
    The existing key "pagelanguage" contains the internal language code format:
     "pagelanguage": "en-gb"
    
    This duplication avoids reimplementing wfBCP47() and a mapping from
    the language code to the direction of the language in JavaScript.
    
    Bug: T117392
    Change-Id: I26d033e33bfa60c8ff72ba70bb564cba4303a8b6

--- includes/api/ApiQueryInfo.php
+++ includes/api/ApiQueryInfo.php
@@ -388,6 +388,8 @@ class ApiQueryInfo extends ApiQueryBase {
 
 		$pageInfo['contentmodel'] = $title->getContentModel();
 		$pageInfo['pagelanguage'] = $title->getPageLanguage()->getCode();
+		$pageInfo['pagelanguagehtmlcode'] = $title->getPageLanguage()->getHtmlCode();
+		$pageInfo['pagelanguagedir'] = $title->getPageLanguage()->getDir();
 
 		if ( $titleExists ) {
 			$pageInfo['touched'] = wfTimestamp( TS_ISO_8601, $this->pageTouched[$pageid] );
