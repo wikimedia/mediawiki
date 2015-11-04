c71695879594be8a152c55d646c4aac65d9fc024
Brian Wolff <bawolff+wn@gmail.com>
2015-11-04 13:25:07 -0700

    Support all values for exif PhotometricInterpretation
    I just happened to notice that it wasn't displaying properly on
    File:Great_falls_of_the_potomac_river_washington_dc.tif .
    The original implementation only used values found in JPEG images,
    extend it to all allowed values in tiff files.
    
    See http://www.awaresystems.be/imaging/tiff/tifftags/photometricinterpretation.html
    for what the values mean
    
    Change-Id: I31b78d093668cec076b7f3599acee528229737f6

--- includes/media/FormatMetadata.php
+++ includes/media/FormatMetadata.php
@@ -190,8 +190,18 @@ class FormatMetadata extends ContextSource {
 
 					case 'PhotometricInterpretation':
 						switch ( $val ) {
+							case 0:
+							case 1:
 							case 2:
+							case 3:
+							case 4:
+							case 5:
 							case 6:
+							case 8:
+							case 9:
+							case 10:
+							case 32803:
+							case 34892:
 								$val = $this->exifMsg( $tag, $val );
 								break;
 							default:
