6971ceaf29f076dee5b2e97ca2e5e825406fddf8
Fomafix <fomafix@googlemail.com>
2015-11-05 16:54:42 +0100

    Rename language codes sr-ec and sr-el to sr-cyrl and sr-latn
    sr-ec and sr-el are not conform to BCP 47.
    BCP 47 explicit mentions sr-Latn and sr-Cyrl as examples.
    
    Bug: T117845
    Change-Id: I75da0af35a2066e7963e50c56c99daf1e07c55e6

--- languages/LanguageConverter.php
+++ languages/LanguageConverter.php
@@ -215,6 +215,9 @@ class LanguageConverter {
 	 * @return mixed Returns the variant if it is valid, null otherwise
 	 */
 	public function validateVariant( $variant = null ) {
+		// Replace deprecated language codes
+		$variant = LanguageCode::replaceDeprecatedCodes( $variant );
+
 		if ( $variant !== null && in_array( $variant, $this->mVariants ) ) {
 			return $variant;
 		}
