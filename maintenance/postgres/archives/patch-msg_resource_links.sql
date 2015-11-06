d43f53057116eb667e8136196c10f0cd8935c930
Timo Tijhof <krinklemail@gmail.com>
2015-11-06 23:43:44 +0000

    resourceloader: Remove obsolete msg_resource_links table
    No longer used as of Ia9131f570.
    
    Change-Id: If009e2620e59002e127d21b90a96bdd29e5d2a9d

--- maintenance/postgres/archives/patch-msg_resource_links.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE msg_resource_links (
-	mrl_resource TEXT NOT NULL,
-	mrl_message TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX mrl_message_resource ON msg_resource_links (mrl_message, mrl_resource);
