4279feff7df2b850b15f827429d6ce560bb0ed31
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-11-12 18:34:12 +0100

    WIP: Convert Special:Search input to OOUI
    Bug: T100898
    Change-Id: I24801495dfad71f457bc4afdd28474f74b219024

--- resources/Resources.php
+++ resources/Resources.php
@@ -1797,6 +1797,7 @@ return array(
 		'position' => 'top',
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.search.js',
 		'styles' => 'resources/src/mediawiki.special/mediawiki.special.search.css',
+		'dependencies' => 'mediawiki.widgets.SearchInputWidget',
 		'messages' => array(
 			'powersearch-togglelabel',
 			'powersearch-toggleall',
