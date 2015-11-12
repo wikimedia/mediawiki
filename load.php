3df53167a2e65688b8d7adb538745db636208d74
Timo Tijhof <krinklemail@gmail.com>
2015-11-12 22:36:50 +0000

    Remove PHP detection from entry points other than index.php
    The following entry points already didn't have it:
    
    * opensearch.php
    * thumb.php
    * thumb_handlers.php
    
    Now removed from api.php and load.php as well. The check was broken
    because these entry points use the 'use' keyword for PHP namespaces,
    which results in a syntax error on PHP < 5.3.
    
    The check remains in index.php, mw-config/index.php, and
    Maintenance (for e.g. install.php).
    
    Bug: T112942
    Change-Id: I9026d69ddfb3e02612256b4a9df525c432bb33c6

--- load.php
+++ load.php
@@ -24,11 +24,6 @@
 
 use MediaWiki\Logger\LoggerFactory;
 
-// Bail on old versions of PHP, or if composer has not been run yet to install
-// dependencies. Using dirname( __FILE__ ) here because __DIR__ is PHP5.3+.
-require_once dirname( __FILE__ ) . '/includes/PHPVersionCheck.php';
-wfEntryPointCheck( 'load.php' );
-
 require __DIR__ . '/includes/WebStart.php';
 
 
