8bd97ccb5c2598477741bdf423c8fe43b26181da
Timo Tijhof <krinklemail@gmail.com>
2015-11-16 21:41:10 +0000

    objectcache: Implement check keys per cache key in WANObjectCache::getMulti()
    To allow batch queries for multiple keys that themselves have different check
    keys. Previously check keys always applied to all keys being retrieved.
    
    Change-Id: I9e5ba198d79020ce05a802a510762e29fcfb2f1b

--- includes/libs/objectcache/WANObjectCache.php
+++ includes/libs/objectcache/WANObjectCache.php
@@ -217,7 +217,8 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 	 *
 	 * @param array $keys List of cache keys
 	 * @param array $curTTLs Map of (key => approximate TTL left) for existing keys [returned]
-	 * @param array $checkKeys List of "check" keys to apply to all of $keys
+	 * @param array $checkKeys List of "check" keys to apply to all $keys. Or associative array
+	 *  of "check key" lists keyed by cache keys.
 	 * @return array Map of (key => value) for keys that exist
 	 */
 	final public function getMulti(
@@ -228,26 +229,32 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 
 		$vPrefixLen = strlen( self::VALUE_KEY_PREFIX );
 		$valueKeys = self::prefixCacheKeys( $keys, self::VALUE_KEY_PREFIX );
-		$checkKeys = self::prefixCacheKeys( $checkKeys, self::TIME_KEY_PREFIX );
+
+		$checksForAll = array();
+		$checksByKey = array();
+		$checkKeysFlat = array();
+		foreach ( $checkKeys as $key => $checks ) {
+			$prefixed = self::prefixCacheKeys( (array)$checks, self::TIME_KEY_PREFIX );
+			$checkKeysFlat = array_merge( $checkKeysFlat, $prefixed );
+			// Is this check keys for a specific cache key, or for all keys being fetched?
+			if ( is_array( $checks ) ) {
+				$checksByKey[$key] = isset( $checksByKey[$key] )
+					? array_merge( $checksByKey[$key], $prefixed )
+					: $prefixed;
+			} else {
+				$checksForAll = array_merge( $checksForAll, $prefixed );
+			}
+		}
 
 		// Fetch all of the raw values
-		$wrappedValues = $this->cache->getMulti( array_merge( $valueKeys, $checkKeys ) );
+		$wrappedValues = $this->cache->getMulti( array_merge( $valueKeys, $checkKeysFlat ) );
 		$now = microtime( true );
 
-		// Get/initialize the timestamp of all the "check" keys
-		$checkKeyTime = null;
-		foreach ( $checkKeys as $checkKey ) {
-			$timestamp = isset( $wrappedValues[$checkKey] )
-				? self::parsePurgeValue( $wrappedValues[$checkKey] )
-				: false;
-			if ( !is_float( $timestamp ) ) {
-				// Key is not set or invalid; regenerate
-				$this->cache->add( $checkKey,
-					self::PURGE_VAL_PREFIX . $now, self::CHECK_KEY_TTL );
-				$timestamp = $now;
-			}
-
-			$checkKeyTime = $checkKeyTime ? min( $checkKeyTime, $timestamp ) : $timestamp;
+		// Get the lowest timestamp of all the "check" keys
+		$checkKeyTimeForAll = $this->processCheckKeys( $checksForAll, $wrappedValues, $now );
+		$checkKeyTimeByKey = array();
+		foreach ( $checksByKey as $key => $checks ) {
+			$checkKeyTimeByKey[$key] = $this->processCheckKeys( $checks, $wrappedValues, $now );
 		}
 
 		// Get the main cache value for each key and validate them
@@ -261,12 +268,18 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 			list( $value, $curTTL ) = $this->unwrap( $wrappedValues[$vKey], $now );
 			if ( $value !== false ) {
 				$result[$key] = $value;
-				if ( $checkKeyTime ) {
-					// Force dependant keys to be invalid for a while after purging
-					// to reduce race conditions involving stale data getting cached
-					$safeTimestamp = $checkKeyTime + self::HOLDOFF_TTL;
+				// Force dependant keys to be invalid for a while after purging
+				// to reduce race conditions involving stale data getting cached
+				if ( $checkKeyTimeForAll ) {
+					$safeTimestamp = $checkKeyTimeForAll + self::HOLDOFF_TTL;
 					if ( $safeTimestamp >= $wrappedValues[$vKey][self::FLD_TIME] ) {
-						$curTTL = min( $curTTL, $checkKeyTime - $now );
+						$curTTL = min( $curTTL, $checkKeyTimeForAll - $now );
+					}
+				}
+				if ( isset( $checkKeyTimeByKey[$key] ) ) {
+					$safeTimestamp = $checkKeyTimeByKey[$key] + self::HOLDOFF_TTL;
+					if ( $safeTimestamp >= $wrappedValues[$vKey][self::FLD_TIME] ) {
+						$curTTL = min( $curTTL, $checkKeyTimeByKey[$key] - $now );
 					}
 				}
 			}
@@ -275,6 +288,25 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 		}
 
 		return $result;
+	}
+
+	/**
+	 * @since 1.27
+	 */
+	private function processCheckKeys( Array $checks, Array $wrappedValues, $now ) {
+		$lowest = null;
+		foreach ( $checks as $check ) {
+			$timestamp = isset( $wrappedValues[$check] )
+				? self::parsePurgeValue( $wrappedValues[$check] )
+				: false;
+			if ( !is_float( $timestamp ) ) {
+				// Key is not set or invalid; regenerate
+				$this->cache->add( $check, self::PURGE_VAL_PREFIX . $now, self::CHECK_KEY_TTL );
+				$timestamp = $now;
+			}
+			$lowest = $lowest ? min( $lowest, $timestamp ) : $timestamp;
+		}
+		return $lowest;
 	}
 
 	/**
