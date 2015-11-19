54ba9d705761e434c9022ea0247cc0db7439e9eb
Timo Tijhof <krinklemail@gmail.com>
2015-11-19 05:57:58 +0000

    [WIP] Add MessageBlobStoreTest
    Change-Id: I3239c45f0243eca1eaf85bc53fca736e4c9a209c

---
+++ tests/phpunit/includes/resourceloader/MessageBlobStoreTest.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * @group Cache
+ * @covers MessageBlobStore
+ */
+class MessageBlobStoreTest extends PHPUnit_Framework_TestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		// MediaWiki tests defaults $wgMainWANCache to CACHE_NONE.
+		// Use hash instead so that caching is observed
+		$this->wanCache = $this->getMockBuilder( 'WANObjectCache' )
+			->setConstructorArgs( array( array(
+				'cache' => new HashBagOStuff(),
+				'pool' => 'test',
+				'relayer' => new EventRelayerNull( array() )
+			) ) )
+			->setMethods( array( 'getHoldOffTTL' ) )
+			->getMock();
+
+		// Disable HOLDOFF_TTL as it messes with testing
+		$this->wanCache->expects( $this->any() )
+			->method( 'getHoldOffTTL' )
+			->will( $this->returnValue( 0 ) );
+	}
+
+	protected function makeBlobStore( $methods = null, $rl = null ) {
+		$blobStore = $this->getMockBuilder( 'MessageBlobStore' )
+			->setConstructorArgs( array( $rl ) )
+			->setMethods( $methods )
+			->getMock();
+
+		$access = TestingAccessWrapper::newFromObject( $blobStore );
+		$access->wanCache = $this->wanCache;
+		return $blobStore;
+	}
+
+	protected function makeModule( array $messages ) {
+		$module = new ResourceLoaderTestModule( array( 'messages' => $messages ) );
+		$module->setName( 'test.blobstore' );
+		return $module;
+	}
+
+	public function testGetBlob() {
+		$blobStore = $this->makeBlobStore( array( 'fetchMessage' ) );
+		$blobStore->expects( $this->once() )
+			->method( 'fetchMessage' )
+			->will( $this->returnValue( 'Example' ) );
+
+		$module = $this->makeModule( array( 'foo' ) );
+		$blob = $blobStore->getBlob( $module, 'en' );
+
+		$this->assertEquals( '{"foo":"Example"}', $blob, 'Generated blob' );
+	}
+
+	public function testGetBlobCached() {
+		$module = $this->makeModule( array( 'example' ) );
+		$rl = new ResourceLoader();
+		$rl->register( $module->getName(), $module );
+
+		$blobStore = $this->makeBlobStore( array( 'fetchMessage' ) );
+		$blobStore->expects( $this->once() )
+			->method( 'fetchMessage' )
+			->will( $this->onConsecutiveCalls( 'First', 'Second' ) );
+
+		$module = $this->makeModule( array( 'example' ) );
+		$blob1 = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( '{"example":"First"}', $blob1, 'Generated blob' );
+
+		$module = $this->makeModule( array( 'example' ) );
+		$blob2 = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( $blob1, $blob2, 'Cache hit' );
+
+	}
+
+	public function testUpdateMessage() {
+		$module = $this->makeModule( array( 'example' ) );
+		$rl = new ResourceLoader();
+		$rl->register( $module->getName(), $module );
+
+		$blobStore = $this->makeBlobStore( array( 'fetchMessage' ), $rl );
+		$blobStore->expects( $this->exactly( 2 ) )
+			->method( 'fetchMessage' )
+			->will( $this->onConsecutiveCalls( 'First', 'Second' ) );
+
+		$blob = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( '{"example":"First"}', $blob, 'Generated blob' );
+
+		$blobStore->updateMessage( 'example' );
+
+		$module = $this->makeModule( array( 'example' ) );
+		$blob = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( '{"example":"Second"}', $blob, 'Updated blob' );
+	}
+
+	public function testValidation() {
+		$module = $this->makeModule( array( 'foo' ) );
+		$rl = new ResourceLoader();
+		$rl->register( $module->getName(), $module );
+
+		$blobStore = $this->makeBlobStore( array( 'fetchMessage' ), $rl );
+		$blobStore->expects( $this->exactly( 1 ) )
+			->method( 'fetchMessage' )
+			->will( $this->returnValueMap( array(
+				array( 'foo', 'en', 'Hello' ),
+			) ) );
+
+		$blob = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( '{"foo":"Hello"}', $blob, 'Generated blob' );
+
+		// Now, imagine a change to the module is deployed. The module now contains
+		// message 'foo' and 'bar'. While updateMessage() was not called (since no
+		// message values were changed) it should detect the change in list of
+		// message keys.
+		$module = $this->makeModule( array( 'foo', 'bar' ) );
+		$rl = new ResourceLoader();
+		$rl->register( $module->getName(), $module );
+
+		$blobStore = $this->makeBlobStore( array( 'fetchMessage' ), $rl );
+		$blobStore->expects( $this->exactly( 2 ) )
+			->method( 'fetchMessage' )
+			->will( $this->returnValueMap( array(
+				array( 'foo', 'en', 'Hello' ),
+				array( 'bar', 'en', 'World' ),
+			) ) );
+
+		$blob = $blobStore->getBlob( $module, 'en' );
+		$this->assertEquals( '{"foo":"Hello","bar":"World"}', $blob, 'Updated blob' );
+	}
+}
