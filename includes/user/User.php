f5db0b307b45cbd236e4426440653e697ef4cf80
Gergő Tisza <tgr.huwiki@gmail.com>
2015-11-21 11:51:02 -0800

    Use hash_equals in User::matchEditToken
    There is no point in using hash_equals for the return value if we
    do a normal comparison before.
    
    Bug: T119309
    Signed-off-by: Chad Horohoe <chadh@wikimedia.org>
    Change-Id: Ia44ec5ed492105b27d0fddd845d58d27a29dc072

--- includes/user/User.php
+++ includes/user/User.php
@@ -4228,7 +4228,7 @@ class User implements IDBAccessObject {
 			$salt, $request ?: $this->getRequest(), $timestamp
 		);
 
-		if ( $val != $sessionToken ) {
+		if ( !hash_equals( $sessionToken, $val ) ) {
 			wfDebug( "User::matchEditToken: broken session data\n" );
 		}
 
