d63e69480a4997d5892b6423fb4f6ed06fba56c6
Brad Jorsch <bjorsch@wikimedia.org>
2015-11-22 20:17:00 +0000

    WIP: Add AuthManager
    This implements the AuthManager class and its needed interfaces and
    subclasses, and integrates them into the backend portion of MediaWiki.
    Integration with frontend portions of MediaWiki (e.g. ApiLogin,
    Special:Login) is left for a followup.
    
    Bug: T91699
    Bug: T71589
    Bug: T111299
    Change-Id: If89d24838e326fe25fe867d02181eebcfbb0e196

---
+++ tests/phpunit/includes/auth/AbstractSecondaryAuthenticationProviderTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace MediaWiki\Auth;
+
+/**
+ * @group AuthManager
+ * @covers MediaWiki\Auth\AbstractSecondaryAuthenticationProvider
+ */
+class AbstractSecondaryAuthenticationProviderTest extends \MediaWikiTestCase {
+	public function testAbstractSecondaryAuthenticationProvider() {
+		$provider = $this->getMockForAbstractClass(
+			'MediaWiki\\Auth\\AbstractSecondaryAuthenticationProvider'
+		);
+
+		try {
+			$provider->continueSecondaryAuthentication( null, array() );
+			$this->fail( 'Expected exception not thrown' );
+		} catch ( \BadMethodCallException $ex ) {
+		}
+
+		try {
+			$provider->continueSecondaryAccountCreation( null, array() );
+			$this->fail( 'Expected exception not thrown' );
+		} catch ( \BadMethodCallException $ex ) {
+		}
+
+		$req = $this->getMockForAbstractClass( 'MediaWiki\\Auth\\AuthenticationRequest' );
+
+		$this->assertTrue( $provider->providerAllowsPropertyChange( 'foo' ) );
+		$this->assertEquals(
+			\StatusValue::newGood( 'ignored' ),
+			$provider->providerAllowsAuthenticationDataChange( $req )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testForAccountCreation( null, null, array() )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testUserForCreation( null, true )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testUserForCreation( null, false )
+		);
+
+		$provider->providerChangeAuthenticationData( $req );
+		$provider->autoCreatedAccount( null );
+	}
+
+	public function testProviderRevokeAccessForUser() {
+		$that = $this;
+		$reqs = array();
+		for ( $i = 0; $i < 3; $i++ ) {
+			$reqs[$i] = $this->getMock( 'MediaWiki\\Auth\\AuthenticationRequest' );
+			$reqs[$i]->done = false;
+		}
+
+		$provider = $this->getMockBuilder( 'MediaWiki\\Auth\\AbstractSecondaryAuthenticationProvider' )
+			->setMethods( array( 'providerChangeAuthenticationData' ) )
+			->getMockForAbstractClass();
+		$provider->expects( $this->once() )->method( 'getAuthenticationRequests' )
+			->with(
+				$this->identicalTo( AuthManager::ACTION_REMOVE ),
+				$this->identicalTo( array( 'username' => 'UTSysop' ) )
+			)
+			->will( $this->returnValue( $reqs ) );
+		$provider->expects( $this->exactly( 3 ) )->method( 'providerChangeAuthenticationData' )
+			->will( $this->returnCallback( function ( $req ) use ( $that ) {
+				$that->assertSame( 'UTSysop', $req->username );
+				$that->assertFalse( $req->done );
+				$req->done = true;
+			} ) );
+
+		$provider->providerRevokeAccessForUser( 'UTSysop' );
+
+		foreach ( $reqs as $i => $req ) {
+			$this->assertTrue( $req->done, "#$i" );
+		}
+	}
+}
