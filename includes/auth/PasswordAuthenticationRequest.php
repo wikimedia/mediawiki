54d15a7e87ca40bff382d9e46477ce821edea7b0
Brad Jorsch <bjorsch@wikimedia.org>
2015-11-22 20:17:00 +0000

    WIP: Add AuthManager
    This implements the AuthManager class and its needed interfaces and
    subclasses, and integrates them into the backend portion of MediaWiki.
    Integration with frontend portions of MediaWiki (e.g. ApiLogin,
    Special:Login) is left for a followup.
    
    Bug: T91699
    Bug: T71589
    Bug: T111299
    Co-Authored-By: Gergő Tisza <gtisza@wikimedia.org>
    Change-Id: If89d24838e326fe25fe867d02181eebcfbb0e196

---
+++ includes/auth/PasswordAuthenticationRequest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Auth
+ */
+
+namespace MediaWiki\Auth;
+
+/**
+ * This is a value object for authentication requests with a username and password
+ * @ingroup Auth
+ * @since 1.27
+ */
+class PasswordAuthenticationRequest extends AuthenticationRequest {
+	/** @var string */
+	protected $type;
+
+	/** @var string Password */
+	public $password = null;
+
+	/** @var string Password, again */
+	public $retype = null;
+
+	/**
+	 * @param string $type One of:
+	 *   - 'login': (default) Used for logging in with an existing password.
+	 *   - 'create': Used for creating a new password (typically for a new account).
+	 *   - 'change': Used for changing an existing password.
+	 *   This will influence what labels are shown, and whether there is a password retype field.
+	 */
+	public function __construct( $type = 'login' ) {
+		if ( !in_array( $type, [ 'login', 'create', 'change' ], true ) ) {
+			throw new \InvalidArgumentException( 'Invalid type: ' . $type );
+		}
+		$this->type = $type;
+	}
+
+	public function getFieldInfo() {
+		// for password change it's nice to make extra clear that we are asking for the new password
+		$passwordLabel = $this->type === 'change' ? 'newpassword' : 'userlogin-yourpassword';
+		$retypeLabel = $this->type === 'change' ? 'retypenew' : 'yourpasswordagain';
+
+		$ret = [
+			'username' => [
+				'type' => 'string',
+				'label' => wfMessage( 'userlogin-yourname' ),
+				'help' => wfMessage( 'authmanager-username-help' ),
+			],
+			'password' => [
+				'type' => 'password',
+				'label' => wfMessage( $passwordLabel ),
+				'help' => wfMessage( 'authmanager-password-help' ),
+			],
+		];
+
+		switch ( $this->action ) {
+			case AuthManager::ACTION_LINK:
+			case AuthManager::ACTION_CHANGE:
+			case AuthManager::ACTION_REMOVE:
+				unset( $ret['username'] );
+				break;
+		}
+
+		if ( $this->type !== 'login' ) {
+			$ret['retype'] = [
+				'type' => 'password',
+				'label' => wfMessage( $retypeLabel ),
+				'help' => wfMessage( 'authmanager-retype-help' ),
+			];
+		}
+
+		return $ret;
+	}
+
+	public function describeCredentials() {
+		return [
+			'provider' => wfMessage( 'authmanager-provider-password' ),
+			'account' => new \RawMessage( '$1', [ $this->username ] ),
+		];
+	}
+}
