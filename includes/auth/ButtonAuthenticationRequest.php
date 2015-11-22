e548020a9461482ebada86b11cba40b6355f88c9
Brad Jorsch <bjorsch@wikimedia.org>
2015-11-22 20:17:00 +0000

    Add AuthManager
    This implements the AuthManager class and its needed interfaces and
    subclasses, and integrates them into the backend portion of MediaWiki.
    Integration with frontend portions of MediaWiki (e.g. ApiLogin,
    Special:Login) is left for a followup.
    
    Bug: T91699
    Bug: T71589
    Bug: T111299
    Co-Authored-By: Gergő Tisza <gtisza@wikimedia.org>
    Change-Id: If89d24838e326fe25fe867d02181eebcfbb0e196

---
+++ includes/auth/ButtonAuthenticationRequest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Auth
+ */
+
+namespace MediaWiki\Auth;
+
+use Message;
+
+/**
+ * This is an authentication request that just implements a simple button.
+ * @ingroup Auth
+ * @since 1.27
+ */
+class ButtonAuthenticationRequest extends AuthenticationRequest {
+	/** @var string */
+	protected $name;
+
+	/** @var Message */
+	protected $label;
+
+	/** @var Message */
+	protected $help;
+
+	/**
+	 * @param string $name Button name
+	 * @param Message $label Button label
+	 * @param Message $help Button help
+	 * @param bool $required The button is required for authentication to proceed.
+	 */
+	public function __construct( $name, Message $label, Message $help, $required = false ) {
+		$this->name = $name;
+		$this->label = $label;
+		$this->help = $help;
+		$this->required = $required ? self::REQUIRED : self::OPTIONAL;
+	}
+
+	public function getUniqueId() {
+		return parent::getUniqueId() . ':' . $this->name;
+	}
+
+	public function getFieldInfo() {
+		return [
+			$this->name => [
+				'type' => 'button',
+				'label' => $this->label,
+				'help' => $this->help,
+			]
+		];
+	}
+
+	/**
+	 * Fetch a ButtonAuthenticationRequest or subclass by name
+	 * @param AuthenticationRequest[] $reqs Requests to search
+	 * @param string $name Name to look for
+	 * @return ButtonAuthenticationRequest|null Returns null if there is not
+	 *  exactly one matching request.
+	 */
+	public static function getRequestByName( array $reqs, $name ) {
+		$requests = array_filter( $reqs, function ( $req ) use ( $name ) {
+			return $req instanceof ButtonAuthenticationRequest && $req->name === $name;
+		} );
+		return count( $requests ) === 1 ? reset( $requests ) : null;
+	}
+
+	/**
+	 * @codeCoverageIgnore
+	 */
+	public static function __set_state( $data ) {
+		if ( !isset( $data['label'] ) ) {
+			$data['label'] = new \RawMessage( '$1', $data['name'] );
+		} elseif ( is_string( $data['label'] ) ) {
+			$data['label'] = new \Message( $data['label'] );
+		} elseif ( is_array( $data['label'] ) ) {
+			$data['label'] = call_user_func_array( 'Message::newFromKey', $data['label'] );
+		}
+		if ( !isset( $data['help'] ) ) {
+			$data['help'] = new \RawMessage( '$1', $data['name'] );
+		} elseif ( is_string( $data['help'] ) ) {
+			$data['help'] = new \Message( $data['help'] );
+		} elseif ( is_array( $data['help'] ) ) {
+			$data['help'] = call_user_func_array( 'Message::newFromKey', $data['help'] );
+		}
+		$ret = new static( $data['name'], $data['label'], $data['help'] );
+		foreach ( $data as $k => $v ) {
+			$ret->$k = $v;
+		}
+		return $ret;
+	}
+}
