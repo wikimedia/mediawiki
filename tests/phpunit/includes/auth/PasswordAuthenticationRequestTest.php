8ac330e747265968736744e2be5c2fce149f04b9
Brad Jorsch <bjorsch@wikimedia.org>
2015-11-22 20:17:00 +0000

    WIP: Add AuthManager
    This implements the AuthManager class and its needed interfaces and
    subclasses, and integrates them into the backend portion of MediaWiki.
    Integration with frontend portions of MediaWiki (e.g. ApiLogin,
    Special:Login) is left for a followup.
    
    Bug: T91699
    Bug: T71589
    Bug: T111299
    Change-Id: If89d24838e326fe25fe867d02181eebcfbb0e196

---
+++ tests/phpunit/includes/auth/PasswordAuthenticationRequestTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace MediaWiki\Auth;
+
+require_once 'AuthenticationRequestTestCase.php';
+
+/**
+ * @group AuthManager
+ * @covers MediaWiki\Auth\PasswordAuthenticationRequest
+ */
+class PasswordAuthenticationRequestTest extends AuthenticationRequestTestCase {
+
+	protected function getInstance( array $args = array() ) {
+		return new PasswordAuthenticationRequest( $args[0] );
+	}
+
+	public static function provideGetFieldInfo() {
+		return array(
+			array( array( false ) ),
+			array( array( true ) ),
+		);
+	}
+
+	public function provideLoadFromSubmission() {
+		return array(
+			array(
+				'Empty request',
+				array( false ),
+				array(),
+				false
+			),
+			array(
+				'Empty request',
+				array( true ),
+				array(),
+				false
+			),
+			array(
+				'Username + password',
+				array( false ),
+				$data = array( 'username' => 'User', 'password' => 'Bar' ),
+				$data + array( 'needsRetype' => false ),
+			),
+			array(
+				'Username + password',
+				array( true ),
+				array( 'username' => 'User', 'password' => 'Bar' ),
+				false
+			),
+			array(
+				'Username + password + retype',
+				array( true ),
+				$data = array( 'username' => 'User', 'password' => 'Bar', 'retype' => 'baz' ),
+				$data + array( 'needsRetype' => true ),
+			),
+			array(
+				'Username empty',
+				array( false ),
+				array( 'username' => '', 'password' => 'Bar' ),
+				false
+			),
+			array(
+				'Username empty',
+				array( true ),
+				array( 'username' => '', 'password' => 'Bar', 'retype' => 'baz' ),
+				false
+			),
+			array(
+				'Password empty',
+				array( false ),
+				array( 'username' => 'User', 'password' => '' ),
+				false
+			),
+			array(
+				'Password empty',
+				array( false ),
+				array( 'username' => 'User', 'password' => '', 'retype' => 'baz' ),
+				false
+			),
+			array(
+				'Retype empty',
+				array( true ),
+				array( 'username' => 'User', 'password' => 'Bar', 'retype' => '' ),
+				false
+			),
+		);
+	}
+
+	public function testDescribeCredentials() {
+		$req = new PasswordAuthenticationRequest;
+		$req->username = 'UTSysop';
+		$ret = $req->describeCredentials();
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'provider', $ret );
+		$this->assertInstanceOf( 'MessageSpecifier', $ret['provider'] );
+		$this->assertSame( 'authmanager-provider-password', $ret['provider']->getKey() );
+		$this->assertArrayHasKey( 'account', $ret );
+		$this->assertInstanceOf( 'MessageSpecifier', $ret['account'] );
+		$this->assertSame( array( 'UTSysop' ), $ret['account']->getParams() );
+	}
+}
