49134396e3c059bfd254301842bfa08c25349eed
Brian Wolff <bawolff+wn@gmail.com>
2015-11-22 02:45:02 -0500

    Add support for blacklisting common passwords
    This changes the default config to not allow the top 25 passwords
    to be used by Sysop/Crats. This should almost certainly be set to
    a higher number, but I think its best to wait until after this is
    comitted to argue over what the best value is.
    
    I would expect that once this is comitted, there would be a config
    change for wmf wikis, so that there is no change until this has
    been discussed with the community.
    
    The included common password file was generated from the first
    50000 entries of
    https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_1000000.txt?raw=true
    50,000 seemed like a good tradeoff between filesize of a file
    that gets included in the tarballs. There's a script to generate
    a different file for people who want a bigger list.
    
    Change-Id: I26a9e8f2318a1eed33d7638b125695e8de3a9796

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -4352,6 +4352,10 @@ $wgActiveUserDays = 30;
  *	- PasswordCannotMatchUsername - Password cannot match username to
  *	- PasswordCannotMatchBlacklist - Username/password combination cannot
  *		match a specific, hardcoded blacklist.
+ *	- PasswordCannotBePopular - Blacklist passwords which are known to be
+ *		commonly chosen. Set to integer n to ban the top n passwords.
+ *		If you want to ban all common passwords on file, use the
+ *		PHP_INT_MAX constant.
  * @since 1.26
  */
 $wgPasswordPolicy = array(
@@ -4360,11 +4364,13 @@ $wgPasswordPolicy = array(
 			'MinimalPasswordLength' => 8,
 			'MinimumPasswordLengthToLogin' => 1,
 			'PasswordCannotMatchUsername' => true,
+			'PasswordCannotBePopular' => 25,
 		),
 		'sysop' => array(
 			'MinimalPasswordLength' => 8,
 			'MinimumPasswordLengthToLogin' => 1,
 			'PasswordCannotMatchUsername' => true,
+			'PasswordCannotBePopular' => 25,
 		),
 		'bot' => array(
 			'MinimalPasswordLength' => 8,
@@ -4384,6 +4390,7 @@ $wgPasswordPolicy = array(
 		'PasswordCannotMatchUsername' => 'PasswordPolicyChecks::checkPasswordCannotMatchUsername',
 		'PasswordCannotMatchBlacklist' => 'PasswordPolicyChecks::checkPasswordCannotMatchBlacklist',
 		'MaximalPasswordLength' => 'PasswordPolicyChecks::checkMaximalPasswordLength',
+		'PasswordCannotBePopular' => 'PasswordPolicyChecks::checkPopularPasswordBlacklist'
 	),
 );
 
@@ -7775,6 +7782,19 @@ $wgVirtualRestConfig = array(
  * @since 1.26
  */
 $wgSearchRunSuggestedQuery = true;
+
+/**
+ * Where popular password file is located.
+ *
+ * Default in core contains 50,000 most popular. This config
+ * allows you to change which file, in case you want to generate
+ * a password file with > 50000 entries in it.
+ *
+ * @see maintenance/createCommonPasswordCdb.php
+ * @since 1.27
+ * @var string path to file
+ */
+$wgPopularPasswordFile = __DIR__ . '/../serialized/commonpasswords.cdb';
 
 /**
  * For really cool vim folding this needs to be at the end:
