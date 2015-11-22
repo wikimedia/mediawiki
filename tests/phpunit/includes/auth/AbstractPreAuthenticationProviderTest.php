e31adedb4d9356a61395d6f02245adbd4f1c81d8
Brad Jorsch <bjorsch@wikimedia.org>
2015-11-22 20:17:00 +0000

    WIP: Add AuthManager
    This implements the AuthManager class and its needed interfaces and
    subclasses, and integrates them into the backend portion of MediaWiki.
    Integration with frontend portions of MediaWiki (e.g. ApiLogin,
    Special:Login) is left for a followup.
    
    Bug: T91699
    Bug: T71589
    Bug: T111299
    Change-Id: If89d24838e326fe25fe867d02181eebcfbb0e196

---
+++ tests/phpunit/includes/auth/AbstractPreAuthenticationProviderTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MediaWiki\Auth;
+
+/**
+ * @group AuthManager
+ * @covers MediaWiki\Auth\AbstractPreAuthenticationProvider
+ */
+class AbstractPreAuthenticationProviderTest extends \MediaWikiTestCase {
+	protected function setUp() {
+		global $wgDisableAuthManager;
+
+		parent::setUp();
+		if ( $wgDisableAuthManager ) {
+			$this->markTestSkipped( '$wgDisableAuthManager is set' );
+		}
+	}
+
+	public function testAbstractPreAuthenticationProvider() {
+		$provider = $this->getMockForAbstractClass(
+			'MediaWiki\\Auth\\AbstractPreAuthenticationProvider'
+		);
+
+		$this->assertEquals(
+			[],
+			$provider->getAuthenticationRequests( AuthManager::ACTION_LOGIN, [] )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testForAuthentication( [] )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testForAccountCreation( null, null, [] )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testUserForCreation( null, true )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testUserForCreation( null, false )
+		);
+		$this->assertEquals(
+			\StatusValue::newGood(),
+			$provider->testForAccountLink( null )
+		);
+	}
+}
