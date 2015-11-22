df6ee466c0f988c54131b6164369b8a910439ef2
Brian Wolff <bawolff+wn@gmail.com>
2015-11-22 02:45:02 -0500

    Add support for blacklisting common passwords
    This changes the default config to not allow the top 25 passwords
    to be used by Sysop/Crats. This should almost certainly be set to
    a higher number, but I think its best to wait until after this is
    comitted to argue over what the best value is.
    
    I would expect that once this is comitted, there would be a config
    change for wmf wikis, so that there is no change until this has
    been discussed with the community.
    
    The included common password file was generated from the first
    50000 entries of
    https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_1000000.txt?raw=true
    50,000 seemed like a good tradeoff between filesize of a file
    that gets included in the tarballs. There's a script to generate
    a different file for people who want a bigger list.
    
    Change-Id: I26a9e8f2318a1eed33d7638b125695e8de3a9796

---
+++ maintenance/createCommonPasswordCdb.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Create serialized/commonpasswords.cdb
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ */
+
+require_once __DIR__ . '/Maintenance.php';
+
+/**
+ * Maintenance script to create common password cdb database.
+ *
+ * Meant to take a file like https://github.com/danielmiessler/SecLists/blob/master/Passwords/10_million_password_list_top_1000000.txt?raw=true as input
+ * @see serialized/commonpasswords.cdb and PasswordPolicyChecks::checkPopularPasswordBlacklist
+ * @ingroup Maintenance
+ */
+class GenerateCommonPassword extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Generate CDB file of common passwords";
+		$this->addOption( 'output', "Alternative location to write CDB file to", false, true, 'o' );
+		$this->addOption( 'limit', "Max number of passwords to write", false, true, 'l' );
+		$this->addArg( 'inputfile', 'List of passwords (one per line) to use or - for stdin', true );
+	}
+
+	public function execute() {
+		global $IP;
+		$outfile = $this->getOption( 'output', "$IP/serialized/commonpasswords.cdb" );
+		$limit = $this->getOption( 'limit', PHP_INT_MAX );
+		$langEn = Language::Factory( 'en' );
+
+		$infile = $this->getArg();
+		if ( $infile === '-' ) {
+			$infile = 'php://stdin';
+		}
+
+		if ( !is_readable( $infile ) && $infile !== 'php://stdin' ) {
+			$this->error( "Cannot open input file $infile for reading", 1 );
+		}
+
+		$file = file_get_contents( $infile );
+		if ( $file === false ) {
+			$this->error( "Cannot read input file $infile", 1 );
+		}
+
+		$lines = explode( "\n", $file );
+		if ( $lines[count( $lines ) - 1] === '' ) {
+			unset( $lines[count( $lines ) - 1] );
+		}
+		try {
+			$db = \Cdb\Writer::open( $outfile );
+
+			$alreadyWritten = array();
+			$skipped = 0;
+			for( $i = 0; $i < count( $lines ) && ($i - $skipped) < $limit; $i++ ) {
+				$line = $langEn->lc( trim( $lines[$i] ) );
+				if ( $line === '' ) {
+					$this->error( "Line number " . ($i + 1) . " is blank?\n" );
+					$skipped++;
+					continue;
+				}
+				if ( isset( $alreadyWritten[$line] ) ) {
+					$this->error( "Password '$line' already written (line " . ($i + 1 ) .")\n" );
+					$skipped++;
+					continue;
+				}
+				$alreadyWritten[$line] = true;
+				$db->set( $line, $i+1-$skipped );
+			}
+			// All caps, so cannot conflict with potential password
+			$db->set( '_TOTALENTRIES', $i - $skipped );
+			$db->close();
+
+			$this->output( "Successfully wrote " . ($i - $skipped) . " (out of $i) passwords to $outfile\n" );
+		} catch( \Cdb\Exception $e ) {
+			$this->error( "Error writing cdb file: " . $e->getMessage(), 2 );
+		}
+
+	}
+}
+
+$maintClass = "GenerateCommonPassword";
+require_once RUN_MAINTENANCE_IF_MAIN;
