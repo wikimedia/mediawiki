343d71f4c319a6c3f086cbbbaa4c6d87eb10374b
Paladox <thomasmulhall410@yahoo.com>
2015-11-23 00:24:47 +0000

    Auth
    Change-Id: I4e24a5f86c7bed5abb6ef76c3ece1ef1d546a63f

---
+++ includes/auth/AuthBlacklist.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MediaWiki\Auth;
+
+use DatabaseBase;
+
+/**
+ * Manages a blacklist of users who should not be allowed to log in (but can perform any other
+ * action). Such users are typically used for system actions (e.g. maintenance scripts).
+ * Meant to be used by PrimaryAccountProvider::providerRevokeAccessForUser().
+ */
+class AuthBlacklist {
+	protected static $instance;
+
+	/** @var DatabaseBase */
+	public $db;
+
+	public static function getInstance() {
+		if ( !self::$instance ) {
+			self::$instance = new self( wfGetDB( DB_MASTER ) );
+		}
+		return self::$instance;
+	}
+
+	protected function __construct( DatabaseBase $db ) {
+		$this->db = $db;
+	}
+
+	/**
+	 * Add user to blacklist
+	 * @param string $username Username in canonical form
+	 */
+	public function add( $username ) {
+		$this->db->insert( 'auth_blacklist', array( 'ab_name' => $username ), __METHOD__,
+			array( 'IGNORE' ) );
+	}
+
+	/**
+	 * Check if a user is on the blacklist
+	 * @param string $username Username in canonical form
+	 * @return bool
+	 */
+	public function has( $username ) {
+		return !!$this->db->selectRowCount( 'auth_blacklist', '*',
+			array( 'ab_name' => $username ), __METHOD__ );
+	}
+}
