248f01203d3145ae444c9b90cd621d689f911ae4
cenarium <cenarium.sysop@gmail.com>
2015-11-25 06:29:47 +0100

    Add per-category editnotices
    This adds support for per-category editnotices using a new page_prop
    property, set during links update of the category page by checking if
    the message defining the associated editnotice exists. In addition,
    to avoid having to null edit the category page, a links update job is
    also run on creation or deletion of the editnotice.
    
    When editing a page, the categorylinks table is already queried with
    page and page_props to retrieve hidden categories. So this also checks
    for the 'editnoticecat' property to retrieve categories with a
    per-category editnotice, and displays it if it exists.
    
    Bug: T85372
    Change-Id: Id318d6184c3a6164bd1961666c73065668d8b34e

--- includes/deferred/LinksUpdate.php
+++ includes/deferred/LinksUpdate.php
@@ -168,6 +168,15 @@ class LinksUpdate extends DataUpdate implements EnqueueableDataUpdate {
 			$scopedLock = self::acquirePageLock( $this->getDB(), $this->mId );
 		}
 
+		# For categories, check if an associated editnotice exists
+		# If it does, set property so that these can be recovered when editing a page,
+		# without having to check all categories the page is in
+		if ( $this->mTitle->getNamespace() == NS_CATEGORY ) {
+			$editnoticeMsg = wfMessage( 'editnotice-category-' . $this->mTitle->getDBkey() );
+			if ( $editnoticeMsg->exists() ) {
+				$this->mProperties['editnoticecat'] = '';
+			}
+		}
 		Hooks::run( 'LinksUpdate', [ &$this ] );
 		$this->doIncrementalUpdate();
 
