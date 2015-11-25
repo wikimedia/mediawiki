84cdb1d68bffada338593c2244cde57610ede1d3
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-11-25 21:01:58 +0100

    MediaWiki Widgets: Add new SearchInputWidget
    SearchInputWidget is similar to a TitleInputWidget, but doesn't has
    a visible loading indication, doesn't highlight the first result and
    uses the opensearch api endpoint for suggestions, instead of
    prefixsearch.
    
    Bug: T118443
    Change-Id: I8b8098041fe2971389fa908d007d2e77255829ec

---
+++ includes/widget/SearchInputWidget.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * MediaWiki Widgets – TitleInputWidget class.
+ *
+ * @copyright 2011-2015 MediaWiki Widgets Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+namespace MediaWiki\Widget;
+
+/**
+ * Title input widget.
+ */
+class SearchInputWidget extends TitleInputWidget {
+
+	protected $pushPending = false;
+	protected $flagEmpty = false;
+	protected $highlightFirst = false;
+
+	/**
+	 * @param array $config Configuration options
+	 * @param int|null $config['pushPending'] Whether the input should be visually marked as
+	 *  "pending", while requesting suggestions (default: true)
+	 */
+	public function __construct( array $config = array() ) {
+		// Parent constructor
+		parent::__construct(
+			array_merge( array( 'infusable' => true, 'maxLength' => 255 ), $config )
+		);
+
+		// Properties, which are ignored in PHP and just shipped back to JS
+		if ( isset( $config['pushPending'] ) ) {
+			$this->pushPending = $config['pushPending'];
+		}
+
+		// Initialization
+		$this->addClasses( array( 'mw-widget-searchInputWidget' ) );
+	}
+
+	protected function getJavaScriptClassName() {
+		return 'mw.widgets.SearchInputWidget';
+	}
+
+	public function getConfig( &$config ) {
+		if ( $this->pushPending !== null ) {
+			$config['pushPending'] = $this->pushPending;
+		}
+		if ( $this->flagEmpty !== null ) {
+			$config['flagEmpty'] = $this->flagEmpty;
+		}
+		return parent::getConfig( $config );
+	}
+}
