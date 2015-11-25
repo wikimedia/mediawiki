f9428ccfb338a0f876ee74c6f57552d006856d11
Dan Duvall <dduvall@wikimedia.org>
2015-11-25 10:21:48 -0800

    [WIP] Experimenting with JS end-to-end tests
    We (Zeljko and Dan) paired on a simple proof of concept using Jasmine as
    a BDD test framework with selenium-webdriver, and implemented a simple
    PageObject pattern.
    
    See `README.md` for instructions on how to get started.
    
    Bug: T108874
    Change-Id: I618df656eea73afe5c62b3579063194a6b97e343

---
+++ tests/end-to-end/README.md
@@ -0,0 +1,6 @@
+Make sure you have a newish version of Node, NPM, and Chromedriver installed
+(`brew install node chromedriver` on OS X).
+
+    cd tests/end-to-end/
+    npm install
+    npm test
---
+++ tests/end-to-end/spec/create-account-spec.js
@@ -0,0 +1,38 @@
+/*jshint node: true */
+
+var selenium = require( 'selenium-webdriver' );
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+var baseURL = 'http://en.wikipedia.beta.wmflabs.org/wiki/';
+
+function Page( path ) {
+	this.url = baseURL + path;
+	this.elements = {};
+}
+
+Page.prototype.defineElements = function ( elements ) {
+	for ( var name in elements ) {
+		this.elements[ name ] = selenium.By.css( elements[ name ] );
+	}
+};
+
+var createAccountPage = new Page( 'Special:CreateAccount' );
+createAccountPage.defineElements( {
+	signUpForm: 'form[name=userlogin2]'
+} );
+
+describe( 'Account creation page', function () {
+	beforeEach( function ( done ) {
+		this.driver = new selenium.Builder(). withCapabilities( selenium.Capabilities.chrome() ). build();
+
+		this.driver.get( createAccountPage.url ).then( done );
+	} );
+
+	afterEach( function ( done ) {
+		this.driver.quit().then( done );
+	} );
+
+	it( 'Contains a sign-up form', function ( done ) {
+		this.driver.findElement( createAccountPage.elements.signUpForm ).then( done );
+	} );
+} );
