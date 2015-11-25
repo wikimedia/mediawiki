0adde8b5b85de331ab0bffb8a749ad1c8b6af492
cenarium <cenarium.sysop@gmail.com>
2015-11-25 06:29:47 +0100

    Add per-category editnotices
    This adds support for per-category editnotices using a new page_prop
    property, set during parse of the category page by checking if the
    message defining the associated editnotice exists. In addition, to
    avoid having to null edit the category page, the page_prop is also
    set on creation of the editnotice.
    
    When editing a page, the categorylinks table is already queried with
    page and page_props to retrieve hidden categories. So this also checks
    for the 'editnoticecat' property to retrieve categories with a
    per-category editnotice, and displays it if it exists.
    
    Bug: T85372
    Change-Id: Id318d6184c3a6164bd1961666c73065668d8b34e

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2493,7 +2493,22 @@ class EditPage {
 
 		$this->setHeaders();
 
-		if ( $this->showHeader() === false ) {
+		// get categories with either "hiddencat" or "editnoticecat" properties
+		$categories =  $this->page->getCategoriesWithProperties();
+		$hiddenCategories = array();
+		$editnoticeCategories = array();
+		foreach ( $categories as $cat => $props ) {
+			// make list of hidden categories
+			if ( isset( $props['hiddencat'] ) ) {
+				$hiddenCategories[] = Title::makeTitle( NS_CATEGORY, $cat );
+			}
+			// make list of categories to check for per-category editnotices
+			if ( isset( $props['editnoticecat'] ) ) {
+				$editnoticeCategories[] = $cat;
+			}
+		}
+
+		if ( $this->showHeader( $editnoticeCategories ) === false ) {
 			return;
 		}
 
@@ -2665,7 +2680,7 @@ class EditPage {
 			Linker::formatTemplates( $this->getTemplates(), $this->preview, $this->section != '' ) ) );
 
 		$wgOut->addHTML( Html::rawElement( 'div', [ 'class' => 'hiddencats' ],
-			Linker::formatHiddenCategories( $this->page->getHiddenCategories() ) ) );
+			Linker::formatHiddenCategories( $hiddenCategories ) ) );
 
 		$wgOut->addHTML( Html::rawElement( 'div', [ 'class' => 'limitreport' ],
 			self::getPreviewLimitReport( $this->mParserOutput ) ) );
@@ -2715,9 +2730,10 @@ class EditPage {
 	}
 
 	/**
+	 * @param array Array of categories to check for per-category editnotices
 	 * @return bool
 	 */
-	protected function showHeader() {
+	protected function showHeader( $editnoticeCategories ) {
 		global $wgOut, $wgUser, $wgMaxArticleSize, $wgLang;
 		global $wgAllowUserCss, $wgAllowUserJs;
 
@@ -2726,7 +2742,7 @@ class EditPage {
 		}
 
 		// Add edit notices
-		$editNotices = $this->mTitle->getEditNotices( $this->oldid );
+		$editNotices = $this->mTitle->getEditNotices( $this->oldid, $editnoticeCategories );
 		if ( count( $editNotices ) ) {
 			$wgOut->addHTML( implode( "\n", $editNotices ) );
 		} else {
