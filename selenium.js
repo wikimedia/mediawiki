519763c2496c932eb2030673cefc235a0f9b88be
Željko Filipin <zeljko.filipin@gmail.com>
2015-11-30 13:21:30 +0100

    WIP WebDriverJs
    Bug: T108874
    Change-Id: I17af185f8f08986ff67180d5ede2e71515467ace

---
+++ selenium.js
@@ -0,0 +1,16 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder().
+   withCapabilities(webdriver.Capabilities.chrome()).
+   build();
+
+driver.get('http://www.google.com');
+driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+driver.findElement(webdriver.By.name('btnG')).click();
+driver.wait(function() {
+ return driver.getTitle().then(function(title) {
+   return title === 'webdriver - Google pretraživanje';
+ });
+}, 1000);
+
+driver.quit();
