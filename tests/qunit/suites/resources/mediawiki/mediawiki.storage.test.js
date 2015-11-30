e2427e7dda694663d76917ee44f41edbd372c6ea
Ori Livneh <ori@wikimedia.org>
2015-11-30 12:41:43 -0800

    mediawiki.storage: Provide a wrapper for sessionStorage too
    T119146 provides a use-case for using sessionStorage. So far mw.storage
    was localStorage-specific. With a small modification, we can allow the
    Storage object to passed to the constructor, which allows us to create a
    wrapper around sessionStorage (mw.storage.session) with minimal code duplication.
    
    Bug: T121646
    Change-Id: I73bc82d9fa2359148fe1e50b6535bfa0dbe8bd3e

--- tests/qunit/suites/resources/mediawiki/mediawiki.storage.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.storage.test.js
@@ -1,35 +1,35 @@
 ( function ( mw ) {
 	QUnit.module( 'mediawiki.storage' );
 
-	QUnit.test( 'set/get with localStorage', 3, function ( assert ) {
-		this.sandbox.stub( mw.storage, 'localStorage', {
+	QUnit.test( 'set/get with browser support', 3, function ( assert ) {
+		this.sandbox.stub( mw.storage, 'store', {
 			setItem: this.sandbox.spy(),
 			getItem: this.sandbox.stub()
 		} );
 
 		mw.storage.set( 'foo', 'test' );
-		assert.ok( mw.storage.localStorage.setItem.calledOnce );
+		assert.ok( mw.storage.store.setItem.calledOnce );
 
-		mw.storage.localStorage.getItem.withArgs( 'foo' ).returns( 'test' );
-		mw.storage.localStorage.getItem.returns( null );
+		mw.storage.store.getItem.withArgs( 'foo' ).returns( 'test' );
+		mw.storage.store.getItem.returns( null );
 		assert.strictEqual( mw.storage.get( 'foo' ), 'test', 'Check value gets stored.' );
 		assert.strictEqual( mw.storage.get( 'bar' ), null, 'Unset values are null.' );
 	} );
 
-	QUnit.test( 'set/get without localStorage', 3, function ( assert ) {
-		this.sandbox.stub( mw.storage, 'localStorage', {
+	QUnit.test( 'set/get without browser support', 3, function ( assert ) {
+		this.sandbox.stub( mw.storage, 'store', {
 			getItem: this.sandbox.stub(),
 			removeItem: this.sandbox.stub(),
 			setItem: this.sandbox.stub()
 		} );
 
-		mw.storage.localStorage.getItem.throws();
+		mw.storage.store.getItem.throws();
 		assert.strictEqual( mw.storage.get( 'foo' ), false );
 
-		mw.storage.localStorage.setItem.throws();
+		mw.storage.store.setItem.throws();
 		assert.strictEqual( mw.storage.set( 'foo', 'test' ), false );
 
-		mw.storage.localStorage.removeItem.throws();
+		mw.storage.store.removeItem.throws();
 		assert.strictEqual( mw.storage.remove( 'foo', 'test' ), false );
 	} );
 
