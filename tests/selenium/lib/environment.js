c6fa56072f30a621b0eef7bcb51ce29b76f7a79c
Željko Filipin <zeljko.filipin@gmail.com>
2015-12-02 13:19:36 +0100

    WIP End-to-end tests with WebDriverJs + Mocha
    Our general approach in this experiment has been to use only a handful
    of well known and well maintained JS libraries as a basic test
    harness, then implement some simple patterns for handling of different
    environments and encapsulation of DOM selection (page objects).
    
    Pre-configured environments have been removed in favor of using
    MW_SCRIPT_PATH set in the environment and querying the API for the right
    article path to be used in qualifying relative article and page-object
    paths.
    
    Bug: T108874
    Change-Id: I540d7ad9d1aeea741a53903992d530d161519b76

---
+++ tests/selenium/lib/environment.js
@@ -0,0 +1,47 @@
+var process = require( 'process' ),
+    webdriver = require( 'selenium-webdriver' ),
+		nodemw = require('nodemw'),
+		url = require('url'),
+    env = {};
+
+env.Environment = function () {
+	this.browser = process.env.BROWSER || 'firefox';
+	this.scriptPath = process.env.MW_SCRIPT_PATH || 'http://127.0.0.1:8080/w';
+
+	this.url = url.parse(this.scriptPath);
+
+	this.api = new nodemw({
+		protocol: this.url.protocol,
+		server: this.url.hostname,
+		port: this.url.port,
+		path: this.url.path
+	});
+};
+
+env.Environment.prototype.init = function (done) {
+	var self = this;
+
+	this.api.getSiteInfo(['general'], function (err, data) {
+		if (err) throw err;
+
+		self.articleURL = function ( path ) {
+			return url.resolve(self.scriptPath, data.general.articlepath.replace('$1', path));
+		};
+
+		done();
+	});
+};
+
+env.Environment.prototype.articleURL = function ( path ) {
+	throw 'the MW article path is unknown';
+};
+
+env.Environment.prototype.pageURL = function ( page ) {
+	return this.articleURL(page.path);
+};
+
+env.Environment.prototype.startBrowser = function () {
+	return new webdriver.Builder().forBrowser( this.browser ).build();
+};
+
+module.exports = env;
