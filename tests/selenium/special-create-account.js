1da6e3a7059837304d4cb2a0556a06faa00c4285
Željko Filipin <zeljko.filipin@gmail.com>
2015-12-02 13:19:36 +0100

    WIP WebDriverJs + Mocha
    Bug: T108874
    Change-Id: I540d7ad9d1aeea741a53903992d530d161519b76

---
+++ tests/selenium/special-create-account.js
@@ -0,0 +1,36 @@
+/*jshint node: true */
+
+var assert = require( 'assert' ),
+	baseURL = 'http://en.wikipedia.beta.wmflabs.org/wiki/',
+	createAccountPage,
+	selenium = require( 'selenium-webdriver' ),
+	seTest = require( 'selenium-webdriver/testing' ),
+	timeout = 10000;
+
+function Page( path ) {
+	this.url = baseURL + path;
+	this.elements = {};
+}
+
+Page.prototype.defineElements = function ( elements ) {
+	for ( var name in elements ) {
+		this.elements[ name ] = selenium.By.css( elements[ name ] );
+	}
+};
+
+createAccountPage = new Page( 'Special:CreateAccount' );
+createAccountPage.defineElements( {
+	signUpForm: 'form[name=userlogin2]'
+} );
+
+seTest.describe( 'Special:CreateAccount', function () {
+	this.timeout( timeout );
+	seTest.it( 'Contains a sign-up form', function () {
+		var driver = new selenium.Builder().withCapabilities( selenium.Capabilities.chrome() ).build();
+		driver.get( createAccountPage.url );
+		driver.isElementPresent( createAccountPage.elements.signUpForm ).then( function ( present ) {
+			assert.equal( present, true );
+		} );
+		driver.quit();
+	} );
+} );
