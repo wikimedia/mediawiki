00a2754ac6a408a6cafd1db58f1949afca498328
Ori Livneh <ori@wikimedia.org>
2015-12-02 14:10:01 -0800

    Added UUIDv1 function to UIDGenerator.
    * Added some more unit tests.
    
    Change-Id: Ieb7e66b1319df2e9c6da1518cc9539f276da1838

--- tests/phpunit/includes/utils/UIDGeneratorTest.php
+++ tests/phpunit/includes/utils/UIDGeneratorTest.php
@@ -63,15 +63,45 @@ class UIDGeneratorTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
+	 * @covers UIDGenerator::newUUIDv1
+	 */
+	public function testUUIDv1() {
+		$ids = array();
+		for ( $i = 0; $i < 100; $i++ ) {
+			$id = UIDGenerator::newUUIDv1();
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{8}-[0-9a-f]{4}-1[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$!', $id ),
+				"UID $id has the right format" );
+			$ids[] = $id;
+
+			$id = UIDGenerator::newRawUUIDv1();
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{12}1[0-9a-f]{3}[89ab][0-9a-f]{15}$!', $id ),
+				"UID $id has the right format" );
+
+			$id = UIDGenerator::newRawUUIDv1( UIDGenerator::QUICK_RAND );
+			$this->assertEquals( true,
+				preg_match( '!^[0-9a-f]{12}1[0-9a-f]{3}[89ab][0-9a-f]{15}$!', $id ),
+				"UID $id has the right format" );
+		}
+
+		$this->assertEquals( array_unique( $ids ), $ids, "All generated IDs are unique." );
+	}
+
+	/**
 	 * @covers UIDGenerator::newUUIDv4
 	 */
 	public function testUUIDv4() {
+		$ids = array();
 		for ( $i = 0; $i < 100; $i++ ) {
 			$id = UIDGenerator::newUUIDv4();
+			$ids[] = $id;
 			$this->assertEquals( true,
 				preg_match( '!^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$!', $id ),
 				"UID $id has the right format" );
 		}
+
+		$this->assertEquals( array_unique( $ids ), $ids, 'All generated IDs are unique.' );
 	}
 
 	/**
