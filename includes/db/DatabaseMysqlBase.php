ea79bb841a490a1e1ecc8a01fc0ec2b33f658a97
Aaron Schulz <aschulz@wikimedia.org>
2015-12-03 16:37:56 -0800

    Make getLagFromPtHeartbeat() always use the LB cluster master entry
    Before, it just used the immediate master entry, which could be another
    slave. In that case, it may not even exists at all.
    
    Bug: T119648
    Change-Id: Iea970b81ad2c9855aafcccf0bb0662fc0b3a8d4d

--- includes/db/DatabaseMysqlBase.php
+++ includes/db/DatabaseMysqlBase.php
@@ -42,7 +42,8 @@ abstract class DatabaseMysqlBase extends Database {
 	 * Additional $params include:
 	 *   - lagDetectionMethod : set to one of (Seconds_Behind_Master,pt-heartbeat).
 	 *                          pt-heartbeat assumes the table is at heartbeat.heartbeat
-	 *                          and uses UTC timestamps in the heartbeat.ts column.
+	 *                          and uses UTC timestamps in the heartbeat.ts column. It also
+	 *                          assumes that binlog files use the default <host>-bin naming.
 	 *                          (https://www.percona.com/doc/percona-toolkit/2.2/pt-heartbeat.html)
 	 * @param array $params
 	 */
@@ -645,27 +646,24 @@ abstract class DatabaseMysqlBase extends Database {
 	 * @return bool|float
 	 */
 	protected function getLagFromPtHeartbeat() {
-		$key = wfMemcKey( 'mysql', 'master-server-id', $this->getServer() );
-		$masterId = intval( $this->srvCache->get( $key ) );
-		if ( !$masterId ) {
-			$res = $this->query( 'SHOW SLAVE STATUS', __METHOD__ );
-			$row = $res ? $res->fetchObject() : false;
-			if ( $row && strval( $row->Master_Server_Id ) !== '' ) {
-				$masterId = intval( $row->Master_Server_Id );
-				$this->srvCache->set( $key, $masterId, 30 );
-			}
-		}
-
-		if ( !$masterId ) {
+		$masterHost = $this->getLBInfo( 'clusterMasterHost' );
+		if ( $masterHost === null ) {
 			return false;
 		}
 
+		// If the master is "db1052", the column will be like "db1052-bin.002419".
+		// See http://dev.mysql.com/doc/refman/5.7/en/binary-log.html. This assumes
+		// the default host-based binary log naming.
+		$encLike = $this->buildLike( "{$masterHost}-bin.", $this->anyString() );
+		// Get the status row for this master; use the oldest for sanity in case the master
+		// has entries listed under different server IDs (which should really not happen).
 		$res = $this->query(
-			"SELECT TIMESTAMPDIFF(MICROSECOND,ts,UTC_TIMESTAMP(6)) AS Lag " .
-			"FROM heartbeat.heartbeat WHERE server_id = $masterId"
+			"SELECT MAX(TIMESTAMPDIFF(MICROSECOND,ts,UTC_TIMESTAMP(6))) AS Lag " .
+			"FROM heartbeat.heartbeat WHERE file $encLike"
 		);
+
 		$row = $res ? $res->fetchObject() : false;
-		if ( $row ) {
+		if ( $row && $row->Lag !== null ) {
 			return max( floatval( $row->Lag ) / 1e6, 0.0 );
 		}
 
