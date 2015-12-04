c0889e9c1a53446332915f332cb662a3135c2277
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-12-04 18:34:49 +0100

    Make mediawiki.page.patrol.ajax and the patrol link more flexible
    * Move the main logic from DifferenceEngine::markPatrolledLink() to
      DifferenceEngine::getMarkPatrolledLinkInfo(). Returning an array of information
      instead of a complete HTML string.
    * Move main logic from mediawiki.page.patrol.ajax.js to mediawiki.page.patrol.js and
      expose its methods under mw.page.patrol.
    
    Bug: T101491
    Change-Id: Id09ccf60aec9b693d7df648a6dfcde629545f620

---
+++ resources/src/mediawiki/page/patrol.ajax.js
@@ -0,0 +1,58 @@
+/*!
+ * Animate patrol links to use asynchronous API requests to
+ * patrol pages, rather than navigating to a different URI.
+ *
+ * @since 1.21
+ * @author Marius Hoch <hoo@online.de>
+ * @author Timo Tijhof <krinklemail@gmail.com>
+ * @author Florian Schmidt <florian.schmidt.welzow@t-online.de>
+ */
+( function ( mw, $ ) {
+	if ( !mw.user.tokens.exists( 'patrolToken' ) ) {
+		// Current user has no patrol right, or an old cached version of user.tokens
+		// that didn't have patrolToken yet.
+		return;
+	}
+	$( function () {
+		var $patrolLinks = $( '.patrollink a' );
+
+		mw.page.patrol.setup( $patrolLinks ).on( 'patrol', function ( promise, node ) {
+			var $spinner;
+
+			// Start preloading the notification module (normally loaded by mw.notify())
+			mw.loader.load( 'mediawiki.notification' );
+
+			// Hide the link and create a spinner to show it inside the brackets.
+			$spinner = $.createSpinner( {
+				size: 'small',
+				type: 'inline'
+			} );
+			$( node ).hide().after( $spinner );
+
+			promise
+			.done( function ( data ) {
+				// Remove all patrollinks from the page (including any spinners inside).
+				$patrolLinks.closest( '.patrollink' ).remove();
+				if ( data.patrol !== undefined ) {
+					// Success
+					var title = new mw.Title( data.patrol.title );
+					mw.notify( mw.msg( 'markedaspatrollednotify', title.toText() ) );
+				} else {
+					// This should never happen as errors should trigger fail
+					mw.notify( mw.msg( 'markedaspatrollederrornotify' ), { type: 'error' } );
+				}
+			} )
+			.fail( function ( error ) {
+				$spinner.remove();
+				// Restore the patrol link. This allows the user to try again
+				$patrolLinks.show();
+				if ( error === 'noautopatrol' ) {
+					// Can't patrol own
+					mw.notify( mw.msg( 'markedaspatrollederror-noautopatrol' ), { type: 'warn' } );
+				} else {
+					mw.notify( mw.msg( 'markedaspatrollederrornotify' ), { type: 'error' } );
+				}
+			} );
+		} );
+	} );
+}( mediaWiki, jQuery ) );
