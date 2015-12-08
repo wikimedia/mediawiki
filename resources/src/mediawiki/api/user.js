89c55dc655ae38e77f54c53fe58d621e2c1d1cb0
Bartosz Dziewoński <matma.rex@gmail.com>
2015-12-08 16:23:13 +0100

    mediawiki.api.user: New module, factored out of mediawiki.user
    The convenient getUserInfo() function can now be used with mw.ForeignApi.
    
    Bug: T117868
    Change-Id: Ic0b5fe3812723bf51a5cf6600437c480dc5abb8f

---
+++ resources/src/mediawiki/api/user.js
@@ -0,0 +1,37 @@
+/**
+ * @class mw.Api.plugin.user
+ * @since 1.27
+ */
+( function ( mw, $ ) {
+
+	$.extend( mw.Api.prototype, {
+
+		/**
+		 * Get the current user's groups and rights.
+		 *
+		 * @return {jQuery.Promise}
+		 * @return {Function} return.done
+		 * @return {Object} return.done.userInfo
+		 * @return {string[]} return.done.userInfo.groups User groups that the current user belongs to
+		 * @return {string[]} return.done.userInfo.rights Current user's rights
+		 */
+		getUserInfo: function () {
+			return this.get( {
+				action: 'query',
+				meta: 'userinfo',
+				uiprop: [ 'groups', 'rights' ]
+			} ).then( function ( data ) {
+				if ( data.query && data.query.userinfo ) {
+					return data.query.userinfo;
+				}
+				return $.Deferred().reject().promise();
+			} );
+		}
+	} );
+
+	/**
+	 * @class mw.Api
+	 * @mixins mw.Api.plugin.user
+	 */
+
+}( mediaWiki, jQuery ) );
