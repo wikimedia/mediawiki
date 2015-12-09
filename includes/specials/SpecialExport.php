50c7a62c74e2d5f87b4a8afd88be1f19eb519ffd
PranavK <pranavmk98@gmail.com>
2015-12-09 17:50:49 +0530

    Removed unnecessary blank line in Special:Export page
    Bug: T103870
    Change-Id: I5b314ef9204ca33d851a9fe88477d0be427e5199

--- includes/specials/SpecialExport.php
+++ includes/specials/SpecialExport.php
@@ -65,7 +65,13 @@ class SpecialExport extends SpecialPage {
 					 */
 					$catpages = $this->getPagesFromCategory( $t );
 					if ( $catpages ) {
-						$page .= implode( "\n", $catpages );
+						if ($request->getText('pages' )== '') {
+							$page .= implode( "\n", $catpages ) . "\n";
+						}
+						else {
+							$page .= "\n" . implode( "\n", $catpages );
+						}
+
 					}
 				}
 			}
