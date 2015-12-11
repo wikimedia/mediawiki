6db06d25dd72550a28da11466e450ca2a5e4049e
Bartosz Dziewoński <matma.rex@gmail.com>
2015-12-11 03:31:55 +0100

    mw.ForeignStructuredUpload.BookletLayout: A/B test of 4 different interfaces
    TODO:
    * Add styles (Prateek)
    * Perfect the wording (James)
    * Submit WikimediaEvents patch to record bucketing (Bartosz)
    * Make texts translateable (???)
    * Ensure that sufficient legalese is present somewhere for each option (???)
    * Ensure that each option suggests UploadWizard as an alternative (???)
    
    It turns out that people click the checkbox affirming that they are
    they author of the file and that they release it under CC BY-SA even
    when neither of those is true. So we're trying some interfaces that
    require a modicum of thought rather than just a click on "I agree".
    
      Option 1: The form we have right now, with a single checkbox.
    
      Option 2: Four checkboxes, each with a label explaining one facet of
        the requirements (own work; no pictures of copyrighted work;
        educational/useful; irrevocably released as CC BY-SA).
    
      Option 3: Some Yes/No questions structured so that 'Yes' is not
        always the right answer to continue uploading.
    
      Option 4: Longer introduction before a single checkbox (as in option
        1), with examples of good and unacceptable content.
    
    As only logged in users are able to upload files, we're able to bucket
    them into four groups by user ID number. When the user completes a
    file upload, the bucket number is saved server-side in a change tag by
    the companion patch Ixxxxxx (we could match the user IDs cross-wiki by
    username, but that sounds painful).
    
    For testing and debugging, add '?uploadbucket=N' to the URL to force
    given interface option to appear. Any completed upload won't count
    towards the bucket.
    
    Note that for expediency, the tested options all assume uploads to
    'shared' repository (that is, Wikimedia Commons). The winner's
    messages will be tweaked to work with 'local' and other targets too.
    
    Bug: T120867
    Change-Id: I557056b867c6a55ef2c9af321eb48893312632a3

--- resources/src/mediawiki/mediawiki.ForeignStructuredUpload.BookletLayout.js
+++ resources/src/mediawiki/mediawiki.ForeignStructuredUpload.BookletLayout.js
@@ -72,7 +72,34 @@
 	 * @inheritdoc
 	 */
 	mw.ForeignStructuredUpload.BookletLayout.prototype.renderUploadForm = function () {
+		var
+			query = location.search && location.search.match( /[?&]uploadbucket=(\d)/ ),
+			userId = mw.config.get( 'wgUserId' );
+		if ( !userId ) {
+			// Anonymous user. This can actually happen, because our software sucks.
+			// Display the UI anyway, and allow them to try submitting, but don't bucket.
+			this.shouldRecordBucket = false;
+			return this.renderUploadForm1();
+		} else {
+			// Testing and debugging
+			if ( query && query[ 1 ] ) {
+				this.shouldRecordBucket = false;
+				this.bucket = Number( query[ 1 ] );
+			} else {
+				// Logged in user
+				this.shouldRecordBucket = true;
+				this.bucket = ( userId % 4 ) + 1; // 1, 2, 3, 4
+			}
+			return this[ 'renderUploadForm' + this.bucket ]();
+		}
+	};
+
+	/**
+	 * Test option 1, the original one. See T120867.
+	 */
+	mw.ForeignStructuredUpload.BookletLayout.prototype.renderUploadForm1 = function () {
 		var fieldset, $ownWorkMessage, $notOwnWorkMessage,
+			onUploadFormChange,
 			ownWorkMessage, notOwnWorkMessage, notOwnWorkLocal,
 			validTargets = mw.config.get( 'wgForeignUploadTargets' ),
 			target = this.target || validTargets[ 0 ] || 'local',
@@ -133,22 +160,220 @@
 		] );
 		this.uploadForm = new OO.ui.FormLayout( { items: [ fieldset ] } );
 
-		// Validation
-		this.selectFileWidget.on( 'change', this.onUploadFormChange.bind( this ) );
-		this.ownWorkCheckbox.on( 'change', this.onUploadFormChange.bind( this ) );
+		onUploadFormChange = function () {
+			var file = this.selectFileWidget.getValue(),
+				ownWork = this.ownWorkCheckbox.isSelected(),
+				valid = !!file && ownWork;
+			this.emit( 'uploadValid', valid );
+		};
 
+		// Validation
+		this.selectFileWidget.on( 'change', onUploadFormChange.bind( this ) );
+		this.ownWorkCheckbox.on( 'change', onUploadFormChange.bind( this ) );
+
+		return this.uploadForm;
+	};
+
+	/**
+	 * Test option 2, idea A from T121021. See T120867.
+	 */
+	mw.ForeignStructuredUpload.BookletLayout.prototype.renderUploadForm2 = function () {
+		var fieldset, checkboxes, fields, onUploadFormChange;
+
+		this.selectFileWidget = new OO.ui.SelectFileWidget();
+		this.licenseCheckboxes = checkboxes = [
+			new OO.ui.CheckboxInputWidget(),
+			new OO.ui.CheckboxInputWidget(),
+			new OO.ui.CheckboxInputWidget(),
+			new OO.ui.CheckboxInputWidget()
+		];
+
+		fields = [
+			new OO.ui.FieldLayout( this.selectFileWidget, {
+				align: 'top',
+				label: mw.msg( 'upload-form-label-select-file' )
+			} ),
+			new OO.ui.FieldLayout( new OO.ui.LabelWidget( {
+				label: 'Thank you for donating an image to be used on {{SITENAME}}. You should only continue if it meets several tests:'
+			} ), {
+				align: 'top'
+			} ),
+			new OO.ui.FieldLayout( checkboxes[ 0 ], {
+				align: 'inline',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-ownwork' ],
+				label: 'It must be entirely your own creation, not just taken from the Web'
+			} ),
+			new OO.ui.FieldLayout( checkboxes[ 1 ], {
+				align: 'inline',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-noderiv' ],
+				label: 'It has to contain no work by anyone else, or inspired by them'
+			} ),
+			new OO.ui.FieldLayout( checkboxes[ 2 ], {
+				align: 'inline',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-useful' ],
+				label: 'It should be educational and useful for teaching others'
+			} ),
+			new OO.ui.FieldLayout( checkboxes[ 3 ], {
+				align: 'inline',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-ccbysa' ],
+				label: 'It must be OK to publish forever on the Internet under the Creative Commons license'
+			} )
+		];
+
+		fieldset = new OO.ui.FieldsetLayout( { items: fields } );
+		this.uploadForm = new OO.ui.FormLayout( { items: [ fieldset ] } );
+
+		this.uploadForm.$element.find( 'a' ).attr( 'target', '_blank' );
+
+		onUploadFormChange = function () {
+			var file = this.selectFileWidget.getValue(),
+				checks = checkboxes.every( function ( checkbox ) {
+					return checkbox.isSelected();
+				} ),
+				valid = !!file && checks;
+			this.emit( 'uploadValid', valid );
+		};
+
+		// Validation
+		this.selectFileWidget.on( 'change', onUploadFormChange.bind( this ) );
+		checkboxes[ 0 ].on( 'change', onUploadFormChange.bind( this ) );
+		checkboxes[ 1 ].on( 'change', onUploadFormChange.bind( this ) );
+		checkboxes[ 2 ].on( 'change', onUploadFormChange.bind( this ) );
+		checkboxes[ 3 ].on( 'change', onUploadFormChange.bind( this ) );
+
+		return this.uploadForm;
+	};
+
+	/**
+	 * Test option 3, idea D from T121021. See T120867.
+	 */
+	mw.ForeignStructuredUpload.BookletLayout.prototype.renderUploadForm3 = function () {
+		var fieldset, selects, selectFields, fields, onUploadFormChange;
+
+		this.selectFileWidget = new OO.ui.SelectFileWidget();
+		this.ownWorkCheckbox = new OO.ui.CheckboxInputWidget();
+
+		selects = [
+			new OO.ui.RadioSelectWidget( {
+				items: [
+					new OO.ui.RadioOptionWidget( { data: false, label: 'Yes' } ),
+					new OO.ui.RadioOptionWidget( { data: true, label: 'No' } )
+				]
+			} ),
+			new OO.ui.RadioSelectWidget( {
+				items: [
+					new OO.ui.RadioOptionWidget( { data: false, label: 'Yes' } ),
+					new OO.ui.RadioOptionWidget( { data: true, label: 'No' } )
+				]
+			} ),
+			new OO.ui.RadioSelectWidget( {
+				items: [
+					new OO.ui.RadioOptionWidget( { data: true, label: 'Yes' } ),
+					new OO.ui.RadioOptionWidget( { data: false, label: 'No' } )
+				]
+			} )
+		];
+
+		this.licenseSelectFields = selectFields = [
+			new OO.ui.FieldLayout( selects[ 0 ], {
+				align: 'top',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-question' ],
+				label: 'Did you download this image from a website?'
+			} ),
+			new OO.ui.FieldLayout( selects[ 1 ], {
+				align: 'top',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-question' ],
+				label: 'Did you get this from an image search?'
+			} ).toggle( false ),
+			new OO.ui.FieldLayout( selects[ 2 ], {
+				align: 'top',
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-question' ],
+				label: 'If its a photograph, did you click it yourself?'
+			} ).toggle( false )
+		];
+
+		// Choosing the right answer to each question shows the next question.
+		// Switching to wrong answer hides all subsequent questions.
+		selects.forEach( function ( select, i ) {
+			select.on( 'choose', function ( selectedOption ) {
+				if ( i + 1 === selectFields.length ) {
+					// Last question
+					return;
+				}
+				if ( !!selectedOption.getData() ) {
+					selectFields[ i + 1 ].toggle( true );
+				} else {
+					selectFields.slice( i + 1 ).forEach( function ( field ) {
+						field.fieldWidget.selectItem( null );
+						field.toggle( false );
+					} );
+				}
+			} );
+		} );
+
+		fields = [
+			new OO.ui.FieldLayout( this.selectFileWidget, {
+				align: 'top',
+				label: mw.msg( 'upload-form-label-select-file' )
+			} ),
+			selectFields[ 0 ],
+			selectFields[ 1 ],
+			selectFields[ 2 ],
+			new OO.ui.FieldLayout( this.ownWorkCheckbox, {
+				align: 'inline',
+				label: mw.message( 'foreign-structured-upload-form-label-own-work-message-shared' ).parseDom()
+			} )
+		];
+
+		fieldset = new OO.ui.FieldsetLayout( { items: fields } );
+		this.uploadForm = new OO.ui.FormLayout( { items: [ fieldset ] } );
+
+		this.uploadForm.$element.find( 'a' ).attr( 'target', '_blank' );
+
+		onUploadFormChange = function () {
+			var file = this.selectFileWidget.getValue(),
+				checkbox = this.ownWorkCheckbox.isSelected(),
+				questions = selects.every( function ( select ) {
+					return select.getSelectedItem() && !!select.getSelectedItem().getData();
+				} ),
+				valid = !!file && checkbox && questions;
+			this.ownWorkCheckbox.setDisabled( !questions );
+			this.emit( 'uploadValid', valid );
+		};
+
+		// Validation
+		this.selectFileWidget.on( 'change', onUploadFormChange.bind( this ) );
+		this.ownWorkCheckbox.on( 'change', onUploadFormChange.bind( this ) );
+		selects[ 0 ].on( 'choose', onUploadFormChange.bind( this ) );
+		selects[ 1 ].on( 'choose', onUploadFormChange.bind( this ) );
+		selects[ 2 ].on( 'choose', onUploadFormChange.bind( this ) );
+
+		return this.uploadForm;
+	};
+
+	/**
+	 * Test option 4, idea E from T121021. See T120867.
+	 */
+	mw.ForeignStructuredUpload.BookletLayout.prototype.renderUploadForm4 = function () {
+		var fieldset;
+		this.renderUploadForm1();
+		fieldset = this.uploadForm.getItems()[ 0 ];
+		// Note the index, we insert after the SelectFileWidget field
+		fieldset.addItems( [
+			new OO.ui.FieldLayout( new OO.ui.LabelWidget( {
+				label: '[additional content goes here...]'
+			} ), {
+				classes: [ 'mw-foreignStructuredUpload-bookletLayout-goodBadExamples' ],
+				align: 'top'
+			} )
+		], 1 );
 		return this.uploadForm;
 	};
 
 	/**
 	 * @inheritdoc
 	 */
-	mw.ForeignStructuredUpload.BookletLayout.prototype.onUploadFormChange = function () {
-		var file = this.selectFileWidget.getValue(),
-			ownWork = this.ownWorkCheckbox.isSelected(),
-			valid = !!file && ownWork;
-		this.emit( 'uploadValid', valid );
-	};
+	mw.ForeignStructuredUpload.BookletLayout.prototype.onUploadFormChange = function () {};
 
 	/**
 	 * @inheritdoc
@@ -247,7 +472,23 @@
 	mw.ForeignStructuredUpload.BookletLayout.prototype.clear = function () {
 		mw.ForeignStructuredUpload.BookletLayout.parent.prototype.clear.call( this );
 
-		this.ownWorkCheckbox.setSelected( false );
+		if ( this.ownWorkCheckbox ) {
+			this.ownWorkCheckbox.setSelected( false );
+		}
+		if ( this.licenseCheckboxes ) {
+			this.licenseCheckboxes.forEach( function ( checkbox ) {
+				checkbox.setSelected( false );
+			} );
+		}
+		if ( this.licenseSelectFields ) {
+			this.licenseSelectFields.forEach( function ( field, i ) {
+				field.fieldWidget.selectItem( null );
+				if ( i !== 0 ) {
+					field.toggle( false );
+				}
+			} );
+		}
+
 		this.categoriesWidget.setItemsFromData( [] );
 		this.dateWidget.setValue( '' ).setValidityFlag( true );
 	};
