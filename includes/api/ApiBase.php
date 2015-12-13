552b55ef3e29c56b3033f11b206ca0b21941090a
Kunal Mehta <legoktm@member.fsf.org>
2015-12-12 19:45:43 -0800

    API: Use $wgExtensionDirectory instead of assuming $IP/extensions
    Change-Id: I06553591b0b6156e945aad5176479eb71517e77d

--- includes/api/ApiBase.php
+++ includes/api/ApiBase.php
@@ -2447,13 +2447,14 @@ abstract class ApiBase extends ContextSource {
 
 		// Build map of extension directories to extension info
 		if ( self::$extensionInfo === null ) {
+			$extDir = $this->getConfig()->get( 'ExtensionDirectory' );
 			self::$extensionInfo = [
 				realpath( __DIR__ ) ?: __DIR__ => [
 					'path' => $IP,
 					'name' => 'MediaWiki',
 					'license-name' => 'GPL-2.0+',
 				],
-				realpath( "$IP/extensions" ) ?: "$IP/extensions" => null,
+				realpath( $extDir ) ?: $extDir => null,
 			];
 			$keep = [
 				'path' => null,
