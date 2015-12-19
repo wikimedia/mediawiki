83205adef9e995f6326d31f49e39d9641156d027
Florian <florian.schmidt.stargatewissen@gmail.com>
2015-12-19 16:25:45 +0100

    Fix not-loaded DbPageLanguage when Title::getPageLanguage() get's called
    If the Title object isn't the title of the current viewed WikiPage, the page_lang
    field of the database isn't requested. This results in the problem, that
    Title::getPageLanguage() always returns the default content language, even if
    the page language is different (changed with Special:PageLanguage, if
    wgPageLanguageUseDB is true). That is problematic for the Translate extension,
    which relies on the correct page language.
    
    This change makes sure, that getPageLanguage() always retusn the correct page
    language. If the page language isn't loaded already, Title::getPageLanguage()
    now does a database lookup (if $wgPageLanguageUseDB is true) to get the correct
    page language.
    
    Bug: T121666
    Change-Id: I0ae5ea39f7a124ed427ca5dfb26c1a116b27a94e

--- includes/specials/SpecialPageLanguage.php
+++ includes/specials/SpecialPageLanguage.php
@@ -110,6 +110,8 @@ class SpecialPageLanguage extends FormSpecialPage {
 	 * @return bool
 	 */
 	public function onSubmit( array $data ) {
+		global $wgContLang;
+
 		$title = Title::newFromText( $data['pagename'] );
 
 		// Check if title is valid
@@ -118,8 +120,7 @@ class SpecialPageLanguage extends FormSpecialPage {
 		}
 
 		// Get the default language for the wiki
-		// Returns the default since the page is not loaded from DB
-		$defLang = $title->getPageLanguage()->getCode();
+		$defLang = $wgContLang->getCode();
 
 		$pageId = $title->getArticleID();
 
