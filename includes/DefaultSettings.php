54195aafc24b293812aa5fd59cbce439f6e15998
Reedy <reedy@wikimedia.org>
2015-12-20 22:12:26 +0000

    Prep 1.26.2
    Change-Id: Iae3ab6fe60b348abf6f2723b2601bd64fb3261ba

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.26.1';
+$wgVersion = '1.26.2';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
