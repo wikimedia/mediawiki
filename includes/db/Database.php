eedaca0c717a589fd4bd2223d2278e8775c1f719
Aaron Schulz <aschulz@wikimedia.org>
2015-12-23 17:16:54 -0800

    Upgrade mismatched begin() warnings to exceptions
    I saw very few WMF logs for these in the last 7 days.
    
    Change-Id: I79f2f2c8117ffa887a550a793a9e490c2a26f721

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -3474,14 +3474,10 @@ abstract class DatabaseBase implements IDatabase {
 			} elseif ( !$this->mTrxAutomatic ) {
 				// We want to warn about inadvertently nested begin/commit pairs, but not about
 				// auto-committing implicit transactions that were started by query() via DBO_TRX
-				$msg = "$fname: Transaction already in progress (from {$this->mTrxFname}), " .
-					" performing implicit commit!";
-				wfWarn( $msg );
-				wfLogDBError( $msg,
-					$this->getLogContext( array(
-						'method' => __METHOD__,
-						'fname' => $fname,
-					) )
+				throw new DBUnexpectedError(
+					$this,
+					"$fname: Transaction already in progress (from {$this->mTrxFname}), " .
+						" performing implicit commit!"
 				);
 			} else {
 				// if the transaction was automatic and has done write operations
