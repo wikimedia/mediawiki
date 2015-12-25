0ab451b880e80e2fd61326c87ec6c36b7fdea1e5
This, that and the other <at.light@live.com.au>
2015-12-25 21:38:54 +1100

    Namespace translations for Gilaki (glk)
    Bug: T122127
    Change-Id: I8cf45d9ecdece90a0ae6d70f2436699fc14cff20

--- languages/messages/MessagesGlk.php
+++ languages/messages/MessagesGlk.php
@@ -12,3 +12,21 @@ $fallback = 'fa';
 
 $rtl = true;
 
+$namespaceNames = array(
+	NS_MEDIA            => 'مديا',
+	NS_SPECIAL          => 'خاص',
+	NS_TALK             => 'گب',
+	NS_USER             => 'کارگير',
+	NS_USER_TALK        => 'کارگيرˇ_گب',
+	NS_PROJECT_TALK     => 'مدي_$1',
+	NS_FILE             => 'فاىل',
+	NS_FILE_TALK        => 'فاىلˇ_گب',
+	NS_MEDIAWIKI        => 'مدياويکي',
+	NS_MEDIAWIKI_TALK   => 'مدياويکي_گب',
+	NS_TEMPLATE         => 'قالب',
+	NS_TEMPLATE_TALK    => 'قالبˇ_گب',
+	NS_HELP             => 'رانما',
+	NS_HELP_TALK        => 'رانما_گب',
+	NS_CATEGORY         => 'جرگه',
+	NS_CATEGORY_TALK    => 'جرگه_گب',
+);
