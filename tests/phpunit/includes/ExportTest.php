cae184e4cdfeba70ce5e622c64ee626c5dce98cc
mhutti1 <mhutti1@gmail.com>
2015-12-25 21:14:02 +0100

    Added a simple smoke test for export
    Added ExportTest.php to check that exported xml is valid.
    
    Bug: T116883
    Change-Id: I76151a6ab4f88ab1904627ad1d00ed487d0ab606

---
+++ tests/phpunit/includes/ExportTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Test class for Export methods.
+ *
+ * @group Database
+ *
+ * @author Isaac Hutt <mhutti1@gmail.com>
+ */
+class ExportTest extends MediaWikiLangTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		$this->setMwGlobals( array(
+			'wgContLang' => Language::factory( 'en' ),
+			'wgLanguageCode' => 'en',
+			'wgCapitalLinks' => true,
+		) );
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+	}
+
+	/**
+	 * @covers WikiExporter::pageByTitle
+	 * @dataProvider providePageTitle
+	 * @param string $pageTitle
+	 */
+	public function testPageByTitle( $pageTitle ) {
+		global $wgContLang;
+
+		$exporter = new WikiExporter(
+			$this->db,
+			WikiExporter::FULL
+		);
+
+		$title = Title::newFromText( $pageTitle );
+
+		ob_start();
+		$exporter->openStream();
+		$exporter->pageByTitle( $title );
+		$exporter->closeStream();
+		$xmlString = ob_get_clean();
+
+		// This throws error if invalid xml output
+		$xmlObject = simplexml_load_string( $xmlString );
+
+		/**
+		 * Check namespaces match xml
+		 * FIXME: PHP 5.3 support. When we don't support PHP 5.3,
+		 * add ->namespace to object and remove from array
+		 */
+		$xmlNamespaces = (array) $xmlObject->siteinfo->namespaces;
+		$xmlNamespaces = str_replace( ' ', '_', $xmlNamespaces['namespace'] );
+		unset ( $xmlNamespaces[ '@attributes' ] );
+		foreach ( $xmlNamespaces as &$namespaceObject ) {
+			if ( is_object( $namespaceObject ) ) {
+				$namespaceObject = '';
+			}
+		}
+
+		$actualNamespaces = (array) $wgContLang->getNamespaces();
+		$actualNamespaces = array_values( $actualNamespaces );
+		$this->assertEquals( $actualNamespaces, $xmlNamespaces );
+
+		// Check xml page title correct
+		$xmlTitle = (array) $xmlObject->page->title;
+		$this->assertEquals( $pageTitle, $xmlTitle[0] );
+
+		// Check xml page text is not empty
+		$text = (array) $xmlObject->page->revision->text;
+		$this->assertNotEquals( '', $text[0] );
+	}
+
+	public function providePageTitle() {
+		return array( array( 'UTPage' ) );
+	}
+
+}
