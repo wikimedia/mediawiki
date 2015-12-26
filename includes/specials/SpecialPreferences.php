69f23b5c3e9837cf56aebec9da7862b34647daf8
Fomafix <fomafix@googlemail.com>
2015-12-26 20:33:49 +0000

    Preferences: Use cookie instead of URL parameter for success
    This change avoids changing the URL for the success message.
    
    The URL manipulation in mediawiki.special.preferences.js is superfluous.
    
    Bug: T26700
    Change-Id: I1c2b011e7a66b2b9379dd4a3fdcc6f978dd43b52

--- includes/specials/SpecialPreferences.php
+++ includes/specials/SpecialPreferences.php
@@ -49,7 +49,10 @@ class SpecialPreferences extends SpecialPage {
 		$out->addModules( 'mediawiki.special.preferences' );
 		$out->addModuleStyles( 'mediawiki.special.preferences.styles' );
 
-		if ( $this->getRequest()->getCheck( 'success' ) ) {
+                $request = $this->getRequest();
+		if ( $request->getCookie( 'Success' ) ) {
+                        // Remove cookie
+                        $request->response()->setCookie( 'Success' );
 			$out->wrapWikiMsg(
 				Html::rawElement(
 					'div',
