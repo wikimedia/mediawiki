cc7fca0c14398baeb88b949cfad71846b6b05c83
Reedy <reedy@wikimedia.org>
2015-12-27 20:21:51 +0000

    Support phpunit-old.phar
    Bug: T122485
    Change-Id: Ic21c6c1ca5afaac0e298cf0259f5dc273dfec128

--- tests/phpunit/phpunit.php
+++ tests/phpunit/phpunit.php
@@ -250,9 +250,16 @@ if ( class_exists( 'PHPUnit_TextUI_Command' ) ) {
 } else {
 	foreach ( array(
 				stream_resolve_include_path( 'phpunit.phar' ),
+				stream_resolve_include_path( 'phpunit-old.phar' ),
 				'PHPUnit/Runner/Version.php',
 				'PHPUnit/Autoload.php'
 			) as $includePath ) {
+
+		if ( $includePath === false ) {
+			// stream_resolve_include_path can return false
+			continue;
+		}
+
 		// @codingStandardsIgnoreStart
 		@include_once $includePath;
 		// @codingStandardsIgnoreEnd
