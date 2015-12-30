28fb2a4217cf2273bb9553a64189f4f7dffe047b
Geoffrey Mon <geofbot@gmail.com>
2015-12-29 23:53:34 -0500

    Implement action=mergehistory
    * New class ApiMergeHistory handles action=mergehistory
    * Merge History functionality moved from SpecialMergeHistory to
      MergeHistory
    * SpecialMergeHistory now uses MergeHistory for actual merging
    * Unit tests and i18n messages for above
    
    Bug: T69742
    Change-Id: Ic5078307dae78a2b3687e34a5d0a584988d483a1

---
+++ tests/phpunit/includes/MergeHistoryTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @group Database
+ */
+class MergeHistoryTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider provideIsValidMerge
+	 * @covers MergeHistory::isValidMerge
+	 * @param $source string Source page
+	 * @param $dest string Destination page
+	 * @param $timestamp string|bool Timestamp up to which revisions are merged (or false for all)
+	 * @param $error string|bool Expected error for test (or true for no error)
+	 */
+	public function testIsValidMove( $source, $dest, $timestamp, $error ) {
+		$this->setMwGlobals( 'wgContentHandlerUseDB', false );
+		$mh = new MergeHistory(
+			Title::newFromText( $source ),
+			Title::newFromText( $dest ),
+			$timestamp
+		);
+		$status = $mh->isValidMerge();
+		if ( $error === true ) {
+			$this->assertTrue( $status->isGood() );
+		} else {
+			$this->assertTrue( $status->hasMessage( $error ) );
+		}
+	}
+
+	/**
+	 * Make some pages to work with
+	 */
+	public function addDBData() {
+		$this->insertPage( 'Test' );
+		$this->insertPage( 'Test2' );
+		$this->insertPage( 'oldpage' );
+
+		// Delay before inserting newpage so it has a timestamp after that of oldpage
+		sleep(1);
+		$this->insertPage( 'newpage' );
+	}
+
+	public static function provideIsValidMerge() {
+		return array(
+			// for MergeHistory::isValidMerge
+			array( 'Test', 'Test2', false, true ),
+			// Although this timestamp is after the latest timestamp of both pages,
+			// MergeHistory should select the latest source timestamp up to this which should
+			// still work for the merge.
+			array( 'Test', 'Test2', strtotime( 'tomorrow' ), true ),
+			array( 'Test', 'Test', false, 'mergehistory-fail-self-merge' ),
+			array( 'newpage', 'oldpage', false, 'mergehistory-fail-timestamps-overlap' ),
+			array( 'Nonexistant', 'Test2', false, 'mergehistory-fail-invalid-source' ),
+			array( 'Test', 'Nonexistant', false, 'mergehistory-fail-invalid-dest' ),
+			array(
+				'Test',
+				'Test2',
+				'This is obviously an invalid timestamp',
+				'mergehistory-fail-bad-timestamp'
+			),
+		);
+	}
+}
