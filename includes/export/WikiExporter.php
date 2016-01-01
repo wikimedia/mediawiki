41bf17a6fac0f22b27057a46fda454ddf58b4415
georggi <bmp2558@gmail.com>
2016-01-02 01:19:19 +0200

    Export: Added namespace aliases to siteinfo
    Bug: T64109
    Change-Id: I8bcf0b8e89431ded1d80da0c84623569f194b072

--- includes/export/WikiExporter.php
+++ includes/export/WikiExporter.php
@@ -2,7 +2,7 @@
 /**
  * Base class for exporting
  *
- * Copyright © 2003, 2005, 2006 Brion Vibber <brion@pobox.com>
+ * Copyright Â© 2003, 2005, 2006 Brion Vibber <brion@pobox.com>
  * https://www.mediawiki.org/
  *
  * This program is free software; you can redistribute it and/or modify
@@ -69,7 +69,7 @@ class WikiExporter {
 	 * @return string
 	 */
 	public static function schemaVersion() {
-		return "0.10";
+		return "0.11";
 	}
 
 	/**
