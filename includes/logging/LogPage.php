c1d57500741a70f70d402d7027a2f675bdea9730
Reedy <reedy@wikimedia.org>
2016-01-02 21:16:05 +0000

    LogPage::logName() and logHeader() were removed
    Change-Id: Ib8ab894e01b5125207a6f1ac7377c8287b0a8fce

--- includes/logging/LogPage.php
+++ includes/logging/LogPage.php
@@ -212,42 +212,6 @@ class LogPage {
 	}
 
 	/**
-	 * Get the name for the given log type
-	 *
-	 * @param string $type Log type
-	 * @return string Log name
-	 * @deprecated since 1.19, warnings in 1.21. Use getName()
-	 */
-	public static function logName( $type ) {
-		global $wgLogNames;
-
-		wfDeprecated( __METHOD__, '1.21' );
-
-		if ( isset( $wgLogNames[$type] ) ) {
-			return str_replace( '_', ' ', wfMessage( $wgLogNames[$type] )->text() );
-		} else {
-			// Bogus log types? Perhaps an extension was removed.
-			return $type;
-		}
-	}
-
-	/**
-	 * Get the log header for the given log type
-	 *
-	 * @todo handle missing log types
-	 * @param string $type Logtype
-	 * @return string Header text of this logtype
-	 * @deprecated since 1.19, warnings in 1.21. Use getDescription()
-	 */
-	public static function logHeader( $type ) {
-		global $wgLogHeaders;
-
-		wfDeprecated( __METHOD__, '1.21' );
-
-		return wfMessage( $wgLogHeaders[$type] )->parse();
-	}
-
-	/**
 	 * Generate text for a log entry.
 	 * Only LogFormatter should call this function.
 	 *
