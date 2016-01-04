248c3202668c77ab76cb56ba56f0355c5f9e74b5
victorbarbu <victorbarbu08@gmail.com>
2016-01-05 01:40:01 +0200

    Provide short URL to file description page in imageinfo API
    Bug: T122439
    Change-Id: I0f00b986e6095bdb9b8d6af6fbc5b01995227e02

--- includes/filerepo/file/ForeignAPIFile.php
+++ includes/filerepo/file/ForeignAPIFile.php
@@ -219,6 +219,22 @@ class ForeignAPIFile extends File {
 	}
 
 	/**
+	 * Get short description URL for a file based on the page ID.
+	 *
+	 * @return null|string
+	 * @since 1.27
+	 */
+	public function getDescriptionShortUrl() {
+		if ( isset( $this->mInfo['descriptionshorturl'] ) ) {
+			return $this->mInfo['descriptionshorturl'];
+		} elseif ( isset( $this->mInfo['pageid'] ) ) {
+			return $this->repo->makeUrl( array( 'curid' => $this->mInfo['pageid'] ) );
+		} else {
+			return null;
+		}
+	}
+
+	/**
 	 * @param string $type
 	 * @return int|null|string
 	 */
