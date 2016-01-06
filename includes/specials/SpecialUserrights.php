ca7bbdebd8fc6cfaa12406f7ae1eb58469a7883f
Fomafix <fomafix@googlemail.com>
2016-01-06 20:34:29 +0000

    SpecialUserrights: Use cookie instead of URL parameter for success
    The cookie gets set in the POST and gets deleted in the GET.
    
    This change avoids changing the URL for the success message.
    A reload of the page does not show the success message again.
    
    Bug: T60492
    Change-Id: Iac8d4e6adc4dc93a3da645485f18770fcd2b3872

--- includes/specials/SpecialUserrights.php
+++ includes/specials/SpecialUserrights.php
@@ -80,6 +80,7 @@ class UserrightsPage extends SpecialPage {
 
 		$user = $this->getUser();
 		$request = $this->getRequest();
+		$response = $request->response();
 		$out = $this->getOutput();
 
 		/*
@@ -125,7 +126,10 @@ class UserrightsPage extends SpecialPage {
 		}
 
 		if ( !$this->userCanChangeRights( $user, true ) ) {
-			if ( $this->isself && $request->getCheck( 'success' ) ) {
+			if ( $this->isself && $request->getCookie( 'Success' ) ) {
+				// Remove cookie for the success message
+				$response->clearCookie( 'Success' );
+
 				// bug 48609: if the user just removed its own rights, this would
 				// leads it in a "permissions error" page. In that case, show a
 				// message that it can't anymore use this page instead of an error
@@ -142,7 +146,10 @@ class UserrightsPage extends SpecialPage {
 		}
 
 		// show a successbox, if the user rights was saved successfully
-		if ( $request->getCheck( 'success' ) && $this->mFetchedUser !== null ) {
+		if ( $request->getCookie( 'Success' ) && $this->mFetchedUser !== null ) {
+			// Remove cookie for the success message
+			$response->clearCookie( 'Success' );
+
 			$out->wrapWikiMsg(
 				"<div class=\"successbox\">\n$1\n</div>",
 				array( 'savedrights', $this->mFetchedUser->getName() )
@@ -191,6 +198,9 @@ class UserrightsPage extends SpecialPage {
 					$targetUser
 				);
 
+				// Set cookie for the success message
+				$response->setCookie( 'Success', 1 );
+
 				$out->redirect( $this->getSuccessURL() );
 
 				return;
@@ -204,7 +214,7 @@ class UserrightsPage extends SpecialPage {
 	}
 
 	function getSuccessURL() {
-		return $this->getPageTitle( $this->mTarget )->getFullURL( array( 'success' => 1 ) );
+		return $this->getPageTitle( $this->mTarget )->getFullURL();
 	}
 
 	/**
