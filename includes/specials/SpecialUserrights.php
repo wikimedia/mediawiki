2a8c6741f69c1a5e0e8e8e87b519a2d4c6f03a5d
Fomafix <fomafix@googlemail.com>
2016-01-07 19:19:22 +0000

    SpecialUserrights: Use session data instead of URL parameter for success
    The session data gets set in the POST and gets deleted in the GET.
    
    This change avoids changing the URL for the success message.
    A reload of the page does not show the success message again.
    
    Bug: T60492
    Change-Id: Iac8d4e6adc4dc93a3da645485f18770fcd2b3872

--- includes/specials/SpecialUserrights.php
+++ includes/specials/SpecialUserrights.php
@@ -125,7 +125,10 @@ class UserrightsPage extends SpecialPage {
 		}
 
 		if ( !$this->userCanChangeRights( $user, true ) ) {
-			if ( $this->isself && $request->getCheck( 'success' ) ) {
+			if ( $this->isself && $request->getSessionData( 'Success' ) ) {
+				// Remove session data for the success message
+				$request->setSessionData( 'Success', null );
+
 				// bug 48609: if the user just removed its own rights, this would
 				// leads it in a "permissions error" page. In that case, show a
 				// message that it can't anymore use this page instead of an error
@@ -142,7 +145,10 @@ class UserrightsPage extends SpecialPage {
 		}
 
 		// show a successbox, if the user rights was saved successfully
-		if ( $request->getCheck( 'success' ) && $this->mFetchedUser !== null ) {
+		if ( $request->getSessionData( 'Success' ) && $this->mFetchedUser !== null ) {
+			// Remove session data for the success message
+			$request->setSessionData( 'Success', null );
+
 			$out->wrapWikiMsg(
 				"<div class=\"successbox\">\n$1\n</div>",
 				array( 'savedrights', $this->mFetchedUser->getName() )
@@ -191,6 +197,9 @@ class UserrightsPage extends SpecialPage {
 					$targetUser
 				);
 
+				// Set session data for the success message
+				$request->setSessionData( 'Success', 1 );
+
 				$out->redirect( $this->getSuccessURL() );
 
 				return;
@@ -204,7 +213,7 @@ class UserrightsPage extends SpecialPage {
 	}
 
 	function getSuccessURL() {
-		return $this->getPageTitle( $this->mTarget )->getFullURL( array( 'success' => 1 ) );
+		return $this->getPageTitle( $this->mTarget )->getFullURL();
 	}
 
 	/**
