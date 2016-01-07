aa4d82f8c7d1192a0ee4b79b63b6d19ae20696df
Luke Faraone <luke@faraone.cc>
2016-01-07 17:18:35 +0000

    Add links to toggle checkbox selections in Special:Log
    This implements a new JavaScript module, mediawiki.checkboxtoggle. The module
    is suitable to be reused in any other list of checkboxes.
    
    Bug: T92230
    Change-Id: I92141a7079fc7fcd7152ef418d82f4f7969b163b

---
+++ resources/src/mediawiki/mediawiki.checkboxtoggle.js
@@ -0,0 +1,37 @@
+/*!
+ * Allows users to perform all / none / invert operations on a list of
+ * checkboxes on the page.
+ *
+ * @licence GNU GPL v2+
+ * @author Luke Faraone <luke at faraone dot cc>
+ *
+ * Based on ext.nuke.js from https://www.mediawiki.org/wiki/Extension:Nuke by
+ * Jeroen De Dauw <jeroendedauw at gmail dot com>
+ */
+
+( function ( mw, $ ) {
+	'use strict';
+
+	var $checkboxes = $( 'li input[type=checkbox]' );
+
+	function selectAll( check ) {
+		$checkboxes.prop( 'checked', check );
+	}
+
+	$( '#checkbox-all' ).click( function ( e ) {
+		selectAll( true );
+		e.preventDefault();
+	} );
+	$( '#checkbox-none' ).click( function ( e ) {
+		selectAll( false );
+		e.preventDefault();
+	} );
+	$( '#checkbox-invert' ).click( function ( e ) {
+		$checkboxes.each( function () {
+			$( this ).prop( 'checked', !$( this ).is( ':checked' ) );
+		} );
+		e.preventDefault();
+	} );
+
+}( mediaWiki, jQuery ) );
+
