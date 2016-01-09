2bd09bfc6affe538bbc22f86cbcef0b681590117
georggi <bmp2558@gmail.com>
2016-01-09 15:16:05 +0200

    Export: Exporting now includes custom tags
    Bug: T22691
    Change-Id: I61f77df1a443e15c92f709a1811b207ae125d3fd

--- includes/export/XmlDumpWriter.php
+++ includes/export/XmlDumpWriter.php
@@ -223,6 +223,21 @@ class XmlDumpWriter {
 			$out .= "      " . Xml::elementClean( 'comment', array(), strval( $row->rev_comment ) ) . "\n";
 		}
 
+		if ( isset( $row->ts_tags ) && !is_null( $row->ts_tags ) ){
+			$indent = "      ";
+			$out .= $indent . "<tags>\n";
+ 			if ( strpos($row->ts_tags, ',') ) {
+				$tags = explode(',' ,$row->ts_tags);
+				//I could do it as separate function if you say so
+				foreach($tags as $tag){
+					$out .= $indent . "  " . Xml::element( 'tag', null, $tag ) . "\n";
+				}
+			} else {
+				$out .= $indent . "  " . Xml::element( 'tag', null, $row->ts_tags ) . "\n";
+			}
+			$out .= $indent . "</tags>\n";
+		}
+
 		if ( isset( $row->rev_content_model ) && !is_null( $row->rev_content_model ) ) {
 			$content_model = strval( $row->rev_content_model );
 		} else {
