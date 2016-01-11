efbe03d5303e9d75c9672d665fd61194fe7020af
Brad Jorsch <bjorsch@wikimedia.org>
2016-01-11 13:20:22 -0500

    API changes for AuthManager
    Changes here are:
    * action=login is deprecated for use other than bot passwords
    * list=users will indicate if a missing user name is creatable.
    * Added action=query&meta=authmanagerinfo
    * Added action=clientlogin is to be used to log into the main account
    * action=createaccount is changed in a non-BC manner
    * Added action=linkaccount
    * Added action=unlinkaccount
    * Added action=changeauthenticationdata
    * Added action=removeauthenticationdata
    * Added action=resetpassword
    
    Bug: T110276
    Bug: T110747
    Bug: T110751
    Bug: T32788
    Bug: T67857
    Bug: T28597
    Bug: T76103
    Change-Id: I244fa9b1e0623247d6d9fa30990411c6df94a496

---
+++ includes/api/ApiLinkAccount.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Copyright © 2016 Brad Jorsch <bjorsch@wikimedia.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+use MediaWiki\Auth\AuthManager;
+use MediaWiki\Auth\AuthenticationRequest;
+use MediaWiki\Auth\AuthenticationResponse;
+
+/**
+ * Link an account with AuthManager
+ *
+ * @ingroup API
+ */
+class ApiLinkAccount extends ApiBase {
+
+	public function __construct( ApiMain $main, $action ) {
+		parent::__construct( $main, $action, 'link' );
+	}
+
+	public function getFinalDescription() {
+		// A bit of a hack to append 'api-help-authmanager-general-usage'
+		$msgs = parent::getFinalDescription();
+		$msgs[] = ApiBase::makeMessage( 'api-help-authmanager-general-usage', $this->getContext(), [
+			$this->getModulePrefix(),
+			$this->getModuleName(),
+			$this->getModulePath(),
+			AuthManager::ACTION_LINK,
+			self::needsToken(),
+		] );
+		return $msgs;
+	}
+
+	public function execute() {
+		if ( !$this->getUser()->isLoggedIn() ) {
+			$this->dieUsage( 'Must be logged in to link accounts', 'notloggedin' );
+		}
+
+		$params = $this->extractRequestParams();
+
+		$this->requireAtLeastOneParameter( $params, 'continue', 'returnurl' );
+
+		if ( $params['returnurl'] !== null ) {
+			$bits = wfParseUrl( $params['returnurl'] );
+			if ( !$bits || $bits['scheme'] === '' ) {
+				$encParamName = $this->encodeParamName( 'returnurl' );
+				$this->dieUsage(
+					"Invalid value '{$params['returnurl']}' for url parameter $encParamName",
+					"badurl_{$encParamName}"
+				);
+			}
+		}
+
+		$helper = new ApiAuthManagerHelper( $this );
+		$manager = AuthManager::singleton();
+
+		// Check security-sensitive operation status
+		$helper->securitySensitiveOperation( 'LinkAccounts' );
+
+		// Make sure it's possible to link accounts
+		if ( !$manager->canLinkAccounts() ) {
+			$this->getResult()->addValue( null, 'linkaccount', $helper->formatAuthenticationResponse(
+				AuthenticationResponse::newFail( $this->msg( 'userlogin-cannot-' . AuthManager::ACTION_LINK ) )
+			) );
+			return;
+		}
+
+		// Perform the link step
+		if ( $params['continue'] ) {
+			$reqs = $helper->loadAuthenticationRequests( AuthManager::ACTION_LINK_CONTINUE );
+			$res = $manager->continueAccountLink( $reqs );
+		} else {
+			$reqs = $helper->loadAuthenticationRequests( AuthManager::ACTION_LINK );
+			$res = $manager->beginAccountLink( $this->getUser(), $reqs, $params['returnurl'] );
+		}
+
+		$this->getResult()->addValue( null, 'linkaccount',
+			$helper->formatAuthenticationResponse( $res ) );
+	}
+
+	public function isReadMode() {
+		return false;
+	}
+
+	public function isWriteMode() {
+		return true;
+	}
+
+	public function needsToken() {
+		return 'csrf';
+	}
+
+	public function getAllowedParams() {
+		return ApiAuthManagerHelper::getStandardParams( AuthManager::ACTION_LINK,
+			'requests', 'messageformat', 'mergerequestfields', 'returnurl', 'continue'
+		);
+	}
+
+	public function dynamicParameterDocumentation() {
+		return [ 'api-help-authmanagerhelper-additional-params', AuthManager::ACTION_LINK ];
+	}
+
+	protected function getExamplesMessages() {
+		return [
+			'action=linkaccount&provider=Example&linkreturnurl=http://example.org/&linktoken=123ABC'
+				=> 'apihelp-linkaccount-example-link',
+		];
+	}
+
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/API:Linkaccount';
+	}
+}
