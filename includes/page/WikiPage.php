wikimedia/mediawiki

commit 0aa6486cbff1e0de05a91cb88158588595f4a872

Use ar_page_id on undeletion
Bug: T28123
Change-Id: I882b8ba09d68e7475e1d0934328730059574e292

includes/page/WikiPage.php
@@ -1158,6 +1158,7 @@ class WikiPage implements Page, IDBAccessObject {
 
 		return true;
 	}
+
 	/**
 	 * Insert a new empty page record for this article.
 	 * This *must* be followed up by creating a revision
@@ -1166,13 +1167,16 @@ class WikiPage implements Page, IDBAccessObject {
 	 * Best if all done inside a transaction.
 	 *
 	 * @param IDatabase $dbw
-	 * @return int|bool The newly created page_id key; false if the title already existed
+	 * @param int|null $pageId Custom page ID that will be used for the insert statement
+	 *
+	 * @return bool|int The newly created page_id key; false if the title already existed
 	 */
-	public function insertOn( $dbw ) {
+	public function insertOn( $dbw, $pageId = null ) {
+		$pageId = $pageId ?: $dbw->nextSequenceValue( 'page_page_id_seq' );
 		$dbw->insert(
 			'page',
 			array(
-				'page_id'           => $dbw->nextSequenceValue( 'page_page_id_seq' ),
+				'page_id'           => $pageId,
 				'page_namespace'    => $this->mTitle->getNamespace(),
 				'page_title'        => $this->mTitle->getDBkey(),
 				'page_restrictions' => '',

includes/specials/SpecialUndelete.php
@@ -568,7 +568,7 @@ class PageArchive {
 				return Status::newFatal( "undeleterevdel" );
 			}
 			// Safe to insert now...
-			$newid = $article->insertOn( $dbw );
+			$newid = $article->insertOn( $dbw, $row->ar_page_id );
 			$pageId = $newid;
 		} else {
 			// Check if a deleted revision will become the current revision...

commit b2da5cf4e4e03c3791db21eecc0af6ef10e5be8b

Update the WikiPage object with the new ID when undeleting
Issue introduced by 0aa6486cbff1e0de05a91cb88158588595f4a872.

Change-Id: I2c4fde5e66f280a6bc2de1b13453f1c40385a20d

includes/page/WikiPage.php
@@ -1172,11 +1172,11 @@ class WikiPage implements Page, IDBAccessObject {
 	 * @return bool|int The newly created page_id key; false if the title already existed
 	 */
 	public function insertOn( $dbw, $pageId = null ) {
-		$pageId = $pageId ?: $dbw->nextSequenceValue( 'page_page_id_seq' );
+		$pageIdForInsert = $pageId ?: $dbw->nextSequenceValue( 'page_page_id_seq' );
 		$dbw->insert(
 			'page',
 			array(
-				'page_id'           => $pageId,
+				'page_id'           => $pageIdForInsert,
 				'page_namespace'    => $this->mTitle->getNamespace(),
 				'page_title'        => $this->mTitle->getDBkey(),
 				'page_restrictions' => '',
@@ -1192,7 +1192,7 @@ class WikiPage implements Page, IDBAccessObject {
 		);
 
 		if ( $dbw->affectedRows() > 0 ) {
-			$newid = $dbw->insertId();
+			$newid = $pageId ?: $dbw->insertId();
 			$this->mId = $newid;
 			$this->mTitle->resetArticleID( $newid );
 