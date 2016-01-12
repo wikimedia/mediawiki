333474077f5d5605f9cdca1abe003fa26b694d27
Reedy <reedy@wikimedia.org>
2016-01-12 23:32:58 +0000

    Zend don't support less than PHP 5.5...
    Change-Id: Iecc771cda2e67eeaf2b6bf27f338e698d3298e96

--- includes/PHPVersionCheck.php
+++ includes/PHPVersionCheck.php
@@ -162,7 +162,7 @@ function wfPHPVersionError( $type, $mwVersion, $minimumVersionPHP, $phpVersion )
 
 	$longHtml = <<<HTML
 			Please consider <a href="http://www.php.net/downloads.php">upgrading your copy of PHP</a>.
-			PHP versions less than 5.3.0 are no longer supported by the PHP Group and will not receive
+			PHP versions less than 5.5.0 are no longer supported by the PHP Group and will not receive
 			security or bugfix updates.
 		</p>
 		<p>
