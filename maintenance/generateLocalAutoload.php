ff4921502e165955ff7f30a5a04225bf7b61a833
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-01-20 19:27:47 +0100

    Structure test: Add autoload.php order check
    If a class is added manually to the autoload.php another run of
    the maintenance script will probably add unnecessary noise to
    another change. The added structure test checks, if the output of
    the maintenance script equals to the contents of the commited
    autoload.php.
    
    Bug: T121921
    Change-Id: I3a426b92892f4c00cab33a13f6a717751120367c

--- maintenance/generateLocalAutoload.php
+++ maintenance/generateLocalAutoload.php
@@ -10,15 +10,8 @@ require_once __DIR__ . '/../includes/utils/AutoloadGenerator.php';
 $base = dirname( __DIR__ );
 
 $generator = new AutoloadGenerator( $base, 'local' );
-foreach ( array( 'includes', 'languages', 'maintenance', 'mw-config' ) as $dir ) {
-	$generator->readDir( $base . '/' . $dir );
-}
-foreach ( glob( $base . '/*.php' ) as $file ) {
-	$generator->readFile( $file );
-}
-
-// This class is not defined, but might be added by the installer
-$generator->forceClassPath( 'MyLocalSettingsGenerator', "$base/mw-config/overrides.php" );
+$generator->initMediaWikiDefault();
 
 // Write out the autoload
-$generator->generateAutoload( 'maintenance/generateLocalAutoload.php' );
+$fileinfo = $generator->getTargetFileinfo();
+file_put_contents( $fileinfo['filename'], $generator->getAutoload( 'maintenance/generateLocalAutoload.php' ) );
