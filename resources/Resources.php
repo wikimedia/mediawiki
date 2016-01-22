ee14a2fe25e684cc3a0c5f9452735b7817c57ee2
jdlrobson <jdlrobson@gmail.com>
2016-01-22 11:29:28 -0800

    Modern module loading (2/2)
    Updates one simple examples:
    * Start sending module and require variables to the RL module
    * Updates pluralRuleParser so it can be used as a module
    * Global moment and OOJS patches to ensure these libraries work in
    new environment.
    
    To test:
    Check out the needed by patch and run
    mw.loader.using('test1');
    require('test1')();
    require('test2').bar();
    
    Bug: T108655
    Change-Id: Idbd054880ee70d659ec760aef8fcb38d0704a394

--- resources/Resources.php
+++ resources/Resources.php
@@ -732,6 +732,7 @@ return array(
 	'moment' => array(
 		'scripts' => array(
 			'resources/lib/moment/moment.js',
+			'resources/src/moment-global.js',
 			'resources/src/moment-local-dmy.js',
 		),
 		'languageScripts' => array(
@@ -2176,6 +2177,8 @@ return array(
 	'oojs' => array(
 		'scripts' => array(
 			'resources/lib/oojs/oojs.jquery.js',
+			// This is here for legacy reasons.
+			'resources/src/oojs-global.js',
 		),
 		'targets' => array( 'desktop', 'mobile' ),
 		'dependencies' => array(
---
+++ resources/src/moment-global.js
@@ -0,0 +1,3 @@
+if ( typeof module !== 'undefined' ) {
+	window.moment = module.exports;
+}
