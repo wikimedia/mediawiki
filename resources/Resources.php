f90c2801d9694ca32e27dbacb602b3a935258bdd
jdlrobson <jdlrobson@gmail.com>
2016-01-22 11:29:28 -0800

    resourceloader: Implement modern module loading (2/2)
    * Send 'module' and 'require' parameters to module closures.
      This depends on Ia925844cc22f143 being deployed one cycle earlier.
    * Patch Moment and OOjs to ensure these libraries continue to expose
      their module as globals as well. AMD/UMD-compatible libraries
      only expose a global *OR* an export, not both. We need both
      for back-compat.
    * Update pluralRuleParser to make use of module export to allow
      usage via require().
    
    To test, check out the patch and run:
    > mw.loader.load('moment');
    > mw.loader.require('moment')()
    > mw.loader.require('moment')('2011-04-01').fromNow()
    
    Bug: T108655
    Change-Id: Idbd054880ee70d659ec760aef8fcb38d0704a394

--- resources/Resources.php
+++ resources/Resources.php
@@ -732,6 +732,7 @@ return [
 	'moment' => [
 		'scripts' => [
 			'resources/lib/moment/moment.js',
+			'resources/src/moment-global.js',
 			'resources/src/moment-local-dmy.js',
 		],
 		'languageScripts' => [
@@ -2233,8 +2234,9 @@ return [
 	'oojs' => [
 		'scripts' => [
 			'resources/lib/oojs/oojs.jquery.js',
-		],
-		'targets' => [ 'desktop', 'mobile' ],
+			'resources/src/oojs-global.js',
+		),
+		'targets' => array( 'desktop', 'mobile' ),
 		'dependencies' => [
 			'es5-shim',
 			'json',
