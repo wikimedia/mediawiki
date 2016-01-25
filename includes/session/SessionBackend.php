3157a95de3b11d69060b728fa9f0cfdf54daf072
Bryan Davis <bd808@wikimedia.org>
2016-01-25 10:04:29 -0700

    Call session_cache_limiter() before starting a session
    Call `session_cache_limiter( 'private, must-revalidate' );` before
    starting a session to specify the cache control headers that PHP will
    automatically emit. The calls are wrapped in MediaWiki\quietCall to
    suppress "headers have already been sent" warnings that may come from PHP.
    
    If not called explicitly PHP will default to using
    the value of the session.cache_limiter ini setting. Some values of that
    setting will cause PHP to add a "Pragma: no-cache" header to the
    response. Certain user agents (e.g. Firefox) treat that particular
    header as a signal to aggressively flush the response from local cache
    to the point that back button navigation will not work.
    
    The value used was present in `wfSetupSession` prior to a73c5b7.
    
    Bug: T124510
    Change-Id: I4c8b46fb259d5d5cdf9a1e9b868304637e854dec
    (cherry picked from commit 7491b52f700e220814a8190781fd794b4dd88a20)

--- includes/session/SessionBackend.php
+++ includes/session/SessionBackend.php
@@ -643,6 +643,7 @@ final class SessionBackend {
 			) {
 				$this->logger->debug( "SessionBackend $this->id: Taking over PHP session" );
 				session_id( (string)$this->id );
+				\MediaWiki\quietCall( 'session_cache_limiter', 'private, must-revalidate' );
 				\MediaWiki\quietCall( 'session_start' );
 			}
 		}
