d75a4b5cd6462fdb02d8bb44393dd9a0acd2ec92
Andrew H <crazy4sb@gmail.com>
2016-01-25 02:07:50 +0000

    Add parameter to API modules to apply change tags to log entries
    Adds support for tagging log entries for the block, import,
    managetags, and move API modules, using a 'tags' parameter.
    
    Bug: T97720
    Change-Id: I9d75d2cece317a7704c4bc6d734ad3cafe24544e

--- includes/specials/SpecialBlock.php
+++ includes/specials/SpecialBlock.php
@@ -706,6 +706,13 @@ class SpecialBlock extends FormSpecialPage {
 			}
 		}
 
+		if ( isset( $data['Tags'] ) && count( $data['Tags'] ) ) {
+			$ableToTag = ChangeTags::canAddTagsAccompanyingChange( $data['Tags'], $performer );
+			if ( !$ableToTag->isOK() ) {
+				return [ 'cantapply-changetags' ];
+			}
+		}
+
 		# Create block object.
 		$block = new Block();
 		$block->setTarget( $target );
@@ -818,9 +825,13 @@ class SpecialBlock extends FormSpecialPage {
 		$blockIds = array_merge( [ $status['id'] ], $status['autoIds'] );
 		$logEntry->setRelations( [ 'ipb_id' => $blockIds ] );
 		$logId = $logEntry->insert();
+
+		if ( isset( $data['Tags'] ) && count( $data['Tags'] ) ) {
+			$logEntry->setTags( $data['Tags'] );
+		}
+
 		$logEntry->publish( $logId );
 
-		# Report to the user
 		return true;
 	}
 
