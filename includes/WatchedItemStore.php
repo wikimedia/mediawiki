37fd908c36b3c3cabda119673d3734eb56b5be39
addshore <addshorewiki@gmail.com>
2016-01-26 17:36:05 +0100

    Move WatchedItem::duplicateEntries logic to own class
    This removes static logic from WatchedItem into a
    class that we can slowly fill with watchlist database
    and storage things.
    
    The logic has been reworked to only require a TitleValue
    rather than a Title object and is also now fully unit tested
    
    Change-Id: Ia67ab1d200ac393c65013b2091e61acefcb3defb

---
+++ includes/WatchedItemStore.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @author Addshore
+ *
+ * @since 1.27
+ */
+class WatchedItemStore {
+
+	/**
+	 * @var IDatabase
+	 */
+	private $masterDb;
+
+	public function __construct( IDatabase $masterDb ) {
+		$this->masterDb = $masterDb;
+	}
+
+	/**
+	 * Check if the given title already is watched by the user, and if so
+	 * add watches on a new title. To be used for page renames and such.
+	 *
+	 * @param TitleValue $oldTitle Page title to duplicate entries from, if present
+	 * @param TitleValue $newTitle Page title to add watches on
+	 */
+	public function duplicateEntries( TitleValue $oldTitle, TitleValue $newTitle ) {
+		$oldNamespace = $oldTitle->getNamespace();
+		if ( MWNamespace::isSubject( $oldNamespace ) ) {
+			$otherNs = MWNamespace::getTalk( $oldNamespace );
+		} else {
+			$otherNs = MWNamespace::getSubject( $oldNamespace );
+		}
+
+		$this->doDuplicateEntries( $oldTitle, $newTitle );
+		$this->doDuplicateEntries(
+			new TitleValue( $otherNs, $oldTitle->getDBkey(), $oldTitle->getFragment() ),
+			new TitleValue( $otherNs, $newTitle->getDBkey(), $newTitle->getFragment() )
+		);
+	}
+
+	/**
+	 * Handle duplicate entries. Backend for duplicateEntries().
+	 *
+	 * @param TitleValue $oldTitle
+	 * @param TitleValue $newTitle
+	 *
+	 * @return bool
+	 */
+	private function doDuplicateEntries( TitleValue $oldTitle, TitleValue $newTitle ) {
+		$oldNamespace = $oldTitle->getNamespace();
+		$newNamespace = $newTitle->getNamespace();
+		$oldDBkey = $oldTitle->getDBkey();
+		$newDBkey = $newTitle->getDBkey();
+
+		$result = $this->masterDb->select(
+			'watchlist',
+			array( 'wl_user', 'wl_notificationtimestamp' ),
+			array( 'wl_namespace' => $oldNamespace, 'wl_title' => $oldDBkey ),
+			__METHOD__,
+			'FOR UPDATE'
+		);
+
+		# Construct array to replace into the watchlist
+		$values = array();
+		foreach ( $result as $row ) {
+			$values[] = array(
+				'wl_user' => $row->wl_user,
+				'wl_namespace' => $newNamespace,
+				'wl_title' => $newDBkey,
+				'wl_notificationtimestamp' => $row->wl_notificationtimestamp,
+			);
+		}
+
+		if ( !empty( $values ) ) {
+			# Perform replace
+			# Note that multi-row replace is very efficient for MySQL but may be inefficient for
+			# some other DBMSes, mostly due to poor simulation by us
+			$this->masterDb->replace(
+				'watchlist',
+				array( array( 'wl_user', 'wl_namespace', 'wl_title' ) ),
+				$values,
+				__METHOD__
+			);
+		}
+	}
+
+}
