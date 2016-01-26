e8ce9d3c1bc43374cbe7d522ebe4fbbebd77779c
addshore <addshorewiki@gmail.com>
2016-01-26 17:36:05 +0100

    Move WatchedItem::duplicateEntries logic to own class
    This removes static logic from WatchedItem into a
    class that we can slowly fill with watchlist database
    and storage things.
    
    The logic has been reworked to only require a TitleValue
    rather than a Title object and is also now fully unit tested
    
    Change-Id: Ia67ab1d200ac393c65013b2091e61acefcb3defb

---
+++ tests/phpunit/includes/WatchedItemStoreTest.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @author Addshore
+ *
+ * @covers WatchedItemStore
+ */
+class WatchedItemStoreTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @return PHPUnit_Framework_MockObject_MockObject|IDatabase
+	 */
+	private function getMockDb() {
+		return $this->getMock( 'IDatabase' );
+	}
+
+	/**
+	 * @return PHPUnit_Framework_MockObject_MockObject|LoadBalancer
+	 */
+	private function getMockLoadbalancer( $mockDb ) {
+		$mock = $this->getMockBuilder( 'LoadBalancer' )
+			->disableOriginalConstructor()
+			->getMock();
+		$mock->expects( $this->any() )
+			->method( 'getConnection' )
+			->will( $this->returnValue( $mockDb ) );
+		return $mock;
+	}
+
+	private function getFakeRow( $userId, $timestamp ) {
+		$fakeRow = new stdClass();
+		$fakeRow->wl_user = $userId;
+		$fakeRow->wl_notificationtimestamp = $timestamp;
+		return $fakeRow;
+	}
+
+	public function testDuplicateEntries_nothingToDuplicate() {
+		$mockDb = $this->getMockDb();
+		$mockDb->expects( $this->exactly( 2 ) )
+			->method( 'select' )
+			->will( $this->returnValue( new FakeResultWrapper( array() ) ) );
+
+		$store = new WatchedItemStore( $this->getMockLoadbalancer( $mockDb ) );
+
+		$store->duplicateEntries(
+			new TitleValue( 0, 'Old_Title' ),
+			new TitleValue( 0, 'New_Title' )
+		);
+	}
+
+	public function testDuplicateEntries_somethingToDuplicate() {
+		$fakeRows = array(
+			$this->getFakeRow( 1, '20151212010101' ),
+			$this->getFakeRow( 2, null ),
+		);
+
+		$mockDb = $this->getMockDb();
+		$mockDb->expects( $this->at( 0 ) )
+			->method( 'select' )
+			->will( $this->returnValue( new FakeResultWrapper( $fakeRows ) ) );
+		$mockDb->expects( $this->at( 1 ) )
+			->method( 'replace' )
+			->with(
+				'watchlist',
+				array( array( 'wl_user', 'wl_namespace', 'wl_title' ) ),
+				array(
+					array(
+						'wl_user' => 1,
+						'wl_namespace' => 0,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => '20151212010101',
+					),
+					array(
+						'wl_user' => 2,
+						'wl_namespace' => 0,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => null,
+					),
+				),
+				$this->isType( 'string' )
+			);
+		$mockDb->expects( $this->at( 2 ) )
+			->method( 'select' )
+			->will( $this->returnValue( new FakeResultWrapper( $fakeRows ) ) );
+		$mockDb->expects( $this->at( 3 ) )
+			->method( 'replace' )
+			->with(
+				'watchlist',
+				array( array( 'wl_user', 'wl_namespace', 'wl_title' ) ),
+				array(
+					array(
+						'wl_user' => 1,
+						'wl_namespace' => 1,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => '20151212010101',
+					),
+					array(
+						'wl_user' => 2,
+						'wl_namespace' => 1,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => null,
+					),
+				),
+				$this->isType( 'string' )
+			);
+
+		$store = new WatchedItemStore( $this->getMockLoadbalancer( $mockDb ) );
+
+		$store->duplicateEntries(
+			new TitleValue( 0, 'Old_Title' ),
+			new TitleValue( 0, 'New_Title' )
+		);
+	}
+
+}
