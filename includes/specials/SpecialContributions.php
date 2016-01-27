cb4ed0b6697d093eac4170c729a4f504fca2f340
Adam Roses Wight <awight@wikimedia.org>
2016-01-26 21:17:21 -0800

    Templatize Special:Contributions lines
    Bug: T122537
    Change-Id: I11aac43de495881e10e393d075a231bd346ea547

--- includes/specials/SpecialContributions.php
+++ includes/specials/SpecialContributions.php
@@ -1095,16 +1095,13 @@ class ContribsPager extends ReverseChronologicalPager {
 				$userlink = '';
 			}
 
+			$flags = array();
 			if ( $rev->getParentId() === 0 ) {
-				$nflag = ChangesList::flag( 'newpage' );
-			} else {
-				$nflag = '';
+				$flags[] = ChangesList::flag( 'newpage' );
 			}
 
 			if ( $rev->isMinor() ) {
-				$mflag = ChangesList::flag( 'minor' );
-			} else {
-				$mflag = '';
+				$flags[] = ChangesList::flag( 'minor' );
 			}
 
 			$del = Linker::getRevDeleteLink( $user, $rev, $page );
@@ -1115,15 +1112,6 @@ class ContribsPager extends ReverseChronologicalPager {
 			$diffHistLinks = $this->msg( 'parentheses' )
 				->rawParams( $difftext . $this->messages['pipe-separator'] . $histlink )
 				->escaped();
-			$ret = "{$del}{$d} {$diffHistLinks}{$chardiff}{$nflag}{$mflag} ";
-			$ret .= "{$link}{$userlink} {$comment} {$topmarktext}";
-
-			# Denote if username is redacted for this edit
-			if ( $rev->isDeleted( Revision::DELETED_USER ) ) {
-				$ret .= " <strong>" .
-					$this->msg( 'rev-deleted-user-contribs' )->escaped() .
-					"</strong>";
-			}
 
 			# Tags, if any.
 			list( $tagSummary, $newClasses ) = ChangeTags::formatSummaryRow(
@@ -1131,20 +1119,48 @@ class ContribsPager extends ReverseChronologicalPager {
 				'contributions'
 			);
 			$classes = array_merge( $classes, $newClasses );
-			$ret .= " $tagSummary";
+
+			$templateParams = array(
+				'articleLink' => $link,
+				'charDifference' => $chardiff,
+				'classes' => $classes,
+				'diffHistLinks' => $diffHistLinks,
+				'flags' => $flags,
+				'logText' => $comment,
+				'revDeleteLink' => $del,
+				'tagSummary' => $tagSummary,
+				'timestamp' => $d,
+				'topmarktext' => $topmarktext,
+				'userlink' => $userlink,
+			);
+
+			# Denote if username is redacted for this edit
+			if ( $rev->isDeleted( Revision::DELETED_USER ) ) {
+				$templateParams['rev-deleted-user-contribs'] =
+					$this->msg( 'rev-deleted-user-contribs' )->escaped();
+			}
+
+			$templateParser = new TemplateParser();
+			$ret = $templateParser->processTemplate(
+				'SpecialContributionsLine',
+				$templateParams
+			);
 		}
 
 		// Let extensions add data
-		Hooks::run( 'ContributionsLineEnding', array( $this, &$ret, $row, &$classes ) );
+		Hooks::run( 'ContributionsLineEnding', array( $this, &$ret, $row, &$templateParams['classes'] ) );
 
-		if ( $classes === array() && $ret === '' ) {
+		// TODO: Handle exceptions in the catch block above.  Do any extensions rely on
+		// receiving empty rows?
+
+		if ( $templateParams['classes'] === array() && $ret === '' ) {
 			wfDebug( "Dropping Special:Contribution row that could not be formatted\n" );
-			$ret = "<!-- Could not format Special:Contribution row. -->\n";
-		} else {
-			$ret = Html::rawElement( 'li', array( 'class' => $classes ), $ret ) . "\n";
+			return "<!-- Could not format Special:Contribution row. -->\n";
 		}
 
-		return $ret;
+		// FIXME: The signature of the ContributionsLineEnding hook makes it
+		// very awkward to move this LI wrapper into the template.
+		return Html::rawElement( 'li', array( 'class' => $templateParams['classes'] ), $ret ) . "\n";
 	}
 
 	/**
