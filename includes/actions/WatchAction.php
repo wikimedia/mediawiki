cc25c44ed68adb4a7236125258062dc0f3fba5c8
addshore <addshorewiki@gmail.com>
2016-02-01 12:53:01 +0100

    Move WatchedItem logic to WatchedItemStore
    This also removes assumptions that when a page
    in one Namespace should be watched / removed
    that the page in the talk / subject ns for the
    page should have the same action applied
    
    TODO tests for the rest of WatchedItemStore
    
    Depends-On: Id96a70a44479ff7d4d47cffa515602a25568d696
    Depends-On: Id14727be7e796712f07ce4359dbe2ed6b0fe97bb
    Depends-On: I9eec07220083e23f0f94eb84c90dec2376bfce58
    Depends-On: I28f237dc3e9bf8a6ea45f189c974a55338d135a6
    Change-Id: Iad9abafe4417bb479151a3bfbee6e1c78a3afe3c

--- includes/actions/WatchAction.php
+++ includes/actions/WatchAction.php
@@ -82,12 +82,12 @@ class WatchAction extends FormAction {
 	 */
 	public static function doWatchOrUnwatch( $watch, Title $title, User $user ) {
 		if ( $user->isLoggedIn() &&
-			$user->isWatched( $title, WatchedItem::IGNORE_USER_RIGHTS ) != $watch
+			$user->isWatched( $title, false ) != $watch
 		) {
 			// If the user doesn't have 'editmywatchlist', we still want to
 			// allow them to add but not remove items via edits and such.
 			if ( $watch ) {
-				return self::doWatch( $title, $user, WatchedItem::IGNORE_USER_RIGHTS );
+				return self::doWatch( $title, $user, User::IGNORE_USER_RIGHTS );
 			} else {
 				return self::doUnwatch( $title, $user );
 			}
@@ -101,15 +101,12 @@ class WatchAction extends FormAction {
 	 * @since 1.22 Returns Status, $checkRights parameter added
 	 * @param Title $title Page to watch/unwatch
 	 * @param User $user User who is watching/unwatching
-	 * @param int $checkRights Passed through to $user->addWatch()
+	 * @param bool $checkRights Passed through to $user->addWatch()
+	 *     Pass User::CHECK_USER_RIGHTS or User::IGNORE_USER_RIGHTS.
 	 * @return Status
 	 */
-	public static function doWatch( Title $title, User $user,
-		$checkRights = WatchedItem::CHECK_USER_RIGHTS
-	) {
-		if ( $checkRights !== WatchedItem::IGNORE_USER_RIGHTS &&
-			!$user->isAllowed( 'editmywatchlist' )
-		) {
+	public static function doWatch( Title $title, User $user, $checkRights = User::CHECK_USER_RIGHTS ) {
+		if ( $checkRights && !$user->isAllowed( 'editmywatchlist' ) ) {
 			return User::newFatalPermissionDeniedStatus( 'editmywatchlist' );
 		}
 
