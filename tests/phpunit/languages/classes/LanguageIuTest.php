f6951733c3b7e750497e0f8eb6283049b301f000
Tim Starling <tstarling@wikimedia.org>
2016-02-01 13:37:36 +1100

    Add tests for LanguageConverter classes that didn't have them
    Some of them don't have many test cases, or have test cases that don't
    represent the ideal transliteration and so are subject to change. But
    this is better than nothing.
    
    Change-Id: I4aae693bd77d9ff365f48113923ed7f9fed8d668

---
+++ tests/phpunit/languages/classes/LanguageIuTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class LanguageIuTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider provideAutoConvertToAllVariants
+	 */
+	public function testAutoConvertToAllVariants( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->autoConvertToAllVariants( $value ) );	
+	}
+
+	public static function provideAutoConvertToAllVariants() {
+		return array(
+			// ike-cans
+			array(
+				array(
+					'ike-cans' => 'ᐴ',
+					'ike-latn' => 'PUU',
+					'iu' => 'PUU',
+				),
+				'PUU'
+			),
+			// ike-latn
+			array(
+				array(
+					'ike-cans' => 'ᐴ',
+					'ike-latn' => 'puu',
+					'iu' => 'ᐴ',
+				),
+				'ᐴ'
+			),
+		);
+	}
+}
