d294f100e5296da0c1fca4cb8242777e687e6b0f
Tim Starling <tstarling@wikimedia.org>
2016-02-01 13:37:36 +1100

    Add tests for LanguageConverter classes that didn't have them
    Some of them don't have many test cases, or have test cases that don't
    represent the ideal transliteration and so are subject to change. But
    this is better than nothing.
    
    Change-Id: I4aae693bd77d9ff365f48113923ed7f9fed8d668

---
+++ tests/phpunit/languages/classes/LanguageShiTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class LanguageShiTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider provideAutoConvertToAllVariants
+	 */
+	public function testAutoConvertToAllVariants( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->autoConvertToAllVariants( $value ) );
+	}
+
+	public static function provideAutoConvertToAllVariants() {
+		return array(
+			array(
+				array(
+					'shi'      => 'AƔ',
+					'shi-tfng' => 'ⴰⵖ',
+					'shi-latn' => 'AƔ',
+				),
+				'AƔ'
+			),
+			array(
+				array(
+					'shi'      => 'ⴰⵖ',
+					'shi-tfng' => 'ⴰⵖ',
+					'shi-latn' => 'aɣ',
+				),
+				'ⴰⵖ'
+			),
+		);
+	}
+}
