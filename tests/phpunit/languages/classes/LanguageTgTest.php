0c6675741f1d6de61820db430674392148663287
Tim Starling <tstarling@wikimedia.org>
2016-02-01 13:37:36 +1100

    Add tests for LanguageConverter classes that didn't have them
    Some of them don't have many test cases, or have test cases that don't
    represent the ideal transliteration and so are subject to change. But
    this is better than nothing.
    
    Change-Id: I4aae693bd77d9ff365f48113923ed7f9fed8d668

---
+++ tests/phpunit/languages/classes/LanguageTgTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class LanguageTgTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider provideAutoConvertToAllVariants
+	 */
+	public function testAutoConvertToAllVariants( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->autoConvertToAllVariants( $value ) );	
+	}
+
+	public static function provideAutoConvertToAllVariants() {
+		return array(
+			array(
+				array(
+					'tg'      => 'г',
+					'tg-latn' => 'g',
+				),
+				'г'
+			),
+			array(
+				array(
+					'tg'      => 'g',
+					'tg-latn' => 'g',
+				),
+				'g'
+			),
+		);
+	}
+}
