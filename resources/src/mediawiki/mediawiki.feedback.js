9f4b60115f58f6149b9582fc82aea7dc74f4bea2
James D. Forrester <jforrester@wikimedia.org>
2016-02-01 09:16:12 -0800

    mw.Feedback: If the message is posted remotely, link the title correctly
    Bug: T125400
    Change-Id: Ic52c24e5d76175e72950dfa10ae1519d6c097ba1

--- resources/src/mediawiki/mediawiki.feedback.js
+++ resources/src/mediawiki/mediawiki.feedback.js
@@ -59,6 +59,7 @@
 		this.feedbackPageTitle = config.title || new mw.Title( 'Feedback' );
 
 		this.messagePosterPromise = mw.messagePoster.factory.create( this.feedbackPageTitle, config.apiUrl );
+		this.foreignApi = config.apiUrl ? new mw.ForeignApi( config.apiUrl ) : null;
 
 		// Links
 		this.bugsTaskSubmissionLink = config.bugsLink || '//phabricator.wikimedia.org/maniphest/task/edit/form/1/';
@@ -94,9 +95,24 @@
 	 *  to the external task reporting site.
 	 */
 	mw.Feedback.prototype.onDialogSubmit = function ( status ) {
-		var dialogConfig = {};
+		var feedbackToolHref,
+			feedbackPageTitle = this.feedbackPageTitle,
+			dialogConfig = {};
+
 		switch ( status ) {
 			case 'submitted':
+				feedbackToolHref = this.foreignApi
+					?
+						this.foreignApi.get( {
+							action: 'query',
+							meta: 'siteinfo',
+							siprop: 'general'
+						} ).then( function ( data ) {
+							return data.server + data.script + '?title=' + feedbackPageTitle;
+						} )
+					:
+						this.feedbackPageTitle.getUrl();
+
 				dialogConfig = {
 					title: mw.msg( 'feedback-thanks-title' ),
 					message: $( '<span>' ).msg(
@@ -104,7 +120,7 @@
 						this.feedbackPageTitle.getNameText(),
 						$( '<a>' ).attr( {
 							target: '_blank',
-							href: this.feedbackPageTitle.getUrl()
+							href: feedbackToolHref
 						} )
 					),
 					actions: [
