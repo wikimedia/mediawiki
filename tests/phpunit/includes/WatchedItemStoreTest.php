d0b3a89dd0cc5f0674a210a0c2698fcc82684dac
addshore <addshorewiki@gmail.com>
2016-02-01 12:53:01 +0100

    Move WatchedItem logic to WatchedItemStore
    This also removes assumptions that when a page
    in one Namespace should be watched / removed
    that the page in the talk / subject ns for the
    page should have the same action applied
    
    This should maintain all backward compatability
    for the WatchedItem class
    
    Tests for the code moved are in a followup
    
    TODO: Release Notes!
    Change-Id: Iad9abafe4417bb479151a3bfbee6e1c78a3afe3c

--- tests/phpunit/includes/WatchedItemStoreTest.php
+++ tests/phpunit/includes/WatchedItemStoreTest.php
@@ -40,7 +40,10 @@ class WatchedItemStoreTest extends PHPUnit_Framework_TestCase {
 			->method( 'select' )
 			->will( $this->returnValue( new FakeResultWrapper( array() ) ) );
 
-		$store = new WatchedItemStore( $this->getMockLoadbalancer( $mockDb ) );
+		$store = new WatchedItemStore(
+			$this->getMockLoadbalancer( $mockDb ),
+			new HashBagOStuff( array( 'maxKeys' => 100 ) )
+		);
 
 		$store->duplicateEntry(
 			Title::newFromText( 'Old_Title' ),
@@ -80,7 +83,10 @@ class WatchedItemStoreTest extends PHPUnit_Framework_TestCase {
 				$this->isType( 'string' )
 			);
 
-		$store = new WatchedItemStore( $this->getMockLoadbalancer( $mockDb ) );
+		$store = new WatchedItemStore(
+			$this->getMockLoadbalancer( $mockDb ),
+			new HashBagOStuff( array( 'maxKeys' => 100 ) )
+		);
 
 		$store->duplicateEntry(
 			Title::newFromText( 'Old_Title' ),
