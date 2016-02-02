af76c5ba605f7061f9bc5eb553cf79ae968bba32
Kunal Mehta <legoktm@member.fsf.org>
2016-02-02 14:37:28 +1100

    [Do not merge] jenkins testing
    Change-Id: I578e61599af04b07ae81a1e50cb52e837faf4a8c

--- index.php
+++ index.php
@@ -35,6 +35,8 @@
 require_once dirname( __FILE__ ) . '/includes/PHPVersionCheck.php';
 wfEntryPointCheck( 'index.php' );
 
+$abc = [];
+
 require __DIR__ . '/includes/WebStart.php';
 
 $mediaWiki = new MediaWiki();
