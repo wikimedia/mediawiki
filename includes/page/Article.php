85c7fc9130d3590d5bc99cc6d86cc0ad4e040313
Chad Horohoe <chadh@wikimedia.org>
2016-02-03 20:29:00 +0000

    Adding random root page back to .12
    Change-Id: Ic2ea3fdaeefcbd53fbf54a995276f59198c088b2

--- includes/page/Article.php
+++ includes/page/Article.php
@@ -1214,7 +1214,7 @@ class Article implements Page {
 		);
 
 		$outputPage->addHTML(
-			"<div class='patrollink'>" .
+			"<div class='patrollink' data-mw='interface'>" .
 				wfMessage( 'markaspatrolledlink' )->rawParams( $link )->escaped() .
 			'</div>'
 		);
