5cbec5245c21b4e6e8e6646aea3b9930e19c0ef1
Brad Jorsch <bjorsch@wikimedia.org>
2016-02-03 12:33:25 -0500

    Avoid unstubbing $wgUser before the end of Setup.php in User::getBlockedStatus()
    Autocreation needs to check if the current IP is blocked from account
    creation.
    
    There are two ways we could go here: treat $wgUser as logged-out, or
    assume it will eventually be the user name specified by the session.
    This patch chooses the former, by the logic that at this early point in
    the setup process we don't have a logged-in user determined yet so no
    username can really be considered to match the logged-in user.
    
    Bug: T124367
    Change-Id: I631bec85291b57f07c378cf6554a8f06cf3fb00c

--- includes/user/User.php
+++ includes/user/User.php
@@ -1539,7 +1539,7 @@ class User implements IDBAccessObject {
 	 *   Check when actually saving should be done against master.
 	 */
 	private function getBlockedStatus( $bFromSlave = true ) {
-		global $wgProxyWhitelist, $wgUser, $wgApplyIpBlocksToXff;
+		global $wgProxyWhitelist, $wgUser, $wgApplyIpBlocksToXff, $wgFullyInitialised;
 
 		if ( -1 != $this->mBlockedby ) {
 			return;
@@ -1557,10 +1557,16 @@ class User implements IDBAccessObject {
 		# We only need to worry about passing the IP address to the Block generator if the
 		# user is not immune to autoblocks/hardblocks, and they are the current user so we
 		# know which IP address they're actually coming from
-		if ( !$this->isAllowed( 'ipblock-exempt' ) && $this->equals( $wgUser ) ) {
-			$ip = $this->getRequest()->getIP();
-		} else {
-			$ip = null;
+		$ip = null;
+		if ( !$this->isAllowed( 'ipblock-exempt' ) ) {
+			// $wgUser->getName() only works after the end of Setup.php. Until
+			// then, assume it's a logged-out user.
+			$globalUserName = $wgFullyInitialised
+				? $wgUser->getName()
+				: IP::sanitizeIP( $wgUser->getRequest()->getIP() );
+			if ( $this->getName() === $globalUserName ) {
+				$ip = $this->getRequest()->getIP();
+			}
 		}
 
 		// User/IP blocking
