aa838b52de9624cf10abe92758c7bbb1416d3001
Alex Monk <krenair@gmail.com>
2016-02-04 00:04:12 +0000

    Try to fix some other broken-looking legacy maintenance script options
    Bug: T125748
    Change-Id: I2004d9b1fd7389623a3f2da6682f9007efd679dc

--- maintenance/storage/moveToExternal.php
+++ maintenance/storage/moveToExternal.php
@@ -24,6 +24,7 @@
 define( 'REPORTING_INTERVAL', 1 );
 
 if ( !defined( 'MEDIAWIKI' ) ) {
+	$optionsWithArgs = [ 'e', 's' ];
 	require_once __DIR__ . '/../commandLine.inc';
 	require_once 'resolveStubs.php';
 
