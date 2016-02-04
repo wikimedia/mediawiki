88312618ab7be777e56a3d6a97177a455d52810c
Lee Bousfield <ljbousfield@gmail.com>
2016-02-04 13:12:41 -0700

    resourceloader: Log each deprecation only once from mw.log.deprecate
    Bug: T100412
    Change-Id: I0111f0ed84b74774b6665aca2625d07ea53cfc8c

--- resources/src/mediawiki/mediawiki.js
+++ resources/src/mediawiki/mediawiki.js
@@ -661,17 +661,43 @@
 				obj[ key ] = val;
 			} : function ( obj, key, val, msg ) {
 				msg = 'Use of "' + key + '" is deprecated.' + ( msg ? ( ' ' + msg ) : '' );
+				var logged;
+				var uniqueTrace = function () {
+					var trace = new Error().stack;
+					if ( logged instanceof Set ) {
+						if ( logged.has( trace ) ) {
+							return false;
+						}
+						logged.add( trace );
+						return true;
+					} else {
+						if ( logged.hasOwnProperty( trace ) ) {
+							return false;
+						}
+						logged[ trace ] = 1;
+						return true;
+					}
+				};
+				if ( window.Set ) {
+					logged = new Set();
+				} else {
+					logged = {};
+				}
 				Object.defineProperty( obj, key, {
 					configurable: true,
 					enumerable: true,
 					get: function () {
-						mw.track( 'mw.deprecate', key );
-						mw.log.warn( msg );
+						if ( uniqueTrace() ) {
+							mw.track( 'mw.deprecate', key );
+							mw.log.warn( msg );
+						}
 						return val;
 					},
 					set: function ( newVal ) {
-						mw.track( 'mw.deprecate', key );
-						mw.log.warn( msg );
+						if ( uniqueTrace() ) {
+							mw.track( 'mw.deprecate', key );
+							mw.log.warn( msg );
+						}
 						val = newVal;
 					}
 				} );
