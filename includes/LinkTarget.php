072fdb91092ef814108d1162f3aa5cf9f0f1f81b
aude <aude.wiki@gmail.com>
2016-02-15 12:56:25 +0100

    Fix typo and improve code comments in LinkTarget
    Change-Id: I53cd07fafd00ded65e2657db96f94170013abed8

--- includes/LinkTarget.php
+++ includes/LinkTarget.php
@@ -8,29 +8,28 @@
 interface LinkTarget {
 
 	/**
-	 * Get the namespace index
+	 * Get the namespace index.
 	 *
 	 * @return int Namespace index
 	 */
 	public function getNamespace();
 
 	/**
-	 * Get the link fragment (i.e.\ the bit after the #) in text form
+	 * Get the link fragment (i.e. the bit after the #) in text form.
 	 *
 	 * @return string link fragment
 	 */
 	public function getFragment();
 
 	/**
-	 * Get the main part with underscores
+	 * Get the main part with underscores.
 	 *
-	 * @return string Main part of the link, with underscores (for use in hrf attributes)
+	 * @return string Main part of the link, with underscores (for use in href attributes)
 	 */
 	public function getDBkey();
 
 	/**
-	 * Returns the link in text form,
-	 * without namespace prefix or fragment.
+	 * Returns the link in text form, without namespace prefix or fragment.
 	 *
 	 * This is computed from the DB key by replacing any underscores with spaces.
 	 *
