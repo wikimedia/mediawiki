7a83bd74728730ece577dfeef42573c89458e42d
Željko Filipin <zeljko.filipin@gmail.com>
2016-02-16 12:53:06 +0100

    Browser test: fix log in Selenium tests
    LoginPage is removed from mediawiki_selenium Ruby gem. This repository
    is the only repository that needs it, so the file is copied here.
    
    Also, user used to be logged in via the API, not really testing if log
    in via the web site works. It is fixed now.
    
    Bug: T127042
    Change-Id: Iff85d66a9a6d3343c212fe648c589539e889a313

---
+++ tests/browser/features/support/pages/login_page.rb
@@ -0,0 +1,27 @@
+require 'page-object'
+
+class LoginPage
+  include PageObject
+
+  page_url 'Special:UserLogin'
+
+  div(:feedback, class: 'errorbox')
+  button(:login, id: 'wpLoginAttempt')
+  li(:logout, id: 'pt-logout')
+  text_field(:password, id: 'wpPassword1')
+  a(:password_strength, text: 'password strength')
+  a(:phishing, text: 'phishing')
+  text_field(:username, id: 'wpName1')
+  a(:username_displayed, title: /Your user page/)
+
+  def logged_in_as_element
+    @browser.div(id: 'mw-content-text').p.b
+  end
+
+  def login_with(username, password, wait_for_logout_element = true)
+    username_element.when_present.send_keys(username)
+    password_element.when_present.send_keys(password)
+    login_element.when_present.click
+    logout_element.when_present(10) if wait_for_logout_element
+  end
+end
