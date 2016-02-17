1e1c8b315f92b488502a33d1eacb5005d189872e
Fomafix <fomafix@googlemail.com>
2016-02-17 12:25:28 +0000

    Add HTML attributes lang and dir to toctitle
    The toctitle is in user interface language. This change add the
    HTML attributes lang and dir to the toctitle.
    
    This allows to copy (a part of) the rendered HTML to a word processing
    and the language for spell checking has the right language.
    
    Change-Id: I17730869f81744560672d7d0acb67ced71e69c4e

--- includes/Linker.php
+++ includes/Linker.php
@@ -1652,7 +1652,12 @@ class Linker {
 		$title = wfMessage( 'toc' )->inLanguage( $lang )->escaped();
 
 		return '<div id="toc" class="toc">'
-			. '<div id="toctitle"><h2>' . $title . "</h2></div>\n"
+			. Html::openElement( 'div', [
+				'id' => 'toctitle',
+				'lang' => $lang->getHtmlCode(),
+				'dir' => $lang->getDir(),
+			] )
+			. '<h2>' . $title . "</h2></div>\n"
 			. $toc
 			. "</ul>\n</div>\n";
 	}
