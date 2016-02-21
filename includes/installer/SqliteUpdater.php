31c0bea883f54468887bfee23a2515f455e4ee6c
Brian Wolff <bawolff+wn@gmail.com>
2016-02-21 16:59:14 -0500

    Add new index to make updateCollation.php painless
    We want to update categories in order, to minimize disruption
    to users. Previous indexes required a filesort to do this, which
    exploded things on large wikis. See bug for details
    
    Bug: T58041
    Change-Id: Iee6cd997ff87a313a46fda19d8ab063d0fed8ce8

--- includes/installer/SqliteUpdater.php
+++ includes/installer/SqliteUpdater.php
@@ -65,7 +65,6 @@ class SqliteUpdater extends DatabaseUpdater {
 			[ 'addField', 'interwiki', 'iw_api', 'patch-iw_api_and_wikiid.sql' ],
 			[ 'dropIndex', 'iwlinks', 'iwl_prefix', 'patch-kill-iwl_prefix.sql' ],
 			[ 'addField', 'categorylinks', 'cl_collation', 'patch-categorylinks-better-collation.sql' ],
-			[ 'doCollationUpdate' ],
 			[ 'addTable', 'module_deps', 'patch-module_deps.sql' ],
 			[ 'dropIndex', 'archive', 'ar_page_revid', 'patch-archive_kill_ar_page_revid.sql' ],
 			[ 'addIndex', 'archive', 'ar_revid', 'patch-archive_ar_revid.sql' ],
@@ -148,6 +147,10 @@ class SqliteUpdater extends DatabaseUpdater {
 			[ 'dropTable', 'msg_resource_links' ],
 			[ 'dropTable', 'msg_resource' ],
 			[ 'addTable', 'bot_passwords', 'patch-bot_passwords.sql' ],
+			[ 'dropIndex', 'categorylinks', 'cl_collation', 'patch-kill-cl_collation_index.sql' ],
+			[ 'addIndex', 'categorylinks', 'cl_collation_ext',
+				'patch-add-cl_collation_ext_index.sql' ],
+			[ 'doCollationUpdate' ],
 		];
 	}
 
