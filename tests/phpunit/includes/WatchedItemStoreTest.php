e4d67b9567330f424183529909543dafc26e53d8
addshore <addshorewiki@gmail.com>
2016-02-26 08:34:40 +0000

    Tests for WatchedItemStore::duplicateAllAssociatedEntries
    Change-Id: Ib777ab69e9516afb0fd620e1e9f596599f81427f

--- tests/phpunit/includes/WatchedItemStoreTest.php
+++ tests/phpunit/includes/WatchedItemStoreTest.php
@@ -148,6 +148,124 @@ class WatchedItemStoreTest extends PHPUnit_Framework_TestCase {
 		);
 	}
 
+	public function testDuplicateAllAssociatedEntries_nothingToDuplicate() {
+		$mockDb = $this->getMockDb();
+		$mockDb->expects( $this->at( 0 ) )
+			->method( 'select' )
+			->with(
+				'watchlist',
+				[
+					'wl_user',
+					'wl_notificationtimestamp',
+				],
+				[
+					'wl_namespace' => 0,
+					'wl_title' => 'Old_Title',
+				]
+			)
+			->will( $this->returnValue( new FakeResultWrapper( [] ) ) );
+		$mockDb->expects( $this->at( 1 ) )
+			->method( 'select' )
+			->with(
+				'watchlist',
+				[
+					'wl_user',
+					'wl_notificationtimestamp',
+				],
+				[
+					'wl_namespace' => 1,
+					'wl_title' => 'Old_Title',
+				]
+			)
+			->will( $this->returnValue( new FakeResultWrapper( [] ) ) );
+
+		$store = new WatchedItemStore(
+			$this->getMockLoadBalancer( $mockDb ),
+			new HashBagOStuff( [ 'maxKeys' => 100 ] )
+		);
+
+		$store->duplicateAllAssociatedEntries(
+			Title::newFromText( 'Old_Title' ),
+			Title::newFromText( 'New_Title' )
+		);
+	}
+
+	public function testDuplicateAllAssociatedEntries_somethingToDuplicate() {
+		$fakeRows = [
+			$this->getFakeRow( [ 'wl_user' => 1, 'wl_notificationtimestamp' => '20151212010101' ] ),
+		];
+
+		$mockDb = $this->getMockDb();
+		$mockDb->expects( $this->at( 0 ) )
+			->method( 'select' )
+			->with(
+				'watchlist',
+				[
+					'wl_user',
+					'wl_notificationtimestamp',
+				],
+				[
+					'wl_namespace' => 0,
+					'wl_title' => 'Old_Title',
+				]
+			)
+			->will( $this->returnValue( new FakeResultWrapper( $fakeRows ) ) );
+		$mockDb->expects( $this->at( 1 ) )
+			->method( 'replace' )
+			->with(
+				'watchlist',
+				[ [ 'wl_user', 'wl_namespace', 'wl_title' ] ],
+				[
+					[
+						'wl_user' => 1,
+						'wl_namespace' => 0,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => '20151212010101',
+					],
+				],
+				$this->isType( 'string' )
+			);
+		$mockDb->expects( $this->at( 2 ) )
+			->method( 'select' )
+			->with(
+				'watchlist',
+				[
+					'wl_user',
+					'wl_notificationtimestamp',
+				],
+				[
+					'wl_namespace' => 1,
+					'wl_title' => 'Old_Title',
+				]
+			)
+			->will( $this->returnValue( new FakeResultWrapper( $fakeRows ) ) );
+		$mockDb->expects( $this->at( 3 ) )
+			->method( 'replace' )
+			->with(
+				'watchlist',
+				[ [ 'wl_user', 'wl_namespace', 'wl_title' ] ],
+				[
+					[
+						'wl_user' => 1,
+						'wl_namespace' => 1,
+						'wl_title' => 'New_Title',
+						'wl_notificationtimestamp' => '20151212010101',
+					],
+				],
+				$this->isType( 'string' )
+			);
+
+		$store = new WatchedItemStore(
+			$this->getMockLoadBalancer( $mockDb ),
+			new HashBagOStuff( [ 'maxKeys' => 100 ] )
+		);
+
+		$store->duplicateAllAssociatedEntries(
+			Title::newFromText( 'Old_Title' ),
+			Title::newFromText( 'New_Title' )
+		);
+	}
+
 	public function testAddWatch_nonAnonymousUser() {
 		$mockDb = $this->getMockDb();
 		$mockDb->expects( $this->once() )
