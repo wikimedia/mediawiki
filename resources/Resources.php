05f3c5a219ea2efe4267fdcc3b2c1e0d7a125ac0
Geoffrey Mon <geofbot@gmail.com>
2016-02-26 17:35:12 -0500

    [WIP] Add and use PHP DateInputWidget in Contribs
    Working prototype.
    
    * Add PHP version of DateInputWidget (in progress)
    * Add said widget to Special:Contributions
    * Add getDateCondWithDateString as a version of getDateCond that takes a
      date string as the offset
    
    Bug: T120733
    Change-Id: I8f80e27304a916bba16ed255c8e78073f9da3813

--- resources/Resources.php
+++ resources/Resources.php
@@ -1897,6 +1897,13 @@ return [
 		'position' => 'top',
 		'styles' => 'resources/src/mediawiki.special/mediawiki.special.comparepages.styles.less',
 	],
+	'mediawiki.special.contributions' => [
+		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.contributions.js',
+		'dependencies' => [
+			'mediawiki.widgets.DateInputWidget',
+			'mediawiki.jqueryMsg',
+		]
+	],
 	'mediawiki.special.edittags' => [
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.edittags.js',
 		'dependencies' => [
