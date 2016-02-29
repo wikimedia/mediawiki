352300a6e180987993cef24ace5513a3d17f5690
Brian Wolff <bawolff+wn@gmail.com>
2016-02-28 23:13:10 -0500

    Initial support for CSP in core [Disabled by default]
    The primary goal here is a defense in depth measure to
    stop an attacker who found a bug in the parser allowing
    them to insert malicious attributes.
    
    This wouldn't stop someone who could insert a full
    script tag (since at current it can't distinguish between
    malicious and legit user js). It also would not prevent
    DOM-based or reflected XSS for anons, as the nonce value
    is guessable for anons when receiving a response cached
    by varnish. However, the limited protection of just stopping
    stored XSS where the attacker only has control of attributes,
    is still a big win in my opinion. (But it wouldn't prevent
    someone who has that type of xss from abusing things like
    data-ooui attribute).
    
    This will likely break many gadgets. Its expected that any
    sort of rollout on Wikimedia will be done very slowly, with
    lots of testing and the report-only option to begin with.
    
    This is behind feature flags that are off by default, so
    merging this patch should not cause any change in default
    behaviour.
    
    This may break some extensions (The most obvious one
    is charinsert (See fe648d41005), but will probably need
    some testing in report-only mode to see if anything else breaks)
    
    This uses the unsafe-eval option of CSP, in order to
    support RL's local storage thingy. For better security,
    we may want to remove some of the sillier uses of eval
    (e.g. jquery.ui.datepicker.js).
    
    For more info, see spec: http://www.w3.org/TR/CSP2/
    Additionally see:
    https://www.mediawiki.org/wiki/Requests_for_comment/Content-Security-Policy
    
    Bug: T135963
    Change-Id: I80f6f469ba4c0b608385483457df96ccb7429ae5

--- autoload.php
+++ autoload.php
@@ -319,6 +319,7 @@ $wgAutoloadLocalClasses = [
 	'CrhConverter' => __DIR__ . '/languages/classes/LanguageCrh.php',
 	'CryptHKDF' => __DIR__ . '/includes/libs/CryptHKDF.php',
 	'CryptRand' => __DIR__ . '/includes/libs/CryptRand.php',
+	'ContentSecurityPolicy' => __DIR__ . '/includes/ContentSecurityPolicy.php',
 	'CssContent' => __DIR__ . '/includes/content/CssContent.php',
 	'CssContentHandler' => __DIR__ . '/includes/content/CssContentHandler.php',
 	'CsvStatsOutput' => __DIR__ . '/maintenance/language/StatOutputs.php',
