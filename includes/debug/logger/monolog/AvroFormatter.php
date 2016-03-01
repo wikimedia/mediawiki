07828eb0de30b61c80d5867722ea6e1ebefe6d32
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-03-01 15:27:27 -0800

    Update AvroFormatter to support avro protocols
    For more complex schema's the avro protocol, being built from
    an .idl file, is much easier for a human to maintain. This adds
    support to mediawiki for avro protocol files.
    
    Bug: T128534
    Change-Id: Ibbf84cdf0c923a0a69a3d68226ffcf1c5c5a3361

--- includes/debug/logger/monolog/AvroFormatter.php
+++ includes/debug/logger/monolog/AvroFormatter.php
@@ -23,7 +23,7 @@ namespace MediaWiki\Logger\Monolog;
 use AvroIODatumWriter;
 use AvroIOBinaryEncoder;
 use AvroIOTypeException;
-use AvroNamedSchemata;
+use AvroProtocol;
 use AvroSchema;
 use AvroStringIO;
 use AvroValidator;
@@ -41,6 +41,10 @@ class AvroFormatter implements FormatterInterface {
 	 * @var Magic byte to encode schema revision id.
 	 */
 	const MAGIC = 0x0;
+	/**
+	 * Namespace of mediawiki schemas
+	 */
+	const SCHEMA_NAMESPACE = "org.wikimedia.analytics.schemas";
 	/**
 	 * @var array Map from schema name to schema definition
 	 */
@@ -132,19 +136,24 @@ class AvroFormatter implements FormatterInterface {
 			return null;
 		}
 
-		if ( !$this->schemas[$channel]['schema'] instanceof AvroSchema ) {
-			$schema = $this->schemas[$channel]['schema'];
-			if ( is_string( $schema ) ) {
-				$this->schemas[$channel]['schema'] = AvroSchema::parse( $schema );
-			} else {
-				$this->schemas[$channel]['schema'] = AvroSchema::real_parse(
-					$schema,
-					null,
-					new AvroNamedSchemata()
+		if ( $this->schemas[$channel]['schema'] instanceof AvroSchema ) {
+			return $this->schemas[$channel]['schema'];
+		} elseif ( isset( $this->schemas[$channel]['protocol'] ) ) {
+			$protocol = AvroProtocol::parse( $this->schemas[$channel]['protocol'] );
+			$schema = $protocol->schemata->schema( $this->schemas[$channel]['schema'] );
+			if ( $schema === null ) {
+				trigger_error( "Named schema not included in protocol: " .
+					$this->schemas[$channel]['schema']
 				);
+				return null;
 			}
+		} else {
+			$schema = AvroSchema::parse( $this->schemas[$channel]['schema'] );
 		}
-		return $this->schemas[$channel]['schema'];
+
+		$this->schemas[$channel]['schema'] = $schema;
+
+		return $schema;
 	}
 
 	/**
