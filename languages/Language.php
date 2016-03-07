aee5820dd83d0fdd345f301fb2be5aa717e76f88
Siebrand Mazeland <siebrand@kitano.nl>
2016-03-07 18:39:26 +0100

    Remove deprecated Language::getLangObj()
    Was deprecated since 1.24. There were no uses in Gerrit extensions
    
    Change-Id: I7ca08416d10fbba5222cb1a1fde75b697d354b09

--- languages/Language.php
+++ languages/Language.php
@@ -4229,16 +4229,7 @@ class Language {
 	public function linkPrefixCharset() {
 		return self::$dataCache->getItem( $this->mCode, 'linkPrefixCharset' );
 	}
-
-	/**
-	 * @deprecated since 1.24, will be removed in 1.25
-	 * @return Language
-	 */
-	function getLangObj() {
-		wfDeprecated( __METHOD__, '1.24' );
-		return $this;
-	}
-
+	
 	/**
 	 * Get the "parent" language which has a converter to convert a "compatible" language
 	 * (in another variant) to this language (eg. zh for zh-cn, but not en for en-gb).
