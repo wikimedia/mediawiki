288a181bfc385c99416762713c18efa3c3d31586
Timo Tijhof <krinklemail@gmail.com>
2016-03-09 15:40:51 +0000

    StubObject: Remove redundant _call() method in StubUserLang
    Already exists the same way in the base class.
    
    Change-Id: If6c89cf42f11655ea45a1d6e1660f6e5e2593169

--- includes/StubObject.php
+++ includes/StubObject.php
@@ -165,18 +165,12 @@ class StubObject {
 }
 
 /**
- * Stub object for the user language. It depends of the user preferences and
- * "uselang" parameter that can be passed to index.php. This object have to be
- * in $wgLang global.
+ * Stub object for the user language. Assigned to the $wgLang global.
  */
 class StubUserLang extends StubObject {
 
 	public function __construct() {
 		parent::__construct( 'wgLang' );
-	}
-
-	public function __call( $name, $args ) {
-		return $this->_call( $name, $args );
 	}
 
 	/**
