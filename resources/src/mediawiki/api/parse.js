c157fb2f29418482ca4f3bfb18634cfa41586edf
Matthew Flaschen <mflaschen@wikimedia.org>
2016-03-10 19:04:15 -0500

    mediawiki.api.parse: Allow parsing pages
    Bug: T46925
    Change-Id: Ic0d762d02c96e0b39b4983d3b66ebdfb6f88d3a2

--- resources/src/mediawiki/api/parse.js
+++ resources/src/mediawiki/api/parse.js
@@ -7,18 +7,30 @@
 		/**
 		 * Convenience method for 'action=parse'.
 		 *
-		 * @param {string} wikitext
+		 * @param {string|mw.Title} content Content to parse, either as a wikitext string or
+		 *   a mw.Title.
+		 * @param {Object} customConfig Config object to set custom settings, e.g. redirects, sectionpreview.
+		 *   prop should not be overridden.
 		 * @return {jQuery.Promise}
 		 * @return {Function} return.done
 		 * @return {string} return.done.data Parsed HTML of `wikitext`.
 		 */
-		parse: function ( wikitext ) {
-			var apiPromise = this.get( {
+		parse: function ( content, customConfig ) {
+			var config = $.extend( {}, customConfig, {
 				formatversion: 2,
 				action: 'parse',
-				contentmodel: 'wikitext',
-				text: wikitext
-			} );
+				contentmodel: 'wikitext'
+			} ), apiPromise;
+
+			if ( $.type( content ) === 'string' ) {
+				// Wikitext
+				config.text = content;
+			} else {
+				// mw.Title
+				config.page = content.getPrefixedDb();
+			}
+
+			apiPromise = this.get( config );
 
 			return apiPromise
 				.then( function ( data ) {
