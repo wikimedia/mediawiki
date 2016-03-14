fa888ea9caf8ac697b55729d5c1d0854f459e0ca
Brian Wolff <bawolff+wn@gmail.com>
2016-03-14 00:10:27 -0400

    Make old parserTests.php work again.
    I still use it locally as it seems a lot faster than phpunit.
    Was throwing exceptions about calling methods that only unit tests
    are supposed to use.
    
    Change-Id: I5dcecee5cef06bb95fb61a448dd9f7a1a21c4ad8

--- tests/parserTests.php
+++ tests/parserTests.php
@@ -30,6 +30,8 @@ $options = [ 'quick', 'color', 'quiet', 'help', 'show-output',
 	'record', 'run-disabled', 'run-parsoid' ];
 $optionsWithArgs = [ 'regex', 'filter', 'seed', 'setversion', 'file' ];
 
+define( 'MW_PHPUNIT_TEST', true );
+
 require_once __DIR__ . '/../maintenance/commandLine.inc';
 require_once __DIR__ . '/TestsAutoLoader.php';
 
