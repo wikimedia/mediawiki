29cd90ede291a1db5e2529bee7548c87cd72178e
addshore <addshorewiki@gmail.com>
2016-03-14 21:07:39 +0000

    Introduce ClearUserWatchlistJob
    Change-Id: Icea573a10078ea3f09dc2e4e9fdc737bf639935d

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -7361,6 +7361,7 @@ $wgJobClasses = [
 	'refreshLinksDynamic' => 'RefreshLinksJob',
 	'activityUpdateJob' => 'ActivityUpdateJob',
 	'categoryMembershipChange' => 'CategoryMembershipChangeJob',
+	'clearUserWatchlist' => 'ClearUserWatchlistJob',
 	'cdnPurge' => 'CdnPurgeJob',
 	'enqueue' => 'EnqueueJob', // local queue for multi-DC setups
 	'null' => 'NullJob'
---
+++ tests/phpunit/includes/jobqueue/jobs/ClearUserWatchlistJobTest.php
@@ -0,0 +1,78 @@
+<?php
+use MediaWiki\MediaWikiServices;
+
+/**
+ * @covers ClearUserWatchlistJob
+ *
+ * @group JobQueue
+ * @group Database
+ *
+ * @licence GNU GPL v2+
+ * @author Addshore
+ */
+class ClearUserWatchlistJobTest extends MediaWikiTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		self::$users['ClearUserWatchlistJobTestUser']
+			= new TestUser( 'ClearUserWatchlistJobTestUser' );
+		$this->runJobs();
+		JobQueueGroup::destroySingletons();
+	}
+
+	private function getUser() {
+		return self::$users['ClearUserWatchlistJobTestUser']->getUser();
+	}
+
+	private function runJobs( $jobLimit = 9999 ) {
+		$runJobs = new RunJobs;
+		$runJobs->loadParamsAndArgs( null, [ 'quiet' => true, 'maxjobs' => $jobLimit ] );
+		$runJobs->execute();
+	}
+
+	private function getWatchedItemStore() {
+		return MediaWikiServices::getInstance()->getWatchedItemStore();
+	}
+
+	public function testRun() {
+		$user = $this->getUser();
+		$watchedItemStore = $this->getWatchedItemStore();
+
+		$watchedItemStore->addWatch( $user, new TitleValue( 0, 'A' ) );
+		$watchedItemStore->addWatch( $user, new TitleValue( 1, 'A' ) );
+		$watchedItemStore->addWatch( $user, new TitleValue( 0, 'B' ) );
+		$watchedItemStore->addWatch( $user, new TitleValue( 1, 'B' ) );
+
+		$maxId = $watchedItemStore->getMaxId();
+
+		$watchedItemStore->addWatch( $user, new TitleValue( 0, 'C' ) );
+		$watchedItemStore->addWatch( $user, new TitleValue( 1, 'C' ) );
+
+		JobQueueGroup::singleton()->push(
+			new ClearUserWatchlistJob(
+				null,
+				[
+					'userId' => $user->getId(),
+					'batchSize' => 2,
+					'maxWatchlistId' => $maxId,
+				]
+			)
+		);
+
+		$this->assertEquals( 1, JobQueueGroup::singleton()->getQueueSizes()['clearUserWatchlist'] );
+		$this->assertEquals( 6, $watchedItemStore->countWatchedItems( $user ) );
+		$this->runJobs( 1 );
+		$this->assertEquals( 1, JobQueueGroup::singleton()->getQueueSizes()['clearUserWatchlist'] );
+		$this->assertEquals( 4, $watchedItemStore->countWatchedItems( $user ) );
+		$this->runJobs( 1 );
+		$this->assertEquals( 1, JobQueueGroup::singleton()->getQueueSizes()['clearUserWatchlist'] );
+		$this->assertEquals( 2, $watchedItemStore->countWatchedItems( $user ) );
+		$this->runJobs( 1 );
+		$this->assertEquals( 0, JobQueueGroup::singleton()->getQueueSizes()['clearUserWatchlist'] );
+		$this->assertEquals( 2, $watchedItemStore->countWatchedItems( $user ) );
+
+		$this->assertTrue( $watchedItemStore->isWatched( $user, new TitleValue( 0, 'C' ) ) );
+		$this->assertTrue( $watchedItemStore->isWatched( $user, new TitleValue( 1, 'C' ) ) );
+	}
+
+}
