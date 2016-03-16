745a311d8e8cd5c8fc56a7bf5c718d3c3bb59f25
addshore <addshorewiki@gmail.com>
2016-03-16 12:24:58 +0000

    PSR-6 BagOStuff wrapper
    Change-Id: Ic00cd2582690f9f92d99b143426e58b7ce3998df

---
+++ tests/phpunit/includes/libs/objectcache/BagOStuffPsrCacheTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Cache\IntegrationTests\CachePoolTest;
+
+/**
+ * @covers BagOStuffPsrCache
+ *
+ * @author Addshore
+ */
+class BagOStuffPsrCacheTest extends CachePoolTest {
+
+	private $bagOStuff;
+
+	public function setUp() {
+		// One HashBagOStuff per used per test (this is a cache after all)...
+		$this->bagOStuff = new HashBagOStuff();
+
+		parent::setUp();
+	}
+
+	public function createCachePool() {
+		return new BagOStuffPsrCache( $this->bagOStuff );
+	}
+
+}
