618a862e26a558a05759d21fbc38684fcf8068f1
Mark A. Hershberger <mah@everybody.org>
2016-03-22 13:36:59 -0400

    Move RemoveUnusedAccount's isInactiveAccount()
    The test here should be more widely accessible.
    
    Use case: I have a wiki with SSO enabled so that user accounts are
        created when someone visits the wiki -- even if they don't
        contribute to the wiki. Periodically, I would like to remove users
        who have left the organisation if they haven't contributed to the
        wiki. This involves querying a user store to see if they've left the
        organisation, so removeUnusedAccounts.php doesn't work.
    
    Also, trim line length in the mantenance script.
    
    Bug: T78816
    Change-Id: If9613e5e06c44182fdf0a044082cb88d74a54dc1

--- maintenance/removeUnusedAccounts.php
+++ maintenance/removeUnusedAccounts.php
@@ -34,8 +34,10 @@ class RemoveUnusedAccounts extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addOption( 'delete', 'Actually delete the account' );
-		$this->addOption( 'ignore-groups', 'List of comma-separated groups to exclude', false, true );
-		$this->addOption( 'ignore-touched', 'Skip accounts touched in last N days', false, true );
+		$this->addOption( 'ignore-groups', 'List of comma-separated groups ' .
+						  'to exclude', false, true );
+		$this->addOption( 'ignore-touched', 'Skip accounts touched in last N ' .
+						  ' days', false, true );
 	}
 
 	public function execute() {
@@ -46,24 +48,29 @@ class RemoveUnusedAccounts extends Maintenance {
 		$this->output( "Checking for unused user accounts...\n" );
 		$del = [];
 		$dbr = $this->getDB( DB_SLAVE );
-		$res = $dbr->select( 'user', [ 'user_id', 'user_name', 'user_touched' ], '', __METHOD__ );
+		$res = $dbr->select( 'user', [ 'user_id', 'user_name', 'user_touched' ],
+							 '', __METHOD__ );
 		if ( $this->hasOption( 'ignore-groups' ) ) {
-			$excludedGroups = explode( ',', $this->getOption( 'ignore-groups' ) );
+			$excludedGroups = explode( ',',
+									   $this->getOption( 'ignore-groups' ) );
 		} else {
 			$excludedGroups = [];
 		}
 		$touched = $this->getOption( 'ignore-touched', "1" );
 		if ( !ctype_digit( $touched ) ) {
-			$this->error( "Please put a valid positive integer on the --ignore-touched parameter.", true );
+			$this->error( "Please put a valid positive integer on the " .
+						  "--ignore-touched parameter.", true );
 		}
 		$touchedSeconds = 86400 * $touched;
 		foreach ( $res as $row ) {
 			# Check the account, but ignore it if it's within a $excludedGroups
 			# group or if it's touched within the $touchedSeconds seconds.
 			$instance = User::newFromId( $row->user_id );
-			if ( count( array_intersect( $instance->getEffectiveGroups(), $excludedGroups ) ) == 0
-				&& $this->isInactiveAccount( $row->user_id, true )
-				&& wfTimestamp( TS_UNIX, $row->user_touched ) < wfTimestamp( TS_UNIX, time() - $touchedSeconds )
+			if ( count( array_intersect( $instance->getEffectiveGroups(),
+										 $excludedGroups ) ) == 0
+				&& $instance->isInactiveAccount( true )
+				&& wfTimestamp( TS_UNIX, $row->user_touched ) <
+				 wfTimestamp( TS_UNIX, time() - $touchedSeconds )
 			) {
 				# Inactive; print out the name and flag it
 				$del[] = $row->user_id;
@@ -76,59 +83,32 @@ class RemoveUnusedAccounts extends Maintenance {
 		# If required, go back and delete each marked account
 		if ( $count > 0 && $this->hasOption( 'delete' ) ) {
 			$this->output( "\nDeleting unused accounts..." );
-			$dbw = $this->getDB( DB_MASTER );
-			$dbw->delete( 'user', [ 'user_id' => $del ], __METHOD__ );
-			$dbw->delete( 'user_groups', [ 'ug_user' => $del ], __METHOD__ );
-			$dbw->delete( 'user_former_groups', [ 'ufg_user' => $del ], __METHOD__ );
-			$dbw->delete( 'user_properties', [ 'up_user' => $del ], __METHOD__ );
-			$dbw->delete( 'logging', [ 'log_user' => $del ], __METHOD__ );
-			$dbw->delete( 'recentchanges', [ 'rc_user' => $del ], __METHOD__ );
-			$this->output( "done.\n" );
-			# Update the site_stats.ss_users field
-			$users = $dbw->selectField( 'user', 'COUNT(*)', [], __METHOD__ );
-			$dbw->update(
-				'site_stats',
-				[ 'ss_users' => $users ],
-				[ 'ss_row_id' => 1 ],
-				__METHOD__
-			);
+			self::removeUnusedAccounts( $del );
 		} elseif ( $count > 0 ) {
-			$this->output( "\nRun the script again with --delete to remove them from the database.\n" );
+			$this->output( "\nRun the script again with --delete to remove " .
+						   "them from the database.\n" );
 		}
 		$this->output( "\n" );
 	}
 
-	/**
-	 * Could the specified user account be deemed inactive?
-	 * (No edits, no deleted edits, no log entries, no current/old uploads)
-	 *
-	 * @param int $id User's ID
-	 * @param bool $master Perform checking on the master
-	 * @return bool
-	 */
-	private function isInactiveAccount( $id, $master = false ) {
-		$dbo = $this->getDB( $master ? DB_MASTER : DB_SLAVE );
-		$checks = [
-			'revision' => 'rev',
-			'archive' => 'ar',
-			'image' => 'img',
-			'oldimage' => 'oi',
-			'filearchive' => 'fa'
-		];
-		$count = 0;
-
-		$this->beginTransaction( $dbo, __METHOD__ );
-		foreach ( $checks as $table => $fprefix ) {
-			$conds = [ $fprefix . '_user' => $id ];
-			$count += (int)$dbo->selectField( $table, 'COUNT(*)', $conds, __METHOD__ );
-		}
-
-		$conds = [ 'log_user' => $id, 'log_type != ' . $dbo->addQuotes( 'newusers' ) ];
-		$count += (int)$dbo->selectField( 'logging', 'COUNT(*)', $conds, __METHOD__ );
-
-		$this->commitTransaction( $dbo, __METHOD__ );
-
-		return $count == 0;
+	public static function removeUnusedAccounts( array $id ) {
+		$dbw = $this->getDB( DB_MASTER );
+		$dbw->delete( 'user', [ 'user_id' => $del ], __METHOD__ );
+		$dbw->delete( 'user_groups', [ 'ug_user' => $del ], __METHOD__ );
+		$dbw->delete( 'user_former_groups', [ 'ufg_user' => $del ],
+					  __METHOD__ );
+		$dbw->delete( 'user_properties', [ 'up_user' => $del ], __METHOD__ );
+		$dbw->delete( 'logging', [ 'log_user' => $del ], __METHOD__ );
+		$dbw->delete( 'recentchanges', [ 'rc_user' => $del ], __METHOD__ );
+		$this->output( "done.\n" );
+		# Update the site_stats.ss_users field
+		$users = $dbw->selectField( 'user', 'COUNT(*)', [], __METHOD__ );
+		$dbw->update(
+		'site_stats',
+		[ 'ss_users' => $users ],
+		[ 'ss_row_id' => 1 ],
+		__METHOD__
+		);
 	}
 }
 
