2a3c15473c6ee6a94eebaf288eb6b78884ea2b97
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-03-30 22:05:25 +0200

    Allow to define exceptions for Special:UncategorizedCategories on-wiki
    The new system message uncategorized-categories-exceptionlist can be changed
    on-wiki to hold a list of categories, that shouldn't be visible on the special
    page Special:UncategorizedCategories (with or without a trailing "* "), even if
    these categories aren't in any category.
    
    Bug: T126117
    Change-Id: I65989e40f3caa2fad7b8b35109c0466e01084f72

--- includes/specials/SpecialUncategorizedcategories.php
+++ includes/specials/SpecialUncategorizedcategories.php
@@ -27,9 +27,58 @@
  * @ingroup SpecialPage
  */
 class UncategorizedCategoriesPage extends UncategorizedPagesPage {
+	/**
+	 * Holds a list of categories, which shouldn't be listed on this special page,
+	 * even if it is uncategorized.
+	 * @var array
+	 */
+	private $exceptionList = null;
+
 	function __construct( $name = 'Uncategorizedcategories' ) {
 		parent::__construct( $name );
 		$this->requestedNamespace = NS_CATEGORY;
+	}
+
+	/**
+	 * Returns an array of categorie titles (usually without the namespace), which
+	 * shouldn't be listed on this page, even if they're uncategorized.
+	 *
+	 * @return array
+	 */
+	private function getExceptionList() {
+		if ( $this->exceptionList === null ) {
+			$cache = ObjectCache::getMainWANInstance();
+			$key = $cache->makeKey( 'specialuncategorizedcategories', 'exceptionlist' );
+			$cachedList = $cache->get( $key );
+
+			if ( $cachedList !== false ) {
+				$this->exceptionList = $cachedList;
+			} else {
+				$exList = $this->msg( 'uncategorized-categories-exceptionlist' )
+					->inContentLanguage()->plain();
+				$proposedTitles = explode( "\n", $exList );
+				foreach ( $proposedTitles as $count => $title ) {
+					$title = preg_replace( "/^\* /", '', $title );
+					$title = Title::newFromText( $title );
+					if ( $title ) {
+						$this->exceptionList[] = $title->getDBKey();
+					}
+				}
+				$cache->set( $key, $this->exceptionList, WANObjectCache::TTL_INDEFINITE );
+			}
+		}
+		return $this->exceptionList;
+	}
+
+	public function getQueryInfo() {
+		$dbr = wfGetDB( DB_SLAVE );
+		$query = parent::getQueryInfo();
+		$exceptionList = $this->getExceptionList();
+		if ( $exceptionList ) {
+			$query['conds'][] = 'page_title not in ( ' . $dbr->makeList( $exceptionList ) . ' )';
+		}
+
+		return $query;
 	}
 
 	/**
