bbea11e58cdfb8e9fc2c619155e0bc424eee0472
dcausse <dcausse@wikimedia.org>
2016-03-31 11:13:21 +0200

    Expose SearchEngine specific profiles
    This patch introduces a way for SearchEngine implementations to expose
    specific search profiles useful to fine-tune the various behaviors related to
    search.
    
    A SearchEngine can expose a list of profiles by overriding
    SearchEngine::getProfiles( $profileType ), profileType stands for the type of
    profile being customized. Currently only "completion" type is supported but we
    could later expose other types like "rescore" to alter the fulltext ranking
    functions.
    
    It allows Api that uses SearchEngine to expose a new param populated with the
    list of supported profiles. In this patch opensearch and prefixsearch have been
    modified to expose a new param that permits to control the behavior of the
    algorithm behind "search as you type" suggestions.
    
    In an other patch I'd like to expose these profiles to the user settings tab to
    address concerns of some users who prefer stricter suggestions.
    
    This patch allows api consumers that might have been confused by fuzzy
    suggestions to switch to stricter profiles and to officialize the behavior
    behind the hidden param cirrusUseCompletionSuggester.
    
    Change-Id: I66be724d8975976c98c91badbf421f237e014f89

--- includes/api/ApiQueryPrefixSearch.php
+++ includes/api/ApiQueryPrefixSearch.php
@@ -23,8 +23,12 @@
  * @ingroup API
  */
 class ApiQueryPrefixSearch extends ApiQueryGeneratorBase {
+	/** @var SearchEngine */
+	private $searchEngine;
+
 	public function __construct( $query, $moduleName ) {
 		parent::__construct( $query, $moduleName, 'ps' );
+		$this->searchEngine = SearchEngine::create();
 	}
 
 	public function execute() {
@@ -45,10 +49,13 @@ class ApiQueryPrefixSearch extends ApiQueryGeneratorBase {
 		$namespaces = $params['namespace'];
 		$offset = $params['offset'];
 
-		$searchEngine = SearchEngine::create();
-		$searchEngine->setLimitOffset( $limit + 1, $offset );
-		$searchEngine->setNamespaces( $namespaces );
-		$titles = $searchEngine->extractTitles( $searchEngine->completionSearchWithVariants( $search ) );
+		$this->searchEngine->setLimitOffset( $limit + 1, $offset );
+		$this->searchEngine->setNamespaces( $namespaces );
+		if ( isset( $params['profile'] ) ) {
+			$this->searchEngine->setFeatureData( SearchEngine::COMPLETION_PROFILE_TYPE, $params['profile'] );
+		}
+		$titles = $this->searchEngine->extractTitles(
+				$this->searchEngine->completionSearchWithVariants( $search ) );
 
 		if ( $resultPageSet ) {
 			$resultPageSet->setRedirectMergePolicy( function( array $current, array $new ) {
@@ -99,29 +106,64 @@ class ApiQueryPrefixSearch extends ApiQueryGeneratorBase {
 	}
 
 	public function getAllowedParams() {
+		$allowedParams = [
+			'search' => [
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			],
+			'namespace' => [
+				ApiBase::PARAM_DFLT => NS_MAIN,
+				ApiBase::PARAM_TYPE => 'namespace',
+				ApiBase::PARAM_ISMULTI => true,
+			],
+			'limit' => [
+				ApiBase::PARAM_DFLT => 10,
+				ApiBase::PARAM_TYPE => 'limit',
+				ApiBase::PARAM_MIN => 1,
+				// Non-standard value for compatibility with action=opensearch
+				ApiBase::PARAM_MAX => 100,
+				ApiBase::PARAM_MAX2 => 200,
+			],
+			'offset' => [
+				ApiBase::PARAM_DFLT => 0,
+				ApiBase::PARAM_TYPE => 'integer',
+			],
+		];
+		$profileParam = static::buildProfileApiParam( $this->searchEngine );
+		if ( $profileParam ) {
+			$allowedParams['profile'] = $profileParam;
+		}
+		return $allowedParams;
+	}
+
+	/**
+	 * Build the profile api param definitions.
+	 * @param SearchEngine $searchEngine
+	 * @return array|null the api param definition or null if profiles are
+	 * not supported by the searchEngine implementation.
+	 */
+	public static function buildProfileApiParam( $searchEngine ) {
+		$profiles = $searchEngine->getProfiles( SearchEngine::COMPLETION_PROFILE_TYPE );
+		if ( $profiles ) {
+			$types = [];
+			$helpMessages = [];
+			$defaultProfile = null;
+			foreach ( $profiles as $profile ) {
+				$types[] = $profile['name'];
+				if ( isset ( $profile['desc-message'] ) ) {
+					$helpMessages[$profile['name']] = $profile['desc-message'];
+				}
+				if ( isset ( $profile['default'] ) && $profile['default'] ) {
+					$defaultProfile = $profile['name'];
+				}
+			}
 			return [
-				'search' => [
-					ApiBase::PARAM_TYPE => 'string',
-					ApiBase::PARAM_REQUIRED => true,
-				],
-				'namespace' => [
-					ApiBase::PARAM_DFLT => NS_MAIN,
-					ApiBase::PARAM_TYPE => 'namespace',
-					ApiBase::PARAM_ISMULTI => true,
-				],
-				'limit' => [
-					ApiBase::PARAM_DFLT => 10,
-					ApiBase::PARAM_TYPE => 'limit',
-					ApiBase::PARAM_MIN => 1,
-					// Non-standard value for compatibility with action=opensearch
-					ApiBase::PARAM_MAX => 100,
-					ApiBase::PARAM_MAX2 => 200,
-				],
-				'offset' => [
-					ApiBase::PARAM_DFLT => 0,
-					ApiBase::PARAM_TYPE => 'integer',
-				],
+				ApiBase::PARAM_TYPE => $types,
+				ApiBase::PARAM_HELP_MSG_PER_VALUE => $helpMessages,
+				ApiBase::PARAM_DFLT => $defaultProfile,
 			];
+		}
+		return null;
 	}
 
 	protected function getExamplesMessages() {
