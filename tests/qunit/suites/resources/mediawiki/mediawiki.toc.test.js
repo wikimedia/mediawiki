7176d4ea87080e7e4ff9ee622a080b4daca3ce4c
Matt Fitzpatrick <mattfitzpat@sbcglobal.net>
2016-03-31 11:18:29 -0700

    Add role="navigation" and label to table of contents div
    Adds accessibility context to the automatically generated table
    of contents div for a page. The div's role is navigation, and
    its label is the heading "Contents".
    
    Most screen readers announce the div's role, "navigation",
    meaning a collection of navigational links.
    
    Additionally, with the navigation role, the div is a landmark
    and a region. Users of screen readers that support jumping to
    landmarks, like NVDA, or jumping to regions, like JAWS, can use
    those methods to jump to the table of contents.
    
    Change-Id: Ie9b291301f683d2be2cb2134f9d6ae3f7a6d831b

--- tests/qunit/suites/resources/mediawiki/mediawiki.toc.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.toc.test.js
@@ -11,9 +11,9 @@
 
 		assert.strictEqual( $( '#toc' ).length, 0, 'There is no table of contents on the page at the beginning' );
 
-		tocHtml = '<div id="toc" class="toc">' +
+		tocHtml = '<div role="navigation" id="toc" class="toc" aria-labelledby="tocheading">' +
 			'<div id="toctitle">' +
-			'<h2>Contents</h2>' +
+			'<h2 id="tocheading">Contents</h2>' +
 			'</div>' +
 			'<ul><li></li></ul>' +
 			'</div>';
