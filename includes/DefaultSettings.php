577dc49eaa25f80df1615ab66fa0bf19e51f601c
MGChecker <hgasuser@gmail.com>
2016-04-02 23:15:31 +0200

    Add possibility to filter for log_action in all core logs
    Adds possibility to filter all core logs with multiple log_actions, Log actions newusers and create for the newusers log are treated as one, because newusers is the predecessor of create and they describe the same actions.
    
    Bug: T16334
    Bug: T109267
    Change-Id: I04a0ef1a6b4ea90c517a11045b6bd73c26b96863

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -7240,11 +7240,35 @@ $wgActionFilteredLogs = [
 		'reblock' => [ 'reblock' ],
 		'unblock' => [ 'unblock' ],
 	],
+	'contentmodel' => [
+		'change' => [ 'change' ],
+		'new' => [ 'new' ],
+	],
 	'delete' => [
 		'delete' => [ 'delete' ],
 		'restore' => [ 'restore' ],
 		'event' => [ 'event' ],
 		'revision' => [ 'revision' ],
+	],
+	'import' => [
+		'interwiki' => [ 'interwiki' ],
+		'upload' => [ 'upload' ],
+	],
+	'managetags' => [
+		'create' => [ 'create' ],
+		'delete' => [ 'delete' ],
+		'activate' => [ 'activate' ],
+		'deactivate' => [ 'deactivate' ],
+	],
+	'move' => [
+		'move' => [ 'move' ],
+		'move_redir' => [ 'move_redir' ],
+	],
+	'newusers' => [
+		'create' => [ 'create', 'newusers' ],
+		'create2' => ['create2' ],
+		'autocreate' => [ 'autocreate' ],
+		'byemail' => [ 'byemail' ],
 	],
 	'patrol' => [
 		'patrol' => [ 'patrol' ],
@@ -7254,6 +7278,18 @@ $wgActionFilteredLogs = [
 		'protect' => [ 'protect' ],
 		'modify' => [ 'modify' ],
 		'unprotect' => [ 'unprotect' ],
+		'move_prot' => ['move_prot'],
+	],
+	'rights' => [
+		'rights' => [ 'rights' ],
+		'autopromote' => [ 'autopromote' ],
+	],
+	'suppress' => [
+		'event' => [ 'event' ],
+		'revision' => [ 'revision' ],
+		'delete' => [ 'delete' ],
+		'block' => [ 'block' ],
+		'reblock' => [ 'reblock' ],
 	],
 	'upload' => [
 		'upload' => [ 'upload' ],
