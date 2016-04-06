693d5d09776bbde20ac9d23db69dc306c46f0913
addshore <addshorewiki@gmail.com>
2016-04-06 11:46:50 +0100

    WatchedItemStore use MediaWikiServices
    Change-Id: I5c8c1a652a35e055d8d65753a60c0d1684e1c37d

--- includes/MediaWikiServices.php
+++ includes/MediaWikiServices.php
@@ -33,6 +33,7 @@ use SiteStore;
 use SpecialPageFactory;
 use Title;
 use User;
+use WatchedItemStore;
 use Wikimedia\Assert\Assert;
 
 /**
@@ -439,6 +440,13 @@ class MediaWikiServices extends ServiceContainer {
 	 */
 	public function getDBLoadBalancer() {
 		return $this->getService( 'DBLoadBalancer' );
+	}
+
+	/**
+	 * @return WatchedItemStore
+	 */
+	public function getWatchedItemStore() {
+		return $this->getService( 'WatchedItemStore' );
 	}
 
 	///////////////////////////////////////////////////////////////////////////
