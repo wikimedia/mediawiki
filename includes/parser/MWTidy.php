8a57d86ea7ec28ac12c60cde1436d688678e9340
Tim Starling <tstarling@wikimedia.org>
2016-04-07 12:24:29 +1000

    Rewrite TidySupport and add option --use-tidy-config
    * Have TidySupport provide $wgTidyConfig instead of the legacy globals
    * Add --use-tidy-config option to parserTests.php. This tells
      TidySupport to use the tidy configuration from LocalSettings.php
      instead of the traditional safe defaults.
    * Add a way for TidySupport to disable tidy via $wgTidyConfig, using
      driver=>disabled
    
    Change-Id: Ie76e68e2d5238d0a1aef49a1a815c0d1cd8bfdae

--- includes/parser/MWTidy.php
+++ includes/parser/MWTidy.php
@@ -135,6 +135,8 @@ class MWTidy {
 				case 'Html5Internal':
 					self::$instance = new MediaWiki\Tidy\Html5Internal( $config );
 					break;
+				case 'disabled':
+					return false;
 				default:
 					throw new MWException( "Invalid tidy driver: \"{$config['driver']}\"" );
 			}
