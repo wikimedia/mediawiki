e60a74067a27cb4b7503a477c310f5e977260c2f
Stanislav Malyshev <smalyshev@gmail.com>
2016-04-15 14:01:23 -0700

    Create Kafka event relayer
    Bug: T125138
    Change-Id: I9d7705cb164bc975c3a0ddf4a33ac54fe7de931c

---
+++ includes/libs/eventrelayer/EventRelayerKafka.php
@@ -0,0 +1,61 @@
+<?php
+use Kafka\Produce;
+
+/**
+ * Event relayer for Apache Kafka.
+ * Configuring for WANCache:
+ * 'relayerConfig' => [ 'class' => 'EventRelayerKafka',
+ * 			'KafkaEventChannel' => 'wancache-purge', 'KafkaEventHost' => 'localhost:9092' ],
+ */
+class EventRelayerKafka extends EventRelayer {
+
+	/**
+	 * Configuration.
+	 *
+	 * @var Config
+	 */
+	protected $config;
+
+	/**
+	 * Kafka producer.
+	 *
+	 * @var Produce
+	 */
+	protected $producer;
+
+	/**
+	 * Create Kafka producer.
+	 *
+	 * @param Config $config
+	 */
+	public function __construct( array $params ) {
+		$this->config = new HashConfig( $params );
+		if ( !$this->config->has( 'KafkaEventHost' ) ) {
+			throw new InvalidArgumentException( "KafkaEventHost must be configured" );
+		}
+	}
+
+	/**
+	 * Get the producer object from kafka-php.
+	 * @return Produce
+	 */
+	protected function getKafkaProducer() {
+		if ( !$this->producer ) {
+			$this->producer = Produce::getInstance( null, null, $this->config->get( 'KafkaEventHost' ) );
+		}
+		return $this->producer;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see EventRelayer::doNotify()
+	 *
+	 */
+	protected function doNotify( $channel, array $events ) {
+		$jsonEvents = array_map( 'json_encode', $events );
+		$producer = $this->getKafkaProducer();
+		$producer->setMessages( $channel, 0, $jsonEvents );
+		$producer->send();
+	}
+}
