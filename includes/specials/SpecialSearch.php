30f734abf1e8a92f92dc3e0bb490a7afe0e901b8
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-04-19 19:30:39 +0200

    Search: Change dataLocation default value and add docs
    Implemented Krinkle's comments of the followed up commit.
    
    Follow up I2480149
    
    Change-Id: I8e51c0d3a3ca1690ea3ea08469a1cd52d9c151f3

--- includes/specials/SpecialSearch.php
+++ includes/specials/SpecialSearch.php
@@ -1225,6 +1225,7 @@ class SpecialSearch extends SpecialPage {
 			'name' => 'search',
 			'autofocus' => trim( $term ) === '',
 			'value' => $term,
+			'dataLocation' => 'content',
 		] );
 
 		$out =
