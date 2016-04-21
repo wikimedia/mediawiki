05d769bd27a2cd0ec31fdd16b3ee8bb762f7fcf2
Fomafix <fomafix@googlemail.com>
2016-04-21 05:39:08 +0000

    Simplify autocomplete attribute in HTMLForm
    Follows-up 7489a3e8
    
    Change-Id: Ifb17c88e39df7031054b3bee83772172c64d0a6b

--- tests/phpunit/includes/htmlform/HTMLFormTest.php
+++ tests/phpunit/includes/htmlform/HTMLFormTest.php
@@ -2,22 +2,56 @@
 
 /**
  * @covers HTMLForm
+ *
+ * @licence GNU GPL v2+
+ * @author Gergő Tisza
+ * @author Thiemo Mättig
  */
 class HTMLFormTest extends MediaWikiTestCase {
-	public function testGetHTML_empty() {
+
+	private function getInstance() {
 		$form = new HTMLForm( [] );
 		$form->setTitle( Title::newFromText( 'Foo' ) );
+		return $form;
+	}
+
+	public function testGetHTML_empty() {
+		$form = $this->getInstance();
 		$form->prepareForm();
 		$html = $form->getHTML( false );
-		$this->assertRegExp( '/<form\b/', $html );
+		$this->assertStringStartsWith( '<form ', $html );
 	}
 
 	/**
 	 * @expectedException LogicException
 	 */
 	public function testGetHTML_noPrepare() {
-		$form = new HTMLForm( [] );
-		$form->setTitle( Title::newFromText( 'Foo' ) );
+		$form = $this->getInstance();
 		$form->getHTML( false );
 	}
+
+	public function testAutocompleteDefaultsToNull() {
+		$form = $this->getInstance();
+		$this->assertNotContains( 'autocomplete', $form->wrapForm( '' ) );
+	}
+
+	public function testAutocompleteWhenSetToNull() {
+		$form = $this->getInstance();
+		$form->setAutocomplete( null );
+		$this->assertNotContains( 'autocomplete', $form->wrapForm( '' ) );
+	}
+
+	public function testAutocompleteWhenSetToFalse() {
+		$form = $this->getInstance();
+		// Previously false was used instead of null to indicate the attribute should not be set
+		$form->setAutocomplete( false );
+		$this->assertNotContains( 'autocomplete', $form->wrapForm( '' ) );
+	}
+
+	public function testAutocompleteWhenSetToOff() {
+		$form = $this->getInstance();
+		$form->setAutocomplete( 'off' );
+		$this->assertContains( ' autocomplete="off"', $form->wrapForm( '' ) );
+	}
+
 }
