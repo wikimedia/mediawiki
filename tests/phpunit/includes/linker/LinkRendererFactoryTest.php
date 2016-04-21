67e62c0b25e991843043794770c02f9392d28fe1
Kunal Mehta <legoktm@member.fsf.org>
2016-04-21 13:13:21 -0700

    Add LinkRenderer (rewrite of Linker::link())
    This is a rewrite of Linker::link() to a non-static, LinkTarget-based
    interface. Users of plain Linker::link() with no options can use the
    LinkRenderer instance provided by MediaWikiServices. Others that
    have specific options should create and configure their own instance,
    which can be used to create as many links as necessary.
    
    The main entrypoints for making links are:
    * ->makeLink( $target, $text, $attribs, $query );
    * ->makeKnownLink( $target, $text, $attribs, $query );
    * ->makeBrokenLink( $target, $text, $attribs, $query );
    
    The order of the parameters are the same as Linker::link(), except
    $options are now part of the LinkRenderer instance, and
    known/broken status requires calling the function explicitly.
    Additionally, instead of passing in raw $html for the link text, the
    $text parameter will automatically be escaped unless it is specially
    marked as safe HTML using the MediaWiki\Linker\HtmlArmor class.
    
    The LinkBegin and LinkEnd hooks are now deprecated, but still function
    for backwards-compatability. Clients should migrate to the nearly-
    equivalent LinkRendererBegin and LinkRendererEnd hooks.
    The main differences between the hooks are:
    * Passing HtmlPageLinkRenderer object instead of deprecated DummyLinker
    * Using LinkTarget instead of Title
    * Begin hook can no longer change known/broken status of link. Use the
    TitleIsAlwaysKnown hook for that.
    * $options are no longer passed, they can be read (but shouldn't be
    modified!) from the LinkRenderer object.
    
    Bug: T469
    Change-Id: I057cc86ae6404a080aa3c8e0e956ecbb10a897d5

---
+++ tests/phpunit/includes/linker/LinkRendererFactoryTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\Linker\LinkRendererFactory;
+use MediaWiki\MediaWikiServices;
+
+/**
+ * @covers LinkRendererFactory
+ */
+class LinkRendererFactoryTest extends MediaWikiLangTestCase {
+
+	/**
+	 * @var TitleFormatter
+	 */
+	private $titleFormatter;
+
+	public function setUp() {
+		parent::setUp();
+		$this->titleFormatter = MediaWikiServices::getInstance()->getTitleFormatter();
+	}
+
+	public static function provideCreateFromLegacyOptions() {
+		return [
+			[
+				[ 'noclasses' ],
+				'getNoClasses',
+				true
+			],
+			[
+				[ 'forcearticlepath' ],
+				'getForceArticlePath',
+				true
+			],
+			[
+				[ 'http' ],
+				'getExpandURLs',
+				PROTO_HTTP
+			],
+			[
+				[ 'https' ],
+				'getExpandURLs',
+				PROTO_HTTPS
+			],
+			[
+				[ 'stubThreshold' => 150 ],
+				'getStubThreshold',
+				150
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideCreateFromLegacyOptions
+	 */
+	public function testCreateFromLegacyOptions( $options, $func, $val ) {
+		$factory = new LinkRendererFactory( $this->titleFormatter );
+		$linkRenderer = $factory->createFromLegacyOptions(
+			$options
+		);
+		$this->assertInstanceOf( LinkRenderer::class, $linkRenderer );
+		$this->assertEquals( $val, $linkRenderer->$func(), $func );
+	}
+
+	public function testCreate() {
+		$factory = new LinkRendererFactory( $this->titleFormatter );
+		$this->assertInstanceOf( LinkRenderer::class, $factory->create() );
+	}
+
+	public function testCreateForUser() {
+		$user = $this->getMock( User::class, [ 'getStubThreshold' ] );
+		$user->expects( $this->once() )
+			->method( 'getStubThreshold' )
+			->willReturn( 15 );
+		$factory = new LinkRendererFactory( $this->titleFormatter );
+		$linkRenderer = $factory->createForUser( $user );
+		$this->assertInstanceOf( LinkRenderer::class, $linkRenderer );
+		$this->assertEquals( 15, $linkRenderer->getStubThreshold() );
+	}
+}
---
+++ tests/phpunit/includes/linker/LinkRendererTest.php
@@ -0,0 +1,134 @@
+<?php
+
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\MediaWikiServices;
+
+/**
+ * @covers LinkRenderer
+ */
+class LinkRendererTest extends MediaWikiLangTestCase {
+
+	/**
+	 * @var TitleFormatter
+	 */
+	private $titleFormatter;
+
+	public function setUp() {
+		parent::setUp();
+		$this->setMwGlobals( [
+			'wgArticlePath' => '/wiki/$1',
+			'wgServer' => '//example.org',
+			'wgCanonicalServer' => 'http://example.org',
+			'wgScriptPath' => '/w',
+			'wgScript' => '/w/index.php',
+		] );
+		$this->titleFormatter = MediaWikiServices::getInstance()->getTitleFormatter();
+	}
+
+	public function testMergeAttribs() {
+		$target = new TitleValue( NS_SPECIAL, 'Blankpage' );
+		$linkRenderer = new LinkRenderer( $this->titleFormatter );
+		$link = $linkRenderer->makeBrokenLink( $target, null, [
+			// Appended to class
+			'class' => 'foobar',
+			// Suppresses href attribute
+			'href' => false,
+			// Extra attribute
+			'bar' => 'baz'
+		] );
+		$this->assertEquals(
+			'<a href="/wiki/Special:BlankPage" class="new foobar" '
+			. 'title="Special:BlankPage (page does not exist)" bar="baz">'
+			. 'Special:BlankPage</a>',
+			$link
+		);
+	}
+
+	public function testMakeKnownLink() {
+		$target = new TitleValue( NS_MAIN, 'Foobar' );
+		$linkRenderer = new LinkRenderer( $this->titleFormatter );
+
+		// Query added
+		$this->assertEquals(
+			'<a href="/w/index.php?title=Foobar&amp;foo=bar" '. 'title="Foobar">Foobar</a>',
+			$linkRenderer->makeKnownLink( $target, null, [], [ 'foo' => 'bar' ] )
+		);
+
+		// forcearticlepath
+		$linkRenderer->setForceArticlePath( true );
+		$this->assertEquals(
+			'<a href="/wiki/Foobar?foo=bar" title="Foobar">Foobar</a>',
+			$linkRenderer->makeKnownLink( $target, null, [], [ 'foo' => 'bar' ] )
+		);
+
+		// expand = HTTPS
+		$linkRenderer->setForceArticlePath( false );
+		$linkRenderer->setExpandURLs( PROTO_HTTPS );
+		$this->assertEquals(
+			'<a href="https://example.org/wiki/Foobar" title="Foobar">Foobar</a>',
+			$linkRenderer->makeKnownLink( $target )
+		);
+	}
+
+	public function testMakeBrokenLink() {
+		$target = new TitleValue( NS_MAIN, 'Foobar' );
+		$special = new TitleValue( NS_SPECIAL, 'Foobar' );
+		$linkRenderer = new LinkRenderer( $this->titleFormatter );
+
+		// action=edit&redlink=1 added
+		$this->assertEquals(
+			'<a href="/w/index.php?title=Foobar&amp;action=edit&amp;redlink=1" '
+			. 'class="new" title="Foobar (page does not exist)">Foobar</a>',
+			$linkRenderer->makeBrokenLink( $target )
+		);
+
+		// action=edit&redlink=1 not added due to action query parameter
+		$this->assertEquals(
+			'<a href="/w/index.php?title=Foobar&amp;action=foobar" class="new" '
+			. 'title="Foobar (page does not exist)">Foobar</a>',
+			$linkRenderer->makeBrokenLink( $target, null, [], [ 'action' => 'foobar' ] )
+		);
+
+		// action=edit&redlink=1 not added due to NS_SPECIAL
+		$this->assertEquals(
+			'<a href="/wiki/Special:Foobar" class="new" title="Special:Foobar '
+			. '(page does not exist)">Special:Foobar</a>',
+			$linkRenderer->makeBrokenLink( $special )
+		);
+
+		// fragment stripped
+		$this->assertEquals(
+			'<a href="/w/index.php?title=Foobar&amp;action=edit&amp;redlink=1" '
+			. 'class="new" title="Foobar (page does not exist)">Foobar</a>',
+			$linkRenderer->makeBrokenLink( $target->createFragmentTarget( 'foobar' ) )
+		);
+	}
+
+	public function testMakeLink() {
+		$linkRenderer = new LinkRenderer( $this->titleFormatter );
+		$foobar = new TitleValue( NS_SPECIAL, 'Foobar' );
+		$blankpage = new TitleValue( NS_SPECIAL, 'Blankpage' );
+		$this->assertEquals(
+			'<a href="/wiki/Special:Foobar" class="new" title="Special:Foobar '
+			. '(page does not exist)">foo</a>',
+			$linkRenderer->makeLink( $foobar, 'foo' )
+		);
+
+		$this->assertEquals(
+			'<a href="/wiki/Special:BlankPage" title="Special:BlankPage">blank</a>',
+			$linkRenderer->makeLink( $blankpage, 'blank' )
+		);
+
+		$this->assertEquals(
+			'<a href="/wiki/Special:Foobar" class="new" title="Special:Foobar '
+			. '(page does not exist)">&lt;script&gt;evil()&lt;/script&gt;</a>',
+			$linkRenderer->makeLink( $foobar, '<script>evil()</script>' )
+		);
+
+		$this->assertEquals(
+			'<a href="/wiki/Special:Foobar" class="new" title="Special:Foobar '
+			. '(page does not exist)"><script>evil()</script></a>',
+			$linkRenderer->makeLink( $foobar, new HtmlArmor( '<script>evil()</script>' ) )
+		);
+	}
+}
