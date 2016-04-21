4b12162429f614bff45277ffa23d50cf09244056
Kunal Mehta <legoktm@member.fsf.org>
2016-04-21 13:13:21 -0700

    Add LinkRenderer (rewrite of Linker::link())
    This is a rewrite of Linker::link() to a non-static, LinkTarget-based
    interface. Users of plain Linker::link() with no options can use the
    LinkRenderer instance provided by MediaWikiServices. Others that
    have specific options should create and configure their own instance,
    which can be used to create as many links as necessary.
    
    The main entrypoints for making links are:
    * ->makeLink( $target, $text, $attribs, $query );
    * ->makeKnownLink( $target, $text, $attribs, $query );
    * ->makeBrokenLink( $target, $text, $attribs, $query );
    
    The order of the parameters are the same as Linker::link(), except
    $options are now part of the LinkRenderer instance, and
    known/broken status requires calling the function explicitly.
    Additionally, instead of passing in raw $html for the link text, the
    $text parameter will automatically be escaped unless it is specially
    marked as safe HTML using the MediaWiki\Linker\HtmlArmor class.
    
    The LinkBegin and LinkEnd hooks are now deprecated, but still function
    for backwards-compatability. Clients should migrate to the nearly-
    equivalent LinkRendererBegin and LinkRendererEnd hooks.
    The main differences between the hooks are:
    * Passing HtmlPageLinkRenderer object instead of deprecated DummyLinker
    * Using LinkTarget instead of Title
    * Begin hook can no longer change known/broken status of link. Use the
    TitleIsAlwaysKnown hook for that.
    * $options are no longer passed, they can be read (but shouldn't be
    modified!) from the LinkRenderer object.
    
    Bug: T469
    Change-Id: I057cc86ae6404a080aa3c8e0e956ecbb10a897d5

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -38,6 +38,8 @@
  */
 
 use MediaWiki\Interwiki\ClassicInterwikiLookup;
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\Linker\LinkRendererFactory;
 use MediaWiki\MediaWikiServices;
 
 return [
@@ -157,6 +159,16 @@ return [
 		return new LinkCache(
 			$services->getTitleFormatter()
 		);
+	},
+
+	'LinkRendererFactory' => function( MediaWikiServices $services ) {
+		return new LinkRendererFactory(
+			$services->getTitleFormatter()
+		);
+	},
+
+	'LinkRenderer' => function( MediaWikiServices $services ) {
+		return $services->getLinkRendererFactory()->create();
 	},
 
 	'GenderCache' => function( MediaWikiServices $services ) {
