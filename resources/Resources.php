4477aff7fb0575743ff80cb9733db1c8e2b2160f
Bartosz Dziewoński <matma.rex@gmail.com>
2016-04-22 15:21:33 +0200

    [WIP] LoginSignupSpecialPage: Convert form to OOUI
    Bug: T85853
    Change-Id: If9fac19db8b25de08fe0ba85b9a3d1ee53f51630

--- resources/Resources.php
+++ resources/Resources.php
@@ -2028,7 +2028,6 @@ return [
 	'mediawiki.special.userlogin.signup.js' => [
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.userlogin.signup.js',
 		'messages' => [
-			'createacct-error',
 			'createacct-emailrequired',
 			'noname',
 			'userexists',
