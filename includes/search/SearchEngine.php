7e18cfc3b5ed4e957a9ed8ac7af4a68d1a95576c
Stanislav Malyshev <smalyshev@gmail.com>
2016-04-26 15:19:58 -0700

    Infrastructure for augmenting search results
    Bug: T117493
    Change-Id: Ia5413a7846cc961026a2dc3542b619493bc76a23

--- includes/search/SearchEngine.php
+++ includes/search/SearchEngine.php
@@ -695,6 +695,37 @@ abstract class SearchEngine {
 		Hooks::run( 'SearchIndexFields', [ &$fields, $this ] );
 		return $fields;
 	}
+
+	/**
+	 * Augment search results with extra data.
+	 *
+	 * @param SearchResultSet $resultSet
+	 */
+	public function augmentSearchResults( SearchResultSet $resultSet ) {
+		$setAugmentors = [];
+		$rowAugmentors = [];
+		Hooks::run( "SearchResultsAugment", [ &$setAugmentors, &$rowAugmentors ] );
+
+		if ( !$setAugmentors && !$rowAugmentors ) {
+			// We're done here
+			return;
+		}
+
+		// Convert row augmentors to set augmentor
+		foreach ( $rowAugmentors as $name => $row ) {
+			if ( isset( $setAugmentors[$name] ) ) {
+				throw new InvalidArgumentException( "Both row and set augmentors are defined for $name" );
+			}
+			$setAugmentors[$name] = new PerRowAugmentor( $row );
+		}
+
+		foreach ( $setAugmentors as $name => $augmentor ) {
+			$data = $augmentor->augmentAll( $resultSet );
+			if ( $data ) {
+				$resultSet->setAugmentedData( $name, $data );
+			}
+		}
+	}
 }
 
 /**
