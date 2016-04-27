f26fd75b2465e9ba4b366597056a98f4fe47259a
Aaron Schulz <aschulz@wikimedia.org>
2016-04-27 15:43:38 -0700

    Fix timestamp check in ApiStashEdit::checkCache
    * The wrong time method was used, and it also was not set.
    * Bumped the threshold a bit while at it, which was basically
      0-5 minutes before.
    
    Bug: T133332
    Change-Id: Ide3e66f551aa6e50410c562e5c917141d59b7f64

--- includes/api/ApiStashEdit.php
+++ includes/api/ApiStashEdit.php
@@ -40,6 +40,8 @@ class ApiStashEdit extends ApiBase {
 	const ERROR_CACHE = 'error_cache';
 	const ERROR_UNCACHEABLE = 'uncacheable';
 
+	const PRESUME_FRESH_TTL = 5;
+
 	public function execute() {
 		$user = $this->getUser();
 		$params = $this->extractRequestParams();
@@ -281,10 +283,10 @@ class ApiStashEdit extends ApiBase {
 			return false;
 		}
 
-		$time = wfTimestamp( TS_UNIX, $editInfo->output->getTimestamp() );
-		if ( ( time() - $time ) <= 3 ) {
+		$age = time() - wfTimestamp( TS_UNIX, $editInfo->output->getCacheTime() );
+		if ( $age <= self::PRESUME_FRESH_TTL ) {
 			$stats->increment( 'editstash.cache_hits.presumed_fresh' );
-			$logger->debug( "Timestamp-based cache hit for key '$key'." );
+			$logger->debug( "Timestamp-based cache hit for key '$key' (age: $age sec)." );
 			return $editInfo; // assume nothing changed
 		}
 
@@ -343,7 +345,7 @@ class ApiStashEdit extends ApiBase {
 		}
 
 		$stats->increment( 'editstash.cache_hits.proven_fresh' );
-		$logger->debug( "Cache hit for key '$key'." );
+		$logger->debug( "Verified cache hit for key '$key' (age: $age sec)." );
 
 		return $editInfo;
 	}
