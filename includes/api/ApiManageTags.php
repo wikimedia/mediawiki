aa4b43a01e0e597ea50d8e409ab17e68c401bcc6
MGChecker <hgasuser@gmail.com>
2016-04-30 19:01:04 +0200

    Split off permission to delete tags from managechangetags permission
    Every permission included in managechangetags is really harmless and can be reverted
    in short time, except of the permission to irriversibly delete tags. That's why
    this should be excluded in an additional permission that other wikis can restrict
    deletechangetags more than other tag management actions.
    
    Bug: T133811
    Change-Id: Ieb9199f2c6997316ae3468ff5a92b2d2456c012f

--- includes/api/ApiManageTags.php
+++ includes/api/ApiManageTags.php
@@ -29,8 +29,8 @@ class ApiManageTags extends ApiBase {
 		$params = $this->extractRequestParams();
 
 		// make sure the user is allowed
-		if ( !$this->getUser()->isAllowed( 'managechangetags' ) ) {
-			$this->dieUsage( "You don't have permission to manage change tags", 'permissiondenied' );
+		if ( !$this->getUser()->isAllowed( 'managechangetags' ) && !$this->getUser()->isAllowed( 'deletechangetags' ) ) {
+			$this->dieUsage( "You don't have permission to manage or delete change tags", 'permissiondenied' );
 		}
 
 		$result = $this->getResult();
@@ -65,9 +65,15 @@ class ApiManageTags extends ApiBase {
 	}
 
 	public function getAllowedParams() {
+		$operationTypes = [ 'create', 'activate', 'deactivate' ];
+		if ( $this->getUser()->isAllowed( 'deletechangetags' ) && $this->getUser()->isAllowed( 'managechangetags' ) ) {
+			$operationTypes = [ 'create', 'delete', 'activate', 'deactivate' ];
+		} else {
+			$operationTypes = [ 'delete'];
+		}
 		return [
 			'operation' => [
-				ApiBase::PARAM_TYPE => [ 'create', 'delete', 'activate', 'deactivate' ],
+				ApiBase::PARAM_TYPE => $operationTypes,
 				ApiBase::PARAM_REQUIRED => true,
 			],
 			'tag' => [
