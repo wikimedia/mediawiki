5bbe80670fd307398bba4773772909fe810ee539
addshore <addshorewiki@gmail.com>
2016-05-06 16:11:08 +0100

    Make deprecated TestUser properties private
    Change-Id: I18d6299bad773d16c04b6ea79331c81af8f538bc
    Depends-On: I5317616f99f1344557fd818d0b00073a82197d05
    Depends-On: I15be3de63b2ce13e0ea07b235efd7756c97cebfc
    Depends-On: I80a50e50493ad04f20cab3a1e094a5c3ecb1c19f

--- tests/phpunit/includes/api/ApiTestCase.php
+++ tests/phpunit/includes/api/ApiTestCase.php
@@ -39,7 +39,7 @@ abstract class ApiTestCase extends MediaWikiLangTestCase {
 		$this->setMwGlobals( [
 			'wgAuth' => new AuthPlugin,
 			'wgRequest' => new FauxRequest( [] ),
-			'wgUser' => self::$users['sysop']->user,
+			'wgUser' => self::$users['sysop']->getUser(),
 		] );
 
 		$this->apiContext = new ApiTestContext();
@@ -186,11 +186,11 @@ abstract class ApiTestCase extends MediaWikiLangTestCase {
 		return $data;
 	}
 
-	protected function getTokenList( $user, $session = null ) {
+	protected function getTokenList( TestUser $user, $session = null ) {
 		$data = $this->doApiRequest( [
 			'action' => 'tokens',
 			'type' => 'edit|delete|protect|move|block|unblock|watch'
-		], $session, false, $user->user );
+		], $session, false, $user->getUser() );
 
 		if ( !array_key_exists( 'tokens', $data[0] ) ) {
 			throw new MWException( 'Api failed to return a token list' );
