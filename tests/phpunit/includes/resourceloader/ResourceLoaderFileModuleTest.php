22de17d2d551efc2df3bccb1216976db159cfa9d
Derk-Jan Hartman <hartman.wiki@gmail.com>
2016-05-11 13:53:59 +0200

    resourceloader: Strip leading BOM when concatenating files
    We read files and concatenate their contents. Files may start with a BOM character.
    BOM characters are only allowed at the beginning of a file, not half way.
    Stripping it should be safe, since we already assume that everything is UTF-8.
    
    Change-Id: I14ad698a684e78976e873e9ae2c367475550a063
    (cherry picked from commit 844199e67ca48a4de5253419fe06cedbdea71a34)

--- tests/phpunit/includes/resourceloader/ResourceLoaderFileModuleTest.php
+++ tests/phpunit/includes/resourceloader/ResourceLoaderFileModuleTest.php
@@ -226,4 +226,24 @@ class ResourceLoaderFileModuleTest extends ResourceLoaderTestCase {
 
 		$this->assertEquals( $rl->getTemplates(), $expected );
 	}
+
+	public function testBomConcatenation() {
+		$basePath = __DIR__ . '/../../data/css';
+		$testModule = new ResourceLoaderFileModule( [
+			'localBasePath' => $basePath,
+			'styles' => [ 'bom.css' ],
+			] );
+		$this->assertEquals(
+			substr( file_get_contents( "$basePath/bom.css" ), 0, 10 ),
+			"\xef\xbb\xbf.efbbbf",
+			'File has leading BOM'
+		);
+
+		$contextLtr = $this->getResourceLoaderContext( 'en', 'ltr' );
+		$this->assertEquals(
+			$testModule->getStyles( $contextLtr ),
+			[ 'all' => ".efbbbf_bom_char_at_start_of_file {}\n" ],
+			'Leading BOM removed when concatenating files'
+		);
+	}
 }
