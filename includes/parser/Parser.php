b8732886087fc09769734271b34502986a5b61b1
Kunal Mehta <legoktm@member.fsf.org>
2016-05-12 17:37:17 -0700

    Parser: Replace Linker::link() with LinkRenderer
    Replaces usage of Linker::link() in Parser and LinkHolderArray with the
    new LinkRenderer.
    
    One notable change is that Linker::getLinkColour() is called inside of
    LinkRenderer at link generation time instead of ahead of time.
    Both should read from LinkCache, so there will be no difference in
    queries, but this means the 'GetLinkColours' hook will not have access
    to those classes. The only two extensions that use that hook in Gerrit
    (Disambiguator and ProofreadPage) won't be affected.
    
    Change-Id: Icb796ef08d70926728732ab5468940c09ba5eaf8

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -20,6 +20,9 @@
  * @file
  * @ingroup Parser
  */
+use MediaWiki\Linker\HtmlArmor;
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\MediaWikiServices;
 
 /**
  * @defgroup Parser Parser
@@ -242,6 +245,11 @@ class Parser {
 
 	/** @var SectionProfiler */
 	protected $mProfiler;
+
+	/**
+	 * @var LinkRenderer
+	 */
+	protected $mLinkRenderer;
 
 	/**
 	 * @param array $conf
@@ -881,6 +889,24 @@ class Parser {
 			$this->mPreprocessor = new $class( $this );
 		}
 		return $this->mPreprocessor;
+	}
+
+	/**
+	 * Get a LinkRenderer instance to make links with
+	 *
+	 * @since 1.28
+	 * @return LinkRenderer
+	 */
+	public function getLinkRenderer() {
+		if ( !$this->mLinkRenderer ) {
+			$this->mLinkRenderer = MediaWikiServices::getInstance()
+				->getLinkRendererFactory()->create();
+			$this->mLinkRenderer->setStubThreshold(
+				$this->getOptions()->getStubThreshold()
+			);
+		}
+
+		return $this->mLinkRenderer;
 	}
 
 	/**
@@ -2346,7 +2372,9 @@ class Parser {
 			$text = htmlspecialchars( $nt->getPrefixedText() );
 		}
 
-		$link = Linker::linkKnown( $nt, "$prefix$text$inside", [], $query );
+		$link = $this->getLinkRenderer()->makeKnownLink(
+			$nt, new HtmlArmor( "$prefix$text$inside" ), $query
+		);
 
 		return $this->armorLinks( $link ) . $trail;
 	}
