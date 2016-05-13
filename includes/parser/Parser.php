122e793e26e0ff13106ec2b2e396025e1d36747d
Kunal Mehta <legoktm@member.fsf.org>
2016-05-12 17:37:17 -0700

    Parser: Replace Linker::link() with LinkRenderer
    Replaces usage of Linker::link() in Parser and LinkHolderArray with the
    new LinkRenderer.
    
    Change-Id: Icb796ef08d70926728732ab5468940c09ba5eaf8

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -20,6 +20,8 @@
  * @file
  * @ingroup Parser
  */
+use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\MediaWikiServices;
 
 /**
  * @defgroup Parser Parser
@@ -247,6 +249,11 @@ class Parser {
 
 	/** @var SectionProfiler */
 	protected $mProfiler;
+
+	/**
+	 * @var LinkRenderer
+	 */
+	protected $mLinkRenderer;
 
 	/**
 	 * @param array $conf
@@ -886,6 +893,24 @@ class Parser {
 			$this->mPreprocessor = new $class( $this );
 		}
 		return $this->mPreprocessor;
+	}
+
+	/**
+	 * Get a LinkRenderer instance to make links with
+	 *
+	 * @since 1.28
+	 * @return LinkRenderer
+	 */
+	public function getLinkRenderer() {
+		if ( !$this->mLinkRenderer ) {
+			$this->mLinkRenderer = MediaWikiServices::getInstance()
+				->getLinkRendererFactory()->create();
+			$this->mLinkRenderer->setStubThreshold(
+				$this->getOptions()->getStubThreshold()
+			);
+		}
+
+		return $this->mLinkRenderer;
 	}
 
 	/**
@@ -2362,7 +2387,9 @@ class Parser {
 			$text = htmlspecialchars( $nt->getPrefixedText() );
 		}
 
-		$link = Linker::linkKnown( $nt, "$prefix$text$inside", [], $query );
+		$link = $this->getLinkRenderer()->makeKnownLink(
+			$nt, new HtmlArmor( "$prefix$text$inside" ), $query
+		);
 
 		return $this->armorLinks( $link ) . $trail;
 	}
