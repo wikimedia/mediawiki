0728e12fe5914c43251705835bb174ad01ad0983
MGChecker <hgasuser@gmail.com>
2016-05-14 00:10:24 +0200

    Fix GUI for Special:Tags if a user has deletechangetags permission only
    In the really rare case a user hasn't got the deletechangetags, but not the
    managechangetags permission, the links to delete tags aren't displayed (because
    the script only built the table column in this case). By API, deletechangetags
    already works properly without managechangetags.
    
    Change-Id: I675813ba62e6188d650c5486408dbe56e9290ca6

--- includes/specials/SpecialTags.php
+++ includes/specials/SpecialTags.php
@@ -259,6 +259,9 @@ class SpecialTags extends SpecialPage {
 					[ 'tag' => $tag ] );
 			}
 
+		}
+
+		if ( $showManageActions || $showDeleteActions) {
 			$newRow .= Xml::tags( 'td', null, $this->getLanguage()->pipeList( $actionLinks ) );
 		}
 
