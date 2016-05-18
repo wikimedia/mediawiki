d86b5f83810d6c23e656b3e55654343342582f24
Ori Livneh <ori@wikimedia.org>
2016-05-18 02:19:20 -0700

    Whenever possible, reuse User objects in unit tests
    The unit tests spend nearly half of their run time resetting the user table for
    each test. But the majority of tests do not depend on the user table having the
    exact value that the setup code resets it to, and do not need to modify the
    user objects they require to run.
    
    Fix that by providing an API for tests to get User objects, and to indicate
    whether the User object will be subject to destructive modification or not.
    This allows User objects to be reused across multiple unit tests.
    
    Depends-On: I8bbc9ebf32a89157fa9c60fd33151a10e7598af8
    Change-Id: I17ef1f519759c5e7796c259282afe730ef722e96

--- tests/phpunit/includes/user/CentralIdLookupTest.php
+++ tests/phpunit/includes/user/CentralIdLookupTest.php
@@ -45,7 +45,7 @@ class CentralIdLookupTest extends MediaWikiTestCase {
 			$this->getMockForAbstractClass( 'CentralIdLookup' )
 		);
 
-		$user = User::newFromName( 'UTSysop' );
+		$user = $this->getTestSysop()->getUser();
 		$this->assertSame( $user, $mock->checkAudience( $user ) );
 
 		$user = $mock->checkAudience( CentralIdLookup::AUDIENCE_PUBLIC );
