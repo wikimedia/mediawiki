a3ab823e5fa1410d4c92b3aed69aad56b82bb8c5
Matthew Flaschen <mflaschen@wikimedia.org>
2016-05-19 17:10:35 -0400

    RevDel: Fix ChangeTags for archived items
    Bug: T128980
    Change-Id: Ibc9ec57da16e1b71c9efa0d2c062f8d7965f59bc

--- includes/revisiondelete/RevDelArchiveList.php
+++ includes/revisiondelete/RevDelArchiveList.php
@@ -41,15 +41,32 @@ class RevDelArchiveList extends RevDelRevisionList {
 			$timestamps[] = $db->timestamp( $id );
 		}
 
-		return $db->select( 'archive', Revision::selectArchiveFields(),
-				[
-					'ar_namespace' => $this->title->getNamespace(),
-					'ar_title' => $this->title->getDBkey(),
-					'ar_timestamp' => $timestamps
-				],
-				__METHOD__,
-				[ 'ORDER BY' => 'ar_timestamp DESC' ]
-			);
+		$tables = [ 'archive' ];
+		$fields = Revision::selectArchiveFields();
+		$conds = [
+			'ar_namespace' => $this->title->getNamespace(),
+			'ar_title' => $this->title->getDBkey(),
+			'ar_timestamp' => $timestamps,
+		];
+		$join_conds = [];
+		$options = [ 'ORDER BY' => 'ar_timestamp DESC' ];
+
+		ChangeTags::modifyDisplayQuery(
+			$tables,
+			$fields,
+			$conds,
+			$join_conds,
+			$options,
+			''
+		);
+
+		return $db->select( $tables,
+			$fields,
+			$conds,
+			__METHOD__,
+			$options,
+			$join_conds
+		);
 	}
 
 	public function newItem( $row ) {
