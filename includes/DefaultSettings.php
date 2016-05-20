5e71f2acacb99d7200ce17bb406597bf86a64475
Chad Horohoe <chadh@wikimedia.org>
2016-05-20 10:14:12 -0700

    Bump to 1.25.6 for tagging
    Change-Id: Ic29a95b5d410fb0b4b6373ed76612717cce3730b

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.25.5';
+$wgVersion = '1.25.6';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
