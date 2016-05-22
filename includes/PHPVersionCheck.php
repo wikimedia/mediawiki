9637991ad90900e534f6303bed865dc42d36780f
Paladox <thomasmulhall410@yahoo.com>
2016-05-22 18:24:31 +0100

    Test
    Change-Id: Ifd97feb76228c170a547e3b98c517f8a478bd8f3

--- includes/PHPVersionCheck.php
+++ includes/PHPVersionCheck.php
@@ -33,8 +33,17 @@ function wfEntryPointCheck( $entryPoint ) {
 	$mwVersion = '1.27';
 	$minimumVersionPHP = '5.5.9';
 	$phpVersion = PHP_VERSION;
+	$minimumVersionHHVM = '3.14';
+	$hhvmVersion = HHVM_VERSION;
 
 	if ( !function_exists( 'version_compare' )
+		|| version_compare( $phpVersion, $minimumVersionPHP ) < 0
+		&& version_compare( $hhvmVersion, $minimumVersionHHVM ) < 0
+	) {
+		wfHHVMVersionError(
+			$entryPoint, $mwVersion, $minimumVersionPHP, $phpVersion, $minimumVersionHHVM, $hhvmVersion
+		);
+	} elseif ( !function_exists( 'version_compare' )
 		|| version_compare( $phpVersion, $minimumVersionPHP ) < 0
 	) {
 		wfPHPVersionError( $entryPoint, $mwVersion, $minimumVersionPHP, $phpVersion );
@@ -173,6 +182,47 @@ function wfPHPVersionError( $type, $mwVersion, $minimumVersionPHP, $phpVersion )
 			for details of which versions are compatible with prior versions of PHP.
 HTML;
 	wfGenericError( $type, $mwVersion, 'Supported PHP versions', $shortText, $longText, $longHtml );
+}
+
+/**
+ * Display an error for the minimum PHP and HHVM version requirement not being satisfied.
+ *
+ * @param string $type See wfGenericError
+ * @param string $mwVersion See wfGenericError
+ * @param string $minimumVersionPHP The minimum PHP version supported by MediaWiki
+ * @param string $phpVersion The current PHP version
+ * @param string $minimumVersionHHVM The minimum HHVM version supported by MediaWiki
+ * @param string $hhvmVersion The current HHVM version
+ */
+function wfHHVMVersionError(
+	$type, $mwVersion, $minimumVersionPHP, $phpVersion, $minimumVersionHHVM, $hhvmVersion
+) {
+	$shortText = "MediaWiki $mwVersion requires at least "
+		. "PHP version $minimumVersionPHP and HHVM version $minimumVersionHHVM,
+			you are using PHP $phpVersion and HHVM $hhvmVersion.";
+
+	$longText = "Error: You might be using on older PHP version. \n"
+		. "MediaWiki $mwVersion needs PHP $minimumVersionPHP and HHVM $minimumVersionHHVM or higher.\n\n"
+		. "Check if you have a newer php executable with a different name, such as php5.\n\n";
+
+	$longHtml = <<<HTML
+			Please consider <a href="http://www.php.net/downloads.php">upgrading your copy of PHP</a>.
+			PHP versions less than 5.5.0 are no longer supported by the PHP Group and will not receive
+			security or bugfix updates.
+
+			Also please consider upgrading <a href="https://docs.hhvm.com/hhvm/installation/introduction">
+			upgrading your copy of HHVM</a>
+		</p>
+		<p>
+			If for some reason you are unable to upgrade your PHP version, you will need to
+			<a href="https://www.mediawiki.org/wiki/Download">download</a> an older version
+			of MediaWiki from our website.  See our
+			<a href="https://www.mediawiki.org/wiki/Compatibility#PHP">compatibility page</a>
+			for details of which versions are compatible with prior versions of PHP.
+HTML;
+	wfGenericError(
+		$type, $mwVersion, 'Supported PHP and HHVM versions', $shortText, $longText, $longHtml
+	);
 }
 
 /**
