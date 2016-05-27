b0d0e54754ae0ad039e53486b11c3d55f3376002
Derk-Jan Hartman <hartman.wiki@gmail.com>
2016-05-27 23:44:25 +0200

    Separate Special:Search styling info into separate module
    Almost all styling of this page has little to do with the JS module.
    Separating it into a styles module.
    Also made the selector for the JS elements far simpler, as there was
    no reason for being overly specific here.
    
    Bug: T136363
    Change-Id: I96a35f930be26b26f139120f3e5b23c33a5d019e

--- resources/Resources.php
+++ resources/Resources.php
@@ -1894,6 +1894,10 @@ return [
 			'powersearch-togglenone',
 		],
 	],
+	'mediawiki.special.search.styles' => [
+		'styles' => 'resources/src/mediawiki.special/mediawiki.special.search.styles.css',
+		'targets' => [ 'desktop', 'mobile' ],
+	],
 	'mediawiki.special.undelete' => [
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.undelete.js',
 	],
