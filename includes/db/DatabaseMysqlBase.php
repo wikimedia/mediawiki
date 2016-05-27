0636192b0ad8539a8751e4858ac110df4a56c22b
Paladox <thomasmulhall410@yahoo.com>
2016-05-27 20:33:36 +0100

    InnoDB now supports fulltext which we need for searchindex so lets use it
    fulltext is now supported in mysql 5.6
    https://dev.mysql.com/doc/refman/5.6/en/innodb-fulltext-index.html
    
    Lets use it if we use mysql 5.6
    
    or fallback to using this if you use MySQL 5.5 <- ENGINE=MyISAM DEFAULT
    CHARSET=utf8
    
    Add MySQL config wgDBTableOptionsSearch so as we can detect what MySQL
    version we use and use innodb if we are on 5.6 and myisam if were on 5.5
    or less.
    
    Bug: T107875
    Change-Id: I0353f844759fe8a55f135cfad43536a3f8b6f284

--- includes/db/DatabaseMysqlBase.php
+++ includes/db/DatabaseMysqlBase.php
@@ -40,6 +40,8 @@ abstract class DatabaseMysqlBase extends Database {
 	/** @var string|null */
 	private $serverVersion = null;
 
+	public $minimumVersion = '5.6';
+
 	/**
 	 * Additional $params include:
 	 *   - lagDetectionMethod : set to one of (Seconds_Behind_Master,pt-heartbeat).
@@ -1261,6 +1263,15 @@ abstract class DatabaseMysqlBase extends Database {
 			'CHARSET=binary',
 			$vars['wgDBTableOptions']
 		);
+		$status = $this->getConnection();
+		if ( !$status->isOK() ) {
+			return $status;
+		}
+		$conn = $status->value;
+		$version = $conn->getServerVersion();
+
+			$vars['wgDBTableOptionsSearch'] = "ENGINE=MyISAM DEFAULT CHARSET=utf8";
+		
 
 		return $vars;
 	}
