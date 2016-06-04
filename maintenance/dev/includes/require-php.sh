9d689cbf24aeff7644b5ed5cc2c9c293c6ad0680
David Abián <da@davidabian.com>
2016-06-04 14:38:52 +0200

    Update require-php.sh
    Use '--' to prevent errors. See <https://www.gnu.org/software/bash/manual/html_node/Shell-Builtin-Commands.html>.

--- maintenance/dev/includes/require-php.sh
+++ maintenance/dev/includes/require-php.sh
@@ -1,6 +1,6 @@
 # Include-able script to require that we have a known php binary we can execute
 
-. "$DEV/includes/php.sh"
+. -- "$DEV/includes/php.sh"
 
 if [ "x$PHP" == "x" -o ! -x "$PHP" ]; then
 	echo "Local copy of PHP is not installed"
