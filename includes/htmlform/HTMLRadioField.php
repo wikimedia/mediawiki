wikimedia/mediawiki

commit 2f33194497bd94d1cecb21dcb88e9bf3abf2ddaa

HTMLMultiSelectField: Use CheckboxMultiselectInputWidget
Depends on I6f9fad0a462b43ac45c117cd3a3e11c36781cf11 in OOjs UI.

Bug: T123935
Change-Id: I62322739b985cc2143b5094d23b3aa5dd1004c25

resources/src/mediawiki/mediawiki.htmlform.ooui.css
@@ -18,7 +18,3 @@
 	margin-right: 5%;
 	width: 39%;
 }
-
-.oo-ui-fieldLayout .oo-ui-fieldLayout.oo-ui-fieldLayout-align-inline {
-  margin-bottom: 0;
-}

includes/htmlform/HTMLMultiSelectField.php
@@ -65,21 +65,7 @@ class HTMLMultiSelectField extends HTMLFormField implements HTMLNestedFilterable
 
 	protected function getOneCheckbox( $checked, $attribs, $label ) {
 		if ( $this->mParent instanceof OOUIHTMLForm ) {
-			if ( $this->mOptionsLabelsNotFromMessage ) {
-				$label = new OOUI\HtmlSnippet( $label );
-			}
-			return new OOUI\FieldLayout(
-				new OOUI\CheckboxInputWidget( [
-					'name' => "{$this->mName}[]",
-					'selected' => $checked,
-				] + OOUI\Element::configFromHtmlAttributes(
-					$attribs
-				) ),
-				[
-					'label' => $label,
-					'align' => 'inline',
-				]
-			);
+			throw new MWException( 'HTMLMultiSelectField#getOneCheckbox() is not supported' );
 		} else {
 			$elementFunc = [ 'Html', $this->mOptionsLabelsNotFromMessage ? 'rawElement' : 'element' ];
 			$checkbox =
@@ -97,6 +83,38 @@ class HTMLMultiSelectField extends HTMLFormField implements HTMLNestedFilterable
 			}
 			return $checkbox;
 		}
+	}
+
+	/**
+	 * Get the OOUI version of this field.
+	 *
+	 * @since 1.28
+	 * @param string[] $value
+	 * @return OOUI\CheckboxMultiselectInputWidget
+	 */
+	public function getInputOOUI( $value ) {
+		$attr = $this->getTooltipAndAccessKey();
+		$attr['id'] = $this->mID;
+		$attr['name'] = "{$this->mName}[]";
+
+		$attr['value'] = $value;
+		$attr['options'] = $this->getOptionsOOUI();
+
+		if ( $this->mOptionsLabelsNotFromMessage ) {
+			foreach ( $attr['options'] as &$option ) {
+				$option['label'] = new OOUI\HtmlSnippet( $option['label'] );
+			}
+		}
+
+		$attr += OOUI\Element::configFromHtmlAttributes(
+			$this->getAttributes( [ 'disabled', 'tabindex' ] )
+		);
+
+		if ( $this->mClass !== '' ) {
+			$attr['classes'] = [ $this->mClass ];
+		}
+
+		return new OOUI\CheckboxMultiselectInputWidget( $attr );
 	}
 
 	/**

commit 5f67d80afd86a4a7d76c6c98f0b3490da25ff179

HTMLForm: Properly support 'flatlist' in OOUI mode
Previously it worked mostly by chance for multiselect fields (until
2f33194497bd94d1cecb21dcb88e9bf3abf2ddaa broke it), and did not work
at all for radio fields.

Bug: T123935
Change-Id: I94396303bcf16110a505883bec2ce352d833fe2b

includes/htmlform/HTMLRadioField.php
@@ -52,7 +52,6 @@ class HTMLRadioField extends HTMLFormField {
 			'id' => $this->mID,
 			'value' => $value,
 			'options' => $options,
-			'classes' => 'mw-htmlform-flatlist-item',
 		] + OOUI\Element::configFromHtmlAttributes(
 			$this->getAttributes( [ 'disabled', 'tabindex' ] )
 		) );

includes/specials/SpecialBlockList.php
@@ -46,7 +46,7 @@ class SpecialBlockList extends SpecialPage {
 		$out = $this->getOutput();
 		$lang = $this->getLanguage();
 		$out->setPageTitle( $this->msg( 'ipblocklist' ) );
-		$out->addModuleStyles( [ 'mediawiki.special', 'mediawiki.special.blocklist' ] );
+		$out->addModuleStyles( [ 'mediawiki.special' ] );
 
 		$request = $this->getRequest();
 		$par = $request->getVal( 'ip', $par );

resources/Resources.php
@@ -1791,10 +1791,6 @@ return [
 		'styles' => 'resources/src/mediawiki.special/mediawiki.special.block.css',
 		'dependencies' => 'mediawiki.util',
 	],
-	'mediawiki.special.blocklist' => [
-		'styles' => 'resources/src/mediawiki.special/mediawiki.special.blocklist.css',
-		'position' => 'top',
-	],
 	'mediawiki.special.changeslist' => [
 		'position' => 'top',
 		'styles' => 'resources/src/mediawiki.special/mediawiki.special.changeslist.css',

resources/src/mediawiki.special/mediawiki.special.blocklist.css
@@ -1,4 +1,0 @@
-.mw-htmlform-flatlist-item {
-	/* FIXME: There should be an option in OOUI to do that */
-	display: inline-block !important;
-}

resources/src/mediawiki/mediawiki.htmlform.ooui.css
@@ -18,3 +18,9 @@
 	margin-right: 5%;
 	width: 39%;
 }
+
+.mw-htmlform-flatlist .oo-ui-optionWidget,
+.mw-htmlform-flatlist .oo-ui-multioptionWidget {
+	display: inline-block;
+	margin-right: 1em;
+}