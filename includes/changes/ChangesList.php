ec5321e52c0797878401d1a3f705cf985e467aa2
Geoffrey Mon <geofbot@gmail.com>
2016-06-10 20:59:58 -0400

    Unwatch link for pages in Special:Watchlist
    Adds a '×' link to each entry of the watchlist that unwatches the page
    of that entry. When clicked, it changes into a '+' which can be used to
    re-watch the page (effectively undoing the earlier unwatch).
    When a page is unwatched, its entries and the entries of its associated
    talk page (or vice versa) become translucent and are struck through.
    
    Without JS, '×'/'+' link to action=(un)watch for the relevant page.
    
    In addition, ChangesList classes have been modified to allow a prefixer
    that adds a prefix to each line (used in this case to put the unwatch
    link) and to add HTML data attributes to reliably determine the target
    page of each entry. Unit tests have been updated accordingly.
    
    Bug: T2424
    Change-Id: I450b2901413d7e75c11de2a446829fdbb22d31e1

--- includes/changes/ChangesList.php
+++ includes/changes/ChangesList.php
@@ -38,6 +38,9 @@ class ChangesList extends ContextSource {
 	protected $rclistOpen;
 	protected $rcMoveIndex;
 
+	/** @var callable */
+	protected $changeLinePrefixer;
+
 	/** @var BagOStuff */
 	protected $watchMsgCache;
 
@@ -156,12 +159,14 @@ class ChangesList extends ContextSource {
 	 * @return array of classes
 	 */
 	protected function getHTMLClasses( $rc, $watched ) {
-		$classes = [];
+		$classes = [ 'mw-changeslist-line' ];
 		$logType = $rc->mAttribs['rc_log_type'];
 
 		if ( $logType ) {
+			$classes[] = 'mw-changeslist-log';
 			$classes[] = Sanitizer::escapeClass( 'mw-changeslist-log-' . $logType );
 		} else {
+			$classes[] = 'mw-changeslist-edit';
 			$classes[] = Sanitizer::escapeClass( 'mw-changeslist-ns' .
 				$rc->mAttribs['rc_namespace'] . '-' . $rc->mAttribs['rc_title'] );
 		}
@@ -700,6 +705,17 @@ class ChangesList extends ContextSource {
 	protected function isCategorizationWithoutRevision( $rcObj ) {
 		return intval( $rcObj->getAttribute( 'rc_type' ) ) === RC_CATEGORIZE
 			&& intval( $rcObj->getAttribute( 'rc_this_oldid' ) ) === 0;
+	}
+
+	/**
+	 * Sets the callable that generates a change line prefix added to the beginning of each line.
+	 *
+	 * @param callable $prefixer Callable to run that generates the change line prefix.
+	 *     Takes three parameters: a RecentChange object, a ChangesList object,
+	 *     and whether the current entry is a grouped entry.
+	 */
+	public function setChangeLinePrefixer( callable $prefixer ) {
+		$this->changeLinePrefixer = $prefixer;
 	}
 
 }
