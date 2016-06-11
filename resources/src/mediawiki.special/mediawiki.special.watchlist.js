4d1087174ca0b34c948805c660d41cff3161f5cb
Geoffrey Mon <geofbot@gmail.com>
2016-06-10 20:59:58 -0400

    [WIP] Unwatch link for pages in Special:Watchlist
    Bug: T2424
    Change-Id: I450b2901413d7e75c11de2a446829fdbb22d31e1

--- resources/src/mediawiki.special/mediawiki.special.watchlist.js
+++ resources/src/mediawiki.special/mediawiki.special.watchlist.js
@@ -1,15 +1,24 @@
-/*!
+/*
  * JavaScript for Special:Watchlist
- *
- * This script is only loaded, if the user opt-in a setting in Special:Preferences,
- * that the watchlist should be automatically reloaded, when a filter option is
- * changed in the header form.
  */
-jQuery( function ( $ ) {
+( function ( mw, $ ) {
 	// add a listener on all form elements in the header form
-	$( '#mw-watchlist-form input, #mw-watchlist-form select' ).on( 'change', function () {
-		// submit the form, when one of the input fields was changed
-		$( '#mw-watchlist-form' ).submit();
+	$( '.mw-unwatch-link' ).click( function ( event ) {
+		var $unwatchLink = $( event.toElement ),
+			$watchlistLine = $unwatchLink.parent(),
+			$title = $watchlistLine.find( '.mw-title' ),
+			pageTitle = $title.text(),
+			api = new mw.Api();
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		api[ 'unwatch' ]( pageTitle )
+			.done( function () {
+				$watchlistLine.text( 'Removed' );
+			} );
+
+
 	} );
 
-} );
+}( mediaWiki, jQuery ) );
