8139217c0df8ea036860a082ed42c6648528ddc0
Aaron Schulz <aschulz@wikimedia.org>
2016-06-16 13:01:38 -0700

    [WIP] Disable expensive REVISION* magic words in miser mode
    The potential to tank page save time is worse than what little
    use they have, which is mostly just hacks for things that have
    nothing to do with the ID/timestamp itself.
    
    Bug: T137900
    Change-Id: Ieff8423ae3804b42d264f630e1a029199abf5976

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -2471,7 +2471,7 @@ class Parser {
 	 * @return string
 	 */
 	public function getVariableValue( $index, $frame = false ) {
-		global $wgContLang, $wgSitename, $wgServer, $wgServerName;
+		global $wgContLang, $wgSitename, $wgServer, $wgServerName, $wgMiserMode;
 		global $wgArticlePath, $wgScriptPath, $wgStylePath;
 
 		if ( is_null( $this->mTitle ) ) {
@@ -2498,6 +2498,20 @@ class Parser {
 
 		$ts = wfTimestamp( TS_UNIX, $this->mOptions->getTimestamp() );
 		Hooks::run( 'ParserGetVariableValueTs', [ &$parser, &$ts ] );
+
+		// In miser mode, disable words that always cause double-parses on page save (T137900)
+		static $slowVaryWords = [
+			'revisionid' => true,
+			'revisiontimestamp' => true,
+			'revisionday' => true,
+			'revisionday2' => true,
+			'revisionmonth' => true,
+			'revisionmonth1' => true,
+			'revisionyear' => true
+		];
+		if ( $wgMiserMode && isset( $slowVaryWords[$index] ) ) {
+			return $this->mRevisionId ? '-' : '';
+		};
 
 		$pageLang = $this->getFunctionLang();
 
