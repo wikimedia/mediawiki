f634ccf8836c1f52faee97a9634fc5e0a4f727c4
Ariel T. Glenn <ariel@wikimedia.org>
2016-06-21 23:08:29 +0300

    add option to XML dump stubs of page ranges with explicit rev_id ordering
    tested for stubs, text, logging with and without start/end values,
    with and without orderrevs, seems to work as expected, with the
    appropriate changes to the query.
    
    Bug: T29112
    Change-Id: I94ca4a06235bdbed384bb997deb7432bb5aaa5b9

--- maintenance/dumpBackup.php
+++ maintenance/dumpBackup.php
@@ -50,6 +50,8 @@ TEXT
 		$this->addOption( 'stable', 'Dump stable versions of pages' );
 		$this->addOption( 'revrange', 'Dump range of revisions specified by revstart and ' .
 			'revend parameters' );
+		$this->addOption( 'orderrevs', 'Dump revisions in ascending revision order ' .
+			'(implies dump of a range of pages)' );
 		$this->addOption( 'pagelist',
 			'Dump only pages included in the file', false, true );
 		// Options
@@ -127,6 +129,7 @@ TEXT
 		$this->skipFooter = $this->hasOption( 'skip-footer' );
 		$this->dumpUploads = $this->hasOption( 'uploads' );
 		$this->dumpUploadFileContents = $this->hasOption( 'include-files' );
+		$this->orderRevs = $this->hasOption( 'orderrevs' );
 	}
 }
 
