758cd9d2371d529450448cdf7eb2f1f6e099cfee
Chad Horohoe <chadh@wikimedia.org>
2016-06-28 13:24:08 -0700

    Bump version number for release + remove alpha warnings
    Change-Id: I892c94eef2840e0494e1e189be72f1e2c5210a82

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.27.0-rc.1';
+$wgVersion = '1.27.0';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
