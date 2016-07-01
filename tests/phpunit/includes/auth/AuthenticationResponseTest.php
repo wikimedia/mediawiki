0aa7d1d7357b1e41b60a994b7551f205528737e7
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-07-01 18:26:20 +0200

    Show warnings in HTMLForm and warnings as warnings on Login/Signup form
    This commit changes the way how HTMLForm handles a Status object
    when executed from a request. It now handles, beside the errors,
    also the warnings of a Status object and prints them out, wrapped
    in a warning box.
    
    The LoginSignupPage uses this feature to show informative warnings
    actually as warnings and not as more disturbing error messages.
    Error messages should be reserved for errors and only for erros. An
    AuthenticationProvider, which returns an UI AuthenticationResponse
    can choose, if the given message is an error or a warning message.
    
    This commit also adds a new function to Status, which has the same
    function as getErrorMessage(), but is public and has a name that
    indicates, that it can be used for errors and warnings, instead of
    just errors.
    
    Bug: T139179
    Change-Id: I9a27911613e62b5c4cb86bea40696cb37c4f49c2

--- tests/phpunit/includes/auth/AuthenticationResponseTest.php
+++ tests/phpunit/includes/auth/AuthenticationResponseTest.php
@@ -25,6 +25,7 @@ class AuthenticationResponseTest extends \MediaWikiTestCase {
 	public function testConstructors( $constructor, $args, $expect ) {
 		if ( is_array( $expect ) ) {
 			$res = new AuthenticationResponse();
+			$res->messageType = 'warning';
 			foreach ( $expect as $field => $value ) {
 				$res->$field = $value;
 			}
@@ -60,6 +61,7 @@ class AuthenticationResponseTest extends \MediaWikiTestCase {
 			[ 'newFail', [ $msg ], [
 				'status' => AuthenticationResponse::FAIL,
 				'message' => $msg,
+				'messageType' => 'error',
 			] ],
 
 			[ 'newRestart', [ $msg ], [
@@ -75,6 +77,21 @@ class AuthenticationResponseTest extends \MediaWikiTestCase {
 				'status' => AuthenticationResponse::UI,
 				'neededRequests' => [ $req ],
 				'message' => $msg,
+				'messageType' => 'warning',
+			] ],
+
+			[ 'newUI', [ [ $req ], $msg, 'warning' ], [
+				'status' => AuthenticationResponse::UI,
+				'neededRequests' => [ $req ],
+				'message' => $msg,
+				'messageType' => 'warning',
+			] ],
+
+			[ 'newUI', [ [ $req ], $msg, 'error' ], [
+				'status' => AuthenticationResponse::UI,
+				'neededRequests' => [ $req ],
+				'message' => $msg,
+				'messageType' => 'error',
 			] ],
 			[ 'newUI', [ [], $msg ],
 				new \InvalidArgumentException( '$reqs may not be empty' )
