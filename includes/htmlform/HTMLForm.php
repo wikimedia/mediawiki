370d1f034f568ab699519359a3de4cc23322a862
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-07-01 18:26:20 +0200

    Show warnings in HTMLForm and warnings as warnings on Login/Signup form
    This commit changes the way how HTMLForm handles a Status object
    when executed from a request. It now handles, beside the errors,
    also the warnings of a Status object and prints them out, wrapped
    in a warning box.
    
    The LoginSignupPage uses this feature to show informative warnings
    actually as warnings and not as more disturbing error messages.
    Error messages should be reserved for errors and only for erros. An
    AuthenticationProvider, which returns an UI AuthenticationResponse
    can choose, if the given message is an error or a warning message.
    
    Bug: T139179
    Change-Id: I9a27911613e62b5c4cb86bea40696cb37c4f49c2

--- includes/htmlform/HTMLForm.php
+++ includes/htmlform/HTMLForm.php
@@ -997,6 +997,7 @@ class HTMLForm extends ContextSource {
 
 		$html = ''
 			. $this->getErrors( $submitResult )
+			. $this->getWarnings( $submitResult )
 			. $this->getHeaderText()
 			. $this->getBody()
 			. $this->getHiddenFields()
@@ -1215,21 +1216,71 @@ class HTMLForm extends ContextSource {
 	 * @return string
 	 */
 	public function getErrors( $errors ) {
-		if ( $errors instanceof Status ) {
-			if ( $errors->isOK() ) {
-				$errorstr = '';
+		return $this->getErrorsOrWarnings( $errors, 'error' );
+	}
+
+	/**
+	 * Format and display an error message stack.
+	 *
+	 * @param string|array|Status $errors
+	 *
+	 * @return string
+	 */
+	public function getWarnings( $warnings ) {
+		return $this->getErrorsOrWarnings( $warnings, 'warning' );
+	}
+
+	/**
+	 * Returns a formatted list of errors or warnings from the given elements.
+	 *
+	 * @param string|array|Status $elements The set of errors/warnings to process.
+	 * @param string $elementsType Should warnings or errors be returned
+	 * @return string
+	 */
+	public function getErrorsOrWarnings( $elements, $elementsType ) {
+		if ( !in_array( $elementsType, [ 'error', 'warning' ] ) ) {
+			throw new DomainException( $elementsType . ' is not a valid type.' );
+		}
+		if ( $elements instanceof Status ) {
+			if ( $elementsType === 'error' ? $elements->isOK() : $elements->isGood() ) {
+				$elementstr = '';
 			} else {
-				$errorstr = $this->getOutput()->parse( $errors->getWikiText() );
+				if ( $elementsType === 'warning' ) {
+					$elementstr = $this->getOutput()->parse(
+						$this->getWikitextFromStatusWarnings(
+							$elements
+						)
+					);
+				} else {
+					$elementstr = $elements->getWikiText();
+				}
 			}
-		} elseif ( is_array( $errors ) ) {
-			$errorstr = $this->formatErrors( $errors );
+		} elseif ( is_array( $elements ) ) {
+			$elementstr = $this->formatErrors( $elements );
 		} else {
-			$errorstr = $errors;
+			$elementstr = $elements;
 		}
 
-		return $errorstr
-			? Html::rawElement( 'div', [ 'class' => 'error' ], $errorstr )
+		return $elementstr
+			? Html::rawElement( 'div', [ 'class' => $elementsType ], $elementstr )
 			: '';
+	}
+
+	/**
+	 * Returns a formatted string of warnings from the given Status object.
+	 *
+	 * @param Status $status
+	 * @return string
+	 */
+	private function getWikitextFromStatusWarnings( Status $status ) {
+		if ( $status->isGood() ) {
+			return '';
+		}
+		$warnings = '';
+		foreach ( $status->getErrorsByType( 'warning' ) as $warning ) {
+			$warnings .= $warning['message'] . "\n";
+		}
+		return $warnings;
 	}
 
 	/**
