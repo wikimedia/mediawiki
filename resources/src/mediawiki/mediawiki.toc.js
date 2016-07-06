1bf5a652d69871e2820d1156250fa75a3adbdd0b
Ed Sanders <esanders@wikimedia.org>
2016-07-06 22:05:24 +0100

    Use classes instead of IDs for TOC collapsing
    One may way to have multiple TOC's on the page (e.g. in VisualEditor).
    
    Change-Id: I19701c4037b653b2944e407752e50f444861f883

--- resources/src/mediawiki/mediawiki.toc.js
+++ resources/src/mediawiki/mediawiki.toc.js
@@ -3,56 +3,60 @@
 
 	// Table of contents toggle
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
-		var $toc, $tocTitle, $tocToggleLink, $tocList, hideToc;
-		$toc = $content.find( '#toc' );
-		$tocTitle = $content.find( '#toctitle' );
-		$tocToggleLink = $content.find( '#togglelink' );
-		$tocList = $toc.find( 'ul' ).eq( 0 );
+		$content.find( '.toc' ).addBack( '.toc' ).each( function () {
+			var hideToc,
+				$this = $( this ),
+				// .toctitle is new so may not exist in HTML caches for a few weeks,
+				// so keep checking for #toctitle for now
+				$tocTitle = $this.find( '.toctitle, #toctitle' ),
+				$tocToggleLink = $this.find( '.togglelink' ),
+				$tocList = $this.find( 'ul' ).eq( 0 );
 
-		// Hide/show the table of contents element
-		function toggleToc() {
-			if ( $tocList.is( ':hidden' ) ) {
-				$tocList.slideDown( 'fast' );
-				$tocToggleLink.text( mw.msg( 'hidetoc' ) );
-				$toc.removeClass( 'tochidden' );
-				mw.cookie.set( 'hidetoc', null );
-			} else {
-				$tocList.slideUp( 'fast' );
-				$tocToggleLink.text( mw.msg( 'showtoc' ) );
-				$toc.addClass( 'tochidden' );
-				mw.cookie.set( 'hidetoc', '1' );
+			// Hide/show the table of contents element
+			function toggleToc() {
+				if ( $tocList.is( ':hidden' ) ) {
+					$tocList.slideDown( 'fast' );
+					$tocToggleLink.text( mw.msg( 'hidetoc' ) );
+					$this.removeClass( 'tochidden' );
+					mw.cookie.set( 'hidetoc', null );
+				} else {
+					$tocList.slideUp( 'fast' );
+					$tocToggleLink.text( mw.msg( 'showtoc' ) );
+					$this.addClass( 'tochidden' );
+					mw.cookie.set( 'hidetoc', '1' );
+				}
 			}
-		}
 
-		// Only add it if there is a complete TOC and it doesn't
-		// have a toggle added already
-		if ( $toc.length && $tocTitle.length && $tocList.length && !$tocToggleLink.length ) {
-			hideToc = mw.cookie.get( 'hidetoc' ) === '1';
+			// Only add it if there is a complete TOC and it doesn't
+			// have a toggle added already
+			if ( $tocTitle.length && $tocList.length && !$tocToggleLink.length ) {
+				hideToc = mw.cookie.get( 'hidetoc' ) === '1';
 
-			$tocToggleLink = $( '<a role="button" tabindex="0" id="togglelink"></a>' )
-				.text( mw.msg( hideToc ? 'showtoc' : 'hidetoc' ) )
-				.on( 'click keypress', function ( e ) {
-					if (
-						e.type === 'click' ||
-						e.type === 'keypress' && e.which === 13
-					) {
-						toggleToc();
-					}
-				} );
+				$tocToggleLink = $( '<a role="button" tabindex="0" class="togglelink"></a>' )
+					.text( mw.msg( hideToc ? 'showtoc' : 'hidetoc' ) )
+					.on( 'click keypress', function ( e ) {
+						if (
+							e.type === 'click' ||
+							e.type === 'keypress' && e.which === 13
+						) {
+							toggleToc();
+						}
+					} );
 
-			$tocTitle.append(
-				$tocToggleLink
-					.wrap( '<span class="toctoggle"></span>' )
-					.parent()
-						.prepend( '&nbsp;[' )
-						.append( ']&nbsp;' )
-			);
+				$tocTitle.append(
+					$tocToggleLink
+						.wrap( '<span class="toctoggle"></span>' )
+						.parent()
+							.prepend( '&nbsp;[' )
+							.append( ']&nbsp;' )
+				);
 
-			if ( hideToc ) {
-				$tocList.hide();
-				$toc.addClass( 'tochidden' );
+				if ( hideToc ) {
+					$tocList.hide();
+					$this.addClass( 'tochidden' );
+				}
 			}
-		}
+		} );
 	} );
 
 }( mediaWiki, jQuery ) );
