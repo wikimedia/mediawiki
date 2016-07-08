486027d7d5bec36728aa17130d32fe98cf58947f
Ed Sanders <esanders@wikimedia.org>
2016-07-08 17:41:51 +0100

    Truncate gallery caption filenames with CSS
    Bug: T139766
    Change-Id: Iba7efb8f89e132b8eb39f3c9ba76d2b1a9181b2f

--- includes/gallery/TraditionalImageGallery.php
+++ includes/gallery/TraditionalImageGallery.php
@@ -189,8 +189,16 @@ class TraditionalImageGallery extends ImageGalleryBase {
 				// Preloaded into LinkCache above
 				Linker::linkKnown(
 					$nt,
-					htmlspecialchars( $lang->truncate( $nt->getText(), $this->mCaptionLength ) )
-				) . "<br />\n" :
+					htmlspecialchars(
+						$this->mCaptionLength !== true ?
+							$lang->truncate( $nt->getText(), $this->mCaptionLength ) :
+							$nt->getText()
+					),
+					[
+						'class' => 'galleryfilename' .
+							( $this->mCaptionLength === true ? ' galleryfilename-truncate' : '' )
+					]
+				) . "\n" :
 				'';
 
 			$galleryText = $textlink . $text . $fileSize;
