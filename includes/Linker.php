7d132b6cf555dde1c4cee5272362065d9aece466
Kunal Mehta <legoktm@member.fsf.org>
2016-07-12 15:26:53 -0700

    Mark Linker::link() as @deprecated
    ...and add a link to the on-wiki LinkRenderer documentation to the
    class.
    
    Change-Id: I2fefb6c38153028d2cc68c13bc62434d3df69cd2

--- includes/Linker.php
+++ includes/Linker.php
@@ -165,6 +165,8 @@ class Linker {
 	}
 
 	/**
+	 * @deprecated since 1.28, use MediaWiki\Linker\LinkRenderer instead
+	 *
 	 * This function returns an HTML link to the given target.  It serves a few
 	 * purposes:
 	 *   1) If $target is a Title, the correct URL to link to will be figured
