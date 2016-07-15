80e5b160e0985304a540a32002ff356aae886e8f
Timo Tijhof <krinklemail@gmail.com>
2016-07-15 15:13:09 +0100

    resourceloader: Move queue formatting out of OutputPage
    HTML formatting of the queue was distributed over several OutputPage methods.
    Each method demanding a snippet of HTML by calling makeResourceLoaderLink()
    with a limited amount of information. As such, makeResourceLoaderLink() was
    unable to provide the client with the proper state information.
    
    Centralising it also allows it to better reduce duplication in HTML output
    and maintain a more accurate state.
    
    Problems fixed by centralising:
    
    1. The 'user' module is special (due to per-user 'version' and 'user' params).
       It is manually requested via script-src. To avoid a separate (and wrong)
       request from something that requires it, we set state=loading directly.
       However, because the module is in the bottom, the old HTML formatter could
       only put state=loading in the bottom also. This sometimes caused a wrong
       request to be fired for modules=user if something in the top queue
       triggered a requirement for it.
    
    2. Since a464d1d4 (T87871) we track states of page-style modules, with purpose
       of allowing dependencies on style modules without risking duplicate loading
       on pages where the styles are loaded already. This didn't work, because the
       state information about page-style modules is output near the stylesheet,
       which is after the script tag with mw.loader.load(). That runs first, and
       mw.loader would still make a duplicate request before it learns the state.
    
    Changes:
    
    * Document reasons for style/script tag order in getHeadHtml (per 09537e83).
    
    * Pass $type from getModuleStyles() to getAllowedModules(). This wasn't needed
      before since a duplicate check in makeResourceLoaderLink() verified the
      origin a second time.
    
    * Declare explicit position 'top' on 'user.options' and 'user.tokens' module.
      Previously, OutputPage hardcoded them in the top. The new formatter doesn't.
    
    * Remove getHeadScripts().
    * Remove getInlineHeadScripts().
    * Remove getExternalHeadScripts().
    * Remove buildCssLinks().
    * Remove getScriptsForBottomQueue().
    
    * Change where Skin::setupSkinUserCss() is called. This methods lets the skin
      add modules to the queue. Previously it was called from buildCssLinks(),
      via headElement(), via prepareQuickTemplate(), via OutputPage::output().
      It's now in OutputPage::output() directly (slightly earlier). This is needed
      because prepareQuickTemplate() calls bottomScripts() before headElement().
      And bottomScript() would lazy-initialise the queue and lock it before
      setupSkinUserCss() is called from headElement().
      This makes execution order more predictable instead of being dependent on
      the arbitrary order of data extraction in prepareQuickTemplate (which varies
      from one skin to another).
    
    * Compute isUserModulePreview() and isKnownEmpty() for the 'user' module early
      on so. This avoids wrongful loading and fixes problem 1.
    
    Effective changes in output:
    
    * mw.loader.state() is now before mw.loader.load(). This fixes problem 2.
    * mw.loader.state() now sets 'user.options' and 'user.tokens' to "loading".
    * mw.loader.state() now sets 'user' (as "loading" or "ready"). Fixes problem 1.
    
    * The <script async src> tag for 'startup' changed position (slightly).
      Previously it was after all inline scripts and stylesheets. It's still after
      all inline scripts and after most stylesheets, but before any user styles.
      Since the queue is now formatted outside OutputPage, it can't inject the
      meta-ResourceLoaderDynamicStyles tag and user-stylesheet hack in the middle
      of existing output. This shouldn't have any noticable impact.
    
    Bug: T87871
    Change-Id: I605b8cd1e1fc009b4662a0edbc54d09dd65ee1df

--- includes/resourceloader/ResourceLoaderUserTokensModule.php
+++ includes/resourceloader/ResourceLoaderUserTokensModule.php
@@ -77,6 +77,13 @@ class ResourceLoaderUserTokensModule extends ResourceLoaderModule {
 	/**
 	 * @return string
 	 */
+	public function getPosition() {
+		return 'top';
+	}
+
+	/**
+	 * @return string
+	 */
 	public function getGroup() {
 		return 'private';
 	}
