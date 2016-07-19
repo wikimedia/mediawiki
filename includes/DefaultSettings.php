94181d60ba75878f907c2b5b79cb742b742ce148
Chad Horohoe <chadh@wikimedia.org>
2016-07-19 19:18:12 +0000

    Creating new WMF 1.28.0-wmf.11 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.28.0-alpha';
+$wgVersion = '1.28.0-wmf.11';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
