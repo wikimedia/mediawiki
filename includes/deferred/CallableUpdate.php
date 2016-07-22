ad84e82bff1af5768a1e208752ceece5e36bdb63
Aaron Schulz <aschulz@wikimedia.org>
2016-07-21 19:32:09 -0700

    Make statsd counts for MWCallableUpdate actually useful
    Include the method that made the callback in the key.
    
    Change-Id: Ifc7c486ee5c8d57b2516456569bb724cf7dc2b99

--- includes/deferred/CallableUpdate.php
+++ includes/deferred/CallableUpdate.php
@@ -4,21 +4,28 @@
  * Deferrable Update for closure/callback
  */
 class MWCallableUpdate implements DeferrableUpdate {
-	/** @var Closure|callable */
+	/** @var callable */
 	private $callback;
+	/** @var string */
+	private $fname;
 
 	/**
 	 * @param callable $callback
-	 * @throws InvalidArgumentException
+	 * @param string $fname Calling method
 	 */
-	public function __construct( $callback ) {
-		if ( !is_callable( $callback ) ) {
-			throw new InvalidArgumentException( 'Not a valid callback/closure!' );
-		}
+	public function __construct( callable $callback, $fname = 'unknown' ) {
 		$this->callback = $callback;
+		$this->fname = $fname;
 	}
 
 	public function doUpdate() {
 		call_user_func( $this->callback );
+	}
+
+	/**
+	 * @return string Originating method name
+	 */
+	public function getOrigin() {
+		return $this->fname;
 	}
 }
