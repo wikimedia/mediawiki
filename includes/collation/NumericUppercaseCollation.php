6b048025028c2dd6a4ab60b6c43e53e8868a9b9d
Bartosz Dziewoński <matma.rex@gmail.com>
2016-07-27 16:43:01 +0200

    Implement NumericUppercaseCollation
    This collation orders text with numbers "naturally", so that
    'Foo 1' < 'Foo 2' < 'Foo 12'.
    
    Note that this only works in terms of sequences of digits, and the
    behavior for decimal fractions or pretty-formatted numbers may be
    unexpected.
    
    This is only expected to work mostly correctly for English-language
    text. Consider it a proof of concept. You probably want to use
    an UCA collation with '-u-kn' suffix rather than this.
    
    Bug: T8948
    Change-Id: Ie268f2d92c5c75d0aaecf54ede2bdda1af3b309d

---
+++ includes/collation/NumericUppercaseCollation.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Collation that orders text with numbers "naturally", so that 'Foo 1' < 'Foo 2' < 'Foo 12'.
+ *
+ * Note that this only works in terms of sequences of digits, and the behavior for decimal fractions
+ * or pretty-formatted numbers may be unexpected.
+ *
+ * @since 1.28
+ */
+class NumericUppercaseCollation extends UppercaseCollation {
+	public function getSortKey( $string ) {
+		$sortkey = parent::getSortKey( $string );
+
+		// For each sequence of digits, insert the digit '0' and then the length of the sequence
+		// (encoded in two bytes) before it. That's all folks, it sorts correctly now! The '0' ensures
+		// correct position (where digits would normally sort), then the length will be compared putting
+		// shorter numbers before longer ones; if identical, then the characters will be compared, which
+		// generates the correct results for numbers of equal length.
+		$sortkey = preg_replace_callback( '/\d+/', function ( $matches ) {
+			$len = strlen( $matches[0] );
+			// This allows sequences of up to 65536 numeric characters to be handled correctly. One byte
+			// would allow only for 256, which doesn't feel future-proof.
+			$prefix = chr( floor( $len / 256 ) ) . chr( $len % 256 );
+			return '0' . $prefix . $matches[0];
+		}, $sortkey );
+
+		return $sortkey;
+	}
+
+	public function getFirstLetter( $string ) {
+		if ( preg_match( '/^\d/', $string ) ) {
+			return wfMessage( 'category-header-numerals' )->numParams( 0, 9 )->text();
+		} else {
+			return parent::getFirstLetter( $string );
+		}
+	}
+}
