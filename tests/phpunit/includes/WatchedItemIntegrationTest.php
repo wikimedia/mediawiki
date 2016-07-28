70bf2e924e1c0501cd7800e4710ff434dc91f8b8
addshore <addshorewiki@gmail.com>
2016-07-28 09:56:05 +0100

    Remove missed WatchedItem::resetNotificationTimestamp in test
    Change-Id: I3ece74fecd55f248025d9c10aaf1960d5cd3fd7a

--- tests/phpunit/includes/WatchedItemIntegrationTest.php
+++ tests/phpunit/includes/WatchedItemIntegrationTest.php
@@ -18,7 +18,6 @@ class WatchedItemIntegrationTest extends MediaWikiTestCase {
 		$this->hideDeprecated( 'WatchedItem::addWatch' );
 		$this->hideDeprecated( 'WatchedItem::removeWatch' );
 		$this->hideDeprecated( 'WatchedItem::isWatched' );
-		$this->hideDeprecated( 'WatchedItem::resetNotificationTimestamp' );
 		$this->hideDeprecated( 'WatchedItem::duplicateEntries' );
 		$this->hideDeprecated( 'WatchedItem::batchAddWatch' );
 	}
