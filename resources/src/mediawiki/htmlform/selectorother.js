c4b672993b70b4175bf46a23badb8f9c54dd6416
Bartosz Dziewoński <matma.rex@gmail.com>
2016-07-31 01:15:07 +0200

    Split the 'mediawiki.htmlform' module code into multiple files
    This module implements several related, but separate enhancements to
    HTMLForm. While it makes sense to use a single ResourceLoader module
    to serve this code, it doesn't make sense to keep all of it in a
    single file. It was approaching 500 lines of code, and pieces of the
    separate features were mixed together.
    
    This commit mostly shuffles code around, only tweaking some
    indentation, 'var' statements and function wrappers.
    
    There is one small functional change: the enhancements now use the
    'htmlform.enhance' mw.hook, which is fired on document-ready and when
    new fields are added dynamically. Previously it was only used to allow
    extensions to define their own HTMLForm enhancements.
    
    (Also moved HTMLForm styles into the same directory as the newly-split
    scripts.)
    
    Change-Id: I22054b39868239ddb59317dadfaaa067653f8804

---
+++ resources/src/mediawiki/htmlform/selectorother.js
@@ -0,0 +1,61 @@
+/*
+ * HTMLForm enhancements:
+ * Animate the SelectOrOther fields, to only show the text field when 'other' is selected.
+ */
+( function ( mw, $ ) {
+
+	/**
+	 * @class jQuery.plugin.htmlform
+	 */
+
+	/**
+	 * jQuery plugin to fade or snap to visible state.
+	 *
+	 * @param {boolean} [instantToggle=false]
+	 * @return {jQuery}
+	 * @chainable
+	 */
+	$.fn.goIn = function ( instantToggle ) {
+		if ( instantToggle === true ) {
+			return this.show();
+		}
+		return this.stop( true, true ).fadeIn();
+	};
+
+	/**
+	 * jQuery plugin to fade or snap to hiding state.
+	 *
+	 * @param {boolean} [instantToggle=false]
+	 * @return {jQuery}
+	 * @chainable
+	 */
+	$.fn.goOut = function ( instantToggle ) {
+		if ( instantToggle === true ) {
+			return this.hide();
+		}
+		return this.stop( true, true ).fadeOut();
+	};
+
+	mw.hook( 'htmlform.enhance' ).add( function ( $root ) {
+		/**
+		 * @ignore
+		 * @param {boolean|jQuery.Event} instant
+		 */
+		function handleSelectOrOther( instant ) {
+			var $other = $root.find( '#' + $( this ).attr( 'id' ) + '-other' );
+			$other = $other.add( $other.siblings( 'br' ) );
+			if ( $( this ).val() === 'other' ) {
+				$other.goIn( instant );
+			} else {
+				$other.goOut( instant );
+			}
+		}
+
+		$root
+			.on( 'change', '.mw-htmlform-select-or-other', handleSelectOrOther )
+			.each( function () {
+				handleSelectOrOther.call( this, true );
+			} );
+	} );
+
+}( mediaWiki, jQuery ) );
