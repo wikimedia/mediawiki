5aa002dc879241cd117dbb28fe777a60732c5b9a
Bartosz Dziewoński <matma.rex@gmail.com>
2016-07-31 01:15:07 +0200

    Split the 'mediawiki.htmlform' module code into multiple files
    This module implements several related, but separate enhancements to
    HTMLForm. While it makes sense to use a single ResourceLoader module
    to serve this code, it doesn't make sense to keep all of it in a
    single file. It was approaching 500 lines of code, and pieces of the
    separate features were mixed together.
    
    This commit mostly shuffles code around, only tweaking some
    indentation, 'var' statements and function wrappers.
    
    There is one small functional change: the enhancements now use the
    'htmlform.enhance' mw.hook, which is fired on document-ready and when
    new fields are added dynamically. Previously it was only used to allow
    extensions to define their own HTMLForm enhancements.
    
    (Also moved HTMLForm styles into the same directory as the newly-split
    scripts.)
    
    Change-Id: I22054b39868239ddb59317dadfaaa067653f8804

---
+++ resources/src/mediawiki/htmlform/multiselect.js
@@ -0,0 +1,67 @@
+/*
+ * HTMLForm enhancements:
+ * Convert multiselect fields from checkboxes to Chosen selector when requested.
+ */
+( function ( mw, $ ) {
+
+	function addMulti( $oldContainer, $container ) {
+		var name = $oldContainer.find( 'input:first-child' ).attr( 'name' ),
+			oldClass = ( ' ' + $oldContainer.attr( 'class' ) + ' ' ).replace( /(mw-htmlform-field-HTMLMultiSelectField|mw-chosen)/g, '' ),
+			$select = $( '<select>' ),
+			dataPlaceholder = mw.message( 'htmlform-chosen-placeholder' );
+		oldClass = $.trim( oldClass );
+		$select.attr( {
+			name: name,
+			multiple: 'multiple',
+			'data-placeholder': dataPlaceholder.plain(),
+			'class': 'htmlform-chzn-select mw-input ' + oldClass
+		} );
+		$oldContainer.find( 'input' ).each( function () {
+			var $oldInput = $( this ),
+			checked = $oldInput.prop( 'checked' ),
+			$option = $( '<option>' );
+			$option.prop( 'value', $oldInput.prop( 'value' ) );
+			if ( checked ) {
+				$option.prop( 'selected', true );
+			}
+			$option.text( $oldInput.prop( 'value' ) );
+			$select.append( $option );
+		} );
+		$container.append( $select );
+	}
+
+	function convertCheckboxesToMulti( $oldContainer, type ) {
+		var $fieldLabel = $( '<td>' ),
+		$td = $( '<td>' ),
+		$fieldLabelText = $( '<label>' ),
+		$container;
+		if ( type === 'tr' ) {
+			addMulti( $oldContainer, $td );
+			$container = $( '<tr>' );
+			$container.append( $td );
+		} else if ( type === 'div' ) {
+			$fieldLabel = $( '<div>' );
+			$container = $( '<div>' );
+			addMulti( $oldContainer, $container );
+		}
+		$fieldLabel.attr( 'class', 'mw-label' );
+		$fieldLabelText.text( $oldContainer.find( '.mw-label label' ).text() );
+		$fieldLabel.append( $fieldLabelText );
+		$container.prepend( $fieldLabel );
+		$oldContainer.replaceWith( $container );
+		return $container;
+	}
+
+	mw.hook( 'htmlform.enhance' ).add( function ( $root ) {
+		if ( $root.find( '.mw-chosen' ).length ) {
+			mw.loader.using( 'jquery.chosen', function () {
+				$root.find( '.mw-chosen' ).each( function () {
+					var type = this.nodeName.toLowerCase(),
+						$converted = convertCheckboxesToMulti( $( this ), type );
+					$converted.find( '.htmlform-chzn-select' ).chosen( { width: 'auto' } );
+				} );
+			} );
+		}
+	} );
+
+}( mediaWiki, jQuery ) );
