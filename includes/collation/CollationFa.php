12e1e2c4dc6f9fdd17044b6d668c2c6919fcf34f
Amir Sarabadani <Ladsgroup@gmail.com>
2016-07-31 19:06:49 +0430

    [WIP] Add CollationFa
    Very untested
    Bug: T139110
    Change-Id: Ie15a2ee1c22ff4a1d2b721ed137227fe83dd12ea

---
+++ includes/collation/CollationFa.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Workaround for incorrect collation of Persian language ('fa') in ICU (bug T139110).
+ *
+ * 'ا' and 'و' should not be considered the same letter for the purposes of collation in Persian.
+ * We work around this by replacing 'W' and 'w' with 'ᴡ' U+1D21 'LATIN LETTER SMALL CAPITAL W'
+ * for sortkey generation, which is collated like 'W' and is not tailored to have the
+ * same primary weight as 'ء' in Persian.
+ *
+ * @since 1.28
+ */
+class CollationFa extends IcuCollation {
+	public function __construct() {
+		parent::__construct( 'fa' );
+	}
+
+	private static function mangle( $string ) {
+		$newString = str_replace(
+			'ا',
+			'ᴡ', // U+1D21 'LATIN LETTER SMALL CAPITAL W'
+			$string
+		);
+
+		$newString = str_replace(
+			'و',
+			'ᴠ', // U+1D20 'LATIN LETTER SMALL CAPITAL v'
+			$newString
+		);
+		return newString;
+	}
+
+	private static function unmangle( $string ) {
+		$newString = str_replace(
+			'ᴡ', // U+1D21 'LATIN LETTER SMALL CAPITAL W'
+			'ا',
+			$string
+		);
+
+		$newString = str_replace(
+			'ᴠ', // U+1D20 'LATIN LETTER SMALL CAPITAL v'
+			'و',
+			$newString
+		);
+		return newString;
+	}
+
+	public function getSortKey( $string ) {
+		return parent::getSortKey( self::mangle( $string ) );
+	}
+
+	public function getFirstLetter( $string ) {
+		return self::unmangle( parent::getFirstLetter( self::mangle( $string ) ) );
+	}
+}
