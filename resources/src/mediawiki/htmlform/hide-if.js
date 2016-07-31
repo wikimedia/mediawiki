c10c8364c9bfd72a950f0724624c6e8be2cdc140
Bartosz Dziewoński <matma.rex@gmail.com>
2016-07-31 16:56:23 +0200

    HTMLForm: Refactor loading of modules required to infuse fields
    Rather than have a master list in autoinfuse.js (duplicated in
    hide-if.js), we put this information in each field class and put it
    in the generated HTML as a separate 'data-' attribute. This also
    allows new fields defined by extensions to be correctly autoinfused.
    
    Change-Id: I3da75706209cbc16b19cc3f02b355e58ca75fec9

--- resources/src/mediawiki/htmlform/hide-if.js
+++ resources/src/mediawiki/htmlform/hide-if.js
@@ -201,25 +201,17 @@
 
 	mw.hook( 'htmlform.enhance' ).add( function ( $root ) {
 		$root.find( '.mw-htmlform-hide-if' ).each( function () {
-			var v, i, fields, test, func, spec, self, modules,
+			var v, i, fields, test, func, spec, self, modules, data,extraModules,
 				$el = $( this );
 
 			modules = [];
 			if ( $el.is( '[data-ooui]' ) ) {
 				modules.push( 'mediawiki.htmlform.ooui' );
-				if ( $el.filter( '.mw-htmlform-field-HTMLTitleTextField' ).length ) {
-					// FIXME: TitleInputWidget should be in its own module
-					modules.push( 'mediawiki.widgets' );
-				}
-				if ( $el.filter( '.mw-htmlform-field-HTMLUserTextField' ).length ) {
-					modules.push( 'mediawiki.widgets.UserInputWidget' );
-				}
-				if (
-					$el.filter( '.mw-htmlform-field-HTMLSelectNamespace' ).length ||
-					$el.filter( '.mw-htmlform-field-HTMLSelectNamespaceWithButton' ).length
-				) {
-					// FIXME: NamespaceInputWidget should be in its own module (probably?)
-					modules.push( 'mediawiki.widgets' );
+				data = $el.data( 'mw-modules' );
+				if ( data ) {
+					// We can trust this value, 'data-mw-*' attributes are banned from user content in Sanitizer
+					extraModules = data.split( ',' );
+					modules.push.apply( modules, extraModules );
 				}
 			}
 
