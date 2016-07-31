ab73cad4fcf0c8997c1eede2c272e36cbd98db41
Bartosz Dziewoński <matma.rex@gmail.com>
2016-07-31 16:56:23 +0200

    HTMLForm: Refactor loading of modules required to infuse fields
    Rather than have a master list in autoinfuse.js (duplicated in
    hide-if.js), we put this information in each field class and put it
    in the generated HTML as a separate 'data-' attribute. This also
    allows new fields defined by extensions to be correctly autoinfused.
    
    Change-Id: I3da75706209cbc16b19cc3f02b355e58ca75fec9

--- resources/src/mediawiki/htmlform/autoinfuse.js
+++ resources/src/mediawiki/htmlform/autoinfuse.js
@@ -2,7 +2,7 @@
  * HTMLForm enhancements:
  * Infuse some OOjs UI HTMLForm fields (those which benefit from always being infused).
  */
-( function ( mw ) {
+( function ( mw, $ ) {
 
 	mw.hook( 'htmlform.enhance' ).add( function ( $root ) {
 		var $oouiNodes, modules;
@@ -12,20 +12,9 @@
 			// The modules are preloaded (added server-side in HTMLFormField, and the individual fields
 			// which need extra ones), but this module doesn't depend on them. Wait until they're loaded.
 			modules = [ 'mediawiki.htmlform.ooui' ];
-			if ( $oouiNodes.filter( '.mw-htmlform-field-HTMLTitleTextField' ).length ) {
-				// FIXME: TitleInputWidget should be in its own module
-				modules.push( 'mediawiki.widgets' );
-			}
-			if ( $oouiNodes.filter( '.mw-htmlform-field-HTMLUserTextField' ).length ) {
-				modules.push( 'mediawiki.widgets.UserInputWidget' );
-			}
-			if (
-				$oouiNodes.filter( '.mw-htmlform-field-HTMLSelectNamespace' ).length ||
-				$oouiNodes.filter( '.mw-htmlform-field-HTMLSelectNamespaceWithButton' ).length
-			) {
-				// FIXME: NamespaceInputWidget should be in its own module (probably?)
-				modules.push( 'mediawiki.widgets' );
-			}
+			$oouiNodes.each( function () {
+				modules = modules.concat( $( this ).data( 'modules' ) || [] );
+			} );
 			mw.loader.using( modules ).done( function () {
 				$oouiNodes.each( function () {
 					OO.ui.infuse( this );
@@ -35,4 +24,4 @@
 
 	} );
 
-}( mediaWiki ) );
+}( mediaWiki, jQuery ) );
