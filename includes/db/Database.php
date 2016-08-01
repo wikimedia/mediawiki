432fa77441d12b1935c7bebfe77ad1aec2d0b0b9
Addshore <addshorewiki@gmail.com>
2016-08-01 18:22:30 +0000

    Revert "Automatically detect READ_ONLY mode for MySQL/MariaDB"
    This reverts commit f4bf52e84388ad0d1d270aac9092b49f05bfc988.
    
    Bug: T141804
    Change-Id: Ifacaf90ca91c9454178ecd536eba9beebd24c663

--- includes/db/Database.php
+++ includes/db/Database.php
@@ -2456,10 +2456,6 @@ abstract class DatabaseBase implements IDatabase {
 		return false;
 	}
 
-	public function serverIsReadOnly() {
-		return false;
-	}
-
 	final public function onTransactionResolution( callable $callback ) {
 		if ( !$this->mTrxLevel ) {
 			throw new DBUnexpectedError( $this, "No transaction is active." );
