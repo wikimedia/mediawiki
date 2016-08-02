wikimedia/mediawiki

commit f4bf52e84388ad0d1d270aac9092b49f05bfc988

Automatically detect READ_ONLY mode for MySQL/MariaDB
This avoids having users think they can make edits when an
exception will just be thrown when they try to save. Likewise
for other write actions.

Bug: T24923
Change-Id: I49c4057b672875ec6f34681a5668a509cec05677

includes/db/loadbalancer/LoadBalancer.php
@@ -49,6 +49,8 @@ class LoadBalancer {
 	private $mLoadMonitor;
 	/** @var BagOStuff */
 	private $srvCache;
+	/** @var WANObjectCache */
+	private $wanCache;
 
 	/** @var bool|DatabaseBase Database connection that caused a problem */
 	private $mErrorConnection;
@@ -76,6 +78,8 @@ class LoadBalancer {
 	const MAX_LAG = 10;
 	/** @var integer Max time to wait for a slave to catch up (e.g. ChronologyProtector) */
 	const POS_WAIT_TIMEOUT = 10;
+	/** @var integer Seconds to cache master server read-only status */
+	const TTL_CACHE_READONLY = 5;
 
 	/**
 	 * @var boolean
@@ -135,6 +139,7 @@ class LoadBalancer {
 		}
 
 		$this->srvCache = ObjectCache::getLocalServerInstance();
+		$this->wanCache = ObjectCache::getMainWANInstance();
 
 		if ( isset( $params['trxProfiler'] ) ) {
 			$this->trxProfiler = $params['trxProfiler'];
@@ -578,7 +583,7 @@ class LoadBalancer {
 
 		if ( $masterOnly ) {
 			# Make master-requested DB handles inherit any read-only mode setting
-			$conn->setLBInfo( 'readOnlyReason', $this->getReadOnlyReason( $wiki ) );
+			$conn->setLBInfo( 'readOnlyReason', $this->getReadOnlyReason( $wiki, $conn ) );
 		}
 
 		return $conn;
@@ -1274,10 +1279,11 @@ class LoadBalancer {
 	/**
 	 * @note This method may trigger a DB connection if not yet done
 	 * @param string|bool $wiki Wiki ID, or false for the current wiki
+	 * @param DatabaseBase|null DB master connection; used to avoid loops [optional]
 	 * @return string|bool Reason the master is read-only or false if it is not
 	 * @since 1.27
 	 */
-	public function getReadOnlyReason( $wiki = false ) {
+	public function getReadOnlyReason( $wiki = false, DatabaseBase $conn = null ) {
 		if ( $this->readOnlyReason !== false ) {
 			return $this->readOnlyReason;
 		} elseif ( $this->getLaggedSlaveMode( $wiki ) ) {
@@ -1288,9 +1294,35 @@ class LoadBalancer {
 				return 'The database has been automatically locked ' .
 					'while the slave database servers catch up to the master.';
 			}
+		} elseif ( $this->masterRunningReadOnly( $wiki, $conn ) ) {
+			return 'The database master is running in read-only mode.';
 		}
 
 		return false;
+	}
+
+	/**
+	 * @param string $wiki Wiki ID, or false for the current wiki
+	 * @param DatabaseBase|null DB master connectionl used to avoid loops [optional]
+	 * @return bool
+	 */
+	private function masterRunningReadOnly( $wiki, DatabaseBase $conn = null ) {
+		$cache = $this->wanCache;
+		$masterServer = $this->getServerName( $this->getWriterIndex() );
+
+		return (bool)$cache->getWithSetCallback(
+			$cache->makeGlobalKey( __CLASS__, 'server-read-only', $masterServer ),
+			self::TTL_CACHE_READONLY,
+			function () use ( $wiki, $conn ) {
+				try {
+					$dbw = $conn ?: $this->getConnection( DB_MASTER, [], $wiki );
+					return (int)$dbw->serverIsReadOnly();
+				} catch ( DBError $e ) {
+					return 0;
+				}
+			},
+			[ 'pcTTL' => $cache::TTL_PROC_LONG, 'busyValue' => 0 ]
+		);
 	}
 
 	/**

includes/db/DBConnRef.php
@@ -417,6 +417,10 @@ class DBConnRef implements IDatabase {
 		return $this->__call( __FUNCTION__, func_get_args() );
 	}
 
+	public function serverIsReadOnly() {
+		return $this->__call( __FUNCTION__, func_get_args() );
+	}
+
 	public function onTransactionResolution( callable $callback ) {
 		return $this->__call( __FUNCTION__, func_get_args() );
 	}

includes/db/Database.php
@@ -2456,6 +2456,10 @@ abstract class DatabaseBase implements IDatabase {
 		return false;
 	}
 
+	public function serverIsReadOnly() {
+		return false;
+	}
+
 	final public function onTransactionResolution( callable $callback ) {
 		if ( !$this->mTrxLevel ) {
 			throw new DBUnexpectedError( $this, "No transaction is active." );

includes/db/DatabaseMysqlBase.php
@@ -885,6 +885,13 @@ abstract class DatabaseMysqlBase extends Database {
 		}
 	}
 
+	public function serverIsReadOnly() {
+		$res = $this->query( "SHOW GLOBAL VARIABLES LIKE 'read_only'", __METHOD__ );
+		$row = $this->fetchObject( $res );
+
+		return $row ? ( strtolower( $row->Value ) === 'on' ) : false;
+	}
+
 	/**
 	 * @param string $index
 	 * @return string

includes/db/IDatabase.php
@@ -1221,6 +1221,12 @@ interface IDatabase {
 	public function getMasterPos();
 
 	/**
+	 * @return bool Whether the DB is marked as read-only server-side
+	 * @since 1.28
+	 */
+	public function serverIsReadOnly();
+
+	/**
 	 * Run a callback as soon as the current transaction commits or rolls back.
 	 * An error is thrown if no transaction is pending. Queries in the function will run in
 	 * AUTO-COMMIT mode unless there are begin() calls. Callbacks must commit any transactions

commit 334962130a52109756e61a270b61bcec82b73fc1

Avoid stack overflow in LoadBalancer with CACHE_DB WAN/server cache
* Add the ability to expose key BagOStuff attributes like whether the
  emulation SQL cache is being used. Several callers use hacks to detect
  this and can be updated later.
* Fallback to a safe empty WAN cache in the CACHE_DB case. This fixes
  a regression from f4bf52e8438.
* Also add this protection to the server cache, which could break
  similarly before too.

Bug: T123829
Bug: T141804
Change-Id: I17ee26138f69e01ec1aaddb55ab27caa4d542193

includes/db/loadbalancer/LoadBalancer.php
@@ -138,8 +138,20 @@ class LoadBalancer {
 			}
 		}
 
-		$this->srvCache = ObjectCache::getLocalServerInstance();
-		$this->wanCache = ObjectCache::getMainWANInstance();
+		// Use APC/memcached style caching, but avoids loops with CACHE_DB (T141804)
+		// @TODO: inject these in via LBFactory at some point
+		$cache = ObjectCache::getLocalServerInstance();
+		if ( $cache->getQoS( $cache::ATTR_EMULATION ) > $cache::QOS_EMULATION_SQL ) {
+			$this->srvCache = $cache;
+		} else {
+			$this->srvCache = new EmptyBagOStuff();
+		}
+		$wCache = ObjectCache::getMainWANInstance();
+		if ( $wCache->getQoS( $wCache::ATTR_EMULATION ) > $wCache::QOS_EMULATION_SQL ) {
+			$this->wanCache = $wCache;
+		} else {
+			$this->wanCache = WANObjectCache::newEmpty();
+		}
 
 		if ( isset( $params['trxProfiler'] ) ) {
 			$this->trxProfiler = $params['trxProfiler'];

includes/libs/objectcache/BagOStuff.php
@@ -46,7 +46,7 @@ abstract class BagOStuff implements IExpiringStore, LoggerAwareInterface {
 	/** @var array[] Lock tracking */
 	protected $locks = [];
 
-	/** @var integer */
+	/** @var integer ERR_* class constant */
 	protected $lastError = self::ERR_NONE;
 
 	/** @var string */
@@ -69,6 +69,9 @@ abstract class BagOStuff implements IExpiringStore, LoggerAwareInterface {
 
 	/** @var bool */
 	private $dupeTrackScheduled = false;
+
+	/** @var integer[] Map of (ATTR_* class constant => QOS_* class constant) */
+	protected $attrMap = [];
 
 	/** Possible values for getLastError() */
 	const ERR_NONE = 0; // no error
@@ -733,5 +736,35 @@ abstract class BagOStuff implements IExpiringStore, LoggerAwareInterface {
 	 */
 	public function makeKey() {
 		return $this->makeKeyInternal( $this->keyspace, func_get_args() );
+	}
+
+	/**
+	 * @param integer $flag ATTR_* class constant
+	 * @return integer QOS_* class constant
+	 * @since 1.28
+	 */
+	public function getQoS( $flag ) {
+		return isset( $this->attrMap[$flag] ) ? $this->attrMap[$flag] : self::QOS_UNKNOWN;
+	}
+
+	/**
+	 * Merge the flag maps of one or more BagOStuff objects into a "lowest common denominator" map
+	 *
+	 * @param BagOStuff[] $bags
+	 * @return integer[] Resulting flag map (class ATTR_* constant => class QOS_* constant)
+	 */
+	protected function mergeFlagMaps( array $bags ) {
+		$map = [];
+		foreach ( $bags as $bag ) {
+			foreach ( $bag->attrMap as $attr => $rank ) {
+				if ( isset( $map[$attr] ) ) {
+					$map[$attr] = min( $map[$attr], $rank );
+				} else {
+					$map[$attr] = $rank;
+				}
+			}
+		}
+
+		return $map;
 	}
 }

includes/libs/objectcache/CachedBagOStuff.php
@@ -42,8 +42,10 @@ class CachedBagOStuff extends HashBagOStuff {
 	 * @param array $params Parameters for HashBagOStuff
 	 */
 	function __construct( BagOStuff $backend, $params = [] ) {
-		$this->backend = $backend;
 		parent::__construct( $params );
+
+		$this->backend = $backend;
+		$this->attrMap = $backend->attrMap;
 	}
 
 	protected function doGet( $key, $flags = 0 ) {
@@ -101,5 +103,4 @@ class CachedBagOStuff extends HashBagOStuff {
 	public function modifySimpleRelayEvent( array $event ) {
 		return $this->backend->modifySimpleRelayEvent( $event );
 	}
-
 }

includes/libs/objectcache/IExpiringStore.php
@@ -42,4 +42,11 @@ interface IExpiringStore {
 	const TTL_PROC_LONG = 30; // loose cache time that can survive slow web requests
 
 	const TTL_INDEFINITE = 0;
+
+	// Attribute and QoS constants; higher QOS values with the same prefix rank higher...
+	// Medium attributes constants related to emulation or media type
+	const ATTR_EMULATION = 1;
+	const QOS_EMULATION_SQL = 1;
+	// Generic "unknown" value that is useful for comparisons (e.g. always good enough)
+	const QOS_UNKNOWN = INF;
 }

includes/libs/objectcache/MultiWriteBagOStuff.php
@@ -83,6 +83,7 @@ class MultiWriteBagOStuff extends BagOStuff {
 				$this->caches[] = ObjectFactory::getObjectFromSpec( $cacheInfo );
 			}
 		}
+		$this->mergeFlagMaps( $this->caches );
 
 		$this->asyncWrites = (
 			isset( $params['replication'] ) &&

includes/libs/objectcache/ReplicatedBagOStuff.php
@@ -65,6 +65,7 @@ class ReplicatedBagOStuff extends BagOStuff {
 		$this->readStore = ( $params['readFactory'] instanceof BagOStuff )
 			? $params['readFactory']
 			: ObjectFactory::getObjectFromSpec( $params['readFactory'] );
+		$this->attrMap = $this->mergeFlagMaps( [ $this->readStore, $this->writeStore ] );
 	}
 
 	public function setDebug( $debug ) {

includes/libs/objectcache/WANObjectCache.php
@@ -977,7 +977,7 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 
 	/**
 	 * Get the "last error" registered; clearLastError() should be called manually
-	 * @return int ERR_* constant for the "last error" registry
+	 * @return int ERR_* class constant for the "last error" registry
 	 */
 	final public function getLastError() {
 		if ( $this->lastRelayError ) {
@@ -1017,6 +1017,15 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 	 */
 	public function clearProcessCache() {
 		$this->procCache->clear();
+	}
+
+	/**
+	 * @param integer $flag ATTR_* class constant
+	 * @return integer QOS_* class constant
+	 * @since 1.28
+	 */
+	public function getQoS( $flag ) {
+		return $this->cache->getQoS( $flag );
 	}
 
 	/**

includes/objectcache/SqlBagOStuff.php
@@ -91,6 +91,9 @@ class SqlBagOStuff extends BagOStuff {
 	 */
 	public function __construct( $params ) {
 		parent::__construct( $params );
+
+		$this->attrMap[self::ATTR_EMULATION] = self::QOS_EMULATION_SQL;
+
 		if ( isset( $params['servers'] ) ) {
 			$this->serverInfos = [];
 			$this->serverTags = [];