6962f42ea89c0c9ec9fc6e3d21188faa5adcbf7a
Gergő Tisza <gtisza@wikimedia.org>
2016-08-04 20:47:57 +0000

    AuthManager: do not rewrite PRIMARY_REQUIRED to REQUIRED
    AuthManager::getAuthenticationRequests() changes
    AuthenticationRequest::$required from REQUIRED to PRIMARY_REQUIRED
    if the request is from a primary; it made an exception when
    all primary providers returned a given request. That exception is
    not particularly useful (AuthenticationRequest::mergeFieldInfo()
    used to rely on it to determine which fields are required, but
    since I9d33bd2 that's not really needed), and knowing which request
    is from a primary is useful for other means.
    
    This changes required field semantics in a corner case: when a
    primary provider returns two required requests, the previous
    behavior was to assume that they are both required; the new one
    is to treat them as alternatives (as if they were returned by
    two different providers). So when all primary providers return
    request X, and one of them returns Y in addition, the fields of X
    will not be marked required, while previously that would have been
    the case.
    
    Instead of overcomplicating the interface for something that is
    unlikely to come up in any real use case, add a new requirement
    to PrimaryAuthenticationProvider that it should not return
    multiple required requests.
    
    Bug: T141471
    Change-Id: I1c1f44d4d6b66f77c876e3459fb97f03483db744

--- tests/phpunit/includes/auth/AuthManagerTest.php
+++ tests/phpunit/includes/auth/AuthManagerTest.php
@@ -3100,7 +3100,7 @@ class AuthManagerTest extends \MediaWikiTestCase {
 		$actual = $this->manager->getAuthenticationRequests( AuthManager::ACTION_LOGIN );
 		$expected = [
 			$rememberReq,
-			$makeReq( "primary-shared", AuthenticationRequest::REQUIRED ),
+			$makeReq( "primary-shared", AuthenticationRequest::PRIMARY_REQUIRED ),
 			$makeReq( "required", AuthenticationRequest::PRIMARY_REQUIRED ),
 			$makeReq( "required2", AuthenticationRequest::PRIMARY_REQUIRED ),
 			$makeReq( "optional", AuthenticationRequest::OPTIONAL ),
@@ -3120,7 +3120,7 @@ class AuthManagerTest extends \MediaWikiTestCase {
 		$actual = $this->manager->getAuthenticationRequests( AuthManager::ACTION_LOGIN );
 		$expected = [
 			$rememberReq,
-			$makeReq( "primary-shared", AuthenticationRequest::REQUIRED ),
+			$makeReq( "primary-shared", AuthenticationRequest::PRIMARY_REQUIRED ),
 			$makeReq( "required", AuthenticationRequest::REQUIRED ),
 			$makeReq( "optional", AuthenticationRequest::OPTIONAL ),
 			$makeReq( "foo", AuthenticationRequest::REQUIRED ),
