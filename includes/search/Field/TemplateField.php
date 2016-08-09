e2731e328e4b580a89b28dda3a0766c69b276254
aude <aude.wiki@gmail.com>
2016-08-09 13:22:09 -0400

    Extract ParserOutput search index data fields from WikiTextContentHandler [WIP]
    @todo tests
    
    (and as always, open to suggestions for better naming)
    
    Bug: T142491
    Change-Id: I69b010b893135e53fac7f16f4b927b8fbcba06d2

---
+++ includes/search/Field/TemplateField.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace MediaWiki\Search\Field;
+
+use ParserOutput;
+use SearchEngine;
+use SearchIndexField;
+use Title;
+
+/**
+ * Search index field for templates used in a page.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 1.28
+ */
+class TemplateField implements ParserOutputDataSearchField {
+
+	/**
+	 * @var SearchEngine
+	 */
+	private $engine;
+
+	/**
+	 * @var string
+	 */
+	private $fieldName;
+
+	/**
+	 * @param SearchEngine $engine
+	 * @param string $fieldName
+	 */
+	public function __construct( SearchEngine $engine, $fieldName ) {
+		$this->engine = $engine;
+		$this->fieldName = $fieldName;
+	}
+
+	/**
+	 * @return SearchIndexField
+	 */
+	public function getMapping() {
+		$field = $this->engine->makeSearchFieldMapping(
+			$this->fieldName,
+			SearchIndexField::INDEX_TYPE_KEYWORD
+		);
+
+		$field->setFlag( SearchIndexField::FLAG_CASEFOLD );
+
+		return $field;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getData( ParserOutput $parserOutput ) {
+		$templates = [];
+
+		foreach ( $parserOutput->getTemplates() as $tNS => $templatesInNS ) {
+			foreach ( array_keys( $templatesInNS ) as $tDbKey ) {
+				$templateTitle = Title::makeTitleSafe( $tNS, $tDbKey );
+				if ( $templateTitle && $templateTitle->exists() ) {
+					$templates[] = $templateTitle->getPrefixedText();
+				}
+			}
+		}
+
+		return $templates;
+	}
+
+}
