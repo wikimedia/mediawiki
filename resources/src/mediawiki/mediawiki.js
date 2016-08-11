80c142e9f54212559f0e4e91c586e3bf28ed1360
Timo Tijhof <krinklemail@gmail.com>
2016-08-10 18:39:30 -0700

    [WIP] resourceloader: Make cache-eval in mw.loader.work asynchronous
    Bug: T142129
    Change-Id: Icd39423aec35f25162d2443b1f0507f6f0c748a3

--- resources/src/mediawiki/mediawiki.js
+++ resources/src/mediawiki/mediawiki.js
@@ -1662,6 +1662,17 @@
 				}
 			}
 
+
+			/**
+			 * Execute a batch of load.php responses retreived from mw.loader.store.
+			 *
+			 * @private
+			 * @param {string[]} concatSource Array of JavaScript source code in the form of
+			 * calls to mw.loader#implement().
+			 */
+			function runSourceBatch( concatSource ) {
+			}
+
 			/* Public Members */
 			return {
 				/**
