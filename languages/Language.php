b60a467f587c996372e3737433ec6790d51ccd8c
zppix1 <support@zppixballee.com>
2016-08-10 19:53:30 -0500

    Add Localisation to the links, add the link to Localisation  in Languages/Language.php
    Change-Id: I996d09f38a007195ba9734e13f10611c4ca1242c

--- languages/Language.php
+++ languages/Language.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Internationalisation code.
+ * See https://www.mediawiki.org/wiki/Special:MyLanguage/Localisation for more information.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
