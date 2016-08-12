64333e12d7ef772f82e9e3dcf8a4367896ad4ac3
Kevin Israel <pleasestand@live.com>
2016-08-12 06:33:37 -0400

    Remove internal use of deprecated $wgRequestTime
    * In wfDebug() and wfReportTime(), use $_SERVER['REQUEST_TIME_FLOAT'] instead.
    
    * In ApiFormatBase and MWDebug, use WebRequest::getElapsedTime() instead.
    
    * In Maintenance.php, remove setting of $wgRequestTime.
    
    * In rebuildFileCache.php, update mocking to $_SERVER['REQUEST_TIME_FLOAT']
      so that we avoid re-introducing bug T24852.
    
    Change-Id: I1b647da2862f815029caa533b592ec8a05b33806

--- includes/debug/MWDebug.php
+++ includes/debug/MWDebug.php
@@ -517,7 +517,7 @@ class MWDebug {
 			return [];
 		}
 
-		global $wgVersion, $wgRequestTime;
+		global $wgVersion;
 		$request = $context->getRequest();
 
 		// HHVM's reported memory usage from memory_get_peak_usage()
@@ -540,7 +540,7 @@ class MWDebug {
 			'gitRevision' => GitInfo::headSHA1(),
 			'gitBranch' => $branch,
 			'gitViewUrl' => GitInfo::headViewUrl(),
-			'time' => microtime( true ) - $wgRequestTime,
+			'time' => $request->getElapsedTime(),
 			'log' => self::$log,
 			'debugLog' => self::$debug,
 			'queries' => self::$query,
