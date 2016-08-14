66a43ba1b72d871047e2cfec83b8a92e8ed9bbd7
Aaron Schulz <aschulz@wikimedia.org>
2016-08-13 21:26:12 -0700

    Make transaction enforcement stricter
    * Nested begin()/commit() will no-op when inside active
      DBO_TRX transactions, even if no writes happened yet.
      This avoids snapshot less from REPEATABLE-READ or SSI
      SERIALABLE (postgres). An error will be logged.
    * Also no-op when begin() happens, no transaction started,
      but DBO_TRX is set. That should not happen as it can make
      multi-DB commits less robust and can clear snapshots early.
      An error will be logged.
    * Throw an error when explicit rollback() is used and DBO_TRX
      is set. This will cause peer DBs to rollback too via the
      logic in MWExceptionHander.
    * Callers should use LBFactory methods for doing commit/rollback
      instead.
    
    Change-Id: I31b8b1b112cf9110b805a023732bce4dcff0604c

--- includes/db/DBConnRef.php
+++ includes/db/DBConnRef.php
@@ -445,7 +445,7 @@ class DBConnRef implements IDatabase {
 		return $this->__call( __FUNCTION__, func_get_args() );
 	}
 
-	public function begin( $fname = __METHOD__ ) {
+	public function begin( $fname = __METHOD__, $mode = '' ) {
 		return $this->__call( __FUNCTION__, func_get_args() );
 	}
 
