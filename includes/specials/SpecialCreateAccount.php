8027751f2af2b033f004509fadf109396c6d74df
Aaron Schulz <aschulz@wikimedia.org>
2016-08-20 15:43:57 -0700

    Make Special:CreateAccount respect ?returnto parameter
    Change-Id: I074ee184ad1fec84ef2e93eea8397d932e96e0cd

--- includes/specials/SpecialCreateAccount.php
+++ includes/specials/SpecialCreateAccount.php
@@ -23,7 +23,6 @@
 
 use MediaWiki\Auth\AuthManager;
 use MediaWiki\Logger\LoggerFactory;
-use Psr\Log\LogLevel;
 
 /**
  * Implements Special:CreateAccount
@@ -120,7 +119,12 @@ class SpecialCreateAccount extends LoginSignupSpecialPage {
 				} else {
 					$out->addWikiMsg( 'accountcreatedtext', $user->getName() );
 				}
-				$out->addReturnTo( $this->getPageTitle() );
+
+				$rt = Title::newFromText( $this->mReturnTo );
+				$out->addReturnTo(
+					( $rt && !$rt->isExternal() ) ? $rt : $this->getPageTitle(),
+					wfCgiToArray( $this->mReturnToQuery )
+				);
 				return;
 			}
 		}
@@ -130,7 +134,7 @@ class SpecialCreateAccount extends LoginSignupSpecialPage {
 		# Run any hooks; display injected HTML
 		$injected_html = '';
 		$welcome_creation_msg = 'welcomecreation-msg';
-		Hooks::run( 'UserLoginComplete', [ &$user, &$injected_html ] );
+		Hooks::run( 'UserLoginComplete', [ &$user, &$injected_html, $direct ] );
 
 		/**
 		 * Let any extensions change what message is shown.
@@ -160,7 +164,7 @@ class SpecialCreateAccount extends LoginSignupSpecialPage {
 	}
 
 	protected function logAuthResult( $success, $status = null ) {
-		LoggerFactory::getInstance( 'authmanager-stats' )->info( 'Account creation attempt', [
+		LoggerFactory::getInstance( 'authevents' )->info( 'Account creation attempt', [
 			'event' => 'accountcreation',
 			'successful' => $success,
 			'status' => $status,
