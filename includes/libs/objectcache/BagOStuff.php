082821e2005b6c57764bc8715c5129ee46a0441f
Aaron Schulz <aschulz@wikimedia.org>
2016-08-24 14:22:11 -0700

    Support masking the WRITE_SYNC latency from ChronologyProtector
    * Use OutputPage::output() as the method to mask latency, since it
      takes a good while to run. By the time it runs, SqlBagOStuff slaves
      should have caught up, so the reap call will likely not block.
    * Also make RedisBagOStuff respect SYNC_WRITE and make use of the
      masking callbacks. This checks if there are slaves and waits
      for them to reach the master offset in the replication log,
      using usleep() in between each check.
    * Store DB master positions *after* PRESEND updates too, since
      they can affect the user and are there for a reason.
    * Merge duplicate shutdown() methods from LBFactory subclasses
      to the base class, as cronProt is also set there too.
    
    Change-Id: Ib23690c302e8033610fef9a0ef451dafe8a5803e

--- includes/libs/objectcache/BagOStuff.php
+++ includes/libs/objectcache/BagOStuff.php
@@ -45,30 +45,25 @@ use Psr\Log\NullLogger;
 abstract class BagOStuff implements IExpiringStore, LoggerAwareInterface {
 	/** @var array[] Lock tracking */
 	protected $locks = [];
-
 	/** @var integer ERR_* class constant */
 	protected $lastError = self::ERR_NONE;
-
 	/** @var string */
 	protected $keyspace = 'local';
-
 	/** @var LoggerInterface */
 	protected $logger;
-
 	/** @var callback|null */
 	protected $asyncHandler;
 
 	/** @var bool */
 	private $debugMode = false;
-
 	/** @var array */
 	private $duplicateKeyLookups = [];
-
 	/** @var bool */
 	private $reportDupes = false;
-
 	/** @var bool */
 	private $dupeTrackScheduled = false;
+	/** @var callable[] */
+	private $busyCallbacks = [];
 
 	/** @var integer[] Map of (ATTR_* class constant => QOS_* class constant) */
 	protected $attrMap = [];
@@ -640,6 +635,44 @@ abstract class BagOStuff implements IExpiringStore, LoggerAwareInterface {
 	 */
 	protected function setLastError( $err ) {
 		$this->lastError = $err;
+	}
+
+	/**
+	 * Let a callback be run to avoid wasting time on special blocking calls
+	 *
+	 * The result of the callback can be fetched by reapBusyCallback() using
+	 * the request ID this returns. It will run the method if this had not done so yet.
+	 *
+	 * @param callable $workCallback
+	 * @since 1.28
+	 */
+	public function addBusyCallback( callable $workCallback ) {
+		$this->busyCallbacks[] = $workCallback;
+	}
+
+	/**
+	 * Run callbacks to due useful things instead of blocking for stuff to happen
+	 *
+	 * @param integer $maxRun Maximum number of tasks to run
+	 * @return integer Number of callbacks run this time
+	 * @since 1.28
+	 */
+	protected function runBusyCallbacks( $maxRun = INF ) {
+		$runCount = 0;
+		foreach ( $this->busyCallbacks as $i => $workCallback ) {
+			if ( $runCount >= $maxRun ) {
+				break;
+			}
+			unset( $this->busyCallbacks[$i] ); // consume
+			try {
+				$workCallback();
+				++$runCount;
+			} finally {
+				// rethrow
+			}
+		}
+
+		return $runCount;
 	}
 
 	/**
