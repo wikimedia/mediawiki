20400cb21fd566c1cea4553cd040171096216654
Aaron Schulz <aschulz@wikimedia.org>
2016-08-24 17:19:58 -0700

    Move HTTP 304 check from performRequest to ViewAction
    * Follow-up to 8b141886edebc
    * The method is now called after the setCdnMaxage() call
      in performAction.
    * Allow any CDN urls for the title now, check $wgDebugToolbar,
      and allows caching redirects. The multi-step redirect case does
      not cache however, for simplicity.
    * Removed now-unused code in Article that calculated $timestamp.
    
    Change-Id: Ic4f4e3a79d7d386c2f15ca5b11dddf5c57ff9e9f

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -286,16 +286,6 @@ class MediaWiki {
 				// may still be a wikipage redirect to another article or URL.
 				$article = $this->initializeArticle();
 				if ( is_object( $article ) ) {
-					$url = $request->getFullRequestURL(); // requested URL
-					if (
-						$request->getMethod() === 'GET' &&
-						$url === $article->getTitle()->getCanonicalURL() &&
-						$article->checkTouched() &&
-						$output->checkLastModified( $article->getTouched() )
-					) {
-						wfDebug( __METHOD__ . ": done 304\n" );
-						return;
-					}
 					$this->performAction( $article, $requestTitle );
 				} elseif ( is_string( $article ) ) {
 					$output->redirect( $article );
--- includes/actions/ViewAction.php
+++ includes/actions/ViewAction.php
@@ -41,6 +41,30 @@ class ViewAction extends FormlessAction {
 	}
 
 	public function show() {
+		$config = $this->context->getConfig();
+
+		if (
+			$config->get( 'DebugToolbar' ) == false && // don't let this get stuck on pages
+			$this->page->checkTouched() // page exists and is not a redirect
+		) {
+			// Include any redirect in the last-modified calculation
+			$redirFromTitle = $this->page->getRedirectedFrom();
+			if ( !$redirFromTitle ) {
+				$touched = $this->page->getTouched();
+			} elseif ( $config->get( 'MaxRedirects' ) <= 1 ) {
+				$touched = max( $this->page->getTouched(), $redirFromTitle->getTouched() );
+			} else {
+				// Don't bother following the chain and getting the max mtime
+				$touched = null;
+			}
+
+			// Send HTTP 304 if the IMS matches or otherwise set expiry/last-modified headers
+			if ( $touched && $this->getOutput()->checkLastModified( $touched ) ) {
+				wfDebug( __METHOD__ . ": done 304\n" );
+				return;
+			}
+		}
+
 		$this->page->view();
 	}
 }
