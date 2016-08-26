72cecb4bc61abb366143769df135a2798feb5532
Kunal Mehta <legoktm@member.fsf.org>
2016-08-25 18:28:22 -0700

    Improve @covers tags for ComposerJson and ComposerLock tests
    Use a class-level @covers tag so things like the constructor are
    included in test coverage.
    
    Change-Id: Ic87603c76e8affc31f497962375c39fb2726e604

--- tests/phpunit/includes/libs/composer/ComposerLockTest.php
+++ tests/phpunit/includes/libs/composer/ComposerLockTest.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * @covers ComposerLock
+ */
 class ComposerLockTest extends MediaWikiTestCase {
 
 	private $lock;
@@ -10,17 +13,11 @@ class ComposerLockTest extends MediaWikiTestCase {
 		$this->lock = "$IP/tests/phpunit/data/composer/composer.lock";
 	}
 
-	/**
-	 * @covers ComposerLock::getHash
-	 */
 	public function testGetHash() {
 		$lock = new ComposerLock( $this->lock );
 		$this->assertEquals( 'a3bb80b0ac4c4a31e52574d48c032923', $lock->getHash() );
 	}
 
-	/**
-	 * @covers ComposerLock::getInstalledDependencies
-	 */
 	public function testGetInstalledDependencies() {
 		$lock = new ComposerLock( $this->lock );
 		$this->assertArrayEquals( [
