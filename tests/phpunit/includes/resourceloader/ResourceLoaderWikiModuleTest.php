248a33e2ba8d5bcbd06832f93fdad751776823fd
Aaron Schulz <aschulz@wikimedia.org>
2016-09-02 01:28:23 -0700

    Add caching to ResourceLoaderWikiModule::preloadTitleInfo()
    This is one of the top three DB queries showing up in xenon
    reverse flamegraph profiling.
    
    It works via a per-wiki check key that is bumped whenever
    someone changes a .js or .css page on that wiki.
    
    Change-Id: I73f419558864ba3403b4601a098f6aaf84a3e7c1

--- tests/phpunit/includes/resourceloader/ResourceLoaderWikiModuleTest.php
+++ tests/phpunit/includes/resourceloader/ResourceLoaderWikiModuleTest.php
@@ -199,6 +199,12 @@ class ResourceLoaderWikiModuleTest extends ResourceLoaderTestCase {
 		$rl->register( 'testmodule', $module );
 		$context = new ResourceLoaderContext( $rl, new FauxRequest() );
 
+		TestResourceLoaderWikiModule::invalidateModuleCache(
+			Title::newFromText( 'MediaWiki:Common.css' ),
+			null,
+			null,
+			wfWikiID()
+		);
 		TestResourceLoaderWikiModule::preloadTitleInfo(
 			$context,
 			wfGetDB( DB_REPLICA ),
