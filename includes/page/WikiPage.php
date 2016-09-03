46f71768477d8a85c30823543fae59e00e84a269
Aaron Schulz <aschulz@wikimedia.org>
2016-09-02 21:43:16 -0700

    objectcache: add WANObjectCacheReaper for assuring purges
    * This fixes keys based on some sort of change log.
      Updates are wrapped in a mutex and keep track of the
      last known good position.
    * Make WANObjectReapUpdate class that cleans up title
      related keys using the recentchanges table. This triggers
      as a deferred updates on RC view.
    
    Change-Id: I7f14b9ca2533032147e62b1a3cc004a23da86579

--- includes/page/WikiPage.php
+++ includes/page/WikiPage.php
@@ -3716,4 +3716,15 @@ class WikiPage implements Page, IDBAccessObject {
 	public function getSourceURL() {
 		return $this->getTitle()->getCanonicalURL();
 	}
+
+	/*
+	 * @param WANObjectCache $cache
+	 * @return string[]
+	 * @since 1.28
+	 */
+	public function getMutableCacheKeys( WANObjectCache $cache ) {
+		$linkCache = MediaWikiServices::getInstance()->getLinkCache();
+
+		return $linkCache->getMutableCacheKeys( $cache, $this->getTitle()->getTitleValue() );
+	}
 }
