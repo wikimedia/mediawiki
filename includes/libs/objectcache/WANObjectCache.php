a2dcaed550e514d77491701f8a154d6e89712647
Aaron Schulz <aschulz@wikimedia.org>
2016-09-02 21:43:16 -0700

    [WIP] objectcache: add WANObjectCacheRepear for replaying purges
    This triggers as a deferred updates on RC view.
    
    Change-Id: I7f14b9ca2533032147e62b1a3cc004a23da86579

--- includes/libs/objectcache/WANObjectCache.php
+++ includes/libs/objectcache/WANObjectCache.php
@@ -970,6 +970,38 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 	}
 
 	/**
+	 * Locally destroy any value or "check" key $key if stale based on $purgeTimestamp
+	 *
+	 * @param string $key Cache key
+	 * @param int $purgeTimestamp UNIX timestamp of purge
+	 * @return bool Success
+	 * @since 1.28
+	 */
+	public function reap( $key, $purgeTimestamp ) {
+		$ok = true;
+
+		$minAsOf = $purgeTimestamp + WANObjectCache::HOLDOFF_TTL;
+		$wrapped = $this->cache->get( self::VALUE_KEY_PREFIX . $key );
+		if ( is_array( $wrapped ) && $wrapped[self::FLD_TIME] < $minAsOf ) {
+			$this->logger->warning( "Reaping bad value key '$key'." );
+			$ok = $this->cache->changeTTL( self::VALUE_KEY_PREFIX . $key, 1 ) && $ok;
+		}
+
+		$rawValue = $this->cache->get( self::TIME_KEY_PREFIX . $key );
+		$purgeValue = $this->parsePurgeValue( $rawValue );
+		if ( $purgeValue && $purgeValue[self::FLD_TIME] < $purgeTimestamp ) {
+			$this->logger->warning( "Reaping bad value key '$key'." );
+			$ok = $this->cache->changeTTL( self::TIME_KEY_PREFIX . $key, 1 ) && $ok;
+		}
+
+		if ( !$ok ) {
+			$this->logger->error( "Could not complete reap of key '$key'." );
+		}
+
+		return $ok;
+	}
+
+	/**
 	 * @see BagOStuff::makeKey()
 	 * @param string ... Key component
 	 * @return string
