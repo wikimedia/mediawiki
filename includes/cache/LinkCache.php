f0a9ff043e447f5d2c70609e17523c110ca3cb5c
Aaron Schulz <aschulz@wikimedia.org>
2016-09-02 21:43:16 -0700

    objectcache: add WANObjectCacheReaper for assuring purges
    * This fixes keys based on some sort of change log.
      Updates are wrapped in a mutex and keep track of the
      last known good position.
    * Make WANObjectReapUpdate class that cleans up title
      related keys using the recentchanges table. This triggers
      as a deferred updates on RC view.
    
    Change-Id: I7f14b9ca2533032147e62b1a3cc004a23da86579

--- includes/cache/LinkCache.php
+++ includes/cache/LinkCache.php
@@ -279,6 +279,20 @@ class LinkCache {
 		return $id;
 	}
 
+	/**
+	 * @param WANObjectCache $cache
+	 * @param TitleValue $t
+	 * @return string[]
+	 * @since 1.28
+	 */
+	public function getMutableCacheKeys( WANObjectCache $cache, TitleValue $t ) {
+		if ( $t->inNamespace( NS_FILE ) || $t->inNamespace( NS_TEMPLATE ) ) {
+			return [ $cache->makeKey( 'page', $t->getNamespace(), sha1( $t->getDBkey() ) ) ];
+		}
+
+		return [];
+	}
+
 	private function isCacheable( LinkTarget $title ) {
 		return ( $title->inNamespace( NS_TEMPLATE ) || $title->inNamespace( NS_FILE ) );
 	}
