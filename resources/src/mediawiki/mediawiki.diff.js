458e750da4e6ff585bb319c4d34fcffaf282841f
Fomafix <fomafix@googlemail.com>
2016-09-06 18:14:53 +0200

    Move hook 'wikipage.diff' from startup.js to mediawiki.diff.js
    The module 'mediawiki.diff' is loaded when there is a diff on the page.
    The hook 'wikipage.diff' can fired from that module, too.
    
    Change-Id: I92d6a8fbced64fd44edc394de0969e910e1ddd34

--- resources/src/mediawiki/mediawiki.diff.js
+++ resources/src/mediawiki/mediawiki.diff.js
@@ -3,6 +3,19 @@
 
 	$( function () {
 		var $diffTable = $( 'table.diff[data-mw="interface"]' );
+
+		/**
+		 * Fired when the diff is added to a page containing a diff
+		 *
+		 * Similar to the {@link mw.hook#event-wikipage_content wikipage.content hook}
+		 * $diff may still be detached when the hook is fired.
+		 *
+		 * @event wikipage_diff
+		 * @member mw.hook
+		 * @param {jQuery} $diff The root element of the MediaWiki diff (`table.diff`).
+		 */
+		mw.hook( 'wikipage.diff' ).fire( $diffTable.eq( 0 ) );
+
 		/**
 		 * Fired when the content of the diff displayed on the diff page changes
 		 *
--- resources/src/mediawiki/page/startup.js
+++ resources/src/mediawiki/page/startup.js
@@ -22,21 +22,6 @@
 		 *   root)
 		 */
 		mw.hook( 'wikipage.content' ).fire( $( '#mw-content-text' ) );
-
-		var $diff = $( 'table.diff[data-mw="interface"]' );
-		if ( $diff.length ) {
-			/**
-			 * Fired when the diff is added to a page containing a diff
-			 *
-			 * Similar to the {@link mw.hook#event-wikipage_content wikipage.content hook}
-			 * $diff may still be detached when the hook is fired.
-			 *
-			 * @event wikipage_diff
-			 * @member mw.hook
-			 * @param {jQuery} $diff The root element of the MediaWiki diff (`table.diff`).
-			 */
-			mw.hook( 'wikipage.diff' ).fire( $diff.eq( 0 ) );
-		}
 	} );
 
 }( mediaWiki, jQuery ) );
