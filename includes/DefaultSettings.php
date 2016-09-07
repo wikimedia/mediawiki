b43ac35351e70f3b6429cc527509ac33f52c6404
Kunal Mehta <legoktm@member.fsf.org>
2016-09-07 11:00:42 -0700

    Grant 'editcontentmodel' permission to 'user' group
    Bug: T85847
    Change-Id: Iad834e757ac6f37ad64ac9e93afe93160aabc842

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5077,6 +5077,7 @@ $wgGroupPermissions['user']['purge'] = true;
 $wgGroupPermissions['user']['sendemail'] = true;
 $wgGroupPermissions['user']['applychangetags'] = true;
 $wgGroupPermissions['user']['changetags'] = true;
+$wgGroupPermissions['user']['editcontentmodel'] = true;
 
 // Implicit group for accounts that pass $wgAutoConfirmAge
 $wgGroupPermissions['autoconfirmed']['autoconfirmed'] = true;
@@ -5107,7 +5108,6 @@ $wgGroupPermissions['sysop']['undelete'] = true;
 $wgGroupPermissions['sysop']['editinterface'] = true;
 $wgGroupPermissions['sysop']['editusercss'] = true;
 $wgGroupPermissions['sysop']['edituserjs'] = true;
-$wgGroupPermissions['sysop']['editcontentmodel'] = true;
 $wgGroupPermissions['sysop']['import'] = true;
 $wgGroupPermissions['sysop']['importupload'] = true;
 $wgGroupPermissions['sysop']['move'] = true;
