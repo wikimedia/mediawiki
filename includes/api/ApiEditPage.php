99485235b0be6a55337c3d26fc4b4c594a5c5e97
Brian Wolff <bawolff+wn@gmail.com>
2016-09-08 23:43:36 +0000

    API: When undoing an edit, allow overriding content model.
    This brings the API in line with web UI changes from Ic528f65d.
    
    Bug: T145044
    Change-Id: Ib97eef38d228c4da4b062ee96ddbbbb926ee665b

--- includes/api/ApiEditPage.php
+++ includes/api/ApiEditPage.php
@@ -97,6 +97,7 @@ class ApiEditPage extends ApiBase {
 		} else {
 			$contentHandler = ContentHandler::getForModelID( $params['contentmodel'] );
 		}
+		$contentModel = $contentHandler->getModelID();
 
 		$name = $titleObj->getPrefixedDBkey();
 		$model = $contentHandler->getModelID();
@@ -111,10 +112,10 @@ class ApiEditPage extends ApiBase {
 		}
 
 		if ( !isset( $params['contentformat'] ) || $params['contentformat'] == '' ) {
-			$params['contentformat'] = $contentHandler->getDefaultFormat();
+			$contentFormat = $contentHandler->getDefaultFormat();
+		} else {
+			$contentFormat = $params['contentformat'];
 		}
-
-		$contentFormat = $params['contentformat'];
 
 		if ( !$contentHandler->isSupportedFormat( $contentFormat ) ) {
 
@@ -265,9 +266,21 @@ class ApiEditPage extends ApiBase {
 			if ( !$newContent ) {
 				$this->dieUsageMsg( 'undo-failure' );
 			}
-
-			$params['text'] = $newContent->serialize( $params['contentformat'] );
-
+			if ( empty( $params['contentmodel'] )
+				&& empty( $params['contentformat'] )
+			) {
+				// If we are reverting content model, the new content model
+				// might not support the current serialization format, in
+				// which case go back to the old serialization format,
+				// but only if the user hasn't specified a format/model
+				// parameter.
+				if ( !$newContent->isSupportedFormat( $contentFormat ) ) {
+					$contentFormat = $undoafterRev->getContentFormat();
+				}
+				// Override content model with model of undid revision.
+				$contentModel = $newContent->getModel();
+			}
+			$params['text'] = $newContent->serialize( $contentFormat );
 			// If no summary was given and we only undid one rev,
 			// use an autosummary
 			if ( is_null( $params['summary'] ) &&
@@ -288,7 +301,7 @@ class ApiEditPage extends ApiBase {
 		$requestArray = [
 			'wpTextbox1' => $params['text'],
 			'format' => $contentFormat,
-			'model' => $contentHandler->getModelID(),
+			'model' => $contentModel,
 			'wpEditToken' => $params['token'],
 			'wpIgnoreBlankSummary' => true,
 			'wpIgnoreBlankArticle' => true,
