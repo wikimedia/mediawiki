57dfe98263cc7afcde89fba01d4f0468c3f2c7fe
Paladox <thomasmulhall410@yahoo.com>
2016-09-11 18:26:36 +0100

    Remove jquery.arrowSteps module
    It was unused by any other extension
    https://github.com/search?q=user%3Awikimedia+jquery.arrowSteps&ref=searchresults&type=Code&utf8=%E2%9C%93
    
    and only used for UploadWizard so it was merged into there with
    I054674c7025ad37a8592ca82ce7ce0efd41d393f
    
    Depends-On: I054674c7025ad37a8592ca82ce7ce0efd41d393f
    
    Bug: T144974
    Change-Id: Id9b6af1f1774bc33ceb024126030eefdc43beba0

--- resources/Resources.php
+++ resources/Resources.php
@@ -165,11 +165,6 @@ return [
 		],
 		'scripts' => 'resources/lib/jquery/jquery.appear.js',
 	],
-	'jquery.arrowSteps' => [
-		'scripts' => 'resources/src/jquery/jquery.arrowSteps.js',
-		'styles' => 'resources/src/jquery/jquery.arrowSteps.css',
-		'targets' => [ 'desktop', 'mobile' ],
-	],
 	'jquery.async' => [
 		'scripts' => 'resources/lib/jquery/jquery.async.js',
 	],
