9fe5e0e696055a653d492f7847399958d1424b0f
Aaron Schulz <aschulz@wikimedia.org>
2016-09-15 10:07:47 -0700

    Dependency inject $wgSharedTables into DatabaseBase
    Change-Id: I3e659b56c587d2059fc3d3f922bc38dec7685df4

--- includes/db/DatabaseSqlite.php
+++ includes/db/DatabaseSqlite.php
@@ -59,7 +59,7 @@ class DatabaseSqlite extends Database {
 	 * @param array $p
 	 */
 	function __construct( array $p ) {
-		global $wgSharedDB, $wgSQLiteDataDir;
+		global $wgSQLiteDataDir;
 
 		$this->dbDir = isset( $p['dbDirectory'] ) ? $p['dbDirectory'] : $wgSQLiteDataDir;
 
@@ -76,8 +76,13 @@ class DatabaseSqlite extends Database {
 			// Super doesn't open when $user is false, but we can work with $dbName
 			if ( $p['dbname'] && !$this->isOpen() ) {
 				if ( $this->open( $p['host'], $p['user'], $p['password'], $p['dbname'] ) ) {
-					if ( $wgSharedDB ) {
-						$this->attachDatabase( $wgSharedDB );
+					$done = [];
+					foreach ( $this->tableAliases as $params ) {
+						if ( isset( $done[$params['dbname']] ) ) {
+							continue;
+						}
+						$this->attachDatabase( $params['dbname'] );
+						$done[$params['dbname']] = 1;
 					}
 				}
 			}
