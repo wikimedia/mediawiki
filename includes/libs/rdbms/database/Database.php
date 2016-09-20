dd36a0b6da6384e98c2563af2212b6ab74f6efc8
Aaron Schulz <aschulz@wikimedia.org>
2016-09-20 08:15:39 -0700

    Set an initial dummy domain in Database::__construct()
    This avoids errors when LoadBalancer calls getWikiId() in
    reallyOpenConnection() after a failed connection.
    
    Change-Id: Idc3d93ae39be09f2da277df57cdc5a564cb3408d

--- includes/libs/rdbms/database/Database.php
+++ includes/libs/rdbms/database/Database.php
@@ -281,6 +281,9 @@ abstract class Database implements IDatabase, LoggerAwareInterface {
 			? $params['queryLogger']
 			: new \Psr\Log\NullLogger();
 
+		// Set initial dummy domain until open() sets the final DB/prefix
+		$this->currentDomain = DatabaseDomain::newUnspecified();
+
 		if ( $user ) {
 			$this->open( $server, $user, $password, $dbName );
 		} elseif ( $this->requiresDatabaseUser() ) {
