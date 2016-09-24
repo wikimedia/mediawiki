wikimedia/mediawiki

commit 86ce9b388a363e533d1fa985959c71d636d81706

Fix IDEA warnings in CloneDatabase
Change-Id: I45231237dd49dfdd8158a68d8f63d769b762f19c

includes/db/CloneDatabase.php
@@ -23,6 +23,7 @@
  * @file
  * @ingroup Database
  */
+use MediaWiki\MediaWikiServices;
 
 class CloneDatabase {
 	/** @var string Table prefix for cloning */
@@ -40,16 +41,19 @@ class CloneDatabase {
 	/** @var bool Whether to use temporary tables or not */
 	private $useTemporaryTables = true;
 
+	/** @var Database */
+	private $db;
+
 	/**
 	 * Constructor
 	 *
-	 * @param IDatabase $db A database subclass
+	 * @param Database $db A database subclass
 	 * @param array $tablesToClone An array of tables to clone, unprefixed
 	 * @param string $newTablePrefix Prefix to assign to the tables
 	 * @param string $oldTablePrefix Prefix on current tables, if not $wgDBprefix
 	 * @param bool $dropCurrentTables
 	 */
-	public function __construct( IDatabase $db, array $tablesToClone,
+	public function __construct( Database $db, array $tablesToClone,
 		$newTablePrefix, $oldTablePrefix = '', $dropCurrentTables = true
 	) {
 		$this->db = $db;
@@ -130,7 +134,7 @@ class CloneDatabase {
 	public static function changePrefix( $prefix ) {
 		global $wgDBprefix;
 
-		$lbFactory = wfGetLBFactory();
+		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
 		$lbFactory->setDomainPrefix( $prefix );
 		$wgDBprefix = $prefix;
 	}

commit 43569e9e1019729c559af41e4a93180de0231f33

Fix unit tests with External Store
This may be caused by 86ce9b388a363e533d1fa985959c71d636d81706 .

Without this, I get:

Argument 1 passed to MediaWikiTestCase::setupDatabaseWithTestPrefix()
must be an instance of DatabaseBase, instance of DBConnRef given,
called in /vagrant/mediawiki/tests/phpunit/MediaWikiTestCase.php on
line 1173 and defined in
/vagrant/mediawiki/tests/phpunit/MediaWikiTestCase.php on line 1077

Change-Id: I4b2c611314eff941ae46fdb4a2d69b9315c6933d

includes/db/CloneDatabase.php
@@ -41,19 +41,19 @@ class CloneDatabase {
 	/** @var bool Whether to use temporary tables or not */
 	private $useTemporaryTables = true;
 
-	/** @var Database */
+	/** @var IDatabase */
 	private $db;
 
 	/**
 	 * Constructor
 	 *
-	 * @param Database $db A database subclass
+	 * @param IDatabase $db A database implementation
 	 * @param array $tablesToClone An array of tables to clone, unprefixed
 	 * @param string $newTablePrefix Prefix to assign to the tables
 	 * @param string $oldTablePrefix Prefix on current tables, if not $wgDBprefix
 	 * @param bool $dropCurrentTables
 	 */
-	public function __construct( Database $db, array $tablesToClone,
+	public function __construct( IDatabase $db, array $tablesToClone,
 		$newTablePrefix, $oldTablePrefix = '', $dropCurrentTables = true
 	) {
 		$this->db = $db;

tests/phpunit/MediaWikiTestCase.php
@@ -1070,11 +1070,11 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 	 * Clones all tables in the given database (whatever database that connection has
 	 * open), to versions with the test prefix.
 	 *
-	 * @param DatabaseBase $db Database to use
+	 * @param IDatabase $db Database to use
 	 * @param string $prefix Prefix to use for test tables
 	 * @return bool True if tables were cloned, false if only the prefix was changed
 	 */
-	protected static function setupDatabaseWithTestPrefix( DatabaseBase $db, $prefix ) {
+	protected static function setupDatabaseWithTestPrefix( IDatabase $db, $prefix ) {
 		$tablesCloned = self::listTables( $db );
 		$dbClone = new CloneDatabase( $db, $tablesCloned, $prefix );
 		$dbClone->useTemporaryTables( self::$useTemporaryTables );
@@ -1123,12 +1123,12 @@ abstract class MediaWikiTestCase extends PHPUnit_Framework_TestCase {
 	 * @note this method only works when first called. Subsequent calls have no effect,
 	 * even if using different parameters.
 	 *
-	 * @param DatabaseBase $db The database connection
+	 * @param IDatabase $db The database connection
 	 * @param string $prefix The prefix to use for the new table set (aka schema).
 	 *
 	 * @throws MWException If the database table prefix is already $prefix
 	 */
-	public static function setupTestDB( DatabaseBase $db, $prefix ) {
+	public static function setupTestDB( IDatabase $db, $prefix ) {
 		if ( self::$dbSetup ) {
 			return;
 		}