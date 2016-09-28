8e5d90f13f5c6810cdc52df5f227c9ab0a20bd09
Kunal Mehta <legoktm@member.fsf.org>
2016-09-28 02:17:49 -0700

    Fix display of categories
    Fixes regression from 1885a1ac462c08, which accidentally removed
    page_namespace and page_title as select fields.
    
    Bug: T146873
    Change-Id: I19cff0cf5bc015da7311196749a9201a8e458b49

--- includes/CategoryViewer.php
+++ includes/CategoryViewer.php
@@ -321,6 +321,8 @@ class CategoryViewer extends ContextSource {
 				array_merge(
 					LinkCache::getSelectFields(),
 					[
+						'page_namespace',
+						'page_title',
 						'cl_sortkey',
 						'cat_id',
 						'cat_title',
