01254c2f33b86ab66a88c6b55de6a88ea44bc88e
Timo Tijhof <krinklemail@gmail.com>
2016-09-28 21:37:20 +0100

    resourceloader: Disable ChronologyProtector to speed up wfGetLB()
    For load.php requests, Xenon in Wikimedia production measures that half
    the time for wfGetLB() is spent in ChronologyProtector::initPositions,
    and its query to Memcached.
    
    Change-Id: I24f90897e56fd679c6bd1acf764722ed58e51c67

--- load.php
+++ load.php
@@ -35,6 +35,12 @@ if ( !$wgRequest->checkUrlExtension() ) {
 	return;
 }
 
+// Don't initialise ChronologyProtector from object cache, and
+// don't wait for unrelated MediaWiki writes when querying ResourceLoader.
+MediaWikiServices::getInstance()->getDBLoadBalancerFactory()->setRequestInfo( [
+	'ChronologyProtection' => 'false',
+] );
+
 // Set up ResourceLoader
 $resourceLoader = new ResourceLoader(
 	ConfigFactory::getDefaultInstance()->makeConfig( 'main' ),
