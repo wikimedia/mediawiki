wikimedia/mediawiki

commit 5819c37b3c1300bf195b51c6f847d63c37c76531

More mediawiki.js cleanup - Convert prototype object modifications into object literal. Saves bandwidth (less characters) and speeds up execution (no need to access 2 level deep object member repetitively). Local testing (Chrome Web Inspector) shows 14.80KB to 14.67KB (non-cached, debug=false), and execution time on cached request 32ms to 25ms. - Un-indent 1 tab for d.setTime (Follows up r92964) - Whitespace consistency - Move var statements to top of loader's addScript
Follows up: r92933

resources/mediawiki/mediawiki.js
@@ -29,79 +29,81 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 		return this;
 	}
 
-	/**
-	 * Get the value of one or multiple a keys.
-	 *
-	 * If called with no arguments, all values will be returned.
-	 *
-	 * @param selection mixed String key or array of keys to get values for.
-	 * @param fallback mixed Value to use in case key(s) do not exist (optional).
-	 * @return mixed If selection was a string returns the value or null,
-	 *  If selection was an array, returns an object of key/values (value is null if not found),
-	 *  If selection was not passed or invalid, will return the 'values' object member (be careful as
-	 *  objects are always passed by reference in JavaScript!).
-	 * @return Values as a string or object, null if invalid/inexistant.
-	 */
-	Map.prototype.get = function( selection, fallback ) {
-		if ( $.isArray( selection ) ) {
-			selection = $.makeArray( selection );
-			var results = {};
-			for ( var i = 0; i < selection.length; i++ ) {
-				results[selection[i]] = this.get( selection[i], fallback );
-			}
-			return results;
-		} else if ( typeof selection === 'string' ) {
-			if ( this.values[selection] === undefined ) {
-				if ( fallback !== undefined ) {
-					return fallback;
+	Map.prototype = {
+		/**
+		 * Get the value of one or multiple a keys.
+		 *
+		 * If called with no arguments, all values will be returned.
+		 *
+		 * @param selection mixed String key or array of keys to get values for.
+		 * @param fallback mixed Value to use in case key(s) do not exist (optional).
+		 * @return mixed If selection was a string returns the value or null,
+		 *  If selection was an array, returns an object of key/values (value is null if not found),
+		 *  If selection was not passed or invalid, will return the 'values' object member (be careful as
+		 *  objects are always passed by reference in JavaScript!).
+		 * @return Values as a string or object, null if invalid/inexistant.
+		 */
+		get: function( selection, fallback ) {
+			if ( $.isArray( selection ) ) {
+				selection = $.makeArray( selection );
+				var results = {};
+				for ( var i = 0; i < selection.length; i++ ) {
+					results[selection[i]] = this.get( selection[i], fallback );
 				}
-				return null;
-			}
-			return this.values[selection];
-		}
-		if ( selection === undefined ) {
-			return this.values;
-		} else {
-			return null; // invalid selection key
-		}
-	};
-
-	/**
-	 * Sets one or multiple key/value pairs.
-	 *
-	 * @param selection mixed String key or array of keys to set values for.
-	 * @param value mixed Value to set (optional, only in use when key is a string)
-	 * @return bool This returns true on success, false on failure.
-	 */
-	Map.prototype.set = function( selection, value ) {
-		if ( $.isPlainObject( selection ) ) {
-			for ( var s in selection ) {
-				this.values[s] = selection[s];
-			}
-			return true;
-		} else if ( typeof selection === 'string' && value !== undefined ) {
-			this.values[selection] = value;
-			return true;
-		}
-		return false;
-	};
-
-	/**
-	 * Checks if one or multiple keys exist.
-	 *
-	 * @param selection mixed String key or array of keys to check
-	 * @return boolean Existence of key(s)
-	 */
-	Map.prototype.exists = function( selection ) {
-		if ( typeof selection === 'object' ) {
-			for ( var s = 0; s < selection.length; s++ ) {
-				if ( !( selection[s] in this.values ) ) {
-					return false;
+				return results;
+			} else if ( typeof selection === 'string' ) {
+				if ( this.values[selection] === undefined ) {
+					if ( fallback !== undefined ) {
+						return fallback;
+					}
+					return null;
 				}
+				return this.values[selection];
 			}
-			return true;
-		} else {
-			return selection in this.values;
+			if ( selection === undefined ) {
+				return this.values;
+			} else {
+				return null; // invalid selection key
+			}
+		},
+
+		/**
+		 * Sets one or multiple key/value pairs.
+		 *
+		 * @param selection mixed String key or array of keys to set values for.
+		 * @param value mixed Value to set (optional, only in use when key is a string)
+		 * @return bool This returns true on success, false on failure.
+		 */
+		set: function( selection, value ) {
+			if ( $.isPlainObject( selection ) ) {
+				for ( var s in selection ) {
+					this.values[s] = selection[s];
+				}
+				return true;
+			} else if ( typeof selection === 'string' && value !== undefined ) {
+				this.values[selection] = value;
+				return true;
+			}
+			return false;
+		},
+
+		/**
+		 * Checks if one or multiple keys exist.
+		 *
+		 * @param selection mixed String key or array of keys to check
+		 * @return boolean Existence of key(s)
+		 */
+		exists: function( selection ) {
+			if ( typeof selection === 'object' ) {
+				for ( var s = 0; s < selection.length; s++ ) {
+					if ( !( selection[s] in this.values ) ) {
+						return false;
+					}
+				}
+				return true;
+			} else {
+				return selection in this.values;
+			}
 		}
 	};
 
@@ -124,91 +126,93 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 		return this;
 	}
 
-	/**
-	 * Appends (does not replace) parameters for replacement to the .parameters property.
-	 *
-	 * @param parameters Array
-	 * @return Message
-	 */
-	Message.prototype.params = function( parameters ) {
-		for ( var i = 0; i < parameters.length; i++ ) {
-			this.parameters.push( parameters[i] );
-		}
-		return this;
-	};
+	Message.prototype = {
+		/**
+		 * Appends (does not replace) parameters for replacement to the .parameters property.
+		 *
+		 * @param parameters Array
+		 * @return Message
+		 */
+		params: function( parameters ) {
+			for ( var i = 0; i < parameters.length; i++ ) {
+				this.parameters.push( parameters[i] );
+			}
+			return this;
+		},
 
-	/**
-	 * Converts message object to it's string form based on the state of format.
-	 *
-	 * @return string Message as a string in the current form or <key> if key does not exist.
-	 */
-	Message.prototype.toString = function() {
-		if ( !this.map.exists( this.key ) ) {
-			// Return <key> if key does not exist
-			return '<' + this.key + '>';
-		}
-		var	text = this.map.get( this.key ),
-			parameters = this.parameters;
+		/**
+		 * Converts message object to it's string form based on the state of format.
+		 *
+		 * @return string Message as a string in the current form or <key> if key does not exist.
+		 */
+		toString: function() {
+			if ( !this.map.exists( this.key ) ) {
+				// Return <key> if key does not exist
+				return '<' + this.key + '>';
+			}
+			var	text = this.map.get( this.key ),
+				parameters = this.parameters;
 
-		text = text.replace( /\$(\d+)/g, function( string, match ) {
-			var index = parseInt( match, 10 ) - 1;
-			return index in parameters ? parameters[index] : '$' + match;
-		} );
+			text = text.replace( /\$(\d+)/g, function( string, match ) {
+				var index = parseInt( match, 10 ) - 1;
+				return index in parameters ? parameters[index] : '$' + match;
+			} );
 
-		if ( this.format === 'plain' ) {
+			if ( this.format === 'plain' ) {
+				return text;
+			}
+			if ( this.format === 'escaped' ) {
+				// According to Message.php this needs {{-transformation, which is
+				// still todo
+				return mw.html.escape( text );
+			}
+
+			/* This should be fixed up when we have a parser
+			if ( this.format === 'parse' && 'language' in mw ) {
+				text = mw.language.parse( text );
+			}
+			*/
 			return text;
+		},
+
+		/**
+		 * Changes format to parse and converts message to string
+		 *
+		 * @return {string} String form of parsed message
+		 */
+		parse: function() {
+			this.format = 'parse';
+			return this.toString();
+		},
+
+		/**
+		 * Changes format to plain and converts message to string
+		 *
+		 * @return {string} String form of plain message
+		 */
+		plain: function() {
+			this.format = 'plain';
+			return this.toString();
+		},
+
+		/**
+		 * Changes the format to html escaped and converts message to string
+		 *
+		 * @return {string} String form of html escaped message
+		 */
+		escaped: function() {
+			this.format = 'escaped';
+			return this.toString();
+		},
+
+		/**
+		 * Checks if message exists
+		 *
+		 * @return {string} String form of parsed message
+		 */
+		exists: function() {
+			return this.map.exists( this.key );
 		}
-		if ( this.format === 'escaped' ) {
-			// According to Message.php this needs {{-transformation, which is
-			// still todo
-			return mw.html.escape( text );
-		}
-
-		/* This should be fixed up when we have a parser
-		if ( this.format === 'parse' && 'language' in mw ) {
-			text = mw.language.parse( text );
-		}
-		*/
-		return text;
-	};
-
-	/**
-	 * Changes format to parse and converts message to string
-	 *
-	 * @return {string} String form of parsed message
-	 */
-	Message.prototype.parse = function() {
-		this.format = 'parse';
-		return this.toString();
-	};
-
-	/**
-	 * Changes format to plain and converts message to string
-	 *
-	 * @return {string} String form of plain message
-	 */
-	Message.prototype.plain = function() {
-		this.format = 'plain';
-		return this.toString();
-	};
-
-	/**
-	 * Changes the format to html escaped and converts message to string
-	 *
-	 * @return {string} String form of html escaped message
-	 */
-	Message.prototype.escaped = function() {
-		this.format = 'escaped';
-		return this.toString();
-	};
-
-	/**
-	 * Checks if message exists
-	 *
-	 * @return {string} String form of parsed message
-	 */
-	Message.prototype.exists = function() {
-		return this.map.exists( this.key );
 	};
 
 	/* Public Members */
@@ -360,7 +364,7 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 					return [a < 10 ? '0' + a : a, b < 10 ? '0' + b : b, c < 10 ? '0' + c : c].join( '' );
 				},
 				d = new Date();
-				d.setTime( timestamp * 1000 );
+			d.setTime( timestamp * 1000 );
 			return [
 				pad( d.getUTCFullYear(), d.getUTCMonth() + 1, d.getUTCDate() ), 'T',
 				pad( d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds() ), 'Z'
@@ -501,11 +505,10 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 							} ) );
 						}
 					} else if ( typeof style === 'string' ) {
-						getMarker().before( mw.html.element(
-							'style',
-							{ 'type': 'text/css', 'media': media },
-							new mw.html.Cdata( style )
-						) );
+						getMarker().before( mw.html.element( 'style', {
+							'type': 'text/css',
+							'media': media
+						}, new mw.html.Cdata( style ) ) );
 					}
 				}
 			}
@@ -673,12 +676,12 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 		 * @param callback Function: Optional callback which will be run when the script is done
 		 */
 		function addScript( src, callback ) {
+			var done = false, script;
 			if ( ready ) {
 				// jQuery's getScript method is NOT better than doing this the old-fassioned way
 				// because jQuery will eval the script's code, and errors will not have sane
 				// line numbers.
-				var	done = false,
-					script = document.createElement( 'script' );
+				script = document.createElement( 'script' );
 				script.setAttribute( 'src', src );
 				script.setAttribute( 'type', 'text/javascript' );
 				if ( $.isFunction( callback ) ) {
@@ -1163,8 +1166,7 @@ window.mw = window.mediaWiki = new ( function( $ ) {
 // Alias $j to jQuery for backwards compatibility
 window.$j = jQuery;
 
-/* Auto-register from pre-loaded startup scripts */
-
+// Auto-register from pre-loaded startup scripts
 if ( jQuery.isFunction( startUp ) ) {
 	startUp();
 	delete startUp;

commit afaf10edc5607d2dc218acf71d7486015d8db80e

resourceloader: Deprecate mw.Map
This has never been used outside mediawiki.js for things like mw.config,
and mw.messages.

The public API of those objects will remain, but the constructor will
no longer be supported in the future in order to allow these to be
backed by native Map in the future (with a minimal polyfill rather than
a complete one).

* Remove 'mw.Map(valuesObject)'. (follows-up 63f8d7b9e5).
  This is incompatible with native Map and isn't actually used anywhere.

* Deprecate mw.Map#values. Undocumented and considered private.
  A few uses have appeared in the wild so keep a short deprecation period.

* Update the MediaWiki QUnit runner to construct new maps in setup/teardown
  instead of swapping the internal values object directly.
  Also update the runner to update mediawiki.jqueryMsg's parser default since
  it kept a reference to the first mw.messages object.

* Add 'logName' parameter to mw.log.deprecate(). Default logName
  of property key made sense for its original use case (deprecation warnings
  for global variables, which are a property of the window object). But
  for keys of any other object, we'll want to some context for the name.

* Remove redundant test expect numbers. (Follows-up 7c36375, c4c7007d)

* Ensure mw.Map.prototype.constructor points to mw.Map.
  This was broken by 5819c37b3, which accidentally made it point to Object.

Bug: T146432
Change-Id: I7ff0b230608f5ca582fc6752b59c4bb998c78ba2

resources/src/mediawiki/mediawiki.js
@@ -66,48 +66,20 @@
 	}() );
 
 	/**
-	 * Create an object that can be read from or written to from methods that allow
+	 * Create an object that can be read from or written to via methods that allow
 	 * interaction both with single and multiple properties at once.
 	 *
-	 *     @example
-	 *
-	 *     var collection, query, results;
-	 *
-	 *     // Create your address book
-	 *     collection = new mw.Map();
-	 *
-	 *     // This data could be coming from an external source (eg. API/AJAX)
-	 *     collection.set( {
-	 *         'John Doe': 'john@example.org',
-	 *         'Jane Doe': 'jane@example.org',
-	 *         'George van Halen': 'gvanhalen@example.org'
-	 *     } );
-	 *
-	 *     wanted = ['John Doe', 'Jane Doe', 'Daniel Jackson'];
-	 *
-	 *     // You can detect missing keys first
-	 *     if ( !collection.exists( wanted ) ) {
-	 *         // One or more are missing (in this case: "Daniel Jackson")
-	 *         mw.log( 'One or more names were not found in your address book' );
-	 *     }
-	 *
-	 *     // Or just let it give you what it can. Optionally fill in from a default.
-	 *     results = collection.get( wanted, 'nobody@example.com' );
-	 *     mw.log( results['Jane Doe'] ); // "jane@example.org"
-	 *     mw.log( results['Daniel Jackson'] ); // "nobody@example.com"
-	 *
+	 * @private
 	 * @class mw.Map
 	 *
 	 * @constructor
-	 * @param {Object|boolean} [values] The value-baring object to be mapped. Defaults to an
-	 *  empty object.
-	 *  For backwards-compatibility with mw.config, this can also be `true` in which case values
-	 *  are copied to the Window object as global variables (T72470). Values are copied in
-	 *  one direction only. Changes to globals are not reflected in the map.
+	 * @param {boolean} [global=false] Whether to synchronise =values to the global
+	 *  window object (for backwards-compatibility with mw.config; T72470). Values are
+	 *  copied in one direction only. Changes to globals do not reflect in the map.
 	 */
-	function Map( values ) {
-		if ( values === true ) {
-			this.values = {};
+	function Map( global ) {
+		this.internalValues = {};
+		if ( global === true ) {
 
 			// Override #set to also set the global variable
 			this.set = function ( selection, value ) {
@@ -125,11 +97,16 @@
 				}
 				return false;
 			};
-
-			return;
 		}
 
-		this.values = values || {};
+		// Deprecated since MediaWiki 1.28
+		log.deprecate(
+			this,
+			'values',
+			this.internalValues,
+			'mw.Map#values is deprecated. Use mw.Map#get() instead.',
+			'Map-values'
+		);
 	}
 
 	/**
@@ -142,8 +119,8 @@
 	 * @param {Mixed} value
 	 */
 	function setGlobalMapValue( map, key, value ) {
-		map.values[ key ] = value;
-		mw.log.deprecate(
+		map.internalValues[ key ] = value;
+		log.deprecate(
 				window,
 				key,
 				value,
@@ -153,6 +130,8 @@
 	}
 
 	Map.prototype = {
+		constructor: Map,
+
 		/**
 		 * Get the value of one or more keys.
 		 *
@@ -162,7 +141,7 @@
 		 * @param {Mixed} [fallback=null] Value for keys that don't exist.
 		 * @return {Mixed|Object| null} If selection was a string, returns the value,
 		 *  If selection was an array, returns an object of key/values.
-		 *  If no selection is passed, the 'values' container is returned. (Beware that,
+		 *  If no selection is passed, the internal container is returned. (Beware that,
 		 *  as is the default in JavaScript, the object is returned by reference.)
 		 */
 		get: function ( selection, fallback ) {
@@ -181,14 +160,14 @@
 			}
 
 			if ( typeof selection === 'string' ) {
-				if ( !hasOwn.call( this.values, selection ) ) {
+				if ( !hasOwn.call( this.internalValues, selection ) ) {
 					return fallback;
 				}
-				return this.values[ selection ];
+				return this.internalValues[ selection ];
 			}
 
 			if ( selection === undefined ) {
-				return this.values;
+				return this.internalValues;
 			}
 
 			// Invalid selection key
@@ -207,12 +186,12 @@
 
 			if ( $.isPlainObject( selection ) ) {
 				for ( s in selection ) {
-					this.values[ s ] = selection[ s ];
+					this.internalValues[ s ] = selection[ s ];
 				}
 				return true;
 			}
 			if ( typeof selection === 'string' && arguments.length > 1 ) {
-				this.values[ selection ] = value;
+				this.internalValues[ selection ] = value;
 				return true;
 			}
 			return false;
@@ -229,13 +208,13 @@
 
 			if ( $.isArray( selection ) ) {
 				for ( s = 0; s < selection.length; s++ ) {
-					if ( typeof selection[ s ] !== 'string' || !hasOwn.call( this.values, selection[ s ] ) ) {
+					if ( typeof selection[ s ] !== 'string' || !hasOwn.call( this.internalValues, selection[ s ] ) ) {
 						return false;
 					}
 				}
 				return true;
 			}
-			return typeof selection === 'string' && hasOwn.call( this.values, selection );
+			return typeof selection === 'string' && hasOwn.call( this.internalValues, selection );
 		}
 	};
 
@@ -475,11 +454,14 @@
 		 * @param {string} key Name of property to create in `obj`
 		 * @param {Mixed} val The value this property should return when accessed
 		 * @param {string} [msg] Optional text to include in the deprecation message
+		 * @param {string} [logName=key] Optional custom name for the feature.
+		 *  This is used instead of `key` in the message and `mw.deprecate` tracking.
 		 */
 		log.deprecate = !Object.defineProperty ? function ( obj, key, val ) {
 			obj[ key ] = val;
-		} : function ( obj, key, val, msg ) {
-			msg = 'Use of "' + key + '" is deprecated.' + ( msg ? ( ' ' + msg ) : '' );
+		} : function ( obj, key, val, msg, logName ) {
+			logName = logName || key;
+			msg = 'Use of "' + logName + '" is deprecated.' + ( msg ? ( ' ' + msg ) : '' );
 			var logged = new StringSet();
 			function uniqueTrace() {
 				var trace = new Error().stack;
@@ -494,14 +476,14 @@
 				enumerable: true,
 				get: function () {
 					if ( uniqueTrace() ) {
-						mw.track( 'mw.deprecate', key );
+						mw.track( 'mw.deprecate', logName );
 						mw.log.warn( msg );
 					}
 					return val;
 				},
 				set: function ( newVal ) {
 					if ( uniqueTrace() ) {
-						mw.track( 'mw.deprecate', key );
+						mw.track( 'mw.deprecate', logName );
 						mw.log.warn( msg );
 					}
 					val = newVal;

tests/qunit/data/testrunner.js
@@ -167,10 +167,11 @@
 	 */
 	QUnit.newMwEnvironment = ( function () {
 		var warn, error, liveConfig, liveMessages,
+			MwMap = mw.config.constructor, // internal use only
 			ajaxRequests = [];
 
-		liveConfig = mw.config.values;
-		liveMessages = mw.messages.values;
+		liveConfig = mw.config;
+		liveMessages = mw.messages;
 
 		function suppressWarnings() {
 			warn = mw.log.warn;
@@ -198,14 +199,14 @@
 			// NOTE: It is important that we suppress warnings because extend() will also access
 			// deprecated properties and trigger deprecation warnings from mw.log#deprecate.
 			suppressWarnings();
-			copy = $.extend( {}, liveConfig, custom );
+			copy = $.extend( {}, liveConfig.get(), custom );
 			restoreWarnings();
 
 			return copy;
 		}
 
 		function freshMessagesCopy( custom ) {
-			return $.extend( /*deep=*/true, {}, liveMessages, custom );
+			return $.extend( /*deep=*/true, {}, liveMessages.get(), custom );
 		}
 
 		/**
@@ -231,8 +232,15 @@
 				setup: function () {
 
 					// Greetings, mock environment!
-					mw.config.values = freshConfigCopy( localEnv.config );
-					mw.messages.values = freshMessagesCopy( localEnv.messages );
+					mw.config = new MwMap();
+					mw.config.set( freshConfigCopy( localEnv.config ) );
+					mw.messages = new MwMap();
+					mw.messages.set( freshMessagesCopy( localEnv.messages ) );
+					// Update reference to mw.messages
+					mw.jqueryMsg.setParserDefaults( {
+						messages: mw.messages
+					} );
+
 					this.suppressWarnings = suppressWarnings;
 					this.restoreWarnings = restoreWarnings;
 
@@ -251,8 +259,12 @@
 					$( document ).off( 'ajaxSend', trackAjax );
 
 					// Farewell, mock environment!
-					mw.config.values = liveConfig;
-					mw.messages.values = liveMessages;
+					mw.config = liveConfig;
+					mw.messages = liveMessages;
+					// Restore reference to mw.messages
+					mw.jqueryMsg.setParserDefaults( {
+						messages: liveMessages
+					} );
 
 					// As a convenience feature, automatically restore warnings if they're
 					// still suppressed by the end of the test.

tests/qunit/suites/resources/mediawiki/mediawiki.test.js
@@ -65,10 +65,13 @@
 		);
 	} );
 
-	QUnit.test( 'mw.Map', 35, function ( assert ) {
+	QUnit.test( 'mw.Map', function ( assert ) {
 		var arry, conf, funky, globalConf, nummy, someValues;
 
+		this.suppressWarnings();
 		conf = new mw.Map();
+		this.restoreWarnings();
+
 		// Dummy variables
 		funky = function () {};
 		arry = [];
@@ -126,15 +129,15 @@
 			lorem: 'ipsum'
 		}, 'Map.get returns multiple values correctly as an object' );
 
-		assert.deepEqual( conf, new mw.Map( conf.values ), 'new mw.Map maps over existing values-bearing object' );
-
 		assert.deepEqual( conf.get( [ 'foo', 'notExist' ] ), {
 			foo: 'bar',
 			notExist: null
 		}, 'Map.get return includes keys that were not found as null values' );
 
 		// Interacting with globals and accessing the values object
+		this.suppressWarnings();
 		assert.strictEqual( conf.get(), conf.values, 'Map.get returns the entire values object by reference (if called without arguments)' );
+		this.restoreWarnings();
 
 		conf.set( 'globalMapChecker', 'Hi' );
 
@@ -174,7 +177,7 @@
 		assert.ok( mw.config instanceof mw.Map, 'mw.config instance of mw.Map' );
 	} );
 
-	QUnit.test( 'mw.message & mw.messages', 100, function ( assert ) {
+	QUnit.test( 'mw.message & mw.messages', function ( assert ) {
 		var goodbye, hello;
 
 		// Convenience method for asserting the same result for multiple formats
@@ -189,7 +192,6 @@
 		}
 
 		assert.ok( mw.messages, 'messages defined' );
-		assert.ok( mw.messages instanceof mw.Map, 'mw.messages instance of mw.Map' );
 		assert.ok( mw.messages.set( 'hello', 'Hello <b>awesome</b> world' ), 'mw.messages.set: Register' );
 
 		hello = mw.message( 'hello' );