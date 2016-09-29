fa0f50727940eb52f60d5399267de22819d4d76e
MarcoAurelio <strigiwm@gmail.com>
2016-09-29 10:26:10 +0200

    MediaWiki core messages for Livvi-Karelian (olo)
    Bug: T146744
    Change-Id: Ia95d821d2efd623cd75959c9cfe78fe68d184db6

--- languages/messages/MessagesOlo.php
+++ languages/messages/MessagesOlo.php
@@ -10,5 +10,24 @@
 
 $fallback = 'ru';
 
+$namespaceNames = [
+	NS_MEDIA            => 'Medii',
+	NS_SPECIAL          => 'Erikoine',
+	NS_TALK             => 'Pagin',
+	NS_USER             => 'Käyttäi',
+	NS_USER_TALK        => 'Käyttäi_pagin',
+	NS_PROJECT_TALK     => '$1_pagin',
+	NS_FILE             => 'Failu',
+	NS_FILE_TALK        => 'Failu_pagin',
+	NS_MEDIAWIKI        => 'MediiWiki',
+	NS_MEDIAWIKI_TALK   => 'MediiWiki_pagin',
+	NS_TEMPLATE         => 'Šablonu',
+	NS_TEMPLATE_TALK    => 'Šablonu_pagin',
+	NS_HELP             => 'Abu',
+	NS_HELP_TALK        => 'Abu_pagin',
+	NS_CATEGORY         => 'Kategourii',
+	NS_CATEGORY_TALK    => 'Kategourii_pagin',
+];
+
 $linkTrail = '/^([a-zčČšŠžŽäÄöÖ]+)(.*)$/sDu';
 
