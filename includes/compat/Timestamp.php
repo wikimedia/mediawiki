cd29cf0bc13dc98ac4dcdc7539560d231526f7b7
Kunal Mehta <legoktm@member.fsf.org>
2016-10-01 21:51:51 -0700

    Use wikimedia/timestamp
    Bug: T100924
    Depends-On: I0a067367cda6885fa45631ed7c18799d653dc9bf
    Change-Id: I69ba64e364df8af089c1c918cdf32f99454e693a

---
+++ includes/compat/Timestamp.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @deprecated since 1.28
+ * @since 1.20
+ */
+class TimestampException extends Wikimedia\Timestamp\TimestampException {
+}
--- includes/libs/filebackend/FSFileBackend.php
+++ includes/libs/filebackend/FSFileBackend.php
@@ -21,6 +21,7 @@
  * @ingroup FileBackend
  * @author Aaron Schulz
  */
+use Wikimedia\Timestamp\ConvertibleTimestamp;
 
 /**
  * @brief Class for a file system (FS) based file backend.
