dba3ce6be817b7b8dcf82d28d666ab17feda6cea
tjlsangria <tjlsangria@gmail.com>
2016-10-02 13:35:47 -0400

    Give sysops the deletelogentry and deleterevision rights by default.
    Bug: T70298
    Change-Id: I9c29d068154c1e9bf7f2d6a101ee1c14e4b73557

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5163,8 +5163,8 @@ $wgGroupPermissions['bureaucrat']['noratelimit'] = true;
 // Permission to export pages including linked pages regardless of $wgExportMaxLinkDepth
 # $wgGroupPermissions['bureaucrat']['override-export-depth'] = true;
 
-# $wgGroupPermissions['sysop']['deletelogentry'] = true;
-# $wgGroupPermissions['sysop']['deleterevision'] = true;
+$wgGroupPermissions['sysop']['deletelogentry'] = true;
+$wgGroupPermissions['sysop']['deleterevision'] = true;
 // To hide usernames from users and Sysops
 # $wgGroupPermissions['suppress']['hideuser'] = true;
 // To hide revisions/log items from users and Sysops
