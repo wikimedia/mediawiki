93b7bb0a6df1d00517551f2126a59938cee3d764
umherirrender <umherirrender_de.wp@web.de>
2016-10-04 21:47:47 +0200

    Refactor Parser::getVariableValue into new class
    Move the variable evalation code into it's own class and remove the
    unflexibel switch/case + hook construction by a registration function
    like there is
    already for parser functions and parser tags.
    
    This makes it easier for extensions to add a variable, because there
    have not to listen to all extension variable calls on the hook, they can
    instead register a callback for the own variable.
    
    This reduce the php footprint for pages with extension variable, because
    not all extensions with a variable have to be loaded to find the correct
    value.
    For each variable now an own function can be seen in the monitoring
    tools and may show expensive variables.
    
    With tests for all the new added functions
    
    Change-Id: I2c811f6cc6985c7a642f5cbe38dcf8d5e56e2c3a

--- autoload.php
+++ autoload.php
@@ -291,6 +291,7 @@ $wgAutoloadLocalClasses = [
 	'CopyJobQueue' => __DIR__ . '/maintenance/copyJobQueue.php',
 	'CoreParserFunctions' => __DIR__ . '/includes/parser/CoreParserFunctions.php',
 	'CoreTagHooks' => __DIR__ . '/includes/parser/CoreTagHooks.php',
+	'CoreVariables' => __DIR__ . '/includes/parser/CoreVariables.php',
 	'CoreVersionChecker' => __DIR__ . '/includes/registration/CoreVersionChecker.php',
 	'CreateAndPromote' => __DIR__ . '/maintenance/createAndPromote.php',
 	'CreateFileOp' => __DIR__ . '/includes/libs/filebackend/fileop/CreateFileOp.php',
---
+++ tests/phpunit/includes/parser/CoreVariablesTest.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for the CoreVariablesTest
+ *
+ * @file
+ */
+
+$magicWords = [];
+
+$magicWords['en'] = [
+#   ID       CASE  SYNONYMS
+	'foo' => [ 1, 'FOO' ],
+	'foo-bar' => [ 1, 'FOO-BAR' ],
+	'foo_bar' => [ 1, 'FOO_BAR' ],
+	'foo bar' => [ 1, 'FOO BAR' ],
+];
