ecaa54738688654764ca391e315bcf0ea38c098e
jdlrobson <jdlrobson@gmail.com>
2016-10-04 13:42:50 -0700

    UC Mini should be blacklisted
    The UC Mini browser should be blacklisted. The experience
    when we try to run JavaScript is suboptimal - very similar
    to how it was for Opera Mini
    
    Bug: T147369
    Change-Id: I60b40bcb8c684aa836a4a65ed1deecf06b28e51c

--- tests/qunit/suites/resources/startup.test.js
+++ tests/qunit/suites/resources/startup.test.js
@@ -10,6 +10,7 @@
 			'Mozilla/5.0 (Windows NT 6.1.1; rv:5.0) Gecko/20100101 Firefox/5.0',
 			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
 			'Mozilla/5.0 (Macintosh; I; Intel Mac OS X 11_7_9; de-LI; rv:1.9b4) Gecko/2012010317 Firefox/10.0a4',
+			'Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0',
 			'Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0',
 			'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1',
 			// Kindle Fire
@@ -47,6 +48,8 @@
 			// Android
 			'Mozilla/5.0 (Linux; U; Android 2.1; en-us; Nexus One Build/ERD62) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17',
 
+			// UC Mini speed mode off
+			'Mozilla/5.0 (Linux; U; Android 6.0.1; en-US; Nexus_5 Build/MMB29S) AppleWebKit/528.5+ (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1 UCBrowser/10.7.6.805 Mobile',
 			/* Grade C */
 
 			// Internet Explorer < 9
@@ -128,7 +131,9 @@
 			// Google Glass
 			'Mozilla/5.0 (Linux; U; Android 4.0.4; en-us; Glass 1 Build/IMM76L; XE11) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
 			// MeeGo
-			'Mozilla/5.0 (MeeGo; NokiaN9) AppleWebKit/534.13 (KHTML, like Gecko) NokiaBrowser/8.5.0 Mobile Safari/534.13'
+			'Mozilla/5.0 (MeeGo; NokiaN9) AppleWebKit/534.13 (KHTML, like Gecko) NokiaBrowser/8.5.0 Mobile Safari/534.13',
+			// UCMini (speed mode on)
+			'Mozilla/5.0 (X11; U; Linux i686; zh-CN; r:1.2.3.4) Gecko/'
 		]
 	};
 
