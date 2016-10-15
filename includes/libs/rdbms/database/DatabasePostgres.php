53d693b1e53996a3a4252024d2b436d5cc8628f8
paladox <paladox@users.noreply.github.com>
2016-10-15 18:26:52 +0100

    Update DatabasePostgres.php

--- includes/libs/rdbms/database/DatabasePostgres.php
+++ includes/libs/rdbms/database/DatabasePostgres.php
@@ -1006,7 +1006,7 @@ __INDEXATTR__;
 	 * @param string $desiredSchema
 	 */
 	function determineCoreSchema( $desiredSchema ) {
-		$this->begin( __METHOD__, self::TRANSACTION_INTERNAL );
+		$this->begin( __METHOD__, self::TRANSACTION_EXPLICIT );
 		if ( $this->schemaExists( $desiredSchema ) ) {
 			if ( in_array( $desiredSchema, $this->getSchemas() ) ) {
 				$this->mCoreSchema = $desiredSchema;
