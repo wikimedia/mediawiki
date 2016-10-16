23a740b7402f7476285b336574d229881256a9e1
Kunal Mehta <legoktm@member.fsf.org>
2016-10-16 13:18:05 -0700

    API: Expose wfIsBadImage() in prop=imageinfo
    If iiprop=badfile is set, expose the wfIsBadImage() status as a boolean.
    This is mainly intended for use by Parsoid, but could be useful for
    other API users too.
    
    Also be a little more paranoid and make sure wfIsBadImage() always
    returns a boolean.
    
    Bug: T148343
    Change-Id: I3432ead92b8d2a17de5243285f2760fc39b501ae

--- includes/api/ApiQueryImageInfo.php
+++ includes/api/ApiQueryImageInfo.php
@@ -99,8 +99,8 @@ class ApiQueryImageInfo extends ApiQueryBase {
 				$start = $title === $fromTitle ? $fromTimestamp : $params['start'];
 
 				if ( !isset( $images[$title] ) ) {
-					if ( isset( $prop['uploadwarning'] ) ) {
-						// Uploadwarning needs info about non-existing files
+					if ( isset( $prop['uploadwarning'] ) || isset( $prop['badfile'] ) ) {
+						// uploadwarning and badfile need info about non-existing files
 						$images[$title] = wfLocalFile( $title );
 					} else {
 						$result->addValue(
@@ -128,10 +128,12 @@ class ApiQueryImageInfo extends ApiQueryBase {
 					break;
 				}
 
-				$fit = $result->addValue(
-					[ 'query', 'pages', intval( $pageId ) ],
-					'imagerepository', $img->getRepoName()
-				);
+				$info = [ 'imagerepository' => $img->getRepoName() ];
+				if ( isset( $prop['badfile'] ) ) {
+					$info['badfile'] = (bool)wfIsBadImage( $title );
+				}
+
+				$fit = $result->addValue( [ 'query', 'pages' ], intval( $pageId ), $info );
 				if ( !$fit ) {
 					if ( count( $pageIds[NS_FILE] ) == 1 ) {
 						// The user is screwed. imageinfo can't be solely
@@ -734,6 +736,7 @@ class ApiQueryImageInfo extends ApiQueryBase {
 				'archivename' => 'apihelp-query+imageinfo-paramvalue-prop-archivename',
 				'bitdepth' => 'apihelp-query+imageinfo-paramvalue-prop-bitdepth',
 				'uploadwarning' => 'apihelp-query+imageinfo-paramvalue-prop-uploadwarning',
+				'badfile' => 'apihelp-query+imageinfo-paramvalue-prop-badfile',
 			],
 			array_flip( $filter )
 		);
