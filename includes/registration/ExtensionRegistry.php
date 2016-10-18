ab224f821196f8723acbf9ff81c89d311c93a8e2
Yuri Astrakhan <yurik@wikimedia.org>
2016-10-18 01:23:56 -0400

    Added array_replace_recursive merge strategy
    For extension registry, add array_replace_recursive merge strategy,
    as some extensions/configuration may prefer that to array_merge_recursive.
    
    In some cases, configuration is merged from multiple extensions,
    such as JsonConfig's $wgJsonConfigs configuration: ZeroBanner defines
    
    	"JsonZeroConfig": {
    		"namespace": 480,
    		"nsName": "Zero",
    		"isLocal": false,
    		"cacheKey": "1"
    	}
    
    and mobile.php overrides it with
    	$wgJsonConfigs['JsonZeroConfig']['isLocal'] = false;
    	$wgJsonConfigs['JsonZeroConfig']['remote'] = [
    		'url' => 'https://zero.wikimedia.org/w/api.php',
    		'username' => $wmgZeroPortalApiUserName,
    		'password' => $wmgZeroPortalApiPassword,
    	];
    
    Having identical value 'isLocal' would be converted into an array
    if array_merge_recursive is used, but the replace strategy fixes it.
    
    Change-Id: Ica6ddd0ae76f23e60de9b6235c6e2a3f2754a95d

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -258,6 +258,9 @@ class ExtensionRegistry {
 				case 'array_merge_recursive':
 					$GLOBALS[$key] = array_merge_recursive( $GLOBALS[$key], $val );
 					break;
+				case 'array_replace_recursive':
+					$GLOBALS[$key] = array_replace_recursive( $GLOBALS[$key], $val );
+					break;
 				case 'array_plus_2d':
 					$GLOBALS[$key] = wfArrayPlus2d( $GLOBALS[$key], $val );
 					break;
