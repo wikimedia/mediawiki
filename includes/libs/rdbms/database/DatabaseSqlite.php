78b49e594528248e994db091280286fd71f97bdf
Aaron Schulz <aschulz@wikimedia.org>
2016-10-24 15:19:21 -0700

    Fix DatabaseSqlite IDEA warnings
    Change-Id: Idd52a27c853653e9ba64d765f4f526f5e8a13bcd

--- includes/libs/rdbms/database/DatabaseSqlite.php
+++ includes/libs/rdbms/database/DatabaseSqlite.php
@@ -23,6 +23,7 @@
  */
 
 /**
+ * @property PDO $mConn
  * @ingroup Database
  */
 class DatabaseSqlite extends Database {
@@ -31,20 +32,15 @@ class DatabaseSqlite extends Database {
 
 	/** @var string Directory */
 	protected $dbDir;
-
 	/** @var string File name for SQLite database file */
 	protected $dbPath;
-
 	/** @var string Transaction mode */
 	protected $trxMode;
 
 	/** @var int The number of rows affected as an integer */
 	protected $mAffectedRows;
-
 	/** @var resource */
 	protected $mLastResult;
-
-	/** @var $mConn PDO */
 
 	/** @var FSLockManager (hopefully on the same server as the DB) */
 	protected $lockMgr;
@@ -144,7 +140,7 @@ class DatabaseSqlite extends Database {
 	 * @param string $dbName
 	 *
 	 * @throws DBConnectionError
-	 * @return PDO
+	 * @return bool
 	 */
 	function open( $server, $user, $pass, $dbName ) {
 		$this->close();
@@ -155,7 +151,7 @@ class DatabaseSqlite extends Database {
 		}
 		$this->openFile( $fileName );
 
-		return $this->mConn;
+		return (bool)$this->mConn;
 	}
 
 	/**
@@ -493,15 +489,12 @@ class DatabaseSqlite extends Database {
 	 * @param string $table
 	 * @param string $index
 	 * @param string $fname
-	 * @return array
+	 * @return array|false
 	 */
 	function indexInfo( $table, $index, $fname = __METHOD__ ) {
 		$sql = 'PRAGMA index_info(' . $this->addQuotes( $this->indexName( $index ) ) . ')';
 		$res = $this->query( $sql, $fname );
-		if ( !$res ) {
-			return null;
-		}
-		if ( $res->numRows() == 0 ) {
+		if ( !$res || $res->numRows() == 0 ) {
 			return false;
 		}
 		$info = [];
