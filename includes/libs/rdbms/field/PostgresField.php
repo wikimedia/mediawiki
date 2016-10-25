933166080a12d363d038312b87b12aebc10af54f
Paladox <thomasmulhall410@yahoo.com>
2016-10-25 21:56:48 +0100

    Test: Do not merge
    Change-Id: I227c9ef44800833f64cd373d03fc60ed05f4ec88

--- includes/libs/rdbms/field/PostgresField.php
+++ includes/libs/rdbms/field/PostgresField.php
@@ -9,7 +9,7 @@ class PostgresField implements Field {
 	 * @param string $field
 	 * @return null|PostgresField
 	 */
-	static function fromText( $db, $table, $field ) {
+	static function fromText( DatabasePostgres $db, $table, $field ) {
 		$q = <<<SQL
 SELECT
  attnotnull, attlen, conname AS conname,
