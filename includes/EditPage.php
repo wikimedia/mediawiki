758410a4526ac7e8f8b32bb2c70e803f2f0ae62c
WMDE-Fisch <christoph.fischer@wikimedia.de>
2016-10-27 15:49:49 +0200

    Extract addition of conflict header explanation
    This is done so it can be altered when using an alternative EditPage.
    
    Bug: T143823
    Change-Id: I31f5f2b7d35806a11925fb00e3272cac47c783ec

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2902,7 +2902,7 @@ ERROR;
 		global $wgAllowUserCss, $wgAllowUserJs;
 
 		if ( $this->isConflict ) {
-			$wgOut->wrapWikiMsg( "<div class='mw-explainconflict'>\n$1\n</div>", 'explainconflict' );
+			$this->addExplainConflictHeader( $wgOut );
 			$this->editRevId = $this->page->getLatest();
 		} else {
 			if ( $this->section != '' && $this->section != 'new' ) {
@@ -4460,5 +4460,13 @@ HTML
 					'msgKey' => [ 'titleprotectedwarning' ],
 					'wrap' => "<div class=\"mw-titleprotectedwarning\">\n$1</div>" ] );
 		}
+	}
+
+	/**
+	 * @param OutputPage &$out
+	 * @since 1.28
+	 */
+	protected function addExplainConflictHeader( OutputPage &$out ) {
+		$out->wrapWikiMsg( "<div class='mw-explainconflict'>\n$1\n</div>", 'explainconflict' );
 	}
 }
