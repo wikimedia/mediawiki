8d61cf2793abd24166f2cc5da10b517a2802d6cb
Aaron Schulz <aschulz@wikimedia.org>
2016-10-26 22:44:08 -0700

    Make LBFactoryTest.php pass for sqlite
    Change-Id: I4fe929e82218231f6c8afa64da8c0ccb42d2c362

--- includes/libs/rdbms/database/DatabaseSqlite.php
+++ includes/libs/rdbms/database/DatabaseSqlite.php
@@ -196,6 +196,10 @@ class DatabaseSqlite extends Database {
 		return false;
 	}
 
+	public function selectDB( $db ) {
+		return false; // doesn't make sense
+	}
+
 	/**
 	 * @return string SQLite DB file path
 	 * @since 1.25
