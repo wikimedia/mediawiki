57165c452d17be7d1f763101276f33eb78b11026
Paladox <thomasmulhall410@yahoo.com>
2016-10-29 22:36:50 +0100

    Test do not submit
    Change-Id: I3e7c6817c9906d27c9e084a2e8143671f2cbf24c

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -57,11 +57,11 @@ CREATE INDEX user_email_token_idx ON mwuser (user_email_token);
 INSERT INTO mwuser
   VALUES (DEFAULT,'Anonymous','',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,now(),now());
 
-CREATE TABLE user_groups (
+CREATE TABLE /*_*/user_groups (
   ug_user   INTEGER      NULL  REFERENCES mwuser(user_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
   ug_group  TEXT     NOT NULL
 );
-CREATE UNIQUE INDEX user_groups_unique ON user_groups (ug_user, ug_group);
+CREATE UNIQUE INDEX /*i*/user_groups_unique ON /*_*/user_groups (ug_user, ug_group);
 
 CREATE TABLE user_former_groups (
   ufg_user   INTEGER      NULL  REFERENCES mwuser(user_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
