95f075217f404f1ae5cc7acc7a6fbf132a4baba3
wikirina <irina.baldassari@gmail.com>
2016-10-29 18:17:08 +0200

    Include short descriptions for extensions bundled in the release
    Bug: T45817
    
    Change-Id: I4e6292e5b0d42a954c0093e4a090fe0abc676738

--- includes/installer/WebInstallerOptions.php
+++ includes/installer/WebInstallerOptions.php
@@ -104,7 +104,7 @@ class WebInstallerOptions extends WebInstallerPage {
 			$this->getFieldsetEnd()
 		);
 
-		$skins = $this->parent->findExtensions( 'skins' );
+		$skins = array_keys($this->parent->findExtensions( 'skins' ));
 		$skinHtml = $this->getFieldsetStart( 'config-skins' );
 
 		$skinNames = array_map( 'strtolower', $skins );
@@ -144,11 +144,12 @@ class WebInstallerOptions extends WebInstallerPage {
 		if ( $extensions ) {
 			$extHtml = $this->getFieldsetStart( 'config-extensions' );
 
-			foreach ( $extensions as $ext ) {
+			foreach ( $extensions as $ext => $descr ) {
 				$extHtml .= $this->parent->getCheckBox( [
 					'var' => "ext-$ext",
 					'rawtext' => $ext,
 				] );
+				$extHtml .= "<div>" . $descr . "</div>";
 			}
 
 			$extHtml .= $this->parent->getHelpBox( 'config-extensions-help' ) .
@@ -345,7 +346,7 @@ class WebInstallerOptions extends WebInstallerPage {
 	 * @return bool
 	 */
 	public function submitSkins() {
-		$skins = $this->parent->findExtensions( 'skins' );
+		$skins = array_keys($this->parent->findExtensions( 'skins' ));
 		$this->parent->setVar( '_Skins', $skins );
 
 		if ( $skins ) {
@@ -398,7 +399,7 @@ class WebInstallerOptions extends WebInstallerPage {
 			$this->setVar( 'wgRightsIcon', '' );
 		}
 
-		$skinsAvailable = $this->parent->findExtensions( 'skins' );
+		$skinsAvailable = array_keys($this->parent->findExtensions( 'skins' ));
 		$skinsToInstall = [];
 		foreach ( $skinsAvailable as $skin ) {
 			$this->parent->setVarsFromRequest( [ "skin-$skin" ] );
@@ -419,7 +420,7 @@ class WebInstallerOptions extends WebInstallerPage {
 			$retVal = false;
 		}
 
-		$extsAvailable = $this->parent->findExtensions();
+		$extsAvailable = array_keys($this->parent->findExtensions());
 		$extsToInstall = [];
 		foreach ( $extsAvailable as $ext ) {
 			$this->parent->setVarsFromRequest( [ "ext-$ext" ] );
