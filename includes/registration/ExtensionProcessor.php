fdd345b02c5d705db2af3645117d74622a6acfd7
Reedy <reedy@wikimedia.org>
2016-10-31 17:12:56 +0000

    Set merge strategy for $wgPasswordPolicy
    Bug: T149597
    Change-Id: I56c0b8932fcd42ec9b748529daa32a998fb19bf8

--- includes/registration/ExtensionProcessor.php
+++ includes/registration/ExtensionProcessor.php
@@ -52,6 +52,7 @@ class ExtensionProcessor implements Processor {
 		'APIListModules',
 		'ValidSkinNames',
 		'FeedClasses',
+		'PasswordPolicy',
 	];
 
 	/**
@@ -74,6 +75,7 @@ class ExtensionProcessor implements Processor {
 		'wgCapitalLinkOverrides' => 'array_plus',
 		'wgRateLimits' => 'array_plus_2d',
 		'wgAuthManagerAutoConfig' => 'array_plus_2d',
+		'wgPasswordPolicy' => 'array_merge_recursive'
 	];
 
 	/**
