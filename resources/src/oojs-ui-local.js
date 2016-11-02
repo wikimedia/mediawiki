4f61e992b9d712112a89c153f9c0ef0835219090
Bartosz Dziewoński <matma.rex@gmail.com>
2016-11-02 16:18:58 +0100

    Connect OOjs UI's deprecation warnings to MediaWiki's logging system
    Change-Id: I0aa24a877ec1cf6505cb66b0129c6969ef90313f

--- resources/src/oojs-ui-local.js
+++ resources/src/oojs-ui-local.js
@@ -1,5 +1,10 @@
-// Connect OOjs UI to MediaWiki's localisation system
 ( function ( mw ) {
+	// Connect OOjs UI to MediaWiki's localisation system
 	OO.ui.getUserLanguages = mw.language.getFallbackLanguageChain;
 	OO.ui.msg = mw.msg;
+	// Connect OOjs UI's deprecation warnings to MediaWiki's logging system
+	OO.ui.warnDeprecation = function ( message ) {
+		mw.track( 'mw.deprecate', 'oojs-ui' );
+		mw.log.warn( message );
+	};
 }( mediaWiki ) );
