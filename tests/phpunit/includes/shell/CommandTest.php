f384c3a91baa6e3b7e45363650dd27ca75784f17
Max Semenik <maxsem.wiki@gmail.com>
2016-11-02 17:27:15 -0700

    WIP: replace wfShellExec() with a class
    This function has gotten so unwieldy that a helper was
    introduced. Instead, I'm proposing this class that makes
    shelling out easier and more readable.
    
    Example usage:
      $command = Shell::command( 'shell command' )
           ->environment( [ 'ENVIRONMENT_VARIABLE' => 'VALUE' ] )
           ->limits( [ 'time' => 300 ] )
           ->execute();
    
      $exitCode = $command->getExitCode();
      $output = $command->getOutput();
    
    This is a minimal change, so lots of stuff remains
    unrefactored - I'd rather limit the scope of this commit.
    A future improvement could be an ability to get stderr
    separately from stdout.
    
    Problem points that I'd like to discuss:
    * I went with a purely OOP approach for handling the 'proc_open()
      is disabled' error - throw an exception and forget. While
      stubbed wfShellExec() retains the old behavior, once callers
      are updated they will either need to catch the exception or they
      will break for some poor souls - another option would be to introduce
      a noThrow() option.
    * proc_open() failures used to just return a status code. I've replaced
      it with exception throw. Don't think it's a problem, for example we've had
      less than one such error per day in WMF production.
    
    Competing commit: I7dccb2b67a4173a8a89b035e444fbda9102e4d0f
    <legoktm> MaxSem: so you should continue working on your patch and I'll
              probably refactor on top of it later after its merged :P
    
    Change-Id: I8ac9858b80d7908cf7e7981d7e19d0fc9c2265c0

---
+++ tests/phpunit/includes/shell/CommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use MediaWiki\Shell\Command;
+
+/**
+ * @group Shell
+ */
+class CommandTest extends PHPUnit_Framework_TestCase {
+	public function setUp() {
+		parent::setUp();
+
+		if ( wfIsWindows() ) {
+			$this->markTestSkipped( 'This test requires a POSIX environment.' );
+		}
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error_Notice
+	 */
+	public function testDestruct() {
+		$command = new Command( 'true' );
+	}
+
+	/**
+	 * @dataProvider provideExecute
+	 */
+	public function testExecute( $commandInput, $expectedExitCode, $expectedOutput ) {
+		$command = new Command( $commandInput );
+		$result = $command->execute();
+		$this->assertSame( $expectedExitCode, $result->getStatusCode() );
+		$this->assertSame( $expectedOutput, $result->getStdout() );
+	}
+
+	public function provideExecute() {
+		return [
+			'success status' => [ 'true', 0, '' ],
+			'failure status' => [ 'false', 1, '' ],
+			'output' => [ [ 'echo', '-n', 'x', '>', 'y' ], 0, 'x > y' ],
+		];
+	}
+
+	public function testEnvironment() {
+		$command = new Command( [ 'printenv', 'FOO' ] );
+		$result = $command->environment( [ 'FOO' => 'bar' ] )->execute();
+		$this->assertSame( "bar\n", $result->getStdout() );
+	}
+
+	public function test() {
+		global $IP;
+
+		$command = new Command( [ 'ls', "$IP/index.php" ] );
+		$result = $command->execute();
+		$this->assertSame( "$IP/index.php", trim( $result->getStdout() ) );
+
+		$command = new Command( [ 'ls', 'index.php', 'no-such-file' ] );
+		$result = $command->includeStderr()->execute();
+		$this->assertRegExp( '/^.+no-such-file.*$/m', $result->getStdout() );
+	}
+}
