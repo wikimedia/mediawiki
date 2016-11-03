77ce3b98a0193b674232e972e7c6993a585412d7
Max Semenik <maxsem.wiki@gmail.com>
2016-11-02 17:27:15 -0700

    Replace wfShellExec() with a class
    This function has gotten so unwieldy that a helper was
    introduced. Instead, here's this class that makes
    shelling out easier and more readable.
    
    Example usage:
      $result = Shell::command( 'shell command' )
           ->environment( [ 'ENVIRONMENT_VARIABLE' => 'VALUE' ] )
           ->limits( [ 'time' => 300 ] )
           ->execute();
    
      $exitCode = $result->getExitCode();
      $output = $result->getStdout();
    
    This is a minimal change, so lots of stuff remains
    unrefactored - I'd rather limit the scope of this commit.
    A future improvement could be an ability to get stderr
    separately from stdout.
    
    Caveat: execution errors (proc_open is disabled/returned error) now
    throw errors instead of returning a status code. wfShellExec() still
    emulates this behavior though.
    
    Competing commit: I7dccb2b67a4173a8a89b035e444fbda9102e4d0f
    <legoktm> MaxSem: so you should continue working on your patch and I'll
              probably refactor on top of it later after its merged :P
    
    Change-Id: I8ac9858b80d7908cf7e7981d7e19d0fc9c2265c0

---
+++ tests/phpunit/includes/shell/ShellTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use MediaWiki\Shell\Shell;
+
+/**
+ * @group Shell
+ */
+class ShellTest extends PHPUnit_Framework_TestCase {
+	public function testIsDisabled() {
+		$this->assertInternalType( 'bool', Shell::isDisabled() ); // sanity
+	}
+
+	/**
+	 * @dataProvider provideEscape
+	 */
+	public function testEscape( $args, $expected ) {
+		if ( wfIsWindows() ) {
+			$this->markTestSkipped( 'This test requires a POSIX environment.' );
+		}
+		$this->assertSame( $expected, call_user_func_array( [ Shell::class, 'escape' ], $args ) );
+	}
+
+	public function provideEscape() {
+		return [
+			'simple' => [ [ 'true' ], "'true'" ],
+			'with args' => [ [ 'convert', '-font', 'font name' ], "'convert' '-font' 'font name'" ],
+			'array' => [ [ [ 'convert', '-font', 'font name' ] ], "'convert' '-font' 'font name'" ],
+		];
+	}
+}
