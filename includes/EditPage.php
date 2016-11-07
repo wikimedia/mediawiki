wikimedia/mediawiki

commit 1255654ed5a89ed57491bda38f544ed87e3bc601

Show wgPageParseReport on page previews too
Also make EditPage::getPreviewLimitReport return the same
output instead of using copy-pasted code from the old hook.

Bug: T142210
Bug: T142214
Change-Id: I16c3c2da067fd24bf7ae564cb1d61beaa3be58c1

includes/EditPage.php
@@ -2714,8 +2714,9 @@ class EditPage {
 		$wgOut->addHTML( Html::rawElement( 'div', [ 'class' => 'hiddencats' ],
 			Linker::formatHiddenCategories( $this->page->getHiddenCategories() ) ) );
 
-		$wgOut->addHTML( Html::rawElement( 'div', [ 'class' => 'limitreport' ],
-			self::getPreviewLimitReport( $this->mParserOutput ) ) );
+		if ( $this->mParserOutput ) {
+			$wgOut->setLimitReportData( $this->mParserOutput->getLimitReportData() );
+		}
 
 		$wgOut->addModules( 'mediawiki.action.edit.collapsibleFooter' );
 
@@ -3442,41 +3443,12 @@ HTML
 			return '';
 		}
 
-		$limitReport = Html::rawElement( 'div', [ 'class' => 'mw-limitReportExplanation' ],
-			wfMessage( 'limitreport-title' )->parseAsBlock()
+		return ResourceLoader::makeInlineScript(
+			ResourceLoader::makeConfigSetScript(
+				[ 'wgPageParseReport' => $output->getLimitReportData() ],
+				true
+			)
 		);
-
-		// Show/hide animation doesn't work correctly on a table, so wrap it in a div.
-		$limitReport .= Html::openElement( 'div', [ 'class' => 'preview-limit-report-wrapper' ] );
-
-		$limitReport .= Html::openElement( 'table', [
-			'class' => 'preview-limit-report wikitable'
-		] ) .
-			Html::openElement( 'tbody' );
-
-		foreach ( $output->getLimitReportData() as $key => $value ) {
-			if ( Hooks::run( 'ParserLimitReportFormat',
-				[ $key, &$value, &$limitReport, true, true ]
-			) ) {
-				$keyMsg = wfMessage( $key );
-				$valueMsg = wfMessage( [ "$key-value-html", "$key-value" ] );
-				if ( !$valueMsg->exists() ) {
-					$valueMsg = new RawMessage( '$1' );
-				}
-				if ( !$keyMsg->isDisabled() && !$valueMsg->isDisabled() ) {
-					$limitReport .= Html::openElement( 'tr' ) .
-						Html::rawElement( 'th', null, $keyMsg->parse() ) .
-						Html::rawElement( 'td', null, $valueMsg->params( $value )->parse() ) .
-						Html::closeElement( 'tr' );
-				}
-			}
-		}
-
-		$limitReport .= Html::closeElement( 'tbody' ) .
-			Html::closeElement( 'table' ) .
-			Html::closeElement( 'div' );
-
-		return $limitReport;
 	}
 
 	protected function showStandardInputs( &$tabindex = 2 ) {

includes/OutputPage.php
@@ -1773,7 +1773,7 @@ class OutputPage extends ContextSource {
 		}
 
 		// Include profiling data
-		$this->limitReportData = $parserOutput->getLimitReportData();
+		$this->setLimitReportData( $parserOutput->getLimitReportData() );
 
 		// Link flags are ignored for now, but may in the future be
 		// used to mark individual language links.
@@ -4050,5 +4050,13 @@ class OutputPage extends ContextSource {
 		// Used by 'skipFunction' of the four 'oojs-ui.styles.*' modules. Please don't treat this as a
 		// public API or you'll be severely disappointed when T87871 is fixed and it disappears.
 		$this->addMeta( 'X-OOUI-PHP', '1' );
+	}
+
+	/**
+	 * @param array $data Data from ParserOutput::getLimitReportData()
+	 * @since 1.28
+	 */
+	public function setLimitReportData( array $data ) {
+		$this->limitReportData = $data;
 	}
 }

commit f8886cea714ba797a84ea11eecd3b9237f9f4277

Allow extensions to use ParserLimitReportPrepare/ParserLimitReportFormat again
* Display data in all "namespaces", not just 'limitreport',
  as extensions may use custom ones (e.g. Scribunto).
  See ParserOutput::setLimitReportData(). The isDisabled()
  checks ensure that "unknown" "namespaces" are still ignored.
* Correct the $key passed to ParserLimitReportFormat.

Follow-up to 1255654ed5a89ed57491bda38f544ed87e3bc601,
which for unclear reasons removed this code entirely,
and 0051f108b954b52b9981d5d85862ac1f292db80c, which was
not a complete fix.

This still does not resolve everything that was broken in
1255654ed5a89ed57491bda38f544ed87e3bc601 (limit report data
without a "namespace" will be ignored). Honestly I would
prefer to revert the whole thing entirely and rethink it.

Bug: T142210
Change-Id: I5af34c07255a8bc3977f474037b748bccf46ae99

includes/EditPage.php
@@ -3550,26 +3550,31 @@ HTML
 		] ) .
 			Html::openElement( 'tbody' );
 
-		foreach ( $output->getLimitReportData()['limitreport'] as $key => $value ) {
-			if ( Hooks::run( 'ParserLimitReportFormat',
-				[ $key, &$value, &$limitReport, true, true ]
-			) ) {
-				$keyMsg = wfMessage( "limitreport-$key" );
-				$valueMsg = wfMessage(
-					[ "limitreport-$key-value-html", "limitreport-$key-value" ]
-				);
-				if ( !$valueMsg->exists() ) {
-					$valueMsg = new RawMessage( '$1' );
-				}
-				if ( !$keyMsg->isDisabled() && !$valueMsg->isDisabled() ) {
-					// If it's a value/limit array, convert it for $1/$2
-					if ( is_array( $value ) && isset( $value['value'] ) ) {
-						$value = [ $value['value'], $value['limit'] ];
+		foreach ( $output->getLimitReportData() as $ns => $data ) {
+			if ( is_array( $data ) ) {
+				foreach ( $data as $name => $value ) {
+					$key = "$ns-$name";
+					if ( Hooks::run( 'ParserLimitReportFormat',
+						[ $key, &$value, &$limitReport, true, true ]
+					) ) {
+						$keyMsg = wfMessage( $key );
+						$valueMsg = wfMessage(
+							[ "$key-value-html", "$key-value" ]
+						);
+						if ( !$valueMsg->exists() ) {
+							$valueMsg = new RawMessage( '$1' );
+						}
+						if ( !$keyMsg->isDisabled() && !$valueMsg->isDisabled() ) {
+							// If it's a value/limit array, convert it for $1/$2
+							if ( is_array( $value ) && isset( $value['value'] ) ) {
+								$value = [ $value['value'], $value['limit'] ];
+							}
+							$limitReport .= Html::openElement( 'tr' ) .
+								Html::rawElement( 'th', null, $keyMsg->parse() ) .
+								Html::rawElement( 'td', null, $valueMsg->params( $value )->parse() ) .
+								Html::closeElement( 'tr' );
+						}
 					}
-					$limitReport .= Html::openElement( 'tr' ) .
-						Html::rawElement( 'th', null, $keyMsg->parse() ) .
-						Html::rawElement( 'td', null, $valueMsg->params( $value )->parse() ) .
-						Html::closeElement( 'tr' );
 				}
 			}
 		}