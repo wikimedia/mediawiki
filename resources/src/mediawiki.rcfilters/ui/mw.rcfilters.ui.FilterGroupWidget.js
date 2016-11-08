eb66e3ccf53d62bfabcfef878497d2099c2f4e59
Moriel Schottlender <moriel@gmail.com>
2016-11-07 17:14:13 -0800

    [super-atomic-WIP] Adding filters to RecentChanges
    Filters are to parameters
    Unlike an engineer is to code.
    
    This is a super super super WIP patch, not ready to be reviewed
    or considered. Don't look at it. Seriously, avert your eyes.
    
    Go ahead.
    
    Do it.
    
    You're still looking.
    
    Stop.
    
    Bug: T149435
    Bug: T149452
    Bug: T144448
    Change-Id: Ic545ff1462998b610d7edae59472ecce2e7d51ea

---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterGroupWidget.js
@@ -0,0 +1,49 @@
+( function ( mw, $ ) {
+	/**
+	 * A group of filters
+	 *
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {string} name Group name
+	 * @param {Object} config Configuration object
+	 * @cfg {string|jQuery} [title] Title for this filter group
+	 */
+	mw.rcfilters.ui.FilterGroupWidget = function MwRcfiltersUiFilterGroupWidget( name, config ) {
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.FilterGroupWidget.parent.call( this, config );
+		// Mixin constructors
+		OO.ui.mixin.GroupWidget.call( this, config );
+
+		this.name = name;
+
+		this.$title = $( '<div>' )
+			.addClass( 'mw-rcfilters-ui-filterGroupWidget-title' );
+
+		if ( config.title ) {
+			this.$element
+				.append(
+					this.$title
+						.text( config.title )
+				);
+		}
+
+		this.$element
+			.addClass( 'mw-rcfilters-ui-filterGroupWidget' )
+			.append(
+				this.$group
+					.addClass( 'mw-rcfilters-ui-filterGroupWidget-group' )
+			);
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.ui.FilterGroupWidget, OO.ui.Widget );
+	OO.mixinClass( mw.rcfilters.ui.FilterGroupWidget, OO.ui.mixin.GroupWidget );
+
+	mw.rcfilters.ui.FilterGroupWidget.prototype.getName = function () {
+		return this.name;
+	};
+} )( mediaWiki, jQuery );
