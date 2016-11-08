8c422fb20d3c68f2d8b694a34f97c785ccb21b2b
niharika29 <nkohli@wikimedia.org>
2016-11-08 14:44:13 +0000

    Add var to User class to store block type for eventlogging purposes
    Bug: T146230
    Change-Id: I70b54243a3018191a4e0cc311e90f66d240e1813

--- includes/user/User.php
+++ includes/user/User.php
@@ -301,6 +301,9 @@ class User implements IDBAccessObject {
 	/** @var integer User::READ_* constant bitfield used to load data */
 	protected $queryFlagsUsed = self::READ_NORMAL;
 
+	/** @var string Indicates type of block (used for eventlogging) */
+	protected $blockTrigger = false;
+
 	public static $idCacheByName = [];
 
 	/**
@@ -1640,6 +1643,7 @@ class User implements IDBAccessObject {
 				if ( $blockIsValid && $useBlockCookie ) {
 					// Use the block.
 					$block = $tmpBlock;
+					$this->blockTrigger = 'cookie-block';
 				} else {
 					// If the block is not valid, clear the block cookie (but don't delete it,
 					// because it needs to be cleared from LocalStorage as well and an empty string
@@ -1657,11 +1661,13 @@ class User implements IDBAccessObject {
 				$block->setBlocker( wfMessage( 'proxyblocker' )->text() );
 				$block->mReason = wfMessage( 'proxyblockreason' )->text();
 				$block->setTarget( $ip );
+				$this->blockTrigger = 'proxy-block';
 			} elseif ( $this->isAnon() && $this->isDnsBlacklisted( $ip ) ) {
 				$block = new Block;
 				$block->setBlocker( wfMessage( 'sorbs' )->text() );
 				$block->mReason = wfMessage( 'sorbsreason' )->text();
 				$block->setTarget( $ip );
+				$this->blockTrigger = 'openproxy-block';
 			}
 		}
 
@@ -1680,6 +1686,7 @@ class User implements IDBAccessObject {
 				# Mangle the reason to alert the user that the block
 				# originated from matching the X-Forwarded-For header.
 				$block->mReason = wfMessage( 'xffblockreason', $block->mReason )->text();
+				$this->blockTrigger = 'xff-block';
 			}
 		}
 
