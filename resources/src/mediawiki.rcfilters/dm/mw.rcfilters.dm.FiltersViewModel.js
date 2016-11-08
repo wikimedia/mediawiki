70b9ae6ca1b3c97a0642a1882bd70df2860037e3
Moriel Schottlender <moriel@gmail.com>
2016-11-07 17:14:13 -0800

    [super-atomic-WIP] Adding filters to RecentChanges
    This is a super super super WIP patch, not ready to be reviewed
    or considered. Don't look at it. Seriously, avert your eyes.
    
    Go ahead.
    
    Do it.
    
    You're still looking.
    
    Stop.
    
    Bug: T149435
    Bug: T149452
    Bug: T144448
    Change-Id: Ic545ff1462998b610d7edae59472ecce2e7d51ea

---
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.FiltersViewModel.js
@@ -0,0 +1,128 @@
+( function ( mw ) {
+	/**
+	 * View model for the filters selection and display
+	 *
+	 * @mixins OO.EventEmitter
+	 * @mixins OO.EmitterList
+	 *
+	 * @constructor
+	 * @param {Object} config Configuration object
+	 */
+	mw.rcfilters.dm.FiltersViewModel = function MwRcfiltersDmFiltersViewModel( config ) {
+		config = config || {};
+
+		// Mixin constructor
+		OO.EventEmitter.call( this );
+		OO.EmitterList.call( this );
+
+		this.groups = {};
+
+		// Events
+		this.aggregate( { update: 'itemUpdate' } );
+	};
+
+	/* Initialization */
+	OO.initClass( mw.rcfilters.dm.FiltersViewModel );
+	OO.mixinClass( mw.rcfilters.dm.FiltersViewModel, OO.EventEmitter );
+	OO.mixinClass( mw.rcfilters.dm.FiltersViewModel, OO.EmitterList );
+
+	/* Events */
+
+	/**
+	 * @event update
+	 *
+	 * Filter list has changed
+	 */
+
+	/* Methods */
+
+	/**
+	 * Set filters and preserve a group relationship based on
+	 * the definition given by an object
+	 *
+	 * @param {Object} filters Filter group definition
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.setFilters = function ( filters ) {
+		var i, filterItem, group,
+			items = [];
+
+		for ( group in filters ) {
+			this.groups[ group ] = this.groups[ group ] || {};
+			this.groups[ group ].filters = this.groups[ group ].filters || [];
+
+			this.groups[ group ].title = filters[ group ].title;
+			for ( i = 0; i < filters[ group ].filters.length; i++ ) {
+				filterItem = new mw.rcfilters.dm.FilterItem( filters[ group ].filters[ i ].name, {
+					group: group,
+					label: filters[ group ].filters[ i ].label,
+					description: filters[ group ].filters[ i ].description
+				} );
+
+				this.groups[ group ].filters.push( filterItem );
+				items.push( filterItem );
+			}
+		}
+
+		this.addItems( items );
+		this.emit( 'update' );
+	};
+
+	/**
+	 * Get the names of all available filters
+	 *
+	 * @return {string[]} An array of filter names
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.getFilterNames = function () {
+		return this.getItems().map( function ( item ) { return item.getName(); } );
+	};
+
+	/**
+	 * Get the object that defines groups and their filter items
+	 *
+	 * @return {Object} Filter groups
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.getFilterGroups = function () {
+		return this.groups;
+	};
+
+	mw.rcfilters.dm.FiltersViewModel.prototype.getItemsByName = function ( names ) {
+		return this.getItems().filter( function ( item ) {
+			return names.indexOf( item.getName() ) > -1;
+		} );
+	};
+
+	/**
+	 * Toggle selected state of items by their names
+	 *
+	 * @param {boolean} isSelected Item is selected
+	 * @param {string[]} names An array of item names
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.toggleSelectItemsByName = function ( isSelected, names ) {
+		this.getItems().forEach( function ( item ) {
+			if ( names.indexOf( item.getName() ) > -1 ) {
+				item.toggleSelected( isSelected );
+			}
+		} );
+	};
+	/**
+	 * Find items whose labels match the given string
+	 *
+	 * @param {String} str Search string
+	 * @return {Object} An object of items to show
+	 *  arranged by their group names
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.findMatches = function ( str ) {
+		var i,
+			result = {},
+			items = this.getItems();
+
+		for ( i = 0; i < items.length; i++ ) {
+			if ( items[ i ].getLabel().toLowerCase().indexOf( str.toLowerCase() ) > -1 ) {
+				result[ items[ i ].getGroup() ] = result[ items[ i ].getGroup() ] || [];
+				result[ items[ i ].getGroup() ].push( items[ i ] );
+			}
+		}
+		return result;
+	};
+
+} )( mediaWiki );
---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FiltersListWidget.js
@@ -0,0 +1,146 @@
+( function ( mw, $ ) {
+	/**
+	 * List displaying all filter groups
+	 *
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {Object} config Configuration object
+	 * @cfg {Object} [filters] A definition of the filter groups in this list
+	 * @cfg {string|jQuery} [title] Title for the list
+	 */
+	mw.rcfilters.ui.FiltersListWidget = function MwRcfiltersUiFiltersListWidget( model, config ) {
+		var titleWidget;
+
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.FiltersListWidget.parent.call( this, config );
+		// Mixin constructors
+		OO.ui.mixin.GroupWidget.call( this, config );
+
+		this.model = model;
+
+		this.noResultsLabel = new OO.ui.LabelWidget( {
+			label: mw.msg( 'rcfilters-filterlist-noresults' ),
+			classes: [ 'mw-rcfilters-ui-filtersListWidget-noresults' ]
+		} );
+
+		if ( config.title ) {
+			titleWidget = new OO.ui.LabelWidget( {
+				label: config.title
+			} );
+			this.$element.append(
+				$( '<div>' )
+					.addClass( 'mw-rcfilters-ui-filtersListWidget-title' )
+					.append( titleWidget.$element )
+			);
+		}
+
+		// Events
+		this.model.connect( this, {
+			update: 'onModelUpdate'
+		} );
+
+		// Initialize
+		this.showNoResultsMessage( false );
+		this.$element
+			.addClass( 'mw-rcfilters-ui-filtersListWidget' )
+			.append(
+				this.$group
+					.addClass( 'mw-rcfilters-ui-filtersListWidget-group' ),
+				this.noResultsLabel.$element
+			);
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.ui.FiltersListWidget, OO.ui.Widget );
+	OO.mixinClass( mw.rcfilters.ui.FiltersListWidget, OO.ui.mixin.GroupWidget );
+
+	/* Methods */
+	mw.rcfilters.ui.FiltersListWidget.prototype.onModelUpdate = function () {
+		var i, group, groupWidget,
+			itemWidgets = [],
+			groupWidgets = [],
+			groups = this.model.getFilterGroups();
+
+		for ( group in groups ) {
+			groupWidget = new mw.rcfilters.ui.FilterGroupWidget( group, {
+				title: groups[ group ].title
+			} );
+			groupWidgets.push( groupWidget );
+
+			itemWidgets = [];
+			if ( groups[ group ].filters ) {
+				for ( i = 0; i < groups[ group ].filters.length; i++ ) {
+					itemWidgets.push(
+						new mw.rcfilters.ui.FilterItemWidget(
+							groups[ group ].filters[ i ],
+							{
+								label: groups[ group ].filters[ i ].getLabel(),
+								description: groups[ group ].filters[ i ].getDescription()
+							}
+						)
+					);
+				}
+
+				groupWidget.addItems( itemWidgets );
+			}
+		}
+
+		this.addItems( groupWidgets );
+	};
+
+	mw.rcfilters.ui.FiltersListWidget.prototype.showNoResultsMessage = function ( showNoResults ) {
+		this.noResultsLabel.toggle( !!showNoResults );
+		this.$group.toggleClass( 'oo-ui-element-hidden', !!showNoResults );
+	};
+
+	/**
+	 * Show only the items matching with the models in the given list
+	 *
+	 * @param {Object} groupItems An object of items to show
+	 *  arranged by their group names
+	 */
+	mw.rcfilters.ui.FiltersListWidget.prototype.filter = function ( groupItems ) {
+		var i, j, groupName, itemWidgets,
+			groupWidgets = this.getItems(),
+			hasItemWithName = function ( itemArr, name ) {
+				return !!itemArr.filter( function ( item ) {
+					return item.getName() === name;
+				} ).length;
+			};
+
+		if ( $.isEmptyObject( groupItems ) ) {
+			// No results. Hide everything, show only 'no results'
+			// message
+			this.showNoResultsMessage( true );
+			return;
+		}
+
+		this.showNoResultsMessage( false );
+		for ( i = 0; i < groupWidgets.length; i++ ) {
+			groupName = groupWidgets[ i ].getName();
+
+			// Hide or show the entire group if it is in
+			// the filtered groups
+			groupWidgets[ i ].toggle( groupItems[ groupName ] );
+
+			if ( !groupItems[ groupName ] ) {
+				// Continue to next group
+				continue;
+			}
+
+			// We have items to show
+			itemWidgets = groupWidgets[ i ].getItems();
+			for ( j = 0; j < itemWidgets.length; j++ ) {
+				// Only show items that are in the filtered list
+				itemWidgets[ j ].toggle(
+					hasItemWithName( groupItems[ groupName ], itemWidgets[ j ].getName() )
+				);
+			}
+		}
+	};
+
+} )( mediaWiki, jQuery );
