2b3351f1d8077d94c66ac623a4b30910f59636ad
Moriel Schottlender <moriel@gmail.com>
2016-11-07 17:14:13 -0800

    [super-atomic-WIP] Adding filters to RecentChanges
    Filters are to parameters
    Unlike an engineer is to code.
    
    This is a super super super WIP patch, not ready to be reviewed
    or considered. Don't look at it. Seriously, avert your eyes.
    
    Go ahead.
    
    Do it.
    
    You're still looking.
    
    Stop.
    
    Bug: T149435
    Bug: T149452
    Bug: T144448
    Change-Id: Ic545ff1462998b610d7edae59472ecce2e7d51ea

---
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.js
@@ -0,0 +1,3 @@
+( function ( mw ) {
+	mw.rcfilters = { dm: {}, ui: {} };
+}( mediaWiki ) );
---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterCapsuleMultiselectWidget.js
@@ -0,0 +1,92 @@
+( function ( mw, $ ) {
+	/**
+	 * Filter-specific CapsuleMultiselectWidget
+	 *
+	 * @extends OO.ui.CapsuleMultiselectWidget
+	 *
+	 * @constructor
+	 * @param {jQuery} filterInput A filter input that focuses the capsule widget
+	 * @param {Object} config Configuration object
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget = function MwRcfiltersUiFilterCapsuleMultiselectWidget( filterInput, config ) {
+		// Parent
+		mw.rcfilters.ui.FilterCapsuleMultiselectWidget.parent.call( this, config );
+
+		this.filterInput = filterInput;
+
+		this.$content.prepend(
+			$( '<div>' )
+				.addClass( 'mw-rcfilters-ui-filterCapsuleMultiselectWidget-content-title' )
+				.append( mw.msg( 'rcfilters-activefilters' ) )
+		);
+
+		// Events
+		// Add the filterInput as trigger
+		this.filterInput.$input
+			.on( {
+				focus: this.onFocusForPopup.bind( this )
+			} );
+
+		this.$element
+			.addClass( 'mw-rcfilters-ui-filterCapsuleMultiselectWidget' );
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.ui.FilterCapsuleMultiselectWidget, OO.ui.CapsuleMultiselectWidget );
+
+	/* Events */
+
+	/**
+	 * @event remove
+	 * @param {string[]} filters Array of names of removed filters
+	 *
+	 * Filters were removed
+	 */
+
+	/* Methods */
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget.prototype.onFocusForPopup = function () {
+		// Override this method; we don't want to focus on the popup, and we
+		// don't want to bind the size to the handle.
+		if ( !this.isDisabled() ) {
+			this.popup.toggle( true );
+		}
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget.prototype.removeItems = function ( items ) {
+		// Parent
+		mw.rcfilters.ui.FilterCapsuleMultiselectWidget.parent.prototype.removeItems.call( this, items );
+
+		this.emit( 'remove', items.map( function ( item ) { return item.getData(); } ) );
+	};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget.prototype.onKeyDown = function () {};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget.prototype.onPopupFocusOut = function () {};
+
+	/**
+	 * @inheritdoc
+	 */
+	mw.rcfilters.ui.FilterCapsuleMultiselectWidget.prototype.clearInput = function () {
+		if ( this.filterInput ) {
+			this.filterInput.setValue( '' );
+			// this.updateInputSize();
+		}
+		this.menu.toggle( false );
+		this.menu.selectItem();
+		this.menu.highlightItem();
+	};
+}( mediaWiki, jQuery ) );
