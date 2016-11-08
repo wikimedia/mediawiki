062339b383075a0981f62a29fc389b046030abd8
Moriel Schottlender <moriel@gmail.com>
2016-11-07 17:14:13 -0800

    [super-atomic-WIP] Adding filters to RecentChanges
    Filters are to parameters
    Unlike an engineer is to code.
    
    This is a super super super WIP patch, not ready to be reviewed
    or considered. Don't look at it. Seriously, avert your eyes.
    
    Go ahead.
    
    Do it.
    
    You're still looking.
    
    Stop.
    
    Bug: T149435
    Bug: T149452
    Bug: T144448
    Change-Id: Ic545ff1462998b610d7edae59472ecce2e7d51ea

---
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.Controller.js
@@ -0,0 +1,82 @@
+( function ( mw, $ ) {
+	/**
+	 * Controller for the filters in Recent Changes
+	 *
+	 * @param {mw.rcfilters.dm.FiltersViewModel} model View model
+	 */
+	mw.rcfilters.Controller = function MwRcfiltersController( model ) {
+		config = config || {};
+
+		this.model = model;
+
+		// TODO: When we are ready, update the URL when a filter is updated
+		// this.model.connect( this, { itemUpdate: 'updateURL' } );
+	};
+
+	/* Initialization */
+	OO.initClass( mw.rcfilters.Controller );
+
+	/**
+	 * Initialize the filter and parameter states
+	 */
+	mw.rcfilters.Controller.prototype.initialize = function () {
+		var uri = new mw.Uri();
+
+		this.model.updateFilters(
+			// Translate the url params to filter select states
+			this.model.getParametersToFilters( uri.query )
+		);
+
+		this.model.updateParameters( uri.query );
+	};
+
+	/**
+	 * Update the state of a filter
+	 *
+	 * @param {string} filterName Filter name
+	 * @param {boolean} isSelected Filter selected state
+	 */
+	mw.rcfilters.Controller.prototype.updateFilter = function ( filterName, isSelected ) {
+		var obj = {};
+
+		obj[ filterName ] = isSelected;
+		this.model.updateFilters( obj );
+	};
+
+	/**
+	 * Update the URL of the page to reflect current filters
+	 */
+	mw.rcfilters.Controller.prototype.updateURL = function () {
+		var uri = new mw.Uri();
+
+		uri.query = this.makeFiltersQuery();
+
+		// Update the URL itself
+		window.history.pushState( uri.query, '', uri.toString() );
+	};
+
+	/**
+	 * Create the object for the url query based on filter states
+	 *
+	 * @return {Object} Filter query
+	 */
+	mw.rcfilters.Controller.prototype.makeFiltersQuery = function () {
+		var filter,
+			uri = new mw.Uri(),
+			currFilters = this.model.getFiltersToParameters();
+
+		// Translate true/false to integer values
+		for ( filter in currFilters ) {
+			currFilters[ filter ] = Number( currFilters[ filter ] );
+		}
+
+		// Add to existing queries in URL
+		currFilters = $.extend( uri.query, currFilters );
+
+		// TODO: Clean up the list of filters; perhaps 'falsy' filters
+		// shouldn't appear at all? Or compare to existing query string
+		// and see if current state of a specific filter is needed?
+		return currFilters;
+	};
+
+}( mediaWiki, jQuery ) );
