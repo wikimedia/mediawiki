4aa572327d4aaf02ced7ee1e99fa869aead5b99e
Brad Jorsch <bjorsch@wikimedia.org>
2016-11-08 16:29:04 -0500

    Validate getExamplesMessages() query strings
    Assert that they don't begin with 'api.php?'.
    
    Change-Id: Idf5e8e08863a379a37a427a11936e5f9ce567396
    Depends-On: I8a82eecefbb0ba327d8e8bc24ec535bcf40e6429
    Depends-On: I476aad09655a2822381a2c61690b4b0ad423151e

--- tests/phpunit/structure/ApiDocumentationTest.php
+++ tests/phpunit/structure/ApiDocumentationTest.php
@@ -137,6 +137,8 @@ class ApiDocumentationTest extends MediaWikiTestCase {
 
 		// Messages for examples.
 		foreach ( $module->getExamplesMessages() as $qs => $msg ) {
+			$this->assertFalse( (bool)preg_match( '/^api\.php\?/', $qs ),
+				"Query string must not begin with 'api.php?' for example $qs" );
 			$this->checkMessage( $msg, "Example $qs" );
 		}
 	}
