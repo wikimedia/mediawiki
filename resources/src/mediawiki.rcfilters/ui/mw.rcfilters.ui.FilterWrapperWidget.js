367d7aa8885629b55e723b61054754bd70b1f8af
Moriel Schottlender <moriel@gmail.com>
2016-11-07 17:14:13 -0800

    [super-atomic-WIP] Adding filters to RecentChanges
    This is a super super super WIP patch, not ready to be reviewed
    or considered. Don't look at it. Seriously, avert your eyes.
    
    Go ahead.
    
    Do it.
    
    You're still looking.
    
    Stop.
    
    Bug: T149435
    Bug: T149452
    Bug: T144448
    Change-Id: Ic545ff1462998b610d7edae59472ecce2e7d51ea

---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterWrapperWidget.js
@@ -0,0 +1,144 @@
+( function ( mw ) {
+	/**
+	 * List displaying all filter groups
+	 *
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {mw.rcfilters.Controller} controller Controller
+	 * @param {mw.rcfilters.dm.FiltersViewModel} model View model
+	 * @param {Object} config Configuration object
+	 * @cfg {Object} [filters] A definition of the filter groups in this list
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget = function MwRcfiltersUiFilterWrapperWidget( controller, model, config ) {
+		var filter, filterStates,
+			existingFilters = [];
+
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.FilterWrapperWidget.parent.call( this, config );
+		// Mixin constructors
+		OO.ui.mixin.PendingElement.call( this, config );
+
+		this.controller = controller;
+		this.model = model;
+
+		this.filterPopup = new mw.rcfilters.ui.FiltersListWidget(
+			this.controller,
+			this.model,
+			{
+				title: mw.msg( 'rcfilters-filterlist-title' )
+			}
+		);
+
+		this.textInput = new OO.ui.TextInputWidget( {
+			classes: [ 'mw-rcfilters-ui-filterWrapperWidget-search' ],
+			icon: 'search',
+			placeholder: mw.msg( 'rcfilters-search-placeholder' )
+		} );
+
+		this.capsule = new mw.rcfilters.ui.FilterCapsuleMultiselectWidget( this.textInput, {
+			popup: {
+				$content: this.filterPopup.$element,
+				$autoCloseIgnore: this.textInput.$element,
+				classes: [ 'mw-rcfilters-ui-filterWrapperWidget-popup' ]
+			},
+		} );
+
+		// Events
+		this.model.connect( this, {
+			update: 'onModelUpdate',
+			itemUpdate: 'onModelItemUpdate'
+		} );
+		this.textInput.connect( this, {
+			change: 'onTextInputChange'
+		} );
+		this.capsule.connect( this, {
+			add: [ 'onCapsuleChange', true ],
+			remove: [ 'onCapsuleChange', false ]
+		} );
+
+		this.$element
+			.addClass( 'mw-rcfilters-ui-filterWrapperWidget' )
+			.append( this.capsule.$element, this.textInput.$element );
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.ui.FilterWrapperWidget, OO.ui.Widget );
+	OO.mixinClass( mw.rcfilters.ui.FilterWrapperWidget, OO.ui.mixin.PendingElement );
+
+	/**
+	 * Respond to text input change
+	 *
+	 * @param {string} newValue Current value
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onTextInputChange = function ( newValue ) {
+		// Filter the results
+		this.filterPopup.filter( this.model.findMatches( newValue ) );
+	};
+
+	/**
+	 * Respond to change in the capsule filters
+	 *
+	 * @param {boolean} isAdding Filters were added. False for removing filters.
+	 * @param {string[]} item Array of items names
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onCapsuleChange = function ( isAdding, itemNames ) {
+		var i,
+			filterObj = {};
+
+		for ( i =0; i < itemNames.length; i++ ) {
+			filterObj[ itemNames ] = isAdding;
+		}
+
+		this.controller.updateFilters( filterObj );
+	};
+
+	/**
+	 * Respond to model update event and set up the available filters to choose
+	 * from.
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onModelUpdate = function () {
+		var i, filter,
+			selectedFilters = [],
+			items = [],
+			filters = this.model.getItems(),
+			filterStates = this.model.getFiltersState();
+
+		// Insert hidden options for the capsule to get its item data from
+		for ( i = 0; i < filters.length; i++ ) {
+			items.push(
+				new OO.ui.MenuOptionWidget( {
+					data: filters[ i ].getName(),
+					label: filters[ i ].getLabel()
+				} )
+			);
+		}
+
+		this.capsule.getMenu().addItems( items );
+
+		// Add initially-selected filters
+		for ( filter in filterStates ) {
+			if ( filterStates[ filter ] ) {
+				selectedFilters.push( filter );
+			}
+		}
+		this.capsule.addItemsFromData( selectedFilters );
+	};
+
+	/**
+	 * Respond to model item update
+	 *
+	 * @param {mw.rcfilters.dm.FilterItem} item Filter item that was updated
+	 * @param {boolean} isSelected State of the filter
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onModelItemUpdate = function ( item, isSelected ) {
+		if ( isSelected ) {
+			this.capsule.addItemsFromData( [ item.getName() ] );
+		} else {
+			this.capsule.removeItemsFromData( [ item.getName() ] );
+		}
+	};
+} )( mediaWiki );
