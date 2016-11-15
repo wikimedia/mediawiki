664fe97a90f9dcfedbfbb76faf54d7af65546dae
Erik Bernhardson <ebernhardson@wikimedia.org>
2016-11-14 16:14:31 -0800

    [WIP] Pull rendering of single result out of SpecialSearch
    First step in refactoring the search results page. Pulls out widgets
    to render our two styles of search result, Full and Simple.
    
    TODO:
    * Need to configure textcat and make sure those still render correctly
    
    Bug: T150390
    Change-Id: If78cb0c29ae394f16e465c15a8e8246c1b56dcea

---
+++ includes/widget/SimpleSearchResultWidget.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MediaWiki\Widget;
+
+use Linker;
+use SearchResult;
+use SpecialSearch;
+use Title;
+
+/**
+ * Renders a simple one-line result
+ */
+class SimpleSearchResultWidget implements SearchResultWidget {
+	/** @var SpecialSearch */
+	protected $specialSearch;
+
+	public function __construct( SpecialSearch $specialSearch ) {
+		$this->specialSearch = $specialSearch;
+	}
+
+	/**
+	 * @param SearchResult $result The result to render
+	 * @param string $terms Terms to be highlighted (@see SearchResult::getTextSnippet)
+	 * @param int $position The result position, including offset
+	 * @return string HTML
+	 */
+	public function render( SearchResult $result, $terms, $position ) {
+		$title = $result->getTitle();
+		$titleSnippet = $result->getTitleSnippet();
+		if ( $titleSnippet === '' ) {
+			$titleSnippet = null;
+		}
+
+		$link = Linker::linkKnown( $title, $titleSnippet );
+
+		$redirectTitle = $result->getRedirectTitle();
+		$redirect = '';
+		if ( $redirectTitle !== null ) {
+			$redirectText = $result->getRedirectSnippet() ?: null;
+			$redirect =
+				"<span class='searchalttitle'>" .
+					$this->specialSearch->msg( 'search-redirect' )->rawParams(
+						Linker::linkKnown( $redirectTitle, $redirectText )
+					)->text() .
+				"</span>";
+		}
+
+		return "<li>{$link} {$redirect}</li>";
+	}
+}
