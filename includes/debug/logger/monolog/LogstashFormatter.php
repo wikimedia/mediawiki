5347fb2aa4496eb12d82f503768d32e66c3840ae
Gergő Tisza <gtisza@wikimedia.org>
2016-11-23 01:01:07 +0000

    Rewrite logstash key conflict warning from I6677dbf6
    * do not warn if something is overwritten with an identical value
      (happens a lot with 'ip')
    * move to LogstashFormatter so we can check for the value
    * instead of spamming errors, just add a flag to the logstash data
    
    Bug: T145133
    Change-Id: I31caee865cd60c785126478ac75c9aefce78eaaf

---
+++ includes/debug/logger/monolog/LogstashFormatter.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace MediaWiki\Logger\Monolog;
+
+class LogstashFormatter extends \Monolog\Formatter\LogstashFormatter {
+	/** @var array Keys which should not be used in log context */
+	protected $reservedKeys = [
+		// from LogstashFormatter
+		'message', 'channel', 'level', 'type',
+		// from WebProcessor
+		'url', 'ip', 'http_method', 'server', 'referrer',
+		// from WikiProcessor
+		'host', 'wiki', 'reqId', 'mwversion',
+		// from config magic
+		'normalized_message',
+	];
+
+	/**
+	 * Prevent key conflicts
+	 * @param array $record
+	 * @return array
+	 */
+	protected function formatV0( array $record ) {
+		if ( $this->contextPrefix ) {
+			return parent::formatV0( $record );
+		}
+
+		$context = !empty( $record['context'] ) ? $record['context'] : [];
+		$record['context'] = [];
+		$message = parent::formatV0( $record );
+
+		$fields = &$message['@fields'];
+		$this->fixKeyConflicts( $fields, $context );
+		return $message;
+	}
+
+	/**
+	 * Prevent key conflicts
+	 * @param array $record
+	 * @return array
+	 */
+	protected function formatV1( array $record ) {
+		if ( $this->contextPrefix ) {
+			return parent::formatV1( $record );
+		}
+
+		$context = !empty( $record['context'] ) ? $record['context'] : [];
+		$record['context'] = [];
+		$message = parent::formatV1( $record );
+
+		$this->fixKeyConflicts( $message, $context );
+		return $message;
+	}
+
+	/**
+	 * Check whether some context field would overwrite another message key. If so, rename
+	 * and flag. (T145133)
+	 * @param array $message
+	 */
+	protected function fixKeyConflicts( array &$message, array &$context ) {
+		foreach ( $context as $key => $val ) {
+			if (
+				in_array( $key, $this->reservedKeys, true ) &&
+				isset( $message[$key] ) && $message[$key] !== $val
+			) {
+				$message['logstash_formatter_key_conflict'][] = $key;
+				$key = 'c_' . $val;
+			}
+			$message[$key] = $val;
+		}
+	}
+}
