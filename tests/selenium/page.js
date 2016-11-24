e31e2be67ed7620b5f53f3f7b2216a610fee2617
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-24 16:35:58 +0100

    The first simple Selenium tests in Node.js
    Bug: T139740
    Change-Id: Idbc3c2aad266b3b7ea363d41718a94fc9a8c951b

---
+++ tests/selenium/page.js
@@ -0,0 +1,84 @@
+/* eslint-env es6, mocha, node */
+var assert = require( 'assert' ),
+	config = require( 'config' ),
+	baseUrl = config.get( 'baseUrl' ),
+	webdriver = require( 'selenium-webdriver' ),
+	By = webdriver.By,
+	test = require( 'selenium-webdriver/testing' );
+
+test.describe( 'Page', function () {
+
+	var driver,
+		helper = require( './helper' );
+
+	test.beforeEach( function () {
+		driver = helper.browser();
+	} );
+
+	test.afterEach( function () {
+		helper.screenshot( driver, this.currentTest.state, this.currentTest.fullTitle() );
+		driver.quit();
+	} );
+
+	test.it( 'should be creatable', function () {
+		let name = Math.random().toString(),
+			content = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + name + '&action=edit' );
+		driver.findElement( By.css( '#wpTextbox1' ) ).sendKeys( content );
+		driver.findElement( By.css( '#wpSave' ) ).click();
+
+		// check name
+		driver.findElement( By.css( '#firstHeading' ) ).getText().then( function ( text ) {
+			assert.equal( text, name );
+		} );
+
+		// check content
+		driver.findElement( By.css( '#mw-content-text' ) ).getText().then( function ( text ) {
+			assert.equal( text, content );
+		} );
+
+	} );
+
+	test.it( 'should be editable', function () {
+		let name = Math.random().toString(),
+			content1 = Math.random().toString(),
+			content2 = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + name + '&action=edit' );
+		driver.findElement( By.css( '#wpTextbox1' ) ).sendKeys( content1 );
+		driver.findElement( By.css( '#wpSave' ) ).click();
+
+		// edit
+		driver.get( baseUrl + name + '&action=edit' );
+		driver.findElement( By.css( '#wpTextbox1' ) ).clear();
+		driver.findElement( By.css( '#wpTextbox1' ) ).sendKeys( content2 );
+		driver.findElement( By.css( '#wpSave' ) ).click();
+
+		// check content
+		driver.findElement( By.css( '#mw-content-text' ) ).getText().then( function ( text ) {
+			assert.equal( text, content2 );
+		} );
+
+	} );
+
+	test.it( 'should have history', function () {
+		let name = Math.random().toString(),
+			content = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + name + '&action=edit' );
+		driver.findElement( By.css( '#wpTextbox1' ) ).sendKeys( content );
+		driver.findElement( By.css( '#wpSave' ) ).click();
+
+		// check history
+		driver.get( baseUrl + name + '&action=history' );
+		driver.findElement( By.css( '#pagehistory .comment' ) ).getText().then( function ( text ) {
+			assert.equal( text, '(Created page with "' + content + '")' );
+		} );
+
+	} );
+
+} );
