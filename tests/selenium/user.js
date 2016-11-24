1c5017702228becdc1cc14bddfda9bdcfb6da79f
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-24 16:35:58 +0100

    The first simple Selenium tests in Node.js
    Bug: T139740
    Change-Id: Idbc3c2aad266b3b7ea363d41718a94fc9a8c951b

---
+++ tests/selenium/user.js
@@ -0,0 +1,62 @@
+/* eslint-env es6, mocha, node */
+var assert = require( 'assert' ),
+	config = require( 'config' ),
+	baseUrl = config.get( 'baseUrl' ),
+	password = config.get( 'password' ),
+	username = config.get( 'username' ),
+	webdriver = require( 'selenium-webdriver' ),
+	By = webdriver.By,
+	test = require( 'selenium-webdriver/testing' );
+
+test.describe( 'User', function () {
+
+	var driver,
+		helper = require( './helper' );
+
+	test.beforeEach( function () {
+		driver = helper.browser();
+	} );
+
+	test.afterEach( function () {
+		helper.screenshot( driver, this.currentTest.state, this.currentTest.fullTitle() );
+		driver.quit();
+	} );
+
+	test.it( 'should be able to create account', function () {
+		let username = Math.random().toString(),
+			password = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + 'Special:CreateAccount' );
+		driver.findElement( By.css( '#wpName2' ) ).sendKeys( username );
+		driver.findElement( By.css( '#wpPassword2' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpRetype' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpCreateaccount' ) ).click();
+
+		// check
+		driver.findElement( By.css( '#firstHeading' ) ).getText().then( function ( text ) {
+			assert.equal( text, 'Welcome, ' + username + '!' );
+		} );
+
+	} );
+
+	test.it( 'should be able to log in', function () {
+		driver.get( baseUrl + 'Special:UserLogin' );
+		driver.findElement( By.id( 'wpLoginAttempt' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+
+	test.it( 'should be able to change preferences', function () {
+		driver.get( baseUrl + 'Special:UserLogin' );
+		driver.findElement( By.id( 'wpName1' ) ).sendKeys( username );
+		driver.findElement( By.id( 'wpPassword1' ) ).sendKeys( password );
+		driver.findElement( By.id( 'wpLoginAttempt' ) ).click();
+
+		driver.get( baseUrl + 'Special:Preferences' );
+		driver.findElement( By.id( 'prefcontrol' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+
+} );
