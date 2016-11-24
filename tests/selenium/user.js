1ea4fab8be38f9d75cbdd9fa6f98ed9c46b9767e
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-24 16:35:58 +0100

    The first simple Selenium tests in Node.js
    Bug: T139740
    Change-Id: Idbc3c2aad266b3b7ea363d41718a94fc9a8c951b

---
+++ tests/selenium/user.js
@@ -0,0 +1,94 @@
+/* eslint-env es6, mocha, node */
+var assert = require( 'assert' ),
+	config = require( 'config' ),
+	baseUrl = config.get( 'baseUrl' ),
+	webdriver = require( 'selenium-webdriver' ),
+	By = webdriver.By,
+	test = require( 'selenium-webdriver/testing' );
+
+test.describe( 'User', function () {
+
+	var driver,
+		helper = require( './helper' );
+
+	test.beforeEach( function () {
+		driver = helper.browser();
+	} );
+
+	test.afterEach( function () {
+		helper.screenshot( driver, this.currentTest.state, this.currentTest.fullTitle() );
+		driver.quit();
+	} );
+
+	test.it( 'should be able to create account', function () {
+		let username = Math.random().toString(),
+			password = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + 'Special:CreateAccount' );
+		driver.findElement( By.css( '#wpName2' ) ).sendKeys( username );
+		driver.findElement( By.css( '#wpPassword2' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpRetype' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpCreateaccount' ) ).click();
+
+		// check
+		driver.findElement( By.css( '#firstHeading' ) ).getText().then( function ( text ) {
+			assert.equal( text, 'Welcome, ' + username + '!' );
+		} );
+
+	} );
+
+	test.it( 'should be able to log in', function () {
+		let username = Math.random().toString(),
+			password = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + 'Special:CreateAccount' );
+		driver.findElement( By.css( '#wpName2' ) ).sendKeys( username );
+		driver.findElement( By.css( '#wpPassword2' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpRetype' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpCreateaccount' ) ).click();
+
+		// logout
+		driver.get( baseUrl + 'Special:UserLogout' );
+
+		// log in
+		driver.get( baseUrl + 'Special:UserLogin' );
+		driver.findElement( By.css( '#wpName1' ) ).clear();
+		driver.findElement( By.css( '#wpName1' ) ).sendKeys( username );
+		driver.findElement( By.css( '#wpPassword1' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpLoginAttempt' ) ).click();
+
+		// check
+		driver.findElement( By.css( '#pt-userpage' ) ).getText().then( function ( text ) {
+			assert.equal( text, username );
+		} );
+
+	} );
+
+	test.it( 'should be able to change preferences', function () {
+		let username = Math.random().toString(),
+			password = Math.random().toString(),
+			realName = Math.random().toString();
+
+		// create
+		driver.get( baseUrl + 'Special:CreateAccount' );
+		driver.findElement( By.css( '#wpName2' ) ).sendKeys( username );
+		driver.findElement( By.css( '#wpPassword2' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpRetype' ) ).sendKeys( password );
+		driver.findElement( By.css( '#wpCreateaccount' ) ).click();
+
+		// change real name
+		driver.get( baseUrl + 'Special:Preferences' );
+		driver.findElement( By.css( '#mw-input-wprealname' ) ).clear();
+		driver.findElement( By.css( '#mw-input-wprealname' ) ).sendKeys( realName );
+		driver.findElement( By.css( '#prefcontrol' ) ).click();
+
+		// check
+		driver.findElement( By.css( '#mw-input-wprealname' ) ).getAttribute( 'value' ).then( function ( text ) {
+			assert.equal( text, realName );
+		} );
+
+	} );
+
+} );
