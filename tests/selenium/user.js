b0e19ff91d36f13175c4ec0036cc8fbb0752dec3
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-24 16:35:58 +0100

    WIP Port Selenium tests from Ruby to Node.js
    Bug: T139740
    Change-Id: Idbc3c2aad266b3b7ea363d41718a94fc9a8c951b

---
+++ tests/selenium/user.js
@@ -0,0 +1,51 @@
+var assert = require( 'assert' ),
+	webdriver = require( 'selenium-webdriver' ),
+	By = webdriver.By,
+	test = require( 'selenium-webdriver/testing' );
+
+test.describe( 'User', function () {
+	var baseUrl, driver;
+
+	test.before( function () {
+		baseUrl = process.env.MW_SERVER + process.env.MW_SCRIPT_PATH + '/';
+	} );
+
+	test.beforeEach( function () {
+		driver = new webdriver.Builder()
+		.forBrowser( 'firefox' )
+		.build();
+	} );
+
+	test.afterEach( function () {
+		driver.takeScreenshot().then((image) => {
+			require('fs').writeFile('log/' + this.currentTest.title.replace(' ', '-') + '.png', image, 'base64');
+		});
+		driver.quit();
+	} );
+
+	test.it( 'should be able to create account', function () {
+		driver.get( baseUrl + 'Special:CreateAccount' );
+		driver.findElement( By.id( 'wpCreateaccount' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+
+	test.it( 'should be able to log in', function () {
+		driver.get( baseUrl + 'Special:UserLogin' );
+		driver.findElement( By.id( 'wpLoginAttempt' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+
+	test.it( 'should be able to change preferences', function () {
+		driver.get( baseUrl + 'Special:UserLogin' );
+		driver.findElement( By.id( 'wpName1' ) ).sendKeys( 'Admin' );
+		driver.findElement( By.id( 'wpPassword1' ) ).sendKeys( 'vagrant' );
+		driver.findElement( By.id( 'wpLoginAttempt' ) ).click();
+
+		driver.get( baseUrl + 'Special:Preferences' );
+		driver.findElement( By.id( 'prefcontrol' ) ).isDisplayed().then( function ( displayed ) {
+			assert( displayed );
+		} );
+	} );
+} );
