b225a84ff95badf1e407c3952e1e0c540507a8e8
Bartosz Dziewoński <matma.rex@gmail.com>
2016-11-30 20:49:31 +0100

    DatabaseUpdater: Stop inserting junk into 'updatelog' every time the updater runs
    Nothing ever looks at these rows, and if you have update.php in a Cron
    job or a Git hook or something, they can accumulate in huge numbers.
    
    Also remove a test which verified that the rows get inserted.
    
    If you reached this place while trying to find out what the weird
    "updatelist-1.28.0-14628929450" rows in your database are, rest
    assured they are useless. If they're causing disk space problems,
    you can delete them:
    
      DELETE FROM updatelog WHERE ul_key LIKE 'updatelist-%';
    
    Change-Id: If94f243810e82ea3724cbd47b301bb96ba9a49d9

--- includes/installer/DatabaseUpdater.php
+++ includes/installer/DatabaseUpdater.php
@@ -32,8 +32,6 @@ require_once __DIR__ . '/../../maintenance/Maintenance.php';
  * @since 1.17
  */
 abstract class DatabaseUpdater {
-	protected static $updateCounter = 0;
-
 	/**
 	 * Array of updates to perform on the database
 	 *
@@ -423,8 +421,6 @@ abstract class DatabaseUpdater {
 	 * @param array $what What updates to perform
 	 */
 	public function doUpdates( $what = [ 'core', 'extensions', 'stats' ] ) {
-		global $wgVersion;
-
 		$this->db->setSchemaVars( $this->getSchemaVars() );
 
 		$what = array_flip( $what );
@@ -441,12 +437,9 @@ abstract class DatabaseUpdater {
 			$this->checkStats();
 		}
 
-		$this->setAppliedUpdates( $wgVersion, $this->updates );
-
 		if ( $this->fileHandle ) {
 			$this->skipSchema = false;
 			$this->writeSchemaUpdateFile();
-			$this->setAppliedUpdates( "$wgVersion-schema", $this->updatesSkipped );
 		}
 	}
 
@@ -480,23 +473,6 @@ abstract class DatabaseUpdater {
 		}
 		$this->updatesSkipped = array_merge( $this->updatesSkipped, $updatesSkipped );
 		$this->updates = array_merge( $this->updates, $updatesDone );
-	}
-
-	/**
-	 * @param string $version
-	 * @param array $updates
-	 */
-	protected function setAppliedUpdates( $version, $updates = [] ) {
-		$this->db->clearFlag( DBO_DDLMODE );
-		if ( !$this->canUseNewUpdatelog() ) {
-			return;
-		}
-		$key = "updatelist-$version-" . time() . self::$updateCounter;
-		self::$updateCounter++;
-		$this->db->insert( 'updatelog',
-			[ 'ul_key' => $key, 'ul_value' => serialize( $updates ) ],
-			__METHOD__ );
-		$this->db->setFlag( DBO_DDLMODE );
 	}
 
 	/**
