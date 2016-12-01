04af022801030ed688b5c582fa98bd28ff576ec5
Brad Jorsch <bjorsch@wikimedia.org>
2016-12-01 18:04:21 -0500

    Update account creation form validation
    Use the cancreateerror returned from list=users&usprop=cancreate for
    username validation.
    
    Use the new action=validatepassword to validate entered passwords.
    
    This also injects the resulting errors in the style of HTMLForm's field
    validation rather than at the top of the form.
    
    Change-Id: Ie8c1270eb605367556fe36b0b2080eb3f957dc54

--- resources/Resources.php
+++ resources/Resources.php
@@ -1078,6 +1078,12 @@ return [
 		],
 		'targets' => [ 'desktop', 'mobile' ],
 	],
+	'mediawiki.htmlform.checker' => [
+		'scripts' => [
+			'resources/src/mediawiki/htmlform/htmlform.Checker.js',
+		],
+		'targets' => [ 'desktop', 'mobile' ],
+	],
 	'mediawiki.htmlform.ooui' => [
 		'scripts' => [
 			'resources/src/mediawiki/htmlform/htmlform.Element.js',
@@ -1996,7 +2002,6 @@ return [
 	'mediawiki.special.userlogin.signup.js' => [
 		'scripts' => 'resources/src/mediawiki.special/mediawiki.special.userlogin.signup.js',
 		'messages' => [
-			'createacct-error',
 			'createacct-emailrequired',
 			'noname',
 			'userexists',
@@ -2005,6 +2010,7 @@ return [
 			'mediawiki.api',
 			'mediawiki.jqueryMsg',
 			'jquery.throttle-debounce',
+			'mediawiki.htmlform.checker',
 		],
 	],
 	'mediawiki.special.unwatchedPages' => [
