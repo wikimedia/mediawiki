6be2ae857b41dd25f7c34b8a7fe1fa87acadfc90
Roan Kattouw <roan.kattouw@gmail.com>
2016-12-01 13:57:09 -0800

    Fix query error to due wrong index name
    Change-Id: I8254a34733e668ce7c8eb2a022dd88e36c4258aa
    Error: 1176 Key 'usertext_timestamp' doesn't exist in table 'archive

--- includes/specials/pagers/DeletedContribsPager.php
+++ includes/specials/pagers/DeletedContribsPager.php
@@ -125,7 +125,7 @@ class DeletedContribsPager extends IndexPager {
 		$condition = [];
 
 		$condition['ar_user_text'] = $this->target;
-		$index = 'usertext_timestamp';
+		$index = 'ar_usertext_timestamp';
 
 		return [ $index, $condition ];
 	}
