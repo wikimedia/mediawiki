a4ac0eba281d58787b07444a50fc3a85ffac1abc
rlot <rj23@protonmail.com>
2016-12-02 16:43:12 +0100

    Replace Linker::link() usage with LinkRenderer
    Replaced deprecated Linker:link() with LinkRenderer
    
    Bug: T149346
    Change-Id: Iab158d49b186835a14c357c91a1c5c4f78986238

--- includes/specials/SpecialPrefixindex.php
+++ includes/specials/SpecialPrefixindex.php
@@ -228,9 +228,9 @@ class SpecialPrefixindex extends SpecialAllPages {
 						$displayed = substr( $displayed, $prefixLength );
 					}
 					$link = ( $title->isRedirect() ? '<div class="allpagesredirect">' : '' ) .
-						Linker::linkKnown(
+						$this->getLinkRenderer()->makeKnownLink(
 							$title,
-							htmlspecialchars( $displayed )
+							$displayed
 						) .
 						( $title->isRedirect() ? '</div>' : '' );
 
@@ -275,9 +275,9 @@ class SpecialPrefixindex extends SpecialAllPages {
 				$query['namespace'] = $namespace;
 			}
 
-			$nextLink = Linker::linkKnown(
+			$nextLink = $this->getLinkRenderer()->makeKnownLink(
 				$this->getPageTitle(),
-				$this->msg( 'nextpage', str_replace( '_', ' ', $nextRow->page_title ) )->escaped(),
+				$this->msg( 'nextpage', str_replace( '_', ' ', $nextRow->page_title ) )->text(),
 				[],
 				$query
 			);
--- includes/specials/SpecialRevisiondelete.php
+++ includes/specials/SpecialRevisiondelete.php
@@ -241,32 +241,33 @@ class SpecialRevisionDelete extends UnlistedSpecialPage {
 	 * Show some useful links in the subtitle
 	 */
 	protected function showConvenienceLinks() {
+		$linkRenderer = $this->getLinkRenderer();
 		# Give a link to the logs/hist for this page
 		if ( $this->targetObj ) {
 			// Also set header tabs to be for the target.
 			$this->getSkin()->setRelevantTitle( $this->targetObj );
 
 			$links = [];
-			$links[] = Linker::linkKnown(
+			$links[] = $linkRenderer->makeKnownLink(
 				SpecialPage::getTitleFor( 'Log' ),
-				$this->msg( 'viewpagelogs' )->escaped(),
+				$this->msg( 'viewpagelogs' )->text(),
 				[],
 				[ 'page' => $this->targetObj->getPrefixedText() ]
 			);
 			if ( !$this->targetObj->isSpecialPage() ) {
 				# Give a link to the page history
-				$links[] = Linker::linkKnown(
+				$links[] = $linkRenderer->makeKnownLink(
 					$this->targetObj,
-					$this->msg( 'pagehist' )->escaped(),
+					$this->msg( 'pagehist' )->text(),
 					[],
 					[ 'action' => 'history' ]
 				);
 				# Link to deleted edits
 				if ( $this->getUser()->isAllowed( 'undelete' ) ) {
 					$undelete = SpecialPage::getTitleFor( 'Undelete' );
-					$links[] = Linker::linkKnown(
+					$links[] = $linkRenderer->makeKnownLink(
 						$undelete,
-						$this->msg( 'deletedhist' )->escaped(),
+						$this->msg( 'deletedhist' )->text(),
 						[],
 						[ 'target' => $this->targetObj->getPrefixedDBkey() ]
 					);
