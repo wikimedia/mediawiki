6551f541a06e27a866671e2808fbcac921562c71
Kunal Mehta <legoktm@member.fsf.org>
2016-12-01 22:02:28 -0800

    registration: Move attributes out of the top level
    This moves attributes out of the top level, and namespaces them under
    each extension. If the extension that it belongs to is not installed,
    the attribute is not exported and dropped.
    
    The full name of the attribute is the name of the extension plus the
    name of the attribute key. This enforces the recommendation that the
    attribute name start with the extension's name.
    
    Add test coverage for attributes under manifest_version 1 and 2.
    
    Bug: T133627
    Depends-On: I5a148763f68989c8da313a4fb1d0213658ee4495
    Change-Id: I8613a027c56e2c9d2c6a83ca14749eb1c8fc23be

--- tests/phpunit/includes/registration/ExtensionProcessorTest.php
+++ tests/phpunit/includes/registration/ExtensionProcessorTest.php
@@ -445,6 +445,72 @@ class ExtensionProcessorTest extends MediaWikiTestCase {
 		];
 	}
 
+	/**
+	 * Attributes under manifest_version 2
+	 *
+	 * @covers ExtensionProcessor::extractAttributes
+	 * @covers ExtensionProcessor::getExtractedInfo
+	 */
+	public function testExtractAttributes() {
+		$processor = new ExtensionProcessor();
+		// Load FooBar extension
+		$processor->extractInfo( $this->dir, [ 'name' => 'FooBar' ], 2 );
+		$processor->extractInfo(
+			$this->dir,
+			[
+				'name' => 'Baz',
+				'attributes' => [
+					// Loaded
+					'FooBar' => [
+						'Plugins' => [
+							'ext.baz.foobar',
+						],
+					],
+					// Not loaded
+					'FizzBuzz' => [
+						'MorePlugins' => [
+							'ext.baz.fizzbuzz',
+						],
+					],
+				],
+			],
+			2
+		);
+
+		$info = $processor->getExtractedInfo();
+		$this->assertArrayHasKey( 'FooBarPlugins', $info['attributes'] );
+		$this->assertSame( [ 'ext.baz.foobar' ], $info['attributes']['FooBarPlugins'] );
+		$this->assertArrayNotHasKey( 'FizzBuzzMorePlugins', $info['attributes'] );
+	}
+
+	/**
+	 * Attributes under manifest_version 1
+	 *
+	 * @covers ExtensionProcessor::extractInfo
+	 */
+	public function testAttributes1() {
+		$processor = new ExtensionProcessor();
+		$processor->extractInfo(
+			$this->dir,
+			[
+				'name' => 'FooBar',
+				'FooBarPlugins' => [
+					'ext.baz.foobar',
+				],
+				'FizzBuzzMorePlugins' => [
+					'ext.baz.fizzbuzz',
+				],
+			],
+			1
+		);
+
+		$info = $processor->getExtractedInfo();
+		$this->assertArrayHasKey( 'FooBarPlugins', $info['attributes'] );
+		$this->assertSame( [ 'ext.baz.foobar' ], $info['attributes']['FooBarPlugins'] );
+		$this->assertArrayHasKey( 'FizzBuzzMorePlugins', $info['attributes'] );
+		$this->assertSame( [ 'ext.baz.fizzbuzz' ], $info['attributes']['FizzBuzzMorePlugins'] );
+	}
+
 	public function testGlobalSettingsDocumentedInSchema() {
 		global $IP;
 		$globalSettings = TestingAccessWrapper::newFromClass(
