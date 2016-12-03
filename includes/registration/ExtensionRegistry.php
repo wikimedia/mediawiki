2e998f38a8b58a7e2842880f915b189d211e0bd7
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-03 18:46:48 +0100

    registration: Don't let extensions load lately
    ExtensionRegistry now offers a way to prevent loading any extension
    after the queue was marked as finish. This allows us to have a
    defined amount of extensions loaded at a specific time, the set of
    extensions isn't allowed to be changed later. This is a requirement
    for extensions/skins that depends on other extensions, so that
    dependencies can't be loaded after the Setup is already finished.
    
    Bug: T117277
    Change-Id: I09b306bd6f6ccf4210f36be0118e7f17f2c3d264

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -60,6 +60,13 @@ class ExtensionRegistry {
 	protected $queued = [];
 
 	/**
+	 * Whether we are done loading things
+	 *
+	 * @var bool
+	 */
+	private $finished = false;
+
+	/**
 	 * Items in the JSON file that aren't being
 	 * set as globals
 	 *
@@ -114,10 +121,20 @@ class ExtensionRegistry {
 		$this->queued[$path] = $mtime;
 	}
 
+	/**
+         * @throws MWException If the queue is already marked as finished (no further things should
+         *  be loaded then).
+         */
 	public function loadFromQueue() {
 		global $wgVersion;
 		if ( !$this->queued ) {
 			return;
+		}
+
+		if ( $this->finished ) {
+			throw new MWException(
+				"The following paths tried to load late: " . implode( ', ', $this->queued )
+			);
 		}
 
 		// A few more things to vary the cache on
@@ -162,6 +179,15 @@ class ExtensionRegistry {
 	 */
 	public function clearQueue() {
 		$this->queued = [];
+	}
+
+	/**
+	 * After this is called, no more extensions can be loaded
+	 *
+	 * @since 1.29
+	 */
+	public function finish() {
+		$this->finished = true;
 	}
 
 	/**
