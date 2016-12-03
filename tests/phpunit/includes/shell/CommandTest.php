926c97c69fb0a07d1e6dc72626de2fe73648c206
Max Semenik <maxsem.wiki@gmail.com>
2016-12-02 16:12:59 -0800

    Return stderr from Shell\Command
    Change-Id: I5551ae4bbe7b539b528a734aa82198b11f103871

--- tests/phpunit/includes/shell/CommandTest.php
+++ tests/phpunit/includes/shell/CommandTest.php
@@ -57,23 +57,61 @@ class CommandTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame( "bar\n", $result->getStdout() );
 	}
 
+	public function testStdout() {
+		$this->requirePosix();
+
+		$command = new Command();
+
+		$result = $command
+			->params( 'bash', '-c', 'echo ThisIsStderr 1>&2' )
+			->execute();
+
+		$this->assertNotContains( 'ThisIsStderr', $result->getStdout() );
+		$this->assertEquals( "ThisIsStderr\n", $result->getStderr() );
+	}
+
+	public function testStdoutRedirection() {
+		$this->requirePosix();
+
+		$command = new Command();
+
+		$result = $command
+			->params( 'bash', '-c', 'echo ThisIsStderr 1>&2' )
+			->includeStderr( true )
+			->execute();
+
+		$this->assertEquals( "ThisIsStderr\n", $result->getStdout() );
+		$this->assertNull( $result->getStderr() );
+	}
+
 	public function testOutput() {
 		global $IP;
 
 		$this->requirePosix();
+		chdir( $IP );
 
 		$command = new Command();
 		$result = $command
-			->params( [ 'ls', "$IP/index.php" ] )
+			->params( [ 'ls', 'index.php' ] )
 			->execute();
-		$this->assertSame( "$IP/index.php", trim( $result->getStdout() ) );
+		$this->assertRegExp( '/^index.php$/m', $result->getStdout() );
+		$this->assertSame( null, $result->getStderr() );
 
 		$command = new Command();
 		$result = $command
 			->params( [ 'ls', 'index.php', 'no-such-file' ] )
 			->includeStderr()
 			->execute();
+		$this->assertRegExp( '/^index.php$/m', $result->getStdout() );
 		$this->assertRegExp( '/^.+no-such-file.*$/m', $result->getStdout() );
+		$this->assertSame( null, $result->getStderr() );
+
+		$command = new Command();
+		$result = $command
+			->params( [ 'ls', 'index.php', 'no-such-file' ] )
+			->execute();
+		$this->assertRegExp( '/^index.php$/m', $result->getStdout() );
+		$this->assertRegExp( '/^.+no-such-file.*$/m', $result->getStderr() );
 	}
 
 	public function testT69870() {
