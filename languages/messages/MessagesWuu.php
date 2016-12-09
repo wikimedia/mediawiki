3fd91fc7056494b25ad9605675870c5d18aab924
MarcoAurelio <strigiwm@gmail.com>
2016-12-09 11:21:34 +0100

    Update date format for Wu language
    Bug: T125047
    Change-Id: I398d93d372ad0d5345b0e4b27657ad69b0079d94

--- languages/messages/MessagesWuu.php
+++ languages/messages/MessagesWuu.php
@@ -19,3 +19,15 @@
 
 $fallback = 'zh-hans';
 
+$datePreferences = [
+	'default',
+	'ISO 8601',
+];
+
+$defaultDateFormat = 'wuu';
+
+$dateFormats = [
+	'wuu time'            => 'H:i',
+	'wuu date'            => 'Y年n月j号 (D)',
+	'wuu both'            => 'Y年n月j号 (D) H:i',
+];
