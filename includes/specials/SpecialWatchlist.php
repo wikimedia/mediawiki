326244e1493acf071887b796b4506470d89ed441
Geoffrey Mon <geofbot@gmail.com>
2016-12-09 20:58:57 -0500

    Move Special:Watchlist auto-reload check from PHP to JS
    This makes the watchlist js load all the time, instead of only
    if the auto-reload preference is set, so we can put other things
    in the same js module.
    
    Bug: T150045
    Change-Id: Ib8acca39593fe3d2369dc3187a9a55413553843d

--- includes/specials/SpecialWatchlist.php
+++ includes/specials/SpecialWatchlist.php
@@ -421,11 +421,7 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		$user = $this->getUser();
 		$out = $this->getOutput();
 
-		// if the user wishes, that the watchlist is reloaded, whenever a filter changes,
-		// add the module for that
-		if ( $user->getBoolOption( 'watchlistreloadautomatically' ) ) {
-			$out->addModules( [ 'mediawiki.special.watchlist' ] );
-		}
+		$out->addModules( [ 'mediawiki.special.watchlist' ] );
 
 		$out->addSubtitle(
 			$this->msg( 'watchlistfor2', $user->getName() )
