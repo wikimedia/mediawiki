998608cb69bec822b8cea947c055de2750b8c66c
Moriel Schottlender <moriel@gmail.com>
2016-12-09 16:18:59 -0800

    RCFilters UI: Add 'remove' and 'restore defaults' to filter list
    Bug: T144448
    Bug: T149391
    Change-Id: I418ad6b34ae8a7456a7e66bc703d4dccf36764a5

--- resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.FiltersViewModel.js
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.FiltersViewModel.js
@@ -14,6 +14,7 @@
 
 		this.groups = {};
 		this.defaultParams = {};
+		this.defaultFiltersEmpty = null;
 
 		// Events
 		this.aggregate( { update: 'itemUpdate' } );
@@ -253,6 +254,39 @@
 	};
 
 	/**
+	 * Check whether the current filter state is set to all false.
+	 *
+	 * @return {boolean} Current filters are all empty
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.areCurrentFiltersEmpty = function () {
+		var currFilters = this.getState();
+
+		return Object.keys( currFilters ).every( function ( filterName ) {
+			return !currFilters[ filterName ];
+		} );
+	};
+
+	/**
+	 * Check whether the default values of the filters are all false.
+	 *
+	 * @return {boolean} Default filters are all false
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.areDefaultFiltersEmpty = function () {
+		var defaultFilters;
+
+		if ( this.defaultFiltersEmpty !== null ) {
+			// We only need to do this test once,
+			// because defaults are set once per session
+			defaultFilters = this.getFiltersFromParameters();
+			this.defaultFiltersEmpty = Object.keys( defaultFilters ).every( function ( filterName ) {
+				return !defaultFilters[ filterName ];
+			} );
+		}
+
+		return this.defaultFiltersEmpty;
+	};
+
+	/**
 	 * This is the opposite of the #getParametersFromFilters method; this goes over
 	 * the given parameters and translates into a selected/unselected value in the filters.
 	 *
@@ -343,6 +377,21 @@
 		return this.getItems().filter( function ( item ) {
 			return name === item.getName();
 		} )[ 0 ];
+	};
+
+	/**
+	 * Set all filters to false or empty/all
+	 * This is equivalent to display all.
+	 */
+	mw.rcfilters.dm.FiltersViewModel.prototype.emptyAllFilters = function () {
+		var filters = {};
+
+		this.getItems().forEach( function ( filterItem ) {
+			filters[ filterItem.getName() ] = false;
+		} );
+
+		// Update filters
+		this.updateFilters( filters );
 	};
 
 	/**
