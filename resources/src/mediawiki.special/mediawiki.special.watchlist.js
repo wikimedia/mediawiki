c7e2e161f47ea15f98a706741caf24ac0b352bd8
Geoffrey Mon <geofbot@gmail.com>
2016-12-09 22:06:18 -0500

    Mark pages on watchlist as visited using JS without reload
    When the "mark pages as visited" is clicked, a dialog appears,
    asking for confirmation. On confirmation, an API request is sent
    to mark all pages as visited, and all unvisited watchlist entries
    are changed to appear visited.
    
    Based on a userscript by User:NQ (from English Wikipedia)
    https://en.wikipedia.org/wiki/User:NQ/WatchlistResetConfirm.js
    
    Bug: T150045
    Change-Id: I45fb02a1edc1b0331925e9a244a2455f86ad3886

--- resources/src/mediawiki.special/mediawiki.special.watchlist.js
+++ resources/src/mediawiki.special/mediawiki.special.watchlist.js
@@ -3,6 +3,32 @@
  */
 ( function ( mw, $ ) {
 	$( function () {
+		var $watchlistReset;
+
+		// If the user wants to reset their watchlist, use an API call to do so (no reload required)
+		// Adapted from a user script by User:NQ of English Wikipedia
+		// (User:NQ/WatchlistResetConfirm.js)
+		$watchlistReset = $( '#mw-watchlist-resetbutton' );
+		$watchlistReset.submit( function ( event ) {
+			event.preventDefault();
+
+			OO.ui.confirm( mw.msg( 'watchlist-mark-all-visited' ) ).done( function ( confirmed ) {
+				if ( confirmed ) {
+					// Use action=setnotificationtimestamp to mark all as visited,
+					// then set all watchlist lines accordingly
+					new mw.Api().postWithEditToken( {
+						action: 'setnotificationtimestamp',
+						entirewatchlist: ''
+					} ).done( function () {
+						$( '.mw-changeslist-line-watched' )
+							.removeClass( 'mw-changeslist-line-watched' )
+							.addClass( 'mw-changeslist-line-not-watched' );
+					} );
+
+				}
+			} );
+		} );
+
 		// if the user wishes to reload the watchlist whenever a filter changes
 		if ( mw.user.options.get( 'watchlistreloadautomatically' ) ) {
 			// add a listener on all form elements in the header form
@@ -13,4 +39,5 @@
 		}
 	} );
 
-}( mediaWiki, jQuery ) );
+}( mediaWiki, jQuery )
+);
