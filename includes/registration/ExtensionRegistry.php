9c00971bd9f95e9ded09ec0c44e906d101851690
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-14 22:02:16 +0100

    PoC: ConfigRepository: Add extension configurations
    This commit adds all configuration options of extensions loaded through
    ExtensionRegistry to the ConfigRepository.
    
    Change-Id: I7fcf094988d15a9a2f931307fd73e14156ec99cd

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -31,7 +31,7 @@ class ExtensionRegistry {
 	/**
 	 * Bump whenever the registration cache needs resetting
 	 */
-	const CACHE_VERSION = 5;
+	const CACHE_VERSION = 6;
 
 	/**
 	 * Special key that defines the merge strategy
@@ -318,6 +318,10 @@ class ExtensionRegistry {
 		}
 		foreach ( $info['autoloaderPaths'] as $path ) {
 			require_once $path;
+		}
+		$configRepo = MediaWikiServices::getInstance()->getConfigRepository();
+		foreach ( $info['config'] as $configItem ) {
+			$configRepo->add( $configItem );
 		}
 
 		$this->loaded += $info['credits'];
