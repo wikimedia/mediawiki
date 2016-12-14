978713216d1707e33e8f63ba1446ea873cc52871
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-14 21:55:56 +0100

    PoC: Add new ConfigRepository and ConfigItem architecture
    This, for now, has the focus for holding meta-information to theoretically
    each configuration option inside of MediaWiki, e.g. MediaWiki core config
    or extension config options. It's very closely to the Config interface and
    friends and uses these implementations to retrieve the actual value of the
    configuration option which is described in a ConfigItem.
    
    The mid-time goal with this change is to implement a basic architecture to
    allow to display the values of the curent configuration of the MediaWiki
    installation on-wiki (e.g. on a special page, which has a new user right, for
    example).
    
    The long-term goal, of course, would be to get a web-interface to really
    configure the MediaWiki installation. But, this is more like a dream, then a
    plan (from the point of view of this commit).
    
    Next steps would be:
     * Enable ExtensionRegistry to "register" the configuration options of
       extensions, so they're available in the repo.
     * Find a good way to get mediawiki/core configurations into this repo
     * Work out an overall architecture to display the different possible values
       (I think about something like different formatters for types).
    
    Change-Id: I9419508eaa85ffc55520db7f33b3e9530fc99f00

---
+++ includes/config/ConfigItem.php
@@ -0,0 +1,119 @@
+<?php
+namespace MediaWiki\Config;
+
+/**
+ * Copyright 2016
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+use MediaWiki\ConfigProvider\ConfigProvider;
+
+/**
+ * Interface, which holds information about a specific config item.
+ *
+ * @since 1.29
+ */
+interface ConfigItem {
+	/**
+	 * Returns the name of this configuration option.
+	 *
+	 * @return string
+	 */
+	public function getName();
+
+	/**
+	 * Returns the current value of the configuration.
+	 *
+	 * This function may get the current value using a configuration storage backend.
+	 *
+	 * @see \Config::get()
+	 *
+	 * @return mixed
+	 */
+	public function getValue();
+
+	/**
+	 * Returns the default value of this configuration. This value may be the same as the value
+	 * of this::getValue().
+	 *
+	 * @return mixed
+	 */
+	public function getDefaultValue();
+
+	/**
+	 * Returns true, if the current value is the default value. It depends upon the
+	 * implementation of this interface, if this returns true if this::getValue() ===
+	 * this::getDefaultValue().
+	 *
+	 * @return mixed
+	 */
+	public function isDefaultValue();
+
+	/**
+	 * Returns the description of this config option or null, if there's no description.
+	 *
+	 * @return boolean
+	 */
+	public function getDescription();
+
+	/**
+	 * Sets the provider which should be used to get the value of this config option. The
+	 * provider may be used by getValue().
+	 *
+	 * @param \Config $provider
+	 */
+	public function setValueProvider( \Config $provider );
+
+	/**
+	 * Returns the provider set to this configuration item, or null if there's no registered
+	 * provider.
+	 *
+	 * @return \Config
+	 */
+	public function getValueProvider();
+
+	/**
+	 * Sets the ConfigProvider which provides this configurationb object.
+	 *
+	 * @return ConfigProvider
+	 */
+	public function setProvider( ConfigProvider $provider );
+
+	/**
+	 * Returns the ConfigProvider which provided this configuration object.
+	 *
+	 * @return ConfigProvider
+	 */
+	public function getProvider();
+
+	/**
+	 * Creates a new ConfigItem from the given object, which should contain at least this required
+	 * information:
+	 *  * name: The name of the configuration
+	 *  * defaultvalue: The default value of the option
+	 *  * provider: An object of the context by which this configuration item is provided
+	 *
+	 * Other options are:
+	 *  * description: The description of the configuration
+	 *  * valueprovider: The provider, from which the value should be retrieved
+	 *
+	 * @param array $array
+	 * @return ConfigItem
+	 */
+	public static function newFromArray( array $array );
+}
---
+++ includes/config/ConfigItemImpl.php
@@ -0,0 +1,129 @@
+<?php
+namespace MediaWiki\Config;
+
+/**
+ * Copyright 2016
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+use MediaWiki\ConfigProvider\ConfigProvider;
+
+/**
+ * A descriptive object for a configuration option.
+ *
+ * @since 1.29
+ */
+class ConfigItemImpl implements ConfigItem {
+
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @var mixed
+	 */
+	private $defaultValue;
+
+	/**
+	 * @var string
+	 */
+	private $description;
+
+	/**
+	 * @var \Config
+	 */
+	private $valueProvider;
+
+	/**
+	 * @var ConfigProvider
+	 */
+	private $provider;
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getValue() {
+		if ( $this->valueProvider === null ) {
+			throw new \ConfigException(
+				'No provider set to retrieve the value of the config item: ' . $this->getName() );
+		}
+
+		return $this->valueProvider->get( $this->getName() );
+	}
+
+	public function getDefaultValue() {
+		return $this->defaultValue;
+	}
+
+	public function isDefaultValue() {
+		return $this->getValue() === $this->getDefaultValue();
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+
+	public function setValueProvider( \Config $provider ) {
+		$this->valueProvider = $provider;
+	}
+
+	public function getValueProvider() {
+		return $this->getValueProvider();
+	}
+
+	public function setProvider( ConfigProvider $provider ) {
+		$this->provider = $provider;
+	}
+
+	public function getProvider() {
+		return $this->provider;
+	}
+
+	public static function newFromArray( array $arr ) {
+		if ( !array_key_exists( 'name', $arr ) ) {
+			throw new \ConfigException( 'The name for a ConfigItem is mandatory. None given.' );
+		}
+		if ( !array_key_exists( 'defaultvalue', $arr ) ) {
+			throw new \ConfigException(
+				'The default value for a ConfigItem is mandatory. None given.' );
+		}
+		if ( !array_key_exists( 'provider', $arr ) ) {
+			throw new \ConfigException(
+				'The provider for a ConfigItem is mandatory. None given.' );
+		}
+
+		$retval = new self();
+		$retval->name = $arr['name'];
+		$retval->provider = $arr['provider'];
+		$retval->defaultValue = $arr['defaultvalue'];
+		if ( isset( $arr['description'] ) ) {
+			$retval->description = $arr['description'];
+		}
+		if ( isset( $arr['valueprovider'] ) ) {
+			$retval->valueProvider = $arr['valueprovider'];
+		}
+
+		return $retval;
+	}
+
+	public function __toString() {
+		return $this->getName();
+	}
+}
