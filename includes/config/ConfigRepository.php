ad4748da7e8fdc72f7fd1f70bc185bc7a8015396
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-14 21:55:56 +0100

    Add new ConfigRepository and ConfigItem architecture
    This, for now, has the focus for holding meta-information to theoretically
    each configuration option inside of MediaWiki, e.g. MediaWiki core config
    or extension config options. It's very closely to the Config interface and
    friends and uses these implementations to retrieve the actual value of the
    configuration option which is described in a ConfigItem.
    
    The mid-time goal with this change is to implement a basic architecture to
    allow to display the values of the curent configuration of the MediaWiki
    installation on-wiki (e.g. on a special page, which has a new user right, for
    example).
    
    The long-term goal, of course, would be to get a web-interface to really
    configure the MediaWiki installation. But, this is more like a dream, then a
    plan (from the point of view of this commit).
    
    Next steps would be:
     * Enable ExtensionRegistry to "register" the configuration options of
       extensions, so they're available in the repo.
     * Find a good way to get mediawiki/core configurations into this repo
     * Work out an overall architecture to display the different possible values
       (I think about something like different formatters for types).
    
    Change-Id: I9419508eaa85ffc55520db7f33b3e9530fc99f00

---
+++ includes/config/ConfigRepository.php
@@ -0,0 +1,139 @@
+<?php
+namespace MediaWiki\Config;
+
+/**
+ * Copyright 2016
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Services\SalvageableService;
+use Wikimedia\Assert\Assert;
+
+/**
+ * Object which holds currently registered configuration options.
+ *
+ * @since 1.29
+ */
+class ConfigRepository implements \Iterator, SalvageableService  {
+	/** @var \ConfigFactory */
+	private $configFactory;
+
+	/** @var array */
+	private $configItems = [];
+
+	/**
+	 * Sets the ConfigFactory instance that should be used by ConfigItems added to this ConfigRepo.
+	 *
+	 * @param \ConfigFactory $configFactory
+	 */
+	public function setConfigFactory( \ConfigFactory $configFactory ) {
+		$this->configFactory = $configFactory;
+	}
+
+	/**
+	 * Returns true, if this repository contains a configuration with a specific name.
+	 *
+	 * @param $name
+	 * @return bool
+	 */
+	public function has( $name ) {
+		return isset( $configItems[$name] );
+	}
+
+	/**
+	 * Returns the ConfigItem with the given name, if there's one. Throws a ConfigException
+	 * otherwise.
+	 *
+	 * @param $name
+	 * @return ConfigItem
+	 * @throws \ConfigException
+	 */
+	public function get( $name ) {
+		if ( !$this->has( $name ) ) {
+			throw new \ConfigException( 'The configuration option ' . $name . ' does not exist.' );
+		}
+		return $this->configItems[$name];
+	}
+
+	/**
+	 * Adds the definition of a configuration to this repository.
+	 *
+	 * @param ConfigItem $item
+	 * @throws \ConfigException
+	 */
+	public function add( ConfigItem $item ) {
+		if ( $this->has( $item->getName() ) ) {
+			throw new \ConfigException( 'A configuration with the name ' . $item->getName() .
+				'does already exist. It is provided by: ' .
+				$this->get( $item->getName() )->getProvider()->getName() );
+		}
+		$this->configItems[] = $item;
+	}
+
+	public function current() {
+		return current( $this->configItems );
+	}
+
+	public function key() {
+		return key( $this->configItems );
+	}
+
+	public function next() {
+		return next( $this->configItems );
+	}
+
+	public function rewind() {
+		reset( $this->configItems );
+	}
+
+	public function valid() {
+		return current( $this->configItems ) !== false;
+	}
+
+	/**
+	 * Re-uses existing Cache objects from $other. Cache objects are only re-used if the
+	 * registered factory function for both is the same.
+	 *
+	 * @see SalvageableService::salvage()
+	 *
+	 * @param SalvageableService $other The object to salvage state from. $other must have the
+	 * exact same type as $this.
+	 */
+	public function salvage( SalvageableService $other ) {
+		Assert::parameterType( self::class, $other, '$other' );
+
+		/** @var ConfigRepository $other */
+		$otherCurrentObj = $other->current();
+		foreach ( $other->configItems as $name => $otherConfig ) {
+			if ( isset( $this->configItems[$name] ) ) {
+				continue;
+			}
+
+			$this->add( $otherConfig );
+
+			// recover the pointer of the other config repository
+			if ( $otherCurrentObj === $otherConfig ) {
+				end( $this->configItems );
+			}
+		}
+
+		// disable $other
+		$other->configItems = [];
+	}
+}
