9de0fef0c66a979cd5d84d28af82bc06f246f1b0
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-14 21:55:56 +0100

    PoC: Add new ConfigRepository and ConfigItem architecture
    This, for now, has the focus for holding meta-information to theoretically
    each configuration option inside of MediaWiki, e.g. MediaWiki core config
    or extension config options. It's very closely to the Config interface and
    friends and uses these implementations to retrieve the actual value of the
    configuration option which is described in a ConfigItem.
    
    The mid-time goal with this change is to implement a basic architecture to
    allow to display the values of the curent configuration of the MediaWiki
    installation on-wiki (e.g. on a special page, which has a new user right, for
    example).
    
    The long-term goal, of course, would be to get a web-interface to really
    configure the MediaWiki installation. But, this is more like a dream, then a
    plan (from the point of view of this commit).
    
    Next steps would be:
     * Enable ExtensionRegistry to "register" the configuration options of
       extensions, so they're available in the repo.
     * Find a good way to get mediawiki/core configurations into this repo
     * Work out an overall architecture to display the different possible values
       (I think about something like different formatters for types).
    
    Change-Id: I9419508eaa85ffc55520db7f33b3e9530fc99f00

---
+++ includes/specials/SpecialConfigurations.php
@@ -0,0 +1,30 @@
+<?php
+class SpecialConfigurations extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'Configurations' );
+	}
+
+	public function execute( $subPage ) {
+		$configRepo = \MediaWiki\MediaWikiServices::getInstance()->getConfigRepository();
+
+		$rows = [];
+		foreach ( $configRepo as $config ) {
+			/** @var \MediaWiki\Config\ConfigItem $value */
+			try {
+				$value = $config->getValue();
+			} catch ( ConfigException $e ) {
+				$value = 'nothing';
+			}
+			$rows[] = [ $config->getName(), $config->getDescription(), $value,
+				$config->getDefaultValue() ];
+		}
+		$this->getOutput()->addHTML( Xml::buildTable( $rows, [
+			'class' => 'wikitable',
+		], [
+			'Configuration',
+			'Description',
+			'Value',
+			'default value'
+		] ) );
+	}
+}
