a4ed9cb6b3c26210520accf419ee03e1e0f9c98d
Geoffrey Mon <geofbot@gmail.com>
2016-12-15 20:35:12 -0500

    Don't show dialog to confirm whether to reset watchlist
    People don't seem to like having a dialog to ask for confirmation to set all
    pages to visited in the watchlist, and the PHP server-side reset
    functionality doesn't ask for a confirmation anyways.
    
    Bug: T150045
    Change-Id: I92aa3c0670925efc691d8bdba2c1c503e17ddb8c

--- resources/src/mediawiki.special/mediawiki.special.watchlist.js
+++ resources/src/mediawiki.special/mediawiki.special.watchlist.js
@@ -17,35 +17,29 @@
 		// Adapted from a user script by User:NQ of English Wikipedia
 		// (User:NQ/WatchlistResetConfirm.js)
 		$resetForm.submit( function ( event ) {
+			var $button = $resetForm.find( 'input[name=mw-watchlist-reset-submit]' );
 			event.preventDefault();
 
-			OO.ui.confirm( mw.msg( 'watchlist-mark-all-visited' ) ).done( function ( confirmed ) {
-				var $button;
+			// Disable reset button to prevent multiple concurrent requests and show progress bar
+			$button.prop( 'disabled', true );
+			$progressBar.css( 'visibility', 'visible' );
 
-				if ( confirmed ) {
-					// Disable reset button to prevent multiple requests and show progress bar
-					$button = $resetForm.find( 'input[name=mw-watchlist-reset-submit]' ).prop( 'disabled', true );
-					$progressBar.css( 'visibility', 'visible' );
-
-					// Use action=setnotificationtimestamp to mark all as visited,
-					// then set all watchlist lines accordingly
-					new mw.Api().postWithToken( 'csrf', {
-						formatversion: 2,
-						action: 'setnotificationtimestamp',
-						entirewatchlist: true
-					} ).done( function () {
-						$button.css( 'visibility', 'hidden' );
-						$progressBar.css( 'visibility', 'hidden' );
-						$( '.mw-changeslist-line-watched' )
-							.removeClass( 'mw-changeslist-line-watched' )
-							.addClass( 'mw-changeslist-line-not-watched' );
-					} ).fail( function () {
-						// On error, fall back to server-side reset
-						// First remove this submit listener and then re-submit the form
-						$resetForm.off( 'submit' ).submit();
-					} );
-
-				}
+			// Use action=setnotificationtimestamp to mark all as visited,
+			// then set all watchlist lines accordingly
+			new mw.Api().postWithToken( 'csrf', {
+				formatversion: 2,
+				action: 'setnotificationtimestamp',
+				entirewatchlist: true
+			} ).done( function () {
+				$button.css( 'visibility', 'hidden' );
+				$progressBar.css( 'visibility', 'hidden' );
+				$( '.mw-changeslist-line-watched' )
+					.removeClass( 'mw-changeslist-line-watched' )
+					.addClass( 'mw-changeslist-line-not-watched' );
+			} ).fail( function () {
+				// On error, fall back to server-side reset
+				// First remove this submit listener and then re-submit the form
+				$resetForm.off( 'submit' ).submit();
 			} );
 		} );
 
