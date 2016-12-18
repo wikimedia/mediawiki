227ebe9786d81f5c0d33cb53dceebddacd96cfc0
Paladox <thomasmulhall410@yahoo.com>
2016-12-18 23:22:39 +0000

    Update some packages
    Update karma to 1.3.0, changelog at
    https://github.com/karma-runner/karma/blob/master/CHANGELOG.md
    
    Update karma-chrome-launcher to 2.0.0, changelog at
    https://github.com/karma-runner/karma-chrome-launcher/releases
    
    Update karma-qunit to 1.2.1, changelog at
    https://github.com/karma-runner/karma-qunit/commits/master
    
    Bug: T153756
    Change-Id: Ic590a8de790552541f3392198d093009616254ac

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,10 +86,10 @@ module.exports = function ( grunt ) {
 				browserNoActivityTimeout: 60 * 1000
 			},
 			main: {
-				browsers: [ 'Chrome' ]
+				browsers: [ 'Chromium' ]
 			},
 			more: {
-				browsers: [ 'Chrome', 'Firefox' ]
+				browsers: [ 'Chromium', 'Firefox' ]
 			}
 		},
 		copy: {
