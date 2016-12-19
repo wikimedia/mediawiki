2a555a9884ea0ad31f56dc780692b47e2889d627
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-19 17:39:29 +0100

    WIP Research webdriver.io
    Bug: T151442
    Change-Id: Ibe7a004a120e82af637ab3e31b725de743134c99

---
+++ tests/selenium/user.js
@@ -0,0 +1,32 @@
+/* global browser */
+var assert = require( 'assert' );
+
+describe( 'User', function () {
+
+	afterEach( function () {
+		if ( this.currentTest.state === 'failed' ) {
+			browser.saveScreenshot('../log/' + this.currentTest.fullTitle() + '.png');
+		}
+	} );
+
+	it( 'should be able to create account', function () {
+		browser.url( '/index.php?title=Special:CreateAccount' );
+		assert( browser.isVisible( '#wpCreateaccount' ) );
+	} );
+
+	it( 'should be able to log in', function () {
+		browser.url( '/index.php?title=Special:UserLogin' );
+		assert( browser.isVisible( '#wpLoginAttempt' ) );
+	} );
+
+	it( 'should be able to change preferences', function () {
+		browser.url( '/index.php?title=Special:UserLogin' );
+		browser.setValue( '#wpName1', 'Admin' );
+		browser.setValue( '#wpPassword1', 'vagrant' );
+		browser.click( '#wpLoginAttempt' );
+
+		browser.url( '/index.php?title=Special:Preferences' );
+		assert( browser.isVisible( '#prefcontrol' ) );
+	} );
+
+} );
