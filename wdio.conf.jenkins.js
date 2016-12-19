0ef5dda2967542a7fe084bed9967feb3affaf8fd
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-19 17:39:29 +0100

    WIP Research webdriver.io
    Bug: T151442
    Change-Id: Ibe7a004a120e82af637ab3e31b725de743134c99

---
+++ wdio.conf.jenkins.js
@@ -0,0 +1,10 @@
+/* eslint no-undef: "error"*/
+/* eslint-env node*/
+var merge = require( 'deepmerge' ),
+	wdioConf = require( './wdio.conf.vagrant.js' );
+// have main config file as default but overwrite environment specific information
+exports.config = merge( wdioConf.config, {
+	// Set a base URL in order to shorten url command calls. If your url parameter starts
+	// with "/", then the base url gets prepended.
+	baseUrl: process.env.MW_SERVER + process.env.MW_SCRIPT_PATH
+} );
