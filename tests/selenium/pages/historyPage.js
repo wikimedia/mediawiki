7790253d3858e0f74cd59e19b79fc0ba4e747a26
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-19 17:39:29 +0100

    Selenium tests in Node.js using WebdriverIO
    Bug: T151442
    Signed-off-by: Antoine Musso <hashar@free.fr>
    Change-Id: Ibe7a004a120e82af637ab3e31b725de743134c99

---
+++ tests/selenium/pages/historyPage.js
@@ -0,0 +1,13 @@
+'use strict';
+var Page = require( './page' );
+
+class HistoryPage extends Page {
+
+	get comment() { return browser.element( '#pagehistory .comment' ); }
+
+	open( name ) {
+		super.open( name + '&action=history' );
+	}
+
+}
+module.exports = new HistoryPage();
---
+++ tests/selenium/user.js
@@ -0,0 +1,59 @@
+'use strict';
+var assert = require( 'assert' ),
+	CreateAccountPage = require( './pages/createAccountPage' ),
+	UserLoginPage = require( './pages/userLoginPage' ),
+	UserLogoutPage = require( './pages/userLogoutPage' ),
+	PreferencesPage = require( './pages/preferencesPage' );
+
+describe( 'User', function () {
+
+	var password,
+		username;
+
+	beforeEach( function () {
+		password = Math.random().toString();
+		username = Math.random().toString();
+	} );
+
+	it( 'should be able to create account', function () {
+
+		// create
+		CreateAccountPage.createAccount( username, password );
+
+		// check
+		assert.equal( CreateAccountPage.heading.getText(), 'Welcome, ' + username + '!' );
+
+	} );
+
+	it( 'should be able to log in', function () {
+
+		// create
+		CreateAccountPage.createAccount( username, password );
+
+		// logout
+		UserLogoutPage.open();
+
+		// log in
+		UserLoginPage.login( username, password );
+
+		// check
+		assert.equal( UserLoginPage.userPage.getText(), username );
+
+	} );
+
+	it( 'should be able to change preferences', function () {
+
+		var realName = Math.random().toString();
+
+		// create
+		CreateAccountPage.createAccount( username, password );
+
+		// change real name
+		PreferencesPage.chageRealName( realName );
+
+		// check
+		assert.equal( PreferencesPage.realName.getValue(), realName );
+
+	} );
+
+} );
