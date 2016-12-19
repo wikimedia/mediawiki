8497e848a6d26f0414d88145baebc7d1bdcb8e87
Željko Filipin <zeljko.filipin@gmail.com>
2016-12-19 17:39:29 +0100

    Selenium tests in Node.js using WebdriverIO
    Introduce the WebdriverIO browser testing framework driven by Node.js.
    The overall intents are:
    * have MediaWiki core to provide a platform to run tests that is shared
      between core and the extensions.
    * phase out ruby driven browser tests eventually.
    
    Code is namespaced in sub directory /tests/selenium
    The 'pages' sub directory provides helper representing a MediaWiki page
    such as Special:Login and human friendly helpers to interact with the
    elements.
    
    Add Grunt task webdriver:test.
    
    Provide a npm script to easily spawn/dispose chromedriver and run above
    grunt task.
    
    wdio.conf.js provides all the configuration. It defaults to point to a
    MediaWiki-Vagrant installation on http://127.0.0.1:8080. Can be
    overriden with environment settings as needed.
    
    glob patterns (specs) are made absolute paths from MediaWiki root path
    that let us run the tests either from the root path (eg the npm
    wrapper or from the tests/selenium directory when invoking wdio
    directly. wdio assumes they are relative to the current working
    directory, hence the normalization.
    
    wdio.conf.jenkins.js extends the configuration and is used solely for
    Jenkins.  The switch is done from the Gruntfile.js whenever JENKINS_HOME
    is set.  Specially we want junit reports to be generated.
    
    Provide a more specific eslint configuration.
    
    References:
    
    * MALU https://phabricator.wikimedia.org/source/malu/
    * T151442 Research WebdriverIO
    * T151443 Research Nightwatch.js
    
    Bug: T139740
    Signed-off-by: Antoine Musso <hashar@free.fr>
    Change-Id: Ibe7a004a120e82af637ab3e31b725de743134c99

---
+++ tests/selenium/specs/page.js
@@ -0,0 +1,54 @@
+'use strict';
+var assert = require( 'assert' ),
+	HistoryPage = require( '../pageobjects/history.page' ),
+	RandomPage = require( '../pageobjects/random.page' );
+
+describe( 'Page', function () {
+
+	var content,
+		name;
+
+	beforeEach( function () {
+		content = Math.random().toString();
+		name = Math.random().toString();
+	} );
+
+	it( 'should be creatable', function () {
+
+		// create
+		RandomPage.edit( name, content );
+
+		// check
+		assert.equal( RandomPage.heading.getText(), name );
+		assert.equal( RandomPage.displayedContent.getText(), content );
+
+	} );
+
+	it( 'should be editable', function () {
+
+		var content2 = Math.random().toString();
+
+		// create
+		RandomPage.edit( name, content );
+
+		// edit
+		RandomPage.edit( name, content2 );
+
+		// check content
+		assert.equal( RandomPage.heading.getText(), name );
+		assert.equal( RandomPage.displayedContent.getText(), content2 );
+
+	} );
+
+	it( 'should have history', function () {
+
+		// create
+		RandomPage.edit( name, content );
+
+		// check
+		HistoryPage.open( name );
+		assert.equal( HistoryPage.comment.getText(), '(Created page with "' + content + '")' );
+
+	} );
+
+} );
