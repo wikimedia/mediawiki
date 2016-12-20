0c1e90405524c095e69abadc5c2df6fc41ca7c27
Antoine Musso <hashar@free.fr>
2016-12-20 16:46:20 +0100

    build: Karma proxy
    By default the Karma proxy does requests against 'localhost', however
    when the target wiki uses a different hostname, requests fails since
    load.php is not found. An example is having MW_SERVER=devwiki.local.
    
    MediaWiki Vagrant or Wikimedia CI are not affected since they use
    'localhost' as a virtual host.
    
    Set Karma proxy to change the hostname accordingly.
    
    Reference:
    https://github.com/karma-runner/karma/issues/1729
    https://github.com/karma-runner/karma/commit/ae05ea4
    
    Bug: T153757
    Change-Id: I317d5686aecd1fb6cf6921cdca77670cded85607

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,10 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-karma' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
-	karmaProxy[ wgScriptPath ] = wgServer + wgScriptPath;
+	karmaProxy[ wgScriptPath ] = {
+		target: wgServer + wgScriptPath,
+		changeOrigin: true
+	};
 
 	grunt.initConfig( {
 		eslint: {
