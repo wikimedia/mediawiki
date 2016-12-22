eb443ad14f8ce083bbdeeedb2a3e0ed4e694cff8
rlot <rj23@protonmail.com>
2016-12-22 15:58:44 +0100

    Fixed PHP issues found by phan
    Fixed "PhanTypeMismatchForeach" PHP issues found by phan static code analysis
    
    Bug: T153252
    Change-Id: I18d94c03286c9d97cecc4620fa51f788c3edd446

--- includes/media/SVG.php
+++ includes/media/SVG.php
@@ -94,7 +94,7 @@ class SvgHandler extends ImageHandler {
 		$langList = [];
 		if ( $metadata ) {
 			$metadata = $this->unpackMetadata( $metadata );
-			if ( isset( $metadata['translations'] ) ) {
+			if ( isset( $metadata['translations'] ) && is_array( $metadata['translations'] ) ) {
 				foreach ( $metadata['translations'] as $lang => $langType ) {
 					if ( $langType === SVGReader::LANG_FULL_MATCH ) {
 						$langList[] = $lang;
@@ -438,6 +438,8 @@ class SvgHandler extends ImageHandler {
 		$visibleFields = $this->visibleMetadataFields();
 
 		$showMeta = false;
+
+		/** @var $metadata array */
 		foreach ( $metadata as $name => $value ) {
 			$tag = strtolower( $name );
 			if ( isset( self::$metaConversion[$tag] ) ) {
@@ -453,6 +455,7 @@ class SvgHandler extends ImageHandler {
 				$tag,
 				$value
 			);
+
 		}
 
 		return $showMeta ? $result : false;
@@ -537,6 +540,8 @@ class SvgHandler extends ImageHandler {
 			return [];
 		}
 		$stdMetadata = [];
+
+		/** @var $metadata array */
 		foreach ( $metadata as $name => $value ) {
 			$tag = strtolower( $name );
 			if ( $tag === 'originalwidth' || $tag === 'originalheight' ) {
