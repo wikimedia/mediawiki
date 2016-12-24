73a7e4ff06913ed0125ef836f0113f641641d5e3
mynk-96 <mayank.harsani@gmail.com>
2016-12-24 17:28:38 +0530

    Bug: T153243 Change-Id: Ieb37b34e76441c5f68ec565c43421c50ac9be7e7

---
+++ includes/exception/MWContentSerializationException.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Exception representing a failure to serialize or unserialize a content object.
+ *
+ * @ingroup Content
+ */
+class MWContentSerializationException extends MWException {
+}
+?>
