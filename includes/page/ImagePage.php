a18a0af4251af151df3c75aa51abe82c200f797f
divadsn <divad.nnamtdeis@gmail.com>
2016-12-25 23:55:27 +0000

    Replace &$this with $this in MediaWiki/core hook system
    Bug: T153505
    Change-Id: Ie287adf8b1d384aa651c659a9d385877379a0f11

--- includes/page/ImagePage.php
+++ includes/page/ImagePage.php
@@ -69,6 +69,7 @@ class ImagePage extends Article {
 		$this->fileLoaded = true;
 
 		$this->displayImg = $img = false;
+
 		Hooks::run( 'ImagePageFindFile', [ $this, &$img, &$this->displayImg ] );
 		if ( !$img ) { // not set by hook?
 			$img = wfFindFile( $this->getTitle() );
@@ -335,7 +336,10 @@ class ImagePage extends Article {
 			$filename = wfEscapeWikiText( $this->displayImg->getName() );
 			$linktext = $filename;
 
-			Hooks::run( 'ImageOpenShowImageInlineBefore', [ &$this, &$out ] );
+			// Use of &$this in hooks triggers warnings in PHP 7.1
+			$imagePage = $this;
+
+			Hooks::run( 'ImageOpenShowImageInlineBefore', [ &$imagePage, &$out ] );
 
 			if ( $this->displayImg->allowInlineDisplay() ) {
 				# image
