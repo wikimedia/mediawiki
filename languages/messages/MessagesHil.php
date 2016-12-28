daf6e53d938db2463f0cf0f963de0d48ad5f4b32
Federico Leva <federicoleva@tiscali.it>
2016-12-28 10:22:47 +0100

    [WIP] Add language fallback to tl (Tagalog/Filipino) for several languages
    https://translatewiki.net/wiki/Thread:Portal_talk:Tl/Use_tl_translations_for_other_languages_of_the_Philippines
    
    All editors of the {bcl,ceb,pag,war}.wikipedia have also been pointed
    to the discussion individually. The main concern is that care will be
    needed on the part of tl translators to avoid "purist" translations for
    certain English loanwords which are more familiar to the speakers.
    
    Change-Id: Ic5fdcfbf159c60156c10780a504b8de914d250b3

---
+++ languages/messages/MessagesHil.php
@@ -0,0 +1,11 @@
+<?php
+/** Hiligaynon (Ilonggo)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'tl';
---
+++ languages/messages/MessagesPag.php
@@ -0,0 +1,11 @@
+<?php
+/** Pangasinan (pangasinan)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'tl';
