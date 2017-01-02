d33f722b11c467744bd0ef1432a4cb8318d2ecff
rlot <rj23@protonmail.com>
2017-01-02 20:00:53 +0100

    Added $wgHTTPImportTimeout setting
    Bug: T17000
    Change-Id: Ic97ae4faec173c32af38df4554831dca7068226b

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -8056,6 +8056,12 @@ $wgShellLocale = 'en_US.utf8';
 $wgHTTPTimeout = 25;
 
 /**
+ * Timeout for HTTP requests done internally in imports, in seconds.
+ * @since 1.29
+ */
+$wgHTTPImportTimeout = 60;
+
+/**
  * Timeout for Asynchronous (background) HTTP requests, in seconds.
  */
 $wgAsyncHTTPTimeout = 25;
