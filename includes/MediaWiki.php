0eadfdeb17f6bcdc752e726aa5859d6cf32670d2
rlot <rj23@protonmail.com>
2017-01-02 14:48:31 +0100

    Fully deprecate 'ContentAction' hook
    Bug: T54027
    Change-Id: Ia765e25a9f76b34f7b10125d3b6e0c8e00fac2ca

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -495,8 +495,15 @@ class MediaWiki {
 			$action->show();
 			return;
 		}
-
-		if ( Hooks::run( 'UnknownAction', [ $request->getVal( 'action', 'view' ), $page ] ) ) {
+		// NOTE: deprecated hook. Add to $wgActions instead
+		if ( Hooks::run(
+			'UnknownAction',
+			[
+				$request->getVal( 'action', 'view' ),
+				$page
+			],
+			'1.19'
+		) ) {
 			$output->setStatusCode( 404 );
 			$output->showErrorPage( 'nosuchaction', 'nosuchactiontext' );
 		}
