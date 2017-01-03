1f8b491dd200179258ff6b302ed253fa46145f84
Subin Siby <subins2000@gmail.com>
2017-01-03 20:05:34 +0530

    Disable statistics update on import with maintenance/importDump.php
    Disable updating statistics in importDump.php to quickly finish imports from command line.
    
    Bug: T144600
    Change-Id: Ib827c068fb20cc03aab47e3106d489f18be1dac6

--- maintenance/importDump.php
+++ maintenance/importDump.php
@@ -109,7 +109,8 @@ TEXT
 		}
 
 		$this->output( "Done!\n" );
-		$this->output( "You might want to run rebuildrecentchanges.php to regenerate RecentChanges\n" );
+		$this->output( "You might want to run rebuildrecentchanges.php to regenerate RecentChanges,\n" );
+		$this->output( "and initSiteStats.php to update page and revision counts\n" );
 	}
 
 	function setNsfilter( array $namespaces ) {
@@ -282,6 +283,9 @@ TEXT
 
 		$source = new ImportStreamSource( $handle );
 		$importer = new WikiImporter( $source, $this->getConfig() );
+
+		// Updating statistics require a lot of time so disable it
+		$importer->disableStatisticsUpdate();
 
 		if ( $this->hasOption( 'debug' ) ) {
 			$importer->setDebug( true );
