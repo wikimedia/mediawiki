3976162eae412071761b32bcb2f1debe3e7d6402
rlot <rj23@protonmail.com>
2017-01-04 18:52:17 +0100

    Added reason suggestion in block/delete/protect forms
    Bug: T34950
    Change-Id: I9778c4992b127c36355949665b4fdf7482e7e0e7

---
+++ resources/src/mediawiki/mediawiki.reasonSuggest.js
@@ -0,0 +1,29 @@
+/*!
+* Add autocomplete suggestions for action forms reasons.
+*/
+( function ( mw, $ ) {
+	$( function () {
+		var api = new mw.Api();
+		var reasons;
+		api.loadMessagesIfMissing([mw.config.get( 'reasons' )])
+		.done( function () {
+			// Convert from string to array, first index is unneeded
+			reasons = mw.msg( mw.config.get( 'reasons' ) ).split( '\n** ' );
+			reasons.splice( 0, 1 );
+		} );
+
+		// Add relevant suggestion
+		$( '#mwProtect-reason, #wpReason, #mw-input-wpReason-other' ).suggestions( {
+			fetch: function () {
+				console.log(reasons[1]);
+				var $this = $( this ), relevantSuggestions;
+				relevantSuggestions = $.grep( reasons, function ( reason ) {
+					return ( reason.toLowerCase().indexOf( $this.val().toLowerCase() ) > -1 );
+				} );
+				$this.suggestions( 'suggestions', relevantSuggestions );
+			},
+			delay: 120,
+			highlightInput: true
+		} );
+	} );
+}( mediaWiki, jQuery ) );
