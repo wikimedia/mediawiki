wikimedia/mediawiki

commit 1f15d1d5828dfbc70d036a52da57460ea0e89175

API: More i18n cleanup
* Use errorformat for action=login Failed responses in non-BC mode.
* We removed 'messageHtml' from action=rollback's response on error, but
  left it for success. Remove it there too, it's even less useful.
* We changed action=watch's reporting of errors, but left the
  mostly-pointless reporting of "success" UI messages. These should be
  handled on the client side.

Change-Id: Ia6c402a4254fbacf4c2c3f125ce8bf0bcc71e509

RELEASE-NOTES-1.29
@@ -58,11 +58,12 @@ production.
 * action=revisiondelete no longer includes a "rendered" property on warnings
   and errors for each item. Use errorformat=wikitext if you're wanting parsed
   output.
-* action=rollback no longer returns a "messageHtml" property on errors. Use
-  errorformat=html if you're wanting HTML formatting of messages.
+* action=rollback no longer returns a "messageHtml" property. Use
+  errorformat=html if you're wanting HTML formatting of error messages.
 * action=upload now reports optional stash failures as an array under key
   'stasherrors' rather than a 'stashfailed' text string.
-* action=watch reports 'errors' and 'warnings' instead of a single 'error'.
+* action=watch reports 'errors' and 'warnings' instead of a single 'error', and
+  no longer returns a 'message' on success.
 
 === Action API internal changes in 1.29 ===
 * New methods were added to ApiBase to handle errors and warnings using i18n

includes/api/ApiRollback.php
@@ -92,17 +92,6 @@ class ApiRollback extends ApiBase {
 			'last_revid' => intval( $details['target']->getID() )
 		];
 
-		$oldUser = $details['current']->getUserText( Revision::FOR_THIS_USER );
-		$lastUser = $details['target']->getUserText( Revision::FOR_THIS_USER );
-		$diffUrl = $titleObj->getFullURL( [
-			'diff' => $info['revid'],
-			'oldid' => $info['old_revid'],
-			'diffonly' => '1'
-		] );
-		$info['messageHtml'] = $this->msg( 'rollback-success-notify' )
-			->params( $oldUser, $lastUser, $diffUrl )
-			->parseAsBlock();
-
 		$this->getResult()->addValue( null, $this->getModuleName(), $info );
 	}
 

resources/Resources.php
@@ -1738,6 +1738,8 @@ return [
 			'mediawiki.api.watch',
 			'mediawiki.notify',
 			'mediawiki.util',
+			'mediawiki.Title',
+			'mediawiki.jqueryMsg',
 			'jquery.accessKeyLabel',
 			'mediawiki.RegExp',
 		],
@@ -1749,6 +1751,10 @@ return [
 			'tooltip-ca-watch',
 			'tooltip-ca-unwatch',
 			'watcherrortext',
+			'addedwatchtext',
+			'addedwatchtext-talk',
+			'removedwatchtext',
+			'removedwatchtext-talk',
 		],
 	],
 	'mediawiki.page.rollback' => [

includes/api/ApiLogin.php
@@ -197,9 +197,14 @@ class ApiLogin extends ApiBase {
 				break;
 
 			case 'Failed':
-				$result['reason'] = ApiErrorFormatter::stripMarkup(
-					$message->useDatabase( false )->inLanguage( 'en' )->text()
-				);
+				$errorFormatter = $this->getErrorFormatter();
+				if ( $errorFormatter instanceof ApiErrorFormatter_BackCompat ) {
+					$result['reason'] = ApiErrorFormatter::stripMarkup(
+						$message->useDatabase( false )->inLanguage( 'en' )->text()
+					);
+				} else {
+					$result['reason'] = $errorFormatter->formatMessage( $message );
+				}
 				break;
 
 			case 'Aborted':

includes/api/ApiWatch.php
@@ -110,19 +110,9 @@ class ApiWatch extends ApiBase {
 		if ( $params['unwatch'] ) {
 			$status = UnwatchAction::doUnwatch( $title, $user );
 			$res['unwatched'] = $status->isOK();
-			if ( $status->isOK() ) {
-				$msgKey = $title->isTalkPage() ? 'removedwatchtext-talk' : 'removedwatchtext';
-				$res['message'] = $this->msg( $msgKey, $title->getPrefixedText() )
-					->title( $title )->parseAsBlock();
-			}
 		} else {
 			$status = WatchAction::doWatch( $title, $user );
 			$res['watched'] = $status->isOK();
-			if ( $status->isOK() ) {
-				$msgKey = $title->isTalkPage() ? 'addedwatchtext-talk' : 'addedwatchtext';
-				$res['message'] = $this->msg( $msgKey, $title->getPrefixedText() )
-					->title( $title )->parseAsBlock();
-			}
 		}
 
 		if ( !$status->isOK() ) {

resources/src/mediawiki/api/watch.js
@@ -19,7 +19,6 @@
 	 *  parameter)
 	 * @return {string} return.done.watch.title Full pagename
 	 * @return {boolean} return.done.watch.watched Whether the page is now watched or unwatched
-	 * @return {string} return.done.watch.message Parsed HTML of the confirmational interface message
 	 */
 	function doWatchInternal( pages, addParams ) {
 		// XXX: Parameter addParams is undocumented because we inherit this

resources/src/mediawiki/page/watch.js
@@ -132,9 +132,19 @@
 
 			api[ action ]( title )
 				.done( function ( watchResponse ) {
-					var otherAction = action === 'watch' ? 'unwatch' : 'watch';
+					var mwTitle, message, otherAction = action === 'watch' ? 'unwatch' : 'watch';
 
-					mw.notify( $.parseHTML( watchResponse.message ), {
+					message = action === 'watch' ? 'addedwatchtext' : 'removedwatchtext';
+					mwTitle = mw.Title.newFromText( title );
+					if ( mwTitle && mwTitle.getNamespaceId() > 0 &&
+						/* eslint-disable no-bitwise */
+						( mwTitle.getNamespaceId() & 1 ) === 1
+						/* eslint-enable no-bitwise */
+					) {
+						message += '-talk';
+					}
+
+					mw.notify( mw.message( message, title ).parseDom(), {
 						tag: 'watch-self'
 					} );
 

commit f74e39f7e6134d68d560f911311583d963d2b783

[WIP] Re-apply "RollbackAction: Implement AJAX interface and require POST"
This reverts commit 5391e328c2. Also follows-up 1f15d1d58 which broke
mediawiki.page.rollback.

Changes:
* Re-enable RollbackAction as a FormAction with confirmation page.

* Re-enable "mediawiki.page.rollback" module for AJAX interface so that
  rollback remains a one-click action.

* (follows-up 1f15d1d58) Add oldUser/lastUser/diffUrl as data properties
  to ApiRollback response so that "rollback-success-notify" can be formatted
  client-side.

Differences compared to 9af38c046c:

* Improve wording of "rollback-success-notify" message to include the
  page title. On Special:Contributions there is no obvious page association.

* Disable 'autoHide' for the notification bubble so that the "Show changes"
  link remains accessible.

* Support "summary" parameter override. This is an undocumented feature
  not used in MediaWiki core but used by various user scripts to customize
  the edit summary used for the rollback edit. The action=rollback form
  and API already supported this and was a regression in 9af38c046c.

Change-Id: I06f0d23af2d37d9843744f385aa4cc70bf6bf18f

RELEASE-NOTES-1.29
@@ -29,6 +29,7 @@ production.
   wikis with $wgJobRunRate > 0.
 
 === New features in 1.29 ===
+* (T88044) Implemented one-click rollback handling via AJAX.
 * (T5233) A cookie can now be set when a user is autoblocked, to track that user if
   they move to a new IP address. This is disabled by default.
 * Added ILocalizedException interface to standardize the use of localized

includes/Linker.php
@@ -1751,6 +1751,8 @@ class Linker {
 	 *
 	 * If the option noBrackets is set the rollback link wont be enclosed in "[]".
 	 *
+	 * See the "mediawiki.page.rollback" module for the client-side handling of this link.
+	 *
 	 * @since 1.16.3. $context added in 1.20. $options added in 1.21
 	 *
 	 * @param Revision $rev
@@ -1778,6 +1780,8 @@ class Linker {
 		if ( !in_array( 'noBrackets', $options, true ) ) {
 			$inner = $context->msg( 'brackets' )->rawParams( $inner )->escaped();
 		}
+
+		$context->getOutput()->addModules( 'mediawiki.page.rollback' );
 
 		return '<span class="mw-rollback-link">' . $inner . '</span>';
 	}
@@ -1873,7 +1877,6 @@ class Linker {
 		$query = [
 			'action' => 'rollback',
 			'from' => $rev->getUserText(),
-			'token' => $context->getUser()->getEditToken( 'rollback' ),
 		];
 		$attrs = [
 			'data-mw' => 'interface',

includes/actions/RollbackAction.php
@@ -25,7 +25,7 @@
  *
  * @ingroup Actions
  */
-class RollbackAction extends FormlessAction {
+class RollbackAction extends FormAction {
 
 	public function getName() {
 		return 'rollback';
@@ -35,20 +35,38 @@ class RollbackAction extends FormlessAction {
 		return 'rollback';
 	}
 
-	/**
-	 * Temporarily unused message keys due to T88044/T136375:
-	 * - confirm-rollback-top
-	 * - confirm-rollback-button
-	 * - rollbackfailed
-	 * - rollback-missingparam
-	 */
+	protected function preText() {
+		return $this->msg( 'confirm-rollback-top' )->parse();
+	}
+
+	protected function alterForm( HTMLForm $form ) {
+		$form->setSubmitTextMsg( 'confirm-rollback-button' );
+		$form->setTokenSalt( 'rollback' );
+
+		// Copy parameters from GET to confirmation form
+		$from = $this->getRequest()->getVal( 'from' );
+		if ( $from === null ) {
+			throw new BadRequestError( 'rollbackfailed', 'rollback-missingparam' );
+		}
+		foreach ( [ 'from', 'bot', 'hidediff', 'summary' ] as $param ) {
+			$val = $this->getRequest()->getVal( $param );
+			if ( $val !== null ) {
+				$form->addHiddenField( $param, $val );
+			}
+		}
+	}
 
 	/**
+	 * This must return true so that HTMLForm::show() will not display the form again after
+	 * submission. For rollback, display either the form or the result (success/error)
+	 * not both.
+	 *
+	 * @return bool|string True for success, false for didn't try, empty string if
+	 *  action failed but error is already output here.
 	 * @throws ErrorPageError
 	 */
-	public function onView() {
-		// TODO: use $this->useTransactionalTimeLimit(); when POST only
-		wfTransactionalTimeLimit();
+	public function onSubmit( $data ) {
+		$this->useTransactionalTimeLimit();
 
 		$request = $this->getRequest();
 		$user = $this->getUser();
@@ -72,7 +90,8 @@ class RollbackAction extends FormlessAction {
 		$errors = $this->page->doRollback(
 			$from,
 			$request->getText( 'summary' ),
-			$request->getVal( 'token' ),
+			// Provided by HTMLForm
+			$request->getVal( 'wpEditToken' ),
 			$request->getBool( 'bot' ),
 			$data,
 			$this->getUser()
@@ -100,7 +119,7 @@ class RollbackAction extends FormlessAction {
 				}
 			}
 
-			return;
+			return '';
 		}
 
 		# NOTE: Permission errors already handled by Action::checkExecute.
@@ -145,7 +164,12 @@ class RollbackAction extends FormlessAction {
 			);
 			$de->showDiff( '', '' );
 		}
-		return;
+		return true;
+	}
+
+	public function onSuccess() {
+		// Required by parent class, but redundant because onSubmit already shows
+		// the success message when needed.
 	}
 
 	protected function getDescription() {

includes/api/ApiRollback.php
@@ -86,10 +86,17 @@ class ApiRollback extends ApiBase {
 			'pageid' => intval( $details['current']->getPage() ),
 			'summary' => $details['summary'],
 			'revid' => intval( $details['newid'] ),
+			'diffUrl' => $titleObj->getFullURL( [
+				'diff' => $info['revid'],
+				'oldid' => $info['old_revid'],
+				'diffonly' => '1'
+			] ),
 			// The revision being reverted (previously the current revision of the page)
 			'old_revid' => intval( $details['current']->getID() ),
+			'oldUser' => $details['current']->getUserText( Revision::FOR_THIS_USER ),
 			// The revision being restored (the last revision before revision(s) by the reverted user)
-			'last_revid' => intval( $details['target']->getID() )
+			'last_revid' => intval( $details['target']->getID() ),
+			'lastUser' => $details['target']->getUserText( Revision::FOR_THIS_USER ),
 		];
 
 		$this->getResult()->addValue( null, $this->getModuleName(), $info );

resources/Resources.php
@@ -1763,6 +1763,7 @@ return [
 		'messages' => [
 			'rollbackfailed',
 			'actioncomplete',
+			'rollback-success-notify',
 		],
 	],
 	'mediawiki.page.image.pagination' => [

resources/src/mediawiki/page/rollback.js
@@ -13,7 +13,8 @@
 				$link = $( this ),
 				url = this.href,
 				page = mw.util.getParamValue( 'title', url ),
-				user = mw.util.getParamValue( 'from', url );
+				user = mw.util.getParamValue( 'from', url ),
+				summary = mw.util.getParamValue( 'summary', url );
 
 			if ( !page || user === null ) {
 				// Let native browsing handle the link
@@ -32,11 +33,21 @@
 
 			// @todo: data.messageHtml is no more. Convert to using errorformat=html.
 			api = new mw.Api();
-			api.rollback( page, user )
+			api.rollback( page, user, summary ? { summary: summary } : null )
 				.then( function ( data ) {
-					mw.notify( $.parseHTML( data.messageHtml ), {
-						title: mw.msg( 'actioncomplete' )
-					} );
+					mw.notify(
+						mw.message(
+							'rollback-success-notify',
+							data.oldUser,
+							data.lastUser,
+							data.diffUrl,
+							data.title
+						),
+						{
+							title: mw.msg( 'actioncomplete' ),
+							autoHide: false
+						}
+					);
 
 					// Remove link container and the subsequent text node containing " | ".
 					if ( e.delegateTarget.nextSibling && e.delegateTarget.nextSibling.nodeType === Node.TEXT_NODE ) {