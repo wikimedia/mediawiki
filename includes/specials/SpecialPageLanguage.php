9880f95a6bbbfd9a5849b51b5706dab02e8fc0c0
MtDu <justin.d128@gmail.com>
2017-01-06 07:04:41 -0600

    Add a reason field to Special:PageLanguage and ApiSetPageLanguage
    Most MediaWiki actions allow a user to enter a comment/reason for taking the action.
    This is logged in the change log. However, currently, both ApiSetPageLanguage and Special:PageLanguage do not do thi.
    This patch fixes this issue.
    
    Bug: T154644
    Change-Id: I6ae021086e1ec4440d6b2894a1b85d62eb36e172

--- includes/specials/SpecialPageLanguage.php
+++ includes/specials/SpecialPageLanguage.php
@@ -89,6 +89,12 @@ class SpecialPageLanguage extends FormSpecialPage {
 			'default' => $this->getConfig()->get( 'LanguageCode' ),
 		];
 
+		// Allow user to enter a comment explaining the change
+		$page['reason'] = [
+			'type' => 'text',
+			'label-message' => 'pagelang-reason'
+		];
+
 		return $page;
 	}
 
@@ -132,7 +138,12 @@ class SpecialPageLanguage extends FormSpecialPage {
 		// Url to redirect to after the operation
 		$this->goToUrl = $title->getFullURL();
 
-		return self::changePageLanguage( $this->getContext(), $title, $newLanguage );
+		return self::changePageLanguage(
+			$this->getContext(),
+			$title,
+			$newLanguage,
+			$data['reason'] ?: ''
+		);
 	}
 
 	/**
@@ -140,10 +151,11 @@ class SpecialPageLanguage extends FormSpecialPage {
 	 * @param Title $title
 	 * @param string $newLanguage Language code
 	 * @param array $tags Change tags to apply to the log entry
+	 * @param string $reason Reason for the change
 	 * @return Status
 	 */
 	public static function changePageLanguage( IContextSource $context, Title $title,
-		$newLanguage, array $tags = [] ) {
+		$newLanguage, array $tags = [], $reason ) {
 		// Get the default language for the wiki
 		$defLang = $context->getConfig()->get( 'LanguageCode' );
 
@@ -219,6 +231,7 @@ class SpecialPageLanguage extends FormSpecialPage {
 		$entry->setTarget( $title );
 		$entry->setParameters( $logParams );
 		$entry->setTags( $tags );
+		$entry->setComment( $reason );
 
 		$logid = $entry->insert();
 		$entry->publish( $logid );
