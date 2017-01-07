44a2c48adb9dde52d54e420dfa21c3b778279860
Timo Tijhof <krinklemail@gmail.com>
2017-01-06 20:41:31 -0800

    rcfeed: Add basic PHPUnit integration test
    Change-Id: Ifb5ddd4bda6cd1be050da7a5419ebe90f5ecf60f

---
+++ tests/phpunit/includes/rcfeed/RCFeedIntegrationTest.php
@@ -0,0 +1,94 @@
+<?php
+
+class RCFeedIntegrationTest extends MediaWikiTestCase {
+	protected function setUp() {
+		parent::setUp();
+		$this->setMwGlobals( [
+			'wgCanonicalServer' => 'https://example.org',
+			'wgServerName' => 'example.org',
+			'wgScriptPath' => '/w',
+			'wgRCFeeds' => [],
+			'wgRCEngines' => [],
+		] );
+	}
+
+	/**
+	 * @covers RecentChange::notifyRCFeeds
+	 * @covers RecentChange::getEngine
+	 * @covers RCFeedEngine
+	 * @covers JSONRCFeedFormatter::formatArray
+	 * @covers MachineReadableRCFeedFormatter::getLine
+	 */
+	public function testNotify() {
+		$stub = $this->getMockBuilder( 'RCFeedEngine' )
+			->setConstructorArgs( [ [ 'formatter' => 'JSONRCFeedFormatter' ] ] )
+			->setMethods( [ 'send' ] )
+			->getMock();
+
+		$stub->method( 'send' )
+			->willReturn( true );
+
+		$stub->expects( $this->once() )
+			->method( 'send' )
+			->with( $this->anything(), $this->callback( function ( $line ) {
+				$this->assertJsonStringEqualsJsonString(
+					json_encode( [
+						'id' => null,
+						'type' => 'log',
+						'namespace' => 0,
+						'title' => 'Example',
+						'comment' => '',
+						'timestamp' => 1301644800,
+						'user' => 'UTSysop',
+						'bot' => false,
+						'log_id' => 0,
+						'log_type' => 'move',
+						'log_action' => 'move',
+						'log_params' => [
+							'color' => 'green',
+							'nr' => 42,
+							'pet' => 'cat',
+						],
+						'log_action_comment' => '',
+						'server_url' => 'https://example.org',
+						'server_name' => 'example.org',
+						'server_script_path' => '/w',
+						'wiki' => 'wiki-unittest_',
+					] ),
+					$line
+				);
+				return true;
+			} ) );
+
+		$this->setMwGlobals( [
+			'wgRCFeeds' => [
+				'myfeed' => [
+					'uri' => 'test://localhost:1234',
+					'formatter' => 'JSONRCFeedFormatter',
+				],
+			],
+			'wgRCEngines' => [
+				'test' => $stub,
+			],
+		] );
+		$logpage = SpecialPage::getTitleFor( 'Log', 'move' );
+		$user = $this->getTestSysop()->getUser();
+		$rc = RecentChange::newLogEntry(
+			'20110401080000',
+			$logpage, // &$title
+			$user, // &$user
+			'', // $actionComment
+			'127.0.0.1', // $ip
+			'move', // $type
+			'move', // $action
+			Title::makeTitle( 0, 'Example' ), // $target
+			'', // $logComment
+			LogEntryBase::makeParamBlob([
+				'4::color' => 'green',
+				'5:number:nr' => 42,
+				'pet' => 'cat',
+			])
+		);
+		$rc->notifyRCFeeds();
+	}
+}
