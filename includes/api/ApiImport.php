82d7a188749c25d361f63cdf9db41b3ee23a0eaf
This, that and the other <at.light@live.com.au>
2017-01-08 17:40:22 +1100

    Add parameter to API modules to apply change tags to log entries
    Adds support for tagging log entries for the block, import,
    managetags, and move API modules, using a 'tags' parameter.
    
    Bug: T97720
    Change-Id: I9d75d2cece317a7704c4bc6d734ad3cafe24544e

--- includes/api/ApiImport.php
+++ includes/api/ApiImport.php
@@ -64,6 +64,14 @@ class ApiImport extends ApiBase {
 			$this->dieStatus( $source );
 		}
 
+		// Check if user can add the log entry tags which were requested
+		if ( count( $params['tags'] ) ) {
+			$ableToTag = ChangeTags::canAddTagsAccompanyingChange( $params['tags'], $user );
+			if ( !$ableToTag->isOK() ) {
+				$this->dieStatus( $ableToTag );
+			}
+		}
+
 		$importer = new WikiImporter( $source->value, $this->getConfig() );
 		if ( isset( $params['namespace'] ) ) {
 			$importer->setTargetNamespace( $params['namespace'] );
@@ -79,6 +87,7 @@ class ApiImport extends ApiBase {
 			$params['interwikisource'],
 			$params['summary']
 		);
+		$reporter->setChangeTags( $params['tags'] );
 
 		try {
 			$importer->doImport();
@@ -140,6 +149,10 @@ class ApiImport extends ApiBase {
 				ApiBase::PARAM_TYPE => 'namespace'
 			],
 			'rootpage' => null,
+			'tags' => [
+				ApiBase::PARAM_TYPE => 'tags',
+				ApiBase::PARAM_ISMULTI => true,
+			],
 		];
 	}
 
--- includes/api/ApiManageTags.php
+++ includes/api/ApiManageTags.php
@@ -27,6 +27,7 @@ class ApiManageTags extends ApiBase {
 
 	public function execute() {
 		$params = $this->extractRequestParams();
+		$user = $this->getUser();
 
 		// make sure the user is allowed
 		if ( $params['operation'] !== 'delete'
@@ -37,10 +38,23 @@ class ApiManageTags extends ApiBase {
 			$this->dieWithError( 'tags-delete-no-permission', 'permissiondenied' );
 		}
 
+		// Check if user can add the log entry tags which were requested
+		if ( count( $params['tags'] ) ) {
+			$ableToTag = ChangeTags::canAddTagsAccompanyingChange( $params['tags'], $user );
+			if ( !$ableToTag->isOK() ) {
+				$this->dieStatus( $ableToTag );
+			}
+		}
+
 		$result = $this->getResult();
 		$funcName = "{$params['operation']}TagWithChecks";
-		$status = ChangeTags::$funcName( $params['tag'], $params['reason'],
-			$this->getUser(), $params['ignorewarnings'] );
+		$status = ChangeTags::$funcName(
+			$params['tag'],
+			$params['reason'],
+			$user,
+			$params['ignorewarnings'],
+			count( $params['tags'] ) ? $params['tags'] : []
+		);
 
 		if ( !$status->isOK() ) {
 			$this->dieStatus( $status );
@@ -57,6 +71,7 @@ class ApiManageTags extends ApiBase {
 		if ( $ret['success'] ) {
 			$ret['logid'] = $status->value;
 		}
+
 		$result->addValue( null, $this->getModuleName(), $ret );
 	}
 
@@ -84,6 +99,10 @@ class ApiManageTags extends ApiBase {
 			'ignorewarnings' => [
 				ApiBase::PARAM_TYPE => 'boolean',
 				ApiBase::PARAM_DFLT => false,
+			],
+			'tags' => [
+				ApiBase::PARAM_TYPE => 'tags',
+				ApiBase::PARAM_ISMULTI => true,
 			],
 		];
 	}
