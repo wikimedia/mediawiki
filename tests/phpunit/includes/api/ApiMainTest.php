b3c325b784e1c82c90c770ab14fc488056e9c03f
Brad Jorsch <bjorsch@wikimedia.org>
2017-01-09 15:38:36 -0800

    API: Add reference to the mailing list in errors and deprecation warnings
    This was suggested at a Developer Summit session as a way to get people
    to know about the mailing list.
    
    This also adds a hook so ApiFeatureUsage can mention itself in
    deprecation warnings too.
    
    Bug: T148855
    Change-Id: I04a7cf89e87e48f6504803dd173e779017a205d0

--- tests/phpunit/includes/api/ApiMainTest.php
+++ tests/phpunit/includes/api/ApiMainTest.php
@@ -546,7 +546,9 @@ class ApiMainTest extends ApiTestCase {
 						[ 'code' => 'existing-error', 'text' => 'existing error', 'module' => 'main' ],
 						[ 'code' => 'ue', 'text' => "Usage exception!", 'data' => [ 'foo' => 'bar' ] ]
 					],
-					'docref' => "See $doclink for API usage.",
+					'docref' => "See $doclink for API usage. Subscribe to " .
+						"mediawiki-api-announce@lists.wikimedia.org for notice of API deprecations " .
+						"and breaking changes.",
 					'servedby' => wfHostname(),
 				]
 			],
@@ -564,7 +566,9 @@ class ApiMainTest extends ApiTestCase {
 						[ 'code' => 'sv-error1', 'text' => 'An error', 'module' => 'foo+bar' ],
 						[ 'code' => 'sv-error2', 'text' => 'Another error', 'module' => 'foo+bar' ],
 					],
-					'docref' => "See $doclink for API usage.",
+					'docref' => "See $doclink for API usage. Subscribe to " .
+						"mediawiki-api-announce@lists.wikimedia.org for notice of API deprecations " .
+						"and breaking changes.",
 					'servedby' => wfHostname(),
 				]
 			],
