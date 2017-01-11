2a633b1d8476115840cbf43ee122631353990872
Fomafix <fomafix@googlemail.com>
2017-01-11 06:29:47 +0100

    mediawiki.reasonSuggest: Execute API request before document ready
    There is no need to wait for document ready for executing the API request.
    
    Change-Id: I1a1a526f8388c336258a46e9ce2c5c2be05fb20d

--- resources/src/mediawiki/mediawiki.reasonSuggest.js
+++ resources/src/mediawiki/mediawiki.reasonSuggest.js
@@ -2,16 +2,16 @@
 * Add autocomplete suggestions for action forms reasons.
 */
 ( function ( mw, $ ) {
-	$( function () {
-		var api = new mw.Api(), reasons = [];
-		// These messages can be really big, so its loaded on-the-go
-		api.loadMessagesIfMissing( [ mw.config.get( 'reasons' ) ] )
-			.done( function () {
-				// Convert from string to array, first index is unneeded
-				reasons = mw.msg( mw.config.get( 'reasons' ) ).split( '\n** ' );
-				reasons.splice( 0, 1 );
-			} );
+	var api = new mw.Api(), reasons = [];
+	// These messages can be really big, so its loaded on-the-go
+	api.loadMessagesIfMissing( [ mw.config.get( 'reasons' ) ] )
+		.done( function () {
+			// Convert from string to array, first index is unneeded
+			reasons = mw.msg( mw.config.get( 'reasons' ) ).split( '\n** ' );
+			reasons.splice( 0, 1 );
+		} );
 
+	$( function () {
 		// Add relevant suggestion
 		$( '#mwProtect-reason, #wpReason, #mw-input-wpReason-other' ).suggestions( {
 			fetch: function () {
