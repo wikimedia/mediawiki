9d43b479d2bd22a44609f65ca5d0ad4642b60df5
Paladox <thomasmulhall410@yahoo.com>
2017-01-15 18:18:29 +0000

    Add support for searching namespaces other then NS_MAIN
    This adds wgSearchSuggestNamespaces config inside js.
    
    If your logged in, it will use the namespaces you have set.
    
    If your logged out, it will use the namespaces set in config
    $wgNamespacesToBeSearchedDefault
    
    Bug: T26214
    Change-Id: I6a545eb8563e81a66de39a8295ef2e40c3e73f95

--- resources/src/mediawiki/mediawiki.searchSuggest.js
+++ resources/src/mediawiki/mediawiki.searchSuggest.js
@@ -2,6 +2,11 @@
  * Add search suggestions to the search form.
  */
 ( function ( mw, $ ) {
+	var searchNS = $.map( mw.config.get( 'wgFormattedNamespaces' ), function ( nsName, nsID ) {
+		if ( nsID > 0 && mw.user.options.get( 'searchNs' + nsID ) ) {
+			return nsID;
+		}
+	} );
 	mw.searchSuggest = {
 		// queries the wiki and calls response with the result
 		request: function ( api, query, response, maxRows, namespace ) {
@@ -9,7 +14,7 @@
 				formatversion: 2,
 				action: 'opensearch',
 				search: query,
-				namespace: namespace || 0,
+				namespace: namespace || searchNS,
 				limit: maxRows,
 				suggest: true
 			} ).done( function ( data, jqXHR ) {
