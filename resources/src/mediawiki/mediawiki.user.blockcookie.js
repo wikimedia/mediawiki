3564830ebe96bfe81702dca20d408064786e5ddc
Sam Wilson <sam@samwilson.id.au>
2017-01-18 15:03:05 +0800

    Add an expiry to the localStorage replication of the block cookie
    The block cookie is replicated to localStorage in order that the cookie
    can be recreated if it's removed. However, this would have recreated
    the cookie even years after the expiry of the original cookie because
    localStorage never expires.
    
    This change adds an expiry time to the localStorage data (by switching
    it to be a JSON string with 'data' and 'expiry' keys) and only recreates
    the cookie if the expiry time is greater than the current time.
    
    Additionally, the loading of the mediawiki.user.blockcookie module is
    moved to only occur when a blocked user is trying to edit.
    
    Bug: T152952
    Change-Id: Ifb06dc2390f4d648d7fcb39e30267de5eddc6941

--- resources/src/mediawiki/mediawiki.user.blockcookie.js
+++ resources/src/mediawiki/mediawiki.user.blockcookie.js
@@ -1,23 +1,54 @@
 ( function ( mw ) {
+	'use strict';
 
 	// If a user has been autoblocked, a cookie is set.
 	// Its value is replicated here in localStorage to guard against cookie-removal.
+	// The localStorage item will be removed after $wgAutoblockExpiry (default 86400 seconds).
 	// This module will only be loaded when $wgCookieSetOnAutoblock is true.
-	// Ref: https://phabricator.wikimedia.org/T5233
+	// Ref: T5233
+	//
+	// This used to just store the block ID, but for T152952 was changed to also store an expiry time in order that the
+	// localStorage data be removed after $wgAutoblockExpiry (default 86400 seconds).
+	//
+	// Note that the cookie's name has an uppercase 'B' but the storage name has a lowercase 'b'.
+	mw.requestIdleCallback( function () {
+		var expiry, cookieVal, storageValue, storedObject, storedExpiry;
 
-	if ( !mw.cookie.get( 'BlockID' ) && mw.storage.get( 'blockID' ) ) {
-		// The block ID exists in storage, but not in the cookie.
-		mw.cookie.set( 'BlockID', mw.storage.get( 'blockID' ) );
+		// Calculate the expiry time (add milliseconds to current time).
+		expiry = Date.now() + ( mw.config.get( 'wgAutoblockExpiry' ) * 1000 );
 
-	} else if ( parseInt( mw.cookie.get( 'BlockID' ), 10 ) > 0 && !mw.storage.get( 'blockID' ) ) {
-		// The block ID exists in the cookie, but not in storage.
-		// (When a block expires the cookie remains but its value is '', hence the integer check above.)
-		mw.storage.set( 'blockID', mw.cookie.get( 'BlockID' ) );
+		// There are three parts to this next bit:
+		// 1. cookie doesn't exist and there's nothing in localStorage;
+		// 2. cookie does exist and there's nothing in localStorage; and
+		// 3. cookie is blank and there is something in localStorage.
 
-	} else if ( mw.cookie.get( 'BlockID' ) === '' && mw.storage.get( 'blockID' ) ) {
-		// If only the empty string is in the cookie, remove the storage value. The block is no longer valid.
-		mw.storage.remove( 'blockID' );
+		if ( !mw.cookie.get( 'BlockID' ) && mw.storage.get( 'blockID' ) ) {
+			// The block ID exists in storage, but the cookie doesn't exist, so we re-create the cookie.
+			// The cookie value is either just the whole stored value, or just the 'data' element if it exists.
+			storageValue = mw.storage.get( 'blockID' );
+			storedObject = JSON.parse( storageValue );
+			if ( typeof storedObject.data === 'undefined' ) {
+				// If there's no 'data' property, this must be an old-style localStorage item, so we give it an expiry date.
+				storedObject = { data: storageValue, expiry: expiry };
+			}
+			storedExpiry = new Date( storedObject.expiry );
+			if ( storedExpiry >= new Date() ) {
+				// localStorage data hasn't expired, so use it to recreate the cookie.
+				mw.cookie.set( 'BlockID', storedObject.data );
+			}
 
-	}
+		} else if ( mw.cookie.get( 'BlockID' ) !== null && !mw.storage.get( 'blockID' ) ) {
+			// The block ID exists in the cookie, but not in storage.
+			// (When a block expires the cookie remains but its value is ''.)
+			cookieVal = mw.cookie.get( 'BlockID' );
+			storedObject = { data: cookieVal, expiry: expiry };
+			mw.storage.set( 'blockID', JSON.stringify( storedObject ) );
+
+		} else if ( mw.cookie.get( 'BlockID' ) === '' && mw.storage.get( 'blockID' ) ) {
+			// If only the empty string is in the cookie, remove the storage value. The block is no longer valid.
+			mw.storage.remove( 'blockID' );
+
+		}
+	} );
 
 }( mediaWiki ) );
