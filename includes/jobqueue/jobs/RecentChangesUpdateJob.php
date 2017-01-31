4c7640d342a73ce4e41ac8985dc839981ed3e385
Marius Hoch <hoo@online.de>
2017-01-31 10:39:56 +0100

    Make RecentChangesUpdateJob::updateActiveUsers more robust
    Do work we can outside of the lock (purge no longer active users)
    and fail more gracefully if the lock is taken (if this is the case,
    we're probably on a high traffic wiki, so this job is going to
    run very often anyway, so no need to warn).
    
    Given this is an asynchronous people can't expect this to be always
    fully consistent with the actual state of the RC table anyway.
    
    Note: This will still sometimes log the warning (if we have a race
    between Database::lockIsFree and Database::lock), but that should be
    negligible.
    
    Bug: T156638
    Change-Id: I2e49a8e12bc64156d64a32ccf54911a76087346a

--- includes/jobqueue/jobs/RecentChangesUpdateJob.php
+++ includes/jobqueue/jobs/RecentChangesUpdateJob.php
@@ -128,8 +128,10 @@ class RecentChangesUpdateJob extends Job {
 				$dbw->setSessionOptions( [ 'connTimeout' => 900 ] );
 
 				$lockKey = wfWikiID() . '-activeusers';
-				if ( !$dbw->lock( $lockKey, __METHOD__, 1 ) ) {
-					return; // exclusive update (avoids duplicate entries)
+				if ( !$dbw->lockIsFree( $lockKey, __METHOD__ ) || !$dbw->lock( $lockKey, __METHOD__, 1 ) ) {
+					// Exclusive update (avoids duplicate entries)… it's usually fine to just drop out here, if the Job
+					// is already running.
+					return;
 				}
 
 				$nowUnix = time();
@@ -167,15 +169,6 @@ class RecentChangesUpdateJob extends Job {
 				foreach ( $res as $row ) {
 					$names[$row->rc_user_text] = $row->lastedittime;
 				}
-
-				// Rotate out users that have not edited in too long (according to old data set)
-				$dbw->delete( 'querycachetwo',
-					[
-						'qcc_type' => 'activeusers',
-						'qcc_value < ' . $dbw->addQuotes( $nowUnix - $days * 86400 ) // TS_UNIX
-					],
-					__METHOD__
-				);
 
 				// Find which of the recently active users are already accounted for
 				if ( count( $names ) ) {
@@ -224,6 +217,16 @@ class RecentChangesUpdateJob extends Job {
 				);
 
 				$dbw->unlock( $lockKey, __METHOD__ );
+
+				// Rotate out users that have not edited in too long (according to old data set)
+				// Note: This can be done after the lock has been given up, conflicts don't matter here.
+				$dbw->delete( 'querycachetwo',
+					[
+						'qcc_type' => 'activeusers',
+						'qcc_value < ' . $dbw->addQuotes( $nowUnix - $days * 86400 ) // TS_UNIX
+					],
+					__METHOD__
+				);
 			},
 			__METHOD__
 		);
