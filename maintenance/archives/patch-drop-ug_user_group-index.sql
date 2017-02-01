f30df2511ef8437f2445aa6ed296dd7efa5a6875
This, that and the other <at.light@live.com.au>
2017-02-01 21:23:05 +1100

    Don't try to drop ug_user_group index if it doesn't exist
    This is needed for MySQL only.
    
    Bug: T156901
    Change-Id: I351fa1b7ce5a9b1c7b19ecb7d26d8a1f699d88f8

---
+++ maintenance/archives/patch-drop-ug_user_group-index.sql
@@ -0,0 +1,5 @@
+-- Drop redundant unique index, to be replaced by a primary key in the next
+-- sequential update (patch-user_groups-ug_expiry.sql)
+
+ALTER TABLE /*$wgDBprefix*/user_groups
+  DROP INDEX ug_user_group;
--- maintenance/archives/patch-user_groups-ug_expiry.sql
+++ maintenance/archives/patch-user_groups-ug_expiry.sql
@@ -1,7 +1,6 @@
 -- Primary key and expiry column in user_groups table
 
 ALTER TABLE /*$wgDBprefix*/user_groups
-  DROP INDEX ug_user_group,
   ADD PRIMARY KEY (ug_user, ug_group),
   ADD COLUMN ug_expiry varbinary(14) NULL default NULL,
   ADD INDEX ug_expiry (ug_expiry);
