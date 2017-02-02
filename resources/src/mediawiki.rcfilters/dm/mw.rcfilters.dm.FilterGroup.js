b46f79dff00244ab32850f05fc3faef29e3a8eb3
Moriel Schottlender <moriel@gmail.com>
2017-02-02 12:13:00 -0800

    [wip^n] Define interaction states for filters
    Interactions of 'conflicts', 'subset's and whether a group is
    'full coverage'.
    
    Bug: T156864
    Bug: T156861
    Bug: T156860
    Change-Id: If20bbe9f1442cfcfce046e56f6150b38dd3a4efc

--- resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.FilterGroup.js
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.FilterGroup.js
@@ -10,21 +10,26 @@
 	 * @cfg {string} [type='send_unselected_if_any'] Group type
 	 * @cfg {string} [title] Group title
 	 * @cfg {string} [separator='|'] Value separator for 'string_options' groups
-	 * @cfg {string} [exclusionType='default'] Group exclusion type
 	 * @cfg {boolean} [active] Group is active
+	 * @cfg {boolean} [fullCoverage] This group has items that are full coverage
 	 */
-	mw.rcfilters.dm.FilterGroup = function MwRcfiltersDmFilterGroup( config ) {
+	mw.rcfilters.dm.FilterGroup = function MwRcfiltersDmFilterGroup( name, config ) {
 		config = config || {};
 
 		// Mixin constructor
 		OO.EventEmitter.call( this );
 		OO.EmitterList.call( this );
 
+		this.name = name;
 		this.type = config.type || 'send_unselected_if_any';
 		this.title = config.title;
 		this.separator = config.separator || '|';
-		this.exclusionType = config.exclusionType || 'default';
+
 		this.active = !!config.active;
+		this.fullCoverage = !!config.fullCoverage;
+
+		this.aggregate( { update: 'filterItemUpdate' } );
+		this.connect( this, { filterItemUpdate: 'onFilterItemUpdate' } );
 	};
 
 	/* Initialization */
@@ -43,23 +48,13 @@
 	/* Methods */
 
 	/**
-	 * Check the active status of the group and set it accordingly.
+	 * Respond to filterItem update event
 	 *
 	 * @fires update
 	 */
-	mw.rcfilters.dm.FilterGroup.prototype.checkActive = function () {
-		var active,
-			count = 0;
-
-		// Recheck group activity
-		this.getItems().forEach( function ( filterItem ) {
-			count += Number( filterItem.isSelected() );
-		} );
-
-		active = (
-			count > 0 &&
-			count < this.getItemCount()
-		);
+	mw.rcfilters.dm.FilterGroup.prototype.onFilterItemUpdate = function () {
+		// Update state
+		var active = this.areAnySelected();
 
 		if ( this.active !== active ) {
 			this.active = active;
@@ -74,6 +69,74 @@
 	 */
 	mw.rcfilters.dm.FilterGroup.prototype.isActive = function () {
 		return this.active;
+	};
+
+	/**
+	 * Get group name
+	 *
+	 * @return {string} Group name
+	 */
+	mw.rcfilters.dm.FilterGroup.prototype.getName = function () {
+		return this.name;
+	};
+
+	/**
+	 * Check whether there are any items selected
+	 */
+	mw.rcfilters.dm.FilterGroup.prototype.areAnySelected = function () {
+		return this.getItems().some( function ( filterItem ) {
+			return filterItem.isSelected();
+		} );
+	};
+
+	/**
+	 * Check whether all items selected
+	 */
+	mw.rcfilters.dm.FilterGroup.prototype.areAllSelected = function () {
+		return this.getItems().every( function ( filterItem ) {
+			return filterItem.isSelected();
+		} );
+	};
+
+	/**
+	 * Get all selected items in this group
+	 *
+	 * @param {mw.rcfilters.dm.FilterItem} [excludeItem] Item to exclude from the list
+	 * @return {mw.rcfilters.dm.FilterItem[]} Selected items
+	 */
+	mw.rcfilters.dm.FilterGroup.prototype.getSelectedItems = function ( excludeItem ) {
+		var items = [],
+			excludeName = ( excludeItem && excludeItem.getName() ) || '';
+
+		this.getItems().forEach( function ( item ) {
+			if ( item.getName() !== excludeName && item.isSelected() ) {
+				items.push( item );
+			}
+		} );
+
+		return items;
+	};
+
+	/**
+	 * Check whether this group has all its selected items conflicting
+	 * with the given item
+	 *
+	 * @param {mw.rcfilters.dm.FilterItem} filterItem Filter item to check conflicts against
+	 * @return {boolean} All selected items are in conflict with the given filter
+	 */
+	mw.rcfilters.dm.FilterGroup.prototype.areAllSelectedConflictWith = function ( filterItem ) {
+		var selectedItems = this.getSelectedItems( filterItem );
+
+		if ( selectedItems.count === 0 ) {
+			return false;
+		}
+
+		return selectedItems.every( function ( selectedItem ) {
+			return (
+				selectedItem.getName() !== filterItem.getName() &&
+				selectedItem.hasConflictWith( filterItem )
+			);
+		} );
 	};
 
 	/**
@@ -104,11 +167,11 @@
 	};
 
 	/**
-	 * Get group exclusion type
+	 * Check whether the group is defined as full coverage
 	 *
-	 * @return {string} Exclusion type
+	 * @return {boolean} Group is full coverage
 	 */
-	mw.rcfilters.dm.FilterGroup.prototype.getExclusionType = function () {
-		return this.exclusionType;
+	mw.rcfilters.dm.FilterGroup.prototype.isFullCoverage = function () {
+		return this.fullCoverage;
 	};
 }( mediaWiki ) );
