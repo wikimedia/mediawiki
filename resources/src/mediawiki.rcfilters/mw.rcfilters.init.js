ddea7f30e3f58b4283a63749c74d3344efd41acd
Moriel Schottlender <moriel@gmail.com>
2017-02-02 12:13:00 -0800

    RCFilters UI: Define interaction states for filters
    This patch sets up the ground for all three interaction types:
    'subset', 'conflict' and 'coverage' as toggle-able properties
    of the item and group models, and sets up the widgets' initial
    logic abou their own "mute" state.
    
    Even though some of the logic ('isConflicted' etc) may not yet
    work, it sets up the widgets to read them without breaking
    functionality, allowing us to add up the model behavior in
    subsequent patches and make things work seamlessly.
    
    The patch also includes the base logic for calcuting interaction
    with subsets (and "supersets" that are derived from them) when
    an item is selected.
    
    Upcoming patches will handle the state-change logic of conflict
    and coverage.
    
    Bug: T156864
    Bug: T156861
    Bug: T156860
    Change-Id: If20bbe9f1442cfcfce046e56f6150b38dd3a4efc

--- resources/src/mediawiki.rcfilters/mw.rcfilters.init.js
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.init.js
@@ -17,6 +17,7 @@
 				registration: {
 					title: mw.msg( 'rcfilters-filtergroup-registration' ),
 					type: 'send_unselected_if_any',
+					fullCoverage: true,
 					filters: [
 						{
 							name: 'hideliu',
@@ -39,6 +40,7 @@
 					// ** In this case, the parameter name is the group name. **
 					type: 'string_options',
 					separator: ',',
+					fullCoverage: false,
 					filters: [
 						{
 							name: 'newcomer',
@@ -68,6 +70,7 @@
 					// the functionality to the UI, whether we are dealing with 2
 					// parameters in the group or more.
 					type: 'send_unselected_if_any',
+					fullCoverage: true,
 					filters: [
 						{
 							name: 'hidemyself',
@@ -84,6 +87,7 @@
 				automated: {
 					title: mw.msg( 'rcfilters-filtergroup-automated' ),
 					type: 'send_unselected_if_any',
+					fullCoverage: true,
 					filters: [
 						{
 							name: 'hidebots',
@@ -102,6 +106,7 @@
 				significance: {
 					title: mw.msg( 'rcfilters-filtergroup-significance' ),
 					type: 'send_unselected_if_any',
+					fullCoverage: true,
 					filters: [
 						{
 							name: 'hideminor',
@@ -118,6 +123,7 @@
 				changetype: {
 					title: mw.msg( 'rcfilters-filtergroup-changetype' ),
 					type: 'send_unselected_if_any',
+					fullCoverage: true,
 					filters: [
 						{
 							name: 'hidepageedits',
@@ -141,7 +147,9 @@
 							name: 'hidelog',
 							label: mw.msg( 'rcfilters-filter-logactions-label' ),
 							description: mw.msg( 'rcfilters-filter-logactions-description' ),
-							'default': false
+							'default': false,
+							// Hack: This is for TESTING ONLY!
+							conflicts: [ 'hidehumans' ]
 						}
 					]
 				}
