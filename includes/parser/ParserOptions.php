31170ba0ec9c63492f363bc64423767f17ef5eb6
Brian Wolff <bawolff+wn@gmail.com>
2017-02-06 03:00:39 +0000

    SECURITY: Disable <html> tag on system messages despite $wgRawHtml = true;
    System messages may take parameters from untrusted sources. This
    may include taking parameters from urls given by unauthenticated
    users even if the wiki is a read-only wiki. Allowing <html> tags
    in such a context seems like an accident waiting to happen.
    
    Bug: T156184
    Change-Id: I661f482986d319cf41da1d3e7b20a0f028a42e90

--- includes/parser/ParserOptions.php
+++ includes/parser/ParserOptions.php
@@ -243,6 +243,21 @@ class ParserOptions {
 	 */
 	private $redirectTarget = null;
 
+	/**
+	 * If the wiki is configured to allow raw html ($wgRawHtml = true)
+	 * is it allowed in the specific case of parsing this page.
+	 *
+	 * This is meant to disable unsafe parser tags in cases where
+	 * a malicious user may control the input to the parser.
+	 *
+	 * @note This is expected to be true for normal pages even if the
+	 *  wiki has $wgRawHtml disabled in general. The setting only
+	 *  signifies that raw html would be unsafe in the current context
+	 *  provided that raw html is allowed at all.
+	 * @var boolean
+	 */
+	private $allowUnsafeRawHtml = true;
+
 	public function getInterwikiMagic() {
 		return $this->mInterwikiMagic;
 	}
@@ -457,6 +472,15 @@ class ParserOptions {
 	public function getMagicRFCLinks() {
 		return $this->mMagicRFCLinks;
 	}
+
+	/**
+	 * @since 1.29
+	 * @return bool
+	 */
+	public function getAllowUnsafeRawHtml() {
+		return $this->allowUnsafeRawHtml;
+	}
+
 	public function setInterwikiMagic( $x ) {
 		return wfSetVar( $this->mInterwikiMagic, $x );
 	}
@@ -594,6 +618,15 @@ class ParserOptions {
 
 	public function setIsPrintable( $x ) {
 		return wfSetVar( $this->mIsPrintable, $x );
+	}
+
+	/**
+	 * @param bool|null Value to set or null to get current value
+	 * @return bool Current value for allowUnsafeRawHtml
+	 * @since 1.29
+	 */
+	public function setAllowUnsafeRawHtml( $x ) {
+		return wfSetVar( $this->allowUnsafeRawHtml, $x );
 	}
 
 	/**
