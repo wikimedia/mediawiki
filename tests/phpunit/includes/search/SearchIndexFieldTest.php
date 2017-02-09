2faee95bd72c659927d4584135c29cfbc99b633c
Stanislav Malyshev <smalyshev@gmail.com>
2017-02-09 13:30:12 -0800

    Allow fields to define their own merge strategy via callback.
    Change-Id: Ic1cc1581f07381224d3f4fcba4feb1eac7085057

--- tests/phpunit/includes/search/SearchIndexFieldTest.php
+++ tests/phpunit/includes/search/SearchIndexFieldTest.php
@@ -9,8 +9,13 @@ class SearchIndexFieldTest extends MediaWikiTestCase {
 	public function getMergeCases() {
 		return [
 			[ 0, 'test', 0, 'test', true ],
-			[ SearchIndexField::INDEX_TYPE_NESTED, 'test',
-			  SearchIndexField::INDEX_TYPE_NESTED, 'test', false ],
+			[
+				SearchIndexField::INDEX_TYPE_NESTED,
+				'test',
+				SearchIndexField::INDEX_TYPE_NESTED,
+				'test',
+				false
+			],
 			[ 0, 'test', 0, 'test2', true ],
 			[ 0, 'test', 1, 'test', false ],
 		];
@@ -18,14 +23,23 @@ class SearchIndexFieldTest extends MediaWikiTestCase {
 
 	/**
 	 * @dataProvider getMergeCases
+	 * @param $t1
+	 * @param $n1
+	 * @param $t2
+	 * @param $n2
+	 * @param $result
 	 */
 	public function testMerge( $t1, $n1, $t2, $n2, $result ) {
-		$field1 = $this->getMockBuilder( 'SearchIndexFieldDefinition' )
-			->setMethods( [ 'getMapping' ] )
-			->setConstructorArgs( [ $n1, $t1 ] )->getMock();
-		$field2 = $this->getMockBuilder( 'SearchIndexFieldDefinition' )
-			->setMethods( [ 'getMapping' ] )
-			->setConstructorArgs( [ $n2, $t2 ] )->getMock();
+		$field1 =
+			$this->getMockBuilder( SearchIndexFieldDefinition::class )
+				->setMethods( [ 'getMapping' ] )
+				->setConstructorArgs( [ $n1, $t1 ] )
+				->getMock();
+		$field2 =
+			$this->getMockBuilder( SearchIndexFieldDefinition::class )
+				->setMethods( [ 'getMapping' ] )
+				->setConstructorArgs( [ $n2, $t2 ] )
+				->getMock();
 
 		if ( $result ) {
 			$this->assertNotFalse( $field1->merge( $field2 ) );
@@ -35,5 +49,10 @@ class SearchIndexFieldTest extends MediaWikiTestCase {
 
 		$field1->setFlag( 0xFF );
 		$this->assertFalse( $field1->merge( $field2 ) );
+
+		$field1->setMergeCallback( function ( $that ) { return "test"; } );
+		$this->assertEquals( "test", $field1->merge( $field2 ) );
+
 	}
+
 }
