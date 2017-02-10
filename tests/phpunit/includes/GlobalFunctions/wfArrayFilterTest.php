742c47eddb3ec190c3d207991b6819f06ba293fa
Gergő Tisza <gtisza@wikimedia.org>
2017-02-10 05:31:32 +0000

    Expose basic properties of change list lines as data attributes
    We have several types of change lists (old RC/watchlist/related
    changes, enhanced RC/watchlist, history) with slightly different
    HTML, each with their own idiosyncracies. Javascript code trying
    to identify lines by log ID / revision ID has to jump through all
    kinds of hoops to work with that.
    
    To simplify the live of frontend / gadget maintainers and provide
    something approaching an API for these pages, expose the basic
    attributes of change lines (revision ID for edits, log type/action
    and ID for log events) as data attributes.
    
    The OldChangesListRecentChangesLine, EnhancedChangesListModifyLineData,
    EnhancedChangesListModifyBlockLineData, PageHistoryLine,
    ContributionsLineEnding and DeletedContributionsLineEnding hooks
    are updated accordingly. New hooks (LogEventsListLineEnding and
    NewPagesLineEnding) are added for the change list pages which did
    not have them so far.
    
    Change-Id: I6dd006d0b1b0fd35c0020f0f9eea9113eca30b35

---
+++ tests/phpunit/includes/GlobalFunctions/wfArrayFilterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+class WfArrayFilterTest extends \PHPUnit_Framework_TestCase {
+	public function testWfArrayFilter() {
+		$arr = [ 'a' => 1, 'b' => 2, 'c' => 3 ];
+		$filtered = wfArrayFilter( $arr, function( $val, $key ) {
+			return $key !== 'b';
+		} );
+		$this->assertSame( [ 'a' => 1, 'c' => 3 ], $filtered );
+
+		$arr = [ 'a' => 1, 'b' => 2, 'c' => 3 ];
+		$filtered = wfArrayFilter( $arr, function( $val, $key ) {
+			return $val !== 2;
+		} );
+		$this->assertSame( [ 'a' => 1, 'c' => 3 ], $filtered );
+
+		$arr = [ 'a', 'b', 'c' ];
+		$filtered = wfArrayFilter( $arr, function( $val, $key ) {
+			return $key !== 0;
+		} );
+		$this->assertSame( [ 1 => 'b',  2 => 'c' ], $filtered );
+	}
+
+	public function testWfArrayFilterByKey() {
+		$arr = [ 'a' => 1, 'b' => 2, 'c' => 3 ];
+		$filtered = wfArrayFilterByKey( $arr, function( $key ) {
+			return $key !== 'b';
+		} );
+		$this->assertSame( [ 'a' => 1, 'c' => 3 ], $filtered );
+
+		$arr = [ 'a', 'b', 'c' ];
+		$filtered = wfArrayFilterByKey( $arr, function( $key ) {
+			return $key !== 0;
+		} );
+		$this->assertSame( [ 1 => 'b',  2 => 'c' ], $filtered );
+	}
+}
