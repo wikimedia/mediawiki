29a6d1f61a56cf8c68ff11e28233aa43cb2c97dc
Stephane Bisson <sbisson@wikimedia.org>
2017-02-10 09:18:02 -0500

    RCFilters UI: Highlight behavior
    Let there be highlight! and there were highlights
    And RCFilters separated the highlight from the darkness
    And it defined highlights as five colors
    The lights are called yellow and green, and the darks red and blue
    And there were colors and there were circles; one highlight.
    
    This is the commit that adds highlight support for filters both in the backend
    and the UI. The backend tags results based on which filter they fit and the
    front end paints those results according to the color chosen by the user.
    Highlights can be toggled off and on.
    
    Also added circle indicators to the capsule items and each line of results
    to indicate whether the line has more than one color affecting it.
    
    Bug: T149467
    Bug: T156164
    Change-Id: I341c3f7c224271a18d455b9e5f5457ec43de802d

---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.HighlightColorPickerWidget.js
@@ -0,0 +1,112 @@
+( function ( mw, $ ) {
+	/**
+	 * A widget representing a filter item highlight color picker
+	 *
+	 * @extends OO.ui.Widget
+	 * @mixins OO.ui.mixin.LabelElement
+	 *
+	 * @constructor
+	 * @param {mw.rcfilters.Controller} controller RCFilters controller
+	 * @param {mw.rcfilters.dm.FilterItem} model Filter item model
+	 * @param {Object} [config] Configuration object
+	 */
+	mw.rcfilters.ui.HighlightColorPickerWidget = function MwRcfiltersUiHighlightColorPickerWidget( controller, model, config ) {
+		var colors = [ 'none' ].concat( mw.rcfilters.HighlightColors );
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.HighlightColorPickerWidget.parent.call( this, config );
+		// Mixin constructors
+		OO.ui.mixin.LabelElement.call( this, $.extend( {}, config, {
+			label: mw.message( 'rcfilters-highlightmenu-title' ).text()
+		} ) );
+
+		this.controller = controller;
+		this.model = model;
+
+		this.currentSelection = '';
+		this.buttonSelect = new OO.ui.ButtonSelectWidget( {
+			items: colors.map( function ( color ) {
+				return new OO.ui.ButtonOptionWidget( {
+					icon: color === 'none' ? 'check' : null,
+					data: color,
+					classes: [
+						'mw-rcfilters-ui-highlightColorPickerWidget-buttonSelect-color',
+						'mw-rcfilters-ui-highlightColorPickerWidget-buttonSelect-color-' + color
+					],
+					framed: false
+				} );
+			} ),
+			classes: 'mw-rcfilters-ui-highlightColorPickerWidget-buttonSelect'
+		} );
+		this.selectColor( 'none' );
+
+		// Event
+		this.model.connect( this, { update: 'onModelUpdate' } );
+		this.buttonSelect.connect( this, { choose: 'onChooseColor' } );
+
+		this.$element
+			.addClass( 'mw-rcfilters-ui-highlightColorPickerWidget' )
+			.append(
+				this.$label
+					.addClass( 'mw-rcfilters-ui-highlightColorPickerWidget-label' ),
+				this.buttonSelect.$element
+			);
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.ui.HighlightColorPickerWidget, OO.ui.Widget );
+	OO.mixinClass( mw.rcfilters.ui.HighlightColorPickerWidget, OO.ui.mixin.LabelElement );
+
+	/* Events */
+
+	/**
+	 * @event chooseColor
+	 * @param {string} The chosen color
+	 *
+	 * A color has been chosen
+	 */
+
+	/* Methods */
+
+	/**
+	 * Respond to item model update event
+	 */
+	mw.rcfilters.ui.HighlightColorPickerWidget.prototype.onModelUpdate = function () {
+		this.selectColor( this.model.getHighlightColor() || 'none' );
+	};
+
+	/**
+	 * Select the color for this widget
+	 *
+	 * @param {string} color Selected color
+	 */
+	mw.rcfilters.ui.HighlightColorPickerWidget.prototype.selectColor = function ( color ) {
+		var previousItem = this.buttonSelect.getItemFromData( this.currentSelection ),
+			selectedItem = this.buttonSelect.getItemFromData( color );
+
+		if ( this.currentSelection !== color ) {
+			this.currentSelection = color;
+
+			this.buttonSelect.selectItem( selectedItem );
+			if ( previousItem ) {
+				previousItem.setIcon( null );
+			}
+
+			if ( selectedItem ) {
+				selectedItem.setIcon( 'check' );
+			}
+		}
+	};
+
+	mw.rcfilters.ui.HighlightColorPickerWidget.prototype.onChooseColor = function ( button ) {
+		var color = button.data;
+		if ( color === 'none' ) {
+			this.controller.clearHighlightColor( this.model.getName() );
+		} else {
+			this.controller.setHighlightColor( this.model.getName(), color );
+		}
+		this.emit( 'chooseColor', color );
+	};
+}( mediaWiki, jQuery ) );
