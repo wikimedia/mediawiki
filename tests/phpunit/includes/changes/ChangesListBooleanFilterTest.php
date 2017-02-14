b044bfd91a4d7d5fda013305c4f5442dc0d8a0f8
Matthew Flaschen <mflaschen@wikimedia.org>
2017-02-14 02:55:37 -0500

    Back-end of new RecentChanges page, refactoring
    Generate old RC, Related changes (it was already displayed and working
    on 'Related changes' before this change), and Watchlist/etc. and data
    for new UI from back-end.
    
    This moves everything used for defining the old (unstructured) and new
    (structured) filters into unified objects, ChangesListFilter and
    ChangesListFilterGroup (and sub-classes).
    
    This includes the query logic (see below) and logic for adding
    CSS attribution classes.
    
    This is a breaking change (for subclasses of ChangesListSpecialpage)
    due to the signature (and name) change of buildMainQueryConds and
    doMainQuery.  An alternative that I don't think is a breaking change
    would be to put the filter->DB logic in runMainQueryHook, but then it's
    doing more than just running a hook.
    
    This is because it used to only build $conds here, but it's clear from
    filterOnUserExperienceLevel filters need more than this.  I added all
    the DB parameters from the hook, but this could be debated.
    
    I have an checked and fixed the WMF-deployed extensions affected by
    this.
    
    Other than that, there should be full back-compat (including legacy
    filters not using the new system).
    
    * add hidepatrolled/hideunpatrolled to new UI.
    
    * Move userExpLevel from RC to ChangesListSpecialPage.  Although for
    now the structured UI only displays on RC anyway, when it displays on
    watchlist, it seems we'll want userExpLevel there.
    
      Change this to make 'all' exclude unregistered users.
    
    * Don't have front-end convert none-selected to 'all' on string_options.
    
    * Needs the hideanons/hideliu special redirect to be done before this
    is merged (T151873)
    
    Bug: T152754
    Bug: T152797
    Change-Id: Iec2d82f6a830403d1c948a280efa58992e0cdee7

---
+++ tests/phpunit/includes/changes/ChangesListBooleanFilterTest.php
@@ -0,0 +1,225 @@
+<?php
+
+/**
+ * @covers ChangesListBooleanFilter
+ */
+class ChangesListBooleanFilterTest extends MediaWikiTestCase {
+	public function testGetJsData() {
+		$group = new ChangesListBooleanFilterGroup( [
+			'name' => 'group',
+			'priority' => 2,
+			'filters' => [],
+		] );
+
+		$definition = [
+			'group' => $group,
+			'label' => 'main-label',
+			'description' => 'main-description',
+			'default' => 1,
+			'priority' => 1,
+		];
+
+		$fooFilter = new ChangesListBooleanFilter(
+			$definition + [ 'name' => 'hidefoo' ]
+		);
+
+		$barFilter = new ChangesListBooleanFilter(
+			$definition + [ 'name' => 'hidebar' ]
+		);
+
+		$bazFilter = new ChangesListBooleanFilter(
+			$definition + [ 'name' => 'hidebaz' ]
+		);
+
+		$fooFilter->conflictsWith(
+			$barFilter,
+			'foo-bar-global-conflict',
+			'foo-conflicts-bar',
+			'bar-conflicts-foo'
+		);
+
+		$fooFilter->setAsSupersetOf( $bazFilter, 'foo-superset-of-baz' );
+
+		$fooData = $fooFilter->getJsData();
+		$this->assertArrayEquals(
+			[
+				'name' => 'hidefoo',
+				'label' => 'main-label',
+				'description' => 'main-description',
+				'default' => 1,
+				'priority' => 1,
+				'cssClass' => null,
+				'conflicts' => [
+					[
+						'group' => 'group',
+						'filter' => 'hidebar',
+						'globalDescription' => 'foo-bar-global-conflict',
+						'contextDescription' => 'foo-conflicts-bar',
+					]
+				],
+				'subset' => [
+					[
+						'group' => 'group',
+						'filter' => 'hidebaz',
+						'contextDescription' => 'foo-superset-of-baz',
+					],
+
+				],
+				'messageKeys' => [
+					'main-label',
+					'main-description',
+					'foo-bar-global-conflict',
+					'foo-conflicts-bar',
+					'foo-superset-of-baz',
+				],
+			],
+			$fooData,
+			/** ordered= */ false,
+			/** named= */ true
+		);
+
+		$barData = $barFilter->getJsData();
+		$this->assertArrayEquals(
+			[
+				'name' => 'hidebar',
+				'label' => 'main-label',
+				'description' => 'main-description',
+				'default' => 1,
+				'priority' => 1,
+				'cssClass' => null,
+				'conflicts' => [
+					[
+						'group' => 'group',
+						'filter' => 'hidefoo',
+						'globalDescription' => 'foo-bar-global-conflict',
+						'contextDescription' => 'bar-conflicts-foo',
+					]
+				],
+				'subset' => [],
+				'messageKeys' => [
+					'main-label',
+					'main-description',
+					'foo-bar-global-conflict',
+					'bar-conflicts-foo',
+				],
+			],
+			$barData,
+			/** ordered= */ false,
+			/** named= */ true
+		);
+	}
+
+	/**
+	 * @expectedException MWException
+	 * @expectedExceptionMessage Supersets can only be defined for filters in the same group
+	 */
+	public function testSetAsSupersetOf() {
+		$groupA = new ChangesListBooleanFilterGroup( [
+			'name' => 'groupA',
+			'priority' => 2,
+			'filters' => [
+				[
+					'name' => 'foo',
+					'default' => false,
+				],
+				[
+					'name' => 'bar',
+					'default' => false,
+				]
+			],
+		] );
+
+		$groupB = new ChangesListBooleanFilterGroup( [
+			'name' => 'groupB',
+			'priority' => 3,
+			'filters' => [
+				[
+					'name' => 'baz',
+					'default' => true,
+				],
+			],
+		] );
+
+		$foo = TestingAccessWrapper::newFromObject( $groupA->getFilter( 'foo' ) );
+
+		$bar = $groupA->getFilter( 'bar' );
+
+		$baz = $groupB->getFilter( 'baz' );
+
+		$foo->setAsSupersetOf( $bar, 'foo-superset-bar' );
+		$this->assertArrayEquals( [
+				[
+					'group' => 'groupA',
+					'filter' => 'bar',
+					'contextDescription' => 'foo-superset-bar',
+				],
+			],
+			$foo->subsetFilters,
+			/** ordered= */ false,
+			/** named= */ true
+		);
+
+		$foo->setAsSupersetOf( $baz, 'some-message' );
+	}
+
+	public function testIsFeatureAvailableOnStructuredUi() {
+		$specialPage = $this->getMockBuilder( 'ChangesListSpecialPage' )
+			->setConstructorArgs( [
+					'ChangesListSpecialPage',
+					'',
+				] )
+			->getMockForAbstractClass();
+
+		$groupA = new ChangesListBooleanFilterGroup( [
+			'name' => 'groupA',
+			'priority' => 1,
+			'filters' => [],
+		] );
+
+		$foo = new ChangesListBooleanFilter( [
+			'name' => 'hidefoo',
+			'group' => $groupA,
+			'label' => 'foo-label',
+			'description' => 'foo-description',
+			'default' => true,
+			'showHide' => 'showhidefoo',
+			'priority' => 2,
+		] );
+
+		$this->assertEquals(
+			true,
+			$foo->isFeatureAvailableOnStructuredUi( $specialPage ),
+			'Same filter appears on both'
+		);
+
+		// Should only be legacy ones that haven't been ported yet
+		$bar = new ChangesListBooleanFilter( [
+			'name' => 'hidebar',
+			'default' => true,
+			'group' => $groupA,
+			'showHide' => 'showhidebar',
+			'priority' => 2,
+		] );
+
+		$this->assertEquals(
+			false,
+			$bar->isFeatureAvailableOnStructuredUi( $specialPage ),
+			'Only on unstructured UI'
+		);
+
+		$baz = new ChangesListBooleanFilter( [
+			'name' => 'hidebaz',
+			'default' => true,
+			'group' => $groupA,
+			'showHide' => 'showhidebaz',
+			'isReplacedInStructuredUi' => true,
+			'priority' => 2,
+		] );
+
+		$this->assertEquals(
+			true,
+			$baz->isFeatureAvailableOnStructuredUi( $specialPage ),
+			'Legacy filter does not appear directly in new UI, but equivalent does and is marked with isReplacedInStructuredUi'
+		);
+	}
+}
