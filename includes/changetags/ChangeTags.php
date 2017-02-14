66b6bb3a0bc0c6c881c27137d81ade8058cb7067
Matthias Mullie <git@mullie.eu>
2017-02-14 12:39:02 +0100

    Enable extensions to allow software-specific tags to be added by users
    Bug: T121880
    Bug: T121872
    Change-Id: I6e054cfc065c8d8ae7d20a518b1963cd0551c5c2

--- includes/changetags/ChangeTags.php
+++ includes/changetags/ChangeTags.php
@@ -402,8 +402,8 @@ class ChangeTags {
 		}
 
 		// to be applied, a tag has to be explicitly defined
-		// @todo Allow extensions to define tags that can be applied by users...
 		$allowedTags = self::listExplicitlyDefinedTags();
+		Hooks::run( 'ChangeTagsAllowedAdd', [ &$allowedTags, $user, $tags ] );
 		$disallowedTags = array_diff( $tags, $allowedTags );
 		if ( $disallowedTags ) {
 			return self::restrictedTagError( 'tags-apply-not-allowed-one',
