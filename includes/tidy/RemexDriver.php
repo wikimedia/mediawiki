082e32f9a844d15fb984f2b34f542e8f08f21853
Tim Starling <tstarling@wikimedia.org>
2017-02-17 15:10:15 +1100

    RemexHtml tidy driver with p-wrapping
    Pull in the RemexHtml library, which is an HTML 5 library I recently
    created.
    
    RemexCompatMunger mutates the event stream, inserting <mw:p-wrap>
    elements where necessary, and occasionally taking even more invasive
    action such as reparenting and removing nodes maintained in Serializer's
    tree.
    
    RemexCompatFormatter produces a MediaWiki-style serialization which is
    relatively compatible with existing parser tests. It also does final
    empty element handling, including translating <mw:p-wrap> to <p>
    
    Tests are imported from both Html5Depurate and Subbu's pwrap.js.
    
    Change-Id: I900155b7dd199b0ae2a3b9cdb6db5136fc4f35a8

---
+++ includes/tidy/RemexDriver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MediaWiki\Tidy;
+
+use RemexHtml\Serializer\Serializer;
+use RemexHtml\Tokenizer\Tokenizer;
+use RemexHtml\TreeBuilder\Dispatcher;
+use RemexHtml\TreeBuilder\TreeBuilder;
+
+
+class RemexDriver extends TidyDriverBase {
+	public function __construct( array $config ) {
+		parent::__construct( $config );
+	}
+
+	public function tidy( $text ) {
+		$formatter = new RemexCompatFormatter;
+		$serializer = new Serializer( $formatter );
+		$munger = new RemexCompatMunger( $serializer );
+		$treeBuilder = new TreeBuilder( $munger, [
+			'ignoreErrors' => true,
+			'ignoreNulls' => true,
+		] );
+		$dispatcher = new Dispatcher( $treeBuilder );
+		$tokenizer = new Tokenizer( $dispatcher, $text, [
+			'ignoreErrors' => true,
+			'ignoreCharRefs' => true,
+			'ignoreNulls' => true,
+			'skipPreprocess' => true,
+		] );
+		$tokenizer->execute( [
+			'fragmentNamespace' => \RemexHtml\HTMLData::NS_HTML,
+			'fragmentName' => 'body'
+		] );
+		return $serializer->getResult();
+	}
+}
