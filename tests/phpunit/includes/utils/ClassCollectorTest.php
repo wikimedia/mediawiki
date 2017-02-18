ce4537000cff059202c6bfa9f51b5c1d88ab111d
Timo Tijhof <krinklemail@gmail.com>
2017-02-17 19:11:33 -0800

    AutoloadGenerator: Add support for class_alias()
    Blob, Field, DatabaseBase are now auto-detected.
    
    Change-Id: Ib8fae2ec3fbb3f5e4aca7965f81631c5f0485ea1

---
+++ tests/phpunit/includes/utils/ClassCollectorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @covers ClassCollector
+ */
+class ClassCollectorTest extends PHPUnit_Framework_TestCase {
+
+	public static function provideCases() {
+		return [
+			[
+				"class Foo {}",
+				[ 'Foo' ],
+			],
+			[
+				"namespace Example;\nclass Foo {}\nclass Bar {}",
+				[ 'Example\Foo', 'Example\Bar' ],
+			],
+			[
+				"class_alias( 'Foo', 'Bar' );",
+				[ 'Bar' ],
+			],
+			[
+				"namespace Example;\nclass Foo {}\nclass_alias( 'Example\Foo', 'Foo' );",
+				[ 'Example\Foo', 'Foo' ],
+			],
+			[
+				"namespace Example;\nclass Foo {}\nclass_alias( 'Example\Foo', 'Bar' );",
+				[ 'Example\Foo', 'Bar' ],
+			],
+			[
+				"class_alias( Foo::class, 'Bar' );",
+				[ 'Bar' ],
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideCases
+	 */
+	public function testGetClasses( $code, array $classes, $message = null ) {
+		$cc = new ClassCollector();
+		$this->assertEquals( $classes, $cc->getClasses( "<?php\n $code" ), $message );
+	}
+}
