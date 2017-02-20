12d97aa833ea42d8348a52cea3b195588153dd9e
Gergő Tisza <tgr.huwiki@gmail.com>
2017-02-20 15:33:02 -0800

    Do not mix suppressed errors with non-suppressed ones
    Bug: T45086
    Change-Id: I34b19837c391ff4acdb55fc0c310c0f554f15da2

--- includes/exception/MWExceptionHandler.php
+++ includes/exception/MWExceptionHandler.php
@@ -638,20 +638,16 @@ TXT;
 		// Filter out unwanted errors manually (e.g. when
 		// MediaWiki\suppressWarnings is active).
 		$suppressed = ( error_reporting() & $e->getSeverity() ) === 0;
-		if ( !$suppressed ) {
-			$logger = LoggerFactory::getInstance( $channel );
-			$logger->log(
-				$level,
-				self::getLogMessage( $e ),
-				self::getLogContext( $e, $catcher )
-			);
-		}
+		$logger = LoggerFactory::getInstance( $suppressed ? "$channel-suppressed" : $channel );
+		$logger->log( $level, self::getLogMessage( $e ), self::getLogContext( $e, $catcher ) );
 
-		// Include all errors in the json log (surpressed errors will be flagged)
-		$json = self::jsonSerializeException( $e, false, FormatJson::ALL_OK, $catcher );
-		if ( $json !== false ) {
-			$logger = LoggerFactory::getInstance( "{$channel}-json" );
-			$logger->log( $level, $json, [ 'private' => true ] );
+		// Include non-suppressed errors in the json log
+		if ( !$suppressed ) {
+			$json = self::jsonSerializeException( $e, false, FormatJson::ALL_OK, $catcher );
+			if ( $json !== false ) {
+				$logger = LoggerFactory::getInstance( "{$channel}-json" );
+				$logger->log( $level, $json, [ 'private' => true ] );
+			}
 		}
 
 		Hooks::run( 'LogException', [ $e, $suppressed ] );
