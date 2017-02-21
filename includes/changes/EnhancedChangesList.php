2d030dfbbaa2253f1e03d8e84b10011dd4b5f325
Eddie Greiner-Petter <git@eddie-sh.de>
2017-02-21 03:25:39 +0100

    Add CSS classes for diff/hist links at Enhanced RC
    CSS classes mw-changeslist-diff and mw-changeslist-history should be
    added to diff and history links in recent changes. There is already a
    patch for the old recent changes page, this one adds the same classes
    onto the enhanced recent changes page (class EnhancedChangesList).
    Follow-up: I2d5ef8c180ae4ff6e7f5d0ab443dc7084f8c4c77
    
    Bug: T157178
    Change-Id: Ib51639a92b5925f2bad0aebd4f7068b178f65017

--- includes/changes/EnhancedChangesList.php
+++ includes/changes/EnhancedChangesList.php
@@ -532,7 +532,7 @@ class EnhancedChangesList extends ChangesList {
 				$links['total-changes'] = $this->linkRenderer->makeKnownLink(
 					$block0->getTitle(),
 					new HtmlArmor( $nchanges[$n] ),
-					[],
+					[ 'class' => 'mw-changeslist-diff' ],
 					$queryParams + [
 						'diff' => $currentRevision,
 						'oldid' => $last->mAttribs['rc_last_oldid'],
@@ -542,7 +542,7 @@ class EnhancedChangesList extends ChangesList {
 					$links['total-changes-since-last'] = $this->linkRenderer->makeKnownLink(
 							$block0->getTitle(),
 							new HtmlArmor( $sinceLastVisitMsg[$sinceLast] ),
-							[],
+							[ 'class' => 'mw-changeslist-diff' ],
 							$queryParams + [
 								'diff' => $currentRevision,
 								'oldid' => $unvisitedOldid,
@@ -564,7 +564,7 @@ class EnhancedChangesList extends ChangesList {
 			$links['history'] = $this->linkRenderer->makeKnownLink(
 					$block0->getTitle(),
 					new HtmlArmor( $this->message['enhancedrc-history'] ),
-					[],
+					[ 'class' => 'mw-changeslist-history' ],
 					$params
 				);
 		}
@@ -719,7 +719,7 @@ class EnhancedChangesList extends ChangesList {
 					. $this->linkRenderer->makeKnownLink(
 						$pageTitle,
 						new HtmlArmor( $this->message['hist'] ),
-						[],
+						[ 'class' => 'mw-changeslist-history' ],
 						$query
 					) )->escaped();
 		return $retVal;
