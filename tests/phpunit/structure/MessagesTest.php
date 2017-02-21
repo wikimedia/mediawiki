59f8eddcb69857a96ec8fcf571daa013f619b360
Timo Tijhof <krinklemail@gmail.com>
2017-02-20 21:41:55 -0800

    phpunit: Add MessagesTest (structure)
    This test ensures that all message keys start with a lowercase
    character. This avoids any conflicts as message keys are expected
    to be first-letter case-insensitive and must be compatible with
    a first-letter case-insensitive 'MediaWiki:'-namespace (by default).
    
    The convention is to start with a lowercase character.
    
    Ref ab128b4f03, which brought up the use of uppercase letters in message
    keys. Further research shows that we actually use them in various places
    already without problem. While the convention is for most messages keys
    to be fully lowercase, there is valid use for uppercase characters and
    there is no problem with that, it's just the first letter.
    
    Change-Id: I74472608dcee8e152a819ba7c64a13fba3f931e6

---
+++ tests/phpunit/structure/MessagesTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class MessagesTest extends PHPUnit_Framework_TestCase {
+	public function testMessageKeys() {
+		$l10nCache = Language::getLocalisationCache();
+		$messageNames = $l10nCache->getSubitemList( 'en', 'messages' );
+		$nonLcfirst = array_filter( $messageNames, function ( $key ) {
+			return $key !== lcfirst( $key );
+		} );
+		// - assertEquals in foreach shows only the first failure.
+		// - assertEquals with full array_map lcfirst result is slow.
+		$this->assertEquals(
+			[],
+			$nonLcfirst,
+			'Message keys must be first-letter case-insensitive (lcfirst is the convention)'
+		);
+	}
+}
