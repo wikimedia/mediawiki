60fee2c7d29e94b5205dd77b4d71ae6b996666a7
shanika <shanika.13@cse.mrt.ac.lk>
2017-02-23 17:13:43 +0530

    Add warning when repeated namespace in title
    Add a pop-up warning message when there is repeated namespace in the new
    title when moving a page. Pop-up will appear when leaving(onBlur) the
    title input field.
    
    Bug: T62399
    Change-Id: I1bf1866b6254ef823cf5380246aee2cab9375b2d

--- resources/src/mediawiki.special/mediawiki.special.movePage.js
+++ resources/src/mediawiki.special/mediawiki.special.movePage.js
@@ -11,3 +11,38 @@ jQuery( function ( $ ) {
 		OO.ui.infuse( 'wpMovetalk-field' );
 	}
 } );
+
+( function ( $ ) {
+	$( '#wpNewTitleMain input' ).on( 'blur', function(){
+		var newTitle = $( '#wpNewTitleMain input' ).val();
+		nsIds = mw.config.get( 'wgNamespaceIds' );
+
+		if ( newTitle.indexOf( ':' ) >= 0 ){
+			var possibleNs = newTitle.split( ':' )[ 0 ];
+			var possibleNsLower = possibleNs.toLowerCase();
+				if ( Object.keys( nsIds ).indexOf( possibleNsLower ) >= 0 ){
+					var messageDialog = new OO.ui.MessageDialog();
+
+					// Create and append a window manager.
+					var windowManager = new OO.ui.WindowManager();
+					$( 'body' ).append( windowManager.$element );
+
+					// Add the dialog to the window manager.
+					windowManager.addWindows( [ messageDialog ] );
+
+					// Configure the message dialog when it is opened with the window manager's openWindow() method.
+					windowManager.openWindow( messageDialog, {
+						title: 'Repeated namespace in the title',
+						message: "Please verify your new page title: " + possibleNs + ":" + newTitle + ". " + possibleNs + ":" + possibleNs + ":' in a page title is usually a mistake.",
+						actions: [
+							{
+								action: 'accept',
+								label: 'OK',
+								flags: 'primary'
+							}
+						]
+					} );
+				}
+			}
+		});
+}( jQuery ) );
