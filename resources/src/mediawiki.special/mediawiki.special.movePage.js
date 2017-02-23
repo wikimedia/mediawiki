74ea8ab6859a1a8630cf58977167cd67850d44e8
shanika <shanika.13@cse.mrt.ac.lk>
2017-02-23 17:13:43 +0530

    Add warning when repeated namespace in title
    Add a pop-up warning message when there is repeated namespace in the new
    title when moving a page. Pop-up will appear when leaving(onBlur) the
    title input field.
    
    Bug: T62399
    Change-Id: I1bf1866b6254ef823cf5380246aee2cab9375b2d

--- resources/src/mediawiki.special/mediawiki.special.movePage.js
+++ resources/src/mediawiki.special/mediawiki.special.movePage.js
@@ -11,3 +11,47 @@ jQuery( function ( $ ) {
 		OO.ui.infuse( 'wpMovetalk-field' );
 	}
 } );
+
+/*
+ * Warning message for possible repeated namespace it the new title
+ */
+( function ( $, mw ) {
+	$( '#wpNewTitleMain input' ).on( 'blur', function () {
+		var newTitle = $( '#wpNewTitleMain input' ).val(),
+			// Namespace IDs
+			nsIds = mw.config.get( 'wgNamespaceIds' ),
+			possibleNs, possibleNsLower, messageDialog, windowManager;
+
+		// Check whether the new title contains character ':'
+		if ( newTitle.indexOf( ':' ) >= 0 ) {
+			// Possible namespace in the new title input
+			possibleNs = newTitle.split( ':' )[ 0 ];
+			possibleNsLower = possibleNs.toLowerCase();
+
+			// Check whether the possible namespace string is in namespaces
+			if ( Object.keys( nsIds ).indexOf( possibleNsLower ) >= 0 ) {
+				messageDialog = new OO.ui.MessageDialog();
+
+				// Create and append a window manager.
+				windowManager = new OO.ui.WindowManager();
+				$( 'body' ).append( windowManager.$element );
+
+				// Add the dialog to the window manager.
+				windowManager.addWindows( [ messageDialog ] );
+
+				// Configure the message dialog when it is opened with the window manager's openWindow() method.
+				windowManager.openWindow( messageDialog, {
+					title: 'Repeated namespace in the title',
+					message: 'Please verify your new page title: ' + possibleNs + ':' + newTitle + '. ' + possibleNs + ':' + possibleNs + ':\' in a page title is usually a mistake.',
+					actions: [
+						{
+							action: 'accept',
+							label: 'OK',
+							flags: 'primary'
+						}
+					]
+				} );
+			}
+		}
+	} );
+}( jQuery, mediaWiki ) );
