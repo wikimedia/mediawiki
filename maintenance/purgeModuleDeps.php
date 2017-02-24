e24884c6b4428bba2c5f9cfc93b382ffa29990a6
Timo Tijhof <krinklemail@gmail.com>
2017-02-24 14:56:31 -0800

    resourceloader: Add purgeModuleDeps.php maintenance script
    Based on cleanupRemovedModules.php.
    
    Bug: T158105
    Change-Id: I05f650a0cfa8ca647f143a40e1087338adbef6da

---
+++ maintenance/purgeModuleDeps.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Remove all cache entries for ResourceLoader modules from the database.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ * @author Timo Tijhof
+ */
+
+require_once __DIR__ . '/Maintenance.php';
+
+/**
+ * Maintenance script to purge the module_deps database cache table.
+ *
+ * @ingroup Maintenance
+ */
+class PurgeModuleDeps extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription(
+			'Remove all cache entries for ResourceLoader modules from the database' );
+		$this->addOption( 'batchsize', 'Delete rows in batches of this size. Default: 500', false, true );
+	}
+
+	public function execute() {
+		$dbw = $this->getDB( DB_MASTER );
+		$limit = max( 1, intval( $this->getOption( 'batchsize', 500 ) ) );
+
+		$this->output( "Cleaning up module_deps table...\n" );
+		$i = 1;
+		$modDeps = $dbw->tableName( 'module_deps' );
+		do {
+			$dbw->query( "DELETE FROM $modDeps WHERE LIMIT $limit", __METHOD__ );
+			$numRows = $dbw->affectedRows();
+			$this->output( "Batch $i: $numRows rows\n" );
+			$i++;
+			wfWaitForSlaves();
+		} while ( $numRows > 0 );
+		$this->output( "done\n" );
+	}
+}
+
+$maintClass = 'PurgeModuleDeps';
+require_once RUN_MAINTENANCE_IF_MAIN;
