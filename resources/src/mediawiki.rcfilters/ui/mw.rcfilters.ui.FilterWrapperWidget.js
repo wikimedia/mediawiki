881b11c8c781403eb527cc48d35a03f73eee5e2b
Moriel Schottlender <moriel@gmail.com>
2017-02-24 16:02:25 -0800

    [wip] RCFilters UI: Scroll to filter when selected
    Change-Id: I416c324eec4bc35ae9b1e0aeabd2897da0158d95

--- resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterWrapperWidget.js
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterWrapperWidget.js
@@ -55,6 +55,10 @@
 		this.textInput.connect( this, {
 			change: 'onTextInputChange'
 		} );
+		this.capsule.connect( this, { capsuleItemClick: 'onCapsuleItemClick' } );
+		this.capsule.popup.connect( this, { toggle: 'onCapsulePopupToggle' } );
+
+		// Initialize
 		this.$element
 			.addClass( 'mw-rcfilters-ui-filterWrapperWidget' )
 			.append( this.capsule.$element, this.textInput.$element );
@@ -66,11 +70,45 @@
 	OO.mixinClass( mw.rcfilters.ui.FilterWrapperWidget, OO.ui.mixin.PendingElement );
 
 	/**
+	 * Respond to capsule item click and make the popup scroll down to the requested item
+	 *
+	 * @param {mw.rcfilters.ui.CapsuleItemWidget} item Clicked item
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onCapsuleItemClick = function ( item ) {
+		var filterName = item.getData(),
+			// Find the item in the popup
+			filterWidget = this.filterPopup.getItemWidget( filterName ),
+			container = OO.ui.Element.static.getClosestScrollableContainer( this.filterPopup.$element[ 0 ], 'y' ),
+			pos = OO.ui.Element.static.getRelativePosition( filterWidget.$element, $( container ) );
+
+		// Highlight item
+		this.filterPopup.select( filterName );
+
+		// Scroll to item
+		$( container ).animate( {
+			scrollTop: pos.top
+		} );
+	};
+
+	/**
+	 * Respond to popup toggle event. Reset selection in the list when the popup is closed.
+	 *
+	 * @param {boolean} isVisible Popup is visible
+	 */
+	mw.rcfilters.ui.FilterWrapperWidget.prototype.onCapsulePopupToggle = function ( isVisible ) {
+		if ( !isVisible ) {
+			this.filterPopup.resetSelection();
+		}
+	};
+
+	/**
 	 * Respond to text input change
 	 *
 	 * @param {string} newValue Current value
 	 */
 	mw.rcfilters.ui.FilterWrapperWidget.prototype.onTextInputChange = function ( newValue ) {
+		this.filterPopup.resetSelection();
+
 		// Filter the results
 		this.filterPopup.filter( this.model.findMatches( newValue ) );
 	};
