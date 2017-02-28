cf7b63346ed86499095614afa107f505fe4bcee4
C. Scott Ananian <cscott@cscott.net>
2017-02-28 15:56:01 -0500

    Export list of languages which will trigger language conversion
    LanguageConverter is enabled on a per-page basis, based on the page
    language and whether it implements `$lang->getConverter()`.  Export
    this in siteinfo so that Parsoid knows whether it should parse
    language converter markup on a given page (based on the page language).
    
    Bug: T153341
    Change-Id: I010aa3812051725166ab79ea5ee3eaf56615fe94

--- includes/api/ApiQuerySiteinfo.php
+++ includes/api/ApiQuerySiteinfo.php
@@ -88,6 +88,9 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 				case 'languages':
 					$fit = $this->appendLanguages( $p );
 					break;
+				case 'languagevariants':
+					$fit = $this->appendLanguageVariants( $p );
+					break;
 				case 'skins':
 					$fit = $this->appendSkins( $p );
 					break;
@@ -713,6 +716,42 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 		return $this->getResult()->addValue( 'query', $property, $data );
 	}
 
+	// Export information about which page languages will trigger
+	// language conversion. (T153341)
+	public function appendLanguageVariants( $property ) {
+		$langNames = LanguageConverter::$languagesWithVariants;
+		if ( $this->getConfig()->get( 'DisableLangConversion' ) ) {
+			// Ensure result is empty if language conversion is disabled.
+			$langNames = [];
+		}
+		sort( $langNames );
+
+		$data = [];
+		foreach ( $langNames as $langCode ) {
+			$lang = Language::factory( $langCode );
+			if ( $lang->getConverter() instanceof FakeConverter ) {
+				// Only languages which do not return instances of
+				// FakeConverter implement language conversion.
+				continue;
+			}
+			$data[$langCode] = [];
+			ApiResult::setIndexedTagName( $data[$langCode], 'variant' );
+
+			$variants = $lang->getVariants();
+			sort( $variants );
+			foreach ( $variants as $v ) {
+				if ( $v === $langCode ) {
+					continue;
+				}
+				$data[$langCode][] = $v;
+			}
+		}
+		ApiResult::setIndexedTagName( $data, 'lang' );
+		ApiResult::setArrayType( $data, 'kvp', 'code' );
+
+		return $this->getResult()->addValue( 'query', $property, $data );
+	}
+
 	public function appendSkins( $property ) {
 		$data = [];
 		$allowed = Skin::getAllowedSkins();
@@ -851,6 +890,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 					'rightsinfo',
 					'restrictions',
 					'languages',
+					'languagevariants',
 					'skins',
 					'extensiontags',
 					'functionhooks',
