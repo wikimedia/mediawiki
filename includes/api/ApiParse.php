855e5dda4ff6f85595ba8b50e92442cc4503a465
Brad Jorsch <bjorsch@wikimedia.org>
2017-02-28 15:52:17 -0500

    Generalize ResourceLoader 'excludepage' functionality
    There has long been a hack for previewing edits to user JS/CSS, where
    OutputPage would pass an 'excludepage' parameter to
    ResourceLoaderUserModule to tell it not to load one particular page and
    would instead embed that page statically. That's nice, but there are
    other places where we could use the same thing.
    
    This patch generalizes it:
    * DerivativeResourceLoaderContext may now contain a callback for mapping
      titles to replacement Content objects.
    * ResourceLoaderWikiModule::getContent() uses the overrides, and
      requests embedding when they're used. All subclasses in Gerrit should
      pick it up automatically.
    * OutputPage gains methods for callers to add to the override mapping,
      which it passes on to RL. It loses a bunch of the special casing it
      had for the 'user' and 'user.styles' modules.
    * EditPage sets the overrides on OutputPage when doing the preview, as
      does ApiParse for prop=headhtml. TemplateSandbox does too in I83fa0856.
    * OutputPage::userCanPreview() gets less specific to editing user CSS
      and JS, since RL now handles the embedding based on the actual
      modules' dependencies and EditPage only requests it on preview.
    
    ApiParse also gets a new hook to support TemplateSandbox's API
    integration (used in I83fa0856).
    
    Bug: T112474
    Change-Id: Ib9d2ce42931c1de8372e231314a1f672d7e2ac0e

--- includes/api/ApiParse.php
+++ includes/api/ApiParse.php
@@ -302,6 +302,7 @@ class ApiParse extends ApiBase {
 
 			$outputPage = new OutputPage( $context );
 			$outputPage->addParserOutputMetadata( $p_result );
+			$outputPage->addContentOverride( $titleObj, $this->content );
 			$context->setOutput( $outputPage );
 
 			if ( $skin ) {
@@ -310,6 +311,8 @@ class ApiParse extends ApiBase {
 					$outputPage->addModules( $group );
 				}
 			}
+
+			Hooks::run( 'ApiParseMakeOutputPage', [ $this, $outputPage ] );
 		}
 
 		if ( !is_null( $oldid ) ) {
