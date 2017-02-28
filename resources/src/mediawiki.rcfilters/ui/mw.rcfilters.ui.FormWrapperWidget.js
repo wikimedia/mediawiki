390779ab837462346b00ea8c1524a2b8d0a34892
Moriel Schottlender <moriel@gmail.com>
2017-02-27 17:56:40 -0800

    RCFilters UI: Ajaxify everything
    Make sure all links and 'show' button form information uses the
    ajax method rather than reloading the page.
    
    Bug: T157594
    Change-Id: I97a452082e2d06f78cbec2235e2ed07a2eb6bca0

--- resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FormWrapperWidget.js
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FormWrapperWidget.js
@@ -6,10 +6,11 @@
 	 *
 	 * @constructor
 	 * @param {mw.rcfilters.dm.ChangesListViewModel} model Changes list view model
+	 * @param {mw.rcfilters.Controller} controller RCfilters controller
 	 * @param {jQuery} $formRoot Root element of the form to attach to
 	 * @param {Object} config Configuration object
 	 */
-	mw.rcfilters.ui.FormWrapperWidget = function MwRcfiltersUiFormWrapperWidget( model, $formRoot, config ) {
+	mw.rcfilters.ui.FormWrapperWidget = function MwRcfiltersUiFormWrapperWidget( model, controller, $formRoot, config ) {
 		config = config || {};
 
 		// Parent
@@ -18,18 +19,103 @@
 		} ) );
 
 		this.model = model;
-		this.$submitButton = this.$element.find( 'input[type=submit]' );
+		this.controller = controller;
+		this.$submitButton = this.$element.find( 'form input[type=submit]' );
+
+		this.$element
+			.find( 'a[data-params]' )
+			.on( 'click', this.onLinkClick.bind( this ) );
+
+		this.$element
+			.find( 'form' )
+			.on( 'submit', this.onFormSubmit.bind( this ) );
 
 		// Events
 		this.model.connect( this, {
 			invalidate: 'onModelInvalidate',
 			update: 'onModelUpdate'
 		} );
+
+		// Initialize
+		this.cleanupForm();
 	};
 
 	/* Initialization */
 
 	OO.inheritClass( mw.rcfilters.ui.FormWrapperWidget, OO.ui.Widget );
+
+	/**
+	 * Clean up the base form we're getting from the back-end.
+	 * Remove <strong> tags and replace those with classes, so
+	 * we can toggle those on click.
+	 */
+	mw.rcfilters.ui.FormWrapperWidget.prototype.cleanupForm = function () {
+		this.$element.find( '[data-keys] strong' ).each( function () {
+			$( this )
+				.parent().addClass( 'mw-rcfilters-staticfilters-selected' );
+
+			$( this )
+				.replaceWith( $( this ).contents() );
+		} );
+	};
+
+	/**
+	 * Respond to link click
+	 *
+	 * @param {jQuery.Event} e Event
+	 * @return {boolean} false
+	 */
+	mw.rcfilters.ui.FormWrapperWidget.prototype.onLinkClick = function ( e ) {
+		var $element = $( e.target ),
+			data = $element.data( 'params' ),
+			keys = $element.data( 'keys' ),
+			$similarElements = $element.parent().find( '[data-keys="' + keys + '"]' );
+
+		// Remove the class from similar elements
+		$similarElements.removeClass( 'mw-rcfilters-staticfilters-selected' );
+		// Add the class to this element
+		$element.addClass( 'mw-rcfilters-staticfilters-selected' );
+
+		// Identify if this is a show/hide feature, and if it is, change its text
+		if ( $element.parents( '.rcshowhideoption' ).length ) {
+			// Toggle the message
+			// Toggle the param value (change the data)
+		}
+
+		this.controller.updateChangesList( data );
+		return false;
+	};
+
+	/**
+	 * Respond to form submit event
+	 *
+	 * @param {jQuery.Event} e Event
+	 * @return {boolean} false
+	 */
+	mw.rcfilters.ui.FormWrapperWidget.prototype.onFormSubmit = function ( e ) {
+		var data = {};
+
+		// Collect all data from form
+		$( e.target ).find( 'input:not([type="hidden"],[type="submit"]), select' ).each( function () {
+			var val;
+
+			if (
+				!$( this ).is( ':checkbox' ) ||
+				(
+					$( this ).is( ':checkbox' ) && $( this ).is( ':checked' )
+				)
+			) {
+				val = $( this ).val();
+			}
+
+			if ( val ) {
+				data[ $( this ).prop( 'name' ) ] = val;
+			}
+		} );
+
+		this.controller.updateChangesList( data );
+		return false;
+	};
 
 	/**
 	 * Respond to model invalidate
