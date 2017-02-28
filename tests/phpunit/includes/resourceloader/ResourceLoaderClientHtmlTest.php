ea39ebf807206a21238a03fc1e936d201346801f
Brad Jorsch <bjorsch@wikimedia.org>
2017-02-28 15:52:17 -0500

    Generalize ResourceLoader 'excludepage' functionality
    There has long been a hack for previewing edits to user JS/CSS, where
    OutputPage would pass an 'excludepage' parameter to
    ResourceLoaderUserModule to tell it not to load one particular page and
    would instead embed that page statically. That's nice, but there are
    other places where we could use the same thing.
    
    This patch generalizes it:
    * DerivativeResourceLoaderContext may now contain a callback for mapping
      titles to replacement Content objects.
    * ResourceLoaderModule has a method to indicate whether the module
      should be embedded.
    * ResourceLoaderClientHtml embeds any module that returns true from this
      new method (like how it already embeds 'private'-group modules), so
      OutputPage doesn't have to worry about it.
    * ResourceLoaderWikiModule::getContent() uses the overrides, and
      requests embedding when they're used. All subclasses in Gerrit should
      pick it up automatically.
    * OutputPage gains methods for callers to add to the override mapping,
      which it passes on to RL. It loses a bunch of the special casing it
      had for the 'user' and 'user.styles' modules.
    * EditPage sets the overrides on OutputPage when doing the preview, as
      does ApiParse for prop=headhtml. TemplateSandbox does too in I83fa0856.
    * OutputPage::userCanPreview() gets less specific to editing user CSS
      and JS, since RL now handles the embedding based on the actual
      modules' dependencies and EditPage only requests it on preview.
    
    ApiParse also gets a new hook to support TemplateSandbox's API
    integration (used in I83fa0856).
    
    Bug: T112474
    Change-Id: Ib9d2ce42931c1de8372e231314a1f672d7e2ac0e

--- tests/phpunit/includes/resourceloader/ResourceLoaderClientHtmlTest.php
+++ tests/phpunit/includes/resourceloader/ResourceLoaderClientHtmlTest.php
@@ -41,6 +41,7 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.top' => [ 'position' => 'top' ],
 			'test.private.top' => [ 'group' => 'private', 'position' => 'top' ],
 			'test.private.bottom' => [ 'group' => 'private', 'position' => 'bottom' ],
+			'test.shouldembed' => [ 'shouldEmbed' => true ],
 
 			'test.styles.pure' => [ 'type' => ResourceLoaderModule::LOAD_STYLES ],
 			'test.styles.mixed' => [],
@@ -48,12 +49,14 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.styles.mixed.user' => [ 'group' => 'user' ],
 			'test.styles.mixed.user.empty' => [ 'group' => 'user', 'isKnownEmpty' => true ],
 			'test.styles.private' => [ 'group' => 'private', 'styles' => '.private{}' ],
+			'test.styles.shouldembed' => [ 'shouldEmbed' => true, 'styles' => '.shouldembed{}' ],
 
 			'test.scripts' => [],
 			'test.scripts.top' => [ 'position' => 'top' ],
 			'test.scripts.mixed.user' => [ 'group' => 'user' ],
 			'test.scripts.mixed.user.empty' => [ 'group' => 'user', 'isKnownEmpty' => true ],
 			'test.scripts.raw' => [ 'isRaw' => true ],
+			'test.scripts.shouldembed' => [ 'shouldEmbed' => true ],
 		];
 		return array_map( function ( $options ) {
 			return self::makeModule( $options );
@@ -86,6 +89,7 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.private.bottom',
 			'test.private.top',
 			'test.top',
+			'test.shouldembed',
 			'test.unregistered',
 		] );
 		$client->setModuleStyles( [
@@ -93,12 +97,14 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.styles.mixed.user.empty',
 			'test.styles.private',
 			'test.styles.pure',
+			'test.styles.shouldembed',
 			'test.unregistered.styles',
 		] );
 		$client->setModuleScripts( [
 			'test.scripts',
 			'test.scripts.mixed.user.empty',
 			'test.scripts.top',
+			'test.scripts.shouldembed',
 			'test.unregistered.scripts',
 		] );
 
@@ -106,12 +112,15 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'states' => [
 				'test.private.top' => 'loading',
 				'test.private.bottom' => 'loading',
+				'test.shouldembed' => 'loading',
 				'test.styles.pure' => 'ready',
 				'test.styles.mixed.user.empty' => 'ready',
 				'test.styles.private' => 'ready',
+				'test.styles.shouldembed' => 'ready',
 				'test.scripts' => 'loading',
 				'test.scripts.top' => 'loading',
 				'test.scripts.mixed.user.empty' => 'ready',
+				'test.scripts.shouldembed' => 'loading',
 			],
 			'general' => [
 				'test',
@@ -124,12 +133,14 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'scripts' => [
 				'test.scripts',
 				'test.scripts.top',
+				'test.scripts.shouldembed',
 			],
 			'embed' => [
-				'styles' => [ 'test.styles.private' ],
+				'styles' => [ 'test.styles.private', 'test.styles.shouldembed' ],
 				'general' => [
 					'test.private.bottom',
 					'test.private.top',
+					'test.shouldembed',
 				],
 			],
 		];
@@ -260,6 +271,37 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 				'modules' => [ 'test.styles.noscript' ],
 				'only' => ResourceLoaderModule::TYPE_STYLES,
 				'output' => '<noscript><link rel="stylesheet" href="/w/load.php?debug=false&amp;lang=nl&amp;modules=test.styles.noscript&amp;only=styles&amp;skin=fallback"/></noscript>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_COMBINED,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",function($,jQuery,require,module){},{"css":[]});});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.styles.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_STYLES,
+				'output' => '<style>.shouldembed{}</style>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.scripts.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_SCRIPTS,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.state({"test.scripts.shouldembed":"ready"});});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test', 'test.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_COMBINED,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",function($,jQuery,require,module){},{"css":[]});mw.loader.load("/w/load.php?debug=false\u0026lang=nl\u0026modules=test\u0026skin=fallback");});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.styles.pure', 'test.styles.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_STYLES,
+				'output' => '<style>.shouldembed{}</style>' . "\n"
+					. '<link rel="stylesheet" href="/w/load.php?debug=false&amp;lang=nl&amp;modules=test.styles.pure&amp;only=styles&amp;skin=fallback"/>',
 			],
 			// @codingStandardsIgnoreEnd
 		];
