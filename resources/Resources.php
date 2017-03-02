4005cf7df5a773ab259583fc10d5483a29b8700f
James D. Forrester <jforrester@wikimedia.org>
2017-03-02 13:54:19 -0800

    [WIP] startup: Drop JavaScript support for ES3-only browsers
    Bug: T128115
    Change-Id: I95400637d1b85d2c607cd91bcb39ce21733295c8

--- resources/Resources.php
+++ resources/Resources.php
@@ -2474,14 +2474,12 @@ return [
 			'oojs-ui-core',
 		],
 	],
+
 	/* es5-shim */
+	// Deprecated since MediaWiki 1.29.0
 	'es5-shim' => [
-		'scripts' => [
-			'resources/lib/es5-shim/es5-shim.js',
-			'resources/src/polyfill-object-create.js',
-		],
+		'deprecated' => 'Use of the "es5-shim" module is deprecated since MediaWiki 1.29.0',
 		'targets' => [ 'desktop', 'mobile' ],
-		'skipFunction' => 'resources/src/es5-skip.js',
 	],
 
 	/* dom-level2-shim */
@@ -2498,9 +2496,6 @@ return [
 			'resources/src/oojs-global.js',
 		],
 		'targets' => [ 'desktop', 'mobile' ],
-		'dependencies' => [
-			'es5-shim',
-		],
 	],
 
 	'mediawiki.router' => [
