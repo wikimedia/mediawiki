f5ebae192eed41b4bff8aa423d858f0f52ec853a
Antoine Musso <hashar@free.fr>
2017-03-07 14:45:15 +0100

    Draft to enhance webdriver.io integration
    For Ibe7a004a120e82af637ab3e31b725de743134c99
    
    Change-Id: I15bd422cf899aa48698e1614aec0fdb8927b6913

---
+++ tests/selenium/wdio.conf.vagrant.js
@@ -0,0 +1,13 @@
+/* eslint no-undef: "error"*/
+/* eslint-env node*/
+'use strict';
+var merge = require( 'deepmerge' ),
+	wdioConf = require( './wdio.conf.js' );
+
+// Overwrite default settings
+exports.config = merge( wdioConf.config, {
+	username: 'Admin',
+	password: 'vagrant',
+	screenshotPath: './log/',
+	baseUrl: 'http://127.0.0.1:8080/w'
+} );
