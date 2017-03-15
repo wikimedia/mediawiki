0028feebb7c7150e76cf8a5939f228dbdd93c568
Brian Wolff <bawolff+wn@gmail.com>
2017-03-15 22:51:13 +0000

    objectcache: Never use CACHE_NONE for CACHE_ANYTHING
    If $wgMainCacheType = CACHE_ACCEL, but there is no APC, then its
    possible that CACHE_ANYTHING will default to CACHE_NONE because
    that's what CACHE_ACCEL would do.
    
    Possibly also T147161
    
    Bug: T160519
    Change-Id: I9ac2d071437b35a0f9cd3678e2279628f7b1931e
    (cherry picked from commit c5a0fa5bed4c5aafdf0721c1d05978576107bd85)

--- includes/objectcache/ObjectCache.php
+++ includes/objectcache/ObjectCache.php
@@ -222,8 +222,14 @@ class ObjectCache {
 		global $wgMainCacheType, $wgMessageCacheType, $wgParserCacheType;
 		$candidates = [ $wgMainCacheType, $wgMessageCacheType, $wgParserCacheType ];
 		foreach ( $candidates as $candidate ) {
+			$cache = false;
 			if ( $candidate !== CACHE_NONE && $candidate !== CACHE_ANYTHING ) {
-				return self::getInstance( $candidate );
+				$cache = self::getInstance( $candidate );
+				// CACHE_ACCEL might default to nothing if no APCu
+				// See includes/ServiceWiring.php
+				if ( !( $cache instanceof EmptyBagOStuff ) ) {
+					return $cache;
+				}
 			}
 		}
 		return self::getInstance( CACHE_DB );
