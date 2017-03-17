c8ad83310f425433791102e8a958397f2d701b65
Bartosz Dziewoński <matma.rex@gmail.com>
2017-03-17 02:32:59 +0100

    Refactor registration of OOjs UI resource modules
    includes/resourceloader/ResourceLoaderOOUIModule.php
    * New trait centralizing some logic for dealing with OOjs UI themes,
      previously duplicated in OutputPage, ResourcesOOUI.php and
      ResourceLoaderOOUIImageModule.
    * Follow-up change I74362f0fc215b26f1f104ce7bdbbac1e106736ad uses this
      as a base to allow skins/extensions to define new OOjs UI themes.
    
    resources/Resources.php
    resources/ResourcesOOUI.php
    includes/resourceloader/ResourceLoader.php
    * OOjs UI resource module definitions are moved back to their rightly
      place in Resources.php. They are again (almost) normal and static.
    * Theme-specific logic is now handled by the module code, definitions
      only specify 'themeScripts'/'themeStyles'/'themeImages'.
    * ResourcesOOUI.php is deleted and no longer loaded by ResourceLoader.
    
    includes/resourceloader/ResourceLoaderOOUIFileModule.php
    includes/resourceloader/ResourceLoaderOOUIImageModule.php
    * Glue code previously existing in ResourcesOOUI.php now lives here.
    * Use the ResourceLoaderOOUIModule trait to avoid code duplication.
    
    Change-Id: I39cc2a735d9625c87bf4ede6f5fb0ec441d47dcc

---
+++ includes/resourceloader/ResourceLoaderOOUIModule.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Convenience methods for dealing with OOUI themes and their relations to MW skins.
+ *
+ * @since 1.30
+ */
+trait ResourceLoaderOOUIModule {
+	protected static $knownScriptsModules = [ 'core' ];
+	protected static $knownStylesModules = [ 'core', 'widgets', 'toolbars', 'windows' ];
+	protected static $knownImagesModules = [
+		'indicators', 'textures',
+		// Extra icons
+		'icons-accessibility',
+		'icons-alerts',
+		'icons-content',
+		'icons-editing-advanced',
+		'icons-editing-core',
+		'icons-editing-list',
+		'icons-editing-styling',
+		'icons-interactions',
+		'icons-layout',
+		'icons-location',
+		'icons-media',
+		'icons-moderation',
+		'icons-movement',
+		'icons-user',
+		'icons-wikimedia',
+	];
+
+	// Note that keys must be lowercase, values TitleCase.
+	protected static $builtinSkinThemeMap = [
+		'default' => 'WikimediaUI',
+	];
+
+	// Note that keys must be TitleCase.
+	protected static $builtinThemePaths = [
+		'WikimediaUI' => [
+			'scripts' => 'resources/lib/oojs-ui/oojs-ui-wikimediaui.js',
+			'styles' => 'resources/lib/oojs-ui/oojs-ui-{module}-wikimediaui.css',
+			'images' => 'resources/lib/oojs-ui/themes/wikimediaui/{module}.json',
+		],
+		'Apex' => [
+			'scripts' => 'resources/lib/oojs-ui/oojs-ui-apex.js',
+			'styles' => 'resources/lib/oojs-ui/oojs-ui-{module}-apex.css',
+			'images' => 'resources/lib/oojs-ui/themes/apex/{module}.json',
+		],
+	];
+
+	/**
+	 * Return a map of skin names (in lowercase) to OOUI theme names, defining which theme a given
+	 * skin should use.
+	 *
+	 * @return array
+	 */
+	public static function getSkinThemeMap() {
+		$themeMap = self::$builtinSkinThemeMap;
+		$themeMap += ExtensionRegistry::getInstance()->getAttribute( 'SkinOOUIThemes' );
+		return $themeMap;
+	}
+
+	/**
+	 * Return a map of theme names to lists of paths from which a given theme should be loaded.
+	 *
+	 * Keys are theme names, values are associative arrays. Keys of the inner array are 'scripts',
+	 * 'styles', or 'images', and values are string paths.
+	 *
+	 * Additionally, the string '{module}' in paths represents the name of the module to load.
+	 *
+	 * @return array
+	 */
+	protected static function getThemePaths() {
+		$themePaths = self::$builtinThemePaths;
+		return $themePaths;
+	}
+
+	/**
+	 * Return a path to load given module of given theme from.
+	 *
+	 * @param string $theme OOUI theme name, for example 'WikimediaUI' or 'Apex'
+	 * @param string $kind Kind of the module: 'scripts', 'styles', or 'images'
+	 * @param string $module Module name, for valid values see $knownScriptsModules,
+	 *     $knownStylesModules, $knownImagesModules
+	 * @return string
+	 */
+	protected function getThemePath( $theme, $kind, $module ) {
+		$paths = self::getThemePaths();
+		$path = $paths[ $theme ][ $kind ];
+		$path = str_replace( '{module}', $module, $path );
+		return $path;
+	}
+
+	/**
+	 * @param string $theme See getThemePath()
+	 * @param string $module See getThemePath()
+	 * @return string
+	 */
+	protected function getThemeScriptsPath( $theme, $module ) {
+		if ( !in_array( $module, self::$knownScriptsModules ) ) {
+			throw new InvalidArgumentException( "Invalid OOUI scripts module '$module'" );
+		}
+		return $this->getThemePath( $theme, 'scripts', $module );
+	}
+
+	/**
+	 * @param string $theme See getThemePath()
+	 * @param string $module See getThemePath()
+	 * @return string
+	 */
+	protected function getThemeStylesPath( $theme, $module ) {
+		if ( !in_array( $module, self::$knownStylesModules ) ) {
+			throw new InvalidArgumentException( "Invalid OOUI styles module '$module'" );
+		}
+		return $this->getThemePath( $theme, 'styles', $module );
+	}
+
+	/**
+	 * @param string $theme See getThemePath()
+	 * @param string $module See getThemePath()
+	 * @return string
+	 */
+	protected function getThemeImagesPath( $theme, $module ) {
+		if ( !in_array( $module, self::$knownImagesModules ) ) {
+			throw new InvalidArgumentException( "Invalid OOUI images module '$module'" );
+		}
+		return $this->getThemePath( $theme, 'images', $module );
+	}
+}
