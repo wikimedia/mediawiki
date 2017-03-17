67bb65b8f93410284eed7295c16946209fd86377
Fomafix <fomafix@googlemail.com>
2017-03-17 15:42:20 +0100

    Replace deprecated jQuery.nodeName
    jQuery.nodeName gets deprecated in jQuery 3.2.0. [1] [2]
    
    [1] https://blog.jquery.com/2017/03/16/jquery-3-2-0-is-out/
    [2] https://github.com/jquery/jquery/issues/3475
    
    Change-Id: I7e6d5191e4db5117dbcfa89b07f25e672ed1e4d2

--- resources/src/jquery/jquery.makeCollapsible.js
+++ resources/src/jquery/jquery.makeCollapsible.js
@@ -159,7 +159,7 @@
 			if (
 				e.type === 'click' &&
 				options.linksPassthru &&
-				$.nodeName( e.target, 'a' ) &&
+				e.target.nodeName.toLowerCase() === 'a' &&
 				$( e.target ).attr( 'href' ) &&
 				$( e.target ).attr( 'href' ) !== '#'
 			) {
