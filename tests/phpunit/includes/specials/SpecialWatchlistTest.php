wikimedia/mediawiki

commit aa063f4c5a19e2a267e694e3614af1c3ce4498da

Back-end of new RecentChanges page, refactoring
Generate old RC, Related changes (it was already displayed and working
on 'Related changes' before this change), and Watchlist/etc. and data
for new UI from back-end.

This moves everything used for defining the old (unstructured) and new
(structured) filters into unified objects, ChangesListFilter and
ChangesListFilterGroup (and sub-classes).

This includes the query logic (see below) and logic for adding
CSS attribution classes.

This is a breaking change (for subclasses of ChangesListSpecialpage)
due to the signature (and name) change of buildMainQueryConds and
doMainQuery.  An alternative that I don't think is a breaking change
would be to put the filter->DB logic in runMainQueryHook, but then it's
doing more than just running a hook.

This is because it used to only build $conds here, but it's clear from
filterOnUserExperienceLevel filters need more than this.  I added all
the DB parameters from the hook, but this could be debated.

I have an checked and fixed the WMF-deployed extensions affected by
this.

Other than that, there should be full back-compat (including legacy
filters not using the new system).

* add hidepatrolled/hideunpatrolled to new UI.

* Move userExpLevel from RC to ChangesListSpecialPage.  Although for
now the structured UI only displays on RC anyway, when it displays on
watchlist, it seems we'll want userExpLevel there.

  Change this to make 'all' exclude unregistered users.

* Don't have front-end convert none-selected to 'all' on string_options.

* Needs the hideanons/hideliu special redirect to be done before this
is merged (T151873)

Bug: T152754
Bug: T152797
Change-Id: Iec2d82f6a830403d1c948a280efa58992e0cdee7

includes/specials/SpecialWatchlist.php
@@ -105,6 +105,56 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 	}
 
 	/**
+	 * @inheritdoc
+	 */
+	protected function registerFiltersFromDefinitions( array $definition ) {
+		foreach ( $definition as $groupName => &$groupDefinition ) {
+			foreach ( $groupDefinition['filters'] as &$filterDefinition ) {
+				if ( isset( $filterDefinition['showHideSuffix'] ) ) {
+					$filterDefinition['showHide'] = 'wl' . $filterDefinition['showHideSuffix'];
+				}
+			}
+		}
+
+		parent::registerFiltersFromDefinitions( $definition );
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function registerFilters() {
+		parent::registerFilters();
+
+		$user = $this->getUser();
+
+		$significance = $this->getFilterGroup( 'significance' );
+		$hideMinor = $significance->getFilter( 'hideminor' );
+		$hideMinor->setDefault( $user->getBoolOption( 'watchlisthideminor' ) );
+
+		$automated = $this->getFilterGroup( 'automated' );
+		$hideBots = $automated->getFilter( 'hidebots' );
+		$hideBots->setDefault( $user->getBoolOption( 'watchlisthidebots' ) );
+
+		$registration = $this->getFilterGroup( 'registration' );
+		$hideAnons = $registration->getFilter( 'hideanons' );
+		$hideAnons->setDefault( $user->getBoolOption( 'watchlisthideanons' ) );
+		$hideLiu = $registration->getFilter( 'hideliu' );
+		$hideLiu->setDefault( $user->getBoolOption( 'watchlisthideliu' ) );
+
+		$reviewStatus = $this->getFilterGroup( 'reviewStatus' );
+		$hidePatrolled = $reviewStatus->getFilter( 'hidepatrolled' );
+		$hidePatrolled->setDefault( $user->getBoolOption( 'watchlisthidepatrolled' ) );
+
+		$authorship = $this->getFilterGroup( 'authorship' );
+		$hideMyself = $authorship->getFilter( 'hidemyself' );
+		$hideMyself->setDefault( $user->getBoolOption( 'watchlisthideown' ) );
+
+		$changeType = $this->getFilterGroup( 'changeType' );
+		$hideCategorization = $changeType->getFilter( 'hidecategorization' );
+		$hideCategorization->setDefault( $user->getBoolOption( 'watchlisthidecategorization' ) );
+	}
+
+	/**
 	 * Get a FormOptions object containing the default options
 	 *
 	 * @return FormOptions
@@ -119,14 +169,6 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 			// The user has submitted the form, so we dont need the default values
 			return $opts;
 		}
-
-		$opts->add( 'hideminor', $user->getBoolOption( 'watchlisthideminor' ) );
-		$opts->add( 'hidebots', $user->getBoolOption( 'watchlisthidebots' ) );
-		$opts->add( 'hideanons', $user->getBoolOption( 'watchlisthideanons' ) );
-		$opts->add( 'hideliu', $user->getBoolOption( 'watchlisthideliu' ) );
-		$opts->add( 'hidepatrolled', $user->getBoolOption( 'watchlisthidepatrolled' ) );
-		$opts->add( 'hidemyself', $user->getBoolOption( 'watchlisthideown' ) );
-		$opts->add( 'hidecategorization', $user->getBoolOption( 'watchlisthidecategorization' ) );
 
 		return $opts;
 	}
@@ -181,32 +223,28 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 	}
 
 	/**
-	 * Return an array of conditions depending of options set in $opts
-	 *
-	 * @param FormOptions $opts
-	 * @return array
+	 * @inheritdoc
 	 */
-	public function buildMainQueryConds( FormOptions $opts ) {
+	protected function buildQuery( &$tables, &$fields, &$conds, &$query_options,
+		&$join_conds, FormOptions $opts ) {
+
 		$dbr = $this->getDB();
-		$conds = parent::buildMainQueryConds( $opts );
+		parent::buildQuery( $tables, $fields, $conds, $query_options, $join_conds,
+			$opts );
 
 		// Calculate cutoff
 		if ( $opts['days'] > 0 ) {
 			$conds[] = 'rc_timestamp > ' .
 				$dbr->addQuotes( $dbr->timestamp( time() - intval( $opts['days'] * 86400 ) ) );
 		}
-
-		return $conds;
 	}
 
 	/**
-	 * Process the query
-	 *
-	 * @param array $conds
-	 * @param FormOptions $opts
-	 * @return bool|ResultWrapper Result or false (for Recentchangeslinked only)
+	 * @inheritdoc
 	 */
-	public function doMainQuery( $conds, $opts ) {
+	protected function doMainQuery( $tables, $fields, $conds, $query_options,
+		$join_conds, FormOptions $opts ) {
+
 		$dbr = $this->getDB();
 		$user = $this->getUser();
 
@@ -231,19 +269,23 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 			$usePage = true;
 		}
 
-		$tables = [ 'recentchanges', 'watchlist' ];
-		$fields = RecentChange::selectFields();
-		$query_options = [ 'ORDER BY' => 'rc_timestamp DESC' ];
-		$join_conds = [
-			'watchlist' => [
-				'INNER JOIN',
-				[
-					'wl_user' => $user->getId(),
-					'wl_namespace=rc_namespace',
-					'wl_title=rc_title'
+		$tables = array_merge( [ 'recentchanges', 'watchlist' ], $tables );
+		$fields = array_merge( RecentChange::selectFields(), $fields );
+
+		$query_options = array_merge( [ 'ORDER BY' => 'rc_timestamp DESC' ], $query_options );
+		$join_conds = array_merge(
+			[
+				'watchlist' => [
+					'INNER JOIN',
+					[
+						'wl_user' => $user->getId(),
+						'wl_namespace=rc_namespace',
+						'wl_title=rc_title'
+					],
 				],
 			],
-		];
+			$join_conds
+		);
 
 		if ( $this->getConfig()->get( 'ShowUpdatedMarker' ) ) {
 			$fields[] = 'wl_notificationtimestamp';
@@ -361,7 +403,7 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 
 		$dbr->dataSeek( $rows, 0 );
 
-		$list = ChangesList::newFromContext( $this->getContext() );
+		$list = ChangesList::newFromContext( $this->getContext(), $this->filterGroups );
 		$list->setWatchlistDivs();
 		$list->initChangesListRows( $rows );
 		$dbr->dataSeek( $rows, 0 );
@@ -448,31 +490,23 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		$cutofflinks = $this->msg( 'wlshowtime' ) . ' ' . $this->cutoffselector( $opts );
 
 		# Spit out some control panel links
-		$filters = [
-			'hideminor' => 'wlshowhideminor',
-			'hidebots' => 'wlshowhidebots',
-			'hideanons' => 'wlshowhideanons',
-			'hideliu' => 'wlshowhideliu',
-			'hidemyself' => 'wlshowhidemine',
-			'hidepatrolled' => 'wlshowhidepatr'
-		];
-
-		if ( $this->getConfig()->get( 'RCWatchCategoryMembership' ) ) {
-			$filters['hidecategorization'] = 'wlshowhidecategorization';
-		}
-
-		foreach ( $this->getRenderableCustomFilters( $this->getCustomFilters() ) as $key => $params ) {
-			$filters[$key] = $params['msg'];
-		}
-
-		// Disable some if needed
-		if ( !$user->useRCPatrol() ) {
-			unset( $filters['hidepatrolled'] );
-		}
-
 		$links = [];
-		foreach ( $filters as $name => $msg ) {
-			$links[] = $this->showHideCheck( $nondefaults, $msg, $name, $opts[$name] );
+		$context = $this->getContext();
+		$namesOfDisplayedFilters = [];
+		foreach ( $this->getFilterGroups() as $groupName => $group ) {
+			if ( !$group->isPerGroupRequestParameter() ) {
+				foreach ( $group->getFilters() as $filterName => $filter ) {
+					if ( $filter->displaysOnUnstructuredUi( $this ) ) {
+						$namesOfDisplayedFilters[] = $filterName;
+						$links[] = $this->showHideCheck(
+							$nondefaults,
+							$filter->getShowHide(),
+							$filterName,
+							$opts[$filterName]
+						);
+					}
+				}
+			}
 		}
 
 		$hiddenFields = $nondefaults;
@@ -481,8 +515,8 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		unset( $hiddenFields['invert'] );
 		unset( $hiddenFields['associated'] );
 		unset( $hiddenFields['days'] );
-		foreach ( $filters as $key => $value ) {
-			unset( $hiddenFields[$key] );
+		foreach ( $namesOfDisplayedFilters as $filterName ) {
+			unset( $hiddenFields[$filterName] );
 		}
 
 		# Create output

RELEASE-NOTES-1.29
@@ -54,6 +54,9 @@ production.
   This might affect some forms that used them and only worked because the
   attributes were not actually being set.
 * Expiry times can now be specified when users are added to user groups.
+* Completely new user interface for the RecentChanges page, which
+  structures filters into user-friendly groups.  This has corresponding
+  changes to how filters are registered by core and extensions.
 
 === External library changes in 1.29 ===
 
@@ -245,6 +248,15 @@ changes to languages because of Phabricator reports.
 * User::comparePasswords() (deprecated in 1.24) was removed.
 * ArchivedFile::getUserText() (deprecated in 1.23) was removed.
 * HTMLFileCache::newFromTitle() (deprecated in 1.24) was removed.
+* BREAKING CHANGE: Internal signature changes to ChangesListSpecialPage
+  and subclasses.  It should only break if you call buildMainQueryConds
+  (changed to buildQuery with new signature) or doMainQuery (new
+  signature).  Subclasses are likely to call at least doMainQuery
+  (possibly both), but other classes might too, because they were
+  public.
+
+  Also, some related hooks were deprecated, but this is not yet a
+  breaking change.
 
 == Compatibility ==
 

autoload.php
@@ -237,7 +237,13 @@ $wgAutoloadLocalClasses = [
 	'ChangeTagsRevisionList' => __DIR__ . '/includes/changetags/ChangeTagsRevisionList.php',
 	'ChangesFeed' => __DIR__ . '/includes/changes/ChangesFeed.php',
 	'ChangesList' => __DIR__ . '/includes/changes/ChangesList.php',
+	'ChangesListBooleanFilter' => __DIR__ . '/includes/changes/ChangesListBooleanFilter.php',
+	'ChangesListBooleanFilterGroup' => __DIR__ . '/includes/changes/ChangesListBooleanFilterGroup.php',
+	'ChangesListFilter' => __DIR__ . '/includes/changes/ChangesListFilter.php',
+	'ChangesListFilterGroup' => __DIR__ . '/includes/changes/ChangesListFilterGroup.php',
 	'ChangesListSpecialPage' => __DIR__ . '/includes/specialpage/ChangesListSpecialPage.php',
+	'ChangesListStringOptionsFilter' => __DIR__ . '/includes/changes/ChangesListStringOptionsFilter.php',
+	'ChangesListStringOptionsFilterGroup' => __DIR__ . '/includes/changes/ChangesListStringOptionsFilterGroup.php',
 	'ChannelFeed' => __DIR__ . '/includes/Feed.php',
 	'CheckBadRedirects' => __DIR__ . '/maintenance/checkBadRedirects.php',
 	'CheckComposerLockUpToDate' => __DIR__ . '/maintenance/checkComposerLockUpToDate.php',

includes/changes/ChangesList.php
@@ -26,6 +26,8 @@ use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\ResultWrapper;
 
 class ChangesList extends ContextSource {
+	const CSS_CLASS_PREFIX = 'mw-changeslist-';
+
 	/**
 	 * @var Skin
 	 */
@@ -48,11 +50,17 @@ class ChangesList extends ContextSource {
 	protected $linkRenderer;
 
 	/**
+	 * @var array
+	 */
+	protected $filterGroups;
+
+	/**
 	 * Changeslist constructor
 	 *
 	 * @param Skin|IContextSource $obj
+	 * @param array $filterGroups Array of ChangesListFilterGroup objects (currently optional)
 	 */
-	public function __construct( $obj ) {
+	public function __construct( $obj, array $filterGroups = [] ) {
 		if ( $obj instanceof IContextSource ) {
 			$this->setContext( $obj );
 			$this->skin = $obj->getSkin();
@@ -63,6 +71,7 @@ class ChangesList extends ContextSource {
 		$this->preCacheMessages();
 		$this->watchMsgCache = new HashBagOStuff( [ 'maxKeys' => 50 ] );
 		$this->linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
+		$this->filterGroups = $filterGroups;
 	}
 
 	/**
@@ -70,16 +79,19 @@ class ChangesList extends ContextSource {
 	 * Some users might want to use an enhanced list format, for instance
 	 *
 	 * @param IContextSource $context
+	 * @param array $groups Array of ChangesListFilterGroup objects (currently optional)
 	 * @return ChangesList
 	 */
-	public static function newFromContext( IContextSource $context ) {
+	public static function newFromContext( IContextSource $context, array $groups = [] ) {
 		$user = $context->getUser();
 		$sk = $context->getSkin();
 		$list = null;
 		if ( Hooks::run( 'FetchChangesList', [ $user, &$sk, &$list ] ) ) {
 			$new = $context->getRequest()->getBool( 'enhanced', $user->getOption( 'usenewrc' ) );
 
-			return $new ? new EnhancedChangesList( $context ) : new OldChangesList( $context );
+			return $new ?
+				new EnhancedChangesList( $context, $groups ) :
+				new OldChangesList( $context, $groups );
 		} else {
 			return $list;
 		}
@@ -159,42 +171,40 @@ class ChangesList extends ContextSource {
 	protected function getHTMLClasses( $rc, $watched ) {
 		$classes = [];
 		$logType = $rc->mAttribs['rc_log_type'];
-		$prefix = 'mw-changeslist-';
 
 		if ( $logType ) {
-			$classes[] = Sanitizer::escapeClass( $prefix . 'log-' . $logType );
+			$classes[] = Sanitizer::escapeClass( self::CSS_CLASS_PREFIX . 'log-' . $logType );
 		} else {
-			$classes[] = Sanitizer::escapeClass( $prefix . 'ns' .
+			$classes[] = Sanitizer::escapeClass( self::CSS_CLASS_PREFIX . 'ns' .
 				$rc->mAttribs['rc_namespace'] . '-' . $rc->mAttribs['rc_title'] );
 		}
 
 		// Indicate watched status on the line to allow for more
 		// comprehensive styling.
 		$classes[] = $watched && $rc->mAttribs['rc_timestamp'] >= $watched
-			? $prefix . 'line-watched'
-			: $prefix . 'line-not-watched';
+			? self::CSS_CLASS_PREFIX . 'line-watched'
+			: self::CSS_CLASS_PREFIX . 'line-not-watched';
 
 		$classes = array_merge( $classes, $this->getHTMLClassesForFilters( $rc ) );
 
 		return $classes;
 	}
 
+	/**
+	 * Get an array of CSS classes attributed to filters for this row
+	 *
+	 * @param RecentChange $rc
+	 * @return array Array of CSS classes
+	 */
 	protected function getHTMLClassesForFilters( $rc ) {
 		$classes = [];
-		$prefix = 'mw-changeslist-';
 
-		$classes[] = $prefix . ( $rc->getAttribute( 'rc_bot' ) ? 'bot' : 'human' );
-		$classes[] = $prefix . ( $rc->getAttribute( 'rc_user' ) ? 'liu' : 'anon' );
-		$classes[] = $prefix . ( $rc->getAttribute( 'rc_minor' ) ? 'minor' : 'major' );
-		$classes[] = $prefix .
-			( $rc->getAttribute( 'rc_patrolled' ) ? 'patrolled' : 'unpatrolled' );
-		$classes[] = $prefix .
-			( $this->getUser()->equals( $rc->getPerformer() ) ? 'self' : 'others' );
-		$classes[] = $prefix . 'src-' . str_replace( '.', '-', $rc->getAttribute( 'rc_source' ) );
-
-		$performer = $rc->getPerformer();
-		if ( $performer && $performer->isLoggedIn() ) {
-			$classes[] = $prefix . 'user-' . $performer->getExperienceLevel();
+		if ( $this->filterGroups !== null ) {
+			foreach ( $this->filterGroups as $filterGroup ) {
+				foreach ( $filterGroup->getFilters() as $filter ) {
+					$filter->applyCssClassIfNeeded( $this, $rc, $classes );
+				}
+			}
 		}
 
 		return $classes;

includes/changes/ChangesListBooleanFilter.php
@@ -1,0 +1,225 @@
+<?php
+/**
+ * Represents a hide-based boolean filter (used on ChangesListSpecialPage and descendants)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license GPL 2+
+ * @author Matthew Flaschen
+ */
+
+/**
+ * An individual filter in a boolean group
+ *
+ * @since 1.29
+ */
+class ChangesListBooleanFilter extends ChangesListFilter {
+	/**
+	 * Name.  Used as URL parameter
+	 *
+	 * @var string $name
+	 */
+
+	// This can sometimes be different on Special:RecentChanges
+	// and Special:Watchlist, due to the double-legacy hooks
+	// (SpecialRecentChangesFilters and SpecialWatchlistFilters)
+
+	// but there will be separate sets of ChangesListFilterGroup and ChangesListFilter instances
+	// for those pages (it should work even if they're both loaded
+	// at once, but that can't happen).
+	/**
+	 * Main unstructured UI i18n key
+	 *
+	 * @var string $showHide
+	 */
+	protected $showHide;
+
+	/**
+	 * Whether there is a feature designed to replace this filter available on the
+	 * structured UI
+	 *
+	 * @var bool $isReplacedInStructuredUi
+	 */
+	protected $isReplacedInStructuredUi;
+
+	/**
+	 * Default
+	 *
+	 * @var bool $defaultValue
+	 */
+	protected $defaultValue;
+
+	/**
+	 * Callable used to do the actual query modification; see constructor
+	 *
+	 * @var callable $queryCallable
+	 */
+	protected $queryCallable;
+
+	/**
+	 * Create a new filter with the specified configuration.
+	 *
+	 * It infers which UI (it can be either or both) to display the filter on based on
+	 * which messages are provided.
+	 *
+	 * If 'label' is provided, it will be displayed on the structured UI.  If
+	 * 'showHide' is provided, it will be displayed on the unstructured UI.  Thus,
+	 * 'label', 'description', and 'showHide' are optional depending on which UI
+	 * it's for.
+	 *
+	 * @param array $filterDefinition ChangesListFilter definition
+	 *
+	 * $filterDefinition['name'] string Name.  Used as URL parameter.
+	 * $filterDefinition['group'] ChangesListFilterGroup Group.  Filter group this
+	 *  belongs to.
+	 * $filterDefinition['label'] string i18n key of label for structured UI.
+	 * $filterDefinition['description'] string i18n key of description for structured
+	 *  UI.
+	 * $filterDefinition['showHide'] string Main i18n key used for unstructured UI.
+	 * $filterDefinition['isReplacedInStructuredUi'] bool Whether there is an
+	 *  equivalent feature available in the structured UI; this is optional, defaulting
+	 *  to true.  It does not need to be set if the exact same filter is simply visible
+	 *  on both.
+	 * $filterDefinition['default'] bool Default
+	 * $filterDefinition['isAllowedCallable'] callable Callable taking two parameters,
+	 *  the class name of the special page and an IContextSource, and returning true
+	 *  if and only if the current user is permitted to use this filter on the current
+	 *  wiki.  If it returns false, it will both hide the UI (in all UIs) and prevent
+	 *  the DB query modification from taking effect. (optional, defaults to allowed)
+	 * $filterDefinition['priority'] int Priority integer.  Higher value means higher
+	 *  up in the group's filter list.
+	 * $filterDefinition['queryCallable'] callable Callable accepting parameters, used
+	 *  to implement filter's DB query modification.  Callback parameters:
+	 *   string $specialPageClassName Class name of current special page
+	 *   IContextSource $context Context, for e.g. user
+	 *   IDatabase $dbr Database, for addQuotes, makeList, and similar
+	 *   array &$tables Array of tables; see IDatabase::select $table
+	 *   array &$fields Array of fields; see IDatabase::select $vars
+	 *   array &$conds Array of conditions; see IDatabase::select $conds
+	 *   array &$query_options Array of query options; see IDatabase::select $options
+	 *   array &$join_conds Array of join conditions; see IDatabase::select $join_conds
+	 *   Optional only for legacy filters that still use the query hooks directly
+	 */
+	public function __construct( $filterDefinition ) {
+		parent::__construct( $filterDefinition );
+
+		if ( isset( $filterDefinition['showHide'] ) ) {
+			$this->showHide = $filterDefinition['showHide'];
+		}
+
+		if ( isset( $filterDefinition['isReplacedInStructuredUi'] ) ) {
+			$this->isReplacedInStructuredUi = $filterDefinition['isReplacedInStructuredUi'];
+		} else {
+			$this->isReplacedInStructuredUi = false;
+		}
+
+		if ( isset( $filterDefinition['default'] ) ) {
+			$this->defaultValue = $filterDefinition['default'];
+		} else {
+			throw new MWException( 'You must set a default' );
+		}
+
+		if ( isset( $filterDefinition['queryCallable'] ) ) {
+			$this->queryCallable = $filterDefinition['queryCallable'];
+		}
+	}
+
+	/**
+	 * @return bool|null Default value
+	 */
+	public function getDefault() {
+		return $this->defaultValue;
+	}
+
+	/**
+	 * Sets default
+	 *
+	 * @param bool Default value
+	 */
+	public function setDefault( $defaultValue ) {
+		$this->defaultValue = $defaultValue;
+	}
+
+	/**
+	 * @return string Main i18n key for unstructured UI
+	 */
+	public function getShowHide() {
+		return $this->showHide;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function displaysOnUnstructuredUi( ChangesListSpecialPage $specialPage ) {
+		return $this->showHide &&
+			$this->isAllowed( $specialPage );
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function isFeatureAvailableOnStructuredUi( ChangesListSpecialPage $specialPage ) {
+		return $this->isReplacedInStructuredUi ||
+			parent::isFeatureAvailableOnStructuredUi( $specialPage );
+	}
+
+	/**
+	 * Modifies the query to include the filter.  This is only called if the filter is
+	 * in effect (taking into account the default).
+	 *
+	 * @param IDatabase $dbr Database, for addQuotes, makeList, and similar
+	 * @param ChangesListSpecialPage $specialPage Current special page
+	 * @param array &$tables Array of tables; see IDatabase::select $table
+	 * @param array &$fields Array of fields; see IDatabase::select $vars
+	 * @param array &$conds Array of conditions; see IDatabase::select $conds
+	 * @param array &$query_options Array of query options; see IDatabase::select $options
+	 * @param array &$join_conds Array of join conditions; see IDatabase::select $join_conds
+	 */
+	public function modifyQuery( IDatabase $dbr, ChangesListSpecialPage $specialPage,
+		&$tables, &$fields, &$conds, &$query_options, &$join_conds ) {
+
+		if ( $this->queryCallable === null ) {
+			return;
+		}
+
+		call_user_func_array(
+			$this->queryCallable,
+			[
+				get_class( $specialPage ),
+				$specialPage->getContext(),
+				$dbr,
+				&$tables,
+				&$fields,
+				&$conds,
+				&$query_options,
+				&$join_conds
+			]
+		);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getJsData() {
+		$output = parent::getJsData();
+
+		$output['default'] = $this->defaultValue;
+
+		return $output;
+	}
+
+}

commit b08e1e6fbddad9313e910db8bf19c33cbe40397c

Watchlist: Fix form and preference overriding
Fix a regression caused by aa063f4c5a19.  Restore the behavior
where:

* Without action=submit: Boolean preferences apply to the displayed form and its
  results, but they can be overriden by the query string.
* With action=submit: Only boolean parameters in the query string apply;
  preferences are not considered.

  (However, boolean preferences not on the form, i.e. 'extended', still
  always apply.)

This behavior is a consequence of how checkboxes work in HTML; only
checked boxes are sent to the server.

Bug: T160734
Change-Id: Ic050dd1445ade9449496bb051f04ca0a87b8b5d7

includes/specials/SpecialWatchlist.php
@@ -165,10 +165,6 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 
 		$opts->add( 'days', $user->getOption( 'watchlistdays' ), FormOptions::FLOAT );
 		$opts->add( 'extended', $user->getBoolOption( 'extendwatchlist' ) );
-		if ( $this->getRequest()->getVal( 'action' ) == 'submit' ) {
-			// The user has submitted the form, so we dont need the default values
-			return $opts;
-		}
 
 		return $opts;
 	}
@@ -212,6 +208,26 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 				$params[$to] = $params[$from];
 				unset( $params[$from] );
 			}
+		}
+
+		if ( $this->getRequest()->getVal( 'action' ) == 'submit' ) {
+			$allBooleansFalse = [];
+
+			// If the user submitted the form, start with a baseline of "all
+			// booleans are false", then change the ones they checked.  This
+			// means we ignore the defaults.
+			//
+			// This is how we handle the fact that HTML forms don't submit
+			// unchecked boxes.
+			foreach ( $this->filterGroups as $filterGroup ) {
+				if ( $filterGroup instanceof ChangesListBooleanFilterGroup ) {
+					foreach ( $filterGroup->getFilters() as $filter ) {
+						$allBooleansFalse[$filter->getName()] = false;
+					}
+				}
+			}
+
+			$params = $params + $allBooleansFalse;
 		}
 
 		// Not the prettiest way to achieve this… FormOptions internally depends on data sanitization

tests/phpunit/includes/specials/SpecialWatchlistTest.php
@@ -8,6 +8,41 @@
  * @covers SpecialWatchlist
  */
 class SpecialWatchlistTest extends SpecialPageTestBase {
+	public function setUp() {
+		parent::setUp();
+
+		$this->setTemporaryHook(
+			'ChangesListSpecialPageFilters',
+			null
+		);
+
+		$this->setTemporaryHook(
+			'SpecialWatchlistQuery',
+			null
+		);
+
+		$this->setTemporaryHook(
+			'ChangesListSpecialPageQuery',
+			null
+		);
+
+		$this->setMwGlobals(
+			'wgDefaultUserOptions',
+			[
+				'extendwatchlist' => 1,
+				'watchlistdays' => 3.0,
+				'watchlisthideanons' => 0,
+				'watchlisthidebots' => 0,
+				'watchlisthideliu' => 0,
+				'watchlisthideminor' => 0,
+				'watchlisthideown' => 0,
+				'watchlisthidepatrolled' => 0,
+				'watchlisthidecategorization' => 1,
+				'watchlistreloadautomatically' => 0,
+			]
+		);
+
+	}
 
 	/**
 	 * Returns a new instance of the special page under test.
@@ -29,4 +64,124 @@ class SpecialWatchlistTest extends SpecialPageTestBase {
 		$this->assertContains( '(nowatchlist)', $html );
 	}
 
+	/**
+	   @dataProvider provideFetchOptionsFromRequest
+	*/
+	public function testFetchOptionsFromRequest( $expectedValues, $preferences, $inputParams ) {
+		$page = TestingAccessWrapper::newFromObject(
+			$this->newSpecialPage()
+		);
+
+		$context = new DerivativeContext( $page->getContext() );
+
+		$fauxRequest = new FauxRequest( $inputParams, /* $wasPosted= */ false );
+		$user = $this->getTestUser()->getUser();
+
+		foreach ( $preferences as $key => $value ) {
+			$user->setOption( $key, $value );
+		}
+
+		$context->setRequest( $fauxRequest );
+		$context->setUser( $user );
+		$page->setContext( $context );
+
+		$page->registerFilters();
+		$formOptions = $page->getDefaultOptions();
+		$page->fetchOptionsFromRequest( $formOptions );
+
+		$this->assertArrayEquals(
+			$expectedValues,
+			$formOptions->getAllValues(),
+			/* $ordered= */ false,
+			/* $named= */ true
+		);
+	}
+
+	public function provideFetchOptionsFromRequest() {
+		// $defaults and $allFalse are just to make the expected values below
+		// shorter by hiding the background.
+
+		$page = TestingAccessWrapper::newFromObject(
+			$this->newSpecialPage()
+		);
+
+		$this->setTemporaryHook(
+			'ChangesListSpecialPageFilters',
+			null
+		);
+
+		$page->registerFilters();
+
+		// Does not consider $preferences, just wiki's defaults
+		$wikiDefaults = $page->getDefaultOptions()->getAllValues();
+
+		$allFalse = $wikiDefaults;
+
+		foreach ( $allFalse as $key => &$value ) {
+			if ( $value === true ) {
+				$value = false;
+			}
+		}
+
+		// This is not exposed on the form (only in preferences) so it
+		// respects the preference.
+		$allFalse['extended'] = true;
+
+		return [
+			[
+				[
+					'hideminor' => true,
+				] + $wikiDefaults,
+				[],
+				[
+					'hideMinor' => 1,
+				],
+			],
+
+			[
+				[
+					// First two same as prefs
+					'hideminor' => true,
+					'hidebots' => false,
+
+					// Second two overriden
+					'hideanons' => false,
+					'hideliu' => true,
+				] + $wikiDefaults,
+				[
+					'watchlisthideminor' => 1,
+					'watchlisthidebots' => 0,
+
+					'watchlisthideanons' => 1,
+					'watchlisthideliu' => 0,
+				],
+				[
+					'hideanons' => 0,
+					'hideliu' => 1,
+				],
+			],
+
+			// Defaults/preferences for form elements are entirely ignored for
+			// action=submit and omitted elements become false
+			[
+				[
+					'hideminor' => false,
+					'hidebots' => true,
+					'hideanons' => false,
+					'hideliu' => true,
+				] + $allFalse,
+				[
+					'watchlisthideminor' => 0,
+					'watchlisthidebots' => 1,
+					'watchlisthideanons' => 1,
+					'watchlisthideliu' => 0,
+				],
+				[
+					'hidebots' => 1,
+					'hideliu' => 1,
+					'action' => 'submit',
+				],
+			],
+		];
+	}
 }