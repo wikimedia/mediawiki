01871c8e9e336f3b52b0532c0d78ffd5469a509b
addshore <addshorewiki@gmail.com>
2017-03-20 13:18:22 +0000

    Add switch for readonly watchlists
    Bug: T160062
    Change-Id: I70d28df48f86e8cae4e454cf3f9097c65dc1d92b

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -6631,6 +6631,13 @@ $wgCommandLineDarkBg = false;
 $wgReadOnly = null;
 
 /**
+ * Set this to a string to put the wiki watchlists into read-only mode.
+ * @var bool
+ * @since 1.31
+ */
+$wgReadOnlyWatchedItemStore = false;
+
+/**
  * If this lock file exists (size > 0), the wiki will be forced into read-only mode.
  * Its contents will be shown to users as part of the read-only warning
  * message.
