b5541bd9560e52d82abd04c1c8b24f9f9d6da5c6
Aaron Schulz <aschulz@wikimedia.org>
2017-03-21 11:47:52 -0700

    [WIP] Add EtcdConfig class
    Bug: T156924
    Change-Id: I60914d31c21484bfb935fe3d8c3168b51a2d5d1b

---
+++ includes/config/EtcdConfig.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Copyright 2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @author Aaron Schulz
+ */
+
+use ActiveCollab\Etcd\Client;
+use ActiveCollab\Etcd\Exception\EtcdException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Wikimedia\WaitConditionLoop;
+
+/**
+ * Interface for configuration instances
+ *
+ * @since 1.29
+ */
+class EtcdConfig implements Config, LoggerAwareInterface {
+	/** @var Client */
+	private $client;
+	/** @var BagOStuff */
+	private $srvCache;
+	/** @var array */
+	private $procCache;
+	/** @var LoggerInterface */
+	private $logger;
+
+	/** @var string */
+	private $directory;
+	/** @var string */
+	private $encoding;
+	/** @var integer */
+	private $baseCacheTTL;
+	/** @var integer */
+	private $skewCacheTTL;
+	/** @var integer */
+	private $timeout;
+	/** @var string */
+	private $directoryHash;
+
+	/**
+	 * @param array $params Parameter map:
+	 *   - host: the host address and port
+	 *   - directory: the etc "directory" were MediaWiki specific variables are located
+	 *   - encoding: one of ("JSON", "YAML")
+	 *   - cache: BagOStuff instance or ObjectFactory spec thereof for a server cache.
+	 *            The cache will also be used as a fallback if etcd is down.
+	 *   - cacheTTL: logical cache TTL in seconds
+	 *   - skewTTL: maximum seconds to randomly lower the assigned TTL on cache save
+	 *   - timeout: seconds to wait for etcd before throwing an error
+	 */
+	public function __construct( array $params ) {
+		$params += [ 'encoding' => 'JSON', 'cacheTTL' => 10, 'skewTTL' => 1, 'timeout' => 10 ];
+
+		$this->client = new Client( $params['host'] );
+		$this->directory = rtrim( $params['directory'], '/' );
+		$this->directoryHash = sha1( $this->directory );
+		$this->encoding = $params['encoding'];
+		$this->skewCacheTTL = $params['skewTTL'];
+		$this->baseCacheTTL = max( $params['cacheTTL'] - $this->skewCacheTTL, 0 );
+		$this->timeout = $params['timeout'];
+
+		if ( !isset( $params['cache'] ) ) {
+			$this->srvCache = new HashBagOStuff( [] );
+		} elseif ( $params['cache'] instanceof BagOStuff ) {
+			$this->srvCache = $params['cache'];
+		} else {
+			$this->srvCache = ObjectFactory::getObjectFromSpec( $params['cache'] );
+		}
+
+		$this->logger = new Psr\Log\NullLogger();
+	}
+
+	public function setLogger( LoggerInterface $logger ) {
+		$this->logger = $logger;
+	}
+
+	public function has( $name ) {
+		$this->load();
+
+		return array_key_exists( $name, $this->procCache['config'] );
+	}
+
+	public function get( $name ) {
+		$this->load();
+
+		if ( !array_key_exists( $name, $this->procCache['config'] ) ) {
+			throw new ConfigException( "No entry found for '$name'." );
+		}
+
+		return $this->procCache['config'][$name];
+	}
+
+	private function load() {
+		if ( $this->procCache !== null ) {
+			return; // already loaded
+		}
+
+		$now = microtime( true );
+		$key = $this->srvCache->makeKey( 'variable', $this->directoryHash );
+
+		// Get the cached value or block until it is regenerated (by this or another thread)...
+		$data = null; // latest config info
+		$loop = new WaitConditionLoop(
+			function () use ( $key, $now, &$data ) {
+				// Check if the values are in cache yet...
+				$data = $this->srvCache->get( $key );
+				if ( is_array( $data ) && $data['expires'] > $now ) {
+					$this->logger->debug( "Found up-to-date etcd configuration cache." );
+
+					return WaitConditionLoop::CONDITION_REACHED;
+				}
+
+				// Cache is either empty or stale;
+				// refresh the cache from etcd, using a mutex to reduce stampedes...
+				if ( $this->srvCache->lock( $key, 0, $this->baseCacheTTL ) ) {
+					try {
+						$config = $this->fetchConfigFromEtcd();
+						// Avoid having all servers expire cache keys at the same time
+						$expiry = microtime( true ) + $this->baseCacheTTL;
+						$expiry += mt_rand( 0, 1e6 ) / 1e6 * $this->skewCacheTTL;
+
+						$data = [ 'config' => $config, 'expires' => $expiry ];
+						$this->srvCache->set( $key, $data, BagOStuff::TTL_INDEFINITE );
+
+						$this->logger->info( "Refreshed stale etcd configuration cache." );
+
+						return WaitConditionLoop::CONDITION_REACHED;
+					} catch ( EtcdException $e ) {
+						$this->logger->error(
+							"Failed to refresh configuration data. Got " .
+							get_class( $e ) . ": {$e->getMessage()}." );
+					} finally {
+						$this->srvCache->unlock( $key ); // release mutex
+					}
+				}
+
+				if ( is_array( $data ) ) {
+					$this->logger->info( "Using stale etcd configuration cache." );
+
+					return WaitConditionLoop::CONDITION_REACHED;
+				}
+
+				return WaitConditionLoop::CONDITION_CONTINUE;
+			},
+			$this->timeout
+		);
+
+		if ( $loop->invoke() !== WaitConditionLoop::CONDITION_REACHED ) {
+			// No cached value exists and etcd query failed; throw an error
+			throw new ConfigException( "Failed to load configuration from etcd." );
+		}
+
+		$this->procCache = $data;
+	}
+
+	/**
+	 * @return array
+	 * @throws EtcdException
+	 * @throws ConfigException
+	 */
+	public function fetchConfigFromEtcd() {
+		// Retrieve all the values under the MediaWiki config directory
+		$values = $this->client->getKeyValueMap( "{$this->directory}/", false );
+
+		$config = [];
+		foreach ( $values as $key => $value ) {
+			$name = basename( $key );
+			$map = $this->unserialize( $value );
+			// Extract the core value out of the etcd object (which contains other metadata)
+			$this->assertMapHasKeys( $map, [ 'val' ], $name );
+			$config[$name] = $map['val'];
+		}
+
+		return $config;
+	}
+
+	/**
+	 * @param string $string
+	 * @return mixed
+	 */
+	private function unserialize( $string ) {
+		if ( $this->encoding === 'YAML' ) {
+			return yaml_parse( $string );
+		} else { // JSON
+			return json_decode( $string, true );
+		}
+	}
+
+	/**
+	 * @param mixed $value
+	 * @param array $keys
+	 * @param string $name
+	 * @throws ConfigException
+	 */
+	private function assertMapHasKeys( $value, array $keys, $name ) {
+		if ( !is_array( $value ) ) {
+			throw new ConfigException( "Value for '$name' is not an array." );
+		}
+
+		foreach ( $keys as $key ) {
+			if ( !array_key_exists( $key, $value ) ) {
+				throw new ConfigException( "Expected key '$key' inside '$name' value." );
+			}
+		}
+	}
+}
