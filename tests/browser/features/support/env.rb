a3144a21bcc253741d8bd169e6f1994b12c3ad8b
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-21 12:10:46 +0100

    Remove Selenium tests written in Ruby
    New Selenium tests in Node.js are in tests/selenium.
    
    Bug: T164004
    Change-Id: I3ddeecb4f012ba4a614b3ada81c5926da28556f4
    Depends-On: I062d68458d8f472d636f1ae15f4fc37110460854

--- tests/browser/features/support/env.rb
+++
@@ -1,3 +0,0 @@
-require 'mediawiki_selenium/cucumber'
-require 'mediawiki_selenium/pages'
-require 'mediawiki_selenium/step_definitions'
--- tests/browser/features/support/pages/edit_page.rb
+++
@@ -1,8 +0,0 @@
-class EditPage
-  include PageObject
-
-  text_area(:edit_page_content, id: 'wpTextbox1')
-  button(:preview_button, css: '#wpPreview > input')
-  button(:show_changes_button, css: '#wpDiff > input')
-  button(:save_button, css: '#wpSave > input')
-end
