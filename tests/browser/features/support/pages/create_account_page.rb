4bd9a71079675762433c099ba39e7a4f7f32a921
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-21 12:10:46 +0100

    Remove Selenium tests written in Ruby
    New Selenium tests in Node.js are in tests/selenium.
    
    Change-Id: I3ddeecb4f012ba4a614b3ada81c5926da28556f4
    Depends-On: I062d68458d8f472d636f1ae15f4fc37110460854

--- tests/browser/features/support/pages/create_account_page.rb
+++
@@ -1,8 +0,0 @@
-class CreateAccountPage
-  include PageObject
-
-  page_url '<%=params[:page_title]%>'
-
-  button(:create_account, id: 'wpCreateaccount')
-  element(:error_message, css: 'input#wpName2:required:invalid')
-end
