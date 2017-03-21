ea38fbaf58a44a42e7bda4fe69bbc5c2644ce622
Željko Filipin <zeljko.filipin@gmail.com>
2017-03-21 12:10:46 +0100

    Remove Selenium tests written in Ruby
    New Selenium tests in Node.js are in tests/selenium.
    
    Change-Id: I3ddeecb4f012ba4a614b3ada81c5926da28556f4
    Depends-On: I062d68458d8f472d636f1ae15f4fc37110460854

--- tests/browser/features/step_definitions/file_steps.rb
+++
@@ -1,7 +0,0 @@
-Given(/^I am at file that does not exist$/) do
-  visit(FileDoesNotExistPage, using_params: { page_name: @random_string })
-end
-
-Then(/^page should show that no such file exists$/) do
-  expect(on(FileDoesNotExistPage).file_does_not_exist_message_element).to be_visible
-end
--- tests/browser/features/support/pages/login_page.rb
+++
@@ -1,9 +0,0 @@
-require 'page-object'
-
-class LoginPage
-  include PageObject
-
-  element(:error_message, css: 'div#userloginForm div.error')
-  element(:password_error, css: 'input#wpPassword1:required:invalid')
-  element(:username_error, css: 'input#wpName1:required:invalid')
-end
