4542db38bd168c728c93732c0c34d364937e45d5
Moriel Schottlender <moriel@gmail.com>
2017-03-27 09:58:29 -0700

    RCFilters: [EXPERIMENTAL] Adjust to use MenuTagMultiselectWidget
    The new widget in OOUI is more stable and easier to manage.
    However, the usage of menu - while theoretically supposed to answer
    a bunch of small issues - may actually show to expose a bunch of
    other worse issues. We should just use the PopupTagMultiselectWidget
    and fix OOUI's menu upstream to be more robust and answer our needs.
    
    NOTE: To run this, you have to use an ooui build with the patch
    https://gerrit.wikimedia.org/r/#/c/344669/
    
    Change-Id: I42be0691304b1e93b4e9c02eba2e3a724a5ffd67
    Depends-On: Ic216769f48e4677da5b7274f491aa08a95aa8076

---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterFloatingMenuSelectWidget.js
@@ -0,0 +1,45 @@
+( function ( mw ) {
+	/**
+	 * A floating menu widget for the filter list
+	 *
+	 * @extends OO.ui.FloatingMenuSelectWidget
+	 *
+	 * @constructor
+	 * @param {Object} config Configuration object
+	 */
+	mw.rcfilters.ui.FilterFloatingMenuSelectWidget = function MwRcfiltersUiFilterFloatingMenuSelectWidget( config ) {
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.FilterFloatingMenuSelectWidget.parent.call( this, config );
+		this.setGroupElement(
+			$( '<div>' )
+				.addClass( 'mw-rcfilters-ui-filterFloatingMenuSelectWidget-group' )
+		);
+		this.setClippableElement( this.$group );
+
+		this.$footer = config.$footer;
+		this.width = config.width || this.$container.width();
+
+		this.$element
+			.addClass( 'mw-rcfilters-ui-filterFloatingMenuSelectWidget' )
+			.append( this.$group );
+
+		if ( config.$footer ) {
+			// TODO: Actually style this to be sticky
+			this.menu.$element.append( $footer );
+		}
+	};
+
+	/* Initialize */
+
+	OO.inheritClass( mw.rcfilters.ui.FilterFloatingMenuSelectWidget, OO.ui.FloatingMenuSelectWidget );
+
+	mw.rcfilters.ui.FilterFloatingMenuSelectWidget.prototype.getItemMatcher = function ( s ) {
+		var results = this.model.findMatches( s );
+
+		return function ( item ) {
+			return results.indexOf( item.getModel() ) > -1;
+		};
+	};
+}( mediaWiki ) );
