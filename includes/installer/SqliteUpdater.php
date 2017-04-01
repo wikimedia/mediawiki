0d68c366075fa8bd6ec207b1fc2ed8ea710e0290
Paladox <thomasmulhall410@yahoo.com>
2017-04-01 12:13:34 +0000

    Add primary key qc_type and qc_value
    Bug: T146571
    Change-Id: I92518d2f14fa100b887aa02e9615e4513b9eb59b

--- includes/installer/SqliteUpdater.php
+++ includes/installer/SqliteUpdater.php
@@ -163,6 +163,7 @@ class SqliteUpdater extends DatabaseUpdater {
 			[ 'addField', 'externallinks', 'el_index_60', 'patch-externallinks-el_index_60.sql' ],
 			[ 'addField', 'user_groups', 'ug_expiry', 'patch-user_groups-ug_expiry.sql' ],
 			[ 'addIndex', 'image', 'img_user_timestamp', 'patch-image-user-index-2.sql' ],
+			[ 'dropIndex', 'querycache', 'qc_type', 'patch-querycache-primary-key.sql' ],
 		];
 	}
 
