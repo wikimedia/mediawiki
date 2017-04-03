25f2b1a5c31bbd14412f5513a47f7c648711cd26
Tim Starling <tstarling@wikimedia.org>
2017-04-03 13:00:30 +1000

    Fix incorrect index name in new 1.28.0 installations
    This index name should be the same in new installations as in all other
    MySQL installations. This is necessary so that
    Special:DeletedContributions does not give an SQL error. Release notes
    will go in the 1.28 branch.
    
    Bug: T154872
    Change-Id: I535a5513ab56203139c4cf64874ea7e7498990e3

--- includes/installer/MysqlUpdater.php
+++ includes/installer/MysqlUpdater.php
@@ -291,6 +291,7 @@ class MysqlUpdater extends DatabaseUpdater {
 			[ 'addField', 'change_tag', 'ct_id', 'patch-change_tag-ct_id.sql' ],
 			[ 'addField', 'tag_summary', 'ts_id', 'patch-tag_summary-ts_id.sql' ],
 			[ 'modifyField', 'recentchanges', 'rc_ip', 'patch-rc_ip_modify.sql' ],
+			[ 'addIndex', 'archive', 'usertext_timestamp', 'patch-rename-ar_usertext_timestamp.sql' ],
 		];
 	}
 
