wikimedia/mediawiki

commit c5a0fa5bed4c5aafdf0721c1d05978576107bd85

objectcache: Never use CACHE_NONE for CACHE_ANYTHING
If $wgMainCacheType = CACHE_ACCEL, but there is no APC, then its
possible that CACHE_ANYTHING will default to CACHE_NONE because
that's what CACHE_ACCEL would do.

Possibly also T147161

Bug: T160519
Change-Id: I9ac2d071437b35a0f9cd3678e2279628f7b1931e

tests/phpunit/includes/objectcache/ObjectCacheTest.php
@@ -1,0 +1,82 @@
+<?php
+
+class ObjectCacheTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		// Parent calls ObjectCache::clear() among other things
+		parent::setUp();
+
+		$this->setCacheConfig();
+		$this->setMwGlobals( [
+			'wgMainCacheType' => CACHE_NONE,
+			'wgMessageCacheType' => CACHE_NONE,
+			'wgParserCacheType' => CACHE_NONE,
+		] );
+	}
+
+	private function setCacheConfig( $arr = [] ) {
+		$defaults = [
+			CACHE_NONE => [ 'class' => 'EmptyBagOStuff' ],
+			CACHE_DB => [ 'class' => 'SqlBagOStuff' ],
+			CACHE_ANYTHING => [ 'factory' => 'ObjectCache::newAnything' ],
+			// Mock ACCEL with 'hash' as being installed.
+			// This makes tests deterministic regardless of APC.
+			CACHE_ACCEL => [ 'class' => 'HashBagOStuff' ],
+			'hash' => [ 'class' => 'HashBagOStuff' ],
+		];
+		$this->setMwGlobals( 'wgObjectCaches', $arr + $defaults );
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function testNewAnythingNothing() {
+		$this->assertInstanceOf(
+			SqlBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'No available types. Fallback to DB'
+		);
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function testNewAnythingHash() {
+		$this->setMwGlobals( [
+			'wgMainCacheType' => 'hash'
+		] );
+
+		$this->assertInstanceOf(
+			HashBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'Use an available type (hash)'
+		);
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function testNewAnythingAccel() {
+		$this->setMwGlobals( [
+			'wgMainCacheType' => CACHE_ACCEL
+		] );
+
+		$this->assertInstanceOf(
+			HashBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'Use an available type (CACHE_ACCEL)'
+		);
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function txestNewAnythingNoAccel() {
+		$this->setMwGlobals( [
+			'wgMainCacheType' => CACHE_ACCEL
+		] );
+
+		$this->setCacheConfig( [
+			// Mock APC not being installed (T160519, T147161)
+			CACHE_ACCEL => [ 'class' => 'EmptyBagOStuff' ]
+		] );
+
+		$this->assertInstanceOf(
+			SqlBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'Fallback to DB if available types fall back to Empty'
+		);
+	}
+}

includes/objectcache/ObjectCache.php
@@ -246,8 +246,14 @@ class ObjectCache {
 		global $wgMainCacheType, $wgMessageCacheType, $wgParserCacheType;
 		$candidates = [ $wgMainCacheType, $wgMessageCacheType, $wgParserCacheType ];
 		foreach ( $candidates as $candidate ) {
+			$cache = false;
 			if ( $candidate !== CACHE_NONE && $candidate !== CACHE_ANYTHING ) {
-				return self::getInstance( $candidate );
+				$cache = self::getInstance( $candidate );
+				// CACHE_ACCEL might default to nothing if no APCu
+				// See includes/ServiceWiring.php
+				if ( !( $cache instanceof EmptyBagOStuff ) ) {
+					return $cache;
+				}
 			}
 		}
 

commit ef35e46d935e4e6bd605a21fe1731719a1ba186a

objectcache: Complete coverage for newAnything()
* Fix typo that disabled testNewAnythingNoAccel().
  Follows-up c5a0fa5bed, accidentally committed a local hack
  to disable the test.

* Add missing case other types falling back and no DB.
* Add missing case of no other types and no DB.

Change-Id: If158f21053f0b3741f2625fe4455fdb31955a22f

tests/phpunit/includes/objectcache/ObjectCacheTest.php
@@ -63,7 +63,7 @@ class ObjectCacheTest extends MediaWikiTestCase {
 	}
 
 	/** @covers ObjectCache::newAnything */
-	public function txestNewAnythingNoAccel() {
+	public function testNewAnythingNoAccel() {
 		$this->setMwGlobals( [
 			'wgMainCacheType' => CACHE_ACCEL
 		] );
@@ -77,6 +77,35 @@ class ObjectCacheTest extends MediaWikiTestCase {
 			SqlBagOStuff::class,
 			ObjectCache::newAnything( [] ),
 			'Fallback to DB if available types fall back to Empty'
+		);
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function testNewAnythingNoAccelNoDb() {
+		$this->setMwGlobals( [
+			'wgMainCacheType' => CACHE_ACCEL
+		] );
+
+		$this->setCacheConfig( [
+			// Mock APC not being installed (T160519, T147161)
+			CACHE_ACCEL => [ 'class' => 'EmptyBagOStuff' ]
+		] );
+
+		MediaWikiServices::disableStorageBackend();
+
+		$this->assertInstanceOf(
+			EmptyBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'Fallback to none if available types and DB are unavailable'
+		);
+	}
+
+	/** @covers ObjectCache::newAnything */
+	public function testNewAnythingNothingNoDb() {
+		$this->assertInstanceOf(
+			EmptyBagOStuff::class,
+			ObjectCache::newAnything( [] ),
+			'No available types or DB. Fallback to none.'
 		);
 	}
 }