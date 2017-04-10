aebb8ffbd55a9ac328bccf4ad1a8ad79f7c4f358
Seb35 <seb35@seb35.fr>
2017-04-10 18:13:30 +0200

    Add a timeout in wfShellExec
    In MediaWiki\Shell\Command::execute() the function stream_select waits
    indefinitely until some data comes; in some race cases it really hangs
    indefinitely the normal execution of MediaWiki. One such race case is
    when more than 1024 files are opened simultaneously and when PHP is
    compiled with FD_SETSIZE=1024; it can be the case in some shared webservers
    or with PHPDBG during unit tests.
    
    This patch adds a timeout of 10 seconds (something like “very long” but
    anyway bounded) and a condition to loop without reading the file descriptors
    if no one is changed (which occurs when the timeout is reached).
    
    Added a test. This test could be dangerous without the associated timeout
    added in wfShellExec because it can halt the tests execution.
    
    Bug: T72357
    Change-Id: I506bfef7cd10ae137c33907fd056caa15000a51f

--- tests/phpunit/includes/GlobalFunctions/wfShellExecTest.php
+++ tests/phpunit/includes/GlobalFunctions/wfShellExecTest.php
@@ -17,4 +17,44 @@ class WfShellExecTest extends MediaWikiTestCase {
 			$this->assertEquals( 333333, strlen( $output ) );
 		}
 	}
+
+	/**
+	 * Stress wfShellExec when there are too much opened files.
+	 *
+	 * WARNING: executing this test with PHP (no issue with HHVM) under the requirements (Linux and
+	 * soft limit of opened files greater than 1536) and without the associated timeout from T72357
+	 * will unconditionally leads to an indefinite halt of the tests execution.
+	 *
+	 * @requires OS Linux
+	 */
+	public function testBugT72357() {
+		$FD_SETSIZE = 1024; // default value in most PHP distributions (set before compilation)
+		$limitFiles = intval( trim( wfShellExec( 'ulimit -Sn' ) ) );
+
+		// `ulimit -Sn` must be greater than FD_SETSIZE and it is better to have some margin for
+		// opened PHP files themselves
+		if ( $limitFiles < $FD_SETSIZE + 512 ) {
+			$this->markTestSkipped(
+				'Environment of bug T72357 cannot be reproduced; you have to ' .
+				'execute `ulimit -Sn ' . ( $FD_SETSIZE + 512 ) . '` before running the tests.'
+			);
+		}
+
+		$files = [];
+		for ( $i = 0; $i < $FD_SETSIZE + 1; $i++ ) {
+			$files[$i] = fopen( __FILE__, 'r' );
+		}
+
+		// Tested function
+		// A more difficult test would be 'echo ok' - it currently does not work (see T72357)
+		$result = wfShellExec( 'echo' );
+
+		// Close as soon as possible to avoid additional issues
+		$numFiles = count( $files );
+		for ( $i = 0; $i < $numFiles; $i++ ) {
+			fclose( $files[$i] );
+		}
+
+		$this->assertEquals( '', trim( $result ), 'The Linux command `echo` shouldn’t return anything.' );
+	}
 }
