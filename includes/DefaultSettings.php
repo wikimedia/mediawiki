57062b01fdeafa5d63387b45b92bd2350d220a88
Chad Horohoe <chadh@wikimedia.org>
2017-04-11 11:38:48 -0700

    Bumping release version to 1.23.17
    Change-Id: Id32ccf1826c09ee134122e6b3f8aa5fd3bcc60be

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -73,7 +73,7 @@ $wgConfigRegistry = array(
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.23.16';
+$wgVersion = '1.23.17';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
