1bfa7ed20d782d14bc42230083c87f6c7221def0
Sébastien Santoro <dereckson@espace-win.org>
2017-04-13 13:48:11 +0000

    Localize namespaces in Doteli
    Translation authors:
      - @Rameshti (Ram Prasad Joshi)
      - @nirajan_pant (Nirajan Pant)
      - @Janak_bhatta (Janak Bhatta)
    
    Bug: T162872
    Change-Id: I3f28ebd5e5486e6e5f3bde7c3cbc42975e26af40

--- languages/messages/MessagesDty.php
+++ languages/messages/MessagesDty.php
@@ -8,4 +8,23 @@
  *
  */
 
+$namespaceNames = [
+	NS_MEDIA            => 'मिडिया',
+	NS_SPECIAL          => 'बिशेष',
+	NS_TALK             => 'कुरणि',
+	NS_USER             => 'प्रयोगकर्ता',
+	NS_USER_TALK        => 'प्रयोगकर्ता_कुरणि',
+	NS_PROJECT_TALK     => '$1_वार्ता',
+	NS_FILE             => 'चित्र',
+	NS_FILE_TALK        => 'चित्र_कुरणि',
+	NS_MEDIAWIKI        => 'मिडियाविकि',
+	NS_MEDIAWIKI_TALK   => 'मिडियाविकि_कुरणि',
+	NS_TEMPLATE         => 'ढाँचा',
+	NS_TEMPLATE_TALK    => 'ढाँचा_कुरणि',
+	NS_HELP             => 'मद्दत',
+	NS_HELP_TALK        => 'मद्दत_कुरणि',
+	NS_CATEGORY         => 'श्रेणी',
+	NS_CATEGORY_TALK    => 'श्रेणी_कुरणि',
+];
+
 $fallback = 'ne';
