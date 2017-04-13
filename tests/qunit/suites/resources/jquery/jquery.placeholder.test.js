74432560bea07c969125c2c52eac35af706b002b
Timo Tijhof <krinklemail@gmail.com>
2017-04-13 14:00:24 -0700

    qunit: Remove test for deprecated jquery.placeholder
    It's not very useful and needlessly emits deprecation warnings
    which unfortunately cannot be surpressed at runtime.
    
    The test just asserts that a no-op function exists.
    
    Change-Id: I2768ba40191a3c0cc7be6202ffa5bed529eabfee

--- tests/qunit/suites/resources/jquery/jquery.placeholder.test.js
+++
@@ -1,8 +0,0 @@
-( function ( $ ) {
-	QUnit.module( 'jquery.placeholder', QUnit.newMwEnvironment() );
-
-	QUnit.test( 'caches results of feature tests', function ( assert ) {
-		assert.strictEqual( typeof $.fn.placeholder.input, 'boolean', '$.fn.placeholder.input' );
-		assert.strictEqual( typeof $.fn.placeholder.textarea, 'boolean', '$.fn.placeholder.textarea' );
-	} );
-}( jQuery ) );
