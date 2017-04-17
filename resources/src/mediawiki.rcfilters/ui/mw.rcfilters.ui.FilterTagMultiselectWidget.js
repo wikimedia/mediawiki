43a28e6693bc07f2601dd9ff71963f0ec718c1fa
Moriel Schottlender <moriel@gmail.com>
2017-04-17 16:45:54 -0700

    RCFilters UI: Select tag when filter item is selected
    Retain the selection behavior from previous versions, and synchronize
    the selection of the filter item in the menu with the tag items
    in the MenuTagMultiselectWidget.
    
    Bug: T163138
    Change-Id: I548a1fca65372c697e24b77d928d56b18f4df1c5

--- resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterTagMultiselectWidget.js
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.FilterTagMultiselectWidget.js
@@ -137,6 +137,7 @@
 		} else {
 			// Clear selection
 			this.getMenu().selectItem( null );
+			this.selectTag( null );
 		}
 	};
 
@@ -207,6 +208,9 @@
 	mw.rcfilters.ui.FilterTagMultiselectWidget.prototype.onMenuChoose = function ( item ) {
 		this.controller.toggleFilterSelect( item.model.getName() );
 
+		// Select the tag if it exists, or reset selection otherwise
+		this.selectTag( this.getItemFromData( item.model.getName() ) );
+
 		this.focus();
 	};
 
@@ -248,6 +252,7 @@
 		mw.rcfilters.ui.FilterTagMultiselectWidget.parent.prototype.onTagSelect.call( this, tagItem );
 
 		this.menu.selectItem( menuOption );
+		this.selectTag( tagItem );
 
 		// Scroll to the item
 		if ( oldInputValue ) {
@@ -264,6 +269,24 @@
 		}
 	};
 
+	/**
+	 * Select a tag by reference. This is what OO.ui.SelectWidget is doing.
+	 * If no items are given, reset selection from all.
+	 *
+	 * @param {mw.rcfilters.ui.FilterTagItemWidget} [item] Tag to select,
+	 *  omit to deselect all
+	 */
+	mw.rcfilters.ui.FilterTagMultiselectWidget.prototype.selectTag = function ( item ) {
+		var i, len, selected,
+			changed = false;
+
+		for ( i = 0, len = this.items.length; i < len; i++ ) {
+			selected = this.items[ i ] === item;
+			if ( this.items[ i ].isSelected() !== selected ) {
+				this.items[ i ].toggleSelected( selected );
+			}
+		}
+	};
 	/**
 	 * @inheritdoc
 	 */
