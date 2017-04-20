c130c8f367a2fe42203b88046724b82fe524a888
WMDE-Fisch <christoph.jauera@wikimedia.de>
2017-04-20 11:27:05 +0200

    Fix EditPage button/input accessors on PageObject
    The DOM was changed again in https://gerrit.wikimedia.org/r/#/c/348765/
    and input elements where used for the buttons.
    
    Bug: T163193
    Change-Id: I910a31a30d5569aab69dc2e5f81b377b6d867f64

--- tests/browser/features/support/pages/edit_page.rb
+++ tests/browser/features/support/pages/edit_page.rb
@@ -2,7 +2,7 @@ class EditPage
   include PageObject
 
   text_area(:edit_page_content, id: 'wpTextbox1')
-  button(:preview_button, css: '#wpPreview > button')
-  button(:show_changes_button, css: '#wpDiff > button')
-  button(:save_button, css: '#wpSave > button')
+  button(:preview_button, css: '#wpPreview > input')
+  button(:show_changes_button, css: '#wpDiff > input')
+  button(:save_button, css: '#wpSave > input')
 end
