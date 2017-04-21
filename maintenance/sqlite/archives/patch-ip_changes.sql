0e88333d933fda03fc47c0d05e44300a31a3aac3
MusikAnimal <musikanimal@gmail.com>
2017-04-21 12:17:59 -0400

    [WIP] Add basic IP range support to Special:Contributions
    This works by using the new table introduced with T163562.
    
    The only thing that differs from normal Special:Contribs is we are
    showing the IP address next to each entry. This is it how it is
    displayed if you request to see newbie contributions:
    https://en.wikipedia.org/wiki/Special:Contributions?contribs=newbie
    
    Various other invalid links such as Uploads and Logs are also
    hidden.
    
    For the time being, Special:DeletedContributions does not support
    IP ranges.
    
    Bug: T163562
    Change-Id: Ice1bdae3d16cf365da14c6df0e8d91d2b914e064

---
+++ maintenance/sqlite/archives/patch-ip_changes.sql
@@ -0,0 +1,23 @@
+--
+-- Every time an edit by a logged out user is saved,
+-- a row is created in ip_changes. This stores
+-- the IP as a hex representation so that we can more
+-- easily find edits within an IP range.
+--
+CREATE TABLE /*_*/ip_changes (
+  -- Foreign key to the revision table, also serves as the unique primary key
+  ipc_rev_id int unsigned NOT NULL PRIMARY KEY DEFAULT '0',
+
+  -- The timestamp of the revision
+  ipc_rev_timestamp binary(14) NOT NULL DEFAULT '',
+
+  -- Hex representation of the IP address, as returned by IP::toHex()
+  -- For IPv4 it will resemble: ABCD1234
+  -- For IPv6: v6-ABCD1234000000000000000000000000
+  -- BETWEEN is then used to identify revisions within a given range
+  ipc_hex varbinary(35) NOT NULL DEFAULT ''
+
+) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/ipc_rev_timestamp ON /*_*/ip_changes (ipc_rev_timestamp);
+CREATE INDEX /*i*/ipc_hex_time ON /*_*/ip_changes (ipc_hex,ipc_rev_timestamp);
