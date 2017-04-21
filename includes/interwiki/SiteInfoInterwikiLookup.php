73539cc3c4231ed435f666f43a11640e184da864
daniel <daniel.kinzler@wikimedia.de>
2017-04-21 22:19:57 +0200

    Introduce SiteInfoLookup and adapters.
    Change-Id: I0a795351ae7741c9cb15ed38057a3cb07703d34e

---
+++ includes/interwiki/SiteInfoInterwikiLookup.php
@@ -0,0 +1,187 @@
+<?php
+namespace MediaWiki\Interwiki;
+
+/**
+ * InterwikiLookupAdapter on top of SiteInfoLookup
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ *
+ * @since 1.29
+ * @ingroup InterwikiLookup
+ *
+ * @license GNU GPL v2+
+ */
+
+use Interwiki;
+use MediaWiki\Site\SiteInfoLookup;
+
+class SiteInfoInterwikiLookup implements InterwikiLookup {
+
+	private $defaults = [
+		SiteInfoLookup::SITE_BASE_URL => '',
+		SiteInfoLookup::SITE_LINK_PATH => '',
+		SiteInfoLookup::SITE_SCRIPT_PATH => '',
+		SiteInfoLookup::SITE_IS_FORWARDABLE => false,
+		SiteInfoLookup::SITE_IS_TRANSCLUDABLE => false,
+	];
+
+	/**
+	 * @var SiteInfoLookup
+	 */
+	private $siteLookup;
+
+	function __construct(
+		SiteInfoLookup $siteLookup
+	) {
+		$this->siteLookup = $siteLookup;
+	}
+
+	/**
+	 * See InterwikiLookup::isValidInterwiki
+	 * It loads the whole interwiki map.
+	 *
+	 * @param string $prefix Interwiki prefix to use
+	 * @return bool Whether it exists
+	 */
+	public function isValidInterwiki( $prefix ) {
+		return $this->siteLookup->resolveLocalId( SiteInfoLookup::INTERWIKI_ID, $prefix ) !== null
+			|| $this->siteLookup->resolveLocalId( SiteInfoLookup::NAVIGATION_ID, $prefix ) !== null;
+	}
+
+	/**
+	 * See InterwikiLookup::fetch
+	 * It loads the whole interwiki map.
+	 *
+	 * @param string $prefix Interwiki prefix to use
+	 * @return Interwiki|null|bool
+	 */
+	public function fetch( $prefix ) {
+		if ( $prefix == '' ) {
+			return null;
+		}
+
+		$siteId = $this->siteLookup->resolveLocalId( SiteInfoLookup::INTERWIKI_ID, $prefix );
+
+		if ( $siteId === null ) {
+			$siteId = $this->siteLookup->resolveLocalId( SiteInfoLookup::NAVIGATION_ID, $prefix );
+		}
+
+		if ( $siteId === null ) {
+			return false;
+		}
+
+		$info = $this->siteLookup->getSiteInfo( $siteId, $this->defaults );
+		$row = $this->buildInterwikiRow( $siteId, $prefix, $info );
+
+		// XXX: cache?! Implement invalidateCache() if we do!
+		$interwiki = new Interwiki(
+			$row['iw_prefix'],
+			$row['iw_url'],
+			$row['iw_api'],
+			$row['iw_wikiid'],
+			$row['iw_local'],
+			$row['iw_trans']
+		);
+
+		return $interwiki;
+	}
+
+	private function buildInterwikiRow( $siteId, $prefix, array $info ) {
+		$id = $this->siteLookup->resolveLocalId( SiteInfoLookup::INTERWIKI_ID, $prefix );
+
+		if ( $id === null ) {
+			$id = $this->siteLookup->resolveLocalId( SiteInfoLookup::NAVIGATION_ID, $prefix );
+		}
+
+		if ( $id === null ) {
+			return false;
+		}
+
+		$info = $this->siteLookup->getSiteInfo( $id, $this->defaults );
+
+		$articleUrl = $info[SiteInfoLookup::SITE_BASE_URL]
+			. $info[SiteInfoLookup::SITE_LINK_PATH];
+
+		$apiUrl = $info[SiteInfoLookup::SITE_BASE_URL]
+			. $info[SiteInfoLookup::SITE_SCRIPT_PATH]
+			. 'api.php';
+
+		$row = [
+			'iw_prefix' => $prefix,
+			'iw_url' => $articleUrl,
+			'iw_api' => $apiUrl,
+			'iw_wikiid' => $siteId,
+			'iw_trans' => $info[SiteInfoLookup::SITE_IS_TRANSCLUDABLE] ? 1 : 0,
+			'iw_local' => $info[SiteInfoLookup::SITE_IS_FORWARDABLE]  ? 1 : 0
+		];
+
+		return $row;
+	}
+
+	/**
+	 * See InterwikiLookup::getAllPrefixes
+	 *
+	 * @param string|null $local If set, limits output to local/non-local interwikis
+	 * @return array[] List of interwiki rows
+	 */
+	public function getAllPrefixes( $local = null ) {
+		// @var array $sites: maps local IDs to global site IDs
+		$sites = array_merge(
+			$this->siteLookup->getIdMap( SiteInfoLookup::INTERWIKI_ID ),
+			$this->siteLookup->getIdMap( SiteInfoLookup::NAVIGATION_ID )
+		);
+
+		if ( $local !== null ) {
+			// @var array $matching: a list of global site IDs
+			$matching = $this->siteLookup->listGroupMembers( SiteInfoLookup::SITE_IS_FORWARDABLE, $local, false );
+			$sites = array_intersect( $sites, $matching );
+		}
+
+		$rows = [];
+		foreach ( $sites as $prefix => $siteId ) {
+			$info = $this->siteLookup->getSiteInfo( $siteId );
+			$rows[] = $this->buildInterwikiRow( $siteId, $prefix, $info );
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * See InterwikiLookup::invalidateCache
+	 *
+	 * @param string $prefix
+	 */
+	public function invalidateCache( $prefix ) {
+		// noop
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSiteDefaults() {
+		return $this->defaults;
+	}
+
+	/**
+	 * @param array $defaults
+	 */
+	public function setSiteDefaults( $defaults ) {
+		$this->defaults = array_merge( $this->defaults, $defaults );
+	}
+
+}
---
+++ tests/phpunit/includes/interwiki/SiteInfoInterwikiLookupTest.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * @covers MediaWiki\Interwiki\SiteInfoInterwikiLookup
+ *
+ * @group MediaWiki
+ * @group Interwiki
+ */
+use MediaWiki\Interwiki\SiteInfoInterwikiLookup;
+use MediaWiki\Site\HashSiteInfoLookup;
+use MediaWiki\Site\SiteInfoLookup;
+
+class SiteInfoInterwikiLookupTest extends MediaWikiTestCase {
+
+	private function getSiteInfoLookup() {
+		$sites = [
+			'enwiki' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://en.wikipedia.org/',
+				SiteInfoLookup::SITE_IS_FORWARDABLE => true,
+			],
+			'enwiktionary' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://en.wiktionary.org/',
+				SiteInfoLookup::SITE_IS_FORWARDABLE => true,
+			],
+			'ruwiki' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://ru.wikipedia.org/',
+				SiteInfoLookup::SITE_IS_FORWARDABLE => true,
+			],
+			'ruwiktionary' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://ru.wiktionary.org/',
+				SiteInfoLookup::SITE_IS_FORWARDABLE => true,
+			],
+			'commonswiki' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://commons.wikimedia.org/',
+				SiteInfoLookup::SITE_IS_FORWARDABLE => true,
+				SiteInfoLookup::SITE_IS_TRANSCLUDABLE => true,
+			],
+			'acme' => [
+				SiteInfoLookup::SITE_BASE_URL => 'https://www.acme.test/',
+				SiteInfoLookup::SITE_LINK_PATH => 'page/$1',
+				SiteInfoLookup::SITE_SCRIPT_PATH => 'mw/',
+			],
+		];
+
+		$ids = [
+			// identifiers for interwiki links
+			SiteInfoLookup::INTERWIKI_ID => [
+				'acme' => 'acme',
+				'commons' => 'commonswiki',
+				'ru' => 'ruwiki',
+				'wiktionary' => 'enwiktionary',
+				'wikt' => 'enwiktionary',
+			],
+			// identifiers for navigation (language links)
+			SiteInfoLookup::NAVIGATION_ID => [
+				'ru' => 'ruwiki',
+				'a' => 'acme',
+			],
+			// another class of identifiers, to be ignored by testGetAllPrefixes
+			'Dummy' => [
+				'ruwi' => 'ruwiktionary',
+			]
+		];
+
+		return new HashSiteInfoLookup( $sites, $ids );
+	}
+
+	private function getInterwikiLookup() {
+		$lookup = new SiteInfoInterwikiLookup( $this->getSiteInfoLookup() );
+		$lookup->setSiteDefaults( [
+			SiteInfoLookup::SITE_LINK_PATH => 'wiki/$1',
+			SiteInfoLookup::SITE_SCRIPT_PATH => 'w/',
+		] );
+		return $lookup;
+	}
+
+	public function provideIsValidInterwiki() {
+		return [
+			'interwiki match' => [ 'commons', true ],
+			'navigation match' => [ 'a', true ],
+			'no match' => [ 'xyzzy', false ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideIsValidInterwiki
+	 */
+	public function testIsValidInterwiki( $prefix, $expected ) {
+		$lookup = $this->getInterwikiLookup();
+
+		$this->assertSame( $expected, $lookup->isValidInterwiki( $prefix ) );
+	}
+
+	private $expectedInterwikiRows = [
+		'commons' => [
+			'iw_prefix' => 'commons',
+			'iw_url' => 'https://commons.wikimedia.org/wiki/$1',
+			'iw_api' => 'https://commons.wikimedia.org/w/api.php',
+			'iw_wikiid' => 'commonswiki',
+			'iw_local' => 1,
+			'iw_trans' => 1,
+		],
+
+		'ru' => [
+			'iw_prefix' => 'ru',
+			'iw_url' => 'https://ru.wikipedia.org/wiki/$1',
+			'iw_api' => 'https://ru.wikipedia.org/w/api.php',
+			'iw_wikiid' => 'ruwiki',
+			'iw_local' => 1,
+			'iw_trans' => 0,
+		],
+
+		'wiktionary' => [
+			'iw_prefix' => 'wiktionary',
+			'iw_url' => 'https://en.wiktionary.org/wiki/$1',
+			'iw_api' => 'https://en.wiktionary.org/w/api.php',
+			'iw_wikiid' => 'enwiktionary',
+			'iw_local' => 1,
+			'iw_trans' => 0,
+		],
+
+		'wikt' => [
+			'iw_prefix' => 'wikt',
+			'iw_url' => 'https://en.wiktionary.org/wiki/$1',
+			'iw_api' => 'https://en.wiktionary.org/w/api.php',
+			'iw_wikiid' => 'enwiktionary',
+			'iw_local' => 1,
+			'iw_trans' => 0,
+		],
+
+		'a' => [
+			'iw_prefix' => 'a',
+			'iw_url' => 'https://www.acme.test/page/$1',
+			'iw_api' => 'https://www.acme.test/mw/api.php',
+			'iw_wikiid' => 'acme',
+			'iw_local' => 0,
+			'iw_trans' => 0
+		],
+
+		'acme' => [
+			'iw_prefix' => 'acme',
+			'iw_url' => 'https://www.acme.test/page/$1',
+			'iw_api' => 'https://www.acme.test/mw/api.php',
+			'iw_wikiid' => 'acme',
+			'iw_local' => 0,
+			'iw_trans' => 0
+		],
+
+	];
+
+	/**
+	 * @param string[] $prefixes
+	 *
+	 * return array[]
+	 */
+	private function getInterwikiRows( $prefixes ) {
+		$rows = [];
+
+		foreach ( $prefixes as $prefix ) {
+			$rows[] = $this->expectedInterwikiRows[$prefix];
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * @param string $prefix
+	 *
+	 * @return bool|Interwiki
+	 */
+	private function getInterwiki( $prefix ) {
+		if ( !isset( $this->expectedInterwikiRows[$prefix] ) ) {
+			return false;
+		}
+
+		$row = $this->expectedInterwikiRows[$prefix];
+
+		return new Interwiki(
+			$row['iw_prefix'],
+			$row['iw_url'],
+			$row['iw_api'],
+			$row['iw_wikiid'],
+			$row['iw_local'],
+			$row['iw_trans']
+		);
+	}
+
+	public function provideFetch() {
+		return [
+			'commons' => [ 'commons', $this->getInterwiki( 'commons' ) ],
+			'ru' => [ 'ru', $this->getInterwiki( 'ru' ) ],
+			'a' => [ 'a', $this->getInterwiki( 'a' ) ],
+			'no match' => [ 'xyzzy', false ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideFetch
+	 */
+	public function testFetch( $prefix, $expected ) {
+		$lookup = $this->getInterwikiLookup();
+
+		$this->assertEquals( $expected, $lookup->fetch( $prefix ) );
+	}
+
+	public function testGetAllPrefixes() {
+		$lookup = $this->getInterwikiLookup();
+
+		$expected = $this->getInterwikiRows( [ 'wiktionary', 'acme', 'commons', 'ru', 'wikt', 'a' ] );
+
+		$this->assertSameRows( $expected, $lookup->getAllPrefixes(), 'iw_prefix' );
+	}
+
+	public function testGetAllPrefixes_local() {
+		$lookup = $this->getInterwikiLookup();
+
+		$expected = $this->getInterwikiRows( [ 'ru', 'commons', 'wiktionary', 'wikt' ] );
+
+		$this->assertSameRows( $expected, $lookup->getAllPrefixes( true ), 'iw_prefix' );
+	}
+
+	public function testGetAllPrefixes_nonlocal() {
+		$lookup = $this->getInterwikiLookup();
+
+		$expected = $this->getInterwikiRows( [ 'a', 'acme' ] );
+
+		$this->assertSameRows( $expected, $lookup->getAllPrefixes( false ), 'iw_prefix' );
+	}
+
+	public function testSetSiteDefaults() {
+		$lookup = $this->getInterwikiLookup();
+
+		$lookup->setSiteDefaults( [
+			SiteInfoLookup::SITE_LINK_PATH => 'LINK/$1',
+			SiteInfoLookup::SITE_SCRIPT_PATH => 'SCRIPT/',
+			SiteInfoLookup::SITE_IS_TRANSCLUDABLE => true,
+		] );
+
+		$defaults = [
+			SiteInfoLookup::SITE_BASE_URL => '',
+			SiteInfoLookup::SITE_LINK_PATH => 'LINK/$1',
+			SiteInfoLookup::SITE_SCRIPT_PATH => 'SCRIPT/',
+			SiteInfoLookup::SITE_IS_FORWARDABLE => false,
+			SiteInfoLookup::SITE_IS_TRANSCLUDABLE => true,
+		];
+
+		$this->assertEquals( $defaults, $lookup->getSiteDefaults() );
+
+		$expected = new Interwiki(
+			'ru',
+			'https://ru.wikipedia.org/LINK/$1',
+			'https://ru.wikipedia.org/SCRIPT/api.php',
+			'ruwiki',
+			1,
+			1
+		);
+
+		$this->assertEquals( $expected, $lookup->fetch( 'ru' ) );
+	}
+
+	private function assertSameRows( $expected, $actual, $key ) {
+		$expected = $this->keyByField( $expected, $key );
+		$actual = $this->keyByField( $actual, $key );
+
+		ksort( $expected );
+		ksort( $actual );
+
+		$missingKeys = array_diff( array_keys( $expected ), array_keys( $actual ) );
+		$extraKeys = array_diff( array_keys( $actual ), array_keys( $expected ) );
+
+		$this->assertEquals( $missingKeys, $extraKeys, "Prefixes" );
+
+		foreach ( $expected as $key => $expRow ) {
+			$actRow = $actual[$key];
+
+			ksort( $expRow );
+			ksort( $actRow );
+
+			$this->assertEquals( $expRow, $actRow, "Fields for prefix $key" );
+		}
+	}
+
+	/**
+	 * @param array[] $listOfRows
+	 * @param string $field
+	 *
+	 * @return array[]
+	 */
+	private function keyByField( array $listOfRows, $field ) {
+		$result = [];
+
+		foreach ( $listOfRows as $row ) {
+			$key = $row[$field];
+			$result[$key] = $row;
+		}
+
+		return $result;
+	}
+
+}
