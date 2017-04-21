b83d7846d5ab4d199def641e2c408111089cc71e
daniel <daniel.kinzler@wikimedia.de>
2017-04-21 22:19:57 +0200

    Introduce SiteInfoLookup and adapters.
    Change-Id: I0a795351ae7741c9cb15ed38057a3cb07703d34e

---
+++ includes/site/FileSiteInfoLookup.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace MediaWiki\Site;
+
+use InvalidArgumentException;
+use OutOfBoundsException;
+
+/**
+ * FIXME
+ *
+ * @since 1.29
+ *
+ * @file
+ * @ingroup Site
+ *
+ * @license GNU GPL v2+
+ */
+class FileSiteInfoLookup extends HashSiteInfoLookup {
+
+	/**
+	 * @var array[]
+	 */
+	private $files;
+
+	/**
+	 * @var bool
+	 */
+	private $loaded = false;
+
+	/**
+	 * HashSiteInfoLookup constructor.
+	 *
+	 * @param array[] $files
+	 */
+	public function __construct( array $files ) {
+		parent::__construct( [], [] );
+
+		// FIXME: assert
+		$this->files = $files;
+	}
+
+	private function load() {
+		if ( $this->loaded ) {
+			return;
+		}
+
+		$data = [];
+		foreach ( $this->files as $file ) {
+			$data = array_merge_recursive( $data, $this->loadFile( $file ) );
+		}
+
+		$this->sites = $data['sites']; // FIXME: check existance
+		$this->ids = $data['ids']; // FIXME: check existance
+
+		$this->loaded = true;
+	}
+
+	/**
+	 * @param string $path
+	 * @param string $suffix
+	 *
+	 * @return bool
+	 */
+	private function hasSuffix( $path, $suffix ) {
+		return substr_compare( $path, $suffix, -strlen( $suffix ) ) === 0;
+	}
+
+	/**
+	 * @param string $path
+	 *
+	 * @return array
+	 */
+	private function loadFile( $path ) {
+		if ( $this->hasSuffix( $path, '.php' ) ) {
+			$data = include( $path ); // FIXME: check return value
+		} elseif ( $this->hasSuffix( $path, '.json' ) ) {
+			$json = file_get_contents( $path ); // FIXME: check return value
+			$data = json_decode( $json ); // FIXME: check return value
+		} else {
+			// FIXME: IAE is a pretty hard failure mode for a configuration error. Be nicer.
+			throw new InvalidArgumentException( 'Unsupported file type: ' . $path );
+		}
+
+		// FIXME: check structure??
+		return $data;
+	}
+
+	/**
+	 * @return array[]
+	 */
+	protected function getSitesArray() {
+		$this->load();
+		return parent::getSitesArray();
+	}
+
+	/**
+	 * @return array[]
+	 */
+	protected function getIdArray() {
+		$this->load();
+		return parent::getIdArray();
+	}
+
+}
