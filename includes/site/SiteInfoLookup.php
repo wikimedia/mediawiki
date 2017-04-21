a834c6567b271563187a3091de6e09cdb9eb3d52
daniel <daniel.kinzler@wikimedia.de>
2017-04-21 22:19:57 +0200

    Introduce SiteInfoLookup and adapters.
    Change-Id: I0a795351ae7741c9cb15ed38057a3cb07703d34e

---
+++ includes/site/SiteInfoLookup.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace MediaWiki\Site;
+
+use OutOfBoundsException;
+
+/**
+ * FIXME overview -> docs/sites.txt
+ * FIXME scopes, local and global ids
+ * FIXME groups
+ * FIXME aliases
+ *
+ * @since 1.29
+ *
+ * @file
+ * @ingroup Site
+ *
+ * @license GNU GPL v2+
+ */
+interface SiteInfoLookup {
+
+	// FIXME: document all the semantics!
+	const TYPE_UNKNOWN = 'unknown';
+	const TYPE_MEDIAWIKI = 'mediawiki';
+
+	// FIXME: document all the semantics!
+	const SITE_TYPE = 'type';
+	const SITE_NAME = 'name';
+	const SITE_LANGUAGE = 'language';
+	const SITE_FAMILY = 'family';
+	const SITE_DB_NAME = 'db-name';
+	const SITE_DB_CLUSTER = 'db-cluster';
+	const SITE_IS_LOCAL = 'local';
+	const SITE_IS_TRANSCLUDABLE = 'transcludable';
+	const SITE_BASE_URL = 'base-url';
+	const SITE_SCRIPT_PATH = 'script-path';
+	const SITE_LINK_PATH = 'link-path';
+
+	// FIXME: document all the semantics!
+	const ALIAS_ID = 'alias';
+	const INTERWIKI_ID = 'interwiki';
+	const NAVIGATION_ID = 'navigation';
+	const DATABASE_ID = 'database';
+	const DOMAIN_ID = 'domain';
+
+	/**
+	 * Returns the info array for the given site.
+	 *
+	 * @param string $siteId The global ID of the desired site. Aliasing applies.
+	 *
+	 * @param array $defaults Defaults to apply to the result.
+	 *
+	 * @throws OutOfBoundsException if $siteId is not a known site
+	 *
+	 * @return array An associative array representing properties of the given site.
+	 *         See SITE_XXX for well known keys.
+	 *
+	 */
+	public function getSiteInfo( $siteId, array $defaults = [] );
+
+	/**
+	 * List all known sites.
+	 *
+	 * @return string[] The site IDs of all known sites.
+	 */
+	public function listSites();
+
+	/**
+	 * List all sites with the given value for the given field in the info array.
+	 *
+	 * @param string $field A field name in the site's info array.
+	 * @param mixed $value The expected value in the site's info array.
+	 * @param mixed $default The value to assume when $field is not set.
+	 *
+	 * @return string[] The site IDs of the sites with the given property value.
+	 */
+	public function listGroupMembers( $field, $value, $default = null );
+
+	/**
+	 * Returns the local IDs that refer to a site in different scopes.
+	 *
+	 * @deprecated This method is born deprecated, it only exists for backwards compatibility
+	 *             reasons. There should be no need to list the local IDs of a site.
+	 *             Use getLocalId() instead.
+	 *
+	 * @param string $siteId The global ID of the desired site. Aliasing applies.
+	 *
+	 * @throws OutOfBoundsException if $siteId is not a known site
+	 *
+	 * @return array[] Associative array giving all identifiers of the given site.
+	 *         Keys represent the scope in which an id is valid, see the XXX_ID constants
+	 *         for well known keys. Values are lists of identifiers, the first entry being the
+	 *         preferred id in a given scope.
+	 */
+	public function getAssociatedLocalIds( $siteId );
+
+	/**
+	 * Returns the local ID that refer to a site in the given scope.
+	 *
+	 * @param string $siteId The global ID of the desired site. Aliasing applies.
+	 * @param string $scope The scope in which to resolve the ID. See XXX_ID for well known
+	 *        values. See class level documentation for more information.
+	 *
+	 * @throws OutOfBoundsException if $siteId is not a known site
+	 *
+	 * @return string|null The site's preferred local ID in the given scope, or null if
+	 *         no local id is defined for $siteId in $scope.
+	 */
+	public function getLocalId( $siteId, $scope );
+
+	/**
+	 * Returns the global site ID of the site identified by the given local ID.
+	 *
+	 * @param string $localId The ID to resolve in the given scopes.
+	 * @param string[]... Additional parameters define the scopes to try to resolve $localId in.
+	 *        If no scopes are given, all scopes will be tried.
+	 *
+	 * @return string|null The site's global site ID, or null if no such site is known.
+	 */
+	public function getSiteId( $localId );
+
+	/**
+	 * Returns the mapping of all local IDs defined in the given scope.
+	 *
+	 * @param string $scope The scope in which to resolve the ID. See XXX_ID for well known
+	 *        values. See class level documentation for more information.
+	 *
+	 * @return array[] An associative array giving the ID resolution for the given scope.
+	 *         Keys are local identifiers valid in the scope, values are global site IDs.
+	 */
+	public function getIdMap( $scope );
+
+}
