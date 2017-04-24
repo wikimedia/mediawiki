91e49be2a789f0813e0af01a98d2ede657d3369c
Ed Sanders <esanders@wikimedia.org>
2017-04-24 19:01:30 +0100

    OOUI-ificiation of TablePager nav
    Depends on next OOUI release for new icons.
    
    Bug: T163715
    Change-Id: Ia750131aae52c35181209277c5b951a447db1b5a

--- includes/pager/TablePager.php
+++ includes/pager/TablePager.php
@@ -293,35 +293,36 @@ abstract class TablePager extends IndexPager {
 			return '';
 		}
 
-		$labels = [
-			'first' => 'table_pager_first',
-			'prev' => 'table_pager_prev',
-			'next' => 'table_pager_next',
-			'last' => 'table_pager_last',
-		];
+		$this->getOutput()->enableOOUI();
 
-		$linkTexts = [];
-		$disabledTexts = [];
-		foreach ( $labels as $type => $label ) {
-			$msgLabel = $this->msg( $label )->escaped();
-			$linkTexts[$type] = "<div class='TablePager_nav-enabled'>$msgLabel</div>";
-			$disabledTexts[$type] = "<div class='TablePager_nav-disabled'>$msgLabel</div>";
-		}
-		$links = $this->getPagingLinks( $linkTexts, $disabledTexts );
+		$types = [ 'first', 'prev', 'next', 'last' ];
 
-		$s = Html::openElement( 'table', [ 'class' => $this->getNavClass() ] );
-		$s .= Html::openElement( 'tr' ) . "\n";
-		$width = 100 / count( $links ) . '%';
-		foreach ( $labels as $type => $label ) {
-			// We want every cell to have the same width. We could use table-layout: fixed; in CSS,
-			// but it only works if we specify the width of a cell or the table and we don't want to.
-			// There is no better way. <https://www.w3.org/TR/CSS2/tables.html#fixed-table-layout>
-			$s .= Html::rawElement( 'td',
-				[ 'style' => "width: $width;", 'class' => "TablePager_nav-$type" ],
-				$links[$type] ) . "\n";
+		$queries = $this->getPagingQueries();
+		$links = [];
+
+		$buttons = [];
+
+		$title = $this->getTitle();
+
+		foreach ( $types as $type ) {
+			$buttons[] = new \OOUI\ButtonWidget( [
+				// Messages used here:
+				// * table_pager_first
+				// * table_pager_prev
+				// * table_pager_next
+				// * table_pager_last
+				'label' => $this->msg( 'table_pager_' . $type )->text(),
+				'href' => $queries[ $type ] ?
+					$title->getLinkURL( $queries[ $type ] + $this->getDefaultQuery() ) :
+					null,
+				'icon' => $type === 'prev' ? 'previous' : $type,
+				'disabled' => $queries[ $type ] === false
+			] );
 		}
-		$s .= Html::closeElement( 'tr' ) . Html::closeElement( 'table' ) . "\n";
-		return $s;
+		return new \OOUI\ButtonGroupWidget( [
+			'classes' => [ $this->getNavClass() ],
+			'items' => $buttons,
+		] );
 	}
 
 	/**
@@ -330,7 +331,7 @@ abstract class TablePager extends IndexPager {
 	 * @return string[]
 	 */
 	public function getModuleStyles() {
-		return [ 'mediawiki.pager.tablePager' ];
+		return [ 'mediawiki.pager.tablePager', 'oojs-ui.styles.icons-movement' ];
 	}
 
 	/**
