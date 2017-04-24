92e506952ebeef55f214bb4eaa34c2d1a0a70655
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-04-24 12:39:38 +0300

    Add Dinka (din) to Names.php and languages/messagess/
    Bug: T163600
    Change-Id: I2bf03c97680c17cddf48d05adee88581640268ec

---
+++ languages/messages/MessagesDin.php
@@ -0,0 +1,29 @@
+<?php
+/** Dinka (Thuɔŋjäŋ)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$namespaceNames = [
+	NS_SPECIAL          => 'Këcëweek',
+	NS_TALK             => 'Jam',
+	NS_USER             => 'Dulooi',
+	NS_USER_TALK        => 'Jam_kekë_dulooi',
+	NS_PROJECT_TALK     => 'Jam_në_biäk_$1',
+	NS_FILE             => 'Apamduööt',
+	NS_FILE_TALK        => 'Jam_në_biäk_apamduööt',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Jam_në_biäk_MediaWiki',
+	NS_TEMPLATE         => 'Macuëc',
+	NS_TEMPLATE_TALK    => 'Jam_në_biäk_macuëc',
+	NS_HELP             => 'Akuny',
+	NS_HELP_TALK        => 'Jam_në_biäk_akuny',
+	NS_CATEGORY         => 'Bekätakthook',
+	NS_CATEGORY_TALK    => 'Jam_në_biäk_bekätakthook',
+];
+
+$linkTrail = '/^([äëɛɛ̈éɣïŋöɔɔ̈óa-z]+)(.*)$/sDu';
