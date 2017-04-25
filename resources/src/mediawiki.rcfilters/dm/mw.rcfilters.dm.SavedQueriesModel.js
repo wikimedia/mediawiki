b77c23903da20a54dcd95e17f51e555795d6f37d
Moriel Schottlender <moriel@gmail.com>
2017-04-25 16:59:50 -0700

    [wip] Add a 'saved queries' quick filters feature
    Change-Id: I5cede87633147736d3b4ee5b8ea178ae21bd441f

---
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.SavedQueriesModel.js
@@ -0,0 +1,154 @@
+( function ( mw, $ ) {
+	/**
+	 * View mdel for saved queries
+	 *
+	 * @mixins OO.EventEmitter
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 */
+	mw.rcfilters.dm.SavedQueriesModel = function MwRcfiltersDmSavedQueriesModel( config ) {
+		config = config || {};
+
+		// Mixin constructor
+		OO.EventEmitter.call( this );
+		OO.EmitterList.call( this );
+
+		this.default = null;
+
+		// Events
+		this.aggregate( { update: 'queryItemUpdate' } );
+		this.connect( this, { queryItemUpdate: [ 'emit', 'itemUpdate' ] } );
+	};
+
+	/* Initialization */
+
+	OO.initClass( mw.rcfilters.dm.SavedQueriesModel );
+	OO.mixinClass( mw.rcfilters.dm.SavedQueriesModel, OO.EventEmitter );
+	OO.mixinClass( mw.rcfilters.dm.SavedQueriesModel, OO.EmitterList );
+
+	/* Events */
+
+	/**
+	 * @event initialize
+	 *
+	 * Model is initialized
+	 */
+
+	/**
+	 * @event itemUpdate
+	 * @param {mw.rcfilters.dm.SavedQueryItemModel} Changed item
+	 *
+	 * An item has changed
+	 */
+
+	/* Methods */
+
+	/**
+	 * Initialize the saved queries model by reading it from the user's settings.
+	 * The structure of the saved queries is:
+	 * {
+	 *    default: (string) Query ID
+	 *    queries:{
+	 *       query_id_1: {
+	 *          data:{
+	 *             filters: (Object) Minimal definition of the filters
+	 *             highlights: (Object) Definition of the highlights
+	 *          },
+	 *          label: (optional) Name of this query
+	 *       }
+	 *    }
+	 * }
+	 *
+	 * @fires initialize
+	 */
+	mw.rcfilters.dm.SavedQueriesModel.prototype.initialize = function ( savedQueries, normalizationFunc ) {
+		var baseFilterList = {},
+			items = [];
+
+		savedQueries = savedQueries || {};
+		normalizationFunc = normalizationFunc || function ( data ) { return data; };
+
+		this.clearItems();
+		$.each( savedQueries.queries, function ( id, obj ) {
+			var data = obj.data,
+				normalizedData = normalizationFunc( obj.data );
+
+			items.push(
+				new mw.rcfilters.dm.SavedQueryItemModel(
+					id,
+					obj.label,
+					normalizedData,
+					{ default: savedQueries.default === id }
+				)
+			);
+		} );
+
+		this.default = savedQueries.default;
+
+		this.addItems( items );
+
+		this.emit( 'initialize' );
+	};
+
+	/**
+	 * Get query by its identifier
+	 *
+	 * @param {string} queryID Query identifier
+	 * @return {mw.rcfilters.dm.SavedQueryItemModel|undefined} Item matching
+	 *  the search. Undefined if not found.
+	 */
+	mw.rcfilters.dm.SavedQueriesModel.prototype.getItemByID = function ( queryID ) {
+		return this.getItems().filter( function ( item ) {
+			return item.getID() === queryID;
+		} )[ 0 ];
+	};
+
+	/**
+	 * Get the object representing the state of the entire model and items
+	 *
+	 * @return {Object} Object representing the state of the model and items
+	 */
+	mw.rcfilters.dm.SavedQueriesModel.prototype.getState = function () {
+		var obj = { queries: {} };
+
+		// Translate the items to the saved object
+		this.getItems().forEach( function ( item ) {
+			var itemState = item.getState();
+
+			obj.queries[ item.getID() ] = itemState;
+		} );
+
+		if ( this.getDefault() ) {
+			obj.default = this.getDefault();
+		}
+
+		return obj;
+	};
+
+	/**
+	 * Set a default query. Null to unset default.
+	 *
+	 * @param {string} itemID Query identifier
+	 * @fires default
+	 */
+	mw.rcfilters.dm.SavedQueriesModel.prototype.setDefault = function ( itemID ) {
+		if ( this.default !== itemID ) {
+			this.default = itemID;
+
+			// Set for individual itens
+			this.getItems().forEach( function ( item ) {
+				item.toggleDefault( item.getID() === itemID );
+			} );
+		}
+	};
+
+	/**
+	 * Get the default query ID
+	 *
+	 * @return {string} Default query identifier
+	 */
+	mw.rcfilters.dm.SavedQueriesModel.prototype.getDefault = function () {
+		return this.default;
+	};
+}( mediaWiki, jQuery ) );
---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.QuickLinksWidget.js
@@ -0,0 +1,108 @@
+( function ( mw ) {
+	/**
+	 * Quick links widget
+	 *
+	 * @extends OO.ui.Widget
+	 * @mixins OO.ui.mixin.PendingElement
+	 *
+	 * @constructor
+	 * @param {mw.rcfilters.Controller} controller Controller
+	 * @param {mw.rcfilters.dm.SavedQueriesModel} model View model
+	 * @param {Object} [config] Configuration object
+	 * @cfg {jQuery} [$overlay] A jQuery object serving as overlay for popups
+	 */
+	mw.rcfilters.ui.QuickLinksWidget = function MwRcfiltersUiQuickLinksWidget( controller, model, config ) {
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.ui.QuickLinksWidget.parent.call( this, config );
+
+		this.controller = controller;
+		this.model = model;
+		this.$overlay = config.$overlay || this.$element;
+
+		this.menu = new OO.ui.ButtonGroupWidget( {
+			classes: [ 'mw-rcfilters-ui-quickLinksWidget-menu' ]
+		} );
+		this.button = new OO.ui.PopupButtonWidget( {
+			classes: [ 'mw-rcfilters-ui-quickLinksWidget-button' ],
+			label: mw.msg( 'rcfilters-quickfilters' ),
+			icon: 'unClip',
+			$overlay: this.$overlay,
+			popup: {
+				width: 250,
+				anchor: false,
+				align: 'forwards',
+				$autoCloseIgnore: this.$overlay,
+				$content: this.menu.$element
+			}
+		} );
+
+		this.menu.aggregate( {
+			click: 'menuItemClick',
+			delete: 'menuItemDelete',
+			default: 'menuItemDefault',
+			edit: 'menuItemEdit'
+		} );
+
+		// Events
+		// this.button.connect( this, { click: 'onButtonClick' } );
+		this.model.connect( this, {
+			add: 'onModelAddItem',
+			remove: 'onModelRemoveItem'
+		} );
+		this.menu.connect( this, {
+			menuItemClick: 'onMenuItemClick',
+			menuItemDelete: 'onMenuItemRemove',
+			menuItemDefault: 'onMenuItemDefault',
+			menuItemEdit: 'onMenuItemEdit'
+		} );
+
+		this.button.toggle( !this.menu.isEmpty() );
+		// Initialize
+		this.$element
+			.addClass( 'mw-rcfilters-ui-quickLinksWidget' )
+			.append( this.button.$element );
+
+	};
+
+	/* Initialization */
+	OO.inheritClass( mw.rcfilters.ui.QuickLinksWidget, OO.ui.Widget );
+
+	// mw.rcfilters.ui.QuickLinksWidget.prototype.onButtonClick = function () {
+	// 	this.button.popup.toggle( true );
+	// };
+
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onMenuItemClick = function ( item ) {
+		this.controller.loadSavedQuery( item.getID() );
+		this.button.popup.toggle( false );
+	};
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onMenuItemRemove = function ( item ) {
+		this.controller.removeSavedQuery( item.getID() );
+		this.menu.removeItems( [ item ] );
+	};
+
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onMenuItemDefault = function ( item, isDefault ) {
+		this.controller.setDefaultSavedQuery( isDefault ? item.getID() : null );
+	};
+
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onMenuItemEdit = function ( item, newLabel ) {
+		this.controller.renameSavedQuery( item.getID(), newLabel );
+	};
+
+
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onModelAddItem = function ( item ) {
+		if ( this.menu.getItemFromData( item.getID() ) ) {
+			return;
+		}
+
+		this.menu.addItems( [
+			new mw.rcfilters.ui.QuickLinkMenuOptionWidget( item, { $overlay: this.$overlay } )
+		] );
+		this.button.toggle( !this.menu.isEmpty() );
+	};
+	mw.rcfilters.ui.QuickLinksWidget.prototype.onModelRemoveItem = function ( item ) {
+		this.menu.removeItems( [ this.model.getItemByID( item.getID() ) ] );
+		this.button.toggle( !this.menu.isEmpty() );
+	};
+}( mediaWiki ) );
