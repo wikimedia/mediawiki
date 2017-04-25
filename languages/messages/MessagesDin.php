b3f7f4e1e2bb7dec0d58d5459627f2b4fc13a3aa
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-04-25 10:52:51 +0300

    Correct the "Help", "Media" and talk namespace names for Dinka
    The corrections come from users Dinkawiki and Mawanmuortat,
    who did most of the translation into this language.
    
    No aliases are needed, because the file was only created recently,
    and not yet used anywhere.
    
    Change-Id: Ic1ddfaca403e05e5971c516d6e7a454311794f3c

--- languages/messages/MessagesDin.php
+++ languages/messages/MessagesDin.php
@@ -9,21 +9,22 @@
  */
 
 $namespaceNames = [
+	NS_MEDIA            => 'Ciɛl',
 	NS_SPECIAL          => 'Këcëweek',
 	NS_TALK             => 'Jam',
 	NS_USER             => 'Dulooi',
 	NS_USER_TALK        => 'Jam_kekë_dulooi',
-	NS_PROJECT_TALK     => 'Jam_në_biäk_$1',
+	NS_PROJECT_TALK     => 'Jam_wɛ̈t_ë_$1',
 	NS_FILE             => 'Apamduööt',
-	NS_FILE_TALK        => 'Jam_në_biäk_apamduööt',
+	NS_FILE_TALK        => 'Jam_wɛ̈t_ë_apamduööt',
 	NS_MEDIAWIKI        => 'MediaWiki',
-	NS_MEDIAWIKI_TALK   => 'Jam_në_biäk_MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Jam_wɛ̈t_ë_MediaWiki',
 	NS_TEMPLATE         => 'Macuëc',
-	NS_TEMPLATE_TALK    => 'Jam_në_biäk_macuëc',
-	NS_HELP             => 'Akuny',
-	NS_HELP_TALK        => 'Jam_në_biäk_akuny',
+	NS_TEMPLATE_TALK    => 'Jam_wɛ̈t_ë_macuëc',
+	NS_HELP             => 'Kuɔny',
+	NS_HELP_TALK        => 'Jam_wɛ̈t_ë_kuɔny',
 	NS_CATEGORY         => 'Bekätakthook',
-	NS_CATEGORY_TALK    => 'Jam_në_biäk_bekätakthook',
+	NS_CATEGORY_TALK    => 'Jam_wɛ̈t_ë_bekätakthook',
 ];
 
 $linkTrail = '/^([äëɛɛ̈éɣïŋöɔɔ̈óa-z]+)(.*)$/sDu';
