16e8b045535dd29d16f413755af4b0a60f93283c
Moriel Schottlender <moriel@gmail.com>
2017-04-25 16:59:50 -0700

    RCFilters UI: Add a 'saved queries' quick filters feature
    Bug: T151994
    Bug: T164128
    Change-Id: I5cede87633147736d3b4ee5b8ea178ae21bd441f

---
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.SavedQueryItemModel.js
@@ -0,0 +1,115 @@
+( function ( mw ) {
+	/**
+	 * View model for a single saved query
+	 *
+	 * @mixins OO.EventEmitter
+	 *
+	 * @constructor
+	 * @param {string} id Unique identifier
+	 * @param {string} label Saved query label
+	 * @param {Object} data Saved query data
+	 * @param {Object} [config] Configuration options
+	 * @param {boolean} [default] This item is the default
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel = function MwRcfiltersDmSavedQueriesModel( id, label, data, config ) {
+		config = config || {};
+
+		// Mixin constructor
+		OO.EventEmitter.call( this );
+
+		this.id = id;
+		this.label = label;
+		this.data = data;
+		this.default = !!config.default;
+	};
+
+	/* Initialization */
+
+	OO.initClass( mw.rcfilters.dm.SavedQueryItemModel );
+	OO.mixinClass( mw.rcfilters.dm.SavedQueryItemModel, OO.EventEmitter );
+
+	/* Events */
+
+	/**
+	 * @update
+	 *
+	 * Model has been updated
+	 */
+
+	/* Methods */
+
+	/**
+	 * Get an object representing the state of this item
+	 *
+	 * @returns {Object} Object representing the current data state
+	 *  of the object
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.getState = function () {
+		return {
+			data: this.getData(),
+			label: this.getLabel()
+		};
+	};
+
+	/**
+	 * Get the query's identifier
+	 *
+	 * @return {string} Query identifier
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.getID = function () {
+		return this.id;
+	};
+
+	/**
+	 * Get query label
+	 *
+	 * @return {label} Query label
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.getLabel = function () {
+		return this.label;
+	};
+
+	/**
+	 * Update the query label
+	 *
+	 * @param {string} newLabel New label
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.updateLabel = function ( newLabel ) {
+		if ( newLabel && this.label !== newLabel ) {
+			this.label = newLabel;
+			this.emit( 'update' );
+		}
+	};
+
+	/**
+	 * Get query data
+	 *
+	 * @return {Object} Object representing parameter and highlight data
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.getData = function () {
+		return this.data;
+	};
+
+	/**
+	 * Check whether this item is the default
+	 *
+	 * @return {boolean} Query is set to be default
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.isDefault = function () {
+		return this.default;
+	};
+
+	/**
+	 * Toggle the default state of this query item
+	 *
+	 * @param {boolean} isDefault Query is default
+	 */
+	mw.rcfilters.dm.SavedQueryItemModel.prototype.toggleDefault = function ( isDefault ) {
+		isDefault = isDefault === undefined ? !this.default : isDefault;
+
+		if ( this.default !== isDefault ) {
+			this.default = isDefault;
+			this.emit( 'update' );
+		}
+	};
+}( mediaWiki ) );
---
+++ resources/src/mediawiki.rcfilters/ui/mw.rcfilters.ui.SavedLinksListItemWidget.js
@@ -0,0 +1,277 @@
+( function ( mw ) {
+	/**
+	 * Quick links menu option widget
+	 *
+	 * @extends OO.ui.Widget
+	 * @mixins OO.ui.mixin.LabelElement
+	 * @mixins OO.ui.mixin.IconElement
+	 *
+	 * @constructor
+	 * @param {mw.rcfilters.dm.SavedQueryItemModel} model View model
+	 * @param {Object} [config] Configuration object
+	 * @cfg {jQuery} [$overlay] A jQuery object serving as overlay for popups
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget = function MwRcfiltersUiSavedLinksListWidget( model, config ) {
+		config = config || {};
+
+		this.model = model;
+
+		// Parent
+		mw.rcfilters.ui.SavedLinksListItemWidget.parent.call( this, $.extend( {
+			data: this.model.getID()
+		}, config ) );
+
+		// Mixin constructors
+		OO.ui.mixin.LabelElement.call( this, $.extend( {
+			label: this.model.getLabel()
+		}, config ) );
+		OO.ui.mixin.IconElement.call( this, $.extend( {
+			icon: ''
+		}, config ) );
+
+		this.edit = false;
+		this.$overlay = config.$overlay || this.$element;
+
+		this.popupButton = new OO.ui.ButtonWidget( {
+			classes: [ 'mw-rcfilters-ui-savedLinksListItemWidget-button' ],
+			icon: 'ellipsis',
+			framed: false
+		} );
+		this.menu = new OO.ui.FloatingMenuSelectWidget( {
+			classes: [ 'mw-rcfilters-ui-savedLinksListItemWidget-menu' ],
+			widget: this.popupButton,
+			width: 200,
+			horizontalPosition: 'end',
+			$container: this.popupButton.$element,
+			items: [
+				new OO.ui.MenuOptionWidget( {
+					data: 'edit',
+					icon: 'edit',
+					label: mw.msg( 'rcfilters-savedqueries-rename' )
+				} ),
+				new OO.ui.MenuOptionWidget( {
+					data: 'delete',
+					icon: 'close',
+					label: mw.msg( 'rcfilters-savedqueries-remove' )
+				} ),
+				new OO.ui.MenuOptionWidget( {
+					data: 'default',
+					icon: 'pushPin',
+					label: mw.msg( 'rcfilters-savedqueries-setdefault' )
+				} )
+			]
+		} );
+
+		this.editInput = new OO.ui.TextInputWidget( {
+			classes: [ 'mw-rcfilters-ui-savedLinksListItemWidget-input' ]
+		} );
+		this.saveButton = new OO.ui.ButtonWidget( {
+			icon: 'check',
+			flags: [ 'primary', 'progressive' ]
+		} );
+		this.toggleEdit( false );
+
+		// Events
+		this.model.connect( this, { update: 'onModelUpdate' } );
+		this.popupButton.connect( this, { click: 'onPopupButtonClick' } );
+		this.menu.connect( this, {
+			choose: 'onMenuChoose'
+		} );
+		this.saveButton.connect( this, { click: 'onSaveButtonClick' } );
+		this.editInput.connect( this, { enter: 'onEditInputEnter' } );
+		this.editInput.$input.on( {
+			blur: this.onInputBlur.bind( this ),
+			keyup: this.onInputKeyup.bind( this )
+		} );
+		this.$element.on( { click: this.emit.bind( this, 'click' ) } );
+		this.$label.on( { click: this.emit.bind( this, 'click' ) } );
+
+		// Initialize
+		this.toggleDefault( !!this.model.isDefault() );
+		this.$overlay.append( this.menu.$element );
+		this.$element
+			.addClass( 'mw-rcfilters-ui-savedLinksListItemWidget' )
+			.addClass( 'mw-rcfilters-ui-savedLinksListItemWidget-query-' + this.model.getID() )
+			.append(
+				$( '<div>' )
+					.addClass( 'mw-rcfilters-ui-table' )
+					.append(
+						$( '<div>' )
+							.addClass( 'mw-rcfilters-ui-row' )
+							.append(
+								$( '<div>' )
+									.addClass( 'mw-rcfilters-ui-cell' )
+									.addClass( 'mw-rcfilters-ui-savedLinksListItemWidget-icon' )
+									.append( this.$icon ),
+								$( '<div>' )
+									.addClass( 'mw-rcfilters-ui-cell' )
+									.addClass( 'mw-rcfilters-ui-savedLinksListItemWidget-content' )
+									.append(
+										this.$label
+											.addClass( 'mw-rcfilters-ui-savedLinksListItemWidget-label' ),
+										this.editInput.$element,
+										this.saveButton.$element
+									),
+								this.popupButton.$element
+									.addClass( 'mw-rcfilters-ui-cell' )
+							)
+					)
+			);
+	};
+
+	/* Initialization */
+	OO.inheritClass( mw.rcfilters.ui.SavedLinksListItemWidget, OO.ui.Widget );
+	OO.mixinClass( mw.rcfilters.ui.SavedLinksListItemWidget, OO.ui.mixin.LabelElement );
+	OO.mixinClass( mw.rcfilters.ui.SavedLinksListItemWidget, OO.ui.mixin.IconElement );
+
+	/* Events */
+
+	/**
+	 * @event delete
+	 *
+	 * The delete option was selected for this item
+	 */
+
+	/**
+	 * @event default
+	 * @param {boolean} default Item is default
+	 *
+	 * The 'make default' option was selected for this item
+	 */
+
+	/**
+	 * @event edit
+	 * @param {string} newLabel New label for the query
+	 *
+	 * The label has been edited
+	 */
+
+	/* Methods */
+
+	/**
+	 * Respond to model update event
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onModelUpdate = function () {
+		this.setLabel( this.model.getLabel() );
+		this.toggleDefault( this.model.isDefault() );
+	};
+
+	/**
+	 * Respond to popup button click event
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onPopupButtonClick = function () {
+		this.menu.toggle();
+	};
+
+	/**
+	 * Respond to menu choose event
+	 *
+	 * @param {OO.ui.MenuOptionWidget} item Chosen item
+	 * @fires delete
+	 * @fires default
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onMenuChoose = function ( item ) {
+		var action = item.getData();
+
+		if ( action === 'edit' ) {
+			this.toggleEdit( true );
+		} else if ( action === 'delete' ) {
+			this.emit( 'delete' );
+		} else if ( action === 'default' ) {
+			this.emit( 'default', !this.default );
+		}
+		this.menu.toggle( false );
+	};
+
+	/**
+	 * Respond to save button click
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onSaveButtonClick = function () {
+		this.emit( 'edit', this.editInput.getValue() );
+		this.toggleEdit( false );
+	};
+
+	/**
+	 * Respond to input enter event
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onEditInputEnter = function () {
+		this.emit( 'edit', this.editInput.getValue() );
+		this.toggleEdit( false );
+	};
+
+	/**
+	 * Respond to input keyup event, this is the way to intercept 'escape' key
+	 *
+	 * @param {jQuery.Event} e Event data
+	 * @returns {boolean} false
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onInputKeyup = function ( e ) {
+		if ( e.which === OO.ui.Keys.ESCAPE ) {
+			// Return the input to the original label
+			this.editInput.setValue( this.getLabel() );
+			this.toggleEdit( false );
+			return false;
+		}
+	};
+
+	/**
+	 * Respond to blur event on the input
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.onInputBlur = function () {
+		this.emit( 'edit', this.editInput.getValue() );
+		this.toggleEdit( false );
+	};
+
+	/**
+	 * Toggle edit mode on this widget
+	 *
+	 * @param {boolean} isEdit Widget is in edit mode
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.toggleEdit = function ( isEdit ) {
+		isEdit = isEdit === undefined ? !this.editing : isEdit;
+
+		if ( this.editing !== isEdit ) {
+			this.$element.toggleClass( 'mw-rcfilters-ui-savedLinksListItemWidget-edit', isEdit );
+			this.editInput.setValue( this.getLabel() );
+
+			this.editInput.toggle( isEdit );
+			this.$label.toggleClass( 'oo-ui-element-hidden', isEdit );
+			this.popupButton.toggle( !isEdit );
+			this.saveButton.toggle( isEdit );
+
+			if ( isEdit ) {
+				this.editInput.$input.focus();
+			}
+			this.editing = isEdit;
+		}
+	};
+
+	/**
+	 * Toggle default this widget
+	 *
+	 * @param {boolean} isDefault This item is default
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.toggleDefault = function ( isDefault ) {
+		isDefault = isDefault === undefined ? !this.default : isDefault;
+
+		if ( this.default !== isDefault ) {
+			this.default = isDefault;
+			this.setIcon( this.default ? 'pushPin' : '' );
+			this.menu.getItemFromData( 'default' ).setLabel(
+				this.default ?
+					mw.msg( 'rcfilters-savedqueries-unsetdefault' ) :
+					mw.msg( 'rcfilters-savedqueries-setdefault' )
+			);
+		}
+	};
+
+	/**
+	 * Get item ID
+	 *
+	 * @returns {string} Query identifier
+	 */
+	mw.rcfilters.ui.SavedLinksListItemWidget.prototype.getID = function () {
+		return this.model.getID();
+	};
+
+}( mediaWiki ) );
