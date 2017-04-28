7439be53223db75140c3b446d1b5c934736567c6
Arlo Breault <abreault@wikimedia.org>
2017-04-28 08:57:04 -0400

    The default page in filerepo/file/File.php is 1
     * Without this, `false` is coerced to 0, which returns 0 width for
       paged media, like PDFs.
    
     * See I94a8c5bd729d25e8742552d785efad52030866ba
    
    Change-Id: Ic611e5379dc8d138034b62ff8d678dfc68ced023

--- includes/Linker.php
+++ includes/Linker.php
@@ -333,7 +333,7 @@ class Linker {
 		}
 
 		// Clean up parameters
-		$page = isset( $handlerParams['page'] ) ? $handlerParams['page'] : false;
+		$page = isset( $handlerParams['page'] ) ? $handlerParams['page'] : 1;
 		if ( !isset( $frameParams['align'] ) ) {
 			$frameParams['align'] = '';
 		}
@@ -530,7 +530,7 @@ class Linker {
 	) {
 		$exists = $file && $file->exists();
 
-		$page = isset( $handlerParams['page'] ) ? $handlerParams['page'] : false;
+		$page = isset( $handlerParams['page'] ) ? $handlerParams['page'] : 1;
 		if ( !isset( $frameParams['align'] ) ) {
 			$frameParams['align'] = 'right';
 		}
