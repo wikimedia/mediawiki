2a920103d942b56b6d99bad771a7fd5561bcc49c
Fomafix <fomafix@googlemail.com>
2017-04-29 21:00:04 +0200

    Load module 'mediawiki.action.view.postEdit' only when needed
    Bug: T164148
    Change-Id: Id780bc280ce4a2fa4606141419932b7dcd45157b

--- includes/page/Article.php
+++ includes/page/Article.php
@@ -653,7 +653,12 @@ class Article implements Page {
 		$this->showViewFooter();
 		$this->mPage->doViewUpdates( $user, $oldid );
 
-		$outputPage->addModules( 'mediawiki.action.view.postEdit' );
+		# Load the postEdit module when this revision was just saved.
+		$request = $this->getContext()->getRequest();
+		$cookieKey = EditPage::POST_EDIT_COOKIE_KEY_PREFIX . $this->getRevIdFetched();
+		if ( $request->getCookie( $cookieKey ) ) {
+			$outputPage->addModules( 'mediawiki.action.view.postEdit' );
+		}
 	}
 
 	/**
