0e4668e377a459c8c767c13c7cc4ffac5154c311
Chad Horohoe <chadh@wikimedia.org>
2017-04-30 11:48:46 -0700

    Bump version number for new point release
    Change-Id: I07770b031c6035cf2c72e62ddfe9597a42dbd480

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.27.2';
+$wgVersion = '1.27.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
