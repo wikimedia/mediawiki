84284ccc45423ca482cb72a547b86619d31374f8
Kaldari <rkaldari@wikimedia.org>
2017-05-04 20:08:00 -0700

    Writing a useful description for the FakeConverter class
    Change-Id: I6575ea13790c224c1b25c7e9373b4bf40d035a30

--- languages/FakeConverter.php
+++ languages/FakeConverter.php
@@ -22,7 +22,12 @@
  */
 
 /**
- * A fake language converter
+ * A fake language variant converter. Languages which do not implement variant
+ * conversion, for example, English, should return a FakeConverter rather than a
+ * LanguageConverter when asked for their converter. The fake converter just
+ * returns text unchanged, i.e. it doesn't do any conversion.
+ *
+ * See https://www.mediawiki.org/wiki/Writing_systems#LanguageConverter.
  *
  * @ingroup Language
  */
