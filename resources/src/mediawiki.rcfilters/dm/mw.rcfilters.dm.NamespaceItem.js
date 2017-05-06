bf86c00561fe2d69d298d32ef12c57afc3dd14f0
Moriel Schottlender <moriel@gmail.com>
2017-05-05 18:07:24 -0700

    [wip] Add namespaces to RCFilters
    Bug: T159942
    Bug: T163521
    Change-Id: I55fedb8924ad7f434df78ed029ba0d4b19a9e9cf

---
+++ resources/src/mediawiki.rcfilters/dm/mw.rcfilters.dm.NamespaceItem.js
@@ -0,0 +1,64 @@
+( function ( mw ) {
+	/**
+	 * Filter item model
+	 *
+	 * @extends mw.rcfilters.dm.ItemModel
+	 *
+	 * @constructor
+	 * @param {number} param Namespace ID
+	 * @param {string} label Namespace label
+	 * @param {Object} config Configuration object
+	 * @cfg {boolean} [inverted] The selection is inverted
+	 */
+	mw.rcfilters.dm.NamespaceItem = function MwRcfiltersDmNamespaceItem( param, label, config ) {
+		config = config || {};
+
+		// Parent
+		mw.rcfilters.dm.NamespaceItem.parent.call( this, param, $.extend( {
+			label: label
+		}, config ) );
+		// Mixin constructor
+		OO.EventEmitter.call( this );
+
+		this.inverted = !!config.inverted;
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.rcfilters.dm.NamespaceItem, mw.rcfilters.dm.ItemModel );
+
+	/* Events */
+
+	/**
+	 * @event update
+	 *
+	 * The model has been updated
+	 */
+
+	/* Methods */
+
+	/**
+	 * Get namespace ID
+	 *
+	 * @return {number} Namespace ID
+	 */
+	mw.rcfilters.dm.NamespaceItem.prototype.getNamespaceID = function () {
+		return +this.getParamName();
+	};
+
+	/**
+	 * Toggle the inverted state of the model
+	 *
+	 * @param {boolean} isInverted Selection is inverted
+	 * @fires update
+	 */
+	mw.rcfilters.dm.NamespaceItem.prototype.toggleInverted = function ( isInverted ) {
+		isInverted = isInverted === undefined ? !this.inverted : !!isInverted;
+
+		if ( this.inverted !== isInverted ) {
+			this.inverted = isInverted;
+
+			this.emit( 'update' );
+		}
+	};
+}( mediaWiki ) );
