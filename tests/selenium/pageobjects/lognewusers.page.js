cdddf1e51ccb32313309ebb97980398cf1c2c1be
Željko Filipin <zeljko.filipin@gmail.com>
2017-05-08 12:10:18 +0200

    Create users and pages for Selenium tests using action API
    This will make tests slightly more robust.
    
    Bug: T164721
    Bug: T167502
    Change-Id: I9b2fea77b28af4f7f521490a0105e7d04730bc87

---
+++ tests/selenium/pageobjects/lognewusers.page.js
@@ -0,0 +1,15 @@
+'use strict';
+const Page = require( './page' );
+
+class LogNewUsersPage extends Page {
+
+	open() {
+		super.open( 'Special:Log/newusers' );
+	}
+
+	user( username ) {
+		return browser.element( `a[title='User:${username} (page does not exist)']` );
+	}
+
+}
+module.exports = new LogNewUsersPage();
