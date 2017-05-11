6da9a38e17a0bd2393d7e70af8e52171ba40d01b
Ed Sanders <esanders@wikimedia.org>
2017-05-11 15:11:45 +0100

    Convert rest of Special:Undelete to OOUI
    Also fix the 'invert selection' button:
    * Don't use type=submit so it doesn't try to post
      if JS is disabled
    * Don't invert the wpUnsuppress checkbox, just
      checkboxes in revision lists.
    
    Bug: T113758
    Change-Id: I84785a5ddcc3343d74b5b7addb4aa4817a469f33

--- resources/src/mediawiki.special/mediawiki.special.undelete.js
+++ resources/src/mediawiki.special/mediawiki.special.undelete.js
@@ -2,10 +2,9 @@
  * JavaScript for Special:Undelete
  */
 jQuery( function ( $ ) {
-	$( '#mw-undelete-invert' ).click( function ( e ) {
-		$( '#undelete input[type="checkbox"]' ).prop( 'checked', function ( i, val ) {
+	$( '#mw-undelete-invert' ).click( function () {
+		$( '.mw-undelete-revlist input[type="checkbox"]' ).prop( 'checked', function ( i, val ) {
 			return !val;
 		} );
-		e.preventDefault();
 	} );
 } );
