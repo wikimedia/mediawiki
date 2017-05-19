wikimedia/mediawiki

commit b8b01aa589cefcb5c1ac516e4007da1643403231

Replace DataUpdate::doUpdates() callers in maintenance/
Change-Id: I51df9f419d6ea1e822fd0eef8c5dc4c43f9855e1

maintenance/refreshLinks.php
@@ -237,8 +237,9 @@ class RefreshLinks extends Maintenance {
 			return;
 		}
 
-		$updates = $content->getSecondaryDataUpdates( $page->getTitle() );
-		DataUpdate::runUpdates( $updates );
+		foreach ( $content->getSecondaryDataUpdates( $page->getTitle() ) as $update ) {
+			DeferredUpdates::addUpdate( $update );
+		}
 	}
 
 	/**

maintenance/namespaceDupes.php
@@ -607,8 +607,8 @@ class NamespaceConflictChecker extends Maintenance {
 		 * accidentally introduce an assumption of title validity to the code we
 		 * are calling.
 		 */
-		$updates = [ new LinksDeletionUpdate( $wikiPage ) ];
-		DataUpdate::runUpdates( $updates );
+		DeferredUpdates::addUpdate( new LinksDeletionUpdate( $wikiPage ) );
+		DeferredUpdates::doUpdates();
 
 		return true;
 	}

commit 0916fa76e81dbcfbbe5fd8ed529b25d4cf358e3a

Add missing doUpdates() call to refreshLinks.php
Follows-up b8b01aa589ce which accidentally removed this call.

Bug: T157545
Change-Id: Id4efdccb60e7829269df601b83f64288f6190db4

maintenance/refreshLinks.php
@@ -278,6 +278,7 @@ class RefreshLinks extends Maintenance {
 			$page->getTitle(), /* $old = */ null, /* $recursive = */ false );
 		foreach ( $updates as $update ) {
 			DeferredUpdates::addUpdate( $update );
+			DeferredUpdates::doUpdates();
 		}
 	}
 