ac7517e48a32b3aa9afb3ab67a7f988e0530ba08
Petr Pchelko <ppchelko@wikimedia.org>
2017-05-25 15:20:16 -0700

    WIP: JobQueue: Create an debugging queue
    Bug: T163380
    Change-Id: I483b8f00f1c593ff3af6ba6826868b553339525f

--- autoload.php
+++ autoload.php
@@ -673,6 +673,7 @@ $wgAutoloadLocalClasses = [
 	'JobQueueAggregatorRedis' => __DIR__ . '/includes/jobqueue/aggregator/JobQueueAggregatorRedis.php',
 	'JobQueueConnectionError' => __DIR__ . '/includes/jobqueue/JobQueue.php',
 	'JobQueueDB' => __DIR__ . '/includes/jobqueue/JobQueueDB.php',
+	'JobQueueDebug' => __DIR__ . '/includes/jobqueue/JobQueueDebug.php',
 	'JobQueueError' => __DIR__ . '/includes/jobqueue/JobQueue.php',
 	'JobQueueFederated' => __DIR__ . '/includes/jobqueue/JobQueueFederated.php',
 	'JobQueueGroup' => __DIR__ . '/includes/jobqueue/JobQueueGroup.php',
