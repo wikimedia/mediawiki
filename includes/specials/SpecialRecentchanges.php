ee90c31e66b43b0eeafb336bfdf14ffa408cee6a
Moriel Schottlender <moriel@gmail.com>
2017-05-29 18:04:35 +0300

    RCFilters: Add edit tags drop down
    Fetches the tags from the wiki and displays them as additional
    filters for RCFilters.
    
    Bug: T159942
    Bug: T161650
    Bug: T164130
    Change-Id: I7bfa99cd5aeb34b6c7de74c15aac158ee40eac2f

--- includes/specials/SpecialRecentchanges.php
+++ includes/specials/SpecialRecentchanges.php
@@ -189,7 +189,45 @@ class SpecialRecentChanges extends ChangesListSpecialPage {
 				'wgStructuredChangeFiltersEnableExperimentalViews',
 				$wgStructuredChangeFiltersEnableExperimentalViews
 			);
+			$out->addJsConfigVars(
+				'wgRCFiltersChangeTags',
+				$this->buildChangeTagList()
+			);
 		}
+	}
+
+	/**
+	 * Use the API response to build the available tag
+	 * list with ordering, then clean it up per the enhanced RCFilters
+	 * specifications.
+	 *
+	 * @return Array Tag data
+	 */
+	protected function buildChangeTagList() {
+		$params = new DerivativeRequest(
+			$this->getRequest(), // Fallback upon $wgRequest if you can't access context.
+			[
+				'action' => 'query',
+				'list' => 'tags',
+				'tgprop' => 'displayname|hitcount|description',
+				'tglimit' => 500
+			]
+		);
+
+		$api = new ApiMain( $params );
+		$api->execute();
+		$data = $api->getResult()->getResultData();
+
+		// Prepare for JavaScript consumption
+		$result = [];
+		foreach ( $data[ 'query' ][ 'tags' ] as &$tag ) {
+			// Parse description
+			$desc = ChangeTags::tagLongDescriptionMessage( $tag[ 'name' ], $this->getContext() );
+			$tag[ 'description' ] = $desc ? $desc->parse() : '';
+			$tag[ 'cssClass' ] = Sanitizer::escapeClass( 'mw-tag-' . $tag[ 'name' ] );
+		}
+
+		return $data[ 'query' ][ 'tags' ];
 	}
 
 	/**
