d5bd85c5d18f7e0488a1b9dbc4f4baea7b660969
Amir Sarabadani <ladsgroup@gmail.com>
2017-06-01 20:09:21 +0430

    Start a very basic version of Special:PageData
    Bug: T163923
    Change-Id: I2a1a12f20a38d8d3c50a8f6c9a363be1cb656c70

---
+++ includes/linkeddata/PageDataRequestHandler.php
@@ -0,0 +1,152 @@
+<?php
+
+use MediaWiki\Http\HttpAcceptParser;
+use MediaWiki\Http\HttpAcceptNegotiator;
+
+/**
+ * Request handler implementing a data interface for mediawiki pages.
+ *
+ * @license GPL-2.0+
+ * @author Daniel Kinzler
+ * @author Amir Sarabadanai
+ */
+
+class PageDataRequestHandler {
+
+	/**
+	 * Checks whether the request is complete, i.e. whether it contains all information needed
+	 * to reply with page data.
+	 *
+	 * This does not check whether the request is valid and will actually produce a successful
+	 * response.
+	 *
+	 * @param string|null $title Page title
+	 * @param WebRequest $request
+	 *
+	 * @return bool
+	 * @throws HttpError
+	 */
+	public function canHandleRequest( $title, WebRequest $request ) {
+		if ( $title === '' || $title === null ) {
+			if ( $request->getText( 'title', '' ) === '' ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Main method for handling requests.
+	 *
+	 * @param string $title Page title
+	 * @param WebRequest $request The request parameters. Known parameters are:
+	 *        - title: the page title
+	 *        - format: the format
+	 *        - oldid|revision: the revision ID
+	 * @param OutputPage $output
+	 *
+	 * @note: Instead of an output page, a WebResponse could be sufficient, but
+	 *        redirect logic is currently implemented in OutputPage.
+	 *
+	 * @throws HttpError
+	 */
+	public function handleRequest( $title, WebRequest $request, OutputPage $output ) {
+		// No matter what: The response is always public
+		$output->getRequest()->response()->header( 'Access-Control-Allow-Origin: *' );
+
+		$revision = 0;
+
+		$title = $request->getText( 'title', $title );
+		$revision = $request->getInt( 'oldid', $revision );
+		$revision = $request->getInt( 'revision', $revision );
+
+		if ( $title === null || $title === '' ) {
+			//TODO: different error message?
+			throw new HttpError( 400, wfMessage( 'pagedata-bad-title', $title ) );
+		}
+
+		try {
+			$title = Title::newFromTextThrow( $title );
+		} catch ( MalformedTitleException $ex ) {
+			throw new HttpError( 400, wfMessage( 'pagedata-bad-title', $title ) );
+		}
+
+		$this->httpContentNegotiation( $request, $output, $title, $revision );
+	}
+
+	/**
+	 * Applies HTTP content negotiation.
+	 * If the negotiation is successful, this method will set the appropriate redirect
+	 * in the OutputPage object and return. Otherwise, an HttpError is thrown.
+	 *
+	 * @param WebRequest $request
+	 * @param OutputPage $output
+	 * @param Title $title
+	 * @param int $revision The desired revision
+	 *
+	 * @throws HttpError
+	 */
+	public function httpContentNegotiation(
+		WebRequest $request,
+		OutputPage $output,
+		Title $title,
+		$revision = 0
+	) {
+		$contentHandler = ContentHandler::getForTitle( $title );
+		$mimeTypes = $contentHandler->getSupportedFormats();
+
+		$headers = $request->getAllHeaders();
+		if ( isset( $headers['ACCEPT'] ) ) {
+			$parser = new HttpAcceptParser();
+			$accept = $parser->parseWeights( $headers['ACCEPT'] );
+		} else {
+			// anything goes
+			$accept = [
+				'*' => 0.1 // just to make extra sure
+			];
+			// prefer the default
+			$accept[$mimeTypes[0]] = 1;
+		}
+
+		$negotiator = new HttpAcceptNegotiator( $mimeTypes );
+		$format = $negotiator->getBestSupportedKey( $accept, null );
+
+		if ( $format === null ) {
+			$format = isset( $accept['text/html'] ) ? 'text/html' : null;
+		}
+
+		if ( $format === null ) {
+			$msg = wfMessage( 'pagedata-not-acceptable', implode( ', ', $mimeTypes ) );
+			throw new HttpError( 406, $msg );
+		}
+
+		$url = $this->getDocUrl( $title, $format, $revision );
+		$output->redirect( $url, 303 );
+	}
+
+	/**
+	 * Returns a url representing the given title.
+	 *
+	 * @param Title $title
+	 * @param string|null $format The (normalized) format name, or ''
+	 * @param int $revision
+	 * @return string
+	 */
+	private function getDocUrl( Title $title, $format = '', $revision = 0 ) {
+		$params = [];
+
+		if ( $revision > 0 ) {
+			$params['oldid'] = $revision;
+		}
+
+		if ( $format === 'text/html' ) {
+			return $title->getFullURL( $params );
+		}
+
+		$params[ 'action' ] = 'raw';
+
+		return $title->getFullURL( $params );
+	}
+
+}
---
+++ includes/specials/SpecialPageData.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Special page to act as an endpoint for accessing raw page data.
+ * The web server should generally be configured to make this accessible via a canonical URL/URI,
+ * such as <http://my.domain.org/data/main/Foo>.
+ *
+ * @license GPL-2.0+
+ */
+class SpecialPageData extends SpecialPage {
+
+	/**
+	 * @var PageDataRequestHandler|null
+	 */
+	private $requestHandler = null;
+
+	public function __construct() {
+		parent::__construct( 'PageData' );
+	}
+
+	/**
+	 * Sets the request handler to be used by the special page.
+	 * May be used when a particular instance of PageDataRequestHandler is already
+	 * known, e.g. during testing.
+	 *
+	 * If no request handler is set using this method, a default handler is created
+	 * on demand by initDependencies().
+	 *
+	 * @param PageDataRequestHandler $requestHandler
+	 */
+	public function setRequestHandler( PageDataRequestHandler $requestHandler ) {
+		$this->requestHandler = $requestHandler;
+	}
+
+	/**
+	 * Initialize any un-initialized members from global context.
+	 * In particular, this initializes $this->requestHandler
+	 */
+	protected function initDependencies() {
+		if ( $this->requestHandler === null ) {
+			$this->requestHandler = $this->newDefaultRequestHandler();
+		}
+	}
+
+	/**
+	 * Creates a PageDataRequestHandler based on global defaults.
+	 *
+	 * @return PageDataRequestHandler
+	 */
+	private function newDefaultRequestHandler() {
+
+		return new PageDataRequestHandler();
+	}
+
+	/**
+	 * @see SpecialWikibasePage::execute
+	 *
+	 * @param string|null $subPage
+	 *
+	 * @throws HttpError
+	 */
+	public function execute( $subPage ) {
+		$this->initDependencies();
+
+		// If there is no title, show an HTML form
+		// TODO: Don't do this if HTML is not acceptable according to HTTP headers.
+		if ( !$this->requestHandler->canHandleRequest( $subPage, $this->getRequest() ) ) {
+			$this->showForm();
+			return;
+		}
+
+		$this->requestHandler->handleRequest( $subPage, $this->getRequest(), $this->getOutput() );
+	}
+
+	/**
+	 * Shows an informative page to the user; Called when there is no page to output.
+	 */
+	public function showForm() {
+		$this->getOutput()->showErrorPage( 'pagedata-title', 'pagedata-text' );
+	}
+
+	public function isListed() {
+		// Do not list this page in Special:SpecialPages
+		return false;
+	}
+
+}
