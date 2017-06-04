e603137874c3b3ee98412c78ea4c03d192a00278
Moriel Schottlender <moriel@gmail.com>
2017-06-04 19:10:05 +0300

    RCFilters: Add UriProcessor and unit tests for URL manipulations
    Bug: T166974
    Bug: T166907
    Change-Id: I066c33a01770b7d8026aa3e039af5fe5d9c4cdf9

---
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.UriProcessor.js
@@ -0,0 +1,266 @@
+( function ( mw, $ ) {
+	/* eslint no-underscore-dangle: "off" */
+	/**
+	 * URI Processor for RCFilters
+	 *
+	 * @param {mw.rcfilters.dm.FiltersViewModel} filtersModel Filters view model
+	 */
+	mw.rcfilters.UriProcessor = function MwRcfiltersController( filtersModel ) {
+		this.emptyParameterState = {};
+		this.filtersModel = filtersModel;
+
+		// Initialize
+		this._buildEmptyParameterState();
+	};
+
+	/* Initialization */
+	OO.initClass( mw.rcfilters.UriProcessor );
+
+	/* Static methods */
+
+	/**
+	 * Replace the url history through replaceState
+	 *
+	 * @param {mw.Uri} newUri New URI to replace
+	 */
+	mw.rcfilters.UriProcessor.static.replaceState = function ( newUri ) {
+		window.history.replaceState(
+			{ tag: 'rcfilters' },
+			document.title,
+			newUri.toString()
+		);
+	};
+
+	/**
+	 * Push the url to history through pushState
+	 *
+	 * @param {mw.Uri} newUri New URI to push
+	 */
+	mw.rcfilters.UriProcessor.static.pushState = function ( newUri ) {
+		window.history.pushState(
+			{ tag: 'rcfilters' },
+			document.title,
+			newUri.toString()
+		);
+	};
+
+	/* Methods */
+
+	/**
+	 * Get the version that this URL query is tagged with.
+	 *
+	 * @param {Object} [uriQuery] URI query
+	 * @return {number} URL version
+	 */
+	mw.rcfilters.UriProcessor.prototype.getVersion = function ( uriQuery ) {
+		uriQuery = uriQuery || new mw.Uri().query;
+
+		return Number( uriQuery.urlversion || 1 );
+	};
+
+	/**
+	 * Update the filters model based on the URI query
+	 * This happens on initialization, and from this moment on,
+	 * we consider the system synchronized, and the model serves
+	 * as the source of truth for the URL.
+	 *
+	 * This methods should only be called once on initialiation.
+	 * After initialization, the model updates the URL, not the
+	 * other way around.
+	 *
+	 * @param {Object} [uriQuery] URI query
+	 */
+	mw.rcfilters.UriProcessor.prototype.updateModelBasedOnQuery = function ( uriQuery ) {
+		var parameters = this._getNormalizedQueryParams( uriQuery || new mw.Uri().query );
+
+		// Update filter states
+		this.filtersModel.toggleFiltersSelected(
+			this.filtersModel.getFiltersFromParameters(
+				parameters
+			)
+		);
+
+		// Update highlight state
+		this.filtersModel.toggleHighlight( !!Number( parameters.highlight ) );
+		this.filtersModel.getItems().forEach( function ( filterItem ) {
+			var color = parameters[ filterItem.getName() + '_color' ];
+			if ( color ) {
+				filterItem.setHighlightColor( color );
+			} else {
+				filterItem.clearHighlightColor();
+			}
+		} );
+
+		// Check all filter interactions
+		this.filtersModel.reassessFilterInteractions();
+	};
+
+	/**
+	 * Get parameters representing the current state of the model
+	 *
+	 * @return {Object} Uri query parameters
+	 */
+	mw.rcfilters.UriProcessor.prototype.getUriParametersFromModel = function () {
+		return $.extend(
+			true,
+			{},
+			this.filtersModel.getParametersFromFilters(),
+			this.filtersModel.getHighlightParameters(),
+			{ highlight: String( Number( this.filtersModel.isHighlightEnabled() ) ) }
+		);
+	};
+
+	/**
+	 * Compare two URI queries to decide whether they are different
+	 * enough to represent a new state.
+	 *
+	 * @param {Object} currentUriQuery Current Uri query
+	 * @param {Object} updatedUriQuery Updated Uri query
+	 * @return {boolean} This is a new state
+	 */
+	mw.rcfilters.UriProcessor.prototype.isNewState = function ( currentUriQuery, updatedUriQuery ) {
+		var currentParamState, updatedParamState,
+			notEquivalent = function ( obj1, obj2 ) {
+				var keys = Object.keys( obj1 ).concat( Object.keys( obj2 ) );
+				return keys.some( function ( key ) {
+					return obj1[ key ] != obj2[ key ]; // eslint-disable-line eqeqeq
+				} );
+			};
+
+		// Compare states instead of parameters
+		// This will allow us to always have a proper check of whether
+		// the requested new url is one to change or not, regardless of
+		// actual parameter visibility/representation in the URL
+		currentParamState = this._buildFullModelParameterState( currentUriQuery );
+		updatedParamState = this._buildFullModelParameterState( updatedUriQuery );
+
+		// Merge back into the uri query so we retain unrecognize params
+		currentParamState = $.extend( true, {}, currentUriQuery, currentParamState );
+		updatedParamState = $.extend( true, {}, updatedUriQuery, updatedParamState );
+
+		return notEquivalent( currentParamState, updatedParamState );
+	};
+
+	/**
+	 * Check whether the given query has parameters that are
+	 * recognized as parameters we should load the system with
+	 *
+	 * @param {mw.Uri} [uriQuery] Given URI query
+	 * @return {boolean} Query contains valid recognized parameters
+	 */
+	mw.rcfilters.UriProcessor.prototype.isQueryValidForLoad = function ( uriQuery ) {
+		var anyValidInUrl,
+			validParameterNames = Object.keys( this._getEmptyParameterState() )
+				.filter( function ( param ) {
+					// Remove 'highlight' parameter from this check;
+					// if it's the only parameter in the URL we still
+					// want to consider the URL 'empty' for defaults to load
+					return param !== 'highlight';
+				} );
+
+		uriQuery = uriQuery || new mw.Uri().query;
+
+		anyValidInUrl = Object.keys( uriQuery ).some( function ( parameter ) {
+			return validParameterNames.indexOf( parameter ) > -1;
+		} );
+
+		// URL version 2 is allowed to be empty or within nonrecognized params
+		return anyValidInUrl || this.getVersion( uriQuery ) === 2;
+	};
+
+	/**
+	 * Remove all parameters that have the same value as the base state
+	 *
+	 * @private
+	 * @param {Object} uriQuery Current uri query
+	 * @return {Object} Minimized query
+	 */
+	mw.rcfilters.UriProcessor.prototype.minimizeQuery = function ( uriQuery ) {
+		var baseParams = this._getEmptyParameterState();
+
+		$.each( uriQuery, function ( paramName, paramValue ) {
+			if (
+				baseParams[ paramName ] !== undefined &&
+				baseParams[ paramName ] === paramValue
+			) {
+				// Remove parameter from query
+				delete uriQuery[ paramName ];
+			}
+		} );
+
+		return uriQuery;
+	};
+
+	/**
+	 * Get the adjusted URI params based on the url version
+	 * If the urlversion is not 2, the parameters are merged with
+	 * the model's defaults.
+	 *
+	 * @private
+	 * @param {Object} uriQuery Current URI query
+	 * @return {Object} Normalized parameters
+	 */
+	mw.rcfilters.UriProcessor.prototype._getNormalizedQueryParams = function ( uriQuery ) {
+		// Check whether we are dealing with urlversion=2
+		// If we are, we do not merge the initial request with
+		// defaults. Not having urlversion=2 means we need to
+		// reproduce the server-side request and merge the
+		// requested parameters (or starting state) with the
+		// wiki default.
+		// Any subsequent change of the URL through the RCFilters
+		// system will receive 'urlversion=2'
+		var base = this.getVersion( uriQuery ) === 2 ?
+			{} :
+			this.filtersModel.getDefaultParams();
+
+		return this.minimizeQuery(
+			$.extend( true, {}, base, uriQuery, { urlversion: '2' } )
+		);
+	};
+
+	/**
+	 * Get the representation of an empty parameter state
+	 *
+	 * @return {Object} Empty parameter state
+	 */
+	mw.rcfilters.UriProcessor.prototype._getEmptyParameterState = function () {
+		return this.emptyParameterState;
+	};
+
+	/**
+	 * Build the full filter state based on parameters
+	 *
+	 * @private
+	 * @param {Object} uriQuery Given URI query
+	 * @return {Object} Full filter state representing the URI query
+	 */
+	mw.rcfilters.UriProcessor.prototype._buildFullModelParameterState = function ( uriQuery ) {
+		var filterRepresentation = this.filtersModel.getFiltersFromParameters( uriQuery );
+		return $.extend( true,
+			{},
+			this.filtersModel.getParametersFromFilters( filterRepresentation ),
+			this.filtersModel.extractHighlightValues( uriQuery ),
+			{ highlight: !!Number( uriQuery.highlight ) }
+		);
+	};
+
+	/**
+	 * Build an empty representation of the parameters, where all parameters
+	 * are either set to '0' or '' depending on their type.
+	 * This must run during initialization, before highlights are set.
+	 *
+	 * @private
+	 */
+	mw.rcfilters.UriProcessor.prototype._buildEmptyParameterState = function () {
+		var emptyParams = this.filtersModel.getParametersFromFilters( {} ),
+			emptyHighlights = this.filtersModel.getHighlightParameters();
+
+		this.emptyParameterState = $.extend(
+			true,
+			{},
+			emptyParams,
+			emptyHighlights,
+			{ highlight: '0' }
+		);
+	};
+}( mediaWiki, jQuery ) );
---
+++ tests/qunit/suites/resources/mediawiki.rcfilters/UriProcessor.test.js
@@ -0,0 +1,244 @@
+/* eslint-disable camelcase */
+/* eslint no-underscore-dangle: "off" */
+( function ( mw, $ ) {
+	var mockFilterStructure = [ {
+			name: 'group1',
+			title: 'Group 1',
+			type: 'send_unselected_if_any',
+			filters: [
+				{ name: 'filter1', default: true },
+				{ name: 'filter2' }
+			]
+		}, {
+			name: 'group2',
+			title: 'Group 2',
+			type: 'send_unselected_if_any',
+			filters: [
+				{ name: 'filter3' },
+				{ name: 'filter4', default: true }
+			]
+		}, {
+			name: 'group3',
+			title: 'Group 3',
+			type: 'string_options',
+			filters: [
+				{ name: 'filter5' },
+				{ name: 'filter6' }
+			]
+		} ],
+		minimalDefaultParams = {
+			filter1: '1',
+			filter4: '1'
+		};
+
+	QUnit.module( 'mediawiki.rcfilters - UriProcessor' );
+
+	QUnit.test( 'getVersion', function ( assert ) {
+		var uriProcessor = new mw.rcfilters.UriProcessor( new mw.rcfilters.dm.FiltersViewModel() );
+
+		assert.ok(
+			uriProcessor.getVersion( { param1: 'foo', urlversion: '2' } ),
+			2,
+			'Retrieving the version from the URI query'
+		);
+
+		assert.ok(
+			uriProcessor.getVersion( { param1: 'foo' } ),
+			1,
+			'Getting version 1 if no version is specified'
+		);
+	} );
+
+	QUnit.test( 'updateModelBasedOnQuery & getUriParametersFromModel', function ( assert ) {
+		var uriProcessor,
+			filtersModel = new mw.rcfilters.dm.FiltersViewModel(),
+			baseParams = {
+				filter1: '0',
+				filter2: '0',
+				filter3: '0',
+				filter4: '0',
+				group3: '',
+				highlight: '0',
+				group1__filter1_color: null,
+				group1__filter2_color: null,
+				group2__filter3_color: null,
+				group2__filter4_color: null,
+				group3__filter5_color: null,
+				group3__filter6_color: null
+			};
+
+		filtersModel.initializeFilters( mockFilterStructure );
+		uriProcessor = new mw.rcfilters.UriProcessor( filtersModel );
+
+		uriProcessor.updateModelBasedOnQuery( {} );
+		assert.deepEqual(
+			uriProcessor.getUriParametersFromModel(),
+			$.extend( true, {}, baseParams, minimalDefaultParams ),
+			'Version 1: Empty url query sets model to defaults'
+		);
+
+		uriProcessor.updateModelBasedOnQuery( { urlversion: '2' } );
+		assert.deepEqual(
+			uriProcessor.getUriParametersFromModel(),
+			baseParams,
+			'Version 2: Empty url query sets model to all-false'
+		);
+
+		uriProcessor.updateModelBasedOnQuery( { filter1: '1', urlversion: '2' } );
+		assert.deepEqual(
+			uriProcessor.getUriParametersFromModel(),
+			$.extend( true, {}, baseParams, { filter1: '1' } ),
+			'Parameters in Uri query set parameter value in the model'
+		);
+
+		uriProcessor.updateModelBasedOnQuery( { highlight: '1', group1__filter1_color: 'c1', urlversion: '2' } );
+		assert.deepEqual(
+			uriProcessor.getUriParametersFromModel(),
+			$.extend( true, {}, baseParams, {
+				highlight: '1',
+				group1__filter1_color: 'c1'
+			} ),
+			'Highlight parameters in Uri query set highlight state in the model'
+		);
+	} );
+
+	QUnit.test( 'isNewState', function ( assert ) {
+		var uriProcessor,
+			filtersModel = new mw.rcfilters.dm.FiltersViewModel(),
+			cases = [
+				{
+					states: {
+						curr: {},
+						new: {}
+					},
+					result: false,
+					message: 'Empty objects are not new state.'
+				},
+				{
+					states: {
+						curr: { filter1: '1' },
+						new: { filter1: '0' }
+					},
+					result: true,
+					message: 'Nulified parameter is a new state'
+				},
+				{
+					states: {
+						curr: { filter1: '1' },
+						new: { filter1: '1', filter2: '1' }
+					},
+					result: true,
+					message: 'Added parameters are a new state'
+				},
+				{
+					states: {
+						curr: { filter1: '1' },
+						new: { filter1: '1', filter2: '0' }
+					},
+					result: false,
+					message: 'Added null parameters are not a new state (normalizing equals old state)'
+				},
+				{
+					states: {
+						curr: { filter1: '1' },
+						new: { filter1: '1', foo: 'bar' }
+					},
+					result: true,
+					message: 'Added unrecognized parameters are a new state'
+				}
+			];
+
+		filtersModel.initializeFilters( mockFilterStructure );
+		uriProcessor = new mw.rcfilters.UriProcessor( filtersModel );
+
+		cases.forEach( function ( testCase ) {
+			assert.equal(
+				uriProcessor.isNewState( testCase.states.curr, testCase.states.new ),
+				testCase.result,
+				testCase.message
+			);
+		} );
+	} );
+
+	QUnit.test( 'isQueryValidForLoad', function ( assert ) {
+		var uriProcessor,
+			filtersModel = new mw.rcfilters.dm.FiltersViewModel(),
+			cases = [
+				{
+					query: {},
+					result: false,
+					message: 'Empty query is not valid for load.'
+				},
+				{
+					query: { highlight: '1' },
+					result: false,
+					message: 'Highlight state alone is not valid for load'
+				},
+				{
+					query: { urlversion: '2' },
+					result: true,
+					message: 'urlversion=2 state alone is valid for load as an empty state'
+				},
+				{
+					query: { filter1: '1', foo: 'bar' },
+					result: true,
+					message: 'Existence of recognized parameters makes the query valid for load'
+				},
+				{
+					query: { foo: 'bar', debug: true },
+					result: false,
+					message: 'Only unrecognized parameters makes the query invalid for load'
+				}
+			];
+
+		filtersModel.initializeFilters( mockFilterStructure );
+		uriProcessor = new mw.rcfilters.UriProcessor( filtersModel );
+
+		cases.forEach( function ( testCase ) {
+			assert.equal(
+				uriProcessor.isQueryValidForLoad( testCase.query ),
+				testCase.result,
+				testCase.message
+			);
+		} );
+	} );
+
+	QUnit.test( '_getNormalizedQueryParams', function ( assert ) {
+		var uriProcessor,
+			filtersModel = new mw.rcfilters.dm.FiltersViewModel(),
+			cases = [
+				{
+					query: {},
+					result: $.extend( true, { urlversion: '2' }, minimalDefaultParams ),
+					message: 'Empty query returns defaults (urlversion 1).'
+				},
+				{
+					query: { urlversion: '2' },
+					result: { urlversion: '2' },
+					message: 'Empty query returns empty (urlversion 2)'
+				},
+				{
+					query: { filter1: '0' },
+					result: { urlversion: '2', filter4: '1' },
+					message: 'urlversion 1 returns query that overrides defaults'
+				},
+				{
+					query: { filter3: '1' },
+					result: { urlversion: '2', filter1: '1', filter4: '1', filter3: '1' },
+					message: 'urlversion 1 with an extra param value returns query that is joined with defaults'
+				}
+			];
+
+		filtersModel.initializeFilters( mockFilterStructure );
+		uriProcessor = new mw.rcfilters.UriProcessor( filtersModel );
+
+		cases.forEach( function ( testCase ) {
+			assert.deepEqual(
+				uriProcessor._getNormalizedQueryParams( testCase.query ),
+				testCase.result,
+				testCase.message
+			);
+		} );
+	} );
+
+}( mediaWiki, jQuery ) );
