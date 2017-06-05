9a9da756ae771b7de934b6f05a719039e44416b3
Prateek Saxena <prtksxna@gmail.com>
2017-06-05 17:43:14 +0530

    [wip] ooui implementation for htmlselectandother
    see T106999
    
    Change-Id: I0f437ed8a8808f8090bf466fd939345d74d57c34

--- includes/htmlform/fields/HTMLSelectOrOtherField.php
+++ includes/htmlform/fields/HTMLSelectOrOtherField.php
@@ -64,8 +64,62 @@ class HTMLSelectOrOtherField extends HTMLTextField {
 		return "$select<br />\n$textbox";
 	}
 
+	protected function getOOUIModules() {
+		return [ 'mediawiki.widgets.SelectWithInputWidget' ];
+	}
+
 	public function getInputOOUI( $value ) {
-		return false;
+		$this->mParent->getOutput()->addModuleStyles( 'mediawiki.widgets.SelectWithInputWidget.styles' );
+		$valInSelect = false;
+
+		if ( $value !== false ) {
+			$value = strval( $value );
+			$valInSelect = in_array(
+				$value, HTMLFormField::flattenOptions( $this->getOptions() ), true
+			);
+		}
+
+		$selected = $valInSelect ? $value : 'other';
+
+		$select = new XmlSelect( $this->mName, $this->mID, $selected );
+		$select->addOptions( $this->getOptions() );
+
+		$select->setAttribute( 'class', 'mw-htmlform-select-or-other' );
+
+		$tbAttribs = [ 'id' => $this->mID . '-other', 'size' => $this->getSize() ];
+
+		if ( !empty( $this->mParams['disabled'] ) ) {
+			$select->setAttribute( 'disabled', 'disabled' );
+			$tbAttribs['disabled'] = 'disabled';
+		}
+
+		if ( isset( $this->mParams['tabindex'] ) ) {
+			$select->setAttribute( 'tabindex', $this->mParams['tabindex'] );
+			$tbAttribs['tabindex'] = $this->mParams['tabindex'];
+		}
+
+		$select = $select->getHTML();
+
+		if ( isset( $this->mParams['maxlength'] ) ) {
+			$tbAttribs['maxlength'] = $this->mParams['maxlength'];
+		}
+
+		if ( $this->mClass !== '' ) {
+			$tbAttribs['class'] = $this->mClass;
+		}
+
+		$textbox = Html::input( $this->mName . '-other', $valInSelect ? '' : $value, 'text', $tbAttribs );
+
+		return $this->getInputWidget( [
+			'dropdowninput' => [
+				'options' => $this->getOptionsOOUI(),
+			],
+			'or' => false
+		] );
+	}
+
+	public function getInputWidget( $params ) {
+		return new Mediawiki\Widget\SelectWithInputWidget( $params );
 	}
 
 	/**
