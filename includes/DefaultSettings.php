9266c098f2548704bd7de1a5a58329bbbf29ce09
Brad Jorsch <bjorsch@wikimedia.org>
2017-06-06 13:39:14 -0400

    WIP: Add `comment` table and code to start using it
    A subsequent patch will remove the old columns.
    
    DONE:
    * Schema change for MySQL and sqlite
    * update.php integration for MySQL and sqlite
    * CommentStore class
    
    TODO:
    * Update everything in core that currently accesses comment columns to
      use CommentStore
    * Update extensions or file bugs for them to be updated
    * Write schema change for other DBs
    
    Bug: T166732
    Change-Id: Ic3a434c061ed6e443ea072bc62dda09acbeeed7f

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -8659,6 +8659,13 @@ $wgExperiencedUserMemberSince = 30; # days
 $wgInterwikiPrefixDisplayTypes = [];
 
 /**
+ * Comment table schema migration stage.
+ * @since 1.30
+ * @var int One of the MIGRATION_* constants
+ */
+$wgCommentTableSchemaMigrationStage = MIGRATION_NEW;
+
+/**
  * For really cool vim folding this needs to be at the end:
  * vim: foldmarker=@{,@} foldmethod=marker
  * @}
