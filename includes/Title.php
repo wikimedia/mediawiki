fbc144965315cb1360500ef08ae9f4af15e76c43
daniel <daniel.kinzler@wikimedia.de>
2017-06-09 18:39:33 +0200

    Make Titles with an unknown namespace ID refer to Special:Badtitle.
    Without this patch, Title::getPrefixedText() would return ":Foo"
    if the namespace was unknown, potentially creating a misleading
    link to the main namespace. With this change, getPrefixedText()
    will return something like "Special:Badtitle/NS12345:Foo".
    
    Note that round trip behavior is broken either way.
    
    Bug: T165149
    Change-Id: I0d491a2b58ff45f207f83ee62ca6e7e6ffbf790a

--- includes/Title.php
+++ includes/Title.php
@@ -1419,13 +1419,22 @@ class Title implements LinkTarget {
 	 * @return string The prefixed text
 	 */
 	private function prefix( $name ) {
+		global $wgContLang;
+
 		$p = '';
 		if ( $this->isExternal() ) {
 			$p = $this->mInterwiki . ':';
 		}
 
 		if ( 0 != $this->mNamespace ) {
-			$p .= $this->getNsText() . ':';
+			$nsText = $this->getNsText();
+
+			if ( $nsText === false ) {
+				// See T165149. Awkward, but better than erroneously linking to the main namespace.
+				$nsText = $wgContLang->getNsText( NS_SPECIAL ) . ":Badtitle/NS{$this->mNamespace}";
+			}
+
+			$p .= $nsText . ':';
 		}
 		return $p . $name;
 	}
