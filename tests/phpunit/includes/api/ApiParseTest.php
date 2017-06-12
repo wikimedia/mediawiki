1e2b3fb37f3452ee9037f6efff24d86275308e7b
Brad Jorsch <bjorsch@wikimedia.org>
2017-06-12 11:02:58 -0400

    ApiParse: Clean up parsing code
    Now that ParserOptions->isSafeToCache() exists, use it where necessary.
    This also moves the use inside the makeParserOptions() method so other
    callers can pick it up as well.
    
    Then pass the flag as $forceParse into WikiPage::getParserOutput()
    instead of duplicating the logic in several cases, and generally clean
    up the logic in the module to let WikiPage decide when to use the cache
    in more cases.
    
    Change-Id: I0079e10a40997e4a3b59ac21ef6c92246a147736

--- tests/phpunit/includes/api/ApiParseTest.php
+++ tests/phpunit/includes/api/ApiParseTest.php
@@ -9,18 +9,117 @@
  */
 class ApiParseTest extends ApiTestCase {
 
-	protected function setUp() {
-		parent::setUp();
-		$this->doLogin();
+	protected static $pageId;
+	protected static $revIds = [];
+
+	public function addDBDataOnce() {
+		$user = static::getTestSysop()->getUser();
+		$title = Title::newFromText( __CLASS__ );
+		$page = WikiPage::factory( $title );
+
+		$status = $page->doEditContent(
+			ContentHandler::makeContent( 'Test for revdel', $title, CONTENT_MODEL_WIKITEXT ),
+			__METHOD__ . ' Test for revdel', 0, false, $user
+		);
+		if ( !$status->isOk() ) {
+			$this->fail( "Failed to create $title: " . $status->getWikiText( false, false, 'en' ) );
+		}
+		self::$pageId = $status->value['revision']->getPage();
+		self::$revIds['revdel'] = $status->value['revision']->getId();
+
+		$status = $page->doEditContent(
+			ContentHandler::makeContent( 'Test for oldid', $title, CONTENT_MODEL_WIKITEXT ),
+			__METHOD__ . ' Test for oldid', 0, false, $user
+		);
+		if ( !$status->isOk() ) {
+			$this->fail( "Failed to edit $title: " . $status->getWikiText( false, false, 'en' ) );
+		}
+		self::$revIds['oldid'] = $status->value['revision']->getId();
+
+		$status = $page->doEditContent(
+			ContentHandler::makeContent( 'Test for latest', $title, CONTENT_MODEL_WIKITEXT ),
+			__METHOD__ . ' Test for latest', 0, false, $user
+		);
+		if ( !$status->isOk() ) {
+			$this->fail( "Failed to edit $title: " . $status->getWikiText( false, false, 'en' ) );
+		}
+		self::$revIds['latest'] = $status->value['revision']->getId();
+
+		RevisionDeleter::createList(
+			'revision', RequestContext::getMain(), $title, [ self::$revIds['revdel'] ]
+		)->setVisibility( [
+			'value' => [
+				Revision::DELETED_TEXT => 1,
+			],
+			'comment' => 'Test for revdel',
+		] );
+
+		Title::clearCaches(); // Otherwise it has the wrong latest revision for some reason
 	}
 
-	public function testParseNonexistentPage() {
-		$somePage = mt_rand();
+	public function testParseByName() {
+		$res = $this->doApiRequest( [
+			'action' => 'parse',
+			'page' => __CLASS__,
+		] );
+		$this->assertContains( 'Test for latest', $res[0]['parse']['text'] );
+
+		$res = $this->doApiRequest( [
+			'action' => 'parse',
+			'page' => __CLASS__,
+			'disablelimitreport' => 1,
+		] );
+		$this->assertContains( 'Test for latest', $res[0]['parse']['text'] );
+	}
+
+	public function testParseById() {
+		$res = $this->doApiRequest( [
+			'action' => 'parse',
+			'pageid' => self::$pageId,
+		] );
+		$this->assertContains( 'Test for latest', $res[0]['parse']['text'] );
+	}
+
+	public function testParseByOldId() {
+		$res = $this->doApiRequest( [
+			'action' => 'parse',
+			'oldid' => self::$revIds['oldid'],
+		] );
+		$this->assertContains( 'Test for oldid', $res[0]['parse']['text'] );
+		$this->assertArrayNotHasKey( 'textdeleted', $res[0]['parse'] );
+		$this->assertArrayNotHasKey( 'textsuppressed', $res[0]['parse'] );
+	}
+
+	public function testParseRevDel() {
+		$user = static::getTestUser()->getUser();
+		$sysop = static::getTestSysop()->getUser();
 
 		try {
 			$this->doApiRequest( [
 				'action' => 'parse',
-				'page' => $somePage ] );
+				'oldid' => self::$revIds['revdel'],
+			], null, null, $user );
+			$this->fail( "API did not return an error as expected" );
+		} catch ( ApiUsageException $ex ) {
+			$this->assertTrue( ApiTestCase::apiExceptionHasCode( $ex, 'permissiondenied' ),
+				"API failed with error 'permissiondenied'" );
+		}
+
+		$res = $this->doApiRequest( [
+			'action' => 'parse',
+			'oldid' => self::$revIds['revdel'],
+		], null, null, $sysop );
+		$this->assertContains( 'Test for revdel', $res[0]['parse']['text'] );
+		$this->assertArrayHasKey( 'textdeleted', $res[0]['parse'] );
+		$this->assertArrayNotHasKey( 'textsuppressed', $res[0]['parse'] );
+	}
+
+	public function testParseNonexistentPage() {
+		try {
+			$this->doApiRequest( [
+				'action' => 'parse',
+				'page' => 'DoesNotExist',
+			] );
 
 			$this->fail( "API did not return an error when parsing a nonexistent page" );
 		} catch ( ApiUsageException $ex ) {
