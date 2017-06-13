eb43ebc59d40f8a4ada9cc7fec56cfba4984af75
Chad Horohoe <chadh@wikimedia.org>
2017-06-13 14:42:33 -0700

    LoadMonitor: Group some logging parameters
    Change-Id: Iefd59db547d799a02edc9f733cadb7668176d4cd

--- includes/libs/rdbms/loadmonitor/LoadMonitor.php
+++ includes/libs/rdbms/loadmonitor/LoadMonitor.php
@@ -161,7 +161,11 @@ class LoadMonitor implements ILoadMonitor {
 			if ( !$conn ) {
 				$lagTimes[$i] = false;
 				$host = $this->parent->getServerName( $i );
-				$this->replLogger->error( __METHOD__ . ": host $host is unreachable" );
+				$this->replLogger->error( __METHOD__ . ": ", $host );
+				$this->replLogger->error(
+					__METHOD__ . ": host {db_server} is unreachable",
+					[ 'db_server' => $host ]
+				);
 				continue;
 			}
 
@@ -171,7 +175,10 @@ class LoadMonitor implements ILoadMonitor {
 				$lagTimes[$i] = $conn->getLag();
 				if ( $lagTimes[$i] === false ) {
 					$host = $this->parent->getServerName( $i );
-					$this->replLogger->error( __METHOD__ . ": host $host is not replicating?" );
+					$this->replLogger->error(
+						__METHOD__ . ": host {db_server} is not replicating?",
+						[ 'db_server' => $host ]
+					);
 				}
 			}
 
