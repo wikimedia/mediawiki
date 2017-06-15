5f4434ef2813703e267944a67e7d47393e36e16c
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-06-15 23:55:13 +0300

    Add namespace names for Atikamekw (atj)
    Bug: T167714
    Change-Id: I5e98c80a8365707d73ed7e259466ab57cca7261b

--- languages/messages/MessagesAtj.php
+++ languages/messages/MessagesAtj.php
@@ -9,3 +9,22 @@
  */
 
 $fallback = 'fr';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Tipatcimoctakewin',
+	NS_SPECIAL          => 'Kotakahi',
+	NS_TALK             => 'Ke_ici_aimihitonaniwok',
+	NS_USER             => 'Ka_notcitatc',
+	NS_USER_TALK        => 'Ke_ici_aimihitonaniwok_notcita_iriniw',
+	NS_PROJECT_TALK     => 'Ke_ici_aimihitonaniwok_$1',
+	NS_FILE             => 'Natisinahikaniwoc',
+	NS_FILE_TALK        => 'Ke_ici_aimihitonaniwok_natisinihikaniwoc',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Ke_ici_aimihitonaniwok_MediaWiki',
+	NS_TEMPLATE         => 'Tapapitcikesinihikan',
+	NS_TEMPLATE_TALK    => 'Ke_ici_arimotcikatek_tapapitcikesinihikan',
+	NS_HELP             => 'Witcihikoiin',
+	NS_HELP_TALK        => 'Ke_ici_aimihitonaniwok_witcihewinik',
+	NS_CATEGORY         => 'Tipanictawin',
+	NS_CATEGORY_TALK    => 'Ke_ici_aimihitonaniwok_tipanictawin',
+];
