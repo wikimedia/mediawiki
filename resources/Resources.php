c1756a10885104d69ceeaaeecdbad5c2bf8268c7
jdlrobson <jdlrobson@gmail.com>
2017-06-20 14:28:35 -0700

    Load mediawiki.action.edit.styles on mobile
    Hopefully uncontroversial.
    Mobile non-js editing is currently feature flagged
    by $wgMFAllowNonJavaScriptEditing so this shouldn't have
    any impact on production but will ensure the editor gets
    styled consistently with future changes here.
    
    Change-Id: If8994370e9ec7fc424ce7cb22df922d029cfc035

--- resources/Resources.php
+++ resources/Resources.php
@@ -1421,6 +1421,7 @@ return [
 		],
 	],
 	'mediawiki.action.edit.styles' => [
+		'targets' => [ 'desktop', 'mobile' ],
 		'styles' => 'resources/src/mediawiki.action/mediawiki.action.edit.styles.css',
 	],
 	'mediawiki.action.edit.collapsibleFooter' => [
