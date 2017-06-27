cd8caa1b59d1c4c84bac6f692ecdbe2571592e91
C. Scott Ananian <cscott@cscott.net>
2017-06-27 17:11:31 -0400

    Fix typo in comment.
    Change-Id: Ia656c55ca22fa30b5a911b8cb3a6218c916d3316

--- languages/ConverterRule.php
+++ languages/ConverterRule.php
@@ -230,7 +230,7 @@ class ConverterRule {
 			if ( $disp === false && array_key_exists( $variant, $unidtable ) ) {
 				$disp = array_values( $unidtable[$variant] )[0];
 			}
-			// or display frist text under disable manual convert
+			// or display first text under disable manual convert
 			if ( $disp === false && $this->mConverter->mManualLevel[$variant] == 'disable' ) {
 				if ( count( $bidtable ) > 0 ) {
 					$disp = array_values( $bidtable )[0];
