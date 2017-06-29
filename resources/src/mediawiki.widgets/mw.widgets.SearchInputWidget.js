95c3cc580639ca8a718d4ea83c285f12cc0bd7a4
Bartosz Dziewoński <matma.rex@gmail.com>
2017-06-29 21:57:05 +0200

    SearchInputWidget: Fix documented default for 'pushPending' option
    Change-Id: Iadee48af255498c6608cfd00fca29eed9b14f1be

--- resources/src/mediawiki.widgets/mw.widgets.SearchInputWidget.js
+++ resources/src/mediawiki.widgets/mw.widgets.SearchInputWidget.js
@@ -14,7 +14,7 @@
 	 *
 	 * @constructor
 	 * @param {Object} [config] Configuration options
-	 * @cfg {boolean} [pushPending=true] Visually mark the input field as "pending", while
+	 * @cfg {boolean} [pushPending=false] Visually mark the input field as "pending", while
 	 *  requesting suggestions.
 	 * @cfg {boolean} [performSearchOnClick=true] If true, the script will start a search when-
 	 *  ever a user hits a suggestion. If false, the text of the suggestion is inserted into the
