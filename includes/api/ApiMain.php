5104a2bb8b151456442637317aee77ab96b03ccb
Aaron Schulz <aschulz@wikimedia.org>
2017-06-30 15:01:33 -0700

    Always log exceptions in rollbackMasterChangesAndLog()
    MWExceptionHandler::rollbackMasterChangesAndLog() only logged exceptions
    if there were already master changes. This is extremely problematic when
    debugging, especially in situations like DeferredUpdates where they were
    silently being swallowed.
    
    This makes it log exceptions in all paths, erring on the side of logging
    the same exception twice (theoretically it's possible I suppose) instead
    of not at all.
    
    Also make the method able to handle DBError exceptions, which most of
    the callers seemed to be assuming. ApiMain was handling this explicitly.
    
    Bug: T168347
    Change-Id: I8739051f824a455ba669344184c3b11ac95cb561

--- includes/api/ApiMain.php
+++ includes/api/ApiMain.php
@@ -581,13 +581,7 @@ class ApiMain extends ApiBase {
 		// T65145: Rollback any open database transactions
 		if ( !( $e instanceof ApiUsageException || $e instanceof UsageException ) ) {
 			// UsageExceptions are intentional, so don't rollback if that's the case
-			try {
-				MWExceptionHandler::rollbackMasterChangesAndLog( $e );
-			} catch ( DBError $e2 ) {
-				// Rollback threw an exception too. Log it, but don't interrupt
-				// our regularly scheduled exception handling.
-				MWExceptionHandler::logException( $e2 );
-			}
+			MWExceptionHandler::rollbackMasterChangesAndLog( $e );
 		}
 
 		// Allow extra cleanup and logging
