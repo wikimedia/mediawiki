50315dbbf16bf3ae29409cf22a87f75b33a1db79
Brad Jorsch <bjorsch@wikimedia.org>
2017-06-30 14:19:38 -0400

    Add ResourceLoaderModule::shouldEmbedModule()
    Rather than only the 'private' group triggering embedding, allow modules
    to explicitly specify if they should be embedded.
    
    The default is still to embed when the group is 'private', and the
    'private' group is still special in that ResourceLoader::respond() will
    still refuse to serve it from load.php.
    
    Change-Id: Ib9a043c566822e278baecc15e87f9c5cebc2eb98

--- tests/phpunit/includes/resourceloader/ResourceLoaderClientHtmlTest.php
+++ tests/phpunit/includes/resourceloader/ResourceLoaderClientHtmlTest.php
@@ -43,6 +43,7 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.top' => [ 'position' => 'top' ],
 			'test.private.top' => [ 'group' => 'private', 'position' => 'top' ],
 			'test.private.bottom' => [ 'group' => 'private', 'position' => 'bottom' ],
+			'test.shouldembed' => [ 'shouldEmbed' => true ],
 
 			'test.styles.pure' => [ 'type' => ResourceLoaderModule::LOAD_STYLES ],
 			'test.styles.mixed' => [],
@@ -64,12 +65,18 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 				'group' => 'private',
 				'styles' => '.private{}',
 			],
+			'test.styles.shouldembed' => [
+				'type' => ResourceLoaderModule::LOAD_STYLES,
+				'shouldEmbed' => true,
+				'styles' => '.shouldembed{}',
+			],
 
 			'test.scripts' => [],
 			'test.scripts.top' => [ 'position' => 'top' ],
 			'test.scripts.user' => [ 'group' => 'user' ],
 			'test.scripts.user.empty' => [ 'group' => 'user', 'isKnownEmpty' => true ],
 			'test.scripts.raw' => [ 'isRaw' => true ],
+			'test.scripts.shouldembed' => [ 'shouldEmbed' => true ],
 		];
 		return array_map( function ( $options ) {
 			return self::makeModule( $options );
@@ -102,6 +109,7 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.private.bottom',
 			'test.private.top',
 			'test.top',
+			'test.shouldembed',
 			'test.unregistered',
 		] );
 		$client->setModuleStyles( [
@@ -109,12 +117,14 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'test.styles.user.empty',
 			'test.styles.private',
 			'test.styles.pure',
+			'test.styles.shouldembed',
 			'test.unregistered.styles',
 		] );
 		$client->setModuleScripts( [
 			'test.scripts',
 			'test.scripts.user.empty',
 			'test.scripts.top',
+			'test.scripts.shouldembed',
 			'test.unregistered.scripts',
 		] );
 
@@ -122,12 +132,15 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'states' => [
 				'test.private.top' => 'loading',
 				'test.private.bottom' => 'loading',
+				'test.shouldembed' => 'loading',
 				'test.styles.pure' => 'ready',
 				'test.styles.user.empty' => 'ready',
 				'test.styles.private' => 'ready',
+				'test.styles.shouldembed' => 'ready',
 				'test.scripts' => 'loading',
 				'test.scripts.top' => 'loading',
 				'test.scripts.user.empty' => 'ready',
+				'test.scripts.shouldembed' => 'loading',
 			],
 			'general' => [
 				'test',
@@ -139,12 +152,14 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 			'scripts' => [
 				'test.scripts',
 				'test.scripts.top',
+				'test.scripts.shouldembed',
 			],
 			'embed' => [
-				'styles' => [ 'test.styles.private' ],
+				'styles' => [ 'test.styles.private', 'test.styles.shouldembed' ],
 				'general' => [
 					'test.private.bottom',
 					'test.private.top',
+					'test.shouldembed',
 				],
 			],
 		];
@@ -275,6 +290,37 @@ class ResourceLoaderClientHtmlTest extends PHPUnit_Framework_TestCase {
 				'modules' => [ 'test.styles.noscript' ],
 				'only' => ResourceLoaderModule::TYPE_STYLES,
 				'output' => '<noscript><link rel="stylesheet" href="/w/load.php?debug=false&amp;lang=nl&amp;modules=test.styles.noscript&amp;only=styles&amp;skin=fallback"/></noscript>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_COMBINED,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",function($,jQuery,require,module){},{"css":[]});});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.styles.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_STYLES,
+				'output' => '<style>.shouldembed{}</style>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.scripts.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_SCRIPTS,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.state({"test.scripts.shouldembed":"ready"});});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test', 'test.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_COMBINED,
+				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",function($,jQuery,require,module){},{"css":[]});mw.loader.load("/w/load.php?debug=false\u0026lang=nl\u0026modules=test\u0026skin=fallback");});</script>',
+			],
+			[
+				'context' => [],
+				'modules' => [ 'test.styles.pure', 'test.styles.shouldembed' ],
+				'only' => ResourceLoaderModule::TYPE_STYLES,
+				'output' => '<style>.shouldembed{}</style>' . "\n"
+					. '<link rel="stylesheet" href="/w/load.php?debug=false&amp;lang=nl&amp;modules=test.styles.pure&amp;only=styles&amp;skin=fallback"/>',
 			],
 			// @codingStandardsIgnoreEnd
 		];
