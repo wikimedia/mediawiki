fe96716a155f930f773640aec5c9b65447207417
Fomafix <fomafix@googlemail.com>
2017-07-03 08:51:10 +0200

    Use double quotes instead of singe quotes for HTML attributes
    htmlspecialchars[1] encodes only double quotes not single quotes.
    Therefor the HTML attribute must enclosed with double quotes for
    correct output encoding.
    
    [1] https://secure.php.net/manual/en/function.htmlspecialchars.php
    
    Change-Id: I3fec5299585187ed70d62b9248007af34b0db85b

--- includes/diff/DifferenceEngine.php
+++ includes/diff/DifferenceEngine.php
@@ -1145,7 +1145,7 @@ class DifferenceEngine extends ContextSource {
 
 		if ( !$diff && !$otitle ) {
 			$header .= "
-			<tr style='vertical-align: top;' lang='{$userLang}'>
+			<tr style='vertical-align: top;' lang=\"{$userLang}\">
 			<td class='diff-ntitle'>{$ntitle}</td>
 			</tr>";
 			$multiColspan = 1;
@@ -1164,7 +1164,7 @@ class DifferenceEngine extends ContextSource {
 			}
 			if ( $otitle || $ntitle ) {
 				$header .= "
-				<tr style='vertical-align: top;' lang='{$userLang}'>
+				<tr style='vertical-align: top;' lang=\"{$userLang}\">
 				<td colspan='$colspan' class='diff-otitle'>{$otitle}</td>
 				<td colspan='$colspan' class='diff-ntitle'>{$ntitle}</td>
 				</tr>";
@@ -1173,11 +1173,11 @@ class DifferenceEngine extends ContextSource {
 
 		if ( $multi != '' ) {
 			$header .= "<tr><td colspan='{$multiColspan}' style='text-align: center;' " .
-				"class='diff-multi' lang='{$userLang}'>{$multi}</td></tr>";
+				"class='diff-multi' lang=\"{$userLang}\">{$multi}</td></tr>";
 		}
 		if ( $notice != '' ) {
 			$header .= "<tr><td colspan='{$multiColspan}' style='text-align: center;' " .
-				"lang='{$userLang}'>{$notice}</td></tr>";
+				"lang=\"{$userLang}\">{$notice}</td></tr>";
 		}
 
 		return $header . $diff . "</table>";
