c8c255535cf5c4e41a3620775cb78ec2478d9d12
addshore <addshorewiki@gmail.com>
2017-07-04 22:07:04 +0100

    Factor UploadImporter & WikiRevisionUpload out of WikiRevision
    Change-Id: I4a6c573fc0a69b06d696cd2afca9226fb492a9bc

---
+++ includes/import/WikiRevisionUploadImporter.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @since 1.31
+ */
+interface WikiRevisionUploadImporter {
+
+	/**
+	 * @since 1.31
+	 *
+	 * @param WikiRevisionUpload $wikiRevision
+	 *
+	 * @return StatusValue On success, the value member contains the
+	 *     archive name, or an empty string if it was a new file.
+	 */
+	public function import( WikiRevisionUpload $wikiRevision );
+
+}
