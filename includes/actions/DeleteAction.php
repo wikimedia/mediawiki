51be1c92de17b7906a6610fb0a69a9a000d5a4ef
Bartosz Dziewoński <matma.rex@gmail.com>
2017-07-12 20:46:15 +0200

    DeleteAction: Remove now-unnecessary RL modules
    After 97a8fb78faa4ca2d9604140e5547f5deae5a2524 these are never used.
    
    Change-Id: Ibb4ee45ad991651afab0f0363056d3066e740b24

--- includes/actions/DeleteAction.php
+++ includes/actions/DeleteAction.php
@@ -42,14 +42,6 @@ class DeleteAction extends FormlessAction {
 
 	public function show() {
 		$this->useTransactionalTimeLimit();
-
-		$out = $this->getOutput();
-		if ( $this->getContext()->getConfig()->get( 'UseMediaWikiUIEverywhere' ) ) {
-			$out->addModuleStyles( [
-				'mediawiki.ui.input',
-				'mediawiki.ui.checkbox',
-			] );
-		}
 		$this->addHelpLink( 'Help:Sysop deleting and undeleting' );
 		$this->page->delete();
 	}
