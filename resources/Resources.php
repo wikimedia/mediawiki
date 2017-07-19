cc45510b9f135ec049cc69d5a04f060f7c955a46
EBernhardson <ebernhardson@wikimedia.org>
2017-07-19 21:36:17 +0000

    Revert "Make mw.widgets.SearchInputWidget extend OO.ui.SearchInputWidget"
    Typing a search query into the main box on Special:Search
    causes the search to be performed for the first autocomplete
    result, rather than the typed query. The first autocomplet 
    result should only be searched for if explicitly selected with
    the mouse or arrow key.
    
    This reverts commit 7882e3b66060f7bbd37e15688baf4dd30954b1b7.
    
    Bug: T171112
    Change-Id: I1af6ba90542fafe3ed1aeca420e9d6df6612f7d0
    (cherry picked from commit baf123f07d50d29ac4161acac536b579fc31a33b)

--- resources/Resources.php
+++ resources/Resources.php
@@ -2546,9 +2546,9 @@ return [
 		],
 		'dependencies' => [
 			'mediawiki.searchSuggest',
-			// FIXME: Needs TitleWidget only
+			'oojs-ui.styles.icons-interactions',
+			// FIXME: Needs TitleInputWidget only
 			'mediawiki.widgets',
-			'oojs-ui-widgets',
 		],
 	],
 	'mediawiki.widgets.SearchInputWidget.styles' => [
