edeeb404f9d3aa082524987644d07960cc3d0348
Prateek Saxena <prtksxna@gmail.com>
2017-07-20 23:10:35 +0530

    Special:AllPages: Use OOUI
    * Don't use vertical-labels
    * Set a max-width on the fieldLayout-header
    
    Bug: T117724
    Change-Id: I1e9b45d687492eed290a825de78c6ecc61013da8

--- includes/specials/SpecialAllPages.php
+++ includes/specials/SpecialAllPages.php
@@ -112,6 +112,7 @@ class SpecialAllPages extends IncludableSpecialPage {
 				'id' => 'nsfrom',
 				'size' => 30,
 				'label-message' => 'allpagesfrom',
+				'vertical-label' => false,
 				'default' => str_replace( '_', ' ', $from ),
 			],
 			'to' => [
@@ -120,6 +121,7 @@ class SpecialAllPages extends IncludableSpecialPage {
 				'id' => 'nsto',
 				'size' => 30,
 				'label-message' => 'allpagesto',
+				'vertical-label' => false,
 				'default' => str_replace( '_', ' ', $to ),
 			],
 			'namespace' => [
@@ -127,6 +129,7 @@ class SpecialAllPages extends IncludableSpecialPage {
 				'name' => 'namespace',
 				'id' => 'namespace',
 				'label-message' => 'namespace',
+				'vertical-label' => false,
 				'all' => null,
 				'value' => $namespace,
 			],
@@ -143,7 +146,7 @@ class SpecialAllPages extends IncludableSpecialPage {
 			unset( $fields['hideredirects'] );
 		}
 
-		$form = HTMLForm::factory( 'table', $fields, $this->getContext() );
+		$form = HTMLForm::factory( 'ooui', $fields, $this->getContext() );
 		$form->setMethod( 'get' )
 			->setWrapperLegendMsg( 'allpages' )
 			->setSubmitTextMsg( 'allpagessubmit' )
