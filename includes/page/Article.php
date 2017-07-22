2bbf9320e5b91339dbfa7200efd2f1ebea116cdf
Étienne Beaulé <beauleetienne0@gmail.com>
2017-07-22 13:46:36 -0300

    Show protection log on creation-protected pages
    This change would add the message 'titleprotectedwarning'
    to pages that have been protected against creation, just
    like how deleted pages have their log visible beforere-creation.
    It is visible to everyone, whether or not they are allowed to
    recreate the page.
    
    Bug: T171338
    Change-Id: I5acf1bee9cea056b20b77fd386bc9df65033cded

--- includes/page/Article.php
+++ includes/page/Article.php
@@ -1180,7 +1180,7 @@ class Article implements Page {
 		$key = $cache->makeKey( 'page-recent-delete', md5( $title->getPrefixedText() ) );
 		$loggedIn = $this->getContext()->getUser()->isLoggedIn();
 		if ( $loggedIn || $cache->get( $key ) ) {
-			$logTypes = [ 'delete', 'move' ];
+			$logTypes = [ 'delete', 'move', 'protect' ];
 
 			$dbr = wfGetDB( DB_REPLICA );
 
