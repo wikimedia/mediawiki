812c92cde5f6e886dcdd3060d59beff45b4191ed
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-07-22 11:01:45 +0300

    Add Saraiki (skr) to Names.php
    Bug: T171347
    Change-Id: I145485b3d94ab2154a7dadbda166b16207f7c21c

---
+++ languages/messages/MessagesSkr.php
@@ -0,0 +1,11 @@
+<?php
+/** Saraiki (multiple scripts)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'skr-arab';
