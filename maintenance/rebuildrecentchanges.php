2ee730512ac41ced15bc3ba76fcb14d16eeaa083
GeoffreyT2000 <geoffreytrang@gmail.com>
2017-07-27 18:44:21 -0700

    Don't generate RC entries for filterable log types
    This change is needed so that filterable log types (e.g. patrol) will not get corresponding recent changes entries after running rebuildrecentchanges.php.
    
    Bug: T20364
    Change-Id: Id18364599b44d16f867f1042e1dc7e8f51b1b369

--- maintenance/rebuildrecentchanges.php
+++ maintenance/rebuildrecentchanges.php
@@ -267,7 +267,7 @@ class RebuildRecentchanges extends Maintenance {
 	 * Rebuild pass 3: Insert `recentchanges` entries for action logs.
 	 */
 	private function rebuildRecentChangesTablePass3() {
-		global $wgLogTypes, $wgLogRestrictions;
+		global $wgLogTypes, $wgLogRestrictions, $wgFilterLogTypes;
 
 		$dbw = $this->getDB( DB_MASTER );
 		$logCommentStore = new CommentStore( 'log_comment' );
@@ -295,9 +295,9 @@ class RebuildRecentchanges extends Maintenance {
 				'log_timestamp > ' . $dbw->addQuotes( $dbw->timestamp( $this->cutoffFrom ) ),
 				'log_timestamp < ' . $dbw->addQuotes( $dbw->timestamp( $this->cutoffTo ) ),
 				'log_user=user_id',
-				// Some logs don't go in RC since they are private.
+				// Some logs don't go in RC since they are private, or can be filtered out.
 				// @FIXME: core/extensions also have spammy logs that don't go in RC.
-				'log_type' => array_diff( $wgLogTypes, array_keys( $wgLogRestrictions ) ),
+				'log_type' => array_diff( $wgLogTypes, array_keys( array_merge( $wgLogRestrictions, $wgFilterLogtypes ) ) ),
 			],
 			__METHOD__,
 			[ 'ORDER BY' => 'log_timestamp DESC' ],
