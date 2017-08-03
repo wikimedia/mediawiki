0612f37769cc63f610dd772f4637d4b77c973e04
Stephane Bisson <sbisson@wikimedia.org>
2017-08-03 13:11:53 -0400

    Enable RCFilters app on Watchlist
    * Add classes prefixed with cl (for ChangesList)
      to both RC and WL so that the JS app can locate
      similar elements using the same selectors
    
    * Make saved queries preference name configurable
      so that RC and WL use different preferences
    
    * Move some code from SpecialRecentchanges.php to
      its base class so it's accessible to SpecialWatchlist.php
    
    To use the RCFilters app on WL, append ?rcfilters=1 to the URL
    
    Bug: T171132
    Bug: T171218
    Change-Id: If7c63284224df24fa02022b4ea74bef116f28ca6

--- resources/src/mediawiki.rcfilters/mw.rcfilters.Controller.js
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.Controller.js
@@ -8,11 +8,15 @@
 	 * @param {mw.rcfilters.dm.FiltersViewModel} filtersModel Filters view model
 	 * @param {mw.rcfilters.dm.ChangesListViewModel} changesListModel Changes list view model
 	 * @param {mw.rcfilters.dm.SavedQueriesModel} savedQueriesModel Saved queries model
+	 * @param {Object} config Additional configuration
+	 * @cfg {string} savedQueriesPreferenceName Where to save the saved queries
 	 */
-	mw.rcfilters.Controller = function MwRcfiltersController( filtersModel, changesListModel, savedQueriesModel ) {
+	mw.rcfilters.Controller = function MwRcfiltersController( filtersModel, changesListModel, savedQueriesModel, config ) {
 		this.filtersModel = filtersModel;
 		this.changesListModel = changesListModel;
 		this.savedQueriesModel = savedQueriesModel;
+		this.savedQueriesPreferenceName = config.savedQueriesPreferenceName;
+
 		this.requestCounter = {};
 		this.baseFilterState = {};
 		this.uriProcessor = null;
@@ -209,7 +213,7 @@
 		);
 
 		try {
-			parsedSavedQueries = JSON.parse( mw.user.options.get( 'rcfilters-saved-queries' ) || '{}' );
+			parsedSavedQueries = JSON.parse( mw.user.options.get( this.savedQueriesPreferenceName ) || '{}' );
 		} catch ( err ) {
 			parsedSavedQueries = {};
 		}
@@ -254,7 +258,7 @@
 			// so it gets processed
 			this.changesListModel.update(
 				$changesList.length ? $changesList : 'NO_RESULTS',
-				$( 'fieldset.rcoptions' ).first(),
+				$( 'fieldset.cloptions' ).first(),
 				true // We're using existing DOM elements
 			);
 		}
@@ -868,9 +872,9 @@
 		}
 
 		// Save the preference
-		new mw.Api().saveOption( 'rcfilters-saved-queries', stringified );
+		new mw.Api().saveOption( this.savedQueriesPreferenceName, stringified );
 		// Update the preference for this session
-		mw.user.options.set( 'rcfilters-saved-queries', stringified );
+		mw.user.options.set( this.savedQueriesPreferenceName, stringified );
 	};
 
 	/**
@@ -1163,7 +1167,7 @@
 						// Changes list
 						changes: $parsed.find( '.mw-changeslist' ).first().contents(),
 						// Fieldset
-						fieldset: $parsed.find( 'fieldset.rcoptions' ).first()
+						fieldset: $parsed.find( 'fieldset.cloptions' ).first()
 					};
 				},
 				// Failure
@@ -1179,7 +1183,7 @@
 					// Force a resolve state to this promise
 					return $.Deferred().resolve( {
 						changes: 'NO_RESULTS',
-						fieldset: $parsed.find( 'fieldset.rcoptions' ).first()
+						fieldset: $parsed.find( 'fieldset.cloptions' ).first()
 					} ).promise();
 				}
 			);
