fda9cb93318e31d2b0adb8651890665fbb347600
Huji Lee <huji.huji@gmail.com>
2017-08-03 22:29:31 -0400

    Links to blocked/locked user accounts should have a class indicating so
    Bug: T172348
    Change-Id: I14ef2f6411f512578acc4777bf7771c89233dfce

--- includes/Linker.php
+++ includes/Linker.php
@@ -39,6 +39,11 @@ class Linker {
 	const TOOL_LINKS_EMAIL = 2;
 
 	/**
+	 * Cache for user block status
+	 */
+	protected static $mUserBlockStatus = [];
+
+	/**
 	 * Return the CSS colour of a known link
 	 *
 	 * @deprecated since 1.28, use LinkRenderer::getLinkClasses() instead
@@ -883,6 +888,38 @@ class Linker {
 	}
 
 	/**
+	 * Get the block/lock status of an acccount from a cache or the database
+	 * @param int $userId User id in database.
+	 * @return array
+	 */
+	public static function getUserBlockStatus( $userId ) {
+		if ( $userId === 0 ) {
+			return false;
+		}
+
+		$output = [];
+
+		if ( array_key_exists ( $userId, self::$mUserBlockStatus ) ) {
+			$cachedStatus = self::$mUserBlockStatus[ $userId ];
+			$output[ 'blocked' ] = $cachedStatus[ 'blocked' ];
+			$output[ 'globally-blocked' ] = $cachedStatus[ 'globally-blocked' ];
+			$output[ 'locked' ] = $cachedStatus[ 'locked' ];
+		} else {
+			$user = User::newFromId( $userId );
+
+			$output[ 'blocked' ] = $user->getBlock() !== null;
+			$output[ 'globally-blocked' ] = $user->isBlockedGlobally();
+			$output[ 'locked' ] = $user->isLocked();
+
+			self::$mUserBlockStatus[ $userId ][ 'blocked' ] = $output[ 'blocked' ];
+			self::$mUserBlockStatus[ $userId ][ 'globally-blocked' ] = $output[ 'globally-blocked' ];
+			self::$mUserBlockStatus[ $userId ][ 'locked' ] = $output[ 'locked' ];
+		}
+
+		return $output;
+	}
+
+	/**
 	 * Make user link (or user contributions for unregistered users)
 	 * @param int $userId User id in database.
 	 * @param string $userName User name in database.
@@ -900,6 +937,22 @@ class Linker {
 			$classes .= ' mw-anonuserlink'; // Separate link class for anons (T45179)
 		} else {
 			$page = Title::makeTitle( NS_USER, $userName );
+		}
+
+		if( $userId != 0 ) {
+			$blockStatus = self::getUserBlockStatus( $userId );
+
+			if ( $blockStatus[ 'blocked' ] ) {
+				$classes .= ' mw-userlink-blocked';
+			}
+
+			if ( $blockStatus[ 'globally-blocked' ] ) {
+				$classes .= ' mw-userlink-globally-blocked';
+			}
+
+			if ( $blockStatus[ 'locked' ] ) {
+				$classes .= ' mw-userlink-locked';
+			}
 		}
 
 		// Wrap the output with <bdi> tags for directionality isolation
@@ -960,11 +1013,17 @@ class Linker {
 			$items[] = self::emailLink( $userId, $userText );
 		}
 
+		$classes = 'mw-usertoollinks';
+		$user = User::newFromId( $userId );
+		if ( $user->getBlock() !== null || $user->isBlockedGlobally( $userText ) ) {
+			$classes .= ' mw-user-link-blocked';
+		}
+
 		Hooks::run( 'UserToolLinksEdit', [ $userId, $userText, &$items ] );
 
 		if ( $items ) {
 			return wfMessage( 'word-separator' )->escaped()
-				. '<span class="mw-usertoollinks">'
+				. '<span class="' . $classes . '">'
 				. wfMessage( 'parentheses' )->rawParams( $wgLang->pipeList( $items ) )->escaped()
 				. '</span>';
 		} else {
