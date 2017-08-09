e360037ca953b3c72f5d40906314c2e2f84fb15b
HakanIST <ozdemir@gmail.com>
2017-08-09 17:37:16 -0400

    Fixed localization for time according to Turkish language usage.
    Bug: T161961
    Change-Id: I344cc500a780ff81f69e217c970a4a6aff288720

--- languages/messages/MessagesTr.php
+++ languages/messages/MessagesTr.php
@@ -352,5 +352,37 @@ $magicWords = [
 	'pagesincategory_files'     => [ '0', 'dosyalar', 'files' ],
 ];
 
+$datePreferences = [
+	'default',
+	'mdy',
+	'dmy',
+	'ymd',
+	'turkish',
+	'ISO 8601',
+];
+
+$dateFormats = [
+	'mdy time' => 'H.i',
+	'mdy date' => 'F j, Y',
+	'mdy monthonly' => 'F Y',
+	'mdy both' => 'H.i, F j, Y',
+	'mdy pretty' => 'F j',
+	'dmy time' => 'H.i',
+	'dmy date' => 'j F Y',
+	'dmy monthonly' => 'F Y',
+	'dmy both' => 'H.i, j F Y',
+	'dmy pretty' => 'j F',
+	'ymd time' => 'H.i',
+	'ymd date' => 'Y F j',
+	'ymd monthonly' => 'Y F',
+	'ymd both' => 'H.i, Y F j',
+	'ymd pretty' => 'F j',
+	'ISO 8601 time' => 'xnH:xni:xns',
+	'ISO 8601 date' => 'xnY-xnm-xnd',
+	'ISO 8601 monthonly' => 'xnY-xnm',
+	'ISO 8601 both' => 'xnY-xnm-xnd"T"xnH:xni:xns',
+	'ISO 8601 pretty' => 'xnm-xnd'
+];
+
 $separatorTransformTable = [ ',' => '.', '.' => ',' ];
 $linkTrail = '/^([a-zÇĞçğİıÖöŞşÜüÂâÎîÛû]+)(.*)$/sDu';
