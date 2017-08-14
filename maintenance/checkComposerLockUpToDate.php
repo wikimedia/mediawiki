69c003591dcc6fece31372cabe061dc704c4a250
Derk-Jan Hartman <hartman.wiki@gmail.com>
2017-08-14 15:02:01 -0400

    update.php: Check for outdated composer deps of extensions
    Bug: T119711
    Change-Id: I6d4f427eb255b6638ac51ae5537d62d2fe54d37d

--- maintenance/checkComposerLockUpToDate.php
+++ maintenance/checkComposerLockUpToDate.php
@@ -10,24 +10,51 @@ require_once __DIR__ . '/Maintenance.php';
  * the composer.json file to see if dependencies are up to date.
  */
 class CheckComposerLockUpToDate extends Maintenance {
+
 	public function __construct() {
 		parent::__construct();
 		$this->addDescription(
 			'Checks whether your composer.lock file is up to date with the current composer.json' );
+		$this->addOption( 'without-extensions', 'Also recurse the extensions folder' );
 	}
 
 	public function execute() {
 		global $IP;
-		$lockLocation = "$IP/composer.lock";
-		$jsonLocation = "$IP/composer.json";
+
+		$this->updateComposerDirectory( "$IP", true );
+
+		if ( $this->hasOption( 'without-extensions' ) ) {
+			return;
+		}
+
+		$installedExtensions = ExtensionRegistry::getInstance()->getAllThings();
+
+		foreach ( $installedExtensions as $extensionName => $extensionInfo ) {
+			$directory = dirname( $extensionInfo[ 'path' ] );
+			if ( !$this->isSuitablePath( $directory ) ) {
+				continue;
+			}
+			$this->output( "Checking composer dependencies for: $extensionName\n" );
+			$this->updateComposerDirectory( dirname( $extensionInfo[ 'path' ] ) );
+		}
+	}
+
+	public function updateComposerDirectory( $directory, $root = false ) {
+		global $IP;
+		$lockLocation = "$directory/composer.lock";
+		$jsonLocation = "$directory/composer.json";
+		$workingDir = $root ? '' : " -d \"{$directory}\"";
+
 		if ( !file_exists( $lockLocation ) ) {
 			// Maybe they're using mediawiki/vendor?
 			$lockLocation = "$IP/vendor/composer.lock";
 			if ( !file_exists( $lockLocation ) ) {
 				$this->error(
-					'Could not find composer.lock file. Have you run "composer install"?',
-					1
+					"Could not find composer.lock file.\n" .
+					"Have you run 'composer install{$workingDir}' ?\n",
+					$root ? true : false
 				);
+				return;
 			}
 		}
 
@@ -53,13 +80,27 @@ class CheckComposerLockUpToDate extends Maintenance {
 		if ( $found ) {
 			$this->error(
 				'Error: your composer.lock file is not up to date. ' .
-					'Run "composer update" to install newer dependencies',
+					"Run 'composer update{$workingDir}' to install newer dependencies",
 				1
 			);
 		} else {
 			// We couldn't find any out-of-date dependencies, so assume everything is ok!
 			$this->output( "Your composer.lock file is up to date with current dependencies!\n" );
 		}
+	}
+
+	/**
+	 * Returns true if $directory is of a type we can check
+	 * @param string $directory
+	 * @return bool
+	 */
+	private function isSuitablePath( $directory ) {
+		$directory = str_replace( '\\', '/', $directory );
+		if ( file_exists( "$directory/composer.json" ) ) {
+			return true;
+		}
+
+		return false;
 	}
 }
 
