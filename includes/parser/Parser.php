ffb06c1b5ce2fdaa1709a1002ee65e8c1b54bfed
Matthew Bowker <matthewrbowker@me.com>
2017-08-16 02:28:59 -0600

    Remove two deprecated functions and one depreciated variable in a function call within Parser.php
    * Parser::getRandomString() (deprecated in 1.26) was removed.
    * Parser::uniqPrefix() (deprecated in 1.26) was removed.
    * Parser::extractTagsAndParams() now only accepts three arguments.  The fourth, $uniq_prefix was deprecated in 1.26 and has now been removed.
    
    Bug: T61113
    Change-Id: I7333fff4eb8b9a754b4596992f2a69bbdaac664d

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -737,17 +737,6 @@ class Parser {
 	}
 
 	/**
-	 * Get a random string
-	 *
-	 * @return string
-	 * @deprecated since 1.26; use wfRandomString() instead.
-	 */
-	public static function getRandomString() {
-		wfDeprecated( __METHOD__, '1.26' );
-		return wfRandomString( 16 );
-	}
-
-	/**
 	 * Set the current user.
 	 * Should only be used when doing pre-save transform.
 	 *
@@ -755,17 +744,6 @@ class Parser {
 	 */
 	public function setUser( $user ) {
 		$this->mUser = $user;
-	}
-
-	/**
-	 * Accessor for mUniqPrefix.
-	 *
-	 * @return string
-	 * @deprecated since 1.26; use Parser::MARKER_PREFIX instead.
-	 */
-	public function uniqPrefix() {
-		wfDeprecated( __METHOD__, '1.26' );
-		return self::MARKER_PREFIX;
 	}
 
 	/**
@@ -973,14 +951,9 @@ class Parser {
 	 * @param array $elements List of element names. Comments are always extracted.
 	 * @param string $text Source text string.
 	 * @param array &$matches Out parameter, Array: extracted tags
-	 * @param string|null $uniq_prefix
 	 * @return string Stripped text
-	 * @since 1.26 The uniq_prefix argument is deprecated.
 	 */
-	public static function extractTagsAndParams( $elements, $text, &$matches, $uniq_prefix = null ) {
-		if ( $uniq_prefix !== null ) {
-			wfDeprecated( __METHOD__ . ' called with $prefix argument', '1.26' );
-		}
+	public static function extractTagsAndParams( $elements, $text, &$matches ) {
 		static $n = 1;
 		$stripped = '';
 		$matches = [];
