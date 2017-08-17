wikimedia/mediawiki

commit 4475e1c8cb4828d1e560afd87dcd715585397de3

Special:Newpages feed now shows first revision instead of latest revision
Bug: T69026
Change-Id: I1f794f314319127848a4e9981c7d4f549fb4fd0f

includes/specials/SpecialNewpages.php
@@ -294,6 +294,19 @@ class SpecialNewpages extends IncludableSpecialPage {
 	}
 
 	/**
+	 * @param stdClass $row Result row from recent changes
+	 * @return Revision|bool
+	 */
+	protected function revisionFromRcResult( stdClass $result ) {
+		return new Revision( [
+			'comment' => $result->rc_comment,
+			'deleted' => $result->rc_deleted,
+			'user_text' => $result->rc_user_text,
+			'user' => $result->rc_user,
+		] );
+	}
+
+	/**
 	 * Format a row, providing the timestamp, links to the page/history,
 	 * size, user links, and a comment
 	 *
@@ -303,14 +316,9 @@ class SpecialNewpages extends IncludableSpecialPage {
 	public function formatRow( $result ) {
 		$title = Title::newFromRow( $result );
 
-		# Revision deletion works on revisions, so we should cast one
-		$row = [
-			'comment' => $result->rc_comment,
-			'deleted' => $result->rc_deleted,
-			'user_text' => $result->rc_user_text,
-			'user' => $result->rc_user,
-		];
-		$rev = new Revision( $row );
+		// Revision deletion works on revisions,
+		// so cast our recent change row to a revision row.
+		$rev = $this->revisionFromRcResult( $result );
 		$rev->setTitle( $title );
 
 		$classes = [];
@@ -477,7 +485,7 @@ class SpecialNewpages extends IncludableSpecialPage {
 	}
 
 	protected function feedItemDesc( $row ) {
-		$revision = Revision::newFromId( $row->rev_id );
+		$revision = $this->revisionFromRcResult( $row );
 		if ( $revision ) {
 			// XXX: include content model/type in feed item?
 			return '<p>' . htmlspecialchars( $revision->getUserText() ) .

commit f45b40fe531a8fa5324ebf4bc8a803ff694b044f

Restore the newFromId() approach in SpecialNewpages::feedItemDesc
Trying to use the row data is problematic since the text related
fields are missing. This fixing a regression from 4475e1c8c.

Also make SpecialNewpages::feedItemDesc() handle null content.

Bug: T173541
Change-Id: I2213675d3392c6e9761bdc7acde35fd1caee4517

includes/specials/SpecialNewpages.php
@@ -494,17 +494,22 @@ class SpecialNewpages extends IncludableSpecialPage {
 	}
 
 	protected function feedItemDesc( $row ) {
-		$revision = $this->revisionFromRcResult( $row );
-		if ( $revision ) {
-			// XXX: include content model/type in feed item?
-			return '<p>' . htmlspecialchars( $revision->getUserText() ) .
-				$this->msg( 'colon-separator' )->inContentLanguage()->escaped() .
-				htmlspecialchars( FeedItem::stripComment( $revision->getComment() ) ) .
-				"</p>\n<hr />\n<div>" .
-				nl2br( htmlspecialchars( $revision->getContent()->serialize() ) ) . "</div>";
+		$revision = Revision::newFromId( $row->rev_id );
+		if ( !$revision ) {
+			return '';
 		}
 
-		return '';
+		$content = $revision->getContent();
+		if ( $content === null ) {
+			return '';
+		}
+
+		// XXX: include content model/type in feed item?
+		return '<p>' . htmlspecialchars( $revision->getUserText() ) .
+			$this->msg( 'colon-separator' )->inContentLanguage()->escaped() .
+			htmlspecialchars( FeedItem::stripComment( $revision->getComment() ) ) .
+			"</p>\n<hr />\n<div>" .
+			nl2br( htmlspecialchars( $content->serialize() ) ) . "</div>";
 	}
 
 	protected function getGroupName() {