5726d90d321895b5d0132e2bd516cc126d73b426
Moriel Schottlender <moriel@gmail.com>
2017-08-18 14:06:09 -0700

    RCFilters: Convert saved queries from filters to parameters
    This will allow us to load them in the backend, and to keep
    consistency between RecentChanges and Watchlist if needed.
    
    Added also a 'backup' preference to keep the previous version
    before the conversion, in case of mangling of the queries.
    
    Bug: T166908
    Change-Id: I8e26b66e43bd16282b7bdb52abc152f92a9c877d

---
+++ tests/qunit/suites/resources/mediawiki.rcfilters/dm.SavedQueryItemModel.test.js
@@ -0,0 +1,90 @@
+/* eslint-disable camelcase */
+( function ( mw ) {
+	var itemData = {
+		params: {
+			param1: '1',
+			param2: 'foo|bar',
+			highlight: '1',
+			invert: '0'
+		},
+		highlights: {
+			param1_color: 'c1',
+			param2_color: 'c2'
+		}
+	};
+
+	QUnit.module( 'mediawiki.rcfilters - SavedQueryItemModel' );
+
+	QUnit.test( 'Initializing and getters', function ( assert ) {
+		var model;
+
+		model = new mw.rcfilters.dm.SavedQueryItemModel(
+			'randomID',
+			'Some label',
+			$.extend( true, {}, itemData )
+		);
+
+		assert.equal(
+			model.getID(),
+			'randomID',
+			'Item ID is retained'
+		);
+
+		assert.equal(
+			model.getLabel(),
+			'Some label',
+			'Item label is retained'
+		);
+
+		assert.deepEqual(
+			model.getData(),
+			itemData,
+			'Item data is retained'
+		);
+
+		assert.ok(
+			!model.isDefault(),
+			'Item default state is retained.'
+		);
+	} );
+
+	QUnit.test( 'Default', function ( assert ) {
+		var model;
+
+		model = new mw.rcfilters.dm.SavedQueryItemModel(
+			'randomID',
+			'Some label',
+			$.extend( true, {}, itemData )
+		);
+
+		assert.ok(
+			!model.isDefault(),
+			'Default state represented when item initialized with default:false.'
+		);
+
+		model.toggleDefault( true );
+		assert.ok(
+			model.isDefault(),
+			'Default state toggles to true successfully'
+		);
+
+		model.toggleDefault( false );
+		assert.ok(
+			!model.isDefault(),
+			'Default state toggles to false successfully'
+		);
+
+		// Reset
+		model = new mw.rcfilters.dm.SavedQueryItemModel(
+			'randomID',
+			'Some label',
+			$.extend( true, {}, itemData ),
+			{ default: true }
+		);
+
+		assert.ok(
+			model.isDefault(),
+			'Default state represented when item initialized with default:true.'
+		);
+	} );
+}( mediaWiki ) );
