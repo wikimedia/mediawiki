f526c2c0673a1c12364810f39a90846d1325ef20
jdlrobson <jdlrobson@gmail.com>
2017-08-18 12:55:35 -0500

    Add mobile and desktop thresholds
    This will allow us to centralise these values similar to tablet
    device width and allow us to remove a Minerva/MobileFrontend
    dependency
    
    Going forward we will encourage explicitly tracked imports
    deviceWidthTablet is retained for backwards compatibility
    but on long term it should be imported directly from mediawiki.ui's
    variables
    
    Bug: T171365
    Change-Id: Ib9f843147db4473ce5590741e0fb490384b0007e

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -3790,7 +3790,7 @@ $wgResourceLoaderValidateStaticJS = false;
  */
 $wgResourceLoaderLESSVars = [
 	/**
-	 * Minimum available screen width at which a device can be considered a tablet/desktop
+	 * Minimum available screen width at which a device can be considered a tablet
 	 * The number is currently based on the device width of a Samsung Galaxy S5 mini and is low
 	 * enough to cover iPad (768px). Number is prone to change with new information.
 	 * @since 1.27
