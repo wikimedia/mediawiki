918e4c394c424a287f32853b2967b0c8d4dfc97c
Brad Jorsch <bjorsch@wikimedia.org>
2017-08-21 12:54:54 -0400

    Delete maintenance/deleteRevision.php
    It hasn't been updated properly since 2006 so many fields aren't being
    copied to the archive table. Tim suggests it'd be best to just delete it
    and, if someone needs the ability to delete or revdel revisions from the
    command line, properly abstract out the deletion code instead of
    duplicating it.
    
    Change-Id: I400b8ac30b31802e7dd9f6e4d0ec10918eba0183

--- autoload.php
+++ autoload.php
@@ -367,7 +367,6 @@ $wgAutoloadLocalClasses = [
 	'DeleteLogFormatter' => __DIR__ . '/includes/logging/DeleteLogFormatter.php',
 	'DeleteOldRevisions' => __DIR__ . '/maintenance/deleteOldRevisions.php',
 	'DeleteOrphanedRevisions' => __DIR__ . '/maintenance/deleteOrphanedRevisions.php',
-	'DeleteRevision' => __DIR__ . '/maintenance/deleteRevision.php',
 	'DeleteSelfExternals' => __DIR__ . '/maintenance/deleteSelfExternals.php',
 	'DeletedContribsPager' => __DIR__ . '/includes/specials/pagers/DeletedContribsPager.php',
 	'DeletedContributionsPage' => __DIR__ . '/includes/specials/SpecialDeletedContributions.php',
