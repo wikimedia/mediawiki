089f21eba8d536a633b4b5f1e01a0185634cd341
petarpetkovic <ppetkovic@wikimedia.org>
2017-08-21 17:23:21 +0200

    Support langlinkscount API
    Add support for new langlinkscount API to mw.widgets
    langlinkscount API was introduced in I8fffe5b2
    
    Enable TitleOptionWidget to display number of languages article exists in
    Enable TitleOptionWidget to display if article is missing in target language
    
    Bug: T111094
    Change-Id: I0c806d104ae43044104279a788abbd7231f76d8c

--- resources/Resources.php
+++ resources/Resources.php
@@ -2382,6 +2382,7 @@ return [
 			// TitleInputWidget
 			'mw-widgets-titleinput-description-new-page',
 			'mw-widgets-titleinput-description-redirect',
+			'mw-widgets-titleinput-missing-in-target-language'
 		],
 		'targets' => [ 'desktop', 'mobile' ],
 	],
