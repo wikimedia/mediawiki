8e8d5a288da6462fc3efd76e9bd67412e93fd2be
Aaron Schulz <aschulz@wikimedia.org>
2017-08-22 14:36:15 -0700

    Avoid DB replication waits for farm cross-wiki redirects
    This previously only worked if $wgLocalVirtualHosts was set, which
    was too specific to check and not used by WMF. Use the more generic
    WikiMap class.
    
    Two methods have been added there to do the work of enumerating
    canonical wiki farm URLs and checking them against a given URL.
    
    Bug: T172357
    Change-Id: Id2415bab5d7f5a08b9f536858c32d329138384a2

--- tests/phpunit/includes/WikiMapTest.php
+++ tests/phpunit/includes/WikiMapTest.php
@@ -16,6 +16,7 @@ class WikiMapTest extends MediaWikiLangTestCase {
 				'enwiki' => 'http://en.example.org',
 				'ruwiki' => '//ru.example.org',
 				'nopathwiki' => '//nopath.example.org',
+				'thiswiki' => '//this.wiki.org'
 			],
 			'wgArticlePath' => [
 				'enwiki' => '/w/$1',
@@ -25,6 +26,10 @@ class WikiMapTest extends MediaWikiLangTestCase {
 		$conf->suffixes = [ 'wiki' ];
 		$this->setMwGlobals( [
 			'wgConf' => $conf,
+			'wgLocalDatabases' => [ 'enwiki', 'ruwiki', 'nopathwiki' ],
+			'wgCanonicalServer' => '//this.wiki.org',
+			'wgDBname' => 'thiswiki',
+			'wgDBprefix' => ''
 		] );
 
 		TestSites::insertIntoDb();
@@ -175,4 +180,52 @@ class WikiMapTest extends MediaWikiLangTestCase {
 		$this->assertEquals( $expected, WikiMap::getForeignURL( $wikiId, $page, $fragment ) );
 	}
 
+	public function testGetCanonicalServerInfoForAllWikis() {
+		$expected = [
+			'thiswiki' => [
+				'url' => '//this.wiki.org',
+				'parts' => [ 'scheme' => '', 'host' => 'this.wiki.org', 'delimiter' => '//' ]
+			],
+			'enwiki' => [
+				'url' => 'http://en.example.org',
+				'parts' => [
+					'scheme' => 'http', 'host' => 'en.example.org', 'delimiter' => '://' ]
+			],
+			'ruwiki' => [
+				'url' => '//ru.example.org',
+				'parts' => [ 'scheme' => '', 'host' => 'ru.example.org', 'delimiter' => '//' ]
+			]
+		];
+
+		$this->assertArrayEquals(
+			$expected,
+			WikiMap::getCanonicalServerInfoForAllWikis(),
+			true,
+			true
+		);
+	}
+
+	public function provideGetWikiFromUrl() {
+		return [
+			[ 'http://this.wiki.org', 'thiswiki' ],
+			[ 'https://this.wiki.org', 'thiswiki' ],
+			[ 'http://this.wiki.org/$1', 'thiswiki' ],
+			[ 'https://this.wiki.org/$2', 'thiswiki' ],
+			[ 'http://en.example.org', 'enwiki' ],
+			[ 'https://en.example.org', 'enwiki' ],
+			[ 'http://en.example.org/$1', 'enwiki' ],
+			[ 'https://en.example.org/$2', 'enwiki' ],
+			[ 'http://ru.example.org', 'ruwiki' ],
+			[ 'https://ru.example.org', 'ruwiki' ],
+			[ 'http://ru.example.org/$1', 'ruwiki' ],
+			[ 'https://ru.example.org/$2', 'ruwiki' ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideGetWikiFromUrl
+	 */
+	public function testGetWikiFromUrl( $url, $wiki ) {
+		$this->assertEquals( $wiki, WikiMap::getWikiFromUrl( $url ) );
+	}
 }
