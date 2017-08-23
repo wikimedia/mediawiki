a69ecd8e05a4d09fd50493102dd6d575b3f70cf3
Chad Horohoe <chadh@wikimedia.org>
2017-08-23 09:59:37 -0700

    Bumping version to 1.29.1 in prep for release
    Change-Id: I0e2430db96f50309d6a1c561bcf636a1d56053db

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.29.0';
+$wgVersion = '1.29.1';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
