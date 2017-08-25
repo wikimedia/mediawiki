wikimedia/mediawiki

commit f598ca81e3f4016626b3abe24086dabbda52b0f7

Improve HTMLCacheUpdate job CDN purge de-duplication
Bug: T124418
Change-Id: I87005f3ffaf73c70f137137849b628217be946d2

includes/jobqueue/jobs/HTMLCacheUpdateJob.php
@@ -102,18 +102,15 @@ class HTMLCacheUpdateJob extends Job {
 			return;
 		}
 
-		// The page_touched field will need to be bumped for these pages.
-		// Only bump it to the present time if no "rootJobTimestamp" was known.
-		// If it is known, it can be used instead, which avoids invalidating output
-		// that was in fact generated *after* the relevant dependency change time
-		// (e.g. template edit). This is particularily useful since refreshLinks jobs
-		// save back parser output and usually run along side htmlCacheUpdate jobs;
-		// their saved output would be invalidated by using the current timestamp.
-		if ( isset( $this->params['rootJobTimestamp'] ) ) {
-			$touchTimestamp = $this->params['rootJobTimestamp'];
-		} else {
-			$touchTimestamp = wfTimestampNow();
-		}
+		// Bump page_touched to the current timestamp. This used to use the root job timestamp
+		// (e.g. template/file edit time), which was a bit more efficient when template edits are
+		// rare and don't effect the same pages much. However, this way allows for better
+		// de-duplication, which is much more useful for wikis with high edit rates. Note that
+		// RefreshLinksJob, which is enqueued alongside HTMLCacheUpdateJob, saves the parser output
+		// since it has to parse anyway. We assume that vast majority of the cache jobs finish
+		// before the link jobs, so using the current timestamp instead of the root timestamp is
+		// not expected to invalidate these cache entries too often.
+		$touchTimestamp = wfTimestampNow();
 
 		$dbw = wfGetDB( DB_MASTER );
 		// Update page_touched (skipping pages already touched since the root job).

commit cb7c910ba72bdf4c2c2f5fa7e7dd307f98e5138e

Fix old regression in HTMLCacheUpdate de-duplication
* The condition timestamp should be the root job timestamp,
  as the comments imply, though that was not the case. Only
  the SET timestamp should be the current timestamp. As it
  was, jobs effectively did not "see" each other since each
  one would be expecting newer than the ones that ran prior.
  The page_touched condition for the UPDATE query was mostly
  useless, since it was always the current timestamp.
* This problem was a regression from f598ca81e3f40166.

Change-Id: I1398366e87c959be1c98e4db959c41309b0ac1b8

includes/jobqueue/jobs/HTMLCacheUpdateJob.php
@@ -113,6 +113,10 @@ class HTMLCacheUpdateJob extends Job {
 		// before the link jobs, so using the current timestamp instead of the root timestamp is
 		// not expected to invalidate these cache entries too often.
 		$touchTimestamp = wfTimestampNow();
+		// If page_touched is higher than this, then something else already bumped it after enqueue
+		$condTimestamp = isset( $this->params['rootJobTimestamp'] )
+			? $this->params['rootJobTimestamp']
+			: $touchTimestamp;
 
 		$dbw = wfGetDB( DB_MASTER );
 		$factory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
@@ -126,7 +130,7 @@ class HTMLCacheUpdateJob extends Job {
 				[ 'page_touched' => $dbw->timestamp( $touchTimestamp ) ],
 				[ 'page_id' => $batch,
 					// don't invalidated pages that were already invalidated
-					"page_touched < " . $dbw->addQuotes( $dbw->timestamp( $touchTimestamp ) )
+					"page_touched < " . $dbw->addQuotes( $dbw->timestamp( $condTimestamp ) )
 				],
 				__METHOD__
 			);