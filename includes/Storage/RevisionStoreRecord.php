4da47823030c48d6601abb5cb476c34d6c2b464e
daniel <daniel.kinzler@wikimedia.de>
2017-08-27 17:29:18 +0200

    [WIP] Break Revision into RevisionRecord and RevisionStore.
    TODO: more tests for the old classes
    TODO: tests for the new classes
    TODO: replace old code, making much of Revision a proxy
    
    Bug: T174025
    Change-Id: I140f43a6fb443b38483f41f268c906b9cea64cf7

---
+++ includes/Storage/RevisionStoreRecord.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Lazy loading representation of a page revision.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Storage;
+
+use CommentStore;
+use CommentStoreComment;
+use Content;
+use InvalidArgumentException;
+use LogicException;
+use MWException;
+use OutOfBoundsException;
+use Title;
+use User;
+use Wikimedia\Assert\Assert;
+
+/**
+ * A RevisionRecord representing an existing revision persisted in the revision table.
+ * RevisionStoreRecord has no optional fields, getters will never return null.
+ *
+ * @since 1.31
+ */
+class RevisionStoreRecord extends RevisionRecord {
+
+	/** @var bool */
+	protected $mCurrent;
+
+	/**
+	 * @note Avoid calling this constructor directly. Use the appropriate methods
+	 * in RevisionStore instead.
+	 *
+	 * @param Title $title The title of the page this Revision is associated with.
+	 * @param CommentStoreComment $comment
+	 * @param object|array $row Either a database row or an array.
+	 *        Structure is subject to change without notice!
+	 * @param RevisionSlots $slots The slots of this revision.
+	 * @param bool|string $wikiId the wiki ID of the site this Revision belongs to,
+	 *        or false for the local site.
+	 */
+	function __construct( Title $title, CommentStoreComment $comment, $row, RevisionSlots $slots, $wikiId = false ) {
+		parent::__construct( $title, $slots, $wikiId );
+		Assert::parameterType( 'object', $row, '$row' );
+
+		$this->mId = intval( $row->rev_id );
+		$this->mPageId = intval( $row->rev_page );
+		$this->mComment = $comment;
+
+		$timestamp = wfTimestamp( TS_MW, $row->rev_timestamp );
+		Assert::parameter( is_string( $timestamp ), '$row->rev_timestamp', 'must be a valid timestamp' );
+
+		$this->mUserId = intval( $row->rev_user );
+		$this->mMinorEdit = boolval( $row->rev_minor_edit );
+		$this->mTimestamp = $timestamp;
+		$this->mDeleted = intval( $row->rev_deleted );
+
+		if ( !isset( $row->rev_parent_id ) ) {
+			$this->mParentId = 0;
+		} else {
+			$this->mParentId = intval( $row->rev_parent_id );
+		}
+
+		if ( !isset( $row->rev_len ) ) {
+			$this->mSize = null;
+		} else {
+			$this->mSize = intval( $row->rev_len );
+		}
+
+		if ( !isset( $row->rev_sha1 ) ) {
+			$this->mSha1 = null;
+		} else {
+			$this->mSha1 = $row->rev_sha1;
+		}
+
+		if ( isset( $row->page_latest ) ) {
+			$this->mCurrent = ( $row->rev_id == $row->page_latest );
+		} else {
+			$this->mCurrent = false;
+		}
+
+		// Use user_name for users and rev_user_text for IPs...
+		if ( isset( $row->user_name ) ) {
+			$this->mUserText = $row->user_name; // logged-in user
+		} else {
+			$this->mUserText = $row->rev_user_text; // IP or unknown user
+		}
+
+		if ( $this->mPageId && $this->mPageId !== $this->mTitle->getArticleID()) {
+			throw new InvalidArgumentException(
+				'The given Title does not belong to page ID ' . $this->mPageId
+			);
+		}
+	}
+
+	/**
+	 * MCR migration note: this replaces Revision::isCurrent
+	 *
+	 * @return bool
+	 */
+	public function isCurrent() {
+		return $this->mCurrent;
+	}
+
+	/**
+	 * MCR migration note: this replaces Revision::isDeleted
+	 *
+	 * @param int $field One of DELETED_* bitfield constants
+	 *
+	 * @return bool
+	 */
+	public function isDeleted( $field ) {
+		if ( $this->isCurrent() && $field === self::DELETED_TEXT ) {
+			// Current revisions of pages cannot have the content hidden. Skipping this
+			// check is very useful for Parser as it fetches templates using newKnownCurrent().
+			// Calling getVisibility() in that case triggers a verification database query.
+			return false; // no need to check
+		}
+
+		return parent::isDeleted( $field );
+	}
+
+	/**
+	 * @return int The revision id, never null.
+	 */
+	public function getId() {
+		// overwritten just to add a guarantee to the contract
+		return parent::getId();
+	}
+
+	/**
+	 * @return int The ID of the parent revision, never null. May be 0 if there is no
+	 *         parent revision, or the parent revision was not tracked.
+	 */
+	public function getParentId() {
+		// overwritten just to add a guarantee to the contract
+		return parent::getParentId();
+	}
+
+	/**
+	 * @return string The nominal revision size, never null. May be computed on the fly.
+	 */
+	public function getSize() {
+		// If length is null, calculate and remember it (potentially SLOW!).
+		// This is for compatibility with old database rows that don't have the field set.
+		if ( $this->mSize === null  ) {
+			$this->mSize = $this->mSlots->computeSize();
+		}
+
+		return $this->mSize;
+	}
+
+	/**
+	 * @return string The revision hash, never null. May be computed on the fly.
+	 */
+	public function getSha1() {
+		// If hash is null, calculate it and remember (potentially SLOW!)
+		// This is for compatibility with old database rows that don't have the field set.
+		if ( $this->mSha1 === null  ) {
+			$this->mSha1 = $this->mSlots->computeSha1();
+		}
+
+		return $this->mSha1;
+	}
+
+	/**
+	 * @param int $audience
+	 * @param User|null $user
+	 *
+	 * @return int The user ID, never null. May be 0 if the user is anonymous or unknown.
+	 */
+	public function getUserId( $audience = self::FOR_PUBLIC, User $user = null ) {
+		// overwritten just to add a guarantee to the contract
+		return parent::getUserId( $audience, $user );
+	}
+
+	/**
+	 * @param int $audience
+	 * @param User|null $user
+	 *
+	 * @return string The user name, never null. Empty if access is forbidden.
+	 */
+	public function getUserText( $audience = self::FOR_PUBLIC, User $user = null ) {
+		// overwritten just to add a guarantee to the contract
+		return parent::getUserText( $audience, $user );
+	}
+
+	/**
+	 * @param int $audience
+	 * @param User|null $user
+	 *
+	 * @return CommentStoreComment The revision comment, never null. Empty if access is forbidden.
+	 */
+	public function getComment( $audience = self::FOR_PUBLIC, User $user = null ) {
+		// overwritten just to add a guarantee to the contract
+		return parent::getComment( $audience, $user );
+	}
+
+	/**
+	 * @return string timestamp, never null
+	 */
+	public function getTimestamp() {
+		// overwritten just to add a guarantee to the contract
+		return parent::getTimestamp();
+	}
+
+}
