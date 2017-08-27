512320a0ffa42152a43b81ccac99d27ea32fbd61
daniel <daniel.kinzler@wikimedia.de>
2017-08-27 17:29:18 +0200

    [WIP] Break Revision into RevisionRecord and RevisionStore.
    TODO: more tests for the old classes
    TODO: tests for the new classes
    TODO: service wiring
    TODO: replace old code, making much of Revision a proxy
    
    Bug: T174025
    Change-Id: I140f43a6fb443b38483f41f268c906b9cea64cf7

---
+++ includes/Storage/MutableRevisionRecord.php
@@ -0,0 +1,391 @@
+<?php
+/**
+ * Lazy loading representation of a page revision.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Storage;
+
+use CommentStore;
+use CommentStoreComment;
+use Content;
+use InvalidArgumentException;
+use LogicException;
+use MWException;
+use OutOfBoundsException;
+use Title;
+use User;
+use Wikimedia\Assert\Assert;
+
+/**
+ * FIXME: update doc!
+ * Lazy loading representation of a page revision.
+ *
+ * @todo RevisionRecord is probably not needed as a separate interface.
+ * It's useful for now to be explicit about what should be exposed, and what shouldn't.
+ *
+ * Callbacks are used for lazy loading, so this class
+ * has no knowledge of the actual storage mechanism.
+ *
+ * @since 1.30
+ */
+class MutableRevisionRecord extends RevisionRecord {
+
+	/**
+	 * Constructs a MutableRevisionRecordbased on pre-1.30 array syntax.
+	 *
+	 * @param Title $title
+	 * @param array $array
+	 * @param RevisionSlots $slots
+	 * @param bool $wikiId
+	 *
+	 * @return RevisionStoreRecord
+	 */
+	public static function newFromArray_1_29( Title $title, array $array, RevisionSlots $slots, $wikiId = false ) {
+		Assert::parameterType( 'array', $array, '$row' );
+
+		$record = new MutableRevisionRecord( $title, $wikiId );
+
+		$userText = isset( $row['user_text'] ) ? strval( $row['user_text'] ) : null;
+		$userId = isset( $row['user'] ) ? intval( $row['user'] ) : null;
+		$timestamp = isset( $row['timestamp'] )
+			? strval( $row['timestamp'] ) : wfTimestampNow(); // XXX: can we get rid of this?
+
+		$pageId = isset( $row['page'] ) ? intval( $row['page'] ) : null;
+
+		if ( $title->exists() && $pageId === null ) {
+			$pageId = $title->getArticleID();
+		}
+
+		$record->setUserIdAndName( $userId, $userText );
+		$record->setTimestamp( $timestamp );
+
+		if ( $pageId ) $record->setPageId( $pageId );
+
+		if ( isset( $row['id'] ) ) $record->setId( intval( $row['id'] ) );
+		if ( isset( $row['parent_id'] ) ) $record->setParentId( intval( $row['parent_id'] ) );
+
+		if ( isset( $row['sha1'] ) ) $record->setSha1( $row['sha1'] );
+		if ( isset( $row['size'] ) ) $record->setSha1( intval( $row['size'] ) );
+
+		if ( isset( $row['minor_edit'] ) ) $record->setMinorEdit( intval( $row['minor_edit'] ) !== 0 );
+		if ( isset( $row['deleted'] ) ) $record->setDeleted( intval( $row['deleted'] ) );
+
+
+		if ( isset( $row['comment'] ) ) {
+			// FIXME: make sure the below is right!
+			if ( $row['comment'] instanceof CommentStoreComment ) {
+				$comment = $row['comment'];
+			} elseif ( isset( $row['comment_cid'] ) ) {
+				$comment = CommentStore::newKey( 'comment' )
+					->getCommentLegacy( wfGetDB( DB_REPLICA ), $row, false ); // FIXME: no wfGetDB!
+			} else {
+				$commentText = trim( strval( $row['comment'] ) );
+				$comment = new CommentStoreComment( null, $commentText );
+			}
+
+			$record->setComment( $comment );
+		}
+
+		return $record;
+	}
+
+	// FIXME: TODO: newFromRevisionRecord()
+
+	/**
+	 * @note Avoid calling this constructor directly. Use the appropriate methods
+	 * in RevisionStore instead.
+	 *
+	 * @param Title $title The title of the page this Revision is associated with.
+	 * @param bool|string $wikiId the wiki ID of the site this Revision belongs to,
+	 *        or false for the local site.
+	 *
+	 * @throws MWException
+	 */
+	function __construct( Title $title, $wikiId = false ) {
+		// FIXME: init mutable RevisionSlots
+
+		parent::__construct( $title, $slots, $wikiId );
+	}
+
+	/**
+	 * @param int $parentId
+	 */
+	public function setParentId( $parentId ) {
+		Assert::parameterType( 'int', $parentId, '$parentId' );
+
+		$this->mParentId = $parentId;
+	}
+
+	/**
+	 * @param RevisionSlots $slots
+	 */
+	public function setSlots( RevisionSlots $slots ) {
+		$this->mSlots = $slots; // FIXME: set individual slots?! remove slots? inherit slots?
+	}
+
+	/**
+	 * @param CommentStoreComment $comment
+	 */
+	public function setComment( CommentStoreComment $comment ) {
+		$this->mComment = $comment;
+	}
+
+	/**
+	 * Set revision hash if known. Prevents getSha1() from re-calculating the hash.
+	 *
+	 * @param string $sha1
+	 */
+	public function setSha1( $sha1 ) {
+		Assert::parameterType( 'string', $sha1, '$sha1' );
+
+		$this->mSha1 = $sha1;
+	}
+
+	/**
+	 * Set nominal revision size known. Prevents getSize() from re-calculating the size.
+	 *
+	 * @param int|null $size
+	 */
+	public function setSize( $size ) {
+		Assert::parameterType( 'int', $size, '$size' );
+
+		$this->mSize = $size;
+	}
+
+	/**
+	 * @param int $deleted
+	 */
+	public function setDeleted( $deleted ) {
+		Assert::parameterType( 'bool', $deleted, '$deleted' );
+
+		$this->mDeleted = $deleted;
+	}
+
+	/**
+	 * @param string $timestamp
+	 */
+	public function setTimestamp( $timestamp ) {
+		Assert::parameterType( 'string', $timestamp, '$timestamp' );
+
+		$this->mTimestamp = $timestamp;
+	}
+
+	/**
+	 * @param boolean $minorEdit
+	 */
+	public function setMinorEdit( $minorEdit ) {
+		Assert::parameterType( 'bool', $minorEdit, '$minorEdit' );
+
+		$this->mMinorEdit = $minorEdit;
+	}
+
+	/**
+	 * @param int|null $userId
+	 * @param string|null $userName
+	 */
+	public function setUserIdAndName( $userId, $userName ) {
+		Assert::parameterType( 'integer|null', $userId, '$userId' );
+		Assert::parameterType( 'string|null', $userName, '$userName' );
+
+		$this->mUserId = $userId;
+		$this->mUserText = $userName;
+	}
+
+	/**
+	 * @param int $pageId
+	 */
+	public function setPageId( $pageId ) {
+		Assert::parameterType( 'integer', $pageId, '$pageId' );
+
+		if ( $this->mTitle->exists() && $pageId !== $this->mTitle->getArticleID() ) {
+			throw new InvalidArgumentException(
+				'The given Title does not belong to page ID ' . $this->mPageId
+			);
+		}
+
+		$this->mPageId = $pageId;
+	}
+
+	/**
+	 * Returns the length of the text in this revision.
+	 *
+	 * MCR migration note: this replaces Revision::getSize
+	 *
+	 * @return int
+	 */
+	public function getSize() {
+		// If no fixed value is given, re-calculate on every call, since slots may change
+		if ( $this->mSize === null  ) {
+			return $this->mSlots->computeSize();
+		}
+
+		return $this->mSize;
+	}
+
+	/**
+	 * Set the revision ID.
+	 *
+	 * @warning Use this with care, especially when preparing a revision for insertion
+	 *          into the database! The revision ID should only be fixed in special cases
+	 *          like preserving the original ID when restoring a revision.
+	 *
+	 * @param int $id
+	 */
+	public function setId( $id ) {
+		Assert::parameterType( 'integer', $id, '$id' );
+
+		$this->mId = $id;
+	}
+
+	/**
+	 * Returns the base36 sha1 of the text in this revision.
+	 *
+	 * MCR migration note: this replaces Revision::getSha1
+	 *
+	 * @return string
+	 */
+	public function getSha1() {
+		// If no fixed value is given, re-calculate on every call, since slots may change
+		if ( $this->mSha1 === null  ) {
+			return $this->mSlots->computeSha1();
+		}
+
+		return $this->mSha1;
+	}
+
+	/**
+	 * @param mixed $value
+	 * @param string $name
+	 *
+	 * @throw IncompleteRevisionException if $value is null
+	 * @return mixed $value, if $value is not null
+	 */
+	private function guarantee( $value, $name ) {
+		if ( $value === null ) {
+			throw new IncompleteRevisionException(
+				"`$name` has not been set on this MutableRevisionRecord"
+			);
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Returns the SlotRecord for the given role, with no audience checks applied.
+	 *
+	 * @param string $role
+	 *
+	 * @throw OutOfBoundsException if no slot is set for the given role.
+	 * @return SlotRecord
+	 */
+	public function guaranteeSlot( $role ) {
+		return $this->getSlot( $role, self::RAW );
+	}
+
+	/**
+	 * Returns the Content for the given role, with no audience checks applied.
+	 *
+	 * @param string $role
+	 *
+	 * @throw OutOfBoundsException if no slot is set for the given role.
+	 * @return Content
+	 */
+	public function guaranteeContent( $role ) {
+		return $this->guaranteeSlot( $role )->getContent();
+	}
+
+	/**
+	 * @return int The ID of the parent revision, never null. May be 0 if there is no parent revision.
+	 * @throw IncompleteRevisionException if the respective field is was not set.
+	 */
+	public function guaranteeParentId() {
+		return $this->guarantee( $this->getParentId(), 'ParentId' );
+	}
+
+	/**
+	 * @return int the nominal revision size, never null
+	 *         Will be calculated if not set.
+	 */
+	public function guaranteeSize() {
+		return $this->guarantee( $this->getSize(), "Size" );
+	}
+
+	/**
+	 * @return string The revision hash, never null.
+	 *         Will be calculated if not set.
+	 */
+	public function guaranteeSha1() {
+		return $this->guarantee( $this->getSha1(), "Sha1" );
+	}
+
+	/**
+	 * @return int The ID of the page this revision belongs to, never null, always greater than 0.
+	 *         Will be calculated if not set needed.
+	 */
+	public function guaranteePageId() {
+		$pageId = $this->guarantee( $this->getPageId(), "PageId" );
+
+		// make sure this is really a page ID
+		if ( $pageId <= 0 ) {
+			throw new IncompleteRevisionException( 'PageId must be positive' );
+		}
+
+		return $pageId;
+	}
+
+	/**
+	 * Returns the user id, with no audience checks applied.
+	 *
+	 * @return int The user ID, never null. May be 0 if the user is anonymous or unknown.
+	 * @throw IncompleteRevisionException if the respective field is was not set.
+	 */
+	public function guaranteeUserId() {
+		return $this->guarantee( $this->getUserId( self::RAW ), "UserId" );
+	}
+
+	/**
+	 * Returns the user name, with no audience checks applied.
+	 *
+	 * @return string The user text, never null.
+	 * @throw IncompleteRevisionException if the respective field is was not set.
+	 */
+	public function guaranteeUserText() {
+		return $this->guarantee( $this->getUserText( self::RAW ), "UserText" );
+	}
+
+	/**
+	 * Returns the revision comment, with no audience checks applied.
+	 *
+	 * @return CommentStoreComment The revision comment, never null. Empty if access is forbidden.
+	 * @throw IncompleteRevisionException if the respective field is was not set.
+	 */
+	public function guaranteeComment() {
+		return $this->guarantee( $this->getComment( self::RAW ), "Comment" );
+	}
+
+	/**
+	 * @return string timestamp, never null
+	 * @throw IncompleteRevisionException if the respective field is was not set.
+	 */
+	public function guaranteeTimestamp() {
+		return $this->guarantee( $this->getTimestamp(), "Timestamp" );
+	}
+
+}
---
+++ includes/Storage/SlotRecord.php
@@ -0,0 +1,366 @@
+<?php
+/**
+ * Value object representing a content slot associated with a page revision.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Storage;
+
+use Closure;
+use Content;
+use LogicException;
+use OutOfBoundsException;
+use Wikimedia\Assert\Assert;
+
+/**
+ * Value object representing a content slot associated with a page revision.
+ * SlotRecord provides direct access to a Content object.
+ * That access may be implemented through a callback.
+ *
+ * @since 1.30
+ */
+class SlotRecord {
+
+	/**
+	 * @var object database result row, as a raw object
+	 */
+	private $row;
+
+	/**
+	 * @var Content|callable
+	 */
+	private $content;
+
+	/**
+	 * Returns a new SlotRecord just like the given $slot, except that calling getContent()
+	 * will fail with an exception.
+	 *
+	 * @param SlotRecord $slot
+	 *
+	 * @return SlotRecord
+	 */
+	public static function newWithSuppressedContent( SlotRecord $slot ) {
+		$row = $slot->row;
+
+		return new SlotRecord( $row, function() {
+			throw new RevisionLookupException( 'Content suppressed!' );
+		} );
+	}
+
+	/**
+	 * Returns a SlotRecord for a new revision, inheriting the content of the given SlotRecord
+	 * of a previous revision.
+	 *
+	 * @param SlotRecord $slot
+	 *
+	 * @return SlotRecord
+	 */
+	public static function newInherited( SlotRecord $slot ) {
+		$row = $slot->row;
+		$row->slot_inherited = true;
+		$row->slot_revision = null; // not yet known
+
+		return new SlotRecord( $row, $slot->content );
+	}
+
+	/**
+	 * Constructs a new Slot from a Content object. This is the preferred way to construct a slot
+	 * for storing Content that resulted from a user edit.
+	 *
+	 * @param string $role
+	 * @param Content $content
+	 *
+	 * @return SlotRecord
+	 */
+	public static function newFromContent( $role, Content $content ) {
+		$row = [
+			'slot_id' => null, // not yet known
+			'slot_address' => null, // not yet known. need setter?
+			'slot_revision' => null, // not yet known
+			'slot_page' => null, // not yet known
+			'slot_inherited' => false,
+			'slot_role' => $role, // FIXME: int or string?!
+		];
+		$row->slot_inherited = true;
+
+		return new SlotRecord( (object)$row, $content );
+	}
+
+	/**
+	 * SlotRecord constructor.
+	 *
+	 * @param object $row A database row composed of fields of the slot and content tables,
+	 *        as a raw object. Any field value can be a callabck that produces the field value
+	 *        given this SlotRecord as a parameter.
+	 * @param Content|callable $content The content object associated with the slot, or a
+	 *        callback that will return that Content object, given this SlotRecord as a parameter.
+	 */
+	public function __construct( $row, $content ) {
+		Assert::parameterType( 'object', $row, '$row' );
+		Assert::parameterType( 'Content|callable', $content, '$content' );
+
+		$this->row = $row;
+		$this->content = $content;
+	}
+
+	/**
+	 * Implemented to defy serialization.
+	 *
+	 * @throws LogicException always
+	 */
+	public function __sleep() {
+		throw new LogicException( __CLASS__ . ' is not serializable.' );
+	}
+
+	/**
+	 * Returns the Content of the given slot.
+	 *
+	 * @note This is free to load Content from whatever subsystem is necessary,
+	 * performing potentially expensive operations and triggering I/O-related
+	 * failure modes.
+	 *
+	 * @note This method does not apply audience filtering.
+	 *
+	 * @return Content The slot's content. This is a direct reference to the internal instance,
+	 * copy before exposing to application logic!
+	 */
+	public function getContent() {
+		if ( $this->content instanceof Content ) {
+			return $this->content;
+		}
+
+		$obj = call_user_func( $this->content, $this );
+
+		Assert::postcondition(
+			$obj instanceof Content,
+			'Slot content callback should return a Content object'
+		);
+
+		$this->content = $obj;
+
+		return $this->content;
+	}
+
+	/**
+	 * Returns the string value of a data field from the database row supplied to the constructor.
+	 * If the field was set to a callback, that callback is invoked and the result returned.
+	 *
+	 * @param string $name
+	 *
+	 * @throws OutOfBoundsException
+	 * @return mixed Returns the field's value, or null if the field is NULL in the DB row.
+	 */
+	private function getField( $name ) {
+		if ( !isset( $this->row->$name ) ) {
+			$fields = get_object_vars( $this->row );
+
+			// distinguish between unknown and uninitialized fields
+			if ( array_key_exists( $name, $fields ) ) {
+				throw new IncompleteRevisionException( 'Uninitialized field: ' . $name );
+			} else {
+				throw new OutOfBoundsException( 'No such field: ' . $name );
+			}
+		}
+
+		$value = $this->row->$name;
+
+		// NOTE: allow callbacks, but don't trust plain string callables from the database!
+		if ( !is_string( $value ) && is_callable( $value ) ) {
+			$value = call_user_func( $value, $this );
+			$this->setField( $name, $value );
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Returns the string value of a data field from the database row supplied to the constructor.
+	 *
+	 * @param string $name
+	 *
+	 * @throws OutOfBoundsException
+	 * @throws IncompleteRevisionException
+	 * @return string Returns the string value
+	 */
+	private function getStringField( $name ) {
+		return strval( $this->getField( $name ) );
+	}
+
+	/**
+	 * Returns the int value of a data field from the database row supplied to the constructor.
+	 *
+	 * @param string $name
+	 *
+	 * @throws OutOfBoundsException
+	 * @throws IncompleteRevisionException
+	 * @return int Returns the int value
+	 */
+	private function getIntField( $name ) {
+		return intval( $this->getField( $name ) );
+	}
+
+	/**
+	 * @param string $name
+	 * @return bool whether this record contains the given field
+	 */
+	public function hasField( $name ) {
+		return isset( $this->row[$name] );
+	}
+
+	/**
+	 * Returns the ID of the page this slot is associated with.
+	 *
+	 * @return int
+	 */
+	public function getPage() {
+		return $this->getIntField( 'slot_page' );
+	}
+
+	/**
+	 * Returns the ID of the revision this slot is associated with.
+	 *
+	 * @return int
+	 */
+	public function getRevision() {
+		return $this->getIntField( 'slot_revision' );
+	}
+
+	/**
+	 * Whether this slot was inherited from an older revision.
+	 *
+	 * @return bool
+	 */
+	public function isInherited() {
+		return $this->getIntField( 'slot_inherited' ) !== 0;
+	}
+
+	/**
+	 * Whether this slot has an address. Slots will have an address if their
+	 * content has been stored. While building a new revision,
+	 * SlotRecords will not have an address associated.
+	 *
+	 * @return bool
+	 */
+	public function hasAddress() {
+		return $this->hasField( 'slot_address' );
+	}
+
+	/**
+	 * Whether this slot has revision ID associated. Slots will have a revision ID associated
+	 * only if they were loaded as part of an existing revision. While building a new revision,
+	 * Slotrecords will not have a revision ID associated.
+	 *
+	 * @return bool
+	 */
+	public function hasRevision() {
+		return $this->hasField( 'slot_revision' );
+	}
+
+	/**
+	 * Returns the role of the slot.
+	 *
+	 * @return string
+	 */
+	public function getRole() {
+		// TODO: resolve role ID to role name here!
+		return $this->getStringField( 'slot_role' );
+	}
+
+	/**
+	 * Returns the address of this slot's content.
+	 * This address can be used with RevisionContentStore to load the Content object.
+	 *
+	 * @return string
+	 */
+	public function getAddress() {
+		return $this->getStringField( 'cont_address' );
+	}
+
+	/**
+	 * Returns the content size
+	 *
+	 * @return int size of the content, in bogo-bytes, as reported by Content::getSize.
+	 */
+	public function getSize() {
+		try {
+			$size = $this->getIntField( 'cont_size' );
+		} catch ( OutOfBoundsException $ex ) {
+			$size = $this->getContent()->getSize();
+			$this->setField( 'cont_size', $size );
+		}
+
+		return $size;
+	}
+
+	/**
+	 * Returns the content size
+	 *
+	 * @return string hash of the content.
+	 */
+	public function getSha1() {
+		try {
+			$sha1 = $this->getStringField( 'cont_sha1' );
+		} catch ( OutOfBoundsException $ex ) {
+			$format = $this->getFormat();
+			$data = $this->getContent()->serialize( $format );
+			$sha1 = self::base36Sha1( $data );
+			$this->setField( 'cont_sha1', $sha1 );
+		}
+
+		return $sha1;
+	}
+
+	/**
+	 * Returns the content model
+	 *
+	 * @return string the content model of the content
+	 */
+	public function getModel() {
+		try {
+			// FIXME: will need mapping once it's an int!
+			$model = $this->getStringField( 'cont_model' );
+		} catch ( OutOfBoundsException $ex ) {
+			$model = $this->getContent()->getModel();
+			$this->setField( 'cont_model', $model );
+		}
+
+		return $model;
+	}
+
+	/**
+	 * @param string $name
+	 * @param string|int|null $value
+	 */
+	private function setField( $name, $value ) {
+		$this->row->$name = $value;
+	}
+
+	/**
+	 * Get the base 36 SHA-1 value for a string of text
+	 *
+	 * MCR migration note: this replaces Revision::base36Sha1
+	 *
+	 * @param string $blob
+	 * @return string
+	 */
+	public static function base36Sha1( $blob ) {
+		return \Wikimedia\base_convert( sha1( $blob ), 16, 36, 31 );
+	}
+
+}
