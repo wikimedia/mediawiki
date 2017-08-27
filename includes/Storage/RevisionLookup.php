900ae54a48df4c05ab446bdc9eae229ab86eab35
daniel <daniel.kinzler@wikimedia.de>
2017-08-27 17:29:18 +0200

    [WIP] Break Revision into RevisionRecord and RevisionStore.
    TODO: more tests for the old classes
    TODO: tests for the new classes
    TODO: replace old code, making much of Revision a proxy
    
    Bug: T174025
    Change-Id: I140f43a6fb443b38483f41f268c906b9cea64cf7

---
+++ includes/Storage/RevisionLookup.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ *  Service for looking up page revisions.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Storage;
+
+use MediaWiki\Linker\LinkTarget;
+use Title;
+use Wikimedia\Rdbms\IDatabase;
+
+/**
+ * Service for looking up page revisions.
+ *
+ * @note This was written to act as a drop-in replacement for the corresponding
+ *       static methods in Revision.
+ *
+ * @since 1.31
+ */
+interface RevisionLookup {
+
+	/**
+	 * Load a page revision from a given revision ID number.
+	 * Returns null if no such revision can be found.
+	 *
+	 * MCR migration note: this replaces Revision::newFromId
+	 *
+	 * $flags include:
+	 *      RevisionRecord::READ_LATEST  : Select the data from the master
+	 *      RevisionRecord::READ_LOCKING : Select & lock the data from the master
+	 *
+	 * @param int $id
+	 * @param int $flags (optional)
+	 * @return RevisionRecord|null
+	 */
+	public function getRevisionById( $id, $flags = 0 );
+
+	/**
+	 * Load either the current, or a specified, revision
+	 * that's attached to a given link target. If not attached
+	 * to that link target, will return null.
+	 *
+	 * MCR migration note: this replaces Revision::newFromTitle
+	 *
+	 * $flags include:
+	 *      RevisionRecord::READ_LATEST  : Select the data from the master
+	 *      RevisionRecord::READ_LOCKING : Select & lock the data from the master
+	 *
+	 * @param LinkTarget $linkTarget
+	 * @param int $id (optional)
+	 * @param int $flags Bitfield (optional)
+	 * @return RevisionRecord|null
+	 */
+	public function getRevisionByTitle( LinkTarget $linkTarget, $id = 0, $flags = 0 );
+
+	/**
+	 * Load either the current, or a specified, revision
+	 * that's attached to a given page ID.
+	 * Returns null if no such revision can be found.
+	 *
+	 * MCR migration note: this replaces Revision::newFromPageId
+	 *
+	 * $flags include:
+	 *      RevisionRecord::READ_LATEST  : Select the data from the master (since 1.20)
+	 *      RevisionRecord::READ_LOCKING : Select & lock the data from the master
+	 *
+	 * @param int $pageId
+	 * @param int $revId (optional)
+	 * @param int $flags Bitfield (optional)
+	 * @return RevisionRecord|null
+	 */
+	public function getRevisionByPageId( $pageId, $revId = 0, $flags = 0 );
+
+	/**
+	 * Get previous revision for this title
+	 *
+	 * MCR migration note: this replaces Revision::getPrevious
+	 *
+	 * @param RevisionRecord $rev
+	 *
+	 * @return RevisionRecord|null
+	 */
+	public function getPreviousRevision( RevisionRecord $rev );
+
+	/**
+	 * Get next revision for this title
+	 *
+	 * MCR migration note: this replaces Revision::getNext
+	 *
+	 * @param RevisionRecord $rev
+	 *
+	 * @return RevisionRecord|null
+	 */
+	public function getNextRevision( RevisionRecord $rev );
+
+	/**
+	 * Load a revision based on a known page ID and current revision ID from the DB
+	 *
+	 * This method allows for the use of caching, though accessing anything that normally
+	 * requires permission checks (aside from the text) will trigger a small DB lookup.
+	 * The title will also be lazy loaded, though setTitle() can be used to preload it.
+	 *
+	 * MCR migration note: this replaces Revision::newKnownCurrent
+	 *
+	 * @param IDatabase $db
+	 * @param int $pageId Page ID
+	 * @param int $revId 1 current revision of this page
+	 * @return RevisionRecord|bool Returns false if missing
+	 * @since 1.31
+	 */
+	public function getKnownCurrentRevision( IDatabase $db, $pageId, $revId );
+}
