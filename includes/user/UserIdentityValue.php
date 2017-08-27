a293a21ced09cfa68a2f2a22d737edef323fd27c
daniel <daniel.kinzler@wikimedia.de>
2017-08-27 17:29:18 +0200

    [MCR] Break Revision into RevisionRecord and RevisionStore
    DO NOT MERGE until test coverage is complete
    
    Bug: T174025
    Change-Id: I140f43a6fb443b38483f41f268c906b9cea64cf7

---
+++ includes/user/UserIdentityValue.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Value object representing a user's identity.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\User;
+
+use Wikimedia\Assert\Assert;
+
+/**
+ * Value object representing a user's identity.
+ *
+ * @since 1.31
+ */
+class UserIdentityValue implements UserIdentity {
+
+	/**
+	 * @var int
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @param int $id
+	 * @param string $name
+	 */
+	public function __construct( $id, $name ) {
+		Assert::parameterType( 'integer', $id, '$id' );
+		Assert::parameterType( 'string', $name, '$name' );
+
+		$this->id = $id;
+		$this->name = $name;
+	}
+
+	/**
+	 * @return int The user ID. May be 0 for anonymous users or for users with no local account.
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return string The user's logical name. May be an IPv4 or IPv6 address for anonymous users.
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+}
