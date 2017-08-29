e62f8dae3df3ec38da11bf520a3ac75cd34882e4
Sam Wilson <sam@samwilson.id.au>
2017-08-29 11:35:16 +0800

    Make it possible to inject a different Preferences object
    This makes some changes relating to subclassing Preferences,
    PreferencesForm, and SpecialPreferences.
    
    Add a new 'PreferencesFactory' service, that extensions can
    redefine if required by implementing the new PreferencesFactory
    interface, and using the MediaWikiServices hook. The
    DefaultPreferencesFactory simply provides a normal Preferences
    object.
    
    Fix the constructor of SpecialPreferences to match that of its
    parent, so subclasses can do the same. Also remove private
    visibility from SpecialPreferences::showResetForm().
    
    Move the setting of the submit callback for the PreferencesForm
    into SpecialPreferences::getFormObject() so that implementors of
    this method can provide their own callback.
    
    Deprecate Preferences::getFormObject() and replace it with the
    non-static Preferences::getForm() which currently is just a proxy
    to the former but as Preferences is refactored can replace it.
    
    Bug: T173476
    Change-Id: I3f7e9067c2faa915c2b05a2ae01312c3f2fee2ea

--- includes/MediaWikiServices.php
+++ includes/MediaWikiServices.php
@@ -10,6 +10,7 @@ use GenderCache;
 use GlobalVarConfig;
 use Hooks;
 use IBufferingStatsdDataFactory;
+use MediaWiki\Preferences\PreferencesFactory;
 use Wikimedia\Rdbms\LBFactory;
 use LinkCache;
 use Wikimedia\Rdbms\LoadBalancer;
@@ -564,6 +565,14 @@ class MediaWikiServices extends ServiceContainer {
 	 */
 	public function getProxyLookup() {
 		return $this->getService( 'ProxyLookup' );
+	}
+
+	/**
+	 * @since 1.31
+	 * @return PreferencesFactory
+	 */
+	public function getPreferencesFactory() {
+		return $this->getService( 'PreferencesFactory' );
 	}
 
 	/**
