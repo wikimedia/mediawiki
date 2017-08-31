9d272f3b2e5caab3a2e0ad29147581b454bff2a8
daniel <daniel.kinzler@wikimedia.de>
2017-08-31 20:41:04 +0200

    Turn Revision into a proxy to the new code.
    Change-Id: I4f24e7fbb683cb51f3fd8b250732bae9c7541ba2

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -41,6 +41,8 @@ use MediaWiki\Interwiki\ClassicInterwikiLookup;
 use MediaWiki\Linker\LinkRendererFactory;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Storage\ContentStore;
+use MediaWiki\Storage\RevisionStore;
 
 return [
 	'DBLoadBalancerFactory' => function ( MediaWikiServices $services ) {
@@ -425,6 +427,21 @@ return [
 		return new ReadOnlyMode(
 			$services->getConfiguredReadOnlyMode(),
 			$services->getDBLoadBalancer()
+		);
+	},
+
+	'RevisionStore' => function ( MediaWikiServices $services ) {
+		return new RevisionStore(
+			$services->getDBLoadBalancer(),
+			$services->getContentStore(),
+			$services->getMainWANObjectCache()
+		);
+	},
+
+	'ContentStore' => function ( MediaWikiServices $services ) {
+		return new ContentStore(
+			$services->getDBLoadBalancer(),
+			$services->getMainWANObjectCache()
 		);
 	},
 
