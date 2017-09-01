855dea035ee3730f6baaaa2d2207ff160e6906ed
Reedy <reedy@wikimedia.org>
2017-09-01 17:39:50 +0100

    Remove patch-log_search-rename-index.sql from MW 1.16 updaters
    Otherwise, the PK on log_search keeps getting dropped, ls_field_val readded and
    this is then repeated on every run of update.php
    
    Change-Id: I6b00933c29b5e24ec2833001fcf45053292d190c
    Follows-up: Id635297838938c7c5dfe65d45285a4d16d65152d

--- maintenance/archives/patch-log_search-rename-index.sql
+++
@@ -1,7 +0,0 @@
--- Rename the primary unique index from PRIMARY to ls_field_val
--- This is for MySQL only and is necessary only for databases which were updated
--- between MW 1.16 development revisions r50567 and r51465.
-ALTER TABLE /*_*/log_search
-	DROP PRIMARY KEY,
-	ADD UNIQUE INDEX ls_field_val (ls_field,ls_value,ls_log_id);
-
