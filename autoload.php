f3399a4eaf1ec854ac1d0b6fb5d81a6aa20b3b85
Reedy <reedy@wikimedia.org>
2017-09-05 19:46:55 +0100

    Add a maintenance script to cleanup invalid querycache rows
    Bug: T174513
    Change-Id: I50db45ff4c166055f645892a8a9c3d229ebf75b0

--- autoload.php
+++ autoload.php
@@ -263,6 +263,7 @@ $wgAutoloadLocalClasses = [
 	'CleanupEmptyCategories' => __DIR__ . '/maintenance/cleanupEmptyCategories.php',
 	'CleanupInvalidDbKeys' => __DIR__ . '/maintenance/cleanupInvalidDbKeys.php',
 	'CleanupPreferences' => __DIR__ . '/maintenance/cleanupPreferences.php',
+	'CleanupQueryCache' => __DIR__ . '/maintenance/cleanupQueryCache.php',
 	'CleanupRemovedModules' => __DIR__ . '/maintenance/cleanupRemovedModules.php',
 	'CleanupSpam' => __DIR__ . '/maintenance/cleanupSpam.php',
 	'ClearInterwikiCache' => __DIR__ . '/maintenance/clearInterwikiCache.php',
