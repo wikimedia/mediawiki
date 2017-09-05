36191b97d7da74631c2d4f849a4b5f623ada2340
Reedy <reedy@wikimedia.org>
2017-09-05 19:46:55 +0100

    Add a maintenance script to cleanup invalid querycache rows
    Bug: T174513
    Change-Id: I50db45ff4c166055f645892a8a9c3d229ebf75b0

---
+++ maintenance/cleanupQueryCache.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ */
+
+require_once __DIR__ . '/Maintenance.php';
+
+/**
+ * Maintenance script to cleanup invalid querycache rows.
+ *
+ * @ingroup Maintenance
+ */
+class CleanupQueryCache extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( "Cleanup invalid querycache rows." );
+	}
+
+	public function execute() {
+		$dbw = $this->getDB( DB_MASTER );
+
+		$selectConds = $where;
+		$prevEnd = false;
+
+		$deletedRows = 0;
+		do {
+			$query = "DELETE FROM " . $dbw->tableName( 'querycache' ) .
+				" WHERE qc_type = '' LIMIT 1000";
+
+			$dbw->query( $query, __METHOD__ );
+
+			$deletedRows += $dbw->affectedRows();
+			$this->output( "$deletedRows rows deleted\n" );
+			wfWaitForSlaves();
+		} while ( $res->numRows() );
+
+		$this->output( "Done!\n" );
+	}
+
+	public function getDbType() {
+		return Maintenance::DB_ADMIN;
+	}
+}
+
+$maintClass = "CleanupQueryCache";
+require_once RUN_MAINTENANCE_IF_MAIN;
