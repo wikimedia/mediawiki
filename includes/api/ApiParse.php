71721be7c17e603c27540380a27ccaaeba77933d
Arlo Breault <abreault@wikimedia.org>
2017-09-13 21:13:15 -0400

    API: Pass revid when parsing text
     * Provides consistency with expandtemplates
    
     * Necessary for properly rendering `text` with {{REVISIONID}}
    
    Change-Id: I310d0778d74c46f15385cd411d2462b541ed339a

--- includes/api/ApiParse.php
+++ includes/api/ApiParse.php
@@ -52,6 +52,7 @@ class ApiParse extends ApiBase {
 		// rest aren't, so just do it in two calls.
 		$this->requireMaxOneParameter( $params, 'page', 'pageid', 'oldid', 'text' );
 		$this->requireMaxOneParameter( $params, 'page', 'pageid', 'oldid', 'title' );
+		$this->requireMaxOneParameter( $params, 'page', 'pageid', 'oldid', 'revid' );
 
 		$text = $params['text'];
 		$title = $params['title'];
@@ -165,9 +166,18 @@ class ApiParse extends ApiBase {
 				);
 			}
 		} else { // Not $oldid, $pageid, $page. Hence based on $text
-			$titleObj = Title::newFromText( $title );
-			if ( !$titleObj || $titleObj->isExternal() ) {
-				$this->dieWithError( [ 'apierror-invalidtitle', wfEscapeWikiText( $title ) ] );
+			$revid = $params['revid'];
+			if ( $revid !== null ) {
+				$rev = Revision::newFromId( $revid );
+				if ( !$rev ) {
+					$this->dieWithError( [ 'apierror-nosuchrevid', $revid ] );
+				}
+				$titleObj = $rev->getTitle();
+			} else {
+				$titleObj = Title::newFromText( $title );
+				if ( !$titleObj || $titleObj->isExternal() ) {
+					$this->dieWithError( [ 'apierror-invalidtitle', wfEscapeWikiText( $title ) ] );
+				}
 			}
 			$wgTitle = $titleObj;
 			if ( $titleObj->canExist() ) {
@@ -247,9 +257,9 @@ class ApiParse extends ApiBase {
 
 			// Not cached (save or load)
 			if ( $params['pst'] ) {
-				$p_result = $this->pstContent->getParserOutput( $titleObj, null, $popts );
+				$p_result = $this->pstContent->getParserOutput( $titleObj, $revid, $popts );
 			} else {
-				$p_result = $this->content->getParserOutput( $titleObj, null, $popts );
+				$p_result = $this->content->getParserOutput( $titleObj, $revid, $popts );
 			}
 		}
 
@@ -784,6 +794,9 @@ class ApiParse extends ApiBase {
 			'title' => null,
 			'text' => [
 				ApiBase::PARAM_TYPE => 'text',
+			],
+			'revid' => [
+				ApiBase::PARAM_TYPE => 'integer',
 			],
 			'summary' => null,
 			'page' => null,
