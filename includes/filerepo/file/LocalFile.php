e5c0bc2ef31fdd5fd3f14b94ad11cf578c4b810d
Reedy <reedy@wikimedia.org>
2017-09-18 01:58:18 +0100

    Return description fields for unprefixed image cache rows
    Bug: T175444
    Change-Id: I5560187d3850253095b695dc7a3cfc954fba9318

--- includes/filerepo/file/LocalFile.php
+++ includes/filerepo/file/LocalFile.php
@@ -351,9 +351,8 @@ class LocalFile extends File {
 		static $results = [];
 
 		if ( $prefix == '' ) {
-			return $fields;
+			return array_merge( $fields, [ 'description' ] );
 		}
-
 		if ( !isset( $results[$prefix] ) ) {
 			$prefixedFields = [];
 			foreach ( $fields as $field ) {
