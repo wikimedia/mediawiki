aa68b5ee5e8bdf9e2bb24f99bec51141accbdaef
daniel <daniel.kinzler@wikimedia.de>
2017-09-18 17:00:26 +0200

    MCR database schema
    Schema additions/updates for MySQL, SQLite, PostgreSQL, MSSQL
    and Oracle to add new tables:
    
    * content
    * content_models
    * slots
    * slot_roles
    
    See also https://www.mediawiki.org/wiki/Multi-Content_Revisions/Database_Schema
    
    Bug: T174028
    Change-Id: I30a3a9834d54d0e6957553d91908b2b73b2c802f

---
+++ maintenance/archives/patch-content.sql
@@ -0,0 +1,22 @@
+--
+-- The content table represents content objects. It's primary purpose is to provide the necessary
+-- meta-data for loading and interpreting a serialized data blob to create a content object.
+--
+CREATE TABLE /*_*/content (
+
+  -- ID of the content object
+  content_id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+
+  -- Nominal size of the content object (not necessarily of the serialized blob)
+  content_size int unsigned NOT NULL,
+
+  -- Nominal hash of the content object (not necessarily of the serialized blob)
+  -- NOTE: nullable so we can stop storing the hash without changing the schema.
+  content_sha1 varbinary(32),
+
+  -- reference to model_id
+  content_model smallint unsigned NOT NULL,
+
+  -- URL-like address of the content blob
+  content_address varbinary(255) NOT NULL
+) /*$wgDBTableOptions*/;
