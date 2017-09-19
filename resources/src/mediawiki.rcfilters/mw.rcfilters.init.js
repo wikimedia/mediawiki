7a28c62e99a9b935cf8c3c94d367b1b5d8d56223
eranroz <eranroz89@gmail.com>
2017-09-20 00:29:33 +0300

    Lazy creation for heavy menu
    Assumptions:
    * Generally if we have a model we don't need UI
    * UI can be created lazy if it is not shown to user
    
    Bug: T176250
    Change-Id: Iba7f889d8610de2eb3056248cd1c664b0cd90940

--- resources/src/mediawiki.rcfilters/mw.rcfilters.init.js
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.init.js
@@ -90,7 +90,12 @@
 		}
 	};
 
-	$( rcfilters.init );
+	// Early execute of init
+	if ( document.readyState === 'interactive' || document.readyState === 'complete' ) {
+		rcfilters.init();
+	} else {
+		$( rcfilters.init );
+	}
 
 	module.exports = rcfilters;
 
