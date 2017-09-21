e8ad642e606f7a3592577afc90dace1fed97a854
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-09-21 09:13:01 -0700

    Timeout autoHide notifications based on visible time
    On supported browsers handle the auto hide timeout with a
    count of cumulative time the page has been visible to the
    user. Old functionality can still be accessed, if desired,
    by setting the visibleTimeout notification option to false.
    On browsers without support for this visibilitychange event
    wall clock time (the old behaviour) is used.
    
    Adds a library function functionally similar to setTimeout that
    only considers time when the page is visible. This is useful
    both for analytics purposes, and when you want to temporarily
    put something on screen and be reasonably certain it doesn't
    go away until a user has seen it.
    
    Bug: T42322
    Change-Id: I7d8ea85602cae9cfc72e0155bc3092049ecafd43

---
+++ tests/qunit/suites/resources/mediawiki/mediawiki.visibleTimeout.test.js
@@ -0,0 +1,98 @@
+( function ( mw ) {
+
+	QUnit.module( 'mediawiki.visibleTimeout', QUnit.newMwEnvironment( {
+		setup: function () {
+			// Document with just enough stuff to make the tests work.
+			var listeners = [];
+			this.mockDocument = {
+				hidden: false,
+				addEventListener: function ( type, listener ) {
+					if ( type === 'visibilitychange' ) {
+						listeners.push( listener );
+					}
+				},
+				removeEventListener: function ( type, listener ) {
+					var i;
+					if ( type === 'visibilitychange' ) {
+						i = listeners.indexOf( listener );
+						if ( i >= 0 ) {
+							listeners.splice( i, 1 );
+						}
+					}
+				},
+				// Helper function to swap visibility and run listeners
+				toggleVisibility: function () {
+					var i;
+					this.hidden = !this.hidden;
+					for ( i = 0; i < listeners.length; i++ ) {
+						listeners[ i ]();
+					}
+				}
+			};
+			mw.setVisibleTimeout.debugOverrideDocument( this.mockDocument );
+
+			this.sandbox.useFakeTimers();
+		}
+	} ) );
+
+	QUnit.test( 'basic usage', function ( assert ) {
+		var called = 0;
+
+		mw.setVisibleTimeout( function () {
+			called++;
+		}, 0 );
+		assert.strictEqual( called, 0 );
+		this.sandbox.clock.tick( 1 );
+		assert.strictEqual( called, 1 );
+
+		mw.setVisibleTimeout( function () {
+			called++;
+		}, 10 );
+		this.sandbox.clock.tick( 10 );
+		assert.strictEqual( called, 2 );
+	} );
+
+	QUnit.test( 'can cancel timeout', function ( assert ) {
+		var called = 0,
+			timeout = mw.setVisibleTimeout( function () {
+				called++;
+			}, 0 );
+
+		mw.clearVisibleTimeout( timeout );
+		this.sandbox.clock.tick( 10 );
+		assert.strictEqual( called, 0 );
+	} );
+
+	QUnit.test( 'start hidden and become visible', function ( assert ) {
+		var called = 0;
+
+		this.mockDocument.hidden = true;
+		mw.setVisibleTimeout( function () {
+			called++;
+		}, 0 );
+		this.sandbox.clock.tick( 10 );
+		assert.strictEqual( called, 0 );
+
+		this.mockDocument.toggleVisibility();
+		this.sandbox.clock.tick( 10 );
+		assert.strictEqual( called, 1 );
+	} );
+
+	QUnit.test( 'timeout is cumulative', function ( assert ) {
+		var called = 0;
+
+		mw.setVisibleTimeout( function () {
+			called++;
+		}, 100 );
+		this.sandbox.clock.tick( 50 );
+		assert.strictEqual( called, 0 );
+
+		this.mockDocument.toggleVisibility();
+		this.sandbox.clock.tick( 1000 );
+		assert.strictEqual( called, 0 );
+
+		this.mockDocument.toggleVisibility();
+		this.sandbox.clock.tick( 50 );
+		assert.strictEqual( called, 1 );
+	} );
+}( mediaWiki ) );
