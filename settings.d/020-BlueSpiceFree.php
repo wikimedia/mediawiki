b3f35c35ae4cef889699f76310f9c428c827c912
Leonid Verhovskij <l.verhovskij@gmail.com>
2017-09-25 13:43:37 +0200

    working draft for bs free init on top of mw 1.30

---
+++ settings.d/020-BlueSpiceFree.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "$IP/extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
+require_once "$IP/extensions/BlueSpiceExtensions/BlueSpiceExtensions.php";
+require_once "$IP/extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php";
+require_once "$IP/skins/BlueSpiceSkin/BlueSpiceSkin.php";
---
+++ settings.d/060-Arrays.php
@@ -0,0 +1,2 @@
+<?php
+require_once( "$IP/extensions/Arrays/Arrays.php" );
