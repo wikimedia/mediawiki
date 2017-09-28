928aed00c51a644af3cb246e94fc78cda43bfc00
jdlrobson <jdlrobson@gmail.com>
2017-09-28 13:42:32 -0500

    Provide message/warning/error box abstraction
    This will help us consolidate the various uses into one single
    method which will help us drive standardisation of these defacto
    widgets.
    
    Hopefully, by being a method of the Html class, which has a very
    low barrier for use will drive down the inconsistent display of
    warning/error boxes across MediaWiki's products
    
    Various usages of warningbox and errorbox have been ported over.
    I've retained some more complicated usages which make use of the
    parser (wrapWikiMsg) and any where id and class are medled with
    - we'll probably want to consider whether we want to encourage
    those going forward as they encourage adjusting the styling.
    
    Bug: T166915
    Change-Id: I2757e1f4ff2599e93a7257fc644cab69063896d2

--- includes/Html.php
+++ includes/Html.php
@@ -697,6 +697,48 @@ class Html {
 	}
 
 	/**
+	 * Return a message box.
+	 * @param string $html of contents of box
+	 * @param string $className corresponding to box
+	 * @param string $heading (optional)
+	 * @return string of html representing a box.
+	 */
+	public static function messageBox( $html, $className, $heading = '' ) {
+		if ( $heading ) {
+			$html = Html::element( 'h2', [], $heading ) . $html . Html::closeElement( 'h2' );
+		}
+		return Html::openElement( 'div', [ 'class' => $className ] ) . $html . Html::closeElement( 'div' );
+	}
+
+	/**
+	 * Return a warning box.
+	 * @param string $html of contents of box
+	 * @return string of html representing a warning box.
+	 */
+	public static function warningBox( $html ) {
+		return self::messageBox( $html, 'warningbox' );
+	}
+
+	/**
+	 * Return an error box.
+	 * @param string $html of contents of error box
+	 * @param string $heading (optional)
+	 * @return string of html representing an error box.
+	 */
+	public static function errorBox( $html, $heading = '' ) {
+		return self::messageBox( $html, 'errorbox', $heading );
+	}
+
+	/**
+	 * Return a success box.
+	 * @param string $html of contents of box
+	 * @return string of html representing a success box.
+	 */
+	public static function successBox( $html ) {
+		return self::messageBox( $html, 'successbox' );
+	}
+
+	/**
 	 * Convenience function to produce a radio button (input element with type=radio)
 	 *
 	 * @param string $name Name attribute
