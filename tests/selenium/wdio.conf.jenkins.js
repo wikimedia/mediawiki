d1439a3e67467dee3c1993943aa7cca1d7904e9e
Željko Filipin <zeljko.filipin@gmail.com>
2017-10-03 11:17:39 +0200

    Selenium tests should log in before creating pages
    MediaWiki Vagrant by default allows creating pages to anonymous users,
    but other environments, like Beta Cluster, require user to log in
    before creating pages.
    
    Bug: T176315
    Change-Id: I34eeaf3cc22a64be6580aa483b28885137ba60b6

--- tests/selenium/wdio.conf.jenkins.js
+++ tests/selenium/wdio.conf.jenkins.js
@@ -1,13 +1,19 @@
 /* eslint no-undef: "error" */
 /* eslint-env node */
 'use strict';
-var merge = require( 'deepmerge' ),
+const merge = require( 'deepmerge' ),
+	password = 'testpass',
+	username = 'WikiAdmin',
 	wdioConf = require( './wdio.conf.js' );
 
 // Overwrite default settings
 exports.config = merge( wdioConf.config, {
-	username: 'WikiAdmin',
-	password: 'testpass',
+	username: process.env.MEDIAWIKI_USER === undefined ?
+		username :
+		process.env.MEDIAWIKI_USER,
+	password: process.env.MEDIAWIKI_PASSWORD === undefined ?
+		password :
+		process.env.MEDIAWIKI_PASSWORD,
 	screenshotPath: '../log/',
 	baseUrl: process.env.MW_SERVER + process.env.MW_SCRIPT_PATH,
 
