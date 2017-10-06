69a6debda6524a8cceb7475a241b77c9fbbfeb6d
Paladox <thomasmulhall410@yahoo.com>
2017-10-06 20:20:57 +0000

    Make contrib link red if user does not exist (optional)
    Bug: T170507
    Change-Id: Id8ac180f002d7c7d00d6fb746cd0cd6965e442f8

--- includes/Linker.php
+++ includes/Linker.php
@@ -921,10 +921,13 @@ class Linker {
 	 * @param int $flags Customisation flags (e.g. Linker::TOOL_LINKS_NOBLOCK
 	 *   and Linker::TOOL_LINKS_EMAIL).
 	 * @param int $edits User edit count (optional, for performance)
+	 * @param bool $redUserLinkIfIp Weather to show a red link for a ip
+	 *   (optional, for backwards compat)
 	 * @return string HTML fragment
 	 */
 	public static function userToolLinks(
-		$userId, $userText, $redContribsWhenNoEdits = false, $flags = 0, $edits = null
+		$userId, $userText, $redContribsWhenNoEdits = false, $flags = 0, $edits = null,
+		$redUserLinkIfIp = false
 	) {
 		global $wgUser, $wgDisableAnonTalk, $wgLang;
 		$talkable = !( $wgDisableAnonTalk && 0 == $userId );
@@ -944,7 +947,7 @@ class Linker {
 					$user = User::newFromId( $userId );
 					$edits = $user->getEditCount();
 				}
-				if ( $edits === 0 ) {
+				if ( $edits === 0 || $userId === 0 && $redUserLinkIfIp ) {
 					$attribs['class'] .= ' new';
 				}
 			}
@@ -978,10 +981,14 @@ class Linker {
 	 * @param int $userId User identifier
 	 * @param string $userText User name or IP address
 	 * @param int $edits User edit count (optional, for performance)
+	 * @param bool $redUserLinkIfIp Weather to show a red link for a ip
+	 *   (optional, for backwards compat)
 	 * @return string
 	 */
-	public static function userToolLinksRedContribs( $userId, $userText, $edits = null ) {
-		return self::userToolLinks( $userId, $userText, true, 0, $edits );
+	public static function userToolLinksRedContribs(
+		$userId, $userText, $edits = null, $redUserLinkIfIp = false
+	) {
+		return self::userToolLinks( $userId, $userText, true, 0, $edits, $redUserLinkIfIp );
 	}
 
 	/**
