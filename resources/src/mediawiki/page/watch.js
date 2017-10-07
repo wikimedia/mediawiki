2ca80fdb66ef217f21075c9e537aef8eed7f9427
Fomafix <fomafix@googlemail.com>
2017-10-07 18:35:14 +0200

    Remove mw.page
    mw.page was deprecated in b3fb2770 in MediaWiki 1.30.
    
    Change-Id: Ia2f040549bb83445a8efcd5841bb64cfc5eee50e

--- resources/src/mediawiki/page/watch.js
+++ resources/src/mediawiki/page/watch.js
@@ -1,8 +1,6 @@
 /**
  * Animate watch/unwatch links to use asynchronous API requests to
  * watch pages, rather than navigating to a different URI.
- *
- * @class mw.page.watch.ajax
  */
 ( function ( mw, $ ) {
 	var watch,
@@ -101,13 +99,6 @@
 		updateWatchLink: updateWatchLink
 	};
 	module.exports = watch;
-
-	// Deprecated since 1.30
-	mw.log.deprecate( mw, 'page',
-		{ watch: watch },
-		'Use require( \'mediawiki.page.watch.ajax\' ) instead.',
-		'mw.page'
-	);
 
 	$( function () {
 		var $links = $( '.mw-watchlink a[data-mw="interface"], a.mw-watchlink[data-mw="interface"]' );
