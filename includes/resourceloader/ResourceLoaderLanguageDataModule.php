12cfc50fc34030dbf66bebabfb1e75c0372ef377
Bartosz Dziewoński <matma.rex@gmail.com>
2017-10-10 22:52:36 +0200

    mediawiki.language: Implement non-digit-grouping of four-digit numbers
    In some languages it's conventional not to insert a thousands
    separator in numbers that are four digits long (1000-9999).
    This now matches the behavior of the PHP Language class.
    
    Bug: T177846
    Change-Id: I6f4e51a907b084bd7fde09cd9c16c9bf41cdc98c

--- includes/resourceloader/ResourceLoaderLanguageDataModule.php
+++ includes/resourceloader/ResourceLoaderLanguageDataModule.php
@@ -40,6 +40,7 @@ class ResourceLoaderLanguageDataModule extends ResourceLoaderModule {
 		return [
 			'digitTransformTable' => $language->digitTransformTable(),
 			'separatorTransformTable' => $language->separatorTransformTable(),
+			'minimumGroupingDigits' => $language->minimumGroupingDigits(),
 			'grammarForms' => $language->getGrammarForms(),
 			'grammarTransformations' => $language->getGrammarTransformations(),
 			'pluralRules' => $language->getPluralRules(),
