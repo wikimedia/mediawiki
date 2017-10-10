70c54139f866603eb1e1b54348b6d564959f53c1
Bartosz Dziewoński <matma.rex@gmail.com>
2017-10-10 22:52:36 +0200

    mediawiki.language: Implement non-digit-grouping of four-digit numbers
    In some languages it's conventional not to insert a thousands
    separator in numbers that are four digits long (1000-9999).
    This now matches the behavior of the PHP Language class.
    
    Bug: T177846
    Change-Id: I6f4e51a907b084bd7fde09cd9c16c9bf41cdc98c

--- tests/qunit/suites/resources/mediawiki/mediawiki.language.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.language.test.js
@@ -31,6 +31,7 @@
 		mw.language.setData( 'en', 'digitGroupingPattern', null );
 		mw.language.setData( 'en', 'digitTransformTable', null );
 		mw.language.setData( 'en', 'separatorTransformTable', null );
+		mw.language.setData( 'en', 'keepFourDigitNumbersUngrouped', null );
 
 		mw.config.set( 'wgUserLanguage', 'en' );
 		// Number grouping patterns are as per http://cldr.unicode.org/translation/number-patterns
@@ -49,11 +50,20 @@
 		mw.language.setData( 'en', 'digitGroupingPattern', null );
 		mw.language.setData( 'en', 'digitTransformTable', null );
 		mw.language.setData( 'en', 'separatorTransformTable', { ',': '.', '.': ',' } );
+		mw.language.setData( 'en', 'keepFourDigitNumbersUngrouped', null );
 		mw.config.set( 'wgUserLanguage', 'en' );
 		mw.config.set( 'wgTranslateNumerals', true );
 
-		assert.equal( mw.language.convertNumber( 1800 ), '1.800', 'formatting' );
+		assert.equal( mw.language.convertNumber( 180 ), '180', 'formatting 3-digit' );
+		assert.equal( mw.language.convertNumber( 1800 ), '1.800', 'formatting 4-digit' );
+		assert.equal( mw.language.convertNumber( 18000 ), '18.000', 'formatting 5-digit' );
+
 		assert.equal( mw.language.convertNumber( '1.800', true ), '1800', 'unformatting' );
+
+		mw.language.setData( 'en', 'keepFourDigitNumbersUngrouped', true );
+		assert.equal( mw.language.convertNumber( 180 ), '180', 'formatting 3-digit with keepFourDigitNumbersUngrouped' );
+		assert.equal( mw.language.convertNumber( 1800 ), '1800', 'formatting 4-digit with keepFourDigitNumbersUngrouped' );
+		assert.equal( mw.language.convertNumber( 18000 ), '18.000', 'formatting 5-digit with keepFourDigitNumbersUngrouped' );
 	} );
 
 	QUnit.test( 'mw.language.convertNumber - digitTransformTable', function ( assert ) {
@@ -61,6 +71,7 @@
 		mw.config.set( 'wgTranslateNumerals', true );
 		mw.language.setData( 'hi', 'digitGroupingPattern', null );
 		mw.language.setData( 'hi', 'separatorTransformTable', { ',': '.', '.': ',' } );
+		mw.language.setData( 'hi', 'keepFourDigitNumbersUngrouped', null );
 
 		// Example from Hindi (MessagesHi.php)
 		mw.language.setData( 'hi', 'digitTransformTable', {
