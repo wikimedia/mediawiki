c5edfc3617b27308355a4d25cbcb0ebd8bb0b032
Chad Horohoe <chadh@wikimedia.org>
2017-10-11 17:17:18 +0000

    Creating new WMF 1.31.0-wmf.3 branch

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.31.0-alpha';
+$wgVersion = '1.31.0-wmf.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
