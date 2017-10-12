c00b40de5575a9b1e1aa0fe54ef8bbd5cad7ceee
smartyrad <smarita.sharma2015@vit.ac.in>
2017-10-12 07:08:44 +0530

    [DO NOT MERGE]Outreachy microtask: collect captcha data from signup page (#2)
    Added a schema called CaptchaMouseData which contains an enum which will
    store the values of the the X and Y coordinate of the mouse click.
    
    BUG: T175331
    Change-Id: I745eea974e62b0d26eb88870d46bf3a79e5d6c0e

---
+++ resources/src/mediawiki.special/mediawiki.special.mouse.click.js
@@ -0,0 +1,9 @@
+/*!
+ * JavaScript for Special:SignUpPageMouseTrack
+ */
+( function ( mw, $ ) {
+	$( '#wpCreateaccount' ).click( function ( e ) {
+		mw.track( 'event.CaptchaMouseData', { MouseClickX: e.pageX, MouseClickY: e.pageY } );
+	} );
+
+}( mediaWiki, jQuery ) );
