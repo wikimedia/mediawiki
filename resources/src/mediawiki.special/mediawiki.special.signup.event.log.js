a6899f6d8372f4cb1efff88fe339af421085f111
Sam0410 <samikshya.chand.ece15@itbhu.ac.in>
2017-10-12 08:44:26 +0530

    [DO NOT MERGE] Outreachy Task T158909
    Change-Id: Id1771466e155a1747d82d71f11bb14c52578b5cc

---
+++ resources/src/mediawiki.special/mediawiki.special.signup.event.log.js
@@ -0,0 +1,15 @@
+/*
+    Event logging on button click
+*/
+( function ( mw, $ ) {
+	$( function () {
+		$( '#wpCreateaccount' ).click( function ( e ) {
+			var info = {
+				userId: mw.user.sessionId(),
+				mouseX: e.clientX,
+				mouseY: e.clientY
+			};
+			mw.track( 'EventLogging', info );
+		} );
+	} );
+}( mediaWiki, jQuery ) );
