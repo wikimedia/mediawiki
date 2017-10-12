e50fe5ec6588d52515d01272900e459dc25d0216
Timo Tijhof <krinklemail@gmail.com>
2017-10-12 21:19:56 +0100

    deferred: Introduce TransactionRoundDefiningUpdate
    This allows scheduling of updates that need to start their own
    transaction round. Specifically for cases where the ability to
    commit early is not enough (which is already possible via LBFactory
    getEmptyTransactionTicket and commitAndWaitForReplication).
    
    Change-Id: I0910587b61c8ddf825f91e92c2f93582cc7ebd80

---
+++ includes/deferred/TransactionRoundDefiningUpdate.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Deferrable update for closure/callback updates that need LBFactory and Database
+ * to be outside anu active transaction round.
+ *
+ * @since 1.31
+ */
+class TransactionRoundDefiningUpdate implements DeferrableUpdate, DeferrableCallback {
+	/** @var callable|null */
+	private $callback;
+	/** @var string */
+	private $fname;
+
+	/**
+	 * @param callable $callback
+	 * @param string $fname Calling method
+	 */
+	public function __construct( callable $callback, $fname = 'unknown' ) {
+		$this->callback = $callback;
+		$this->fname = $fname;
+	}
+
+	public function doUpdate() {
+		call_user_func( $this->callback );
+	}
+
+	public function getOrigin() {
+		return $this->fname;
+	}
+}
