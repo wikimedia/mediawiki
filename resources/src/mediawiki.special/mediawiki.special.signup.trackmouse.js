79db41cb758bc59e0bc0f5520007abed2b693670
vinithegit <vinithacse@gmail.com>
2017-10-17 23:58:21 +0530

    T158909  Automatically detect spambot registration using machine learning: Tracking mouse click position on the create your account button
    Change-Id: If6c73791a2a7089817c495c5f1deb0211a482fde

---
+++ resources/src/mediawiki.special/mediawiki.special.signup.trackmouse.js
@@ -0,0 +1,16 @@
+( function ( mw ) {
+	var elapsed, now, trackingData, averageTypingSpeed = 0, numKeyPresses = 0, startTime = new Date().getTime();
+	$( document ).keyup( function () {
+		numKeyPresses++;
+		now = new Date().getTime();
+		elapsed = now - startTime;
+		averageTypingSpeed = ( elapsed + ( numKeyPresses - 1 ) * averageTypingSpeed ) / numKeyPresses;
+		trackingData = {
+			averageTypingSpeed: averageTypingSpeed
+		};
+		startTime = now;
+	} );
+	$( '#wpCreateaccount' ).click( function( event ) {
+		mw.track( "mediawiki.special.signup.trackmouse", trackingData );
+	});
+}( mediaWiki ) );
