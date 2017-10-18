5243413a40b99cfd94e53568c6665fe8e38af83c
Kunal Mehta <legoktm@member.fsf.org>
2017-10-17 23:54:19 -0700

    shell: Optionally restrict commands' network access with firejail
    Introduces a FirejailCommand class, which can be used to add additional
    restrictions to a command, for increased security. This commit introduces
    Shell::NO_NETWORK to restrict commands from having any external network
    access.
    
    $wgShellRestrictionMethod can be set to false for no restriction system,
    'firejail' to explicitly use it, or null to autodetect whatever system is
    available. In the future the default should be changed to autodetection
    once firejail is tested more.
    
    Bug: T173370
    Change-Id: Id74df0dbba40e1e7c07c4368aacffb6eb06a17c5

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -42,6 +42,7 @@ use MediaWiki\Linker\LinkRendererFactory;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Shell\CommandFactory;
+use MediaWiki\Shell\FirejailCommand;
 
 return [
 	'DBLoadBalancerFactory' => function ( MediaWikiServices $services ) {
@@ -440,7 +441,12 @@ return [
 		];
 		$cgroup = $config->get( 'ShellCgroup' );
 
-		$factory = new CommandFactory( $limits, $cgroup );
+		$restrictionMethod = $config->get( 'ShellRestrictionMethod' );
+		if ( $restrictionMethod === null && FirejailCommand::isUsable() ) {
+			$restrictionMethod = 'firejail';
+		}
+
+		$factory = new CommandFactory( $limits, $cgroup, $restrictionMethod );
 		$factory->setLogger( LoggerFactory::getInstance( 'exec' ) );
 
 		return $factory;
