411c6ae35e457ad12e429da32524e73455f9d064
Michael Holloway <mholloway@wikimedia.org>
2017-10-18 15:42:42 -0400

    Add varianttitles parameter to API query prop=info
    Returns the displaytitle (if present) or title->getPrefixedText for a
    page, converted for each language variant configured on the wiki.
    
    Bug: T178446
    Change-Id: I35100af3824ca65c4fe5c106d4a6fbe4e5f75046

--- includes/api/ApiQueryInfo.php
+++ includes/api/ApiQueryInfo.php
@@ -38,7 +38,7 @@ class ApiQueryInfo extends ApiQueryBase {
 		$fld_readable = false, $fld_watched = false,
 		$fld_watchers = false, $fld_visitingwatchers = false,
 		$fld_notificationtimestamp = false,
-		$fld_preload = false, $fld_displaytitle = false;
+		$fld_preload = false, $fld_displaytitle = false, $fld_varianttitles = false;
 
 	private $params;
 
@@ -53,7 +53,7 @@ class ApiQueryInfo extends ApiQueryBase {
 		$pageLatest, $pageLength;
 
 	private $protections, $restrictionTypes, $watched, $watchers, $visitingwatchers,
-		$notificationtimestamps, $talkids, $subjectids, $displaytitles;
+		$notificationtimestamps, $talkids, $subjectids, $displaytitles, $variantTitles;
 	private $showZeroWatchers = false;
 
 	private $tokenFunctions;
@@ -310,6 +310,7 @@ class ApiQueryInfo extends ApiQueryBase {
 			$this->fld_readable = isset( $prop['readable'] );
 			$this->fld_preload = isset( $prop['preload'] );
 			$this->fld_displaytitle = isset( $prop['displaytitle'] );
+			$this->fld_varianttitles = isset( $prop['varianttitles'] );
 		}
 
 		$pageSet = $this->getPageSet();
@@ -370,6 +371,10 @@ class ApiQueryInfo extends ApiQueryBase {
 
 		if ( $this->fld_displaytitle ) {
 			$this->getDisplayTitle();
+		}
+
+		if ( $this->fld_varianttitles ) {
+			$this->getVariantTitles();
 		}
 
 		/** @var Title $title */
@@ -511,6 +516,12 @@ class ApiQueryInfo extends ApiQueryBase {
 				$pageInfo['displaytitle'] = $this->displaytitles[$pageid];
 			} else {
 				$pageInfo['displaytitle'] = $title->getPrefixedText();
+			}
+		}
+
+		if ( $this->fld_varianttitles ) {
+			if ( isset( $this->variantTitles[$pageid] ) ) {
+				$pageInfo['varianttitles'] = $this->variantTitles[$pageid];
 			}
 		}
 
@@ -744,6 +755,32 @@ class ApiQueryInfo extends ApiQueryBase {
 		}
 	}
 
+	private function getVariantTitles() {
+		if ( !count( $this->titles ) ) {
+			return;
+		}
+		$this->variantTitles = [];
+		foreach ( $this->titles as $pageId => $t ) {
+			$this->variantTitles[$pageId] = isset( $this->displaytitles[$pageId] )
+				? $this->getAllVariants( $this->displaytitles[$pageId] )
+				: $this->getAllVariants( $t->getText(), $t->getNamespace() );
+		}
+	}
+
+	private function getAllVariants( $text, $ns = NS_MAIN ) {
+		$lang = $this->getLanguage();
+		$result = [];
+		foreach ( $lang->getVariants() as $variant ) {
+			$convertTitle = $lang->autoConvert( $text, $variant );
+			if ( $ns !== NS_MAIN ) {
+				$convertNs = $lang->convertNamespace( $ns, $variant );
+				$convertTitle = $convertNs . ':' . $convertTitle;
+			}
+			$result[$variant] = $convertTitle;
+		}
+		return $result;
+	}
+
 	/**
 	 * Get information about watched status and put it in $this->watched
 	 * and $this->notificationtimestamps
@@ -883,6 +920,7 @@ class ApiQueryInfo extends ApiQueryBase {
 			'url',
 			'preload',
 			'displaytitle',
+			'varianttitles',
 		];
 		if ( array_diff( (array)$params['prop'], $publicProps ) ) {
 			return 'private';
@@ -916,6 +954,7 @@ class ApiQueryInfo extends ApiQueryBase {
 					'readable', # private
 					'preload',
 					'displaytitle',
+					'varianttitles',
 					// If you add more properties here, please consider whether they
 					// need to be added to getCacheMode()
 				],
