f04e4d2d31084968101ba5c2bd73ca648035ae64
Brian Wolff <bawolff+wn@gmail.com>
2017-10-18 05:28:43 +0000

    SECURITY: Escape internal error message
    This message contains the request url, which is semi-user controlled.
    Most browsers percent escape < and > so its probably not exploitable
    (curl is an exception here), but nonetheless its not good.
    
    Bug: T178451
    Change-Id: I19358471ddf1b28377aad8e0fb54797c817bb6f6

--- includes/exception/MWException.php
+++ includes/exception/MWException.php
@@ -144,13 +144,15 @@ class MWException extends Exception {
 			$logId = WebRequest::getRequestId();
 			$type = get_class( $this );
 			return "<div class=\"errorbox\">" .
-			'[' . $logId . '] ' .
-			gmdate( 'Y-m-d H:i:s' ) . ": " .
-			$this->msg( "internalerror-fatal-exception",
-				"Fatal exception of type $1",
-				$type,
-				$logId,
-				MWExceptionHandler::getURL( $this )
+			htmlspecialchars(
+				'[' . $logId . '] ' .
+				gmdate( 'Y-m-d H:i:s' ) . ": " .
+				$this->msg( "internalerror-fatal-exception",
+					"Fatal exception of type $1",
+					$type,
+					$logId,
+					MWExceptionHandler::getURL( $this )
+				)
 			) . "</div>\n" .
 			"<!-- Set \$wgShowExceptionDetails = true; " .
 			"at the bottom of LocalSettings.php to show detailed " .
