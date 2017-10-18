c5c091869b11010ac11e25cd6237e711287311e1
Cormac Parle <cparle@wikimedia.org>
2017-10-18 13:38:48 +0100

    Treat svg systemLanguage attribs as arbitrary strings
    Also implement matching of systemLanguage attribs as
    specified in the SVG spec
    
    Note that librsvg that we use for rendering pngs of svg
    files has a bug, and matches languages in the following
    way instead of what is implemented in SVG::getMatchedLanguage()
    
    public function getMatchedLanguage( $userPreferredLanguage, array $svgLanguages ) {
      foreach ( $svgLanguages as $svgLang ) {
        if ($svgLang == $userPreferredLanguage) {
          return $svgLang;
        }
        $dashPosition = strpos( $userPreferredLanguage, '-' );
        if ( $dashPosition !== false ) {
          if (
            strtolower( substr( $svgLang, 0, $dashPosition ) ) ==
            strtolower( substr( $userPreferredLanguage, 0, $dashPosition ) )
          ) {
            return $svgLang;
          }
        }
        return null;
    }
    
    Bug: T154132
    Change-Id: Ibff66a0844f0cecfae0260c6a7d20aeedc2849a2

--- includes/media/SVG.php
+++ includes/media/SVG.php
@@ -96,23 +96,53 @@ class SvgHandler extends ImageHandler {
 			$metadata = $this->unpackMetadata( $metadata );
 			if ( isset( $metadata['translations'] ) ) {
 				foreach ( $metadata['translations'] as $lang => $langType ) {
-					if ( $langType === SVGReader::LANG_FULL_MATCH ) {
-						$langList[] = $lang;
-					}
+					$langList[] = $lang;
 				}
 			}
 		}
-		return $langList;
+		return array_unique( $langList );
 	}
 
 	/**
-	 * What language to render file in if none selected.
+	 * SVG's systemLanguage matching rules state:
+	 * 'The `systemLanguage` attribute ... [e]valuates to "true" if one of the languages indicated
+	 * by user preferences exactly equals one of the languages given in the value of this parameter,
+	 * or if one of the languages indicated by user preferences exactly equals a prefix of one of
+	 * the languages given in the value of this parameter such that the first tag character
+	 * following the prefix is "-".'
+	 *
+	 * Return the first element of $svgLanguages that matches $userPreferredLanguage
+	 *
+	 * @see https://www.w3.org/TR/SVG/struct.html#SystemLanguageAttribute
+	 * @param string $userPreferredLanguage
+	 * @param array $svgLanguages
+	 * @return bool
+	 */
+	public function getMatchedLanguage( $userPreferredLanguage, array $svgLanguages ) {
+		foreach ( $svgLanguages as $svgLang ) {
+			if ( $svgLang == $userPreferredLanguage ) {
+				return $svgLang;
+			}
+			list ( $svgLangPrefix, ) = explode( '-', $svgLang );
+			if ( $svgLangPrefix == $userPreferredLanguage ) {
+				return $svgLang;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * What language to render file in if none selected
+	 *
+	 * SVG does not have a way of specifying which language is default, therefore return the user's
+	 * interface language
 	 *
 	 * @param File $file
-	 * @return string Language code.
+	 * @return string
 	 */
 	public function getDefaultRenderLanguage( File $file ) {
-		return 'en';
+		global $wgLang;
+		return $wgLang->getCode();
 	}
 
 	/**
@@ -479,7 +509,7 @@ class SvgHandler extends ImageHandler {
 			return ( $value > 0 );
 		} elseif ( $name == 'lang' ) {
 			// Validate $code
-			if ( $value === '' || !Language::isValidBuiltInCode( $value ) ) {
+			if ( $value === '' || !Language::isValidCode( $value ) ) {
 				wfDebug( "Invalid user language code\n" );
 
 				return false;
