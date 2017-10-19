5420a7ac44fea985fa7ab66c0118f36f6b431efa
Leonid Verhovskij <info@leonv.de>
2017-10-19 11:04:16 +0200

    Update 020-BlueSpiceFree.php
    reactivate moved extensions

--- settings.d/020-BlueSpiceFree.php
+++ settings.d/020-BlueSpiceFree.php
@@ -2,5 +2,10 @@
 
 require_once __DIR__ . "/../extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
 require_once __DIR__ . "/../extensions/BlueSpiceExtensions/BlueSpiceExtensions.php";
-require_once __DIR__ . "/../extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php"; //activate after repo have been changed to public one and files have been moved
+wfLoadExtension("BlueSpiceSmartList");
+wfLoadExtension("BlueSpiceExtendedStatistics");
+wfLoadExtension("BlueSpiceAuthors");
+wfLoadExtension("BlueSpiceUserManager");
+wfLoadExtension("BlueSpicePageTemplates");
+require_once __DIR__ . "/../extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php";
 require_once __DIR__ . "/../skins/BlueSpiceSkin/BlueSpiceSkin.php";
