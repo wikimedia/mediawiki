19c30a9b92a8634b0b8b19d5a625fe732a06343e
Huji Lee <huji.huji@gmail.com>
2017-11-11 20:44:00 -0500

    UserGroupsChanged hook should specify the performer of the change
    Otherwise, there will be a unit-testing error when Echo is enabled
    
    Bug: T180292
    Change-Id: Ibc185c82ad2a03e06e5727a633e6ab6bccce3345

--- tests/phpunit/includes/auth/AuthPluginPrimaryAuthenticationProviderTest.php
+++ tests/phpunit/includes/auth/AuthPluginPrimaryAuthenticationProviderTest.php
@@ -73,7 +73,7 @@ class AuthPluginPrimaryAuthenticationProviderTest extends \MediaWikiTestCase {
 			);
 		$provider = new AuthPluginPrimaryAuthenticationProvider( $plugin );
 
-		\Hooks::run( 'UserGroupsChanged', [ $user, [ 'added' ], [ 'removed' ] ] );
+		\Hooks::run( 'UserGroupsChanged', [ $user, [ 'added' ], [ 'removed' ], $user ] );
 	}
 
 	public function testOnUserLoggedIn() {
