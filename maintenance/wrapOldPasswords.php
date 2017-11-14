cc61b621e3a5c96761213171eec350432e36c1e7
Gergő Tisza <tgr.huwiki@gmail.com>
2017-11-14 15:01:38 -0800

    Remove $wgAuth usage from wrapOldPasswords.php
    AuthPlugin::allowSetLocalPassword() has been deprecated with no
    real replacement. (Authentication providers largely describe things
    from a client POV, so we can check whether the authentication system
    is configured to use passwords but not whether it uses the
    user_password field. We could check whether the auth providers include
    LocalPasswordAuthenticationProvider but that's rather fragile.)
    
    There does not seem to be much need, either; we can assume the site
    admin has at least a basic knowledge of what they are using and would
    not run wrapOldPasswords.php if the site wasn't using local passwords.
    
    Bug: T180537
    Change-Id: I34f2d8ad0d801b5460b768ad1d52b29b0a1b08db
    (cherry picked from commit 1fe65bd2ad2bc92c175273a8dfa9af4626d00509)

--- maintenance/wrapOldPasswords.php
+++ maintenance/wrapOldPasswords.php
@@ -40,12 +40,6 @@ class WrapOldPasswords extends Maintenance {
 	}
 
 	public function execute() {
-		global $wgAuth;
-
-		if ( !$wgAuth->allowSetLocalPassword() ) {
-			$this->error( '$wgAuth does not allow local passwords. Aborting.', true );
-		}
-
 		$passwordFactory = new PasswordFactory();
 		$passwordFactory->init( RequestContext::getMain()->getConfig() );
 
