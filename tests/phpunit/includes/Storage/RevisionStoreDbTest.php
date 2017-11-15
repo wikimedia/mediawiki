771079e0d5144c921e5251147f2eacf7de83d4e6
addshore <addshorewiki@gmail.com>
2017-11-15 12:02:40 +0000

    WIP RevisionStoreTest
    Change-Id: Iefad870baf2d16f12e9901b303246c64d6431ca6

---
+++ tests/phpunit/includes/Storage/RevisionStoreDbTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace MediaWiki\Tests\Storage;
+
+use MediaWikiTestCase;
+
+/**
+ * @group Database
+ */
+class RevisionStoreTest extends MediaWikiTestCase {
+
+	public function testInsertRevisionOn() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testNewNullRevision() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testIsUnpatrolled() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetRecentChange() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetRevisionById() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetRevisionByTitle() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetRevisionByPageId() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetRevisionFromTimestamp() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testNewRevisionFromArchiveRow() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testNewRevisionFromRow() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testNewMutableRevisionFromArray() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testLoadRevisionFromId() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testLoadRevisionFromPageId() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testLoadRevisionFromTitle() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testLoadRevisionFromTimestamp() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetParentLengths() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetPreviousRevision() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetNextRevision() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetTimestampFromId() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testCountRevisionsByPageId() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testCountRevisionsByTitle() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testUserWasLastToEdit() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+
+	public function testGetKnownCurrentRevision() {
+		// TODO write me
+		$this->markTestSkipped( 'Not yet written.' );
+	}
+	
+
+}
