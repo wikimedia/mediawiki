ecd65750aa4a9cd30fd40afff16e33fa8f65ce77
Roan Kattouw <roan.kattouw@gmail.com>
2017-11-22 17:31:34 -0500

    SpecialListgrouprights: Fix "Namespace restrictions" header ID
    * Strip the '#' from the start of the <h2>'s id attribute
    * Don't parse the header message. It doesn't contain HTML, and it's used
      as plain text anyway.
    
    Change-Id: I380c20293437dbcd3419f83ea8b7a7ee0bb504f1

--- includes/specials/SpecialListgrouprights.php
+++ includes/specials/SpecialListgrouprights.php
@@ -149,11 +149,11 @@ class SpecialListGroupRights extends SpecialPage {
 			return;
 		}
 
-		$header = $this->msg( 'listgrouprights-namespaceprotection-header' )->parse();
+		$header = $this->msg( 'listgrouprights-namespaceprotection-header' )->text();
 		$out->addHTML(
 			Html::rawElement( 'h2', [], Html::element( 'span', [
 				'class' => 'mw-headline',
-				'id' => $wgParser->guessSectionNameFromWikiText( $header )
+				'id' => substr( $wgParser->guessSectionNameFromWikiText( $header ), 1 )
 			], $header ) ) .
 			Xml::openElement( 'table', [ 'class' => 'wikitable' ] ) .
 			Html::element(
