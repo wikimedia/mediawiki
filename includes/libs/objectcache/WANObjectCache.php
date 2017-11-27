300c65537711b10d4765bbe20f7add01307cd188
Aaron Schulz <aschulz@wikimedia.org>
2017-11-27 10:51:32 -0800

    Use time forcing methods to avoid WANObjectCacheTest flakeiness
    Use of microtime() is now just for baselines, and it is no longer
    assumed to be increasing with each call. Such an assumption is
    particuliarly bad on Windows.
    
    I've done 100X run rounds with now failures on Windows.
    
    Change-Id: Ica2a47982495bc95b10ca507414972744ea9507e

--- includes/libs/objectcache/WANObjectCache.php
+++ includes/libs/objectcache/WANObjectCache.php
@@ -1662,6 +1662,9 @@ class WANObjectCache implements IExpiringStore, LoggerAwareInterface {
 
 		$ageStale = abs( $curTTL ); // seconds of staleness
 		$curGTTL = ( $graceTTL - $ageStale ); // current grace-time-to-live
+		if ( $curGTTL <= 0 ) {
+			return false; //  already out of grace period
+		}
 
 		// Chance of using a stale value is the complement of the chance of refreshing it
 		return !$this->worthRefreshExpiring( $curGTTL, $graceTTL );
