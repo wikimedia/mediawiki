3be2ffe2c28ab1d4cd816d829b71f615af87836d
Leonid Verhovskij <info@leonv.de>
2017-11-28 08:34:36 +0100

    Update 020-BlueSpiceWikiAdmin.php

--- settings.d/020-BlueSpiceWikiAdmin.php
+++ settings.d/020-BlueSpiceWikiAdmin.php
@@ -1,5 +1,5 @@
 <?php
-wfLoadExtension("BlueSpiceExtensions/WikiAdmin");
+//wfLoadExtension("BlueSpiceExtensions/WikiAdmin");
 //Workaround from pwirth as of 28.11.2017, this should be removed after module has been defined on another place
 class WikiAdmin {
 	static function registerModuleClass() {}
