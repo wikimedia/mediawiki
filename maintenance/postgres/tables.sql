45f0e755dd633e0f8d2543be91dda0d572bb60d5
Reedy <reedy@wikimedia.org>
2017-11-29 21:02:22 +0000

    Add ip_changes to postgres/tables.sql
    Follow-up Ic11c64813ee04e8520771bfa156f8e51404273e7
    
    Bug: T177258
    Change-Id: I69a278c1928ed91ef5cbe5b66e590eef629a2981

--- maintenance/postgres/tables.sql
+++ maintenance/postgres/tables.sql
@@ -785,3 +785,14 @@ CREATE TABLE site_identifiers (
 CREATE UNIQUE INDEX si_type_key ON site_identifiers (si_type, si_key);
 CREATE INDEX si_site ON site_identifiers (si_site);
 CREATE INDEX si_key ON site_identifiers (si_key);
+
+CREATE SEQUENCE ip_changes_ipc_rev_id_seq;
+
+CREATE TABLE ip_changes (
+  ipc_rev_id        INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('ip_changes_ipc_rev_id_seq'),
+  ipc_rev_timestamp TIMESTAMPTZ NOT NULL,
+  ipc_hex           BYTEA NOT NULL DEFAULT ''
+);
+
+CREATE INDEX ipc_rev_timestamp ON ip_changes (ipc_rev_timestamp);
+CREATE INDEX ipc_hex_time ON ip_changes (ipc_hex,ipc_rev_timestamp);
