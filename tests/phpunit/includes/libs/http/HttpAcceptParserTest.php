bee9f4db96b4aec51feeddb8ccdca353c034f12f
Timo Tijhof <krinklemail@gmail.com>
2017-12-04 19:25:31 +0000

    Remove various redundant '@license' tags in file headers
    Redundant given this is the project-wide license already,
    especially in file headers that already include the GPL license
    header.
    
    This and other minor fixups based on feedback from Ie0cea0ef5027c7e5.
    
    * Add @file where missing.
    * Move @ingroup and @deprecated from file to class doc where needed.
    
    Change-Id: I7067abb7abee1f0c238cb2536e16192e946d8daa

--- tests/phpunit/includes/libs/http/HttpAcceptParserTest.php
+++ tests/phpunit/includes/libs/http/HttpAcceptParserTest.php
@@ -5,7 +5,6 @@ use Wikimedia\Http\HttpAcceptParser;
 /**
  * @covers Wikimedia\Http\HttpAcceptParser
  *
- * @license GPL-2.0+
  * @author Daniel Kinzler
  */
 class HttpAcceptParserTest extends \PHPUnit_Framework_TestCase {
