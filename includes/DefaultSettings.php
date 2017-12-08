830bb58fa2d24eb93d45135c6becd53f6c50d491
Reedy <reedy@wikimedia.org>
2017-12-08 21:58:24 +0000

    Bump $wgVersion
    Remove "alpha quality" stuff from RELEASE-NOTES-1.30
    
    Change-Id: Ia5c5d539d204abedbf8a47c18ed0c6757d6274da

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.30.0-rc.0';
+$wgVersion = '1.30.0';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
