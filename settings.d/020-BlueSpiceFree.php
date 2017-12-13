4d4878efcd0efea1d0e0472f184a6ca7ee486648
Leonid Verhovskij <info@leonv.de>
2017-12-13 10:38:34 +0100

    Update 020-BlueSpiceFree.php

--- settings.d/020-BlueSpiceFree.php
+++ settings.d/020-BlueSpiceFree.php
@@ -2,18 +2,5 @@
 
 require_once __DIR__ . "/../extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
 require_once __DIR__ . "/../extensions/BlueSpiceExtensions/BlueSpiceExtensions.php";
-wfLoadExtension("BlueSpiceContextMenu");
-wfLoadExtension("BlueSpiceEmoticons");
-wfLoadExtension("BlueSpiceDashboards");
-wfLoadExtension("BlueSpiceCountThings");
-wfLoadExtension("BlueSpiceAvatars");
-wfLoadExtension("BlueSpiceInterWikiLinks");
-wfLoadExtension("BlueSpicePagesVisited");
-wfLoadExtension("BlueSpiceSmartList");
-wfLoadExtension("BlueSpiceExtendedStatistics");
-wfLoadExtension("BlueSpiceAuthors");
-wfLoadExtension("BlueSpiceUserManager");
-wfLoadExtension("BlueSpicePageTemplates");
-wfLoadExtension("BlueSpiceExtendedSearch");
 require_once __DIR__ . "/../extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php";
 require_once __DIR__ . "/../skins/BlueSpiceSkin/BlueSpiceSkin.php";
