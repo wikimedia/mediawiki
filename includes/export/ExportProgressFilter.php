16735948acee2012de4d263b215d5b8b3b9f2563
Albert221 <w.albert221@gmail.com>
2017-12-13 17:59:45 +0100

    Fix autoloading of ExportProgressFilter
    Bug: T177239
    Change-Id: Ieb5d5aa78d569af8cd8f8bfa32ce10a33482cb84

---
+++ includes/export/ExportProgressFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @ingroup Dump
+ */
+class ExportProgressFilter extends DumpFilter {
+    /**
+     * @var BackupDumper
+     */
+    private $progress;
+
+    function __construct(&$sink, &$progress ) {
+        parent::__construct( $sink );
+        $this->progress = $progress;
+    }
+
+    function writeClosePage( $string ) {
+        parent::writeClosePage( $string );
+        $this->progress->reportPage();
+    }
+
+    function writeRevision( $rev, $string ) {
+        parent::writeRevision( $rev, $string );
+        $this->progress->revCount();
+    }
+}
