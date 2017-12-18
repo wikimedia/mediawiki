a92c119bbc57509514c452bd6dbb6b15b082b612
Matthias Mullie <git@mullie.eu>
2017-12-18 15:14:38 +0100

    Allow extension of the Special:Upload form
    * Most of the license selection code has been turned into a
      more generic thing:
      * Licenses -> TemplateSelector
      * License -> TemplateSelectorLine
      * uploadLicense -> uploadTemplatePreview
      Extension:3D will make use of these for patent selection in
      Iafb1e7e5da4b67f4c5ae7dda511d130ae10f748c
    * License/TemplateSelectorLine has been modified so the text
      can be wikitext (as needed for patent labels)
    * Like window.wgUploadWarningObj, uploadTemplatePreview is now
      also added to `window` so it can be re-used from elsewhere
      (to preview patent templates)
    
    Bug: T182683
    Change-Id: I0c097442aa557dd90eb5825553ebf892f9af8a01

--- includes/specials/formfields/Licenses.php
+++ includes/specials/formfields/Licenses.php
@@ -27,163 +27,40 @@
 /**
  * A License class for use on Special:Upload
  */
-class Licenses extends HTMLFormField {
-	/** @var string */
-	protected $msg;
-
-	/** @var array */
-	protected $licenses = [];
-
-	/** @var string */
-	protected $html;
-	/**#@-*/
-
+class Licenses extends TemplateSelector {
 	/**
-	 * @param array $params
+	 * {@inheritdoc}
 	 */
 	public function __construct( $params ) {
-		parent::__construct( $params );
-
-		$this->msg = empty( $params['licenses'] )
+		$params['message'] = empty( $params['licenses'] )
 			? wfMessage( 'licenses' )->inContentLanguage()->plain()
 			: $params['licenses'];
-		$this->selected = null;
 
-		$this->makeLicenses();
+		parent::__construct( $params );
 	}
 
 	/**
-	 * @private
-	 */
-	protected function makeLicenses() {
-		$levels = [];
-		$lines = explode( "\n", $this->msg );
-
-		foreach ( $lines as $line ) {
-			if ( strpos( $line, '*' ) !== 0 ) {
-				continue;
-			} else {
-				list( $level, $line ) = $this->trimStars( $line );
-
-				if ( strpos( $line, '|' ) !== false ) {
-					$obj = new License( $line );
-					$this->stackItem( $this->licenses, $levels, $obj );
-				} else {
-					if ( $level < count( $levels ) ) {
-						$levels = array_slice( $levels, 0, $level );
-					}
-					if ( $level == count( $levels ) ) {
-						$levels[$level - 1] = $line;
-					} elseif ( $level > count( $levels ) ) {
-						$levels[] = $line;
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * @param string $str
-	 * @return array
-	 */
-	protected function trimStars( $str ) {
-		$numStars = strspn( $str, '*' );
-		return [ $numStars, ltrim( substr( $str, $numStars ), ' ' ) ];
-	}
-
-	/**
-	 * @param array &$list
-	 * @param array $path
-	 * @param mixed $item
-	 */
-	protected function stackItem( &$list, $path, $item ) {
-		$position =& $list;
-		if ( $path ) {
-			foreach ( $path as $key ) {
-				$position =& $position[$key];
-			}
-		}
-		$position[] = $item;
-	}
-
-	/**
-	 * @param array $tagset
-	 * @param int $depth
-	 */
-	protected function makeHtml( $tagset, $depth = 0 ) {
-		foreach ( $tagset as $key => $val ) {
-			if ( is_array( $val ) ) {
-				$this->html .= $this->outputOption(
-					$key, '',
-					[
-						'disabled' => 'disabled',
-						'style' => 'color: GrayText', // for MSIE
-					],
-					$depth
-				);
-				$this->makeHtml( $val, $depth + 1 );
-			} else {
-				$this->html .= $this->outputOption(
-					$val->text, $val->template,
-					[ 'title' => '{{' . $val->template . '}}' ],
-					$depth
-				);
-			}
-		}
-	}
-
-	/**
-	 * @param string $message
-	 * @param string $value
-	 * @param null|array $attribs
-	 * @param int $depth
-	 * @return string
-	 */
-	protected function outputOption( $message, $value, $attribs = null, $depth = 0 ) {
-		$msgObj = $this->msg( $message );
-		$text = $msgObj->exists() ? $msgObj->text() : $message;
-		$attribs['value'] = $value;
-		if ( $value === $this->selected ) {
-			$attribs['selected'] = 'selected';
-		}
-
-		$val = str_repeat( /* &nbsp */ "\xc2\xa0", $depth * 2 ) . $text;
-		return str_repeat( "\t", $depth ) . Xml::element( 'option', $attribs, $val ) . "\n";
-	}
-
-	/**#@-*/
-
-	/**
-	 *  Accessor for $this->licenses
+	 * Accessor for $this->lines
 	 *
 	 * @return array
+	 *
+	 * @deprecated since 1.31 Use getLines() instead
 	 */
 	public function getLicenses() {
-		return $this->licenses;
+		return $this->getLines();
 	}
 
 	/**
-	 * Accessor for $this->html
-	 *
-	 * @param bool $value
-	 *
-	 * @return string
+	 * {@inheritdoc}
 	 */
 	public function getInputHTML( $value ) {
-		$this->selected = $value;
+		// add a default "no license selected" option
+		$default = $this->buildLine( '|nolicense' );
+		array_unshift( $this->lines, $default );
 
-		$this->html = $this->outputOption( wfMessage( 'nolicense' )->text(), '',
-			(bool)$this->selected ? null : [ 'selected' => 'selected' ] );
-		$this->makeHtml( $this->getLicenses() );
+		$html = parent::getInputHTML( $value );
 
-		$attribs = [
-			'name' => $this->mName,
-			'id' => $this->mID
-		];
-		if ( !empty( $this->mParams['disabled'] ) ) {
-			$attibs['disabled'] = 'disabled';
-		}
-
-		return Html::rawElement( 'select', $attribs, $this->html );
+		array_shift( $this->lines );
+		return $html;
 	}
 }
