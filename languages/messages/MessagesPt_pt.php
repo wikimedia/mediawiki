df13a1ccb5ad7ecbdcc97286a73ebaa827f1ec08
Fomafix <fomafix@googlemail.com>
2017-12-20 06:57:39 +0100

    Introduce language codes 'de-de', 'en-us', 'ko-kr' and 'pt-pt'
    'de-de' (German (Germany)) falls back to 'de' (German).
    'en-us' (American English) falls back to 'en' (English).
    'ko-kr' (Korean (Republic of Korea)) falls back to 'ko' (Korean).
    'pt-pt' (Portuguese (Portugal)) falls back to 'pt' (Portuguese).
    
    This change ensures that
      Accept-Language: de-DE;q=0.9, de-AT;q=0.8, de-CH;q=0.7, de;q=0.6
    selects 'de-de' and not 'de-at'.
    
    Bug: T183295
    Change-Id: I6d5376c9af612ee0ebad3287ae4db84f031b7d5d

---
+++ languages/messages/MessagesPt_pt.php
@@ -0,0 +1,11 @@
+<?php
+/** Portuguese (Portugal) (português de Portugal)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'pt';
