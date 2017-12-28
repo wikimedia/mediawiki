29eebc677349bf173001c452290f2272bffc59c6
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-12-28 16:21:02 +0100

    Remove very, very old disabled test cases and todos
    Some of this is from 2011.
    
    Change-Id: I3cf78e6b451af92063746af87c25c1d352ce94da

--- tests/phpunit/includes/content/WikitextContentHandlerTest.php
+++ tests/phpunit/includes/content/WikitextContentHandlerTest.php
@@ -336,23 +336,6 @@ class WikitextContentHandlerTest extends MediaWikiLangTestCase {
 	}
 
 	/**
-	 * @todo Text case requires database, should be done by a test class in the Database group
-	 */
-	/*
-	public function testGetAutoDeleteReason( Title $title, &$hasHistory ) {}
-	*/
-
-	/**
-	 * @todo Text case requires database, should be done by a test class in the Database group
-	 */
-	/*
-	public function testGetUndoContent( Revision $current, Revision $undo,
-		Revision $undoafter = null
-	) {
-	}
-	*/
-
-	/**
 	 * @covers WikitextContentHandler::getDataForSearchIndex
 	 */
 	public function testDataIndexFieldsFile() {
