997ad244f511a67212159f3f2d3afe93ce2ed24e
Prateek Saxena <prtksxna@gmail.com>
2017-12-29 10:57:44 +0530

    mw.widgets: Add SizeFilterWidget and its PHP implementation
    Bug: T183765
    Change-Id: Ieb551e901405749489059366de9af6316f369cc0

---
+++ includes/widget/SizeFilterWidget.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MediaWiki\Widget;
+
+use \OOUI\RadioSelectInputWidget;
+use \OOUI\TextInputWidget;
+
+/**
+ * Select and input widget.
+ *
+ * @copyright 2011-2017 MediaWiki Widgets Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+class SizeFilterWidget extends \OOUI\Widget {
+
+	protected $radioselect = null;
+	protected $textinput = null;
+
+	/**
+	 * RadioSelectInputWidget and a TextInputWidget to set minimum or maximum byte size
+	 *
+	 * @param array $config Configuration options
+	 *   - array $config['textinput'] Configuration for the TextInputWidget
+	 *   - array $config['radioselectinput'] Configuration for the RadioSelectWidget
+	 *   - bool $congif['selectMin'] Whether to select 'min', false would select 'max'
+	 */
+	public function __construct( array $config = [] ) {
+		// Configuration initialization
+		$config = array_merge( [
+			'selectMin' => true,
+			'textinput' => [],
+			'radioselectinput' => []
+		], $config );
+		$config['textinput'] = array_merge( [
+			'placeholder' => wfMessage( 'pagesize' )->text()
+		], $config['textinput'] );
+		$config['radioselectinput'] = array_merge( [ 'options' => [
+			[
+				'data' => 'min',
+				'label' => wfMessage( 'minimum-size' )->text()
+			],
+			[
+				'data' => 'max',
+				'label' => wfMessage( 'maximum-size' )->text()
+			]
+		] ], $config[ 'radioselectinput' ] );
+
+		// Parent constructor
+		parent::__construct( $config );
+
+		// Properties
+		$this->config = $config;
+		$this->radioselect = new RadioSelectInputWidget( $config[ 'radioselectinput'] );
+		$this->textinput = new TextInputWidget( $config[ 'textinput' ] );
+
+		// Initialization
+		$this->radioselect->setValue( $config[ 'selectMin' ] ? 'min' : 'max' );
+		$this
+			->addClasses( [ 'mw-widget-sizeFilterWidget' ] )
+			->appendContent( $this->radioselect, $this->textinput );
+	}
+
+	protected function getJavaScriptClassName() {
+		return 'mw.widgets.SizeFilterWidget';
+	}
+
+	public function getConfig( &$config ) {
+		$config['textinput'] = $this->config['textinput'];
+		$config['radioselectinput'] = $this->config['radioselectinput'];
+		$config['selectMin'] = $this->config['selectMin'];
+		return parent::getConfig( $config );
+	}
+}
