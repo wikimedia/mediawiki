fff8bab0224cfca457e57e7817b46d8a7e7c769e
RazeSoldier <liguangjie4399@hotmail.com>
2017-12-29 22:03:55 +0800

    Display the file sha1 value in the file info page
    When a visitor accesses an info page of an existing file, base-36 sha1 value of this file will displayed in the table of basic information.
    
    Bug: T181651
    Change-Id: Ib8cae3e4070a186e28e8625556d3c510af0701c2

--- includes/actions/InfoAction.php
+++ includes/actions/InfoAction.php
@@ -437,6 +437,18 @@ class InfoAction extends FormlessAction {
 			];
 		}
 
+		// Display image SHA-1 value
+		if ( $title->inNamespace( NS_FILE ) ) {
+			$fileObj = wfFindFile( $title );
+			if ( $fileObj !== false ) {
+				$output = $fileObj->getSha1();
+				$pageInfo['header-basic'][] = [
+					$this->msg( 'pageinfo-file-hash' ),
+					$output
+				];
+			}
+		}
+
 		// Page protection
 		$pageInfo['header-restrictions'] = [];
 
