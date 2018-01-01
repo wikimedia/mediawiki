040a7372cb29df6a60772e723fdaae4fd47c8a91
Phantom42 <nikitav30@gmail.com>
2018-01-01 21:36:09 +0200

    Add tests for ApiFormatRaw
    Bug: T183767
    Change-Id: I63ce42dd61f6e47f7278c436cad1e4f05e287b04

---
+++ tests/phpunit/includes/api/format/ApiFormatRawTest.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * @group API
+ * @covers ApiFormatRaw
+ */
+class ApiFormatRawTest extends ApiFormatTestBase {
+
+	protected $printerName = 'raw';
+	protected $printerClass = 'ApiFormatRaw';
+
+	/**
+	 * Only basic types are tested here as ApiFormatRaw does not work with
+	 * arrays or objects
+	 * @return array datasets
+	 */
+	public static function provideGeneralEncoding() {
+		return [
+			[
+				[ 'mime' => 'text/plain', 'text' => 'foo' ],
+				'foo',
+				[ 'mime' => 'text/plain' ]
+			],
+			[
+				[ 'mime' => 'text/plain', 'text' => 'fóo' ],
+				'fóo',
+				[ 'mime' => 'text/plain' ]
+			],
+			[
+				[ 'text' => 'some text' ],
+				null,
+				[ 'exception' => [ MWException::class, 'No MIME type set for raw formatter' ] ]
+			],
+			[
+				[ 'mime' => 'text/plain' ],
+				null,
+				[ 'exception' => [ MWException::class, 'No text given for raw formatter' ] ]
+			],
+			'test error fallback' => [
+				[ 'mime' => 'text/plain', 'text' => 'some text', 'error' => 'some error' ],
+				'{"mime":"text/plain","text":"some text","error":"some error"}',
+				[ 'mime' => 'application/json' ]
+			],
+			'test setting filename' => [
+				[ 'mime' => 'text/plain', 'text' => 'some text', 'filename' => 'whatever.raw' ],
+				'some text',
+				[ 'mime' => 'text/plain', 'filename' => 'whatever.raw' ]
+			],
+			'test error fallback with filename' => [
+				[
+					'mime' => 'text/plain',
+					'text' => 'some text',
+					'error' => 'some error',
+					'filename' => 'whatever.raw'
+				],
+				'{"mime":"text/plain","text":"some text","error":"some error","filename":"whatever.raw"}',
+				[ 'mime' => 'application/json', 'filename' => 'api-result.json' ]
+			]
+		];
+	}
+
+	/**
+	 * Prepare factory of ApiFormatRaw printer
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->printerFactory = function ( ApiMain $main ) {
+			return new ApiFormatRaw( $main, new ApiFormatJson( $main, 'json' ) );
+		};
+	}
+
+	/**
+	 * Test using ApiFormatRaw without fallback formatter
+	 */
+	public function testNoFallback() {
+		$this->printerFactory = function ( ApiMain $main ) {
+			return new ApiFormatRaw( $main );
+		};
+		$this->testGeneralEncoding(
+			[ 'mime' => 'text/plain', 'text' => 'some text' ],
+			'some text'
+		);
+	}
+
+	/**
+	 * Check that setting failWithHTTPError to true will result in 400 response status code
+	 */
+	public function testFailWithHTTPError() {
+		$apiMain = null;
+
+		$this->printerFactory = function ( ApiMain $main ) use ( &$apiMain ) {
+			$apiMain = $main;
+			$printer = new ApiFormatRaw( $main, new ApiFormatJson( $main, 'json' ) );
+			$printer->setFailWithHTTPError( true );
+			return $printer;
+		};
+
+		$this->testGeneralEncoding(
+			[ 'mime' => 'text/plain', 'text' => 'some text', 'error' => 'some error' ],
+			'{"mime":"text/plain","text":"some text","error":"some error"}'
+		);
+		$this->assertEquals( 400, $apiMain->getRequest()->response()->getStatusCode() );
+	}
+
+}
