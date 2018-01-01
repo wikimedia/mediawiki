5ca579ad1dca8ae11cf702a2ec6476847f743f21
Phantom42 <nikitav30@gmail.com>
2018-01-01 21:36:09 +0200

    Add tests for ApiFormatRaw
    Bug: T183767
    Change-Id: I63ce42dd61f6e47f7278c436cad1e4f05e287b04

--- tests/phpunit/includes/api/format/ApiFormatTestBase.php
+++ tests/phpunit/includes/api/format/ApiFormatTestBase.php
@@ -58,6 +58,14 @@ abstract class ApiFormatTestBase extends MediaWikiTestCase {
 		$printer = $main->createPrinterByName( $this->printerName );
 		$printer->initPrinter();
 		$printer->execute();
+
+		if ( isset( $params['mime'] ) ) {
+			$this->assertEquals( $params['mime'], $printer->getMimeType() );
+		}
+		if ( isset( $params['filename'] ) ) {
+			$this->assertEquals( $params['filename'], $printer->getFilename() );
+		}
+
 		ob_start();
 		try {
 			$printer->closePrinter();
