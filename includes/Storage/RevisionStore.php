2fdfa65ffc4ef25396988d5c8a533f2d675196d4
addshore <addshorewiki@gmail.com>
2018-01-03 14:42:13 +0000

    WIP DNM MCR RevisionStore::checkDatabaseWikiId use DatabaseDomain
    This still doesn't work for the case in T183914 as the dbname includes
    a - and wfSplitWikiID splits this off assuming it is a prefix..
    
    > var_dump(wfSplitWikiID(wfWikiID()));
    array(2) {
      [0] =>
      string(12) "s53402__wmde"
      [1] =>
      string(8) "uca-test"
    }
    
    We can not comapre a wikiId with a dbDomainId here.
    So convert any wikiIds to dbDomainIds before comapring.
    
    Bug: T183914
    Change-Id: I45a151a332145438c484e02bbe96cd85dd602b9e

--- includes/Storage/RevisionStore.php
+++ includes/Storage/RevisionStore.php
@@ -1476,10 +1476,20 @@ class RevisionStore implements IDBAccessObject, RevisionFactory, RevisionLookup 
 		$storeWiki = $storeWiki ?: wfWikiID();
 		$dbWiki = $dbWiki ?: wfWikiID();
 
-		if ( $dbWiki !== $storeWiki ) {
-			throw new MWException( "RevisionStore for $storeWiki "
-				. "cannot be used with a DB connection for $dbWiki" );
+		if ( $dbWiki === $storeWiki ) {
+			return;
 		}
+
+		// HACK: counteract encoding imposed by DatabaseDomain
+		$storeWiki = str_replace( '?h', '-', $storeWiki );
+		$dbWiki = str_replace( '?h', '-', $dbWiki );
+
+		if ( $dbWiki === $storeWiki ) {
+			return;
+		}
+
+		throw new MWException( "RevisionStore for $storeWiki "
+			. "cannot be used with a DB connection for $dbWiki" );
 	}
 
 	/**
