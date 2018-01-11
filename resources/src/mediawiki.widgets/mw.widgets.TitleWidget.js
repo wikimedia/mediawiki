151e8fe5587d82b29b726db8902eaccaf62c87b1
Fomafix <fomafix@googlemail.com>
2018-01-11 23:27:28 +0100

    [WIP] Widgets: Allow titles with name of Object.prototypes
    Bug: T184776
    Change-Id: I1ba75e779e9c4f0ce5957d752a02da27044b9d07

--- resources/src/mediawiki.widgets/mw.widgets.TitleWidget.js
+++ resources/src/mediawiki.widgets/mw.widgets.TitleWidget.js
@@ -5,6 +5,7 @@
  * @license The MIT License (MIT); see LICENSE.txt
  */
 ( function ( $, mw ) {
+	var hasOwn = Object.prototype.hasOwnProperty;
 
 	/**
 	 * Mixin for title widgets
@@ -254,7 +255,7 @@
 				titles.push( suggestionPage.title );
 			}
 
-			redirects = redirectsTo[ suggestionPage.title ] || [];
+			redirects = hasOwn.call( redirectsTo, suggestionPage.title ) ? redirectsTo[ suggestionPage.title ] : [];
 			for ( i = 0, len = redirects.length; i < len; i++ ) {
 				pageData[ redirects[ i ] ] = {
 					missing: false,
@@ -278,7 +279,7 @@
 		// mismatch where normalisation would make them matching (T50476)
 
 		pageExistsExact = (
-			Object.prototype.hasOwnProperty.call( pageData, this.getQueryValue() ) &&
+			hasOwn.call( pageData, this.getQueryValue() ) &&
 			(
 				!pageData[ this.getQueryValue() ].missing ||
 				pageData[ this.getQueryValue() ].known
@@ -286,7 +287,7 @@
 		);
 		pageExists = pageExistsExact || (
 			titleObj &&
-			Object.prototype.hasOwnProperty.call( pageData, titleObj.getPrefixedText() ) &&
+			hasOwn.call( pageData, titleObj.getPrefixedText() ) &&
 			(
 				!pageData[ titleObj.getPrefixedText() ].missing ||
 				pageData[ titleObj.getPrefixedText() ].known
@@ -303,7 +304,7 @@
 		}
 
 		for ( i = 0, len = titles.length; i < len; i++ ) {
-			page = pageData[ titles[ i ] ] || {};
+			page = hasOwn.call( pageData, titles[ i ] ) ? pageData[ titles[ i ] ] : {};
 			items.push( this.createOptionWidget( this.getOptionWidgetData( titles[ i ], page ) ) );
 		}
 
