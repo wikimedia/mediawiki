976a882b141af85fe7448a2a01439c80bb552014
addshore <addshorewiki@gmail.com>
2018-01-13 14:25:09 +0000

    selenium: add new message banner test to user spec
    Change-Id: I92dc612d215d1d3eb8dff02c2ff4d57da0105d69

--- tests/selenium/pageobjects/page.js
+++ tests/selenium/pageobjects/page.js
@@ -1,6 +1,7 @@
 // From http://webdriver.io/guide/testrunner/pageobjects.html
 'use strict';
 class Page {
+	get usermessage() { return browser.element( 'div.usermessage' ); }
 	open( path ) {
 		browser.url( '/index.php?title=' + path );
 	}
