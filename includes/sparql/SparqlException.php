f16516498c022b54d80002894b260918c49407bb
Stanislav Malyshev <smalyshev@gmail.com>
2018-01-12 17:00:28 -0800

    [WIP] Add SPARQL client to core
    This will be used for deep category search implementation,
    also Wikibase one in repo/maintenance will be changed
    to use the same codebase.
    
    Bug: T184840
    Change-Id: Ie8dd4a5aff55d90f02426f1430ed5214c7327bbc

---
+++ includes/sparql/SparqlException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SPARQL;
+
+use Exception;
+
+/**
+ * Exception for SPARQLClient
+ * @license GPL-2.0+
+ * @author Stas Malyshev
+ */
+class SparqlException extends Exception {
+}
