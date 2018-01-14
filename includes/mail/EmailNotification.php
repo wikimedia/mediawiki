ddd212d9d32f7749d52a53eeeb8a93f168025dc7
Alex Monk <krenair@gmail.com>
2018-01-14 15:46:05 +0000

    EmailNotification: Add newline before minor edit text
    Otherwise you get something like this:
    "Editor's summary: interwiki fix This is a minor edit"
    It's not entirely clear that the editor's summary has ended and the minor edit
    text has been added by the software.
    
    Change-Id: Iab79b5f8a1f6f319e9fd284835b7a4e86c3f0394

--- includes/mail/EmailNotification.php
+++ includes/mail/EmailNotification.php
@@ -342,8 +342,8 @@ class EmailNotification {
 
 		$keys['$PAGETITLE'] = $this->title->getPrefixedText();
 		$keys['$PAGETITLE_URL'] = $this->title->getCanonicalURL();
-		$keys['$PAGEMINOREDIT'] = $this->minorEdit ?
-			wfMessage( 'enotif_minoredit' )->inContentLanguage()->text() : '';
+		$keys['$PAGEMINOREDIT'] = "\n" . ( $this->minorEdit ?
+			wfMessage( 'enotif_minoredit' )->inContentLanguage()->text() : '' );
 		$keys['$UNWATCHURL'] = $this->title->getCanonicalURL( 'action=unwatch' );
 
 		if ( $this->editor->isAnon() ) {
