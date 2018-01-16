0c3809c7be87a90a6599af05e3c9de5760c0817e
addshore <addshorewiki@gmail.com>
2018-01-16 13:53:22 +0000

    WIP [MCR] NameTableSqlStore
    General porpose cached store for things like:
     - content_models (id,name)
     - slot_roles (id,name)
    And in the future possiblly namespaces & content_formats
    as mentioned at:
    https://www.mediawiki.org/wiki/Multi-Content_Revisions/Schema_Migration#Name_tables
    
    Change-Id: Ia550ef7fe30af25ac3fee5ac8a89d032544563bf

---
+++ includes/Storage/NameTableSqlStore.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Storage;
+
+use IExpiringStore;
+use LogicException;
+use Psr\Log\LoggerInterface;
+use WANObjectCache;
+use Wikimedia\Assert\Assert;
+use Wikimedia\Rdbms\Database;
+use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\IResultWrapper;
+use Wikimedia\Rdbms\LoadBalancer;
+
+/**
+ * @author Addshore
+ * @since 1.31
+ */
+class NameTableSqlStore {
+
+	/** @var LoadBalancer */
+	private $loadBalancer;
+
+	/** @var WANObjectCache */
+	private $cache;
+
+	/** @var LoggerInterface */
+	private $logger;
+
+	/** @var string[] */
+	private $tableCache = null;
+
+	/** @var bool|string */
+	private $wikiId = false;
+
+	/** @var string */
+	private $table;
+	/** @var string */
+	private $idField;
+	/** @var string */
+	private $nameField;
+
+	/**
+	 * @param LoadBalancer $dbLoadBalancer A load balancer for acquiring database connections
+	 * @param WANObjectCache $cache A cache manager for caching data
+	 * @param LoggerInterface $logger
+	 * @param string $table
+	 * @param string $idField
+	 * @param string $nameField
+	 * @param bool|string $wikiId The ID of the target wiki database. Use false for the local wiki.
+	 */
+	public function __construct(
+		LoadBalancer $dbLoadBalancer,
+		WANObjectCache $cache,
+		LoggerInterface $logger,
+		$table,
+		$idField,
+		$nameField,
+		$wikiId = false
+	) {
+		$this->loadBalancer = $dbLoadBalancer;
+		$this->cache = $cache;
+		$this->logger = $logger;
+		$this->table = $table;
+		$this->idField = $idField;
+		$this->nameField = $nameField;
+		$this->wikiId = $wikiId;
+	}
+
+	/**
+	 * @param int $index A database index, like DB_MASTER or DB_REPLICA
+	 *
+	 * @return IDatabase
+	 */
+	private function getDBConnection( $index ) {
+		return $this->loadBalancer->getConnection( $index, [], $this->wikiId );
+	}
+
+	private function getCacheKey() {
+		return $this->cache->makeKey( 'NameTableSqlStore', $this->table );
+	}
+
+	/**
+	 * Acquire the id of the given name.
+	 * This creates a row in the table if it doesn't already exist.
+	 *
+	 * @param string $name
+	 * @throws NameTableAccessException
+	 * @return int
+	 */
+	public function acquireId( $name ) {
+		$table = $this->getCachedTable();
+		$searchResult = array_search( $name, $table, true );
+		if ( $searchResult === false ) {
+			$id = $this->store( $name );
+			if ( $id === null ) {
+				// RACE: $name was already in the db, probably just inserted, so load from master
+				$dbMaster = $this->getDBConnection( DB_MASTER );
+				$table = $this->loadTable( $dbMaster );
+				$searchResult = array_search( $name, $table, true );
+				if ( $searchResult === false ) {
+					// Insert failed due to IGNORE flag, but DB_MASTER didn't give us the data
+					throw new NameTableAccessException(
+						"No insert possible but master didn't give us a record for: {$name} in {$this->table}"
+					);
+				}
+				$this->updateCachedTable( $table, $dbMaster );
+			} else {
+				$table[$id] = $name;
+				$searchResult = $id;
+				$this->updateCachedTable( $table );
+			}
+		}
+
+		return $searchResult;
+	}
+
+	/**
+	 * Get the id of the given name.
+	 * If the name doesn't exist this will throw.
+	 * This should be used in cases where we believe the name already exists or want to check for
+	 * existence.
+	 *
+	 * @param string $name
+	 * @throws NameTableAccessException The name does not exist
+	 * @return int Id
+	 */
+	public function getId( $name ) {
+		Assert::parameterType( 'string', $name, '$name' );
+
+		$table = $this->getCachedTable();
+		$searchResult = array_search( $name, $table, true );
+
+		if ( $searchResult !== false ) {
+			return $searchResult;
+		}
+
+		throw NameTableAccessException::newFromDetails( $this->table, 'name', $name );
+	}
+
+	/**
+	 * Get the name of the given id.
+	 * If the id doesn't exist this will throw.
+	 * This should be used in cases where we believe the id already exists or want to check for
+	 * existence.
+	 *
+	 * @param int $id
+	 * @throws NameTableAccessException The id does not exist
+	 * @return string name
+	 */
+	public function getName( $id ) {
+		Assert::parameterType( 'integer', $id, '$id' );
+
+		$table = $this->getCachedTable();
+		if ( array_key_exists( $id, $table ) ) {
+			return $table[$id];
+		}
+
+		// TODO the accessor knows the id, so they probably know this exists, fall back to master
+		// if it doesn't appear to?
+		throw NameTableAccessException::newFromDetails( $this->table, 'id', $id );
+	}
+
+	/**
+	 * Get the whole table, in no particular order.
+	 * This method could be subject to DB or cache lag.
+	 *
+	 * @return string[] keys are the name ids, values are the names themselves
+	 *  Example: [ 1 => 'foo', 3 => 'bar' ]
+	 */
+	public function getTable() {
+		return $this->getCachedTable();
+	}
+
+	/**
+	 * @return string[]
+	 */
+	private function getCachedTable() {
+		if ( $this->tableCache !== null ) {
+			return $this->tableCache;
+		}
+		$table = $this->cache->getWithSetCallback(
+			$this->getCacheKey(),
+			IExpiringStore::TTL_MONTH,
+			function ( $unused, &$ttl, &$setOpts ) {
+				$dbr = $this->getDBConnection( DB_REPLICA );
+				$setOpts += Database::getCacheSetOptions( $dbr );
+				return $this->loadTable( $dbr );
+			}
+		);
+
+		$this->tableCache = $table;
+
+		return $table;
+	}
+
+	/**
+	 * Stores the table in the WAN cache and tableCache property
+	 *
+	 * @param string[] $table
+	 * @param IDatabase $db Database that the data came from (optional)
+	 */
+	private function updateCachedTable( $table, IDatabase $db = null ) {
+		$this->cache->set(
+			$this->getCacheKey(),
+			$table,
+			IExpiringStore::TTL_MONTH,
+			( $db !== null ? Database::getCacheSetOptions( $db ) : [] )
+		);
+		$this->tableCache = $table;
+	}
+
+	/**
+	 * Gets the table from the db
+	 *
+	 * @param IDatabase $db
+	 *
+	 * @return string[]
+	 */
+	private function loadTable( IDatabase $db ) {
+		$result = $db->select(
+			$this->table,
+			[
+				'id' => $this->idField,
+				'name' => $this->nameField
+			],
+			[],
+			__METHOD__
+		);
+
+		$assocArray = [];
+		foreach ( $result as $row ) {
+			$assocArray[$row->id] = $row->name;
+		}
+
+		return $assocArray;
+	}
+	/**
+	 * Stores the given name in the DB, returning the ID when an insert occours.
+	 *
+	 * @param string $name
+	 * @return int|null int if we know the ID, null if we don't
+	 */
+	private function store( $name ) {
+		Assert::parameterType( 'string', $name, '$name' );
+
+		$dbw = $this->getDBConnection( DB_MASTER );
+
+		$dbw->insert(
+			$this->table,
+			[ $this->nameField => $name ],
+			__METHOD__,
+			[ 'IGNORE' ]
+		);
+
+		if ( $dbw->affectedRows() === 0 ) {
+			$this->logger->info(
+				'Tried to insert name into table ' . $this->table . ', but value already existed.'
+			);
+			return null;
+		}
+
+		return $dbw->insertId();
+	}
+
+}
---
+++ tests/phpunit/includes/Storage/NameTableSqlStoreTest.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace MediaWiki\Tests\Storage;
+
+use BagOStuff;
+use EmptyBagOStuff;
+use HashBagOStuff;
+use MediaWiki\Storage\NameTableAccessException;
+use MediaWiki\Storage\NameTableSqlStore;
+use MediaWikiTestCase;
+use Psr\Log\NullLogger;
+use WANObjectCache;
+use Wikimedia\Rdbms\Database;
+use Wikimedia\Rdbms\LoadBalancer;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @author Addshore
+ * @group Database
+ * @covers \MediaWiki\Storage\NameTableSqlStore
+ */
+class NameTableSqlStoreTest extends MediaWikiTestCase {
+
+	public function setUp() {
+		$this->tablesUsed[] = 'slot_roles';
+		parent::setUp();
+	}
+
+	private function populateTable( $values ) {
+		$insertValues = [];
+		foreach ( $values as $name ) {
+			$insertValues[] = [ 'role_name' => $name ];
+		}
+		$this->db->insert( 'slot_roles', $insertValues );
+	}
+
+	private function getHashWANObjectCache( $cacheBag ) {
+		return new WANObjectCache( [ 'cache' => $cacheBag ] );
+	}
+
+	/**
+	 * @param $db
+	 * @return \PHPUnit_Framework_MockObject_MockObject|LoadBalancer
+	 */
+	private function getMockLoadBalancer( $db ) {
+		$mock = $this->getMockBuilder( LoadBalancer::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$mock->expects( $this->any() )
+			->method( 'getConnection' )
+			->willReturn( $db );
+		return $mock;
+	}
+
+	private function getCallCheckingDb( $insertCalls, $selectCalls ) {
+		$mock = $this->getMockBuilder( Database::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$mock->expects( $this->exactly( $insertCalls ) )
+			->method( 'insert' )
+			->willReturnCallback( function () {
+				return call_user_func_array( [ $this->db, 'insert' ], func_get_args() );
+			} );
+		$mock->expects( $this->exactly( $selectCalls ) )
+			->method( 'select' )
+			->willReturnCallback( function () {
+				return call_user_func_array( [ $this->db, 'select' ], func_get_args() );
+			} );
+		$mock->expects( $this->exactly( $insertCalls ) )
+			->method( 'affectedRows' )
+			->willReturnCallback( function () {
+				return call_user_func_array( [ $this->db, 'affectedRows' ], func_get_args() );
+			} );
+		$mock->expects( $this->exactly( $insertCalls ) )
+			->method( 'insertId' )
+			->willReturnCallback( function () {
+				return call_user_func_array( [ $this->db, 'insertId' ], func_get_args() );
+			} );
+		return $mock;
+	}
+
+	private function getNameTableSqlStore( BagOStuff $cacheBag, $insertCalls, $selectCalls ) {
+		return new NameTableSqlStore(
+			$this->getMockLoadBalancer( $this->getCallCheckingDb( $insertCalls, $selectCalls ) ),
+			$this->getHashWANObjectCache( $cacheBag ),
+			new NullLogger(),
+			'slot_roles', 'role_id', 'role_name'
+		);
+	}
+
+	public function provideGetAndAcquireId() {
+		return [
+			'no wancache, empty table' =>
+				[ new EmptyBagOStuff(), true, 2, [], 'foo', 1 ],
+			'no wancache, one matching value' =>
+				[ new EmptyBagOStuff(), false, 2, [ 'foo' ], 'foo', 1 ],
+			'no wancache, one not matching value' =>
+				[ new EmptyBagOStuff(), true, 2, [ 'bar' ], 'foo', 2 ],
+			'no wancache, multiple, one matching value' =>
+				[ new EmptyBagOStuff(), false, 2, [ 'foo', 'bar' ], 'bar', 2 ],
+			'no wancache, multiple, no matching value' =>
+				[ new EmptyBagOStuff(), true, 2, [ 'foo', 'bar' ], 'baz', 3 ],
+			'wancache, empty table' =>
+				[ new HashBagOStuff(), true, 1, [], 'foo', 1 ],
+			'wancache, one matching value' =>
+				[ new HashBagOStuff(), false, 1, [ 'foo' ], 'foo', 1 ],
+			'wancache, one not matching value' =>
+				[ new HashBagOStuff(), true, 1, [ 'bar' ], 'foo', 2 ],
+			'wancache, multiple, one matching value' =>
+				[ new HashBagOStuff(), false, 1, [ 'foo', 'bar' ], 'bar', 2 ],
+			'wancache, multiple, no matching value' =>
+				[ new HashBagOStuff(), true, 1, [ 'foo', 'bar' ], 'baz', 3 ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideGetAndAcquireId
+	 * @param BagOStuff $cacheBag to use in the WANObjectCache service
+	 * @param bool $needsInsert Does the value we are testing need to be inserted?
+	 * @param int $selectCalls Number of times the select DB method will be called
+	 * @param string[] $existingValues to be added to the db table
+	 * @param string $name name to acquire
+	 * @param int $expectedId the id we expect the name to have
+	 */
+	public function testGetAndAcquireId(
+		$cacheBag,
+		$needsInsert,
+		$selectCalls,
+		$existingValues,
+		$name,
+		$expectedId
+	) {
+		$this->populateTable( $existingValues );
+		$store = $this->getNameTableSqlStore( $cacheBag, (int)$needsInsert, $selectCalls );
+
+		// Some names will not initially exist
+		try {
+			$result = $store->getId( $name );
+			$this->assertSame( $expectedId, $result );
+		} catch ( NameTableAccessException $e ) {
+			if ( $needsInsert ) {
+				$this->assertTrue( true ); // Expected exception
+			} else {
+				$this->fail( 'Did not expect an exception, but got one: ' . $e->getMessage() );
+			}
+		}
+
+		// All names should return their id here
+		$this->assertSame( $expectedId, $store->acquireId( $name ) );
+
+		// acquireId inserted these names, so now everything should exist with getId
+		$this->assertSame( $expectedId, $store->getId( $name ) );
+
+		// If we reset the class cache, this second getId will either hit the WAN cache or DB
+		TestingAccessWrapper::newFromObject( $store )->tableCache = null;
+		$this->assertSame( $expectedId, $store->getId( $name ) );
+	}
+
+	public function provideGetName() {
+		return [
+			[ new HashBagOStuff(), 3, 1 ],
+			[ new EmptyBagOStuff(), 3, 3 ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideGetName
+	 */
+	public function testGetName( $cacheBag, $insertCalls, $selectCalls ) {
+		$store = $this->getNameTableSqlStore( $cacheBag, $insertCalls, $selectCalls );
+
+		// Get 1 ID and make sure getName returns correctly
+		$fooId = $store->acquireId( 'foo' );
+		$this->assertSame( 'foo', $store->getName( $fooId ) );
+
+		// Get another ID and make sure getName returns correctly
+		$barId = $store->acquireId( 'bar' );
+		$this->assertSame( 'bar', $store->getName( $barId ) );
+
+		// Blitz the cache and make sure it still returns
+		TestingAccessWrapper::newFromObject( $store )->tableCache = null;
+		$this->assertSame( 'foo', $store->getName( $fooId ) );
+		$this->assertSame( 'bar', $store->getName( $barId ) );
+
+		// Blitz the cache again and get another ID and make sure getName returns correctly
+		TestingAccessWrapper::newFromObject( $store )->tableCache = null;
+		$bazId = $store->acquireId( 'baz' );
+		$this->assertSame( 'baz', $store->getName( $bazId ) );
+		$this->assertSame( 'baz', $store->getName( $bazId ) );
+	}
+
+	public function testGetTable_empty() {
+		$this->populateTable( [] );
+		$store = $this->getNameTableSqlStore( new HashBagOStuff(), 0, 1 );
+		$table = $store->getTable();
+		$this->assertSame( [], $table );
+	}
+
+	public function testGetTable_twoValues() {
+		$this->populateTable( [ 'foo', 'bar' ] );
+		$store = $this->getNameTableSqlStore( new HashBagOStuff(), 0, 1 );
+
+		// We are using a cache, so 2 calls should only result in 1 select on the db
+		$store->getTable();
+		$table = $store->getTable();
+
+		$expected = [ 2 => 'bar', 1 => 'foo' ];
+		$this->assertSame( $expected, $table );
+		// Make sure the table returned is the same as the cached table
+		$this->assertSame( $expected, TestingAccessWrapper::newFromObject( $store )->tableCache );
+	}
+
+	public function testCacheRaceCondition() {
+		$wanHashBag = new HashBagOStuff();
+		$store1 = $this->getNameTableSqlStore( $wanHashBag, 1, 1 );
+		$store2 = $this->getNameTableSqlStore( $wanHashBag, 1, 0 );
+		$store3 = $this->getNameTableSqlStore( $wanHashBag, 0, 0 );
+
+		// Cache the current table in all instances (WAN and proc cache)
+		$store1->getTable();
+		$store2->getTable();
+		$store3->getTable();
+
+		// Store 2 separate names in each instance
+		$fooId = $store1->acquireId( 'foo' );
+		$barId = $store2->acquireId( 'bar' );
+
+		// Try to get the IDs of both name added above
+		$this->assertSame( $fooId, $store3->getId( 'foo' ) );
+		$this->assertSame( $barId, $store3->getId( 'bar' ) );
+	}
+
+}
