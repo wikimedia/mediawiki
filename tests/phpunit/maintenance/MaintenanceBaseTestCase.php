87b780700f7f213193647479075215c13028e9f2
Adam Roses Wight <awight@wikimedia.org>
2018-01-16 18:41:39 -0500

    Refactor MaintenanceTest to use a base test case
    Bug: T184775
    Change-Id: I0cb2320f765e847a59c721918401da07a3a2b24a

---
+++ tests/phpunit/maintenance/MaintenanceBaseTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MediaWiki\Tests\Maintenance;
+
+use MediaWikiTestCase;
+
+class MaintenanceBaseTestCase extends MediaWikiTestCase {
+
+	/**
+	 * The main Maintenance instance that is used for testing, wrapped and mockable.
+	 *
+	 * @var Maintenance
+	 */
+	protected $maintenance;
+
+	/**
+	 * Do a little stream cleanup to prevent output in case the child class
+	 * hasn't tested the capture buffer.
+	 */
+	protected function tearDown() {
+		if ( $this->maintenance ) {
+			$this->maintenance->outputChanneled( false );
+		}
+		parent::tearDown();
+	}
+
+	/**
+	 * Asserts the output before and after simulating shutdown
+	 *
+	 * This function simulates shutdown of self::maintenance.
+	 *
+	 * @param string $preShutdownOutput Expected output before simulating shutdown
+	 * @param bool $expectNLAppending Whether or not shutdown simulation is expected
+	 *   to add a newline to the output. If false, $preShutdownOutput is the
+	 *   expected output after shutdown simulation. Otherwise,
+	 *   $preShutdownOutput with an appended newline is the expected output
+	 *   after shutdown simulation.
+	 */
+	protected function assertOutputPrePostShutdown( $preShutdownOutput, $expectNLAppending ) {
+		$this->assertEquals( $preShutdownOutput, $this->getActualOutput(),
+				"Output before shutdown simulation" );
+
+		$this->maintenance->outputChanneled( false );
+
+		$postShutdownOutput = $preShutdownOutput . ( $expectNLAppending ? "\n" : "" );
+		$this->expectOutputString( $postShutdownOutput );
+	}
+
+}
+
