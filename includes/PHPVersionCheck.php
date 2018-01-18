5f3e273e563b93dbb4da46e11719e80e781b5ff2
James D. Forrester <jforrester@wikimedia.org>
2018-01-18 15:34:32 -0800

    [DNM] Bump PHP version requirement to 7.0.0+
    Intended to be merged into master before the branch of REL1_31, pending the
    upgrade and clean-up of Wikimedia production to make this possible.
    
    Bug: T172165
    Change-Id: I740f32ac859d9bb3787fdf8414f82ae6f410492f

--- includes/PHPVersionCheck.php
+++ includes/PHPVersionCheck.php
@@ -95,7 +95,7 @@ class PHPVersionCheck {
 			'version' => PHP_VERSION,
 			'vendor' => 'the PHP Group',
 			'upstreamSupported' => '5.6.0',
-			'minSupported' => '5.5.9',
+			'minSupported' => '7.0.0',
 			'upgradeURL' => 'https://secure.php.net/downloads.php',
 		);
 	}
