90d7bd363733071bcfc71a648459d859ea5015c5
Max Semenik <maxsem.wiki@gmail.com>
2018-01-18 17:44:03 -0800

    Deprecate wfShellWikiCmd()
    Bug: T184339
    Change-Id: Ic86a451e0e9d609e06865a4969560d151efa844c

--- tests/phpunit/maintenance/MaintenanceTest.php
+++ tests/phpunit/maintenance/MaintenanceTest.php
@@ -4,6 +4,8 @@ namespace MediaWiki\Tests\Maintenance;
 
 use Maintenance;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Shell\Command;
+use MediaWiki\Shell\Shell;
 use Wikimedia\TestingAccessWrapper;
 
 /**
@@ -532,5 +534,73 @@ class MaintenanceTest extends MaintenanceBaseTestCase {
 		$this->assertEquals( [ [ 'multi', 'yo' ] ], $m2->orderedOptions );
 
 		$m2->cleanupChanneled();
+	}
+
+	/**
+	 * @covers Maintenance::makeScriptCommand
+	 * @dataProvider provideMakeScriptCommand
+	 *
+	 * @param string $expected
+	 * @param string $script
+	 * @param string[] $parameters
+	 * @param string[] $options
+	 * @param callable|null $hook
+	 */
+	public function testMakeScriptCommand( $expected,
+		$script,
+		$parameters,
+		$options = [],
+		$hook = null
+	) {
+		// Running tests under Vagrant involves MWMultiVersion that uses the below hook
+		$this->setMwGlobals( 'wgHooks', [] );
+
+		if ( $hook ) {
+			$this->setTemporaryHook( 'wfShellWikiCmd', $hook );
+		}
+
+		$command = Maintenance::makeScriptCommand( $script, $parameters, $options );
+		$command->params( 'safe' )
+			->unsafeParams( 'unsafe' );
+
+		$this->assertType( Command::class, $command );
+
+		$wrapper = TestingAccessWrapper::newFromObject( $command );
+		$this->assertEquals( $expected, $wrapper->command );
+		$this->assertEquals( 0, $wrapper->restrictions & Shell::NO_LOCALSETTINGS );
+	}
+
+	public function provideMakeScriptCommand() {
+		global $wgPhpCli;
+
+		return [
+			[
+				"'$wgPhpCli' 'maintenance/foobar.php' 'bar'\\''\"baz' 'safe' unsafe",
+				'maintenance/foobar.php',
+				[ 'bar\'"baz' ],
+			],
+			[
+				"'$wgPhpCli' 'changed.php' '--wiki=somewiki' 'bar'\\''\"baz' 'safe' unsafe",
+				'maintenance/foobar.php',
+				[ 'bar\'"baz' ],
+				[],
+				function ( &$script, array &$parameters ) {
+					$script = 'changed.php';
+					array_unshift( $parameters, '--wiki=somewiki' );
+				}
+			],
+			[
+				"'/bin/perl' 'maintenance/foobar.php' 'bar'\\''\"baz' 'safe' unsafe",
+				'maintenance/foobar.php',
+				[ 'bar\'"baz' ],
+				[ 'php' => '/bin/perl' ],
+			],
+			[
+				"'$wgPhpCli' 'foobinize' 'maintenance/foobar.php' 'bar'\\''\"baz' 'safe' unsafe",
+				'maintenance/foobar.php',
+				[ 'bar\'"baz' ],
+				[ 'wrapper' => 'foobinize' ],
+			],
+		];
 	}
 }
