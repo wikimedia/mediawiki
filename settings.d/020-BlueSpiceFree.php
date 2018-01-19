f1eca30fc20ff8f78fb97a73616408e34597433e
Benedikt Hofmann <hofmann@hallowelt.com>
2018-01-19 12:48:35 +0100

    Delete 020-BlueSpiceFree.php
    The BlueSpiceFoundation should be loaded before any other BlueSpice-Extension (020-*).
    Also the TagCloud is an own extension (now 020-BlueSpiceTagCloud.php

--- settings.d/020-BlueSpiceFree.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
-require_once __DIR__ . "/../extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php";
-require_once __DIR__ . "/../skins/BlueSpiceSkin/BlueSpiceSkin.php";
