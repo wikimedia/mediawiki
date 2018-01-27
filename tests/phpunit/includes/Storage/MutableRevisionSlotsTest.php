3eab35b93f64d8bda9ae014a73020672dc23b734
daniel <daniel.kinzler@wikimedia.de>
2018-01-26 17:48:19 -0800

    [MCR][DNM] Factor PageUpdater out of WikiPage
    DO NOT MERGE before it has been tested on a dedicated test wiki,
    see T192306!
    
    This introduces PageUpdater to replace WikiPage::doEditContent,
    and DerivedPageDataUpdater, to replace WikiPage::doEditUpdates
    and WikiPage::prepareContentForEdit.
    
    See docs/pageupdater.txt for a description of their
    functionality.
    
    MCR migration notes:
    
    * The interface of PageUpdater is expected to
    remain mostly stable after this patch. Code that has been using
    WikiPage::doEditContent can be confidently migrated to using the
    new mechanism for revision creation.
    
    * This patch keeps the code inside PageUpdater largely aligned
    with the old code in WikiPage, to make review easier to to avoid
    mistakes. It is intended to be refactored further, moving
    application logic into stateless services.
    
    * DerivedPageDataUpdate is intended as a stepping stone for further
    refactoring. Its behavior is designed to be compatible with
    callback code that currently relies on
    WikiPage::prepareContentForEdit. Much of the code that currently
    lives in DerivedPageDataUpdate should be factored out into
    services, all behavior relevant to calling code should be exposed
    via narrow interfaces.
    
    Bug: T174038
    Change-Id: If610c68f4912e89af616cdcac1d35a1be3946afa

--- tests/phpunit/includes/Storage/MutableRevisionSlotsTest.php
+++ tests/phpunit/includes/Storage/MutableRevisionSlotsTest.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Tests\Storage;
 
+use Content;
 use InvalidArgumentException;
 use MediaWiki\Storage\MutableRevisionSlots;
 use MediaWiki\Storage\RevisionAccessException;
@@ -75,6 +76,32 @@ class MutableRevisionSlotsTest extends RevisionSlotsTest {
 		$this->assertSame( [ 'main' => $slotB ], $slots->getSlots() );
 	}
 
+	/**
+	 * @param string $role
+	 * @param Content $content
+	 * @return SlotRecord
+	 */
+	private function newSavedSlot( $role, Content $content ) {
+		return SlotRecord::newSaved( 7, 7, 'xyz', SlotRecord::newUnsaved( $role, $content ) );
+	}
+
+	public function testInheritSlotOverwritesSlot() {
+		$slots = new MutableRevisionSlots();
+		$slotA = SlotRecord::newUnsaved( 'main', new WikitextContent( 'A' ) );
+		$slots->setSlot( $slotA );
+		$slotB = $this->newSavedSlot( 'main', new WikitextContent( 'B' ) );
+		$slotC = $this->newSavedSlot( 'foo', new WikitextContent( 'C' ) );
+		$slots->inheritSlot( $slotB );
+		$slots->inheritSlot( $slotC );
+		$this->assertSame( [ 'main', 'foo' ], $slots->getSlotRoles() );
+		$this->assertNotSame( $slotB, $slots->getSlot( 'main' ) );
+		$this->assertNotSame( $slotC, $slots->getSlot( 'foo' ) );
+		$this->assertTrue( $slots->getSlot( 'main' )->isInherited() );
+		$this->assertTrue( $slots->getSlot( 'foo' )->isInherited() );
+		$this->assertSame( $slotB->getContent(), $slots->getSlot( 'main' )->getContent() );
+		$this->assertSame( $slotC->getContent(), $slots->getSlot( 'foo' )->getContent() );
+	}
+
 	public function testSetContentOfExistingSlotOverwritesContent() {
 		$slots = new MutableRevisionSlots();
 
@@ -100,6 +127,22 @@ class MutableRevisionSlotsTest extends RevisionSlotsTest {
 		$this->assertSame( [], $slots->getSlots() );
 		$this->setExpectedException( RevisionAccessException::class );
 		$slots->getSlot( 'main' );
+	}
+
+	public function testNewFromParentRevisionSlots() {
+		/** @var SlotRecord[] $parentSlots */
+		$parentSlots = [
+			'some' => $this->newSavedSlot( 'some', new WikitextContent( 'X' ) ),
+			'other' => $this->newSavedSlot( 'other', new WikitextContent( 'Y' ) ),
+		];
+		$slots = MutableRevisionSlots::newFromParentRevisionSlots( $parentSlots );
+		$this->assertSame( [ 'some', 'other' ], $slots->getSlotRoles() );
+		$this->assertNotSame( $parentSlots['some'], $slots->getSlot( 'some' ) );
+		$this->assertNotSame( $parentSlots['other'], $slots->getSlot( 'other' ) );
+		$this->assertTrue( $slots->getSlot( 'some' )->isInherited() );
+		$this->assertTrue( $slots->getSlot( 'other' )->isInherited() );
+		$this->assertSame( $parentSlots['some']->getContent(), $slots->getContent( 'some' ) );
+		$this->assertSame( $parentSlots['other']->getContent(), $slots->getContent( 'other' ) );
 	}
 
 }
