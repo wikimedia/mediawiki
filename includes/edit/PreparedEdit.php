13f7e6dac7dbd3d69e939be7e03e347dab6c5e77
daniel <daniel.kinzler@wikimedia.de>
2018-01-26 17:48:19 -0800

    [MCR] Prepare PreparedEdit for MCR
    Change-Id: I76b9804ed3074a6f89e8a91986c6c8859ab81a7b

--- includes/edit/PreparedEdit.php
+++ includes/edit/PreparedEdit.php
@@ -21,8 +21,13 @@
 namespace MediaWiki\Edit;
 
 use Content;
+use MediaWiki\Linker\LinkTarget;
+use MediaWiki\Storage\RevisionRecord;
+use MediaWiki\Storage\RevisionSlots;
+use MediaWiki\User\UserIdentity;
 use ParserOptions;
 use ParserOutput;
+use Title;
 
 /**
  * Represents information returned by WikiPage::prepareContentForEdit()
@@ -32,16 +37,22 @@ use ParserOutput;
 class PreparedEdit {
 
 	/**
-	 * Time this prepared edit was made
+	 * Unused, always null since 1.31.
 	 *
-	 * @var string
+	 * @var null
+	 * @deprecated since 1.31
+	 * @todo remove in 1.32
 	 */
 	public $timestamp;
 
 	/**
-	 * Revision ID
+	 * The revision ID, if the ParserOutput depends on it.
 	 *
-	 * @var int|null
+	 * @note As of 1.31, the only known user is the FlaggedRevs extension.
+	 *
+	 * @var int
+	 * @deprecated since 1.31, use getRevisionId() instead.
+	 * @todo make private in 1.32
 	 */
 	public $revid;
 
@@ -49,13 +60,17 @@ class PreparedEdit {
 	 * Content after going through pre-save transform
 	 *
 	 * @var Content|null
+	 * @deprecated since 1.31, use getTransformedContentSlots() instead
+	 * @todo remove in 1.32
 	 */
 	public $pstContent;
 
 	/**
-	 * Content format
+	 * Unused, always null since 1.31.
 	 *
-	 * @var string
+	 * @var null
+	 * @deprecated since 1.31
+	 * @todo remove in 1.32
 	 */
 	public $format;
 
@@ -63,6 +78,8 @@ class PreparedEdit {
 	 * Parser options used to get parser output
 	 *
 	 * @var ParserOptions
+	 * @deprecated since 1.31, use getParserOptions() instead
+	 * @todo make this private in 1.32
 	 */
 	public $popts;
 
@@ -70,6 +87,8 @@ class PreparedEdit {
 	 * Parser output
 	 *
 	 * @var ParserOutput|null
+	 * @deprecated since 1.31, use getParserOutput() instead
+	 * @todo make this private in 1.32
 	 */
 	public $output;
 
@@ -77,14 +96,148 @@ class PreparedEdit {
 	 * Content that is being saved (before PST)
 	 *
 	 * @var Content
+	 * @deprecated since 1.31
+	 * @todo remove in 1.32
 	 */
 	public $newContent;
 
 	/**
-	 * Current content of the page, if any
+	 * Unused, always null since 1.31.
 	 *
-	 * @var Content|null
+	 * @var null
+	 * @deprecated since 1.31
+	 * @todo remove in 1.32
 	 */
 	public $oldContent;
+
+	/** @var  RevisionSlots */
+	private $newContentSlots;
+
+	/** @var  RevisionSlots */
+	private $transformedContentSlots;
+
+	/**
+	 * @var string
+	 */
+	private $signature;
+
+	/**
+	 * @param LinkTarget $title
+	 * @param RevisionSlots $newContentSlots
+	 * @param RevisionSlots $transformedContentSlots Content of the new revision, after pre-safe
+	 *        transform, including inherited slots.
+	 * @param UserIdentity $user
+	 * @param ParserOptions $popts
+	 * @param ParserOutput $output
+	 * @param int $revid Revision ID, if ParserOutput depends on it.
+	 */
+	public function __construct(
+		LinkTarget $title,
+		RevisionSlots $newContentSlots,
+		RevisionSlots $transformedContentSlots,
+		UserIdentity $user,
+		ParserOptions $popts,
+		ParserOutput $output,
+		$revid = 0
+	) {
+		$this->newContentSlots = $newContentSlots;
+		$this->transformedContentSlots = $transformedContentSlots;
+		$this->revid = $revid;
+		$this->popts = $popts;
+		$this->output = $output;
+
+		// Backwards compatibility, remove in 1.32:
+		$this->newContent = $newContentSlots->getContent( 'main' );
+		$this->pstContent = $transformedContentSlots->getContent( 'main' );
+		$this->oldContent = null;
+		$this->format = null;
+
+		$this->signature = self::makeSignature(
+			$title,
+			$newContentSlots,
+			$user,
+			$revid
+		);
+	}
+
+	/**
+	 * Returns the signature of this prepared edit.
+	 *
+	 * If the signature of a PreparedEdit matches the signature returned by makeSignature for
+	 * a set of parameters, this indicates that the PreparedEdit can be used for that set
+	 * of parameters.
+	 *
+	 * @return string
+	 */
+	public function getSignature() {
+		return $this->signature;
+	}
+
+	/**
+	 * Returns the signature for the given parameters.
+	 * The signature determines whether a PreparedEdit can be re-sued for a given set of parameters.
+	 *
+	 * @param RevisionSlots $newContent
+	 * @param UserIdentity $user
+	 * @param int $revid Revision ID, if ParserOutput depends on it.
+	 * @return string
+	 */
+	public static function makeSignature(
+		LinkTarget $title,
+		RevisionSlots $newContentSlots,
+		UserIdentity $user,
+		$revid = 0
+	) {
+		// TODO: $title should really be some kind of PageIdentity
+
+		$sig = '';
+		$sig .= $newContentSlots->computeSha1(); // NOTE: should consider content model, see T185793
+		$sig .= '|';
+		$sig .= $user->getName();
+		$sig .= '|ns';
+		$sig .= $title->getNamespace();
+		$sig .= '|';
+		$sig .= $title->getDBkey();
+
+		if ( $revid ) {
+			$sig .= '|r';
+			$sig .= $revid;
+		}
+
+		return $sig;
+	}
+
+	/**
+	 * @return RevisionSlots
+	 */
+	public function getTransformedContentSlots() {
+		return $this->transformedContentSlots;
+	}
+
+	/**
+	 * @return ParserOutput
+	 */
+	public function getParserOutput() {
+		return $this->output;
+	}
+
+	/**
+	 * @return ParserOptions
+	 */
+	public function getParserOptions() {
+		return $this->popts;
+	}
+
+	/**
+	 * The ID of the Revision the edit created, if known.
+	 *
+	 * This is typically only set if ParserOutput actually depends on the revision ID,
+	 * that is if the vary-revision flag is set.
+	 *
+	 * @return int
+	 */
+	public function getRevisionId() {
+		return $this->revid;
+	}
 
 }
