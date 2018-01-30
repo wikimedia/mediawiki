f91814c9c46e37d42905bfa223e5a700f7ee51fd
Aaron Schulz <aschulz@wikimedia.org>
2018-01-30 04:39:36 -0800

    rdbms: avoid pointless "SHOW SLAVE STATUS" calls in masterPosWait()
    This code branch is not useful in the GTID case
    
    Change-Id: Ia112c9b4b9c1f1297cc4eaac7f4a5a1ca882f02e

--- includes/libs/rdbms/database/DatabaseMysqlBase.php
+++ includes/libs/rdbms/database/DatabaseMysqlBase.php
@@ -889,8 +889,10 @@ abstract class DatabaseMysqlBase extends Database {
 			return 0; // already reached this point for sure
 		}
 
+		$useGTID = ( $this->useGTIDs && $pos->gtids );
+
 		// Call doQuery() directly, to avoid opening a transaction if DBO_TRX is set
-		if ( $this->useGTIDs && $pos->gtids ) {
+		if ( $useGTID ) {
 			// Wait on the GTID set (MariaDB only)
 			$gtidArg = $this->addQuotes( implode( ',', $pos->gtids ) );
 			$res = $this->doQuery( "SELECT MASTER_GTID_WAIT($gtidArg, $timeout)" );
@@ -909,10 +911,10 @@ abstract class DatabaseMysqlBase extends Database {
 
 		// Result can be NULL (error), -1 (timeout), or 0+ per the MySQL manual
 		$status = ( $row[0] !== null ) ? intval( $row[0] ) : null;
-		if ( $status === null ) {
+		if ( $status === null && !$useGTID ) {
 			// T126436: jobs programmed to wait on master positions might be referencing binlogs
-			// with an old master hostname. Such calls make MASTER_POS_WAIT() return null. Try
-			// to detect this and treat the replica DB as having reached the position; a proper master
+			// with an old master hostname. Such calls make MASTER_POS_WAIT() return null. Try to
+			// detect this and treat the replica DB as having reached the position; any master
 			// switchover already requires that the new master be caught up before the switch.
 			$replicationPos = $this->getReplicaPos();
 			if ( $replicationPos && !$replicationPos->channelsMatch( $pos ) ) {
