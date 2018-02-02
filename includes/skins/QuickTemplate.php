e08701b1d52b2d46b120fe5c34f4519cff201166
Timo Tijhof <krinklemail@gmail.com>
2018-02-02 12:39:57 -0800

    [DNM] skins: Remove MediaWikiI18N class and QuickTemplate::setTranslator
    Deprecated in 1.31.
    
    Bug: T186090
    Change-Id: I2ff268868bf6f112add6fc3a5718e83467781b13

--- includes/skins/QuickTemplate.php
+++ includes/skins/QuickTemplate.php
@@ -103,16 +103,6 @@ abstract class QuickTemplate {
 	}
 
 	/**
-	 * @param MediaWikiI18N &$t
-	 * @deprecate since 1.31 Use BaseTemplate::msg() or Skin::msg() instead for setting
-	 *  message parameters.
-	 */
-	public function setTranslator( &$t ) {
-		wfDeprecated( __METHOD__, '1.31' );
-		$this->translator = &$t;
-	}
-
-	/**
 	 * Main function, used by classes that subclass QuickTemplate
 	 * to show the actual HTML output
 	 */
