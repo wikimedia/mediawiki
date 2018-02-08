6ab91885abfc053731bef035808018d331ef5035
Bartosz Dziewoński <matma.rex@gmail.com>
2018-02-08 22:22:34 +0100

    Allow limiting comment length by characters rather than bytes in JS
    For unfortunate historical reasons, browsers' native maxlength counts
    the number of UTF-16 code units rather than Unicode codepoints [1],
    which means that codepoints outside the Basic Multilingual Plane
    (e.g. many emojis) count as 2 characters each. That could be good
    enough, but we want our software to be excellent rather than merely
    good enough.
    
    1. Introduce a few new functions, added to the existing modules:
       * mediawiki.String:
         * codePointLength() counts the length of a string in Unicode
           codepoints (characters).
         * trimCodePointLength() trims a string to the desired length in
           Unicode codepoints (characters).
       * jquery.lengthLimit:
         * $.fn.codePointLimit() enforces the specified maximum length in
           codepoints of an input field.
       * mediawiki.widgets.visibleLengthLimit:
         * mw.widgets.visibleCodePointLimit() enforces the maximum length
           in codepoints of an OOUI widget and displays the remaining
           length in an inline label.
    
    2. Add client-side mw.config variables:
       * wgCommentByteLimit for the old byte limit, equal to 255.
       * wgCommentCodePointLimit for the new codepoint limit, equal to 1000.
    
       Only one of them may be set, depending on which limit should be applied.
    
    3. Make use of both of these new features. For the sake of an example,
       I updated the forms shown on action=edit (using visibleCodePointLimit)
       and on action=protect (using codePointLimit). Many remain to be updated.
    
    [1] https://www.w3.org/TR/html5/sec-forms.html#limiting-user-input-length-the-maxlength-attribute
    
    Bug: T185948
    Change-Id: Ia1269fd898dabbcf1582618eab46cef97e10a3b1

--- resources/src/mediawiki.widgets.visibleLengthLimit/mediawiki.widgets.visibleLengthLimit.js
+++ resources/src/mediawiki.widgets.visibleLengthLimit/mediawiki.widgets.visibleLengthLimit.js
@@ -1,6 +1,7 @@
 ( function ( mw ) {
 
-	var byteLength = require( 'mediawiki.String' ).byteLength;
+	var byteLength = require( 'mediawiki.String' ).byteLength,
+		codePointLength = require( 'mediawiki.String' ).codePointLength;
 
 	/**
 	 * @class mw.widgets
@@ -26,6 +27,28 @@
 
 		// Actually enforce limit
 		textInputWidget.$input.byteLimit( limit );
+	};
+
+	/**
+	 * Add a visible codepoint (character) limit label to a TextInputWidget.
+	 *
+	 * Uses jQuery#codePointLimit to enforce the limit.
+	 *
+	 * @param {OO.ui.TextInputWidget} textInputWidget Text input widget
+	 * @param {number} [limit] Byte limit, defaults to $input's maxlength
+	 */
+	mw.widgets.visibleCodePointLimit = function ( textInputWidget, limit ) {
+		limit = limit || +textInputWidget.$input.attr( 'maxlength' );
+
+		function updateCount() {
+			textInputWidget.setLabel( ( limit - codePointLength( textInputWidget.getValue() ) ).toString() );
+		}
+		textInputWidget.on( 'change', updateCount );
+		// Initialise value
+		updateCount();
+
+		// Actually enforce limit
+		textInputWidget.$input.codePointLimit( limit );
 	};
 
 }( mediaWiki ) );
