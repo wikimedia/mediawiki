334d6b808d4182e3209c5d6b38a3de4866443fa2
Matthias Mullie <git@mullie.eu>
2018-02-13 17:51:29 +0100

    Only add header when description not already has it
    Bug: T187302
    Change-Id: Ic57196cc366fc53d83c624a7cd9290fb1972eab1

---
+++ tests/phpunit/includes/specials/SpecialUploadTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @covers SpecialUpload
+ */
+class SpecialUploadTest extends MediaWikiTestCase {
+	/**
+	 * @dataProvider provideGetInitialPageText
+	 */
+	public function testGetInitialPageText( $expected, $inputParams ) {
+		$result = call_user_func_array( [ 'SpecialUpload', 'getInitialPageText' ], $inputParams );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function provideGetInitialPageText() {
+		return [
+			[
+				// expect:
+				"== Summary ==\nthis is a test\n",
+				// params:
+				[
+					'this is a test'
+				],
+			],
+			[
+				// expect:
+				"== Summary ==\nthis is a test\n",
+				// params:
+				[
+					"== Summary ==\nthis is a test",
+				],
+			],
+		];
+	}
+}
