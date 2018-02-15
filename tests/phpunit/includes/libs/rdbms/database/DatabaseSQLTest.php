f35a359ef2080993e56a72cc6f935dba143b3090
Aaron Schulz <aschulz@wikimedia.org>
2018-02-14 19:46:04 -0800

    rdbms: make selectRowCount() use $var argument to exclude NULLs
    If the $var argument is provided, then it will make the resulting
    count exclude rows where the value for that column is NULL.
    
    Also add buildCalculatedTable() method and CalculatedTable
    wrapper class for use with select() for calculated tables.
    
    Change-Id: I549d629af99afdf370602de095f7fba6d1546c37

--- tests/phpunit/includes/libs/rdbms/database/DatabaseSQLTest.php
+++ tests/phpunit/includes/libs/rdbms/database/DatabaseSQLTest.php
@@ -2,6 +2,7 @@
 
 use InvalidArgumentException;
 use Wikimedia\Rdbms\LikeMatch;
+use Wikimedia\Rdbms\Database;
 
 /**
  * Test the parts of the Database abstract class that deal
@@ -11,7 +12,7 @@ class DatabaseSQLTest extends PHPUnit\Framework\TestCase {
 
 	use MediaWikiCoversValidator;
 
-	/** @var DatabaseTestHelper */
+	/** @var DatabaseTestHelper|Database */
 	private $database;
 
 	protected function setUp() {
@@ -236,6 +237,47 @@ class DatabaseSQLTest extends PHPUnit\Framework\TestCase {
 					'options' => [ 'FOR UPDATE' ],
 				],
 				"SELECT field FROM table      FOR UPDATE"
+			],
+		];
+	}
+
+	/**
+	 * @covers Wikimedia\Rdbms\CalculatedSQLTable
+	 * @dataProvider provideSelectRowCount
+	 * @param $sql
+	 * @param $sqlText
+	 */
+	public function testSelectRowCount( $sql, $sqlText ) {
+		$this->database->selectRowCount(
+			$sql['tables'],
+			$sql['field'],
+			isset( $sql['conds'] ) ? $sql['conds'] : [],
+			__METHOD__,
+			isset( $sql['options'] ) ? $sql['options'] : [],
+			isset( $sql['join_conds'] ) ? $sql['join_conds'] : []
+		);
+		$this->assertLastSql( $sqlText );
+	}
+
+	public static function provideSelectRowCount() {
+		return [
+			[
+				[
+					'tables' => 'table',
+					'field' => [ '*' ],
+					'conds' => [ 'alias' => 'text' ],
+				],
+				"SELECT COUNT(*) AS rowcount FROM " .
+				"(SELECT 1 FROM table WHERE alias = 'text'  ) tmp_count"
+			],
+			[
+				[
+					'tables' => 'table',
+					'field' => [ 'column' ],
+					'conds' => [ 'alias' => 'text' ],
+				],
+				"SELECT COUNT(*) AS rowcount FROM " .
+				"(SELECT 1 FROM table WHERE alias = 'text' AND (column IS NOT NULL)  ) tmp_count"
 			],
 		];
 	}
