303a8ba0fed6bcbb48ba16e4559dfc77b3caec40
Fomafix <fomafix@googlemail.com>
2018-02-16 11:36:59 +0100

    Use module.exports for mediawiki.libs.jpegmeta
    Deprecate mw.libs.jpegmeta. Use
      mw.loader.require( 'mediawiki.libs.jpegmeta' )
    instead.
    
    Change-Id: I8a7fb7377248a2ce1cf9c6d3c888ca9835cadaad

--- resources/src/mediawiki.libs/mediawiki.libs.jpegmeta.js
+++ resources/src/mediawiki.libs/mediawiki.libs.jpegmeta.js
@@ -7,6 +7,7 @@
  * Changes:
  * - Add closure.
  * - Add this.JpegMeta assignment to expose it as global.
+ * - Add export as module.
  * - Add mw.libs.jpegmeta wrapper.
  */
 
@@ -729,9 +730,13 @@
 	    }
 	};
 
-	// MediaWiki: Add mw.libs wrapper
-	mw.libs.jpegmeta = function( fileReaderResult, fileName ) {
+	// MediaWiki: Export as module
+	module.exports = function( fileReaderResult, fileName ) {
 		return new JpegMeta.JpegFile( fileReaderResult, fileName );
 	};
+
+	// MediaWiki: Add mw.libs wrapper
+	// @deprecated since 1.31
+	mw.log.deprecate( mw.libs, 'jpegmeta', module.exports );
 
 }( mediaWiki ) );
