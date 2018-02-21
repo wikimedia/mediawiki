65fcf1a19d9d64c973ec55b6a525a7edc213d1d9
GeoffreyT2000 <geoffreytrang@gmail.com>
2018-02-20 16:32:03 -0800

    Set the previous size for page creations to zero in rebuildrecentchanges.php
    The rebuildrecentchanges.php script should set the previous size for page creations to zero, not null, in order to make the new page's size actually appear on Special:RecentChanges.
    
    Bug: T187842
    Change-Id: I9823c5fe6f0503208b347e1ece9a1f894dba24c3

--- maintenance/rebuildrecentchanges.php
+++ maintenance/rebuildrecentchanges.php
@@ -218,7 +218,7 @@ class RebuildRecentchanges extends Maintenance {
 				} else {
 					# No previous edit
 					$lastOldId = 0;
-					$lastSize = null;
+					$lastSize = 0;
 					$new = 1; // probably true
 				}
 			}
