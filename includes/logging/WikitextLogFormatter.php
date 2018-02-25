40fa250a450130d5a952acd50505dac4d31941c3
SamanthaNguyen <samanthanguyen1116@gmail.com>
2018-02-24 18:19:31 -0600

    Create a custom log formatter that allows log entries to contain wikitext
    This creates a new log formatter called WikitextLogFormatter, which
    is a simple class that allows log entries to contain properly
    formatted wikitext. This makes it easier for extensions so they don't
    have to create their own subclass of LogFormatter.
    
    Change-Id: I2b7fddf5c6ef017a0925b4bf75cfd47cb55aa5de

---
+++ includes/logging/WikitextLogFormatter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Formatter to allow log entries to contain formatted wikitext.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/**
+ * Log formatter specifically for log entries containing wikitext.
+ * @since 1.31
+ */
+class WikitextLogFormatter extends LogFormatter {
+	/**
+	 * @return string
+	 */
+	public function getActionMessage() {
+		return parent::getActionMessage()->parse();
+	}
+}
