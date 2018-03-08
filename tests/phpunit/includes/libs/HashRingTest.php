89e59aba6e1d6a10726b2fa3420fa73b74a7c170
Aaron Schulz <aschulz@wikimedia.org>
2018-03-07 19:03:32 -0800

    [WIP] Add TYPE_CONSISTENT mode to HashRing
    This mode avoids disruptive re-hashing when a location
    is ejected. This allowed use of weight values is more
    restrictive however.
    
    Make JobQueueFederated use this (for job de-duplication).
    Switching from one mode to the other will cause some of
    duplicated effort from old jobs only temporarily.
    
    Change-Id: Iff432a4e5dc3fb40b2dda70fb3dbe9dda0b74933

--- tests/phpunit/includes/libs/HashRingTest.php
+++ tests/phpunit/includes/libs/HashRingTest.php
@@ -56,4 +56,62 @@ class HashRingTest extends PHPUnit\Framework\TestCase {
 		];
 		$this->assertEquals( $expectedLocations, $locations, 'Items placed at proper locations' );
 	}
+
+	/**
+	 * @covers HashRing
+	 */
+	public function testHashRing2() {
+		$ring = new HashRing(
+			[ 's1' => 1, 's2' => 1, 's3' => 2, 's4' => 2, 's5' => 2, 's6' => 3 ],
+			HashRing::TYPE_CONSISTENT
+		);
+
+		$locations = [];
+		for ( $i = 0; $i < 25; $i++ ) {
+			$locations[ "hello$i"] = $ring->getLocation( "hello$i" );
+		}
+		$expectedLocations = [
+			"hello0" => "s2",
+			"hello1" => "s4",
+			"hello2" => "s6",
+			"hello3" => "s2",
+			"hello4" => "s4",
+			"hello5" => "s3",
+			"hello6" => "s2",
+			"hello7" => "s3",
+			"hello8" => "s2",
+			"hello9" => "s2",
+			"hello10" => "s3",
+			"hello11" => "s4",
+			"hello12" => "s6",
+			"hello13" => "s6",
+			"hello14" => "s6",
+			"hello15" => "s2",
+			"hello16" => "s3",
+			"hello17" => "s2",
+			"hello18" => "s4",
+			"hello19" => "s6",
+			"hello20" => "s2",
+			"hello21" => "s1",
+			"hello22" => "s6",
+			"hello23" => "s6",
+			"hello24" => "s3"
+		];
+
+		$this->assertEquals( $expectedLocations, $locations, 'Items placed at proper locations' );
+
+		$locations = [];
+		for ( $i = 0; $i < 5; $i++ ) {
+			$locations[ "hello$i"] = $ring->getLocations( "hello$i", 2 );
+		}
+
+		$expectedLocations = [
+			"hello0" => [ "s2", "s3" ],
+			"hello1" => [ "s4", "s5" ],
+			"hello2" => [ "s6", "s5" ],
+			"hello3" => [ "s2", "s3" ],
+			"hello4" => [ "s4", "s5" ],
+		];
+		$this->assertEquals( $expectedLocations, $locations, 'Items placed at proper locations' );
+	}
 }
