836890daf0efee8b452e432fcd52d024d21d5db2
Antoine Musso <hashar@free.fr>
2018-03-13 10:16:15 +0100

    Let install.php detect and inject extensions [2]
    For CI, I went with a dirty trick to find extensions and either
    wfLoadExtension() or include them. That has since been ported to
    Installer::findExtensions() and LocalSettingsGenerator::getText().
    
    The WebInstaller() relies on that to detect extensions and let the user
    tick the ones to be installed.
    
    Add --with-extensions to install.php so one can include all extensions
    from the command line.
    
    Note: The CliInstaller always wfLoad() any skin it can find.
    
    The original patch 53926d9d was broken and got reverted. This is the
    proper one.
    
    Bug: T189567
    Change-Id: I41820fce12e01224d64ab711e016835ef4d4febc

--- includes/installer/CliInstaller.php
+++ includes/installer/CliInstaller.php
@@ -107,6 +107,11 @@ class CliInstaller extends Installer {
 			$this->setVar( '_AdminPassword', $option['pass'] );
 		}
 
+		// Detect and inject any extension found
+		if ( isset( $option['with-extensions'] ) ) {
+			$this->setVar( '_Extensions', array_keys( $this->findExtensions() ) );
+		}
+
 		// Set up the default skins
 		$skins = array_keys( $this->findExtensions( 'skins' ) );
 		$this->setVar( '_Skins', $skins );
