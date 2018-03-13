18b315b2acd1e4d210771d030896773644e3c671
Chad Horohoe <chadh@wikimedia.org>
2018-03-13 18:43:30 +0000

    SECURITY: Make 'newbie' limit in $wgRateLimits really override 'user' limit
    The order of operations was incorrect.
    
    Bug: T169545
    Change-Id: Ia910aa2a494914d3b0017daac9ab294ea9fa8705

--- includes/user/User.php
+++ includes/user/User.php
@@ -1809,10 +1809,6 @@ class User implements IDBAccessObject {
 			if ( isset( $limits['user'] ) ) {
 				$userLimit = $limits['user'];
 			}
-			// limits for newbie logged-in users
-			if ( $isNewbie && isset( $limits['newbie'] ) ) {
-				$keys[wfMemcKey( 'limiter', $action, 'user', $id )] = $limits['newbie'];
-			}
 		}
 
 		// limits for anons and for newbie logged-in users
@@ -1842,6 +1838,11 @@ class User implements IDBAccessObject {
 					$userLimit = $limits[$group];
 				}
 			}
+		}
+
+		// limits for newbie logged-in users (override all the normal user limits)
+		if ( $id !== 0 && $isNewbie && isset( $limits['newbie'] ) ) {
+			$userLimit = $limits['newbie'];
 		}
 
 		// Set the user limit key
