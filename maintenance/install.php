1d138aaf03d939d715e6ab095436065b94ee8537
Hashar <hashar@free.fr>
2018-03-14 22:10:20 +0000

    Revert "Let install.php detect and inject extensions"
    It is blatantly broken using non existing variables:
    $installer and $options
    
    This reverts commit 53926d9d6940eaa7f40086c9cf89af7bc726e73f.
    
    Bug: T189567
    Change-Id: Idd1f002bbd5d474f9fe386cd0e326e1fbfdec097

--- maintenance/install.php
+++ maintenance/install.php
@@ -88,8 +88,6 @@ class CommandLineInstaller extends Maintenance {
 			false, true );
 		*/
 		$this->addOption( 'env-checks', "Run environment checks only, don't change anything" );
-
-		$this->addOption( 'with-extensions', "Detect and include extensions" );
 	}
 
 	public function getDbType() {
