1e845bc348197b0c15cfff9b1025cd1063cf622a
Fomafix <fomafix@googlemail.com>
2018-03-14 06:34:17 +0100

    [WIP] Wrap title with an element with class, lang and dir attributes
    Todo:
    * Update tests.
    * Use a getPageTitleLanguage like in
      I0ff707d5f04218bef5721e6fc162c6359bb7538a.
    
    The lang attribute on the firstHeading in the skins can be removed:
    * MonoBook: I1e6968574e31b895356002fd3e9c8647b11a01be
    * Vector: Id6916d0141aa26df46443d894c687907d77ed6c3
    
    Bug: T36514
    Bug: T65880
    Change-Id: I1f3fc251bb3b3a0e18d813ebec04e7de342d42b6

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2438,7 +2438,9 @@ ERROR;
 		#       setPageTitle() treats the input as wikitext, which should be safe in either case.
 		$displayTitle = isset( $this->mParserOutput ) ? $this->mParserOutput->getDisplayTitle() : false;
 		if ( $displayTitle === false ) {
-			$displayTitle = $contextTitle->getPrefixedText();
+			$displayTitle = $contextTitle->getWrappedPrefixedText();
+		} else {
+			$displayTitle = Title::wrapTitle( $displayTitle, $contextTitle->getPageViewLanguage() );
 		}
 		$out->setPageTitle( $this->context->msg( $msg, $displayTitle ) );
 
