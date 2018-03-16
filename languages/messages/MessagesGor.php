448c14c8ea9c15f26a5941ed73da0702b4c93746
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-03-16 21:11:13 +0000

    New language file
    Bug: T189127
    Change-Id: Ib15fcc1b1b63674afdc0642e7139e11972898c95

---
+++ languages/messages/MessagesGor.php
@@ -0,0 +1,31 @@
+<?php
+/** Gorontalo
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'en';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Spesial',
+	NS_MAIN             => ''
+	NS_TALK             => 'Lo'iya',
+	NS_USER             => 'Ta ohu'uwo',
+	NS_USER_TALK        => 'Lo'iya ta ohu'owo',
+	NS_PROJECT_TALK     => 'Lo'iya_$1',
+	NS_FILE             => 'Berkas',
+	NS_FILE_TALK        => 'Lo'iya_berkas',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Lo'iya_MediaWiki',
+	NS_TEMPLATE         => 'Templat',
+	NS_TEMPLATE_TALK    => 'Lo'iya_templat',
+	NS_HELP             => 'Wubodu',
+	NS_HELP_TALK        => 'Lo'iya_wubodu',
+	NS_CATEGORY         => 'Dalala',
+	NS_CATEGORY_TALK    => 'Lo'iya_dalala',
+];
