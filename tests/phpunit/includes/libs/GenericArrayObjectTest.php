a2bb8124b7f45c74412332754e35de412cc9c7b9
Umherirrender <umherirrender_de.wp@web.de>
2018-03-16 17:50:59 +0100

    Remove @dataProvider and @covers from non-test functions
    Only functions beginning with test* should have that annotation
    
    Change-Id: I85cd795970205943aebe692d85434145b3cf3f5b

--- tests/phpunit/includes/libs/GenericArrayObjectTest.php
+++ tests/phpunit/includes/libs/GenericArrayObjectTest.php
@@ -173,8 +173,6 @@ abstract class GenericArrayObjectTest extends PHPUnit\Framework\TestCase {
 	 * @since 1.20
 	 *
 	 * @param callable $function
-	 *
-	 * @covers GenericArrayObject::getObjectType
 	 */
 	protected function checkTypeChecks( $function ) {
 		$excption = null;
@@ -206,7 +204,7 @@ abstract class GenericArrayObjectTest extends PHPUnit\Framework\TestCase {
 	 * @since 1.20
 	 *
 	 * @param array $elements
-	 *
+	 * @covers GenericArrayObject::getObjectType
 	 * @covers GenericArrayObject::offsetSet
 	 */
 	public function testOffsetSet( array $elements ) {
