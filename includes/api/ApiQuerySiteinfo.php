wikimedia/mediawiki

commit bd31195f7925a50c86d22d5e0ff71d9e3749b102

API: Some cleanup * Aggressively intval() numeric things; addresses the concerns raised on bug 11633 * Use ApiQueryBase::addTitleInfo() everywhere * Remove a drug-induced-looking line from ApiQueryWatchlist.php
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@47865 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/api/ApiQuerySiteinfo.php
@@ -137,7 +137,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 			$offset = 0;
 		}
 		$data['timezone'] = $tz;
-		$data['timeoffset'] = $offset;
+		$data['timeoffset'] = intval($offset);
 
 		return $this->getResult()->addValue( 'query', $property, $data );
 	}
@@ -148,7 +148,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 		foreach( $wgContLang->getFormattedNamespaces() as $ns => $title )
 		{
 			$data[$ns] = array(
-				'id' => $ns
+				'id' => intval($ns)
 			);
 			ApiResult :: setContent( $data[$ns], $title );
 			$canonical = MWNamespace::getCanonicalName( $ns );
@@ -176,7 +176,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 				continue;
 			}
 			$item = array(
-				'id' => $ns
+				'id' => intval($ns)
 			);
 			ApiResult :: setContent( $item, strtr( $title, '_', ' ' ) );
 			$data[] = $item;
@@ -272,7 +272,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 			list( $host, $lag ) = wfGetLB()->getMaxLag();
 			$data[] = array(
 				'host' => $wgShowHostnames ? $host : '',
-				'lag' => $lag
+				'lag' => intval( $lag )
 			);
 		}
 

includes/api/ApiBlock.php
@@ -94,7 +94,7 @@ class ApiBlock extends ApiBase {
 			$this->dieUsageMsg($retval);
 
 		$res['user'] = $params['user'];
-		$res['userID'] = $userID;
+		$res['userID'] = intval($userID);
 		$res['expiry'] = ($expiry == Block::infinity() ? 'infinite' : wfTimestamp(TS_ISO_8601, $expiry));
 		$res['reason'] = $params['reason'];
 		if($params['anononly'])

includes/api/ApiEditPage.php
@@ -249,7 +249,7 @@ class ApiEditPage extends ApiBase {
 				$r['new'] = '';
 			case EditPage::AS_SUCCESS_UPDATE:
 				$r['result'] = "Success";
-				$r['pageid'] = $titleObj->getArticleID();
+				$r['pageid'] = intval($titleObj->getArticleID());
 				$r['title'] = $titleObj->getPrefixedText();
 				# HACK: We create a new Article object here because getRevIdFetched()
 				# refuses to be run twice, and because Title::getLatestRevId()
@@ -261,8 +261,8 @@ class ApiEditPage extends ApiBase {
 					$r['nochange'] = '';
 				else
 				{
-					$r['oldrevid'] = $oldRevId;
-					$r['newrevid'] = $newRevId;
+					$r['oldrevid'] = intval($oldRevId);
+					$r['newrevid'] = intval($newRevId);
 				}
 				break;
 			default:

includes/api/ApiImport.php
@@ -162,7 +162,7 @@ class ApiImportReporter extends ImportReporter {
 		// Add a result entry
 		$r = array();
 		ApiQueryBase::addTitleInfo($r, $title);
-		$r['revisions'] = $successCount;
+		$r['revisions'] = intval($successCount);
 		$this->mResultArr[] = $r;
 
 		// Piggyback on the parent to do the logging

includes/api/ApiLogin.php
@@ -82,7 +82,7 @@ class ApiLogin extends ApiBase {
 				wfRunHooks('UserLoginComplete', array(&$wgUser, &$injected_html));
 
 				$result['result'] = 'Success';
-				$result['lguserid'] = $wgUser->getId();
+				$result['lguserid'] = intval($wgUser->getId());
 				$result['lgusername'] = $wgUser->getName();
 				$result['lgtoken'] = $wgUser->getToken();
 				$result['cookieprefix'] = $wgCookiePrefix;
@@ -114,7 +114,7 @@ class ApiLogin extends ApiBase {
 			case LoginForm :: THROTTLED :
 				global $wgPasswordAttemptThrottle;
 				$result['result'] = 'Throttled';
-				$result['wait'] = $wgPasswordAttemptThrottle['seconds'];
+				$result['wait'] = intval($wgPasswordAttemptThrottle['seconds']);
 				break;
 			default :
 				ApiBase :: dieDebug(__METHOD__, "Unhandled case value: {$authRes}");

includes/api/ApiParse.php
@@ -156,7 +156,7 @@ class ApiParse extends ApiBase {
 							$p_result->getDisplayTitle() :
 							$titleObj->getPrefixedText();
 		if(!is_null($oldid))
-			$result_array['revid'] = $oldid;
+			$result_array['revid'] = intval($oldid);
 
 		$result_mapping = array(
 			'redirects' => 'r',

includes/api/ApiPatrol.php
@@ -60,7 +60,7 @@ class ApiPatrol extends ApiBase {
 		if($retval)
 			$this->dieUsageMsg(current($retval));
 		
-		$result = array('rcid' => $rc->getAttribute('rc_id'));
+		$result = array('rcid' => intval($rc->getAttribute('rc_id')));
 		ApiQueryBase::addTitleInfo($result, $rc->getTitle());
 		$this->getResult()->addValue(null, $this->getModuleName(), $result);
 	}

includes/api/ApiQueryAllCategories.php
@@ -103,10 +103,10 @@ class ApiQueryAllCategories extends ApiQueryGeneratorBase {
 				$item = array();
 				$result->setContent( $item, $titleObj->getText() );
 				if( isset( $prop['size'] ) ) {
-					$item['size'] = $row->cat_pages;
+					$item['size'] = intval($row->cat_pages);
 					$item['pages'] = $row->cat_pages - $row->cat_subcats - $row->cat_files;
-					$item['files'] = $row->cat_files;
-					$item['subcats'] = $row->cat_subcats;
+					$item['files'] = intval($row->cat_files);
+					$item['subcats'] = intval($row->cat_subcats);
 				}
 				if( isset( $prop['hidden'] ) && $row->cat_hidden )
 					$item['hidden'] = '';

commit 1f64fa48d397fca02befcd4fd7502655b9463619

api: Restore fractions for default siprop=dbreplag response
Without this fix, the situation is that a query for 'siprop=dbreplag'
shows a db server with lag=0, while the siprop=dbreplag&sishowalldb=1
query shows the same server having non-zero lag. That's confusing.

Regression from r47865 (88f35cdf6e).

Change-Id: Id4854c8453206a6f1f8c369dc89c3a8bd910f83f

includes/api/ApiQuerySiteinfo.php
@@ -465,7 +465,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 				'host' => $showHostnames
 						? $lb->getServerName( $index )
 						: '',
-				'lag' => intval( $lag )
+				'lag' => $lag
 			];
 		}
 