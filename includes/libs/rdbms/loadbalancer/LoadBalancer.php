76deb516821d5c6b9765aa71ef819497fb8bc533
Aaron Schulz <aschulz@wikimedia.org>
2018-03-18 22:26:11 -0700

    [WIP] rdbms: clean up DBO_TRX behavior for onTransaction*
    * Make onTransactionIdle() wait until any transaction round
      is gone, even if there is no SQL transaction active. This
      is what onTransactionPreCommitOrIdle() already does.
    * Decouple "transaction round mode" (DBO_TRX) from whether a
      round is active via a 'trxRoundId' LB info field. If rounds
      are enabled, but not is started, then the transaction state
      should be interpreted as "idle".
    * Improved related documentation.
    * Add more related unit tests.
    
    Change-Id: I3ab18f577ec0375897fcb63f18f4ee2deeb436e9

--- includes/libs/rdbms/loadbalancer/LoadBalancer.php
+++ includes/libs/rdbms/loadbalancer/LoadBalancer.php
@@ -1401,7 +1401,6 @@ class LoadBalancer implements ILoadBalancer {
 
 		return $e;
 	}
-
 	public function rollbackMasterChanges( $fname = __METHOD__ ) {
 		$restore = ( $this->trxRoundId !== false );
 		$this->trxRoundId = false;
@@ -1437,6 +1436,8 @@ class LoadBalancer implements ILoadBalancer {
 			// setting DBO_DEFAULT, then respect that. Forcing no transactions is useful
 			// for things like blob stores (ExternalStore) which want auto-commit mode.
 		}
+
+		$conn->setLBInfo( 'trxRoundId', $this->trxRoundId );
 	}
 
 	/**
@@ -1446,6 +1447,8 @@ class LoadBalancer implements ILoadBalancer {
 		if ( $conn->getLBInfo( 'autoCommitOnly' ) ) {
 			return; // transaction rounds do not apply to these connections
 		}
+
+		$conn->setLBInfo( 'trxRoundId', false );
 
 		if ( $conn->getFlag( $conn::DBO_DEFAULT ) ) {
 			$conn->restoreFlags( $conn::RESTORE_PRIOR );
