97953c9f917b33fd872a080288055ddb73e5342a
Fomafix <fomafix@googlemail.com>
2018-03-22 11:48:55 +0100

    Strip Unicode 6.3.0 bidi characters from title
    Unicode 6.3.0 (September 2013) the added additional bidi characters:
    
    U+061C ARABIC LETTER MARK
    U+2066 LEFT-TO-RIGHT ISOLATE
    U+2067 RIGHT-TO-LEFT ISOLATE
    U+2068 FIRST STRONG ISOLATE
    U+2069 POP DIRECTIONAL ISOLATE
    
    https://www.fileformat.info/info/unicode/version/6.3/index.htm
    
    This change strips the new bidi characters from the title like the bidi
    characters from Unicode 1.1.0 (June 1993).
    
    Any existing titles containing the new Unicode bidi characters get
    stripped by a run of maintenance/cleanupTitles.php after deployment.
    This change allows to insert the new Unicode bidi characters into the
    DISPLAYTITLE.
    
    Change-Id: I2279f51048f5252c2e4280ec6a13f060ff9967cb

--- resources/src/mediawiki/mediawiki.Title.js
+++ resources/src/mediawiki/mediawiki.Title.js
@@ -149,7 +149,7 @@
 		rWhitespace = /[ _\u00A0\u1680\u180E\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]+/g,
 
 		// From MediaWikiTitleCodec::splitTitleString() in PHP
-		rStripCharacters = /[\u00AD\u200E\u200F\u202A-\u202E]/g,
+		rStripCharacters = /[\u00AD\u061c\u200E\u200F\u202A-\u202E\u2066-\u2069]/g,
 
 		/**
 		 * Slightly modified from Flinfo. Credit goes to Lupo and Flominator.
