8e3c3ceddf0d3ecea9ec2f2762c7a7e9c5a8ba48
Aaron Schulz <aschulz@wikimedia.org>
2018-03-23 17:32:19 -0700

    [WIP] rdbms: add DBAtomicSectionCancelError exception class
    This makes it more practical for callers to make use of the
    cancelable atomic sections from IDatabase::doAtomicSection()
    
    Add comments and examples to the related IDatabase methods
    
    Change-Id: I5e3e0aeba4e54126fd0baf5c7a24a4b88a13096d

---
+++ includes/libs/rdbms/exception/DBAtomicSectionCancelError.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Database
+ */
+
+namespace Wikimedia\Rdbms;
+
+use Exception;
+
+/**
+ * @ingroup Database
+ */
+class DBAtomicSectionCancelError extends DBExpectedError {
+	public function __construct(
+		IDatabase $db, Exception $origError, Exception $cancelError, $fname
+	) {
+		$message = "An unrecoverable error occurred in an atomic section. \n" .
+			"Section: $fname\n" .
+			"Original error: {$origError->getMessage()}\n" .
+			"Section cancel error: {$cancelError->getMessage()}\n";
+
+		parent::__construct( $db, $message );
+	}
+}
