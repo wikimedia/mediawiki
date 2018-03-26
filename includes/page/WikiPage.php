19770d6f65680a72895d6e86794ee370c1a4f9ee
MaxSem <maxsem.wiki@gmail.com>
2018-03-26 22:28:46 +0000

    Revert "Pass revision being reverted to edit code"
    Too many regressions, doing it the different way.
    
    This reverts commit b7737ebed1e8940df4dfd4c50c1224d0edc8639d.
    
    Bug: T190374
    Change-Id: I12cb7470aeb7852ddea319c7e499ca3a7b45c84c

--- includes/page/WikiPage.php
+++ includes/page/WikiPage.php
@@ -3254,8 +3254,7 @@ class WikiPage implements Page, IDBAccessObject {
 			$target->getId(),
 			$guser,
 			null,
-			$tags,
-			$current->getId()
+			$tags
 		);
 
 		// Set patrolling and bot flag on the edits, which gets rollbacked.
