069537bd00d2735476774ee71d47dac743e651f3
daniel <daniel.kinzler@wikimedia.de>
2018-03-28 19:41:42 +0200

    MCR: RevisionStore: test old and intermediate schema
    This runs unit tests for RevisionStore in MIGRATION_OLD and
    MIGRATION_WRITE_BOTH modes, with the appropriate DB schemas.
    
    This makes sure that we don't use any of the new tables in
    MIGRATION_OLD mode, and that behavior in both modes is consistent.
    
    Change-Id: If259b1e1c49ceaa4356c6010222f6dc574f9e1ea

---
+++ tests/phpunit/includes/Storage/McrRevisionStoreDbTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daki
+ * Date: 28.03.18
+ * Time: 17:33
+ */
+
+namespace MediaWiki\Tests\Storage;
+
+use MediaWiki\Storage\RevisionRecord;
+
+class McrRevisionStoreDbTest extends RevisionStoreDbTestBase {
+
+	public function __construct( $name = '', $data = [], $dataName = '' ) {
+		parent::__construct( $name, $data, $dataName );
+	}
+
+	protected function getMcrMigrationStage() {
+		return MIGRATION_NEW;
+	}
+
+	protected function getSchemaOverrides() {
+		$tables = [];
+		$scripts = [];
+		$patchDir = $GLOBALS['IP'] . '/maintenance/archives';
+
+		if ( !$this->hasMcrTables() ) {
+			$tables = [ 'slots', 'content', 'slot_roles', 'content_models', ];
+			$scripts[] = $patchDir . '/slots.sql';
+		}
+
+		if ( $this->hasPreMcrFields() ) {
+			$tables[] = 'revision';
+			$scripts[] = __DIR__ . '/drop-pre-mcr-fields.sql';
+		}
+
+		return [ $tables, $scripts ];
+	}
+
+	protected function assertRevisionExistsInDatabase( RevisionRecord $rev ) {
+		parent::assertRevisionExistsInDatabase( $rev );
+
+		$this->assertSelect(
+			'slots', [ 'count(*)' ], [ 'slot_revision_id' => $rev->getId() ], [ [ '1' ] ]
+		);
+		$this->assertSelect(
+			'content',
+			[ 'count(*)' ],
+			[ 'content_address' => $rev->getSlot( 'main' )->getAddress() ],
+			[ [ '1' ] ]
+		);
+	}
+
+	public function setUp() {
+		parent::setUp();
+		$this->tablesUsed[] = 'content';
+		$this->tablesUsed[] = 'content_models';
+		$this->tablesUsed[] = 'slots';
+		$this->tablesUsed[] = 'slot_roles';
+	}
+
+}
