8efe259f28662a0755644a9aa81f841b2827327f
daniel <daniel.kinzler@wikimedia.de>
2018-03-28 19:44:12 +0200

    MCR: RevisionStore: test for MIGRATION_NEW.
    This runs unit tests for RevisionStore in MIGRATION_NEW mode,
    with the old fields removed from the revision table.
    
    Change-Id: I8083514db58e5840a6f43e34a92174cf3a00d936

---
+++ tests/phpunit/includes/Storage/McrRevisionStoreDbTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daki
+ * Date: 28.03.18
+ * Time: 17:33
+ */
+
+namespace MediaWiki\Tests\Storage;
+
+use MediaWiki\Storage\RevisionRecord;
+use Wikimedia\Rdbms\IMaintainableDatabase;
+
+class McrRevisionStoreDbTest extends RevisionStoreDbTestBase {
+
+	public function __construct( $name = '', $data = [], $dataName = '' ) {
+		parent::__construct( $name, $data, $dataName );
+	}
+
+	protected function getMcrMigrationStage() {
+		return MIGRATION_NEW;
+	}
+
+	protected function getSchemaOverrides( IMaintainableDatabase $db ) {
+		$patchDir = $GLOBALS['IP'] . '/maintenance/archives';
+		$overrides = [
+			'scripts' => [],
+			'drop' => [],
+			'create' => [],
+			'alter' => [],
+		];
+
+		if ( !$this->hasMcrTables() ) {
+			$overrides['create'] = [ 'slots', 'content', 'slot_roles', 'content_models', ];
+			$overrides['scripts'][] = $patchDir . '/slots.sql';
+		}
+
+		if ( $this->hasPreMcrFields() ) {
+			$overrides['alter'][] = 'revision';
+			$overrides['scripts'][] = __DIR__ . '/drop-pre-mcr-fields.sql';
+		}
+
+		return $overrides;
+	}
+
+	protected function assertRevisionExistsInDatabase( RevisionRecord $rev ) {
+		parent::assertRevisionExistsInDatabase( $rev );
+
+		$this->assertSelect(
+			'slots', [ 'count(*)' ], [ 'slot_revision_id' => $rev->getId() ], [ [ '1' ] ]
+		);
+		$this->assertSelect(
+			'content',
+			[ 'count(*)' ],
+			[ 'content_address' => $rev->getSlot( 'main' )->getAddress() ],
+			[ [ '1' ] ]
+		);
+	}
+
+	public function setUp() {
+		parent::setUp();
+		$this->tablesUsed[] = 'content';
+		$this->tablesUsed[] = 'content_models';
+		$this->tablesUsed[] = 'slots';
+		$this->tablesUsed[] = 'slot_roles';
+	}
+
+}
