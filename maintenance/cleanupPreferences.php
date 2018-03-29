f41fd4714a420929b201911d85a41d3a463f92f8
Sam Wilson <sam@samwilson.id.au>
2018-03-29 14:36:51 +0800

    Exclude GlobalPreferences' local-exception prefs from cleanup
    Add '-local-exception' preferences to the list of those to
    ignore when cleaning up unknown preferences.
    
    Bug: T188966
    Change-Id: Ief3e0292fe9b05091a2da0bc78d2b41a3fd8755c

--- maintenance/cleanupPreferences.php
+++ maintenance/cleanupPreferences.php
@@ -84,15 +84,24 @@ class CleanupPreferences extends Maintenance {
 			}
 		}
 
-		// Remove unknown preferences. Special-case gadget- and userjs- as we can't
-		// control those names.
+		// Remove unknown preferences. Ignore 'userjs-*' (core), 'gadget-*' (Gadgets),
+		// and '*-local-exception' (GlobalPreferences) as we can't control those names.
 		if ( $unknown ) {
+			$globalPrefsClause = 'true';
+			if ( ExtensionRegistry::getInstance()->isLoaded( 'GlobalPreferences' ) ) {
+				$like = $dbw->buildLike(
+					$dbw->anyString(),
+					\GlobalPreferences\GlobalPreferencesFactory::LOCAL_EXCEPTION_SUFFIX
+				);
+				$globalPrefsClause = "up_property NOT $like";
+			}
 			$this->deleteByWhere(
 				$dbw,
 				'Dropping unknown preferences',
 				[
 					'up_property NOT' . $dbw->buildLike( 'gadget-', $dbw->anyString() ),
 					'up_property NOT' . $dbw->buildLike( 'userjs-', $dbw->anyString() ),
+					$globalPrefsClause,
 					'up_property NOT IN (' . $dbw->makeList( array_keys( $wgDefaultUserOptions ) ) . ')',
 				]
 			);
