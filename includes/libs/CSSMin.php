714bbeb9888eb293f82e8f8231138a43131ab619
20after4 <mmodell@wikimedia.org>
2018-04-03 14:47:19 +0000

    Revert "Don't leave file and query undefined"
    This reverts commit fa9188159667492b23f06000d1e71fc8473a6dcb.
    
    Change-Id: Ibc79a5eb0e7a169f6dc774eba479207ee9a6ad10

--- includes/libs/CSSMin.php
+++ includes/libs/CSSMin.php
@@ -448,9 +448,6 @@ class CSSMin {
 			} elseif ( isset( $match['file2'] ) && $match['file2'][1] !== -1 ) {
 				$match['file'] = $match['file2'];
 				$match['query'] = $match['query2'];
-			} else {
-				$match['file'] = '';
-				$match['query'] = '';
 			}
 		} else {
 			if ( isset( $match['file0'] ) && $match['file0'] !== '' ) {
@@ -462,9 +459,6 @@ class CSSMin {
 			} elseif ( isset( $match['file2'] ) && $match['file2'] !== '' ) {
 				$match['file'] = $match['file2'];
 				$match['query'] = $match['query2'];
-			} else {
-				$match['file'] = '';
-				$match['query'] = '';
 			}
 		}
 	}
