0662f3ad56c691c7f20dd7f073461f2cb211b01d
Ed Sanders <esanders@wikimedia.org>
2018-04-03 22:08:52 +0100

    Define which languages explicitly fallback to 'en'
    In the message store, all message fall through to English,
    but only a few languages should actually fallback to English
    (English variants and dialects).
    
    Change-Id: I6b68a17f4d69341bccdae748727b5133a600d8bc

---
+++ languages/messages/MessagesJam.php
@@ -0,0 +1,11 @@
+<?php
+/** Jamaican Creole English (Patois)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'en';
