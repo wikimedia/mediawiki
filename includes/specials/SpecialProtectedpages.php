9e1bed06a8673aee5c6910826f08cd88be306712
Prateek Saxena <prtksxna@gmail.com>
2018-04-05 11:08:06 +0530

    Special:ProtectedPages: Use MultiSelectField instead of checks
    Bug: T117722
    Change-Id: I89193ab9d595a8f4749d9e871917e71a4f8f925c

--- includes/specials/SpecialProtectedpages.php
+++ includes/specials/SpecialProtectedpages.php
@@ -45,9 +45,12 @@ class SpecialProtectedpages extends SpecialPage {
 		$sizetype = $request->getVal( 'size-mode' );
 		$size = $request->getIntOrNull( 'size' );
 		$ns = $request->getIntOrNull( 'namespace' );
-		$indefOnly = $request->getBool( 'indefonly' ) ? 1 : 0;
-		$cascadeOnly = $request->getBool( 'cascadeonly' ) ? 1 : 0;
-		$noRedirect = $request->getBool( 'noredirect' ) ? 1 : 0;
+
+		$filters = $request->getArray( 'wpfilters' );
+		$filters = is_null( $filters ) ? [] : $filters;
+		$indefOnly = in_array( 'indefonly', $filters );
+		$cascadeOnly = in_array( 'cascadeonly', $filters );
+		$noRedirect = in_array( 'noredirect', $filters );
 
 		$pager = new ProtectedPagesPager(
 			$this,
@@ -69,9 +72,7 @@ class SpecialProtectedpages extends SpecialPage {
 			$level,
 			$sizetype,
 			$size,
-			$indefOnly,
-			$cascadeOnly,
-			$noRedirect
+			$filters
 		) );
 
 		if ( $pager->getNumRows() ) {
@@ -93,7 +94,7 @@ class SpecialProtectedpages extends SpecialPage {
 	 * @return string Input form
 	 */
 	protected function showOptions( $namespace, $type = 'edit', $level, $sizetype,
-		$size, $indefOnly, $cascadeOnly, $noRedirect
+		$size, $filters
 	) {
 		$formDescriptor = [
 			'namespace' => [
@@ -106,23 +107,16 @@ class SpecialProtectedpages extends SpecialPage {
 			],
 			'typemenu' => $this->getTypeMenu( $type ),
 			'levelmenu' => $this->getLevelMenu( $level ),
-			'expirycheck' => [
-				'type' => 'check',
-				'label' => $this->msg( 'protectedpages-indef' )->text(),
-				'name' => 'indefonly',
-				'id' => 'indefonly',
-			],
-			'cascadecheck' => [
-				'type' => 'check',
-				'label' => $this->msg( 'protectedpages-cascade' )->text(),
-				'name' => 'cascadeonly',
-				'id' => 'cascadeonly',
-			],
-			'redirectcheck' => [
-				'type' => 'check',
-				'label' => $this->msg( 'protectedpages-noredirect' )->text(),
-				'name' => 'noredirect',
-				'id' => 'noredirect',
+			'filters' => [
+				'class' => 'HTMLMultiSelectField',
+				'label' => 'Filters',
+				'flatlist' => true,
+				'options' => [
+					$this->msg( 'protectedpages-indef' )->text() => 'indefonly',
+					$this->msg( 'protectedpages-cascade' )->text() => 'cascadeonly',
+					$this->msg( 'protectedpages-noredirect' )->text() => 'noredirect',
+				],
+				'default' => $filters,
 			],
 			'sizelimit' => [
 				'class' => HTMLSizeFilterField::class,
