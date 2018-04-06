7482efe0ddaeb2a5f2039b12a887ea8d0377a4f2
Željko Filipin <zeljko.filipin@gmail.com>
2018-04-06 16:44:53 +0200

    WIP Selenium: run Cucumber tests
    Bug: T179190
    Change-Id: Ia989462e86057c00f96defeed5fb7db2ea2d9a25
    Depends-On: Iad954405a5ae0608fd5dc90dd5dfa434b3781037

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -2,11 +2,8 @@
 
 const password = 'vagrant',
 	path = require( 'path' ),
-	username = 'Admin';
-
-function relPath( foo ) {
-	return path.resolve( __dirname, '../..', foo );
-}
+	username = 'Admin',
+	relPath = ( foo ) => path.resolve( __dirname, '../..', foo );
 
 exports.config = {
 	// ======
@@ -24,14 +21,6 @@ exports.config = {
 		password :
 		process.env.MEDIAWIKI_PASSWORD,
 	//
-	// ======
-	// Sauce Labs
-	// ======
-	//
-	services: [ 'sauce' ],
-	user: process.env.SAUCE_USERNAME,
-	key: process.env.SAUCE_ACCESS_KEY,
-	//
 	// ==================
 	// Specify Test Files
 	// ==================
@@ -41,10 +30,8 @@ exports.config = {
 	// directory is where your package.json resides, so `wdio` will be called from there.
 	//
 	specs: [
-		relPath( './tests/selenium/specs/**/*.js' ),
-		relPath( './extensions/*/tests/selenium/specs/**/*.js' ),
-		relPath( './extensions/VisualEditor/modules/ve-mw/tests/selenium/specs/**/*.js' ),
-		relPath( './skins/*/tests/selenium/specs/**/*.js' )
+		// relPath( './tests/selenium/specs/user.js' ) // mocha
+		relPath( './skins/*/tests/selenium/features/*.feature' ) // cucumber
 	],
 	// Patterns to exclude.
 	exclude: [
@@ -165,7 +152,8 @@ exports.config = {
 	//
 	// Make sure you have the wdio adapter package for the specific framework installed
 	// before running any tests.
-	framework: 'mocha',
+	// framework: 'mocha',
+	framework: 'cucumber',
 	//
 	// Test reporter for stdout.
 	// The only one supported by default is 'dot'
@@ -176,7 +164,27 @@ exports.config = {
 	// See the full list at http://mochajs.org/
 	mochaOpts: {
 		ui: 'bdd',
-		timeout: 20000
+		timeout: 40000
+	},
+	// If you are using Cucumber you need to specify the location of your step definitions.
+	cucumberOpts: {
+		require: [ // <string[]> (file/dir) require files before executing features
+			relPath( './skins/MinervaNeue/tests/selenium/features/support/*.js' ),
+			relPath( './skins/MinervaNeue/tests/selenium/features/step_definitions/*.js' )
+		],
+		backtrace: false, // <boolean> show full backtrace for errors
+		compiler: [], // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+		dryRun: false, // <boolean> invoke formatters without executing steps
+		failFast: false, // <boolean> abort the run on first failure
+		format: [ 'pretty' ], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+		colors: true, // <boolean> disable colors in formatter output
+		snippets: true, // <boolean> hide step definition snippets for pending steps
+		source: true, // <boolean> hide source uris
+		profile: [], // <string[]> (name) specify the profile to use
+		strict: false, // <boolean> fail if there are any undefined or pending steps
+		tags: [], // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+		timeout: 20000, // <number> timeout for step definitions
+		ignoreUndefinedDefinitions: false // <boolean> Enable this config to treat undefined definitions as warnings.
 	},
 	//
 	// =====
@@ -301,4 +309,43 @@ exports.config = {
 	*/
 	// onComplete: function(exitCode, config, capabilities) {
 	// }
+	//
+	// Cucumber hooks
+	//
+	/**
+	* Runs before a Cucumber feature
+	* @param {Object} feature feature details
+	*/
+	// beforeFeature: function (feature) {
+	// },
+	/**
+	* Runs before a Cucumber scenario
+	* @param {Object} scenario scenario details
+	*/
+	// beforeScenario: function (scenario) {
+	// },
+	/**
+	* Runs before a Cucumber step
+	* @param {Object} step step details
+	*/
+	// beforeStep: function (step) {
+	// },
+	/**
+	* Runs after a Cucumber step
+	* @param {Object} stepResult step result
+	*/
+	// afterStep: function (stepResult) {
+	// },
+	/**
+	* Runs after a Cucumber scenario
+	* @param {Object} scenario scenario details
+	*/
+	// afterScenario: function (scenario) {
+	// },
+	/**
+	* Runs after a Cucumber feature
+	* @param {Object} feature feature details
+	*/
+	// afterFeature: function (feature) {
+	// },
 };
