93aa83ad985f029122bd23d869b4ad6d0cb98d74
Željko Filipin <zeljko.filipin@gmail.com>
2018-04-06 16:44:53 +0200

    WIP Selenium: run both Mocha and Cucumber tests
    Bug: T179190
    Change-Id: Ia989462e86057c00f96defeed5fb7db2ea2d9a25
    Depends-On: Iad954405a5ae0608fd5dc90dd5dfa434b3781037

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -202,6 +202,26 @@ exports.config = {
 		ui: 'bdd',
 		timeout: 20000
 	},
+	// If you are using Cucumber you need to specify the location of your step definitions.
+	cucumberOpts: {
+		require: [ // <string[]> (file/dir) require files before executing features
+			relPath( './skins/MinervaNeue/tests/selenium/features/support/*.js' ),
+			relPath( './skins/MinervaNeue/tests/selenium/features/step_definitions/*.js' )
+		],
+		backtrace: false, // <boolean> show full backtrace for errors
+		compiler: [], // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+		dryRun: false, // <boolean> invoke formatters without executing steps
+		failFast: false, // <boolean> abort the run on first failure
+		format: [ 'pretty' ], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+		colors: true, // <boolean> disable colors in formatter output
+		snippets: true, // <boolean> hide step definition snippets for pending steps
+		source: true, // <boolean> hide source uris
+		profile: [], // <string[]> (name) specify the profile to use
+		strict: false, // <boolean> fail if there are any undefined or pending steps
+		tags: [], // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+		timeout: 20000, // <number> timeout for step definitions
+		ignoreUndefinedDefinitions: false // <boolean> Enable this config to treat undefined definitions as warnings.
+	},
 	//
 	// =====
 	// Hooks
@@ -217,6 +237,11 @@ exports.config = {
 	*/
 	// onPrepare: function (config, capabilities) {
 	// },
+	onPrepare: function ( config ) {
+		if ( config.framework === 'cucumber' ) {
+			config.specs = [ relPath( './skins/*/tests/selenium/features/*.feature' ) ];
+		}
+	},
 	/**
 	* Gets executed just before initialising the webdriver session and test framework. It allows you
 	* to manipulate configurations depending on the capability or spec.
@@ -325,4 +350,43 @@ exports.config = {
 	*/
 	// onComplete: function(exitCode, config, capabilities) {
 	// }
+	//
+	// Cucumber hooks
+	//
+	/**
+	* Runs before a Cucumber feature
+	* @param {Object} feature feature details
+	*/
+	// beforeFeature: function (feature) {
+	// },
+	/**
+	* Runs before a Cucumber scenario
+	* @param {Object} scenario scenario details
+	*/
+	// beforeScenario: function (scenario) {
+	// },
+	/**
+	* Runs before a Cucumber step
+	* @param {Object} step step details
+	*/
+	// beforeStep: function (step) {
+	// },
+	/**
+	* Runs after a Cucumber step
+	* @param {Object} stepResult step result
+	*/
+	// afterStep: function (stepResult) {
+	// },
+	/**
+	* Runs after a Cucumber scenario
+	* @param {Object} scenario scenario details
+	*/
+	// afterScenario: function (scenario) {
+	// },
+	/**
+	* Runs after a Cucumber feature
+	* @param {Object} feature feature details
+	*/
+	// afterFeature: function (feature) {
+	// },
 };
