cc2093744db6d8e4b8fc97416b1fa2e260f4f900
Željko Filipin <zeljko.filipin@gmail.com>
2018-04-07 16:35:26 +0200

    Selenium: move logic for running tests from package.json to selenium.sh
    Just running `npm run selenium` in CI does not work. If WebdriverIO fails, the
    job still passes since the last command to run is  `killall chromedriver`.
    Reusing the script from CI that starts and stops Chromedriver should fix the
    problem.
    
    Bug: T179190
    Change-Id: If29227263d23a3e4f26e2329dfa13c49f976cf8e
    (cherry picked from commit d972780d8bb5c1d04ad3f136d63108ae74d61b57)

---
+++ tests/selenium/selenium.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+chromedriver --url-base=/wd/hub --port=4444 &
+# Make sure it is killed to prevent file descriptors leak
+function kill_chromedriver() {
+    killall chromedriver > /dev/null
+}
+trap kill_chromedriver EXIT
+./node_modules/.bin/grunt webdriver:test
