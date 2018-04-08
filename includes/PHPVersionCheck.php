24ae4318b36f88f2dd43db5989074b65776e2cf8
Kunal Mehta <legoktm@member.fsf.org>
2018-04-08 12:39:46 -0700

    Don't use `phpcs:ignoreFile` to selectively ignore sniffs
    Because it doesn't work. `phpcs:ignoreFile` is intended as a performance
    shortcut, and gives up on the file as soon as that token is found.
    Instead, use `phpcs:disable` which does support selectively disabling
    some sniffs. And since disabling is local to the file, there's no need
    to re-enable it at the bottom of the file.
    
    <https://github.com/squizlabs/PHP_CodeSniffer/issues/1903> has some
    relevant discussion and clarification from upstream on this.
    
    And make the files that are now being re-enabled pass PHPCS.
    
    Change-Id: Ia4d8c45045f61cf9f24eb1d8631ff98d99c82d69

--- includes/PHPVersionCheck.php
+++ includes/PHPVersionCheck.php
@@ -1,5 +1,4 @@
 <?php
-// phpcs:ignoreFile Generic.Arrays.DisallowLongArraySyntax,MediaWiki.Usage.DirUsage.FunctionFound
 /**
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +18,7 @@
  * @file
  */
 
+// phpcs:disable Generic.Arrays.DisallowLongArraySyntax,PSR2.Classes.PropertyDeclaration,MediaWiki.Usage.DirUsage
 /**
  * Check PHP Version, as well as for composer dependencies in entry points,
  * and display something vaguely comprehensible in the event of a totally
@@ -124,7 +124,7 @@ class PHPVersionCheck {
 				. "{$otherInfo['minSupported']}.\n\nCheck if you have a"
 				. " newer php executable with a different name, such as php5.\n\n";
 
-			// phpcs:ignore Generic.Files.LineLength
+			// phpcs:disable Generic.Files.LineLength
 			$longHtml = <<<HTML
 			Please consider <a href="{$phpInfo['upgradeURL']}">upgrading your copy of
 			{$phpInfo['implementation']}</a>.
@@ -139,7 +139,7 @@ class PHPVersionCheck {
 			See our<a href="https://www.mediawiki.org/wiki/Compatibility#PHP">compatibility page</a>
 			for details of which versions are compatible with prior versions of {$phpInfo['implementation']}.
 HTML;
-			// phpcs:enable
+			// phpcs:enable Generic.Files.LineLength
 			$this->triggerError(
 				"Supported {$phpInfo['implementation']} versions",
 				$shortText,
@@ -164,14 +164,14 @@ HTML;
 				. "https://www.mediawiki.org/wiki/Download_from_Git#Fetch_external_libraries\n"
 				. "for help on installing the required components.";
 
-			// phpcs:ignore Generic.Files.LineLength
+			// phpcs:disable Generic.Files.LineLength
 			$longHtml = <<<HTML
 		MediaWiki now also has some external dependencies that need to be installed via
 		composer or from a separate git repo. Please see
 		<a href="https://www.mediawiki.org/wiki/Download_from_Git#Fetch_external_libraries">mediawiki.org</a>
 		for help on installing the required components.
 HTML;
-			// phpcs:enable
+			// phpcs:enable Generic.Files.LineLength
 
 			$this->triggerError( 'External dependencies', $shortText, $longText, $longHtml );
 		}
