e8459f722318ceee2f8e335dc7e88e9b0ab64e17
jdlrobson <jdlrobson@gmail.com>
2018-04-09 18:22:13 -0700

    Skins: getDefaultStyles can now define render blocking CSS
    This optimisation attempts to minimise loading the styles in places
    they are not needed.
    
    The logic is kept inside Skin::getDefaultModules to avoid fragmentation
    of where modules get defined.
    
    Bug: T42792
    Bug: T42812
    Change-Id: I59f02a7bab3baa9d43f6bc2ef1f549d9d31d8456

---
+++ tests/phpunit/includes/skins/SkinTest.php
@@ -0,0 +1,27 @@
+<?php
+class TestSkinStyleModules extends Skin {
+	public function getDefaultModules() {
+		$modules = parent::getDefaultModules();
+		$modules['styles']['foo'] = [ 'bar' ];
+		return $modules;
+	}
+}
+
+class SkinTest extends MediaWikiTestCase {
+
+	/**
+	 * @covers Skin::getDefaultModules
+	 */
+	public function testGetDefaultModules() {
+		$skin = $this->getMockBuilder( Skin::class )
+			->setMethods( [ 'outputPage', 'setupSkinUserCss' ] )
+			->getMock();
+
+		$modules = $skin->getDefaultModules();
+		$skinStyle = new TestSkinStyleModules();
+		$modulesStyle = $skinStyle->getDefaultModules();
+		$this->assertTrue( isset( $modules['core'] ), 'core key is set by default' );
+		$this->assertTrue( isset( $modules['styles'] ), 'style key is set by default' );
+		$this->assertTrue( isset( $modulesStyle['styles']['foo'] ), 'foo style key is set by override' );
+	}
+}
