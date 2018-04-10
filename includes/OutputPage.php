a01d8be82c6eb6a39b9715b40b71f78817161a2a
jdlrobson <jdlrobson@gmail.com>
2018-04-09 18:22:13 -0700

    Skins: getDefaultStyles can now define render blocking CSS
    This optimisation attempts to minimise loading the styles in places
    they are not needed.
    
    The logic is kept inside Skin::getDefaultModules to avoid fragmentation
    of where modules get defined.
    
    Update ApiParse to avoid repetition of code.
    
    Bug: T42792
    Bug: T42812
    Change-Id: I59f02a7bab3baa9d43f6bc2ef1f549d9d31d8456

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -2332,6 +2332,23 @@ class OutputPage extends ContextSource {
 	}
 
 	/**
+	 * Transfer styles and JavaScript modules from skin.
+	 *
+	 * @param Skin $sk to load modules for
+	 */
+	public function loadSkinModules( $sk ) {
+		foreach ( $sk->getDefaultModules() as $group => $modules ) {
+			if ( $group === 'styles' ) {
+				foreach ( $modules as $key => $moduleMembers ) {
+					$this->addModuleStyles( $moduleMembers );
+				}
+			} else {
+				$this->addModules( $modules );
+			}
+		}
+	}
+
+	/**
 	 * Finally, all the text has been munged and accumulated into
 	 * the object, let's actually output it:
 	 *
@@ -2424,9 +2441,7 @@ class OutputPage extends ContextSource {
 			}
 
 			$sk = $this->getSkin();
-			foreach ( $sk->getDefaultModules() as $group ) {
-				$this->addModules( $group );
-			}
+			$this->loadSkinModules( $sk );
 
 			MWDebug::addModules( $this );
 
