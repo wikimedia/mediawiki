c61af00ef68b824960a68520d1be17f2eb8e7c5c
daniel <daniel.kinzler@wikimedia.de>
2018-04-10 17:31:33 +0200

    Tests for preserving content ID and origin during undeletion
    Change-Id: I6d9a1390c7080d910d43097afb703d13cd500c1b

--- includes/Storage/RevisionStore.php
+++ includes/Storage/RevisionStore.php
@@ -830,6 +830,11 @@ class RevisionStore
 			// keep with ar prefix:
 			'ar_id'        => 'ar_id',
 
+			// keep slot and content fields:
+			'content_id'        => 'content_id',
+			'content_address'   => 'content_address',
+			'slot_origin'       => 'slot_origin',
+
 			// not the same suffix:
 			'ar_page_id'        => 'rev_page',
 			'ar_rev_id'         => 'rev_id',
