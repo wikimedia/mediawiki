cdea709df5c9cf058deb814e23f38627df12172c
daniel <daniel.kinzler@wikimedia.de>
2018-04-11 14:16:18 +0200

    [MCR] [DNM] Set MCR migration stage to MIGRATION_WRITE_BOTH.
    This patch exists to see if CI passes with MIGRATION_WRITE_BOTH.
    
    NO NOT MERGE before either $wgMultiContentRevisionSchemaMigrationStage
    is set to MIGRATION_OLD in production config, or the MCR tables have
    been created in all production DBs.
    
    Bug: T174024
    Change-Id: I6d6c642a0e34964672604340473932fcc0c6e965

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -8892,7 +8892,7 @@ $wgCommentTableSchemaMigrationStage = MIGRATION_OLD;
  * @since 1.32
  * @var int One of the MIGRATION_* constants
  */
-$wgMultiContentRevisionSchemaMigrationStage = MIGRATION_OLD;
+$wgMultiContentRevisionSchemaMigrationStage = MIGRATION_WRITE_BOTH;
 
 /**
  * Actor table schema migration stage.
