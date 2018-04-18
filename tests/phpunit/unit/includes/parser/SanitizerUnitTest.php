a52c933998418d71340b7f3920423fc57557ac17
James D. Forrester <jforrester@wikimedia.org>
2018-04-17 18:25:20 -0700

    Drop Sanitizer::escapeId(), deprecated in MediaWiki 1.30
    Hard deprecation was in b79c1e2, which shipped in MediaWiki 1.35.
    
    Change-Id: I7186462c95d346f362ba0cf84b136c083d66a7d3

--- tests/phpunit/unit/includes/parser/SanitizerUnitTest.php
+++ tests/phpunit/unit/includes/parser/SanitizerUnitTest.php
@@ -199,42 +199,6 @@ class SanitizerUnitTest extends MediaWikiUnitTestCase {
 	}
 
 	/**
-	 * Test Sanitizer::escapeId
-	 *
-	 * @dataProvider provideEscapeId
-	 * @covers Sanitizer::escapeId
-	 */
-	public function testEscapeId( $input, $output ) {
-		$this->hideDeprecated( 'Sanitizer::escapeId' );
-		$this->assertSame(
-			$output,
-			Sanitizer::escapeId( $input, [ 'noninitial', 'legacy' ] )
-		);
-	}
-
-	public static function provideEscapeId() {
-		return [
-			[ '+', '.2B' ],
-			[ '&', '.26' ],
-			[ '=', '.3D' ],
-			[ ':', ':' ],
-			[ ';', '.3B' ],
-			[ '@', '.40' ],
-			[ '$', '.24' ],
-			[ '-_.', '-_.' ],
-			[ '!', '.21' ],
-			[ '*', '.2A' ],
-			[ '/', '.2F' ],
-			[ '[]', '.5B.5D' ],
-			[ '<>', '.3C.3E' ],
-			[ '\'', '.27' ],
-			[ '§', '.C2.A7' ],
-			[ 'Test:A & B/Here', 'Test:A_.26_B.2FHere' ],
-			[ 'A&B&amp;C&amp;amp;D&amp;amp;amp;E', 'A.26B.26amp.3BC.26amp.3Bamp.3BD.26amp.3Bamp.3Bamp.3BE' ],
-		];
-	}
-
-	/**
 	 * @dataProvider provideIsReservedDataAttribute
 	 * @covers Sanitizer::isReservedDataAttribute
 	 */
