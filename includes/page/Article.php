ed4fc48323a04c79c58d428a9513c1ed2f83cd31
Reedy <reedy@wikimedia.org>
2018-04-19 19:01:26 +0000

    Don't pass USE INDEX to a $dbType parameter
    Bug: T192584
    Change-Id: Ia2b761bad570532696419b4f23f84bc85ff2c0e8

--- includes/page/Article.php
+++ includes/page/Article.php
@@ -1046,8 +1046,7 @@ class Article implements Page {
 						'rc_namespace' => NS_FILE,
 						'rc_cur_id' => $title->getArticleID()
 					],
-					__METHOD__,
-					[ 'USE INDEX' => 'rc_timestamp' ]
+					__METHOD__
 				);
 				if ( $rc ) {
 					// Use patrol message specific to files
