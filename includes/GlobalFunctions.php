ccface015fd222744b87585843ecd80a875e1978
Max Semenik <maxsem.wiki@gmail.com>
2018-04-19 14:11:28 -0700

    Begin introducing PHP 5.6 variadic parameters where appropriate
    Change-Id: I5670b8482e8d3bcb0b3a2b4d2ce9834cfc37e171

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -1336,21 +1336,19 @@ function wfGetLangObj( $langcode = false ) {
  * This function replaces all old wfMsg* functions.
  *
  * @param string|string[]|MessageSpecifier $key Message key, or array of keys, or a MessageSpecifier
- * @param mixed $params,... Normal message parameters
+ * @param string[] $params Normal message parameters
  * @return Message
  *
  * @since 1.17
  *
  * @see Message::__construct
  */
-function wfMessage( $key /*...*/ ) {
+function wfMessage( $key, ...$params ) {
 	$message = new Message( $key );
 
 	// We call Message::params() to reduce code duplication
-	$params = func_get_args();
-	array_shift( $params );
 	if ( $params ) {
-		call_user_func_array( [ $message, 'params' ], $params );
+		$message->params( ...$params );
 	}
 
 	return $message;
@@ -1368,9 +1366,8 @@ function wfMessage( $key /*...*/ ) {
  *
  * @see Message::newFallbackSequence
  */
-function wfMessageFallback( /*...*/ ) {
-	$args = func_get_args();
-	return call_user_func_array( 'Message::newFallbackSequence', $args );
+function wfMessageFallback( ...$keys ) {
+	return Message::newFallbackSequence( ...$keys );
 }
 
 /**
