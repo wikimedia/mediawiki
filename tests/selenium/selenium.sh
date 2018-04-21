315fbfda447dfcb300d5a0743eca158956efe314
Timo Tijhof <krinklemail@gmail.com>
2018-04-21 02:10:56 +0100

    selenium: Run wdio directly without grunt
    Doesn't seem to add anything, except complexity.
    
    This has the benefit of matching more closely the way the README
    recommends running individual tests.
    
    Change-Id: Id95f18927b5443defe679a77a82c5cbdd127c716

--- tests/selenium/selenium.sh
+++ tests/selenium/selenium.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
+# Check the command before running in background so
+# that it can actually fail and have a descriptive error
+hash chromedriver
 chromedriver --url-base=/wd/hub --port=4444 &
 # Make sure it is killed to prevent file descriptors leak
 function kill_chromedriver() {
     killall chromedriver > /dev/null
 }
 trap kill_chromedriver EXIT
-./node_modules/.bin/grunt webdriver:test
+./node_modules/.bin/wdio ./tests/selenium/wdio.conf.js
