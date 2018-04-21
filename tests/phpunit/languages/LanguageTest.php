22944fdee814cc8a0ea2e479ee47c4c7ab4be81b
superyetkin <superyetkin@yahoo.com>
2018-04-22 00:12:02 +0300

    Fix the bug for dates between 1912 and 1941 in Thai language
    Added an if-else block to see if the parameters passed to the function
    designate a year between 1912 and 1941 or not. Resulting month values
    are also adjusted.
    Added a unit test for the related formatting.
    
    Bug: T68648
    Change-Id: Ic676b5c140de8878971a786a1a1811770a848016

--- tests/phpunit/languages/LanguageTest.php
+++ tests/phpunit/languages/LanguageTest.php
@@ -1030,6 +1030,13 @@ class LanguageTest extends LanguageClassesTestCase {
 				'Thai year'
 			],
 			[
+				'xkY',
+				'19400101090705',
+				'2482',
+				'2482',
+				'Thai year'
+			],
+			[
 				'xoY',
 				'20120102090705',
 				'101',
