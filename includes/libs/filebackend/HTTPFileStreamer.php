b6853e00097b545521e09289b714f505cf55f55d
Aaron Schulz <aschulz@wikimedia.org>
2018-04-26 19:16:02 -0700

    Remove bogus @throws from HTTPFileStreamer
    Change-Id: I28928a6f01b107b2aed8ab6fbeb013410a8f9718

--- includes/libs/filebackend/HTTPFileStreamer.php
+++ includes/libs/filebackend/HTTPFileStreamer.php
@@ -64,7 +64,6 @@ class HTTPFileStreamer {
 	 * @param bool $sendErrors Send error messages if errors occur (like 404)
 	 * @param array $optHeaders HTTP request header map (e.g. "range") (use lowercase keys)
 	 * @param int $flags Bitfield of STREAM_* constants
-	 * @throws MWException
 	 * @return bool Success
 	 */
 	public function stream(
