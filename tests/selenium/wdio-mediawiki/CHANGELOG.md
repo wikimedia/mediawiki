ccf7548a86dd0064afb029b8f0078849cd7a67af
Timo Tijhof <krinklemail@gmail.com>
2018-05-02 18:48:24 +0100

    selenium: Initial version of wdio-mediawiki package
    This is new package will be reusable by other repositories for
    their browser tests, without having to reference the internal
    selenium/pageobjects/ directory from MediaWiki core.
    
    In addition to not requiring direct imports, it will also avoid
    problems in the future by allowing the package to be versioned
    and iterated upon without forcing an atomic global upgrade
    (or broken master builds), everytime we change something.
    
    See README for details.
    
    Within MediaWiki core itself, the package is used using the
    'file' specifier in its package.json, so that we always test
    and develop using its working copy, which makes drafting and
    testing changes easier.
    
    Bug: T193088
    Change-Id: I547a7899e7a97693a93567dd763784e637433d55

---
+++ tests/selenium/wdio-mediawiki/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Notable changes
+
+## [Unreleased]
+
+* Api: Added initial version.
+* Page: Added initial version.
+* BlankPage: Added initial version.
+* LoginPage: Added initial version.
---
+++ tests/selenium/wdio-mediawiki/LoginPage.js
@@ -0,0 +1,25 @@
+const Page = require( 'wdio-mediawiki/Page' );
+
+class LoginPage extends Page {
+	get username() { return browser.element( '#wpName1' ); }
+	get password() { return browser.element( '#wpPassword1' ); }
+	get loginButton() { return browser.element( '#wpLoginAttempt' ); }
+	get userPage() { return browser.element( '#pt-userpage' ); }
+
+	open() {
+		super.openTitle( 'Special:UserLogin' );
+	}
+
+	login( username, password ) {
+		this.open();
+		this.username.setValue( username );
+		this.password.setValue( password );
+		this.loginButton.click();
+	}
+
+	loginAdmin() {
+		this.login( browser.options.username, browser.options.password );
+	}
+}
+
+module.exports = new LoginPage();
