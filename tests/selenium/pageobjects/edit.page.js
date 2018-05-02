733c6722427a0bb4ed6275573433d0abe769f08d
Timo Tijhof <krinklemail@gmail.com>
2018-05-02 04:48:36 +0100

    selenium: Minor clean-up in preparation for packaging
    This is functionally a no-op, purely refactoring (mostly style).
    
    * Consistently require packages at the top of a file.
      (e.g. MWBot in edit.page.js).
    
    * Remove unused .call(this) from mwbot interaction closures,
      which didn't use 'this'.
    
    * Use Node.js regular Promise chaining with then(), instead of
      complex bluebird.coroutine generator function yields, which
      are intended to emulate async-await, but the syntax is quite
      error-prone for inexperienced developers and hard to debug.
      Once we require Node 7+ for the selenium tests, we can use
      async-await here natively, but until then, might as well use
      regular then() syntax, which we already use elsewhere in the
      tests, and is also what MWBot documentation uses.
    
    * Also applied some minor whitespace changes for consistency
      among these files and other MediaWiki JS. E.g. no empty line
      before the first statement of a function. Add a new line between
      differnet methods, and between the end of a class and the
      export statement.
    
    * Remove unused bluebird dependency.
    
    Bug: T193088
    Change-Id: I59f9211299e8e884c28c7733bcee3b7b28542610

--- tests/selenium/pageobjects/edit.page.js
+++ tests/selenium/pageobjects/edit.page.js
@@ -1,8 +1,8 @@
-'use strict';
-const Page = require( './page' );
+const Page = require( './page' ),
+	// https://github.com/Fannon/mwbot
+	MWBot = require( 'mwbot' );
 
 class EditPage extends Page {
-
 	get content() { return browser.element( '#wpTextbox1' ); }
 	get displayedContent() { return browser.element( '#mw-content-text' ); }
 	get heading() { return browser.element( '#firstHeading' ); }
@@ -19,21 +19,16 @@ class EditPage extends Page {
 	}
 
 	apiEdit( name, content ) {
-
-		const MWBot = require( 'mwbot' ), // https://github.com/Fannon/mwbot
-			Promise = require( 'bluebird' );
 		let bot = new MWBot();
 
-		return Promise.coroutine( function* () {
-			yield bot.loginGetEditToken( {
-				apiUrl: `${browser.options.baseUrl}/api.php`,
-				username: browser.options.username,
-				password: browser.options.password
-			} );
-			yield bot.edit( name, content, `Created page with "${content}"` );
-		} ).call( this );
-
+		return bot.loginGetEditToken( {
+			apiUrl: `${browser.options.baseUrl}/api.php`,
+			username: browser.options.username,
+			password: browser.options.password
+		} ).then( function () {
+			return bot.edit( name, content, `Created page with "${content}"` );
+		} );
 	}
-
 }
+
 module.exports = new EditPage();
