bd331877940513fe682cf300a9472db32c2c597c
Antoine Musso <hashar@free.fr>
2018-05-04 13:20:41 +0200

    installer: load extension when including them
    When an extension has extension registry (extension.json), the global
    variables it defines are not loaded in the installer.  Only
    $wgAutoloadClasses is being used.
    
    GeoData has a LoadExtensionSchemaUpdates hook which would throw an
    exception whenever wgGeoDataBackend is not set. That is the case when
    installing with GeoData cloned and using: install.php --with-extension
    
    Change the installer extension includer to load all settings. Which
    includes defined global settings.
    Clean up the $wgAutoloadClasses manual loading which is no more needed.
    
    Bug: T193855
    Change-Id: Ia8b0e9bdd43e4acb8a68d8b454d2316b7e52dd3c

--- includes/installer/Installer.php
+++ includes/installer/Installer.php
@@ -1441,31 +1441,19 @@ abstract class Installer {
 		// double load extensions
 		define( 'MW_EXTENSIONS_LOADED', true );
 
-		/**
-		 * We need to include DefaultSettings before including extensions to avoid
-		 * warnings about unset variables. However, the only thing we really
-		 * want here is $wgHooks['LoadExtensionSchemaUpdates']. This won't work
-		 * if the extension has hidden hook registration in $wgExtensionFunctions,
-		 * but we're not opening that can of worms
-		 * @see https://phabricator.wikimedia.org/T28857
-		 */
-		global $wgAutoloadClasses;
-		$wgAutoloadClasses = [];
 		$queue = [];
 
 		require "$IP/includes/DefaultSettings.php";
 
+		$registry = new ExtensionRegistry();
 		foreach ( $exts as $e ) {
 			if ( file_exists( "$IP/extensions/$e/extension.json" ) ) {
-				$queue["$IP/extensions/$e/extension.json"] = 1;
+				$registry->queue( "$IP/extensions/$e/extension.json" );
 			} else {
 				require_once "$IP/extensions/$e/$e.php";
 			}
 		}
-
-		$registry = new ExtensionRegistry();
-		$data = $registry->readFromQueue( $queue );
-		$wgAutoloadClasses += $data['autoload'];
+		$registry->loadFromQueue();
 
 		$hooksWeWant = isset( $wgHooks['LoadExtensionSchemaUpdates'] ) ?
 			/** @suppress PhanUndeclaredVariable $wgHooks is set by DefaultSettings */
