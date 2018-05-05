9a1f8516d14c4333eccb073731ecc7e353c50cd5
Volker E <volker.e@wikimedia.org>
2018-05-05 16:19:08 -0700

    Special:PrefixIndex: Override subpage in form field
    * This hack originally comes from Icce7cc4585e90742a8dd3513e7c9f7276e479cd7
    * Id and default value of 'prefix' is Regression
      from Ieb9713f8346316e9c3cf1e83eae00848f3921b43
    
    Bug: T193927
    Change-Id: I9a3477af89a7e303a67f1769859a649b86113604

---
+++ tests/phpunit/includes/specials/SpecialPrefixindexTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Test class for Prefixindex class
+ *
+ * @group Database
+ */
+class SpecialPrefixindexTest extends MediaWikiTestCase {
+	public function testSubPageRedirect() {
+		$this->setMwGlobals( [
+			'wgScript' => '/w/index.php',
+		] );
+
+		$ctx = new RequestContext;
+		$sp = Title::newFromText( 'Special:Prefixindex/foo_bar' );
+		SpecialPageFactory::executePath( $sp, $ctx );
+		$url = $ctx->getOutput()->getRedirect();
+		// some older versions of hhvm have a bug that doesn't parse relative
+		// urls with a port, so help it out a little bit.
+		// https://github.com/facebook/hhvm/issues/7136
+		$url = wfExpandUrl( $url, PROTO_CURRENT );
+
+		$parts = parse_url( $url );
+		$this->assertEquals( '/w/index.php/Special:PrefixIndex/foo_bar', $parts['path'] );
+		parse_str( $parts['query'], $query );
+		$this->assertEquals( 'Special:Prefixindex', $query['title'] );
+		$this->assertEquals( 'foo bar', $query['prefix'] );
+	}
+}
