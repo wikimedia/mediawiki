39ca88374ed3bb9c40ea7fe0ff4c5e79e1ebef6e
jdlrobson <jdlrobson@gmail.com>
2018-05-07 13:46:41 -0700

    jquery.tablesorter: Allow this module to load on mobile
    If any special pages are loading this code, let it load.
    Skins can disable this code if necessary (see Minerva for example)
    In T111565 we will look to enabling this on the Minerva skin.
    
    Bug: T111565
    Change-Id: I837c16bdad7769f5cb4ce0bcfadb380b34c48bd4

--- resources/Resources.php
+++ resources/Resources.php
@@ -321,6 +321,7 @@ return [
 		'scripts' => 'resources/src/jquery/jquery.tabIndex.js',
 	],
 	'jquery.tablesorter' => [
+		'targets' => [ 'desktop', 'mobile' ],
 		'scripts' => 'resources/src/jquery.tablesorter/jquery.tablesorter.js',
 		'styles' => 'resources/src/jquery.tablesorter/jquery.tablesorter.less',
 		'messages' => [ 'sort-descending', 'sort-ascending' ],
@@ -1648,6 +1649,7 @@ return [
 	],
 
 	'mediawiki.language.months' => [
+		'targets' => [ 'desktop', 'mobile' ],
 		'scripts' => 'resources/src/mediawiki.language/mediawiki.language.months.js',
 		'dependencies' => 'mediawiki.language',
 		'messages' => array_merge(
