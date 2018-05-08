74b6bd5f22687843bd91044105d87e7e39b27b4d
Bartosz Dziewoński <matma.rex@gmail.com>
2018-05-08 21:12:00 +0200

    mw.page.gallery: Prevent jumpiness due to viewport height changes
    * Do not recalculate if only viewport height has changed, but not
      width. Viewport height doesn't affect this at all.
    * Set a min-height on the gallery element before resetting image
      dimensions so that content following it only reflows once, when
      we calculate dimensions again.
    
    Bug: T194193
    Change-Id: I6362863cc0f3f7b76c9ec7437d37ff6762ecf153

--- resources/src/mediawiki/page/gallery.js
+++ resources/src/mediawiki/page/gallery.js
@@ -5,6 +5,8 @@
 ( function ( mw, $ ) {
 	var $galleries,
 		bound = false,
+		lastWidth = window.innerWidth,
+		justifyNeeded = false,
 		// Is there a better way to detect a touchscreen? Current check taken from stack overflow.
 		isTouchScreen = !!( window.ontouchstart !== undefined ||
 			window.DocumentTouch !== undefined && document instanceof window.DocumentTouch
@@ -205,6 +207,16 @@
 	}
 
 	function handleResizeStart() {
+		// Only do anything if window width changed. We don't care about the height.
+		if ( lastWidth === window.innerWidth ) {
+			return;
+		}
+
+		justifyNeeded = true;
+		$galleries.each( function () {
+			// Temporarily set height, so that content following the gallery is not reflowed
+			$( this ).css( 'min-height', $( this ).height() );
+		} );
 		$galleries.children( 'li.gallerybox' ).each( function () {
 			var imgWidth = $( this ).data( 'imgWidth' ),
 				imgHeight = $( this ).data( 'imgHeight' ),
@@ -232,7 +244,14 @@
 	}
 
 	function handleResizeEnd() {
-		$galleries.each( justify );
+		// If window width never changed during the resize, don't do anything.
+		if ( justifyNeeded ) {
+			justifyNeeded = false;
+			lastWidth = window.innerWidth;
+			// Reset temporary height
+			$galleries.css( 'min-height', 'auto' );
+			$galleries.each( justify );
+		}
 	}
 
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
