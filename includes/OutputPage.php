cb6a24d3c420430f6c7c0fcebbb0c3dfb32aee49
Timo Tijhof <krinklemail@gmail.com>
2018-05-08 19:17:52 +0100

    resourceloader: Bump severity of targets violation to WARNING
    Now that we've all known violations we can safely enable this
    in production. It was mainly held back because the Gadgets
    extension violated it "by default" due to not checking targets
    and letting core handle it. This has been fixed by f59eaacb4f72d8
    in MobileFrontend and 2008def8063 in Gadgets.
    
    Bug: T127268
    Change-Id: I43aed5011f96160565f2dfc3422034d0e8fa95c4

--- includes/OutputPage.php
+++ includes/OutputPage.php
@@ -510,7 +510,7 @@ class OutputPage extends ContextSource {
 			return;
 		}
 		$warnings[$this->mTarget][$moduleName] = true;
-		$this->getResourceLoader()->getLogger()->debug(
+		$this->getResourceLoader()->getLogger()->warning(
 			'Module "{module}" not loadable on target "{target}".',
 			[
 				'module' => $moduleName,
