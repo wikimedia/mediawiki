973900c4484c27102d1513de128aec8eae007408
Erik Bernhardson <ebernhardson@wikimedia.org>
2018-05-10 15:03:55 -0700

    Convert SearchResultSet to typical iteration
    The funky iteration here was at best annoying. Switch
    it over to an iterator based approach with appropriate
    BC code to simulate the old iteration style.
    
    Change-Id: I9fccda15dd58a0dc35771d3b5cd7a6e8b02514a0

--- includes/search/SqlSearchResultSet.php
+++ includes/search/SqlSearchResultSet.php
@@ -7,8 +7,11 @@ use Wikimedia\Rdbms\ResultWrapper;
  * @ingroup Search
  */
 class SqlSearchResultSet extends SearchResultSet {
+	/** @var ResultWrapper Result object from database */
 	protected $resultSet;
+	/** @var string Requested search query */
 	protected $terms;
+	/** @var int|null Total number of hits for $terms */
 	protected $totalHits;
 
 	function __construct( ResultWrapper $resultSet, $terms, $total = null ) {
@@ -29,25 +32,21 @@ class SqlSearchResultSet extends SearchResultSet {
 		return $this->resultSet->numRows();
 	}
 
-	function next() {
+	public function extractResults() {
 		if ( $this->resultSet === false ) {
-			return false;
+			return [];
 		}
 
-		$row = $this->resultSet->fetchObject();
-		if ( $row === false ) {
-			return false;
-		}
-
-		return SearchResult::newFromTitle(
-			Title::makeTitle( $row->page_namespace, $row->page_title ), $this
-		);
-	}
-
-	function rewind() {
-		if ( $this->resultSet ) {
+		if ( $this->results === null ) {
+			$this->results = [];
 			$this->resultSet->rewind();
+			while ( ( $row = $this->resultSet->fetchObject() ) !== false ) {
+				$this->results[] = SearchResult::newFromTitle(
+					Title::makeTitle( $row->page_namespace, $row->page_title ), $this
+				);
+			}
 		}
+		return $this->results;
 	}
 
 	function free() {
