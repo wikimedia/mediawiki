c0eed57e055a21755fe20f9ece0853170061ea82
Aaron Schulz <aschulz@wikimedia.org>
2018-05-10 16:18:19 -0700

    Give ChronologyProtector cookies a lower TTL
    Since it takes time for the agent to get the response and set the
    cookie and, as well, the time into a request that a LoadBalancer is
    initialized varies by many seconds (cookies loaded from the start),
    give the cookie a much lower TTL than the DB positions in the stash.
    
    This avoids having to wait for a position with a given cpPosIndex
    value, when the position already expired from the store, which is
    a waste of time.
    
    Bug: T194403
    Bug: T190082
    Change-Id: I97d8f108dec59c5ccead66432a097cda8ef4a178

--- includes/MediaWiki.php
+++ includes/MediaWiki.php
@@ -25,6 +25,7 @@ use Psr\Log\LoggerInterface;
 use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\ChronologyProtector;
 use Wikimedia\Rdbms\LBFactory;
+use Wikimedia\Rdbms\LoadBalancer;
 use Wikimedia\Rdbms\DBConnectionError;
 use Liuggio\StatsdClient\Sender\SocketSender;
 
@@ -636,7 +637,8 @@ class MediaWiki {
 
 		if ( $cpIndex > 0 ) {
 			if ( $allowHeaders ) {
-				$expires = time() + ChronologyProtector::POSITION_TTL;
+				// T194403: this should be safely less than ChronologyProtector::POSITION_TTL
+				$expires = time() + LoadBalancer::MAX_WAIT_DEFAULT;
 				$options = [ 'prefix' => '' ];
 				$request->response()->setCookie( 'cpPosIndex', $cpIndex, $expires, $options );
 			}
