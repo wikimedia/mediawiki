be42121ab028d1db9d9b738f685d3be4cae36b71
Tim Starling <tstarling@wikimedia.org>
2018-05-20 16:50:41 +0200

    Maintenance script to wrap the mysql binary
    This is more or less equivalent to the WMF shell script "sql", but it is
    reasonably portable and written in a less hackish way.
    
    Change-Id: I240d8d4e65ce46680cac257ee376a9b026c13f92

---
+++ maintenance/mysql.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Execute the MySQL client binary, connecting to the wiki's DB.
+ * Note that this will not do table prefixing or variable substitution.
+ * To safely run schema patches, use sql.php.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ */
+
+use MediaWiki\Shell\Shell;
+use MediaWiki\MediaWikiServices;
+
+require_once __DIR__ . '/Maintenance.php';
+
+class MysqlMaintenance extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( "Execute the MySQL client binary. " .
+			"Non-option arguments will be passed through to mysql." );
+		$this->addOption( 'write', 'Connect to the master database',false, false );
+		$this->addOption( 'group', 'Specify query group', false, false );
+		$this->addOption( 'host', 'Connect to a specific MySQL server', false, true );
+		$this->addOption( 'list-hosts', 'List the available DB hosts', false, false );
+		$this->addOption( 'cluster', 'Use an external cluster by name', false, true );
+		$this->addOption( 'wikidb',
+			'The database wiki ID to use if not the current one', false, true );
+	}
+
+	public function execute() {
+		$dbName = $this->getOption( 'wikidb', false );
+		$lbf = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
+		if ( $this->hasOption( 'cluster' ) ) {
+			try {
+				$lb = $lbf->getExternalLB( $this->getOption( 'cluster' ) );
+			} catch ( InvalidArgumentException $e ) {
+				$this->error( "Error: invalid cluster" );
+				return 1;
+			}
+		} else {
+			$lb = $lbf->getMainLB( $dbName );
+		}
+		if ( $this->hasOption( 'list-hosts' ) ) {
+			$serverCount = $lb->getServerCount();
+			for ( $index = 0; $index < $serverCount; ++$index ) {
+				echo $lb->getServerName( $index ) . "\n";
+			}
+			return 0;
+		}
+		if ( $this->hasOption( 'host' ) ) {
+			$host = $this->getOption( 'host' );
+			$serverCount = $lb->getServerCount();
+			for ( $index = 0; $index < $serverCount; ++$index ) {
+				$serverInfo = $lb->getServerInfo( $index );
+				
+				if ( $lb->getServerName( $index ) === $host ) {
+					break;
+				}
+			}
+			if ( $index >= $serverCount ) {
+				$this->error( "Error: Host not configured: \"$host\"" );
+				return 1;
+			}
+		} elseif ( $this->hasOption( 'write' ) ) {
+			$index = $lb->getWriterIndex();
+		} else {
+			$group = $this->getOption( 'group', false );
+			$index = $lb->getReaderIndex( $group, $dbName );
+			if ( $index === false ) {
+				$this->error( "Error: unable to get reader index" );
+				return 1;
+			}
+		}
+
+		// Write the password to an option file to avoid disclosing it to other
+		// processes running on the system
+		$info = $lb->getServerInfo( $index );
+		$tmpFile = TempFSFile::factory( 'mw-mysql', 'ini' );
+		chmod( $tmpFile->getPath(), 0600 );
+		file_put_contents( $tmpFile->getPath(), "[client]\npassword={$info['password']}\n" );
+
+		// stdin/stdout need to be the actual file descriptors rather than
+		// PHP's pipe wrappers so that mysql can use them as an interactive
+		// terminal.
+		$desc = [
+			0 => STDIN,
+			1 => STDOUT,
+			2 => STDERR,
+		];
+
+		// Split host and port as in DatabaseMysqli::mysqlConnect()
+		$realServer = $info['host'];
+		$hostAndPort = IP::splitHostAndPort( $realServer );
+		$socket = false;
+		$port = false;
+		if ( $hostAndPort ) {
+			$realServer = $hostAndPort[0];
+			if ( $hostAndPort[1] ) {
+				$port = $hostAndPort[1];
+			}
+		} elseif ( substr_count( $realServer, ':' ) == 1 ) {
+			// If we have a colon and something that's not a port number
+			// inside the hostname, assume it's the socket location
+			$hostAndSocket = explode( ':', $realServer );
+			$realServer = $hostAndSocket[0];
+			$socket = $hostAndSocket[1];
+		}
+
+		if ( $dbName === false ) {
+			$dbName = $info['dbname']; 
+		}
+
+		$args = [
+			'mysql',
+			"--defaults-extra-file={$tmpFile->getPath()}",
+			"--user={$info['user']}",
+			"--database={$dbName}",
+		];
+		if ( $socket !== false ) {
+			$args[] = "--socket={$socket}";
+		} else {
+			$args[] = "--host={$realServer}";
+		}
+		if ( $port !== false ) {
+			$args[] = "--port={$port}";
+		}
+
+		$args = array_merge( $args, $this->mArgs );
+		
+		$pipes = [];
+		$proc = proc_open( Shell::escape( $args ), $desc, $pipes );
+		if ( $proc === false ) {
+			$this->error( "Unable to execute mysql" );
+			return 1;
+		}
+		$ret = proc_close( $proc );
+		if ( $ret === -1 ) {
+			$this->error( "proc_close() returned -1" );
+			return 1;
+		}
+		return $ret;
+	}
+}
+
+$maintClass = 'MysqlMaintenance';
+require_once RUN_MAINTENANCE_IF_MAIN;
