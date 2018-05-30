35d1ea57e1f48b768bfe1af5fa325778949a4b64
Fomafix <fomafix@googlemail.com>
2018-05-30 06:33:01 +0200

    Allow and use type Language instead of string for $lang of doEditSectionLink
    Hard-deprecate that the parameters $tooltip and $lang are optional.
    Hard-deprecate other types than Language for parameter $lang.
    
    Change-Id: I3ce048e3d0de9a4e96e2aa05d75a4ce41c3ec964
    Depends-On: Ibe295e7020e995eea52e319feaf59c635f8bb4dc

--- includes/parser/ParserOutput.php
+++ includes/parser/ParserOutput.php
@@ -338,7 +338,7 @@ class ParserOutput extends CacheTime {
 					return $skin->doEditSectionLink( $editsectionPage,
 						$editsectionSection,
 						$editsectionContent,
-						$wgLang->getCode()
+						$wgLang
 					);
 				},
 				$text
--- includes/skins/Skin.php
+++ includes/skins/Skin.php
@@ -1610,15 +1610,20 @@ abstract class Skin extends ContextSource {
 	 * @param string $section The designation of the section being pointed to,
 	 *   to be included in the link, like "&section=$section"
 	 * @param string|null $tooltip The tooltip to use for the link: will be escaped
-	 *   and wrapped in the 'editsectionhint' message
-	 * @param string $lang Language code
+	 *   and wrapped in the 'editsectionhint' message.
+	 *   Not setting this parameter is deprecated.
+	 * @param Language|string $lang Language object or language code string.
+	 *   Type string is deprecated. Not setting this parameter is deprecated.
 	 * @return string HTML to use for edit link
 	 */
 	public function doEditSectionLink( Title $nt, $section, $tooltip = null, $lang = false ) {
 		// HTML generated here should probably have userlangattributes
 		// added to it for LTR text on RTL pages
 
-		$lang = wfGetLangObj( $lang );
+		if ( !$lang instanceof Language ) {
+			wfDeprecated( __METHOD__ . ' with other type than Language for $lang', '1.32' );
+			$lang = wfGetLangObj( $lang );
+		}
 
 		$attribs = [];
 		if ( !is_null( $tooltip ) ) {
