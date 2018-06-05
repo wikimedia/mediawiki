2cd1c45167d0d244f0988a335167d6016d69572f
rvogel <vogel@hallowelt.biz>
2018-06-05 09:12:56 +0200

    Add extensions from BlueSpiceBookmaker and BlueSpiceServiceCenter

---
+++ settings.d/050-BlueSpiceServiceCenter.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once( "$IP/extensions/BlueSpiceServiceCenter/CategoryCheck/CategoryCheck.setup.php" );
+require_once( "$IP/extensions/BlueSpiceServiceCenter/_templates/_templates.php" );
