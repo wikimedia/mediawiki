6d5c3f69a2daec3e8220f14af844b9567349929d
rvogel <vogel@hallowelt.biz>
2018-06-05 11:55:02 +0200

    Remove duplicate settings file

--- 050-BlueSpiceServiceCenter.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once( "$IP/extensions/BlueSpiceServiceCenter/CategoryCheck/CategoryCheck.setup.php" );
-require_once( "$IP/extensions/BlueSpiceServiceCenter/_templates/_templates.php" );
