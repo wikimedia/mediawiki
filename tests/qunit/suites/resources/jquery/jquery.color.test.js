81547bfbcb65bd1b2b5b8d9666294ec6143cb8ce
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2018-06-06 16:54:09 +0200

    Try to unbreak jquery.color QUnit test
    This consistently fails in the AdvancedSearch codebase with an
    "Pending AJAX requests: 1 (active: 1)" error. I suggest to set the
    duration to 0. Yes, I'm aware this will then test an other codepath.
    This is intentional in favor of unbreaking the CI.
    
    Please speak up if you have other ideas of what might be the reason
    for the failure we see.
    
    Example:
    https://integration.wikimedia.org/ci/job/quibble-vendor-mysql-php70-docker/5395/console
    
    Change-Id: Icb3492f2e41e6737bc84b7eeab4e9889c8e218df

--- tests/qunit/suites/resources/jquery/jquery.color.test.js
+++ tests/qunit/suites/resources/jquery/jquery.color.test.js
@@ -5,7 +5,7 @@
 		var done = assert.async(),
 			$canvas = $( '<div>' ).css( 'background-color', '#fff' ).appendTo( '#qunit-fixture' );
 
-		$canvas.animate( { 'background-color': '#000' }, 3 ).promise()
+		$canvas.animate( { 'background-color': '#000' }, 0 ).promise()
 			.done( function () {
 				var endColors = $.colorUtil.getRGB( $canvas.css( 'background-color' ) );
 				assert.deepEqual( endColors, [ 0, 0, 0 ], 'end state' );
