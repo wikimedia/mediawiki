0d033069a2ec303cbb2969f1252aa08e115056f2
Robert Vogel <robert.vogel@hallowelt.com>
2018-06-08 09:56:07 +0200

    Merge branch 'master' into 'master'
    Move CategoryCheck out of ServiceCenter
    
    See merge request BlueSpice/mediawiki!22
    
    (cherry picked from commit 16c10fde62abfe27c07709ff6a5cbb3b537c92c9)
    
    c99d89d5 Move CategoryCheck out of ServiceCenter

--- settings.d/050-BlueSpiceServiceCenter.php
+++ settings.d/050-BlueSpiceServiceCenter.php
@@ -1,4 +1,3 @@
 <?php
 
-require_once( "$IP/extensions/BlueSpiceServiceCenter/CategoryCheck/CategoryCheck.setup.php" );
 require_once( "$IP/extensions/BlueSpiceServiceCenter/_templates/_templates.php" );
