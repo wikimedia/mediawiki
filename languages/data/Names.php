bc9e865ab7434098a0d18e40948bfb52877a187f
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-06-25 23:42:30 +0200

    Add Manipuri/Meitei to Names.php
    Bug: T198132
    Change-Id: I43620c1f34eecda69c61ea0bb13a213e0e6a457d

--- languages/data/Names.php
+++ languages/data/Names.php
@@ -297,6 +297,7 @@ class Names {
 		'mk' => 'македонски', # Macedonian
 		'ml' => 'മലയാളം', # Malayalam
 		'mn' => 'монгол', # Halh Mongolian (Cyrillic) (ISO 639-3: khk)
+		'mni' => 'মেইতেই লোন্', # Manipuri/Meitei
 		'mo' => 'молдовеняскэ', # Moldovan, deprecated
 		'mr' => 'मराठी', # Marathi
 		'mrj' => 'кырык мары', # Hill Mari
