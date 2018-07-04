6cfdbdf60e69cce31d2f28f0a0e7e478785fe886
daniel <daniel.kinzler@wikimedia.de>
2018-07-04 17:20:31 +0200

    Add support for extra database connections in unit tests.
    This adds a way to take any connection to the wiki database and
    allow it to be used for unit tests.
    
    This is needed to test code that requires two independent DB
    connections, to the same database or to a different wiki's database.
    
    Change-Id: I0b84a0c4d174cbde830786028ee7568c4ea9fb5d

--- includes/db/CloneDatabase.php
+++ includes/db/CloneDatabase.php
@@ -82,10 +82,10 @@ class CloneDatabase {
 			# works correctly across DB engines, we need to change the pre-
 			# fix back and forth so tableName() works right.
 
-			self::changePrefix( $this->oldTablePrefix );
+			$this->db->tablePrefix( $this->oldTablePrefix );
 			$oldTableName = $this->db->tableName( $tbl, 'raw' );
 
-			self::changePrefix( $this->newTablePrefix );
+			$this->db->tablePrefix( $this->newTablePrefix );
 			$newTableName = $this->db->tableName( $tbl, 'raw' );
 
 			// Postgres: Temp tables are automatically deleted upon end of session
@@ -116,12 +116,12 @@ class CloneDatabase {
 	 */
 	public function destroy( $dropTables = false ) {
 		if ( $dropTables ) {
-			self::changePrefix( $this->newTablePrefix );
+			$this->db->tablePrefix( $this->newTablePrefix );
 			foreach ( $this->tablesToClone as $tbl ) {
 				$this->db->dropTable( $tbl );
 			}
 		}
-		self::changePrefix( $this->oldTablePrefix );
+		$this->db->tablePrefix( $this->oldTablePrefix );
 	}
 
 	/**
