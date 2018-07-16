46fc7d142d01d800c56140573fb9b0050af07a40
Brad Jorsch <bjorsch@wikimedia.org>
2018-07-16 13:13:34 -0400

    Set default wgMultiContentRevisionSchemaMigrationStage to write-both
    In progress of slowly rolling out MCR, this changes the default to
    write both schemas while still reading only the old.
    
    Bug: T198563
    Change-Id: Ibd3279058d6ec3a2ce1cc6a2b3a503813d133917

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -8912,7 +8912,7 @@ $wgCommentTableSchemaMigrationStage = MIGRATION_OLD;
  * @since 1.32
  * @var int An appropriate combination of SCHEMA_COMPAT_XXX flags.
  */
-$wgMultiContentRevisionSchemaMigrationStage = MIGRATION_OLD;
+$wgMultiContentRevisionSchemaMigrationStage = SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD;
 
 /**
  * Actor table schema migration stage.
