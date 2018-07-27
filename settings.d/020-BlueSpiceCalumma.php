7a7151e4c10618cb9a85b1ba5bc6019eff0a634b
dvogel <daniel.vogel@hallowelt.com>
2018-07-27 13:19:41 +0200

    020-BlueSpiceCalumma: Fix logo path. Skip chameleon skin in user settings.

--- settings.d/020-BlueSpiceCalumma.php
+++ settings.d/020-BlueSpiceCalumma.php
@@ -1,9 +1,10 @@
 <?php
 wfLoadSkin( 'BlueSpiceCalumma' );
-$wgDefaultSkin = "bluespicecalumma";
+$GLOBALS['wgSkipSkins'] = [ 'chameleon' ];
+$GLOBALS['wgDefaultSkin'] = "bluespicecalumma";
 $GLOBALS['egChameleonLayoutFile'] = "$IP/skins/BlueSpiceCalumma/layouts/calumma-master.xml";
 
-global $wgLogo;
+global $wgLogo, $wgResourceBasePath;
 if( $wgLogo == "$wgResourceBasePath/resources/assets/wiki.png" ){
-    $wgLogo = "$IP/extensions/BlueSpiceFoundation/resources/bluespice/images/bs-logo.png";
+    $wgLogo = "$wgResourceBasePath/extensions/BlueSpiceFoundation/resources/bluespice/images/bs-logo.png";
 }
