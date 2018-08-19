9f86db20286c2c39ae5f134a31ad2927441340d0
Timo Tijhof <krinklemail@gmail.com>
2018-08-19 22:23:51 +0100

    resources: Move non-jquery files from /resources/lib/jquery to /resources/lib
    Per discussion on T193826, these are not part of jquery (the library, or the
    module), and should not be in the same subdirectory.
    
    To follow the new convention that all entries directly in /resources/lib
    should correspond to single library only (either as file, or as directory),
    move them one directory up.
    
    Bug: T193826
    Change-Id: I24c05ec5fc5f0a2d54d501a4a022d829675bf850

--- resources/Resources.php
+++ resources/Resources.php
@@ -174,7 +174,7 @@ return [
 		'targets' => [ 'mobile', 'desktop' ],
 	],
 	'jquery.async' => [
-		'scripts' => 'resources/lib/jquery/jquery.async.js',
+		'scripts' => 'resources/lib/jquery.async.js',
 	],
 	'jquery.byteLength' => [
 		'scripts' => 'resources/src/jquery/jquery.byteLength.js',
@@ -221,14 +221,14 @@ return [
 		'dependencies' => 'mediawiki.jqueryMsg',
 	],
 	'jquery.cookie' => [
-		'scripts' => 'resources/lib/jquery/jquery.cookie.js',
+		'scripts' => 'resources/lib/jquery.cookie.js',
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 	'jquery.form' => [
-		'scripts' => 'resources/lib/jquery/jquery.form.js',
+		'scripts' => 'resources/lib/jquery.form.js',
 	],
 	'jquery.fullscreen' => [
-		'scripts' => 'resources/lib/jquery/jquery.fullscreen.js',
+		'scripts' => 'resources/lib/jquery.fullscreen.js',
 	],
 	'jquery.getAttrs' => [
 		'targets' => [ 'desktop', 'mobile' ],
@@ -248,7 +248,7 @@ return [
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 	'jquery.hoverIntent' => [
-		'scripts' => 'resources/lib/jquery/jquery.hoverIntent.js',
+		'scripts' => 'resources/lib/jquery.hoverIntent.js',
 	],
 	'jquery.i18n' => [
 		'scripts' => [
@@ -295,7 +295,7 @@ return [
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 	'jquery.mockjax' => [
-		'scripts' => 'resources/lib/jquery/jquery.mockjax.js',
+		'scripts' => 'resources/lib/jquery.mockjax.js',
 	],
 	'jquery.mw-jump' => [
 		'scripts' => 'resources/src/jquery/jquery.mw-jump.js',
@@ -313,7 +313,7 @@ return [
 	],
 	'jquery.jStorage' => [
 		'deprecated' => 'Please use "mediawiki.storage" instead.',
-		'scripts' => 'resources/lib/jquery/jquery.jStorage.js',
+		'scripts' => 'resources/lib/jquery.jStorage.js',
 	],
 	'jquery.suggestions' => [
 		'targets' => [ 'desktop', 'mobile' ],
@@ -340,11 +340,11 @@ return [
 		'targets' => [ 'mobile', 'desktop' ],
 	],
 	'jquery.throttle-debounce' => [
-		'scripts' => 'resources/lib/jquery/jquery.ba-throttle-debounce.js',
+		'scripts' => 'resources/lib/jquery.ba-throttle-debounce.js',
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 	'jquery.xmldom' => [
-		'scripts' => 'resources/lib/jquery/jquery.xmldom.js',
+		'scripts' => 'resources/lib/jquery.xmldom.js',
 	],
 
 	/* jQuery Tipsy */
