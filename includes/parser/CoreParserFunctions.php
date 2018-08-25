2912873a61fd2f00fed4b2b8a308a026d4b06d7d
Kunal Mehta <legoktm@member.fsf.org>
2018-08-25 01:20:43 -0700

    parser: Simplify code for {{#int:}} if the message doesn't exist
    Ever since 184658eb32f6, the output of a non-existing message will be
    HTML safe, regardless of output format, so we can treat non-existing messages
    exactly the same as messages that do exist.
    
    The pre-existing "int keyword - non-existing message" parser test verifies
    that no change in output has ocurred in this patch.
    
    Change-Id: I0e32be14f1b420d7f222ac3c76e1cc266f912b69

--- includes/parser/CoreParserFunctions.php
+++ includes/parser/CoreParserFunctions.php
@@ -98,11 +98,6 @@ class CoreParserFunctions {
 			$args = array_slice( func_get_args(), 2 );
 			$message = wfMessage( $part1, $args )
 				->inLanguage( $parser->getOptions()->getUserLangObj() );
-			if ( !$message->exists() ) {
-				// When message does not exists, the message name is surrounded by angle
-				// and can result in a tag, therefore escape the angles
-				return $message->escaped();
-			}
 			return [ $message->plain(), 'noparse' => false ];
 		} else {
 			return [ 'found' => false ];
