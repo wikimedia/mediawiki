wikimedia/mediawiki

commit dec800968eb618c1f8632df7be49783fc82078d2

resourceloader: Combine base modules and page modules requests
This commit implements step 4 and step 5 of the plan outlined at T192623.

Before this task began, the typical JavaScript execution flow was:

* HTML triggers request for startup module (js req 1).
* Startup module contains registry, site config, and triggers
  a request for the base modules (js req 2).
* After the base modules arrive (which define jQuery and mw.loader),
  the startup module invokes a callback that processes RLQ,
  which is what will request modules for this page (js req 3).

In past weeks, we have:

* Made mediawiki.js independent of jQuery.
* Spun off 'mediawiki.base' from mediawiki.js – for everything
  that wasn't needed for defining `mw.loader`.
* Moved mediawiki.js from the base module request to being embedded
  as part of startup.js.

The concept of dependencies is native to ResourceLoader, and thanks to the
use of closures in mw.loader.implement() responses, we can download any
number of interdependant modules in a single request (or parallel requests).
Then, when a response arrives, mw.loader takes care to pause or resume
execution as-needed. It is normal for ResourceLoader to batch several modules
together, including their dependencies.

As such, we can eliminate one of the two roundtrips required before a
page can request modules. Specifically, we can eliminate "js req 2" (above),
by making the two remaining base modules ("jquery" and "mediawiki.base") an
implied dependency for all other modules, which ResourceLoader will naturally
fetch and execute in the right order as part of the batch request.

Bug: T192623
Change-Id: I17cd13dffebd6ae476044d8d038dc3974a1fa176

resources/src/startup/startup.js
@@ -80,7 +80,7 @@ window.isCompatible = function ( str ) {
 };
 
 ( function () {
-	var NORLQ, script;
+	var NORLQ;
 	// Handle Grade C
 	if ( !isCompatible() ) {
 		// Undo speculative Grade A <html> class. See ResourceLoaderClientHtml::getDocumentAttributes().
@@ -119,19 +119,20 @@ window.isCompatible = function ( str ) {
 		// Process callbacks for Grade A
 		// Must be after registrations and mw.config.set, which mw.loader depends on.
 		var queue = window.RLQ;
-		window.RLQ = {
-			push: function ( fn ) {
-				if ( typeof fn === 'function' ) {
-					fn();
-				} else {
-					// This callback has a requirement.
-					mw.loader.using( fn[ 0 ], fn[ 1 ] );
-				}
+		// Redefine push(), but keep type as array for storing callbacks that require modules.
+		window.RLQ = [];
+		/* global RLQ */
+		RLQ.push = function ( fn ) {
+			if ( typeof fn === 'function' ) {
+				fn();
+			} else {
+				// This callback requires a module, handled in mediawiki.base.
+				RLQ[ RLQ.length ] = fn;
 			}
 		};
 		while ( queue && queue[ 0 ] ) {
 			// Re-use our push()
-			window.RLQ.push( queue.shift() );
+			RLQ.push( queue.shift() );
 		}
 
 		// Clear and disable the Grade C queue
@@ -147,14 +148,5 @@ window.isCompatible = function ( str ) {
 	// This embeds mediawiki.js, which defines 'mw' and 'mw.loader'.
 	$CODE.defineLoader();
 
-	script = document.createElement( 'script' );
-	script.src = $VARS.baseModulesUri;
-	script.onload = function () {
-		// Clean up
-		script.onload = null;
-		script = null;
-		// Callback
-		startUp();
-	};
-	document.head.appendChild( script );
+	mw.requestIdleCallback( startUp );
 }() );

includes/resourceloader/ResourceLoader.php
@@ -1484,7 +1484,7 @@ MESSAGE;
 	}
 
 	/**
-	 * Wraps JavaScript code to run after startup and base modules.
+	 * Wraps JavaScript code to run after the startup module.
 	 *
 	 * @param string $script JavaScript code
 	 * @return string JavaScript code

includes/resourceloader/ResourceLoaderStartUpModule.php
@@ -320,17 +320,6 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	}
 
 	/**
-	 * @param ResourceLoaderContext $context
-	 * @return array
-	 */
-	public function getPreloadLinks( ResourceLoaderContext $context ) {
-		$url = $this->getBaseModulesUrl( $context );
-		return [
-			$url => [ 'as' => 'script' ]
-		];
-	}
-
-	/**
 	 * Internal modules used by ResourceLoader that cannot be depended on.
 	 *
 	 * These module(s) should have isRaw() return true, and are not
@@ -354,9 +343,17 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	}
 
 	/**
+	 * @private For internal use by SpecialJavaScriptTest
+	 * @since 1.32
+	 * @return array
+	 */
+	public function getBaseModulesInternal() {
+		return $this->getBaseModules();
+	}
+
+	/**
 	 * Base modules implicitly available to all modules.
 	 *
-	 * @since 1.32
 	 * @return array
 	 */
 	private function getBaseModules() {
@@ -371,25 +368,6 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 	}
 
 	/**
-	 * Get the load URL of the startup modules.
-	 *
-	 * This is a helper for getScript().
-	 *
-	 * @param ResourceLoaderContext $context
-	 * @return string
-	 */
-	private function getBaseModulesUrl( ResourceLoaderContext $context ) {
-		$rl = $context->getResourceLoader();
-		$derivative = new DerivativeResourceLoaderContext( $context );
-		$derivative->setModules( $this->getBaseModules() );
-		$derivative->setOnly( 'scripts' );
-		// Must setModules() before makeVersionQuery()
-		$derivative->setVersion( $rl->makeVersionQuery( $derivative ) );
-
-		return $rl->createLoaderURL( 'local', $derivative );
-	}
-
-	/**
 	 * @param ResourceLoaderContext $context
 	 * @return string JavaScript code
 	 */
@@ -399,35 +377,43 @@ class ResourceLoaderStartUpModule extends ResourceLoaderModule {
 			return '/* Requires only=script */';
 		}
 
-		$out = file_get_contents( "$IP/resources/src/startup/startup.js" );
+		$startupCode = file_get_contents( "$IP/resources/src/startup/startup.js" );
 
-		// Keep in sync with maintenance/jsduck/eg-iframe.html and,
-		// keep in sync with 'fileHashes' in StartUpModule::getDefinitionSummary().
+		// The files read here MUST be kept in sync with maintenance/jsduck/eg-iframe.html,
+		// and MUST be considered by 'fileHashes' in StartUpModule::getDefinitionSummary().
 		$mwLoaderCode = file_get_contents( "$IP/resources/src/startup/mediawiki.js" ) .
 			file_get_contents( "$IP/resources/src/startup/mediawiki.requestIdleCallback.js" );
 		if ( $context->getDebug() ) {
 			$mwLoaderCode .= file_get_contents( "$IP/resources/src/startup/mediawiki.log.js" );
 		}
 
-		$pairs = array_map( function ( $value ) {
+		$mapToJson = function ( $value ) {
 			$value = FormatJson::encode( $value, ResourceLoader::inDebugMode(), FormatJson::ALL_OK );
 			// Fix indentation
 			$value = str_replace( "\n", "\n\t", $value );
 			return $value;
-		}, [
+		};
+
+		// Perform replacements for mediawiki.js
+		$mwLoaderCode = strtr( $mwLoaderCode, [
+			'$VARS.baseModules' => $mapToJson( $this->getBaseModules() ),
+		] );
+
+		// Perform replacements for startup.js
+		$pairs = array_map( $mapToJson, [
 			'$VARS.wgLegacyJavaScriptGlobals' => $this->getConfig()->get( 'LegacyJavaScriptGlobals' ),
 			'$VARS.configuration' => $this->getConfigSettings( $context ),
-			// This url may be preloaded. See getPreloadLinks().
-			'$VARS.baseModulesUri' => $this->getBaseModulesUrl( $context ),
 		] );
+		// Raw JavaScript code (not for JSON)
 		$pairs['$CODE.registrations();'] = str_replace(
 			"\n",
 			"\n\t",
 			trim( $this->getModuleRegistrations( $context ) )
 		);
 		$pairs['$CODE.defineLoader();'] = $mwLoaderCode;
+		$startupCode = strtr( $startupCode, $pairs );
 
-		return strtr( $out, $pairs );
+		return $startupCode;
 	}
 
 	/**

includes/specials/SpecialJavaScriptTest.php
@@ -103,65 +103,56 @@ class SpecialJavaScriptTest extends SpecialPage {
 		$query['only'] = 'scripts';
 		$startupContext = new ResourceLoaderContext( $rl, new FauxRequest( $query ) );
 
-		$query['raw'] = true;
-
 		$modules = $rl->getTestModuleNames( 'qunit' );
 
 		// Disable autostart because we load modules asynchronously. By default, QUnit would start
 		// at domready when there are no tests loaded and also fire 'QUnit.done' which then instructs
-		// Karma to end the run before the tests even started.
+		// Karma to exit the browser process before the tests even finished loading.
 		$qunitConfig = 'QUnit.config.autostart = false;'
 			. 'if (window.__karma__) {'
 			// karma-qunit's use of autostart=false and QUnit.start conflicts with ours.
-			// Hack around this by replacing 'karma.loaded' with a no-op and call it ourselves later.
-			// See <https://github.com/karma-runner/karma-qunit/issues/27>.
+			// Hack around this by replacing 'karma.loaded' with a no-op and perfom its duty of calling
+			// `__karma__.start()` ourselves. See <https://github.com/karma-runner/karma-qunit/issues/27>.
 			. 'window.__karma__.loaded = function () {};'
 			. '}';
 
 		// The below is essentially a pure-javascript version of OutputPage::headElement().
-		$startup = $rl->makeModuleResponse( $startupContext, [
+		$code = $rl->makeModuleResponse( $startupContext, [
 			'startup' => $rl->getModule( 'startup' ),
 		] );
-		// Embed page-specific mw.config variables.
-		// The current Special page shouldn't be relevant to tests, but various modules (which
-		// are loaded before the test suites), reference mw.config while initialising.
-		$code = ResourceLoader::makeConfigSetScript( $out->getJSVars() );
-		// Embed private modules as they're not allowed to be loaded dynamically
-		$code .= $rl->makeModuleResponse( $embedContext, [
-			'user.options' => $rl->getModule( 'user.options' ),
-			'user.tokens' => $rl->getModule( 'user.tokens' ),
-		] );
-		// Catch exceptions (such as "dependency missing" or "unknown module") so that we
-		// always start QUnit. Re-throw so that they are caught and reported as global exceptions
-		// by QUnit and Karma.
-		$modules = Xml::encodeJsVar( $modules );
-		$code .= <<<CODE
-(function () {
+		// The following has to be deferred via RLQ because the startup module is asynchronous.
+		$code .= ResourceLoader::makeLoaderConditionalScript(
+			// Embed page-specific mw.config variables.
+			// The current Special page shouldn't be relevant to tests, but various modules (which
+			// are loaded before the test suites), reference mw.config while initialising.
+			ResourceLoader::makeConfigSetScript( $out->getJSVars() )
+			// Embed private modules as they're not allowed to be loaded dynamically
+			. $rl->makeModuleResponse( $embedContext, [
+				'user.options' => $rl->getModule( 'user.options' ),
+				'user.tokens' => $rl->getModule( 'user.tokens' ),
+			] )
+			// Load all the test suites
+			. Xml::encodeJsCall( 'mw.loader.load', [ $modules ] )
+		);
+		$encModules = Xml::encodeJsVar( $modules );
+		$code .= ResourceLoader::makeInlineCodeWithModule( 'mediawiki.base', <<<JAVASCRIPT
 	var start = window.__karma__ ? window.__karma__.start : QUnit.start;
-	try {
-		mw.loader.using( $modules )
-			.always( function () {
-				start();
-			} )
-			.fail( function ( e ) {
-				setTimeout( function () {
-					throw e;
-				} );
-			} );
-	} catch ( e ) {
-		start();
-		throw e;
-	}
-}());
-CODE;
+	mw.loader.using( $encModules ).always( start );
+	mw.trackSubscribe( 'resourceloader.exception', function ( topic, err ) {
+		// Things like "dependency missing" or "unknown module".
+		// Re-throw so that they are reported as global exceptions by QUnit and Karma.
+		setTimeout( function () {
+			throw e;
+		} );
+	} );
+JAVASCRIPT
+	);
 
 		header( 'Content-Type: text/javascript; charset=utf-8' );
 		header( 'Cache-Control: private, no-cache, must-revalidate' );
 		header( 'Pragma: no-cache' );
 		echo $qunitConfig;
-		echo $startup;
-		// The following has to be deferred via RLQ because the startup module is asynchronous.
-		echo ResourceLoader::makeLoaderConditionalScript( $code );
+		echo $code;
 	}
 
 	private function plainQUnit() {

maintenance/jsduck/eg-iframe.html
@@ -35,7 +35,9 @@
 	</script>
 	<script>
 		// Mock startup.js
-		var mwNow = Date.now;
+		window.$VARS = {
+			baseModules: []
+		};
 
 		function startUp() {
 			mw.config = new mw.Map();

resources/Resources.php
@@ -143,7 +143,6 @@ return [
 			'resources/lib/jquery/jquery.js',
 			'resources/lib/jquery/jquery.migrate.js',
 		],
-		'raw' => true,
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 
@@ -834,11 +833,14 @@ return [
 	/* MediaWiki */
 
 	'mediawiki.base' => [
-		// Keep in sync with maintenance/jsduck/eg-iframe.html
 		'scripts' => [
+			// This MUST be kept in sync with maintenance/jsduck/eg-iframe.html
 			'resources/src/mediawiki.base/mediawiki.errorLogger.js',
 			'resources/src/mediawiki.base/mediawiki.base.js',
 		],
+		// - These dependencies MUST NOT also have dependencies (would cause recursion).
+		// - These dependencies MUST also be returned from StartUpModule::getBaseModules().
+		'dependencies' => 'jquery',
 		'targets' => [ 'desktop', 'mobile' ],
 	],
 	'mediawiki.apihelp' => [

resources/lib/jquery/jquery.migrate.js
@@ -3,6 +3,9 @@
  * Copyright jQuery Foundation and other contributors
  *
  * Patched for MediaWiki:
+ * - Qualify the global lookup for 'jQuery' as 'window.jQuery',
+ *   because within mw.loader.implement() for 'jquery', the closure
+ *   specifies '$' and 'jQuery', which are undefined.
  * - Add mw.track instrumentation for statistics.
  * - Disable jQuery.migrateTrace by default. They are slow and
  *   redundant given console.warn() already provides a trace.
@@ -20,7 +23,8 @@
 	} else {
 
 		// Browser globals
-		factory( jQuery, window );
+		// PATCH: Qualify jQuery lookup as window.jQuery. --Krinkle
+		factory( window.jQuery, window );
 	}
 } )( function( jQuery, window ) {
 "use strict";

resources/src/mediawiki.base/mediawiki.base.js
@@ -22,7 +22,8 @@
 		mwLoaderTrack = mw.track,
 		trackCallbacks = $.Callbacks( 'memory' ),
 		trackHandlers = [],
-		hasOwn = Object.prototype.hasOwnProperty;
+		hasOwn = Object.prototype.hasOwnProperty,
+		queue;
 
 	/**
 	 * Object constructor for messages.
@@ -642,4 +643,17 @@
 	// Alias $j to jQuery for backwards compatibility
 	// @deprecated since 1.23 Use $ or jQuery instead
 	mw.log.deprecate( window, '$j', $, 'Use $ or jQuery instead.' );
+
+	// Process callbacks for Grade A that require modules.
+	// Plain ones were already processed by startup.js.
+	queue = window.RLQ;
+	// Redefine publicly to capture any late arrivals
+	window.RLQ = {
+		push: function ( entry ) {
+			mw.loader.using( entry[ 0 ], entry[ 1 ] );
+		}
+	};
+	while ( queue[ 0 ] ) {
+		window.RLQ.push( queue.shift() );
+	}
 }() );

commit 5a9d2231c80d244c019f238587d0b5ae4f3ae7a4

resourceloader: Don't defer discovery of critical request via rIC()
== Overview

This reverses a small part of the big dec800968eb commit. That
commit eliminated the 2nd of 3 JS requests required on a page.

The first JS request is now modules=startup with the registry and
mw.loader. The main JS request for modules (now 2nd) is triggered
from RLQ, which fires when startup.js calls 'startUp()'.

== Regression

In commit dec800968eb, I thought I was being smart by letting the
browser "take a breath" at the end of startup.js before calling
'startUp()'. I now believe that this artifical is a mistake.

I thought it was a good idea because, before dec800968eb startup.js
made an http request (for jquery). During that network request the
main thread was free to do rendering. Then, when jquery came back,
we called startUp() to trigger our main request for modules (3rd,
now 2nd). While this request no longer exists, the idea was to keep
that thread yield at the end to startup.js.

I now think it was a mistake because requestIdleCallback is not
a "small" thread yield. Rather, I now find in production that in
the majority of cases (all mobile, and desktop first views) the
rIC is not called until *after* the page load event fires.

This means that during the entire page rendering time, the network
and javascript thread were sitting idle. Prioritising rendering and
making the page load complete 0.5s faster is great. But,
underutilising the network and delaying time to JS interation by 1-2
seconds, is bad.

== Cost

The startUp() function is quite light. All it does is a bunch of
pure JavaScript processing (no DOM), and then creates 1 script tag

On desktop (MacBook) with 6x CPU slowdown and mobile emulation
(enwiki/Obama) it clocks startUp() at 35ms.

On a real mobile device (Nexus 5, Android 4.4, the oldest currently
supported Android version/device I could find on BrowserStack) it
clocks startUp() on enwiki/Obama at 44ms.

resources/src/startup/startup.js
@@ -145,5 +145,5 @@ window.isCompatible = function ( str ) {
 	// This embeds mediawiki.js, which defines 'mw' and 'mw.loader'.
 	$CODE.defineLoader();
 
-	mw.requestIdleCallback( startUp );
+	startUp();
 }() );