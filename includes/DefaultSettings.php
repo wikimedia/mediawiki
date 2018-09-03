5eda979890704fa8974f3ee9df6a67175a0f78e7
Kunal Mehta <legoktm@member.fsf.org>
2018-09-03 01:29:59 -0700

    Bump 1.27.5
    Change-Id: Ie3a700f85b2dda76b37b2cec28f09e9804531a8a

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -75,7 +75,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.27.4';
+$wgVersion = '1.27.5';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
