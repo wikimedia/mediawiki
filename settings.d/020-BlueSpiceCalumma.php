c522b7682362113c8e8ad248792f0774ddccd875
dvogel <daniel.vogel@hallowelt.com>
2018-10-11 14:25:53 +0200

    Update BlueSpiceCalumma settings - new footer icon size

--- settings.d/020-BlueSpiceCalumma.php
+++ settings.d/020-BlueSpiceCalumma.php
@@ -13,8 +13,8 @@ if( $wgLogo == "$wgResourceBasePath/resources/assets/wiki.png" ){
  */
 $GLOBALS['wgFooterIcons']['poweredby']['mediawiki']['src'] = $wgScriptPath . "/skins/BlueSpiceCalumma/resources/images/common/footer/MediaWiki.png";
 $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] += [
-	"height" => "32",
-	"width" => "88"
+	'height' => '27',
+	'width' => '149'
 ];
 
 /*
@@ -25,16 +25,14 @@ $GLOBALS['wgFooterIcons']['poweredby'] += [
 	'bluespice' => [
 		"src" => $wgScriptPath . "/skins/BlueSpiceCalumma/resources/images/common/footer/BlueSpice.png",
 		"url" => "http://bluespice.com",
-		"alt" => "Powered by BlueSpice",
-		"height" => "32",
-		"width" => "150"
+		'height' => '27',
+		'width' => '149'
 	],
 	'semanticmediawiki' => [
 		'src' => $wgScriptPath . "/skins/BlueSpiceCalumma/resources/images/common/footer/SemanticMediaWiki.png",
 		'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
 		'alt' => 'Powered by Semantic MediaWiki',
-		"height" => "32",
-		"width" => "150"
+		'height' => '27',
+		'width' => '149'
 	]
 ];
-
