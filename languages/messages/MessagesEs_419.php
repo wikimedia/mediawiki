7cb057e034e80922542c271c07801726fc060493
Elliott Eggleston <ejegg@ejegg.com>
2018-10-19 14:46:19 -0500

    Add an actual messages file for es-419
    This seems to be necessary for the uselang and language parameters
    to be respected.
    
    Bug: T207507
    Change-Id: I155bb73e8a2f6dd93a1a9b2d4a5065b38e7c7779

---
+++ languages/messages/MessagesEs_419.php
@@ -0,0 +1,14 @@
+<?php
+/** Spanish (Latin America) (español de América Latina)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Ejegg
+ */
+
+# Inherit everything for now
+$fallback = 'es';
+
