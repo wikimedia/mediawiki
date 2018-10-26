cb7e0ea507c55b49fd22f734fbfe048fe87d5557
Timo Tijhof <krinklemail@gmail.com>
2018-10-26 14:33:26 -0700

    resoureloader: Restore support for plain callbacks in mediawiki.base's RLQ
    Follows-up dec800968e, which moved the processing of callbacks that require
    'modules' from startup.js to mediawiki.base.js.
    
    In doing so, it made an incorrect assumption. It assumed that the simple
    signature of RLQ.push(Function) is not needed after 'mediawiki.base' loads.
    
    It is true that RLQ.push() is mostly an internal interface, and we only
    use it from within the HTML output, and that once the async pipeline has
    finished and startup.js has processed the simple callbacks, only calls with
    secondary signatures remain in the queue.
    
    But, while it is true that we don't use RLQ.push() outside the HTML, it is
    not true that the HTML will fully load and execute inline scripts before
    any of the async scripts execute. As such, the call to RLQ.push() in the
    HTML footer was sometimes being ignored because 'mediawiki.base' had already
    loaded by now.
    
    Bug: T208093
    Change-Id: I25012a2c6f41968b1b4f85614a3bc0416512d530

--- tests/qunit/suites/resources/mediawiki/mediawiki.base.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.base.test.js
@@ -126,12 +126,21 @@
 	QUnit.test( 'RLQ.push', function ( assert ) {
 		/* global RLQ */
 		var loaded = 0,
+			called = 0,
 			done = assert.async();
 		mw.loader.testCallback = function () {
 			loaded++;
 			delete mw.loader.testCallback;
 		};
-		mw.loader.implement( 'test.rlq-push', [ QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/mwLoaderTestCallback.js' ) ] );
+		mw.loader.implement( 'test.rlq-push', [
+			QUnit.fixurl( mw.config.get( 'wgScriptPath' ) + '/tests/qunit/data/mwLoaderTestCallback.js' )
+		] );
+
+		// Regression test for T208093
+		RLQ.push( function () {
+			called++;
+		} );
+		assert.strictEqual( called, 1, 'Invoke plain callbacks' );
 
 		RLQ.push( [ 'test.rlq-push', function () {
 			assert.strictEqual( loaded, 1, 'Load the required module' );
