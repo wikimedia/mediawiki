0fb6cbf682af4e195e884637d77e2d537de9d8e9
Brian Wolff <bawolff+wn@gmail.com>
2018-11-21 16:15:28 +0000

    SECURITY: rate-limit and prevent blocked users from changing email
    This is to counter spam where people use Special:ChangeEmail to
    spam people with the confirmation email and using the username
    to promote their thing
    
    Bug: T209794
    Change-Id: I8b2bd0f60c66f44c91dc78e3512a73e4237df2f3

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5690,6 +5690,10 @@ $wgRateLimits = [
 		'newbie' => [ 5, 86400 ],
 		'user' => [ 20, 86400 ],
 	],
+	'changeemail' => [
+		'ip-all' => [ 10, 3600 ],
+		'user' => [ 4, 86400 ]
+	],
 	// Purging pages
 	'purge' => [
 		'ip' => [ 30, 60 ],
