f12db3804882272794b4dafc72db1c5d787375bf
Tim Starling <tstarling@wikimedia.org>
2019-01-15 14:15:18 +1100

    Better detection for old MS Office files
    * Introduce MSCompoundFileReader, which reads the CFB directory and
      detects the file type from well-known names in the root directory
    * Do not detect a ZIP file if the EOCDR is not at the end. Other
      containers, especially CFB files, may contain ZIP files embedded
      within them in the last 64KB, but this is not a security concern
      unless the EOCDR is exactly at the end of the file.
    
    Bug: T40432
    Change-Id: Id5b1a258ccf3c3c8951e32f6b7a5b1bafe941082

---
+++ tests/phpunit/includes/libs/mime/MSCompoundFileReaderTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright 2019 Wikimedia Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * @group Media
+ * @covers MSCompoundFileReader
+ */
+class MSCompoundFileReaderTest extends PHPUnit\Framework\TestCase {
+	public static function provideValid() {
+		return [
+			[ 'calc.xls', 'application/vnd.ms-excel' ],
+			[ 'excel2016-compat97.xls', 'application/vnd.ms-excel' ],
+			[ 'gnumeric.xls', 'application/vnd.ms-excel' ],
+			[ 'impress.ppt', 'application/vnd.ms-powerpoint' ],
+			[ 'powerpoint2016-compat97.ppt', 'application/vnd.ms-powerpoint' ],
+			[ 'word2016-compat97.doc', 'application/msword' ],
+			[ 'writer.doc', 'application/msword' ],
+		];
+	}
+
+	/** @dataProvider provideValid */
+	public function testReadFile( $fileName, $expectedMime ) {
+		global $IP;
+
+		$info = MSCompoundFileReader::readFile( "$IP/tests/phpunit/data/MSCompoundFileReader/$fileName" );
+		$this->assertTrue( $info['valid'] );
+		$this->assertSame( $expectedMime, $info['mime'] );
+	}
+
+	public static function provideInvalid() {
+		return [
+			[ 'dir-beyond-end.xls', 'ERROR_READ_PAST_END' ],
+			[ 'fat-loop.xls', 'ERROR_INVALID_FORMAT' ],
+			[ 'invalid-signature.xls', 'ERROR_INVALID_SIGNATURE' ],
+		];
+	}
+
+	/** @dataProvider provideInvalid */
+	public function testReadFileInvalid( $fileName, $expectedError ) {
+		global $IP;
+
+		$info = MSCompoundFileReader::readFile( "$IP/tests/phpunit/data/MSCompoundFileReader/$fileName" );
+		$this->assertFalse( $info['valid'] );
+		$this->assertSame( constant( MSCompoundFileReader::class . '::' . $expectedError ),
+			$info['errorCode'] );
+	}
+}
