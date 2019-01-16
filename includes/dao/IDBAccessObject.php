324f03ede5eedd428d0f81a9e4d75c37d2a0be5f
Derick Alangi <alangiderick@gmail.com>
2019-01-16 14:29:53 +0100

    dao: Add a type hint to getConnection() and some extra newlines
    Change-Id: I57a060394403a6b7335fda68edaeabc02888e14d

--- includes/dao/IDBAccessObject.php
+++ includes/dao/IDBAccessObject.php
@@ -56,10 +56,13 @@ interface IDBAccessObject {
 	/** Constants for object loading bitfield flags (higher => higher QoS) */
 	/** @var int Read from a replica DB/non-quorum */
 	const READ_NORMAL = 0;
+
 	/** @var int Read from the master/quorum */
 	const READ_LATEST = 1;
+
 	/* @var int Read from the master/quorum and lock out other writers */
 	const READ_LOCKING = self::READ_LATEST | 2; // READ_LATEST (1) and "LOCK IN SHARE MODE" (2)
+
 	/** @var int Read from the master/quorum and lock out other writers and locking readers */
 	const READ_EXCLUSIVE = self::READ_LOCKING | 4; // READ_LOCKING (3) and "FOR UPDATE" (4)
 
