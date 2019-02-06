b0a75622d3872e6cde6020a1b0f8775a19be3b23
Umherirrender <umherirrender_de.wp@web.de>
2019-02-06 22:22:01 +0100

    Move upload stash related exceptions to own files
    Change-Id: I23e81af2f1bf73f5cd78a01bcc2fcebb4c4ea038

---
+++ includes/upload/exception/UploadStashException.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Implements UploadStashException
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * @ingroup Upload
+ */
+class UploadStashException extends MWException implements ILocalizedException {
+	/** @var string|array|MessageSpecifier */
+	protected $messageSpec;
+
+	/**
+	 * @param string|array|MessageSpecifier $messageSpec See Message::newFromSpecifier
+	 * @param int $code Exception code
+	 * @param Exception|Throwable|null $previous The previous exception used for the exception
+	 *  chaining.
+	 */
+	public function __construct( $messageSpec, $code = 0, $previous = null ) {
+		$this->messageSpec = $messageSpec;
+
+		$msg = $this->getMessageObject()->text();
+		$msg = preg_replace( '!</?(var|kbd|samp|code)>!', '"', $msg );
+		$msg = Sanitizer::stripAllTags( $msg );
+		parent::__construct( $msg, $code, $previous );
+	}
+
+	public function getMessageObject() {
+		return Message::newFromSpecifier( $this->messageSpec );
+	}
+}
