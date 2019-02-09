7a8cace5fd7cba47c08c855c8a56f5148a67340a
Roan Kattouw <roan.kattouw@gmail.com>
2019-02-08 16:53:48 -0800

    mediawiki.language: Deprecate mw.language.specialCharacters
    Use require( 'mediawiki.language.specialCharacters' ) instead.
    
    Remove mw.language.setSpecialCharacters(), because it was only used by
    the specialCharacters module. There already was no getter.
    
    Depends-On: I9cf525a1e1aadc12d72284d08b98882b0ab95f5e
    Depends-On: I74e706a6603bd8fa899193de91e49f246608c4c1
    Change-Id: I02e0e84b79309caa314954925cc9c0c2874b9bde

--- resources/src/mediawiki.language/mediawiki.language.specialCharacters.js
+++ resources/src/mediawiki.language/mediawiki.language.specialCharacters.js
@@ -1,5 +1,9 @@
 ( function () {
 	var specialCharacters = require( './specialcharacters.json' );
-	mw.language.setSpecialCharacters( specialCharacters );
+	// Deprecated since 1.33
+	mw.log.deprecate( mw.language, 'specialCharacters', specialCharacters,
+		'Use require( \'mediawiki.language.specialCharacters\' ) instead',
+		'mw.language.specialCharacters'
+	);
 	module.exports = specialCharacters;
 }() );
