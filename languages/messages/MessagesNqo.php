04039b48c874063f4729a7f980bb8549c12caf00
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2019-03-04 14:52:08 +0200

    Add MessagesNqo.php
    The export from translatewiki was not done yet,
    but there is good localization activity,
    and having this file will allow correct RTL layout.
    
    Bug: T152908
    Change-Id: I8cfc718af5e96ce9d902d1bf5400a3bb2971acde

---
+++ languages/messages/MessagesNqo.php
@@ -0,0 +1,24 @@
+<?php
+/** N'Ko (ߒߞߏ)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$rtl = true;
+
+$digitTransformTable = [
+	'0' => '߀', # U+07C0
+	'1' => '߁', # U+07C1
+	'2' => '߂', # U+07C2
+	'3' => '߃', # U+07C3
+	'4' => '߄', # U+07C4
+	'5' => '߅', # U+07C5
+	'6' => '߆', # U+07C6
+	'7' => '߇', # U+07C7
+	'8' => '߈', # U+07C8
+	'9' => '߉', # U+07C9
+];
