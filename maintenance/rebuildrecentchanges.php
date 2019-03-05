72207a0c696d574c820b374d025e1c06825f111d
Jack Phoenix <ashley@uncyclomedia.co>
2019-03-06 01:13:59 +0200

    Fix a rather fatal typo in rebuildrecentchanges.php
    The JOIN condition was being ignored because there is no table called "user_group" in MediaWiki core.
    Thus if and when using $wgSharedDB, the query would end up listing *all* registered users from the shared user table.
    And even without $wgSharedDB, running rebuildrecentchanges.php would result in everyone's edits being marked as bot edits (recentchanges.rc_bot = 1) and thus hidden from the Special:RecentChanges page.
    
    Thanks to Lcawte for reporting this bug.
    
    Follow-up to 27c61fb1e94da9114314468fd00bcf129ec064b6
    
    Change-Id: I18d658b67c50f2200341f732783c2e7524dd27f1

--- maintenance/rebuildrecentchanges.php
+++ maintenance/rebuildrecentchanges.php
@@ -372,7 +372,7 @@ class RebuildRecentchanges extends Maintenance {
 				[ 'ug_group' => $botgroups ],
 				__METHOD__,
 				[ 'DISTINCT' ],
-				[ 'user_group' => [ 'JOIN', 'user_id = ug_user' ] ] + $userQuery['joins']
+				[ 'user_groups' => [ 'JOIN', 'user_id = ug_user' ] ] + $userQuery['joins']
 			);
 
 			$botusers = [];
