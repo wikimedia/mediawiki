cc69954a9df31b3c31d72074ec84c0a0d9464e29
Jack Phoenix <ashley@uncyclomedia.co>
2019-03-06 11:07:26 +0200

    user_group, the nonexistent table that keeps on giving
    Follow-up to 27c61fb1e94da9114314468fd00bcf129ec064b6.
    
    Bug: T199474
    Change-Id: Ie8e054f5898209c51538669149e966bee7754f1e

--- maintenance/rebuildrecentchanges.php
+++ maintenance/rebuildrecentchanges.php
@@ -426,7 +426,7 @@ class RebuildRecentchanges extends Maintenance {
 				[ 'ug_group' => $autopatrolgroups ],
 				__METHOD__,
 				[ 'DISTINCT' ],
-				[ 'user_group' => [ 'JOIN', 'user_id = ug_user' ] ] + $userQuery['joins']
+				[ 'user_groups' => [ 'JOIN', 'user_id = ug_user' ] ] + $userQuery['joins']
 			);
 
 			foreach ( $res as $obj ) {
