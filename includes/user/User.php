63cfe7cf4bf4cef3016b95b8df7d713cbef6b3ee
Gergő Tisza <tgr.huwiki@gmail.com>
2019-03-18 14:50:48 -0700

    Replace $wgUser with RequestContext::getUser in User::getBlockedStatus
    $wgUser is not guaranteed to exist until MediaWiki has been fully
    initialized; block status needs to be checked early on for
    authentication-related permission checks.
    
    Bug: T218608
    Change-Id: I16315c071855024bc0412d5360c95f843420d9a9

--- includes/user/User.php
+++ includes/user/User.php
@@ -1558,7 +1558,7 @@ class User implements IDBAccessObject {
 	 *   Check when actually saving should be done against master.
 	 */
 	private function getBlockedStatus( $bFromSlave = true ) {
-		global $wgProxyWhitelist, $wgUser, $wgApplyIpBlocksToXff;
+		global $wgProxyWhitelist, $wgApplyIpBlocksToXff;
 
 		if ( -1 != $this->mBlockedby ) {
 			return;
@@ -1578,11 +1578,12 @@ class User implements IDBAccessObject {
 		# know which IP address they're actually coming from
 		$ip = null;
 		if ( !$this->isAllowed( 'ipblock-exempt' ) ) {
-			// $wgUser->getName() only works after the end of Setup.php. Until
-			// then, assume it's a logged-out user.
-			$globalUserName = $wgUser->isSafeToLoad()
-				? $wgUser->getName()
-				: IP::sanitizeIP( $wgUser->getRequest()->getIP() );
+			$sessionUser = RequestContext::getMain()->getUser();
+			// the session user is set up towards the end of Setup.php. Until then,
+			// assume it's a logged-out user.
+			$globalUserName = $sessionUser->isSafeToLoad()
+				? $sessionUser->getName()
+				: IP::sanitizeIP( $sessionUser->getRequest()->getIP() );
 			if ( $this->getName() === $globalUserName ) {
 				$ip = $this->getRequest()->getIP();
 			}
@@ -1639,9 +1640,9 @@ class User implements IDBAccessObject {
 		}
 
 		// Avoid PHP 7.1 warning of passing $this by reference
-		$user = $this;
+		$thisUser = $this;
 		// Extensions
-		Hooks::run( 'GetBlockedStatus', [ &$user ] );
+		Hooks::run( 'GetBlockedStatus', [ &$thisUser ] );
 	}
 
 	/**
