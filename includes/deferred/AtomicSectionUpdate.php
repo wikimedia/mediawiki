17080453a1a94d317247197b639472f87e2aaa03
Aaron Schulz <aschulz@wikimedia.org>
2019-03-21 14:05:35 -0700

    Support more coupled DBs in AtomicSectionUpdate/AutoCommitUpdate
    This is useful for updating a secondary database based on a
    commit to the main one, especially for using auto-commit.
    
    Change-Id: Iad9084d2fb0490ecdfd5fcc97db33bfbcbfa5d21

--- includes/deferred/AtomicSectionUpdate.php
+++ includes/deferred/AtomicSectionUpdate.php
@@ -15,18 +15,22 @@ class AtomicSectionUpdate implements DeferrableUpdate, DeferrableCallback {
 	private $callback;
 
 	/**
-	 * @param IDatabase $dbw
+	 * @param IDatabase $dbw DB handle; update aborts if a transaction now this rolls back
 	 * @param string $fname Caller name (usually __METHOD__)
 	 * @param callable $callback
+	 * @param IDatabase[] $conns Abort if a transaction now on one of these rolls back [optional]
 	 * @see IDatabase::doAtomicSection()
 	 */
-	public function __construct( IDatabase $dbw, $fname, callable $callback ) {
+	public function __construct( IDatabase $dbw, $fname, callable $callback, array $conns = [] ) {
 		$this->dbw = $dbw;
 		$this->fname = $fname;
 		$this->callback = $callback;
-
-		if ( $this->dbw->trxLevel() ) {
-			$this->dbw->onTransactionResolution( [ $this, 'cancelOnRollback' ], $fname );
+		// Register DB connections for which uncommitted changes are related to this update
+		$conns[] = $dbw;
+		foreach ( $conns as $conn ) {
+			if ( $conn->trxLevel() ) {
+				$conn->onTransactionResolution( [ $this, 'cancelOnRollback' ], $fname );
+			}
 		}
 	}
 
@@ -36,6 +40,10 @@ class AtomicSectionUpdate implements DeferrableUpdate, DeferrableCallback {
 		}
 	}
 
+	/**
+	 * @private This method is public so that it works with onTransactionResolution()
+	 * @param int $trigger
+	 */
 	public function cancelOnRollback( $trigger ) {
 		if ( $trigger === IDatabase::TRIGGER_ROLLBACK ) {
 			$this->callback = null;
--- includes/deferred/AutoCommitUpdate.php
+++ includes/deferred/AutoCommitUpdate.php
@@ -15,17 +15,21 @@ class AutoCommitUpdate implements DeferrableUpdate, DeferrableCallback {
 	private $callback;
 
 	/**
-	 * @param IDatabase $dbw
+	 * @param IDatabase $dbw DB handle; update aborts if a transaction now this rolls back
 	 * @param string $fname Caller name (usually __METHOD__)
 	 * @param callable $callback Callback that takes (IDatabase, method name string)
+	 * @param IDatabase[] $conns Abort if a transaction now on one of these rolls back [optional]
 	 */
-	public function __construct( IDatabase $dbw, $fname, callable $callback ) {
+	public function __construct( IDatabase $dbw, $fname, callable $callback, array $conns = [] ) {
 		$this->dbw = $dbw;
 		$this->fname = $fname;
 		$this->callback = $callback;
-
-		if ( $this->dbw->trxLevel() ) {
-			$this->dbw->onTransactionResolution( [ $this, 'cancelOnRollback' ], $fname );
+		// Register DB connections for which uncommitted changes are related to this update
+		$conns[] = $dbw;
+		foreach ( $conns as $conn ) {
+			if ( $conn->trxLevel() ) {
+				$conn->onTransactionResolution( [ $this, 'cancelOnRollback' ], $fname );
+			}
 		}
 	}
 
@@ -50,6 +54,10 @@ class AutoCommitUpdate implements DeferrableUpdate, DeferrableCallback {
 		}
 	}
 
+	/**
+	 * @private This method is public so that it works with onTransactionResolution()
+	 * @param int $trigger
+	 */
 	public function cancelOnRollback( $trigger ) {
 		if ( $trigger === IDatabase::TRIGGER_ROLLBACK ) {
 			$this->callback = null;
