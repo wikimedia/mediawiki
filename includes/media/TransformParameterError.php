b233e9d1887de9e131b824ed5b659e3ab954ba6e
Umherirrender <umherirrender_de.wp@web.de>
2019-03-30 12:16:51 +0100

    Move MediaTransformOutput classes to own files
    Change-Id: I1c676f18101250ffc28ea412213d568c5a4f527d

---
+++ includes/media/TransformParameterError.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Base class for the output of file transformation methods.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Media
+ */
+
+/**
+ * Shortcut class for parameter validation errors
+ *
+ * @ingroup Media
+ */
+class TransformParameterError extends MediaTransformError {
+	function __construct( $params ) {
+		parent::__construct( 'thumbnail_error',
+			max( $params['width'] ?? 0, 120 ),
+			max( $params['height'] ?? 0, 120 ),
+			wfMessage( 'thumbnail_invalid_params' )
+		);
+	}
+
+	function getHttpStatusCode() {
+		return 400;
+	}
+}
