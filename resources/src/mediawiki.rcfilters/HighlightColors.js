a85f40b5bd95e8c896847838fc878d357f998020
Fomafix <fomafix@googlemail.com>
2019-04-07 10:15:00 +0200

    Remove redundant closure for all packageFiles with own directory
    The modules loaded with packageFiles, which are always executed in
    module scope (with a closure), even in debug mode.
    
    The behaviour of non-packageFiles debug mode is the only reason files
    have closures.
    
    Bug: T50886
    Change-Id: I38b1b426930763e5ddf61fa29235c2df829310c3

--- resources/src/mediawiki.rcfilters/HighlightColors.js
+++ resources/src/mediawiki.rcfilters/HighlightColors.js
@@ -1,12 +1,10 @@
-( function () {
-	/**
-	 * Supported highlight colors.
-	 * Warning: These are also hardcoded in "styles/mw.rcfilters.variables.less"
-	 *
-	 * @member mw.rcfilters
-	 * @property {string[]}
-	 */
-	var HighlightColors = [ 'c1', 'c2', 'c3', 'c4', 'c5' ];
+/**
+ * Supported highlight colors.
+ * Warning: These are also hardcoded in "styles/mw.rcfilters.variables.less"
+ *
+ * @member mw.rcfilters
+ * @property {string[]}
+ */
+var HighlightColors = [ 'c1', 'c2', 'c3', 'c4', 'c5' ];
 
-	module.exports = HighlightColors;
-}() );
+module.exports = HighlightColors;
--- resources/src/mediawiki.rcfilters/mw.rcfilters.js
+++ resources/src/mediawiki.rcfilters/mw.rcfilters.js
@@ -1,61 +1,59 @@
-( function () {
-	/**
-	 * @class
-	 * @singleton
-	 */
-	mw.rcfilters = {
-		Controller: require( './Controller.js' ),
-		UriProcessor: require( './UriProcessor.js' ),
-		dm: {
-			ChangesListViewModel: require( './dm/ChangesListViewModel.js' ),
-			FilterGroup: require( './dm/FilterGroup.js' ),
-			FilterItem: require( './dm/FilterItem.js' ),
-			FiltersViewModel: require( './dm/FiltersViewModel.js' ),
-			ItemModel: require( './dm/ItemModel.js' ),
-			SavedQueriesModel: require( './dm/SavedQueriesModel.js' ),
-			SavedQueryItemModel: require( './dm/SavedQueryItemModel.js' )
-		},
-		ui: {},
-		utils: {
-			addArrayElementsUnique: function ( arr, elements ) {
-				elements = Array.isArray( elements ) ? elements : [ elements ];
+/**
+ * @class
+ * @singleton
+ */
+mw.rcfilters = {
+	Controller: require( './Controller.js' ),
+	UriProcessor: require( './UriProcessor.js' ),
+	dm: {
+		ChangesListViewModel: require( './dm/ChangesListViewModel.js' ),
+		FilterGroup: require( './dm/FilterGroup.js' ),
+		FilterItem: require( './dm/FilterItem.js' ),
+		FiltersViewModel: require( './dm/FiltersViewModel.js' ),
+		ItemModel: require( './dm/ItemModel.js' ),
+		SavedQueriesModel: require( './dm/SavedQueriesModel.js' ),
+		SavedQueryItemModel: require( './dm/SavedQueryItemModel.js' )
+	},
+	ui: {},
+	utils: {
+		addArrayElementsUnique: function ( arr, elements ) {
+			elements = Array.isArray( elements ) ? elements : [ elements ];
 
-				elements.forEach( function ( element ) {
-					if ( arr.indexOf( element ) === -1 ) {
-						arr.push( element );
-					}
-				} );
-
-				return arr;
-			},
-			normalizeParamOptions: function ( givenOptions, legalOptions ) {
-				var result = [];
-
-				if ( givenOptions.indexOf( 'all' ) > -1 ) {
-					// If anywhere in the values there's 'all', we
-					// treat it as if only 'all' was selected.
-					// Example: param=valid1,valid2,all
-					// Result: param=all
-					return [ 'all' ];
+			elements.forEach( function ( element ) {
+				if ( arr.indexOf( element ) === -1 ) {
+					arr.push( element );
 				}
+			} );
 
-				// Get rid of any dupe and invalid parameter, only output
-				// valid ones
-				// Example: param=valid1,valid2,invalid1,valid1
-				// Result: param=valid1,valid2
-				givenOptions.forEach( function ( value ) {
-					if (
-						legalOptions.indexOf( value ) > -1 &&
-						result.indexOf( value ) === -1
-					) {
-						result.push( value );
-					}
-				} );
+			return arr;
+		},
+		normalizeParamOptions: function ( givenOptions, legalOptions ) {
+			var result = [];
 
-				return result;
+			if ( givenOptions.indexOf( 'all' ) > -1 ) {
+				// If anywhere in the values there's 'all', we
+				// treat it as if only 'all' was selected.
+				// Example: param=valid1,valid2,all
+				// Result: param=all
+				return [ 'all' ];
 			}
-		}
-	};
 
-	module.exports = mw.rcfilters;
-}() );
+			// Get rid of any dupe and invalid parameter, only output
+			// valid ones
+			// Example: param=valid1,valid2,invalid1,valid1
+			// Result: param=valid1,valid2
+			givenOptions.forEach( function ( value ) {
+				if (
+					legalOptions.indexOf( value ) > -1 &&
+					result.indexOf( value ) === -1
+				) {
+					result.push( value );
+				}
+			} );
+
+			return result;
+		}
+	}
+};
+
+module.exports = mw.rcfilters;
