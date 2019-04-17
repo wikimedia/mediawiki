7f9850b1b05926ee0591a0c753aae4ed56a1a707
Peter Boehm <boehm@hallowelt.com>
2019-04-17 14:17:34 +0200

    Remove old TimeZone Setting
    The old settign $wgDefaultUserOptions['timecorrection'] was only used for very old MW Versions and is not needed any more, see:
    
    https://www.mediawiki.org/wiki/Manual:Timezone
    Found while investigating ERM#14919

--- settings.d/005-DefaultSettings.php
+++ settings.d/005-DefaultSettings.php
@@ -2,7 +2,6 @@
 
 $wgLocalTZoffset = date("Z") / 60;
 $wgLocaltimezone = 'Europe/Berlin';
-$wgDefaultUserOptions['timecorrection'] = 'ZoneInfo|' . (date("I") ? 120 : 60) . '|Europe/Berlin';
 $wgUrlProtocols[] = 'file://';
 $wgNamespacesWithSubpages[NS_MAIN] = true;
 $wgExternalLinkTarget = '_blank';
