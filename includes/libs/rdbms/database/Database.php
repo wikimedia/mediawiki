99a2ea61f930ca895fc0e340c19a83339cdafd96
Aaron Schulz <aschulz@wikimedia.org>
2019-06-09 08:22:45 +0100

    rdbms: implement strictor ownership of LoadBalancer by LBFactory
    Change-Id: Idafd2cf05e016a5f88b90e4c4c74f82c212d61c9

--- includes/libs/rdbms/database/Database.php
+++ includes/libs/rdbms/database/Database.php
@@ -3976,7 +3976,7 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 		}
 	}
 
-	final public function rollback( $fname = __METHOD__, $flush = '' ) {
+	final public function rollback( $fname = __METHOD__, $flush = self::FLUSHING_ONE ) {
 		$trxActive = $this->trxLevel;
 
 		if ( $flush !== self::FLUSHING_INTERNAL
