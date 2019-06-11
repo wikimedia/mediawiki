b8b8a3c692c82c467601c402462a65d15c9a0b89
Bartosz Dziewoński <matma.rex@gmail.com>
2019-06-12 00:50:02 +0200

    ResourceLoaderOOUIIconPackModule: Always use MW core 'localBasePath'
    Otherwise, extensions defining icon pack modules in extension.json
    would need to specify "localBasePath": "../..", which is silly.
    
    Follow-up to a8dae2212cc5e23e181023af2ba1891078b14355.
    
    Change-Id: If8f02867955d649c809b6b41de0f77e594b633d6

--- includes/resourceloader/ResourceLoaderOOUIIconPackModule.php
+++ includes/resourceloader/ResourceLoaderOOUIIconPackModule.php
@@ -69,4 +69,13 @@ class ResourceLoaderOOUIIconPackModule extends ResourceLoaderOOUIImageModule {
 
 		return $definition;
 	}
+
+	public static function extractLocalBasePath( $options, $localBasePath = null ) {
+		global $IP;
+		if ( $localBasePath === null ) {
+			$localBasePath = $IP;
+		}
+		// Ignore any 'localBasePath' present in $options, this always refers to files in MediaWiki core
+		return $localBasePath;
+	}
 }
