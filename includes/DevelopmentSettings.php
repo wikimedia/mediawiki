wikimedia/mediawiki

commit 5a090c29822285782d2027ffb8919a5440fa2f03

Set cache types to APC/APCu/WinCache in DevelopmentSettings.php
Bug: T225496
Change-Id: I14dd3fea9795a501e29383a3cae2b3e9a0077e69

includes/DevelopmentSettings.php
@@ -53,3 +53,9 @@ if ( $logDir ) {
 	$wgDebugLogGroups['error'] = "$logDir/mw-error.log";
 }
 unset( $logDir );
+// Make caching faster
+$wgMainCacheType = CACHE_ACCEL;
+$wgMessageCacheType = CACHE_ACCEL;
+$wgParserCacheType = CACHE_ACCEL;
+$wgSessionCacheType = CACHE_ACCEL;
+$wgLanguageConverterCacheType = CACHE_ACCEL;

commit 35fc6499f79f792d2fb11907a1b16fbffd1cee3b

DevelopmentSettings: Remove redundant CacheType overrides
Follows-up 5a090c29822. This is no longer needed as of
7e0fb4fff6, which will add this to the generated LocalSettings.php
file automatically.

The installer now sets MainCacheType for CLI installs, the same
way as the web installer did already.

The secondary cache types have as default CACHE_ANYTHING (not
CACHE_NONE) and will automatically inherit from MainCacheType.

This commit unbreaks situations where developers have MainCache
set to something (e.g. Redis, or Memc) which after 5a090c29822
caused other types to go to APC instead of Memc.

Bug: T225496
Change-Id: Ib2824a6d316912daf09672de748726b91e4de449

includes/DevelopmentSettings.php
@@ -14,7 +14,7 @@
  */
 
 /**
- * Debugging: PHP
+ * Debugging for PHP
  */
 
 // Enable showing of errors
@@ -22,7 +22,7 @@ error_reporting( -1 );
 ini_set( 'display_errors', 1 );
 
 /**
- * Debugging: MediaWiki
+ * Debugging for MediaWiki
  */
 global $wgDevelopmentWarnings, $wgShowExceptionDetails, $wgShowHostnames,
 	$wgDebugRawPage, $wgSQLMode, $wgCommandLineMode, $wgDebugLogFile,
@@ -53,9 +53,3 @@ if ( $logDir ) {
 	$wgDebugLogGroups['error'] = "$logDir/mw-error.log";
 }
 unset( $logDir );
-// Make caching faster
-$wgMainCacheType = CACHE_ACCEL;
-$wgMessageCacheType = CACHE_ACCEL;
-$wgParserCacheType = CACHE_ACCEL;
-$wgSessionCacheType = CACHE_ACCEL;
-$wgLanguageConverterCacheType = CACHE_ACCEL;
35fc6499f79f792d2fb11907a1b16fbffd1cee3b
Timo Tijhof <krinklemail@gmail.com>
2019-06-13 02:08:53 +0100

    DevelopmentSettings: Remove redundant CacheType overrides
    Follows-up 5a090c29822. This is no longer needed as of
    7e0fb4fff6, which will add this to the generated LocalSettings.php
    file automatically.
    
    The installer now sets MainCacheType for CLI installs, the same
    way as the web installer did already.
    
    The secondary cache types have as default CACHE_ANYTHING (not
    CACHE_NONE) and will automatically inherit from MainCacheType.
    
    This commit unbreaks situations where developers have MainCache
    set to something (e.g. Redis, or Memc) which after 5a090c29822
    caused other types to go to APC instead of Memc.
    
    Bug: T225496
    Change-Id: Ib2824a6d316912daf09672de748726b91e4de449

--- includes/DevelopmentSettings.php
+++ includes/DevelopmentSettings.php
@@ -14,7 +14,7 @@
  */
 
 /**
- * Debugging: PHP
+ * Debugging for PHP
  */
 
 // Enable showing of errors
@@ -22,7 +22,7 @@ error_reporting( -1 );
 ini_set( 'display_errors', 1 );
 
 /**
- * Debugging: MediaWiki
+ * Debugging for MediaWiki
  */
 global $wgDevelopmentWarnings, $wgShowExceptionDetails, $wgShowHostnames,
 	$wgDebugRawPage, $wgSQLMode, $wgCommandLineMode, $wgDebugLogFile,
@@ -53,9 +53,3 @@ if ( $logDir ) {
 	$wgDebugLogGroups['error'] = "$logDir/mw-error.log";
 }
 unset( $logDir );
-// Make caching faster
-$wgMainCacheType = CACHE_ACCEL;
-$wgMessageCacheType = CACHE_ACCEL;
-$wgParserCacheType = CACHE_ACCEL;
-$wgSessionCacheType = CACHE_ACCEL;
-$wgLanguageConverterCacheType = CACHE_ACCEL;
