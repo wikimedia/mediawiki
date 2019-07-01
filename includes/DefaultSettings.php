f5d789b0f6589a06ce07d047a3cafca5217dd2b9
Reedy <reedy@wikimedia.org>
2019-07-02 00:43:07 +0100

    Prepare 1.32.3
    Bug: T227046
    Change-Id: Ie271597fff356a4d8efaefde57b048acbeff8e16

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -71,7 +71,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.32.2';
+$wgVersion = '1.32.3';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
