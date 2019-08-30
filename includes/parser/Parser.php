a6e8182b9b2b40fb87fde53d6491b4dec18530e7
Fomafix <fomafix@googlemail.com>
2019-08-30 11:43:35 +0200

    Parser: Use 'Special:Badtitle/Parser' instead of 'NO TITLE'
    Change-Id: I818ac7d4766c3e52b56c2bd4abddb88e4140003b

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -908,7 +908,7 @@ class Parser {
 	 */
 	public function setTitle( $t ) {
 		if ( !$t ) {
-			$t = Title::newFromText( 'NO TITLE' );
+			$t = Title::makeTitle( NS_SPECIAL, 'Badtitle/Parser' );
 		}
 
 		if ( $t->hasFragment() ) {
