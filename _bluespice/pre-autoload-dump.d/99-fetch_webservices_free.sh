1e75c6430cd2c89d6324b373f813e6a4ac812204
Gokhan Geyik <iletisim@gokhangeyik.net>
2019-09-17 14:50:31 +0200

    free and pro webservices downloader separated

--- _bluespice/pre-autoload-dump.d/99-fetch_webservices_free.sh
+++ _bluespice/pre-autoload-dump.d/99-fetch_webservices_free.sh
@@ -6,18 +6,21 @@ BRANCH=${BRANCH##[[:space:]]}
 
 if [ "$BRANCH" == "REL1_31_dev" ] || [ "$BRANCH" == "REL1_31" ];
 then
-        mkdir extensions/BlueSpiceUEModulePDF/webapps
+        mkdir extensions/BlueSpiceUEModulePDF/webservices
+        rm -f extensions/BlueSpiceUEModulePDF/webservices/BShtml2PDF.war
         /usr/bin/env wget -P extensions/BlueSpiceUEModulePDF/webservices https://buildservice.bluespice.com/webservices/REL1_31/BShtml2PDF.war
 
 elif [ "$BRANCH" == "REL1_27" ];
 then
-        mkdir extensions/BlueSpiceUEModulePDF/webapps
+        mkdir extensions/BlueSpiceUEModulePDF/webservices
+        rm -f extensions/BlueSpiceUEModulePDF/webservices/BShtml2PDF.war
         /usr/bin/env wget -P extensions/BlueSpiceUEModulePDF/webservices https://buildservice.bluespice.com/webservices/REL1_27/BShtml2PDF.war
 
  # This condition can be removed after the merge with branch REL1_31_dev, REL1_31 or REL1_27
 elif [ "$BRANCH" == "REL1_31_dev-webservices-autodownload" ];
 then
-        mkdir extensions/BlueSpiceUEModulePDF/webapps
+        mkdir extensions/BlueSpiceUEModulePDF/webservices
+        rm -f extensions/BlueSpiceUEModulePDF/webservices/BShtml2PDF.war
         /usr/bin/env wget -P extensions/BlueSpiceUEModulePDF/webservices https://buildservice.bluespice.com/webservices/REL1_31/BShtml2PDF.war
  # This condition can be removed after the merge with branch REL1_31_dev, REL1_31 or REL1_27
 
