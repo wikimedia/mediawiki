f45441aaa7dd98b6106a36882d165a12ac9d682e
Brad Jorsch <bjorsch@wikimedia.org>
2019-09-18 11:32:13 -0400

    Clean up ar_usertext_timestamp index mess
    See T233221 for the sordid history. The short version is that it might
    exist as `ar_usertext_timestamp`, `usertext_timestamp`, or both on
    MySQL, so dropping it for the actor migration needs to drop both names
    if they exist.
    
    We can also remove the now-obsolete aliasing from MWLBFactory.
    
    Bug: T233221
    Bug: T104756
    Bug: T229712
    Change-Id: I429e7ade2c86a2514aafe947012f096b03aa67ce

--- maintenance/archives/patch-drop-user-fields.sql
+++ maintenance/archives/patch-drop-user-fields.sql
@@ -4,7 +4,6 @@
 -- T188327. Drop old xx_user and xx_user_text fields, and defaults from xx_actor fields.
 
 ALTER TABLE /*_*/archive
-  DROP INDEX /*i*/ar_usertext_timestamp,
   DROP COLUMN ar_user,
   DROP COLUMN ar_user_text,
   ALTER COLUMN ar_actor DROP DEFAULT;
