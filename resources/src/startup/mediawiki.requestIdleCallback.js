f4975bc6de776c8eb06490932329a3c8d657eabd
Timo Tijhof <krinklemail@gmail.com>
2019-09-24 01:09:04 +0100

    resourceloader: Remove redundant closure of some startup and base files
    * errorLogger: Contains only top-level assignments and statements,
      no scope needed. If it does need a scope at some point, then it
      should be converted to packageFiles first so that the closure
      still isn't needed (see T50886).
    
    * requestIdleCallback: This file should do nothing in modern browsers,
      except alias a native method, and expose the fallback for testing.
      Remove the closure and with it the (usually) unused local var.
    
    * mediawiki.js: Use the shorter Object.hasOwnProperty instead,
      we already do this in several files. Both provide a quick reference
      to the native method. Object.prototype.hasOwnProperty is more direct
      visually, but also less direct at run-time (3 lookups instead of 2).
      MW does not support environments that extend native classes such as
      Object or their prototypes, so this makes no difference in that
      regard and is already done in several core files. (More wide changes
      to be done separately).
    
    Change-Id: I98ed4da4807c54254669053cef47a3e67b4ad2cf

--- resources/src/startup/mediawiki.requestIdleCallback.js
+++ resources/src/startup/mediawiki.requestIdleCallback.js
@@ -1,54 +1,51 @@
 /* global mw */
-( function () {
-	var maxBusy = 50;
+mw.requestIdleCallbackInternal = function ( callback ) {
+	setTimeout( function () {
+		var start = mw.now();
+		callback( {
+			didTimeout: false,
+			timeRemaining: function () {
+				// Hard a target maximum busy time of 50 milliseconds
+				return Math.max( 0, 50 - ( mw.now() - start ) );
+			}
+		} );
+	}, 1 );
+};
 
-	mw.requestIdleCallbackInternal = function ( callback ) {
-		setTimeout( function () {
-			var start = mw.now();
-			callback( {
-				didTimeout: false,
-				timeRemaining: function () {
-					return Math.max( 0, maxBusy - ( mw.now() - start ) );
-				}
-			} );
-		}, 1 );
-	};
-
-	/**
-	 * Schedule a deferred task to run in the background.
-	 *
-	 * This allows code to perform tasks in the main thread without impacting
-	 * time-critical operations such as animations and response to input events.
-	 *
-	 * Basic logic is as follows:
-	 *
-	 * - User input event should be acknowledged within 100ms per [RAIL].
-	 * - Idle work should be grouped in blocks of upto 50ms so that enough time
-	 *   remains for the event handler to execute and any rendering to take place.
-	 * - Whenever a native event happens (e.g. user input), the deadline for any
-	 *   running idle callback drops to 0.
-	 * - As long as the deadline is non-zero, other callbacks pending may be
-	 *   executed in the same idle period.
-	 *
-	 * See also:
-	 *
-	 * - <https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback>
-	 * - <https://w3c.github.io/requestidlecallback/>
-	 * - <https://developers.google.com/web/updates/2015/08/using-requestidlecallback>
-	 * [RAIL]: https://developers.google.com/web/fundamentals/performance/rail
-	 *
-	 * @member mw
-	 * @param {Function} callback
-	 * @param {Object} [options]
-	 * @param {number} [options.timeout] If set, the callback will be scheduled for
-	 *  immediate execution after this amount of time (in milliseconds) if it didn't run
-	 *  by that time.
-	 */
-	mw.requestIdleCallback = window.requestIdleCallback ?
-		// Bind because it throws TypeError if context is not window
-		window.requestIdleCallback.bind( window ) :
-		mw.requestIdleCallbackInternal;
-	// Note: Polyfill was previously disabled due to
-	// https://bugs.chromium.org/p/chromium/issues/detail?id=647870
-	// See also <http://codepen.io/Krinkle/full/XNGEvv>
-}() );
+/**
+ * Schedule a deferred task to run in the background.
+ *
+ * This allows code to perform tasks in the main thread without impacting
+ * time-critical operations such as animations and response to input events.
+ *
+ * Basic logic is as follows:
+ *
+ * - User input event should be acknowledged within 100ms per [RAIL].
+ * - Idle work should be grouped in blocks of upto 50ms so that enough time
+ *   remains for the event handler to execute and any rendering to take place.
+ * - Whenever a native event happens (e.g. user input), the deadline for any
+ *   running idle callback drops to 0.
+ * - As long as the deadline is non-zero, other callbacks pending may be
+ *   executed in the same idle period.
+ *
+ * See also:
+ *
+ * - <https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback>
+ * - <https://w3c.github.io/requestidlecallback/>
+ * - <https://developers.google.com/web/updates/2015/08/using-requestidlecallback>
+ * [RAIL]: https://developers.google.com/web/fundamentals/performance/rail
+ *
+ * @member mw
+ * @param {Function} callback
+ * @param {Object} [options]
+ * @param {number} [options.timeout] If set, the callback will be scheduled for
+ *  immediate execution after this amount of time (in milliseconds) if it didn't run
+ *  by that time.
+ */
+mw.requestIdleCallback = window.requestIdleCallback ?
+	// Bind because it throws TypeError if context is not window
+	window.requestIdleCallback.bind( window ) :
+	mw.requestIdleCallbackInternal;
+// Note: Polyfill was previously disabled due to
+// https://bugs.chromium.org/p/chromium/issues/detail?id=647870
+// See also <http://codepen.io/Krinkle/full/XNGEvv>
