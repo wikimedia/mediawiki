c20e8781514376640721b868857fbda0c737ab7d
Jon Harald Søby <jhsoby@gmail.com>
2019-10-07 00:35:22 +0300

    Add Balinese namespace translations
    Change-Id: Ifb2e5f1fa0b415549740f16692fb2e8e232fa549

--- languages/messages/MessagesBan.php
+++ languages/messages/MessagesBan.php
@@ -9,3 +9,22 @@
  */
 
 $fallback = 'id';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Média',
+	NS_SPECIAL          => 'Kusus',
+	NS_TALK             => 'Pabligbagan',
+	NS_USER             => 'Sang_Anganggé',
+	NS_USER_TALK        => 'Pabligbagan_Sang_Anganggé',
+	NS_PROJECT_TALK     => 'Pabligbagan_$1',
+	NS_FILE             => 'Berkas',
+	NS_FILE_TALK        => 'Pabligbagan_Berkas',
+	NS_MEDIAWIKI        => 'MédiaWiki',
+	NS_MEDIAWIKI_TALK   => 'Pabligbagan_MédiaWiki',
+	NS_TEMPLATE         => 'Mal',
+	NS_TEMPLATE_TALK    => 'Pabligbagan_Mal',
+	NS_HELP             => 'Wantuan',
+	NS_HELP_TALK        => 'Pabligbagan_Wantuan',
+	NS_CATEGORY         => 'Kategori',
+	NS_CATEGORY_TALK    => 'Pabligbagan_Kategori',
+];
