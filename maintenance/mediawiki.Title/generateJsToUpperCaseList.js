130fe3783c3e85afc60d2c75a51d5ae3d9a233e1
Ed Sanders <esanders@wikimedia.org>
2019-10-08 13:07:25 +0100

    eslint: Use /server rules for maintenance script
    Change-Id: Ieba8c4bb2acf33e5fdd35152e193cb34fba404ac

--- maintenance/mediawiki.Title/generateJsToUpperCaseList.js
+++ maintenance/mediawiki.Title/generateJsToUpperCaseList.js
@@ -1,8 +1,6 @@
-/* eslint-env node, es6 */
 var i, chars = [];
 
 for ( i = 0; i <= 0x10ffff; i++ ) {
-	// eslint-disable-next-line no-restricted-properties
 	chars.push( String.fromCodePoint( i ).toUpperCase() );
 }
 // eslint-disable-next-line no-console
