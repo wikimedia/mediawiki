da191ad0eb6edb139ba7b7bf17e0e07ac36a4625
LiangYanZhejiang <LiangYanZhejiang@users.noreply.github.com>
2019-10-25 15:35:41 +0800

    primary key can't added if index is not exists
    if index ug_user_group is not exists, primary key will not be added.

--- maintenance/archives/patch-user_groups-ug_expiry.sql
+++ maintenance/archives/patch-user_groups-ug_expiry.sql
@@ -1,5 +1,6 @@
 -- Add expiry column in user_groups table
 
 ALTER TABLE /*$wgDBprefix*/user_groups
+  ADD PRIMARY KEY (ug_user, ug_group),
   ADD COLUMN ug_expiry varbinary(14) NULL default NULL,
   ADD INDEX ug_expiry (ug_expiry);
