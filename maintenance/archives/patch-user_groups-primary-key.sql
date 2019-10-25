cebb9c388b96bc30304ca3ee981ea3caf09f479b
LiangYanZhejiang <LiangYanZhejiang@users.noreply.github.com>
2019-10-25 15:33:04 +0800

    drop index ug_user_group only
    if index ug_user_group is not exists, primary key will not be added.

--- maintenance/archives/patch-user_groups-primary-key.sql
+++ maintenance/archives/patch-user_groups-primary-key.sql
@@ -1,5 +1,4 @@
 -- Convert unique index into a primary key on user_groups
 
 ALTER TABLE /*$wgDBprefix*/user_groups
-  DROP INDEX ug_user_group,
-  ADD PRIMARY KEY (ug_user, ug_group);
+  DROP INDEX ug_user_group;
