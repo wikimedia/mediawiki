1f891df091175c9870e0a4c4f3bb9b03a7d29f83
Jeena Huneidi <jhuneidi@wikimedia.org>
2019-10-31 22:25:21 -0700

    Exit on error for .pipeline/dev_prereq.sh
    Make sure the script exits if there is an error to avoid
    publishing broken images.
    
    As a temporary fix, change submodule update --init to depth 10
    because the submodule might be at a commit not present in the
    shallow clone.
    
    Bug: T237310
    Change-Id: Id05f487798b1c74550c802be886f5a770f431d43

--- .pipeline/dev_prereq.sh
+++ .pipeline/dev_prereq.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir /tmp/php
+set -eo pipefail
+
+mkdir -p /tmp/php
 mkdir -p extensions
 
 git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/extensions/VisualEditor.git /var/www/html/extensions/VisualEditor
 git clone --depth 1 https://gerrit.wikimedia.org/r/mediawiki/skins/Vector /var/www/html/skins/Vector
 cd /var/www/html/extensions/VisualEditor
-git submodule update --depth 1 --init
+git submodule update --depth 10 --init
 
 cd /var/www/html
 composer install
