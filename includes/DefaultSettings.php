6170b0c2638fa6326a9dec2aeb4836ddac016b85
Reedy <reedy@wikimedia.org>
2019-11-05 21:39:34 +0000

    Bump $wgVersion 1.34.0-rc.1
    Change-Id: Ia14a51d05256e8110359865f7c239fd13d700f40

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -72,7 +72,7 @@ $wgConfigRegistry = [
  * MediaWiki version number
  * @since 1.2
  */
-$wgVersion = '1.34.0-rc.0';
+$wgVersion = '1.34.0-rc.1';
 
 /**
  * Name of the site. It must be changed in LocalSettings.php
