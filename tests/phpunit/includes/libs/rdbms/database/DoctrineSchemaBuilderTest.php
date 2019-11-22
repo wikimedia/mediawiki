6d4dc90831472fc8050bc63e06ae51c462d6815a
Amir Sarabadani <Ladsgroup@gmail.com>
2019-11-22 19:37:58 +0100

    Improve tests in DoctrineSchemaBuilder
    Also changing its structure to be more readable and more like
    https://w.wiki/CXo
    
    My plan is to add more tables to this test.
    
    Bug: T230428
    Change-Id: Ia19d5c875cc95238f7fd7cd9adbed1ddb92af078

--- tests/phpunit/includes/libs/rdbms/database/DoctrineSchemaBuilderTest.php
+++ tests/phpunit/includes/libs/rdbms/database/DoctrineSchemaBuilderTest.php
@@ -7,50 +7,23 @@ class DoctrineSchemaBuilderTest extends \PHPUnit\Framework\TestCase {
 
 	/**
 	 * @covers \Wikimedia\Rdbms\DoctrineSchemaBuilder
-	 * @dataProvider provideGetResult
 	 */
-	public function testGetResult( $table, $expected, $platform = null ) {
-		if ( $platform === null ) {
-			$platform = new MySqlPlatform();
-		}
+	public function testGetResultAllTables() {
+		$platform = new MySqlPlatform();
 		$builder = new DoctrineSchemaBuilder( $platform );
-		$builder->addTable( $table );
+		$tables = json_decode( file_get_contents( __DIR__ . '/../../../../data/db/tables.json' ), true );
+		$expected = file_get_contents( __DIR__ . '/../../../../data/db/mysql/tables.sql' );
+		foreach ( $tables as $table ) {
+			$builder->addTable( $table );
+		}
 
-		$actual = $builder->getSql();
+		$actual = implode( "\n", $builder->getSql() );
+		$actual = preg_replace( "/\s*?(\n|$)/m", "", $actual );
+		$expected = preg_replace( "/\s*?(\n|$)/m", "", $expected );
 
 		$this->assertSame(
 			$expected,
 			$actual
 		);
 	}
-
-	public function provideGetResult() {
-		return [
-			[
-				[
-					'name' => 'actor',
-					'columns' => [
-						[ 'actor_id', 'bigint', [ 'Unsigned' => true, 'Notnull' => true ] ],
-						[ 'actor_user', 'integer', [ 'Unsigned' => true ] ],
-						[ 'actor_name', 'string', [ 'Length' => 255, 'Notnull' => true ] ],
-					],
-					'indexes' => [
-						[ 'actor_user', [ 'actor_user' ], 'unique' => true ],
-						[ 'actor_name', [ 'actor_name' ], 'unique' => true ]
-					],
-					'pk' => [ 'actor_id' ]
-				],
-				[
-					'CREATE TABLE /*_*/actor (' .
-					'actor_id BIGINT UNSIGNED NOT NULL, ' .
-					'actor_user INT UNSIGNED NOT NULL, ' .
-					'actor_name VARCHAR(255) NOT NULL, ' .
-					'UNIQUE INDEX actor_user (actor_user), ' .
-					'UNIQUE INDEX actor_name (actor_name), ' .
-					'PRIMARY KEY(actor_id)) '
-				],
-			],
-		];
-	}
-
 }
