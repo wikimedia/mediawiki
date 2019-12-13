wikimedia/mediawiki

commit df010dd28453e215f7a59fd731768c6f7eb60e0d

resourceloader: Remove redundant 'window' indirection
Use global variables directly as globals, except for the specific
case of accessing it when it may not exist. In those cases we
use the fact that undefined properties of an object yield the
`undefined` value which we can cast to false. Accessing an undefined
variable would yield a ReferenceError exception.

Change-Id: I1d9e9aa5845ba3c756ad6e31358d8594e003b04b

resources/src/startup/startup.js
@@ -3,8 +3,8 @@
  *
  * - Beware: This file MUST parse without errors on even the most ancient of browsers!
  */
-/* eslint-disable no-implicit-globals, vars-on-top, no-unmodified-loop-condition */
-/* global $VARS, $CODE */
+/* eslint-disable no-implicit-globals */
+/* global $VARS, $CODE, RLQ:true, NORLQ:true */
 
 /**
  * See <https://www.mediawiki.org/wiki/Compatibility#Browsers>
@@ -85,17 +85,17 @@ if ( !isCompatible( navigator.userAgent ) ) {
 		.replace( /(^|\s)client-js(\s|$)/, '$1client-nojs$2' );
 
 	// Process any callbacks for Grade C
-	while ( window.NORLQ && window.NORLQ[ 0 ] ) {
-		window.NORLQ.shift()();
+	while ( window.NORLQ && NORLQ[ 0 ] ) {
+		NORLQ.shift()();
 	}
-	window.NORLQ = {
+	NORLQ = {
 		push: function ( fn ) {
 			fn();
 		}
 	};
 
 	// Clear and disable the Grade A queue
-	window.RLQ = {
+	RLQ = {
 		push: function () {}
 	};
 } else {
@@ -137,24 +137,25 @@ if ( !isCompatible( navigator.userAgent ) ) {
 		// arrivals will also be processed. Late arrival can happen because
 		// startup.js is executed asynchronously, concurrently with the streaming
 		// response of the HTML.
-		var queue = window.RLQ;
-		window.RLQ = [];
-		/* global RLQ */
+		RLQ = window.RLQ || [];
 		RLQ.push = function ( fn ) {
 			if ( typeof fn === 'function' ) {
 				fn();
 			} else {
-				// This callback requires a module, handled in mediawiki.base.
+				// If the first parameter is not a function, then it is an array
+				// containing a list of required module names and a function.
+				// Do an actual push for now, as this signature is handled
+				// later by mediawiki.base.js.
 				RLQ[ RLQ.length ] = fn;
 			}
 		};
-		while ( queue && queue[ 0 ] ) {
-			// Re-use our new push() method
-			RLQ.push( queue.shift() );
+		while ( RLQ[ 0 ] ) {
+			// Process all values gathered so far
+			RLQ.push( RLQ.shift() );
 		}
 
 		// Clear and disable the Grade C queue
-		window.NORLQ = {
+		NORLQ = {
 			push: function () {}
 		};
 	}() );

includes/resourceloader/ResourceLoader.php
@@ -1482,7 +1482,7 @@ MESSAGE;
 	 */
 	public static function makeLoaderConditionalScript( $script ) {
 		// Adds a function to lazy-created RLQ
-		return '(window.RLQ=window.RLQ||[]).push(function(){' .
+		return '(RLQ=window.RLQ||[]).push(function(){' .
 			trim( $script ) . '});';
 	}
 
@@ -1496,7 +1496,7 @@ MESSAGE;
 	 */
 	public static function makeInlineCodeWithModule( $modules, $script ) {
 		// Adds an array to lazy-created RLQ
-		return '(window.RLQ=window.RLQ||[]).push(['
+		return '(RLQ=window.RLQ||[]).push(['
 			. self::encodeJsonForScript( $modules ) . ','
 			. 'function(){' . trim( $script ) . '}'
 			. ']);';
@@ -1527,7 +1527,7 @@ MESSAGE;
 
 		return new WrappedString(
 			Html::inlineScript( $js, $nonce ),
-			"<script$escNonce>(window.RLQ=window.RLQ||[]).push(function(){",
+			"<script$escNonce>(RLQ=window.RLQ||[]).push(function(){",
 			'});</script>'
 		);
 	}

tests/phpunit/includes/OutputPageTest.php
@@ -2711,7 +2711,7 @@ class OutputPageTest extends MediaWikiTestCase {
 			// Single only=scripts load
 			[
 				[ 'test.foo', ResourceLoaderModule::TYPE_SCRIPTS ],
-				"<script nonce=\"secret\">(window.RLQ=window.RLQ||[]).push(function(){"
+				"<script nonce=\"secret\">(RLQ=window.RLQ||[]).push(function(){"
 					. 'mw.loader.load("http://127.0.0.1:8080/w/load.php?lang=en\u0026modules=test.foo\u0026only=scripts\u0026skin=fallback");'
 					. "});</script>"
 			],
@@ -2724,14 +2724,14 @@ class OutputPageTest extends MediaWikiTestCase {
 			// Private embed (only=scripts)
 			[
 				[ 'test.quux', ResourceLoaderModule::TYPE_SCRIPTS ],
-				"<script nonce=\"secret\">(window.RLQ=window.RLQ||[]).push(function(){"
+				"<script nonce=\"secret\">(RLQ=window.RLQ||[]).push(function(){"
 					. "mw.test.baz({token:123});\nmw.loader.state({\"test.quux\":\"ready\"});"
 					. "});</script>"
 			],
 			// Load private module (combined)
 			[
 				[ 'test.quux', ResourceLoaderModule::TYPE_COMBINED ],
-				"<script nonce=\"secret\">(window.RLQ=window.RLQ||[]).push(function(){"
+				"<script nonce=\"secret\">(RLQ=window.RLQ||[]).push(function(){"
 					. "mw.loader.implement(\"test.quux@1ev0ijv\",function($,jQuery,require,module){"
 					. "mw.test.baz({token:123});},{\"css\":[\".mw-icon{transition:none}"
 					. "\"]});});</script>"
@@ -2749,7 +2749,7 @@ class OutputPageTest extends MediaWikiTestCase {
 			// Load two modules in separate groups
 			[
 				[ [ 'test.group.foo', 'test.group.bar' ], ResourceLoaderModule::TYPE_COMBINED ],
-				"<script nonce=\"secret\">(window.RLQ=window.RLQ||[]).push(function(){"
+				"<script nonce=\"secret\">(RLQ=window.RLQ||[]).push(function(){"
 					. 'mw.loader.load("http://127.0.0.1:8080/w/load.php?lang=en\u0026modules=test.group.bar\u0026skin=fallback");'
 					. 'mw.loader.load("http://127.0.0.1:8080/w/load.php?lang=en\u0026modules=test.group.foo\u0026skin=fallback");'
 					. "});</script>"

tests/phpunit/includes/resourceloader/ResourceLoaderClientHtmlTest.php
@@ -113,7 +113,7 @@ Deprecation message.' ]
 			. 'RLSTATE={"test.exempt":"ready","test.private":"loading","test.styles.pure":"ready","test.styles.private":"ready","test.styles.deprecated":"ready"};'
 			. 'RLPAGEMODULES=["test"];'
 			. '</script>' . "\n"
-			. '<script>(window.RLQ=window.RLQ||[]).push(function(){'
+			. '<script>(RLQ=window.RLQ||[]).push(function(){'
 			. 'mw.loader.implement("test.private@{blankVer}",null,{"css":[]});'
 			. '});</script>' . "\n"
 			. '<link rel="stylesheet" href="/w/load.php?lang=nl&amp;modules=test.styles.deprecated%2Cpure&amp;only=styles&amp;skin=fallback"/>' . "\n"
@@ -190,7 +190,7 @@ Deprecation message.' ]
 			'test.styles.deprecated',
 		] );
 		// phpcs:disable Generic.Files.LineLength
-		$expected = '<script>(window.RLQ=window.RLQ||[]).push(function(){'
+		$expected = '<script>(RLQ=window.RLQ||[]).push(function(){'
 			. 'mw.log.warn("This page is using the deprecated ResourceLoader module \"test.styles.deprecated\".\nDeprecation message.");'
 			. '});</script>';
 		// phpcs:enable
@@ -220,7 +220,7 @@ Deprecation message.' ]
 				'modules' => [ 'test.private' ],
 				'only' => ResourceLoaderModule::TYPE_COMBINED,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.private@{blankVer}",null,{"css":[]});});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.private@{blankVer}",null,{"css":[]});});</script>',
 			],
 			[
 				'context' => [],
@@ -242,14 +242,14 @@ Deprecation message.' ]
 				'modules' => [ 'test.scripts.user' ],
 				'only' => ResourceLoaderModule::TYPE_SCRIPTS,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test.scripts.user\u0026only=scripts\u0026skin=fallback\u0026user=Example\u0026version=0a56zyi");});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test.scripts.user\u0026only=scripts\u0026skin=fallback\u0026user=Example\u0026version=0a56zyi");});</script>',
 			],
 			[
 				'context' => [],
 				'modules' => [ 'test.user' ],
 				'only' => ResourceLoaderModule::TYPE_COMBINED,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test.user\u0026skin=fallback\u0026user=Example\u0026version=0a56zyi");});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test.user\u0026skin=fallback\u0026user=Example\u0026version=0a56zyi");});</script>',
 			],
 			[
 				'context' => [ 'debug' => 'true' ],
@@ -278,7 +278,7 @@ Deprecation message.' ]
 				'modules' => [ 'test.shouldembed' ],
 				'only' => ResourceLoaderModule::TYPE_COMBINED,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",null,{"css":[]});});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.implement("test.shouldembed@09p30q0",null,{"css":[]});});</script>',
 			],
 			[
 				'context' => [],
@@ -292,14 +292,14 @@ Deprecation message.' ]
 				'modules' => [ 'test.scripts.shouldembed' ],
 				'only' => ResourceLoaderModule::TYPE_SCRIPTS,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.state({"test.scripts.shouldembed":"ready"});});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.state({"test.scripts.shouldembed":"ready"});});</script>',
 			],
 			[
 				'context' => [],
 				'modules' => [ 'test', 'test.shouldembed' ],
 				'only' => ResourceLoaderModule::TYPE_COMBINED,
 				'extra' => [],
-				'output' => '<script>(window.RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test\u0026skin=fallback");mw.loader.implement("test.shouldembed@09p30q0",null,{"css":[]});});</script>',
+				'output' => '<script>(RLQ=window.RLQ||[]).push(function(){mw.loader.load("/w/load.php?lang=nl\u0026modules=test\u0026skin=fallback");mw.loader.implement("test.shouldembed@09p30q0",null,{"css":[]});});</script>',
 			],
 			[
 				'context' => [],

commit c84604e6b7b40eb0ebc2b50011b45685ef14f767

resourceloader: Fix infinite loop in RLQ processing
RLQ.push( RLQ.shift() ); looks like it'd obviously be an infinite loop.
The reason it isn't is because we've overridden RLQ.push to execute its
argument. But it only does that if the argument is a function;
otherwise, it does add the argument to the array, causing an infinite
loop.

MediaWiki currently doesn't put non-function elements in RLQ early
enough for this code to run, but we want to be able to start doing that
without crashing everyone's browsers.

This was broken by df010dd28453e215f7a59fd731768c6f7eb60e0d. Go back to
putting the old RLQ value in a separate variable and resetting the
global RLQ to an empty array before processing the old array. That way
we're protected from an infinite loop, because the .push() and .shift()
operations happen on different arrays.

Change-Id: Idb10f5384f09e3cd681d417db28cca70d6e8dec2

resources/src/startup/startup.js
@@ -113,6 +113,7 @@ if ( !isCompatible( navigator.userAgent ) ) {
 	 */
 	( function () {
 		/* global mw */
+		var queue;
 
 		$CODE.registrations();
 
@@ -136,7 +137,11 @@ if ( !isCompatible( navigator.userAgent ) ) {
 		// arrivals will also be processed. Late arrival can happen because
 		// startup.js is executed asynchronously, concurrently with the streaming
 		// response of the HTML.
-		RLQ = window.RLQ || [];
+		queue = window.RLQ || [];
+		// Replace RLQ with an empty array, then process the things that were
+		// in RLQ previously. We have to do this to avoid an infinite loop:
+		// non-function items are added back to RLQ by the processing step.
+		RLQ = [];
 		RLQ.push = function ( fn ) {
 			if ( typeof fn === 'function' ) {
 				fn();
@@ -148,9 +153,9 @@ if ( !isCompatible( navigator.userAgent ) ) {
 				RLQ[ RLQ.length ] = fn;
 			}
 		};
-		while ( RLQ[ 0 ] ) {
+		while ( queue[ 0 ] ) {
 			// Process all values gathered so far
-			RLQ.push( RLQ.shift() );
+			RLQ.push( queue.shift() );
 		}
 
 		// Clear and disable the Grade C queue