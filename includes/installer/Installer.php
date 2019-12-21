wikimedia/mediawiki

commit b118bddc15f1dfb6d57326a3901fbd593ae09900

installer: Rename Doxygen group from "Deployment" to "Installer"
Also improve the description a bit and explain at a high level
how this code is used, and which classes back those entry points.

Change-Id: Ia69531858e714d0b43a29a7d5b6dcc5c7edd8240

includes/installer/Installer.php
@@ -21,7 +21,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 use MediaWiki\Interwiki\NullInterwikiLookup;
@@ -29,9 +29,15 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Shell\Shell;
 
 /**
- * This documentation group collects source code files with deployment functionality.
+ * The Installer helps admins create or upgrade their wiki.
  *
- * @defgroup Deployment Deployment
+ * The installer classes are exposed through these human interfaces:
+ *
+ * - The `maintenance/install.php` script, backed by CliInstaller.
+ * - The `maintenance/update.php` script, backed by DatabaseUpdater.
+ * - The `mw-config/index.php` web entry point, backed by WebInstaller.
+ *
+ * @defgroup Installer installer
  */
 
 /**
@@ -40,7 +46,7 @@ use MediaWiki\Shell\Shell;
  * This class provides the base for installation and update functionality
  * for both MediaWiki core and extensions.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 abstract class Installer {

includes/installer/CliInstaller.php
@@ -18,7 +18,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 use MediaWiki\Installer\InstallException;
@@ -27,7 +27,7 @@ use MediaWiki\MediaWikiServices;
 /**
  * Class for the core installer command line interface.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 class CliInstaller extends Installer {

includes/installer/DatabaseInstaller.php
@@ -18,7 +18,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 use MediaWiki\MediaWikiServices;
@@ -31,7 +31,7 @@ use Wikimedia\Rdbms\DBConnectionError;
 /**
  * Base class for DBMS-specific installation helper classes.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 abstract class DatabaseInstaller {

includes/installer/DatabaseUpdater.php
@@ -18,7 +18,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\IMaintainableDatabase;
@@ -30,7 +30,7 @@ require_once __DIR__ . '/../../maintenance/Maintenance.php';
  * Class for handling database updates. Roughly based off of updaters.inc, with
  * a few improvements :)
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 abstract class DatabaseUpdater {

includes/installer/InstallerSessionProvider.php
@@ -20,7 +20,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 use MediaWiki\Session\SessionProvider;

includes/installer/LocalSettingsGenerator.php
@@ -18,13 +18,13 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 /**
  * Class for generating LocalSettings.php file.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 class LocalSettingsGenerator {

includes/installer/MysqlInstaller.php
@@ -18,7 +18,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 
 use Wikimedia\Rdbms\Database;
@@ -28,7 +28,7 @@ use Wikimedia\Rdbms\DBConnectionError;
 /**
  * Class for setting up the MediaWiki database using MySQL.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  */
 class MysqlInstaller extends DatabaseInstaller {

includes/installer/MysqlUpdater.php
@@ -18,7 +18,7 @@
  * http://www.gnu.org/copyleft/gpl.html
  *
  * @file
- * @ingroup Deployment
+ * @ingroup Installer
  */
 use Wikimedia\Rdbms\MySQLField;
 use Wikimedia\Rdbms\IDatabase;
@@ -27,7 +27,7 @@ use MediaWiki\MediaWikiServices;
 /**
  * Mysql update list and mysql-specific update functions.
  *
- * @ingroup Deployment
+ * @ingroup Installer
  * @since 1.17
  * @property Wikimedia\Rdbms\DatabaseMysqlBase $db
  */

commit aacf3cc49fc796f5e85c4f69a7909dc2408987c1

installer: Start doc group name with uppercase I
Follows-up b118bddc15, accidentally started it lowercase, which is
different from all other doc groups in core.

Change-Id: I51262f71bc24e1caf943f73e254f16b481860f27

includes/installer/Installer.php
@@ -37,7 +37,7 @@ use MediaWiki\Shell\Shell;
  * - The `maintenance/update.php` script, backed by DatabaseUpdater.
  * - The `mw-config/index.php` web entry point, backed by WebInstaller.
  *
- * @defgroup Installer installer
+ * @defgroup Installer Installer
  */
 
 /**