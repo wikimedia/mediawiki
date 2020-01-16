a136005a35b74ac629684171ec3e3451788d197e
Petr Pchelko <ppchelko@wikimedia.org>
2020-01-16 15:40:58 -0800

    REST: /page/{title}/{bare,html,with_html} endpoints backed by RESTBase.
    Bug: T234377
    Bug: T234375
    Change-Id: I77709c17e951e3efb542028e5c0d53eedda8c7bf

--- tests/api-testing/REST/PageHTMLBare.js
+++ tests/api-testing/REST/PageHTMLBare.js
@@ -1,7 +1,7 @@
 const { action, assert, REST, utils } = require( 'api-testing' );
 
-describe( 'Page Source', () => {
-	const page = utils.title( 'PageSource ' );
+describe( 'Page HTML Bare', () => {
+	const page = utils.title( 'PageHTMLBare ' );
 	const client = new REST( 'rest.php/coredev/v0' );
 	const anon = action.getAnon();
 
@@ -9,28 +9,28 @@ describe( 'Page Source', () => {
 		await anon.edit( page, { text: "''Edit 1'' and '''Edit 2'''" } );
 	} );
 
-	describe( 'GET /page/{title}', () => {
-		it( 'Should successfully return page source and metadata for Wikitext page', async () => {
-			const { status, body } = await client.get( `/page/${page}` );
+	describe( 'GET /page/{title}/bare', () => {
+		it( 'Should successfully return page bare', async () => {
+			const { status, body } = await client.get( `/page/${page}/bare` );
 			assert.deepEqual( status, 200 );
-			assert.containsAllKeys( body, [ 'latest', 'id', 'key', 'license', 'title', 'content_model', 'source' ] );
+			assert.containsAllKeys( body, [ 'latest', 'id', 'key', 'license', 'title', 'content_model', 'html_url' ] );
 			assert.nestedPropertyVal( body, 'content_model', 'wikitext' );
 			assert.nestedPropertyVal( body, 'title', page );
 			assert.nestedPropertyVal( body, 'key', utils.dbkey( page ) );
-			assert.nestedPropertyVal( body, 'source', "''Edit 1'' and '''Edit 2'''" );
+			assert.match( body.html_url, new RegExp( `/page/${encodeURIComponent( page )}/html$` ) );
 		} );
 		it( 'Should return 404 error for non-existent page', async () => {
 			const dummyPageTitle = utils.title( 'DummyPage_' );
-			const { status } = await client.get( `/page/${dummyPageTitle}` );
+			const { status } = await client.get( `/page/${dummyPageTitle}/bare` );
 			assert.deepEqual( status, 404 );
 		} );
 		it( 'Should have appropriate response headers', async () => {
-			const preEditResponse = await client.get( `/page/${page}` );
+			const preEditResponse = await client.get( `/page/${page}/bare` );
 			const preEditDate = new Date( preEditResponse.body.latest.timestamp );
 			const preEditEtag = preEditResponse.headers.etag;
 
 			await anon.edit( page, { text: "'''Edit 3'''" } );
-			const postEditResponse = await client.get( `/page/${page}` );
+			const postEditResponse = await client.get( `/page/${page}/bare` );
 			const postEditDate = new Date( postEditResponse.body.latest.timestamp );
 			const postEditHeaders = postEditResponse.headers;
 			const postEditEtag = postEditResponse.headers.etag;
@@ -40,8 +40,8 @@ describe( 'Page Source', () => {
 			assert.match( postEditHeaders[ 'cache-control' ], /^maxage=\d/ );
 			assert.strictEqual( isNaN( preEditDate.getTime() ), false );
 			assert.strictEqual( isNaN( postEditDate.getTime() ), false );
-			assert.notEqual( preEditDate, postEditDate );
-			assert.notEqual( preEditEtag, postEditEtag );
+			assert.notStrictEqual( preEditDate, postEditDate );
+			assert.notStrictEqual( preEditEtag, postEditEtag );
 		} );
 	} );
 } );
