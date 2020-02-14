9fb25f7d2e164b48264483bdd58c1bc3309014e6
James D. Forrester <jforrester@wikimedia.org>
2020-02-14 12:21:46 -0800

    Follow-up 37a69a2f: ResourceLoaderSkinModule: Fix wfDeprecated() syntax
    Change-Id: Iadeec9520867bc1b6ace885fd4d93b856f6373d4

--- includes/resourceloader/ResourceLoaderSkinModule.php
+++ includes/resourceloader/ResourceLoaderSkinModule.php
@@ -336,7 +336,7 @@ class ResourceLoaderSkinModule extends ResourceLoaderFileModule {
 			$logoHD = $conf->get( 'LogoHD' );
 			// make sure not false
 			if ( $logoHD ) {
-				wfDeprecated( '$wgLogoHD', '1.35', 'Rename configuration variable to $wgLogos' );
+				wfDeprecated( __METHOD__ . ' with $wgLogoHD set instead of $wgLogos', '1.35', false, 1 );
 				$logos += $logoHD;
 			}
 		} catch ( ConfigException $e ) {
