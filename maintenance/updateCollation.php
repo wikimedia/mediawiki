23f99c4fd151d1cca6dc80461b6f6256e964338c
Ostrzyciel <magwac69@gmail.com>
2020-02-25 18:13:09 +0100

    updateCollation.php: fix PHP error
    Currently the updateCollation maintenance script throws an error
    when ran, as it calls MediaWikiServices before it's initialized.
    See the phab task for more details.
    
    Bug: T246127
    Change-Id: Ib9d6b485b55760897ff5152b5d6f22b0d6a36daa

--- maintenance/updateCollation.php
+++ maintenance/updateCollation.php
@@ -44,12 +44,11 @@ class UpdateCollation extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 
-		$categoryCollation = $this->getConfig()->get( 'CategoryCollation' );
 		$this->addDescription( <<<TEXT
 This script will find all rows in the categorylinks table whose collation is
-out-of-date (cl_collation != '$categoryCollation') and repopulate cl_sortkey
-using the page title and cl_sortkey_prefix.  If all collations are
-up-to-date, it will do nothing.
+out-of-date (cl_collation is not the same as \$wgCategoryCollation) and
+repopulate cl_sortkey using the page title and cl_sortkey_prefix. If all
+collations are up-to-date, it will do nothing.
 TEXT
 		);
 
