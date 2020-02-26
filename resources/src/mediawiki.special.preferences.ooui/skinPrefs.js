8ea8171c2fac4fa9f333fe04be7e3261ac055011
jdlrobson <jdlrobson@gmail.com>
2020-02-26 15:42:33 -0800

    Generalise skin preferences so it can be used by Monobook
    Vector and Monobook both register skin-specific preferences.
    These should appear under skin and only be revealed when the
    appropriate skin is selected.
    
    Having the heading of this reserved section in core aids with
    discoverability and adoption.
    
    See also: Ibd74cc03f3ccbdc0042163c18ab0f71b6aa556f6
    
    Bug: T246296
    Change-Id: Idd06bcfe7935e16732a6a95c1253dbf95c8aca2e

--- resources/src/mediawiki.special.preferences.ooui/skinPrefs.js
+++ resources/src/mediawiki.special.preferences.ooui/skinPrefs.js
@@ -6,10 +6,11 @@
 ( function () {
 	function invalidateSkinPrefsDisplay( $root ) {
 		var
-			// The skin preferences section. Skins with preferences should specify this section ID when
-			// adding preferences in their onGetPreferences() hook. At time of writing, this
-			// functionality is only used by Vector.
-			$skinPrefs = $root.find( '#mw-prefsection-rendering-skin-prefs' ),
+			// The skin preferences section. Skins with preferences should use the
+			// section `rendering/skin/skin-prefs` toa dd skin specific preferences
+			// in their onGetPreferences() hook.
+			// At time of writing, this functionality is only used by Vector and Monobook.
+			$skinPrefs = $root.find( '#mw-prefsection-rendering-skin-skin-prefs' ),
 			// oo-ui-fieldLayout is the wrapper class name for each preference. When all wrappers are
 			// disabled by hide-if (oo-ui-fieldLayout-disabled), the whole section should be hidden.
 			show = !!$skinPrefs.find( '.oo-ui-fieldLayout' ).not( '.oo-ui-fieldLayout-disabled' ).length;
