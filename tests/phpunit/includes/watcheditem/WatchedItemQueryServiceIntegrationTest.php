cc9dba469d8ea609c425bfaeb7bfc92a3021577e
hmonroy <hmonroy@wikimedia.org>
2020-03-02 16:29:28 -0800

    Add check to api that filters out expired watchlist items
    Add logic to API module that outputs the watchlist (api.php?action=query&list=watchlist)
    to filter out expired watched items when $wgWatchlistExpiry feature flag is turned on.
    
    Bug: T247029
    Change-Id: I49e7959b5166e74c95106a20d965dec6a0590a87

--- tests/phpunit/includes/watcheditem/WatchedItemQueryServiceIntegrationTest.php
+++ tests/phpunit/includes/watcheditem/WatchedItemQueryServiceIntegrationTest.php
@@ -78,4 +78,69 @@ class WatchedItemQueryServiceIntegrationTest extends MediaWikiIntegrationTestCas
 		$result = $queryService->getWatchedItemsForUser( $user );
 		$this->assertCount( $initialCount + 1, $result );
 	}
+
+	public function testGetWatchedItemsWithRecentChangeInfo_watchlistExpiry(): void {
+		$store = MediaWikiServices::getInstance()->getWatchedItemStore();
+		$queryService = MediaWikiServices::getInstance()->getWatchedItemQueryService();
+		$user = self::$users[ 'WatchedItemQueryServiceIntegrationTestUser' ]->getUser();
+		$options = [];
+		$startFrom = null;
+		$initialCount = count( $queryService->getWatchedItemsWithRecentChangeInfo( $user,
+				$options, $startFrom ) );
+
+		// Add two watched items, one of which is already expired, and check that only 1 is returned.
+		$userEditTarget1 = new TitleValue( 0, __METHOD__ . ' no expiry 1' );
+		$this->editPage( $userEditTarget1->getDBkey(), 'First Revision' );
+		$store->addWatch(
+			$user,
+			$userEditTarget1
+		);
+
+		$userEditTarget2 = new TitleValue( 0, __METHOD__ . ' expired a week ago or in a week' );
+		$this->editPage( $userEditTarget2->getDBkey(), 'First Revision' );
+		$store->addWatch(
+			$user,
+			$userEditTarget2,
+			'1 week ago'
+		);
+
+		$result1 = $queryService->getWatchedItemsWithRecentChangeInfo( $user,  $options, $startFrom );
+		$this->assertCount( $initialCount + 1, $result1 );
+
+		// Add another of each type of item, and make sure the new results are as expected.
+		$userEditTarget3 = new TitleValue( 0, __METHOD__ . ' no expiry 2' );
+		$this->editPage( $userEditTarget3->getDBkey(), 'First Revision' );
+		$store->addWatch(
+			$user,
+			$userEditTarget3
+		);
+
+		$userEditTarget4 = new TitleValue( 0, __METHOD__ . ' expired a week ago 2' );
+		$this->editPage( $userEditTarget4->getDBkey(), 'First Revision' );
+		$store->addWatch(
+			$user,
+			$userEditTarget4,
+			'1 week ago'
+		);
+		$result2 = $queryService->getWatchedItemsWithRecentChangeInfo( $user );
+		$this->assertCount( $initialCount + 2, $result2 );
+
+		// Make one of the expired items permanent, and check again.
+		$store->addWatch(
+			$user,
+			$userEditTarget4,
+			'infinity'
+		);
+		$result3 = $queryService->getWatchedItemsWithRecentChangeInfo( $user );
+		$this->assertCount( $initialCount + 3, $result3 );
+
+		// Make the other expired item expire in a week's time, and make sure it appears in the list.
+		$store->addWatch(
+			$user,
+			$userEditTarget2,
+			'1 week'
+		);
+		$result4 = $queryService->getWatchedItemsWithRecentChangeInfo( $user );
+		$this->assertCount( $initialCount + 4, $result4 );
+	}
 }
