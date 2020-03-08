f172dc461b4e4d49509a4ecf7b0317e17428b80a
Timo Tijhof <krinklemail@gmail.com>
2020-03-08 18:32:53 +0000

    phpunit: Speed up MediaWikiCoversValidator trait
    Using a data provider for a dynamic or otherwise potentially large amount
    of cases tends to come with significant overhead due to all the setup
    and test abstraction involved.
    
    Especially when the host class has expensive setUp() or tearDown(),
    logic such as the case for API integration tests and other test cases
    that have `group Database`. Each of the test methods invokes the
    DB reset logic for no good reason.
    
    As an example, ApiQueryRecentChangesIntegrationTest takes about
    55-60 seconds to run locally for me on master, but takes only 35-40s
    after this patch.
    
    Before:
    
    > slow tests (>50ms):
    >  …
    >  44. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #25
    >  45. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #2
    >  46. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #7
    >  47. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #8
    >  48. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #14
    >  49. 107ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #17
    >  50. 106ms to run ApiQueryRecentChangesIn…t:testValidCovers with d… #18
    > ...and there are 3 more above your threshold hidden from view
    >
    > Time: 59.13 seconds
    > OK (53 tests)
    
    After:
    
    > slow tests (>50ms):
    > …
    > 26. 122ms to run ApiQueryRecentChangesIntegrationTest:testValidCovers
    > 27. 108ms to run ApiQueryRecentChangesIntegrationTest:testApiTestGroup
    >
    > Time: 40.8 seconds
    > OK (27 tests)
    
    And that's just for running ApiQueryRecentChangesIntegrationTest.php
    in isolation. I expect the CI jobs overall to come down by much more
    than that.
    
    Bug: T225730
    Change-Id: Idae95f7c6f44c02371e2abc6e41d79b9eaced425

--- tests/phpunit/MediaWikiCoversValidator.php
+++ tests/phpunit/MediaWikiCoversValidator.php
@@ -22,35 +22,36 @@ use PHPUnit\Framework\CodeCoverageException;
 use PHPUnit\Util\Test;
 
 /**
- * Trait that checks that covers tags are valid, since PHPUnit
- * won't do it unless you run it with coverage, which is super
- * slow.
+ * Check that `@covers` tags are valid. PHPUnit only does this when generating
+ * code coverage reports, which is slow and we generally don't do that when
+ * running tests during development and pre-merge in CI.
  *
  * @since 1.31
  */
 trait MediaWikiCoversValidator {
 
 	/**
-	 * Test that all methods in this class that begin
-	 * with "test" have valid covers tags.
+	 * Assert that all "test*" methods in the host class have valid `@covers` tags.
 	 *
-	 * @dataProvider providePHPUnitTestMethodNames
+	 * Don't use a data provider here because this assertion will be run many
+	 * thousands of times in CI, and the implicit overhead from PHPUnit with
+	 * generating and executing a test case around each becomes rather significant
+	 * at that scale. Also, when using a data provider, the setUp() and tearDown()
+	 * of the host class would be re-run for every check, which becomes very
+	 * expensive for integration tests that involve databases.
 	 */
-	public function testValidCovers( $class, $method ) {
-		try {
-			Test::getLinesToBeCovered( $class, $method );
-		} catch ( CodeCoverageException $ex ) {
-			$this->fail( "$class::$method: " . $ex->getMessage() );
+	public function testValidCovers() {
+		$class = static::class;
+		foreach ( get_class_methods( $this ) as $method ) {
+			if ( strncmp( $method, 'test', 4 ) === 0 ) {
+				try {
+					Test::getLinesToBeCovered( $class, $method );
+				} catch ( CodeCoverageException $ex ) {
+					$this->fail( "$class::$method: " . $ex->getMessage() );
+				}
+			}
 		}
 
 		$this->addToAssertionCount( 1 );
-	}
-
-	public function providePHPUnitTestMethodNames() {
-		foreach ( get_class_methods( $this ) as $method ) {
-			if ( strncmp( $method, 'test', 4 ) === 0 ) {
-				yield [ static::class, $method ];
-			}
-		}
 	}
 }
