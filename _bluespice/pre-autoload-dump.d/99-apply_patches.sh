2c033d5981a6d7281763e307b4ebe7a8666a39d6
Gokhan Geyik <geyik@hallowelt.com>
2020-03-12 11:34:29 +0100

    fix: 99-apply_patches.sh

--- _bluespice/pre-autoload-dump.d/99-apply_patches.sh
+++ _bluespice/pre-autoload-dump.d/99-apply_patches.sh
@@ -9,13 +9,14 @@ NC='\033[0m'
 
 while read -r patch_file
 do
-    original_file=$(echo $patch_file | sed "s/\.diff//")
+    original_file=$(echo $patch_file | sed "s/\.diff//g")
+    original_file=$(echo $original_file | sed "s/_bluespice\/patches\///g")
     printf "\n${PURPLE}Patching: ${GREEN}$original_file${NC} ==> "
-    cmdout=$(/usr/bin/env patch --dry-run $original_file $patch_file)
+    cmdout=$(/usr/bin/env patch --ignore-whitespace --fuzz 3 --dry-run $original_file $patch_file)
     if [[ "$cmdout" == *"FAILED"* ]]; then
         printf "${RED}FAILED!${NC}\n"
     else
         cmdout=$(/usr/bin/env patch -s $original_file $patch_file)
         printf "${GREEN}DONE!${NC}\n"
     fi
-done < <(/usr/sbin/env find $patch_dir -type f -name "*\.diff")
+done < <(find $patch_dir -type f -name "*\.diff")
