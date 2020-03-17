0ac79b199c7ecb2bd7e56a97b78650d94cf1d128
DannyS712 <DannyS712.enwiki@gmail.com>
2020-03-17 02:03:54 +0000

    ApiLogoutTest: Remove use of global $wgUser
    Bug: T243708
    Change-Id: I30f82c71377219b35d1cc386380c8795f4cfdac9

--- tests/phpunit/includes/api/ApiLogoutTest.php
+++ tests/phpunit/includes/api/ApiLogoutTest.php
@@ -10,66 +10,65 @@
 class ApiLogoutTest extends ApiTestCase {
 
 	protected function setUp() : void {
-		global $wgRequest, $wgUser;
+		global $wgRequest;
 
 		parent::setUp();
 
-		// Link the user to the Session properly so User::doLogout() doesn't complain.
-		$wgRequest->getSession()->setUser( $wgUser );
-		$wgUser = User::newFromSession( $wgRequest );
-		$this->apiContext->setUser( $wgUser );
+		$user = $this->getTestSysop()->getUser();
+		$wgRequest->getSession()->setUser( $user );
+		$this->apiContext->setUser( $user );
 	}
 
 	public function testUserLogoutBadToken() {
-		global $wgUser;
+		$user = $this->getTestSysop()->getUser();
 
 		$this->setExpectedApiException( 'apierror-badtoken' );
-
 		try {
 			$token = 'invalid token';
-			$this->doUserLogout( $token );
+			$this->doUserLogout( $token, $user );
 		} finally {
-			$this->assertTrue( $wgUser->isLoggedIn(), 'not logged out' );
+			$this->assertTrue( $user->isLoggedIn(), 'not logged out' );
 		}
 	}
 
 	public function testUserLogout() {
-		global $wgUser;
+		$user = $this->getTestSysop()->getUser();
 
-		$this->assertTrue( $wgUser->isLoggedIn(), 'sanity check' );
-		$token = $this->getUserCsrfTokenFromApi();
-		$this->doUserLogout( $token );
-		$this->assertFalse( $wgUser->isLoggedIn() );
+		$this->assertTrue( $user->isLoggedIn(), 'sanity check' );
+		$token = $this->getUserCsrfTokenFromApi( $user );
+		$this->doUserLogout( $token, $user );
+		$this->assertFalse( $user->isLoggedIn() );
 	}
 
 	public function testUserLogoutWithWebToken() {
-		global $wgUser, $wgRequest;
+		global $wgRequest;
 
-		$this->assertTrue( $wgUser->isLoggedIn(), 'sanity check' );
+		$user = $this->getTestSysop()->getUser();
+		$this->assertTrue( $user->isLoggedIn(), 'sanity check' );
 
 		// Logic copied from SkinTemplate.
-		$token = $wgUser->getEditToken( 'logoutToken', $wgRequest );
+		$token = $user->getEditToken( 'logoutToken', $wgRequest );
 
-		$this->doUserLogout( $token );
-		$this->assertFalse( $wgUser->isLoggedIn() );
+		$this->doUserLogout( $token, $user );
+		$this->assertFalse( $user->isLoggedIn() );
 	}
 
-	private function getUserCsrfTokenFromApi() {
+	private function getUserCsrfTokenFromApi( User $user ) {
 		$retToken = $this->doApiRequest( [
 			'action' => 'query',
 			'meta' => 'tokens',
 			'type' => 'csrf'
-		] );
+		], null, false, $user );
 
 		$this->assertArrayNotHasKey( 'warnings', $retToken );
 
 		return $retToken[0]['query']['tokens']['csrftoken'];
 	}
 
-	private function doUserLogout( $logoutToken ) {
+	private function doUserLogout( $logoutToken, User $user ) {
 		return $this->doApiRequest( [
 			'action' => 'logout',
 			'token' => $logoutToken
-		] );
+		], null, false, $user );
 	}
 }
