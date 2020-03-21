a9a13a3174793d294d8abd1087839512528ad634
JJMC89 <JJMC89.Wikimedia@gmail.com>
2020-03-21 20:54:11 +0000

    Remove skipcaptcha from the basic grant
    skipcaptcha is a part of ConfirmEdit, not core.
    
    Bug: T241118
    Change-Id: Ifa9de57a25147e2e0672e7f3b97c9c779b21ca16
    Depends-On: I6c17fff97d868090cf9f770d2dd66c089154eae2

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -5834,7 +5834,6 @@ $wgGrantPermissions['basic']['nominornewtalk'] = true;
 $wgGrantPermissions['basic']['patrolmarks'] = true;
 $wgGrantPermissions['basic']['purge'] = true;
 $wgGrantPermissions['basic']['read'] = true;
-$wgGrantPermissions['basic']['skipcaptcha'] = true;
 $wgGrantPermissions['basic']['writeapi'] = true;
 
 $wgGrantPermissions['highvolume']['bot'] = true;
