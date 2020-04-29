wikimedia/mediawiki

commit ca8aa4d6b25eca09fd60b0c29974d2be9e54dcbd

Replace uses and hard deprecate Article::getRevisionFetched
Returns a Revision object; only deployed uses are in core
Make fetchRevisionRecord public as the replacement
Also removes a use of Revision::getContentHandler

Bug: T249393
Bug: T250981
Change-Id: Ib8f399e451c3a9b942001b48ea9a138f405c894c

includes/EditPage.php
@@ -682,12 +682,13 @@ class EditPage {
 			return;
 		}
 
-		$revision = $this->mArticle->getRevisionFetched();
+		$revRecord = $this->mArticle->fetchRevisionRecord();
 		// Disallow editing revisions with content models different from the current one
 		// Undo edits being an exception in order to allow reverting content model changes.
-		if ( $revision
-			&& $revision->getContentModel() !== $this->contentModel
-		) {
+		$revContentModel = $revRecord ?
+			$revRecord->getSlot( SlotRecord::MAIN, RevisionRecord::RAW )->getModel() :
+			false;
+		if ( $revContentModel && $revContentModel !== $this->contentModel ) {
 			$prevRev = null;
 			if ( $this->undidRev ) {
 				$undidRevObj = Revision::newFromId( $this->undidRev );
@@ -701,7 +702,7 @@ class EditPage {
 					$this->getContentObject(),
 					$this->context->msg(
 						'contentmodelediterror',
-						$revision->getContentModel(),
+						$revContentModel,
 						$this->contentModel
 					)->plain()
 				);
@@ -1448,25 +1449,27 @@ class EditPage {
 			if ( $content === false ) {
 				// Hack for restoring old revisions while EditPage
 				// can't handle multi-slot editing.
-				// TODO replace use of Revision objects entirely once
-				// Article::fetchRevisionRecord is public
-
 				$curRevisionRecord = $this->page->getRevisionRecord();
-				$oldRevision = $this->mArticle->getRevisionFetched();
+				$oldRevisionRecord = $this->mArticle->fetchRevisionRecord();
 
 				if ( $curRevisionRecord
-					&& $oldRevision
-					&& $curRevisionRecord->getId() !== $oldRevision->getId()
+					&& $oldRevisionRecord
+					&& $curRevisionRecord->getId() !== $oldRevisionRecord->getId()
 					&& ( WikiPage::hasDifferencesOutsideMainSlot(
-						$oldRevision->getRevisionRecord(),
+						$oldRevisionRecord,
 						$curRevisionRecord
-					) || !$this->isSupportedContentModel( $oldRevision->getContentModel() ) )
+					) || !$this->isSupportedContentModel(
+						$oldRevisionRecord->getSlot(
+							SlotRecord::MAIN,
+							RevisionRecord::RAW
+						)->getModel()
+					) )
 				) {
 					$this->context->getOutput()->redirect(
 						$this->mTitle->getFullURL(
 							[
 								'action' => 'mcrrestore',
-								'restore' => $oldRevision->getId(),
+								'restore' => $oldRevisionRecord->getId(),
 							]
 						)
 					);
@@ -1502,13 +1505,13 @@ class EditPage {
 		if ( $this->section == 'new' ) {
 			return $this->getCurrentContent();
 		}
-		$revision = $this->mArticle->getRevisionFetched();
-		if ( $revision === null ) {
+		$revRecord = $this->mArticle->fetchRevisionRecord();
+		if ( $revRecord === null ) {
 			return $this->contentHandlerFactory
 				->getContentHandler( $this->contentModel )
 				->makeEmptyContent();
 		}
-		return $revision->getContent( RevisionRecord::FOR_THIS_USER, $user );
+		return $revRecord->getContent( SlotRecord::MAIN, RevisionRecord::FOR_THIS_USER, $user );
 	}
 
 	/**
@@ -3341,12 +3344,12 @@ ERROR;
 			}
 
 			if ( $this->section != 'new' ) {
-				$revision = $this->mArticle->getRevisionFetched();
-				if ( $revision ) {
+				$revRecord = $this->mArticle->fetchRevisionRecord();
+				if ( $revRecord ) {
 					// Let sysop know that this will make private content public if saved
 
 					if ( !RevisionRecord::userCanBitfield(
-						$revision->getVisibility(),
+						$revRecord->getVisibility(),
 						RevisionRecord::DELETED_TEXT,
 						$user
 					) ) {
@@ -3354,15 +3357,15 @@ ERROR;
 							"<div class='mw-warning plainlinks'>\n$1\n</div>\n",
 							'rev-deleted-text-permission'
 						);
-					} elseif ( $revision->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+					} elseif ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 						$out->wrapWikiMsg(
 							"<div class='mw-warning plainlinks'>\n$1\n</div>\n",
 							'rev-deleted-text-view'
 						);
 					}
 
-					if ( !$revision->isCurrent() ) {
-						$this->mArticle->setOldSubtitle( $revision->getId() );
+					if ( !$revRecord->isCurrent() ) {
+						$this->mArticle->setOldSubtitle( $revRecord->getId() );
 						$out->wrapWikiMsg(
 							Html::warningBox( "\n$1\n" ),
 							'editingold'

RELEASE-NOTES-1.35
@@ -733,6 +733,9 @@ because of Phabricator reports.
   Instead, use ContentHandler::getUndoContent.
 * Article::doDeleteUpdates and ::doEditUpdates are deprecated. Instead,
   use WikiPage::doDeleteUpdates and ::doEditUpdates.
+* Article::getRevisionFetched is deprecated. Instead, use the
+  fetchRevisionRecord method, which has been converted from protected to
+  public.
 * LocalFileDeleteBatch was migrated to a new constructor signature with the
   user as the second parameter. Support for the old signature is hard
   deprecated, and once removed the user parameter will be required. At the

includes/actions/InfoAction.php
@@ -93,15 +93,15 @@ class InfoAction extends FormlessAction {
 		// Validate revision
 		$oldid = $this->getArticle()->getOldID();
 		if ( $oldid ) {
-			$revision = $this->getArticle()->getRevisionFetched();
+			$revRecord = $this->getArticle()->fetchRevisionRecord();
 
 			// Revision is missing
-			if ( $revision === null ) {
+			if ( $revRecord === null ) {
 				return $this->msg( 'missing-revision', $oldid )->parse();
 			}
 
 			// Revision is not current
-			if ( !$revision->isCurrent() ) {
+			if ( !$revRecord->isCurrent() ) {
 				return $this->msg( 'pageinfo-not-current' )->plain();
 			}
 		}

includes/page/Article.php
@@ -282,7 +282,7 @@ class Article implements Page {
 	 * This function has side effects! Do not use this function if you
 	 * only want the real revision text if any.
 	 *
-	 * @deprecated since 1.32, use getRevisionFetched() or fetchRevisionRecord() instead.
+	 * @deprecated since 1.32, use fetchRevisionRecord() instead.
 	 *
 	 * @return Content
 	 *
@@ -449,9 +449,11 @@ class Article implements Page {
 	 * the revision actually loaded from the database, and any errors encountered while doing
 	 * that.
 	 *
+	 * Public since 1.35
+	 *
 	 * @return RevisionRecord|null
 	 */
-	protected function fetchRevisionRecord() {
+	public function fetchRevisionRecord() {
 		if ( $this->fetchResult ) {
 			return $this->mRevision ? $this->mRevision->getRevisionRecord() : null;
 		}
@@ -572,10 +574,13 @@ class Article implements Page {
 	 * wrapping content supplied by an extension. Refer to $this->fetchResult for the
 	 * revision actually loaded from the database.
 	 *
+	 * @deprecated since 1.35
+	 *
 	 * @since 1.19
 	 * @return Revision|null
 	 */
 	public function getRevisionFetched() {
+		wfDeprecated( __METHOD__, '1.35' );
 		$this->fetchRevisionRecord();
 
 		if ( $this->fetchResult->isOK() ) {
@@ -924,7 +929,7 @@ class Article implements Page {
 		$unhide = $request->getInt( 'unhide' ) == 1;
 		$oldid = $this->getOldID();
 
-		$rev = $this->getRevisionFetched();
+		$rev = $this->fetchRevisionRecord();
 
 		if ( !$rev ) {
 			$this->getContext()->getOutput()->setPageTitle( wfMessage( 'errorpagetitle' ) );
@@ -936,7 +941,11 @@ class Article implements Page {
 			return;
 		}
 
-		$contentHandler = $rev->getContentHandler();
+		$contentHandler = MediaWikiServices::getInstance()
+			->getContentHandlerFactory()
+			->getContentHandler(
+				$rev->getSlot( SlotRecord::MAIN, RevisionRecord::RAW )->getModel()
+			);
 		$de = $contentHandler->createDifferenceEngine(
 			$this->getContext(),
 			$oldid,

tests/phpunit/includes/page/ArticleViewTest.php
@@ -59,6 +59,8 @@ class ArticleViewTest extends MediaWikiTestCase {
 	 * @covers Article::getRevIdFetched()
 	 */
 	public function testGetOldId() {
+		$this->hideDeprecated( 'Article::getRevisionFetched' );
+
 		$revisions = [];
 		$page = $this->getPage( __METHOD__, [ 1 => 'Test A', 2 => 'Test B' ], $revisions );
 

commit c757ec5fd9277bfe9feeef8e87953b284c60eb78

EditPage::showHeader - only warn editing an old revision if it  exists
Caused by ca8aa4d6b25eca09fd60b0c29974d2be9e54dcbd

Bug: T251404
Change-Id: I772c322cfe9450e1bc444a32753c81f48a1f3210

includes/EditPage.php
@@ -29,6 +29,7 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\RevisionStore;
+use MediaWiki\Revision\RevisionStoreRecord;
 use MediaWiki\Revision\SlotRecord;
 use Wikimedia\ScopedCallback;
 
@@ -3345,7 +3346,7 @@ ERROR;
 
 			if ( $this->section != 'new' ) {
 				$revRecord = $this->mArticle->fetchRevisionRecord();
-				if ( $revRecord ) {
+				if ( $revRecord && $revRecord instanceof RevisionStoreRecord ) {
 					// Let sysop know that this will make private content public if saved
 
 					if ( !RevisionRecord::userCanBitfield(