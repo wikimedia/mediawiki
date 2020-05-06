wikimedia/mediawiki

commit 49707c59da2c4776b7752a5f2d5710f772c8d541

Replace various uses of Revision objects
Each file's changes are independent of the other files and can be
reviewed separately.

Bug: T250579
Bug: T250714
Bug: T250580
Change-Id: I223dd6dfba27e560aff89be6705d91c40c550dd8

includes/revisionlist/RevisionItem.php
@@ -38,6 +38,17 @@ class RevisionItem extends RevisionItemBase {
 		$this->context = $list->getContext();
 	}
 
+	/**
+	 * Get the RevisionRecord for the item
+	 *
+	 * @todo remove use of Revision entirely
+	 *
+	 * @return RevisionRecord
+	 */
+	protected function getRevisionRecord() : RevisionRecord {
+		return $this->revision->getRevisionRecord();
+	}
+
 	public function getIdField() {
 		return 'rev_id';
 	}
@@ -56,7 +67,7 @@ class RevisionItem extends RevisionItemBase {
 
 	public function canView() {
 		return RevisionRecord::userCanBitfield(
-			$this->revision->getVisibility(),
+			$this->getRevisionRecord()->getVisibility(),
 			RevisionRecord::DELETED_RESTRICTED,
 			$this->context->getUser()
 		);
@@ -64,14 +75,14 @@ class RevisionItem extends RevisionItemBase {
 
 	public function canViewContent() {
 		return RevisionRecord::userCanBitfield(
-			$this->revision->getVisibility(),
+			$this->getRevisionRecord()->getVisibility(),
 			RevisionRecord::DELETED_TEXT,
 			$this->context->getUser()
 		);
 	}
 
 	public function isDeleted() {
-		return $this->revision->isDeleted( RevisionRecord::DELETED_TEXT );
+		return $this->getRevisionRecord()->isDeleted( RevisionRecord::DELETED_TEXT );
 	}
 
 	/**
@@ -82,8 +93,9 @@ class RevisionItem extends RevisionItemBase {
 	 * @return string
 	 */
 	protected function getRevisionLink() {
+		$revRecord = $this->getRevisionRecord();
 		$date = $this->list->getLanguage()->userTimeAndDate(
-			$this->revision->getTimestamp(), $this->list->getUser() );
+			$revRecord, $this->list->getUser() );
 
 		if ( $this->isDeleted() && !$this->canViewContent() ) {
 			return htmlspecialchars( $date );
@@ -94,7 +106,7 @@ class RevisionItem extends RevisionItemBase {
 			$date,
 			[],
 			[
-				'oldid' => $this->revision->getId(),
+				'oldid' => $revRecord->getId(),
 				'unhide' => 1
 			]
 		);
@@ -113,15 +125,15 @@ class RevisionItem extends RevisionItemBase {
 		} else {
 			$linkRenderer = $this->getLinkRenderer();
 			return $linkRenderer->makeKnownLink(
-					$this->list->title,
-					$this->list->msg( 'diff' )->text(),
-					[],
-					[
-						'diff' => $this->revision->getId(),
-						'oldid' => 'prev',
-						'unhide' => 1
-					]
-				);
+				$this->list->title,
+				$this->list->msg( 'diff' )->text(),
+				[],
+				[
+					'diff' => $this->getRevisionRecord()->getId(),
+					'oldid' => 'prev',
+					'unhide' => 1
+				]
+			);
 		}
 	}
 
@@ -135,8 +147,8 @@ class RevisionItem extends RevisionItemBase {
 		$difflink = $this->context->msg( 'parentheses' )
 			->rawParams( $this->getDiffLink() )->escaped();
 		$revlink = $this->getRevisionLink();
-		$userlink = Linker::revUserLink( $this->revision->getRevisionRecord() );
-		$comment = Linker::revComment( $this->revision->getRevisionRecord() );
+		$userlink = Linker::revUserLink( $this->getRevisionRecord() );
+		$comment = Linker::revComment( $this->getRevisionRecord() );
 		if ( $this->isDeleted() ) {
 			$revlink = "<span class=\"history-deleted\">$revlink</span>";
 		}

includes/actions/pagers/HistoryPager.php
@@ -324,17 +324,24 @@ class HistoryPager extends ReverseChronologicalPager {
 	function historyLine( $row, $next, $notificationtimestamp = false,
 		$dummy = false, $firstInList = false ) {
 		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
-		$rev = new Revision( $row, 0, $this->getTitle() );
+
+		$revRecord = $this->revisionStore->newRevisionFromRow(
+			$row,
+			RevisionStore::READ_NORMAL,
+			$this->getTitle()
+		);
 
 		if ( is_object( $next ) ) {
-			$prevRev = new Revision( $next, 0, $this->getTitle() );
+			$previousRevRecord = $this->revisionStore->newRevisionFromRow(
+				$next,
+				RevisionStore::READ_NORMAL,
+				$this->getTitle()
+			);
 		} else {
-			$prevRev = null;
+			$previousRevRecord = null;
 		}
 
-		$revRecord = $rev->getRevisionRecord();
-
-		$latest = $rev->getId() === $this->getWikiPage()->getLatest();
+		$latest = $revRecord->getId() === $this->getWikiPage()->getLatest();
 		$curlink = $this->curLink( $revRecord );
 		$lastlink = $this->lastLink( $revRecord, $next );
 		$curLastlinks = Html::rawElement( 'span', [], $curlink ) .
@@ -356,7 +363,7 @@ class HistoryPager extends ReverseChronologicalPager {
 		$canRevDelete = $permissionManager->userHasRight( $user, 'deleterevision' );
 		// Show checkboxes for each revision, to allow for revision deletion and
 		// change tags
-		$visibility = $rev->getVisibility();
+		$visibility = $revRecord->getVisibility();
 		if ( $canRevDelete || $this->showTagEditUI ) {
 			$this->preventClickjacking();
 			// If revision was hidden from sysops and we don't need the checkbox
@@ -372,7 +379,7 @@ class HistoryPager extends ReverseChronologicalPager {
 			// Otherwise, enable the checkbox...
 			} else {
 				$del = Xml::check( 'showhiderevisions', false,
-					[ 'name' => 'ids[' . $rev->getId() . ']' ] );
+					[ 'name' => 'ids[' . $revRecord->getId() . ']' ] );
 			}
 		// User can only view deleted revisions...
 		} elseif ( $visibility && $permissionManager->userHasRight( $user, 'deletedhistory' ) ) {
@@ -385,10 +392,16 @@ class HistoryPager extends ReverseChronologicalPager {
 				$del = Linker::revDeleteLinkDisabled( false );
 			// Otherwise, show the link...
 			} else {
-				$query = [ 'type' => 'revision',
-					'target' => $this->getTitle()->getPrefixedDBkey(), 'ids' => $rev->getId() ];
-				$del .= Linker::revDeleteLink( $query,
-					$rev->isDeleted( RevisionRecord::DELETED_RESTRICTED ), false );
+				$query = [
+					'type' => 'revision',
+					'target' => $this->getTitle()->getPrefixedDBkey(),
+					'ids' => $revRecord->getId()
+				];
+				$del .= Linker::revDeleteLink(
+					$query,
+					$revRecord->isDeleted( RevisionRecord::DELETED_RESTRICTED ),
+					false
+				);
 			}
 		}
 		if ( $del ) {
@@ -401,24 +414,24 @@ class HistoryPager extends ReverseChronologicalPager {
 		$s .= " $link";
 		$s .= $dirmark;
 		$s .= " <span class='history-user'>" .
-			Linker::revUserTools( $rev->getRevisionRecord(), true, false ) . "</span>";
+			Linker::revUserTools( $revRecord, true, false ) . "</span>";
 		$s .= $dirmark;
 
-		if ( $rev->isMinor() ) {
+		if ( $revRecord->isMinor() ) {
 			$s .= ' ' . ChangesList::flag( 'minor', $this->getContext() );
 		}
 
 		# Sometimes rev_len isn't populated
-		if ( $rev->getSize() !== null ) {
+		if ( $revRecord->getSize() !== null ) {
 			# Size is always public data
 			$prevSize = $this->parentLens[$row->rev_parent_id] ?? 0;
-			$sDiff = ChangesList::showCharacterDifference( $prevSize, $rev->getSize() );
-			$fSize = Linker::formatRevisionSize( $rev->getSize() );
+			$sDiff = ChangesList::showCharacterDifference( $prevSize, $revRecord->getSize() );
+			$fSize = Linker::formatRevisionSize( $revRecord->getSize() );
 			$s .= ' <span class="mw-changeslist-separator"></span> ' . "$fSize $sDiff";
 		}
 
 		# Text following the character difference is added just before running hooks
-		$s2 = Linker::revComment( $rev->getRevisionRecord(), false, true, false );
+		$s2 = Linker::revComment( $revRecord, false, true, false );
 
 		if ( $notificationtimestamp && ( $row->rev_timestamp >= $notificationtimestamp ) ) {
 			$s2 .= ' <span class="updatedmarker">' . $this->msg( 'updatedmarker' )->escaped() . '</span>';
@@ -429,13 +442,15 @@ class HistoryPager extends ReverseChronologicalPager {
 
 		# Rollback and undo links
 
-		if ( $prevRev && $permissionManager->quickUserCan( 'edit', $user, $this->getTitle() ) ) {
+		if ( $previousRevRecord &&
+			$permissionManager->quickUserCan( 'edit', $user, $this->getTitle() )
+		) {
 			if ( $latest && $permissionManager->quickUserCan( 'rollback',
 					$user, $this->getTitle() )
 			) {
 				// Get a rollback link without the brackets
 				$rollbackLink = Linker::generateRollback(
-					$rev->getRevisionRecord(),
+					$revRecord,
 					$this->getContext(),
 					[ 'verify', 'noBrackets' ]
 				);
@@ -445,8 +460,8 @@ class HistoryPager extends ReverseChronologicalPager {
 				}
 			}
 
-			if ( !$rev->isDeleted( RevisionRecord::DELETED_TEXT )
-				&& !$prevRev->isDeleted( RevisionRecord::DELETED_TEXT )
+			if ( !$revRecord->isDeleted( RevisionRecord::DELETED_TEXT )
+				&& !$previousRevRecord->isDeleted( RevisionRecord::DELETED_TEXT )
 			) {
 				# Create undo tooltip for the first (=latest) line only
 				$undoTooltip = $latest
@@ -458,15 +473,21 @@ class HistoryPager extends ReverseChronologicalPager {
 					$undoTooltip,
 					[
 						'action' => 'edit',
-						'undoafter' => $prevRev->getId(),
-						'undo' => $rev->getId()
+						'undoafter' => $previousRevRecord->getId(),
+						'undo' => $revRecord->getId()
 					]
 				);
 				$tools[] = "<span class=\"mw-history-undo\">{$undolink}</span>";
 			}
 		}
 		// Allow extension to add their own links here
-		Hooks::run( 'HistoryRevisionTools', [ $rev, &$tools, $prevRev, $user ] );
+		// TODO replace hook with one using RevisionRecord
+		Hooks::run( 'HistoryRevisionTools', [
+			new Revision( $revRecord ),
+			&$tools,
+			$previousRevRecord ? new Revision( $previousRevRecord ) : null,
+			$user
+		] );
 
 		if ( $tools ) {
 			$s2 .= ' ' . Html::openElement( 'span', [ 'class' => 'mw-changeslist-links' ] );
@@ -492,7 +513,7 @@ class HistoryPager extends ReverseChronologicalPager {
 			$s .= ' <span class="mw-changeslist-separator"></span> ' . $s2;
 		}
 
-		$attribs = [ 'data-mw-revid' => $rev->getId() ];
+		$attribs = [ 'data-mw-revid' => $revRecord->getId() ];
 
 		Hooks::run( 'PageHistoryLineEnding', [ $this, &$row, &$s, &$classes, &$attribs ] );
 		$attribs = array_filter( $attribs,
@@ -579,7 +600,11 @@ class HistoryPager extends ReverseChronologicalPager {
 			);
 		}
 
-		$nextRev = new Revision( $next, 0, $this->getTitle() );
+		$nextRev = $this->revisionStore->newRevisionFromRow(
+			$next,
+			RevisionStore::READ_NORMAL,
+			$this->getTitle()
+		);
 
 		if ( !RevisionRecord::userCanBitfield(
 				$prevRev->getVisibility(),

includes/revisiondelete/RevDelRevisionItem.php
@@ -46,6 +46,17 @@ class RevDelRevisionItem extends RevDelItem {
 		return new Revision( $row );
 	}
 
+	/**
+	 * Get the RevisionRecord for the item
+	 *
+	 * @todo remove use of Revision entirely
+	 *
+	 * @return RevisionRecord
+	 */
+	protected function getRevisionRecord() : RevisionRecord {
+		return $this->revision->getRevisionRecord();
+	}
+
 	public function getIdField() {
 		return 'rev_id';
 	}
@@ -68,7 +79,7 @@ class RevDelRevisionItem extends RevDelItem {
 
 	public function canView() {
 		return RevisionRecord::userCanBitfield(
-			$this->revision->getVisibility(),
+			$this->getRevisionRecord()->getVisibility(),
 			RevisionRecord::DELETED_RESTRICTED,
 			$this->list->getUser()
 		);
@@ -76,24 +87,26 @@ class RevDelRevisionItem extends RevDelItem {
 
 	public function canViewContent() {
 		return RevisionRecord::userCanBitfield(
-			$this->revision->getVisibility(),
+			$this->getRevisionRecord()->getVisibility(),
 			RevisionRecord::DELETED_TEXT,
 			$this->list->getUser()
 		);
 	}
 
 	public function getBits() {
-		return $this->revision->getVisibility();
+		return $this->getRevisionRecord()->getVisibility();
 	}
 
 	public function setBits( $bits ) {
+		$revRecord = $this->getRevisionRecord();
+
 		$dbw = wfGetDB( DB_MASTER );
 		// Update revision table
 		$dbw->update( 'revision',
 			[ 'rev_deleted' => $bits ],
 			[
-				'rev_id' => $this->revision->getId(),
-				'rev_page' => $this->revision->getPage(),
+				'rev_id' => $revRecord->getId(),
+				'rev_page' => $revRecord->getPageId(),
 				'rev_deleted' => $this->getBits() // cas
 			],
 			__METHOD__
@@ -109,7 +122,7 @@ class RevDelRevisionItem extends RevDelItem {
 				'rc_patrolled' => RecentChange::PRC_AUTOPATROLLED
 			],
 			[
-				'rc_this_oldid' => $this->revision->getId(), // condition
+				'rc_this_oldid' => $revRecord->getId(), // condition
 			],
 			__METHOD__
 		);
@@ -118,7 +131,7 @@ class RevDelRevisionItem extends RevDelItem {
 	}
 
 	public function isDeleted() {
-		return $this->revision->isDeleted( RevisionRecord::DELETED_TEXT );
+		return $this->getRevisionRecord()->isDeleted( RevisionRecord::DELETED_TEXT );
 	}
 
 	public function isHideCurrentOp( $newBits ) {
@@ -133,7 +146,9 @@ class RevDelRevisionItem extends RevDelItem {
 	 */
 	protected function getRevisionLink() {
 		$date = $this->list->getLanguage()->userTimeAndDate(
-			$this->revision->getTimestamp(), $this->list->getUser() );
+			$this->getRevisionRecord()->getTimestamp(),
+			$this->list->getUser()
+		);
 
 		if ( $this->isDeleted() && !$this->canViewContent() ) {
 			return htmlspecialchars( $date );
@@ -144,7 +159,7 @@ class RevDelRevisionItem extends RevDelItem {
 			$date,
 			[],
 			[
-				'oldid' => $this->revision->getId(),
+				'oldid' => $this->getRevisionRecord()->getId(),
 				'unhide' => 1
 			]
 		);
@@ -160,15 +175,15 @@ class RevDelRevisionItem extends RevDelItem {
 			return $this->list->msg( 'diff' )->escaped();
 		} else {
 			return $this->getLinkRenderer()->makeKnownLink(
-					$this->list->title,
-					$this->list->msg( 'diff' )->text(),
-					[],
-					[
-						'diff' => $this->revision->getId(),
-						'oldid' => 'prev',
-						'unhide' => 1
-					]
-				);
+				$this->list->title,
+				$this->list->msg( 'diff' )->text(),
+				[],
+				[
+					'diff' => $this->getRevisionRecord()->getId(),
+					'oldid' => 'prev',
+					'unhide' => 1
+				]
+			);
 		}
 	}
 
@@ -177,11 +192,13 @@ class RevDelRevisionItem extends RevDelItem {
 	 * change tags and everything
 	 */
 	public function getHTML() {
+		$revRecord = $this->getRevisionRecord();
+
 		$difflink = $this->list->msg( 'parentheses' )
 			->rawParams( $this->getDiffLink() )->escaped();
 		$revlink = $this->getRevisionLink();
-		$userlink = Linker::revUserLink( $this->revision->getRevisionRecord() );
-		$comment = Linker::revComment( $this->revision->getRevisionRecord() );
+		$userlink = Linker::revUserLink( $revRecord );
+		$comment = Linker::revComment( $revRecord );
 		if ( $this->isDeleted() ) {
 			$revlink = "<span class=\"history-deleted\">$revlink</span>";
 		}
@@ -208,32 +225,34 @@ class RevDelRevisionItem extends RevDelItem {
 	}
 
 	public function getApiData( ApiResult $result ) {
-		$rev = $this->revision;
+		$revRecord = $this->getRevisionRecord();
 		$user = $this->list->getUser();
 		$ret = [
-			'id' => $rev->getId(),
-			'timestamp' => wfTimestamp( TS_ISO_8601, $rev->getTimestamp() ),
-			'userhidden' => (bool)$rev->isDeleted( RevisionRecord::DELETED_USER ),
-			'commenthidden' => (bool)$rev->isDeleted( RevisionRecord::DELETED_COMMENT ),
-			'texthidden' => (bool)$rev->isDeleted( RevisionRecord::DELETED_TEXT ),
+			'id' => $revRecord->getId(),
+			'timestamp' => wfTimestamp( TS_ISO_8601, $revRecord->getTimestamp() ),
+			'userhidden' => (bool)$revRecord->isDeleted( RevisionRecord::DELETED_USER ),
+			'commenthidden' => (bool)$revRecord->isDeleted( RevisionRecord::DELETED_COMMENT ),
+			'texthidden' => (bool)$revRecord->isDeleted( RevisionRecord::DELETED_TEXT ),
 		];
 		if ( RevisionRecord::userCanBitfield(
-			$rev->getVisibility(),
+			$revRecord->getVisibility(),
 			RevisionRecord::DELETED_USER,
 			$user
 		) ) {
+			$revUser = $revRecord->getUser( RevisionRecord::FOR_THIS_USER, $user );
 			$ret += [
-				'userid' => $rev->getUser( RevisionRecord::FOR_THIS_USER, $user ),
-				'user' => $rev->getUserText( RevisionRecord::FOR_THIS_USER, $user ),
+				'userid' => $revUser ? $revUser->getId() : 0,
+				'user' => $revUser ? $revUser->getName() : '',
 			];
 		}
 		if ( RevisionRecord::userCanBitfield(
-			$rev->getVisibility(),
+			$revRecord->getVisibility(),
 			RevisionRecord::DELETED_COMMENT,
 			$user
 		) ) {
+			$revComment = $revRecord->getComment( RevisionRecord::FOR_THIS_USER, $user );
 			$ret += [
-				'comment' => $rev->getComment( RevisionRecord::FOR_THIS_USER, $user ),
+				'comment' => $revComment ? $revComment->text : ''
 			];
 		}
 

includes/specials/SpecialMergeHistory.php
@@ -21,6 +21,7 @@
  * @ingroup SpecialPage
  */
 
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\RevisionRecord;
 
 /**
@@ -277,7 +278,9 @@ class SpecialMergeHistory extends SpecialPage {
 	}
 
 	function formatRevisionRow( $row ) {
-		$rev = new Revision( $row );
+		$revRecord = MediaWikiServices::getInstance()
+			->getRevisionFactory()
+			->newRevisionFromRow( $row );
 
 		$linkRenderer = $this->getLinkRenderer();
 
@@ -290,25 +293,25 @@ class SpecialMergeHistory extends SpecialPage {
 		$user = $this->getUser();
 
 		$pageLink = $linkRenderer->makeKnownLink(
-			$rev->getTitle(),
+			$revRecord->getPageAsLinkTarget(),
 			$this->getLanguage()->userTimeAndDate( $ts, $user ),
 			[],
-			[ 'oldid' => $rev->getId() ]
+			[ 'oldid' => $revRecord->getId() ]
 		);
-		if ( $rev->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+		if ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 			$pageLink = '<span class="history-deleted">' . $pageLink . '</span>';
 		}
 
 		# Last link
 		if ( !RevisionRecord::userCanBitfield(
-			$rev->getVisibility(),
+			$revRecord->getVisibility(),
 			RevisionRecord::DELETED_TEXT,
 			$user
 		) ) {
 			$last = $this->msg( 'last' )->escaped();
 		} elseif ( isset( $this->prevId[$row->rev_id] ) ) {
 			$last = $linkRenderer->makeKnownLink(
-				$rev->getTitle(),
+				$revRecord->getPageAsLinkTarget(),
 				$this->msg( 'last' )->text(),
 				[],
 				[
@@ -318,13 +321,13 @@ class SpecialMergeHistory extends SpecialPage {
 			);
 		}
 
-		$userLink = Linker::revUserTools( $rev->getRevisionRecord() );
+		$userLink = Linker::revUserTools( $revRecord );
 
 		$size = $row->rev_len;
 		if ( $size !== null ) {
 			$stxt = Linker::formatRevisionSize( $size );
 		}
-		$comment = Linker::revComment( $rev->getRevisionRecord() );
+		$comment = Linker::revComment( $revRecord );
 
 		return Html::rawElement( 'li', [],
 			$this->msg( 'mergehistory-revisionrow' )

includes/specials/SpecialUndelete.php
@@ -436,7 +436,7 @@ class SpecialUndelete extends SpecialPage {
 		}
 		$revRecord = $rev->getRevisionRecord();
 
-		if ( $rev->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+		if ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 			if ( !RevisionRecord::userCanBitfield(
 				$revRecord->getVisibility(),
 				RevisionRecord::DELETED_TEXT,
@@ -444,7 +444,7 @@ class SpecialUndelete extends SpecialPage {
 			) ) {
 				$out->wrapWikiMsg(
 					"<div class='mw-warning plainlinks'>\n$1\n</div>\n",
-				$rev->isDeleted( RevisionRecord::DELETED_RESTRICTED ) ?
+				$revRecord->isDeleted( RevisionRecord::DELETED_RESTRICTED ) ?
 					'rev-suppressed-text-permission' : 'rev-deleted-text-permission'
 				);
 
@@ -461,9 +461,9 @@ class SpecialUndelete extends SpecialPage {
 		}
 
 		if ( $this->mDiff ) {
-			$previousRev = $archive->getPreviousRevision( $timestamp );
-			if ( $previousRev ) {
-				$this->showDiff( $previousRev->getRevisionRecord(), $revRecord );
+			$previousRevRecord = $archive->getPreviousRevisionRecord( $timestamp );
+			if ( $previousRevRecord ) {
+				$this->showDiff( $previousRevRecord, $revRecord );
 				if ( $this->mDiffOnly ) {
 					return;
 				}
@@ -488,7 +488,11 @@ class SpecialUndelete extends SpecialPage {
 		$t = $lang->userTime( $timestamp, $user );
 		$userLink = Linker::revUserTools( $revRecord );
 
-		$content = $rev->getContent( RevisionRecord::FOR_THIS_USER, $user );
+		$content = $revRecord->getContent(
+			SlotRecord::MAIN,
+			RevisionRecord::FOR_THIS_USER,
+			$user
+		);
 
 		// TODO: MCR: this will have to become something like $hasTextSlots and $hasNonTextSlots
 		$isText = ( $content instanceof TextContent );
@@ -987,7 +991,6 @@ class SpecialUndelete extends SpecialPage {
 				RevisionFactory::READ_NORMAL,
 				$this->mTargetObj
 			);
-		$revObject = new Revision( $revRecord );
 
 		$revTextSize = '';
 		$ts = wfTimestamp( TS_MW, $row->ar_timestamp );
@@ -1019,7 +1022,7 @@ class SpecialUndelete extends SpecialPage {
 				$pageLink = htmlspecialchars( $this->getLanguage()->userTimeAndDate( $ts, $user ) );
 				$last = $this->msg( 'diff' )->escaped();
 			} elseif ( $remaining > 0 || ( $earliestLiveTime && $ts > $earliestLiveTime ) ) {
-				$pageLink = $this->getPageLink( $revObject, $titleObj, $ts );
+				$pageLink = $this->getPageLink( $revRecord, $titleObj, $ts );
 				$last = $this->getLinkRenderer()->makeKnownLink(
 					$titleObj,
 					$this->msg( 'diff' )->text(),
@@ -1031,7 +1034,7 @@ class SpecialUndelete extends SpecialPage {
 					]
 				);
 			} else {
-				$pageLink = $this->getPageLink( $revObject, $titleObj, $ts );
+				$pageLink = $this->getPageLink( $revRecord, $titleObj, $ts );
 				$last = $this->msg( 'diff' )->escaped();
 			}
 		} else {
@@ -1129,17 +1132,17 @@ class SpecialUndelete extends SpecialPage {
 	/**
 	 * Fetch revision text link if it's available to all users
 	 *
-	 * @param Revision $rev
+	 * @param RevisionRecord $revRecord
 	 * @param Title $titleObj
 	 * @param string $ts Timestamp
 	 * @return string
 	 */
-	function getPageLink( $rev, $titleObj, $ts ) {
+	private function getPageLink( RevisionRecord $revRecord, $titleObj, $ts ) {
 		$user = $this->getUser();
 		$time = $this->getLanguage()->userTimeAndDate( $ts, $user );
 
 		if ( !RevisionRecord::userCanBitfield(
-			$rev->getVisibility(),
+			$revRecord->getVisibility(),
 			RevisionRecord::DELETED_TEXT,
 			$user
 		) ) {
@@ -1156,7 +1159,7 @@ class SpecialUndelete extends SpecialPage {
 			]
 		);
 
-		if ( $rev->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+		if ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 			$link = '<span class="history-deleted">' . $link . '</span>';
 		}
 

includes/specials/pagers/DeletedContribsPager.php
@@ -24,6 +24,7 @@
  */
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionFactory;
 use MediaWiki\Revision\RevisionRecord;
 use Wikimedia\Rdbms\FakeResultWrapper;
 use Wikimedia\Rdbms\IDatabase;
@@ -294,17 +295,13 @@ class DeletedContribsPager extends IndexPager {
 
 		$linkRenderer = $this->getLinkRenderer();
 
-		$rev = new Revision( [
-			'title' => $page,
-			'id' => $row->ar_rev_id,
-			'comment' => CommentStore::getStore()->getComment( 'ar_comment', $row )->text,
-			'user' => $row->ar_user,
-			'user_text' => $row->ar_user_text,
-			'actor' => $row->ar_actor ?? null,
-			'timestamp' => $row->ar_timestamp,
-			'minor_edit' => $row->ar_minor_edit,
-			'deleted' => $row->ar_deleted,
-		] );
+		$revRecord = MediaWikiServices::getInstance()
+			->getRevisionFactory()
+			->newRevisionFromRow(
+				$row,
+				RevisionFactory::READ_NORMAL,
+				$page
+			);
 
 		$undelete = SpecialPage::getTitleFor( 'Undelete' );
 
@@ -334,7 +331,7 @@ class DeletedContribsPager extends IndexPager {
 				[],
 				[
 					'target' => $page->getPrefixedText(),
-					'timestamp' => $rev->getTimestamp(),
+					'timestamp' => $revRecord->getTimestamp(),
 					'diff' => 'prev'
 				]
 			);
@@ -342,12 +339,12 @@ class DeletedContribsPager extends IndexPager {
 			$last = htmlspecialchars( $this->messages['diff'] );
 		}
 
-		$comment = Linker::revComment( $rev->getRevisionRecord() );
-		$date = $this->getLanguage()->userTimeAndDate( $rev->getTimestamp(), $user );
+		$comment = Linker::revComment( $revRecord );
+		$date = $this->getLanguage()->userTimeAndDate( $revRecord->getTimestamp(), $user );
 
 		if ( !$permissionManager->userHasRight( $user, 'undelete' ) ||
 			 !RevisionRecord::userCanBitfield(
-				$rev->getVisibility(),
+				$revRecord->getVisibility(),
 				RevisionRecord::DELETED_TEXT,
 				$user
 			)
@@ -360,12 +357,12 @@ class DeletedContribsPager extends IndexPager {
 				[ 'class' => 'mw-changeslist-date' ],
 				[
 					'target' => $page->getPrefixedText(),
-					'timestamp' => $rev->getTimestamp()
+					'timestamp' => $revRecord->getTimestamp()
 				]
 			);
 		}
 		// Style deleted items
-		if ( $rev->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
+		if ( $revRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 			$link = '<span class="history-deleted">' . $link . '</span>';
 		}
 
@@ -375,14 +372,14 @@ class DeletedContribsPager extends IndexPager {
 			[ 'class' => 'mw-changeslist-title' ]
 		);
 
-		if ( $rev->isMinor() ) {
+		if ( $revRecord->isMinor() ) {
 			$mflag = ChangesList::flag( 'minor' );
 		} else {
 			$mflag = '';
 		}
 
 		// Revision delete link
-		$del = Linker::getRevDeleteLink( $user, $rev->getRevisionRecord(), $page );
+		$del = Linker::getRevDeleteLink( $user, $revRecord, $page );
 		if ( $del ) {
 			$del .= ' ';
 		}
@@ -398,7 +395,7 @@ class DeletedContribsPager extends IndexPager {
 		$ret = "{$del}{$link} {$tools} {$separator} {$mflag} {$pagelink} {$comment}";
 
 		# Denote if username is redacted for this edit
-		if ( $rev->isDeleted( RevisionRecord::DELETED_USER ) ) {
+		if ( $revRecord->isDeleted( RevisionRecord::DELETED_USER ) ) {
 			$ret .= " <strong>" . $this->msg( 'rev-deleted-user-contribs' )->escaped() . "</strong>";
 		}
 

commit 9753db190925363f68f570a5207656b16958c224

RevisionItem: Fix providing timestamp in getRevisionLink
Caused by: 49707c59da2c4776b7752a5f2d5710f772c8d541

Bug: T252072
Change-Id: I801c6c07283f220ad5bac828d8204e600ba1db1f

includes/revisionlist/RevisionItem.php
@@ -96,7 +96,7 @@ class RevisionItem extends RevisionItemBase {
 	protected function getRevisionLink() {
 		$revRecord = $this->getRevisionRecord();
 		$date = $this->list->getLanguage()->userTimeAndDate(
-			$revRecord, $this->list->getUser() );
+			$revRecord->getTimestamp(), $this->list->getUser() );
 
 		if ( $this->isDeleted() && !$this->canViewContent() ) {
 			return htmlspecialchars( $date );