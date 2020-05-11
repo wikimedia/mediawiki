d9a1d95b6fae92b7b58db169d5f24a65080e0cc4
Tim Starling <tstarling@wikimedia.org>
2020-05-11 15:17:10 +1000

    Add HookRunner methods for new SearchHandler hooks
    Change-Id: I5313b57bf9136dd4808e92179bc7ffe6a07e13ec

--- includes/HookContainer/HookRunner.php
+++ includes/HookContainer/HookRunner.php
@@ -478,6 +478,8 @@ class HookRunner implements
 	\MediaWiki\ResourceLoader\Hook\ResourceLoaderSiteModulePagesHook,
 	\MediaWiki\ResourceLoader\Hook\ResourceLoaderSiteStylesModulePagesHook,
 	\MediaWiki\ResourceLoader\Hook\ResourceLoaderTestModulesHook,
+	\MediaWiki\Rest\Hook\SearchResultProvideDescriptionHook,
+	\MediaWiki\Rest\Hook\SearchResultProvideThumbnailHook,
 	\MediaWiki\Revision\Hook\ContentHandlerDefaultModelForHook,
 	\MediaWiki\Revision\Hook\RevisionInsertCompleteHook,
 	\MediaWiki\Revision\Hook\RevisionRecordInsertedHook,
@@ -3297,6 +3299,20 @@ class HookRunner implements
 		return $this->container->run(
 			'SearchResultInitFromTitle',
 			[ $title, &$id ]
+		);
+	}
+
+	public function onSearchResultProvideDescription( array $pageIdentities, &$descriptions ) {
+		return $this->container->run(
+			'SearchResultProvideDescription',
+			[ $pageIdentities, &$descriptions ]
+		);
+	}
+
+	public function onSearchResultProvideThumbnail( array $pageIdentities, &$thumbnails ) {
+		return $this->container->run(
+			'SearchResultProvideThumbnail',
+			[ $pageIdentities, &$thumbnails ]
 		);
 	}
 
