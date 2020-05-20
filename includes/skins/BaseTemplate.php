9ff8d6c96f6fad0b6d4f6325294ec6cc1de9de60
Derick A <alangiderick@gmail.com>
2020-05-20 23:08:45 +0100

    skins: Followup on I43da8e0fce878560bb692074578
    It's actually `->get()` and not `->getConfig()`.
    
    Change-Id: I63900273f150eb6280d16c8a3fea228d6131e144

--- includes/skins/BaseTemplate.php
+++ includes/skins/BaseTemplate.php
@@ -316,7 +316,8 @@ abstract class BaseTemplate extends QuickTemplate {
 	 * display the text from footericons instead of the images and don't want a
 	 * duplicate copyright statement because footerlinks already rendered one.
 	 * @param string|null $option
-	 * @deprecated 1.35 read FooterIcons from getConfig instead.
+	 * @deprecated 1.35 read footer icons from template data requested via
+	 *     $this->get('footericons')
 	 * @return array
 	 */
 	protected function getFooterIcons( $option = null ) {
