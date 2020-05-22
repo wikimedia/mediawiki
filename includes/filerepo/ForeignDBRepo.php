96ad0db6b482ee19208cab821407280c791048b1
Aaron Schulz <aschulz@wikimedia.org>
2020-05-22 14:17:01 -0700

    filerepo: use makeGlobalKey() in ForiegnDBViaLBRepo::getSharedCacheKey()
    Keys used by multiple wikis should use this key generation method.
    It also means settings like "coalesceKeys" in WANObjectCache will
    correctly identify these keys as being global.
    
    This change should be deployed on all wikis at once.
    
    Bug: T253405
    Change-Id: Ie19613ef7643af92aeb56db203e3d79139143231

--- includes/filerepo/ForeignDBRepo.php
+++ includes/filerepo/ForeignDBRepo.php
@@ -117,13 +117,6 @@ class ForeignDBRepo extends LocalRepo {
 		return $this->hasSharedCache;
 	}
 
-	/**
-	 * Get a key on the primary cache for this repository.
-	 * Returns false if the repository's cache is not accessible at this site.
-	 * The parameters are the parts of the key, as for wfMemcKey().
-	 * @param mixed ...$args
-	 * @return bool|mixed
-	 */
 	public function getSharedCacheKey( ...$args ) {
 		if ( $this->hasSharedCache() ) {
 			return $this->wanCache->makeGlobalKey(
