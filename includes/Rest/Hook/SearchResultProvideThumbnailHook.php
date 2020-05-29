590d8a9b062bbbcaa68b94c3141421de4517f040
apaskulin <apaskulin@wikimedia.org>
2020-05-29 11:10:32 -0700

    docs: Fix typo in doc comment
    Fixes a typo in the doc comment for two hook interfaces.
    
    Change-Id: I8b2075ea0029620baef4514439c22b727a1f9c42

--- includes/Rest/Hook/SearchResultProvideThumbnailHook.php
+++ includes/Rest/Hook/SearchResultProvideThumbnailHook.php
@@ -4,8 +4,8 @@ namespace MediaWiki\Rest\Hook;
 
 /**
  * Called by REST SearchHandler in order to allow extensions to fill the 'thumbnail'
- * field in rest search results. Warning: this hook as well as SearchResultPageIdentity
- * interface is being under development and still unstable.
+ * field in rest search results. Warning: this hook, as well as SearchResultPageIdentity
+ * interface, is under development and still unstable.
  *
  * @unstable
  * @ingroup Hooks
@@ -17,8 +17,8 @@ interface SearchResultProvideThumbnailHook {
 	 *
 	 * @since 1.35
 	 *
-	 * @param array $pageIdentities an array (string=>SearchResultPageIdentity) where key is pageId.
-	 * @param array &$thumbnails an output array (string=>SearchResultThumbnail|null) where key
+	 * @param array $pageIdentities Array (string=>SearchResultPageIdentity) where key is pageId
+	 * @param array &$thumbnails Output array (string=>SearchResultThumbnail|null) where key
 	 *   is pageId and value is either a valid SearchResultThumbnail for given page or null
 	 */
 	public function onSearchResultProvideThumbnail( array $pageIdentities, &$thumbnails );
