6363d641124314bdcf8721cc2a4fae021f9b1a8a
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-06-18 14:19:41 +0200

    parser: Add Title type hint to LinkHolderArray::makeHolder
    We *know* this can never be anything but a Title object:
    https://codesearch.wmflabs.org/search/?q=makeHolder
    
    Change-Id: Id6de0df627f2aeda79c6483f12a6d500ccd7853f

--- includes/parser/LinkHolderArray.php
+++ includes/parser/LinkHolderArray.php
@@ -135,35 +135,30 @@ class LinkHolderArray {
 	 * @param string $prefix [optional]
 	 * @return string
 	 */
-	public function makeHolder( $nt, $text = '', $query = [], $trail = '', $prefix = '' ) {
-		if ( !is_object( $nt ) ) {
-			# Fail gracefully
-			$retVal = "<!-- ERROR -->{$prefix}{$text}{$trail}";
-		} else {
-			# Separate the link trail from the rest of the link
-			list( $inside, $trail ) = Linker::splitTrail( $trail );
+	public function makeHolder( Title $nt, $text = '', $query = [], $trail = '', $prefix = '' ) {
+		# Separate the link trail from the rest of the link
+		list( $inside, $trail ) = Linker::splitTrail( $trail );
 
-			$key = $this->parent->nextLinkID();
-			$entry = [
-				'title' => $nt,
-				'text' => $prefix . $text . $inside,
-				'pdbk' => $nt->getPrefixedDBkey(),
-			];
-			if ( $query !== [] ) {
-				$entry['query'] = $query;
-			}
-
-			if ( $nt->isExternal() ) {
-				// Use a globally unique ID to keep the objects mergable
-				$this->interwikis[$key] = $entry;
-				$retVal = "<!--IWLINK'\" $key-->{$trail}";
-			} else {
-				$ns = $nt->getNamespace();
-				$this->internals[$ns][$key] = $entry;
-				$retVal = "<!--LINK'\" $ns:$key-->{$trail}";
-			}
-			$this->size++;
+		$key = $this->parent->nextLinkID();
+		$entry = [
+			'title' => $nt,
+			'text' => $prefix . $text . $inside,
+			'pdbk' => $nt->getPrefixedDBkey(),
+		];
+		if ( $query !== [] ) {
+			$entry['query'] = $query;
 		}
+
+		if ( $nt->isExternal() ) {
+			// Use a globally unique ID to keep the objects mergable
+			$this->interwikis[$key] = $entry;
+			$retVal = "<!--IWLINK'\" $key-->{$trail}";
+		} else {
+			$ns = $nt->getNamespace();
+			$this->internals[$ns][$key] = $entry;
+			$retVal = "<!--LINK'\" $ns:$key-->{$trail}";
+		}
+		$this->size++;
 		return $retVal;
 	}
 
