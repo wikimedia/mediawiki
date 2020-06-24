4093985c288da7892d0f279045bd5c4b1bb8a4d5
Clara Andrew-Wani <candrewwani@gmail.com>
2020-06-24 14:39:14 -0400

    Hard deprecate User::getNewtalk and User::setNewtalk
    Bug: T239640
    Change-Id: I7f901963ea67e8cdac1611838570ff9717d11232

--- tests/phpunit/includes/user/UserTest.php
+++ tests/phpunit/includes/user/UserTest.php
@@ -2736,6 +2736,8 @@ class UserTest extends MediaWikiTestCase {
 	}
 
 	private function doTestNewTalk( User $user ) {
+		$this->hideDeprecated( 'User::getNewtalk' );
+		$this->hideDeprecated( 'User::setNewtalk' );
 		$this->assertFalse( $user->getNewtalk(), 'Should be false before updated' );
 		$user->setNewtalk( true );
 		$this->assertTrue( $user->getNewtalk(), 'Should be true after updated' );
@@ -2770,6 +2772,7 @@ class UserTest extends MediaWikiTestCase {
 	public function testGetNewMessageLinks() {
 		$this->hideDeprecated( 'User::getNewMessageLinks' );
 		$this->hideDeprecated( 'User::getNewMessageRevisionId' );
+		$this->hideDeprecated( 'User::setNewtalk' );
 
 		$user = $this->getTestUser()->getUser();
 		$userTalk = $user->getTalkPage();
