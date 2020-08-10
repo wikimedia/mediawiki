wikimedia/mediawiki

commit 8acef095b9f23674e41ccc008ce00d1e9ef06594

Refactor SkinMustache and make template variables name consistent
These variables are renamed
* 'html-bodycontent' => 'html-body-content'
* 'html-undelete' => 'html-undelete-link'
* 'html-sitenotice' => 'html-site-notices'
* 'html-catlinks' =>  'html-categories'
* 'html-aftercontent' => 'html-after-content'
* 'html-userlanguageattributes' => 'html-user-language-attributes'

Others
* 'html-printfooter' is now subsumed into 'html-body-content'
* Factored out the bottom elements into a standalone method to
properly separate roles and make presentation a bit more clear:
 => headElement() -> body composition -> tailElement().

Bug: T257630
Change-Id: Ibac9729eaa96a38bc730a0132b102a894f6a172d

includes/skins/SkinMustache.php
@@ -51,48 +51,54 @@ class SkinMustache extends SkinTemplate {
 	 */
 	public function generateHTML() {
 		$this->setupTemplateContext();
-		$templateParser = $this->getTemplateParser();
-		return $templateParser->processTemplate(
-			$this->options['template'] ?? 'skin',
-			$this->getTemplateData()
-		);
+		$out = $this->getOutput();
+		$tp = $this->getTemplateParser();
+		$template = $this->options['template'] ?? 'skin';
+
+		$html = $out->headElement( $this );
+		$html .= $tp->processTemplate( $template, $this->getTemplateData() );
+		$html .= $this->tailElement( $out );
+
+		return $html;
 	}
 
 	/**
 	 * Subclasses may extend this method to add additional
 	 * template data.
 	 *
+	 * The data keys should be valid English words. Compound words should
+	 * be hypenated except if they are normally written as one word. Each
+	 * key should be prefixed with a type hint, this may be enforced by the
+	 * class PHPUnit test.
+	 *
+	 * Plain strings are prefixed with 'html-', plain arrays with 'array-'
+	 * and complex array data with 'data-'. 'is-' and 'has-' prefixes can
+	 * be used for boolean variables.
+	 *
 	 * @return array Data for a mustache template
 	 */
 	public function getTemplateData() {
 		$out = $this->getOutput();
-		$tail = [
-			MWDebug::getDebugHTML( $this->getContext() ),
-			$this->bottomScripts(),
-			wfReportTime( $out->getCSP()->getNonce() ),
-			MWDebug::getHTMLDebugLog() . '</body></html>',
-		];
+		$printSource = Html::rawElement( 'div', [ 'class' => 'printfooter' ], $this->printSource() );
+		$bodyContent = $out->getHTML() . "\n" . $printSource;
 
-		return [
+		$data = [
 			// Array objects
 			'array-indicators' => $this->getIndicatorsData( $out->getIndicators() ),
-
 			// Data objects
 			'data-search-box' => $this->buildSearchProps(),
-
 			// HTML strings
-			'html-headelement' => $out->headElement( $this ),
-			'html-sitenotice' => $this->getSiteNotice(),
+			'html-site-notices' => $this->getSiteNotice(),
 			'html-title' => $out->getPageTitle(),
 			'html-subtitle' => $this->prepareSubtitle(),
-			'html-bodycontent' => $this->wrapHTML( $out->getTitle(), $out->getHTML() ),
-			'html-catlinks' => $this->getCategories(),
-			'html-aftercontent' => $this->afterContentHook(),
-			'html-undelete' => $this->prepareUndeleteLink() ?: null,
-			'html-userlangattributes' => $this->prepareUserLanguageAttributes(),
-			'html-printfooter' => $this->printSource(),
-			'html-printtail' => WrappedStringList::join( "\n", $tail ),
+			'html-body-content' => $this->wrapHTML( $out->getTitle(), $bodyContent ),
+			'html-categories' => $this->getCategories(),
+			'html-after-content' => $this->afterContentHook(),
+			'html-undelete-link' => $this->prepareUndeleteLink(),
+			'html-user-language-attributes' => $this->prepareUserLanguageAttributes(),
 		];
+
+		return $data;
 	}
 
 	/**
@@ -100,6 +106,7 @@ class SkinMustache extends SkinTemplate {
 	 */
 	private function buildSearchProps() : array {
 		$config = $this->getConfig();
+
 		$props = [
 			'form-action' => $config->get( 'Script' ),
 			'html-button-search-fallback' => $this->makeSearchButton(
@@ -114,6 +121,27 @@ class SkinMustache extends SkinTemplate {
 			'msg-search' => $this->msg( 'search' )->text(),
 			'page-title' => SpecialPage::getTitleFor( 'Search' )->getPrefixedDBkey(),
 		];
+
 		return $props;
+	}
+
+	/**
+	 * The final bits that go to the bottom of a page
+	 * HTML document including the closing tags
+	 *
+	 * @param OutputPage $out
+	 * @return string
+	 */
+	private function tailElement( $out ) {
+		$tail = [
+			MWDebug::getDebugHTML( $this ),
+			$this->bottomScripts(),
+			wfReportTime( $out->getCSP()->getNonce() ),
+			MWDebug::getHTMLDebugLog()
+			. Html::closeElement( 'body' )
+			. Html::closeElement( 'html' )
+		];
+
+		return WrappedStringList::join( "\n", $tail );
 	}
 }

includes/skins/templates/apioutput/skin.mustache
@@ -1,8 +1,6 @@
-{{{html-headelement}}}
 <div class="mw-body" role="main">
 	<h1 id="firstHeading">{{{html-title}}}</h1>
 	<div class="mw-body-content">
-		{{{html-bodycontent}}}
+		{{{html-body-content}}}
 	</div>
 </div>
-{{{html-printtail}}}

includes/skins/templates/fallback/skin.mustache
@@ -1,4 +1,3 @@
-{{{html-headelement}}}
 {{{html-fallback-warning}}}
 {{#data-search-box}}
 <form action="{{form-action}}">
@@ -15,8 +14,7 @@
 	<h1 id="firstHeading">{{{html-title}}}</h1>
 	{{{html-subtitle}}}
 	<div class="mw-body-content">
-		{{{html-bodycontent}}}
-		{{{html-catlinks}}}
+		{{{html-body-content}}}
+		{{{html-categories}}}
 	</div>
 </div>
-{{{html-printtail}}}

commit ac9852ac04effdeac221cab1ffbfb00e244a858a

skins: Call headElement() after getTemplateData() in SkinMustache
OutputPage::headElement calls OutputPage::getRlClient, which should be
delayed for as long as practical, as it prevents the addition of any
further ResourceLoader modules. One such module is
ext.echo.styles.alert, which is added during a hook run during
SkinVector::getTemplateData.

Fixes regression introduced in 8acef095b9f23674e41ccc008ce00d1e9ef06594

Bug: T259872
Change-Id: Ibe1f07346cb19a7e40b00db3f72a615bb9cde6a5

includes/skins/SkinMustache.php
@@ -54,11 +54,15 @@ class SkinMustache extends SkinTemplate {
 		$out = $this->getOutput();
 		$tp = $this->getTemplateParser();
 		$template = $this->options['template'] ?? 'skin';
+		$data = $this->getTemplateData();
 
+		// T259955: OutputPage::headElement must be called last (after getTemplateData)
+		// as it calls OutputPage::getRlClient, which freezes the ResourceLoader
+		// modules queue for the current page load.
 		$html = $out->headElement( $this );
-		$html .= $tp->processTemplate( $template, $this->getTemplateData() );
-		$html .= $this->tailElement( $out );
 
+		$html .= $tp->processTemplate( $template, $data );
+		$html .= $this->tailElement( $out );
 		return $html;
 	}
 