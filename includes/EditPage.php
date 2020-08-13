f9b3c881d8716fba9b29606da3f2e7c2ff755665
Dayllan Maza <dmaza@wikimedia.org>
2020-08-13 00:13:05 -0400

    Fix race condition on edit page
    Remove WatchAction::doWatchOrUnwatch from DeferredUpdates so to avoid
    a possible race condition when the post-edit redirect happens if the
    pendingUpdates queue is too large to finish in time.
    
    This race condition was causing the "star" reflecting the watch state
    to display incorrectly (old state)
    
    Bug: T259564
    Change-Id: Ibba8b21c48df1efeccd5a52630c0842e25f7ff43
    (cherry picked from commit 65235005869d14d5e1668d3c92c0f4fca71dcaab)

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -2525,10 +2525,11 @@ ERROR;
 		$title = $this->mTitle;
 		$watch = $this->watchthis;
 		$watchlistExpiry = $this->watchlistExpiry;
-		// Do this in its own transaction to reduce contention...
-		DeferredUpdates::addCallableUpdate( function () use ( $user, $title, $watch, $watchlistExpiry ) {
-			WatchAction::doWatchOrUnwatch( $watch, $title, $user, $watchlistExpiry );
-		} );
+
+		// This can't run as a DeferredUpdate due to a possible race condition
+		// when the post-edit redirect happens if the pendingUpdates queue is
+		// too large to finish in time (T259564)
+		WatchAction::doWatchOrUnwatch( $watch, $title, $user, $watchlistExpiry );
 
 		// Add a job to purge expired watchlist items. Jobs will only be added at the rate
 		// specified by $wgWatchlistPurgeRate, which by default is every tenth edit.
