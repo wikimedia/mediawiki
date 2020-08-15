31dd3a9ebed62a9ae74af5ad7c37848c0055b2a7
Amir Sarabadani <Ladsgroup@gmail.com>
2020-08-15 21:01:57 +0200

    Migrate pagelinks table to abstract schema
    In order to migrate MySQL and Sqlite to abstract schema changed the
    pl_title data type from varchar binary to varbinary. This wouldn't
    affect production.
    
    For migrating Postgres:
     - Dropping foreign key on pl_from
     - Changing data type of pl_namespace from SMALLINT to INT and setting
       its default
     - Setting default of empty string for pl_title
     - Completely rewriting indexes to make it synced with MySQL
    
    Bug: T164898
    Bug: T230428
    Change-Id: I4af8202742a1826e6b3f8ff36cf4f7b612b82690

--- includes/installer/SqliteUpdater.php
+++ includes/installer/SqliteUpdater.php
@@ -298,6 +298,7 @@ class SqliteUpdater extends DatabaseUpdater {
 			[ 'modifyField', 'actor', 'actor_name', 'patch-actor-actor_name-varbinary.sql' ],
 			[ 'modifyField', 'content', 'content_id', 'patch-content-content_id-fix_not_null.sql' ],
 			[ 'modifyField', 'redirect', 'rd_title', 'patch-redirect-rd_title-varbinary.sql' ],
+			[ 'modifyField', 'pagelinks', 'pl_title', 'patch-pagelinks-pl_title-varbinary.sql' ],
 		];
 	}
 
