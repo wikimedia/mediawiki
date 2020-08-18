c0c8dad053411fd0fd2a2d81222b33b87eb23e7a
Ammar Abdulhamid <ammarpad@yahoo.com>
2020-08-18 12:51:48 +0100

    Deprecate Skin::makeVariablesScript()
    Bug: T257995
    Change-Id: I9789bb1a209a8f07cb2bb07140f2cc4fd778436a

--- includes/Hook/MakeGlobalVariablesScriptHook.php
+++ includes/Hook/MakeGlobalVariablesScriptHook.php
@@ -17,8 +17,7 @@ interface MakeGlobalVariablesScriptHook {
 	 *
 	 * @since 1.35
 	 *
-	 * @param array &$vars Variable (or multiple variables) to be added into the output of
-	 *   Skin::makeVariablesScript
+	 * @param array &$vars Variable (or multiple variables)
 	 * @param OutputPage $out OutputPage which called the hook, can be used to get the real title
 	 * @return bool|void True or no return value to continue or false to abort
 	 */
