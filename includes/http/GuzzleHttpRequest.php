b346501f7ef1d41012f92e340d517327c54aa91e
Reedy <reedy@wikimedia.org>
2020-09-14 02:08:52 +0100

    Updating guzzlehttp/guzzle (6.5.5 => 7.0.1)
    Change-Id: I02bdd522844c5174892bc8ead40b8f3dd550c624
    Depends-On: I2968abb84303737233d99249f57e212675013f4e

--- includes/http/GuzzleHttpRequest.php
+++ includes/http/GuzzleHttpRequest.php
@@ -235,7 +235,6 @@ class GuzzleHttpRequest extends MWHttpRequest {
 				}
 			}
 		} catch ( GuzzleHttp\Exception\GuzzleException $e ) {
-			// @phan-suppress-previous-line PhanRedefinedClassReference False positive
 			$this->status->fatal( 'http-internal-error' );
 		}
 
