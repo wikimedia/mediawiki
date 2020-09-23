deacee9088948e074722af0148000ad9455b07df
Ppchelko <ppchelko@wikimedia.org>
2020-09-23 23:12:02 +0000

    Revert "Revert "Hard deprecate all public properties in CacheTime and ParserOutput""
    This reverts commit a4dc6d82af8e64c72fe6ee321d226b83331c4f47.
    
    I've reverted the merged patch since I didn't do enough testing
    on serialized/reserialized ParserOutput and CacheTime. Now I'm
    confident serialization/deserialization works.
    
    Changes since original reverted version:
     - Use __get/__set instead of DeprecationHelper in order to
       avoid $deprecateProperties array to be serialized.
     - Add test for old format serialization new format deserialization.
    
    Change-Id: Ic911c2724ad709931d3316e609781fb89b5b7b28

--- includes/parser/CacheTime.php
+++ includes/parser/CacheTime.php
@@ -30,29 +30,68 @@ class CacheTime {
 	/**
 	 * @var string[] ParserOptions which have been taken into account to produce output.
 	 */
-	public $mUsedOptions;
+	private $mUsedOptions;
 
 	/**
-	 * @var string|null Compatibility check
+	 * @var true[] List of ParserOptions (stored in the keys).
 	 */
-	public $mVersion = Parser::VERSION;
+	protected $mAccessedOptions = [];
+
+	/**
+	 * @var string Compatibility check
+	 */
+	private $mVersion = Parser::VERSION;
 
 	/**
 	 * @var string|int TS_MW timestamp when this object was generated, or -1 for not cacheable. Used
 	 * in ParserCache.
 	 */
-	public $mCacheTime = '';
+	private $mCacheTime = '';
 
 	/**
 	 * @var int|null Seconds after which the object should expire, use 0 for not cacheable. Used in
 	 * ParserCache.
 	 */
-	public $mCacheExpiry = null;
+	private $mCacheExpiry = null;
 
 	/**
 	 * @var int|null Revision ID that was parsed
 	 */
-	public $mCacheRevisionId = null;
+	private $mCacheRevisionId = null;
+
+	/**
+	 * Deprecate access to all public properties.
+	 * Do not use deprecation helper to avoid serializaing
+	 * the list of deprecated methods.
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function __get( $name ) {
+		wfDeprecatedMsg(
+			'get ' . __CLASS__ . '::' . $name, '1.36' );
+		if ( property_exists( $this, $name ) ) {
+			return $this->$name;
+		} else {
+			trigger_error( 'Inaccessible property via __get(): ' . $name, E_USER_NOTICE );
+		}
+	}
+
+	/**
+	 * Deprecate access to all public properties.
+	 * Do not use deprecation helper to avoid serializaing
+	 * the list of deprecated methods.
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	public function __set( $name, $value ) {
+		wfDeprecatedMsg(
+			'set ' . __CLASS__ . '::' . $name, '1.36' );
+		if ( property_exists( $this, $name ) ) {
+			$this->$name = $value;
+		} else {
+			trigger_error( 'Inaccessible property via __set(): ' . $name, E_USER_NOTICE );
+		}
+	}
 
 	/**
 	 * @return string TS_MW timestamp
@@ -192,5 +231,33 @@ class CacheTime {
 	public function isDifferentRevision( $id ) {
 		$cached = $this->getCacheRevisionId();
 		return $cached !== null && $id !== $cached;
+	}
+
+	/**
+	 * Returns the options from its ParserOptions which have been taken
+	 * into account to produce this output.
+	 * @return string[]
+	 */
+	public function getUsedOptions() {
+		// If this is set, the CacheTime public property was used
+		// to set the field, thus nothing else could've set mAccessedOptions
+		if ( isset( $this->mUsedOptions ) ) {
+			return $this->mUsedOptions;
+		}
+
+		if ( !isset( $this->mAccessedOptions ) ) {
+			return [];
+		}
+		return array_keys( $this->mAccessedOptions );
+	}
+
+	/**
+	 * Sets the list of accessed ParserOptions which have been taken
+	 * into account to produce the output stored under this key.
+	 * @param array $options the list of options
+	 */
+	public function setUsedOptions( array $options ) {
+		$this->mAccessedOptions = array_flip( $options );
+		$this->mAccessedOptions = array_fill_keys( array_keys( $this->mAccessedOptions ), true );
 	}
 }
