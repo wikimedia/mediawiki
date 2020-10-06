c8de2ac751f497b887bf878fb4dd419810ef4fc3
Reedy <reedy@wikimedia.org>
2020-10-06 01:23:47 +0100

    Remove ipb_anon_only from ipb_address_unique index addition
    Should've been removed from these patches as part of the fix for
    T251188 in 39448a9cf6542883a89820c7fd0481c1c79d281e
    
    Bug: T264683
    Change-Id: I49f3ba2bec87d4a701f1370b8c67eac9f58629a7

--- maintenance/archives/patch-ipblocks-fix-ipb_address_unique.sql
+++ maintenance/archives/patch-ipblocks-fix-ipb_address_unique.sql
@@ -1,1 +1,1 @@
-ALTER TABLE /*_*/ipblocks DROP INDEX /*i*/ipb_address_unique, ADD UNIQUE INDEX /*i*/ipb_address_unique (ipb_address(255), ipb_user, ipb_auto );
+ALTER TABLE /*_*/ipblocks DROP INDEX /*i*/ipb_address_unique, ADD UNIQUE INDEX /*i*/ipb_address_unique (ipb_address(255), ipb_user, ipb_auto);
