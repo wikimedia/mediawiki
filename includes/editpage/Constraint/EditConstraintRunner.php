1aa3a1395c8dd0fcf8a16c2b7b223432ef65c6e8
DannyS712 <DannyS712.enwiki@gmail.com>
2020-10-09 02:26:20 +0000

    Introduce EditConstraint system, migrate unicode check to constraint
    This is the start of "Phase 1" of the migration described in
    T157658#6191234. For now, only a single constraint, the
    requirement that EditPage::$unicodeCheck match the expected
    value, is moved to the new system, in the UnicodeConstraint class.
    
    Both the EditConstraintRunner and the new UnicodeConstraint
    have 100% test coverage with pure unit tests.
    
    Bug: T157658
    Change-Id: I4fc806ec0fd631e265948a59244295eebec691fa

---
+++ includes/editpage/Constraint/EditConstraintRunner.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\EditPage\Constraint;
+
+use Wikimedia\Assert\Assert;
+
+/**
+ * Back end to process the edit constraints
+ *
+ * Constraints reflect possible errors that need to be checked
+ *
+ * @since 1.36
+ * @internal
+ * @author DannyS712
+ */
+class EditConstraintRunner {
+
+	/**
+	 * @var IEditConstraint[]
+	 *
+	 * Constraints to check.
+	 */
+	private $constraints = [];
+
+	/**
+	 * @var IEditConstraint|bool
+	 *
+	 * The constraint that failed, so that its status can be fetched, or false if none failed.
+	 */
+	private $failedConstraint = false;
+
+	/**
+	 * Create a new runner
+	 */
+	public function __construct() {
+		// TODO allow passing an array here as the starting constraints?
+	}
+
+	/**
+	 * Add a constraint to check
+	 *
+	 * Not all constraints are applicable to the action api or other methods of submitting
+	 * an edit
+	 *
+	 * For constraints that have dependencies, use the EditConstraintFactory (once that is created).
+	 *
+	 * @param IEditConstraint $constraint
+	 */
+	public function addConstraint( IEditConstraint $constraint ) {
+		$this->constraints[] = $constraint;
+	}
+
+	/**
+	 * Run constraint checks
+	 *
+	 * Returns true if all constraints pass, false otherwise.
+	 * Check getLegacyStatus for the reason
+	 *
+	 * @return bool
+	 */
+	public function checkConstraints() : bool {
+		foreach ( $this->constraints as $constraint ) {
+			if ( $constraint->checkConstraint() !== IEditConstraint::CONSTRAINT_PASSED ) {
+				$this->failedConstraint = $constraint;
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Get the constraint that failed
+	 *
+	 * @return IEditConstraint
+	 */
+	public function getFailedConstraint() : IEditConstraint {
+		Assert::precondition(
+			$this->failedConstraint !== false,
+			'getFailedConstraint called with no failed constraint'
+		);
+		return $this->failedConstraint;
+	}
+
+}
