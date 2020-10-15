7b734cfbff8f9c60e209ef1d08bfc2471f5dc95d
Umherirrender <umherirrender_de.wp@web.de>
2020-10-15 21:51:00 +0200

    Inject NamespaceInfo into SpecialStatistics
    This covers only directly used services by this special page
    Services used by the base class are not part of this patch set
    
    Bug: T259960
    Change-Id: Ib4f4f07a5583a3dbb0d4ad14bc14dd4e2e652b15

--- includes/specialpage/SpecialPageFactory.php
+++ includes/specialpage/SpecialPageFactory.php
@@ -308,7 +308,12 @@ class SpecialPageFactory {
 				'PermissionManager',
 			],
 		],
-		'Statistics' => \SpecialStatistics::class,
+		'Statistics' => [
+			'class' => \SpecialStatistics::class,
+			'services' => [
+				'NamespaceInfo',
+			]
+		],
 		'Allmessages' => [
 			'class' => \SpecialAllMessages::class,
 			'services' => [
