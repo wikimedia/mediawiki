ef2ad01d7b7a9a5b3df617ce4d904ff1d4b18c2f
Robert Vogel <vogel.robert@googlemail.com>
2020-10-21 11:41:03 +0200

    Delete 03-modify_git_submodules.sh
    REL1_30 of SyntaxHighlight_GeSHi is not compatible to MediaWiki 1.35+ anymore
    
    [3.3]
    
    ERM21228
    ERM21309

--- _bluespice/pre-autoload-dump.d/03-modify_git_submodules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-EXT_PATH_SYNTAXHIGHLIGHT="extensions/SyntaxHighlight_GeSHi"
-
-if [ -d $EXT_PATH_SYNTAXHIGHLIGHT ] ; then
-        cd $EXT_PATH_SYNTAXHIGHLIGHT && git checkout -b REL1_30 origin/REL1_30 && composer update --no-dev
-fi
