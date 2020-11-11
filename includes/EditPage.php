d165f7fbff1fe3ef33e2a65d668bbf869c99938f
Umherirrender <umherirrender_de.wp@web.de>
2020-11-11 22:52:45 +0100

    Replace deprecated WikiPage::factory in EditPage
    Change-Id: Ica08eb98ab382fca2a9917a935acd1f8a7356d31

--- includes/EditPage.php
+++ includes/EditPage.php
@@ -39,6 +39,7 @@ use MediaWiki\EditPage\TextConflictHelper;
 use MediaWiki\HookContainer\ProtectedHookAccessorTrait;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Page\WikiPageFactory;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\RevisionStore;
@@ -462,6 +463,11 @@ class EditPage implements IEditObject {
 	private $revisionStore;
 
 	/**
+	 * @var WikiPageFactory
+	 */
+	private $wikiPageFactory;
+
+	/**
 	 * @stable to call
 	 * @param Article $article
 	 */
@@ -492,6 +498,7 @@ class EditPage implements IEditObject {
 		$this->watchlistExpiryEnabled = $this->getContext()->getConfig() instanceof Config
 			&& $this->getContext()->getConfig()->get( 'WatchlistExpiry' );
 		$this->watchedItemStore = $services->getWatchedItemStore();
+		$this->wikiPageFactory = $services->getWikiPageFactory();
 
 		$this->deprecatePublicProperty( 'mBaseRevision', '1.35', __CLASS__ );
 	}
@@ -1607,7 +1614,7 @@ class EditPage implements IEditObject {
 			return $handler->makeEmptyContent();
 		}
 
-		$page = WikiPage::factory( $title );
+		$page = $this->wikiPageFactory->newFromTitle( $title );
 		if ( $page->isRedirect() ) {
 			$title = $page->getRedirectTarget();
 			# Same as before
@@ -1615,7 +1622,7 @@ class EditPage implements IEditObject {
 				// TODO: somehow show a warning to the user!
 				return $handler->makeEmptyContent();
 			}
-			$page = WikiPage::factory( $title );
+			$page = $this->wikiPageFactory->newFromTitle( $title );
 		}
 
 		$parserOptions = ParserOptions::newFromUser( $user );
