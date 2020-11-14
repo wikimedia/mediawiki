64ed5859538ad458cf5144cfb1aa35f19bb48474
Aaron Schulz <aschulz@wikimedia.org>
2020-11-13 16:14:02 -0800

    rdbms: cleanup erroneous and duplicated logging in LoadBalancer::waitForMasterPos()
    Previously, successful cases were spamming the logs with bogus errors. This was
    likely caused by some kind of incorrectly resolved merge conflict in the code.
    
    Bug: T221159
    Change-Id: I104a6154f8ad6c9a316a1a6c192d9a63a085663e

--- includes/libs/rdbms/loadbalancer/LoadBalancer.php
+++ includes/libs/rdbms/loadbalancer/LoadBalancer.php
@@ -2264,7 +2264,8 @@ class LoadBalancer implements ILoadBalancer {
 		}
 
 		if ( !$pos ) {
-			// Get the current master position, opening a connection if needed
+			// Get the current master position, opening a connection only if needed
+			$this->replLogger->debug( __METHOD__ . ': no position passed; using current' );
 			$index = $this->getWriterIndex();
 			$flags = self::CONN_SILENCE_ERRORS;
 			$masterConn = $this->getAnyOpenConnection( $index, $flags );
@@ -2284,24 +2285,13 @@ class LoadBalancer implements ILoadBalancer {
 		}
 
 		if ( $pos instanceof DBMasterPos ) {
-			$start = microtime( true );
+			$this->replLogger->debug( __METHOD__ . ': waiting' );
 			$result = $conn->masterPosWait( $pos, $timeout );
-			$seconds = max( microtime( true ) - $start, 0 );
-
 			$ok = ( $result !== null && $result != -1 );
 			if ( $ok ) {
-				$this->replLogger->warning(
-					__METHOD__ . ': timed out waiting on {dbserver} pos {pos} [{seconds}s]',
-					[
-						'dbserver' => $conn->getServer(),
-						'db_domain' => $conn->getDomainID(),
-						'pos' => $pos,
-						'seconds' => round( $seconds, 6 ),
-						'exception' => new RuntimeException(),
-					]
-				);
+				$this->replLogger->debug( __METHOD__ . ': done waiting (success)' );
 			} else {
-				$this->replLogger->debug( __METHOD__ . ': done waiting' );
+				$this->replLogger->debug( __METHOD__ . ': done waiting (failure)' );
 			}
 		} else {
 			$ok = false; // something is misconfigured
