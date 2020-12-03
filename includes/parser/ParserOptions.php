5413678f068c47325ce4e9c7c612746cf3c459a0
Petr Pchelko <ppchelko@wikimedia.org>
2020-12-02 21:38:25 -0600

    Record all used options in metadata.
    Bug: T269293
    Change-Id: I69ac24fcf8bf81de1d25c5a331b2155d9fe21977

--- includes/parser/ParserOptions.php
+++ includes/parser/ParserOptions.php
@@ -138,9 +138,7 @@ class ParserOptions {
 		}
 
 		$this->lazyLoadOption( $name );
-		if ( !empty( self::$inCacheKey[$name] ) ) {
-			$this->optionUsed( $name );
-		}
+		$this->optionUsed( $name );
 		return $this->options[$name];
 	}
 
