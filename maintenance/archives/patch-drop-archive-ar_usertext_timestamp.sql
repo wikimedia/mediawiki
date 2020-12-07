8e09dbe83ab092cecdbf6e7cbc17a3de315643f4
Reedy <reedy@wikimedia.org>
2020-12-07 03:19:38 +0000

    Split dropping of image.img_user_timestamp
    Splits the dropping of image.img_user_timestamp (which was added
    in MW 1.29) from patch-drop-image-img_user_timestamp.sql because
    if you're upgrading from a much older MW version, the index may
    not exist, and will cause the whole
    patch-drop-image-img_user_timestamp.sql patch to fail from the
    index not existing.
    
    It probably should have been added by the ifTableNotExists/actor
    entry with patch-image-user-index-2.sql (as actor wasn't added
    until 1.31)...
    
    Also fix up some comments relating to the patch filenames I
    missed whilst I split them in T257407.
    
    Bug: T267543
    Change-Id: I3cdecf6df459d44b4d51a9ad9e1ff00997ff0c02

--- maintenance/archives/patch-drop-archive-ar_usertext_timestamp.sql
+++ maintenance/archives/patch-drop-archive-ar_usertext_timestamp.sql
@@ -1,6 +1,6 @@
 -- T233221: The index on `archive` variously known as `ar_usertext_timestamp`
 -- and `usertext_timestamp` has a long and sordid history. We're dropping the
--- `ar_user_text` column entirely now (see patch-drop-user-fields.sql), but
+-- `ar_user_text` column entirely now (see patch-drop-archive-user-fields.sql), but
 -- this index needs special care thanks to said history.
 
 -- Do not use the /*i*/ thing here!
