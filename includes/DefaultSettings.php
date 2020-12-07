0b15729fca4f7d78a1e0aa3b75714c186402f4f3
DannyS712 <DannyS712.enwiki@gmail.com>
2020-12-07 23:26:17 +0000

    Add `Access-Control-Max-Age` to `$wgAllowedCorsHeaders`
    Bug: T269636
    Change-Id: Idfbb5a56a4b96b57a26e3e562dba3fd9c077b010
    (cherry picked from commit db8443c2a2c2eb40f7062cdc7ca791ef4a06d7a5)

--- includes/DefaultSettings.php
+++ includes/DefaultSettings.php
@@ -8760,6 +8760,8 @@ $wgAllowedCorsHeaders = [
 	/* MediaWiki whitelist */
 	'User-Agent',
 	'Api-User-Agent',
+	/* Allowing caching preflight requests, see T269636 */
+	'Access-Control-Max-Age',
 ];
 
 /**
