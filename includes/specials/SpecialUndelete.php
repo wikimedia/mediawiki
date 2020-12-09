1473989a35acd941c2ac15317f355eba676961ee
DannyS712 <DannyS712.enwiki@gmail.com>
2020-12-09 20:07:06 +0000

    Merge mediawiki.special.undelete into mediawiki.misc-authed-ooui
    Keep the former as a deprecated alias for now
    
    Change-Id: Ib6fb6493ad5f7b2f3aae5607a5a94c0fd0738a0a

--- includes/specials/SpecialUndelete.php
+++ includes/specials/SpecialUndelete.php
@@ -849,7 +849,7 @@ class SpecialUndelete extends SpecialPage {
 
 		$out = $this->getOutput();
 		if ( $this->mAllowed ) {
-			$out->addModules( 'mediawiki.special.undelete' );
+			$out->addModules( 'mediawiki.misc-authed-ooui' );
 		}
 		$out->wrapWikiMsg(
 			"<div class='mw-undelete-pagetitle'>\n$1\n</div>\n",
