1adaca51c3e6e648bda79323b00156c6748a6bb7
Ammar Abdulhamid <ammarpad@yahoo.com>
2020-12-21 05:09:08 +0100

    Rename change_tag indexes to have ct_ prefix
    Bug: T270033
    Change-Id: I8a429726c99f6cadea0d671fd871f66b5611c856

--- maintenance/postgres/tables-generated.sql
+++ maintenance/postgres/tables-generated.sql
@@ -117,13 +117,13 @@ CREATE TABLE change_tag (
   PRIMARY KEY(ct_id)
 );
 
-CREATE UNIQUE INDEX change_tag_rc_tag_id ON change_tag (ct_rc_id, ct_tag_id);
+CREATE UNIQUE INDEX ct_rc_tag_id ON change_tag (ct_rc_id, ct_tag_id);
 
-CREATE UNIQUE INDEX change_tag_log_tag_id ON change_tag (ct_log_id, ct_tag_id);
+CREATE UNIQUE INDEX ct_log_tag_id ON change_tag (ct_log_id, ct_tag_id);
 
-CREATE UNIQUE INDEX change_tag_rev_tag_id ON change_tag (ct_rev_id, ct_tag_id);
+CREATE UNIQUE INDEX ct_rev_tag_id ON change_tag (ct_rev_id, ct_tag_id);
 
-CREATE INDEX change_tag_tag_id_id ON change_tag (
+CREATE INDEX ct_tag_id_id ON change_tag (
   ct_tag_id, ct_rc_id, ct_rev_id, ct_log_id
 );
 
