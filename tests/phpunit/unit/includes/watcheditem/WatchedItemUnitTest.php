9188f6cf7a5b4235a697fcf165f2c8cb9690f72d
DannyS712 <DannyS712.enwiki@gmail.com>
2021-01-10 02:45:54 +0000

    Make WatchedItemUnitTest a pure unit test
    Change-Id: If390e61d4d4800b1d20a5d0bec87f9aa7ad83e10

--- tests/phpunit/unit/includes/watcheditem/WatchedItemUnitTest.php
+++ tests/phpunit/unit/includes/watcheditem/WatchedItemUnitTest.php
@@ -6,7 +6,7 @@ use Wikimedia\Timestamp\ConvertibleTimestamp;
 /**
  * @covers WatchedItem
  */
-class WatchedItemUnitTest extends MediaWikiIntegrationTestCase {
+class WatchedItemUnitTest extends MediaWikiUnitTestCase {
 
 	public function testIsExpired() {
 		$user = new UserIdentityValue( 7, 'MockUser', 0 );
@@ -58,41 +58,53 @@ class WatchedItemUnitTest extends MediaWikiIntegrationTestCase {
 	public function testgetExpiryInDaysText() {
 		$user = new UserIdentityValue( 7, 'MockUser', 0 );
 		$target = new TitleValue( 0, 'SomeDbKey' );
-		$context = RequestContext::getMain();
+
+		$messageLocalizer = $this->createMock( MessageLocalizer::class );
+		$messageLocalizer->method( 'msg' )->willReturnCallback(
+			function ( $key, ...$params ) {
+				// Right now we aren't worrying about the message formatting,
+				// just testing if the correct parameter is there by adding it
+				// to the end. Use MediaWikiTestCaseTrait::getMockMessage
+				return $this->getMockMessage(
+					$params ? ( $key . '-' . $params[0][0] ) : $key
+				);
+			}
+		);
 
 		// Fake current time to be 2020-05-27T00:00:00Z
 		$fakeTime = ConvertibleTimestamp::setFakeTime( '20200527000000' );
 
 		// Adding a watched item with an expiry of a month from the frozen time
 		$watchedItemMonth = new WatchedItem( $user, $target, null, '20200627000000' );
-		$daysRemainingMonthText = $watchedItemMonth->getExpiryInDaysText( $context );
-		$this->assertSame( '31 days left in your watchlist', $daysRemainingMonthText );
+		$daysRemainingMonthText = $watchedItemMonth->getExpiryInDaysText( $messageLocalizer );
+		$this->assertSame( 'watchlist-expiring-days-full-text-31', $daysRemainingMonthText );
 
 		// Adding a watched item with an expiry of 7 days from the frozen time
 		$watchedItemWeek = new WatchedItem( $user, $target, null, '20200603000000' );
-		$daysRemainingWeekText = $watchedItemWeek->getExpiryInDaysText( $context );
-		$this->assertSame( '7 days left in your watchlist', $daysRemainingWeekText );
+		$daysRemainingWeekText = $watchedItemWeek->getExpiryInDaysText( $messageLocalizer );
+		$this->assertSame( 'watchlist-expiring-days-full-text-7', $daysRemainingWeekText );
 
 		// Show a watched item with an expiry of 7 days from the frozen time in a dropdown option
-		$daysRemainingWeekText = $watchedItemWeek->getExpiryInDaysText( $context, true );
-		$this->assertSame( '7 days left', $daysRemainingWeekText );
+		$daysRemainingWeekText = $watchedItemWeek->getExpiryInDaysText( $messageLocalizer, true );
+		$this->assertSame( 'watchlist-expiry-days-left-7', $daysRemainingWeekText );
 
 		// Adding a watched item with an expiry of 1 day from the frozen time
 		$watchedItemDay = new WatchedItem( $user, $target, null, '20200528000000' );
-		$daysRemainingDayText = $watchedItemDay->getExpiryInDaysText( $context );
-		$this->assertSame( '1 day left in your watchlist', $daysRemainingDayText );
+		$daysRemainingDayText = $watchedItemDay->getExpiryInDaysText( $messageLocalizer );
+		$this->assertSame( 'watchlist-expiring-days-full-text-1', $daysRemainingDayText );
 
 		// Adding a watched item with an expiry in less than 1 day from the frozen time
 		$watchedItemSoon = new WatchedItem( $user, $target, null, '20200527010001' );
-		$daysRemainingSoonText = $watchedItemSoon->getExpiryInDaysText( $context );
-		$this->assertSame( 'A few hours left in your watchlist', $daysRemainingSoonText );
+		$daysRemainingSoonText = $watchedItemSoon->getExpiryInDaysText( $messageLocalizer );
+		$this->assertSame( 'watchlist-expiring-hours-full-text', $daysRemainingSoonText );
+
 		// Text is for a watchlist expiry dropdown
-		$daysRemainingSoonText = $watchedItemSoon->getExpiryInDaysText( $context, true );
-		$this->assertSame( 'A few hours left', $daysRemainingSoonText );
+		$daysRemainingSoonText = $watchedItemSoon->getExpiryInDaysText( $messageLocalizer, true );
+		$this->assertSame( 'watchlist-expiry-hours-left', $daysRemainingSoonText );
 
 		// Adding a watched item with no expiry
 		$permamentlyWatchedItem = new WatchedItem( $user, $target, null, null );
-		$daysText = $permamentlyWatchedItem->getExpiryInDaysText( $context );
+		$daysText = $permamentlyWatchedItem->getExpiryInDaysText( $messageLocalizer );
 		$this->assertSame( '', $daysText );
 	}
 
