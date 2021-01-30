57ef9fcaf4a4bb33e03711c58fe2ffd6ff8b812e
Ammarpad <ammarpad@yahoo.com>
2021-01-30 11:23:23 +0100

    ApiResult: Avoid huge conditional clause
    Huge `if` clause is almost always bad. It impacts code readability and
    requires a lot of scrolling (both mental and physical) to see the `else`
    block and make sense of everything together.
    
    Use early return to improve the code readability. The functionality of
    the logic remains the same.
    
    Change-Id: Ibdb706cd35a5fbd219c042a882326c5b21cd18ba

--- includes/api/ApiResult.php
+++ includes/api/ApiResult.php
@@ -902,94 +902,94 @@ class ApiResult implements ApiSerializable {
 				throw new InvalidArgumentException( __METHOD__ . ': Unknown value for "Strip"' );
 		}
 
-		// Type transformation
-		if ( $transformTypes !== null ) {
-			if ( $defaultType === 'array' && $maxKey !== count( $data ) - 1 ) {
-				$defaultType = 'assoc';
-			}
-
-			// Override type, if provided
-			$type = $defaultType;
-			if ( isset( $metadata[self::META_TYPE] ) && $metadata[self::META_TYPE] !== 'default' ) {
-				$type = $metadata[self::META_TYPE];
-			}
-			if ( ( $type === 'kvp' || $type === 'BCkvp' ) &&
-				empty( $transformTypes['ArmorKVP'] )
-			) {
-				$type = 'assoc';
-			} elseif ( $type === 'BCarray' ) {
-				$type = 'array';
-			} elseif ( $type === 'BCassoc' ) {
-				$type = 'assoc';
-			}
-
-			// Apply transformation
-			switch ( $type ) {
-				case 'assoc':
-					$metadata[self::META_TYPE] = 'assoc';
-					$data += $keepMetadata;
-					return empty( $transformTypes['AssocAsObject'] ) ? $data : (object)$data;
-
-				case 'array':
-					ksort( $data );
-					$data = array_values( $data );
-					$metadata[self::META_TYPE] = 'array';
-					return $data + $keepMetadata;
-
-				case 'kvp':
-				case 'BCkvp':
-					$key = $metadata[self::META_KVP_KEY_NAME] ?? $transformTypes['ArmorKVP'];
-					$valKey = isset( $transforms['BC'] ) ? '*' : 'value';
-					$assocAsObject = !empty( $transformTypes['AssocAsObject'] );
-					$merge = !empty( $metadata[self::META_KVP_MERGE] );
-
-					$ret = [];
-					foreach ( $data as $k => $v ) {
-						if ( $merge && ( is_array( $v ) || is_object( $v ) ) ) {
-							$vArr = (array)$v;
-							if ( isset( $vArr[self::META_TYPE] ) ) {
-								$mergeType = $vArr[self::META_TYPE];
-							} elseif ( is_object( $v ) ) {
-								$mergeType = 'assoc';
-							} else {
-								$keys = array_keys( $vArr );
-								sort( $keys, SORT_NUMERIC );
-								$mergeType = ( $keys === array_keys( $keys ) ) ? 'array' : 'assoc';
-							}
-						} else {
-							$mergeType = 'n/a';
-						}
-						if ( $mergeType === 'assoc' ) {
-							$item = $vArr + [
-								$key => $k,
-							];
-							if ( $strip === 'none' ) {
-								self::setPreserveKeysList( $item, [ $key ] );
-							}
-						} else {
-							$item = [
-								$key => $k,
-								$valKey => $v,
-							];
-							if ( $strip === 'none' ) {
-								$item += [
-									self::META_PRESERVE_KEYS => [ $key ],
-									self::META_CONTENT => $valKey,
-									self::META_TYPE => 'assoc',
-								];
-							}
-						}
-						$ret[] = $assocAsObject ? (object)$item : $item;
-					}
-					$metadata[self::META_TYPE] = 'array';
-
-					return $ret + $keepMetadata;
-
-				default:
-					throw new UnexpectedValueException( "Unknown type '$type'" );
-			}
-		} else {
+		// No type transformation
+		if ( $transformTypes === null ) {
 			return $data + $keepMetadata;
+		}
+
+		if ( $defaultType === 'array' && $maxKey !== count( $data ) - 1 ) {
+			$defaultType = 'assoc';
+		}
+
+		// Override type, if provided
+		$type = $defaultType;
+		if ( isset( $metadata[self::META_TYPE] ) && $metadata[self::META_TYPE] !== 'default' ) {
+			$type = $metadata[self::META_TYPE];
+		}
+		if ( ( $type === 'kvp' || $type === 'BCkvp' ) &&
+			empty( $transformTypes['ArmorKVP'] )
+		) {
+			$type = 'assoc';
+		} elseif ( $type === 'BCarray' ) {
+			$type = 'array';
+		} elseif ( $type === 'BCassoc' ) {
+			$type = 'assoc';
+		}
+
+		// Apply transformation
+		switch ( $type ) {
+			case 'assoc':
+				$metadata[self::META_TYPE] = 'assoc';
+				$data += $keepMetadata;
+				return empty( $transformTypes['AssocAsObject'] ) ? $data : (object)$data;
+
+			case 'array':
+				ksort( $data );
+				$data = array_values( $data );
+				$metadata[self::META_TYPE] = 'array';
+				return $data + $keepMetadata;
+
+			case 'kvp':
+			case 'BCkvp':
+				$key = $metadata[self::META_KVP_KEY_NAME] ?? $transformTypes['ArmorKVP'];
+				$valKey = isset( $transforms['BC'] ) ? '*' : 'value';
+				$assocAsObject = !empty( $transformTypes['AssocAsObject'] );
+				$merge = !empty( $metadata[self::META_KVP_MERGE] );
+
+				$ret = [];
+				foreach ( $data as $k => $v ) {
+					if ( $merge && ( is_array( $v ) || is_object( $v ) ) ) {
+						$vArr = (array)$v;
+						if ( isset( $vArr[self::META_TYPE] ) ) {
+							$mergeType = $vArr[self::META_TYPE];
+						} elseif ( is_object( $v ) ) {
+							$mergeType = 'assoc';
+						} else {
+							$keys = array_keys( $vArr );
+							sort( $keys, SORT_NUMERIC );
+							$mergeType = ( $keys === array_keys( $keys ) ) ? 'array' : 'assoc';
+						}
+					} else {
+						$mergeType = 'n/a';
+					}
+					if ( $mergeType === 'assoc' ) {
+						$item = $vArr + [
+							$key => $k,
+						];
+						if ( $strip === 'none' ) {
+							self::setPreserveKeysList( $item, [ $key ] );
+						}
+					} else {
+						$item = [
+							$key => $k,
+							$valKey => $v,
+						];
+						if ( $strip === 'none' ) {
+							$item += [
+								self::META_PRESERVE_KEYS => [ $key ],
+								self::META_CONTENT => $valKey,
+								self::META_TYPE => 'assoc',
+							];
+						}
+					}
+					$ret[] = $assocAsObject ? (object)$item : $item;
+				}
+				$metadata[self::META_TYPE] = 'array';
+
+				return $ret + $keepMetadata;
+
+			default:
+				throw new UnexpectedValueException( "Unknown type '$type'" );
 		}
 	}
 
