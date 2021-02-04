4209079e3326d4e21f8d312191179f8f305d2236
Ammarpad <ammarpad@yahoo.com>
2021-02-04 15:21:58 +0100

    Drop 1.11 and 1.12 upgrade patch files
    7 files for 1.11, 1 file for 1.2. No corresponding files for
    Postgres or Sqlite
    
    Bug: T272199
    Change-Id: I40afee1b8ecaa27a083d6de7ef9ee5347025754d

--- maintenance/archives/patch-image-user-index.sql
+++
@@ -1,8 +0,0 @@
---
--- image-user-index.sql
---
--- Add user_text/timestamp index to current image versions
---
-
-ALTER TABLE /*$wgDBprefix*/image
-   ADD INDEX img_usertext_timestamp (img_user_text,img_timestamp);
