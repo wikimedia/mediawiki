00d22d1b68758b6b5beca34d8d9718dae7a3427b
Ammarpad <ammarpad@yahoo.com>
2021-02-13 08:15:04 +0100

    Drop 1.24 upgrade patch files
    11 files for Mysql and Sqlite (Some shared)
    
    Bug: T272199
    Change-Id: I76b4235f13aef0674ed6b35c2a97edf6a4573d94

--- maintenance/archives/patch-tl_from_namespace.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE /*_*/templatelinks
-	ADD COLUMN tl_from_namespace int NOT NULL default 0;
-
-CREATE INDEX /*i*/tl_backlinks_namespace ON /*_*/templatelinks (tl_from_namespace,tl_namespace,tl_title,tl_from);
