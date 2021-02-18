115410f077e091c79ce06f67cf748402840d42e9
C. Scott Ananian <cscott@cscott.net>
2021-02-18 13:09:17 -0500

    Parser::__construct(): Remove deprecated argument variants
    A number of different argument variants were deprecated in 1.34,
    and direct calls to the Parser constructor were deprecated at the
    same time (a ParserFactory should be used instead).  These were
    hard-deprecated in 1.35.  They should be safe to remove now.
    
    Code search:
    https://codesearch.wmcloud.org/deployed/?q=new%20Parser%5C%28&i=nope&files=%5C.php%24&excludeFiles=&repos=
    
    Bug: T236811
    Change-Id: I58f7b3ba1b1d62851b2db71197a8d9129e8d473d

--- tests/phpunit/includes/parser/ParserTest.php
+++ tests/phpunit/includes/parser/ParserTest.php
@@ -4,7 +4,11 @@
  * @covers Parser::__construct
  */
 class ParserTest extends MediaWikiIntegrationTestCase {
-	public function provideConstructorArguments() {
+	/**
+	 * Helper method to create mocks
+	 * @return array
+	 */
+	private function createConstructorArguments() {
 		// Create a mock Config object that will satisfy ServiceOptions::__construct
 		$mockConfig = $this->createMock( Config::class );
 		$mockConfig->method( 'has' )->willReturn( true );
@@ -32,7 +36,7 @@ class ParserTest extends MediaWikiIntegrationTestCase {
 		$mwFactory
 			->method( 'getVariableIDs' )->willReturn( [] );
 
-		$newArgs = [
+		return [
 			$this->createMock( MediaWiki\Config\ServiceOptions::class ),
 			$mwFactory,
 			$this->createMock( Language::class ),
@@ -47,72 +51,23 @@ class ParserTest extends MediaWikiIntegrationTestCase {
 			$this->createMock( MediaWiki\HookContainer\HookContainer::class ),
 			$this->createMock( MediaWiki\Tidy\TidyDriverBase::class ),
 		];
-
-		$oldArgs = [
-			[],
-			$mwFactory,
-			$this->createMock( Language::class ),
-			$this->createMock( ParserFactory::class ),
-			'a snail can sleep for three years',
-			$this->createMock( MediaWiki\Special\SpecialPageFactory::class )
-		];
-
-		yield 'current_args' => [
-			$newArgs,
-		];
-
-		yield 'backward_compatible_args_minimal' => [
-			array_merge( $oldArgs ),
-		];
-
-		yield 'backward_compatible_args_with_config' => [
-			array_merge( $oldArgs, [ $mockConfig ] ),
-		];
-
-		yield 'backward_compatible_args_with_link_renderer' => [
-			array_merge( $oldArgs, [
-				$mockConfig,
-				$this->createMock( MediaWiki\Linker\LinkRendererFactory::class )
-			] ),
-		];
-
-		yield 'backward_compatible_args_with_ns_info' => [
-			array_merge( $oldArgs, [
-				$mockConfig,
-				$this->createMock( MediaWiki\Linker\LinkRendererFactory::class ),
-				$this->createMock( NamespaceInfo::class )
-			] ),
-		];
 	}
 
 	/**
-	 * @dataProvider provideConstructorArguments
 	 * @covers Parser::__construct
 	 */
-	public function testBackwardsCompatibleConstructorArguments( $args ) {
-		$this->hideDeprecated( 'Parser::__construct' );
-		$this->hideDeprecated( 'old calling convention for Parser::__construct' );
-		$parser = new Parser( ...$args );
+	public function testConstructorArguments() {
+		$args = $this->createConstructorArguments();
 
-		$refObject = new ReflectionObject( $parser );
-
-		// If testing backwards compatibility, test service options separately
-		if ( is_array( $args[0] ) ) {
-			$svcOptionsProp = $refObject->getProperty( 'svcOptions' );
-			$svcOptionsProp->setAccessible( true );
-			$this->assertInstanceOf(
-				MediaWiki\Config\ServiceOptions::class,
-				$svcOptionsProp->getValue( $parser )
-			);
-			unset( $args[0] );
-
-			// If a Config is passed, the fact that we were able to create a ServiceOptions
-			// instance without error from it proves that this argument works.
-			if ( isset( $args[6] ) ) {
-				unset( $args[6] );
-			}
+		// Fool Parser into thinking we are constructing via a ParserFactory
+		ParserFactory::$inParserFactory += 1;
+		try {
+			$parser = new Parser( ...$args );
+		} finally {
+			ParserFactory::$inParserFactory -= 1;
 		}
 
+		$refObject = new ReflectionObject( $parser );
 		foreach ( $refObject->getProperties() as $prop ) {
 			$prop->setAccessible( true );
 			foreach ( $args as $idx => $mockTest ) {
