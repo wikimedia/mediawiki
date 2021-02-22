wikimedia/mediawiki

commit c12af6e1682b1f9a86214ac053343adbc4d62d3b

Normalize and de-dupe usernames in HTMLUsersMultiselectField
In no-js, there is no on the fly normalization of usernames in
HTMLUsersMultiselectField so both "User A" and "User_A" are
valid representations of "User A" (the canonical representation).

It's also possible to add the same user multiple times with no-js
and this will be considered valid and count toward the max limit.

These are not problems with js enabled since there will be an api
call for every new entry and that call both filters for selected users
and only returns canonical names.

This patchset reproduces that functionality in the PHP layer so that
no-js functions like the infused widget.

Bug: T274568
Change-Id: Ie78c8f37fa8a38b67eeaa6de098e41df2dac3e3e

includes/htmlform/fields/HTMLUsersMultiselectField.php
@@ -24,8 +24,19 @@ class HTMLUsersMultiselectField extends HTMLUserTextField {
 		$usersArray = array_values( array_filter( $usersArray, static function ( $username ) {
 			return trim( $username ) !== '';
 		} ) );
+
+		// Normalize usernames
+		$normalizedUsers = [];
+		foreach ( $usersArray as $user ) {
+			$canonicalUser = User::getCanonicalName( $user );
+			$normalizedUsers[] = $canonicalUser;
+		}
+
+		// Remove any duplicate usernames
+		$uniqueUsers = array_unique( $normalizedUsers );
+
 		// This function is expected to return a string
-		return implode( "\n", $usersArray );
+		return implode( "\n", $uniqueUsers );
 	}
 
 	public function validate( $value, $alldata ) {

commit 505433c5028ad054688520ca8425244c034856f1

HTMLUsersMultiselectField: Keep IP addresses while normalizing
Fixes a bug introduced in c12af6e1682b1f9a86214ac053343adbc4d62d3b
where invalid usernames are normalized out.

Nonexistent usernames will still fail the validation step, including
IP addresses if the field has been set up not to accept them.

Bug: T274568
Change-Id: I229afdfff2144fd4db8d49825262010f58f1fe54

includes/htmlform/fields/HTMLUsersMultiselectField.php
@@ -28,7 +28,7 @@ class HTMLUsersMultiselectField extends HTMLUserTextField {
 		// Normalize usernames
 		$normalizedUsers = [];
 		foreach ( $usersArray as $user ) {
-			$canonicalUser = User::getCanonicalName( $user );
+			$canonicalUser = User::getCanonicalName( $user, false );
 			$normalizedUsers[] = $canonicalUser;
 		}
 