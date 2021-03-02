07b05d3db9b03243d6f27be57acf350c96fbc7fd
Amir Sarabadani <Ladsgroup@gmail.com>
2021-03-02 22:15:14 +0100

    Drop unused patch-userlevels.sql patch
    This patch was marked as obsolete and removed from updater in r9367
    (2005) and any mention of it was removed from documentation in r69187
    (2010). Since then, you can't find any mention of this file.
    
    Change-Id: I3a45f4661434737fa977c721ba6cf74864984d6a

--- maintenance/archives/patch-userlevels.sql
+++
@@ -1,8 +0,0 @@
-
--- Relation table between user and groups
-CREATE TABLE /*$wgDBprefix*/user_groups (
-	ug_user int unsigned NOT NULL default '0',
-	ug_group varbinary(16) NOT NULL default '0',
-	PRIMARY KEY  (ug_user,ug_group)
-  KEY (ug_group)
-) /*$wgDBTableOptions*/;
