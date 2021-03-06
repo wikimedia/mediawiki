a0d6a4ccd1161bb3bc302388141b98e064f336c0
Ammarpad <ammarpad@yahoo.com>
2021-03-06 09:20:04 +0100

    wfDeprecated: Use InvalidArgumentException for invalid arguments
    Change-Id: I0bf84810cee8c8aa4ca15b7f94fa9467a52b0278

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -1028,14 +1028,14 @@ function wfLogDBError( $text, array $context = [] ) {
  *    caller. 2 = function that called the function that called
  *    wfDeprecated (Added in 1.20).
  *
- * @throws Exception If the MediaWiki version number specified by $version is neither a string nor
- *    false, an exception is thrown to indicate invalid arguments.
+ * @throws InvalidArgumentException If the MediaWiki version
+ *     number specified by $version is neither a string nor false.
  */
 function wfDeprecated( $function, $version = false, $component = false, $callerOffset = 2 ) {
 	if ( is_string( $version ) || $version === false ) {
 		MWDebug::deprecated( $function, $version, $component, $callerOffset + 1 );
 	} else {
-		throw new Exception(
+		throw new InvalidArgumentException(
 			"MediaWiki version must either be a string or false. " .
 			"Example valid version: '1.33'"
 		);
