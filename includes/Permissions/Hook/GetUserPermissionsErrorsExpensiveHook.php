17fdcffc7fc627b83981429a21cc4e96d801ab09
Umherirrender <umherirrender_de.wp@web.de>
2021-03-07 12:30:27 +0100

    Fix doc for hook error in GetUserPermissionsErrorsExpensiveHook
    It allows all from PermissionManager::resultToError,
    but the false is not needed to document as b/c
    
    Change-Id: Ica08934bf8a5c76e6a0f70c5617f50bfe7f2d6e0

--- includes/Permissions/Hook/GetUserPermissionsErrorsExpensiveHook.php
+++ includes/Permissions/Hook/GetUserPermissionsErrorsExpensiveHook.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Permissions\Hook;
 
+use MessageSpecifier;
 use Title;
 use User;
 
@@ -26,7 +27,7 @@ interface GetUserPermissionsErrorsExpensiveHook {
 	 * @param Title $title Title being checked against
 	 * @param User $user Current user
 	 * @param string $action Action being checked
-	 * @param string &$result User permissions error to add. If none, return true.
+	 * @param array|string|MessageSpecifier &$result User permissions error to add. If none, return true.
 	 *   For consistency, error messages should be plain text with no special coloring,
 	 *   bolding, etc. to show that they're errors; presenting them properly to the
 	 *   user as errors is done by the caller.
