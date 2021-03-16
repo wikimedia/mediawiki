54c9b830a43116160c5521a3148265993ff0fe52
ZabeMath <alec@vc-celle.de>
2021-03-16 22:28:33 +0100

    Hard deprecate User::getFirstEditTimestamp() and ::getLatestEditTimestamp()
    User::getFirstEditTimestamp() and User::getLatestEditTimestamp() are soft deprecated and unused and thus should be hard deprecated.
    
    Bug: T274211
    Change-Id: Ib2da53256d7290f6d9a768c0c0708ab9e8e072ed

--- includes/user/User.php
+++ includes/user/User.php
@@ -4129,6 +4129,7 @@ class User implements Authority, IDBAccessObject, UserIdentity {
 	 *  non-existent/anonymous user accounts.
 	 */
 	public function getFirstEditTimestamp() {
+		wfDeprecated( __METHOD__, '1.36' );
 		return MediaWikiServices::getInstance()
 			->getUserEditTracker()
 			->getFirstEditTimestamp( $this );
@@ -4144,6 +4145,7 @@ class User implements Authority, IDBAccessObject, UserIdentity {
 	 *  non-existent/anonymous user accounts.
 	 */
 	public function getLatestEditTimestamp() {
+		wfDeprecated( __METHOD__, '1.36' );
 		return MediaWikiServices::getInstance()
 			->getUserEditTracker()
 			->getLatestEditTimestamp( $this );
