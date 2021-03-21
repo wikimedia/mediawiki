5954b380fc865c662e5887ecd78a8b25bf54f3c1
Ammarpad <ammarpad@yahoo.com>
2021-03-21 16:32:35 +0100

    Migrate revision table to abstract schema
    Postgres:
     - Drop foreign key from rev_page
     - Make rev_page not nullable
     - Change rev_comment_id from int to bigint
     - Change rev_actor from int to bigint
     - Sync rev_page_id index with MySQL
    
    MySQL/SQlite:
     - Drop default from rev_timestamp
    
    Additional changes in the generator script to handle more
    formatting issues due to use of additional custom table options
    
    Bug: T230428
    Bug: T164898
    Change-Id: Ia07dd52e43123473a1728523a3f863280537db8e

--- maintenance/sqlite/tables-generated.sql
+++ maintenance/sqlite/tables-generated.sql
@@ -863,3 +863,28 @@ CREATE UNIQUE INDEX user_name ON /*_*/user (user_name);
 CREATE INDEX user_email_token ON /*_*/user (user_email_token);
 
 CREATE INDEX user_email ON /*_*/user (user_email);
+
+
+CREATE TABLE /*_*/revision (
+  rev_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  rev_page INTEGER UNSIGNED NOT NULL,
+  rev_comment_id BIGINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_actor BIGINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_timestamp BLOB NOT NULL, rev_minor_edit SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_deleted SMALLINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_len INTEGER UNSIGNED DEFAULT NULL,
+  rev_parent_id INTEGER UNSIGNED DEFAULT NULL,
+  rev_sha1 BLOB DEFAULT '' NOT NULL
+);
+
+CREATE INDEX rev_page_id ON /*_*/revision (rev_page, rev_id);
+
+CREATE INDEX rev_timestamp ON /*_*/revision (rev_timestamp);
+
+CREATE INDEX rev_page_timestamp ON /*_*/revision (rev_page, rev_timestamp);
+
+CREATE INDEX rev_actor_timestamp ON /*_*/revision (rev_actor, rev_timestamp, rev_id);
+
+CREATE INDEX rev_page_actor_timestamp ON /*_*/revision (
+  rev_page, rev_actor, rev_timestamp
+);
--- maintenance/tables-generated.sql
+++ maintenance/tables-generated.sql
@@ -861,3 +861,25 @@ CREATE TABLE /*_*/user (
   ),
   PRIMARY KEY(user_id)
 ) /*$wgDBTableOptions*/;
+
+
+CREATE TABLE /*_*/revision (
+  rev_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  rev_page INT UNSIGNED NOT NULL,
+  rev_comment_id BIGINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_actor BIGINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_timestamp BINARY(14) NOT NULL,
+  rev_minor_edit TINYINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_deleted TINYINT UNSIGNED DEFAULT 0 NOT NULL,
+  rev_len INT UNSIGNED DEFAULT NULL,
+  rev_parent_id INT UNSIGNED DEFAULT NULL,
+  rev_sha1 VARBINARY(32) DEFAULT '' NOT NULL,
+  INDEX rev_page_id (rev_page, rev_id),
+  INDEX rev_timestamp (rev_timestamp),
+  INDEX rev_page_timestamp (rev_page, rev_timestamp),
+  INDEX rev_actor_timestamp (rev_actor, rev_timestamp, rev_id),
+  INDEX rev_page_actor_timestamp (
+    rev_page, rev_actor, rev_timestamp
+  ),
+  PRIMARY KEY(rev_id)
+) /*$wgDBTableOptions*/ MAX_ROWS=10000000 AVG_ROW_LENGTH=1024;
