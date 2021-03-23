f4f0ad970e08ddeb1795e74dbf0230019a850a7b
Aaron Schulz <aschulz@wikimedia.org>
2021-03-22 17:25:17 -0700

    Avoid HTTP protocol errors when fastcgi_finish_request() is unavailable
    This fixes problem that arise with apache2/mod_php due to deferred updates
    
    * Do not send unnecessary and invalid "Content-Encoding: identity" header
    * Do not send "Connection: close" if HTTP2 is detected (per the HTTP spec)
      pending, which reduces the use of output buffer and HTTP header tricks
    * Make sure that no output is emitted in doPostOutputShutdown() from any
      deferred updates since the response will have already been flushed to
      the client by that point
    * Make the Content-Length header logic in outputResponsePayload() account
      for cases where there is a non-empty output buffer, cases where there
      are several output buffers (bail out), and limit the use of the header
      to HTTP 200/404 responses (avoids violation of the HTTP spec)
    * Make sure OutputHandler::handle() does not send payloads for responses
      that must not have one (e.g. "204 No Content")
    * If an output buffer using OutputHandler::handle is active, then let it
      handle the setting of Content-Length rather than outputResponsePayload()
    * Do not bother trying to disable zlib.output_compression, since that did
      not actually stop the client from getting blocked
    * Set "no-gzip" via apache_setenv() unconditionally
    
    Bug: T235554
    Change-Id: I26f16457698c2c45e561b0c79c78a74e7f47126c

--- includes/OutputHandler.php
+++ includes/OutputHandler.php
@@ -29,22 +29,51 @@ class OutputHandler {
 	/**
 	 * Standard output handler for use with ob_start.
 	 *
+	 * Output buffers using this method should only be started from MW_SETUP_CALLBACK,
+	 * and only if there are no parent output buffers.
+	 *
 	 * @param string $s Web response output
 	 * @return string
 	 */
 	public static function handle( $s ) {
 		global $wgDisableOutputCompression, $wgMangleFlashPolicy;
+
 		if ( $wgMangleFlashPolicy ) {
 			$s = self::mangleFlashPolicy( $s );
 		}
-		if ( !$wgDisableOutputCompression && !ini_get( 'zlib.output_compression' ) ) {
-			if ( !defined( 'MW_NO_OUTPUT_COMPRESSION' ) ) {
-				$s = self::handleGzip( $s );
-			}
-			if ( !ini_get( 'output_handler' ) ) {
-				self::emitContentLength( strlen( $s ) );
-			}
+
+		// Sanity check if a compression output buffer is already enabled via php.ini. Such
+		// buffers exists at the start of the request and are reflected by ob_get_level().
+		$phpHandlesCompression = (
+			ini_get( 'output_handler' ) === 'ob_gzhandler' ||
+			ini_get( 'zlib.output_handler' ) === 'ob_gzhandler' ||
+			!in_array(
+				strtolower( ini_get( 'zlib.output_compression' ) ),
+				[ '', 'off', '0' ]
+			)
+		);
+
+		if (
+			// Compression is not already handled by an internal PHP buffer
+			!$phpHandlesCompression &&
+			// Compression is not disabled by the application entry point
+			!defined( 'MW_NO_OUTPUT_COMPRESSION' ) &&
+			// Compression is not disabled by site configuration
+			$wgDisableOutputCompression
+		) {
+			$s = self::handleGzip( $s );
 		}
+
+		if (
+			// Response body length does not depend on internal PHP compression buffer
+			!$phpHandlesCompression &&
+			// Response body length does not depend on mangling by a custom buffer
+			!ini_get( 'output_handler' ) &&
+			!ini_get( 'zlib.output_handler' )
+		) {
+			self::emitContentLength( strlen( $s ) );
+		}
+
 		return $s;
 	}
 
@@ -81,7 +110,9 @@ class OutputHandler {
 	/**
 	 * Handler that compresses data with gzip if allowed by the Accept header.
 	 *
-	 * Unlike ob_gzhandler, it works for HEAD requests too.
+	 * Unlike ob_gzhandler, it works for HEAD requests too. This assumes that the application
+	 * processes them as normal GET request and that the webserver is tasked with stripping out
+	 * the response body before sending the response the client.
 	 *
 	 * @param string $s Web response output
 	 * @return string
@@ -102,6 +133,13 @@ class OutputHandler {
 			// This confuses Safari and triggers a download of the page,
 			// even though it's pretty clearly labeled as viewable HTML.
 			// Bad Safari! Bad!
+			return $s;
+		}
+
+		if ( $s === '' ) {
+			// Do not gzip empty HTTP responses since that would not only bloat the body
+			// length, but it would result in invalid HTTP responses when the HTTP status code
+			// is one that must not be accompanied by a body (e.g. "204 No Content").
 			return $s;
 		}
 
@@ -143,14 +181,29 @@ class OutputHandler {
 	}
 
 	/**
-	 * Add a Content-Length header if possible. This makes it cooperate with CDN better.
+	 * Set the Content-Length header if possible
+	 *
+	 * This sets Content-Length for the following cases:
+	 *  - When the response body is meaningful (HTTP 200/404).
+	 *  - On any HTTP 1.0 request response. This improves cooperation with certain CDNs.
+	 *
+	 * This assumes that HEAD requests are processed as GET requests by MediaWiki and that
+	 * the webserver is tasked with stripping out the body.
+	 *
+	 * Setting Content-Length can prevent clients from getting stuck waiting on PHP to finish
+	 * while deferred updates are running.
 	 *
 	 * @param int $length
 	 */
 	private static function emitContentLength( $length ) {
-		if ( !headers_sent()
-			&& isset( $_SERVER['SERVER_PROTOCOL'] )
-			&& $_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.0'
+		if ( headers_sent() ) {
+			wfDebug( __METHOD__ . "() headers already sent" );
+			return;
+		}
+
+		if (
+			in_array( http_response_code(), [ 200, 404 ], true ) ||
+			( $_SERVER['SERVER_PROTOCOL'] ?? null ) === 'HTTP/1.0'
 		) {
 			header( "Content-Length: $length" );
 		}
