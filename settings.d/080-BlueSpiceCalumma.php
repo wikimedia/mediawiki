95157dd2b8d0d3b02d65de7e553c3ac7a6d017be
rvogel <vogel@hallowelt.biz>
2021-03-26 14:53:54 +0100

    BlueSpice free shows a SMW banner, but SMW is not included
    [3.2]
    
    ERM19184

--- settings.d/080-BlueSpiceCalumma.php
+++ settings.d/080-BlueSpiceCalumma.php
@@ -32,12 +32,21 @@ $GLOBALS['wgFooterIcons']['poweredby'] += [
 		'alt' => 'Powered by BlueSpice',
 		'height' => '27',
 		'width' => '149'
-	],
-	'semanticmediawiki' => [
-		'src' => $wgScriptPath . '/skins/BlueSpiceCalumma/resources/images/common/footer/SemanticMediaWiki.png',
-		'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
-		'alt' => 'Powered by Semantic MediaWiki',
-		'height' => '27',
-		'width' => '149'
 	]
 ];
+
+if ( array_key_exists( 'semanticmediawiki', $GLOBALS['wgFooterIcons']['poweredby'] ) ) {
+	$footerIcons = [
+		'mediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'],
+		'bluespice' => $GLOBALS['wgFooterIcons']['poweredby']['bluespice'],
+		'semanticmediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki']
+	];
+
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['bluespice'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki'] );
+
+	$footerIcons += $GLOBALS['wgFooterIcons']['poweredby'];
+
+	$GLOBALS['wgFooterIcons']['poweredby'] = $footerIcons;
+}
