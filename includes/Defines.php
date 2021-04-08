301645397d2d73cdc6e6f99a885324a48faa8f00
James D. Forrester <jforrester@wikimedia.org>
2021-04-08 13:33:33 -0700

    Add REL1_36 sub-modules
    Bug: T279456
    Change-Id: Ic6e207ee6cfbad9601c2e3deb292234ce106d53f

--- includes/Defines.php
+++ includes/Defines.php
@@ -33,7 +33,7 @@ use Wikimedia\Rdbms\IDatabase;
  *
  * @since 1.35
  */
-define( 'MW_VERSION', '1.36.0-alpha' );
+define( 'MW_VERSION', '1.36.0-beta' );
 
 /** @{
  * Obsolete IDatabase::makeList() constants
