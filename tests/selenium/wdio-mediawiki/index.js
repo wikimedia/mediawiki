50b4df35151ad374186591844a5d4eb0f9be5920
Željko Filipin <zeljko.filipin@gmail.com>
2021-04-28 16:58:10 +0200

    selenium: File name friendly version of ISO 8601 date and time
    Two tasks (T214686 and T280970) need a timestamp in file name.
    
    It's currently implemented as a simple constant, but it's duplicated
    in two files, tests/selenium/wdio.conf.js and
    tests/selenium/wdio-mediawiki/index.js.
    
    To reduce duplication, this patch creates a simple function in
    wdio-mediawiki.
    
    Bug: T281372
    Change-Id: Id2a9a340e7e3be253238463b9bd25483647e227c

--- tests/selenium/wdio-mediawiki/index.js
+++ tests/selenium/wdio-mediawiki/index.js
@@ -2,26 +2,37 @@
 
 const fs = require( 'fs' );
 
-module.exports = {
-	/**
-	 * Based on <https://github.com/webdriverio/webdriverio/issues/269#issuecomment-306342170>
-	 *
-	 * @since 1.0.0
-	 * @param {string} title Description (will be sanitised and used as file name)
-	 * @return {string} File path
-	 */
-	saveScreenshot( title ) {
-		// Create sane file name for current test title
-		const filename = encodeURIComponent( title.replace( /\s+/g, '-' ) );
-		const filePath = `${browser.config.screenshotPath}/${filename}.png`;
-		// Ensure directory exists, based on WebDriverIO#saveScreenshotSync()
-		try {
-			fs.statSync( browser.config.screenshotPath );
-		} catch ( err ) {
-			fs.mkdirSync( browser.config.screenshotPath );
-		}
-		// Create and save screenshot
-		browser.saveScreenshot( filePath );
-		return filePath;
+/**
+ * @since 1.1.0
+ * @return {string} File name friendly version of ISO 8601 date and time
+ */
+function makeFilenameDate() {
+	return new Date().toISOString().replace( /[:.]/g, '-' );
+}
+
+/**
+ * Based on <https://github.com/webdriverio/webdriverio/issues/269#issuecomment-306342170>
+ *
+ * @since 1.0.0
+ * @param {string} title Description (will be sanitised and used as file name)
+ * @return {string} File path
+ */
+function saveScreenshot( title ) {
+	// Create sane file name for current test title
+	const filename = encodeURIComponent( title.replace( /\s+/g, '-' ) );
+	const filePath = `${browser.config.screenshotPath}/${filename}.png`;
+	// Ensure directory exists, based on WebDriverIO#saveScreenshotSync()
+	try {
+		fs.statSync( browser.config.screenshotPath );
+	} catch ( err ) {
+		fs.mkdirSync( browser.config.screenshotPath );
 	}
+	// Create and save screenshot
+	browser.saveScreenshot( filePath );
+	return filePath;
+}
+
+module.exports = {
+	makeFilenameDate,
+	saveScreenshot
 };
