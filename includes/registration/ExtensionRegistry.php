aac2408b7f7149c6da1322a5c9b5cd0e5abcf323
Tim Starling <tstarling@wikimedia.org>
2021-05-04 14:18:37 +1000

    Add extension.json merge strategy "provide_default"
    Add a merge strategy which sets the whole value only if the global
    has not previously been set. This can be used to allow LocalSettings.php
    to remove elements from an array created by extension.json.
    
    Bug: T142663
    Change-Id: Id250f9c349ee4eafbc759452ca6ea2a918baeb11
    (cherry picked from commit 076b93fba0a02a53655a1ace3ca45577d58eb4a8)

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -471,6 +471,13 @@ class ExtensionRegistry {
 				$mergeStrategy = 'array_merge';
 			}
 
+			if ( $mergeStrategy === 'provide_default' ) {
+				if ( !array_key_exists( $key, $GLOBALS ) ) {
+					$GLOBALS[$key] = $val;
+				}
+				continue;
+			}
+
 			// Optimistic: If the global is not set, or is an empty array, replace it entirely.
 			// Will be O(1) performance.
 			if ( !array_key_exists( $key, $GLOBALS ) || ( is_array( $GLOBALS[$key] ) && !$GLOBALS[$key] ) ) {
