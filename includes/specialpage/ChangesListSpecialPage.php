wikimedia/mediawiki

commit fd01c537787dd05eb61c1fd4d17884d1a96e2afa

Stop using legacy ActorMigration field "rc_user"
Bug: T278917
Change-Id: I4d6c175c637a2cb26c63e0b2b27d5ea8ce6b1e0d

includes/specialpage/ChangesListSpecialPage.php
@@ -131,11 +131,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						'queryCallable' => static function ( $specialClassName, $ctx, $dbr, &$tables, &$fields, &$conds,
 							&$query_options, &$join_conds
 						) {
-							$actorMigration = ActorMigration::newMigration();
-							$actorQuery = $actorMigration->getJoin( 'rc_user' );
-							$tables += $actorQuery['tables'];
-							$join_conds += $actorQuery['joins'];
-							$conds[] = $actorMigration->isAnon( $actorQuery['fields']['rc_user'] );
+							$conds['actor_user'] = null;
 						},
 						'isReplacedInStructuredUi' => true,
 
@@ -149,11 +145,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						'queryCallable' => static function ( $specialClassName, $ctx, $dbr, &$tables, &$fields, &$conds,
 							&$query_options, &$join_conds
 						) {
-							$actorMigration = ActorMigration::newMigration();
-							$actorQuery = $actorMigration->getJoin( 'rc_user' );
-							$tables += $actorQuery['tables'];
-							$join_conds += $actorQuery['joins'];
-							$conds[] = $actorMigration->isNotAnon( $actorQuery['fields']['rc_user'] );
+							$conds[] = 'actor_user IS NOT NULL';
 						},
 						'isReplacedInStructuredUi' => true,
 					]
@@ -238,10 +230,13 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						'queryCallable' => static function ( $specialClassName, $ctx, $dbr, &$tables, &$fields, &$conds,
 							&$query_options, &$join_conds
 						) {
-							$actorQuery = ActorMigration::newMigration()->getWhere( $dbr, 'rc_user', $ctx->getUser() );
-							$tables += $actorQuery['tables'];
-							$join_conds += $actorQuery['joins'];
-							$conds[] = 'NOT(' . $actorQuery['conds'] . ')';
+							/** @var IContextSource $ctx */
+							$user = $ctx->getUser();
+							if ( $user->isAnon() ) {
+								$conds[] = 'actor_name<>' . $dbr->addQuotes( $user->getName() );
+							} else {
+								$conds[] = 'actor_user<>' . $dbr->addQuotes( $user->getId() );
+							}
 						},
 						'cssClassSuffix' => 'self',
 						'isRowApplicableCallable' => static function ( $ctx, $rc ) {
@@ -256,11 +251,13 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						'queryCallable' => static function ( $specialClassName, $ctx, $dbr, &$tables, &$fields, &$conds,
 							&$query_options, &$join_conds
 						) {
-							$actorQuery = ActorMigration::newMigration()
-								->getWhere( $dbr, 'rc_user', $ctx->getUser(), false );
-							$tables += $actorQuery['tables'];
-							$join_conds += $actorQuery['joins'];
-							$conds[] = $actorQuery['conds'];
+							/** @var IContextSource $ctx */
+							$user = $ctx->getUser();
+							if ( $user->isAnon() ) {
+								$conds['actor_name'] = $user->getName();
+							} else {
+								$conds['actor_user'] = $user->getId();
+							}
 						},
 						'cssClassSuffix' => 'others',
 						'isRowApplicableCallable' => static function ( $ctx, $rc ) {
@@ -1885,27 +1882,22 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 			return;
 		}
 
-		$actorMigration = ActorMigration::newMigration();
-		$actorQuery = $actorMigration->getJoin( 'rc_user' );
-		$tables += $actorQuery['tables'];
-		$join_conds += $actorQuery['joins'];
-
 		// 'registered' but not 'unregistered', experience levels, if any, are included in 'registered'
 		if (
 			in_array( 'registered', $selectedExpLevels ) &&
 			!in_array( 'unregistered', $selectedExpLevels )
 		) {
-			$conds[] = $actorMigration->isNotAnon( $actorQuery['fields']['rc_user'] );
+			$conds[] = 'actor_user IS NOT NULL';
 			return;
 		}
 
 		if ( $selectedExpLevels === [ 'unregistered' ] ) {
-			$conds[] = $actorMigration->isAnon( $actorQuery['fields']['rc_user'] );
+			$conds['actor_user'] = null;
 			return;
 		}
 
 		$tables[] = 'user';
-		$join_conds['user'] = [ 'LEFT JOIN', $actorQuery['fields']['rc_user'] . ' = user_id' ];
+		$join_conds['user'] = [ 'LEFT JOIN', 'actor_user=user_id' ];
 
 		if ( $now === 0 ) {
 			$now = time();
@@ -1941,7 +1933,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 
 		if ( in_array( 'unregistered', $selectedExpLevels ) ) {
 			$selectedExpLevels = array_diff( $selectedExpLevels, [ 'unregistered' ] );
-			$conditions[] = $actorMigration->isAnon( $actorQuery['fields']['rc_user'] );
+			$conditions['actor_user'] = null;
 		}
 
 		if ( $selectedExpLevels === [ 'newcomer' ] ) {
@@ -1963,7 +1955,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 		} elseif ( $selectedExpLevels === [ 'experienced', 'learner' ] ) {
 			$conditions[] = $aboveNewcomer;
 		} elseif ( $selectedExpLevels === [ 'experienced', 'learner', 'newcomer' ] ) {
-			$conditions[] = $actorMigration->isNotAnon( $actorQuery['fields']['rc_user'] );
+			$conditions[] = 'actor_user IS NOT NULL';
 		}
 
 		if ( count( $conditions ) > 1 ) {

tests/phpunit/includes/specialpage/ChangesListSpecialPageTest.php
@@ -260,12 +260,10 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 	}
 
 	public function testRcHidemyselfFilter() {
-		$actorNormalization = $this->getServiceContainer()->getActorNormalization();
 		$user = $this->getTestUser()->getUser();
-		$actorId = $actorNormalization->acquireActorId( $user, $this->db );
 		$this->assertConditions(
 			[ # expected
-				"NOT((rc_actor = {$actorId}))",
+				"actor_user<>{$user->getId()}",
 			],
 			[
 				'hidemyself' => 1,
@@ -275,10 +273,9 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		);
 
 		$user = User::newFromName( '10.11.12.13', false );
-		$actorId = $actorNormalization->acquireActorId( $user, $this->db );
 		$this->assertConditions(
 			[ # expected
-				"NOT((rc_actor = {$actorId}))",
+				"actor_name<>'10.11.12.13'",
 			],
 			[
 				'hidemyself' => 1,
@@ -289,12 +286,10 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 	}
 
 	public function testRcHidebyothersFilter() {
-		$actorNormalization = $this->getServiceContainer()->getActorNormalization();
 		$user = $this->getTestUser()->getUser();
-		$actorId = $actorNormalization->acquireActorId( $user, $this->db );
 		$this->assertConditions(
 			[ # expected
-				"(rc_actor = {$actorId})",
+				'actor_user' => $user->getId(),
 			],
 			[
 				'hidebyothers' => 1,
@@ -304,10 +299,9 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		);
 
 		$user = User::newFromName( '10.11.12.13', false );
-		$actorId = $actorNormalization->acquireActorId( $user, $this->db );
 		$this->assertConditions(
 			[ # expected
-				"(rc_actor = {$actorId})",
+				'actor_name' => '10.11.12.13',
 			],
 			[
 				'hidebyothers' => 1,
@@ -523,7 +517,7 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		$this->assertConditions(
 			[
 				# expected
-				'actor_rc_user.actor_user IS NOT NULL',
+				'actor_user IS NOT NULL',
 			],
 			[
 				'userExpLevel' => 'newcomer;learner;experienced',
@@ -536,7 +530,7 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		$this->assertConditions(
 			[
 				# expected
-				'actor_rc_user.actor_user IS NOT NULL',
+				'actor_user IS NOT NULL',
 			],
 			[
 				'userExpLevel' => 'registered',
@@ -549,7 +543,7 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		$this->assertConditions(
 			[
 				# expected
-				'actor_rc_user.actor_user IS NULL',
+				'actor_user' => null,
 			],
 			[
 				'userExpLevel' => 'unregistered',
@@ -562,7 +556,7 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		$this->assertConditions(
 			[
 				# expected
-				'actor_rc_user.actor_user IS NOT NULL',
+				'actor_user IS NOT NULL',
 			],
 			[
 				'userExpLevel' => 'registered;learner',
@@ -575,7 +569,7 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 		$conds = $this->buildQuery( [ 'userExpLevel' => 'unregistered;experienced' ] );
 
 		$this->assertRegExp(
-			'/\(actor_rc_user\.actor_user IS NULL\) OR '
+			'/actor_user IS NULL OR '
 				. '\(\(user_editcount >= 500\) AND \(\(user_registration IS NULL\) OR '
 				. '\(user_registration <= \'[^\']+\'\)\)\)/',
 			reset( $conds ),

includes/ServiceWiring.php
@@ -1609,11 +1609,9 @@ return [
 		return new WatchedItemQueryService(
 			$services->getDBLoadBalancer(),
 			$services->getCommentStore(),
-			$services->getActorMigration(),
 			$services->getWatchedItemStore(),
 			$services->getPermissionManager(),
 			$services->getHookContainer(),
-			$services->getUserFactory(),
 			$services->getMainConfig()->get( 'WatchlistExpiry' )
 		);
 	},
@@ -1739,7 +1737,8 @@ return [
 			$services->getHookContainer(),
 			$services->getWikiPageFactory(),
 			$services->getUserFactory(),
-			$services->getActorMigration()
+			$services->getActorMigration(),
+			$services->getActorNormalization()
 		);
 	},
 

includes/Storage/PageEditStash.php
@@ -22,7 +22,6 @@
 
 namespace MediaWiki\Storage;
 
-use ActorMigration;
 use BagOStuff;
 use Content;
 use Liuggio\StatsdClient\Factory\StatsdDataFactoryInterface;
@@ -386,15 +385,13 @@ class PageEditStash {
 	private function lastEditTime( UserIdentity $user ) {
 		$db = $this->lb->getConnectionRef( DB_REPLICA );
 
-		$actorQuery = ActorMigration::newMigration()->getWhere( $db, 'rc_user', $user, false );
-		$time = $db->selectField(
-			[ 'recentchanges' ] + $actorQuery['tables'],
-			'MAX(rc_timestamp)',
-			[ $actorQuery['conds'] ],
-			__METHOD__,
-			[],
-			$actorQuery['joins']
-		);
+		$time = $db->newSelectQueryBuilder()
+			->select( 'MAX(rc_timestamp)' )
+			->from( 'recentchanges' )
+			->join( 'actor', null, 'actor_id=rc_actor' )
+			->where( [ 'actor_name' => $user->getName() ] )
+			->caller( __METHOD__ )
+			->fetchField();
 
 		return wfTimestampOrNull( TS_MW, $time );
 	}

includes/api/ApiQueryRecentChanges.php
@@ -234,15 +234,13 @@ class ApiQueryRecentChanges extends ApiQueryGeneratorBase {
 			$this->addWhereIf( 'rc_bot = 0', isset( $show['!bot'] ) );
 			$this->addWhereIf( 'rc_bot != 0', isset( $show['bot'] ) );
 			if ( isset( $show['anon'] ) || isset( $show['!anon'] ) ) {
-				$actorMigration = ActorMigration::newMigration();
-				$actorQuery = $actorMigration->getJoin( 'rc_user' );
-				$this->addTables( $actorQuery['tables'] );
-				$this->addJoinConds( $actorQuery['joins'] );
+				$this->addTables( 'actor', 'actor' );
+				$this->addJoinConds( [ 'actor' => [ 'JOIN', 'actor_id=rc_actor' ] ] );
 				$this->addWhereIf(
-					$actorMigration->isAnon( $actorQuery['fields']['rc_user'] ), isset( $show['anon'] )
+					'actor_user IS NULL', isset( $show['anon'] )
 				);
 				$this->addWhereIf(
-					$actorMigration->isNotAnon( $actorQuery['fields']['rc_user'] ), isset( $show['!anon'] )
+					'actor_user IS NOT NULL', isset( $show['!anon'] )
 				);
 			}
 			$this->addWhereIf( 'rc_patrolled = 0', isset( $show['!patrolled'] ) );
@@ -277,22 +275,31 @@ class ApiQueryRecentChanges extends ApiQueryGeneratorBase {
 
 		$this->requireMaxOneParameter( $params, 'user', 'excludeuser' );
 
+		if ( $params['prop'] !== null ) {
+			$prop = array_flip( $params['prop'] );
+
+			/* Set up internal members based upon params. */
+			$this->initProperties( $prop );
+		}
+
+		if ( $this->fld_user
+			|| $this->fld_userid
+			// Token needs actor_user for RecentChange::newFromRow/User::newFromAnyId (T228425)
+			|| $this->token !== null
+			|| $params['user'] !== null
+			|| $params['excludeuser'] !== null
+		) {
+			$this->addTables( 'actor', 'actor' );
+			$this->addFields( [ 'actor_name', 'actor_user', 'rc_actor' ] );
+			$this->addJoinConds( [ 'actor' => [ 'JOIN', 'actor_id=rc_actor' ] ] );
+		}
+
 		if ( $params['user'] !== null ) {
-			// Don't query by user ID here, it might be able to use the rc_user_text index.
-			$actorQuery = ActorMigration::newMigration()
-				->getWhere( $this->getDB(), 'rc_user', $params['user'], false );
-			$this->addTables( $actorQuery['tables'] );
-			$this->addJoinConds( $actorQuery['joins'] );
-			$this->addWhere( $actorQuery['conds'] );
+			$this->addWhereFld( 'actor_name', $params['user'] );
 		}
 
 		if ( $params['excludeuser'] !== null ) {
-			// Here there's no chance to use the rc_user_text index, so allow ID to be used.
-			$actorQuery = ActorMigration::newMigration()
-				->getWhere( $this->getDB(), 'rc_user', $params['excludeuser'] );
-			$this->addTables( $actorQuery['tables'] );
-			$this->addJoinConds( $actorQuery['joins'] );
-			$this->addWhere( 'NOT(' . $actorQuery['conds'] . ')' );
+			$this->addWhere( 'actor_name<>' . $this->getDB()->addQuotes( $params['excludeuser'] ) );
 		}
 
 		/* Add the fields we're concerned with to our query. */
@@ -309,11 +316,6 @@ class ApiQueryRecentChanges extends ApiQueryGeneratorBase {
 		$showRedirects = false;
 		/* Determine what properties we need to display. */
 		if ( $params['prop'] !== null ) {
-			$prop = array_flip( $params['prop'] );
-
-			/* Set up internal members based upon params. */
-			$this->initProperties( $prop );
-
 			if ( $this->fld_patrolled && !$user->useRCPatrol() && !$user->useNPPatrol() ) {
 				$this->dieWithError( 'apierror-permissiondenied-patrolflag', 'permissiondenied' );
 			}
@@ -404,14 +406,6 @@ class ApiQueryRecentChanges extends ApiQueryGeneratorBase {
 			$this->addTables( $commentQuery['tables'] );
 			$this->addFields( $commentQuery['fields'] );
 			$this->addJoinConds( $commentQuery['joins'] );
-		}
-
-		if ( $this->fld_user || $this->fld_userid || $this->token !== null ) {
-			// Token needs rc_user for RecentChange::newFromRow/User::newFromAnyId (T228425)
-			$actorQuery = ActorMigration::newMigration()->getJoin( 'rc_user' );
-			$this->addTables( $actorQuery['tables'] );
-			$this->addFields( $actorQuery['fields'] );
-			$this->addJoinConds( $actorQuery['joins'] );
 		}
 
 		if ( $params['slot'] !== null ) {
@@ -575,14 +569,14 @@ class ApiQueryRecentChanges extends ApiQueryGeneratorBase {
 			}
 			if ( RevisionRecord::userCanBitfield( $row->rc_deleted, RevisionRecord::DELETED_USER, $user ) ) {
 				if ( $this->fld_user ) {
-					$vals['user'] = $row->rc_user_text;
+					$vals['user'] = $row->actor_name;
 				}
 
 				if ( $this->fld_userid ) {
-					$vals['userid'] = (int)$row->rc_user;
+					$vals['userid'] = (int)$row->actor_user;
 				}
 
-				if ( !$row->rc_user ) {
+				if ( !$row->actor_user ) {
 					$vals['anon'] = true;
 				}
 			}

includes/api/ApiQueryWatchlist.php
@@ -352,7 +352,7 @@ class ApiQueryWatchlist extends ApiQueryGeneratorBase {
 					$vals['user'] = $recentChangeInfo['rc_user_text'];
 				}
 
-				$vals['anon'] = $recentChangeInfo['rc_user'] == 0;
+				$vals['anon'] = !$recentChangeInfo['rc_user'];
 			}
 		}
 

includes/changes/RecentChange.php
@@ -246,6 +246,11 @@ class RecentChange implements Taggable {
 	/**
 	 * Return the tables, fields, and join conditions to be selected to create
 	 * a new recentchanges object.
+	 *
+	 * Since 1.34, rc_user and rc_user_text have not been present in the
+	 * database, but they continue to be available in query results as
+	 * aliases.
+	 *
 	 * @since 1.31
 	 * @return array With three keys:
 	 *   - tables: (string[]) to include in the `$table` to `IDatabase->select()`
@@ -254,9 +259,11 @@ class RecentChange implements Taggable {
 	 */
 	public static function getQueryInfo() {
 		$commentQuery = CommentStore::getStore()->getJoin( 'rc_comment' );
-		$actorQuery = ActorMigration::newMigration()->getJoin( 'rc_user' );
 		return [
-			'tables' => [ 'recentchanges' ] + $commentQuery['tables'] + $actorQuery['tables'],
+			'tables' => [
+				'recentchanges',
+				'recentchanges_actor' => 'actor'
+			] + $commentQuery['tables'],
 			'fields' => [
 				'rc_id',
 				'rc_timestamp',
@@ -279,8 +286,13 @@ class RecentChange implements Taggable {
 				'rc_log_type',
 				'rc_log_action',
 				'rc_params',
-			] + $commentQuery['fields'] + $actorQuery['fields'],
-			'joins' => $commentQuery['joins'] + $actorQuery['joins'],
+				'rc_actor',
+				'rc_user' => 'recentchanges_actor.actor_user',
+				'rc_user_text' => 'recentchanges_actor.actor_name',
+			] + $commentQuery['fields'],
+			'joins' => [
+				'recentchanges_actor' => [ 'JOIN', 'actor_id=rc_actor' ]
+			] + $commentQuery['joins'],
 		];
 	}
 

includes/jobqueue/jobs/RecentChangesUpdateJob.php
@@ -153,15 +153,14 @@ class RecentChangesUpdateJob extends Job {
 		$eTimestamp = min( $sTimestamp + $window, $nowUnix );
 
 		// Get all the users active since the last update
-		$actorQuery = ActorMigration::newMigration()->getJoin( 'rc_user' );
 		$res = $dbw->select(
-			[ 'recentchanges' ] + $actorQuery['tables'],
+			[ 'recentchanges', 'actor' ],
 			[
-				'rc_user_text' => $actorQuery['fields']['rc_user_text'],
+				'actor_name',
 				'lastedittime' => 'MAX(rc_timestamp)'
 			],
 			[
-				$actorQuery['fields']['rc_user'] . ' > 0', // actual accounts
+				'actor_user IS NOT NULL', // actual accounts
 				'rc_type != ' . $dbw->addQuotes( RC_EXTERNAL ), // no wikidata
 				'rc_log_type IS NULL OR rc_log_type != ' . $dbw->addQuotes( 'newusers' ),
 				'rc_timestamp >= ' . $dbw->addQuotes( $dbw->timestamp( $sTimestamp ) ),
@@ -169,14 +168,16 @@ class RecentChangesUpdateJob extends Job {
 			],
 			__METHOD__,
 			[
-				'GROUP BY' => [ $actorQuery['fields']['rc_user_text'] ],
+				'GROUP BY' => 'actor_name',
 				'ORDER BY' => 'NULL' // avoid filesort
 			],
-			$actorQuery['joins']
+			[
+				'actor' => [ 'JOIN', 'actor_id=rc_actor' ]
+			]
 		);
 		$names = [];
 		foreach ( $res as $row ) {
-			$names[$row->rc_user_text] = $row->lastedittime;
+			$names[$row->actor_name] = $row->lastedittime;
 		}
 
 		// Find which of the recently active users are already accounted for

commit 8327d2f0cac5918f0138391507304eaa2dde387d

Fix changes list "hide myself" feature
Broken by fd01c537787dd05eb61c1fd4d17884d1a96e2afa

Bug: T282183
Change-Id: I0ae68702f9ea160a1e649ca6c52f88cdf9a8e8e5

includes/specialpage/ChangesListSpecialPage.php
@@ -232,11 +232,7 @@ abstract class ChangesListSpecialPage extends SpecialPage {
 						) {
 							/** @var IContextSource $ctx */
 							$user = $ctx->getUser();
-							if ( $user->isAnon() ) {
-								$conds[] = 'actor_name<>' . $dbr->addQuotes( $user->getName() );
-							} else {
-								$conds[] = 'actor_user<>' . $dbr->addQuotes( $user->getId() );
-							}
+							$conds[] = 'actor_name<>' . $dbr->addQuotes( $user->getName() );
 						},
 						'cssClassSuffix' => 'self',
 						'isRowApplicableCallable' => static function ( $ctx, $rc ) {

tests/phpunit/includes/specialpage/ChangesListSpecialPageTest.php
@@ -265,9 +265,10 @@ class ChangesListSpecialPageTest extends AbstractChangesListSpecialPageTestCase 
 
 	public function testRcHidemyselfFilter() {
 		$user = $this->getTestUser()->getUser();
+		$encName = $this->db->addQuotes( $user->getName() );
 		$this->assertConditions(
 			[ # expected
-				"actor_user<>{$user->getId()}",
+				"actor_name<>$encName",
 			],
 			[
 				'hidemyself' => 1,