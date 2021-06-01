9f4a392544d6187d8ced6070fa84443dabcfb0d4
Alexander Vorwerk <alec@vc-celle.de>
2021-06-01 23:06:07 +0200

    GrantsInfo service to replace MWGrants
    This patch adds a service as a replacement for MWGrants. This is
    done as it allows proper dependency injection of used services
    and configuration settings.
    
    Bug: T253077
    Change-Id: Iac52dba15fdaf6869da14deebd89fef59b817255

--- includes/MediaWikiServices.php
+++ includes/MediaWikiServices.php
@@ -64,6 +64,7 @@ use MediaWiki\Page\ParserOutputAccess;
 use MediaWiki\Page\RollbackPageFactory;
 use MediaWiki\Page\WikiPageFactory;
 use MediaWiki\Parser\ParserCacheFactory;
+use MediaWiki\Permissions\GrantsInfo;
 use MediaWiki\Permissions\GroupPermissionsLookup;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\Preferences\PreferencesFactory;
@@ -869,6 +870,14 @@ class MediaWikiServices extends ServiceContainer {
 	 */
 	public function getGlobalIdGenerator() : GlobalIdGenerator {
 		return $this->getService( 'GlobalIdGenerator' );
+	}
+
+	/**
+	 * @since 1.37
+	 * @return GrantsInfo
+	 */
+	public function getGrantsInfo() : GrantsInfo {
+		return $this->getService( 'GrantsInfo' );
 	}
 
 	/**
