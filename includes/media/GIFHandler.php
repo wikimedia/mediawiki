3ff89b6295eea5eccbd77cece5a9d4dd8fcc3bf9
Amir Sarabadani <Ladsgroup@gmail.com>
2021-06-24 02:27:51 +0200

    media: Make the file metadata "_error" check looser
    Follows-up I039785d5b6 and I0ccb9971c7b6d99.
    It basically cancelled out when the value of error is 0, which is falsy.
    
    Bug: T285431
    Change-Id: I79a3c021973e43cf8012a783c24e40bbd33d8652

--- includes/media/GIFHandler.php
+++ includes/media/GIFHandler.php
@@ -164,11 +164,7 @@ class GIFHandler extends BitmapHandler {
 
 		$metadata = $image->getMetadataArray();
 
-		if (
-			!$metadata ||
-			( isset( $metadata['_error'] ) && $metadata['_error'] ) ||
-			$metadata['frameCount'] <= 0
-		) {
+		if ( !$metadata || isset( $metadata['_error'] ) || $metadata['frameCount'] <= 0 ) {
 			return $original;
 		}
 
