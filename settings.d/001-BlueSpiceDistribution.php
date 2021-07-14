d86439faa0c8e3937e25246d95d1423ce141bd09
rvogel <vogel@hallowelt.biz>
2021-07-14 15:40:44 +0200

    Enable Extension:UserFunctions in all namespaces
    ERM24717
    ERM24015
    ERM25047
    
    [3.2.x]

--- settings.d/001-BlueSpiceDistribution.php
+++ settings.d/001-BlueSpiceDistribution.php
@@ -18,5 +18,6 @@ wfLoadExtension( 'BlueSpiceEchoConnector' );
 wfLoadExtension( 'BlueSpiceDistributionConnector' );
 require_once __DIR__ . "/../extensions/BlueSpiceUserMergeConnector/BlueSpiceUserMergeConnector.php";
 require_once __DIR__ . "/../extensions/UserFunctions/UserFunctions.php";
+$GLOBALS['wgUFAllowedNamespaces'] = array_fill( 0, 5000, true );
 require_once __DIR__ . "/../extensions/UrlGetParameters/UrlGetParameters.php";
 wfLoadExtension( 'FlexiSkin' );
