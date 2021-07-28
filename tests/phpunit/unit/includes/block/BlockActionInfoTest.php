07911fc915ee4b331bd0273453b0f83ec3d2d251
DannyS712 <dannys712.wiki@gmail.com>
2021-07-28 21:35:31 +0000

    Expand BlockActionInfo test coverage
    Add tests for getActionFromId and for calling
    getIdFromAction for an action added via hook,
    reduce duplication around creating the BlockActionInfo
    by always registering the hook.
    
    Change-Id: Ibd0bd452019939340ffa9f728fb4d6b60456453e

--- tests/phpunit/unit/includes/block/BlockActionInfoTest.php
+++ tests/phpunit/unit/includes/block/BlockActionInfoTest.php
@@ -1,54 +1,60 @@
 <?php
 
 use MediaWiki\Block\BlockActionInfo;
-use MediaWiki\HookContainer\HookContainer;
 
 /**
  * @group Blocking
  * @coversDefaultClass \MediaWiki\Block\BlockActionInfo
  */
 class BlockActionInfoTest extends MediaWikiUnitTestCase {
-	/** @var HookContainer */
-	private $hookContainer;
+	/** @var BlockActionInfo */
+	private $blockActionInfo;
 
 	protected function setUp(): void {
 		parent::setUp();
-		$this->hookContainer = $this->createHookContainer();
+		$this->blockActionInfo = new BlockActionInfo(
+			$this->createHookContainer( [
+				'GetAllBlockActions' => static function ( array &$actions ) {
+					$actions[ 'test' ] = 100;
+				},
+			] )
+		);
 	}
 
 	/**
 	 * @covers ::getAllBlockActions
 	 */
 	public function testAddBlockAction() {
-		$this->hookContainer->register(
-			'GetAllBlockActions',
-			static function ( array &$actions ) {
-				$actions[ 'test' ] = 100;
-			}
-		);
-		$blockActionInfo = new BlockActionInfo( $this->hookContainer );
-		$blockActions = $blockActionInfo->getAllBlockActions();
+		// 'test' action in added by hook with id 100
+		$blockActions = $this->blockActionInfo->getAllBlockActions();
 
 		// Confirm new action is added
-		$this->assertContains( 100, $blockActions );
+		$this->assertArrayHasKey( 'test', $blockActions, 'Action added via hook' );
+		$this->assertSame( 100, $blockActions['test'], 'Id of action added via hook' );
 	}
 
 	/**
-	 * @dataProvider provideGetIdFromAction
+	 * @dataProvider provideIdAndAction
+	 * @param string|false $action False if the id is invalid
+	 * @param int|false $id False if the action is invalid
 	 * @covers ::getIdFromAction
+	 * @covers ::getActionFromId
 	 */
-	public function testGetIdFromAction( $action, $expected ) {
-		$blockActionInfo = new BlockActionInfo( $this->createMock( HookContainer::class ) );
-		$this->assertSame(
-			$expected,
-			$blockActionInfo->getIdFromAction( $action )
-		);
+	public function testIdAndAction( $action, $id ) {
+		if ( $id !== false ) {
+			$this->assertSame( $action, $this->blockActionInfo->getActionFromId( $id ) );
+		}
+		if ( $action !== false ) {
+			$this->assertSame( $id, $this->blockActionInfo->getIdFromAction( $action ) );
+		}
 	}
 
-	public static function provideGetIdFromAction() {
+	public static function provideIdAndAction() {
 		return [
-			'Valid action' => [ 'upload', 1 ],
-			'Invalid action' => [ 'invalidaction', false ],
+			'Valid core action' => [ 'upload', 1 ],
+			'Valid hook action' => [ 'test', 100 ],
+			'Invalid action name' => [ 'invalid', false ],
+			'Invalid action id' => [ false, 200 ],
 		];
 	}
 
