7c39f764520f60109dedbcb904fe8327aa5ddedb
Timo Tijhof <krinklemail@gmail.com>
2021-07-31 22:23:21 +0100

    Move Language subclasses to includes/
    Depending on which namespace we want these classes to have after
    T166010 they could either stay in includes/languages/ (plural) in
    their own MediaWiki\Languages\-namespace dedicated to Language
    subclasses, or they could go in into a subdirectory like
    `includes/language/languages/` if we want to keep them in the same
    top-level namespace as other Language classes and services, but in
    a more nested namespace.
    
    For now, I've made the smaller change and kept the Language subclasses
    in their own directory directly under includes/, not nested further.
    
    Bug: T225756
    Change-Id: I01015424707b442853879fd50c97f00215e5c2fa

---
+++ tests/phpunit/includes/languages/LanguageGvTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Santhosh Thottingal
+ * @copyright Copyright © 2013, Santhosh Thottingal
+ * @file
+ */
+
+/**
+ * Tests for Manx (Gaelg)
+ *
+ * @group Language
+ */
+class LanguageGvTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider providePlural
+	 * @covers Language::convertPlural
+	 */
+	public function testPlural( $result, $value ) {
+		$forms = [ 'one', 'two', 'few', 'other' ];
+		$this->assertEquals( $result, $this->getLang()->convertPlural( $value, $forms ) );
+	}
+
+	/**
+	 * @dataProvider providePlural
+	 * @covers Language::getPluralRuleType
+	 */
+	public function testGetPluralRuleType( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->getPluralRuleType( $value ) );
+	}
+
+	public static function providePlural() {
+		return [
+			[ 'few', 0 ],
+			[ 'one', 1 ],
+			[ 'two', 2 ],
+			[ 'other', 3 ],
+			[ 'few', 20 ],
+			[ 'one', 21 ],
+			[ 'two', 22 ],
+			[ 'other', 23 ],
+			[ 'other', 50 ],
+			[ 'few', 60 ],
+			[ 'few', 80 ],
+			[ 'few', 100 ]
+		];
+	}
+}
