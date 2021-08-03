wikimedia/mediawiki

commit 166ed5cd95a2f33d62dfd62242431a9c81c1c138

BlockUser: Provide correct information to the post-block hook
Previously, BlockUser::placeBlockInternal attempted to insert
the new block with possible failure due to an existing block.
Regardless of the result, it would then retrieve the current
block which could be the just now inserted block. This one
was treated as the prior block and incorrectly provided to
the hook as such.

Refactor the method a little by retrieving the prior block
before attempting to insert a new one to make it more
efficient and to ensure the hook will have the correct value
for the prior block.

Change-Id: I70480ce32545f28f8317bb58f9c724d64cc54ccf

includes/block/BlockUser.php
@@ -549,30 +549,31 @@ class BlockUser {
 			return $status;
 		}
 
-		// Try to insert block. Is there a conflicting block?
-		$insertStatus = $this->databaseBlockStore->insertBlock( $block );
-		$priorBlock = DatabaseBlock::newFromTarget( $this->target );
+		// Is there a conflicting block?
+		$priorBlock = DatabaseBlock::newFromTarget( $this->target, null, /*fromPrimary=*/true );
+
 		$isReblock = false;
-		if ( !$insertStatus ) {
-			// Reblock if the caller wants so
-			if ( $reblock ) {
-
-				if ( $priorBlock === null ) {
-					$this->logger->warning( 'Block could not be inserted. No existing block was found.' );
-					return Status::newFatal( 'ipb-block-not-found', $block->getTargetName() );
-				}
-
-				if ( $block->equals( $priorBlock ) ) {
-					// Block settings are equal => user is already blocked
-					return Status::newFatal( 'ipb_already_blocked', $block->getTargetName() );
-				}
-
-				$currentBlock = $this->configureBlock( $priorBlock );
-				$this->databaseBlockStore->updateBlock( $currentBlock ); // TODO handle failure
-				$isReblock = true;
-				$block = $currentBlock;
-			} else {
+		if ( $priorBlock !== null ) {
+			// Reblock only if the caller wants so
+			if ( !$reblock ) {
 				return Status::newFatal( 'ipb_already_blocked', $block->getTargetName() );
+			}
+
+			if ( $block->equals( $priorBlock ) ) {
+				// Block settings are equal => user is already blocked
+				return Status::newFatal( 'ipb_already_blocked', $block->getTargetName() );
+			}
+
+			$currentBlock = $this->configureBlock( $priorBlock );
+			$this->databaseBlockStore->updateBlock( $currentBlock ); // TODO handle failure
+			$isReblock = true;
+			$block = $currentBlock;
+		} else {
+			// Try to insert block.
+			$insertStatus = $this->databaseBlockStore->insertBlock( $block );
+			if ( !$insertStatus ) {
+				$this->logger->warning( 'Block could not be inserted. No existing block was found.' );
+				return Status::newFatal( 'ipb-block-not-found', $block->getTargetName() );
 			}
 		}
 

tests/phpunit/integration/includes/block/BlockUserTest.php
@@ -108,4 +108,107 @@ class BlockUserTest extends MediaWikiIntegrationTestCase {
 		$this->assertFalse( $status->isOK() );
 		$this->assertTrue( $status->hasMessage( 'cant-block-nonexistent-page' ) );
 	}
+
+	/**
+	 * @covers MediaWiki\Block\BlockUser::placeBlockInternal
+	 */
+	public function testReblock() {
+		$blockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test block'
+		)->placeBlockUnsafe();
+		$this->assertTrue( $blockStatus->isOK() );
+		$priorBlock = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $priorBlock );
+		$this->assertSame( 'test block', $priorBlock->getReasonComment()->text );
+
+		$blockId = $priorBlock->getId();
+
+		$reblockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test reblock'
+		)->placeBlockUnsafe( /*reblock=*/false );
+		$this->assertFalse( $reblockStatus->isOK() );
+
+		$this->user->clearInstanceCache();
+		$block = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $block );
+		$this->assertSame( $blockId, $block->getId() );
+
+		$reblockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test block'
+		)->placeBlockUnsafe( /*reblock=*/true );
+		$this->assertFalse( $reblockStatus->isOK() );
+
+		$this->user->clearInstanceCache();
+		$block = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $block );
+		$this->assertSame( $blockId, $block->getId() );
+
+		$reblockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test reblock'
+		)->placeBlockUnsafe( /*reblock=*/true );
+		$this->assertTrue( $reblockStatus->isOK() );
+
+		$this->user->clearInstanceCache();
+		$block = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $block );
+		$this->assertSame( 'test reblock', $block->getReasonComment()->text );
+	}
+
+	/**
+	 * @covers MediaWiki\Block\BlockUser::placeBlockInternal
+	 */
+	public function testPostHook() {
+		$hookBlock = false;
+		$hookPriorBlock = false;
+		$this->setTemporaryHook(
+			'BlockIpComplete',
+			static function ( $block, $legacyUser, $priorBlock )
+			use ( &$hookBlock, &$hookPriorBlock )
+			{
+				$hookBlock = $block;
+				$hookPriorBlock = $priorBlock;
+			}
+		);
+
+		$blockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test block'
+		)->placeBlockUnsafe();
+		$this->assertTrue( $blockStatus->isOK() );
+		$priorBlock = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $priorBlock );
+		$this->assertSame( $priorBlock->getId(), $hookBlock->getId() );
+		$this->assertNull( $hookPriorBlock );
+
+		$hookBlock = false;
+		$hookPriorBlock = false;
+		$reblockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test reblock'
+		)->placeBlockUnsafe( /*reblock=*/true );
+		$this->assertTrue( $reblockStatus->isOK() );
+
+		$this->user->clearInstanceCache();
+		$newBlock = $this->user->getBlock();
+		$this->assertInstanceOf( DatabaseBlock::class, $newBlock );
+		$this->assertSame( $newBlock->getId(), $hookBlock->getId() );
+		$this->assertSame( $priorBlock->getId(), $hookPriorBlock->getId() );
+	}
+
 }

commit 1902efbcc76631f614ef2803c8aefb6ed73feb54

BlockUser: Restore blocking autoblocked IP addresses
This seems to be a regression from 166ed5c.
DatabaseBlock::newFromTarget may return an autoblock for an IP
unless there is an existing manual block. If so, ignore it
because it is allowed to have an IP address both manually
blocked and autoblocked (see ipb_address_unique index).

Also add an integration test case.

Bug: T287798
Change-Id: I7c9a66ba0ffe759f43f4d0821c30fb94649d3dea

includes/block/BlockUser.php
@@ -555,6 +555,18 @@ class BlockUser {
 		// xxx: there is an identical call at the beginning of ::placeBlock
 		$priorBlock = DatabaseBlock::newFromTarget( $this->target, null, /*fromPrimary=*/true );
 
+		// T287798: we are blocking an IP that is currently autoblocked
+		// we can ignore the block because ipb_address_unique allows the IP address
+		// be both manually blocked and autoblocked
+		// this will work as long as DatabaseBlock::newLoad prefers manual IP blocks
+		// over autoblocks
+		if ( $priorBlock !== null
+			&& $priorBlock->getType() === AbstractBlock::TYPE_AUTO
+			&& $this->targetType === AbstractBlock::TYPE_IP
+		) {
+			$priorBlock = null;
+		}
+
 		$isReblock = false;
 		if ( $priorBlock !== null ) {
 			// Reblock only if the caller wants so

tests/phpunit/integration/includes/block/BlockUserTest.php
@@ -211,4 +211,53 @@ class BlockUserTest extends MediaWikiIntegrationTestCase {
 		$this->assertSame( $priorBlock->getId(), $hookPriorBlock->getId() );
 	}
 
+	/**
+	 * @covers MediaWiki\Block\BlockUser::placeBlockInternal
+	 */
+	public function testIPBlockAllowedAutoblockPreserved() {
+		$blockStatus = $this->blockUserFactory->newBlockUser(
+			$this->user,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test block with autoblocking',
+			[ 'isAutoblocking' => true ]
+		)->placeBlockUnsafe();
+		$this->assertTrue( $blockStatus->isOK() );
+		$block = $blockStatus->getValue();
+
+		$target = '1.2.3.4';
+		$autoBlockId = $block->doAutoblock( $target );
+		$this->assertNotFalse( $autoBlockId );
+
+		$hookPriorBlock = false;
+		$this->setTemporaryHook(
+			'BlockIpComplete',
+			static function ( $block, $legacyUser, $priorBlock )
+			use ( &$hookPriorBlock )
+			{
+				$hookPriorBlock = $priorBlock;
+			}
+		);
+
+		$IPBlockStatus = $this->blockUserFactory->newBlockUser(
+			$target,
+			$this->mockAnonUltimateAuthority(),
+			'infinity',
+			'test IP block'
+		)->placeBlockUnsafe();
+		$this->assertTrue( $IPBlockStatus->isOK() );
+		$IPBlock = $IPBlockStatus->getValue();
+		$this->assertInstanceOf( DatabaseBlock::class, $IPBlock );
+		$this->assertNull( $hookPriorBlock );
+
+		$blockIds = array_map(
+			static function ( DatabaseBlock $block ) {
+				return $block->getId();
+			},
+			DatabaseBlock::newListFromTarget( $target, null, /*fromPrimary=*/true )
+		);
+		$this->assertContains( $autoBlockId, $blockIds );
+		$this->assertContains( $IPBlock->getId(), $blockIds );
+	}
+
 }