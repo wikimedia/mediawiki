5312ac54972ace22e59e1b8eb09219220b6b4b93
Kunal Mehta <legoktm@debian.org>
2021-08-19 09:07:50 -0700

    http: Add $wgLocalHTTPProxy to set a proxy for local requests
    Currently requests to domains listed in $wgLocalVirtualHosts bypass
    use of the standard $wgHttpProxy. With WMF's migration to Kubernetes, we
    limit outgoing traffic in a much stricter manner, so even internal
    requests will need to go over a proxy (e.g. Envoy).
    
    If the domain passes MWHttpRequest::isLocalURL(), then $wgLocalHTTPProxy
    will be used if set, otherwise no proxy will be used (current behavior).
    
    Bug: T288848
    Change-Id: Ifd0cbab02fa8f14a82ca34ebc7ad95b2be174434

--- includes/http/MWHttpRequest.php
+++ includes/http/MWHttpRequest.php
@@ -224,17 +224,23 @@ abstract class MWHttpRequest implements LoggerAwareInterface {
 	 * @return void
 	 */
 	protected function proxySetup() {
-		// If there is an explicit proxy set and proxies are not disabled, then use it
-		if ( $this->proxy && !$this->noProxy ) {
+		global $wgHTTPProxy, $wgLocalHTTPProxy;
+		// If proxies are disabled, clear any other proxy
+		if ( $this->noProxy ) {
+			$this->proxy = '';
 			return;
 		}
 
-		// Otherwise, fallback to $wgHTTPProxy if this is not a machine
-		// local URL and proxies are not disabled
-		if ( self::isLocalURL( $this->url ) || $this->noProxy ) {
-			$this->proxy = '';
+		// If there is an explicit proxy already set, use it
+		if ( $this->proxy ) {
+			return;
+		}
+
+		// Otherwise, fallback to $wgLocalHTTPProxy for local URLs
+		// or $wgHTTPProxy for everything else
+		if ( self::isLocalURL( $this->url ) ) {
+			$this->proxy = (string)$wgLocalHTTPProxy;
 		} else {
-			global $wgHTTPProxy;
 			$this->proxy = (string)$wgHTTPProxy;
 		}
 	}
