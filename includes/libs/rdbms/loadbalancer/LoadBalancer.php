bea440aa3355d38adf926f05435c5e36e96478a8
James D. Forrester <jforrester@wikimedia.org>
2021-09-07 12:49:01 -0700

    ILoadBalancer/ILBFactory::hasOrMadeRecentMasterChanges: Hard deprecate now it's unused in prod
    Bug: T282894
    Depends-On: I832df66933d4ac2db8ccbdd22afb4f8d86adb065
    Depends-On: Ibd33a214a4a2b7c049748a5fcfda860daef4267e
    Depends-On: Iff2f634b3f32f6a16ef2b461719d89af6ade13d4
    Depends-On: I14cafb467b419a9320c529a75c3ca77193886655
    Change-Id: I501706abe5ad8095604747a93996d8f08e12c686

--- includes/libs/rdbms/loadbalancer/LoadBalancer.php
+++ includes/libs/rdbms/loadbalancer/LoadBalancer.php
@@ -2159,7 +2159,7 @@ class LoadBalancer implements ILoadBalancer {
 	}
 
 	public function hasOrMadeRecentMasterChanges( $age = null ) {
-		// wfDeprecated( __METHOD__, '1.37' );
+		wfDeprecated( __METHOD__, '1.37' );
 		return $this->hasOrMadeRecentPrimaryChanges( $age );
 	}
 
