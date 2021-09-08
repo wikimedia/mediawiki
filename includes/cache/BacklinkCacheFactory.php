96bc83e8ebaa69f2f4c48c1fdd06e4097a996ae6
Derick Alangi <alangiderick@gmail.com>
2021-09-08 18:19:11 +0100

    Add BacklinkCacheFactory Service
    Bug: T279433
    Change-Id: I2943935e2d8148fce4457f76eca0234be72a5a5a

---
+++ includes/cache/BacklinkCacheFactory.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Remember the page that was previously loaded.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Cache
+ */
+
+namespace MediaWiki\Cache;
+
+use BacklinkCache;
+use MediaWiki\Page\PageReference;
+use WANObjectCache;
+
+/**
+ * @since 1.37
+ */
+class BacklinkCacheFactory {
+	/** @var BacklinkCache */
+	private $latestBacklinkCache;
+
+	/** @var WANObjectCache */
+	private $wanCache;
+
+	/**
+	 * @param WANObjectCache $wanCache
+	 */
+	public function __construct( WANObjectCache $wanCache ) {
+		$this->wanCache = $wanCache;
+	}
+
+	/**
+	 * Returns a BacklinkCache for $page. May re-use previously
+	 * created instances.
+	 *
+	 * Currently, only one cache instance can exist; callers that
+	 * need multiple backlink cache objects should keep them in scope.
+	 *
+	 * @param PageReference $page Page to get a backlink cache for
+	 * @return BacklinkCache
+	 */
+	public function getBacklinkCache( PageReference $page ): BacklinkCache {
+		if ( !$this->latestBacklinkCache || !$this->latestBacklinkCache->getPage()->isSamePageAs( $page ) ) {
+			$this->latestBacklinkCache = new BacklinkCache( $this->wanCache, $page );
+		}
+		return $this->latestBacklinkCache;
+	}
+}
