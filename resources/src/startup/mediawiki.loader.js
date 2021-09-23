28173a23b7dea163be755ec15317720a8c3c914c
Krinkle <krinklemail@gmail.com>
2021-09-23 00:37:51 +0000

    Revert "resourceloader: add local variable for mw.loader"
    This reverts commit 0faa22fc6d3294a99ee622b3efbf258d9f7a9590.
    
    Reason for revert: Gzip is usually (and in this case) more efficient at
    natively deduplicating repeated strings when transferring code from
    server to client, than is possible through manual abbreviating through
    a variable.
    
    Fresnel confirmed this patch reduces JS transfer size by 10B. Not much,
    but enough for the optimisation to not be needed.
    
    Change-Id: I4ad22ca1220f33f018a6bc7e3da8389246cf5f9c

--- resources/src/startup/mediawiki.loader.js
+++ resources/src/startup/mediawiki.loader.js
@@ -11,7 +11,6 @@
 
 	var StringSet,
 		store,
-		loader,
 		hasOwn = Object.hasOwnProperty;
 
 	function defineFallbacks() {
@@ -349,7 +348,7 @@
 	 */
 	function allReady( modules ) {
 		for ( var i = 0; i < modules.length; i++ ) {
-			if ( loader.getState( modules[ i ] ) !== 'ready' ) {
+			if ( mw.loader.getState( modules[ i ] ) !== 'ready' ) {
 				return false;
 			}
 		}
@@ -379,7 +378,7 @@
 	 */
 	function anyFailed( modules ) {
 		for ( var i = 0; i < modules.length; i++ ) {
-			var state = loader.getState( modules[ i ] );
+			var state = mw.loader.getState( modules[ i ] );
 			if ( state === 'error' || state === 'missing' ) {
 				return modules[ i ];
 			}
@@ -695,7 +694,7 @@
 			var fileName = resolveRelativePath( moduleName, basePath );
 			if ( fileName === null ) {
 				// Not a relative path, so it's a module name
-				return loader.require( moduleName );
+				return mw.loader.require( moduleName );
 			}
 
 			if ( hasOwn.call( moduleObj.packageExports, fileName ) ) {
@@ -765,7 +764,7 @@
 		pendingRequests.push( function () {
 			// Keep in sync with execute()/runScript().
 			if ( moduleName !== 'jquery' ) {
-				window.require = loader.require;
+				window.require = mw.loader.require;
 				window.module = registry[ moduleName ].module;
 			}
 			addScript( src, function () {
@@ -882,7 +881,7 @@
 			}
 		} );
 
-		loader.work();
+		mw.loader.work();
 	}
 
 	/**
@@ -939,7 +938,7 @@
 					} else {
 						// Pass jQuery twice so that the signature of the closure which wraps
 						// the script can bind both '$' and 'jQuery'.
-						script( window.$, window.$, loader.require, registry[ module ].module );
+						script( window.$, window.$, mw.loader.require, registry[ module ].module );
 					}
 					markModuleReady();
 				} else if ( typeof script === 'object' && script !== null ) {
@@ -1254,7 +1253,7 @@
 							modules[ i ].length + 3; // '%7C'.length == 3
 
 					// If the url would become too long, create a new one, but don't create empty requests
-					if ( currReqModules.length && l + bytesAdded > loader.maxQueryLength ) {
+					if ( currReqModules.length && l + bytesAdded > mw.loader.maxQueryLength ) {
 						// Dispatch what we've got...
 						doRequest();
 						// .. and start again.
@@ -1370,9 +1369,7 @@
 
 	/* Public Members */
 
-	// We use a local variable `loader` so that its easier to access, but also need to set
-	// this as mw.loader so its exported - combine the two
-	mw.loader = loader = {
+	mw.loader = {
 		/**
 		 * The module registry is exposed as an aid for debugging and inspecting page
 		 * state; it is not a public interface for modifying the registry.
@@ -1424,7 +1421,7 @@
 				var module = queue[ q ];
 				// Only consider modules which are the initial 'registered' state,
 				// and ignore duplicates
-				if ( loader.getState( module ) === 'registered' &&
+				if ( mw.loader.getState( module ) === 'registered' &&
 					!batch.has( module )
 				) {
 					// Progress the state machine
@@ -1592,7 +1589,7 @@
 				version = split.version;
 			// Automatically register module
 			if ( !( name in registry ) ) {
-				loader.register( name );
+				mw.loader.register( name );
 			}
 			// Check for duplicate implementation
 			if ( registry[ name ].script !== undefined ) {
@@ -1669,7 +1666,7 @@
 		state: function ( states ) {
 			for ( var module in states ) {
 				if ( !( module in registry ) ) {
-					loader.register( module );
+					mw.loader.register( module );
 				}
 				setAndPropagate( module, states[ module ] );
 			}
@@ -1712,7 +1709,7 @@
 		 */
 		require: function ( moduleName ) {
 			// Only ready modules can be required
-			if ( loader.getState( moduleName ) !== 'ready' ) {
+			if ( mw.loader.getState( moduleName ) !== 'ready' ) {
 				// Module may've forgotten to declare a dependency
 				throw new Error( 'Module "' + moduleName + '" is not loaded' );
 			}
@@ -1772,7 +1769,7 @@
 
 	// We use a local variable `store` so that its easier to access, but also need to set
 	// this in mw.loader so its exported - combine the two
-	loader.store = store = {
+	mw.loader.store = store = {
 		// Whether the store is in use on this page.
 		enabled: null,
 
