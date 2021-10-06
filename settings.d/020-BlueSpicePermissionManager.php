2c635a6b937cbda92700a177c0d4f6e4fd10a6d8
rvogel <vogel@hallowelt.biz>
2021-10-06 15:20:23 +0200

    Disable `custom` permission preset
    ERM25948
    
    [4.x]

--- settings.d/020-BlueSpicePermissionManager.php
+++ settings.d/020-BlueSpicePermissionManager.php
@@ -1,2 +1,4 @@
 <?php
 wfLoadExtension("BlueSpicePermissionManager");
+$GLOBALS['bsgPermissionManagerActivePreset'] = 'private';
+$GLOBALS['bsgOverridePermissionManagerAllowedPresets'] = [ 'public', 'protected', 'private' ];
