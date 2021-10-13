3893aab376d34cbd674246d31283b060a0be63bf
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2021-10-13 15:44:41 +0300

    Add S'gaw Karen (ksw) to Names.php
    Bug: T292612
    Change-Id: I101a8f53b688730a52e4f031f43f1b875720587b

---
+++ languages/messages/MessagesKsw.php
@@ -0,0 +1,12 @@
+<?php
+/** S'gaw Karen (စှီၤ)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Amir E. Aharoni
+ */
+
+$fallback = 'my';
