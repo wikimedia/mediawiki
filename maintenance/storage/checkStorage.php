4458701a71359f703ff45fa7fe06299476c4a33f
Universal Omega <1010delta1010@gmail.com>
2021-10-21 06:49:01 +0000

    Fix checkStorage.php
    Fixes checkStorage.php --fix
    
    * importRevision() is required to be public for WikiImporter::setRevisionCallback()
    * $revision must not be passed by reference
    * $importer is unused, so removed
    
    Change-Id: I53ef3825308af3b6e632cb88b9a3eb2f40ab5239

--- maintenance/storage/checkStorage.php
+++ maintenance/storage/checkStorage.php
@@ -512,7 +512,7 @@ class CheckStorage {
 		$importer->doImport();
 	}
 
-	private function importRevision( &$revision, &$importer ) {
+	public function importRevision( $revision ) {
 		$id = $revision->getID();
 		$content = $revision->getContent( RevisionRecord::RAW );
 		$id = $id ?: '';
