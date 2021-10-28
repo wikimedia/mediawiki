1c423cd49c9b6bcce8694cf2a5e2420fa78f9820
rvogel <vogel@hallowelt.biz>
2021-10-28 18:01:29 +0200

    Fix `bluespice-cmd buildtarball` issue

---
+++ _bluespice/pre-autoload-dump.d/06-remove_broken_symlink_from_vendor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Helper script to avoid issue `Could not get real path for `_build/bluespice/vendor/pear/net_smtp/README.rst`" with `bluespice-cmd buildtarball`
+# Copyright: 2021
+# License: GPLv3
+
+rm vendor/pear/net_smtp/README.rst
