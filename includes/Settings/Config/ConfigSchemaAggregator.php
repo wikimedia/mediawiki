7f5d76d7061b8df8feb8f9511f6ab04d3c735765
Petr Pchelko <ppchelko@wikimedia.org>
2021-11-12 11:56:33 -0800

    SettingsBuilder: Apply merge strategy from the schema to the config
    Change-Id: Ia75f501c29d7589782cbbe99f723841dcd2acb75

---
+++ includes/Settings/Config/ConfigSchemaAggregator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace MediaWiki\Settings\Config;
+
+use MediaWiki\Settings\SettingsBuilderException;
+
+/**
+ * Aggregates multiple config schema arrays into a single config schema.
+ */
+class ConfigSchemaAggregator {
+
+	/** @var array */
+	private $schema = [];
+
+	/**
+	 * Add a config schema to the aggregator.
+	 *
+	 * @param array $newSchema an associating array with config key as key
+	 *   and a schema for the config value as value.
+	 * @param string $sourceName
+	 */
+	public function addSchemas( array $newSchema, string $sourceName = 'unknown' ) {
+		$schemaOverrides = array_intersect_key( $this->schema, $newSchema );
+		if ( !empty( $schemaOverrides ) ) {
+			throw new SettingsBuilderException( 'Overriding config schema in {source}', [
+				'source' => $sourceName,
+				'override_keys' => implode( ',', array_keys( $schemaOverrides ) ),
+			] );
+		}
+		$this->schema = array_merge( $this->schema, $newSchema );
+	}
+
+	/**
+	 * Check whether schema for $key is defined.
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function hasSchemaFor( string $key ): bool {
+		return array_key_exists( $key, $this->schema );
+	}
+
+	/**
+	 * Check if the $key has a default values set in the schema.
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function hasDefaultFor( string $key ): bool {
+		return array_key_exists( $key, $this->schema ) &&
+			array_key_exists( 'default', $this->schema[$key] );
+	}
+
+	/**
+	 * Get default value for the $key.
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function getDefaultFor( string $key ) {
+		return $this->schema[$key]['default'];
+	}
+
+	/**
+	 * Get the merge strategy defined for the $key, or null if none defined.
+	 *
+	 * @param string $key
+	 * @return MergeStrategy|null
+	 * @throws SettingsBuilderException if merge strategy name is invalid.
+	 */
+	public function getMergeStrategyFor( string $key ): ?MergeStrategy {
+		$strategyName = $this->schema[$key]['mergeStrategy'] ?? null;
+		if ( $strategyName === null ) {
+			return null;
+		}
+		return MergeStrategy::newFromName( $strategyName );
+	}
+}
