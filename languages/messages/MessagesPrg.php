28ae56486d0375c8955ddbb1a43165113c5bfc26
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2021-11-13 11:20:23 +0200

    Update configuration for the Prussian language (prg)
    * Add MessagesPrg.php with namespace names and linkTrail.
    * Change the autonym in Names.php: the uppercase letter
      is not necessary.
    
    Contributed by User:Nērtiks Jōrgesuns on translatewiki.
    
    Change-Id: Ia5b4c1627fd796f2849d24cd7396d7c5fdef004f

---
+++ languages/messages/MessagesPrg.php
@@ -0,0 +1,31 @@
+<?php
+/** Prussian (prūsiskan)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Nērtiks Jōrgesuns
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Mēdijai',
+	NS_SPECIAL          => 'Speciālin',
+	NS_TALK             => 'Diskusiōni',
+	NS_USER             => 'Tērpautajs',
+	NS_USER_TALK        => 'Tērpautajas_diskusiōni',
+	NS_PROJECT_TALK     => '$1_diskusiōni',
+	NS_FILE             => 'Zūrbrukis',
+	NS_FILE_TALK        => 'Zūrbrukes_diskusiōni',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_diskusiōni',
+	NS_TEMPLATE         => 'Šablōni',
+	NS_TEMPLATE_TALK    => 'Šablōnis_diskusiōni',
+	NS_HELP             => 'Pagalba',
+	NS_HELP_TALK        => 'Pagalbas_diskusiōni',
+	NS_CATEGORY         => 'Kategōrija',
+	NS_CATEGORY_TALK    => 'Kategōrijas_diskusiōni',
+];
+
+$linkTrail = '/^([A-Za-zĀāḐḑĒēĢģĪīĶķŅņŌōŖŗŠšŢţŪū]+)(.*)$/sDu';
