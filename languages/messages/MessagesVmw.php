0407689e8c9551a746d514f06d5fa61ebe5da1ad
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2021-11-28 14:32:30 +0200

    Add Makhuwa (vmw) to Names.php
    Bug: T296612
    Change-Id: I70b3ca826a7c74ed2894a2c9456b2800da9e7633

---
+++ languages/messages/MessagesVmw.php
@@ -0,0 +1,15 @@
+<?php
+/** Makhuwa (emakhuwa)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Amir E. Aharoni
+ */
+
+$fallback = 'pt';
+
+// Override the fallback
+$namespaceGenderAliases = [];
