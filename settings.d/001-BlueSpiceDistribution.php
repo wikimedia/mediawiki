1e2f2ab27812a5ff276cb4e42d11166a359daca1
miriamschlindwein <miriam.schlindwein@stud.uni-regensburg.de>
2021-12-01 10:29:29 +0100

    Move footer icons for both skins (#211)
    ERM26619
    
    Co-authored-by: miriam schlindwein <schlindwein@hallowelt.com>

--- settings.d/001-BlueSpiceDistribution.php
+++ settings.d/001-BlueSpiceDistribution.php
@@ -40,3 +40,42 @@ $GLOBALS['wgGroupTypes'] = [
 	'smwadministrator' => 'extension-extended',
 	'widgeteditor'     => 'extension-extended'
 ];
+
+/*
+ * Use an other image for MediaWiki.org
+ */
+$GLOBALS['wgFooterIcons']['poweredby']['mediawiki']['src'] = $wgScriptPath . "/extensions/BlueSpiceDistributionConnector/resources/images/footer/MediaWiki.png";
+$GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] += [
+	'height' => '27',
+	'width' => '149'
+];
+
+/*
+ * We want to use an other image for this extensions but the config files are processed to early.
+ * So we have to set the complete items.
+ */
+$GLOBALS['wgFooterIcons']['poweredby'] += [
+	'bluespice' => [
+		'src' => $wgScriptPath . '/extensions/BlueSpiceDistributionConnector/resources/images/footer/BlueSpice.png',
+		'url' => 'http://bluespice.com',
+		'alt' => 'Powered by BlueSpice',
+		'height' => '27',
+		'width' => '149'
+	]
+];
+
+if ( array_key_exists( 'semanticmediawiki', $GLOBALS['wgFooterIcons']['poweredby'] ) ) {
+	$footerIcons = [
+		'mediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'],
+		'bluespice' => $GLOBALS['wgFooterIcons']['poweredby']['bluespice'],
+		'semanticmediawiki' => $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki']
+	];
+
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['mediawiki'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['bluespice'] );
+	unset( $GLOBALS['wgFooterIcons']['poweredby']['semanticmediawiki'] );
+
+	$footerIcons += $GLOBALS['wgFooterIcons']['poweredby'];
+
+	$GLOBALS['wgFooterIcons']['poweredby'] = $footerIcons;
+}
