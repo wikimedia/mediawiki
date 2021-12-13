3f028d61a1d65cf2f4896843386c058bcbb6789a
Kunal Mehta <legoktm@debian.org>
2021-12-12 18:38:47 -0800

    SECURITY: Fix permissions check in action=rollback (CVE-2021-45038)
    Because RollbackAction (as of 0a8403271109) overrided
    FormAction::show(), it was no longer checking that the user had the
    "rollback" userright. This restores that check, so people without the
    "rollback" right will not be able to even get to the rollback form.
    
    Then escape the user-supplied "from" parameter so it can't be used to
    reveal the contents of other pages through transclusion, e.g.
    "{{:Secret}}". wfEscapeWikiText() is also good practice for usernames in
    general, as they can contain markup like bullets or single quotes that
    affect output.
    
    Bug: T297574
    Change-Id: I7424f67f1217482b977f9617f0275c41fb94b60f

--- includes/actions/RollbackAction.php
+++ includes/actions/RollbackAction.php
@@ -121,6 +121,8 @@ class RollbackAction extends FormAction {
 	 */
 	public function show() {
 		$this->setHeaders();
+		// This will throw exceptions if there's a problem
+		$this->checkCanExecute( $this->getUser() );
 
 		if ( !$this->userOptionsLookup->getOption( $this->getUser(), 'showrollbackconfirmation' ) ||
 			$this->getRequest()->wasPosted()
@@ -151,7 +153,7 @@ class RollbackAction extends FormAction {
 		if ( $from !== $userText ) {
 			throw new ErrorPageError( 'rollbackfailed', 'alreadyrolled', [
 				$this->getTitle()->getPrefixedText(),
-				$from,
+				wfEscapeWikiText( $from ),
 				$userText
 			] );
 		}
