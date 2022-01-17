a02398740e1ce4d0b2f56b07ae3e1591faabdaa0
Jon Harald Søby <>
2022-01-17 13:27:26 +0100

    Add country-specific locales for Northern Sami
    Add se-FI, se-NO and se-SE for Finland, Norway and Sweden,
    respectively. The fallback languages for each are set to
    the country's major official languages.
    
    Bug: T299341
    Change-Id: I54959494336e439337f622ae3eebb127883db789

---
+++ languages/messages/MessagesSe_fi.php
@@ -0,0 +1,12 @@
+<?php
+/** Northern Sámi (Finland) (davvisámegiella (Suoma bealde))
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Jon Harald Søby
+ */
+
+$fallback = 'se, fi, sv';
---
+++ languages/messages/MessagesSe_no.php
@@ -0,0 +1,12 @@
+<?php
+/** Northern Sámi (Norway) (davvisámegiella (Norgga bealde))
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Jon Harald Søby
+ */
+
+$fallback = 'se, nb, nn';
