4a6e986141d27a55964be55f2fff0debf33203c6
Amir Sarabadani <ladsgroup@gmail.com>
2022-01-18 17:41:37 +0100

    Add linktarget table
    Bug: T299416
    Change-Id: Icae4513dd99635335857100d8a0c7102986933e5

--- maintenance/tables-generated.sql
+++ maintenance/tables-generated.sql
@@ -893,3 +893,12 @@ CREATE TABLE /*_*/searchindex (
   FULLTEXT INDEX si_title (si_title),
   FULLTEXT INDEX si_text (si_text)
 ) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE /*_*/linktarget (
+  lt_id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+  lt_namespace INT NOT NULL,
+  lt_title VARBINARY(255) NOT NULL,
+  UNIQUE INDEX lt_namespace_title (lt_namespace, lt_title),
+  PRIMARY KEY(lt_id)
+) /*$wgDBTableOptions*/;
