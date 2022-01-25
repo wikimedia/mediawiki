wikimedia/mediawiki

commit c82d30d19cc2b2182ddba9dcc84656dcf94ce84f

Move OrderedStreamingForkController class from CirrusSearch to core.
This class may be useful for scripts outside CirrusSearch.

Change-Id: I1ed993a3b0b64d828ae5caba6c4b540b77b2f6f5

includes/OrderedStreamingForkController.php
@@ -1,0 +1,216 @@
+<?php
+
+/**
+ * Reads lines of work from an input stream and farms them out to multiple
+ * child streams. Each child has exactly one piece of work in flight at a given
+ * moment. Writes the result of child's work to an output stream. If numProcs
+ * <= zero the work will be performed in process.
+ *
+ * This class amends ForkController with the requirement that the output is
+ * produced in the same exact order as input values were.
+ *
+ * Currently used by CirrusSearch extension to implement CLI search script.
+ *
+ * @ingroup Maintenance
+ * @since 1.30
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+class OrderedStreamingForkController extends ForkController {
+	/** @var callable */
+	protected $workCallback;
+	/** @var resource */
+	protected $input;
+	/** @var resource */
+	protected $output;
+	/** @var int */
+	protected $nextOutputId;
+	/** @var string[] Int key indicates order, value is data */
+	protected $delayedOutputData = [];
+
+	/**
+	 * @param int $numProcs The number of worker processes to fork
+	 * @param callable $workCallback A callback to call in the child process
+	 *  once for each line of work to process.
+	 * @param resource $input A socket to read work lines from
+	 * @param resource $output A socket to write the result of work to.
+	 */
+	public function __construct( $numProcs, $workCallback, $input, $output ) {
+		parent::__construct( $numProcs );
+		$this->workCallback = $workCallback;
+		$this->input = $input;
+		$this->output = $output;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function start() {
+		if ( $this->procsToStart > 0 ) {
+			$status = parent::start();
+			if ( $status === 'child' ) {
+				$this->consume();
+			}
+		} else {
+			$status = 'parent';
+			$this->consumeNoFork();
+		}
+		return $status;
+	}
+
+	/**
+	 * @param int $numProcs
+	 * @return string
+	 */
+	protected function forkWorkers( $numProcs ) {
+		$this->prepareEnvironment();
+
+		$childSockets = [];
+		// Create the child processes
+		for ( $i = 0; $i < $numProcs; $i++ ) {
+			$sockets = stream_socket_pair( STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP );
+			// Do the fork
+			$pid = pcntl_fork();
+			if ( $pid === -1 || $pid === false ) {
+				echo "Error creating child processes\n";
+				exit( 1 );
+			}
+
+			if ( !$pid ) {
+				$this->initChild();
+				$this->childNumber = $i;
+				$this->input = $sockets[0];
+				$this->output = $sockets[0];
+				fclose( $sockets[1] );
+				return 'child';
+			} else {
+				// This is the parent process
+				$this->children[$pid] = true;
+				fclose( $sockets[0] );
+				$childSockets[] = $sockets[1];
+			}
+		}
+		$this->feedChildren( $childSockets );
+		foreach ( $childSockets as $socket ) {
+			// if a child has already shutdown the sockets will be closed,
+			// closing a second time would raise a warning.
+			if ( is_resource( $socket ) ) {
+				fclose( $socket );
+			}
+		}
+		return 'parent';
+	}
+
+	/**
+	 * Child worker process. Reads work from $this->input and writes the
+	 * result of that work to $this->output when completed.
+	 */
+	protected function consume() {
+		while ( !feof( $this->input ) ) {
+			$line = trim( fgets( $this->input ) );
+			if ( $line ) {
+				list( $id, $data ) = json_decode( $line );
+				$result = call_user_func( $this->workCallback, $data );
+				fwrite( $this->output, json_encode( [ $id, $result ] ) . "\n" );
+			}
+		}
+	}
+
+	/**
+	 * Special cased version of self::consume() when no forking occurs
+	 */
+	protected function consumeNoFork() {
+		while ( !feof( $this->input ) ) {
+			$line = trim( fgets( $this->input ) );
+			if ( $line ) {
+				$result = call_user_func( $this->workCallback, $line );
+				fwrite( $this->output, "$result\n" );
+			}
+		}
+	}
+
+	/**
+	 * Reads lines of work from $this->input and farms them out to
+	 * the provided socket.
+	 *
+	 * @param resource[] $sockets
+	 */
+	protected function feedChildren( array $sockets ) {
+		$used = [];
+		$id = 0;
+		$this->nextOutputId = 0;
+
+		while ( !feof( $this->input ) ) {
+			$data = fgets( $this->input );
+			if ( $used ) {
+				do {
+					$this->updateAvailableSockets( $sockets, $used, $sockets ? 0 : 5 );
+				} while ( !$sockets );
+			}
+			$data = trim( $data );
+			if ( !$data ) {
+				continue;
+			}
+			$socket = array_pop( $sockets );
+			fwrite( $socket, json_encode( [ $id++, $data ] ) . "\n" );
+			$used[] = $socket;
+		}
+		while ( $used ) {
+			$this->updateAvailableSockets( $sockets, $used, 5 );
+		}
+	}
+
+	/**
+	 * Moves sockets from $used to $sockets when they are available
+	 * for more work
+	 *
+	 * @param resource[] &$sockets List of sockets that are waiting for work
+	 * @param resource[] &$used List of sockets currently performing work
+	 * @param int $timeout The number of seconds to block waiting. 0 for
+	 *  non-blocking operation.
+	 */
+	protected function updateAvailableSockets( &$sockets, &$used, $timeout ) {
+		$read = $used;
+		$write = $except = [];
+		stream_select( $read, $write, $except, $timeout );
+		foreach ( $read as $socket ) {
+			$line = fgets( $socket );
+			list( $id, $data ) = json_decode( trim( $line ) );
+			$this->receive( (int)$id, $data );
+			$sockets[] = $socket;
+			$idx = array_search( $socket, $used );
+			unset( $used[$idx] );
+		}
+	}
+
+	/**
+	 * @param int $id
+	 * @param string $data
+	 */
+	protected function receive( $id, $data ) {
+		if ( $id !== $this->nextOutputId ) {
+			$this->delayedOutputData[$id] = $data;
+			return;
+		}
+		fwrite( $this->output, $data . "\n" );
+		$this->nextOutputId = $id + 1;
+		while ( isset( $this->delayedOutputData[$this->nextOutputId] ) ) {
+			fwrite( $this->output, $this->delayedOutputData[$this->nextOutputId] . "\n" );
+			unset( $this->delayedOutputData[$this->nextOutputId] );
+			$this->nextOutputId++;
+		}
+	}
+}

autoload.php
@@ -1043,6 +1043,7 @@ $wgAutoloadLocalClasses = [
 	'OldLocalFile' => __DIR__ . '/includes/filerepo/file/OldLocalFile.php',
 	'OracleInstaller' => __DIR__ . '/includes/installer/OracleInstaller.php',
 	'OracleUpdater' => __DIR__ . '/includes/installer/OracleUpdater.php',
+	'OrderedStreamingForkController' => __DIR__ . '/includes/OrderedStreamingForkController.php',
 	'OrphanStats' => __DIR__ . '/maintenance/storage/orphanStats.php',
 	'Orphans' => __DIR__ . '/maintenance/orphans.php',
 	'OutputPage' => __DIR__ . '/includes/OutputPage.php',

commit 54c735354fc24014124b196aea4344b83e4fc841

Stop using is_resource() where possible
* Enforce the ban on is_resource in phpcs
* In OrderedStreamingForkController, the comment was incorrect. I
  confirmed using a small test script that if the child closes one end
  of a socket pair, the other end will still be open, and is_resource()
  will still return true, and fclose() will not fail. The issue was
  introduced in c82d30d19cc2b2182, it was not present in the
  CirrusSearch copy of the class.
* Allow is_resource() for debug logging.
* Allow is_resource() for parameter validation where a stream may be
  passed to a function, since there is no alternative.

Bug: T260735
Change-Id: I59a752f7bb4b043ddfb2434b52a02f9221bf54db

.phpcs.xml
@@ -17,7 +17,6 @@
 		<exclude name="MediaWiki.Usage.DeprecatedGlobalVariables.Deprecated$wgTitle" />
 		<exclude name="MediaWiki.Usage.DeprecatedGlobalVariables.Deprecated$wgVersion" />
 		<exclude name="MediaWiki.Usage.ExtendClassUsage.FunctionVarUsage" />
-		<exclude name="MediaWiki.Usage.ForbiddenFunctions.is_resource" />
 		<exclude name="MediaWiki.Usage.ForbiddenFunctions.passthru" />
 		<exclude name="MediaWiki.Usage.SuperGlobalsUsage.SuperGlobals" />
 		<exclude name="MediaWiki.WhiteSpace.SpaceBeforeSingleLineComment.NewLineComment" />

includes/OrderedStreamingForkController.php
@@ -105,11 +105,7 @@ class OrderedStreamingForkController extends ForkController {
 		}
 		$this->feedChildren( $childSockets );
 		foreach ( $childSockets as $socket ) {
-			// if a child has already shutdown the sockets will be closed,
-			// closing a second time would raise a warning.
-			if ( is_resource( $socket ) ) {
-				fclose( $socket );
-			}
+			fclose( $socket );
 		}
 		return 'parent';
 	}

includes/api/ApiResult.php
@@ -363,6 +363,7 @@ class ApiResult implements ApiSerializable {
 			}
 		} elseif ( $value !== null && !is_scalar( $value ) ) {
 			$type = gettype( $value );
+			// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 			if ( is_resource( $value ) ) {
 				$type .= '(' . get_resource_type( $value ) . ')';
 			}

includes/debug/logger/LegacyLogger.php
@@ -453,6 +453,7 @@ class LegacyLogger extends AbstractLogger {
 			return '[Object ' . get_class( $item ) . ']';
 		}
 
+		// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 		if ( is_resource( $item ) ) {
 			return '[Resource ' . get_resource_type( $item ) . ']';
 		}

includes/debug/logger/monolog/LegacyHandler.php
@@ -149,7 +149,7 @@ class LegacyHandler extends AbstractProcessingHandler {
 		}
 		restore_error_handler();
 
-		if ( !is_resource( $this->sink ) ) {
+		if ( !$this->sink ) {
 			$this->sink = null;
 			throw new UnexpectedValueException( sprintf(
 				'The stream or file "%s" could not be opened: %s',
@@ -223,7 +223,7 @@ class LegacyHandler extends AbstractProcessingHandler {
 	}
 
 	public function close(): void {
-		if ( is_resource( $this->sink ) ) {
+		if ( $this->sink ) {
 			if ( $this->useUdp() ) {
 				socket_close( $this->sink );
 

includes/libs/http/MultiHttpClient.php
@@ -373,6 +373,7 @@ class MultiHttpClient implements LoggerAwareInterface {
 
 		if ( $req['method'] === 'PUT' ) {
 			curl_setopt( $ch, CURLOPT_PUT, 1 );
+			// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 			if ( is_resource( $req['body'] ) ) {
 				curl_setopt( $ch, CURLOPT_INFILE, $req['body'] );
 				if ( isset( $req['headers']['content-length'] ) ) {
@@ -403,6 +404,7 @@ class MultiHttpClient implements LoggerAwareInterface {
 			curl_setopt( $ch, CURLOPT_POST, 1 );
 			curl_setopt( $ch, CURLOPT_POSTFIELDS, $req['body'] );
 		} else {
+			// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 			if ( is_resource( $req['body'] ) || $req['body'] !== '' ) {
 				throw new Exception( "HTTP body specified for a non PUT/POST request." );
 			}

includes/libs/objectcache/utils/MemcachedClient.php
@@ -348,7 +348,7 @@ class MemcachedClient {
 		}
 
 		$sock = $this->get_sock( $key );
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return false;
 		}
 
@@ -390,7 +390,7 @@ class MemcachedClient {
 		}
 
 		$sock = $this->get_sock( $key );
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return false;
 		}
 
@@ -483,7 +483,7 @@ class MemcachedClient {
 
 		$sock = $this->get_sock( $key );
 
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return false;
 		}
 
@@ -541,7 +541,7 @@ class MemcachedClient {
 		$socks = array();
 		foreach ( $keys as $key ) {
 			$sock = $this->get_sock( $key );
-			if ( !is_resource( $sock ) ) {
+			if ( !$sock ) {
 				continue;
 			}
 			$key = is_array( $key ) ? $key[1] : $key;
@@ -633,7 +633,7 @@ class MemcachedClient {
 	 * @return array Output array
 	 */
 	public function run_command( $sock, $cmd ) {
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return array();
 		}
 
@@ -895,7 +895,7 @@ class MemcachedClient {
 		for ( $tries = 0; $tries < 20; $tries++ ) {
 			$host = $this->_buckets[$hv % $this->_bucketcount];
 			$sock = $this->sock_to_host( $host );
-			if ( is_resource( $sock ) ) {
+			if ( $sock ) {
 				return $sock;
 			}
 			$hv = $this->_hashfunc( $hv . $realkey );
@@ -941,7 +941,7 @@ class MemcachedClient {
 		}
 
 		$sock = $this->get_sock( $key );
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return null;
 		}
 
@@ -1074,7 +1074,7 @@ class MemcachedClient {
 		}
 
 		$sock = $this->get_sock( $key );
-		if ( !is_resource( $sock ) ) {
+		if ( !$sock ) {
 			return false;
 		}
 
@@ -1310,7 +1310,7 @@ class MemcachedClient {
 	 * @param Resource $f
 	 */
 	function _flush_read_buffer( $f ) {
-		if ( !is_resource( $f ) ) {
+		if ( !$f ) {
 			return;
 		}
 		$r = array( $f );

includes/libs/rdbms/database/Database.php
@@ -6101,6 +6101,7 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 			: spl_object_hash( $this );
 
 		$description = $this->getType() . ' object #' . $id;
+		// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 		if ( is_resource( $this->conn ) ) {
 			$description .= ' (' . (string)$this->conn . ')'; // "resource id #<ID>"
 		} elseif ( is_object( $this->conn ) ) {

includes/resourceloader/ResourceLoaderImage.php
@@ -432,7 +432,7 @@ class ResourceLoaderImage {
 				$pipes
 			);
 
-			if ( is_resource( $process ) ) {
+			if ( $process ) {
 				fwrite( $pipes[0], $svg );
 				fclose( $pipes[0] );
 				$png = stream_get_contents( $pipes[1] );

includes/site/SiteExporter.php
@@ -38,6 +38,7 @@ class SiteExporter {
 	 * @param resource $sink A file handle open for writing
 	 */
 	public function __construct( $sink ) {
+		// phpcs:ignore MediaWiki.Usage.ForbiddenFunctions.is_resource
 		if ( !is_resource( $sink ) || get_resource_type( $sink ) !== 'stream' ) {
 			throw new InvalidArgumentException( '$sink must be a file handle' );
 		}