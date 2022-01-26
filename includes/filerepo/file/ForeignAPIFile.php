cdf7207df823269bea217a50c765e36d5278786b
Derk-Jan Hartman <hartman.wiki@gmail.com>
2022-01-26 01:01:36 +0100

    Support audio on filepage in InstantCommons
    The transform for ForeignAPIFile generally delegates transforms to the
    thumburl of the remote. This works well, unless the file in question
    has no inherent visual area, like audio files. Audio files on the file
    description page when using InstantCommons have a 0x0 size, and the
    Commons API returns errors for thumbnails of 0 size.
    
    Instead, since we already know we don't need a thumbnail url for audio
    fragments, just skip that step directly and simply pass an empty
    string thumburl to the HTML generator of TMH.
    
    Bug: T300085
    Change-Id: Iea0e16de97e5e5da8c84bdbed8a2961ad194c040

--- includes/filerepo/file/ForeignAPIFile.php
+++ includes/filerepo/file/ForeignAPIFile.php
@@ -134,13 +134,20 @@ class ForeignAPIFile extends File {
 		$otherParams = $this->handler->makeParamString( $params );
 		$width = $params['width'] ?? -1;
 		$height = $params['height'] ?? -1;
+		$thumbUrl = false;
 
-		$thumbUrl = $this->repo->getThumbUrlFromCache(
-			$this->getName(),
-			$width,
-			$height,
-			$otherParams
-		);
+		if ( $width > 0 && $height > 0 ) {
+			// Only query the remote if there are dimensions
+			$thumbUrl = $this->repo->getThumbUrlFromCache(
+				$this->getName(),
+				$width,
+				$height,
+				$otherParams
+			);
+		} elseif ( $this->getMediaType() === MEDIATYPE_AUDIO ) {
+			// This has no dimensions, but we still need to pass a value to getTransform()
+			$thumbUrl = '/';
+		}
 		if ( $thumbUrl === false ) {
 			global $wgLang;
 
