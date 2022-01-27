wikimedia/mediawiki

commit 014369874b469dfdf01b2a5c5577f2d7d13f55c9

rdbms: cleanup the use of QUERY_ flags to query() in Database
Change-Id: I9c538f392687db942250624d53ab564ebb2177a0

includes/libs/rdbms/database/Database.php
@@ -1129,7 +1129,8 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 	protected function isWriteQuery( $sql, $flags ) {
 		if (
 			$this->fieldHasBit( $flags, self::QUERY_CHANGE_ROWS ) ||
-			$this->fieldHasBit( $flags, self::QUERY_CHANGE_SCHEMA )
+			$this->fieldHasBit( $flags, self::QUERY_CHANGE_SCHEMA ) ||
+			$this->fieldHasBit( $flags, self::QUERY_CHANGE_LOCKS )
 		) {
 			return true;
 		} elseif ( $this->fieldHasBit( $flags, self::QUERY_CHANGE_NONE ) ) {
@@ -3720,8 +3721,11 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 	 */
 	public function textFieldSize( $table, $field ) {
 		$table = $this->tableName( $table );
-		$sql = "SHOW COLUMNS FROM $table LIKE \"$field\"";
-		$res = $this->query( $sql, __METHOD__, self::QUERY_CHANGE_NONE );
+		$res = $this->query(
+			"SHOW COLUMNS FROM $table LIKE \"$field\"",
+			__METHOD__,
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+		);
 		$row = $this->fetchObject( $res );
 
 		$m = [];
@@ -5795,7 +5799,7 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 		// https://dev.mysql.com/doc/refman/8.0/en/drop-table.html
 		// https://www.postgresql.org/docs/9.2/sql-truncate.html
 		$sql = "DROP TABLE " . $this->tableName( $table ) . " CASCADE";
-		$this->query( $sql, $fname, self::QUERY_IGNORE_DBO_TRX );
+		$this->query( $sql, $fname, self::QUERY_CHANGE_SCHEMA );
 	}
 
 	public function truncate( $tables, $fname = __METHOD__ ) {

includes/libs/rdbms/database/DatabaseMysqlBase.php
@@ -1036,7 +1036,7 @@ abstract class DatabaseMysqlBase extends Database {
 		$res = $this->query(
 			"SELECT IS_FREE_LOCK($encName) AS unlocked",
 			$method,
-			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+			self::QUERY_CHANGE_LOCKS
 		);
 		$row = $res->fetchObject();
 
@@ -1052,7 +1052,7 @@ abstract class DatabaseMysqlBase extends Database {
 		$res = $this->query(
 			"SELECT IF(GET_LOCK($encName,$timeout),UNIX_TIMESTAMP(SYSDATE(6)),NULL) AS acquired",
 			$method,
-			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+			self::QUERY_CHANGE_LOCKS
 		);
 		$row = $res->fetchObject();
 
@@ -1065,7 +1065,7 @@ abstract class DatabaseMysqlBase extends Database {
 		$res = $this->query(
 			"SELECT RELEASE_LOCK($encName) AS released",
 			$method,
-			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+			self::QUERY_CHANGE_LOCKS
 		);
 		$row = $res->fetchObject();
 

includes/libs/rdbms/database/DatabasePostgres.php
@@ -1252,14 +1252,14 @@ SQL;
 			$this->query(
 				'LOCK TABLE ONLY ' . implode( ',', $tablesWrite ) . ' IN EXCLUSIVE MODE',
 				$method,
-				self::QUERY_CHANGE_ROWS
+				self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_ROWS
 			);
 		}
 		if ( $tablesRead ) {
 			$this->query(
 				'LOCK TABLE ONLY ' . implode( ',', $tablesRead ) . ' IN SHARE MODE',
 				$method,
-				self::QUERY_CHANGE_ROWS
+				self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_ROWS
 			);
 		}
 
@@ -1274,7 +1274,7 @@ SQL;
 			"SELECT (CASE(pg_try_advisory_lock($key))
 			WHEN 'f' THEN 'f' ELSE pg_advisory_unlock($key) END) AS unlocked",
 			$method,
-			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+			self::QUERY_CHANGE_LOCKS
 		);
 		$row = $res->fetchObject();
 
@@ -1294,7 +1294,7 @@ SQL;
 						"ELSE NULL " .
 					"END) AS acquired",
 					$method,
-					self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_ROWS
+					self::QUERY_CHANGE_LOCKS
 				);
 				$row = $res->fetchObject();
 
@@ -1320,7 +1320,7 @@ SQL;
 		$result = $this->query(
 			"SELECT pg_advisory_unlock($key) AS released",
 			$method,
-			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_ROWS
+			self::QUERY_CHANGE_LOCKS
 		);
 		$row = $result->fetchObject();
 

includes/libs/rdbms/database/DatabaseSqlite.php
@@ -180,7 +180,7 @@ class DatabaseSqlite extends Database {
 		$this->currentDomain = new DatabaseDomain( $db, null, $tablePrefix );
 
 		try {
-			$flags = self::QUERY_IGNORE_DBO_TRX | self::QUERY_NO_RETRY;
+			$flags = self::QUERY_CHANGE_TRX | self::QUERY_NO_RETRY;
 			// Enforce LIKE to be case sensitive, just like MySQL
 			$this->query( 'PRAGMA case_sensitive_like = 1', __METHOD__, $flags );
 			// Set any connection-level custom PRAGMA options
@@ -334,7 +334,7 @@ class DatabaseSqlite extends Database {
 		return $this->query(
 			"ATTACH DATABASE $encFile AS $name",
 			$fname,
-			self::QUERY_IGNORE_DBO_TRX
+			self::QUERY_CHANGE_TRX
 		);
 	}
 
@@ -462,7 +462,7 @@ class DatabaseSqlite extends Database {
 		$res = $this->query(
 			"SELECT 1 FROM sqlite_master WHERE type='table' AND name=$encTable",
 			__METHOD__,
-			self::QUERY_IGNORE_DBO_TRX
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
 		);
 
 		return $res->numRows() ? true : false;
@@ -480,7 +480,7 @@ class DatabaseSqlite extends Database {
 	 */
 	public function indexInfo( $table, $index, $fname = __METHOD__ ) {
 		$sql = 'PRAGMA index_info(' . $this->addQuotes( $this->indexName( $index ) ) . ')';
-		$res = $this->query( $sql, $fname, self::QUERY_IGNORE_DBO_TRX );
+		$res = $this->query( $sql, $fname, self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE );
 		if ( !$res || $res->numRows() == 0 ) {
 			return false;
 		}
@@ -564,7 +564,11 @@ class DatabaseSqlite extends Database {
 		$encTable = $this->tableName( $table );
 		list( $sqlColumns, $sqlTuples ) = $this->makeInsertLists( $rows );
 		// https://sqlite.org/lang_insert.html
-		$this->query( "REPLACE INTO $encTable ($sqlColumns) VALUES $sqlTuples", $fname );
+		$this->query(
+			"REPLACE INTO $encTable ($sqlColumns) VALUES $sqlTuples",
+			$fname,
+			self::QUERY_CHANGE_ROWS
+		);
 	}
 
 	/**
@@ -664,8 +668,11 @@ class DatabaseSqlite extends Database {
 	 */
 	public function fieldInfo( $table, $field ) {
 		$tableName = $this->tableName( $table );
-		$sql = 'PRAGMA table_info(' . $this->addQuotes( $tableName ) . ')';
-		$res = $this->query( $sql, __METHOD__, self::QUERY_IGNORE_DBO_TRX );
+		$res = $this->query(
+			'PRAGMA table_info(' . $this->addQuotes( $tableName ) . ')',
+			__METHOD__,
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
+		);
 		foreach ( $res as $row ) {
 			if ( $row->name == $field ) {
 				return new SQLiteField( $row, $tableName );
@@ -677,9 +684,9 @@ class DatabaseSqlite extends Database {
 
 	protected function doBegin( $fname = '' ) {
 		if ( $this->trxMode != '' ) {
-			$this->query( "BEGIN {$this->trxMode}", $fname );
+			$this->query( "BEGIN {$this->trxMode}", $fname, self::QUERY_CHANGE_TRX );
 		} else {
-			$this->query( 'BEGIN', $fname );
+			$this->query( 'BEGIN', $fname, self::QUERY_CHANGE_TRX );
 		}
 	}
 
@@ -889,44 +896,50 @@ class DatabaseSqlite extends Database {
 	public function duplicateTableStructure(
 		$oldName, $newName, $temporary = false, $fname = __METHOD__
 	) {
-		$queryFlags = self::QUERY_PSEUDO_PERMANENT | self::QUERY_IGNORE_DBO_TRX;
-
 		$res = $this->query(
 			"SELECT sql FROM sqlite_master WHERE tbl_name=" .
 			$this->addQuotes( $oldName ) . " AND type='table'",
 			$fname,
-			$queryFlags
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
 		);
 		$obj = $res->fetchObject();
 		if ( !$obj ) {
 			throw new RuntimeException( "Couldn't retrieve structure for table $oldName" );
 		}
-		$sql = $obj->sql;
-		$sql = preg_replace(
+		$sqlCreateTable = $obj->sql;
+		$sqlCreateTable = preg_replace(
 			'/(?<=\W)"?' .
 				preg_quote( trim( $this->addIdentifierQuotes( $oldName ), '"' ), '/' ) .
 				'"?(?=\W)/',
 			$this->addIdentifierQuotes( $newName ),
-			$sql,
+			$sqlCreateTable,
 			1
 		);
 		if ( $temporary ) {
-			if ( preg_match( '/^\\s*CREATE\\s+VIRTUAL\\s+TABLE\b/i', $sql ) ) {
+			if ( preg_match( '/^\\s*CREATE\\s+VIRTUAL\\s+TABLE\b/i', $sqlCreateTable ) ) {
 				$this->queryLogger->debug(
 					"Table $oldName is virtual, can't create a temporary duplicate." );
 			} else {
-				$sql = str_replace( 'CREATE TABLE', 'CREATE TEMPORARY TABLE', $sql );
+				$sqlCreateTable = str_replace(
+					'CREATE TABLE',
+					'CREATE TEMPORARY TABLE',
+					$sqlCreateTable
+				 );
 			}
 		}
 
 		// @phan-suppress-next-line SecurityCheck-SQLInjection SQL is taken from database
-		$res = $this->query( $sql, $fname, $queryFlags );
+		$res = $this->query(
+			$sqlCreateTable,
+			$fname,
+			self::QUERY_CHANGE_SCHEMA | self::QUERY_PSEUDO_PERMANENT
+		);
 
 		// Take over indexes
 		$indexList = $this->query(
 			'PRAGMA INDEX_LIST(' . $this->addQuotes( $oldName ) . ')',
 			$fname,
-			$queryFlags
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
 		);
 		foreach ( $indexList as $index ) {
 			if ( strpos( $index->name, 'sqlite_autoindex' ) === 0 ) {
@@ -946,7 +959,7 @@ class DatabaseSqlite extends Database {
 			$indexInfo = $this->query(
 				'PRAGMA INDEX_INFO(' . $this->addQuotes( $index->name ) . ')',
 				$fname,
-				$queryFlags
+				self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
 			);
 			$fields = [];
 			foreach ( $indexInfo as $indexInfoRow ) {
@@ -955,7 +968,11 @@ class DatabaseSqlite extends Database {
 
 			$sqlIndex .= '(' . implode( ',', $fields ) . ')';
 
-			$this->query( $sqlIndex, __METHOD__ );
+			$this->query(
+				$sqlIndex,
+				__METHOD__,
+				self::QUERY_CHANGE_SCHEMA | self::QUERY_PSEUDO_PERMANENT
+			);
 		}
 
 		return $res;
@@ -973,7 +990,7 @@ class DatabaseSqlite extends Database {
 		$result = $this->query(
 			"SELECT name FROM sqlite_master WHERE type = 'table'",
 			$fname,
-			self::QUERY_IGNORE_DBO_TRX
+			self::QUERY_IGNORE_DBO_TRX | self::QUERY_CHANGE_NONE
 		);
 
 		$endArray = [];
@@ -999,7 +1016,7 @@ class DatabaseSqlite extends Database {
 
 		// No CASCADE support; https://www.sqlite.org/lang_droptable.html
 		$sql = "DROP TABLE " . $this->tableName( $table );
-		$this->query( $sql, $fname, self::QUERY_IGNORE_DBO_TRX );
+		$this->query( $sql, $fname, self::QUERY_CHANGE_SCHEMA );
 
 		return true;
 	}

includes/libs/rdbms/database/IDatabase.php
@@ -123,14 +123,16 @@ interface IDatabase {
 	public const QUERY_IGNORE_DBO_TRX = 8;
 	/** @var int Do not try to retry the query if the connection was lost */
 	public const QUERY_NO_RETRY = 16;
-	/** @var int Query is known to be a read-only Data Query Language query */
+	/** @var int Query is a read-only Data Query Language query */
 	public const QUERY_CHANGE_NONE = 32;
-	/** @var int Query is known to be a Transaction Control Language command */
+	/** @var int Query is a Transaction Control Language command (BEGIN, USE, SET, ...) */
 	public const QUERY_CHANGE_TRX = 64 | self::QUERY_IGNORE_DBO_TRX;
-	/** @var int Query is known to be a Data Manipulation Language command */
+	/** @var int Query is a Data Manipulation Language command (INSERT, DELETE, LOCK, ...) */
 	public const QUERY_CHANGE_ROWS = 128;
-	/** @var int Query is known to be a Data Definition Language command */
+	/** @var int Query is a Data Definition Language command */
 	public const QUERY_CHANGE_SCHEMA = 256 | self::QUERY_IGNORE_DBO_TRX;
+	/** @var int Query is a command for advisory locks */
+	public const QUERY_CHANGE_LOCKS = 512 | self::QUERY_IGNORE_DBO_TRX;
 
 	/** Flag to return the lock acquisition timestamp (null if not acquired) */
 	public const LOCK_TIMESTAMP = 1;

commit 6bc69591e3165c312e4c495fa0427c5f753cff49

Don't consider lock waits to be write queries
Don't count lock waits as write queries in the transaction profiler.
They are not replicated, so it doesn't make sense to count them.

I think 014369874b469dfdf0 accidentally inverted the meaning of
QUERY_CHANGE_LOCKS, since previously lock changes used
QUERY_CHANGE_NONE, which forced isWriteQuery() to return false.

Bug: T300194
Change-Id: I29a02c87156a36a5a094e226effb52d513cce76d

includes/libs/rdbms/database/Database.php
@@ -1129,11 +1129,13 @@ abstract class Database implements IDatabase, IMaintainableDatabase, LoggerAware
 	protected function isWriteQuery( $sql, $flags ) {
 		if (
 			$this->fieldHasBit( $flags, self::QUERY_CHANGE_ROWS ) ||
-			$this->fieldHasBit( $flags, self::QUERY_CHANGE_SCHEMA ) ||
-			$this->fieldHasBit( $flags, self::QUERY_CHANGE_LOCKS )
+			$this->fieldHasBit( $flags, self::QUERY_CHANGE_SCHEMA )
 		) {
 			return true;
-		} elseif ( $this->fieldHasBit( $flags, self::QUERY_CHANGE_NONE ) ) {
+		} elseif (
+			$this->fieldHasBit( $flags, self::QUERY_CHANGE_NONE ) ||
+			$this->fieldHasBit( $flags, self::QUERY_CHANGE_LOCKS )
+		) {
 			return false;
 		}
 		// BEGIN and COMMIT queries are considered read queries here.