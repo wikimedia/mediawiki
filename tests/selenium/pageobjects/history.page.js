ab4b1b2b6dd65a4a3874dffe824412432a8444f7
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-01-27 16:15:17 +0100

    [selenium] Remove leftovers from rollback tests
    Leftover from Ieaccf37ad016f2e811aeb98d2bd2f061c9b13fbf
    
    Bug: T280652
    Change-Id: I87c8440d970bf0ae33160fb2031abb9e07dcc76f

--- tests/selenium/pageobjects/history.page.js
+++ tests/selenium/pageobjects/history.page.js
@@ -1,31 +1,13 @@
 'use strict';
 
 const Page = require( 'wdio-mediawiki/Page' );
-const Util = require( 'wdio-mediawiki/Util' );
 
 class HistoryPage extends Page {
 	get heading() { return $( '#firstHeading' ); }
 	get comment() { return $( '#pagehistory .comment' ); }
-	get rollback() { return $( '.mw-rollback-link' ); }
-	get rollbackLink() { return $( '.mw-rollback-link a' ); }
-	get rollbackConfirmable() { return $( '.mw-rollback-link .jquery-confirmable-text' ); }
-	get rollbackConfirmableYes() { return $( '.mw-rollback-link .jquery-confirmable-button-yes' ); }
-	get rollbackConfirmableNo() { return $( '.mw-rollback-link .jquery-confirmable-button-no' ); }
-	get rollbackNonJsConfirmable() { return $( '.mw-htmlform .oo-ui-fieldsetLayout-header .oo-ui-labelElement-label' ); }
-	get rollbackNonJsConfirmableYes() { return $( '.mw-htmlform .mw-htmlform-submit-buttons button' ); }
 
 	open( title ) {
 		super.openTitle( title, { action: 'history' } );
-	}
-
-	toggleRollbackConfirmationSetting( enable ) {
-		Util.waitForModuleState( 'mediawiki.api', 'ready', 5000 );
-		return browser.execute( function ( en ) {
-			return new mw.Api().saveOption(
-				'showrollbackconfirmation',
-				en ? '1' : '0'
-			);
-		}, enable );
 	}
 }
 
