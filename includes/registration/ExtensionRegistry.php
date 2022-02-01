ff5863b63a8b1697371a0bd1dc4951abb567eda9
Tim Starling <tstarling@wikimedia.org>
2022-02-01 20:35:15 +1100

    ExtensionRegistry: Add process cache for lazy attributes
    getLazyLoadedAttribute() accounts for about 3% of load.php CPU usage in
    production, mostly because each ResourceLoaderFileModule calls it for
    SkinLessImportPaths, and makeCacheKey() is quite slow.
    
    So, cache lazy attributes in a process cache. There was already
    $this->lazyAttributes, which was unused since the original commit. I
    assume it was meant to be used for this, so I used  it.
    
    I also added a process cache for makeCacheKey() which is invalidated
    when the queue changes, so that requests for different lazy attributes
    in the same request will be fast.
    
    Change-Id: I83d22b58bb9bf5d06d934d5d2eef8ae7d5f72857
    (cherry picked from commit 5586bb1a57bfb50a9ddb061c8b459c96f52776b1)

--- includes/registration/ExtensionRegistry.php
+++ includes/registration/ExtensionRegistry.php
@@ -111,6 +111,13 @@ class ExtensionRegistry {
 	protected $lazyAttributes = [];
 
 	/**
+	 * The hash of cache-varying options, lazy-initialised
+	 *
+	 * @var string|null
+	 */
+	private $varyHash;
+
+	/**
 	 * Whether to check dev-requires
 	 *
 	 * @var bool
@@ -147,6 +154,7 @@ class ExtensionRegistry {
 	 */
 	public function setCheckDevRequires( $check ) {
 		$this->checkDev = $check;
+		$this->invalidateProcessCache();
 	}
 
 	/**
@@ -178,6 +186,7 @@ class ExtensionRegistry {
 			}
 		}
 		$this->queued[$path] = $mtime;
+		$this->invalidateProcessCache();
 	}
 
 	private function getCache() : BagOStuff {
@@ -187,23 +196,41 @@ class ExtensionRegistry {
 	}
 
 	private function makeCacheKey( BagOStuff $cache, $component, ...$extra ) {
-		// Everything we vary the cache on
-		$vary = [
-			'registration' => self::CACHE_VERSION,
-			'mediawiki' => MW_VERSION,
-			'abilities' => $this->getAbilities(),
-			'checkDev' => $this->checkDev,
-			'queue' => $this->queued,
-		];
-
 		// Allow reusing cached ExtensionRegistry metadata between wikis (T274648)
 		return $cache->makeGlobalKey(
 			"registration-$component",
-			// We vary the cache on the current queue (what will be or already was loaded)
-			// plus various versions of stuff for VersionChecker
-			md5( json_encode( $vary ) ),
+			$this->getVaryHash(),
 			...$extra
 		);
+	}
+
+	/**
+	 * Get the cache varying hash
+	 *
+	 * @return string
+	 */
+	private function getVaryHash() {
+		if ( $this->varyHash === null ) {
+			// We vary the cache on the current queue (what will be or already was loaded)
+			// plus various versions of stuff for VersionChecker
+			$vary = [
+				'registration' => self::CACHE_VERSION,
+				'mediawiki' => MW_VERSION,
+				'abilities' => $this->getAbilities(),
+				'checkDev' => $this->checkDev,
+				'queue' => $this->queued,
+			];
+			$this->varyHash = md5( json_encode( $vary ) );
+		}
+		return $this->varyHash;
+	}
+
+	/**
+	 * Invalidate the cache of the vary hash and the lazy options.
+	 */
+	private function invalidateProcessCache() {
+		$this->varyHash = null;
+		$this->lazyAttributes = [];
 	}
 
 	/**
@@ -280,6 +307,7 @@ class ExtensionRegistry {
 	 */
 	public function clearQueue() {
 		$this->queued = [];
+		$this->invalidateProcessCache();
 	}
 
 	/**
@@ -599,12 +627,16 @@ class ExtensionRegistry {
 		if ( isset( $this->testAttributes[$name] ) ) {
 			return $this->testAttributes[$name];
 		}
+		if ( isset( $this->lazyAttributes[$name] ) ) {
+			return $this->lazyAttributes[$name];
+		}
 
 		// See if it's in the cache
 		$cache = $this->getCache();
 		$key = $this->makeCacheKey( $cache, 'lazy-attrib', $name );
 		$data = $cache->get( $key );
 		if ( $data !== false ) {
+			$this->lazyAttributes[$name] = $data;
 			return $data;
 		}
 
@@ -618,6 +650,7 @@ class ExtensionRegistry {
 		$result = $this->readFromQueue( $paths );
 		$data = $result['attributes'][$name] ?? [];
 		$this->saveToCache( $cache, $result );
+		$this->lazyAttributes[$name] = $data;
 
 		return $data;
 	}
