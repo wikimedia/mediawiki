a3406011f6ddc03532b91ba9a561c62caa5831ac
Timo Tijhof <krinkle@fastmail.com>
2022-03-01 17:07:49 +0000

    installer: Simplify wfDetectLocalSettingsFile and document gotchass
    Follows-up I7747f83481cb05a6d.
    
    While at it:
    
    * Avoid confusing preg_match, use strpos or str_contains instead.
      If this is not the same, then I have proven my point that it is
      unclear (but also picked the wrong alternative).
    
    * Use silence operator (at) instead of AtEase. Also note that in the
      previous change this was incorrectly expanded to silence errors
      in wfDetectLocalSettingsFile, which I assume was unintentional
      and among the reasons we avoid it in new code (T253461).
    
    Change-Id: I0dda95d3c0f21c872e43697acba192d6a19fb0d5

--- includes/BootstrapHelperFunctions.php
+++ includes/BootstrapHelperFunctions.php
@@ -6,44 +6,40 @@
  */
 
 /**
- * Detect the path to the primary settings file that is
- * to be loaded by Setup.php and define MW_CONFIG_FILE.
+ * Decide and remember where to load LocalSettings from.
  *
- * The primary settings file would traditionally be
- * LocalSettings.php in the installation root, but can be
- * a different file specified via the MW_CONFIG_FILE constant
- * or the environment variable of the same name.
+ * This is used by Setup.php and will (if not already) store the result
+ * in the MW_CONFIG_FILE constant.
  *
- * If the settings file has the .yaml or .json extension, it is expected
- * to use the format described on
+ * The primary settings file is traditionally LocalSettings.php under the %MediaWiki
+ * installation path, but can also be placed differently and specified via the
+ * MW_CONFIG_FILE constant (from an entrypoint wrapper) or via a `MW_CONFIG_FILE`
+ * environment variable (from the web server).
+ *
+ * Experimental: The settings file can use the `.yaml` or `.json` extension, which
+ * must use the format described on
  * https://www.mediawiki.org/wiki/Manual:YAML_settings_file_format.
  *
- * @internal
- *
- * @param string $installationPath The installation's base path, typically global $IP.
- *
- * @return string The path to the primary settings file
- *
+ * @internal Only for use by Setup.php and Installer.
  * @since 1.38
+ * @param string $installationPath The installation's base path, typically global $IP.
+ * @return string The path to the settings file
  */
 function wfDetectLocalSettingsFile( string $installationPath ): string {
 	if ( defined( 'MW_CONFIG_FILE' ) ) {
 		return MW_CONFIG_FILE;
 	}
 
-	$configFile = getenv( 'MW_CONFIG_FILE' );
-	if ( $configFile !== false ) {
-		if ( !preg_match( '@[\\\\/]@', $configFile ) ) {
-			$configFile = "$installationPath/$configFile";
-		}
-	} else {
-		// NOTE: We could look for LocalSettings.yaml and LocalSettings.json,
-		//      and use them if they exist. But having them in a web accessible
-		//      place is dangerous, so better not to encourage that.
-		//      In order to use LocalSettings.yaml and LocalSettings.json, they
-		//      will have to be referenced explicitly by MW_CONFIG_FILE.
-
-		$configFile = "$installationPath/LocalSettings.php";
+	// We could look for LocalSettings.yaml and LocalSettings.json,
+	// and use them if they exist. But having them in a web accessible
+	// place is dangerous, so better not to encourage that.
+	// In order to use LocalSettings.yaml and LocalSettings.json, they
+	// will have to be referenced explicitly by MW_CONFIG_FILE.
+	$configFile = getenv( 'MW_CONFIG_FILE' ) ?: "LocalSettings.php";
+	// Can't use str_contains because for maintenance scripts (update.php, install.php),
+	// this is called *before* Setup.php and vendor (polyfill-php80) are included.
+	if ( strpos( $configFile, '/' ) === false ) {
+		$configFile = "$installationPath/$configFile";
 	}
 
 	define( 'MW_CONFIG_FILE', $configFile );
