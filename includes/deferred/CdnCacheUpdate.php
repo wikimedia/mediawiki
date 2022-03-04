d3fe5abda4d6506869890befc341184a0ae0b6f6
RhinosF1 <rhinosf1@gmail.com>
2022-03-04 15:52:12 +0000

    deferred: Improve port handling in CdnCacheUpdate::naivePurge
    Avoid null on php internal and cast to int
    
    Found by phan strict checks
    
    Change-Id: Ifb874b366d88cde51960004e5489a9c57ca9c4c6

--- includes/deferred/CdnCacheUpdate.php
+++ includes/deferred/CdnCacheUpdate.php
@@ -310,8 +310,9 @@ class CdnCacheUpdate implements DeferrableUpdate, MergeableUpdate {
 				$url = self::expand( $url );
 				$urlInfo = wfParseUrl( $url );
 				$urlHost = strlen( $urlInfo['port'] ?? null )
-				$urlHost = strlen( $urlInfo['port'] ?? null )
-					? IPUtils::combineHostAndPort( $urlInfo['host'], $urlInfo['port'] )
+				$urlHost = strlen( $urlInfo['port'] ?? '' )
+					? IPUtils::combineHostAndPort( $urlInfo['host'],
+					 (int)$urlInfo['port'] )
 				$baseReq = [
 					'method' => 'PURGE',
 					'url' => $url,
