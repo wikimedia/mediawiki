wikimedia/mediawiki

commit 9f14fbd002713abcf65a5b5cddc5d52dee90a977

Add Sanitizer::removeSomeTags() which uses Remex to tokenize
The existing Sanitizer::removeHTMLtags() method, in addition to having
dodgy capitalization, uses regular expressions to parse the HTML.
That produces corner cases like T298401 and T67747 and is not guaranteed
to yield balanced or well-formed HTML.

Instead, introduce and use a new Sanitizer::removeSomeTags() method
which is guaranteed to always return balanced and well-formed HTML.

Note that Sanitizer::removeHTMLtags()/::removeSomeTags() take a callback
argument which (as far as I can tell) is never used outside core. Mark
that argument as @internal, and clean up the version used by
::removeSomeTags().

Use the new ::removeSomeTags() method in the two places where
DISPLAYTITLE is handled (following up on T67747).  The use by the
legacy parser is more difficult to replace (and would have a
performace cost), so leave the old ::removeHTMLtags() method in place
for that call site for now: when the legacy parser is replaced by
Parsoid the need for the old ::removeHTMLtags() will go away.  In a
follow-up patch we'll rename ::removeHTMLtags() and mark it @internal
so that we can deprecate ::removeHTMLtags() for external use.

Some benchmarking code added.  On my machine, with PHP 7.4, the new
method tidies short 30-character title strings at a rate of about
6764/s while the tidy-based method being replaced here managed 6384/s.
Sanitizer::removeHTMLtags blazes through short strings 20x faster
(120,915/s); some of this difference is due to the set up cost of
creating the tag whitelist and the Remex pipeline, so further
optimizations could doubtless be done if Sanitizer::removeSomeTags()
is more widely used.

Bug: T299722
Bug: T67747
Change-Id: Ic864c01471c292f11799c4fbdac4d7d30b8bc50f

includes/parser/Sanitizer.php
@@ -25,8 +25,14 @@
  */
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Parser\RemexRemoveTagHandler;
+use MediaWiki\Parser\RemexStripTagHandler;
+use MediaWiki\Tidy\RemexCompatFormatter;
 use Wikimedia\RemexHtml\HTMLData;
+use Wikimedia\RemexHtml\Serializer\Serializer as RemexSerializer;
 use Wikimedia\RemexHtml\Tokenizer\Tokenizer as RemexTokenizer;
+use Wikimedia\RemexHtml\TreeBuilder\Dispatcher as RemexDispatcher;
+use Wikimedia\RemexHtml\TreeBuilder\TreeBuilder as RemexTreeBuilder;
 
 /**
  * HTML sanitizer for MediaWiki
@@ -138,20 +144,26 @@ class Sanitizer {
 
 	/**
 	 * Return the various lists of recognized tags
-	 * @param array $extratags For any extra tags to include
-	 * @param array $removetags For any tags (default or extra) to exclude
+	 * @param string[] $extratags For any extra tags to include
+	 * @param string[] $removetags For any tags (default or extra) to exclude
 	 * @return array
+	 * @internal
 	 */
 	public static function getRecognizedTagData( $extratags = [], $removetags = [] ) {
 		global $wgAllowImageTag;
+		static $commonCase, $staticInitialised;
+		$isCommonCase = ( $extratags === [] && $removetags === [] );
+		if ( $staticInitialised === $wgAllowImageTag && $isCommonCase ) {
+			return $commonCase;
+		}
 
 		static $htmlpairsStatic, $htmlsingle, $htmlsingleonly, $htmlnest, $tabletags,
-			$htmllist, $listtags, $htmlsingleallowed, $htmlelementsStatic, $staticInitialised;
+			$htmllist, $listtags, $htmlsingleallowed, $htmlelementsStatic;
 
 		// Base our staticInitialised variable off of the global config state so that if the globals
 		// are changed (like in the screwed up test system) we will re-initialise the settings.
 		$globalContext = $wgAllowImageTag;
-		if ( !$staticInitialised || $staticInitialised != $globalContext ) {
+		if ( !$staticInitialised || $staticInitialised !== $globalContext ) {
 			$htmlpairsStatic = [ # Tags that must be closed
 				'b', 'bdi', 'del', 'i', 'ins', 'u', 'font', 'big', 'small', 'sub', 'sup', 'h1',
 				'h2', 'h3', 'h4', 'h5', 'h6', 'cite', 'code', 'em', 's',
@@ -160,6 +172,9 @@ class Sanitizer {
 				'ruby', 'rb', 'rp', 'rt', 'rtc', 'p', 'span', 'abbr', 'dfn',
 				'kbd', 'samp', 'data', 'time', 'mark'
 			];
+			# These tags can be self-closed. For tags not also on
+			# $htmlsingleonly, a self-closed tag will be emitted as
+			# an empty element (open-tag/close-tag pair).
 			$htmlsingle = [
 				'br', 'wbr', 'hr', 'li', 'dt', 'dd', 'meta', 'link'
 			];
@@ -213,7 +228,7 @@ class Sanitizer {
 		$htmlpairs = array_merge( $extratags, $htmlpairsStatic );
 		$htmlelements = array_diff_key( array_merge( $extratags, $htmlelementsStatic ), $removetags );
 
-		return [
+		$result = [
 			'htmlpairs' => $htmlpairs,
 			'htmlsingle' => $htmlsingle,
 			'htmlsingleonly' => $htmlsingleonly,
@@ -224,14 +239,29 @@ class Sanitizer {
 			'htmlsingleallowed' => $htmlsingleallowed,
 			'htmlelements' => $htmlelements,
 		];
+		if ( $isCommonCase ) {
+			$commonCase = $result;
+		}
+		return $result;
 	}
 
 	/**
 	 * Cleans up HTML, removes dangerous tags and attributes, and
-	 * removes HTML comments
+	 * removes HTML comments; BEWARE there may be unmatched HTML
+	 * tags in the result.
+	 *
+	 * @note Callers are recommended to use `::removeSomeTags()` instead
+	 * of this method.  `Sanitizer::removeSomeTags()` is safer and will
+	 * always return well-formed HTML; however, it is significantly
+	 * slower (especially for short strings where setup costs
+	 * predominate).  For internal use where we know the result will
+	 * be cleaned up in a subsequent tidy pass, this method is a
+	 * fast alternative.
+	 *
 	 * @param string $text
-	 * @param callable|null $processCallback Callback to do any variable or parameter
-	 *   replacements in HTML attribute values
+	 * @param callable|null $processCallback Callback to do any variable or
+	 *   parameter replacements in HTML attribute values.
+	 *   This argument should be considered @internal.
 	 * @param array|bool $args Arguments for the processing callback
 	 * @param array $extratags For any extra tags to include
 	 * @param array $removetags For any tags (default or extra) to exclude
@@ -241,14 +271,8 @@ class Sanitizer {
 		$args = [], $extratags = [], $removetags = []
 	) {
 		$tagData = self::getRecognizedTagData( $extratags, $removetags );
-		$htmlpairs = $tagData['htmlpairs'];
 		$htmlsingle = $tagData['htmlsingle'];
 		$htmlsingleonly = $tagData['htmlsingleonly'];
-		$htmlnest = $tagData['htmlnest'];
-		$tabletags = $tagData['tabletags'];
-		$htmllist = $tagData['htmllist'];
-		$listtags = $tagData['listtags'];
-		$htmlsingleallowed = $tagData['htmlsingleallowed'];
 		$htmlelements = $tagData['htmlelements'];
 
 		# Remove HTML comments
@@ -295,6 +319,64 @@ class Sanitizer {
 			$text .= '&lt;' . str_replace( '>', '&gt;', $x );
 		}
 		return $text;
+	}
+
+	/**
+	 * Cleans up HTML, removes dangerous tags and attributes, and
+	 * removes HTML comments; the result will always be balanced and
+	 * tidy HTML.
+	 * @param string $text
+	 * @param array $options Options controlling the cleanup:
+	 *    string[] $options['extraTags'] Any extra tags to allow
+	 *    string[] $options['removeTags'] Any tags (default or extra) to exclude
+	 *    callable(Attributes,...):Attributes $options['attrCallback'] Callback
+	 *      to do any variable or parameter replacements in HTML attribute
+	 *      values before further cleanup; should be considered @internal
+	 *      and not for external use.
+	 *    array $options['attrCallbackArgs'] Additional arguments for the
+	 *      attribute callback
+	 * @return string The cleaned up HTML
+	 * @since 1.38
+	 */
+	public static function removeSomeTags(
+		string $text, array $options = []
+	): string {
+		$extraTags = $options['extraTags'] ?? [];
+		$removeTags = $options['removeTags'] ?? [];
+		// These options are @internal:
+		$attrCallback = $options['attrCallback'] ?? null;
+		$attrCallbackArgs = $options['attrCallbackArgs'] ?? [];
+		$tidy = $options['tidy'] ?? true;
+
+		// This disallows HTML5-style "missing trailing semicolon" attributes
+		// In wikitext "clean&copy" does *not* contain an entity.
+		$text = self::normalizeCharReferences( $text );
+
+		$tagData = self::getRecognizedTagData( $extraTags, $removeTags );
+		// Use RemexHtml to tokenize $text and remove the barred tags
+		$formatter = new RemexCompatFormatter;
+		$serializer = new RemexSerializer( $formatter );
+		$treeBuilder = new RemexTreeBuilder( $serializer, [
+			'ignoreErrors' => true,
+			'ignoreNulls' => true,
+		] );
+		$dispatcher = new RemexDispatcher( $treeBuilder );
+		$tokenHandler = $dispatcher;
+		$remover = new RemexRemoveTagHandler(
+			$tokenHandler, $text, $tagData,
+			$attrCallback, $attrCallbackArgs
+		);
+		$tokenizer = new RemexTokenizer( $remover, $text, [
+			'ignoreErrors' => true,
+			// don't ignore char refs, we want them to be decoded
+			'ignoreNulls' => true,
+			'skipPreprocess' => true,
+		] );
+		$tokenizer->execute( [
+			'fragmentNamespace' => HTMLData::NS_HTML,
+			'fragmentName' => 'body',
+		] );
+		return $serializer->getResult();
 	}
 
 	/**
@@ -351,6 +433,8 @@ class Sanitizer {
 	 * @param string $params
 	 * @param string $element
 	 * @return bool
+	 *
+	 * @see RemexRemoveTagHandler::validateTag()
 	 */
 	private static function validateTag( $params, $element ) {
 		$params = self::decodeTagAttributes( $params );

autoload.php
@@ -1089,6 +1089,8 @@ $wgAutoloadLocalClasses = [
 	'MediaWiki\\Parser\\ParserCacheMetadata' => __DIR__ . '/includes/parser/ParserCacheMetadata.php',
 	'MediaWiki\\Parser\\ParserObserver' => __DIR__ . '/includes/parser/ParserObserver.php',
 	'MediaWiki\\Parser\\ParserOutputFlags' => __DIR__ . '/includes/parser/ParserOutputFlags.php',
+	'MediaWiki\\Parser\\RemexRemoveTagHandler' => __DIR__ . '/includes/parser/RemexRemoveTagHandler.php',
+	'MediaWiki\\Parser\\RemexStripTagHandler' => __DIR__ . '/includes/parser/RemexStripTagHandler.php',
 	'MediaWiki\\Parser\\RevisionOutputCache' => __DIR__ . '/includes/parser/RevisionOutputCache.php',
 	'MediaWiki\\ProcOpenError' => __DIR__ . '/includes/exception/ProcOpenError.php',
 	'MediaWiki\\ShellDisabledError' => __DIR__ . '/includes/exception/ShellDisabledError.php',
@@ -1335,7 +1337,6 @@ $wgAutoloadLocalClasses = [
 	'RefreshLinks' => __DIR__ . '/maintenance/refreshLinks.php',
 	'RefreshLinksJob' => __DIR__ . '/includes/jobqueue/jobs/RefreshLinksJob.php',
 	'RefreshSecondaryDataUpdate' => __DIR__ . '/includes/deferred/RefreshSecondaryDataUpdate.php',
-	'RemexStripTagHandler' => __DIR__ . '/includes/parser/RemexStripTagHandler.php',
 	'RemoteIcuCollation' => __DIR__ . '/includes/collation/RemoteIcuCollation.php',
 	'RemoveInvalidEmails' => __DIR__ . '/maintenance/removeInvalidEmails.php',
 	'RemoveUnusedAccounts' => __DIR__ . '/maintenance/removeUnusedAccounts.php',

includes/OutputPage.php
@@ -20,7 +20,6 @@
  * @file
  */
 
-use MediaWiki\Config\ServiceOptions;
 use MediaWiki\HookContainer\ProtectedHookAccessorTrait;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\MediaWikiServices;
@@ -28,7 +27,6 @@ use MediaWiki\Page\PageRecord;
 use MediaWiki\Page\PageReference;
 use MediaWiki\Permissions\PermissionStatus;
 use MediaWiki\Session\SessionManager;
-use MediaWiki\Tidy\RemexDriver;
 use Wikimedia\AtEase\AtEase;
 use Wikimedia\Rdbms\IResultWrapper;
 use Wikimedia\RelPath;
@@ -994,15 +992,7 @@ class OutputPage extends ContextSource {
 
 		# change "<script>foo&bar</script>" to "&lt;script&gt;foo&amp;bar&lt;/script&gt;"
 		# but leave "<i>foobar</i>" alone
-		# == start explicit tidy ==
-		# T299722/T298401: The explicit tidy here can be removed once
-		# Sanitizer::removeHTMLtags() is made more robust.
-		$tidy = new RemexDriver( new ServiceOptions( [ 'TidyConfig' ], [
-			'TidyConfig' => [ 'pwrap' => false ],
-		] ) );
-		$nameWithTags = $tidy->tidy( $name, [ Sanitizer::class, 'armorFrenchSpaces' ] );
-		# == end explicit tidy ==
-		$nameWithTags = Sanitizer::normalizeCharReferences( Sanitizer::removeHTMLtags( $nameWithTags ) );
+		$nameWithTags = Sanitizer::removeSomeTags( $name );
 		$this->mPageTitle = $nameWithTags;
 
 		# change "<i>foo&amp;bar</i>" to "foo&bar"
@@ -1046,7 +1036,7 @@ class OutputPage extends ContextSource {
 			return htmlspecialchars( $this->getTitle()->getPrefixedText(), ENT_NOQUOTES );
 		}
 
-		return Sanitizer::normalizeCharReferences( Sanitizer::removeHTMLtags( $html ) );
+		return Sanitizer::removeSomeTags( $html );
 	}
 
 	/**

includes/parser/CoreParserFunctions.php
@@ -26,6 +26,8 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Parser\ParserOutputFlags;
 use MediaWiki\Revision\RevisionAccessException;
 use MediaWiki\Revision\RevisionRecord;
+use Wikimedia\RemexHtml\Tokenizer\Attributes;
+use Wikimedia\RemexHtml\Tokenizer\PlainAttributes;
 
 /**
  * Various core parser functions, registered in every Parser
@@ -457,6 +459,7 @@ class CoreParserFunctions {
 		// remove stripped text (e.g. the UNIQ-QINU stuff) that was generated by tag extensions/whatever
 		$text = $parser->killMarkers( $text );
 
+		// See T28547 for rationale for this processing.
 		// list of disallowed tags for DISPLAYTITLE
 		// these will be escaped even though they are allowed in normal wiki text
 		$bad = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'blockquote', 'ol', 'ul', 'li', 'hr',
@@ -464,8 +467,10 @@ class CoreParserFunctions {
 
 		// disallow some styles that could be used to bypass $wgRestrictDisplayTitle
 		if ( $restrictDisplayTitle ) {
-			$htmlTagsCallback = static function ( &$params ) {
-				$decoded = Sanitizer::decodeTagAttributes( $params );
+			// This code is tested with the cases marked T28547 in
+			// parserTests.txt
+			$htmlTagsCallback = static function ( Attributes $attr ): Attributes {
+				$decoded = $attr->getValues();
 
 				if ( isset( $decoded['style'] ) ) {
 					// this is called later anyway, but we need it right now for the regexes below to be safe
@@ -477,7 +482,7 @@ class CoreParserFunctions {
 					}
 				}
 
-				$params = Sanitizer::safeEncodeTagAttributes( $decoded );
+				return new PlainAttributes( $decoded );
 			};
 		} else {
 			$htmlTagsCallback = null;
@@ -486,13 +491,10 @@ class CoreParserFunctions {
 		// only requested titles that normalize to the actual title are allowed through
 		// if $wgRestrictDisplayTitle is true (it is by default)
 		// mimic the escaping process that occurs in OutputPage::setPageTitle
-		$text = Sanitizer::normalizeCharReferences( Sanitizer::removeHTMLtags(
-			$text,
-			$htmlTagsCallback,
-			[],
-			[],
-			$bad
-		) );
+		$text = Sanitizer::removeSomeTags( $text, [
+			'attrCallback' => $htmlTagsCallback,
+			'removeTags' => $bad,
+		] );
 		$title = Title::newFromText( Sanitizer::stripAllTags( $text ) );
 
 		if ( !$restrictDisplayTitle ||

includes/parser/RemexRemoveTagHandler.php
@@ -1,0 +1,172 @@
+<?php
+
+namespace MediaWiki\Parser;
+
+use Sanitizer;
+use Wikimedia\RemexHtml\Tokenizer\Attributes;
+use Wikimedia\RemexHtml\Tokenizer\PlainAttributes;
+use Wikimedia\RemexHtml\Tokenizer\RelayTokenHandler;
+use Wikimedia\RemexHtml\Tokenizer\TokenHandler;
+
+/**
+ * Helper class for Sanitizer::removeSomeTags().
+ * @internal
+ */
+class RemexRemoveTagHandler extends RelayTokenHandler {
+	/**
+	 * @var string The original HTML source string (used for fallback text
+	 * when rejecting an HTML tag).
+	 */
+	private $source;
+
+	/**
+	 * @var array<string,true> Set of HTML tags which can be self-closed.
+	 */
+	private $htmlsingle;
+
+	/**
+	 * @var array<string,true> Self-closed tags which are on $htmlsingle
+	 * but not on $htmlsingleonly will be emitted as an empty element.
+	 */
+	private $htmlsingleonly;
+
+	/**
+	 * @var array<string,true> Set of allowed HTML open/close tags.
+	 */
+	private $htmlelements;
+
+	/**
+	 * @var ?callable(Attributes,mixed...):Attributes Callback to mutate or
+	 * sanitize attributes.
+	 */
+	private $attrCallback;
+
+	/**
+	 * @var ?array $args Optional extra arguments to provide to the
+	 * $attrCallback.
+	 */
+	private $callbackArgs;
+
+	/**
+	 * @param TokenHandler $nextHandler Handler to relay accepted tokens.
+	 * @param string $source Input source string.
+	 * @param array $tagData Information about allowed/rejected tags.
+	 * @param ?callable $attrCallback Attribute handler callback.
+	 *   The full signature is ?callable(Attributes,mixed...):Attributes
+	 * @param ?array $callbackArgs Optional arguments to attribute handler.
+	 */
+	public function __construct(
+		TokenHandler $nextHandler,
+		string $source,
+		array $tagData,
+		?callable $attrCallback,
+		?array $callbackArgs
+	) {
+		parent::__construct( $nextHandler );
+		$this->source = $source;
+		$this->htmlsingle = $tagData['htmlsingle'];
+		$this->htmlsingleonly = $tagData['htmlsingleonly'];
+		$this->htmlelements = $tagData['htmlelements'];
+		$this->attrCallback = $attrCallback;
+		$this->callbackArgs = $callbackArgs ?? [];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function comment( $text, $sourceStart, $sourceLength ) {
+		// Don't relay comments.
+	}
+
+	/**
+	 * Takes attribute names and values for a tag and the tag name and
+	 * validates that the tag is allowed to be present.
+	 * This DOES NOT validate the attributes, nor does it validate the
+	 * tags themselves. This method only handles the special circumstances
+	 * where we may want to allow a tag within content but ONLY when it has
+	 * specific attributes set.
+	 *
+	 * @param string $element
+	 * @param Attributes $attrs
+	 * @return bool
+	 *
+	 * @see Sanitizer::validateTag()
+	 */
+	private static function validateTag( string $element, Attributes $attrs ): bool {
+		if ( $element == 'meta' || $element == 'link' ) {
+			$params = $attrs->getValues();
+			if ( !isset( $params['itemprop'] ) ) {
+				// <meta> and <link> must have an itemprop="" otherwise they are not valid or safe in content
+				return false;
+			}
+			if ( $element == 'meta' && !isset( $params['content'] ) ) {
+				// <meta> must have a content="" for the itemprop
+				return false;
+			}
+			if ( $element == 'link' && !isset( $params['href'] ) ) {
+				// <link> must have an associated href=""
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function startTag( $name, Attributes $attrs, $selfClose, $sourceStart, $sourceLength ) {
+		// Handle a start tag from the tokenizer: either relay it to the
+		// next stage, or re-emit it as raw text.
+
+		$badtag = false;
+		$t = strtolower( $name );
+		if ( isset( $this->htmlelements[$t] ) ) {
+			if ( $this->attrCallback ) {
+				$attrs = ( $this->attrCallback )( $attrs, ...$this->callbackArgs );
+			}
+			if ( $selfClose && !( isset( $this->htmlsingle[$t] ) || isset( $this->htmlsingleonly[$t] ) ) ) {
+				// Remove the self-closing slash, to be consistent with
+				// HTML5 semantics. T134423
+				$selfClose = false;
+			}
+			if ( !self::validateTag( $t, $attrs ) ) {
+				$badtag = true;
+			}
+			$fixedAttrs = Sanitizer::validateTagAttributes( $attrs->getValues(), $t );
+			$attrs = new PlainAttributes( $fixedAttrs );
+			if ( !$badtag ) {
+				if ( $selfClose && !isset( $this->htmlsingleonly[$t] ) ) {
+					// Interpret self-closing tags as empty tags even when
+					// HTML5 would interpret them as start tags.  Such input
+					// is commonly seen on Wikimedia wikis with this intention.
+					$this->nextHandler->startTag( $name, $attrs, false, $sourceStart, $sourceLength );
+					$this->nextHandler->endTag( $name, $sourceStart + $sourceLength, 0 );
+				} else {
+					$this->nextHandler->startTag( $name, $attrs, $selfClose, $sourceStart, $sourceLength );
+				}
+				return;
+			}
+		}
+		// Emit this as a text node instead.
+		$this->nextHandler->characters( $this->source, $sourceStart, $sourceLength, $sourceStart, $sourceLength );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function endTag( $name, $sourceStart, $sourceLength ) {
+		// Handle an end tag from the tokenizer: either relay it to the
+		// next stage, or re-emit it as raw text.
+
+		$t = strtolower( $name );
+		if ( isset( $this->htmlelements[$t] ) ) {
+			// This is a good tag, relay it.
+			$this->nextHandler->endTag( $name, $sourceStart, $sourceLength );
+		} else {
+			// Emit this as a text node instead.
+			$this->nextHandler->characters( $this->source, $sourceStart, $sourceLength, $sourceStart, $sourceLength );
+		}
+	}
+
+}

includes/parser/RemexStripTagHandler.php
@@ -1,9 +1,12 @@
 <?php
 
+namespace MediaWiki\Parser;
+
 use Wikimedia\RemexHtml\Tokenizer\Attributes;
 use Wikimedia\RemexHtml\Tokenizer\NullTokenHandler;
 
 /**
+ * Helper class for Sanitizer::stripAllTags().
  * @internal
  */
 class RemexStripTagHandler extends NullTokenHandler {

maintenance/benchmarks/benchmarkSanitizer.php
@@ -31,11 +31,13 @@ class BenchmarkSanitizer extends Benchmarker {
 		parent::__construct();
 		$this->addDescription( 'Benchmark for Sanitizer methods.' );
 		$this->addOption( 'method', 'One of "validateEmail", "encodeAttribute", '
-			. '"safeEncodeAttribute", "removeHTMLtags", or "stripAllTags". '
+			. '"safeEncodeAttribute", "removeHTMLtags", "removeSomeTags", "tidy", or "stripAllTags". '
 			. 'Default: (All)', false, true );
 	}
 
 	public function execute() {
+		# text with no html simulates an interface message string or a title
+		$textWithNoHtml = 'This could be an article title';
 		$textWithHtmlSm = 'Before <wrap><in>and</in> another <unclose> <in>word</in></wrap>.';
 		$textWithHtmlLg = str_repeat(
 				// 28K (28 chars * 1000)
@@ -71,13 +73,50 @@ class BenchmarkSanitizer extends Benchmarker {
 			};
 		}
 		if ( !$method || $method === 'removeHTMLtags' ) {
+			$tiny = strlen( $textWithNoHtml );
 			$sm = strlen( $textWithHtmlSm );
 			$lg = round( strlen( $textWithHtmlLg ) / 1000 ) . 'K';
+			$benches["Sanitizer::removeHTMLtags (input: $tiny)"] = static function () use ( $textWithNoHtml ) {
+				Sanitizer::removeHTMLtags( $textWithNoHtml );
+			};
 			$benches["Sanitizer::removeHTMLtags (input: $sm)"] = static function () use ( $textWithHtmlSm ) {
 				Sanitizer::removeHTMLtags( $textWithHtmlSm );
 			};
 			$benches["Sanitizer::removeHTMLtags (input: $lg)"] = static function () use ( $textWithHtmlLg ) {
 				Sanitizer::removeHTMLtags( $textWithHtmlLg );
+			};
+		}
+		if ( !$method || $method === 'tidy' ) {
+			# This matches what DISPLAYTITLE was previously doing to sanitize
+			# title strings
+			$tiny = strlen( $textWithNoHtml );
+			$sm = strlen( $textWithHtmlSm );
+			$lg = round( strlen( $textWithHtmlLg ) / 1000 ) . 'K';
+			$doit = static function ( $text ) {
+				return static function () use ( $text ) {
+					$tidy = new \MediaWiki\Tidy\RemexDriver( new \MediaWiki\Config\ServiceOptions( [ 'TidyConfig' ], [
+						'TidyConfig' => [ 'pwrap' => false ],
+					] ) );
+					$textWithTags = $tidy->tidy( $text, [ Sanitizer::class, 'armorFrenchSpaces' ] );
+					$textWithTags = Sanitizer::normalizeCharReferences( Sanitizer::removeHTMLtags( $textWithTags ) );
+				};
+			};
+			$benches["DISPLAYTITLE tidy (input: $tiny)"] = $doit( $textWithNoHtml );
+			$benches["DISPLAYTITLE tidy (input: $sm)"] = $doit( $textWithHtmlSm );
+			$benches["DISPLAYTITLE tidy (input: $lg)"] = $doit( $textWithHtmlLg );
+		}
+		if ( !$method || $method === 'removeSomeTags' ) {
+			$tiny = strlen( $textWithNoHtml );
+			$sm = strlen( $textWithHtmlSm );
+			$lg = round( strlen( $textWithHtmlLg ) / 1000 ) . 'K';
+			$benches["Sanitizer::removeSomeTags (input: $tiny)"] = static function () use ( $textWithNoHtml ) {
+				Sanitizer::removeSomeTags( $textWithNoHtml );
+			};
+			$benches["Sanitizer::removeSomeTags (input: $sm)"] = static function () use ( $textWithHtmlSm ) {
+				Sanitizer::removeSomeTags( $textWithHtmlSm );
+			};
+			$benches["Sanitizer::removeSomeTags (input: $lg)"] = static function () use ( $textWithHtmlLg ) {
+				Sanitizer::removeSomeTags( $textWithHtmlLg );
 			};
 		}
 		if ( !$method || $method === 'stripAllTags' ) {

tests/phpunit/includes/parser/SanitizerTest.php
@@ -26,6 +26,28 @@ class SanitizerTest extends MediaWikiIntegrationTestCase {
 		}
 	}
 
+	/**
+	 * @covers Sanitizer::removeSomeTags
+	 * @dataProvider provideHtml5Tags
+	 *
+	 * @param string $tag Name of an HTML5 element (ie: 'video')
+	 * @param bool $escaped Whether sanitizer let the tag in or escape it (ie: '&lt;video&gt;')
+	 */
+	public function testRemoveSomeTagsOnHtml5Tags( $tag, $escaped ) {
+		if ( $escaped ) {
+			$this->assertEquals( "&lt;$tag&gt;",
+				Sanitizer::removeSomeTags( "<$tag>" )
+			);
+		} else {
+			$this->assertEquals( "<$tag></$tag>\n",
+				Sanitizer::removeSomeTags( "<$tag></$tag>\n" )
+			);
+			$this->assertEquals( "<$tag></$tag>",
+				Sanitizer::removeSomeTags( "<$tag>" )
+			);
+		}
+	}
+
 	public static function provideHtml5Tags() {
 		$ESCAPED = true; # We want tag to be escaped
 		$VERBATIM = false; # We want to keep the tag
@@ -73,6 +95,14 @@ class SanitizerTest extends MediaWikiIntegrationTestCase {
 	 */
 	public function testRemoveHTMLtags( $input, $output, $msg = null ) {
 		$this->assertEquals( $output, Sanitizer::removeHTMLtags( $input ), $msg );
+	}
+
+	/**
+	 * @dataProvider dataRemoveHTMLtags
+	 * @covers Sanitizer::removeSomeTags
+	 */
+	public function testRemoveSomeTags( $input, $output, $msg = null ) {
+		$this->assertEquals( $output, Sanitizer::removeSomeTags( $input ), $msg );
 	}
 
 	/**

commit d5cddb0c2475bea47dc08238ba9bded6a6780d35

Ensure that the recognizedTagData static cache is properly initialized
This fixes a bug introduced inadvertently in
9f14fbd002713abcf65a5b5cddc5d52dee90a977 when the static cache for
Sanitizer::getRecognizedTagData() was refactored to reduce setup
overhead.

Bug: T303360
Change-Id: I606eeda1fcdbb6d4c62e2dc8db5b6e1659ae3f3f

includes/parser/Sanitizer.php
@@ -153,7 +153,7 @@ class Sanitizer {
 		global $wgAllowImageTag;
 		static $commonCase, $staticInitialised;
 		$isCommonCase = ( $extratags === [] && $removetags === [] );
-		if ( $staticInitialised === $wgAllowImageTag && $isCommonCase ) {
+		if ( $staticInitialised === $wgAllowImageTag && $isCommonCase && $commonCase ) {
 			return $commonCase;
 		}
 