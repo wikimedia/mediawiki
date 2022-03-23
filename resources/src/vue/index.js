931b29b99720b09d6c43f94a82b8c1e0d29b4397
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-23 17:28:16 +0100

    Replace Vue 3 migration build with pure Vue 3
    It is now time to stop shipping the migration build of Vue 3.
    
    Bug: T289020
    Bug: T289104
    Change-Id: I452e8275e7de248d8db2ddb3f14bf5218c546fb5

--- resources/src/vue/index.js
+++ resources/src/vue/index.js
@@ -5,28 +5,6 @@
 	const teleportTarget = require( 'mediawiki.page.ready' ).teleportTarget;
 
 	/**
-	 * Replace the given DOM element with its children.
-	 *
-	 * This changes the DOM structure resulting from a Vue 3-style mount to mimic the result of a
-	 * Vue 2-style mount.
-	 *
-	 * @param {HTMLElement} el DOM element the component was mounted to
-	 */
-	function unwrapElement( el ) {
-		if ( !el.parentNode ) {
-			return;
-		}
-		while ( el.firstChild ) {
-			el.parentNode.insertBefore( el.firstChild, el );
-		}
-		el.parentNode.removeChild( el );
-	}
-
-	// Only needed for Vue 2 compatibility; remove this when migrating away from the Vue 3 compat build
-	Vue.use( errorLogger );
-	Vue.use( i18n );
-
-	/**
 	 * @class Vue
 	 */
 	/**
@@ -36,67 +14,22 @@
 	 * To ensure all Vue code has the i18n plugin and the error handler installed, use of
 	 * Vue.createMwApp() is recommended anywhere one would normally use Vue.createApp().
 	 *
-	 * @param {Object} options
-	 * @param {...Mixed} otherArgs
+	 * @param {...Mixed} args
 	 * @return {Object} Vue app instance
 	 */
-	Vue.createMwApp = function ( options, ...otherArgs ) {
-		const app = Vue.createApp( options, ...otherArgs );
+	Vue.createMwApp = function ( ...args ) {
+		const app = Vue.createApp( ...args );
 		app.use( errorLogger );
 		app.use( i18n );
 		app.provide( 'CdxTeleportTarget', teleportTarget );
 
-		if ( options.store ) {
-			// Provide backwards compatibility for callers expecting Vuex 3
-			mw.log.warn( 'Use of Vue.createMwApp( { store } ) is deprecated. Use Vue.createMwApp(...).use( store ) instead.' );
-			mw.track( 'mw.deprecate', 'Vue.createMwApp.store' );
-
-			app.use( options.store );
-		}
 		return app;
 	};
-
-	// Proxy the Vue object to intercept its constructor and the .$mount() method, to make the
-	// mounting API backwards compatible with Vue 2 (the Vue 3 migration build chose not to do this
-	// for some reason)
-	const proxiedVue = new Proxy( Vue, {
-		construct( Target, constructorArgs ) {
-			mw.log.warn( 'Use of new Vue(...) is deprecated. Use Vue.createMwApp() instead.' );
-			mw.track( 'mw.deprecate', 'Vue.new' );
-
-			// Call the real Vue constructor
-			const vueInstance = new Target( ...constructorArgs );
-
-			// If this was a call like new Vue( { el: '#foo' } ), mimic Vue 2's behavior by
-			// unwrapping #foo
-			const [ options ] = constructorArgs;
-			if ( options && options.el ) {
-				// vueInstance.$el is the first child of the node identified by options.el
-				unwrapElement( vueInstance.$el.parentNode );
-			}
-
-			// Proxy calls to vueInstance.$mount() so that they also mimic Vue 2's behavior
-			return new Proxy( vueInstance, {
-				get( target, key ) {
-					if ( key === '$mount' ) {
-						return function ( ...mountArgs ) {
-							const mountResult = target.$mount( ...mountArgs );
-							// target.$el is the first child of the node that was mounted to
-							unwrapElement( target.$el.parentNode );
-							return mountResult;
-						};
-					} else {
-						return target[ key ];
-					}
-				}
-			} );
-		}
-	} );
 
 	// HACK: the global build of Vue that we're using assumes that Vue is globally available
 	// in eval()ed code, because it expects var Vue = ...; to run in the global scope
 	// Satisfy that assumption
-	window.Vue = proxiedVue;
+	window.Vue = Vue;
 
-	module.exports = proxiedVue;
+	module.exports = Vue;
 }() );
