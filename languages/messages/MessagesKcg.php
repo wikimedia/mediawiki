938f968cd9acedf0a50206b1a975d58feba0f17c
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-03-25 23:19:30 +0300

    Add namespaces names for Tyap (kcg)
    Change-Id: I588d24f712d6876f220385fcea119417d997e1c6

---
+++ languages/messages/MessagesKcg.php
@@ -0,0 +1,31 @@
+<?php
+/** Tyap
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Kambai Akau
+ * @author Zbobai
+ */
+
+$namespaceNames = [
+  NS_SPECIAL          => 'A‌̱áíkhapsak',
+  NS_TALK             => 'A‌̱áílyiat',
+  NS_USER             => 'A‌̱áítyunta̱m',
+  NS_USER_TALK        => 'A‌̱áílyiat_a‌̱áítyunta‌̱áím',
+  NS_PROJECT_TALK     => 'A‌̱áílyiat_$1',
+  NS_FILE             => 'Fail',
+  NS_FILE_TALK        => 'A‌̱áílyiat_fail',
+  NS_MEDIAWIKI        => 'MediaWiki',
+  NS_MEDIAWIKI_TALK   => 'A‌̱áílyiat_MediaWiki',
+  NS_TEMPLATE         => 'Ta‌̱áímpi‌̱áílet',
+  NS_TEMPLATE_TALK    => 'A‌̱áílyiat_ta‌̱áímpi‌̱áílet',
+  NS_HELP             => 'Beang',
+  NS_HELP_TALK        => 'A‌̱áílyiat_beang',
+  NS_CATEGORY         => 'Sot',
+  NS_CATEGORY_TALK    => 'A‌̱áílyiat_nsot',
+];
+
+$linkTrail = '/^([a-z‌̱áí]+)(.*)$/sDu';
