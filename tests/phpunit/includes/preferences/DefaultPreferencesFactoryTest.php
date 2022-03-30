93818f94263d96e207ef479d298fff86bf3bb251
Func <Funcer@outlook.com>
2022-03-30 18:32:27 +0800

    phpunit: Fallback to global default user options
    The real option manager would do such fallback, this behavior should
    be kept in tests.
    (Depends-On I814050cfa86ec1e86291d380843eb319fddc924a)
    
    [Cherry-picked for php8.1 support]
    Bug: T314208
    Bug: T297082
    Change-Id: I55f7d1e17bde904f2245b31df2df853cf0ca803f
    (cherry picked from commit 983b16d0928a8ace8900b57c5c38a58b985620d2)

--- tests/phpunit/includes/preferences/DefaultPreferencesFactoryTest.php
+++ tests/phpunit/includes/preferences/DefaultPreferencesFactoryTest.php
@@ -461,18 +461,20 @@ class DefaultPreferencesFactoryTest extends \MediaWikiIntegrationTestCase {
 	 * @return UserOptionsManager
 	 */
 	private function createUserOptionsManagerMock( array $userOptions, bool $defaultOptions = false ) {
+		$services = $this->getServiceContainer();
+		$defaults = $services->getMainConfig()->get( 'DefaultUserOptions' );
+		$defaults['language'] = $services->getContentLanguage()->getCode();
+		$defaults['skin'] = Skin::normalizeKey( $services->getMainConfig()->get( 'DefaultSkin' ) );
+		$userOptions += $defaults;
+
 		$mock = $this->createMock( UserOptionsManager::class );
 		$mock->method( 'getOptions' )->willReturn( $userOptions );
-		$services = $this->getServiceContainer();
 		$mock->method( 'getOption' )->willReturnCallback(
 			static function ( $user, $option ) use ( $userOptions ) {
 				return $userOptions[$option] ?? null;
 			}
 		);
 		if ( $defaultOptions ) {
-			$defaults = $services->getMainConfig()->get( 'DefaultUserOptions' );
-			$defaults['language'] = $services->getContentLanguage()->getCode();
-			$defaults['skin'] = Skin::normalizeKey( $services->getMainConfig()->get( 'DefaultSkin' ) );
 			$mock->method( 'getDefaultOptions' )->willReturn( $defaults );
 		}
 		return $mock;
