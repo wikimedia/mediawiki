5b63f99c5354ab27fb35ee3b03753377961d3bc2
Aryeh Gregor <ayg@aryeh.name>
2022-04-11 15:06:27 +0300

    Type hints for ArrayAccess
    This interfaces' methods have tentative return types in PHP 8.1,
    which causes code without the type hints to raise warnings. Where the
    type hint is "mixed", we need to use the special declaration
    [\ReturnTypeWillChange] in a comment to suppress the warning as long as
    we still support PHP < 8.0, which doesn't have a "mixed" type hint.
    
    Bug: T289879
    Change-Id: I1a126e602e92b8d13c7795eb6d790effd5ddc986

--- includes/session/Session.php
+++ includes/session/Session.php
@@ -630,7 +630,7 @@ class Session implements \Countable, \Iterator, \ArrayAccess {
 	 *  rather than array_key_exists(). So do that.
 	 * @inheritDoc
 	 */
-	public function offsetExists( $offset ) {
+	public function offsetExists( $offset ): bool {
 		$data = &$this->backend->getData();
 		return isset( $data[$offset] );
 	}
@@ -643,6 +643,7 @@ class Session implements \Countable, \Iterator, \ArrayAccess {
 	 *  be created with a null value, and does not raise a PHP warning.
 	 * @inheritDoc
 	 */
+	#[\ReturnTypeWillChange]
 	public function &offsetGet( $offset ) {
 		$data = &$this->backend->getData();
 		if ( !array_key_exists( $offset, $data ) ) {
@@ -653,12 +654,12 @@ class Session implements \Countable, \Iterator, \ArrayAccess {
 	}
 
 	/** @inheritDoc */
-	public function offsetSet( $offset, $value ) {
+	public function offsetSet( $offset, $value ): void {
 		$this->set( $offset, $value );
 	}
 
 	/** @inheritDoc */
-	public function offsetUnset( $offset ) {
+	public function offsetUnset( $offset ): void {
 		$this->remove( $offset );
 	}
 
