8ffee676119d3e00872d2f6c6b00e21ee84b4228
Timo Tijhof <krinkle@fastmail.com>
2022-04-13 23:18:05 +0100

    Revision: Log traces as 'exception.trace' instead of 'trace'
    Various in RevisionStore did this already, but we forgot a few.
    
    Bug: T233342
    Change-Id: Ib678868b239bcd4ff4ae62b2ab47022b5a70e8be

--- includes/debug/logger/monolog/LogstashFormatter.php
+++ includes/debug/logger/monolog/LogstashFormatter.php
@@ -33,7 +33,7 @@ class LogstashFormatter extends \Monolog\Formatter\LogstashFormatter {
 	protected $version;
 
 	/**
-	 * See T247675 for removing this override.
+	 * TODO: See T247675 for removing this override.
 	 *
 	 * @param string $applicationName The application that sends the data, used as the "type"
 	 * field of logstash
@@ -198,8 +198,8 @@ class LogstashFormatter extends \Monolog\Formatter\LogstashFormatter {
 	}
 
 	/**
-	 * Check whether some context field would overwrite another message key. If so, rename
-	 * and flag.
+	 * Rename any context field that would otherwise overwrite a message key.
+	 *
 	 * @param array $fields Fields to be sent to logstash
 	 * @param array $context Copy of the original $record['context']
 	 * @return array Updated version of $fields
@@ -219,7 +219,8 @@ class LogstashFormatter extends \Monolog\Formatter\LogstashFormatter {
 	}
 
 	/**
-	 * Use a more user-friendly trace format than NormalizerFormatter
+	 * Use a more user-friendly trace format than Monolog\Formatter\NormalizerFormatter.
+	 *
 	 * @param \Throwable $e
 	 * @param int $depth
 	 * @return array
