db70fc7ba865a8e9f4fac2b1a7478c8402850d10
Umherirrender <umherirrender_de.wp@web.de>
2022-04-22 19:26:19 +0200

    Remove index detection 'rev_page_timestamp'
    Rename in 1.37, part of updater. No release notes needed
    
    Change-Id: I4e8e2c3ca8aa319ce4164d296666a5b351012317

--- includes/actions/HistoryAction.php
+++ includes/actions/HistoryAction.php
@@ -353,10 +353,6 @@ class HistoryAction extends FormlessAction {
 		$page_id = $this->getWikiPage()->getId();
 
 		$revQuery = MediaWikiServices::getInstance()->getRevisionStore()->getQueryInfo();
-		// T270033 Index renaming
-		$revIndex = $dbr->indexExists( 'revision', 'page_timestamp',  __METHOD__ )
-			? 'page_timestamp'
-			: 'rev_page_timestamp';
 		return $dbr->select(
 			$revQuery['tables'],
 			$revQuery['fields'],
@@ -364,7 +360,7 @@ class HistoryAction extends FormlessAction {
 			__METHOD__,
 			[
 				'ORDER BY' => "rev_timestamp $dirs",
-				'USE INDEX' => [ 'revision' => $revIndex ],
+				'USE INDEX' => [ 'revision' => 'rev_page_timestamp' ],
 				'LIMIT' => $limit
 			],
 			$revQuery['joins']
--- includes/page/RollbackPage.php
+++ includes/page/RollbackPage.php
@@ -280,10 +280,6 @@ class RollbackPage {
 		}
 
 		$dbw = $this->loadBalancer->getConnectionRef( DB_PRIMARY );
-		// T270033 Index renaming
-		$revIndex = $dbw->indexExists( 'revision', 'page_timestamp',  __METHOD__ )
-			? 'page_timestamp'
-			: 'rev_page_timestamp';
 
 		// TODO: move this query to RevisionSelectQueryBuilder when it's available
 		// Get the last edit not by this person...
@@ -298,7 +294,7 @@ class RollbackPage {
 			],
 			__METHOD__,
 			[
-				'USE INDEX' => [ 'revision' => $revIndex ],
+				'USE INDEX' => [ 'revision' => 'rev_page_timestamp' ],
 				'ORDER BY' => [ 'rev_timestamp DESC', 'rev_id DESC' ]
 			],
 			$actorWhere['joins']
