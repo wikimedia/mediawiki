ed024835451ae448587f292528d87340cbcc7f2e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-28 14:10:00 -0600

    Fix weird conflict in MaintenanceTest whereas testOptionGetters was duplicated

--- tests/phpunit/maintenance/MaintenanceTest.php
+++ tests/phpunit/maintenance/MaintenanceTest.php
@@ -549,27 +549,4 @@ class MaintenanceTest extends MaintenanceBaseTestCase {
 			'Non existent option falls back to a new default'
 		);
 	}
-
-	public function testOptionGetters() {
-		$this->assertSame(
-			false,
-			$this->maintenance->hasOption( 'somearg' ),
-			'Non existent option not found'
-		);
-		$this->assertSame(
-			'default',
-			$this->maintenance->getOption( 'somearg', 'default' ),
-			'Non existent option falls back to default'
-		);
-		$this->assertSame(
-			false,
-			$this->maintenance->hasOption( 'somearg' ),
-			'Non existent option not found after getting'
-		);
-		$this->assertSame(
-			'newdefault',
-			$this->maintenance->getOption( 'somearg', 'newdefault' ),
-			'Non existent option falls back to a new default'
-		);
-	}
 }
