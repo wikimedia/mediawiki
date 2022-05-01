fb05bccd4d7d2c2cb7d06cbeacc8af47ef9c8bbd
cicalese <cindom@gmail.com>
2022-05-01 08:18:15 -0400

    Fix old_name in UserLogoutComplete hook.
    I056a97d466bde8342f7091a110f70bba3f105844 broke the behavior of the
    UserLogoutComplete hook, since it gets the old user name after the
    call to $user->logout(), causing the old user name to always be that of
    the anonymous user. The old user name must be saved before the call to
    $user->logout().
    
    Change-Id: I971d13e2984d6dc07f6ef82f3b27cf8d6186296c
    (cherry picked from commit 45d57449f802414db4c600709157087752e6a6f3)

---
+++ tests/phpunit/includes/specials/SpecialUserLogoutTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @covers SpecialUserLogout
+ */
+class SpecialUserLogoutTest extends SpecialPageTestBase {
+
+	/**
+	 * Returns a new instance of the special page under test.
+	 *
+	 * @return SpecialPage
+	 */
+	protected function newSpecialPage() {
+		return new SpecialUserLogout();
+	}
+
+	public function testUserLogoutComplete() {
+		$oldName = __METHOD__;
+		$user = new TestUser( $oldName );
+
+		$session = RequestContext::getMain()->getRequest()->getSession();
+		$fauxRequest = new FauxRequest(
+			[ 'wpEditToken' => $session->getToken( 'logoutToken' ) ],
+			/* $wasPosted= */ true,
+			$session
+		);
+
+		$oldNameInHook = null;
+		$this->setTemporaryHook(
+			'UserLogoutComplete',
+			static function ( $user, $injected_html, $oldName ) use ( &$oldNameInHook ) {
+				$oldNameInHook = $oldName;
+			}
+		);
+		$this->executeSpecialPage( '', $fauxRequest, 'qqx', $user->getUser() );
+		$this->assertEquals(
+			$oldName,
+			$oldNameInHook,
+			'old name in UserLogoutComplete hook was incorrect'
+		);
+	}
+}
