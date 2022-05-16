8ad4530c4a3a09d4a767d70457a3341120e05931
EpicPupper <epicpupper@outlook.com>
2022-05-16 08:53:58 +0000

    Clarify wording for high-volume grants
    Bug: T70312
    Change-Id: Id289cd298393e213426d2e4fe38b3a32f53b5dfd

--- tests/phpunit/integration/includes/Permissions/GrantsLocalizationTest.php
+++ tests/phpunit/integration/includes/Permissions/GrantsLocalizationTest.php
@@ -96,7 +96,7 @@ class GrantsLocalizationTest extends MediaWikiIntegrationTestCase {
 	 */
 	public function testGetGrantsWikiText() {
 		$this->assertSame(
-			"*<span class=\"mw-grantgroup\">Perform high volume activity</span>\n:High-volume editing\n\n",
+			"*<span class=\"mw-grantgroup\">Perform high volume activity</span>\n:High-volume (bot) access\n\n",
 			$this->grantsLocalization->getGrantsWikiText( [ 'highvolume' ] )
 		);
 	}
