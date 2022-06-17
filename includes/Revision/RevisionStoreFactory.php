05a175b439a2d4f439ad757f7ecb2fedbe483956
Umherirrender <umherirrender_de.wp@web.de>
2022-06-17 23:46:29 +0200

    Simplify assertion for false around wiki id and domain id
    false is accepted by Assert::class since 9090085
    
    Change-Id: I243a63dca310a198c6f7a8bc52298c5c95939eb6

--- includes/Revision/RevisionStoreFactory.php
+++ includes/Revision/RevisionStoreFactory.php
@@ -141,12 +141,12 @@ class RevisionStoreFactory {
 	/**
 	 * @since 1.32
 	 *
-	 * @param bool|string $dbDomain DB domain of the relevant wiki or false for the current one
+	 * @param false|string $dbDomain DB domain of the relevant wiki or false for the current one
 	 *
 	 * @return RevisionStore for the given wikiId with all necessary services
 	 */
 	public function getRevisionStore( $dbDomain = false ) {
-		Assert::parameterType( 'string|boolean', $dbDomain, '$dbDomain' );
+		Assert::parameterType( [ 'string', 'false' ], $dbDomain, '$dbDomain' );
 
 		$store = new RevisionStore(
 			$this->dbLoadBalancerFactory->getMainLB( $dbDomain ),
