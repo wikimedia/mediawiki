9d978b833f060ed6f9231b73834c034a73d44b38
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-06-20 10:58:05 +0300

    Add linkTrail for Tumbuka (tum) and Chewa/Nyanja (ny)
    The languages are related and both use the letter Ŵ,
    but different tone diacritics.
    
    Tumbuka has been in Names.php for a long time, but
    localization was finally added now, thanks to
    User:Tumbuka_Arch on translatewiki.
    
    Bug: T310976
    Change-Id: Ide2bc74046f5e793170df4f72ac25cf004c96348

---
+++ languages/messages/MessagesNy.php
@@ -0,0 +1,12 @@
+<?php
+/** Nyanja/Chewa (Chinyanja/Chichewa)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Amir A. Aharoni
+ */
+
+$linkTrail = '/^([A-Za-źŴŵ]+)(.*)$/sDu';
