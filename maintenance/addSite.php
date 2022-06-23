b3db022cf1324f24338261e56dff30f761b3a0ab
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-06-23 23:14:57 +0200

    Call parent constructor of AddSite maintenance script first
    Call parent constructor of AddSite maintenance script first as other
    maintenance scripts do as well.
    
    Bug: T311272
    Change-Id: I6c0a22dfcced8419c3c7fe9b07364c83df2229cd
    (cherry picked from commit e8f7e292603ea4d7d96490e898abd31cc81e48c7)

--- maintenance/addSite.php
+++ maintenance/addSite.php
@@ -17,6 +17,8 @@ require_once $basePath . '/maintenance/Maintenance.php';
 class AddSite extends Maintenance {
 
 	public function __construct() {
+		parent::__construct();
+
 		$this->addDescription( 'Add a site definition into the sites table.' );
 
 		$this->addArg( 'globalid', 'The global id of the site to add, e.g. "wikipedia".', true );
@@ -28,8 +30,6 @@ class AddSite extends Maintenance {
 			' https://example.com/wiki/\$1.', false, true );
 		$this->addOption( 'filepath', 'The URL to files of this site, e.g. https://example' .
 			'.com/w/\$1.', false, true );
-
-		parent::__construct();
 	}
 
 	/**
