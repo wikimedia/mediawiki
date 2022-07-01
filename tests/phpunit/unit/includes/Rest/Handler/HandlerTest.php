aad4f8719a3bd8d794e65926ea3a9859dd5d7e40
daniel <dkinzler@wikimedia.org>
2022-07-01 15:21:34 +0200

    Clarify handling of verifier headers in POST and PUT
    Besides being used for caching of the responses of GET requests,
    the ETag and Last-Modified headers can be used with PUT and POST
    requests to control the conditional execution of requests using
    the If-Match and If-Unmodified-Since headers.
    
    For that reason it is important to distinguish between the ETag and
    modification date of a resource at the time a request is received
    (before the resource is modified), and at the time the response is
    being sent (after the resource has been modified). The before-state
    is used to check conditionals, while the after-state can be sent back
    to the client in the response.
    
    For a GET request, it is reasonable to re-use the ETag and Last-Modified
    values that were used to check conditionals in the response.
    For PUT and POST, that would generally be wrong, since the resource has
    since changed.
    
    Bug: T311819
    Change-Id: I1a09b0ef35ae2365e0b261fb5295003aec1f5b17

--- tests/phpunit/unit/includes/Rest/Handler/HandlerTest.php
+++ tests/phpunit/unit/includes/Rest/Handler/HandlerTest.php
@@ -7,6 +7,7 @@ use MediaWiki\Rest\Handler;
 use MediaWiki\Rest\HttpException;
 use MediaWiki\Rest\LocalizedHttpException;
 use MediaWiki\Rest\RequestData;
+use MediaWiki\Rest\Response;
 use MediaWiki\Rest\ResponseFactory;
 use MediaWiki\Rest\ResponseInterface;
 use MediaWiki\Rest\Router;
@@ -17,6 +18,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 use Wikimedia\Message\MessageValue;
 use Wikimedia\ParamValidator\ParamValidator;
 use Wikimedia\TestingAccessWrapper;
+use Wikimedia\Timestamp\ConvertibleTimestamp;
 
 /**
  * @covers \MediaWiki\Rest\Handler\SearchHandler
@@ -354,6 +356,38 @@ class HandlerTest extends \MediaWikiUnitTestCase {
 		$this->assertEquals( $expectedMessage, $ex->getMessageValue() );
 
 		$this->assertFalse( $handler->getSession()->getProvider()->safeAgainstCsrf() );
+	}
+
+	public function testThatVerifierHeadersAreLoopedThroughForGet() {
+		$handler = $this->newHandler( [ 'getETag', 'getLastModified' ] );
+		$handler->method( 'getETag' )->willReturn( '"TEST"' );
+		$handler->method( 'getLastModified' )->willReturn( '20220101223344' );
+
+		$params = [ 'method' => 'GET' ];
+		$this->initHandler( $handler, new RequestData( $params ) );
+		$handler->checkPreconditions();
+
+		$response = new Response();
+		$handler->applyConditionalResponseHeaders( $response );
+		$this->assertSame( '"TEST"', $response->getHeaderLine( 'ETag' ) );
+
+		$lastModified = ConvertibleTimestamp::convert( TS_MW, $response->getHeaderLine( 'Last-Modified' ) );
+		$this->assertSame( '20220101223344', $lastModified );
+	}
+
+	public function testThatVerifierHeadersAreNotLoopedThroughForPost() {
+		$handler = $this->newHandler( [ 'getETag', 'getLastModified' ] );
+		$handler->method( 'getETag' )->willReturn( '"TEST"' );
+		$handler->method( 'getLastModified' )->willReturn( '20220101223344' );
+
+		$params = [ 'method' => 'POST' ];
+		$this->initHandler( $handler, new RequestData( $params ) );
+		$handler->checkPreconditions();
+
+		$response = new Response();
+		$handler->applyConditionalResponseHeaders( $response );
+		$this->assertEmpty( $response->getHeaderLine( 'ETag' ) );
+		$this->assertEmpty( $response->getHeaderLine( 'Last-Modified' ) );
 	}
 
 }
