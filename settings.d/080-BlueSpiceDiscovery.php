a555ef718698846e708bc0fd4de2181350bfea1c
rvogel <vogel@hallowelt.biz>
2022-07-01 15:56:03 +0200

    Sync with REL1_35

--- settings.d/080-BlueSpiceDiscovery.php
+++ settings.d/080-BlueSpiceDiscovery.php
@@ -2,9 +2,3 @@
 
 wfLoadSkin( 'BlueSpiceDiscovery' );
 $GLOBALS['wgDefaultSkin'] = "bluespicediscovery";
-
-if ( $GLOBALS['wgLogos'] === false  ){
-	$GLOBALS['wgLogos'] = [
-		'1x' => $GLOBALS['wgResourceBasePath'] . '/skins/BlueSpiceDiscovery/resources/images/bs_logo.png'
-	];
-}
