a3095fbb94ef3aa1f2e949ea8b7a4a268b7d6d2b
Reedy <reedy@wikimedia.org>
2022-07-02 15:32:37 +0000

    Add return type to jsonSerialize()
    Bug: T311919
    Change-Id: I469deae973ab58ef41aac6a56cea0653a988c05c

--- includes/Storage/EditResult.php
+++ includes/Storage/EditResult.php
@@ -273,8 +273,7 @@ class EditResult implements JsonSerializable {
 	 *
 	 * @since 1.36
 	 */
-	#[\ReturnTypeWillChange]
-	public function jsonSerialize() {
+	public function jsonSerialize(): array {
 		return [
 			'isNew' => $this->isNew,
 			'originalRevisionId' => $this->originalRevisionId,
