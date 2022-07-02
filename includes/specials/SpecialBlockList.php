be50035753a0b503e51a765a05d5ba7dd8ce4bab
Reedy <reedy@wikimedia.org>
2022-07-02 16:55:26 +0100

    SpecialBlockList: Prevent passing null to trim()
    Bug: T311920
    Change-Id: I0c101cdaa3a7b83d0fdc31bf03376df4c88a99d7
    (cherry picked from commit 2870b279bde889b80e6944507e113c660a0083dd)

--- includes/specials/SpecialBlockList.php
+++ includes/specials/SpecialBlockList.php
@@ -90,7 +90,7 @@ class SpecialBlockList extends SpecialPage {
 		$out->addModuleStyles( [ 'mediawiki.special' ] );
 
 		$request = $this->getRequest();
-		$par = $request->getVal( 'ip', $par );
+		$par = $request->getVal( 'ip', $par ?? '' );
 		$this->target = trim( $request->getVal( 'wpTarget', $par ) );
 
 		$this->options = $request->getArray( 'wpOptions', [] );
