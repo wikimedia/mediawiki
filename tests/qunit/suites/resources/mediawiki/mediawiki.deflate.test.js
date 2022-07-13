b24c10445af6494613b60389874e35f1168c5d1a
Timo Tijhof <krinkle@fastmail.com>
2022-07-12 18:16:49 -0700

    mediawiki.deflate: Use QUnit.test.each() data provider
    Follows-up b0fd96b6bff6 (I5591949a033).
    
    This removes the need for composed/repeated assert messages, and
    makes the test results a bit easier to scan as well and allows for
    more localised re-running.
    
    Change-Id: Ia370dcf783bb1377a510adaa14644192ffa00334

--- tests/qunit/suites/resources/mediawiki/mediawiki.deflate.test.js
+++ tests/qunit/suites/resources/mediawiki/mediawiki.deflate.test.js
@@ -1,54 +1,45 @@
-( function () {
-	QUnit.module( 'mediawiki.deflate', QUnit.newMwEnvironment() );
-
+QUnit.module( 'mediawiki.deflate', function () {
 	var seed = 1234567890;
 	function getPseudoRandom() {
 		seed = seed * 16807 % 2147483646;
 		return seed;
 	}
 
-	QUnit.test( 'deflate', function ( assert ) {
-		var longData = new TextDecoder( 'utf-8' ).decode(
-			// eslint-disable-next-line no-undef
-			Uint32Array.from(
-				{ length: 5 * 1024 * 1024 },
-				getPseudoRandom
-			)
-		);
+	var longData = new TextDecoder( 'utf-8' ).decode(
+		// eslint-disable-next-line no-undef
+		Uint32Array.from(
+			{ length: 5 * 1024 * 1024 },
+			getPseudoRandom
+		)
+	);
 
-		var cases = [
-			{
-				data: 'foobar',
-				expected: 'rawdeflate,S8vPT0osAgA='
-			},
-			{
-				data: 'ℳ𝒲♥𝓊𝓃𝒾𝒸ℴ𝒹ℯ',
-				expected: 'rawdeflate,e9Sy+cPcSZsezVz6Ye7kLiBuBnL3AfGORy1bgNTORy3rAQ==',
-				msg: 'Unicode'
-			},
-			{
-				data: '😂𐅀𐅁𐅂𐅃𐅄𐅅𐅆𐅇𐅈𐅉𐅊𐅋𐅌𐅍𐅎𐅏',
-				expected: 'rawdeflate,Fca3EQAgDACx1Ukmp5KOFT0CT6E76T1OtxhY/HsECCISMgoqGjoGJtYD',
-				msg: 'Non BMP unicode'
-			},
-			{
-				data: longData,
-				expectedLength: 330607,
-				expectedHead: '7Nbbsi10mR',
-				expectedTail: 'Inf9LJDw==',
-				msg: '5MB data'
-			}
-		];
-		cases.forEach( function ( caseItem ) {
-			if ( caseItem.expected ) {
-				assert.strictEqual( mw.deflate( caseItem.data ), caseItem.expected, caseItem.msg || caseItem.data );
-			} else {
-				var deflated = mw.deflate( caseItem.data );
-				assert.strictEqual( deflated.length, caseItem.expectedLength, caseItem.msg + ': length' );
-				assert.strictEqual( deflated.slice( 11, 21 ), caseItem.expectedHead, caseItem.msg + ': head' );
-				assert.strictEqual( deflated.slice( -10 ), caseItem.expectedTail, caseItem.msg + ': tail' );
-			}
-		} );
+	QUnit.test.each( 'deflate', {
+		foobar: {
+			data: 'foobar',
+			expected: 'rawdeflate,S8vPT0osAgA='
+		},
+		Unicode: {
+			data: 'ℳ𝒲♥𝓊𝓃𝒾𝒸ℴ𝒹ℯ',
+			expected: 'rawdeflate,e9Sy+cPcSZsezVz6Ye7kLiBuBnL3AfGORy1bgNTORy3rAQ=='
+		},
+		'Non BMP unicode': {
+			data: '😂𐅀𐅁𐅂𐅃𐅄𐅅𐅆𐅇𐅈𐅉𐅊𐅋𐅌𐅍𐅎𐅏',
+			expected: 'rawdeflate,Fca3EQAgDACx1Ukmp5KOFT0CT6E76T1OtxhY/HsECCISMgoqGjoGJtYD'
+		},
+		'5MB data': {
+			data: longData,
+			expectedLength: 330607,
+			expectedHead: '7Nbbsi10mR',
+			expectedTail: 'Inf9LJDw=='
+		}
+	}, function ( assert, data ) {
+		if ( data.expected ) {
+			assert.strictEqual( mw.deflate( data.data ), data.expected );
+		} else {
+			var deflated = mw.deflate( data.data );
+			assert.strictEqual( deflated.length, data.expectedLength, 'length' );
+			assert.strictEqual( deflated.slice( 11, 21 ), data.expectedHead, 'head' );
+			assert.strictEqual( deflated.slice( -10 ), data.expectedTail, 'tail' );
+		}
 	} );
-
-}() );
+} );
