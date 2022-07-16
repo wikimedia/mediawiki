fdf1ee26fae161f48350118947607531476ce40a
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-07-16 13:25:26 +0300

    Add linktrail for Sotho (st)
    Change-Id: Iccb718196ee63dd2b3d9eb4f81b4650ead8b0b5c

---
+++ languages/messages/MessagesSt.php
@@ -0,0 +1,12 @@
+<?php
+/** Sotho (Sesotho)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Amir E. Aharoni
+ */
+
+$linkTrail = '/^([A-Za-zŠÒŌÈĒšòōèē]+)(.*)$/sDu';
