9ca241036ddefabf65935ac40ac712bb9b350cc7
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2022-07-24 07:28:27 +0300

    Add namespaces and linkTrail for Tswana (tn)
    Contributed by User:Rebelagent at
    https://translatewiki.net/wiki/Thread:User_talk:Rebelagent/Setswana_namespaces
    
    Change-Id: I2addd5c4acc07ac93ce7ef660ba5bbea0ec6a0a1

---
+++ languages/messages/MessagesTn.php
@@ -0,0 +1,32 @@
+<?php
+/** Tswana (Setswana)
+ *
+ * To improve a translation please visit https://translatewiki.net
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Rebelagent
+ * @author Amir E. Aharoni
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => "Pego",
+	NS_SPECIAL          => "Faphegileng",
+	NS_TALK             => "Puisano",
+	NS_USER             => "Modirisi",
+	NS_USER_TALK        => "Puisano_ya_modirisi",
+	NS_PROJECT_TALK     => "Puisano_ya_$1",
+	NS_FILE             => "Setshwantsho",
+	NS_FILE_TALK        => "Puisano_ya_setshwantsho",
+	NS_MEDIAWIKI        => "MediaWiki",
+	NS_MEDIAWIKI_TALK   => "Puisano_ya_MediaWiki",
+	NS_TEMPLATE         => "Tempolete",
+	NS_TEMPLATE_TALK    => "Puisano_ya_tempolete",
+	NS_HELP             => "Thuso",
+	NS_HELP_TALK        => "Puisano_ya_thuso",
+	NS_CATEGORY         => "Karolo",
+	NS_CATEGORY_TALK    => "Puisano_ya_karolo",
+];
+
+$linkTrail = "/^([a-zêšô]+)(.*)$/sDu";
