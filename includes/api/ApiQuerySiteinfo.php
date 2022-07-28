3c5287060f1c128460bb33fc05dc0d1428412c34
Brian Wolff <bawolff+wn@gmail.com>
2022-07-28 08:50:28 -0700

    Avoid testing strlen on null in ApiQuerySiteinfo [php 8.1 compat]
    Cast to string. We need to check for both empty string and null.
    PHP 8.1 does not like passing null to strlen.
    
    Bug: T313663
    Change-Id: Icedb96fcf39bba70193fe30de5a8385b62040175

--- includes/api/ApiQuerySiteinfo.php
+++ includes/api/ApiQuerySiteinfo.php
@@ -769,7 +769,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 		$config = $this->getConfig();
 		$rightsPage = $config->get( 'RightsPage' );
 		// The default value is null, but the installer sets it to empty string
-		if ( strlen( $rightsPage ) ) {
+		if ( strlen( (string)$rightsPage ) ) {
 			$title = Title::newFromText( $rightsPage );
 			$url = wfExpandUrl( $title->getLinkURL(), PROTO_CURRENT );
 		} else {
@@ -777,7 +777,7 @@ class ApiQuerySiteinfo extends ApiQueryBase {
 			$url = $config->get( 'RightsUrl' );
 		}
 		$text = $config->get( 'RightsText' );
-		if ( $title && !strlen( $text ) ) {
+		if ( $title && !strlen( (string)$text ) ) {
 			$text = $title->getPrefixedText();
 		}
 
