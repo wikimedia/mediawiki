50e5f9de0b84b5561ddb3752406f104eb8e4685c
Aaron Schulz <aschulz@wikimedia.org>
2022-08-25 16:05:47 -0700

    rdbms: improve documentation about sections/clusters in LBFactory classes
    Bug: T316299
    Change-Id: Ia87b8ec274db462e527a9a05cdf189922ec3ad94

--- includes/libs/rdbms/lbfactory/ILBFactory.php
+++ includes/libs/rdbms/lbfactory/ILBFactory.php
@@ -49,8 +49,8 @@ interface ILBFactory {
 	 *  - wanCache: WANObjectCache instance [optional]
 	 *  - cliMode: Whether the execution context is a CLI script. [optional]
 	 *  - maxLag: Try to avoid DB replicas with lag above this many seconds [optional]
-	 *  - profiler: Callback that takes a section name argument and returns
-	 *      a ScopedCallback instance that ends the profile section in its destructor [optional]
+	 *  - profiler: Callback that takes a profile section name and returns a ScopedCallback
+	 *     that ends the profile section in its destructor [optional]
 	 *  - trxProfiler: TransactionProfiler instance. [optional]
 	 *  - replLogger: PSR-3 logger instance. [optional]
 	 *  - connLogger: PSR-3 logger instance. [optional]
