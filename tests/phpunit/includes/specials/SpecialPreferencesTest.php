c71d7d9b8ea20968df30b955b990fd4960a363e6
Aaron Schulz <aschulz@wikimedia.org>
2022-08-26 13:03:38 -0700

    Avoid PHP 8.1 failures in SpecialPreferencesTest::testT43337
    Inject more default preferences to avoid null values getting passed into
    methods like strtolower() via RequestContext::sanitizeLangCode() and
    Skin::normalizeKey().
    
    Bug: T248925
    Change-Id: I008a25f7b039b8e7c417f63515f76b3a3b0cd609

--- tests/phpunit/includes/specials/SpecialPreferencesTest.php
+++ tests/phpunit/includes/specials/SpecialPreferencesTest.php
@@ -7,6 +7,7 @@
  */
 
 use MediaWiki\MainConfigNames;
+use MediaWiki\MainConfigSchema;
 use MediaWiki\User\UserOptionsLookup;
 
 /**
@@ -39,9 +40,48 @@ class SpecialPreferencesTest extends MediaWikiIntegrationTestCase {
 		$userOptionsLookup = $this->createMock( UserOptionsLookup::class );
 		$userOptionsLookup->method( 'getOption' )
 			->willReturnMap( [
-				[ $user, 'nickname', null, false, UserOptionsLookup::READ_NORMAL, 'superlongnickname' ],
-			]
-			);
+				[
+					$user,
+					'nickname',
+					null,
+					false,
+					UserOptionsLookup::READ_NORMAL,
+					'superlongnickname'
+				],
+				[
+					$user,
+					'language',
+					null,
+					false,
+					UserOptionsLookup::READ_NORMAL,
+					MainConfigSchema::LanguageCode['default']
+				],
+				[
+					$user,
+					'skin',
+					null,
+					false,
+					UserOptionsLookup::READ_NORMAL,
+					MainConfigSchema::DefaultSkin['default']
+				],
+				[
+					$user,
+					'timecorrection',
+					null,
+					false,
+					UserOptionsLookup::READ_NORMAL,
+					"System|-420"
+				],
+				// MessageCache::getParserOptions() uses the main context
+				[
+					RequestContext::getMain()->getUser(),
+					'language',
+					null,
+					false,
+					UserOptionsLookup::READ_NORMAL,
+					MainConfigSchema::LanguageCode['default']
+				],
+			] );
 		$this->setService( 'UserOptionsLookup', $userOptionsLookup );
 
 		// isAnyAllowed used to return null from the mock,
