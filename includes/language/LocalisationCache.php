wikimedia/mediawiki

commit 5fa2b6362bda3842f03ca42ade33eaa1ca27924f

SpecialPages: Can formally register navigation links
* Introduces Special::getShortDescription for identifying
page titles
* Introduces Skin::supportsMenu for checking if a menu is used
by the skin
* Introduces SpecialPAge::getRelatedNavigationLinks for special
pages to register menus

You can see this in use in the following patchsets:
 * AbuseFilter (Ief51a9c60125c11e3b735fabee2a4544b7955f64)
 * Special:Contributions (I8d94a1686699a327f697c51e26bd48cfb86303f4)

Bug: T313349
Change-Id: I812eb1ec14fd347d416a6945c52cc2611ad47f3a

includes/language/LocalisationCache.php
@@ -1001,7 +1001,24 @@ class LocalisationCache {
 		# Replace spaces with underscores in namespace names
 		$allData['namespaceNames'] = str_replace( ' ', '_', $allData['namespaceNames'] );
 
+		# Search messages for aliases and add them when found. (T89947)
+		foreach ( $allData['messages'] as $key => $msg ) {
+			if ( strpos( $key, 'special-page-alias' ) === 0 ) {
+				[ $prefix, $name ] = explode( 'special-page-alias-', $key );
+				$aliases = explode( "\n", trim( $msg ) );
+				if ( array_key_exists( $name, $allData['specialPageAliases'] ) ) {
+					$allData['specialPageAliases'][$name] = array_merge(
+						$allData['specialPageAliases'][$name],
+						$aliases
+					);
+				} else {
+					$allData['specialPageAliases'][$name] = $aliases;
+				}
+			}
+		}
+
 		# And do the same for special page aliases. $page is an array.
+		// @phan-suppress-next-line PhanTypePossiblyInvalidDimOffset phan #3569
 		foreach ( $allData['specialPageAliases'] as &$page ) {
 			$page = str_replace( ' ', '_', $page );
 		}

includes/skins/Skin.php
@@ -2583,7 +2583,33 @@ abstract class Skin extends ContextSource {
 			// the <html>, <head> and <body> tags. For SkinMustache this is always true and
 			// ignored.
 			'bodyOnly' => false,
+			'menus' => [
+				// Modern keys
+				'user-interface-preferences',
+				'user-page',
+				'user-menu',
+				'notifications',
+				// Legacy keys that are enabled by default for backwards compatibility
+				'namespaces',
+				'views',
+				'actions',
+				'variants',
+				// Opt-in menus
+				// * 'associatedPages'
+			]
 		];
+	}
+
+	/**
+	 * Does the skin support the named menu?
+	 *
+	 * @since 1.39
+	 * @param string $menu See Skin::getOptions for menu names.
+	 * @return bool
+	 */
+	public function supportsMenu( string $menu ): bool {
+		$options = $this->getOptions();
+		return in_array( $menu, $options['menus'] );
 	}
 
 	/**

includes/skins/SkinTemplate.php
@@ -1253,12 +1253,14 @@ class SkinTemplate extends Skin {
 		$categoriesData = $this->getCategoryPortletsData( $this->getOutput()->getCategoryLinks() );
 		$userPageLink = [];
 		$this->addPersonalPageItem( $userPageLink, '-2' );
+
 		$content_navigation = $categoriesData + [
 			// Modern keys: Please ensure these get unset inside Skin::prepareQuickTemplate
 			'user-interface-preferences' => [],
 			'user-page' => $userPageLink,
 			'user-menu' => $this->buildPersonalUrls( false ),
 			'notifications' => [],
+			'associatedPages' => [],
 			// Legacy keys
 			'namespaces' => [],
 			'views' => [],
@@ -1266,11 +1268,13 @@ class SkinTemplate extends Skin {
 			'variants' => []
 		];
 
+		$associatedPages = [];
+		$namespaces = [];
 		$userCanRead = $this->getAuthority()->probablyCan( 'read', $title );
 
 		// Checks if page is some kind of content
 		if ( $title->canExist() ) {
-			// Gets page objects for the related namespaces
+			// Gets page objects for the associatedPages namespaces
 			$subjectPage = $title->getSubjectPage();
 			$talkPage = $title->getTalkPage();
 
@@ -1309,19 +1313,19 @@ class SkinTemplate extends Skin {
 				array_unshift( $subjectMsg, 'nstab-mainpage' );
 			}
 
-			$content_navigation['namespaces'][$subjectId] = $this->tabAction(
+			$associatedPages[$subjectId] = $this->tabAction(
 				$subjectPage, $subjectMsg, !$isTalk, '', $userCanRead
 			);
-			$content_navigation['namespaces'][$subjectId]['context'] = 'subject';
+			$associatedPages[$subjectId]['context'] = 'subject';
 			// Use the following messages if defined or talk if not:
 			// * nstab-talk, nstab-user_talk, nstab-media_talk, nstab-project_talk
 			// * nstab-image_talk, nstab-mediawiki_talk, nstab-template_talk
 			// * nstab-help_talk, nstab-category_talk,
 			// * nstab-<subject namespace key>_talk
-			$content_navigation['namespaces'][$talkId] = $this->tabAction(
+			$associatedPages[$talkId] = $this->tabAction(
 				$talkPage, [ "nstab-$talkId", "talk" ], $isTalk, '', $userCanRead
 			);
-			$content_navigation['namespaces'][$talkId]['context'] = 'talk';
+			$associatedPages[$talkId]['context'] = 'talk';
 
 			if ( $userCanRead ) {
 				// Adds "view" view link
@@ -1545,6 +1549,7 @@ class SkinTemplate extends Skin {
 					}
 				}
 			}
+			$namespaces = $associatedPages;
 		} else {
 			// If it's not content, and a request URL is set it's got to be a special page
 			try {
@@ -1552,13 +1557,17 @@ class SkinTemplate extends Skin {
 			} catch ( MWException $e ) {
 				$url = false;
 			}
-			$content_navigation['namespaces']['special'] = [
+			$namespaces['special'] = [
 				'class' => 'selected',
 				'text' => $this->msg( 'nstab-special' )->text(),
 				'href' => $url, // @see: T4457, T4510
 				'context' => 'subject'
 			];
+			$associatedPages += $this->getSpecialPageRelatedLinks( $title );
 		}
+
+		$content_navigation['namespaces'] = $namespaces;
+		$content_navigation['associatedPages'] = $associatedPages;
 		$this->runOnSkinTemplateNavigationHooks( $this, $content_navigation );
 
 		// Setup xml ids and tooltip info
@@ -1605,6 +1614,44 @@ class SkinTemplate extends Skin {
 
 		$this->contentNavigationCached = $content_navigation;
 		return $content_navigation;
+	}
+
+	/**
+	 * Return a list of pages that have been marked as related to the special
+	 * page for display.
+	 *
+	 * @param Title $title
+	 * @return array
+	 */
+	private function getSpecialPageRelatedLinks( Title $title ): array {
+		$related = [];
+		$specialFactory = MediaWikiServices::getInstance()->getSpecialPageFactory();
+		$special = $specialFactory->getPage( $title->getText() );
+		if ( $special === null ) {
+			// not a valid special page
+			return [];
+		}
+		$specialRelatedLinks = $special->getRelatedNavigationLinks();
+		// If no sub pages we should not render.
+		if ( count( $specialRelatedLinks ) === 0 ) {
+			return [];
+		}
+
+		foreach ( $specialRelatedLinks as $i => $relatedTitleText ) {
+			$relatedTitle = Title::newFromText( $relatedTitleText );
+			$special = $specialFactory->getPage( $relatedTitle->getText() );
+			if ( $special === null ) {
+				$text = $relatedTitle->getText();
+			} else {
+				$text = $special->getShortDescription( $relatedTitle->getSubpageText() );
+			}
+			$related['special-related-link-' . strval( $i ) ] = [
+				'text' => $text,
+				'href' => $relatedTitle->getLocalUrl(),
+				'class' => $relatedTitle->equals( $title ) ? 'selected' : '',
+			];
+		}
+		return $related;
 	}
 
 	/**

includes/specialpage/SpecialPage.php
@@ -609,6 +609,17 @@ class SpecialPage implements MessageLocalizer {
 	}
 
 	/**
+	 * Return an array of strings representing page titles that are discoverable to end users via UI.
+	 *
+	 * @since 1.39
+	 * @stable to call or override
+	 * @return string[] strings representing page titles that can be rendered by skins if required.
+	 */
+	public function getRelatedNavigationLinks() {
+		return [];
+	}
+
+	/**
 	 * Perform a regular substring search for prefixSearchSubpages
 	 * @since 1.36 Added $searchEngineFactory parameter
 	 * @param string $search Prefix to search for
@@ -774,6 +785,25 @@ class SpecialPage implements MessageLocalizer {
 	 */
 	public function getDescription() {
 		return $this->msg( strtolower( $this->mName ) )->text();
+	}
+
+	/**
+	 * Similar to getDescription but takes into account sub pages and designed for display
+	 * in tabs.
+	 *
+	 * @since 1.39
+	 * @stable to override if special page has complex parameter handling. Use default message keys
+	 * where possible.
+	 *
+	 * @param string $path (optional)
+	 * @return string
+	 */
+	public function getShortDescription( string $path = '' ): string {
+		$lowerPath = strtolower( str_replace( '/', '-', $path ) );
+		$shortKey = 'special-tab-' . $lowerPath;
+		$shortKey .= '-short';
+		$msgShort = $this->msg( $shortKey );
+		return $msgShort->text();
 	}
 
 	/**

commit 635c063b5939776a909ba72ce898ae28f949823e

LocalisationCache: Remove accidentally checked in code
Partially undo 5fa2b63 which accidentally introduced code
from Ic14549433ebbb3dd5b8aee68f0feb17ae681c767

Bug: T313349
Change-Id: I07632a6e685db90dc842c173c5b44830a84e5f3a

includes/language/LocalisationCache.php
@@ -1001,24 +1001,7 @@ class LocalisationCache {
 		# Replace spaces with underscores in namespace names
 		$allData['namespaceNames'] = str_replace( ' ', '_', $allData['namespaceNames'] );
 
-		# Search messages for aliases and add them when found. (T89947)
-		foreach ( $allData['messages'] as $key => $msg ) {
-			if ( strpos( $key, 'special-page-alias' ) === 0 ) {
-				[ $prefix, $name ] = explode( 'special-page-alias-', $key );
-				$aliases = explode( "\n", trim( $msg ) );
-				if ( array_key_exists( $name, $allData['specialPageAliases'] ) ) {
-					$allData['specialPageAliases'][$name] = array_merge(
-						$allData['specialPageAliases'][$name],
-						$aliases
-					);
-				} else {
-					$allData['specialPageAliases'][$name] = $aliases;
-				}
-			}
-		}
-
 		# And do the same for special page aliases. $page is an array.
-		// @phan-suppress-next-line PhanTypePossiblyInvalidDimOffset phan #3569
 		foreach ( $allData['specialPageAliases'] as &$page ) {
 			$page = str_replace( ' ', '_', $page );
 		}