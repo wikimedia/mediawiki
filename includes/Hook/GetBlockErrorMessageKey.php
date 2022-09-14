cc55e848e4f0768fda521020b23b8ba2a6926c60
AnaïsGueyte <agueyte@wikimedia.org>
2022-09-14 14:24:53 -0400

    Create Hook to check block's error messages from extensions
    Bug: T317201
    Change-Id: I1c20e7e62e0f2b453735689e3dc543164b5fd067

---
+++ includes/Hook/GetBlockErrorMessageKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MediaWiki\Hook;
+
+use MediaWiki\Block\Block;
+
+/**
+ * This is a hook handler interface, see docs/Hooks.md.
+ * Use the hook name "GetBlockErrorMessageKey" to register handlers implementing this interface.
+ *
+ * @stable to implement
+ * @ingroup Hooks
+ */
+interface GetBlockErrorMessageKey {
+	/**
+	 * This hook is called in BlockErrorFormatter to allow
+	 * extensions to override the message that will be displayed
+	 * to the user.
+	 *
+	 * @since 1.40
+	 *
+	 * @param Block $block
+	 * @param string &$key
+	 * @return bool|void True or no return value to continue or false to abort
+	 */
+	public function onGetBlockErrorMessageKey( $block, &$key );
+}
