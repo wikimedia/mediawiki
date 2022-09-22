b708c981a658c63bde062ba9cdc889e0714d558b
Timo Tijhof <krinkle@fastmail.com>
2022-09-22 22:21:19 +0100

    UserGroupManager: Document when/why to use purgeExpiredUserrights.php
    Change-Id: I2870b30002ff48e8369c8df60b80a0774f98c6f3

--- maintenance/purgeExpiredUserrights.php
+++ maintenance/purgeExpiredUserrights.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Remove expired userrights from user_groups table and move them to former_user_groups
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -12,12 +10,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
  *
- * @copyright GPLv2 http://www.gnu.org/copyleft/gpl.html
- * @author Eddie Greiner-Petter <wikimedia.org at eddie-sh.de>
- * @ingroup Maintenance
+ * @file
  */
 
 use MediaWiki\MediaWikiServices;
@@ -25,11 +23,21 @@ use MediaWiki\MediaWikiServices;
 require_once __DIR__ . '/Maintenance.php';
 
 /*
- * Maintenance script to move expired userrights to user_former_groups
+ * Remove expired userrights from user_groups table and move them to former_user_groups.
+ *
+ * By default, this does not need to be run. The UserGroupManager service naturally
+ * takes care of detecting expired rows when it is written to (e.g. from SpecialUserrights)
+ * and queues UserGroupExpiryJob to purge expired rows.
+ *
+ * Large wiki farms may experience stale rows if their users manage local groups
+ * via a central wiki. In that case, UserGroupExpiryJob may run rarely or never
+ * from local wikis, in which case this script can help to periodically clean up
+ * expired rows.
  *
  * @since 1.31
+ * @ingroup Maintenance
+ * @author Eddie Greiner-Petter <wikimedia.org at eddie-sh.de>
  */
-
 class PurgeExpiredUserrights extends Maintenance {
 	public function __construct() {
 		parent::__construct();
