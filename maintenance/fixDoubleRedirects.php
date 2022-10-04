9ad71e8ef98a5f3687dde2198f7d0269bd9b5e22
Umherirrender <umherirrender_de.wp@web.de>
2022-10-04 20:44:50 +0200

    fixDoubleRedirects.php: Work only with proper titles
    Bug: T314735
    Change-Id: I7a1b3ae3431a4bc41b314bbd70b0458b87ae5296

--- maintenance/fixDoubleRedirects.php
+++ maintenance/fixDoubleRedirects.php
@@ -99,6 +99,15 @@ class FixDoubleRedirects extends Maintenance {
 		foreach ( $res as $row ) {
 			$titleA = Title::makeTitle( $row->pa_namespace, $row->pa_title );
 			$titleB = Title::makeTitle( $row->pb_namespace, $row->pb_title );
+			if ( !$titleA->canExist() || !$titleB->canExist() ) {
+				$this->error( "Cannot fix redirect from" .
+					( $titleA->canExist() ? "" : " invalid" ) . " title " . $titleA->getPrefixedText()
+					. " to new" .
+					( $titleB->canExist() ? "" : " invalid" ) . " target " . $titleB->getPrefixedText()
+					. "\n"
+				);
+				continue;
+			}
 
 			$processedTitles .= "* [[$titleA]]\n";
 
