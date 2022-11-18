e0a84b032b027b95760b390056deacf8ad32ca1c
Máté Szabó <mszabo@fandom.com>
2022-11-18 15:19:54 +0100

    Parser: Fix extractSections() behavior for PHP >= 8.0
    PHP 8.0 changed the behavior of numeric comparisons such that
    non-numeric strings no longer weakly equal 0.[1] This breaks the logic
    in Parser::extractSections(), which was relying on the old comparison
    behavior for section indexes and in turn causes the revisions API to
    return a bogus 'nosuchsection' for error when called with rvsection=new.
    
    Fix the logic by explicitly casting the section index to a number, which
    will yield the appropriate numeric section index for a numbered section
    index and 0 for a non-numeric section index (like 'new'). Also add test
    cases for the relevant API module.
    --
    [1] https://wiki.php.net/rfc/string_to_number_comparison
    
    Change-Id: If32aa4d575cff66bd4eee56f9e3b0b0d9ba04fde
    Bug: T323373
    (cherry picked from commit b523ab0081d200d847248f93514b6aa661c3a6b7)

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -5794,7 +5794,10 @@ class Parser {
 		# Process section extraction flags
 		$flags = 0;
 		$sectionParts = explode( '-', $sectionId );
-		$sectionIndex = array_pop( $sectionParts );
+		// The section ID may either be a magic string such as 'new' (which should be treated as 0),
+		// or a numbered section ID in the format of "T-<section index>".
+		// Explicitly coerce the section index into a number accordingly. (T323373)
+		$sectionIndex = (int)array_pop( $sectionParts );
 		foreach ( $sectionParts as $part ) {
 			if ( $part === 'T' ) {
 				$flags |= Preprocessor::DOM_FOR_INCLUSION;
@@ -5804,7 +5807,7 @@ class Parser {
 		# Check for empty input
 		if ( strval( $text ) === '' ) {
 			# Only sections 0 and T-0 exist in an empty document
-			if ( $sectionIndex == 0 ) {
+			if ( $sectionIndex === 0 ) {
 				if ( $mode === 'get' ) {
 					return '';
 				}
@@ -5827,7 +5830,7 @@ class Parser {
 		$node = $root->getFirstChild();
 
 		# Find the target section
-		if ( $sectionIndex == 0 ) {
+		if ( $sectionIndex === 0 ) {
 			# Section zero doesn't nest, level=big
 			$targetLevel = 1000;
 		} else {
