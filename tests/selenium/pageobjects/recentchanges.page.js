f4a4b07500f9c9af31520f3a88090e002c17c471
Željko Filipin <zeljko.filipin@gmail.com>
2022-11-25 14:42:59 +0100

    selenium: Run most tests targeting the beta cluster
    Only creating account via the web interface does not work on the beta
    cluster, because of CAPTCHA.
    
    Bug: T323824
    Change-Id: I7e9c6169f1c72450b4b620a8712629af7637f06b

--- tests/selenium/pageobjects/recentchanges.page.js
+++ tests/selenium/pageobjects/recentchanges.page.js
@@ -4,10 +4,11 @@ const Page = require( 'wdio-mediawiki/Page' );
 
 class RecentChangesPage extends Page {
 	get changesList() { return $( '.mw-changeslist' ); }
+	get liveUpdates() { return $( '.mw-rcfilters-ui-liveUpdateButtonWidget' ); }
 	get titles() { return this.changesList.$$( '.mw-changeslist-title' ); }
 
 	open() {
-		super.openTitle( 'Special:RecentChanges' );
+		super.openTitle( 'Special:RecentChanges', { hidebots: 0 } );
 	}
 
 }
