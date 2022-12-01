c75df5c39b0f62a9cb23aa0f8d6df2ad673de475
Winston Sung <winston21.sung@gmail.com>
2022-12-01 23:47:23 +0800

    Merge gan translations back to gan-hans and gan-hant respectively
    Bug: T286291
    Bug: T296188
    Change-Id: I18c90f3c26396699ec60b077bc438d626a0c5c7e

--- languages/messages/MessagesGan.php
+++ languages/messages/MessagesGan.php
@@ -7,24 +7,9 @@
 
 $fallback = 'gan-hant, gan-hans, zh-hant, zh, zh-hans';
 
-$namespaceNames = [
-	NS_MEDIA            => '媒體',
-	NS_SPECIAL          => '特別',
-	NS_TALK             => '談詑',
-	NS_USER             => '用戶',
-	NS_USER_TALK        => '用戶・談詑',
-	NS_PROJECT_TALK     => '$1・談詑',
-	NS_FILE             => '文檔',
-	NS_FILE_TALK        => '文檔・談詑',
-	NS_MEDIAWIKI_TALK   => 'MediaWiki・談詑',
-	NS_TEMPLATE         => '模板',
-	NS_TEMPLATE_TALK    => '模板・談詑',
-	NS_HELP             => '幫助',
-	NS_HELP_TALK        => '幫助・談詑',
-	NS_CATEGORY         => '分類',
-	NS_CATEGORY_TALK    => '分類・談詑',
-];
-
-$namespaceAliases = [
-	'$1_談詑' => NS_PROJECT_TALK,
-];
+# Store namespace aliases, special page aliases and magic words of variant languages in their
+#  message files respectively.
+#
+# $namespaceAliases = [];
+# $specialPageAliases = [];
+# $magicWords = [];
