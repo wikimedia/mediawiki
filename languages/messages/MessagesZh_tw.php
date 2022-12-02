64c005972c1b5de34e35485c6c5ca093f99a04c1
Winston Sung <winston21.sung@gmail.com>
2022-12-02 15:13:11 +0800

    Merge zh and zh-tw namespace translations back to zh-hans, zh-hant,  zh-hk respectively
    * Merge zh-tw translations back to zh-hant
    * Split zh-hans, zh-hant, zh-hk translations and aliases from zh to
      respective message files
    * Language->getNamespaceAliases() with the updated language fallback
      chain can already fetch aliases from language variant i18n files
    
    All translation authors are added when merging those translations.
    
    Bug: T286291
    Bug: T296188
    Change-Id: Ia2aa17fa15fe6d6b9396bf06b472d28b3ab12f68

--- languages/messages/MessagesZh_tw.php
+++ languages/messages/MessagesZh_tw.php
@@ -23,20 +23,6 @@
 
 $fallback = 'zh-hant, zh-hk, zh, zh-hans';
 
-$namespaceNames = [
-	NS_USER             => '使用者',
-	NS_USER_TALK        => '使用者討論',
-	NS_HELP             => '使用說明',
-	NS_HELP_TALK        => '使用說明討論',
-];
-
-$namespaceAliases = [
-	'Image' => NS_FILE,
-	'Image_talk' => NS_FILE_TALK,
-	"圖片" => NS_FILE,
-	"圖片討論" => NS_FILE_TALK,
-];
-
 /** @phpcs-require-sorted-array */
 $specialPageAliases = [
 	'Allmessages'               => [ '所有訊息' ],
