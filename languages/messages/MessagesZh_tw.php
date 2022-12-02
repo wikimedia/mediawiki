096ef8b7d5b5906267061a869f8a49b23dc877ad
Winston Sung <winston21.sung@gmail.com>
2022-12-02 15:22:41 +0800

    Merge zh-Hant-TW special page aliases translations back to zh-Hant
    Bug: T286291
    Bug: T296188
    Change-Id: I35970dfbeba5f060240216ee22845983b2f3493c

--- languages/messages/MessagesZh_tw.php
+++ languages/messages/MessagesZh_tw.php
@@ -23,32 +23,6 @@
 
 $fallback = 'zh-hant, zh-hk, zh, zh-hans';
 
-/** @phpcs-require-sorted-array */
-$specialPageAliases = [
-	'Allmessages'               => [ '所有訊息' ],
-	'Ancientpages'              => [ '最舊頁面' ],
-	'Block'                     => [ '封鎖使用者' ],
-	'CreateAccount'             => [ '建立帳號' ],
-	'FileDuplicateSearch'       => [ '搜尋重復檔案' ],
-	'Invalidateemail'           => [ '無法識別的電郵位址' ],
-	'LinkSearch'                => [ '搜尋網頁連結' ],
-	'Listfiles'                 => [ '檔案清單' ],
-	'Listredirects'             => [ '重新導向頁面清單' ],
-	'Lockdb'                    => [ '鎖定資料庫' ],
-	'MIMEsearch'                => [ 'MIME搜尋' ],
-	'Newimages'                 => [ '新增檔案' ],
-	'Randomredirect'            => [ '隨機重新導向頁面' ],
-	'Recentchanges'             => [ '近期變動' ],
-	'Revisiondelete'            => [ '刪除或恢復版本' ],
-	'Unblock'                   => [ '解除封鎖' ],
-	'Unlockdb'                  => [ '解除資料庫鎖定' ],
-	'Unwatchedpages'            => [ '未被監視的頁面' ],
-	'Userrights'                => [ '使用者權限' ],
-	'Watchlist'                 => [ '監視清單' ],
-	'Whatlinkshere'             => [ '連入頁面' ],
-	'Withoutinterwiki'          => [ '沒有跨語言連結的頁面' ],
-];
-
 $datePreferences = [
 	'default',
 	'minguo',
