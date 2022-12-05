d32c260ed0c5f24ef7c1233501f684d612fef5a9
daniel <dkinzler@wikimedia.org>
2022-12-05 20:32:20 +0100

    REST: collect metrics on endpoint access
    This collects metrics on how often each endpoint is hit, and with which
    code it responds.
    
    Change-Id: Ie282bc5b5f5df0bbd6a40c8362ba73fcbbf36c2e

--- includes/Rest/EntryPoint.php
+++ includes/Rest/EntryPoint.php
@@ -63,6 +63,8 @@ class EntryPoint {
 			$authority
 		);
 
+		$metrics = $services->getStatsdDataFactory();
+
 		return ( new Router(
 			self::getRouteFiles( $conf ),
 			ExtensionRegistry::getInstance()->getAttribute( 'RestRoutes' ),
@@ -76,7 +78,9 @@ class EntryPoint {
 			new MWErrorReporter(),
 			$services->getHookContainer(),
 			$context->getRequest()->getSession()
-		) )->setCors( $cors );
+		) )
+			->setCors( $cors )
+			->setMetrics( $metrics );
 	}
 
 	/**
