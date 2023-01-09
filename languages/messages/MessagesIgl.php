145f2d488d5ec5bff0c4f5e44730a322ee9b789a
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2023-01-09 10:21:33 +0200

    Add support for the Igala language (igl)
    There are several variants for the spelling of
    the autonym, with and without diacritics.
    After consultation with User:AgnesAbah,
    who contributed articles to Wikimedia Incubator
    and wrote all the translatewiki translations,
    I'm defining it using a simple string, which
    is identical to English and without any diacritics.
    After a community forms, this can be reconsidered.
    
    Bug: T326526
    Change-Id: I7b97f1f7be2c6e00d5f2a4254baf2262d08465c0

---
+++ languages/messages/MessagesIgl.php
@@ -0,0 +1,10 @@
+<?php
+/** Igala (Igala)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Amir E. Aharoni
+ */
+
+$linkTrail = '/^([a-zA-ŽñÑẹ̀Ẹ́ọ̄ỘàèìòùáéíóúâêîôûāēīōūǎěǐǒǔÀÈÌÒÙÁÉÍÓÚÂÊÎÔÛĀĒĪŌŪǍĚǏǑǓ]+)(.*)$/sDu';
