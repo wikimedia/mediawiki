wikimedia/mediawiki

commit 5156ae0404cd8cc544febd883037db207e456d6b

Allow sharding in site_stats update
This would be useful in cases that lots of edits happen at the same time
and this would reduce the time waiting to get the row lock.

Bug: T306589
Change-Id: I3b869e3b85dfd66575390ef4247f2f81f19c878e

includes/SiteStats.php
@@ -237,15 +237,26 @@ class SiteStats {
 
 	/**
 	 * @param IDatabase $db
-	 * @return stdClass|bool
+	 * @return stdClass
 	 */
 	private static function doLoadFromDB( IDatabase $db ) {
-		return $db->selectRow(
+		$rows = $db->select(
 			'site_stats',
 			self::selectFields(),
-			[ 'ss_row_id' => 1 ],
+			'*',
 			__METHOD__
 		);
+		$finalRow = new stdClass();
+		foreach ( $rows as $row ) {
+			foreach ( self::selectFields() as $field ) {
+				$finalRow->$field = $finalRow->$field ?? 0;
+				if ( $row->$field ) {
+					$finalRow->$field += $row->$field;
+				}
+			}
+
+		}
+		return $finalRow;
 	}
 
 	/**

docs/config-schema.yaml
@@ -2176,6 +2176,17 @@ config-schema:
             enqueued for later.
             There should be no reason to set this in a normal production environment.
             @since 1.38
+    MultiShardSiteStats:
+        default: false
+        type: boolean
+        description: |-
+            Whether site_stats table should have multiple rows. If set to true, in each update,
+            one of ten rows gets updated at random to reduce lock wait time in wikis
+            that have lots of concurrent edits.
+            It should be set to true in really large wikis with big flow of edits,
+            Otherwise it can cause inaccuracy in data.
+            @since 1.39
+            @warning EXPERIMENTAL!
     CacheDirectory:
         default: false
         description: |-

includes/MainConfigNames.php
@@ -1413,6 +1413,12 @@ class MainConfigNames {
 	public const ForceDeferredUpdatesPreSend = 'ForceDeferredUpdatesPreSend';
 
 	/**
+	 * Name constant for the MultiShardSiteStats setting, for use with Config::get()
+	 * @see MainConfigSchema::MultiShardSiteStats
+	 */
+	public const MultiShardSiteStats = 'MultiShardSiteStats';
+
+	/**
 	 * Name constant for the CacheDirectory setting, for use with Config::get()
 	 * @see MainConfigSchema::CacheDirectory
 	 */

includes/MainConfigSchema.php
@@ -3416,6 +3416,21 @@ class MainConfigSchema {
 		'default' => false,
 	];
 
+	/**
+	 * Whether site_stats table should have multiple rows. If set to true, in each update,
+	 * one of ten rows gets updated at random to reduce lock wait time in wikis
+	 * that have lots of concurrent edits.
+	 * It should be set to true in really large wikis with big flow of edits,
+	 * Otherwise it can cause inaccuracy in data.
+	 *
+	 * @since 1.39
+	 * @warning EXPERIMENTAL!
+	 */
+	public const MultiShardSiteStats = [
+		'default' => false,
+		'type' => 'boolean',
+	];
+
 	// endregion -- end performance hacks
 
 	/***************************************************************************/

includes/config-schema.php
@@ -424,6 +424,7 @@ return [
 			'LinkHolderBatchSize' => 1000,
 			'MaximumMovedPages' => 100,
 			'ForceDeferredUpdatesPreSend' => false,
+			'MultiShardSiteStats' => false,
 			'CacheDirectory' => false,
 			'MainCacheType' => 0,
 			'MessageCacheType' => -1,
@@ -2569,6 +2570,7 @@ return [
 				0 => 'integer',
 				1 => 'boolean',
 			],
+			'MultiShardSiteStats' => 'boolean',
 			'ObjectCaches' => 'object',
 			'MainWANCache' => [
 				0 => 'integer',

includes/config-vars.php
@@ -1397,6 +1397,12 @@ $wgMaximumMovedPages = null;
 $wgForceDeferredUpdatesPreSend = null;
 
 /**
+ * Config variable stub for the MultiShardSiteStats setting, for use by phpdoc and IDEs.
+ * @see MediaWiki\MainConfigSchema::MultiShardSiteStats
+ */
+$wgMultiShardSiteStats = null;
+
+/**
  * Config variable stub for the CacheDirectory setting, for use by phpdoc and IDEs.
  * @see MediaWiki\MainConfigSchema::CacheDirectory
  */

includes/deferred/SiteStatsUpdate.php
@@ -37,6 +37,9 @@ class SiteStatsUpdate implements DeferrableUpdate, MergeableUpdate {
 	/** @var int */
 	protected $images = 0;
 
+	private const SHARDS_OFF = 1;
+	private const SHARDS_ON = 10;
+
 	/** @var string[] Map of (table column => counter type) */
 	private const COUNTERS = [
 		'ss_total_edits'   => 'edits',
@@ -98,6 +101,8 @@ class SiteStatsUpdate implements DeferrableUpdate, MergeableUpdate {
 	public function doUpdate() {
 		$services = MediaWikiServices::getInstance();
 		$stats = $services->getStatsdDataFactory();
+		$shards = $services->getMainConfig()->get( MainConfigNames::MultiShardSiteStats ) ?
+			self::SHARDS_ON : self::SHARDS_OFF;
 
 		$deltaByType = [];
 		foreach ( self::COUNTERS as $type ) {
@@ -111,16 +116,26 @@ class SiteStatsUpdate implements DeferrableUpdate, MergeableUpdate {
 		( new AutoCommitUpdate(
 			$services->getDBLoadBalancer()->getConnectionRef( DB_PRIMARY ),
 			__METHOD__,
-			static function ( IDatabase $dbw, $fname ) use ( $deltaByType ) {
+			static function ( IDatabase $dbw, $fname ) use ( $deltaByType, $shards ) {
 				$set = [];
+				$initValues = [];
+				if ( $shards > 1 ) {
+					$shard = mt_rand( 1, $shards );
+				} else {
+					$shard = 1;
+				}
+
+				$hasNegativeDelta = false;
 				foreach ( self::COUNTERS as $field => $type ) {
 					$delta = (int)$deltaByType[$type];
+					$initValues[$field] = $delta;
 					if ( $delta > 0 ) {
 						$set[] = "$field=" . $dbw->buildGreatest(
 							[ $field => $dbw->addIdentifierQuotes( $field ) . '+' . abs( $delta ) ],
 							0
 						);
 					} elseif ( $delta < 0 ) {
+						$hasNegativeDelta = true;
 						$set[] = "$field=" . $dbw->buildGreatest(
 							[ 'new' => $dbw->addIdentifierQuotes( $field ) . '-' . abs( $delta ) ],
 							0
@@ -129,7 +144,17 @@ class SiteStatsUpdate implements DeferrableUpdate, MergeableUpdate {
 				}
 
 				if ( $set ) {
-					$dbw->update( 'site_stats', $set, [ 'ss_row_id' => 1 ], $fname );
+					if ( $hasNegativeDelta ) {
+						$dbw->update( 'site_stats', $set, [ 'ss_row_id' => $shard ], $fname );
+					} else {
+						$dbw->upsert(
+							'site_stats',
+							array_merge( [ 'ss_row_id' => $shard ], $initValues ),
+							'ss_row_id',
+							$set,
+							$fname
+						);
+					}
 				}
 			}
 		) )->doUpdate();

commit 2b44fbe3eb457a28c454254fd9ebaee584267d54

SiteStats: Remove unneeded SiteStats::salvageIncorrectRow
The function SiteStats::salvageIncorrectRow is a noop,
the variable $map is not returned, instead the unmodified $row is
returned, function added in 6535091

self::doLoadFromDB on second attempt after the initialize (via
doPlaceholderInit or doAllAndCommit) returns a valid row, making this
extra check no longer needed (possible since 27c76fa).

Fix doLoadFromDB on first attempt for empty table, broken since 5156ae0

Change-Id: Iec2b4bbdb2ca546eb377a6d12106d62b5219886f

includes/SiteStats.php
@@ -80,12 +80,6 @@ class SiteStats {
 			$row = self::doLoadFromDB( $lb->getConnectionRef( DB_PRIMARY ) );
 		}
 
-		if ( !self::isRowSensible( $row ) ) {
-			wfDebug( __METHOD__ . ": site_stats persistently nonsensical o_O" );
-			// Always return a row-like object
-			$row = self::salvageIncorrectRow( $row );
-		}
-
 		return $row;
 	}
 
@@ -238,7 +232,7 @@ class SiteStats {
 
 	/**
 	 * @param IDatabase $db
-	 * @return stdClass
+	 * @return stdClass|false
 	 */
 	private static function doLoadFromDB( IDatabase $db ) {
 		$fields = self::selectFields();
@@ -247,6 +241,9 @@ class SiteStats {
 			->from( 'site_stats' )
 			->caller( __METHOD__ )
 			->fetchResultSet();
+		if ( !$rows->numRows() ) {
+			return false;
+		}
 		$finalRow = new stdClass();
 		foreach ( $rows as $row ) {
 			foreach ( $fields as $field ) {
@@ -265,7 +262,7 @@ class SiteStats {
 	 *
 	 * Checks only fields which are filled by SiteStatsInit::refresh.
 	 *
-	 * @param bool|stdClass $row
+	 * @param stdClass|false $row
 	 * @return bool
 	 */
 	private static function isRowSensible( $row ) {
@@ -289,22 +286,6 @@ class SiteStats {
 		}
 
 		return true;
-	}
-
-	/**
-	 * @param stdClass|bool $row
-	 * @return stdClass
-	 */
-	private static function salvageIncorrectRow( $row ) {
-		$map = $row ? (array)$row : [];
-		// Fill in any missing values with zero
-		$map += array_fill_keys( self::selectFields(), 0 );
-		// Convert negative values to zero
-		foreach ( $map as $field => $value ) {
-			$map[$field] = max( 0, $value );
-		}
-
-		return (object)$row;
 	}
 
 	/**

tests/phpunit/includes/SiteStatsTest.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * @group Database
+ */
 class SiteStatsTest extends MediaWikiIntegrationTestCase {
 
 	/**
@@ -32,4 +35,14 @@ class SiteStatsTest extends MediaWikiIntegrationTestCase {
 		$this->assertSame( 0, SiteStats::jobs() );
 	}
 
+	/**
+	 * @covers SiteStats
+	 */
+	public function testInit() {
+		$this->db->delete( 'site_stats', IDatabase::ALL_ROWS, __METHOD__ );
+		SiteStats::unload();
+
+		SiteStats::edits();
+		$this->assertNotFalse( $this->db->selectRow( 'site_stats', '1', IDatabase::ALL_ROWS, __METHOD__ ) );
+	}
 }