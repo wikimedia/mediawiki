aac9ee6b6301ecb1c5d8851570f373ceef1d8b80
Reedy <reedy@wikimedia.org>
2023-02-25 00:32:11 +0000

    SpecialEditTags: Set default of '' for wpReason
    Bug: T330529
    Change-Id: If0fcebbb5926cc6264dc51ce3f6ff9b3fb54a4e9
    (cherry picked from commit 5b15c7c4393222be037dd9a2ea0ade0d4e20ef0c)

--- includes/specials/SpecialEditTags.php
+++ includes/specials/SpecialEditTags.php
@@ -125,7 +125,7 @@ class SpecialEditTags extends UnlistedSpecialPage {
 			$this->ids
 		);
 
-		$this->reason = $request->getVal( 'wpReason' );
+		$this->reason = $request->getVal( 'wpReason', '' );
 		// We need a target page!
 		if ( $this->targetObj === null ) {
 			$output->addWikiMsg( 'undelete-header' );
