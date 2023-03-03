c1ce83bc9a5948efcbdfcbc65e48493828c6e3d2
Tim Starling <tstarling@wikimedia.org>
2023-03-03 11:14:57 +1100

    Add basic test for SpecialRenameuser
    Bug: T27482
    Change-Id: Ica6cc4acaca9378243f3819f339120b6d4bcd738

---
+++ tests/phpunit/includes/specials/SpecialRenameuserTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @group Database
+ * @covers SpecialRenameuser
+ * @covers \MediaWiki\RenameUser\RenameuserSQL
+ */
+class SpecialRenameuserTest extends SpecialPageTestBase {
+	protected function newSpecialPage() {
+		$services = $this->getServiceContainer();
+		return new SpecialRenameuser(
+			$services->getContentLanguage(),
+			$services->getMovePageFactory(),
+			$services->getPermissionManager(),
+			$services->getTitleFactory(),
+			$services->getUserFactory(),
+			$services->getUserNamePrefixSearch(),
+			$services->getUserNameUtils()
+		);
+	}
+
+	public static function provideRenameAndMove() {
+		return [
+			'no move' => [ false, false ],
+			'normal move' => [ true, false ],
+			'suppress redirect' => [ true, true ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideRenameAndMove
+	 * @param bool $movePages
+	 * @param bool $suppressRedirects
+	 */
+	public function testRenameAndMove( $movePages, $suppressRedirects ) {
+		$userFactory = $this->getServiceContainer()->getUserFactory();
+		$titleFactory = $this->getServiceContainer()->getTitleFactory();
+
+		$performer = $this->getTestSysop()->getUser();
+		$oldUser = $this->getTestUser()->getUser();
+		$oldName = $oldUser->getName();
+		$newName = $oldName . ' new';
+		$oldPage = $oldUser->getUserPage();
+		$this->editPage( $oldPage, 'user page' );
+		$this->editPage( $oldPage->getSubpage( 'subpage' ), 'subpage' );
+
+		$formData = [
+			'token' => $performer->getEditToken(),
+			'oldusername' => $oldName,
+			'newusername' => $newName,
+			'reason' => 'r',
+		];
+		if ( $movePages ) {
+			$formData['movepages'] = '1';
+		}
+		if ( $suppressRedirects ) {
+			$formData['suppressredirect'] = '1';
+		}
+
+		$this->executeSpecialPage(
+			'',
+			new FauxRequest( $formData, true ),
+			null,
+			$performer
+		);
+
+		$this->assertTrue( $userFactory->newFromName( $newName )->isRegistered(),
+			'new user exists' );
+		$this->assertSame(
+			$movePages,
+			$titleFactory->makeTitle( NS_USER, $newName )->exists(),
+			'new user page exists'
+		);
+		$this->assertSame(
+			$movePages,
+			$titleFactory->makeTitle( NS_USER, "$newName/subpage" )->exists(),
+			'new user subpage exists'
+		);
+
+		$oldPage->resetArticleID( false );
+		$this->assertSame( !$suppressRedirects, $oldPage->exists() );
+	}
+}
