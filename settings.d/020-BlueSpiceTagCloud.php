3b488ccac79a01f2d1219615321768aed7994735
a-Bec <97602229+a-Bec@users.noreply.github.com>
2023-03-16 15:57:42 +0100

    new global variable (#238)
    excluding  List added for default TagCloudFilter in BlueSpiceTagCloud
    
    ERM30707
    
    Co-authored-by: Arwed Beck <abeck@hallowelt.com>

--- settings.d/020-BlueSpiceTagCloud.php
+++ settings.d/020-BlueSpiceTagCloud.php
@@ -1,3 +1,19 @@
 <?php
 
 wfLoadExtension( "BlueSpiceTagCloud" );
+
+$GLOBALS['bsgTagCloudTypeCategoryExclude'] = [
+	'BPMN Association',
+	'BPMN DataInputAssociation',
+	'BPMN DataStoreReference',
+	'BPMN EndEvent',
+	'BPMN ExclusiveGateway',
+	'BPMN IntermediateThrowEvent',
+	'BPMN Lane',
+	'BPMN Participant',
+	'BPMN SequenceFlow',
+	'BPMN StartEvent',
+	'BPMN SubProcess',
+	'BPMN TextAnnotation',	
+	'Imported vocabulary'
+];
