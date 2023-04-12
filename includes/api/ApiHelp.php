wikimedia/mediawiki

commit 153a415742087b5e51af46d98f5b72b56dc1b49d

Use a SectionMetadata object in Linker::generateTOC()
This updates Linker::generateTOC() so it uses a TOCData object, not
a "legacy" associative array.

Change-Id: I8fa83afd17b769df69bdd61ebd1b2ef3fe8b540f

includes/api/ApiHelp.php
@@ -26,6 +26,7 @@ use MediaWiki\Linker\Linker;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
 use Wikimedia\ParamValidator\ParamValidator;
+use Wikimedia\Parsoid\Core\TOCData;
 use Wikimedia\RemexHtml\Serializer\SerializerNode;
 
 /**
@@ -183,8 +184,7 @@ class ApiHelp extends ApiBase {
 		$haveModules = [];
 		$html = self::getHelpInternal( $context, $modules, $options, $haveModules );
 		if ( !empty( $options['toc'] ) && $haveModules ) {
-			// @phan-suppress-next-line SecurityCheck-DoubleEscaped Triggered by Linker?
-			$out->addHTML( Linker::generateTOC( $haveModules, $context->getLanguage() ) );
+			$out->addHTML( Linker::generateTOC( TOCData::fromLegacy( $haveModules ), $context->getLanguage() ) );
 		}
 		$out->addHTML( $html );
 

includes/linker/Linker.php
@@ -48,7 +48,7 @@ use TitleValue;
 use User;
 use WatchedItem;
 use Wikimedia\IPUtils;
-use Wikimedia\Parsoid\Core\SectionMetadata;
+use Wikimedia\Parsoid\Core\TOCData;
 use Wikimedia\Rdbms\SelectQueryBuilder;
 use Wikimedia\RemexHtml\Serializer\SerializerNode;
 use Xml;
@@ -1763,21 +1763,18 @@ class Linker {
 	 * @internal For use by ParserOutput and API modules
 	 * Generate a table of contents from a section tree.
 	 *
-	 * @since 1.16.3. $lang added in 1.17
-	 * @param array[]|SectionMetadata[] $tree Return value of ParserOutput::getSections()
+	 * @since 1.16.3. $lang added in 1.17. Parameters changed in 1.40.
+	 * @param ?TOCData $tocData Return value of ParserOutput::getSections()
 	 * @param Language|null $lang Language for the toc title, defaults to user language
 	 * @param array $options FIXME: Document
 	 * @return string HTML fragment
 	 */
-	public static function generateTOC( $tree, Language $lang = null, array $options = [] ): string {
+	public static function generateTOC( ?TOCData $tocData, Language $lang = null, array $options = [] ): string {
 		$toc = '';
 		$lastLevel = 0;
 		$maxTocLevel = $options['maxtoclevel'] ?? null;
-		foreach ( $tree as $section ) {
-			if ( $section instanceof SectionMetadata ) {
-				$section = $section->toLegacy();
-			}
-			$tocLevel = $section['toclevel'];
+		foreach ( ( $tocData ? $tocData->getSections() : [] ) as $section ) {
+			$tocLevel = $section->tocLevel;
 			if ( $maxTocLevel !== null && $tocLevel < $maxTocLevel ) {
 				if ( $tocLevel > $lastLevel ) {
 					$toc .= self::tocIndent();
@@ -1792,9 +1789,9 @@ class Linker {
 					$toc .= self::tocLineEnd();
 				}
 
-				$toc .= self::tocLine( $section['linkAnchor'],
-					$section['line'], $section['number'],
-					$tocLevel, $section['index'] );
+				$toc .= self::tocLine( $section->linkAnchor,
+					$section->line, $section->number,
+					$tocLevel, $section->index );
 				$lastLevel = $tocLevel;
 			}
 		}

includes/parser/ParserOutput.php
@@ -487,7 +487,9 @@ class ParserOutput extends CacheTime implements ContentMetadataCollector {
 					$config = $services->getMainConfig();
 					$maxTocLevel = $config->get( MainConfigNames::MaxTocLevel );
 					$toc = Linker::generateTOC(
-						$this->getSections(), $userLang, [ "maxtoclevel" => $maxTocLevel ] );
+						$this->getTOCData(), $userLang, [
+							"maxtoclevel" => $maxTocLevel,
+						] );
 					// language conversion needs to be done on the TOC fetched
 					// from parser cache
 					if ( !$this->getOutputFlag( ParserOutputFlags::NO_TOC_CONVERSION ) ) {

commit 05535be6a8bdee2f98df423650c4e1878dba3486

Ensure ApiHelp correctly types values in TOCData objects
* Broken since 153a415742087b5e51af46d98f5b72b56dc1b49d

Bug: T334551
Change-Id: Id46ba8096beb29d4ece35a6c41b94237e5d9fe1e

includes/api/ApiHelp.php
@@ -335,7 +335,7 @@ class ApiHelp extends ApiBase {
 					'anchor' => $anchor,
 					'line' => $headerContent,
 					'number' => implode( '.', $tocnumber ),
-					'index' => false,
+					'index' => '',
 				];
 				if ( empty( $options['noheader'] ) ) {
 					$help['header'] .= Html::rawElement(

includes/api/ApiMain.php
@@ -2348,7 +2348,7 @@ class ApiMain extends ApiBase {
 					'anchor' => 'main/datatypes',
 					'line' => $header,
 					'number' => implode( '.', $tocnumber ),
-					'index' => false,
+					'index' => '',
 				];
 			}
 
@@ -2377,7 +2377,7 @@ class ApiMain extends ApiBase {
 					'anchor' => 'main/templatedparams',
 					'line' => $header,
 					'number' => implode( '.', $tocnumber ),
-					'index' => false,
+					'index' => '',
 				];
 			}
 
@@ -2405,7 +2405,7 @@ class ApiMain extends ApiBase {
 					'anchor' => 'main/credits',
 					'line' => $header,
 					'number' => implode( '.', $tocnumber ),
-					'index' => false,
+					'index' => '',
 				];
 			}
 		}