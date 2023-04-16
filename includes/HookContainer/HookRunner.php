aaa0690cd67dca963e607d493e82cbc00601939e
Umherirrender <umherirrender_de.wp@web.de>
2023-04-16 19:24:00 +0200

    Add Hook suffix to GetBlockErrorMessageKeyHook
    Common naming convention for hook runner interfaces
    
    The hook is new in 1.40 and the rename should be backported,
    but the old name needs alias as it is already used in extensions.
    
    Bug: T334689
    Change-Id: I3f9e6d33f1bf29e937c82860fcebd088dde647da
    (cherry picked from commit 033e75cb26353dfb2f59cc1d62ce78e7547862b9)

--- includes/HookContainer/HookRunner.php
+++ includes/HookContainer/HookRunner.php
@@ -181,7 +181,7 @@ class HookRunner implements
 	\MediaWiki\Hook\FileUploadHook,
 	\MediaWiki\Hook\FormatAutocommentsHook,
 	\MediaWiki\Hook\GalleryGetModesHook,
-	\MediaWiki\Hook\GetBlockErrorMessageKey,
+	\MediaWiki\Hook\GetBlockErrorMessageKeyHook,
 	\MediaWiki\Hook\GetCacheVaryCookiesHook,
 	\MediaWiki\Hook\GetCanonicalURLHook,
 	\MediaWiki\Hook\GetDefaultSortkeyHook,
