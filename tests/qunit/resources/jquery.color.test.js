f311c36cb10a6f0c375575d3ffcbdee680980996
Timo Tijhof <krinkle@fastmail.com>
2023-04-15 22:39:38 -0700

    jquery.color: Simplify async test with native async-await
    * Simplify async test by using async-await
      Ref https://api.qunitjs.com/QUnit/test/.
    
    * Use current coding style for separate var statements (instead of one-var),
      and (within test suites) switch also to const/let.
    
    * Remove needless QUnit.newMwEnvironment.
    
      This module doesn't use mw.config or mw.msg, so there's no
      need for QUnit.newMwEnvironment's hooks (pre-existing issue).
    
    * Prefer QUnit.module scope over file scope.
    
      Previously we often used file scope to ensure local variables
      in classic debug=1 debug mode. Since QUnit 2 introduced module
      scopes, useful for things like hooks.beforeEach(), we can use those
      instead and not need an additional file closure regardless of which
      debug mode is used.
    
    * Prefer arrow functions for simple closures possible.
      Showcased in upstream docs.
      Ref https://api.qunitjs.com/QUnit/test/.
    
    Bug: T250045
    Change-Id: I4010200207de4d37701c9fb1e71fd962bdc40db9

--- tests/qunit/resources/jquery.color.test.js
+++ tests/qunit/resources/jquery.color.test.js
@@ -1,16 +1,15 @@
-( function () {
-	QUnit.module( 'jquery.color', QUnit.newMwEnvironment() );
+QUnit.module( 'jquery.color', () => {
 
-	QUnit.test( 'animate', function ( assert ) {
-		var done = assert.async(),
-			$canvas = $( '<div>' ).css( 'background-color', '#fff' ).appendTo( '#qunit-fixture' );
+	QUnit.test( 'animate', async ( assert ) => {
+		const $canvas = $( '<div>' )
+			.css( 'background-color', '#fff' )
+			.appendTo( '#qunit-fixture' );
 
 		// eslint-disable-next-line no-jquery/no-animate
-		$canvas.animate( { 'background-color': '#000' }, 3 ).promise()
-			.done( function () {
-				var endColors = $.colorUtil.getRGB( $canvas.css( 'background-color' ) );
-				assert.deepEqual( endColors, [ 0, 0, 0 ], 'end state' );
-			} )
-			.always( done );
+		await $canvas.animate( { 'background-color': '#000' }, 3 ).promise();
+
+		const endColors = $.colorUtil.getRGB( $canvas.css( 'background-color' ) );
+		assert.deepEqual( endColors, [ 0, 0, 0 ], 'end state' );
 	} );
-}() );
+
+} );
