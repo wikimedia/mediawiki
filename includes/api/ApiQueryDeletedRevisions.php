88e50ae95ec44098875873bf01ad55fb6cbedc12
Umherirrender <umherirrender_de.wp@web.de>
2023-04-22 10:53:55 +0200

    api: Do not use rvslots=* in example urls
    This ends up as real code and is not good from a performance view,
    it is better to specify what needed and not all
    
    Change-Id: I10d53af1d4809e0429fa35e5073e4903651a9d7d

--- includes/api/ApiQueryDeletedRevisions.php
+++ includes/api/ApiQueryDeletedRevisions.php
@@ -329,7 +329,7 @@ class ApiQueryDeletedRevisions extends ApiQueryRevisionsBase {
 	protected function getExamplesMessages() {
 		return [
 			'action=query&prop=deletedrevisions&titles=Main%20Page|Talk:Main%20Page&' .
-				'drvslots=*&drvprop=user|comment|content'
+				'drvslots=main&drvprop=user|comment|content'
 				=> 'apihelp-query+deletedrevisions-example-titles',
 			'action=query&prop=deletedrevisions&revids=123456'
 				=> 'apihelp-query+deletedrevisions-example-revids',
