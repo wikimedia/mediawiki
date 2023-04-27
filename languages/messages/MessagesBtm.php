75df8fa611ccbee632299e75d2d5bb2f3cceec33
Jon Harald Søby <jhsoby@gmail.com>
2023-04-27 11:43:21 +0200

    Add namespace translations for Mandailing (btm)
    Add namespace translations for Mandailing, provided by
    User:IHLubis at https://incubator.wikimedia.org/wiki/Talk:Wt/btm
    
    Also add separator and date definitions identical to the
    ones for Indonesian (MessagesId.php).
    
    Bug: T335217
    Change-Id: Id43da97bd0b04705e24bda3fb61dd0a7bba0f4c7

--- languages/messages/MessagesBtm.php
+++ languages/messages/MessagesBtm.php
@@ -6,3 +6,43 @@
  */
 
 $fallback = 'id';
+
+$separatorTransformTable = [ ',' => '.', '.' => ',' ];
+
+$namespaceNames = [
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Husus',
+	NS_TALK             => 'Obar',
+	NS_USER             => 'Pamake',
+	NS_USER_TALK        => 'Obar_pamake',
+	NS_PROJECT_TALK     => 'Obar_$1',
+	NS_FILE             => 'Borkas',
+	NS_FILE_TALK        => 'Obar_borkas',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Obar_MediaWiki',
+	NS_TEMPLATE         => 'Templat',
+	NS_TEMPLATE_TALK    => 'Obar_templat',
+	NS_HELP             => 'Tolongi',
+	NS_HELP_TALK        => 'Obar_tolongi',
+	NS_CATEGORY         => 'Kategori',
+	NS_CATEGORY_TALK    => 'Obar_kategori',
+];
+
+$datePreferences = [
+	'default',
+	'dmy',
+	'ymd',
+	'ISO 8601',
+];
+
+$defaultDateFormat = 'dmy';
+
+$dateFormats = [
+	'dmy time' => 'H.i',
+	'dmy date' => 'j F Y',
+	'dmy both' => 'j F Y H.i',
+
+	'ymd time' => 'H.i',
+	'ymd date' => 'Y F j',
+	'ymd both' => 'Y F j H.i',
+];
