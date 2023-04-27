69ed1ae7df2bcc1ccd720d4887c59fd43e083dd4
Timo Tijhof <krinkle@fastmail.com>
2023-04-27 19:37:54 +0100

    jquery.lengthLimit: Adopt QUnit.test.each() and clean up
    * Remove file closure.
    
    * Modernize syntax.
    
    * Adopt QUnit.test.each() to avoid untested code outside QUnit.test()
      calls. This also improves detail in CI/HTML output and ability to
      re-run more precisely via browser and CLI runner.
    
    * Remove needless QUnit.newMwEnvironment() call as this module
      doesn't use mw.config or mw.messages (slow, as per
      https://gerrit.wikimedia.org/r912936/)
    
    Bug: T250045
    Change-Id: Id3c7985a1230fda67784d81943e953ee0f097771

--- tests/qunit/resources/jquery.lengthLimit.test.js
+++ tests/qunit/resources/jquery.lengthLimit.test.js
@@ -1,193 +1,161 @@
-( function () {
-	var simpleSample, U_20AC, poop, mbSample;
-
-	QUnit.module( 'jquery.lengthLimit', QUnit.newMwEnvironment() );
-
+QUnit.module( 'jquery.lengthLimit', function () {
 	// Simple sample (20 chars, 20 bytes)
-	simpleSample = '12345678901234567890';
+	const simpleSample = '12345678901234567890';
 
 	// 3 bytes (euro-symbol)
-	U_20AC = '\u20AC';
+	const U_20AC = '\u20AC';
 
 	// Outside of the BMP (pile of poo emoji)
-	poop = '\uD83D\uDCA9'; // "💩"
+	const poop = '\uD83D\uDCA9'; // "💩"
 
 	// Multi-byte sample (22 chars, 26 bytes)
-	mbSample = '1234567890' + U_20AC + '1234567890' + U_20AC;
+	const mbSample = '1234567890' + U_20AC + '1234567890' + U_20AC;
 
 	// Basic sendkey-implementation
 	function addChars( $input, charstr ) {
-		var c, len;
-
 		function x( $el, i ) {
 			// Add character to the value
 			return $el.val() + charstr.charAt( i );
 		}
 
-		for ( c = 0, len = charstr.length; c < len; c += 1 ) {
+		for ( var c = 0; c < charstr.length; c++ ) {
 			$input
 				.val( x( $input, c ) )
 				.trigger( 'change' );
 		}
 	}
 
-	/**
-	 * Test factory for $.fn.byteLimit
-	 *
-	 * @param {Object} options
-	 * @param {string} options.description Test name
-	 * @param {jQuery} options.$input jQuery object in an input element
-	 * @param {string} options.sample Sequence of characters to simulate being
-	 *  added one by one
-	 * @param {string} options.expected Expected final value of `$input`
-	 */
-	function byteLimitTest( options ) {
-		var opt = $.extend( {
-			description: '',
-			$input: null,
-			sample: '',
+	QUnit.test.each( 'byteLimit()', {
+
+		'Plain text input': {
+			$input: $( '<input>' ).attr( 'type', 'text' ),
+			sample: simpleSample,
+			expected: simpleSample
+		},
+
+		'Plain text input. Calling byteLimit with no parameters and no maxlength attribute (T38310)': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit(),
+			sample: simpleSample,
+			expected: simpleSample
+		},
+
+		'Limit using the maxlength attribute': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.attr( 'maxlength', '10' )
+				.byteLimit(),
+			sample: simpleSample,
+			expected: '1234567890'
+		},
+
+		'Limit using a custom value': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 10 ),
+			sample: simpleSample,
+			expected: '1234567890'
+		},
+
+		'Limit using a custom value, overriding maxlength attribute': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.attr( 'maxlength', '10' )
+				.byteLimit( 15 ),
+			sample: simpleSample,
+			expected: '123456789012345'
+		},
+
+		'Limit using a custom value (multibyte)': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 14 ),
+			sample: mbSample,
+			expected: '1234567890' + U_20AC + '1'
+		},
+
+		'Limit using a custom value (multibyte, outside BMP)': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 3 ),
+			sample: poop,
 			expected: ''
-		}, options );
+		},
 
-		QUnit.test( opt.description, function ( assert ) {
-			opt.$input.appendTo( '#qunit-fixture' );
+		'Limit using a custom value (multibyte) overlapping a byte': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 12 ),
+			sample: mbSample,
+			expected: '123456789012'
+		},
 
-			// Simulate pressing keys for each of the sample characters
-			addChars( opt.$input, opt.sample );
+		'Pass the limit and a callback as input filter': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 6, function ( val ) {
+					var title = mw.Title.newFromText( String( val ) );
+					// Return without namespace prefix
+					return title ? title.getMain() : '';
+				} ),
+			sample: 'User:Sample',
+			expected: 'User:Sample'
+		},
 
-			assert.strictEqual(
-				opt.$input.val(),
-				opt.expected,
-				'New value matches the expected string'
-			);
-		} );
-	}
+		'Limit using the maxlength attribute and pass a callback as input filter': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.attr( 'maxlength', '6' )
+				.byteLimit( function ( val ) {
+					var title = mw.Title.newFromText( String( val ) );
+					// Return without namespace prefix
+					return title ? title.getMain() : '';
+				} ),
+			sample: 'User:Sample',
+			expected: 'User:Sample'
+		},
 
-	byteLimitTest( {
-		description: 'Plain text input',
-		$input: $( '<input>' ).attr( 'type', 'text' ),
-		sample: simpleSample,
-		expected: simpleSample
-	} );
+		'Truncate with exceeded limit and filter callback': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 6, function ( val ) {
+					var title = mw.Title.newFromText( String( val ) );
+					// Return without namespace prefix
+					return title ? title.getMain() : '';
+				} ),
+			sample: 'User:Example',
+			// The callback alters the value to be used to calculeate
+			// the length. The altered value is "Exampl" which has
+			// a length of 6, the "e" would exceed the limit.
+			expected: 'User:Exampl'
+		},
 
-	byteLimitTest( {
-		description: 'Plain text input. Calling byteLimit with no parameters and no maxlength attribute (T38310)',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit(),
-		sample: simpleSample,
-		expected: simpleSample
-	} );
+		'Input filter that increases the length': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 10, function ( text ) {
+					return 'prefix' + text;
+				} ),
+			sample: simpleSample,
+			// Prefix adds 6 characters, limit is reached after 4
+			expected: '1234'
+		},
 
-	byteLimitTest( {
-		description: 'Limit using the maxlength attribute',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.attr( 'maxlength', '10' )
-			.byteLimit(),
-		sample: simpleSample,
-		expected: '1234567890'
-	} );
+		// Regression tests for T43450
+		'Input filter of which the base exceeds the limit': {
+			$input: $( '<input>' ).attr( 'type', 'text' )
+				.byteLimit( 3, function ( text ) {
+					return 'prefix' + text;
+				} ),
+			sample: simpleSample,
+			expected: ''
+		},
+		'Unpaired surrogates do not crash': {
+			$input: $( '<input>' ).attr( 'type', 'text' ).byteLimit( 4 ),
+			sample: '\uD800\uD800\uDFFF',
+			expected: '\uD800'
+		}
+	}, function ( assert, opt ) {
+		opt.$input.appendTo( '#qunit-fixture' );
 
-	byteLimitTest( {
-		description: 'Limit using a custom value',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 10 ),
-		sample: simpleSample,
-		expected: '1234567890'
-	} );
+		// Simulate pressing keys for each of the sample characters
+		addChars( opt.$input, opt.sample );
 
-	byteLimitTest( {
-		description: 'Limit using a custom value, overriding maxlength attribute',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.attr( 'maxlength', '10' )
-			.byteLimit( 15 ),
-		sample: simpleSample,
-		expected: '123456789012345'
-	} );
-
-	byteLimitTest( {
-		description: 'Limit using a custom value (multibyte)',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 14 ),
-		sample: mbSample,
-		expected: '1234567890' + U_20AC + '1'
-	} );
-
-	byteLimitTest( {
-		description: 'Limit using a custom value (multibyte, outside BMP)',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 3 ),
-		sample: poop,
-		expected: ''
-	} );
-
-	byteLimitTest( {
-		description: 'Limit using a custom value (multibyte) overlapping a byte',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 12 ),
-		sample: mbSample,
-		expected: '123456789012'
-	} );
-
-	byteLimitTest( {
-		description: 'Pass the limit and a callback as input filter',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 6, function ( val ) {
-				var title = mw.Title.newFromText( String( val ) );
-				// Return without namespace prefix
-				return title ? title.getMain() : '';
-			} ),
-		sample: 'User:Sample',
-		expected: 'User:Sample'
-	} );
-
-	byteLimitTest( {
-		description: 'Limit using the maxlength attribute and pass a callback as input filter',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.attr( 'maxlength', '6' )
-			.byteLimit( function ( val ) {
-				var title = mw.Title.newFromText( String( val ) );
-				// Return without namespace prefix
-				return title ? title.getMain() : '';
-			} ),
-		sample: 'User:Sample',
-		expected: 'User:Sample'
-	} );
-
-	byteLimitTest( {
-		description: 'Pass the limit and a callback as input filter',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 6, function ( val ) {
-				var title = mw.Title.newFromText( String( val ) );
-				// Return without namespace prefix
-				return title ? title.getMain() : '';
-			} ),
-		sample: 'User:Example',
-		// The callback alters the value to be used to calculeate
-		// the length. The altered value is "Exampl" which has
-		// a length of 6, the "e" would exceed the limit.
-		expected: 'User:Exampl'
-	} );
-
-	byteLimitTest( {
-		description: 'Input filter that increases the length',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 10, function ( text ) {
-				return 'prefix' + text;
-			} ),
-		sample: simpleSample,
-		// Prefix adds 6 characters, limit is reached after 4
-		expected: '1234'
-	} );
-
-	// Regression tests for T43450
-	byteLimitTest( {
-		description: 'Input filter of which the base exceeds the limit',
-		$input: $( '<input>' ).attr( 'type', 'text' )
-			.byteLimit( 3, function ( text ) {
-				return 'prefix' + text;
-			} ),
-		sample: simpleSample,
-		expected: ''
+		assert.strictEqual(
+			opt.$input.val(),
+			opt.expected,
+			'New value matches the expected string'
+		);
 	} );
 
 	QUnit.test( 'Confirm properties and attributes set', function ( assert ) {
@@ -275,12 +243,4 @@
 
 		assert.strictEqual( $el.val(), expected, 'Pasted emoji correctly trimmed at the end' );
 	} );
-
-	byteLimitTest( {
-		description: 'Unpaired surrogates do not crash',
-		$input: $( '<input>' ).attr( 'type', 'text' ).byteLimit( 4 ),
-		sample: '\uD800\uD800\uDFFF',
-		expected: '\uD800'
-	} );
-
-}() );
+} );
