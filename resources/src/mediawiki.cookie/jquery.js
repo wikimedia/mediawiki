5b5ae6384e7b616790fa41f9b524013b4fd1a9da
Timo Tijhof <krinkle@fastmail.com>
2023-05-01 22:36:15 +0100

    mediawiki.cookie: Adds docs for internal jar and $.cookie
    It's quite a quirky method to use as the basis for mw.cookie, but
    it is was it is right now. Start documenting how it actually works.
    
    Change-Id: Ia79901627fb2fd6594865cb84a0956feba7abd06

--- resources/src/mediawiki.cookie/jquery.js
+++ resources/src/mediawiki.cookie/jquery.js
@@ -1,4 +1,24 @@
+/**
+ * To use, load the `mediawiki.cookie` module.
+ *
+ * @class jQuery.plugin.cookie
+ */
+
 var jar = require( './jar.js' );
-// Export jQuery methods
+
+/**
+ * @method cookie
+ * @inheritdoc mw.cookie.jar#cookie
+ */
 $.cookie = jar.cookie;
+
+/**
+ * @method removeCookie
+ * @inheritdoc mw.cookie.jar#removeCookie
+ */
 $.removeCookie = jar.removeCookie;
+
+/**
+ * @class jQuery
+ * @mixins jQuery.plugin.cookie
+ */
