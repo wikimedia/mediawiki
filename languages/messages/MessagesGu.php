e99d4a99238f580f8259cbadf65874a2c7eea778
Kartik Mistry <kartik.mistry@gmail.com>
2023-05-01 17:36:16 +0530

    Fix typo in Special:WhatLinksHere (Gujarati)
    Change-Id: Id79533edaeb51269dacd51d00b3a28a84418d52e

--- languages/messages/MessagesGu.php
+++ languages/messages/MessagesGu.php
@@ -105,7 +105,7 @@ $specialPageAliases = [
 	'Wantedpages'               => [ 'જોઇતા_પાનાં', 'ત્રુટક_કડી' ],
 	'Wantedtemplates'           => [ 'જોઇતા_ઢાંચા' ],
 	'Watchlist'                 => [ 'ધ્યાનસૂચિ' ],
-	'Whatlinkshere'             => [ 'અહિં_શું_જોડાય_છે?' ],
+	'Whatlinkshere'             => [ 'અહીં_શું_જોડાય_છે?', 'અહિં_શું_જોડાય_છે?' ],
 	'Withoutinterwiki'          => [ 'આંતરવિકિવિહીન' ],
 ];
 
