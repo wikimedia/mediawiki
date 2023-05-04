e9bc754e6c3a85f831fe92399aa7525c2df3e36a
Jon Harald Søby <jhsoby@gmail.com>
2023-05-04 19:34:35 +0200

    Add MessagesGpe.php for Ghanaian Pidgin
    Add MessagesGpe for Ghanaian Pidgin. All namespace names
    are the same as in English, so they have been left out, but with
    a comment explaining their absence in the file.
    
    Bug: T335970
    Change-Id: I0f0f6cb41c878a44a8efc17d8ab6fc9195238d44

---
+++ languages/messages/MessagesGpe.php
@@ -0,0 +1,17 @@
+<?php
+/** Ghanaian Pidgin (Ghanaian Pidgin)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Jon Harald Søby
+ */
+
+$fallback = 'en';
+
+/* Ghanaian Pidgin is an English-based creole language, and
+   according to the contributors, the namespaces should be
+   the same as in English. Thus there is no need for a
+   $namespaceNames entry here */
+
+$linkTrail = '/^([a-z]+)(.*)$/sD';
