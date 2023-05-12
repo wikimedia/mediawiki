0a1179dd17df46ea9d200f7fc2da50eec4a9ed64
Lucas Werkmeister <mail@lucaswerkmeister.de>
2023-05-12 19:12:00 +0200

    Add Authorization to default $wgAllowedCorsHeaders
    This is necessary for client-side OAuth 2.0 applications.
    
    Bug: T322944
    Change-Id: Ifae42f2dd6c5139b26e2fa3c96fef9505603859e
    (cherry picked from commit 608da7026deae31181f9cf30b418b7d5411dbbd9)

--- includes/MainConfigSchema.php
+++ includes/MainConfigSchema.php
@@ -11965,6 +11965,8 @@ class MainConfigSchema {
 			'Api-User-Agent',
 			/* Allowing caching preflight requests, see T269636 */
 			'Access-Control-Max-Age',
+			/* OAuth 2.0, see T322944 */
+			'Authorization',
 		],
 		'type' => 'list',
 	];
