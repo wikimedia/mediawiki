289466b38048a038ddfbf5a555c73fdb55b13554
Jon Robson <jdlrobson@gmail.com>
2023-05-12 10:40:55 -0700

    VFormHTMLForm: Remove dead code
    VFormHTMLForm is only ever used when the display format "vform" is
    used in HTMLForm and that only happens in
    LoginSignupSpecialPage.php
    
    The LoginSignupSpecialPage calls suppressDefaultSubmit
    and never makes use of buttons or a reset or cancel button
    which means this code never gets invoked, and always returns
    empty string so lets remove it
    
    Bug: T182050
    Change-Id: I9d7989de71c3e77c636da89a6330c3f4173397af

--- includes/htmlform/VFormHTMLForm.php
+++ includes/htmlform/VFormHTMLForm.php
@@ -22,7 +22,6 @@
  */
 
 use MediaWiki\Html\Html;
-use MediaWiki\Linker\Linker;
 
 /**
  * Compact stacked vertical format for forms.
@@ -74,88 +73,5 @@ class VFormHTMLForm extends HTMLForm {
 	public function wrapForm( $html ) {
 		// Always discard $this->mWrapperLegend
 		return Html::rawElement( 'form', $this->getFormAttributes(), $html );
-	}
-
-	public function getButtons() {
-		$buttons = '';
-
-		if ( $this->mShowSubmit ) {
-			$attribs = [];
-
-			if ( isset( $this->mSubmitID ) ) {
-				$attribs['id'] = $this->mSubmitID;
-			}
-
-			if ( isset( $this->mSubmitName ) ) {
-				$attribs['name'] = $this->mSubmitName;
-			}
-
-			if ( isset( $this->mSubmitTooltip ) ) {
-				$attribs += Linker::tooltipAndAccesskeyAttribs( $this->mSubmitTooltip );
-			}
-
-			$attribs['class'] = [
-				'mw-htmlform-submit',
-				'mw-ui-button mw-ui-big mw-ui-block',
-			];
-			foreach ( $this->mSubmitFlags as $flag ) {
-				$attribs['class'][] = 'mw-ui-' . $flag;
-			}
-
-			$buttons .= Xml::submitButton( $this->getSubmitText(), $attribs ) . "\n";
-		}
-
-		if ( $this->mShowReset ) {
-			$buttons .= Html::element(
-				'input',
-				[
-					'type' => 'reset',
-					'value' => $this->msg( 'htmlform-reset' )->text(),
-					'class' => 'mw-ui-button mw-ui-big mw-ui-block',
-				]
-			) . "\n";
-		}
-
-		if ( $this->mShowCancel ) {
-			$target = $this->getCancelTargetURL();
-			$buttons .= Html::element(
-					'a',
-					[
-						'class' => 'mw-ui-button mw-ui-big mw-ui-block',
-						'href' => $target,
-					],
-					$this->msg( 'cancel' )->text()
-				) . "\n";
-		}
-
-		foreach ( $this->mButtons as $button ) {
-			$attrs = [
-				'type' => 'submit',
-				'name' => $button['name'],
-				'value' => $button['value']
-			];
-
-			// @phan-suppress-next-line PhanTypePossiblyInvalidDimOffset Always set in HTMLForm::addButton
-			if ( $button['attribs'] ) {
-				// @phan-suppress-next-line PhanTypePossiblyInvalidDimOffset Always set in HTMLForm::addButton
-				$attrs += $button['attribs'];
-			}
-
-			if ( isset( $button['id'] ) ) {
-				$attrs['id'] = $button['id'];
-			}
-
-			$attrs['class'] = isset( $attrs['class'] ) ? (array)$attrs['class'] : [];
-			$attrs['class'][] = 'mw-ui-button mw-ui-big mw-ui-block';
-
-			$buttons .= Html::element( 'input', $attrs ) . "\n";
-		}
-
-		if ( !$buttons ) {
-			return '';
-		}
-
-		return Html::rawElement( 'div',
-			[ 'class' => 'mw-htmlform-submit-buttons' ], "\n$buttons" ) . "\n";
 	}
 }
