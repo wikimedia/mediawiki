8d622a9d3be738ad8add00fefd74c7a4acc9ebd4
Umherirrender <umherirrender_de.wp@web.de>
2023-05-19 22:14:03 +0200

    tests: Add RL\HookRunner to HookRunnerTest
    Change-Id: I7d1990f2b1ad846e1a99020f69aa186dfa4e7e74

--- tests/phpunit/unit/includes/HookContainer/HookRunnerTest.php
+++ tests/phpunit/unit/includes/HookContainer/HookRunnerTest.php
@@ -4,11 +4,13 @@ namespace MediaWiki\Tests\HookContainer;
 
 use MediaWiki\Api\ApiHookRunner;
 use MediaWiki\HookContainer\HookRunner;
+use MediaWiki\ResourceLoader as RL;
 
 /**
  * Tests that all arguments passed into HookRunner are passed along to HookContainer.
  * @covers \MediaWiki\HookContainer\HookRunner
  * @covers \MediaWiki\Api\ApiHookRunner
+ * @covers \MediaWiki\ResourceLoader\HookRunner
  * @package MediaWiki\Tests\HookContainer
  */
 class HookRunnerTest extends HookRunnerTestBase {
@@ -16,5 +18,6 @@ class HookRunnerTest extends HookRunnerTestBase {
 	public static function provideHookRunners() {
 		yield ApiHookRunner::class => [ ApiHookRunner::class ];
 		yield HookRunner::class => [ HookRunner::class ];
+		yield RL\HookRunner::class => [ RL\HookRunner::class ];
 	}
 }
