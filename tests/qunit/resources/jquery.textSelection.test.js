0fc9c163f2e81bd041115010f2109c3de6dac78f
Timo Tijhof <krinkle@fastmail.com>
2023-06-04 12:03:57 +0100

    jquery.textSelection: mediawiki.Uri: Modernize test syntax, adopt test.each()
    * Remove unused default values for opt, opt.before, and opt.after.
      The only value that had an effect was `selected: null` because the
      test modified the default from undefined to null, and checked its
      own modification in a conditional branch. The indirection of null
      is not helping here and is not needed for the source code either.
      Change the check to undefined directly.
    
    * Remove redundant file closure.
    
    * Remove needless QUnit.newMwEnvironment() calls as the tested
      methods don't alter mw.config or mw.messages. Slow, as per
      https://gerrit.wikimedia.org/r/c/mediawiki/core/+/912936/
    
    * Adopt QUnit.test.each() to improve static analysis and to reduce
      the amount of custom test-logic (which was complex enough that it
      probably was worth having a test for the test...). By using
      test.each() we gain confidence in the test suite as any error now
      would naturally lead to a failing test instead of some quiet error
      in our custom test logic.
    
    * Remove redundant `among()` function, use assert.strictEqual directly.
      The custom `among` function existed for a dynamic array test case,
      but that test case has been disabled for over ten years (2012).
    
    * Speaking of the disabled test, I've re-enabled it without the variable
      "one of these two expectations" logic. That logic exited because we
      wanted the test to pass with different expectations, one way for modern
      browsers and another way for IE6. We no longer support IE6, so
      I've re-enabled the test and simplified it.
    
    Change-Id: Ic97d449793d528be5a8ef95c142bb8949eb903bd

--- tests/qunit/resources/jquery.textSelection.test.js
+++ tests/qunit/resources/jquery.textSelection.test.js
@@ -1,266 +1,181 @@
-( function () {
-	var caretSample,
-		sig = {
-			pre: '--~~~~'
+QUnit.module( 'jquery.textSelection', function () {
+	const sig = {
+		pre: '--~~~~'
+	};
+	const bold = {
+		pre: '\'\'\'',
+		peri: 'Bold text',
+		post: '\'\'\''
+	};
+	const h2 = {
+		pre: '== ',
+		peri: 'Heading 2',
+		post: ' ==',
+		regex: /^(\s*)(={1,6})(.*?)\2(\s*)$/,
+		regexReplace: '$1==$3==$4',
+		ownline: true
+	};
+	const ulist = {
+		pre: '* ',
+		peri: 'Bulleted list item',
+		post: '',
+		ownline: true,
+		splitlines: true
+	};
+
+	QUnit.test.each( 'encapsulateText', {
+		'Adding sig to end of text': {
+			before: {
+				text: 'Wikilove dude! ',
+				start: 15,
+				end: 15
+			},
+			after: {
+				text: 'Wikilove dude! --~~~~',
+				selected: ''
+			},
+			replace: sig
 		},
-		bold = {
-			pre: '\'\'\'',
-			peri: 'Bold text',
-			post: '\'\'\''
+		'Adding bold to empty': {
+			before: {
+				text: '',
+				start: 0,
+				end: 0
+			},
+			after: {
+				text: '\'\'\'Bold text\'\'\'',
+				selected: 'Bold text' // selected because it's the default
+			},
+			replace: bold
 		},
-		h2 = {
-			pre: '== ',
-			peri: 'Heading 2',
-			post: ' ==',
-			regex: /^(\s*)(={1,6})(.*?)\2(\s*)$/,
-			regexReplace: '$1==$3==$4',
-			ownline: true
+		'Adding bold to existing text': {
+			before: {
+				text: 'Now is the time for all good men to come to the aid of their country',
+				start: 20,
+				end: 32
+			},
+			after: {
+				text: 'Now is the time for \'\'\'all good men\'\'\' to come to the aid of their country',
+				selected: '' // empty because it's not the default'
+			},
+			replace: bold
 		},
-		ulist = {
-			pre: '* ',
-			peri: 'Bulleted list item',
-			post: '',
-			ownline: true,
-			splitlines: true
-		};
-
-	QUnit.module( 'jquery.textSelection', QUnit.newMwEnvironment() );
-
-	/**
-	 * Test factory for $.fn.textSelection( 'encapsulateText' )
-	 *
-	 * @param {Object} options Associative configuration array
-	 * @param {string} options.description Description
-	 * @param {string} options.input Input
-	 * @param {string} options.output Output
-	 * @param {number} options.start Starting char for selection
-	 * @param {number} options.end Ending char for selection
-	 * @param {Object} options.params Additional parameters for $().textSelection( 'encapsulateText' )
-	 */
-	function encapsulateTest( options ) {
-		var opt = $.extend( {
-			description: '',
-			before: {},
-			after: {},
-			replace: {}
-		}, options );
-
-		opt.before = $.extend( {
-			text: '',
-			start: 0,
-			end: 0
-		}, opt.before );
-		opt.after = $.extend( {
-			text: '',
-			selected: null
-		}, opt.after );
-
-		QUnit.test( opt.description, function ( assert ) {
-			var $textarea, start, end, opts, text, selected;
-
-			$textarea = $( '<textarea>' );
-
-			$( '#qunit-fixture' ).append( $textarea );
-
-			$textarea.textSelection( 'setContents', opt.before.text );
-
-			start = opt.before.start;
-			end = opt.before.end;
-
-			// Clone opt.replace
-			opts = $.extend( {}, opt.replace );
-			opts.selectionStart = start;
-			opts.selectionEnd = end;
-			$textarea.textSelection( 'encapsulateSelection', opts );
-
-			text = $textarea.textSelection( 'getContents' ).replace( /\r\n/g, '\n' );
-
-			assert.strictEqual( text, opt.after.text, 'Checking full text after encapsulation' );
-
-			if ( opt.after.selected !== null ) {
-				selected = $textarea.textSelection( 'getSelection' );
-				assert.strictEqual( selected, opt.after.selected, 'Checking selected text after encapsulation.' );
-			}
-
-		} );
-	}
-
-	encapsulateTest( {
-		description: 'Adding sig to end of text',
-		before: {
-			text: 'Wikilove dude! ',
-			start: 15,
-			end: 15
+		'ownline option: adding new h2': {
+			before: {
+				text: 'Before\nAfter',
+				start: 7,
+				end: 7
+			},
+			after: {
+				text: 'Before\n== Heading 2 ==\nAfter',
+				selected: 'Heading 2'
+			},
+			replace: h2
 		},
-		after: {
-			text: 'Wikilove dude! --~~~~',
-			selected: ''
+		'ownline option: turn a whole line into new h2': {
+			before: {
+				text: 'Before\nMy heading\nAfter',
+				start: 7,
+				end: 17
+			},
+			after: {
+				text: 'Before\n== My heading ==\nAfter',
+				selected: ''
+			},
+			replace: h2
 		},
-		replace: sig
+		'ownline option: turn a partial line into new h2': {
+			before: {
+				text: 'BeforeMy headingAfter',
+				start: 6,
+				end: 16
+			},
+			after: {
+				text: 'Before\n== My heading ==\nAfter',
+				selected: ''
+			},
+			replace: h2
+		},
+		'splitlines option: no selection, insert new list item': {
+			before: {
+				text: 'Before\nAfter',
+				start: 7,
+				end: 7
+			},
+			after: {
+				text: 'Before\n* Bulleted list item\nAfter'
+			},
+			replace: ulist
+		},
+		'splitlines option: single partial line selection, insert new list item': {
+			before: {
+				text: 'BeforeMy List ItemAfter',
+				start: 6,
+				end: 18
+			},
+			after: {
+				text: 'Before\n* My List Item\nAfter'
+			},
+			replace: ulist
+		},
+		'splitlines option: multiple lines': {
+			before: {
+				text: 'Before\nFirst\nSecond\nThird\nAfter',
+				start: 7,
+				end: 25
+			},
+			after: {
+				text: 'Before\n* First\n* Second\n* Third\nAfter'
+			},
+			replace: ulist
+		}
+	}, function ( assert, opt ) {
+		var $textarea = $( '<textarea>' ).appendTo( '#qunit-fixture' );
+		$textarea.textSelection( 'setContents', opt.before.text );
+		var replace = Object.assign( {
+			selectionStart: opt.before.start,
+			selectionEnd: opt.before.end
+		}, opt.replace );
+		$textarea.textSelection( 'encapsulateSelection', replace );
+
+		var text = $textarea.textSelection( 'getContents' ).replace( /\r\n/g, '\n' );
+		assert.strictEqual( text, opt.after.text, 'after encapsulation' );
+
+		if ( opt.after.selected !== undefined ) {
+			var selected = $textarea.textSelection( 'getSelection' );
+			assert.strictEqual( selected, opt.after.selected, 'selected text' );
+		}
 	} );
 
-	encapsulateTest( {
-		description: 'Adding bold to empty',
-		before: {
-			text: '',
-			start: 0,
-			end: 0
-		},
-		after: {
-			text: '\'\'\'Bold text\'\'\'',
-			selected: 'Bold text' // selected because it's the default
-		},
-		replace: bold
+	const caretSample = 'Some big text that we like to work with. Nothing fancy... you know what I mean?';
+
+	// Default/empty selection, T36820, T33847
+	QUnit.test( 'getCaretPosition [initial]', function ( assert ) {
+		const $textarea = $( '<textarea>' ).text( caretSample ).appendTo( '#qunit-fixture' );
+		const pos = $textarea.textSelection( 'getCaretPosition', { startAndEnd: true } );
+		assert.strictEqual( pos[ 0 ], 0, 'default start' );
+		assert.strictEqual( pos[ 1 ], 0, 'default end' );
 	} );
 
-	encapsulateTest( {
-		description: 'Adding bold to existing text',
-		before: {
-			text: 'Now is the time for all good men to come to the aid of their country',
-			start: 20,
-			end: 32
-		},
-		after: {
-			text: 'Now is the time for \'\'\'all good men\'\'\' to come to the aid of their country',
-			selected: '' // empty because it's not the default'
-		},
-		replace: bold
-	} );
-
-	encapsulateTest( {
-		description: 'ownline option: adding new h2',
-		before: {
-			text: 'Before\nAfter',
+	QUnit.test.each( 'getCaretPosition', {
+		'forced empty selection': {
 			start: 7,
 			end: 7
 		},
-		after: {
-			text: 'Before\n== Heading 2 ==\nAfter',
-			selected: 'Heading 2'
-		},
-		replace: h2
-	} );
-
-	encapsulateTest( {
-		description: 'ownline option: turn a whole line into new h2',
-		before: {
-			text: 'Before\nMy heading\nAfter',
-			start: 7,
-			end: 17
-		},
-		after: {
-			text: 'Before\n== My heading ==\nAfter',
-			selected: ''
-		},
-		replace: h2
-	} );
-
-	encapsulateTest( {
-		description: 'ownline option: turn a partial line into new h2',
-		before: {
-			text: 'BeforeMy headingAfter',
+		'small selection': {
 			start: 6,
-			end: 16
-		},
-		after: {
-			text: 'Before\n== My heading ==\nAfter',
-			selected: ''
-		},
-		replace: h2
-	} );
+			end: 11
+		}
+	}, function ( assert, options ) {
+		const $textarea = $( '<textarea>' ).text( caretSample ).appendTo( '#qunit-fixture' );
 
-	encapsulateTest( {
-		description: 'splitlines option: no selection, insert new list item',
-		before: {
-			text: 'Before\nAfter',
-			start: 7,
-			end: 7
-		},
-		after: {
-			text: 'Before\n* Bulleted list item\nAfter'
-		},
-		replace: ulist
-	} );
-
-	encapsulateTest( {
-		description: 'splitlines option: single partial line selection, insert new list item',
-		before: {
-			text: 'BeforeMy List ItemAfter',
-			start: 6,
-			end: 18
-		},
-		after: {
-			text: 'Before\n* My List Item\nAfter'
-		},
-		replace: ulist
-	} );
-
-	encapsulateTest( {
-		description: 'splitlines option: multiple lines',
-		before: {
-			text: 'Before\nFirst\nSecond\nThird\nAfter',
-			start: 7,
-			end: 25
-		},
-		after: {
-			text: 'Before\n* First\n* Second\n* Third\nAfter'
-		},
-		replace: ulist
-	} );
-
-	function caretTest( options ) {
-		QUnit.test( options.description, function ( assert ) {
-			var pos,
-				$textarea = $( '<textarea>' ).text( options.text );
-
-			$( '#qunit-fixture' ).append( $textarea );
-
-			if ( options.mode === 'set' ) {
-				$textarea.textSelection( 'setSelection', {
-					start: options.start,
-					end: options.end
-				} );
-			}
-
-			function among( actual, expected, message ) {
-				if ( Array.isArray( expected ) ) {
-					assert.true( expected.includes( actual ), message + ' (got ' + actual + '; expected one of ' + expected.join( ', ' ) + ')' );
-				} else {
-					assert.strictEqual( actual, expected, message );
-				}
-			}
-
-			pos = $textarea.textSelection( 'getCaretPosition', { startAndEnd: true } );
-			among( pos[ 0 ], options.start, 'Caret start should be where we set it.' );
-			among( pos[ 1 ], options.end, 'Caret end should be where we set it.' );
+		$textarea.textSelection( 'setSelection', {
+			start: options.start,
+			end: options.end
 		} );
-	}
 
-	caretSample = 'Some big text that we like to work with. Nothing fancy... you know what I mean?';
-
-	/* @broken: Disabled per T36820
-	caretTest({
-		description: 'getCaretPosition with original/empty selection - T33847 with IE 6/7/8',
-		text: caretSample,
-		start: [0, caretSample.length], // Opera and Firefox (prior to FF 6.0) default caret to the end of the box (caretSample.length)
-		end: [0, caretSample.length], // Other browsers default it to the beginning (0), so check both.
-		mode: 'get'
-	});
-	*/
-
-	caretTest( {
-		description: 'set/getCaretPosition with forced empty selection',
-		text: caretSample,
-		start: 7,
-		end: 7,
-		mode: 'set'
+		const pos = $textarea.textSelection( 'getCaretPosition', { startAndEnd: true } );
+		assert.strictEqual( pos[ 0 ], options.start, 'Caret start should be where we set it.' );
+		assert.strictEqual( pos[ 1 ], options.end, 'Caret end should be where we set it.' );
 	} );
-
-	caretTest( {
-		description: 'set/getCaretPosition with small selection',
-		text: caretSample,
-		start: 6,
-		end: 11,
-		mode: 'set'
-	} );
-}() );
+} );
