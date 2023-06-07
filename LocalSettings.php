09c2b2924515999925c53ad8ccd2bf1ff07dcb10
Chaftiel <124165191+Chaftiel@users.noreply.github.com>
2023-06-06 21:04:52 -0400

    Skin

--- LocalSettings.php
+++ LocalSettings.php
@@ -126,7 +126,7 @@ $wgGroupPermissions['*']['edit'] = false;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "vector-2022";
+## $wgDefaultSkin = "vector-2022";
 
 # End of automatically generated settings.
 # Add more configuration options below.
