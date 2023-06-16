wikimedia/mediawiki

commit 23ad1fd91b8a73e30124a6d55486cb304967580f

Add tests for Special:Log/suppress, fix for PostgreSQL
Also handle unknown "offender" actors more nicely.

Change-Id: Iaf30aae9f14c5a8edc44b89ea67acd6a96909d66

tests/phpunit/includes/specials/SpecialLogTest.php
@@ -5,9 +5,11 @@
  */
 
 use MediaWiki\Request\FauxRequest;
+use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Specials\SpecialLog;
 
 /**
+ * @group Database
  * @covers \MediaWiki\Specials\SpecialLog
  */
 class SpecialLogTest extends SpecialPageTestBase {
@@ -39,6 +41,50 @@ class SpecialLogTest extends SpecialPageTestBase {
 			'qqx'
 		);
 		$this->assertStringContainsString( '(log-summary)', $html );
+	}
+
+	public function testSuppressionLog() {
+		// Have "BadGuy" create a revision
+		$user = ( new TestUser( 'BadGuy' ) )->getUser();
+		$title = $this->insertPage( 'Foo', 'Bar', null, $user )['title'];
+		$revId = $title->getLatestRevID();
+
+		// Hide our revision's comment
+		$list = RevisionDeleter::createList( 'revision', RequestContext::getMain(), $title, [ $revId ] );
+		$status = $list->setVisibility( [
+			'value' => [
+				RevisionRecord::DELETED_RESTRICTED => 1,
+				RevisionRecord::DELETED_COMMENT => 1
+			],
+			'comment' => 'SpecialLogTest'
+		] );
+		$this->assertTrue( $status->isGood() );
+
+		// Allow everyone to read the suppression log
+		$this->mergeMwGlobalArrayValue(
+			'wgGroupPermissions', [
+				'*' => [
+					'suppressionlog' => true
+				]
+			]
+		);
+
+		[ $html, ] = $this->executeSpecialPage(
+			'suppress',
+			new FauxRequest( [ 'offender' => 'BadGuy' ] ),
+			'qqx'
+		);
+		$this->assertStringNotContainsString( '(logempty)', $html );
+		$this->assertStringContainsString( '(logentry-suppress-revision', $html );
+
+		// Suppression log for unknown user should be empty
+		[ $html, ] = $this->executeSpecialPage(
+			'suppress',
+			new FauxRequest( [ 'offender' => 'GoodGuy' ] ),
+			'qqx'
+		);
+		$this->assertStringContainsString( '(logempty)', $html );
+		$this->assertStringNotContainsString( '(logentry-suppress-revision', $html );
 	}
 
 }

includes/specials/SpecialLog.php
@@ -149,7 +149,10 @@ class SpecialLog extends SpecialPage {
 			$offenderName = $opts->getValue( 'offender' );
 			$offenderId = $this->actorNormalization->findActorIdByName( $offenderName, $dbr );
 			if ( $offenderId ) {
-				$qc = [ 'ls_field' => 'target_author_actor', 'ls_value' => $offenderId ];
+				$qc = [ 'ls_field' => 'target_author_actor', 'ls_value' => strval( $offenderId ) ];
+			} else {
+				// Unknown offender, thus results have to be empty
+				$qc = [ '1=0' ];
 			}
 		} else {
 			// Allow extensions to add relations to their search types

commit 8b1eff6f44bc3abe116d993d9289f988965614da

SpecialLogTest: Test getting full suppression log
This was recently broken in 23ad1fd91b8a and
subsequently fixed in dc9a6b29412a.

Change-Id: I282dc5db7f29458ed6879a6a99ddff8bc07bada4

tests/phpunit/includes/specials/SpecialLogTest.php
@@ -77,6 +77,15 @@ class SpecialLogTest extends SpecialPageTestBase {
 		$this->assertStringNotContainsString( '(logempty)', $html );
 		$this->assertStringContainsString( '(logentry-suppress-revision', $html );
 
+		// Full suppression log
+		[ $html, ] = $this->executeSpecialPage(
+			'suppress',
+			new FauxRequest(),
+			'qqx'
+		);
+		$this->assertStringNotContainsString( '(logempty)', $html );
+		$this->assertStringContainsString( '(logentry-suppress-revision', $html );
+
 		// Suppression log for unknown user should be empty
 		[ $html, ] = $this->executeSpecialPage(
 			'suppress',