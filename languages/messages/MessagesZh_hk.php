c02cf77ce1465626f382b47bcbf1ef5554226a02
hamishz <hamish.zou@stu.hunau.edu.cn>
2023-06-25 05:00:26 +0800

    MessagesZh_hk: Add zh-hk localised date format
    Added support for HK en dmy date format which is offcially used in HK
    
    Bug: T339009
    Change-Id: Ide8e24e3942e8d5bc2d76e2a043e45dca9c30774

--- languages/messages/MessagesZh_hk.php
+++ languages/messages/MessagesZh_hk.php
@@ -4,6 +4,7 @@
  * @file
  * @ingroup Languages
  *
+ * @author Hamish
  * @author Horacewai2
  * @author Kayau
  * @author Mark85296341
@@ -24,4 +25,22 @@ $fallback8bitEncoding = 'Big5-HKSCS';
 $specialPageAliases = [
 	'ComparePages'              => [ '頁面比較' ],
 	'Unblock'                   => [ '解除封禁' ],
+];
+
+$datePreferences = [
+	'default',
+	'ISO 8601',
+	'HK dmy',
+];
+
+$defaultDateFormat = 'zh';
+
+$dateFormats = [
+	'zh time' => 'H:i',
+	'zh date' => 'Y年n月j日 (l)',
+	'zh both' => 'Y年n月j日 (D) H:i',
+
+	'HK dmy time' => 'H:i',
+	'HK dmy date' => 'd-m-Y',
+	'HK dmy both' => 'd-m-Y H:i',
 ];
