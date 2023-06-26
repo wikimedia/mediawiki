dc7713184a0213a0540f25e57f8414ba673db988
Reedy <reedy@wikimedia.org>
2023-06-26 23:15:19 +0100

    generateLocalAutoload.php: Fix typo
    Follows up I431ba0c3af83ffe0f1df457c7640b1a187048418
    
    Change-Id: Id44b8d8f0110a0c27fa6af761f318e7bc0d7fd98

--- maintenance/generateLocalAutoload.php
+++ maintenance/generateLocalAutoload.php
@@ -1,5 +1,5 @@
 <?php
 
-// The generateLocalAutoload scrip was renamed to generateAutoload in MW 1.41.
+// The generateLocalAutoload script was renamed to generateAutoload in MW 1.41.
 // This stub exists for backwards compatibility.
 require_once __DIR__ . '/generateAutoload.php';
