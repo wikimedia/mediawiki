aac0c8007ae3fae3a8b1d3306dd884609cd030eb
Winston Sung <winston21.sung@gmail.com>
2023-07-09 18:29:36 +0000

    ZhConverter: Fix language variant fallback chain
    Fix the fallback chain for the language converter for zh by:
    
    * Changed fallback order into
      * zh-hk' <=> 'zh-mo' first, 'zh-hant' second
      * 'zh-sg' <=> 'zh-my' first, 'zh-hans' second
    * Added 'zh' to all variants (except 'zh') to reduce
      "converter-manual-rule-error" messages
      ('Error detected in manual language conversion rule').
    
    Bug: T352554
    Change-Id: Ia006c0cb00bcc809f32267b1c1feca773daadb3b

--- includes/language/converters/ZhConverter.php
+++ includes/language/converters/ZhConverter.php
@@ -55,14 +55,14 @@ class ZhConverter extends LanguageConverter {
 				'zh-mo',
 				'zh-my'
 			],
-			'zh-hans' => [ 'zh-cn', 'zh-sg', 'zh-my' ],
-			'zh-hant' => [ 'zh-tw', 'zh-hk', 'zh-mo' ],
-			'zh-cn' => [ 'zh-hans', 'zh-sg', 'zh-my' ],
-			'zh-sg' => [ 'zh-hans', 'zh-cn', 'zh-my' ],
-			'zh-my' => [ 'zh-hans', 'zh-sg', 'zh-cn' ],
-			'zh-tw' => [ 'zh-hant', 'zh-hk', 'zh-mo' ],
-			'zh-hk' => [ 'zh-hant', 'zh-mo', 'zh-tw' ],
-			'zh-mo' => [ 'zh-hant', 'zh-hk', 'zh-tw' ],
+			'zh-hans' => [ 'zh-cn', 'zh-sg', 'zh-my', 'zh' ],
+			'zh-hant' => [ 'zh-tw', 'zh-hk', 'zh-mo', 'zh' ],
+			'zh-cn' => [ 'zh-hans', 'zh-sg', 'zh-my', 'zh' ],
+			'zh-sg' => [ 'zh-my', 'zh-hans', 'zh-cn', 'zh' ],
+			'zh-my' => [ 'zh-sg', 'zh-hans', 'zh-cn', 'zh' ],
+			'zh-tw' => [ 'zh-hant', 'zh-hk', 'zh-mo', 'zh' ],
+			'zh-hk' => [ 'zh-mo', 'zh-hant', 'zh-tw', 'zh' ],
+			'zh-mo' => [ 'zh-hk', 'zh-hant', 'zh-tw', 'zh' ],
 		];
 	}
 
