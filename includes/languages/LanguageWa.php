f88bce2ef55be27e3d65fc97b18fe512318ac788
Bartosz Dziewoński <matma.rex@gmail.com>
2023-08-08 13:22:34 +0200

    LanguageWa: Fix double timezone adjustment
    A call to userAdjust() will happen when calling parent::timeanddate(),
    $this->date() or $this->time() below. Calling it here meant that we
    tried to adjust it twice.
    
    Bug: T343797
    Change-Id: I7dc4fde56edaaa6c476418cbf65207bb860b6ee8
    (cherry picked from commit e5f2c365a570a26ed53e1ac5fe0265a0093905c2)

--- includes/languages/LanguageWa.php
+++ includes/languages/LanguageWa.php
@@ -87,9 +87,6 @@ class LanguageWa extends Language {
 	 * @return string
 	 */
 	public function timeanddate( $ts, $adj = false, $format = true, $tc = false ) {
-		if ( $adj ) {
-			$ts = $this->userAdjust( $ts, $tc );
-		}
 		$datePreference = $this->dateFormat( $format );
 		if ( $datePreference == 'ISO 8601' ) {
 			return parent::timeanddate( $ts, $adj, $format, $tc );
