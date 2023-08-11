809a6e7164dc24b14744a7f94f1f4012ace5162b
Daimona Eaytoy <daimona.wiki@gmail.com>
2023-08-11 22:13:03 +0200

    Make isTestInDatabaseGroup static and case-sensitive
    Subclasses should use needsDB instead of this method, so make it private.
    
    Also remove the case-insensitive modifiers from the regex, because
    PHPUnit group *are* case-sensitive.
    
    And finally, add a word boundary to avoid catching groups whose name
    starts with "Database".
    
    Depends-On: I2f2a091fcb8ae60d969b7144f8d556ab20eef463
    Change-Id: Ib92244953072eeab255e47d03bf93f4806d6caa1

--- tests/phpunit/MediaWikiGroupValidator.php
+++ tests/phpunit/MediaWikiGroupValidator.php
@@ -28,10 +28,10 @@ trait MediaWikiGroupValidator {
 	 * @return bool
 	 * @since 1.34
 	 */
-	protected static function isTestInDatabaseGroup() {
+	private static function isTestInDatabaseGroup() {
 		// If the test class says it belongs to the Database group, it needs the database.
 		// NOTE: This ONLY checks for the group in the class level doc comment.
 		$rc = new ReflectionClass( static::class );
-		return (bool)preg_match( '/@group +Database/im', $rc->getDocComment() );
+		return (bool)preg_match( '/@group +Database\b/m', $rc->getDocComment() );
 	}
 }
