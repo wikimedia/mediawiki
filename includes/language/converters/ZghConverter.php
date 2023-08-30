1df54ce20e28271778eabae7883879d228fb3625
Jon Harald Søby <jhsoby@gmail.com>
2023-08-30 11:21:23 +0200

    Implement language converter for Standard Moroccan Amazigh (zgh)
    Requested by the zgh community in the Wikimedia Incubator at [1];
    the converter is the same as for Shilha (`shi`); the main difference
    between the two languages is that the main script for `zgh` is
    Tifinagh, which can't be losslessly converted to the Latin script
    (the former is unicameral, the latter is bicameral), so the
    converter and configuration added here is essentially one-directional.
    
    [1] https://incubator.wikimedia.org/wiki/Special:PermaLink/6060805
    
    Change-Id: I483a1594f001226439497f0870176e9a1e447458

---
+++ includes/language/converters/ZghConverter.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+/**
+ * Standard Moroccan Amazigh specific code.
+ *
+ * Conversion script for Tifinagh to lowercase Latin for Standard Moroccan Amazigh.
+ *
+ *
+ * Based on:
+ *   - LanguageShi.php
+ *   - https://fr.wikipedia.org/wiki/Tifinagh
+ *
+ * @ingroup Languages
+ */
+class ZghConverter extends LanguageConverterSpecific {
+	// The Tifinagh alphabet sequence is based on
+	// "Dictionnaire Général de la Langue Amazighe Informatisé"
+	// by IRCAM (https://tal.ircam.ma/dglai/lexieam.php, DGLAi),
+	// with the labio-velarization mark in the end
+	public $mToLatin = [
+		'ⴰ' => 'a',
+		'ⴱ' => 'b',
+		'ⴳ' => 'g',
+		'ⴷ' => 'd',
+		'ⴹ' => 'ḍ',
+		'ⴻ' => 'e',
+		'ⴼ' => 'f',
+		'ⴽ' => 'k',
+		'ⵀ' => 'h',
+		'ⵃ' => 'ḥ',
+		'ⵄ' => 'ɛ',
+		'ⵅ' => 'x',
+		'ⵇ' => 'q',
+		'ⵉ' => 'i',
+		'ⵊ' => 'j',
+		'ⵍ' => 'l',
+		'ⵎ' => 'm',
+		'ⵏ' => 'n',
+		'ⵓ' => 'u',
+		'ⵔ' => 'r',
+		'ⵕ' => 'ṛ',
+		'ⵖ' => 'ɣ',
+		'ⵙ' => 's',
+		'ⵚ' => 'ṣ',
+		'ⵛ' => 'c',
+		'ⵜ' => 't',
+		'ⵟ' => 'ṭ',
+		'ⵡ' => 'w',
+		'ⵢ' => 'y',
+		'ⵣ' => 'z',
+		'ⵥ' => 'ẓ',
+		'ⵯ' => 'ʷ',
+	];
+
+	public function getMainCode(): string {
+		return 'zgh';
+	}
+
+	public function getLanguageVariants(): array {
+		return [ 'zgh', 'zgh-latn' ];
+	}
+
+	public function getVariantsFallbacks(): array {
+		return [];
+	}
+
+	protected function loadDefaultTables(): array {
+		return [
+			'zgh-latn' => new ReplacementArray( $this->mToLatin ),
+			'zgh' => new ReplacementArray()
+		];
+	}
+
+	public function translate( $text, $toVariant ) {
+		// We only convert zgh (zgh-Tfng) to zgh-Latn, not the
+		// other way around. We also don't need to try to
+		// convert if there is no text.
+		if ( $toVariant === 'zgh' || !trim( $text ) ) {
+			return $text;
+		}
+
+		$this->loadTables();
+		$text = $this->mTables[$toVariant]->replace( $text );
+		return $text;
+	}
+}
