248f6294f9311c043941e8d16e8523b19de92f66
Kosta Harlan <kharlan@wikimedia.org>
2023-08-30 10:54:18 +0200

    rollback: Fire postEdit JavaScript hook on success
    Why:
    
    - Front-end code should be able to respond to actions that result in
      edits
    
    What:
    
    - Fire core's postEdit hook after a successful rollback action
    - Add the relevant code to the existing mediawiki.misc-authed-curate
      ResourceLoader module to avoid adding another RL Module
    - Output the mediawiki.misc-authed-curate module for successful
      scenarios in RollbackAction.php
    - Fire the postEdit hook without any messages. It's possible that a
      toast message makes sense, but that can be added later, if someone has
      a need for it; perhaps in the context of T88044
    
    Bug: T345229
    Change-Id: Icb33535d8d5ba3a386459befacab11971ae44f49

--- resources/src/mediawiki.misc-authed-curate/rollback.js
+++ resources/src/mediawiki.misc-authed-curate/rollback.js
@@ -1,7 +1,24 @@
 /*!
- * JavaScript for rollback confirmation prompt
+ * JavaScript for:
+ * - rollback confirmation prompt
+ * - firing the postEdit hook upon a successful rollback edit
  */
 ( function () {
+	// If a rollback was successful, RollbackAction.php will export wgRollbackSuccess
+	var wgRollbackSuccess = mw.config.get( 'wgRollbackSuccess' );
+	if ( wgRollbackSuccess ) {
+		mw.loader.using( 'mediawiki.action.view.postEdit', function () {
+			// wgCurRevisionId is set to the revision of the rollback
+			// in OutputPage::getJSVars. wgRevisionId is 0, since we're looking at
+			// the success page for the rollback and not the content of the page.
+			// It might make sense to display a success message in a toast, like other
+			// implementations of the postEdit hook, but for now we just want to
+			// enable code that relies on postEdit firing to know that an edit
+			// was made.
+			mw.hook( 'postEdit' ).fire();
+		} );
+	}
+
 	if ( Number( mw.user.options.get( 'showrollbackconfirmation' ) ) !== 1 ) {
 		// Support both 1 or "1" (T54542)
 		return;
