5a843fe43844e1d24954fa014b86a8ff3c921370
Srishakatux <ssethi@wikimedia.org>
2023-09-08 18:38:45 -0700

    Add language Karekare (kai)
    * Added language code to `Names.php`
    * Added namespace translations provided in incubator:
    https://incubator.wikimedia.org/wiki/Talk:Wp/kai#Namespace_translations
    * Added language code to release notes
    
    Bug: T345807
    Change-Id: I4ec9b2cb6c981f6e2f89b26427bbdbe7b8820ad9

---
+++ languages/messages/MessagesKai.php
@@ -0,0 +1,26 @@
+<?php
+/** Karekare (Karai-karai)
+ *
+ * @file
+ * @ingroup Languages
+ * @author De-Invincible
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Mìdiyà',
+	NS_SPECIAL          => 'Wadì',
+	NS_TALK             => 'Ɗàfu',
+	NS_USER             => 'Ba_wàanò',
+	NS_USER_TALK        => 'Ɗàfu_mà_Ba_wàanò',
+	NS_PROJECT_TALK     => 'Ɗafu_mà_$1',
+	NS_FILE             => 'Payiil',
+	NS_FILE_TALK        => 'Payiil_chàchā',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_chàchā',
+	NS_TEMPLATE         => 'Tampileti',
+	NS_TEMPLATE_TALK    => 'Tampileti_chàchā',
+	NS_HELP             => 'Gàɗu',
+	NS_HELP_TALK        => 'Gàɗu_chàchā',
+	NS_CATEGORY         => 'Shìnge',
+	NS_CATEGORY_TALK    => 'Shìnge_chàchā',
+];
