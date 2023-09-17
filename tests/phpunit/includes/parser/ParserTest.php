6cf91bbb4cb1692d56a4ad4cebe822ddb86377ae
tacsipacsi <tacsipacsi@jnet.hu>
2023-09-17 20:48:30 +0200

    Parser: use PHPDoc comments on properties, typed private properties
    Many private and even public properties and class constants were
    documented using #-style comments, which were not available in Doxygen
    documentation and editor tooling. Move these comments to PHPDoc comments
    to make them accessible.
    
    Add type declarations to private and internal properties wherever
    possible. Remove PHPDoc documentation made redundant by this, but
    add/keep PHPDoc documentation where it provides additional value
    (human-readable documentation, array types, union types). Don’t add type
    declarations to non-internal public properties as it potentially causes
    breakage in case some external code not only uses the deprecated
    property, but even writes it. These type declarations should be added
    when the properties are made private or internal.
    
    Change-Id: I247643b9bf0cabdc92a7e893d653edeaed9a1307

--- tests/phpunit/includes/parser/ParserTest.php
+++ tests/phpunit/includes/parser/ParserTest.php
@@ -86,7 +86,7 @@ class ParserTest extends MediaWikiIntegrationTestCase {
 		foreach ( $refObject->getProperties() as $prop ) {
 			$prop->setAccessible( true );
 			foreach ( $args as $idx => $mockTest ) {
-				if ( $prop->getValue( $parser ) === $mockTest ) {
+				if ( $prop->isInitialized( $parser ) && $prop->getValue( $parser ) === $mockTest ) {
 					unset( $args[$idx] );
 				}
 			}
