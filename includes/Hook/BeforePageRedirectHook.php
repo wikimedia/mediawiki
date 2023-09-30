2de8b0801472e754b4d2e0e2337aa4e8fe74c0ff
Timo Tijhof <krinkle@fastmail.com>
2023-09-29 17:47:51 -0700

    OutputPage: Fix typo in BeforePageRedirectHook docs
    Follows-up 6dacea8ec695887c512b8772bf37f269498a8125.
    
    Change-Id: I352d9c29365ab490f1679afa9e32bf193ea67015

--- includes/Hook/BeforePageRedirectHook.php
+++ includes/Hook/BeforePageRedirectHook.php
@@ -20,7 +20,7 @@ interface BeforePageRedirectHook {
 	 * @since 1.35
 	 *
 	 * @param OutputPage $out
-	 * @param string &$redirect Absolute or path-relatvie URL, modifiable
+	 * @param string &$redirect Absolute or path-relative URL, modifiable
 	 * @param string &$code HTTP code (eg '301' or '302'), modifiable
 	 * @return bool|void True or no return value to continue or false to abort
 	 */
