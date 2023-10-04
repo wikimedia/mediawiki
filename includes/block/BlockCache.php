03ceb372b3bac6ddc6af2d0a1113a2db9dd09868
Tim Starling <tstarling@wikimedia.org>
2023-10-04 11:26:46 +1100

    Add cache to BlockManager
    Add a cache to BlockManager, so that we can get rid of the block cache in
    User.
    
    The cache stores up to three blocks -- a request block, a user block and
    an anonymous (IP) block. Using object identity to compare keys means
    that we can avoid serializing the WebRequest and UserIdentity.
    
    Bug: T345683
    Change-Id: I7639766519861690a759629144c8680767539293

---
+++ includes/block/BlockCache.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MediaWiki\Block;
+
+use MediaWiki\User\UserIdentity;
+
+/**
+ * @internal For use by BlockManager
+ */
+class BlockCache {
+	/** @var BlockCacheEntry[] The cache entries indexed by partial key */
+	private $cache = [];
+
+	/**
+	 * Get a cached Block for the given key, or null if there is no cache entry,
+	 * or false if there is a cache entry indicating that the given target is
+	 * not blocked.
+	 *
+	 * @param BlockCacheKey $key
+	 * @return AbstractBlock|false|null
+	 */
+	public function get( BlockCacheKey $key ) {
+		$entry = $this->cache[$key->getPartialKey()] ?? null;
+		if ( $entry === null ) {
+			return null;
+		}
+		return $key->matchesStored( $entry->key ) ? $entry->block : null;
+	}
+
+	/**
+	 * Set a block cache entry
+	 *
+	 * @param BlockCacheKey $key The bundled block cache parameters
+	 * @param AbstractBlock|false $value The block, or false to indicate that
+	 *   the target is not blocked.
+	 */
+	public function set( BlockCacheKey $key, $value ) {
+		$this->cache[$key->getPartialKey()] = new BlockCacheEntry( $key, $value );
+	}
+
+	/**
+	 * Clear all block cache entries associated with a user
+	 *
+	 * @param UserIdentity $user
+	 */
+	public function clearUser( UserIdentity $user ) {
+		foreach ( $this->cache as $partialKey => $entry ) {
+			if ( $entry->key->isUser( $user ) ) {
+				unset( $this->cache[$partialKey] );
+			}
+		}
+	}
+}
---
+++ tests/phpunit/unit/includes/block/BlockCacheTest.php
@@ -0,0 +1,113 @@
+<?php
+
+use MediaWiki\Block\BlockCache;
+use MediaWiki\Block\BlockCacheKey;
+use MediaWiki\Block\SystemBlock;
+use MediaWiki\User\UserIdentityValue;
+
+/**
+ * @covers \MediaWiki\Block\BlockCache
+ * @covers \MediaWiki\Block\BlockCacheEntry
+ * @covers \MediaWiki\Block\BlockCacheKey
+ */
+class BlockCacheTest extends MediaWikiUnitTestCase {
+	public function testGet() {
+		// The exact semantics of partial keys and evictions in BlockCache are
+		// implementation details, not really part of the public contract.
+		// But we have some tests for them here to confirm that the code as
+		// implemented at least reflects the developer's intention.
+
+		$cache = new BlockCache;
+
+		$req1 = new FauxRequest();
+		$req2 = new FauxRequest();
+
+		$user1 = new UserIdentityValue( 1, 'User 1' );
+		$user2 = new UserIdentityValue( 2, 'User 2' );
+		$anon = new UserIdentityValue( 0, '127.0.0.1' );
+
+		$keyR1U1 = new BlockCacheKey( $req1, $user1, false );
+		$keyR2U1 = new BlockCacheKey( $req2, $user1, false );
+		$keyR2U2 = new BlockCacheKey( $req2, $user2, false );
+		$keyR2U2P = new BlockCacheKey( $req2, $user2, true );
+		$keyU1 = new BlockCacheKey( null, $user1, false );
+		$keyU2P = new BlockCacheKey( null, $user2, true );
+		$keyA1 = new BlockCacheKey( null, $anon, false );
+
+		$block1 = new SystemBlock();
+		$block2 = new SystemBlock();
+		$block3 = new SystemBlock();
+		$block4 = new SystemBlock();
+		$block5 = new SystemBlock();
+
+		// Empty cache
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertNull( $cache->get( $keyR2U2 ) );
+		$this->assertNull( $cache->get( $keyR2U2P ) );
+		$this->assertNull( $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set a request block to absent
+		$cache->set( $keyR1U1, false );
+		$this->assertFalse( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertNull( $cache->get( $keyR2U2 ) );
+		$this->assertNull( $cache->get( $keyR2U2P ) );
+		$this->assertNull( $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set another request block with a different request
+		$cache->set( $keyR2U1, $block1 );
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertSame( $block1, $cache->get( $keyR2U1 ) );
+		$this->assertNull( $cache->get( $keyR2U2 ) );
+		$this->assertNull( $cache->get( $keyR2U2P ) );
+		$this->assertNull( $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set another request block with a different user
+		$cache->set( $keyR2U2, $block2 );
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertSame( $block2, $cache->get( $keyR2U2 ) );
+		$this->assertNull( $cache->get( $keyR2U2P ) );
+		$this->assertNull( $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set another request block with a different fromPrimary flag
+		$cache->set( $keyR2U2P, $block3 );
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2P ) );
+		$this->assertNull( $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set a user block with no request
+		$cache->set( $keyU1, $block4 );
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2P ) );
+		$this->assertSame( $block4, $cache->get( $keyU1 ) );
+		$this->assertNull( $cache->get( $keyA1 ) );
+
+		// Set an anonymous block with no request
+		$cache->set( $keyA1, $block5 );
+		$this->assertNull( $cache->get( $keyR1U1 ) );
+		$this->assertNull( $cache->get( $keyR2U1 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2 ) );
+		$this->assertSame( $block3, $cache->get( $keyR2U2P ) );
+		$this->assertSame( $block4, $cache->get( $keyU1 ) );
+		$this->assertSame( $block5, $cache->get( $keyA1 ) );
+
+		// The weak reference causes the request cache entry to disappear when
+		// the request is destroyed
+		$req2 = null;
+		$this->assertNull( $cache->get( $keyR2U2P ) );
+
+		// An non-request block does not match a destroyed weak reference
+		$this->assertNull( $cache->get( $keyU2P ) );
+	}
+}
