30edaf6e46edc2db1309bbacb019563a975df4e8
Dringsim <dringsim@qq.com>
2023-10-16 16:37:26 +0800

    EmailUserTest: check user in getOption() in mutelist test
    Change-Id: Ieb51d0bc391c7d768e7dc5f2c411dfbeb1ef4dae

--- tests/phpunit/unit/includes/mail/EmailUserTest.php
+++ tests/phpunit/unit/includes/mail/EmailUserTest.php
@@ -15,6 +15,7 @@ use MediaWiki\User\CentralId\CentralIdLookup;
 use MediaWiki\User\Options\UserOptionsLookup;
 use MediaWiki\User\User;
 use MediaWiki\User\UserFactory;
+use MediaWiki\User\UserIdentity;
 use MediaWikiUnitTestCase;
 use StatusValue;
 use Wikimedia\Message\IMessageFormatterFactory;
@@ -125,9 +126,12 @@ class EmailUserTest extends MediaWikiUnitTestCase {
 		$muteListOptionsLookup = $this->createMock( UserOptionsLookup::class );
 		$muteListOptionsLookup->expects( $this->atLeast( 2 ) )
 			->method( 'getOption' )
-			->willReturnCallback( static function ( $_, $option ) use ( $senderCentralID ) {
-				return $option === 'email-blacklist' ? (string)$senderCentralID : true;
-			} );
+			->willReturnCallback(
+				function ( UserIdentity $user, string $option ) use ( $validTarget, $senderCentralID ) {
+					$this->assertSame( $validTarget->getUser(), $user );
+					return $option === 'email-blacklist' ? (string)$senderCentralID : null;
+				}
+			);
 		$centralIdLookup = $this->createMock( CentralIdLookup::class );
 		$centralIdLookup->expects( $this->atLeastOnce() )
 			->method( 'centralIdFromLocalUser' )
