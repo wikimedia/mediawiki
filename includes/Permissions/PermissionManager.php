e28679cbb65b9b087aaa9be052de27adc42789d0
Aaron Schulz <aschulz@wikimedia.org>
2023-10-19 10:58:51 -0700

    permissions: rename IMPLICIT_RIGHTS constant to CORE_IMPLICIT_RIGHTS
    Change-Id: Ia4d734295e9d14ec1fdf0ddb9cd02c8855343a62

--- includes/Permissions/PermissionManager.php
+++ includes/Permissions/PermissionManager.php
@@ -246,7 +246,7 @@ class PermissionManager {
 	 * "right-$right".
 	 * @showinitializer
 	 */
-	private const IMPLICIT_RIGHTS = [
+	private const CORE_IMPLICIT_RIGHTS = [
 		'renderfile',
 		'renderfile-nonstandard',
 		'stashedit',
@@ -1715,7 +1715,7 @@ class PermissionManager {
 	public function getImplicitRights(): array {
 		if ( $this->implicitRights === null ) {
 			$rights = array_unique( array_merge(
-				self::IMPLICIT_RIGHTS,
+				self::CORE_IMPLICIT_RIGHTS,
 				$this->options->get( MainConfigNames::ImplicitRights )
 			) );
 
