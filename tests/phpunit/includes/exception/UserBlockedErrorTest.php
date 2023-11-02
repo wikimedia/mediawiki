527be9ff5494d016b7011be1c1620d7a74520fd9
Dreamy Jazz <wbrown-ctr@wikimedia.org>
2023-11-02 13:00:16 +0000

    Test UserBlockedError
    Why:
    * Recent additions of multiple message display mean that
      adding tests for UserBlockedError is useful in reviewing
      the change.
    * As I made these tests to review the change, they can be
      added into the repo as tests to increase test coverage.
    
    What:
    * Add two integration tests for UserBlockedError. These
      are integration tests because the class relies on the
      global state (calling MediaWikiServices::getInstance in
      all cases).
    
    Bug: T349826
    Change-Id: I9f27d792ac7faf9378278e729a8cef2ec39c2bba

---
+++ tests/phpunit/includes/exception/UserBlockedErrorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use MediaWiki\Block\AbstractBlock;
+use MediaWiki\Block\BlockErrorFormatter;
+use MediaWiki\User\UserIdentity;
+
+/**
+ * @covers UserBlockedError
+ */
+class UserBlockedErrorTest extends MediaWikiIntegrationTestCase {
+
+	private function setUpMockBlockFormatter(
+		$expectedBlock, $expectedUser, $expectedLanguage, $expectedIp, $returnMessages
+	) {
+		$mockBlockErrorFormatter = $this->createMock( BlockErrorFormatter::class );
+		$mockBlockErrorFormatter->expects( $this->once() )
+			->method( 'getMessages' )
+			->with( $expectedBlock, $expectedUser, $expectedLanguage, $expectedIp )
+			->willReturn( $returnMessages );
+		$this->overrideMwServices( null, [
+			'BlockErrorFormatter' => static function () use ( $mockBlockErrorFormatter ) {
+				return $mockBlockErrorFormatter;
+			}
+		] );
+	}
+
+	public function testConstructionProvidedOnlyBlockParameter() {
+		$context = RequestContext::getMain();
+		$block = $this->createMock( AbstractBlock::class );
+		$this->setUpMockBlockFormatter(
+			$block, $context->getUser(), $context->getLanguage(), $context->getRequest()->getIP(),
+			[ new RawMessage( 'testing' ) ]
+		);
+		$e = new UserBlockedError( $block );
+		$this->assertSame(
+			( new RawMessage( 'testing' ) )->text(),
+			$e->getMessageObject()->text(),
+			'UserBlockedError did not use the expected message.'
+		);
+	}
+
+	public function testConstructionProvidedAllParametersWithMultipleBlockMessages() {
+		$mockUser = $this->createMock( UserIdentity::class );
+		$mockLanguage = $this->createMock( Language::class );
+		$block = $this->createMock( AbstractBlock::class );
+		$this->setUpMockBlockFormatter(
+			$block, $mockUser, $mockLanguage, '1.2.3.4',
+			[ new RawMessage( 'testing' ), new RawMessage( 'testing2' ) ]
+		);
+		$e = new UserBlockedError( $block, $mockUser, $mockLanguage, '1.2.3.4' );
+		$this->assertSame(
+			"* testing\n* testing2",
+			$e->getMessageObject()->text(),
+			'UserBlockedError did not use the expected message.'
+		);
+	}
+}
