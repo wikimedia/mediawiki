a1775e4fd79a04eefd2cb7729de6d4dd3eabf26e
Jon Harald Søby <jhsoby@gmail.com>
2023-11-06 14:56:38 +0100

    [Languages] Add namespace translations for zgh
    Add namespace translations for Standard Moroccan Amazigh.
    Provided by User:Lhoussine AIT TAYFST in
    https://incubator.wikimedia.org/w/index.php?oldid=6136681
    
    Change-Id: I15fb86355e953f32b37046551d7b611ecf8024c2

--- languages/messages/MessagesZgh.php
+++ languages/messages/MessagesZgh.php
@@ -3,6 +3,32 @@
  *
  * @file
  * @ingroup Languages
+ *
+ * @author Lhoussine AIT TAYFST
  */
 
 $fallback = 'kab';
+
+$namespaceNames = [
+	NS_MEDIA            => 'ⵎⵉⴷⵢⴰ',
+	NS_SPECIAL          => 'ⴰⵎⵥⵍⵉ',
+	NS_TALK             => 'ⴰⵎⵙⴳⴷⴰⵍ',
+	NS_USER             => 'ⴰⵏⵙⵙⵎⵔⵙ',
+	NS_USER_TALK        => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵓⵏⵙⵙⵎⵔⵙ',
+	NS_PROJECT_TALK     => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_$1',
+	NS_FILE             => 'ⴰⴼⴰⵢⵍⵓ',
+	NS_FILE_TALK        => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵓⴼⴰⵢⵍⵓ',
+	NS_MEDIAWIKI        => 'ⵎⵉⴷⵢⴰⵡⵉⴽⵉ',
+	NS_MEDIAWIKI_TALK   => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵎⵉⴷⵢⴰⵡⵉⴽⵉ',
+	NS_TEMPLATE         => 'ⴰⵍⴱⵓⴹ',
+	NS_TEMPLATE_TALK    => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵓⵍⴱⵓⴹ',
+	NS_HELP             => 'ⵜⵉⵡⵉⵙⵉ',
+	NS_HELP_TALK        => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵜⵡⵉⵙⵉ',
+	NS_CATEGORY         => 'ⴰⵙⵎⵉⵍ',
+	NS_CATEGORY_TALK    => 'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵓⵙⵎⵉⵍ',
+];
+
+$namespaceAliases = [
+	'ⴰⵍⴱⵓⴹ'            => NS_TEMPLATE,
+	'ⴰⵎⵙⴳⴷⴰⵍ_ⵏ_ⵓⵍⴱⵓⴹ'   => NS_TEMPLATE_TALK,
+];
