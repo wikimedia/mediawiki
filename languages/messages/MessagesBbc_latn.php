16d56c438a7f908cdde876614a51edf38ce18eb1
Jon Harald Søby <jhsoby@gmail.com>
2023-11-07 15:19:09 +0100

    [Languages] Add namespaces names for dga and bbc-latn
    Add namespace names for the Dagaare language. Submitted by
    Alhassan Mohammed Awal at
    https://incubator.wikimedia.org/w/index.php?oldid=6137090
    
    Add namespace names for the Batak Toba language. Submitted
    by Gazartashlomo0907 at
    https://incubator.wikimedia.org/w/index.php?oldid=6137271
    
    Change-Id: I963e76702811716985b37bcf0c4806ec040f21f0

--- languages/messages/MessagesBbc_latn.php
+++ languages/messages/MessagesBbc_latn.php
@@ -3,6 +3,27 @@
  *
  * @file
  * @ingroup Languages
+ *
+ * @author Gazartashlomo0907
  */
 
 $fallback = 'id';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Marhasurungan',
+	NS_TALK             => 'Panghataion',
+	NS_USER             => 'Sipanghasea',
+	NS_USER_TALK        => 'Panghataion_ni_Sipanghasea',
+	NS_PROJECT_TALK     => 'Panghataion_ni_$1',
+	NS_FILE             => 'Ugasan',
+	NS_FILE_TALK        => 'Panghataion_ni_Ugasan',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Panghataion_ni_MediaWiki',
+	NS_TEMPLATE         => 'Templat',
+	NS_TEMPLATE_TALK    => 'Panghataion_ni_Templat',
+	NS_HELP             => 'Pangurupion',
+	NS_HELP_TALK        => 'Panghataion_ni_Pangurupion',
+	NS_CATEGORY         => 'Horong',
+	NS_CATEGORY_TALK    => 'Panghataion_ni_Horong',
+];
--- languages/messages/MessagesDga.php
+++ languages/messages/MessagesDga.php
@@ -5,6 +5,26 @@
  * @ingroup Languages
  *
  * @author Amir E. Aharoni
+ * @author Alhassan Mohammed Awal
  */
 
 $linkTrail = '/^([ɓɛɩŋɔʋƴƁƐƖŊƆƲƳa-z]+)(.*)$/sDu';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Duoro_bimbu_zie',
+	NS_SPECIAL          => 'Be_o_yoŋ',
+	NS_TALK             => 'Yeli',
+	NS_USER             => 'Toma_daana',
+	NS_USER_TALK        => 'Toma_daana_yeli',
+	NS_PROJECT_TALK     => '$1_yeli',
+	NS_FILE             => 'Duoro_kɔre',
+	NS_FILE_TALK        => 'Duoro_kɔre_yeli',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_yeli',
+	NS_TEMPLATE         => 'Tɛmpileti',
+	NS_TEMPLATE_TALK    => 'Tɛmpileti_yeli',
+	NS_HELP             => 'Sombo',
+	NS_HELP_TALK        => 'Sombo_yeli',
+	NS_CATEGORY         => 'Gbuli',
+	NS_CATEGORY_TALK    => 'Gbuli_yeli',
+];
