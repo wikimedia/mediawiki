miraheze/mediawiki-archived

PoolCounterConnectionManager: Add support for ipv6
Change-Id: I50c9cf17efcb974aa76068814d270c50699865c0

includes/poolcounter/PoolCounterConnectionManager.php
@@ -22,6 +22,7 @@ namespace MediaWiki\PoolCounter;
 
 use MWException;
 use Status;
+use Wikimedia\IPUtils;
 
 /**
  * Helper for \MediaWiki\PoolCounter\PoolCounterClient.
@@ -74,12 +75,14 @@ class PoolCounterConnectionManager {
 				return Status::newGood(
 					[ 'conn' => $this->conns[$hostName], 'hostName' => $hostName ] );
 			}
-			$parts = explode( ':', $hostName, 2 );
-			if ( count( $parts ) < 2 ) {
-				$parts[] = 7531;
+			$servers = IPUtils::splitHostAndPort( $hostName );
+			if ( $servers === false ) {
+				continue;
 			}
+			$host = $servers[0];
+			$port = $servers[1] ?? 7531;
 			// phpcs:ignore Generic.PHP.NoSilencedErrors.Discouraged
-			$conn = @$this->open( $parts[0], $parts[1], $errno, $errstr );
+			$conn = @$this->open( $host, $port, $errno, $errstr );
 			if ( $conn ) {
 				break;
 			}

wikimedia/mediawiki

Bug: T350615
(cherry picked from commit 4344b2fb80727daa44eb461e316421ac803d8df1)

includes/poolcounter/PoolCounterConnectionManager.php
@@ -22,6 +22,7 @@ namespace MediaWiki\PoolCounter;
 
 use MediaWiki\Status\Status;
 use MWException;
+use Wikimedia\IPUtils;
 
 /**
  * Helper for \MediaWiki\PoolCounter\PoolCounterClient.
@@ -74,12 +75,15 @@ class PoolCounterConnectionManager {
 				return Status::newGood(
 					[ 'conn' => $this->conns[$hostName], 'hostName' => $hostName ] );
 			}
-			$parts = explode( ':', $hostName, 2 );
-			if ( count( $parts ) < 2 ) {
-				$parts[] = 7531;
+			$parts = IPUtils::splitHostAndPort( $hostName );
+			if ( $parts === false ) {
+				$errstr = '\'servers\' config incorrectly configured.';
+				return Status::newFatal( 'poolcounter-connection-error', $errstr, $hostName );
 			}
+			$host = $parts[0];
+			$port = $parts[1] ?? 7531;
 			// phpcs:ignore Generic.PHP.NoSilencedErrors.Discouraged
-			$conn = @$this->open( $parts[0], $parts[1], $errno, $errstr );
+			$conn = @$this->open( $host, $port, $errno, $errstr );
 			if ( $conn ) {
 				break;
 			}