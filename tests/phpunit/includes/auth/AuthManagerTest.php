684b9e655cd92fc3f46208494219647bcc98ae55
Umherirrender <umherirrender_de.wp@web.de>
2023-11-22 21:52:13 +0100

    tests: Avoid dynamic properties in AuthenticationProvider Test
    Deprecated in php8.2
    
    Bug: T326478
    Change-Id: I6a883d9d4bdd210e098cde90458af51857e5f136
    (cherry picked from commit 2eca47315f74b84c0fe02038e16bae11e198d604)

--- tests/phpunit/includes/auth/AuthManagerTest.php
+++ tests/phpunit/includes/auth/AuthManagerTest.php
@@ -1231,7 +1231,6 @@ class AuthManagerTest extends \MediaWikiIntegrationTestCase {
 		$rememberReq->action = AuthManager::ACTION_LOGIN;
 
 		$req = $this->getMockForAbstractClass( AuthenticationRequest::class );
-		$req->foobar = 'baz';
 		$restartResponse = AuthenticationResponse::newRestart(
 			$this->message( 'authmanager-authn-no-local-user' )
 		);
