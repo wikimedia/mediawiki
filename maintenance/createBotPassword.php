15402237af7b161025252f01537463864623c2dc
Samuel Guebo <samuelguebo@gmail.com>
2023-12-13 20:20:45 -0500

    Split User::getRightDescription into two methods
    - Add getRightDescriptionHtml() to return HTML
      the change handles cases where the message contains wikitext
    - Use the new method getRightDescriptionHtml() in createBotPassword
    
    Bug: T312819
    Change-Id: If3b9bce2f02806572cc6cc1194a07cb7d5b8d6da

--- maintenance/createBotPassword.php
+++ maintenance/createBotPassword.php
@@ -143,7 +143,7 @@ class CreateBotPassword extends Maintenance {
 		foreach ( $permissions as $permission ) {
 			$this->output(
 				str_pad( $permission, self::SHOWGRANTS_COLUMN_WIDTH ) . " " .
-				User::getRightDescription( $permission ) . "\n"
+				User::getRightDescriptionHtml( $permission ) . "\n"
 			);
 		}
 	}
