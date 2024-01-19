0a9d969f1261857fddd09faa0011a87f57ddc678
Doğu Abaris <ydogu@wikimedia.org.tr>
2024-01-19 11:30:16 +0100

    Replace deprecated WebRequest::getRequestId() with Telemetry
    The WikiProcessor::__invoke() method was using the deprecated
    WebRequest::getRequestId() to obtain the request ID. This patch
    updates the method to use Telemetry::getInstance()->getRequestId()
    instead, as per the deprecation notice in MediaWiki 1.41.
    
    Change-Id: I542d76af00613023db219e55ab57ac9bd807c6c3

--- includes/debug/logger/monolog/WikiProcessor.php
+++ includes/debug/logger/monolog/WikiProcessor.php
@@ -20,6 +20,7 @@
 
 namespace MediaWiki\Logger\Monolog;
 
+use MediaWiki\Http\Telemetry;
 use MediaWiki\WikiMap\WikiMap;
 
 /**
@@ -37,10 +38,11 @@ class WikiProcessor {
 	 * @return array
 	 */
 	public function __invoke( array $record ) {
+		$telemetry = Telemetry::getInstance();
 		$record['extra']['host'] = wfHostname();
 		$record['extra']['wiki'] = WikiMap::getCurrentWikiId();
 		$record['extra']['mwversion'] = MW_VERSION;
-		$record['extra']['reqId'] = \MediaWiki\Request\WebRequest::getRequestId();
+		$record['extra']['reqId'] = $telemetry->getRequestId();
 		if ( wfIsCLI() && isset( $_SERVER['argv'] ) ) {
 			$record['extra']['cli_argv'] = implode( ' ', $_SERVER['argv'] );
 		}
