56e9e1a817bcd479821a17be71bc45e4e16f491c
C. Scott Ananian <cscott@cscott.net>
2024-02-15 17:26:29 -0500

    Parser::getExternalLinkAttribs: Don't set rel attribute to null
    Paser::getExternalLinkRel() is defined to return `null` if there's
    no attribute to add, but then ParserOptions::getExternalLinkTarget()
    may try to append to it and external users might try to actually pass
    the $attribs to (eg) Xml::element() and become unhappy if the value
    is `null`.
    
    Bug: T357668
    Followup-To: Ifec733a923f193b72eaba9a1e604ad4e56c0aef2
    Change-Id: I907c22ef070616d81b9a50b0e807a7b8f78b59b5
    (cherry picked from commit f7ba84855a9061f47f43a6e7a0805a41a7e52ad1)

--- includes/parser/Parser.php
+++ includes/parser/Parser.php
@@ -2268,7 +2268,7 @@ class Parser {
 	 */
 	public function getExternalLinkAttribs( $url ) {
 		$attribs = [];
-		$rel = self::getExternalLinkRel( $url, $this->getTitle() );
+		$rel = self::getExternalLinkRel( $url, $this->getTitle() ) ?? '';
 
 		$target = $this->mOptions->getExternalLinkTarget();
 		if ( $target ) {
@@ -2283,7 +2283,9 @@ class Parser {
 				$rel .= 'noreferrer noopener';
 			}
 		}
-		$attribs['rel'] = $rel;
+		if ( $rel !== '' ) {
+			$attribs['rel'] = $rel;
+		}
 		return $attribs;
 	}
 
