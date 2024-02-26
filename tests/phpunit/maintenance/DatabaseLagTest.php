31bf78b0581c2f6e0b9c063fa2dc00139b2cace4
Antoine Musso <hashar@free.fr>
2024-02-26 17:31:41 +0100

    maintenance: add test for lag.php
    Test the output of maintenance/lag.php in both one off and continuous
    reporting modes (when it is passed -r).  The test coverage gives the
    basis to later adjust the script output, notably when a replication is
    broken (which prints the misleading "false").
    
    Bug: T358484
    Change-Id: Ib2e5597e5cd3b9b1b71c1c61c12814be5c5e7091

---
+++ tests/phpunit/maintenance/DatabaseLagTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MediaWiki\Tests\Maintenance;
+
+use Wikimedia\Rdbms\ILoadBalancer;
+
+/**
+ * @covers \DatabaseLag
+ * @group Maintenance
+ */
+class DatabaseLagTest extends MaintenanceBaseTestCase {
+
+	public function getMaintenanceClass() {
+		return \DatabaseLag::class;
+	}
+
+	public static function provideLagTimes() {
+		return [
+			'No lag' => [ 0, 'db-nolag-01', '/^db-nolag-01\s+0$/m' ],
+			'Some lag' => [ 42, 'db-somelag-02', '/^db-somelag-02\s+42$/m' ],
+			'Not replicating' => [
+				false, 'db-not-replicating-03', '/db-not-replicating-03\s+false$/m'
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideLagTimes
+	 */
+	public function testReportedOutput( $lag, $servername, $expected ) {
+		$lb = $this->createMock( ILoadBalancer::class );
+		$lb->method( 'getLagTimes' )->willReturn( [ $lag ] );
+		$lb->method( 'getServerName' )->willReturn( $servername );
+
+		$this->setService( 'DBLoadBalancer', $lb );
+
+		$this->maintenance->setOption( '-r', false );
+		$this->maintenance->execute();
+
+		$this->maintenance->setOption( '-r', true );
+		$this->maintenance->stopReporting = true;
+		$this->maintenance->execute();
+
+		$this->expectOutputRegex( $expected );
+	}
+
+}
