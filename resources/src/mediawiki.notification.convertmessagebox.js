86c70fbd8671b8a25b9fded4cf01d8e7155677b4
apaskulin <apaskulin@wikimedia.org>
2024-03-06 17:29:23 -0800

    docs: Migrate convertmessagebox to JSDoc
    Bug: T352308
    Change-Id: I47121aabf65e8a86949cd1da8945a29123946049

--- resources/src/mediawiki.notification.convertmessagebox.js
+++ resources/src/mediawiki.notification.convertmessagebox.js
@@ -1,63 +1,50 @@
 /**
+ * @module mediawiki.notification.convertmessagebox
+ */
+
+/**
+ * Convert a messagebox to a notification.
+ *
  * @example
  * var convertmessagebox = require( 'mediawiki.notification.convertmessagebox' );
  *
- * @class mw.plugin.convertmessagebox
- * @singleton
+ * @param {Object} [options] Options
+ * @param {mw.Message} [options.msg] Message key (must be loaded already)
  */
-( function () {
-	'use strict';
+module.exports = function ( options ) {
+	var $msgBox, type, autoHide, msg, notif,
+		$successBox = $( '.mw-notify-success' ),
+		$warningBox = $( '.mw-notify-warning' ),
+		$errorBox = $( '.mw-notify-error' );
 
-	/**
-	 * Convert a messagebox to a notification.
-	 *
-	 * Checks if a message box with class `.mw-notify-success`, `.mw-notify-warning`, or `.mw-notify-error`
-	 * exists and converts it into a mw.Notification with the text of the element or a given message key.
-	 *
-	 * By default the notification will automatically hide after 5s, or when the user clicks the element.
-	 * This can be overridden by setting attribute `data-mw-autohide="true"`.
-	 *
-	 * @param {Object} [options] Options
-	 * @param {mw.Message} [options.msg] Message key (must be loaded already)
-	 */
-	function convertmessagebox( options ) {
-		var $msgBox, type, autoHide, msg, notif,
-			$successBox = $( '.mw-notify-success' ),
-			$warningBox = $( '.mw-notify-warning' ),
-			$errorBox = $( '.mw-notify-error' );
-
-		// If there is a message box and javascript is enabled, use a slick notification instead!
-		if ( $successBox.length ) {
-			$msgBox = $successBox;
-			type = 'info';
-		} else if ( $warningBox.length ) {
-			$msgBox = $warningBox;
-			type = 'warn';
-		} else if ( $errorBox.length ) {
-			$msgBox = $errorBox;
-			type = 'error';
-		} else {
-			return;
-		}
-
-		autoHide = $msgBox.attr( 'data-mw-autohide' ) === 'true';
-
-		// If the msg param is given, use it, otherwise use the text of the successbox
-		msg = options && options.msg || $msgBox.text();
-		$msgBox.detach();
-
-		notif = mw.notification.notify( msg, { autoHide: autoHide, type: type } );
-		if ( !autoHide ) {
-			// 'change' event not reliable!
-			$( document ).one( 'keydown mousedown', function () {
-				if ( notif ) {
-					notif.close();
-					notif = null;
-				}
-			} );
-		}
+	// If there is a message box and javascript is enabled, use a slick notification instead!
+	if ( $successBox.length ) {
+		$msgBox = $successBox;
+		type = 'info';
+	} else if ( $warningBox.length ) {
+		$msgBox = $warningBox;
+		type = 'warn';
+	} else if ( $errorBox.length ) {
+		$msgBox = $errorBox;
+		type = 'error';
+	} else {
+		return;
 	}
 
-	module.exports = convertmessagebox;
+	autoHide = $msgBox.attr( 'data-mw-autohide' ) === 'true';
 
-}() );
+	// If the msg param is given, use it, otherwise use the text of the successbox
+	msg = options && options.msg || $msgBox.text();
+	$msgBox.detach();
+
+	notif = mw.notification.notify( msg, { autoHide: autoHide, type: type } );
+	if ( !autoHide ) {
+		// 'change' event not reliable!
+		$( document ).one( 'keydown mousedown', function () {
+			if ( notif ) {
+				notif.close();
+				notif = null;
+			}
+		} );
+	}
+};
