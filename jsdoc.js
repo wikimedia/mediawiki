42fed59aefbd6d99fdeb5796591f352202a942e6
apaskulin <apaskulin@wikimedia.org>
2024-03-06 18:03:07 -0800

    docs: Include moment in JSDoc
    Bug: T352308
    Change-Id: I770de08c86b6cf4be714e8be7345dead58c7ff35

--- jsdoc.js
+++ jsdoc.js
@@ -22,7 +22,6 @@ module.exports = {
 			 got round to reviewing them and incorporating them into the documentation page yet. */
 			'resources/src/mediawiki.router',
 			'resources/src/mediawiki.language.specialCharacters',
-			'resources/src/moment',
 			'resources/src/oojs-global.js',
 			'resources/src/mediawiki.notification.convertmessagebox.js',
 			'resources/src/ooui-local.js'
--- resources/src/moment/moment-module.js
+++ resources/src/moment/moment-module.js
@@ -1,2 +1,6 @@
-// Allow loading Moment like a module, using require( 'moment' )
+/**
+ * Allow loading [Moment](https://momentjs.com/) (v2.25.2) like a module, using `require( 'moment' )`.
+ *
+ * @exports moment
+ */
 module.exports = window.moment;
