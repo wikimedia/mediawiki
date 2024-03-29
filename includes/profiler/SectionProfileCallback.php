4a7d5bb940065b73f07e9e01066a403b33eb4cf7
Doğu Abaris <abaris@null.net>
2024-03-30 00:23:04 +0100

    Use typed properties in SectionProfileCallback
    Introduce typed properties for $profiler and $section in
    SectionProfileCallback, eliminating docblock annotations for
    a cleaner and type-safe codebase.
    
    Change-Id: I385a2d2f769bc1efdf0e0f7f3ca8040aae206c71

--- includes/profiler/SectionProfileCallback.php
+++ includes/profiler/SectionProfileCallback.php
@@ -26,16 +26,15 @@ use Wikimedia\ScopedCallback;
  * @internal For use by SectionProfiler
  */
 class SectionProfileCallback extends ScopedCallback {
-	/** @var SectionProfiler */
-	protected $profiler;
-	/** @var string */
-	protected $section;
+
+	protected SectionProfiler $profiler;
+	protected string $section;
 
 	/**
 	 * @param SectionProfiler $profiler
 	 * @param string $section
 	 */
-	public function __construct( SectionProfiler $profiler, $section ) {
+	public function __construct( SectionProfiler $profiler, string $section ) {
 		parent::__construct( null );
 		$this->profiler = $profiler;
 		$this->section = $section;
