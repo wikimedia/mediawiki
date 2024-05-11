73de566949cf8eabf9a66cb61ef97d3451a6c947
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-05-11 20:10:35 +0200

    Use 'scalar' type alias to shorten PHPDoc annotations
    'string|int|float|bool' (in any order) can be replaced by 'scalar'.
    'string|int|float|bool|null' (likewise) can be replaced by '?scalar'.
    
    This is convenient for functions that can accept any primitive value,
    which comes up sometimes when serializing things as SQL, JSON etc.
    
    Change-Id: I4a711ee59611d76d6745f3640e4aa6bebec02918

--- includes/libs/rdbms/expression/Expression.php
+++ includes/libs/rdbms/expression/Expression.php
@@ -23,7 +23,7 @@ class Expression implements IExpression {
 	 * @param-taint $field exec_sql
 	 * @param string $op One of '>', '<', '!=', '=', '>=', '<=', IExpression::LIKE, IExpression::NOT_LIKE
 	 * @param-taint $op exec_sql
-	 * @param string|int|float|bool|Blob|null|LikeValue|non-empty-list<string|int|float|bool|Blob> $value
+	 * @param ?scalar|Blob|LikeValue|non-empty-list<scalar|Blob> $value
 	 * @param-taint $value escapes_sql
 	 * @internal Outside of rdbms, Use IReadableDatabase::expr() to create an expression object.
 	 */
@@ -63,7 +63,7 @@ class Expression implements IExpression {
 	 * @param-taint $field exec_sql
 	 * @param string $op One of '>', '<', '!=', '=', '>=', '<=', IExpression::LIKE, IExpression::NOT_LIKE
 	 * @param-taint $op exec_sql
-	 * @param string|int|float|bool|Blob|null|LikeValue|non-empty-list<string|int|float|bool|Blob> $value
+	 * @param ?scalar|Blob|LikeValue|non-empty-list<scalar|Blob> $value
 	 * @param-taint $value escapes_sql
 	 * @phan-side-effect-free
 	 */
@@ -77,7 +77,7 @@ class Expression implements IExpression {
 	 * @param-taint $field exec_sql
 	 * @param string $op One of '>', '<', '!=', '=', '>=', '<=', IExpression::LIKE, IExpression::NOT_LIKE
 	 * @param-taint $op exec_sql
-	 * @param string|int|float|bool|Blob|null|LikeValue|non-empty-list<string|int|float|bool|Blob> $value
+	 * @param ?scalar|Blob|LikeValue|non-empty-list<scalar|Blob> $value
 	 * @param-taint $value escapes_sql
 	 * @phan-side-effect-free
 	 */
