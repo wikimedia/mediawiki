709cb1e09b87d1d0f0d7f9b024676d3bec047e05
Atieno <pnjira@wikimedia.org>
2024-05-12 19:50:19 +0300

    arrayDef: Implement JSON schema validation in ArrayDef
    Bug: T362108
    Change-Id: Ia64e96e2b368d28a560f1277c9f31ba225a6e1c6

--- includes/ParamValidator/TypeDef/ArrayDef.php
+++ includes/ParamValidator/TypeDef/ArrayDef.php
@@ -2,6 +2,11 @@
 
 namespace MediaWiki\ParamValidator\TypeDef;
 
+use InvalidArgumentException;
+use JsonSchema\Constraints\Constraint;
+use JsonSchema\Validator;
+use LogicException;
+use Wikimedia\Message\DataMessageValue;
 use Wikimedia\ParamValidator\TypeDef;
 
 /**
@@ -11,11 +16,15 @@ use Wikimedia\ParamValidator\TypeDef;
  * Failure codes:
  *  - 'notarray': The value is not an array.
  *
- * @todo implement validation based on a JSON schema
- *
  * @since 1.42
  */
 class ArrayDef extends TypeDef {
+
+	/**
+	 * (object) Schema settings.
+	 *
+	 */
+	public const PARAM_SCHEMA = 'param-schema';
 
 	public function supportsArrays() {
 		return true;
@@ -27,7 +36,44 @@ class ArrayDef extends TypeDef {
 			$this->failure( 'notarray', $name, $value, $settings, $options );
 		}
 
+		if ( isset( $settings[ self::PARAM_SCHEMA ] ) ) {
+			$schema = $settings[ self::PARAM_SCHEMA ];
+
+			if ( !isset( $schema[ 'type' ] ) ) {
+				throw new InvalidArgumentException( "Schema type not set " );
+			}
+			$types = $schema['type'];
+			foreach ( $types as $type ) {
+				// @todo:  start using JsonSchemaTrait::normalizeJsonSchema
+				// so we can also support the "list" and "map" types
+				if ( ( $type !== 'object' ) && ( $type !== 'array' ) ) {
+					throw new LogicException( 'Invalid data type' );
+				}
+			}
+
+			$validator = new Validator();
+			$validator->validate( $value, $schema, Constraint::CHECK_MODE_TYPE_CAST );
+			if ( !$validator->isValid() ) {
+				$errorCode = 'schema-validation-failed';
+				foreach ( $validator->getErrors() as $error ) {
+					$message = DataMessageValue::new(
+						"paramvalidator-$errorCode",
+						[ $error[ 'message' ] ],
+						$errorCode,
+						[ 'schema-validation-error' => $error ]
+					);
+					$this->failure( $message, $name, $value, $settings, $options );
+				}
+			}
+		}
 		return $value;
 	}
 
+	public function stringifyValue( $name, $value, array $settings, array $options ) {
+		if ( !is_array( $value ) ) {
+			return parent::stringifyValue( $name, $value, $settings, $options );
+		}
+
+		return json_encode( $value );
+	}
 }
--- tests/phpunit/unit/includes/ParamValidator/TypeDef/ArrayDefTest.php
+++ tests/phpunit/unit/includes/ParamValidator/TypeDef/ArrayDefTest.php
@@ -3,7 +3,9 @@
 namespace MediaWiki\Tests\ParamValidator\TypeDef;
 
 use MediaWiki\ParamValidator\TypeDef\ArrayDef;
+use Wikimedia\Message\DataMessageValue;
 use Wikimedia\ParamValidator\SimpleCallbacks;
+use Wikimedia\ParamValidator\ValidationException;
 
 /**
  * @covers \MediaWiki\ParamValidator\TypeDef\ArrayDef
@@ -23,5 +25,73 @@ class ArrayDefTest extends TypeDefUnitTestCase {
 		yield 'null' => [ null, $notComplex ];
 		yield 'string' => [ 'foo', $notComplex ];
 		yield 'zero' => [ 0, $notComplex ];
+
+		// test json with schema passed in $settings array
+		yield 'valid structure' => [
+			[ 'key1' => 'value1' ],
+			[ 'key1' => 'value1' ],
+			[ ArrayDef::PARAM_SCHEMA =>
+				[
+					'type' => [ 'object' ],
+					'required' => [ 'key1' ]
+				]
+			]
+		];
+
+		yield 'nested one level valid structure' => [
+			[ 'key1' => [ 'nested1' => 'value1' ] ],
+			[ 'key1' => [ 'nested1' => 'value1' ] ],
+			[ ArrayDef::PARAM_SCHEMA =>
+				[
+					'type' => [ 'object' ],
+					'required' => [ 'key1' ]
+				]
+			]
+		];
+
+		yield 'nested two levels valid structure' => [
+			[ 'key1' => [ 'nested1' => [ 'nested2' => 'value1' ] ] ],
+			[ 'key1' => [ 'nested1' => [ 'nested2' => 'value1' ] ] ],
+			[ ArrayDef::PARAM_SCHEMA =>
+				[
+					'type' => [ 'object' ],
+					'required' => [ 'key1' ]
+				]
+			]
+		];
+		yield 'object with numeric keys' => [
+			[ 1 => 'value 1', 'key1' => 'value' ],
+			[ 1 => 'value 1', 'key1' => 'value' ],
+			[ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'key1' ] ] ]
+		];
+		$missingSchemaSettings = [ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'required' ] ] ];
+		$schemaValidationFailed = new ValidationException(
+			DataMessageValue::new(
+				'schema-validation-failed',
+				[ '' ],
+				'schema-validation-failed',
+				[ 'schema-validation-error' => [
+					'property' => 'required',
+					'pointer' => '/required',
+					'message' => 'The property required is required',
+					'constraint' => 'required',
+					'context' => 1
+				] ]
+			),
+			'test',
+			[ 'notrequired' => 'value' ],
+			$missingSchemaSettings
+		);
+		yield 'empty object is not valid' => [
+			[], $schemaValidationFailed, [ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'required' ] ] ]
+		];
+		yield 'empty array is valid' => [
+			[], [], [ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'array' ] ] ]
+		];
+		yield 'missing required in schema' => [
+			[ 'notrequired' => 'value' ],
+			$schemaValidationFailed,
+			$missingSchemaSettings
+		];
 	}
 }
