25563eb316a0daf51b9341e40454fcb707d58b1c
Jack Phoenix <ashley@uncyclomedia.co>
2024-06-14 01:57:48 +0000

    Name the PagerTools array entries to allow hooks to unset them
    In MW 1.39 in DifferenceEngine where similar-ish code exists, the entries are already named and this enables hooked functions to conditionally also unset an array item.
    
    Change-Id: Ic0e57e83bc295c985696226e8cfb73759339071f
    (cherry picked from commit 0e44ffdf7115cfb37cc1a95b4c5ec7d4ab8c0f6b)

--- includes/specials/pagers/PagerTools.php
+++ includes/specials/pagers/PagerTools.php
@@ -53,7 +53,7 @@ class PagerTools {
 				);
 				if ( $rollbackLink ) {
 					$this->preventClickjacking = true;
-					$tools[] = $rollbackLink;
+					$tools['mw-rollback'] = $rollbackLink;
 				}
 			}
 		}
@@ -75,7 +75,7 @@ class PagerTools {
 						'undo' => $revRecord->getId()
 					]
 				);
-				$tools[] = "<span class=\"mw-history-undo\">{$undolink}</span>";
+				$tools['mw-undo'] = "<span class=\"mw-history-undo\">{$undolink}</span>";
 			}
 		}
 		// Allow extension to add their own links here
