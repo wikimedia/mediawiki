c213cd0e1b979ccb2cafddc3e267308d915a3591
Justarandomamerican <76439161+Justarandomamerican@users.noreply.github.com>
2024-06-18 15:01:39 -0600

    Revert "Reapply "Add report extension config settings""
    This reverts commit bd51d8566bc8ffb53ab0f18a1b3b83d477ccf714.

--- LocalSettings.php
+++ LocalSettings.php
@@ -338,7 +338,6 @@ $wgGroupPermissions['sysop']['editsitejson'] = false;
 $wgGroupPermissions['sysop']['edituserjson'] = false;
 $wgGroupPermissions['sysop']['importupload'] = false;
 $wgGroupPermissions['sysop']['import'] = true;
-$wgGroupPermissions['sysop']['handle-reports'] = false;
 $wgGroupPermissions['sysop']['interwiki'] = false;
 $wgGroupPermissions['sysop']['mwoauthproposeconsumer'] = true;
 $wgGroupPermissions['sysop']['mwoauthupdateownconsumer'] = true;
@@ -397,7 +396,6 @@ $wgGroupPermissions['steward']['mwoauthsuppress'] = true;
 $wgGroupPermissions['steward']['mwoauthupdateownconsumer'] = true;
 $wgGroupPermissions['steward']['mwoauthviewprivate'] = true;
 $wgGroupPermissions['steward']['mwoauthviewsuppressed'] = true;
-$wgGroupPermissions['steward']['handle-reports'] = true;
 $wgGroupPermissions['steward']['override-export-depth'] = true;
 $wgGroupPermissions['steward']['renameuser'] = true;
 $wgGroupPermissions['steward']['replacetext'] = true;
@@ -417,7 +415,6 @@ $wgGroupPermissions['sysadmin']['unblockself'] = true;
 $wgGroupPermissions['sysadmin']['userrights'] = true;
 
 /// Non-Steward suppressors
-$wgGroupPermissions['non-stewardsuppress']['handle-reports'] = true;
 $wgGroupPermissions['non-stewardsuppress']['suppressionlog'] = true;
 $wgGroupsAddToSelf['non-stewardsuppress'] = array( 'suppress' );
 $wgGroupsRemoveFromSelf['non-stewardsuppress'] = array( 'suppress' );
