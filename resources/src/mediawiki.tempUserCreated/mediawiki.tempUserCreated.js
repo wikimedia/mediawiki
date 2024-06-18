2aeea2e880c3b7d2dd8256107155b93fad5db4ed
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-06-18 03:59:54 +0200

    Use notification for first-time "Temporary account" message
    We already used it on some skins, now we will use it on all skins.
    
    Bug: T344934
    Change-Id: I50837d7833e554f5f95e81afef5a7d318e8a7fb8

--- resources/src/mediawiki.tempUserCreated/mediawiki.tempUserCreated.js
+++ resources/src/mediawiki.tempUserCreated/mediawiki.tempUserCreated.js
@@ -20,31 +20,11 @@
 			mw.util.getUrl( 'Special:CreateAccount' ),
 			contLangMessages[ 'tempuser-helppage' ]
 		).parseDom();
-
-		var $username = $( '.mw-temp-user-banner-username' );
-		if ( $username.length ) {
-			// If supported by the skin, display a popup anchored to the username in the banner
-			var popup = new OO.ui.PopupWidget( {
-				padded: true,
-				head: true,
-				label: title,
-				$content: $content,
-				$floatableContainer: $username,
-				classes: [ 'postedit-tempuserpopup' ],
-				// Work around T307062
-				position: 'below',
-				autoFlip: false
-			} );
-			$( OO.ui.getTeleportTarget() ).append( popup.$element );
-			popup.toggle( true );
-		} else {
-			// Otherwise display a mw.notify message
-			mw.notify( $content, {
-				title: title,
-				classes: [ 'postedit-tempuserpopup' ],
-				autoHide: false
-			} );
-		}
+		mw.notify( $content, {
+			title: title,
+			classes: [ 'postedit-tempuserpopup' ],
+			autoHide: false
+		} );
 	};
 
 }() );
