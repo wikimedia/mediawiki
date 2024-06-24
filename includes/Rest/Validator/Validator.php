c72674c9ba9d9a907a77f210ef120b0b8329155f
daniel <dkinzler@wikimedia.org>
2024-06-24 19:03:48 +0200

    REST: fix error message for rest-extraneous-body-fields
    List the actual field names
    
    Change-Id: I111e84fc68aa5e204f41fa28cff81f4dcd56ba28

--- includes/Rest/Validator/Validator.php
+++ includes/Rest/Validator/Validator.php
@@ -195,7 +195,7 @@ class Validator {
 			throw new LocalizedHttpException(
 				new MessageValue(
 					'rest-extraneous-body-fields',
-					[ new ListParam( ListType::COMMA, array_keys( $unvalidatedKeys ) ) ]
+					[ new ListParam( ListType::COMMA, $unvalidatedKeys ) ]
 				),
 				400,
 				[ // match fields used by validateBodyParams()
