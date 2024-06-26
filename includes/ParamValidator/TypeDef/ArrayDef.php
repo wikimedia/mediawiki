c7030d5bf89f408855d9a3b21e51b8fbfa1752bb
bpirkle <bpirkle@wikimedia.org>
2024-06-26 15:20:17 -0500

    Add ArrayDef convenience methods for JSON Schema generation
    ArrayDef accepts a PARAM_SCHEMA settings value, which defines the
    JSON Schema accepted for that parameter. Writing JSON Schemas can
    be difficult, so add convenience methods for the most common cases.
    
    Bug: T368131
    Change-Id: I13d3e5d736fb6cb559193a6a0acfa6547dcad461

--- includes/ParamValidator/TypeDef/ArrayDef.php
+++ includes/ParamValidator/TypeDef/ArrayDef.php
@@ -79,4 +79,146 @@ class ArrayDef extends TypeDef {
 
 		return json_encode( $value );
 	}
+
+	/**
+	 * Returns a JSON Schema of type array, with the input schema for each array item.
+	 *
+	 * If $itemSchema is a string, it must be a valid JSON type, and all list entries will be
+	 * validated to be of that type.
+	 *
+	 * If $itemSchema is an array, it must represent a valid schema, and all list entries will
+	 * be validated to be of that schema. Nested lists are supported, as are lists of maps and
+	 * more complicated schemas.
+	 *
+	 * Examples:
+	 *  A list of integers, like [ 1, 2, 3 ]
+	 *   ArrayDef::makeListSchema( "integer" )
+	 *  A list of strings, where each value must be either "a" or "b", like [ "a", "a", "b", "b" ]
+	 * *   ArrayDef::makeListSchema( [ 'enum' => [ 'a', 'b' ] ] )
+	 *  A list of lists of strings, like [ [ "foo", 'bar" ], [ "baz", "qux" ] ]
+	 *   ArrayDef::makeListSchema( ArrayDef::makeListSchema( "string" ) )
+	 *
+	 * @since 1.43
+	 *
+	 * @param array|string $itemSchema
+	 *
+	 * @return array
+	 */
+	public static function makeListSchema( $itemSchema ): array {
+		return [
+			'type' => 'array',
+			'items' => static::normalizeSchema( $itemSchema )
+		];
+	}
+
+	/**
+	 * Returns a JSON Schema of type object, with the input schema for each array item.
+	 *
+	 * If $entrySchema is a string, it must be a valid JSON type, and all map entries will be
+	 * validated to be of that type.
+	 *
+	 * If $entrySchema is an array, it must represent a valid schema, and all map entries will
+	 * be validated to be of that schema. Nested maps are supported, as are maps of lists and
+	 * more complicated schemas
+	 *
+	 * Examples:
+	 *  A map of integers, like [ 'key1' => 1, 'key2' => 2, 'key3' => 3 ]
+	 *   ArrayDef::makeMapSchema( "integer" )
+	 *  A map of where each value must be 0 or 1, like [ 'key1' => 1, 'key2' => 1, 'key3' => 0 ]
+	 *   ArrayDef::makeMapSchema( [ 'enum' => [ 0, 1 ] ] )
+	 *  A map of maps, like [ 'k1' => [ 'k2' => 'a' ], 'k3' => [ 'k4' => 'b', 'k5' => 'c' ] ]
+	 *   ArrayDef::makeMapSchema( ArrayDef::makeMapSchema( "string" ) )
+	 *
+	 * @since 1.43
+	 *
+	 * @param array|string $entrySchema
+	 *
+	 * @return array
+	 */
+	public static function makeMapSchema( $entrySchema ): array {
+		return [
+			'type' => 'object',
+			'additionalProperties' => static::normalizeSchema( $entrySchema )
+		];
+	}
+
+	/**
+	 * Returns a JSON Schema of type object, with properties defined by the function params.
+	 *
+	 * Any input schemas must either be a string corresponding to valid JSON types, or valid
+	 * schemas. Nested schemas are supported.
+	 *
+	 * Examples:
+	 *  An object with required parameters "a" and "b", where "a" must be an integer and "b" can
+	 *  have one of the values "x", "y", or "z", no optional parameters, and additional parameters
+	 *  are disallowed:
+	 *   ArrayDef::makeObjectSchema( [ 'a' => 'integer', 'b' => [ 'enum' => [ 'x', 'y', 'z' ] ] ] )
+	 *  The same object, but parameter "b" is optional:
+	 *   ArrayDef::makeObjectSchema( [ 'a' => 'integer' ], [ 'b' => [ 'enum' => [ 'x', 'y', 'z' ] ] ] )
+	 *  An object with no required properties, an optional property "a" of type string, with
+	 *  arbitrary additional properties allowed (effectively, an arbitrary object, but if "a"
+	 *  is present, it must be a string):
+	 *   ArrayDef::makeObjectSchema( [ ], [ 'a' => 'string' ], true )
+	 * @since 1.43
+	 *
+	 * @param array $required properties that are required to be present, as name/schema pairs
+	 * @param array $optional properties that may or may not be present, as name/schema pairs
+	 * @param array|bool|string $additional schema additional properties must match, or false
+	 * 	to disallow additional properties, or true to allow arbitrary additional properties
+	 *
+	 * @return array
+	 */
+	public static function makeObjectSchema(
+		array $required = [], array $optional = [], $additional = false
+	): array {
+		$schema = [ 'type' => 'object' ];
+
+		if ( $required ) {
+			foreach ( $required as $propertyName => $propertySchema ) {
+				$schema['required'][] = $propertyName;
+				$schema['properties'][$propertyName] = static::normalizeSchema( $propertySchema );
+			}
+		}
+
+		if ( $optional ) {
+			foreach ( $optional as $propertyName => $propertySchema ) {
+				if ( isset( $schema['properties'][$propertyName] ) ) {
+					throw new InvalidArgumentException(
+						"Property {$propertyName} defined as both required and optional"
+					);
+				}
+				$schema['properties'][$propertyName] = static::normalizeSchema( $propertySchema );
+			}
+		}
+
+		// The easiest way to allow all extra properties is to not specify additionalProperties
+		if ( $additional !== true ) {
+			// A value of false disallows additional properties
+			if ( $additional === false ) {
+				$schema['additionalProperties'] = false;
+			} else {
+				$schema['additionalProperties'] = static::normalizeSchema( $additional );
+			}
+		}
+
+		return $schema;
+	}
+
+	/**
+	 * Returns a representation of the input schema
+	 *
+	 * If $schema is a string, it must be a valid JSON type.
+	 * If $schema is an array, it must represent a valid schema.
+	 *
+	 * @param array|string $schema
+	 *
+	 * @return array
+	 */
+	private static function normalizeSchema( $schema ): array {
+		if ( is_array( $schema ) ) {
+			return $schema;
+		} else {
+			return [ 'type' => $schema ];
+		}
+	}
 }
--- tests/phpunit/unit/includes/ParamValidator/TypeDef/ArrayDefTest.php
+++ tests/phpunit/unit/includes/ParamValidator/TypeDef/ArrayDefTest.php
@@ -16,6 +16,26 @@ class ArrayDefTest extends TypeDefUnitTestCase {
 		return new ArrayDef( $callbacks );
 	}
 
+	private function makeValidationException( $schemaSettings, $property, $pointer, $message, $constraint ) {
+		return new ValidationException(
+			DataMessageValue::new(
+				'schema-validation-failed',
+				[ '' ],
+				'schema-validation-failed',
+				[ 'schema-validation-error' => [
+					'property' => $property,
+					'pointer' => $pointer,
+					'message' => $message,
+					'constraint' => $constraint,
+					'context' => 1
+				] ]
+			),
+			'test',
+			[ 'notrequired' => 'value' ],
+			$schemaSettings
+		);
+	}
+
 	public function provideValidate() {
 		yield 'assoc array' => [ [ 'x' => 1 ], [ 'x' => 1 ], ];
 		yield 'indexed array' => [ [ 'x' ], [ 'x' ], ];
@@ -65,22 +85,8 @@ class ArrayDefTest extends TypeDefUnitTestCase {
 			[ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'key1' ] ] ]
 		];
 		$missingSchemaSettings = [ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'required' ] ] ];
-		$schemaValidationFailed = new ValidationException(
-			DataMessageValue::new(
-				'schema-validation-failed',
-				[ '' ],
-				'schema-validation-failed',
-				[ 'schema-validation-error' => [
-					'property' => 'required',
-					'pointer' => '/required',
-					'message' => 'The property required is required',
-					'constraint' => 'required',
-					'context' => 1
-				] ]
-			),
-			'test',
-			[ 'notrequired' => 'value' ],
-			$missingSchemaSettings
+		$schemaValidationFailed = $this->makeValidationException(
+			$missingSchemaSettings, 'required', '/required', 'The property required is required', 'required'
 		);
 
 		yield 'empty object is not valid' => [
@@ -92,6 +98,7 @@ class ArrayDefTest extends TypeDefUnitTestCase {
 					'required' => [ 'required' ],
 				],
 			],
+			[ ArrayDef::PARAM_SCHEMA => [ 'type' => [ 'object' ], 'required' => [ 'required' ] ] ]
 		];
 		yield 'empty array is valid' => [
 			[], [], [ ArrayDef::PARAM_SCHEMA => [ 'type' => 'array' ] ]
@@ -117,5 +124,188 @@ class ArrayDefTest extends TypeDefUnitTestCase {
 				]
 			]
 		];
+	}
+
+	public function provideListSchema() {
+		yield 'simple list of strings' => [
+			'string',
+			[
+				'type' => 'array',
+				'items' => [
+					'type' => 'string'
+				]
+			]
+		];
+
+		yield 'list where each value must be either in an enum' => [
+			[ 'enum' => [ 'a', 'b' ] ],
+			[
+				'type' => 'array',
+				'items' => [ 'enum' => [ 'a', 'b' ] ]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideListSchema
+	 * @param mixed $schema Schema to test
+	 * @param array $expect Expected schema array
+	 */
+	public function testListSchema( $schema, array $expect ) {
+		$paramSchema = ArrayDef::makeListSchema( $schema );
+		$this->assertArrayEquals( $expect, $paramSchema );
+	}
+
+	public function testNestedListSchema() {
+		$expect = [
+			'type' => 'array',
+			'items' => [
+				'type' => 'array',
+				'items' => [
+					'type' => 'string'
+				]
+			]
+		];
+
+		$paramSchema = ArrayDef::makeListSchema( ArrayDef::makeListSchema( 'string' ) );
+		$this->assertArrayEquals( $expect, $paramSchema );
+	}
+
+	public function provideMapSchema() {
+		yield 'simple map of strings' => [
+			'string',
+			[
+				'type' => 'object',
+				'additionalProperties' => [
+					'type' => 'string'
+				]
+			]
+		];
+
+		yield 'map where each value must be in an enum' => [
+			[ 'enum' => [ 0, 1 ] ],
+			[
+				'type' => 'object',
+				'additionalProperties' => [ 'enum' => [ 0, 1 ] ]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideMapSchema
+	 * @param mixed $schema Schema to test
+	 * @param array $expect Expected schema array
+	 */
+	public function testMapSchema( $schema, array $expect ) {
+		$paramSchema = ArrayDef::makeMapSchema( $schema );
+		$this->assertArrayEquals( $expect, $paramSchema );
+	}
+
+	public function testNestedMapSchema() {
+		$expect = [
+			'type' => 'object',
+			'items' => [
+				'type' => 'object',
+				'additionalProperties' => [
+					'type' => 'string'
+				]
+			]
+		];
+
+		$paramSchema = ArrayDef::makeMapSchema( ArrayDef::makeMapSchema( 'string' ) );
+		$this->assertArrayEquals( $expect, $paramSchema );
+	}
+
+	public function provideObjectSchema() {
+		yield 'object with two required properties, one an integer and another an enum' => [
+			[ 'a' => 'integer', 'b' => [ 'enum' => [ 'x', 'y', 'z' ] ] ],
+			[],
+			false,
+			[
+				'type' => 'object',
+				'required' => [ 'a', 'b' ],
+				'properties' => [
+					'a' => [ 'type' => 'integer' ],
+					'b' => [ 'enum' => [ 'x', 'y', 'z' ] ],
+				],
+				'additionalProperties' => false
+			]
+		];
+
+		yield 'object with two properties, one an integer an another an enum, only one is required' => [
+			[ 'a' => 'integer' ],
+			[ 'b' => [ 'enum' => [ 'x', 'y', 'z' ] ] ],
+			false,
+			[
+				'type' => 'object',
+				'required' => [ 'a' ],
+				'properties' => [
+					'a' => [ 'type' => 'integer' ],
+					'b' => [ 'enum' => [ 'x', 'y', 'z' ] ],
+				],
+				'additionalProperties' => false
+			]
+		];
+
+		yield 'object with one optional property of type string, additional properties allowed' => [
+			[],
+			[ 'a' => 'string' ],
+			true,
+			[
+				'type' => 'object',
+				'properties' => [
+					'a' => [ 'type' => 'string' ],
+				]
+			]
+		];
+
+		yield 'object with properties of any name, but all properties must be lists of strings' => [
+			[],
+			[],
+			[
+				'type' => 'array',
+				'items' => [
+					'type' => 'string',
+				],
+			],
+			[
+				'type' => 'object',
+				'additionalProperties' => [
+					'type' => 'array',
+					'items' => [
+						'type' => 'string',
+					]
+				]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideObjectSchema
+	 * @param array $required
+	 * @param array $optional
+	 * @param array|bool|string $additional
+	 * @param array $expect Expected schema array
+	 */
+	public function testObjectSchema( array $required, array $optional, $additional, array $expect ) {
+		$paramSchema = ArrayDef::makeObjectSchema( $required, $optional, $additional );
+		$this->assertArrayEquals( $expect, $paramSchema );
+	}
+
+	public function provideInvalidObjectSchema() {
+		yield 'object with property defined as both required and optional' => [
+			[ 'a' => 'integer' ],
+			[ 'a' => 'integer' ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideInvalidObjectSchema
+	 * @param array $required
+	 * @param array $optional
+	 */
+	public function testInvalidObjectSchema( array $required, array $optional ) {
+		$this->expectException( \InvalidArgumentException::class );
+		ArrayDef::makeObjectSchema( $required, $optional, false );
 	}
 }
