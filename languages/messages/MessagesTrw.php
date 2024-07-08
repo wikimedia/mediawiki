96c23fe7ba3a1be0b5d963eaf27e42dcfb87a640
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-07-08 19:50:49 -0400

    Add a MessagesTrw.php file
    Minimal support for this language to
    allow right to left reading and writing.
    Similar to MessagesPhr.php.
    
    Bug: T369578
    Change-Id: I977c8aa3964ef0d04afda46ab29609d0c7ce5594

---
+++ languages/messages/MessagesTrw.php
@@ -0,0 +1,11 @@
+<?php
+/** Torwali (توروالی)
+ *
+ * Limited support language without full localization yet.
+ * This file only exists to provide correct content direction.
+ *
+ * @file
+ * @ingroup Languages
+ */
+
+$rtl = true;
