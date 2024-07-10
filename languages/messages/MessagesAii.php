1b9f830cabd4a8b588ca00e2b36ba56a30792794
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-07-10 11:33:36 -0400

    Add MessagesAii.php for correct content direction
    Bug: T369727
    Change-Id: I1845d3949eecfb66593c422031ba50a180b56718

---
+++ languages/messages/MessagesAii.php
@@ -0,0 +1,11 @@
+<?php
+/** Assyrian Neo-Aramaic (ܣܘܪܝܬ)
+ *
+ * Limited support language without full localization yet.
+ * This file only exists to provide correct content direction.
+ *
+ * @file
+ * @ingroup Languages
+ */
+
+$rtl = true;
