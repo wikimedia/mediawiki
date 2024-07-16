wikimedia/mediawiki

commit e4078e9940c32ced3cf9eea059fe6f081449de42

LinkSearch: Change default protocol to http:// and https:// in READ_NEW
Now that el_to_domain is much smaller and indexed, this shouldn't be
taxing on the database anymore.

It's not perfect but it works beautifully.

Bug: T14810
Change-Id: I123662f40f6efbfd24f280984cd824ced6892840

includes/specials/SpecialLinkSearch.php
@@ -102,7 +102,7 @@ class SpecialLinkSearch extends QueryPage {
 
 		$target2 = Parser::normalizeLinkUrl( $target );
 		// Get protocol, default is http://
-		$protocol = 'http://';
+		$protocol = null;
 		$bits = $this->urlUtils->parse( $target );
 		if ( isset( $bits['scheme'] ) && isset( $bits['delimiter'] ) ) {
 			$protocol = $bits['scheme'] . $bits['delimiter'];

includes/ExternalLinks/LinkFilter.php
@@ -29,6 +29,7 @@ use StringUtils;
 use TextContent;
 use Wikimedia\IPUtils;
 use Wikimedia\Rdbms\LikeMatch;
+use Wikimedia\Rdbms\Platform\ISQLPlatform;
 
 /**
  * Some functions to help implement an external link filter for spam control.
@@ -343,33 +344,44 @@ class LinkFilter {
 			return self::getQueryConditionsOld( $filterEntry, $options );
 		}
 		$options += [
-			'protocol' => 'http://',
+			'protocol' => [ 'http://', 'https://' ],
 			'oneWildcard' => false,
 			'db' => null,
 		];
 
-		// First, get the like array
-		$like = self::makeLikeArray( $filterEntry, $options['protocol'] );
-		if ( $like === false ) {
-			return $like;
+		if ( is_string( $options['protocol'] ) ) {
+			$options['protocol'] = [ $options['protocol'] ];
+		} elseif ( $options['protocol'] === null ) {
+			$options['protocol'] = [ 'http://', 'https://' ];
 		}
-		[ $likeDomain, $likePath ] = $like;
 
-		// Get the constant prefix (i.e. everything up to the first wildcard)
-		$trimmedlikeDomain = self::keepOneWildcard( $likeDomain );
-		$trimmedlikePath = self::keepOneWildcard( $likePath );
-		if ( $trimmedlikeDomain[count( $trimmedlikeDomain ) - 1] instanceof LikeMatch ) {
-			array_pop( $trimmedlikeDomain );
+		$domainConditions = [];
+		$db = $options['db'] ?: wfGetDB( DB_REPLICA );
+		foreach ( $options['protocol'] as $protocol ) {
+			$like = self::makeLikeArray( $filterEntry, $protocol );
+			if ( $like === false ) {
+				continue;
+			}
+			[ $likeDomain, $likePath ] = $like;
+			$trimmedlikeDomain = self::keepOneWildcard( $likeDomain );
+			if ( $trimmedlikeDomain[count( $trimmedlikeDomain ) - 1] instanceof LikeMatch ) {
+				array_pop( $trimmedlikeDomain );
+			}
+			$index1 = implode( '', $trimmedlikeDomain );
+			$domainConditions[] = "el_to_domain_index" . $db->buildLike( $index1, $db->anyString() );
 		}
+		if ( !$domainConditions ) {
+			return false;
+		}
+		// @phan-suppress-next-line PhanPossiblyUndeclaredVariable
+		$trimmedlikePath = self::keepOneWildcard( $likePath );
 		if ( $trimmedlikePath[count( $trimmedlikePath ) - 1] instanceof LikeMatch ) {
 			array_pop( $trimmedlikePath );
 		}
-		$db = $options['db'] ?: wfGetDB( DB_REPLICA );
-		$index1 = implode( '', $trimmedlikeDomain );
 		$index2 = implode( '', $trimmedlikePath );
 
 		return [
-			"el_to_domain_index" . $db->buildLike( $index1, $db->anyString() ),
+			$db->makeList( $domainConditions, ISQLPlatform::LIST_OR ),
 			"el_to_path" . $db->buildLike( $index2, $db->anyString() ),
 		];
 	}
@@ -380,6 +392,10 @@ class LinkFilter {
 			'oneWildcard' => false,
 			'db' => null,
 		];
+
+		if ( $options['protocol'] === null ) {
+			$options['protocol'] = 'http://';
+		}
 
 		// First, get the like array
 		$like = self::makeLikeArray( $filterEntry, $options['protocol'] );

tests/phpunit/includes/LinkFilterTest.php
@@ -566,7 +566,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'example.com',
 				[],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE \'/%\' ESCAPE \'`\' ',
 				],
 			],
@@ -574,7 +575,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'example.com/foobar',
 				[],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE \'/foobar%\' ESCAPE \'`\' ',
 				],
 			],
@@ -582,7 +584,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'*.example.com',
 				[],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE \'/%\' ESCAPE \'`\' ',
 				],
 			],
@@ -590,7 +593,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'*.example.com/foobar',
 				[],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE \'/foobar%\' ESCAPE \'`\' ',
 				],
 			],
@@ -598,7 +602,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'*.example.com/foobar',
 				[ 'oneWildcard' => true ],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE \'/foobar%\' ESCAPE \'`\' ',
 				],
 			],
@@ -606,7 +611,8 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'example.com/blah/blah/blah/blah/blah/blah/blah/blah/blah/blah?foo=',
 				[],
 				[
-					'el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ',
+					'(el_to_domain_index LIKE \'http://com.example.%\' ESCAPE \'`\' ) OR ' .
+					'(el_to_domain_index LIKE \'https://com.example.%\' ESCAPE \'`\' )',
 					'el_to_path LIKE ' .
 					'\'/blah/blah/blah/blah/blah/blah/blah/blah/blah/blah?foo=%\' ' .
 					'ESCAPE \'`\' ',
@@ -621,7 +627,7 @@ class LinkFilterTest extends MediaWikiLangTestCase {
 				'example.com',
 				[ 'protocol' => 'https://' ],
 				[
-					"el_to_domain_index LIKE 'https://com.example.%' ESCAPE '`' ",
+					"(el_to_domain_index LIKE 'https://com.example.%' ESCAPE '`' )",
 					"el_to_path LIKE '/%' ESCAPE '`' ",
 				],
 			],

commit 6da5ac4b2a53ed2e93554a5a265d5aedc823a278

specials: Build mungedQuery with default protocol on Special:LinkSearch
When no protocol is given, assume https:// and http://,
for the munged query part this was broken since e4078e9940

Bug: T369813
Follow-Up: I123662f40f6efbfd24f280984cd824ced6892840
Change-Id: I0d1bbb36c5d3deb98d45055bff32f7616bce9d5c

includes/specials/SpecialLinkSearch.php
@@ -188,9 +188,16 @@ class SpecialLinkSearch extends QueryPage {
 			'urlpath' => 'el_to_path'
 		];
 		if ( $this->mQuery === '*' && $this->mProt !== '' ) {
-			$this->mungedQuery = [
-				$dbr->expr( $field, IExpression::LIKE, new LikeValue( $this->mProt, $dbr->anyString() ) ),
-			];
+			if ( $this->mProt !== null ) {
+				$this->mungedQuery = [
+					$dbr->expr( $field, IExpression::LIKE, new LikeValue( $this->mProt, $dbr->anyString() ) ),
+				];
+			} else {
+				$this->mungedQuery = [
+					$dbr->expr( $field, IExpression::LIKE, new LikeValue( 'http://', $dbr->anyString() ) )
+						->or( $field, IExpression::LIKE, new LikeValue( 'https://', $dbr->anyString() ) ),
+				];
+			}
 		} else {
 			$this->mungedQuery = LinkFilter::getQueryConditions( $this->mQuery, [
 				'protocol' => $this->mProt,