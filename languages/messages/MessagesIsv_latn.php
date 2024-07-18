6340a6f1f271265b00c9067ca274e5d086e71b82
Srishakatux <ssethi@wikimedia.org>
2024-07-17 18:42:16 -0700

    Add namespace translations for isv-latn
    Based on translations provided here: https://translatewiki.net/wiki/User_talk:IJzeren_Jan#Namespaces
    
    Bug: T367688
    Change-Id: I9aec55d47be7ca85ad82ed3ea97645107ad8998a

---
+++ languages/messages/MessagesIsv_latn.php
@@ -0,0 +1,23 @@
+<?php
+/** Interslavic (Latin script) (medžuslovjansky)
+ *
+ * @file
+ * @ingroup Languages
+ */
+
+$namespaceNames = [
+	NS_TALK             => 'Besěda',
+	NS_USER             => 'Koristnik',
+	NS_USER_TALK        => 'Besěda_s_koristnikom',
+	NS_PROJECT_TALK     => 'Besěda_o_$1',
+	NS_FILE             => 'Fajl',
+	NS_FILE_TALK        => 'Besěda_o_fajlu',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Besěda_o_MediaWiki',
+	NS_TEMPLATE         => 'Šablon',
+	NS_TEMPLATE_TALK    => 'Besěda_o_šablonu',
+	NS_HELP             => 'Pomoč',
+	NS_HELP_TALK        => 'Besěda_o_pomoči',
+	NS_CATEGORY         => 'Kategorija',
+	NS_CATEGORY_TALK    => 'Besěda_o_kategoriji',
+];
