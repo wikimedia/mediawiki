e76297010b3e7566d28981cde0c3da36cc487107
Umherirrender <umherirrender_de.wp@web.de>
2024-07-20 22:44:35 +0200

    tests: Use LanguageFactory in FormatMetadataTest
    Bug: T343771
    Change-Id: Ib390ffe80ffd62b7f22efe86f6a08108959a970c

--- tests/phpunit/includes/media/FormatMetadataTest.php
+++ tests/phpunit/includes/media/FormatMetadataTest.php
@@ -147,16 +147,16 @@ class FormatMetadataTest extends MediaWikiMediaTestCase {
 	/**
 	 * @covers \FormatMetadata::getPriorityLanguages
 	 * @dataProvider provideGetPriorityLanguagesData
-	 * @param string $languageClass
+	 * @param string $language
 	 * @param string[] $expected
 	 */
 	public function testGetPriorityLanguagesInternal_language_expect(
-		string $languageClass,
+		string $language,
 		array $expected
 	): void {
 		$formatMetadata = TestingAccessWrapper::newFromObject( new FormatMetadata() );
 		$context = $formatMetadata->getContext();
-		$context->setLanguage( new $languageClass() );
+		$context->setLanguage( $this->getServiceContainer()->getLanguageFactory()->getLanguage( $language ) );
 
 		$x = $formatMetadata->getPriorityLanguages();
 		$this->assertSame( $expected, $x );
@@ -165,15 +165,15 @@ class FormatMetadataTest extends MediaWikiMediaTestCase {
 	public static function provideGetPriorityLanguagesData() {
 		return [
 			'LanguageMl' => [
-				LanguageMl::class,
+				'ml',
 				[ 'ml', 'en' ],
 			],
 			'LanguageEn' => [
-				LanguageEn::class,
+				'en',
 				[ 'en', 'en' ],
 			],
 			'LanguageQqx' => [
-				LanguageQqx::class,
+				'qqx',
 				[ 'qqx', 'en' ],
 			],
 		];
