738bc855913926291e9671fbfd19aec93b0f17dd
Cole White <cwhite@wikimedia.org>
2024-07-23 13:56:35 +0000

    Stats: combine testSendGaugeMetric into testSendMetrics
    * Removes testSendGaugeMetric
    * Ensure we're checking for calls to timing, gauge, and updateCount
    * Tune expectations to exactly how many times we expect the calls
    
    Bug: T370636
    Change-Id: I73f05b1876e3afbf0995d5605cb17fa5dc6146d2

--- tests/phpunit/unit/includes/libs/Stats/StatsEmitterTest.php
+++ tests/phpunit/unit/includes/libs/Stats/StatsEmitterTest.php
@@ -21,7 +21,9 @@ class StatsEmitterTest extends TestCase {
 	public function testSendMetrics() {
 		// set up a mock statsd data factory
 		$statsd = $this->createMock( IBufferingStatsdDataFactory::class );
-		$statsd->expects( $this->atLeastOnce() )->method( "updateCount" );
+		$statsd->expects( $this->exactly( 3 ) )->method( "updateCount" );
+		$statsd->expects( $this->once() )->method( "gauge" );
+		$statsd->expects( $this->once() )->method( "timing" );
 
 		// initialize cache
 		$cache = new StatsCache();
@@ -37,7 +39,11 @@ class StatsEmitterTest extends TestCase {
 		$transport = $this->createMock( UDPTransport::class );
 		$transport->expects( $this->once() )->method( "emit" )
 			->with(
-				"mediawiki.test.bar:1|c\nmediawiki.test.bar:1|c\nmediawiki.test.foo:3.14|ms\nmediawiki.test.stats_buffered_total:3|c\n"
+				"mediawiki.test.bar:1|c\n" .
+				"mediawiki.test.bar:1|c\n" .
+				"mediawiki.test.foo:3.14|ms\n" .
+				"mediawiki.test.gauge:1|g\n" .
+				"mediawiki.test.stats_buffered_total:4|c\n"
 			);
 		$emitter = $emitter->withTransport( $transport );
 
@@ -53,52 +59,14 @@ class StatsEmitterTest extends TestCase {
 		// fetch same metric from cache and use it
 		$metric = $m->getCounter( 'bar' );
 		$metric->increment();
-		$metric = $m->getTiming( 'foo' );
+		$metric = $m->getTiming( 'foo' )->copyToStatsdAt( 'test.metric.timing' );
 		$metric->observe( 3.14 );
 
 		// name collision: gauge should not appear in output nor throw exception
 		$metric = @$m->getGauge( 'bar' );
 		$metric->set( 42 );
 
-		// send metrics
-		$m->flush();
-	}
-
-	public function testSendGaugeMetric() {
-		// set up a mock statsd data factory
-		$statsd = $this->createMock( IBufferingStatsdDataFactory::class );
-		$statsd->expects( $this->atLeastOnce() )->method( "gauge" );
-
-		// initialize cache
-		$cache = new StatsCache();
-
-		// emitter
-		$emitter = OutputFormats::getNewEmitter(
-			'mediawiki',
-			$cache,
-			OutputFormats::getNewFormatter( OutputFormats::DOGSTATSD )
-		);
-
-		// transport
-		$transport = $this->createMock( UDPTransport::class );
-		$transport->expects( $this->once() )->method( "emit" )
-			->with(
-				"mediawiki.test.bar:1|g\nmediawiki.test.bar:2|g\nmediawiki.test.stats_buffered_total:2|c\n"
-			);
-		$emitter = $emitter->withTransport( $transport );
-
-		// initialize metrics factory
-		$m = new StatsFactory( $cache, $emitter, new NullLogger, 'test' );
-
-		// inject statsd factory
-		$m->withStatsdDataFactory( $statsd );
-
-		// populate metric with statsd copy
-		$m->getGauge( 'bar' )->copyToStatsdAt( 'test.metric' )->set( 1 );
-
-		// fetch same metric from cache and use it
-		$metric = $m->getGauge( 'bar' );
-		$metric->set( 2 );
+		$m->getGauge( 'gauge' )->copyToStatsdAt( 'test.metric.gauge' )->set( 1 );
 
 		// send metrics
 		$m->flush();
