4ed379cf8ae70efbe3eadbd13f49d83b089f0cf8
Derick Alangi <alangiderick@gmail.com>
2024-07-24 23:58:05 +0200

    CommentStore: Typehint param in CommentStoreComment constructor
    This had caused issues in the past like: T355751 but after doing a
    recent check of production for these warnings for the last 6 months,
    I don't seem to see anything.
    
    Maybe they've all been addressed so let's try again and see how it
    goes.
    
    Change-Id: Iaffe22ae420f043b8e71386178c0a8619bc85349

--- includes/CommentStore/CommentStoreComment.php
+++ includes/CommentStore/CommentStoreComment.php
@@ -54,16 +54,9 @@ class CommentStoreComment {
 	 * @param Message|null $message
 	 * @param array|null $data
 	 */
-	public function __construct( $id, $text, Message $message = null, array $data = null ) {
-		if ( $text === null ) {
-			// TODO: Turn this warning into a proper type hint once we have
-			// found and fixed any offenders (T355751).
-			wfLogWarning( 'Comment text should not be null!' );
-			$text = '';
-		}
-
+	public function __construct( $id, string $text, Message $message = null, array $data = null ) {
 		$this->id = (int)$id;
-		$this->text = (string)$text;
+		$this->text = $text;
 		$this->message = $message
 			?: new RawMessage(
 				'$1',
