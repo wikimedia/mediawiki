f6222ec9a73f779405f4303972685a538728fb96
Thalia <thalia.e.chan@googlemail.com>
2024-08-01 19:51:28 +0100

    Improve test coverage of DeletedContribsPager
    Why:
    
    * In T370438 we are making extensive changes to DeletedContribsPager.
      Ahead of this, we should ensure good test coverage.
    
    What:
    
    * Add some integration tests to DeletedContribsPagerTest.
    * Also update coverage annotations to use the recommended style.
    
    Bug: T370438
    Bug: T354968
    Change-Id: I6538b787c784efcd0f13dd1d55d38907d80c9f5c

--- tests/phpunit/includes/specials/DeletedContribsPagerTest.php
+++ tests/phpunit/includes/specials/DeletedContribsPagerTest.php
@@ -6,13 +6,17 @@ use MediaWiki\Context\RequestContext;
 use MediaWiki\HookContainer\HookContainer;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\Pager\DeletedContribsPager;
+use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\RevisionStore;
 use Wikimedia\Rdbms\IConnectionProvider;
 
 /**
  * @group Database
+ * @covers \MediaWiki\Pager\DeletedContribsPager
  */
 class DeletedContribsPagerTest extends MediaWikiIntegrationTestCase {
+	private static User $user;
+
 	/** @var DeletedContribsPager */
 	private $pager;
 
@@ -65,8 +69,6 @@ class DeletedContribsPagerTest extends MediaWikiIntegrationTestCase {
 	 * Flow uses DeletedContribsPager::reallyDoQuery hook to provide something other then
 	 * stdClass as a row, and then manually formats its own row in ContributionsLineEnding.
 	 * Emulate this behaviour and check that it works.
-	 *
-	 * @covers \MediaWiki\Pager\DeletedContribsPager::formatRow
 	 */
 	public function testDeletedContribProvidedByHook() {
 		$this->setTemporaryHook( 'DeletedContribsPager::reallyDoQuery', static function ( &$data ) {
@@ -84,5 +86,75 @@ class DeletedContribsPagerTest extends MediaWikiIntegrationTestCase {
 		} );
 		$pager = $this->getDeletedContribsPager();
 		$this->assertStringContainsString( 'FROM_HOOK!', $pager->getBody() );
+	}
+
+	public static function provideEmptyResultIntegration() {
+		$cases = [
+			[ 'target' => '', 'namespace' => '' ],
+			[ 'target' => '127.0.0.1', 'namespace' => '' ],
+			[ 'target' => 'UserWithNoEdits', 'namespace' => 1 ],
+		];
+		foreach ( $cases as $case ) {
+			yield [ $case ];
+		}
+	}
+
+	/**
+	 * Confirm that the query is valid for various filter options.
+	 *
+	 * @dataProvider provideEmptyResultIntegration
+	 */
+	public function testEmptyResultIntegration( $options ) {
+		$pager = $this->getDeletedContribsPager(
+			$options['target'],
+			$options['namespace'],
+		);
+		$this->assertIsString( $pager->getBody() );
+		$this->assertSame( 0, $pager->getNumRows() );
+	}
+
+	public function testPopulatedIntegrationNoPermissions() {
+		$pager = $this->getDeletedContribsPager( self::$user->getName() );
+
+		$this->assertIsString( $pager->getBody() );
+		$this->assertSame( 1, $pager->getNumRows() );
+	}
+
+	public function testPopulatedIntegrationWithPermissions() {
+		$this->setGroupPermissions( [ '*' => [
+			'deletedhistory' => true,
+			'deletedtext' => true,
+			'undelete' => true,
+		] ] );
+
+		$pager = $this->getDeletedContribsPager( self::$user->getName() );
+		$this->assertIsString( $pager->getBody() );
+		$this->assertSame( 2, $pager->getNumRows() );
+	}
+
+	public function addDBDataOnce() {
+		self::$user = $this->getTestUser()->getUser();
+		$title = Title::makeTitle( NS_MAIN, 'DeletedContribsPagerTest' );
+
+		// Make two edits (one will be suppressed)
+		$this->editPage( $title, '', '', NS_MAIN, self::$user );
+		$status = $this->editPage( $title, 'Test content.', '', NS_MAIN, self::$user );
+
+		// Delete the page where the edits were made
+		$page = $this->getServiceContainer()->getWikiPageFactory()->newFromTitle( $title );
+		$this->deletePage( $page );
+
+		// Suppress the second edit
+		$this->getDb()->newUpdateQueryBuilder()
+			->update( 'archive' )
+			->set( [
+				'ar_deleted' => RevisionRecord::DELETED_USER | RevisionRecord::DELETED_TEXT,
+				// This is to ensure the minor edits path doesn't encounter an error
+				'ar_minor_edit' => 1,
+			] )
+			->where( [
+				'ar_rev_id' => $status->getNewRevision()->getId()
+			] )
+			->execute();
 	}
 }
