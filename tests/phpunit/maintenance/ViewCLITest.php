37a966ec66c33e5ef3f2e7657a91353c4411a3c3
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2024-08-02 11:27:29 +0100

    Test view.php
    Why:
    * The maintenance scripts in core are mostly untested and testing
      the less complex scripts will improve the test coverage in core.
    
    What:
    * Add an integration test for view.php
    
    Bug: T371167
    Change-Id: I2c77e3832fe326b4c2602217974e8ab0f8d193fb

---
+++ tests/phpunit/maintenance/ViewCLITest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MediaWiki\Tests\Maintenance;
+
+use MediaWiki\Content\TextContent;
+use MediaWiki\Revision\RevisionRecord;
+use ViewCLI;
+
+/**
+ * @covers \ViewCLI
+ * @group Database
+ * @author Dreamy Jazz
+ */
+class ViewCLITest extends MaintenanceBaseTestCase {
+
+	protected function getMaintenanceClass() {
+		return ViewCLI::class;
+	}
+
+	public function testExecute() {
+		$testPage = $this->getExistingTestPage();
+		// Call ::execute
+		$this->maintenance->setArg( 'title', $testPage );
+		$this->maintenance->execute();
+		// Verify that the content of the last revision of the page was outputted.
+		$content = $testPage->getContent( RevisionRecord::RAW );
+		$this->assertInstanceOf( TextContent::class, $content );
+		$expectedOutput = $content->getText();
+		$this->expectOutputString( $expectedOutput );
+	}
+
+	/** @dataProvider provideExecuteForInvalidTitles */
+	public function testExecuteForInvalidTitles( string $title, string $expectedOutputRegex ) {
+		$this->expectCallToFatalError();
+		$this->expectOutputRegex( $expectedOutputRegex );
+		// Call ::execute
+		$this->maintenance->setArg( 'title', $title );
+		$this->maintenance->execute();
+	}
+
+	public static function provideExecuteForInvalidTitles() {
+		return [
+			'Empty title' => [ '', '/Invalid title/' ],
+			'Special page title' => [ 'Special:Test', '/Special Pages not supported/' ],
+			'Non-existent title' => [ 'Non-existing-test-page-1234', '/Page does not exist/' ],
+		];
+	}
+}
