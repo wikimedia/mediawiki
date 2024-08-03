be9bb3e048f049065bb219bd9eeba579db09cc4b
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-08-03 13:01:02 -0400

    Add Nupe (nup) namespace translations
    Contributed by User:Misbahu umar at
    https://translatewiki.net/w/i.php?title=User_talk:Misbahu_umar&oldid=12560109#Namespaces
    
    Bug: T370123
    Change-Id: Ied715350350606bd09887af7abf77ad34edf2ab1

---
+++ languages/messages/MessagesNup.php
@@ -0,0 +1,28 @@
+<?php
+/** Nupe (Nupe)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Misbahu Umar
+ * @author Amir E. Aharoni
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Gbyagulazhi',
+	NS_SPECIAL          => 'Kayin',
+	NS_TALK             => 'Egan',
+	NS_USER             => 'Ezaloci',
+	NS_USER_TALK        => 'Egan_ezaloci',
+	NS_PROJECT_TALK     => 'Egan_$1',
+	NS_FILE             => 'Gbofa',
+	NS_FILE_TALK        => 'Egan_gbofa',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Egan_MediaWiki',
+	NS_TEMPLATE         => 'Nyanga',
+	NS_TEMPLATE_TALK    => 'Egan_nyanga',
+	NS_HELP             => 'Banjire',
+	NS_HELP_TALK        => 'Egan_banjire',
+	NS_CATEGORY         => 'Ekpo',
+	NS_CATEGORY_TALK    => 'Egan_ekpo',
+];
