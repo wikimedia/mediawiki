b65be1c0e198cf4ee2f32af52693250af6ad5d34
Thalia <thalia.e.chan@googlemail.com>
2024-08-05 17:26:30 +0100

    Improve test coverage of SpecialDeletedContributions
    Why:
    
    * In T370438 we are refactoring SpecialDeletedContributions to
      extend ContributionsSpecialPage, a parent class shared with
      other special pages showing contributions. Ahead of this, we
      should ensure good test coverage to reduce the risk of these
      changes introducing bugs.
    
    What:
    
    * Add some tests for the main functionality of the
      Special:DeletedContributions page.
    * Coverage is not 100% becuase there are a some blocks of code
      that will be removed from SpecialDeletedContributions that
      are the same as code that will now be run from the parent
      instead, and are already covered by existing tests.
    
    Bug: T370438
    Change-Id: I4d5bda9beb25f4b355a6013159363eb5be4d13a3

--- tests/phpunit/includes/specials/SpecialDeletedContributionsTest.php
+++ tests/phpunit/includes/specials/SpecialDeletedContributionsTest.php
@@ -7,6 +7,8 @@ use MediaWiki\Specials\SpecialDeletedContributions;
  * @covers \MediaWiki\Specials\SpecialDeletedContributions
  */
 class SpecialDeletedContributionsTest extends SpecialPageTestBase {
+	private static User $sysop;
+
 	protected function newSpecialPage(): SpecialDeletedContributions {
 		$services = $this->getServiceContainer();
 
@@ -24,14 +26,50 @@ class SpecialDeletedContributionsTest extends SpecialPageTestBase {
 		);
 	}
 
-	public function testExecuteNoResults() {
-		$sysop = $this->getTestSysop()->getUser();
+	public function testExecuteNoTarget() {
 		[ $html ] = $this->executeSpecialPage(
-			$sysop->getName(),
+			'',
 			null,
 			null,
-			$sysop,
+			self::$sysop,
 		);
 		$this->assertStringNotContainsString( 'mw-pager-body', $html );
+	}
+
+	public function testExecuteNoResults() {
+		[ $html ] = $this->executeSpecialPage(
+			'127.0.0.1',
+			null,
+			null,
+			self::$sysop,
+		);
+		$this->assertStringNotContainsString( 'mw-pager-body', $html );
+	}
+
+	public function testExecuteNamespaceFilter() {
+		[ $html ] = $this->executeSpecialPage(
+			self::$sysop->getName(),
+			new FauxRequest( [
+				'namespace' => NS_TALK,
+			] ),
+			null,
+			self::$sysop,
+		);
+		$this->assertStringContainsString( 'mw-pager-body', $html );
+	}
+
+	public function addDBDataOnce() {
+		self::$sysop = $this->getTestSysop()->getUser();
+
+		$title = Title::makeTitle( NS_MAIN, 'DeletedContribsPagerTest' );
+		$title = Title::makeTitle( NS_TALK, 'DeletedContribsPagerTest' );
+
+		// Make two edits (one will be suppressed)
+		$this->editPage( $title, '', '', NS_MAIN, self::$sysop );
+		$status = $this->editPage( $title, 'Test content.', '', NS_MAIN, self::$sysop );
+
+		// Delete the page where the edits were made
+		$page = $this->getServiceContainer()->getWikiPageFactory()->newFromTitle( $title );
+		$this->deletePage( $page );
 	}
 }
