322d198d76e79c9ff62a54c1957ff0d387c82d97
Jon Harald Søby <jhsoby@gmail.com>
2024-08-08 15:09:13 +0200

    Add namespace translations for Xhosa and Swazi
    Add namespace submitted by User:Bobbyshabangu during the
    Wikimania hackathon.
    
    Bug: T372057
    Change-Id: I1d7ea7f70f306e9df1993f942743c334e9eedc19

---
+++ languages/messages/MessagesSs.php
@@ -0,0 +1,27 @@
+<?php
+/** Swazi (siSwati)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Bobbyshabangu
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Tinkundla',
+	NS_SPECIAL          => 'Lelibalulekile',
+	NS_TALK             => 'Khuluma',
+	NS_USER             => 'Bantfu',
+	NS_USER_TALK        => 'Khuluma_nebantfu',
+	NS_PROJECT_TALK     => 'Khuluma_nge$1',
+	NS_FILE             => 'Lifayela',
+	NS_FILE_TALK        => 'Khuluma_ngelifayela',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Khuluma_ngeMediaWiki',
+	NS_TEMPLATE         => 'iThempleti',
+	NS_TEMPLATE_TALK    => 'Khuluma_ngethempleti',
+	NS_HELP             => 'Lusito',
+	NS_HELP_TALK        => 'Khuluma_ngelusito',
+	NS_CATEGORY         => 'Umkhakha',
+	NS_CATEGORY_TALK    => 'Khuluma_ngemkhakha',
+];
