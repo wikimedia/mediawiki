wikimedia/mediawiki

commit c106f679b03ca221fcee26f1a0f5aff40381e3f5

Make blockUsers.php capable of both blocking and unblocking
Bug: T248640
Change-Id: I0f8ae29e04f563060ab0edea0f0478698f47adcb

maintenance/blockUsers.php
@@ -22,7 +22,6 @@
 
 require_once __DIR__ . '/Maintenance.php';
 
-use MediaWiki\Block\DatabaseBlock;
 use MediaWiki\MediaWikiServices;
 
 class BlockUsers extends Maintenance {
@@ -36,7 +35,8 @@ class BlockUsers extends Maintenance {
 			"list you've generated with an SQL query or similar.\n\n" .
 			'All users are hard blocked, auto blocked from any current and subsequent IP ' .
 			'addresses, email disabled, unable to write to their user page and unable to ' .
-			'create further accounts with no expiry to this block.'
+			'create further accounts with no expiry to this block. You can change the expiry ' .
+			'with --expiry parameter.'
 		);
 
 		$this->addArg(
@@ -48,7 +48,7 @@ class BlockUsers extends Maintenance {
 		$this->addOption(
 			'performer',
 			'User to make the blocks',
-			true,
+			false,
 			true
 		);
 
@@ -65,123 +65,88 @@ class BlockUsers extends Maintenance {
 			false,
 			false
 		);
-	}
 
-	public function execute() {
-		$performerName = $this->getOption( 'performer' );
-		$performer = User::newFromName( $performerName );
+		$this->addOption(
+			'expiry',
+			'Expiry of your block',
+			false,
+			true
+		);
 
-		if ( !$performer ) {
-			$this->fatalError( "Username '{$performerName}' isn't valid.\n" );
-		}
-
-		if ( !$performer->getId() ) {
-			$this->fatalError( "User '{$performerName}' doesn't exist.\n" );
-		}
-
-		$permManager = MediaWikiServices::getInstance()->getPermissionManager();
-		if ( !$permManager->userHasRight( $performer, 'block' ) ) {
-			$this->fatalError( "User '{$performerName}' doesn't have blocking rights.\n" );
-		}
-
-		$reblock = $this->hasOption( 'reblock' );
-
-		$users = null;
-		if ( $this->hasArg( 0 ) ) {
-			$users = explode(
-				"\n",
-				trim( file_get_contents( $this->getArg( 0 ) ) )
-			);
-		} else {
-			$stdin = $this->getStdin();
-			$users = [];
-			while ( !feof( $stdin ) ) {
-				$name = trim( fgets( $stdin ) );
-				if ( $name ) {
-					$users[] = $name;
-				}
-			}
-		}
-
-		$this->blockUsers(
-			$users,
-			$performer,
-			$this->getOption( 'reason', '' ),
-			$reblock
+		$this->addOption(
+			'unblock',
+			'Should this unblock?',
+			false,
+			false
 		);
 	}
 
-	/**
-	 * @param string[] $users
-	 * @param User $performer
-	 * @param string $reason
-	 * @param bool $reblock
-	 * @throws MWException
-	 */
-	private function blockUsers( $users, $performer, $reason, $reblock ) {
-		$blockStore = MediaWikiServices::getInstance()->getDatabaseBlockStore();
+	public function execute() {
+		$performerName = $this->getOption( 'performer', false );
+		$reason = $this->getOption( 'reason', '' );
+		$unblocking = $this->getOption( 'unblock', false );
+		$reblock = $this->hasOption( 'reblock' );
+		$expiry = $this->getOption( 'expiry', 'indefinite' );
 
-		foreach ( $users as $name ) {
-			$user = User::newFromName( $name );
+		$performer = null;
+		if ( $this->hasOption( 'performer' ) ) {
+			$performer = User::newFromName( $performerName );
+		} else {
+			$performer = User::newSystemUser( 'Maintenance script', [ 'steal' => true ] );
+		}
 
-			// User is invalid, skip
-			if ( !$user || !$user->getId() ) {
-				$this->output( "Blocking '{$name}' skipped (user doesn't exist or is invalid).\n" );
+		if ( $performer === null ) {
+			$this->fatalError( "Unable to parse performer's username" );
+		}
+
+		if ( $this->hasArg( 0 ) ) {
+			$file = fopen( $this->getArg( 0 ), 'r' );
+		} else {
+			$file = $this->getStdin();
+		}
+
+		# Setup
+		if ( !$file ) {
+			$this->fatalError( "Unable to read file, exiting" );
+		}
+
+		# Handle each entry
+		$blockUserFactory = MediaWikiServices::getInstance()->getBlockUserFactory();
+		$unblockUserFactory = MediaWikiServices::getInstance()->getUnblockUserFactory();
+		$action = $unblocking ? "Unblocking" : "Blocking";
+		for ( $linenum = 1; !feof( $file ); $linenum++ ) {
+			$line = trim( fgets( $file ) );
+			if ( $line == '' ) {
 				continue;
 			}
 
-			$priorBlock = DatabaseBlock::newFromTarget( $user );
-			if ( $priorBlock === null ) {
-				$block = new DatabaseBlock();
-			} elseif ( $reblock ) {
-				$block = clone $priorBlock;
+			if ( $unblocking ) {
+				$res = $unblockUserFactory->newUnblockUser(
+					$line,
+					$performer,
+					$reason
+				)->unblockUnsafe();
 			} else {
-				$this->output( "Blocking '{$name}' skipped (user already blocked).\n" );
-				continue;
+				$res = $blockUserFactory->newBlockUser(
+					$line,
+					$performer,
+					$expiry,
+					$reason,
+					[
+						'isCreateAccountBlocked' => true,
+						'isEmailBlocked' => true,
+						'isUserTalkEditBlocked' => true,
+						'isHardBlock' => true,
+						'isAutoblocking' => true,
+					]
+				)->placeBlockUnsafe( $reblock );
 			}
 
-			$block->setTarget( $name );
-			$block->setBlocker( $performer );
-			$block->setReason( $reason );
-			$block->isHardblock( true );
-			$block->isAutoblocking( true );
-			$block->isCreateAccountBlocked( true );
-			$block->isEmailBlocked( true );
-			$block->isUsertalkEditAllowed( false );
-			$block->setExpiry( SpecialBlock::parseExpiryInput( 'infinity' ) );
-
-			if ( $priorBlock === null ) {
-				$success = $blockStore->insertBlock( $block );
+			if ( $res->isOK() ) {
+				$this->output( "{$action} '{$line}' succeeded.\n" );
 			} else {
-				$success = $blockStore->updateBlock( $block );
-			}
-
-			if ( $success ) {
-				// Fire any post block hooks
-				$this->getHookRunner()->onBlockIpComplete( $block, $performer, $priorBlock );
-				// Log it only if the block was successful
-				$flags = [
-					'nocreate',
-					'noemail',
-					'nousertalk',
-				];
-				$logParams = [
-					'5::duration' => 'indefinite',
-					'6::flags' => implode( ',', $flags ),
-				];
-
-				$logEntry = new ManualLogEntry( 'block', 'block' );
-				$logEntry->setTarget( Title::makeTitle( NS_USER, $name ) );
-				$logEntry->setComment( $reason );
-				$logEntry->setPerformer( $performer );
-				$logEntry->setParameters( $logParams );
-				$blockIds = array_merge( [ $success['id'] ], $success['autoIds'] );
-				$logEntry->setRelations( [ 'ipb_id' => $blockIds ] );
-				$logEntry->publish( $logEntry->insert() );
-
-				$this->output( "Blocking '{$name}' succeeded.\n" );
-			} else {
-				$this->output( "Blocking '{$name}' failed.\n" );
+				$errorsText = $res->getMessage()->text();
+				$this->output( "{$action} '{$line}' failed ({$errorsText}).\n" );
 			}
 		}
 	}

commit 70b8b61d72fd403a16dc27620382afc8d284a45c

Fix blockUsers.php handling of an invalid performer option
Why:
* The blockUsers.php maintenance script can set the performer's
  username as the username provided via the --performer option.
* The username is validated by the script, but the validation broke
  in c106f679b03ca221fcee26f1a0f5aff40381e3f5. This was because both:
** The check for whether the username was invalid changed to only
   looking for 'null' values returned by User::newSystemUser /
   User::newFromName.
** The User::newFromName method returns false on an invalid username.
* Replacing the deprecated User::newFromName with the call to
  UserFactory::newFromName fixes this issue, as the replacement
  method returns null on an invalid username.
** This also allows replacement of a deprecated method, which is
   preferrable to changing the 'null' check to be a falsey check.

What:
* Replace the call to User::newFromName with UserFactory
  ::newFromName to fix this issue.
* Add a test to verify that this fix worked.

Bug: T371167
Change-Id: Ie181c16a1dda559a81b8ae4d53cd8f69b1af9b75

maintenance/blockUsers.php
@@ -119,7 +119,7 @@ class BlockUsers extends Maintenance {
 		$expiry = $this->getOption( 'expiry', 'indefinite' );
 
 		if ( $performerName ) {
-			$performer = User::newFromName( $performerName );
+			$performer = $this->getServiceContainer()->getUserFactory()->newFromName( $performerName );
 		} else {
 			$performer = User::newSystemUser( User::MAINTENANCE_SCRIPT_USER, [ 'steal' => true ] );
 		}

tests/phpunit/maintenance/BlockUsersTest.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Tests\Maintenance;
 
 use BlockUsers;
+use MediaWiki\MainConfigNames;
 use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
 use MediaWiki\User\User;
 use MediaWiki\User\UserIdentity;
@@ -163,5 +164,13 @@ class BlockUsersTest extends MaintenanceBaseTestCase {
 
 	public function testExecuteForBlockWithSpecifiedPerformer() {
 		$this->testExecuteForBlock( [ 'performer' => $this->getTestSysop()->getUserIdentity()->getName() ], 1, 0 );
+	}
+
+	public function testExecuteWhenPerformerNameInvalid() {
+		// Set wgMaxNameChars to 3, so that the performer username will be invalid
+		$this->overrideConfigValue( MainConfigNames::MaxNameChars, 3 );
+		$this->expectCallToFatalError();
+		$this->expectOutputRegex( '/Unable to parse.*username/' );
+		$this->commonTestExecute( [ 'performer' => 'Username-which-is-too-long' ], '' );
 	}
 }