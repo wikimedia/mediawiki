e037af51b83b1767a0313d8e272c0f7fdbe039b8
Winston Sung <winston21.sung@gmail.com>
2024-08-15 15:37:35 +0800

    Languages: Add nan-latn-tailo (Minnan - Tâi-lô) to Names.php
    First export in commit 5c6b75659f59ca6baa00218e2ac5c1276fc03468
    (Change-Id Ied5002d2ca82a05d69e55442561f254f610e0d5c ).
    
    Language codes nan, nan-hani, nan-latn will be used for language
    converter codes.
    
    Bug: T354937
    Bug: T369899
    Change-Id: If68512a24cac67daa033a634b1fb57d872d0039c

---
+++ languages/messages/MessagesNan_latn_tailo.php
@@ -0,0 +1,47 @@
+<?php
+/** Minnan (Tâi-lô) (Bân-lâm-gú (Tâi-lô))
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Hiong3-eng5
+ * @author Ianbu
+ * @author Kaihsu
+ * @author Winston Sung
+ */
+
+$fallback = 'nan-latn, nan-latn-pehoeji, nan, nan-hant, cdo, zh-hant, zh, zh-hans';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Muî-thé',
+	NS_SPECIAL          => 'Tik-pia̍t',
+	NS_TALK             => 'Thó-lūn',
+	NS_USER             => 'Iōng-tsiá',
+	NS_USER_TALK        => 'Iōng-tsiá_thó-lūn',
+	NS_PROJECT_TALK     => '$1_thó-lūn',
+	NS_FILE             => 'Tóng-àn',
+	NS_FILE_TALK        => 'Tóng-àn_thó-lūn',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_thó-lūn',
+	NS_TEMPLATE         => 'Pang-bôo',
+	NS_TEMPLATE_TALK    => 'Pang-bôo_thó-lūn',
+	NS_HELP             => 'Suat-bîng',
+	NS_HELP_TALK        => 'Suat-bîng_thó-lūn',
+	NS_CATEGORY         => 'Luī',
+	NS_CATEGORY_TALK    => 'Luī_thó-lūn',
+];
+
+$datePreferences = [
+	'default',
+	'ISO 8601',
+];
+
+$defaultDateFormat = 'nan-latn-tailo';
+
+$dateFormats = [
+	'nan-latn-tailo time' => 'H:i',
+	'nan-latn-tailo date' => 'Y-"nî" n-"gue̍h" j-"ji̍t" (l)',
+	'nan-latn-tailo monthonly' => 'Y-"nî" n-"gue̍h"',
+	'nan-latn-tailo both' => 'Y-"nî" n-"gue̍h" j-"ji̍t" (l) H:i',
+	'nan-latn-tailo pretty' => 'n-"gue̍h" j-"ji̍t"',
+];
