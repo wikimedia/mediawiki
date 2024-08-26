ef832a2fc0838430d36e3733495420657d7ce650
Željko Filipin <zeljko.filipin@gmail.com>
2024-08-26 18:01:48 +0200

    selenium: Example code for Selenium/Explanation/Page object pattern
    https://www.mediawiki.org/wiki/Selenium/Explanation/Page_object_pattern
    
    Bug: T368535
    Change-Id: Ib69e0696060885476e9f880f37067539ed7e1904

---
+++ tests/selenium/docs/Page_object_pattern/specs/login.js
@@ -0,0 +1,42 @@
+// Example code for Selenium/Explanation/Page object pattern
+// https://www.mediawiki.org/wiki/Selenium/Explanation/Page_object_pattern
+
+'use strict';
+
+const Api = require( 'wdio-mediawiki/Api' );
+const assert = require( 'assert' );
+const Util = require( 'wdio-mediawiki/Util' );
+
+// baseUrl is required for our continuous integration.
+// If you don't have MW_SERVER and MW_SCRIPT_PATH environment variables set
+// you can probably hardcode it to something like this:
+// const baseUrl = 'http://localhost:8080/wiki/';
+const baseUrl = `${ process.env.MW_SERVER }${ process.env.MW_SCRIPT_PATH }/index.php?title=`;
+
+describe( 'User', () => {
+	let password, username, bot;
+
+	before( async () => {
+		bot = await Api.bot();
+	} );
+
+	beforeEach( async () => {
+		username = Util.getTestString( 'User-' );
+		password = Util.getTestString();
+	} );
+
+	it( 'should be able to log in without page object', async () => {
+		// create
+		await Api.createAccount( bot, username, password );
+
+		// log in
+		await browser.url( `${ baseUrl }Special:UserLogin` );
+		await $( '#wpName1' ).setValue( username );
+		await $( '#wpPassword1' ).setValue( password );
+		await $( '#wpLoginAttempt' ).click();
+
+		// check
+		const actualUsername = await browser.execute( () => mw.config.get( 'wgUserName' ) );
+		assert.strictEqual( await actualUsername, username );
+	} );
+} );
