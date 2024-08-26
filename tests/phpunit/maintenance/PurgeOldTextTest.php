36e06c9f702dc5b24a78d3846d65cf181c2b1581
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2024-08-26 10:45:29 +0100

    Test purgeOldText.php
    Why:
    * Maintenance scripts in core are mostly untested and testing them
      will help to avoid regressions.
    
    What:
    * Create PurgeOldTextTest that tests the maintenance script
      by mocking the ::purgeRedundantText method.
    ** Mocking this method is done because it should be tested
       directly instead of indirectly through this maintenance
       script
    ** Using this method of mocking avoids the need for a database
       test, which speeds up the execution of this test class.
    
    Bug: T371167
    Change-Id: I0a79fa79f151ff4102f746becaea2c800a7cd064

---
+++ tests/phpunit/maintenance/PurgeOldTextTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MediaWiki\Tests\Maintenance;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PurgeOldText;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @covers PurgeOldText
+ * @author Dreamy Jazz
+ */
+class PurgeOldTextTest extends MaintenanceBaseTestCase {
+
+	/** @var \Maintenance|MockObject */
+	protected $maintenance;
+
+	public function getMaintenanceClass() {
+		return PurgeOldText::class;
+	}
+
+	protected function createMaintenance() {
+		// Mock ::purgeRedundantText as this should be separately tested
+		$obj = $this->getMockBuilder( $this->getMaintenanceClass() )
+			->onlyMethods( [ 'purgeRedundantText' ] )
+			->getMock();
+		return TestingAccessWrapper::newFromObject( $obj );
+	}
+
+	/** @dataProvider providePurgeOptionValues */
+	public function testExecute( $purgeOptionProvided ) {
+		$this->maintenance->expects( $this->once() )
+			->method( 'purgeRedundantText' )
+			->with( $purgeOptionProvided );
+		if ( $purgeOptionProvided ) {
+			$this->maintenance->setOption( 'purge', 1 );
+		}
+		$this->maintenance->execute();
+	}
+
+	public static function providePurgeOptionValues() {
+		return [
+			'--purge not provided' => [ false ],
+			'--purge provided' => [ true ],
+		];
+	}
+}
