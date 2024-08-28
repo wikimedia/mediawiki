3353abe82c733e80c04e9a086c7670a56378de9f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-08-27 23:49:00 -0400

    Add plural forms for Levantine Arabic
    Bug: T363690
    Change-Id: I9d451e1fa53424ccccb1ef0df21e28fc1d2cfdf4

---
+++ tests/phpunit/includes/languages/LanguageApcTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @group Language
+ */
+class LanguageApcTest extends LanguageClassesTestCase {
+	/**
+	 * @dataProvider providePlural
+	 * @covers \Language::convertPlural
+	 */
+	public function testPlural( $result, $value ) {
+		$forms = [ 'one', 'two', 'few', 'other' ];
+		$this->assertEquals( $result, $this->getLang()->convertPlural( $value, $forms ) );
+	}
+
+	/**
+	 * @dataProvider providePlural
+	 * @covers \Language::getPluralRuleType
+	 */
+	public function testGetPluralRuleType( $result, $value ) {
+		$this->assertEquals( $result, $this->getLang()->getPluralRuleType( $value ) );
+	}
+
+	public static function providePlural() {
+		return [
+			[ 'few', 0 ],
+			[ 'one', 1 ],
+			[ 'two', 2 ],
+			[ 'few', 3 ],
+			[ 'few', 9 ],
+			[ 'few', 10 ],
+			[ 'other', 11 ],
+			[ 'other', 12 ],
+			[ 'other', 99 ],
+			[ 'other', 100 ],
+			[ 'other', 101 ],
+			[ 'other', 102 ],
+			[ 'few', 103 ],
+			[ 'few', 104 ],
+			[ 'few', 109 ],
+			[ 'few', 110 ],
+			[ 'other', 111 ],
+			[ 'other', 112 ],
+			[ 'other', 9999 ],
+			[ 'other', 1000 ],
+			[ 'few', 1003 ],
+			[ 'other', 1.7 ],
+		];
+	}
+}
