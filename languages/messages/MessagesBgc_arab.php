750e74f4c1112ba7174ba89ffe5db98ce6776cc6
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-09-01 18:32:52 -0400

    Add a Messages file for Haryanvi in Arabic script
    To allow correct RTL display before full localization support.
    
    Bug: T373561
    Change-Id: I2edda23063a4c7d5490da7b920c5f7867ec3c5b2

---
+++ languages/messages/MessagesBgc_arab.php
@@ -0,0 +1,11 @@
+<?php
+/** Haryanvi (Arabic script; ہریانوی)
+ *
+ * Limited support language without full localization yet.
+ * This file only exists to provide correct content direction.
+ *
+ * @file
+ * @ingroup Languages
+ */
+
+$rtl = true;
