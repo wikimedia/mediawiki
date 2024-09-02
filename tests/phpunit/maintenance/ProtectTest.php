5a5a28f817720dffb6549cca25c2c6a57880db18
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2024-09-02 10:59:15 +0100

    Expand ProtectTest
    Why:
    * Maintenance scripts in core have low test coverage
    * Improving this will reduce the chances of regressions and bugs
    * Some files are mostly covered, and expanding their associated
      tests should fully cover them or at least further improve
      coverage.
    
    What:
    * Expand ProtectTest
    
    Bug: T371167
    Change-Id: Iff86988967d8a69d71353fea4ba72705062aa95f

--- tests/phpunit/maintenance/ProtectTest.php
+++ tests/phpunit/maintenance/ProtectTest.php
@@ -41,6 +41,32 @@ class ProtectTest extends MaintenanceBaseTestCase {
 		];
 	}
 
+	public function testExecuteForUnprotect() {
+		$testPage = $this->getExistingTestPage();
+		// Protect the test page so that we can unprotect it
+		$cascade = false;
+		$testPage->doUpdateRestrictions(
+			[ 'edit' => 'sysop', 'move' => 'sysop' ], [], $cascade, '', $this->getTestSysop()->getUser()
+		);
+		// Verify that the specified protection level has been applied
+		$resultingPageRestrictions = $this->getServiceContainer()->getRestrictionStore()
+			->getAllRestrictions( $testPage );
+		foreach ( $resultingPageRestrictions as $restrictions ) {
+			$this->assertContains( 'sysop', $restrictions );
+		}
+		// Call ::execute to unprotect the page
+		$this->maintenance->setArg( 'title', $testPage );
+		$this->maintenance->setOption( 'unprotect', 1 );
+		$this->maintenance->execute();
+		// Verify that the specified protection level has been applied
+		$this->expectOutputString( "Updating protection status...done\n" );
+		$resultingPageRestrictions = $this->getServiceContainer()->getRestrictionStore()
+			->getAllRestrictions( $testPage );
+		foreach ( $resultingPageRestrictions as $restrictions ) {
+			$this->assertCount( 0, $restrictions );
+		}
+	}
+
 	public function testExecuteWhenReadOnly() {
 		// Get a test page and then add it as an argument to the maintenance script
 		$testPage = $this->getExistingTestPage();
