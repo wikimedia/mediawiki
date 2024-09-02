5f3826d19fb34d33688fec7df7b4ae5e64ae1747
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2024-09-02 10:57:11 +0100

    Expand CleanupWatchlistTest
    Why:
    * Maintenance scripts in core have low test coverage
    * Improving this will reduce the chances of regressions and bugs
    * Some files are mostly covered, and expanding their associated
      tests should fully cover them or at least further improve
      coverage.
    
    What:
    * Expand CleanupWatchlistTest
    
    Bug: T371167
    Change-Id: I245c5550b3759d2267a89a5f81e34bec3fb97f24

--- tests/phpunit/maintenance/CleanupWatchlistTest.php
+++ tests/phpunit/maintenance/CleanupWatchlistTest.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Tests\Maintenance;
 
 use CleanupWatchlist;
+use MediaWiki\MainConfigNames;
 use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
 
 /**
@@ -27,16 +28,21 @@ class CleanupWatchlistTest extends MaintenanceBaseTestCase {
 				true,
 				// The expected row count in the watchlist table after the script has run
 				4,
+				// The expected row count in the watchlist_expiry table after the script has run
+				2,
 			],
-			'Invalid user ID with dry run' => [ null, 0, true, 4 ],
-			'Invalid title' => [ ':::', null, false, 2 ],
-			'Invalid user ID' => [ null, 0, false, 2 ],
-			'No invalid data' => [ null, null, false, 4 ],
+			'Invalid user ID with dry run' => [ null, 0, true, 4, 2 ],
+			'Invalid title' => [ ':::', null, false, 2, 0 ],
+			'Invalid user ID' => [ null, 0, false, 2, 0 ],
+			'No invalid data' => [ null, null, false, 4, 2 ],
 		];
 	}
 
 	/** @dataProvider provideCleanup */
-	public function testCleanup( $title, $userId, $dryRun, $expectedRowCountAfterExecution ) {
+	public function testCleanup(
+		$title, $userId, $dryRun, $expectedWatchlistRowCountAfterExecution, $expectedWatchlistExpiryRowCountAfterExecution
+	) {
+		$this->overrideConfigValue( MainConfigNames::WatchlistExpiry, true );
 		$existingTestPage = $this->getExistingTestPage();
 		// Add a test watchlist entry which will not be broken
 		$this->getServiceContainer()->getWatchlistManager()
@@ -44,7 +50,7 @@ class CleanupWatchlistTest extends MaintenanceBaseTestCase {
 		// Add another watchlist entry and then break it.
 		$testUser1 = $this->getMutableTestUser()->getAuthority();
 		$this->getServiceContainer()->getWatchlistManager()
-			->addWatch( $testUser1, $existingTestPage );
+			->addWatch( $testUser1, $existingTestPage, '1 week' );
 		$this->getDb()->newUpdateQueryBuilder()
 			->update( 'watchlist' )
 			->set( [
@@ -56,14 +62,24 @@ class CleanupWatchlistTest extends MaintenanceBaseTestCase {
 			] )
 			->caller( __METHOD__ )
 			->execute();
+		// Check that watchlist_expiry is correctly set up for the test.
+		$this->newSelectQueryBuilder()
+			->select( 'COUNT(*)' )
+			->from( 'watchlist_expiry' )
+			->assertFieldValue( 2 );
 		// Run the maintenance script and check that the table has been fixed if this is not a dry run
 		if ( !$dryRun ) {
 			$this->maintenance->setOption( 'fix', 1 );
 		}
 		$this->maintenance->execute();
+		// Check that the DB is as expected after running the script.
 		$this->newSelectQueryBuilder()
 			->select( 'COUNT(*)' )
 			->from( 'watchlist' )
-			->assertFieldValue( $expectedRowCountAfterExecution );
+			->assertFieldValue( $expectedWatchlistRowCountAfterExecution );
+		$this->newSelectQueryBuilder()
+			->select( 'COUNT(*)' )
+			->from( 'watchlist_expiry' )
+			->assertFieldValue( $expectedWatchlistExpiryRowCountAfterExecution );
 	}
 }
