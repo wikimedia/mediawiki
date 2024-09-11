e56db8f32e36dd8e98edf6f0f7db8d58901675a0
C. Scott Ananian <cscott@cscott.net>
2024-09-11 17:27:05 -0400

    Skin: don't reparse language link titles
    When there are conflicts between namespace prefixes and interwiki prefixes,
    the Skin was inadvertently converting from an interwiki link to a local
    link by reparsing the title.  Use a TitleValue as an intermediary to
    ensure the interwiki link stays an interwiki link.
    
    Bug: T204792
    Bug: T363538
    Change-Id: I351dfcff7cd6b32f4fa67507e5c5a432dce61a01

--- tests/phpunit/includes/skins/SkinTest.php
+++ tests/phpunit/includes/skins/SkinTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use MediaWiki\Context\RequestContext;
+use MediaWiki\HookContainer\HookContainer;
+use MediaWiki\Interwiki\InterwikiLookup;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\MainConfigNames;
 use MediaWiki\Output\OutputPage;
@@ -660,5 +662,81 @@ class SkinTest extends MediaWikiIntegrationTestCase {
 			[ 'contributions', 'log', 'blockip', 'mute', 'print', 'specialpages' ],
 			array_keys( $foo1->buildSidebar()['TOOLBOX'] )
 		);
+	}
+
+	public function testGetLanguagesHidden() {
+		$this->overrideConfigValues( [
+			MainConfigNames::HideInterlanguageLinks => true,
+		] );
+		$skin = new class extends Skin {
+			public function outputPage() {
+			}
+		};
+		$this->assertSame( [], $skin->getLanguages() );
+	}
+
+	public function testGetLanguages() {
+		$this->overrideConfigValues( [
+			MainConfigNames::HideInterlanguageLinks => false,
+			MainConfigNames::InterlanguageLinkCodeMap => [ 'talk' => 'fr' ],
+			MainConfigNames::LanguageCode => 'qqx',
+		] );
+
+		$mockOutputPage = $this->createMock( OutputPage::class );
+		$mockOutputPage->method( 'getLanguageLinks' )
+			// The 'talk' interwiki is a deliberate conflict with the
+			// Talk namespace (T363538)
+			->willReturn( [ 'en:Foo', 'talk:Page' ] );
+
+		$fakeContext = new RequestContext();
+		$fakeContext->setTitle( Title::makeTitle( NS_MAIN, 'Test' ) );
+		$fakeContext->setOutput( $mockOutputPage );
+		$fakeContext->setLanguage( 'en' );
+
+		$hookContainer = $this->createMock( HookContainer::class );
+		$this->setService( 'HookContainer', $hookContainer );
+
+		$mockIwLookup = $this->createMock( InterwikiLookup::class );
+		$mockIwLookup->method( 'isValidInterwiki' )->willReturn( true );
+		$mockIwLookup->method( 'fetch' )->willReturnCallback( static function ( string $prefix ) {
+			return new Interwiki(
+				$prefix,
+				"https://$prefix.example.com/$1"
+			);
+		} );
+		$this->setService( 'InterwikiLookup', $mockIwLookup );
+
+		$skin = new class extends Skin {
+			public function outputPage() {
+			}
+		};
+		$skin->setContext( $fakeContext );
+
+		$this->assertSame( [
+			[
+				'href' => 'https://en.example.com/Foo',
+				'text' => 'English',
+				'title' => 'Foo – English',
+				'class' => 'interlanguage-link interwiki-en',
+				'link-class' => 'interlanguage-link-target',
+				'lang' => 'en',
+				'hreflang' => 'en',
+				'data-title' => 'Foo',
+				'data-language-autonym' => 'English',
+				'data-language-local-name' => 'English',
+			],
+			[
+				'href' => 'https://talk.example.com/Page',
+				'text' => 'Français',
+				'title' => 'Page – français',
+				'class' => 'interlanguage-link interwiki-talk',
+				'link-class' => 'interlanguage-link-target',
+				'lang' => 'fr',
+				'hreflang' => 'fr',
+				'data-title' => 'Page',
+				'data-language-autonym' => 'Français',
+				'data-language-local-name' => 'français',
+			],
+		], $skin->getLanguages() );
 	}
 }
