8d7246ae81c4b67d54f7245491e96bb74408e575
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-09-22 23:27:38 -0400

    Add namespace translations for Tigre (tig)
    Contributed by User:Bbeshir at
    https://incubator.wikimedia.org/w/index.php?title=Talk:Wp/tig/Main_Page&oldid=6385826
    
    Bug: T375052
    Change-Id: Iae1e539ba96dc0447a8a84b7b7c3f041156f654c

--- languages/messages/MessagesTig.php
+++ languages/messages/MessagesTig.php
@@ -4,7 +4,27 @@
  * @file
  * @ingroup Languages
  *
+ * @author Bbeshir
  * @author Amir E. Aharoni
  */
+
+$namespaceNames = [
+	NS_MEDIA            => 'ሜድያ',
+	NS_SPECIAL          => 'ፍንቱይ',
+	NS_TALK             => 'ህድግ',
+	NS_USER             => 'መትነፈዓይ',
+	NS_USER_TALK        => 'ህድግ_መትነፈዓይ',
+	NS_PROJECT_TALK     => 'ህድግ_$1',
+	NS_FILE             => 'ፋይል',
+	NS_FILE_TALK        => 'ህድግ_ፋይል',
+	NS_MEDIAWIKI        => 'ሜድያዊኪ',
+	NS_MEDIAWIKI_TALK   => 'ህድግ_ሜድያዊኪ',
+	NS_TEMPLATE         => 'ሞደል',
+	NS_TEMPLATE_TALK    => 'ህድግ_ሞደል',
+	NS_HELP             => 'ሰዳየት',
+	NS_HELP_TALK        => 'ህድግ_ሰዳየት',
+	NS_CATEGORY         => 'ከረርም',
+	NS_CATEGORY_TALK    => 'ህድግ_ከረርም',
+];
 
 $linkTrail = "/^([a-z\x{1200}-\x{137F}]+)(.*)$/sDu";
