045a561d6ecf96ecaca5a02182f9e23536b5bb46
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-09-27 13:37:28 -0400

    Fix Manchu language configuration
    This reflects the actual situation on translatewiki
    and in the i18n json files:
    * mnc is the main code, and it is in the Latin script
    * mnc-mong is in the Mongolian script
    * mnc-latn is a compatibility redirect to mnc
    
    In the future, we may swap between mnc and mnc-latn,
    but for now, let's at least make sure that the two
    variants are supported in core properly.
    
    This also adds namespace translations
    given by User:MiiCii at
    https://translatewiki.net/w/i.php?title=User_talk:MiiCii&oldid=12654825
    (Note the different "Category" translation towards
    the end.)
    
    Bug: T284043
    Change-Id: I3998d84aa51f3fced5642f611ba235687f4fe003

---
+++ languages/messages/MessagesMnc.php
@@ -0,0 +1,30 @@
+<?php
+/** Manchu (manju gisun)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author MiiCii
+ * @author Amir E. Aharoni
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Mediya',
+	NS_SPECIAL          => 'Cohotoi',
+	NS_TALK             => 'Leolen',
+	NS_USER             => 'Baitalasi',
+	NS_USER_TALK        => 'Baitalasi_-i_leolen',
+	NS_PROJECT_TALK     => '$1_-i_leolen',
+	NS_FILE             => 'Bithe',
+	NS_FILE_TALK        => 'Bithei_leolen',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_-i_leolen',
+	NS_TEMPLATE         => 'Template',
+	NS_TEMPLATE_TALK    => 'Template_-i_leolen',
+	NS_HELP             => 'Aisilan',
+	NS_HELP_TALK        => 'Aisilan_-i_leolen',
+	NS_CATEGORY         => 'Duwalibun',
+	NS_CATEGORY_TALK    => 'Duwalibun_-i_leolen',
+];
+
+$linkTrail = '/^([a-zŠšŪūŽž]+)(.*)$/sDu';
---
+++ languages/messages/MessagesMnc_latn.php
@@ -0,0 +1,11 @@
+<?php
+/** Manchu (manju gisun; Latin script)
+ *
+ * @file
+ * @ingroup Languages
+ * @comment falls back to Manchu without explicit script
+ *
+ * @author Amir E. Aharoni
+ */
+
+$fallback = 'mnc';
