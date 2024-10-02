6e9637a7ec04b140799f5c8fac298d5b96abcc98
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-10-01 22:30:22 -0400

    Add Duala language (dua)
    Bug: T376248
    Change-Id: I70a4b323cbf1b6e34b9a079799daa8e33240cb30

---
+++ languages/messages/MessagesDua.php
@@ -0,0 +1,10 @@
+<?php
+/** Duala (Duálá)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Amir E. Aharoni
+ */
+
+$linkTrail = '/^([ɓɗɛŋɲɔƁƊƐŊƝƆa-zÁáÀàÂâǍǎÉéÈèÊêĚěÍíÌìÎîǏǐÓóÒòÔôǑǒÚúÙùÛûǓǔ]+)(.*)$/sDu';
