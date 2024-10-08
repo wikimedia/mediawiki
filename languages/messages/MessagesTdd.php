88de582e951c8796fb083ce365cda63a6ea9538b
Srishakatux <ssethi@wikimedia.org>
2024-10-07 21:47:49 -0700

    Add namespace translations for Tai Nüa (tdd)
    More information about these translations can be found in the Phabricator ticket linked to this patch.
    
    Bug: T375421
    Change-Id: I3240a3f01a93196cdc2996fd11e46106ee8e65dc
    (cherry picked from commit 46c503bef6ef0e8b97943729701577da53744d18)

---
+++ languages/messages/MessagesTdd.php
@@ -0,0 +1,30 @@
+<?php
+/** Tai Nüa (ᥖᥭᥰ ᥖᥬᥲ ᥑᥨᥒᥰ)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Yang Yan Ao Thuan
+ * @author Srishti Sethi
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'ᥛᥤᥱ_ᥖᥤᥱ_ᥕᥣᥱ',
+	NS_SPECIAL          => 'ᥑᥪᥐ_ᥖᥩᥢᥰ',
+	NS_TALK             => 'ᥟᥧᥙᥱ_ᥐᥧᥛ',
+	NS_USER             => 'ᥚᥧᥱ_ᥕᥧᥒᥱ_ᥖᥪᥰ',
+	NS_USER_TALK        => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥚᥧᥱ_ᥕᥧᥒᥱ_ᥖᥪᥰ',
+	NS_PROJECT_TALK     => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_$1',
+	NS_FILE             => 'ᥜᥣᥭᥱ',
+	NS_FILE_TALK        => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥜᥣᥭᥱ',
+	NS_MEDIAWIKI        => 'ᥛᥤᥱ_ᥖᥤᥱ_ᥕᥣᥱ_ᥝᥤᥱ_ᥑᥤᥱ',
+	NS_MEDIAWIKI_TALK   => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥛᥤᥱ_ᥖᥤᥱ_ᥕᥣᥱ_ᥝᥤᥱ_ᥑᥤᥱ',
+	NS_TEMPLATE         => 'ᥗᥦᥛᥰ_ᥙᥣ_ᥘᥥᥐᥳ',
+	NS_TEMPLATE_TALK    => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥗᥦᥛᥰ_ᥙᥣ_ᥘᥥᥐᥳ',
+	NS_HELP             => 'ᥘᥩᥒ_ᥓᥩᥭ_ᥗᥦᥛ',
+	NS_HELP_TALK        => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥘᥩᥒ_ᥓᥩᥭ_ᥗᥦᥛᥴ',
+	NS_CATEGORY         => 'ᥙᥫᥒ_ᥗᥣᥢ',
+	NS_CATEGORY_TALK    => 'ᥟᥧᥙᥱ_ᥐᥧᥛ_ᥙᥫᥒ_ᥗᥣᥢ',
+];
+
+$linkTrail = '/^([ᥐ-ᥴ]+)(.*)$/sDu';
