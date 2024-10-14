8afa1e3eaca69c1729dc686a902cc0fc11fd69be
Pppery <mapreader@olum.org>
2024-10-14 15:52:25 -0400

    Set namespaces for dtp
    Bug: T371068
    Change-Id: Ic76f156b4a18757a79e255abe2f132c4bdd14825

--- languages/messages/MessagesDtp.php
+++ languages/messages/MessagesDtp.php
@@ -3,6 +3,28 @@
  *
  * @file
  * @ingroup Languages
+ *
+ * @author Rombituon
+ * @author Ultron90
  */
 
 $fallback = 'ms';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Poimbida',
+	NS_TALK             => 'Pibarasan',
+	NS_USER             => 'Moomomoguno',
+	NS_USER_TALK        => 'Pibarasan_moomomoguno',
+	NS_PROJECT_TALK     => 'Pibarasan_$1',
+	NS_FILE             => 'Fail',
+	NS_FILE_TALK        => 'Pibarasan_fail',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Pibarasan_MediaWiki',
+	NS_TEMPLATE         => 'Templat',
+	NS_TEMPLATE_TALK    => 'Pibarasan_templat',
+	NS_HELP             => 'Kouhupan',
+	NS_HELP_TALK        => 'Pibarasan_kouhupan',
+	NS_CATEGORY         => 'Kategori',
+	NS_CATEGORY_TALK    => 'Pibarasan_kategori',
+];
