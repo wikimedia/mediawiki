3dbfb74a6f851f7d97f023074652cb5fbe98c9dc
bpirkle <bpirkle@wikimedia.org>
2024-10-15 14:36:56 -0500

    REST: ability to validate responses against response schemas in tests
    REST API endpoints can define response schemas in their OpenAPI spec.
    But because these schemas are not generated from the actual code,
    they are not guaranteed to match the actual response. Introduce
    support for integration tests to validate response schemas against
    actual responses, to ensure the schemas reflect reality.
    
    Bug: T376339
    Change-Id: I68524ac865240b1521c3858c2defec945e7a2ea6

--- tests/api-testing/REST/RevisionLegacy.js
+++ tests/api-testing/REST/RevisionLegacy.js
@@ -5,6 +5,6 @@ describe( 'legacy Revision', () => {
 	// Doing this twice protects against changes in test execution order
 	const testsFile = __dirname + '/content.v1/Revision.js';
 	delete require.cache[ testsFile ];
-	require( testsFile ).init( 'rest.php/v1' );
+	require( testsFile ).init( '/v1', '/-' );
 	delete require.cache[ testsFile ];
 } );
