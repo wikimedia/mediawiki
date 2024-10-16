4a8c6c4838845989efa58f6f44ec773ff28ccc99
MusikAnimal <musikanimal@gmail.com>
2024-10-16 18:56:46 -0400

    SpecialBlock [Vue]: load extension-provided messages
    Extension:Thanks adds an action to block user from sending thanks.
    This uses the 'ipb-action-thanks' message, which we don't want to
    explicitly ask for because it is not a Core message.
    
    Bug: T377402
    Change-Id: Iceceb860e2bc7c373fd26624cfc3f57467136555

--- resources/src/mediawiki.special.block/init.js
+++ resources/src/mediawiki.special.block/init.js
@@ -6,7 +6,13 @@ if ( outer ) {
 	const Vue = require( 'vue' );
 	const App = require( './SpecialBlock.vue' );
 	const { createPinia } = require( 'pinia' );
-	Vue.createMwApp( App )
-		.use( createPinia() )
-		.mount( outer );
+
+	// Load any extension-provided messages added by the PHP GetAllBlockActions hook.
+	( new mw.Api() ).loadMessagesIfMissing(
+		Object.keys( mw.config.get( 'partialBlockActionOptions' ) )
+	).then( () => {
+		Vue.createMwApp( App )
+			.use( createPinia() )
+			.mount( outer );
+	} );
 }
