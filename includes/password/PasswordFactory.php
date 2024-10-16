ab23461db2c6984c8a867630d4bec302cb64490d
Reedy <reedy@wikimedia.org>
2024-10-16 16:17:17 +0100

    PasswordFactory: Move magic number used for random password string into const
    Noticed while looking at T377342
    
    Change-Id: I19cf5e0e4d0b2c7b0490acb4f2f9c1c8af061eeb

--- includes/password/PasswordFactory.php
+++ includes/password/PasswordFactory.php
@@ -55,6 +55,8 @@ final class PasswordFactory {
 		'' => [ 'type' => '', 'class' => InvalidPassword::class ],
 	];
 
+	private const MIN_RANDOM_PASSWORD_LENGTH = 10;
+
 	/**
 	 * Most of the time you'll want to use MediaWikiServices::getInstance()->getPasswordFactory
 	 * instead.
@@ -244,8 +246,8 @@ final class PasswordFactory {
 	 */
 	public static function generateRandomPasswordString( int $minLength = 10 ): string {
 		// Decide the final password length based on our min password length,
-		// stopping at a minimum of 10 chars.
-		$length = max( 10, $minLength );
+		// requiring at least a minimum of self::MIN_RANDOM_PASSWORD_LENGTH chars.
+		$length = max( self::MIN_RANDOM_PASSWORD_LENGTH, $minLength );
 		// Multiply by 1.25 to get the number of hex characters we need
 		$hex = MWCryptRand::generateHex( ceil( $length * 1.25 ) );
 		// Convert from base 16 to base 32 to get a proper password like string
