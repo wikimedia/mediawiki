595a197c64039540bb68feeed209e73fe12962ae
Pppery <mapreader@olum.org>
2024-10-19 18:11:21 -0400

    Add namespace translations for kus
    From https://incubator.wikimedia.org/wiki/Talk:Wp/kus
    
    Bug: T377510
    Change-Id: I80b90930f20f2d144b19e26c0d72936fdae827a1

--- languages/messages/MessagesKus.php
+++ languages/messages/MessagesKus.php
@@ -5,6 +5,26 @@
  * @ingroup Languages
  *
  * @author Amir E. Aharoni
+ * @author Agurwin Isaac
  */
 
 $linkTrail = '/^([ɛɩŋɔʋƐƖ̀Ŋ́Ɔ̃Ʋ̄a-z]+)(.*)$/sDu';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Miidia',
+	NS_SPECIAL          => 'Lakir_kanɛ_bɛ_di_kɔn',
+	NS_TALK             => 'Sɔnsʋg_zin\'ig',
+	NS_USER             => 'Tʋmtʋm',
+	NS_USER_TALK        => 'Tʋmtʋm_sɔnsʋg_zin\'ig',
+	NS_PROJECT_TALK     => '$1_sɔnsʋg_zin\'ig',
+	NS_FILE             => 'Faal',
+	NS_FILE_TALK        => 'Faal_sɔnsʋg_zin\'ig',
+	NS_MEDIAWIKI        => 'Miidiawiki',
+	NS_MEDIAWIKI_TALK   => 'Miidiawiki_sɔnsʋg_zin\'ig',
+	NS_TEMPLATE         => 'Tɛmpilet',
+	NS_TEMPLATE_TALK    => 'Tɛmpilet_sɔnsʋg_zin\'ig',
+	NS_HELP             => 'Sʋŋir_zin\'ig',
+	NS_HELP_TALK        => 'Sʋŋir_sɔnsʋg_zin\'ig',
+	NS_CATEGORY         => 'Bɛn',
+	NS_CATEGORY_TALK    => 'Bɛn_sɔnsʋg_zin\'ig',
+];
