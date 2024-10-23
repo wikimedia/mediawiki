41f2eea06df7d08050c6f4e5f346f4e7cef2960c
Ammarpad <ammarpad@yahoo.com>
2024-10-23 16:30:29 +0100

    specials: Avoid passing null to str_replace()
    Do null-coalescing at call site since the function already
    expects these arguments to be strings
    
    Bug: T377916
    Change-Id: I1bf57646b5a4ef03d49d178e0e75d4e61cf90c00
    (cherry picked from commit 6f5777fe5ab48e4b3bf25ebd2dcc9ebf0e2d1fa6)

--- includes/specials/SpecialAllPages.php
+++ includes/specials/SpecialAllPages.php
@@ -349,7 +349,7 @@ class SpecialAllPages extends IncludableSpecialPage {
 			);
 		}
 
-		$this->outputHTMLForm( $namespace, $from, $to, $hideredirects );
+		$this->outputHTMLForm( $namespace, $from ?? '', $to ?? '', $hideredirects );
 
 		if ( count( $navLinks ) ) {
 			// Add pagination links
