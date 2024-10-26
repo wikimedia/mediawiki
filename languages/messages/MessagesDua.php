7c8d3b8c72d66e8748b09ec833d2baa159ccc132
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-10-26 15:08:16 -0400

    Add namespace translations for Duala
    Contributed by User:NdumbèNdumbè on
    translatewiki.net:
    https://translatewiki.net/w/i.php?title=User_talk:Olivier_Moukodi&oldid=12700351
    
    Bug: T376248
    Change-Id: I1c7a8cf49d822c531f8c660939690e31c92b777a

--- languages/messages/MessagesDua.php
+++ languages/messages/MessagesDua.php
@@ -4,10 +4,30 @@
  * @file
  * @ingroup Languages
  *
+ * @author NdumbèNdumbè
  * @author Amir E. Aharoni
  */
 
 $fallback = 'fr';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Media',
+	NS_SPECIAL          => 'Tóɓótóɓó',
+	NS_TALK             => 'Ekwálí',
+	NS_USER             => 'Muɓolánέ_lambo',
+	NS_USER_TALK        => 'Ekwálí_á_muɓolánέ_lambo',
+	NS_PROJECT_TALK     => 'Ekwálí_á_$1',
+	NS_FILE             => 'Ɓeɓola',
+	NS_FILE_TALK        => 'Ekwálí_á_ɓeɓola',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Ekwálí_á_MediaWiki',
+	NS_TEMPLATE         => 'Eɓáí',
+	NS_TEMPLATE_TALK    => 'Ekwálí_á_eɓáí',
+	NS_HELP             => 'Jǒŋgwanɛ',
+	NS_HELP_TALK        => 'Ekwálí_á_jǒŋgwanɛ',
+	NS_CATEGORY         => 'Nyai',
+	NS_CATEGORY_TALK    => 'Nyai_a_ekwálí',
+];
 
 $namespaceGenderAliases = [];
 
