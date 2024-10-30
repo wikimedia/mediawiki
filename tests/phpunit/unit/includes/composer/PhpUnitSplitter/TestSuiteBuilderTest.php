9df16787938197be0152cbd0984eeae9698dbf08
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-10-30 09:48:57 +0100

    Allocate tests to groups sequentially instead of round-robin
    The round-robin allocation of tests to `split_groups` resulted in
    `split_groups` containing tests from a large number of different
    extensions, and the composition of `split_groups` changing
    significantly depending on which extensions are currently loaded.
    
    Instead, we now add tests to buckets in their alphabetical order so
    that tests are more or less grouped by extension. At the same time
    we try to preserve the time balance of the buckets.
    
    Bug: T378478
    Change-Id: I1da2848b041a2b6a879c64f3a3ee8dd9471fd06e

--- tests/phpunit/unit/includes/composer/PhpUnitSplitter/TestSuiteBuilderTest.php
+++ tests/phpunit/unit/includes/composer/PhpUnitSplitter/TestSuiteBuilderTest.php
@@ -27,23 +27,69 @@ class TestSuiteBuilderTest extends TestCase {
 			[
 				"list" => [
 					"MediaWiki/VTest.php",
-					"MediaWiki/YTest.php",
-				],
-				"time" => 0
-			],
-			[
-				"list" => [
 					"MediaWiki/WTest.php",
-					"MediaWiki/ZTest.php",
 				],
 				"time" => 0
 			],
 			[
 				"list" => [
 					"MediaWiki/XTest.php",
+					"MediaWiki/YTest.php",
+				],
+				"time" => 0
+			],
+			[
+				"list" => [
+					"MediaWiki/ZTest.php",
 				],
 				"time" => 0
 			]
+		];
+		$this->assertEquals( $expected, $suites, "Expected suites to be built correctly" );
+	}
+
+	public function testGroupsWithSimilarDurations() {
+		$testList = [
+			new TestDescriptor( "ZTest", [ "MediaWiki" ], "MediaWiki/ZTest.php", 400 ),
+			new TestDescriptor( "YTest", [ "MediaWiki" ], "MediaWiki/YTest.php", 200 ),
+			new TestDescriptor( "XTest", [ "MediaWiki" ], "MediaWiki/XTest.php", 200 ),
+			new TestDescriptor( "WTest", [ "MediaWiki" ], "MediaWiki/WTest.php", 100 ),
+			new TestDescriptor( "VTest", [ "MediaWiki" ], "MediaWiki/VTest.php", 100 ),
+			new TestDescriptor( "UTest", [ "MediaWiki" ], "MediaWiki/UTest.php", 0 ),
+			new TestDescriptor( "TTest", [ "MediaWiki" ], "MediaWiki/TTest.php", 0 ),
+			new TestDescriptor( "STest", [ "MediaWiki" ], "MediaWiki/STest.php", 0 ),
+			new TestDescriptor( "RTest", [ "MediaWiki" ], "MediaWiki/RTest.php", 0 ),
+			new TestDescriptor( "QTest", [ "MediaWiki" ], "MediaWiki/QTest.php", 0 ),
+			new TestDescriptor( "PTest", [ "MediaWiki" ], "MediaWiki/PTest.php", 0 ),
+		];
+		$suites = ( new TestSuiteBuilder() )->buildSuites( $testList, 3 );
+		$expected = [
+			[
+				"list" => [
+					"MediaWiki/PTest.php",
+					"MediaWiki/QTest.php",
+					"MediaWiki/RTest.php",
+					"MediaWiki/STest.php",
+				],
+				"time" => 0
+			],
+			[
+				"list" => [
+					"MediaWiki/TTest.php",
+					"MediaWiki/UTest.php",
+					"MediaWiki/VTest.php",
+					"MediaWiki/WTest.php",
+				],
+				"time" => 200
+			],
+			[
+				"list" => [
+					"MediaWiki/XTest.php",
+					"MediaWiki/YTest.php",
+					"MediaWiki/ZTest.php",
+				],
+				"time" => 800
+			],
 		];
 		$this->assertEquals( $expected, $suites, "Expected suites to be built correctly" );
 	}
