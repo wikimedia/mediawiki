d68306289227d4a0f54cb67053e13652fd3520ff
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-11-06 15:11:34 -0500

    Add namespace translations for Luba-Lulua
    Translations taken from:
    https://incubator.wikimedia.org/w/index.php?title=Talk:Wp/lua/Dibeji_dia_kumpala&oldid=6451406
    
    Bug: T375999
    Change-Id: If786197ed76f606f14809e75a61ccd09fcba664d

--- languages/messages/MessagesLua.php
+++ languages/messages/MessagesLua.php
@@ -4,9 +4,29 @@
  * @file
  * @ingroup Languages
  *
+ * @author Chrisdanielkazadi
  * @author Amir E. Aharoni
  */
 
 $fallback = 'fr';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Bimwesha',
+	NS_SPECIAL          => 'Tshia_mushinga',
+	NS_TALK             => 'Muyiiki',
+	NS_USER             => 'Mukuatshi_wa_mudimu',
+	NS_USER_TALK        => 'Muyiiki_wa_mukuatshi_wa_mudimu',
+	NS_PROJECT_TALK     => 'Muyiiki_wa_$1',
+	NS_FILE             => 'Muaba_wa_malubu',
+	NS_FILE_TALK        => 'Muyiiki_wa_muaba_wa_malubu',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Muyiiki_wa_MediaWiki',
+	NS_TEMPLATE         => 'Tshilejelu',
+	NS_TEMPLATE_TALK    => 'Muyiiki_wa_tshilejelu',
+	NS_HELP             => 'Diambulwisha',
+	NS_HELP_TALK        => 'Muyiiki_wa_diambulwisha',
+	NS_CATEGORY         => 'Mushindu',
+	NS_CATEGORY_TALK    => 'Muyiiki_wa_mushindu',
+];
 
 $namespaceGenderAliases = [];
