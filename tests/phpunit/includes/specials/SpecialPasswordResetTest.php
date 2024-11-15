e27be818690820c0df227cb06206da499eb94d38
Dreamy Jazz <wpgbrown@wikimedia.org>
2024-11-15 20:03:34 +0000

    Reject temporary account usernames on Special:PasswordReset
    Why:
    * Special:PasswordReset allows users to send an email containing
      a temporary password to the email address attached to an account.
    * Temporary accounts do not have passwords and cannot have an email
      address set (as they cannot access Special:Preferences).
    * Therefore, Special:PasswordReset does not provide holders of
      temporary accounts a way to get access back into the temporary
      account if they loose access.
    * However, the form currently accepts temporary account usernames
      and we should update the functionality to reject such usernames.
    
    What:
    * Update SpecialPasswordReset to reject temporary account usernames.
    * Expand tests for SpecialPasswordReset to check this fix has worked.
    
    Bug: T380085
    Change-Id: I004453d4d16cd2a0448ac3922e4d13c24a158c8d

--- tests/phpunit/includes/specials/SpecialPasswordResetTest.php
+++ tests/phpunit/includes/specials/SpecialPasswordResetTest.php
@@ -1,9 +1,18 @@
 <?php
 
+use MediaWiki\Request\FauxRequest;
 use MediaWiki\Specials\SpecialPasswordReset;
 use MediaWiki\Tests\SpecialPage\FormSpecialPageTestCase;
+use MediaWiki\Tests\User\TempUser\TempUserTestTrait;
 
+/**
+ * @covers \MediaWiki\Specials\SpecialPasswordReset
+ * @group Database
+ */
 class SpecialPasswordResetTest extends FormSpecialPageTestCase {
+
+	use TempUserTestTrait;
+
 	/**
 	 * @inheritDoc
 	 */
@@ -11,5 +20,27 @@ class SpecialPasswordResetTest extends FormSpecialPageTestCase {
 		return new SpecialPasswordReset(
 			$this->getServiceContainer()->getPasswordReset()
 		);
+	}
+
+	public function testView() {
+		[ $html ] = $this->executeSpecialPage();
+		// Check that the form fields are as expected
+		$this->assertStringContainsString( '(passwordreset-username', $html );
+		$this->assertStringContainsString( '(passwordreset-email', $html );
+		$this->assertStringContainsString( '(passwordreset-text-many', $html );
+		$this->assertStringContainsString( '(mailmypassword', $html );
+	}
+
+	public function testExecuteForTemporaryAccountUsername() {
+		// Get an existing temporary account to test with
+		$this->enableAutoCreateTempUser();
+		$tempUser = $this->getServiceContainer()->getTempUserCreator()->create( null, new FauxRequest() )->getUser();
+		// Make a fake POST request that tries to use a temporary account for the password
+		$request = new FauxRequest(
+			[ 'wpUsername' => $tempUser->getName() ],
+			true
+		);
+		[ $html ] = $this->executeSpecialPage( '', $request );
+		$this->assertStringContainsString( '(htmlform-user-not-valid', $html );
 	}
 }
