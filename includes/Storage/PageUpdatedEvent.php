d168fee9cb35e42f0d55aea18537ebeda817ef6b
daniel <dkinzler@wikimedia.org>
2024-11-25 17:20:08 +0100

    PageUpdatedEvent: improve documentation
    Bug: T376063
    Change-Id: I25880313c58824cbf80247bd18d751b74dd421c8

--- includes/Storage/PageUpdatedEvent.php
+++ includes/Storage/PageUpdatedEvent.php
@@ -28,6 +28,19 @@ use MediaWiki\User\UserIdentity;
 /**
  * Domain event representing a page edit.
  *
+ * This event is emitted by PageUpdater. It can be used by core components and
+ * extensions to follow up on page edits.
+ *
+ * Extensions that want to subscribe to this event should list "PageUpdated"
+ * as a subscribed event type.
+ *
+ * Subscribers based on EventSubscriberBase should implement the
+ * handlePageUpdatedEventAfterCommit() listener method to be informed when
+ * a page edit has been committed to the database.
+ *
+ * See the documentation of EventSubscriberBase and DomainEventSource for
+ * more options and details.
+ *
  * @unstable until 1.45
  */
 class PageUpdatedEvent extends DomainEvent {
@@ -40,12 +53,23 @@ class PageUpdatedEvent extends DomainEvent {
 	private ?EditResult $editResult;
 	private array $tags;
 	private int $flags;
-	private int $patrolStatus;
 
+	/**
+	 * @param RevisionSlotsUpdate $slotsUpdate Page content changed by the edit.
+	 * @param RevisionRecord $newRevision The revision object resulting from the
+	 *        edit.
+	 * @param RevisionRecord|null $oldRevision The revision that used to be
+	 *        current before the edit.
+	 * @param EditResult|null $editResult An EditResult representing the effects
+	 *        of an edit.
+	 * @param array $tags Applicable tags, see ChangeTags.
+	 * @param int $flags See PageUpdater::setFlags
+	 * @param int $patrolStatus See PageUpdater::setRcPatrolStatus()
+	 */
 	public function __construct(
 		RevisionSlotsUpdate $slotsUpdate,
 		RevisionRecord $newRevision,
-		?RevisionRecord $oldrevision,
+		?RevisionRecord $oldRevision,
 		?EditResult $editResult,
 		array $tags = [],
 		int $flags = 0,
@@ -55,63 +79,99 @@ class PageUpdatedEvent extends DomainEvent {
 
 		$this->slotsUpdate = $slotsUpdate;
 		$this->newRevision = $newRevision;
-		$this->oldRevision = $oldrevision;
+		$this->oldRevision = $oldRevision;
 		$this->editResult = $editResult;
 		$this->tags = $tags;
 		$this->flags = $flags;
 		$this->patrolStatus = $patrolStatus;
 	}
 
+	private int $patrolStatus;
+
+	/**
+	 * Whether the edit created the page.
+	 */
 	public function isNew(): bool {
 		return $this->oldRevision === null;
 	}
 
+	/**
+	 * Returns the page that was edited.
+	 */
 	public function getPage(): PageIdentity {
 		return $this->newRevision->getPage();
 	}
 
+	/**
+	 * Returns the user that performed the edit.
+	 */
 	public function getAuthor(): UserIdentity {
 		return $this->newRevision->getUser( RevisionRecord::RAW );
 	}
 
+	/**
+	 * Page content changed by the edit. Can be used to determine which slots
+	 * were changed, and whether slots were added or removed.
+	 */
 	public function getSlotsUpdate(): RevisionSlotsUpdate {
 		return $this->slotsUpdate;
 	}
 
+	/**
+	 * Whether the given slot was modified by the edit.
+	 * Slots that were removed do not count as modified.
+	 */
 	public function isModifiedSlot( string $slotRole ): bool {
 		return $this->getSlotsUpdate()->isModifiedSlot( $slotRole );
 	}
 
 	/**
-	 * @return ?EditResult
+	 * An EditResult representing the effects of an edit.
+	 * Can be used to determine whether the edit was a revert
+	 * and which edits were reverted.
+	 * Will be null for page creations.
 	 */
 	public function getEditResult(): ?EditResult {
 		return $this->editResult;
 	}
 
 	/**
-	 * @return ?RevisionRecord
+	 * Returned the revision that used to be current before the edit.
+	 * Will be null if the edit created the page.
+	 * Will be the same as $newRevision if the edit was a "null-edit".
+	 * Note that this is not necessarily the revision the edit was based
+	 * on: in the case of edit conflicts, manual reverts, or imports,
+	 * the base revision at the beginning of the edit process may be
+	 * different from the parent revision after the conclusion of the edit
+	 * process.
 	 */
 	public function getOldRevision(): ?RevisionRecord {
 		return $this->oldRevision;
 	}
 
 	/**
-	 * @return RevisionRecord
+	 * The revision that became the current one because of the edit.
 	 */
 	public function getNewRevision(): RevisionRecord {
 		return $this->newRevision;
 	}
 
 	/**
+	 * Returns any flags set for the edit.
+	 * @see PageUpdater::setFlags()
 	 * @see global EDIT_XXX constants
-	 * @return int
 	 */
 	public function getFlags(): int {
 		return $this->flags;
 	}
 
 	/**
+	 * Checks flags set for the edit.
+	 * This is a bitmap check.
+	 * This returns true if all bits that are set in $flags were also
+	 * set for the edit.
+	 *
+	 * @see PageUpdater::setFlags()
 	 * @see global EDIT_XXX constants
 	 */
 	public function hasFlag( int $flag ): bool {
@@ -119,16 +179,17 @@ class PageUpdatedEvent extends DomainEvent {
 	}
 
 	/**
+	 * Returns any tags applied to the edit.
 	 * @see ChangeTags
-	 * @return string[]
 	 */
 	public function getTags(): array {
 		return $this->tags;
 	}
 
 	/**
+	 * Returns the edit's initial patrol status.
+	 * @see PageUpdater::setRcPatrolStatus()
 	 * @see RecentChange::PRC_XXX
-	 * @return int
 	 */
 	public function getPatrolStatus(): int {
 		return $this->patrolStatus;
