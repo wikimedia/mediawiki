4f327a6b56249b532c6953e2b97001a578707ce0
Siddharth VP <siddharthvp@gmail.com>
2024-11-28 16:28:26 +0530

    htmlform: support namespace parameter for titlesmultiselect fields
    Change-Id: If9217731be62694a0d953f6405da7677131315ad

--- includes/widget/TitlesMultiselectWidget.php
+++ includes/widget/TitlesMultiselectWidget.php
@@ -14,6 +14,10 @@ class TitlesMultiselectWidget extends TagMultiselectWidget {
 	protected $showMissing = null;
 	/** @var bool|null */
 	protected $excludeDynamicNamespaces = null;
+	/** @var int|null */
+	protected $namespace = null;
+	/** @var bool|null */
+	protected $relative = null;
 	/** @var bool|null */
 	protected $allowEditTags = null;
 
@@ -22,6 +26,8 @@ class TitlesMultiselectWidget extends TagMultiselectWidget {
 	 *   - bool $config['showMissing'] Show missing pages in the typeahead dropdown
 	 *     (ie. allow adding pages that don't exist)
 	 *   - bool $config['excludeDynamicNamespaces'] Exclude pages in negative namespaces
+	 *   - bool $config['namespace'] Shows pages only from the specified namespace
+	 *   - bool $config['relative'] Include namespace names in form data, if 'namespace' config is used
 	 *   - bool $config['allowEditTags'] Allow editing of the tags by clicking them
 	 */
 	public function __construct( array $config = [] ) {
@@ -33,6 +39,12 @@ class TitlesMultiselectWidget extends TagMultiselectWidget {
 		}
 		if ( isset( $config['excludeDynamicNamespaces'] ) ) {
 			$this->excludeDynamicNamespaces = $config['excludeDynamicNamespaces'];
+		}
+		if ( isset( $config['namespace'] ) ) {
+			$this->namespace = $config['namespace'];
+		}
+		if ( isset( $config['relative'] ) ) {
+			$this->relative = $config['relative'];
 		}
 		if ( isset( $config['allowEditTags'] ) ) {
 			$this->allowEditTags = $config['allowEditTags'];
@@ -51,6 +63,12 @@ class TitlesMultiselectWidget extends TagMultiselectWidget {
 		}
 		if ( $this->excludeDynamicNamespaces !== null ) {
 			$config['excludeDynamicNamespaces'] = $this->excludeDynamicNamespaces;
+		}
+		if ( $this->namespace !== null ) {
+			$config['namespace'] = $this->namespace;
+		}
+		if ( $this->relative !== null ) {
+			$config['relative'] = $this->relative;
 		}
 		if ( $this->allowEditTags !== null ) {
 			$config['allowEditTags'] = $this->allowEditTags;
