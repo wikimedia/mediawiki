57d9755ff714096cae4b0f3e39b878f1adbf99c0
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-12-02 14:02:27 +0100

    Fix missing split group error log output
    Changes introduced in I550b11f81c15eba01359c30e5c59db0715afd4cd for
    T379764 resulted in the log files for the phpunit split groups
    being generated with invalid names (`phpunit_output_1_1.log` instead
    of `phpunit_output_1_database.log`). This meant the
    PhpUnitConsoleLogOutputProcessor was no longer able to find the log
    files to present a summary at the end of a failed run.
    
    This patch fixes the bug, and also goes some way to refactoring the
    code to make it more testable and debuggable.
    
    Bug: T378481
    Change-Id: I0c345ea56529e6768f80a00440e9a13aa05ad069

---
+++ includes/composer/PhpUnitSplitter/SplitGroupExecutor.php
@@ -0,0 +1,101 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace MediaWiki\Composer\PhpUnitSplitter;
+
+use Composer\Script\Event;
+use MediaWiki\Composer\ComposerLaunchParallel;
+use MediaWiki\Composer\ComposerSystemInterface;
+use Shellbox\Command\UnboxedExecutor;
+use Shellbox\Command\UnboxedResult;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class SplitGroupExecutor {
+
+	private UnboxedExecutor $executor;
+	private ?Event $event;
+	private ComposerSystemInterface $composerSystemInterface;
+
+	public function __construct(
+		UnboxedExecutor $shellExecutor,
+		?Event $event,
+		?ComposerSystemInterface $composerSystemInterface
+	) {
+		$this->executor = $shellExecutor;
+		$this->event = $event;
+		$this->composerSystemInterface = $composerSystemInterface ?? new ComposerSystemInterface();
+	}
+
+	public function executeSplitGroup(
+		string $testSuite,
+		array $groups,
+		array $excludeGroups,
+		?string $resultsCacheFile = null,
+		?int $groupId = null
+	): UnboxedResult {
+		$command = $this->executor->createCommand()
+			->params(
+				'composer', 'run',
+				'--timeout=0',
+				'phpunit:entrypoint',
+				'--',
+				'--testsuite', $testSuite,
+				'--exclude-group', implode( ",", $excludeGroups )
+			);
+		if ( count( $groups ) ) {
+			$command->params( '--group', implode( ',', $groups ) );
+		}
+		if ( $resultsCacheFile ) {
+			$command->params(
+				"--cache-result-file=$resultsCacheFile"
+			);
+		}
+		$command->includeStderr( true );
+		if ( $groupId !== null ) {
+			$command->environment( [ 'MW_PHPUNIT_SPLIT_GROUP_ID' => $groupId ] );
+		}
+		$this->consoleLog( "Running command '" . $command->getCommandString() . "' ..." . PHP_EOL );
+		return $command->execute();
+	}
+
+	private function warning( string $warning ) {
+		if ( $this->event ) {
+			$this->event->getIO()->warning( $warning );
+		}
+	}
+
+	private function composerLog( string $text ) {
+		if ( $this->event ) {
+			$this->event->getIO()->write( $text );
+		}
+	}
+
+	private function consoleLog( string $outputText ) {
+		$this->composerSystemInterface->print( $outputText );
+	}
+
+	public function runLinearFallback( string $testSuite ) {
+		$this->warning( "Test suite splitting failed - falling back to linear run" );
+		$this->composerLog( "Running " . $testSuite . " phpunit suite databaseless tests..." );
+		$databaselessResult = $this->executeSplitGroup(
+			$testSuite,
+			ComposerLaunchParallel::DATABASELESS_GROUPS,
+			ComposerLaunchParallel::getDatabaselessExcludeGroups()
+		);
+		$this->consoleLog( $databaselessResult->getStdout() );
+		if ( $databaselessResult->getExitCode() !== 0 ) {
+			return;
+		}
+		$this->composerLog( "Running " . $testSuite . " phpunit suite database tests..." );
+		$databaseResult = $this->executeSplitGroup(
+			$testSuite,
+			ComposerLaunchParallel::DATABASE_GROUPS,
+			ComposerLaunchParallel::getDatabaseExcludeGroups()
+		);
+		$this->consoleLog( $databaseResult->getStdout() );
+	}
+
+}
---
+++ tests/phpunit/unit/includes/composer/ComposerLaunchParallelTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace MediaWiki\Tests\Unit\composer;
+
+use MediaWiki\Composer\ComposerLaunchParallel;
+use MediaWiki\Composer\ComposerSystemInterface;
+use MediaWiki\Composer\PhpUnitSplitter\SplitGroupExecutor;
+use PHPUnit\Framework\TestCase;
+use Shellbox\Command\UnboxedResult;
+use Wikimedia\TestingAccessWrapper;
+
+/**
+ * @license GPL-2.0-or-later
+ * @covers \MediaWiki\Composer\ComposerLaunchParallel
+ */
+class ComposerLaunchParallelTest extends TestCase {
+
+	private function getMockCommandResult(): UnboxedResult {
+		$result = $this->createMock( UnboxedResult::class );
+		$result->method( 'getStdout' )
+			->willReturn( 'logs from test' );
+		$result->method( 'getExitCode' )
+			->willReturn( 1 );
+		return $result;
+	}
+
+	private function getMockSystemInterface( string $logFileName ): ComposerSystemInterface {
+		$systemInterface = $this->createMock( ComposerSystemInterface::class );
+		$systemInterface->expects( $this->once() )
+			->method( 'exit' )
+			->with( 1 );
+		$systemInterface->expects( $this->once() )
+			->method( 'putFileContents' )
+			->with( $logFileName, 'logs from test' );
+		return $systemInterface;
+	}
+
+	public function testExecuteDatabaseSuite() {
+		$executor = $this->createMock( SplitGroupExecutor::class );
+		$systemInterface = $this->getMockSystemInterface( 'phpunit_output_1_database.log' );
+		$composerLaunchParallel = new ComposerLaunchParallel(
+			[ 'Database' ],
+			[ 'Broken' ],
+			null,
+			$executor,
+			$systemInterface
+		);
+		$result = $this->getMockCommandResult();
+		$executor->expects( $this->once() )
+			->method( 'executeSplitGroup' )
+			->with( 'split_group_1',
+				[ 'Database' ],
+				[ 'Broken' ],
+				".phpunit_group_1_database.result.cache",
+				1
+			)
+			->willReturn( $result );
+		$wrapper = TestingAccessWrapper::newFromObject( $composerLaunchParallel );
+		$wrapper->runTestSuite( 1 );
+	}
+
+	public function testExecuteDatabaselessSuite() {
+		$executor = $this->createMock( SplitGroupExecutor::class );
+		$systemInterface = $this->getMockSystemInterface( 'phpunit_output_1_databaseless.log' );
+		$composerLaunchParallel = new ComposerLaunchParallel(
+			[],
+			[ 'Broken', 'Standalone', 'Database' ],
+			null,
+			$executor,
+			$systemInterface
+		);
+		$result = $this->getMockCommandResult();
+		$executor->expects( $this->once() )
+			->method( 'executeSplitGroup' )
+			->with( 'split_group_1',
+				[],
+				[ 'Broken', 'Standalone', 'Database' ],
+				".phpunit_group_1_databaseless.result.cache",
+				1
+			)
+			->willReturn( $result );
+		$wrapper = TestingAccessWrapper::newFromObject( $composerLaunchParallel );
+		$wrapper->runTestSuite( 1 );
+	}
+}
