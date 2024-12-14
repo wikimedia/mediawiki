770fa6a60418ba579cc3d0720b719f57d9a8dedd
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-12-14 10:53:52 -0500

    Add namespaces for Coptic
    Translations by User:Bloomaround on translatewiki.
    
    Bug: T381463
    Change-Id: If4fba03f7ae3aa5a5f6113278bb2412175818c26

--- languages/messages/MessagesCop.php
+++ languages/messages/MessagesCop.php
@@ -4,7 +4,38 @@
  * @file
  * @ingroup Languages
  *
+ * @author Bloomaround
  * @author Amir E. Aharoni
  */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Ⲫⲙⲉⲥⲟⲧⲏⲥ',
+	NS_SPECIAL          => 'Ⲫⲁ_ϯⲟⲓⲕⲟⲛⲟⲙⲓⲁ̀',
+	NS_TALK             => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ',
+	NS_USER             => 'Ⲫⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ',
+	NS_USER_TALK        => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲙ̀ⲫⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ',
+	NS_PROJECT_TALK     => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲛ̀$1',
+	NS_FILE             => 'Ⲡⲕⲟⲓϩⲓ',
+	NS_FILE_TALK        => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲛ̀ⲕⲟⲓϩⲓ',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲙ̀MediaWiki',
+	NS_TEMPLATE         => 'Ⲯⲙⲟⲧ',
+	NS_TEMPLATE_TALK    => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲛ̀ⲥⲙⲟⲧ',
+	NS_HELP             => 'Ⲫⲃⲟⲏⲑⲟⲥ',
+	NS_HELP_TALK        => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲙ̀ⲃⲟⲏⲑⲟⲥ',
+	NS_CATEGORY         => 'Ⲡⲅⲉⲛⲟⲥ',
+	NS_CATEGORY_TALK    => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲛ̀ⲅⲉⲛⲟⲥ',
+];
+
+$namespaceGenderAliases = [
+	NS_USER => [
+		'male'   => 'Ⲫⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ',
+		'female' => 'Ⲑⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ'
+	],
+	NS_USER_TALK => [
+		'male'   => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲙ̀ⲫⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ',
+		'female' => 'Ⲫⲙⲁ_ⲛ̀ⲥⲁϫⲓ_ⲛ̀ⲑⲣⲉϥⲉⲣⲭⲣⲁⲥⲑⲉ'
+	],
+];
 
 $linkTrail = '/^([\x{2C80}-\x{2CFF}]+)(.*)$/sDu';
