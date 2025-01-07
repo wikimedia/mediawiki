dc9d5be0a35ddabaf91961a3c043f972d747dd0d
Daimona Eaytoy <daimona.wiki@gmail.com>
2025-01-07 19:18:03 +0100

    checkboxShift.js: prevent handler from being applied twice
    Move the click handler to a name function, that can later be passed to
    jQuery's `off` to make sure the handler isn't added more than once.
    
    This allows us to trigger checkboxShift (via the wikipage.content hook)
    on a checkbox container whose content can change over time, with old
    checkboxes already having the listener attached.
    
    Note that this click handler seems to be mostly idempotent, and multiple
    invocations on the same checkboxes shouldn't have side effects, unless
    those checkboxes have `change` listeners attached that are themselves
    not idempotent. Nonetheless, it generally seems preferable to only run
    this code once.
    
    Bug: T318261
    Change-Id: I80b8425b8ab5eb9393177a60d984b3e8b03ae8f0

--- resources/src/mediawiki.page.ready/checkboxShift.js
+++ resources/src/mediawiki.page.ready/checkboxShift.js
@@ -1,33 +1,38 @@
+let prev;
+
+function clickHandler( e ) {
+	// When our boxes are clicked and one has been clicked before...
+	if ( prev && e.shiftKey ) {
+		// Check or uncheck this one and all in-between checkboxes,
+		// except for disabled ones
+		const $checkboxes = e.data.$checkboxes;
+		$checkboxes
+			.slice(
+				Math.min( $checkboxes.index( prev ), $checkboxes.index( e.target ) ),
+				Math.max( $checkboxes.index( prev ), $checkboxes.index( e.target ) ) + 1
+			)
+			.filter( function () {
+				return !this.disabled && this.checked !== e.target.checked;
+			} )
+			.prop( 'checked', e.target.checked )
+			// Since the state change is a consequence of direct user action,
+			// fire the 'change' event (see T313077).
+			.trigger( 'change' );
+	}
+	// Either way, remember this as the last clicked one
+	prev = e.target;
+}
+
 /**
  * Enable checkboxes to be checked or unchecked in a row by clicking one,
  * holding shift and clicking another one.
  *
  * @method checkboxShift
  * @memberof module:mediawiki.page.ready
- * @param {jQuery} $box
+ * @param {jQuery} $checkboxes
  */
-module.exports = function ( $box ) {
-	let prev;
-	// When our boxes are clicked..
-	$box.on( 'click', ( e ) => {
-		// And one has been clicked before...
-		if ( prev && e.shiftKey ) {
-			// Check or uncheck this one and all in-between checkboxes,
-			// except for disabled ones
-			$box
-				.slice(
-					Math.min( $box.index( prev ), $box.index( e.target ) ),
-					Math.max( $box.index( prev ), $box.index( e.target ) ) + 1
-				)
-				.filter( function () {
-					return !this.disabled && this.checked !== e.target.checked;
-				} )
-				.prop( 'checked', e.target.checked )
-				// Since the state change is a consequence of direct user action,
-				// fire the 'change' event (see T313077).
-				.trigger( 'change' );
-		}
-		// Either way, remember this as the last clicked one
-		prev = e.target;
-	} );
+module.exports = function ( $checkboxes ) {
+	$checkboxes
+		.off( 'click', clickHandler )
+		.on( 'click', { $checkboxes: $checkboxes }, clickHandler );
 };
