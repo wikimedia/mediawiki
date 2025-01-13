4df33709ceffab58d0eb010d7959db3e6b50a4f3
Atieno <pnjira@wikimedia.org>
2025-01-13 18:02:42 +0300

    REST: Search: Validate responses against response schemas in tests
    Bug: T382455
    Change-Id: Ia10c5226dad5162b35c4e9b139876de735cf4a1b

--- tests/api-testing/REST/Search.js
+++ tests/api-testing/REST/Search.js
@@ -4,8 +4,13 @@ const { action, assert, REST, utils, wiki } = require( 'api-testing' );
 const superagent = require( 'superagent' );
 const xml2js = require( 'xml2js' );
 
+const chai = require( 'chai' );
+const expect = chai.expect;
+
+const chaiResponseValidator = require( 'chai-openapi-response-validator' ).default;
+
 describe( 'Search', () => {
-	const client = new REST( 'rest.php/v1' );
+	const client = new REST( 'rest.php' );
 	const sharedTitleTerm = 'X' + utils.uniq(); // NOTE: must start with upper-case letter
 	const pageWithBothTerms = utils.title( `${ sharedTitleTerm }XXX` );
 	const pageWithOneTerm = utils.title( `${ sharedTitleTerm }YYY` );
@@ -14,6 +19,7 @@ describe( 'Search', () => {
 	const searchTerm2 = utils.uniq();
 	let alice;
 	let mindy;
+	let openApiSpec;
 
 	before( async () => {
 		alice = await action.alice();
@@ -22,18 +28,28 @@ describe( 'Search', () => {
 		await alice.edit( pageWithOneTerm, { text: searchTerm2 } );
 		await alice.edit( pageWithOwnTitle, { text: pageWithOwnTitle } );
 		await wiki.runAllJobs();
+
+		const { status, text } = await client.get( '/specs/v0/module/-' );
+		assert.deepEqual( status, 200 );
+
+		openApiSpec = JSON.parse( text );
+		chai.use( chaiResponseValidator( openApiSpec ) );
 	} );
 
 	describe( 'GET /search/page?q={term}', () => {
 		it( 'should return empty array when search term has no title or text matches', async () => {
 			const nonExistentTerm = utils.uniq();
-			const { body, headers } = await client.get( `/search/page?q=${ nonExistentTerm }` );
+			const res = await client.get( `/v1/search/page?q=${ nonExistentTerm }` );
+			const { body, headers } = res;
 			const noResultsResponse = { pages: [] };
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.deepEqual( noResultsResponse, body );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return array of pages when there is only a text match', async () => {
-			const { body, headers } = await client.get( `/search/page?q=${ searchTerm }` );
+			const res = await client.get( `/v1/search/page?q=${ searchTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			const returnPage = body.pages[ 0 ];
@@ -52,9 +68,13 @@ describe( 'Search', () => {
 				assert.notMatch( headers[ 'cache-control' ], /\bmax-age=0*[1-9]\b/ );
 				assert.notMatch( headers[ 'cache-control' ], /\bs-maxage=0*[1-9]\b/ );
 			}
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
+
 		} );
 		it( 'should return array of pages when there is only title match', async () => {
-			const { body, headers } = await client.get( `/search/page?q=${ pageWithBothTerms }` );
+			const res = await client.get( `/v1/search/page?q=${ pageWithBothTerms }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			const returnPage = body.pages[ 0 ];
@@ -65,9 +85,12 @@ describe( 'Search', () => {
 			assert.nestedPropertyVal( returnPage, 'thumbnail', null );
 			assert.nestedPropertyVal( returnPage, 'description', null );
 			assert.nestedPropertyVal( returnPage, 'matched_title', null );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return a single page when there is a title and text match on the same page', async () => {
-			const { body, headers } = await client.get( `/search/page?q=${ pageWithOwnTitle }` );
+			const res = await client.get( `/v1/search/page?q=${ pageWithOwnTitle }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			const returnPage = body.pages[ 0 ];
@@ -78,19 +101,27 @@ describe( 'Search', () => {
 			assert.nestedPropertyVal( returnPage, 'thumbnail', null );
 			assert.nestedPropertyVal( returnPage, 'description', null );
 			assert.nestedPropertyVal( returnPage, 'matched_title', null );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return two pages when both pages match', async () => {
-			const { body, headers } = await client.get( `/search/page?q=${ searchTerm2 }` );
+			const res = await client.get( `/v1/search/page?q=${ searchTerm2 }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 2 );
 			const returnedPages = [ body.pages[ 0 ].title, body.pages[ 1 ].title ];
 			const expectedPages = [ pageWithBothTerms, pageWithOneTerm ];
 			assert.equal( expectedPages.sort().join( '|' ), returnedPages.sort().join( '|' ) );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return only one page when two pages match but limit is 1', async () => {
-			const { body, headers } = await client.get( `/search/page?q=${ searchTerm2 }&limit=1` );
+			const res = await client.get( `/v1/search/page?q=${ searchTerm2 }&limit=1` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should not return results when page with term has been deleted', async () => {
 			const pageToDelete = 'Delete Page';
@@ -102,9 +133,12 @@ describe( 'Search', () => {
 				token: await mindy.token( 'csrf' )
 			}, 'POST' );
 			await wiki.runAllJobs();
-			const { body, headers } = await client.get( `/search/page?q=${ deleteTerm }` );
+			const res = await client.get( `/v1/search/page?q=${ deleteTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 0 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should ignore duplicate redirect source and target if both pages are a match', async () => {
 			const redirectSource = utils.title( 'redirect_source_' );
@@ -118,27 +152,37 @@ describe( 'Search', () => {
 			);
 
 			await wiki.runAllJobs();
-			const { body, headers } = await client.get( `/search/page?q=${ uniquePageText }` );
+			const res = await client.get( `/v1/search/page?q=${ uniquePageText }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			assert.nestedPropertyVal( body.pages[ 0 ], 'title', redirectTargetTitle );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 	} );
 
 	describe( 'GET /search/title?q={term}', () => {
 		it( 'should return empty array when search term has no title matches', async () => {
 			const nonExistentTerm = utils.uniq();
-			const { body, headers } = await client.get( `/search/title?q=${ nonExistentTerm }` );
+			const res = await client.get( `/v1/search/title?q=${ nonExistentTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 0 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should not return pages when there is only a text match', async () => {
-			const { body, headers } = await client.get( `/search/title?q=${ searchTerm }` );
+			const res = await client.get( `/v1/search/title?q=${ searchTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 0 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return array of pages when there is a title match', async () => {
-			const { body, headers } = await client.get( `/search/title?q=${ pageWithBothTerms }` );
+			const res = await client.get( `/v1/search/title?q=${ pageWithBothTerms }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			const returnPage = body.pages[ 0 ];
@@ -154,19 +198,27 @@ describe( 'Search', () => {
 			assert.nestedProperty( headers, 'cache-control' );
 			assert.match( headers[ 'cache-control' ], /\bpublic\b/ );
 			assert.match( headers[ 'cache-control' ], /\bmax-age=[1-9]\d*/ );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return two pages when both pages match', async () => {
-			const { body, headers } = await client.get( `/search/title?q=${ sharedTitleTerm }` );
+			const res = await client.get( `/v1/search/title?q=${ sharedTitleTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 2 );
 			const returnedPages = [ body.pages[ 0 ].title, body.pages[ 1 ].title ];
 			const expectedPages = [ pageWithBothTerms, pageWithOneTerm ];
 			assert.equal( expectedPages.sort().join( '|' ), returnedPages.sort().join( '|' ) );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should return only one page when two pages match but limit is 1', async () => {
-			const { body, headers } = await client.get( `/search/title?q=${ sharedTitleTerm }&limit=1` );
+			const res = await client.get( `/v1/search/title?q=${ sharedTitleTerm }&limit=1` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should not return deleted page', async () => {
 			const deleteTerm = utils.uniq();
@@ -178,9 +230,12 @@ describe( 'Search', () => {
 				token: await mindy.token( 'csrf' )
 			}, 'POST' );
 			await wiki.runAllJobs();
-			const { body, headers } = await client.get( `/search/title?q=${ deleteTerm }` );
+			const res = await client.get( `/v1/search/title?q=${ deleteTerm }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 0 );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 		it( 'should include redirect for page if one exists', async () => {
 			const redirectSource = utils.title( 'redirect_source_' );
@@ -194,11 +249,14 @@ describe( 'Search', () => {
 
 			await wiki.runAllJobs();
 
-			const { body, headers } = await client.get( `/search/title?q=${ redirectSourceTitle }` );
+			const res = await client.get( `/v1/search/title?q=${ redirectSourceTitle }` );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/json/ );
 			assert.lengthOf( body.pages, 1 );
 			assert.nestedPropertyVal( body.pages[ 0 ], 'title', redirectTargetTitle );
 			assert.nestedPropertyVal( body.pages[ 0 ], 'matched_title', redirectSourceTitle );
+			// eslint-disable-next-line no-unused-expressions
+			expect( res ).to.satisfyApiSpec;
 		} );
 	} );
 
@@ -216,15 +274,17 @@ describe( 'Search', () => {
 		superagent.parse[ 'application/opensearchdescription+xml' ] = parseXML;
 
 		it( 'should return an OpenSearch Description document', async () => {
-			const { body, headers } = await client.get( '/search' );
+			const res = await client.get( '/v1/search' );
+			const { body, headers } = res;
 			assert.match( headers[ 'content-type' ], /^application\/opensearchdescription\+xml/ );
 			assert.nestedProperty( body, 'OpenSearchDescription' );
 			// TODO: assert more about the structure
 		} );
 
 		it( 'should support plain XML output', async () => {
-			const { body, headers } = await client.get( '/search' )
+			const res = await client.get( '/v1/search' )
 				.query( { ctype: 'application/xml' } );
+			const { body, headers } = res;
 
 			assert.match( headers[ 'content-type' ], /^application\/xml/ );
 			assert.nestedProperty( body, 'OpenSearchDescription' );
