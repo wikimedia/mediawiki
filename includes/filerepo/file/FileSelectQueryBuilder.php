d8e9413f2b0053d99b5968459121ce65f40a52b8
Amir Sarabadani <ladsgroup@gmail.com>
2025-01-21 11:52:52 +0100

    file: Add support for read new in FileSelectQueryBuilder
    Depends-On: I5c7c192a063a14bbe14755aee5342e2330834fe9
    Bug: T383496
    Change-Id: Ia13910fced0dc040e979bc840c1d9bf4f685526e

--- includes/filerepo/file/FileSelectQueryBuilder.php
+++ includes/filerepo/file/FileSelectQueryBuilder.php
@@ -21,10 +21,14 @@
 namespace MediaWiki\FileRepo\File;
 
 use InvalidArgumentException;
+use MediaWiki\MainConfigNames;
+use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\IReadableDatabase;
 use Wikimedia\Rdbms\SelectQueryBuilder;
 
 class FileSelectQueryBuilder extends SelectQueryBuilder {
+
+	private int $migrationStage;
 
 	/**
 	 * @internal use ::newFor* instead.
@@ -35,10 +39,21 @@ class FileSelectQueryBuilder extends SelectQueryBuilder {
 	 */
 	public function __construct( IReadableDatabase $db, string $type = 'file', array $options = [] ) {
 		parent::__construct( $db );
+		$this->migrationStage = MediaWikiServices::getInstance()->getMainConfig()->get(
+			MainConfigNames::FileSchemaMigrationStage
+		);
 		if ( $type === 'file' ) {
-			$this->initFile( $options );
+			if ( $this->migrationStage & SCHEMA_COMPAT_READ_OLD ) {
+				$this->initFileOld( $options );
+			} else {
+				$this->initFileNew( $options );
+			}
 		} elseif ( $type === 'oldfile' ) {
-			$this->initOldFile( $options );
+			if ( $this->migrationStage & SCHEMA_COMPAT_READ_OLD ) {
+				$this->initOldFileOld( $options );
+			} else {
+				$this->initOldFileNew( $options );
+			}
 		} elseif ( $type === 'archivedfile' ) {
 			$this->initArchivedFile( $options );
 		} else {
@@ -58,7 +73,7 @@ class FileSelectQueryBuilder extends SelectQueryBuilder {
 		return new FileSelectQueryBuilder( $db, 'archivedfile', $options );
 	}
 
-	private function initFile( $options ) {
+	private function initFileOld( $options ) {
 		$this->table( 'image' )
 			->join( 'actor', 'image_actor', 'actor_id=img_actor' )
 			->join(
@@ -97,7 +112,59 @@ class FileSelectQueryBuilder extends SelectQueryBuilder {
 		}
 	}
 
-	private function initOldFile( $options ) {
+	private function initFileNew( $options ) {
+		$subquery = $this->newSubquery();
+		$subquery->table( 'file' )
+			->join( 'filerevision', null, 'file_latest = fr_id' )
+			->join( 'filetypes', null, 'file_type = ft_id' )
+			->join( 'actor', 'image_actor', 'actor_id=fr_actor' )
+			->join(
+				'comment',
+				'comment_img_description',
+				'comment_img_description.comment_id = fr_description_id'
+			);
+
+		if ( !in_array( 'omit-nonlazy', $options, true ) ) {
+			$subquery->fields(
+				[
+					'img_file_id' => 'file_id',
+					'img_filerevision_id' => 'fr_id',
+					'img_name' => 'file_name',
+					'img_size' => 'fr_size',
+					'img_width' => 'fr_width',
+					'img_height' => 'fr_height',
+					'img_metadata' => 'fr_metadata',
+					'img_bits' => 'fr_bits',
+					'img_media_type' => 'ft_media_type',
+					'img_major_mime' => 'ft_major_mime',
+					'img_minor_mime' => 'ft_minor_mime',
+					'img_timestamp' => 'fr_timestamp',
+					'img_sha1' => 'fr_sha1',
+					'img_actor' => 'fr_actor',
+					'img_user' => 'image_actor.actor_user',
+					'img_user_text' => 'image_actor.actor_name',
+					'img_description_text' => 'comment_img_description.comment_text',
+					'img_description_data' => 'comment_img_description.comment_data',
+					'img_description_cid' => 'comment_img_description.comment_id'
+				]
+			);
+		}
+		if ( !in_array( 'omit-lazy', $options, true ) ) {
+			// Note: Keep this in sync with LocalFile::getLazyCacheFields() and
+			// LocalFile::loadExtraFromDB()
+			$subquery->field( 'fr_metadata', 'img_metadata' );
+		}
+
+		$subquery->where( [ 'file_deleted' => 0 ] );
+
+		// Without the wrapper, callers can't make conditions
+		// on the old field names so all of them would need updating.
+		// See https://stackoverflow.com/a/8370146
+		$this->field( '*' )
+			->from( $subquery );
+	}
+
+	private function initOldFileOld( $options ) {
 		$this->table( 'oldimage' )
 			->join( 'actor', 'oldimage_actor', 'actor_id=oi_actor' )
 			->join(
@@ -135,6 +202,55 @@ class FileSelectQueryBuilder extends SelectQueryBuilder {
 			// LocalFile::loadExtraFromDB()
 			$this->field( 'oi_metadata' );
 		}
+	}
+
+	private function initOldFileNew( $options ) {
+		$subquery = $this->newSubquery();
+		$subquery->table( 'filerevision' )
+			->join( 'file', null, 'fr_file = file_id' )
+			->join( 'filetypes', null, 'file_type = ft_id' )
+			->join( 'actor', 'oldimage_actor', 'actor_id=oi_actor' )
+			->join(
+				'comment',
+				'comment_oi_description',
+				'comment_oi_description.comment_id = oi_description_id'
+			);
+
+		if ( !in_array( 'omit-nonlazy', $options, true ) ) {
+			$subquery->fields(
+				[
+					'oi_file_id' => 'file_id',
+					'oi_filerevision_id' => 'fr_id',
+					'oi_name' => 'file_name',
+					'oi_archive_name' => 'fr_archive_name',
+					'oi_size' => 'fr_size',
+					'oi_width' => 'fr_width',
+					'oi_height' => 'fr_height',
+					'oi_bits' => 'fr_bits',
+					'oi_media_type' => 'ft_media_type',
+					'oi_major_mime' => 'ft_major_mime',
+					'oi_minor_mime' => 'ft_minor_mime',
+					'oi_timestamp' => 'fr_timestamp',
+					'oi_deleted' => 'fr_deleted',
+					'oi_sha1' => 'fr_sha1',
+					'oi_actor' => 'fr_actor',
+					'oi_user' => 'oldimage_actor.actor_user',
+					'oi_user_text' => 'oldimage_actor.actor_name',
+					'oi_description_text' => 'comment_oi_description.comment_text',
+					'oi_description_data' => 'comment_oi_description.comment_data',
+					'oi_description_cid' => 'comment_oi_description.comment_id'
+				]
+			);
+		}
+		if ( !in_array( 'omit-lazy', $options, true ) ) {
+			// Note: Keep this in sync with LocalFile::getLazyCacheFields() and
+			// LocalFile::loadExtraFromDB()
+			$subquery->field( 'fr_metadata', 'oi_metadata' );
+		}
+
+		$subquery->where( 'file_latest != fr_id' );
+		$this->field( '*' )
+			->from( $subquery );
 	}
 
 	private function initArchivedFile( $options ) {
