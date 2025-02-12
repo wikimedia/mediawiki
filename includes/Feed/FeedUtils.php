14205eb16d32439b745537906c1d37146b60793b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2025-02-12 11:29:35 +0100

    Escape newpage message in FeedUtils
    Bug: T386175
    Change-Id: Ida72c4c03c379cf5a340b2f229f0fe842694559c

--- includes/Feed/FeedUtils.php
+++ includes/Feed/FeedUtils.php
@@ -251,7 +251,7 @@ class FeedUtils {
 				$diffText = Html::rawElement(
 					'p',
 					[],
-					Html::rawElement( 'b', [], wfMessage( 'newpage' )->text() )
+					Html::element( 'b', [], wfMessage( 'newpage' )->text() )
 				);
 				$diffText .= Html::rawElement( 'div', [], $html );
 			}
