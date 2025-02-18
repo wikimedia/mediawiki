90ca609265709310d19b75eb4f96870d964318fa
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2025-02-18 10:19:13 -0500

    Add namespaces and linkTrail for Chavacano (cbk-zam)
    Bug: T386722
    Change-Id: Iba25b22aac9d5d8cecc8eab8c373820321850ca9

--- languages/messages/MessagesCbk_zam.php
+++ languages/messages/MessagesCbk_zam.php
@@ -6,3 +6,40 @@
  */
 
 $fallback = 'es';
+
+$namespaceNames = [
+	NS_MEDIA          => 'Medio',
+	NS_SPECIAL        => 'Especial',
+	NS_TALK           => 'Discusión',
+	NS_USER           => 'Usuario',
+	NS_USER_TALK      => 'Discusión_del_usuario',
+	NS_PROJECT_TALK   => 'Discusión_del_$1',
+	NS_FILE           => 'File',
+	NS_FILE_TALK      => 'Discusión_del_file',
+	NS_MEDIAWIKI      => 'MediaWiki',
+	NS_MEDIAWIKI_TALK => 'Discusión_del_MediaWiki',
+	NS_TEMPLATE       => 'Plantilla',
+	NS_TEMPLATE_TALK  => 'Discusión_del_plantilla',
+	NS_HELP           => 'Ayuda',
+	NS_HELP_TALK      => 'Discusión_del_ayuda',
+	NS_CATEGORY       => 'Categoría',
+	NS_CATEGORY_TALK  => 'Discusión_del_categoría',
+];
+
+$namespaceAliases = [
+	'Imagen'              => NS_FILE,
+	'Imagen_discusión'    => NS_FILE_TALK,
+  'Usuaria'             => NS_USER,
+  'Usuario_discusión'   => NS_USER_TALK,
+  'Usuaria_discusión'   => NS_USER_TALK,
+	'Archivo'             => NS_FILE,
+	'Archivo_discusión'   => NS_FILE_TALK,
+	'MediaWiki_discusión' => NS_MEDIAWIKI_TALK,
+	'Plantilla_discusión' => NS_TEMPLATE_TALK,
+	'Ayuda_discusión'     => NS_HELP_TALK,
+	'Categoría_discusión' => NS_CATEGORY_TALK,
+];
+
+$namespaceGenderAliases = [];
+
+$linkTrail = '/^([a-záéíóúñ]+)(.*)$/sDu';
