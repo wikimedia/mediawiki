3d97eccfe0c6669a5fc3886b15c5699342a1078a
Winston Sung <winston21.sung@gmail.com>
2025-02-18 15:09:45 +0000

    Add Hakka (hak-hans, hak-hant, hak-latn) date time formats
    Bug: T371883
    Change-Id: I63383c32b88e2d5438d27582b2df8ac603210e8d

--- languages/messages/MessagesHak_hans.php
+++ languages/messages/MessagesHak_hans.php
@@ -6,3 +6,18 @@
  */
 
 $fallback = 'hak, hak-hant, hak-latn, zh-hant, zh, zh-hans';
+
+$datePreferences = [
+	'default',
+	'ISO 8601',
+];
+
+$defaultDateFormat = 'hak-hans';
+
+$dateFormats = [
+	'hak-hans time' => 'H:i',
+	'hak-hans date' => 'Y年n月j日（l）',
+	'hak-hans monthonly' => 'Y年n月',
+	'hak-hans both' => 'Y年n月j日（l）H:i',
+	'hak-hans pretty' => 'n月j日',
+];
