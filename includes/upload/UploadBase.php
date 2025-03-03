d695e1e2924ef3aba23b2b6f2711c4e5e3b12e7b
Ammarpad <ammarpad@yahoo.com>
2025-03-03 09:59:22 +0100

    upload: Suppress warnings from iconv()
    iconv() can still emit notices even when '//IGNORE'
    string flag is passed.
    
    Bug: T387690
    Change-Id: I16f1e99f7c25457aa0b35cb428391c42dec7b91d
    (cherry picked from commit 357f2b61e815e071147583e07b388801189462bf)

--- includes/upload/UploadBase.php
+++ includes/upload/UploadBase.php
@@ -1384,7 +1384,9 @@ abstract class UploadBase {
 		}
 
 		if ( $enc !== null ) {
+			AtEase::suppressWarnings();
 			$chunk = iconv( $enc, "ASCII//IGNORE", $chunk );
+			AtEase::restoreWarnings();
 		}
 
 		$chunk = trim( $chunk );
