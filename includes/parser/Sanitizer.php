0efb0e2068b3db08592ff227e86ff241d412e7b5
David Causse <dcausse@wikimedia.org>
2025-03-18 10:36:36 +0100

    Sanitizer::normalizeWhitespace: simplify redundant preg_replace
    The extraneous sequence \r\n is not required.
    Avoid the use of hexcode to avoid future confusions.
    
    Bug: T388733
    Change-Id: I1092ff76ed5e8221e43ea7b70cf0c9d9d3abb1f3
    (cherry picked from commit 6753123a0629de81ce4899958180272736e7ba61)

--- includes/parser/Sanitizer.php
+++ includes/parser/Sanitizer.php
@@ -1171,7 +1171,7 @@ class Sanitizer {
 	}
 
 	private static function normalizeWhitespace( string $text ): string {
-		$normalized = preg_replace( '/(?:\r\n|[\x20\x0d\x0a\x09])+/', ' ', $text );
+		$normalized = preg_replace( '/[ \r\n\t]+/', ' ', $text );
 		if ( $normalized === null ) {
 			wfLogWarning( __METHOD__ . ': Failed to normalize whitespace: ' . preg_last_error() );
 			return '';
