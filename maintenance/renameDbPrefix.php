8c7c119ba690453468293c6b51825314b6047f2d
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2025-04-07 15:03:01 +0100

    Partly test renameDbPrefix.php
    Why:
    * Testing maintenance scripts in core have found a few bugs with
      existing maintenance scripts.
    ** Therefore writing tests for untested scripts helps to make them
       more reliable and avoid future regressions.
    * renameDbPrefix.php has a minor bug where the default value for
      --old and -new are considered not as no prefix but instead the
      prefix of '0', as well as not returning early if the old and new
      prefixes are the same.
    
    What:
    * Create RenameDbPrefixTest which partly tests the script.
    ** This is not a Database test and does not cover the code that
       does the rename because it would be hard to reliably undo the
       renames after the test.
    * Fix the bugs in renameDbPrefix.php found while writing the tests.
    
    Bug: T371167
    Change-Id: I3fbedf91fc3aaa88421b872566203ffdef8602cc

--- maintenance/renameDbPrefix.php
+++ maintenance/renameDbPrefix.php
@@ -50,7 +50,7 @@ class RenameDbPrefix extends Maintenance {
 		$dbName = $this->getConfig()->get( MainConfigNames::DBname );
 
 		// Allow for no old prefix
-		if ( $this->getOption( 'old', 0 ) === '0' ) {
+		if ( $this->getOption( 'old', '0' ) === '0' ) {
 			$old = '';
 		} else {
 			// Use nice safe, sensible, prefixes
@@ -58,7 +58,7 @@ class RenameDbPrefix extends Maintenance {
 			$old = $m[0] ?? false;
 		}
 		// Allow for no new prefix
-		if ( $this->getOption( 'new', 0 ) === '0' ) {
+		if ( $this->getOption( 'new', '0' ) === '0' ) {
 			$new = '';
 		} else {
 			// Use nice safe, sensible, prefixes
@@ -71,6 +71,7 @@ class RenameDbPrefix extends Maintenance {
 		}
 		if ( $old === $new ) {
 			$this->output( "Same prefix. Nothing to rename!\n" );
+			return;
 		}
 
 		$this->output( "Renaming DB prefix for tables of $dbName from '$old' to '$new'\n" );
