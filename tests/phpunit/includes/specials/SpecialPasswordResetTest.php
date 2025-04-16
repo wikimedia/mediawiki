ef1672c4064e22062e76584bd8bc60fee2be4e6d
Máté Szabó <mszabo@wikimedia.org>
2025-04-16 20:56:25 +0200

    specials: Add tests for Special:PasswordReset success path
    Why:
    
    - Special:PasswordReset renders some informative messages on success.
    - It queries request body values to do so, which can cause warnings if a
      client submitted crafted output with multi-value fields.
    - Since the logic is untested, we should add tests before trying to fix
      it.
    
    What:
    
    - Add integration tests verifying the success messages on
      Special:PasswordReset.
    - Clear AuthManagerFilterProvider handlers and AuthManager configs
      before each test to avoid failures on test wikis with SUL3
      CentralAuth.
    
    Bug: T392086
    Change-Id: Ifd552ab4a09f1ab6e54ecb79bd7aebf9387745db

--- tests/phpunit/includes/specials/SpecialPasswordResetTest.php
+++ tests/phpunit/includes/specials/SpecialPasswordResetTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use MediaWiki\MainConfigNames;
 use MediaWiki\Request\FauxRequest;
 use MediaWiki\Specials\SpecialPasswordReset;
 use MediaWiki\Tests\SpecialPage\FormSpecialPageTestCase;
@@ -12,6 +13,24 @@ use MediaWiki\Tests\User\TempUser\TempUserTestTrait;
 class SpecialPasswordResetTest extends FormSpecialPageTestCase {
 
 	use TempUserTestTrait;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		// Test the default primary and secondary authentication providers
+		// irrespective of any potentially conflicting local configuration.
+		$authConfig = $this->getServiceContainer()
+			->getConfigSchema()
+			->getDefaultFor( MainConfigNames::AuthManagerAutoConfig );
+
+		$this->overrideConfigValues( [
+			MainConfigNames::AuthManagerConfig => null,
+			MainConfigNames::AuthManagerAutoConfig => $authConfig
+		] );
+
+		// Avoid failures if CentralAuth is loaded and configured to use SUL3 locally.
+		$this->clearHook( 'AuthManagerFilterProviders' );
+	}
 
 	/**
 	 * @inheritDoc
@@ -42,5 +61,29 @@ class SpecialPasswordResetTest extends FormSpecialPageTestCase {
 		);
 		[ $html ] = $this->executeSpecialPage( '', $request );
 		$this->assertStringContainsString( '(htmlform-user-not-valid', $html );
+	}
+
+	/**
+	 * @dataProvider provideSuccessfulReset
+	 */
+	public function testSuccessfulReset( string $email ): void {
+		$user = $this->getMutableTestUser()->getUser();
+		$user->setEmail( 'test@example.com' );
+		$user->saveSettings();
+
+		$request = new FauxRequest( [
+			'wpUsername' => $user->getName(),
+			'wpEmail' => $email,
+		], true );
+
+		[ $html ] = $this->executeSpecialPage( '', $request );
+
+		$this->assertStringContainsString( "(passwordreset-username) {$user->getName()}", $html );
+		$this->assertStringContainsString( "(passwordreset-email) {$email}", $html );
+	}
+
+	public static function provideSuccessfulReset(): iterable {
+		yield 'matching email' => [ 'test@example.com' ];
+		yield 'different email' => [ 'other@example.com' ];
 	}
 }
