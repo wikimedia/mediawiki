0479ea0491d37e686195021fae8db517d42639a5
toluayo <tayodele-ctr@wikimedia.org>
2025-04-16 13:00:53 -0400

    Add namespace for Buginese(Latin Script)
    Bug: T389916
    Change-Id: I63498f1ac52111b647261f56717e430f48127817

--- languages/messages/MessagesBug.php
+++ languages/messages/MessagesBug.php
@@ -6,3 +6,22 @@
  */
 
 $fallback = 'id';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Médiya',
+	NS_SPECIAL          => 'Mallebbi',
+	NS_TALK             => 'Panennaq',
+	NS_USER             => 'Pagguna',
+	NS_USER_TALK        => 'Panennaq_pagguna',
+	NS_PROJECT_TALK     => 'Panennaq_$1',
+	NS_FILE             => 'Attarong',
+	NS_FILE_TALK        => 'Panennaq_attarong',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Panennaq_MediaWiki',
+	NS_TEMPLATE         => 'Accuwang',
+	NS_TEMPLATE_TALK    => 'Panennaq_accuwang',
+	NS_HELP             => 'Bantungeng',
+	NS_HELP_TALK        => 'Panennaq_bantungeng',
+	NS_CATEGORY         => 'Katagori',
+	NS_CATEGORY_TALK    => 'Panennaq_katagori',
+];
