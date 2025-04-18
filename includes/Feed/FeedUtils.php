e97f448080eac7257de259a04e91f902bda111e0
Kunal Mehta <legoktm@debian.org>
2025-04-17 22:10:48 -0400

    FeedUtils: Avoid using rawElement when unecessary
    Inspired by T386175 and c57c1fce46d7, use Html::element() for an error
    message which doesn't need any HTML. I checked all callers and $newid
    will always be an integer, but let's use the safer method so future
    reviewers don't need to audit it.
    
    Change-Id: I42c39330dee9b8f9a266d29b9c019e89eb5a561a

--- includes/Feed/FeedUtils.php
+++ includes/Feed/FeedUtils.php
@@ -206,7 +206,7 @@ class FeedUtils {
 				$diffText = self::getDiffLink( $title, $newid, $oldid );
 			} elseif ( $diffText === false ) {
 				// Error in diff engine, probably a missing revision
-				$diffText = Html::rawElement(
+				$diffText = Html::element(
 					'p',
 					[],
 					"Can't load revision $newid"
