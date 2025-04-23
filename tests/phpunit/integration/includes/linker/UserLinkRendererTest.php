585a3c831085b263a4001c2d3b0ff12064d2fc41
Máté Szabó <mszabo@wikimedia.org>
2025-04-23 17:46:03 +0000

    Revert "UserLinkRenderer: Convert mw-tempuserlink-expired tooltip to plain CSS"
    This reverts commit 39e23498d0ddd3416407e7963a8dc2873fd0ab87.
    
    Reason for revert: T392522
    
    Bug: T392522
    Change-Id: Ia28037497dc7df916dc29160bc1c55225a1b591c

--- tests/phpunit/integration/includes/linker/UserLinkRendererTest.php
+++ tests/phpunit/integration/includes/linker/UserLinkRendererTest.php
@@ -74,7 +74,10 @@ class UserLinkRendererTest extends MediaWikiLangTestCase {
 
 		$this->outputPage->expects( $this->once() )
 			->method( 'addModuleStyles' )
-			->with( [ 'mediawiki.interface.helpers.styles', 'mediawiki.interface.helpers.linker.styles' ] );
+			->with( [ 'mediawiki.interface.helpers.styles' ] );
+		$this->outputPage->expects( $this->once() )
+			->method( 'addModules' )
+			->with( [ 'mediawiki.interface.helpers' ] );
 
 		$actual = $this->userLinkRenderer->userLink(
 			$user,
@@ -212,9 +215,9 @@ class UserLinkRendererTest extends MediaWikiLangTestCase {
 			'Expired temporary user link' => [
 				'<a href="/wiki/Special:Contributions/~2023-1" '
 				. 'class="mw-userlink mw-tempuserlink mw-tempuserlink-expired" '
-				. 'aria-description="(tempuser-expired-link-tooltip)"><bdi>~2023-1</bdi>'
-				. '<span role="presentation" class="cdx-tooltip mw-tempuserlink-expired--tooltip">(tempuser-expired-link-tooltip)</span>'
-				. '</a>',
+				. 'title="" aria-describedby="mw-tempuserlink-expired-tooltip-0"><bdi>~2023-1</bdi></a>'
+				. '<div id="mw-tempuserlink-expired-tooltip-0" role="tooltip" '
+				. 'class="cdx-tooltip mw-tempuserlink-expired--tooltip">(tempuser-expired-link-tooltip)</div>',
 				new UserIdentityValue( 2, self::EXPIRED_TEMP_USER_NAME )
 			],
 
@@ -254,7 +257,10 @@ class UserLinkRendererTest extends MediaWikiLangTestCase {
 
 		$this->outputPage->expects( $this->exactly( 2 ) )
 			->method( 'addModuleStyles' )
-			->with( [ 'mediawiki.interface.helpers.styles', 'mediawiki.interface.helpers.linker.styles' ] );
+			->with( [ 'mediawiki.interface.helpers.styles' ] );
+		$this->outputPage->expects( $this->exactly( 2 ) )
+			->method( 'addModules' )
+			->with( [ 'mediawiki.interface.helpers' ] );
 
 		$firstCall = $this->userLinkRenderer->userLink(
 			$user,
@@ -297,5 +303,42 @@ class UserLinkRendererTest extends MediaWikiLangTestCase {
 			false,
 			new UserIdentityValue( 2, 'OtherUser' )
 		];
+	}
+
+	public function testDoesNotCacheExpiredTemporaryAccountLink(): void {
+		$user = new UserIdentityValue( 2, self::EXPIRED_TEMP_USER_NAME );
+		$nextId = 0;
+
+		$userLinkRenderer = new UserLinkRenderer(
+			$this->getServiceContainer()->getTempUserConfig(),
+			$this->getServiceContainer()->getSpecialPageFactory(),
+			$this->getServiceContainer()->getLinkRenderer(),
+			$this->tempUserDetailsLookup,
+			static function ( string $prefix ) use ( &$nextId ): string {
+				return $prefix . $nextId++;
+			}
+		);
+
+		$this->outputPage->expects( $this->exactly( 2 ) )
+			->method( 'addModuleStyles' )
+			->with( [ 'mediawiki.interface.helpers.styles' ] );
+		$this->outputPage->expects( $this->exactly( 2 ) )
+			->method( 'addModules' )
+			->with( [ 'mediawiki.interface.helpers' ] );
+
+		$this->tempUserDetailsLookup->method( 'isExpired' )
+			->with( $user )
+			->willReturn( true );
+
+		$firstCall = $userLinkRenderer->userLink(
+			$user,
+			$this->context
+		);
+		$otherCall = $userLinkRenderer->userLink(
+			$user,
+			$this->context
+		);
+
+		$this->assertNotEquals( $firstCall, $otherCall );
 	}
 }
