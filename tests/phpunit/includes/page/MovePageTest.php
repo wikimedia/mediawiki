37215fe0c29609e5563fa9520bc416f9d770bff5
Gabriele Modena <gmodena@wikimedia.org>
2025-05-14 12:16:54 +0200

    PageMovedEvent: add page redirect information.
    Adds data about redirects created during a page move action.
    
    Propagates the redirect PageRecord from
    MovePage::moveToInternal() to the method caller.
    
    Introduces two new public methods in PageMovedEvent:
    
    - getRedirectPage(): returns the redirect PageRecord, if
    one was created.
    
    - wasRedirectCreated(): indicates whether a redirect was
    created during the move.
    
    Bug: T394049
    Change-Id: Ib0d2de9d60fe00b340b7fabb2b078ccb6c7b2f0a

--- tests/phpunit/includes/page/MovePageTest.php
+++ tests/phpunit/includes/page/MovePageTest.php
@@ -711,12 +711,49 @@ class MovePageTest extends MediaWikiIntegrationTestCase {
 				Assert::assertSame( $oldPageId, $event->getPageId() );
 
 				Assert::assertSame( $reason, $event->getReason(), 'getReason()' );
+
+				// Default settings: Move is expected to create a redirect.
+				Assert::assertTrue( $event->wasRedirectCreated() );
+				Assert::assertNotNull( $event->getRedirectPage() );
+				Assert::assertSame( $old->getDBkey(), $event->getRedirectPage()->getDBkey(), "getRedirectArticle()" );
+				Assert::assertNotSame( $oldPageId, $event->getRedirectPage()->getId() );
 			}
 		);
 
 		// Now move the page
 		$obj = $this->newMovePageWithMocks( $old, $new, [ 'db' => $this->getDb() ] );
 		$obj->move( $mover, $reason );
+	}
+
+	/**
+	 * Test case for T394049
+	 */
+	public function testEventEmissionWithoutCreatingRedirect() {
+		$old = Title::makeTitle( NS_MEDIAWIKI, 'Foo' );
+
+		$this->getExistingTestPage( $old );
+
+		$new = Title::makeTitle( NS_MEDIAWIKI, 'Bar' );
+		$this->getNonexistingTestPage( $new );
+
+		$mover = $this->getTestSysop()->getUser();
+
+		// clear the queue
+		$this->runJobs();
+
+		$this->expectDomainEvent(
+			PageMovedEvent::TYPE, 1,
+			static function ( PageMovedEvent $event )
+			use ( $old, $new, $mover )
+			{
+				Assert::assertFalse( $event->wasRedirectCreated() );
+				Assert::assertNull( $event->getRedirectPage() );
+			}
+		);
+
+		// Now move the page without creating a redirect
+		$obj = $this->newMovePageWithMocks( $old, $new, [ 'db' => $this->getDb() ] );
+		$obj->move( $mover, null, false );
 	}
 
 	public static function provideUpdatePropagation() {
