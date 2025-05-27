9af7566887bae5f3424d460d95c596d699de9f1f
trainbranchbot <trainbranchbot@releases-jenkins.wikimedia.org>
2025-05-27 01:08:08 +0000

    Branch commit for wmf/1.45.0-wmf.3
    Bug: T392173
    Change-Id: I2539a920eb958ac92e28180631389698a7434e99

--- includes/Defines.php
+++ includes/Defines.php
@@ -34,7 +34,7 @@ use Wikimedia\Rdbms\IDatabase;
  *
  * @since 1.35 (also backported to 1.33.3 and 1.34.1)
  */
-define( 'MW_VERSION', '1.45.0-alpha' );
+define( 'MW_VERSION', '1.45.0-wmf.3' );
 
 /** @{
  * Obsolete IDatabase::makeList() constants
