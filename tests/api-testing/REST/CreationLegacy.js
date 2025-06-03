1fc94182b71f25bc7c0d00226ecd7e552e037256
bpirkle <bpirkle@wikimedia.org>
2025-06-02 21:21:49 -0500

    REST: Beta Modules - introduce integration testing pattern
    Move integration tests for all versions of a module under a
    directory named for the module. This gives a natural place
    for common testing code across module versions.
    
    Bug: T395715
    Change-Id: I17b89ea1047461245087e160c4d88052a80b99f6

--- tests/api-testing/REST/CreationLegacy.js
+++ tests/api-testing/REST/CreationLegacy.js
@@ -3,7 +3,7 @@
 describe( 'legacy POST /page', () => {
 	// Cache deletion ensures tests will execute for both legacy and module paths
 	// Doing this twice protects against changes in test execution order
-	const testsFile = __dirname + '/content.v1/Creation.js';
+	const testsFile = __dirname + '/content/v1/Creation.js';
 	delete require.cache[ testsFile ];
 	require( testsFile ).init( '/v1', '/-' );
 	delete require.cache[ testsFile ];
--- tests/api-testing/REST/UpdateLegacy.js
+++ tests/api-testing/REST/UpdateLegacy.js
@@ -3,7 +3,7 @@
 describe( 'legacy PUT /page/{title}', () => {
 	// Cache deletion ensures tests will execute for both legacy and module paths
 	// Doing this twice protects against changes in test execution order
-	const testsFile = __dirname + '/content.v1/Update.js';
+	const testsFile = __dirname + '/content/v1/Update.js';
 	delete require.cache[ testsFile ];
 	require( testsFile ).init( '/v1', '/-' );
 	delete require.cache[ testsFile ];
