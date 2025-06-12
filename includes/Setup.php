78d252789ce0d22a862411232e6aa58854d83b83
Timo Tijhof <krinkle@fastmail.com>
2025-06-11 18:13:59 -0700

    Setup: Update error message for composer dependencies check
    == Why ==
    
    While psr/log represents our original an first use of Composer,
    this is nowadays "just" another dependency among many. It does not
    justify or help understand what to do next.
    
    == What ==
    
    Use the same "external libraries" terminology, as on mediawiki.org.
    
    == History ==
    
    * 2014 (Ie66794441): Add first ever Composer dependency (psr/log)
    * 2015 (Ie47467657): Add LoggerFactory with check for missing Composer
                         dependency, to address a then-common issue.
    * 2015 (Ib60261237): Move check earlier, to WebStart.
    * 2017 (I633a6ff23): Move check earlier, to Setup.
    * 2021 (Ia81903fb2): Remove redundant exit(1).
    * 2025 (I6050ec4ca): Replace deprecated E_USER_ERROR.
    
    Change-Id: I890d1f285997ccbc04cdb378a69815982c2bf64f
    (cherry picked from commit b538453cdc6438419d044854cc730706988cb908)

--- includes/Setup.php
+++ includes/Setup.php
@@ -129,12 +129,12 @@ require_once MW_INSTALL_PATH . '/includes/Defines.php';
 // Assert that composer dependencies were successfully loaded
 if ( !interface_exists( LoggerInterface::class ) ) {
 	$message = (
-		'<strong>Error:</strong> MediaWiki requires the <a href="https://github.com/php-fig/log">PSR-3 logging ' .
-		"library</a> to be present. This library is not embedded directly in MediaWiki's " .
-		"git repository and must be installed separately by the end user.\n\n" .
+		'<strong>Error: Missing external libraries.</strong> ' .
+		'MediaWiki depends on external libraries bundled with most MediaWiki distributions. ' .
+		"When installing MediaWiki from its Git reposistory, these must be installed separately.\n\n" .
 		'Please see the <a href="https://www.mediawiki.org/wiki/Download_from_Git' .
 		'#Fetch_external_libraries">instructions for installing libraries</a> on mediawiki.org ' .
-		'for help on installing the required components.'
+		'for help on installing the required libraries.'
 	);
 	http_response_code( 500 );
 	echo $message;
