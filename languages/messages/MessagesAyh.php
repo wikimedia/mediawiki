2d9ce791a120b6e8f13948574334e993e97cb2af
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2025-06-24 15:46:54 -0400

    Add limited RTL support for Hadhrami Arabic
    Bug: T397355
    Change-Id: If5dec973c71e469410c8843070966a819d44c642

---
+++ languages/messages/MessagesAyh.php
@@ -0,0 +1,11 @@
+<?php
+/** Hadhrami Arabic (الحضرمية)
+ *
+ * Limited support language without full localization yet.
+ * This file only exists to provide correct content direction.
+ *
+ * @file
+ * @ingroup Languages
+ */
+
+$rtl = true;
