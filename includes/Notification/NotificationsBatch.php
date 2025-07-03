e3e056674e919e271dd77ca20b1a81e5a5134a62
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-07-03 21:58:46 +0200

    NotificationsBatch: Fix potentially quadratic-time performance
    Envelopes were stored in a numeric array, so removing one was a
    linear-time operation (required iterating over the whole array).
    Trying to replace some (but not all) envelopes, which seems like a
    common middleware task, would result in quadratic complexity.
    
    Use an associative array instead, so that removing is a constant-time
    operation. NotificationEnvelope objects are already compared by
    identity, so we can use spl_object_id() as the array keys.
    
    Change-Id: If10d6887611acc4d69af03bf69fc37cd24f50fcd

--- includes/Notification/NotificationsBatch.php
+++ includes/Notification/NotificationsBatch.php
@@ -12,30 +12,28 @@ use IteratorAggregate;
  */
 class NotificationsBatch implements Countable, IteratorAggregate {
 
+	/** @var NotificationEnvelope[] */
 	private array $envelopes = [];
 
 	public function __construct( NotificationEnvelope ...$envelopes ) {
-		$this->envelopes = $envelopes;
+		foreach ( $envelopes as $envelope ) {
+			$this->add( $envelope );
+		}
 	}
 
-	public function add( NotificationEnvelope $envelope ) {
-		$this->envelopes[] = $envelope;
+	public function add( NotificationEnvelope $envelope ): void {
+		$this->envelopes[ spl_object_id( $envelope ) ] = $envelope;
 	}
 
-	public function remove( NotificationEnvelope $toRemove ) {
-		$this->filter( static function ( NotificationEnvelope $existing ) use ( $toRemove ) {
-			return !$existing->equals( $toRemove );
-		} );
+	public function remove( NotificationEnvelope $envelope ): void {
+		unset( $this->envelopes[ spl_object_id( $envelope ) ] );
 	}
 
 	/**
 	 * @param callable(NotificationEnvelope): bool $callback Filter, return true to preserve the $envelope
 	 */
 	public function filter( $callback ): void {
-		$this->envelopes =
-			array_values(
-				array_filter( $this->envelopes, $callback )
-			);
+		$this->envelopes = array_filter( $this->envelopes, $callback );
 	}
 
 	public function count(): int {
@@ -46,6 +44,6 @@ class NotificationsBatch implements Countable, IteratorAggregate {
 	 * @return ArrayIterator<NotificationEnvelope>
 	 */
 	public function getIterator(): ArrayIterator {
-		return new ArrayIterator( $this->envelopes );
+		return new ArrayIterator( array_values( $this->envelopes ) );
 	}
 }
