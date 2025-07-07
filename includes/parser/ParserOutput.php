199f372f1782308674ce0130ce0dfa95557c212d
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2025-07-07 10:33:43 +0200

    Remove ParserOutput::getText method
    This method has been hard-deprecated in 1.44 and can be removed.
    
    Bug: T293512
    Change-Id: Ie75b5fe73f31876b10bc7e222c4ffb2a1689d642

--- includes/parser/ParserOutput.php
+++ includes/parser/ParserOutput.php
@@ -428,63 +428,6 @@ class ParserOutput extends CacheTime implements ContentMetadataCollector {
 	}
 
 	/**
-	 * Get the output HTML
-	 *
-	 * T293512: in the future, ParserOutput::getText() will be deprecated in favor of invoking
-	 * the OutputTransformPipeline directly on a ParserOutput.
-	 * @param array $options (since 1.31) Transformations to apply to the HTML
-	 * 	- allowClone: (bool) Whether to clone the ParserOutput before
-	 *     applying transformations. Default is false.
-	 *  - allowTOC: (bool) Show the TOC, assuming there were enough headings
-	 *     to generate one and `__NOTOC__` wasn't used. Default is true,
-	 *     but might be statefully overridden.
-	 *  - injectTOC: (bool) Replace the TOC_PLACEHOLDER with TOC contents;
-	 *     otherwise the marker will be left in the article (and the skin
-	 *     will be responsible for replacing or removing it).  Default is
-	 *     true.
-	 *  - enableSectionEditLinks: (bool) Include section edit links, assuming
-	 *     section edit link tokens are present in the HTML. Default is true,
-	 *     but might be statefully overridden.
-	 *  - userLang: (Language) Language object used for localizing UX messages,
-	 *    for example the heading of the table of contents. If omitted, will
-	 *    use the language of the main request context.
-	 *  - skin: (Skin) Skin object used for transforming section edit links.
-	 *  - unwrap: (bool) Return text without a wrapper div. Default is false,
-	 *    meaning a wrapper div will be added if getWrapperDivClass() returns
-	 *    a non-empty string.
-	 *  - wrapperDivClass: (string) Wrap the output in a div and apply the given
-	 *    CSS class to that div. This overrides the output of getWrapperDivClass().
-	 *    Setting this to an empty string has the same effect as 'unwrap' => true.
-	 *  - deduplicateStyles: (bool) When true, which is the default, `<style>`
-	 *    tags with the `data-mw-deduplicate` attribute set are deduplicated by
-	 *    value of the attribute: all but the first will be replaced by `<link
-	 *    rel="mw-deduplicated-inline-style" href="mw-data:..."/>` tags, where
-	 *    the scheme-specific-part of the href is the (percent-encoded) value
-	 *    of the `data-mw-deduplicate` attribute.
-	 *  - absoluteURLs: (bool) use absolute URLs in all links. Default: false
-	 *  - includeDebugInfo: (bool) render PP limit report in HTML. Default: false
-	 * @return string HTML
-	 * @return-taint escaped
-	 * @deprecated since 1.42, this method has side-effects on the ParserOutput
-	 *  (see T353257) and so should be avoided in favor of directly invoking
-	 *  the default output pipeline on a ParserOutput; for now, use of
-	 *  ::runOutputPipeline() is preferred to ensure that ParserOptions are
-	 *  available.
-	 */
-	public function getText( $options = [] ) {
-		wfDeprecated( __METHOD__, '1.42' );
-		$oldText = $this->mRawText; // T353257
-		$options += [ 'allowClone' => false ];
-		$po = $this->runPipelineInternal( null, $options );
-		$newText = $po->getContentHolderText();
-		// T353257: for back-compat only mutations to metadata performed by
-		// the pipeline should be preserved; mutations to $mText should be
-		// discarded.
-		$this->setRawText( $oldText );
-		return $newText;
-	}
-
-	/**
 	 * @unstable This method is transitional and will be replaced by a method
 	 * in another class, maybe ContentRenderer.  It allows us to break our
 	 * porting work into two steps; in the first we bring ParserOptions to
