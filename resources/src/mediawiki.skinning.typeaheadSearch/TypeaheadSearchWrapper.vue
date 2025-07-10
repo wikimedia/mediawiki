5e65acd5abf5654ad5fe3fd453bf187d8f1307b2
Bernard Wang <bwang@wikimedia.org>
2025-07-10 11:51:06 -0500

    Override Codex TAHS styles to ensure search results are always visible, also improve styling of search results footer
    Bug: T399224
    Change-Id: I9b313b9d569b49234348634c21a8a1fcc70d4007

--- resources/src/mediawiki.skinning.typeaheadSearch/TypeaheadSearchWrapper.vue
+++ resources/src/mediawiki.skinning.typeaheadSearch/TypeaheadSearchWrapper.vue
@@ -63,3 +63,77 @@ module.exports = exports = defineComponent( {
 } );
 
 </script>
+
+<style lang="less">
+@import 'mediawiki.skin.variables.less';
+
+@header-height: 55px;
+
+@media screen {
+	// Override the default Codex Dialog component to look like an overlay
+	/* stylelint-disable-next-line selector-class-pattern */
+	.cdx-dialog.skin-dialog-search {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		box-sizing: border-box;
+		width: 100%;
+		max-width: none;
+		max-height: none;
+		background: transparent;
+
+		@media ( max-width: @max-width-breakpoint-mobile ) {
+			background-color: @background-color-base;
+		}
+
+		.cdx-dialog__header {
+			padding: 0;
+			background-color: @background-color-interactive;
+		}
+
+		.cdx-dialog__header > div {
+			display: flex;
+			box-sizing: border-box;
+			padding: 0 8px;
+			align-items: center;
+			height: @header-height;
+
+			.cdx-typeahead-search {
+				flex-grow: 1;
+			}
+		}
+
+		@media ( max-width: @max-width-breakpoint-mobile ) {
+			.cdx-menu {
+				top: @header-height;
+				left: 0;
+				right: 0;
+				position: fixed;
+				bottom: 0;
+				margin-top: -1px;
+				overflow-y: auto;
+				// Always show the menu on mobile even when the input is unfocused
+				display: block !important; /* stylelint-disable-line declaration-no-important */
+			}
+
+			.cdx-menu-item {
+				// Prevent Minerva list item styles from applying
+				margin: 0;
+			}
+
+			.cdx-menu--has-footer {
+				.cdx-menu__listbox {
+					// Remove padding from Codex needed for absolutely positioned footer
+					margin-bottom: 0 !important; /* stylelint-disable-line declaration-no-important */
+				}
+
+				.cdx-menu-item:last-of-type {
+					position: relative;
+				}
+			}
+		}
+	}
+}
+</style>
