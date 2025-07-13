f5740d296612f0a2be46e4daac78036fdd040b34
toluayo <tayodele-ctr@wikimedia.org>
2025-07-13 13:54:26 -0400

    Add namespace for Antiguan and Barbudan Creole (aig)
    Bug: T398790
    Change-Id: I359fb70f85a29f8af68e431df155645c3528c215

---
+++ languages/messages/MessagesAig.php
@@ -0,0 +1,27 @@
+<?php
+/** Antigua and Barbuda Creole (Aanteegan an' Baabyuudan)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Toluayo
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Meediya',
+	NS_SPECIAL          => 'Speshal',
+	NS_TALK             => 'Taak',
+	NS_USER             => 'Yuuza',
+	NS_USER_TALK        => 'Yuuza_taak',
+	NS_PROJECT_TALK     => '$1_taak',
+	NS_FILE             => 'Fail',
+	NS_FILE_TALK        => 'Fail_taak',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_taak',
+	NS_TEMPLATE         => 'Templet',
+	NS_TEMPLATE_TALK    => 'Templet_taak',
+	NS_HELP             => '\'elp',
+	NS_HELP_TALK        => '\'elp_taak',
+	NS_CATEGORY         => 'Katagri',
+	NS_CATEGORY_TALK    => 'Katagri_taak',
+];
