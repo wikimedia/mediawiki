d27cacec63cfbc4e05c260035ae6558211c4ef55
Timo Tijhof <krinkle@fastmail.com>
2025-07-17 20:32:14 -0700

    AutoLoader: Reduce generateAutoload.php crashes
    Quite often when moving a class, generateAutoload.php
    doesn't work due to a crash early on, like so:
    
    ```
    Error: Class "Wikimedia\MapCacheLRU\MapCacheLRU" not found
    from /mediawiki/includes/language/LanguageFactory.php
    at /mediawiki/includes/ServiceWiring.php
    at …
    at /mediawiki/includes/MediaWikiServices.php
    at /mediawiki/includes/session/SessionManager.php
    at /mediawiki/includes/Setup.php
    at /mediawiki/maintenance/doMaintenance.php
    at /mediawiki/maintenance/generateAutoload.php
    at {main}
    ```
    
    The SessionManager::singleton() call in Setup.php is responsible
    for indirectly initiating a large number of classes in MediaWiki.
    
    Fortunately, it can be skipped by setting MW_NO_SESSION_HANDLER=1.
    We do this in the Installer to operate MediaWiki in a minimal state.
    This seems appropiate for generateAutoload.php too.
    
    Change-Id: I75d33f7fb185a5820ac7a746105b2d846fb8daa0

--- maintenance/generateAutoload.php
+++ maintenance/generateAutoload.php
@@ -4,6 +4,15 @@ use MediaWiki\Maintenance\Maintenance;
 
 // @codeCoverageIgnoreStart
 require_once __DIR__ . '/Maintenance.php';
+// This maintenance script is run after renaming class files,
+// and is ideally standalone, invoking only a handful of classes.
+// Otherwise, it will crash and defeat its purpose for being when
+// it calls a class impacted by the renamed file.
+// The below skips SessionManager::singleton() in Setup.php, which
+// indirectly loads thousands of classes across the codebase.
+define( 'MW_NO_SESSION_HANDLER', 1 );
+// If something still tries to call it, fail hard.
+define( 'MW_NO_SESSION', 1 );
 // @codeCoverageIgnoreEnd
 
 class GenerateAutoload extends Maintenance {
