38a7cad89d3b5ab7c7ba6c9a5ec1292e2154b010
Timo Tijhof <krinkle@fastmail.com>
2025-07-24 18:16:36 +0100

    parser: Add tests for Parser::extractBody edge cases
    Follows-up I7987d7ceab, Ia9fc6e6366, If6860ca268.
    
    Change-Id: I2d3f9e1cb33e7d18940171c16c3ffbc436447101

--- tests/phpunit/includes/OutputTransform/Stages/ExtractBodyTest.php
+++ tests/phpunit/includes/OutputTransform/Stages/ExtractBodyTest.php
@@ -3,6 +3,7 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Tests\OutputTransform\Stages;
 
+use Generator;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\OutputTransform\OutputTransformStage;
 use MediaWiki\OutputTransform\Stages\ExtractBody;
@@ -35,17 +36,78 @@ class ExtractBodyTest extends OutputTransformStageTestBase {
 		];
 	}
 
-	public static function provideTransform(): array {
-		$pageTemplate = <<<EOF
+	public static function provideTransform(): Generator {
+		$pageTemplate = <<<HTML
 <!DOCTYPE html>
 <html><head><base href="https://www.example.com/w/"/></head><body>__BODY__
 </body></html>
-EOF;
+HTML;
 		$testData = [
-			[
+			'basic' => [
 				"title" => "Foo",
 				"body" => "<p><a href=\"./Hello\">hello</a></p>",
 				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>\n"
+			],
+			'body with attributes' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<body class="bar" data-x="XX"><p><a href="./Hello">hello</a></p>
+					</body></html>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>\n"
+			],
+			'multiple close tags' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<body class="bar" data-x="XX"><p><a href="./Hello">hello</a></p>
+					</body>
+					<p>world</p>
+					</body></html>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>\n\n<p>world</p>\n"
+			],
+			'truncated with missing close tag' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<body class="bar" data-x="XX"><p><a href="./Hello">hello</a></p>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>"
+			],
+			'early false-positive close tag in comment' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<!-- This is a red </body> herring. -->
+					<body class="bar" data-x="XX"><p><a href="./Hello">hello</a></p>
+					</body></html>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>\n"
+			],
+			'early false-positive close tag in open tag' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<body class="bar" data-x="XX" data-y=</body><p><a href="./Hello">hello</a></p>
+					</body></html>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>\n"
+			],
+			'early false-positive close tag in open tag and truncated close tags' => [
+				"title" => "Foo",
+				"doc" => <<<HTML
+					<!DOCTYPE html>
+					<html><head><base href="https://www.example.com/w/"/></head>
+					<body class="bar" data-x="XX" data-y=</body><p><a href="./Hello">hello</a></p>
+					HTML,
+				"result" => "<p><a href=\"https://www.example.com/w/Hello\">hello</a></p>"
 			],
 			// Rest of these tests ensure that self-page cite fragments are converted to fragment urls
 			// Tests different title scenarios to exercise edge cases
@@ -81,18 +143,15 @@ EOF;
 				"result" => "<p><a href=\"#cite1\">hello</a></p>\n"
 			]
 		];
-		$opts = [];
-		$tests = [];
 		// Set test title in parser output extension data
-		foreach ( $testData as $t ) {
+		foreach ( $testData as $label => $t ) {
 			$titleDBKey = strtr( $t['title'], ' ', '_' );
-			$text = str_replace( "__BODY__", $t['body'], $pageTemplate );
-			$poInput = new ParserOutput( $text );
+			$docHtml = $t['doc'] ?? str_replace( "__BODY__", $t['body'], $pageTemplate );
+			$poInput = new ParserOutput( $docHtml );
 			$poOutput = new ParserOutput( $t['result'] );
 			$poInput->setExtensionData( ParsoidParser::PARSOID_TITLE_KEY, $titleDBKey );
 			$poOutput->setExtensionData( ParsoidParser::PARSOID_TITLE_KEY, $titleDBKey );
-			$tests[] = [ $poInput, null, $opts, $poOutput ];
+			yield $label => [ $poInput, null, [], $poOutput ];
 		}
-		return $tests;
 	}
 }
