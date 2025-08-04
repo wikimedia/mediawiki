70d5b2a02cbb913f8b6ad970cb1fea271309563d
Thalia <thalia.e.chan@googlemail.com>
2025-08-04 15:30:14 +0100

    Add a test for SpecialMytalk in temp user IP block appeal mode
    Bug: T398673
    Change-Id: I69e8a5fd8ab7e16d64fdaf4f83a640d9f54b86a3

--- tests/phpunit/includes/specials/SpecialMytalkTest.php
+++ tests/phpunit/includes/specials/SpecialMytalkTest.php
@@ -2,11 +2,16 @@
 
 use MediaWiki\Block\AnonIpBlockTarget;
 use MediaWiki\Block\DatabaseBlock;
+use MediaWiki\Context\RequestContext;
 use MediaWiki\Exception\UserNotLoggedIn;
 use MediaWiki\Permissions\Authority;
+use MediaWiki\Request\FauxRequest;
+use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\Specials\Redirects\SpecialMytalk;
 use MediaWiki\Tests\Unit\Permissions\MockAuthorityTrait;
 use MediaWiki\Tests\User\TempUser\TempUserTestTrait;
+use MediaWiki\User\LoggedOutEditToken;
+use MediaWiki\User\User;
 use MediaWiki\User\UserIdentityValue;
 
 /**
@@ -84,5 +89,49 @@ class SpecialMytalkTest extends SpecialPageTestBase {
 			$this->mockUserAuthorityWithBlock( $user, $block )
 		);
 		$this->assertStringContainsString( 'mytalk-appeal-submit', $html );
+	}
+
+	public function testLoggedOutAndBlockedWithTempAccountsEnabledSubmit() {
+		$this->enableAutoCreateTempUser();
+
+		$user = new UserIdentityValue( 0, '127.0.0.1' );
+		$block = new DatabaseBlock( [
+			'target' => new AnonIpBlockTarget( '127.0.0.1' ),
+			'by' => $this->getTestSysop()->getUser(),
+			'createAccount' => true,
+			'allowUsertalk' => true,
+		] );
+		$this->getServiceContainer()
+			->getDatabaseBlockStoreFactory()
+			->getDatabaseBlockStore( $block->getWikiId() )
+			->insertBlock( $block );
+
+		// After TempUserCreator auto-creates a new temp user, AuthManager adds the newly
+		// created temp user to the main context's request's session, because AuthManager
+		// is constructed using the main context. Therefore the special page must be
+		// executed with the main context.
+		$context = RequestContext::getMain();
+		$request = new FauxRequest( [], true );
+		$request->setVal( 'wpEditToken', new LoggedOutEditToken() );
+		$context->setRequest( $request );
+		$context->setTitle( SpecialPage::getTitleFor( 'Mytalk' ) );
+		$context->setLanguage( 'qqx' );
+		$context->setUser( User::newFromIdentity( $user ) );
+		$context->setAuthority( $this->mockUserAuthorityWithBlock( $user, $block ) );
+
+		[ , $response ] = $this->executeSpecialPage(
+			'',
+			null,
+			null,
+			null,
+			false,
+			$context
+		);
+
+		$tempUserName = $request->getSession()->getUser()->getName();
+		$this->assertStringContainsString(
+			'User_talk:' . $tempUserName,
+			$response->getHeader( 'LOCATION' )
+		);
 	}
 }
