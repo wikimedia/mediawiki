b29b435096429de7f23354ebf21b1616389df5bb
Elliott Eggleston <ejegg@ejegg.com>
2025-08-05 13:42:13 -0400

    Revert "Add vue router package to fundraising mediawiki"
    This reverts commit 14817a50003db02f7c12615dceb3a319a2c38b3b.
    
    Now done in the extension!
    
    Change-Id: I0125932fe743cb505d85496eef078c826f56e908

--- resources/Resources.php
+++ resources/Resources.php
@@ -578,34 +578,6 @@ return [
 		'deprecated' => '[1.42] Use Pinia instead. See migration guidelines: https://w.wiki/7pLU'
 	],
 
-	'vue-router' => [
-		'packageFiles' => [
-			[
-				'name' => 'resources/lib/vue-router/vue-router.js',
-				'callback' => static function ( Context $context, Config $config ) {
-					// Use the development version if development mode is enabled, or if we're in debug mode
-					$file = $config->get( MainConfigNames::VueDevelopmentMode ) || $context->getDebug() ?
-						'resources/lib/vue-router/vue-router.global.js' :
-						'resources/lib/vue-router/vue-router.global.prod.js';
-					// The file shipped by Vuex does var Vuex = ...;, but doesn't export it
-					// Add module.exports = Vuex; programmatically, and import Vue
-					return "var Vue=require('vue');" .
-						file_get_contents( MW_INSTALL_PATH . "/$file" ) .
-						';module.exports=VueRouter;';
-				},
-				'versionCallback' => static function ( Context $context, Config $config ) {
-					$file = $config->get( MainConfigNames::VueDevelopmentMode ) || $context->getDebug() ?
-						'resources/lib/vue-router/vue-router.global.js' :
-						'resources/lib/vue-router/vue-router.global.prod.js';
-					return new FilePath( $file );
-				}
-			],
-		],
-		'dependencies' => [
-			'vue',
-		],
-	],
-
 	'pinia' => [
 		'packageFiles' => [
 			[
