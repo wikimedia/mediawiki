30889b28200909cf50ffce3bbde32b4df3c778b4
Maryum Styles <mstyles@wikimedia.org>
2025-08-19 21:53:01 -0700

    config: Change Reauthenticate Time Default
    Update the default Reauthenticate time to 3600
    moving from 1 minute timeout to 1 hour to improve
    user experience
    
    Bug: T402037
    Change-Id: Ic9a4585afcfe72f795868cbf7d5281a809e6a7c5
    (cherry picked from commit fa04ae9ab260082b859876bee7b162b8c833c85b)

--- includes/config-schema.php
+++ includes/config-schema.php
@@ -933,7 +933,7 @@ return [
 			],
 			'RememberMe' => 'choose',
 			'ReauthenticateTime' => [
-				'default' => 300,
+				'default' => 3600,
 			],
 			'AllowSecuritySensitiveOperationIfCannotReauthenticate' => [
 				'default' => true,
