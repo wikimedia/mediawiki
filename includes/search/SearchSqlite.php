34b3476a3b6f57969db00e2c4fd94c06e67b259f
Derk-Jan Hartman <hartman.wiki@gmail.com>
2025-09-06 16:28:03 +0200

    SearchSqlite: Convert to querybuilder
    Change-Id: I2d954d1d33e8b31785e2014833ddbe5c753bc958

--- includes/search/SearchSqlite.php
+++ includes/search/SearchSqlite.php
@@ -23,7 +23,7 @@
 
 use MediaWiki\MediaWikiServices;
 use Wikimedia\AtEase\AtEase;
-use Wikimedia\Rdbms\IDatabase;
+use Wikimedia\Rdbms\SelectQueryBuilder;
 
 /**
  * Search engine hook for SQLite
@@ -31,10 +31,9 @@ use Wikimedia\Rdbms\IDatabase;
  */
 class SearchSqlite extends SearchDatabase {
 	/**
-	 * Whether fulltext search is supported by current schema
-	 * @return bool
+	 * Whether fulltext search is supported by the current schema
 	 */
-	private function fulltextSearchSupported() {
+	private function fulltextSearchSupported(): bool {
 		$dbr = $this->dbProvider->getReplicaDatabase();
 		$sql = (string)$dbr->newSelectQueryBuilder()
 			->select( 'sql' )
@@ -48,12 +47,8 @@ class SearchSqlite extends SearchDatabase {
 	/**
 	 * Parse the user's query and transform it into an SQL fragment which will
 	 * become part of a WHERE clause
-	 *
-	 * @param string $filteredText
-	 * @param bool $fulltext
-	 * @return string
 	 */
-	private function parseQuery( $filteredText, $fulltext ) {
+	private function parseQuery( string $filteredText, bool $fulltext ): string {
 		$lc = $this->legalSearchChars( self::CHARS_NO_SYNTAX ); // Minus syntax chars (" and *)
 		$searchon = '';
 		$this->searchTerms = [];
@@ -127,13 +122,13 @@ class SearchSqlite extends SearchDatabase {
 				}
 
 				// Match individual terms or quoted phrase in result highlighting...
-				// Note that variants will be introduced in a later stage for highlighting!
+				// Note that variants will be introduced at a later stage for highlighting!
 				$regexp = $this->regexTerm( $term, $wildcard );
 				$this->searchTerms[] = $regexp;
 			}
 
 		} else {
-			wfDebug( __METHOD__ . ": Can't understand search query '{$filteredText}'" );
+			wfDebug( __METHOD__ . ": Can't understand search query '$filteredText'" );
 		}
 
 		$dbr = $this->dbProvider->getReplicaDatabase();
@@ -170,22 +165,12 @@ class SearchSqlite extends SearchDatabase {
 		return $searchChars;
 	}
 
-	/**
-	 * Perform a full text search query and return a result set.
-	 *
-	 * @param string $term Raw search term
-	 * @return SqlSearchResultSet|null
-	 */
+	/** @inheritDoc */
 	protected function doSearchTextInDB( $term ) {
 		return $this->searchInternal( $term, true );
 	}
 
-	/**
-	 * Perform a title-only search query and return a result set.
-	 *
-	 * @param string $term Raw search term
-	 * @return SqlSearchResultSet|null
-	 */
+	/** @inheritDoc */
 	protected function doSearchTitleInDB( $term ) {
 		return $this->searchInternal( $term, false );
 	}
@@ -197,110 +182,77 @@ class SearchSqlite extends SearchDatabase {
 
 		$filteredTerm =
 			$this->filter( MediaWikiServices::getInstance()->getContentLanguage()->lc( $term ) );
-		$dbr = $this->dbProvider->getReplicaDatabase();
-		// The real type is still IDatabase, but IReplicaDatabase is used for safety.
-		'@phan-var IDatabase $dbr';
-		// phpcs:ignore MediaWiki.Usage.DbrQueryUsage.DbrQueryFound
-		$resultSet = $dbr->query( $this->getQuery( $filteredTerm, $fulltext ), __METHOD__ );
 
-		$total = null;
-		// phpcs:ignore MediaWiki.Usage.DbrQueryUsage.DbrQueryFound
-		$totalResult = $dbr->query( $this->getCountQuery( $filteredTerm, $fulltext ), __METHOD__ );
-		$row = $totalResult->fetchObject();
-		if ( $row ) {
-			$total = intval( $row->c );
-		}
-		$totalResult->free();
+		$queryBuilder = $this->getQueryBuilder( $filteredTerm, $fulltext );
+		$resultSet = $queryBuilder->caller( __METHOD__ )->fetchResultSet();
+
+		$countBuilder = $this->getCountQueryBuilder( $filteredTerm, $fulltext );
+		$total = (int)$countBuilder->caller( __METHOD__ )->fetchField();
 
 		return new SqlSearchResultSet( $resultSet, $this->searchTerms, $total );
 	}
 
 	/**
-	 * Return a partial WHERE clause to limit the search to the given namespaces
-	 * @return string
+	 * Add namespace conditions
 	 */
-	private function queryNamespaces() {
-		if ( $this->namespaces === null ) {
-			return '';  # search all
+	private function queryNamespaces( SelectQueryBuilder $queryBuilder ): void {
+		if ( is_array( $this->namespaces ) ) {
+			if ( count( $this->namespaces ) === 0 ) {
+				$this->namespaces[] = NS_MAIN;
+			}
+			$queryBuilder->andWhere( [ 'page_namespace' => $this->namespaces ] );
 		}
-		if ( $this->namespaces === [] ) {
-			$namespaces = NS_MAIN;
-		} else {
-			$dbr = $this->dbProvider->getReplicaDatabase();
-			$namespaces = $dbr->makeList( $this->namespaces );
-		}
-		return 'AND page_namespace IN (' . $namespaces . ')';
 	}
 
 	/**
-	 * Returns a query with limit for number of results set.
-	 * @param string $sql
-	 * @return string
+	 * Construct the SQL query builder to do the search.
 	 */
-	private function limitResult( $sql ) {
-		return $this->dbProvider->getReplicaDatabase()->limitResult( $sql, $this->limit, $this->offset );
-	}
+	private function getQueryBuilder( string $filteredTerm, bool $fulltext ): SelectQueryBuilder {
+		$queryBuilder = $this->dbProvider->getReplicaDatabase()->newSelectQueryBuilder();
 
-	/**
-	 * Construct the full SQL query to do the search.
-	 * The guts shoulds be constructed in queryMain()
-	 * @param string $filteredTerm
-	 * @param bool $fulltext
-	 * @return string
-	 */
-	private function getQuery( $filteredTerm, $fulltext ) {
-		return $this->limitResult(
-			$this->queryMain( $filteredTerm, $fulltext ) . ' ' .
-			$this->queryNamespaces()
-		);
+		$this->queryMain( $queryBuilder, $filteredTerm, $fulltext );
+		$this->queryNamespaces( $queryBuilder );
+		$queryBuilder->limit( $this->limit )
+			->offset( $this->offset );
+
+		return $queryBuilder;
 	}
 
 	/**
 	 * Picks which field to index on, depending on what type of query.
-	 * @param bool $fulltext
-	 * @return string
 	 */
-	private function getIndexField( $fulltext ) {
+	private function getIndexField( bool $fulltext ): string {
 		return $fulltext ? 'si_text' : 'si_title';
 	}
 
 	/**
-	 * Get the base part of the search query.
-	 *
-	 * @param string $filteredTerm
-	 * @param bool $fulltext
-	 * @return string
+	 * Get the base part of the search query using a builder.
 	 */
-	private function queryMain( $filteredTerm, $fulltext ) {
+	private function queryMain( SelectQueryBuilder $queryBuilder, string $filteredTerm, bool $fulltext ): void {
 		$match = $this->parseQuery( $filteredTerm, $fulltext );
-		$dbr = $this->dbProvider->getReplicaDatabase();
-		$page = $dbr->tableName( 'page' );
-		$searchindex = $dbr->tableName( 'searchindex' );
-		return "SELECT $searchindex.rowid, page_namespace, page_title " .
-			"FROM $searchindex CROSS JOIN $page " .
-			"WHERE page_id=$searchindex.rowid AND $match";
-	}
-
-	private function getCountQuery( string $filteredTerm, bool $fulltext ): string {
-		$match = $this->parseQuery( $filteredTerm, $fulltext );
-		$dbr = $this->dbProvider->getReplicaDatabase();
-		$page = $dbr->tableName( 'page' );
-		$searchindex = $dbr->tableName( 'searchindex' );
-		return "SELECT COUNT(*) AS c " .
-			"FROM $searchindex CROSS JOIN $page " .
-			"WHERE page_id=$searchindex.rowid AND $match " .
-			$this->queryNamespaces();
+		$queryBuilder->select( [ 'page_id', 'page_namespace', 'page_title' ] )
+			->from( 'page' )
+			->join( 'searchindex', null, 'page_id=searchindex.rowid' )
+			->where( $match );
 	}
 
 	/**
-	 * Create or update the search index record for the given page.
-	 * Title and text should be pre-processed.
-	 *
-	 * @param int $id
-	 * @param string $title
-	 * @param string $text
+	 * Build a count query for total matches
 	 */
-	public function update( $id, $title, $text ) {
+	private function getCountQueryBuilder( string $filteredTerm, bool $fulltext ): SelectQueryBuilder {
+		$match = $this->parseQuery( $filteredTerm, $fulltext );
+		$queryBuilder = $this->dbProvider->getReplicaDatabase()->newSelectQueryBuilder()
+			->select( 'COUNT(*)' )
+			->from( 'page' )
+			->join( 'searchindex', null, 'page_id=searchindex.rowid' )
+			->where( $match );
+
+		$this->queryNamespaces( $queryBuilder );
+		return $queryBuilder;
+	}
+
+	/** @inheritDoc */
+	public function update( $id, $title, $text ): void {
 		if ( !$this->fulltextSearchSupported() ) {
 			return;
 		}
@@ -317,14 +269,8 @@ class SearchSqlite extends SearchDatabase {
 			->caller( __METHOD__ )->execute();
 	}
 
-	/**
-	 * Update a search index record's title only.
-	 * Title should be pre-processed.
-	 *
-	 * @param int $id
-	 * @param string $title
-	 */
-	public function updateTitle( $id, $title ) {
+	/** @inheritDoc */
+	public function updateTitle( $id, $title ): void {
 		if ( !$this->fulltextSearchSupported() ) {
 			return;
 		}
