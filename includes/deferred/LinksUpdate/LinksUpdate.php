b11b8b16669d3750d9afe23c2f7670e362bc6f0e
Amir Sarabadani <ladsgroup@gmail.com>
2025-09-24 19:29:47 +0200

    LinksUpdate: Reduce lock timeout for the database even furhter
    Krinkle in T366938#11066554 has established that these lock timeouts
    are too strict and better avoided.
    
    It also contributes to outages by pilling up connection over one single
    row.
    
    We have reduced it from 15 to 5 and nothing happened. We are slowly
    reducing it furhter.
    
    Bug: T366938
    Change-Id: I568160416e4dfdbee833007c827202cb7d113eb4

--- includes/deferred/LinksUpdate/LinksUpdate.php
+++ includes/deferred/LinksUpdate/LinksUpdate.php
@@ -193,7 +193,7 @@ class LinksUpdate extends DataUpdate {
 	 */
 	public static function acquirePageLock( IDatabase $dbw, $pageId, $why = 'atomicity' ) {
 		$key = "{$dbw->getDomainID()}:LinksUpdate:$why:pageid:$pageId"; // per-wiki
-		$scopedLock = $dbw->getScopedLockAndFlush( $key, __METHOD__, 5 );
+		$scopedLock = $dbw->getScopedLockAndFlush( $key, __METHOD__, 1 );
 		if ( !$scopedLock ) {
 			$logger = LoggerFactory::getInstance( 'SecondaryDataUpdate' );
 			$logger->info( "Could not acquire lock '{key}' for page ID '{page_id}'.", [
