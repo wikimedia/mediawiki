8468c2a281246b4634215b126a5d63c5593832fa
C. Scott Ananian <cscott@cscott.net>
2025-10-04 23:32:21 -0400

    ParserOutputFlags: documentation update, add @since
    Follows-Up: I053096ca20a7cdd000860e405f4668ce76269b10
    Change-Id: I7239ed3638c8729a7475325cef1ba19cbbd41f2f

--- includes/parser/ParserOutputFlags.php
+++ includes/parser/ParserOutputFlags.php
@@ -23,6 +23,7 @@ namespace MediaWiki\Parser;
  * in order to prevent namespace conflicts with legacy flags.
  *
  * @package MediaWiki\Parser
+ * @since 1.38
  */
 enum ParserOutputFlags: string {
 
@@ -37,6 +38,7 @@ enum ParserOutputFlags: string {
 	 * @see MainConfigSchema::CategoryMagicGallery
 	 * @see ParserOutput::getNoGallery()
 	 * @see ParserOutput::setNoGallery()
+	 * @since 1.38
 	 */
 	case NO_GALLERY = 'mw-NoGallery';
 
@@ -44,6 +46,7 @@ enum ParserOutputFlags: string {
 	 * Whether OOUI should be enabled.
 	 * @see ParserOutput::getEnableOOUI()
 	 * @see ParserOutput::setEnableOOUI()
+	 * @since 1.38
 	 */
 	case ENABLE_OOUI = 'mw-EnableOOUI';
 
@@ -51,6 +54,7 @@ enum ParserOutputFlags: string {
 	 * Force index policy to be 'index'.
 	 * @see ParserOutput::getIndexPolicy()
 	 * @see ParserOutput::setIndexPolicy()
+	 * @since 1.38
 	 */
 	case INDEX_POLICY = 'mw-IndexPolicy';
 
@@ -58,6 +62,7 @@ enum ParserOutputFlags: string {
 	 * Force index policy to be 'noindex'.
 	 * @see ParserOutput::getIndexPolicy()
 	 * @see ParserOutput::setIndexPolicy()
+	 * @since 1.38
 	 */
 	case NO_INDEX_POLICY = 'mw-NoIndexPolicy';
 
@@ -65,6 +70,7 @@ enum ParserOutputFlags: string {
 	 * Show a new section link?
 	 * @see ParserOutput::getNewSection()
 	 * @see ParserOutput::setNewSection()
+	 * @since 1.38
 	 */
 	case NEW_SECTION = 'mw-NewSection';
 
@@ -72,13 +78,18 @@ enum ParserOutputFlags: string {
 	 * Hide the new section link?
 	 * @see ParserOutput::getHideNewSection()
 	 * @see ParserOutput::setHideNewSection()
+	 * @since 1.38
 	 */
 	case HIDE_NEW_SECTION = 'mw-HideNewSection';
 
 	/**
-	 * The prevent-clickjacking flag
+	 * The prevent-clickjacking flag.
+	 * If true, we emit X-Frame-Options: DENY.
+	 * This controls if anti-clickjacking / frame-breaking headers will
+	 * be sent. This should be done for pages where edit actions are possible.
 	 * @see ParserOutput::getPreventClickjacking()
 	 * @see ParserOutput::setPreventClickjacking()
+	 * @since 1.38
 	 */
 	case PREVENT_CLICKJACKING = 'mw-PreventClickjacking';
 
@@ -90,6 +101,7 @@ enum ParserOutputFlags: string {
 	 * This is a /suggestion/ based on whether the TOC is "large
 	 * enough" and other factors, and is intended mostly for skins
 	 * which want to match the behavior of the traditional inline ToC.
+	 * @since 1.39
 	 */
 	case SHOW_TOC = 'show-toc';
 
@@ -99,6 +111,7 @@ enum ParserOutputFlags: string {
 	 * This reflects the use of the `__NOTOC__` magic word in the
 	 * article (possibly modified by `__TOC__` or `__FORCETOC__`), and
 	 * represents an explicit request from the author to hide the TOC.
+	 * @since 1.41
 	 */
 	case NO_TOC = 'no-toc';
 
@@ -108,6 +121,7 @@ enum ParserOutputFlags: string {
 	 * This reflects the `ParserOptions::getSuppressSectionEditLinks()`
 	 * flag and affects the default value of `enableSectionEditLinks`
 	 * in `ParserOutput::getText()`.
+	 * @since 1.42
 	 */
 	case NO_SECTION_EDIT_LINKS = 'no-section-edit-links';
 
@@ -116,11 +130,20 @@ enum ParserOutputFlags: string {
 	 *
 	 * This reflects the ParserOptions::getCollapsibleSections()
 	 * flag.
+	 * @since 1.43
 	 */
 	case COLLAPSIBLE_SECTIONS = 'collapsible-sections';
 
+	// See RenderedRevision::outputVariesOnRevisionMetadata for the
+	// following flags.
+
 	/**
-	 * Informs the edit saving system that...
+	 * Informs the edit saving system that the canonical output for
+	 * this page may change after revision insertion; for instance
+	 * if it uses some properties of the page itself (like categories)
+	 * which will not be updated until the RefreshLinksJob is run after
+	 * the revision is saved.
+	 * @since 1.38
 	 */
 	case VARY_REVISION = 'vary-revision';
 
@@ -128,26 +151,35 @@ enum ParserOutputFlags: string {
 	 * Similar to VARY_REVISION, but used if we didn't
 	 * guess the ID correctly. Informs the edit saving system that
 	 * getting the canonical output after revision insertion requires
-	 * a parse that used that exact revision ID.
+	 * a parse that used that exact revision ID, for instance if the
+	 * page used {{REVISIONID}} to fetch its own revision ID.
+	 * @since 1.38
 	 */
 	case VARY_REVISION_ID = 'vary-revision-id';
 
 	/**
-	 * Similar to VARY_REVISION, but used if we didn't
+	 * Similar to VARY_REVISION_ID, but used if we didn't
 	 * guess the timestamp correctly. Informs the edit saving system
 	 * that getting the canonical output after revision insertion
 	 * requires a parse that used an actual revision timestamp.
+	 * @since 1.38
 	 */
 	case VARY_REVISION_TIMESTAMP = 'vary-revision-timestamp';
 
 	/**
 	 * Similar to VARY_REVISION, but used if we didn't guess the
-	 * content correctly.
+	 * content correctly.  For example, a self-transclusion will
+	 * set this flag, since the lookup of the transcluded content
+	 * (itself) will be stale until the new revision of this page
+	 * is actually stored in the DB.
+	 * @since 1.38
 	 */
 	case VARY_REVISION_SHA1 = 'vary-revision-sha1';
 
 	/**
-	 * Similar to VARY_REVISION
+	 * Similar to VARY_REVISION, but used if the output will change
+	 * once this page exists in the database.
+	 * @since 1.38
 	 */
 	case VARY_REVISION_EXISTS = 'vary-revision-exists';
 
@@ -156,6 +188,7 @@ enum ParserOutputFlags: string {
 	 * page id correctly.  Informs the edit saving system that getting the
 	 * canonical output after page insertion requires a parse that used that
 	 * exact page id.
+	 * @since 1.38
 	 */
 	case VARY_PAGE_ID = 'vary-page-id';
 
@@ -163,6 +196,7 @@ enum ParserOutputFlags: string {
 	 * Similar to VARY_REVISION. Informs the edit saving
 	 * system that getting the canonical output after revision
 	 * insertion requires a parse that used the actual user ID.
+	 * @since 1.38
 	 */
 	case VARY_USER = 'vary-user';
 
@@ -170,6 +204,7 @@ enum ParserOutputFlags: string {
 	 * Used to avoid extremely stale user signature timestamps
 	 * (T84843). Set if the signature wikitext contains another '~~~~' or
 	 * similar (T230652).
+	 * @since 1.38
 	 */
 	case USER_SIGNATURE = 'user-signature';
 
@@ -180,6 +215,7 @@ enum ParserOutputFlags: string {
 	 * taken to work around the fact that the parsed text does not yet
 	 * have an actual revision ID, revision time, etc.
 	 * @see ParserOptions::getIsPreview()
+	 * @since 1.42
 	 */
 	case IS_PREVIEW = 'is-preview';
 
@@ -188,6 +224,7 @@ enum ParserOutputFlags: string {
 	 * parse was done with the legacy parser (or unknown).
 	 *
 	 * @see ParserOptions::getUseParsoid()
+	 * @since 1.45
 	 */
 	case USE_PARSOID = 'use-parsoid';
 
@@ -200,6 +237,7 @@ enum ParserOutputFlags: string {
 	 * cache and the page is reparsed, RefreshLinksJob will also be
 	 * re-run since the content could be different from the last
 	 * parse. (T373256)
+	 * @since 1.44
 	 */
 	case HAS_ASYNC_CONTENT = 'has-async-content';
 
@@ -208,6 +246,7 @@ enum ParserOutputFlags: string {
 	 * was not ready by the time the output was generated.
 	 *
 	 * At present this reduces the cache TTL. (T373256)
+	 * @since 1.44
 	 */
 	case ASYNC_NOT_READY = 'async-not-ready';
 
