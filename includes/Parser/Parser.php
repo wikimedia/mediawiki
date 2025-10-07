2ab9d37cc693dfa37e4b21acb12f4efe89fd67d9
Tacsipacsi <tacsipacsi@jnet.hu>
2025-10-07 21:28:18 +0200

    chore(Parser): Remove no longer used dynamic property
    Bug: T343229
    Depends-On: Ib9d96bea63f5157ed80f309a66d12de8f2e25c0c
    Change-Id: I9a6ccc8ad513efd34c589ee098047dc509f0ca3d

--- includes/Parser/Parser.php
+++ includes/Parser/Parser.php
@@ -290,8 +290,6 @@ class Parser {
 	# Deprecated "dynamic" properties
 	# These used to be dynamic properties added to the parser, but these
 	# have been deprecated since 1.42.
-	/** @deprecated since 1.42: T343229 */
-	public $scribunto_engine;
 	/** @deprecated since 1.42: T343230 */
 	public $extCite;
 	/** @deprecated since 1.42: T203531 */
