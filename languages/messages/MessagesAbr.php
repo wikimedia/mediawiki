181f8d4b04a0f5df1c4b9f941a0a336a0e8337f6
toluayo <tayodele-ctr@wikimedia.org>
2025-10-07 10:34:32 -0400

    Add Bono (abr) to MessagesAbr.php
    Added fallback and linktrail to MessagesAbr.php
    
    Bug: T406198
    Change-Id: I5e371c54d12e0eb9e1b2e01ae35879579b683730

---
+++ languages/messages/MessagesAbr.php
@@ -0,0 +1,12 @@
+<?php
+/** Bono (Abron)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author ToluAyo
+ */
+
+$fallback = 'tw, fr';
+
+$linkTrail = '/^([a-zɛɔàâçéèêîôûäëïöüùÇÉÂÊÎÔÛÄËÏÖÜÀÈÙ]+)(.*)$/sDu';
