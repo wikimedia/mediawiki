6f3fc0e2ec68b81cd6036b9ab8acbabd6eb24c9d
toluayo <tayodele-ctr@wikimedia.org>
2025-10-19 23:57:07 -0400

    Add namespace for Bono (abr)
    Bug: T406198
    Change-Id: Ib820694f623b3bfefb78887c0a89d9073b0c5c5a

--- languages/messages/MessagesAbr.php
+++ languages/messages/MessagesAbr.php
@@ -9,4 +9,23 @@
 
 $fallback = 'tw, fr';
 
+$namespaceNames = [
+	NS_MEDIA            => 'Miidia',
+	NS_SPECIAL          => 'Soronko',
+	NS_TALK             => 'Nkitahodeɛq',
+	NS_USER             => 'Dwumadini',
+	NS_USER_TALK        => 'Dwumadini_nkitahodeɛ',
+	NS_PROJECT_TALK     => '$1_nkitahodeɛ',
+	NS_FILE             => 'Fael',
+	NS_FILE_TALK        => 'Fael_nkitahodeɛ',
+	NS_MEDIAWIKI        => 'MiidiaWiki',
+	NS_MEDIAWIKI_TALK   => 'MiidiaWiki_nkitahodeɛ',
+	NS_TEMPLATE         => 'Nhwɛsoɔ',
+	NS_TEMPLATE_TALK    => 'Nhwɛsoɔ_nkitahodeɛ',
+	NS_HELP             => 'Mmoa',
+	NS_HELP_TALK        => 'Mmoa_nkitahodeɛ',
+	NS_CATEGORY         => 'Nkyekyεmu',
+	NS_CATEGORY_TALK    => 'Nkyekyεmu_nkitahodeɛ',
+];
+
 $linkTrail = '/^([a-zɛɔàâçéèêîôûäëïöüùÇÉÂÊÎÔÛÄËÏÖÜÀÈÙ]+)(.*)$/sDu';
