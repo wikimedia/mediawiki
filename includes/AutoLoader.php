787b91a79bfec4905f5dd63eea1f6007ec770658
Reedy <reedy@wikimedia.org>
2025-10-30 12:16:19 +0000

    Shell: Capitalise folder for PSR-4 compliance
    Bug: T398513
    Change-Id: I34d3c0a046e6117e3e94bc807c262c10748224de

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -97,7 +97,6 @@ class AutoLoader {
 		'MediaWiki\\Search\\' => __DIR__ . '/search/',
 		'MediaWiki\\Search\\SearchWidgets\\' => __DIR__ . '/search/searchwidgets/',
 		'MediaWiki\\Session\\' => __DIR__ . '/session/',
-		'MediaWiki\\Shell\\' => __DIR__ . '/shell/',
 		'MediaWiki\\Skin\\' => __DIR__ . '/skins/',
 		'MediaWiki\\SpecialPage\\' => __DIR__ . '/specialpage/',
 		'MediaWiki\\Specials\\' => __DIR__ . '/specials/',
