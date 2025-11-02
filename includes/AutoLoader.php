d0a7696f043e03f693a4c090edcd820eca1ae355
Sam Reed <sam@reedyboy.net>
2025-11-02 15:42:02 +0000

    Watchlist: Capitalise folder for PSR-4 compliance
    Bug: T398513
    Change-Id: I706177f10f85a6830a6a7feb0e7a25f1105effc6

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -85,7 +85,6 @@ class AutoLoader {
 		'MediaWiki\\Specials\\Redirects\\' => __DIR__ . '/specials/redirects/',
 		'MediaWiki\\Title\\' => __DIR__ . '/title/',
 		'MediaWiki\\User\\' => __DIR__ . '/user/',
-		'MediaWiki\\Watchlist\\' => __DIR__ . '/watchlist/',
 		'MediaWiki\\Widget\\' => __DIR__ . '/widget/',
 		'Wikimedia\\' => __DIR__ . '/libs/',
 		'Wikimedia\\DependencyStore\\' => __DIR__ . '/ResourceLoader/dependencystore/',
