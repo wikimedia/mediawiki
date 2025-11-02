27085070fe4cb59a4dcfb47eee33fee6c7be1526
toluayo <tayodele-ctr@wikimedia.org>
2025-11-01 23:09:43 -0400

    Add namespace for Jju (kaj)
    Bug: T408150
    Change-Id: Ice803b2ead74c88e95fceafba85fd7d0c3b362e8

---
+++ languages/messages/MessagesKaj.php
@@ -0,0 +1,29 @@
+<?php
+/** Jju
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author ToluAyo
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'Ka̱zzu_nkkang',
+	NS_SPECIAL          => 'A̱nyan_di',
+	NS_TALK             => 'Ba̱ryat',
+	NS_USER             => 'A̱byi',
+	NS_USER_TALK        => 'Ba̱ryat_a̱byi',
+	NS_PROJECT_TALK     => 'Ba̱ryat_$1',
+	NS_FILE             => 'Fayil',
+	NS_FILE_TALK        => 'Ba̱ryat_nfayil',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Ba̱ryat_MediaWiki',
+	NS_TEMPLATE         => 'Ka̱zzuan',
+	NS_TEMPLATE_TALK    => 'Ba̱ryat_ka̱zzuan',
+	NS_HELP             => 'Brang',
+	NS_HELP_TALK        => 'Ba̱ryat_brang',
+	NS_CATEGORY         => 'Ka̱srong',
+	NS_CATEGORY_TALK    => 'Ba̱ryat_ka̱srong',
+];
+
+$linkTrail = '/^([a-z‌̱a̱A̱u̱U̱]+)(.*)$/sDu';
