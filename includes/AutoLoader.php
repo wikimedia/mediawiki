9c295981ea93c24d032472e8a49616debc99a630
Sam Reed <sam@reedyboy.net>
2025-11-03 11:16:13 +0000

    JobQueue\Jobs: Capitalise folder for PSR-4 compliance
    Bug: T398513
    Change-Id: Ia64623f07d11ecc3389026e9978561d6a02c0633

--- includes/AutoLoader.php
+++ includes/AutoLoader.php
@@ -49,7 +49,6 @@ class AutoLoader {
 		'MediaWiki\\Block\\' => __DIR__ . '/block/',
 		'MediaWiki\\Installer\\' => __DIR__ . '/installer/',
 		'MediaWiki\\JobQueue\\' => __DIR__ . '/jobqueue/',
-		'MediaWiki\\JobQueue\\Jobs\\' => __DIR__ . '/jobqueue/jobs/',
 		'MediaWiki\\Json\\' => __DIR__ . '/json/',
 		'MediaWiki\\Languages\\Data\\' => __DIR__ . '/languages/data/',
 		'MediaWiki\\Logger\\Monolog\\' => __DIR__ . '/debug/logger/monolog/',
