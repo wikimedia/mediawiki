wikimedia/mediawiki

commit 2a60c670cd4aa7c69a36195dbb4aa26b978737f3

recentchanges: Add ChangesListQuery
Split a database backend class hierarchy out of ChangesListSpecialPage.

There should be no user-visible changes.

SQL changes:

* watchednew/watched conditions omit `wl_user IS NOT NULL` and instead
  use a plain join, like watchlist. This allows the filters to be
  reused for the actual watchlist.
* notwatched/watched conditions treat we_expiry = $now as expired, for
  consistency with WatchedItemStore. For example the previous condition
  for unexpired entries was we_expiry >= $now, but I changed it to
  we_expiry > $now.
* changeType filters now use rc_source instead of rc_type. Combine
  conditions and invert the != operator to allow index usage.
* hidemyself actor join changed from plain to straight.

Details:

* Move DB code out of the CLSP filter definitions. Filter definitions
  instead specify an action like "require named" and this is
  interpreted by a set of filter classes inside ChangesListQuery.
* Filter modules implement an implicit union of required values.
  Required values are combined with OR. Whereas entire modules are
  combined with AND to form the overall query. This reflects the
  previous situation.
* If you tell ChangesListQuery that you want to both require and exclude
  a value, no results will be returned, whereas CLSP generally treats
  this as allowing all values. This appears to be comprehensively
  implemented in the UI layer, so even without JS enabled, all results
  are returned when you enable conflicting filters.
* The ChangesListSpecialPageQuery hook can still abort the query,
  although this happens a bit later. No extension in CodeSearch is
  aborting it.
* I considered making RecentChangeLookup be the factory for
  ChangesListQuery, but there was a dependency loop.
* Client-side filtering (isRowApplicableCallable) formerly acted on
  RecentChange objects, but ChangesListQuery doesn't need or benefit
  from this abstraction. The new implementation in prepareCapture() and

includes/specials/SpecialWatchlist.php
@@ -7,18 +7,19 @@
 namespace MediaWiki\Specials;
 
 use MediaWiki\ChangeTags\ChangeTagsStore;
-use MediaWiki\Context\IContextSource;
 use MediaWiki\Exception\UserNotLoggedIn;
 use MediaWiki\Html\FormOptions;
 use MediaWiki\Html\Html;
-use MediaWiki\Logging\LogPage;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\RecentChanges\ChangesList;
 use MediaWiki\RecentChanges\ChangesListBooleanFilterGroup;
+use MediaWiki\RecentChanges\ChangesListQuery\ChangesListQuery;
+use MediaWiki\RecentChanges\ChangesListQuery\ChangesListQueryFactory;
 use MediaWiki\RecentChanges\ChangesListStringOptionsFilterGroup;
 use MediaWiki\RecentChanges\EnhancedChangesList;
 use MediaWiki\RecentChanges\RecentChange;
+use MediaWiki\RecentChanges\RecentChangeFactory;
 use MediaWiki\Request\DerivativeRequest;
 use MediaWiki\SpecialPage\ChangesListSpecialPage;
 use MediaWiki\SpecialPage\SpecialPage;
@@ -32,10 +33,7 @@ use MediaWiki\Watchlist\WatchedItemStoreInterface;
 use MediaWiki\Watchlist\WatchlistManager;
 use MediaWiki\Xml\XmlSelect;
 use Wikimedia\Message\MessageValue;
-use Wikimedia\Rdbms\IReadableDatabase;
 use Wikimedia\Rdbms\IResultWrapper;
-use Wikimedia\Rdbms\RawSQLExpression;
-use Wikimedia\Rdbms\RawSQLValue;
 
 /**
  * @defgroup Watchlist Users watchlist handling
@@ -74,13 +72,17 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		UserOptionsLookup $userOptionsLookup,
 		ChangeTagsStore $changeTagsStore,
 		UserIdentityUtils $userIdentityUtils,
-		TempUserConfig $tempUserConfig
+		TempUserConfig $tempUserConfig,
+		RecentChangeFactory $recentChangeFactory,
+		ChangesListQueryFactory $changesListQueryFactory,
 	) {
 		parent::__construct(
 			'Watchlist',
 			'viewmywatchlist',
 			$userIdentityUtils,
-			$tempUserConfig
+			$tempUserConfig,
+			$recentChangeFactory,
+			$changesListQueryFactory,
 		);
 
 		$this->watchedItemStore = $watchedItemStore;
@@ -198,12 +200,7 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 						'isReplacedInStructuredUi' => true,
 						'activeValue' => false,
 						'default' => $this->userOptionsLookup->getBoolOption( $this->getUser(), 'extendwatchlist' ),
-						'queryCallable' => static function ( string $specialClassName, IContextSource $ctx,
-							IReadableDatabase $dbr, &$tables, &$fields, &$conds, &$query_options, &$join_conds
-						) {
-							$conds[] = $dbr->expr( 'rc_this_oldid', '=', new RawSQLValue( 'page_latest' ) )
-								->or( 'rc_this_oldid', '=', 0 );
-						},
+						'action' => [ 'require', 'latest' ],
 					]
 				],
 			],
@@ -219,40 +216,17 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 						'label' => 'rcfilters-filter-watchlistactivity-unseen-label',
 						'description' => 'rcfilters-filter-watchlistactivity-unseen-description',
 						'cssClassSuffix' => 'watchedunseen',
-						'isRowApplicableCallable' => function ( IContextSource $ctx, RecentChange $rc ) {
-							return !$this->isChangeEffectivelySeen( $rc );
-						},
+						'action' => [ 'exclude', 'seen' ],
 					],
 					[
 						'name' => 'seen',
 						'label' => 'rcfilters-filter-watchlistactivity-seen-label',
 						'description' => 'rcfilters-filter-watchlistactivity-seen-description',
 						'cssClassSuffix' => 'watchedseen',
-						'isRowApplicableCallable' => function ( IContextSource $ctx, RecentChange $rc ) {
-							return $this->isChangeEffectivelySeen( $rc );
-						}
+						'action' => [ 'require', 'seen' ],
 					],
 				],
 				'default' => ChangesListStringOptionsFilterGroup::NONE,
-				'queryCallable' => static function (
-					string $specialPageClassName,
-					IContextSource $context,
-					IReadableDatabase $dbr,
-					&$tables,
-					&$fields,
-					&$conds,
-					&$query_options,
-					&$join_conds,
-					$selectedValues
-				) {
-					if ( $selectedValues === [ 'seen' ] ) {
-						$conds[] = $dbr->expr( 'wl_notificationtimestamp', '=', null )
-							->orExpr( new RawSQLExpression( 'rc_timestamp < wl_notificationtimestamp' ) );
-					} elseif ( $selectedValues === [ 'unseen' ] ) {
-						$conds[] = $dbr->expr( 'wl_notificationtimestamp', '!=', null )
-							->andExpr( new RawSQLExpression( 'rc_timestamp >= wl_notificationtimestamp' ) );
-					}
-				}
 			]
 		];
 	}
@@ -351,103 +325,39 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 	/**
 	 * @inheritDoc
 	 */
-	protected function doMainQuery( $tables, $fields, $conds, $query_options,
-		$join_conds, FormOptions $opts
-	) {
-		$dbr = $this->getDB();
-		$user = $this->getUser();
+	protected function modifyQuery( ChangesListQuery $query, FormOptions $opts ) {
+		if ( !$this->getUser()->isRegistered() ) {
+			// Broken but reachable from tests
+			$query->forceEmptySet();
+			return;
+		}
+		$query->requireWatched()
+			->watchlistFields( [ 'wl_notificationtimestamp', 'we_expiry' ] );
 
-		$rcQuery = RecentChange::getQueryInfo( RecentChange::STRAIGHT_JOIN_ACTOR );
-		$tables = array_merge( $rcQuery['tables'], $tables, [ 'watchlist' ] );
-		$fields = array_merge( $rcQuery['fields'], $fields );
-
-		$join_conds = array_merge(
-			[
-				'watchlist' => [
-					'JOIN',
-					[
-						'wl_user' => $user->getId(),
-						'wl_namespace=rc_namespace',
-						'wl_title=rc_title'
-					],
-				],
-			],
-			$rcQuery['joins'],
-			$join_conds
+		// TODO: migrate to $query->requireChangeTags( ... )
+		$query->legacyMutator(
+			function ( &$tables, &$fields, &$conds, &$query_options, &$join_conds ) use ( $opts ) {
+				$tagFilter = $opts['tagfilter'] !== '' ? explode( '|', $opts['tagfilter'] ) : [];
+				$this->changeTagsStore->modifyDisplayQuery(
+					$tables,
+					$fields,
+					$conds,
+					$join_conds,
+					$query_options,
+					$tagFilter,
+					$opts['inverttags']
+				);
+				if ( in_array( 'DISTINCT', $query_options ) ) {
+					// ChangeTagsStore::modifyDisplayQuery() adds DISTINCT when filtering on multiple tags.
+					// In order to prevent DISTINCT from causing query performance problems,
+					// we have to GROUP BY the primary key. This in turn requires us to add
+					// the primary key to the end of the ORDER BY, and the old ORDER BY to the
+					// start of the GROUP BY
+					$query_options['ORDER BY'] = 'rc_timestamp DESC, rc_id DESC';
+					$query_options['GROUP BY'] = 'rc_timestamp, rc_id';
+				}
+			}
 		);
-
-		if ( $this->getConfig()->get( MainConfigNames::WatchlistExpiry ) ) {
-			$tables[] = 'watchlist_expiry';
-			$fields[] = 'we_expiry';
-			$join_conds['watchlist_expiry'] = [ 'LEFT JOIN', 'wl_id = we_item' ];
-			$conds[] = $dbr->expr( 'we_expiry', '=', null )->or( 'we_expiry', '>', $dbr->timestamp() );
-		}
-
-		$tables[] = 'page';
-		$fields[] = 'page_latest';
-		$join_conds['page'] = [ 'LEFT JOIN', 'rc_cur_id=page_id' ];
-
-		$fields[] = 'wl_notificationtimestamp';
-
-		// Log entries with DELETED_ACTION must not show up unless the user has
-		// the necessary rights.
-		$authority = $this->getAuthority();
-		if ( !$authority->isAllowed( 'deletedhistory' ) ) {
-			$bitmask = LogPage::DELETED_ACTION;
-		} elseif ( !$authority->isAllowedAny( 'suppressrevision', 'viewsuppressed' ) ) {
-			$bitmask = LogPage::DELETED_ACTION | LogPage::DELETED_RESTRICTED;
-		} else {
-			$bitmask = 0;
-		}
-		if ( $bitmask ) {
-			$conds[] = $dbr->expr( 'rc_source', '!=', RecentChange::SRC_LOG )
-				->orExpr( new RawSQLExpression( $dbr->bitAnd( 'rc_deleted', $bitmask ) . " != $bitmask" ) );
-		}
-
-		$tagFilter = $opts['tagfilter'] !== '' ? explode( '|', $opts['tagfilter'] ) : [];
-		$this->changeTagsStore->modifyDisplayQuery(
-			$tables,
-			$fields,
-			$conds,
-			$join_conds,
-			$query_options,
-			$tagFilter,
-			$opts['inverttags']
-		);
-
-		$this->runMainQueryHook( $tables, $fields, $conds, $query_options, $join_conds, $opts );
-
-		if ( $this->areFiltersInConflict() ) {
-			return false;
-		}
-
-		$orderByAndLimit = [
-			'ORDER BY' => 'rc_timestamp DESC',
-			'LIMIT' => $opts['limit']
-		];
-		if ( in_array( 'DISTINCT', $query_options ) ) {
-			// ChangeTagsStore::modifyDisplayQuery() adds DISTINCT when filtering on multiple tags.
-			// In order to prevent DISTINCT from causing query performance problems,
-			// we have to GROUP BY the primary key. This in turn requires us to add
-			// the primary key to the end of the ORDER BY, and the old ORDER BY to the
-			// start of the GROUP BY
-			$orderByAndLimit['ORDER BY'] = 'rc_timestamp DESC, rc_id DESC';
-			$orderByAndLimit['GROUP BY'] = 'rc_timestamp, rc_id';
-		}
-		// array_merge() is used intentionally here so that hooks can, should
-		// they so desire, override the ORDER BY / LIMIT condition(s)
-		$query_options = array_merge( $orderByAndLimit, $query_options );
-		$query_options['MAX_EXECUTION_TIME'] =
-			$this->getConfig()->get( MainConfigNames::MaxExecutionTimeForExpensiveQueries );
-		$queryBuilder = $dbr->newSelectQueryBuilder()
-			->tables( $tables )
-			->conds( $conds )
-			->fields( $fields )
-			->options( $query_options )
-			->joinConds( $join_conds )
-			->caller( __METHOD__ );
-
-		return $queryBuilder->fetchResultSet();
 	}
 
 	public function outputFeedLinks() {
@@ -481,14 +391,12 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		}
 
 		// If there are no rows to display, show message before trying to render the list
-		if ( $rows->numRows() == 0 ) {
+		if ( iterator_count( $rows ) == 0 ) {
 			$output->wrapWikiMsg(
 				"<div class='mw-changeslist-empty'>\n$1\n</div>", 'recentchanges-noresult'
 			);
 			return;
 		}
-
-		$rows->seek( 0 );
 
 		$list = ChangesList::newFromContext( $this->getContext(), $this->filterGroups );
 		$list->setWatchlistDivs();
@@ -528,8 +436,6 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 				}
 			} );
 		}
-		$rows->seek( 0 );
-
 		$s = $list->beginRecentChangesList();
 
 		if ( $this->isStructuredFilterUiEnabled() ) {
@@ -540,7 +446,7 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 		$counter = 1;
 		foreach ( $rows as $obj ) {
 			// Make RC entry
-			$rc = RecentChange::newFromRow( $obj );
+			$rc = $this->newRecentChangeFromRow( $obj );
 
 			// Skip CatWatch entries for hidden cats based on user preference
 			if (

includes/recentchanges/ChangesListQuery/SeenCondition.php
@@ -1,0 +1,96 @@
+<?php
+
+namespace MediaWiki\RecentChanges\ChangesListQuery;
+
+use MediaWiki\Title\TitleValue;
+use MediaWiki\User\UserIdentity;
+use MediaWiki\Watchlist\WatchedItemStoreInterface;
+use stdClass;
+use Wikimedia\Rdbms\IReadableDatabase;
+use Wikimedia\Rdbms\RawSQLExpression;
+
+/**
+ * Check if the recentchange row has been seen by the current watchlist user.
+ *
+ * @since 1.45
+ */
+class SeenCondition extends ChangesListConditionBase {
+	private ?UserIdentity $user = null;
+
+	public function __construct(
+		private WatchedItemStoreInterface $watchedItemStore
+	) {
+	}
+
+	public function setUser( UserIdentity $user ) {
+		if ( $user->getId() ) {
+			$this->user = $user;
+		} else {
+			$this->user = null;
+		}
+	}
+
+	/** @inheritDoc */
+	public function evaluate( stdClass $row, $value ): bool {
+		if ( !$this->user ) {
+			return false;
+		}
+		$firstUnseen = $this->getLatestNotificationTimestamp( $row );
+		return $firstUnseen === null || $firstUnseen > $row->rc_timestamp;
+	}
+
+	/**
+	 * @param stdClass $row
+	 * @return string|null TS_MW timestamp of first unseen revision or null if there isn't one
+	 */
+	private function getLatestNotificationTimestamp( $row ) {
+		return $this->watchedItemStore->getLatestNotificationTimestamp(
+			$row->wl_notificationtimestamp,
+			$this->user,
+			new TitleValue( (int)$row->rc_namespace, $row->rc_title )
+		);
+	}
+
+	/**
+	 * @param null $value
+	 * @return null
+	 */
+	public function validateValue( $value ) {
+		if ( $value !== null ) {
+			throw new \InvalidArgumentException(
+				'boolean filter "named" does not take a value' );
+		}
+		return $value;
+	}
+
+	/** @inheritDoc */
+	protected function prepareCapture( IReadableDatabase $dbr, QueryBackend $query ) {
+		if ( $this->user ) {
+			$query->joinForFields( 'watchlist' )->weakLeft();
+			$query->fields( [ 'rc_timestamp', 'rc_namespace', 'rc_title', 'wl_notificationtimestamp' ] );
+		}
+	}
+
+	/** @inheritDoc */
+	protected function prepareConds( IReadableDatabase $dbr, QueryBackend $query ) {
+		if ( !$this->user ) {
+			if ( $this->required ) {
+				$query->forceEmptySet();
+			}
+			return;
+		}
+
+		[ $required, $excluded ] = $this->getUniqueValues();
+		if ( $required === [] ) {
+			$query->forceEmptySet();
+		} elseif ( $required ) {
+			$query->joinForConds( 'watchlist' )->weakLeft();
+			$query->where( $dbr->expr( 'wl_notificationtimestamp', '=', null )
+				->orExpr( new RawSQLExpression( 'rc_timestamp < wl_notificationtimestamp' ) ) );
+		} elseif ( $excluded ) {
+			$query->joinForConds( 'watchlist' )->weakLeft();
+			$query->where( $dbr->expr( 'wl_notificationtimestamp', '!=', null )
+				->andExpr( new RawSQLExpression( 'rc_timestamp >= wl_notificationtimestamp' ) ) );
+		}
+	}
+}

autoload.php
@@ -2406,6 +2406,29 @@ $wgAutoloadLocalClasses = [
 	'MediaWiki\\RecentChanges\\ChangesListFilterFactory' => __DIR__ . '/includes/recentchanges/ChangesListFilterFactory.php',
 	'MediaWiki\\RecentChanges\\ChangesListFilterGroup' => __DIR__ . '/includes/recentchanges/ChangesListFilterGroup.php',
 	'MediaWiki\\RecentChanges\\ChangesListFilterGroupContainer' => __DIR__ . '/includes/recentchanges/ChangesListFilterGroupContainer.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\BasicJoin' => __DIR__ . '/includes/recentchanges/ChangesListQuery/BasicJoin.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\BooleanFieldCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/BooleanFieldCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListConditionBase' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListConditionBase.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListHighlight' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListHighlight.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListJoinBuilder' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListJoinBuilder.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListJoinModule' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListJoinModule.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListQuery' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListQuery.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListQueryFactory' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListQueryFactory.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ChangesListResult' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ChangesListResult.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\EnumFieldCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/EnumFieldCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\ExperienceCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/ExperienceCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\FieldEqualityCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/FieldEqualityCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\JoinDependencyProvider' => __DIR__ . '/includes/recentchanges/ChangesListQuery/JoinDependencyProvider.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\NamedCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/NamedCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\NamedConditionHelper' => __DIR__ . '/includes/recentchanges/ChangesListQuery/NamedConditionHelper.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\QueryBackend' => __DIR__ . '/includes/recentchanges/ChangesListQuery/QueryBackend.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\RevisionTypeCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/RevisionTypeCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\SeenCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/SeenCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\SubpageOfCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/SubpageOfCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\UserCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/UserCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\WatchedCondition' => __DIR__ . '/includes/recentchanges/ChangesListQuery/WatchedCondition.php',
+	'MediaWiki\\RecentChanges\\ChangesListQuery\\WatchlistJoin' => __DIR__ . '/includes/recentchanges/ChangesListQuery/WatchlistJoin.php',
 	'MediaWiki\\RecentChanges\\ChangesListStringOptionsFilter' => __DIR__ . '/includes/recentchanges/ChangesListStringOptionsFilter.php',
 	'MediaWiki\\RecentChanges\\ChangesListStringOptionsFilterGroup' => __DIR__ . '/includes/recentchanges/ChangesListStringOptionsFilterGroup.php',
 	'MediaWiki\\RecentChanges\\EnhancedChangesList' => __DIR__ . '/includes/recentchanges/EnhancedChangesList.php',

includes/MediaWikiServices.php
@@ -128,6 +128,7 @@ use MediaWiki\Permissions\RestrictionStore;
 use MediaWiki\PoolCounter\PoolCounterFactory;
 use MediaWiki\Preferences\PreferencesFactory;
 use MediaWiki\Preferences\SignatureValidatorFactory;
+use MediaWiki\RecentChanges\ChangesListQuery\ChangesListQueryFactory;
 use MediaWiki\RecentChanges\PatrolManager;
 use MediaWiki\RecentChanges\RecentChangeFactory;
 use MediaWiki\RecentChanges\RecentChangeLookup;
@@ -895,6 +896,14 @@ class MediaWikiServices extends ServiceContainer {
 	 */
 	public function getCentralIdLookupFactory(): CentralIdLookupFactory {
 		return $this->getService( 'CentralIdLookupFactory' );
+	}
+
+	/**
+	 * @since 1.45
+	 * @return ChangesListQueryFactory
+	 */
+	public function getChangesListQueryFactory(): ChangesListQueryFactory {
+		return $this->getService( 'ChangesListQueryFactory' );
 	}
 
 	/**

includes/ServiceWiring.php
@@ -178,6 +178,7 @@ use MediaWiki\Preferences\DefaultPreferencesFactory;
 use MediaWiki\Preferences\PreferencesFactory;
 use MediaWiki\Preferences\SignatureValidator;
 use MediaWiki\Preferences\SignatureValidatorFactory;
+use MediaWiki\RecentChanges\ChangesListQuery\ChangesListQueryFactory;
 use MediaWiki\RecentChanges\ChangeTrackingEventIngress;
 use MediaWiki\RecentChanges\PatrolManager;
 use MediaWiki\RecentChanges\RecentChangeFactory;
@@ -528,6 +529,17 @@ return [
 			$services->getObjectFactory(),
 			$services->getUserIdentityLookup(),
 			$services->getUserFactory()
+		);
+	},
+
+	'ChangesListQueryFactory' => static function ( MediaWikiServices $services ): ChangesListQueryFactory {
+		return new ChangesListQueryFactory(
+			new ServiceOptions( ChangesListQueryFactory::CONSTRUCTOR_OPTIONS, $services->getMainConfig() ),
+			$services->getRecentChangeLookup(),
+			$services->getWatchedItemStore(),
+			$services->getTempUserConfig(),
+			$services->getUserFactory(),
+			$services->getConnectionProvider(),
 		);
 	},
 

includes/recentchanges/ChangesListBooleanFilter.php
@@ -218,9 +218,7 @@ class ChangesListBooleanFilter extends ChangesListFilter {
 	}
 
 	/**
-	 * @param FormOptions $opts Query parameters merged with defaults
-	 * @param bool $isStructuredUI Whether the structured UI is currently enabled
-	 * @return bool Whether this filter should be considered active
+	 * @inheritDoc
 	 */
 	public function isActive( FormOptions $opts, $isStructuredUI ) {
 		if ( $this->isReplacedInStructuredUi && $isStructuredUI ) {

commit c2e8e71888fec7d8722cf43095f50838c43c9e0d

recentchanges: Fix watchlistactivity=all, i.e. seen/unseen conflict
The "seen" and "unseen" filters are supposed to implement a union, so
that if both are selected, all changes are shown. This was broken by
2a60c670cd4aa7c69a36195dbb4aa26b978737f3

So, make SeenCondition take a boolean value so that both false and true
can be required. The implicit union between required values means that
all results are shown.

Review of other cases of require and exclude being used in the same
filter group:

* hidemajor/hideminor: by the names, an empty result set is correct.
  If both are selected, the JS frontend doesn't send either. The legacy
  UI doesn't have hidemajor. So it doesn't come up in practice.
* hidebots/hidehumans: same
* hidemyself/hidebyothers: same
* hideliu/hideanons: In this case, both can be selected in the legacy
  UI. The conflict is detected and the special page redirects to disable
  both options.

The bug is specific to require/exclude conflict with a string options
filter with isFullCoverage, and seen/unseen is the only instance of
that.

With "hide" names and full coverage, selecting all options should
naturally lead to an empty result set, hence the existing use of
require/exclude which provides this behaviour.

With positive names like "seen" and "unseen", a union is expected and
thus require should be used consistently.

Bug: T408167
Change-Id: I9814af27105963213a8ea5ced10d118906353a62

includes/RecentChanges/ChangesListQuery/SeenCondition.php
@@ -34,11 +34,13 @@ class SeenCondition extends ChangesListConditionBase {
 	/** @inheritDoc */
 	public function evaluate( stdClass $row, $value ): bool {
 		if ( !$this->user ) {
-			return false;
+			$seen = false;
+		} else {
+			$firstUnseen = $this->getLatestNotificationTimestamp( $row );
+			$seen = $firstUnseen === null
+				|| $firstUnseen > ConvertibleTimestamp::convert( TS_MW, $row->rc_timestamp );
 		}
-		$firstUnseen = $this->getLatestNotificationTimestamp( $row );
-		return $firstUnseen === null
-			|| $firstUnseen > ConvertibleTimestamp::convert( TS_MW, $row->rc_timestamp );
+		return $seen === $value;
 	}
 
 	/**
@@ -57,13 +59,13 @@ class SeenCondition extends ChangesListConditionBase {
 	}
 
 	/**
-	 * @param null $value
-	 * @return null
+	 * @param bool $value
+	 * @return bool
 	 */
 	public function validateValue( $value ) {
-		if ( $value !== null ) {
+		if ( !is_bool( $value ) ) {
 			throw new \InvalidArgumentException(
-				'boolean filter "named" does not take a value' );
+				'filter "seen" requires a boolean value' );
 		}
 		return $value;
 	}
@@ -78,24 +80,25 @@ class SeenCondition extends ChangesListConditionBase {
 
 	/** @inheritDoc */
 	protected function prepareConds( IReadableDatabase $dbr, QueryBackend $query ) {
-		if ( !$this->user ) {
-			if ( $this->required ) {
+		$values = $this->getEnumValues( [ true, false ] );
+		if ( $values === [] ) {
+			$query->forceEmptySet();
+		} elseif ( $values === [ true ] ) {
+			// Require seen
+			if ( $this->user ) {
+				$query->joinForConds( 'watchlist' )->weakLeft();
+				$query->where( $dbr->expr( 'wl_notificationtimestamp', '=', null )
+					->orExpr( new RawSQLExpression( 'rc_timestamp < wl_notificationtimestamp' ) ) );
+			} else {
 				$query->forceEmptySet();
 			}
-			return;
-		}
-
-		[ $required, $excluded ] = $this->getUniqueValues();
-		if ( $required === [] ) {
-			$query->forceEmptySet();
-		} elseif ( $required ) {
-			$query->joinForConds( 'watchlist' )->weakLeft();
-			$query->where( $dbr->expr( 'wl_notificationtimestamp', '=', null )
-				->orExpr( new RawSQLExpression( 'rc_timestamp < wl_notificationtimestamp' ) ) );
-		} elseif ( $excluded ) {
-			$query->joinForConds( 'watchlist' )->weakLeft();
-			$query->where( $dbr->expr( 'wl_notificationtimestamp', '!=', null )
-				->andExpr( new RawSQLExpression( 'rc_timestamp >= wl_notificationtimestamp' ) ) );
+		} elseif ( $values === [ false ] ) {
+			// Require unseen
+			if ( $this->user ) {
+				$query->joinForConds( 'watchlist' )->weakLeft();
+				$query->where( $dbr->expr( 'wl_notificationtimestamp', '!=', null )
+					->andExpr( new RawSQLExpression( 'rc_timestamp >= wl_notificationtimestamp' ) ) );
+			}
 		}
 	}
 }

includes/specials/SpecialWatchlist.php
@@ -215,14 +215,14 @@ class SpecialWatchlist extends ChangesListSpecialPage {
 						'label' => 'rcfilters-filter-watchlistactivity-unseen-label',
 						'description' => 'rcfilters-filter-watchlistactivity-unseen-description',
 						'cssClassSuffix' => 'watchedunseen',
-						'action' => [ 'exclude', 'seen' ],
+						'action' => [ 'require', 'seen', false ],
 					],
 					[
 						'name' => 'seen',
 						'label' => 'rcfilters-filter-watchlistactivity-seen-label',
 						'description' => 'rcfilters-filter-watchlistactivity-seen-description',
 						'cssClassSuffix' => 'watchedseen',
-						'action' => [ 'require', 'seen' ],
+						'action' => [ 'require', 'seen', true ],
 					],
 				],
 				'default' => ChangesListStringOptionsFilterGroup::NONE,

tests/phpunit/includes/recentchanges/ChangesListQuery/ChangesListQueryTest.php
@@ -675,7 +675,7 @@ class ChangesListQueryTest extends \MediaWikiIntegrationTestCase {
 				[ 'watchlist-expiry' => true ],
 			],
 			'require seen' => [
-				[ [ 'require', 'seen' ] ],
+				[ [ 'require', 'seen', true ] ],
 				array_merge( $defaultInfo, $leftJoinWatchlist, [
 					'conds' => '((wl_notificationtimestamp IS NULL ' .
 						'OR rc_timestamp < wl_notificationtimestamp))',
@@ -683,17 +683,33 @@ class ChangesListQueryTest extends \MediaWikiIntegrationTestCase {
 				array_diff( $allIds, [ $rcIds['unseen'] ] ),
 			],
 			'exclude seen' => [
-				[ [ 'exclude', 'seen' ] ],
+				[ [ 'exclude', 'seen', true ] ],
 				array_merge( $defaultInfo, $leftJoinWatchlist, [
 					'conds' => '((wl_notificationtimestamp IS NOT NULL ' .
 						'AND rc_timestamp >= wl_notificationtimestamp))',
 				] ),
 				[ $rcIds['unseen'] ],
 			],
+			'require seen false' => [
+				[ [ 'require', 'seen', false ] ],
+				array_merge( $defaultInfo, $leftJoinWatchlist, [
+					'conds' => '((wl_notificationtimestamp IS NOT NULL ' .
+						'AND rc_timestamp >= wl_notificationtimestamp))',
+				] ),
+				[ $rcIds['unseen'] ],
+			],
+			'require seen+unseen (T408167)' => [
+				[
+					[ 'require', 'seen', true ],
+					[ 'require', 'seen', false ],
+				],
+				$defaultInfo,
+				$allIds,
+			],
 			'require watched+seen' => [
 				[
 					[ 'exclude', 'watched', 'notwatched' ],
-					[ 'require', 'seen' ]
+					[ 'require', 'seen', true ]
 				],
 				array_merge( $defaultInfo, $joinWatchlist, [
 					'conds' => '((wl_notificationtimestamp IS NULL ' .