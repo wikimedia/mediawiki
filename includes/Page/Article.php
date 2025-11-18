f51ca206e219d061d1e5d79b586aa71fc5573f70
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2025-11-18 18:07:26 +0100

    Clone ParserOutput in Article before post-processing (take 2)
    Some of the ParserOutputs that we are post-processing could be coming
    from an in-memory cache, which ideally we'd want to avoid modifying
    (which might lead to multiple post-processing).
    This "no-clone" option was mitigating T353257, which is normally
    fixed by the new paths used to have DiscussionTools edit the page.
    
    This is a replay of I6bc6d9a0d7ea1670b912e96719b74b51bc812396, but
    guarded by a configuration option this time so that we can revert via
    a config change during train deploy if necessary.
    
    Bug: T410923
    Bug: T348255
    Co-Authored-By: C. Scott Ananian <cscott@cscott.net>
    Follows-Up: I6bc6d9a0d7ea1670b912e96719b74b51bc812396
    Follows-Up: Id7aa2e76da82df4d61ddd6bafcc927afd0a73274
    Change-Id: Icac94394ff5fad4cdc0dacecbbdb2836d7b13d9a
    (cherry picked from commit 67d057590125e69ac4fdb3f76f4e64768604d5d7)

--- includes/Page/Article.php
+++ includes/Page/Article.php
@@ -710,8 +710,8 @@ class Article implements Page {
 		$skin = $outputPage->getSkin();
 		$skinOptions = $skin->getOptions();
 		$textOptions += [
-			// T371022
-			'allowClone' => false,
+			// T371022, T410923
+			'allowClone' => $this->getContext()->getConfig()->get( MainConfigNames::CloneArticleParserOutput ),
 			'skin' => $skin,
 			'injectTOC' => $skinOptions['toc'],
 		];
@@ -740,7 +740,7 @@ class Article implements Page {
 			);
 
 			if ( $pOutput ) {
-				$this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $skin );
+				$pOutput = $this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $skin );
 				$this->doOutputFromPostProcessedParserCache( $pOutput, $outputPage );
 				$this->doOutputMetaData( $pOutput, $outputPage );
 				return true;
@@ -779,7 +779,7 @@ class Article implements Page {
 				);
 
 				if ( $pOutput ) {
-					$this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $skin );
+					$pOutput = $this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $skin );
 					$this->doOutputFromPostProcessedParserCache( $pOutput, $outputPage );
 					$this->doOutputMetaData( $pOutput, $outputPage );
 					return true;
@@ -922,16 +922,17 @@ class Article implements Page {
 
 	private function postProcessOutput(
 		ParserOutput $pOutput, ParserOptions $parserOptions, array $textOptions, Skin $skin
-	) {
+	): ParserOutput {
 		$skinOptions = $skin->getOptions();
 		$textOptions += [
-			// T371022
-			'allowClone' => false,
+			// T371022, T410923
+			'allowClone' => $this->getContext()->getConfig()->get( MainConfigNames::CloneArticleParserOutput ),
 			'skin' => $skin,
 			'injectTOC' => $skinOptions['toc'],
 		];
 		$pipeline = MediaWikiServices::getInstance()->getDefaultOutputPipeline();
-		$pipeline->run( $pOutput, $parserOptions, $textOptions );
+		$pOutput = $pipeline->run( $pOutput, $parserOptions, $textOptions );
+		return $pOutput;
 	}
 
 	private function doOutputFromPostProcessedParserCache(
@@ -991,7 +992,7 @@ class Article implements Page {
 
 		// TODO this will probably need to be conditional on cache access and/or hoisted one level above but for
 		// now let's keep things in the same place and avoid editing StatusValues.
-		$this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $outputPage->getSkin() );
+		$pOutput = $this->postProcessOutput( $pOutput, $parserOptions, $textOptions, $outputPage->getSkin() );
 		$outputPage->addPostProcessedParserOutput( $pOutput );
 
 		if ( $this->getRevisionRedirectTarget( $rev ) ) {
