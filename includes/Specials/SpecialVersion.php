0e1e608c1487eaccc4fd79ec41171c39c16293ab
Sam Reed <sam@reedyboy.net>
2025-11-24 16:59:22 +0000

    SpecialVersion: Fix "Cannot use bool as array" warning
    Bug: T410913
    Change-Id: Iec50741a8c95656d8a151b698aee5e10a65f33b8

--- includes/Specials/SpecialVersion.php
+++ includes/Specials/SpecialVersion.php
@@ -1126,9 +1126,9 @@ class SpecialVersion extends SpecialPage {
 			$memcKey = $cache->makeKey(
 				'specialversion-ext-version-text', $extension['path'], $this->coreId
 			);
-			[ $vcsVersion, $vcsLink, $vcsDate ] = $cache->get( $memcKey );
+			$res = $cache->get( $memcKey );
 
-			if ( !$vcsVersion ) {
+			if ( $res === false ) {
 				wfDebug( "Getting VCS info for extension {$extension['name']}" );
 				$gitInfo = new GitInfo( $extensionPath );
 				$vcsVersion = $gitInfo->getHeadSHA1();
@@ -1140,6 +1140,7 @@ class SpecialVersion extends SpecialPage {
 				$cache->set( $memcKey, [ $vcsVersion, $vcsLink, $vcsDate ], 60 * 60 * 24 );
 			} else {
 				wfDebug( "Pulled VCS info for extension {$extension['name']} from cache" );
+				[ $vcsVersion, $vcsLink, $vcsDate ] = $res;
 			}
 		}
 
