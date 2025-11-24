5e8a0d6c2fbe8703b7f502909cbee6bf46233b98
Sam Reed <sam@reedyboy.net>
2025-11-24 21:34:55 +0000

    Remove noop xml_parser_free() calls
    Bug: T410934
    Change-Id: I48da67bc29380a7486f7602e8429945b855b2ccc

--- maintenance/includes/TextPassDumper.php
+++ maintenance/includes/TextPassDumper.php
@@ -454,8 +454,6 @@ TEXT
 					$byte . ( $chunk === false ? '' : ( '; "' . substr( $chunk, $byte - $offset, 16 ) . '"' ) ),
 					xml_error_string( xml_get_error_code( $parser ) ) )->escaped();
 
-				xml_parser_free( $parser );
-
 				throw new MWException( $msg );
 			}
 			$offset += strlen( $chunk );
@@ -487,7 +485,6 @@ TEXT
 				$this->egress->closeAndRename( $newFilenames );
 			}
 		}
-		xml_parser_free( $parser );
 
 		return true;
 	}
