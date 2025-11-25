5ea218aae48cf56ef946d7ab783916f3eb228eef
Sam Reed <sam@reedyboy.net>
2025-11-25 14:16:15 +0000

    Logging: Set default for log type on dropdown via LogEventsList
    Avoid passing null to constructor of LogPage
    
    Bug: T411019
    Change-Id: I0cda7935c6f06cc35636925f33c931c2818c059b

--- includes/logging/LogEventsList.php
+++ includes/logging/LogEventsList.php
@@ -272,6 +272,7 @@ class LogEventsList extends ContextSource {
 			'class' => HTMLSelectField::class,
 			'name' => 'type',
 			'options' => array_flip( $typesByName ),
+			'default' => '',
 		];
 	}
 
