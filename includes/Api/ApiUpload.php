f7ce0f2a6abbc6ec7bd06fef5c57151453a8bf67
Ammarpad <ammarpad@yahoo.com>
2025-11-26 11:56:46 +0100

    Api: Initialise reference variable
    Also remove the null default since this is a private method.
    
    Bug: T411075
    Change-Id: I413086e97d8779808ea185414bf16efb5f8b440d
    (cherry picked from commit 54bf11b5833252ad8f0d8af6db133face048d52c)

--- includes/Api/ApiUpload.php
+++ includes/Api/ApiUpload.php
@@ -510,10 +510,10 @@ class ApiUpload extends ApiBase {
 	 *   - When 'optional', only add a 'stashfailed' key to the data and return null.
 	 *     Use this when some error happened for a non-stash upload and we're stashing the file
 	 *     only to save the client the trouble of re-uploading it.
-	 * @param array|null &$data API result to which to add the information
+	 * @param array &$data API result to which to add the information
 	 * @return string|null File key
 	 */
-	private function performStash( $failureMode, &$data = null ) {
+	private function performStash( $failureMode, &$data = [] ) {
 		if ( $failureMode === 'optional' && $this->mUpload->getStashFile() !== null ) {
 			return null;
 		}
@@ -590,6 +590,7 @@ class ApiUpload extends ApiBase {
 	 * @return never
 	 */
 	private function dieRecoverableError( $errors, $parameter = null ): never {
+		$data = [];
 		$this->performStash( 'optional', $data );
 
 		if ( $parameter ) {
