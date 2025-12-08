b9b5906d74f5cf83cc73833f1a7351a508f0f30e
toluayo <tayodele-ctr@wikimedia.org>
2025-12-08 17:01:22 -0500

    Add Javanese (Javanese script) namespace translations to MessagesJv_java.php
    Bug: T409393
    Change-Id: Ic7fd61bf1dfbc06284f89ca9caedbbde89bddda3

---
+++ languages/messages/MessagesJv_java.php
@@ -0,0 +1,27 @@
+<?php
+/** Javanese (Javanese script) (ꦗꦮ)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author ToluAyo
+ */
+
+$namespaceNames = [
+	NS_MEDIA            => 'ꦩꦺꦝꦶꦪꦃ',
+	NS_SPECIAL          => 'ꦩꦶꦫꦸꦁꦒꦤ꧀',
+	NS_TALK             => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀',
+	NS_USER             => 'ꦤꦫꦒꦸꦤ',
+	NS_USER_TALK        => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦤꦫꦒꦸꦤ',
+	NS_PROJECT_TALK     => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀$1',
+	NS_FILE             => 'ꦧꦂꦏꦱ꧀',
+	NS_FILE_TALK        => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦧꦂꦏꦱ꧀',
+	NS_MEDIAWIKI        => 'ꦩꦺꦝꦶꦪꦮꦶꦏꦶ',
+	NS_MEDIAWIKI_TALK   => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦩꦺꦝꦶꦪꦮꦶꦏꦶ',
+	NS_TEMPLATE         => 'ꦕꦶꦛꦏ꧀ꦏꦤ꧀',
+	NS_TEMPLATE_TALK    => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦕꦶꦛꦏ꧀ꦏꦤ꧀',
+	NS_HELP             => 'ꦥꦶꦠꦸꦭꦸꦁ',
+	NS_HELP_TALK        => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦥꦶꦠꦸꦭꦸꦁ',
+	NS_CATEGORY         => 'ꦏꦠꦺꦒꦺꦴꦫꦶ',
+	NS_CATEGORY_TALK    => 'ꦉꦩ꧀ꦧꦸꦒ꧀ꦒꦤ꧀ꦏꦠꦺꦒꦺꦴꦫꦶ',
+];
