7563b9ec1c1724c5706be83e7544ac1fd2af4c83
toluayo <tayodele-ctr@wikimedia.org>
2025-12-17 14:27:47 -0500

    Add namespace translations to MessagesSas.php
    Bug: T402159
    Change-Id: I5a2b5090a0b0e733e3263628d3dbede23c15060e

--- languages/messages/MessagesSas.php
+++ languages/messages/MessagesSas.php
@@ -8,3 +8,22 @@
  */
 
 $fallback = 'id';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Média',
+	NS_SPECIAL          => 'Kusus',
+	NS_TALK             => 'Raosan',
+	NS_USER             => 'Pengadu',
+	NS_USER_TALK        => 'Raosan_pengadu',
+	NS_PROJECT_TALK     => 'Raosan_$1',
+	NS_FILE             => 'Berkas',
+	NS_FILE_TALK        => 'Raosan_berkas',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'Raosan_MediaWiki',
+	NS_TEMPLATE         => 'Mal',
+	NS_TEMPLATE_TALK    => 'Raosan_mal',
+	NS_HELP             => 'Bantuan',
+	NS_HELP_TALK        => 'Raosan_bantuan',
+	NS_CATEGORY         => 'Bangse',
+	NS_CATEGORY_TALK    => 'Raosan_bangse',
+];
