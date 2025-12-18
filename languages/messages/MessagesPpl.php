760a09862171a513716644439089e42f5096117b
Jon Harald Søby <jhsoby@gmail.com>
2025-12-18 22:13:44 +0100

    Add MessagesPpl.php for the Nawat language
    Add namespace translations and fallback language for Nawat.
    No specific linktrail rule necessary, because it uses the basic
    Latin alphabet.
    
    Namespace translations supplied by User:TimumachtikanNawat in
    https://incubator.wikimedia.org/wiki/Special:GoToComment/c-Jon_Harald_Søby-20251205182200-Namespace_translations
    
    Bug: T411884
    Change-Id: I8e2e816559fd37605839866fbff95eba38e87732

---
+++ languages/messages/MessagesPpl.php
@@ -0,0 +1,33 @@
+<?php
+/** Nawat (Nawat)
+ *
+ * @file
+ * @ingroup Languages
+ *
+ * @author Jon Harald Søby
+ * @author TimumachtikanNawat
+ */
+
+$fallback = 'es';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Takwikwil',
+	NS_SPECIAL          => 'Wey',
+	NS_TALK             => 'Titajtaketzakan',
+	NS_USER             => 'Tuknew',
+	NS_USER_TALK        => 'Tuknew_titajtaketzakan',
+	NS_PROJECT_TALK     => '$1_titajtaketzakan',
+	NS_FILE             => 'Taanalis',
+	NS_FILE_TALK        => 'Taanalis_titajtaketzakan',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_titajtaketzakan',
+	NS_TEMPLATE         => 'Tailwitilis',
+	NS_TEMPLATE_TALK    => 'Tailwitilis_titajtaketzakan',
+	NS_HELP             => 'Shinechpalewikan',
+	NS_HELP_TALK        => 'Shinechpalewikan_titajtaketzakan',
+	NS_CATEGORY         => 'Takutun',
+	NS_CATEGORY_TALK    => 'Takutun_titajtaketzakan',
+];
+
+// Remove Spanish gender aliases
+$namespaceGenderAliases = [];
