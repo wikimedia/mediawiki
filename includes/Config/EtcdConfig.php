c9b88b30f5c911e5529a01f81ebb88b8ef4eefbe
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2026-01-07 11:38:21 +0100

    Config: Use `use function array_key_exists` some more
    Per code review in change Ifabc1a4133 – these files are also likely to
    be “hot”. See also T413958 where we may spread this pattern to more
    files and/or apply it to more functions.
    
    Change-Id: Idf1ffadb4d56f88cb515d9d824a2bae46b9cb15e
    Co-Authored-By: Umherirrender <umherirrender_de.wp@web.de>

--- includes/Config/EtcdConfig.php
+++ includes/Config/EtcdConfig.php
@@ -13,6 +13,7 @@ use Wikimedia\ObjectCache\BagOStuff;
 use Wikimedia\ObjectCache\HashBagOStuff;
 use Wikimedia\ObjectFactory\ObjectFactory;
 use Wikimedia\WaitConditionLoop;
+use function array_key_exists;
 
 /**
  * Interface for configuration instances
