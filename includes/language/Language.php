62886bcf8b0d43f6d36f02c6262dd9399bc201e8
Ammarpad <ammarpad@yahoo.com>
2026-01-16 14:33:08 +0100

    Language: Handle NAN coercion to string in formatting numbers.
    Public methods that call Language::formatNumInternal() accept floats
    (supertype of NAN) and want to unconditonally cast their arguments to
    string, before calling formatNumInternal(), since it requires string.
    
    However, casting NAN to other types has been deprecated in PHP 8.5.
    
    The public methods are overriden in subclasses, so fixing them require
    individually fixing all subclass overiddes to not do unconditional
    casting to string, because of float NAN.
    
    Suppressing the warning with @ operator, could also be a short term
    alternative but neeeds subclass overidde fixes and not very forward-
    compatible if PHP upstream decides to raise the warning level.
    
    As alternative, we can relax the type hint on the internal method
    and do this normalization once inside and with greater confidence.
    
    Bug: T414350
    Change-Id: I38f9fc1d2cecc0b25dca09351166a8f9aedf5ae2

--- includes/language/Language.php
+++ includes/language/Language.php
@@ -3141,26 +3141,28 @@ class Language implements Bcp47Code {
 	 * See $separatorTransformTable on MessageIs.php for
 	 * the , => . and . => , implementation.
 	 *
-	 * @param string|int|float $number Expected to be a pre-formatted (e.g. leading zeros, number
-	 *  of decimal places) numeric string. Any non-string will be cast to string.
+	 * @param string|int|float $number The numeric string to be formatted, or integer
+	 *  or a floating point number.
 	 * @return string
 	 */
 	public function formatNum( $number ) {
-		return $this->formatNumInternal( (string)$number, false, false );
+		return $this->formatNumInternal( $number, false, false );
 	}
 
 	/**
 	 * Internal implementation function, shared between formatNum and formatNumNoSeparators.
 	 *
-	 * @param string $number The stringification of a valid PHP number
+	 * @param string|int|float $number Expected to be a pre-formatted (e.g. leading zeros, number
+	 *  of decimal places) numeric string. Any non-string will be cast to string.
 	 * @param bool $noTranslate Whether to translate digits and separators
 	 * @param bool $noSeparators Whether to add separators
 	 * @return string
 	 */
-	private function formatNumInternal(
-		string $number, bool $noTranslate, bool $noSeparators
-	): string {
-		$translateNumerals = $this->config->get( MainConfigNames::TranslateNumerals );
+	private function formatNumInternal( $number, bool $noTranslate, bool $noSeparators ): string {
+		// From PHP 8.5, we can't cast NAN to string, and since the method
+		// accepts float, there's no way to exclude NAN, which is subtype.
+		// So handle it explicitly.
+		$number = is_float( $number ) && is_nan( $number ) ? 'NAN' : (string)$number;
 
 		if ( $number === '' ) {
 			return $number;
@@ -3264,7 +3266,7 @@ class Language implements Bcp47Code {
 		}
 
 		if ( !$noTranslate ) {
-			if ( $translateNumerals ) {
+			if ( $this->config->get( MainConfigNames::TranslateNumerals ) ) {
 				// This is often unnecessary: PHP's NumberFormatter will often
 				// do the digit transform itself (T267614)
 				$s = $this->digitTransformTable();
@@ -3292,13 +3294,13 @@ class Language implements Bcp47Code {
 	/**
 	 * Front-end for non-commafied formatNum
 	 *
-	 * @param string|int|float $number The string to be formatted, should be an integer
+	 * @param string|int|float $number The numeric string to be formatted, or integer
 	 *        or a floating point number.
 	 * @since 1.21
 	 * @return string
 	 */
 	public function formatNumNoSeparators( $number ) {
-		return $this->formatNumInternal( (string)$number, false, true );
+		return $this->formatNumInternal( $number, false, true );
 	}
 
 	/**
