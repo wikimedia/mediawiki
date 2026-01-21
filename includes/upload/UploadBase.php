20596a2111cfdee8bcd7ece9d0214b0ca0efccf9
Umherirrender <umherirrender_de.wp@web.de>
2026-01-21 21:20:25 +0100

    Upload: Do not attempt to get handler for unknown file types
    If the file type cannot be determined, it will be impossible to find a
    matching media handler. This patch avoids a PHP 8.5 deprecation warning
    when the current code tried to do that anyway.
    
    Bug: T413926
    Change-Id: Id16e173c1c04e0018f51f531beaf6215fb983251
    (cherry picked from commit 897d3d1b1da2d0f967458e845f7e1a151c8f5e78)

--- includes/upload/UploadBase.php
+++ includes/upload/UploadBase.php
@@ -541,7 +541,7 @@ abstract class UploadBase {
 			}
 		}
 
-		$handler = MediaHandler::getHandler( $mime );
+		$handler = $mime !== null ? MediaHandler::getHandler( $mime ) : null;
 		if ( $handler ) {
 			$handlerStatus = $handler->verifyUpload( $this->mTempPath );
 			if ( !$handlerStatus->isOK() ) {
