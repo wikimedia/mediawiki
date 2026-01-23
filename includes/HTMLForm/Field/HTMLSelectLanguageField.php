792a628e3ace335b71cd75a9b9adf3d1dfda0666
Huei Tan <htan@wikimedia.org>
2026-01-23 14:06:42 +0800

    HTMLSelectLanguageField: Allow codex display format
    This supports the LanguageSelectWidget in 'language' field
    
    Bug: T415013
    Depends-On: I9d21a41fa0570a31348381181cc7efa5de11435c
    Change-Id: I9c6ee2e88e806f8bf92491e61bff8423a4cd6596

--- includes/HTMLForm/Field/HTMLSelectLanguageField.php
+++ includes/HTMLForm/Field/HTMLSelectLanguageField.php
@@ -5,6 +5,7 @@ namespace MediaWiki\HTMLForm\Field;
 use MediaWiki\HTMLForm\HTMLForm;
 use MediaWiki\MainConfigNames;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Widget\LanguageSelectWidget;
 
 /**
  * Language select field.
@@ -12,12 +13,16 @@ use MediaWiki\MediaWikiServices;
  * @stable to extend
  */
 class HTMLSelectLanguageField extends HTMLSelectField {
+	private bool $useCodex = false;
+	/** @var array<string, string> Language code => language name */
+	private array $languages = [];
 
 	/**
 	 * @stable to call
 	 * @inheritDoc
 	 */
 	public function __construct( $params ) {
+		$this->useCodex = $params['useCodex'] ?? false;
 		parent::__construct( $params );
 
 		if ( $this->mParent instanceof HTMLForm ) {
@@ -28,23 +33,60 @@ class HTMLSelectLanguageField extends HTMLSelectField {
 				MainConfigNames::LanguageCode );
 		}
 
-		$languages = MediaWikiServices::getInstance()
-			->getLanguageNameUtils()
-			->getLanguageNames();
+		// Use provided languages array if given, otherwise fetch all languages
+		if ( isset( $params['languages'] ) && is_array( $params['languages'] ) ) {
+			$this->languages = $params['languages'];
+		} else {
+			$this->languages = MediaWikiServices::getInstance()
+				->getLanguageNameUtils()
+				->getLanguageNames();
+		}
 
 		// Make sure the site language is in the list;
 		// a custom language code might not have a defined name…
-		if ( !array_key_exists( $languageCode, $languages ) ) {
-			$languages[$languageCode] = $languageCode;
+		if ( !array_key_exists( $languageCode, $this->languages ) ) {
+			$this->languages[$languageCode] = $languageCode;
 		}
 
-		ksort( $languages );
+		ksort( $this->languages );
 
-		foreach ( $languages as $code => $name ) {
+		foreach ( $this->languages as $code => $name ) {
 			$this->mParams['options'][$code . ' - ' . $name] = $code;
 		}
 
 		$this->mParams['default'] ??= $languageCode;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @stable to override
+	 */
+	public function getInputOOUI( $value ) {
+		if ( $this->useCodex ) {
+			return $this->getInputCodex( $value, false );
+		}
+		return parent::getInputOOUI( $value );
+	}
+
+	/**
+	 * @inheritDoc
+	 * @stable to override
+	 */
+	public function getInputCodex( $value, $hasErrors ) {
+		// Add module for language selector widget
+		$this->mParent->getOutput()->addModules( 'mediawiki.widgets.LanguageSelectWidget' );
+
+		$standardAttribs = $this->getAttributes( [ 'disabled', 'required', 'multiple' ] );
+
+		$widget = new LanguageSelectWidget( [
+			'languages' => $this->languages,
+			'name' => $this->mName,
+			'value' => $value !== null && $value !== '' ? $value : ( $this->mParams['default'] ?? null ),
+			'id' => $this->mID,
+			'cssclass' => trim( $this->mClass . ' cdx-text-input__input' ),
+		] + $standardAttribs );
+
+		return $widget->toString();
 	}
 }
 
