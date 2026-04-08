89c871c30b18b9439a2c91e482a5bfae6c7f363e
Sam Reed <sam@reedyboy.net>
2026-04-08 18:09:19 +0100

    SECURITY: Check for editmyprivateinfo right in more places
    CVE-2026-58029
    
    Bug: T422676
    Change-Id: I3cdfecc4d5bb5efeed989c77117ed98df03c1c71

--- includes/Specials/SpecialUnlinkAccounts.php
+++ includes/Specials/SpecialUnlinkAccounts.php
@@ -28,6 +28,11 @@ class SpecialUnlinkAccounts extends AuthManagerSpecialPage {
 	}
 
 	/** @inheritDoc */
+	public function getRestriction(): string {
+		return 'editmyprivateinfo';
+	}
+
+	/** @inheritDoc */
 	protected function getLoginSecurityLevel() {
 		return 'UnlinkAccount';
 	}
