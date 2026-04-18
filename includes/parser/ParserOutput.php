8d41913d3bec683cf926c2e7b306c33aa5848e59
C. Scott Ananian <cscott@cscott.net>
2026-04-17 20:08:13 -0400

    ParserOutput: Add JsonCodec hints for TOCData and WarningMsgs
    This is forward-compatibility for a future patch which removes
    the ONLY_FOR_DECODE hint and allows serialization without the
    `_type_` fields.
    
    Change-Id: Ieb9fd86a49ee8af51a8b1759ce4dec42eeb2ecf0
    (cherry picked from commit 3d9afacd908b2e66286fc2f216f20d6777d2bf5f)

--- includes/parser/ParserOutput.php
+++ includes/parser/ParserOutput.php
@@ -33,6 +33,7 @@ use MediaWiki\Title\TitleValue;
 use UnexpectedValueException;
 use Wikimedia\Bcp47Code\Bcp47Code;
 use Wikimedia\Bcp47Code\Bcp47CodeValue;
+use Wikimedia\JsonCodec\Hint;
 use Wikimedia\Message\MessageValue;
 use Wikimedia\Parsoid\Core\ContentMetadataCollector;
 use Wikimedia\Parsoid\Core\ContentMetadataCollectorCompat;
@@ -3081,6 +3082,15 @@ class ParserOutput extends CacheTime implements ContentMetadataCollector {
 		$parserOutput = new ParserOutput();
 		$parserOutput->initFromJson( $json );
 		return $parserOutput;
+	}
+
+	/** @inheritDoc */
+	public static function jsonClassHintFor( string $keyName ) {
+		return match ( $keyName ) {
+			'TOCData' => Hint::build( TOCData::class, Hint::ONLY_FOR_DECODE ),
+			'WarningMsgs' => Hint::build( MessageValue::class, Hint::LIST, Hint::ONLY_FOR_DECODE ),
+			default => null,
+		};
 	}
 
 	/**
