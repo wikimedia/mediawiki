a157479646cb774a144ab006bb77744df47e501c
Umherirrender <umherirrender_de.wp@web.de>
2026-04-24 22:18:16 +0200

    Upload: Avoid null array key in UploadBase::getMaxUploadSize
    The $forType argument can be null,
    null array keys are deprecated with php8.5
    
    Change-Id: I0236f659d11282da34626c4142541151282584e4

--- includes/upload/UploadBase.php
+++ includes/upload/UploadBase.php
@@ -2305,7 +2305,10 @@ abstract class UploadBase {
 		$maxUploadSize = MediaWikiServices::getInstance()->getMainConfig()->get( MainConfigNames::MaxUploadSize );
 
 		if ( is_array( $maxUploadSize ) ) {
-			return $maxUploadSize[$forType] ?? $maxUploadSize['*'];
+			if ( $forType !== null && isset( $maxUploadSize[$forType] ) ) {
+				return $maxUploadSize[$forType];
+			}
+			return $maxUploadSize['*'];
 		}
 		return intval( $maxUploadSize );
 	}
