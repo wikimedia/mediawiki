a25318192aa6ede7f767ccc496cb1f145c796621
MGChecker <hgasuser@gmail.com>
2026-05-03 18:59:50 +0000

    Remove MediaHandler language fallback
    Change-Id: I1a27d621d3732887fd763f3ab6a8ec6da18433a3

--- includes/Media/MediaHandler.php
+++ includes/Media/MediaHandler.php
@@ -8,7 +8,6 @@ namespace MediaWiki\Media;
 
 use InvalidArgumentException;
 use MediaWiki\Context\IContextSource;
-use MediaWiki\Context\RequestContext;
 use MediaWiki\FileRepo\File\File;
 use MediaWiki\FileRepo\File\LocalFile;
 use MediaWiki\HookContainer\HookRunner;
@@ -62,14 +61,7 @@ abstract class MediaHandler {
 	 */
 	public function getLanguage(): Language {
 		if ( !$this->lang ) {
-			wfDeprecatedMsg(
-				"Accessing the language without explicitly setting it via " .
-				"MediaHandler:setLanguage, MediaHandler::getHandler, or " .
-				"MediaHandlerFactory::getHandler was deprecated in 1.46.",
-				'1.46'
-			);
-			$this->lang = RequestContext::getMain()->getLanguage();
-			// throw new \RuntimeException( "Need to set language before accessing." );
+			throw new \RuntimeException( "Need to set language before accessing." );
 		}
 		return $this->lang;
 	}
