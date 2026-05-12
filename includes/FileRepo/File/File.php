88964394267dea8d00fabacea44961c6fcb36d49
MGChecker <hgasuser@gmail.com>
2026-05-12 21:31:49 +0000

    Revert^2 "Remove MediaHandler language fallback"
    This reverts commit 4bcfcf747a415e33e4f04826957b582fe650a5a5 and
    fixed the underlying issue in File::getHandler instead.
    
    File::getHandler needs to ensure the language requested is set
    even if a handler was already requested earlier, since generally
    calls may have different languages (or none).
    As MediaHandlerFactory::getHandler constucts a new handler
    per File instance (since I7749f261365f46735c66a09ca02291a08db9c646),
    the language set here should not be able to propagate too much.
    
    Bug: T425988
    Change-Id: I08ac73194391f332cec830807d88bd000ad60338

--- includes/FileRepo/File/File.php
+++ includes/FileRepo/File/File.php
@@ -1604,7 +1604,9 @@ abstract class File implements MediaHandlerState {
 	 * Get a MediaHandler instance for this file
 	 *
 	 * @param Language|null $lang The language this media handler will use to localize
-	 *   descriptions.
+	 *   descriptions. If null, descriptions will not work.
+	 *   Note that $this->handler->getLanguage() will return the language specified in
+	 *   the last call of this function (or MediaHandler::setLanguage directly).
 	 * @return MediaHandler|false Registered MediaHandler for file's MIME type
 	 *   or false if none found
 	 */
@@ -1612,6 +1614,8 @@ abstract class File implements MediaHandlerState {
 		if ( !$this->handler ) {
 			$lang ??= RequestContext::getMain()->getLanguage();
 			$this->handler = MediaHandler::getHandler( $this->getMimeType(), $lang );
+		} elseif ( $lang ) {
+			$this->handler->setLanguage( $lang );
 		}
 
 		return $this->handler;
