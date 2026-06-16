49cf55446f1517ce58c061f99141444bfe8d0530
Suraj <sethsuraj520@gmail.com>
2026-06-16 09:30:31 +0530

    Add namespace translations and linkTrail for Oostfräisk (frs)
    Added localized namespace names for East Frisian (Oostfräisk) in
    MessagesFrs.php based on the finalized community feedback from
    Translatewiki.
    
    Also configured the project talk namespace pattern and defined a custom
    $linkTrail regex to correctly support trailing link characters unique to
    the language layout (āâêîôûğ), alongside standard German fallback
    characters.
    
    Bug: T423174
    Change-Id: I373080213c8fff57a4f31e544bdd3afcfb2bde6e

--- languages/messages/MessagesFrs.php
+++ languages/messages/MessagesFrs.php
@@ -5,3 +5,30 @@
  */
 
 $fallback = 'de';
+
+$namespaceNames = [
+	NS_MEDIA            => 'Medium',
+	NS_SPECIAL          => 'Spesjóól',
+	NS_TALK             => 'Diskusjoon',
+	NS_USER             => 'Benütser',
+	NS_USER_TALK        => 'Benütser_diskusjoon',
+	NS_PROJECT_TALK     => '$1_diskusjoon',
+	NS_FILE             => 'Dóótäj',
+	NS_FILE_TALK        => 'Dóótäj_diskusjoon',
+	NS_MEDIAWIKI        => 'MediaWiki',
+	NS_MEDIAWIKI_TALK   => 'MediaWiki_diskusjoon',
+	NS_TEMPLATE         => 'Föörlóóeğ',
+	NS_TEMPLATE_TALK    => 'Föörlóóeğ_diskusjoon',
+	NS_HELP             => 'Hüelp',
+	NS_HELP_TALK        => 'Hüelp_diskusjoon',
+	NS_CATEGORY         => 'Kategoorii',
+	NS_CATEGORY_TALK    => 'Kategoorii_diskusjoon',
+];
+
+$linkTrail = '/^([a-zäöüßâêîóôûğ]+)(.*)$/sDu';
+
+$dateFormats = [
+	'dmy time' => 'H:i "üer"',
+	'dmy date' => 'j. F Y',
+	'dmy both' => 'j. F Y, H:i "üer"',
+];
