417ec01ef8fe041e5a2a64d4686c51046df6081f
Marcin Szwarc <mszwarc@wikimedia.org>
2026-07-09 10:10:44 +0200

    Fix UserEditTracker spoiling ActorStore cache for cross-wiki lookups
    Why:
    - UserEditTracker::initializeUserEditCount and ::getUserEditTimestamp
      called ActorNormalization::findActorId.
      - This method was called on a local ActorNormalization object, which
        didn't expect the checked users to come from different wikis.
      - Even though ActorNormalization properly checked the database to
        read from, it messed up the cache, which resulted in a simple
        mapping "user name => id".
      - Documentation comments suggest that ActorStore (ActorNormalization
        implementation) should be eventually single-wiki-only, but due to
        some preexisting issues it couldn't have been enforced yet.
    - Spoiling cache could result in unexpected behaviors across two
      unrelated code parts, as seen in the userrights interwiki bug (which
      was triggered by EventBus extension using UserEditTracker).
    
    What:
    - Update UserEditTracker to accept ActorStoreFactory and create a
      Store relevant to the target user's wiki.
    - Add test to verify that a proper actor store is requested.
    
    Bug: T431176
    Change-Id: Ibc85931efdb76ceeba802c2179aa027051fd4699

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -2915,7 +2915,7 @@ return [
 
 	'UserEditTracker' => static function ( MediaWikiServices $services ): UserEditTracker {
 		return new UserEditTracker(
-			$services->getActorNormalization(),
+			$services->getActorStoreFactory(),
 			$services->getConnectionProvider(),
 			$services->getJobQueueGroup(),
 			$services->getMainWANObjectCache()
