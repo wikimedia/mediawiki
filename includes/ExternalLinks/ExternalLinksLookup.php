0e5cab44ae3fa098788e9fe37d366345eb0f9b3f
Umherirrender <umherirrender_de.wp@web.de>
2026-07-16 20:52:15 +0200

    ExternalLinks: Remove ExternalLinksLookup::getExternalLinksForPage
    Hard-deprecated in 1.45 (1995a92dcd)
    
    Change-Id: I8ced846ca8845855d21e3fe67907b945000264fe

--- includes/ExternalLinks/ExternalLinksLookup.php
+++ includes/ExternalLinks/ExternalLinksLookup.php
@@ -8,36 +8,11 @@ namespace MediaWiki\ExternalLinks;
 
 use MediaWiki\Deferred\LinksUpdate\ExternalLinksTable;
 use Wikimedia\Rdbms\IConnectionProvider;
-use Wikimedia\Rdbms\IReadableDatabase;
 
 /**
  * Functions for looking up externallinks table
  */
 class ExternalLinksLookup {
-
-	/**
-	 * Return an array of external links for a given page id
-	 *
-	 * @stable to call
-	 * @param int $pagId
-	 * @param IReadableDatabase $dbr
-	 * @param string $fname
-	 * @return string[] array of external links
-	 * @deprecated since 1.45 use ::getExtLinksForPage() instead
-	 */
-	public static function getExternalLinksForPage( int $pagId, IReadableDatabase $dbr, $fname ) {
-		wfDeprecated( __METHOD__, '1.45' );
-		$links = [];
-		$res = $dbr->newSelectQueryBuilder()
-			->select( [ 'el_to_domain_index', 'el_to_path' ] )
-			->from( 'externallinks' )
-			->where( [ 'el_from' => $pagId ] )
-			->caller( $fname )->fetchResultSet();
-		foreach ( $res as $row ) {
-			$links[] = LinkFilter::reverseIndexes( $row->el_to_domain_index ) . $row->el_to_path;
-		}
-		return $links;
-	}
 
 	/**
 	 * Return an array of external links for a given page id
