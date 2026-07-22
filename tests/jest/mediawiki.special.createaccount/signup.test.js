978b856f480f265c4775f6a9bab52c57318e83d8
Michael Große <mgrosse@wikimedia.org>
2026-07-22 17:33:56 +0200

    test: create a proper smoke test for signup.js
    Change-Id: I0fd31b5baff37eaab67dcd39fa463d7ff3c654a8

--- tests/jest/mediawiki.special.createaccount/signup.test.js
+++ tests/jest/mediawiki.special.createaccount/signup.test.js
@@ -1,13 +1,22 @@
 'use strict';
 
 const mockGet = global.mw.Api.prototype.get;
-describe( 'Signup', () => {
+const mockPost = global.mw.Api.prototype.post;
+global.mw.Api.AbortController = class {
+	abort() {}
+};
+function flushPromises() {
+	return new Promise( ( resolve ) => {
+		setTimeout( resolve );
+	} );
+}
+describe( 'Signup smoke test', () => {
+	let htmlformEnhanceFireCallback;
 	beforeEach( () => {
 		mockGet.mockReset();
-	} );
-	it( 'adjusts the username to not trigger the respective warning', () => {
-		mockGet.mockResolvedValue( { query: { users: [] } } );
-		let callback;
+		mockPost.mockReset();
+		global.mw.config.get.mockReset();
+
 		mw.hook = ( hookname ) => {
 			if ( hookname !== 'htmlform.enhance' ) {
 				throw new Error( 'Unexpected hook called: ' + hookname );
@@ -15,27 +24,74 @@ describe( 'Signup', () => {
 
 			return {
 				add( callable ) {
-					callback = callable;
+					htmlformEnhanceFireCallback = callable;
 				}
 			};
 		};
+		mw.track = () => {};
+		mw.message = ( messageKey ) => ( {
+			parseDom: () => $( `<div><span>${ messageKey }</span></div>` )
+		} );
+	} );
+
+	it( 'validates user input for username, password, and confirm password', async () => {
+		mockGet.mockResolvedValue( { query: { users: [] } } );
+		global.mw.config.get.mockImplementation( ( key ) => ( { wgUserLanguage: 'qqx' }[ key ] ) );
+
 		const $form = $( '<form>' );
 		const $usernameField = $( '<div>' ).addClass( 'cdx-field' ).appendTo( $form );
-		const $input = $( '<input>' ).attr( 'id', 'wpName2' ).appendTo( $usernameField );
+		const $usernameInput = $( '<input>' ).attr( 'id', 'wpName2' ).appendTo( $usernameField );
 		const $pwField = $( '<div>' ).addClass( 'cdx-field' ).appendTo( $form );
-		$( '<input>' ).attr( 'id', 'wpPassword2' ).appendTo( $pwField );
+		const $passwordInput = $( '<input>' ).attr( 'id', 'wpPassword2' ).appendTo( $pwField );
 		const $confirmPwField = $( '<div>' ).addClass( 'cdx-field' ).appendTo( $form );
-		$( '<input>' ).attr( 'id', 'wpRetype' ).appendTo( $confirmPwField );
+		const $confirmPwInput = $( '<input>' ).attr( 'id', 'wpRetype' ).appendTo( $confirmPwField );
+		const $emailField = $( '<div>' ).addClass( 'cdx-field' ).appendTo( $form );
+		$( '<input>' ).attr( 'id', 'wpEmail' ).appendTo( $emailField );
 
 		require( '../../../resources/src/mediawiki.special.createaccount/signup.js' );
 
-		callback( $form );
+		htmlformEnhanceFireCallback( $form );
 
-		$input.val( ' _ab__cD ' );
-		$input[ 0 ].setSelectionRange( 5, 5 );
-		$input.trigger( 'input' );
+		$usernameInput.val( ' _ab__cD ' );
+		$usernameInput[ 0 ].setSelectionRange( 5, 5 );
+		$usernameInput.trigger( 'input' );
 
-		expect( $input.val() ).toBe( 'Ab  cD ' );
-		expect( $input[ 0 ].selectionStart ).toBe( 3 );
+		expect( $usernameInput.val() ).toBe( 'Ab  cD ' );
+		expect( $usernameInput[ 0 ].selectionStart ).toBe( 3 );
+		expect( mockGet ).toHaveBeenCalledWith( {
+			action: 'query',
+			list: 'users',
+			ususers: 'Ab cD',
+			usprop: 'cancreate',
+			formatversion: 2,
+			errorformat: 'html',
+			errorsuselocal: true,
+			uselang: 'qqx'
+		}, { signal: undefined } );
+
+		mockPost.mockResolvedValue( { validatepassword: { validity: 'Good' } } );
+		$passwordInput.val( 'correct horse battery staple' );
+		$passwordInput.trigger( 'blur' );
+		expect( mockPost ).toHaveBeenCalledWith( {
+			action: 'validatepassword',
+			user: $usernameInput.val(),
+			password: $passwordInput.val(),
+			email: '',
+			realname: '',
+			formatversion: 2,
+			errorformat: 'html',
+			errorsuselocal: true,
+			uselang: 'qqx'
+		}, { signal: undefined } );
+
+		$confirmPwInput.val( 'not the same password' );
+		$confirmPwInput.trigger( 'blur' );
+		await flushPromises();
+		expect( $confirmPwField.html() ).toContain( 'badretype' );
+
+		$confirmPwInput.val( 'correct horse battery staple' );
+		$confirmPwInput.trigger( 'blur' );
+		await flushPromises();
+		expect( $confirmPwField.html() ).not.toContain( 'badretype' );
 	} );
 } );
