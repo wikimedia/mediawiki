bdc34de7cc198faa28c8f72a3b791d5931888d25
James D. Forrester <jforrester@wikimedia.org>
2026-07-27 18:28:22 +0100

    build: Update QUnit from 2.24.1 to 2.26.0
    Change-Id: Iabaf06fc3c4002e1625d8ef22e15e1fa7e8c04f3

--- resources/lib/qunitjs/qunit.js
+++ resources/lib/qunitjs/qunit.js
@@ -1,5 +1,5 @@
 /*!
- * QUnit 2.24.1
+ * QUnit 2.26.0
  * https://qunitjs.com/
  *
  * Copyright OpenJS Foundation and other contributors
@@ -710,6 +710,8 @@
     failOnZeroTests: true,
     // Select by pattern or case-insensitive substring match against "moduleName: testName"
     filter: undefined,
+    ignoreUnhandledRejections: false,
+    testFilter: null,
     // TODO: Make explicit in QUnit 3.
     // fixture: undefined,
 
@@ -1378,6 +1380,40 @@
       }
     }
   }
+
+  /**
+   * Clear the SuiteReport tree of all tests and leave only current module as child suite
+   *
+   * This should be called before defining the first module.only() or test.only()
+   * because otherwise:
+   * - `runEnd.testCounts` is too high.
+   * - UI (HtmlReporter) and TAP (TapReporter) display totals too high.
+   * - Test runners like QTap might timeout because the TAP plan
+   *   would be printed as "1..9" even if only 2 tests are run,
+   *   which means tap-finished will wait for 3-9.
+   */
+  function clearSuiteReports(currentModule) {
+    var childSuite = null;
+    var suiteReport = currentModule.suiteReport;
+    while (suiteReport) {
+      suiteReport.tests.length = 0;
+      var i = suiteReport.childSuites.indexOf(childSuite);
+      if (i === -1) {
+        suiteReport.childSuites.length = 0;
+      } else {
+        // Reduce in-place to just currentModule.suiteReport or its intermediary
+        suiteReport.childSuites.splice(0, i);
+        suiteReport.childSuites.splice(1);
+      }
+      if (suiteReport === runSuite) {
+        suiteReport = null;
+      } else {
+        childSuite = suiteReport;
+        currentModule = currentModule.parentModule;
+        suiteReport = currentModule && currentModule.suiteReport || runSuite;
+      }
+    }
+  }
   var focused$1 = false; // indicates that the "only" filter was used
 
   function module$1(name, options, scope) {
@@ -1392,6 +1428,7 @@
       // delete any and all previously registered modules and tests.
       config.modules.length = 0;
       config.queue.length = 0;
+      clearSuiteReports(config.currentModule);
 
       // Ignore any tests declared after this block within the same
       // module parent. https://github.com/qunitjs/qunit/issues/1645
@@ -1903,6 +1940,7 @@
           expected: expected,
           message: message
         });
+        return actual;
       }
     }, {
       key: "rejects",
@@ -1944,6 +1982,7 @@
             message: message
           });
           done();
+          return actual;
         });
       }
     }]);
@@ -3255,12 +3294,32 @@
         return false;
       }
       var filter = config.filter;
-      if (!filter) {
-        return true;
+      if (filter) {
+        var regexFilter = /^(!?)\/([\w\W]*)\/(i?$)/.exec(filter);
+        var fullName = this.module.name + ': ' + this.testName;
+        if (regexFilter) {
+          if (!this.regexFilter(!!regexFilter[1], regexFilter[2], regexFilter[3], fullName)) {
+            return false;
+          }
+        } else if (!this.stringFilter(filter, fullName)) {
+          return false;
+        }
       }
-      var regexFilter = /^(!?)\/([\w\W]*)\/(i?$)/.exec(filter);
-      var fullName = this.module.name + ': ' + this.testName;
-      return regexFilter ? this.regexFilter(!!regexFilter[1], regexFilter[2], regexFilter[3], fullName) : this.stringFilter(filter, fullName);
+      if (typeof config.testFilter === 'function') {
+        var testInfo = {
+          testId: this.testId,
+          testName: this.testName,
+          module: this.module.name,
+          skip: !!this.skip
+        };
+        try {
+          return !!config.testFilter(testInfo);
+        } catch (error) {
+          Logger.warn('Error in QUnit.config.testFilter callback: ', error);
+          return false;
+        }
+      }
+      return true;
     },
     regexFilter: function regexFilter(exclude, pattern, flags, fullName) {
       var regex = new RegExp(pattern, flags);
@@ -3334,6 +3393,7 @@
     }
     if (!focused) {
       config.queue.length = 0;
+      clearSuiteReports(config.currentModule);
       focused = true;
     }
     var newTest = new Test(settings);
@@ -3905,7 +3965,10 @@
       // cause data loss or invalid YAML syntax.
       //
       // - Quotes, escapes, line breaks, or JSON-like stuff.
-      var rSpecialJson = /['"\\/[{}\]\r\n]/;
+      // - Not allowed in YAML unquoted strings per https://yaml.org/spec/1.2.2/#733-plain-style
+      //   * ": " (colon followed by space)
+      //   * " #" (space followed by hash)
+      var rSpecialJson = /['"\\/[{}\]\r\n|:#]/;
 
       // - Characters that are special at the start of a YAML value
       var rSpecialYaml = /[-?:,[\]{}#&*!|=>'"%@`]/;
@@ -5463,7 +5526,7 @@
   QUnit.isLocal = window$1 && window$1.location && window$1.location.protocol === 'file:';
 
   // Expose the current QUnit version
-  QUnit.version = '2.24.1';
+  QUnit.version = '2.26.0';
   extend(QUnit, {
     config: config,
     diff: diff,
@@ -7394,7 +7457,9 @@
       return ret;
     };
     window$1.addEventListener('unhandledrejection', function (event) {
-      QUnit.onUncaughtException(event.reason);
+      if (!QUnit.config.ignoreUnhandledRejections) {
+        QUnit.onUncaughtException(event.reason);
+      }
     });
   })();
 
