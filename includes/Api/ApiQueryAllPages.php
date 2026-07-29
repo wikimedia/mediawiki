2608edfffe695529fc1705b5934aec2075b5acef
Alexander Vorwerk <zabe@avorwerk.net>
2026-07-29 22:32:43 +0200

    ApiQueryAllPages: Migrate querying langlinks to virtual domain
    Similar approach as in a7e2d731190 (I510820413b6) together with
    a72efed211 (I653a249bc7c5).
    
    Bug: T422519
    Change-Id: I6c4d92e4d02b622c0c3e6b6fec72599d9dc725fa

--- includes/Api/ApiQueryAllPages.php
+++ includes/Api/ApiQueryAllPages.php
@@ -9,6 +9,7 @@
 namespace MediaWiki\Api;
 
 use MediaWiki\Cache\GenderCache;
+use MediaWiki\Deferred\LinksUpdate\LangLinksTable;
 use MediaWiki\MainConfigNames;
 use MediaWiki\Permissions\RestrictionStore;
 use MediaWiki\Title\NamespaceInfo;
@@ -62,46 +63,12 @@ class ApiQueryAllPages extends ApiQueryGeneratorBase {
 	 */
 	private function run( $resultPageSet = null ) {
 		$db = $this->getDB();
+		$this->setVirtualDomain( LangLinksTable::VIRTUAL_DOMAIN );
+		$langLinksDb = $this->getDB();
 
 		$params = $this->extractRequestParams();
 
-		// Page filters
-		$this->addTables( 'page' );
-
-		if ( $params['continue'] !== null ) {
-			$cont = $this->parseContinueParamOrDie( $params['continue'], [ 'string' ] );
-			$op = $params['dir'] == 'descending' ? '<=' : '>=';
-			$this->addWhere( $db->expr( 'page_title', $op, $cont[0] ) );
-		}
-
 		$miserMode = $this->getConfig()->get( MainConfigNames::MiserMode );
-		if ( !$miserMode ) {
-			if ( $params['filterredir'] == 'redirects' ) {
-				$this->addWhereFld( 'page_is_redirect', 1 );
-			} elseif ( $params['filterredir'] == 'nonredirects' ) {
-				$this->addWhereFld( 'page_is_redirect', 0 );
-			}
-		}
-
-		$this->addWhereFld( 'page_namespace', $params['namespace'] );
-		$dir = ( $params['dir'] == 'descending' ? 'older' : 'newer' );
-		$from = ( $params['from'] === null
-			? null
-			: $this->titlePartToKey( $params['from'], $params['namespace'] ) );
-		$to = ( $params['to'] === null
-			? null
-			: $this->titlePartToKey( $params['to'], $params['namespace'] ) );
-		$this->addWhereRange( 'page_title', $dir, $from, $to );
-
-		if ( isset( $params['prefix'] ) ) {
-			$this->addWhere(
-				$db->expr(
-					'page_title',
-					IExpression::LIKE,
-					new LikeValue( $this->titlePartToKey( $params['prefix'], $params['namespace'] ), $db->anyString() )
-				)
-			);
-		}
 
 		if ( $resultPageSet === null ) {
 			$selectFields = [
@@ -125,87 +92,204 @@ class ApiQueryAllPages extends ApiQueryGeneratorBase {
 			}
 		}
 
-		$this->addFields( $selectFields );
-		$forceNameTitleIndex = true;
-		if ( isset( $params['minsize'] ) ) {
-			$this->addWhere( 'page_len>=' . (int)$params['minsize'] );
-			$forceNameTitleIndex = false;
-		}
-
-		if ( !$miserMode && isset( $params['maxsize'] ) ) {
-			$this->addWhere( 'page_len<=' . (int)$params['maxsize'] );
-			$forceNameTitleIndex = false;
-		}
-
-		// Page protection filtering
-		if ( $params['prtype'] || $params['prexpiry'] != 'all' ) {
-			$this->addTables( 'page_restrictions' );
-			$this->addWhere( 'page_id=pr_page' );
-			$this->addWhere(
-				$db->expr( 'pr_expiry', '>', $db->timestamp() )->or( 'pr_expiry', '=', null )
-			);
-
-			if ( $params['prtype'] ) {
-				$this->addWhereFld( 'pr_type', $params['prtype'] );
-
-				if ( isset( $params['prlevel'] ) ) {
-					// Remove the empty string and '*' from the prlevel array
-					$prlevel = array_diff( $params['prlevel'], [ '', '*' ] );
-
-					if ( count( $prlevel ) ) {
-						$this->addWhereFld( 'pr_level', $prlevel );
-					}
-				}
-				if ( $params['prfiltercascade'] == 'cascading' ) {
-					$this->addWhereFld( 'pr_cascade', 1 );
-				} elseif ( $params['prfiltercascade'] == 'noncascading' ) {
-					$this->addWhereFld( 'pr_cascade', 0 );
-				}
-			}
-			$forceNameTitleIndex = false;
-
-			if ( $params['prexpiry'] == 'indefinite' ) {
-				$this->addWhereFld( 'pr_expiry', [ $db->getInfinity(), null ] );
-			} elseif ( $params['prexpiry'] == 'definite' ) {
-				$this->addWhere( $db->expr( 'pr_expiry', '!=', $db->getInfinity() ) );
-			}
-
-			$this->addOption( 'DISTINCT' );
-		} elseif ( isset( $params['prlevel'] ) ) {
-			$this->dieWithError(
-				[ 'apierror-invalidparammix-mustusewith', 'prlevel', 'prtype' ], 'invalidparammix'
-			);
-		}
-
-		if ( $params['filterlanglinks'] == 'withoutlanglinks' ) {
-			$this->addTables( 'langlinks' );
-			$this->addJoinConds( [ 'langlinks' => [ 'LEFT JOIN', 'page_id=ll_from' ] ] );
-			$this->addWhere( [ 'll_from' => null ] );
-			$forceNameTitleIndex = false;
-		} elseif ( $params['filterlanglinks'] == 'withlanglinks' ) {
-			$this->addTables( 'langlinks' );
-			$this->addWhere( 'page_id=ll_from' );
-			$this->addOption( 'STRAIGHT_JOIN' );
-
-			$dbType = $db->getType();
-			if ( $dbType === 'mysql' || $dbType === 'sqlite' ) {
-				// Avoid MySQL filesort reported in 2015 (T78276)
-				$this->addOption( 'GROUP BY', [ 'page_title' ] );
-			} else {
-				// SQL:1999 rules only counting primary keys
-				$this->addOption( 'GROUP BY', [ 'page_title', 'page_id' ] );
-			}
-
-			$forceNameTitleIndex = false;
-		}
-
-		if ( $forceNameTitleIndex ) {
-			$this->addOption( 'USE INDEX', 'page_name_title' );
-		}
+		$forceNameTitleIndex = !(
+			isset( $params['minsize'] ) ||
+			( !$miserMode && isset( $params['maxsize'] ) ) ||
+			( $params['prtype'] || $params['prexpiry'] != 'all' )
+		);
 
 		$limit = $params['limit'];
-		$this->addOption( 'LIMIT', $limit + 1 );
-		$res = $this->select( __METHOD__ );
+
+		// Use loop-based query to handle langlinks filtering separately
+		// (langlinks table is in a different cluster)
+		$filteredRows = [];
+		$pagesWithLangLinks = [];
+		$filterWithLangLinks = $params['filterlanglinks'] == 'withlanglinks';
+		$needLangLinksFilter = $params['filterlanglinks'] !== 'all';
+
+		$continueFrom = null;
+		if ( $params['continue'] !== null ) {
+			$cont = $this->parseContinueParamOrDie( $params['continue'], [ 'string' ] );
+			$continueFrom = $cont[0];
+		}
+
+		$isFirstBatch = true;
+		$loopCount = 0;
+		$maxLoops = 20;
+		while ( count( $filteredRows ) < $limit + 1 ) {
+			if ( $loopCount > $maxLoops ) {
+				// Safety limit to prevent excessive iterations
+				break;
+			}
+
+			$queryBuilder = $db->newSelectQueryBuilder()
+				->select( $selectFields )
+				->from( 'page' )
+				->where( [ 'page_namespace' => $params['namespace'] ] )
+				->caller( __METHOD__ );
+
+			if ( $continueFrom !== null ) {
+				// Use strict comparison for subsequent batches to skip the continue row
+				if ( $isFirstBatch ) {
+					$op = $params['dir'] == 'descending' ? '<=' : '>=';
+				} else {
+					$op = $params['dir'] == 'descending' ? '<' : '>';
+				}
+				$queryBuilder->andWhere( $db->expr( 'page_title', $op, $continueFrom ) );
+			}
+
+			$dir = ( $params['dir'] == 'descending' ? 'older' : 'newer' );
+			$from = ( $params['from'] === null
+				? null
+				: $this->titlePartToKey( $params['from'], $params['namespace'] ) );
+			$to = ( $params['to'] === null
+				? null
+				: $this->titlePartToKey( $params['to'], $params['namespace'] ) );
+
+			if ( $dir === 'newer' ) {
+				if ( $from !== null ) {
+					$queryBuilder->andWhere( $db->expr( 'page_title', '>=', $from ) );
+				}
+				if ( $to !== null ) {
+					$queryBuilder->andWhere( $db->expr( 'page_title', '<=', $to ) );
+				}
+			} else {
+				if ( $from !== null ) {
+					$queryBuilder->andWhere( $db->expr( 'page_title', '<=', $from ) );
+				}
+				if ( $to !== null ) {
+					$queryBuilder->andWhere( $db->expr( 'page_title', '>=', $to ) );
+				}
+			}
+
+			if ( isset( $params['prefix'] ) ) {
+				$queryBuilder->andWhere(
+					$db->expr(
+						'page_title',
+						IExpression::LIKE,
+						new LikeValue(
+							$this->titlePartToKey( $params['prefix'], $params['namespace'] ),
+							$db->anyString()
+						)
+					)
+				);
+			}
+
+			if ( !$miserMode ) {
+				if ( $params['filterredir'] == 'redirects' ) {
+					$queryBuilder->andWhere( [ 'page_is_redirect' => 1 ] );
+				} elseif ( $params['filterredir'] == 'nonredirects' ) {
+					$queryBuilder->andWhere( [ 'page_is_redirect' => 0 ] );
+				}
+			}
+
+			if ( isset( $params['minsize'] ) ) {
+				$queryBuilder->andWhere( $db->expr( 'page_len', '>=', (int)$params['minsize'] ) );
+			}
+
+			if ( !$miserMode && isset( $params['maxsize'] ) ) {
+				$queryBuilder->andWhere( $db->expr( 'page_len', '<=', (int)$params['maxsize'] ) );
+			}
+
+			// Page protection filtering
+			if ( $params['prtype'] || $params['prexpiry'] != 'all' ) {
+				$queryBuilder->join( 'page_restrictions', null, 'page_id=pr_page' );
+				$queryBuilder->andWhere(
+					$db->expr( 'pr_expiry', '>', $db->timestamp() )->or( 'pr_expiry', '=', null )
+				);
+
+				if ( $params['prtype'] ) {
+					$queryBuilder->andWhere( [ 'pr_type' => $params['prtype'] ] );
+
+					if ( isset( $params['prlevel'] ) ) {
+						$prlevel = array_diff( $params['prlevel'], [ '', '*' ] );
+						if ( count( $prlevel ) ) {
+							$queryBuilder->andWhere( [ 'pr_level' => $prlevel ] );
+						}
+					}
+					if ( $params['prfiltercascade'] == 'cascading' ) {
+						$queryBuilder->andWhere( [ 'pr_cascade' => 1 ] );
+					} elseif ( $params['prfiltercascade'] == 'noncascading' ) {
+						$queryBuilder->andWhere( [ 'pr_cascade' => 0 ] );
+					}
+				}
+
+				if ( $params['prexpiry'] == 'indefinite' ) {
+					$queryBuilder->andWhere( [ 'pr_expiry' => [ $db->getInfinity(), null ] ] );
+				} elseif ( $params['prexpiry'] == 'definite' ) {
+					$queryBuilder->andWhere( $db->expr( 'pr_expiry', '!=', $db->getInfinity() ) );
+				}
+
+				$queryBuilder->distinct();
+			} elseif ( isset( $params['prlevel'] ) ) {
+				$this->dieWithError(
+					[ 'apierror-invalidparammix-mustusewith', 'prlevel', 'prtype' ], 'invalidparammix'
+				);
+			}
+
+			if ( $forceNameTitleIndex ) {
+				$queryBuilder->useIndex( 'page_name_title' );
+			}
+
+			$sort = ( $params['dir'] == 'descending' ? ' DESC' : '' );
+			$queryBuilder->orderBy( 'page_title' . $sort );
+			$queryBuilder->limit( $limit + 1 );
+
+			$res = $queryBuilder->fetchResultSet();
+
+			$isFirstBatch = false;
+
+			$batchRows = [];
+			$pageIds = [];
+
+			foreach ( $res as $row ) {
+				$batchRows[] = $row;
+				$pageIds[] = $row->page_id;
+			}
+
+			if ( $pageIds === [] ) {
+				// No more rows available
+				break;
+			}
+
+			if ( $needLangLinksFilter ) {
+				$langLinksQueryBuilder = $langLinksDb->newSelectQueryBuilder()
+					->select( 'll_from' )
+					->from( 'langlinks' )
+					->where( [ 'll_from' => $pageIds ] )
+					->caller( __METHOD__ );
+
+				$langLinksRes = $langLinksQueryBuilder->fetchResultSet();
+
+				foreach ( $langLinksRes as $langLinksRow ) {
+					$pagesWithLangLinks[$langLinksRow->ll_from] = true;
+				}
+			}
+
+			foreach ( $batchRows as $row ) {
+				if ( $needLangLinksFilter ) {
+					$hasLangLinks = isset( $pagesWithLangLinks[$row->page_id] );
+					if ( $filterWithLangLinks === $hasLangLinks ) {
+						$filteredRows[] = $row;
+					}
+				} else {
+					$filteredRows[] = $row;
+				}
+			}
+
+			if ( count( $batchRows ) < $limit + 1 ) {
+				break;
+			}
+
+			$lastRow = end( $batchRows );
+			$continueFrom = $lastRow->page_title;
+
+			$loopCount++;
+		}
+
+		$this->resetVirtualDomain();
+
+		$res = $filteredRows;
 
 		// Get gender information
 		if ( $this->namespaceInfo->hasGenderDistinction( $params['namespace'] ) ) {
@@ -214,7 +298,6 @@ class ApiQueryAllPages extends ApiQueryGeneratorBase {
 				$users[] = $row->page_title;
 			}
 			$this->genderCache->doQuery( $users, __METHOD__ );
-			$res->rewind(); // reset
 		}
 
 		$count = 0;
