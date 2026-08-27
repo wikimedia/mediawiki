0054ca7dbd23117f8a89db9458d2bac5398480b3
Jon Robson <jdlrobson@gmail.com>
2026-08-27 09:29:10 -0600

    Add vue-router to core for use by extensions/skins
    Bug: T290916
    Bug: T436156
    Change-Id: I429c25e60ff5011bbdde4777be2bde3fe5cc6081

--- resources/Resources.php
+++ resources/Resources.php
@@ -607,6 +607,34 @@ return [
 		]
 	],
 
+	'vue-router' => [
+		'packageFiles' => [
+			[
+				'name' => 'resources/lib/vue-router/vue-router.js',
+				'callback' => static function ( Context $context, Config $config ) {
+					// Use the development version if development mode is enabled, or if we're in debug mode
+					$file = $config->get( MainConfigNames::VueDevelopmentMode ) || $context->getDebug() ?
+						'resources/lib/vue-router/vue-router.global.js' :
+						'resources/lib/vue-router/vue-router.global.prod.js';
+					// The file shipped by Vue Router does var VueRouter = ...;, but doesn't export it
+					// Add module.exports = VueRouter; programmatically, and import Vue
+					return "var Vue=require('vue');" .
+						file_get_contents( MW_INSTALL_PATH . "/$file" ) .
+						';module.exports=VueRouter;';
+				},
+				'versionCallback' => static function ( Context $context, Config $config ) {
+					$file = $config->get( MainConfigNames::VueDevelopmentMode ) || $context->getDebug() ?
+						'resources/lib/vue-router/vue-router.global.js' :
+						'resources/lib/vue-router/vue-router.global.prod.js';
+					return new FilePath( $file );
+				}
+			],
+		],
+		'dependencies' => [
+			'vue',
+		],
+	],
+
 	'vuex' => [
 		'packageFiles' => [
 			[
