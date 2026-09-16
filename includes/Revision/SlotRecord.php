5b3e7f6f67705eb6959f4e3a800bf8d55383feda
Umherirrender <umherirrender_de.wp@web.de>
2026-09-16 22:09:38 +0200

    Revision: Use namespaced MediaWiki\Content\Content class
    Change-Id: I004e42b790e33e44a12d821c4b20be0ebcc9afde

--- includes/Revision/SlotRecord.php
+++ includes/Revision/SlotRecord.php
@@ -239,7 +239,7 @@ class SlotRecord {
 	 *        in revision history.
 	 */
 	public function __construct( \stdClass $row, $content, bool $derived = false ) {
-		Assert::parameterType( [ 'Content', 'callable' ], $content, '$content' );
+		Assert::parameterType( [ Content::class, 'callable' ], $content, '$content' );
 
 		Assert::parameter(
 			property_exists( $row, 'slot_revision_id' ),
