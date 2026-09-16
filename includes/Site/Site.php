5745ddf43675c768591dbd1e5409747a29318d03
Adam Wight <adam.wight@wikimedia.de>
2026-09-16 14:18:18 +0200

    Deprecate Site `forward` and `config`
    These are never set or meaningfully used in production, we can
    deprecate and then remove support.
    
    Bug: T437691
    Change-Id: I137968010b48fd31bf4d2683740ad39d12c484a5

--- includes/Site/Site.php
+++ includes/Site/Site.php
@@ -179,6 +179,7 @@ class Site {
 	/**
 	 * Returns the source of the site data (ie 'local', 'wikidata', 'my-magical-repo').
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 *
 	 * @return string
@@ -190,6 +191,7 @@ class Site {
 	/**
 	 * Sets the source of the site data (ie 'local', 'wikidata', 'my-magical-repo').
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 * @param string $source
 	 */
@@ -201,6 +203,7 @@ class Site {
 	 * Gets if site.tld/path/key:pageTitle should forward users to  the page on
 	 * the actual site, where "key" is the local identifier.
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 *
 	 * @return bool
@@ -213,6 +216,7 @@ class Site {
 	 * Sets if site.tld/path/key:pageTitle should forward users to  the page on
 	 * the actual site, where "key" is the local identifier.
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 * @param bool $shouldForward
 	 */
@@ -388,6 +392,7 @@ class Site {
 	/**
 	 * Returns the type specific config.
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 *
 	 * @return array
@@ -399,6 +404,7 @@ class Site {
 	/**
 	 * Sets the type specific config.
 	 *
+	 * @deprecated since 1.47
 	 * @since 1.21
 	 *
 	 * @param array $extraConfig
