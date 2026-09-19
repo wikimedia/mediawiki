1040e9e79c6712c501cdfe779153cc1fde09ba9b
Timo Tijhof <krinkle@fastmail.com>
2026-09-19 22:36:46 +0100

    Skin: Simplify useCombinedLoginLink logic
    Bug: T302623
    Change-Id: I50e9f69d6c58a536ba1090a89cd872dde8c915d2

--- includes/Skin/SkinTemplate.php
+++ includes/Skin/SkinTemplate.php
@@ -562,27 +562,25 @@ class SkinTemplate extends Skin {
 	}
 
 	/**
-	 * Returns if a combined login/signup link will be used
-	 * @unstable
+	 * Whether a combined login/signup link should be used
 	 *
+	 * @unstable
 	 * @return bool
 	 */
 	protected function useCombinedLoginLink() {
 		$useCombinedLoginLink = $this->getConfig()->get( MainConfigNames::UseCombinedLoginLink );
 
-		if ( !$useCombinedLoginLink ) {
-			// Optimization: Return early and skip AuthManager checks if the combined link is disabled anyway
-			return false;
+		// Optimization: Skip AuthManager checks if the combined link is disabled (T302623)
+		if ( $useCombinedLoginLink ) {
+			$authManager = MediaWikiServices::getInstance()->getAuthManager();
+			if ( $authManager->canCreateAccounts() && $authManager->canAuthenticateNow() ) {
+				// Only replace login/signup with a single combined link, if both are actually
+				// available. Otherwise addAccountLinks() wouldn't output both anyway.
+				return true;
+			}
 		}
 
-		$services = MediaWikiServices::getInstance();
-		$authManager = $services->getAuthManager();
-		if ( !$authManager->canCreateAccounts() || !$authManager->canAuthenticateNow() ) {
-			// don't show combined login/signup link if one of those is actually not available
-			$useCombinedLoginLink = false;
-		}
-
-		return $useCombinedLoginLink;
+		return false;
 	}
 
 	/**
